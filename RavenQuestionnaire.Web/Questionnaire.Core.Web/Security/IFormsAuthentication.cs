c3bae1b21d68a179506968163e57ec1a741188ce
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2011-12-19 15:41:25 -0500

    [*] layout update
    Former-commit-id: 2d5f91245a270ed6b23fbb197e69327f47db0b58

---
+++ RavenQuestionnaire.Web/Questionnaire.Core.Web/Security/IFormsAuthentication.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Questionnaire.Core.Web.Membership
+{
+    public interface IFormsAuthentication
+    {
+        void SignIn(string userName, bool rememberMe);
+        void SignOut();
+        string GetUserIdForCurrentUser();
+    }
+}
