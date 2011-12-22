89ec1170e1beb78821522eed290901164d7547fb
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2011-12-22 15:38:46 -0500

    [*] Fix consistency
    Former-commit-id: f8ed2ac21f211d334b851271ddeba08686ae945a

---
+++ RavenQuestionnaire.Web/RavenQuestionnaire.Web/Controllers/AdministrationController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace RavenQuestionnaire.Web.Controllers
+{
+    public class AdministrationController : Controller
+    {
+        //
+        // GET: /Administration/
+
+        public ActionResult Menu()
+        {
+            return View();
+        }
+
+    }
+}
