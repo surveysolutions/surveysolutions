66d490442cb543f23f6576c9e0ed77f5d556b7f9
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2012-01-19 14:28:21 -0500

    partially inplemented auto handling of statuses
    Former-commit-id: 43a5f54264f00c4b2574b88be1a35c4026c229fb

--- RavenQuestionnaire.Core/RavenQuestionnaire.Core/Entities/Iterators/QuestionnaireSimpleIterator.cs
+++ RavenQuestionnaire.Core/RavenQuestionnaire.Core/Entities/Iterators/QuestionnaireSimpleIterator.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using RavenQuestionnaire.Core.Entities.SubEntities;
 using RavenQuestionnaire.Core.Entities.SubEntities.Complete;
 using RavenQuestionnaire.Core.ExpressionExecutors;
 
---
+++ RavenQuestionnaire.Core/RavenQuestionnaire.Core/Views/Status/Processing/StatusProcessViewFactory.cs
@@ -0,0 +1,25 @@
+﻿using Raven.Client;
+using RavenQuestionnaire.Core.Documents;
+
+namespace RavenQuestionnaire.Core.Views.Status.Processing
+{
+    public class StatusProcessViewFactory: IViewFactory<StatusProcessViewInputModel, StatusProcessView>
+    {
+        private IDocumentSession documentSession;
+
+
+        public StatusProcessViewFactory(IDocumentSession documentSession)
+        {
+            this.documentSession = documentSession;
+        }
+
+        public StatusProcessView Load(StatusProcessViewInputModel input)
+        {
+            StatusDocument doc = documentSession.Load<StatusDocument>(input.StatusId);
+
+            return doc == null ? null
+                : new StatusProcessView(doc.Id, doc.Title, doc.IsVisible, doc.FlowRules, 
+                    doc.DefaultIfNoConditions, doc.QuestionnaireId);
+        }
+    }
+}
