2d84097f34181281b1cac538f08223dfb4012f0f
Anastasiya Kholodova <nastya.kholodova@gmail.com>
2012-02-03 12:57:52 +0530

    binded question
    Former-commit-id: 05a8ef03b09acf5d28790d79f02aa9ab318ed0fb

---
+++ RavenQuestionnaire.Core/RavenQuestionnaire.Core/Entities/SubEntities/BindedQuestion.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+using RavenQuestionnaire.Core.Entities.Composite;
+using RavenQuestionnaire.Core.Entities.SubEntities.Complete;
+
+namespace RavenQuestionnaire.Core.Entities.SubEntities
+{
+    public class BindedQuestion : Question, IBinded
+    {
+        public BindedQuestion()
+        {
+            this.template=new Question();
+            PublicKey = Guid.NewGuid();
+        }
+        public BindedQuestion(IQuestion<Answer> template)
+        {
+            this.template=template;
+        }
+
+        private readonly IQuestion<Answer> template;
+
+        public Guid PublicKey { get; set; }
+
+        public Guid ParentPublicKey
+        {
+            get { return template.PublicKey; }
+            set { template.PublicKey = value; }
+        }
+
+        [XmlIgnore]
+        public string QuestionText
+        {
+            get { return template.QuestionText; }
+            set { throw new InvalidOperationException("question text can't be changed at binded question");}
+        }
+        [XmlIgnore]
+        public QuestionType QuestionType
+        {
+            get { return template.QuestionType; }
+            set { throw new InvalidOperationException("QuestionType can't be changed at binded question"); }
+        }
+        [XmlIgnore]
+        public List<Answer> Answers
+        {
+            get { return template.Answers; }
+            set { throw new InvalidOperationException("Answers can't be changed at binded question"); }
+        }
+        [XmlIgnore]
+        public string ConditionExpression
+        {
+            get { return template.ConditionExpression; }
+            set { throw new InvalidOperationException("ConditionExpression can't be changed at binded question"); }
+        }
+        [XmlIgnore]
+        public string StataExportCaption
+        {
+            get { return template.StataExportCaption; }
+            set { throw new InvalidOperationException("StataExportCaption can't be changed at binded question"); }
+        }
+
+        public void Add(IComposite c, Guid? parent)
+        {
+            throw new CompositeException();
+        }
+
+        public void Remove(IComposite c)
+        {
+            throw new CompositeException();
+        }
+        public void Remove<T>(Guid publicKey) where T : class, IComposite
+        {
+            throw new CompositeException();
+        }
+
+        public T Find<T>(Guid publicKey) where T : class, IComposite
+        {
+            return null;
+        }
+    }
+}
---
+++ RavenQuestionnaire.Core/RavenQuestionnaire.Core/Entities/SubEntities/Complete/BindedCompleteQuestion.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+using RavenQuestionnaire.Core.Entities.Composite;
+
+namespace RavenQuestionnaire.Core.Entities.SubEntities.Complete
+{
+    public class BindedCompleteQuestion : ICompleteQuestion<CompleteAnswer>, IBinded
+    {
+        public BindedCompleteQuestion()
+        {
+            this.template = new CompleteQuestion();
+            PublicKey = Guid.NewGuid();
+        }
+
+        public BindedCompleteQuestion(ICompleteQuestion<CompleteAnswer> template)
+        {
+            this.template=template;
+        }
+        public static explicit operator BindedCompleteQuestion(RavenQuestionnaire.Core.Entities.SubEntities.BindedQuestion doc)
+        {
+            BindedCompleteQuestion result = new BindedCompleteQuestion
+            {
+                ParentPublicKey = doc.ParentPublicKey,
+                PublicKey = doc.PublicKey,
+                ConditionExpression = doc.ConditionExpression,
+                QuestionText = doc.QuestionText,
+                QuestionType = doc.QuestionType,
+                StataExportCaption = doc.StataExportCaption
+            };
+            result.Answers = doc.Answers.Select(a => (CompleteAnswer)a).ToList();
+            return result;
+        }
+        private readonly ICompleteQuestion<CompleteAnswer> template;
+        #region Implementation of IComposite
+
+        public void Add(IComposite c, Guid? parent)
+        {
+            throw new CompositeException();
+        }
+
+        public void Remove(IComposite c)
+        {
+            throw new CompositeException();
+        }
+
+        public void Remove<T>(Guid publicKey) where T : class, IComposite
+        {
+            throw new CompositeException();
+        }
+
+        public T Find<T>(Guid publicKey) where T : class, IComposite
+        {
+            return null;
+        }
+
+        #endregion
+
+        #region Implementation of IQuestion
+
+        public Guid PublicKey { get; set; }
+        [XmlIgnore]
+        public string QuestionText
+        {
+            get { return template.QuestionText; }
+            set { throw new InvalidOperationException("question text can't be changed at binded question"); }
+        }
+        [XmlIgnore]
+        public QuestionType QuestionType
+        {
+            get { return template.QuestionType; }
+            set { throw new InvalidOperationException("QuestionType can't be changed at binded question"); }
+        }
+        [XmlIgnore]
+        public string ConditionExpression
+        {
+            get { return template.QuestionText; }
+            set { throw new InvalidOperationException("ConditionExpression can't be changed at binded question"); }
+        }
+        [XmlIgnore]
+        public string StataExportCaption
+        {
+            get { return template.QuestionText; }
+            set { throw new InvalidOperationException("StataExportCaption can't be changed at binded question"); }
+        }
+
+        #endregion
+
+        #region Implementation of ICompleteQuestion
+        [XmlIgnore]
+        public bool Enabled
+        {
+            get { return false; }
+            set { throw new InvalidOperationException("StataExportCaption can't be changed at binded question"); }
+        }
+
+        #endregion
+
+        #region Implementation of IQuestion<CompleteAnswer>
+
+        [XmlIgnore]
+        public List<CompleteAnswer> Answers
+        {
+            get { return template.Answers; }
+            set { throw new InvalidOperationException("Answers can't be changed at binded question"); }
+        }
+
+        #endregion
+
+        #region Implementation of IBinded
+
+        public Guid ParentPublicKey
+        {
+            get { return template.PublicKey; }
+            set { template.PublicKey = value; }
+        }
+
+        #endregion
+    }
+}
