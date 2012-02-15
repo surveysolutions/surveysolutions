4e654cb1f10c66b3489c078192f630a1375fdaa2
Anastasiya Kholodova <nastya.kholodova@gmail.com>
2012-02-15 18:49:02 +0530

    reactive extensions
    Former-commit-id: 3f0f9adb723ec405b1c00f3106f8a8b5c75ff9f8

---
+++ RavenQuestionnaire.Web/RavenQuestionnaire.Core.Tests/Entities/GroupTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Text;
+using NUnit.Framework;
+using RavenQuestionnaire.Core.Entities.Composite;
+using RavenQuestionnaire.Core.Entities.SubEntities;
+
+namespace RavenQuestionnaire.Core.Tests.Entities
+{
+    [TestFixture]
+    public class GroupTest
+    {
+        [Test]
+        public void ObserveChanges_All_ObserversNotified()
+        {
+            Group target = new Group("under test");
+            var test = false;
+            //   Observable.FromEventPattern<CompositeAddedEventArgs>(target.s)
+            var observer =
+                target.Where(e => e is CompositeAddedEventArgs).Select(e => e as CompositeAddedEventArgs).Where(e=>e.AddedComposite is Question).Subscribe(
+                    Observer.Create<CompositeAddedEventArgs>(
+                        (a) => { Assert.AreEqual(((Question)a.AddedComposite).QuestionText, "se");
+                                   test = true;
+                        }));
+            target.Add(new Question("se", QuestionType.Text), null);
+            Assert.IsTrue(test);
+            target.Add(new Group("test"), null);
+            observer.Dispose();
+            target.Add(new Question("sea", QuestionType.Text), null);
+            test = false;
+            var observerRemove =
+                target.Where(e => e is CompositeRemovedEventArgs).Select(e => e as CompositeRemovedEventArgs).Subscribe(
+                    Observer.Create<CompositeRemovedEventArgs>(
+                        (a) => { Assert.IsNotNull(a);
+                                   test = true;
+                        }));
+            target.Remove(target.Groups[0]);
+            Assert.IsTrue(test);
+            //target.Subscribe()
+        }
+
+        [Test]
+        public void ObserveChanges_InnerObjects_ObserversNotified()
+        {
+            Group target = new Group("under test");
+            var test = false;
+            //   Observable.FromEventPattern<CompositeAddedEventArgs>(target.s)
+            var observer =
+                target.Where(e => e is CompositeAddedEventArgs).Select(e => e as CompositeAddedEventArgs).Where(e => e.AddedComposite is Question).Subscribe(
+                    Observer.Create<CompositeAddedEventArgs>(
+                        (a) =>
+                        {
+                            Assert.AreEqual(((Question)a.AddedComposite).QuestionText, "se");
+                            test = true;
+                        }));
+            target.Add(new Group("test"), null);
+            target.Add(new Question("se", QuestionType.Text), target.Groups[0].PublicKey);
+            Assert.IsTrue(test);
+        }
+    }
+}
