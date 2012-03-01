ee8e21148569d2fc7649b167b4b8a965e10868d4
Dmitry Sverbilov <dsverbilov@dbbest.com>
2012-03-01 20:32:19 +0200

    [+] Add Vertical Complete Questionnaire view (QuestionV)
    Former-commit-id: 32527cb5e849145a79b11ca192d6121339780d1b

---
+++ RavenQuestionnaire.Core/RavenQuestionnaire.Core/Views/CompleteQuestionnaire/Vertical/CompleteQuestionnaireEnumerableViewVFactory.cs
@@ -0,0 +1,62 @@
+﻿#region
+
+using System.Linq;
+using Raven.Client;
+using RavenQuestionnaire.Core.Documents;
+using RavenQuestionnaire.Core.Entities.Iterators;
+using RavenQuestionnaire.Core.Entities.SubEntities.Complete;
+
+#endregion
+
+namespace RavenQuestionnaire.Core.Views.CompleteQuestionnaire.Vertical
+{
+    public class CompleteQuestionnaireEnumerableViewFactoryV :
+        IViewFactory<CompleteQuestionnaireViewInputModel, CompleteQuestionnaireViewV>
+    {
+        private readonly IDocumentSession documentSession;
+
+        public CompleteQuestionnaireEnumerableViewFactoryV(IDocumentSession documentSession)
+        {
+            this.documentSession = documentSession;
+        }
+
+        #region IViewFactory<CompleteQuestionnaireViewInputModel,CompleteQuestionnaireViewV> Members
+
+        public CompleteQuestionnaireViewV Load(CompleteQuestionnaireViewInputModel input)
+        {
+            if (!string.IsNullOrEmpty(input.CompleteQuestionnaireId))
+            {
+                var doc = documentSession.Load<CompleteQuestionnaireDocument>(input.CompleteQuestionnaireId);
+                var completeQuestionnaireRoot = new Entities.CompleteQuestionnaire(doc);
+                ICompleteGroup group = null;
+
+                Iterator<ICompleteGroup> iterator =
+                    new QuestionnaireScreenIterator(doc);
+                if (input.CurrentGroupPublicKey.HasValue)
+                {
+                    group = completeQuestionnaireRoot.Find<CompleteGroup>(input.CurrentGroupPublicKey.Value);
+                }
+                else if (input.PreviousGroupPublicKey.HasValue)
+                {
+                    iterator.SetCurrent(completeQuestionnaireRoot.Find<CompleteGroup>(input.PreviousGroupPublicKey.Value));
+                    group = input.IsReverse
+                                ? iterator.Previous
+                                : iterator.Next;
+                }
+                else
+                {
+                    group = input.IsReverse ? iterator.Last() : iterator.First();
+                }
+                return new CompleteQuestionnaireViewV(doc, group);
+            }
+            if (!string.IsNullOrEmpty(input.TemplateQuestionanireId))
+            {
+                var doc = documentSession.Load<QuestionnaireDocument>(input.TemplateQuestionanireId);
+                return new CompleteQuestionnaireViewV((CompleteQuestionnaireDocument)doc);
+            }
+            return null;
+        }
+
+        #endregion
+    }
+}
---
+++ RavenQuestionnaire.Core/RavenQuestionnaire.Core/Views/CompleteQuestionnaire/Vertical/CompleteQuestionnaireViewEnumerable.cs
@@ -0,0 +1,134 @@
+﻿#region
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using RavenQuestionnaire.Core.AbstractFactories;
+using RavenQuestionnaire.Core.Documents;
+using RavenQuestionnaire.Core.Entities.SubEntities;
+using RavenQuestionnaire.Core.Entities.SubEntities.Complete;
+using RavenQuestionnaire.Core.Views.Question;
+
+#endregion
+
+namespace RavenQuestionnaire.Core.Views.CompleteQuestionnaire.Vertical
+{
+    public class CompleteGroupHeaders
+    {
+        public Guid PublicKey { get; set; }
+
+        public string GroupText { get; set; }
+
+        public bool IsCurrent { get; set; }
+    }
+
+    public class CompleteGroupViewV
+    {
+        public CompleteGroupViewV()
+        {
+            Questions = new List<CompleteQuestionView>();
+            Groups = new List<CompleteGroupViewV>();
+            Propagated = Propagate.None;
+            PropagatedQuestions = new List<PropagatedQuestion>();
+            AutoPropagate = false;
+        }
+        public CompleteGroupViewV(CompleteQuestionnaireDocument doc, CompleteGroup currentGroup)
+            : this()
+        {
+            PublicKey = currentGroup.PublicKey;
+            GroupText = currentGroup.Title;
+
+            if (currentGroup.Questions.Count > 0)
+            {
+                var questionQgroup = new CompleteGroupViewV();
+                questionQgroup.Questions = currentGroup.Questions.Select(q => new CompleteQuestionFactory().CreateQuestion(doc, currentGroup, q)).ToList();
+                questionQgroup.PublicKey = Guid.Empty;
+                questionQgroup.GroupText = "Main";
+                Groups.Add(questionQgroup);
+            }
+            // grouping by group's PublicKey
+            var propGroups = new Dictionary<Guid, List<CompleteGroup>>();
+            foreach (var @group in currentGroup.Groups)
+            {
+                if (!propGroups.ContainsKey(@group.PublicKey))
+                {
+                    propGroups.Add(@group.PublicKey, new List<CompleteGroup>());
+                }
+                propGroups[@group.PublicKey].Add(@group as CompleteGroup);
+            }
+            foreach (var k in propGroups)
+            {
+                var prop = Propagate.Propagated;
+                if (k.Value.Count == 1 && (k.Value[0] as PropagatableCompleteGroup) == null)
+                {
+                    prop = Propagate.None;
+                }
+                Groups.Add(prop != Propagate.Propagated
+                               ? new CompleteGroupViewV(doc, k.Value[0] as CompleteGroup)
+                               : new CompleteGroupViewV(doc, k.Value));
+            }
+        }
+        public CompleteGroupViewV(CompleteQuestionnaireDocument doc, List<CompleteGroup> propGroups)
+            : this()
+        {
+            var propagatable = propGroups.Single(g => (g as PropagatableCompleteGroup) == null);
+            var qf = new CompleteQuestionFactory();
+            PublicKey = propagatable.PublicKey;
+            GroupText = propagatable.Title;
+            Propagated = Propagate.Propagated;
+
+            var propagated = propGroups.Where(g => g != propagatable).Select(g => g as PropagatableCompleteGroup).ToList();
+            if (propagated.Count > 0)
+            {
+                AutoPropagate = propagated[0].AutoPropagate;
+                PropogationPublicKeys = propagated.Select(g => g.PropogationPublicKey).ToList();
+            }
+            for (int i = 0; i < propagatable.Questions.Count; i++)
+            {
+                var question = propagatable.Questions[i];
+                var pq = new PropagatedQuestion
+                             {
+                                 PublicKey = question.PublicKey,
+                                 QuestionText = question.QuestionText,
+                                 Questions = new List<CompleteQuestionView>()
+                                 
+                             };
+
+                foreach (var p in propagated)
+                {
+                    pq.Questions.Add(qf.CreateQuestion(doc, p, p.Questions[i]));
+                }
+                PropagatedQuestions.Add(pq);
+            }
+           
+        }
+
+        public Guid PublicKey { get; set; }
+
+        public bool AutoPropagate { get; set; }
+
+        public string GroupText { get; set; }
+
+        public Propagate Propagated { get; set; }
+
+        public List<Guid> PropogationPublicKeys { get; set; }
+
+        public List<CompleteQuestionView> Questions { get; set; }
+
+        public List<CompleteGroupViewV> Groups { get; set; }
+
+        public List<PropagatedQuestion> PropagatedQuestions { get; set; }
+
+        public virtual string GetClientId(string prefix)
+        {
+            return string.Format("{0}_{1}", prefix, PublicKey);
+        }
+    }
+    public class PropagatedQuestion
+    {
+        public Guid PublicKey { get; set; }
+        public string QuestionText { get; set; }
+        
+        public List<CompleteQuestionView> Questions { get; set; }
+    }
+}
