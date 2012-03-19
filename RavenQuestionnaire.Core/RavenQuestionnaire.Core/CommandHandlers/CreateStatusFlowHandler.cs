efb4235bbeba4244e6f874bb17a903d1cfc28689
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2012-03-18 23:12:13 -0400

    status update
    Former-commit-id: f1344cded2c8c70ffc7ec98c118db2e0fb0c1f2a

--- RavenQuestionnaire.Core/RavenQuestionnaire.Core/CommandHandlers/CreateStatusFlowHandler.cs
+++ RavenQuestionnaire.Core/RavenQuestionnaire.Core/CommandHandlers/CreateStatusFlowHandler.cs
@@ -1,7 +1,12 @@
-﻿namespace RavenQuestionnaire.Core.CommandHandlers
-{
-    class CreateStatusFlowHandler //: ICommandHandler<>
-    {
+﻿using RavenQuestionnaire.Core.Commands;
 
+namespace RavenQuestionnaire.Core.CommandHandlers
+{
+    class CreateStatusFlowHandler : ICommandHandler<AddNewStatusFlowItem>
+    {
+        public void Handle(AddNewStatusFlowItem command)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
