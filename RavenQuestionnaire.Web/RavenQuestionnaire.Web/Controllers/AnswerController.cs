fecefd4c927961402db6358d79beaff7d1dacfc1
julia@toidev.com <julia@toidev.com>
2012-04-13 18:31:43 +0300

    fix previous merge
    Former-commit-id: d0744ef88c77a6ca17d8a286f08bc0fbf6f0e204

--- RavenQuestionnaire.Web/RavenQuestionnaire.Web/Controllers/AnswerController.cs
+++ RavenQuestionnaire.Web/RavenQuestionnaire.Web/Controllers/AnswerController.cs
@@ -1,6 +1,7 @@
 ﻿#region
 
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
 using RavenQuestionnaire.Core;
@@ -31,6 +32,7 @@ namespace RavenQuestionnaire.Web.Controllers
         public ActionResult Create(Guid questionPublicKey)
         {
             LoadImages();
+            return PartialView("_EditRow", new AnswerView { QuestionId = questionPublicKey, PublicKey = Guid.NewGuid() });
         }
 
         [QuestionnaireAuthorize(UserRoles.Administrator)]
