9056e335956cb8a0c1e592cc6c85e25ebbd8fb91
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2012-04-20 10:33:14 -0400

    hierarchical group update
    Former-commit-id: 31a5330875d5819430254c8dee16c81cb70e2ec4

---
+++ QApp/View/QuestionnaireDetailView.xaml.cs
@@ -0,0 +1,12 @@
+
+using DevExpress.RealtorWorld.Xpf.View;
+
+namespace QApp.View {
+    public partial class QuestionnaireDetailView : ModuleView
+    {
+        public QuestionnaireDetailView()
+        {
+            InitializeComponent();
+        }
+    }
+}
