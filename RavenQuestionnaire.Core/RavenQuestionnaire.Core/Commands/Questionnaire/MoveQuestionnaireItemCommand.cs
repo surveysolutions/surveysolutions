04f180a41fe9c69c64be421104358d86b4d71ca3
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2012-05-01 19:04:30 -0400

    case 1537
    Former-commit-id: 0b6e62e23ce1147f38ad4f3856c206b06ae879e3

---
+++ RavenQuestionnaire.Core/RavenQuestionnaire.Core/Commands/Questionnaire/MoveQuestionnaireItemCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RavenQuestionnaire.Core.Entities.SubEntities;
+using RavenQuestionnaire.Core.Utility;
+
+namespace RavenQuestionnaire.Core.Commands.Questionnaire
+{
+    public class MoveQuestionnaireItemCommand: ICommand
+    {
+        
+        public string QuestionnaireId { get; set; }
+        public Guid PublicKey { get; set; }
+        public Guid? AfterItemKey { get; set; }
+        public UserLight Executor { get; set; }
+
+        public MoveQuestionnaireItemCommand(string questionnaireId, Guid publicKey, Guid? afterItem, UserLight executor)
+        {
+            this.QuestionnaireId = IdUtil.CreateQuestionnaireId(questionnaireId);
+            this.PublicKey = publicKey;
+            this.AfterItemKey = afterItem;
+            Executor = executor;
+        }
+    }
+}
