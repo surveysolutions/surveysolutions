de5ec4fa2ecbd5a12fd2b2e1faa9c3b5f46a5525
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2012-05-08 01:15:26 -0400

    status db update
    Former-commit-id: 3050c45d11b46bf86cde7cdb9d8d9f586fdec686

--- RavenQuestionnaire.Web/RavenQuestionnaire.Core.Tests/CommandHandlers/CreateNewStatusHandlerTest.cs
+++ RavenQuestionnaire.Web/RavenQuestionnaire.Core.Tests/CommandHandlers/CreateNewStatusHandlerTest.cs
@@ -2,6 +2,7 @@
 using NUnit.Framework;
 using RavenQuestionnaire.Core.CommandHandlers.Status;
 using RavenQuestionnaire.Core.Commands.Status;
+using RavenQuestionnaire.Core.Documents;
 using RavenQuestionnaire.Core.Entities;
 using RavenQuestionnaire.Core.Repositories;
 
@@ -14,10 +15,16 @@ namespace RavenQuestionnaire.Core.Tests.CommandHandlers
         [Test]
         public void WhenCommandIsReceived_NewStatusIsAddedToRepository()
         {
+
+            StatusDocument innerDocument = new StatusDocument();
+            innerDocument.Id = "sID";
+            Status entity = new Status(innerDocument);
             Mock<IStatusRepository> statusRepositoryMock = new Mock<IStatusRepository>();
+            statusRepositoryMock.Setup(x => x.Load("statusdocuments/sID")).Returns(entity);
+           
             CreateNewStatusHandler handler=new CreateNewStatusHandler(statusRepositoryMock.Object);
-            handler.Handle(new CreateNewStatusCommand("Create", true, "SId", "QID", null));
-            statusRepositoryMock.Verify(x=>x.Add(It.IsAny<Status>()), Times.Once());
+            handler.Handle(new CreateNewStatusCommand("Create", true, innerDocument.Id, "QID", null));
+            statusRepositoryMock.Verify(x => x.Add(It.IsAny<Status>()), Times.Once());
         }
     }
 }
