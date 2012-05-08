a45398997a6851b6757488d37dc9a3d98745d0dc
julia@toidev.com <julia@toidev.com>
2012-05-08 15:23:26 +0300

    case 1540 (add pictures for type Image)
    Former-commit-id: 1959e84bda119ba90172fffe492b21f8cb5700a1

---
+++ QApp/Helpers/Extensions/ImageConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Windows.Data;
+using System.Globalization;
+using System.Windows.Media.Imaging;
+using RavenQuestionnaire.Core.Services;
+
+namespace QApp.Helpers.Extensions
+{
+    public class ImageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+            {
+                IFileStorageService fileStorage = Initializer.Kernel.GetService(typeof(RavenFileStorageService)) as RavenFileStorageService;
+                if (fileStorage != null)
+                {
+                    var ms = new MemoryStream(fileStorage.RetrieveFile(((RavenQuestionnaire.Core.Views.Card.CardView)(value)).Thumb));
+                    var image = new BitmapImage();
+                    image.BeginInit();
+                    image.StreamSource = ms;
+                    image.DecodePixelWidth = 200;
+                    image.DecodePixelHeight = 200;
+                    image.EndInit();
+                    return image;
+                }
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
