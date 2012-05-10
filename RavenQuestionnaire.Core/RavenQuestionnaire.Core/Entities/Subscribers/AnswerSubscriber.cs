be40fb6804a9fc8ca3e3931d6e1affc2903f81a9
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2012-05-10 17:03:34 -0400

    answer startegy
    Former-commit-id: 3a4fc22c1098181586d2c8df8b72a4016dce1d90

---
+++ RavenQuestionnaire.Core/RavenQuestionnaire.Core/Entities/Subscribers/AnswerSubscriber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Text;
+using RavenQuestionnaire.Core.Entities.Composite;
+using RavenQuestionnaire.Core.Entities.Extensions;
+using RavenQuestionnaire.Core.Entities.SubEntities.Complete;
+
+namespace RavenQuestionnaire.Core.Entities.Subscribers
+{
+    public class AnswerSubscriber : EntitySubscriber<ICompleteGroup>
+    {
+        #region Overrides of EntitySubscriber<ICompleteQuestion>
+
+        public override void Subscribe(ICompleteGroup target)
+        {
+            var questions = target.GetAllQuestions<ICompleteQuestion>();
+            foreach (ICompleteQuestion completeQuestion in questions)
+            {
+                completeQuestion.GetAllAnswerAddedEvents().Subscribe(
+                    Observer.Create<CompositeAddedEventArgs>(
+                        (e) =>
+                            {
+                                ((ICompleteAnswer) e.AddedComposite).QuestionPublicKey = target.PublicKey;
+                            }));
+            }
+          
+        }
+
+        #endregion
+    }
+}
