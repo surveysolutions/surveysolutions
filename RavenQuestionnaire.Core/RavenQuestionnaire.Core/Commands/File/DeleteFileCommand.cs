dbf93d3739dbccbd3026ed8b9b8644d04bf8bbe8
Slava Scherbak <slava.scherbak@gmail.com>
2012-05-10 18:15:50 +0300

    DB export commands in create_new_DB implement delete images feature
    Former-commit-id: 27d3eff695d2d5fd8090a0d9be27c6b12436d00c

---
+++ RavenQuestionnaire.Core/RavenQuestionnaire.Core/Commands/File/DeleteFileCommand.cs
@@ -0,0 +1,21 @@
+﻿using RavenQuestionnaire.Core.Entities.SubEntities;
+
+namespace RavenQuestionnaire.Core.Commands.File
+{
+    public class DeleteFileCommand : ICommand
+    {
+        public DeleteFileCommand(string id, UserLight executor)
+        {
+            Executor = executor;
+            Id = Utility.IdUtil.CreateFileId(id);
+        }
+
+        public string Id { get; private set; }
+
+        #region ICommand Members
+
+        public UserLight Executor { get; set; }
+
+        #endregion
+    }
+}
