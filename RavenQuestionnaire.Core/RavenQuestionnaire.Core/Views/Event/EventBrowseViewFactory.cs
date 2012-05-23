22006ac4543ab92ca5cab2c19b2443aabf31fa63
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2012-05-23 10:26:44 -0400

    client settings
    Former-commit-id: c3156c7cd6bf3448f0920833c619117351df1a76

--- RavenQuestionnaire.Core/RavenQuestionnaire.Core/Views/Event/EventBrowseViewFactory.cs
+++ RavenQuestionnaire.Core/RavenQuestionnaire.Core/Views/Event/EventBrowseViewFactory.cs
@@ -20,14 +20,28 @@ namespace RavenQuestionnaire.Core.Views.Event
 
           public EventBrowseView Load(EventBrowseInputModel input)
           {
-              var count = documentSession.Query<EventDocument>().Count();
-              if (count == 0)
-                  return new EventBrowseView(0, count, count, new EventBrowseItem[0]);
-              // Perform the paged query
-              var query = documentSession.Query<EventDocument>()
-                  .Take(count);
-            
-
+              IQueryable<EventDocument> query;
+              int count;
+              if (!input.PublickKey.HasValue)
+              {
+                  count = documentSession.Query<EventDocument>().Count();
+                  if (count == 0)
+                      return new EventBrowseView(0, count, count, new EventBrowseItem[0]);
+                  // Perform the paged query
+                  query = documentSession.Query<EventDocument>()
+                      .Take(count);
+              }
+              else
+              {
+                  var last =
+                      documentSession.Query<EventDocument>().FirstOrDefault(e => e.PublicKey == input.PublickKey.Value);
+                  count = documentSession.Query<EventDocument>().Count(e => e.CreationDate>last.CreationDate);
+                  if (count == 0)
+                      return new EventBrowseView(0, count, count, new EventBrowseItem[0]);
+                  // Perform the paged query
+                  query = documentSession.Query<EventDocument>().Where(e => e.CreationDate > last.CreationDate)
+                      .Take(count);
+              }
 
               // And enact this query
               var items = query
--- Synchronization/DataEntryClient/CompleteQuestionnaire/CompleteQuestionnaireSync.cs
+++ Synchronization/DataEntryClient/CompleteQuestionnaire/CompleteQuestionnaireSync.cs
@@ -8,8 +8,10 @@ using Raven.Client.Document;
 using RavenQuestionnaire.Core;
 using RavenQuestionnaire.Core.Commands.Questionnaire.Completed;
 using RavenQuestionnaire.Core.Documents;
+using RavenQuestionnaire.Core.Views.ClientSettings;
 using RavenQuestionnaire.Core.Views.Event;
 using SynchronizationMessages.CompleteQuestionnaire;
+using SynchronizationMessages.Handshake;
 
 namespace DataEntryClient.CompleteQuestionnaire
 {
@@ -23,24 +25,57 @@ namespace DataEntryClient.CompleteQuestionnaire
             this.viewRepository = viewRepository;
         }
 
-       
+
         public void Execute()
         {
-           ChannelFactory<ICompleteQuestionnaireService> ChannelFactory = new ChannelFactory<ICompleteQuestionnaireService>("test");
-            ICompleteQuestionnaireService client = ChannelFactory.CreateChannel();
-            Guid syncKey = Guid.NewGuid();
 
+            Guid syncKey =
+                this.viewRepository.Load<ClientSettingsInputModel, ClientSettingsView>(new ClientSettingsInputModel()).
+                    PublicKey;
+            Guid? lastSyncEventGuid = GetLastSyncEventGuid(syncKey);
+            UploadEvents(syncKey, lastSyncEventGuid);
+
+        }
+        protected Guid? GetLastSyncEventGuid(Guid clientKey)
+        {
+            Guid? result=null;
+            ChannelFactory<IGetLastSyncEvent> ChannelFactory = new ChannelFactory<IGetLastSyncEvent>("");
+            IGetLastSyncEvent client = ChannelFactory.CreateChannel();
             try
             {
-                var events = viewRepository.Load<EventBrowseInputModel, EventBrowseView>(new EventBrowseInputModel(null));
+
+                result = client.Process(clientKey);
+            }
+            finally
+            {
+                try
+                {
+                    ((IChannel)client).Close();
+                }
+                catch
+                {
+                    ((IChannel)client).Abort();
+                }
+            }
+            return result;
+        }
+
+        protected void UploadEvents(Guid clientKey, Guid? lastSyncEvent)
+        {
+            ChannelFactory<ICompleteQuestionnaireService> ChannelFactory = new ChannelFactory<ICompleteQuestionnaireService>("");
+            ICompleteQuestionnaireService client = ChannelFactory.CreateChannel();
+            try
+            {
+
+                var events = viewRepository.Load<EventBrowseInputModel, EventBrowseView>(new EventBrowseInputModel(lastSyncEvent));
                 foreach (var eventItem in events.Items)
                 {
                     var message = new EventSyncMessage
                     {
-                        SynchronizationKey = syncKey,
+                        SynchronizationKey = clientKey,
                         Command = eventItem.Command
                     };
-                    
+
                     ErrorCodes returnCode = client.Process(message);
                 }
             }
