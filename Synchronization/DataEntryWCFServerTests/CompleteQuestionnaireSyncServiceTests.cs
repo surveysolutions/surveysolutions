25d8088756c6561f286a86d924ad63ad80e27a67
Anastasya Kholodova <nastya.kholodova@gmail.com>
2012-05-23 14:58:04 -0400

    server unit tests
    Former-commit-id: 7b274cc8bfcecec04e65ab35e83fa1b6ae240ace

---
+++ Synchronization/DataEntryWCFServerTests/CompleteQuestionnaireSyncServiceTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DataEntryWCFServer;
+using Moq;
+using NUnit.Framework;
+using RavenQuestionnaire.Core;
+using RavenQuestionnaire.Core.Commands;
+using RavenQuestionnaire.Core.Views.Event;
+using SynchronizationMessages.CompleteQuestionnaire;
+
+namespace DataEntryWCFServerTests
+{
+    [TestFixture]
+    public class CompleteQuestionnaireSyncServiceTests
+    {
+        [Test]
+        public void Process_AventArrived_Eventprocessed()
+        {
+            Mock<ICommandInvoker> invokerMock = new Mock<ICommandInvoker>();
+
+            CompleteQuestionnaireSyncService target = new CompleteQuestionnaireSyncService(invokerMock.Object);
+
+            for (int i = 0; i < 10; i++)
+            {
+
+
+                var result = target.Process(new EventSyncMessage());
+                Assert.AreEqual(result, ErrorCodes.None);
+            }
+            invokerMock.Verify(x => x.Execute(It.IsAny<ICommand>(), It.IsAny<Guid>(), It.IsAny<Guid>()),
+                               Times.Exactly(10));
+        }
+    }
+}
