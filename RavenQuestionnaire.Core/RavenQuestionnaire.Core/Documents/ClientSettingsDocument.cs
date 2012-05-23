1590a965125eba6c175d2e6ff643c22459739090
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2012-05-23 10:27:39 -0400

    client settings
    Former-commit-id: 8b3904d1bc3323e7bd7f6dbc952e53cfaf6755e2

---
+++ RavenQuestionnaire.Core/RavenQuestionnaire.Core/Documents/ClientSettingsDocument.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RavenQuestionnaire.Core.Documents
+{
+    public class ClientSettingsDocument
+    {
+        public string Id { get; set; }
+        public Guid PublicKey { get; set; }
+    }
+}
