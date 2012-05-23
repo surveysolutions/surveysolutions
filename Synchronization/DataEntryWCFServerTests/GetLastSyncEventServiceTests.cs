9cd273f312cc3a8897b63b31f234e3f612a61ab9
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2012-05-23 14:58:04 -0400

    server unit tests
    Former-commit-id: 7b274cc8bfcecec04e65ab35e83fa1b6ae240ace

---
+++ Synchronization/DataEntryWCFServerTests/GetLastSyncEventServiceTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using DataEntryWCFServer;
+using Moq;
+using NUnit.Framework;
+using RavenQuestionnaire.Core;
+using RavenQuestionnaire.Core.Views.Event;
+
+namespace DataEntryWCFServerTests
+{
+    [TestFixture]
+    public class GetLastSyncEventServiceTests
+    {
+        [Test]
+        public void Process_FirstSync_NullReturned()
+        {
+            Mock<ICommandInvoker> invokerMock = new Mock<ICommandInvoker>();
+            Mock<IViewRepository> repositoryMock = new Mock<IViewRepository>();
+            EventView eventGuid = null;
+            repositoryMock.Setup(x => x.Load<EventViewInputModel, EventView>(It.IsAny<EventViewInputModel>())).Returns(
+                eventGuid);
+
+            GetLastSyncEventService target = new GetLastSyncEventService(invokerMock.Object, repositoryMock.Object);
+
+            var result = target.Process(Guid.NewGuid());
+            Assert.IsNull(result);
+        }
+
+        [Test]
+        public void Process_NotFirstSync_GuidReturned()
+        {
+            Mock<ICommandInvoker> invokerMock = new Mock<ICommandInvoker>();
+            Mock<IViewRepository> repositoryMock = new Mock<IViewRepository>();
+            EventView eventGuid = new EventView(Guid.NewGuid(), Guid.NewGuid(), DateTime.Now, null);
+            repositoryMock.Setup(x => x.Load<EventViewInputModel, EventView>(It.IsAny<EventViewInputModel>())).Returns(
+                eventGuid);
+
+            GetLastSyncEventService target = new GetLastSyncEventService(invokerMock.Object, repositoryMock.Object);
+
+            var result = target.Process(Guid.NewGuid());
+            Assert.AreEqual(result, eventGuid.PublicKey);
+        }
+    }
+}
