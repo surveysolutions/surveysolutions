840c58eed14ec3a76ce7597aac702e321757796d
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2012-06-08 00:20:45 -0400

    ninject update
    Former-commit-id: a0fac481e8ad957c4b872ca6860a250b1a703ecb

--- RavenQuestionnaire.Core/RavenQuestionnaire.Core/Conventions/RegisterFirstInstanceOfInterface.cs
+++ RavenQuestionnaire.Core/RavenQuestionnaire.Core/Conventions/RegisterFirstInstanceOfInterface.cs
@@ -1,15 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using Ninject;
 using Ninject.Activation;
 using Ninject.Extensions.Conventions;
+using Ninject.Extensions.Conventions.BindingGenerators;
+using Ninject.Syntax;
 
 namespace RavenQuestionnaire.Core.Conventions
 {
     public class RegisterFirstInstanceOfInterface : IBindingGenerator
     {
-        public void Process(Type type, Func<IContext, object> scopeCallback, IKernel kernel)
+       /* public void Process(Type type, Func<IContext, object> scopeCallback, IKernel kernel)
         {
             if (!type.IsInterface)
             {
@@ -31,6 +34,30 @@ namespace RavenQuestionnaire.Core.Conventions
                 return;
             kernel.Bind(type).To(matchedType);
           //  registry.For(type).Use(matchedType);
+        }*/
+
+        public IEnumerable<IBindingWhenInNamedWithOrOnSyntax<object>> CreateBindings(Type type, IBindingRoot bindingRoot)
+        {
+            IEnumerable<IBindingWhenInNamedWithOrOnSyntax<object>> y =
+                Enumerable.Empty<IBindingWhenInNamedWithOrOnSyntax<object>>();
+
+            if (!type.IsInterface)
+                return y;
+            Assembly containingAssembly = type.Assembly;
+
+            if (type.IsGenericType)
+                return y;
+
+            var matchedType =
+                containingAssembly.GetTypes().FirstOrDefault(
+                    x => x.Namespace == type.Namespace && x.GetInterface(type.FullName) != null);
+            if (matchedType == null)
+                return y;
+            return
+                new
+                    IBindingWhenInNamedWithOrOnSyntax<object>[1] {bindingRoot.Bind(new Type[1] {type}).To(matchedType)};
+
+
         }
     }
 }
