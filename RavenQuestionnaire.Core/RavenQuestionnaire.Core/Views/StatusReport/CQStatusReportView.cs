2b5d32637229fa3be4006c93136818a3157f86cb
Slava Scherbak <vs@cloudwk.com>
2012-06-19 17:57:50 +0300

    supervisor workflow 2
    Former-commit-id: b47cabb3a4bc92315dee4be01b8008ab1dd619f4

---
+++ RavenQuestionnaire.Core/RavenQuestionnaire.Core/Views/StatusReport/CQStatusReportView.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using RavenQuestionnaire.Core.Documents;
+using RavenQuestionnaire.Core.Documents.Statistics;
+using RavenQuestionnaire.Core.Entities.SubEntities;
+using RavenQuestionnaire.Core.Views.Statistics;
+using RavenQuestionnaire.Core.Views.Status.StatusElement;
+
+namespace RavenQuestionnaire.Core.Views.StatusReport
+{
+    public class CQStatusReportView
+    {
+        public string QuestionnaireId { get; set; }
+        public string Title { get; set; }
+        public List<CQStatusReportItemView> Items { get; set; }
+        public string StatusName { get; set; }
+
+        public CQStatusReportView(StatusItem status, IEnumerable<CompleteQuestionnaireStatisticDocument> statisticDocuments)
+        {
+            StatusName = status.Title;
+            
+            Items = new List<CQStatusReportItemView>();
+            foreach (var statisticDocument in statisticDocuments)
+            {
+                var view = new CQStatusReportItemView()
+                               {
+                                   AssignToUser = statisticDocument.Responsible,
+                                   LastChangeDate = statisticDocument.LastEntryDate,
+                                   LastSyncDate = DateTime.Now,
+                                   Description = string.Join(", ", statisticDocument.FeturedQuestions.Select(f => f.QuestionText + ": " + f.AnswerText))
+                               };
+                Items.Add(view);
+            }
+        }
+    }
+}
