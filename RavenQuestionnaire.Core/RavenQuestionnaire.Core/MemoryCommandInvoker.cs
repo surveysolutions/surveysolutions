e15c3dcb52e97c1cfa87b5c531fef657dfeac188
Anastasya Kholodova <nastya.kholodova@gmail.com>
2012-06-20 17:49:15 -0400

    import/export
    Former-commit-id: 68a3cd478b0bdcec11ef08a710f94fef264e2f8e

--- RavenQuestionnaire.Core/RavenQuestionnaire.Core/MemoryCommandInvoker.cs
+++ RavenQuestionnaire.Core/RavenQuestionnaire.Core/MemoryCommandInvoker.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using Ninject;
 using Raven.Client;
 using RavenQuestionnaire.Core.ClientSettingsProvider;
@@ -29,12 +30,13 @@ namespace RavenQuestionnaire.Core
           //  var documentSession = container.Get<IDocumentSession>();
             var handler = container.Get<ICommandHandler<T>>();
             handler.Handle(command);
-            SaveEvent(handler.GetType(), command, container.Get<IDocumentSession>(), Guid.NewGuid());
+            SaveEvent(handler.GetType(), command, container.Get<IDocumentSession>(), Guid.NewGuid(), clientSettingsProvider.ClientSettings.PublicKey);
+            InvokeSubscribers(command);
         }
 
         //TODO remove that spike after event soursing implementation
         //protected void SaveEvent<T>(Type handlerType, T command, IDocumentSession documentSession) where T : ICommand
-        protected void SaveEvent<T>(Type handlerType, T command, IDocumentSession documentSession, Guid eventPublicKey) where T : ICommand
+        protected void SaveEvent<T>(Type handlerType, T command, IDocumentSession documentSession, Guid eventPublicKey, Guid clientPublicKey) where T : ICommand
         {
             System.Attribute[] attrs = System.Attribute.GetCustomAttributes(handlerType); // Reflection.
 
@@ -52,22 +54,42 @@ namespace RavenQuestionnaire.Core
             }
             //documentSession.Store(new EventDocument(command, Guid.NewGuid(),
             //                                        clientSettingsProvider.ClientSettings.PublicKey));
-            documentSession.Store(new EventDocument(command, eventPublicKey,
-                                                   clientSettingsProvider.ClientSettings.PublicKey));
+            documentSession.Store(new EventDocument(command, eventPublicKey, clientPublicKey));
+        }
+        protected Type GetGenericObject(Type generic, ICommand command)
+        {
+            var commandHandler = generic;
+            Type[] typeArgs = {command.GetType()};
+            return commandHandler.MakeGenericType(typeArgs);
         }
 
         public void Execute(ICommand command, Guid eventPublicKey, Guid clientPublicKey)
         {
-           // var documentSession = container.Get<IDocumentSession>();
-            var commandHandler = typeof (ICommandHandler<>);
-            Type[] typeArgs = {command.GetType()};
-            var reflectionGeneric = commandHandler.MakeGenericType(typeArgs);
+            var reflectionGeneric = GetGenericObject(typeof (ICommandHandler<>), command);
 
-            var handler = container.Get(reflectionGeneric);
+            var handler = container.Get(GetGenericObject(typeof (ICommandHandler<>), command));
             reflectionGeneric.GetMethod("Handle").Invoke(handler, new object[] {command});
             //handler.Handle(command);
             //store the command in the store
-            SaveEvent(handler.GetType(), command, container.Get<IDocumentSession>(), eventPublicKey);
+            SaveEvent(handler.GetType(), command, container.Get<IDocumentSession>(), eventPublicKey, clientPublicKey);
+            InvokeSubscribers(command);
+        }
+
+        protected void InvokeSubscribers<T>(T command) where T : ICommand
+        {
+            /*   WaitCallback callback = (state) =>
+                                           {*/
+            var reflectionGeneric = GetGenericObject(typeof (IEventSubscriber<>), command);
+            var subscribers =
+                container.GetAll(GetGenericObject(typeof (IEventSubscriber<>), command));
+
+            foreach (object eventSubscriber in subscribers)
+            {
+                reflectionGeneric.GetMethod("Invoke").Invoke(eventSubscriber,
+                                                             new object[] {command});
+            }
+            /*    };
+ThreadPool.QueueUserWorkItem(callback, command);*/
 
         }
 
