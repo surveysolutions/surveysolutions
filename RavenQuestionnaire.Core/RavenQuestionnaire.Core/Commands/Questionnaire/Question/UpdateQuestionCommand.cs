121e4888bc75e46094b1516e6cfc0f804e4b11ac
Juliya Pushkalova <julia@toidev.com>
2012-06-21 15:45:19 +0300

    case 1668 (fix import/export exceptions)
    Former-commit-id: 7ab55d736512367b261150cf3b15122836b2437c

--- RavenQuestionnaire.Core/RavenQuestionnaire.Core/Commands/Questionnaire/Question/UpdateQuestionCommand.cs
+++ RavenQuestionnaire.Core/RavenQuestionnaire.Core/Commands/Questionnaire/Question/UpdateQuestionCommand.cs
@@ -1,73 +1,55 @@
 ﻿using System;
 using System.Linq;
-using RavenQuestionnaire.Core.Entities.SubEntities;
+using Newtonsoft.Json;
 using RavenQuestionnaire.Core.Utility;
 using RavenQuestionnaire.Core.Views.Answer;
+using RavenQuestionnaire.Core.Entities.SubEntities;
 
 namespace RavenQuestionnaire.Core.Commands.Questionnaire.Question
 {
     public class UpdateQuestionCommand : ICommand
     {
+        #region Properties
+
         public string QuestionnaireId { get; set; }
         public Guid QuestionPublicKey { get; set; }
-
         public string QuestionText { get; set; }
         public QuestionType QuestionType { get; set; }
-        public string ConditionExpression
-        {
-            get;
-            private set;
-        }
-        public string ValidationExpression
-        {
-            get;
-            private set;
-        }
-        public string StataExportCaption
-        {
-            get;
-            private set;
-        }
-
-        public string Instructions
-        {
-            get;
-            private set;
-        }
-
-        public Order AnswerOrder
-        {
-            get;
-            private set;
-        }
-        public bool Featured
-        {
-            get;
-            private set;
-        }
+        public string ConditionExpression { get; private set; }
+        public string ValidationExpression { get; private set; }
+        public string StataExportCaption { get; private set; }
+        public string Instructions { get; private set; }
+        public Order AnswerOrder { get; private set; }
+        public bool Featured { get; private set; }
         public UserLight Executor { get; set; }
-
         public Answer[] Answers { get; set; }
-        public UpdateQuestionCommand(string questionnaireId, Guid questionPublicKey, string text,
-            string stataExport, QuestionType type, string condition, string validation, bool featured, string instructions, Order answerOrder, UserLight executor)
+
+        #endregion
+
+        #region Constructor
+
+        [JsonConstructor]
+        public UpdateQuestionCommand(string questionnaireId, Guid questionPublicKey, string questionText,
+            string stataExportCaption, QuestionType questionType, string conditionExpression, string validationExpression, 
+            bool featured, string instructions, Order answerOrder, UserLight executor)
         {
             this.QuestionnaireId = IdUtil.CreateQuestionnaireId(questionnaireId);
             this.AnswerOrder = answerOrder;
             this.QuestionPublicKey = questionPublicKey;
-            this.QuestionText = text;
-            this.StataExportCaption = stataExport;
-            this.QuestionType = type;
+            this.QuestionText = questionText;
+            this.StataExportCaption = stataExportCaption;
+            this.QuestionType = questionType;
             this.Answers = new Answer[0];
-            this.ConditionExpression = condition;
-            this.ValidationExpression = validation;
+            this.ConditionExpression = conditionExpression;
+            this.ValidationExpression = validationExpression;
             this.Executor = executor;
             this.Featured = featured;
             this.Instructions = instructions;
         }
 
-        public UpdateQuestionCommand(string questionnaireId, Guid questionPublicKey, string text,
-           string stataExport, QuestionType type, string condition, string validation, bool featured, string instructions, AnswerView[] answers, Order answerOrder, UserLight executor) :
-            this(questionnaireId, questionPublicKey, text, stataExport, type, condition, validation,featured, instructions, answerOrder, executor)
+        public UpdateQuestionCommand(string questionnaireId, Guid questionPublicKey, string questionText,
+           string stataExportCaption, QuestionType questionType, string conditionExpression, string validationExpression, bool featured, string instructions, AnswerView[] answers, Order answerOrder, UserLight executor) :
+            this(questionnaireId, questionPublicKey, questionText, stataExportCaption, questionType, conditionExpression, validationExpression, featured, instructions, answerOrder, executor)
         {
             if (answers != null)
                 this.Answers =
@@ -83,5 +65,7 @@ namespace RavenQuestionnaire.Core.Commands.Questionnaire.Question
                                 AnswerImage = string.IsNullOrEmpty(a.AnswerImage)? "": IdUtil.CreateFileId(a.AnswerImage)
                             }).ToArray();
         }
+
+        #endregion
     }
 }
