2ce75432c3677c31c00bc7a9d2b5208f908d9f38
Anastasya Kholodova <nastya.kholodova@gmail.com>
2012-07-10 19:32:10 -0400

    supervisor pages
    Former-commit-id: 508130e4986420826f36711294940f57937d16fe

--- RavenQuestionnaire.Core/RavenQuestionnaire.Core/Views/StatusReport/CQStatusReportFactory.cs
+++ RavenQuestionnaire.Core/RavenQuestionnaire.Core/Views/StatusReport/CQStatusReportFactory.cs
@@ -1,35 +1,38 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using Raven.Client;
+using RavenQuestionnaire.Core.Denormalizers;
 using RavenQuestionnaire.Core.Documents;
 using RavenQuestionnaire.Core.Documents.Statistics;
 using RavenQuestionnaire.Core.Entities;
+using RavenQuestionnaire.Core.Entities.SubEntities;
 using RavenQuestionnaire.Core.Indexes;
 using RavenQuestionnaire.Core.Utility;
+using RavenQuestionnaire.Core.Views.CompleteQuestionnaire;
 using RavenQuestionnaire.Core.Views.Questionnaire;
 
 namespace RavenQuestionnaire.Core.Views.StatusReport
 {
     public class CQStatusReportFactory : IViewFactory<CQStatusReportViewInputModel, CQStatusReportView>
     {
-        private IDocumentSession documentSession;
+      //  private IDocumentSession documentSession;
 
-
-        public CQStatusReportFactory(IDocumentSession documentSession)
+        private IDenormalizerStorage<CompleteQuestionnaireBrowseItem> documentItemSession;
+        public CQStatusReportFactory(IDenormalizerStorage<CompleteQuestionnaireBrowseItem> documentItemSession)
         {
-            this.documentSession = documentSession;
+            this.documentItemSession = documentItemSession;
         }
 
         public CQStatusReportView Load(CQStatusReportViewInputModel input)
         {
-            var statuses = documentSession.Query<StatusDocument>().FirstOrDefault(u => u.QuestionnaireId == input.QuestionnaireId);
-
-            if (statuses == null)
+          //  var statuses = documentSession.Query<StatusDocument>().FirstOrDefault(u => u.QuestionnaireId == input.QuestionnaireId);
+            var statuseFirst = SurveyStatus.GetAllStatuses().FirstOrDefault();
+            if (statuseFirst == null)
                 return null; //no satelite status document 
 
-            var status = statuses.Statuses.FirstOrDefault(s => s.PublicKey == input.StatusId);
+            var status = new StatusItem(){ Title = statuseFirst.Name};// statuses.FirstOrDefault(s => s.PublicId == input.StatusId);}
 
-            var query = documentSession.Query<CompleteQuestionnaireStatisticDocument>().Where(x => (x.TemplateId == input.QuestionnaireId) && (x.Status.PublicId == input.StatusId)).ToList();
+            var query = this.documentItemSession.Query().Where(x => (x.TemplateId == input.QuestionnaireId) && (x.Status.PublicId == input.StatusId)).ToList();
 
             return new CQStatusReportView(status, query);
         }
--- RavenQuestionnaire.Core/RavenQuestionnaire.Core/Views/StatusReport/StatusReportViewFactory.cs
+++ RavenQuestionnaire.Core/RavenQuestionnaire.Core/Views/StatusReport/StatusReportViewFactory.cs
@@ -1,46 +1,59 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using Raven.Client;
+using RavenQuestionnaire.Core.Denormalizers;
 using RavenQuestionnaire.Core.Documents;
 using RavenQuestionnaire.Core.Documents.Statistics;
 using RavenQuestionnaire.Core.Entities;
+using RavenQuestionnaire.Core.Entities.SubEntities;
 using RavenQuestionnaire.Core.Indexes;
 using RavenQuestionnaire.Core.Utility;
+using RavenQuestionnaire.Core.ViewSnapshot;
+using RavenQuestionnaire.Core.Views.CompleteQuestionnaire;
+using RavenQuestionnaire.Core.Views.CompleteQuestionnaire.Grouped;
 using RavenQuestionnaire.Core.Views.Questionnaire;
 
 namespace RavenQuestionnaire.Core.Views.StatusReport
 {
     public class StatusReportViewFactory : IViewFactory<StatusReportViewInputModel, StatusReportView>
     {
-        private IDocumentSession documentSession;
+      //  private IDocumentSession documentSession;
 
-
-        public StatusReportViewFactory(IDocumentSession documentSession)
+        private IDenormalizerStorage<CompleteQuestionnaireBrowseItem> documentItemSession;
+        private IDenormalizerStorage<CQGroupItem> documentGroupSession;
+        public StatusReportViewFactory(IDenormalizerStorage<CompleteQuestionnaireBrowseItem> documentItemSession, IDenormalizerStorage<CQGroupItem> documentGroupSession)
         {
-            this.documentSession = documentSession;
+            this.documentItemSession = documentItemSession;
+            this.documentGroupSession = documentGroupSession;
         }
 
         public StatusReportView Load(StatusReportViewInputModel input)
         {
             var statusGroups = new List<StatusReportGroupView>();
-            var query = documentSession.Query<QuestionnaireDocument>().ToList();
+            var query = this.documentGroupSession.Query().ToList();
+            var questionnaires = this.documentItemSession.Query();
 
             foreach (var q in query)
             {
-                var statusGroup = new StatusReportGroupView(q.Id, q.Title);
+                var statusGroup = new StatusReportGroupView(q.SurveyId, q.SurveyTitle);
 
-                var statuses = (from s in documentSession.Query<StatusDocument>()
-                                         .FirstOrDefault(u => u.QuestionnaireId == q.Id).Statuses
+              /*  var statuses = (from s in documentSession.Query<StatusDocument>()
+                                         .FirstOrDefault(u => u.QuestionnaireId == q.SurveyId).Statuses
                                select new StatusReportItemView
                                           {
                                               StatusId = s.PublicKey,
                                               StatusTitle = s.Title,
                                               QuestionnaireCount = 0
-                                          }).ToList();
-
-                var cqs_raw = documentSession.Query<CompleteQuestionnaireStatisticDocument>().Where(x => x.TemplateId == q.Id)
-                        .Select(s => new { StatusId = s.Status.PublicId, Count = 1 }).ToList();
-
+                                          }).ToList();*/
+                var statuses = SurveyStatus.GetAllStatuses().Select(s => new StatusReportItemView()
+                                                                             {
+                                                                                 StatusId = s.PublicId,
+                                                                                 StatusTitle = s.Name,
+                                                                                 QuestionnaireCount = 0
+                                                                             }).ToList();
+                /*var cqs_raw = documentSession.Query<CompleteQuestionnaireStatisticDocument>().Where(x => x.TemplateId == q.Id)
+                        .Select(s => new { StatusId = s.Status.PublicId, Count = 1 }).ToList();*/
+                var cqs_raw = questionnaires.Where(x => x.TemplateId == q.SurveyId).Select(s => new { StatusId = s.Status.PublicId, Count = 1 }).ToList();
                 var grouped = from cq in cqs_raw
                               group cq by cq.StatusId
                                   into g
