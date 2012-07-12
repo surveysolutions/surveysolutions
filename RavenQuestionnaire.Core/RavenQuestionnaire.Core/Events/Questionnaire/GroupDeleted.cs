904dcf3efb44823b43c5ebe0e7dc6c563162c06c
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2012-07-12 16:26:26 -0400

    group delete/update
    Former-commit-id: 34805ab8c4251f3c45d4940187395f978c84fa66

---
+++ RavenQuestionnaire.Core/RavenQuestionnaire.Core/Events/Questionnaire/GroupDeleted.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ncqrs.Eventing.Storage;
+
+namespace RavenQuestionnaire.Core.Events.Questionnaire
+{
+    [Serializable]
+    [EventName("RavenQuestionnaire.Core:Events:GroupDeleted")]
+    public class GroupDeleted
+    {
+        public Guid GroupPublicKey { get; set; }
+    }
+}
