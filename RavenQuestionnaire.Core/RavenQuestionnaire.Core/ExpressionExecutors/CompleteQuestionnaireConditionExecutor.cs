cd24e93b797b828b5684f79fe7f5262a25463ed5
Anastasya Kholodova <nastya.kholodova@gmail.com>
2012-07-27 12:46:52 -0400

    conditions on question are fixed
    Former-commit-id: e1eb16cbd79a0ff363bf3c029ceb4e11d31b933d

--- RavenQuestionnaire.Core/RavenQuestionnaire.Core/ExpressionExecutors/CompleteQuestionnaireConditionExecutor.cs
+++ RavenQuestionnaire.Core/RavenQuestionnaire.Core/ExpressionExecutors/CompleteQuestionnaireConditionExecutor.cs
@@ -22,67 +22,18 @@ namespace RavenQuestionnaire.Core.ExpressionExecutors
         //        completeQuestion.Enabled = Execute(completeQuestion);
         //    }
         //}
-
-        public bool Execute(ICompleteGroup group)
-        {
-            if (string.IsNullOrEmpty(group.ConditionExpression))
-                return true;
-            var e = new Expression(group.ConditionExpression);
-            e.EvaluateParameter += (name, args) =>
-                                       {
-                                           Guid nameGuid = Guid.Parse(name);
-                                           Guid? propagationKey = group.PropogationPublicKey;
-                                           var value = hash[nameGuid, propagationKey].GetAnswerObject();
-                                           args.Result = value ?? string.Empty;
-
-                                       };
-            bool result = false;
-            try
-            {
-                result = (bool)e.Evaluate();
-            }
-            catch (Exception)
-            {
-            }
-            UpdateAllQuestionInGroup(group, result);
-            return result;
-        }
-
-        private void UpdateAllQuestionInGroup(ICompleteGroup group, bool result)
-        {
-            foreach (IComposite child in group.Children)
-            {
-                if (!result)
-                {
-                    var question = child as ICompleteQuestion;
-                    if (question != null)
-                        question.Enabled = result;
-                    else
-                    {
-                        var gr = child as ICompleteGroup;
-                        if (gr != null)
-                            UpdateAllQuestionInGroup(gr, result);
-                    }
-                }
-                else if (child as ICompleteQuestion == null)
-                    (child as ICompleteGroup).Enabled = Execute(child as ICompleteGroup);
-                else
-                    (child as ICompleteQuestion).Enabled = Execute(child as ICompleteQuestion);
-            }
-        }
-
         public bool Execute(ICompleteQuestion question)
         {
             if (string.IsNullOrEmpty(question.ConditionExpression))
                 return true;
             var e = new Expression(question.ConditionExpression);
             e.EvaluateParameter += (name, args) =>
-                                       {
-                                           Guid nameGuid = Guid.Parse(name);
-                                           Guid? propagationKey = question.PropogationPublicKey;
-                                           var value = hash[nameGuid, propagationKey].GetAnswerObject();
-                                           args.Result = value ?? string.Empty;
-                                       };
+            {
+                Guid nameGuid = Guid.Parse(name);
+                Guid? propagationKey = question.PropogationPublicKey;
+                var value = hash[nameGuid, propagationKey].GetAnswerObject();
+                args.Result = value ?? string.Empty;
+            };
             bool result = false;
             try
             {
@@ -93,5 +44,54 @@ namespace RavenQuestionnaire.Core.ExpressionExecutors
             }
             return result;
         }
+        public bool Execute(ICompleteGroup group)
+        {
+            bool result = ExecuteGroup(group);
+            UpdateAllChildElementsInGroup(group, result);
+            return result;
+        }
+        private bool ExecuteGroup(ICompleteGroup group)
+        {
+            if (string.IsNullOrEmpty(group.ConditionExpression))
+                return true;
+            var e = new Expression(group.ConditionExpression);
+            e.EvaluateParameter += (name, args) =>
+            {
+                Guid nameGuid = Guid.Parse(name);
+                Guid? propagationKey = group.PropogationPublicKey;
+                var value = hash[nameGuid, propagationKey].GetAnswerObject();
+                args.Result = value ?? string.Empty;
+
+            };
+            bool result = false;
+            try
+            {
+                result = (bool)e.Evaluate();
+            }
+            catch (Exception)
+            {
+            }
+            return result;
+        }
+
+        private void UpdateAllChildElementsInGroup(ICompleteGroup group, bool result)
+        {
+            foreach (IComposite child in group.Children)
+            {
+                var question = child as ICompleteQuestion;
+                if (question != null)
+                {
+                    question.Enabled = result && Execute(question);
+                    continue;
+                }
+
+                var gr = child as ICompleteGroup;
+                if (gr != null)
+                    gr.Enabled = result && Execute(gr);
+
+            }
+        }
+
+       
     }
 }
