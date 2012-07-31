413e7a65de66edf8073316273c23651439609e60
Anastasya Kholodova <nastya.kholodova@gmail.com>
2012-07-31 19:05:21 -0400

    keyboard bug fix
    Former-commit-id: 710818f701af6b26ba12f4e5c24dcd518d338f8b

--- RavenQuestionnaire.Web/Web.CAPI/Scripts/take.js
+++ RavenQuestionnaire.Web/Web.CAPI/Scripts/take.js
@@ -321,7 +321,6 @@ function updateCounter() {
             });
 
             button.parent().bind('tap', function () {
-                targetInput.focus();
                 targetInput.click();
             });
 
@@ -329,58 +328,95 @@ function updateCounter() {
     },
     $.fn.createKeyBoard = function (layout) {
         
-        var kbOptions = {
-            keyBinding: 'mousedown touchstart',
-            position: {
-                of: null, // optional - null (attach to input/textarea) or a jQuery object (attach elsewhere)
-                my: 'center top',
-                at: 'center top',
-                at2: 'center bottom' // used when "usePreview" is false (centers the keyboard at the bottom of the input/textarea)
-            },
-            autoAccept: true,
-            css: {
-                input: '',
-                container: '',
-                buttonDefault: '',
-                buttonHover: '',
-                buttonActive: '',
-                buttonDisabled: ''
-            }
-        };
-        var mobileOptions = {
-        // keyboard wrapper theme
-            container: { theme: 'c' },
-            // theme added to all regular buttons
-            buttonMarkup: { theme: 'c', shadow: 'true', corners: 'false' },
-            // theme added to all buttons when they are being hovered
-            buttonHover: { theme: 'c' },
-            // theme added to action buttons (e.g. tab, shift, accept, cancel);
-            // parameters here will override the settings in the buttonMarkup
-            buttonAction: { theme: 'b' },
-            // theme added to button when it is active (e.g. shift is down)
-            // All extra parameters will be ignored
-            buttonActive: { theme: 'e' }
-        };
+       
         var keyboardInputs = this.find('input[draw-key-board=true][type=text]');
         var numericInputs = this.find('input[draw-key-board=true][type!=text]');
-        var allInputs = keyboardInputs.add(numericInputs);
-        keyboardInputs.removeAttr("draw-key-board");
-        
-        var numericOptions= $.extend({},kbOptions, { layout: 'numOnly', min_width: null });
-       var keyboardOptions= $.extend({},kbOptions, { layout: 'qwertyNoEnter', min_width: '888px' });
-        
-        numericInputs.keyboard(numericOptions);
-        keyboardInputs.keyboard(keyboardOptions);
-        allInputs.addMobile(mobileOptions);
-        allInputs.bind('accepted.keyboard', function (event) {
-            $(this).change();
-            $(this.form).submit();
-        });
-        allInputs.bind('visible.keyboard', function (event) {
-            var input = $(this);
-            var keyboard = input.getkeyboard();
-            keyboard.$preview.caretToEnd();
-        });
+       
+    //    keyboardInputs.add(numericInputs).removeAttr("draw-key-board");
+
+        createKeyBoard({ layout: 'qwertyNoEnter', min_width: '888px' }, "dummyTxtKeyBoard",keyboardInputs);
+       
+        createKeyBoard({ layout: 'numOnly', min_width: null }, "dummyNumericKeyBoard", numericInputs);
+       
+        function createKeyBoard(options, name, inputs) {
+
+            var input = initInput(options, name);
+            input.bind('accepted.keyboard', function(event) {
+               // var jInput = $(this);
+                var target = $('#' + input.attr('target-input'));
+                target.val(input.val());
+                input.attr('target-input', '');
+                var keyboard = input.getkeyboard();
+                keyboard.$preview.val('');
+                target.change();
+                target.closest("form").submit();
+            });
+            input.bind('visible.keyboard', function(event) {
+               // var jInput = $(this);
+                var target = $('#' + input.attr('target-input'));
+                var keyboard = input.getkeyboard();
+                keyboard.$preview.val(target.val());
+        //        alert(input.val());
+                keyboard.$preview.caretToEnd();
+            });
+            inputs.click(function() {
+             /*   if(input.attr('target-input') && input.attr('target-input')!='')
+                    return;*/
+                input.attr('target-input', this.id);
+                input.focus();
+             //   input.click();
+               
+            });
+            return input;
+        }
+
+        function initInput(options, name) {
+            var keyBoardInited = $('#' + name);
+            if (keyBoardInited.length > 0)
+                return keyBoardInited;
+            var input = $('<input>').attr({
+                type: 'text',
+                style:'display:none',
+                id: name,
+                name: name
+            });
+            var kbOptions = {
+                keyBinding: 'mousedown touchstart',
+                position: {
+                    of: null, // optional - null (attach to input/textarea) or a jQuery object (attach elsewhere)
+                    my: 'center top',
+                    at: 'center top',
+                    at2: 'center bottom' // used when "usePreview" is false (centers the keyboard at the bottom of the input/textarea)
+                },
+                autoAccept: true,
+                css: {
+                    input: '',
+                    container: '',
+                    buttonDefault: '',
+                    buttonHover: '',
+                    buttonActive: '',
+                    buttonDisabled: ''
+                }
+            };
+            var extendedOptions = $.extend({ }, kbOptions, options);
+            var mobileOptions = {
+            // keyboard wrapper theme
+                container: { theme: 'c' },
+                // theme added to all regular buttons
+                buttonMarkup: { theme: 'c', shadow: 'true', corners: 'false' },
+                // theme added to all buttons when they are being hovered
+                buttonHover: { theme: 'c' },
+                // theme added to action buttons (e.g. tab, shift, accept, cancel);
+                // parameters here will override the settings in the buttonMarkup
+                buttonAction: { theme: 'b' },
+                // theme added to button when it is active (e.g. shift is down)
+                // All extra parameters will be ignored
+                buttonActive: { theme: 'e' }
+            };
+            input.appendTo('body');
+            input.keyboard(extendedOptions).addMobile(mobileOptions);
+            return input;
+        }
     };
     $.fn.initPage = function() {
 
@@ -442,12 +478,12 @@ function scrollToQuestion(question) {
     var scrollContainer = $(question).offsetParent();
     var position = scrollContainer.find('#scroller').offset().top - $(question).offset().top;
 
-  /*  for (var j = 0; j < scrolls.length; j++) {
+    for (var j = 0; j < scrolls.length; j++) {
         if ($(scrolls[j].scroller).attr('class') == $(scrollContainer.find('#scroller')).attr('class')) {
             scrolls[j].refresh();
             scrolls[j].scrollTo(0, position, 1500, false);
         }
-    }*/
+    }
 }
 $(document).bind('pagebeforeshow', function () {
     var doc = $('#content_container');
