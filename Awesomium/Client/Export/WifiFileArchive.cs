baf2cb1612e11cf3f5970d5bd680013ff8a3c9ea
nikolay@toidev.com <nikolay@toidev.com>
2012-08-03 12:32:42 +0300

    export interface
    Former-commit-id: 1756b55f6d09592b853c4e74c9d615b01cfcff1b

---
+++ Awesomium/Client/Export/WifiFileArchive.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using Client.Properties;
+
+namespace Client
+{
+    class WifiFileArchive: ISaveFileArchive
+    {
+        private string network;
+        private List<string> targetList;
+
+        public WifiFileArchive()
+        {
+
+        }
+
+        public void SaveArchive(byte[] data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte[] LoadArchive()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string getTargetName()
+        {
+            throw new NotImplementedException();
+        }
+ 
+        public void FlushTargetList()
+        {
+            List<string> networks = new List<string>();
+            NetworkInterface[] networkCards = System.Net.NetworkInformation.NetworkInterface.GetAllNetworkInterfaces();
+            foreach (var networkInterface in networkCards)
+            {
+                if (networkInterface.OperationalStatus == OperationalStatus.Up)
+                {
+                    networks.Add(networkInterface.Name);
+                }
+
+            }
+            this.targetList = networks;
+        }
+    }
+}
