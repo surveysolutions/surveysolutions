3e3c287d009120a668247a151418b36721dcd2ce
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2012-08-06 12:57:26 -0400

    new user managing support
    Former-commit-id: 29a610e35db40158c5c885fc46bfff79862f72c0

---
+++ RavenQuestionnaire.Core/RavenQuestionnaire.Core/Domain/UserAR.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Ncqrs.Domain;
+using RavenQuestionnaire.Core.Entities.SubEntities;
+using RavenQuestionnaire.Core.Events.User;
+
+namespace RavenQuestionnaire.Core.Domain
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class UserAR :  AggregateRootMappedByConvention
+    {
+        private string _userName;
+        private string _email;
+        private string _password;
+        private bool _isLocked;
+        private UserRoles[] _roles;
+        
+
+        public UserAR()
+        {
+        }
+
+        public UserAR(Guid publicKey, string userName, string password, string email, UserRoles[] roles, bool isLocked)
+            : base(publicKey)
+        {
+
+            //Check for unique of person
+
+
+            ApplyEvent(new NewUserCreated()
+                           {
+                               Name = userName,
+                               Password = password,
+                               Email = email,
+                               IsLocked = isLocked,
+                               Roles = roles
+                           });
+        }
+
+        // Event handler for the NewUserCreated event. This method
+        // is automaticly wired as event handler based on convension.
+        protected void OnNewQuestionnaireCreated(NewUserCreated e)
+        {
+            _userName = e.Name;
+            _email = e.Email;
+            _password = e.Password;
+            _isLocked = e.IsLocked;
+            _roles = e.Roles;
+
+        }
+
+
+
+
+    }
+}
