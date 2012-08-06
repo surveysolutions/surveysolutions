c5e9bb416ac77f688bc656e4469c7c548f7dff6d
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2012-08-06 12:57:26 -0400

    new user managing support
    Former-commit-id: 29a610e35db40158c5c885fc46bfff79862f72c0

---
+++ RavenQuestionnaire.Core/RavenQuestionnaire.Core/Events/User/NewUserCreated.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Ncqrs.Eventing.Storage;
+using RavenQuestionnaire.Core.Entities.SubEntities;
+
+namespace RavenQuestionnaire.Core.Events.User
+{
+    [Serializable]
+    [EventName("RavenQuestionnaire.Core:Events:UserCreated")]
+    public class NewUserCreated
+    {
+        public Guid PublicKey { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool IsLocked { set; get; }
+
+        public UserRoles[] Roles { set; get; }
+
+    }
+}
