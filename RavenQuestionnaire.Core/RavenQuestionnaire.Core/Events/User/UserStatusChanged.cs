ad033926fe103d24dcb880f737960f71b8022606
Slava Scherbak <slava.scherbak@gmail.com>
2012-08-07 15:59:24 +0300

    add UserStatusChanged event, fix headers and footers on CAPI
    Former-commit-id: 0a2f0f2a89477d7e642f656677e35d778d56b291

---
+++ RavenQuestionnaire.Core/RavenQuestionnaire.Core/Events/User/UserStatusChanged.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Ncqrs.Eventing.Storage;
+using RavenQuestionnaire.Core.Entities.SubEntities;
+
+namespace RavenQuestionnaire.Core.Events.User
+{
+    [Serializable]
+    [EventName("RavenQuestionnaire.Core:Events:UserStatusChanged")]
+    public class UserStatusChanged
+    {
+        public Guid PublicKey { get; set; }
+        public bool IsLocked { set; get; }
+    }
+}
