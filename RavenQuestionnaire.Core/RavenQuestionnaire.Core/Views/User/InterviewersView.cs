b48b01e39bd2dab4d5da9608daa667bc9e15616e
Slava Scherbak <slava.scherbak@gmail.com>
2012-08-07 13:48:04 +0300

    Add Interviewer's views Add ChangeAssignment event
    Former-commit-id: a6b1789cee8e5237eab3766f2574583a0f04be7f

---
+++ RavenQuestionnaire.Core/RavenQuestionnaire.Core/Views/User/InterviewersView.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RavenQuestionnaire.Core.Views.User
+{
+    public class InterviewersView
+    {
+          public int PageSize
+        {
+            get;
+            private set;
+        }
+
+        public int Page
+        {
+            get;
+            private set;
+        }
+
+        public int TotalCount { get; private set; }
+
+        public IEnumerable<InterviewersItem> Items
+        {
+            get;
+            private set;
+        }
+
+        public InterviewersView(int page, int pageSize, int totalCount, IEnumerable<InterviewersItem> items)
+        {
+            this.Page = page;
+            this.TotalCount = totalCount;
+            this.PageSize = pageSize;
+            this.Items = items;
+        }
+    }
+}
