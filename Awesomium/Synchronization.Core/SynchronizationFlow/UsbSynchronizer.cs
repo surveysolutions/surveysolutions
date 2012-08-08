0f07fadde7d5e2b7ca42b1c68f63843003efd486
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2012-08-08 18:01:37 -0400

    usn pull
    Former-commit-id: ed6f4435c282fd855c1795c16e3981ff525e413d

--- Awesomium/Synchronization.Core/SynchronizationFlow/UsbSynchronizer.cs
+++ Awesomium/Synchronization.Core/SynchronizationFlow/UsbSynchronizer.cs
@@ -145,64 +145,16 @@ namespace Synchronization.Core.SynchronizationFlow
         }
         protected void SubmitFile(UsbFileArchive usbArchive)
         {
-            string boundary = "----------------------------" +
-                              DateTime.Now.Ticks.ToString("x");
-
-            HttpWebRequest httpWebRequest2 = (HttpWebRequest)WebRequest.Create("http://localhost:8083/Synchronizations/Import");
-            httpWebRequest2.ContentType = "multipart/form-data; boundary=" +
-                                          boundary;
-            httpWebRequest2.Method = "POST";
-            httpWebRequest2.KeepAlive = true;
-            httpWebRequest2.Credentials =
-                System.Net.CredentialCache.DefaultCredentials;
-            Stream memStream = new System.IO.MemoryStream();
-            byte[] boundarybytes = System.Text.Encoding.ASCII.GetBytes("\r\n--" +
-                                                                       boundary + "\r\n");
-            string formdataTemplate = "\r\n--" + boundary +
-                                      "\r\nContent-Disposition: form-data; name=\"{0}\";\r\n\r\n{1}";
-
+            ManualResetEvent done = new ManualResetEvent(false);
+            WebClient client = new WebClient();
+            client.UploadProgressChanged += (s, e) =>
+                                                {
+                                                    OnPullProgressChanged(new SynchronizationEvent((int)(((decimal)(e.TotalBytesToSend - e.BytesSent) / e.TotalBytesToSend) * 100)));
+                                                };
+            client.UploadFileCompleted += (s, e) => { done.Set(); };
             
-            string formitem = string.Format(formdataTemplate, "myfile", usbArchive.FileName);
-                byte[] formitembytes = System.Text.Encoding.UTF8.GetBytes(formitem);
-                memStream.Write(formitembytes, 0, formitembytes.Length);
-           
-            memStream.Write(boundarybytes, 0, boundarybytes.Length);
-
-            string headerTemplate =
-                "Content-Disposition: form-data; name=\"{0}\"; filename=\"{1}\"\r\n Content-Type: application/octet-stream\r\n\r\n";
-
-            string header = string.Format(headerTemplate, "myfile", usbArchive.FileName);
-
-            byte[] headerbytes = System.Text.Encoding.UTF8.GetBytes(header);
-
-            memStream.Write(headerbytes, 0, headerbytes.Length);
-
-            byte[] fromUsb = usbArchive.LoadArchive();
-            memStream.Write(fromUsb, 0, fromUsb.Length);
-            memStream.Write(boundarybytes, 0, boundarybytes.Length);
-            httpWebRequest2.ContentLength = memStream.Length;
-
-            Stream requestStream = httpWebRequest2.GetRequestStream();
-
-            memStream.Position = 0;
-            byte[] tempBuffer = new byte[memStream.Length];
-            memStream.Read(tempBuffer, 0, tempBuffer.Length);
-            memStream.Close();
-            requestStream.Write(tempBuffer, 0, tempBuffer.Length);
-            requestStream.Close();
-
-
-            WebResponse webResponse2 = httpWebRequest2.GetResponse();
-
-            Stream stream2 = webResponse2.GetResponseStream();
-            StreamReader reader2 = new StreamReader(stream2);
-
-
-            string response = reader2.ReadToEnd();
-
-            webResponse2.Close();
-            httpWebRequest2 = null;
-            webResponse2 = null;
+            client.UploadFileAsync(new Uri("http://localhost:8083/Synchronizations/Import"), usbArchive.FileName);
+            done.WaitOne();
         }
 
         #endregion
