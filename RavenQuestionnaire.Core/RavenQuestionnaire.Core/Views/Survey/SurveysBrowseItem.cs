89691efc2243bde6119d511e2c1d4096bb7feeb2
Juliya Pushkalova <julia@toidev.com>
2012-08-09 23:02:18 +0300

    beta asign
    Former-commit-id: 97b3cbdddbe2c49a1ad4332d7fa0c7f41fb48ca4

---
+++ RavenQuestionnaire.Core/RavenQuestionnaire.Core/Views/Survey/SurveysBrowseItem.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using RavenQuestionnaire.Core.Entities.SubEntities;
+
+namespace RavenQuestionnaire.Core.Views.Survey
+{
+    public class SurveysBrowseItem
+    {
+
+        public Guid Id { get; set; }
+
+        public SurveyStatus Status { get; set; }
+
+        public string TemplateId { get; set; }
+
+        public string Title { get; set; }
+
+        public UserLight Responsible { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public Dictionary<string, int> Statistics { get; set; }
+
+        public SurveysBrowseItem()
+        {
+            this.Statistics=new Dictionary<string, int>();
+        }
+
+        public SurveysBrowseItem(Guid id, string templateId, string title, UserLight responsible, SurveyStatus status):this()
+        {
+            this.Id = id;
+            this.EndDate = DateTime.Now;
+            this.StartDate = DateTime.Now;
+            this.TemplateId = templateId;
+            this.Title = title;
+            this.Responsible = responsible;
+            this.Status = status;
+        }
+    }
+}
