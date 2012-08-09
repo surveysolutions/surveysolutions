27c1775470e624a58ca097851b0db346a3cb8a8b
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2012-08-09 16:10:59 -0400

    clean up
    Former-commit-id: c810978e07fc0d9c882c88d3dea946514a3280b9

--- RavenQuestionnaire.Core/RavenQuestionnaire.Core/Views/Collection/CollectionBrowseViewFactory.cs
+++ RavenQuestionnaire.Core/RavenQuestionnaire.Core/Views/Collection/CollectionBrowseViewFactory.cs
@@ -1,24 +1,25 @@
 ﻿using System.Linq;
-using Raven.Client;
+using RavenQuestionnaire.Core.Denormalizers;
 using RavenQuestionnaire.Core.Documents;
 
 namespace RavenQuestionnaire.Core.Views.Collection
 {
     public class CollectionBrowseViewFactory : IViewFactory<CollectionBrowseInputModel, CollectionBrowseView>
     {
-        private IDocumentSession documentSession;
+        private readonly IDenormalizerStorage<CollectionDocument> documentItemSession;
 
-        public CollectionBrowseViewFactory(IDocumentSession documentSession)
+        public CollectionBrowseViewFactory(IDenormalizerStorage<CollectionDocument> documentItemSession)
         {
-            this.documentSession = documentSession;
+            this.documentItemSession = documentItemSession;
         }
 
         public CollectionBrowseView Load(CollectionBrowseInputModel input)
         {
-           var count = documentSession.Query<CollectionDocument>().Count();
+            var count = documentItemSession.Count();
             if (count == 0)
                 return new CollectionBrowseView(input.Page, input.PageSize, count, new CollectionBrowseItem[0]);
-            var query = documentSession.Query<CollectionDocument>().Skip((input.Page - 1) * input.PageSize).Take(input.PageSize).ToList();
+
+            var query = documentItemSession.Query().Skip((input.Page - 1) * input.PageSize).Take(input.PageSize).ToList();
             var items = query.Select(x => new CollectionBrowseItem(x.Id, x.Name)).ToArray();
             return new CollectionBrowseView(input.Page, input.PageSize, count, items);
         }
