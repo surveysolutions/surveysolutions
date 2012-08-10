ef2d59fa8213f3f5ca5d78c6114a86f9e0ce010d
Slava Scherbak <vs@cloudwk.com>
2012-08-10 18:03:24 +0300

    fix user view
    Former-commit-id: 2be37ef6734cb72c7305ef2d95383d278d308c97

--- RavenQuestionnaire.Core/RavenQuestionnaire.Core/Domain/UserAR.cs
+++ RavenQuestionnaire.Core/RavenQuestionnaire.Core/Domain/UserAR.cs
@@ -8,64 +8,84 @@ namespace RavenQuestionnaire.Core.Domain
     /// <summary>
     /// 
     /// </summary>
-    public class UserAR :  AggregateRootMappedByConvention
+    public class UserAR : AggregateRootMappedByConvention
     {
         private Guid _publicKey;
+
         private string _userName;
         private string _email;
         private string _password;
         private bool _isLocked;
         private UserRoles[] _roles;
-        
+        private UserLight _supervisor;
 
-        public UserAR()
-        {
-        }
+        public UserAR() { }
 
-        public UserAR(Guid publicKey, string userName, string password, string email, UserRoles[] roles, bool isLocked)
+        public UserAR(Guid publicKey, string userName, string password, string email, UserRoles[] roles,
+            bool isLocked, UserLight supervisor)
             : base(publicKey)
         {
 
-            //Check for unique of person
+            //Check for uniqueness of person name and email!
+
 
 
             ApplyEvent(new NewUserCreated()
-                           {
-                               PublicKey = publicKey,
-                               Name = userName,
-                               Password = password,
-                               Email = email,
-                               IsLocked = isLocked,
-                               Roles = roles
-                           });
+            {
+                Name = userName,
+                Password = password,
+                Email = email,
+                IsLocked = isLocked,
+                Roles = roles,
+                Supervisor = supervisor,
+                PublicKey = publicKey
+            });
         }
 
         // Event handler for the NewUserCreated event. This method
         // is automaticly wired as event handler based on convension.
         protected void OnNewQuestionnaireCreated(NewUserCreated e)
         {
-            _publicKey = e.PublicKey;
             _userName = e.Name;
             _email = e.Email;
             _password = e.Password;
             _isLocked = e.IsLocked;
             _roles = e.Roles;
-
+            _supervisor = e.Supervisor;
         }
 
-        public void ChangeUserStatus(string userId, bool isLocked)
+        public void ChangeUser(string email, bool isLocked, UserRoles[] roles)
         {
-            ApplyEvent(new UserStatusChanged()
+            ApplyEvent(new UserChanged()
             {
-                PublicKey = _publicKey,
-                IsLocked = isLocked
+                Email = email,
+                IsLocked = isLocked,
+                Roles = roles,
+                PublicKey = _publicKey
             });
         }
 
-        protected void OnLockStatusChanged(UserStatusChanged e)
+
+        public void SetUserLockState(bool isLocked)
+        {
+            ApplyEvent(new UserStatusChanged()
+            {
+                IsLocked = isLocked,
+                PublicKey = _publicKey
+            });
+        }
+
+        protected void OnSetUserLockState(UserStatusChanged e)
         {
             _isLocked = e.IsLocked;
         }
 
+        protected void OnUserChange(UserChanged e)
+        {
+            _email = e.Email;
+            _isLocked = e.IsLocked;
+            _roles = e.Roles;
+
+        }
     }
 }
