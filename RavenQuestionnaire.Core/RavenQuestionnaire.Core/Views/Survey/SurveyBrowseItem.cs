32bbf9fd2e9dfa04ccafe328702400e5e9a74edb
Juliya Pushkalova <julia@toidev.com>
2012-08-13 02:02:11 +0300

    commit survey statistic(prefinish, not working select users)
    Former-commit-id: ea3b07cb71d4a55cbd9767afe4a1ba945c8e1603

--- RavenQuestionnaire.Core/RavenQuestionnaire.Core/Views/Survey/SurveyBrowseItem.cs
+++ RavenQuestionnaire.Core/RavenQuestionnaire.Core/Views/Survey/SurveyBrowseItem.cs
@@ -13,16 +13,24 @@ namespace RavenQuestionnaire.Core.Views.Survey
         public UserLight Responsible { get; set; }
         public int UnAssignment { get; set; }
         public Dictionary<Guid, SurveyStatus> AllQuestionnaire { get; set; }
+        public Dictionary<string, int> Statistics { get; set; }
+        public Dictionary<string, string> FeatureadValue { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
 
         public SurveyBrowseItem()
         {
             this.AllQuestionnaire=new Dictionary<Guid, SurveyStatus>();
+            this.Statistics = new Dictionary<string, int>();
+            this.FeatureadValue=new Dictionary<string, string>();
         }
 
         public SurveyBrowseItem(Guid id, string title, string templateId, SurveyStatus status, UserLight responsible):this()
         {
             this.Id = id;
             this.Title = title;
+            this.EndDate = DateTime.Now;
+            this.StartDate = DateTime.Now;
             this.TemplateId = templateId;
             this.Status = status;
             this.Responsible = responsible;
