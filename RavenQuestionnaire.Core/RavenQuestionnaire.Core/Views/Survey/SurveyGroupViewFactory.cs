4438a364eeb7ffa00f2dae2eb42765840501efb9
Juliya Pushkalova <julia@toidev.com>
2012-08-13 02:02:34 +0300

    small changes
    Former-commit-id: 03dfd08cab3cefe1978531156a7e85578e4b7f9e

---
+++ RavenQuestionnaire.Core/RavenQuestionnaire.Core/Views/Survey/SurveyGroupViewFactory.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using RavenQuestionnaire.Core.Denormalizers;
+using RavenQuestionnaire.Core.Views.Statistics;
+using RavenQuestionnaire.Core.Views.CompleteQuestionnaire;
+
+namespace RavenQuestionnaire.Core.Views.Survey
+{
+    public class SurveyGroupViewFactory : IViewFactory<SurveyGroupInputModel, SurveyBrowseView>
+    {
+        private readonly IDenormalizerStorage<CompleteQuestionnaireBrowseItem> documentItemSession;
+
+        public SurveyGroupViewFactory(IDenormalizerStorage<CompleteQuestionnaireBrowseItem> documentItemSession)
+        {
+            this.documentItemSession = documentItemSession;
+        }
+
+        public SurveyBrowseView Load(SurveyGroupInputModel input)
+        {
+            var questionnaires = documentItemSession.Query().Where(x => x.TemplateId == input.Id).ToList<CompleteQuestionnaireBrowseItem>();
+            var model = new SurveyBrowseView();
+            foreach (var name in questionnaires.SelectMany(item => item.FeaturedQuestions.Select(t => t.QuestionText).Where(name => !model.Headers.Contains(name))))
+                model.Headers.Add(name);
+            if (!string.IsNullOrEmpty(input.QuestionnaireId))
+            {
+                var inputQuestionnaire = questionnaires.Where(t => t.CompleteQuestionnaireId == input.QuestionnaireId).FirstOrDefault();
+                questionnaires = new List<CompleteQuestionnaireBrowseItem>() { inputQuestionnaire };                
+            }
+            foreach (var item in questionnaires)
+            {
+                var surveyItem = new SurveyBrowseItem(Guid.Parse(item.CompleteQuestionnaireId),
+                                                     item.CompleteQuestionnaireId, item.TemplateId, item.Status,
+                                                     item.Responsible);
+                foreach (var nameField in model.Headers)
+                {
+                    var val = item.FeaturedQuestions.Where(t => t.QuestionText == nameField) as QuestionStatisticView;
+                    surveyItem.FeatureadValue.Add(nameField, (val!=null ? val.AnswerValue : string.Empty));
+                }
+                model.Items.Add(surveyItem);
+            }
+            return model;
+        }
+    }
+}
---
+++ RavenQuestionnaire.Core/RavenQuestionnaire.Core/Views/Survey/SurveyViewFactory.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using RavenQuestionnaire.Core.Denormalizers;
+using RavenQuestionnaire.Core.Entities.SubEntities;
+
+namespace RavenQuestionnaire.Core.Views.Survey
+{
+    public class SurveyViewFactory : IViewFactory<SurveyViewInputModel, SurveyBrowseView>
+    {
+        private readonly IDenormalizerStorage<SurveyBrowseItem> documentItemSession;
+
+        public SurveyViewFactory(IDenormalizerStorage<SurveyBrowseItem> documentItemSession)
+        {
+            this.documentItemSession = documentItemSession;
+        }
+
+        public SurveyBrowseView Load(SurveyViewInputModel input)
+        {
+            var model = new SurveyBrowseView();
+            var statuses = SurveyStatus.GetAllStatuses().Select(s => s.Name).ToList();
+            statuses.Insert(0, "UnAssignment");
+            model.Headers = statuses;
+            var alltemplate = documentItemSession.Query().Select(x => x.TemplateId).Distinct();
+            foreach (var template in alltemplate)
+            {
+                var title = documentItemSession.Query().Where(t => t.TemplateId == template).FirstOrDefault().Title;
+                var item = new SurveyBrowseItem(Guid.NewGuid(), title, template, null, null);
+                foreach (var statusename in statuses)
+                {
+                    int count = 0;
+                    if (statusename == "UnAssignment")
+                        count = documentItemSession.Query().Where(t => t.TemplateId == template).Sum(x => x.UnAssignment);
+                    else
+                        count += Enumerable.Count(documentItemSession.Query().Where(t => t.TemplateId == template), bitem => bitem.Status.Name == statusename && bitem.UnAssignment == 0);
+                    item.Statistics.Add(statusename, count);
+                }
+                model.Items.Add(item);
+            }
+            return model;
+        }
+    }
+}
