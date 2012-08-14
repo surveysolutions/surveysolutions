d7d9fd5700b75c29d8dfc66bfd3c1d64ad0b4fad
julia <none@none>
2012-08-14 17:09:32 +0300

    refactor views needs small fixes with display after assigment
    Former-commit-id: 7b88111def92bb91e59e8023d0ff5c7185eae1f2

--- RavenQuestionnaire.Web/Web.Supervisor/Controllers/SurveyController.cs
+++ RavenQuestionnaire.Web/Web.Supervisor/Controllers/SurveyController.cs
@@ -2,17 +2,15 @@
 using System;
 using System.Linq;
 using System.Web.Mvc;
-using Web.Supervisor.Models;
+using Questionnaire.Core.Web.Helpers;
 using RavenQuestionnaire.Core;
 using Ncqrs.Commanding.ServiceModel;
-using Questionnaire.Core.Web.Helpers;
 using RavenQuestionnaire.Core.Views.User;
 using RavenQuestionnaire.Core.Views.Survey;
 using RavenQuestionnaire.Core.Denormalizers;
 using RavenQuestionnaire.Core.Entities.SubEntities;
 using RavenQuestionnaire.Core.Views.CompleteQuestionnaire;
 using RavenQuestionnaire.Core.Commands.Questionnaire.Completed;
-
 
 
 namespace Web.Supervisor.Controllers
@@ -23,13 +21,13 @@ namespace Web.Supervisor.Controllers
         private readonly IDenormalizerStorage<CompleteQuestionnaireBrowseItem> documentItemSession;
         private IGlobalInfoProvider globalInfo;
 
-
-        public SurveyController(IViewRepository viewRepository,
-                                IDenormalizerStorage<CompleteQuestionnaireBrowseItem> documentItemSession, IGlobalInfoProvider provider)
+        public SurveyController(IViewRepository viewRepository, 
+                                IDenormalizerStorage<CompleteQuestionnaireBrowseItem> documentItemSession,
+             IGlobalInfoProvider globalInfo)
         {
             this.viewRepository = viewRepository;
             this.documentItemSession = documentItemSession;
-            this.globalInfo = provider;
+            this.globalInfo = globalInfo;
         }
 
         public ActionResult Index()
@@ -37,41 +35,39 @@ namespace Web.Supervisor.Controllers
             var model = viewRepository.Load<SurveyViewInputModel, SurveyBrowseView>(new SurveyViewInputModel());
             return View(model);
         }
-
+        
         public ActionResult Assigments(string id)
         {
-            var user = globalInfo.GetCurrentUser();
-            var users = viewRepository.Load<InterviewersInputModel, InterviewersView>(new InterviewersInputModel { Supervisor = user });
-            ViewBag.Users = new SelectList(users.Items, "Id", "Login");
             var model = viewRepository.Load<SurveyGroupInputModel, SurveyGroupView>(new SurveyGroupInputModel(id));
             return View(model);
         }
 
         [HttpGet]
-        public ActionResult Assign(string questionnaireId, string responsibleId, string responsibleName, int columnsCount)
+        public ActionResult Assign(UserBrowseInputModel input, string questionnaireId)
         {
             var user = globalInfo.GetCurrentUser();
             var users = viewRepository.Load<InterviewersInputModel, InterviewersView>(new InterviewersInputModel { Supervisor = user });
             ViewBag.Users = new SelectList(users.Items, "Id", "Login");
-            var model = new AssigmentModel()
-                            {
-                                CompleteQuestionnaireId = questionnaireId,
-                                Responsible = new UserLight(responsibleId, responsibleName),
-                                ColumnsCount = columnsCount
-                            };
-            return PartialView("EditColumn", model);
+            var questionnaire = documentItemSession.Query().Where(x=>x.CompleteQuestionnaireId==questionnaireId).SingleOrDefault();
+            var model = viewRepository.Load<SurveyGroupInputModel, SurveyGroupView>(new SurveyGroupInputModel(questionnaire.TemplateId, questionnaireId));
+            return PartialView("EditColumn", model.Items[0]);
         }
 
         [HttpPost]
-        public ActionResult Assign(string Id, string userId)
+        public ActionResult Assign(string Id, string userId, string Save, string Cancel)
         {
+
             var user = viewRepository.Load<UserViewInputModel, UserView>(new UserViewInputModel(userId));
-            var responsible = (user != null) ? new UserLight(user.UserId, user.UserName) : new UserLight();
-
-            var commandService = NcqrsEnvironment.Get<ICommandService>();
-            commandService.Execute(new ChangeAssignmentCommand(Guid.Parse(Id), responsible) );
-
-            return Json(new { status = "Ok" });
+            var responsible = (user!=null) ? new UserLight(user.UserId, user.UserName) : new UserLight();
+            if (!string.IsNullOrEmpty(Save))
+            {
+                var commandService = NcqrsEnvironment.Get<ICommandService>();
+                commandService.Execute(new ChangeAssignmentCommand(Guid.Parse(Id), responsible ));
+            }
+            var row = documentItemSession.Query().Where(x => x.CompleteQuestionnaireId == Id).SingleOrDefault();
+            var model = viewRepository.Load<SurveyGroupInputModel, SurveyGroupView>(new SurveyGroupInputModel(row.TemplateId, Id));
+            return PartialView("DisplayColumn", model.Items[0]);
         }
+
     }
 }
