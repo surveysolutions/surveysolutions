5b0bc803569282b3fce5c3a62917a70f58f2baa4
Juliya Pushkalova <julia@toidev.com>
2012-08-14 17:01:32 +0300

    error RavenQuestionnaire.Core.Events.Questionnaire.QuestionnaireTemplateLocaded event
    Former-commit-id: 7cd3e5a317c1d9c9c0f1967e3ad79933b6be5d37

--- RavenQuestionnaire.Core/RavenQuestionnaire.Core/Views/Survey/SurveyGroupItem.cs
+++ RavenQuestionnaire.Core/RavenQuestionnaire.Core/Views/Survey/SurveyGroupItem.cs
@@ -1,30 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using RavenQuestionnaire.Core.Entities.SubEntities;
+using RavenQuestionnaire.Core.Views.CompleteQuestionnaire;
 
 namespace RavenQuestionnaire.Core.Views.Survey
 {
     public class SurveyGroupItem
     {
         public string Title { get; set; }
-        public Guid Id { get; set; }
+        public string Id { get; set; }
         public UserLight Responsible { get; set; }
         public string TemplateId { get; set; }
         public SurveyStatus Status { get; set; }
-        public Dictionary<string, string> FeatureadValue { get; set; }
+        public Dictionary<Guid, string> FeatureadValue { get; set; }
         
-        public SurveyGroupItem()
+        private SurveyGroupItem()
         {
-            this.FeatureadValue = new Dictionary<string, string>();
+            this.FeatureadValue = new Dictionary<Guid, string>();
         }
 
-        public SurveyGroupItem(Guid id, string title, string templateId, SurveyStatus status, UserLight responsible):this()
+        public SurveyGroupItem(CompleteQuestionnaireBrowseItem it, Dictionary<Guid, string> headers):this()
         {
-            this.Id = id;
-            this.Title = title;
-            this.TemplateId = templateId;
-            this.Status = status;
-            this.Responsible = responsible;
+            this.Title = it.QuestionnaireTitle;
+            this.Id = it.CompleteQuestionnaireId;
+            this.Responsible = it.Responsible;
+            this.TemplateId = it.TemplateId;
+            this.Status = it.Status;
+            foreach (var header in headers)
+            {
+                var question = it.FeaturedQuestions.FirstOrDefault(t=>t.PublicKey==header.Key);
+                this.FeatureadValue.Add(header.Key, question != null ? question.AnswerValue : string.Empty);
+            }
         }
     }
 }
