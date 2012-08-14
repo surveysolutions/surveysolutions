b38fd7ca080e1b06fbe32fb7961d114b7031ffe0
Juliya Pushkalova <julia@toidev.com>
2012-08-14 14:26:43 +0300

    local commit
    Former-commit-id: f76f578933beb6997b133d66ad6e066737e09a92

--- RavenQuestionnaire.Core/RavenQuestionnaire.Core/EventHandlers/StatisticQuestionnaireBrowseItemDenormalizer.cs
+++ RavenQuestionnaire.Core/RavenQuestionnaire.Core/EventHandlers/StatisticQuestionnaireBrowseItemDenormalizer.cs
@@ -1,4 +1,7 @@
-﻿using RavenQuestionnaire.Core.Events;
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using RavenQuestionnaire.Core.Events;
 using Ncqrs.Eventing.ServiceModel.Bus;
 using RavenQuestionnaire.Core.Views.Survey;
 using RavenQuestionnaire.Core.Denormalizers;
@@ -14,31 +17,58 @@ namespace RavenQuestionnaire.Core.EventHandlers
     {
 
         private IDenormalizerStorage<SurveyBrowseItem> documentItemStore;
-        public int UnAssignment { get; set; }
-
 
         public StatisticQuestionnaireBrowseItemDenormalizer(IDenormalizerStorage<SurveyBrowseItem> documentItemStore)
         {
             this.documentItemStore = documentItemStore;
-
         }
 
         public void Handle(IPublishedEvent<NewCompleteQuestionnaireCreated> evnt)
         {
-            this.documentItemStore.Store(new SurveyBrowseItem(evnt.Payload.CompletedQuestionnaireId, evnt.Payload.QuestionnaireId, evnt.Payload.QuestionnaireId, evnt.Payload.Status, evnt.Payload.Responsible), evnt.Payload.CompletedQuestionnaireId);
-            if (evnt.Payload.Responsible == null)
-                UnAssignment++;
+            var item =
+                this.documentItemStore.Query().Where(t => t.Id==evnt.Payload.QuestionnaireId.ToString()).FirstOrDefault();
+            if (item == null)
+            {
+                var surveyitem = new SurveyItem(evnt.Payload.CreationDate, evnt.Payload.CreationDate, 
+                    evnt.Payload.QuestionnaireId, evnt.Payload.Status, evnt.Payload.Responsible);
+                var statistic = new Dictionary<Guid, SurveyItem> { { evnt.Payload.CompletedQuestionnaireId, surveyitem } };
+                this.documentItemStore.Store(
+                    new SurveyBrowseItem(
+                        evnt.Payload.QuestionnaireId.ToString(),
+                        evnt.Payload.Questionnaire.Title,
+                        evnt.Payload.Responsible == null ? 1 : 0, 
+                        statistic), 
+                    evnt.Payload.CompletedQuestionnaireId);
+            }
+            else
+            {
+                if (evnt.Payload.Responsible == null)
+                    item.UnAssigment++;
+                item.Statistic.Add(
+                    evnt.Payload.QuestionnaireId, 
+                    new SurveyItem(
+                        evnt.Payload.CreationDate, 
+                        evnt.Payload.CreationDate, 
+                        evnt.Payload.CompletedQuestionnaireId, 
+                        evnt.Payload.Status, evnt.Payload.Responsible));
+            }
         }
 
         public void Handle(IPublishedEvent<QuestionnaireStatusChanged> evnt)
         {
-            
 
         }
 
         public void Handle(IPublishedEvent<QuestionnaireAssignmentChanged> evnt)
         {
-            UnAssignment--;
+            var list = documentItemStore.Query().ToList();
+            foreach (var item in list.Where(i => i.Statistic.Any(surveyItem => surveyItem.Key == evnt.Payload.CompletedQuestionnaireId)))
+            {
+                SurveyItem val = item.Statistic.Where(t => t.Key == evnt.Payload.CompletedQuestionnaireId).Select(t=>t.Value).FirstOrDefault();
+                if (val.Responsible == null)
+                    item.UnAssigment--;
+                val.Responsible = evnt.Payload.Responsible;
+            }
         }
 
         public void Handle(IPublishedEvent<CompleteQuestionnaireDeleted> evnt)
--- RavenQuestionnaire.Core/RavenQuestionnaire.Core/Views/Survey/SurveyBrowseItem.cs
+++ RavenQuestionnaire.Core/RavenQuestionnaire.Core/Views/Survey/SurveyBrowseItem.cs
@@ -1,36 +1,62 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 using RavenQuestionnaire.Core.Entities.SubEntities;
 
 namespace RavenQuestionnaire.Core.Views.Survey
 {
+    
     public class SurveyBrowseItem
     {
-        public Guid Id { get; set; }
         public string Title { get; set; }
-        public UserLight Responsible { get; set; }
-        public string TemplateId { get; set; }
-        public SurveyStatus Status { get; set; }
-        public Dictionary<string, int> Statistics { get; set; }
-        public Dictionary<string, string> FeatureadValue { get; set; }
-        public DateTime StartDate { get; set; }
-        public DateTime EndDate { get; set; }
+        public string Id { get; set; }
+        public int UnAssigment { get; set; }
+        public Dictionary<Guid, SurveyItem> Statistic { get; set; }
+        public Dictionary<string, int> Grid { get; set; }
 
         public SurveyBrowseItem()
         {
-            this.Statistics = new Dictionary<string, int>();
-            this.FeatureadValue = new Dictionary<string, string>();
+            this.Grid = new Dictionary<string, int>();
+            var statuses = SurveyStatus.GetAllStatuses().Select(s => s.Name).ToList();
+            statuses.Insert(0, "Unassign");
+            foreach (var statuse in statuses)
+                this.Grid.Add(statuse, 0);
         }
 
-        public SurveyBrowseItem(Guid id, string title, string templateId, SurveyStatus status, UserLight responsible):this()
+        public SurveyBrowseItem(string id, string title, int unAssigment, Dictionary<Guid, SurveyItem> statistic):this()
         {
             this.Id = id;
             this.Title = title;
-            this.EndDate = DateTime.Now;
-            this.StartDate = DateTime.Now;
-            this.TemplateId = templateId;
+            this.UnAssigment = unAssigment;
+            this.Grid["Unassign"] = unAssigment;
+            this.Statistic = statistic;
+            var items = statistic.Values.Where(t=>t.Responsible!=null).GroupBy(r => r.Status).Select(g => new { Status = g.Key, Count = g.Count() }).ToList();
+            foreach (var item in items)
+            {
+                if (Grid.ContainsKey(item.Status.Name))
+                    Grid[item.Status.Name] += 1;
+                else
+                    Grid.Add(item.Status.Name, 1);
+            }
+        }
+    }
+
+
+    public class SurveyItem
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public Guid CompleteQuestionnaireId { get; set; }
+        public SurveyStatus Status { get; set; }
+        public UserLight Responsible { get; set; }
+
+        public SurveyItem(DateTime startDate, DateTime endDate, Guid completeQuestionnaireId, SurveyStatus status, UserLight responsible)
+        {
             this.Status = status;
+            this.EndDate = endDate;
+            this.StartDate = startDate;
             this.Responsible = responsible;
+            this.CompleteQuestionnaireId = completeQuestionnaireId;
         }
     }
 }
