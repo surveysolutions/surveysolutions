a838f1995cdd14f242de4e9fa58bf06c92538c83
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2012-08-15 15:59:35 -0400

    unbrackable transactions
    Former-commit-id: 083191692fc0fb045b49d3e57875066288f7c974

--- RavenQuestionnaire.Core/RavenQuestionnaire.Core/Utility/EnumerableExtensions.cs
+++ RavenQuestionnaire.Core/RavenQuestionnaire.Core/Utility/EnumerableExtensions.cs
@@ -18,5 +18,38 @@ namespace RavenQuestionnaire.Core.Utility
                 source = source.Skip(chunksize);
             }
         }
+        /// <summary>
+        /// Break a list of items into chunks of a specific size
+        /// </summary>
+        public static IEnumerable<IEnumerable<T>> Chunk<T>(this IEnumerable<T> source, int chunksize, Func<T,T,  bool> skipCondition) where T:class 
+        {
+
+            while (source.Any())
+            {
+                T previous = null;
+                var result = source.TakeWhile((x, i) =>
+                                                  {
+                                                      var isSkip= i < chunksize ||
+                                                             (i >= chunksize && skipCondition(x, previous));
+                                                      previous = x;
+                                                      return isSkip;
+                                                  });
+                yield return result;
+                //   chunksize = chunksize + justSkipped;
+                source = source.Skip(result.Count());
+            }
+            
+        }
+        /// <summary>
+        /// Break a list of items into chunks of a specific size
+        /// </summary>
+        public static IEnumerable<IEnumerable<T>> Chunk<T>(this IEnumerable<T> source, int chunksize, Func<T, bool> skipCondition)
+        {
+            while (source.Any())
+            {
+                yield return source.TakeWhile((x, i) => i < chunksize || (i >= chunksize && skipCondition(x)));
+                source = source.SkipWhile((x, i) => i < chunksize || (i >= chunksize && skipCondition(x)));
+            }
+        }
     }
 }
