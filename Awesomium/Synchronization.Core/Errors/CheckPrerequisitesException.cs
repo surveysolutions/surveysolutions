fc584a3da1632cf84b4415b465448bb9b5b69494
Anastasya Kholodova <nastya.kholodova@gmail.com>
2012-08-20 13:18:23 -0400

    Prerequisites
    Former-commit-id: 6bb7fa24c8b4a4312b3089b62c4443f766318fda

---
+++ Awesomium/Synchronization.Core/Errors/CheckPrerequisitesException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Synchronization.Core.SynchronizationFlow;
+
+namespace Synchronization.Core.Errors
+{
+    public class CheckPrerequisitesException : SynchronizationException
+    {
+        public SyncType SychronizationType { get; private set; }
+
+        public CheckPrerequisitesException(string message,SyncType type, Exception innerException)
+            : base(message, innerException)
+        {
+            this.SychronizationType = type;
+        }
+    }
+}
