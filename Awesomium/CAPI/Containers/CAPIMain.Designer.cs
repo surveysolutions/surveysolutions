c40f2b48d0e43323220e774852c9fde5445ed6fd
Anastasya Kholodova <nastya.kholodova@gmail.com>
2012-08-23 19:58:16 -0400

    big buttons reogange
    Former-commit-id: e9c6f99f33c2ebb2571017a937cc1bc54b871bb8

---
+++ Awesomium/CAPI/Containers/CAPIMain.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Browsing.CAPI.Containers
+{
+    partial class CAPIMain
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CAPIMain));
+            this.btnLogin = new Browsing.CAPI.Controls.FlatButton();
+            this.btnDashboard = new Browsing.CAPI.Controls.FlatButton();
+            this.btnSyncronization = new Browsing.CAPI.Controls.FlatButton();
+            this.btnSettings = new Browsing.CAPI.Controls.FlatButton();
+            this.btnLogout = new Browsing.CAPI.Controls.FlatButton();
+            this.SuspendLayout();
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.FlatAppearance.BorderSize = 0;
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.Image = ((System.Drawing.Image)(resources.GetObject("btnLogin.Image")));
+            this.btnLogin.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnLogin.Location = new System.Drawing.Point(0, 0);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(0);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(200, 200);
+            this.btnLogin.TabIndex = 0;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(btnLogin_Click);
+            // 
+            // btnDashboard
+            // 
+            this.btnDashboard.FlatAppearance.BorderSize = 0;
+            this.btnDashboard.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDashboard.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDashboard.Image = ((System.Drawing.Image)(resources.GetObject("btnDashboard.Image")));
+            this.btnDashboard.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnDashboard.Location = new System.Drawing.Point(240, 0);
+            this.btnDashboard.Margin = new System.Windows.Forms.Padding(0);
+            this.btnDashboard.Name = "btnDashboard";
+            this.btnDashboard.Size = new System.Drawing.Size(200, 200);
+            this.btnDashboard.TabIndex = 1;
+            this.btnDashboard.Text = "Dashboard";
+            this.btnDashboard.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnDashboard.UseVisualStyleBackColor = true;
+            this.btnDashboard.Click += new System.EventHandler(this.btnDashboard_Click);
+            // 
+            // btnSyncronization
+            // 
+            this.btnSyncronization.FlatAppearance.BorderSize = 0;
+            this.btnSyncronization.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSyncronization.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSyncronization.Image = ((System.Drawing.Image)(resources.GetObject("btnSyncronization.Image")));
+            this.btnSyncronization.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnSyncronization.Location = new System.Drawing.Point(480, 0);
+            this.btnSyncronization.Margin = new System.Windows.Forms.Padding(0);
+            this.btnSyncronization.Name = "btnSyncronization";
+            this.btnSyncronization.Size = new System.Drawing.Size(200, 200);
+            this.btnSyncronization.TabIndex = 2;
+            this.btnSyncronization.Text = "Synchronization";
+            this.btnSyncronization.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnSyncronization.UseVisualStyleBackColor = true;
+            this.btnSyncronization.Click += new System.EventHandler(this.btnSyncronization_Click);
+            // 
+            // btnSettings
+            // 
+            this.btnSettings.FlatAppearance.BorderSize = 0;
+            this.btnSettings.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSettings.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSettings.Image = ((System.Drawing.Image)(resources.GetObject("btnSettings.Image")));
+            this.btnSettings.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnSettings.Location = new System.Drawing.Point(720, 0);
+            this.btnSettings.Margin = new System.Windows.Forms.Padding(0);
+            this.btnSettings.Name = "btnSettings";
+            this.btnSettings.Size = new System.Drawing.Size(200, 200);
+            this.btnSettings.TabIndex = 3;
+            this.btnSettings.Text = "Settings";
+            this.btnSettings.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnSettings.UseVisualStyleBackColor = true;
+            this.btnSettings.Click += new System.EventHandler(this.btnSettings_Click);
+            // 
+            // btnLogout
+            // 
+            this.btnLogout.FlatAppearance.BorderSize = 0;
+            this.btnLogout.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogout.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogout.Image = ((System.Drawing.Image)(resources.GetObject("btnLogout.Image")));
+            this.btnLogout.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnLogout.Location = new System.Drawing.Point(0, 0);
+            this.btnLogout.Margin = new System.Windows.Forms.Padding(0);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Size = new System.Drawing.Size(200, 200);
+            this.btnLogout.TabIndex = 4;
+            this.btnLogout.Text = "Logout";
+            this.btnLogout.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnLogout.UseVisualStyleBackColor = true;
+            this.btnLogout.Visible = false;
+            this.btnLogout.Click += new System.EventHandler(this.btnLogout_Click);
+            // 
+            // CAPIMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.Controls.Add(this.btnLogout);
+            this.Controls.Add(this.btnSettings);
+            this.Controls.Add(this.btnSyncronization);
+            this.Controls.Add(this.btnDashboard);
+            this.Controls.Add(this.btnLogin);
+            this.Margin = new System.Windows.Forms.Padding(0);
+            this.Name = "CAPIMain";
+            this.Size = new System.Drawing.Size(920, 200);
+            this.ResumeLayout(false);
+
+        }
+
+        
+    
+        #endregion
+
+        private Controls.FlatButton btnLogin;
+        private Controls.FlatButton btnDashboard;
+        private Controls.FlatButton btnSyncronization;
+        private Controls.FlatButton btnSettings;
+        private Controls.FlatButton btnLogout;
+    }
+}
