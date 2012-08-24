114d107af0444f78c535d6a49d3a57f1a9515e4e
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2012-08-23 20:13:19 -0400

    fix build
    Former-commit-id: 6c54cb99423ee9cca0d6264a6479c3b842ad5c84

---
+++ RavenQuestionnaire.Core/RavenQuestionnaire.Core/Entities/SubEntities/VisitedGroup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace RavenQuestionnaire.Core.Entities.SubEntities
+{
+    public class VisitedGroup
+    {
+        public VisitedGroup(Guid groupKey, Guid? propagationKey)
+        {
+            GroupKey = groupKey;
+            PropagationKey = propagationKey;
+        }
+
+        public Guid GroupKey { get; private set; }
+        public Guid? PropagationKey { get; private set; }
+    }
+}
