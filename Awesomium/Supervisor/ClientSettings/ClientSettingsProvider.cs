c8447056b7a457602d9564e8e1b10be25efd05f0
julia <none@none>
2012-08-29 11:14:09 +0300

    added start page on supervisor browser
    Former-commit-id: be5da8d959a64d6fcdb27b9dd554146a6a475a17

---
+++ Awesomium/Supervisor/ClientSettings/ClientSettingsProvider.cs
@@ -0,0 +1,35 @@
+﻿using Synchronization.Core.Interface;
+
+namespace Browsing.Supervisor.ClientSettings
+{
+    public class ClientSettingsProvider : ISettingsProvider
+    {
+        #region Properties
+
+        private ISettings clientSettings;
+
+        public ISettings Settings
+        {
+            get {
+                return this.clientSettings ??
+                       (this.clientSettings =
+                        new ClientSettings(Properties.Settings.Default.ClientId, Properties.Settings.Default.ParentId));
+            }
+        }
+
+        #endregion
+
+        #region Constructor
+
+        public ClientSettingsProvider()
+        {
+        }
+
+        public ClientSettingsProvider(ISettings clientSettings)
+        {
+            this.clientSettings = clientSettings;
+        }
+
+        #endregion
+    }
+}
