4027819e33c62b138f26a537c1eb52867305b423
julia <none@none>
2012-08-30 13:35:44 +0300

    small fixes
    Former-commit-id: 484240465acdb5b514c78ac14388bb2cc752e3b1

---
+++ Awesomium/Supervisor/Containers/SyncCapiProcessPage.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Browsing.Supervisor.Containers
+{
+    using Browsing.Supervisor.Controls;
+
+    using Common.Utils;
+
+    using global::Synchronization.Core.Events;
+    using global::Synchronization.Core.Interface;
+
+    public partial class SyncCapiProcessPage : Screen
+    {
+        #region Fields
+
+        private bool repaint;
+        //private PleaseWaitControl pleaseWait;
+        //private CapiSyncManager syncManager;
+        private StatusStrip statusStrip1;
+        private ISettingsProvider clientSettings;
+
+        #endregion
+
+        public SyncCapiProcessPage(ISettingsProvider clientSettings, IRequesProcessor requestProcessor, IUrlUtils utils, ScreenHolder holder)
+            : base(holder, true)
+        {
+            InitializeComponent();
+            //this.pleaseWait = new PleaseWaitControl();
+            //this.clientSettings = clientSettings;
+            //this.syncManager = new CapiSyncManager(this.pleaseWait, this.clientSettings, requestProcessor, utils);
+            //this.syncManager.EndOfSync += new EventHandler<SynchronizationCompletedEvent>(sync_EndOfSync);
+            //this.syncManager.BgnOfSync += new EventHandler<SynchronizationEvent>(sync_BgnOfSync);
+            this.statusStrip1.Hide();
+            //var host = new ToolStripControlHost(this.pleaseWait);
+            //host.Size = this.statusStrip1.Size;
+            //this.statusStrip1.Items.AddRange(new ToolStripItem[] {host});
+        }
+
+        private void EnableDisableMenuItems(bool enable)
+        {
+            this.btnPull.Enabled = enable;
+            this.btnPush.Enabled = enable;
+            this.btnCancel.Visible = !enable;
+        }
+
+        private void sync_EndOfSync(object sender, SynchronizationCompletedEvent e)
+        {
+            if (this.InvokeRequired)
+                this.Invoke(new MethodInvoker(() =>
+                                                  {
+                                                      MessageBox.Show(this, e.Log);
+
+                                                      EnableDisableMenuItems(true);
+                                                  }));
+        }
+
+        private void sync_BgnOfSync(object sender, SynchronizationEvent e)
+        {
+            if (this.InvokeRequired)
+                this.Invoke(new MethodInvoker(() =>
+                                                  {
+                                                      EnableDisableMenuItems(false);
+                                                  }));
+        }
+
+        #region Event Handlers
+
+        private void btnPush_Click(object sender, EventArgs e)
+        {
+            //try
+            //{
+            //    this.syncManager.ExportQuestionaries();
+            //}
+            //catch
+            //{
+            //}
+
+        }
+
+        private void btnPull_Click(object sender, EventArgs e)
+        {
+            //try
+            //{
+            //    this.syncManager.ImportQuestionaries();
+            //}
+            //catch
+            //{
+            //}
+
+        }
+
+        #endregion
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            //try
+            //{
+            //    this.syncManager.Stop();
+            //}
+            //catch
+            //{
+            //}
+        }
+
+        private void HQSynchronization_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected override void OnUpdateConfigDependencies()
+        {
+            //base.OnUpdateConfigDependencies();
+            //this.syncManager.UpdateSynchronizersList();
+        }
+    }
+}
--- RavenQuestionnaire.Web/Web.Supervisor/Controllers/SynchronizationController.cs
+++ RavenQuestionnaire.Web/Web.Supervisor/Controllers/SynchronizationController.cs
@@ -1,15 +1,26 @@
 ﻿using System.Web.Mvc;
+using RavenQuestionnaire.Core;
+using Questionnaire.Core.Web.Export;
+using Questionnaire.Core.Web.Helpers;
+using RavenQuestionnaire.Core.Events;
 
 namespace Web.Supervisor.Controllers
 {
+
     public class SynchronizationController : Controller
     {
-        //
-        // GET: /Synchronization/
+        private readonly IGlobalInfoProvider _globalProvider;
+        private readonly IViewRepository viewRepository;
+        private readonly IExportImport exportimportEvents ;
+        private readonly IEventSync synchronizer;
 
-        public ActionResult Index()
+        public SynchronizationController( IViewRepository viewRepository,
+                                          IGlobalInfoProvider globalProvider, IExportImport exportImport, IEventSync synchronizer)
         {
-            return View();
+            this.exportimportEvents = exportImport;
+            this.viewRepository = viewRepository;
+            this._globalProvider = globalProvider;
+            this.synchronizer = synchronizer;
         }
     }
 }
