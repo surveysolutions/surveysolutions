00583515f7fe838580e77a9679e15f7fc12a9332
Slava Scherbak <vs@cloudwk.com>
2012-08-31 11:59:21 +0300

    fix bug with link to user
    Former-commit-id: ec3d1c6997474b374d4c69d7d838b8f4ab549bd4

--- RavenQuestionnaire.Web/Web.Supervisor/Controllers/ImportExportController.cs
+++ RavenQuestionnaire.Web/Web.Supervisor/Controllers/ImportExportController.cs
@@ -10,12 +10,6 @@ namespace Web.Supervisor.Controllers
     {
         private readonly IExportImport exportimportEvents;
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="ImportExportController"/> class.
-        /// </summary>
-        /// <param name="exportImport">
-        /// The export import.
-        /// </param>
         public ImportExportController(IExportImport exportImport)
         {
             exportimportEvents = exportImport;
@@ -23,21 +17,12 @@ namespace Web.Supervisor.Controllers
 
         #region PublicMethod
 
-        /// <summary>
-        /// </summary>
-        /// <returns>
-        /// </returns>
         [AcceptVerbs(HttpVerbs.Get)]
         public ActionResult Import()
         {
             return View("ViewTestUploadFile");
         }
 
-        /// <summary>
-        /// </summary>
-        /// <param name="myfile">
-        /// The myfile.
-        /// </param>
         [AcceptVerbs(HttpVerbs.Post)]
         public void ImportAsync(HttpPostedFileBase myfile)
         {
@@ -45,55 +30,40 @@ namespace Web.Supervisor.Controllers
             {
                 AsyncManager.OutstandingOperations.Increment();
                 AsyncQuestionnaireUpdater.Update(() =>
-                                                     {
-                                                         exportimportEvents.Import(myfile);
-                                                         AsyncManager.OutstandingOperations.Decrement();
-                                                     });
+                {
+                    exportimportEvents.Import(myfile);
+                    AsyncManager.OutstandingOperations.Decrement();
+                });
             }
         }
 
-        /// <summary>
-        /// </summary>
-        /// <returns>
-        /// </returns>
         public ActionResult ImportCompleted()
         {
             return RedirectToAction("Index", "Dashboard");
         }
 
-        /// </summary>
-        /// <param name="clientGuid">
-        /// The client guid.
-        /// </param>
         public void ExportAsync(Guid? clientGuid)
         {
             if (!clientGuid.HasValue || clientGuid.Value == Guid.Empty)
                 return;
-            
+
 
             AsyncManager.OutstandingOperations.Increment();
             AsyncQuestionnaireUpdater.Update(() =>
-                                                 {
-                                                     try
-                                                     {
-                                                         AsyncManager.Parameters["result"] =
-                                                             exportimportEvents.Export(clientGuid.Value);
-                                                     }
-                                                     catch
-                                                     {
-                                                         AsyncManager.Parameters["result"] = null;
-                                                     }
-                                                     AsyncManager.OutstandingOperations.Decrement();
-                                                 });
+            {
+                try
+                {
+                    AsyncManager.Parameters["result"] =
+                        exportimportEvents.Export(clientGuid.Value);
+                }
+                catch
+                {
+                    AsyncManager.Parameters["result"] = null;
+                }
+                AsyncManager.OutstandingOperations.Decrement();
+            });
         }
 
-        /// <summary>
-        /// </summary>
-        /// <param name="result">
-        /// The result.
-        /// </param>
-        /// <returns>
-        /// </returns>
         public ActionResult ExportCompleted(byte[] result)
         {
             return File(result, "application/zip",
