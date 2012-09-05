5d8225f680998eb46c7aee200a393913f65d16b8
nikolay@toidev.com <nikolay@toidev.com>
2012-09-05 14:46:43 +0300

    mutex: messagebox customization
    Former-commit-id: a2084dc8fef66e056a7c222f443f6dd66ddd5062

--- Awesomium/Supervisor/Program.cs
+++ Awesomium/Supervisor/Program.cs
@@ -11,48 +11,47 @@ namespace Supervisor
 {
     internal static class Program
     {
-      
+
 
         private static void Main(string[] args)
         {
             bool createdNew = true;
             using (Mutex mutex = new Mutex(true, "Supervisor", out createdNew))
             {
-               if (!createdNew)
-               {
-                   MessageBox.Show("One copy of the application is already running");
-                   return;
-               }
-               else
+                if (!createdNew)
                 {
-                    // Checks if this is a child rendering process and if so,
-                    // transfers control of the process to Awesomium.
-                    if (WebCore.IsChildProcess)
-                    {
-                        WebCore.ChildProcessMain();
-                        // When our process is not used any more, exit it.
-                        return;
-                    }
-
-                    Application.EnableVisualStyles();
-                    Application.SetCompatibleTextRenderingDefault(false);
-
-                    // Using our executable as a child rendering process, is not
-                    // available when debugging in VS.
-                    if (!Process.GetCurrentProcess().ProcessName.EndsWith("vshost"))
-                    {
-                        // Initialize the WebCore specifying that this executable
-                        // can be used as a child rendering process.
-                        WebCore.Initialize(new WebCoreConfig()
-                                               {
-                                                   ChildProcessPath = WebCoreConfig.CHILD_PROCESS_SELF,
-                                                   LogLevel = LogLevel.Verbose,
-                                               });
-                    }
-
-                    Application.Run(new WebForm());
+                    MessageBox.Show("One copy of the Supervisor application is already running", "Launch error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
                 }
-                
+
+                // Checks if this is a child rendering process and if so,
+                // transfers control of the process to Awesomium.
+                if (WebCore.IsChildProcess)
+                {
+                    WebCore.ChildProcessMain();
+                    // When our process is not used any more, exit it.
+                    return;
+                }
+
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+
+                // Using our executable as a child rendering process, is not
+                // available when debugging in VS.
+                if (!Process.GetCurrentProcess().ProcessName.EndsWith("vshost"))
+                {
+                    // Initialize the WebCore specifying that this executable
+                    // can be used as a child rendering process.
+                    WebCore.Initialize(new WebCoreConfig()
+                                           {
+                                               ChildProcessPath = WebCoreConfig.CHILD_PROCESS_SELF,
+                                               LogLevel = LogLevel.Verbose,
+                                           });
+                }
+
+                Application.Run(new WebForm());
+
+
             }
         }
     }
