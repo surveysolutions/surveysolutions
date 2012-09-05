6e38e202dac8f657688638843c4d8803315166bb
nikolay@toidev.com <nikolay@toidev.com>
2012-09-05 14:46:43 +0300

    mutex: messagebox customization
    Former-commit-id: a2084dc8fef66e056a7c222f443f6dd66ddd5062

--- Awesomium/CAPI/Program.cs
+++ Awesomium/CAPI/Program.cs
@@ -19,38 +19,38 @@ namespace Browsing.CAPI
             {
                 if (!createdNew)
                 {
-                    MessageBox.Show("One copy of the application is already running");
+                    MessageBox.Show("One copy of the CAPI application is already running", "Launch error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                     return;
                 }
-                else
+
+
+                // Checks if this is a child rendering process and if so,
+                // transfers control of the process to Awesomium.
+                if (WebCore.IsChildProcess)
                 {
-                    // Checks if this is a child rendering process and if so,
-                    // transfers control of the process to Awesomium.
-                    if (WebCore.IsChildProcess)
-                    {
-                        WebCore.ChildProcessMain();
-                        // When our process is not used any more, exit it.
-                        return;
-                    }
-
-                    Application.EnableVisualStyles();
-                    Application.SetCompatibleTextRenderingDefault(false);
-
-                    // Using our executable as a child rendering process, is not
-                    // available when debugging in VS.
-                    if (!Process.GetCurrentProcess().ProcessName.EndsWith("vshost"))
-                    {
-                        // Initialize the WebCore specifying that this executable
-                        // can be used as a child rendering process.
-                        WebCore.Initialize(new WebCoreConfig()
-                                               {
-                                                   ChildProcessPath = WebCoreConfig.CHILD_PROCESS_SELF,
-                                                   LogLevel = LogLevel.Verbose,
-                                               });
-                    }
-
-                    Application.Run(new WebForm());
+                    WebCore.ChildProcessMain();
+                    // When our process is not used any more, exit it.
+                    return;
                 }
+
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+
+                // Using our executable as a child rendering process, is not
+                // available when debugging in VS.
+                if (!Process.GetCurrentProcess().ProcessName.EndsWith("vshost"))
+                {
+                    // Initialize the WebCore specifying that this executable
+                    // can be used as a child rendering process.
+                    WebCore.Initialize(new WebCoreConfig()
+                                           {
+                                               ChildProcessPath = WebCoreConfig.CHILD_PROCESS_SELF,
+                                               LogLevel = LogLevel.Verbose,
+                                           });
+                }
+
+                Application.Run(new WebForm());
+
             }
         }
     }
