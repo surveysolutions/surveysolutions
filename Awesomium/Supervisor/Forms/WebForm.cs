1cb52414b9e9230a347b0f432d5a85f15a9a49e3
Mike Orlov <mike@toidev.com>
2012-09-18 17:25:49 +0300

    exclude debug snippets in 1679
    Former-commit-id: 616ba9b770c45f19d8e79568cf6ab839472fa6b5

--- Awesomium/Supervisor/Forms/WebForm.cs
+++ Awesomium/Supervisor/Forms/WebForm.cs
@@ -26,7 +26,7 @@ namespace Browsing.Supervisor.Forms
         {
             InitializeComponent();
 
-#if DEBUG
+#if DEBUG__
             Properties.Settings.Default.RunClient = false;
             Properties.Settings.Default.DefaultUrl = "http://192.168.3.113/DevKharkiv-Supervisor/";
             Properties.Settings.Default.Save();
