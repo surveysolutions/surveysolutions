d80d5d96d42ddd9900b4cb98e89ab063c770fba0
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2012-09-21 11:38:02 -0400

    exception logging 500 error preparation
    Former-commit-id: 223e8d32f850caa9ce6760c52a5eb8a1021f3d86

--- RavenQuestionnaire.Web/Web.CAPI/Global.asax.cs
+++ RavenQuestionnaire.Web/Web.CAPI/Global.asax.cs
@@ -1,36 +1,92 @@
-﻿using System;
-using System.Web.Configuration;
-using System.Web.Mvc;
-using System.Web.Routing;
-using Ninject;
-using Questionnaire.Core.Web.Helpers;
-using Raven.Client.Document;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Global.asax.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   The mvc application.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace Web.CAPI
 {
-    using RavenQuestionnaire.Core;
+    using System;
+    using System.Web;
+    using System.Web.Configuration;
+    using System.Web.Mvc;
+    using System.Web.Routing;
+
+    using Ninject;
+
+    using NLog;
+
+    using Questionnaire.Core.Web.Helpers;
+
+    using Raven.Client.Document;
 
     // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
     // visit http://go.microsoft.com/?LinkId=9394801
 
-    public class MvcApplication : System.Web.HttpApplication
+    /// <summary>
+    /// The mvc application.
+    /// </summary>
+    public class MvcApplication : HttpApplication
     {
+        #region Fields
+
+        /// <summary>
+        /// The logger.
+        /// </summary>
+        private readonly Logger logger = LogManager.GetCurrentClassLogger();
+
+        #endregion
+
+        #region Public Methods and Operators
+
+        /// <summary>
+        /// The register global filters.
+        /// </summary>
+        /// <param name="filters">
+        /// The filters.
+        /// </param>
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            //filters.Add(new HandleErrorAttribute());
+            // filters.Add(new HandleErrorAttribute());
         }
 
+        /// <summary>
+        /// The register routes.
+        /// </summary>
+        /// <param name="routes">
+        /// The routes.
+        /// </param>
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                "Default", // Route name
-                "{controller}/{action}/{id}", // URL with parameters
+                "Default", 
+                // Route name
+                "{controller}/{action}/{id}", 
+                // URL with parameters
                 new { controller = "Survey", action = "Dashboard", id = UrlParameter.Optional } // Parameter defaults
                 );
         }
 
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// The application_ error.
+        /// </summary>
+        protected void Application_Error()
+        {
+            this.logger.Fatal(this.Server.GetLastError());
+        }
+
+        /// <summary>
+        /// The application_ start.
+        /// </summary>
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
@@ -40,22 +96,44 @@ namespace Web.CAPI
             NCQRSInit.RebuildReadLayer(KernelLocator.Kernel.Get<DocumentStore>());
             ViewEngines.Engines.Clear();
             ViewEngines.Engines.Add(new RazorViewEngine());
+
+            AppDomain current = AppDomain.CurrentDomain;
+            current.UnhandledException += this.CurrentUnhandledException;
         }
+
+        /// <summary>
+        /// The host services.
+        /// </summary>
         protected void HostServices()
         {
             bool isDiscovereble;
             if (!bool.TryParse(WebConfigurationManager.AppSettings["WCFVisible"], out isDiscovereble))
+            {
                 return;
+            }
+
             if (!isDiscovereble)
+            {
                 return;
+            }
+
             // i need to ping wcf server to make it visible or install app fabric
         }
 
-        protected void Application_Error()
+        /// <summary>
+        /// The current_ unhandled exception.
+        /// </summary>
+        /// <param name="sender">
+        /// The sender.
+        /// </param>
+        /// <param name="e">
+        /// The e.
+        /// </param>
+        private void CurrentUnhandledException(object sender, UnhandledExceptionEventArgs e)
         {
-            Exception lastException = Server.GetLastError();
-            NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
-            logger.Fatal(lastException);
+            this.logger.Fatal(e.ExceptionObject);
         }
+
+        #endregion
     }
 }
