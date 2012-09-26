53a989ee2577aab2cb9c2f4e5c73fcad843431bd
Mike Orlov <mike@toidev.com>
2012-09-26 18:25:24 +0300

    case 1912
    Former-commit-id: 4f3d990d80a098d2d4d8b90426e01672991285d4

---
+++ Awesomium/Common/UrlValidator.cs
@@ -0,0 +1,33 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="UrlValidator.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Common
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Text.RegularExpressions;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public class UrlValidator
+    {
+        private static string UrlPattern = @"^(http|https|tcp)\://[a-zA-Z0-9\-\.]+\.[a-zA-Z0-9]{1,3}(:[0-9]{1,5})?((/|\\)([a-zA-Z0-9\-\._\?\,\'/\\\+&amp;%\$#\=~])+)*(/|\\)$";
+        private static Regex RegEx = new Regex(UrlPattern);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        public static bool IsValid(string url)
+        {
+            return RegEx.IsMatch(url);
+        }
+    }
+}
