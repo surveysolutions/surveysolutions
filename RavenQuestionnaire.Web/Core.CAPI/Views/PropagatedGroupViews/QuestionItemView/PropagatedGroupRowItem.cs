33b6cd404c6ea7876b24d24800f449d7c81ae42c
Anastasya Kholodova <nastya.kholodova@gmail.com>
2012-09-27 17:33:19 -0400

    grids
    Former-commit-id: 2401ac4fe9cd6d371244969af304492447efe0db

---
+++ RavenQuestionnaire.Web/Core.CAPI/Views/PropagatedGroupViews/QuestionItemView/PropagatedGroupRowItem.cs
@@ -0,0 +1,35 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="PropagatedGroupRowItem.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using Main.Core.Entities.SubEntities.Complete;
+
+namespace Core.CAPI.Views.PropagatedGroupViews.QuestionItemView
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class PropagatedGroupRowItem
+    {
+        public PropagatedGroupRowItem(ICompleteGroup group, string title)
+        {
+            if (!group.PropogationPublicKey.HasValue)
+                throw new ArgumentException("group is not propagateble");
+            this.PropagationKey = group.PropogationPublicKey.Value;
+            this.Title = title;
+            this.Answers = new Dictionary<Guid, object>();
+            foreach (ICompleteQuestion question in group.Children.OfType<ICompleteQuestion>())
+            {
+                this.Answers.Add(question.PublicKey, question.GetAnswerObject());
+            }
+        }
+
+        public Guid PropagationKey { get; set; }
+        public string Title { get; set; }
+        public Dictionary<Guid, object> Answers { get; set; }
+    }
+}
