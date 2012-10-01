02ab2f9b716e507e45082fa1d646190d01a77734
julia <none@none>
2012-10-01 17:19:49 +0300

    case 2021
    Former-commit-id: 359a525ce8343e0cc0c78d7355183a23ac05693c

---
+++ RavenQuestionnaire.Web/Main.Core/Events/UserEventSync.cs
@@ -0,0 +1,87 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="UserEventSync.cs" company="WorldBank">
+// 2012
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Main.Core.Events
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Main.Core.Denormalizers;
+    using Main.Core.Documents;
+    using Main.Core.Domain;
+    using Main.Core.Entities.SubEntities;
+    using Main.Core.Events.User;
+
+    using Ncqrs;
+    using Ncqrs.Eventing.Storage;
+
+    public interface IUserEventSync
+    {
+        IEnumerable<AggregateRootEvent> GetUsers(UserRoles? role);
+    }
+
+    public class UserEventSync : AbstractSnapshotableEventSync, IUserEventSync
+    {
+        #region Fields
+
+        /// <summary>
+        /// myEventStore object
+        /// </summary>
+        private readonly IEventStore myEventStore;
+
+        /// <summary>
+        /// ViewRepository  object
+        /// </summary>
+        private readonly IDenormalizer denormalizer;
+
+        #endregion
+
+        #region Constructor
+
+        public UserEventSync(IDenormalizer denormalizer)
+        {
+            this.denormalizer = denormalizer;
+            this.myEventStore = NcqrsEnvironment.Get<IEventStore>();
+            if (this.myEventStore == null)
+            {
+                throw new Exception("IEventStore is not correct.");
+            }
+        }
+
+        #endregion
+
+        #region PublicMethods
+
+        public IEnumerable<AggregateRootEvent> GetUsers(UserRoles? role)
+        {
+            var retval = new List<AggregateRootEvent>();
+            this.AddUsers(retval);
+            if (role != null)
+            {
+                return retval.Where(aggregateRootEvent => (aggregateRootEvent.Payload as NewUserCreated).Roles.Where(t => t == role).Count() > 0).ToList();
+            }
+            return retval.OrderBy(x => x.EventTimeStamp).ToList();
+        }
+
+        /// <summary>
+        /// Responsible for load and added users from database
+        /// </summary>
+        /// <param name="retval">
+        /// The retval.
+        /// </param>
+        protected void AddUsers(List<AggregateRootEvent> retval)
+        {
+            IQueryable<UserDocument> model = this.denormalizer.Query<UserDocument>();
+            foreach (UserDocument item in model)
+            {
+                retval.AddRange(base.GetEventStreamById(item.PublicKey, typeof(UserAR)));
+            }
+        }
+
+        #endregion
+    }
+}
