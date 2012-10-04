c2829808cea0ceff8063fbf9436a54133715b2f3
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2012-10-04 14:31:18 -0400

    contain function test added
    Former-commit-id: bc0a471db588f7c6e05b25da43e9c783c7e2f649

---
+++ RavenQuestionnaire.Web/Main.Core.Tests/ExpressionExecutors/ExpressionExtentions/ExtentionFunctionsTests.cs
@@ -0,0 +1,66 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ExtentionFunctionsTests.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   Defines the ExtentionFunctionsTests type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Main.Core.Tests.ExpressionExecutors.ExpressionExtentions
+{
+    using Main.Core.Documents;
+    using Main.Core.Entities.Extensions;
+    using Main.Core.Entities.SubEntities;
+    using Main.Core.Entities.SubEntities.Complete;
+    using Main.Core.Entities.SubEntities.Complete.Question;
+    using Main.Core.ExpressionExecutors;
+
+    using NUnit.Framework;
+
+    /// <summary>
+    /// The extention functions tests.
+    /// </summary>
+    [TestFixture]
+    public class ExtentionFunctionsTests
+    {
+        /// <summary>
+        /// The create objects.
+        /// </summary>
+        [SetUp]
+        public void CreateObjects()
+        {
+        }
+
+        /// <summary>
+        /// The expression should evaluate contain functions with parameters.
+        /// </summary>
+        [Test]
+        public void ExpressionShouldEvaluateContainFunctionsWithParameters()
+        {
+            var answer = new CompleteAnswer(new Answer() { });
+            answer.AnswerValue = "1";
+            
+            var answer1 = new CompleteAnswer(new Answer() { });
+            answer1.AnswerValue = "2";
+            answer1.Selected = true;
+
+            var doc = new CompleteQuestionnaireDocument();
+            var question = new MultyOptionsCompleteQuestion(string.Empty);
+
+            question.ConditionExpression = "contains([" + question.PublicKey + "],1)";
+            question.ValidationExpression = "contains([" + question.PublicKey + "],2)";
+            doc.Children.Add(question);
+            question.Children.Add(answer);
+            question.Children.Add(answer1);
+
+            var executorC = new CompleteQuestionnaireConditionExecutor(new GroupHash(doc));
+            bool result = executorC.Execute(question);
+            Assert.AreEqual(result, false);
+
+            var executorE = new CompleteQuestionnaireValidationExecutor(new GroupHash(doc));
+            bool result1 = executorE.Execute(question);
+            Assert.AreEqual(result1, true);
+        }
+    }
+}
