6465788d739cac71b8cfe1c335498e48a64b9bd8
Mike Orlov <mike@toidev.com>
2012-10-12 12:34:18 +0300

    absent file fix
    Former-commit-id: 3b1ba98cd8abd0d227929b0347007946e6e10d64

---
+++ RavenQuestionnaire.Web/Main.Core/Events/User/UserBaseEvent.cs
@@ -0,0 +1,41 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="UserBaseEvent.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Main.Core.Events.User
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    using Main.Core.Entities.SubEntities;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public class UserBaseEvent
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether is locked.
+        /// </summary>
+        public bool IsLocked { get; set; }
+
+        public bool IsAssignedRole(UserRoles role)
+        {
+            return DoCheckIsAssignedRole(role);
+        }
+
+        /// <summary>
+        /// Check if the event assigns a role
+        /// </summary>
+        /// <param name="role">Role to check</param>
+        /// <returns>True if assigned</returns>
+        protected virtual bool DoCheckIsAssignedRole(UserRoles role)
+        {
+            return false;
+        }
+    }
+}
