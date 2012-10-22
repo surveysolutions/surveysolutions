175372536c51d01b5b771e81e8841034f7b936d6
julia <none@none>
2012-10-22 14:17:56 +0300

    case 2140
    Former-commit-id: 170699b00d419a1c2e920af4d86b550054b32073

---
+++ RavenQuestionnaire.Web/Main.Core/Domain/DeviceAR.cs
@@ -0,0 +1,99 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="DeviceAR.cs" company="WorldBank">
+// 2012
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Main.Core.Domain
+{
+    using System;
+    using Main.Core.Events.Synchronization;
+    using Ncqrs.Domain;
+
+    /// <summary>
+    /// Class for registered device information
+    /// </summary>
+    public class DeviceAR : AggregateRootMappedByConvention
+    {
+        #region Fields
+
+        /// <summary>
+        /// Field RegisteredDate
+        /// </summary>
+        private DateTime RegisteredDate;
+
+        /// <summary>
+        /// Field TabletId
+        /// </summary>
+        private Guid TabletId;
+
+        /// <summary>
+        /// Field PublicKey
+        /// </summary>
+        private byte[] SecretKey;
+
+        /// <summary>
+        /// Field Description
+        /// </summary>
+        private string Description;
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DeviceAR"/> class.
+        /// </summary>
+        public DeviceAR()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DeviceAR"/> class.
+        /// </summary>
+        /// <param name="description">
+        /// The description.
+        /// </param>
+        /// <param name="tabletId">
+        /// The tablet id.
+        /// </param>
+        /// <param name="publicKey">
+        /// The public key.
+        /// </param>
+        /// <param name="registeredDate">
+        /// The registered date.
+        /// </param>
+        public DeviceAR(string description, Guid tabletId, byte[] publicKey, DateTime registeredDate)
+        {
+            this.ApplyEvent(
+               new NewDeviceRegistered
+               {
+                   SecretKey = publicKey,
+                   RegisteredDate = registeredDate,
+                   TabletId = tabletId,
+                   Description = description
+               });
+        }
+
+        #endregion
+        
+        #region Methods
+
+        /// <summary>
+        /// Event handler for the NewDeviceRegistered event. This method
+        /// is automaticly wired as event handler based on convension.
+        /// </summary>
+        /// <param name="e">
+        /// The e.
+        /// </param>
+        protected void OnNewDeviceRegister(NewDeviceRegistered e)
+        {
+            this.Description = e.Description;
+            this.SecretKey = e.SecretKey;
+            this.RegisteredDate = e.RegisteredDate;
+            this.TabletId = e.TabletId;
+        }
+
+        #endregion
+    }
+}
---
+++ RavenQuestionnaire.Web/Main.Core/Services/IRSACryptoService.cs
@@ -0,0 +1,24 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="IRSACryptoService.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Main.Core.Services
+{
+    using System.Security.Cryptography;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public interface IRSACryptoService
+    {
+        RSAParameters GetPublicKey();
+
+        RSAParameters GetPrivateKey();
+
+        string Encrypt(string inputString, int dwKeySize, string xmlString);
+
+        string Decrypt(string inputString, int dwKeySize, string xmlString);
+    }
+}
