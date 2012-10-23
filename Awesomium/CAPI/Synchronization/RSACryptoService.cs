605fc4a82c26951a695356f094cb797f01fd67df
Juliya Pushkalova <julia@toidev.com>
2012-10-23 13:39:03 +0300

    case 2178
    Former-commit-id: 39dfc31819d5c21f065383b68d400aed7fad42fd

---
+++ Awesomium/CAPI/Synchronization/RSACryptoService.cs
@@ -0,0 +1,61 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="RSACryptoService.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Browsing.CAPI.Synchronization
+{
+    using System.Security.Cryptography;
+
+    interface IRSACryptoService
+    {
+        /// <summary>
+        /// Generate PublicKey
+        /// </summary>
+        /// <returns>
+        /// Return PrivateKey
+        /// </returns>
+        RSAParameters GetPublicKey();
+
+        /// <summary>
+        /// Generate PrivateKey
+        /// </summary>
+        /// <returns>
+        /// Return PrivateKey
+        /// </returns>
+        RSAParameters GetPrivateKey();
+    }
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public class RSACryptoService:IRSACryptoService
+    {
+        /// <summary>
+        /// Generic PublicKey
+        /// </summary>
+        /// <returns>
+        /// Return PublicKey RSACrypt
+        /// </returns>
+        public RSAParameters GetPublicKey()
+        {
+            var rsa = new RSACryptoServiceProvider(2000);
+            var publicKey = rsa.ExportParameters(false);
+            return publicKey;
+        }
+
+        /// <summary>
+        /// Generic PrivateKey
+        /// </summary>
+        /// <returns>
+        /// Return PrivateKey RSACrypt
+        /// </returns>
+        public RSAParameters GetPrivateKey()
+        {
+            var rsa = new RSACryptoServiceProvider(2000);
+            var privateKey = rsa.ExportParameters(true);
+            return privateKey;
+        }
+    }
+}
