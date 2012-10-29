0f2f278fda860806e329472adf2e5bd26d74e44c
Slava Scherbak <vs@cloudwk.com>
2012-10-29 16:23:35 +0200

    merge fix
    Former-commit-id: 27ed3e16c86d0d27e25e90e0cc5a13ef70cf2a9f

--- RavenQuestionnaire.Web/Web.CAPI/Controllers/AccountController.cs
+++ RavenQuestionnaire.Web/Web.CAPI/Controllers/AccountController.cs
@@ -1,69 +1,169 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web.Mvc;
-using System.Web.Security;
-using Main.Core.Events;
-using Questionnaire.Core.Web.Helpers;
-using Questionnaire.Core.Web.Security;
-using Main.Core.Utility;
-using Web.CAPI.Models;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AccountController.cs" company="">
+//   2012
+// </copyright>
+// <summary>
+//   The account controller.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace Web.CAPI.Controllers
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web.Mvc;
+    using System.Web.Security;
+
+    using Main.Core.Entities.SubEntities;
+    using Main.Core.Events;
+    using Main.Core.Utility;
+
+    using Questionnaire.Core.Web.Helpers;
+    using Questionnaire.Core.Web.Security;
+
+    using Web.CAPI.Models;
+
+    /// <summary>
+    /// The account controller.
+    /// </summary>
     public class AccountController : Controller
     {
+        #region Constants and Fields
+
+        /// <summary>
+        /// The _global provider.
+        /// </summary>
         private readonly IGlobalInfoProvider _globalProvider;
-        private IFormsAuthentication authentication;
-        private IUserEventSync _userEventSync;
 
-        public AccountController(IFormsAuthentication auth, IGlobalInfoProvider globalProvider, IUserEventSync userEventSync)
+        /// <summary>
+        /// The _user event sync.
+        /// </summary>
+        private readonly IUserEventSync _userEventSync;
+
+        /// <summary>
+        /// The authentication.
+        /// </summary>
+        private readonly IFormsAuthentication authentication;
+
+        #endregion
+
+        #region Constructors and Destructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AccountController"/> class.
+        /// </summary>
+        /// <param name="auth">
+        /// The auth.
+        /// </param>
+        /// <param name="globalProvider">
+        /// The global provider.
+        /// </param>
+        /// <param name="userEventSync">
+        /// The user event sync.
+        /// </param>
+        public AccountController(
+            IFormsAuthentication auth, IGlobalInfoProvider globalProvider, IUserEventSync userEventSync)
         {
-            authentication = auth;
-            _globalProvider = globalProvider;
-            _userEventSync = userEventSync;
+            this.authentication = auth;
+            this._globalProvider = globalProvider;
+            this._userEventSync = userEventSync;
         }
 
-        //
+        #endregion
+
         // GET: /Account/LogOn
-        public ActionResult LogOn()
+        #region Public Methods and Operators
+
+        /// <summary>
+        /// Generate guid of current user
+        /// </summary>
+        /// <returns>
+        /// Guid of current user
+        /// </returns>
+        public Guid GetCurrentUser()
         {
-            return View();
+            UserLight currentUser = this._globalProvider.GetCurrentUser();
+            return currentUser != null ? currentUser.Id : Guid.Empty;
         }
 
+        /// <summary>
+        /// The is logged in.
+        /// </summary>
+        /// <returns>
+        /// The is logged in.
+        /// </returns>
         public bool IsLoggedIn()
         {
-            return _globalProvider.GetCurrentUser() != null;
+            return this._globalProvider.GetCurrentUser() != null;
         }
 
+        /// <summary>
+        /// The is user in base.
+        /// </summary>
+        /// <returns>
+        /// The is user in base.
+        /// </returns>
+        public bool IsUserInBase()
+        {
+            IEnumerable<AggregateRootEvent> count = this._userEventSync.GetUsers(UserRoles.Operator);
+            if (count == null)
+            {
+                return false;
+            }
+
+            return count.ToList().Count > 0;
+        }
+
+        /// <summary>
+        /// The log off.
+        /// </summary>
+        /// <returns>
+        /// </returns>
+        public ActionResult LogOff()
+        {
+            this.authentication.SignOut();
+            return this.Redirect("~/");
+        }
+
+        /// <summary>
+        /// The log on.
+        /// </summary>
+        /// <returns>
+        /// </returns>
+        public ActionResult LogOn()
+        {
+            return this.View();
+        }
+
+        /// <summary>
+        /// The log on.
+        /// </summary>
+        /// <param name="model">
+        /// The model.
+        /// </param>
+        /// <param name="returnUrl">
+        /// The return url.
+        /// </param>
+        /// <returns>
+        /// </returns>
         [HttpPost]
         public ActionResult LogOn(LogOnModel model, string returnUrl)
         {
-            if (ModelState.IsValid)
+            if (this.ModelState.IsValid)
             {
                 if (Membership.ValidateUser(model.UserName, SimpleHash.ComputeHash(model.Password)))
                 {
-                    authentication.SignIn(model.UserName, false);
-                    return Redirect("~/");
+                    this.authentication.SignIn(model.UserName, false);
+                    return this.Redirect("~/");
                 }
-                ModelState.AddModelError("", "The user name or password provided is incorrect.");
+
+                this.ModelState.AddModelError(string.Empty, "The user name or password provided is incorrect.");
             }
+
             return View(model);
         }
 
-        //
-        // GET: /Account/LogOff
-        public ActionResult LogOff()
-        {
-            authentication.SignOut();
-            return Redirect("~/");
-        }
-
-        public bool IsUserInBase()
-        {
-            var count = _userEventSync.GetUsers(Main.Core.Entities.SubEntities.UserRoles.Operator);
-            if (count == null) return false;
-            return count.ToList().Count > 0;
-        }
+        #endregion
     }
 }
