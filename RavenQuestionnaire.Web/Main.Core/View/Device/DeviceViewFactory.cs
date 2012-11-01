26f6b3d6edc05243f75cf7803a040ac5e06da68f
julia <none@none>
2012-11-01 14:20:41 +0200

    case 2188
    Former-commit-id: 1957274fa64a7bbd6dd3d4d486e5bcd398ce12cb

---
+++ RavenQuestionnaire.Web/Main.Core/View/Device/DeviceViewFactory.cs
@@ -0,0 +1,64 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="DeviceViewFactory.cs" company="WorldBank">
+// 2012
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Main.Core.View.Device
+{
+    using System.Linq;
+    using Main.Core.Documents;
+    using Main.DenormalizerStorage;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public class DeviceViewFactory : IViewFactory<DeviceViewInputModel, DeviceView>
+    {
+        #region Field
+
+        /// <summary>
+        /// Devices field devices
+        /// </summary>
+        private readonly IDenormalizerStorage<SyncDeviceRegisterDocument> devices;
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DeviceViewFactory"/> class.
+        /// </summary>
+        /// <param name="devices">
+        /// The devices.
+        /// </param>
+        public DeviceViewFactory(IDenormalizerStorage<SyncDeviceRegisterDocument> devices)
+        {
+            this.devices = devices;
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Find device from storage in memory
+        /// </summary>
+        /// <param name="input">
+        /// The input.
+        /// </param>
+        /// <returns>
+        /// return DeviceView
+        /// </returns>
+        public DeviceView Load(DeviceViewInputModel input)
+        {
+            int count = devices.Count();
+            if (count == 0) 
+                return new DeviceView();
+            var dev = devices.Query().Where(t =>t.TabletId == input.TabletId).FirstOrDefault();
+            return new DeviceView(dev.Description, dev.CreationDate, dev.SecretKey, dev.TabletId);
+        }
+
+        #endregion
+    }
+}
