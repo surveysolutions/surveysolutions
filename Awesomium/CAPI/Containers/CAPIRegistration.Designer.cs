ad762ba1be84e0c0b8e3189b66003d913d450b49
nikolay@toidev.com <nikolay@toidev.com>
2012-11-06 13:45:55 +0200

    case 2189
    Former-commit-id: 843cf6ed2e60891f0e1c8982282cfb160a47beaa

---
+++ Awesomium/CAPI/Containers/CAPIRegistration.Designer.cs
@@ -0,0 +1,43 @@
+﻿namespace Browsing.CAPI.Containers
+{
+    partial class CAPIRegistration
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Margin = new System.Windows.Forms.Padding(0);
+            //this.MinimumSize = new System.Drawing.Size(920, 200);
+            this.Name = "CAPIRegistration";
+            this.Size = new System.Drawing.Size(920, 200);
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
---
+++ Awesomium/CAPI/Containers/CAPIRegistration.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Browsing.CAPI.Registration;
+using Browsing.Common.Containers;
+using Browsing.Common.Controls;
+using Common.Utils;
+using Synchronization.Core.Interface;
+using Synchronization.Core.Registration;
+
+namespace Browsing.CAPI.Containers
+{
+    public partial class CAPIRegistration : Browsing.Common.Containers.Registration
+    {
+        private string registrationFirstStep = Properties.Settings.Default.RegistrationStatus;
+
+        public CAPIRegistration(IRequesProcessor requestProcessor, IUrlUtils urlUtils, ScreenHolder holder)
+            : base(requestProcessor, urlUtils, holder)
+        {
+            InitializeComponent();
+         
+        }
+
+        #region Override Methods
+
+        protected override RegistrationManager DoInstantiateRegistrationManager()
+        {
+            return new CapiRegistrationManager();
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            if (registrationFirstStep.Equals("First"))
+                base.ChangeResultlabel("First registration step complited");
+            else 
+                if (registrationFirstStep.Equals("Second"))
+                    base.ChangeResultlabel("Second registration step complited");
+        }
+
+        protected override void OnFirstRegistrationStepButtonClicked(DriveInfo drive)
+        {
+            if (drive == null)
+                return;
+
+            if (String.IsNullOrEmpty(this.registrationFirstStep) || registrationFirstStep.Equals("Second"))
+            {
+                try
+                {
+                     if (this.RegistrationManager.StartRegistration(drive.Name))
+                         base.ChangeResultlabel("First registration step complited");
+                }
+                catch (Exception ex)
+                {
+                    base.ChangeResultlabel("Registration failed: "+ex.Message, true);
+                    return;
+                }
+
+                Properties.Settings.Default.RegistrationStatus = "First";
+                Properties.Settings.Default.Save();
+                registrationFirstStep = "First";
+
+               
+            }
+            else if (registrationFirstStep.Equals("First"))
+            {
+                try
+                {
+                    var res = RegistrationManager.FinalizeRegistration(drive.Name,
+                                                                       this.urlUtils.GetRegistrationCapiPath());
+                    if (res)
+                    {
+                        base.ChangeResultlabel("Registration Completed");
+                        Properties.Settings.Default.RegistrationStatus = "Second";
+                        Properties.Settings.Default.Save();
+                        registrationFirstStep = "Second";
+                    }
+                    else
+                    {
+                        base.ChangeResultlabel("Registration Failed", true);
+                    }
+                }
+                catch(Exception ex)
+                {
+                    base.ChangeResultlabel("Registration failed: " + ex.Message, true);
+                    return;
+                }
+            }
+        }
+
+        #endregion
+
+    }
+}
