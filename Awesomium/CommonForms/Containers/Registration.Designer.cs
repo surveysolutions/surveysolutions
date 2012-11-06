5fdf12fe9fcc6d6f55398a763f5ab3da9ec5ae6d
nikolay@toidev.com <nikolay@toidev.com>
2012-11-06 13:45:55 +0200

    case 2189
    Former-commit-id: 843cf6ed2e60891f0e1c8982282cfb160a47beaa

---
+++ Awesomium/CommonForms/Containers/Registration.Designer.cs
@@ -0,0 +1,118 @@
+﻿using System.Windows.Forms;
+using Browsing.Common.Controls;
+
+namespace Browsing.Common.Containers
+{
+    partial class Registration
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SyncPanel));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.usbStatusPanel = new Browsing.Common.Containers.UsbStatusPanel();
+            this.registrationButton = new Browsing.Common.Controls.FlatButton();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 5;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 200F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 200F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 25F));
+            this.tableLayoutPanel1.Controls.Add(this.usbStatusPanel, 1, 5);
+            this.tableLayoutPanel1.Controls.Add(this.registrationButton, 1, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(30);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 6;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 24F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(794, 554);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // usbStatusPanel
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.usbStatusPanel, 3);
+            this.usbStatusPanel.Location = new System.Drawing.Point(28, 249);
+            this.usbStatusPanel.Name = "usbStatusPanel";
+            this.usbStatusPanel.Size = new System.Drawing.Size(738, 272);
+            this.usbStatusPanel.TabIndex = 0;
+            // 
+            // registrationButton
+            // 
+            this.registrationButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.registrationButton.FlatAppearance.BorderSize = 0;
+            this.registrationButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.registrationButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.registrationButton.Image = ((System.Drawing.Image)(resources.GetObject("pullButton.Image")));
+            this.registrationButton.Location = new System.Drawing.Point(28, 23);
+            this.registrationButton.Name = "registrationButton";
+            this.registrationButton.Size = new System.Drawing.Size(194, 200);
+            this.registrationButton.TabIndex = 0;
+            this.registrationButton.Text = "Registration";
+            this.registrationButton.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.registrationButton.UseVisualStyleBackColor = true;
+            this.registrationButton.Click += new System.EventHandler(this.registrationButton_Click);
+            // 
+            // Registration
+            // 
+
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tableLayoutPanel1.Name = "syncPanel";
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(700, 500);
+            this.tableLayoutPanel1.TabIndex = 1;
+
+
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Registration";
+            this.Size = new System.Drawing.Size(794, 554);
+            this.Controls.SetChildIndex(this.tableLayoutPanel1, 0);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private FlatButton registrationButton;
+        private UsbStatusPanel usbStatusPanel;
+        private TableLayoutPanel tableLayoutPanel1;
+       
+    }
+}
---
+++ Awesomium/CommonForms/Containers/UsbStatusPanel.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using Browsing.Common.Controls;
+
+namespace Browsing.Common.Containers
+{
+    public partial class UsbStatusPanel : UserControl
+    {
+        private ManualResetEvent inactiveStatus = new ManualResetEvent(true);
+
+        private DriveInfo chosenUSB = null;
+
+        public EventHandler UsbPressed;
+
+        #region Public Methods
+
+        public UsbStatusPanel()
+        {
+            InitializeComponent();
+        }
+
+        public void ChangeAvlUsbLabel(string status, bool error = false)
+        {
+            SetLabel(this.labelAvlUsb, status, error);
+        }
+
+        public void ChangeStatusLabel(string status, bool error = false)
+        {
+            SetLabel(this.statusLabel, status, error);
+        }
+
+        public void ChangeResultLabel(string status, bool error = false)
+        {
+            SetLabel(this.resultLabel, status, error);
+        }
+
+        public void AddStripItem(ToolStripItem item)
+        {
+            this.usbStrip.Items.Add(item);
+        }
+
+        public void ClearStrip()
+        {
+            this.usbStrip.Items.Clear();
+        }
+
+        public void ChangeStripVisible(bool value)
+        {
+            this.usbStrip.Visible =value;
+        }
+
+        /// <summary>
+        /// Helper method to dissapear in diffrent thread if wait operation expected before hiding the form
+        /// </summary>
+        /// <param name="immediately"></param>
+        public void Deactivate(bool immediately)
+        {
+            if (immediately)
+                MakeInvisibleStatus(0);
+            else
+                new Thread(MakeInvisibleStatus).Start(10000);
+        }
+
+        public List<DriveInfo> ReviewDriversList()
+        {
+            List<DriveInfo> drivers = new List<DriveInfo>();
+            DriveInfo[] listDrives = DriveInfo.GetDrives();
+
+            foreach (var drive in listDrives)
+            {
+                if (drive.DriveType == DriveType.Removable)
+                    drivers.Add(drive);
+            }
+
+            return drivers;
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        public DriveInfo ChosenUsb
+        {
+            get { return this.chosenUSB; }
+        }
+
+        public ManualResetEvent InactiveStatus
+        {
+            get { return this.inactiveStatus; }
+        }
+
+
+        #endregion
+        private void UsbChoosen(object sender, EventArgs args)
+        {
+            this.chosenUSB = null;
+
+            var stripItems = GetStripItems();
+            foreach (var usbItem in stripItems)
+            {
+                var control = (usbItem as ToolStripControlHost).Control;
+                var button = control as FlatButton;
+                if (button == null)
+                    continue;
+
+                if (button == sender)
+                {
+                    this.chosenUSB = button.Tag as DriveInfo;
+                    button.Image = GetImages(1);
+                }
+                else
+                    button.Image = GetImages(0);
+            }
+
+            if (UsbPressed != null)
+                UsbPressed(sender, args);
+        }
+
+
+        private void MakeInvisibleStatus(object waitTime)
+        {
+            Thread.Sleep((int)waitTime);
+
+            ChangeResultLabel(null);
+            ChangeStatusLabel(null);
+
+            this.inactiveStatus.Set();
+        }
+        
+        public void UpdateLook()
+        {
+            MakeInvisibleStatus(0);
+            ChangeAvlUsbLabel(null);
+            
+            UpdateUsbList();
+        }
+        
+        public void UpdateUsbList()
+        {
+            ClearStrip();
+
+            this.chosenUSB = null;
+
+            var drives = ReviewDriversList();
+
+            ComponentResourceManager resources = new ComponentResourceManager(typeof(SyncPanel));
+            foreach (var drive in drives)
+            {
+                int imageIndex = 0;
+                bool theDriverIsChoozen = this.chosenUSB != null && string.Compare(drive.Name, this.chosenUSB.Name, true) == 0;
+                if (theDriverIsChoozen)
+                {
+                    this.chosenUSB = drive;
+                    imageIndex = 1;
+                }
+
+                FlatButton item = new FlatButton()
+                {
+                    Text = drive.Name.Trim(new char[] { '/', '\\' }),
+                    Tag = drive,
+
+                    Image = GetImages(imageIndex),
+                    Font = new Font(this.tableLayoutPanel2.Font.FontFamily, this.tableLayoutPanel2.Font.Size * 0.8f, FontStyle.Italic | FontStyle.Bold | FontStyle.Italic),
+                    ImageAlign = ContentAlignment.TopCenter,
+                    TextAlign = ContentAlignment.BottomCenter,
+                };
+
+                item.Click += UsbChoosen;
+
+                AddStripItem(new ToolStripControlHost(item));
+            }
+
+            if (GetStripItems().Count == 0)
+            {
+                ChangeStripVisible(false);
+                ChangeAvlUsbLabel(null);
+            }
+            else
+            {
+                ChangeStripVisible(true);
+                ChangeAvlUsbLabel("Available USB drivers:");
+            }
+        }
+
+        public ToolStripItemCollection GetStripItems()
+        {
+            return this.usbStrip.Items;
+        }
+
+        public Image GetImages(int index)
+        {
+            return imageList1.Images[index];
+        }
+
+        /// <summary>
+        /// Assign text content to status label
+        /// </summary>
+        /// <param name="status"></param>
+        private void SetLabel(Label label, string status, bool error = false)
+        {
+            if (label.InvokeRequired)
+            {
+                label.Invoke(new MethodInvoker(() =>
+                {
+                    label.Visible = !string.IsNullOrEmpty(status);
+                    label.Text = status;
+                    label.ForeColor = error ? System.Drawing.Color.Red : System.Drawing.Color.Black;
+                }));
+            }
+            else
+            {
+                label.Visible = !string.IsNullOrEmpty(status);
+                label.Text = status;
+                label.ForeColor = error ? System.Drawing.Color.Red : System.Drawing.Color.Black;
+            }
+        }
+    }
+}
