0e4a2693e84f7172843329165462f9af48c4f12c
Slava Scherbak <vs@cloudwk.com>
2012-11-12 12:15:45 +0200

    new tests
    Former-commit-id: 7ec31897690bdf270e1cc9412e66b2e30b3664b0

---
+++ RavenQuestionnaire.Web/Web.CAPI.Tests/SynchronizationsControllerTest.cs
@@ -0,0 +1,112 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="SynchronizationsControllerTest.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   This is a test class for SynchronizationsControllerTest and is intended
+//   to contain all SynchronizationsControllerTest Unit Tests
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace RavenQuestionnaire.Web.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Web.Mvc;
+
+    using global::Core.CAPI.Views.ExporStatistics;
+
+    using Main.Core.Events;
+    using Main.Core.View;
+    using Main.Core.View.CompleteQuestionnaire;
+
+    using Moq;
+
+    using Ncqrs.Eventing;
+
+    using NUnit.Framework;
+
+    using Questionnaire.Core.Web.Helpers;
+
+    using global::Web.CAPI.Controllers;
+
+    /// <summary>
+    /// This is a test class for SynchronizationsControllerTest and is intended
+    /// to contain all SynchronizationsControllerTest Unit Tests
+    /// </summary>
+    [TestFixture]
+    public class SynchronizationsControllerTest
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// Gets or sets Controller.
+        /// </summary>
+        public SynchronizationsController Controller { get; set; }
+
+        /// <summary>
+        /// Gets or sets GlobalProvider.
+        /// </summary>
+        public Mock<IGlobalInfoProvider> GlobalProvider { get; set; }
+
+        /// <summary>
+        /// Gets or sets Synchronizer.
+        /// </summary>
+        public Mock<IEventSync> Synchronizer { get; set; }
+
+        /// <summary>
+        /// Gets or sets ViewRepository.
+        /// </summary>
+        public Mock<IViewRepository> ViewRepository { get; set; }
+
+        #endregion
+
+        #region Public Methods and Operators
+
+        /// <summary>
+        /// The create objects.
+        /// </summary>
+        [SetUp]
+        public void CreateObjects()
+        {
+            this.GlobalProvider = new Mock<IGlobalInfoProvider>();
+            this.Synchronizer = new Mock<IEventSync>();
+            this.ViewRepository = new Mock<IViewRepository>();
+        }
+
+        /// <summary>
+        /// A test for ExportStatistics
+        /// </summary>
+        [Test]
+        public void ExportStatistics_ReturnsNotNull()
+        {
+            this.Synchronizer.Setup(s => s.ReadEvents()).Returns(
+                new List<AggregateRootEvent> {
+                        new AggregateRootEvent(
+                            new CommittedEvent(
+                            Guid.Empty, Guid.Empty, Guid.Empty, 1, DateTime.Now, new object(), new Version(1, 0))), 
+                        new AggregateRootEvent(
+                            new CommittedEvent(
+                            Guid.Empty, Guid.Empty, Guid.Empty, 1, DateTime.Now, new object(), new Version(1, 0))), 
+                        new AggregateRootEvent(
+                            new CommittedEvent(
+                            Guid.Empty, Guid.Empty, Guid.NewGuid(), 1, DateTime.Now, new object(), new Version(1, 0))), 
+                    });
+
+            this.ViewRepository.Setup(
+                v =>
+                v.Load<ExporStatisticsInputModel, ExportStatisticsView>(
+                    new ExporStatisticsInputModel(new List<Guid> { Guid.Empty }))).Returns(
+                        new ExportStatisticsView(new List<CompleteQuestionnaireBrowseItem>()));
+
+            this.Controller = new SynchronizationsController(
+                this.ViewRepository.Object, this.GlobalProvider.Object, this.Synchronizer.Object);
+
+            JsonResult expected = null;
+            JsonResult actual = this.Controller.ExportStatistics();
+
+            Assert.AreNotEqual(null, actual);
+        }
+        #endregion
+    }
+}
