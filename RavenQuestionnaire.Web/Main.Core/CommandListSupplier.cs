9418fd34114f713a621fba6510dd0004b6821f2e
Anastasya Kholodova <nastya.kholodova@gmail.com>
2012-11-19 16:19:42 -0500

    injection mechanism improvements
    Former-commit-id: 15ed8fb5207c80715d9ad156f2500e8de6d7afac

---
+++ RavenQuestionnaire.Web/Main.Core/CommandListSupplier.cs
@@ -0,0 +1,35 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="CommandListSupplier.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Main.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public class CommandListSupplier:ICommandListSupplier
+    {
+        #region Implementation of ICommandListSupplier
+
+        public CommandListSupplier(IEnumerable<Type> commands)
+        {
+            this.commands = commands;
+        }
+
+        public IEnumerable<Type> GetCommandList()
+        {
+            return commands;
+        }
+
+        #endregion
+
+        private IEnumerable<Type> commands;
+    }
+}
