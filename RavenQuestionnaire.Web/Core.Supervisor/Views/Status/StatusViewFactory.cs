f906de141fc39269f699f0c81cdb80ae8a36a69e
Slava Scherbak <vs@cloudwk.com>
2012-11-20 16:45:54 +0200

    case 2230
    Former-commit-id: 7e9e5db7b2ff2de356f5919112c260bd35cdb129

--- RavenQuestionnaire.Web/Core.Supervisor/Views/Status/StatusViewFactory.cs
+++ RavenQuestionnaire.Web/Core.Supervisor/Views/Status/StatusViewFactory.cs
@@ -11,7 +11,11 @@ namespace Core.Supervisor.Views.Status
     using System.Linq;
     using System.Text;
 
+    using Core.Supervisor.Views.Summary;
+
     using Main.Core.Documents;
+    using Main.Core.Entities;
+    using Main.Core.Entities.SubEntities;
     using Main.Core.View;
     using Main.Core.View.CompleteQuestionnaire;
     using Main.DenormalizerStorage;
@@ -57,9 +61,54 @@ namespace Core.Supervisor.Views.Status
         /// </returns>
         public StatusView Load(StatusViewInputModel input)
         {
-            var view = new StatusView();
+            var interviewers = this.users.Query().Where(u => u.Supervisor != null && u.Supervisor.Id == input.Supervisor.Id).Select(u => u.PublicKey).ToList();
+            var status = SurveyStatus.GetAllStatuses().FirstOrDefault(s => s.PublicId == input.StatusId)
+                         ?? new SurveyStatus(Guid.Empty, "Any");
+            
+            var headers = this.surveys.Query().Select(s => new TemplateLight(s.TemplateId, s.QuestionnaireTitle)).Distinct().ToList();
 
-            return view;
+            var items = this.BuildItems(
+                    (status.PublicId == Guid.Empty
+                         ? this.surveys.Query().Where(
+                             x => x.Responsible != null && interviewers.Contains(x.Responsible.Id))
+                         : this.surveys.Query().Where(x => x.Responsible != null && interviewers.Contains(x.Responsible.Id)
+                                                 && (x.Status.PublicId == status.PublicId))).GroupBy(x => x.Responsible),
+                headers).AsQueryable();
+
+            var retval = new StatusView(input.Page, input.PageSize, 0, status, headers);
+            if (input.Orders.Count > 0)
+            {/*
+                items = input.Orders[0].Direction == OrderDirection.Asc
+                                                      ? items.OrderBy(input.Orders[0].Field)
+                                                      : items.OrderByDescending(input.Orders[0].Field);
+              */
+            }
+
+            retval.BuildSummary(items, headers);
+
+            retval.TotalCount = items.Count();
+
+            retval.Items = items.Skip((input.Page - 1) * input.PageSize).Take(input.PageSize).ToList();
+            return retval;
+        }
+
+        /// <summary>
+        /// Builds items
+        /// </summary>
+        /// <param name="grouped">
+        /// The grouped.
+        /// </param>
+        /// <param name="headers">
+        /// The list of templates
+        /// </param>
+        /// <returns>
+        /// The build items.
+        /// </returns>
+        protected IEnumerable<StatusViewItem> BuildItems(IQueryable<IGrouping<UserLight, CompleteQuestionnaireBrowseItem>> grouped, List<TemplateLight> headers)
+        {
+            return from templateGroup in grouped 
+                   let tgroup = templateGroup.GroupBy(g => g.TemplateId).ToDictionary(k => k.Key, v => v.Count()) 
+                   select new StatusViewItem(templateGroup.Key, tgroup, headers);
         }
     }
 }
