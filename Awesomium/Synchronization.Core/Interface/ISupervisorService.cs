c65399f0cdba860269389099d1c89f33e7661476
Mike Orlov <mike@toidev.com>
2012-11-26 13:17:16 +0200

    case 2234
    Former-commit-id: f4fa0aed54e9d5aa64907d4b42e6b104211c4757

---
+++ Awesomium/Synchronization.Core/Interface/ISupervisorService.cs
@@ -0,0 +1,24 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ISupervisorService.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Synchronization.Core.Interface
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.ServiceModel;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    [ServiceContract]
+    public interface ISupervisorService
+    {
+        [OperationContract]
+        string GetSupervisorPath();
+    }
+}
