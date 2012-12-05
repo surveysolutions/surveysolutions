53eda022c3691e1f841a522072b183b5a53402d8
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2012-12-05 15:48:29 -0500

    push forgotten files
    Former-commit-id: d694783acc07af181fc92c5471ce82dc913cb6f7

---
+++ RavenQuestionnaire.Web/Main.Core/Events/Questionnaire/Completed/ConditionalStatusChanged.cs
@@ -0,0 +1,36 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ConditionalStatusChanged.cs" company="The World Bank">
+//   2012
+// </copyright>
+// <summary>
+//   Defines the EnableStatusChanged type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Main.Core.Events.Questionnaire.Completed
+{
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// The enable status changed.
+    /// </summary>
+    [Serializable]
+    public class ConditionalStatusChanged
+    {
+        /// <summary>
+        /// Gets or sets the completed questionnaire id.
+        /// </summary>
+        public Guid CompletedQuestionnaireId { get; set; }
+
+        /// <summary>
+        /// ResultGroupsStatus 
+        /// </summary>
+        public Dictionary<string, bool?> ResultGroupsStatus { get; set; }
+
+        /// <summary>
+        /// ResultQuestionsStatus 
+        /// </summary>
+        public Dictionary<string, bool?> ResultQuestionsStatus { get; set; }
+    }
+}
