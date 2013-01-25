8f60baca4bfd40ccd9f00833cef7f9433dcce2aa
Slava Scherbak <vs@cloudwk.com>
2013-01-25 15:29:07 +0200

    case 2351
    Former-commit-id: 7a84ce3138f00032de0fde53e2d20d09aa11f983

--- RavenQuestionnaire.Web/Core.Supervisor/Views/Status/StatusViewFactory.cs
+++ RavenQuestionnaire.Web/Core.Supervisor/Views/Status/StatusViewFactory.cs
@@ -62,7 +62,7 @@ namespace Core.Supervisor.Views.Status
             var status = SurveyStatus.GetStatusByIdOrDefault(input.StatusId);
             if (status == SurveyStatus.Unknown)
             {
-                status = new SurveyStatus { PublicId = Guid.Empty, Name = "Any" };
+                status = new SurveyStatus { PublicId = Guid.Empty, Name = "All" };
             }
 
             var headers = this.surveys.Query().Select(s => new TemplateLight(s.TemplateId, s.QuestionnaireTitle)).Distinct().ToList();
