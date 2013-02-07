d17a30262ef728fb58ae4c9b17a73f393c721f7d
Anatoliy 'TLK' Kolesnick <none@none>
2013-02-07 16:09:13 +0200

    UserAR.ChangeUser: covered ChangeUser part with tests
    Former-commit-id: 56218622571140f131c46d89665a7fdfd112df63

--- RavenQuestionnaire.Web/Main.Core.Tests/Domain/UserARTests.cs
+++ RavenQuestionnaire.Web/Main.Core.Tests/Domain/UserARTests.cs
@@ -1,5 +1,6 @@
 ﻿namespace Main.Core.Tests.Domain
 {
+    using System.Collections;
     using System.Collections.Generic;
     using System.Linq;
 
@@ -33,7 +34,7 @@
         public void Lock_When_called_Then_raised_UserLocked_event()
         {
             // arrange
-            UserAR user = this.CreateUserAR();
+            UserAR user = CreateUserAR();
 
             // act
             user.Lock();
@@ -46,7 +47,7 @@
         public void Unlock_When_called_Then_raised_UserUnlocked_event()
         {
             // arrange
-            UserAR user = this.CreateUserAR();
+            UserAR user = CreateUserAR();
 
             // act
             user.Unlock();
@@ -59,11 +60,11 @@
         public void ChangeUser_When_is_locked_set_to_true_Then_raised_UserLocked_event()
         {
             // arrange
-            UserAR user = this.CreateUserAR();
+            UserAR user = CreateUserAR();
             bool isLocked = true;
 
             // act
-            user.ChangeUser("mail", isLocked, new UserRoles[] { });
+            user.ChangeUser("mail@domain.net", isLocked, new UserRoles[] { });
 
             // assert
             Assert.That(this.GetRaisedEvents<UserLocked>().Count(), Is.EqualTo(1));
@@ -73,19 +74,52 @@
         public void ChangeUser_When_is_locked_set_to_false_Then_raised_UserUnlocked_event()
         {
             // arrange
-            UserAR user = this.CreateUserAR();
+            UserAR user = CreateUserAR();
             bool isLocked = false;
 
             // act
-            user.ChangeUser("mail", isLocked, new UserRoles[] { });
+            user.ChangeUser("mail@domain.net", isLocked, new UserRoles[] { });
 
             // assert
             Assert.That(this.GetRaisedEvents<UserUnlocked>().Count(), Is.EqualTo(1));
         }
 
-        private UserAR CreateUserAR()
+        [Test]
+        public void ChangeUser_When_email_is_specified_Then_raised_UserChanged_event_with_specified_email()
+        {
+            // arrange
+            UserAR user = CreateUserAR();
+            string specifiedEmail = "user@example.com";
+
+            // act
+            user.ChangeUser(specifiedEmail, false, new UserRoles[] { });
+
+            // assert
+            Assert.That(this.GetSingleRaisedEvent<UserChanged>().Email, Is.EqualTo(specifiedEmail));
+        }
+
+        [Test]
+        public void ChangeUser_When_two_roles_are_specified_Then_raised_UserChanged_event_with_specified_roles()
+        {
+            // arrange
+            UserAR user = CreateUserAR();
+            IEnumerable<UserRoles> twoSpecifedRoles = new [] { UserRoles.Administrator, UserRoles.User };
+
+            // act
+            user.ChangeUser("mail@domain.net", false, twoSpecifedRoles.ToArray());
+
+            // assert
+            Assert.That(this.GetSingleRaisedEvent<UserChanged>().Roles, Is.EquivalentTo(twoSpecifedRoles));
+        }
+
+        private static UserAR CreateUserAR()
         {
             return new UserAR();
+        }
+
+        private T GetSingleRaisedEvent<T>()
+        {
+            return this.GetRaisedEvents<T>().Single();
         }
 
         private IEnumerable<T> GetRaisedEvents<T>()
