3bf0798bc8577a4f03020552c12c1091e7871c51
Anatoliy 'TLK' Kolesnick <none@none>
2013-02-07 13:28:21 +0200

    UserAR.Lock: covered with tests
    Former-commit-id: 6410f9fb1ce39e30dddf77657a90fda66cec01dd

---
+++ RavenQuestionnaire.Web/Main.Core.Tests/Domain/UserARTests.cs
@@ -0,0 +1,58 @@
+﻿namespace Main.Core.Tests.Domain
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Main.Core.Domain;
+    using Main.Core.Events.User;
+
+    using NUnit.Framework;
+
+    using Ncqrs.Spec;
+
+    [TestFixture]
+    public class UserARTests
+    {
+        private EventContext eventContext;
+
+        [SetUp]
+        public void Init()
+        {
+            this.eventContext = new EventContext();
+        }
+
+        [TearDown]
+        public void Dispose()
+        {
+            this.eventContext.Dispose();
+            this.eventContext = null;
+        }
+
+        [Test]
+        public void Lock_When_called_Then_raised_UserLocked_event()
+        {
+            // arrange
+            UserAR user = this.CreateUserAR();
+
+            // act
+            user.Lock();
+
+            // assert
+            Assert.That(this.GetRaisedEvents<UserLocked>().Count(), Is.EqualTo(1));
+        }
+
+        private UserAR CreateUserAR()
+        {
+            return new UserAR();
+        }
+
+        private IEnumerable<T> GetRaisedEvents<T>()
+        {
+            return this.eventContext
+                .Events
+                .Where(e => e.Payload is T)
+                .Select(e => e.Payload)
+                .Cast<T>();
+        }
+    }
+}
