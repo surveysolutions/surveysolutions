fd0b7670727ed788a72b255a06b599871411d0d0
Anatoliy 'TLK' Kolesnick <none@none>
2013-02-07 16:36:44 +0200

    UserAR.ctor: covered with tests except supervisor part (which needs discussion)
    Former-commit-id: 12354f0d7c93b3f16d4908a85ff4b2775e338c89

--- RavenQuestionnaire.Web/Main.Core.Tests/Domain/UserARTests.cs
+++ RavenQuestionnaire.Web/Main.Core.Tests/Domain/UserARTests.cs
@@ -1,5 +1,6 @@
 ﻿namespace Main.Core.Tests.Domain
 {
+    using System;
     using System.Collections;
     using System.Collections.Generic;
     using System.Linq;
@@ -110,6 +111,84 @@
 
             // assert
             Assert.That(this.GetSingleRaisedEvent<UserChanged>().Roles, Is.EquivalentTo(twoSpecifedRoles));
+        }
+
+        [Test]
+        public void ctor_When_is_locked_set_to_true_Then_raised_NewUserCreated_event_with_is_locked_set_to_true()
+        {
+            // arrange
+            bool isLocked = true;
+
+            // act
+            new UserAR(Guid.NewGuid(), "name", "pwd", "my@email.com", new UserRoles[] { }, isLocked, null);
+
+            // assert
+            Assert.That(this.GetSingleRaisedEvent<NewUserCreated>().IsLocked, Is.EqualTo(true));
+        }
+
+        [Test]
+        public void ctor_When_name_is_specified_Then_raised_NewUserCreated_event_with_specified_name()
+        {
+            // arrange
+            string specifiedName = "Green Lantern";
+
+            // act
+            new UserAR(Guid.NewGuid(), specifiedName, "pwd", "my@email.com", new UserRoles[] { }, false, null);
+
+            // assert
+            Assert.That(this.GetSingleRaisedEvent<NewUserCreated>().Name, Is.EqualTo(specifiedName));
+        }
+
+        [Test]
+        public void ctor_When_password_is_specified_Then_raised_NewUserCreated_event_with_specified_password()
+        {
+            // arrange
+            string specifiedPassword = "hhg<8923s:0";
+
+            // act
+            new UserAR(Guid.NewGuid(), "name", specifiedPassword, "my@email.com", new UserRoles[] { }, false, null);
+
+            // assert
+            Assert.That(this.GetSingleRaisedEvent<NewUserCreated>().Password, Is.EqualTo(specifiedPassword));
+        }
+
+        [Test]
+        public void ctor_When_email_is_specified_Then_raised_NewUserCreated_event_with_specified_email()
+        {
+            // arrange
+            string specifiedEmail = "gmail@chucknorris.com";
+
+            // act
+            new UserAR(Guid.NewGuid(), "name", "pwd", specifiedEmail, new UserRoles[] { }, false, null);
+
+            // assert
+            Assert.That(this.GetSingleRaisedEvent<NewUserCreated>().Email, Is.EqualTo(specifiedEmail));
+        }
+
+        [Test]
+        public void ctor_When_public_key_is_specified_Then_raised_NewUserCreated_event_with_specified_public_key()
+        {
+            // arrange
+            Guid specifiedPublicKey = Guid.NewGuid();
+
+            // act
+            new UserAR(specifiedPublicKey, "name", "pwd", "my@email.com", new UserRoles[] { }, false, null);
+
+            // assert
+            Assert.That(this.GetSingleRaisedEvent<NewUserCreated>().PublicKey, Is.EqualTo(specifiedPublicKey));
+        }
+
+        [Test]
+        public void ctor_When_three_roles_are_specified_Then_raised_NewUserCreated_event_with_specified_roles()
+        {
+            // arrange
+            IEnumerable<UserRoles> threeSpecifedRoles = new [] { UserRoles.Supervisor, UserRoles.Operator, UserRoles.User };
+
+            // act
+            new UserAR(Guid.NewGuid(), "name", "pwd", "my@email.com", threeSpecifedRoles.ToArray(), false, null);
+
+            // assert
+            Assert.That(this.GetSingleRaisedEvent<NewUserCreated>().Roles, Is.EquivalentTo(threeSpecifedRoles));
         }
 
         private static UserAR CreateUserAR()
