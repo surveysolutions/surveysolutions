4263e734cef83169f86ca9b2c816f34cb714585a
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2013-02-08 18:33:43 -0500

    fix ref in project
    --HG--
    branch : monodroid-next
    
    
    Former-commit-id: cae40f4aedcc0705fe895eee405d0c81bf35d559

--- Mono/AndroidMain.Synchronization/RemoteCollector.cs
+++ Mono/AndroidMain.Synchronization/RemoteCollector.cs
@@ -1,18 +1,97 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="RemoteCollector.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   The remote collector.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace AndroidMain.Synchronization
 {
-    class RemoteCollector
+    using System;
+    using System.Collections.Generic;
+
+    using Main.Core.Events;
+    using Main.Core.View.SyncProcess;
+    using Main.Synchronization.SyncStreamCollector;
+
+    /// <summary>
+    /// The remote collector.
+    /// </summary>
+    internal class RemoteCollector : ISyncStreamCollector
     {
+        #region Public Properties
+
+        /// <summary>
+        /// Gets the max chunk size.
+        /// </summary>
+        public int MaxChunkSize { get; private set; }
+
+        /// <summary>
+        /// Gets a value indicating whether support sync stat.
+        /// </summary>
+        public bool SupportSyncStat
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        #endregion
+
+        #region Public Methods and Operators
+
+        /// <summary>
+        /// The collect.
+        /// </summary>
+        /// <param name="chunk">
+        /// The chunk.
+        /// </param>
+        /// <returns>
+        /// The <see cref="bool"/>.
+        /// </returns>
+        /// <exception cref="NotImplementedException">
+        /// </exception>
+        public bool Collect(IEnumerable<AggregateRootEvent> chunk)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// The finish.
+        /// </summary>
+        /// <exception cref="NotImplementedException">
+        /// </exception>
+        public void Finish()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// The get stat.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="List"/>.
+        /// </returns>
+        /// <exception cref="NotImplementedException">
+        /// </exception>
+        public List<UserSyncProcessStatistics> GetStat()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// The prepare to collect.
+        /// </summary>
+        /// <exception cref="NotImplementedException">
+        /// </exception>
+        public void PrepareToCollect()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
     }
 }
