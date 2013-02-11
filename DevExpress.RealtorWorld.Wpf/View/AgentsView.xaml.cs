10824df029a84daba5f834a6c5835f806af7ef0c
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2013-02-11 18:43:19 +0200

    removed no longer needed wpf projects
    --HG--
    branch : wpf
    
    
    Former-commit-id: bff8aa4eacdb5a9fb4f33f51efd16d3d54e24dd8

--- DevExpress.RealtorWorld.Wpf/View/AgentsView.xaml.cs
+++
@@ -1,8 +0,0 @@
-
-namespace DevExpress.RealtorWorld.Xpf.View {
-    public partial class AgentsView : ModuleView {
-        public AgentsView() {
-            InitializeComponent();
-        }
-    }
-}
