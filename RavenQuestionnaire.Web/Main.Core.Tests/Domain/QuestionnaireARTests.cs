3555a62ea63a7dbe30f5eb7a1b29814d7ecd8116
Anatoliy 'TLK' Kolesnick <none@none>
2013-02-12 13:11:07 +0200

    QuestionnaireAR\Tests: MoveQuestionnaireItem method covered with tests
    --HG--
    branch : next
    
    
    Former-commit-id: 67eface08113996df165d2aa16d81dd09f5ea894

--- RavenQuestionnaire.Web/Main.Core.Tests/Domain/QuestionnaireARTests.cs
+++ RavenQuestionnaire.Web/Main.Core.Tests/Domain/QuestionnaireARTests.cs
@@ -517,6 +517,57 @@ namespace Main.Core.Tests.Domain
             }
         }
 
+        [Test]
+        public void MoveQuestionnaireItem_When_public_key_specified_Then_raised_QuestionnaireItemMoved_event_with_same_public_key()
+        {
+            using (var eventContext = new EventContext())
+            {
+                // arrange
+                QuestionnaireAR questionnaire = CreateQuestionnaireAR();
+                var publicKey = Guid.NewGuid();
+
+                // act
+                questionnaire.MoveQuestionnaireItem(publicKey, null, null);
+
+                // assert
+                Assert.That(GetSingleEvent<QuestionnaireItemMoved>(eventContext).PublicKey, Is.EqualTo(publicKey));
+            }
+        }
+
+        [Test]
+        public void MoveQuestionnaireItem_When_group_public_key_specified_Then_raised_QuestionnaireItemMoved_event_with_same_group_public_key()
+        {
+            using (var eventContext = new EventContext())
+            {
+                // arrange
+                QuestionnaireAR questionnaire = CreateQuestionnaireAR();
+                var groupPublicKey = Guid.NewGuid();
+
+                // act
+                questionnaire.MoveQuestionnaireItem(Guid.NewGuid(), groupPublicKey, null);
+
+                // assert
+                Assert.That(GetSingleEvent<QuestionnaireItemMoved>(eventContext).GroupKey, Is.EqualTo(groupPublicKey));
+            }
+        }
+
+        [Test]
+        public void MoveQuestionnaireItem_When_public_key_of_item_to_put_after_specified_Then_raised_QuestionnaireItemMoved_event_with_same_public_key_of_item_to_put_after()
+        {
+            using (var eventContext = new EventContext())
+            {
+                // arrange
+                QuestionnaireAR questionnaire = CreateQuestionnaireAR();
+                var afterItemPublicKey = Guid.NewGuid();
+
+                // act
+                questionnaire.MoveQuestionnaireItem(Guid.NewGuid(), null, afterItemPublicKey);
+
+                // assert
+                Assert.That(GetSingleEvent<QuestionnaireItemMoved>(eventContext).AfterItemKey, Is.EqualTo(afterItemPublicKey));
+            }
+        }
+
         private static T GetSingleEvent<T>(EventContext eventContext)
         {
             return (T) eventContext.Events.Single(e => e.Payload is T).Payload;
