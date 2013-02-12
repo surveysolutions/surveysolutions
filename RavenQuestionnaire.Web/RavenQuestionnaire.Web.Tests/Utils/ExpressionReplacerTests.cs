f3f1b1481547cac38fbf1bb01f7c359f913aa81f
Slava Scherbak <slava.scherbak@gmail.com>
2013-02-12 11:04:09 +0200

    designer.ExpressionReplacer: add tests for empty expressions
    --HG--
    branch : next
    
    
    Former-commit-id: ae783b2f891c58e1e97cd89c27ffb7b99b02e2ba

---
+++ RavenQuestionnaire.Web/RavenQuestionnaire.Web.Tests/Utils/ExpressionReplacerTests.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Main.Core.Events;
+using Main.Core.Export;
+using Main.Core.View;
+using Main.Core.View.Export;
+using Moq;
+using NUnit.Framework;
+using Ninject;
+using RavenQuestionnaire.Core.Views.Questionnaire;
+using RavenQuestionnaire.Web.Tests.Export;
+using RavenQuestionnaire.Web.Utils;
+
+namespace RavenQuestionnaire.Web.Tests.Utils
+{
+    [TestFixture]
+    public class ExpressionReplacerTests
+    {
+        public IKernel Kernel { get; set; }
+
+        public Mock<IViewRepository> ViewRepositoryMock { get; set; }
+
+        [SetUp]
+        public void CreateObjects()
+        {
+            this.Kernel = new StandardKernel();
+            this.ViewRepositoryMock = new Mock<IViewRepository>();
+            this.ViewRepositoryMock.Setup(
+                x =>
+                x.Load<QuestionnaireViewInputModel, QuestionnaireStataMapView>(It.IsAny<QuestionnaireViewInputModel>()))
+                .Returns(new QuestionnaireStataMapView()
+                    {
+                        StataMap = new List<KeyValuePair<Guid, string>>()
+                            {
+                                {new KeyValuePair<Guid, string>(Guid.NewGuid(), "caption1")},
+                                {new KeyValuePair<Guid, string>(Guid.NewGuid(), "caption2")},
+                                {new KeyValuePair<Guid, string>(Guid.NewGuid(), "caption3")},
+                                {new KeyValuePair<Guid, string>(Guid.NewGuid(), "caption4")},
+                                {new KeyValuePair<Guid, string>(Guid.NewGuid(), "caption5")}
+                            }
+                    });
+            this.Kernel.Bind<IViewRepository>().ToConstant(this.ViewRepositoryMock.Object);
+        }
+
+        [Test]
+        public void ReplaceStataCaptionsWithGuids_When_expression_is_empty_Then_factory_doesnot_loading()
+        {
+            // Arrange
+            var replacer = CreateExpressionReplacer();
+            var emptyExpression = "";
+
+            // Act
+            replacer.ReplaceStataCaptionsWithGuids(emptyExpression, Guid.NewGuid());
+
+            // Assert
+            ViewRepositoryMock.Verify(x => x.Load<QuestionnaireViewInputModel, QuestionnaireStataMapView>(It.IsAny<QuestionnaireViewInputModel>()), Times.Never());
+        }
+
+        [Test]
+        public void ReplaceGuidsWithStataCaptions_When_expression_is_empty_Then_factory_doesnot_loading()
+        {
+            // Arrange
+            var replacer = CreateExpressionReplacer();
+            var emptyExpression = "";
+
+            // Act
+            replacer.ReplaceGuidsWithStataCaptions(emptyExpression, Guid.NewGuid());
+            
+            // Assert
+            ViewRepositoryMock.Verify(x => x.Load<QuestionnaireViewInputModel, QuestionnaireStataMapView>(It.IsAny<QuestionnaireViewInputModel>()), Times.Never());
+        }
+
+        private ExpressionReplacer CreateExpressionReplacer()
+        {
+            return new ExpressionReplacer(ViewRepositoryMock.Object);
+        }
+    }
+}
