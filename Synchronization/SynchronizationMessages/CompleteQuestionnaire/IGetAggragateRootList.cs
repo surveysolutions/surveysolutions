746a35f978e95753c27db1d6d33e380e187b0948
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2013-02-15 17:27:58 -0500

    manual merge
    --HG--
    branch : monodroid-next
    
    
    Former-commit-id: 8893f2d3b4d06b3e79064363a1ae8a0202158c10

--- Synchronization/SynchronizationMessages/CompleteQuestionnaire/IGetAggragateRootList.cs
+++ Synchronization/SynchronizationMessages/CompleteQuestionnaire/IGetAggragateRootList.cs
@@ -11,7 +11,6 @@ namespace SynchronizationMessages.CompleteQuestionnaire
 {
     using System.IO;
     using System.ServiceModel;
-    using System.ServiceModel.Web;
 
     /// <summary>
     /// The GetAggragateRootList interface.
