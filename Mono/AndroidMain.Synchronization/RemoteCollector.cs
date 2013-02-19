6056670151f8489416f3bf58a0088686f01092e5
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2013-02-19 15:46:46 -0500

    fix content type to avoid asp.net JsonValueProviderFactory content handling
    --HG--
    branch : monodroid-next
    
    
    Former-commit-id: 3d03992feb490a3ae7d05c7f1377ee0ecf8c1510

--- Mono/AndroidMain.Synchronization/RemoteCollector.cs
+++ Mono/AndroidMain.Synchronization/RemoteCollector.cs
@@ -110,15 +110,9 @@ namespace AndroidMain.Synchronization
                 var settings = new JsonSerializerSettings();
                 settings.TypeNameHandling = TypeNameHandling.Objects;
                 string item = JsonConvert.SerializeObject(message, Formatting.None, settings);
-                
-                /*MemoryStream stream = new MemoryStream();
-                message.WriteTo(stream);
-                stream.Position = 0L;
-                var item = new StreamReader(stream).ReadToEnd();*/
-                
-                request.AddParameter("application/json; charset=utf-8", item, ParameterType.RequestBody);
-                
-                //request.AddParameter("request", item);
+
+                // text must be changed to  "application/json; charset=utf-8"
+                request.AddParameter("text; charset=utf-8", item, ParameterType.RequestBody);
                 
                 IRestResponse response = restClient.Execute(request);
                 if (string.IsNullOrWhiteSpace(response.Content) || response.StatusCode != HttpStatusCode.OK)
