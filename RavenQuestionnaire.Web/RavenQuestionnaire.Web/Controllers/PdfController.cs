3ae37d5d6b1fcf3edde1d29478494cc90405e8f0
Anatoliy 'TLK' Kolesnick <none@none>
2013-02-20 14:18:47 +0200

    added PDF preview page stub
    Former-commit-id: 2912c9649823a2f97fa5088b7619239a78e107c7

---
+++ RavenQuestionnaire.Web/RavenQuestionnaire.Web/Controllers/PdfController.cs
@@ -0,0 +1,32 @@
+﻿namespace RavenQuestionnaire.Web.Controllers
+{
+    using System;
+    using System.Web.Mvc;
+
+    using Main.Core.View;
+
+    using RavenQuestionnaire.Core.Views.Questionnaire;
+
+    [Authorize]
+    public class PdfController : Controller
+    {
+        private readonly IViewRepository viewRepository;
+
+        public PdfController(IViewRepository viewRepository)
+        {
+            this.viewRepository = viewRepository;
+        }
+
+        public ActionResult PreviewQuestionnaire(Guid id)
+        {
+            QuestionnaireView viewModel = this.LoadQuestionnaire(id);
+
+            return this.View(viewModel);
+        }
+
+        private QuestionnaireView LoadQuestionnaire(Guid id)
+        {
+            return this.viewRepository.Load<QuestionnaireViewInputModel, QuestionnaireView>(new QuestionnaireViewInputModel(id));
+        }
+    }
+}
