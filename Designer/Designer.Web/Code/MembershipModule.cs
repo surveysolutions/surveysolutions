71ec33b1fb87d7f2aee0653b51ff888947ffd89e
Roman Lytvyn <litvin.roma@gmail.com>
2013-02-25 13:31:37 -0500

    Updated some packeges. Deleted duplicated packages. Added Designer project.
    Former-commit-id: e6e808724fe4dee0f5389a8120c8b4a57af81795

---
+++ Designer/Designer.Web/Code/MembershipModule.cs
@@ -0,0 +1,24 @@
+﻿using Designer.Web.Providers.Membership;
+using Designer.Web.Providers.Membership.PasswordStrategies;
+using Designer.Web.Providers.Repositories.CQRS;
+using Designer.Web.Providers.Roles;
+using Main.Core.View;
+using Ncqrs;
+using Ncqrs.Commanding.ServiceModel;
+using Ninject;
+using Ninject.Modules;
+
+namespace Designer.Web
+{
+    public class MembershipModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IPasswordStrategy>().To<HashPasswordStrategy>().InSingletonScope();
+            Bind<IPasswordPolicy>().ToConstant(PasswordPolicyFactory.CreatePasswordPolicy());
+            Bind<IAccountRepository>().ToConstructor(x => new CQRSAccountRepository(x.Inject<IViewRepository>(), 
+                NcqrsEnvironment.Get<ICommandService>())).InSingletonScope();
+            Bind<IRoleRepository>().To<CQRSRoleRepository>().InSingletonScope();
+        }
+    }
+}
