e1ae2bad21005fa62a688806e36ab417d34700ef
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2013-03-04 16:46:42 +0200

    HQ: made Dashboard.Questionnaires a default action
    --HG--
    branch : hq
    
    
    Former-commit-id: 8928a998532506a4bbdb90c622a7d59bbdbce47d

--- src/UI/WB.UI.Headquarter/App_Start/RouteConfig.cs
+++ src/UI/WB.UI.Headquarter/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace WB.UI.Headquarter
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Dashboard", action = "Questionnaires", id = UrlParameter.Optional }
             );
         }
     }
