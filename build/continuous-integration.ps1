604528617c2a9569e3cd07fc63cef6b2632631a9
Anatoliy 'TLK' Kolesnick <none@none>
2013-03-07 20:06:12 +0200

    build: minor formatting changes
    Former-commit-id: 7dc89af30532795b8bd45ba228858e098d006107

--- build/continuous-integration.ps1
+++ build/continuous-integration.ps1
@@ -2,6 +2,7 @@ $scriptFolder = (Get-Item $MyInvocation.MyCommand.Path).Directory.FullName
 
 . "$scriptFolder\functions.ps1"
 
+
 CleanBinAndObjFolders
 
 BuildSolutions 'Debug' | %{ if (-not $_) { Exit } }
--- build/deploy-designer.ps1
+++ build/deploy-designer.ps1
@@ -2,6 +2,7 @@ $scriptFolder = (Get-Item $MyInvocation.MyCommand.Path).Directory.FullName
 
 . "$scriptFolder\functions.ps1"
 
+
 CleanBinAndObjFolders
 
 BuildSolution '.\src\Designer.sln' 'Release' | %{ if (-not $_) { Exit } }
