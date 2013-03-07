8c6e746a68e8f2990bef05d54b5818daa41d784b
Anatoliy 'TLK' Kolesnick <none@none>
2013-03-07 20:47:10 +0200

    build\deploy\designer: added copying of deployment package to target server
    Former-commit-id: a168936acae1688c5945ed92a9bc14d6ba701bc5

--- build/deploy-designer.ps1
+++ build/deploy-designer.ps1
@@ -5,8 +5,12 @@ $scriptFolder = (Get-Item $MyInvocation.MyCommand.Path).Directory.FullName
 
 CleanBinAndObjFolders
 
-BuildSolution '.\src\Designer.sln' 'Release' | %{ if (-not $_) { Exit } }
+BuildSolution 'src\Designer.sln' 'Release' | %{ if (-not $_) { Exit } }
 
 RunTests 'Release'
 
-& (GetPathToMSBuild) '.\src\UI\Designer\WB.UI.Designer\WB.UI.Designer.csproj' '/t:Package' '/p:Configuration=Release'
+& (GetPathToMSBuild) 'src\UI\Designer\WB.UI.Designer\WB.UI.Designer.csproj' '/t:Package' '/p:Configuration=Release'
+
+Remove-Item \\192.168.3.113\Web\Designer\* -Force -Recurse
+
+Copy-Item src\UI\Designer\WB.UI.Designer\obj\Release\Package\PackageTmp\* \\192.168.3.113\Web\Designer -Recurse
