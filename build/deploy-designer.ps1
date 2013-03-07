c1fd1cbc5efadae92dfbe0d8908930faa0be17cf
Anatoliy 'TLK' Kolesnick <none@none>
2013-03-07 17:19:24 +0200

    build\deploy\designer: added building of deployment package
    Former-commit-id: 4b8fcc243ef37ee4d5ebdf0d10ac319f52a867d1

--- build/deploy-designer.ps1
+++ build/deploy-designer.ps1
@@ -7,3 +7,5 @@ CleanBinAndObjFolders
 BuildSolution '.\src\Designer.sln' 'Release' | %{ if (-not $_) { Exit } }
 
 RunTests 'Release'
+
+& (GetPathToMSBuild) '.\src\UI\Designer\WB.UI.Designer\WB.UI.Designer.csproj' '/t:Package' '/p:Configuration=Release'
