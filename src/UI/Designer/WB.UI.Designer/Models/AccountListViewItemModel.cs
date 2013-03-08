162ec61afb9ca5f8f2a7377f506ffe0b5f5d7dcf
Roman Lytvyn <litvin.roma@gmail.com>
2013-03-08 13:15:27 -0500

    removed unused files and parts of code. fixed design. (part II)
    Former-commit-id: 72564ab121d6da2f3e2f748283ad118321591015

---
+++ src/UI/Designer/WB.UI.Designer/Models/AccountListViewItemModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WB.UI.Designer.Models
+{
+    public class AccountListViewItemModel
+    {
+        [Key]
+        public string Id { get; set; }
+        [Display(Name = "Name")]
+        public string UserName { get; set; }
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+        [Display(Name = "Created date")]
+        public string CreationDate { get; set; }
+        [Display(Name = "Last login")]
+        public string LastLoginDate { get; set; }
+        [Display(Name = "Approved?")]
+        public bool IsApproved { get; set; }
+        [Display(Name = "Locked?")]
+        public bool IsLockedOut { get; set; }
+    }
+}
---
+++ src/UI/Designer/WB.UI.Designer/Models/AccountViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace WB.UI.Designer.Models
+{
+    [DisplayName("Account")]
+    public class AccountViewModel
+    {
+        [Key]
+        public string Id { get; set; }
+
+        [Display(Name = "Name")]
+        public string UserName { get; set; }
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+        [Display(Name = "Created date")]
+        public DateTime CreationDate { get; set; }
+        [Display(Name = "Last login")]
+        public DateTime LastLoginDate { get; set; }
+        [Display(Name = "Is Approved")]
+        public bool IsApproved { get; set; }
+        [Display(Name = "Is Locked Out")]
+        public bool IsLockedOut { get; set; }
+        [Display(Name = "Is Online")]
+        public bool IsOnline { get; set; }
+        [Display(Name = "Last Activity Date")]
+        public DateTime LastActivityDate { get; set; }
+        [Display(Name = "Last Lockout Date")]
+        public DateTime LastLockoutDate { get; set; }
+        [Display(Name = "Last Password Changed Date")]
+        public DateTime LastPasswordChangedDate { get; set; }
+        [Display(Name = "Password Question")]
+        public string PasswordQuestion { get; set; }
+        [Display(Name = "Comment")]
+        public string Comment { get; set; }
+    }
+}
