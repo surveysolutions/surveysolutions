850587fc66cc4783c1adec79d0d1791dbbcb44e4
Roman Lytvyn <litvin.roma@gmail.com>
2013-03-08 13:15:27 -0500

    removed unused files and parts of code. fixed design. (part II)
    Former-commit-id: 72564ab121d6da2f3e2f748283ad118321591015

--- src/UI/Designer/WB.UI.Designer/Models/ExternalLogin.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WB.UI.Designer.Models
-{
-    public class ExternalLogin
-    {
-        public string Provider { get; set; }
-        public string ProviderDisplayName { get; set; }
-        public string ProviderUserId { get; set; }
-    }
-}
