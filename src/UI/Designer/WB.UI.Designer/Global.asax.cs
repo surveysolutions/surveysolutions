80ef521356cdefa9214ad33e0b2edfada581b7b9
Roman Lytvyn <none@none>
2013-03-11 14:24:27 -0400

    added handling exceptions
    Former-commit-id: 47b032abb660c76c9fca9b09348c69180cec54fb

--- src/UI/Designer/WB.UI.Designer/Global.asax.cs
+++ src/UI/Designer/WB.UI.Designer/Global.asax.cs
@@ -1,11 +1,14 @@
-﻿using System.Web.Http;
+﻿using System;
+using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using WB.UI.Designer.Controllers;
 
 namespace WB.UI.Designer
 {
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : HttpApplication
     {
         protected void Application_Start()
         {
@@ -19,6 +22,54 @@ namespace WB.UI.Designer
             BootstrapBundleConfig.RegisterBundles(BundleTable.Bundles);
 
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            var httpContext = ((MvcApplication) sender).Context;
+            var currentController = string.Empty;
+            var currentAction = string.Empty;
+            var currRouteData = RouteTable.Routes.GetRouteData(new HttpContextWrapper(httpContext));
+            if (currRouteData != null)
+            {
+                currentController = (string) currRouteData.Values["controller"] ?? string.Empty;
+                currentAction = (string) currRouteData.Values["action"] ?? string.Empty;
+            }
+
+            var ex = Server.GetLastError();
+
+            NLog.LogManager.GetCurrentClassLogger().Error(ex);
+
+            var controller = new ErrorController();
+            var routeData = new RouteData();
+            var action = "Index";
+
+            if (ex is HttpException)
+            {
+                var httpEx = ex as HttpException;
+
+                switch (httpEx.GetHttpCode())
+                {
+                    case 404:
+                        action = "NotFound";
+                        break;
+
+                    case 401:
+                        action = "AccessDenied";
+                        break;
+                }
+            }
+
+            httpContext.ClearError();
+            httpContext.Response.Clear();
+            httpContext.Response.StatusCode = ex is HttpException ? ((HttpException) ex).GetHttpCode() : 500;
+            httpContext.Response.TrySkipIisCustomErrors = true;
+
+            routeData.Values["controller"] = "Error";
+            routeData.Values["action"] = action;
+
+            controller.ViewData.Model = new HandleErrorInfo(ex, currentController, currentAction);
+            ((IController) controller).Execute(new RequestContext(new HttpContextWrapper(httpContext), routeData));
         }
     }
 }
