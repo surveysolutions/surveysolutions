c1b0c98e03b91b49bb0372ee6be2406a267d2e22
Anatoliy 'TLK' Kolesnick <none@none>
2013-03-11 13:51:29 +0200

    RavenQuestionnaire.Web: registered Ninject kernel to CommonServiceLocator instead of KernelLocator
    --HG--
    branch : common-service-locator
    
    
    Former-commit-id: 5b36743ec85e4b3ab19ad977e6ebf887694873a1

--- RavenQuestionnaire.Web/RavenQuestionnaire.Web/App_Start/NinjectWebCommon.cs
+++ RavenQuestionnaire.Web/RavenQuestionnaire.Web/App_Start/NinjectWebCommon.cs
@@ -13,10 +13,13 @@ namespace RavenQuestionnaire.Web.App_Start
     using System;
     using System.Web;
 
+    using Microsoft.Practices.ServiceLocation;
     using Microsoft.Web.Infrastructure.DynamicModuleHelper;
 
     using Ninject;
     using Ninject.Web.Common;
+
+    using NinjectAdapter;
 
     public static class NinjectWebCommon 
     {
@@ -60,7 +63,7 @@ namespace RavenQuestionnaire.Web.App_Start
 
             ModelBinders.Binders.DefaultBinder = new GenericBinderResolver(kernel);
 
-            KernelLocator.SetKernel(kernel);
+            ServiceLocator.SetLocatorProvider(() => new NinjectServiceLocator(kernel));
             kernel.Bind<Func<IKernel>>().ToMethod(ctx => () => new Bootstrapper().Kernel);
             kernel.Bind<IHttpModule>().To<HttpApplicationInitializationHttpModule>();
             NcqrsInit.Init(/*WebConfigurationManager.AppSettings["Raven.DocumentStore"],*/ kernel);
