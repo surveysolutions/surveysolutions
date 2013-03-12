cc14a04c11ef67bd883be5d83b204e7771b40e8c
Anatoliy 'TLK' Kolesnick <none@none>
2013-03-12 13:44:38 +0200

    Designer: added empty command controller
    Former-commit-id: 7fb61e6f33bb491591ff42feeb99b8b2e3afc407

---
+++ src/UI/Designer/WB.UI.Designer/Controllers/CommandController.cs
@@ -0,0 +1,13 @@
+﻿namespace WB.UI.Designer.Controllers
+{
+    using System.Web.Mvc;
+
+    using Ncqrs.Commanding;
+
+    public class CommandController : Controller
+    {
+        public ActionResult Execute(object command)
+        {
+        }
+    }
+}
