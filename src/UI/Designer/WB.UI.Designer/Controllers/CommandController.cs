c82b0f2144e3abe1a9d11181db202ff65a959a2a
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2013-03-12 15:02:40 +0200

    Designer\Commands: added rough handling of update group command
    Former-commit-id: c6607041bcc0fd24275d66559f4c5b9cbc17431f

--- src/UI/Designer/WB.UI.Designer/Controllers/CommandController.cs
+++ src/UI/Designer/WB.UI.Designer/Controllers/CommandController.cs
@@ -1,14 +1,34 @@
 ﻿namespace WB.UI.Designer.Controllers
 {
+    using System;
     using System.Web.Mvc;
 
+    using Main.Core.Commands.Questionnaire.Group;
+
     using Ncqrs.Commanding;
+    using Ncqrs.Commanding.ServiceModel;
+
+    using Newtonsoft.Json.Linq;
 
     public class CommandController : Controller
     {
+        private readonly ICommandService commandService;
+
+        public CommandController(ICommandService commandService)
+        {
+            this.commandService = commandService;
+        }
+
         [HttpPost]
         public ActionResult Execute(string command)
         {
+            dynamic parsedCommand = JObject.Parse(command);
+
+            var concreteCommand = new NewUpdateGroupCommand(
+                Guid.Parse((string)parsedCommand.questionnaireId), Guid.Parse((string)parsedCommand.groupId), (string)parsedCommand.title);
+
+            this.commandService.Execute(concreteCommand);
+
             return new EmptyResult();
         }
     }
