c7d17f66bdb60e630b57b866692ea28b4c74a376
Slava Scherbak <slava.scherbak@gmail.com>
2013-03-13 12:58:26 +0200

    designer\views: added QuestionnaireView factory and view
    Former-commit-id: 24b6c104b2e999093be2569d7ca8da27e76f143c

---
+++ src/UI/Designer/WB.UI.Designer.Views/Questionnaire/QuestionnaireViewInputModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WB.UI.Designer.Views.Questionnaire
+{
+    public class QuestionnaireViewInputModel
+    {
+        public QuestionnaireViewInputModel(Guid id)
+        {
+            this.QuestionnaireId = id;
+        }
+       
+        public Guid QuestionnaireId { get; private set; }
+    }
+}
