3aada24746b494e9f37e888c3402845d266e8428
Roman Lytvyn <litvin.roma@gmail.com>
2013-03-13 10:12:38 -0400

    delete for questionnaire. my and public questionnaires.
    Former-commit-id: 0323d01ccc0fcf64e271d185cd191602021dbd45

---
+++ src/UI/Designer/WB.UI.Designer/Models/DeleteQuestionnaireModel.cs
@@ -0,0 +1,34 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="DeleteQuestionnaireModel.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   The delete questionnaire model.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace WB.UI.Designer.Models
+{
+    using System;
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
+
+    /// <summary>
+    /// The delete questionnaire model.
+    /// </summary>
+    [DisplayName("Questionnaire")]
+    public class DeleteQuestionnaireModel
+    {
+        /// <summary>
+        /// Gets or sets the id.
+        /// </summary>
+        [Key]
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the title.
+        /// </summary>
+        [Display(Name = "Title")]
+        public string Title { get; set; }
+    }
+}
