58b584382e5c3ae9452a2f0613bada59c04c111f
Roman Lytvyn <none@none>
2013-03-13 10:12:38 -0400

    delete for questionnaire. my and public questionnaires.
    Former-commit-id: 0323d01ccc0fcf64e271d185cd191602021dbd45

---
+++ RavenQuestionnaire.Web/Main.Core/Commands/Questionnaire/DeleteQuestionnaireCommand.cs
@@ -0,0 +1,49 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="DeleteQuestionnaireCommand.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   The Delete questionnaire command.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+namespace Main.Core.Commands.Questionnaire
+{
+    using System;
+
+    using Main.Core.Domain;
+
+    using Ncqrs.Commanding;
+    using Ncqrs.Commanding.CommandExecution.Mapping.Attributes;
+
+    /// <summary>
+    /// The Delete questionnaire command.
+    /// </summary>
+    [Serializable]
+    [MapsToAggregateRootMethod(typeof(QuestionnaireAR), "DeleteQuestionnaire")]
+    public class DeleteQuestionnaireCommand : CommandBase
+    {
+        #region Constructors and Destructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DeleteQuestionnaireCommand"/> class. 
+        /// </summary>
+        /// <param name="questionnaireId">
+        /// </param>
+        public DeleteQuestionnaireCommand(Guid questionnaireId)
+        {
+            this.QuestionnaireId = questionnaireId;
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        /// <summary>
+        /// Gets or sets the questionnaire id.
+        /// </summary>
+        [AggregateRootId]
+        public Guid QuestionnaireId { get; set; }
+
+        #endregion
+    }
+}
