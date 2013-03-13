15849d7e547fe8879b21918f24652ae4b2a7a1a8
Roman Lytvyn <litvin.roma@gmail.com>
2013-03-13 19:20:37 -0400

    designer: added paging and sorting for grids
    Former-commit-id: eb295bf4bd991ca659a8a372ada406b1ff684838

---
+++ src/UI/Designer/WB.UI.Designer/Code/GlobalHelper.cs
@@ -0,0 +1,54 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="GlobalHelper.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   The global helper.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace WB.UI.Designer
+{
+    using System.Web;
+
+    /// <summary>
+    /// The global helper.
+    /// </summary>
+    public static class GlobalHelper
+    {
+        #region Constants
+
+        /// <summary>
+        /// The grid page items count.
+        /// </summary>
+        public const int GridPageItemsCount = 10;
+
+        #endregion
+
+        #region Public Properties
+
+        /// <summary>
+        /// Gets the current action.
+        /// </summary>
+        public static string CurrentAction
+        {
+            get
+            {
+                return (string)HttpContext.Current.Request.RequestContext.RouteData.Values["action"];
+            }
+        }
+
+        /// <summary>
+        /// Gets the current controller.
+        /// </summary>
+        public static string CurrentController
+        {
+            get
+            {
+                return (string)HttpContext.Current.Request.RequestContext.RouteData.Values["controller"];
+            }
+        }
+
+        #endregion
+    }
+}
