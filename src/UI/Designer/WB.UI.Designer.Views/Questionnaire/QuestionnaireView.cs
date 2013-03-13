d2f13f70cd71ffa775798bc4e66e334dd2b09649
Slava Scherbak <slava.scherbak@gmail.com>
2013-03-13 12:58:26 +0200

    designer\views: added QuestionnaireView factory and view
    Former-commit-id: 24b6c104b2e999093be2569d7ca8da27e76f143c

---
+++ src/UI/Designer/WB.UI.Designer.Views/Questionnaire/QuestionnaireView.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Main.Core.Documents;
+using Main.Core.Entities.Composite;
+using Main.Core.Entities.SubEntities;
+using Main.Core.View;
+using RavenQuestionnaire.Core.Views.Group;
+
+namespace WB.UI.Designer.Views.Questionnaire
+{
+    public class QuestionnaireView
+    {
+        public QuestionnaireView(IQuestionnaireDocument doc)
+        {
+            this.PublicKey = doc.PublicKey;
+            this.Title = doc.Title;
+            this.PublicKey = doc.PublicKey;
+            this.Title = doc.Title;
+            this.CreationDate = doc.CreationDate;
+            this.LastEntryDate = doc.LastEntryDate;
+
+            this.Children = ConvertChildrenFromQuestionnaireDocument(doc).ToList();
+        }
+
+        public List<ICompositeView> Children { get; set; }
+        public Guid? Parent { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime LastEntryDate { get; set; }
+        public Guid PublicKey { get; set; }
+        public string Title { get; set; }
+
+        private static IEnumerable<ICompositeView> ConvertChildrenFromQuestionnaireDocument(IQuestionnaireDocument doc)
+        {
+            return doc.Children
+                .Cast<IGroup>()
+                .Select(@group => new GroupView(doc, @group));
+        }
+    }
+}
