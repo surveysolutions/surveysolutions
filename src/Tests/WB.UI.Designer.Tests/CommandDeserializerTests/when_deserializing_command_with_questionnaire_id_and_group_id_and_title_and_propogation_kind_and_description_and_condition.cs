1e6cdfd1eed4513aaa41020f6f19716bbb8a7343
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2013-03-14 13:41:35 +0200

    Designer\UpdateGroupCommand: added all properties from interface
    Former-commit-id: b2247d9de0feb303dbfd314ad7b5e233d4863baa

--- src/Tests/WB.UI.Designer.Tests/CommandDeserializerTests/when_deserializing_command_with_questionnaire_id_and_group_id_and_title_and_propogation_kind_and_description_and_condition.cs
+++ src/Tests/WB.UI.Designer.Tests/CommandDeserializerTests/when_deserializing_command_with_questionnaire_id_and_group_id_and_title_and_propogation_kind_and_description_and_condition.cs
@@ -5,24 +5,31 @@
     using Machine.Specifications;
 
     using Main.Core.Commands.Questionnaire.Group;
+    using Main.Core.Entities.SubEntities;
 
     using Ncqrs.Commanding;
 
     using WB.UI.Designer.Code.Helpers;
 
-    internal class when_deserializing_command_with_questionnaire_id_and_group_id_and_title : CommandDeserializerTestsContext
+    internal class when_deserializing_command_with_questionnaire_id_and_group_id_and_title_and_propogation_kind_and_description_and_condition : CommandDeserializerTestsContext
     {
         Establish context = () =>
         {
             title = "MAIN";
             questionnaireId = "11111111-1111-1111-1111-111111111111";
             groupId = "22222222-2222-2222-2222-222222222222";
+            propagationKind = "AutoPropagated";
+            description = "Some description";
+            condition = "1 == 2";
 
             command = string.Format(@"{{
                 ""questionnaireId"": ""{0}"",
                 ""groupId"": ""{1}"",
-                ""title"": ""{2}""
-            }}", questionnaireId, groupId, title);
+                ""title"": ""{2}"",
+                ""propagationKind"": ""{3}"",
+                ""description"": ""{4}"",
+                ""condition"": ""{5}""
+            }}", questionnaireId, groupId, title, propagationKind, description, condition);
 
             deserializer = CreateCommandDeserializer();
         };
@@ -42,11 +49,23 @@
         It should_return_same_group_id_in_NewUpdateGroupCommand = () =>
             ((NewUpdateGroupCommand)result).GroupId.ShouldEqual(Guid.Parse(groupId));
 
+        It should_return_same_propagation_kind_in_NewUpdateGroupCommand = () =>
+            ((NewUpdateGroupCommand)result).PropagationKind.ShouldEqual(Enum.Parse(typeof(Propagate), propagationKind));
+
+        It should_return_same_description_in_NewUpdateGroupCommand = () =>
+            ((NewUpdateGroupCommand)result).Description.ShouldEqual(description);
+
+        It should_return_same_condition_in_NewUpdateGroupCommand = () =>
+            ((NewUpdateGroupCommand)result).Condition.ShouldEqual(condition);
+
         private static ICommand result;
         private static CommandDeserializer deserializer;
         private static string command;
         private static string title;
         private static string questionnaireId;
         private static string groupId;
+        private static string propagationKind;
+        private static string description;
+        private static string condition;
     }
 }
