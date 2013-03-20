faa974ea36871f8db503fa2fe1ae4622f2695dc4
Anatoliy 'TLK' Kolesnick <none@none>
2013-03-21 00:38:49 +0200

    Core: added DomainException
    Former-commit-id: 943ce7997ead1883344d388c97b1068631036d39

---
+++ RavenQuestionnaire.Web/Main.Core/Domain/Exceptions/DomainException.cs
@@ -0,0 +1,23 @@
+﻿namespace Main.Core.Domain
+{
+    using System;
+    using System.Runtime.Serialization;
+
+    /// <summary>
+    /// Exception which represents domain logic and is usually thrown by aggregate root command handlers.
+    /// </summary>
+    [Serializable]
+    public class DomainException : Exception
+    {
+        public DomainException() {}
+
+        public DomainException(string message)
+            : base(message) {}
+
+        public DomainException(string message, Exception innerException)
+            : base(message, innerException) {}
+
+        protected DomainException(SerializationInfo info, StreamingContext context)
+            : base(info, context) {}
+    }
+}
