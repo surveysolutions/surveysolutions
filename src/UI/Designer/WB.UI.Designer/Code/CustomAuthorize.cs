1d7116d44005bca2471946f9c1ed36f51001f644
Roman Lytvyn <litvin.roma@gmail.com>
2013-03-20 18:47:28 -0400

    designer: added configuration for log. changed email notificator. fixed some bugs with membership provider.
    Former-commit-id: 5957872c451222b40541f1ccb27b485eb16f30ab

---
+++ src/UI/Designer/WB.UI.Designer/Code/CustomAuthorize.cs
@@ -0,0 +1,66 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="CustomAuthorize.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   The custom authorize.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace WB.UI.Designer
+{
+    using System.Web.Mvc;
+    using System.Web.Routing;
+
+    using WB.UI.Designer.Controllers;
+    using WB.UI.Designer.Models;
+
+    /// <summary>
+    /// The custom authorize.
+    /// </summary>
+    public class CustomAuthorize : AuthorizeAttribute
+    {
+        #region Methods
+
+        public override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            var isInvalidUser = false;
+
+            if (filterContext.HttpContext.User.Identity.IsAuthenticated)
+            {
+                isInvalidUser = UserHelper.CurrentUser == null || UserHelper.CurrentUser.IsLockedOut
+                                      || !UserHelper.CurrentUser.IsApproved;
+
+                if (UserHelper.CurrentUser != null)
+                {
+                    var baseController = filterContext.Controller as AlertController;
+                    if (baseController != null)
+                    {
+                        if (!UserHelper.CurrentUser.IsApproved)
+                        {
+                            baseController.Error("Please, confirm your account first. Check your emails.");
+                        }
+                        else if (UserHelper.CurrentUser.IsLockedOut)
+                        {
+                            baseController.Error(
+                                "Your account is blocked. Contact the administrator for unblocking account");
+                        }
+                    }
+                }
+            }
+
+            if (isInvalidUser)
+            {
+                UserHelper.Logout();
+                filterContext.Result =
+                    new RedirectToRouteResult(new RouteValueDictionary(new { controller = "Account", action = "Login" }));
+            }
+            else
+            {
+                base.OnAuthorization(filterContext);
+            }
+        }
+
+        #endregion
+    }
+}
