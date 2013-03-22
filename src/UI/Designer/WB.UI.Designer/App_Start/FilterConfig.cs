407ea6c041af377bcf1b803fb89d984d24c55306
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2013-03-22 19:13:33 +0200

    Designer: RequiresReadLayerFilter is no longer global, but is assigned to all controllers except ErrorController
    Former-commit-id: 55f7606f9919440fd395361c6f8ec89e99d95d2d

--- src/UI/Designer/WB.UI.Designer/App_Start/FilterConfig.cs
+++ src/UI/Designer/WB.UI.Designer/App_Start/FilterConfig.cs
@@ -3,14 +3,11 @@ using System.Web.Mvc;
 
 namespace WB.UI.Designer
 {
-    using WB.UI.Designer.Filters;
-
     public class FilterConfig
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
-            filters.Add(new RequiresReadLayerFilter());
         }
     }
 }
