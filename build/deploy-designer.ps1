b8c84c1fe31ab6f5ce085f28f291ab804618ee0c
Anatoliy 'TLK' Kolesnick <none@none>
2013-03-26 16:22:41 +0200

    build\deploy: extracted separate method for all deployment operations
    Former-commit-id: 0afc143cd55e85e01046299f61b120ab227c8d81

--- build/deploy-designer.ps1
+++ build/deploy-designer.ps1
@@ -3,14 +3,25 @@ $scriptFolder = (Get-Item $MyInvocation.MyCommand.Path).Directory.FullName
 . "$scriptFolder\functions.ps1"
 
 
-CleanBinAndObjFolders
+function Deploy($Solution, $Project, $BuildConfiguration, $SourceFolder, $TargetFolder) {
 
-BuildSolution 'src\Designer.sln' 'Release' | %{ if (-not $_) { Exit } }
+    CleanBinAndObjFolders
 
-RunTests 'Release'
+    BuildSolution $Solution $BuildConfiguration | %{ if (-not $_) { Exit } }
 
-& (GetPathToMSBuild) 'src\UI\Designer\WB.UI.Designer\WB.UI.Designer.csproj' '/t:Package' '/p:Configuration=Release' | Write-Host
+    RunTests $BuildConfiguration
 
-Remove-Item \\192.168.3.113\Web\Designer\* -Force -Recurse
+    & (GetPathToMSBuild) $Project '/t:Package' "/p:Configuration=$BuildConfiguration" | Write-Host
 
-Copy-Item src\UI\Designer\WB.UI.Designer\obj\Release\Package\PackageTmp\* \\192.168.3.113\Web\Designer -Recurse
+    Remove-Item "$TargetFolder\*" -Force -Recurse
+
+    Copy-Item "$SourceFolder\*" $TargetFolder -Recurse
+}
+
+
+Deploy `
+    -Solution 'src\Designer.sln' `
+    -Project 'src\UI\Designer\WB.UI.Designer\WB.UI.Designer.csproj' `
+    -BuildConfiguration 'Release' `
+    -SourceFolder 'src\UI\Designer\WB.UI.Designer\obj\Release\Package\PackageTmp' `
+    -TargetFolder '\\192.168.3.113\Web\Designer' `
