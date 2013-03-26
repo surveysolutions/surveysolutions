c0085cf32155af8ed41a6277e8bea4e4d57b43fa
Roman Lytvyn <litvin.roma@gmail.com>
2013-03-26 12:22:02 -0400

    Designer: deleted miniprofiler. added popup for delete items.
    --HG--
    branch : next
    
    
    Former-commit-id: 1f4b5660d29b3f74b0adeae88c0d3580f8d364d7

---
+++ src/UI/Designer/WB.UI.Designer/Code/DefaultAttribute.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="DefaultAttribute.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   The default attribute.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace WB.UI.Designer
+{
+    using System;
+
+    /// <summary>
+    ///     The default attribute.
+    /// </summary>
+    public class DefaultAttribute : Attribute
+    {
+    }
+}
---
+++ src/UI/Designer/WB.UI.Designer/Scripts/common.js
@@ -0,0 +1,16 @@
+﻿function ItemViewModel() {
+    var self = this;
+    self.itemId = ko.observable('');
+    self.itemName = ko.observable('');
+    self.itemType = ko.observable('');
+
+    self.deleteItem = function (id, type, name) {
+        self.itemName(name);
+        self.itemType(type);
+        self.itemId(id);
+    };
+}
+
+$(function () {
+    ko.applyBindings(new ItemViewModel());
+});
