89a75ba6feb797a7d3687a7df549ab532dbbe752
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2013-03-26 14:13:00 +0200

    Designer\CommandController: refactoring
    --HG--
    branch : next
    
    
    Former-commit-id: 0b99f85b77d47ac44622d73cc5e7508980971b93

---
+++ src/UI/Designer/WB.UI.Designer/Utils/IExpressionReplacer.cs
@@ -0,0 +1,33 @@
+﻿namespace WB.UI.Designer.Utils
+{
+    using System;
+
+    public interface IExpressionReplacer 
+    {
+        /// <summary>
+        /// Replaces all occurrences of stata captions in expression with public keys (guids) 
+        /// </summary>
+        /// <param name="expression">
+        /// Condition or validation expression to encode
+        /// </param>
+        /// <param name="questionnaireKey">
+        /// Questionnaire public key
+        /// </param>
+        /// <returns>
+        /// Encoded expression with public keys instead of stata captions
+        /// </returns>
+        string ReplaceStataCaptionsWithGuids(string expression, Guid questionnaireKey);
+
+        /// <summary>
+        /// Replaces all occurrences question public keys in expression with stata caption
+        /// </summary>
+        /// <param name="expression">
+        ///  Condition or validation expression to decode
+        /// </param>
+        /// <param name="questionnaireKey">
+        /// Decode expression with stata captions instead of public keys
+        /// </param>
+        /// <returns></returns>
+        string ReplaceGuidsWithStataCaptions(string expression, Guid questionnaireKey);
+    }
+}
