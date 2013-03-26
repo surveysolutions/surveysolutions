c98623009083065dad0fd77b587798511c1bdeb1
Anatoliy 'TLK' Kolesnick <none@none>
2013-03-26 17:07:46 +0200

    build\deploy: extracted deployment method to separate file
    Former-commit-id: 02f4ef1a7f50ba31e0695d2eaa067e895967ecca

--- build/deploy-designer.ps1
+++ build/deploy-designer.ps1
@@ -1,22 +1,6 @@
 $scriptFolder = (Get-Item $MyInvocation.MyCommand.Path).Directory.FullName
 
-. "$scriptFolder\functions.ps1"
-
-
-function Deploy($Solution, $Project, $BuildConfiguration, $SourceFolder, $TargetFolder) {
-
-    CleanBinAndObjFolders
-
-    BuildSolution $Solution $BuildConfiguration | %{ if (-not $_) { Exit } }
-
-    RunTests $BuildConfiguration
-
-    & (GetPathToMSBuild) $Project '/t:Package' "/p:Configuration=$BuildConfiguration" | Write-Host
-
-    Remove-Item "$TargetFolder\*" -Force -Recurse
-
-    Copy-Item "$SourceFolder\*" $TargetFolder -Recurse
-}
+. "$scriptFolder\deployment-functions.ps1"
 
 
 Deploy `
