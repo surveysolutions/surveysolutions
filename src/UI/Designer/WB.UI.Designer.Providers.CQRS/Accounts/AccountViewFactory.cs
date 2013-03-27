69b9175486efdf6a8292e5b97cc46a894b0e17c6
Roman Lytvyn <none@none>
2013-03-27 16:42:49 -0400

    Designer: Highlighting not valid fields for editable forms. User name validation. Any manipulations with user by user id.
    --HG--
    branch : next
    
    
    Former-commit-id: 842414e56b3775e0433d8684018a9c674156363e

--- src/UI/Designer/WB.UI.Designer.Providers.CQRS/Accounts/AccountViewFactory.cs
+++ src/UI/Designer/WB.UI.Designer.Providers.CQRS/Accounts/AccountViewFactory.cs
@@ -1,25 +1,34 @@
-﻿using WB.UI.Designer.Providers.CQRS.Accounts.View;
-using Main.Core.View;
-using Main.DenormalizerStorage;
-using System;
-using System.Linq;
-using Main.Core.Utility;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AccountViewFactory.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   The account view factory.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace WB.UI.Designer.Providers.CQRS.Accounts
 {
+    using System;
+    using System.Linq;
+
+    using Main.Core.Utility;
+    using Main.Core.View;
+    using Main.DenormalizerStorage;
+
     using WB.UI.Designer.Providers.CQRS.Accounts.View;
 
     /// <summary>
-    /// The account view factory.
+    ///     The account view factory.
     /// </summary>
     public class AccountViewFactory : IViewFactory<AccountViewInputModel, AccountView>
     {
         #region Fields
 
         /// <summary>
-        /// The accounts.
+        ///     The accounts.
         /// </summary>
-        private readonly IDenormalizerStorage<AccountDocument> _accounts;
+        private readonly IDenormalizerStorage<AccountDocument> accounts;
 
         #endregion
 
@@ -33,7 +42,7 @@ namespace WB.UI.Designer.Providers.CQRS.Accounts
         /// </param>
         public AccountViewFactory(IDenormalizerStorage<AccountDocument> accounts)
         {
-            _accounts = accounts;
+            this.accounts = accounts;
         }
 
         #endregion
@@ -51,20 +60,20 @@ namespace WB.UI.Designer.Providers.CQRS.Accounts
         /// </returns>
         public AccountView Load(AccountViewInputModel input)
         {
-            Func<AccountDocument,bool> query = (x) => false;
+            Func<AccountDocument, bool> query = (x) => false;
             if (input.ProviderUserKey != null)
             {
-                query = (x) => x.ProviderUserKey == input.ProviderUserKey;
+                query = (x) => x.ProviderUserKey.Equals(input.ProviderUserKey);
             }
-            else if(!string.IsNullOrEmpty( input.AccountName))
+            else if (!string.IsNullOrEmpty(input.AccountName))
             {
                 query = (x) => x.UserName.Compare(input.AccountName);
             }
-            else if(!string.IsNullOrEmpty(input.AccountEmail))
+            else if (!string.IsNullOrEmpty(input.AccountEmail))
             {
                 query = (x) => x.Email.Compare(input.AccountEmail);
             }
-            else if(!string.IsNullOrEmpty(input.ConfirmationToken))
+            else if (!string.IsNullOrEmpty(input.ConfirmationToken))
             {
                 query = (x) => x.ConfirmationToken == input.ConfirmationToken;
             }
@@ -73,34 +82,41 @@ namespace WB.UI.Designer.Providers.CQRS.Accounts
                 query = (x) => x.PasswordResetToken == input.ResetPasswordToken;
             }
 
-            return _accounts.Query().Where(query).Select(x=>new AccountView()
-                {
-                    ApplicationName = x.ApplicationName,
-                    ProviderUserKey = x.ProviderUserKey,
-                    UserName = x.UserName,
-                    Comment = x.Comment,
-                    ConfirmationToken = x.ConfirmationToken,
-                    CreatedAt = x.CreatedAt,
-                    Email = x.Email,
-                    FailedPasswordAnswerWindowAttemptCount = x.FailedPasswordAnswerWindowAttemptCount,
-                    FailedPasswordAnswerWindowStartedAt = x.FailedPasswordAnswerWindowStartedAt,
-                    FailedPasswordWindowAttemptCount = x.FailedPasswordWindowAttemptCount,
-                    FailedPasswordWindowStartedAt = x.FailedPasswordWindowStartedAt,
-                    IsConfirmed = x.IsConfirmed,
-                    IsLockedOut = x.IsLockedOut,
-                    IsOnline = x.IsOnline,
-                    LastActivityAt = x.LastActivityAt,
-                    LastLockedOutAt = x.LastLockedOutAt,
-                    LastLoginAt = x.LastLoginAt,
-                    LastPasswordChangeAt = x.LastPasswordChangeAt,
-                    Password = x.Password,
-                    PasswordSalt = x.PasswordSalt,
-                    PasswordQuestion = x.PasswordQuestion,
-                    PasswordAnswer = x.PasswordAnswer,
-                    PasswordResetToken = x.PasswordResetToken,
-                    PasswordResetExpirationDate = x.PasswordResetExpirationDate,
-                    SimpleRoles = x.SimpleRoles
-                }).FirstOrDefault();
+            return
+                this.accounts.Query()
+                    .Where(query)
+                    .Select(
+                        x =>
+                        new AccountView
+                            {
+                                ApplicationName = x.ApplicationName, 
+                                ProviderUserKey = x.ProviderUserKey, 
+                                UserName = x.UserName, 
+                                Comment = x.Comment, 
+                                ConfirmationToken = x.ConfirmationToken, 
+                                CreatedAt = x.CreatedAt, 
+                                Email = x.Email, 
+                                FailedPasswordAnswerWindowAttemptCount =
+                                    x.FailedPasswordAnswerWindowAttemptCount, 
+                                FailedPasswordAnswerWindowStartedAt = x.FailedPasswordAnswerWindowStartedAt, 
+                                FailedPasswordWindowAttemptCount = x.FailedPasswordWindowAttemptCount, 
+                                FailedPasswordWindowStartedAt = x.FailedPasswordWindowStartedAt, 
+                                IsConfirmed = x.IsConfirmed, 
+                                IsLockedOut = x.IsLockedOut, 
+                                IsOnline = x.IsOnline, 
+                                LastActivityAt = x.LastActivityAt, 
+                                LastLockedOutAt = x.LastLockedOutAt, 
+                                LastLoginAt = x.LastLoginAt, 
+                                LastPasswordChangeAt = x.LastPasswordChangeAt, 
+                                Password = x.Password, 
+                                PasswordSalt = x.PasswordSalt, 
+                                PasswordQuestion = x.PasswordQuestion, 
+                                PasswordAnswer = x.PasswordAnswer, 
+                                PasswordResetToken = x.PasswordResetToken, 
+                                PasswordResetExpirationDate = x.PasswordResetExpirationDate, 
+                                SimpleRoles = x.SimpleRoles
+                            })
+                    .FirstOrDefault();
         }
 
         #endregion
