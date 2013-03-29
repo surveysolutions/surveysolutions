d647291a6d3827c4bb5b8a8b290c65e8b0bd0c13
Slava Scherbak <slava.scherbak@gmail.com>
2013-03-29 17:47:33 +0200

    designer\edit: fixed bug with group type changing
    Former-commit-id: a04e2c892fc9d3ad5fcbb82be8d4d92f8769cf3c

--- src/UI/Designer/WB.UI.Designer/Scripts/details/model.group.js
+++ src/UI/Designer/WB.UI.Designer/Scripts/details/model.group.js
@@ -35,7 +35,7 @@
                 self.isSelected = ko.observable();
                 self.typeOptions = config.groupTypes;
                 self.isNullo = false;
-                self.dirtyFlag = new ko.DirtyFlag([self.title, self.type, self.description, self.condition]);
+                self.dirtyFlag = new ko.DirtyFlag([self.title, self.gtype, self.description, self.condition]);
                 self.dirtyFlag().reset();
                 
                 self.errors = ko.validation.group(self);
