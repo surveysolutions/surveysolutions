8f2dd21ed3da6526e4861bed30088c69d2810235
Anatoliy 'TLK' Kolesnick <none@none>
2013-03-29 19:38:59 +0200

    build\deploy: added creation of deployment package
    Former-commit-id: 99b55dd9337a0d29e23f8b49a233ed72d2ac7243

--- build/deployment-functions.ps1
+++ build/deployment-functions.ps1
@@ -3,6 +3,21 @@ $scriptFolder = (Get-Item $MyInvocation.MyCommand.Path).Directory.FullName
 . "$scriptFolder\functions.ps1"
 
 
+function CreateZipPackage($SourceFolder, $TargetFile) {
+    Write-Host "##teamcity[blockOpened name='Creating zip package']"
+    Write-Host "##teamcity[progressStart 'Creating zip package']"
+
+    Remove-Item $TargetFile
+
+    [Reflection.Assembly]::LoadWithPartialName("System.IO.Compression.FileSystem")
+    [System.AppDomain]::CurrentDomain.GetAssemblies()
+
+    [System.IO.Compression.ZipFile]::CreateFromDirectory((Join-Path (Get-Location).Path $SourceFolder), (Join-Path (Get-Location).Path $TargetFile), [System.IO.Compression.CompressionLevel]::Optimal, $false)
+
+    Write-Host "##teamcity[progressFinish 'Creating zip package']"
+    Write-Host "##teamcity[blockClosed name='Creating zip package']"
+}
+
 function Deploy($Solution, $Project, $BuildConfiguration, $SourceFolder, $TargetFolder) {
 
     CleanBinAndObjFolders
@@ -12,6 +27,8 @@ function Deploy($Solution, $Project, $BuildConfiguration, $SourceFolder, $Target
     RunTests $BuildConfiguration
 
     & (GetPathToMSBuild) $Project '/t:Package' "/p:Configuration=$BuildConfiguration" | Write-Host
+
+    CreateZipPackage $SourceFolder 'package.zip'
 
     Remove-Item "$TargetFolder\*" -Force -Recurse
 
