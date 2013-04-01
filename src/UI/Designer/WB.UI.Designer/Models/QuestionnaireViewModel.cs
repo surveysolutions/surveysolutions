dea0a04e2239d7d862c1ce3139c7bd4b8b7c5cdb
Roman Lytvyn <litvin.roma@gmail.com>
2013-04-01 14:30:06 -0400

    Designer: Fixed bug with bad questionnaire/user name when delete
    Former-commit-id: 1f507a4f622809f6bdc69f115588eb3cfbcd024b

--- src/UI/Designer/WB.UI.Designer/Models/QuestionnaireViewModel.cs
+++ src/UI/Designer/WB.UI.Designer/Models/QuestionnaireViewModel.cs
@@ -22,7 +22,6 @@ namespace WB.UI.Designer.Models
         ///     Gets or sets the title.
         /// </summary>
         [Required]
-        [RegularExpression("^[a-z0-9_]{3,50}$", ErrorMessage = "Questionnaire name needs to be between 3 and 50 characters and contains only letters, digits and symbol \"_\".")]
         [Display(Name = "Title")]
         public string Title { get; set; }
 
