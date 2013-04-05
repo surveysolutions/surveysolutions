b44647982569530f5be91c5f72f5abf0f5c690d0
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2013-04-05 18:33:12 -0400

    mocks are changed on stubs
    Former-commit-id: 7af6d6cecf6c25af5ef5b58e4c386f214c8dd933

--- src/Tests/WB.Core.Questionnaire.Tests/DefaultImportServiceTests.cs
+++ src/Tests/WB.Core.Questionnaire.Tests/DefaultImportServiceTests.cs
@@ -22,66 +22,90 @@ namespace WB.Core.Questionnaire.Tests
     [TestFixture]
     public class DefaultImportServiceTests
     {
-        protected Mock<IUnitOfWorkContext> unitOfWorkContextMock;
-        protected Mock<IEventStore> eventStoreMock;
-        protected Mock<IEventBus> eventBusMock;
-        protected Mock<IUnitOfWorkFactory> unitOfWorkFactoryMock;
-       
+      
+        protected EventStoreStub eventStoreStub;
+        protected EventBusStub eventBusStub;
+        
         [SetUp]
         public void Setup()
         {
-            unitOfWorkContextMock=new Mock<IUnitOfWorkContext>();
-            eventStoreMock=new Mock<IEventStore>();
-            eventBusMock = new Mock<IEventBus>();
-            unitOfWorkFactoryMock=new Mock<IUnitOfWorkFactory>();
-            unitOfWorkFactoryMock.Setup(x => x.CreateUnitOfWork(It.IsAny<Guid>())).Returns(unitOfWorkContextMock.Object);
-            NcqrsEnvironment.SetDefault(eventStoreMock.Object);
-            NcqrsEnvironment.SetDefault(eventBusMock.Object);
+            eventStoreStub = new EventStoreStub();
+            eventBusStub = new EventBusStub();
+            NcqrsEnvironment.SetDefault<IEventStore>(eventStoreStub);
+            NcqrsEnvironment.SetDefault<IEventBus>(eventBusStub);
         }
 
         [Test]
-        public void Execute_When_UserExistsSourceIsDocument_Then_NewQuestionnaireIsSaved()
+        public void Execute_When_UserExistsSourceIsDocument_Then_NewQuestionnaireIsSavedToEventStore()
         {
             // arrange
             DefaultImportService importService = CreateDefaultImportService();
             var questionnaireId = Guid.NewGuid();
             var creatorId = Guid.NewGuid();
             // act
+            string questionnaireTitle = "new questionnaire";
             importService.Execute(new ImportQuestionnaireCommand(creatorId,
                                                                  new QuestionnaireDocument()
                                                                      {
-                                                                         Title = "new questionnaire",
+                                                                         Title = questionnaireTitle,
                                                                          PublicKey = questionnaireId
                                                                      }));
             // assert
-            Func<QuestionnaireDocument, bool> validateEventFunk = (evt) =>
-                {
-                    if (questionnaireId != evt.PublicKey)
-                        return false;
-                    if (creatorId != evt.CreatedBy)
-                        return false;
-                    if ("new questionnaire" != evt.Title)
-                        return false;
-                    return true;
-                };
-            ValidateFirstEventStored(validateEventFunk);
-            ValidateFirstEventPublished(validateEventFunk);
+            var storedEvent =GetSingleEventFromStore<SnapshootLoaded>();
+            Assert.That(storedEvent.Template.Payload, Is.TypeOf<QuestionnaireDocument>());
+            var storedDocument = storedEvent.Template.Payload as QuestionnaireDocument;
+            Assert.That(storedDocument.CreatedBy, Is.EqualTo(creatorId));
+            Assert.That(storedDocument.PublicKey, Is.EqualTo(questionnaireId));
+            Assert.That(storedDocument.Title, Is.EqualTo(questionnaireTitle));
+
+            
         }
+
         [Test]
-        public void Execute_When_QuestionnairePresentWithSameId_Then_ArgumentException_should_be_thrown()
+        public void Execute_When_UserExistsSourceIsDocument_Then_StoredEventEqualToPublished()
         {
             // arrange
             DefaultImportService importService = CreateDefaultImportService();
             var questionnaireId = Guid.NewGuid();
-            this.unitOfWorkContextMock.Setup(x => x.GetById<QuestionnaireAR>(questionnaireId))
-                .Returns(new QuestionnaireAR());
+            var creatorId = Guid.NewGuid();
             // act
-            TestDelegate act =
-                () =>
-                importService.Execute(new ImportQuestionnaireCommand(Guid.NewGuid(),
-                                                                     new QuestionnaireDocument(){ PublicKey = questionnaireId}));
+            string questionnaireTitle = "new questionnaire";
+            importService.Execute(new ImportQuestionnaireCommand(creatorId,
+                                                                 new QuestionnaireDocument()
+                                                                 {
+                                                                     Title = questionnaireTitle,
+                                                                     PublicKey = questionnaireId
+                                                                 }));
             // assert
-            Assert.Throws<ArgumentException>(act);
+            var storedEvent = GetSingleEventFromStore<SnapshootLoaded>();
+            var publishedEvent = GetSingleEventFromBus<SnapshootLoaded>();
+            Assert.That(publishedEvent, Is.EqualTo(storedEvent));
+        }
+
+        [Test]
+        public void Execute_When_QuestionnairePresentWithSameId_Then_TemplateLoadedAsLastEventInStream()
+        {
+            // arrange
+
+            var ar = new QuestionnaireAR();
+            ar.InitializeFromHistory(new CommittedEventStream(ar.EventSourceId,
+                                                              new CommittedEvent(Guid.NewGuid(), Guid.NewGuid(),
+                                                                                 ar.EventSourceId, 1, DateTime.Now,
+                                                                                 new NewQuestionnaireCreated(),
+                                                                                 new Version())));
+
+            DefaultImportService importService = CreateDefaultImportServiceWhichIsReturnPassedAR(ar);
+            // act
+
+            importService.Execute(new ImportQuestionnaireCommand(Guid.NewGuid(),
+                                                                 new QuestionnaireDocument()
+                                                                     {
+                                                                         PublicKey = ar.EventSourceId
+                                                                     }));
+            // assert
+            var storedEvent = GetSingleEventFromStore<SnapshootLoaded>();
+            Assert.That(storedEvent.Template.Version, Is.EqualTo(2));
+            //Assert.That(storedEvent.);
         }
 
         [Test]
@@ -98,28 +122,30 @@ namespace WB.Core.Questionnaire.Tests
             Assert.Throws<ArgumentException>(act);
         }
 
+        private T GetSingleEventFromStore<T>()
+        {
+            return (T)eventStoreStub.Events.Single(e => e.Payload is T).Payload;
+        }
+
+        private T GetSingleEventFromBus<T>()
+        {
+            return (T)eventBusStub.Events.Single(e => e.Payload is T).Payload;
+        }
+
         private DefaultImportService CreateDefaultImportService()
         {
+            return new DefaultImportService();
+        }
+
+        private DefaultImportService CreateDefaultImportServiceWhichIsReturnPassedAR(QuestionnaireAR ar)
+        {
+            Mock<IUnitOfWorkContext> unitOfWorkContextMock = new Mock<IUnitOfWorkContext>();
+            Mock<IUnitOfWorkFactory> unitOfWorkFactoryMock = new Mock<IUnitOfWorkFactory>();
+            unitOfWorkFactoryMock.Setup(x => x.CreateUnitOfWork(It.IsAny<Guid>())).Returns(unitOfWorkContextMock.Object);
+            unitOfWorkContextMock.Setup(x => x.GetById<QuestionnaireAR>(ar.EventSourceId))
+               .Returns(ar);
             return new DefaultImportService(unitOfWorkFactoryMock.Object);
-        }
 
-        private void ValidateFirstEventStored<T>(Func<T, bool> validator) where T : class
-        {
-            eventStoreMock.Verify(x => x.Store(It.Is<UncommittedEventStream>(s => ValidateStream(s, validator))),
-                                  Times.Once());
-        }
-
-        private void ValidateFirstEventPublished<T>(Func<T, bool> validator) where T : class
-        {
-            eventBusMock.Verify(x => x.Publish(It.Is<UncommittedEvent>(e => ValudateEvent(e, validator))),
-                                  Times.Once());
-        }
-
-        private bool ValidateStream<T>(UncommittedEventStream stream, Func<T, bool> validator) where T : class
-        {
-            if (!stream.Any())
-                return false;
-            return ValudateEvent(stream.First(), validator);
         }
 
         private bool ValudateEvent<T>(UncommittedEvent evt, Func<T, bool> validator) where T : class
