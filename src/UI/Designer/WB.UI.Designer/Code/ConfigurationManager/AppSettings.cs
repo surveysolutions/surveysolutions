888de84be5cbd70d8666793ae22140bd6f7f4f87
Roman Lytvyn <litvin.roma@gmail.com>
2013-04-10 15:38:36 +0300

    Dsigner: enable/disable for recaptcha.
    Former-commit-id: aa7100351cdeb5b524c03cd7ed8ef2e91cf6e5a2

---
+++ src/UI/Designer/WB.UI.Designer/Code/ConfigurationManager/AppSettings.cs
@@ -0,0 +1,17 @@
+﻿
+namespace WB.UI.Designer
+{
+    public sealed class AppSettings: WebConfigHelper
+    {
+        public static readonly AppSettings Instance = new AppSettings();
+
+        const string ISRECATPCHAENABLED = "IsReCaptchaEnabled";
+
+        public bool IsReCaptchaEnabled { get; private set; }
+
+        private AppSettings()
+        {
+            IsReCaptchaEnabled = GetBoolean(ISRECATPCHAENABLED, true);
+        }
+    }
+}
---
+++ src/UI/Designer/WB.UI.Designer/Code/ObjectExtensions.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace WB.UI.Designer
+{
+    public static class ObjectExtensions
+    {
+        public static bool IsNull(this object obj)
+        {
+            return obj == null;
+        }
+        public static bool IsNotNull(this object obj)
+        {
+            return !obj.IsNull();
+        }
+
+        public static int ToInt(this string value)
+        {
+            return value.ToInt(default(int));
+        }
+        public static byte ToByte(this string value)
+        {
+            return value.ToByte(default(byte));
+        }
+        public static bool ToBoolean(this string value)
+        {
+            return value.ToBoolean(false);
+        }
+        public static decimal ToDecimal(this string value)
+        {
+            return value.ToDecimal(decimal.Zero);
+        }
+
+        public static int ToInt(this string value, int @default)
+        {
+            int result;
+            if (int.TryParse(value, out result))
+            {
+                return result;
+            }
+            return @default;
+        }
+        public static byte ToByte(this string value, byte @default)
+        {
+            byte result;
+            if (byte.TryParse(value, out result))
+            {
+                return result;
+            }
+            return @default;
+        }
+        public static bool ToBoolean(this string value, bool @default)
+        {
+            bool result;
+            if (value.HasValue() && bool.TryParse(value, out result))
+            {
+                return result;
+            }
+            return @default;
+        }
+        public static decimal ToDecimal(this string value, decimal @default)
+        {
+            decimal result;
+            if (value.HasValue() && decimal.TryParse(value, out result))
+            {
+                return result;
+            }
+            return @default;
+        }
+
+        public static DateTime ToDateTime(this string value, DateTime @default)
+        {
+            DateTime result;
+            if (value.HasValue() && DateTime.TryParse(value, out result))
+            {
+                return result;
+            }
+            return @default;
+        }
+
+        public static bool HasValue(this string value)
+        {
+            return !string.IsNullOrEmpty(value);
+        }
+
+    }
+}
