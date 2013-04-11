361d57ecde11e3b672853ff901a7108e777025a8
Roman Litvin <litvin.roma@gmail.com>
2013-04-11 17:51:44 +0300

    Designer: Replaced direct app config parameter
    --HG--
    branch : next
    
    
    Former-commit-id: 1ed1768d9f5eabf4ef94c06332dfa6403d25ae3d

--- src/UI/Designer/WB.UI.Designer/Code/ConfigurationManager/AppSettings.cs
+++ src/UI/Designer/WB.UI.Designer/Code/ConfigurationManager/AppSettings.cs
@@ -6,12 +6,15 @@ namespace WB.UI.Designer
         public static readonly AppSettings Instance = new AppSettings();
 
         const string ISRECATPCHAENABLED = "IsReCaptchaEnabled";
+        const string RAVENDOCUMENTSTORE = "Raven.DocumentStore";
 
         public bool IsReCaptchaEnabled { get; private set; }
+        public string RavenDocumentStore { get; private set; }
 
         private AppSettings()
         {
             IsReCaptchaEnabled = GetBoolean(ISRECATPCHAENABLED, true);
+            RavenDocumentStore = GetString(RAVENDOCUMENTSTORE);
         }
     }
 }
