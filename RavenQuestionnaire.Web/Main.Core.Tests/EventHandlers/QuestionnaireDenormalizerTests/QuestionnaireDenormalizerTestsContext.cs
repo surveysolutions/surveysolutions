244aeacbb8672831d6e32584ca7ac557474b3892
Anatoliy 'TLK' Kolesnick <none@none>
2013-04-11 18:27:32 -0400

    QuestionnaireDenormalizer: added test on removing one of 3 groups which have the same id
    --HG--
    branch : questionnaire-uniqueness-checks
    
    
    Former-commit-id: d14a92cb3e5489cf2fabacd6f0f9ccc5990a7a0e

---
+++ RavenQuestionnaire.Web/Main.Core.Tests/EventHandlers/QuestionnaireDenormalizerTests/QuestionnaireDenormalizerTestsContext.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Main.Core.Tests.Domain.QuestionnaireDenormalizerTests
+{
+    using Machine.Specifications;
+
+    using Main.Core.AbstractFactories;
+    using Main.Core.Documents;
+    using Main.Core.Entities.SubEntities;
+    using Main.Core.EventHandlers;
+    using Main.Core.Events.Questionnaire;
+    using Main.DenormalizerStorage;
+
+    using Moq;
+
+    using Ncqrs.Eventing;
+    using Ncqrs.Eventing.ServiceModel.Bus;
+
+    [Subject(typeof(QuestionnaireDenormalizer))]
+    internal class QuestionnaireDenormalizerTestsContext
+    {
+        protected static IPublishedEvent<T> ToPublishedEvent<T>(T @event)
+            where T : class
+        {
+            return Mock.Of<IPublishedEvent<T>>(publishedEvent
+                => publishedEvent.Payload == @event);
+        }
+
+        protected static QuestionnaireDenormalizer CreateQuestionnaireDenormalizer(
+            IDenormalizerStorage<QuestionnaireDocument> documentStorage = null, ICompleteQuestionFactory questionFactory = null)
+        {
+            return new QuestionnaireDenormalizer(
+                documentStorage ?? Mock.Of<IDenormalizerStorage<QuestionnaireDocument>>(),
+                questionFactory ?? Mock.Of<ICompleteQuestionFactory>());
+        }
+
+        protected static QuestionnaireDocument CreateQuestionnaireDocument()
+        {
+            return new QuestionnaireDocument();
+        }
+
+        protected static Group CreateGroup(Guid? groupId = null, string title = "Group X")
+        {
+            return new Group
+            {
+                PublicKey = groupId ?? Guid.NewGuid(), 
+                Title = title,
+            };
+        }
+
+        protected static IPublishedEvent<GroupDeleted> CreateGroupDeletedEvent(Guid groupId)
+        {
+            return ToPublishedEvent(new GroupDeleted
+            {
+                GroupPublicKey = groupId,
+            });
+        }
+    }
+}
---
+++ RavenQuestionnaire.Web/Main.Core.Tests/EventHandlers/QuestionnaireDenormalizerTests/when_deleting_group_by_id_and_there_are_3_groups_with_same_id.cs
@@ -0,0 +1,69 @@
+﻿namespace Main.Core.Tests.Domain.QuestionnaireDenormalizerTests
+{
+    using System;
+
+    using Machine.Specifications;
+
+    using Main.Core.Documents;
+    using Main.Core.Entities.SubEntities;
+    using Main.Core.EventHandlers;
+    using Main.Core.Events.Questionnaire;
+    using Main.DenormalizerStorage;
+
+    using Moq;
+
+    using Ncqrs.Eventing.ServiceModel.Bus;
+
+    using It = Machine.Specifications.It;
+    using it = Moq.It;
+
+    internal class when_deleting_group_by_id_and_there_are_3_groups_with_same_id : QuestionnaireDenormalizerTestsContext
+    {
+        Establish context = () =>
+        {
+            groupId = Guid.Parse("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+
+            firstGroupTitle = "Group 1";
+            secondGroupTitle = "Group 2";
+            thirdGroupTitle = "Group 3";
+
+            groupDeletedEvent = CreateGroupDeletedEvent(groupId);
+
+            questionnaire = CreateQuestionnaireDocument();
+            questionnaire.Children.AddRange(new []
+            {
+                CreateGroup(groupId: groupId, title: firstGroupTitle),
+                CreateGroup(groupId: groupId, title: secondGroupTitle),
+                CreateGroup(groupId: groupId, title: thirdGroupTitle),
+            });
+
+            var documentStorage = Mock.Of<IDenormalizerStorage<QuestionnaireDocument>>(storage
+                => storage.GetByGuid(it.IsAny<Guid>()) == questionnaire);
+
+            denormalizer = CreateQuestionnaireDenormalizer(documentStorage: documentStorage);
+        };
+
+        Because of = () =>
+            denormalizer.Handle(groupDeletedEvent);
+
+        It should_be_only_2_groups = () =>
+            questionnaire.Children.Count.ShouldEqual(2);
+
+        It should_remove_first_group = () =>
+            questionnaire.Children.ShouldNotContain(group => ((IGroup)group).Title == firstGroupTitle);
+
+        It should_not_remove_second_group = () =>
+            questionnaire.Children.ShouldContain(group => ((IGroup)group).Title == secondGroupTitle);
+
+        It should_not_remove_third_group = () =>
+            questionnaire.Children.ShouldContain(group => ((IGroup)group).Title == thirdGroupTitle);
+
+        private static QuestionnaireDocument questionnaire;
+        private static string firstGroupTitle;
+        private static string secondGroupTitle;
+        private static string thirdGroupTitle;
+        private static QuestionnaireDenormalizer denormalizer;
+        private static Guid groupId;
+        private static IPublishedEvent<GroupDeleted> groupDeletedEvent;
+    }
+}
