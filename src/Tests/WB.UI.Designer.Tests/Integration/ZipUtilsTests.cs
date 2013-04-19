2528f43df321efa346180d9ce0f58b4366e04c9d
Anastasya Kholodova <nastya.kholodova@gmail.com>
2013-04-19 17:46:56 -0400

    test for encoding is added
    Former-commit-id: b7c4b8f7c2e026abac8892e78481350c5b86da87

---
+++ src/Tests/WB.UI.Designer.Tests/Integration/ZipUtilsTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ionic.Zip;
+using NUnit.Framework;
+using WB.UI.Designer.Utils;
+
+namespace WB.UI.Designer.Tests.Integration
+{
+    [TestFixture]
+    public class ZipUtilsTests
+    {
+        [Test]
+        public void ZipDate_When_DataIsNotEmptyString_Then_FileIsCreatedWithUtf8Encodig()
+        {
+            // arrange
+            ZipUtils target = CreateZipUtils();
+
+            // act
+            string helloworld = "helloworld";
+
+            var file = target.ZipDate(helloworld);
+
+            // assert
+
+            using (ZipFile zip = ZipFile.Read(new MemoryStream(file)))
+            {
+                foreach (ZipEntry e in zip)
+                {
+                    Assert.That(e.AlternateEncoding, Is.EqualTo(Encoding.UTF8));
+                }
+            }
+
+        }
+
+        private ZipUtils CreateZipUtils()
+        {
+            return new ZipUtils();
+        }
+    }
+}
