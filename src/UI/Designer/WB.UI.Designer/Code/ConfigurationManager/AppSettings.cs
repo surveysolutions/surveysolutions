5d0592c54a59d23800f5d86d8ad5691c894b54f9
Slava Scherbak <slava.scherbak@gmail.com>
2013-04-23 11:34:21 +0300

    Designer: revert to b4ece891b5cf commit
    Former-commit-id: e763882860575a921478565e330d61745605d622

--- src/UI/Designer/WB.UI.Designer/Code/ConfigurationManager/AppSettings.cs
+++ src/UI/Designer/WB.UI.Designer/Code/ConfigurationManager/AppSettings.cs
@@ -1,4 +1,5 @@
-﻿namespace WB.UI.Designer
+﻿
+namespace WB.UI.Designer
 {
     public sealed class AppSettings: WebConfigHelper
     {
@@ -8,15 +9,11 @@
         const string RAVENDOCUMENTSTORE = "Raven.DocumentStore";
         const string WKHTMLTOPDFEXECUTABLEPATH = "WKHtmlToPdfExecutablePath";
         const string ISLOCKINGACCOUNTPOLICYFORCED = "IsLockingAccountPolicyForced";
-        const string ISANTIFORGERYENABLED = "IsAntiForgeryEnabled";
-
 
         public bool IsReCaptchaEnabled { get; private set; }
         public string RavenDocumentStore { get; private set; }
         public string WKHtmlToPdfExecutablePath { get; private set; }
         public bool IsLockingAccountPolicyForced { get; private set; }
-        public bool IsAntiForgeryEnabled { get; private set; }
-        
 
         private AppSettings()
         {
@@ -24,7 +21,6 @@
             RavenDocumentStore = GetString(RAVENDOCUMENTSTORE);
             WKHtmlToPdfExecutablePath = GetString(WKHTMLTOPDFEXECUTABLEPATH);
             IsLockingAccountPolicyForced = GetBoolean(ISLOCKINGACCOUNTPOLICYFORCED, true);
-            IsAntiForgeryEnabled = this.GetBoolean(ISANTIFORGERYENABLED, true);
         }
     }
 }
