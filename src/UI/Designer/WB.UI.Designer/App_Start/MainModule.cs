662546469696cbe267b3aa96f964d5ac53d04ce3
Roman Lytvyn <litvin.roma@gmail.com>
2013-04-24 15:04:14 -0400

    Designer: removed some shit. edit/delete for my questionnaires in public questionnaires
    Former-commit-id: 29b5169facb9e38f343f25d327dbed5a95765b4e

--- src/UI/Designer/WB.UI.Designer/App_Start/MainModule.cs
+++ src/UI/Designer/WB.UI.Designer/App_Start/MainModule.cs
@@ -2,6 +2,8 @@
 {
     using Ncqrs.Commanding.ServiceModel;
     using Ninject.Modules;
+    using Ninject.Web.Mvc.FilterBindingSyntax;
+
 
     /// <summary>
     /// The main module.
@@ -10,6 +12,7 @@
     {
         public override void Load()
         {
+            this.BindFilter<CustomAuthorizeFilter>(System.Web.Mvc.FilterScope.Controller, 0).WhenControllerHas<CustomAuthorizeAttribute>().InSingletonScope();
             this.Bind<ICommandService>().ToConstant(Ncqrs.NcqrsEnvironment.Get<ICommandService>());
         }
     }
