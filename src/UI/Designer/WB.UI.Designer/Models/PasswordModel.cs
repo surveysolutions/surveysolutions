a6f110edb33bfead77dcbbac28fc18e82446baf0
Roman Lytvyn <none@none>
2013-05-02 19:58:55 -0400

    Designer: password policy for all password fields for account controller
    --HG--
    branch : fix-security-issues
    
    
    Former-commit-id: c822a4923b318448f639a0d05bc500a788d99f10

---
+++ src/UI/Designer/WB.UI.Designer/Models/PasswordModel.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="PasswordModel.cs" company="">
+//   
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace WB.UI.Designer.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    /// <summary>
+    /// The password model.
+    /// </summary>
+    public class PasswordModel
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// Gets or sets the confirm password.
+        /// </summary>
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password", Order = 3)]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        /// <summary>
+        /// Gets or sets the new password.
+        /// </summary>
+        [Required]
+        [CustomStringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password", Order = 2)]
+        public string Password { get; set; }
+
+        #endregion
+    }
+}
