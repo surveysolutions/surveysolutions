ee59e797f1249813f5d49399d2a6ef1b67ea02ff
Roman Lytvyn <litvin.roma@gmail.com>
2013-05-02 19:59:30 -0400

    Designer: password policy for all password fields for account controller
    --HG--
    branch : fix-security-issues
    
    
    Former-commit-id: d3a4db4f51fd2866a35660991798b4ff3b4bb356

---
+++ src/UI/Designer/WB.UI.Designer/Code/CustomStringLengthAttribute.cs
@@ -0,0 +1,66 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="CustomStringLengthAttribute.cs" company="">
+//   
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace WB.UI.Designer
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using Microsoft.Practices.ServiceLocation;
+
+    using WB.UI.Designer.Providers.Membership;
+
+    /// <summary>
+    ///     The custom string length attribute.
+    /// </summary>
+    public class CustomStringLengthAttribute : StringLengthAttribute
+    {
+        #region Constructors and Destructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CustomStringLengthAttribute"/> class.
+        /// </summary>
+        /// <param name="maximumLength">
+        /// The maximum length.
+        /// </param>
+        public CustomStringLengthAttribute(int maximumLength)
+            : base(maximumLength)
+        {
+            base.MinimumLength = this.MinimumLength;
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        /// <summary>
+        /// Gets the minimum length.
+        /// </summary>
+        public new int MinimumLength
+        {
+            get
+            {
+                return this.passwordPolicy.PasswordMinimumLength;
+            }
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets the password policy.
+        /// </summary>
+        private IPasswordPolicy passwordPolicy
+        {
+            get
+            {
+                return ServiceLocator.Current.GetInstance<IPasswordPolicy>();
+            }
+        }
+
+        #endregion
+    }
+}
