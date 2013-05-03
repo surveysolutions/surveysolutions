8bcd74cd45ed70300aef00110960a64baa3cf016
Anatoliy 'TLK' Kolesnick <none@none>
2013-05-04 02:17:57 +0300

    build\deploy\Supervisor: corrected package folder according to new build configuration
    --HG--
    branch : djibouti
    
    
    Former-commit-id: 7242e748c7c2984fb778e1cbcad6e53dfaf283ac

--- build/deploy-supervisor-dev.ps1
+++ build/deploy-supervisor-dev.ps1
@@ -8,5 +8,5 @@ Deploy `
     -Solution 'RavenQuestionnaire.Web\RavenQuestionnaire.Web.sln' `
     -Project 'RavenQuestionnaire.Web\Web.Supervisor\Web.Supervisor.csproj' `
     -BuildConfiguration 'Release' `
-    -SourceFolder 'RavenQuestionnaire.Web\Web.Supervisor\obj\Deploy-dev\Package\PackageTmp' `
+    -SourceFolder 'RavenQuestionnaire.Web\Web.Supervisor\obj\Release\Package\PackageTmp' `
     -TargetFolder '\\192.168.3.113\Dev\Supervisor' `
