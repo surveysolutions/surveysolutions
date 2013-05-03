d83eb27643f83e5ac8166e44b55075508df0c5dd
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2013-05-03 20:37:15 +0300

    Designer\RequiresReadLayerFilterProvider: refactored to simplify extraction to Shared.Web assembly
    --HG--
    branch : djibouti
    
    
    Former-commit-id: c669e3eb646a111735334f9ca8a83867ac086169

---
+++ src/UI/Designer/WB.UI.Designer/Filters/RequiresReadLayerFilterProvider.cs
@@ -0,0 +1,49 @@
+namespace WB.UI.Designer.Filters
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web.Mvc;
+
+    using Microsoft.Practices.ServiceLocation;
+
+    using WB.UI.Designer.Controllers;
+
+    public class RequiresReadLayerFilterProvider : IFilterProvider
+    {
+        private readonly IServiceLocator serviceLocator;
+
+        public RequiresReadLayerFilterProvider(IServiceLocator serviceLocator)
+        {
+            this.serviceLocator = serviceLocator;
+        }
+
+        private static Type[] ControllersWhichWorkWithoutReadLayer
+        {
+            get
+            {
+                return new[]
+                {
+                    typeof(ErrorController),
+                    typeof(MaintenanceController),
+                };
+            }
+        }
+
+        public IEnumerable<Filter> GetFilters(ControllerContext controllerContext, ActionDescriptor actionDescriptor)
+        {
+            if (IsReadLayerRequiredByController(actionDescriptor.ControllerDescriptor))
+            {
+                yield return new Filter(this.serviceLocator.GetInstance<RequiresReadLayerFilter>(), FilterScope.Controller, order: null);
+            }
+        }
+
+        private static bool IsReadLayerRequiredByController(ControllerDescriptor controllerDescriptor)
+        {
+            var isControllerWhichWorksWithoutReadLayer =
+                ControllersWhichWorkWithoutReadLayer.Contains(controllerDescriptor.ControllerType);
+
+            return !isControllerWhichWorksWithoutReadLayer;
+        }
+    }
+}
