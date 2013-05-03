c60e3ab03ea9bdd331a8c336580f1e0519cd8f0f
Anatoliy 'TLK' Kolesnick <none@none>
2013-05-04 01:52:55 +0300

    build\deploy\Supervisor: switched build configuration to Release
    --HG--
    branch : djibouti
    
    
    Former-commit-id: d998e5e6589ae7bacbc5ad5be3534dc177d2e2f6

--- build/deploy-supervisor-dev.ps1
+++ build/deploy-supervisor-dev.ps1
@@ -7,6 +7,6 @@ $ErrorActionPreference = "Stop"
 Deploy `
     -Solution 'RavenQuestionnaire.Web\RavenQuestionnaire.Web.sln' `
     -Project 'RavenQuestionnaire.Web\Web.Supervisor\Web.Supervisor.csproj' `
-    -BuildConfiguration 'Deploy-dev' `
+    -BuildConfiguration 'Release' `
     -SourceFolder 'RavenQuestionnaire.Web\Web.Supervisor\obj\Deploy-dev\Package\PackageTmp' `
     -TargetFolder '\\192.168.3.113\Dev\Supervisor' `
