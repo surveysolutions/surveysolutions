41d5892837034ad4ca61dc6ac0a31c4bbaa599a6
Slava Scherbak <slava.scherbak@gmail.com>
2013-05-03 12:01:16 +0300

    Supervisor\HQ\Dashboard: added list of questionnaires
    Former-commit-id: c67fc7c68c6fba4c43e6d22d31ad5b1d7b8fd691

--- RavenQuestionnaire.Web/Web.Supervisor/Controllers/DashboardController.cs
+++ RavenQuestionnaire.Web/Web.Supervisor/Controllers/DashboardController.cs
@@ -36,7 +36,8 @@ namespace Web.Supervisor.Controllers
 
         public ActionResult Index()
         {
-            return this.View();
+            var model = this.Repository.Load<QuestionnaireBrowseInputModel, QuestionnaireBrowseView>(new QuestionnaireBrowseInputModel());
+            return this.View(model);
         }
       
         public ActionResult Questionnaires(QuestionnaireBrowseInputModel input)
