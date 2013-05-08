84cbbe7b6e360049fcbf8ff275599ca1dc089d76
Anatoliy 'TLK' Kolesnick <none@none>
2013-05-08 14:04:25 +0300

    Core: removed autogenerated xml comments
    --HG--
    branch : queryable-denormalizer-storage
    
    
    Former-commit-id: 6282a62d512c396f6712bedc3a4ce938d58de208

--- RavenQuestionnaire.Web/Main.DenormalizerStorage/IDenormalizerStorage.cs
+++ RavenQuestionnaire.Web/Main.DenormalizerStorage/IDenormalizerStorage.cs
@@ -1,12 +1,3 @@
-// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="IDenormalizerStorage.cs" company="The World Bank">
-//   2012
-// </copyright>
-// <summary>
-//   The DenormalizerStorage interface.
-// </summary>
-// --------------------------------------------------------------------------------------------------------------------
-
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
@@ -15,55 +6,15 @@ using System.Linq.Expressions;
 
 namespace Main.DenormalizerStorage
 {
-    /// <summary>
-    /// The DenormalizerStorage interface.
-    /// </summary>
-    /// <typeparam name="T">
-    /// </typeparam>
     public interface IDenormalizerStorage<T>
         where T : class
     {
-        #region Public Methods and Operators
-
-        /// <summary>
-        /// The count.
-        /// </summary>
-        /// <returns>
-        /// The System.Int32.
-        /// </returns>
         int Count();
 
-        /// <summary>
-        /// The get by guid.
-        /// </summary>
-        /// <param name="key">
-        /// The key.
-        /// </param>
-        /// <returns>
-        /// The T.
-        /// </returns>
         T GetByGuid(Guid key);
 
-        /// <summary>
-        /// The remove.
-        /// </summary>
-        /// <param name="key">
-        /// The key.
-        /// </param>
         void Remove(Guid key);
 
-        /// <summary>
-        /// The store.
-        /// </summary>
-        /// <param name="denormalizer">
-        /// The denormalizer.
-        /// </param>
-        /// <param name="key">
-        /// The key.
-        /// </param>
         void Store(T denormalizer, Guid key);
-
-        #endregion
     }
-
 }
