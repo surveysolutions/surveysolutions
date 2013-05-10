22a49226abf29435bd1edfdc64cd7351206fcfed
Roman Lytvyn <litvin.roma@gmail.com>
2013-05-10 16:16:56 -0400

    Supervisor/Designer: extracted logging into separated project and ninject it
    Former-commit-id: ae95e33a14e84d3f78985957bb9d37de52276477

---
+++ src/UI/Shared/WB.UI.Shared.NLog/LogManager.cs
@@ -0,0 +1,32 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="LogManager.cs" company="">
+//   
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+namespace WB.UI.Shared.NLog
+{
+    using Microsoft.Practices.ServiceLocation;
+
+    using WB.UI.Shared.Log;
+
+    /// <summary>
+    ///     The log manager.
+    /// </summary>
+    public class LogManager
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// Gets the logger.
+        /// </summary>
+        public static ILog Logger
+        {
+            get
+            {
+                return ServiceLocator.Current.GetInstance<ILog>();
+            }
+        }
+
+        #endregion
+    }
+}
