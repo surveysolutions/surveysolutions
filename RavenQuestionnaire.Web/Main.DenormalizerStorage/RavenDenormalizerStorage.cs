ceaa5df61008fbe250715f8ab5e558a6928868fe
Anatoliy 'TLK' Kolesnick <none@none>
2013-05-13 12:35:48 +0300

    Core: added draft of RavenDenormalizerStorage
    --HG--
    branch : supervisor-read-layer
    
    
    Former-commit-id: 9bd02645ca1f5a4359ad37febba29be1ca8d8737

---
+++ RavenQuestionnaire.Web/Main.DenormalizerStorage/RavenDenormalizerStorage.cs
@@ -0,0 +1,53 @@
+﻿namespace Main.DenormalizerStorage
+{
+    using System;
+    using System.Linq;
+
+    using Raven.Client;
+    using Raven.Client.Document;
+
+    public class RavenDenormalizerStorage<TView> : IQueryableDenormalizerStorage<TView>
+        where TView : class
+    {
+        private const int Timeout = 120;
+
+        private readonly DocumentStore ravenStore;
+
+        public RavenDenormalizerStorage(DocumentStore ravenStore)
+        {
+            this.ravenStore = ravenStore;
+        }
+
+        public int Count()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TView GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Store(TView view, Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<TView> Query()
+        {
+            #warning TLK: this is incorrect because session is already closed when we return IQueryable
+
+            using (IDocumentSession session = this.ravenStore.OpenSession())
+            {
+                return session
+                    .Query<TView>()
+                    .Customize(customization => customization.WaitForNonStaleResults(TimeSpan.FromSeconds(Timeout)));
+            }
+        }
+    }
+}
