fe78816ddfcbf81df550c2a31457933a1cbadf87
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2013-05-23 10:13:32 -0400

    login and password request is fixed
    Former-commit-id: c231f895e860a002ccf88defccda2044e4447d7b

--- Synchronization/SynchronizationMessages/Synchronization/PackageHelper.cs
+++ Synchronization/SynchronizationMessages/Synchronization/PackageHelper.cs
@@ -1,4 +1,6 @@
-﻿namespace SynchronizationMessages.Synchronization
+﻿using System.Collections.Generic;
+
+namespace SynchronizationMessages.Synchronization
 {
     using System;
     using System.IO;
@@ -7,43 +9,38 @@
 
     public static class PackageHelper
     {
-        public static string Compress(string text)
+        public static byte[] Compress(string text)
         {
             byte[] buffer = Encoding.UTF8.GetBytes(text);
-            MemoryStream ms = new MemoryStream();
-            using (GZipStream zip = new GZipStream(ms, CompressionMode.Compress, true))
+            using (var ms = new MemoryStream())
             {
-                zip.Write(buffer, 0, buffer.Length);
-            }
-
-            ms.Position = 0;
-            byte[] compressed = new byte[ms.Length];
-            ms.Read(compressed, 0, compressed.Length);
-
-            byte[] gzBuffer = new byte[compressed.Length + 4];
-            System.Buffer.BlockCopy(compressed, 0, gzBuffer, 4, compressed.Length);
-            System.Buffer.BlockCopy(BitConverter.GetBytes(buffer.Length), 0, gzBuffer, 0, 4);
-            return Convert.ToBase64String(gzBuffer);
-        }
-
-        public static string Decompress(string compressedText)
-        {
-            byte[] gzBuffer = Convert.FromBase64String(compressedText);
-            using (MemoryStream ms = new MemoryStream())
-            {
-                int msgLength = BitConverter.ToInt32(gzBuffer, 0);
-                ms.Write(gzBuffer, 4, gzBuffer.Length - 4);
-
-                byte[] buffer = new byte[msgLength];
-
-                ms.Position = 0;
-                using (GZipStream zip = new GZipStream(ms, CompressionMode.Decompress))
+                using (GZipStream zip = new GZipStream(ms, CompressionMode.Compress, true))
                 {
-                    zip.Read(buffer, 0, buffer.Length);
+                    zip.Write(buffer, 0, buffer.Length);
                 }
 
-                return Encoding.UTF8.GetString(buffer);
+                ms.Position = 0;
+
+                byte[] compressed = new byte[ms.Length];
+                ms.Read(compressed, 0, compressed.Length);
+                return compressed;
             }
+        }
+
+        public static string Decompress(Stream stream)
+        {
+            stream.Position = 0;
+            var result = new List<byte>();
+            using (GZipStream zip = new GZipStream(stream, CompressionMode.Decompress))
+            {
+                byte[] buffer = new byte[1024];
+                while (zip.Read(buffer, 0, buffer.Length) > 0)
+                {
+                    result.AddRange(buffer);
+                    buffer = new byte[1024];
+                }
+            }
+            return Encoding.UTF8.GetString(result.ToArray());
         }
     }
 }
