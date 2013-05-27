d1551b3549bf2d8ed6d9d597ec89a5e23d15f7b3
Anatoliy 'TLK' Kolesnick <none@none>
2013-05-28 00:18:43 +0300

    Core\RavenDenormalizerStorage: added tests for situation when read layer views are being rebuilt
    --HG--
    branch : supervisor-read-layer
    
    
    Former-commit-id: a36322d42e9fe9b6e7052da32bf49ef46826fefd

---
+++ RavenQuestionnaire.Web/Main.DenormalizerStorage.Tests/RavenDenormalizerStorageTests/RavenDenormalizerStorageTestsContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+using Raven.Client.Document;
+
+using WB.Core.Infrastructure;
+
+namespace Main.DenormalizerStorage.Tests.RavenDenormalizerStorageTests
+{
+    internal class RavenDenormalizerStorageTestsContext
+    {
+        internal static RavenDenormalizerStorage<TView> CreateRavenDenormalizerStorage<TView>(
+            DocumentStore ravenStore = null, IReadLayerStatusService readLayerStatusService = null)
+            where TView : class
+        {
+            return new RavenDenormalizerStorage<TView>(
+                ravenStore ?? new DocumentStore());
+        }
+    }
+}
