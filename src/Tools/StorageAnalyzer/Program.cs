5593d9aeb8088ad2250862b0f9d733eab9b8f97e
Slava Scherbak <slava.scherbak@gmail.com>
2013-05-28 17:06:48 +0300

    Tools: composed Tools solution with core projects
    --HG--
    branch : next
    
    
    Former-commit-id: 44c317e95865c3d65b540143c879121f5baa76e6

---
+++ src/Tools/StorageAnalyzer/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Ncqrs.Eventing.Storage.RavenDB;
+using Raven.Client;
+using Raven.Client.Document;
+
+namespace StorageAnalyzer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            var store = (new DocumentStore { Url = "http://localhost:8080" }).Initialize();
+            int pageSize = 1024;
+            int timeout = 120;
+            IQueryable<StoredEvent> result = Enumerable.Empty<StoredEvent>().AsQueryable();
+            int page = 0;
+            while (true)
+            {
+                using (IDocumentSession session = store.OpenSession())
+                {
+                    List<StoredEvent> chunk = session
+                        .Query<StoredEvent>()
+                        .Customize(x => x.WaitForNonStaleResults(TimeSpan.FromSeconds(timeout)))
+                        .OrderBy(e => e.EventSequence)
+                        .Skip(page * pageSize)
+                        .Take(pageSize)
+                        .ToList();
+
+                    if (chunk.Count == 0)
+                    {
+                        break;
+                    }
+
+                    result = result.Concat(chunk);
+                    page++;
+                }
+            }
+            */
+        }
+    }
+}
