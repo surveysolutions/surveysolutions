939cf3a6fed8ebf002d12edb599d96e4c1af4626
Anatoliy 'TLK' Kolesnick <none@none>
2013-05-28 21:02:15 +0300

    IReadLayerAdministrationService: added infrastractural implementation (locks, async, flags, etc) without core code itself
    --HG--
    branch : supervisor-read-layer
    
    
    Former-commit-id: 718f834e6e4dba1c08665fca9bb306213ff1ab4a

--- src/Core/Infrastructure/WB.Core.Infrastructure/Implementation/ReadLayerService.cs
+++ src/Core/Infrastructure/WB.Core.Infrastructure/Implementation/ReadLayerService.cs
@@ -1,10 +1,65 @@
-﻿namespace WB.Core.Infrastructure.Implementation
+﻿using System;
+using System.Threading.Tasks;
+
+namespace WB.Core.Infrastructure.Implementation
 {
-    internal class ReadLayerService : IReadLayerStatusService
+    internal class ReadLayerService : IReadLayerStatusService, IReadLayerAdministrationService
     {
+        private static readonly object LockObject = new object();
+
+        private static string statusMessage = "No administration operations were performed so far.";
+        private static bool areViewsBeingRebuiltNow = false;
+
+        #region IReadLayerStatusService implementation
+
         public bool AreViewsBeingRebuiltNow()
         {
-            return false;
+            return areViewsBeingRebuiltNow;
+        }
+
+        #endregion // IReadLayerStatusService implementation
+
+        #region IReadLayerAdministrationService implementation
+
+        public string GetReadableStatus()
+        {
+            return string.Format("{0}{1}Are views being rebuilt now: {2}",
+                statusMessage, Environment.NewLine, areViewsBeingRebuiltNow ? "Yes" : "No");
+        }
+
+        public void RebuildAllViewsAsync()
+        {
+            new Task(this.RebuildAllViews).Start();
+        }
+
+        #endregion // IReadLayerAdministrationService implementation
+
+        private void RebuildAllViews()
+        {
+            if (!areViewsBeingRebuiltNow)
+            {
+                lock (LockObject)
+                {
+                    if (!areViewsBeingRebuiltNow)
+                    {
+                        this.RebuildAllViewsImpl();
+                    }
+                }
+            }
+        }
+
+        private void RebuildAllViewsImpl()
+        {
+            try
+            {
+                areViewsBeingRebuiltNow = true;
+
+                throw new NotImplementedException();
+            }
+            finally
+            {
+                areViewsBeingRebuiltNow = false;
+            }
         }
     }
 }
