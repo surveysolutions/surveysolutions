e56625d81d05bcb875176ccae1f2ae3b2f6e31df
Roman Lytvyn <litvin.roma@gmail.com>
2013-06-03 12:48:11 -0400

    designer: changed email transmitter
    --HG--
    branch : fix-security-issues
    
    
    Former-commit-id: e4eefd03bb918523bd4c4e78ac8b89b909c1de71

---
+++ src/UI/Designer/WB.UI.Designer/Models/EmailConfirmationModel.cs
@@ -0,0 +1,33 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="EmailConfirmationModel.cs" company="">
+//   
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace WB.UI.Designer.Models
+{
+    /// <summary>
+    /// The email confirmation model.
+    /// </summary>
+    public class EmailConfirmationModel
+    {
+        #region Public Properties
+
+        /// <summary>
+        ///     Gets or sets the confirmation token.
+        /// </summary>
+        public string ConfirmationToken { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the email.
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the user name.
+        /// </summary>
+        public string UserName { get; set; }
+
+        #endregion
+    }
+}
