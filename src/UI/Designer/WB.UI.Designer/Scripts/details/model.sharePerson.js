9357d34b61d93442f176ed2bc4822a0284a5cf70
Roman Lytvyn <litvin.roma@gmail.com>
2013-06-04 12:06:11 -0400

    designer: share questionnaire between users
    --HG--
    branch : questionnaire-shared-persons
    
    
    Former-commit-id: 63aefbb17c14952969253d114906c06aff8e4cb6

---
+++ src/UI/Designer/WB.UI.Designer/Scripts/details/model.sharePerson.js
@@ -0,0 +1,18 @@
+﻿define('model.sharePerson',
+    ['ko','config'],
+    function (ko) {
+        var SharePerson = function () {
+            var self = this;
+
+            self.id = ko.observable(Math.uuid());
+            self.userName = ko.observable().extend({ required: true });
+            
+            self.isNullo = false;
+            return self;
+        };
+
+        SharePerson.Nullo = new SharePerson().id(0).userName('');
+        SharePerson.Nullo.isNullo = true;
+
+        return SharePerson;
+});
