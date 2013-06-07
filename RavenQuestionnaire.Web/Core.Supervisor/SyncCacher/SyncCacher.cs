9ce7f9b594f1a28a9b7377a7f02ea875997e4185
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2013-06-07 17:47:11 -0400

    fix and merge
    --HG--
    branch : capi-sync-deltas
    
    
    Former-commit-id: 7950f6be5a8ab33b48f5f8e552d4dd0fc5992e20

---
+++ RavenQuestionnaire.Web/Core.Supervisor/SyncCacher/SyncCacher.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Supervisor.SyncCacher
+{
+    public class SyncCacher : ISyncCacher
+    {
+        public void CacheItem()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CacheItem(Guid item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetCachedItem()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RavenQuestionnaire.Web/WB.Core.Synchronization/ClientIdentifier.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WB.Core.Synchronization
+{
+    public class ClientIdentifier
+    {
+        public Guid ClientInstanceKey;
+        public string ClientDeviceKey;
+        public string ClientVersionNumber;
+        
+        public Guid? LastSyncIdentifier;
+
+        public Guid? CurrentProcessKey;
+    }
+}
