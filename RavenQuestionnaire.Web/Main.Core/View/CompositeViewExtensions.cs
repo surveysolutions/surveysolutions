6eea8b3306991b0291745cc456e9654bc373afc4
Andrey Kozhyn <hedgic@gmail.com>
2013-06-10 11:31:14 +0300

    Added null check
    --HG--
    branch : pdf
    
    
    Former-commit-id: 106aa78c162cd8cfb9b189c9feb288d4e2968bd2

--- RavenQuestionnaire.Web/Main.Core/View/CompositeViewExtensions.cs
+++ RavenQuestionnaire.Web/Main.Core/View/CompositeViewExtensions.cs
@@ -1,9 +1,12 @@
+using System;
+
 namespace Main.Core.View
 {
     public static class CompositeViewExtensions
     {
         public static int GetDepthIn(this ICompositeView view)
         {
+            if (view == null) throw new ArgumentNullException("view");
             int result = 0;
             var parent = view.ParentView;
             while (parent != null)
