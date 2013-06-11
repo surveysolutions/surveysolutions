d1c269258aa0228f4b279db61ba5f1b55ce7e927
Andrii Kozhyn <hedgic@gmail.com>
2013-06-11 17:18:05 +0300

    Initial version with cover and statistics added
    --HG--
    branch : pdf
    
    
    Former-commit-id: 1e4e8a04d438713dfbbf9abb9d3f5e76e048bedf

--- src/UI/Designer/WB.UI.Designer/Pdf/TitlePageDto.cs
+++ src/UI/Designer/WB.UI.Designer/Pdf/TitlePageDto.cs
@@ -7,5 +7,11 @@ namespace WB.UI.Designer.Pdf
         public string SurveyName { get; set; }
 
         public string CreationDate { get; set; }
+
+        public int ChaptersCount { get; set; }
+
+        public int QuestionsCount { get; set; }
+
+        public int QuestionsWithConditionsCount { get; set; }
     }
 }
