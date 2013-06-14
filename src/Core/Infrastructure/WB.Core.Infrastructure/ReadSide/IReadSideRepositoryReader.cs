7f0088aba8f86fe2a2bedd4f1fbccc910dd2c212
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2013-06-14 17:51:23 +0300

    Core\Infrastructure: read side repository accessors now temporarily implement IQueryableDenormalizerStorage (to simplify it's removance)
    Former-commit-id: 665ad78d28b93fc316a26917be4d5c9346ffbc4f

--- src/Core/Infrastructure/WB.Core.Infrastructure/ReadSide/IReadSideRepositoryReader.cs
+++ src/Core/Infrastructure/WB.Core.Infrastructure/ReadSide/IReadSideRepositoryReader.cs
@@ -3,6 +3,6 @@ namespace WB.Core.Infrastructure.ReadSide
     /// <summary>
     /// Accessor for read-side repository which should be used to perform queries.
     /// </summary>
-    public interface IReadSideRepositoryReader<TEntity>
-        where TEntity : class, IReadSideRepositoryEntity { }
+    public interface IReadSideRepositoryReader<TEntity> : IQueryableDenormalizerStorage<TEntity>
+        where TEntity : class, IReadSideRepositoryEntity, IView { }
 }
