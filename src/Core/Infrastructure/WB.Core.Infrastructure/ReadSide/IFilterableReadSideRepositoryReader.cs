a8f253d02fdb41f37aec8e3ba65cb06150e1cf6c
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2013-06-14 20:03:04 +0300

    Core.Infrastructure.ReadSide: class hierarchy updated
    Former-commit-id: 92a0e6558181ed01edc5e90d7a692f8650b8e308

--- src/Core/Infrastructure/WB.Core.Infrastructure/ReadSide/IFilterableReadSideRepositoryReader.cs
+++ src/Core/Infrastructure/WB.Core.Infrastructure/ReadSide/IFilterableReadSideRepositoryReader.cs
@@ -1,9 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
 namespace WB.Core.Infrastructure.ReadSide
 {
     /// <summary>
     /// Accessor for read-side repository which should be used to perform queries.
     /// Also supports filtering.
     /// </summary>
-    public interface IFilterableReadSideRepositoryReader<TEntity> : IFilterableDenormalizerStorage<TEntity>
-        where TEntity : class, IReadSideRepositoryEntity, IView { }
+    public interface IFilterableReadSideRepositoryReader<TEntity> : IReadSideRepositoryReader<TEntity>
+        where TEntity : class, IReadSideRepositoryEntity, IView
+    {
+        IEnumerable<TEntity> Filter(Expression<Func<TEntity, bool>> predicate);
+    }
 }
