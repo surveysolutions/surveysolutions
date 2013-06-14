94863f4893d4e074ee743c2f7e5f0bada2eb62e2
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2013-06-14 20:49:12 +0300

    Core.Infrastructure: IReadSideRepositoryReader now has it's own interface
    Former-commit-id: 9c43e1d1ef0e1714a9faa270a4108ed8697f59a7

--- src/Core/Infrastructure/WB.Core.Infrastructure/ReadSide/IReadSideRepositoryReader.cs
+++ src/Core/Infrastructure/WB.Core.Infrastructure/ReadSide/IReadSideRepositoryReader.cs
@@ -1,8 +1,15 @@
+using System;
+
 namespace WB.Core.Infrastructure.ReadSide
 {
     /// <summary>
     /// Accessor for read-side repository which should be used to perform queries.
     /// </summary>
-    public interface IReadSideRepositoryReader<TEntity> : IDenormalizerStorage<TEntity>
-        where TEntity : class, IReadSideRepositoryEntity, IView { }
+    public interface IReadSideRepositoryReader<TEntity>
+        where TEntity : class, IReadSideRepositoryEntity
+    {
+        int Count();
+
+        TEntity GetById(Guid id);
+    }
 }
