7811d3a4dcb8b1edfad7464f7152a37868ef4581
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2013-06-17 14:22:40 +0300

    Mono: replaced usages of IDenormalizerStorage with IReadSideRepositoryReader and IReadSideRepositoryWriter (or their descendants)
    Former-commit-id: 296847b9f0f71f507817729011a8e9a289a6854a

---
+++ src/Core/Infrastructure/WB.Core.Infrastructure/ReadSide/IFilterableReadSideRepositoryWriter.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace WB.Core.Infrastructure.ReadSide
+{
+    /// <summary>
+    /// Accessor for read-side repository which should be used to update repository with new data from write-side.
+    /// Please note, that reader (IReadSideRepositoryReader) should not be used to get data needed for such an update.
+    /// All needed querying methods are already in this interface and should be used because they might be optimized for such operations.
+    /// Also supports filtering.
+    /// </summary>
+    public interface IFilterableReadSideRepositoryWriter<TEntity> : IReadSideRepositoryWriter<TEntity>
+        where TEntity : class, IReadSideRepositoryEntity
+    {
+        IEnumerable<TEntity> Filter(Expression<Func<TEntity, bool>> predicate);
+    }
+}
