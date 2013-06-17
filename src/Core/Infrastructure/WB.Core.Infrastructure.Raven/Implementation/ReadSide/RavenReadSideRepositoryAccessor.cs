c8cd1df8b5d56ed500f23b9e6bf064de5ea9d415
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2013-06-17 20:59:58 +0300

    Core.Infrastructure.Raven: added implementations for read-side readers and writers
    Former-commit-id: 42928d95ce0738f628218f6cc0e4f0e312626a6d

---
+++ src/Core/Infrastructure/WB.Core.Infrastructure.Raven/Implementation/ReadSide/RavenReadSideRepositoryAccessor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+using Raven.Client;
+using Raven.Client.Document;
+using Raven.Client.Extensions;
+
+using WB.Core.Infrastructure.ReadSide;
+
+namespace WB.Core.Infrastructure.Raven.Implementation.ReadSide
+{
+    #warning TLK: make string identifiers here after switch to new storage
+    public abstract class RavenReadSideRepositoryAccessor<TEntity>
+        where TEntity : class, IReadSideRepositoryEntity
+    {
+        private readonly DocumentStore ravenStore;
+
+        protected RavenReadSideRepositoryAccessor(DocumentStore ravenStore)
+        {
+            this.ravenStore = ravenStore;
+        }
+
+        private static string ViewName
+        {
+            get { return typeof(TEntity).FullName; }
+        }
+
+        protected IDocumentSession OpenSession()
+        {
+            this.ravenStore.DatabaseCommands.EnsureDatabaseExists("Views");
+            return this.ravenStore.OpenSession("Views");
+        }
+
+        protected static string ToRavenId(Guid id)
+        {
+            return string.Format("{0}${1}", ViewName, id.ToString());
+        }
+    }
+}
---
+++ src/Core/Infrastructure/WB.Core.Infrastructure.Raven/Implementation/ReadSide/RavenReadSideRepositoryWriter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+
+using Raven.Client;
+using Raven.Client.Document;
+using Raven.Client.Extensions;
+
+using WB.Core.Infrastructure.ReadSide;
+
+namespace WB.Core.Infrastructure.Raven.Implementation.ReadSide
+{
+    #warning TLK: make string identifiers here after switch to new storage
+    public class RavenReadSideRepositoryWriter<TEntity> : RavenReadSideRepositoryAccessor<TEntity>, IReadSideRepositoryWriter<TEntity>
+        where TEntity : class, IReadSideRepositoryEntity
+    {
+        public RavenReadSideRepositoryWriter(DocumentStore ravenStore)
+            : base(ravenStore) { }
+
+        public TEntity GetById(Guid id)
+        {
+            string ravenId = ToRavenId(id);
+
+            using (var session = this.OpenSession())
+            {
+                return session.Load<TEntity>(id: ravenId);
+            }
+        }
+
+        public void Remove(Guid id)
+        {
+            string ravenId = ToRavenId(id);
+
+            using (var session = this.OpenSession())
+            {
+                var view = session.Load<TEntity>(id: ravenId);
+
+                session.Delete(view);
+                session.SaveChanges();
+            }
+        }
+
+        public void Store(TEntity view, Guid id)
+        {
+            string ravenId = ToRavenId(id);
+
+            using (var session = this.OpenSession())
+            {
+                session.Store(entity: view, id: ravenId);
+                session.SaveChanges();
+            }
+        }
+    }
+}
