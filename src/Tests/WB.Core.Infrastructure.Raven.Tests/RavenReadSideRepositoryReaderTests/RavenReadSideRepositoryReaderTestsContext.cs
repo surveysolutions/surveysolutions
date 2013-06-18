686627406330c42753e57a7cdf1769ac5a77476f
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2013-06-18 16:58:57 +0300

    Core.Infrastructure\RavenReadSideRepositoryReader: added tests requiring maintenance exception when views are being rebuilt
    Former-commit-id: b364138051b2a0c8c633f888ec478f552926d624

---
+++ src/Tests/WB.Core.Infrastructure.Raven.Tests/RavenReadSideRepositoryReaderTests/RavenReadSideRepositoryReaderTestsContext.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+using Raven.Client.Document;
+
+using WB.Core.Infrastructure.Raven.Implementation.ReadSide;
+using WB.Core.Infrastructure.ReadSide;
+
+namespace WB.Core.Infrastructure.Raven.Tests.RavenReadSideRepositoryReaderTests
+{
+    internal class RavenReadSideRepositoryReaderTestsContext
+    {
+        internal static RavenReadSideRepositoryReader<TEntity> CreateRavenReadSideRepositoryReader<TEntity>(
+            DocumentStore ravenStore = null, IReadLayerStatusService readLayerStatusService = null)
+            where TEntity : class, IReadSideRepositoryEntity
+        {
+            return new RavenReadSideRepositoryReader<TEntity>(
+                ravenStore ?? new DocumentStore());
+        }
+    }
+}
