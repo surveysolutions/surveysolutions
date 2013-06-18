772e9b55eb761bbdd46bb9160267a4ef248512b2
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2013-06-18 17:04:45 +0300

    Core.Infrastructure\RavenReadSideRepositoryReader: implemented tests requirements about maintenance exception when views are being rebuilt
    Former-commit-id: 2b05bd45bc0c54dabe137fb433a90de79a59b725

--- src/Core/Infrastructure/WB.Core.Infrastructure.Raven/Implementation/ReadSide/RavenReadSideRepositoryReader.cs
+++ src/Core/Infrastructure/WB.Core.Infrastructure.Raven/Implementation/ReadSide/RavenReadSideRepositoryReader.cs
@@ -13,11 +13,18 @@ namespace WB.Core.Infrastructure.Raven.Implementation.ReadSide
     public class RavenReadSideRepositoryReader<TEntity> : RavenReadSideRepositoryAccessor<TEntity>, IQueryableReadSideRepositoryReader<TEntity>
         where TEntity : class, IReadSideRepositoryEntity
     {
-        public RavenReadSideRepositoryReader(DocumentStore ravenStore)
-            : base(ravenStore) { }
+        private readonly IReadLayerStatusService readLayerStatusService;
+
+        public RavenReadSideRepositoryReader(DocumentStore ravenStore, IReadLayerStatusService readLayerStatusService)
+            : base(ravenStore)
+        {
+            this.readLayerStatusService = readLayerStatusService;
+        }
 
         public int Count()
         {
+            this.ThrowIfRepositoryIsNotAccessible();
+
             using (IDocumentSession session = this.OpenSession())
             {
                 return
@@ -29,6 +36,8 @@ namespace WB.Core.Infrastructure.Raven.Implementation.ReadSide
 
         public TEntity GetById(Guid id)
         {
+            this.ThrowIfRepositoryIsNotAccessible();
+
             string ravenId = ToRavenId(id);
 
             using (IDocumentSession session = this.OpenSession())
@@ -39,11 +48,19 @@ namespace WB.Core.Infrastructure.Raven.Implementation.ReadSide
 
         public TResult Query<TResult>(Func<IQueryable<TEntity>, TResult> query)
         {
+            this.ThrowIfRepositoryIsNotAccessible();
+
             using (IDocumentSession session = this.OpenSession())
             {
                 return query.Invoke(
                     session.Query<TEntity>());
             }
+        }
+
+        private void ThrowIfRepositoryIsNotAccessible()
+        {
+            if (this.readLayerStatusService.AreViewsBeingRebuiltNow())
+                throw new MaintenanceException("Views are currently being rebuilt. Therefore your request cannot be complete now.");
         }
     }
 }
