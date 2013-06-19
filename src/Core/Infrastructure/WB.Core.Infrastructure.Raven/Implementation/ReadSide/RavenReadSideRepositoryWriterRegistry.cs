d999d2a1a282c280bfcc3955c92c7acfe8f40db9
Anatoliy 'TLK' Kolesnick <none@none>
2013-06-19 13:43:27 +0300

    Core.Infrastructure.Raven: added empty implementation for RavenReadSideRepositoryWriterRegistry
    Former-commit-id: ec2914e1b39c9e4903027b3fec1cd16af42ceb3f

---
+++ src/Core/Infrastructure/WB.Core.Infrastructure.Raven/Implementation/ReadSide/RavenReadSideRepositoryWriterRegistry.cs
@@ -0,0 +1,9 @@
+﻿namespace WB.Core.Infrastructure.Raven.Implementation.ReadSide
+{
+    internal class RavenReadSideRepositoryWriterRegistry : IRavenReadSideRepositoryWriterRegistry
+    {
+        public void Register(IRavenReadSideRepositoryWriter writer)
+        {
+        }
+    }
+}
