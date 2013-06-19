d9285bea4933aba9b1c617d2807bcec5712d2551
Anastasya Kholodova <nastya.kholodova@gmail.com>
2013-06-19 14:39:19 -0400

    id is renamed to publickey in order to avoid conflicts with ravendv id field
    Former-commit-id: 199740de8b6a2a84f9d92c756bf3b628d1b4b6cb

--- src/Core/Synchronization/WB.Core.Synchronization/SyncStorage/SynchronizationDelta.cs
+++ src/Core/Synchronization/WB.Core.Synchronization/SyncStorage/SynchronizationDelta.cs
@@ -6,15 +6,15 @@ namespace WB.Core.Synchronization.SyncStorage
 {
     public class SynchronizationDelta : IView
     {
-        public SynchronizationDelta(Guid id, string content, long sequence, Guid userId)
+        public SynchronizationDelta(Guid publicKey, string content, long sequence, Guid userId)
         {
-            Id = id;
+            PublicKey = publicKey;
             Content = content;
             Sequence = sequence;
             UserId = userId;
         }
 
-        public Guid Id { get; private set; }
+        public Guid PublicKey { get; private set; }
         public string Content { get; private set; }
         public long Sequence { get; private set; }
         public Guid UserId { get; private set; }
