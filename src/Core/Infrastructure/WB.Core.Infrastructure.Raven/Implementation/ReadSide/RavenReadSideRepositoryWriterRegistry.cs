0afaf682d4e5759f50f8c314900a21b29192b291
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2013-06-19 14:04:31 +0300

    Core.Infrastructure.Raven: RavenReadSideRepositoryWriterRegistry implemented
    Former-commit-id: bc3bacac8e9bc5a521db65faa0a9bdda81a89629

--- src/Core/Infrastructure/WB.Core.Infrastructure.Raven/Implementation/ReadSide/RavenReadSideRepositoryWriterRegistry.cs
+++ src/Core/Infrastructure/WB.Core.Infrastructure.Raven/Implementation/ReadSide/RavenReadSideRepositoryWriterRegistry.cs
@@ -1,9 +1,19 @@
-﻿namespace WB.Core.Infrastructure.Raven.Implementation.ReadSide
+﻿using System.Collections.Generic;
+
+namespace WB.Core.Infrastructure.Raven.Implementation.ReadSide
 {
     internal class RavenReadSideRepositoryWriterRegistry : IRavenReadSideRepositoryWriterRegistry
     {
+        private readonly List<IRavenReadSideRepositoryWriter> writers = new List<IRavenReadSideRepositoryWriter>();
+
         public void Register(IRavenReadSideRepositoryWriter writer)
         {
+            this.writers.Add(writer);
+        }
+
+        public IEnumerable<IRavenReadSideRepositoryWriter> GetAll()
+        {
+            return this.writers;
         }
     }
 }
