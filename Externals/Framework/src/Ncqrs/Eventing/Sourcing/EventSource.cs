5a2932d4badf791aa6ab11c07acf38bd9e950ce3
Anastasya Kholodova <nastya.kholodova@gmail.com>
2013-06-20 12:21:59 -0400

    test for ncqrs are fixed
    Former-commit-id: e82c872cd00485e0fe26849cd2f03a44be93d1bd

--- Externals/Framework/src/Ncqrs/Eventing/Sourcing/EventSource.cs
+++ Externals/Framework/src/Ncqrs/Eventing/Sourcing/EventSource.cs
@@ -110,9 +110,11 @@ namespace Ncqrs.Eventing.Sourcing
         /// <param name="history">The history.</param>
         public virtual void InitializeFromHistory(CommittedEventStream history)
         {
-                #if USE_CONTRACTS
+            #if USE_CONTRACTS
             Contract.Requires<ArgumentNullException>(history != null, "The history cannot be null.");
             #endif
+            if (history == null)
+                throw new ArgumentNullException("The history cannot be null.");
                 if (_initialVersion != Version)
             {
                 throw new InvalidOperationException("Cannot apply history when instance has uncommitted changes.");
