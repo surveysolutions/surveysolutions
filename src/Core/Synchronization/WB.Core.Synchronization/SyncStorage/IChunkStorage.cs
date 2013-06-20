03c65fe7c481b8146725278b701bb9081379750b
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2013-06-20 12:47:51 -0400

    implementations are marked as internal
    Former-commit-id: 33c44312b4d5886f41e969d9958c68b810ebfa2b

--- src/Core/Synchronization/WB.Core.Synchronization/SyncStorage/IChunkStorage.cs
+++ src/Core/Synchronization/WB.Core.Synchronization/SyncStorage/IChunkStorage.cs
@@ -4,7 +4,7 @@ using WB.Core.SharedKernel.Structures.Synchronization;
 
 namespace WB.Core.Synchronization.SyncStorage
 {
-    public interface IChunkStorage
+    internal interface IChunkStorage
     {
         void StoreChunk(SyncItem syncItem, Guid? userId);
         SyncItem ReadChunk(Guid id);
