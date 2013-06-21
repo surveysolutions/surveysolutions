85f32352cca3c2139396a694d2ab739bb88d277a
Anastasya Kholodova <nastya.kholodova@gmail.com>
2013-06-21 11:54:23 -0400

    manual merge
    Former-commit-id: aea59a4e93b9ff2935fd574b44808cd10c2340ef

--- Mono/CAPI.Android/Syncronization/Pull/RestPull.cs
+++ Mono/CAPI.Android/Syncronization/Pull/RestPull.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using CAPI.Android.Syncronization.RestUtils;
 using WB.Core.SharedKernel.Structures.Synchronization;
 
--- Mono/CAPI.Android/Syncronization/Push/RestPush.cs
+++ Mono/CAPI.Android/Syncronization/Push/RestPush.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Threading;
 using CAPI.Android.Syncronization.RestUtils;
 using Newtonsoft.Json;
 using WB.Core.SharedKernel.Structures.Synchronization;
