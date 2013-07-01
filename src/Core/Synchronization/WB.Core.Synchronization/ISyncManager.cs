eb80324d6ef5461f3685c93116e5adbb4d53e2e3
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2013-07-01 19:24:36 -0400

    fix 2612: Synchronization (Push) problem
    Former-commit-id: 451937daac468f64c1609b01c9c3872e7a0d4468

--- src/Core/Synchronization/WB.Core.Synchronization/ISyncManager.cs
+++ src/Core/Synchronization/WB.Core.Synchronization/ISyncManager.cs
@@ -7,23 +7,16 @@ namespace WB.Core.Synchronization
     public interface ISyncManager
     {
         HandshakePackage ItitSync(ClientIdentifier identifier);
-
-        bool InitSending(ClientIdentifier identifier);
-
-        bool InitReceiving(ClientIdentifier identifier);
-
+        
         bool SendSyncPackage(SyncPackage package);
-
         bool SendSyncItem(SyncItem package);
 
-        HandshakePackage CheckAndCreateNewProcess(ClientIdentifier clientIdentifier);
-        
         IEnumerable<Guid> GetAllARIds(Guid userId, Guid clientRegistrationKey);
-
         IEnumerable<KeyValuePair<long,Guid>> GetAllARIdsWithOrder(Guid userId, Guid clientRegistrationKey);
 
         SyncPackage ReceiveSyncPackage(Guid clientRegistrationId, Guid id, long sequence);
 
-        /*SyncPackage ReceiveLastSyncPackage(Guid clientRegistrationId, long sequence);*/
+        int GetNumberToGet(Guid userId, Guid clientRegistrationId, long sequence);
+        
     }
 }
