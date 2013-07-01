64781238acb789e67c3d1f5e9162adc9fb65ea0d
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2013-07-01 15:46:35 -0400

    case 2616
    Former-commit-id: 766fefe2bb0ebd97b02317e38a75bf2c230cacef

---
+++ src/Core/Synchronization/WB.Core.Synchronization/IBackupManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SynchronizationMessages.Export;
+
+namespace WB.Core.Synchronization
+{
+    public interface IBackupManager
+    {
+        ZipFileData Backup();
+        /// <summary>
+        /// restore from zip archive
+        /// <b>current implementation of restore DO NOT clean up database. it is only adding new date</b>
+        /// </summary>
+        /// <param name="zipData"></param>
+        
+        void Restore(List<string> zipData);
+    }
+}
--- src/Core/Synchronization/WB.Core.Synchronization/IImportManager.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-
-namespace WB.Core.Synchronization
-{
-    public interface IImportManager
-    {
-        void Import(List<string> zipData);
-    }
-}
