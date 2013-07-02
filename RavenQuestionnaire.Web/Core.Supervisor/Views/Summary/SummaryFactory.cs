3b006a5168af972213ee42d58502c260e33ca3f4
Roman Litvin <litvin.roma@gmail.com>
2013-07-02 18:48:39 +0300

    supervisor: summary row for survey, summary views
    Former-commit-id: f3a6dee01be533dc981e7b2b59474aa89e020a3a

--- RavenQuestionnaire.Web/Core.Supervisor/Views/Summary/SummaryFactory.cs
+++ RavenQuestionnaire.Web/Core.Supervisor/Views/Summary/SummaryFactory.cs
@@ -1,5 +1,4 @@
-﻿using WB.Core.Infrastructure.ReadSide;
-using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
+﻿using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
 
 namespace Core.Supervisor.Views.Summary
 {
@@ -25,58 +24,54 @@ namespace Core.Supervisor.Views.Summary
         {
             return this.summary.Query(
                 _ =>
+                {
+                    if (input.ViewerStatus == ViewerStatus.Headquarter)
                     {
-                        if (input.ViewerStatus == ViewerStatus.Headquarter)
-                        {
-                            _ = _.Where(x => x.ResponsibleSupervisorId == null);
-                        }
-                        else if (input.ViewerStatus == ViewerStatus.Supervisor)
-                        {
-                            _ = _.Where(x => x.ResponsibleSupervisorId == input.ViewerId);
-                        }
+                        _ = _.Where(x => x.ResponsibleSupervisorId == null);
+                    }
+                    else if (input.ViewerStatus == ViewerStatus.Supervisor)
+                    {
+                        _ = _.Where(x => x.ResponsibleSupervisorId == input.ViewerId);
+                    }
 
-                        if (input.TemplateId.HasValue)
-                        {
-                            _ = _.Where(x => x.TemplateId == input.TemplateId);
-                        }
+                    if (input.TemplateId.HasValue)
+                    {
+                        _ = _.Where(x => x.TemplateId == input.TemplateId);
+                    }
 
-                        var all = _.ToList().GroupBy(
-                                 x => x.ResponsibleId,
-                                 y => y,
-                                 (x, y) =>
-                                 new SummaryViewItem()
-                                     {
-                                         User = new UserLight(x, y.FirstOrDefault().ResponsibleName),
-                                         Approved = y.Sum(z => z.ApprovedCount),
-                                         Completed = y.Sum(z => z.CompletedCount),
-                                         Error = y.Sum(z => z.CompletedWithErrorsCount),
-                                         Initial = y.Sum(z => z.InitialCount),
-                                         Redo = y.Sum(z => z.RedoCount),
-                                         Unassigned = y.Sum(z => z.UnassignedCount),
-                                         Total = y.Sum(z => z.TotalCount)
-                                     }).AsQueryable().OrderUsingSortExpression(input.Order);
+                    var all = _.ToList().GroupBy(
+                        x => x.ResponsibleId,
+                        y => y,
+                        (x, y) =>
+                            new SummaryViewItem()
+                            {
+                                User = new UserLight(x, y.FirstOrDefault().ResponsibleName),
+                                Approved = y.Sum(z => z.ApprovedCount),
+                                Completed = y.Sum(z => z.CompletedCount),
+                                Error = y.Sum(z => z.CompletedWithErrorsCount),
+                                Initial = y.Sum(z => z.InitialCount),
+                                Redo = y.Sum(z => z.RedoCount),
+                                Unassigned = y.Sum(z => z.UnassignedCount),
+                                Total = y.Sum(z => z.TotalCount)
+                            }).AsQueryable().OrderUsingSortExpression(input.Order);
 
-                        return new SummaryView()
-                                   {
-                                       TotalCount = all.Count(),
-                                       Items =
-                                           all.Skip((input.Page - 1) * input.PageSize)
-                                              .Take(input.PageSize)
-                                              .Union(
-                                                  new[]
-                                                      {
-                                                          new SummaryViewItem(
-                                                              new UserLight(Guid.Empty, "Summary"),
-                                                              all.Sum(x => x.Total),
-                                                              all.Sum(x => x.Initial),
-                                                              all.Sum(x => x.Error),
-                                                              all.Sum(x => x.Completed),
-                                                              all.Sum(x => x.Approved),
-                                                              all.Sum(x => x.Redo),
-                                                              all.Sum(x => x.Unassigned))
-                                                      })
-                                   };
-                    });
+                    return new SummaryView()
+                    {
+                        TotalCount = all.Count(),
+                        Items =
+                            all.Skip((input.Page - 1)*input.PageSize)
+                                .Take(input.PageSize),
+                        ItemsSummary = new SummaryViewItem(
+                            new UserLight(Guid.Empty, "Summary"),
+                            all.Sum(x => x.Total),
+                            all.Sum(x => x.Initial),
+                            all.Sum(x => x.Error),
+                            all.Sum(x => x.Completed),
+                            all.Sum(x => x.Approved),
+                            all.Sum(x => x.Redo),
+                            all.Sum(x => x.Unassigned))
+                    };
+                });
         }
     }
 }
