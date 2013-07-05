165bd1910bbe8344a1eeed1c7087c51dc2980796
Slava Scherbak <slava.scherbak@gmail.com>
2013-07-05 12:52:21 +0300

    GenericSubdomains: moved logger from SharedKernel to GenericSubdomins folder
    Former-commit-id: a342a604c74d86f954065a2bbb5e96cac173e7e1

--- src/Core/GenericSubdomains/WB.Core.GenericSubdomains.Logging/Properties/AssemblyInfo.cs
+++ src/Core/GenericSubdomains/WB.Core.GenericSubdomains.Logging/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WB.Core.SharedKernel.Logger")]
+[assembly: AssemblyTitle("WB.Core.GenericSubdomains.Logging")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WB.Core.SharedKernel.Logger")]
+[assembly: AssemblyProduct("WB.Core.GenericSubdomains.Logging")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
