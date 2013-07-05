ba02e84ecdaadf836493c8391b7ee5bba2509b78
Slava Scherbak <slava.scherbak@gmail.com>
2013-07-05 18:03:09 +0300

    GenericSubdomains\Logging: replaced two override method with one with optional parameter
    Former-commit-id: 6f506094652a526926779e7fbfb1f47adcdb5db8

--- src/Core/GenericSubdomains/Logging/WB.Core.GenericSubdomains.Logging/ILogger.cs
+++ src/Core/GenericSubdomains/Logging/WB.Core.GenericSubdomains.Logging/ILogger.cs
@@ -4,24 +4,14 @@ namespace WB.Core.GenericSubdomains.Logging
 {
     public interface ILogger
     {
-        void Debug(object message);
+        void Debug(object message, Exception exception = null);
 
-        void Debug(object message, Exception exception);
+        void Error(object message, Exception exception = null);
 
-        void Error(object message);
+        void Fatal(object message, Exception exception = null);
 
-        void Error(object message, Exception exception);
+        void Info(object message, Exception exception = null);
 
-        void Fatal(object message);
-
-        void Fatal(object message, Exception exception);
-
-        void Info(object message);
-
-        void Info(object message, Exception exception);
-
-        void Warn(object message);
-
-        void Warn(object message, Exception exception);
+        void Warn(object message, Exception exception = null);
     }
 }
