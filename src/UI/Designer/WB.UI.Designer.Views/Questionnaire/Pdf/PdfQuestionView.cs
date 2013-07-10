edd918ce33dcc3b11d8d5b2eaa7ab4a9c5633b4d
Andrii Kozhyn <hedgic@gmail.com>
2013-07-10 20:20:08 +0300

    Added DTOs for PDF
    Former-commit-id: a1cf97d1416302cbe1dee4a374a69d1c46d47441

---
+++ src/UI/Designer/WB.UI.Designer.Views/Questionnaire/Pdf/PdfQuestionView.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Main.Core.Entities.SubEntities;
+
+namespace WB.UI.Designer.Views.Questionnaire.Pdf
+{
+    public class PdfQuestionView : PdfEntityView
+    {
+        public QuestionType QuestionType { get; set; }
+
+        public List<PdfAnswerView> Answers { get; set; }
+    }
+}
---
+++ src/UI/Designer/WB.UI.Designer.Views/Questionnaire/Pdf/PdfQuestionnaireView.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WB.UI.Designer.Views.Questionnaire.Pdf
+{
+    public class PdfQuestionnaireView
+    {
+        public string CreatedBy { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public string Title { get; set; }
+
+        public int ChaptersCount { get; set; }
+
+        public int QuestionsCount { get; set; }
+
+        public int QuestionsWithConditionsCount { get; set; }
+
+        public int GroupsCount { get; set; }
+
+        public List<PdfGroupView> Groups { get; set; }
+    }
+}
