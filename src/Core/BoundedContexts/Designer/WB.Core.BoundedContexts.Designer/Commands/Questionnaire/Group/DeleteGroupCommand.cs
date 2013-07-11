b61ba6e18034c5e9711d9c7703e97284a621bba5
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2013-07-11 16:38:37 +0300

    Designer: copied questionnaire commands to separate bounded context
    --HG--
    branch : data-collection-shared-kernel
    
    
    Former-commit-id: 9e803cea877f79f77b17a3e2a76a3a256ccb1bcc

---
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Commands/Questionnaire/Group/DeleteGroupCommand.cs
@@ -0,0 +1,18 @@
+﻿using Main.Core.Commands.Questionnaire.Base;
+
+namespace Main.Core.Commands.Questionnaire.Group
+{
+    using System;
+
+    using Main.Core.Domain;
+
+    using Ncqrs.Commanding.CommandExecution.Mapping.Attributes;
+
+    [Serializable]
+    [MapsToAggregateRootMethod(typeof(QuestionnaireAR), "NewDeleteGroup")]
+    public class DeleteGroupCommand : GroupCommand
+    {
+        public DeleteGroupCommand(Guid questionnaireId, Guid groupId)
+            : base(questionnaireId, groupId) {}
+    }
+}
