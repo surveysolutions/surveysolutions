a92f6c14815189f8ba12fa3a0c3752eb34cab577
Slava Scherbak <slava.scherbak@gmail.com>
2013-07-13 12:54:50 +0300

    Web.UI\Shared: command deserializatoin was separated as ninject module
    Former-commit-id: 4e061ff77d3cc5f24ef26fd930b03883dc2e068c

---
+++ src/UI/Shared/WB.UI.Shared.Web/CommandDeserialization/SupervisorCommandDeserializer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Main.Core.Commands.Questionnaire;
+using Main.Core.Commands.Questionnaire.Group;
+using Main.Core.Commands.Questionnaire.Question;
+
+namespace WB.UI.Shared.Web.CommandDeserialization
+{
+    internal class SupervisorCommandDeserializer : CommandDeserializer
+    {
+        protected override Dictionary<string, Type> KnownCommandTypes
+        {
+            get
+            {
+                return new Dictionary<string, Type>
+                    {
+                        {"UpdateQuestionnaire", typeof (UpdateQuestionnaireCommand)}
+                    };
+            }
+        }
+    }
+}
