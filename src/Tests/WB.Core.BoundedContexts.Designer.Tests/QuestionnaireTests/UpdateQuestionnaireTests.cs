b8f1957326c5d7ac8285113b890363dfb0f95aaf
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2013-07-15 19:09:00 +0300

    Tests: splitted Questionnaire tests between SharedKernels.DataCollection tests and BoundedContexts.Designer tests
    --HG--
    branch : data-collection-shared-kernel
    
    
    Former-commit-id: ae2cc2a277543692833594781d65793da53f2238

---
+++ src/Tests/WB.Core.BoundedContexts.Designer.Tests/QuestionnaireTests/UpdateQuestionnaireTests.cs
@@ -0,0 +1,53 @@
+﻿using Main.Core.Domain;
+using Main.Core.Events.Questionnaire;
+using Microsoft.Practices.ServiceLocation;
+using Moq;
+using Ncqrs.Spec;
+using NUnit.Framework;
+using WB.Core.BoundedContexts.Designer.Aggregates;
+
+namespace WB.Core.BoundedContexts.Designer.Tests.QuestionnaireTests
+{
+    [TestFixture]
+    public class UpdateQuestionnaireTests : QuestionnaireTestsContext
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            ServiceLocator.SetLocatorProvider(() => new Mock<IServiceLocator> { DefaultValue = DefaultValue.Mock }.Object);
+        }
+
+        [TestCase("")]
+        [TestCase("   ")]
+        [TestCase("\t")]
+        public void UpdateQuestionnaire_When_questionnaire_title_is_empty_or_contains_whitespaces_only_Then_throws_DomainException_with_type_QuestionnaireTitleRequired(string emptyTitle)
+        {
+            // arrange
+            Questionnaire questionnaire = CreateQuestionnaire();
+
+            // act
+            TestDelegate act = () => questionnaire.UpdateQuestionnaire(emptyTitle);
+
+            // assert
+            var domainException = Assert.Throws<DomainException>(act);
+            Assert.That(domainException.ErrorType, Is.EqualTo(DomainExceptionType.QuestionnaireTitleRequired));
+        }
+
+        [Test]
+        public void UpdateQuestionnaire_When_questionnaire_title_is_not_empty_Then_raised_QuestionnaireUpdated_event_contains_questionnaire_title()
+        {
+            using (var eventContext = new EventContext())
+            {
+                // arrange
+                var nonEmptyTitle = "Title";
+                Questionnaire questionnaire = CreateQuestionnaire();
+
+                // act
+                questionnaire.UpdateQuestionnaire(nonEmptyTitle);
+
+                // assert
+                Assert.That(GetSingleEvent<QuestionnaireUpdated>(eventContext).Title, Is.EqualTo(nonEmptyTitle));
+            }
+        }
+    }
+}
