3d9256a5cbcce02d6023b87917c38ed47add1ddc
Anatoliy 'TLK' Kolesnick <none@none>
2013-07-17 19:06:58 +0300

    Core: QuestionnaireBrowseViewFactory classes splitted to separate contexts
    --HG--
    branch : data-collection-shared-kernel
    
    
    Former-commit-id: c78e978ec22736aeaddde357eedd7ea46a067070

--- src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Views/Questionnaire/BrowseItem/QuestionnaireBrowseViewFactory.cs
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Views/Questionnaire/BrowseItem/QuestionnaireBrowseViewFactory.cs
@@ -1,11 +1,4 @@
-using System.Linq.Expressions;
-
-using Main.DenormalizerStorage;
-using System;
 using System.Linq;
-
-using WB.Core.Infrastructure;
-using WB.Core.Infrastructure.ReadSide;
 using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
 
 namespace Main.Core.View.Questionnaire
