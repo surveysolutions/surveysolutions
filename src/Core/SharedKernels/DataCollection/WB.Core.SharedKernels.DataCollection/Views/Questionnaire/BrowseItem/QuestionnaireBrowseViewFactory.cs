5f2a6a9f549c7446a79ceddb93c35a573f5dd945
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2013-07-17 19:06:58 +0300

    Core: QuestionnaireBrowseViewFactory classes splitted to separate contexts
    --HG--
    branch : data-collection-shared-kernel
    
    
    Former-commit-id: c78e978ec22736aeaddde357eedd7ea46a067070

---
+++ src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/Views/Questionnaire/BrowseItem/QuestionnaireBrowseViewFactory.cs
@@ -0,0 +1,93 @@
+using System.Linq;
+using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
+
+namespace Main.Core.View.Questionnaire
+{
+    using Main.Core.Utility;
+
+    /// <summary>
+    /// The questionnaire browse view factory.
+    /// </summary>
+    public class QuestionnaireBrowseViewFactory : IViewFactory<QuestionnaireBrowseInputModel, QuestionnaireBrowseView>
+    {
+        #region Fields
+
+        /// <summary>
+        /// The document group session.
+        /// </summary>
+        private readonly IQueryableReadSideRepositoryReader<QuestionnaireBrowseItem> documentGroupSession;
+
+        #endregion
+
+        #region Constructors and Destructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="QuestionnaireBrowseViewFactory"/> class.
+        /// </summary>
+        /// <param name="documentGroupSession">
+        /// The document group session.
+        /// </param>
+        public QuestionnaireBrowseViewFactory(IQueryableReadSideRepositoryReader<QuestionnaireBrowseItem> documentGroupSession)
+        {
+            this.documentGroupSession = documentGroupSession;
+        }
+
+        #endregion
+
+        #region Public Methods and Operators
+
+        /// <summary>
+        /// The load.
+        /// </summary>
+        /// <param name="input">
+        /// The input.
+        /// </param>
+        /// <returns>
+        /// The QuestionnaireBrowseView.
+        /// </returns>
+        public QuestionnaireBrowseView Load(QuestionnaireBrowseInputModel input)
+        {
+            // Adjust the model appropriately
+            int count = this.documentGroupSession.Count();
+            if (count == 0)
+            {
+                return new QuestionnaireBrowseView(
+                    input.Page, input.PageSize, count, new QuestionnaireBrowseItem[0], string.Empty);
+            }
+
+            return this.documentGroupSession.Query(queryable =>
+            {
+                IQueryable<QuestionnaireBrowseItem> query = queryable;
+
+                if (input.IsAdminMode.HasValue)
+                {
+                    if (input.IsOnlyOwnerItems)
+                    {
+                        query = query.Where(x => x.CreatedBy == input.CreatedBy);
+                    }
+
+                    if (!input.IsAdminMode.Value)
+                    {
+                        query = query.Where(x => !x.IsDeleted);
+                    }
+
+                    if (!string.IsNullOrEmpty(input.Filter))
+                    {
+                        #warning ReadLayer: ToList materialization because not supported by Raven
+                        query = query.ToList().AsQueryable().Where(x => x.Title.ContainsIgnoreCaseSensitive(input.Filter));
+                    }
+                }
+
+                #warning ReadLayer: ToList materialization because not supported by Raven
+                var queryResult = query.ToList().AsQueryable().OrderUsingSortExpression(input.Order);
+
+                var questionnaireItems = queryResult.Skip((input.Page - 1) * input.PageSize).Take(input.PageSize).ToArray();
+
+
+                return new QuestionnaireBrowseView(input.Page, input.PageSize, queryResult.Count(), questionnaireItems, input.Order);
+            });
+        }
+
+        #endregion
+    }
+}
