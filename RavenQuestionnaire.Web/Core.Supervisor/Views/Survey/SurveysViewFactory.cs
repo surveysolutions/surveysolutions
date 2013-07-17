7fa987ba7a6f3b2ead1e80d4f39ec911184781ac
Anastasya Kholodova <nastya.kholodova@gmail.com>
2013-07-17 18:15:01 -0400

    SummaryDenormalizer tests and counter fixes
    Former-commit-id: 328fced347784b26a8c1580cbc233e46819ec341

--- RavenQuestionnaire.Web/Core.Supervisor/Views/Survey/SurveysViewFactory.cs
+++ RavenQuestionnaire.Web/Core.Supervisor/Views/Survey/SurveysViewFactory.cs
@@ -24,78 +24,65 @@ namespace Core.Supervisor.Views.Survey
 
         public SurveysView Load(SurveysInputModel input)
         {
-            var items =
-                indexAccessor.Query<SummaryItem>(typeof(SummaryItemByTemplate).Name);
-      
-                    /*if (input.ViewerStatus == ViewerStatus.Headquarter)
-                    {
-                        items = items.Where(x => x.ResponsibleSupervisorId == null);
-                    }
-                    else*/ if (input.ViewerStatus == ViewerStatus.Supervisor)
-                    {
-                        items = items.Where(x => x.ResponsibleSupervisorId == input.ViewerId);
-                    }
+            var items = Enumerable.Empty<SummaryItem>().AsQueryable();
 
-                    if (input.UserId.HasValue)
-                    {
-                        if (input.ViewerStatus == ViewerStatus.Headquarter)
-                        {
-                            items = items.Where(x => x.ResponsibleSupervisorId == input.UserId && x.ResponsibleId == Guid.Empty);
-                        }
-                        else
-                        {
-                            items = items.Where(x => x.ResponsibleId == input.UserId);
-                        }
-                        
-                    }
-                    else
-                    {
-                        items = items.Where(x => x.ResponsibleId == Guid.Empty);
-                    }
+            if (input.ViewerStatus == ViewerStatus.Supervisor)
+            {
+                items =
+                    indexAccessor.Query<SummaryItem>(typeof(SummaryItemByTemplate).Name)
+                                 .Where(x => x.ResponsibleSupervisorId == input.ViewerId);
+            }
+            else
+            {
 
-                /*    var all = items.ToList().GroupBy(
-                        x => x.TemplateId,
-                        y => y,
-                        (x, y) =>
-                            new SurveysViewItem()
-                            {
-                                Template = new TemplateLight(x, y.FirstOrDefault().TemplateName),
-                                Approved = y.Sum(z => z.ApprovedCount),
-                                Completed = y.Sum(z => z.CompletedCount),
-                                Error = y.Sum(z => z.CompletedWithErrorsCount),
-                                Initial = y.Sum(z => z.InitialCount),
-                                Redo = y.Sum(z => z.RedoCount),
-                                Unassigned = y.Sum(z => z.UnassignedCount),
-                                Total = y.Sum(z => z.TotalCount)
-                            }).AsQueryable().OrderUsingSortExpression(input.Order);*/
-                    var all = items.OrderUsingSortExpression(input.Order).Skip((input.Page - 1) * input.PageSize).Take(input.PageSize).Select(y => new SurveysViewItem()
+                items = indexAccessor.Query<SummaryItem>(typeof(SummaryItemForHQByTemplate).Name);
+            }
+
+            if (input.UserId.HasValue)
+            {
+                if (input.ViewerStatus == ViewerStatus.Headquarter)
                 {
-                    Template = new TemplateLight(y.TemplateId, y.TemplateName),
-                    Approved = y.ApprovedCount,
-                    Completed = y.CompletedCount,
-                    Error = y.CompletedWithErrorsCount,
-                    Initial = y.InitialCount,
-                    Redo = y.RedoCount,
-                    Unassigned = y.UnassignedCount,
-                    Total = y.TotalCount
-                }).ToList();
+                    items = items.Where(x => x.ResponsibleSupervisorId == input.UserId);
+                }
+                else
+                {
+                    items = items.Where(x => x.ResponsibleId == input.UserId);
+                }
 
-                    return new SurveysView()
-                    {
-                        TotalCount = all.Count(),
-                        Items =
-                            all,
-                        ItemsSummary = new SurveysViewItem(
-                            new TemplateLight(Guid.Empty, "Summary"),
-                            all.Sum(x => x.Total),
-                            all.Sum(x => x.Initial),
-                            all.Sum(x => x.Error),
-                            all.Sum(x => x.Completed),
-                            all.Sum(x => x.Approved),
-                            all.Sum(x => x.Redo),
-                            all.Sum(x => x.Unassigned))
-                    };
-               // });
+            }
+            else
+            {
+                items = items.Where(x => x.ResponsibleId == Guid.Empty);
+            }
+
+            var all = items.OrderUsingSortExpression(input.Order).Skip((input.Page - 1) * input.PageSize).Take(input.PageSize).Select(y => new SurveysViewItem()
+        {
+            Template = new TemplateLight(y.TemplateId, y.TemplateName),
+            Approved = y.ApprovedCount,
+            Completed = y.CompletedCount,
+            Error = y.CompletedWithErrorsCount,
+            Initial = y.InitialCount,
+            Redo = y.RedoCount,
+            Unassigned = y.UnassignedCount,
+            Total = y.TotalCount
+        }).ToList();
+
+            return new SurveysView()
+            {
+                TotalCount = all.Count(),
+                Items =
+                    all,
+                ItemsSummary = new SurveysViewItem(
+                    new TemplateLight(Guid.Empty, "Summary"),
+                    all.Sum(x => x.Total),
+                    all.Sum(x => x.Initial),
+                    all.Sum(x => x.Error),
+                    all.Sum(x => x.Completed),
+                    all.Sum(x => x.Approved),
+                    all.Sum(x => x.Redo),
+                    all.Sum(x => x.Unassigned))
+            };
+            // });
         }
     }
 }
