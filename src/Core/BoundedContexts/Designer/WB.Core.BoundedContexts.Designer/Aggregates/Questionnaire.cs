2ad9fae9766757d9157299da1ea3d0837c27c507
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2013-07-17 20:17:37 +0300

    BoundedContexts.Designer/Questionnaire: removed interview creation methods
    --HG--
    branch : data-collection-shared-kernel
    
    
    Former-commit-id: 88877d046c354f9b0ac7005c3d8962d7fac8eeb4

--- src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Aggregates/Questionnaire.cs
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Aggregates/Questionnaire.cs
@@ -166,21 +166,6 @@ namespace WB.Core.BoundedContexts.Designer.Aggregates
         }
 
 
-        public void CreateInterviewWithFeaturedQuestions(Guid interviewId, UserLight creator, UserLight responsible, List<QuestionAnswer> featuredAnswers)
-        #warning probably a factory should be used here
-        {
-            // TODO: check is it good to create new AR form another?
-            new CompleteQuestionnaireAR(interviewId, this.innerDocument, creator, responsible, featuredAnswers);
-        }
-
-        public void CreateCompletedQ(Guid completeQuestionnaireId, UserLight creator)
-        #warning probably a factory should be used here
-        {
-            // TODO: check is it good to create new AR form another?
-            // Do we need Saga here?
-            new CompleteQuestionnaireAR(completeQuestionnaireId, this.innerDocument, creator);
-        }
-
         public void DeleteImage(Guid questionKey, Guid imageKey)
         {
             this.ApplyEvent(new ImageDeleted { ImageKey = imageKey, QuestionKey = questionKey });
