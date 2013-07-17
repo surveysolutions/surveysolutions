50bf7631b0c3bd99c9f8dce53b4fb745bde7d365
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2013-07-17 18:12:05 +0300

    Tests: questionnaire denormalizer tests moved to designer bounded context tests
    --HG--
    branch : data-collection-shared-kernel
    
    
    Former-commit-id: dbbe6a7bdb4a82caa7d4a6a950919e4a4776032c

---
+++ src/Tests/WB.Core.BoundedContexts.Designer.Tests/QuestionnaireDenormalizerTests/QuestionnaireDenormalizerTestsContext.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using Machine.Specifications;
+using Main.Core.AbstractFactories;
+using Main.Core.Documents;
+using Main.Core.Entities.Composite;
+using Main.Core.Entities.SubEntities;
+using Main.Core.Entities.SubEntities.Question;
+using Main.Core.Events.Questionnaire;
+using Moq;
+using Ncqrs.Eventing.ServiceModel.Bus;
+using WB.Core.BoundedContexts.Designer.Views.Questionnaire.Document;
+using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
+
+namespace WB.Core.BoundedContexts.Designer.Tests.QuestionnaireDenormalizerTests
+{
+    [Subject(typeof(QuestionnaireDenormalizer))]
+    internal class QuestionnaireDenormalizerTestsContext
+    {
+        protected static IPublishedEvent<T> ToPublishedEvent<T>(T @event)
+            where T : class
+        {
+            return Mock.Of<IPublishedEvent<T>>(publishedEvent
+                => publishedEvent.Payload == @event);
+        }
+
+        protected static QuestionnaireDenormalizer CreateQuestionnaireDenormalizer(
+            IReadSideRepositoryWriter<QuestionnaireDocument> documentStorage = null, ICompleteQuestionFactory questionFactory = null)
+        {
+            return new QuestionnaireDenormalizer(
+                documentStorage ?? Mock.Of<IReadSideRepositoryWriter<QuestionnaireDocument>>(),
+                questionFactory ?? Mock.Of<ICompleteQuestionFactory>());
+        }
+
+        protected static QuestionnaireDocument CreateQuestionnaireDocument(
+            IEnumerable<IComposite> children = null)
+        {
+            var questionnaire = new QuestionnaireDocument();
+
+            if (children != null)
+            {
+                questionnaire.Children.AddRange(children);
+            }
+
+            return questionnaire;
+        }
+
+        protected static Group CreateGroup(Guid? groupId = null, string title = "Group X",
+            IEnumerable<IComposite> children = null)
+        {
+            var group = new Group
+            {
+                PublicKey = groupId ?? Guid.NewGuid(),
+                Title = title,
+            };
+
+            if (children != null)
+            {
+                group.Children.AddRange(children);
+            }
+
+            return group;
+        }
+
+        protected static AbstractQuestion CreateQuestion(Guid? questionId = null, string title = null)
+        {
+            return new TextQuestion
+            {
+                PublicKey = questionId ?? Guid.NewGuid(),
+                QuestionText = title,
+                QuestionType = QuestionType.Text,
+            };
+        }
+
+        protected static IPublishedEvent<GroupDeleted> CreateGroupDeletedEvent(Guid groupId)
+        {
+            return ToPublishedEvent(new GroupDeleted
+            {
+                GroupPublicKey = groupId,
+            });
+        }
+
+        protected static IPublishedEvent<QuestionDeleted> CreateQuestionDeletedEvent(Guid questionId)
+        {
+            return ToPublishedEvent(new QuestionDeleted
+            {
+                QuestionId = questionId,
+            });
+        }
+
+        protected static IPublishedEvent<NewGroupAdded> CreateNewGroupAddedEvent(Guid groupId, string title = "New Group X")
+        {
+            return ToPublishedEvent(new NewGroupAdded
+            {
+                PublicKey = groupId,
+                GroupText = title,
+            });
+        }
+
+        protected static IPublishedEvent<NewQuestionAdded> CreateNewQuestionAddedEvent(Guid questionId, Guid? groupId = null, string title = "New Question X")
+        {
+            return ToPublishedEvent(new NewQuestionAdded
+            {
+                PublicKey = questionId,
+                GroupPublicKey = groupId,
+                QuestionText = title,
+                QuestionType = QuestionType.Numeric,
+            });
+        }
+
+        protected static IPublishedEvent<GroupUpdated> CreateGroupUpdatedEvent(Guid groupId, string title)
+        {
+            return ToPublishedEvent(new GroupUpdated
+            {
+                GroupPublicKey = groupId,
+                GroupText = title,
+            });
+        }
+
+        protected static IPublishedEvent<QuestionChanged> CreateQuestionChangedEvent(Guid questionId, string title)
+        {
+            return ToPublishedEvent(new QuestionChanged
+            {
+                PublicKey = questionId,
+                QuestionText = title,
+            });
+        }
+
+        protected static IPublishedEvent<QuestionnaireItemMoved> CreateQuestionnaireItemMovedEvent(Guid itemId, Guid? targetGroupId)
+        {
+            return ToPublishedEvent(new QuestionnaireItemMoved
+            {
+                PublicKey = itemId,
+                GroupKey = targetGroupId,
+            });
+        }
+    }
+}
