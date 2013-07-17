a0c4839e232841f827ff1c5447f1cec18484105c
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2013-07-17 17:26:01 +0300

    SharedKernels\DataCollection: removed event handlers which are not available in context
    --HG--
    branch : data-collection-shared-kernel
    
    
    Former-commit-id: 181bf32009d9d828a5054a1c7211128e60e1477f

--- src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/Views/Questionnaire/BrowseItem/QuestionnaireBrowseItemDenormalizer.cs
+++ src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/Views/Questionnaire/BrowseItem/QuestionnaireBrowseItemDenormalizer.cs
@@ -6,11 +6,7 @@ using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
 
 namespace WB.Core.SharedKernels.DataCollection.Views.Questionnaire.BrowseItem
 {
-    public class QuestionnaireBrowseItemDenormalizer :
-        IEventHandler<NewQuestionnaireCreated>,
-        IEventHandler<TemplateImported>,
-        IEventHandler<QuestionnaireUpdated>,
-        IEventHandler<QuestionnaireDeleted>
+    public class QuestionnaireBrowseItemDenormalizer : IEventHandler<TemplateImported>
     {
         private readonly IReadSideRepositoryWriter<QuestionnaireBrowseItem> documentStorage;
 
@@ -19,41 +15,10 @@ namespace WB.Core.SharedKernels.DataCollection.Views.Questionnaire.BrowseItem
             this.documentStorage = documentStorage;
         }
 
-        public void Handle(IPublishedEvent<NewQuestionnaireCreated> evnt)
-        {
-            this.documentStorage.Store(
-                new QuestionnaireBrowseItem(
-                    evnt.EventSourceId,
-                    evnt.Payload.Title,
-                    evnt.Payload.CreationDate,
-                    DateTime.Now,
-                    evnt.Payload.CreatedBy,
-                    evnt.Payload.IsPublic),
-                evnt.EventSourceId);
-        }
-
-        public void Handle(IPublishedEvent<QuestionnaireUpdated> evnt)
-        {
-            var browseItem = this.documentStorage.GetById(evnt.EventSourceId);
-            if (browseItem != null)
-            {
-                browseItem.Title = evnt.Payload.Title;
-                browseItem.IsPublic = evnt.Payload.IsPublic;
-            }
-        }
-
-        public void Handle(IPublishedEvent<QuestionnaireDeleted> evnt)
-        {
-            var browseItem = this.documentStorage.GetById(evnt.EventSourceId);
-            if (browseItem != null)
-            {
-                browseItem.IsDeleted = true;
-            }
-        }
-
         public void Handle(IPublishedEvent<TemplateImported> evnt)
         {
             var document = evnt.Payload.Source;
+
             var browseItem = new QuestionnaireBrowseItem(
                 document.PublicKey,
                 document.Title,
@@ -62,7 +27,6 @@ namespace WB.Core.SharedKernels.DataCollection.Views.Questionnaire.BrowseItem
                 document.CreatedBy,
                 document.IsPublic);
 
-            //}
             this.documentStorage.Store(browseItem, document.PublicKey);
         }
     }
