d971e6fbc6dc07d6a9e96835a24cbf6f2bb33fc4
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2013-07-19 00:40:07 -0400

    fix tests
    Former-commit-id: 9bbbdfa1fc8269f36acf720dbb144f0a8ee1f97a

---
+++ src/Tests/WB.Core.Synchronization.Tests/SyncManagerTests/SyncManagerTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Moq;
+using NUnit.Framework;
+using WB.Core.Synchronization.Implementation.SyncManager;
+using WB.Core.Synchronization.SyncProvider;
+
+namespace WB.Core.Synchronization.Tests.SyncManagerTests
+{
+    [TestFixture]
+    class SyncManagerTests
+    {
+        [Test]
+        public void ItitSync_when_New_ClientIdentifier_Arrived_New_Device_Is_Stored()
+        {
+            //Arrange
+            var clientIdentifier = new ClientIdentifier()
+                {
+                    ClientDeviceKey = "device1",
+                    ClientRegistrationKey = null,
+                    ClientVersionIdentifier = "v1",
+                    ClientInstanceKey = Guid.NewGuid(),
+                    CurrentProcessKey = null,
+                    LastSyncKey = null
+                };
+            var syncProvider = new Mock<ISyncProvider>();
+
+            ISyncManager manager = CreateDefaultSyncManager(syncProvider.Object);
+
+            //Act
+            var result = manager.ItitSync(clientIdentifier);
+
+            //Assert
+            syncProvider.Verify(x => x.CheckAndCreateNewSyncActivity(It.IsAny<ClientIdentifier>()), Times.Once());
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result.ClientInstanceKey, Is.EqualTo(clientIdentifier.ClientInstanceKey));
+
+        }
+
+
+        private SyncManager CreateDefaultSyncManager(ISyncProvider syncProvider)
+        {
+            return new SyncManager(syncProvider);
+        }
+    }
+}
