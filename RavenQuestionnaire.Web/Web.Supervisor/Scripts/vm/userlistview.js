259f56e0c8231c43e04782a92101e1aaa3590607
Slava Scherbak <slava.scherbak@gmail.com>
2013-07-24 15:02:04 +0300

    Supervisor\UI: added icon font and notification about successfull interview creation
    --HG--
    branch : next
    
    
    Former-commit-id: 80b60c41045fea0e94cd4438641c13d63f9fc528

--- RavenQuestionnaire.Web/Web.Supervisor/Scripts/vm/userlistview.js
+++ RavenQuestionnaire.Web/Web.Supervisor/Scripts/vm/userlistview.js
@@ -7,10 +7,17 @@
         self.ListView.ToggleFilter();
     };
     self.lock = function () {
+
+        self.ListView.CheckForRequestComplete();
+        
+        self.ListView.IsAjaxComplete(false);
+        
         var user = this;
         $.post(self.LockUrl, { UserId: user.UserId, IsLocked: user.IsLocked }, null, "json")
             .done(function(o) {
                 user.IsLocked(!user.IsLocked());
+                
+                self.ListView.IsAjaxComplete(true);
             });
     };
 
