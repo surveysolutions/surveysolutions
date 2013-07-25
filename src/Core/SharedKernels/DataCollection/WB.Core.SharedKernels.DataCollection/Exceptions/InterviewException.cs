b80db3aa89cc91e878ed1b8c2a2408633a96150f
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2013-07-25 23:32:10 +0300

    DataCollection: InterviewException updated, constructors made internal
    --HG--
    branch : data-collection-shared-kernel
    
    
    Former-commit-id: ea5bc7eaa20289f4ffd9551aeea257c9389b0994

--- src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/Exceptions/InterviewException.cs
+++ src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/Exceptions/InterviewException.cs
@@ -7,18 +7,12 @@ using System.Threading.Tasks;
 
 namespace WB.Core.SharedKernels.DataCollection.Exceptions
 {
-    [Serializable]
     public class InterviewException : Exception
     {
-        public InterviewException() {}
-
-        public InterviewException(string message)
+        internal InterviewException(string message)
             : base(message) {}
 
-        public InterviewException(string message, Exception inner)
-            : base(message, inner) {}
-
-        protected InterviewException(SerializationInfo info, StreamingContext context)
-            : base(info, context) {}
+        internal InterviewException(string message, Exception innerException)
+            : base(message, innerException) {}
     }
 }
