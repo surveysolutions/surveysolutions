538cb1d56369d823e4d5b58360d99aeee23436d2
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2013-07-25 15:09:50 +0300

    DataCollection\QuestionnaireRepository: added method which returns latest version of questionnaire
    --HG--
    branch : data-collection-shared-kernel
    
    
    Former-commit-id: 7ff514315b437d8cd4cc1ef15877b526cd9336c7

--- src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/Implementation/Repositories/IQuestionnaireRepository.cs
+++ src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/Implementation/Repositories/IQuestionnaireRepository.cs
@@ -6,6 +6,11 @@ namespace WB.Core.SharedKernels.DataCollection.Implementation.Repositories
     internal interface IQuestionnaireRepository
     {
         /// <summary>
+        /// Returns latest version of questionnaire.
+        /// </summary>
+        IQuestionnaire GetQuestionnaire(Guid id);
+
+        /// <summary>
         /// Returns questionnaire in one of it's previous states specified by version.
         /// </summary>
         IQuestionnaire GetHistoricalQuestionnaire(Guid id, long version);
