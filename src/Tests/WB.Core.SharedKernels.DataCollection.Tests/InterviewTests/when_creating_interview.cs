bb263a596c3b407ebab392e31062f9aaf783f36e
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2013-07-25 17:14:54 +0300

    Tests\DataCollection\Interview: added empty test stub for ctor, installed mspec
    --HG--
    branch : data-collection-shared-kernel
    
    
    Former-commit-id: 72e68d9cbea3d14d2c976050867e844dfc25bd8c

---
+++ src/Tests/WB.Core.SharedKernels.DataCollection.Tests/InterviewTests/when_creating_interview.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WB.Core.SharedKernels.DataCollection.Tests.InterviewTests
+{
+    internal class when_creating_interview : InterviewTestsContext
+    {
+    }
+}
