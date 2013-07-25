de2aeb502183a8c81cb1133a3d450ae2a01fec86
Anatoliy 'TLK' Kolesnick <none@none>
2013-07-25 16:20:01 +0300

    DataCollection: added CreateInterview command and empty ctor handler
    --HG--
    branch : data-collection-shared-kernel
    
    
    Former-commit-id: bf7bd41ca5c8cfe07f0b538d52f37e6ec537e11f

---
+++ src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/Implementation/Aggregates/Interview.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ncqrs.Domain;
+
+namespace WB.Core.SharedKernels.DataCollection.Implementation.Aggregates
+{
+    internal class Interview : AggregateRootMappedByConvention
+    {
+        public Interview(Guid questionnaireId)
+        {
+        }
+    }
+}
