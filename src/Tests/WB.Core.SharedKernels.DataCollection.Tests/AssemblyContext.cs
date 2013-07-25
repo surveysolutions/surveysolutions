e2d8a79e8c7e441e4e8fbf9d40f074b80bdc166d
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2013-07-25 23:21:58 +0300

    Tests\DataCollection\Interview: added ctor test requiring to check existance of questionnaire
    --HG--
    branch : data-collection-shared-kernel
    
    
    Former-commit-id: a49ec76f1184ddeed44a150044f1e6c49ddeded5

---
+++ src/Tests/WB.Core.SharedKernels.DataCollection.Tests/AssemblyContext.cs
@@ -0,0 +1,16 @@
+﻿using Machine.Specifications;
+using Microsoft.Practices.ServiceLocation;
+using Moq;
+
+namespace WB.Core.SharedKernels.DataCollection.Tests
+{
+    public class AssemblyContext : IAssemblyContext
+    {
+        public void OnAssemblyStart()
+        {
+            ServiceLocator.SetLocatorProvider(() => new Mock<IServiceLocator> { DefaultValue = DefaultValue.Mock }.Object);
+        }
+
+        public void OnAssemblyComplete() {}
+    }
+}
