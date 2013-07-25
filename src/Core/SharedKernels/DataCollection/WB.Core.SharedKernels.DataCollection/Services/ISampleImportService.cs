1fcef537b8cf14ecc0819c9c0d3d5d46eb360ec2
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2013-07-25 13:14:43 -0400

    new branch was created in order to implement csv sample import
    --HG--
    branch : csv-import-tool-migration
    
    
    Former-commit-id: d18b04b9a3d693f99b227f528c00865ea0500c0b

---
+++ src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/Services/ISampleImportService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Main.Core.Documents;
+
+namespace WB.Core.SharedKernels.DataCollection.Services
+{
+    public interface ISampleImportService
+    {
+        IEnumerable<CompleteQuestionnaireDocument> GetSampleList(Guid templateId, TextReader textReader);
+    }
+}
---
+++ src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/Services/TempFileImportData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WB.Core.Infrastructure.ReadSide;
+
+namespace WB.Core.SharedKernels.DataCollection.Services
+{
+    public class TempFileImportData:IView
+    {
+        public TempFileImportData(Guid publicKey, Guid templateId, string[][] values, string[] header)
+        {
+            PublicKey = publicKey;
+            TemplateId = templateId;
+            Values = values;
+            Header = header;
+        }
+
+        public TempFileImportData()
+        {
+        }
+
+        public Guid PublicKey { get;  set; }
+        public Guid TemplateId { get;  set; }
+        public string[][] Values { get;  set; }
+        public string[] Header { get;  set; }
+    }
+}
