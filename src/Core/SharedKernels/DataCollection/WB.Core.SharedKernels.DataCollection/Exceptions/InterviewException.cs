d78c8ea6b40ae8b54b70ea83e4b4047d8c79092d
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2013-07-25 23:21:58 +0300

    Tests\DataCollection\Interview: added ctor test requiring to check existance of questionnaire
    --HG--
    branch : data-collection-shared-kernel
    
    
    Former-commit-id: a49ec76f1184ddeed44a150044f1e6c49ddeded5

---
+++ src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/Exceptions/InterviewException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WB.Core.SharedKernels.DataCollection.Exceptions
+{
+    [Serializable]
+    public class InterviewException : Exception
+    {
+        public InterviewException() {}
+
+        public InterviewException(string message)
+            : base(message) {}
+
+        public InterviewException(string message, Exception inner)
+            : base(message, inner) {}
+
+        protected InterviewException(SerializationInfo info, StreamingContext context)
+            : base(info, context) {}
+    }
+}
