5050071f06e9c20843730cb7bfda606cf9c0d547
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2013-07-26 16:51:47 -0400

    Supervisor\Core: first dirty version of csv parse
    --HG--
    branch : csv-import-tool-migration
    
    
    Former-commit-id: 1d8ed7b7aa2c9dc22b66a5143983a66a90ad0980

---
+++ src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/Services/ImportResult.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WB.Core.SharedKernels.DataCollection.Services
+{
+    public class ImportResult
+    {
+        public ImportResult(Guid id, bool isCompleted, string errorMessage, string[] header, List<string[]> values)
+        {
+            Id = id;
+            IsCompleted = isCompleted;
+            ErrorMessage = errorMessage;
+            Header = header;
+            if (values != null)
+                Values = values.ToArray();
+        }
+
+        public Guid Id { get; private set; }
+        public bool IsCompleted { get; private set; }
+        public string ErrorMessage { get; private set; }
+        public string[] Header { get; private set; }
+        public string[][] Values { get; private set; }
+
+        public bool IsSuccessed {
+            get { return string.IsNullOrEmpty(this.ErrorMessage); }
+        }
+    }
+}
--- src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/Services/TempFileImportData.cs
+++ src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/Services/TempFileImportData.cs
@@ -4,26 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using WB.Core.Infrastructure.ReadSide;
+using WB.Core.SharedKernels.DataCollection.Views.Questionnaire;
 
 namespace WB.Core.SharedKernels.DataCollection.Services
 {
     public class TempFileImportData:IView
     {
-        public TempFileImportData(Guid publicKey, Guid templateId, string[][] values, string[] header)
-        {
-            PublicKey = publicKey;
-            TemplateId = templateId;
-            Values = values;
-            Header = header;
-        }
 
-        public TempFileImportData()
+        public void CompleteImport()
         {
+            IsCompleted = true;
+        }
+        
+        public void AddValueBatch(string[][] values)
+        {
+            if(Values==null)
+                this.Values=new List<string[]>();
+            for (int i = 0; i < values.Length; i++)
+            {
+                Values.Add(values[i]);
+            }
         }
 
         public Guid PublicKey { get;  set; }
         public Guid TemplateId { get;  set; }
-        public string[][] Values { get;  set; }
-        public string[] Header { get;  set; }
+        public List<string[]> Values { get;  set; }
+        public string[]  Header { get;  set; }
+        public bool IsCompleted { get; set; }
+        public string ErrorMassage { get; set; }
     }
 }
