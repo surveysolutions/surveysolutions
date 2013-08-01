4a81293b37b1e31dd28e4ae25cb2518ae3d87f32
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2013-08-01 20:01:25 +0300

    Interview: finished creation functionality
    --HG--
    branch : data-collection-shared-kernel
    
    
    Former-commit-id: 2ccdbe1809db67379c34614522813f6577fb2173

---
+++ src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/Events/Interview/InterviewCreated.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WB.Core.SharedKernels.DataCollection.Events.Interview
+{
+    public class InterviewCreated
+    {
+        public Guid QuestionnaireId { get; private set; }
+        public long Version { get; private set; }
+
+        public InterviewCreated(Guid questionnaireId, long version)
+        {
+            this.QuestionnaireId = questionnaireId;
+            this.Version = version;
+        }
+    }
+}
