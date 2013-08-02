c2c6de52c0a72d12d5156b8ba61ea80d9678cc17
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2013-08-02 22:33:48 +0300

    ReadSide: added QueryAll method to IReadSideRepositoryWriter
    --HG--
    branch : next
    
    
    Former-commit-id: 79c55a9336e399cbc6b7b9287c793283d8b7f89b

--- src/Core/Infrastructure/WB.Core.Infrastructure/ReadSide/Repository/Accessors/IQueryableReadSideRepositoryWriter.cs
+++ src/Core/Infrastructure/WB.Core.Infrastructure/ReadSide/Repository/Accessors/IQueryableReadSideRepositoryWriter.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace WB.Core.Infrastructure.ReadSide.Repository.Accessors
@@ -8,5 +9,8 @@ namespace WB.Core.Infrastructure.ReadSide.Repository.Accessors
     public interface IQueryableReadSideRepositoryWriter<TEntity> : IReadSideRepositoryWriter<TEntity> where TEntity : class, IReadSideRepositoryEntity
     {
         TResult Query<TResult>(Func<IQueryable<TEntity>, TResult> query);
+
+        #warning this method is here only because of our problems with Raven
+        IEnumerable<TEntity> QueryAll(Expression<Func<TEntity, bool>> query);
     }
 }
