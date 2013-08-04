390e004832d9ce572021e79afe8a140e7e8be78a
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2013-08-04 14:25:57 +0300

    Interview\Events: added validation events
    --HG--
    branch : data-collection-shared-kernel
    
    
    Former-commit-id: dab0f032ef20d4e44dc7ef868a706ec0da9e1df1

--- src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/Events/Interview/Base/QuestionAnswered.cs
+++ src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/Events/Interview/Base/QuestionAnswered.cs
@@ -2,15 +2,13 @@
 
 namespace WB.Core.SharedKernels.DataCollection.Events.Interview.Base
 {
-    public abstract class QuestionAnswered : InterviewEvent
+    public abstract class QuestionAnswered : AnswerActiveEvent
     {
-        public Guid QuestionId { get; private set; }
         public DateTime AnswerTime { get; private set; }
 
         protected QuestionAnswered(Guid userId, Guid questionId, DateTime answerTime)
-            : base(userId)
+            : base(userId, questionId)
         {
-            this.QuestionId = questionId;
             this.AnswerTime = answerTime;
         }
     }
