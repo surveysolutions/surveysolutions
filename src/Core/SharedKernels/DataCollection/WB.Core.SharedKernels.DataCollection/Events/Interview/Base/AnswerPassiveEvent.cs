1445bd324d9152bcf878e69b206ee8722fff7e91
Anatoliy 'TLK' Kolesnick <none@none>
2013-08-04 14:25:57 +0300

    Interview\Events: added validation events
    --HG--
    branch : data-collection-shared-kernel
    
    
    Former-commit-id: dab0f032ef20d4e44dc7ef868a706ec0da9e1df1

---
+++ src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/Events/Interview/Base/AnswerPassiveEvent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WB.Core.SharedKernels.DataCollection.Events.Interview.Base
+{
+    public abstract class AnswerPassiveEvent : InterviewPassiveEvent
+    {
+        public Guid QuestionId { get; private set; }
+
+        protected AnswerPassiveEvent(Guid questionId)
+        {
+            this.QuestionId = questionId;
+        }
+    }
+}
