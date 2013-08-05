49a65312e01a99607f9ccd9bba2fbd8931ac7fd8
Anatoliy 'TLK' Kolesnick <none@none>
2013-08-05 17:13:06 +0300

    DataCollection: added expression processor which can extract used identifiers from expressions (covered with test)
    --HG--
    branch : data-collection-shared-kernel
    
    
    Former-commit-id: 88f984b89e17aef087ec5f1c0baff698c37dedeb

---
+++ src/Tests/WB.Core.SharedKernels.DataCollection.Tests/ExpressionProcessorTests/ExpressionProcessorTestsContext.cs
@@ -0,0 +1,12 @@
+﻿using WB.Core.SharedKernels.DataCollection.Implementation.Services;
+
+namespace WB.Core.SharedKernels.DataCollection.Tests.ExpressionProcessorTests
+{
+    internal class ExpressionProcessorTestsContext
+    {
+        protected static ExpressionProcessor CreateExpressionProcessor()
+        {
+            return new ExpressionProcessor();
+        }
+    }
+}
