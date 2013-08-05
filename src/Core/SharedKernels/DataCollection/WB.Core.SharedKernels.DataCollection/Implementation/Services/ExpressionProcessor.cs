0aa20a3d87ec0cb3a1c64df25261af7a18117fda
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2013-08-05 17:13:06 +0300

    DataCollection: added expression processor which can extract used identifiers from expressions (covered with test)
    --HG--
    branch : data-collection-shared-kernel
    
    
    Former-commit-id: 88f984b89e17aef087ec5f1c0baff698c37dedeb

---
+++ src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/Implementation/Services/ExpressionProcessor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NCalc;
+using NCalc.Domain;
+
+namespace WB.Core.SharedKernels.DataCollection.Implementation.Services
+{
+    internal class ExpressionProcessor : IExpressionProcessor
+    {
+        public IEnumerable<string> GetIdentifiersUsedInExpression(string expression)
+        {
+            LogicalExpression parsedExpression = ParseExpressionOrThrow(expression);
+
+            var identifierCollector = new IdentifierCollector();
+
+            parsedExpression.Accept(identifierCollector);
+
+            return identifierCollector.GetCollectedIdentifiers();
+        }
+
+        private static LogicalExpression ParseExpressionOrThrow(string stringExpression)
+        {
+            var expression = new Expression(stringExpression);
+
+            bool hasErrors = expression.HasErrors(); // HasErrors method forces expression to be parsed (initializes ParsedExpression property)
+
+            #warning TLK: decide do we need to throw exceptions in such cases or log it as warning and ignore
+            if (hasErrors)
+                throw new ArgumentException(string.Format("Failed to parse following expression: '{0}'.", stringExpression));
+
+            return expression.ParsedExpression;
+        }
+    }
+}
---
+++ src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/Implementation/Services/IExpressionProcessor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WB.Core.SharedKernels.DataCollection.Implementation.Services
+{
+    internal interface IExpressionProcessor
+    {
+        IEnumerable<string> GetIdentifiersUsedInExpression(string expression);
+    }
+}
