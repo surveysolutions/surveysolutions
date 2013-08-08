e37a7a6b2adb72c4bf26419088640f0dc520537b
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2013-08-08 18:00:21 -0400

    open for editing and defalt value is empty
    --HG--
    branch : open-supervisor-connection-changing
    
    
    Former-commit-id: 9babc75c72092b67fd517be6b014a77554ba4980

--- Mono/CAPI.Android/Settings/SettingsManager.cs
+++ Mono/CAPI.Android/Settings/SettingsManager.cs
@@ -17,12 +17,12 @@ namespace CAPI.Android.Settings
         /// <summary>
         /// The remote sync node.
         /// </summary>
-        private const string RemoteSyncNode = 
-            "http://ec2-54-217-244-125.eu-west-1.compute.amazonaws.com/";
+        private const string RemoteSyncNode = "";
+        //  "http://ec2-54-217-244-125.eu-west-1.compute.amazonaws.com/";
         //"http://192.168.173.1:8000/";
         // "http://217.12.197.135/DEV-Supervisor/";
         
-        // "http://10.0.2.2";  //access to attached computer by cord
+        // "http://10.0.2.2";  //access to hosted computer from emulator
 
         
         #endregion
