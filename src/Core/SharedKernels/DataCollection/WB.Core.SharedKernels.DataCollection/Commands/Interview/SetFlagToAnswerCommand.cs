f95bf69ea16a7f69311206c420c213363e5b5e23
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2013-08-09 14:36:11 +0300

    Interview: introduced propagation vector to commands and their handlers KP-2373
    Former-commit-id: e09447cb1ea31bf52c32a5eb09d0af0d2f1eb23b

--- src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/Commands/Interview/SetFlagToAnswerCommand.cs
+++ src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/Commands/Interview/SetFlagToAnswerCommand.cs
@@ -7,7 +7,7 @@ namespace WB.Core.SharedKernels.DataCollection.Commands.Interview
     [MapsToAggregateRootMethod(typeof(Implementation.Aggregates.Interview), "SetFlagToAnswer")]
     public class SetFlagToAnswerCommand : QuestionCommand
     {
-        public SetFlagToAnswerCommand(Guid interviewId, Guid userId, Guid questionId)
-            : base(interviewId, userId, questionId) {}
+        public SetFlagToAnswerCommand(Guid interviewId, Guid userId, Guid questionId, int[] propagationVector)
+            : base(interviewId, userId, questionId, propagationVector) {}
     }
 }
