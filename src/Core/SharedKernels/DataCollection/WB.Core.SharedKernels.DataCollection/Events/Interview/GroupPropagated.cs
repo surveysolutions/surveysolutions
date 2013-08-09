64af18aed9eb93a9ec9951ba883937308f4c6c62
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2013-08-09 13:12:07 +0300

    Interview: added firing of group propagated events and checks on input values (needs new methods in IQuestionnaire) KP-2373
    Former-commit-id: 15473c4ac930536847a355630aaeea30c0934420

---
+++ src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/Events/Interview/GroupPropagated.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using WB.Core.SharedKernels.DataCollection.Events.Interview.Base;
+
+namespace WB.Core.SharedKernels.DataCollection.Events.Interview
+{
+    public class GroupPropagated : GroupPassiveEvent
+    {
+        public int Count { get; set; }
+
+        public GroupPropagated(Guid groupId, int count)
+            : base(groupId)
+        {
+            this.Count = count;
+        }
+    }
+}
