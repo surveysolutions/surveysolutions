e61e6de56bc9ba7d9c48720631a553e1e4e76efb
Anatoliy 'TLK' Kolesnick <none@none>
2013-08-10 23:21:58 +0300

    Interview: implemented assignments of interviewers and supervisors KP-2414 KP-2413
    Former-commit-id: d943a0c0a3a24a644a0fa550c4cf30dc73fd4b62

---
+++ src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/Commands/Interview/AssignInterviewerCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Ncqrs.Commanding.CommandExecution.Mapping.Attributes;
+using WB.Core.SharedKernels.DataCollection.Commands.Interview.Base;
+
+namespace WB.Core.SharedKernels.DataCollection.Commands.Interview
+{
+    [MapsToAggregateRootMethod(typeof(Implementation.Aggregates.Interview), "AssignInterviewer")]
+    public class AssignInterviewerCommand : InterviewCommand
+    {
+        public Guid InterviewerId { get; private set; }
+
+        public AssignInterviewerCommand(Guid interviewId, Guid userId, Guid interviewerId)
+            : base(interviewId, userId)
+        {
+            this.InterviewerId = interviewerId;
+        }
+    }
+}
---
+++ src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/Events/Interview/SupervisorAssigned.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using WB.Core.SharedKernels.DataCollection.Events.Interview.Base;
+
+namespace WB.Core.SharedKernels.DataCollection.Events.Interview
+{
+    public class SupervisorAssigned : InterviewActiveEvent
+    {
+        public Guid SupervisorId { get; private set; }
+
+        public SupervisorAssigned(Guid userId, Guid supervisorId)
+            : base(userId)
+        {
+            this.SupervisorId = supervisorId;
+        }
+    }
+}
