5cfab0dbaf89a58d987208d2f9712e4fb5fe1369
Anastasya Kholodova <nastya.kholodova@gmail.com>
2013-08-12 14:24:15 -0400

    Supervisor\Synchronization: interview synchronization event
    --HG--
    branch : apply-interview-aggregate-root
    
    
    Former-commit-id: b2e264b14eb7b60a44429118d9ce7abf629f0944

--- src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/DataTransferObjects/Synchronization/InterviewSynchronizationDto.cs
+++ src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/DataTransferObjects/Synchronization/InterviewSynchronizationDto.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using WB.Core.SharedKernels.DataCollection.ValueObjects.Interview;
 
 namespace WB.Core.SharedKernels.DataCollection.DataTransferObjects.Synchronization
 {
@@ -9,17 +10,17 @@ namespace WB.Core.SharedKernels.DataCollection.DataTransferObjects.Synchronizati
         {
         }
 
-        public InterviewSynchronizationDto(Guid id, Guid statusId, Guid userId, Guid questionnaireId, Dictionary<Guid, AnswerSynchronizationDto> answers)
+        public InterviewSynchronizationDto(Guid id, InterviewStatus status, Guid userId, Guid questionnaireId, Dictionary<Guid, AnswerSynchronizationDto> answers)
         {
             Id = id;
-            StatusId = statusId;
+            Status = status;
             UserId = userId;
             QuestionnaireId = questionnaireId;
             Answers = answers;
         }
 
         public Guid Id { get; private set; }
-        public Guid StatusId { get; private set; }
+        public InterviewStatus Status { get; private set; }
         public Guid UserId { get; private set; }
         public Guid QuestionnaireId { get; private set; }
         public Dictionary<Guid, AnswerSynchronizationDto> Answers { get; private set; }
