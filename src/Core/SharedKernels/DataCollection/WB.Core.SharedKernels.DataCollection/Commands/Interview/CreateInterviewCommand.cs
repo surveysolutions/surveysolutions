d9ef14821209e5112b42ff10e6e8154856de3009
Slava Scherbak <slava.scherbak@gmail.com>
2013-08-12 18:02:54 +0300

    InterviewAR: added supervisorId parameter to constructor because interview should be assigned on supervisor
    --HG--
    branch : apply-interview-aggregate-root
    
    
    Former-commit-id: 3c51410fd0149c6c2409321f8cbfb880595fbe03

--- src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/Commands/Interview/CreateInterviewCommand.cs
+++ src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/Commands/Interview/CreateInterviewCommand.cs
@@ -12,15 +12,17 @@ namespace WB.Core.SharedKernels.DataCollection.Commands.Interview
     public class CreateInterviewCommand : InterviewCommand
     {
         public Guid QuestionnaireId { get; private set; }
+        public Guid SupervisorId { get; private set; }
         public Dictionary<Guid, object> AnswersToFeaturedQuestions { get; private set; }
         public DateTime AnswersTime { get; private set; }
 
-        public CreateInterviewCommand(Guid interviewId, Guid userId, Guid questionnaireId, Dictionary<Guid, object> answersToFeaturedQuestions, DateTime answersTime)
+        public CreateInterviewCommand(Guid interviewId, Guid userId, Guid questionnaireId, Dictionary<Guid, object> answersToFeaturedQuestions, DateTime answersTime, Guid supervisorId)
             : base(interviewId, userId)
         {
             this.QuestionnaireId = questionnaireId;
             this.AnswersToFeaturedQuestions = answersToFeaturedQuestions;
             this.AnswersTime = answersTime;
+            this.SupervisorId = supervisorId;
         }
     }
 }
