50996fa94f5c3dc3fa47565dfb4b6a7a32d2bb6d
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2013-08-13 13:32:43 -0400

    Core\Sycnchronization: sycnronization dto is updated
    --HG--
    branch : apply-interview-aggregate-root
    
    
    Former-commit-id: 150596d3c0742c234dbe77df2d89b0a9f5ddb5d5

--- src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/DataTransferObjects/Synchronization/InterviewSynchronizationDto.cs
+++ src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/DataTransferObjects/Synchronization/InterviewSynchronizationDto.cs
@@ -10,19 +10,31 @@ namespace WB.Core.SharedKernels.DataCollection.DataTransferObjects.Synchronizati
         {
         }
 
-        public InterviewSynchronizationDto(Guid id, InterviewStatus status, Guid userId, Guid questionnaireId, Dictionary<Guid, AnswerSynchronizationDto> answers)
+        public InterviewSynchronizationDto(Guid id, InterviewStatus status, Guid userId, Guid questionnaireId,
+                                           IList<AnswerSynchronizationDto> answers,
+                                           HashSet<ItemPublicKey> disabledGroups,
+                                           HashSet<ItemPublicKey> disabledQuestions,
+                                           HashSet<ItemPublicKey> invalidAnsweredQuestions)
         {
             Id = id;
             Status = status;
             UserId = userId;
             QuestionnaireId = questionnaireId;
             Answers = answers;
+            DisabledGroups = disabledGroups;
+            DisabledQuestions = disabledQuestions;
+            InvalidAnsweredQuestions = invalidAnsweredQuestions;
         }
 
         public Guid Id { get; private set; }
         public InterviewStatus Status { get; private set; }
         public Guid UserId { get; private set; }
         public Guid QuestionnaireId { get; private set; }
-        public Dictionary<Guid, AnswerSynchronizationDto> Answers { get; private set; }
+        public IList<AnswerSynchronizationDto> Answers { get; private set; }
+        public HashSet<ItemPublicKey> DisabledGroups { get; private set; }
+        public HashSet<ItemPublicKey> DisabledQuestions { get; private set; }
+        public HashSet<ItemPublicKey> InvalidAnsweredQuestions { get; private set; }
+        /*    
+        private readonly Dictionary<string, int> propagatedGroupInstanceCounts = new Dictionary<string, int>();*/
     }
 }
