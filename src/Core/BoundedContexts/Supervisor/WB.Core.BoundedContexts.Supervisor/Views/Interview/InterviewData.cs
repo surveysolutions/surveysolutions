f6aa96a5aedc4d8eedf70f48b0ab543d5f39f655
Slava Scherbak <slava.scherbak@gmail.com>
2013-08-15 16:01:48 +0300

    BC\Supervisor: added InterviewSummaryDenormalize to show interviews  on HQ\Interview page
    --HG--
    branch : apply-interview-aggregate-root
    
    
    Former-commit-id: 76adacc7ecffec114712c132acfeb26ce4861f13

--- src/Core/BoundedContexts/Supervisor/WB.Core.BoundedContexts.Supervisor/Views/Interview/InterviewData.cs
+++ src/Core/BoundedContexts/Supervisor/WB.Core.BoundedContexts.Supervisor/Views/Interview/InterviewData.cs
@@ -5,18 +5,9 @@ using WB.Core.SharedKernels.DataCollection.ValueObjects.Interview;
 
 namespace WB.Core.BoundedContexts.Supervisor.Views.Interview
 {
-    public class InterviewData : IView
+    public class InterviewData : InterviewBrief
     {
-        public Guid InterviewId { get; set; }
-
-        public Guid QuestionnaireId { get; set; }
-        public long QuestionnaireVersion { get; set; }
-
-        public InterviewStatus Status { get; set; }
-
-        public Guid ResponsibleId { get; set; }
         public UserRoles ResponsibleRole { get; set; }
-
         public DateTime UpdateDate { get; set; }
     }
 }
---
+++ src/Core/BoundedContexts/Supervisor/WB.Core.BoundedContexts.Supervisor/Views/Interview/InterviewSummary.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using Main.Core.Entities.SubEntities;
+using WB.Core.Infrastructure.ReadSide;
+using WB.Core.SharedKernels.DataCollection.ValueObjects.Interview;
+using WB.Core.SharedKernels.DataCollection.Views.Questionnaire;
+
+namespace WB.Core.BoundedContexts.Supervisor.Views.Interview
+{
+    public class InterviewSummary : InterviewBrief  {
+
+        public InterviewSummary() {}
+
+        public InterviewSummary(QuestionnaireBrowseItem questionnaireSummary)
+        {
+            AnswersToFeaturedQuestions = new Dictionary<Guid, QuestionAnswer>();
+            foreach (var featuredQuestion in questionnaireSummary.FeaturedQuestions)
+            {
+                AnswersToFeaturedQuestions[featuredQuestion.Id] = new QuestionAnswer
+                    {
+                        Id = featuredQuestion.Id,
+                        Title = featuredQuestion.Title,
+                        Answer = string.Empty
+                    };
+            }
+        }
+
+        public string ResponsibleName { get; set; }
+        public UserRoles ResponsibleRole { get; set; }
+        public DateTime UpdateDate { get; set; }
+        public Dictionary<Guid, QuestionAnswer> AnswersToFeaturedQuestions { get; set; }
+    }
+
+    public class QuestionAnswer
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Answer { get; set; }
+    }
+}
