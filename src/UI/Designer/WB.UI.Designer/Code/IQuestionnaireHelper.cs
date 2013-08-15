65bd0889ed71300196cad3034d84501356cc7119
Roman Lytvyn <litvin.roma@gmail.com>
2013-08-15 17:46:03 +0300

    designer: shared status for questionnaires in "my questionnaires" list KP-2425
    --HG--
    branch : questionnaire-shared-persons
    
    
    Former-commit-id: 288116b398341962594eaf49398cd5c7d0ffb2a1

---
+++ src/UI/Designer/WB.UI.Designer/Code/IQuestionnaireHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using WB.UI.Designer.BootstrapSupport.HtmlHelpers;
+using WB.UI.Designer.Models;
+
+namespace WB.UI.Designer
+{
+    public interface IQuestionnaireHelper
+    {
+        IPagedList<QuestionnairePublicListViewModel> GetPublicQuestionnaires(
+            Guid viewerId, 
+            int? pageIndex = null, 
+            string sortBy = null, 
+            int? sortOrder = null, 
+            string filter = null);
+
+        IPagedList<QuestionnaireListViewModel> GetQuestionnaires(
+            Guid viewerId, 
+            int? pageIndex = null, 
+            string sortBy = null, 
+            int? sortOrder = null, 
+            string filter = null);
+
+        IPagedList<QuestionnaireListViewModel> GetQuestionnairesByViewerId(Guid viewerId);
+    }
+}
