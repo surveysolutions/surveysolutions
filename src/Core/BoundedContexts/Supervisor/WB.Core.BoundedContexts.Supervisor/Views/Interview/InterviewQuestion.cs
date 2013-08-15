41805beca2d739035fabedb283038f1171bd0c3b
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2013-08-15 17:59:23 -0400

    BoundedContext\Supervisor: InterviewData is epanded KP-2432
    --HG--
    branch : apply-interview-aggregate-root
    
    
    Former-commit-id: d0b5f7f05cfa742224b93af551873b1a75c938b3

---
+++ src/Core/BoundedContexts/Supervisor/WB.Core.BoundedContexts.Supervisor/Views/Interview/InterviewQuestion.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WB.Core.BoundedContexts.Supervisor.Views.Interview
+{
+    public class InterviewQuestion
+    {
+        public InterviewQuestion(Guid id)
+        {
+            Id = id;
+            Valid = true;
+            Enabled = true;
+            Flagged = false;
+        }
+
+        public InterviewQuestion(Guid id, object answer, string comments, bool valid, bool enabled, bool flagged)
+        {
+            Id = id;
+            Answer = answer;
+            Comments = comments;
+            Valid = valid;
+            Enabled = enabled;
+            Flagged = flagged;
+        }
+
+        public Guid Id { get; private set; }
+        public object Answer { get;  set; }
+        public string Comments { get;  set; }
+        public bool Valid { get;  set; }
+        public bool Enabled { get;  set; }
+        public bool Flagged { get;  set; }
+
+        
+    }
+}
