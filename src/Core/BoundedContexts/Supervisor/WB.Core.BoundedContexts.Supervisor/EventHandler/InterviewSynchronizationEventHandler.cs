f741320cc21ea5de8e65dd8d131463af0e537e04
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2013-08-19 12:06:57 -0400

    BoundedContext\Supervisor: handling synchronization of interviews
    --HG--
    branch : apply-interview-aggregate-root
    
    
    Former-commit-id: da3d26aea24ecf3919296536d48cf02bf880805b

--- src/Core/BoundedContexts/Supervisor/WB.Core.BoundedContexts.Supervisor/EventHandler/InterviewSynchronizationEventHandler.cs
+++ src/Core/BoundedContexts/Supervisor/WB.Core.BoundedContexts.Supervisor/EventHandler/InterviewSynchronizationEventHandler.cs
@@ -7,9 +7,12 @@ using Main.Core.Documents;
 using Main.Core.Events.Questionnaire.Completed;
 using Ncqrs.Eventing.ServiceModel.Bus;
 using Ncqrs.Eventing.ServiceModel.Bus.ViewConstructorEventBus;
+using WB.Core.BoundedContexts.Supervisor.Views.Interview;
+using WB.Core.BoundedContexts.Supervisor.Views.Questionnaire;
 using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
 using WB.Core.SharedKernels.DataCollection.DataTransferObjects.Synchronization;
 using WB.Core.SharedKernels.DataCollection.Events.Interview;
+using WB.Core.SharedKernels.DataCollection.ValueObjects.Interview;
 using WB.Core.Synchronization;
 using WB.Core.Synchronization.SyncStorage;
 using InterviewDeleted = Main.Core.Events.Questionnaire.Completed.InterviewDeleted;
@@ -23,21 +26,112 @@ namespace WB.Core.BoundedContexts.Supervisor.EventHandler
                                                         IEventHandler
     {
         private readonly ISynchronizationDataStorage syncStorage;
-        // private readonly IReadSideRepositoryWriter<InterviewSynchronizationDto> interviewWriter;
+        private readonly IReadSideRepositoryWriter<InterviewData> interviewDataWriter;
+        private readonly IReadSideRepositoryWriter<QuestionnairePropagationStructure> questionnriePropagationStructures;
 
-        public InterviewSynchronizationEventHandler(ISynchronizationDataStorage syncStorage)
+        public InterviewSynchronizationEventHandler(ISynchronizationDataStorage syncStorage,
+                                                    IReadSideRepositoryWriter<InterviewData> interviewDataWriter,
+                                                    IReadSideRepositoryWriter<QuestionnairePropagationStructure>
+                                                        questionnriePropagationStructures)
         {
             this.syncStorage = syncStorage;
+            this.interviewDataWriter = interviewDataWriter;
+            this.questionnriePropagationStructures = questionnriePropagationStructures;
         }
 
         public void Handle(IPublishedEvent<InterviewerAssigned> evnt)
         {
-            syncStorage.SaveInterview(new InterviewSynchronizationDto(), evnt.Payload.UserId);
+            var interview = interviewDataWriter.GetById(evnt.EventSourceId);
+
+            var interviewSyncData = BuildSynchronizationDto(interview);
+            
+            syncStorage.SaveInterview(interviewSyncData, evnt.Payload.UserId);
         }
 
         public void Handle(IPublishedEvent<InterviewRejected> evnt)
         {
-            syncStorage.SaveInterview(new InterviewSynchronizationDto(), evnt.Payload.UserId);
+            var interview = interviewDataWriter.GetById(evnt.EventSourceId);
+
+            var interviewSyncData = BuildSynchronizationDto(interview);
+
+            syncStorage.SaveInterview(interviewSyncData, evnt.Payload.UserId);
+        }
+
+        private InterviewSynchronizationDto BuildSynchronizationDto(InterviewData interview)
+        {
+            var answeredQuestions = new List<AnsweredQuestionSynchronizationDto>();
+            var disabledGroups = new HashSet<ItemPublicKey>();
+            var disabledQuestions = new HashSet<ItemPublicKey>();
+            var invalidQuestions = new HashSet<ItemPublicKey>();
+            var propagatedGroupInstanceCounts = new Dictionary<ItemPublicKey, int>();
+
+            var questionnariePropagationStructure = this.questionnriePropagationStructures.GetById(interview.QuestionnaireId);
+
+            foreach (var interviewLevel in interview.Levels.Values)
+            {
+                foreach (var interviewQuestion in interviewLevel.Questions)
+                {
+                    var answeredQuestion = new AnsweredQuestionSynchronizationDto(interviewQuestion.Id,
+                                                                                  interviewQuestion.Answer,
+                                                                                  interviewQuestion.Comments);
+                    answeredQuestions.Add(answeredQuestion);
+                    if (!interviewQuestion.Enabled)
+                        disabledQuestions.Add(new ItemPublicKey(interviewQuestion.Id, interviewLevel.PropagationVector));
+                    if (!interviewQuestion.Valid)
+                        invalidQuestions.Add(new ItemPublicKey(interviewQuestion.Id, interviewLevel.PropagationVector));
+                }
+                foreach (var disabledGroup in interviewLevel.DisabledGroups)
+                {
+                    disabledGroups.Add(new ItemPublicKey(disabledGroup, interviewLevel.PropagationVector));
+                }
+
+                FillPropagatedGroupInstancesOfCurrentLevelForQuestionnarie(questionnariePropagationStructure, interviewLevel, propagatedGroupInstanceCounts);
+            }
+            return new InterviewSynchronizationDto(interview.InterviewId,
+                                                   interview.Status,
+                                                   interview.ResponsibleId, interview.QuestionnaireId,
+                                                   answeredQuestions, disabledGroups, disabledQuestions,
+                                                   invalidQuestions, propagatedGroupInstanceCounts);
+        }
+
+        private void FillPropagatedGroupInstancesOfCurrentLevelForQuestionnarie(
+            QuestionnairePropagationStructure questionnariePropagationStructure, InterviewLevel interviewLevel,
+            Dictionary<ItemPublicKey, int> propagatedGroupInstanceCounts)
+        {
+            if (interviewLevel.PropagationVector.Length == 0)
+                return;
+
+            var outerVector = CreateOuterVector(interviewLevel);
+
+            foreach (var groupId in questionnariePropagationStructure.PropagationScopes[interviewLevel.ScopeId])
+            {
+                var groupKey = new ItemPublicKey(groupId, outerVector);
+
+                AddPropagatedGroupToDictionary(propagatedGroupInstanceCounts, groupKey);
+            }
+        }
+
+        private void AddPropagatedGroupToDictionary(Dictionary<ItemPublicKey, int> propagatedGroupInstanceCounts,
+                                                    ItemPublicKey groupKey)
+        {
+            if (propagatedGroupInstanceCounts.ContainsKey(groupKey))
+            {
+                propagatedGroupInstanceCounts[groupKey] = propagatedGroupInstanceCounts[groupKey] + 1;
+            }
+            else
+            {
+                propagatedGroupInstanceCounts.Add(groupKey, 1);
+            }
+        }
+
+        private int[] CreateOuterVector(InterviewLevel interviewLevel)
+        {
+            var outerVector = new int[interviewLevel.PropagationVector.Length - 1];
+            for (int i = 0; i < interviewLevel.PropagationVector.Length - 1; i++)
+            {
+                outerVector[i] = interviewLevel.PropagationVector[i];
+            }
+            return outerVector;
         }
 
         public void Handle(IPublishedEvent<InterviewCompleted> evnt)
@@ -56,7 +150,7 @@ namespace WB.Core.BoundedContexts.Supervisor.EventHandler
         }
 
         public Type[] UsesViews {
-            get { return new Type[] { }; }
+            get { return new Type[] {typeof (InterviewData), typeof (QuestionnairePropagationStructure)}; }
         }
 
         public Type[] BuildsViews
