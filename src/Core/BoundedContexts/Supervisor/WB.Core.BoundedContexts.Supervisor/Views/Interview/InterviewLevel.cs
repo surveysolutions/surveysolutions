32c31f792612434cf5cff8086a94a6a1ca3758b8
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2013-08-19 12:06:57 -0400

    BoundedContext\Supervisor: handling synchronization of interviews
    --HG--
    branch : apply-interview-aggregate-root
    
    
    Former-commit-id: da3d26aea24ecf3919296536d48cf02bf880805b

--- src/Core/BoundedContexts/Supervisor/WB.Core.BoundedContexts.Supervisor/Views/Interview/InterviewLevel.cs
+++ src/Core/BoundedContexts/Supervisor/WB.Core.BoundedContexts.Supervisor/Views/Interview/InterviewLevel.cs
@@ -8,13 +8,15 @@ namespace WB.Core.BoundedContexts.Supervisor.Views.Interview
 {
     public class InterviewLevel
     {
-        public InterviewLevel(Guid scopeId)
+        public InterviewLevel(Guid scopeId, int[] vector)
         {
             ScopeId = scopeId;
+            PropagationVector = vector;
             Questions = new List<InterviewQuestion>();
             DisabledGroups = new HashSet<Guid>();
         }
 
+        public int[] PropagationVector { get; private set; }
         public Guid ScopeId { get; private set; }
         public List<InterviewQuestion> Questions { get; private set; }
         public HashSet<Guid> DisabledGroups { get; private set; }
