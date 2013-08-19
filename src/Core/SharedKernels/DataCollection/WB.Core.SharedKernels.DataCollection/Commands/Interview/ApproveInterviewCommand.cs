a8794519b2c1296403610fe5df2ce934fe96f779
Slava Scherbak <slava.scherbak@gmail.com>
2013-08-19 19:59:45 +0300

    Supervisor\Supervisor: added command send on interview state change. reformatted controllers
    --HG--
    branch : apply-interview-aggregate-root
    
    
    Former-commit-id: b32fcb6111b87f9df5c4e55486235657d0d7891e

--- src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/Commands/Interview/ApproveInterviewCommand.cs
+++ src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/Commands/Interview/ApproveInterviewCommand.cs
@@ -7,7 +7,12 @@ namespace WB.Core.SharedKernels.DataCollection.Commands.Interview
     [MapsToAggregateRootMethod(typeof (Implementation.Aggregates.Interview), "Approve")]
     public class ApproveInterviewCommand : InterviewCommand
     {
-        public ApproveInterviewCommand(Guid interviewId, Guid userId)
-            : base(interviewId, userId) {}
+        public ApproveInterviewCommand(Guid interviewId, Guid userId, string comment)
+            : base(interviewId, userId)
+        {
+            this.Comment = comment;
+        }
+
+        public string Comment { get; set; }
     }
 }
