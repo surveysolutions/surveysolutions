efced036ed5e00abb095dfbadfaf895343365314
Anastasya Kholodova <nastya.kholodova@gmail.com>
2013-08-23 17:52:19 -0400

    Supervisor\UI: show all template versions on supervisor dashboard
    --HG--
    branch : apply-interview-aggregate-root
    
    
    Former-commit-id: 64ad9bbaeb18cddbc9064b8310d4db286a3f1ab9

--- src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/Views/Questionnaire/QuestionnaireBrowseViewFactory.cs
+++ src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/Views/Questionnaire/QuestionnaireBrowseViewFactory.cs
@@ -82,7 +82,7 @@ namespace WB.Core.SharedKernels.DataCollection.Views.Questionnaire
                 }
 
 #warning ReadLayer: ToList materialization because not supported by Raven
-                var queryResult = query.ToList().Distinct(new TemplateComparer()).AsQueryable().OrderUsingSortExpression(input.Order);
+                var queryResult = query.ToList().AsQueryable().OrderUsingSortExpression(input.Order);
 
                 var questionnaireItems = queryResult.Skip((input.Page - 1) * input.PageSize).Take(input.PageSize).ToArray();
 
@@ -92,20 +92,5 @@ namespace WB.Core.SharedKernels.DataCollection.Views.Questionnaire
         }
 
         #endregion
-
-        public class TemplateComparer : IEqualityComparer<QuestionnaireBrowseItem>
-        {
-            public bool Equals(QuestionnaireBrowseItem x, QuestionnaireBrowseItem y)
-            {
-                return x.QuestionnaireId == y.QuestionnaireId;
-
-            }
-
-            public int GetHashCode(QuestionnaireBrowseItem obj)
-            {
-                if (Object.ReferenceEquals(obj, null)) return 0;
-                return obj.QuestionnaireId.GetHashCode();
-            }
-        }
     }
 }
