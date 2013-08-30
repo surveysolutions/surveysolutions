fcc027abaf68f8225894a1e73e9c715fcb31fd5c
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2013-08-30 16:53:32 -0400

    formatting of coordinates displaying changed
    --HG--
    branch : gps-question-type-support
    
    
    Former-commit-id: 8b0770a8ca8e83ed627b728e4e4d9daafbcfec64

--- Mono/CAPI.Android/Controls/QuestionnaireDetails/ScreenItems/GeoPositionQuestionView.cs
+++ Mono/CAPI.Android/Controls/QuestionnaireDetails/ScreenItems/GeoPositionQuestionView.cs
@@ -1,8 +1,10 @@
 using System;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Android.App;
 using Android.Content;
+using Android.Graphics;
 using Android.Views;
 using Android.Widget;
 using CAPI.Android.Core.Model.ViewModel.QuestionnaireDetails;
@@ -35,9 +37,12 @@ namespace CAPI.Android.Controls.QuestionnaireDetails.ScreenItems
 
             //geoWrapper.Orientation = Orientation.Horizontal;
 
-            locaionDisplay = new TextView(this.Context);
-            locaionDisplay.Text = Model.AnswerString;
+            //locationDisplay = CreateTable(Model.AnswerObject as GeoPosition);
             
+            locationText = new TextView(this.Context);
+            locationText.SetTypeface(null, TypefaceStyle.Bold);
+            locationText.Text = RenderPositionAsText(Model.AnswerObject as GeoPosition);
+
             var updateLocationButton = new Button(this.Context) {Text = "Get Location"};
             var layoutParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WrapContent, 
                                                                ViewGroup.LayoutParams.WrapContent);
@@ -46,12 +51,15 @@ namespace CAPI.Android.Controls.QuestionnaireDetails.ScreenItems
             updateLocationButton.Click += GetLocation;
 
             geoWrapper.AddView(updateLocationButton);
-            geoWrapper.AddView(locaionDisplay);
+            geoWrapper.AddView(locationText);
+            //geoWrapper.AddView(locationDisplay);
 
             llWrapper.AddView(geoWrapper);
         }
 
-        private TextView locaionDisplay;
+        //private TableLayout locationDisplay;
+
+        private TextView locationText;
 
         private void GetLocation(object sender, EventArgs e)
         {
@@ -111,11 +119,69 @@ namespace CAPI.Android.Controls.QuestionnaireDetails.ScreenItems
                     
                     CommandService.Execute(new AnswerGeoLocationQuestionCommand(this.QuestionnairePublicKey, CapiApplication.Membership.CurrentUser.Id, Model.PublicKey.PublicKey,
                                                                   this.Model.PublicKey.PropagationVector, DateTime.UtcNow, position));
-                    locaionDisplay.Text = position.ToString();
+
+                    locationText.Text = RenderPositionAsText(position);
+                    //locationDisplay = CreateTable(position);
                     
                     SaveAnswer();
                 }
             }));
         }
+
+        private TableLayout CreateTable(GeoPosition position)
+        {
+            var table = new TableLayout(this.Context);
+
+
+            TableRow th = new TableRow(this.Context);
+            TextView columnName = new TextView(this.Context);
+            columnName.Text = "Latitude:";
+            TextView columnValue = new TextView(this.Context);
+            columnValue.Text = position.Latitude.ToString("{0:N4}");
+
+            th.AddView(columnName);
+            th.AddView(columnValue);
+            table.AddView(th);
+
+             th = new TableRow(this.Context);
+             columnName = new TextView(this.Context);
+            columnName.Text = "Longitude:";
+             columnValue = new TextView(this.Context);
+            columnValue.Text = position.Longitude.ToString("{0:N4}");
+
+            th.AddView(columnName);
+            th.AddView(columnValue);
+            table.AddView(th);
+
+             th = new TableRow(this.Context);
+             columnName = new TextView(this.Context);
+             columnName.Text = "Accuracy:";
+             columnValue = new TextView(this.Context);
+             columnValue.Text = position.Accuracy.ToString("{0:N2}");
+
+            th.AddView(columnName);
+            th.AddView(columnValue);
+            table.AddView(th);
+
+            return table;
+        }
+
+        private string RenderPositionAsText(GeoPosition position)
+        {
+            StringBuilder sb = new StringBuilder();
+            if (position != null)
+            {
+                sb.AppendLine(string.Format("Latitude:\t\t\t{0}{1:N4}", position.Latitude > 0 ? "\t" : "", position.Latitude));
+                sb.AppendLine(string.Format("Longitude:\t\t{0}{1:N4}", position.Longitude > 0 ? "\t" : "", position.Longitude));
+                sb.AppendLine(string.Format("Accuracy:\t\t{0}{1:N2}", position.Accuracy > 0 ? "\t" : "", position.Accuracy));
+            }
+            else
+            {
+                sb.Append("N/A");
+            }
+
+            return sb.ToString();
+        }
+
     }
-}
+};
