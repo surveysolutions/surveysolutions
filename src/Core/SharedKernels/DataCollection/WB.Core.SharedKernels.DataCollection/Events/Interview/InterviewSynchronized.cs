ae72cdf9f0a51cd36f9995c83178792464eade10
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2013-09-04 15:33:25 -0400

    DataCollection: refactoring for InterviewSynchronized  event KP-2527
    --HG--
    branch : apply-interview-aggregate-root
    
    
    Former-commit-id: d393898f0bbacbecde075bd8a8270988491f5abb

--- src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/Events/Interview/InterviewSynchronized.cs
+++ src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/Events/Interview/InterviewSynchronized.cs
@@ -11,32 +11,12 @@ namespace WB.Core.SharedKernels.DataCollection.Events.Interview
 {
     public class InterviewSynchronized : InterviewActiveEvent
     {
-        public InterviewSynchronized(Guid userId, Guid questionnaireId, InterviewStatus status,
-                                     long questionnaireVersion, IList<AnsweredQuestionSynchronizationDto> answeredQuestions,
-                                     HashSet<InterviewItemId> disabledGroups, HashSet<InterviewItemId> disabledQuestions,
-                                     HashSet<InterviewItemId> validAnsweredQuestions, HashSet<InterviewItemId> invalidAnsweredQuestions,
-                                     Dictionary<InterviewItemId, int> propagatedGroupInstanceCounts)
-            : base(userId)
+        public InterviewSynchronized(InterviewSynchronizationDto interviewData)
+            : base(interviewData.UserId)
         {
-            QuestionnaireId = questionnaireId;
-            Status = status;
-            QuestionnaireVersion = questionnaireVersion;
-            AnsweredQuestions = answeredQuestions?? new List<AnsweredQuestionSynchronizationDto>();
-            DisabledGroups = disabledGroups?? new HashSet<InterviewItemId>();
-            DisabledQuestions = disabledQuestions?? new HashSet<InterviewItemId>();
-            ValidAnsweredQuestions = validAnsweredQuestions?? new HashSet<InterviewItemId>();
-            InvalidAnsweredQuestions = invalidAnsweredQuestions?? new HashSet<InterviewItemId>();
-            PropagatedGroupInstanceCounts = propagatedGroupInstanceCounts?? new Dictionary<InterviewItemId, int>();
+            InterviewData = interviewData;
         }
 
-        public Guid QuestionnaireId { get; private set; }
-        public InterviewStatus Status { get; private set; }
-        public long QuestionnaireVersion { get; private set; }
-        public IList<AnsweredQuestionSynchronizationDto> AnsweredQuestions { get; private set; }
-        public HashSet<InterviewItemId> DisabledGroups { get; private set; }
-        public HashSet<InterviewItemId> DisabledQuestions { get; private set; }
-        public HashSet<InterviewItemId> ValidAnsweredQuestions { get; private set; }
-        public HashSet<InterviewItemId> InvalidAnsweredQuestions { get; private set; }
-        public Dictionary<InterviewItemId, int> PropagatedGroupInstanceCounts { get; private set; }
+        public InterviewSynchronizationDto InterviewData { get; private set; }
     }
 }
