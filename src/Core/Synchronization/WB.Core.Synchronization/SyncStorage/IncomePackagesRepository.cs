aeeefad48a39d7d8e5577392999a50170c957411
Anastasya Kholodova <nastya.kholodova@gmail.com>
2013-09-09 19:22:15 -0400

    Supervisor\Synchronization: eventtimestamp is fixed
    Former-commit-id: ce23bddeceb5045dbc26634b579d6e66a112e303

--- src/Core/Synchronization/WB.Core.Synchronization/SyncStorage/IncomePackagesRepository.cs
+++ src/Core/Synchronization/WB.Core.Synchronization/SyncStorage/IncomePackagesRepository.cs
@@ -94,7 +94,7 @@ namespace WB.Core.Synchronization.SyncStorage
             var i = sequence + 1;
             foreach (var aggregateRootEvent in stream)
             {
-                uncommitedStream.Append(aggregateRootEvent.CreateUncommitedEvent(i, 0));
+                uncommitedStream.Append(aggregateRootEvent.CreateUncommitedEvent(i, 0, DateTime.Now));
                 i++;
             }
             return uncommitedStream;
