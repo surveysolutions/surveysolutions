df473af08ba15e058c4987cae93b4916b1ad1a6a
Slava Scherbak <slava.scherbak@gmail.com>
2013-09-17 17:22:29 -0400

    Supervisor: added AntiXSS libriry to encode json, fixed xss bug on details page
    --HG--
    branch : release-candidate
    
    
    Former-commit-id: 64045252d63f94eefdcd8f101efed938aa79a994

---
+++ RavenQuestionnaire.Web/Web.Supervisor/Code/HtmlEncodeStringPropertiesConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.Security.Application;
+using Newtonsoft.Json;
+
+namespace Web.Supervisor.Code
+{
+    public class HtmlEncodeStringPropertiesConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(string);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            writer.WriteValue(Encoder.HtmlEncode(value.ToString()));
+        }
+    }
+}
