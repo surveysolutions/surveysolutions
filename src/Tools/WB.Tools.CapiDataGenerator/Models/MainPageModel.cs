4eaef713f2982e6ea82ce1cd6d96eb6a1fd0f945
Slava Scherbak <slava.scherbak@gmail.com>
2013-09-19 19:42:10 -0400

    Interview: fixed tests and added new parametr to InterviewSynchronizationDto
    --HG--
    branch : release-candidate
    
    
    Former-commit-id: 64d00c52c25fdb163acbd1835eddf65af88dbf44

--- src/Tools/WB.Tools.CapiDataGenerator/Models/MainPageModel.cs
+++ src/Tools/WB.Tools.CapiDataGenerator/Models/MainPageModel.cs
@@ -561,7 +561,8 @@ namespace CapiDataGenerator
                                                                            disabledQuestions: new HashSet<InterviewItemId>(), 
                                                                            validAnsweredQuestions: new HashSet<InterviewItemId>(), 
                                                                            invalidAnsweredQuestions: new HashSet<InterviewItemId>(), 
-                                                                           propagatedGroupInstanceCounts: new Dictionary<InterviewItemId, int>())));
+                                                                           propagatedGroupInstanceCounts: new Dictionary<InterviewItemId, int>(),
+                                                                           interviewWasCompleted: false)));
                 UpdateProgress();
             }
 
