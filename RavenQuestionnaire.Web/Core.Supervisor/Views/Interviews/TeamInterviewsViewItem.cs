22e28340a5b30bea7a0eb1acd3a49a04abbca9f4
Slava Scherbak <slava.scherbak@gmail.com>
2013-09-25 17:46:42 -0400

    Supervisor\Interviews: added "has errors" flag in grid
    Former-commit-id: 79e51d4eb91ec939738a4482d3e3809b6173de22

--- RavenQuestionnaire.Web/Core.Supervisor/Views/Interviews/TeamInterviewsViewItem.cs
+++ RavenQuestionnaire.Web/Core.Supervisor/Views/Interviews/TeamInterviewsViewItem.cs
@@ -8,16 +8,8 @@ using Main.Core.Entities.SubEntities;
 
 namespace Core.Supervisor.Views.Interviews
 {
-    public class TeamInterviewsViewItem
+    public class TeamInterviewsViewItem : BaseInterviewGridItem
     {
-        public IEnumerable<InterviewFeaturedQuestion> FeaturedQuestions { get; set; }
-        public Guid InterviewId { get; set; }
-        public Guid ResponsibleId { get; set; }
-        public string ResponsibleName { get; set; }
-        public UserRoles ResponsibleRole { get; set; }
-
-        public string Status { get; set; }
-        public string LastEntryDate { get; set; }
         public bool CanBeReassigned { get; set; }
     }
 }
