c316b7e5fe6331235dd7e79507550ec4f73cd8a8
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2013-09-26 12:09:57 -0400

    DataCollection\MandatoryQuestions: failed test are added KP-2473 KP-2551
    Former-commit-id: fb73b3a8348cf3ffc212779bb04167688f3380f6

---
+++ src/Tests/WB.Core.SharedKernels.DataCollection.Tests/InterviewTests/when_creating_interview_and_questionnaire_has_question_marked_as_mandatory.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Machine.Specifications;
+using Microsoft.Practices.ServiceLocation;
+using Moq;
+using Ncqrs.Spec;
+using WB.Core.SharedKernels.DataCollection.Aggregates;
+using WB.Core.SharedKernels.DataCollection.Events.Interview;
+using WB.Core.SharedKernels.DataCollection.Implementation.Aggregates;
+using WB.Core.SharedKernels.DataCollection.Implementation.Repositories;
+using It = Machine.Specifications.It;
+
+namespace WB.Core.SharedKernels.DataCollection.Tests.InterviewTests
+{
+    internal class when_creating_interview_and_questionnaire_has_question_marked_as_mandatory : InterviewTestsContext
+    {
+        private Establish context = () =>
+        {
+            interviewId = Guid.Parse("11111111111111111111111111111111");
+            questionnaireId = Guid.Parse("22220000000000000000000000000000");
+            userId = Guid.Parse("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+            supervisorId = Guid.Parse("BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB");
+            mandatoryQuestionId = Guid.Parse("CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC");
+            answersToFeaturedQuestions = new Dictionary<Guid, object>();
+            answersTime = new DateTime(2013, 09, 01);
+
+            var questionaire = Mock.Of<IQuestionnaire>(_
+                => true);
+
+            var questionnaireRepository = Mock.Of<IQuestionnaireRepository>(repository
+                => repository.GetQuestionnaire(questionnaireId) == questionaire);
+
+            Mock.Get(ServiceLocator.Current)
+                .Setup(locator => locator.GetInstance<IQuestionnaireRepository>())
+                .Returns(questionnaireRepository);
+
+            eventContext = new EventContext();
+        };
+        
+        Because of = () =>
+            new Interview(interviewId, userId, questionnaireId, answersToFeaturedQuestions, answersTime, supervisorId);
+
+        Cleanup stuff = () =>
+        {
+            eventContext.Dispose();
+            eventContext = null;
+        };
+
+        It should_not_raise_AnswerDeclaredValid_event = () =>
+             eventContext.ShouldNotContainEvent<AnswerDeclaredValid>(@event
+              => @event.QuestionId == mandatoryQuestionId);
+
+        It should_raise_AnswerDeclaredInvalid_event = () =>
+             eventContext.ShouldNotContainEvent<AnswerDeclaredInvalid>(@event
+              => @event.QuestionId == mandatoryQuestionId);
+
+        private static EventContext eventContext;
+        private static Guid interviewId;
+        private static Guid userId;
+        private static Guid questionnaireId;
+        private static Dictionary<Guid, object> answersToFeaturedQuestions;
+        private static DateTime answersTime;
+        private static Guid supervisorId;
+        private static Guid mandatoryQuestionId;
+    }
+}
