bcc71da6072b2b9c4f96fbf0aa6a1392be7f64d1
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2013-09-27 15:22:07 -0400

    DataCollection\Synchronization: reevaluation interview state for unanswered questions with validation
    Former-commit-id: 464c5719d4c574b18601f2ba5579bf5a26088112

--- src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/Implementation/Aggregates/Interview.cs
+++ src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/Implementation/Aggregates/Interview.cs
@@ -879,6 +879,11 @@ namespace WB.Core.SharedKernels.DataCollection.Implementation.Aggregates
                     if (IsQuestionOrParentGroupDisabled(questionIdAtInterview, questionnaire, (question) => groupsToBeDisabled.Any(q => AreEqual(q, question)), (question) => questionsToBeDisabled.Any(q => AreEqual(q, question))))
                         continue;
 
+                    string questionKey = ConvertIdAndPropagationVectorToString(questionIdAtInterview.Id, questionIdAtInterview.PropagationVector);
+
+                    if(!answeredQuestions.Contains(questionKey))
+                        continue;
+
                     bool? dependentQuestionValidationResult = this.PerformCustomValidationOfQuestion(questionIdAtInterview, questionnaire,
                         GetAnswerSupportedInExpressionsForEnabledOrNull, a => null);
 
