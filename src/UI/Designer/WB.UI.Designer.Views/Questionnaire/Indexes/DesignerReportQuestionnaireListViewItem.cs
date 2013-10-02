ac3a0b1439a33fc37d6ae98fdb3d14527efa59dd
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2013-10-02 10:34:48 -0400

    Designer\ReadSide: index for search by user name and title
    --HG--
    branch : designer-raven-readside
    
    
    Former-commit-id: 173dfb5c07cf82ca75415326ef316c48ff1a9023

---
+++ src/UI/Designer/WB.UI.Designer.Views/Questionnaire/Indexes/DesignerReportQuestionnaireListViewItem.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using Raven.Abstractions.Indexing;
+using Raven.Client.Indexes;
+
+namespace WB.UI.Designer.Views.Questionnaire.Indexes
+{
+    public class DesignerReportQuestionnaireListViewItem : AbstractIndexCreationTask<QuestionnaireListViewItem>
+    {
+        public DesignerReportQuestionnaireListViewItem()
+        {
+            this.Map = questionnaires => from questionnaire in questionnaires
+                                    select new
+                                        {
+                                            questionnaire.PublicId, 
+                                            questionnaire.SharedPersons,
+                                            questionnaire.Owner,
+                                            questionnaire.LastEntryDate,
+                                            questionnaire.IsPublic,
+                                            questionnaire.IsDeleted,
+                                            questionnaire.CreationDate,
+                                            questionnaire.Title, 
+                                            questionnaire.CreatorName, 
+                                            questionnaire.CreatedBy
+                                        };
+
+            this.Index(x => x.Title, FieldIndexing.Analyzed);
+            this.Index(x => x.CreatorName, FieldIndexing.Analyzed);
+        }
+    }
+}
