94fd21e98174456231265e6dccebc07d15774f8b
Anastasya Kholodova <nastya.kholodova@gmail.com>
2013-10-03 10:12:15 -0400

    Designer\Questionnaire: sorting is fixed by questionnarie title
    --HG--
    branch : designer-raven-readside
    
    
    Former-commit-id: 2fa318f45e69d52b7598648d4eac63365b6f9924

---
+++ src/UI/Designer/WB.UI.Designer.Views/Questionnaire/QuestionnaireListViewItemSearchable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WB.UI.Designer.Views.Questionnaire
+{
+    public class QuestionnaireListViewItemSearchable : QuestionnaireListViewItem
+    {
+        public string TitleIndexed { get; set; }
+    }
+}
