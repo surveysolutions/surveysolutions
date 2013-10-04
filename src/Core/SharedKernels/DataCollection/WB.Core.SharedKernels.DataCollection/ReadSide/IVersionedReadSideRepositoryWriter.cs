78596a414835e36235147b85260c30a9442b2c03
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2013-10-04 17:06:24 -0400

    fix: please remove commitdenormalizer as redundant KP-2573 Re-init action is too long on CAPI
    Former-commit-id: 8a963cf6c82c86a5a25fa672a131d95b67f57968

--- src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/ReadSide/IVersionedReadSideRepositoryWriter.cs
+++ src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/ReadSide/IVersionedReadSideRepositoryWriter.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using WB.Core.Infrastructure.ReadSide.Repository;
 using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
 
 namespace WB.Core.SharedKernels.DataCollection.ReadSide
