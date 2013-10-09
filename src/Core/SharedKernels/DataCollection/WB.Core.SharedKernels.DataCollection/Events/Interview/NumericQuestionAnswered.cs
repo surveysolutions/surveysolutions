37658911ea85838ce9f2d2984e174ca76ef09ab4
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2013-10-09 16:40:48 -0400

    DataCollection\IntegerReal: support of obsolet event NumericQuestionAnswered added KP-2734
    --HG--
    branch : integer-real-questions
    
    
    Former-commit-id: 2986ba6a382b7d875feb01e9e06a03147f8a8388

---
+++ src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/Events/Interview/NumericQuestionAnswered.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WB.Core.SharedKernels.DataCollection.Events.Interview.Base;
+
+namespace WB.Core.SharedKernels.DataCollection.Events.Interview
+{
+    [Obsolete]
+    public class NumericQuestionAnswered: QuestionAnswered
+    {
+        public decimal Answer { get; private set; }
+
+        public NumericQuestionAnswered(Guid userId, Guid questionId, int[] propagationVector, DateTime answerTime, decimal answer)
+            : base(userId, questionId, propagationVector, answerTime)
+        {
+            this.Answer = answer;
+        }
+    }
+}
