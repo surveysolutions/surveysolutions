bc1e8eb2b04c518a9249741b4388273cacfaddd7
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2013-10-15 14:30:24 -0400

    Designer\Core: separate command in order to create/udate/clone numeric questions was introduced KP-2756
    --HG--
    branch : integer-real-questions
    
    
    Former-commit-id: d205258d5e1c2a1675d0c18880e51e5e0813405e

---
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Commands/Questionnaire/Base/AbstractQuestionCommand.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Main.Core.Entities.SubEntities;
+
+namespace WB.Core.BoundedContexts.Designer.Commands.Questionnaire.Base
+{
+    public abstract class AbstractQuestionCommand : QuestionCommand
+    {
+        protected AbstractQuestionCommand(Guid questionnaireId, Guid questionId,
+            string title, QuestionType type, string alias, bool isMandatory, bool isFeatured, bool isHeaderOfPropagatableGroup,
+            QuestionScope scope, string condition, string validationExpression, string validationMessage, string instructions,Guid responsibleId)
+            : base(questionnaireId, questionId, responsibleId)
+        {
+            this.Title = title;
+            this.Type = type;
+            this.Alias = alias;
+            this.IsMandatory = isMandatory;
+            this.IsFeatured = isFeatured;
+            this.IsHeaderOfPropagatableGroup = isHeaderOfPropagatableGroup;
+            this.Scope = scope;
+            this.Condition = condition;
+            this.ValidationExpression = validationExpression;
+            this.ValidationMessage = validationMessage;
+            this.Instructions = instructions;
+        }
+        public string Title { get; private set; }
+
+        public QuestionType Type { get; private set; }
+
+        public string Alias { get; private set; }
+
+        public bool IsMandatory { get; private set; }
+
+        public bool IsFeatured { get; private set; }
+
+        public bool IsHeaderOfPropagatableGroup { get; private set; }
+
+        public QuestionScope Scope { get; private set; }
+
+        public string Condition { get; set; }
+
+        public string ValidationExpression { get; set; }
+
+        public string ValidationMessage { get; private set; }
+
+        public string Instructions { get; private set; }
+    }
+}
