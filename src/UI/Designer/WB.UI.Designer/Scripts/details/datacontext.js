1e059fd524073680934f53fb3466d3ab0c2dfc0d
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2013-10-16 16:54:01 +0300

    Designer\Details\question\title: added validation on question type of all specified variables KP-2752
    --HG--
    branch : question-text-substitution
    
    
    Former-commit-id: 21695415c47822138c827e8683e11c9661b67fa2

--- src/UI/Designer/WB.UI.Designer/Scripts/details/datacontext.js
+++ src/UI/Designer/WB.UI.Designer/Scripts/details/datacontext.js
@@ -171,7 +171,11 @@
             });
             return items;
         };
-        
+
+        questions.getLocalByVariable = function(variable) {
+            return _.find(questions.getAllLocal(), function(question) { return question.alias() == variable; });
+        };
+
         questions.cleanTriggers = function (group) {
             _.each(questions.getAllLocal(), function (question) {
                 var child = _.find(question.triggers(), { 'key': group.id });
@@ -190,6 +194,7 @@
                 return question.alias();
             });
         };
+
 
         var getChildItemByIdAndType = function(item) {
             if (item.type === "GroupView")
