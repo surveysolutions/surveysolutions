ecd57c58eb39cea40ec88f5c186b279f203a6686
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2013-10-18 18:15:45 -0400

    Supervisor\Export: unit test for export of top level KP-2621
    --HG--
    branch : next
    
    
    Former-commit-id: cb6196db4cd7962ed75949a0556f36203e6e1095

--- src/Tests/WB.Core.BoundedContexts.Supervisor.Tests/Views/InterviewDataExportFactoryTestContext.cs
+++ src/Tests/WB.Core.BoundedContexts.Supervisor.Tests/Views/InterviewDataExportFactoryTestContext.cs
@@ -1,79 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Machine.Specifications;
 using Main.Core.Documents;
+using Main.Core.Entities.SubEntities.Question;
 using Microsoft.Practices.ServiceLocation;
 using Moq;
-using NUnit.Framework;
 using WB.Core.BoundedContexts.Supervisor.Views.DataExport;
 using WB.Core.BoundedContexts.Supervisor.Views.Interview;
 using WB.Core.BoundedContexts.Supervisor.Views.Questionnaire;
 using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
-using WB.Core.SharedKernels.DataCollection.Implementation.Aggregates;
 using WB.Core.SharedKernels.DataCollection.ReadSide;
 using WB.Core.SharedKernels.DataCollection.Views.Questionnaire;
-using It = Machine.Specifications.It;
-
 
 namespace WB.Core.BoundedContexts.Supervisor.Tests.Views
 {
-    [Subject(typeof(InterviewDataExportFactory))]
-    internal class when_requesing_data_by_top_level_of_interview_and_only_2_approved_interviews_are_present
-    {
-        private Establish context = () =>
-        {
-            interviewDataExportFactory = CreateInterviewDataExportFactory(2);
-        };
+    internal class InterviewDataExportFactoryTestContext {
 
-        Because of = () =>
-           result = interviewDataExportFactory.Load(new InterviewDataExportInputModel(Guid.NewGuid(), 1, null));
-
-        private It should_records_count_equals_2 = () =>
-            result.Records.Length.ShouldEqual(2);
-
-        private It should_first_record_id_equals_0 = () =>
-            result.Records[0].RecordId.ShouldEqual(0);
-
-        private It should_second_record_id_equals_1 = () =>
-            result.Records[1].RecordId.ShouldEqual(1);
-
-        private static InterviewDataExportFactory interviewDataExportFactory;
-        private static InterviewDataExportView result;
-
-        public void Load_When_2_approved_interviews_and_top_level_is_requested_Then_2_records_returned()
-        {
-            //arrange
-
-            InterviewDataExportFactory target = CreateInterviewDataExportFactory(2);
-
-            //act
-
-            InterviewDataExportView result = target.Load(new InterviewDataExportInputModel(Guid.NewGuid(), 1, null));
-
-            //assert
-            Assert.That(result.Records.Length, Is.EqualTo(2));
-            Assert.That(result.Records[0].RecordId, Is.EqualTo(0));
-            Assert.That(result.Records[1].RecordId, Is.EqualTo(1));
-        }
-
-        protected static InterviewDataExportFactory CreateInterviewDataExportFactory(int interviewCount = 0)
+        protected static InterviewDataExportFactory CreateInterviewDataExportFactoryForQuestionnarieWithColumns(int interviewCount = 0, params string[] variableNames)
         {
             ServiceLocator.SetLocatorProvider(() => new Mock<IServiceLocator> { DefaultValue = DefaultValue.Mock }.Object);
 
             var interviewSummaryStorageMock = new Mock<IQueryableReadSideRepositoryReader<InterviewSummary>>();
 
-            interviewSummaryStorageMock.Setup(x => x.Query(Moq.It.IsAny<Func<IQueryable<InterviewSummary>, InterviewSummary[]>>()))
-                .Returns(CreateListOfApprovedInterviews(interviewCount));
+            interviewSummaryStorageMock.Setup(x => x.Query(Moq.It.IsAny<Func<IQueryable<InterviewSummary>, InterviewSummary[]>>())).Returns((InterviewSummary[]) CreateListOfApprovedInterviews(interviewCount));
 
             var interviewDataStorageMock = new Mock<IReadSideRepositoryReader<InterviewData>>();
-            interviewDataStorageMock.Setup(x => x.GetById(Moq.It.IsAny<Guid>())).Returns(CreateInterviewData);
+            interviewDataStorageMock.Setup(x => x.GetById(Moq.It.IsAny<Guid>())).Returns((Func<InterviewData>) CreateInterviewData);
 
             var questionnaireStorageMock = new Mock<IVersionedReadSideRepositoryReader<QuestionnaireDocumentVersioned>>();
             questionnaireStorageMock.Setup(x => x.GetById(Moq.It.IsAny<Guid>(), Moq.It.IsAny<long>()))
-                .Returns(new QuestionnaireDocumentVersioned() { Questionnaire = new QuestionnaireDocument() });
+                .Returns(new QuestionnaireDocumentVersioned() { Questionnaire = CreateQuestionnaireDocument(variableNames) });
 
             var propagationStructureStorageMock = new Mock<IVersionedReadSideRepositoryReader<QuestionnairePropagationStructure>>();
             propagationStructureStorageMock.Setup(x => x.GetById(Moq.It.IsAny<Guid>(), Moq.It.IsAny<long>()))
@@ -91,9 +47,15 @@ namespace WB.Core.BoundedContexts.Supervisor.Tests.Views
                 linkedQuestionStructureStorageMock.Object);
         }
 
-        protected static QuestionnaireDocument CreateQuestionnaireDocument()
+        protected static QuestionnaireDocument CreateQuestionnaireDocument(params string[] variableNames)
         {
             var questionnaire = new QuestionnaireDocument();
+
+            foreach (var variableName in variableNames)
+            {
+                questionnaire.Children.Add(new NumericQuestion() { StataExportCaption = variableName, PublicKey = Guid.NewGuid() });
+            }
+
             return questionnaire;
         }
 
