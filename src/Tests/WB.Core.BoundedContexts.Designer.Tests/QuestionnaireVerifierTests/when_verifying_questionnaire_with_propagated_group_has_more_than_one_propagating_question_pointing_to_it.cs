2042b704e3ca5249ccaad7f11c3e9b86c6eedb40
Anastasya Kholodova <nastya.kholodova@gmail.com>
2013-10-23 18:20:43 -0400

    Designer\Verifier: Linked question reference questions of not supported type KP-2781
    Former-commit-id: 9b2a9080f4cb1d55de50e19de8b13de08ba3df21

--- src/Tests/WB.Core.BoundedContexts.Designer.Tests/QuestionnaireVerifierTests/when_verifying_questionnaire_with_propagated_group_has_more_than_one_propagating_question_pointing_to_it.cs
+++ src/Tests/WB.Core.BoundedContexts.Designer.Tests/QuestionnaireVerifierTests/when_verifying_questionnaire_with_propagated_group_has_more_than_one_propagating_question_pointing_to_it.cs
@@ -12,33 +12,34 @@ using WB.Core.BoundedContexts.Designer.ValueObjects.Verification;
 
 namespace WB.Core.BoundedContexts.Designer.Tests.QuestionnaireVerifierTests
 {
-    internal class when_propagated_group_has_no_propagating_question_pointing_to_it : QuestionnaireVerifierTestsContext
+    internal class when_verifying_questionnaire_with_propagated_group_has_more_than_one_propagating_question_pointing_to_it : QuestionnaireVerifierTestsContext
     {
-        Establish context = () =>
+        private Establish context = () =>
         {
             autopropagatedGroupId = Guid.Parse("10000000000000000000000000000000");
             questionnaire = CreateQuestionnaireDocument();
-            questionnaire.Children.Add(new TextQuestion("random question"));
+            questionnaire.Children.Add(new AutoPropagateQuestion("question 1") { Triggers = new List<Guid>() { autopropagatedGroupId } });
+            questionnaire.Children.Add(new AutoPropagateQuestion("question 2") { Triggers = new List<Guid>() { autopropagatedGroupId } });
             questionnaire.Children.Add(new Group() { PublicKey = autopropagatedGroupId, Propagated = Propagate.AutoPropagated });
             verifier = CreateQuestionnaireVerifier();
         };
 
-        Because of = () =>
+        private Because of = () =>
             resultErrors = verifier.Verify(questionnaire);
 
-        It should_return_1_error = () =>
+        private It should_return_1_error = () =>
             resultErrors.Count().ShouldEqual(1);
 
-        It should_return_error_with_code__WB0009__ = () =>
-            resultErrors.Single().Code.ShouldEqual("WB0009");
+        private It should_return_error_with_code__WB0010__ = () =>
+            resultErrors.Single().Code.ShouldEqual("WB0010");
 
-        It should_return_error_with_one_references = () =>
+        private It should_return_error_with_one_references = () =>
             resultErrors.Single().References.Count().ShouldEqual(1);
 
-        It should_return_error_reference_with_type_group = () =>
+        private It should_return_error_reference_with_type_group = () =>
             resultErrors.Single().References.Single().Type.ShouldEqual(QuestionnaireVerificationReferenceType.Group);
 
-        It should_return_error_reference_with_id_of_autopropagatedGroupId = () =>
+        private It should_return_error_reference_with_id_of_autopropagatedGroupId = () =>
             resultErrors.Single().References.Single().Id.ShouldEqual(autopropagatedGroupId);
 
         private static IEnumerable<QuestionnaireVerificationError> resultErrors;
