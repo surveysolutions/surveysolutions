9b51d98860930ba4035497987b744ca86d5bd07f
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2013-10-23 16:44:22 +0300

    Designer\Verifier: signature updated, now always 1 error is returned KP-2780
    Former-commit-id: 3a5a3810c36c3cfe75116e6be1c1ee9ab6de1812

---
+++ src/Tests/WB.Core.BoundedContexts.Designer.Tests/QuestionnaireVerifierTests/when_verifying_empty_questionnaire.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using System.Linq;
+using Machine.Specifications;
+using Main.Core.Documents;
+using WB.Core.BoundedContexts.Designer.Implementation.Services;
+using WB.Core.BoundedContexts.Designer.ValueObjects.Verification;
+
+namespace WB.Core.BoundedContexts.Designer.Tests.QuestionnaireVerifierTests
+{
+    internal class when_verifying_empty_questionnaire : QuestionnaireVerifierTestsContext
+    {
+        Establish context = () =>
+        {
+            questionnaire = CreateQuestionnaireDocument();
+
+            verifier = CreateQuestionnaireVerifier();
+        };
+
+        Because of = () =>
+            resultErrors = verifier.Verify(questionnaire);
+
+        It should_return_1_error = () =>
+            resultErrors.Count().ShouldEqual(1);
+
+        private static IEnumerable<QuestionnaireVerificationError> resultErrors;
+        private static QuestionnaireVerifier verifier;
+        private static QuestionnaireDocument questionnaire;
+    }
+}
