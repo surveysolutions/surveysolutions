2cc3b564620af0c67abb6b5387984cfc00879fa0
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2013-10-23 17:14:07 -0400

    Designer\Verifier: propagating question has no associated groups KP-2781
    Former-commit-id: 61e82180c08f9c9a784d2bc13b5fbeef5cd0f863

--- src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Implementation/Services/QuestionnaireVerifier.cs
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Implementation/Services/QuestionnaireVerifier.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Main.Core.Documents;
 using Main.Core.Entities.SubEntities;
+using Main.Core.Entities.SubEntities.Question;
 using WB.Core.BoundedContexts.Designer.Services;
 using WB.Core.BoundedContexts.Designer.ValueObjects.Verification;
 
@@ -15,12 +16,23 @@ namespace WB.Core.BoundedContexts.Designer.Implementation.Services
             if (NoQuestionsExist(questionnaire))
                 return new[] { new QuestionnaireVerificationError("WB0001", VerificationMessages.WB0001_NoQuestions) };
 
-            return Enumerable.Empty<QuestionnaireVerificationError>();
+            var errorList = new List<QuestionnaireVerificationError>();
+            errorList.AddRange(this.GetListOfErrorsByPropagatingQuestionsThatHasNoAssociatedGroups(questionnaire));
+            return errorList;
         }
 
         private bool NoQuestionsExist(QuestionnaireDocument questionnaire)
         {
             return !questionnaire.Find<IQuestion>(_ => true).Any();
+        }
+
+        private IEnumerable<QuestionnaireVerificationError> GetListOfErrorsByPropagatingQuestionsThatHasNoAssociatedGroups(
+            QuestionnaireDocument questionnaire)
+        {
+            return questionnaire.Find<IAutoPropagateQuestion>(question => question.Triggers.Count == 0).Select(
+                question =>
+                    new QuestionnaireVerificationError("WB0008", VerificationMessages.WB0008_PropagatingQuestionHasNoAssociatedGroups,
+                        new QuestionnaireVerificationReference(QuestionnaireVerificationReferenceType.Question, question.PublicKey)));
         }
     }
 }
