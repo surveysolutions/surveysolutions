8489e8f9a3b9a8ab32ea104f063990013d756069
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2013-10-25 16:08:17 -0400

    QuestionnaireVerifier: WB.Core.SharedKernels.QuestionnaireVerification is linked for android app KP-2783
    Former-commit-id: ced093675e9cd9cb9de8d7324aefbb7edb074917

---
+++ src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/Exceptions/QuestionnaireVerificationException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using WB.Core.SharedKernels.QuestionnaireVerification.ValueObjects.Verification;
+
+namespace WB.Core.SharedKernels.DataCollection.Exceptions
+{
+    public class QuestionnaireVerificationException : QuestionnaireException
+    {
+        internal QuestionnaireVerificationException(string message, Exception innerException, QuestionnaireVerificationError[] errors)
+            : base(message, innerException)
+        {
+            this.Errors = errors;
+        }
+
+        internal QuestionnaireVerificationException(string message, QuestionnaireVerificationError[] errors)
+            : base(message)
+        {
+            this.Errors = errors;
+        }
+
+        public QuestionnaireVerificationError[] Errors { get; private set; }
+    }
+}
