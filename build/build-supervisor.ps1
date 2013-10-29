a74b98aa257110c20e06e3bbf5bed756415321a2
Zurab Sajaia <zsajaia@worldbank.org>
2013-10-29 15:11:06 -0400

    Scripts for building and deploying designer and supervisor to the amazon servers Once we migrate completely, will have to clean up the old scripts
    Former-commit-id: 2d6f39e24ec3e373426ae2c98350b54c020eba92

---
+++ build/build-supervisor.ps1
@@ -0,0 +1,41 @@
+param([string]$VersionPrefix,
+[INT]$BuildNumber,
+[string]$KeystorePassword,
+[string]$BuildConfiguration='release')
+
+$ErrorActionPreference = "Stop"
+
+#do not allow empty prefix
+if([string]::IsNullOrWhiteSpace($VersionPrefix)){
+	Write-Host "##teamcity[buildStatus status='FAILURE' text='VersionPrefix param is not set']"
+	Exit 
+}
+#do not allow empty build number	
+if(!$BuildNumber){
+	Write-Host "##teamcity[buildStatus status='FAILURE' text='BuildNumber param is not set']"
+	Exit 
+}
+
+#do not allow empty KeystorePassword
+if([string]::IsNullOrWhiteSpace($KeystorePassword)){
+	Write-Host "##teamcity[buildStatus status='FAILURE' text='VersionPrefix param is not set']"
+	Exit 
+}
+
+$scriptFolder = (Get-Item $MyInvocation.MyCommand.Path).Directory.FullName
+. "$scriptFolder\build-functions.ps1"
+
+try {
+	BuildSupervisor `
+		-Solution 'src\Supervisor.sln' `
+		-Project 'RavenQuestionnaire.Web\Web.Supervisor\Web.Supervisor.csproj' `
+		-CapiProject 'Mono\CAPI.Android\CAPI.Android.csproj' `
+		-BuildConfiguration $BuildConfiguration `
+		-VersionPrefix $VersionPrefix `
+		-BuildNumber $BuildNumber
+}
+catch {
+	Write-Host "##teamcity[message status='ERROR' text='Unexpected error occurred']"
+	Write-Host "##teamcity[buildStatus status='FAILURE' text='Unexpected error occurred']"
+	throw
+}
