ccab2b90b5535f9389a98f67dfdf66d1b28b70c7
Slava Scherbak <slava.scherbak@gmail.com>
2013-11-01 12:06:56 +0200

    Designer: refactored QuestionView class to reduce size of serialized Questionnire View
    --HG--
    branch : designer-huge-questionnaire-perfomance-fix
    
    
    Former-commit-id: 1b2c9a8b592841b3908967684dc8b01d5f01fd35

---
+++ src/UI/Designer/WB.UI.Designer.Views/Questionnaire/EditQuestionnaireView.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Main.Core.Documents;
+using Main.Core.Entities.SubEntities;
+using Main.Core.View;
+
+namespace WB.UI.Designer.Views.Questionnaire
+{
+    public class EditQuestionnaireView
+    {
+        public EditQuestionnaireView(QuestionnaireDocument source)
+        {
+            CreatedBy = source.CreatedBy;
+            CreationDate = source.CreationDate;
+            LastEntryDate = source.LastEntryDate;
+            PublicKey = source.PublicKey;
+            IsPublic = source.IsPublic;
+            Title = source.Title;
+            Children  = source.Children.Cast<IGroup>().Select(@group => new GroupView(source, @group)).ToList();
+        }
+
+        public IEnumerable<ICompositeView> Children  { get; set; }
+
+        public Guid? CreatedBy { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public DateTime LastEntryDate { get; set; }
+
+        public Guid? Parent { get; set; }
+
+        public Guid PublicKey { get; set; }
+
+        public string Title { get; set; }
+
+        public bool IsPublic { get; set; }
+    }
+}
+
