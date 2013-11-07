a4fbb5c5488dcf2df00359eb5739cb80ce055aeb
Slava Scherbak <slava.scherbak@gmail.com>
2013-11-07 13:17:40 +0200

    Main.Core: cleaned up Export folder
    Former-commit-id: a9c987d9b4b90ead3ac71a8d18195e1c3bacf252

--- src/Core/BoundedContexts/Supervisor/WB.Core.BoundedContexts.Supervisor/Implementation/Services/DataExport/DataExportService.cs
+++ src/Core/BoundedContexts/Supervisor/WB.Core.BoundedContexts.Supervisor/Implementation/Services/DataExport/DataExportService.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
-using Main.Core.Export;
 using Main.Core.View;
 using WB.Core.BoundedContexts.Supervisor.Services;
 using WB.Core.BoundedContexts.Supervisor.Views.DataExport;
