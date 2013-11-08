e9a08a382c7b7a6d51b59905770cd29b709fa9af
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2013-11-08 13:56:35 +0200

    Shared.Web: removed WebConfigHelper, introduced NameValueCollectionExtensions instead KP-2802
    Former-commit-id: e78d1b90f56f077497d31f1a9213c46aa0c4339a

---
+++ src/UI/Shared/WB.UI.Shared.Web/Extensions/NameValueCollectionExtensions.cs
@@ -0,0 +1,23 @@
+using System.Collections.Specialized;
+
+namespace WB.UI.Shared.Web.Extensions
+{
+    public static class NameValueCollectionExtensions
+    {
+        public static string GetString(this NameValueCollection collection, string key, string @default = null)
+        {
+            return collection[key] ?? @default;
+        }
+
+        public static bool GetBool(this NameValueCollection collection, string key, bool @default)
+        {
+            return collection.GetString(key).ToBool(@default);
+        }
+
+        public static int GetInt(this NameValueCollection collection, string key, int @default)
+        {
+            return collection.GetString(key).ToInt(@default);
+        }
+    }
+
+}
