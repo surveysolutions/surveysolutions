d34ade2b9d6ce907451ac40098a39c67f77a9a7e
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2013-11-08 17:52:16 -0500

    CAPI.Android moved and renamed KP-2818
    --HG--
    branch : Capi-Tester
    
    
    Former-commit-id: 6b5d8da0a13a80822055ce59a4c9d78564c86ff8

---
+++ src/UI/Capi/WB.UI.Capi.DataCollection/Controls/Navigation/NavigationSpinnerAdapter.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using Android.Views;
+using Android.Widget;
+
+namespace WB.UI.Capi.DataCollection.Controls.Navigation
+{
+    public class NavigationSpinnerAdapter : BaseAdapter
+    {
+        private readonly IList<NavigationItem> _spinnerItems;
+
+        public NavigationSpinnerAdapter(IList<NavigationItem> items)
+        {
+            this._spinnerItems = items;
+        }
+
+        public override Java.Lang.Object GetItem(int position)
+        {
+            return this._spinnerItems[position].Title;
+        }
+
+        public override long GetItemId(int position)
+        {
+            return position;
+        }
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            var view = convertView as TextView ?? new TextView(parent.Context);
+
+            view.Text = this._spinnerItems[position].Title;
+            view.Gravity = GravityFlags.Right;
+            view.SetPadding(10, 10, 10, 10);
+
+            return view;
+        }
+
+        public override int Count
+        {
+            get { return this._spinnerItems.Count; }
+        }
+    }
+}
---
+++ src/UI/Capi/WB.UI.Capi.DataCollection/Core/AjustableListView.cs
@@ -0,0 +1,30 @@
+using Android.Content;
+using Android.Graphics;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+
+namespace WB.UI.Capi.DataCollection.Core
+{
+    public class AjustableListView : ListView
+    {
+        private ViewGroup.LayoutParams layoutParams;
+        private int old_count = 0;
+
+        public AjustableListView(Context context, IAttributeSet attrs) : base(context, attrs)
+        {
+        }
+
+        protected override void OnDraw(Canvas canvas)
+        {
+            if (this.Count != this.old_count)
+            {
+                this.old_count = this.Count;
+                this.layoutParams = this.LayoutParameters;
+                this.layoutParams.Height = this.Count * (this.old_count > 0 ? this.GetChildAt(0).Height : 0);
+                this.LayoutParameters = this.layoutParams;
+            }
+            base.OnDraw(canvas);
+        }
+    }
+}
