b27b4bfc4461a4fc8f9ce965a747c7072f7177ea
Anatoliy 'TLK' Kolesnick <none@none>
2013-11-08 13:58:42 +0200

    Shared.Web: removed not used methods from string extenstions KP-2802
    Former-commit-id: a9f5f40be3d53b79f8fb351c69a3bd3aecc4f2d6

--- src/UI/Shared/WB.UI.Shared.Web/Extensions/StringExtensions.cs
+++ src/UI/Shared/WB.UI.Shared.Web/Extensions/StringExtensions.cs
@@ -22,37 +22,11 @@ namespace WB.UI.Shared.Web.Extensions
             return Int32.TryParse(value, out result) ? result : @default;
         }
 
-        public static byte ToByte(this string value, byte @default = 0)
-        {
-            byte result;
-
-            return Byte.TryParse(value, out result) ? result : @default;
-        }
-
         public static bool ToBool(this string value, bool @default = false)
         {
             bool result;
 
-            return value.HasValue() && Boolean.TryParse(value, out result) ? result : @default;
-        }
-
-        public static decimal ToDecimal(this string value, decimal @default = 0)
-        {
-            decimal result;
-
-            return value.HasValue() && Decimal.TryParse(value, out result) ? result : @default;
-        }
-
-        public static DateTime ToDateTime(this string value, DateTime @default)
-        {
-            DateTime result;
-
-            return value.HasValue() && DateTime.TryParse(value, out result) ? result : @default;
-        }
-
-        public static bool HasValue(this string value)
-        {
-            return !String.IsNullOrEmpty(value);
+            return Boolean.TryParse(value, out result) ? result : @default;
         }
     }
 }
