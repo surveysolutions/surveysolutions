ddc40a0d6abe068d76d433f5d108487a51f3493b
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2013-11-08 19:17:16 -0500

    Designer: minor Routes Fix
    --HG--
    branch : Capi-Tester
    
    
    Former-commit-id: 657c1201c09e0636b0ddcc13244fe4b4afb5208b

--- src/UI/Designer/WB.UI.Designer/App_Start/WebApiConfig.cs
+++ src/UI/Designer/WB.UI.Designer/App_Start/WebApiConfig.cs
@@ -11,6 +11,11 @@ namespace WB.UI.Designer.App_Start
             config.MessageHandlers.Add(new BasicAuthMessageHandler());
 
             config.Routes.MapHttpRoute(
+                name: "DefaultApiWithAction",
+                routeTemplate: "api/{controller}/{action}/{id}",
+                defaults: new { id = RouteParameter.Optional });
+
+            config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
