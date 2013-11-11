884cf1087827cce117c534d5b300c452f64dd5c2
Anatoliy 'TLK' Kolesnick <none@none>
2013-11-11 19:12:13 +0200

    Designer\Questionnaire: added tests on command handlers roster properties KP-2824
    Former-commit-id: 0be0fdb4714f512552e8b030baf963cd62f937c0

---
+++ src/Tests/WB.Core.BoundedContexts.Designer.Tests/QuestionnaireTests/when_updating_group_and_roster_size_question_id_is_not_specified.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Machine.Specifications;
+using Main.Core.Entities.SubEntities;
+using Main.Core.Events.Questionnaire;
+using Ncqrs.Spec;
+using WB.Core.BoundedContexts.Designer.Aggregates;
+
+namespace WB.Core.BoundedContexts.Designer.Tests.QuestionnaireTests
+{
+    internal class when_updating_group_and_roster_size_question_id_is_not_specified : QuestionnaireTestsContext
+    {
+        Establish context = () =>
+        {
+            responsibleId = Guid.Parse("DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD");
+            groupId = Guid.Parse("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+            rosterSizeQuestionId = null;
+
+            questionnaire = CreateQuestionnaireWithOneGroup(responsibleId, groupId: groupId);
+
+            eventContext = new EventContext();
+        };
+
+        Because of = () =>
+            questionnaire.UpdateGroup(groupId, responsibleId, "title", Propagate.None, rosterSizeQuestionId, null, null);
+
+        Cleanup stuff = () =>
+        {
+            eventContext.Dispose();
+            eventContext = null;
+        };
+
+        It should_raise_GroupUpdated_event = () =>
+            eventContext.ShouldContainEvent<GroupUpdated>();
+
+        It should_raise_GroupUpdated_event_with_IsRoster_equal_false = () =>
+            eventContext.GetSingleEvent<GroupUpdated>()
+                .IsRoster.ShouldEqual(false);
+
+        private static EventContext eventContext;
+        private static Questionnaire questionnaire;
+        private static Guid responsibleId;
+        private static Guid groupId;
+        private static Guid? rosterSizeQuestionId;
+    }
+}
---
+++ src/Tests/WB.Core.BoundedContexts.Designer.Tests/ShouldExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Machine.Specifications;
+using Ncqrs.Spec;
+
+namespace WB.Core.BoundedContexts.Designer.Tests
+{
+    internal static class ShouldExtensions
+    {
+        public static void ShouldContainEvent<TEvent>(this EventContext eventContext, Func<TEvent, bool> condition = null)
+        {
+            if (condition == null)
+            {
+                eventContext.Events.ShouldContain(@event
+                    => @event.Payload is TEvent);
+            }
+            else
+            {
+                eventContext.Events.ShouldContain(@event
+                    => @event.Payload is TEvent
+                        && condition.Invoke((TEvent)@event.Payload));
+            }
+        }
+
+        public static void ShouldNotContainEvent<TEvent>(this EventContext eventContext, Func<TEvent, bool> condition = null)
+        {
+            if (condition == null)
+            {
+                eventContext.Events.ShouldNotContain(@event
+                    => @event.Payload is TEvent);
+            }
+            else
+            {
+                eventContext.Events.ShouldNotContain(@event
+                    => @event.Payload is TEvent
+                        && condition.Invoke((TEvent)@event.Payload));
+            }
+        }
+    }
+}
