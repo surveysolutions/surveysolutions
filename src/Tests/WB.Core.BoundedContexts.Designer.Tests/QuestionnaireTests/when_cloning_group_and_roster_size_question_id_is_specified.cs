d9edcbcc368c7468a94e91d7c6489d8cca002e49
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2013-11-11 19:12:13 +0200

    Designer\Questionnaire: added tests on command handlers roster properties KP-2824
    Former-commit-id: 0be0fdb4714f512552e8b030baf963cd62f937c0

---
+++ src/Tests/WB.Core.BoundedContexts.Designer.Tests/QuestionnaireTests/when_cloning_group_and_roster_size_question_id_is_specified.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Machine.Specifications;
+using Main.Core.Entities.SubEntities;
+using Main.Core.Events.Questionnaire;
+using Ncqrs.Spec;
+using WB.Core.BoundedContexts.Designer.Aggregates;
+
+namespace WB.Core.BoundedContexts.Designer.Tests.QuestionnaireTests
+{
+    internal class when_cloning_group_and_roster_size_question_id_is_specified : QuestionnaireTestsContext
+    {
+        Establish context = () =>
+        {
+            responsibleId = Guid.Parse("DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD");
+            sourceGroupId = Guid.Parse("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+            rosterSizeQuestionId = Guid.Parse("11111111111111111111111111111111");
+
+            questionnaire = CreateQuestionnaire(responsibleId: responsibleId);
+            questionnaire.Apply(new NewGroupAdded { PublicKey = sourceGroupId });
+            questionnaire.Apply(new NewQuestionAdded { PublicKey = rosterSizeQuestionId, QuestionType = QuestionType.Numeric });
+
+            eventContext = new EventContext();
+        };
+
+        Because of = () =>
+            questionnaire.CloneGroupWithoutChildren(Guid.NewGuid(), responsibleId, "title", Propagate.None, rosterSizeQuestionId, null, null, null, sourceGroupId, 0);
+
+        Cleanup stuff = () =>
+        {
+            eventContext.Dispose();
+            eventContext = null;
+        };
+
+        It should_raise_GroupCloned_event = () =>
+            eventContext.ShouldContainEvent<GroupCloned>();
+
+        It should_raise_GroupCloned_event_with_IsRoster_equal_true = () =>
+            eventContext.GetSingleEvent<GroupCloned>()
+                .IsRoster.ShouldEqual(true);
+
+        It should_raise_GroupCloned_event_with_RosterSizeQuestionId_equal_to_specified_question_id = () =>
+            eventContext.GetSingleEvent<GroupCloned>()
+                .RosterSizeQuestionId.ShouldEqual(rosterSizeQuestionId);
+
+        private static EventContext eventContext;
+        private static Questionnaire questionnaire;
+        private static Guid responsibleId;
+        private static Guid sourceGroupId;
+        private static Guid rosterSizeQuestionId;
+    }
+}
---
+++ src/Tests/WB.Core.BoundedContexts.Designer.Tests/QuestionnaireTests/when_updating_group_and_roster_size_question_id_is_specified.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Machine.Specifications;
+using Main.Core.Entities.SubEntities;
+using Main.Core.Events.Questionnaire;
+using Ncqrs.Spec;
+using WB.Core.BoundedContexts.Designer.Aggregates;
+
+namespace WB.Core.BoundedContexts.Designer.Tests.QuestionnaireTests
+{
+    internal class when_updating_group_and_roster_size_question_id_is_specified : QuestionnaireTestsContext
+    {
+        Establish context = () =>
+        {
+            responsibleId = Guid.Parse("DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD");
+            groupId = Guid.Parse("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+            rosterSizeQuestionId = Guid.Parse("11111111111111111111111111111111");
+
+            questionnaire = CreateQuestionnaire(responsibleId: responsibleId);
+            questionnaire.Apply(new NewGroupAdded { PublicKey = groupId });
+            questionnaire.Apply(new NewQuestionAdded { PublicKey = rosterSizeQuestionId, QuestionType = QuestionType.Numeric });
+
+            eventContext = new EventContext();
+        };
+
+        Because of = () =>
+            questionnaire.UpdateGroup(groupId, responsibleId, "title", Propagate.None, rosterSizeQuestionId, null, null);
+
+        Cleanup stuff = () =>
+        {
+            eventContext.Dispose();
+            eventContext = null;
+        };
+
+        It should_raise_GroupUpdated_event = () =>
+            eventContext.ShouldContainEvent<GroupUpdated>();
+
+        It should_raise_GroupUpdated_event_with_IsRoster_equal_true = () =>
+            eventContext.GetSingleEvent<GroupUpdated>()
+                .IsRoster.ShouldEqual(true);
+
+        It should_raise_GroupUpdated_event_with_RosterSizeQuestionId_equal_to_specified_question_id = () =>
+            eventContext.GetSingleEvent<GroupUpdated>()
+                .RosterSizeQuestionId.ShouldEqual(rosterSizeQuestionId);
+
+        private static EventContext eventContext;
+        private static Questionnaire questionnaire;
+        private static Guid responsibleId;
+        private static Guid groupId;
+        private static Guid rosterSizeQuestionId;
+    }
+}
