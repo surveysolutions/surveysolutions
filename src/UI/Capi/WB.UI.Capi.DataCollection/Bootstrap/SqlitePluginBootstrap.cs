11d6dd6f218af831c514ee5675882a006a8d2bd8
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2013-11-11 12:17:07 -0500

    fix slite references
    --HG--
    branch : Capi-Tester
    
    
    Former-commit-id: d4c93c2a7db7ae63f8311e4f9d39c49c572be6e5

--- src/UI/Capi/WB.UI.Capi.DataCollection/Bootstrap/SqlitePluginBootstrap.cs
+++ src/UI/Capi/WB.UI.Capi.DataCollection/Bootstrap/SqlitePluginBootstrap.cs
@@ -3,7 +3,7 @@ using Cirrious.CrossCore.Plugins;
 namespace WB.UI.Capi.DataCollection.Bootstrap
 {
     public class SqlitePluginBootstrap
-        : MvxPluginBootstrapAction<Cirrious.MvvmCross.Community.Plugins.Sqlite.PluginLoader>
+        : MvxPluginBootstrapAction<Cirrious.MvvmCross.Plugins.Sqlite.PluginLoader>
     {
     }
 }
