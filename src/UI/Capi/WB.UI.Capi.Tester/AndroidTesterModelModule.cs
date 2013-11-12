d2dc165e9ae6decaf59a0a742a0d5622d65666ed
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2013-11-12 11:02:22 -0500

    Capi: Alpha version of new Tester App
    --HG--
    branch : Capi-Tester
    
    
    Former-commit-id: b90fb4dfc74388c9bc1b3e1e597467975b25c82f

---
+++ src/UI/Capi/WB.UI.Capi.Tester/AndroidTesterModelModule.cs
@@ -0,0 +1,75 @@
+using CAPI.Android.Core.Model.Authorization;
+using CAPI.Android.Core.Model.Backup;
+using CAPI.Android.Core.Model.ChangeLog;
+
+using CAPI.Android.Core.Model.ReadSideStore;
+using CAPI.Android.Core.Model.SnapshotStore;
+
+using CAPI.Android.Core.Model.ViewModel.Dashboard;
+
+using Main.Core.View;
+using Ncqrs.Domain.Storage;
+using Ncqrs.Eventing.Storage;
+using Ninject.Modules;
+using WB.Core.BoundedContexts.Capi;
+using WB.Core.BoundedContexts.Capi.Views.InterviewDetails;
+using WB.Core.Infrastructure.Backup;
+using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
+using WB.Core.SharedKernel.Structures.Synchronization;
+using WB.Core.SharedKernels.DataCollection.Views.Questionnaire;
+
+namespace CAPI.Android.Tester
+{
+    public class AndroidTesterModelModule : NinjectModule
+    {
+        public override void Load()
+        {
+            var evenStore = new InMemoryEventStore(); //new MvvmCrossSqliteEventStore(EventStoreDatabaseName);
+            var snapshotStore = new AndroidSnapshotStore();
+            //var denormalizerStore = new SqliteDenormalizerStore(ProjectionStoreName);
+            //var loginStore = new SqliteReadSideRepositoryAccessor<LoginDTO>(denormalizerStore);
+            //var surveyStore = new SqliteReadSideRepositoryAccessor<SurveyDto>(denormalizerStore);
+            //var questionnaireStore = new SqliteReadSideRepositoryAccessor<QuestionnaireDTO>(denormalizerStore);
+            //var publicStore = new SqliteReadSideRepositoryAccessor<PublicChangeSetDTO>(denormalizerStore);
+            //var draftStore = new SqliteReadSideRepositoryAccessor<DraftChangesetDTO>(denormalizerStore);
+            //var fileSystem = new FileStorageService();
+            //var interviewMetaInfoFactory = new InterviewMetaInfoFactory(questionnaireStore);
+            //var changeLogStore = new FileChangeLogStore(interviewMetaInfoFactory);
+            //var syncCacher = new FileSyncCacher();
+            //var sharedPreferencesBackup = new SharedPreferencesBackupOperator();
+            
+            var templateStore = new FileReadSideRepositoryWriter<QuestionnaireDocumentVersioned>();
+            var propagationStructureStore = new FileReadSideRepositoryWriter<QuestionnairePropagationStructure>();
+
+            //var bigSurveyStore = new BackupableInMemoryReadSideRepositoryAccessor<InterviewViewModel>();
+
+            this.Bind<IEventStore>().ToConstant(evenStore);
+            this.Bind<ISnapshotStore>().ToConstant(snapshotStore);
+            this.Bind<IReadSideRepositoryWriter<QuestionnaireDocumentVersioned>>().ToConstant(templateStore);
+            this.Bind<IReadSideRepositoryWriter<QuestionnairePropagationStructure>>().ToConstant(propagationStructureStore);
+            //this.Bind<IReadSideRepositoryWriter<LoginDTO>>().ToConstant(loginStore);
+            //this.Bind<IReadSideRepositoryReader<LoginDTO>>().ToConstant(loginStore);
+            //this.Bind<IFilterableReadSideRepositoryReader<LoginDTO>>().ToConstant(loginStore);
+            //this.Bind<IReadSideRepositoryWriter<InterviewViewModel>>().ToConstant(bigSurveyStore);
+            //this.Bind<IReadSideRepositoryReader<InterviewViewModel>>().ToConstant(bigSurveyStore);
+            //this.Bind<IReadSideRepositoryWriter<SurveyDto>>().ToConstant(surveyStore);
+            //this.Bind<IFilterableReadSideRepositoryReader<SurveyDto>>().ToConstant(surveyStore);
+            //this.Bind<IReadSideRepositoryWriter<QuestionnaireDTO>>().ToConstant(questionnaireStore);
+            //this.Bind<IFilterableReadSideRepositoryReader<QuestionnaireDTO>>().ToConstant(questionnaireStore);
+            //this.Bind<IReadSideRepositoryWriter<PublicChangeSetDTO>>().ToConstant(publicStore);
+            //this.Bind<IFilterableReadSideRepositoryWriter<DraftChangesetDTO>>().ToConstant(draftStore);
+            //this.Bind<IFileStorageService>().ToConstant(fileSystem);
+            //this.Bind<IChangeLogManipulator>().To<ChangeLogManipulator>().InSingletonScope();
+            //this.Bind<IDataCollectionAuthentication, IAuthentication>().To<AndroidAuthentication>().InSingletonScope();
+            //this.Bind<IChangeLogStore>().ToConstant(changeLogStore);
+            //this.Bind<ISyncCacher>().ToConstant(syncCacher);
+            this.Bind<IViewFactory<DashboardInput, DashboardModel>>().To<DashboardFactory>();
+            //this.Bind<IViewFactory<InterviewMetaInfoInputModel, InterviewMetaInfo>>().ToConstant(interviewMetaInfoFactory);
+
+            /*this.Bind<IBackup>()
+                .ToConstant(new DefaultBackup(evenStore, changeLogStore, fileSystem, denormalizerStore,
+                                              bigSurveyStore, syncCacher, sharedPreferencesBackup, templateStore, propagationStructureStore));*/
+
+        }
+    }
+}
