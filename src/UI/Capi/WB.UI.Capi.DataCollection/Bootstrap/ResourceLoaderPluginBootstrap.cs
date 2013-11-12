28f5d86a6f48546ecdfba4084ad3baeb8eaf05f6
Anastasya Kholodova <nastya.kholodova@gmail.com>
2013-11-12 15:33:08 -0500

    Tester: code review KP-2818
    Former-commit-id: 801c79eef7020e7a617424be2de60e3823d6cb4b

--- src/UI/Capi/WB.UI.Capi.DataCollection/Bootstrap/ResourceLoaderPluginBootstrap.cs
+++
@@ -1,9 +0,0 @@
-using Cirrious.CrossCore.Plugins;
-
-namespace WB.UI.Capi.DataCollection.Bootstrap
-{
-    public class ResourceLoaderPluginBootstrap
-        : MvxPluginBootstrapAction<Cirrious.MvvmCross.Plugins.ResourceLoader.PluginLoader>
-    {
-    }
-}
