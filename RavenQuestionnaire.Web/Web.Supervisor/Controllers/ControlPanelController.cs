377c8a3cf5d1a767cb7a4230d45635abbc1a9086
Anatoliy 'TLK' Kolesnick <none@none>
2013-11-13 17:02:17 +0200

    Supervisor\ControlPanel: renamed ReadLayer -> ReadSide
    Former-commit-id: e50d607efde24061526457232baaca5db99a3281

--- RavenQuestionnaire.Web/Web.Supervisor/Controllers/ControlPanelController.cs
+++ RavenQuestionnaire.Web/Web.Supervisor/Controllers/ControlPanelController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Web.Mvc;
+using Microsoft.Practices.ServiceLocation;
 using WB.Core.Infrastructure.ReadSide;
 
 namespace Web.Supervisor.Controllers
@@ -6,43 +7,57 @@ namespace Web.Supervisor.Controllers
     [AllowAnonymous]
     public class ControlPanelController : Controller
     {
-        private readonly IReadSideAdministrationService readSideAdministrationService;
+        private readonly IServiceLocator serviceLocator;
 
-        public ControlPanelController(IReadSideAdministrationService readSideAdministrationService)
+        public ControlPanelController(IServiceLocator serviceLocator)
         {
-            this.readSideAdministrationService = readSideAdministrationService;
+            this.serviceLocator = serviceLocator;
+        }
+
+        /// <remarks>
+        /// Getting dependency via service location ensures that parts of control panel not using it will always work.
+        /// E.g. If Raven connection fails to be established then NConfig info still be available.
+        /// </remarks>
+        private IReadSideAdministrationService ReadSideAdministrationService
+        {
+            get { return this.serviceLocator.GetInstance<IReadSideAdministrationService>(); }
         }
 
         public ActionResult ReadLayer()
         {
-            return this.View(this.readSideAdministrationService.GetAllAvailableHandlers());
+            return this.RedirectToActionPermanent("ReadSide");
+        }
+
+        public ActionResult ReadSide()
+        {
+            return this.View(this.ReadSideAdministrationService.GetAllAvailableHandlers());
         }
 
         [OutputCache(NoStore = true, Duration = 0, VaryByParam = "*")]
-        public string GetReadLayerStatus()
+        public string GetReadSideStatus()
         {
-            return this.readSideAdministrationService.GetReadableStatus();
+            return this.ReadSideAdministrationService.GetReadableStatus();
         }
 
-        public ActionResult RebuildReadLayerPartially(string[] handlers)
+        public ActionResult RebuildReadSidePartially(string[] handlers)
         {
-            this.readSideAdministrationService.RebuildViewsAsync(handlers);
+            this.ReadSideAdministrationService.RebuildViewsAsync(handlers);
             this.TempData["CheckedHandlers"] = handlers;
-            return this.RedirectToAction("ReadLayer");
+            return this.RedirectToAction("ReadSide");
         }
 
-        public ActionResult RebuildReadLayer()
+        public ActionResult RebuildReadSide()
         {
-            this.readSideAdministrationService.RebuildAllViewsAsync();
+            this.ReadSideAdministrationService.RebuildAllViewsAsync();
 
-            return this.RedirectToAction("ReadLayer");
+            return this.RedirectToAction("ReadSide");
         }
 
-        public ActionResult StopReadLayerRebuilding()
+        public ActionResult StopReadSideRebuilding()
         {
-            this.readSideAdministrationService.StopAllViewsRebuilding();
+            this.ReadSideAdministrationService.StopAllViewsRebuilding();
 
-            return this.RedirectToAction("ReadLayer");
+            return this.RedirectToAction("ReadSide");
         }
     }
 }
