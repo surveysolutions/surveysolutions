4ac6cf4f0552643dd4d19c30f9121d8da5203b98
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2013-11-13 16:03:01 -0500

    Designer: Returned types for tester Sevices are changed
    --HG--
    branch : Capi-Tester
    
    
    Former-commit-id: 85a642ab0253c12d1181038b108dafcc03210d37

---
+++ src/Core/SharedKernel/WB.Core.SharedKernel.Structures/Synchronization/Designer/QuestionnaireListItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace WB.Core.SharedKernel.Structures.Synchronization.Designer
+{
+    public class QuestionnaireListItem
+    {
+        public Guid Id;
+        public string Title;
+
+        public QuestionnaireListItem(Guid id, string title)
+        {
+            Id = id;
+            Title = title;
+        }
+    }
+}
---
+++ src/Core/SharedKernel/WB.Core.SharedKernel.Structures/Synchronization/Designer/QuestionnaireSyncPackage.cs
@@ -0,0 +1,12 @@
+﻿namespace WB.Core.SharedKernel.Structures.Synchronization.Designer
+{
+    public class QuestionnaireSyncPackage
+    {
+        public string Questionnaire;
+
+        public QuestionnaireSyncPackage(string questionnaire)
+        {
+            Questionnaire = questionnaire;
+        }
+    }
+}
