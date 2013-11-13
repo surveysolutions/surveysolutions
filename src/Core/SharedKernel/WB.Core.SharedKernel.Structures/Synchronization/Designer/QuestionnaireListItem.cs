7efa8e16078c469e4361599b6c3f7a357e44cfab
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2013-11-13 18:34:00 -0500

    Designer: Fix serrialization Issue on Tester services
    --HG--
    branch : Capi-Tester
    
    
    Former-commit-id: 1b3505ea00ad6ae2bb10b8f8488c8fe61c7fe166

--- src/Core/SharedKernel/WB.Core.SharedKernel.Structures/Synchronization/Designer/QuestionnaireListItem.cs
+++ src/Core/SharedKernel/WB.Core.SharedKernel.Structures/Synchronization/Designer/QuestionnaireListItem.cs
@@ -4,13 +4,7 @@ namespace WB.Core.SharedKernel.Structures.Synchronization.Designer
 {
     public class QuestionnaireListItem
     {
-        public Guid Id;
-        public string Title;
-
-        public QuestionnaireListItem(Guid id, string title)
-        {
-            Id = id;
-            Title = title;
-        }
+        public Guid Id { get; set; }
+        public string Title { get; set; }
     }
 }
