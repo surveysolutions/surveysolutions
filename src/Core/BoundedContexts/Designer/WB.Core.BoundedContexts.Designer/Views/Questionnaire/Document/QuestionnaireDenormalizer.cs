30a7f43a1fa47cc473e2428df0ca85e1d5c59096
Slava Scherbak <slava.scherbak@gmail.com>
2013-11-18 15:24:31 +0200

    BC\Designer: updated Questionnaire denormalizer to support capability with old propagation logic KP-2864
    Former-commit-id: fcdf2e91368452ff4222c3d7589b81eee4be19e4

--- src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Views/Questionnaire/Document/QuestionnaireDenormalizer.cs
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Views/Questionnaire/Document/QuestionnaireDenormalizer.cs
@@ -231,8 +231,7 @@ namespace WB.Core.BoundedContexts.Designer.Views.Questionnaire.Document
         protected void AddQuestion(IPublishableEvent evnt, Guid groupId, QuestionData data)
         {
             QuestionnaireDocument item = this.documentStorage.GetById(evnt.EventSourceId);
-            IQuestion result =
-                new QuestionFactory().CreateQuestion(data);
+            IQuestion result = new QuestionFactory().CreateQuestion(data);
 
             if (result == null)
             {
@@ -240,6 +239,9 @@ namespace WB.Core.BoundedContexts.Designer.Views.Questionnaire.Document
             }
 
             item.Add(result, groupId, null);
+
+            item.UpdateRosterGroupsIfNeeded(data.Triggers, data.PublicKey);
+
             this.UpdateQuestionnaire(evnt, item);
         }
 
@@ -257,21 +259,24 @@ namespace WB.Core.BoundedContexts.Designer.Views.Questionnaire.Document
 
             item.ReplaceQuestionWithNew(question, newQuestion);
 
+            item.UpdateRosterGroupsIfNeeded(data.Triggers, data.PublicKey);
+
             this.UpdateQuestionnaire(evnt, item);
         }
 
         protected void CloneQuestion(IPublishableEvent evnt, Guid groupId,int index, QuestionData data)
         {
             QuestionnaireDocument item = this.documentStorage.GetById(evnt.EventSourceId);
-            IQuestion result =
-                new QuestionFactory().CreateQuestion(data);
+            IQuestion result = new QuestionFactory().CreateQuestion(data);
 
             if (result == null)
             {
                 return;
             }
-#warning Slava: uncomment this line and get rig of read and write side objects
             item.Insert(index, result, groupId);
+
+            item.UpdateRosterGroupsIfNeeded(data.Triggers, data.PublicKey);
+
             this.UpdateQuestionnaire(evnt, item);
         }
 
