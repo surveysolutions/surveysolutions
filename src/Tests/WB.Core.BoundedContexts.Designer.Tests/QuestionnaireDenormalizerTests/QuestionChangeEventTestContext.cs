ae4fac823c5c848f2479a0a93c069dd0bb5cbc76
Slava Scherbak <slava.scherbak@gmail.com>
2013-11-19 18:21:38 +0200

    BC\Designer: removed Paropagateble field from group's events KP-2859
    --HG--
    branch : next
    
    
    Former-commit-id: 0be691a956bd25925e52a3df1f76ba8bd0fd3ad5

--- src/Tests/WB.Core.BoundedContexts.Designer.Tests/QuestionnaireDenormalizerTests/QuestionChangeEventTestContext.cs
+++ src/Tests/WB.Core.BoundedContexts.Designer.Tests/QuestionnaireDenormalizerTests/QuestionChangeEventTestContext.cs
@@ -66,8 +66,7 @@ namespace WB.Core.BoundedContexts.Designer.Tests.QuestionnaireDenormalizerTests
         {
             return new GroupUpdated
             {
-                GroupPublicKey = groupId,
-                Propagateble = propagationKind
+                GroupPublicKey = groupId
             };
         }
 
