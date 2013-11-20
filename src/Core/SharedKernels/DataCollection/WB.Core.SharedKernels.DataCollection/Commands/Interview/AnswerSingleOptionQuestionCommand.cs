d14aaf383bc4bcbcda831cc6d7e9634475238f58
Roman Lytvyn <litvin.roma@gmail.com>
2013-11-20 19:45:28 +0200

    supervisor: fixed exception with roster vactors on an interview details page. KP-2881
    Former-commit-id: dcf6c955fc750dcbbbc8a0e6f46fc4dca24422dc

--- src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/Commands/Interview/AnswerSingleOptionQuestionCommand.cs
+++ src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/Commands/Interview/AnswerSingleOptionQuestionCommand.cs
@@ -9,8 +9,8 @@ namespace WB.Core.SharedKernels.DataCollection.Commands.Interview
     {
         public decimal SelectedValue { get; private set; }
 
-        public AnswerSingleOptionQuestionCommand(Guid interviewId, Guid userId, Guid questionId, int[] propagationVector, DateTime answerTime, decimal selectedValue)
-            : base(interviewId, userId, questionId, propagationVector, answerTime)
+        public AnswerSingleOptionQuestionCommand(Guid interviewId, Guid userId, Guid questionId, int[] rosterVector, DateTime answerTime, decimal selectedValue)
+            : base(interviewId, userId, questionId, rosterVector, answerTime)
         {
             this.SelectedValue = selectedValue;
         }
--- src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/Commands/Interview/AnswerTextQuestionCommand.cs
+++ src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/Commands/Interview/AnswerTextQuestionCommand.cs
@@ -9,8 +9,8 @@ namespace WB.Core.SharedKernels.DataCollection.Commands.Interview
     {
         public string Answer { get; private set; }
 
-        public AnswerTextQuestionCommand(Guid interviewId, Guid userId, Guid questionId, int[] propagationVector, DateTime answerTime, string answer)
-            : base(interviewId, userId, questionId, propagationVector, answerTime)
+        public AnswerTextQuestionCommand(Guid interviewId, Guid userId, Guid questionId, int[] rosterVector, DateTime answerTime, string answer)
+            : base(interviewId, userId, questionId, rosterVector, answerTime)
         {
             this.Answer = answer;
         }
