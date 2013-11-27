78934687e3e7f5565394149b43fa24cc3b80bb3f
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2013-11-27 18:20:35 -0500

    Infrastructure: update new ravenDB files
    --HG--
    branch : rebuild-speedup
    
    
    Former-commit-id: 4712f4341cbe9477b875f962bc9a1b9ef3f99637

---
+++ src/Core/GenericSubdomains/Logging/WB.Core.GenericSubdomains.Logging.NLog/DummyLogger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NLog;
+
+namespace WB.Core.GenericSubdomains.Logging.NLog
+{
+    internal class DummyLogger : ILogger
+    {
+        public DummyLogger()
+        {}
+
+        public void Debug(string message, Exception exception = null)
+        {}
+
+        public void Info(string message, Exception exception = null)
+        {}
+
+
+        public void Warn(string message, Exception exception = null)
+        {}
+
+        public void Error(string message, Exception exception = null)
+        {}
+
+        public void Fatal(string message, Exception exception = null)
+        {}
+    }
+}
