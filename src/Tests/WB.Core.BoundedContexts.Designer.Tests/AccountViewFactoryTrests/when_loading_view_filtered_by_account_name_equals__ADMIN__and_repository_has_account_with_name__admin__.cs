b55f21a9c45fcaa7761186ab19182437a5e64b2c
Slava Scherbak <slava.scherbak@gmail.com>
2013-11-28 16:54:50 +0200

    Designer: added fixes after code review. Added test for account factory KP-2873
    Former-commit-id: 2ee466b155e627c06db2b4dc9e0f6d9f429f0cb4

---
+++ src/Tests/WB.Core.BoundedContexts.Designer.Tests/AccountViewFactoryTrests/when_loading_view_filtered_by_account_name_equals__ADMIN__and_repository_has_account_with_name__admin__.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using Machine.Specifications;
+using Moq;
+using WB.Core.BoundedContexts.Designer.Views.Account;
+using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
+using It = Machine.Specifications.It;
+using it = Moq.It;
+
+
+namespace WB.Core.BoundedContexts.Designer.Tests.AccountViewFactoryTrests
+{
+    public class when_loading_view_filtered_by_account_name_equals__ADMIN__and_repository_has_account_with_name__admin__ : AccountFactoryTestsContext
+    {
+        Establish context = () =>
+        {
+            inputWithFilterByAccountName = CreateAccountViewInputModel(accountName: "ADMIN");
+
+            var accountsRepositoryMock = new Mock<IQueryableReadSideRepositoryReader<AccountDocument>>();
+
+            var repositoryDocuments = new[] { CreateAccountDocument(userName: "admin") };
+
+            accountsRepositoryMock
+                .Setup(x => x.Query<AccountView>(it.IsAny<Func<IQueryable<AccountDocument>, AccountView>>()))
+                .Returns<Func<IQueryable<AccountDocument>, AccountView>>(func => func.Invoke(repositoryDocuments.AsQueryable()));
+
+            accountFactory = CreateAccountViewFactory(accountsRepositoryMock: accountsRepositoryMock.Object);
+        };
+
+        Because of = () =>
+            filteredAccount = accountFactory.Load(inputWithFilterByAccountName);
+
+        It should_find_one_account = () =>
+            filteredAccount.ShouldNotBeNull();
+
+        private static AccountView filteredAccount;
+        private static AccountViewInputModel inputWithFilterByAccountName;
+        private static AccountViewFactory accountFactory;
+    }
+}
