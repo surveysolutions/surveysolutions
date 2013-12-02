2c3eeab91841c56e568bfbd5a05e9b5e55395a19
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2013-12-02 14:23:16 +0200

    Tests\ExportedHeaderCollection: corrected test names (they were mismatched) KP-2910
    Former-commit-id: bd54a28029d593f1f53600e16125323f7537c654

--- src/Tests/WB.Core.BoundedContexts.Supervisor.Tests/Views/ExportedHeaderCollectionTests/when_adding_multy_option_linked_question_to_collection_from_questionnaire_containing_2_autopropagated_groups_with_one___.cs
+++ src/Tests/WB.Core.BoundedContexts.Supervisor.Tests/Views/ExportedHeaderCollectionTests/when_adding_multy_option_linked_question_to_collection_from_questionnaire_containing_2_autopropagated_groups_with_one___.cs
@@ -12,28 +12,31 @@ using It = Machine.Specifications.It;
 
 namespace WB.Core.BoundedContexts.Supervisor.Tests.Views.ExportedHeaderCollectionTests
 {
-    internal class when_questionnarie_template_contains_2_autopropagated_groups_with_one_trigger_max_2_rows_and_multy_option_linked_question : ExportedHeaderCollectionTestsContext
+    internal class when_adding_multy_option_linked_question_to_collection_from_questionnaire_containing_2_autopropagated_groups_with_one_trigger_max_2_rows : ExportedHeaderCollectionTestsContext
     {
         Establish context = () =>
         {
-            var autoPropagateQuestionId = Guid.NewGuid();
+            var numericTriggerQuestionId = Guid.NewGuid();
             linkedQuestionId = Guid.NewGuid();
             referencedQuestionId = Guid.NewGuid();
 
             var questionnaireDocument = CreateQuestionnaireDocumentWithOneChapter(
-                new AutoPropagateQuestion("i am auto propagate") { PublicKey = autoPropagateQuestionId, MaxValue = 2 });
+                new NumericQuestion("i am auto propagate") { PublicKey = numericTriggerQuestionId, MaxValue = 2 },
+                new Group("i am roster1") { IsRoster = true, RosterSizeQuestionId = numericTriggerQuestionId },
+                new Group("i am roster2") { IsRoster = true, RosterSizeQuestionId = numericTriggerQuestionId });
 
-            var referenceInfoForLinkedQuestions = CreateReferenceInfoForLinkedQuestionsWithOneLink(linkedQuestionId, autoPropagateQuestionId,
-                referencedQuestionId);
+            var referenceInfoForLinkedQuestions = CreateReferenceInfoForLinkedQuestionsWithOneLink(linkedQuestionId,
+                numericTriggerQuestionId, referencedQuestionId);
 
             headerCollection = CreateExportedHeaderCollection(referenceInfoForLinkedQuestions, questionnaireDocument);
         };
 
         Because of = () =>
-            headerCollection.Add(new MultyOptionsQuestion() { LinkedToQuestionId = referencedQuestionId,PublicKey = linkedQuestionId, QuestionType = QuestionType.MultyOption});
+            headerCollection.Add(new MultyOptionsQuestion() { LinkedToQuestionId = referencedQuestionId, PublicKey = linkedQuestionId, QuestionType = QuestionType.MultyOption });
 
         It should_create_header_with_2_column = () =>
             headerCollection[linkedQuestionId].ColumnNames.Length.ShouldEqual(2);
+
 
         private static ExportedHeaderCollection headerCollection;
         private static Guid linkedQuestionId;
