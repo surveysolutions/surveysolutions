994d2bf6534ac41bce51989b4615bcebdb3cc195
Slava Scherbak <slava.scherbak@gmail.com>
2013-12-03 10:11:08 +0200

    Designer: added tests for Questionnaire AR's new command validations KP-2892
    Former-commit-id: 45d7bd0e88c381ba4fdbce6db1878779ba888ba8

---
+++ src/Tests/WB.Core.BoundedContexts.Designer.Tests/QuestionnaireTests/when_updating_roster_group_that_contains_head_question.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Machine.Specifications;
+using Main.Core.Entities.SubEntities;
+using Main.Core.Events.Questionnaire;
+using Ncqrs.Spec;
+using WB.Core.BoundedContexts.Designer.Aggregates;
+using WB.Core.BoundedContexts.Designer.Events.Questionnaire;
+using WB.Core.BoundedContexts.Designer.Exceptions;
+
+namespace WB.Core.BoundedContexts.Designer.Tests.QuestionnaireTests
+{
+    internal class when_updating_roster_group_that_contains_head_question : QuestionnaireTestsContext
+    {
+        Establish context = () =>
+        {
+            var chapterId = Guid.Parse("CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC");
+            var headQuestionId = Guid.Parse("22222222222222222222222222222222");
+
+            questionnaire = CreateQuestionnaire(responsibleId: responsibleId);
+            questionnaire.Apply(new NewGroupAdded { PublicKey = chapterId });
+            questionnaire.Apply(new NumericQuestionAdded { PublicKey = rosterSizeQuestionId, IsInteger = true, GroupPublicKey = chapterId });
+            questionnaire.Apply(new NewGroupAdded { PublicKey = rosterId });
+            questionnaire.Apply(new NumericQuestionAdded { PublicKey = headQuestionId, IsInteger = true, GroupPublicKey = rosterId, Capital = true});
+
+            questionnaire.Apply(new GroupBecameARoster(responsibleId, rosterId));
+            questionnaire.Apply(new RosterChanged(responsibleId, rosterId, rosterSizeQuestionId));
+
+            eventContext = new EventContext();
+        };
+
+        Because of = () =>
+            exception = Catch.Exception(() =>questionnaire.UpdateGroup(rosterId, responsibleId, "title", null, null, null));
+
+        Cleanup stuff = () =>
+        {
+            eventContext.Dispose();
+            eventContext = null;
+        };
+
+        It should_throw_QuestionnaireException = () =>
+             exception.ShouldBeOfType<QuestionnaireException>();
+
+        It should_throw_exception_with_message_containting__roster__ = () =>
+            exception.Message.ToLower().ShouldContain("roster");
+        
+        It should_throw_exception_with_message_containting__missing__ = () =>
+            exception.Message.ToLower().ShouldContain("become");
+
+        It should_throw_exception_with_message_containting__head__ = () =>
+            exception.Message.ToLower().ShouldContain("head");
+
+        It should_throw_exception_with_message_containting__question__ = () =>
+            exception.Message.ToLower().ShouldContain("question");
+        
+        private static EventContext eventContext;
+        private static Questionnaire questionnaire;
+        private static Guid responsibleId = Guid.Parse("DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD");
+        private static Guid rosterId =  Guid.Parse("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+        private static Guid rosterSizeQuestionId = Guid.Parse("11111111111111111111111111111111");
+        private static Exception exception;
+    }
+}
