79ee788bb6bf9db752a6719de4f475d7f6477805
Slava Scherbak <slava.scherbak@gmail.com>
2013-12-03 15:21:33 +0200

    Supervisor\Batch uploading: added validation, thay file can't be empty KP-2666
    Former-commit-id: d855d0ec1abb1faa2477c0304c1a75ca8f616f4b

---
+++ RavenQuestionnaire.Web/Web.Supervisor.Tests/HQControllerTests/HQControllerTestContext.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Web;
+using Core.Supervisor.Views.Survey;
+using Core.Supervisor.Views.TakeNew;
+using Core.Supervisor.Views.User;
+using Core.Supervisor.Views.UsersAndQuestionnaires;
+using Main.Core.View;
+using Moq;
+using Ncqrs.Commanding.ServiceModel;
+using Questionnaire.Core.Web.Helpers;
+using WB.Core.BoundedContexts.Supervisor.Services;
+using WB.Core.GenericSubdomains.Logging;
+using WB.Core.SharedKernels.DataCollection.Views.Questionnaire;
+using WB.Core.SharedKernels.DataCollection.Views.Questionnaire.BrowseItem;
+using Web.Supervisor.Controllers;
+using Web.Supervisor.Models;
+
+namespace Web.Supervisor.Tests.HQControllerTests
+{
+    internal class HqControllerTestContext
+    {
+        protected static QuestionnaireBrowseItem CreateQuestionnaireBrowseItem()
+        {
+            return new QuestionnaireBrowseItem();
+        }
+
+        protected static BatchUploadModel CreateBatchUploadModel(HttpPostedFileBase file, Guid questionnaireId)
+        {
+            return new BatchUploadModel
+            {
+                QuestionnaireId = questionnaireId,
+                File = file
+            };
+        }
+
+        protected static HQController CreateHqController(
+            IViewFactory<QuestionnaireItemInputModel, QuestionnaireBrowseItem> questionnaireItemFactoryMock = null,
+            ISampleImportService sampleImportServiceMock = null)
+        {
+            return new HQController(
+                Mock.Of<ICommandService>(),
+                Mock.Of<IGlobalInfoProvider>(),
+                Mock.Of<ILogger>(),
+                Mock.Of<IViewFactory<QuestionnaireBrowseInputModel, QuestionnaireBrowseView>>(),
+                questionnaireItemFactoryMock ?? Mock.Of<IViewFactory<QuestionnaireItemInputModel, QuestionnaireBrowseItem>>(),
+                Mock.Of<IViewFactory<UserListViewInputModel, UserListView>>(),
+                Mock.Of<IViewFactory<SurveyUsersViewInputModel, SurveyUsersView>>(),
+                Mock.Of<IViewFactory<TakeNewInterviewInputModel, TakeNewInterviewView>>(),
+                Mock.Of<IViewFactory<UserListViewInputModel, UserListView>>(),
+                sampleImportServiceMock ?? Mock.Of<ISampleImportService>(),
+                Mock.Of<IViewFactory<AllUsersAndQuestionnairesInputModel, AllUsersAndQuestionnairesView>>());
+        }
+    }
+}
---
+++ RavenQuestionnaire.Web/Web.Supervisor/Code/ValidateFileAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web;
+
+namespace Web.Supervisor.Controllers
+{
+    public class ValidateFileAttribute : RequiredAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            var file = value as HttpPostedFileBase;
+
+            return file != null;
+        }
+    }
+}
