0fcb69e4b888479f746ce045316b2746a87bd888
Slava Scherbak <slava.scherbak@gmail.com>
2013-12-05 18:58:05 +0200

    Supervisor: added page to Revalidation of interview by it's id KP-2893
    Former-commit-id: 3ec8f562be8f0f4e90acaa27f532de17012b9537

--- RavenQuestionnaire.Web/Core.Supervisor/Views/Revalidate/RevalidateInterviewFactory.cs
+++ RavenQuestionnaire.Web/Core.Supervisor/Views/Revalidate/RevalidateInterviewFactory.cs
@@ -1,42 +1,47 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Supervisor.Views.Interview;
+using Core.Supervisor.Views.UsersAndQuestionnaires;
+using Main.Core.Documents;
+using Main.Core.View;
+using WB.Core.BoundedContexts.Supervisor.Views.Interview;
 using WB.Core.BoundedContexts.Supervisor.Views.Questionnaire;
+using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
 using WB.Core.SharedKernels.DataCollection.ReadSide;
 using WB.Core.SharedKernels.DataCollection.Views.Questionnaire;
 
-namespace Core.Supervisor.Views.Interview
+namespace Core.Supervisor.Views.Revalidate
 {
-    using Main.Core.Documents;
-    using Main.Core.View;
-    using WB.Core.BoundedContexts.Supervisor.Views.Interview;
-    using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
-
-    public class InterviewDetailsViewFactory : IViewFactory<InterviewDetailsInputModel, InterviewDetailsView>
+    public class RevalidateInterviewFactory : IViewFactory<RevalidateInterviewInputModel, RevalidatInterviewView>
     {
+        public IInterviewDataAndQuestionnaireMerger merger;
         private readonly IReadSideRepositoryReader<InterviewData> interviewStore;
         private readonly IReadSideRepositoryReader<UserDocument> userStore;
         private readonly IVersionedReadSideRepositoryReader<QuestionnaireDocumentVersioned> questionnaireStore;
         private readonly IVersionedReadSideRepositoryReader<QuestionnaireRosterStructure> questionnaireRosterStructures;
         private readonly IVersionedReadSideRepositoryReader<ReferenceInfoForLinkedQuestions> questionnaireReferenceInfoForLinkedQuestions;
-        private readonly IInterviewDataAndQuestionnaireMerger merger;
 
-        public InterviewDetailsViewFactory(IReadSideRepositoryReader<InterviewData> interviewStore,
+        public RevalidateInterviewFactory(IReadSideRepositoryReader<InterviewData> interviewStore,
             IReadSideRepositoryReader<UserDocument> userStore,
             IVersionedReadSideRepositoryReader<QuestionnaireDocumentVersioned> questionnaireStore,
             IVersionedReadSideRepositoryReader<QuestionnaireRosterStructure> questionnaireRosterStructures,
             IVersionedReadSideRepositoryReader<ReferenceInfoForLinkedQuestions> questionnaireReferenceInfoForLinkedQuestions,
             IInterviewDataAndQuestionnaireMerger merger)
         {
+            this.merger = merger;
             this.interviewStore = interviewStore;
             this.userStore = userStore;
             this.questionnaireStore = questionnaireStore;
             this.questionnaireRosterStructures = questionnaireRosterStructures;
             this.questionnaireReferenceInfoForLinkedQuestions = questionnaireReferenceInfoForLinkedQuestions;
-            this.merger = merger;
         }
 
-        public InterviewDetailsView Load(InterviewDetailsInputModel input)
+        public RevalidatInterviewView Load(RevalidateInterviewInputModel input)
         {
-            var interview = this.interviewStore.GetById(input.CompleteQuestionnaireId);
+            var interview = this.interviewStore.GetById(input.InterviewId);
             if (interview == null || interview.IsDeleted)
                 return null;
 
@@ -47,13 +52,30 @@ namespace Core.Supervisor.Views.Interview
 
             var questionnaireReferenceInfo = this.questionnaireReferenceInfoForLinkedQuestions.GetById(interview.QuestionnaireId, interview.QuestionnaireVersion);
 
-             var questionnaireRosters = this.questionnaireRosterStructures.GetById(interview.QuestionnaireId, interview.QuestionnaireVersion);
+            var questionnaireRosters = this.questionnaireRosterStructures.GetById(interview.QuestionnaireId, interview.QuestionnaireVersion);
 
             var user = this.userStore.GetById(interview.ResponsibleId);
             if (user == null)
                 throw new ArgumentException(string.Format("User with id {0} is not found.", interview.ResponsibleId));
 
-            return merger.Merge(interview, questionnaire, questionnaireReferenceInfo, questionnaireRosters, user);
+            var mergedInterview = merger.Merge(interview, questionnaire, questionnaireReferenceInfo, questionnaireRosters, user);
+
+
+            var revalidateInterviewView = new RevalidatInterviewView
+            {
+                Responsible = mergedInterview.Responsible,
+                QuestionnairePublicKey = mergedInterview.QuestionnairePublicKey,
+                Title = mergedInterview.Title,
+                Description = mergedInterview.Description,
+                PublicKey = mergedInterview.PublicKey,
+                Status = mergedInterview.Status,
+                InterviewId = input.InterviewId
+            };
+
+            revalidateInterviewView.FeaturedQuestions.AddRange(mergedInterview.Groups.SelectMany(group => group.Questions.Where(q => q.IsFeatured)));
+            revalidateInterviewView.MandatoryQuestions.AddRange(mergedInterview.Groups.SelectMany(group => group.Questions.Where(q => q.IsMandatory && q.IsEnabled)));
+
+            return revalidateInterviewView;
         }
     }
 }
