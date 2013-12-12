79c85939b8037bf6fae901afee8b587b340c42c6
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2013-12-12 14:31:16 -0500

    Supervisor\Synchronizaton: log restore is fixed KP-2943
    Former-commit-id: 00489afa04f29f8631362b7240aaa25d4c965a9a

---
+++ src/Core/Infrastructure/WB.Core.Infrastructure.FunctionalDenormalization/Implementation/EventDispatcher/InMemoryTransaction.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Ncqrs.Eventing;
+using WB.Core.Infrastructure.FunctionalDenormalization.EventHandlers;
+
+namespace WB.Core.Infrastructure.FunctionalDenormalization.Implementation.EventDispatcher
+{
+    public class InMemoryTransaction : IDisposable
+    {
+        private readonly IList<IFunctionalEventHandler> functionalEventHandlers;
+        private readonly Guid eventSourceId;
+
+        public InMemoryTransaction(IList<IFunctionalEventHandler> functionalEventHandlers, Guid eventSourceId)
+        {
+            this.functionalEventHandlers = functionalEventHandlers;
+            this.eventSourceId = eventSourceId;
+
+            foreach (var functionalHandler in functionalEventHandlers)
+            {
+                functionalHandler.ChangeForSingleEventSource(eventSourceId);
+            }
+        }
+
+        public void Dispose()
+        {
+            foreach (var functionalHandler in this.functionalEventHandlers)
+            {
+                functionalHandler.FlushDataToPersistentStorage(this.eventSourceId);
+            }
+        }
+    }
+}
