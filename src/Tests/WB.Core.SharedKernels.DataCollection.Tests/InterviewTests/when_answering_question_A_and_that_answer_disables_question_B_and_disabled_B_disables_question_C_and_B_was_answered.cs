477208d57e4c537297ae3d08c72d9afc148d6895
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2013-12-13 14:34:45 +0200

    Interview\Tests: added test on disabled questions chain requiring null to be supplied to expression processor for just disabled questions (fails right now) KP-2685
    Former-commit-id: 68292c55478e3dc6611ae29dcc6dc356d1d22991

--- src/Tests/WB.Core.SharedKernels.DataCollection.Tests/InterviewTests/when_answering_question_A_and_that_answer_disables_question_B_and_disabled_B_disables_question_C_and_B_was_answered.cs
+++ src/Tests/WB.Core.SharedKernels.DataCollection.Tests/InterviewTests/when_answering_question_A_and_that_answer_disables_question_B_and_disabled_B_disables_question_C_and_B_was_answered.cs
@@ -8,13 +8,14 @@ using WB.Core.SharedKernels.DataCollection.Aggregates;
 using WB.Core.SharedKernels.DataCollection.Events.Interview;
 using WB.Core.SharedKernels.DataCollection.Implementation.Aggregates;
 using WB.Core.SharedKernels.DataCollection.Implementation.Repositories;
+using WB.Core.SharedKernels.DataCollection.ValueObjects.Questionnaire;
 using WB.Core.SharedKernels.ExpressionProcessor.Services;
 using It = Machine.Specifications.It;
 using it = Moq.It;
 
 namespace WB.Core.SharedKernels.DataCollection.Tests.InterviewTests
 {
-    internal class when_answering_question_A_and_that_answer_disables_question_B_and_disabled_B_disables_question_C : InterviewTestsContext
+    internal class when_answering_question_A_and_that_answer_disables_question_B_and_disabled_B_disables_question_C_and_B_was_answered : InterviewTestsContext
     {
         Establish context = () =>
         {
@@ -26,8 +27,11 @@ namespace WB.Core.SharedKernels.DataCollection.Tests.InterviewTests
             questionBId = Guid.Parse("BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB");
             questionCId = Guid.Parse("CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC");
 
+            string questionAVariableName = "a";
+            questionBVariableName = "b";
+
             string questionBEnablementCondition = "disable B on demand please";
-            string questionCEnablementCondition = "disable C on demand please";
+            questionCEnablementCondition = "disable C on demand please";
 
 
             Expression<Func<Guid, bool>> abcQuestionId = id => id == questionAId || id == questionBId || id == questionCId;
@@ -40,20 +44,29 @@ namespace WB.Core.SharedKernels.DataCollection.Tests.InterviewTests
                 && _.GetQuestionsWhichCustomEnablementConditionDependsOnSpecifiedQuestion(questionBId) == new [] { questionCId }
                 && _.GetCustomEnablementConditionForQuestion(questionBId) == questionBEnablementCondition
                 && _.GetCustomEnablementConditionForQuestion(questionCId) == questionCEnablementCondition
+                && _.GetQuestionsInvolvedInCustomEnablementConditionOfQuestion(questionBId) == new [] { new QuestionIdAndVariableName(questionAId, questionAVariableName) }
+                && _.GetQuestionsInvolvedInCustomEnablementConditionOfQuestion(questionCId) == new[] { new QuestionIdAndVariableName(questionBId, questionBVariableName) }
             );
 
-            var expressionProcessor = Mock.Of<IExpressionProcessor>
+            expressionProcessor = Mock.Of<IExpressionProcessor>
             (_
                 => _.EvaluateBooleanExpression(it.IsAny<string>(), it.IsAny<Func<string, object>>()) == true
                 && _.EvaluateBooleanExpression(questionBEnablementCondition, it.IsAny<Func<string, object>>()) == false
-                && _.EvaluateBooleanExpression(questionCEnablementCondition, it.IsAny<Func<string, object>>()) == false
             );
+
+            Mock.Get(expressionProcessor)
+                .Setup(_ => _.EvaluateBooleanExpression(questionCEnablementCondition, it.IsAny<Func<string, object>>()))
+                .Callback<string, Func<string, object>>((expression, getValueForIdentifier) =>
+                    funcSuppliedWhenEvaluatingQuestionCEnablementCondition = getValueForIdentifier)
+                .Returns(false);
+
 
             SetupInstanceToMockedServiceLocator<IQuestionnaireRepository>(
                 CreateQuestionnaireRepositoryStubWithOneQuestionnaire(questionnaireId, questionaire));
             SetupInstanceToMockedServiceLocator<IExpressionProcessor>(expressionProcessor);
 
             interview = CreateInterview(questionnaireId: questionnaireId);
+            interview.Apply(new TextQuestionAnswered(userId, questionBId, emptyRosterVector, DateTime.Now, "answer to B"));
 
             eventContext = new EventContext();
         };
@@ -75,6 +88,9 @@ namespace WB.Core.SharedKernels.DataCollection.Tests.InterviewTests
             eventContext.ShouldContainEvent<QuestionDisabled>(@event
                 => @event.QuestionId == questionCId);
 
+        It should_supply_null_answer_for_question_B_when_evaluating_question_C_disablement_condition = () =>
+            funcSuppliedWhenEvaluatingQuestionCEnablementCondition.Invoke(questionBVariableName).ShouldEqual(null);
+
         private static EventContext eventContext;
         private static Guid questionAId;
         private static Guid questionBId;
@@ -82,5 +98,9 @@ namespace WB.Core.SharedKernels.DataCollection.Tests.InterviewTests
         private static Interview interview;
         private static Guid userId;
         private static decimal[] emptyRosterVector;
+        private static IExpressionProcessor expressionProcessor;
+        private static string questionCEnablementCondition;
+        private static Func<string, object> funcSuppliedWhenEvaluatingQuestionCEnablementCondition;
+        private static string questionBVariableName;
     }
 }
