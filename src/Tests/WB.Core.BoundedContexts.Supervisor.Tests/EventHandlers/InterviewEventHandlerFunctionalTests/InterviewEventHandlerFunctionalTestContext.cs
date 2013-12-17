8e7e690815ebb4e357e93a072104073825411768
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2013-12-17 13:56:09 -0500

    Roster: new tests for interview supervisor's eventhandler KP-2934
    Former-commit-id: d8f27ec3503cb0ca5115696398162f8e01003150

---
+++ src/Tests/WB.Core.BoundedContexts.Supervisor.Tests/EventHandlers/InterviewEventHandlerFunctionalTests/InterviewEventHandlerFunctionalTestContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Main.Core.Documents;
+using Moq;
+using Ncqrs.Eventing.ServiceModel.Bus;
+using WB.Core.BoundedContexts.Supervisor.EventHandler;
+using WB.Core.BoundedContexts.Supervisor.Views.Interview;
+using WB.Core.Infrastructure.FunctionalDenormalization.Implementation.ReadSide;
+using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
+using WB.Core.SharedKernels.DataCollection.ReadSide;
+using WB.Core.SharedKernels.DataCollection.Views.Questionnaire;
+
+namespace WB.Core.BoundedContexts.Supervisor.Tests.EventHandlers.InterviewEventHandlerFunctionalTests
+{
+    internal class InterviewEventHandlerFunctionalTestContext
+    {
+        protected static InterviewEventHandlerFunctional CreateInterviewEventHandlerFunctional(QuestionnaireRosterStructure rosterStructure=null)
+        {
+            var questionnaireRosterStructureMockStorage = new Mock<IVersionedReadSideRepositoryWriter<QuestionnaireRosterStructure>>();
+            questionnaireRosterStructureMockStorage.Setup(x => x.GetById(It.IsAny<Guid>())).Returns(rosterStructure);
+            questionnaireRosterStructureMockStorage.Setup(x => x.GetById(It.IsAny<Guid>(), It.IsAny<long>())).Returns(rosterStructure);
+            return new InterviewEventHandlerFunctional(
+                new Mock<IReadSideRepositoryWriter<UserDocument>>().Object,
+                questionnaireRosterStructureMockStorage.Object,
+                new Mock<IReadSideRepositoryWriter<ViewWithSequence<InterviewData>>>().Object);
+        }
+
+        protected static QuestionnaireRosterStructure CreateQuestionnaireRosterStructure(Guid scopeId, params Guid[] groupIdsFromScope)
+        {
+            var rosterStructure= new QuestionnaireRosterStructure();
+            rosterStructure.RosterScopes.Add(scopeId, new HashSet<Guid>(groupIdsFromScope));
+            return rosterStructure;
+        }
+
+        protected static ViewWithSequence<InterviewData> CreateViewWithSequenceOfInterviewData()
+        {
+            return new ViewWithSequence<InterviewData>(new InterviewData(), 1);
+        }
+
+        protected static IPublishedEvent<T> CreatePublishableEvent<T>(T payload)
+        {
+            var publishableEventMock = new Mock<IPublishedEvent<T>>();
+            publishableEventMock.Setup(x => x.Payload).Returns(payload);
+            return publishableEventMock.Object;
+        }
+    }
+}
---
+++ src/Tests/WB.Core.BoundedContexts.Supervisor.Tests/EventHandlers/InterviewEventHandlerFunctionalTests/when_GrpupPropagated_event_recived_and_new_roster_row_need_to_be_add.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Machine.Specifications;
+using WB.Core.BoundedContexts.Supervisor.EventHandler;
+using WB.Core.BoundedContexts.Supervisor.Views.Interview;
+using WB.Core.Infrastructure.FunctionalDenormalization.Implementation.ReadSide;
+using WB.Core.SharedKernels.DataCollection.Events.Interview;
+
+namespace WB.Core.BoundedContexts.Supervisor.Tests.EventHandlers.InterviewEventHandlerFunctionalTests
+{
+    internal class when_GrpupPropagated_event_recived_and_new_roster_row_need_to_be_add : InterviewEventHandlerFunctionalTestContext
+    {
+        Establish context = () =>
+        {
+            rosterGroupId = Guid.Parse("10000000000000000000000000000000");
+            rosterScopeId = Guid.Parse("12222222222222222222222222222222");
+            viewState = CreateViewWithSequenceOfInterviewData();
+            var questionnaireRosterStructure = CreateQuestionnaireRosterStructure(rosterScopeId, rosterGroupId);
+
+            interviewEventHandlerFunctional = CreateInterviewEventHandlerFunctional(questionnaireRosterStructure);
+        };
+
+        Because of = () =>
+            viewState= interviewEventHandlerFunctional.Update(viewState,
+                CreatePublishableEvent(new GroupPropagated(rosterGroupId, new decimal[0], 1)));
+
+        It should_interview_levels_count_be_equal_to_1 = () =>
+            viewState.Document.Levels.Keys.Count.ShouldEqual(1);
+
+        It should_interview_level_with_id_0_be_present = () =>
+            viewState.Document.Levels.Keys.ShouldContain("0");
+
+        private static InterviewEventHandlerFunctional interviewEventHandlerFunctional;
+        private static ViewWithSequence<InterviewData> viewState;
+        private static Guid rosterGroupId;
+        private static Guid rosterScopeId;
+    }
+}
