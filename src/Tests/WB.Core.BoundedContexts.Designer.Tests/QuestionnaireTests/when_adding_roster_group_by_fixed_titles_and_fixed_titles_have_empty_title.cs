ff69a3905361474a9a89d70811188fc72239e4ee
Roman Lytvyn <litvin.roma@gmail.com>
2013-12-17 18:47:30 +0200

    designer: questionnaire AR. tests for roster by fixed titles KP-2927 Designer: add switcher for roster's triggering kind KP-2928 Designer: add support of different kinds of sourses of roster titles
    Former-commit-id: 493ff57f8b4ab51ca659442ab5ce839ea6a8603e

---
+++ src/Tests/WB.Core.BoundedContexts.Designer.Tests/QuestionnaireTests/when_adding_roster_group_by_fixed_titles_and_fixed_titles_have_empty_title.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Machine.Specifications;
+using Main.Core.Entities.SubEntities;
+using Main.Core.Events.Questionnaire;
+using WB.Core.BoundedContexts.Designer.Aggregates;
+using WB.Core.BoundedContexts.Designer.Exceptions;
+
+namespace WB.Core.BoundedContexts.Designer.Tests.QuestionnaireTests
+{
+    internal class when_adding_roster_group_by_fixed_titles_and_fixed_titles_have_empty_title : QuestionnaireTestsContext
+    {
+        Establish context = () =>
+        {
+            responsibleId = Guid.Parse("DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD");
+            var chapterId = Guid.Parse("CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC");
+            parentGroupId = Guid.Parse("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+            groupId = Guid.Parse("BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB");
+            rosterSizeSourceType = RosterSizeSourceType.FixedTitles;
+            rosterFixedTitles = new[] { "fixed title 1", " ", "fixed title 3" };
+
+            questionnaire = CreateQuestionnaire(responsibleId: responsibleId);
+            questionnaire.Apply(new NewGroupAdded { PublicKey = chapterId });
+            questionnaire.Apply(new NewQuestionAdded()
+            {
+                PublicKey = Guid.NewGuid(),
+                GroupPublicKey = chapterId,
+                QuestionType = QuestionType.Text
+            });
+            questionnaire.Apply(new NewGroupAdded { PublicKey = parentGroupId });
+        };
+
+        Because of = () =>
+            exception = Catch.Exception(
+                () =>
+                    questionnaire.AddGroup(groupId: groupId, responsibleId: responsibleId, title: "title", parentGroupId: parentGroupId,
+                        description: null, condition: null, rosterSizeQuestionId: null, isRoster: true,
+                        rosterSizeSource: rosterSizeSourceType, rosterFixedTitles: rosterFixedTitles, rosterTitleQuestionId: null));
+
+        It should_throw_QuestionnaireException = () =>
+            exception.ShouldBeOfType<QuestionnaireException>();
+
+
+        It should_throw_exception_with_message_containting__not__ = () =>
+            exception.Message.ToLower().ShouldContain("not");
+
+        It should_throw_exception_with_message_containting__empty__ = () =>
+            exception.Message.ToLower().ShouldContain("empty");
+
+        It should_throw_exception_with_message_containting__title__ = () =>
+            exception.Message.ToLower().ShouldContain("title");
+
+        private static Questionnaire questionnaire;
+        private static Guid responsibleId;
+        private static Guid groupId;
+        private static Guid parentGroupId;
+        private static RosterSizeSourceType rosterSizeSourceType;
+        private static string[] rosterFixedTitles;
+        private static Exception exception;
+    }
+}
