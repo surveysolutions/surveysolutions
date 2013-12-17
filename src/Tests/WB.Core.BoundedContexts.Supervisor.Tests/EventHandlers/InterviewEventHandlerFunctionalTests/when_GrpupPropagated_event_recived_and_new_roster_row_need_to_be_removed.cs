13925f6eb0c231c13c80d60f8d76b505092308d8
Anastasya Kholodova <nastya.kholodova@gmail.com>
2013-12-17 13:56:09 -0500

    Roster: new tests for interview supervisor's eventhandler KP-2934
    Former-commit-id: d8f27ec3503cb0ca5115696398162f8e01003150

---
+++ src/Tests/WB.Core.BoundedContexts.Supervisor.Tests/EventHandlers/InterviewEventHandlerFunctionalTests/when_GrpupPropagated_event_recived_and_new_roster_row_need_to_be_removed.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Machine.Specifications;
+using WB.Core.BoundedContexts.Supervisor.EventHandler;
+using WB.Core.BoundedContexts.Supervisor.Views.Interview;
+using WB.Core.Infrastructure.FunctionalDenormalization.Implementation.ReadSide;
+using WB.Core.SharedKernels.DataCollection.Events.Interview;
+
+namespace WB.Core.BoundedContexts.Supervisor.Tests.EventHandlers.InterviewEventHandlerFunctionalTests
+{
+    internal class when_GrpupPropagated_event_recived_and_new_roster_row_need_to_be_removed : InterviewEventHandlerFunctionalTestContext
+    {
+        Establish context = () =>
+        {
+            rosterGroupId = Guid.Parse("10000000000000000000000000000000");
+            rosterScopeId = Guid.Parse("12222222222222222222222222222222");
+            viewState = CreateViewWithSequenceOfInterviewData();
+            viewState.Document.Levels.Add("0",new InterviewLevel(rosterScopeId,null,new decimal[0]));
+
+            var questionnaireRosterStructure = CreateQuestionnaireRosterStructure(rosterScopeId, rosterGroupId);
+
+            interviewEventHandlerFunctional = CreateInterviewEventHandlerFunctional(questionnaireRosterStructure);
+        };
+
+        Because of = () =>
+            viewState = interviewEventHandlerFunctional.Update(viewState,
+                CreatePublishableEvent(new GroupPropagated(rosterGroupId, new decimal[0], 0)));
+
+        It should_interview_levels_count_be_equal_to_0 = () =>
+            viewState.Document.Levels.Keys.Count.ShouldEqual(0);
+
+        It should_interview_level_with_id_0_be_present = () =>
+            viewState.Document.Levels.Keys.ShouldNotContain("0");
+
+        private static InterviewEventHandlerFunctional interviewEventHandlerFunctional;
+        private static ViewWithSequence<InterviewData> viewState;
+        private static Guid rosterGroupId;
+        private static Guid rosterScopeId;
+    }
+}
