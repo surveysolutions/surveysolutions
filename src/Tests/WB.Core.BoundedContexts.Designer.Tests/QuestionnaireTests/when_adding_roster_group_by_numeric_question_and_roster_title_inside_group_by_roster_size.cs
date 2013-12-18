bbe944ecad482afafc73c53d046a540f4b7c2ca2
Roman Lytvyn <none@none>
2013-12-18 12:14:08 +0200

    designer: questionnaire AR. tests for roster by numeric question that have specified roster title question KP-2927 KP-2928
    Former-commit-id: d65bdaa7653b98fe7ae2ac851fd70572452cef25

---
+++ src/Tests/WB.Core.BoundedContexts.Designer.Tests/QuestionnaireTests/when_adding_roster_group_by_numeric_question_and_roster_title_inside_group_by_roster_size.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Machine.Specifications;
+using Main.Core.Entities.SubEntities;
+using Main.Core.Events.Questionnaire;
+using Ncqrs.Spec;
+using WB.Core.BoundedContexts.Designer.Aggregates;
+using WB.Core.BoundedContexts.Designer.Events.Questionnaire;
+
+namespace WB.Core.BoundedContexts.Designer.Tests.QuestionnaireTests
+{
+    internal class when_adding_roster_group_by_numeric_question_and_roster_title_inside_group_by_roster_size : QuestionnaireTestsContext
+    {
+        Establish context = () =>
+        {
+            responsibleId = Guid.Parse("DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD");
+            var chapterId = Guid.Parse("CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC");
+            parentGroupId = Guid.Parse("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+            groupId = Guid.Parse("BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB");
+            rosterTitleQuestionId = Guid.Parse("1BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB");
+            rosterSizeQuestionId = Guid.Parse("2BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB");
+            rosterGroupWithRosterTitleQuestionId = Guid.Parse("3BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB");
+            rosterSizeSourceType = RosterSizeSourceType.Question;
+
+            questionnaire = CreateQuestionnaire(responsibleId: responsibleId);
+            questionnaire.Apply(new NewGroupAdded { PublicKey = chapterId });
+            questionnaire.Apply(new NewGroupAdded { PublicKey = rosterGroupWithRosterTitleQuestionId, ParentGroupPublicKey = chapterId });
+            questionnaire.Apply(new GroupBecameARoster(responsibleId: responsibleId, groupId: rosterGroupWithRosterTitleQuestionId));
+            questionnaire.Apply(new RosterChanged(responsibleId: responsibleId, groupId: rosterGroupWithRosterTitleQuestionId,
+                rosterFixedTitles: null, rosterSizeSource: RosterSizeSourceType.Question, rosterSizeQuestionId: rosterSizeQuestionId,
+                rosterTitleQuestionId: null));
+            questionnaire.Apply(new NewQuestionAdded()
+            {
+                PublicKey = rosterTitleQuestionId,
+                GroupPublicKey = rosterGroupWithRosterTitleQuestionId,
+                QuestionType = QuestionType.Text
+            });
+            questionnaire.Apply(new NewQuestionAdded()
+            {
+                PublicKey = rosterSizeQuestionId,
+                GroupPublicKey = chapterId,
+                QuestionType = QuestionType.Numeric,
+                IsInteger = true
+            });
+            questionnaire.Apply(new NewGroupAdded { PublicKey = parentGroupId });
+
+            eventContext = new EventContext();
+        };
+
+        Because of = () =>
+            questionnaire.AddGroup(groupId: groupId, responsibleId: responsibleId, title: "title",
+                description: null, condition: null, rosterSizeQuestionId: rosterSizeQuestionId,
+                isRoster: true, rosterSizeSource: rosterSizeSourceType, rosterFixedTitles: null,
+                rosterTitleQuestionId: rosterTitleQuestionId, parentGroupId: parentGroupId);
+
+        Cleanup stuff = () =>
+        {
+            eventContext.Dispose();
+            eventContext = null;
+        };
+
+
+        It should_raise_GroupBecameARoster_event_with_GroupId_specified = () =>
+            eventContext.GetSingleEvent<GroupBecameARoster>()
+                .GroupId.ShouldEqual(groupId);
+
+        It should_raise_RosterChanged_event_with_GroupId_specified = () =>
+            eventContext.GetSingleEvent<RosterChanged>()
+                .GroupId.ShouldEqual(groupId);
+
+        It should_raise_RosterChanged_event_with_RosterSizeQuestionId_equal_to_specified_question_id = () =>
+            eventContext.GetSingleEvent<RosterChanged>()
+                .RosterSizeQuestionId.ShouldEqual(rosterSizeQuestionId);
+
+        It should_raise_RosterChanged_event_with_RosterTitleQuestionId_equal_to_specified_question_id = () =>
+            eventContext.GetSingleEvent<RosterChanged>()
+                .RosterTitleQuestionId.ShouldEqual(rosterTitleQuestionId);
+
+        private static EventContext eventContext;
+        private static Questionnaire questionnaire;
+        private static Guid responsibleId;
+        private static Guid groupId;
+        private static Guid rosterTitleQuestionId;
+        private static Guid rosterSizeQuestionId;
+        private static Guid parentGroupId;
+        private static Guid rosterGroupWithRosterTitleQuestionId;
+        private static RosterSizeSourceType rosterSizeSourceType;
+    }
+}
---
+++ src/Tests/WB.Core.BoundedContexts.Designer.Tests/QuestionnaireTests/when_adding_roster_group_by_numeric_question_and_roster_title_not_in_groups_by_roster_size.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Machine.Specifications;
+using Main.Core.Entities.SubEntities;
+using Main.Core.Events.Questionnaire;
+using WB.Core.BoundedContexts.Designer.Aggregates;
+using WB.Core.BoundedContexts.Designer.Exceptions;
+
+namespace WB.Core.BoundedContexts.Designer.Tests.QuestionnaireTests
+{
+    internal class when_adding_roster_group_by_numeric_question_and_roster_title_not_in_groups_by_roster_size : QuestionnaireTestsContext
+    {
+        Establish context = () =>
+        {
+            responsibleId = Guid.Parse("DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD");
+            var chapterId = Guid.Parse("CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC");
+            parentGroupId = Guid.Parse("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+            groupId = Guid.Parse("BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB");
+            rosterTitleQuestionId = Guid.Parse("1BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB");
+            rosterSizeQuestionId = Guid.Parse("2BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB");
+            rosterSizeSourceType = RosterSizeSourceType.Question;
+
+            questionnaire = CreateQuestionnaire(responsibleId: responsibleId);
+            questionnaire.Apply(new NewGroupAdded { PublicKey = chapterId });
+            questionnaire.Apply(new NewQuestionAdded()
+            {
+                PublicKey = rosterTitleQuestionId,
+                GroupPublicKey = chapterId,
+                QuestionType = QuestionType.Text
+            });
+            questionnaire.Apply(new NewQuestionAdded()
+            {
+                PublicKey = rosterSizeQuestionId,
+                GroupPublicKey = chapterId,
+                QuestionType = QuestionType.Numeric,
+                IsInteger = true
+            });
+            questionnaire.Apply(new NewGroupAdded { PublicKey = parentGroupId });
+        };
+
+        Because of = () =>
+            exception = Catch.Exception(
+                () =>
+                    questionnaire.AddGroup(groupId: groupId, responsibleId: responsibleId, title: "title",
+                        description: null, condition: null, rosterSizeQuestionId: rosterSizeQuestionId,
+                        isRoster: true, rosterSizeSource: rosterSizeSourceType, rosterFixedTitles: null,
+                        rosterTitleQuestionId: rosterTitleQuestionId, parentGroupId: parentGroupId));
+
+        It should_throw_QuestionnaireException = () =>
+            exception.ShouldBeOfType<QuestionnaireException>();
+
+        It should_throw_exception_with_message_containting__roster__ = () =>
+            exception.Message.ToLower().ShouldContain("roster");
+
+        It should_throw_exception_with_message_containting__title__ = () =>
+            exception.Message.ToLower().ShouldContain("title");
+
+        It should_throw_exception_with_message_containting__question__ = () =>
+            exception.Message.ToLower().ShouldContain("question");
+
+        It should_throw_exception_with_message_containting__only__ = () =>
+            exception.Message.ToLower().ShouldContain("only");
+
+        It should_throw_exception_with_message_containting__by__ = () =>
+            exception.Message.ToLower().ShouldContain("by");
+
+        It should_throw_exception_with_message_containting__size__ = () =>
+            exception.Message.ToLower().ShouldContain("size");
+
+        private static Questionnaire questionnaire;
+        private static Guid responsibleId;
+        private static Guid groupId;
+        private static Guid rosterTitleQuestionId;
+        private static Guid rosterSizeQuestionId;
+        private static Guid parentGroupId;
+        private static RosterSizeSourceType rosterSizeSourceType;
+        private static Exception exception;
+    }
+}
