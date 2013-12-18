9099215aeb3bc0e08b13d560b0ae7436e112ad83
Anatoliy 'TLK' Kolesnick <none@none>
2013-12-18 18:04:30 +0200

    Interview\events: renamed RosterTitleChanged -> RosterRowTitleChanged KP-2932
    Former-commit-id: f6ce44161acad7875b6cb7d19b862bf2b1a57f40

--- src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/Events/Interview/RosterRowTitleChanged.cs
+++ src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/Events/Interview/RosterRowTitleChanged.cs
@@ -3,11 +3,11 @@ using WB.Core.SharedKernels.DataCollection.Events.Interview.Base;
 
 namespace WB.Core.SharedKernels.DataCollection.Events.Interview
 {
-    public class RosterTitleChanged : RosterRowEvent
+    public class RosterRowTitleChanged : RosterRowEvent
     {
         public string Title { private set; get; }
 
-        public RosterTitleChanged(Guid groupId, decimal[] outerRosterVector, decimal rosterInstanceId, string title)
+        public RosterRowTitleChanged(Guid groupId, decimal[] outerRosterVector, decimal rosterInstanceId, string title)
             : base(groupId, outerRosterVector, rosterInstanceId)
         {
             this.Title = title;
