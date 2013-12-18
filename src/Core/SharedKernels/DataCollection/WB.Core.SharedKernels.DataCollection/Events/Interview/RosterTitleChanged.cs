9b10a12f79faefe47433b1a17e864e21b53b50d3
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2013-12-18 18:02:57 +0200

    Interview\events: updated RosterTitleChanged event so now it's properties are the same as in other roster row events KP-2932
    Former-commit-id: 334e578fb7ae577b4e2b343b521361b31d21773d

--- src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/Events/Interview/RosterTitleChanged.cs
+++ src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/Events/Interview/RosterTitleChanged.cs
@@ -1,16 +1,16 @@
-﻿namespace WB.Core.SharedKernels.DataCollection.Events.Interview
+﻿using System;
+using WB.Core.SharedKernels.DataCollection.Events.Interview.Base;
+
+namespace WB.Core.SharedKernels.DataCollection.Events.Interview
 {
-    using System;
-    using Base;
-
-    public class RosterTitleChanged : QuestionActiveEvent
+    public class RosterTitleChanged : RosterRowEvent
     {
-        public string RosterTitle { set; get; }
+        public string Title { private set; get; }
 
-        public RosterTitleChanged(Guid userId, Guid questionId, decimal[] propagationVector, string rosterTitle)
-            : base(userId, questionId, propagationVector)
+        public RosterTitleChanged(Guid groupId, decimal[] outerRosterVector, decimal rosterInstanceId, string title)
+            : base(groupId, outerRosterVector, rosterInstanceId)
         {
-            this.RosterTitle = rosterTitle;
+            this.Title = title;
         }
     }
 }
