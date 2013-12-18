8ce1b49af893a607e2a644ec243ede2c1402f892
Roman Lytvyn <litvin.roma@gmail.com>
2013-12-18 11:09:18 +0200

    designer: questionnaire AR. tests for roster by numeric question that have specified roster title question not existing in questionnaire. KP-2927 KP-2928
    Former-commit-id: 25449bc1ce18547b6aac90bced40e3f803d90586

---
+++ src/Tests/WB.Core.BoundedContexts.Designer.Tests/QuestionnaireTests/when_adding_roster_group_by_numeric_question_and_roster_title_specified_but_not_existing.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Machine.Specifications;
+using Main.Core.Entities.SubEntities;
+using Main.Core.Events.Questionnaire;
+using WB.Core.BoundedContexts.Designer.Aggregates;
+using WB.Core.BoundedContexts.Designer.Exceptions;
+
+namespace WB.Core.BoundedContexts.Designer.Tests.QuestionnaireTests
+{
+    internal class when_adding_roster_group_by_numeric_question_and_roster_title_specified_but_not_existing : QuestionnaireTestsContext
+    {
+        Establish context = () =>
+        {
+            responsibleId = Guid.Parse("DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD");
+            var chapterId = Guid.Parse("CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC");
+            parentGroupId = Guid.Parse("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+            groupId = Guid.Parse("BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB");
+            rosterTitleQuestionId = Guid.Parse("1BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB");
+            rosterSizeQuestionId = Guid.Parse("2BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB");
+            rosterSizeSourceType = RosterSizeSourceType.Question;
+
+            questionnaire = CreateQuestionnaire(responsibleId: responsibleId);
+            questionnaire.Apply(new NewGroupAdded { PublicKey = chapterId });
+            questionnaire.Apply(new NewQuestionAdded()
+            {
+                PublicKey = rosterSizeQuestionId,
+                GroupPublicKey = chapterId,
+                QuestionType = QuestionType.Numeric,
+                IsInteger = true
+            });
+            questionnaire.Apply(new NewGroupAdded { PublicKey = parentGroupId });
+        };
+
+        Because of = () =>
+            exception = Catch.Exception(
+                () =>
+                    questionnaire.AddGroup(groupId: groupId, responsibleId: responsibleId, title: "title", parentGroupId: parentGroupId,
+                        description: null, condition: null, rosterSizeQuestionId: rosterSizeQuestionId, isRoster: true,
+                        rosterSizeSource: rosterSizeSourceType, rosterFixedTitles: null, rosterTitleQuestionId: rosterTitleQuestionId));
+
+        It should_throw_QuestionnaireException = () =>
+            exception.ShouldBeOfType<QuestionnaireException>();
+
+        It should_throw_exception_with_message_containting__roster__ = () =>
+            exception.Message.ToLower().ShouldContain("roster");
+
+        It should_throw_exception_with_message_containting__title__ = () =>
+            exception.Message.ToLower().ShouldContain("title");
+
+        It should_throw_exception_with_message_containting__question__ = () =>
+            exception.Message.ToLower().ShouldContain("question");
+
+        It should_throw_exception_with_message_containting__missing__ = () =>
+            exception.Message.ToLower().ShouldContain("missing");
+
+        private static Questionnaire questionnaire;
+        private static Guid responsibleId;
+        private static Guid groupId;
+        private static Guid rosterTitleQuestionId;
+        private static Guid rosterSizeQuestionId;
+        private static Guid parentGroupId;
+        private static RosterSizeSourceType rosterSizeSourceType;
+        private static Exception exception;
+    }
+}
