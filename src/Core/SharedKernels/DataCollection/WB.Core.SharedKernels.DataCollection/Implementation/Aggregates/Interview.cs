ba122069fd6e4d7a6a102ffae4025b8ad813448b
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2013-12-18 12:54:19 -0500

    MandatoryQuestion: hot fix of KP-2955
    Former-commit-id: 3e646a751051f06a2ec7d92016765fe232922c6e

--- src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/Implementation/Aggregates/Interview.cs
+++ src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/Implementation/Aggregates/Interview.cs
@@ -1085,7 +1085,10 @@ namespace WB.Core.SharedKernels.DataCollection.Implementation.Aggregates
                     string questionKey = ConvertIdAndRosterVectorToString(questionIdAtInterview.Id, questionIdAtInterview.RosterVector);
 
                     if (!this.answeredQuestions.Contains(questionKey))
+                    {
+                        questionsDeclaredInvalid.Add(questionIdAtInterview);
                         continue;
+                    }
 
                     questionsDeclaredValid.Add(questionIdAtInterview);
                 }
