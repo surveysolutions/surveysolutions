1a5bb7c8408d0c17066587aab2017fafdc087daa
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2013-12-18 17:45:23 +0200

    Interview\events: extracted base events for roster row events KP-2932
    Former-commit-id: 7a3d3f65d18e5dbb7f86cb0257cbead9f331a11c

---
+++ src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/Events/Interview/Base/RosterRowEvent.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace WB.Core.SharedKernels.DataCollection.Events.Interview.Base
+{
+    public abstract class RosterRowEvent : InterviewPassiveEvent
+    {
+        public Guid GroupId { get; private set; }
+        public decimal[] OuterRosterVector { get; private set; }
+        public decimal RosterInstanceId { get; private set; }
+
+        protected RosterRowEvent(Guid groupId, decimal[] outerRosterVector, decimal rosterInstanceId)
+        {
+            this.GroupId = groupId;
+            this.OuterRosterVector = outerRosterVector;
+            this.RosterInstanceId = rosterInstanceId;
+        }
+    }
+}
