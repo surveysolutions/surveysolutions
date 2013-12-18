a83be45c09e9d05ca1f7b66cc000ba1e687edbb8
Anatoliy 'TLK' Kolesnick <none@none>
2013-12-18 16:37:19 +0200

    Interview\Tests: added tests on unchecking options when question is used as roster size KP-2932
    Former-commit-id: 9b4f467b8f12dec4235213b1a4984dfe807a8b30

---
+++ src/Tests/WB.Core.SharedKernels.DataCollection.Tests/InterviewTests/when_unchecking_previously_checked_2nd_and_4th_options_in_multioption_question_used_as_roster_size_and_roster_level_of_question_and_rost___.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Linq;
+using Machine.Specifications;
+using Main.Core.Entities.SubEntities;
+using Moq;
+using Ncqrs.Spec;
+using WB.Core.SharedKernels.DataCollection.Aggregates;
+using WB.Core.SharedKernels.DataCollection.Events.Interview;
+using WB.Core.SharedKernels.DataCollection.Implementation.Aggregates;
+using WB.Core.SharedKernels.DataCollection.Implementation.Repositories;
+using It = Machine.Specifications.It;
+
+namespace WB.Core.SharedKernels.DataCollection.Tests.InterviewTests
+{
+    internal class when_unchecking_previously_checked_2nd_and_4th_options_in_multioption_question_used_as_roster_size_and_roster_level_of_question_and_roster_is_zero : InterviewTestsContext
+    {
+        Establish context = () =>
+        {
+            emptyRosterVector = new decimal[] { };
+            userId = Guid.Parse("CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC");
+            Guid questionnaireId = Guid.Parse("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+
+            questionId = Guid.Parse("11111111111111111111111111111111");
+            rosterId = Guid.Parse("44444444444444444444444444444444");
+
+            var availableOptions = new[]
+            {
+                (decimal) 14.7,
+                option2 = (decimal) 18.4,
+                option3 = 3,
+                option4 = -1,
+                (decimal) 256.128,
+            };
+
+            var questionaire = Mock.Of<IQuestionnaire>
+            (_
+                => _.HasQuestion(questionId) == true
+                && _.GetQuestionType(questionId) == QuestionType.MultyOption
+                && _.GetAnswerOptionsAsValues(questionId) == availableOptions
+                && _.GetRosterGroupsByRosterSizeQuestion(questionId) == new[] { rosterId }
+                && _.HasGroup(rosterId) == true
+                && _.IsRosterGroup(rosterId) == true
+            );
+
+            SetupInstanceToMockedServiceLocator<IQuestionnaireRepository>(
+                CreateQuestionnaireRepositoryStubWithOneQuestionnaire(questionnaireId, questionaire));
+
+            interview = CreateInterview(questionnaireId: questionnaireId);
+            interview.AnswerMultipleOptionsQuestion(userId, questionId, emptyRosterVector, DateTime.Now, new[] { option2, option3, option4 });
+
+            eventContext = new EventContext();
+        };
+
+        Because of = () =>
+            interview.AnswerMultipleOptionsQuestion(userId, questionId, emptyRosterVector, DateTime.Now, new[] { option3 });
+
+        Cleanup stuff = () =>
+        {
+            eventContext.Dispose();
+            eventContext = null;
+        };
+
+        It should_raise_MultipleOptionsQuestionAnswered_event = () =>
+            eventContext.ShouldContainEvent<MultipleOptionsQuestionAnswered>();
+
+        It should_not_raise_RosterRowAdded_event = () =>
+            eventContext.ShouldNotContainEvent<RosterRowAdded>();
+
+        It should_raise_2_RosterRowRemoved_events = () =>
+            eventContext.ShouldContainEvents<RosterRowRemoved>(count: 2);
+
+        It should_set_roster_id_to_all_RosterRowRemoved_events = () =>
+            eventContext.GetEvents<RosterRowRemoved>()
+                .ShouldEachConformTo(@event => @event.GroupId == rosterId);
+
+        It should_set_empty_outer_roster_vector_to_all_RosterRowRemoved_events = () =>
+            eventContext.GetEvents<RosterRowRemoved>()
+                .ShouldEachConformTo(@event => @event.OuterRosterVector.Length == 0);
+
+        It should_set_2nd_and_4th_options_as_roster_instance_ids_in_RosterRowRemoved_events = () =>
+            eventContext.GetEvents<RosterRowRemoved>().Select(@event => @event.RosterInstanceId).ToArray()
+                .ShouldContainOnly(option2, option4);
+
+        private static EventContext eventContext;
+        private static Interview interview;
+        private static Guid userId;
+        private static Guid questionId;
+        private static decimal[] emptyRosterVector;
+        private static decimal option2;
+        private static decimal option3;
+        private static decimal option4;
+        private static Guid rosterId;
+    }
+}
