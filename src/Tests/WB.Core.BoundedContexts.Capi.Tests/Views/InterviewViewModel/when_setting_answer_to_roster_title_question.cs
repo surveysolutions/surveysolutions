10c8ce8f4eead6b406d3ebdd1573b82d5f020735
Anatoliy 'TLK' Kolesnick <none@none>
2013-12-23 17:16:09 +0200

    some changes after task code review KP-2936
    Former-commit-id: bda849336a4d37abb43a18aa62bb38c669cb2636

--- src/Tests/WB.Core.BoundedContexts.Capi.Tests/Views/InterviewViewModel/when_setting_answer_to_roster_title_question.cs
+++ src/Tests/WB.Core.BoundedContexts.Capi.Tests/Views/InterviewViewModel/when_setting_answer_to_roster_title_question.cs
@@ -1,17 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cirrious.CrossCore;
+using Cirrious.CrossCore.Core;
+using Cirrious.CrossCore.IoC;
+using Cirrious.MvvmCross;
+using Cirrious.MvvmCross.Platform;
+using Cirrious.MvvmCross.ViewModels;
 using Machine.Specifications;
 using Main.Core.Documents;
 using Main.Core.Entities.SubEntities;
 using Main.Core.Entities.SubEntities.Question;
+using Moq;
 using WB.Core.BoundedContexts.Capi.Views.InterviewDetails;
 using WB.Core.SharedKernels.DataCollection.DataTransferObjects.Synchronization;
 using WB.Core.SharedKernels.DataCollection.ValueObjects.Interview;
 using WB.Core.SharedKernels.DataCollection.Views.Questionnaire;
+using It = Machine.Specifications.It;
 
 namespace WB.Core.BoundedContexts.Capi.Tests.Views.InterviewViewModel
 {
-    internal class whent_update_roster_title_called : InterviewViewModelTestContext
+    internal class when_setting_answer_to_roster_title_question : InterviewViewModelTestContext
     {
         Establish context = () =>
         {
@@ -19,8 +31,12 @@ namespace WB.Core.BoundedContexts.Capi.Tests.Views.InterviewViewModel
             rosterTitleQuestionId = Guid.Parse("11111111111111111111111111111111");
             rosterSizeQuestionId = Guid.Parse("33333333333333333333333333333333");
 
-            var rosterGroup = new Group() { PublicKey = rosterGroupId, IsRoster = true, RosterSizeQuestionId = rosterSizeQuestionId, RosterTitleQuestionId = rosterTitleQuestionId};
-            rosterGroup.Children.Add(new NumericQuestion() { PublicKey = rosterTitleQuestionId });
+            var rosterGroup = new Group() { PublicKey = rosterGroupId, IsRoster = true, RosterSizeQuestionId = rosterSizeQuestionId, RosterTitleQuestionId = rosterTitleQuestionId };
+            rosterGroup.Children.Add(new NumericQuestion()
+            {
+                PublicKey = rosterTitleQuestionId,
+                QuestionType = QuestionType.Numeric
+            });
 
             questionnarie = CreateQuestionnaireDocumentWithOneChapter(
                 new NumericQuestion()
@@ -36,13 +52,16 @@ namespace WB.Core.BoundedContexts.Capi.Tests.Views.InterviewViewModel
                 answers: new AnsweredQuestionSynchronizationDto[0],
                 propagatedGroupInstanceCounts: new Dictionary<InterviewItemId, RosterSynchronizationDto[]>()
                 {
-                    { new InterviewItemId(rosterGroupId, new decimal[0]), new []{ new RosterSynchronizationDto(rosterGroupId, new decimal[0], 0, null, null)} }
+                    {
+                        new InterviewItemId(rosterGroupId, new decimal[0]),
+                        new[] { new RosterSynchronizationDto(rosterGroupId, new decimal[0], 0, null, null) }
+                    }
                 });
             interviewViewModel = CreateInterviewViewModel(questionnarie, rosterStructure, interviewSynchronizationDto);
         };
 
         Because of = () =>
-            interviewViewModel.UpdateRosterRowTitle(rosterGroupId, new decimal[] { }, 0, rosterTitle);
+            interviewViewModel.SetAnswer(new InterviewItemId(rosterTitleQuestionId, new decimal[] { 0 }), rosterTitle);
 
         It should_roster_screen_title_rosterTitle = () =>
             ((QuestionnairePropagatedScreenViewModel)interviewViewModel.Screens[new InterviewItemId(rosterGroupId, new decimal[] { 0 })]).ScreenName.ShouldEqual(rosterTitle);
