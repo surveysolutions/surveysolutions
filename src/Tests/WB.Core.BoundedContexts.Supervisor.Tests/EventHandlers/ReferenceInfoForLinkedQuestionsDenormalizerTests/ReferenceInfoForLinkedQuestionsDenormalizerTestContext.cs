426b1b5400d547845f5c527ba546a6b99f2ba8d8
Slava Scherbak <slava.scherbak@gmail.com>
2013-12-23 14:31:02 +0200

    Supervisor: fixed bug with template import with fixed roster and linked question KP-2961
    Former-commit-id: 9b6cfffdf61388a42e460d32a8e807af194afcd9

---
+++ src/Tests/WB.Core.BoundedContexts.Supervisor.Tests/EventHandlers/ReferenceInfoForLinkedQuestionsDenormalizerTests/ReferenceInfoForLinkedQuestionsDenormalizerTestContext.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections.Generic;
+using Main.Core.Documents;
+using Main.Core.Entities.Composite;
+using Main.Core.Entities.SubEntities;
+using Main.Core.Entities.SubEntities.Question;
+using Main.Core.Events.Questionnaire;
+using Moq;
+using Ncqrs.Eventing.ServiceModel.Bus;
+using WB.Core.BoundedContexts.Supervisor.EventHandler;
+using WB.Core.BoundedContexts.Supervisor.Views.Questionnaire;
+using WB.Core.SharedKernels.DataCollection.ReadSide;
+
+namespace WB.Core.BoundedContexts.Supervisor.Tests.EventHandlers.ReferenceInfoForLinkedQuestionsDenormalizerTests
+{
+    internal class ReferenceInfoForLinkedQuestionsDenormalizerTestContext
+    {
+        protected static IPublishedEvent<T> ToPublishedEvent<T>(T @event, Guid? eventSourceId = null) where T : class
+        {
+            return Mock.Of<IPublishedEvent<T>>(publishedEvent => publishedEvent.Payload == @event && publishedEvent.EventSourceId == (eventSourceId ?? Guid.Parse("33333333333333333333333333333333")));
+        }
+
+        protected static IPublishedEvent<TemplateImported> CreateTemplateImportedEvent(QuestionnaireDocument questionnaire = null)
+        {
+            var questionnaireDocument = questionnaire ?? new QuestionnaireDocument();
+            return ToPublishedEvent(new TemplateImported
+            {
+                Source = questionnaireDocument
+            },
+                questionnaireDocument.PublicKey);
+        }
+
+        protected static ReferenceInfoForLinkedQuestionsDenormalizer CreateReferenceInfoForLinkedQuestionsDenormalizer(
+            IVersionedReadSideRepositoryWriter<ReferenceInfoForLinkedQuestions> questionnaires = null)
+        {
+            return
+                new ReferenceInfoForLinkedQuestionsDenormalizer(questionnaires ??
+                    Mock.Of<IVersionedReadSideRepositoryWriter<ReferenceInfoForLinkedQuestions>>());
+        }
+
+        protected static QuestionnaireDocument CreateQuestionnaireDocumentWithRosterAndNumericQuestionAndLinedQuestionAfter(Guid questionnaireId, Guid rosterId, Guid linkedId)
+        {
+            Guid numericId = Guid.Parse("55552222111100000000111122225555");
+            return new QuestionnaireDocument
+            {
+                PublicKey = questionnaireId,
+                Children = new List<IComposite>
+                {
+                    new Group("Roster")
+                    {
+                        PublicKey = rosterId,
+                        IsRoster = true,
+                        RosterSizeSource = RosterSizeSourceType.FixedTitles,
+                        RosterFixedTitles = new []{"Title1", "Title2"},
+                        Children = new List<IComposite>
+                        {
+                            new NumericQuestion("Numeric")
+                            {
+                                PublicKey = numericId,
+                                QuestionType = QuestionType.Numeric,
+                                IsInteger = true
+                            }
+                        }
+                    },
+                    new MultyOptionsQuestion("MultyOption")
+                    {
+                        PublicKey = linkedId,
+                        QuestionType = QuestionType.MultyOption,
+                        LinkedToQuestionId = numericId
+                    }
+                }
+            };
+        }
+    }
+}
