dcf6c4b389b13aaf9247c81b4d7c2c21cf80a3c2
Roman Lytvyn <none@none>
2013-12-25 19:10:24 +0200

    designer: fixed confirming user registration by administrator. KP-2944
    --HG--
    branch : next
    
    
    Former-commit-id: 63ea638c31c3fcd4a382f2bcf65d164622824952

---
+++ src/Tests/WB.Core.BoundedContexts.Designer.Tests/AccountTests/when_updating_account.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Machine.Specifications;
+using Ncqrs.Spec;
+using WB.Core.BoundedContexts.Designer.Aggregates;
+using WB.UI.Designer.Providers.CQRS.Accounts.Events;
+
+namespace WB.Core.BoundedContexts.Designer.Tests.AccountTests
+{
+    internal class when_updating_account : AccountTestsContext
+    {
+        Establish context = () =>
+        {
+            accounId = Guid.Parse("11111111111111111111111111111111");
+            userName = "user name";
+            userEmail = "user@e.mail";
+            comment = "some comment";
+            passwordQuestion = "secret question";
+
+            account = CreateAccount(accounId);
+
+            eventContext = new EventContext();
+        };
+
+        Because of = () =>
+            account.Update(userName: userName, comment: comment, email: userEmail, passwordQuestion: passwordQuestion, isLockedOut: true,
+                isConfirmed: true);
+
+        Cleanup stuff = () =>
+        {
+            eventContext.Dispose();
+            eventContext = null;
+        };
+
+        It should_raise_AccountUpdated_event = () =>
+            eventContext.ShouldContainEvent<AccountUpdated>();
+
+        It should_raise_AccountUpdated_event_with_UserName_equal_to_userName = () =>
+            eventContext.GetSingleEvent<AccountUpdated>().UserName.ShouldEqual(userName);
+
+        It should_raise_AccountUpdated_event_with_Email_equal_to_question_id = () =>
+            eventContext.GetSingleEvent<AccountUpdated>().Email.ShouldEqual(userEmail);
+
+        It should_raise_AccountUpdated_event_with_Comment_equal_to_comment = () =>
+            eventContext.GetSingleEvent<AccountUpdated>().Comment.ShouldEqual(comment);
+
+        It should_raise_AccountUpdated_event_with_PasswordQuestion_equal_to_passwordQuestion = () =>
+            eventContext.GetSingleEvent<AccountUpdated>().PasswordQuestion.ShouldEqual(passwordQuestion);
+
+        It should_raise_AccountConfirmed_event = () =>
+            eventContext.ShouldContainEvent<AccountConfirmed>();
+
+        It should_raise_AccountLocked_event = () =>
+            eventContext.ShouldContainEvent<AccountLocked>();
+
+        private static EventContext eventContext;
+        private static AccountAR account;
+        private static Guid accounId;
+        private static string userName;
+        private static string userEmail;
+        private static string comment;
+        private static string passwordQuestion;
+    }
+}
