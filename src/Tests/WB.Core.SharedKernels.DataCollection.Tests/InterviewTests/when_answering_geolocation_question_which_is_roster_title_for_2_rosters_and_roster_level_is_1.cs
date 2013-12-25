2b8369cb8eaffdbf45e72b6f2bc3cd66f0d37071
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2013-12-25 17:31:16 +0200

    Interview: implemented roster title changed event firing for GPS questions (with tests) KP-2956
    Former-commit-id: b2d50e190f026e8ec2bea55536c7179338df92fd

---
+++ src/Tests/WB.Core.SharedKernels.DataCollection.Tests/InterviewTests/when_answering_geolocation_question_which_is_roster_title_for_2_rosters_and_roster_level_is_1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Linq;
+using Machine.Specifications;
+using Main.Core.Entities.SubEntities;
+using Moq;
+using Ncqrs.Spec;
+using WB.Core.SharedKernels.DataCollection.Aggregates;
+using WB.Core.SharedKernels.DataCollection.Events.Interview;
+using WB.Core.SharedKernels.DataCollection.Implementation.Aggregates;
+using WB.Core.SharedKernels.DataCollection.Implementation.Repositories;
+using It = Machine.Specifications.It;
+
+namespace WB.Core.SharedKernels.DataCollection.Tests.InterviewTests
+{
+    internal class when_answering_geolocation_question_which_is_roster_title_for_2_rosters_and_roster_level_is_1 : InterviewTestsContext
+    {
+        Establish context = () =>
+        {
+            userId = Guid.Parse("FFFFFFFFFFFFFFFFFFFFFF1111111111");
+            var questionnaireId = Guid.Parse("DDDDDDDDDDDDDDDDDDDDDD0000000000");
+
+            emptyRosterVector = new decimal[] { };
+            var rosterInstanceId = (decimal)22.5;
+            rosterVector = emptyRosterVector.Concat(new[] { rosterInstanceId }).ToArray();
+
+            questionId = Guid.Parse("11111111111111111111111111111111");
+            rosterAId = Guid.Parse("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+            rosterBId = Guid.Parse("BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB");
+
+
+            var questionnaire = Mock.Of<IQuestionnaire>
+            (_
+                => _.HasQuestion(questionId) == true
+                && _.GetQuestionType(questionId) == QuestionType.GpsCoordinates
+                && _.GetRostersFromTopToSpecifiedQuestion(questionId) == new[] { rosterAId }
+                && _.DoesQuestionSpecifyRosterTitle(questionId) == true
+                && _.GetRostersAffectedByRosterTitleQuestion(questionId) == new[] { rosterAId, rosterBId }
+            );
+
+
+            SetupInstanceToMockedServiceLocator<IQuestionnaireRepository>(
+                CreateQuestionnaireRepositoryStubWithOneQuestionnaire(questionnaireId, questionnaire));
+
+            interview = CreateInterview(questionnaireId: questionnaireId);
+            interview.Apply(new RosterRowAdded(rosterAId, emptyRosterVector, rosterInstanceId, sortIndex: null));
+            interview.Apply(new RosterRowAdded(rosterBId, emptyRosterVector, rosterInstanceId, sortIndex: null));
+
+            eventContext = new EventContext();
+        };
+
+        Because of = () =>
+            interview.AnswerGeoLocationQuestion(
+                userId, questionId, rosterVector, DateTime.Now,
+                latitude: -1.234, longitude: 1.00025, accuracy: 0, timestamp: new DateTimeOffset(DateTime.Now));
+
+        Cleanup stuff = () =>
+        {
+            eventContext.Dispose();
+            eventContext = null;
+        };
+
+        It should_raise_GeoLocationQuestionAnswered_event = () =>
+            eventContext.ShouldContainEvent<GeoLocationQuestionAnswered>();
+
+        It should_raise_2_RosterRowTitleChanged_events = () =>
+            eventContext.ShouldContainEvents<RosterRowTitleChanged>(count: 2);
+
+        It should_set_2_affected_roster_ids_in_RosterRowTitleChanged_events = () =>
+            eventContext.GetEvents<RosterRowTitleChanged>().Select(@event => @event.GroupId).ToArray()
+                .ShouldContainOnly(rosterAId, rosterBId);
+
+        It should_set_empty_outer_roster_vector_to_all_RosterRowTitleChanged_events = () =>
+            eventContext.GetEvents<RosterRowTitleChanged>()
+                .ShouldEachConformTo(@event => Enumerable.SequenceEqual(@event.OuterRosterVector, emptyRosterVector));
+
+        It should_set_last_element_of_roster_vector_to_roster_instance_id_in_all_RosterRowTitleChanged_events = () =>
+            eventContext.GetEvents<RosterRowTitleChanged>()
+                .ShouldEachConformTo(@event => @event.RosterInstanceId == rosterVector.Last());
+
+        It should_set_title_to_latitude_and_longitude_divided_by_semicolon_in_square_brackets_in_all_RosterRowTitleChanged_events = () =>
+            eventContext.GetEvents<RosterRowTitleChanged>().Select(@event => @event.Title)
+                .ShouldEachConformTo(title => title == "[-1.234;1.00025]");
+
+        private static EventContext eventContext;
+        private static Interview interview;
+        private static Guid userId;
+        private static Guid questionId;
+        private static decimal[] rosterVector;
+        private static decimal[] emptyRosterVector;
+        private static Guid rosterAId;
+        private static Guid rosterBId;
+    }
+}
