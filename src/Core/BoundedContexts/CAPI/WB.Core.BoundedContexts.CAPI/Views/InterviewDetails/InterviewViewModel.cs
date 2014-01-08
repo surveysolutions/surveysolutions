c9ccddb60b9549906c7b605ce4f4256383a6c707
Anastasya Kholodova <nastya.kholodova@gmail.com>
2014-01-08 15:43:01 -0500

    Roster: improvements by restore questionnarie roster structure from old template KP-2974
    Former-commit-id: 0d22fb301a8779de02f34cf8117b4bef8388dca4

--- src/Core/BoundedContexts/CAPI/WB.Core.BoundedContexts.CAPI/Views/InterviewDetails/InterviewViewModel.cs
+++ src/Core/BoundedContexts/CAPI/WB.Core.BoundedContexts.CAPI/Views/InterviewDetails/InterviewViewModel.cs
@@ -149,8 +149,8 @@ namespace WB.Core.BoundedContexts.Capi.Views.InterviewDetails
             {
                 foreach (var headQuestionId in rosterDescription.RosterIdToRosterTitleQuestionIdMap.Values)
                 {
-                    if (headQuestionId!=null)
-                        this.listOfHeadQuestionsMappedOnScope.Add(headQuestionId.QuestionId, rosterDescription.ScopeId);
+                    if (headQuestionId != null)
+                        this.listOfHeadQuestionsMappedOnScope[headQuestionId.QuestionId] = rosterDescription.ScopeId;
                 }
             }
         }
