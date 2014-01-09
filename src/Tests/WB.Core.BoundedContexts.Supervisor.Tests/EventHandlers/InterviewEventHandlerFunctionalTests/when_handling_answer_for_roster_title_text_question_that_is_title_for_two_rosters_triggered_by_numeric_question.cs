3c59a1a0142ac0420f0e34758bb4e671840cca0c
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2014-01-09 16:37:39 +0200

    Interview\EventHandlers\Tests: updated test which highlights bug with multiple rosters and roster title KP-2936
    Former-commit-id: 87c63f2161a1ec878bb4580c83a803a714f6ab15

--- src/Tests/WB.Core.BoundedContexts.Supervisor.Tests/EventHandlers/InterviewEventHandlerFunctionalTests/when_handling_answer_for_roster_title_text_question_that_is_title_for_two_rosters_triggered_by_numeric_question.cs
+++ src/Tests/WB.Core.BoundedContexts.Supervisor.Tests/EventHandlers/InterviewEventHandlerFunctionalTests/when_handling_answer_for_roster_title_text_question_that_is_title_for_two_rosters_triggered_by_numeric_question.cs
@@ -8,8 +8,8 @@ using WB.Core.SharedKernels.DataCollection.Events.Interview;
 
 namespace WB.Core.BoundedContexts.Supervisor.Tests.EventHandlers.InterviewEventHandlerFunctionalTests
 {
-    [Ignore]
-    internal class when_answered_head_text_question_that_in_questionnaire_with_2_rosters_triggered_by_numeric_question : InterviewEventHandlerFunctionalTestContext
+    [Ignore("highlights bug in KP-2936")]
+    internal class when_handling_answer_for_roster_title_text_question_that_is_title_for_two_rosters_triggered_by_numeric_question : InterviewEventHandlerFunctionalTestContext
     {
         Establish context = () =>
         {
@@ -18,12 +18,14 @@ namespace WB.Core.BoundedContexts.Supervisor.Tests.EventHandlers.InterviewEventH
 
             rosterScopeId = Guid.Parse("12222222222222222222222222222222");
 
-            headQuestionId = Guid.Parse("13333333333333333333333333333333");
+            rosterTitleQuestionId = Guid.Parse("13333333333333333333333333333333");
 
             viewState = CreateViewWithSequenceOfInterviewData();
 
             var questionnaireRosterStructure = CreateQuestionnaireRosterStructure(rosterScopeId,
-                new Dictionary<Guid, Guid?>() { { rosterGroup1Id, headQuestionId }, { rosterGroup2Id, headQuestionId } });
+                new Dictionary<Guid, Guid?>() { { rosterGroup1Id, rosterTitleQuestionId }, { rosterGroup2Id, rosterTitleQuestionId } });
+
+            textQuestionAnsweredEvent = CreateTextQuestionAnsweredEvent(rosterTitleQuestionId, new decimal[] { 0 }, receivedAnswer);
 
             interviewEventHandlerFunctional = CreateInterviewEventHandlerFunctional(questionnaireRosterStructure);
             viewState = interviewEventHandlerFunctional.Update(viewState, CreatePublishableEvent(new RosterRowAdded(rosterGroup1Id, new decimal[0], 0, null)));
@@ -32,20 +34,21 @@ namespace WB.Core.BoundedContexts.Supervisor.Tests.EventHandlers.InterviewEventH
 
         Because of = () =>
             viewState = interviewEventHandlerFunctional.Update(viewState,
-                CreatePublishableEvent(new TextQuestionAnswered(Guid.NewGuid(), headQuestionId, new decimal[] { 0 }, DateTime.Now, helloRoster)));
+                CreatePublishableEvent(textQuestionAnsweredEvent));
 
-        It should_roster_title_be_equal_to_recived_answer_for_the_first_roster = () =>
-            viewState.Document.Levels["0"].RosterRowTitles[rosterGroup1Id].ShouldEqual(helloRoster);
+        It shoult_set_first_roster_title_to_received_answer = () =>
+            viewState.Document.Levels["0"].RosterRowTitles[rosterGroup1Id].ShouldEqual(receivedAnswer);
 
-        It should_roster_title_be_equal_to_recived_answer_for_the_second_roster = () =>
-            viewState.Document.Levels["0"].RosterRowTitles[rosterGroup2Id].ShouldEqual(helloRoster);
+        It shoult_set_second_roster_title_to_received_answer = () =>
+            viewState.Document.Levels["0"].RosterRowTitles[rosterGroup2Id].ShouldEqual(receivedAnswer);
 
         private static InterviewEventHandlerFunctional interviewEventHandlerFunctional;
         private static ViewWithSequence<InterviewData> viewState;
         private static Guid rosterGroup1Id;
         private static Guid rosterGroup2Id;
         private static Guid rosterScopeId;
-        private static Guid headQuestionId;
-        private static string helloRoster = "hello roster";
+        private static Guid rosterTitleQuestionId;
+        private static string receivedAnswer = "hello roster";
+        private static TextQuestionAnswered textQuestionAnsweredEvent;
     }
 }
