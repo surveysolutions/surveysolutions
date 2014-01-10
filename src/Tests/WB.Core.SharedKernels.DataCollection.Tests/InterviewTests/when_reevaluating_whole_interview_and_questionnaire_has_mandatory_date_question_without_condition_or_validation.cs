15cb750a14fd566743e5974c3a37a129c0e0cbea
Slava Scherbak <slava.scherbak@gmail.com>
2014-01-10 16:18:43 +0200

    Interview: added changes after code review KP-2893
    Former-commit-id: 6c6b2e84dadc0d47b432552f0437673d9604c555

---
+++ src/Tests/WB.Core.SharedKernels.DataCollection.Tests/InterviewTests/when_reevaluating_whole_interview_and_questionnaire_has_mandatory_date_question_without_condition_or_validation.cs
@@ -0,0 +1,59 @@
+using System;
+using Machine.Specifications;
+using Main.Core.Entities.SubEntities;
+using Moq;
+using Ncqrs.Spec;
+using WB.Core.SharedKernels.DataCollection.Aggregates;
+using WB.Core.SharedKernels.DataCollection.Events.Interview;
+using WB.Core.SharedKernels.DataCollection.Implementation.Aggregates;
+using WB.Core.SharedKernels.DataCollection.Implementation.Repositories;
+using WB.Core.SharedKernels.DataCollection.ValueObjects.Questionnaire;
+using It = Machine.Specifications.It;
+using it = Moq.It;
+
+namespace WB.Core.SharedKernels.DataCollection.Tests.InterviewTests
+{
+    internal class when_reevaluating_whole_interview_and_questionnaire_has_mandatory_date_question_without_condition_or_validation : InterviewTestsContext
+    {
+        Establish context = () =>
+        {
+            var userId = Guid.Parse("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF");
+
+            var questionaire = Mock.Of<IQuestionnaire>
+            (_
+                => _.HasQuestion(dateQuestionId) == true
+                && _.GetQuestionType(dateQuestionId) == QuestionType.DateTime
+                && _.GetAllMandatoryQuestions() == new[] { dateQuestionId }
+            );
+
+            var questionnaireRepository = CreateQuestionnaireRepositoryStubWithOneQuestionnaire(questionnaireId, questionaire);
+
+            SetupInstanceToMockedServiceLocator<IQuestionnaireRepository>(questionnaireRepository);
+
+            eventContext = new EventContext();
+
+            interview = CreateInterview(questionnaireId: questionnaireId);
+            interview.Apply(new DateTimeQuestionAnswered(userId, dateQuestionId, new decimal[0], DateTime.Now, new DateTime(1985, 6, 3)));
+        };
+
+        Cleanup stuff = () =>
+        {
+            eventContext.Dispose();
+            eventContext = null;
+        };
+
+        Because of = () =>
+            interview.ReevaluateSynchronizedInterview();
+
+        It should_raise_AnswerDeclaredInvalid_event_with_QuestionId_equal_to_dateQuestionId = () =>
+            eventContext.ShouldContainEvent<AnswerDeclaredInvalid>(@event => @event.QuestionId == dateQuestionId);
+
+        It should_raise_AnswerDeclaredValid_event_with_QuestionId_equal_to_dateQuestionId = () =>
+            eventContext.ShouldContainEvent<AnswerDeclaredValid>(@event => @event.QuestionId == dateQuestionId);
+
+        private static EventContext eventContext;
+        private static Interview interview;
+        private static Guid questionnaireId = Guid.Parse("10000000000000000000000000000000");
+        private static Guid dateQuestionId = Guid.Parse("20000000000000000000000000000000");
+    }
+}
