dcff31efd0f1b262cb1a3977f646b21a2c8fa6e9
Roman Lytvyn <none@none>
2014-01-14 16:02:56 +0200

    designer: added additional tests for AccountAR. KP-2944
    --HG--
    branch : next
    
    
    Former-commit-id: 520f01b69631e93debe82e42d5e248a39463477e

--- src/Tests/WB.Core.BoundedContexts.Designer.Tests/AccountTests/when_updating_account_and_it_should_be_unlocked.cs
+++ src/Tests/WB.Core.BoundedContexts.Designer.Tests/AccountTests/when_updating_account_and_it_should_be_unlocked.cs
@@ -6,13 +6,13 @@ using WB.UI.Designer.Providers.CQRS.Accounts.Events;
 
 namespace WB.Core.BoundedContexts.Designer.Tests.AccountTests
 {
-    internal class when_updating_locked_account : AccountTestsContext
+    internal class when_updating_account_and_it_should_be_unlocked : AccountTestsContext
     {
         Establish context = () =>
         {
-            accounId = Guid.Parse("11111111111111111111111111111111");
+            var accountId = Guid.Parse("11111111111111111111111111111111");
 
-            account = CreateAccount(accounId);
+            account = CreateAccount(accountId);
             account.Apply(new AccountLocked());
 
             eventContext = new EventContext();
@@ -32,6 +32,5 @@ namespace WB.Core.BoundedContexts.Designer.Tests.AccountTests
 
         private static EventContext eventContext;
         private static AccountAR account;
-        private static Guid accounId;
     }
 }
