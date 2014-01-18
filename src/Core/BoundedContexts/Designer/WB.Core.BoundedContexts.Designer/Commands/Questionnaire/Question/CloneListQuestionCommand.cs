bcab54ec96ef5c7b907ad4fd0f714f70f6a14b07
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2014-01-17 21:34:30 -0500

    Designer: TextList question update [KP-3006]
    Former-commit-id: c2958ad0efe5f3c532bdcacadf5bf8b5bb9a4ee3

---
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Commands/Questionnaire/Question/CloneListQuestionCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Main.Core.Entities.SubEntities;
+using Ncqrs.Commanding.CommandExecution.Mapping.Attributes;
+using WB.Core.BoundedContexts.Designer.Commands.Questionnaire.Base;
+
+namespace WB.Core.BoundedContexts.Designer.Commands.Questionnaire.Question
+{
+    [Serializable]
+    [MapsToAggregateRootMethod(typeof(Aggregates.Questionnaire), "CloneListQuestion")]
+    public class CloneListQuestionCommand : AbstractListQuestionCommand
+    {
+        public CloneListQuestionCommand(Guid questionnaireId, Guid questionId, Guid groupId, Guid sourceQuestionId, int targetIndex,
+            string title, string alias, bool isMandatory, bool isFeatured, QuestionScope scope, string condition, 
+            string validationExpression, string validationMessage, string instructions, Guid responsibleId, int? countOfDecimalPlaces)
+            : base(questionnaireId, questionId, title, alias, isMandatory, isFeatured, scope, condition, 
+                   validationExpression, validationMessage, instructions, responsibleId, countOfDecimalPlaces)
+        {
+            this.GroupId = groupId;
+            this.SourceQuestionId = sourceQuestionId;
+            this.TargetIndex = targetIndex;
+        }
+
+        public Guid GroupId { get; private set; }
+        public Guid SourceQuestionId { get; private set; }
+        public int TargetIndex { get; private set; }
+    }
+}
---
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Events/Questionnaire/ListQuestionCloned.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace WB.Core.BoundedContexts.Designer.Events.Questionnaire
+{
+    public class ListQuestionCloned : AbstractListQuestionDataEvent
+    {
+        public Guid SourceQuestionId { get; set; }
+        public Guid GroupPublicKey { get; set; }
+        public int TargetIndex { get; set; }
+    }
+}
