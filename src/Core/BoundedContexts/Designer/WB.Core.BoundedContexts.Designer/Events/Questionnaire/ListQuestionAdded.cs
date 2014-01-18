2c5dae3ffa406c7b1057b7b3e7166b90fce94223
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2014-01-17 21:34:30 -0500

    Designer: TextList question update [KP-3006]
    Former-commit-id: c2958ad0efe5f3c532bdcacadf5bf8b5bb9a4ee3

---
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Events/Questionnaire/ListQuestionAdded.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace WB.Core.BoundedContexts.Designer.Events.Questionnaire
+{
+    public class ListQuestionAdded : AbstractListQuestionDataEvent
+    {
+        public Guid GroupPublicKey { get; set; }
+    }
+}
