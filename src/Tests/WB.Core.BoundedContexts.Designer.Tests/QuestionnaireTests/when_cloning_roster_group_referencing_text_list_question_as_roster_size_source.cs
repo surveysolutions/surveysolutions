d3c05e45fe811ed7eb3db8615e2c7cfbca4e1a6f
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2014-01-20 14:31:24 +0200

    Designer\Questionnaire\rosters: added tests requiring exception not to be thrown when referencing text list size source question when cloning and updating KP-3002
    Former-commit-id: 3c3a5a78716898c5c31681fb0df5441231a5a25f

--- src/Tests/WB.Core.BoundedContexts.Designer.Tests/QuestionnaireTests/when_cloning_roster_group_referencing_text_list_question_as_roster_size_source.cs
+++ src/Tests/WB.Core.BoundedContexts.Designer.Tests/QuestionnaireTests/when_cloning_roster_group_referencing_text_list_question_as_roster_size_source.cs
@@ -6,21 +6,22 @@ using WB.Core.BoundedContexts.Designer.Aggregates;
 
 namespace WB.Core.BoundedContexts.Designer.Tests.QuestionnaireTests
 {
-    internal class when_adding_roster_group_referencing_text_list_question : QuestionnaireTestsContext
+    internal class when_cloning_roster_group_referencing_text_list_question_as_roster_size_source : QuestionnaireTestsContext
     {
         Establish context = () =>
         {
             questionnaire = CreateQuestionnaire(responsibleId);
             questionnaire.Apply(new NewGroupAdded { PublicKey = chapterId });
             questionnaire.Apply(new NewQuestionAdded { QuestionType = QuestionType.TextList, PublicKey = rosterSizeQuestionId, GroupPublicKey = chapterId });
+            questionnaire.Apply(new NewGroupAdded { PublicKey = sourceGroupId, ParentGroupPublicKey = chapterId });
         };
 
         Because of = () =>
             exception = Catch.Exception(() =>
-                questionnaire.AddGroup(
+                questionnaire.CloneGroupWithoutChildren(
                     groupId, responsibleId, "title", rosterSizeQuestionId, "description", null, chapterId,
                     isRoster: true, rosterSizeSource: RosterSizeSourceType.Question, rosterFixedTitles: new string[]{}, 
-                    rosterTitleQuestionId: null));
+                    rosterTitleQuestionId: null, sourceGroupId: sourceGroupId, targetIndex: 0));
 
         It should_not_fail = () =>
             exception.ShouldEqual(null);
@@ -29,6 +30,7 @@ namespace WB.Core.BoundedContexts.Designer.Tests.QuestionnaireTests
         private static Questionnaire questionnaire;
 
         private static Guid chapterId = Guid.Parse("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+        private static Guid sourceGroupId = Guid.Parse("DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD");
         private static Guid groupId = Guid.Parse("BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB");
         private static Guid responsibleId = Guid.Parse("CCCCCCCCCCCCCCCCDDDDDDDDDDDDDDDD");
         private static Guid rosterSizeQuestionId = Guid.Parse("11111111111111111111111111111111");
