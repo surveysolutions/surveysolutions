1157fc711481fced6e1a5d9804cf54e87316c471
Slava Scherbak <slava.scherbak@gmail.com>
2014-01-20 15:57:03 +0200

    Interview\Core: added invariants, event and command for List question KP-2996
    Former-commit-id: 4e4eac5125d88983ce2a1ee4581360cbfc13b405

---
+++ src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/Commands/Interview/AnswerTextListQuestionCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Ncqrs.Commanding.CommandExecution.Mapping.Attributes;
+using WB.Core.SharedKernels.DataCollection.Commands.Interview.Base;
+
+namespace WB.Core.SharedKernels.DataCollection.Commands.Interview
+{
+    [MapsToAggregateRootMethod(typeof(Implementation.Aggregates.Interview), "AnswerTextListQuestion")]
+    public class AnswerTextListQuestionCommand : AnswerQuestionCommand
+    {
+        public Tuple<decimal, string>[] Answers { get; private set; }
+
+        public AnswerTextListQuestionCommand(Guid interviewId, Guid userId, Guid questionId, decimal[] rosterVector, DateTime answerTime, Tuple<decimal, string>[] answers)
+            : base(interviewId, userId, questionId, rosterVector, answerTime)
+        {
+            this.Answers = answers;
+        }
+    }
+}
