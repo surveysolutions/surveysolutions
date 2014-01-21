ee900511dde9eb653833d92f7bf52f5e5c5aa435
Slava Scherbak <slava.scherbak@gmail.com>
2014-01-21 17:05:26 +0200

    Designer\Edit: updated TextList question events KP-3009, KP-3006
    Former-commit-id: 8e1ead4738d45c9f9eb28fff3716828c3abb08ec

--- src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Events/Questionnaire/AbstractListQuestionDataEvent.cs
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Events/Questionnaire/AbstractListQuestionDataEvent.cs
@@ -1,7 +1,17 @@
-﻿namespace WB.Core.BoundedContexts.Designer.Events.Questionnaire
+﻿using System;
+using Main.Core.Entities.SubEntities;
+using Main.Core.Events.Questionnaire;
+
+namespace WB.Core.BoundedContexts.Designer.Events.Questionnaire
 {
-    public class AbstractListQuestionDataEvent : AbstractQuestionDataEvent
+    public class AbstractListQuestionDataEvent : QuestionnaireActiveEvent
     {
+        public string ConditionExpression { get; set; }
+        public string Instructions { get; set; }
+        public bool Mandatory { get; set; }
+        public Guid PublicKey { get; set; }
+        public string QuestionText { get; set; }
+        public string StataExportCaption { get; set; }
         public int? MaxAnswerCount { get; set; }
     }
 }
