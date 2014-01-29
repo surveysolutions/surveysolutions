727f1595fae24bc6045dca0ea26618d8bf7aed27
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2014-01-28 17:01:37 -0800

    Supervisor\UI: handling of  text list questio is added KP-3012
    Former-commit-id: 144c3502006222834e10ab07fd5a9dffb2e36ce7

---
+++ src/Core/BoundedContexts/Supervisor/WB.Core.BoundedContexts.Supervisor/Views/Interview/InterviewTextListAnswer.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+
+namespace WB.Core.BoundedContexts.Supervisor.Views.Interview
+{
+    public class InterviewTextListAnswer
+    {
+        public InterviewTextListAnswer(decimal value, string answer)
+        {
+            this.Value = value;
+            this.Answer = answer;
+        }
+
+        public decimal Value { get; private set; }
+        public string Answer { get; private set; }
+    }
+}
