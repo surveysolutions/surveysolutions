9a9fb35890e571299a16b00cc1aaa345f4035508
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2014-01-30 21:07:01 -0500

    Android: Refactoring TextListQuestionView and fix keyboard KP-3011
    Former-commit-id: 76897c59b55089486a146a461213379e13f06426

---
+++ src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/Views/Interview/InterviewTextListAnswer.cs
@@ -0,0 +1,15 @@
+﻿namespace WB.Core.SharedKernels.DataCollection.Views.Interview
+{
+    public class InterviewTextListAnswer
+    {
+        public InterviewTextListAnswer(decimal value, string answer)
+        {
+            this.Value = value;
+            this.Answer = answer;
+        }
+
+        public decimal Value { get; private set; }
+
+        public string Answer { get; private set; }
+    }
+}
