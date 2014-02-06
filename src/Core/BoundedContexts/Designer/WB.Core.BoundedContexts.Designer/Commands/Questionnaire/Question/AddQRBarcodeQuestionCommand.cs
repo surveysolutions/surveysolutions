6a60e9be558a4d25f4b2b717601e54567f93a421
Roman Lytvyn <none@none>
2014-02-06 19:18:55 +0200

    designer: fixed add QR Barcode command. KP-3059
    Former-commit-id: f95227f304f78f7fbe1215fc248d7a106bf304e1

--- src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Commands/Questionnaire/Question/AddQRBarcodeQuestionCommand.cs
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Commands/Questionnaire/Question/AddQRBarcodeQuestionCommand.cs
@@ -6,12 +6,12 @@ namespace WB.Core.BoundedContexts.Designer.Commands.Questionnaire.Question
 {
     [Serializable]
     [MapsToAggregateRootMethod(typeof (Aggregates.Questionnaire), "AddQRBarcodeQuestion")]
-    public class AddQRBarcodeQuestionCommand : AbstractUpdateQuestionCommand
+    public class AddQRBarcodeQuestionCommand : AbstractAddQuestionCommand
     {
         public AddQRBarcodeQuestionCommand(Guid questionnaireId, Guid questionId, string title, string variableName, bool isMandatory,
-            string condition, string instructions, Guid responsibleId)
+            string condition, string instructions, Guid responsibleId, Guid parentGroupId)
             : base(
                 responsibleId: responsibleId, questionnaireId: questionnaireId, questionId: questionId, title: title,
-                variableName: variableName, isMandatory: isMandatory, condition: condition, instructions: instructions) {}
+                variableName: variableName, isMandatory: isMandatory, condition: condition, instructions: instructions, ) {}
     }
 }
