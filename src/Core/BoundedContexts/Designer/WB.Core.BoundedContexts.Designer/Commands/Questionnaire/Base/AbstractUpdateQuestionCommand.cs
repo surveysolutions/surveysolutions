92b0e8a344a88fb9dabe2eeee5886a04616f5e14
Roman Lytvyn <litvin.roma@gmail.com>
2014-02-06 16:27:50 +0200

    designer: added commands for QR Barcode question. KP-3059
    Former-commit-id: 42496595e67b6f6bca798c4ff6adc0aab85ed31e

---
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Commands/Questionnaire/Base/AbstractUpdateQuestionCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace WB.Core.BoundedContexts.Designer.Commands.Questionnaire.Base
+{
+    public abstract class AbstractUpdateQuestionCommand : QuestionCommand
+    {
+        protected AbstractUpdateQuestionCommand(Guid responsibleId,
+            Guid questionnaireId,
+            Guid questionId, string title, string variableName, bool isMandatory, string condition, string instructions)
+            : base(questionnaireId, questionId, responsibleId)
+        {
+            Title = title;
+            VariableName = variableName;
+            IsMandatory = isMandatory;
+            Condition = condition;
+            Instructions = instructions;
+        }
+
+        public string Title { get; private set; }
+        public string VariableName { get; private set; }
+        public bool IsMandatory { get; private set; }
+        public string Condition { get; set; }
+        public string Instructions { get; private set; }
+    }
+}
