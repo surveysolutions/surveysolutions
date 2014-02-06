d542df36c2a9f4ae537b90c82084229678699aa8
Roman Lytvyn <litvin.roma@gmail.com>
2014-02-06 16:27:50 +0200

    designer: added commands for QR Barcode question. KP-3059
    Former-commit-id: 42496595e67b6f6bca798c4ff6adc0aab85ed31e

---
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Commands/Questionnaire/Base/AbstractAddQuestionCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace WB.Core.BoundedContexts.Designer.Commands.Questionnaire.Base
+{
+    public abstract class AbstractAddQuestionCommand : AbstractUpdateQuestionCommand
+    {
+        protected AbstractAddQuestionCommand(Guid responsibleId, Guid questionnaireId, Guid questionId, string title, string variableName,
+            bool isMandatory, string condition, string instructions, Guid groupId)
+            : base(
+                questionnaireId: questionnaireId, questionId: questionId, responsibleId: responsibleId, title: title,
+                variableName: variableName, isMandatory: isMandatory, condition: condition, instructions: instructions)
+        {
+            this.GroupId = groupId;
+        }
+
+        public Guid GroupId { get; private set; }
+    }
+}
---
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Commands/Questionnaire/Base/AbstractCloneQuestionCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace WB.Core.BoundedContexts.Designer.Commands.Questionnaire.Base
+{
+    public abstract class AbstractCloneQuestionCommand : AbstractAddQuestionCommand
+    {
+        protected AbstractCloneQuestionCommand(Guid responsibleId, Guid questionnaireId, Guid questionId, string title, string variableName,
+            bool isMandatory, string condition, string instructions, Guid groupId, Guid sourceQuestionId, int targetIndex)
+            : base(
+                questionnaireId: questionnaireId, questionId: questionId, responsibleId: responsibleId, title: title,
+                variableName: variableName, isMandatory: isMandatory, condition: condition, instructions: instructions, groupId: groupId)
+        {
+            this.SourceQuestionId = sourceQuestionId;
+            this.TargetIndex = targetIndex;
+        }
+
+        public Guid SourceQuestionId { get; private set; }
+        public int TargetIndex { get; private set; }
+    }
+}
