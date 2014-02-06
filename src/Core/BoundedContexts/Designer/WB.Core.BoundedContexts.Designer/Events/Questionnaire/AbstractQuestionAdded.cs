cadee3c0bec0ec0002c8010785c96305b8076f46
Roman Lytvyn <none@none>
2014-02-06 19:20:06 +0200

    designer: added events for QR Barcode question. KP-3059
    Former-commit-id: 68fa149e4a38d6e59b6b903d19b24770dd748eea

---
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Events/Questionnaire/AbstractQuestionAdded.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace WB.Core.BoundedContexts.Designer.Events.Questionnaire
+{
+    public abstract class AbstractQuestionAdded : AbstractQuestionUpdated
+    {
+        public Guid ParentGroupId { get; set; }
+    }
+}
---
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Events/Questionnaire/QRBarcodeQuestionCloned.cs
@@ -0,0 +1,6 @@
+﻿namespace WB.Core.BoundedContexts.Designer.Events.Questionnaire
+{
+    public class QRBarcodeQuestionCloned : AbstractQuestionCloned
+    {
+    }
+}
