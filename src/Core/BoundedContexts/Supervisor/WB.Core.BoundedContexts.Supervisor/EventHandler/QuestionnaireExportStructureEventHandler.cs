5ca18feffa52f70c61cc3914aef8cd8764d99394
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2014-02-11 14:20:20 -0800

    Supervisor\Export: remove weard implementations of IReadSideRepositoryWriter KP-2948
    Former-commit-id: c55e8d7df696ef01e359c22b67993b64617e98fb

--- src/Core/BoundedContexts/Supervisor/WB.Core.BoundedContexts.Supervisor/EventHandler/QuestionnaireExportStructureEventHandler.cs
+++ src/Core/BoundedContexts/Supervisor/WB.Core.BoundedContexts.Supervisor/EventHandler/QuestionnaireExportStructureEventHandler.cs
@@ -7,28 +7,46 @@ using Main.Core.Events.Questionnaire;
 using Ncqrs.Eventing.ServiceModel.Bus;
 using WB.Core.BoundedContexts.Supervisor.Services;
 using WB.Core.BoundedContexts.Supervisor.Views.DataExport;
+using WB.Core.Infrastructure.FunctionalDenormalization;
 using WB.Core.Infrastructure.FunctionalDenormalization.EventHandlers;
 using WB.Core.SharedKernels.DataCollection.ReadSide;
 using WB.Core.SharedKernels.DataCollection.Views.Questionnaire;
 
 namespace WB.Core.BoundedContexts.Supervisor.EventHandler
 {
-    public class QuestionnaireExportStructureEventHandler : AbstractFunctionalEventHandler<QuestionnaireExportStructure>,
-        ICreateHandler<QuestionnaireExportStructure, TemplateImported>
+    public class QuestionnaireExportStructureEventHandler : IEventHandler<TemplateImported>, IEventHandler
     {
-        public QuestionnaireExportStructureEventHandler(IVersionedReadSideRepositoryWriter<QuestionnaireExportStructure> readsideRepositoryWriter)
-            : base(readsideRepositoryWriter)
+        private readonly IVersionedReadSideRepositoryWriter<QuestionnaireExportStructure> readsideRepositoryWriter;
+        private readonly IDataExportService dataExportService;
+        public QuestionnaireExportStructureEventHandler(IVersionedReadSideRepositoryWriter<QuestionnaireExportStructure> readsideRepositoryWriter, IDataExportService dataExportService)
         {
+            this.readsideRepositoryWriter = readsideRepositoryWriter;
+            this.dataExportService = dataExportService;
         }
 
-        public override Type[] UsesViews
+        public string Name
+        {
+            get { return this.GetType().Name; }
+        }
+
+        public Type[] UsesViews
         {
             get { return new Type[0]; }
         }
 
+        public Type[] BuildsViews { get { return new Type[] { typeof(QuestionnaireExportStructure) }; } }
+
         public QuestionnaireExportStructure Create(IPublishedEvent<TemplateImported> evnt)
         {
             return new QuestionnaireExportStructure(evnt.Payload.Source, evnt.EventSequence);
+        }
+
+        public void Handle(IPublishedEvent<TemplateImported> evnt)
+        {
+            var questionnaireExportStructure = new QuestionnaireExportStructure(evnt.Payload.Source, evnt.EventSequence);
+
+            this.dataExportService.CreateExportedDataStructureByTemplate(questionnaireExportStructure);
+            this.readsideRepositoryWriter.Store(questionnaireExportStructure, evnt.EventSourceId.ToString());
         }
     }
 }
