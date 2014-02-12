f51900ba8ab640fbd1e34a5f2781ff7cd699eb05
Anastasya Kholodova <nastya.kholodova@gmail.com>
2014-02-11 19:59:45 -0800

    Supervisor\Export: unit tests for ImportExportController exports methods KP-2948
    Former-commit-id: 32a9e8a221d1857c814665b4c26e3448164a8f4b

---
+++ RavenQuestionnaire.Web/Web.Supervisor.Tests/ImportExportControllerTests/when_GetExportedDataAsync_is_called_with_not_empty_id.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Machine.Specifications;
+using Moq;
+using WB.Core.BoundedContexts.Supervisor.Services;
+using Web.Supervisor.Controllers;
+using It = Machine.Specifications.It;
+
+namespace Web.Supervisor.Tests.ImportExportControllerTests
+{
+    internal class when_GetExportedDataAsync_is_called_with_not_empty_id : ImportExportControllerTestContext
+    {
+        Establish context = () =>
+        {
+            dataExportServiceMock=new Mock<IDataExportService>();
+            questionnaireId = Guid.NewGuid();
+            dataExportServiceMock.Setup(x => x.GetFilePathToExportedCompressedData(questionnaireId, 1)).Returns("hello.txt");
+            controller = CreateImportExportController(dataExportServiceMock.Object);
+        };
+
+        Because of = () => ExecuteAsync(controller, () => controller.GetExportedDataAsync(questionnaireId, 1), () =>
+        {
+            result = controller.GetExportedDataCompleted(controller.AsyncManager.Parameters["result"].ToString()) as FilePathResult;
+        });
+
+        It should_DataExportService_be_called_once = () =>
+            dataExportServiceMock.Verify(x => x.GetFilePathToExportedCompressedData(questionnaireId, 1), Times.Once());
+
+        It should_return_FilePathResult = () =>
+            result.ShouldNotBeNull();
+
+        private static ImportExportController controller;
+        private static Guid questionnaireId;
+        private static Mock<IDataExportService> dataExportServiceMock;
+        private static FilePathResult result;
+    }
+}
