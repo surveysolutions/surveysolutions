41c60393f5ae3410043fdb2c54158c00cc1587fe
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2014-02-11 17:31:36 -0800

    Supervisor\Export: csvwriter and stata generators are implemented as servises KP-2948
    Former-commit-id: 270a6775776cf86d74351008d75ba3f93c770806

--- src/Tests/WB.Core.BoundedContexts.Supervisor.Tests/ServiceTests/DataExport/StataEnvironmentContentGeneratorTests/when_HeaderStructureForLevel_has_level_labels.cs
+++ src/Tests/WB.Core.BoundedContexts.Supervisor.Tests/ServiceTests/DataExport/StataEnvironmentContentGeneratorTests/when_HeaderStructureForLevel_has_level_labels.cs
@@ -18,10 +18,10 @@ namespace WB.Core.BoundedContexts.Supervisor.Tests.ServiceTests.DataExport.Stata
 
             oneQuestionHeaderStructureForLevel.LevelLabels = new[] { CreateLabelItem("c1", "t1"), CreateLabelItem("c2", "t2") };
 
-            stataEnvironmentContentGenerator = CreateStataEnvironmentContentGenerator(oneQuestionHeaderStructureForLevel, dataFileName);
+            stataEnvironmentContentService = CreateStataEnvironmentContentGenerator();
         };
 
-        Because of = () => stataGeneratedContent = stataEnvironmentContentGenerator.ContentOfAdditionalFile;
+        Because of = () => stataGeneratedContent = stataEnvironmentContentService.BuildContentOfAdditionalFile(oneQuestionHeaderStructureForLevel, dataFileName);
 
         It should_contain_stata_script_for_insheet_file = () =>
             stataGeneratedContent.ShouldContain(string.Format("insheet using \"{0}\", comma\r\n", dataFileName));
@@ -32,7 +32,7 @@ namespace WB.Core.BoundedContexts.Supervisor.Tests.ServiceTests.DataExport.Stata
         It should_contain_label_definition_for_id = () =>
             stataGeneratedContent.ShouldContain(string.Format("label define l{0} c1 `\"t1\"' c2 `\"t2\"'", oneQuestionHeaderStructureForLevel.LevelIdColumnName));
 
-        private static StataEnvironmentContentGenerator stataEnvironmentContentGenerator;
+        private static StataEnvironmentContentService stataEnvironmentContentService;
         private static HeaderStructureForLevel oneQuestionHeaderStructureForLevel;
         private static string dataFileName = "data file name";
 
