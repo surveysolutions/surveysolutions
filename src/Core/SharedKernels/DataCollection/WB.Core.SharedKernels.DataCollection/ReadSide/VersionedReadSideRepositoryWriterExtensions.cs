400fa111e56ed41b589e9cf361bc76ae5ab9fa03
Slava Scherbak <slava.scherbak@gmail.com>
2014-02-12 16:45:04 +0200

    ReadSide: changed guid format and renamed extension classes
    Former-commit-id: 7c14a0c81059071c96d3216ce12f8d65ee1f027a

--- src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/ReadSide/VersionedReadSideRepositoryWriterExtensions.cs
+++ src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/ReadSide/VersionedReadSideRepositoryWriterExtensions.cs
@@ -1,17 +1,19 @@
 ﻿using System;
+using Main.Core.Utility;
+using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
 
 namespace WB.Core.SharedKernels.DataCollection.ReadSide
 {
-    public static class VersionedReadSideRepositoryWriterExtension
+    public static class VersionedReadSideRepositoryWriterExtensions
     {
         public static T GetById<T>(this IVersionedReadSideRepositoryWriter<T> writer, Guid id, long version) where T : class, IVersionedView
         {
-            return writer.GetById(id.ToString(), version);
+            return writer.GetById(id.FormatGuid(), version);
         }
 
         public static void Remove<T>(this IVersionedReadSideRepositoryWriter<T> writer, Guid id, long version) where T : class, IVersionedView
         {
-            writer.Remove(id.ToString(), version);
+            writer.Remove(id.FormatGuid(), version);
         }
     }
 }
