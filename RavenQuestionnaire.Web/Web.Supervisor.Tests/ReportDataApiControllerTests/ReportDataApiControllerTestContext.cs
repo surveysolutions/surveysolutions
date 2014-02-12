cba4141cd5d4c9b9df680f45b30799a1013eac2c
Slava Scherbak <slava.scherbak@gmail.com>
2014-02-12 12:36:17 +0200

    HQ: added test for ReportDataApiController's MapReport action KP-3065
    --HG--
    branch : map-report
    
    
    Former-commit-id: 0a700e9693eb40211d0b893da8f36be02a7ab337

---
+++ RavenQuestionnaire.Web/Web.Supervisor.Tests/ReportDataApiControllerTests/ReportDataApiControllerTestContext.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Supervisor.Views.Reposts.InputModels;
+using Core.Supervisor.Views.Reposts.Views;
+using Main.Core.View;
+using Moq;
+using Ncqrs.Commanding.ServiceModel;
+using Questionnaire.Core.Web.Helpers;
+using WB.Core.GenericSubdomains.Logging;
+using Web.Supervisor.Controllers;
+
+namespace Web.Supervisor.Tests.ReportDataApiControllerTests
+{
+    internal class ReportDataApiControllerTestContext
+    {
+        protected static ReportDataApiController CreateReportDataApiController(
+            ICommandService commandService = null,
+            IGlobalInfoProvider provider = null,
+            ILogger logger = null,
+            IViewFactory<HeadquarterSurveysAndStatusesReportInputModel, HeadquarterSurveysAndStatusesReportView>
+                headquarterSurveysAndStatusesReport = null,
+            IViewFactory<HeadquarterSupervisorsAndStatusesReportInputModel, HeadquarterSupervisorsAndStatusesReportView>
+                headquarterSupervisorsAndStatusesReport = null,
+            IViewFactory<SupervisorTeamMembersAndStatusesReportInputModel, SupervisorTeamMembersAndStatusesReportView>
+                supervisorTeamMembersAndStatusesReport = null,
+            IViewFactory<SupervisorSurveysAndStatusesReportInputModel, SupervisorSurveysAndStatusesReportView>
+                supervisorSurveysAndStatusesReport = null,
+            IViewFactory<MapReportInputModel, MapReportView> mapReport = null)
+        {
+            return new ReportDataApiController(
+                commandService ?? Mock.Of<ICommandService>(),
+                provider ?? Mock.Of<IGlobalInfoProvider>(),
+                logger ?? Mock.Of<ILogger>(),
+                headquarterSurveysAndStatusesReport ??
+                    Mock.Of<IViewFactory<HeadquarterSurveysAndStatusesReportInputModel, HeadquarterSurveysAndStatusesReportView>>(),
+                headquarterSupervisorsAndStatusesReport ??
+                    Mock.Of<IViewFactory<HeadquarterSupervisorsAndStatusesReportInputModel, HeadquarterSupervisorsAndStatusesReportView>>(),
+                supervisorTeamMembersAndStatusesReport ??
+                    Mock.Of<IViewFactory<SupervisorTeamMembersAndStatusesReportInputModel, SupervisorTeamMembersAndStatusesReportView>>(),
+                supervisorSurveysAndStatusesReport ??
+                    Mock.Of<IViewFactory<SupervisorSurveysAndStatusesReportInputModel, SupervisorSurveysAndStatusesReportView>>(),
+                mapReport ?? Mock.Of<IViewFactory<MapReportInputModel, MapReportView>>());
+        }
+    }
+}
---
+++ RavenQuestionnaire.Web/Web.Supervisor.Tests/ReportDataApiControllerTests/when_map_report_data_is_being_queried.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Supervisor.Views.Reposts.InputModels;
+using Core.Supervisor.Views.Reposts.Views;
+using Machine.Specifications;
+using Main.Core.View;
+using Moq;
+using Web.Supervisor.Controllers;
+using It = Machine.Specifications.It;
+
+namespace Web.Supervisor.Tests.ReportDataApiControllerTests
+{
+    internal class when_map_report_data_is_being_queried : ReportDataApiControllerTestContext
+    {
+        Establish context = () =>
+        {
+            mapReportFactoryMock = new Mock<IViewFactory<MapReportInputModel, MapReportView>>();
+            controller = CreateReportDataApiController(mapReport: mapReportFactoryMock.Object);
+        };
+
+        Because of = () =>
+            controller.MapReport(input);
+
+        It should_load_data_from_factory_once = () =>
+            mapReportFactoryMock.Verify(x => x.Load(input), Times.Once());
+
+        private static MapReportInputModel input;
+        private static ReportDataApiController controller;
+        private static Mock<IViewFactory<MapReportInputModel, MapReportView>> mapReportFactoryMock;
+
+    }
+}
