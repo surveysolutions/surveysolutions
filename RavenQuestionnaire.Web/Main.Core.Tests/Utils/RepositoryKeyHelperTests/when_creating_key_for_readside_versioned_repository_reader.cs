08cdf875fe677ad2be2f24dc0c911a91817d9969
Slava Scherbak <slava.scherbak@gmail.com>
2014-02-13 13:30:14 +0200

    ReadSide: added test for RepositoryKey helper
    Former-commit-id: c5e2ba0d63fb4ce8ed8d60b92082256e583ee719

---
+++ RavenQuestionnaire.Web/Main.Core.Tests/Utils/RepositoryKeyHelperTests/when_creating_key_for_readside_versioned_repository_reader.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Machine.Specifications;
+using Main.Core.Utility;
+
+namespace Main.Core.Tests.Utils
+{
+    public class when_creating_key_for_readside_versioned_repository_reader
+    {
+        Establish context = () => {};
+
+        Because of = () => 
+            key = RepositoryKeysHelper.GetVersionedKey("11111111111111111111111111111111", 1);
+
+        private It should_create_key_11111111111111111111111111111111_1 = () =>
+            key.ShouldEqual("11111111111111111111111111111111-1");
+
+        private static string key;
+    }
+}
