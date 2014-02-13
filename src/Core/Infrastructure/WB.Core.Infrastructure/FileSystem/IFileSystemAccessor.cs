a19267c680c65bb5a975389ae2d9d5d4c0fd091f
Anastasya Kholodova <nastya.kholodova@gmail.com>
2014-02-12 17:07:12 -0800

    Supervisor\Export: new abstract level for file system usages is added KP-2948
    Former-commit-id: 2b08c5623e5a79bc5f24d2ee88e5ccc12545bffc

---
+++ src/Core/Infrastructure/WB.Core.Infrastructure/FileSystem/IFileSystemAccessor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WB.Core.Infrastructure.FileSystem
+{
+    public interface IFileSystemAccessor
+    {
+        string CombinePath(params string[] pathParts);
+        string GetFileName(string fileName);
+        bool IsDirectoryExists(string pathToDirectory);
+        void CreateDirectory(string path);
+        void DeleteDirectory(string path);
+
+        bool IsFileExists(string pathToFile);
+        void DeleteFile(string pathToFile);
+
+        string MakeValidFileName(string name);
+
+        string[] GetDirectoriesInDirectory(string pathToDirectory);
+        string[] GetFilesInDirectory(string pathToDirectory);
+
+        void WriteAllText(string pathToFile, string content);
+    }
+}
