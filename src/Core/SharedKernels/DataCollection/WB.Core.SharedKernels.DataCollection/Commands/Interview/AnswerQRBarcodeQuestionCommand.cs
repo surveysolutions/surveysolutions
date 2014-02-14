74a873d40b8177f8eb42ea490593358e33600374
Roman Lytvyn <litvin.roma@gmail.com>
2014-02-14 19:16:44 +0200

    supervisor: added QRBarcodeQuestionAnswered event, AnswerQRBarcodeQuestionCommand command to shared kernel. KP-3061
    Former-commit-id: 30a0cd22fe4ffafca8131d2eb80705dc65c7666c

---
+++ src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/Commands/Interview/AnswerQRBarcodeQuestionCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Ncqrs.Commanding.CommandExecution.Mapping.Attributes;
+using WB.Core.SharedKernels.DataCollection.Commands.Interview.Base;
+
+namespace WB.Core.SharedKernels.DataCollection.Commands.Interview
+{
+    [MapsToAggregateRootMethod(typeof(Implementation.Aggregates.Interview), "AnswerQRBarcodeQuestion")]
+    public class AnswerQRBarcodeQuestionCommand : AnswerQuestionCommand
+    {
+        public string Answer { get; private set; }
+
+        public AnswerQRBarcodeQuestionCommand(Guid interviewId, Guid userId, Guid questionId, decimal[] rosterVector, DateTime answerTime, string answer)
+            : base(interviewId, userId, questionId, rosterVector, answerTime)
+        {
+            this.Answer = answer;
+        }
+    }
+}
