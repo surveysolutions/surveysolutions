a7b478984614a147ae6fd9797cc8b455d74e3a0f
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2014-02-14 09:16:01 -0500

    Supervisor: Api model extending and routing update [KP-3080]
    Former-commit-id: 080ba9c54e9a2e0780dede9ea88a48994f39db5b

---
+++ RavenQuestionnaire.Web/Web.Supervisor/Models/API/InterviewApiDetails.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.Serialization;
+using Core.Supervisor.Views.Interview;
+
+namespace Web.Supervisor.Models.API
+{
+    public class InterviewApiDetails
+    {
+        public InterviewApiDetails(InterviewDetailsView interview)
+        {
+            this.Interview = interview;
+        }
+
+        //should be changed to close internal information
+        [DataMember]
+        public InterviewDetailsView Interview { get; set; }
+    }
+}
