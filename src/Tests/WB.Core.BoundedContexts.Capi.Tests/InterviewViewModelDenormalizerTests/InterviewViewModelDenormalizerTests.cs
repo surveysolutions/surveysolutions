3f078bd969836d625b4a0beb8871753fdf62674d
Roman Lytvyn <litvin.roma@gmail.com>
2014-02-17 15:06:26 +0200

    capi: added tests for changes by InterviewViewModelDenormalizer. KP-3069
    Former-commit-id: 0a7e7fed1a78b57dd335e7f34875673e1f0178ca

--- src/Tests/WB.Core.BoundedContexts.Capi.Tests/InterviewViewModelDenormalizerTests/InterviewViewModelDenormalizerTests.cs
+++ src/Tests/WB.Core.BoundedContexts.Capi.Tests/InterviewViewModelDenormalizerTests/InterviewViewModelDenormalizerTests.cs
@@ -1,84 +1,19 @@
 ﻿using System;
 using System.Linq;
-using Main.Core.Documents;
 using Main.Core.Entities.SubEntities;
 using Main.Core.Entities.SubEntities.Question;
-using Microsoft.Practices.ServiceLocation;
 using Moq;
 using NUnit.Framework;
-using Ncqrs.Eventing;
-using Ncqrs.Eventing.ServiceModel.Bus;
-using WB.Core.BoundedContexts.Capi.EventHandler;
 using WB.Core.BoundedContexts.Capi.Views.InterviewDetails;
-using WB.Core.BoundedContexts.Supervisor.Factories;
-using WB.Core.BoundedContexts.Supervisor.Implementation.Factories;
 using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
 using WB.Core.SharedKernels.DataCollection.Events.Interview;
-using WB.Core.SharedKernels.DataCollection.ReadSide;
 using WB.Core.SharedKernels.DataCollection.ValueObjects.Interview;
 using WB.Core.SharedKernels.DataCollection.Views.Questionnaire;
 
 namespace WB.Core.BoundedContexts.Capi.Tests.InterviewViewModelDenormalizerTests
 {
-    internal class InterviewViewModelDenormalizerTests
+    internal class InterviewViewModelDenormalizerTests : InterviewViewModelDenormalizerTestsContext
     {
-        private static QuestionnaireDocument CreateQuestionnaireDocument(Guid questionnaireId)
-        {
-            ServiceLocator.SetLocatorProvider(() => new Mock<IServiceLocator> { DefaultValue = DefaultValue.Mock }.Object);
-            var innerDocument = new QuestionnaireDocument
-            {
-                Title = string.Format("Questionnaire {0}", questionnaireId),
-                PublicKey = questionnaireId
-            };
-            return innerDocument;
-        }
-
-        private static InterviewViewModelDenormalizer CreateInterviewViewModelDenormalizer(
-            Mock<IReadSideRepositoryWriter<InterviewViewModel>> storageStub,
-            Mock<IVersionedReadSideRepositoryWriter<QuestionnaireDocumentVersioned>> versionedStorageStub)
-        {
-            var denormalizer = new InterviewViewModelDenormalizer(storageStub.Object,
-                versionedStorageStub.Object,
-                Mock.Of<IVersionedReadSideRepositoryWriter<QuestionnaireRosterStructure>>(), new QuestionnaireRosterStructureFactory());
-
-            return denormalizer;
-        }
-
-        private static Mock<IReadSideRepositoryWriter<InterviewViewModel>> CreateInterviewViewModelDenormalizerStorageStub(
-            InterviewViewModel document)
-        {
-            var storageStub = new Mock<IReadSideRepositoryWriter<InterviewViewModel>>();
-
-            storageStub.Setup(d => d.GetById(document.PublicKey.FormatGuid())).Returns(document);
-
-            return storageStub;
-        }
-
-        private static Mock<IVersionedReadSideRepositoryWriter<QuestionnaireDocumentVersioned>> CreateQuestionnaireDocumentVersionedStorageStub(
-            QuestionnaireDocument document)
-        {
-            var questionnaireStorageMock = new Mock<IVersionedReadSideRepositoryWriter<QuestionnaireDocumentVersioned>>();
-            questionnaireStorageMock.Setup(x => x.GetById(Moq.It.IsAny<string>(), Moq.It.IsAny<long>()))
-                .Returns(new QuestionnaireDocumentVersioned()
-                {
-                    Questionnaire = document
-                });
-            return questionnaireStorageMock;
-        }
-
-        private static IPublishedEvent<T> CreatePublishedEvent<T>(Guid questionnaireId, T evnt)
-        {
-            IPublishedEvent<T> e = new PublishedEvent<T>(new UncommittedEvent(Guid.NewGuid(),
-                                                                              questionnaireId,
-                                                                              1,
-                                                                              1,
-                                                                              DateTime.Now,
-                                                                              evnt,
-                                                                              new Version(1, 0))
-                );
-            return e;
-        }
-
         [Test]
         public void HandleInterviewForTestingCreated_When_InterviewForTestingCreated_event_is_come_Then_ViewModel_Is_Stored()
         {
