eae265cc1611bc6841f18b8f7204e1f2591b4787
Slava Scherbak <slava.scherbak@gmail.com>
2014-02-19 14:56:09 +0200

    Verifier: fixed bug with roster that referencing it's child question KP-3093
    Former-commit-id: 1a116b2a6a9fc333753d6eff828073122cb4bd54

--- src/Tests/WB.Core.SharedKernels.QuestionnaireVerification.Tests/CustomEnablementConditions/when_verifying_questionnaire_with_group_that_has_custom_condition_referencing_question_with_deeper_roster_level.cs
+++ src/Tests/WB.Core.SharedKernels.QuestionnaireVerification.Tests/CustomEnablementConditions/when_verifying_questionnaire_with_group_that_has_custom_condition_referencing_question_with_deeper_roster_level.cs
@@ -3,33 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using Machine.Specifications;
 using Main.Core.Documents;
-using Main.Core.Entities.SubEntities;
-using Main.Core.Entities.SubEntities.Question;
 using Moq;
 using WB.Core.SharedKernels.ExpressionProcessor.Services;
 using WB.Core.SharedKernels.QuestionnaireVerification.Implementation.Services;
+using WB.Core.SharedKernels.QuestionnaireVerification.Tests.QuestionnaireVerifierTests;
 using WB.Core.SharedKernels.QuestionnaireVerification.ValueObjects;
 using It = Machine.Specifications.It;
 
-namespace WB.Core.SharedKernels.QuestionnaireVerification.Tests.QuestionnaireVerifierTests
+namespace WB.Core.SharedKernels.QuestionnaireVerification.Tests.CustomEnablementConditions
 {
-    class when_verifying_questionnaire_with_question_that_has_custom_condition_referencing_question_with_deeper_roster_level : QuestionnaireVerifierTestsContext
+    internal class when_verifying_questionnaire_with_group_that_has_custom_condition_referencing_question_with_deeper_roster_level : QuestionnaireVerifierTestsContext
     {
-        private Establish context = () =>
+        Establish context = () =>
         {
-            questionnaire = CreateQuestionnaireWithRosterAndQuestionAfterWithConditionReferencingQuestionInRoster(
+            questionnaire = CreateQuestionnaireWithRosterAndGroupAfterWithConditionReferencingQuestionInRoster(
                 underDeeperRosterLevelQuestionId,
-                questionWithCustomCondition);
+                groupWithCustomCondition);
 
             var expressionProcessor = new Mock<IExpressionProcessor>();
 
-            expressionProcessor
-                .Setup(x => x.IsSyntaxValid(Moq.It.IsAny<string>()))
-                .Returns(true);
+            expressionProcessor.Setup(x => x.IsSyntaxValid(Moq.It.IsAny<string>())).Returns(true);
 
-            expressionProcessor
-                .Setup(x => x.GetIdentifiersUsedInExpression(Moq.It.IsAny<string>()))
-                .Returns(new[] { underDeeperRosterLevelQuestionId.ToString() });
+            expressionProcessor.Setup(x => x.GetIdentifiersUsedInExpression(Moq.It.IsAny<string>()))
+                .Returns(new string[] { underDeeperRosterLevelQuestionId.ToString() });
 
             verifier = CreateQuestionnaireVerifier(expressionProcessor.Object);
         };
@@ -46,11 +42,11 @@ namespace WB.Core.SharedKernels.QuestionnaireVerification.Tests.QuestionnaireVer
         It should_return_error_with_two_references = () =>
             resultErrors.Single().References.Count().ShouldEqual(2);
 
-        It should_return_first_error_reference_with_type_Question = () =>
-            resultErrors.Single().References.First().Type.ShouldEqual(QuestionnaireVerificationReferenceType.Question);
+        It should_return_first_error_reference_with_type_Group = () =>
+            resultErrors.Single().References.First().Type.ShouldEqual(QuestionnaireVerificationReferenceType.Group);
 
-        It should_return_first_error_reference_with_id_of_questionWithCustomCondition = () =>
-            resultErrors.Single().References.First().Id.ShouldEqual(questionWithCustomCondition);
+        It should_return_first_error_reference_with_id_of_groupWithCustomCondition = () =>
+            resultErrors.Single().References.First().Id.ShouldEqual(groupWithCustomCondition);
 
         It should_return_last_error_reference_with_type_Question = () =>
             resultErrors.Single().References.Last().Type.ShouldEqual(QuestionnaireVerificationReferenceType.Question);
@@ -62,7 +58,7 @@ namespace WB.Core.SharedKernels.QuestionnaireVerification.Tests.QuestionnaireVer
         private static QuestionnaireVerifier verifier;
         private static QuestionnaireDocument questionnaire;
 
-        private static readonly Guid questionWithCustomCondition = Guid.Parse("10000000000000000000000000000000");
-        private static readonly Guid underDeeperRosterLevelQuestionId = Guid.Parse("12222222222222222222222222222222");
+        private static Guid groupWithCustomCondition = Guid.Parse("10000000000000000000000000000000");
+        private static Guid underDeeperRosterLevelQuestionId = Guid.Parse("12222222222222222222222222222222");
     }
 }
