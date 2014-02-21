f8044df4e9cc10f241593cee5c6dd5bb5a9bdbd7
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2014-02-21 18:53:28 -0500

    CAPI: Tests for TabletInformationSender KP-3053
    Former-commit-id: 5e2551a0986f57e6ebf20ba889e28614a69002ec

---
+++ src/Tests/WB.Core.GenericSubdomains.ErrorReporting.Tests/AssemblyContext.cs
@@ -0,0 +1,16 @@
+﻿using Machine.Specifications;
+using Microsoft.Practices.ServiceLocation;
+using Moq;
+
+namespace WB.Core.GenericSubdomains.ErrorReporting.Tests
+{
+    public class AssemblyContext : IAssemblyContext
+    {
+        public void OnAssemblyStart()
+        {
+            ServiceLocator.SetLocatorProvider(() => new Mock<IServiceLocator> { DefaultValue = DefaultValue.Mock }.Object);
+        }
+
+        public void OnAssemblyComplete() {}
+    }
+}
---
+++ src/Tests/WB.Core.GenericSubdomains.ErrorReporting.Tests/TabletInformationSenderTests/TabletInformationSenderTestContext.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Moq;
+using WB.Core.GenericSubdomain.Rest;
+using WB.Core.GenericSubdomains.ErrorReporting.Implementation.TabletInformation;
+using WB.Core.GenericSubdomains.ErrorReporting.Services.CapiInformationService;
+using WB.Core.Infrastructure.FileSystem;
+using WB.Core.SharedKernel.Utils.Serialization;
+
+namespace WB.Core.GenericSubdomains.ErrorReporting.Tests.TabletInformationSenderTests
+{
+    internal class TabletInformationSenderTestContext
+    {
+        protected static TabletInformationSender CreateTabletInformationSender(bool isNetworkEnabled = true, string pathToInfoArchive = "", bool isSentSuccessfully = false)
+        {
+            return new TabletInformationSender(
+                Mock.Of<ICapiInformationService>(_ => _.CreateInformationPackage() == pathToInfoArchive),
+                Mock.Of<INetworkService>(_ => _.IsNetworkEnabled() == isNetworkEnabled),
+                Mock.Of<IFileSystemAccessor>(_ =>
+                    _.IsFileExists(It.IsAny<string>()) == true
+                        && _.ReadAllBytes(It.IsAny<string>()) == new byte[0]),
+                Mock.Of<IJsonUtils>(), string.Empty, string.Empty, string.Empty,
+                Mock.Of<IRestServiceWrapperFactory>(_ => _.CreateRestServiceWrapper(
+                    Moq.It.IsAny<string>()) ==
+                    Mock.Of<IRestServiceWrapper>(r_ => r_.ExecuteRestRequestAsync<bool>(It.IsAny<string>(), It.IsAny<CancellationToken>(),
+                        It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()) == isSentSuccessfully)));
+        }
+
+        protected static bool WaitUntilOperationEndsReturnFalseIfCanceled(TabletInformationSender tabletInformationSender, Action<TabletInformationSender> action)
+        {
+            var result = false;
+            var remoteCommandDoneEvent = new AutoResetEvent(false);
+            tabletInformationSender.ProcessCanceled += (s, e) =>
+            {
+                result = false;
+                remoteCommandDoneEvent.Set();
+            };
+            tabletInformationSender.ProcessFinished += (s, e) =>
+            {
+                result = true;
+                remoteCommandDoneEvent.Set();
+            };
+            action(tabletInformationSender);
+            remoteCommandDoneEvent.WaitOne();
+            return result;
+        }
+    }
+}
