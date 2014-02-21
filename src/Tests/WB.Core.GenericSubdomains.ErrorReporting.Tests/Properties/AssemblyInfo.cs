04095b4b925524ef5f6f96fb2679eb07bcb169f3
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2014-02-21 17:57:11 -0500

    CAPI: unit test for CapiInformationService KP-3052
    Former-commit-id: af763ed4145bf467dbecc8fb3b8927c1ebe9f5b9

--- src/Tests/WB.Core.GenericSubdomains.ErrorReporting.Tests/Properties/AssemblyInfo.cs
+++ src/Tests/WB.Core.GenericSubdomains.ErrorReporting.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WB.Core.GenericSubdomains.ErrorReporting")]
+[assembly: AssemblyTitle("WB.Core.GenericSubdomains.ErrorReporting.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WB.Core.GenericSubdomains.ErrorReporting")]
+[assembly: AssemblyProduct("WB.Core.GenericSubdomains.ErrorReporting.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("694c56b7-5275-48b1-ad13-1ce996057dd4")]
+[assembly: Guid("de559d04-7a37-4ce5-ac0b-74043eeb7022")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -34,4 +34,3 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: InternalsVisibleTo("WB.Core.GenericSubdomains.ErrorReporting.Tests")]
