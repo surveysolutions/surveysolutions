896eb62ddc45e5fbf7e558b37cd4e58ca53d135b
Andrii Kozhyn <hedgic@gmail.com>
2014-03-04 15:48:53 +0200

    Headquarters: removed old solution. Implemented demo page KP-3161
    Former-commit-id: 87b6ae1b3eef8f0ab531037abe310023252f116a

---
+++ src/UI/Headquarters/WB.UI.Headquarters/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace WB.UI.Headquarters.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
