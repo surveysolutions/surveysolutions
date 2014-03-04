b66cd3ff59f11c3d32c85497c6e2f95643195e3b
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2014-03-04 13:16:26 +0200

    Headquarters: updated bootstrap package. Fixed solution build. Added missing projects KP-3161
    Former-commit-id: 915325759f0c227bdf454b32ff622cf430a0e144

--- src/UI/Headquarters/WB.UI.Headquarters/Global.asax.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/Global.asax.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Http;
+﻿using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Routing;
+using NConfig;
 
 namespace WB.UI.Headquarters
 {
-    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
-    // visit http://go.microsoft.com/?LinkId=9394801
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
@@ -19,6 +14,13 @@ namespace WB.UI.Headquarters
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            SetupNConfig();
+        }
+
+        private static void SetupNConfig()
+        {
+            NConfigurator.UsingFiles(@"~\Configuration\Headquarters.Web.config").SetAsSystemDefault();
         }
     }
 }
