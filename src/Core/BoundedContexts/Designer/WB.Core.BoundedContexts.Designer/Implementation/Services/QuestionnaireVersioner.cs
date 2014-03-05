a2101b04cdd7598e5a60bc766fa428ad37715dc5
Slava Scherbak <slava.scherbak@gmail.com>
2014-03-05 16:01:48 +0200

    QuestionnaireVersion: added logic for version 1.6.1 and 1.6.2 calculation with tests KP-3192
    Former-commit-id: b89fb535333855ac97c1aabeedabef4c52268058

--- src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Implementation/Services/QuestionnaireVersioner.cs
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Implementation/Services/QuestionnaireVersioner.cs
@@ -1,4 +1,9 @@
+using System.Collections.Generic;
+using System.Linq;
 using Main.Core.Documents;
+using Main.Core.Entities.Composite;
+using Main.Core.Entities.SubEntities;
+using Main.Core.Entities.SubEntities.Question;
 using WB.Core.BoundedContexts.Designer.Services;
 using WB.Core.SharedKernels.QuestionnaireVerification.ValueObjects;
 
@@ -6,9 +11,45 @@ namespace WB.Core.BoundedContexts.Designer.Implementation.Services
 {
     internal class QuestionnaireVersioner : IQuestionnaireVersioner
     {
+        private static readonly QuestionnaireVersion version_1_6_1 = new QuestionnaireVersion(1, 6, 1);
+        private static readonly QuestionnaireVersion version_1_6_2 = new QuestionnaireVersion(1, 6, 2);
+
         public QuestionnaireVersion GetVersion(QuestionnaireDocument questionnaire)
         {
-            return new QuestionnaireVersion(1, 6, 0);
+            var version = new QuestionnaireVersion(1, 6, 0);
+            questionnaire.ConnectChildrenWithParent();
+
+            int qrBarcodeQuestionCount = GetQrBarcodeQuestionsCount(questionnaire);
+            if (qrBarcodeQuestionCount > 0 && version < version_1_6_1)
+                version = version_1_6_1;
+
+            int nestedRostersCount = GetNestedRostersCount(questionnaire);
+            if (nestedRostersCount > 0 && version < version_1_6_2) 
+                version = version_1_6_2;
+
+            return version;
+        }
+
+        private int GetNestedRostersCount(QuestionnaireDocument questionnaire)
+        {
+            return questionnaire.Find<IGroup>(x => x.IsRoster).Count(x => GetAllParentGroups(questionnaire, x).Count(y => y.IsRoster) > 0);
+        }
+
+        private IEnumerable<IGroup> GetAllParentGroups(QuestionnaireDocument questionnaire, IComposite entity)
+        {
+            var currentParent = (IGroup)entity.GetParent();
+
+            while (currentParent != null)
+            {
+                yield return currentParent;
+
+                currentParent = (IGroup)currentParent.GetParent();
+            }
+        }
+
+        private static int GetQrBarcodeQuestionsCount(QuestionnaireDocument questionnaire)
+        {
+            return questionnaire.Find<QRBarcodeQuestion>(x => true).Count();
         }
     }
 }
---
+++ src/Tests/WB.Core.BoundedContexts.Designer.Tests/QuestionnaireVersionerTests/QuestionnaireVersionerTestContext.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Main.Core.Documents;
+using Main.Core.Entities.Composite;
+using Main.Core.Entities.SubEntities;
+using Main.Core.Entities.SubEntities.Question;
+using WB.Core.BoundedContexts.Designer.Implementation.Services;
+
+namespace WB.Core.BoundedContexts.Designer.Tests.QuestionnaireVersionerTests
+{
+    internal class QuestionnaireVersionerTestContext
+    {
+        protected static QuestionnaireVersioner CreateQuestionnaireVersioner()
+        {
+            return new QuestionnaireVersioner();
+        }
+
+        protected static QuestionnaireDocument CreateQuestionnaireDocument()
+        {
+            return new QuestionnaireDocument
+            {
+                Children = new List<IComposite>
+                {
+                    new TextQuestion() {QuestionType = QuestionType.Text}
+                }
+            }; 
+        }
+
+        protected static QuestionnaireDocument CreateQuestionnaireDocumentWithQRQuestion()
+        {
+            return new QuestionnaireDocument
+            {
+                Children = new List<IComposite>
+                {
+                    new QRBarcodeQuestion(){ QuestionType = QuestionType.QRBarcode },
+                    new TextQuestion() {QuestionType = QuestionType.Text}
+                }
+            };
+        }
+
+        protected static QuestionnaireDocument CreateQuestionnaireDocumentWithNestedRosters()
+        {
+            return new QuestionnaireDocument
+            {
+                Children = new List<IComposite>
+                {
+                    new Group
+                    {
+                        IsRoster = true,
+                        Children = new List<IComposite>
+                        {
+                            new QRBarcodeQuestion() { QuestionType = QuestionType.QRBarcode },
+                            new Group { IsRoster = true }
+                        }
+                    }
+
+                }
+            };
+        }
+    }
+}
