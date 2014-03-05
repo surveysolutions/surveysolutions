97c9a2384eb80b643a6de5990f00aca76ee35ab3
Slava Scherbak <slava.scherbak@gmail.com>
2014-03-05 16:55:34 +0200

    QuestionnaireVersion: added test for version provider for supervisor app KP-3164
    --HG--
    branch : hotfixes-for-v-1-6-2
    
    
    Former-commit-id: 18e13657b763324202cfb14cdc9b4a08e7e99655

---
+++ src/Tests/WB.Core.BoundedContexts.Supervisor.Tests/SupportedVersionProviderTests/when_getting_supported_questionnaire_version_for_supervisor_app.cs
@@ -0,0 +1,30 @@
+using Machine.Specifications;
+using WB.Core.BoundedContexts.Supervisor.Implementation.Services;
+using WB.Core.SharedKernels.QuestionnaireVerification.ValueObjects;
+
+namespace WB.Core.BoundedContexts.Supervisor.Tests.SupportedVersionProviderTests
+{
+    internal class when_getting_supported_questionnaire_version_for_supervisor_app : SupportedVersionProviderTestContext
+    {
+        Establish context = () =>
+        {
+            versionProvider = CreateSupportedVersionProvider();
+        };
+
+        Because of = () =>
+            supportedVersion = versionProvider.GetMaximalQuestionnaireVersion();
+
+        It should_set_Major_property_to_1 = () =>
+            supportedVersion.Major.ShouldEqual(1);
+
+        It should_set_Minor_property_to_6 = () =>
+            supportedVersion.Minor.ShouldEqual(6);
+
+        It should_set_Patch_property_to_0 = () =>
+            supportedVersion.Patch.ShouldEqual(0);
+
+        private static QuestionnaireVersion supportedVersion;
+        private static SupportedVersionProvider versionProvider;
+
+    }
+}
