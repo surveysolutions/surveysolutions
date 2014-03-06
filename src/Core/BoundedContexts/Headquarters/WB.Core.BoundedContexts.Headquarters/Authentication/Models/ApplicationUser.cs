89bfa8b6f5125b707b574e2124a896960df65a69
Andrii Kozhyn <hedgic@gmail.com>
2014-03-06 13:53:05 +0200

    Headquarters: Added identity package with storage configured for it KP-3165
    Former-commit-id: 7c9d7c1c2597147116b0433d7ac0e4b84efe5c2e

---
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Authentication/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using AspNet.Identity.RavenDB.Entities;
+
+namespace WB.Core.BoundedContexts.Headquarters.Authentication.Models
+{
+    public class ApplicationUser : RavenUser
+    {
+         
+    }
+}
---
+++ src/UI/Headquarters/WB.UI.Headquarters/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+using Raven.Client;
+using Raven.Client.Document;
+using Raven.Client.Extensions;
+
+namespace WB.UI.Headquarters.Controllers
+{
+    public class AccountController : Controller
+    {
+         
+    }
+}
