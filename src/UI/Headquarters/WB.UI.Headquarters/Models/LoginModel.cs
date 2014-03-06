685b23932ea1a31a371b010bb76d1fce0f02f9b5
Andrii Kozhyn <hedgic@gmail.com>
2014-03-06 16:59:38 +0200

    Headquarters: Added login form KP-3165
    Former-commit-id: 534eafabd99a6ff6290c0a48e8422c0538b35ca9

---
+++ src/UI/Headquarters/WB.UI.Headquarters/Models/LoginModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WB.UI.Headquarters.Models
+{
+    public class LoginModel
+    {
+        [Required]
+        public string Login { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+}
