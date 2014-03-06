094983e3cb06d0be4102ba87d143fd96565a2845
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2014-03-06 16:59:38 +0200

    Headquarters: Added login form KP-3165
    Former-commit-id: 534eafabd99a6ff6290c0a48e8422c0538b35ca9

--- src/UI/Headquarters/WB.UI.Headquarters/Controllers/AccountController.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/Controllers/AccountController.cs
@@ -1,12 +1,37 @@
 ﻿using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
 using Raven.Client;
 using Raven.Client.Document;
 using Raven.Client.Extensions;
+using WB.Core.BoundedContexts.Headquarters.Authentication.Models;
+using WB.UI.Headquarters.Models;
 
 namespace WB.UI.Headquarters.Controllers
 {
+    [AllowAnonymous]
     public class AccountController : Controller
     {
-         
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        public ActionResult Login()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                
+            }
+
+            return this.View(model);
+        }
     }
 }
