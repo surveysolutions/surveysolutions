81f22a86fa1d0c940d7415db9764ce961ae7d282
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2014-03-06 15:59:46 -0500

    Designer: invariant for parallel roster structure of nested roster KP-3171
    --HG--
    branch : nested-rosters
    
    
    Former-commit-id: b0c2a681828ab16c08c69b4a0986be14d0f118fb

---
+++ src/Tests/WB.Core.BoundedContexts.Designer.Tests/QuestionnaireTests/when_adding_roster_group_and_roster_size_question_is_paralell_roster_scope.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Machine.Specifications;
+using Main.Core.Entities.SubEntities;
+using Main.Core.Events.Questionnaire;
+using WB.Core.BoundedContexts.Designer.Aggregates;
+using WB.Core.BoundedContexts.Designer.Events.Questionnaire;
+using WB.Core.BoundedContexts.Designer.Exceptions;
+
+namespace WB.Core.BoundedContexts.Designer.Tests.QuestionnaireTests
+{
+    internal class when_adding_roster_group_and_roster_size_question_is_paralell_roster_scope : QuestionnaireTestsContext
+    {
+        Establish context = () =>
+        {
+            responsibleId = Guid.Parse("DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD");
+            chapterId = Guid.Parse("CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC");
+            var anotherRosterId = Guid.Parse("EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE");
+            parallelRosterId = Guid.Parse("21111111111111111111111111111111");
+            groupId = Guid.Parse("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+            rosterSizeQuestionId = Guid.Parse("11111111111111111111111111111111");
+
+            questionnaire = CreateQuestionnaire(responsibleId: responsibleId);
+            questionnaire.Apply(new NewGroupAdded { PublicKey = chapterId });
+
+            questionnaire.Apply(new NewGroupAdded { PublicKey = anotherRosterId, ParentGroupPublicKey = chapterId });
+            questionnaire.Apply(new GroupBecameARoster(responsibleId, anotherRosterId));
+            questionnaire.Apply(new NumericQuestionAdded { PublicKey = rosterSizeQuestionId, IsInteger = true, GroupPublicKey = anotherRosterId });
+
+            questionnaire.Apply(new NewGroupAdded { PublicKey = parallelRosterId, ParentGroupPublicKey = chapterId });
+            questionnaire.Apply(new GroupBecameARoster(responsibleId, parallelRosterId));
+        };
+
+        Because of = () =>
+            exception = Catch.Exception(() =>
+                questionnaire.AddGroup(groupId, responsibleId, "title", rosterSizeQuestionId, null, null, parallelRosterId, true,
+                    RosterSizeSourceType.Question, rosterFixedTitles: null, rosterTitleQuestionId: null));
+
+        It should_throw_QuestionnaireException = () =>
+            exception.ShouldBeOfExactType<QuestionnaireException>();
+
+        It should_throw_exception_with_message_containting__roster__ = () =>
+            exception.Message.ToLower().ShouldContain("roster");
+
+        It should_throw_exception_with_message_containting__question__ = () =>
+            exception.Message.ToLower().ShouldContain("question");
+
+        It should_throw_exception_with_message_containting__under__ = () =>
+            exception.Message.ToLower().ShouldContain("under");
+
+        private static Exception exception;
+        private static Guid responsibleId;
+        private static Guid groupId;
+        private static Guid rosterSizeQuestionId;
+        private static Questionnaire questionnaire;
+        private static Guid chapterId;
+        private static Guid parallelRosterId;
+    }
+}
---
+++ src/Tests/WB.Core.BoundedContexts.Designer.Tests/QuestionnaireTests/when_moving_roster_in_paralell_roster_scope_but_roster_size_question_is_left.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Machine.Specifications;
+using Main.Core.Entities.SubEntities;
+using Main.Core.Events.Questionnaire;
+using WB.Core.BoundedContexts.Designer.Aggregates;
+using WB.Core.BoundedContexts.Designer.Events.Questionnaire;
+using WB.Core.BoundedContexts.Designer.Exceptions;
+
+namespace WB.Core.BoundedContexts.Designer.Tests.QuestionnaireTests
+{
+    internal class when_moving_roster_in_paralell_roster_scope_but_roster_size_question_is_left: QuestionnaireTestsContext
+    {
+        Establish context = () =>
+        {
+            var rosterSizeQuestionId = Guid.Parse("31111111111111111111111111111111");
+            questionnaire = CreateQuestionnaire(responsibleId: responsibleId);
+
+            questionnaire.Apply(new NewGroupAdded { PublicKey = chapterId });
+
+            questionnaire.Apply(new NewGroupAdded { PublicKey = anotherRosterId, ParentGroupPublicKey = chapterId });
+            questionnaire.Apply(new GroupBecameARoster(responsibleId, anotherRosterId));
+
+            questionnaire.Apply(new NewGroupAdded { PublicKey = parallelRosterId, ParentGroupPublicKey = chapterId });
+            questionnaire.Apply(new GroupBecameARoster(responsibleId, parallelRosterId));
+
+            questionnaire.Apply(new NumericQuestionAdded { PublicKey = rosterSizeQuestionId, IsInteger = true, GroupPublicKey = parallelRosterId });
+
+
+            questionnaire.Apply(new NewGroupAdded { PublicKey = groupId, ParentGroupPublicKey = parallelRosterId });
+            questionnaire.Apply(new GroupBecameARoster(responsibleId, groupId));
+            questionnaire.Apply(new RosterChanged(responsibleId, groupId, rosterSizeQuestionId, RosterSizeSourceType.Question, null, null));
+        };
+
+        Because of = () =>
+            exception = Catch.Exception(() =>
+                questionnaire.MoveGroup(groupId, anotherRosterId, 0, responsibleId));
+
+        It should_throw_QuestionnaireException = () =>
+            exception.ShouldBeOfExactType<QuestionnaireException>();
+
+        It should_throw_exception_with_message_containting__group__ = () =>
+            exception.Message.ToLower().ShouldContain("group");
+
+        It should_throw_exception_with_message_containting__with__ = () =>
+            exception.Message.ToLower().ShouldContain("size");
+
+        It should_throw_exception_with_message_containting__into__ = () =>
+            exception.Message.ToLower().ShouldContain("question");
+
+        It should_throw_exception_with_message_containting__roster_twice__ = () =>
+            exception.Message.ToLower().Split().Count(s => s == "roster").ShouldEqual(2);
+
+        private static Questionnaire questionnaire;
+        private static Guid responsibleId = Guid.Parse("DDDD0000000000000000000000000000");
+        private static Guid groupId = Guid.Parse("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+        private static Guid anotherRosterId = Guid.Parse("EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE");
+        private static Guid parallelRosterId = Guid.Parse("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF");
+        private static Guid chapterId = Guid.Parse("CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC");
+        private static Exception exception;
+    }
+}
