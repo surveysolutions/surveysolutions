0010b0fa2e37aa8fdeef17bb0d58804b21fbc953
Andrii Kozhyn <hedgic@gmail.com>
2014-03-06 13:53:05 +0200

    Headquarters: Added identity package with storage configured for it KP-3165
    Former-commit-id: 7c9d7c1c2597147116b0433d7ac0e4b84efe5c2e

---
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Authentication/AuthenticationModule.cs
@@ -0,0 +1,28 @@
+﻿using AspNet.Identity.RavenDB.Stores;
+using Microsoft.AspNet.Identity;
+using Ninject;
+using Ninject.Web.Common;
+using Ninject.Modules;
+using Raven.Client;
+using WB.Core.BoundedContexts.Headquarters.Authentication.Models;
+using WB.Core.Infrastructure.Raven.PlainStorage;
+
+namespace WB.Core.BoundedContexts.Headquarters.Authentication
+{
+    public class AuthenticationModule : NinjectModule
+    {
+        public override void Load()
+        {
+
+            Kernel.Bind<IUserStore<ApplicationUser>>()
+                .ToMethod(context => new RavenUserStore<ApplicationUser>(GetSession(), false)).InRequestScope();
+
+            Kernel.Bind<UserManager<ApplicationUser>>().ToSelf().InTransientScope();
+        }
+
+        private IAsyncDocumentSession GetSession()
+        {
+            return this.Kernel.Get<IRavenPlainStorageProvider>().GetDocumentStore().OpenAsyncSession();
+        }
+    }
+}
---
+++ src/Core/Infrastructure/WB.Core.Infrastructure.Raven/PlainStorage/IRavenPlainStorageProvider.cs
@@ -0,0 +1,9 @@
+﻿using Raven.Client;
+
+namespace WB.Core.Infrastructure.Raven.PlainStorage
+{
+    public interface IRavenPlainStorageProvider
+    {
+        IDocumentStore GetDocumentStore();
+    }
+}
