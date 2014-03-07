e120d0bd2e736ff72de89d6408ae863732addccb
Anatoliy 'TLK' Kolesnick <none@none>
2014-03-07 13:40:52 +0200

    Headquarters: added empty Survey aggregate root KP-3168
    Former-commit-id: 6d9ca53c582d3a899425911db22a2d0939b361f4

---
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Implementation/Aggregates/Survey.cs
@@ -0,0 +1,10 @@
+﻿using Ncqrs.Domain;
+
+namespace WB.Core.BoundedContexts.Headquarters.Implementation.Aggregates
+{
+    internal class Survey : AggregateRootMappedByConvention
+    {
+        /// <remarks>Is used to restore aggregate from event stream.</remarks>
+        public Survey() { }
+    }
+}
