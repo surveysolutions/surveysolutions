791706ecb1c9fa7d8b48ab17a3825addcef0f131
Slava Scherbak <slava.scherbak@gmail.com>
2014-03-07 12:26:22 +0200

    QuestionnaireVersion: added custom InconsistentVersionException exception if version is not supported KP-3159
    --HG--
    branch : hotfixes-for-v-1-6-2
    
    
    Former-commit-id: 8ebd887a3e656fea9c762d7961243098290cb4cf

---
+++ src/Core/SharedKernels/QuestionnaireVerification/WB.Core.SharedKernels.QuestionnaireVerification/ValueObjects/InconsistentVersionException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace WB.Core.SharedKernels.QuestionnaireVerification.ValueObjects
+{
+    [DataContract]
+    public class InconsistentVersionException : Exception
+    {
+        [DataMember]
+        public string Reason { get; set; }
+    }
+}
