bc8258e2922eba65ca88d6b56ef19dd906f44e38
Andrii Kozhyn <hedgic@gmail.com>
2014-03-07 18:30:18 +0200

    Headquarters: Implemented login form validation KP-3165
    Former-commit-id: c8252d282e2e86564f8f6715c3b1e4c4c739979b

--- src/UI/Headquarters/WB.UI.Headquarters/App_Start/BundleConfig.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/App_Start/BundleConfig.cs
@@ -13,6 +13,8 @@ namespace WB.UI.Headquarters.App_Start
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery-{version}.js"));
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include("~/Scripts/jquery.validate*"));
+
             bundles.Add(new StyleBundle("~/content/bootstrap").Include("~/content/bootstrap.css"));
         }
     }
