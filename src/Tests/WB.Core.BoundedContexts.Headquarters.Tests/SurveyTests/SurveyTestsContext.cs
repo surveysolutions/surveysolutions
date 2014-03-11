0df056b536890a2da132031911f3cf6f2ee21f73
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2014-03-11 15:44:09 +0200

    Headquarters\Survey: implemented basic survey creation method (with tests) KP-3168
    Former-commit-id: 10a95394ce9e6b04e4e7402bd865446ea0f0b69b

---
+++ src/Tests/WB.Core.BoundedContexts.Headquarters.Tests/SurveyTests/SurveyTestsContext.cs
@@ -0,0 +1,14 @@
+﻿using Machine.Specifications;
+using WB.Core.BoundedContexts.Headquarters.Implementation.Aggregates;
+
+namespace WB.Core.BoundedContexts.Headquarters.Tests.SurveyTests
+{
+    [Subject(typeof(Survey))]
+    internal class SurveyTestsContext
+    {
+        protected static Survey CreateSurvey()
+        {
+            return new Survey();
+        }
+    }
+}
---
+++ src/Tests/WB.Core.BoundedContexts.Headquarters.Tests/SurveyTests/when_creating_new_survey.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Machine.Specifications;
+using Ncqrs.Spec;
+using WB.Core.BoundedContexts.Headquarters.Events.Survey;
+using WB.Core.BoundedContexts.Headquarters.Implementation.Aggregates;
+
+namespace WB.Core.BoundedContexts.Headquarters.Tests.SurveyTests
+{
+    internal class when_creating_new_survey : SurveyTestsContext
+    {
+        Establish context = () =>
+        {
+            surveyId = Guid.Parse("11111111111111111111111111111111");
+
+            eventContext = new EventContext();
+        };
+
+        Because of = () =>
+            new Survey(surveyId);
+
+        Cleanup stuff = () =>
+        {
+            eventContext.Dispose();
+            eventContext = null;
+        };
+
+        It should_raise_NewSurveyStarted_event = () =>
+            eventContext.ShouldContainEvent<NewSurveyStarted>();
+
+        private static EventContext eventContext;
+        private static Guid surveyId;
+    }
+}
