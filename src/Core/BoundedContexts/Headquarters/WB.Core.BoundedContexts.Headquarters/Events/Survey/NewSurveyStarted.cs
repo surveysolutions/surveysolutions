0c2eb1bf4549e463227f114ca87b2701851c96d0
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2014-03-11 15:49:19 +0200

    Headquarters\Survey\CreateNew: added survey name property KP-3168
    Former-commit-id: bf20c26726ef502715768ddb65279ed35a779bc7

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Events/Survey/NewSurveyStarted.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Events/Survey/NewSurveyStarted.cs
@@ -2,5 +2,13 @@
 
 namespace WB.Core.BoundedContexts.Headquarters.Events.Survey
 {
-    public class NewSurveyStarted : SurveyEvent {}
+    public class NewSurveyStarted : SurveyEvent
+    {
+        public string Name { get; private set; }
+
+        public NewSurveyStarted(string name)
+        {
+            this.Name = name;
+        }
+    }
 }
--- src/Tests/WB.Core.BoundedContexts.Headquarters.Tests/SurveyTests/when_creating_new_survey.cs
+++ src/Tests/WB.Core.BoundedContexts.Headquarters.Tests/SurveyTests/when_creating_new_survey.cs
@@ -11,12 +11,13 @@ namespace WB.Core.BoundedContexts.Headquarters.Tests.SurveyTests
         Establish context = () =>
         {
             surveyId = Guid.Parse("11111111111111111111111111111111");
+            surveyName = "MySurvey";
 
             eventContext = new EventContext();
         };
 
         Because of = () =>
-            new Survey(surveyId);
+            new Survey(surveyId, surveyName);
 
         Cleanup stuff = () =>
         {
@@ -27,7 +28,12 @@ namespace WB.Core.BoundedContexts.Headquarters.Tests.SurveyTests
         It should_raise_NewSurveyStarted_event = () =>
             eventContext.ShouldContainEvent<NewSurveyStarted>();
 
+        It should_raise_NewSurveyStarted_event_with_Name_equal_to_specified_survey_name = () =>
+            eventContext.GetSingleEvent<NewSurveyStarted>()
+                .Name.ShouldEqual(surveyName);
+
         private static EventContext eventContext;
         private static Guid surveyId;
+        private static string surveyName;
     }
 }
