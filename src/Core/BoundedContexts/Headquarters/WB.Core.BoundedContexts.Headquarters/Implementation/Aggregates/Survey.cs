7b1d98d75e9874f7dbd79feef9414d5d6c0ef5c3
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2014-03-11 15:49:19 +0200

    Headquarters\Survey\CreateNew: added survey name property KP-3168
    Former-commit-id: bf20c26726ef502715768ddb65279ed35a779bc7

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Implementation/Aggregates/Survey.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Implementation/Aggregates/Survey.cs
@@ -15,10 +15,10 @@ namespace WB.Core.BoundedContexts.Headquarters.Implementation.Aggregates
         /// <remarks>Is used to restore aggregate from event stream.</remarks>
         public Survey() { }
 
-        public Survey(Guid id)
+        public Survey(Guid id, string name)
             : base(id)
         {
-            this.ApplyEvent(new NewSurveyStarted());
+            this.ApplyEvent(new NewSurveyStarted(name));
         }
     }
 }
