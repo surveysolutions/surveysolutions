3cdb3d05a745d7fede9184de59dbd2f2c6d396bd
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2014-03-11 15:44:09 +0200

    Headquarters\Survey: implemented basic survey creation method (with tests) KP-3168
    Former-commit-id: 10a95394ce9e6b04e4e7402bd865446ea0f0b69b

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Implementation/Aggregates/Survey.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Implementation/Aggregates/Survey.cs
@@ -1,10 +1,24 @@
-﻿using Ncqrs.Domain;
+﻿using System;
+using Ncqrs.Domain;
+using WB.Core.BoundedContexts.Headquarters.Events.Survey;
 
 namespace WB.Core.BoundedContexts.Headquarters.Implementation.Aggregates
 {
     internal class Survey : AggregateRootMappedByConvention
     {
+        #region State
+
+        private void Apply(NewSurveyStarted @event) {}
+
+        #endregion
+
         /// <remarks>Is used to restore aggregate from event stream.</remarks>
         public Survey() { }
+
+        public Survey(Guid id)
+            : base(id)
+        {
+            this.ApplyEvent(new NewSurveyStarted());
+        }
     }
 }
