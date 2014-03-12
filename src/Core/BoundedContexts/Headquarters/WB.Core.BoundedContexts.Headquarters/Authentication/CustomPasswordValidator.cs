aa3d1a5814ac30a3757775b1ce896a0ec306a7fd
Andrii Kozhyn <hedgic@gmail.com>
2014-03-12 12:35:53 +0200

    Headquarters: implemented security policy KP-3205
    Former-commit-id: 0ebd14d4487871a092815e302901160def1a989e

---
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Authentication/CustomPasswordValidator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+
+namespace WB.Core.BoundedContexts.Headquarters.Authentication
+{
+    public class CustomPasswordValidator : IIdentityValidator<string>
+    {
+        public int RequiredLength { get; set; }
+
+        public CustomPasswordValidator(int length)
+        {
+            RequiredLength = length;
+        }
+
+        public Task<IdentityResult> ValidateAsync(string item)
+        {
+            if (String.IsNullOrEmpty(item) || item.Length < RequiredLength)
+            {
+                return Task.FromResult(IdentityResult.Failed(String.Format("Password should be of length {0}", RequiredLength)));
+            }
+
+            string pattern = @"^(?=.*[a-z])(?=.*[0-9])(?=.*[A-Z]).*$";
+
+            if (!Regex.IsMatch(item, pattern))
+            {
+                return Task.FromResult(IdentityResult.Failed("Password must contain at least one number, one upper case character and one lower case character"));
+            }
+
+            return Task.FromResult(IdentityResult.Success);
+        }
+    }
+}
