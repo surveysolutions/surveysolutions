2b081dd84e12b46c63ca56fa31789a8af0155b6b
Andrii Kozhyn <hedgic@gmail.com>
2014-03-12 17:25:37 +0200

    Headquarter: added tests for roles management KP-3165
    Former-commit-id: b753cfecdb71a3cf5947c0b7b143e80be0283304

---
+++ src/Tests/WB.Core.BoundedContexts.Headquarters.Tests/AuthenticationTests/ApplicationUserTests/when_removing_IsAdministrator_flag.cs
@@ -0,0 +1,25 @@
+﻿using System.Security.Claims;
+using Machine.Specifications;
+using WB.Core.BoundedContexts.Headquarters.Authentication;
+using WB.Core.BoundedContexts.Headquarters.Authentication.Models;
+
+namespace WB.Core.BoundedContexts.Headquarters.Tests.AuthenticationTests.ApplicationUserTests
+{
+     [Subject(typeof(ApplicationUser))]
+    public class when_removing_IsAdministrator_flag
+    {
+        private static ApplicationUser user;
+
+        Establish context = () =>
+        {
+            user = new ApplicationUser("11");
+        };
+
+        Because of = () => user.IsAdministrator = false;
+
+        It should_remove_role_claim_in_identity = () =>
+            user.Claims.ShouldNotContain(claim => claim.ClaimType == ClaimTypes.Role && claim.ClaimValue == ApplicationRoles.Administrator);
+
+        It should_return_false = () => user.IsAdministrator.ShouldBeFalse();
+    }
+}
