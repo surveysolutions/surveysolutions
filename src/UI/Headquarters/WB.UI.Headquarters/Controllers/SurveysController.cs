bbd1e5e9a3bb176b3f23b908c62e8bc64eb0773c
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2014-03-12 16:21:48 +0200

    Headquarters: added interface for adding new survey KP-3168
    Former-commit-id: a60a71a01e46d5c7293e4652f7b49cf7a4bcd855

--- src/UI/Headquarters/WB.UI.Headquarters/Controllers/SurveysController.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/Controllers/SurveysController.cs
@@ -2,6 +2,7 @@ using System.Web.Mvc;
 using WB.Core.BoundedContexts.Headquarters.Authentication;
 using WB.Core.BoundedContexts.Headquarters.ViewFactories;
 using WB.Core.BoundedContexts.Headquarters.Views.Survey;
+using WB.UI.Headquarters.Models;
 
 namespace WB.UI.Headquarters.Controllers
 {
@@ -20,6 +21,17 @@ namespace WB.UI.Headquarters.Controllers
             SurveyLineView[] surveys = this.surveyViewFactory.GetAllLineViews();
 
             return this.View(surveys);
+        }
+
+        public ActionResult StartNew()
+        {
+            return this.View(new NewSurveyModel());
+        }
+
+        [HttpPost]
+        public ActionResult StartNew(NewSurveyModel model)
+        {
+            return this.RedirectToAction("Index");
         }
     }
 }
