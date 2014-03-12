442de06e646694ae881ffdd523c8ac7edd392c61
Anatoliy 'TLK' Kolesnick <none@none>
2014-03-12 17:31:51 +0200

    Headquarters: added command execution to start new survey (however no command execution infrastructure is here) KP-3168
    Former-commit-id: e6436e2fcde86cd1f1e3937d5e1fc61c649f816a

---
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Commands/Survey/StartNewSurvey.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Ncqrs.Commanding.CommandExecution.Mapping.Attributes;
+using WB.Core.BoundedContexts.Headquarters.Commands.Survey.Base;
+
+namespace WB.Core.BoundedContexts.Headquarters.Commands.Survey
+{
+    [MapsToAggregateRootConstructor(typeof(Implementation.Aggregates.Survey))]
+    public class StartNewSurvey : SurveyCommand
+    {
+        public string Name { get; private set; }
+
+        public StartNewSurvey(Guid surveyId, string name)
+            : base(surveyId)
+        {
+            this.Name = name;
+        }
+    }
+}
