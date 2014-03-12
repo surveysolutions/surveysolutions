337732681b2470bb83715d2006f35e56aaab8bbb
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2014-03-12 16:21:48 +0200

    Headquarters: added interface for adding new survey KP-3168
    Former-commit-id: a60a71a01e46d5c7293e4652f7b49cf7a4bcd855

---
+++ src/UI/Headquarters/WB.UI.Headquarters/Models/NewSurveyModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WB.UI.Headquarters.Models
+{
+    public class NewSurveyModel
+    {
+        [Required]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+    }
+}
