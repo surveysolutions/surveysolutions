e0aab882aec2d79106665e7c20c91a8a04a45a14
Andrii Kozhyn <hedgic@gmail.com>
2014-03-12 17:26:06 +0200

    Headquarters: added tests for password policy KP-3205
    Former-commit-id: 4cd9050cbe5e9b60f4b666df83757fcb52d8c86d

---
+++ src/Tests/WB.Core.BoundedContexts.Headquarters.Tests/AuthenticationTests/CustomPasswordValidatorTests/when_using_valid_password.cs
@@ -0,0 +1,22 @@
+﻿using Machine.Specifications;
+using Microsoft.AspNet.Identity;
+using WB.Core.BoundedContexts.Headquarters.Authentication;
+
+namespace WB.Core.BoundedContexts.Headquarters.Tests.AuthenticationTests.CustomPasswordValidatorTests
+{
+    [Subject(typeof(CustomPasswordValidator))]
+    public class when_using_valid_password
+    {
+        static CustomPasswordValidator validator;
+        static IdentityResult identityResult;
+
+        Establish context = () =>
+        {
+            validator = new CustomPasswordValidator(10);
+        };
+
+        Because of = async () => identityResult = await validator.ValidateAsync("Qwert12341");
+
+        It should_provide_failed_identity_result = () => identityResult.Succeeded.ShouldBeTrue();
+    }
+}
