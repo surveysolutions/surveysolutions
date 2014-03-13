89f6c7b4be043101c0c6f75d1077d35ef17d95cf
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2014-03-13 15:08:26 +0200

    Headquarters: implemented survey line view denormalizer KP-3168
    Former-commit-id: d8d59ddd45493dbe136ba444d54ffabf6a7df39b

---
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Implementation/EventHandlers/SurveyLineViewDenormalizer.cs
@@ -0,0 +1,23 @@
+using Ncqrs.Eventing.ServiceModel.Bus;
+using WB.Core.BoundedContexts.Headquarters.Events.Survey;
+using WB.Core.BoundedContexts.Headquarters.Views.Survey;
+using WB.Core.Infrastructure.FunctionalDenormalization.EventHandlers;
+using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
+
+namespace WB.Core.BoundedContexts.Headquarters.Implementation.EventHandlers
+{
+    internal class SurveyLineViewDenormalizer : AbstractFunctionalEventHandler<SurveyLineView>,
+        IUpdateHandler<SurveyLineView, NewSurveyStarted>
+    {
+        public SurveyLineViewDenormalizer(IReadSideRepositoryWriter<SurveyLineView> repositoryWriter)
+            : base(repositoryWriter) {}
+
+        public SurveyLineView Update(SurveyLineView state, IPublishedEvent<NewSurveyStarted> @event)
+        {
+            return new SurveyLineView
+            {
+                Name = @event.Payload.Name,
+            };
+        }
+    }
+}
