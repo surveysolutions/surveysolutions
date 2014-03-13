530ecef412bc122d12d3992e9952ac123dc9a73b
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2014-03-13 15:43:25 +0200

    removed in-memory read side stuff KP-3168
    Former-commit-id: 357ca8fd74ef294e1dd9687229516f63f6b2aa86

--- src/UI/Designer/WB.UI.Designer/Code/DesignerRegistry.cs
+++ src/UI/Designer/WB.UI.Designer/Code/DesignerRegistry.cs
@@ -17,8 +17,6 @@ namespace WB.UI.Designer.Code
 {
     using System;
     using System.Web.Mvc;
-
-    using WB.UI.Designer.Filters;
     using WB.UI.Designer.WebServices;
 
     public class DesignerRegistry : CoreRegistry
@@ -38,21 +36,12 @@ namespace WB.UI.Designer.Code
 
         protected override object GetReadSideRepositoryWriter(IContext context)
         {
-            return ShouldUsePersistentReadLayer()
-                ? this.Kernel.Get(typeof(RavenReadSideRepositoryWriter<>).MakeGenericType(context.GenericArguments[0]))
-                : this.GetInMemoryReadSideRepositoryAccessor(context);
+            return this.Kernel.Get(typeof(RavenReadSideRepositoryWriter<>).MakeGenericType(context.GenericArguments[0]));
         }
 
         protected override object GetReadSideRepositoryReader(IContext context)
         {
-            return ShouldUsePersistentReadLayer()
-                ? this.Kernel.Get(typeof(RavenReadSideRepositoryReader<>).MakeGenericType(context.GenericArguments[0]))
-                : this.GetInMemoryReadSideRepositoryAccessor(context);
-        }
-
-        private static bool ShouldUsePersistentReadLayer()
-        {
-            return bool.Parse(WebConfigurationManager.AppSettings["ShouldUsePersistentReadLayer"]);
+            return this.Kernel.Get(typeof(RavenReadSideRepositoryReader<>).MakeGenericType(context.GenericArguments[0]));
         }
     }
 }
