8e9693b0033f3286e0f017a44547b64a554ff502
Andrii Kozhyn <hedgic@gmail.com>
2014-03-13 17:38:06 +0200

    Headquarter: Added tests on login process KP-3165
    Former-commit-id: f7a4919c450bb4fa40570a43e38efb3a249c545f

---
+++ src/Tests/WB.UI.Headquarters.Tests/Controllers/AccountControllerSpecs/when_user_tries_to_login_with_invalid_credentials.cs
@@ -0,0 +1,45 @@
+﻿using System.Security.Principal;
+using System.Web.Mvc;
+using Machine.Specifications;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+using Moq;
+using WB.Core.BoundedContexts.Headquarters.Authentication.Models;
+using WB.UI.Headquarters.Controllers;
+using WB.UI.Headquarters.Models;
+using WB.UI.Headquarters.Resources;
+using It = Machine.Specifications.It;
+
+namespace WB.UI.Headquarters.Tests.Controllers.AccountControllerSpecs
+{
+    [Subject(typeof(AccountController))]
+    public class when_user_tries_to_login_with_invalid_credentials
+    {
+        static AccountController controller;
+        static LoginModel loginModel;
+        static ActionResult actionReult;
+
+        Establish context = () =>
+        {
+            loginModel = new LoginModel
+            {
+                Login = "login",
+                Password = "password"
+            };
+
+            var userStore = new Mock<IUserStore<ApplicationUser>>();
+            var userManager = new Mock<UserManager<ApplicationUser>>(userStore.Object);
+
+            controller = new AccountController(userManager.Object, Mock.Of<IAuthenticationManager>())
+            {
+                Url = Mock.Of<UrlHelper>()
+            };
+        };
+
+        Because of = async () => actionReult = await controller.Login(loginModel, null);
+
+        It should_add_model_error = () => controller.ModelState[""].Errors.ShouldContain(error => error.ErrorMessage == LoginPageResources.IvalidUserNameOrPassword);
+
+        It should_return_view = () => actionReult.ShouldBeOfExactType<ViewResult>();
+    }
+}
---
+++ src/Tests/WB.UI.Headquarters.Tests/Create.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+using Moq;
+using WB.Core.BoundedContexts.Headquarters.Authentication.Models;
+using WB.UI.Headquarters.Controllers;
+
+namespace WB.UI.Headquarters.Tests
+{
+    public static class Create
+    {
+        public static AccountController AccountController(UserManager<ApplicationUser> userManager = null,
+            IAuthenticationManager authenticationManager = null)
+        {
+            if (userManager == null)
+            {
+                userManager = new Mock<UserManager<ApplicationUser>>(Mock.Of<IUserStore<ApplicationUser>>()).Object;
+            }
+
+            if (authenticationManager == null)
+            {
+                authenticationManager = Mock.Of<IAuthenticationManager>();
+            }
+
+            return new AccountController(userManager, authenticationManager);
+        }
+    }
+}
