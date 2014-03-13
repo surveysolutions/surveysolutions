17d8a6e446d802bd0fc84bc675db7660b223c5e6
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2014-03-13 17:38:06 +0200

    Headquarter: Added tests on login process KP-3165
    Former-commit-id: f7a4919c450bb4fa40570a43e38efb3a249c545f

---
+++ src/Tests/WB.UI.Headquarters.Tests/Controllers/AccountControllerSpecs/when_user_logges_in_with_valid_credentials.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using System.Security.Principal;
+using System.Web.Mvc;
+using Machine.Specifications;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+using WB.Core.BoundedContexts.Headquarters.Authentication.Models;
+using WB.UI.Headquarters.Controllers;
+using WB.UI.Headquarters.Models;
+using It = Machine.Specifications.It;
+
+namespace WB.UI.Headquarters.Tests.Controllers.AccountControllerSpecs
+{
+    [Subject(typeof(AccountController))]
+    public class when_user_logges_in_without_return_url: user_credentials_are_valid
+    {
+        Because of = async () => actionResult = await controller.Login(LoginModel, null);
+
+        It should_redirect_to_index = () =>
+        {
+            var redirectToRouteResult = actionResult as RedirectToRouteResult;
+            redirectToRouteResult.ShouldNotBeNull();
+
+            redirectToRouteResult.RouteValues["controller"].ShouldEqual("Home");
+            redirectToRouteResult.RouteValues["action"].ShouldEqual("Index");
+        };
+
+        It should_sign_in_provided_identity = () => authenticationManager.Verify(x => x.SignIn(Moq.It.IsAny<AuthenticationProperties>(), testIdentity));
+
+        It should_sign_out_any_other_identity = () => authenticationManager.Verify(x => 
+            x.SignOut(Moq.It.Is<string[]>(param => param.Contains(DefaultAuthenticationTypes.ExternalCookie))));
+    }
+}
