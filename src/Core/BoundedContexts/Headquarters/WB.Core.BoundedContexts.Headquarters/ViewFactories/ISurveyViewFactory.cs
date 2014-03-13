1b2b97d670259bec6e8723fff234150bff57fcef
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2014-03-13 17:54:09 +0200

    Headquarters: added survey details view projection KP-3168
    Former-commit-id: 1862a7b186dfbe8660281d05e4f6d69ecb09407e

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/ViewFactories/ISurveyViewFactory.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/ViewFactories/ISurveyViewFactory.cs
@@ -1,9 +1,12 @@
-﻿using WB.Core.BoundedContexts.Headquarters.Views.Survey;
+﻿using System;
+using WB.Core.BoundedContexts.Headquarters.Views.Survey;
 
 namespace WB.Core.BoundedContexts.Headquarters.ViewFactories
 {
     public interface ISurveyViewFactory
     {
         SurveyLineView[] GetAllLineViews();
+
+        SurveyDetailsView GetDetailsView(string surveyId);
     }
 }
---
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Views/Survey/SurveyDetailsView.cs
@@ -0,0 +1,9 @@
+﻿using WB.Core.Infrastructure.ReadSide;
+
+namespace WB.Core.BoundedContexts.Headquarters.Views.Survey
+{
+    public class SurveyDetailsView : IView
+    {
+        public string Name { get; set; }
+    }
+}
