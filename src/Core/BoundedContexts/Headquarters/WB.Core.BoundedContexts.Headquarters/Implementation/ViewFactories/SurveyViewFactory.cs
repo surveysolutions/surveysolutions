8d7ea183168c6939697179d0195ebe3ad8121563
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2014-03-13 15:08:26 +0200

    Headquarters: implemented survey line view denormalizer KP-3168
    Former-commit-id: d8d59ddd45493dbe136ba444d54ffabf6a7df39b

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Implementation/ViewFactories/SurveyViewFactory.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Implementation/ViewFactories/SurveyViewFactory.cs
@@ -1,17 +1,22 @@
-﻿using WB.Core.BoundedContexts.Headquarters.ViewFactories;
+﻿using System.Linq;
+using WB.Core.BoundedContexts.Headquarters.ViewFactories;
 using WB.Core.BoundedContexts.Headquarters.Views.Survey;
+using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
 
 namespace WB.Core.BoundedContexts.Headquarters.Implementation.ViewFactories
 {
     internal class SurveyViewFactory : ISurveyViewFactory
     {
+        private readonly IQueryableReadSideRepositoryReader<SurveyLineView> surveyLineViewRepositoryReader;
+
+        public SurveyViewFactory(IQueryableReadSideRepositoryReader<SurveyLineView> surveyLineViewRepositoryReader)
+        {
+            this.surveyLineViewRepositoryReader = surveyLineViewRepositoryReader;
+        }
+
         public SurveyLineView[] GetAllLineViews()
         {
-            return new[]
-            {
-                new SurveyLineView { Name = "Survey 1" },
-                new SurveyLineView { Name = "Survey 2" },
-            };
+            return this.surveyLineViewRepositoryReader.Query(views => views.ToArray());
         }
     }
 }
