d11c05d5ef54b0abe9e19d54ada84cad760ee138
Andrii Kozhyn <hedgic@gmail.com>
2014-03-14 16:19:26 +0200

    Headquarters: added tests for edit user account method KP-3165
    Former-commit-id: 1c4d7b78be1a3fc14c3d49c09abb483560bd3560

---
+++ src/Tests/WB.UI.Headquarters.Tests/Controllers/MvcExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Web.Mvc;
+using Machine.Specifications.Annotations;
+using WB.UI.Headquarters.Models;
+
+namespace WB.UI.Headquarters.Tests.Controllers
+{
+    public static class MvcExtensions
+    {
+        public static T GetModel<T>([NotNull] this ActionResult actionResult) where T : class
+        {
+            if (actionResult == null) throw new ArgumentNullException("actionResult");
+
+            var viewResult = actionResult as ViewResult;
+            var model = viewResult.Model as T;
+            return model;
+        }
+    }
+}
