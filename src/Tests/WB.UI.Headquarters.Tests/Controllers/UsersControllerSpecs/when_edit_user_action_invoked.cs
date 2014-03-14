9b8fd65205180872da2dcad6ed43159132de78de
Andrii Kozhyn <hedgic@gmail.com>
2014-03-14 16:19:26 +0200

    Headquarters: added tests for edit user account method KP-3165
    Former-commit-id: 1c4d7b78be1a3fc14c3d49c09abb483560bd3560

--- src/Tests/WB.UI.Headquarters.Tests/Controllers/UsersControllerSpecs/when_edit_user_action_invoked.cs
+++ src/Tests/WB.UI.Headquarters.Tests/Controllers/UsersControllerSpecs/when_edit_user_action_invoked.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNet.Identity;
 using NSubstitute;
 using WB.Core.BoundedContexts.Headquarters.Authentication.Models;
 using WB.UI.Headquarters.Controllers;
+using WB.UI.Headquarters.Models;
 
 namespace WB.UI.Headquarters.Tests.Controllers.UsersControllerSpecs
 {
@@ -16,17 +17,35 @@ namespace WB.UI.Headquarters.Tests.Controllers.UsersControllerSpecs
             var userStore = Substitute.For<IUserStore<ApplicationUser>>();
 
             var userManager = Substitute.For<UserManager<ApplicationUser>>(userStore);
-            userManager.FindAsync(null, null)
-                .ReturnsForAnyArgs(Task.FromResult(new ApplicationUser("1")));
+            applicationUser = new ApplicationUser("1")
+            {
+                IsAdministrator = true,
+                IsHeadquarter = true
+            };
 
-            controller = Create.UsersController();
+            userManager.FindByIdAsync(null)
+                .ReturnsForAnyArgs(Task.FromResult(applicationUser));
+
+            controller = Create.UsersController(userManager);
         };
 
         Because of = async () => actionResult = await controller.EditAccount("1");
 
-        //It should_return_view = () => actionResult.ShouldBeOfExactType<ViewResult>();
+        It should_return_view = () => actionResult.ShouldBeOfExactType<ViewResult>();
+
+        It should_fill_view_model = () =>
+        {
+            var model = actionResult.GetModel<AccountModel>();
+
+            model.Id.ShouldEqual("1");
+            model.AdminRoleEnabled.ShouldBeTrue();
+            model.HeadquarterRoleEnabled.ShouldBeTrue();
+        };
+
+        It should_not_allow_to_change_user_name = () => actionResult.GetModel<AccountModel>().UserNameChangeAllowed.ShouldBeFalse();
 
         private static UsersController controller;
         private static ActionResult actionResult;
+        private static ApplicationUser applicationUser;
     }
 }
