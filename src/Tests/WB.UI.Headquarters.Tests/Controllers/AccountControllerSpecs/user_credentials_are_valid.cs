64bf643c56982cef02890b0d61f9ce2cfdc8e44f
Andrii Kozhyn <hedgic@gmail.com>
2014-03-14 15:48:09 +0200

    Headquarters: replaced Moq with NSubstitute KP-3165
    Former-commit-id: c8e8dec6caa3bcbc658613e032adee4be075cd15

--- src/Tests/WB.UI.Headquarters.Tests/Controllers/AccountControllerSpecs/user_credentials_are_valid.cs
+++ src/Tests/WB.UI.Headquarters.Tests/Controllers/AccountControllerSpecs/user_credentials_are_valid.cs
@@ -1,14 +1,14 @@
-﻿using System.Linq;
+﻿using System.Security.Claims;
 using System.Security.Principal;
+using System.Threading.Tasks;
 using System.Web.Mvc;
 using Machine.Specifications;
 using Microsoft.AspNet.Identity;
 using Microsoft.Owin.Security;
-using Moq;
+using NSubstitute;
 using WB.Core.BoundedContexts.Headquarters.Authentication.Models;
 using WB.UI.Headquarters.Controllers;
 using WB.UI.Headquarters.Models;
-using It = Machine.Specifications.It;
 
 namespace WB.UI.Headquarters.Tests.Controllers.AccountControllerSpecs
 {
@@ -17,8 +17,8 @@ namespace WB.UI.Headquarters.Tests.Controllers.AccountControllerSpecs
         protected static ActionResult actionResult;
         protected static AccountController controller;
         protected static LoginModel LoginModel;
-        protected static Mock<IAuthenticationManager> authenticationManager;
-        protected static GenericIdentity testIdentity;
+        protected static IAuthenticationManager authenticationManager;
+        protected static ClaimsIdentity testIdentity;
 
         Establish context = () =>
         {
@@ -28,18 +28,20 @@ namespace WB.UI.Headquarters.Tests.Controllers.AccountControllerSpecs
                 Password = "password"
             };
 
-            var userStore = new Mock<IUserStore<ApplicationUser>>();
+            var userStore = Substitute.For<IUserStore<ApplicationUser>>();
 
-            var userManager = new Mock<UserManager<ApplicationUser>>(userStore.Object);
-            userManager.Setup(x => x.FindAsync(LoginModel.Login, LoginModel.Password))
-                .ReturnsAsync(new ApplicationUser("1"));
+            var userManager = Substitute.For<UserManager<ApplicationUser>>(userStore);
+            userManager.FindAsync(LoginModel.Login, LoginModel.Password)
+                .Returns(Task.FromResult(new ApplicationUser("1")));
+
             testIdentity = new GenericIdentity("test identity");
-            userManager.Setup(x => x.CreateIdentityAsync(Moq.It.IsAny<ApplicationUser>(), DefaultAuthenticationTypes.ApplicationCookie))
-                .ReturnsAsync(testIdentity);
 
-            authenticationManager = new Mock<IAuthenticationManager>();
-            controller = Create.AccountController(userManager.Object, authenticationManager.Object);
-            controller.Url = Mock.Of<UrlHelper>();
+            userManager.CreateIdentityAsync(Arg.Any<ApplicationUser>(), DefaultAuthenticationTypes.ApplicationCookie)
+                .Returns(Task.FromResult(testIdentity));
+
+            authenticationManager = Substitute.For<IAuthenticationManager>();
+            controller = Create.AccountController(userManager, authenticationManager);
+            controller.Url = Substitute.For<UrlHelper>();
         };
     }
 }
---
+++ src/Tests/WB.UI.Headquarters.Tests/Controllers/UsersControllerSpecs/when_edit_user_action_invoked.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using System.Web.Mvc;
+using Machine.Specifications;
+using Microsoft.AspNet.Identity;
+using NSubstitute;
+using WB.Core.BoundedContexts.Headquarters.Authentication.Models;
+using WB.UI.Headquarters.Controllers;
+
+namespace WB.UI.Headquarters.Tests.Controllers.UsersControllerSpecs
+{
+    [Subject(typeof(UsersController))]
+    public class when_EditUser_action_invoked
+    {
+        Establish context = () =>
+        {
+            var userStore = Substitute.For<IUserStore<ApplicationUser>>();
+
+            var userManager = Substitute.For<UserManager<ApplicationUser>>(userStore);
+            userManager.FindAsync(null, null)
+                .ReturnsForAnyArgs(Task.FromResult(new ApplicationUser("1")));
+
+            controller = Create.UsersController();
+        };
+
+        Because of = async () => actionResult = await controller.EditAccount("1");
+
+        //It should_return_view = () => actionResult.ShouldBeOfExactType<ViewResult>();
+
+        private static UsersController controller;
+        private static ActionResult actionResult;
+    }
+}
