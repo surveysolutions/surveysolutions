76f74456a8f13cdabba5da2888c8f9bde40f5583
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2014-03-17 19:43:49 -0400

    DataCollection: second part group condition reevaluation is fixed KP-3220
    --HG--
    branch : hotfixes-for-v-1-6-2
    
    
    Former-commit-id: c7a7926453d07bb086bd4b47f38d6aacdffc757d

---
+++ src/Tests/WB.Core.SharedKernels.DataCollection.Tests/InterviewTests/when_reevaluating_whole_interview_and_questionnaire_has_group_depending_on_recently_enabled_question.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Machine.Specifications;
+using Main.Core.Entities.SubEntities;
+using Microsoft.Practices.ServiceLocation;
+using Moq;
+using Ncqrs.Spec;
+using WB.Core.SharedKernels.DataCollection.Aggregates;
+using WB.Core.SharedKernels.DataCollection.Events.Interview;
+using WB.Core.SharedKernels.DataCollection.Implementation.Aggregates;
+using WB.Core.SharedKernels.DataCollection.Implementation.Repositories;
+using WB.Core.SharedKernels.ExpressionProcessor.Services;
+using It = Machine.Specifications.It;
+
+namespace WB.Core.SharedKernels.DataCollection.Tests.InterviewTests
+{
+    internal class when_reevaluating_whole_interview_and_questionnaire_has_group_depending_on_recently_enabled_question : InterviewTestsContext
+    {
+        Establish context = () =>
+        {
+            questionnaireId = Guid.Parse("10000000000000000000000000000000");
+
+            var userId = Guid.Parse("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF");
+
+            conditionallyDependentGroupId = Guid.Parse("33333333333333333333333333333333");
+            conditionallyRecentlyEnabledQuestionId = Guid.Parse("43333333333333333333333333333333");
+
+            var questionaire = Mock.Of<IQuestionnaire>(_ =>
+                _.GetAllGroupsWithNotEmptyCustomEnablementConditions() == new Guid[] { conditionallyDependentGroupId }
+                    &&
+                    _.GetQuestionsInvolvedInCustomEnablementConditionOfGroup(conditionallyDependentGroupId) ==
+                        new [] { new QuestionIdAndVariableName(conditionallyRecentlyEnabledQuestionId, "q1") }
+                    && _.GetCustomEnablementConditionForGroup(conditionallyDependentGroupId)=="[q1]==2"
+                    && _.GetCustomEnablementConditionForQuestion(conditionallyRecentlyEnabledQuestionId)=="2==2"
+                    && _.GetQuestionsInvolvedInCustomEnablementConditionOfQuestion(conditionallyRecentlyEnabledQuestionId) == new[] { new QuestionIdAndVariableName(Guid.NewGuid(), "qsomething") });
+
+            var expressionProcessor = new WB.Core.SharedKernels.ExpressionProcessor.Implementation.Services.ExpressionProcessor();
+            var questionnaireRepository = CreateQuestionnaireRepositoryStubWithOneQuestionnaire(questionnaireId,
+                                                                                                questionaire);
+
+            Mock.Get(ServiceLocator.Current)
+                .Setup(locator => locator.GetInstance<IQuestionnaireRepository>())
+                .Returns(questionnaireRepository);
+
+            Mock.Get(ServiceLocator.Current)
+                .Setup(locator => locator.GetInstance<IExpressionProcessor>())
+                .Returns(expressionProcessor);
+
+            interview = CreateInterview(questionnaireId: questionnaireId);
+
+            interview.Apply(new GroupDisabled(conditionallyDependentGroupId, new decimal[0]));
+
+            interview.Apply(new QuestionDisabled(conditionallyRecentlyEnabledQuestionId, new decimal[0]));
+            interview.Apply(new NumericIntegerQuestionAnswered(userId, conditionallyRecentlyEnabledQuestionId, new decimal[0],
+                DateTime.Now, 2));
+            eventContext = new EventContext();
+        };
+
+        Cleanup stuff = () =>
+        {
+            eventContext.Dispose();
+            eventContext = null;
+        };
+
+        Because of = () =>
+            interview.ReevaluateSynchronizedInterview();
+
+        It should_not_raise_GroupDisabled_event_with_GroupId_equal_to_conditionallyDependentGroupId = () =>
+            eventContext.ShouldNotContainEvent<GroupDisabled>(@event
+             => @event.GroupId == conditionallyDependentGroupId);
+
+        It should_raise_GroupEnabled_event_with_GroupId_equal_to_conditionallyDependentGroupId = () =>
+           eventContext.ShouldContainEvent<GroupEnabled>(@event
+            => @event.GroupId == conditionallyDependentGroupId);
+
+        private static EventContext eventContext;
+        private static Guid questionnaireId;
+        private static Interview interview;
+        private static Guid conditionallyDependentGroupId;
+        private static Guid conditionallyRecentlyEnabledQuestionId;
+    }
+}
