c4c621e3d1f73484ef1c84d54814da1b62beeca0
Slava Scherbak <slava.scherbak@gmail.com>
2014-03-17 13:41:07 +0200

    HQ\Survey: added tests for supervisor registration command handler. Fix Service locator setup on assembly start KP-3166
    Former-commit-id: 67236ef5199a5d93b1b2ade139f2c22f9350abdf

---
+++ src/Tests/WB.Core.BoundedContexts.Headquarters.Tests/SurveyTests/when_registering_supervisor_with_not_unique_login.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Machine.Specifications;
+using Moq;
+using Ncqrs.Spec;
+using WB.Core.BoundedContexts.Headquarters.Exceptions;
+using WB.Core.BoundedContexts.Headquarters.Implementation.Aggregates;
+using WB.Core.BoundedContexts.Headquarters.Services;
+using It = Machine.Specifications.It;
+
+namespace WB.Core.BoundedContexts.Headquarters.Tests.SurveyTests
+{
+    internal class when_registering_supervisor_with_not_unique_login : SurveyTestsContext
+    {
+        Establish context = () =>
+        {
+            loginChecker = Mock.Of<ILoginsChecker>(x => x.IsUnique(login) == false);
+
+            SetupInstanceToMockedServiceLocator<ILoginsChecker>(loginChecker);
+
+            survey = CreateSurvey();
+
+            eventContext = new EventContext();
+        };
+
+        private Because of = () =>
+            exception = Catch.Exception(() =>
+                survey.RegisterSupervisor(login, password));
+
+        Cleanup stuff = () =>
+        {
+            eventContext.Dispose();
+            eventContext = null;
+        };
+
+        It should_throw_SurveyException = () =>
+            exception.ShouldBeOfExactType<SurveyException>();
+
+        It should_throw_exception_with_message_containing__name____empty__ = () =>
+            exception.Message.ToLower().ToSeparateWords().ShouldContain("supervisor's", "login", "taken");
+
+        private static EventContext eventContext;
+        private static Survey survey;
+        private static string login = "Vasya";
+        private static string password = "VasyaLovesMeat";
+        private static ILoginsChecker loginChecker;
+        private static Exception exception;
+    }
+}
--- src/Tests/WB.UI.Designer.Tests/AssemblyContext.cs
+++ src/Tests/WB.UI.Designer.Tests/AssemblyContext.cs
@@ -13,7 +13,9 @@ namespace WB.UI.Designer.Tests
     {
         public void OnAssemblyStart()
         {
-            ServiceLocator.SetLocatorProvider(() => new Mock<IServiceLocator> { DefaultValue = DefaultValue.Mock }.Object);
+            var serviceLocator = new Mock<IServiceLocator> { DefaultValue = DefaultValue.Mock }.Object;
+
+            ServiceLocator.SetLocatorProvider(() => serviceLocator);
         }
 
         public void OnAssemblyComplete()
