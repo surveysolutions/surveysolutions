c174467abc6d76bcccfdc92dd9f8414843a6fe69
Andrii Kozhyn <hedgic@gmail.com>
2014-03-17 13:17:01 +0200

    Headquarter: localized edit user form. Added label to roles checkboxes KP-3165
    Former-commit-id: 8e3a87db978b520de4ba098546df02bb8090ca80

---
+++ src/UI/Headquarters/WB.UI.Headquarters/Extensions/LocalizationExtensions.cs
@@ -0,0 +1,12 @@
+﻿using WB.UI.Headquarters.Resources.Users;
+
+namespace WB.UI.Headquarters.Extensions
+{
+    public static class LocalizationExtensions
+    {
+        public static string LocalizedRoleName(this string role)
+        {
+            return UsersResources.ResourceManager.GetString("ApplicationRole_" + role);
+        }
+    }
+}
