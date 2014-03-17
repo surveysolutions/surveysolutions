4e392de8f8df45982e80f64b0b5e44020e8187d9
Slava Scherbak <slava.scherbak@gmail.com>
2014-03-17 15:57:20 +0200

    HQ\Survey\Denormalizers: added tests for SurvisorLoginsDenormalizer and SurveyDetailsViewDenormalizerTestsContext KP-3166
    Former-commit-id: 591a47e5de50fe7ca746c5388799c0523eea77b0

---
+++ src/Tests/WB.Core.BoundedContexts.Headquarters.Tests/SurvisorLoginsDenormalizerTests/when_handling_supervisor_regstered_event.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Machine.Specifications;
+using Moq;
+using Ncqrs.Eventing.ServiceModel.Bus;
+using WB.Core.BoundedContexts.Headquarters.Events.Survey;
+using WB.Core.BoundedContexts.Headquarters.Implementation.EventHandlers;
+using WB.Core.BoundedContexts.Headquarters.Views.Survey;
+using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
+using It = Machine.Specifications.It;
+using it = Moq.It;
+namespace WB.Core.BoundedContexts.Headquarters.Tests.SurvisorLoginsDenormalizerTests
+{
+    internal class when_handling_supervisor_regstered_event : SurvisorLoginsDenormalizerTestContext
+    {
+        Establish context = () =>
+        {
+            repositoryWriterMock = new Mock<IReadSideRepositoryWriter<SupervisorLoginView>>();
+            repositoryWriterMock
+                .Setup(x => x.Store(it.IsAny<SupervisorLoginView>(), login))
+                .Callback((SupervisorLoginView lw, string l) => passedView = lw);
+            denormalizer = CreateSurveyDetailsViewDenormalizer(repositoryWriter: repositoryWriterMock.Object);
+            var surveyId = Guid.Parse("11111111111111111111111111111111");
+            evnt = ToPublishedEvent(surveyId, new SupervisorRegistered(login, passwordHash));
+        };
+
+        Because of = () =>
+            denormalizer.Handle(evnt);
+
+        It should_pass_not_empty_supervisor_login_view = () => 
+            passedView.ShouldNotBeNull();
+
+        private static SupervisorLoginView passedView;
+        private static SurvisorLoginsDenormalizer denormalizer;
+        private static IPublishedEvent<SupervisorRegistered> evnt;
+        private static Mock<IReadSideRepositoryWriter<SupervisorLoginView>> repositoryWriterMock;
+        private static string login = "Sidor";
+        private static string passwordHash = "SidorsPassword";
+    }
+}
