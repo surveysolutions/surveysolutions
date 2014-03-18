4f2fb74f32426c4ef29f7f159ef29ac04cc79878
Slava Scherbak <slava.scherbak@gmail.com>
2014-03-18 19:11:02 +0200

    HQ: added  tests for password hasher and project for them KP-3166
    Former-commit-id: 719fdc0c0677a7f5d16a2fe3eb491c0f2b6886a5

---
+++ src/Tests/WB.Core.GenericSubdomains.Utils.Tests/PasswordHasherTests/PasswordHasherTestContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WB.Core.GenericSubdomains.Utils.Implementation;
+
+namespace WB.Core.GenericSubdomains.Utils.Tests.PasswordHasherTests
+{
+    internal class PasswordHasherTestContext
+    {
+        protected static PasswordHasher CreatePasswordHasher()
+        {
+            return new PasswordHasher();
+        }
+    }
+}
