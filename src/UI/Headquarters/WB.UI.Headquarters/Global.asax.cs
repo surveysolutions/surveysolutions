c1f72683d9d4f9f4c7f89a47465431fd2180e61d
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2014-03-19 12:27:22 +0200

    Headquarters: changed configuration to support webapi KP-3217
    Former-commit-id: 11f49129d865affc1b6b872b344c73d6a0e3a9ae

--- src/UI/Headquarters/WB.UI.Headquarters/Global.asax.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/Global.asax.cs
@@ -17,8 +17,7 @@ namespace WB.UI.Headquarters
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-
-            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             
