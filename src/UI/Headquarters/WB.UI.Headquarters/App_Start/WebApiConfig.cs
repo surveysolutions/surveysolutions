c1abe922f3245821ce28fda69f640cafade07180
Andrii Kozhyn <hedgic@gmail.com>
2014-03-19 12:27:22 +0200

    Headquarters: changed configuration to support webapi KP-3217
    Former-commit-id: 11f49129d865affc1b6b872b344c73d6a0e3a9ae

--- src/UI/Headquarters/WB.UI.Headquarters/App_Start/WebApiConfig.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/App_Start/WebApiConfig.cs
@@ -6,6 +6,8 @@ namespace WB.UI.Headquarters.App_Start
     {
         public static void Register(HttpConfiguration config)
         {
+            config.MapHttpAttributeRoutes();
+
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
