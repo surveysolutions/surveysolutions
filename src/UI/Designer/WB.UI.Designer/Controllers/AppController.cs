201da5a0b0e62d3b315f9778ce8fad7b5043fa04
Dima Pasko <dimapasko@gmail.com>
2014-03-19 12:54:38 +0200

    Init infrastructure for Updated Designer UI
    Former-commit-id: 99e67b9335de5e3020defae5efe807da48eb4ea1

---
+++ src/UI/Designer/WB.UI.Designer/Controllers/AppController.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+using Ncqrs.Commanding.ServiceModel;
+using WB.UI.Shared.Web.Membership;
+
+namespace WB.UI.Designer.Controllers
+{
+    public class AppController : BaseController
+    {
+        private readonly ICommandService commandService;
+        private readonly IMembershipUserService userHelper;
+
+        public AppController(
+            ICommandService commandService,
+            IMembershipUserService userHelper)
+            : base(userHelper)
+        {
+            this.commandService = commandService;
+            this.userHelper = userHelper;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
