f84e58cac5aa31f2473650ef9bf5b7b2b683be91
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2014-03-24 14:32:37 -0400

    Supervisor: hot fix of return unstale result with streaming
    --HG--
    branch : hotfixes-for-v-1-6-2
    
    
    Former-commit-id: 351e2d8e74b97c772979b3752700fd837c896717

--- src/Core/Infrastructure/WB.Core.Infrastructure.Raven/RavenWriteSideInfrastructureModule.cs
+++ src/Core/Infrastructure/WB.Core.Infrastructure.Raven/RavenWriteSideInfrastructureModule.cs
@@ -1,12 +1,8 @@
 ﻿using Ncqrs;
 using Ncqrs.Eventing.Storage;
-using Ncqrs.Eventing.Storage.RavenDB;
 using Ninject;
-using Raven.Client.Document;
-using WB.Core.Infrastructure.Implementation;
 using WB.Core.Infrastructure.Raven.Implementation;
 using WB.Core.Infrastructure.Raven.Implementation.WriteSide;
-using WB.Core.Infrastructure.ReadSide;
 
 namespace WB.Core.Infrastructure.Raven
 {
@@ -15,15 +11,12 @@ namespace WB.Core.Infrastructure.Raven
         private readonly int pageSize;
         private IStreamableEventStore singleEventStore;
         private readonly bool useStreamingForAllEvents;
-        private readonly bool useStreamingForEntity;
 
-        public RavenWriteSideInfrastructureModule(RavenConnectionSettings settings, bool useStreamingForAllEvents = true,
-            bool useStreamingForEntity = true, int pageSize = 50)
+        public RavenWriteSideInfrastructureModule(RavenConnectionSettings settings, bool useStreamingForAllEvents = true,int pageSize = 50)
             : base(settings)
         {
             this.pageSize = pageSize; 
             this.useStreamingForAllEvents = useStreamingForAllEvents;
-            this.useStreamingForEntity = useStreamingForEntity;
         }
 
         public override void Load()
@@ -41,7 +34,7 @@ namespace WB.Core.Infrastructure.Raven
             return this.singleEventStore ?? (this.singleEventStore =
                 new RavenDBEventStore(
                     this.Kernel.Get<DocumentStoreProvider>().CreateSeparateInstanceForEventStore(),
-                    this.pageSize, useStreamingForAllEvents, useStreamingForEntity));
+                    this.pageSize, useStreamingForAllEvents));
         }
     }
 }
