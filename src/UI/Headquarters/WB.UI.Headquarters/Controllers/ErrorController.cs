7bc04086c45208b99fa0829c47cdf2b720eca624
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2014-03-24 18:02:22 +0200

    Headquarters: added empty error pages KP-3230
    Former-commit-id: eb60a14f2f59ab98205b0d8d4752bf4edc2de21a

---
+++ src/UI/Headquarters/WB.UI.Headquarters/Controllers/ErrorController.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Mvc;
+
+namespace WB.UI.Headquarters.Controllers
+{
+    [AllowAnonymous]
+    public class ErrorController : Controller
+    {
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+
+        public ActionResult NotFound()
+        {
+            return this.View();
+        }
+
+        public ActionResult AccessDenied()
+        {
+            return this.View();
+        }
+
+        public ActionResult Forbidden()
+        {
+            return this.View();
+        }
+    }
+}
