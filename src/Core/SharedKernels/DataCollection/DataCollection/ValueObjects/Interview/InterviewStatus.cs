f85bc45b1ebafbe43a51c0cbc5fe6139537028ae
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2014-03-25 00:24:27 -0400

    Android: Interview creation/deleting on client application [KP-3155][KP-3227]
    Former-commit-id: 2b3e2e2e7697455c98dc7b8cdd481941e3aebcc4

--- src/Core/SharedKernels/DataCollection/DataCollection/ValueObjects/Interview/InterviewStatus.cs
+++ src/Core/SharedKernels/DataCollection/DataCollection/ValueObjects/Interview/InterviewStatus.cs
@@ -2,18 +2,19 @@
 {
     public enum InterviewStatus
     {
-        Created = 20,
-        SupervisorAssigned = 40,
         Deleted = -1,
         Restored = 0,
+
+        Created = 20,
+        SupervisorAssigned = 40,
         InterviewerAssigned = 60,
         ReadyForInterview = 80,
         SentToCapi = 85,
-        Completed = 100,
         Restarted = 95,
+        Completed = 100,
 
-        ApprovedBySupervisor = 120,
         RejectedBySupervisor = 65,
+        ApprovedBySupervisor = 120,
 
         RejectedByHeadquarters = 125,
         ApprovedByHeadquarters = 130
