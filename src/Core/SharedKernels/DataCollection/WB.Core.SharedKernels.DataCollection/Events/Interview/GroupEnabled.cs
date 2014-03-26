f9aeb0f874e3bcc8eaa1ed2e2e67d597b58a6738
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2014-03-26 18:12:15 +0200

    DataCollection: added composite group disabled/enabled events KP-3237
    --HG--
    branch : composite-interview-events
    
    
    Former-commit-id: 39b44660a4f92955d1f2d92270bc85e50236355e

--- src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/Events/Interview/GroupEnabled.cs
+++ src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/Events/Interview/GroupEnabled.cs
@@ -3,6 +3,7 @@ using WB.Core.SharedKernels.DataCollection.Events.Interview.Base;
 
 namespace WB.Core.SharedKernels.DataCollection.Events.Interview
 {
+    [Obsolete]
     public class GroupEnabled : GroupPassiveEvent
     {
         public GroupEnabled(Guid groupId, decimal[] propagationVector)
---
+++ src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/Events/Interview/GroupsDisabled.cs
@@ -0,0 +1,11 @@
+﻿using WB.Core.SharedKernels.DataCollection.Events.Interview.Base;
+using WB.Core.SharedKernels.DataCollection.Events.Interview.Dtos;
+
+namespace WB.Core.SharedKernels.DataCollection.Events.Interview
+{
+    public class GroupsDisabled : GroupsPassiveEvent
+    {
+        public GroupsDisabled(Identity[] groups)
+            : base(groups) {}
+    }
+}
