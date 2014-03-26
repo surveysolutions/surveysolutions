b5fb02cd265abeb686a1fe5a6cacea484eaa6bd9
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2014-03-26 19:12:44 +0200

    Interview: started publishing of composite answers removed event instead of separate KP-3237
    --HG--
    branch : composite-interview-events
    
    
    Former-commit-id: 6245fdad1be28291447c64b0d5b12e0e07bed539

--- src/Tests/WB.Core.SharedKernels.DataCollection.Tests/InterviewTests/when_answering_propagating_question_with_2_and_previous_answer_was_4_and_there_were_answers_given_to_propagated_questions_with_indexes_1___.cs
+++ src/Tests/WB.Core.SharedKernels.DataCollection.Tests/InterviewTests/when_answering_propagating_question_with_2_and_previous_answer_was_4_and_there_were_answers_given_to_propagated_questions_with_indexes_1___.cs
@@ -61,25 +61,25 @@ namespace WB.Core.SharedKernels.DataCollection.Tests.InterviewTests
         Because of = () =>
             interview.AnswerNumericIntegerQuestion(userId, propagatingQuestionId, new decimal[] { }, DateTime.Now, 2);
 
-        It should_not_raise_AnswerRemoved_event_with_QuestionId_equal_to_propagated_question_and_PropagationVector_equal_to_0 = () =>
-            eventContext.ShouldNotContainEvent<AnswerRemoved>(@event
-                => @event.QuestionId == propagatedQuestionId
-                && @event.PropagationVector.SequenceEqual(new decimal[] { 0 }));
+        It should_not_raise_AnswersRemoved_event_with_QuestionId_equal_to_propagated_question_and_PropagationVector_equal_to_0 = () =>
+            eventContext.ShouldNotContainEvent<AnswersRemoved>(@event => @event.Questions.Any(question
+                => question.Id == propagatedQuestionId
+                && question.RosterVector.SequenceEqual(new decimal[] { 0 })));
 
-        It should_not_raise_AnswerRemoved_event_with_QuestionId_equal_to_propagated_question_and_PropagationVector_equal_to_1 = () =>
-            eventContext.ShouldNotContainEvent<AnswerRemoved>(@event
-                => @event.QuestionId == propagatedQuestionId
-                && @event.PropagationVector.SequenceEqual(new decimal[] { 1 }));
+        It should_not_raise_AnswersRemoved_event_with_QuestionId_equal_to_propagated_question_and_PropagationVector_equal_to_1 = () =>
+            eventContext.ShouldNotContainEvent<AnswersRemoved>(@event => @event.Questions.Any(question
+                => question.Id == propagatedQuestionId
+                && question.RosterVector.SequenceEqual(new decimal[] { 1 })));
 
-        It should_raise_AnswerRemoved_event_with_QuestionId_equal_to_propagated_question_and_PropagationVector_equal_to_2 = () =>
-            eventContext.ShouldContainEvent<AnswerRemoved>(@event
-                => @event.QuestionId == propagatedQuestionId
-                && @event.PropagationVector.SequenceEqual(new decimal[] { 2 }));
+        It should_raise_AnswersRemoved_event_with_QuestionId_equal_to_propagated_question_and_PropagationVector_equal_to_2 = () =>
+            eventContext.ShouldContainEvent<AnswersRemoved>(@event => @event.Questions.Any(question
+                => question.Id == propagatedQuestionId
+                && question.RosterVector.SequenceEqual(new decimal[] { 2 })));
 
-        It should_not_raise_AnswerRemoved_event_with_QuestionId_equal_to_propagated_question_and_PropagationVector_equal_to_3 = () =>
-            eventContext.ShouldNotContainEvent<AnswerRemoved>(@event
-                => @event.QuestionId == propagatedQuestionId
-                && @event.PropagationVector.SequenceEqual(new decimal[] { 3 }));
+        It should_not_raise_AnswersRemoved_event_with_QuestionId_equal_to_propagated_question_and_PropagationVector_equal_to_3 = () =>
+            eventContext.ShouldNotContainEvent<AnswersRemoved>(@event => @event.Questions.Any(question
+                => question.Id == propagatedQuestionId
+                && question.RosterVector.SequenceEqual(new decimal[] { 3 })));
 
         Cleanup stuff = () =>
         {
