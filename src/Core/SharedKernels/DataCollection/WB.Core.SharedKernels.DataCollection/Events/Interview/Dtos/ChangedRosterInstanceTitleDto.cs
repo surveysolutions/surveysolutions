717390c52b1bd4aa18ae4369ac634c30b7b718a3
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2014-03-27 13:01:57 +0200

    DataCollection: renamed RosterRow -> RosterInstance for composite events KP-3237
    --HG--
    branch : composite-interview-events
    
    
    Former-commit-id: aa438a5c7df1cea2638a4823f31318568126dc3d

--- src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/Events/Interview/Dtos/ChangedRosterInstanceTitleDto.cs
+++ src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/Events/Interview/Dtos/ChangedRosterInstanceTitleDto.cs
@@ -1,15 +1,15 @@
 namespace WB.Core.SharedKernels.DataCollection.Events.Interview.Dtos
 {
     /// <remarks>Make sure not to reuse this class on read or write side. Use your own copies.</remarks>
-    public struct ChangedRosterRowTitleDto
+    public struct ChangedRosterInstanceTitleDto
     {
-        public RosterRowIdentity Row { get; private set; }
+        public RosterInstanceIdentity Instance { get; private set; }
         public string Title { private set; get; }
 
-        public ChangedRosterRowTitleDto(RosterRowIdentity row, string title)
+        public ChangedRosterInstanceTitleDto(RosterInstanceIdentity instance, string title)
             : this()
         {
-            this.Row = row;
+            this.Instance = instance;
             this.Title = title;
         }
     }
