81f4352baeb42276434081a7e932b3f7e7e70464
Anatoliy 'TLK' Kolesnick <none@none>
2014-03-27 13:01:57 +0200

    DataCollection: renamed RosterRow -> RosterInstance for composite events KP-3237
    --HG--
    branch : composite-interview-events
    
    
    Former-commit-id: aa438a5c7df1cea2638a4823f31318568126dc3d

---
+++ src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/Events/Interview/RosterInstancesTitleChanged.cs
@@ -0,0 +1,16 @@
+using System.Linq;
+using WB.Core.SharedKernels.DataCollection.Events.Interview.Base;
+using WB.Core.SharedKernels.DataCollection.Events.Interview.Dtos;
+
+namespace WB.Core.SharedKernels.DataCollection.Events.Interview
+{
+    public class RosterInstancesTitleChanged : InterviewPassiveEvent
+    {
+        public ChangedRosterInstanceTitleDto[] ChangedInstances { get; private set; }
+
+        public RosterInstancesTitleChanged(ChangedRosterInstanceTitleDto[] changedInstances)
+        {
+            this.ChangedInstances = changedInstances.ToArray();
+        }
+    }
+}
