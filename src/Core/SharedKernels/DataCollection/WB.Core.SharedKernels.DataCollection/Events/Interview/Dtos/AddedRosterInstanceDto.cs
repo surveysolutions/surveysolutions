f7ef7cd685c4aaef91f4f63d98a47017c0592198
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2014-03-27 13:01:57 +0200

    DataCollection: renamed RosterRow -> RosterInstance for composite events KP-3237
    --HG--
    branch : composite-interview-events
    
    
    Former-commit-id: aa438a5c7df1cea2638a4823f31318568126dc3d

--- src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/Events/Interview/Dtos/AddedRosterInstanceDto.cs
+++ src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/Events/Interview/Dtos/AddedRosterInstanceDto.cs
@@ -1,15 +1,15 @@
 namespace WB.Core.SharedKernels.DataCollection.Events.Interview.Dtos
 {
     /// <remarks>Make sure not to reuse this class on read or write side. Use your own copies.</remarks>
-    public struct AddedRosterRowDto
+    public struct AddedRosterInstanceDto
     {
-        public RosterRowIdentity Row { get; private set; }
+        public RosterInstanceIdentity Instance { get; private set; }
         public int? SortIndex { get; private set; }
 
-        public AddedRosterRowDto(RosterRowIdentity row, int? sortIndex)
+        public AddedRosterInstanceDto(RosterInstanceIdentity instance, int? sortIndex)
             : this()
         {
-            this.Row = row;
+            this.Instance = instance;
             this.SortIndex = sortIndex;
         }
     }
--- src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/Events/Interview/Dtos/RosterInstanceIdentity.cs
+++ src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/Events/Interview/Dtos/RosterInstanceIdentity.cs
@@ -3,13 +3,13 @@ using System;
 namespace WB.Core.SharedKernels.DataCollection.Events.Interview.Dtos
 {
     /// <remarks>Make sure not to reuse this class on read or write side. Use your own copies.</remarks>
-    public struct RosterRowIdentity
+    public struct RosterInstanceIdentity
     {
         public Guid GroupId { get; private set; }
         public decimal[] OuterRosterVector { get; private set; }
         public decimal RosterInstanceId { get; private set; }
 
-        public RosterRowIdentity(Guid groupId, decimal[] outerRosterVector, decimal rosterInstanceId)
+        public RosterInstanceIdentity(Guid groupId, decimal[] outerRosterVector, decimal rosterInstanceId)
             : this()
         {
             this.GroupId = groupId;
