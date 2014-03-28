118c32d298ac1ff0a8a0985f99dea4d9ef00353f
Andrii Kozhyn <hedgic@gmail.com>
2014-03-28 13:16:14 +0200

    Headquarters: implemented display of desiger questionnaires KP-3254
    Former-commit-id: 6b1b934b1673d045bde5d14d1e278cc2acb283b2

--- src/UI/Headquarters/WB.UI.Headquarters/App_Start/WebApiConfig.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/App_Start/WebApiConfig.cs
@@ -12,6 +12,11 @@ namespace WB.UI.Headquarters.App_Start
             config.Filters.Add(new UnhandledExceptionFilter());
 
             config.Routes.MapHttpRoute(
+              name: "DefaultApiWithAction",
+              routeTemplate: "api/{controller}/{action}/{id}",
+              defaults: new { id = RouteParameter.Optional });
+
+            config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
--- src/UI/Headquarters/WB.UI.Headquarters/Controllers/QuestionnaireController.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/Controllers/QuestionnaireController.cs
@@ -1,14 +1,28 @@
 ﻿using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using Main.Core.View;
 using WB.Core.BoundedContexts.Headquarters.Authentication;
+using WB.Core.SharedKernels.DataCollection.Views.Questionnaire;
+using WB.Core.SharedKernels.DataCollection.Views.Questionnaire.BrowseItem;
 
 namespace WB.UI.Headquarters.Controllers
 {
     [Authorize(Roles = ApplicationRoles.Headquarter)]
     public class QuestionnairesController : Controller
     {
+        private readonly IViewFactory<QuestionnaireBrowseInputModel, QuestionnaireBrowseView> questionnaireBrowseViewFactory;
+
+        public QuestionnairesController(IViewFactory<QuestionnaireBrowseInputModel, QuestionnaireBrowseView> questionnaireBrowseViewFactory)
+        {
+            this.questionnaireBrowseViewFactory = questionnaireBrowseViewFactory;
+        }
+
         public ActionResult Index()
         {
-            return this.View();
+            var model = this.questionnaireBrowseViewFactory.Load(new QuestionnaireBrowseInputModel {
+                PageSize = 1024
+            });
+            return this.View(model);
         }
     }
 }
