d2f08294639765a711f706112f824dddef7117ab
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2014-03-31 12:43:05 -0400

    Supervisor\Export: fix export of default with tests KP-3288
    Former-commit-id: 96d3165f6bd6da7b583e6075ec9da78d2820ce94

---
+++ src/Tests/WB.Core.BoundedContexts.Supervisor.Tests/ServiceTests/DataExport/FileBasedDataExportServiceTests/when_CreateExportedDataStructureByTemplate_is_called_and_questionnaire_has_2_rosters_with_the_same_name__.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Machine.Specifications;
+using Moq;
+using WB.Core.BoundedContexts.Supervisor.Implementation.Services.DataExport;
+using WB.Core.BoundedContexts.Supervisor.Services;
+using WB.Core.BoundedContexts.Supervisor.Views.DataExport;
+using WB.Core.Infrastructure.FileSystem;
+using It = Machine.Specifications.It;
+
+namespace WB.Core.BoundedContexts.Supervisor.Tests.ServiceTests.DataExport.FileBasedDataExportServiceTests
+{
+    internal class when_CreateExportedDataStructureByTemplate_is_called_and_questionnaire_has_2_rosters_with_the_same_name_but_different_cases :
+        FileBasedDataExportServiceTestContext
+    {
+        private Establish context = () =>
+        {
+            fileSystemAccessorMock = new Mock<IFileSystemAccessor>();
+            fileSystemAccessorMock.Setup(x => x.IsDirectoryExists(Moq.It.IsAny<string>())).Returns(false);
+            fileSystemAccessorMock.Setup(x => x.MakeValidFileName(Moq.It.IsAny<string>())).Returns<string>((fileName) => fileName);
+            environmentContentService = new Mock<IEnvironmentContentService>();
+            dataFileExportService = new Mock<IDataFileExportService>();
+
+            fileBasedDataExportService = CreateFileBasedDataExportService(fileSystemAccessorMock.Object, dataFileExportService.Object,
+                environmentContentService.Object);
+
+            questionnaireExportStructure = new QuestionnaireExportStructure();
+
+            AddLevelToExportStructure(questionnaireExportStructure, Guid.NewGuid(), "level");
+            AddLevelToExportStructure(questionnaireExportStructure, Guid.NewGuid(), "Level");
+        };
+
+        private Because of = () => fileBasedDataExportService.CreateExportedDataStructureByTemplate(questionnaireExportStructure);
+
+        private It should_GetEnvironmentContentFileName_be_called_with_parameter_level = () =>
+            environmentContentService.Verify(x => x.GetEnvironmentContentFileName("level"));
+
+        private It should_GetEnvironmentContentFileName_be_called_with_parameter_level1 = () =>
+            environmentContentService.Verify(x => x.GetEnvironmentContentFileName("level1"));
+
+        private It should_GetInterviewExportedDataFileName_be_called_with_parameter_level = () =>
+            dataFileExportService.Verify(x => x.GetInterviewExportedDataFileName("level"));
+
+        private It should_GetInterviewExportedDataFileName_be_called_with_parameter_level1 = () =>
+            dataFileExportService.Verify(x => x.GetInterviewExportedDataFileName("level1"));
+
+        private static FileBasedDataExportService fileBasedDataExportService;
+        private static QuestionnaireExportStructure questionnaireExportStructure;
+        private static Mock<IFileSystemAccessor> fileSystemAccessorMock;
+        private static Mock<IEnvironmentContentService> environmentContentService;
+        private static Mock<IDataFileExportService> dataFileExportService;
+    }
+}
