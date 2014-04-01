8e3a4280c29a80f8866f27642543b5990564c954
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2014-04-01 00:09:27 -0400

    Headquarters: Create interview partially moved [KP-3236]
    Former-commit-id: 3668019e98ba9da695c01e282686bfbd464660d2

---
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Questionnaires/Views/TakeNew/TakeNewInterviewView.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Main.Core.Documents;
+using Main.Core.Entities.SubEntities;
+using WB.Core.SharedKernels.DataCollection.Utils;
+
+namespace WB.Core.BoundedContexts.Headquarters.Questionnaires.Views.TakeNew
+{
+    public class TakeNewInterviewView
+    {
+        public TakeNewInterviewView(IQuestionnaireDocument questionnaire, long questionnaireVersion)
+        {
+            this.QuestionnaireTitle = questionnaire.Title;
+            this.QuestionnaireId = questionnaire.PublicKey;
+            this.QuestionnaireVersion = questionnaireVersion;
+            this.FeaturedQuestions = new List<FeaturedQuestionView>();
+            
+            //this.Supervisors = new List<UserDocument>();
+
+            foreach (IQuestion q in questionnaire.GetAllQuestions().Where(x => x.Featured).ToList())
+            {
+                var questionView = new FeaturedQuestionView(q, null);
+                this.FeaturedQuestions.Add(questionView);
+            }
+        }
+
+        public List<FeaturedQuestionView> FeaturedQuestions { get; set; }
+
+        public string QuestionnaireTitle { get; set; }
+
+        public Guid QuestionnaireId { get; set; }
+
+        //public List<UserDocument> Supervisors { get; set; }
+
+        public long QuestionnaireVersion { get; set; }
+    }
+}
--- src/Core/SharedKernels/DataCollection/DataCollection/Events/Questionnaire/TemplateImported.cs
+++ src/Core/SharedKernels/DataCollection/DataCollection/Events/Questionnaire/TemplateImported.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Main.Core.Documents;
+﻿using Main.Core.Documents;
 
 namespace Main.Core.Events.Questionnaire
 {
