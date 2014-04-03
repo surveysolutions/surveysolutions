b4c20c922d53c9e6952cdbd23df20b87eaf2af56
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2014-04-03 18:45:47 -0400

    Supervisor\Export: export of linked questions is fixed KP-3174
    Former-commit-id: 1680b81feaa2b7eb957d6b3d2a64ee9a105071f2

--- src/Core/BoundedContexts/Supervisor/WB.Core.BoundedContexts.Supervisor/Views/Questionnaire/ReferenceInfoByQuestion.cs
+++ src/Core/BoundedContexts/Supervisor/WB.Core.BoundedContexts.Supervisor/Views/Questionnaire/ReferenceInfoByQuestion.cs
@@ -4,13 +4,15 @@ namespace WB.Core.BoundedContexts.Supervisor.Views.Questionnaire
 {
     public class ReferenceInfoByQuestion
     {
-        public ReferenceInfoByQuestion(Guid scopeId, Guid referencedQuestionId)
+        public ReferenceInfoByQuestion(Guid scopeId, Guid referencedQuestionId, int lengthOfRosterVectorWhichNeedToBeExported)
         {
             this.ScopeId = scopeId;
             this.ReferencedQuestionId = referencedQuestionId;
+            this.LengthOfRosterVectorWhichNeedToBeExported = lengthOfRosterVectorWhichNeedToBeExported;
         }
 
         public Guid ScopeId { get; private set; }
+        public int LengthOfRosterVectorWhichNeedToBeExported { get; private set; }
         public Guid ReferencedQuestionId { get; private set; }
     }
 }
