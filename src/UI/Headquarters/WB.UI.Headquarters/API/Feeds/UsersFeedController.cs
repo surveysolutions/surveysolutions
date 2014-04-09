2fbed3cf5a4b69e6594b49228db001c36313e5a6
Andrii Kozhyn <hedgic@gmail.com>
2014-04-09 12:17:42 +0300

    HQ: added initial controller that is capable of returning atom feed.
    Former-commit-id: 6ce95c5e99ac538e1adb79ea4dcb583838b5bdb7

---
+++ src/UI/Headquarters/WB.UI.Headquarters/API/Feeds/UsersFeedController.cs
@@ -0,0 +1,30 @@
+﻿using System.ServiceModel.Syndication;
+using System.Web.Http;
+
+namespace WB.UI.Headquarters.API.Feeds
+{
+    public class UsersFeedController : ApiController
+    {
+
+        //private SyndicationItem BuildSyndicationItem(Url u)
+        //{
+        //    var item = new SyndicationItem()
+        //    {
+        //        Title = new TextSyndicationContent(u.Title),
+        //        BaseUri = new Uri(u.Address),
+        //        LastUpdatedTime = u.CreatedAt,
+        //        Content = new TextSyndicationContent(u.Description)
+        //    };
+        //    item.Authors.Add(new SyndicationPerson() { Name = u.CreatedBy });
+
+        //    return item;
+        //}
+
+        [Route("api/feeds/users/v1")]
+        [HttpGet]
+        public SyndicationFeed Index()
+        {
+            return new SyndicationFeed("Title", "Description", null);
+        }
+    }
+}
