797c2efe653f818425f9f628b9757bc7ede55ea0
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2014-04-09 12:26:11 +0300

    HQ: class cleanup
    Former-commit-id: 58bfd32b9fc831d66aaeefef00884f61093abe98

--- RavenQuestionnaire.Web/Questionnaire.Core.Web/Security/QuestionnaireMembershipProvider.cs
+++ RavenQuestionnaire.Web/Questionnaire.Core.Web/Security/QuestionnaireMembershipProvider.cs
@@ -1,22 +1,14 @@
-﻿namespace Questionnaire.Core.Web.Security
+﻿using Main.Core.View;
+using Main.Core.View.User;
+using Microsoft.Practices.ServiceLocation;
+using Ncqrs;
+using Ncqrs.Commanding.ServiceModel;
+using System;
+using System.Web;
+using System.Web.Security;
+
+namespace Questionnaire.Core.Web.Security
 {
-    using System;
-    using System.Data;
-    using System.Web;
-    using System.Web.Security;
-
-    using Main.Core.View;
-    using Main.Core.View.User;
-
-    using Microsoft.Practices.ServiceLocation;
-
-    using Ncqrs;
-    using Ncqrs.Commanding.ServiceModel;
-
-    using Ninject;
-
-    using Questionnaire.Core.Web.Helpers;
-
     public class QuestionnaireMembershipProvider : MembershipProvider
     {
         private static readonly string PERSON_TO_REQUEST = "FEF8050CBFAC46edBDF6B73C5C14DF0B.";
@@ -28,11 +20,6 @@
             get { return ServiceLocator.Current.GetInstance<IViewFactory<UserViewInputModel, UserView>>(); }
         }
 
-        #region Public Properties
-
-        /// <summary>
-        /// Gets or sets the application name.
-        /// </summary>
         public override string ApplicationName
         {
             get
@@ -51,9 +38,6 @@
             get { return NcqrsEnvironment.Get<ICommandService>(); }
         }
 
-        /// <summary>
-        /// Gets a value indicating whether enable password reset.
-        /// </summary>
         public override bool EnablePasswordReset
         {
             get
@@ -62,10 +46,6 @@
             }
         }
 
-        /// <summary>
-        /// Indicates whether the membership provider is configured to allow users to retrieve their passwords.
-        /// </summary>
-        /// <value>false</value>
         public override bool EnablePasswordRetrieval
         {
             get
@@ -74,9 +54,6 @@
             }
         }
 
-        /// <summary>
-        /// Gets the max invalid password attempts.
-        /// </summary>
         public override int MaxInvalidPasswordAttempts
         {
             get
@@ -85,9 +62,6 @@
             }
         }
 
-        /// <summary>
-        /// Gets the min required non alphanumeric characters.
-        /// </summary>
         public override int MinRequiredNonAlphanumericCharacters
         {
             get
@@ -96,9 +70,6 @@
             }
         }
 
-        /// <summary>
-        /// Gets the min required password length.
-        /// </summary>
         public override int MinRequiredPasswordLength
         {
             get
@@ -107,11 +78,6 @@
             }
         }
 
-        /// <summary>
-        /// Gets the number of minutes in which a maximum number of invalid password or 
-        /// password-answer attempts are allowed before the membership user is locked out.
-        /// </summary>
-        /// <value>10</value>
         public override int PasswordAttemptWindow
         {
             get
@@ -120,10 +86,6 @@
             }
         }
 
-        /// <summary>
-        /// Gets a value indicating the format for storing passwords in the membership data store.
-        /// </summary>
-        /// <value><see cref="MembershipPasswordFormat.Hashed" /></value>
         public override MembershipPasswordFormat PasswordFormat
         {
             get
@@ -132,9 +94,6 @@
             }
         }
 
-        /// <summary>
-        /// Gets the regular expression used to evaluate a password.
-        /// </summary>
         public override string PasswordStrengthRegularExpression
         {
             get
@@ -143,9 +102,6 @@
             }
         }
 
-        /// <summary>
-        /// Gets a value indicating whether requires question and answer.
-        /// </summary>
         public override bool RequiresQuestionAndAnswer
         {
             get
@@ -154,11 +110,6 @@
             }
         }
 
-        /// <summary>
-        /// Gets a value indicating whether the membership provider is configured to require a 
-        /// unique e-mail address for each user name.
-        /// </summary>
-        /// <value>false</value>
         public override bool RequiresUniqueEmail
         {
             get
@@ -167,90 +118,17 @@
             }
         }
 
-        #endregion
-
-        #region Public Methods and Operators
-
-        /// <summary>
-        /// The change password.
-        /// </summary>
-        /// <param name="username">
-        /// The username.
-        /// </param>
-        /// <param name="oldPassword">
-        /// The old password.
-        /// </param>
-        /// <param name="newPassword">
-        /// The new password.
-        /// </param>
-        /// <returns>
-        /// The System.Boolean.
-        /// </returns>
-        /// <exception cref="NotImplementedException">
-        /// </exception>
         public override bool ChangePassword(string username, string oldPassword, string newPassword)
         {
             throw new NotImplementedException();
         }
 
-        /// <summary>
-        /// The change password question and answer.
-        /// </summary>
-        /// <param name="username">
-        /// The username.
-        /// </param>
-        /// <param name="password">
-        /// The password.
-        /// </param>
-        /// <param name="newPasswordQuestion">
-        /// The new password question.
-        /// </param>
-        /// <param name="newPasswordAnswer">
-        /// The new password answer.
-        /// </param>
-        /// <returns>
-        /// The System.Boolean.
-        /// </returns>
-        /// <exception cref="NotImplementedException">
-        /// </exception>
         public override bool ChangePasswordQuestionAndAnswer(
             string username, string password, string newPasswordQuestion, string newPasswordAnswer)
         {
             throw new NotImplementedException();
         }
 
-        /// <summary>
-        /// The create user.
-        /// </summary>
-        /// <param name="username">
-        /// The username.
-        /// </param>
-        /// <param name="password">
-        /// The password.
-        /// </param>
-        /// <param name="email">
-        /// The email.
-        /// </param>
-        /// <param name="passwordQuestion">
-        /// The password question.
-        /// </param>
-        /// <param name="passwordAnswer">
-        /// The password answer.
-        /// </param>
-        /// <param name="isApproved">
-        /// The is approved.
-        /// </param>
-        /// <param name="providerUserKey">
-        /// The provider user key.
-        /// </param>
-        /// <param name="status">
-        /// The status.
-        /// </param>
-        /// <returns>
-        /// The System.Web.Security.MembershipUser.
-        /// </returns>
-        /// <exception cref="NotImplementedException">
-        /// </exception>
         public override MembershipUser CreateUser(
             string username, 
             string password, 
@@ -264,138 +142,38 @@
             throw new NotImplementedException();
         }
 
-        /// <summary>
-        /// The delete user.
-        /// </summary>
-        /// <param name="username">
-        /// The username.
-        /// </param>
-        /// <param name="deleteAllRelatedData">
-        /// The delete all related data.
-        /// </param>
-        /// <returns>
-        /// The System.Boolean.
-        /// </returns>
-        /// <exception cref="NotSupportedException">
-        /// </exception>
         public override bool DeleteUser(string username, bool deleteAllRelatedData)
         {
             throw new NotSupportedException("The method or operation is not implemented.");
         }
 
-        /// <summary>
-        /// The find users by email.
-        /// </summary>
-        /// <param name="emailToMatch">
-        /// The email to match.
-        /// </param>
-        /// <param name="pageIndex">
-        /// The page index.
-        /// </param>
-        /// <param name="pageSize">
-        /// The page size.
-        /// </param>
-        /// <param name="totalRecords">
-        /// The total records.
-        /// </param>
-        /// <returns>
-        /// The System.Web.Security.MembershipUserCollection.
-        /// </returns>
-        /// <exception cref="NotImplementedException">
-        /// </exception>
         public override MembershipUserCollection FindUsersByEmail(
             string emailToMatch, int pageIndex, int pageSize, out int totalRecords)
         {
             throw new NotImplementedException();
         }
 
-        /// <summary>
-        /// The find users by name.
-        /// </summary>
-        /// <param name="usernameToMatch">
-        /// The username to match.
-        /// </param>
-        /// <param name="pageIndex">
-        /// The page index.
-        /// </param>
-        /// <param name="pageSize">
-        /// The page size.
-        /// </param>
-        /// <param name="totalRecords">
-        /// The total records.
-        /// </param>
-        /// <returns>
-        /// The System.Web.Security.MembershipUserCollection.
-        /// </returns>
-        /// <exception cref="NotImplementedException">
-        /// </exception>
         public override MembershipUserCollection FindUsersByName(
             string usernameToMatch, int pageIndex, int pageSize, out int totalRecords)
         {
             throw new NotImplementedException();
         }
 
-        /// <summary>
-        /// The get all users.
-        /// </summary>
-        /// <param name="pageIndex">
-        /// The page index.
-        /// </param>
-        /// <param name="pageSize">
-        /// The page size.
-        /// </param>
-        /// <param name="totalRecords">
-        /// The total records.
-        /// </param>
-        /// <returns>
-        /// The System.Web.Security.MembershipUserCollection.
-        /// </returns>
-        /// <exception cref="NotImplementedException">
-        /// </exception>
         public override MembershipUserCollection GetAllUsers(int pageIndex, int pageSize, out int totalRecords)
         {
             throw new NotImplementedException();
         }
 
-        /// <summary>
-        /// Gets the number of users currently accessing an application. Always returns <c>0</c>.
-        /// </summary>
-        /// <returns>Always returns <c>0</c></returns>
         public override int GetNumberOfUsersOnline()
         {
             return 0;
         }
 
-        /// <summary>
-        /// Returns the password of the specified user from the database. 
-        /// The <see cref="QuestionnaireMembershipProvider"/> class does not support this method. 
-        /// </summary>
-        /// <param name="username">
-        /// The user to retrieve the password for.
-        /// </param>
-        /// <param name="answer">
-        /// The password answer for the user.
-        /// </param>
-        /// <returns>
-        /// Always throws a <see cref="NotSupportedException"/> exception.
-        /// </returns>
         public override string GetPassword(string username, string answer)
         {
             throw new NotSupportedException("The method or operation is not implemented.");
         }
 
-        /// <summary>
-        /// Gets information from the data source for a membership user.
-        /// </summary>
-        /// <param name="providerUserKey">
-        /// The unique identifier for the membership user to get information for.
-        /// </param>
-        /// <param name="userIsOnline">
-        /// <c>true</c> to update the last-activity date/time stamp for the user; <c>false</c> to return user information without updating the last-activity date/time stamp for the user.
-        /// </param>
-        /// <returns>
-        /// A <see cref="MembershipUser"/> object populated with the specified user's information from the data source.
-        /// </returns>
         public override MembershipUser GetUser(object providerUserKey, bool userIsOnline)
         {
             if (!(providerUserKey is byte[]))
@@ -421,18 +199,6 @@
             return retval;
         }
 
-        /// <summary>
-        /// Gets information from the data source for a membership user.
-        /// </summary>
-        /// <param name="username">
-        /// The name of the user to get information for.
-        /// </param>
-        /// <param name="userIsOnline">
-        /// <c>true</c> to update the last-activity date/time stamp for the user; <c>false</c> to return user information without updating the last-activity date/time stamp for the user.
-        /// </param>
-        /// <returns>
-        /// A <see cref="MembershipUser"/> object populated with the specified user's information from the data source.
-        /// </returns>
         public override MembershipUser GetUser(string username, bool userIsOnline)
         {
             if (string.IsNullOrEmpty(username))
@@ -459,81 +225,27 @@
             return retval;
         }
 
-        /// <summary>
-        /// The get user name by email.
-        /// </summary>
-        /// <param name="email">
-        /// The email.
-        /// </param>
-        /// <returns>
-        /// The System.String.
-        /// </returns>
-        /// <exception cref="NotImplementedException">
-        /// </exception>
         public override string GetUserNameByEmail(string email)
         {
             throw new NotImplementedException();
         }
 
-        /// <summary>
-        /// The reset password.
-        /// </summary>
-        /// <param name="username">
-        /// The username.
-        /// </param>
-        /// <param name="answer">
-        /// The answer.
-        /// </param>
-        /// <returns>
-        /// The System.String.
-        /// </returns>
-        /// <exception cref="NotImplementedException">
-        /// </exception>
         public override string ResetPassword(string username, string answer)
         {
             throw new NotImplementedException();
         }
 
-        /// <summary>
-        /// The unlock user.
-        /// </summary>
-        /// <param name="userName">
-        /// The user name.
-        /// </param>
-        /// <returns>
-        /// The System.Boolean.
-        /// </returns>
         public override bool UnlockUser(string userName)
         {
             // TODO   (new PersonRepository()).UpdatePersonLockStatus(userName, false);
             return false;
         }
 
-        /// <summary>
-        /// The update user.
-        /// </summary>
-        /// <param name="user">
-        /// The user.
-        /// </param>
-        /// <exception cref="NotImplementedException">
-        /// </exception>
         public override void UpdateUser(MembershipUser user)
         {
             throw new NotImplementedException();
         }
 
-        /// <summary>
-        /// The validate user.
-        /// </summary>
-        /// <param name="username">
-        /// The username.
-        /// </param>
-        /// <param name="password">
-        /// The password.
-        /// </param>
-        /// <returns>
-        /// The System.Boolean.
-        /// </returns>
         public override bool ValidateUser(string username, string password)
         {
             UserView user =
@@ -545,19 +257,6 @@
             return user != null && !user.IsLocked;
         }
 
-        #endregion
-
-        #region Methods
-
-        /// <summary>
-        /// Create new <see cref="MembershipUser"/>.
-        /// </summary>
-        /// <param name="person">
-        /// A <see cref="DataRow"/> object.
-        /// </param>
-        /// <returns>
-        /// A <see cref="MembershipUser"/> object populated with the information from the <see cref="DataRow"/>.
-        /// </returns>
         private MembershipUser MembershipInstance(UserView person)
         {
             // byte[] bsid = new System.Text.UTF8Encoding().GetBytes(person.PublicKey);
@@ -579,7 +278,5 @@
 
             return mu;
         }
-
-        #endregion
     }
 }
