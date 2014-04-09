e65a336523850aec98685ea2d9c49f3e9218d1d6
Andrii Kozhyn <hedgic@gmail.com>
2014-04-09 16:23:40 +0300

    HQ: added controller for plain users access KP-3337
    Former-commit-id: 9d18b6aca398b2d2a4b1729e9204e2222c003959

---
+++ src/UI/Headquarters/WB.UI.Headquarters/API/Resources/UsersResourceController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Security;
+using Main.Core.Documents;
+using Main.Core.Entities.SubEntities;
+using Main.Core.Utility;
+using WB.Core.GenericSubdomains.Utils;
+using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
+
+namespace WB.UI.Headquarters.API.Resources
+{
+    [RoutePrefix("api/resources/users/v1")]
+    public class UsersResourceController : ApiController
+    {
+        private readonly IQueryableReadSideRepositoryReader<UserDocument> users;
+
+        public UsersResourceController(IQueryableReadSideRepositoryReader<UserDocument> users)
+        {
+            this.users = users;
+        }
+
+        [Route("{id}", Name = "api.userDetails")]
+        [HttpGet]
+        public UserDocument Details(string id)
+        {
+            return users.GetById(id);
+        }
+
+        [Route("validateSupervisor")]
+        [HttpGet]
+        public SupervisorValidationResult ValidateSupervisor(string login, string passwordHash)
+        {
+            UserDocument userDocument = users.Query(_ => _.Where(user => user.UserName == login && user.Password == passwordHash)).FirstOrDefault();
+
+            if (userDocument == null)
+            {
+                return new SupervisorValidationResult
+                {
+                    isValid = false
+                };
+            }
+
+            var result = new SupervisorValidationResult
+            {
+                isValid = !userDocument.IsLocked && !userDocument.IsDeleted && userDocument.Roles.Contains(UserRoles.Supervisor),
+                userId =  userDocument.PublicKey.FormatGuid()
+            };
+
+            return result;
+        }
+    }
+}
