5433c12dbae47fa8c39bef1e78b9f459b316dc1c
Roman Lytvyn <litvin.roma@gmail.com>
2014-04-10 18:38:25 +0300

    designer: questionnaire details. added api method that get chapter information. KP-3245
    Former-commit-id: 079bd3cd3474b92bd09233d0de0614d9b5415b54

---
+++ src/UI/Designer/WB.UI.Designer/Api/QuestionnaireController.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Http;
+using Main.Core.View;
+using WB.Core.BoundedContexts.Designer.Views.Questionnaire.Edit;
+
+namespace WB.UI.Designer.Api
+{
+    [Authorize]
+    public class QuestionnaireController : ApiController
+    {
+        private readonly IViewFactory<ChapterInfoViewInputModel, ChapterInfoView> chapterInfoViewFactory;
+
+        public QuestionnaireController(IViewFactory<ChapterInfoViewInputModel, ChapterInfoView> chapterInfoViewFactory)
+        {
+            this.chapterInfoViewFactory = chapterInfoViewFactory;
+        }
+
+        public ChapterInfoView GetChapterInfo(ChapterInfoViewInputModel input)
+        {
+            return chapterInfoViewFactory.Load(input);
+        }
+        
+    }
+}
