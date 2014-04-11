72f13f5ff2013d65684939406e8fcaa92e73e9bf
Roman Lytvyn <litvin.roma@gmail.com>
2014-04-11 12:52:57 +0300

    designer: questionnaire details. added api method that get questionnaire information. KP-3245
    Former-commit-id: a30073c26d53f7b7d1c7ffc1b394806d392db771

--- src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Views/Questionnaire/Edit/ChapterInfoView.cs
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Views/Questionnaire/Edit/ChapterInfoView.cs
@@ -1,8 +1,4 @@
-﻿using Main.Core.Entities.SubEntities;
-using System.Collections.Generic;
-using System.Linq;
-
-namespace WB.Core.BoundedContexts.Designer.Views.Questionnaire.Edit
+﻿namespace WB.Core.BoundedContexts.Designer.Views.Questionnaire.Edit
 {
     public class ChapterInfoView : GroupInfoView
     {
@@ -10,68 +6,4 @@ namespace WB.Core.BoundedContexts.Designer.Views.Questionnaire.Edit
         {
         }
     }
-
-    public class GroupInfoView
-    {
-        public string GroupId { get; set; }
-        public string Title { get; set; }
-
-        internal readonly bool isRoster;
-        public readonly ChapterInfoStatisticsView Statistics;
-
-        public GroupInfoView(bool isRoster)
-        {
-            this.isRoster = isRoster;
-
-            this.Questions = new List<QuestionInfoView>();
-            this.Groups = new List<GroupInfoView>();
-            this.Statistics = new ChapterInfoStatisticsView(groups: this.Groups, questions: this.Questions);
-        }
-
-        public List<QuestionInfoView> Questions { get; private set; }
-        public List<GroupInfoView> Groups { get; private set; }
-    }
-
-    public class ChapterInfoStatisticsView
-    {
-        private readonly IEnumerable<GroupInfoView> groups;
-        private readonly IEnumerable<QuestionInfoView> questions;
-
-        public ChapterInfoStatisticsView(IEnumerable<GroupInfoView> groups, IEnumerable<QuestionInfoView> questions)
-        {
-            this.groups = groups;
-            this.questions = questions;
-        }
-
-        public int QuestionsCount
-        {
-            get { return this.questions.Count() + this.groups.Sum(group => group.Statistics.QuestionsCount); }
-        }
-
-        public int GroupsCount
-        {
-            get { return this.groups.Count() + this.groups.Sum(group => group.Statistics.GroupsCount); }
-        }
-
-        public int RostersCount
-        {
-            get
-            {
-                return this.groups.Count(group => @group.isRoster) +
-                       this.groups.Sum(group => group.Statistics.RostersCount);
-            }
-        }
-
-    }
-
-    public class QuestionInfoView
-    {
-        public string QuestionId { get; set; }
-        public string Title { get; set; }
-        public string Variable { get; set; }
-        public QuestionType Type { get; set; }
-        public IEnumerable<string> LinkedVariables { get; set; }
-        public IEnumerable<string> BrokenLinkedVariables { get; set; }
-    }
-
 }
--- src/UI/Designer/WB.UI.Designer/Api/QuestionnaireController.cs
+++ src/UI/Designer/WB.UI.Designer/Api/QuestionnaireController.cs
@@ -8,10 +8,18 @@ namespace WB.UI.Designer.Api
     public class QuestionnaireController : ApiController
     {
         private readonly IViewFactory<ChapterInfoViewInputModel, ChapterInfoView> chapterInfoViewFactory;
+        private readonly IViewFactory<QuestionnaireInfoViewInputModel, QuestionnaireInfoView> questionnaireInfoViewFactory;
 
-        public QuestionnaireController(IViewFactory<ChapterInfoViewInputModel, ChapterInfoView> chapterInfoViewFactory)
+        public QuestionnaireController(IViewFactory<ChapterInfoViewInputModel, ChapterInfoView> chapterInfoViewFactory,
+                                       IViewFactory<QuestionnaireInfoViewInputModel, QuestionnaireInfoView> questionnaireInfoViewFactory)
         {
             this.chapterInfoViewFactory = chapterInfoViewFactory;
+            this.questionnaireInfoViewFactory = questionnaireInfoViewFactory;
+        }
+
+        public QuestionnaireInfoView GetQuestionnaireInfo(QuestionnaireInfoViewInputModel input)
+        {
+            return questionnaireInfoViewFactory.Load(input);
         }
 
         public ChapterInfoView GetChapterInfo(ChapterInfoViewInputModel input)
