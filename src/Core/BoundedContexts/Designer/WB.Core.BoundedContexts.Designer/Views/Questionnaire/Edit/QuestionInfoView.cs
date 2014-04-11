164891de1fefebbd2c1de7961d02732dc3e3ccdc
Roman Lytvyn <none@none>
2014-04-11 12:52:57 +0300

    designer: questionnaire details. added api method that get questionnaire information. KP-3245
    Former-commit-id: a30073c26d53f7b7d1c7ffc1b394806d392db771

---
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Views/Questionnaire/Edit/QuestionInfoView.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Main.Core.Entities.SubEntities;
+
+namespace WB.Core.BoundedContexts.Designer.Views.Questionnaire.Edit
+{
+    public class QuestionInfoView
+    {
+        public string QuestionId { get; set; }
+        public string Title { get; set; }
+        public string Variable { get; set; }
+        public QuestionType Type { get; set; }
+        public IEnumerable<string> LinkedVariables { get; set; }
+        public IEnumerable<string> BrokenLinkedVariables { get; set; }
+    }
+}
