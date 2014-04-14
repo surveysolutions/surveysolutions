4fb38fff31f6a95e922f5239c3a4c34b05703322
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2014-04-14 14:27:24 -0400

    Fix approve/reject interview (additional commit) [KP-3365]
    --HG--
    branch : hotfixes-for-v-1-6-2
    
    
    Former-commit-id: 80f538c52bd6139826309725c346464598e40f8b

--- src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/Events/Interview/Dtos/Identity.cs
+++ src/Core/SharedKernels/DataCollection/WB.Core.SharedKernels.DataCollection/Events/Interview/Dtos/Identity.cs
@@ -3,13 +3,12 @@
 namespace WB.Core.SharedKernels.DataCollection.Events.Interview.Dtos
 {
     /// <remarks>Make sure not to reuse this class on read or write side. Use your own copies.</remarks>
-    public struct Identity
+    public class Identity
     {
         public Guid Id { get; private set; }
         public decimal[] RosterVector { get; private set; }
 
-        public Identity(Guid id, decimal[] rosterVector)
-            : this()
+        public Identity(Guid id, decimal[] rosterVector)            
         {
             this.Id = id;
             this.RosterVector = rosterVector;
