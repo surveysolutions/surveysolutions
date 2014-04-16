f03835a2ef38afd341e69aac1652320d98b7fce9
Andrii Kozhyn <hedgic@gmail.com>
2014-04-16 11:33:24 +0300

    HQ: implemented resource for interview data KP-3326
    Former-commit-id: 4f4a083ddec05400ca8fde903cb0bfc49279d926

---
+++ src/UI/Headquarters/WB.UI.Headquarters/API/Resources/InterviewsResourceController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Web.Http;
+using WB.Core.Infrastructure.FunctionalDenormalization.Implementation.ReadSide;
+using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
+using WB.Core.SharedKernels.DataCollection.DataTransferObjects.Synchronization;
+using WB.Core.SharedKernels.DataCollection.ValueObjects.Interview;
+using WB.Core.SharedKernels.SurveyManagement.Factories;
+using WB.Core.SharedKernels.SurveyManagement.Views.Interview;
+
+namespace WB.UI.Headquarters.API.Resources
+{
+    [RoutePrefix("api/resources/interviews/v1")]
+    public class InterviewsResourceController : ApiController
+    {
+        private readonly IReadSideRepositoryReader<ViewWithSequence<InterviewData>> interviewDataReader;
+        private readonly IInterviewSynchronizationDtoFactory factory;
+
+        public InterviewsResourceController(IReadSideRepositoryReader<ViewWithSequence<InterviewData>> interviewDataReader,
+            IInterviewSynchronizationDtoFactory factory)
+        {
+            this.interviewDataReader = interviewDataReader;
+            this.factory = factory;
+        }
+
+        [Route("{id}", Name = "api.interviewDetails")]
+        public InterviewSynchronizationDto Get(string id)
+        {
+            var interviewData = this.interviewDataReader.GetById(id);
+
+            InterviewData document = interviewData.Document;
+            InterviewSynchronizationDto interviewSynchronizationDto = 
+                factory.BuildFrom(document, Guid.NewGuid(), InterviewStatus.ApprovedByHeadquarters);
+            
+            return interviewSynchronizationDto;
+        }
+    }
+}
--- src/UI/Headquarters/WB.UI.Headquarters/Models/CompleteQuestionSettings.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/Models/CompleteQuestionSettings.cs
@@ -2,23 +2,12 @@
 
 namespace WB.UI.Headquarters.Models
 {
-    /// <summary>
-    /// Define complete question settings
-    /// </summary>
     public class CompleteQuestionSettings
     {
         public Guid QuestionnaireId { get; set; }
-        /// Gets or sets QuestionnaireId.
-        /// </summary>
-
-        /// <summary>
-        /// Gets or sets ParentGroupPublicKey.
-        /// </summary>
+        
         public Guid ParentGroupPublicKey { get; set; }
 
-        /// <summary>
-        /// Gets or sets PropogationPublicKey.
-        /// </summary>
         public Guid? PropogationPublicKey { get; set; }
     }
 }
