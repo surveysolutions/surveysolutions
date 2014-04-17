10abb030760e50563dc0dc87b8787f5289acfeec
Roman Lytvyn <litvin.roma@gmail.com>
2014-04-17 11:47:02 +0300

    designer: questionnaire details page. added chapters info denormalizer. KP-3245
    Former-commit-id: 4ca71a14ab5190c6ac73e425b259683279aed4c2

--- src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Views/Questionnaire/Edit/ChapterInfo/GroupInfoView.cs
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Views/Questionnaire/Edit/ChapterInfo/GroupInfoView.cs
@@ -1,8 +1,9 @@
 using System.Collections.Generic;
+using WB.Core.Infrastructure.ReadSide.Repository;
 
 namespace WB.Core.BoundedContexts.Designer.Views.Questionnaire.Edit.ChapterInfo
 {
-    public class GroupInfoView : GroupInfoStatisticsView
+    public class GroupInfoView : GroupInfoStatisticsView, IReadSideRepositoryEntity
     {
         public string GroupId { get; set; }
         public string Title { get; set; }
