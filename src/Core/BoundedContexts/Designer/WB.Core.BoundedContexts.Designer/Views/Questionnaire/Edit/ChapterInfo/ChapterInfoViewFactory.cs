c3e0401db36b4984e4b41c15b1f9aa970c4aced2
Roman Lytvyn <none@none>
2014-04-17 11:41:57 +0300

    designer: questionnaire details page. changed view factory. KP-3244
    Former-commit-id: 158658a1232def682c0b8af3e7655c73bc17edf0

--- src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Views/Questionnaire/Edit/ChapterInfo/ChapterInfoViewFactory.cs
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Views/Questionnaire/Edit/ChapterInfo/ChapterInfoViewFactory.cs
@@ -1,84 +1,22 @@
-﻿using System.Linq;
-using Main.Core.Documents;
-using Main.Core.Entities.SubEntities;
-using Main.Core.View;
-using WB.Core.GenericSubdomains.Utils;
+﻿using Main.Core.View;
 using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
-using WB.Core.SharedKernels.ExpressionProcessor.Services;
 
 namespace WB.Core.BoundedContexts.Designer.Views.Questionnaire.Edit.ChapterInfo
 {
-    public class ChapterInfoViewFactory : IViewFactory<ChapterInfoViewInputModel, ChapterInfoView>
+    public class ChapterInfoViewFactory : IViewFactory<ChapterInfoViewInputModel, GroupInfoView>
     {
-        private readonly IReadSideRepositoryReader<QuestionnaireDocument> questionnaireStorage;
-        private readonly IExpressionProcessor expressionProcessor;
+        private readonly IReadSideRepositoryReader<GroupInfoView> readSideReader;
 
-        public ChapterInfoViewFactory(IReadSideRepositoryReader<QuestionnaireDocument> questionnaireStorage,
-            IExpressionProcessor expressionProcessor)
+        public ChapterInfoViewFactory(IReadSideRepositoryReader<GroupInfoView> readSideReader)
         {
-            this.questionnaireStorage = questionnaireStorage;
-            this.expressionProcessor = expressionProcessor;
+            this.readSideReader = readSideReader;
         }
 
-        public ChapterInfoView Load(ChapterInfoViewInputModel input)
+        public GroupInfoView Load(ChapterInfoViewInputModel input)
         {
-            var questionnaire = this.questionnaireStorage.GetById(input.QuestionnaireId);
-            if (questionnaire == null) return null;
-
-            var chapter =
-                questionnaire.FirstOrDefault<IGroup>(group => group.PublicKey.FormatGuid().Equals(input.ChapterId));
-            if (chapter == null) return null;
-
-            var chapterInfoView = new ChapterInfoView {Title = chapter.Title, ChapterId = chapter.PublicKey.FormatGuid()};
-
-            FillGroupsFromChapter(questionnaire: questionnaire, chapterOrGroup: chapter,
-                currentGroupInfoView: null);
-
-            return chapterInfoView;
-        }
-
-        private void FillGroupsFromChapter(QuestionnaireDocument questionnaire, IGroup chapterOrGroup,
-             GroupInfoView currentGroupInfoView)
-        {
-            foreach (var groupOrQuestion in chapterOrGroup.Children)
-            {
-                var group = groupOrQuestion as IGroup;
-                if (group != null)
-                {
-                    var childGroupInfoView = new GroupInfoView()
-                    {
-                        GroupId = group.PublicKey.FormatGuid(),
-                        Title = group.Title
-                    };
-                    currentGroupInfoView.Groups.Add(childGroupInfoView);
-
-                    FillGroupsFromChapter(questionnaire: questionnaire, chapterOrGroup: group,
-                        currentGroupInfoView:  childGroupInfoView);
-                }
-
-                var question = groupOrQuestion as IQuestion;
-                if (question != null)
-                {
-                    var questionsUsedInConditionExpression = string.IsNullOrEmpty(question.ConditionExpression)
-                        ? new string[0]
-                        : expressionProcessor.GetIdentifiersUsedInExpression(question.ConditionExpression);
-
-                    currentGroupInfoView.Questions.Add(new QuestionInfoView()
-                    {
-                        QuestionId = question.PublicKey.FormatGuid(),
-                        Title = question.QuestionText,
-                        Variable = question.StataExportCaption,
-                        Type = question.QuestionType,
-                        LinkedVariables = questionsUsedInConditionExpression,
-                        BrokenLinkedVariables =
-                            questionsUsedInConditionExpression.Where(
-                                variable =>
-                                    questionnaire.FirstOrDefault<IQuestion>(
-                                        dependentQuestion => dependentQuestion.StataExportCaption.Equals(variable)) ==
-                                    null)
-                    });
-                }
-            }
+            return
+                this.readSideReader.GetById(input.QuestionnaireId)
+                    .Groups.Find(chapter => chapter.GroupId == input.ChapterId);
         }
     }
 }
