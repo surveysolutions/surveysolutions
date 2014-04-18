7abfc023e9db746d595ac9be3a4075271c225e4d
Roman Lytvyn <litvin.roma@gmail.com>
2014-04-18 15:01:28 +0300

    designer: questionnaire details page. updated chapter info denormalizer. KP-3244
    Former-commit-id: d8bbe3d274ff6cbf9f537b9a51298a4a3cec13c2

--- src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Views/Questionnaire/Edit/ChapterInfo/QuestionInfoView.cs
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Views/Questionnaire/Edit/ChapterInfo/QuestionInfoView.cs
@@ -3,9 +3,9 @@ using Main.Core.Entities.SubEntities;
 
 namespace WB.Core.BoundedContexts.Designer.Views.Questionnaire.Edit.ChapterInfo
 {
-    public class QuestionInfoView
+    public class QuestionInfoView : IQuestionnaireItem
     {
-        public string QuestionId { get; set; }
+        public string Id { get; set; }
         public string Title { get; set; }
         public string Variable { get; set; }
         public QuestionType Type { get; set; }
