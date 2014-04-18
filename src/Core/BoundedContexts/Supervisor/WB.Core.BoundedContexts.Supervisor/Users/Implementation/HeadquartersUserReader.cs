195f62677071bcf5b6875d6d465fb2ed974fb10a
Anatoliy 'TLK' Kolesnick <none@none>
2014-04-18 18:56:50 +0300

    Supervisor,Headquarters: fixed synchronization issues KP-3330
    Former-commit-id: 83252ba6ec3ef4684ea57d586db125b6bcc0df49

--- src/Core/BoundedContexts/Supervisor/WB.Core.BoundedContexts.Supervisor/Users/Implementation/HeadquartersUserReader.cs
+++ src/Core/BoundedContexts/Supervisor/WB.Core.BoundedContexts.Supervisor/Users/Implementation/HeadquartersUserReader.cs
@@ -3,11 +3,15 @@ using System.Threading.Tasks;
 using Main.Core.Documents;
 using Main.Core.View.User;
 using WB.Core.BoundedContexts.Supervisor.Synchronization.Implementation;
+using WB.Core.SharedKernel.Utils.Serialization;
 
 namespace WB.Core.BoundedContexts.Supervisor.Users.Implementation
 {
     internal class HeadquartersUserReader : HeadquartersEntityReader, IHeadquartersUserReader
     {
+        public HeadquartersUserReader(IJsonUtils jsonUtils)
+            : base(jsonUtils) {}
+
         public async Task<UserView> GetUserByUri(Uri headquartersUserUri)
         {
             return await GetEntityByUri<UserView>(headquartersUserUri).ConfigureAwait(false);
