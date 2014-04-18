e43f83c9b73e765abce0368cebdad406606fbb85
Roman Lytvyn <litvin.roma@gmail.com>
2014-04-18 19:21:01 +0300

    designer: questionnaire details page. added tests for questionnaire info view factory. KP-3244
    Former-commit-id: 61fcd636415680854bfd47ef827d9723c2f93056

---
+++ src/Tests/WB.Core.BoundedContexts.Designer.Tests/QuestionnaireInfoViewFactoryTests/QuestionnaireInfoViewFactoryContext.cs
@@ -0,0 +1,27 @@
+﻿using Moq;
+using WB.Core.BoundedContexts.Designer.Views.Questionnaire.Edit.QuestionnaireInfo;
+using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
+
+namespace WB.Core.BoundedContexts.Designer.Tests.QuestionnaireInfoViewFactoryTests
+{
+    internal class QuestionnaireInfoViewFactoryContext
+    {
+        protected static QuestionnaireInfoView CreateQuestionnaireInfoView(string questionnaireId, string questionnaireTitle)
+        {
+            return new QuestionnaireInfoView() {QuestionnaireId = questionnaireId, Title = questionnaireTitle};
+        }
+
+        protected static QuestionnaireInfoViewInputModel CreateQuestionnaireInfoViewInputModel(string questionnaireId)
+        {
+            return new QuestionnaireInfoViewInputModel() {QuestionnaireId = questionnaireId};
+        }
+
+        protected static QuestionnaireInfoViewFactory CreateQuestionnaireInfoViewFactory(
+            IQueryableReadSideRepositoryReader<QuestionnaireInfoView> repository = null)
+        {
+            return
+                new QuestionnaireInfoViewFactory(repository ??
+                                                 Mock.Of<IQueryableReadSideRepositoryReader<QuestionnaireInfoView>>());
+        }
+    }
+}
