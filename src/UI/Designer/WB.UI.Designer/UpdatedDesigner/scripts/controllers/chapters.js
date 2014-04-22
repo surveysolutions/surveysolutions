17164588f77ab95170b49b88db8f7d4e87c05d28
Dima Pasko <dimapasko@gmail.com>
2014-04-22 15:59:10 +0300

    Designer: extrtact chapters controller KP-3247 UI: Chapters UI
    Former-commit-id: ad2b7db2a96fd30e28076665205efe21ed806423

---
+++ src/UI/Designer/WB.UI.Designer/UpdatedDesigner/scripts/controllers/chapters.js
@@ -0,0 +1,71 @@
+﻿'use strict';
+
+angular.module('designerApp')
+    .controller('ChaptersCtrl', function ($scope, $routeParams, $location, $route, commandService) {
+
+        $scope.chapters = [];
+
+        $scope.isFolded = false;
+
+        $scope.unfold = function () {
+            $scope.isFolded = true;
+        };
+
+        $scope.foldback = function () {
+            $scope.isFolded = false;
+        };
+
+        $scope.addNewChapter = function () {
+            var newId = guid();
+
+            var newChapter = {
+                Title: 'New Chapter',
+                ChapterId: newId,
+                QuestionsCount: 0,
+                GroupsCount: 0,
+                RostersCount: 0
+            };
+
+            commandService.addGroup($routeParams.questionnaireId, newChapter).success(function () {
+                $scope.questionnaire.Chapters.push(newChapter);
+            });
+        };
+
+        $scope.cloneChapter = function (chapter) {
+            var newId = guid();
+            var chapterDescription = "";
+
+            commandService.cloneGroupWithoutChildren($routeParams.questionnaireId, newId, chapter, chapterDescription).success(function () {
+                var newChapter = {
+                    Title: chapter.Title,
+                    ChapterId: newId,
+                    QuestionsCount: 0,
+                    GroupsCount: 0,
+                    RostersCount: 0
+                };
+                $scope.questionnaire.Chapters.push(newChapter);
+            });
+        };
+
+        $scope.deleteChapter = function (chapter) {
+            if (confirm("Are you sure want to delete?")) {
+                commandService.deleteGroup($routeParams.questionnaireId, chapter).success(function () {
+                    var index = $scope.questionnaire.Chapters.indexOf(chapter);
+                    if (index > -1) {
+                        $scope.questionnaire.Chapters.splice(index, 1);
+                    }
+                });
+            }
+        };
+
+        function guid() {
+            function s4() {
+                return Math.floor((1 + Math.random()) * 0x10000)
+                    .toString(16)
+                    .substring(1);
+            }
+
+            return s4() + s4() + s4() + s4() +
+                s4() + s4() + s4() + s4();
+        };
+    });
--- src/UI/Designer/WB.UI.Designer/UpdatedDesigner/scripts/controllers/main.js
+++ src/UI/Designer/WB.UI.Designer/UpdatedDesigner/scripts/controllers/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('designerApp')
-    .controller('MainCtrl', function ($scope, $routeParams, $location, $route, commandService, questionnaireService, verificationService) {
+    .controller('MainCtrl', function ($scope, $routeParams, $location, $route, questionnaireService, verificationService) {
 
         $scope.chapters = [];
 
@@ -16,14 +16,6 @@ angular.module('designerApp')
             errors: []
         };
 
-        $scope.isFolded = false;
-
-        $scope.changeChapter = function(chapter) {
-            $location.path('/' + $routeParams.questionnaireId + '/chapter/' + chapter.ChapterId);
-            $scope.currentChapterId = chapter.ChapterId;
-            loadChapterDetails($routeParams.questionnaireId, $scope.currentChapterId);
-        };
-
         $scope.setItem = function (item) {
             $location.path('/' + $routeParams.questionnaireId + '/chapter/' + $scope.currentChapterId + '/item/' + item.Id);
             $scope.currentItemId = item.Id;
@@ -33,62 +25,11 @@ angular.module('designerApp')
             console.log('submit');
         };
 
-        $scope.unfold = function() {
-            $scope.isFolded = true;
-        };
-
-        $scope.foldback = function() {
-            $scope.isFolded = false;
-        };
-
         $scope.verify = function() {
             verificationService.verify($routeParams.questionnaireId).success(function (result) {
                     $scope.verificationStatus.errors = result.errors;
                     $scope.verificationStatus.errorsCount = result.errors.length;
                 });
-        };
-
-        $scope.addNewChapter = function() {
-            var newId = guid();
-
-            var newChapter = {
-                Title: 'New Chapter',
-                ChapterId: newId,
-                QuestionsCount: 0,
-                GroupsCount: 0,
-                RostersCount: 0
-            };
-
-            commandService.addGroup($routeParams.questionnaireId, newChapter).success(function () {
-                $scope.questionnaire.Chapters.push(newChapter);
-            });
-        };
-
-        $scope.cloneChapter = function(chapter) {
-            var newId = guid();
-            var chapterDescription = "";
-
-            commandService.cloneGroupWithoutChildren($routeParams.questionnaireId, newId, chapter, chapterDescription).success(function () {
-                var newChapter = {
-                    Title: chapter.Title,
-                    ChapterId: newId,
-                    QuestionsCount: 0,
-                    GroupsCount: 0,
-                    RostersCount: 0
-                };
-                $scope.questionnaire.Chapters.push(newChapter);
-            });
-        };
-
-        $scope.deleteChapter = function(chapter) {
-            if (confirm("Are you sure want to delete?")) {
-                commandService.deleteGroup($routeParams.questionnaireId, chapter).success(function () {
-                    var index = $scope.questionnaire.Chapters.indexOf(chapter);
-                    if (index > -1) {
-                        $scope.questionnaire.Chapters.splice(index, 1);
-                    }
-                });
-            }
         };
 
         $scope.editChapter = function(chapter) {
