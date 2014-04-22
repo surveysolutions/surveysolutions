94ff2c37186cb08041d31ba267b2b29262294f38
Roman Lytvyn <litvin.roma@gmail.com>
2014-04-22 19:54:13 +0300

    designer: questionnaire details page. added tests for questionnaire info view denormalizer. KP-3244
    Former-commit-id: fdd03db9eff6c84f8340e1bb891ddb30960ec644

---
+++ src/Tests/WB.Core.BoundedContexts.Designer.Tests/QuestionnaireInfoViewDenormalizerTests/when_QuestionnaireUpdated_event_received.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Machine.Specifications;
+using Main.Core.Events.Questionnaire;
+using WB.Core.BoundedContexts.Designer.Views.Questionnaire.Edit.QuestionnaireInfo;
+
+namespace WB.Core.BoundedContexts.Designer.Tests.QuestionnaireInfoViewDenormalizerTests
+{
+    internal class when_QuestionnaireUpdated_event_received : QuestionnaireInfoViewDenormalizerTestContext
+    {
+        Establish context = () =>
+        {
+            viewState = CreateQuestionnaireInfoView();
+            denormalizer = CreateDenormalizer();
+        };
+
+        Because of = () =>
+            viewState =
+                denormalizer.Update(viewState,
+                    CreatePublishableEvent(new QuestionnaireUpdated() {Title = questionnaireTitle},
+                        new Guid(questionnaireId)));
+
+        It should_questionnnaireInfoView_Title_be_equal_to_questionnaireTitle = () =>
+            viewState.Title.ShouldEqual(questionnaireTitle);
+
+        private static string questionnaireId = "33333333333333333333333333333333";
+        private static string questionnaireTitle = "questionnaire title";
+        
+        private static QuestionnaireInfoViewDenormalizer denormalizer;
+        private static QuestionnaireInfoView viewState;
+    }
+}
