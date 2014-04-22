07ac776cc1a6e5f44e9ac9b03f43f78a1f58f0af
Roman Lytvyn <litvin.roma@gmail.com>
2014-04-22 11:07:20 +0300

    designer: questionnaire details page. added tests for chapter info view factory. KP-3245
    Former-commit-id: d129f17d0cd62fd4e32e73843242c753e38a2e3f

---
+++ src/Tests/WB.Core.BoundedContexts.Designer.Tests/ChapterInfoViewFactoryTests/when_loading_view_and_chapter_exists.cs
@@ -0,0 +1,39 @@
+﻿using Machine.Specifications;
+using Moq;
+using WB.Core.BoundedContexts.Designer.Views.Questionnaire.Edit.ChapterInfo;
+using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
+using It = Machine.Specifications.It;
+
+namespace WB.Core.BoundedContexts.Designer.Tests.ChapterInfoViewFactoryTests
+{
+    internal class when_loading_view_and_chapter_exists : ChapterInfoViewFactoryContext
+    {
+        Establish context = () =>
+        {
+            input = CreateChapterInfoViewInputModel(questionnaireId, chapterId);
+
+            var repositoryMock = new Mock<IQueryableReadSideRepositoryReader<GroupInfoView>>();
+
+            repositoryMock
+                .Setup(x => x.GetById(questionnaireId))
+                .Returns(CreateChapterInfoView(questionnaireId, chapterId));
+
+            factory = CreateChapterInfoViewFactory(repository: repositoryMock.Object);
+        };
+
+        Because of = () =>
+            view = factory.Load(input);
+
+        It should_find_chapter = () =>
+            view.ShouldNotBeNull();
+
+        It should_chapter_id_be_equal_chapterId = () =>
+            view.Id.ShouldEqual(chapterId);
+
+        private static IQuestionnaireItem view;
+        private static ChapterInfoViewInputModel input;
+        private static ChapterInfoViewFactory factory;
+        private static string questionnaireId = "11111111111111111111111111111111";
+        private static string chapterId = "22222222222222222222222222222222";
+    }
+}
---
+++ src/Tests/WB.Core.BoundedContexts.Designer.Tests/ChapterInfoViewFactoryTests/when_loading_view_and_chapter_is_absent.cs
@@ -0,0 +1,36 @@
+﻿using Machine.Specifications;
+using Moq;
+using WB.Core.BoundedContexts.Designer.Views.Questionnaire.Edit.ChapterInfo;
+using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
+using It = Machine.Specifications.It;
+
+namespace WB.Core.BoundedContexts.Designer.Tests.ChapterInfoViewFactoryTests
+{
+    internal class when_loading_view_and_chapter_is_absent : ChapterInfoViewFactoryContext
+    {
+        Establish context = () =>
+        {
+            input = CreateChapterInfoViewInputModel(questionnaireId, chapterId);
+
+            var repositoryMock = new Mock<IQueryableReadSideRepositoryReader<GroupInfoView>>();
+
+            repositoryMock
+                .Setup(x => x.GetById(questionnaireId))
+                .Returns(CreateChapterInfoViewWithoutChapters(questionnaireId, chapterId));
+
+            factory = CreateChapterInfoViewFactory(repository: repositoryMock.Object);
+        };
+
+        Because of = () =>
+            view = factory.Load(input);
+
+        It should_chapter_be_null = () =>
+            view.ShouldBeNull();
+
+        private static IQuestionnaireItem view;
+        private static ChapterInfoViewInputModel input;
+        private static ChapterInfoViewFactory factory;
+        private static string questionnaireId = "11111111111111111111111111111111";
+        private static string chapterId = "22222222222222222222222222222222";
+    }
+}
