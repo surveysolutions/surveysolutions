a1d79cd741757f0f6ca8e483fe37c1330478f502
Roman Lytvyn <litvin.roma@gmail.com>
2014-04-23 13:23:17 +0300

    designer: questionnaire details page. chapter info view denormalizer. added tests. KP-3245
    Former-commit-id: 550efd30f5b61917e1fe407f3222a036af85b9b0

---
+++ src/Tests/WB.Core.BoundedContexts.Designer.Tests/ChapterInfoViewDenormalizerTests/when_QuestionDeleted_event_received.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Machine.Specifications;
+using Main.Core.Events.Questionnaire;
+using WB.Core.BoundedContexts.Designer.Views.Questionnaire.Edit.ChapterInfo;
+
+namespace WB.Core.BoundedContexts.Designer.Tests.ChapterInfoViewDenormalizerTests
+{
+    internal class when_QuestionDeleted_event_received : ChaptersInfoViewDenormalizerTestContext
+    {
+        Establish context = () =>
+        {
+            viewState = CreateGroupInfoViewWith1ChapterAnd1QuestionInsideChapter(chapterId, questionId);
+            denormalizer = CreateDenormalizer(viewState);
+        };
+
+        Because of = () =>
+            viewState =
+                denormalizer.Update(viewState,
+                    CreatePublishableEvent(new QuestionDeleted()
+                    {
+                        QuestionId = Guid.Parse(questionId)
+                    }));
+
+        It should_groupInfoView_first_chapter_items_be_empty = () =>
+            ((GroupInfoView)viewState.Items[0]).Items.ShouldBeEmpty();
+
+        private static string chapterId = "33333333333333333333333333333333";
+        private static string questionId = "22222222222222222222222222222222";
+        
+        private static ChaptersInfoViewDenormalizer denormalizer;
+        private static GroupInfoView viewState;
+    }
+}
