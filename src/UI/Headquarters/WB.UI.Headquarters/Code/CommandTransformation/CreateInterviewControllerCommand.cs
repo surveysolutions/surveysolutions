292efb0a9bd08fc8abed5767a38af20bc313d35f
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2014-04-23 13:11:45 +0300

    DataCollection: reverted fix of interview creation command KP-3317 Backed out changeset: 3575613e0501
    Former-commit-id: bfe7a57a4c4f7e7408cc4faf5d3633f6524adea0

--- src/UI/Headquarters/WB.UI.Headquarters/Code/CommandTransformation/CreateInterviewControllerCommand.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/Code/CommandTransformation/CreateInterviewControllerCommand.cs
@@ -6,7 +6,6 @@ namespace WB.UI.Headquarters.Code.CommandTransformation
     internal class CreateInterviewControllerCommand : IntreviewCommand
     {
         public Guid QuestionnaireId { get; set; }
-        public long QuestionnaireVersion { get; set; }
         public Guid SupervisorId { get; set; }
         public List<UntypedQuestionAnswer> AnswersToFeaturedQuestions { get; set; }
     }
