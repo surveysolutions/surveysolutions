27b5ad0061b61ff95cee2df83f9041cdba739a83
Slava Scherbak <slava.scherbak@gmail.com>
2014-04-25 11:29:25 +0300

    Designer: removed manual binding for IQuestionnaireInfoFactory (because it cause duplicating of automate binding) KP-3378
    Former-commit-id: 2bb30fd52ba0c1746d1993dbbb3f95bd7969a928

--- src/Tests/WB.UI.Designer.Tests/QuestionnaireApiControllerTests/when_getting_absent_questionnaire.cs
+++ src/Tests/WB.UI.Designer.Tests/QuestionnaireApiControllerTests/when_getting_absent_questionnaire.cs
@@ -1,10 +1,36 @@
 using System;
+using System.Net;
 using System.Web;
 using Machine.Specifications;
 using WB.UI.Designer.Api;
 
 namespace WB.UI.Designer.Tests.QuestionnaireApiControllerTests
 {
+    internal class when_getting_edit_question_info_and_question_is_absent : QuestionnaireApiControllerTestContext
+    {
+        Establish context = () =>
+        {
+            controller = CreateQuestionnaireController();
+        };
+
+        Because of = () =>
+            exception = Catch.Exception(() => controller.EditQuestion(questionnaireId, questionId));
+
+        It should_throw_exception = () =>
+            exception.ShouldNotBeNull();
+
+        It should_throw_HttpException_exception = () =>
+            exception.ShouldBeOfExactType(typeof(HttpException));
+
+        It should_throw_HttpException_exception1 = () =>
+            (exception as HttpException).GetHttpCode().ShouldEqual((int)HttpStatusCode.NotFound);
+
+        private static QuestionnaireController controller;
+        private static Exception exception;
+        private static string questionnaireId = "22222222222222222222222222222222";
+        private static Guid questionId = Guid.Parse("11111111111111111111111111111111");
+    }
+
     internal class when_getting_absent_questionnaire : QuestionnaireApiControllerTestContext
     {
         Establish context = () =>
