d79e2425d591031fb60ab1059a01c4125ecaf252
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2014-04-25 14:06:52 +0300

    HQ: added interview rejected event to feed KP-3368
    Former-commit-id: 148446acc08e682645e40a4d3c93daedf2929520

--- src/Core/SharedKernels/SurveyManagement/SurveyManagement/Synchronization/Interview/EntryType.cs
+++ src/Core/SharedKernels/SurveyManagement/SurveyManagement/Synchronization/Interview/EntryType.cs
@@ -3,6 +3,7 @@ namespace WB.Core.SharedKernels.SurveyManagement.Synchronization.Interview
     public enum EntryType
     {
         SupervisorAssigned = 1,
-        InterviewUnassigned = 2
+        InterviewUnassigned = 2,
+        InterviewRejected = 3
     }
 }
---
+++ src/Tests/WB.Core.BoundedContexts.Headquarters.Tests/Create.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Ncqrs.Eventing;
+using Ncqrs.Eventing.ServiceModel.Bus;
+using NSubstitute;
+using WB.Core.BoundedContexts.Headquarters.Interviews.Denormalizers;
+using WB.Core.Infrastructure.FunctionalDenormalization.Implementation.ReadSide;
+using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
+using WB.Core.SharedKernels.SurveyManagement.Synchronization.Interview;
+using WB.Core.SharedKernels.SurveyManagement.Views.Interview;
+
+namespace WB.Core.BoundedContexts.Headquarters.Tests
+{
+    internal static class Create
+    {
+        public static InterviewsFeedDenormalizer InterviewsFeedDenormalizer(IReadSideRepositoryWriter<InterviewFeedEntry> feedEntryWriter = null, 
+            IReadSideRepositoryWriter<ViewWithSequence<InterviewData>> interviewsRepository = null)
+        {
+            return new InterviewsFeedDenormalizer(feedEntryWriter ?? Substitute.For<IReadSideRepositoryWriter<InterviewFeedEntry>>(),
+                interviewsRepository ?? Substitute.For<IReadSideRepositoryWriter<ViewWithSequence<InterviewData>>>());
+        }
+
+        public static IPublishedEvent<T> PublishedEvent<T>(Guid eventSourceId, T evnt)
+        {
+            IPublishedEvent<T> e = new PublishedEvent<T>(new UncommittedEvent(Guid.NewGuid(),
+                                                                              eventSourceId,
+                                                                              1,
+                                                                              1,
+                                                                              DateTime.Now,
+                                                                              evnt,
+                                                                              new Version(1, 0))
+                );
+            return e;
+        }
+    }
+}
