621302067597f79582b36c5c08d565b1d50bd08f
Slava Scherbak <slava.scherbak@gmail.com>
2014-04-28 19:23:50 +0300

    Designer: added tests for QuestionnaireInfoFactory KP-3378
    Former-commit-id: 8ef352a1680e7867faf0d4764aa2087e220fe67c

--- src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Views/Questionnaire/Edit/NewEditRosterView.cs
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Views/Questionnaire/Edit/NewEditRosterView.cs
@@ -4,7 +4,7 @@ namespace WB.Core.BoundedContexts.Designer.Views.Questionnaire.Edit
 {
     public class NewEditRosterView
     {
-        public GroupDetailsView Roster { get; set; }
+        public RosterDetailsView Roster { get; set; }
         public Dictionary<string, QuestionBrief[]> NumericIntegerQuestions { get; set; }
         public Dictionary<string, QuestionBrief[]> NotLinkedMultiOptionQuestions { get; set; }
         public Dictionary<string, QuestionBrief[]> TextListsQuestions { get; set; }
