8ddb703b9ddfc88a1bcf93911a196edd1323c664
Anastasya Kholodova <nastya.kholodova@gmail.com>
2014-04-28 19:17:29 -0400

    HQ\Preloading: unit tests for inconsistant data KP-3340
    Former-commit-id: 043eeb37d2143deafe75e8da9c86766579a87e48

---
+++ src/Tests/WB.Core.SharedKernels.SurveyManagement.Tests/PreloadedDataVerifierTests/when_verifying_preloaded_data_file_has_inconsistent_roster_row.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Machine.Specifications;
+using Main.Core.Documents;
+using Main.Core.Entities.SubEntities;
+using WB.Core.SharedKernels.SurveyManagement.Implementation.Services.Preloading;
+using WB.Core.SharedKernels.SurveyManagement.ValueObjects.PreloadedData;
+using WB.Core.SharedKernels.SurveyManagement.Views.PreloadedData;
+
+namespace WB.Core.SharedKernels.SurveyManagement.Tests.PreloadedDataVerifierTests
+{
+    internal class when_verifying_preloaded_data_file_has_roster_row : PreloadedDataVerifierTestContext
+    {
+        Establish context = () =>
+        {
+            questionnaireId = Guid.Parse("11111111111111111111111111111111");
+            questionnaire =
+                CreateQuestionnaireDocumentWithOneChapter(new Group(rosterTitle)
+                {
+                    IsRoster = true,
+                    PublicKey = Guid.NewGuid(),
+                    RosterSizeSource = RosterSizeSourceType.FixedTitles,
+                    RosterFixedTitles = new[] { "a" }
+                });
+            questionnaire.Title = questionnaireTitle;
+            preloadedDataByFileTopLevel = CreatePreloadedDataByFile(new[] { "Id", "ParentId" }, new string[][] { new string[] { "1", "" } },
+                questionnaireTitle + ".csv");
+            preloadedDataByFileRosterLevel = CreatePreloadedDataByFile(new[] { "Id", "ParentId" }, new string[][] { new string[] { "5", "1" } },
+                rosterTitle + ".csv");
+            preloadedDataVerifier = CreatePreloadedDataVerifier(questionnaire);
+        };
+
+        Because of =
+            () =>
+                result =
+                    preloadedDataVerifier.Verify(questionnaireId, 1, new[] { preloadedDataByFileTopLevel, preloadedDataByFileRosterLevel });
+
+        It should_result_has_1_error = () =>
+            result.Count().ShouldEqual(1);
+
+        It should_error_has_code_PL0008 = () =>
+            result.First().Code.ShouldEqual("PL0009");
+
+        It should_error_has_type_of_reference_cell = () =>
+            result.First().References.First().Type.ShouldEqual(PreloadedDataVerificationReferenceType.Cell);
+
+        It should_error_PositionX_be_equal_to_0 = () =>
+          result.First().References.First().PositionX.ShouldEqual(0);
+
+        It should_error_PositionY_be_equal_to_0 = () =>
+          result.First().References.First().PositionY.ShouldEqual(0);
+
+        It should_error_has_content_id_of_inconsistent_record = () =>
+            result.First().References.First().Content.ShouldEqual("5");
+
+        private static PreloadedDataVerifier preloadedDataVerifier;
+        private static IEnumerable<PreloadedDataVerificationError> result;
+        private static QuestionnaireDocument questionnaire;
+        private static Guid questionnaireId;
+        private static PreloadedDataByFile preloadedDataByFileTopLevel;
+        private static PreloadedDataByFile preloadedDataByFileRosterLevel;
+        private static string questionnaireTitle = "questionnaire";
+        private static string rosterTitle = "roster";
+    }
+}
