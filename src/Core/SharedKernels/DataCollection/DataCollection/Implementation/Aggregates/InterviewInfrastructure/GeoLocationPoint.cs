3bc28bbd76272b796e9f3e3986fb38b9e1e2c969
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2014-04-28 00:26:34 -0400

    DataCollection|Interview: Interview changes after review (file added) [KP-3369]
    Former-commit-id: 7a7e0ea6927a65fe6ede9268fb5b960077567af4

---
+++ src/Core/SharedKernels/DataCollection/DataCollection/Implementation/Aggregates/InterviewInfrastructure/GeoLocationPoint.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WB.Core.SharedKernels.DataCollection.Implementation.Aggregates
+{
+    internal class GeoLocationPoint
+    {
+        public GeoLocationPoint(double latitude, double longitude, double accuracy, DateTimeOffset timestamp)
+        {
+            this.Latitude = latitude;
+            this.Longitude = longitude;
+            this.Accuracy = accuracy;
+            this.Timestamp = timestamp;
+        }
+
+        public DateTimeOffset Timestamp { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public double Accuracy { get; set; }
+
+    }
+}
