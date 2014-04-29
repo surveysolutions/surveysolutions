ad8e3f0897f76e68ef12bc359c12212ba525fcd9
Anastasya Kholodova <nastya.kholodova@gmail.com>
2014-04-29 15:33:35 -0400

    Core: unit test for parsing geoposition string KP-3339
    Former-commit-id: 777fbaac3eafa80d875183adb9f9dc395f04d746

---
+++ RavenQuestionnaire.Web/Main.Core.Tests/Entities/SubEntities/GeoPositionTests/when_parsing_string_i_order_to_create_GeoPosition_object.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Machine.Specifications;
+using Main.Core.Entities.SubEntities;
+
+namespace Main.Core.Tests.Entities.SubEntities.GeoPositionTests
+{
+    [Subject(typeof(GeoPosition))]
+    internal class when_parsing_string_i_order_to_create_GeoPosition_object
+    {
+        Because of =
+            () =>
+            {
+                foreach (var possibleGeoPositionString in PossibleGeoPositionStrings)
+                {
+                    Results.Add(possibleGeoPositionString,GeoPosition.Parse(possibleGeoPositionString));
+                }
+            };
+
+        It should_result_be_null_for_empty_string = () =>
+            Results[EmptyString].ShouldBeNull();
+
+        It should_result_be_null_for_string_only_literals = () =>
+           Results[UnparsedAtAll].ShouldBeNull();
+
+        It should_result_be_null_for_missing_coodinats_value = () =>
+            Results[CoordinatesAreMissing].ShouldBeNull();
+
+        It should_result_be_null_for_only_one_coordinate = () =>
+            Results[OnlyOneCoordinate].ShouldBeNull();
+
+        It should_result_be_null_for_invalid_latitude = () =>
+            Results[InvalidLatitude].ShouldBeNull();
+
+        It should_result_be_null_for_invalid_longitude = () =>
+            Results[InvalidLongitude].ShouldBeNull();
+
+        It should_result_be_null_for_invalid_accuracy = () =>
+            Results[UnparsedAccuracy].ShouldBeNull();
+
+        It should_result_be_not_null_for_valid_format = () =>
+            Results[ValidFormat].ShouldNotBeNull();
+
+        It should_parsed_result_accuracy_be_equal_3 = () =>
+           Results[ValidFormat].Accuracy.ShouldEqual(3);
+
+        It should_parsed_result_latitude_be_equal_1 = () =>
+           Results[ValidFormat].Latitude.ShouldEqual(1);
+
+        It should_parsed_result_longitude_be_equal_2 = () =>
+          Results[ValidFormat].Longitude.ShouldEqual(2);
+
+        protected static string[] PossibleGeoPositionStrings = new[]
+        {
+            EmptyString, UnparsedAtAll, CoordinatesAreMissing, OnlyOneCoordinate, InvalidLatitude, InvalidLongitude,
+            UnparsedAccuracy, ValidFormat
+        };
+
+        protected static Dictionary<string, GeoPosition> Results = new Dictionary<string, GeoPosition>();
+        private const string EmptyString = "";
+        private const string UnparsedAtAll = "unparsed at all";
+        private const string CoordinatesAreMissing = "[coordinates are missing]";
+        private const string OnlyOneCoordinate = "34[only one coordinate]";
+        private const string InvalidLatitude = "a,4[invalid latitude]";
+        private const string InvalidLongitude = "4,b[invalid longitude]";
+        private const string UnparsedAccuracy = "1,2[unparsed accuracy]";
+        private const string ValidFormat = "1,2[3]";
+    }
+}
