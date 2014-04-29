f8718a21a56a4da116225fb7b81d81ed386ab0d6
Andrii Kozhyn <hedgic@gmail.com>
2014-04-29 10:42:12 +0300

    Supervisor: delete ghost files that should not exist
    Former-commit-id: d170cc2734278c0080fd6aed21221a7508c8d346

--- src/Core/BoundedContexts/Supervisor/WB.Core.BoundedContexts.Supervisor/Services/IHeadquartersSynchronizer.cs
+++
@@ -1,7 +0,0 @@
-namespace WB.Core.BoundedContexts.Supervisor.Services
-{
-    public interface IHeadquartersSynchronizer
-    {
-        void Pull(string login, string password);
-    }
-}
--- src/Core/BoundedContexts/Supervisor/WB.Core.BoundedContexts.Supervisor/Services/IQuestionnaireCacheInitializer.cs
+++
@@ -1,9 +0,0 @@
-using Main.Core.Documents;
-
-namespace WB.Core.BoundedContexts.Supervisor.Services
-{
-    public interface IQuestionnaireCacheInitializer
-    {
-        void InitializeQuestionnaireDocumentWithCaches(QuestionnaireDocument document);
-    }
-}
