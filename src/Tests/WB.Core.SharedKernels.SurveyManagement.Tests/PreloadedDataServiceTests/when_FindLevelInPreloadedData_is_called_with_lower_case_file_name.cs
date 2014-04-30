b610a8d66c8a0d62bab020432211cdd0153b1afc
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2014-04-30 11:30:01 -0400

    Hq\Preloading: unit tests for PreloadedDataService KP-3340
    Former-commit-id: 5ab8db966f95e44f1f21b7bf4009c806868bc039

---
+++ src/Tests/WB.Core.SharedKernels.SurveyManagement.Tests/PreloadedDataServiceTests/when_FindLevelInPreloadedData_is_called_with_lower_case_file_name.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Machine.Specifications;
+using Main.Core.Documents;
+using Main.Core.Entities.SubEntities;
+using WB.Core.SharedKernels.SurveyManagement.Implementation.Services.Preloading;
+using WB.Core.SharedKernels.SurveyManagement.Views.DataExport;
+
+namespace WB.Core.SharedKernels.SurveyManagement.Tests.PreloadedDataServiceTests
+{
+    internal class when_FindLevelInPreloadedData_is_called_with_lower_case_file_name : PreloadedDataServiceTestContext
+    {
+        Establish context = () =>
+        {
+            questionnaireDocument =
+                CreateQuestionnaireDocumentWithOneChapter(new Group("Roster Group")
+                {
+                    IsRoster = true,
+                    RosterSizeSource = RosterSizeSourceType.FixedTitles,
+                    RosterFixedTitles = new[] { "1" },
+                    PublicKey = rosterGroupId
+                });
+
+            preloadedDataService = CreatePreloadedDataService(questionnaireDocument);
+        };
+
+        Because of =
+           () =>
+               result =
+                   preloadedDataService.FindLevelInPreloadedData("roster group");
+
+        It should_result_be_not_null = () =>
+           result.ShouldNotBeNull();
+
+        It should_result_levelId_be_equal_to_rosterGroupId = () =>
+          result.LevelId.ShouldEqual(rosterGroupId);
+
+        private static PreloadedDataService preloadedDataService;
+        private static QuestionnaireDocument questionnaireDocument;
+        private static HeaderStructureForLevel result;
+        private static Guid rosterGroupId = Guid.NewGuid();
+    }
+}
