ac9e0426e808ef0b1fb5b423c95b9985f070908f
Anastasya Kholodova <nastya.kholodova@gmail.com>
2014-04-30 11:30:01 -0400

    Hq\Preloading: unit tests for PreloadedDataService KP-3340
    Former-commit-id: 5ab8db966f95e44f1f21b7bf4009c806868bc039

---
+++ src/Tests/WB.Core.SharedKernels.SurveyManagement.Tests/PreloadedDataServiceTests/PreloadedDataServiceTestContext.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Machine.Specifications;
+using Main.Core.Documents;
+using Main.Core.Entities.Composite;
+using Main.Core.Entities.SubEntities;
+using Moq;
+using WB.Core.GenericSubdomains.Utils;
+using WB.Core.SharedKernels.DataCollection.Implementation.Factories;
+using WB.Core.SharedKernels.SurveyManagement.Implementation.Factories;
+using WB.Core.SharedKernels.SurveyManagement.Implementation.Services.Preloading;
+using WB.Core.SharedKernels.SurveyManagement.Services;
+using WB.Core.SharedKernels.SurveyManagement.Views.PreloadedData;
+
+namespace WB.Core.SharedKernels.SurveyManagement.Tests.PreloadedDataServiceTests
+{
+    [Subject(typeof(PreloadedDataService))]
+    internal class PreloadedDataServiceTestContext
+    {
+        protected static PreloadedDataService CreatePreloadedDataService(QuestionnaireDocument questionnaireDocument = null)
+        {
+            var questionnaireExportStructure = (questionnaireDocument == null
+                ? null
+                : new ExportViewFactory(new ReferenceInfoForLinkedQuestionsFactory(),
+                    new QuestionnaireRosterStructureFactory()).CreateQuestionnaireExportStructure(questionnaireDocument, 1));
+            var questionnaireRosterStructure = (questionnaireDocument == null
+                ? null
+                : new QuestionnaireRosterStructureFactory().CreateQuestionnaireRosterStructure(questionnaireDocument, 1));
+            var dataFileServiceMock = new Mock<IDataFileService>();
+            dataFileServiceMock.Setup(x => x.CreateValidFileName(Moq.It.IsAny<string>())).Returns<string>((fileName => fileName));
+            return new PreloadedDataService(questionnaireExportStructure, questionnaireRosterStructure, questionnaireDocument, dataFileServiceMock.Object, new QuestionDataParser());
+        }
+
+        protected static QuestionnaireDocument CreateQuestionnaireDocumentWithOneChapter(params IComposite[] chapterChildren)
+        {
+            return new QuestionnaireDocument
+            {
+                Title = "some title",
+                Children = new List<IComposite>
+                {
+                    new Group("Chapter")
+                    {
+                        PublicKey = Guid.Parse("FFF000AAA111EE2DD2EE111AAA000FFF"),
+                        Children = chapterChildren.ToList(),
+                        IsRoster = false
+                    }
+                }
+            };
+        }
+
+        protected static PreloadedDataByFile CreatePreloadedDataByFile(string[] header = null, string[][] content = null, string fileName = null)
+        {
+            return new PreloadedDataByFile(Guid.NewGuid().FormatGuid(), fileName ?? "some file", header ?? new string[] { "Id", "ParentId" },
+                content ?? new string[0][]);
+        }
+    }
+}
---
+++ src/Tests/WB.Core.SharedKernels.SurveyManagement.Tests/PreloadedDataServiceTests/when_GetAvalibleIdListForParent_is_called_for_file_corresponding_to_fixed_roster.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Machine.Specifications;
+using Main.Core.Documents;
+using Main.Core.Entities.SubEntities;
+using WB.Core.SharedKernels.SurveyManagement.Implementation.Services.Preloading;
+using WB.Core.SharedKernels.SurveyManagement.Views.PreloadedData;
+
+namespace WB.Core.SharedKernels.SurveyManagement.Tests.PreloadedDataServiceTests
+{
+    internal class when_GetAvalibleIdListForParent_is_called_for_file_corresponding_to_fixed_roster : PreloadedDataServiceTestContext
+    {
+        Establish context = () =>
+        {
+            questionnaireDocument =
+                CreateQuestionnaireDocumentWithOneChapter(new Group("Roster Group")
+                {
+                    IsRoster = true,
+                    RosterSizeSource = RosterSizeSourceType.FixedTitles,
+                    RosterFixedTitles = new[] { "1","2" },
+                    PublicKey = rosterGroupId
+                });
+
+            preloadedDataService = CreatePreloadedDataService(questionnaireDocument);
+        };
+
+        Because of =
+            () =>
+                result =
+                    preloadedDataService.GetAvalibleIdListForParent(
+                        CreatePreloadedDataByFile(new string[] { "Id" }, new string[][] { new string[] { "1" } },
+                            questionnaireDocument.Title), rosterGroupId, "1");
+
+        It should_result_be_not_null = () =>
+            result.ShouldNotBeNull();
+
+        It should_result_have_2_ids_1_and_2 = () =>
+            result.SequenceEqual(new decimal[] { 1, 2 });
+
+        private static PreloadedDataService preloadedDataService;
+        private static QuestionnaireDocument questionnaireDocument;
+        private static decimal[] result;
+        private static Guid rosterGroupId = Guid.NewGuid();
+    }
+}
