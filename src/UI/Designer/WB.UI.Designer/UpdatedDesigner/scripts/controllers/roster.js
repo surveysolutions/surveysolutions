70aef936962ec83cd94dee1eb2ca7b01db1f2913
Dima Pasko <dimapasko@gmail.com>
2014-05-02 14:17:52 +0300

    Designer: initial infrastructure commit KP-3404 UI: edit roster
    Former-commit-id: 2f79fe5b684be9a77901dad1e25f11fdb0ac8027

---
+++ src/UI/Designer/WB.UI.Designer/UpdatedDesigner/scripts/controllers/roster.js
@@ -0,0 +1,9 @@
+﻿'use strict';
+
+angular.module('designerApp')
+    .controller('RosterCtrl', [
+        '$scope', '$routeParams', 'questionnaireService', 'commandService', 'utilityService', 'navigationService',
+        function($scope, $routeParams, questionnaireService, commandService, math, navigationService) {
+
+        }
+    ])
