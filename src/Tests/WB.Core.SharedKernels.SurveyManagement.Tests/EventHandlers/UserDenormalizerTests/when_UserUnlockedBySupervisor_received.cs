40ed08bfbd96213e8fa954bb54c5a57bd54b0c46
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2014-05-05 19:17:10 -0400

    Tests for UserDenormalizer covered [KP-3410]
    Former-commit-id: 3ad9667cbd5715dbe78a051701f2659899198ff1

---
+++ src/Tests/WB.Core.SharedKernels.SurveyManagement.Tests/EventHandlers/UserDenormalizerTests/when_UserUnlockedBySupervisor_received.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Machine.Specifications;
+using Main.Core.Documents;
+using Moq;
+using Ncqrs.Eventing.ServiceModel.Bus;
+using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
+using WB.Core.SharedKernels.DataCollection.Events.User;
+using WB.Core.SharedKernels.SurveyManagement.EventHandler;
+using WB.Core.Synchronization;
+using It = Machine.Specifications.It;
+
+namespace WB.Core.SharedKernels.SurveyManagement.Tests.EventHandlers.UserDenormalizerTests
+{
+    internal class when_UserUnlockedBySupervisor_received : UserDenormalizerContext
+    {
+        private Establish context = () =>
+        {
+            userId = Guid.Parse("22222222222222222222222222222222");
+            commandExecutorId = Guid.Parse("33333333333333333333333333333333");
+
+            syncStorage = new Mock<ISynchronizationDataStorage>();
+            syncStorage.Setup(x => x.SaveUser(Moq.It.IsAny<UserDocument>()))
+                .Callback((UserDocument userDoc) => userToSave = userDoc);
+            
+            var user = new UserDocument() { PublicKey = userId };
+
+            var userDocumentMockStorage = new Mock<IReadSideRepositoryWriter<UserDocument>>();
+            userDocumentMockStorage.Setup(x => x.GetById(Moq.It.IsAny<string>())).Returns(user);
+
+            denormalizer = CreateUserDenormalizer(users: userDocumentMockStorage.Object, syncStorage: syncStorage.Object);
+            
+            userUnlockedBySupervisorEvnt = CreateUserUnlockedBySupervisor(commandExecutorId, userId);
+        };
+
+        private Because of = () =>
+            denormalizer.Handle(userUnlockedBySupervisorEvnt);
+
+        private It should_sync_storage_stores_new_state = () =>
+            syncStorage.Verify(x => x.SaveUser(Moq.It.IsAny<UserDocument>()), Times.Once);
+
+        private It should_user_be_locked_by_supervisor = () =>
+            userToSave.IsLockedBySupervisor.ShouldEqual(false);
+        
+        private static UserDenormalizer denormalizer;
+        private static Guid commandExecutorId;
+        private static IPublishedEvent<UserUnlockedBySupervisor> userUnlockedBySupervisorEvnt;
+        private static Guid userId;
+        private static Mock<ISynchronizationDataStorage> syncStorage;
+
+        private static UserDocument userToSave;
+
+    }
+}
