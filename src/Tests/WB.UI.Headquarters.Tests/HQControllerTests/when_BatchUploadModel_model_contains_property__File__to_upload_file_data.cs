55f82002134a30bc9bba11293a877908eb840e2e
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2014-05-05 15:15:40 +0300

    Tests\UI.Headquarters: copied tests from Supervisor KP-3296
    Former-commit-id: 71135b56cb046f8cc34494a74a829cf2f0b24c59

---
+++ src/Tests/WB.UI.Headquarters.Tests/HQControllerTests/when_BatchUploadModel_model_contains_property__File__to_upload_file_data.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using System.Reflection;
+using Machine.Specifications;
+using WB.UI.Headquarters.Code;
+using WB.UI.Headquarters.Controllers;
+using WB.UI.Headquarters.Models;
+
+namespace WB.UI.Headquarters.Tests.HQControllerTests
+{
+    public class when_BatchUploadModel_model_contains_property__File__to_upload_file_data
+    {
+        Establish context = () => 
+            propertyInfo = typeof(BatchUploadModel).GetProperty("File");
+
+        Because of = () =>
+            attribute = propertyInfo.GetCustomAttributes(typeof(ValidateFileAttribute))
+                .Cast<ValidateFileAttribute>()
+                .FirstOrDefault();
+
+        It should_have_ValidateFile_attribute_for_that_property_to_check_file_validity = () => 
+            attribute.ShouldNotBeNull();
+
+        private static PropertyInfo propertyInfo;
+        private static ValidateFileAttribute attribute;
+    }
+}
---
+++ src/Tests/WB.UI.Headquarters.Tests/ReportDataApiControllerTests/when_questionnaires_data_is_being_queried.cs
@@ -0,0 +1,36 @@
+﻿using Machine.Specifications;
+using Main.Core.View;
+using Moq;
+using WB.Core.SharedKernels.DataCollection.Views.Questionnaire;
+using WB.Core.SharedKernels.SurveyManagement.Views.Reposts.Views;
+using WB.UI.Headquarters.Controllers;
+using It = Machine.Specifications.It;
+
+namespace WB.UI.Headquarters.Tests.ReportDataApiControllerTests
+{
+    internal class when_questionnaires_data_is_being_queried : ReportDataApiControllerTestContext
+    {
+        Establish context = () =>
+        {
+            questionnaireBrowseViewFactory = new Mock<IViewFactory<QuestionnaireBrowseInputModel, QuestionnaireAndVersionsView>>();
+            questionnaireBrowseViewFactory.Setup(x => x.Load(input)).Returns(view);
+            controller = CreateReportDataApiController(questionnaireBrowseViewFactory: questionnaireBrowseViewFactory.Object);
+        };
+
+        Because of = () =>
+            resultView = controller.Questionnaires(input);
+
+        It should_load_data_from_factory_once = () =>
+            questionnaireBrowseViewFactory.Verify(x => x.Load(input), Times.Once());
+
+        It should_return_same_view_as_was_setted_up = () => 
+            resultView.ShouldBeTheSameAs(view);
+
+        private static QuestionnaireBrowseInputModel input;
+        private static QuestionnaireAndVersionsView view;
+        private static QuestionnaireAndVersionsView resultView;
+        private static ReportDataApiController controller;
+        private static Mock<IViewFactory<QuestionnaireBrowseInputModel, QuestionnaireAndVersionsView>> questionnaireBrowseViewFactory;
+
+    }
+}
