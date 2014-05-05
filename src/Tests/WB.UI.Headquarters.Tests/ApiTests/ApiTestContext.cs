eca469b751b3e0e0137e7276f2cbee3b77f860e3
Anatoliy 'TLK' Kolesnick <none@none>
2014-05-05 15:15:40 +0300

    Tests\UI.Headquarters: copied tests from Supervisor KP-3296
    Former-commit-id: 71135b56cb046f8cc34494a74a829cf2f0b24c59

---
+++ src/Tests/WB.UI.Headquarters.Tests/ApiTests/ApiTestContext.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Main.Core.View;
+using Moq;
+using WB.Core.GenericSubdomains.Logging;
+using WB.Core.SharedKernels.DataCollection.Views.Questionnaire;
+using WB.Core.SharedKernels.DataCollection.Views.Questionnaire.BrowseItem;
+using WB.Core.SharedKernels.SurveyManagement.Views.Interview;
+using WB.Core.SharedKernels.SurveyManagement.Views.Interviewer;
+using WB.Core.SharedKernels.SurveyManagement.Views.User;
+using WB.UI.Headquarters.API;
+
+namespace WB.UI.Headquarters.Tests.ApiTests
+{
+    internal class ApiTestContext
+    {
+        protected static UserView CreateUserView(Guid userId, string userName)
+        {
+            return new UserView() { PublicKey = userId, UserName = userName };
+        }
+
+        protected static InterviewDetailsView CreateInterviewDetailsView(Guid interviewId)
+        {
+            return new InterviewDetailsView() { PublicKey = interviewId};
+        }
+
+        protected static UsersController CreateUsersController(
+            ILogger logger = null,
+            IViewFactory<InterviewersInputModel, InterviewersView> interviewersViewViewFactory = null,
+            IViewFactory<UserListViewInputModel, UserListView> userListViewViewFactory = null,
+            IViewFactory<UserViewInputModel, UserView> userViewViewFactory = null)
+        {
+            return new UsersController(
+                logger ?? Mock.Of<ILogger>(),
+                interviewersViewViewFactory ?? Mock.Of<IViewFactory<InterviewersInputModel, InterviewersView>>(),
+                userListViewViewFactory ?? Mock.Of<IViewFactory<UserListViewInputModel, UserListView>>(),
+                userViewViewFactory ?? Mock.Of<IViewFactory<UserViewInputModel, UserView>>());
+        }
+
+        protected static QuestionnairesController CreateQuestionnairesController(
+            ILogger logger = null,
+            IViewFactory<QuestionnaireBrowseInputModel, QuestionnaireBrowseView> questionnaireBrowseViewViewFactory = null,
+            IViewFactory<QuestionnaireItemInputModel, QuestionnaireBrowseItem> questionnaireBrowseItemViewFactory = null
+            )
+        {
+            return new QuestionnairesController(
+                logger ?? Mock.Of<ILogger>(),
+                questionnaireBrowseViewViewFactory ?? Mock.Of<IViewFactory<QuestionnaireBrowseInputModel, QuestionnaireBrowseView>>(),
+                questionnaireBrowseItemViewFactory ?? Mock.Of<IViewFactory<QuestionnaireItemInputModel, QuestionnaireBrowseItem>>());
+        }
+
+        protected static InterviewsController CreateInterviewsController(
+            ILogger logger = null,
+            IViewFactory<AllInterviewsInputModel, AllInterviewsView> allInterviewsViewViewFactory = null,
+            IViewFactory<InterviewDetailsInputModel, InterviewDetailsView> interviewDetailsView = null)
+        {
+            return new InterviewsController(
+                logger ?? Mock.Of<ILogger>(),
+                allInterviewsViewViewFactory ?? Mock.Of<IViewFactory<AllInterviewsInputModel, AllInterviewsView>>(),
+                interviewDetailsView ?? Mock.Of<IViewFactory<InterviewDetailsInputModel, InterviewDetailsView>>());
+        }
+    }
+}
---
+++ src/Tests/WB.UI.Headquarters.Tests/ReportDataApiControllerTests/ReportDataApiControllerTestContext.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Main.Core.View;
+using Moq;
+using Ncqrs.Commanding.ServiceModel;
+using Questionnaire.Core.Web.Helpers;
+using WB.Core.GenericSubdomains.Logging;
+using WB.Core.SharedKernels.DataCollection.Views.Questionnaire;
+using WB.Core.SharedKernels.DataCollection.Views.Questionnaire.BrowseItem;
+using WB.Core.SharedKernels.SurveyManagement.Views.Reposts.InputModels;
+using WB.Core.SharedKernels.SurveyManagement.Views.Reposts.Views;
+using WB.UI.Headquarters.Controllers;
+
+namespace WB.UI.Headquarters.Tests.ReportDataApiControllerTests
+{
+    internal class ReportDataApiControllerTestContext
+    {
+        protected static ReportDataApiController CreateReportDataApiController(
+            ICommandService commandService = null,
+            IGlobalInfoProvider provider = null,
+            ILogger logger = null,
+            IViewFactory<HeadquarterSurveysAndStatusesReportInputModel, HeadquarterSurveysAndStatusesReportView>
+                headquarterSurveysAndStatusesReport = null,
+            IViewFactory<HeadquarterSupervisorsAndStatusesReportInputModel, HeadquarterSupervisorsAndStatusesReportView>
+                headquarterSupervisorsAndStatusesReport = null,
+            IViewFactory<SupervisorTeamMembersAndStatusesReportInputModel, SupervisorTeamMembersAndStatusesReportView>
+                supervisorTeamMembersAndStatusesReport = null,
+            IViewFactory<SupervisorSurveysAndStatusesReportInputModel, SupervisorSurveysAndStatusesReportView>
+                supervisorSurveysAndStatusesReport = null,
+            IViewFactory<MapReportInputModel, MapReportView> mapReport = null,
+            IViewFactory<QuestionnaireBrowseInputModel, QuestionnaireAndVersionsView> questionnaireBrowseViewFactory = null,
+            IViewFactory<QuestionnaireQuestionInfoInputModel, QuestionnaireQuestionInfoView> questionInforFactory = null)
+        {
+            return new ReportDataApiController(
+                commandService ?? Mock.Of<ICommandService>(),
+                provider ?? Mock.Of<IGlobalInfoProvider>(),
+                logger ?? Mock.Of<ILogger>(),
+                headquarterSurveysAndStatusesReport ??
+                    Mock.Of<IViewFactory<HeadquarterSurveysAndStatusesReportInputModel, HeadquarterSurveysAndStatusesReportView>>(),
+                headquarterSupervisorsAndStatusesReport ??
+                    Mock.Of<IViewFactory<HeadquarterSupervisorsAndStatusesReportInputModel, HeadquarterSupervisorsAndStatusesReportView>>(),
+                supervisorTeamMembersAndStatusesReport ??
+                    Mock.Of<IViewFactory<SupervisorTeamMembersAndStatusesReportInputModel, SupervisorTeamMembersAndStatusesReportView>>(),
+                supervisorSurveysAndStatusesReport ??
+                    Mock.Of<IViewFactory<SupervisorSurveysAndStatusesReportInputModel, SupervisorSurveysAndStatusesReportView>>(),
+                mapReport ?? Mock.Of<IViewFactory<MapReportInputModel, MapReportView>>(),
+                questionnaireBrowseViewFactory ?? Mock.Of<IViewFactory<QuestionnaireBrowseInputModel, QuestionnaireAndVersionsView>>(),
+                questionInforFactory ?? Mock.Of<IViewFactory<QuestionnaireQuestionInfoInputModel, QuestionnaireQuestionInfoView>>()
+                );
+        }
+    }
+}
