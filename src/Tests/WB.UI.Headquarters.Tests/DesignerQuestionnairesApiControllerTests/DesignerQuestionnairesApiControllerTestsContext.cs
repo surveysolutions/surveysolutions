a655b68b93e01da0695b7617e55248aaac9d64e6
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2014-05-05 15:15:40 +0300

    Tests\UI.Headquarters: copied tests from Supervisor KP-3296
    Former-commit-id: 71135b56cb046f8cc34494a74a829cf2f0b24c59

---
+++ src/Tests/WB.UI.Headquarters.Tests/DesignerQuestionnairesApiControllerTests/DesignerQuestionnairesApiControllerTestsContext.cs
@@ -0,0 +1,32 @@
+using System;
+using Machine.Specifications;
+using Moq;
+using Ncqrs.Commanding.ServiceModel;
+using Questionnaire.Core.Web.Helpers;
+using WB.Core.GenericSubdomains.Logging;
+using WB.Core.SharedKernel.Utils.Compression;
+using WB.Core.SharedKernels.SurveyManagement.Services;
+using WB.UI.Headquarters.Controllers;
+using WB.UI.Headquarters.PublicService;
+
+namespace WB.UI.Headquarters.Tests.DesignerQuestionnairesApiControllerTests
+{
+    [Subject(typeof(DesignerQuestionnairesApiController))]
+    internal class DesignerQuestionnairesApiControllerTestsContext
+    {
+        protected static DesignerQuestionnairesApiController CreateDesignerQuestionnairesApiController(
+            ICommandService commandService = null, IGlobalInfoProvider globalInfo = null, IStringCompressor zipUtils = null,
+            ILogger logger = null, Func<IGlobalInfoProvider, IPublicService> getDesignerService = null,
+            ISupportedVersionProvider supportedVersionProvider = null)
+        {
+            return new DesignerQuestionnairesApiController(
+                supportedVersionProvider ?? new Mock<ISupportedVersionProvider> { DefaultValue = DefaultValue.Mock }.Object,
+                commandService ?? Mock.Of<ICommandService>(),
+                globalInfo ?? new Mock<IGlobalInfoProvider> { DefaultValue = DefaultValue.Mock }.Object,
+                zipUtils ?? new Mock<IStringCompressor> { DefaultValue = DefaultValue.Mock }.Object,
+                logger ?? Mock.Of<ILogger>(),
+                getDesignerService ?? (_ => new Mock<IPublicService> { DefaultValue = DefaultValue.Mock }.Object)
+               );
+        }
+    }
+}
