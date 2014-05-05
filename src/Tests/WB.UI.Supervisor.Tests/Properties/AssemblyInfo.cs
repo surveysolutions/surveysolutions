6d0bfb069a766bed134850cf5ed7cbe5f4017065
Anatoliy 'TLK' Kolesnick <none@none>
2014-05-05 16:01:47 +0300

    Tests: updated AssemblyInfo, csproj and slns due to rename of WB.UI.Supervisor.Tests KP-3296
    Former-commit-id: 5d1ed9bab442a5faf0fef3640ca64626dfc8d701

--- src/Tests/WB.UI.Supervisor.Tests/Properties/AssemblyInfo.cs
+++ src/Tests/WB.UI.Supervisor.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Web.Supervisor.Tests")]
+[assembly: AssemblyTitle("WB.UI.Supervisor.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Web.Supervisor.Tests")]
+[assembly: AssemblyProduct("WB.UI.Supervisor.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
