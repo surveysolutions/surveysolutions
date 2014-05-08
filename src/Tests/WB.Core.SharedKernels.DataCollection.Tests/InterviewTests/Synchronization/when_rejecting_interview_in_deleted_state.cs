204049a2c22e1f6bdb2c07af1eef6976723d3b3a
Andrii Kozhyn <hedgic@gmail.com>
2014-05-08 12:58:16 +0300

    KP-3368: Interview is not downloaded twice in synchronization Events in interview are raised only if needed Code review comments
    --HG--
    branch : release-candidate
    
    
    Former-commit-id: 47292b04927a3380e688ba1a067978703fcc2a4d

---
+++ src/Tests/WB.Core.SharedKernels.DataCollection.Tests/InterviewTests/Synchronization/when_rejecting_interview_in_deleted_state.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Machine.Specifications;
+using Ncqrs.Spec;
+using WB.Core.SharedKernels.DataCollection.DataTransferObjects.Synchronization;
+using WB.Core.SharedKernels.DataCollection.Events.Interview;
+using WB.Core.SharedKernels.DataCollection.Implementation.Aggregates;
+using WB.Core.SharedKernels.DataCollection.ValueObjects.Interview;
+
+namespace WB.Core.SharedKernels.DataCollection.Tests.InterviewTests.Synchronization
+{
+    internal class when_rejecting_interview_from_HQ_in_deleted_state : InterviewTestsContext
+    {
+        Establish context = () =>
+        {
+            interview = CreateInterview();
+            interview.Apply(new InterviewStatusChanged(InterviewStatus.Deleted, null));
+            eventContext = new EventContext();
+        };
+
+        Because of = () => interview.RejectInterviewFromHeadquarters(userId, Guid.NewGuid(), Guid.NewGuid(), new InterviewSynchronizationDto(), DateTime.Now, "comment");
+
+        It should_restore_interview = () => eventContext.ShouldContainEvent<InterviewRestored>(@event => @event.UserId == userId);
+
+        static Interview interview;
+        static EventContext eventContext;
+        static Guid userId = Guid.NewGuid();
+    }
+}
