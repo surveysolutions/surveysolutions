e629203e966654fa094af7a4485c79ae9c8666e7
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2014-05-08 00:18:35 -0400

    Interview: Fix answering supervisor questions after sync with hq [KP-3399]
    --HG--
    branch : release-candidate
    
    
    Former-commit-id: 7189ccd7f754f027ed21ac46b65a3b365e2e71b7

--- src/Core/SharedKernels/DataCollection/DataCollection/Implementation/Aggregates/InterviewInfrastructure/InterviewStateDependentOnAnswers.cs
+++ src/Core/SharedKernels/DataCollection/DataCollection/Implementation/Aggregates/InterviewInfrastructure/InterviewStateDependentOnAnswers.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using WB.Core.SharedKernels.DataCollection.Events.Interview.Dtos;
+using WB.Core.SharedKernels.DataCollection.Implementation.Aggregates.InterviewInfrastructure;
 
 namespace WB.Core.SharedKernels.DataCollection.Implementation.Aggregates
 {
@@ -108,7 +109,7 @@ namespace WB.Core.SharedKernels.DataCollection.Implementation.Aggregates
         {
             foreach (var instance in instances)
             {
-                string rosterGroupKey = ConvertIdAndRosterVectorToString(instance.GroupId, instance.OuterRosterVector);
+                string rosterGroupKey = ConversionHelper.ConvertIdAndRosterVectorToString(instance.GroupId, instance.OuterRosterVector);
                 DistinctDecimalList rosterRowInstances = RosterGroupInstanceIds.ContainsKey(rosterGroupKey)
                     ? RosterGroupInstanceIds[rosterGroupKey]
                     : new DistinctDecimalList();
@@ -123,7 +124,7 @@ namespace WB.Core.SharedKernels.DataCollection.Implementation.Aggregates
         {
             foreach (var instance in instances)
             {
-                string rosterGroupKey = ConvertIdAndRosterVectorToString(instance.GroupId, instance.OuterRosterVector);
+                string rosterGroupKey = ConversionHelper.ConvertIdAndRosterVectorToString(instance.GroupId, instance.OuterRosterVector);
 
                 var rosterRowInstances = RosterGroupInstanceIds.ContainsKey(rosterGroupKey)
                     ? RosterGroupInstanceIds[rosterGroupKey]
@@ -206,19 +207,7 @@ namespace WB.Core.SharedKernels.DataCollection.Implementation.Aggregates
         /// </remarks>
         private static string ConvertEventIdentityToString(Events.Interview.Dtos.Identity identity)
         {
-            return ConvertIdAndRosterVectorToString(identity.Id, identity.RosterVector);
+            return ConversionHelper.ConvertIdAndRosterVectorToString(identity.Id, identity.RosterVector);
         }
-
-        /// <remarks>
-        /// The opposite operation (get id or vector from string) should never be performed!
-        /// This is one-way transformation. Opposite operation is too slow.
-        /// If you need to compactify data and get it back, you should use another datatype, not a string.
-        /// </remarks>
-        private static string ConvertIdAndRosterVectorToString(Guid id, decimal[] rosterVector)
-        {
-            return string.Format("{0:N}[{1}]", id, string.Join("-", rosterVector));
-        }
-        
-        
     }
 }
