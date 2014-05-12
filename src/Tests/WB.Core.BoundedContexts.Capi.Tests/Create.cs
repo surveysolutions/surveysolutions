5f3bac727a875423bfa9dfe8c9d51079ff8c150e
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2014-05-12 11:32:19 -0400

    CAPI\Tests: clean up unit test KP-3455
    --HG--
    branch : release-candidate
    
    
    Former-commit-id: 2d603225f54b2b362323817539723d3ad4f80737

---
+++ src/Tests/WB.Core.BoundedContexts.Capi.Tests/Create.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WB.Core.SharedKernels.DataCollection.ValueObjects.Interview;
+
+namespace WB.Core.BoundedContexts.Capi.Tests
+{
+    internal static class Create
+    {
+        public static InterviewItemId CreateInterviewItemId(Guid id, decimal[] rosterVector = null)
+        {
+            return new InterviewItemId(id, rosterVector);
+        }
+    }
+}
