c35cdfe2c98e24302e1f9ad7d912a9ec46866bd3
Zurab Sajaia <zsajaia@worldbank.org>
2014-05-13 15:55:52 -0400

    move building of products into CI process
    --HG--
    branch : release-candidate
    
    
    Former-commit-id: d2a4c3eb4ef3fe4b3fa8038deaf3010056befa32

--- build/continuous-integration.ps1
+++ build/continuous-integration.ps1
@@ -1,4 +1,9 @@
-param([switch] $Deep)
+param([switch] $Deep,
+[string]$VersionPrefix,
+[INT]$BuildNumber,
+[string]$BuildConfiguration='Release',
+[string]$KeystorePassword)
+
 
 $ErrorActionPreference = "Stop"
 
@@ -6,23 +11,51 @@ $scriptFolder = (Get-Item $MyInvocation.MyCommand.Path).Directory.FullName
 
 . "$scriptFolder\functions.ps1"
 
-
 try {
 
-    CheckPrerequisites | %{ if (-not $_) { Exit } }
-    CleanBinAndObjFolders
+	CheckPrerequisites | %{ if (-not $_) { Exit } }
+	CleanBinAndObjFolders
 
-if ($Deep) {
-    BuildSolutions 'Release' -ClearBinAndObjFoldersBeforeEachSolution | %{ if (-not $_) { Exit } }
-} else {
-    BuildSolutions 'Release' | %{ if (-not $_) { Exit } }
-}
+	if ($Deep) {
+		BuildSolutions $BuildConfiguration -ClearBinAndObjFoldersBeforeEachSolution | %{ if (-not $_) { Exit } }
+	} else {
+		BuildSolutions $BuildConfiguration | %{ if (-not $_) { Exit } }
+	}
 
-    RunTests 'Release'
+	$PackageName = 'WBCapi.apk'
+	$VersionName = $VersionPrefix + $BuildNumber
+	. "$scriptFolder\build-android-package.ps1" `
+		-VersionName $VersionName `
+		-VersionCode $BuildNumber `
+		-BuildConfiguration $BuildConfiguration `
+		-KeystorePassword $KeystorePassword `
+		-KeystoreName 'WBCapi.keystore' `
+		-KeystoreAlias 'wbcapipublish' `
+		-CapiProject 'src\UI\Capi\WB.UI.Capi\WB.UI.Capi.csproj' `
+		-OutFileName $PackageName | %{ if (-not $_) { Exit } }
 
+
+	RunTests $BuildConfiguration
+
+
+	$Project = 'src\UI\Designer\WB.UI.Designer\WB.UI.Designer.csproj'
+	RunConfigTransform $Project $BuildConfiguration
+	BuildWebPackage $Project $BuildConfiguration | %{ if (-not $_) { Exit } }
+	AddArtifacts $Project $BuildConfiguration -folder "Designer"
+
+	$Project = 'src\UI\Headquarters\WB.UI.Headquarters\WB.UI.Headquarters.csproj'
+	RunConfigTransform $Project $BuildConfiguration
+	BuildWebPackage $Project $BuildConfiguration | %{ if (-not $_) { Exit } }
+	AddArtifacts $Project $BuildConfiguration -folder "Headquarters"
+
+	$Project = 'src\UI\Supervisor\WB.UI.Supervisor\WB.UI.Supervisor.csproj'
+	RunConfigTransform $Project $BuildConfiguration
+	CopyCapi -Project $Project -PathToFinalCapi $PackageName -BuildNumber $BuildNumber
+	BuildWebPackage $Project $BuildConfiguration | %{ if (-not $_) { Exit } }
+	AddArtifacts $Project $BuildConfiguration -folder "Supervisor"
 }
 catch {
-    Write-Host "##teamcity[message status='ERROR' text='Unexpected error occurred']"
-    Write-Host "##teamcity[buildProblem description='Unexpected error occurred']"
-    throw
+	Write-Host "##teamcity[message status='ERROR' text='Unexpected error occurred']"
+	Write-Host "##teamcity[buildProblem description='Unexpected error occurred']"
+	throw
 }
