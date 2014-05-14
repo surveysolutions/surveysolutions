a0cac286b7421d9b87a206eba760c1b953b52452
Roman Lytvyn <litvin.roma@gmail.com>
2014-05-14 12:10:43 +0300

    questionnaire verifier: categorical linked question cannot used in validations/conditions. fixed for manual categorical questions. KP-3381
    --HG--
    branch : release-candidate
    
    
    Former-commit-id: 6067303fc8bcc3dc49a237b56bb4f6be93196112

---
+++ src/Tests/WB.Core.SharedKernels.QuestionnaireVerification.Tests/QuestionnaireVerifierTests/Categorical/when_verifying_questionnaire_with_question_that_has_validation_expression_referencing_to_multi_not_linked.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using Machine.Specifications;
+using Main.Core.Documents;
+using Main.Core.Entities.SubEntities;
+using Main.Core.Entities.SubEntities.Question;
+using Moq;
+using WB.Core.SharedKernels.ExpressionProcessor.Services;
+using WB.Core.SharedKernels.QuestionnaireVerification.Implementation.Services;
+using WB.Core.SharedKernels.QuestionnaireVerification.ValueObjects;
+using It = Machine.Specifications.It;
+
+namespace WB.Core.SharedKernels.QuestionnaireVerification.Tests.QuestionnaireVerifierTests.Categorical
+{
+    internal class when_verifying_questionnaire_with_question_that_has_validation_expression_referencing_to_categorical_multi_not_linked_question : QuestionnaireVerifierTestsContext
+    {
+        Establish context = () =>
+        {
+            questionnaire = CreateQuestionnaireDocument(
+                new MultyOptionsQuestion()
+                {
+                    PublicKey = categoricalQuestionId,
+                    StataExportCaption = categoricalQuestionId.ToString(),
+                    Answers =
+                        new List<Answer>()
+                        {
+                            new Answer() {AnswerValue = "1", AnswerText = "opt 1"},
+                            new Answer() {AnswerValue = "2", AnswerText = "opt 2"}
+                        }
+                }, 
+                new NumericQuestion
+                {
+                    PublicKey = questionWithValidationExpressionId,
+                    ValidationExpression = "some validation",
+                    StataExportCaption = questionWithValidationExpressionId.ToString()
+                });
+
+            var expressionProcessor = Mock.Of<IExpressionProcessor>(processor
+                => processor.IsSyntaxValid(Moq.It.IsAny<string>()) == true
+                && processor.GetIdentifiersUsedInExpression(Moq.It.IsAny<string>()) == new[] { categoricalQuestionId.ToString() });
+
+            verifier = CreateQuestionnaireVerifier(expressionProcessor);
+        };
+
+        Because of = () =>
+            resultErrors = verifier.Verify(questionnaire);
+
+        It should_have_no_errors = () =>
+           resultErrors.ShouldBeEmpty();
+
+        private static IEnumerable<QuestionnaireVerificationError> resultErrors;
+        private static QuestionnaireVerifier verifier;
+        private static QuestionnaireDocument questionnaire;
+
+        private static Guid questionWithValidationExpressionId = Guid.Parse("10000000000000000000000000000000");
+        private static Guid categoricalQuestionId = Guid.Parse("12222222222222222222222222222222");
+    }
+}
