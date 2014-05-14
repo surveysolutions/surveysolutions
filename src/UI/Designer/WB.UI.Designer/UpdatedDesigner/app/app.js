cfacb2485c15621520b491775af25c7acbfcb826
Dima Pasko <dimapasko@gmail.com>
2014-05-14 13:02:40 +0300

    Designer: restructure project - cleanup KP-3431 Required Infrastructure
    Former-commit-id: 2e89290a16f1c2b4d08e85d59537bdb16acb6e18

---
+++ src/UI/Designer/WB.UI.Designer/UpdatedDesigner/app/app.js
@@ -0,0 +1,34 @@
+'use strict';
+
+angular.module('designerApp', [
+        'ngCookies',
+        'ngResource',
+        'ngSanitize',
+        'ngRoute',
+        'angular-underscore'
+    ])
+    .config(function($routeProvider) {
+        $routeProvider
+            .when('/:questionnaireId', {
+                templateUrl: 'app/views/main.html',
+                controller: 'MainCtrl'
+            })
+            .when('/:questionnaireId/chapter/:chapterId', {
+                templateUrl: 'app/views/main.html',
+                controller: 'MainCtrl',
+                reloadOnSearch: false
+            })
+            .when('/:questionnaireId/chapter/:chapterId/item/:itemId', {
+                templateUrl: 'app/views/main.html',
+                controller: 'MainCtrl',
+                reloadOnSearch: false
+            })
+            .when('/:questionnaireId/editchapter/:chapterId', {
+                templateUrl: 'app/views/main.html',
+                controller: 'MainCtrl',
+                reloadOnSearch: false
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    });
