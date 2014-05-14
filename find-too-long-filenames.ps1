e863513a2e094e0ef8358933531755b4759a312f
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2014-05-14 16:51:20 +0300

    Interview: implemented validation of questions which depend on just enabled or disabled questions or groups KP-2945
    --HG--
    branch : release-candidate
    
    
    Former-commit-id: 7e278fb7fb84197aea69624e57516ea6d8cc1fc1

--- find-too-long-filenames.ps1
+++ find-too-long-filenames.ps1
@@ -2,6 +2,6 @@
 
 Get-ChildItem -Recurse `
     | %{ GetPathRelativeToCurrectLocation $_.FullName } `
-    | ?{ $_.Length -gt 210 } `
-    | %{ New-Object PSObject -Property @{ File = $_; ExcessiveCharacters = $_.Length - 210 } } `
+    | ?{ $_.Length -gt 219 } `
+    | %{ New-Object PSObject -Property @{ File = $_; ExcessiveCharacters = $_.Length - 219 } } `
     | Format-List `
