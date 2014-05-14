14b1368a7c5f8cea2787c7c719aaf67e003807b7
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2014-05-14 17:02:25 -0400

    Changes for tester link on designer [KP-3477]
    --HG--
    branch : release-candidate
    
    
    Former-commit-id: d14e68278aacf7970f86056e17b1ea8578c81cf4

--- src/UI/Designer/WB.UI.Designer/Controllers/TesterController.cs
+++ src/UI/Designer/WB.UI.Designer/Controllers/TesterController.cs
@@ -1,62 +1,19 @@
-﻿using System.Globalization;
-using System.IO;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using WB.UI.Shared.Web.Membership;
 
 namespace WB.UI.Designer.Controllers
 {
     public class TesterController : BaseController
     {
-        private readonly string capiTesterFileName = "WBCapiTester.apk";
-        private readonly string pathToSearchVersions = "~/App_Data/Capi";
+        private const string appLink = "https://play.google.com/store/apps/details?id=org.worldbank.solutions.Vtester";
 
-        public TesterController(IMembershipUserService userHelper) : base(userHelper)
-        {
-        }
-        
+        public TesterController(IMembershipUserService userHelper)
+            : base(userHelper) {}
+
         [AllowAnonymous]
         public ActionResult Index()
         {
-            int maxVersion = this.GetLastVersionNumber();
-
-            if (maxVersion > 0)
-            {
-                var targetToSearchVersions = Server.MapPath(pathToSearchVersions);
-                string path = Path.Combine(targetToSearchVersions, maxVersion.ToString(CultureInfo.InvariantCulture));
-
-                string pathToFile = Path.Combine(path, this.capiTesterFileName);
-                if (System.IO.File.Exists(pathToFile))
-                    return File(pathToFile, 
-                                "application/vnd.android.package-archive", 
-                                GetApkFileNameWithVersion(this.capiTesterFileName, maxVersion));
-            }
-            return null;
+            return this.RedirectPermanent(appLink);
         }
-
-        private string GetApkFileNameWithVersion(string fileName, int version)
-        {
-            return fileName.Replace(".apk", string.Format(".v{0}.apk", version));
-        }
-
-        private int GetLastVersionNumber()
-        {
-            int maxVersion = 0;
-
-            var targetToSearchVersions = Server.MapPath(pathToSearchVersions);
-            if (Directory.Exists(targetToSearchVersions))
-            {
-                var dirInfo = new DirectoryInfo(targetToSearchVersions);
-                foreach (DirectoryInfo directoryInfo in dirInfo.GetDirectories())
-                {
-                    int value;
-                    if (int.TryParse(directoryInfo.Name, out value))
-                        if (maxVersion < value)
-                            maxVersion = value;
-                }
-            }
-
-            return maxVersion;
-        }
-
     }
 }
