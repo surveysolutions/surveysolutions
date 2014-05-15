6a5c751dce53b09d841fc607931dca8223439964
Anastasya Kholodova <nastya.kholodova@gmail.com>
2014-05-15 11:59:49 -0400

    DataCollection: unit tests for ValueVector KP-3340
    --HG--
    branch : release-candidate
    
    
    Former-commit-id: 6810a4c8dbd1d6c657c7722c25d25065f1b2f298

---
+++ src/Tests/WB.Core.SharedKernels.DataCollection.Tests/ValueVectorTests/when_call_ToString_for_ValueVector_with_empty_vector_inside.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Machine.Specifications;
+using WB.Core.SharedKernels.DataCollection.ValueObjects;
+
+namespace WB.Core.SharedKernels.DataCollection.Tests.ValueVectorTests
+{
+    internal class when_call_ToString_for_ValueVector_with_empty_vector_inside
+    {
+        Establish context = () =>
+        {
+            valueVector = new ValueVector<Guid>();
+        };
+
+        Because of = () =>
+           result = valueVector.ToString();
+
+        It should_return__Empty__result = () =>
+             result.ShouldEqual("Empty");
+
+        private static ValueVector<Guid> valueVector;
+        private static string result;
+    }
+}
