b18bcc2b43eda1531a34e607fd0c36edd9ebb7f5
Anastasya Kholodova <nastya.kholodova@gmail.com>
2014-05-16 19:06:36 -0400

    DataCollection: fix roster titles
    --HG--
    branch : release-candidate
    
    
    Former-commit-id: b7ea97a75d4744c81d8b6c3686bd729a44d2681a

--- src/Core/SharedKernels/DataCollection/DataCollection/Events/Interview/Dtos/ChangedRosterInstanceTitleDto.cs
+++ src/Core/SharedKernels/DataCollection/DataCollection/Events/Interview/Dtos/ChangedRosterInstanceTitleDto.cs
@@ -1,13 +1,12 @@
 namespace WB.Core.SharedKernels.DataCollection.Events.Interview.Dtos
 {
     /// <remarks>Make sure not to reuse this class on read or write side. Use your own copies.</remarks>
-    public struct ChangedRosterInstanceTitleDto
+    public class ChangedRosterInstanceTitleDto
     {
         public RosterInstance RosterInstance { get; private set; }
         public string Title { private set; get; }
 
         public ChangedRosterInstanceTitleDto(RosterInstance rosterInstance, string title)
-            : this()
         {
             this.RosterInstance = rosterInstance;
             this.Title = title;
