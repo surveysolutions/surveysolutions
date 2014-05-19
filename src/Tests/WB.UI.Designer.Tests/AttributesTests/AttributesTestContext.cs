ac527eacb5c63af898de38445be85fbf70f16c1c
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2014-05-19 08:49:44 -0400

    Tests for covering HttpsVerifier and ApiBasicAuthAttribute [KP-3476]
    --HG--
    branch : release-candidate
    
    
    Former-commit-id: f1c8ceb5aa5edee9c825d93476cc892ace1f6871

---
+++ src/Tests/WB.UI.Designer.Tests/AttributesTests/AttributesTestContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+using WB.UI.Designer.Api.Attributes;
+
+namespace WB.UI.Designer.Tests.AttributesTests
+{
+    public class AttributesTestContext
+    {
+        public static ApiBasicAuthAttribute CreateApiBasicAuthAttribute(Func<string, string, bool> validateUserCredentials = null)
+        {
+            return new ApiBasicAuthAttribute(validateUserCredentials ?? ((s, s1) => true) );
+        }
+        
+        public static string EncodeToBase64(string value)
+        {
+            byte[] toEncodeAsBytes = Encoding.UTF8.GetBytes(value);
+            return Convert.ToBase64String(toEncodeAsBytes);
+        }
+    }
+}
