0f104255a93d39bea7d79cd1ec1b6589967eb735
Roman Lytvyn <litvin.roma@gmail.com>
2014-05-20 13:47:43 +0300

    supervisor: added test for fixing crashes when creating interview. KP-3480
    --HG--
    branch : release-candidate
    
    
    Former-commit-id: 0174ed73aec3f2e1cd4f3f92621b1ae814864562

---
+++ src/Tests/WB.Core.SharedKernels.DataCollection.Tests/InterviewTests/when_creating_interview_with_fixed_roster_and_roster_size_numeric_question_inside_it.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using Machine.Specifications;
+using Main.Core.Entities.Composite;
+using Main.Core.Entities.SubEntities;
+using Main.Core.Entities.SubEntities.Question;
+using Microsoft.Practices.ServiceLocation;
+using Moq;
+using WB.Core.SharedKernels.DataCollection.Implementation.Aggregates;
+using WB.Core.SharedKernels.DataCollection.Implementation.Entities;
+using WB.Core.SharedKernels.DataCollection.Repositories;
+using It = Machine.Specifications.It;
+
+namespace WB.Core.SharedKernels.DataCollection.Tests.InterviewTests
+{
+    internal class when_creating_interview_with_fixed_roster_and_roster_size_numeric_question_inside_it : InterviewTestsContext
+    {
+        Establish context = () =>
+        {
+            var questionnaire = CreateQuestionnaireDocumentWithOneChapter(
+                new Group("fixed roster")
+                {
+                    PublicKey = fixedRosteId,
+                    IsRoster = true,
+                    RosterSizeSource = RosterSizeSourceType.FixedTitles,
+                    RosterFixedTitles = new[] {"fixed1", "fixed2"},
+                    Children = new List<IComposite>
+                    {
+                        new NumericQuestion("roster size question")
+                        {
+                            PublicKey = rosterSizeQuestionId,
+                            QuestionType = QuestionType.Numeric,
+                            IsInteger = true,
+                            MaxValue = 4
+                        },
+                        new Group("nested roster")
+                        {
+                            PublicKey = nestedRosterId,
+                            IsRoster = true,
+                            RosterSizeSource = RosterSizeSourceType.Question,
+                            RosterSizeQuestionId = rosterSizeQuestionId
+                        }
+                    }
+                });
+
+            var questionnaireRepository = CreateQuestionnaireRepositoryStubWithOneQuestionnaire(questionnaireId,
+                new PlainQuestionnaire(questionnaire, 1));
+
+            Mock.Get(ServiceLocator.Current)
+                .Setup(locator => locator.GetInstance<IQuestionnaireRepository>())
+                .Returns(questionnaireRepository);
+        };
+
+        Because of = () => 
+            exception = Catch.Exception(() => new Interview(new Guid(), new Guid(), questionnaireId, new Dictionary<Guid, object>(),
+                DateTime.Now));
+
+        It should_exception_be_null = () =>
+            exception.ShouldBeNull();
+
+        private static Guid fixedRosteId = Guid.Parse("11111111111111111111111111111111");
+        private static Guid nestedRosterId = Guid.Parse("22222222222222222222222222222222");
+        private static Guid rosterSizeQuestionId = Guid.Parse("33333333333333333333333333333333");
+        private static Guid questionnaireId = Guid.Parse("44444444444444444444444444444444");
+        private static Exception exception;
+    }
+}
