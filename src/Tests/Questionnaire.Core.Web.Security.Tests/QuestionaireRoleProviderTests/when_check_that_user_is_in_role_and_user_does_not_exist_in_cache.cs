fd2ecd61f4e5ee2f6b0f42eeea9f66bfeab265d6
Roman Lytvyn <litvin.roma@gmail.com>
2014-05-20 12:07:54 +0300

    hq-supervisor: fixed crash when login to supervisor, but supervisor's database is empty. KP-3449
    --HG--
    branch : release-candidate
    
    
    Former-commit-id: 31766735d03719ee904434e443377a1aca041338

---
+++ src/Tests/Questionnaire.Core.Web.Security.Tests/QuestionaireRoleProviderTests/when_check_that_user_is_in_role_and_user_does_not_exist_in_cache.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Machine.Specifications;
+
+namespace Questionnaire.Core.Web.Security.Tests.QuestionaireRoleProviderTests
+{
+    internal class when_check_that_user_is_in_role_and_user_does_not_exist_in_cache : QuestionnaireRoleProviderTestsContext
+    {
+        Establish context = () =>
+        {
+            provider = CreateProvider();
+        };
+
+        Because of = () => 
+            exception = Catch.Exception(() => provider.IsUserInRole("some_user_name", "some_role"));
+
+        It should_exception_be_null =
+            exception.ShouldBeNull;
+
+        private static QuestionnaireRoleProvider provider;
+        private static Exception exception;
+    }
+}
