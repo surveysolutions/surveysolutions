fdf93475044c8fe16da46430ce393b050976c64f
Anastasya Kholodova <nastya.kholodova@gmail.com>
2014-05-21 18:48:52 -0400

    CAPI: move statistic calculation in interview view model KP-3515
    --HG--
    branch : release-candidate
    
    
    Former-commit-id: 9f2382f7d08da41cc48684baaa66e76d7a9d7eab

--- src/UI/QuestionnaireTester/WB.UI.QuestionnaireTester/Implementations/Fragments/TesterStatisticsContentFragment.cs
+++ src/UI/QuestionnaireTester/WB.UI.QuestionnaireTester/Implementations/Fragments/TesterStatisticsContentFragment.cs
@@ -1,5 +1,6 @@
 using System;
 using Ninject;
+using WB.Core.BoundedContexts.Capi.Views.InterviewDetails;
 using WB.Core.SharedKernels.DataCollection.Commands.Interview;
 using WB.Core.SharedKernels.DataCollection.ValueObjects.Interview;
 using WB.Core.BoundedContexts.Capi.Views.Statistics;
@@ -10,31 +11,16 @@ namespace WB.UI.QuestionnaireTester.Implementations.Fragments
 {
     public class TesterStatisticsContentFragment : StatisticsContentFragment
     {
-        protected override StatisticsViewModel GetStatisticsViewModel(Guid questionnaireId)
+        protected override InterviewViewModel GetInterviewViewModel(Guid interviewId)
         {
-            return CapiTesterApplication.LoadView<StatisticsInput, StatisticsViewModel>(new StatisticsInput(questionnaireId));
+            return CapiTesterApplication.LoadView<QuestionnaireScreenInput, InterviewViewModel>(new QuestionnaireScreenInput(interviewId));
         }
+
         protected override void PreCompleteAction()
         {
             base.PreCompleteAction();
 
             this.Activity.ClearAllBackStack<QuestionnaireListActivity>();
-           /* var logManipulator = CapiTesterApplication.Kernel.Get<IChangeLogManipulator>();
-
-            if (this.Model.Status == InterviewStatus.Completed)
-            {
-                CapiTesterApplication.CommandService.Execute(
-                    new RestartInterviewCommand(this.Model.QuestionnaireId, CapiTesterApplication.DesignerMembership.CurrentUser.Id, this.etComments.Text));
-
-                logManipulator.CreateOrReopenDraftRecord(this.Model.QuestionnaireId);
-            }
-            else
-            {
-                CapiTesterApplication.CommandService.Execute(
-                    new CompleteInterviewCommand(this.Model.QuestionnaireId, CapiTesterApplication.DesignerMembership.CurrentUser.Id, this.etComments.Text));
-
-                logManipulator.CloseDraftRecord(this.Model.QuestionnaireId);
-            }*/
         }
     }
 }
