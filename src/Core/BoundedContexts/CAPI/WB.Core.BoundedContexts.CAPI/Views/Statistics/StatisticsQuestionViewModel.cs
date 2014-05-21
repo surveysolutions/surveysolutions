12f564fbdbfd6f200017c7e5848192d8b8c9e771
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2014-05-21 16:22:03 -0400

    CAPI: lazy loading for popups in complete tab KP-3515
    --HG--
    branch : release-candidate
    
    
    Former-commit-id: 1f931b9153a11ab2c08e31579d44bda7cd78b1a9

--- src/Core/BoundedContexts/CAPI/WB.Core.BoundedContexts.CAPI/Views/Statistics/StatisticsQuestionViewModel.cs
+++ src/Core/BoundedContexts/CAPI/WB.Core.BoundedContexts.CAPI/Views/Statistics/StatisticsQuestionViewModel.cs
@@ -6,17 +6,15 @@ namespace WB.Core.BoundedContexts.Capi.Views.Statistics
 {
     public class StatisticsQuestionViewModel
     {
-        public StatisticsQuestionViewModel(InterviewItemId publicKey, InterviewItemId parentPublicKey, string text, string answerString, string errorMessage)
+        public StatisticsQuestionViewModel(InterviewItemId publicKey, string text, string answerString, string errorMessage)
         {
             PublicKey = publicKey;
-            ParentKey = parentPublicKey;
             Text = text;
             AnswerString = answerString;
             ErrorMessage = errorMessage;
         }
 
         public InterviewItemId PublicKey { get; private set; }
-        public InterviewItemId ParentKey { get; private set; }
         public string Text { get; private set; }
         public string AnswerString { get; protected set; }
         public string ErrorMessage { get; private set; }
