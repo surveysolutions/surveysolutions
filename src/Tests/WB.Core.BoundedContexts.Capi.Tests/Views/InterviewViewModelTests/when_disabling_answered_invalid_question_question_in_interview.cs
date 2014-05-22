eebce880a8c706342b55ce757505b5d021efac4b
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2014-05-22 12:36:53 -0400

    CAPI: unit test for statistics KP-3515
    --HG--
    branch : release-candidate
    
    
    Former-commit-id: 23c8b4a28af451563b5391b5d0a09c94be9c9ec1

---
+++ src/Tests/WB.Core.BoundedContexts.Capi.Tests/Views/InterviewViewModelTests/when_disabling_answered_invalid_question_question_in_interview.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Machine.Specifications;
+using Main.Core.Documents;
+using Main.Core.Entities.SubEntities;
+using Main.Core.Entities.SubEntities.Question;
+using WB.Core.BoundedContexts.Capi.Views.InterviewDetails;
+using WB.Core.SharedKernels.DataCollection.DataTransferObjects.Synchronization;
+using WB.Core.SharedKernels.DataCollection.ValueObjects.Interview;
+using WB.Core.SharedKernels.DataCollection.Views.Questionnaire;
+
+namespace WB.Core.BoundedContexts.Capi.Tests.Views.InterviewViewModelTests
+{
+    internal class when_disabling_answered_invalid_question_question_in_interview : InterviewViewModelTestContext
+    {
+        Establish context = () =>
+        {
+            targetQuestionId = Guid.Parse("33333333333333333333333333333333");
+            questionnarie = CreateQuestionnaireDocumentWithOneChapter(
+                new NumericQuestion()
+                {
+                    PublicKey = targetQuestionId,
+                    QuestionType = QuestionType.Numeric
+                });
+
+            rosterStructure = CreateQuestionnaireRosterStructure(questionnarie);
+
+            interviewSynchronizationDto = CreateInterviewSynchronizationDto(
+               answers: new AnsweredQuestionSynchronizationDto[0],
+               propagatedGroupInstanceCounts: new Dictionary<InterviewItemId, RosterSynchronizationDto[]>());
+
+            interviewViewModel = CreateInterviewViewModel(questionnarie, rosterStructure,
+            interviewSynchronizationDto);
+            interviewViewModel.SetAnswer(new InterviewItemId(targetQuestionId, new decimal[0]), 3);
+            interviewViewModel.SetQuestionValidity(new InterviewItemId(targetQuestionId, new decimal[0]), false);
+        };
+
+        Because of = () =>
+             interviewViewModel.SetQuestionStatus(new InterviewItemId(targetQuestionId, new decimal[0]), false);
+
+        It should_UnansweredQuestions_in_statistic_cont_has_zero_elements = () =>
+           interviewViewModel.Statistics.UnansweredQuestions.ShouldBeEmpty();
+
+        It should_answeredQuestions_in_statistic_cont_has_zero_elements = () =>
+           interviewViewModel.Statistics.AnsweredQuestions.ShouldBeEmpty();
+
+        It should_invalidQuestions_in_statistic_be_empty = () =>
+          interviewViewModel.Statistics.InvalidQuestions.ShouldBeEmpty();
+
+        private static InterviewViewModel interviewViewModel;
+        private static QuestionnaireDocument questionnarie;
+        private static QuestionnaireRosterStructure rosterStructure;
+        private static InterviewSynchronizationDto interviewSynchronizationDto;
+        private static Guid targetQuestionId;
+    }
+}
