9ab51ddaa079363bbe83ff9a5479d61b034b2574
Slava Scherbak <slava.scherbak@gmail.com>
2014-05-22 13:26:45 +0300

    Supervisor: fixed bug with comments in status history KP-3497
    --HG--
    branch : release-candidate
    
    
    Former-commit-id: 4c51be348641c4b3d433f18fbad46f069e142f22

--- src/Tests/WB.Core.SharedKernels.SurveyManagement.Tests/EventHandlers/Interview/InterviewSummaryEventHandlerFunctionalTests/when_handling_InterviewStatusChanged_event_and_status_history_has_2_item_with_not_same_last_status.cs
+++ src/Tests/WB.Core.SharedKernels.SurveyManagement.Tests/EventHandlers/Interview/InterviewSummaryEventHandlerFunctionalTests/when_handling_InterviewStatusChanged_event_and_status_history_has_2_item_with_not_same_last_status.cs
@@ -7,12 +7,11 @@ using WB.Core.SharedKernels.SurveyManagement.Views.Interview;
 
 namespace WB.Core.SharedKernels.SurveyManagement.Tests.EventHandlers.Interview.InterviewSummaryEventHandlerFunctionalTests
 {
-    internal class when_handling_InterviewStatusChanged_event_and_status_history_has_2_item_with_same_status :
+    internal class when_handling_InterviewStatusChanged_event_and_status_history_has_2_item_with_not_same_last_status :
         InterviewSummaryEventHandlerFunctionalTestsContext
     {
         private Establish context = () =>
         {
-
             viewModel = new InterviewSummary
             {
                 CommentedStatusesHistory = new List<InterviewCommentedStatus>
@@ -24,7 +23,7 @@ namespace WB.Core.SharedKernels.SurveyManagement.Tests.EventHandlers.Interview.I
                     },
                     new InterviewCommentedStatus
                     {
-                        Status = InterviewStatus.Completed,
+                        Status = InterviewStatus.RejectedBySupervisor,
                         Comment = null
                     }
                 }
@@ -36,15 +35,15 @@ namespace WB.Core.SharedKernels.SurveyManagement.Tests.EventHandlers.Interview.I
         private Because of = () =>
             viewModel = denormalizer.Update(viewModel, Create.InterviewStatusChangedEvent(InterviewStatus.Completed, comment));
 
-        private It should_set_comment_to_last_status_with_same_value = () =>
-            viewModel.CommentedStatusesHistory.Last().Comment.ShouldEqual(comment);
+        private It should_leave_statuses_history_intact = () =>
+            viewModel.CommentedStatusesHistory.Last().Comment.ShouldBeNull();
 
         private It should_set_not_change_comment_for_first_status = () =>
             viewModel.CommentedStatusesHistory.First().Comment.ShouldEqual(thisCommentShouldNotBeChanged);
 
         private static InterviewSummaryEventHandlerFunctional denormalizer;
         private static InterviewSummary viewModel;
-        private static string thisCommentShouldNotBeChanged = "Should not be changed";
         private const string comment = "Some status change comment";
+        private static string thisCommentShouldNotBeChanged = "Should not be changed";
     }
 }
