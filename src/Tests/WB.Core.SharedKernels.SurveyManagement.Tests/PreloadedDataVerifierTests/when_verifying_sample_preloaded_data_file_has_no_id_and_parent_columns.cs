920c6470696b5da7fb9dafd9fe056d81b6f5a994
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2014-05-23 15:48:56 -0400

    Supervisor\HQ: unit tests for sample upload KP-3529
    --HG--
    branch : release-candidate
    
    
    Former-commit-id: 0bdea1e1a7161f3b94137dd9f3a5d87f0b7c62ce

---
+++ src/Tests/WB.Core.SharedKernels.SurveyManagement.Tests/PreloadedDataVerifierTests/when_verifying_sample_preloaded_data_file_has_no_id_and_parent_columns.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Machine.Specifications;
+using Main.Core.Documents;
+using Moq;
+using WB.Core.SharedKernels.SurveyManagement.Implementation.Services.Preloading;
+using WB.Core.SharedKernels.SurveyManagement.Services.Preloading;
+using WB.Core.SharedKernels.SurveyManagement.ValueObjects.PreloadedData;
+using WB.Core.SharedKernels.SurveyManagement.Views.DataExport;
+using It = Machine.Specifications.It;
+
+namespace WB.Core.SharedKernels.SurveyManagement.Tests.PreloadedDataVerifierTests
+{
+    internal class when_verifying_sample_preloaded_data_file_has_no_id_and_parent_columns : PreloadedDataVerifierTestContext
+    {
+        Establish context = () =>
+        {
+            questionnaire = CreateQuestionnaireDocumentWithOneChapter();
+            questionnaire.Title = "questionnaire";
+            questionnaireId = Guid.Parse("11111111111111111111111111111111");
+
+            preloadedDataServiceMock = new Mock<IPreloadedDataService>();
+
+            preloadedDataServiceMock.Setup(x => x.FindLevelInPreloadedData(QuestionnaireCsvFileName)).Returns(new HeaderStructureForLevel());
+
+            preloadedDataVerifier = CreatePreloadedDataVerifier(questionnaire, null, preloadedDataServiceMock.Object);
+        };
+
+        Because of =
+            () =>
+                result =
+                    preloadedDataVerifier.VerifySample(questionnaireId, 1, CreatePreloadedDataByFile(new string[0], null, QuestionnaireCsvFileName) );
+
+        It should_result_has_no_errors = () =>
+           result.ShouldBeEmpty();
+
+        private static PreloadedDataVerifier preloadedDataVerifier;
+        private static IEnumerable<PreloadedDataVerificationError> result;
+        private static QuestionnaireDocument questionnaire;
+        private static Guid questionnaireId;
+        private const string QuestionnaireCsvFileName = "questionnaire.csv";
+        private static Mock<IPreloadedDataService> preloadedDataServiceMock;
+    }
+}
