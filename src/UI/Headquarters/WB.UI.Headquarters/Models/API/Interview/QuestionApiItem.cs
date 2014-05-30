bf3cc4b524ef4cced253c3aa8794f95207ea3b86
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2014-05-29 22:51:41 -0400

    Api updated for HQ
    Former-commit-id: 0302560eb4fc2002ed4afe1c1102fe07ed3c718a

---
+++ src/UI/Headquarters/WB.UI.Headquarters/Models/API/Interview/QuestionApiItem.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.Serialization;
+
+namespace WB.UI.Headquarters.Models.API.Interview
+{
+    public class QuestionApiItem
+    {
+        public QuestionApiItem(string name, object value)
+        {
+            this.Value = value;
+            this.Name = name;
+        }
+
+        [DataMember]
+        public string Name { set; get; }
+        [DataMember]
+        public object Value { set; get; }
+    }
+}
--- src/UI/Headquarters/WB.UI.Headquarters/Models/API/InterviewApiDetails.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/Models/API/InterviewApiDetails.cs
@@ -1,5 +1,10 @@
-﻿using System.Runtime.Serialization;
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.Serialization;
 using WB.Core.SharedKernels.SurveyManagement.Views.Interview;
+using WB.UI.Headquarters.Models.API.Interview;
 
 namespace WB.UI.Headquarters.Models.API
 {
@@ -7,11 +12,71 @@ namespace WB.UI.Headquarters.Models.API
     {
         public InterviewApiDetails(InterviewDetailsView interview)
         {
-            this.Interview = interview;
+            this.Questions = new List<QuestionApiItem>();
+            this.Rosters = new List<RosterApiItem>();
+
+            var rosters = new Dictionary<string, RosterApiItem>();
+
+            if (interview != null)
+            {
+                foreach (var interviewGroupView in interview.Groups)
+                {
+                    if (interviewGroupView.RosterVector.Length == 0)
+                        AddQuestionsToRoster(this.Questions, interviewGroupView.Questions);
+                    else
+                    {
+                        var key = CreateLeveKeyFromPropagationVector(interviewGroupView.RosterVector);
+                        RosterApiItem item;
+
+                        if (rosters.ContainsKey(key))
+                            item = rosters[key];
+                        else
+                        {
+                            item = new RosterApiItem()
+                            {
+                                Id = interviewGroupView.Id,
+                                RosterVector = interviewGroupView.RosterVector,
+                                Item = interviewGroupView.RosterVector.Last()
+                            };
+
+                            rosters.Add(key, item);
+                        }
+
+                        AddQuestionsToRoster(item.Questions, interviewGroupView.Questions);
+                    }
+                }
+
+                foreach (var rosterApiItem in rosters.Values)
+                {
+                    if (rosterApiItem.RosterVector.Length > 1)
+                    {
+                        var key = CreateLeveKeyFromPropagationVector(rosterApiItem.RosterVector.Take(rosterApiItem.RosterVector.Length - 1).ToArray());
+
+                        if (!rosters.ContainsKey(key))
+                            throw new Exception("Error in structure");
+
+                        rosters[key].Rosters.Add(rosterApiItem);
+                    }
+                }
+
+                this.Rosters = rosters.Values.Where(i => i.RosterVector.Length == 1).ToList();
+            }
         }
 
-        //should be changed to close internal information
+        private static string CreateLeveKeyFromPropagationVector(decimal[] vector)
+        {
+            return string.Join(",", vector.Select(v => v.ToString("0.############################", CultureInfo.InvariantCulture)));
+        }
+
+        private static void AddQuestionsToRoster(List<QuestionApiItem> questions, IEnumerable<InterviewQuestionView> questionsToAdd)
+        {
+            questions.AddRange(questionsToAdd.Select(question => new QuestionApiItem(question.Variable, question.Answer ?? string.Empty)));
+        }
+
         [DataMember]
-        public InterviewDetailsView Interview { get; set; }
+        public List<QuestionApiItem> Questions { set; get; }
+
+        [DataMember]
+        public List<RosterApiItem> Rosters { set; get; }
     }
 }
