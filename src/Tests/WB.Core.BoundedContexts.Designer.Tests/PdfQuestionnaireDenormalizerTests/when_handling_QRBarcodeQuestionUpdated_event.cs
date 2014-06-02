4e50960b7f45d1db9be692ca2f6b77e9dfa1c505
Roman Lytvyn <litvin.roma@gmail.com>
2014-06-02 11:34:41 +0300

    designer: fixed bug with qr barcode question for exported pdf. KP-3569
    Former-commit-id: 96db8a2c2908df1d95919e24eabef9b53a5095b3

---
+++ src/Tests/WB.Core.BoundedContexts.Designer.Tests/PdfQuestionnaireDenormalizerTests/when_handling_QRBarcodeQuestionUpdated_event.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Machine.Specifications;
+using Main.Core.Entities.SubEntities;
+using Moq;
+using WB.Core.BoundedContexts.Designer.Views.Questionnaire.Pdf;
+using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
+using It = Machine.Specifications.It;
+
+namespace WB.Core.BoundedContexts.Designer.Tests.PdfQuestionnaireDenormalizerTests
+{
+    internal class when_handling_QRBarcodeQuestionUpdated_event : PdfQuestionnaireDenormalizerTestContext
+    {
+        Establish context = () =>
+        {
+            var pdfGroupView = CreateGroup(Guid.Parse(parentGroupId));
+            pdfGroupView.AddChild(CreateQuestion(Guid.Parse(questionId)));
+            pdfQuestionnaireDocument = CreatePdfQuestionnaire(pdfGroupView);
+
+            var documentStorage =
+                Mock.Of<IReadSideRepositoryWriter<PdfQuestionnaireView>>(
+                    writer => writer.GetById(Moq.It.IsAny<string>()) == pdfQuestionnaireDocument);
+
+            denormalizer = CreatePdfQuestionnaireDenormalizer(documentStorage: documentStorage);
+        };
+
+        Because of = () =>
+            denormalizer.Handle(Create.QRBarcodeQuestionUpdatedEvent(questionId: questionId,
+                questionTitle: questionTitle, questionVariable: questionVariable,
+                questionConditionExpression: questionEnablementCondition));
+
+        It should_question_not_be_null = () =>
+            GetQuestion().ShouldNotBeNull();
+
+        It should_question_type_be_QRBarcode = () =>
+            GetQuestion().QuestionType.ShouldEqual(QuestionType.QRBarcode);
+
+        It should_question_title_be_equal_to_specified_title = () =>
+            GetQuestion().Title.ShouldEqual(questionTitle);
+
+        It should_question_title_be_equal_to_specified_var_name = () =>
+            GetQuestion().Variable.ShouldEqual(questionVariable);
+
+        It should_question_title_be_equal_to_specified_enablement_condition = () =>
+            GetQuestion().ConditionExpression.ShouldEqual(questionEnablementCondition);
+
+        private static PdfQuestionView GetQuestion()
+        {
+            return pdfQuestionnaireDocument.GetQuestion(Guid.Parse(questionId));
+        }
+
+        private static PdfQuestionnaireDenormalizer denormalizer;
+        private static string questionId = "11111111111111111111111111111111";
+        private static string parentGroupId = "22222222222222222222222222222222";
+        private static string questionTitle = "someTitle";
+        private static string questionVariable = "var";
+        private static string questionEnablementCondition = "some condition";
+        private static PdfQuestionnaireView pdfQuestionnaireDocument;
+    }
+}
