0d933092ffa05693691a54b010f14e6d0d7e389a
Anatoliy 'TLK' Kolesnick <none@none>
2014-06-03 16:41:05 +0300

    CapiTester: updated answer progress indicator image and layout KP-3561
    Former-commit-id: 5c4c45bff8d902dbe076919aec1a30c32fc37dab

--- src/UI/QuestionnaireTester/WB.UI.QuestionnaireTester/Resources/Resource.Designer.cs
+++ src/UI/QuestionnaireTester/WB.UI.QuestionnaireTester/Resources/Resource.Designer.cs
@@ -140,49 +140,52 @@ namespace WB.UI.QuestionnaireTester
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int bookmarkStyle = 2130837504;
+			public const int answer_progress_indicator = 2130837504;
 			
 			// aapt resource value: 0x7f020001
-			public const int capi = 2130837505;
+			public const int bookmarkStyle = 2130837505;
 			
 			// aapt resource value: 0x7f020002
-			public const int CounterRoundShape = 2130837506;
+			public const int capi = 2130837506;
 			
 			// aapt resource value: 0x7f020003
-			public const int donecountershape = 2130837507;
+			public const int CounterRoundShape = 2130837507;
 			
 			// aapt resource value: 0x7f020004
-			public const int errorwarningstyle = 2130837508;
+			public const int donecountershape = 2130837508;
 			
 			// aapt resource value: 0x7f020005
-			public const int grid_headerItem = 2130837509;
+			public const int errorwarningstyle = 2130837509;
 			
 			// aapt resource value: 0x7f020006
-			public const int Icon = 2130837510;
+			public const int grid_headerItem = 2130837510;
 			
 			// aapt resource value: 0x7f020007
-			public const int questionAnsweredShape = 2130837511;
+			public const int Icon = 2130837511;
 			
 			// aapt resource value: 0x7f020008
-			public const int questionDisabledShape = 2130837512;
+			public const int questionAnsweredShape = 2130837512;
 			
 			// aapt resource value: 0x7f020009
-			public const int questionInvalidShape = 2130837513;
+			public const int questionDisabledShape = 2130837513;
 			
 			// aapt resource value: 0x7f02000a
-			public const int questionShape = 2130837514;
+			public const int questionInvalidShape = 2130837514;
 			
 			// aapt resource value: 0x7f02000b
-			public const int splash = 2130837515;
+			public const int questionShape = 2130837515;
 			
 			// aapt resource value: 0x7f02000c
-			public const int statistics_row_style = 2130837516;
+			public const int splash = 2130837516;
 			
 			// aapt resource value: 0x7f02000d
-			public const int tester_icon = 2130837517;
+			public const int statistics_row_style = 2130837517;
 			
 			// aapt resource value: 0x7f02000e
-			public const int tester_splash = 2130837518;
+			public const int tester_icon = 2130837518;
+			
+			// aapt resource value: 0x7f02000f
+			public const int tester_splash = 2130837519;
 			
 			static Drawable()
 			{
