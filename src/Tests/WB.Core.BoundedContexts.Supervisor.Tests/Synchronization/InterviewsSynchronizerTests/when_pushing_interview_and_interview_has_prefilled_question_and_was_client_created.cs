393b937c424e96acbcd88ecbcc619f799afce3eb
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2014-06-03 20:07:11 -0400

    InterviewsSynchronizer: Tests were added [KP-3557]
    Former-commit-id: 7295f9986fa34805081bf56b84035868fbfc333d

---
+++ src/Tests/WB.Core.BoundedContexts.Supervisor.Tests/Synchronization/InterviewsSynchronizerTests/when_pushing_interview_and_interview_has_prefilled_question_and_was_client_created.cs
@@ -0,0 +1,134 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Machine.Specifications;
+using Main.Core.Events;
+using Moq;
+using Moq.Protected;
+using Ncqrs.Commanding.ServiceModel;
+using Ncqrs.Eventing;
+using Ncqrs.Eventing.Storage;
+using WB.Core.BoundedContexts.Supervisor.Interviews.Implementation.Views;
+using WB.Core.BoundedContexts.Supervisor.Synchronization.Implementation;
+using WB.Core.GenericSubdomains.Logging;
+using WB.Core.GenericSubdomains.Utils;
+using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
+using WB.Core.SharedKernel.Structures.Synchronization;
+using WB.Core.SharedKernel.Utils.Serialization;
+using WB.Core.SharedKernels.SurveyManagement.Views.Interview;
+using it = Moq.It;
+using It = Machine.Specifications.It;
+
+namespace WB.Core.BoundedContexts.Supervisor.Tests.Synchronization.InterviewsSynchronizerTests
+{
+    internal class when_pushing_interview_and_interview_has_prefilled_question_and_was_client_created : InterviewsSynchronizerTestsContext
+    {
+        private Establish context = () =>
+        {
+            string positiveResponse = ":)";
+
+            loggerMock
+                .Setup(logger => logger.Error(it.IsAny<string>(), it.IsAny<Exception>()))
+                .Callback<string, Exception>((error, exception) => lastLoggedException = exception);
+
+            var httpMessageHandler = Mock.Of<HttpMessageHandler>();
+
+            Mock.Get(httpMessageHandler)
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+                .Returns(Task<HttpResponseMessage>.Factory.StartNew(() =>
+                    new HttpResponseMessage(HttpStatusCode.OK) { Content = new StringContent(positiveResponse) }))
+                .Callback<HttpRequestMessage, CancellationToken>((message, token) =>
+                    contentSentToHq = message.Content.ReadAsStringAsync().Result);
+
+            var readyToSendInterviewsRepositoryWriter =
+                Mock.Of<IQueryableReadSideRepositoryWriter<ReadyToSendToHeadquartersInterview>>(writer
+                    =>
+                    writer.QueryAll(it.IsAny<Expression<Func<ReadyToSendToHeadquartersInterview, bool>>>()) ==
+                        new[] { new ReadyToSendToHeadquartersInterview(interviewId) });
+
+            interviewEvent = Create.CommittedEvent(eventSourceId: interviewId, origin: null);
+            var eventStore = Mock.Of<IEventStore>(store
+                => store.ReadFrom(interviewId, 0, it.IsAny<long>()) == new CommittedEventStream(interviewId, interviewEvent));
+
+            InterviewSummary interviewSummary = Create.InterviewSummary();
+            interviewSummary.WasCreatedOnClient = true;
+            var answersToFeaturedQuestions = new Dictionary<Guid,QuestionAnswer>();
+            answersToFeaturedQuestions.Add(questionId,
+                new QuestionAnswer() { Answer = questionAnswer, Id = answerId, Title = questionTitle });
+            interviewSummary.AnswersToFeaturedQuestions = answersToFeaturedQuestions; 
+
+            var interviewSummaryRepositoryWriter = Mock.Of<IReadSideRepositoryWriter<InterviewSummary>>(writer
+                => writer.GetById(interviewId.FormatGuid()) == interviewSummary);
+
+            var jsonUtils = Mock.Of<IJsonUtils>(utils
+                => utils.Deserrialize<bool>(positiveResponse) == true);
+
+            Mock.Get(jsonUtils)
+                .Setup(utils => utils.GetItemAsContent(it.IsAny<AggregateRootEvent[]>()))
+                .Returns(eventsJson)
+                .Callback<object>(entity => events = (AggregateRootEvent[]) entity);
+
+            Mock.Get(jsonUtils)
+                .Setup(utils => utils.GetItemAsContent(it.IsAny<InterviewMetaInfo>()))
+                .Returns(metadataJson)
+                .Callback<object>(entity => metadata = (InterviewMetaInfo) entity);
+
+            Mock.Get(jsonUtils)
+                .Setup(utils => utils.GetItemAsContent(it.IsAny<SyncItem>()))
+                .Returns(syncItemJson)
+                .Callback<object>(entity => syncItem = (SyncItem) entity);
+
+            interviewsSynchronizer = Create.InterviewsSynchronizer(
+                readyToSendInterviewsRepositoryWriter: readyToSendInterviewsRepositoryWriter,
+                interviewSummaryRepositoryWriter: interviewSummaryRepositoryWriter,
+                eventStore: eventStore,
+                logger: loggerMock.Object,
+                jsonUtils: jsonUtils,
+                httpMessageHandler: httpMessageHandler,
+                commandService: commandServiceMock.Object);
+        };
+
+        private Because of = () =>
+            interviewsSynchronizer.Push(userId);
+
+
+        private It should_set_metadata_featuredquestions_meta_not_empty = () =>
+            metadata.FeaturedQuestionsMeta.Count().ShouldEqual(1);
+
+        private It should_set_metadata_featuredquestion_public_key_to_questionId = () =>
+            metadata.FeaturedQuestionsMeta.Single().PublicKey.ShouldEqual(questionId);
+
+        private It should_set_metadata_featuredquestion_title_to_question_title = () =>
+            metadata.FeaturedQuestionsMeta.Single().Title.ShouldEqual(questionTitle);
+
+        private It should_set_metadata_featuredquestion_value_to_question_answer = () =>
+            metadata.FeaturedQuestionsMeta.Single().Value.ShouldEqual(questionAnswer);
+
+
+        private static InterviewsSynchronizer interviewsSynchronizer;
+        private static Guid userId = Guid.Parse("DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD");
+        private static Guid interviewId = Guid.Parse("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+        private static Mock<ILogger> loggerMock = new Mock<ILogger>();
+        private static Exception lastLoggedException;
+        private static string contentSentToHq;
+        private static string syncItemJson = "sync item json";
+        private static string eventsJson = "events json";
+        private static string metadataJson = "metadata json";
+        private static AggregateRootEvent[] events;
+        private static InterviewMetaInfo metadata;
+        private static SyncItem syncItem;
+        private static CommittedEvent interviewEvent;
+        private static Mock<ICommandService> commandServiceMock = new Mock<ICommandService>();
+
+        private static Guid questionId = Guid.Parse("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAB");
+        private static Guid answerId = Guid.Parse("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAc");
+        private static string questionTitle = "title";
+        private static string questionAnswer = "answer";
+    }
+}
