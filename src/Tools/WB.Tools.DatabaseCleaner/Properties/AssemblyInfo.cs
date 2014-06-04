e57656eb018a60f0d0254868df60c982ec797325
Anastasya Kholodova <nastya.kholodova@gmail.com>
2014-06-04 16:28:59 -0400

    CleaningTool: first version of cleaning tool KP-3585
    --HG--
    branch : hotfixes-for-v-2-0-1
    
    
    Former-commit-id: 97a6f8dcf48c7de46f174f55dcb9132fc5d0c103

--- src/Tools/WB.Tools.DatabaseCleaner/Properties/AssemblyInfo.cs
+++ src/Tools/WB.Tools.DatabaseCleaner/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WB.Core.Infrastructure.Raven")]
+[assembly: AssemblyTitle("WB.Tools.DatabaseCleaner")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WB.Core.Infrastructure.Raven")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyProduct("WB.Tools.DatabaseCleaner")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5063700b-8316-43fc-bcac-72b5b540b3a7")]
+[assembly: Guid("655ee94e-5d2a-4e3e-a31b-696ace83f994")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -34,11 +34,3 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-
-[assembly: InternalsVisibleTo("WB.Core.Infrastructure.Raven.Tests")]
-[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
-[assembly: InternalsVisibleTo("LoadTestDataGenerator")]
-[assembly: InternalsVisibleTo("WB.Tools.CapiDataGenerator")]
-[assembly: InternalsVisibleTo("WB.Tools.EventsConverter")]
-[assembly: InternalsVisibleTo("WB.Tools.InterviewExtractor")]
-[assembly: InternalsVisibleTo("WB.Tools.DatabaseCleaner")]
