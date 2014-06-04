67d3d4b94f9411fbc59063c6560a639e7499b809
Anatoliy 'TLK' Kolesnick <none@none>
2014-06-04 14:00:37 +0300

    Capi: implemented answer progress indicator KP-3561
    Former-commit-id: fcb1b9aa45a1099975fdf5ecbff25e973b79eb30

--- src/UI/Capi/WB.UI.Capi/Implementations/Activities/DataCollectionDetailsActivity.cs
+++ src/UI/Capi/WB.UI.Capi/Implementations/Activities/DataCollectionDetailsActivity.cs
@@ -1,7 +1,11 @@
 using System;
 using Android.App;
 using Android.Content.PM;
+using Android.OS;
+using Android.Views;
+using Android.Widget;
 using CAPI.Android.Core.Model.SnapshotStore;
+using Microsoft.Practices.ServiceLocation;
 using Ncqrs;
 using Ncqrs.Eventing.Storage;
 using WB.Core.BoundedContexts.Capi.Views.InterviewDetails;
@@ -9,12 +13,18 @@ using WB.Core.SharedKernels.DataCollection.ValueObjects.Interview;
 using WB.UI.Capi.Implementations.Adapters;
 using WB.UI.Shared.Android.Activities;
 using WB.UI.Shared.Android.Adapters;
+using WB.UI.Shared.Android.Controls.ScreenItems;
 
 namespace WB.UI.Capi.Implementations.Activities
 {
     [Activity(ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.KeyboardHidden | ConfigChanges.ScreenSize)]
     public class DataCollectionDetailsActivity : DetailsActivity
     {
+        private IAnswerProgressIndicator AnswerProgressIndicator
+        {
+            get { return ServiceLocator.Current.GetInstance<IAnswerProgressIndicator>(); }
+        }
+
         protected override ContentFrameAdapter CreateFrameAdapter(InterviewItemId? screenId)
         {
             return new DataCollectionContentFrameAdapter(this.SupportFragmentManager, this.ViewModel as InterviewViewModel, screenId);
@@ -25,6 +35,12 @@ namespace WB.UI.Capi.Implementations.Activities
             return CapiApplication.LoadView<QuestionnaireScreenInput, InterviewViewModel>(new QuestionnaireScreenInput(interviewId));
         }
 
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            this.SetupActionBar();
+        }
+
         public override void Finish()
         {
             base.Finish();
@@ -32,6 +48,24 @@ namespace WB.UI.Capi.Implementations.Activities
             var snapshotStore = NcqrsEnvironment.Get<ISnapshotStore>() as AndroidSnapshotStore;
             if (snapshotStore != null)
                 snapshotStore.PersistShapshot(this.QuestionnaireId);
+        }
+
+        private void SetupActionBar()
+        {
+            this.ActionBar.SetDisplayShowHomeEnabled(false);
+            this.ActionBar.SetDisplayShowTitleEnabled(false);
+            this.ActionBar.SetDisplayShowCustomEnabled(true);
+            this.ActionBar.SetDisplayUseLogoEnabled(true);
+            this.ActionBar.SetCustomView(Resource.Layout.InterviewActionBar);
+
+            var txtTitle = (TextView)this.ActionBar.CustomView.FindViewById(Resource.Id.txtTitle);
+            txtTitle.Text = this.Model.Title;
+
+            var imgProgress = (ImageView)this.ActionBar.CustomView.FindViewById(Resource.Id.imgAnswerProgress);
+
+            this.AnswerProgressIndicator.Setup(
+                show: () => this.RunOnUiThread(() => imgProgress.Visibility = ViewStates.Visible),
+                hide: () => this.RunOnUiThread(() => imgProgress.Visibility = ViewStates.Invisible));
         }
     }
 }
