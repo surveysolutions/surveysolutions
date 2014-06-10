f7939f65e2c74059a5cdaa5287c3c19256c42b5e
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2014-06-10 12:12:13 +0300

    SurveyManagement.Web: installed asp.net mvc and added area registration KP-3302
    Former-commit-id: 73e1231e1185c967e883c95cf968a7bf9be1f4b1

---
+++ src/Core/SharedKernels/SurveyManagement/SurveyManagement.Web/SurveyManagementAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace WB.Core.SharedKernels.SurveyManagement.Web
+{
+    public class SurveyManagementAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "SurveyManagement";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "SurveyManagement_default",
+                "SurveyManagement/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
