4da6ceef7828bc1c13483801dc84a404675525e1
Slava Scherbak <slava.scherbak@gmail.com>
2014-06-10 21:27:17 +0300

    Syncronization\Handshake: added version parameter to handshake between CAPRI and SV and error message if versions are incompatible KP-3584
    --HG--
    branch : hotfixes-for-v-2-0-2
    
    
    Former-commit-id: f5b4c25211594f24572f3508f351296249ecb398

--- src/UI/Capi/WB.UI.Capi/Syncronization/Handshake/RestHandshake.cs
+++ src/UI/Capi/WB.UI.Capi/Syncronization/Handshake/RestHandshake.cs
@@ -1,7 +1,8 @@
 using System.Collections.Generic;
-using RestSharp;
+using System.Globalization;
 using WB.Core.GenericSubdomains.Rest;
 using WB.Core.SharedKernel.Structures.Synchronization;
+using WB.UI.Capi.Settings;
 
 namespace WB.UI.Capi.Syncronization.Handshake
 {
@@ -21,6 +22,7 @@ namespace WB.UI.Capi.Syncronization.Handshake
             var package = this.webExecutor.ExecuteRestRequest<HandshakePackage>(handshakePath,
                 login, password, null,
                 new KeyValuePair<string, string>("clientId", appID),
+                new KeyValuePair<string, string>("version", SettingsManager.AppVersionCode().ToString(CultureInfo.InvariantCulture)),
                 new KeyValuePair<string, string>("clientRegistrationId", registrationKey),
                 new KeyValuePair<string, string>("androidId", androidId));
 
