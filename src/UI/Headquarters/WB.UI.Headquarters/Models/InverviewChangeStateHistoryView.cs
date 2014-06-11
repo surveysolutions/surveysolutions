5ab0ee0cae9638584108f7cf0fd798cd878aef64
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2014-06-11 19:43:13 +0300

    SurveyManagement.Web: extracted models except api models and app-specific models (namespaces not updated so far) KP-3302
    Former-commit-id: 47ca23a274e73fe022c8117cadf43fcd2211067b

--- src/UI/Headquarters/WB.UI.Headquarters/Models/InverviewChangeStateHistoryView.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace WB.UI.Headquarters.Models
-{
-    public class InverviewChangeStateHistoryView
-    {
-        public IEnumerable<HistoryItemView> HistoryItems { get; set; }
-    }
-}
