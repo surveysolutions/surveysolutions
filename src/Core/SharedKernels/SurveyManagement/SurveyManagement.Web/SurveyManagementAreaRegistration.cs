c389f66ce77f4d3c308a5b3f2ab5c30d780ea47f
Anatoliy 'TLK' Kolesnick <none@none>
2014-06-11 19:21:25 +0300

    SurveyManagement.Web: refactored js bundles registrations KP-3302
    Former-commit-id: c617bde668a13dd083456013863dc9af8195930f

--- src/Core/SharedKernels/SurveyManagement/SurveyManagement.Web/SurveyManagementAreaRegistration.cs
+++ src/Core/SharedKernels/SurveyManagement/SurveyManagement.Web/SurveyManagementAreaRegistration.cs
@@ -6,6 +6,69 @@ namespace WB.Core.SharedKernels.SurveyManagement.Web
 {
     public class SurveyManagementAreaRegistration : AreaRegistration 
     {
+        private static readonly Dictionary<string, string[]> ScriptBundles = new Dictionary<string, string[]>
+        {
+            {
+                "~/js/main", new[]
+                {
+                    "~/Scripts/jquery-{version}.js",
+                    "~/Scripts/moment.js",
+                    "~/Scripts/modernizr-{version}.js",
+                    "~/Scripts/bootstrap.js",
+                    "~/Scripts/director.js",
+                    "~/Scripts/knockout-{version}.js",
+                    "~/Scripts/knockout.mapping-latest.js",
+                    "~/Scripts/query-string.js",
+                    "~/Scripts/supervisor.framework.js",
+                    "~/Scripts/viewmodels/viewmodelbase.js",
+                    "~/Scripts/viewmodels/pagebase.js",
+                }
+            },
+            {
+                "~/validate", new[]
+                {
+                    "~/Scripts/jquery.validate.js",
+                    "~/Scripts/jquery.validate.unobtrusive-custom-for-bootstrap.js"
+                }
+            },
+            {
+                "~/js/list", new[]
+                {
+                    "~/Scripts/ko.pager.js",
+                    "~/Scripts/viewmodels/listview.js"
+                }
+            },
+            {
+                "~/js/interview-general", new[]
+                {
+                    "~/Scripts/knockout.validation.js",
+                    "~/Scripts/bootstrap-datepicker.js",
+                    "~/Scripts/Math.uuid.js",
+                    "~/Scripts/viewmodels/pages/interview/custom.js",
+                    "~/Scripts/lodash.underscore.js",
+                }
+            },
+            {
+                "~/js/interview-new", new[]
+                {
+                    "~/Scripts/viewmodels/pages/interview/new/datacontext.js",
+                    "~/Scripts/viewmodels/pages/interview/new/mapper.js",
+                    "~/Scripts/viewmodels/pages/interview/new/model.js",
+                    "~/Scripts/viewmodels/pages/interview/new/newinterview.js"
+                }
+            },
+            {
+                "~/js/interview-details", new[]
+                {
+                    "~/Scripts/director.js",
+                    "~/Scripts/viewmodels/pages/interview/details/config.js",
+                    "~/Scripts/viewmodels/pages/interview/details/datacontext.js",
+                    "~/Scripts/viewmodels/pages/interview/details/interviewdetailssettings.js",
+                    "~/Scripts/viewmodels/pages/interview/details/interviewdetails.js",
+                }
+            },
+        };
+
         public override string AreaName 
         {
             get 
@@ -22,58 +85,10 @@ namespace WB.Core.SharedKernels.SurveyManagement.Web
 
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/js/main").Include(new[]
+            foreach (KeyValuePair<string, string[]> scriptBundle in ScriptBundles)
             {
-                "~/Scripts/jquery-{version}.js",
-                "~/Scripts/moment.js",
-                "~/Scripts/modernizr-{version}.js",
-                "~/Scripts/bootstrap.js",
-                "~/Scripts/director.js",
-                "~/Scripts/knockout-{version}.js",
-                "~/Scripts/knockout.mapping-latest.js",
-                "~/Scripts/query-string.js",
-                "~/Scripts/supervisor.framework.js",
-                "~/Scripts/viewmodels/viewmodelbase.js",
-                "~/Scripts/viewmodels/pagebase.js"
-            }));
-
-            bundles.Add(new ScriptBundle("~/validate").Include(new[]
-            {
-                "~/Scripts/jquery.validate.js",
-                "~/Scripts/jquery.validate.unobtrusive-custom-for-bootstrap.js"
-            }));
-
-            bundles.Add(new ScriptBundle("~/js/list").Include(new[]
-            {
-                "~/Scripts/ko.pager.js",
-                "~/Scripts/viewmodels/listview.js"
-            }));
-
-            bundles.Add(new ScriptBundle("~/js/interview-general").Include(new[]
-            {
-                "~/Scripts/knockout.validation.js",
-                "~/Scripts/bootstrap-datepicker.js",
-                "~/Scripts/Math.uuid.js",
-                "~/Scripts/viewmodels/pages/interview/custom.js",
-                "~/Scripts/lodash.underscore.js",
-            }));
-
-            bundles.Add(new ScriptBundle("~/js/interview-new").Include(new[]
-            {
-                "~/Scripts/viewmodels/pages/interview/new/datacontext.js",
-                "~/Scripts/viewmodels/pages/interview/new/mapper.js",
-                "~/Scripts/viewmodels/pages/interview/new/model.js",
-                "~/Scripts/viewmodels/pages/interview/new/newinterview.js"
-            }));
-
-            bundles.Add(new ScriptBundle("~/js/interview-details").Include(new[]
-            {
-                "~/Scripts/director.js",
-                "~/Scripts/viewmodels/pages/interview/details/config.js",
-                "~/Scripts/viewmodels/pages/interview/details/datacontext.js",
-                "~/Scripts/viewmodels/pages/interview/details/interviewdetailssettings.js",
-                "~/Scripts/viewmodels/pages/interview/details/interviewdetails.js",
-            }));
+                bundles.Add(new ScriptBundle(scriptBundle.Key).Include(scriptBundle.Value));
+            }
         }
 
         private static void RegisterRoutes(AreaRegistrationContext context)
