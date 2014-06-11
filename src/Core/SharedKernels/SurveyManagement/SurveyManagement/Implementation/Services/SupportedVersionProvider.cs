3ab8c4d0dd95bdd19ac1b77c9df0ff7fbf89ff21
Slava Scherbak <slava.scherbak@gmail.com>
2014-06-11 18:47:14 +0300

    Supervisor: refactored code after code review. getting of version was moved to version provider KP-3584
    --HG--
    branch : hotfixes-for-v-2-0-2
    
    
    Former-commit-id: 6dda5e1c75972f35a36f73467386338c51519e82

--- src/Core/SharedKernels/SurveyManagement/SurveyManagement/Implementation/Services/SupportedVersionProvider.cs
+++ src/Core/SharedKernels/SurveyManagement/SurveyManagement/Implementation/Services/SupportedVersionProvider.cs
@@ -1,4 +1,5 @@
-﻿using WB.Core.SharedKernels.QuestionnaireVerification.ValueObjects;
+﻿using System;
+using WB.Core.SharedKernels.QuestionnaireVerification.ValueObjects;
 using WB.Core.SharedKernels.SurveyManagement.Services;
 
 namespace WB.Core.SharedKernels.SurveyManagement.Implementation.Services
@@ -6,18 +7,30 @@ namespace WB.Core.SharedKernels.SurveyManagement.Implementation.Services
     public class SupportedVersionProvider : ISupportedVersionProvider
     {
         private static QuestionnaireVersion supportedQuestionnaireVersion;
+        private static bool isDebugMode;
+        private static Version buildVersion;
 
-        public SupportedVersionProvider(ApplicationVersionSettings settings)
+        public SupportedVersionProvider(ApplicationVersionSettings settings, bool isDebug, Version applicationBuildVersion)
         {
             supportedQuestionnaireVersion = new QuestionnaireVersion(
                 settings.SupportedQuestionnaireVersionMajor,
                 settings.SupportedQuestionnaireVersionMinor,
                 settings.SupportedQuestionnaireVersionPatch);
+
+            isDebugMode = isDebug;
+            buildVersion = applicationBuildVersion;
         }
 
         public QuestionnaireVersion GetSupportedQuestionnaireVersion()
         {
             return supportedQuestionnaireVersion;
+        }
+
+        public int? GetApplicationBuildNumber()
+        {
+            if (isDebugMode)
+                return null;
+            return buildVersion.Revision;
         }
     }
 }
