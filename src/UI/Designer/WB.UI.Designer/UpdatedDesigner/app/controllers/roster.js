dbc3e2465344c56d1e9faf420d3acdce0d94c963
Dima Pasko <dimapasko@gmail.com>
2014-06-11 19:33:01 -0400

    Designer: bind fixed titles KP-3404 UI: edit roster
    Former-commit-id: 6443715063e6c15affede98a95747db38a5a5e03

--- src/UI/Designer/WB.UI.Designer/UpdatedDesigner/app/controllers/roster.js
+++ src/UI/Designer/WB.UI.Designer/UpdatedDesigner/app/controllers/roster.js
@@ -8,11 +8,13 @@ angular.module('designerApp')
             $scope.loadRoster = function() {
                 questionnaireService.getRosterDetailsById($routeParams.questionnaireId, $scope.activeRoster.itemId).success(function(result) {
                         $scope.activeRoster.breadcrumbs = result.breadcrumbs;
+                        $scope.activeRoster.numerics = result.numericIntegerQuestions["new Chapter"];
 
                         var roster = result.roster;
                         $scope.activeRoster.description = roster.description;
                         $scope.activeRoster.enablementCondition = roster.enablementCondition;
                         $scope.activeRoster.rosterSizeSourceType = roster.rosterSizeSourceType;
+                        $scope.activeRoster.rosterFixedTitles = roster.rosterFixedTitles;
                     }
                 );
             }
