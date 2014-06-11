fb4ade13d79f91d9ffb1f420e15a7c0855db2972
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2014-06-11 19:00:59 +0300

    SurveyManagement.Web: extracted js bundles registrations KP-3302
    Former-commit-id: b9dc005c79d310d87c6f679e9399bc80947e7d7a

--- src/Core/SharedKernels/SurveyManagement/SurveyManagement.Web/SurveyManagementAreaRegistration.cs
+++ src/Core/SharedKernels/SurveyManagement/SurveyManagement.Web/SurveyManagementAreaRegistration.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
 using System.Web.Optimization;
 
 namespace WB.Core.SharedKernels.SurveyManagement.Web
@@ -21,6 +22,50 @@ namespace WB.Core.SharedKernels.SurveyManagement.Web
 
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new ScriptBundle("~/js/main").Include(new[]
+            {
+                "~/Scripts/jquery-{version}.js",
+                "~/Scripts/moment.js",
+                "~/Scripts/modernizr-{version}.js",
+                "~/Scripts/bootstrap.js",
+                "~/Scripts/director.js",
+                "~/Scripts/knockout-{version}.js",
+                "~/Scripts/knockout.mapping-latest.js",
+                "~/Scripts/query-string.js",
+                "~/Scripts/supervisor.framework.js",
+                "~/Scripts/viewmodels/viewmodelbase.js",
+                "~/Scripts/viewmodels/pagebase.js"
+            }));
+
+            bundles.Add(new ScriptBundle("~/validate").Include(new[]
+            {
+                "~/Scripts/jquery.validate.js",
+                "~/Scripts/jquery.validate.unobtrusive-custom-for-bootstrap.js"
+            }));
+
+            bundles.Add(new ScriptBundle("~/js/list").Include(new[]
+            {
+                "~/Scripts/ko.pager.js",
+                "~/Scripts/viewmodels/listview.js"
+            }));
+
+            bundles.Add(new ScriptBundle("~/js/interview-general").Include(new[]
+            {
+                "~/Scripts/knockout.validation.js",
+                "~/Scripts/bootstrap-datepicker.js",
+                "~/Scripts/Math.uuid.js",
+                "~/Scripts/viewmodels/pages/interview/custom.js",
+                "~/Scripts/lodash.underscore.js",
+            }));
+
+            bundles.Add(new ScriptBundle("~/js/interview-new").Include(new[]
+            {
+                "~/Scripts/viewmodels/pages/interview/new/datacontext.js",
+                "~/Scripts/viewmodels/pages/interview/new/mapper.js",
+                "~/Scripts/viewmodels/pages/interview/new/model.js",
+                "~/Scripts/viewmodels/pages/interview/new/newinterview.js"
+            }));
+
             bundles.Add(new ScriptBundle("~/js/interview-details").Include(new[]
             {
                 "~/Scripts/director.js",
