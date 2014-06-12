82c9f65e9955d50c3baa6c2840e0173eee8f57c2
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2014-06-12 12:22:40 -0400

    CAPI: unit tests for pull questionnaire template by capi KP-3600
    Former-commit-id: 4fb17802bf7ff9ab1f2e27de41addd4ff98e3ba3

---
+++ src/Tests/WB.Core.BoundedContexts.Capi.Synchronization.Tests/PullDataProcessorTests/when_sync_pachage_contains_information_about_existing_user.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Machine.Specifications;
+using Main.Core.Documents;
+using Main.Core.Entities.SubEntities;
+using Main.Core.View;
+using Moq;
+using Ncqrs.Commanding.ServiceModel;
+using WB.Core.BoundedContexts.Capi.Synchronization.Synchronization.Pull;
+using WB.Core.BoundedContexts.Capi.Synchronization.Views.Login;
+using WB.Core.SharedKernel.Structures.Synchronization;
+using WB.Core.SharedKernel.Utils.Serialization;
+using WB.Core.SharedKernels.DataCollection.Commands.User;
+using It = Machine.Specifications.It;
+
+namespace WB.Core.BoundedContext.Capi.Synchronization.Tests.PullDataProcessorTests
+{
+    internal class when_sync_pachage_contains_information_about_existing_user : PullDataProcessorTestContext
+    {
+        Establish context = () =>
+        {
+            userDocument = new UserDocument()
+            {
+                PublicKey = Guid.NewGuid(),
+                Email = "test@ddd.cc",
+                Roles = new List<UserRoles> { UserRoles.Operator },
+                UserName = "name",
+                Password = "password",
+                IsLockedByHQ = false,
+                IsLockedBySupervisor = false,
+                Supervisor = new UserLight(Guid.NewGuid(), "super")
+            };
+
+            var jsonUtilsMock = new Mock<IJsonUtils>();
+            jsonUtilsMock.Setup(x => x.Deserrialize<UserDocument>(Moq.It.IsAny<string>())).Returns(userDocument);
+            syncItem = new SyncItem() { ItemType = SyncItemType.User, IsCompressed = false, Content = "some content" };
+
+            commandService = new Mock<ICommandService>();
+
+            var loginViewFactoryMock = new Mock<IViewFactory<LoginViewInput, LoginView>>();
+            loginViewFactoryMock.Setup(x => x.Load(Moq.It.IsAny<LoginViewInput>())).Returns(new LoginView(Guid.NewGuid(), "test"));
+            pullDataProcessor = CreatePullDataProcessor(commandService.Object, jsonUtilsMock.Object, loginViewFactoryMock.Object);
+        };
+
+        Because of = () => pullDataProcessor.Process(syncItem);
+
+        private It should_call_ChangeUserCommand_once =
+            () =>
+                commandService.Verify(
+                    x =>
+                        x.Execute(
+                            Moq.It.Is<ChangeUserCommand>(
+                                param =>
+                                    param.PublicKey == userDocument.PublicKey && param.Email == userDocument.Email &&
+                                        param.IsLockedByHQ == userDocument.IsLockedByHQ && 
+                                        param.IsLockedBySupervisor == userDocument.IsLockedBySupervisor &&
+                                        param.Roles.SequenceEqual(userDocument.Roles) &&
+                                        param.PasswordHash == userDocument.Password), null),
+                    Times.Once);
+
+        private static PullDataProcessor pullDataProcessor;
+        private static SyncItem syncItem;
+        private static UserDocument userDocument;
+        private static Mock<ICommandService> commandService;
+    }
+}
---
+++ src/Tests/WB.Core.BoundedContexts.Capi.Synchronization.Tests/PullDataProcessorTests/when_sync_pachage_contains_information_about_questionnaire_with_broken_metadata.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Machine.Specifications;
+using Main.Core.Documents;
+using Moq;
+using Ncqrs.Commanding.ServiceModel;
+using WB.Core.BoundedContexts.Capi.Synchronization.Synchronization.Pull;
+using WB.Core.SharedKernel.Structures.Synchronization;
+using WB.Core.SharedKernel.Utils.Serialization;
+using WB.Core.SharedKernels.DataCollection.Commands.Questionnaire;
+using WB.Core.SharedKernels.DataCollection.Repositories;
+using It = Machine.Specifications.It;
+
+namespace WB.Core.BoundedContext.Capi.Synchronization.Tests.PullDataProcessorTests
+{
+    internal class when_sync_pachage_contains_information_about_questionnaire_with_broken_metadata : PullDataProcessorTestContext
+    {
+        Establish context = () =>
+        {
+            questionnaireDocument = new QuestionnaireDocument()
+            {
+                PublicKey = Guid.NewGuid()
+            };
+
+            syncItem = new SyncItem() { ItemType = SyncItemType.Template, IsCompressed = true, Content = "some content", MetaInfo = "some metadata" };
+
+            var jsonUtilsMock = new Mock<IJsonUtils>();
+            jsonUtilsMock.Setup(x => x.Deserrialize<QuestionnaireDocument>(syncItem.Content)).Returns(questionnaireDocument);
+            jsonUtilsMock.Setup(x => x.Deserrialize<QuestionnaireMetadata>(syncItem.MetaInfo)).Throws<NullReferenceException>();
+
+            commandService = new Mock<ICommandService>();
+
+            plainQuestionnaireRepositoryMock = new Mock<IPlainQuestionnaireRepository>();
+
+            pullDataProcessor = CreatePullDataProcessor(commandService.Object, jsonUtilsMock.Object, null,
+                plainQuestionnaireRepositoryMock.Object);
+        };
+
+        Because of = () => exception = Catch.Exception(() =>pullDataProcessor.Process(syncItem));
+
+        It should_call_RegisterPlainQuestionnaire_zero_times =
+            () =>
+                commandService.Verify(
+                    x =>
+                        x.Execute(
+                            Moq.It.Is<RegisterPlainQuestionnaire>(
+                                param =>
+                                    param.QuestionnaireId == questionnaireDocument.PublicKey && param.Version == Moq.It.IsAny<long>()), null),
+                    Times.Never);
+
+        It should_store_questionnaire_in_plaine_storage_zero_times =
+            () =>
+                plainQuestionnaireRepositoryMock.Verify(
+                    x => x.StoreQuestionnaire(questionnaireDocument.PublicKey, Moq.It.IsAny<long>(), questionnaireDocument),
+                    Times.Never);
+
+        It should_throw_ArgumentException = () =>
+            exception.ShouldBeOfType<ArgumentException>();
+
+        private static PullDataProcessor pullDataProcessor;
+        private static SyncItem syncItem;
+        private static QuestionnaireDocument questionnaireDocument;
+        private static Mock<ICommandService> commandService;
+        private static Mock<IPlainQuestionnaireRepository> plainQuestionnaireRepositoryMock;
+        private static Exception exception;
+    }
+}
