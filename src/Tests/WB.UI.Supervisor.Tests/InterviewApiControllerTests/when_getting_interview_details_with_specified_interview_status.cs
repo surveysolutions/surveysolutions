1390fd5614182d1b56898af1379e707e6d2c39ed
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2014-06-13 19:16:45 +0300

    SurveyManagement.Web: extracted api controllers from controllers folder KP-3302
    Former-commit-id: 4ebc12828ac49c7efc2171593a5abb4e6260c79c

--- src/Tests/WB.UI.Supervisor.Tests/InterviewApiControllerTests/when_getting_interview_details_with_specified_interview_status.cs
+++ src/Tests/WB.UI.Supervisor.Tests/InterviewApiControllerTests/when_getting_interview_details_with_specified_interview_status.cs
@@ -6,6 +6,7 @@ using Moq;
 using WB.Core.SharedKernels.DataCollection.ValueObjects.Interview;
 using WB.Core.SharedKernels.SurveyManagement.Views.Interview;
 using WB.Core.SharedKernels.SurveyManagement.Web.Code;
+using WB.Core.SharedKernels.SurveyManagement.Web.Controllers;
 using WB.Core.SharedKernels.SurveyManagement.Web.Models;
 using WB.UI.Supervisor.Controllers;
 using WB.UI.Supervisor.Models;
