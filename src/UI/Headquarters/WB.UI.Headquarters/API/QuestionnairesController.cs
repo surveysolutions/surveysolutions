e26061c64a1f86f3f7adbca9c3ee458fe5396317
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2014-06-13 17:43:28 +0300

    SurveyManagement.Web: extracted api controller UsersController KP-3302
    Former-commit-id: 5ab7393c95ea253ce17609d9f83f87de04a378e1

--- src/UI/Headquarters/WB.UI.Headquarters/API/QuestionnairesController.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/API/QuestionnairesController.cs
@@ -6,6 +6,7 @@ using WB.Core.GenericSubdomains.Logging;
 using WB.Core.SharedKernels.DataCollection.ValueObjects.Interview;
 using WB.Core.SharedKernels.DataCollection.Views.Questionnaire;
 using WB.Core.SharedKernels.DataCollection.Views.Questionnaire.BrowseItem;
+using WB.Core.SharedKernels.SurveyManagement.Web.Api;
 using WB.Core.SharedKernels.SurveyManagement.Web.Models.Api;
 
 namespace WB.UI.Headquarters.API
