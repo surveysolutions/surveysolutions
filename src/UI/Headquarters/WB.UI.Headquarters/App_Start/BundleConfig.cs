563601cb90787a3bef087a7ddc86117dfa3aa836
Anatoliy 'TLK' Kolesnick <none@none>
2014-06-13 13:14:18 +0300

    SurveyManagement.Web: extracted css files KP-3302
    Former-commit-id: 6b737250c941334956af1c89158c9e8a3b4615c0

--- src/UI/Headquarters/WB.UI.Headquarters/App_Start/BundleConfig.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/App_Start/BundleConfig.cs
@@ -12,31 +12,9 @@ namespace WB.UI.Headquarters
             bundles.IgnoreList.Ignore("*-vsdoc.js");
             bundles.IgnoreList.Ignore("*intellisense.js");
 
-            bundles.Add(new StyleBundle("~/Content/main").Include(
-                "~/Content/bootstrap.css",
-                "~/Content/font-awesome.min.css",
-                "~/Content/bootstrap-mvc-validation.css",
-                "~/Content/jquery.pnotify.default.css",
-                "~/Content/app.css"));
-
-            bundles.Add(new StyleBundle("~/css/main-not-loggedin").Include(
-                "~/Content/bootstrap.css",
-                "~/Content/bootstrap-mvc-validation.css",
-                "~/Content/main-not-logged.css"));
-
             bundles.Add(new StyleBundle("~/css/admin").Include(
                 "~/Content/bootstrap.css",
                 "~/Content/admin.css"));
-
-            bundles.Add(new StyleBundle("~/css/list").Include("~/Content/listview.css"));
-
-            bundles.Add(new StyleBundle("~/css/interview-new").Include(
-                "~/Content/bootstrap-editable.css",
-                "~/Content/datepicker.css"));
-
-            bundles.Add(new StyleBundle("~/css/interview").Include(
-                "~/Content/bootstrap-editable.css",
-                "~/Content/datepicker.css"));
         }
     }
 }
