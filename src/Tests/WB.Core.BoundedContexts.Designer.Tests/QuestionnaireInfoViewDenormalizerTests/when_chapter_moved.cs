d7d572234eca58447ec23506339f46a91285eac9
Andrii Kozhyn <hedgic@gmail.com>
2014-06-20 23:20:01 +0300

    KP-3654: Initial implementation of chapters move
    Former-commit-id: c8e6698e4299722f3c5d0b3cba65535f3b5c164a

---
+++ src/Tests/WB.Core.BoundedContexts.Designer.Tests/QuestionnaireInfoViewDenormalizerTests/when_chapter_moved.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Machine.Specifications;
+using Main.Core.Events.Questionnaire;
+using Ncqrs.Eventing.ServiceModel.Bus;
+using WB.Core.BoundedContexts.Designer.Views.Questionnaire.Edit.QuestionnaireInfo;
+using WB.Core.GenericSubdomains.Utils;
+
+namespace WB.Core.BoundedContexts.Designer.Tests.QuestionnaireInfoViewDenormalizerTests
+{
+    internal class when_chapter_moved : QuestionnaireInfoViewDenormalizerTestContext
+    {
+        Establish context = () =>
+        {
+            viewState = CreateQuestionnaireInfoViewWith1Chapter(questionnaireId);
+            denormalizer = CreateDenormalizer(viewState);
+            movedItemId = Guid.NewGuid().FormatGuid();
+
+            viewState = denormalizer.Create(Create.NewQuestionnaireCreatedEvent(questionnaireId));
+            denormalizer.Update(viewState, Create.NewGroupAddedEvent(groupId: groupId, parentGroupId: questionnaireId, groupTitle: "group title"));
+            denormalizer.Update(viewState, Create.NewGroupAddedEvent(groupId: movedItemId, parentGroupId: questionnaireId, groupTitle: "group title to move"));
+        };
+
+        Because of = () =>
+            viewState =
+                denormalizer.Update(viewState, Create.QuestionnaireItemMovedEvent(itemId: movedItemId));
+
+        It should_move_chapter = () => viewState.Chapters[0].ItemId.ShouldEqual(movedItemId);
+
+        It should_keep_amout_of_chapters_the_same = () => viewState.Chapters.Count.ShouldEqual(2);
+
+        private static string questionnaireId = "33333333333333333333333333333333";
+        private static string groupId = "22222222222222222222222222222222";
+
+        private static QuestionnaireInfoViewDenormalizer denormalizer;
+        private static QuestionnaireInfoView viewState;
+        private static string movedItemId;
+    }
+}
+
