fb1487a2e4826e74b5cd6eff986edccd537256cc
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2014-06-23 12:27:04 -0400

    HQ: close feeds in hibrid mode KP-3616
    Former-commit-id: c82ae0fcb20bebb3fc20a46528b15fe0c35375f4

---
+++ src/UI/Headquarters/WB.UI.Headquarters/API/Filters/HeadquarterFeatureOnlyFilter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+using WB.UI.Headquarters.Code;
+
+namespace WB.UI.Headquarters.API.Filters
+{
+    public class HeadquarterFeatureOnlyFilter : IActionFilter
+    {
+        public Task<HttpResponseMessage> ExecuteActionFilterAsync(HttpActionContext actionContext, CancellationToken cancellationToken,
+            Func<Task<HttpResponseMessage>> continuation)
+        {
+            if (!LegacyOptions.SupervisorFunctionsEnabled)
+                return continuation();
+
+            actionContext.Response = new HttpResponseMessage
+            {
+                StatusCode = HttpStatusCode.BadRequest,
+                RequestMessage = actionContext.ControllerContext.Request,
+                ReasonPhrase = "headquarter features are missing with current web site configuration",
+                Content = new StringContent("headquarter features are missing with current web site configuration")
+            };
+
+            return this.FromResult(actionContext.Response);
+
+        }
+
+        public bool AllowMultiple
+        {
+            get { return false; }
+        }
+
+        private Task<HttpResponseMessage> FromResult(HttpResponseMessage result)
+        {
+            var source = new TaskCompletionSource<HttpResponseMessage>();
+            source.SetResult(result);
+            return source.Task;
+        }
+    }
+}
