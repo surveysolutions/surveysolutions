b4aa6be5120fddbacac1accdefe0f1ba60924f0d
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2014-06-24 00:04:34 +0300

    Added more animations
    Former-commit-id: 1d5b8cc0dcc0c31ae234d6ab0511c64c692b7515

--- src/UI/Designer/WB.UI.Designer/UpdatedDesigner/app/directives/animations.js
+++ src/UI/Designer/WB.UI.Designer/UpdatedDesigner/app/directives/animations.js
@@ -1,17 +1,67 @@
 ﻿(function() {
-    angular.module('designerApp').animation('.slide', function() {
-        var NgHideClassName = 'ng-hide';
+    angular.module('designerApp')
+    .animation('.slide', function () {
+        var ngHideClassName = 'ng-hide';
         return {
             beforeAddClass: function(element, className, done) {
-                if (className === NgHideClassName) {
+                if (className === ngHideClassName) {
                     jQuery(element).slideUp(done);
                 }
             },
             removeClass: function(element, className, done) {
-                if (className === NgHideClassName) {
+                if (className === ngHideClassName) {
                     jQuery(element).hide().slideDown(done);
                 }
             }
-        }
+        };
+    })
+    .animation('.filter-animate', function () {
+        return {
+            enter: function(element, done) {
+                element.css('opacity', 0);
+                jQuery(element).animate({
+                    opacity: 1
+                }, done);
+
+                // optional onDone or onCancel callback
+                // function to handle any post-animation
+                // cleanup operations
+                return function(isCancelled) {
+                    if (isCancelled) {
+                        jQuery(element).stop();
+                    }
+                };
+            },
+            leave: function(element, done) {
+                element.css('opacity', 1);
+                jQuery(element).animate({
+                    opacity: 0
+                }, done);
+
+                // optional onDone or onCancel callback
+                // function to handle any post-animation
+                // cleanup operations
+                return function(isCancelled) {
+                    if (isCancelled) {
+                        jQuery(element).stop();
+                    }
+                };
+            },
+            move: function(element, done) {
+                element.css('opacity', 0);
+                jQuery(element).animate({
+                    opacity: 1
+                }, done);
+
+                // optional onDone or onCancel callback
+                // function to handle any post-animation
+                // cleanup operations
+                return function(isCancelled) {
+                    if (isCancelled) {
+                        jQuery(element).stop();
+                    }
+                };
+            }
+        };
     });
 })();
