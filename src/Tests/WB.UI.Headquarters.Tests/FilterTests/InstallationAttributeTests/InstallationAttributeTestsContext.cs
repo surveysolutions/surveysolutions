a1ebbb20756af8b960291ea86c78b0af85318c59
Roman Lytvyn <litvin.roma@gmail.com>
2014-06-26 19:16:31 +0300

    hq: added page for creating hq user. KP-3667
    Former-commit-id: c1b7e003f119d57f91cdee0e1033b1a06b3b4011

---
+++ src/Tests/WB.UI.Headquarters.Tests/FilterTests/InstallationAttributeTests/InstallationAttributeTestsContext.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Security.Principal;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Microsoft.Practices.ServiceLocation;
+using Moq;
+using WB.UI.Headquarters.Code;
+using WB.UI.Headquarters.Filters;
+
+namespace WB.UI.Headquarters.Tests.FilterTests.InstallationAttributeTests
+{
+    internal class InstallationAttributeTestsContext
+    {
+        protected static InstallationAttribute Create(IIdentityManager identityManager = null)
+        {
+            var serviceLocatorMock = new Mock<IServiceLocator> { DefaultValue = DefaultValue.Mock };
+            ServiceLocator.SetLocatorProvider(() => serviceLocatorMock.Object);
+
+            serviceLocatorMock.Setup(_ => _.GetInstance<IIdentityManager>())
+                .Returns(identityManager ?? Mock.Of<IIdentityManager>());
+
+            return new InstallationAttribute();
+        }
+
+        protected static ActionExecutingContext CreateFilterContext(bool isUserAuthenticated,
+            ControllerBase specifiedController = null)
+        {
+            var identityMock = new Mock<IIdentity>();
+            identityMock.Setup(_ => _.IsAuthenticated).Returns(isUserAuthenticated);
+
+            var principalMock = new Mock<IPrincipal>();
+            principalMock.Setup(_ => _.Identity).Returns(identityMock.Object);
+
+            var httpContext = new Mock<HttpContextBase>();
+            httpContext.Setup(_ => _.User).Returns(principalMock.Object);
+
+            var requestContext = new RequestContext(httpContext.Object, new RouteData());
+
+            var controllerContext = new ControllerContext(requestContext,
+                specifiedController ?? Mock.Of<ControllerBase>());
+
+            return new ActionExecutingContext(controllerContext, new Mock<ActionDescriptor>().Object,
+                new Dictionary<string, object>());
+        }
+    }
+}
---
+++ src/Tests/WB.UI.Headquarters.Tests/FilterTests/InstallationAttributeTests/when_action_executing_and_user_authenticated_and_install_controller.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+using Machine.Specifications;
+using Moq;
+using WB.UI.Headquarters.Code;
+using WB.UI.Headquarters.Controllers;
+using WB.UI.Headquarters.Filters;
+using It = Machine.Specifications.It;
+
+namespace WB.UI.Headquarters.Tests.FilterTests.InstallationAttributeTests
+{
+    internal class when_action_executing_and_user_authenticated_and_install_controller : InstallationAttributeTestsContext
+    {
+        Establish context = () =>
+        {
+            var identityManagerMock = new Mock<IIdentityManager>();
+            identityManagerMock.Setup(_ => _.GetUsersInRole(Moq.It.IsAny<string>())).Returns(new string[0]);
+
+            attribute = Create(identityManagerMock.Object);
+        };
+
+        Because of = () =>
+            exception =
+                Catch.Exception(
+                    () =>
+                        attribute.OnActionExecuting(CreateFilterContext(isUserAuthenticated: true,
+                            specifiedController: new InstallController(null, null, null))));
+
+        It should_exception_not_be_null = () =>
+            exception.ShouldNotBeNull();
+
+        It should_exception_be_type_of_HttpException = () =>
+            exception.ShouldBeOfExactType<HttpException>();
+
+        It should_exception_status_code_be_equal_to_404 = () =>
+            ((HttpException)exception).GetHttpCode().ShouldEqual(404);
+
+        private static InstallationAttribute attribute;
+        private static Exception exception;
+    }
+}
