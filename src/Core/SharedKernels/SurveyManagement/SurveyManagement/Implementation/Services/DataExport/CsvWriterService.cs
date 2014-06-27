5638106a60fd41253a6538b6bba50b7f001cf9e4
Anastasya Kholodova <nastya.kholodova@gmail.com>
2014-06-27 18:37:29 -0400

    Export: unit tests for CsvDataFileExportService KP-3669
    Former-commit-id: e069108ff59dd7471e1f4e36b4ca7b01073d25a4

---
+++ src/Core/SharedKernels/SurveyManagement/SurveyManagement/Implementation/Services/DataExport/CsvWriterService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper;
+using WB.Core.SharedKernels.SurveyManagement.Services;
+
+namespace WB.Core.SharedKernels.SurveyManagement.Implementation.Services.DataExport
+{
+    internal class CsvWriterService : ICsvWriterService
+    {
+        private readonly StreamWriter streamWriter;
+        private readonly CsvWriter csvWriter;
+        private readonly string delimiter = ",";
+
+        public CsvWriterService(Stream stream)
+        {
+            this.streamWriter = new StreamWriter(stream, Encoding.UTF8);
+            this.csvWriter = new CsvWriter(streamWriter);
+            this.csvWriter.Configuration.Delimiter = this.delimiter;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                streamWriter.Flush();
+                csvWriter.Dispose();
+                streamWriter.Dispose();
+            }
+        }
+        public void WriteField<T>(T cellValue)
+        {
+            csvWriter.WriteField(cellValue);
+        }
+
+        public void NextRecord()
+        {
+            csvWriter.NextRecord();
+        }
+    }
+}
---
+++ src/Tests/WB.Core.SharedKernels.SurveyManagement.Tests/ServiceTests/DataExport/CsvDataFileExportServiceTests/when_GetInterviewExportedDataFileName_called_for_level_name.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Machine.Specifications;
+using Moq;
+using WB.Core.SharedKernels.SurveyManagement.Implementation.Services.DataExport;
+using WB.Core.SharedKernels.SurveyManagement.Services;
+using It = Machine.Specifications.It;
+
+namespace WB.Core.SharedKernels.SurveyManagement.Tests.ServiceTests.DataExport.CsvDataFileExportServiceTests
+{
+    internal class when_GetInterviewExportedDataFileName_called_for_level_name : CsvDataFileExportServiceTestContext
+    {
+        Establish context = () =>
+        {
+            csvDataFileExportService = CreateCsvDataFileExportService();
+        };
+
+        Because of = () =>
+            result = csvDataFileExportService.GetInterviewExportedDataFileName(levelName);
+
+        It should_result_be_equal_to_level_name_csv = () =>
+            result.ShouldEqual(levelName+".csv");
+
+        private static CsvDataFileExportService csvDataFileExportService;
+        private static readonly string levelName = "level name";
+        private static string result;
+    }
+}
