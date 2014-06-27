06bba0948c79ed0c0a5222e072d5faf348b70d84
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2014-06-27 02:12:21 -0400

    Dynamic generation of assembly [KP-3699]
    --HG--
    branch : csharp-business-language
    
    
    Former-commit-id: 2ce92fcbe546647c4875451edf04199a5da6997c

---
+++ src/Core/Infrastructure/WB.Core.Infrastructure.Compilation/IDynamicCompiler.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Microsoft.CodeAnalysis.Emit;
+
+namespace WB.Core.Infrastructure.Compilation
+{
+    public interface IDynamicCompiler
+    {
+        EmitResult GenerateAssemblyAsString(Guid templateId, string classCode, out string generatedAssembly);
+    }
+}
---
+++ src/Tests/WB.Core.Infrastructure.Compilation.Tests/InterviewCompilerTests/when_loading_assembly_from_string.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Reflection;
+using Machine.Specifications;
+using Microsoft.CodeAnalysis.Emit;
+using WB.Core.Infrastructure.BaseStructures;
+
+namespace WB.Core.Infrastructure.Compilation.Tests.InterviewCompilerTests
+{
+    internal class when_loading_assembly_from_string
+    {
+
+        Establish context = () =>
+        {
+            compiler = new InterviewCompiler();
+            emitResult = compiler.GenerateAssemblyAsString(id, testClass, out resultAssembly);
+            if (emitResult.Success == true && !string.IsNullOrEmpty(resultAssembly))
+            {
+                var compiledAssembly = Assembly.Load(Convert.FromBase64String(resultAssembly));
+                Type calculator = compiledAssembly.GetType("InterviewEvaluator");
+                evaluator =  Activator.CreateInstance(calculator) as IInterviewEvaluator;
+            }
+        };
+
+        private Because of = () =>
+            evaluationResult = evaluator.Test();
+
+
+        private It should_result_succeded = () =>
+            evaluationResult.ShouldEqual(42);
+
+        private static IDynamicCompiler compiler;
+        private static Guid id = Guid.Parse("11111111111111111111111111111111");
+        private static string resultAssembly;
+        private static EmitResult emitResult;
+        private static IInterviewEvaluator evaluator;
+
+        private static int evaluationResult; 
+
+        public static string testClass = @"
+            public class InterviewEvaluator : IInterviewEvaluator
+            {
+                public static object Evaluate()
+                {
+                    return 2+2*2;
+                }
+
+                public int Test()
+                {
+                    return 40 + 2;
+                }
+ 
+            }";
+    }
+}
