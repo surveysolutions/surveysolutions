090d0dc4992485e57ba3d5079238a1cc69ef0a0b
Slava Scherbak <slava.scherbak@gmail.com>
2014-06-27 12:58:50 +0300

    Solution: optimized internal project references KP-3709
    Former-commit-id: 251c76977ea7b4b0274deab02fa8ca4bc10eb6b3

--- src/Core/SharedKernels/DataCollection/DataCollection/Implementation/Aggregates/Interview.cs
+++ src/Core/SharedKernels/DataCollection/DataCollection/Implementation/Aggregates/Interview.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
-using System.Text;
 using Main.Core.Entities.SubEntities;
 using Microsoft.Practices.ServiceLocation;
 using Ncqrs.Domain;
