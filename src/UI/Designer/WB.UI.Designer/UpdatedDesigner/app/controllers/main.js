15ac093dfb712dfb466cbb1529ad24d308a53033
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2014-07-03 17:31:59 +0300

    KP-3666: Splitting tree and chapters panel
    Former-commit-id: ab47f250e8c4dc16572b0c27ae2661df23dc4864

--- src/UI/Designer/WB.UI.Designer/UpdatedDesigner/app/controllers/main.js
+++ src/UI/Designer/WB.UI.Designer/UpdatedDesigner/app/controllers/main.js
@@ -6,43 +6,10 @@
             '$scope', '$stateParams', 'questionnaireService', 'commandService', 'verificationService', 'utilityService', 'hotkeys', '$state', '$modal', '$log',
             function ($scope, $stateParams, questionnaireService, commandService, verificationService, utilityService, hotkeys, $state, $modal, $log) {
                 var me = this;
-                hotkeys.add({
-                    combo: 'ctrl+f',
-                    description: 'Search for groups and questions in chapter',
-                    callback: function (event) {
-                        $scope.showSearch();
-                        event.preventDefault();
-                    }
-                });
-
-                hotkeys.add('down', 'Navigate to next sibling', function (event) {
-                    event.preventDefault();
-                    $scope.goToNextItem();
-                });
-
-                hotkeys.add('up', 'Navigate to previous sibling', function (event) {
-                    event.preventDefault();
-                    $scope.goToPrevItem();
-                });
-
-                hotkeys.add('left', 'Navigate to parent', function (event) {
-                    event.preventDefault();
-                    $scope.goToParent();
-                });
-
-                hotkeys.add('right', 'Navigate to child', function (event) {
-                    event.preventDefault();
-                    $scope.goToChild();
-                });
 
                 $scope.verificationStatus = {
                     errorsCount: null,
                     errors: []
-                };
-
-                var filtersBlockModes = {
-                    default: 'default',
-                    search: 'search'
                 };
 
                 $scope.verify = function () {
@@ -67,25 +34,6 @@
                             // ---
                         }
                     });
-                };
-
-                $scope.searchItem = function (item) {
-                    if (!$scope.search.searchText) return true;
-                    var variableMatches = item.variable && item.variable.toLowerCase().indexOf($scope.search.searchText.toLowerCase()) != -1;
-                    var titleMatches = item.title && item.title.toLowerCase().indexOf($scope.search.searchText.toLowerCase()) != -1;
-
-                    var found = variableMatches || titleMatches;
-
-                    if (!found) {
-                        angular.forEach(item.items, function (item) {
-                            var match = $scope.searchItem(item);
-                            if (match) {
-                                found = true;
-                            }
-                        });
-                    }
-
-                    return found;
                 };
 
                 $scope.navigateTo = function (itemId, chapterId) {
@@ -116,11 +64,7 @@
 
                 $scope.item = null;
 
-                $scope.search = { searchText: '' };
-
                 $scope.questionnaire = null;
-
-                $scope.filtersBoxMode = filtersBlockModes.default;
 
                 $scope.chaptersTree = {
                     dropped: function (event) {
@@ -144,56 +88,6 @@
                     }
                 };
 
-                $scope.groupsTree = {
-                    accept: function (sourceNodeScope, destNodesScope) {
-                        var message = _.isNull(destNodesScope.item) || $scope.isGroup(destNodesScope.item);
-                        return message;
-                    },
-                    beforeDrop: function (event) {
-                        me.draggedFrom = event.source.nodeScope.item.getParentItem();
-                    },
-                    dropped: function (event) {
-
-                        var movedItem = event.source.nodeScope.item;
-                        var destItem = event.dest.nodesScope.item;
-                        var destGroupId = destItem ? destItem.itemId : $scope.questionnaire.chapters[0].itemId;
-                        var putItem = function (item, parent, index) {
-                            var dropFrom = item.parent || $scope;
-
-                            dropFrom.items.splice(_.indexOf(dropFrom.items, item), 1);
-                            var itemsToAddTo = _.isNull(parent) ? $scope.items : parent.items;
-                            itemsToAddTo.splice(index, 0, item);
-
-                            connectTree();
-                        };
-
-                        if (event.dest.nodesScope !== event.source.nodesScope || event.dest.index !== event.source.index) {
-                            if ($scope.isQuestion(movedItem)) {
-                                questionnaireService.moveQuestion(movedItem.itemId, event.dest.index, destGroupId, $stateParams.questionnaireId)
-                                    .success(function (data) {
-                                        if (!data.IsSuccess) {
-                                            putItem(movedItem, me.draggedFrom, event.source.index);
-                                        }
-                                    })
-                                    .error(function () {
-                                        putItem(movedItem, me.draggedFrom, event.source.index);
-                                    });
-                            } else {
-                                questionnaireService.moveGroup(movedItem.itemId, event.dest.index, destGroupId, $stateParams.questionnaireId)
-                                    .success(function (data) {
-                                        if (!data.IsSuccess) {
-                                            putItem(movedItem, me.draggedFrom, event.source.index);
-
-                                        }
-                                    })
-                                    .error(function () {
-                                        putItem(movedItem, me.draggedFrom, event.source.index);
-                                    });
-                            }
-                        }
-                    }
-                };
-
                 $scope.currentChapter = null;
 
                 $scope.currentChapterId = null;
@@ -203,51 +97,6 @@
                     $scope.currentItemId = null;
                     $scope.activeRoster = null;
                     $scope.activeQuestion = null;
-                };
-
-                var upDownMove = function (updownStepValue) {
-                    if ($scope.items && $scope.currentItem) {
-                        var parent = $scope.currentItem.getParentItem();
-
-                        if (_.isNull(parent)) {
-                            var siblingIndex = _.indexOf($scope.items, $scope.currentItem) + updownStepValue;
-                            if (siblingIndex < $scope.items.length && siblingIndex >= 0) {
-                                $scope.nav($stateParams.questionnaireId, $scope.currentChapterId, $scope.items[siblingIndex]);
-                            }
-                            return;
-                        }
-
-                        var nextItemIndex = _.indexOf(parent.items, $scope.currentItem) + updownStepValue;
-
-                        if (nextItemIndex < $scope.items.length && nextItemIndex >= 0) {
-                            $scope.nav($stateParams.questionnaireId, $scope.currentChapterId, parent.items[nextItemIndex]);
-                        }
-                    }
-                };
-
-                $scope.goToNextItem = function () {
-                    upDownMove(1);
-                };
-
-                $scope.goToPrevItem = function () {
-                    upDownMove(-1);
-                };
-
-                $scope.goToParent = function () {
-                    if ($scope.items && $scope.currentItem) {
-                        var parent = $scope.currentItem.getParentItem();
-                        if (parent != null) {
-                            $scope.nav($stateParams.questionnaireId, $scope.currentChapterId, parent);
-                        }
-                    }
-                };
-
-                $scope.goToChild = function () {
-                    if ($scope.items && $scope.currentItem) {
-                        if (!_.isEmpty($scope.currentItem.items)) {
-                            $scope.nav($stateParams.questionnaireId, $scope.currentChapterId, $scope.currentItem.items[0]);
-                        }
-                    }
                 };
 
                 $scope.nav = function (questionnaireId, currentChapterId, item) {
@@ -269,10 +118,6 @@
                         $scope.activeChapter = item;
                         navigationService.openGroup(questionnaireId, currentChapterId, item.itemId);
                     }
-                };
-
-                $scope.setItem = function (item) {
-                    $scope.nav($stateParams.questionnaireId, $scope.currentChapterId, item);
                 };
 
                 $scope.isQuestion = function (item) {
@@ -355,16 +200,6 @@
 
                 $scope.toggle = function (scope) {
                     scope.toggle();
-                };
-
-                $scope.showSearch = function () {
-                    $scope.filtersBoxMode = filtersBlockModes.search;
-                    utilityService.focus('focusSearch');
-                };
-
-                $scope.hideSearch = function () {
-                    $scope.filtersBoxMode = filtersBlockModes.default;
-                    $scope.search.searchText = '';
                 };
 
                 questionnaireService.getQuestionnaireById($stateParams.questionnaireId)
--- src/UI/Designer/WB.UI.Designer/UpdatedDesigner/app/controllers/tree.js
+++ src/UI/Designer/WB.UI.Designer/UpdatedDesigner/app/controllers/tree.js
@@ -3,24 +3,187 @@
 angular.module('designerApp')
     .controller('TreeCtrl', [
         '$scope', '$stateParams', 'questionnaireId', 'questionnaireService', 'commandService', 'verificationService', 'utilityService', 'hotkeys', '$state', '$modal', '$log',
-        function($scope, $stateParams, questionnaireId, questionnaireService, commandService, verificationService, utilityService, hotkeys, $state, $modal, $log) {
+        function ($scope, $stateParams, questionnaireId, questionnaireService, commandService, verificationService, utilityService, hotkeys, $state, $modal, $log) {
             'use strict';
+            var me = this;
 
-              var connectTree = function () {
-                    var setParent = function (item, parent) {
-                        item.getParentItem = function () {
-                            return parent;
-                        };
-                        _.each(item.items, function (child) {
-                            setParent(child, item);
-                        });
+            var filtersBlockModes = {
+                default: 'default',
+                search: 'search'
+            };
+
+            $scope.search = { searchText: '' };
+            $scope.filtersBoxMode = filtersBlockModes.default;
+
+            hotkeys.add({
+                combo: 'ctrl+f',
+                description: 'Search for groups and questions in chapter',
+                callback: function (event) {
+                    $scope.showSearch();
+                    event.preventDefault();
+                }
+            });
+
+            hotkeys.add('down', 'Navigate to next sibling', function (event) {
+                event.preventDefault();
+                $scope.goToNextItem();
+            });
+
+            hotkeys.add('up', 'Navigate to previous sibling', function (event) {
+                event.preventDefault();
+                $scope.goToPrevItem();
+            });
+
+            hotkeys.add('left', 'Navigate to parent', function (event) {
+                event.preventDefault();
+                $scope.goToParent();
+            });
+
+            hotkeys.add('right', 'Navigate to child', function (event) {
+                event.preventDefault();
+                $scope.goToChild();
+            });
+
+            $scope.showSearch = function () {
+                $scope.filtersBoxMode = filtersBlockModes.search;
+                utilityService.focus('focusSearch');
+            };
+
+            $scope.hideSearch = function () {
+                $scope.filtersBoxMode = filtersBlockModes.default;
+                $scope.search.searchText = '';
+            };
+
+            $scope.searchItem = function (item) {
+                if (!$scope.search.searchText) return true;
+                var variableMatches = item.variable && item.variable.toLowerCase().indexOf($scope.search.searchText.toLowerCase()) != -1;
+                var titleMatches = item.title && item.title.toLowerCase().indexOf($scope.search.searchText.toLowerCase()) != -1;
+
+                var found = variableMatches || titleMatches;
+
+                if (!found) {
+                    angular.forEach(item.items, function (item) {
+                        var match = $scope.searchItem(item);
+                        if (match) {
+                            found = true;
+                        }
+                    });
+                }
+
+                return found;
+            };
+
+
+            var upDownMove = function (updownStepValue) {
+                if ($scope.items && $scope.currentItem) {
+                    var parent = $scope.currentItem.getParentItem();
+
+                    if (_.isNull(parent)) {
+                        var siblingIndex = _.indexOf($scope.items, $scope.currentItem) + updownStepValue;
+                        if (siblingIndex < $scope.items.length && siblingIndex >= 0) {
+                            $scope.nav($stateParams.questionnaireId, $scope.currentChapterId, $scope.items[siblingIndex]);
+                        }
+                        return;
+                    }
+
+                    var nextItemIndex = _.indexOf(parent.items, $scope.currentItem) + updownStepValue;
+
+                    if (nextItemIndex < $scope.items.length && nextItemIndex >= 0) {
+                        $scope.nav($stateParams.questionnaireId, $scope.currentChapterId, parent.items[nextItemIndex]);
+                    }
+                }
+            };
+
+            $scope.goToNextItem = function () {
+                upDownMove(1);
+            };
+
+            $scope.goToPrevItem = function () {
+                upDownMove(-1);
+            };
+
+            $scope.goToParent = function () {
+                if ($scope.items && $scope.currentItem) {
+                    var parent = $scope.currentItem.getParentItem();
+                    if (parent != null) {
+                        $scope.nav($stateParams.questionnaireId, $scope.currentChapterId, parent);
+                    }
+                }
+            };
+
+            $scope.goToChild = function () {
+                if ($scope.items && $scope.currentItem) {
+                    if (!_.isEmpty($scope.currentItem.items)) {
+                        $scope.nav($stateParams.questionnaireId, $scope.currentChapterId, $scope.currentItem.items[0]);
+                    }
+                }
+            };
+
+            var connectTree = function () {
+                var setParent = function (item, parent) {
+                    item.getParentItem = function () {
+                        return parent;
+                    };
+                    _.each(item.items, function (child) {
+                        setParent(child, item);
+                    });
+                };
+
+                _.each($scope.items, function (item) {
+                    setParent(item, null);
+                });
+            };
+
+            $scope.groupsTree = {
+                accept: function (sourceNodeScope, destNodesScope) {
+                    var message = _.isNull(destNodesScope.item) || $scope.isGroup(destNodesScope.item);
+                    return message;
+                },
+                beforeDrop: function (event) {
+                    me.draggedFrom = event.source.nodeScope.item.getParentItem();
+                },
+                dropped: function (event) {
+
+                    var movedItem = event.source.nodeScope.item;
+                    var destItem = event.dest.nodesScope.item;
+                    var destGroupId = destItem ? destItem.itemId : $scope.questionnaire.chapters[0].itemId;
+                    var putItem = function (item, parent, index) {
+                        var dropFrom = item.parent || $scope;
+
+                        dropFrom.items.splice(_.indexOf(dropFrom.items, item), 1);
+                        var itemsToAddTo = _.isNull(parent) ? $scope.items : parent.items;
+                        itemsToAddTo.splice(index, 0, item);
+
+                        connectTree();
                     };
 
-                    _.each($scope.items, function (item) {
-                        setParent(item, null);
-                    });
-                };  
-           
+                    if (event.dest.nodesScope !== event.source.nodesScope || event.dest.index !== event.source.index) {
+                        if ($scope.isQuestion(movedItem)) {
+                            questionnaireService.moveQuestion(movedItem.itemId, event.dest.index, destGroupId, $stateParams.questionnaireId)
+                                .success(function (data) {
+                                    if (!data.IsSuccess) {
+                                        putItem(movedItem, me.draggedFrom, event.source.index);
+                                    }
+                                })
+                                .error(function () {
+                                    putItem(movedItem, me.draggedFrom, event.source.index);
+                                });
+                        } else {
+                            questionnaireService.moveGroup(movedItem.itemId, event.dest.index, destGroupId, $stateParams.questionnaireId)
+                                .success(function (data) {
+                                    if (!data.IsSuccess) {
+                                        putItem(movedItem, me.draggedFrom, event.source.index);
+
+                                    }
+                                })
+                                .error(function () {
+                                    putItem(movedItem, me.draggedFrom, event.source.index);
+                                });
+                        }
+                    }
+                }
+            };
+
             questionnaireService.getChapterById(questionnaireId, $stateParams.chapterId)
                 .success(function (result) {
                     $scope.items = result.items;
