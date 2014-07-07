f5ae7e9cd8e58dcc221478fd107d9809c80a8197
Slava Scherbak <slava.scherbak@gmail.com>
2014-07-07 20:00:55 +0300

    StronglyTypedInterviewEvaluator: fixed bug with handling rosters
    --HG--
    branch : csharp-business-language
    
    
    Former-commit-id: fb631e4daf8f38a72fff1fe3f95ee1bd6582be7f

--- src/Core/Infrastructure/WB.Core.Infrastructure.BaseStructures/StronglyTypedInterviewEvaluator.cs
+++ src/Core/Infrastructure/WB.Core.Infrastructure.BaseStructures/StronglyTypedInterviewEvaluator.cs
@@ -15,6 +15,19 @@ namespace WB.Core.Infrastructure.BaseStructures
         void Validate(IEnumerable<IValidatable> rosters, List<Identity> questionsToBeValid, List<Identity> questionsToBeInvalid);
     }
 
+    public abstract class AbstractRosterLevel : IValidatableRoster
+    {
+        public readonly decimal[] rosterVector;
+
+        protected AbstractRosterLevel(decimal[] rosterVector)
+        {
+            this.rosterVector = rosterVector;
+        }
+
+        public abstract void Validate(IEnumerable<IValidatable> rosters, List<Identity> questionsToBeValid,
+            List<Identity> questionsToBeInvalid);
+    }
+
     public class QuestionnaireLevel : IValidatable
     {
         private readonly decimal[] rosterVector;
@@ -27,27 +40,10 @@ namespace WB.Core.Infrastructure.BaseStructures
         public string id;
         public int? persons_count;
 
-        //nested roster
-        public HhMember[] Members;
-
         public void Validate(List<Identity> questionsToBeValid, List<Identity> questionsToBeInvalid)
         {
             //Members.Count(m => m.age > 15);
         }
-
-    }
-
-    public abstract class AbstractRosterLevel : IValidatableRoster
-    {
-        public readonly decimal[] rosterVector;
-
-        protected AbstractRosterLevel(decimal[] rosterVector)
-        {
-            this.rosterVector = rosterVector;
-        }
-
-        public abstract void Validate(IEnumerable<IValidatable> rosters, List<Identity> questionsToBeValid,
-            List<Identity> questionsToBeInvalid);
     }
 
     public class HhMember : AbstractRosterLevel, IValidatable, IValidatableRoster
@@ -82,19 +78,19 @@ namespace WB.Core.Infrastructure.BaseStructures
         private bool food_IsValid()
         {
             // children should not dring alcohol
-            return food.Contains(38) && role == 3 && age < 21;
+            return food.Contains(38) && role == 3 && age >= 21;
         }
 
         private void Validate(IEnumerable<HhMember> roters, List<Identity> questionsToBeValid, List<Identity> questionsToBeInvalid)
         {
             //if (age_Validation().CanBeProcessed)
             {
-                var ageIdentity = new Identity(Id.ageId, rosterVector);
+                var ageIdentity = new Identity(IdOf.age, rosterVector);
                 (this.age_IsValid() ? questionsToBeValid : questionsToBeInvalid).Add(ageIdentity);
             }
             //if (food_IsValid().CanBeProcessed)
             {
-                var ageIdentity = new Identity(Id.ageId, rosterVector);
+                var ageIdentity = new Identity(IdOf.age, rosterVector);
                 (this.age_IsValid() ? questionsToBeValid : questionsToBeInvalid).Add(ageIdentity);
             }
         }
@@ -124,7 +120,7 @@ namespace WB.Core.Infrastructure.BaseStructures
         public string id { get { return parent.id; } }
         public int? persons_count { get { return parent.persons_count; } }
         public string name { get { return parent.name; } }
-        public int? age { get { return parent.age; } } 
+        public int? age { get { return parent.age; } }
         public DateTime? date { get { return parent.date; } }
         public decimal? sex { get { return parent.sex; } }
         public decimal? role { get { return parent.role; } }
@@ -153,64 +149,108 @@ namespace WB.Core.Infrastructure.BaseStructures
         }
     }
 
-    public static class Id
+    public static class IdOf
     {
-        public static readonly Guid idId = Guid.Parse("45ba5cc2-9bd7-4c76-8f13-94432405917a");
-        public static readonly Guid persons_countId = Guid.Parse("4139db8c-d3cc-49a7-a86f-0e1b5fd31edd");
-        public static readonly Guid nameId = Guid.Parse("03430fc9-3ab3-4e65-8a87-5af620b8e500");
-        public static readonly Guid ageId = Guid.Parse("552c411f-e0ed-4883-bccd-138e1893a64d");
-        public static readonly Guid dateId = Guid.Parse("6d7c3c4b-cbc5-4a13-a14f-7a882b54cf61");
-        public static readonly Guid sexId = Guid.Parse("51b9d76b-d9a5-426c-842e-33a930cf155e");
-        public static readonly Guid roleId = Guid.Parse("7e630572-6f23-428b-8d75-ceda05c1e969");
-        public static readonly Guid foodId = Guid.Parse("072be626-db13-4cd5-aeff-013e56e65932");
-        public static readonly Guid times_per_weekId = Guid.Parse("e0fe6bf7-ea6b-4897-b526-e0e96d7d0b0b");
-        public static readonly Guid price_for_foodId = Guid.Parse("571919ee-ff41-498d-9fed-c564b4ef80d6");
-        public static readonly Guid has_jobId = Guid.Parse("9aedd04e-b370-4281-b20c-aaa85988a4c9");
-        public static readonly Guid job_titleId = Guid.Parse("f93a4462-5b3d-4782-8e6a-af9af02db3a3");
-        public static readonly Guid best_job_ownerId = Guid.Parse("246ad6a8-cd73-4d0c-b5fc-1aa21c101ffe");
+        public static readonly Guid id = Guid.Parse("45ba5cc2-9bd7-4c76-8f13-94432405917a");
+        public static readonly Guid persons_count = Guid.Parse("4139db8c-d3cc-49a7-a86f-0e1b5fd31edd");
+        public static readonly Guid name = Guid.Parse("03430fc9-3ab3-4e65-8a87-5af620b8e500");
+        public static readonly Guid age = Guid.Parse("552c411f-e0ed-4883-bccd-138e1893a64d");
+        public static readonly Guid date = Guid.Parse("6d7c3c4b-cbc5-4a13-a14f-7a882b54cf61");
+        public static readonly Guid sex = Guid.Parse("51b9d76b-d9a5-426c-842e-33a930cf155e");
+        public static readonly Guid role = Guid.Parse("7e630572-6f23-428b-8d75-ceda05c1e969");
+        public static readonly Guid food = Guid.Parse("072be626-db13-4cd5-aeff-013e56e65932");
+        public static readonly Guid times_per_week = Guid.Parse("e0fe6bf7-ea6b-4897-b526-e0e96d7d0b0b");
+        public static readonly Guid price_for_food = Guid.Parse("571919ee-ff41-498d-9fed-c564b4ef80d6");
+        public static readonly Guid has_job = Guid.Parse("9aedd04e-b370-4281-b20c-aaa85988a4c9");
+        public static readonly Guid job_title = Guid.Parse("f93a4462-5b3d-4782-8e6a-af9af02db3a3");
+        public static readonly Guid best_job_owner = Guid.Parse("246ad6a8-cd73-4d0c-b5fc-1aa21c101ffe");
 
-        public static readonly Guid[] hhMemberScopeIds = new[] { persons_countId };
-        public static readonly Guid[] foodConsumptionIds = new[] { persons_countId, foodId };
+        public static readonly Guid[] hhMemberScopeIds = new[] { persons_count };
+        public static readonly Guid[] foodConsumptionIds = new[] { persons_count, food };
+
+        public static readonly Guid questionnaire = Guid.Parse("72897e3f-3dc8-4115-81e0-8a9c1cadec2d");
+        public static readonly Guid hhMember = Guid.Parse("69b02bcf-0ed1-4b5a-80bb-bd465ab096da");
+        public static readonly Guid foodConsumption = Guid.Parse("6df41d95-c785-4452-8303-bed3985d4c20");
+        public static readonly Guid jobActivity = Guid.Parse("7556ddf0-457a-4a9b-a021-45ac3bad05a8");
+
+        public static Dictionary<Guid, Guid[]> parentsMap = new Dictionary<Guid, Guid[]>
+        {
+            { id, new Guid[0] },
+            { persons_count, new Guid[0] },
+            { name, hhMemberScopeIds },
+            { age, hhMemberScopeIds },
+            { date, hhMemberScopeIds },
+            { sex, hhMemberScopeIds },
+            { role, hhMemberScopeIds },
+            { food, hhMemberScopeIds },
+            { times_per_week, foodConsumptionIds },
+            { price_for_food, foodConsumptionIds },
+            { has_job, hhMemberScopeIds },
+            { job_title, hhMemberScopeIds },
+            { best_job_owner, hhMemberScopeIds },
+        };
+
+        public static Dictionary<Guid, Guid[]> rostersIdToScopeMap = new Dictionary<Guid, Guid[]>
+        {
+            {hhMember, hhMemberScopeIds},
+            {foodConsumption, foodConsumptionIds},
+            {jobActivity, hhMemberScopeIds}
+        };
     }
 
     public class StronglyTypedInterviewEvaluator : IInterviewEvaluator
     {
-        private readonly Dictionary<decimal[], IValidatable> interviewScopes = new Dictionary<decimal[], IValidatable>();
-
-        private static decimal[] GetParentVector(decimal[] rosterVector)
-        {
-            return rosterVector.Take(rosterVector.Length - 1).ToArray();
-        }
-
+        private readonly Dictionary<Identity[], IValidatable> interviewScopes = new Dictionary<Identity[], IValidatable>();
 
         public StronglyTypedInterviewEvaluator()
         {
             var emptyRosterVector = new decimal[0];
             var questionnaireLevel = new QuestionnaireLevel(emptyRosterVector);
-            interviewScopes.Add(emptyRosterVector, questionnaireLevel);
+            var questionnaireIdentityKey = new[]
+            {
+                new Identity(IdOf.questionnaire, emptyRosterVector)
+            };
+            interviewScopes.Add(questionnaireIdentityKey, questionnaireLevel);
         }
 
-        public void AddRoster(Guid[] rosterScopeIds, decimal[] rosterVector)
+        private Identity[] GetRosterKey(Guid[] rosterScopeIds, decimal[] rosterVector)
         {
-            decimal[] parentRosterVector = GetParentVector(rosterVector);
-            var parent = interviewScopes[parentRosterVector];
+            return rosterScopeIds.Select(x => new Identity(x, rosterVector)).ToArray();
+        }
 
-            if (rosterScopeIds.SequenceEqual(Id.hhMemberScopeIds))
+        public void AddRoster(Guid rosterId, decimal[] rosterVector)
+        {
+            if (!IdOf.rostersIdToScopeMap.ContainsKey(rosterId))
+            {
+                return;
+            }
+
+            decimal[] parentRosterVector = rosterVector.Shrink();
+
+            var rosterParentIdentityKey = parentRosterVector.Length == 0 
+                ? this.GetRosterKey(new[] { IdOf.questionnaire }, new decimal[0]) 
+                : this.GetRosterKey(IdOf.rostersIdToScopeMap[rosterId].Shrink(), parentRosterVector);
+
+            var parent = interviewScopes[rosterParentIdentityKey];
+            var rosterIdentityKey = this.GetRosterKey(IdOf.rostersIdToScopeMap[rosterId], rosterVector);
+
+            if (rosterId == IdOf.hhMember || rosterId == IdOf.jobActivity)
             {
                 var rosterLevel = new HhMember(rosterVector, parent as QuestionnaireLevel);
-                interviewScopes.Add(rosterVector, rosterLevel);
+                interviewScopes.Add(rosterIdentityKey, rosterLevel);
             }
 
-            if (rosterScopeIds.SequenceEqual(Id.foodConsumptionIds))
+            if (rosterId == IdOf.foodConsumption)
             {
                 var rosterLevel = new FoodConsumption(rosterVector, parent as HhMember);
-                interviewScopes.Add(rosterVector, rosterLevel);
+                interviewScopes.Add(rosterIdentityKey, rosterLevel);
             }
         }
 
-        public void RemoveRoster(decimal[] rosterVector)
+        public void RemoveRoster(Guid rosterId, decimal[] rosterVector)
         {
-            var dependentRosters = interviewScopes.Keys.Where(x => x.Take(rosterVector.Length).SequenceEqual(rosterVector));
+            var rosterIdentityKey = this.GetRosterKey(IdOf.rostersIdToScopeMap[rosterId], rosterVector);
+            var dependentRosters = interviewScopes.Keys.Where(x => x.Take(rosterVector.Length).SequenceEqual(rosterIdentityKey));
             foreach (var rosterVectorKey in dependentRosters)
             {
                 interviewScopes.Remove(rosterVectorKey);
@@ -218,95 +258,101 @@ namespace WB.Core.Infrastructure.BaseStructures
         }
         public void UpdateIntAnswer(Guid questionId, decimal[] rosterVector, int answer)
         {
-            var targetLevel = interviewScopes.ContainsKey(rosterVector) ? interviewScopes[rosterVector] : null;
+            var rosterKey = this.GetRosterKey(IdOf.parentsMap[questionId], rosterVector);
+            var targetLevel = interviewScopes.ContainsKey(rosterKey) ? interviewScopes[rosterKey] : null;
             if (targetLevel == null) return;
 
-            if (questionId == Id.persons_countId)
+            if (questionId == IdOf.persons_count)
             {
                 (targetLevel as QuestionnaireLevel).persons_count = answer;
             }
 
-            if (questionId == Id.ageId)
+            if (questionId == IdOf.age)
             {
                 (targetLevel as HhMember).age = answer;
             }
 
-            if (questionId == Id.times_per_weekId)
+            if (questionId == IdOf.times_per_week)
             {
                 (targetLevel as FoodConsumption).times_per_week = answer;
             }
         }
         public void UpdateDecimalAnswer(Guid questionId, decimal[] rosterVector, decimal answer)
         {
-            var targetLevel = interviewScopes.ContainsKey(rosterVector) ? interviewScopes[rosterVector] : null;
+            var rosterKey = this.GetRosterKey(IdOf.parentsMap[questionId], rosterVector);
+            var targetLevel = interviewScopes.ContainsKey(rosterKey) ? interviewScopes[rosterKey] : null;
             if (targetLevel == null) return;
 
-            if (questionId == Id.price_for_foodId)
+            if (questionId == IdOf.price_for_food)
             {
                 (targetLevel as FoodConsumption).price_for_food = answer;
             }
         }
         public void UpdateDateAnswer(Guid questionId, decimal[] rosterVector, DateTime answer)
         {
-            var targetLevel = interviewScopes.ContainsKey(rosterVector) ? interviewScopes[rosterVector] : null;
+            var rosterKey = this.GetRosterKey(IdOf.parentsMap[questionId], rosterVector);
+            var targetLevel = interviewScopes.ContainsKey(rosterKey) ? interviewScopes[rosterKey] : null;
             if (targetLevel == null) return;
 
-            if (questionId == Id.dateId)
+            if (questionId == IdOf.date)
             {
                 (targetLevel as HhMember).date = answer;
             }
         }
         public void UpdateTextAnswer(Guid questionId, decimal[] rosterVector, string answer)
         {
-            var targetLevel = interviewScopes.ContainsKey(rosterVector) ? interviewScopes[rosterVector] : null;
+            var rosterKey = this.GetRosterKey(IdOf.parentsMap[questionId], rosterVector);
+            var targetLevel = interviewScopes.ContainsKey(rosterKey) ? interviewScopes[rosterKey] : null;
             if (targetLevel == null) return;
 
-            if (questionId == Id.idId)
+            if (questionId == IdOf.id)
             {
                 (targetLevel as QuestionnaireLevel).id = answer;
             }
 
-            if (questionId == Id.nameId)
+            if (questionId == IdOf.name)
             {
                 (targetLevel as HhMember).name = answer;
             }
 
-            if (questionId == Id.job_titleId)
+            if (questionId == IdOf.job_title)
             {
                 (targetLevel as HhMember).job_title = answer;
             }
         }
         public void UpdateSingleOptionAnswer(Guid questionId, decimal[] rosterVector, decimal answer)
         {
-            var targetLevel = interviewScopes.ContainsKey(rosterVector) ? interviewScopes[rosterVector] : null;
+            var rosterKey = this.GetRosterKey(IdOf.parentsMap[questionId], rosterVector);
+            var targetLevel = interviewScopes.ContainsKey(rosterKey) ? interviewScopes[rosterKey] : null;
             if (targetLevel == null) return;
 
-            if (questionId == Id.sexId)
+            if (questionId == IdOf.sex)
             {
                 (targetLevel as HhMember).sex = answer;
             }
 
-            if (questionId == Id.roleId)
+            if (questionId == IdOf.role)
             {
                 (targetLevel as HhMember).role = answer;
             }
 
-            if (questionId == Id.has_jobId)
+            if (questionId == IdOf.has_job)
             {
                 (targetLevel as HhMember).has_job = answer;
             }
 
-            if (questionId == Id.best_job_ownerId)
+            if (questionId == IdOf.best_job_owner)
             {
                 (targetLevel as HhMember).best_job_owner = answer;
             }
         }
         public void UpdateMultiOptionAnswer(Guid questionId, decimal[] rosterVector, decimal[] answer)
         {
-            var targetLevel = interviewScopes.ContainsKey(rosterVector) ? interviewScopes[rosterVector] : null;
+            var rosterKey = this.GetRosterKey(IdOf.parentsMap[questionId], rosterVector);
+            var targetLevel = interviewScopes.ContainsKey(rosterKey) ? interviewScopes[rosterKey] : null;
             if (targetLevel == null) return;
 
-            if (questionId == Id.foodId)
+            if (questionId == IdOf.food)
             {
                 (targetLevel as HhMember).food = answer;
             }
@@ -323,7 +369,7 @@ namespace WB.Core.Infrastructure.BaseStructures
                 {
                     var interviewScope = interviewScopeKvp.Value as AbstractRosterLevel;
 
-                    var parentRosterVector = GetParentVector(interviewScopeKvp.Key);
+                    var parentRosterVector = interviewScopeKvp.Key.Shrink();
 
                     var parentRosterVectorLength = parentRosterVector.Length;
                     var rosters = interviewScopes.Where(x
@@ -350,7 +396,15 @@ namespace WB.Core.Infrastructure.BaseStructures
         {
             throw new NotImplementedException();
         }
-        
+    }
+
+    public static class Helpers
+    {
+        public static T[] Shrink<T>(this IEnumerable<T> vector)
+        {
+            var enumerable = vector as T[] ?? vector.ToArray();
+            return enumerable.Take(enumerable.Count() - 1).ToArray();
+        }
     }
 }
 // ReSharper restore InconsistentNaming
