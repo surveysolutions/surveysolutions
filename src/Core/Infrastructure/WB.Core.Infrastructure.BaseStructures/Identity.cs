c26c401b0fabf162f0aa8581adb45f9b3bae9f13
Slava Scherbak <slava.scherbak@gmail.com>
2014-07-07 20:00:55 +0300

    StronglyTypedInterviewEvaluator: fixed bug with handling rosters
    --HG--
    branch : csharp-business-language
    
    
    Former-commit-id: fb631e4daf8f38a72fff1fe3f95ee1bd6582be7f

--- src/Core/Infrastructure/WB.Core.Infrastructure.BaseStructures/Identity.cs
+++ src/Core/Infrastructure/WB.Core.Infrastructure.BaseStructures/Identity.cs
@@ -4,6 +4,19 @@ namespace WB.Core.Infrastructure.BaseStructures
 {
     public class Identity
     {
+        protected bool Equals(Identity other)
+        {
+            return this.Id.Equals(other.Id) && Equals(this.RosterVector, other.RosterVector);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (this.Id.GetHashCode()*397) ^ (this.RosterVector != null ? this.RosterVector.GetHashCode() : 0);
+            }
+        }
+
         // should be shared
         public Guid Id { get; private set; }
         public decimal[] RosterVector { get; private set; }
@@ -11,6 +24,24 @@ namespace WB.Core.Infrastructure.BaseStructures
         {
             this.Id = id;
             this.RosterVector = rosterVector;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Identity) obj);
+        }
+
+        public static bool operator ==(Identity x, Identity y)
+        {
+            return x.Equals(y);
+        }
+
+        public static bool operator !=(Identity x, Identity y)
+        {
+            return !(x == y);
         }
     }
 }
