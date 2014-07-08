bb231cee3a54876c45c04f59560ee33e7441482f
Roman Lytvyn <litvin.roma@gmail.com>
2014-07-09 02:11:44 +0300

    designer: added tests for update/clone static text in questionnaire ar. added validations for parent of entity. removed unusable validation. KP-3692
    Former-commit-id: 158cdb15031b8ff560d47bc4937daa29378a3e01

---
+++ src/Tests/WB.Core.BoundedContexts.Designer.Tests/CloneStaticTextHandlerTests/when_cloning_static_text_and_parent_does_not_exists.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Machine.Specifications;
+using Main.Core.Events.Questionnaire;
+using WB.Core.BoundedContexts.Designer.Aggregates;
+using WB.Core.BoundedContexts.Designer.Events.Questionnaire;
+using WB.Core.BoundedContexts.Designer.Exceptions;
+using WB.Core.BoundedContexts.Designer.Tests.QuestionnaireTests;
+
+namespace WB.Core.BoundedContexts.Designer.Tests.CloneStaticTextHandlerTests
+{
+    internal class when_cloning_static_text_and_parent_does_not_exists : QuestionnaireTestsContext
+    {
+        Establish context = () =>
+        {
+            questionnaire = CreateQuestionnaire(responsibleId: responsibleId);
+            questionnaire.Apply(new NewGroupAdded { PublicKey = chapterId });
+            questionnaire.Apply(new StaticTextAdded() {EntityId = sourceEntityId, ParentId = chapterId});
+        };
+
+        Because of = () =>
+            exception = Catch.Exception(() =>
+                questionnaire.CloneStaticText(entityId: targetEntityId, parentId: notExistingParentId, text: "text",
+                    responsibleId: responsibleId, sourceEntityId: sourceEntityId, targetIndex: 0));
+
+        It should_throw_QuestionnaireException = () =>
+            exception.ShouldBeOfExactType<QuestionnaireException>();
+
+        It should_throw_exception_with_message_containting__group_cant_found__ = () =>
+             new[] { "group", "can't", "found" }.ShouldEachConformTo(
+                    keyword => exception.Message.ToLower().Contains(keyword));
+
+        
+        private static Questionnaire questionnaire;
+        private static Exception exception;
+        private static Guid sourceEntityId = Guid.Parse("11111111111111111111111111111111");
+        private static Guid targetEntityId = Guid.Parse("33333333333333333333333333333333");
+        private static Guid notExistingParentId = Guid.Parse("22222222222222222222222222222222");
+        private static Guid chapterId = Guid.Parse("CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC");
+        private static Guid responsibleId = Guid.Parse("DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD");
+    }
+}
