b9217493466dd780174eb7c2cb57171cf3ec7941
Dima Pasko <dimapasko@gmail.com>
2014-07-09 13:29:35 +0300

    Created confirm window infrastructure KP-3730 Create stubs for Bootstrap alerts
    Former-commit-id: 9b24475578f3b62266e3e732a314e2bc01db8f61

---
+++ src/UI/Designer/WB.UI.Designer/UpdatedDesigner/app/controllers/confirm.js
@@ -0,0 +1,16 @@
+﻿(function() {
+    'use strict';
+    angular.module('designerApp').controller('confirmCtrl',
+    [
+        '$scope', '$log', '$modalInstance',
+        function($scope, $log, $modalInstance) {
+            $scope.ok = function() {
+                $modalInstance.close('ok');
+            };
+
+            $scope.cancel = function() {
+                $modalInstance.dismiss('cancel');
+            };
+        }
+    ]);
+})();
