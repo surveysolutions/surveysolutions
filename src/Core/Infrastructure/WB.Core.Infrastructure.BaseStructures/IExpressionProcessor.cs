79b7f1c8fe7807e638dd709ed034642f5ad96bb0
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2014-07-09 00:04:57 -0400

    Creating copy of Evaluator [KP-3736]
    --HG--
    branch : csharp-business-language
    
    
    Former-commit-id: 89558a2be1cab40df091e0fe05ab7c491ec10187

--- src/Core/Infrastructure/WB.Core.Infrastructure.BaseStructures/IExpressionProcessor.cs
+++ src/Core/Infrastructure/WB.Core.Infrastructure.BaseStructures/IExpressionProcessor.cs
@@ -12,16 +12,21 @@ namespace WB.Core.Infrastructure.BaseStructures
         void UpdateQrBarcodeAnswer(Guid questionId, decimal[] rosterVector, string answer);
         void UpdateSingleOptionAnswer(Guid questionId, decimal[] rosterVector, decimal answer);
         void UpdateMultiOptionAnswer(Guid questionId, decimal[] rosterVector, decimal[] answer);
-        void ProcessExpressions(List<Identity> questionsToBeValid, List<Identity> questionsToBeInvalid);
         void UpdateGeoLocationAnswer(Guid questionId, decimal[] propagationVector, double latitude, double longitude);
         void UpdateTextListAnswer(Guid questionId, decimal[] propagationVector, Tuple<decimal, string>[] answers);
         void UpdateLinkedSingleOptionAnswer(Guid questionId, decimal[] propagationVector, decimal[] selectedPropagationVector);
         void UpdateLinkedMultiOptionAnswer(Guid questionId, decimal[] propagationVector, decimal[][] selectedPropagationVectors);
+        
         void DisableGroups(IEnumerable<Identity> groupsToDisable);
         void EnableGroups(IEnumerable<Identity> groupsToEnable);
         void DisableQuestions(IEnumerable<Identity> questionsToDisable);
         void EnableQuestions(IEnumerable<Identity> questionsToEnable);
         void AddRoster(Guid rosterId, decimal[] outerRosterVector, decimal rosterInstanceId, int? sortIndex);
         void RemoveRoster(Guid rosterId, decimal[] rosterVector, decimal rosterInstanceId);
+
+        void ProcessValidationExpressions(List<Identity> questionsToBeValid, List<Identity> questionsToBeInvalid);
+
+
+        IExpressionProcessor Copy();
     }
 }
