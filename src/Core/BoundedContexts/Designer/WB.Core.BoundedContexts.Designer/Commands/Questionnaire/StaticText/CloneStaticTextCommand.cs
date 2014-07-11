3c7a9cc2f7d3d203e5d7c5e4a9c7c3c6b484b61b
Roman Lytvyn <litvin.roma@gmail.com>
2014-07-11 05:07:11 +0300

    designer: added questionnaire commands for static text. KP-3692
    Former-commit-id: 874e72be33bf6ea1b9d36ff0d2aa94f25b7ab90f

---
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Commands/Questionnaire/StaticText/CloneStaticTextCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Ncqrs.Commanding.CommandExecution.Mapping.Attributes;
+
+namespace WB.Core.BoundedContexts.Designer.Commands.Questionnaire.StaticText
+{
+    [Serializable]
+    [MapsToAggregateRootMethod(typeof(Aggregates.Questionnaire), "CloneStaticText")]
+    public class CloneStaticTextCommand : AddStaticTextCommand
+    {
+        public CloneStaticTextCommand(Guid questionnaireId, Guid entityId, string text, Guid responsibleId,
+            Guid parentId, Guid sourceEntityId, int targetIndex)
+            : base(
+                responsibleId: responsibleId, questionnaireId: questionnaireId, entityId: entityId, text: text,
+                parentId: parentId)
+        {
+            this.SourceEntityId = sourceEntityId;
+            this.TargetIndex = targetIndex;
+        }
+
+        public int TargetIndex { get; set; }
+        public Guid SourceEntityId { get; set; }
+    }
+}
---
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Commands/Questionnaire/StaticText/UpdateStaticTextCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Ncqrs.Commanding.CommandExecution.Mapping.Attributes;
+using WB.Core.BoundedContexts.Designer.Commands.Questionnaire.Base;
+
+namespace WB.Core.BoundedContexts.Designer.Commands.Questionnaire.StaticText
+{
+    [Serializable]
+    [MapsToAggregateRootMethod(typeof(Aggregates.Questionnaire), "UpdateStaticText")]
+    public class UpdateStaticTextCommand : QuestionnaireCommand
+    {
+        public UpdateStaticTextCommand(Guid questionnaireId, Guid entityId, string text, Guid responsibleId)
+            : base(responsibleId: responsibleId, questionnaireId: questionnaireId)
+        {
+            this.EntityId = entityId;
+            this.Text = text;
+        }
+
+        public Guid EntityId { get; set; }
+        public string Text { get; set; }
+    }
+}
