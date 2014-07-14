55707b832b09179a80b9f6c58ddf4cce1bb2919b
Roman Lytvyn <litvin.roma@gmail.com>
2014-07-15 00:02:39 +0300

    designer: added tests for delete/move static text command handlers in questionnaire ar. KP-3692
    Former-commit-id: 44d1287e7fa19cf23bd35d995bddc59a7a5ed466

---
+++ src/Tests/WB.Core.BoundedContexts.Designer.Tests/MoveStaticTextHandlerTests/when_moving_static_text_and_entity_does_not_exists.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Machine.Specifications;
+using Main.Core.Events.Questionnaire;
+using WB.Core.BoundedContexts.Designer.Aggregates;
+using WB.Core.BoundedContexts.Designer.Events.Questionnaire;
+using WB.Core.BoundedContexts.Designer.Exceptions;
+using WB.Core.BoundedContexts.Designer.Tests.QuestionnaireTests;
+
+namespace WB.Core.BoundedContexts.Designer.Tests.MoveStaticTextHandlerTests
+{
+    internal class when_moving_static_text_and_entity_does_not_exists : QuestionnaireTestsContext
+    {
+        Establish context = () =>
+        {
+            questionnaire = CreateQuestionnaire(responsibleId: responsibleId);
+            questionnaire.Apply(new NewGroupAdded { PublicKey = chapterId });
+            questionnaire.Apply(new StaticTextAdded() {EntityId = entityId, ParentId = chapterId});
+        };
+
+        Because of = () =>
+            exception = Catch.Exception(() =>
+                questionnaire.MoveStaticText(entityId: notExistingEntityId, responsibleId: responsibleId, targetEntityId: chapterId, targetIndex: 0));
+
+        It should_throw_QuestionnaireException = () =>
+            exception.ShouldBeOfExactType<QuestionnaireException>();
+
+        It should_throw_exception_with_message_containting__item_cant_found__ = () =>
+             new[] { "item", "can't", "found" }.ShouldEachConformTo(
+                    keyword => exception.Message.ToLower().Contains(keyword));
+
+        
+        private static Questionnaire questionnaire;
+        private static Exception exception;
+        private static Guid entityId = Guid.Parse("11111111111111111111111111111111");
+        private static Guid notExistingEntityId = Guid.Parse("22222222222222222222222222222222");
+        private static Guid chapterId = Guid.Parse("CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC");
+        private static Guid responsibleId = Guid.Parse("DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD");
+    }
+}
