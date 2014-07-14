ed2f885efb1cc90fad92511596b0413a05ac548e
Roman Lytvyn <none@none>
2014-07-15 01:13:52 +0300

    questionnaire verifier: added validation that static text should not be empty. KP-3692
    Former-commit-id: b86acacb3fdcecee4a82e96b282a0f92de3a8e88

---
+++ src/Tests/WB.Core.SharedKernels.QuestionnaireVerification.Tests/QuestionnaireVerifierTests/when_verifying_questionnaire_with_empty_static_text.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Machine.Specifications;
+using Main.Core.Documents;
+using Main.Core.Entities.Composite;
+using Main.Core.Entities.SubEntities;
+using Main.Core.Entities.SubEntities.Question;
+using WB.Core.SharedKernels.QuestionnaireVerification.Implementation.Services;
+using WB.Core.SharedKernels.QuestionnaireVerification.ValueObjects;
+
+namespace WB.Core.SharedKernels.QuestionnaireVerification.Tests.QuestionnaireVerifierTests
+{
+    class when_verifying_questionnaire_with_empty_static_text : QuestionnaireVerifierTestsContext
+    {
+
+        private Establish context = () =>
+        {
+            questionnaire = CreateQuestionnaireDocumentWithOneChapter(new Group("Group")
+            {
+                Children = new List<IComposite>()
+                {
+                    new StaticText(entityId: staticTextId, text: string.Empty),
+                    new TextQuestion() {StataExportCaption = "var"}
+                }
+            });
+
+            verifier = CreateQuestionnaireVerifier();
+        };
+
+        Because of = () => 
+            resultErrors = verifier.Verify(questionnaire);
+
+        It should_return_1_error = () =>
+            resultErrors.Count().ShouldEqual(1);
+
+        It should_return_error_with_code__WB0067 = () =>
+            resultErrors.Single().Code.ShouldEqual("WB0067");
+
+        It should_return_error_with_1_references = () =>
+            resultErrors.Single().References.Count().ShouldEqual(1);
+
+        It should_return_error_reference_with_type_Question = () =>
+            resultErrors.Single().References.First().Type.ShouldEqual(QuestionnaireVerificationReferenceType.StaticText);
+
+        It should_return_error_reference_with_id_of_questionId = () =>
+            resultErrors.Single().References.First().Id.ShouldEqual(staticTextId);
+
+        private static QuestionnaireVerifier verifier;
+        private static QuestionnaireDocument questionnaire;
+
+        private static IEnumerable<QuestionnaireVerificationError> resultErrors;
+
+        private static Guid staticTextId = Guid.Parse("10000000000000000000000000000000");
+    }
+}
