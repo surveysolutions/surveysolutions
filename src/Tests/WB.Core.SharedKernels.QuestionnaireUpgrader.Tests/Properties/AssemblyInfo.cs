02e2fc94563f636122dbcd5a00348590e566c9d1
Anastasya Kholodova <nastya.kholodova@gmail.com>
2014-07-14 12:40:45 -0400

    Tools\RosterVariableNames: unit tests for roster variable names generator KP-3690
    Former-commit-id: 1737aab44b323f554c766e92d1c07dfd853ef854

--- src/Tests/WB.Core.SharedKernels.QuestionnaireUpgrader.Tests/Properties/AssemblyInfo.cs
+++ src/Tests/WB.Core.SharedKernels.QuestionnaireUpgrader.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WB.Core.SharedKernels.QuestionnaireUpgrader")]
+[assembly: AssemblyTitle("WB.Core.SharedKernels.QuestionnaireUpgrader.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WB.Core.SharedKernels.QuestionnaireUpgrader")]
+[assembly: AssemblyProduct("WB.Core.SharedKernels.QuestionnaireUpgrader.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ea2f279a-3974-4a3c-bcfe-25f406e69a5e")]
+[assembly: Guid("3c61d98c-7e9d-4e61-97cd-a573d17f4257")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -34,5 +34,3 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: InternalsVisibleTo("WB.Tools.QuestionnaireDocumentsConverter")]
-[assembly: InternalsVisibleTo("WB.Core.SharedKernels.QuestionnaireUpgrader.Tests")]
