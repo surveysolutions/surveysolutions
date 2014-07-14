f1be03d46fb019b3ea902fac2636971f47a033e8
Roman Lytvyn <litvin.roma@gmail.com>
2014-07-15 00:02:39 +0300

    designer: added tests for delete/move static text command handlers in questionnaire ar. KP-3692
    Former-commit-id: 44d1287e7fa19cf23bd35d995bddc59a7a5ed466

---
+++ src/Tests/WB.Core.BoundedContexts.Designer.Tests/DeleteStaticTextHandlerTests/when_deleting_static_text_and_all_parameters_specified.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Machine.Specifications;
+using Main.Core.Events.Questionnaire;
+using Ncqrs.Spec;
+using WB.Core.BoundedContexts.Designer.Aggregates;
+using WB.Core.BoundedContexts.Designer.Events.Questionnaire;
+using WB.Core.BoundedContexts.Designer.Tests.QuestionnaireTests;
+
+namespace WB.Core.BoundedContexts.Designer.Tests.DeleteStaticTextHandlerTests
+{
+    internal class when_deleting_static_text_and_all_parameters_specified : QuestionnaireTestsContext
+    {
+        Establish context = () =>
+        {
+            questionnaire = CreateQuestionnaire(responsibleId: responsibleId);
+            questionnaire.Apply(new NewGroupAdded { PublicKey = chapterId });
+            questionnaire.Apply(new StaticTextAdded() { EntityId = entityId, ParentId = chapterId });
+
+            eventContext = new EventContext();
+        };
+
+        Because of = () =>            
+                questionnaire.DeleteStaticText(entityId: entityId, responsibleId: responsibleId);
+
+        Cleanup stuff = () =>
+        {
+            eventContext.Dispose();
+            eventContext = null;
+        };
+
+        It should_raise_StaticTextDeleted_event = () =>
+            eventContext.ShouldContainEvent<StaticTextDeleted>();
+
+        It should_raise_StaticTextDeleted_event_with_EntityId_specified = () =>
+            eventContext.GetSingleEvent<StaticTextDeleted>().EntityId.ShouldEqual(entityId);
+        
+        private static EventContext eventContext;
+        private static Questionnaire questionnaire;
+        private static Guid entityId = Guid.Parse("11111111111111111111111111111111");
+        private static Guid chapterId = Guid.Parse("CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC");
+        private static Guid responsibleId = Guid.Parse("DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD");
+    }
+}
