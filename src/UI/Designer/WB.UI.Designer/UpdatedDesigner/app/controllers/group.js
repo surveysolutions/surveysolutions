7575c6b659f609e4928ff2cba150d3afddb516ff
Dima Pasko <dimapasko@gmail.com>
2014-07-15 18:18:27 +0300

    Designer: fix Cancel feature on the group page
    Former-commit-id: a5f3dc9192f71c0b24c2d54a2a61627d2760727b

--- src/UI/Designer/WB.UI.Designer/UpdatedDesigner/app/controllers/group.js
+++ src/UI/Designer/WB.UI.Designer/UpdatedDesigner/app/controllers/group.js
@@ -6,17 +6,29 @@
             '$scope', '$stateParams', 'questionnaireService', 'commandService', '$log',
             function($scope, $stateParams, questionnaireService, commandService, $log) {
 
+                var dataBind = function(result) {
+                    $scope.activeChapter = result;
+                    $scope.activeChapter.group.itemId = $stateParams.itemId;
+                    $scope.activeChapter.group.variableName = $stateParams.variableName;
+                }
+
                 $scope.loadGroup = function() {
                     questionnaireService.getGroupDetailsById($stateParams.questionnaireId, $stateParams.itemId).success(function(result) {
-                            $scope.activeChapter = result;
-                            $scope.activeChapter.group.itemId = $stateParams.itemId;
-                            $scope.activeChapter.group.variableName = $stateParams.variableName;
+                            dataBind(result);
+                            $scope.initialGroup = angular.copy(result);
                         }
                     );
                 };
 
-                $scope.saveChapter = function () {
-                    commandService.updateGroup($stateParams.questionnaireId, $scope.activeChapter.group);
+                $scope.saveChapter = function() {
+                    commandService.updateGroup($stateParams.questionnaireId, $scope.activeChapter.group).success(function(result) {
+                        $scope.initialGroup = angular.copy($scope.activeChapter);
+                    });
+                };
+
+                $scope.cancelGroup = function() {
+                    var temp = angular.copy($scope.initialGroup);
+                    dataBind(temp);
                 };
 
                 $scope.moveToChapter = function(chapterId) {
