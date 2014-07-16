797f4edce24fffa8b7db0e7961bae53a2ff73ebe
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2014-07-15 23:25:42 -0400

    Preparation to generation of expression exeutor
    --HG--
    branch : csharp-business-language
    
    
    Former-commit-id: 5aa95b03b7082e3b794f6d8f2dc9bd368d5f7ccd

---
+++ src/Core/SharedKernels/DataCollection/ExpressionProcessing/AbstractInterviewExpressionState.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WB.Core.SharedKernels.ExpressionProcessing
+{
+    public abstract class AbstractInterviewExpressionState : IInterviewExpressionState
+    {
+        public Dictionary<string, IValidatable> InterviewScopes = new Dictionary<string, IValidatable>();
+        public Dictionary<string, List<string>> SiblingRosters = new Dictionary<string, List<string>>();
+
+
+        public abstract void AddRoster(Guid rosterId, decimal[] outerRosterVector, decimal rosterInstanceId, int? sortIndex);
+        public abstract void RemoveRoster(Guid rosterId, decimal[] rosterVector, decimal rosterInstanceId);
+
+
+        public abstract void UpdateIntAnswer(Guid questionId, decimal[] rosterVector, long answer);
+        public abstract void UpdateDecimalAnswer(Guid questionId, decimal[] rosterVector, decimal answer);
+        public abstract void UpdateDateAnswer(Guid questionId, decimal[] rosterVector, DateTime answer);
+        public abstract void UpdateTextAnswer(Guid questionId, decimal[] rosterVector, string answer);
+        public abstract void UpdateQrBarcodeAnswer(Guid questionId, decimal[] rosterVector, string answer);
+        public abstract void UpdateSingleOptionAnswer(Guid questionId, decimal[] rosterVector, decimal answer);
+        public abstract void UpdateMultiOptionAnswer(Guid questionId, decimal[] rosterVector, decimal[] answer);
+        public abstract void UpdateGeoLocationAnswer(Guid questionId, decimal[] propagationVector, double latitude, double longitude);
+        public abstract void UpdateTextListAnswer(Guid questionId, decimal[] propagationVector, Tuple<decimal, string>[] answers);
+        public abstract void UpdateLinkedSingleOptionAnswer(Guid questionId, decimal[] propagationVector, decimal[] selectedPropagationVector);
+        public abstract void UpdateLinkedMultiOptionAnswer(Guid questionId, decimal[] propagationVector, decimal[][] selectedPropagationVectors);
+
+        public void DeclareAnswersInvalid(IEnumerable<Identity> invalidQuestions)
+        {
+            foreach (var identity in invalidQuestions)
+            {
+                var targetLevel = this.GetRosterByIdAndVector(identity.Id, identity.RosterVector);
+                if (targetLevel == null) return;
+
+                targetLevel.DeclareAnswerInvalid(identity.Id);
+            }
+        }
+
+        public void DeclareAnswersValid(IEnumerable<Identity> validQuestions)
+        {
+            foreach (var identity in validQuestions)
+            {
+                var targetLevel = this.GetRosterByIdAndVector(identity.Id, identity.RosterVector);
+                if (targetLevel == null) return;
+
+                targetLevel.DeclareAnswerInvalid(identity.Id);
+            }
+        }
+
+        public void DisableGroups(IEnumerable<Identity> groupsToDisable)
+        {
+            foreach (var identity in groupsToDisable)
+            {
+                var targetLevel = this.GetRosterByIdAndVector(identity.Id, identity.RosterVector);
+                if (targetLevel == null) return;
+
+                targetLevel.DisableGroup(identity.Id);
+            }
+        }
+
+        public void EnableGroups(IEnumerable<Identity> groupsToEnable)
+        {
+            foreach (var identity in groupsToEnable)
+            {
+                var targetLevel = this.GetRosterByIdAndVector(identity.Id, identity.RosterVector);
+                if (targetLevel == null) return;
+
+                targetLevel.EnableGroup(identity.Id);
+            }
+        }
+
+        public void DisableQuestions(IEnumerable<Identity> questionsToDisable)
+        {
+            foreach (var identity in questionsToDisable)
+            {
+                var targetLevel = this.GetRosterByIdAndVector(identity.Id, identity.RosterVector);
+                if (targetLevel == null) return;
+
+                targetLevel.DisableQuestion(identity.Id);
+            }
+        }
+
+        public void EnableQuestions(IEnumerable<Identity> questionsToEnable)
+        {
+            foreach (var identity in questionsToEnable)
+            {
+                var targetLevel = this.GetRosterByIdAndVector(identity.Id, identity.RosterVector);
+                if (targetLevel == null) return;
+
+                targetLevel.EnableQuestion(identity.Id);
+            }
+        }
+
+        
+        public abstract void ProcessValidationExpressions(List<Identity> questionsToBeValid, List<Identity> questionsToBeInvalid);
+        public abstract void ProcessConditionExpressions(List<Identity> questionsToBeEnabled, List<Identity> questionsToBeDisabled, List<Identity> groupsToBeEnabled, List<Identity> groupsToBeDisabled);
+        
+        public abstract IInterviewExpressionState Clone();
+
+
+        protected IValidatable GetRosterByIdAndVector(Guid questionId, decimal[] rosterVector)
+        {
+            if (!IdOf.parentsMap.ContainsKey(questionId))
+                return null;
+
+            var rosterKey = Util.GetRosterKey(IdOf.parentsMap[questionId], rosterVector);
+            var rosterStringKey = Util.GetRosterStringKey(rosterKey);
+            return this.InterviewScopes.ContainsKey(rosterStringKey) ? this.InterviewScopes[rosterStringKey] : null;
+        }
+
+    }
+}
---
+++ src/Core/SharedKernels/DataCollection/ExpressionProcessing/ConditionalState.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WB.Core.SharedKernels.ExpressionProcessing
+{
+    public interface IValidatable
+    {
+        IValidatable CopyMembers();
+
+        Identity[] GetRosterKey();
+        void SetParent(IValidatable parentLevel);
+        IValidatable GetParent();
+
+
+        void DisableQuestion(Guid questionId);
+        void EnableQuestion(Guid questionId);
+
+        void DisableGroup(Guid groupId);
+        void EnableGroup(Guid groupId);
+
+        void DeclareAnswerValid(Guid questionId);
+        void DeclareAnswerInvalid(Guid questionId);
+
+    }
+
+    public interface IValidatableRoster
+    {
+        void Validate(IEnumerable<IValidatable> rosters, List<Identity> questionsToBeValid, List<Identity> questionsToBeInvalid);
+        
+        void RunConditions(IEnumerable<IValidatable> rosters, List<Identity> questionsToBeEnabled, List<Identity> questionsToBeDisabled,
+            List<Identity> groupsToBeEnabled, List<Identity> groupsToBeDisabled);
+    }
+
+    //could be replaces with bool?
+
+    public enum State
+    {
+        Unknown = 0,
+        Enabled = 1,
+        Disabled = 2
+    }
+    public enum ItemType
+    {
+        Question = 1,
+        Group = 10
+    }
+
+    public class ConditionalState
+    {
+        public ConditionalState(Guid itemId, ItemType type = ItemType.Question, State state = State.Enabled, State previousState = State.Enabled)
+        {
+            this.Type = type;
+            this.ItemId = itemId;
+            this.State = state;
+            this.PreviousState = previousState;
+        }
+
+        public Guid ItemId { get; set; }
+        public ItemType Type { get; set; }
+        public State State { get; set; }
+        public State PreviousState { get; set; }
+    }
+
+    public abstract class AbstractConditionalLevel<T> where T : IValidatable
+    {
+        public decimal[] RosterVector { get; private set; }
+        public Identity[] RosterKey { get; private set; }
+
+        protected Dictionary<Guid, ConditionalState> EnablementStates = new Dictionary<Guid, ConditionalState>();
+
+        protected HashSet<Guid> ValidAnsweredQuestions = new HashSet<Guid>();
+        protected HashSet<Guid> InvalidAnsweredQuestions = new HashSet<Guid>();
+
+
+        protected abstract IEnumerable<Action<T[]>> ConditionExpressions { get; }
+
+        protected AbstractConditionalLevel(decimal[] rosterVector, Identity[] rosterKey)
+        {
+            this.RosterVector = rosterVector;
+            this.RosterKey = rosterKey;
+        }
+
+        private State RunConditionExpression(Func<T[], bool> expression, T[] rosters)
+        {
+            try
+            {
+                return expression(rosters) ? State.Enabled : State.Disabled;
+            }
+            catch
+            {
+                return State.Disabled;
+            }
+        }
+
+        protected void DisableAllDependentQuestions(Guid itemId)
+        {
+            if (!IdOf.conditionalDependencies.ContainsKey(itemId) || !IdOf.conditionalDependencies[itemId].Any()) return;
+
+            var stack = new Queue<Guid>(IdOf.conditionalDependencies[itemId]);
+            while (stack.Any())
+            {
+                var id = stack.Dequeue();
+
+                //var questionState = this.enablementStatus.FirstOrDefault(x => x.ItemId == id);
+
+                if (this.EnablementStates.ContainsKey(id))
+                {
+                    //delete
+                    //questionState.State = State.Disabled;
+
+                    this.EnablementStates[id].State = State.Disabled;
+                }
+
+                if (IdOf.conditionalDependencies.ContainsKey(id) && IdOf.conditionalDependencies[id].Any())
+                {
+                    foreach (var dependentQuestionId in IdOf.conditionalDependencies[id])
+                    {
+                        stack.Enqueue(dependentQuestionId);
+                    }
+                }
+            }
+        }
+
+        protected Action<T[]> Verifier(Func<T[], bool> isEnabled, Guid questionId, ConditionalState questionState)
+        {
+            return rosters =>
+            {
+                if (questionState.State == State.Disabled)
+                    return;
+
+                questionState.State = this.RunConditionExpression(isEnabled, rosters);
+                if (questionState.State == State.Disabled)
+                {
+                    this.DisableAllDependentQuestions(questionId);
+                }
+            };
+        }
+
+        public void EvaluateConditions(T[] roters, List<Identity> questionsToBeEnabled, List<Identity> questionsToBeDisabled,
+            List<Identity> groupsToBeEnabled, List<Identity> groupsToBeDisabled)
+        {
+            foreach (var state in this.EnablementStates.Values)
+            {
+                state.PreviousState = state.State;
+                state.State = State.Unknown;
+            }
+
+            foreach (Action<T[]> verifier in this.ConditionExpressions)
+            {
+                verifier(roters);
+            }
+
+            var questionsToBeEnabledArray = this.EnablementStates.Values
+                .Where(x => x.State == State.Enabled && x.State != x.PreviousState && x.Type == ItemType.Question)
+                .Select(x => new Identity(x.ItemId, this.RosterVector))
+                .ToArray();
+
+            var questionsToBeDisabledArray = this.EnablementStates.Values
+                .Where(x => x.State == State.Disabled && x.State != x.PreviousState && x.Type == ItemType.Question)
+                .Select(x => new Identity(x.ItemId, this.RosterVector))
+                .ToArray();
+
+            var groupsToBeEnabledArray = this.EnablementStates.Values
+                .Where(x => x.State == State.Enabled && x.State != x.PreviousState && x.Type == ItemType.Group)
+                .Select(x => new Identity(x.ItemId, this.RosterVector))
+                .ToArray();
+
+            var groupsToBeDisabledArray = this.EnablementStates.Values
+                .Where(x => x.State == State.Disabled && x.State != x.PreviousState && x.Type == ItemType.Group)
+                .Select(x => new Identity(x.ItemId, this.RosterVector));
+
+            questionsToBeEnabled.AddRange(questionsToBeEnabledArray);
+            questionsToBeDisabled.AddRange(questionsToBeDisabledArray);
+            groupsToBeEnabled.AddRange(groupsToBeEnabledArray);
+            groupsToBeDisabled.AddRange(groupsToBeDisabledArray);
+        }
+
+        public void DeclareAnswerValid(Guid questionId)
+        {
+            ValidAnsweredQuestions.Add(questionId);
+            InvalidAnsweredQuestions.Remove(questionId);
+        }
+
+        public void DeclareAnswerInvalid(Guid questionId)
+        {
+            InvalidAnsweredQuestions.Add(questionId);
+            ValidAnsweredQuestions.Remove(questionId);
+        }
+
+
+        public void DisableQuestion(Guid questionId)
+        {
+            if (EnablementStates.ContainsKey(questionId))
+                EnablementStates[questionId].State = State.Disabled;
+        }
+
+        public void EnableQuestion(Guid questionId)
+        {
+            if (EnablementStates.ContainsKey(questionId))
+                EnablementStates[questionId].State = State.Enabled;
+        }
+
+        public void DisableGroup(Guid groupId)
+        {
+            if (EnablementStates.ContainsKey(groupId))
+                EnablementStates[groupId].State = State.Disabled;
+        }
+
+        public void EnableGroup(Guid groupId)
+        {
+            if (EnablementStates.ContainsKey(groupId))
+                EnablementStates[groupId].State = State.Enabled;
+        }
+
+        public Identity[] GetRosterKey()
+        {
+            return this.RosterKey;
+        }
+    }
+
+    public abstract class AbstractRosterLevel<T> : AbstractConditionalLevel<T>, IValidatableRoster where T : IValidatable
+    {
+        protected AbstractRosterLevel(decimal[] rosterVector, Identity[] rosterKey) : base(rosterVector, rosterKey) {}
+
+        protected Dictionary<Identity, Func<T[], bool>> validationExpressions = new Dictionary<Identity, Func<T[], bool>>();
+
+        public abstract void Validate(IEnumerable<IValidatable> rosters, List<Identity> questionsToBeValid,
+            List<Identity> questionsToBeInvalid);
+
+        public abstract void RunConditions(IEnumerable<IValidatable> rosters, List<Identity> questionsToBeEnabled,
+            List<Identity> questionsToBeDisabled, List<Identity> groupsToBeEnabled, List<Identity> groupsToBeDisabled);
+    }
+
+}
