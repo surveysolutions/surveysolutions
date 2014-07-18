1594e3193d1584b6888628203242e5e52f4775da
Slava Scherbak <slava.scherbak@gmail.com>
2014-07-18 14:57:04 +0300

    updated sample questionnaire with fixed roster and 2 questions
    --HG--
    branch : csharp-business-language
    
    
    Former-commit-id: 522a9ce63c2d73bc482c2723707bbd6241836374

--- src/Core/SharedKernels/DataCollection/ExpressionProcessing/QuestionnaireClasses.cs
+++ src/Core/SharedKernels/DataCollection/ExpressionProcessing/QuestionnaireClasses.cs
@@ -12,11 +12,12 @@ namespace WB.Core.SharedKernels.ExpressionProcessing
             : base(rosterVector, rosterKey, getInstances)
         {
             EnablementStates.Add(id_state.ItemId, id_state);
+            EnablementStates.Add(edu_visit_state.ItemId, edu_visit_state);
             //EnablementStates.Add(persons_count_state.ItemId, persons_count_state);
         }
 
         private string @__id;
-        private ConditionalState id_state = new ConditionalState(IdOf.id);
+        private readonly ConditionalState id_state = new ConditionalState(IdOf.id);
         public string id
         {
             get { return id_state.State == State.Enabled ? @__id : String.Empty; }
@@ -24,13 +25,21 @@ namespace WB.Core.SharedKernels.ExpressionProcessing
         }
 
         private long? @__persons_count;
-        private ConditionalState persons_count_state = new ConditionalState(IdOf.persons_count);
+        private readonly ConditionalState persons_count_state = new ConditionalState(IdOf.persons_count);
         public long? persons_count
         {
             get { return persons_count_state.State == State.Enabled ? @__persons_count : null; }
             set { @__persons_count = value; }
         }
-        
+
+        private decimal? @__edu_visit;
+        private readonly ConditionalState edu_visit_state = new ConditionalState(IdOf.edu_visit);
+        public decimal? edu_visit
+        {
+            get { return edu_visit_state.State == State.Enabled ? @__edu_visit : null; }
+            set { @__edu_visit = value; }
+        }
+
         public IValidatable CopyMembers()
         {
             var level = new QuestionnaireLevel(this.RosterVector, this.RosterKey, this.GetInstances);
@@ -51,20 +60,13 @@ namespace WB.Core.SharedKernels.ExpressionProcessing
             return level;
         }
 
-
-        /*public HhMember_type[] hhMembers
+        private bool edu_visit_IsEnabled()
         {
-            get
-            {
-                var key = this.RosterKey;
-                var rosters = this.GetInstances(key);
-                return rosters == null ? new HhMember_type[0] : rosters.Select(x => x as HhMember_type).ToArray();
-            }
-        }*/
+            return true;
+        }
 
         public void SetParent(IValidatable parentLevel)
         {
-            
         }
 
         public IValidatable GetParent()
@@ -74,13 +76,35 @@ namespace WB.Core.SharedKernels.ExpressionProcessing
 
         public void CalculateValidationChanges(List<Identity> questionsToBeValid, List<Identity> questionsToBeInvalid)
         {
-            
         }
-        
 
         protected override IEnumerable<Action> ConditionExpressions
         {
-            get { return Enumerable.Empty<Action>(); }
+            get
+            {
+                return new[]
+                {
+                    Verifier(edu_visit_IsEnabled, edu_visit_state.ItemId, edu_visit_state)
+                };
+            }
+        }
+
+        public HhMember_type[] hhMembers
+        {
+            get
+            {
+                var rosters = this.GetInstances(this.RosterKey);
+                return rosters == null ? new HhMember_type[0] : rosters.Select(x => x as HhMember_type).ToArray();
+            }
+        }
+
+        public Education_type[] educations
+        {
+            get
+            {
+                var rosters = this.GetInstances(this.RosterKey);
+                return rosters == null ? new Education_type[0] : rosters.Select(x => x as Education_type).ToArray();
+            }
         }
     }
     
@@ -96,7 +120,6 @@ namespace WB.Core.SharedKernels.ExpressionProcessing
         public HhMember_type(decimal[] rosterVector, Identity[] rosterKey, Func<Identity[], IEnumerable<IValidatable>> getInstances)
             : base(rosterVector, rosterKey, getInstances)
         {
-
             validationExpressions.Add(new Identity(IdOf.name, this.RosterVector), new Func<bool>[] { name_IsMandatory });
             validationExpressions.Add(new Identity(IdOf.age, this.RosterVector), new Func<bool>[] { age_IsValid });
             validationExpressions.Add(new Identity(IdOf.food, this.RosterVector), new Func<bool>[] { food_IsValid });
@@ -125,9 +148,20 @@ namespace WB.Core.SharedKernels.ExpressionProcessing
             }
         }
 
+        public FoodConsumption_type[] foods
+        {
+            get
+            {
+                var rosters = this.GetInstances(this.RosterKey);
+                return rosters == null ? new FoodConsumption_type[0] : rosters.Select(x => x as FoodConsumption_type).ToArray();
+            }
+        }
+
         public string id { get { return this.@__parent.id; } }
 
         public long? persons_count { get { return this.@__parent.persons_count; } }
+
+        public decimal? edu_visit { get { return this.@__parent.edu_visit; } }
 
         
         public string name { get; set; }
@@ -373,10 +407,9 @@ namespace WB.Core.SharedKernels.ExpressionProcessing
 
         public string id { get { return this.@__parent.id; } }
 
-        public long? persons_count
-        {
-            get { return this.@__parent.persons_count; }
-        }
+        public long? persons_count { get { return this.@__parent.persons_count; } }
+
+        public decimal? edu_visit { get { return this.@__parent.edu_visit; } }
 
         public string name
         {
@@ -508,6 +541,83 @@ namespace WB.Core.SharedKernels.ExpressionProcessing
         }
     }
 
+    public class Education_type : AbstractRosterLevel<Education_type>, IValidatable
+    {
+        public Education_type(decimal[] rosterVector, Identity[] rosterKey, QuestionnaireLevel parent, Func<Identity[], IEnumerable<IValidatable>> getInstances)
+            : this(rosterVector, rosterKey, getInstances)
+        {
+            this.@__parent = parent;
+        }
+
+        public Education_type(decimal[] rosterVector, Identity[] rosterKey, Func<Identity[], IEnumerable<IValidatable>> getInstances)
+            : base(rosterVector, rosterKey, getInstances)
+        {
+        }
+
+        private QuestionnaireLevel @__parent;
+
+        public Education_type[] educations
+        {
+            get
+            {
+                var rosters = this.GetInstances(this.RosterKey);
+                return rosters == null ? new Education_type[0] : rosters.Select(x => x as Education_type).ToArray();
+            }
+        }
+
+        public string id { get { return this.@__parent.id; } }
+
+        public long? persons_count { get { return this.@__parent.persons_count; } }
+
+        public decimal? edu_visit { get { return this.@__parent.edu_visit; } }
+
+        public decimal? edu { get; set; }
+
+
+        protected override IEnumerable<Action> ConditionExpressions
+        {
+            get
+            {
+                return new Action[]
+                {
+                };
+            }
+        }
+
+        public IValidatable CopyMembers()
+        {
+            var level = new Education_type(this.RosterVector, this.RosterKey, this.GetInstances)
+            {
+                ValidAnsweredQuestions = new HashSet<Guid>(this.ValidAnsweredQuestions),
+                InvalidAnsweredQuestions = new HashSet<Guid>(this.InvalidAnsweredQuestions),
+
+                edu = this.edu
+            };
+            foreach (var state in level.EnablementStates)
+            {
+                var originalState = this.EnablementStates[state.Key];
+                state.Value.PreviousState = originalState.PreviousState;
+                state.Value.State = originalState.State;
+            }
+
+            return level;
+        }
+
+        public void SetParent(IValidatable parentLevel)
+        {
+            this.@__parent = parentLevel as QuestionnaireLevel;
+        }
+
+        public IValidatable GetParent()
+        {
+            return this.@__parent;
+        }
+
+        public void CalculateValidationChanges(List<Identity> questionsToBeValid, List<Identity> questionsToBeInvalid)
+        {
+            this.Validate(questionsToBeValid, questionsToBeInvalid);
+        }
+    }
     
     public static class IdOf
     {
@@ -527,25 +637,32 @@ namespace WB.Core.SharedKernels.ExpressionProcessing
         public static readonly Guid person_id = Guid.Parse("9a5b5a4c-1746-4c31-a095-90a8d23e1a65");
         public static readonly Guid marital_status = Guid.Parse("0b84fcf2-e21c-4e9d-8257-7a9f988cb25b");
         public static readonly Guid married_with = Guid.Parse("b4434cee-3df0-47b2-9e27-ed81bd356d71");
-
-        public static readonly Guid[] hhMemberScopeIds = new[] { persons_count };
-        public static readonly Guid[] foodConsumptionIds = new[] { persons_count, food };
-
+        public static readonly Guid edu = Guid.Parse("4031fc8f-cc03-4803-882a-7ce882f68df5");
+        public static readonly Guid edu_visit = Guid.Parse("52f2a00a-9d15-454e-8702-f9f15871f463");
+        
         public static readonly Guid questionnaire = Guid.Parse("72897e3f-3dc8-4115-81e0-8a9c1cadec2d");
         public static readonly Guid hhMember = Guid.Parse("69b02bcf-0ed1-4b5a-80bb-bd465ab096da");
         public static readonly Guid foodConsumption = Guid.Parse("6df41d95-c785-4452-8303-bed3985d4c20");
         public static readonly Guid jobActivity = Guid.Parse("7556ddf0-457a-4a9b-a021-45ac3bad05a8");
 
         public static readonly Guid groupId = Guid.Parse("039ed69e-5583-46af-b983-488568f20e1c");
+        public static readonly Guid fixedId = Guid.Parse("a7b0d842-0355-4eab-a943-968c9c013d97");
+
+        public static readonly Guid[] eduScopeIds = new[] { fixedId };
+        public static readonly Guid[] hhMemberScopeIds = new[] { persons_count };
+        public static readonly Guid[] foodConsumptionIds = new[] { persons_count, food };
 
         //somehow should be generated
         public static Dictionary<Guid, Guid[]> conditionalDependencies = new Dictionary<Guid, Guid[]>()
         {
             { id, new Guid[] { } },
             { persons_count, new Guid[] { married_with } },
+            { edu_visit, new Guid[] { fixedId } },
+
             { name, new Guid[] { age } },
             { age, new Guid[] { groupId, has_job } },
             { groupId, new Guid[] { person_id, marital_status, married_with } },
+            
             { person_id, new Guid[] { } },
             { marital_status, new Guid[] { married_with } },
             { married_with, new Guid[] { } },
@@ -554,17 +671,20 @@ namespace WB.Core.SharedKernels.ExpressionProcessing
             { best_job_owner, new Guid[] { } },
 
             { sex, new Guid[] { food } },
-            { role, new Guid[] { food } },
+            { role, new Guid[] { food, edu_visit } },
             { food, new Guid[] { } },
 
             { times_per_week, new Guid[] { price_for_food } },
             { price_for_food, new Guid[] { } },
+
+            { fixedId, new Guid[] { edu } },
         };
 
         public static Dictionary<Guid, Guid[]> parentsMap = new Dictionary<Guid, Guid[]>
         {
             { id, new []{questionnaire} },
             { persons_count, new []{questionnaire}},
+            { edu_visit, new []{questionnaire} },
             { name, hhMemberScopeIds },
             { age, hhMemberScopeIds },
             { date, hhMemberScopeIds },
@@ -579,9 +699,11 @@ namespace WB.Core.SharedKernels.ExpressionProcessing
             { person_id, hhMemberScopeIds },
             { marital_status, hhMemberScopeIds },
             { married_with, hhMemberScopeIds },
+            { edu, eduScopeIds },
 
             //groups
             { groupId, hhMemberScopeIds },
+            { fixedId, eduScopeIds },
             { hhMember, hhMemberScopeIds },
             { foodConsumption, foodConsumptionIds },
             
@@ -589,6 +711,7 @@ namespace WB.Core.SharedKernels.ExpressionProcessing
 
         public static Dictionary<Guid, Guid[]> rostersIdToScopeMap = new Dictionary<Guid, Guid[]>
         {
+            { fixedId, eduScopeIds },
             { hhMember, hhMemberScopeIds },
             { foodConsumption, foodConsumptionIds },
             { jobActivity, hhMemberScopeIds }
