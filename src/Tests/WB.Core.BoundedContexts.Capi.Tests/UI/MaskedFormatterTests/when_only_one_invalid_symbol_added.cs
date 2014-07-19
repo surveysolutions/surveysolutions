6c91f289b72f77e5e49d8502f859489d930f987b
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2014-07-19 15:14:50 -0400

    CAPI: more tests for masked formatter KP-3698
    Former-commit-id: 1ab76dd380514c401a45832ee56f99de1d3de013

--- src/Tests/WB.Core.BoundedContexts.Capi.Tests/UI/MaskedFormatterTests/when_only_one_invalid_symbol_added.cs
+++ src/Tests/WB.Core.BoundedContexts.Capi.Tests/UI/MaskedFormatterTests/when_only_one_invalid_symbol_added.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,26 +10,27 @@ using WB.Core.BoundedContexts.Capi.UI.MaskFormatter;
 namespace WB.Core.BoundedContexts.Capi.Tests.UI.MaskedFormatterTests
 {
     [Subject(typeof(MaskedFormatter))]
-    internal class when_symbol_added_in_the_middle
+    class when_only_one_invalid_symbol_added
     {
+
         Establish context = () =>
         {
-            maskedFormatter=new MaskedFormatter(mask);
+            maskedFormatter = new MaskedFormatter(mask);
         };
 
         Because of = () =>
-            result=maskedFormatter.ValueToString(value,ref cursorPosition);
+            result = maskedFormatter.ValueToString(value, ref cursorPosition);
 
         It should_result_be_equal_to_passed_value = () =>
-            result.ShouldEqual("w2-9__-____");
+            result.ShouldEqual("__-___-____");
 
-        It should_cursor_be_equal_to_4 = () =>
-            cursorPosition.ShouldEqual(4);
+        It should_cursor_be_equal_to_0 = () =>
+            cursorPosition.ShouldEqual(0);
 
         private static MaskedFormatter maskedFormatter;
         private static string result;
         private static string mask = "a*-999-a999";
-        private static string value = "w2-9___-____";
-        private static int cursorPosition = 4;
+        private static string value = "1";
+        private static int cursorPosition = 1;
     }
 }
