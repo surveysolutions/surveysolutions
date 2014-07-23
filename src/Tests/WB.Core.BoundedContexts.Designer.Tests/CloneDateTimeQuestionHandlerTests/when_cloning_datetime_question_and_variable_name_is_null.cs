34aacf8b2d8bc8923d7db32bdb7a6af7e7cb0034
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2014-07-23 15:32:52 +0300

    Designer: resolved issue with inability to update question to numeric with empty variable name KP-3818
    Former-commit-id: e687de069b7c9c15eb1a10573a9d2a04f91b5349

---
+++ src/Tests/WB.Core.BoundedContexts.Designer.Tests/CloneDateTimeQuestionHandlerTests/when_cloning_datetime_question_and_variable_name_is_null.cs
@@ -0,0 +1,69 @@
+using System;
+using Machine.Specifications;
+using Main.Core.Entities.SubEntities;
+using Main.Core.Events.Questionnaire;
+using WB.Core.BoundedContexts.Designer.Aggregates;
+using WB.Core.BoundedContexts.Designer.Events.Questionnaire;
+using WB.Core.BoundedContexts.Designer.Tests.QuestionnaireTests;
+
+namespace WB.Core.BoundedContexts.Designer.Tests.CloneDateTimeQuestionHandlerTests
+{
+    internal class when_cloning_datetime_question_and_variable_name_is_null : QuestionnaireTestsContext
+    {
+        Establish context = () =>
+        {
+            questionnaire = CreateQuestionnaire(responsibleId: responsibleId);
+            questionnaire.Apply(new NewGroupAdded { PublicKey = chapterId });
+            questionnaire.Apply(new QRBarcodeQuestionAdded()
+            {
+                QuestionId = sourceQuestionId,
+                ParentGroupId = chapterId,
+                Title = "old title",
+                VariableName = "old_variable_name",
+                IsMandatory = false,
+                Instructions = "old instructions",
+                EnablementCondition = "old condition",
+                ResponsibleId = responsibleId
+            });
+        };
+
+        Because of = () =>
+            exception = Catch.Exception(() =>
+                questionnaire.CloneDateTimeQuestion(
+                    questionId: questionId,
+                    title: title,
+                    variableName: null,
+                    variableLabel: null,
+                    isMandatory: isMandatory,
+                    isPreFilled: isPreFilled,
+                    scope: scope,
+                    enablementCondition: enablementCondition,
+                    validationExpression: validationExpression,
+                    validationMessage: validationMessage,
+                    instructions: instructions,
+                    parentGroupId: parentGroupId,
+                    sourceQuestionId: sourceQuestionId,
+                    targetIndex: targetIndex,
+                    responsibleId: responsibleId));
+
+        It should_not_throw_exception = () => exception.ShouldBeNull();
+
+
+        private static Questionnaire questionnaire;
+        private static Exception exception;
+        private static Guid questionId = Guid.Parse("11111111111111111111111111111111");
+        private static Guid sourceQuestionId = Guid.Parse("22222222222222222222222222222222");
+        private static Guid chapterId = Guid.Parse("CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC");
+        private static Guid responsibleId = Guid.Parse("DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD");
+        private static Guid parentGroupId = Guid.Parse("CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC");
+        private static bool isMandatory = true;
+        private static string title = "title";
+        private static string instructions = "intructions";
+        private static int targetIndex = 1;
+        private static bool isPreFilled = false;
+        private static QuestionScope scope = QuestionScope.Interviewer;
+        private static string enablementCondition = null;
+        private static string validationExpression = null;
+        private static string validationMessage = null;
+    }
+}
