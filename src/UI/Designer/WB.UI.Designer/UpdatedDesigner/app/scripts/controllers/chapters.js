37ba2832fb318c954bec5832ae3364a069f1ee0f
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2014-07-23 12:59:36 +0300

    KP-3732: Added ability to clone chapter from chapter details page
    Former-commit-id: f5ad235e12147b3a3b6f089108887363868b852a

--- src/UI/Designer/WB.UI.Designer/UpdatedDesigner/app/scripts/controllers/chapters.js
+++ src/UI/Designer/WB.UI.Designer/UpdatedDesigner/app/scripts/controllers/chapters.js
@@ -1,105 +1,103 @@
-﻿(function() {
-    'use strict';
+﻿angular.module('designerApp')
+    .controller('ChaptersCtrl', [
+        '$rootScope', '$scope', '$state', 'commandService', 'utilityService', '$log', 'confirmService', 'questionnaireService',
+        function ($rootScope, $scope, $state, commandService, math, $log, confirmService, questionnaireService) {
+            'use strict';
+            $scope.chapters = [];
 
-    angular.module('designerApp')
-        .controller('ChaptersCtrl', [
-            '$rootScope', '$scope', '$state', 'commandService', 'utilityService', '$log', 'confirmService', 'questionnaireService',
-            function($rootScope, $scope, $state, commandService, math, $log, confirmService, questionnaireService) {
+            $scope.isFolded = false;
 
-                $scope.chapters = [];
+            $scope.unfold = function () {
+                $scope.isFolded = true;
+            };
 
+            $scope.foldback = function () {
                 $scope.isFolded = false;
+            };
 
-                $scope.unfold = function() {
-                    $scope.isFolded = true;
+            $scope.editChapter = function (chapter) {
+                $state.go('questionnaire.chapter.group', { chapterId: chapter.itemId, itemId: chapter.itemId });
+            };
+
+            $scope.addNewChapter = function () {
+                var newId = math.guid();
+
+                var newChapter = {
+                    title: 'New Chapter',
+                    itemId: newId
                 };
 
-                $scope.foldback = function() {
-                    $scope.isFolded = false;
-                };
-
-                $scope.editChapter = function(chapter) {
-                    $state.go('questionnaire.chapter.group', { chapterId: chapter.itemId, itemId: chapter.itemId });
-                };
-
-                $scope.addNewChapter = function() {
-                    var newId = math.guid();
-
-                    var newChapter = {
-                        title: 'New Chapter',
-                        itemId: newId
-                    };
-
-                    commandService.addChapter($state.params.questionnaireId, newChapter).success(function() {
-                        $scope.questionnaire.chapters.push(newChapter);
-                        $state.go('questionnaire.chapter.group', { chapterId: newChapter.itemId, itemId: newChapter.itemId });
-                        $rootScope.$emit('groupAdded');
-                    });
-                };
-
-                $scope.cloneChapter = function(chapter) {
-                    var newId = math.guid();
-                    var indexOf = _.indexOf($scope.questionnaire.chapters, chapter) + 1;
-
-                    commandService.cloneGroup($state.params.questionnaireId, chapter.itemId, indexOf, newId).success(function(result) {
-                        if (result.IsSuccess) {
-                            var newChapter = {
-                                title: chapter.title,
-                                itemId: newId
-                            };
-                            $scope.questionnaire.chapters.splice(indexOf, 0, newChapter);
-                            $rootScope.$emit('chapterCloned');
-                            $state.go('questionnaire.chapter.group', { chapterId: newId, itemId: newId });
-                        }
-                    });
-                };
-
-                $scope.deleteChapter = function (chapter) {
-                    var itemIdToDelete = chapter.itemId || $state.params.itemId;
-
-                    var modalInstance = confirmService.open(chapter);
-
-                    modalInstance.result.then(function (confirmResult) {
-                        if (confirmResult === 'ok') {
-                            commandService.deleteGroup($state.params.questionnaireId, itemIdToDelete)
-                                .success(function (result) {
-                                    if (result.IsSuccess) {
-                                        var index = $scope.questionnaire.chapters.indexOf(chapter);
-                                        if (index > -1) {
-                                            $scope.questionnaire.chapters.splice(index, 1);
-                                            $rootScope.$emit('chapterDeleted');
-                                        }
-
-                                        questionnaireService.getQuestionnaireById($state.params.questionnaireId).success(function (questionnaire) {
-                                            $scope.questionnaire = questionnaire;
-                                            if (questionnaire.chapters.length > 0) {
-                                                var defaultChapter = _.first(questionnaire.chapters);
-                                                var itemId = defaultChapter.itemId;
-                                                $scope.currentChapter = defaultChapter;
-                                                $state.go('questionnaire.chapter.group', { chapterId: itemId, itemId: itemId });
-                                            }
-                                        });
-                                    }
-                                });
-                        }
-                    });
-                };
-
-
-                $rootScope.$on('$stateChangeSuccess', function() {
-                    $scope.foldback();
+                commandService.addChapter($state.params.questionnaireId, newChapter).success(function () {
+                    $scope.questionnaire.chapters.push(newChapter);
+                    $state.go('questionnaire.chapter.group', { chapterId: newChapter.itemId, itemId: newChapter.itemId });
+                    $rootScope.$emit('groupAdded');
                 });
+            };
 
-                $rootScope.$on('groupUpdated', function(event, data) {
-                    var chapter = questionnaireService.findItem($scope.questionnaire.chapters, data.itemId);
-                    if (chapter != null) {
-                        chapter.title = data.title;
+            $rootScope.cloneChapter = function (chapterId) {
+                var idToClone = chapterId || $state.params.itemId;
+                var newId = math.guid();
+                var chapter = _.find($scope.questionnaire.chapters, { itemId: idToClone });
+                var targetIndex = _.indexOf($scope.questionnaire.chapters, chapter) + 1;
+
+                commandService.cloneGroup($state.params.questionnaireId, idToClone, targetIndex, newId).success(function (result) {
+                    if (result.IsSuccess) {
+                        var newChapter = {
+                            title: chapter.title,
+                            itemId: newId
+                        };
+                        $scope.questionnaire.chapters.splice(targetIndex, 0, newChapter);
+                        $rootScope.$emit('chapterCloned');
+                        $state.go('questionnaire.chapter.group', { chapterId: newId, itemId: newId });
                     }
                 });
+            };
 
-                $rootScope.$on('deleteChapter', function (event, data) {
-                    $scope.deleteChapter(data.chapter);
+            $scope.deleteChapter = function (chapter) {
+                var itemIdToDelete = chapter.itemId || $state.params.itemId;
+
+                var modalInstance = confirmService.open(chapter);
+
+                modalInstance.result.then(function (confirmResult) {
+                    if (confirmResult === 'ok') {
+                        commandService.deleteGroup($state.params.questionnaireId, itemIdToDelete)
+                            .success(function (result) {
+                                if (result.IsSuccess) {
+                                    var index = $scope.questionnaire.chapters.indexOf(chapter);
+                                    if (index > -1) {
+                                        $scope.questionnaire.chapters.splice(index, 1);
+                                        $rootScope.$emit('chapterDeleted');
+                                    }
+
+                                    questionnaireService.getQuestionnaireById($state.params.questionnaireId).success(function (questionnaire) {
+                                        $scope.questionnaire = questionnaire;
+                                        if (questionnaire.chapters.length > 0) {
+                                            var defaultChapter = _.first(questionnaire.chapters);
+                                            var itemId = defaultChapter.itemId;
+                                            $scope.currentChapter = defaultChapter;
+                                            $state.go('questionnaire.chapter.group', { chapterId: itemId, itemId: itemId });
+                                        }
+                                    });
+                                }
+                            });
+                    }
                 });
-            }
-        ]);
-}());
+            };
+
+
+            $rootScope.$on('$stateChangeSuccess', function () {
+                $scope.foldback();
+            });
+
+            $rootScope.$on('groupUpdated', function (event, data) {
+                var chapter = questionnaireService.findItem($scope.questionnaire.chapters, data.itemId);
+                if (chapter != null) {
+                    chapter.title = data.title;
+                }
+            });
+
+            $rootScope.$on('deleteChapter', function (event, data) {
+                $scope.deleteChapter(data.chapter);
+            });
+        }
+    ]);
