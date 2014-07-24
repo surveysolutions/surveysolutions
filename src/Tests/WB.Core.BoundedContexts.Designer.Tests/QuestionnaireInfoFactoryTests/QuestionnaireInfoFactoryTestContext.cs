5a8104dba2ca9a5f8ac845b3f520dd93008d99e6
Dima Pasko <dimapasko@gmail.com>
2014-07-24 14:07:03 +0300

    Added Unit test NumericIntegerTitles for as requested in Code Review KP-3404 UI: edit roster
    Former-commit-id: 52741b5aea28dd0b2e3005ab006512dddc6db078

--- src/Tests/WB.Core.BoundedContexts.Designer.Tests/QuestionnaireInfoFactoryTests/QuestionnaireInfoFactoryTestContext.cs
+++ src/Tests/WB.Core.BoundedContexts.Designer.Tests/QuestionnaireInfoFactoryTests/QuestionnaireInfoFactoryTestContext.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
 using Main.Core.Entities.SubEntities;
 using Moq;
 using WB.Core.BoundedContexts.Designer.Views.Questionnaire.Edit;
