9922b86c6ada1f4b7f3e0543a5acac733307f249
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2014-07-24 00:03:57 -0400

    generation of executor Questionnaire level
    --HG--
    branch : csharp-business-language
    
    
    Former-commit-id: dcc252faeabbaf98833d8a496d217f73af538286

---
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Implementation/Services/CodeGeneration/Templates/QuestionnaireLevelTemplateExt.cs
@@ -0,0 +1,12 @@
+﻿namespace WB.Core.BoundedContexts.Designer.Implementation.Services.CodeGeneration.Templates
+{
+    public partial class QuestionnaireLevelTemplate
+    {
+        protected QuestionnaireLevelTemplateModel Model { private set; get; }
+
+        public QuestionnaireLevelTemplate(QuestionnaireLevelTemplateModel model)
+        {
+            this.Model = model;
+        }
+    }
+}
