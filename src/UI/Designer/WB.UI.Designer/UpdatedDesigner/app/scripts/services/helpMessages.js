0272b1d5d0587e99b39f17e75e954cb51eb4322f
Dima Pasko <dimapasko@gmail.com>
2014-07-25 16:08:11 +0300

    Created Test messages infrastructure KP-3835 Help links should lead somewhere
    Former-commit-id: da6d3feb61e3a34a13cd53588b98203e08805663

---
+++ src/UI/Designer/WB.UI.Designer/UpdatedDesigner/app/scripts/services/helpMessages.js
@@ -0,0 +1,26 @@
+﻿angular.module('designerApp')
+    .factory('helpService', [
+        function() {
+            var helpService = {
+                'title': 'Title',
+                'description': 'Description',
+                'conditionExpression': 'Condition expression',
+                'validationExpression': 'Validation Expression',
+                'validationMessage': 'Validation Message',
+                'instruction': 'Instruction',
+                'mask': 'Mask',
+                'rosterType': 'Roster Type',
+                'variableName': 'Variable Name',
+                'sourceQuestion': 'Source question (answer generates number of rows)',
+                'titleQuestion': 'Titles of rows',
+                'fixedTitles': 'Fixed roster titles',
+
+            };
+
+            helpService.getHelpMessage = function(key) {
+                return helpService[key];
+            };
+
+            return helpService;
+        }
+    ]);
