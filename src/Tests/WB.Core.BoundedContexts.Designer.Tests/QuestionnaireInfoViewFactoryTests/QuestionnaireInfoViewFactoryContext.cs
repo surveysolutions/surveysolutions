3c6f8f2ae41d4f883520a0bf97d57a8193a2404c
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2014-07-25 16:38:27 -0400

    Designer: fix KP-3859
    Former-commit-id: 5602ad75d818678bbd2d9b40bcfbef518cec4630

--- src/Tests/WB.Core.BoundedContexts.Designer.Tests/QuestionnaireInfoViewFactoryTests/QuestionnaireInfoViewFactoryContext.cs
+++ src/Tests/WB.Core.BoundedContexts.Designer.Tests/QuestionnaireInfoViewFactoryTests/QuestionnaireInfoViewFactoryContext.cs
@@ -1,5 +1,6 @@
 ﻿using Main.Core.Documents;
 using Moq;
+using WB.Core.BoundedContexts.Designer.Views.Account;
 using WB.Core.BoundedContexts.Designer.Views.Questionnaire.Edit.QuestionnaireInfo;
 using WB.Core.BoundedContexts.Designer.Views.Questionnaire.SharedPersons;
 using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
@@ -16,12 +17,14 @@ namespace WB.Core.BoundedContexts.Designer.Tests.QuestionnaireInfoViewFactoryTes
         protected static QuestionnaireInfoViewFactory CreateQuestionnaireInfoViewFactory(
             IReadSideRepositoryReader<QuestionnaireDocument> documentReader = null, 
             IQueryableReadSideRepositoryReader<QuestionnaireInfoView> repository = null, 
-            IReadSideRepositoryReader<QuestionnaireSharedPersons> sharedWith = null)
+            IReadSideRepositoryReader<QuestionnaireSharedPersons> sharedWith = null,
+            IReadSideRepositoryReader<AccountDocument> accountsDocumentReader = null)
         {
             return
                 new QuestionnaireInfoViewFactory(repository ?? Mock.Of<IQueryableReadSideRepositoryReader<QuestionnaireInfoView>>(),
                                                 sharedWith ?? Mock.Of<IReadSideRepositoryReader<QuestionnaireSharedPersons>>(),
-                                                documentReader ?? Mock.Of<IReadSideRepositoryReader<QuestionnaireDocument>>(x => x.GetById(It.IsAny<string>()) == new QuestionnaireDocument()));
+                                                documentReader ?? Mock.Of<IReadSideRepositoryReader<QuestionnaireDocument>>(x => x.GetById(It.IsAny<string>()) == new QuestionnaireDocument()),
+                                                accountsDocumentReader ?? Mock.Of<IReadSideRepositoryReader<AccountDocument>>());
         }
     }
 }
