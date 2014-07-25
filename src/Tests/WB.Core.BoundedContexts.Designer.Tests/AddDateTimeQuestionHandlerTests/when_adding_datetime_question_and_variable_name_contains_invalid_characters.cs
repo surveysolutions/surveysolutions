c216b52c9e512b30746b0a003c29f5c32b5ee8f8
Dima Pasko <dimapasko@gmail.com>
2014-07-25 18:05:41 +0300

    Fixed typo in Unit Tests KP-3869 error message spelling
    Former-commit-id: ec7b79f90603668e804f39e829e6c1b29c7bc0a3

--- src/Tests/WB.Core.BoundedContexts.Designer.Tests/AddDateTimeQuestionHandlerTests/when_adding_datetime_question_and_variable_name_contains_invalid_characters.cs
+++ src/Tests/WB.Core.BoundedContexts.Designer.Tests/AddDateTimeQuestionHandlerTests/when_adding_datetime_question_and_variable_name_contains_invalid_characters.cs
@@ -37,7 +37,7 @@ namespace WB.Core.BoundedContexts.Designer.Tests.AddDateTimeQuestionHandlerTests
             exception.ShouldBeOfExactType<QuestionnaireException>();
 
         It should_throw_exception_with_message_containting__variable__contains__characters__ = () =>
-            new[] { "variable", "contains", "character" }.ShouldEachConformTo(
+            new[] { "variable", "contain", "character" }.ShouldEachConformTo(
                 keyword => exception.Message.ToLower().Contains(keyword));
 
 
--- src/Tests/WB.Core.BoundedContexts.Designer.Tests/AddTextQuestionHandlerTests/when_adding_text_question_and_variable_name_contains_invalid_characters.cs
+++ src/Tests/WB.Core.BoundedContexts.Designer.Tests/AddTextQuestionHandlerTests/when_adding_text_question_and_variable_name_contains_invalid_characters.cs
@@ -38,7 +38,7 @@ namespace WB.Core.BoundedContexts.Designer.Tests.AddTextQuestionHandlerTests
             exception.ShouldBeOfExactType<QuestionnaireException>();
 
         It should_throw_exception_with_message_containting__variable__contains__characters__ = () =>
-            new[] { "variable", "contains", "character" }.ShouldEachConformTo(
+            new[] { "variable", "contain", "character" }.ShouldEachConformTo(
                 keyword => exception.Message.ToLower().Contains(keyword));
 
 
