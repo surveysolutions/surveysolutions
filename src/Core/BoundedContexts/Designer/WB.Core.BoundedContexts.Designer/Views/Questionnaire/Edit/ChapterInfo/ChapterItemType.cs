9d2e23d79200b65412fd0a803cfbafe61e269634
Andrii Kozhyn <hedgic@gmail.com>
2014-07-28 13:41:48 +0300

    KP-3797: added property 'ItemType' to remove method call in bindings
    Former-commit-id: 5ff116d1740fee0690b80d000e9cd03386956048

---
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Views/Questionnaire/Edit/ChapterInfo/ChapterItemType.cs
@@ -0,0 +1,9 @@
+﻿namespace WB.Core.BoundedContexts.Designer.Views.Questionnaire.Edit.ChapterInfo
+{
+    public enum ChapterItemType
+    {
+        Question = 1,
+        Group = 2,
+        StaticText = 3
+    }
+}
