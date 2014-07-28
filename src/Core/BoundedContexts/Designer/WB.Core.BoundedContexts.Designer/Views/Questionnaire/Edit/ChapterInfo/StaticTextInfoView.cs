d343c2fcf6a9694052dd3aa9284318d4df89b067
Andrii Kozhyn <hedgic@gmail.com>
2014-07-28 13:41:48 +0300

    KP-3797: added property 'ItemType' to remove method call in bindings
    Former-commit-id: 5ff116d1740fee0690b80d000e9cd03386956048

--- src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Views/Questionnaire/Edit/ChapterInfo/StaticTextInfoView.cs
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Views/Questionnaire/Edit/ChapterInfo/StaticTextInfoView.cs
@@ -3,6 +3,7 @@
     public class StaticTextInfoView : IQuestionnaireItem
     {
         public string ItemId { get; set; }
+        public ChapterItemType ItemType { get { return ChapterItemType.StaticText; }}
         public string Text { get; set; }
     }
 }
