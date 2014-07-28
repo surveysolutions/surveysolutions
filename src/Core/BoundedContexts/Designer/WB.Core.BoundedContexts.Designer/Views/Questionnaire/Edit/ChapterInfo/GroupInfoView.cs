dcdf956931614fcb6e8cdd3738601ca0358df173
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2014-07-28 13:41:48 +0300

    KP-3797: added property 'ItemType' to remove method call in bindings
    Former-commit-id: 5ff116d1740fee0690b80d000e9cd03386956048

--- src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Views/Questionnaire/Edit/ChapterInfo/GroupInfoView.cs
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Views/Questionnaire/Edit/ChapterInfo/GroupInfoView.cs
@@ -6,6 +6,7 @@ namespace WB.Core.BoundedContexts.Designer.Views.Questionnaire.Edit.ChapterInfo
     public class GroupInfoView : IQuestionnaireItem, IReadSideRepositoryEntity
     {
         public string ItemId { get; set; }
+        public ChapterItemType ItemType { get { return ChapterItemType.Group; }}
         public string Title { get; set; }
         public bool IsRoster { get; set; }
         public List<IQuestionnaireItem> Items { get; set; }
--- src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Views/Questionnaire/Edit/ChapterInfo/QuestionInfoView.cs
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Views/Questionnaire/Edit/ChapterInfo/QuestionInfoView.cs
@@ -6,6 +6,7 @@ namespace WB.Core.BoundedContexts.Designer.Views.Questionnaire.Edit.ChapterInfo
     public class QuestionInfoView : IQuestionnaireItem
     {
         public string ItemId { get; set; }
+        public ChapterItemType ItemType { get {return ChapterItemType.Question;}}
         public string Title { get; set; }
         public string Variable { get; set; }
         public string LinkedToQuestionId { get; set; }
