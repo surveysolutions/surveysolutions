057ac93c974b5cd8d3441047f97dd7ac2e0e11fb
Roman Lytvyn <none@none>
2014-07-30 00:13:04 +0300

    designer: added maintenance page. KP-3868
    Former-commit-id: 00619a4c2ca2d55ad8175b751f08ff7a042a2503

---
+++ src/UI/Designer/WB.UI.Designer/Filters/MaintenanceFilter.cs
@@ -0,0 +1,34 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+using Microsoft.Practices.ServiceLocation;
+using WB.Core.Infrastructure.ReadSide;
+using WB.UI.Designer.Controllers;
+
+namespace WB.UI.Designer.Filters
+{
+    public class MaintenanceFilter : ActionFilterAttribute
+    {
+        private IReadSideStatusService readSideStatusService
+        {
+            get { return ServiceLocator.Current.GetInstance<IReadSideStatusService>(); }
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (filterContext.Controller is ControlPanelController) return;
+
+            if (!(filterContext.Controller is MaintenanceController) && readSideStatusService.AreViewsBeingRebuiltNow())
+            {
+                filterContext.Result =
+                    new RedirectToRouteResult(
+                        new RouteValueDictionary(
+                            new
+                            {
+                                controller = "Maintenance",
+                                action = "WaitForReadLayerRebuild",
+                                returnUrl = filterContext.RequestContext.HttpContext.Request.Url.ToString()
+                            }));
+            }
+        }
+    }
+}
