17ede374d7efcad43cc05dae55be078780c3ac17
Dima Pasko <dimapasko@gmail.com>
2014-07-29 13:32:51 +0300

    Add comments for Zurab KP-3835 Help links should lead somewhere
    Former-commit-id: bc8770cbd48acef17661af21f9494cdca588cd23

--- src/UI/Designer/WB.UI.Designer/UpdatedDesigner/app/scripts/services/helpMessages.js
+++ src/UI/Designer/WB.UI.Designer/UpdatedDesigner/app/scripts/services/helpMessages.js
@@ -2,21 +2,24 @@
     .factory('helpService', [
         function() {
             var helpService = {
+                // need to add messages
                 'title': 'Title',
                 'description': 'Description',
-                'conditionExpression': 'A logical expression that activates(disactivates) the current question(group) depending on the answers on the other questions.',
-                'validationExpression': 'A logical expression that validates an answer to the current question. Might include values of other questions.',
-                'validationMessage': 'Validation message is shown when an answer to the current question fails a validation condition.',
                 'instruction': 'Instruction',
                 'mask': 'Mask',
                 'rosterType': 'Roster Type',
                 'variableName': 'Variable Name',
-                'sourceQuestion': 'Source question (answer generates number of rows)',
                 'titleQuestion': 'Titles of rows',
-                'fixedTitles': 'Number of items determines the roster size. Each element of the list corresponds to a title of the roster row. Each element of the list should be placed on a separate line.',
                 'questionType': 'Question Type',
                 'variable': 'Variable',
-                'variableLabel': 'Variable Label'
+                'variableLabel': 'Variable Label',
+
+                // messages from old designer
+                'conditionExpression': 'A logical expression that activates(disactivates) the current question(group) depending on the answers on the other questions.',
+                'validationExpression': 'A logical expression that validates an answer to the current question. Might include values of other questions.',
+                'validationMessage': 'Validation message is shown when an answer to the current question fails a validation condition.',
+                'sourceQuestion': 'Source question (answer generates number of rows)',                
+                'fixedTitles': 'Number of items determines the roster size. Each element of the list corresponds to a title of the roster row. Each element of the list should be placed on a separate line.'
             };
 
             helpService.getHelpMessage = function(key) {
