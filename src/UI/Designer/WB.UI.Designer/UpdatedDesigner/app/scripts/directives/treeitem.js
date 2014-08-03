408eba62176bb0379ffd9d470078ca67d258ed1e
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2014-08-03 12:34:17 +0300

    include missing files
    Former-commit-id: 0d4b1cb653b0219ced4dee62cf5f468b28ab1add

--- src/UI/Designer/WB.UI.Designer/UpdatedDesigner/app/scripts/directives/treeitem.js
+++
@@ -1,9 +0,0 @@
-angular.module('designerApp')
-  .directive('treeItem', function () {
-    return {
-      templateUrl: function(element, attr) {
-  		return 'tree' + attr.itemType;
-      },
-      restrict: 'E'
-    };
-  });
