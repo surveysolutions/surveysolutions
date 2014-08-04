6126927b62cbf8c2aeafb13bfe9324b4769943ba
Dima Pasko <dimapasko@gmail.com>
2014-08-04 14:16:35 +0300

    Add Unit Tests KP-3894 Questionnaire with deleted status causes 'Request failed unexpectedly'
    Former-commit-id: 435b6cd92df603189f75356e8ca823394d57db15

---
+++ src/Tests/WB.Tests.Unit/Applications/Designer/QuestionnaireHelper/QuestionnaireHelperTestContext.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using Main.Core.View;
+using Moq;
+using WB.Core.BoundedContexts.Designer.Views.Questionnaire.QuestionnaireList;
+using WB.UI.Shared.Web.Membership;
+
+namespace WB.Tests.Unit.Applications.Designer.QuestionnaireHelper
+{
+    internal class QuestionnaireHelperTestContext
+    {
+        protected static UI.Designer.Code.QuestionnaireHelper CreateQuestionnaireHelper()
+        {
+            var userHelperMock = new Mock<IMembershipUserService>();
+            var userViewFactoryMock = new Mock<IViewFactory<QuestionnaireListInputModel, QuestionnaireListView>>();
+            var user = CreateAdminUser(userHelperMock);
+
+            userHelperMock.Setup(_ => _.WebUser).Returns(user);
+
+            userViewFactoryMock.Setup(_ => _.Load(It.IsAny<QuestionnaireListInputModel>()))
+                .Returns(new QuestionnaireListView(1,2,2,
+                    new List<QuestionnaireListViewItem>
+                    {
+                        CreateDeletedQuestionnaire(user.UserId),
+                        CreateNotDeletedQuestionnaire(user.UserId)
+                    },
+                    string.Empty
+                    ));
+
+            return new UI.Designer.Code.QuestionnaireHelper(userHelperMock.Object, userViewFactoryMock.Object);
+        }
+
+        protected static IMembershipWebUser CreateAdminUser(Mock<IMembershipUserService> userHelperMock)
+        {
+            var user = new Mock<IMembershipWebUser>();
+
+            var userId = Guid.NewGuid();
+
+            user.Setup(_ => _.IsAdmin).Returns(true);
+            user.Setup(_ => _.UserId).Returns(userId);
+
+            return user.Object;
+        }
+
+        protected static QuestionnaireListViewItem CreateNotDeletedQuestionnaire(Guid userGuid)
+        {
+            var listViewItem = new QuestionnaireListViewItem() { IsDeleted = false };
+            listViewItem.SharedPersons.Add(userGuid);
+            return listViewItem;
+        }
+
+        protected static QuestionnaireListViewItem CreateDeletedQuestionnaire(Guid userGuid)
+        {
+            var questionnaireListViewItem = new QuestionnaireListViewItem() { IsDeleted = true, };
+            questionnaireListViewItem.SharedPersons.Add(userGuid);
+            return questionnaireListViewItem;
+        }
+    }
+}
---
+++ src/Tests/WB.Tests.Unit/Applications/Designer/QuestionnaireHelper/when_getting_questionnaire_data_deleted_questionnaires_should_be_editable_.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Machine.Specifications;
+using WB.UI.Designer.BootstrapSupport.HtmlHelpers;
+using WB.UI.Designer.Models;
+
+namespace WB.Tests.Unit.Applications.Designer.QuestionnaireHelper
+{
+    internal class when_getting_questionnaire_data_deleted_questionnaires_should_be_editable_ : QuestionnaireHelperTestContext
+    {
+        Establish context = () =>
+        {
+            helper = CreateQuestionnaireHelper();
+        };
+
+        Because of = () =>
+            result = helper.GetPublicQuestionnaires(Guid.NewGuid());
+
+        It should_be_not_allowed_to_edit_deleted_questionnaire = () =>
+            result[0].CanEdit.ShouldEqual(false);
+
+        It should_be_allowed_to_edit_not_deleted_questionnaire = () =>
+            result[1].CanEdit.ShouldEqual(true);
+
+        private static UI.Designer.Code.QuestionnaireHelper helper;
+        private static IPagedList<QuestionnairePublicListViewModel> result;
+    }
+}
