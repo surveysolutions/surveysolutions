10a00f252d9c9a3315fb77fa7fb61808a07a75d9
Kirill Rudenko <cyril.rudenko@gmail.com>
2014-08-05 16:28:39 +0300

    KP-3933 Navbar overlay Tooltip
    Former-commit-id: 98d372b4b75781779675b4945056adf4b885a1bf

--- src/UI/Designer/WB.UI.Designer/UpdatedDesigner/app/scripts/directives/help.js
+++ src/UI/Designer/WB.UI.Designer/UpdatedDesigner/app/scripts/directives/help.js
@@ -10,7 +10,7 @@
                 link: function(scope, element, attrs) {
                     scope.message = helpService.getHelpMessage(scope.key);
                 },
-                template: '<span tooltip="{{message}}">(<a href="javascript:void(0);">?</a>)</span>',
+                template: '<span tooltip="{{message}}" tooltip-append-to-body="true">(<a href="javascript:void(0);">?</a>)</span>',
                 replace: false //otherwise we are getting conflict with angular ui popover directive
             };
         }
