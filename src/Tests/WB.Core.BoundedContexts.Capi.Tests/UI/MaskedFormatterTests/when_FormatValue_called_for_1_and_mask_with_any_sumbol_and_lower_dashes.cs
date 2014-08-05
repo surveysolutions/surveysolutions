064a0257f2600ae2995e7758dd148c56b1af0a04
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2014-08-05 12:52:46 -0400

    CAPI: fix mask issues KP-3698
    Former-commit-id: baaad8983508b3356d2d84c422e58db5301b8ac5

---
+++ src/Tests/WB.Core.BoundedContexts.Capi.Tests/UI/MaskedFormatterTests/when_FormatValue_called_for_1_and_mask_with_any_sumbol_and_lower_dashes.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Machine.Specifications;
+using WB.Core.BoundedContexts.Capi.UI.MaskFormatter;
+
+namespace WB.Core.BoundedContexts.Capi.Tests.UI.MaskedFormatterTests
+{
+    [Subject(typeof(MaskedFormatter))]
+    internal class when_FormatValue_called_for_1_and_mask_with_any_sumbol_and_lower_dashes
+    {
+        Establish context = () =>
+        {
+            maskedFormatter = new MaskedFormatter(mask);
+        };
+
+        Because of = () =>
+            result = maskedFormatter.FormatValue(value, ref cursorPosition);
+
+        It should_result_be_equal_to_formatted_value = () =>
+            result.ShouldEqual("1_-__-___");
+
+        It should_cursor_be_equal_to_4 = () =>
+            cursorPosition.ShouldEqual(1);
+
+        private static MaskedFormatter maskedFormatter;
+        private static string result;
+        private static string mask = "**-**-***";
+        private static string value = "1__-__-___";
+        private static int cursorPosition = 1;
+    }
+}
