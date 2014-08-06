caaeaed4c963bd81d501c76b51e9c6e85ec65a17
Anastasya Kholodova <nastya.kholodova@gmail.com>
2014-08-06 14:02:56 -0400

    HQ\Supervisor: synchronization of questionnaire in cencus mode KP-3688
    Former-commit-id: a3a8c06d3e87dccc5bd4a3e5dbe7f18aa17fe415

---
+++ src/Core/BoundedContexts/Supervisor/WB.Core.BoundedContexts.Supervisor/Synchronization/Implementation/LocalQuestionnaireFeedEntry.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WB.Core.SharedKernels.SurveyManagement.Synchronization.Questionnaire;
+
+namespace WB.Core.BoundedContexts.Supervisor.Synchronization.Implementation
+{
+    public class LocalQuestionnaireFeedEntry : QuestionnaireFeedEntry
+    {
+        public bool Processed { get; set; }
+
+        public bool ProcessedWithError { get; set; }
+
+        public Uri QuestionnaireUri { get; set; }
+    }
+}
