059ba9d9138ef7dc28b184d3069f70a744b3a78e
Slava Scherbak <slava.scherbak@gmail.com>
2014-08-08 15:21:52 +0300

    Designer: fixed bug with moving items inside it's chapter KP-3939
    --HG--
    branch : hotfixes-for-2-1-0
    
    
    Former-commit-id: c12aa9f2682e1b4a232479eb75f73fed753c6fb8

--- src/UI/Designer/WB.UI.Designer/UpdatedDesigner/app/scripts/controllers/tree.js
+++ src/UI/Designer/WB.UI.Designer/UpdatedDesigner/app/scripts/controllers/tree.js
@@ -207,7 +207,7 @@
                     connectTree();
                     var movedItem = event.source.nodeScope.item;
                     var destItem = event.dest.nodesScope.item;
-                    var destGroupId = destItem ? destItem.itemId : $scope.questionnaire.chapters[0].itemId;
+                    var destGroupId = destItem ? destItem.itemId : $state.params.chapterId;
                     var putItem = function(item, parent, index) {
                         var dropFrom = item.getParentItem() || $scope;
 
