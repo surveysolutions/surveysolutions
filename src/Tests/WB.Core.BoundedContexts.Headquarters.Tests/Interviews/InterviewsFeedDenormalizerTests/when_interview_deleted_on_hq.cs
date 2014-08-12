23974ab10ed451c614364b9dbaa35cfd4a542477
Anastasya Kholodova <nastya.kholodova@gmail.com>
2014-08-12 12:25:15 -0400

    DataCollection: unit tests for KP-3989 and KP-3975
    Former-commit-id: 34befdd36afb1896a331fdd5e6cab3f64ab18569

---
+++ src/Tests/WB.Core.BoundedContexts.Headquarters.Tests/Interviews/InterviewsFeedDenormalizerTests/when_interview_deleted_on_hq.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Machine.Specifications;
+using Ncqrs.Eventing.ServiceModel.Bus;
+using NSubstitute;
+using WB.Core.BoundedContexts.Headquarters.Interviews.Denormalizers;
+using WB.Core.GenericSubdomains.Utils;
+using WB.Core.Infrastructure.FunctionalDenormalization.Implementation.ReadSide;
+using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
+using WB.Core.SharedKernels.DataCollection.Events.Interview;
+using WB.Core.SharedKernels.SurveyManagement.Synchronization.Interview;
+using WB.Core.SharedKernels.SurveyManagement.Views.Interview;
+
+namespace WB.Core.BoundedContexts.Headquarters.Tests.Interviews.InterviewsFeedDenormalizerTests
+{
+    [Subject(typeof(InterviewsFeedDenormalizer))]
+    internal class when_interview_deleted_on_hq
+    {
+        Establish context = () =>
+        {
+            userId = Guid.NewGuid();
+            supervisorId = Guid.NewGuid();
+            interviewDeletedEvent = Create.PublishedEvent(Guid.NewGuid(), new InterviewDeleted(userId));
+
+            writer = Substitute.For<IReadSideRepositoryWriter<InterviewFeedEntry>>();
+            var interviews = Substitute.For<IReadSideRepositoryWriter<ViewWithSequence<InterviewData>>>();
+            interviews.GetById(interviewDeletedEvent.EventSourceId.FormatGuid())
+                .Returns(new ViewWithSequence<InterviewData>(new InterviewData { SupervisorId = supervisorId, CreatedOnClient = false }, 1));
+
+            denormalizer = Create.InterviewsFeedDenormalizer(writer, interviews);
+        };
+
+        private Because of = () => denormalizer.Handle(interviewDeletedEvent);
+
+        private It should_write_new_event_to_feed = () =>
+            writer.Received().Store(
+                Arg.Is<InterviewFeedEntry>(x =>
+                    x.InterviewId == interviewDeletedEvent.EventSourceId.FormatGuid() &&
+                    x.EntryType == EntryType.InterviewUnassigned &&
+                    x.Timestamp == interviewDeletedEvent.EventTimeStamp &&
+                    x.SupervisorId == supervisorId.FormatGuid() &&
+                    x.UserId == userId.FormatGuid() &&
+                    x.EntryId == interviewDeletedEvent.EventIdentifier.FormatGuid()),
+
+                interviewDeletedEvent.EventIdentifier.FormatGuid());
+
+        static InterviewsFeedDenormalizer denormalizer;
+        static IPublishedEvent<InterviewDeleted> interviewDeletedEvent;
+        static IReadSideRepositoryWriter<InterviewFeedEntry> writer;
+        private static Guid supervisorId;
+        private static Guid userId;
+    }
+}
