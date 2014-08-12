52c5950e532b9874787555b25ce563d3019215a9
Slava Scherbak <slava.scherbak@gmail.com>
2014-08-12 17:32:46 +0300

    Designer: added tests for titles limitation KP-3992
    --HG--
    branch : hotfixes-for-2-1-0
    
    
    Former-commit-id: e0207c5599e0dd9979f5bff6b4669044c1f71abb

---
+++ src/Tests/WB.Core.BoundedContexts.Designer.Tests/UpdateTextQuestionHandlerTests/when_updating_text_question_and_title_is_too_long.cs
@@ -0,0 +1,72 @@
+using System;
+using Machine.Specifications;
+using Main.Core.Entities.SubEntities;
+using Main.Core.Events.Questionnaire;
+using WB.Core.BoundedContexts.Designer.Aggregates;
+using WB.Core.BoundedContexts.Designer.Events.Questionnaire;
+using WB.Core.BoundedContexts.Designer.Exceptions;
+using WB.Core.BoundedContexts.Designer.Tests.QuestionnaireTests;
+
+namespace WB.Core.BoundedContexts.Designer.Tests.UpdateTextQuestionHandlerTests
+{
+    internal class when_updating_text_question_and_title_is_too_long : QuestionnaireTestsContext
+    {
+        Establish context = () =>
+        {
+            questionnaire = CreateQuestionnaire(responsibleId: responsibleId);
+            questionnaire.Apply(new NewGroupAdded { PublicKey = chapterId });
+            questionnaire.Apply(new QRBarcodeQuestionAdded()
+            {
+                QuestionId = questionId,
+                ParentGroupId = chapterId,
+                Title = "old title",
+                VariableName = "old_variable_name",
+                IsMandatory = false,
+                Instructions = "old instructions",
+                EnablementCondition = "old condition",
+                ResponsibleId = responsibleId
+            });
+        };
+
+        Because of = () =>
+            exception = Catch.Exception(() =>
+                questionnaire.UpdateTextQuestion(
+                    questionId: questionId,
+                    title: tooLongTitle,
+                    variableName: variableName,
+                    variableLabel: null,
+                    isMandatory: isMandatory,
+                    isPreFilled: isPreFilled,
+                    scope: scope,
+                    enablementCondition: enablementCondition,
+                    validationExpression: validationExpression,
+                    validationMessage: validationMessage,
+                    instructions: instructions,
+                    mask: null,
+                    responsibleId: responsibleId
+
+                    ));
+
+        It should_throw_QuestionnaireException = () =>
+            exception.ShouldBeOfExactType<QuestionnaireException>();
+
+        It should_throw_exception_with_code_equals_TitleIsTooLarge = () =>
+            (exception as QuestionnaireException).ErrorType.ShouldEqual(DomainExceptionType.TitleIsTooLarge);
+
+
+        private static Questionnaire questionnaire;
+        private static Exception exception;
+        private static Guid questionId = Guid.Parse("11111111111111111111111111111111");
+        private static Guid chapterId = Guid.Parse("CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC");
+        private static Guid responsibleId = Guid.Parse("DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD");
+        private static string variableName = "qr_barcode_question";
+        private static bool isMandatory = true;
+        private static string tooLongTitle = "A".PadRight(251, 'A');
+        private static string instructions = "intructions";
+        private static bool isPreFilled = false;
+        private static QuestionScope scope = QuestionScope.Interviewer;
+        private static string enablementCondition = null;
+        private static string validationExpression = null;
+        private static string validationMessage = "";
+    }
+}
