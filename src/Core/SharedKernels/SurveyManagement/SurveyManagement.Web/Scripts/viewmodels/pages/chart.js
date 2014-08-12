ec127432d822af7fb30ca37b3431f2f3bb1e9b55
Dima Pasko <dimapasko@gmail.com>
2014-08-12 17:38:35 +0300

    Created JS infrastructure KP-3965 Design Chart Area Chart(Cumulative Flow) Chart
    Former-commit-id: 1d3adabe42cb5a30b5df0265e60c0ae9eb633a37

---
+++ src/Core/SharedKernels/SurveyManagement/SurveyManagement.Web/Scripts/viewmodels/pages/chart.js
@@ -0,0 +1,11 @@
+﻿Supervisor.VM.ChartPage = function (serviceUrl, interviewDetailsUrl, users, commandExecutionUrl) {
+    Supervisor.VM.ChartPage.superclass.constructor.apply(this, [serviceUrl, commandExecutionUrl]);
+
+    var self = this;
+
+    self.load = function () {
+        self.search();
+    };
+
+};
+Supervisor.Framework.Classes.inherit(Supervisor.VM.ChartPage, Supervisor.VM.ListView);
