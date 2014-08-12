644c04d65461afce7664490af886124800ae4e01
Roman Lytvyn <litvin.roma@gmail.com>
2014-08-12 13:58:40 +0300

    designer: fixed old tests after adding new parameter "IsFilteredCombobox" to add/clone/update categorical one answer question in questionnaire ar methods. KP-3950
    Former-commit-id: 29de6fad111b5f981c61dba8cc457f339c0ca629

--- src/Tests/WB.Core.BoundedContexts.Designer.Tests/CloneSingleOptionQuestionHandlerTests/when_cloning_single_option_question_that_is_linked_and_referecing_question_not_under_roster.cs
+++ src/Tests/WB.Core.BoundedContexts.Designer.Tests/CloneSingleOptionQuestionHandlerTests/when_cloning_single_option_question_that_is_linked_and_referecing_question_not_under_roster.cs
@@ -58,7 +58,8 @@ namespace WB.Core.BoundedContexts.Designer.Tests.CloneSingleOptionQuestionHandle
                     targetIndex: targetIndex,
                     responsibleId: responsibleId,
                     options: options,
-                    linkedToQuestionId: linkedToQuestionId));
+                    linkedToQuestionId: linkedToQuestionId,
+                    isFilteredCombobox: isFilteredCombobox));
 
 
         It should_throw_QuestionnaireException = () =>
@@ -88,5 +89,6 @@ namespace WB.Core.BoundedContexts.Designer.Tests.CloneSingleOptionQuestionHandle
         private static Guid linkedToQuestionId = Guid.Parse("EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE");
         private static QuestionScope scope = QuestionScope.Interviewer;
         private static int targetIndex = 1;
+        private static bool isFilteredCombobox = false;
     }
 }
