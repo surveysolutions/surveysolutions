ba8758f9ac85e65e03abd565620028e0a8b0fb38
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2014-08-12 10:21:02 -0400

    Validation test added
    --HG--
    branch : csharp-business-language
    
    
    Former-commit-id: 91d9c13ac75efed005989a36740c40523bff6485

---
+++ src/Tests/WB.Core.BoundedContexts.Designer.Tests/CodeGenerationTests/when_expression_state_processes_validation_expressions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using Machine.Specifications;
+using Main.Core.Documents;
+using Microsoft.Practices.ServiceLocation;
+using Moq;
+using Ncqrs.Spec;
+using WB.Core.SharedKernels.DataCollection;
+using It = Machine.Specifications.It;
+
+namespace WB.Core.BoundedContexts.Designer.Tests.CodeGenerationTests
+{
+    internal class when_expression_state_processes_validation_expressions : CodeGenerationTestsContext
+    {
+
+        Establish context = () =>
+        {
+            eventContext = new EventContext();
+
+            var serviceLocatorMock = new Mock<IServiceLocator> { DefaultValue = DefaultValue.Mock };
+            ServiceLocator.SetLocatorProvider(() => serviceLocatorMock.Object);
+            
+            questionnaireDocument = CreateQuestionnaireWithOneNumericIntegerQuestionWithValidation(questionnaireId, questionId);
+
+            IInterviewExpressionStateProvider interviewExpressionStateProvider = GetInterviewExpressionStateProvider(questionnaireDocument);
+
+            
+            Mock.Get(ServiceLocator.Current)
+                .Setup(locator => locator.GetInstance<IInterviewExpressionStateProvider>())
+                .Returns(interviewExpressionStateProvider);
+
+            state = interviewExpressionStateProvider.GetExpressionState(questionnaireId, 0).Clone();
+
+            state.UpdateIntAnswer(questionId, new decimal[0], 4);
+
+        };
+
+        private Because of = () =>
+            state.ProcessValidationExpressions(out questionsToBeValid, out questionsToBeInvalid);
+
+        private It should_valid_question_count_equal_1 = () =>
+            questionsToBeValid.Count.ShouldEqual(1);
+            
+        private static Guid questionnaireId = Guid.Parse("11111111111111111111111111111111");
+        private static Guid questionId = Guid.Parse("11111111111111111111111111111112");
+        private static QuestionnaireDocument questionnaireDocument;
+
+        private static IInterviewExpressionState state;
+        private static List<Identity> questionsToBeValid;
+
+        private static List<Identity> questionsToBeInvalid;
+        private static EventContext eventContext;
+    }
+}
