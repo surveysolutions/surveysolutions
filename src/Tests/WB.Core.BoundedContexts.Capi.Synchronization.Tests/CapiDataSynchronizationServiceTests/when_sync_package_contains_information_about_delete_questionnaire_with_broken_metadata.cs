a261351e5d347dba0c1ea1eee4fe26c109b04af3
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2014-08-12 12:25:15 -0400

    DataCollection: unit tests for KP-3989 and KP-3975
    Former-commit-id: 34befdd36afb1896a331fdd5e6cab3f64ab18569

---
+++ src/Tests/WB.Core.BoundedContexts.Capi.Synchronization.Tests/CapiDataSynchronizationServiceTests/when_sync_package_contains_information_about_delete_questionnaire_with_broken_metadata.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Machine.Specifications;
+using Main.Core.Documents;
+using Moq;
+using Ncqrs.Commanding.ServiceModel;
+using WB.Core.BoundedContexts.Capi.Synchronization.ChangeLog;
+using WB.Core.BoundedContexts.Capi.Synchronization.Implementation.Services;
+using WB.Core.SharedKernel.Structures.Synchronization;
+using WB.Core.SharedKernel.Utils.Serialization;
+using WB.Core.SharedKernels.DataCollection.Commands.Questionnaire;
+using WB.Core.SharedKernels.DataCollection.Repositories;
+using It = Machine.Specifications.It;
+
+namespace WB.Core.BoundedContext.Capi.Synchronization.Tests.CapiDataSynchronizationServiceTests
+{
+    internal class when_sync_package_contains_information_about_delete_questionnaire_with_broken_metadata : CapiDataSynchronizationServiceTestContext
+    {
+        Establish context = () =>
+        {
+            syncItem = new SyncItem() { ItemType = SyncItemType.DeleteTemplate, IsCompressed = true, Content = "some content", MetaInfo = "some metadata", Id = Guid.NewGuid() };
+
+            var jsonUtilsMock = new Mock<IJsonUtils>();
+            jsonUtilsMock.Setup(x => x.Deserrialize<QuestionnaireMetadata>(syncItem.MetaInfo)).Throws<NullReferenceException>();
+
+            commandService = new Mock<ICommandService>();
+
+            plainQuestionnaireRepositoryMock = new Mock<IPlainQuestionnaireRepository>();
+            changeLogManipulator = new Mock<IChangeLogManipulator>();
+            capiDataSynchronizationService = CreateCapiDataSynchronizationService(changeLogManipulator.Object, commandService.Object, jsonUtilsMock.Object, null,
+                plainQuestionnaireRepositoryMock.Object);
+        };
+
+        Because of = () => exception = Catch.Exception(() => capiDataSynchronizationService.SavePulledItem(syncItem));
+
+        It should_not_call_RegisterPlainQuestionnaire =
+            () =>
+                commandService.Verify(
+                    x =>
+                        x.Execute(
+                            Moq.It.Is<DeleteQuestionnaire>(
+                                param =>
+                                    param.QuestionnaireId == Moq.It.IsAny<Guid>() && param.QuestionnaireVersion == Moq.It.IsAny<long>()), null),
+                    Times.Never);
+
+        It should_not_store_questionnaire_in_pline_storage =
+            () =>
+                plainQuestionnaireRepositoryMock.Verify(
+                    x => x.DeleteQuestionnaireDocument(Moq.It.IsAny<Guid>(), Moq.It.IsAny<long>()),
+                    Times.Never);
+
+        It should_throw_ArgumentException = () =>
+            exception.ShouldBeOfType<ArgumentException>();
+
+        It should_not_create_public_record_in_change_log_for_sync_item =
+        () =>
+            changeLogManipulator.Verify(
+                x =>
+                    x.CreatePublicRecord(syncItem.Id),
+                Times.Never);
+
+        private static CapiDataSynchronizationService capiDataSynchronizationService;
+        private static SyncItem syncItem;
+        private static Mock<ICommandService> commandService;
+        private static Mock<IPlainQuestionnaireRepository> plainQuestionnaireRepositoryMock;
+        private static Exception exception;
+        private static Mock<IChangeLogManipulator> changeLogManipulator;
+    }
+}
