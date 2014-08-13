d62f407d3b64f2c57b5ad7ffa6f07b22192303dc
Slava Scherbak <slava.scherbak@gmail.com>
2014-08-13 20:25:55 +0300

    Designer: bad fix of bug with encode questionnaire item titles KP-4001
    --HG--
    branch : hotfixes-for-2-2-1
    
    
    Former-commit-id: cbc20b6e6d4198aec81e14265c5cbe8fb5ab4f72

--- src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Views/Questionnaire/Edit/ChapterInfo/GroupInfoView.cs
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Views/Questionnaire/Edit/ChapterInfo/GroupInfoView.cs
@@ -5,9 +5,16 @@ namespace WB.Core.BoundedContexts.Designer.Views.Questionnaire.Edit.ChapterInfo
 {
     public class GroupInfoView : IQuestionnaireItem, IReadSideRepositoryEntity
     {
+        private string title;
         public string ItemId { get; set; }
         public ChapterItemType ItemType { get { return ChapterItemType.Group; }}
-        public string Title { get; set; }
+
+        public string Title
+        {
+            get { return this.title; }
+            set { this.title = System.Web.HttpUtility.HtmlDecode(value); }
+        }
+
         public bool IsRoster { get; set; }
         public List<IQuestionnaireItem> Items { get; set; }
         public int QuestionsCount { get; set; }
