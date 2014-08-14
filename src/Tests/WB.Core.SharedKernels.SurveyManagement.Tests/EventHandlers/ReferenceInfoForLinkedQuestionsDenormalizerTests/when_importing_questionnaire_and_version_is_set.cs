358e56386122ff99feb102e206ba81e392103ba8
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2014-08-14 17:36:30 -0400

    DataCollection\QuestionnaireVersion: unit tests KP-3998
    Former-commit-id: adefb445a9be30b36f5336b3ebf84e566bf1fed2

---
+++ src/Tests/WB.Core.SharedKernels.SurveyManagement.Tests/EventHandlers/ReferenceInfoForLinkedQuestionsDenormalizerTests/when_importing_questionnaire_and_version_is_set.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Machine.Specifications;
+using Main.Core.Documents;
+using Main.Core.Events.Questionnaire;
+using Moq;
+using Ncqrs.Eventing.ServiceModel.Bus;
+using WB.Core.GenericSubdomains.Utils;
+using WB.Core.SharedKernels.DataCollection.ReadSide;
+using WB.Core.SharedKernels.SurveyManagement.EventHandler;
+using WB.Core.SharedKernels.SurveyManagement.Views.Questionnaire;
+using It = Machine.Specifications.It;
+
+namespace WB.Core.SharedKernels.SurveyManagement.Tests.EventHandlers.ReferenceInfoForLinkedQuestionsDenormalizerTests
+{
+    internal class when_importing_questionnaire_and_version_is_set : ReferenceInfoForLinkedQuestionsDenormalizerTestContext
+    {
+        Establish context = () =>
+        {
+            var id = Guid.Parse("33332222111100000000111122223333");
+            importeDocument = CreateQuestionnaireDocumentWithRosterAndNumericQuestionAndLinedQuestionAfter(id, rosterId, linkedId);
+            referenceInfoForLinkedQuestionsWriter = new Mock<IVersionedReadSideRepositoryWriter<ReferenceInfoForLinkedQuestions>>();
+            denormalizer = CreateReferenceInfoForLinkedQuestionsDenormalizer(referenceInfoForLinkedQuestionsWriter.Object);
+            evnt = CreateTemplateImportedEvent(importeDocument,3);
+        };
+
+        Because of = () =>
+            denormalizer.Handle(evnt);
+
+        It should_create_one_row_with_mapping_of_linked_question_on_roster_scope = () =>
+            referenceInfoForLinkedQuestionsWriter.Verify(s => s.Store(
+                Moq.It.Is<ReferenceInfoForLinkedQuestions>(d => d.ReferencesOnLinkedQuestions.Count == 1 && d.Version==3),
+                Moq.It.Is<string>(g => g == importeDocument.PublicKey.FormatGuid())));
+
+        private static Guid rosterId = Guid.Parse("11111111111111111111111111111111");
+        private static Guid linkedId = Guid.Parse("22222222222222222222222222222222");
+        private static QuestionnaireDocument importeDocument;
+        private static ReferenceInfoForLinkedQuestionsDenormalizer denormalizer;
+        private static IPublishedEvent<TemplateImported> evnt;
+        private static Mock<IVersionedReadSideRepositoryWriter<ReferenceInfoForLinkedQuestions>> referenceInfoForLinkedQuestionsWriter;
+    }
+}
