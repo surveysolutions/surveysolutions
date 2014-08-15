abc930d39e95c16bb6c6b7c1e63c45c61f167889
Roman Lytvyn <none@none>
2014-08-15 12:12:54 +0300

    designer: fixed new designer when create new question. KP-4017
    Former-commit-id: 7d466d6628ff20c95298c54102fd3c132d731f9d

--- src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Commands/Questionnaire/Base/FullQuestionDataCommand.cs
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Commands/Questionnaire/Base/FullQuestionDataCommand.cs
@@ -8,10 +8,10 @@ namespace WB.Core.BoundedContexts.Designer.Commands.Questionnaire.Base
         protected FullQuestionDataCommand(Guid questionnaireId, Guid questionId,
             string title, QuestionType type, string variableName, string variableLabel, string mask, bool isMandatory, bool isPreFilled,
             QuestionScope scope, string enablementCondition, string validationExpression, string validationMessage, string instructions,
-            Option[] options,Guid responsibleId, Guid? linkedToQuestionId, bool areAnswersOrdered, int? maxAllowedAnswers)
+            Option[] options, Guid responsibleId, Guid? linkedToQuestionId, bool areAnswersOrdered, int? maxAllowedAnswers, bool isFilteredCombobox)
             : base(
                 questionnaireId, questionId, title, variableName, mask, isMandatory, isPreFilled, scope, enablementCondition,
-                validationExpression, validationMessage, instructions, responsibleId, variableLabel)
+                validationExpression, validationMessage, instructions, responsibleId, variableLabel, isFilteredCombobox)
         {
             this.Options = options;
             this.LinkedToQuestionId = linkedToQuestionId;
