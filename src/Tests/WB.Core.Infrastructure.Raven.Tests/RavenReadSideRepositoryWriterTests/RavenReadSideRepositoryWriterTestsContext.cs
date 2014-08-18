267180f407667643799fc3dd680183778c4251dc
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2014-08-18 13:50:16 +0300

    KP-4023: renamed .Raven project to .Storage
    Former-commit-id: eb09a24f9921e732e50259a904af18d86ddfe4ce

--- src/Tests/WB.Core.Infrastructure.Raven.Tests/RavenReadSideRepositoryWriterTests/RavenReadSideRepositoryWriterTestsContext.cs
+++ src/Tests/WB.Core.Infrastructure.Raven.Tests/RavenReadSideRepositoryWriterTests/RavenReadSideRepositoryWriterTestsContext.cs
@@ -5,7 +5,6 @@ using Moq;
 using Raven.Client.Document;
 using WB.Core.Infrastructure.Raven.Raven.Implementation.ReadSide;
 using WB.Core.Infrastructure.Raven.Raven.Implementation.ReadSide.RepositoryAccessors;
-using WB.Core.Infrastructure.ReadSide;
 using WB.Core.Infrastructure.ReadSide.Repository;
 
 using it = Moq.It;
