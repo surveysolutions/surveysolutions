3f2740703f29968b252a9ab201698ebcd4744a5a
Slava Scherbak <slava.scherbak@gmail.com>
2014-08-18 18:16:21 +0300

    Designer: fixed bug with email validation in share persons modal KP-3984
    --HG--
    branch : hotfixes-for-2-2-1
    
    
    Former-commit-id: cb2335b7586365fad5a99ac6127884ca9f10aa1d

--- src/UI/Designer/WB.UI.Designer/UpdatedDesigner/app/scripts/directives/directives.js
+++ src/UI/Designer/WB.UI.Designer/UpdatedDesigner/app/scripts/directives/directives.js
@@ -1,21 +1,5 @@
 ﻿(function() {
-    angular.module('designerApp').directive('bsHasError', [
-        function() {
-            return {
-                restrict: "A",
-                link: function(scope, element) {
-                    var input = element.find('input[ng-model]');
-                    if (input) {
-                        scope.$watch(function() {
-                            return input.hasClass('ng-invalid');
-                        }, function(isInvalid) {
-                            element.toggleClass('has-error', isInvalid);
-                        });
-                    }
-                }
-            };
-        }
-    ]).directive('focusOn', function() {
+    angular.module('designerApp').directive('focusOn', function() {
         return function(scope, elem, attr) {
             scope.$on('focusOn', function(e, name) {
                 if (name === attr.focusOn) {
