d52959ee8602d57cfca2763d005f96ad752078b7
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2014-08-19 15:12:53 +0300

    KP-4023: Added configuration of event store provider
    Former-commit-id: 71e933d3f2337a8c4cd66ca2125dbf4d22511fbb

---
+++ src/UI/Shared/WB.UI.Shared.Web/Settings/CoreSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Web.Configuration;
+
+namespace WB.UI.Shared.Web.Settings
+{
+    public static class CoreSettings
+    {
+        public static StoreProviders EventStoreProvider
+        {
+            get { return (StoreProviders) Enum.Parse(typeof(StoreProviders), WebConfigurationManager.AppSettings["Core.EventStoreProvider"], true); }
+        } 
+    }
+}
