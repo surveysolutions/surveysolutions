e94d053da6c2525471060053b562ff987884957d
Slava Scherbak <slava.scherbak@gmail.com>
2014-08-20 12:44:37 +0300

    Designer: reverted bad fix of encoding problem because only 1 questionnaire was affected on production server and it was fixed manually KP-4001
    --HG--
    branch : hotfixes-for-2-2-1
    
    
    Former-commit-id: 43844d9128abc85f0263a592e85a8fc621ce4507

--- src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Views/Questionnaire/Edit/ChapterInfo/QuestionInfoView.cs
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Views/Questionnaire/Edit/ChapterInfo/QuestionInfoView.cs
@@ -5,15 +5,10 @@ namespace WB.Core.BoundedContexts.Designer.Views.Questionnaire.Edit.ChapterInfo
 {
     public class QuestionInfoView : IQuestionnaireItem
     {
-        private string title;
         public string ItemId { get; set; }
         public ChapterItemType ItemType { get {return ChapterItemType.Question;}}
 
-        public string Title
-        {
-            get { return this.title; }
-            set { this.title = System.Web.HttpUtility.HtmlDecode(value); }
-        }
+        public string Title { get; set; }
 
         public string Variable { get; set; }
         public string LinkedToQuestionId { get; set; }
--- src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Views/Questionnaire/Edit/ChapterInfo/StaticTextInfoView.cs
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Views/Questionnaire/Edit/ChapterInfo/StaticTextInfoView.cs
@@ -2,14 +2,9 @@
 {
     public class StaticTextInfoView : IQuestionnaireItem
     {
-        private string text;
         public string ItemId { get; set; }
         public ChapterItemType ItemType { get { return ChapterItemType.StaticText; }}
 
-        public string Text
-        {
-            get { return this.text; }
-            set { this.text = System.Web.HttpUtility.HtmlDecode(value); }
-        }
+        public string Text { get; set; }
     }
 }
