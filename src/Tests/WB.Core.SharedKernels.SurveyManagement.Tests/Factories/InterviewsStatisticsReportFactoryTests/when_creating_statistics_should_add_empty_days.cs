7afe5a19ef89aa33c6d59268c069e5c18860f502
Dima Pasko <dimapasko@gmail.com>
2014-08-22 13:53:08 +0300

    HQ. Chart: reimplemented View Factory + added Unit tests KP-3966 Create Data Service for Chart
    Former-commit-id: 222db679f76feef1acbe5e7a3a0835192f97ccdd

--- src/Tests/WB.Core.SharedKernels.SurveyManagement.Tests/Factories/InterviewsStatisticsReportFactoryTests/when_creating_statistics_should_add_empty_days.cs
+++ src/Tests/WB.Core.SharedKernels.SurveyManagement.Tests/Factories/InterviewsStatisticsReportFactoryTests/when_creating_statistics_should_add_empty_days.cs
@@ -1,23 +1,99 @@
-﻿//using Machine.Specifications;
-//using WB.Core.SharedKernels.SurveyManagement.Views.Interviews;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Machine.Specifications;
+using Moq;
+using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
+using WB.Core.SharedKernels.SurveyManagement.Views.Interview;
+using WB.Core.SharedKernels.SurveyManagement.Views.Interviews;
+using It = Machine.Specifications.It;
 
-//namespace WB.Core.SharedKernels.SurveyManagement.Tests.Factories.InterviewsStatisticsReportFactoryTests
-//{
-//    internal class when_creating_statistics_should_add_empty_days : InterviewsStatisticsReportFactoryTestsContext
-//    {
-//        private Establish context = () =>
-//        {
-//            interviewsStatisticsReportFactory = CreateInterviewsStatisticsReportFactory();
-            
-//            interviewsStatisticsReportFactory.
-//        };
+namespace WB.Core.SharedKernels.SurveyManagement.Tests.Factories.InterviewsStatisticsReportFactoryTests
+{
+    internal class when_creating_statistics_should_add_empty_days : InterviewsStatisticsReportFactoryTestsContext
+    {
+        private Establish context = () =>
+        {
+            var stats = Mock.Of<IQueryableReadSideRepositoryReader<StatisticsLineGroupedByDateAndTemplate>>();
 
-//        private Because of = () => ;
+            var questionnaireId = Guid.NewGuid();
+            var baseDate = Convert.ToDateTime("8/22/2014");
+            var questionnaireVersion = 1;
 
-//        private It should_create_header_with_2_column = () => ;
+            var data = new List<StatisticsLineGroupedByDateAndTemplate>
+            {
+                new StatisticsLineGroupedByDateAndTemplate
+                {
+                    QuestionnaireId = questionnaireId,
+                    QuestionnaireVersion = questionnaireVersion,
+                    Date = baseDate.AddDays(-2),
+                    ApprovedByHeadquartersCount = 1,
+                    ApprovedBySupervisorCount = 1,
+                    CompletedCount = 1,
+                    InterviewerAssignedCount = 1,
+                    RejectedByHeadquartersCount = 1,
+                    RejectedBySupervisorCount = 1,
+                    SupervisorAssignedCount = 1
+                },
+                new StatisticsLineGroupedByDateAndTemplate
+                {
+                    QuestionnaireId = questionnaireId,
+                    QuestionnaireVersion = questionnaireVersion,
+                    Date = baseDate.AddDays(0),
+                    ApprovedByHeadquartersCount = 2,
+                    ApprovedBySupervisorCount = 2,
+                    CompletedCount = 2,
+                    InterviewerAssignedCount = 2,
+                    RejectedByHeadquartersCount = 2,
+                    RejectedBySupervisorCount = 2,
+                    SupervisorAssignedCount = 2
+                }
+            }.AsQueryable();
 
-//        private It should_create_header_with_nullable_level_labels = () => ;
+            Mock.Get(stats)
+                .Setup(
+                    _ =>
+                        _.Query(
+                            Moq.It
+                                .IsAny
+                                <Func<IQueryable<StatisticsLineGroupedByDateAndTemplate>, List<StatisticsLineGroupedByDateAndTemplate>>>()))
+                .Returns<Func<IQueryable<StatisticsLineGroupedByDateAndTemplate>, List<StatisticsLineGroupedByDateAndTemplate>>>(
+                    query => query.Invoke(data)
+                );
 
-//        private static InterviewsStatisticsReportFactory interviewsStatisticsReportFactory;
-//    }
-//}
+            interviewsStatisticsReportFactory = CreateInterviewsStatisticsReportFactory(stats);
+
+            input = new InterviewsStatisticsReportInputModel
+            {
+                CurrentDate = baseDate,
+                QuestionnaireId = questionnaireId,
+                QuestionnaireVersion = questionnaireVersion
+            };
+        };
+
+        Because of = () => view = interviewsStatisticsReportFactory.Load(input);
+
+        It should_have_days_count_muliply_two_records = () => view.Ticks.Length.ShouldEqual(3*2);
+
+        It should_have_0_rownumber_equals_1 = () => view.Ticks[0, 0].ShouldEqual("1");
+        It should_have_0_date_correct = () => view.Ticks[0, 1].ShouldEqual("8/20/2014");
+
+        It should_have_1_rownumber_equals_2 = () => view.Ticks[1, 0].ShouldEqual("2");
+        It should_have_1_date_correct = () => view.Ticks[1, 1].ShouldEqual("8/21/2014");
+
+        It should_have_2_rownumber_equals_3 = () => view.Ticks[2, 0].ShouldEqual("3");
+        It should_have_2_date_correct = () => view.Ticks[2, 1].ShouldEqual("8/22/2014");
+
+        It should_have_supervisorAssignedData_correct = () => view.Stats[0].ShouldEqual(new[] { 1, 1, 2 });
+        It should_have_interviewerAssignedData_correct = () => view.Stats[1].ShouldEqual(new[] { 1, 1, 2 });
+        It should_have_completedData_correct = () => view.Stats[2].ShouldEqual(new[] { 1, 1, 2 });
+        It should_have_rejectedBySupervisor_correct = () => view.Stats[3].ShouldEqual(new[] { 1, 1, 2 });
+        It should_have_approvedBySupervisor_correct = () => view.Stats[4].ShouldEqual(new[] { 1, 1, 2 });
+        It should_have_rejectedByHeadquarters_correct = () => view.Stats[5].ShouldEqual(new[] { 1, 1, 2 });
+        It should_have_approvedByHeadquarters_correct = () => view.Stats[6].ShouldEqual(new[] { 1, 1, 2 });
+
+        private static InterviewsStatisticsReportFactory interviewsStatisticsReportFactory;
+        private static InterviewsStatisticsReportInputModel input;
+        private static InterviewsStatisticsReportView view;
+    }
+}
