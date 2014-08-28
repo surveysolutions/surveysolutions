541322c4d0dcd4a368c60f2e810f65d962219fad
Slava Scherbak <slava.scherbak@gmail.com>
2014-08-28 09:56:07 +0300

    Designer: added tests for cutted options for editable filtered combobox question KP-4054
    Former-commit-id: 29389d6dff9eff5c351db0d81a54a18d452528cd

---
+++ src/Tests/WB.Tests.Unit/Applications/Designer/QuestionnaireApiControllerTests/when_getting_singleoption_filtered_combobox_question_info_with_more_than_18_options.cs
@@ -0,0 +1,38 @@
+using System;
+using Machine.Specifications;
+using Moq;
+using WB.Core.BoundedContexts.Designer.Views.Questionnaire.Edit;
+using WB.UI.Designer.Api;
+using It = Machine.Specifications.It;
+
+namespace WB.Tests.Unit.Applications.Designer.QuestionnaireApiControllerTests
+{
+    internal class when_getting_singleoption_filtered_combobox_question_info_with_more_than_18_options : QuestionnaireApiControllerTestContext
+    {
+        Establish context = () =>
+        {
+            var singleoptionFilteredCombobox = CreateSingleoptionFilteredCombobox(questionId, optionsCount: 18, isFilteredCombobox: true);
+
+            questionnaireInfoViewFactoryMock = new Mock<IQuestionnaireInfoFactory>();
+            questionnaireInfoViewFactoryMock
+                .Setup(x => x.GetQuestionEditView(questionnaireId, questionId))
+                .Returns(singleoptionFilteredCombobox);
+            controller = CreateQuestionnaireController(questionnaireInfoFactory: questionnaireInfoViewFactoryMock.Object);
+        };
+
+        Because of = () =>
+            result = controller.EditQuestion(questionnaireId, questionId);
+
+        It should_return_edit_question_details = () =>
+            result.ShouldNotBeNull();
+
+        It should_return_edit_question_details_with_18_options = () =>
+            result.Options.Length.ShouldEqual(18);
+
+        private static QuestionnaireController controller;
+        private static string questionnaireId = "22222222222222222222222222222222";
+        private static Guid questionId = Guid.Parse("11111111111111111111111111111111");
+        private static Mock<IQuestionnaireInfoFactory> questionnaireInfoViewFactoryMock;
+        private static NewEditQuestionView result;
+    }
+}
