f352172cb4bde60d4bf138ace57469f92e8db5c8
Slava Scherbak <slava.scherbak@gmail.com>
2014-08-28 16:27:23 +0300

    Designer: added tests for EditOptions action of QuestionnaireController KP-4055
    Former-commit-id: 3bc3f23b189e2ab6866303bed03dcc0eb3105559

---
+++ src/Tests/WB.Tests.Unit/Applications/Designer/QuestionnaireControllerTests/when_editing_options_and_non_csv_file_was_uploaded.cs
@@ -0,0 +1,46 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Web;
+using Machine.Specifications;
+using Moq;
+using WB.UI.Designer.Controllers;
+using It = Machine.Specifications.It;
+
+namespace WB.Tests.Unit.Applications.Designer.QuestionnaireControllerTests
+{
+    internal class when_editing_options_and_non_csv_file_was_uploaded : QuestionnaireControllerTestContext
+    {
+        Establish context = () =>
+        {
+            controller = CreateQuestionnaireController();
+            SetControllerContextWithSession(controller, "options", new QuestionnaireController.EditOptionsViewModel());
+
+            stream = new MemoryStream();
+            bmp = new Bitmap(10, 10);
+
+            var graphics = Graphics.FromImage(bmp);
+            graphics.FillRectangle(Brushes.Orange, 0, 0, 10, 10);
+            bmp.Save(stream, ImageFormat.Jpeg);
+
+            stream.Position = 0;
+            postedFile = Mock.Of<HttpPostedFileBase>(pf => pf.InputStream == stream && pf.FileName == "image.jpeg");
+        };
+
+        Because of = () => controller.EditOptions(postedFile);
+
+        It should_add_error_message_to_temp_data = () =>
+            controller.TempData["error"].ShouldEqual("Only .csv (comma-separated values) files are accepted");
+
+        Cleanup stuff = () =>
+        {
+            stream.Dispose();
+            bmp.Dispose();
+        };
+
+        private static QuestionnaireController controller;
+        private static HttpPostedFileBase postedFile;
+        private static Stream stream = new MemoryStream();
+        private static Bitmap bmp;
+    }
+}
