0dd98f7bd7eb13864f187b3571dbea1495cd832f
Dima Pasko <dimapasko@gmail.com>
2014-08-28 22:53:49 +0300

    HQ. Chart: add incoming from, to dates verification KP-4074 Exception after setting incorrect date in the filter by date
    Former-commit-id: 85bd199ba987f52512b47b3b4640d93e23521414

---
+++ src/Tests/WB.Core.SharedKernels.SurveyManagement.Tests/Factories/InterviewsStatisticsReportFactoryTests/when_creating_statistics_should__do_allow_from_date_bigger_then_to_date.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Machine.Specifications;
+using Moq;
+using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
+using WB.Core.SharedKernels.SurveyManagement.Views.Interview;
+using WB.Core.SharedKernels.SurveyManagement.Views.Interviews;
+using It = Machine.Specifications.It;
+
+namespace WB.Core.SharedKernels.SurveyManagement.Tests.Factories.InterviewsStatisticsReportFactoryTests
+{
+    internal class when_creating_statistics_should__do_allow_from_date_bigger_then_to_date : InterviewsStatisticsReportFactoryTestsContext
+    {
+        Establish context = () =>
+        {
+            var stats = Mock.Of<IQueryableReadSideRepositoryReader<StatisticsLineGroupedByDateAndTemplate>>();
+
+            var questionnaireId = Guid.NewGuid();
+            var baseDate = new DateTime(2014, 8, 22);
+            var questionnaireVersion = 1;
+
+            var data = new List<StatisticsLineGroupedByDateAndTemplate>
+            {
+                new StatisticsLineGroupedByDateAndTemplate
+                {
+                    QuestionnaireId = questionnaireId,
+                    QuestionnaireVersion = questionnaireVersion,
+                    Date = baseDate,
+                    DateTicks = baseDate.Ticks,
+                    ApprovedByHeadquartersCount = 0,
+                    ApprovedBySupervisorCount = 0,
+                    CompletedCount = 0,
+                    InterviewerAssignedCount = 0,
+                    RejectedByHeadquartersCount = 0,
+                    RejectedBySupervisorCount = 0,
+                    SupervisorAssignedCount = 0
+                }
+            }.AsQueryable();
+
+            Mock.Get(stats)
+                .Setup(
+                    _ =>
+                        _.Query(
+                            Moq.It
+                                .IsAny
+                                <Func<IQueryable<StatisticsLineGroupedByDateAndTemplate>, List<StatisticsLineGroupedByDateAndTemplate>>>()))
+                .Returns<Func<IQueryable<StatisticsLineGroupedByDateAndTemplate>, List<StatisticsLineGroupedByDateAndTemplate>>>(
+                    query => query.Invoke(data)
+                );
+
+            interviewsStatisticsReportFactory = CreateInterviewsStatisticsReportFactory(stats);
+
+            input = new InterviewsStatisticsReportInputModel
+            {
+                CurrentDate = baseDate,
+                QuestionnaireId = questionnaireId,
+                QuestionnaireVersion = questionnaireVersion,
+                From = baseDate.AddDays(-1),
+                To = baseDate.AddDays(-2)
+            };
+        };
+
+        Because of = () => view = interviewsStatisticsReportFactory.Load(input);
+
+        It should_have_days_count_muliply_two_records = () => view.Ticks.Length.ShouldEqual(0);
+
+        private static InterviewsStatisticsReportFactory interviewsStatisticsReportFactory;
+        private static InterviewsStatisticsReportInputModel input;
+        private static InterviewsStatisticsReportView view;
+
+    }
+}
