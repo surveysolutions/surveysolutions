ad0357f7790deb848f183476f9f8ddd279992b94
Dima Pasko <dimapasko@gmail.com>
2014-08-28 14:42:08 +0300

    HQ, Chart: implemented filter backend and middle tier KP-3967 Filters
    Former-commit-id: 3381b753af8cddbf1412823ac6497ed77431c924

--- src/Tests/WB.Core.SharedKernels.SurveyManagement.Tests/Factories/InterviewsStatisticsReportFactoryTests/when_creating_statistics_should_allow_date_range_with_no_data.cs
+++ src/Tests/WB.Core.SharedKernels.SurveyManagement.Tests/Factories/InterviewsStatisticsReportFactoryTests/when_creating_statistics_should_allow_date_range_with_no_data.cs
@@ -10,7 +10,7 @@ using It = Machine.Specifications.It;
 
 namespace WB.Core.SharedKernels.SurveyManagement.Tests.Factories.InterviewsStatisticsReportFactoryTests
 {
-    internal class when_creating_statistics_should_add_empty_days : InterviewsStatisticsReportFactoryTestsContext
+    internal class when_creating_statistics_should_allow_date_range_with_no_data : InterviewsStatisticsReportFactoryTestsContext
     {
         Establish context = () =>
         {
@@ -26,8 +26,8 @@ namespace WB.Core.SharedKernels.SurveyManagement.Tests.Factories.InterviewsStati
                 {
                     QuestionnaireId = questionnaireId,
                     QuestionnaireVersion = questionnaireVersion,
-                    Date = baseDate.AddDays(-2),
-                    DateTicks = baseDate.AddDays(-2).Ticks,
+                    Date = baseDate.AddDays(-3),
+                    DateTicks = baseDate.AddDays(-3).Ticks,
                     ApprovedByHeadquartersCount = 1,
                     ApprovedBySupervisorCount = 1,
                     CompletedCount = 1,
@@ -35,20 +35,6 @@ namespace WB.Core.SharedKernels.SurveyManagement.Tests.Factories.InterviewsStati
                     RejectedByHeadquartersCount = 1,
                     RejectedBySupervisorCount = 1,
                     SupervisorAssignedCount = 1
-                },
-                new StatisticsLineGroupedByDateAndTemplate
-                {
-                    QuestionnaireId = questionnaireId,
-                    QuestionnaireVersion = questionnaireVersion,
-                    Date = baseDate.AddDays(0),
-                    DateTicks = baseDate.Date.Ticks,
-                    ApprovedByHeadquartersCount = 2,
-                    ApprovedBySupervisorCount = 2,
-                    CompletedCount = 2,
-                    InterviewerAssignedCount = 2,
-                    RejectedByHeadquartersCount = 2,
-                    RejectedBySupervisorCount = 2,
-                    SupervisorAssignedCount = 2
                 }
             }.AsQueryable();
 
@@ -70,31 +56,16 @@ namespace WB.Core.SharedKernels.SurveyManagement.Tests.Factories.InterviewsStati
                 CurrentDate = baseDate,
                 QuestionnaireId = questionnaireId,
                 QuestionnaireVersion = questionnaireVersion,
-                From = baseDate.AddDays(-100),
-                To = baseDate.AddDays(100)
+                From = baseDate.AddDays(-2),
+                To = baseDate.AddDays(-1)
             };
         };
 
         Because of = () => view = interviewsStatisticsReportFactory.Load(input);
 
-        It should_have_days_count_muliply_two_records = () => view.Ticks.Length.ShouldEqual(3*2);
+        It should_have_days_count_muliply_two_records = () => view.Ticks.Length.ShouldEqual(2 * 2);
 
-        It should_have_0_rownumber_equals_1 = () => view.Ticks[0, 0].ShouldEqual("1");
-        It should_have_0_date_correct = () => view.Ticks[0, 1].ShouldEqual(new DateTime(2014, 8, 20).ToShortDateString());
-
-        It should_have_1_rownumber_equals_2 = () => view.Ticks[1, 0].ShouldEqual("2");
-        It should_have_1_date_correct = () => view.Ticks[1, 1].ShouldEqual(new DateTime(2014, 8, 21).ToShortDateString());
-
-        It should_have_2_rownumber_equals_3 = () => view.Ticks[2, 0].ShouldEqual("3");
-        It should_have_2_date_correct = () => view.Ticks[2, 1].ShouldEqual(new DateTime(2014, 8, 22).ToShortDateString());
-
-        It should_have_supervisorAssignedData_correct = () => view.Stats[0].ShouldEqual(new[] { 1, 1, 2 });
-        It should_have_interviewerAssignedData_correct = () => view.Stats[1].ShouldEqual(new[] { 1, 1, 2 });
-        It should_have_completedData_correct = () => view.Stats[2].ShouldEqual(new[] { 1, 1, 2 });
-        It should_have_rejectedBySupervisor_correct = () => view.Stats[3].ShouldEqual(new[] { 1, 1, 2 });
-        It should_have_approvedBySupervisor_correct = () => view.Stats[4].ShouldEqual(new[] { 1, 1, 2 });
-        It should_have_rejectedByHeadquarters_correct = () => view.Stats[5].ShouldEqual(new[] { 1, 1, 2 });
-        It should_have_approvedByHeadquarters_correct = () => view.Stats[6].ShouldEqual(new[] { 1, 1, 2 });
+        It should_have_supervisorAssignedData_correct = () => view.Stats[0].ShouldEqual(new[] { 1, 1 });
 
         private static InterviewsStatisticsReportFactory interviewsStatisticsReportFactory;
         private static InterviewsStatisticsReportInputModel input;
