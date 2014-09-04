b2bf89fba92b6ca38356240a0f95dfe144643dfa
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2014-09-04 13:58:00 +0300

    removed filtering animation that worked slow
    Former-commit-id: 413dd8e1088f25459f0e37a889f4a06ffbcb13f2

--- src/UI/Designer/WB.UI.Designer/UpdatedDesigner/app/scripts/directives/animations.js
+++ src/UI/Designer/WB.UI.Designer/UpdatedDesigner/app/scripts/directives/animations.js
@@ -1,67 +1,16 @@
-﻿(function() {
-    angular.module('designerApp')
+﻿angular.module('designerApp')
     .animation('.slide', function () {
         var ngHideClassName = 'ng-hide';
         return {
-            beforeAddClass: function(element, className, done) {
+            beforeAddClass: function (element, className, done) {
                 if (className === ngHideClassName) {
                     jQuery(element).slideUp(done);
                 }
             },
-            removeClass: function(element, className, done) {
+            removeClass: function (element, className, done) {
                 if (className === ngHideClassName) {
                     jQuery(element).hide().slideDown(done);
                 }
             }
         };
-    })
-    .animation('.filter-animate', function () {
-        return {
-            enter: function(element, done) {
-                element.css('opacity', 0);
-                jQuery(element).animate({
-                    opacity: 1
-                }, done);
-
-                // optional onDone or onCancel callback
-                // function to handle any post-animation
-                // cleanup operations
-                return function(isCancelled) {
-                    if (isCancelled) {
-                        jQuery(element).stop();
-                    }
-                };
-            },
-            leave: function(element, done) {
-                element.css('opacity', 1);
-                jQuery(element).animate({
-                    opacity: 0
-                }, done);
-
-                // optional onDone or onCancel callback
-                // function to handle any post-animation
-                // cleanup operations
-                return function(isCancelled) {
-                    if (isCancelled) {
-                        jQuery(element).stop();
-                    }
-                };
-            },
-            move: function(element, done) {
-                element.css('opacity', 0);
-                jQuery(element).animate({
-                    opacity: 1
-                }, done);
-
-                // optional onDone or onCancel callback
-                // function to handle any post-animation
-                // cleanup operations
-                return function(isCancelled) {
-                    if (isCancelled) {
-                        jQuery(element).stop();
-                    }
-                };
-            }
-        };
     });
-})();
