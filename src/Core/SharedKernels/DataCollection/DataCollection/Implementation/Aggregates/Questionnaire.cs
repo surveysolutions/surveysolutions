61684c0cb093f5a4bdd3904100439602ad2fdb18
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2014-09-05 14:12:16 -0400

    DataCollection\Questionanire: code review KP-4109
    --HG--
    branch : release-candidate
    
    
    Former-commit-id: e7cd40916748f5e45d40aed4c55b607d40feee99

--- src/Core/SharedKernels/DataCollection/DataCollection/Implementation/Aggregates/Questionnaire.cs
+++ src/Core/SharedKernels/DataCollection/DataCollection/Implementation/Aggregates/Questionnaire.cs
@@ -30,26 +30,10 @@ namespace WB.Core.SharedKernels.DataCollection.Implementation.Aggregates
         private bool isProxyToPlainQuestionnaireRepository;
         private Dictionary<long, IQuestionnaire> availableVersions = new Dictionary <long, IQuestionnaire>();
 
-        public IQuestionnaire GetQuestionnaire()
-        {
-            var presentVersions = availableVersions.Where(v => v.Value != null).ToList();
-            if (!presentVersions.Any())
-                return null;
-            var maxVersion = presentVersions.Max(k => k.Key);
-            return presentVersions.FirstOrDefault(v => v.Key == maxVersion).Value;
-        }
-
-        public IQuestionnaire GetHistoricalQuestionnaire(long version)
-        {
-            if (availableVersions.ContainsKey(version))
-                return availableVersions[version];
-            return null;
-        }
-
         protected internal void Apply(TemplateImported e)
         {
-            var templateVersion = e.Version ?? (this.Version+1);
-            availableVersions[templateVersion]= new PlainQuestionnaire(e.Source, () => templateVersion);
+            var templateVersion = e.Version ?? (this.Version + 1);
+            availableVersions[templateVersion] = new PlainQuestionnaire(e.Source, () => templateVersion);
         }
 
         protected internal void Apply(QuestionnaireDeleted e)
@@ -61,22 +45,6 @@ namespace WB.Core.SharedKernels.DataCollection.Implementation.Aggregates
         {
             this.isProxyToPlainQuestionnaireRepository = true;
             availableVersions[e.Version] = null;
-        }
-
-        public QuestionnaireState CreateSnapshot()
-        {
-            return this.isProxyToPlainQuestionnaireRepository
-                ? new QuestionnaireState(
-                    isProxyToPlainQuestionnaireRepository: true, availableVersions: this.availableVersions)
-                : new QuestionnaireState(
-                    isProxyToPlainQuestionnaireRepository: false, availableVersions: this.availableVersions);
-        }
-
-        public void RestoreFromSnapshot(QuestionnaireState snapshot)
-        {
-            this.isProxyToPlainQuestionnaireRepository = snapshot.IsProxyToPlainQuestionnaireRepository;
-
-            this.availableVersions = snapshot.AvailableVersions;
         }
 
         #endregion
@@ -95,8 +63,7 @@ namespace WB.Core.SharedKernels.DataCollection.Implementation.Aggregates
 
         #endregion
 
-
-        public Questionnaire() {}
+        public Questionnaire() { }
 
         public Questionnaire(Guid createdBy, IQuestionnaireDocument source, bool allowCensusMode)
             : base(source.PublicKey)
@@ -116,13 +83,36 @@ namespace WB.Core.SharedKernels.DataCollection.Implementation.Aggregates
             this.RegisterPlainQuestionnaire(id, version, allowCensusMode);
         }
 
-        private void ImportFromQuestionnaireDocument(IQuestionnaireDocument source)
+        public IQuestionnaire GetQuestionnaire()
         {
-            QuestionnaireDocument document = CastToQuestionnaireDocumentOrThrow(source);
-            this.ThrowIfCurrentAggregateIsUsedOnlyAsProxyToPlainQuestionnaireRepository();
+            var presentVersions = availableVersions.Where(v => v.Value != null).ToList();
+            if (!presentVersions.Any())
+                return null;
+            var maxVersion = presentVersions.Max(k => k.Key);
+            return presentVersions.FirstOrDefault(v => v.Key == maxVersion).Value;
+        }
 
+        public IQuestionnaire GetHistoricalQuestionnaire(long version)
+        {
+            if (availableVersions.ContainsKey(version))
+                return availableVersions[version];
+            return null;
+        }
 
-            this.ApplyEvent(new TemplateImported { Source = document, Version = GetNextVersion() });
+        public QuestionnaireState CreateSnapshot()
+        {
+            return this.isProxyToPlainQuestionnaireRepository
+                ? new QuestionnaireState(
+                    isProxyToPlainQuestionnaireRepository: true, availableVersions: this.availableVersions)
+                : new QuestionnaireState(
+                    isProxyToPlainQuestionnaireRepository: false, availableVersions: this.availableVersions);
+        }
+
+        public void RestoreFromSnapshot(QuestionnaireState snapshot)
+        {
+            this.isProxyToPlainQuestionnaireRepository = snapshot.IsProxyToPlainQuestionnaireRepository;
+
+            this.availableVersions = snapshot.AvailableVersions;
         }
 
         public void ImportFromDesigner(Guid createdBy, IQuestionnaireDocument source, bool allowCensusMode)
@@ -133,13 +123,6 @@ namespace WB.Core.SharedKernels.DataCollection.Implementation.Aggregates
 
 
             this.ApplyEvent(new TemplateImported { Source = document, AllowCensusMode = allowCensusMode, Version = GetNextVersion() });
-        }
-
-        private long GetNextVersion()
-        {
-            if (!availableVersions.Any())
-                return 1;
-            return this.availableVersions.Keys.Max() + 1;
         }
 
         public void ImportFromSupervisor(IQuestionnaireDocument source)
@@ -182,6 +165,22 @@ namespace WB.Core.SharedKernels.DataCollection.Implementation.Aggregates
                     source.GetType()));
 
             return document;
+        }
+
+        private void ImportFromQuestionnaireDocument(IQuestionnaireDocument source)
+        {
+            QuestionnaireDocument document = CastToQuestionnaireDocumentOrThrow(source);
+            this.ThrowIfCurrentAggregateIsUsedOnlyAsProxyToPlainQuestionnaireRepository();
+
+
+            this.ApplyEvent(new TemplateImported { Source = document, Version = GetNextVersion() });
+        }
+
+        private long GetNextVersion()
+        {
+            if (!availableVersions.Any())
+                return 1;
+            return this.availableVersions.Keys.Max() + 1;
         }
 
         private static void ThrowIfVerifierFindsErrors(QuestionnaireDocument document)
