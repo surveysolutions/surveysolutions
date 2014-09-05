100b57b3fad065d0bf1fd8b4b46ce4c1f4175457
Dima Pasko <dimapasko@gmail.com>
2014-09-05 13:11:11 +0300

    HQ. Chart: disable future dates on datapicvker control KP-4100 Deny setting future date in "From" and "To" parameter of the filter
    Former-commit-id: 2816d84e59a14bb8fc40c2bec6368f66bf1e6b93

--- src/Core/SharedKernels/SurveyManagement/SurveyManagement.Web/Scripts/viewmodels/pages/chart.js
+++ src/Core/SharedKernels/SurveyManagement/SurveyManagement.Web/Scripts/viewmodels/pages/chart.js
@@ -97,6 +97,7 @@
             keyboardNavigation: false,
             autoclose: true,
             todayHighlight: true,
+            endDate: '+0d',
             forseParse: false
         }).on("hide", function (e) {
             if (e.date !== undefined) {
