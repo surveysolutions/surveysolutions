f52d54349db130c37654556b00cfe8d76afbaf5c
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2014-09-09 16:28:30 +0300

    ChartStatisticsViewFactory: corrected date formatting and corresponding tests KP-4097
    --HG--
    branch : release-candidate
    
    
    Former-commit-id: bae88bebbb785de6d5fde705dea8700907aa3cee

--- src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/ChartStatisticsViewFactoryTests/when_building_view_from_statistics_which_has_3_days_with_incremental_data_for_each_count.cs
+++ src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/ChartStatisticsViewFactoryTests/when_building_view_from_statistics_which_has_3_days_with_incremental_data_for_each_count.cs
@@ -82,13 +82,13 @@ namespace WB.Tests.Unit.SharedKernels.SurveyManagement.ChartStatisticsViewFactor
             view.Lines.Length.ShouldEqual(7);
 
         It should_set_1st_point_horizontal_coord_of_all_lines_equal_to_2014_08_20 = () =>
-            view.Lines.ShouldEachConformTo(line => (string)line[0][0] == "08-20-2014");
+            view.Lines.ShouldEachConformTo(line => (string)line[0][0] == "08/20/2014");
 
         It should_set_2nd_point_horizontal_coord_of_all_lines_equal_to_2014_08_21 = () =>
-            view.Lines.ShouldEachConformTo(line => (string)line[1][0] == "08-21-2014");
+            view.Lines.ShouldEachConformTo(line => (string)line[1][0] == "08/21/2014");
 
         It should_set_3rd_point_horizontal_coord_of_all_lines_equal_to_2014_08_22 = () =>
-            view.Lines.ShouldEachConformTo(line => (string)line[2][0] == "08-22-2014");
+            view.Lines.ShouldEachConformTo(line => (string)line[2][0] == "08/22/2014");
 
         It should_set_1st_point_vertical_size_of_all_lines_equal_to_1 = () =>
             view.Lines.ShouldEachConformTo(line => (int) line[0][1] == 1);
