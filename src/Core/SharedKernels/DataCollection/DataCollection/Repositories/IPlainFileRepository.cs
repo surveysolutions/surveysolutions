9a2168254ca1662b1cdbf30dd2653bdc0ade568a
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2014-09-12 13:57:22 -0400

    Multimedia:  add PlainFileRepository KP-4186
    --HG--
    branch : multimedia-question
    
    
    Former-commit-id: 4dbe782c08e157610a47050bebddde38ab632877

---
+++ src/Core/SharedKernels/DataCollection/DataCollection/Repositories/IPlainFileRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WB.Core.SharedKernels.DataCollection.Repositories
+{
+    public interface IPlainFileRepository
+    {
+        byte[] GetInterviewBinaryData(Guid interviewId, string fileName);
+        string[] GetAllIdsOfBinaryDataByInterview(Guid interviewId);
+        void StoreInterviewBinaryData(Guid interviewId, string fileName, byte[] data);
+        void RemoveInterviewBinaryData(Guid interviewId, string fileName);
+        void RemoveAllBinaryDataForInterview(Guid interviewId);
+    }
+}
