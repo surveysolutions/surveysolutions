7aff3f674e6c3174ac95dee9da7ff4ea40ab3863
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2014-09-17 15:16:31 -0400

    SurveyManagment: cover file synchronization with tests KP-4187
    Former-commit-id: a8ab96d6fd750efef5293e2ffa73a49fc132fc0d

---
+++ src/Tests/WB.UI.Supervisor.Tests/SyncControllerTests/when_pushing_file_which_rise_exception_during_saving.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Machine.Specifications;
+using Main.Core.View;
+using Main.Core.View.User;
+using Moq;
+using WB.Core.SharedKernels.DataCollection.Repositories;
+using WB.Core.SharedKernels.SurveyManagement.Web.Controllers;
+using It = Machine.Specifications.It;
+
+namespace WB.UI.Supervisor.Tests.SyncControllerTests
+{
+    internal class when_pushing_file_which_rise_exception_during_saving : SyncControllerTestContext
+    {
+        Establish context = () =>
+        {
+            var user = new UserView();
+            var userFactory = Mock.Of<IViewFactory<UserViewInputModel, UserView>>(x => x.Load(Moq.It.IsAny<UserViewInputModel>()) == user);
+            plainFileRepository = new Mock<IPlainFileRepository>();
+            plainFileRepository.Setup(x => x.StoreInterviewBinaryData(iterviewId, fileName, Moq.It.IsAny<byte[]>()))
+                .Throws<ArgumentException>();
+            controller = CreateSyncControllerWithFile(viewFactory: userFactory, stream: new MemoryStream(), plainFileRepository: plainFileRepository.Object, fileName: fileName);
+        };
+
+        Because of = () =>
+            result = (JsonResult)controller.PostFile("login", "password", iterviewId);
+
+        It should_return_false_result = () =>
+            ((bool)result.Data).ShouldEqual(false);
+
+        private static SyncController controller;
+        private static JsonResult result;
+        private static Mock<IPlainFileRepository> plainFileRepository;
+        private static Guid iterviewId = Guid.NewGuid();
+        private static string fileName = "file name";
+    }
+}
