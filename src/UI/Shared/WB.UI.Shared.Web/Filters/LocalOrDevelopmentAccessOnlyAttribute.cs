39c18825bbbe58eebc25288a0aff8b01ef846a4e
Roman Lytvyn <litvin.roma@gmail.com>
2014-09-17 16:02:18 +0300

    ui web: added action filter. KP-4217
    Former-commit-id: 0ca16c30170a52b22c374b3593d7f20469f64570

---
+++ src/UI/Shared/WB.UI.Shared.Web/Filters/LocalOrDevelopmentAccessOnlyAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using WB.UI.Shared.Web.Settings;
+
+namespace WB.UI.Shared.Web.Filters
+{
+    public class LocalOrDevelopmentAccessOnlyAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (!filterContext.HttpContext.Request.IsLocal && !CoreSettings.IsUnderDevelopment)
+            {
+                throw new HttpException(404, "controller is missing with current web site configuration");
+            }
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
