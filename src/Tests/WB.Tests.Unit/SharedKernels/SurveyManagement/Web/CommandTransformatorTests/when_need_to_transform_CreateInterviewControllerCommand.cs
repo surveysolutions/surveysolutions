6228886a40804bfc602ddc35c46213ced02de048
Roman Lytvyn <none@none>
2014-09-19 11:51:44 +0300

    tests: test for command transformator. KP-4176
    Former-commit-id: ff479d557e2b529d27b82a83cae8a073206dfb93

---
+++ src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/Web/CommandTransformatorTests/when_need_to_transform_CreateInterviewControllerCommand.cs
@@ -0,0 +1,25 @@
+﻿using Machine.Specifications;
+using Ncqrs.Commanding;
+using WB.Core.SharedKernels.DataCollection.Commands.Interview;
+using WB.Core.SharedKernels.SurveyManagement.Web.Code.CommandTransformation;
+using It = Machine.Specifications.It;
+
+namespace WB.Tests.Unit.SharedKernels.SurveyManagement.Web.CommandTransformatorTests
+{
+    internal class when_need_to_transform_CreateInterviewControllerCommand: CommandTransformatorTestsContext
+    {
+        Establish context = () =>
+        {
+            commandTransformator = CreateCommandTransformator();
+        };
+
+        Because of = () =>
+            command = commandTransformator.TransformCommnadIfNeeded(command);
+
+        It should_command_be_type_of_CreateInterviewCommand = () =>
+            command.ShouldBeOfExactType<CreateInterviewCommand>();
+
+        private static CommandTransformator commandTransformator;
+        private static ICommand command = Create.CreateInterviewControllerCommand();
+    }
+}
