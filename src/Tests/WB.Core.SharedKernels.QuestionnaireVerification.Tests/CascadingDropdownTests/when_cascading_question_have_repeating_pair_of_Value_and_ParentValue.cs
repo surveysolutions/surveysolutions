40c9a7dd3b5f58b464b90a5c1013ac63944ca554
Andrii Kozhyn <hedgic@gmail.com>
2014-09-24 13:20:31 +0300

    KP-4232: added checks for amout of answers and uniquness of answers for cascading categorical questions
    Former-commit-id: b4bcd4d0b6fc6840aec8b404c647d292dacaa344

---
+++ src/Tests/WB.Core.SharedKernels.QuestionnaireVerification.Tests/CascadingDropdownTests/when_cascading_question_have_repeating_pair_of_Value_and_ParentValue.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Machine.Specifications;
+using Main.Core.Documents;
+using Main.Core.Entities.SubEntities;
+using Main.Core.Entities.SubEntities.Question;
+using WB.Core.SharedKernels.QuestionnaireVerification.Implementation.Services;
+using WB.Core.SharedKernels.QuestionnaireVerification.Tests.QuestionnaireVerifierTests;
+using WB.Core.SharedKernels.QuestionnaireVerification.ValueObjects;
+
+namespace WB.Core.SharedKernels.QuestionnaireVerification.Tests.CascadingDropdownTests
+{
+    internal class when_cascading_question_have_repeating_pair_of_Value_and_ParentValue : QuestionnaireVerifierTestsContext
+    {
+        Establish context = () =>
+        {
+            parentSingleOptionQuestionId = Guid.Parse("9E96D4AB-DF91-4FC9-9585-23FA270B25D7");
+            childCascadedComboboxId = Guid.Parse("C6CC807A-3E81-406C-A110-1044AE3FD89B");
+
+            questionnaire = CreateQuestionnaireDocumentWithOneChapter(new SingleQuestion
+            {
+                PublicKey = parentSingleOptionQuestionId,
+                StataExportCaption = "var",
+                QuestionType = QuestionType.SingleOption,
+                Answers = new List<Answer> {
+                            new Answer { AnswerText = "one", AnswerValue = "1", PublicKey = Guid.NewGuid() },
+                            new Answer { AnswerText = "two", AnswerValue = "2", PublicKey = Guid.NewGuid() }
+                        }
+            },
+                    new SingleQuestion
+                    {
+                        PublicKey = childCascadedComboboxId,
+                        QuestionType = QuestionType.SingleOption,
+                        StataExportCaption = "var1",
+                        CascadeFromQuestionId = parentSingleOptionQuestionId,
+                        Answers = new List<Answer> {
+                            new Answer { AnswerText = "child 1", AnswerValue = "1", PublicKey = Guid.NewGuid(), ParentValue = "1" },
+                            new Answer { AnswerText = "child 1", AnswerValue = "1", PublicKey = Guid.NewGuid(), ParentValue = "2" },
+                            new Answer { AnswerText = "child 2", AnswerValue = "2", PublicKey = Guid.NewGuid(), ParentValue = "2" },
+                        }
+                    }
+                );
+            verifier = CreateQuestionnaireVerifier();
+        };
+
+        Because of = () => verificationErrors = verifier.Verify(questionnaire);
+
+        It should_return_WB0089_error = () => verificationErrors.First().Code.ShouldEqual("WB0089");
+
+        static Guid parentSingleOptionQuestionId;
+        static Guid childCascadedComboboxId;
+        static QuestionnaireDocument questionnaire;
+        static QuestionnaireVerifier verifier;
+        static IEnumerable<QuestionnaireVerificationError> verificationErrors;
+    }
+}
+
