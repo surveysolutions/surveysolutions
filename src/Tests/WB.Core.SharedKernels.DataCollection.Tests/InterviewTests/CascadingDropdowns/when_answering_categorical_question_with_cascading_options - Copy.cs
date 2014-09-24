b78939dc6a02b7a8e2c0859df279791c96e96f05
Dima Pasko <dimapasko@gmail.com>
2014-09-24 15:33:51 +0300

    Core: add unit tests to test verification for answer parent consistency KP-4259 Interview: add validations, that answered cascading questions is consistent with it's parent
    Former-commit-id: d5568d7e754a26eb8542bb4dd830df0c10ac1c5b

---
+++ src/Tests/WB.Core.SharedKernels.DataCollection.Tests/InterviewTests/CascadingDropdowns/when_answering_categorical_question_with_cascading_options - Copy.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using Machine.Specifications;
+using Main.Core.Entities.SubEntities;
+using Main.Core.Entities.SubEntities.Question;
+using Microsoft.Practices.ServiceLocation;
+using Moq;
+using Ncqrs.Spec;
+using WB.Core.SharedKernels.DataCollection.Exceptions;
+using WB.Core.SharedKernels.DataCollection.Implementation.Aggregates;
+using WB.Core.SharedKernels.DataCollection.Repositories;
+using It = Machine.Specifications.It;
+
+namespace WB.Core.SharedKernels.DataCollection.Tests.InterviewTests.CascadingDropdowns
+{
+    [Subject(typeof(Interview))]
+    internal class when_answering_categorical_question_with_cascading_options_1111 : InterviewTestsContext
+    {
+        private Establish context = () =>
+        {
+            parentSingleOptionQuestionId = Guid.Parse("9E96D4AB-DF91-4FC9-9585-23FA270B25D7");
+            childCascadedComboboxId = Guid.Parse("C6CC807A-3E81-406C-A110-1044AE3FD89B");
+
+            var questionnaireId = Guid.NewGuid();
+            comboShouldNotBeRemoved = Guid.NewGuid();
+            actorId = Guid.NewGuid();
+
+            var questionnaire = Create.Questionnaire(actorId,
+                CreateQuestionnaireDocumentWithOneChapter(
+                    new SingleQuestion
+                    {
+                        PublicKey = parentSingleOptionQuestionId,
+                        QuestionType = QuestionType.SingleOption,
+                        Answers = new List<Answer>
+                                        {
+                                            new Answer { AnswerText = "one", AnswerValue = "1", PublicKey = Guid.NewGuid() },
+                                            new Answer { AnswerText = "two", AnswerValue = "2", PublicKey = Guid.NewGuid() }
+                                        }
+                    },
+                    new SingleQuestion
+                    {
+                        PublicKey = childCascadedComboboxId,
+                        QuestionType = QuestionType.SingleOption,
+                        CascadeFromQuestionId = parentSingleOptionQuestionId,
+                        Answers = new List<Answer>
+                        {
+                            new Answer { AnswerText = "child 1", AnswerValue = "1", PublicKey = Guid.NewGuid(), ParentValue = "3" }
+                        }
+                    }));
+
+            var questionnaireRepository = CreateQuestionnaireRepositoryStubWithOneQuestionnaire(questionnaireId, questionnaire.GetQuestionnaire());
+
+            Mock.Get(ServiceLocator.Current)
+                .Setup(locator => locator.GetInstance<IQuestionnaireRepository>())
+                .Returns(questionnaireRepository);
+
+            interview = CreateInterview(questionnaireId: questionnaireId);
+
+            interview.AnswerSingleOptionQuestion(actorId, parentSingleOptionQuestionId, new decimal[] { }, DateTime.Now, 1);
+            
+            eventContext = new EventContext();
+        };
+
+        Because of = () =>
+            exception = Catch.Exception(() =>
+                interview.AnswerSingleOptionQuestion(actorId, childCascadedComboboxId, new decimal[] { }, DateTime.Now, 1)
+            );
+
+        It should_throw_InterviewException = () =>
+            exception.ShouldBeOfExactType<InterviewException>();
+
+        private static Exception exception;
+        private static Interview interview;
+        private static EventContext eventContext;
+        private static Guid parentSingleOptionQuestionId;
+        private static Guid childCascadedComboboxId;
+        private static Guid comboShouldNotBeRemoved;
+        private static Guid actorId;
+    }
+}
+
