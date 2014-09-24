a4a17da675d71b8c7871d801d677bac98902dddf
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2014-09-24 16:54:35 -0400

    HQ\UI: code review KP-4197
    Former-commit-id: 79fe0f5e4c1136707beb62dbee3c1209bab098d4

---
+++ src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/ResourceControllerTests/ResourceControllerTestContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Machine.Specifications;
+using Moq;
+using Ncqrs.Commanding.ServiceModel;
+using WB.Core.GenericSubdomains.Logging;
+using WB.Core.SharedKernels.DataCollection.Repositories;
+using WB.Core.SharedKernels.SurveyManagement.Web.Controllers;
+using WB.Core.SharedKernels.SurveyManagement.Web.Utils.Membership;
+
+namespace WB.Tests.Unit.SharedKernels.SurveyManagement.ResourceControllerTests
+{
+    [Subject(typeof(ResourceController))]
+    internal class ResourceControllerTestContext
+    {
+        protected static ResourceController CreateController(ICommandService commandService = null,
+            IGlobalInfoProvider globalInfoProvider = null,
+            ILogger logger = null, IPlainInterviewFileStorage plainInterviewFileStorage = null)
+        {
+            return new ResourceController(
+                commandService ?? Mock.Of<ICommandService>(),
+                globalInfoProvider ?? Mock.Of<IGlobalInfoProvider>(),
+                logger ?? Mock.Of<ILogger>(), plainInterviewFileStorage ?? Mock.Of<IPlainInterviewFileStorage>());
+        }
+    }
+}
