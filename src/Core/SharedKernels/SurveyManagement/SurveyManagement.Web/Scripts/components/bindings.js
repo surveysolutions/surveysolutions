fc9580202983e4bde2fb6c697d0d785c98f6a421
Roman Lytvyn <none@none>
2014-09-26 15:50:05 +0300

    hq: questionnaire page changed. KP-4228
    Former-commit-id: 5242f7b1e5556a64d9198553ffc550b1f7a14120

--- src/Core/SharedKernels/SurveyManagement/SurveyManagement.Web/Scripts/components/bindings.js
+++ src/Core/SharedKernels/SurveyManagement/SurveyManagement.Web/Scripts/components/bindings.js
@@ -9,4 +9,62 @@
             ko.applyBindingsToNode(element, { event: { keyup: wrapper } }, context);
         }
     };
+
+    ko.bindingHandlers.sortby = {
+        init: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+            var listView = bindingContext.$root;
+
+            var value = valueAccessor();
+
+            var sort = "sorting";
+            var sortAsc = "sorting-up";
+            var sortDesc = "sorting-down";
+
+            var refreshUI = function () {
+
+                $(element).addClass(sort);
+
+                $(element).removeClass(sortAsc);
+                $(element).removeClass(sortDesc);
+
+                if (listView.SortOrder() == value) {
+                    if (listView.SortDirection()) {
+                        $(element).addClass(sortAsc);
+                    } else {
+                        $(element).addClass(sortDesc);
+                    }
+                }
+            };
+
+            listView.SortOrder.subscribe(refreshUI);
+            listView.SortDirection.subscribe(refreshUI);
+
+            $(element).click(function () {
+                listView.sort(value);
+            });
+
+            refreshUI();
+        }
+    };
+
+    ko.bindingHandlers.date = {
+        update: function (element, valueAccessor, allBindingsAccessor, viewModel) {
+            var value = valueAccessor();
+            var allBindings = allBindingsAccessor();
+            var valueUnwrapped = ko.utils.unwrapObservable(value);
+
+            var pattern = allBindings.format || 'MM/DD/YYYY';
+
+            var output = "-";
+            if (valueUnwrapped !== null && valueUnwrapped !== undefined && valueUnwrapped.length > 0) {
+                output = moment(valueUnwrapped).format(pattern);
+            }
+
+            if ($(element).is("input") === true) {
+                $(element).val(output);
+            } else {
+                $(element).text(output);
+            }
+        }
+    };
 })(ko);
