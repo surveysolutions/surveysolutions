7814b1f3b48c39f9ce227c4bac46555810590730
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2014-09-29 23:56:17 -0400

    tests added [KP-3705]
    --HG--
    branch : csharp-business-language
    
    
    Former-commit-id: 4edca597cddb2ae849b85e93cc2237821920c8a6

---
+++ src/Tests/WB.Core.SharedKernels.DataCollection.Tests/QuestionnaireAssemblyFileAccessorTests/when_getting_assembly_as_byte_array.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Machine.Specifications;
+using Moq;
+using WB.Core.Infrastructure.FileSystem;
+using WB.Core.SharedKernels.DataCollection.Accessors;
+using It = Machine.Specifications.It;
+
+namespace WB.Core.SharedKernels.DataCollection.Tests.QuestionnaireAssemblyFileAccessorTests
+{
+    internal class when_getting_assembly_as_byte_array : QuestionnaireAssemblyFileAccessorTestsContext
+    {
+        Establish context = () =>
+        {
+            FileSystemAccessorMock.Setup(x => x.ReadAllBytes(Moq.It.IsAny<string>())).Returns(data1);
+            questionnaireAssemblyFileAccessor = CreateQuestionnaireAssemblyFileAccessor(fileSystemAccessor: FileSystemAccessorMock.Object);
+        };
+
+        Because of = () => result = questionnaireAssemblyFileAccessor.GetAssemblyAsByteArray(questionnaireId, version);
+
+        It should_data_of_returned_file_be_equal_to_data1 = () =>
+            result.ShouldEqual(data1);
+
+        private static QuestionnaireAssemblyFileAccessor questionnaireAssemblyFileAccessor;
+        private static readonly Mock<IFileSystemAccessor> FileSystemAccessorMock = CreateIFileSystemAccessorMock();
+        private static Guid questionnaireId = Guid.Parse("33332222111100000000111122223333");
+        private static long version = 3;
+
+        private static byte[] data1 = new byte[] { 1 };
+
+        private static byte[] result;
+    }
+}
---
+++ src/Tests/WB.Core.SharedKernels.DataCollection.Tests/QuestionnaireAssemblyFileAccessorTests/when_getting_full_path_to_assembly.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Machine.Specifications;
+using Moq;
+using WB.Core.GenericSubdomains.Utils;
+using WB.Core.Infrastructure.FileSystem;
+using WB.Core.SharedKernels.DataCollection.Accessors;
+using It = Machine.Specifications.It;
+
+namespace WB.Core.SharedKernels.DataCollection.Tests.QuestionnaireAssemblyFileAccessorTests
+{
+    internal class when_getting_full_path_to_assembly : QuestionnaireAssemblyFileAccessorTestsContext
+    {
+        Establish context = () =>
+        {
+            questionnaireAssemblyFileAccessor = CreateQuestionnaireAssemblyFileAccessor(fileSystemAccessor: FileSystemAccessorMock.Object);
+        };
+
+        Because of = () => questionnaireAssemblyFileAccessor.GetFullPathToAssembly(questionnaireId, version);
+
+        It should_combine_path_called = () =>
+            FileSystemAccessorMock.Verify(x => x.CombinePath(Moq.It.IsAny<string>(), Moq.It.Is<string>(name => name.Contains(questionnaireId.ToString()))), Times.Once);
+
+        private static QuestionnaireAssemblyFileAccessor questionnaireAssemblyFileAccessor;
+        private static readonly Mock<IFileSystemAccessor> FileSystemAccessorMock = CreateIFileSystemAccessorMock();
+        private static Guid questionnaireId = Guid.Parse("33332222111100000000111122223333");
+        private static long version = 3;
+
+    }
+}
