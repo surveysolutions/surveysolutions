8070d5e2eb10c578ced4d87a8544d3a946d09efe
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2014-09-29 23:56:17 -0400

    tests added [KP-3705]
    --HG--
    branch : csharp-business-language
    
    
    Former-commit-id: 4edca597cddb2ae849b85e93cc2237821920c8a6

---
+++ src/Tests/WB.Core.SharedKernels.DataCollection.Tests/QuestionnaireAssemblyFileAccessorTests/when_getting_assembly_as_base_64_string.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Machine.Specifications;
+using Moq;
+using WB.Core.Infrastructure.FileSystem;
+using WB.Core.SharedKernels.DataCollection.Accessors;
+using It = Machine.Specifications.It;
+
+namespace WB.Core.SharedKernels.DataCollection.Tests.QuestionnaireAssemblyFileAccessorTests
+{
+    internal class when_getting_assembly_as_base_64_string : QuestionnaireAssemblyFileAccessorTestsContext
+    {
+        Establish context = () =>
+        {
+            FileSystemAccessorMock.Setup(x => x.ReadAllBytes(Moq.It.IsAny<string>())).Returns(data1);
+            questionnaireAssemblyFileAccessor = CreateQuestionnaireAssemblyFileAccessor(fileSystemAccessor: FileSystemAccessorMock.Object);
+        };
+
+        Because of = () => result = questionnaireAssemblyFileAccessor.GetAssemblyAsBase64String(questionnaireId, version);
+
+        It should_data_of_returned_file_be_equal_to_expected = () =>
+            result.ShouldEqual(expected);
+
+        private static QuestionnaireAssemblyFileAccessor questionnaireAssemblyFileAccessor;
+        private static readonly Mock<IFileSystemAccessor> FileSystemAccessorMock = CreateIFileSystemAccessorMock();
+        private static Guid questionnaireId = Guid.Parse("33332222111100000000111122223333");
+        private static long version = 3;
+
+        private static byte[] data1 = new byte[] { 1 };
+        private static string expected = Convert.ToBase64String(data1);
+
+        private static string result;
+    }
+}
