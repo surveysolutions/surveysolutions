b3fe954ed61a352dcbbcc258c020a3de2bfd3725
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2014-09-30 11:03:45 +0300

    KP-4177: added sorting
    Former-commit-id: 91d67eb353243672fb6877cb859b6139087ac2a2

--- src/Core/SharedKernels/SurveyManagement/SurveyManagement.Web/Models/DocumentFilter.cs
+++ src/Core/SharedKernels/SurveyManagement/SurveyManagement.Web/Models/DocumentFilter.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Ajax.Utilities;
 using WB.Core.SharedKernels.SurveyManagement.Views.Reposts.Views;
 using WB.Core.SharedKernels.SurveyManagement.Views.Survey;
 
@@ -6,9 +8,36 @@ namespace WB.Core.SharedKernels.SurveyManagement.Web.Models
 {
     public class DocumentFilter
     {
-        public IEnumerable<UsersViewItem> Responsibles { get; set; }
-        public IEnumerable<TemplateViewItem> Templates { get; set; }
-        public IEnumerable<SurveyStatusViewItem> Statuses { get; set; }
-        public IEnumerable<UsersViewItem> Users { get; set; }
+        private IEnumerable<UsersViewItem> responsibles;
+        private IEnumerable<SurveyStatusViewItem> statuses;
+        private IEnumerable<UsersViewItem> users;
+        private IEnumerable<TemplateViewItem> templates;
+
+        public IEnumerable<UsersViewItem> Responsibles
+        {
+            get { return this.responsibles; }
+            set { this.responsibles = value; }
+        }
+
+        public IEnumerable<TemplateViewItem> Templates
+        {
+            get
+            {
+                return this.templates.OrderBy(x => x.TemplateName).ThenBy(x => x.TemplateVersion);
+            }
+            set { this.templates = value; }
+        }
+
+        public IEnumerable<SurveyStatusViewItem> Statuses
+        {
+            get { return this.statuses.OrderBy(x => x.StatusName); }
+            set { this.statuses = value; }
+        }
+
+        public IEnumerable<UsersViewItem> Users
+        {
+            get { return this.users.OrderBy(x => x.UserName); }
+            set { this.users = value; }
+        }
     }
 }
