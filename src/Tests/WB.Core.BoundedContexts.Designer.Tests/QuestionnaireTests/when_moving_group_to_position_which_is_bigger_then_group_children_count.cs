54a5665e8ebeed95929b3848cc072ec3d787610d
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2014-09-30 11:16:18 -0400

    Designer\Core: fix KP-4296
    Former-commit-id: a5e7c5322a2a24ccc0e2512cd9f1967ca20865de

---
+++ src/Tests/WB.Core.BoundedContexts.Designer.Tests/QuestionnaireTests/when_moving_group_to_position_which_is_bigger_then_group_children_count.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Machine.Specifications;
+using Main.Core.Events.Questionnaire;
+using Ncqrs.Spec;
+using WB.Core.BoundedContexts.Designer.Aggregates;
+using WB.Core.BoundedContexts.Designer.Exceptions;
+
+namespace WB.Core.BoundedContexts.Designer.Tests.QuestionnaireTests
+{
+    internal class when_moving_group_to_position_which_is_bigger_then_group_children_count : QuestionnaireTestsContext
+    {
+        Establish context = () =>
+        {
+            responsibleId = Guid.Parse("DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD");
+            chapterId = Guid.Parse("CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC");
+            groupId = Guid.Parse("21111111111111111111111111111111");
+            parentGroupId = Guid.Parse("33333333333333333333333333333333");
+
+            questionnaire = CreateQuestionnaire(responsibleId: responsibleId);
+            questionnaire.Apply(new NewGroupAdded { PublicKey = chapterId });
+            questionnaire.Apply(new NewGroupAdded { PublicKey = parentGroupId, ParentGroupPublicKey = chapterId });
+            questionnaire.Apply(new NewGroupAdded { PublicKey = groupId, ParentGroupPublicKey = parentGroupId });
+
+            eventContext = new EventContext();
+        };
+
+        Because of = () => exception = Catch.Exception(() => questionnaire.MoveGroup(groupId, chapterId, 2, responsibleId));
+
+        Cleanup stuff = () =>
+        {
+            eventContext.Dispose();
+            eventContext = null;
+        };
+
+        It should_throw_QuestionnaireException = () =>
+            exception.ShouldBeOfExactType<QuestionnaireException>();
+
+        It should_throw_exception_with_message_containting__move____group____position____acceptable__ = () =>
+          new[] { "move", "group", "position", "acceptable" }.ShouldEachConformTo(
+          keyword => exception.Message.ToLower().Contains(keyword));
+
+
+        private static EventContext eventContext;
+        private static Questionnaire questionnaire;
+        private static Guid chapterId;
+        private static Guid parentGroupId;
+        private static Guid groupId;
+        private static Guid responsibleId;
+        private static Exception exception;
+    }
+}
