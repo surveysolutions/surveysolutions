91cfd1643a1457980cc1f51138c37bd1dcdf645b
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2014-09-29 21:17:18 -0400

    Fix review [KP-3705]
    --HG--
    branch : csharp-business-language
    
    
    Former-commit-id: 27e056510e6a0397bb315db3c365844882cf6b78

---
+++ src/Core/SharedKernels/DataCollection/DataCollection/Accessors/QuestionnaireAssemblyFileAccessor.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.Practices.ServiceLocation;
+using WB.Core.GenericSubdomains.Logging;
+using WB.Core.Infrastructure.FileSystem;
+
+namespace WB.Core.SharedKernels.DataCollection.Accessors
+{
+    public class QuestionnaireAssemblyFileAccessor : IQuestionnaireAssemblyFileAccessor
+    {
+        private readonly IFileSystemAccessor fileSystemAccessor;
+        private readonly string pathToStore;
+
+        private static ILogger Logger
+        {
+            get { return ServiceLocator.Current.GetInstance<ILogger>(); }
+        }
+
+        public QuestionnaireAssemblyFileAccessor(IFileSystemAccessor fileSystemAccessor, string folderPath, string assemblyDirectoryName)
+        {
+            this.fileSystemAccessor = fileSystemAccessor;
+
+            this.pathToStore = fileSystemAccessor.CombinePath(folderPath, assemblyDirectoryName);
+            if (!fileSystemAccessor.IsDirectoryExists(this.pathToStore))
+                fileSystemAccessor.CreateDirectory(this.pathToStore);
+        }
+
+        public string GetFullPathToAssembly(Guid questionnaireId, long questionnaireVersion)
+        {
+            string assemblyFileName = this.GetAssemblyFileName(questionnaireId, questionnaireVersion);
+            return this.fileSystemAccessor.CombinePath(this.pathToStore, assemblyFileName);
+        }
+
+        public void StoreAssembly(Guid questionnaireId, long questionnaireVersion, string assemblyAsBase64)
+        {
+            string assemblyFileName = this.GetAssemblyFileName(questionnaireId, questionnaireVersion);
+            var pathToSaveAssembly = this.fileSystemAccessor.CombinePath(this.pathToStore, assemblyFileName);
+
+            if (this.fileSystemAccessor.IsFileExists(pathToSaveAssembly))
+                this.fileSystemAccessor.DeleteFile(pathToSaveAssembly);
+
+            this.fileSystemAccessor.WriteAllBytes(pathToSaveAssembly, Convert.FromBase64String(assemblyAsBase64));
+        }
+
+        public void RemoveAssembly(Guid questionnaireId, long questionnaireVersion)
+        {
+            string assemblyFileName = this.GetAssemblyFileName(questionnaireId, questionnaireVersion);
+            var pathToSaveAssembly = this.fileSystemAccessor.CombinePath(this.pathToStore, assemblyFileName);
+
+            //loaded assembly could be locked
+            try
+            {
+                this.fileSystemAccessor.DeleteFile(pathToSaveAssembly);
+            }
+            catch (Exception e)
+            {
+                Logger.Error("Error on assembly deletion");
+                Logger.Error(e.Message, e);
+            }
+        }
+
+        public string GetAssemblyAsBase64String(Guid questionnaireId, long questionnaireVersion)
+        {
+            byte[] assemblyAsByteArray = this.GetAssemblyAsByteArray(questionnaireId, questionnaireVersion);
+
+            return Convert.ToBase64String(assemblyAsByteArray);
+        }
+
+        public byte[] GetAssemblyAsByteArray(Guid questionnaireId, long questionnaireVersion)
+        {
+            string assemblyFileName = this.GetAssemblyFileName(questionnaireId, questionnaireVersion);
+            string pathToAssembly = this.fileSystemAccessor.CombinePath(this.pathToStore, assemblyFileName);
+
+            return this.fileSystemAccessor.ReadAllBytes(pathToAssembly);
+        }
+
+        private string GetAssemblyFileName(Guid questionnaireId, long questionnaireVersion)
+        {
+            return String.Format("assembly_{0}_v{1}.dll", questionnaireId, questionnaireVersion);
+        }
+    }
+}
