f0808c4298e2e1d89a119220e17d2dfb0fd808c2
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2014-09-29 23:56:17 -0400

    tests added [KP-3705]
    --HG--
    branch : csharp-business-language
    
    
    Former-commit-id: 4edca597cddb2ae849b85e93cc2237821920c8a6

---
+++ src/Tests/WB.Core.SharedKernels.DataCollection.Tests/QuestionnaireAssemblyFileAccessorTests/QuestionnaireAssemblyFileAccessorTestsContext.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using Machine.Specifications;
+using Moq;
+using WB.Core.Infrastructure.FileSystem;
+using WB.Core.SharedKernels.DataCollection.Accessors;
+
+namespace WB.Core.SharedKernels.DataCollection.Tests.QuestionnaireAssemblyFileAccessorTests
+{
+    [Subject(typeof(QuestionnaireAssemblyFileAccessor))]
+    class QuestionnaireAssemblyFileAccessorTestsContext
+    {
+        protected static QuestionnaireAssemblyFileAccessor CreateQuestionnaireAssemblyFileAccessor(IFileSystemAccessor fileSystemAccessor = null)
+        {
+            return new QuestionnaireAssemblyFileAccessor(fileSystemAccessor ?? CreateIFileSystemAccessorMock().Object, "", "QuestionnaireAssembly");
+        }
+
+        protected static Mock<IFileSystemAccessor> CreateIFileSystemAccessorMock()
+        {
+            var result = new Mock<IFileSystemAccessor>();
+            result.Setup(x => x.CombinePath(Moq.It.IsAny<string>(), Moq.It.IsAny<string>()))
+                .Returns<string, string>(Path.Combine);
+            return result;
+        }
+    }
+}
