5ad0321d00ac64de6409cfa05868d728a7cc56ae
Dima Pasko <dimapasko@gmail.com>
2014-10-01 14:06:01 +0300

    Core: Add validation to the numeric check for Parent Value in AR KP-4313 'Something goes wrong' error appears when user imports file with non Numeric character in ParenValue column
    Former-commit-id: 0e8ff1e2b1d7d07b8fda3fdd85a703089b21106e

---
+++ src/Tests/WB.Core.BoundedContexts.Designer.Tests/UpdateFilteredComboboxOptionsHandlerTests/when_updating_cascading_combobox_options_and_1_option_has_not_decimal_parent_value.cs
@@ -0,0 +1,67 @@
+using System;
+using Machine.Specifications;
+using Main.Core.Entities.SubEntities;
+using Main.Core.Events.Questionnaire;
+using WB.Core.BoundedContexts.Designer.Aggregates;
+using WB.Core.BoundedContexts.Designer.Exceptions;
+using WB.Core.BoundedContexts.Designer.Tests.QuestionnaireTests;
+
+namespace WB.Core.BoundedContexts.Designer.Tests.UpdateSingleOptionQuestionHandlerTests
+{
+    internal class when_updating_cascading_combobox_options_and_1_option_has_not_decimal_parent_value : QuestionnaireTestsContext
+    {
+        Establish context = () =>
+        {
+            questionnaire = CreateQuestionnaire(responsibleId: responsibleId);
+            questionnaire.Apply(new NewGroupAdded { PublicKey = chapterId });
+            questionnaire.Apply(new NewQuestionAdded
+            {
+                PublicKey = parentQuestionId,
+                GroupPublicKey = chapterId,
+                QuestionType = QuestionType.SingleOption,
+                QuestionText = "text",
+                StataExportCaption = "var",
+                ResponsibleId = responsibleId,
+                Answers = new Answer[]
+                {
+                    new Answer { AnswerText = "Option 1", AnswerValue = "1" },
+                    new Answer { AnswerText = "Option 2", AnswerValue = "2" }
+                }
+            });
+            questionnaire.Apply(new NewQuestionAdded
+            {
+                PublicKey = questionId,
+                GroupPublicKey = chapterId,
+                QuestionType = QuestionType.SingleOption,
+                QuestionText = "text",
+                StataExportCaption = "var2",
+                ResponsibleId = responsibleId,
+                CascadeFromQuestionId = parentQuestionId
+            });
+        };
+
+        Because of = () =>
+            exception = Catch.Exception(() =>
+                questionnaire.UpdateCascadingComboboxOptions(questionId: questionId, responsibleId: responsibleId, options: options));
+
+        It should_throw_QuestionnaireException = () =>
+            exception.ShouldBeOfExactType<QuestionnaireException>();
+
+        It should_throw_exception_with_message_containting__empty____parentvalue__ = () =>
+            new[] { "not numeric", "parentvalue" }.ShouldEachConformTo(
+                keyword => exception.Message.ToLower().Contains(keyword));
+
+
+        private static Questionnaire questionnaire;
+        private static Exception exception;
+        private static Guid questionId = Guid.Parse("11111111111111111111111111111111");
+        private static Guid parentQuestionId = Guid.Parse("22222222222222222222222222222222");
+        private static Guid chapterId = Guid.Parse("CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC");
+        private static Guid responsibleId = Guid.Parse("DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD");
+        private static Option[] options =
+        {
+            new Option(Guid.NewGuid(), "1", "Option 1", "1"), 
+            new Option(Guid.NewGuid(), "2", "Option 2", "b")
+        };
+    }
+}
