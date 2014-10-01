01337cc54afb9ff88e4c832cf20a481fb2ecf2e3
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2014-10-01 16:54:43 +0300

    Tests\Interview: unignored first test KP-3791
    --HG--
    branch : csharp-business-language
    
    
    Former-commit-id: d7d0ae18af361802759c8a4c13e869b76e65a03e

--- src/Tests/WB.Core.SharedKernels.DataCollection.Tests/Create.cs
+++ src/Tests/WB.Core.SharedKernels.DataCollection.Tests/Create.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Main.Core.Documents;
 using WB.Core.SharedKernels.DataCollection.Implementation.Aggregates;
 
@@ -9,6 +10,21 @@ namespace WB.Core.SharedKernels.DataCollection.Tests
         public static Questionnaire Questionnaire(Guid creatorId, QuestionnaireDocument document)
         {
             return new Questionnaire(new Guid(), document, false, string.Empty);
+        }
+
+        public static EnablementChanges EnablementChanges(List<Identity> groupsToBeDisabled = null, List<Identity> groupsToBeEnabled = null,
+            List<Identity> questionsToBeDisabled = null, List<Identity> questionsToBeEnabled = null)
+        {
+            return new EnablementChanges(
+                groupsToBeDisabled ?? new List<Identity>(),
+                groupsToBeEnabled ?? new List<Identity>(),
+                questionsToBeDisabled ?? new List<Identity>(),
+                questionsToBeEnabled ?? new List<Identity>());
+        }
+
+        public static Identity Identity(Guid id, decimal[] rosterVector)
+        {
+            return new Identity(id, rosterVector);
         }
     }
 }
