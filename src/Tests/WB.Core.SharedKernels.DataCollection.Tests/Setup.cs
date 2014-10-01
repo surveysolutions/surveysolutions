03a7bcb314cbf6e2e62339f2f3d9270e835bb21c
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2014-10-01 18:26:15 +0300

    Tests\Interview: unignored test ignored back because it caused errors in other tests, minor refactoring performed KP-3791
    --HG--
    branch : csharp-business-language
    
    
    Former-commit-id: fb837628bc0f27c8b52f224628da75a659e541cf

---
+++ src/Tests/WB.Core.SharedKernels.DataCollection.Tests/Setup.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq.Expressions;
+using Microsoft.Practices.ServiceLocation;
+using Moq;
+using WB.Core.SharedKernels.DataCollection.Aggregates;
+using WB.Core.SharedKernels.DataCollection.Repositories;
+
+namespace WB.Core.SharedKernels.DataCollection.Tests
+{
+    internal static class Setup
+    {
+        public static void InstanceToMockedServiceLocator<TInstance>(TInstance instance)
+        {
+            Mock.Get(ServiceLocator.Current)
+                .Setup(locator => locator.GetInstance<TInstance>())
+                .Returns(instance);
+        }
+
+        public static void SelfCloningInterviewExpressionStateStubWithProviderToMockedServiceLocator(
+            Guid questionnaireId, Expression<Func<IInterviewExpressionState, bool>> expressionStateMoqPredicate)
+        {
+            var expressionState = Mock.Of<IInterviewExpressionState>(expressionStateMoqPredicate);
+
+            Mock.Get(expressionState).Setup(_ => _.Clone()).Returns(() => expressionState);
+
+            var interviewExpressionStatePrototypeProvider = Mock.Of<IInterviewExpressionStatePrototypeProvider>(_
+                => _.GetExpressionState(questionnaireId, It.IsAny<long>()) == expressionState);
+
+            Setup.InstanceToMockedServiceLocator<IInterviewExpressionStatePrototypeProvider>(interviewExpressionStatePrototypeProvider);
+        }
+
+        public static void QuestionnaireWithRepositoryToMockedServiceLocator(
+            Guid questionnaireId, Expression<Func<IQuestionnaire, bool>> questionnaireMoqPredicate)
+        {
+            var questionnaire = Mock.Of<IQuestionnaire>(questionnaireMoqPredicate);
+
+            Setup.InstanceToMockedServiceLocator<IQuestionnaireRepository>(
+                Create.QuestionnaireRepositoryStubWithOneQuestionnaire(questionnaireId, questionnaire));
+        }
+    }
+}
