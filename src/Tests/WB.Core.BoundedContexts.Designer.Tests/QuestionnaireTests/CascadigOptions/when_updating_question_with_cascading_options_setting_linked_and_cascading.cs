3e16c34ab3e620ea94ef810f0d3e830948468520
Dima Pasko <dimapasko@gmail.com>
2014-10-03 13:56:13 +0300

    Designer: add unit tests for UpdateSungleQuestion command as requested in Code Review KP-4331 'Answer option value is required' message appears when trying to add options if empty values were added to single option
    --HG--
    branch : release-candidate
    
    
    Former-commit-id: 0a8ce0de7cf882dac2ee454487cced39f7813b78

---
+++ src/Tests/WB.Core.BoundedContexts.Designer.Tests/QuestionnaireTests/CascadigOptions/when_updating_question_with_cascading_options_setting_linked_and_cascading.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Machine.Specifications;
+using Main.Core.Entities.SubEntities;
+using Main.Core.Events.Questionnaire;
+using WB.Core.BoundedContexts.Designer.Aggregates;
+using WB.Core.BoundedContexts.Designer.Exceptions;
+
+namespace WB.Core.BoundedContexts.Designer.Tests.QuestionnaireTests.CascadigOptions
+{
+    internal class when_updating_question_with_cascading_options_setting_linked_and_cascading : QuestionnaireTestsContext
+    {
+        Establish context = () =>
+        {
+            rootGroupId = Guid.NewGuid();
+            actorId = Guid.NewGuid();
+            questionnaire = CreateQuestionnaireWithOneGroup(actorId, groupId: rootGroupId);
+
+            parentQuestionId = Guid.NewGuid();
+            updatedQuestionId = Guid.NewGuid();
+
+            questionnaire.Apply(new NewQuestionAdded
+            {
+                PublicKey = parentQuestionId,
+                QuestionType = QuestionType.SingleOption,
+                Answers = new Answer[] { new Answer
+                {
+                    AnswerText = "one", 
+                    AnswerValue = "1", 
+                    PublicKey = Guid.NewGuid()
+                } }
+            });
+            questionnaire.Apply(new NewQuestionAdded
+            {
+                PublicKey = updatedQuestionId,
+                QuestionType = QuestionType.SingleOption,
+            });
+        };
+
+        Because of = () => exception = Catch.Exception(() => questionnaire.UpdateSingleOptionQuestion(
+            updatedQuestionId,
+            "title",
+            "var",
+            null,
+            false,
+            false,
+            QuestionScope.Interviewer,
+            null,
+            null,
+            null,
+            null,
+            actorId,
+            new Option[]{}, 
+            parentQuestionId,
+            false,
+            cascadeFromQuestionId: parentQuestionId
+            ));
+
+        It should_not_allow_to_set_both_linked_and_cascading_qestion_at_the_same_time = () =>
+        {
+            var ex = exception as QuestionnaireException;
+            ex.ShouldNotBeNull();
+
+            new []{"cascading", "linked", "same", "time"}.ShouldEachConformTo(keyword => ex.Message.ToLower().Contains(keyword));
+        };
+
+
+        private static Questionnaire questionnaire;
+        private static Guid parentQuestionId;
+        private static Exception exception;
+        private static Guid rootGroupId;
+        private static Guid updatedQuestionId;
+        private static Guid actorId;
+    }
+}
+
