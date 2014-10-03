b2b049bface04bba05c5e483c6e2fec4a80f2e59
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2014-10-02 23:03:25 -0400

    Tests fix
    --HG--
    branch : csharp-business-language
    
    
    Former-commit-id: 06ae8226d2cd5cbc297f10753487a38c0a9b962a

--- src/Tests/WB.Core.SharedKernels.DataCollection.Tests/InterviewTests/when_creating_interview_and_questionnaire_has_group_with_custom_enablement_condition_and_group_has_propagatable_ancestor_group.cs
+++ src/Tests/WB.Core.SharedKernels.DataCollection.Tests/InterviewTests/when_creating_interview_and_questionnaire_has_group_with_custom_enablement_condition_and_group_has_propagatable_ancestor_group.cs
@@ -7,6 +7,7 @@ using Ncqrs.Spec;
 using WB.Core.SharedKernels.DataCollection.Aggregates;
 using WB.Core.SharedKernels.DataCollection.Events.Interview;
 using WB.Core.SharedKernels.DataCollection.Implementation.Aggregates;
+using WB.Core.SharedKernels.DataCollection.Implementation.Providers;
 using WB.Core.SharedKernels.DataCollection.Implementation.Repositories;
 using WB.Core.SharedKernels.DataCollection.Repositories;
 using It = Machine.Specifications.It;
@@ -37,6 +38,8 @@ namespace WB.Core.SharedKernels.DataCollection.Tests.InterviewTests
             Mock.Get(ServiceLocator.Current)
                 .Setup(locator => locator.GetInstance<IQuestionnaireRepository>())
                 .Returns(questionnaireRepository);
+
+            SetupInstanceToMockedServiceLocator<IInterviewExpressionStatePrototypeProvider>(CreateInterviewExpressionStateProviderStub());
 
             eventContext = new EventContext();
         };
