7f5be85682112f07dfbb2c84fb3cdab2d1a87f1e
Dima Pasko <dimapasko@gmail.com>
2014-10-06 16:07:23 +0300

    Add more UpdateCascadingComboboxOptions command handler Unit tests KP-4349 No error appears when user clicks 'Apply' in 'Edit Options' window if there are no options.
    --HG--
    branch : release-candidate
    
    
    Former-commit-id: acb146a43a51df1c59e6d19aac18a48d098bcfea

--- src/Tests/WB.Core.BoundedContexts.Designer.Tests/UpdateCascadingComboboxOptionsHandlerTests/when_updating_cascading_combobox_options_and_1_option_not_number.cs
+++ src/Tests/WB.Core.BoundedContexts.Designer.Tests/UpdateCascadingComboboxOptionsHandlerTests/when_updating_cascading_combobox_options_and_1_option_not_number.cs
@@ -6,9 +6,9 @@ using WB.Core.BoundedContexts.Designer.Aggregates;
 using WB.Core.BoundedContexts.Designer.Exceptions;
 using WB.Core.BoundedContexts.Designer.Tests.QuestionnaireTests;
 
-namespace WB.Core.BoundedContexts.Designer.Tests.UpdateSingleOptionQuestionHandlerTests
+namespace WB.Core.BoundedContexts.Designer.Tests.UpdateCascadingComboboxOptionsHandlerTests
 {
-    internal class when_updating_cascading_combobox_options_and_1_option_has_not_decimal_parent_value : QuestionnaireTestsContext
+    internal class when_updating_cascading_combobox_options_and_1_option_not_number : QuestionnaireTestsContext
     {
         Establish context = () =>
         {
@@ -28,13 +28,14 @@ namespace WB.Core.BoundedContexts.Designer.Tests.UpdateSingleOptionQuestionHandl
                     new Answer { AnswerText = "Option 2", AnswerValue = "2" }
                 }
             });
-            questionnaire.Apply(new NewQuestionAdded
+            questionnaire.Apply(new NewQuestionAdded()
             {
                 PublicKey = questionId,
                 GroupPublicKey = chapterId,
                 QuestionType = QuestionType.SingleOption,
                 QuestionText = "text",
-                StataExportCaption = "var2",
+                StataExportCaption = "var",
+                IsFilteredCombobox = false,
                 ResponsibleId = responsibleId,
                 CascadeFromQuestionId = parentQuestionId
             });
@@ -47,9 +48,9 @@ namespace WB.Core.BoundedContexts.Designer.Tests.UpdateSingleOptionQuestionHandl
         It should_throw_QuestionnaireException = () =>
             exception.ShouldBeOfExactType<QuestionnaireException>();
 
-        It should_throw_exception_with_message_containting__not_numeric____parentvalue__ = () =>
-            new[] { "not numeric", "parentvalue" }.ShouldEachConformTo(
-                keyword => exception.Message.ToLower().Contains(keyword));
+        It should_throw_exception_with_message_containting__value_only_number__ = () =>
+             new[] { "value", "only number" }.ShouldEachConformTo(
+                    keyword => exception.Message.ToLower().Contains(keyword));
 
 
         private static Questionnaire questionnaire;
@@ -58,10 +59,6 @@ namespace WB.Core.BoundedContexts.Designer.Tests.UpdateSingleOptionQuestionHandl
         private static Guid parentQuestionId = Guid.Parse("22222222222222222222222222222222");
         private static Guid chapterId = Guid.Parse("CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC");
         private static Guid responsibleId = Guid.Parse("DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD");
-        private static Option[] options =
-        {
-            new Option(Guid.NewGuid(), "1", "Option 1", "1"), 
-            new Option(Guid.NewGuid(), "2", "Option 2", "b")
-        };
+        private static Option[] options = new[] { new Option(Guid.NewGuid(), "1", "Option 1"), new Option(Guid.NewGuid(), "not number value", "Option 2") };
     }
 }
