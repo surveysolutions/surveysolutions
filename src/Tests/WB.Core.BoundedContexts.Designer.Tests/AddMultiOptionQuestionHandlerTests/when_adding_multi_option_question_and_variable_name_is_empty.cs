f1bc2641ac4c636564b9a4e316e7fce989142d4d
Dima Pasko <dimapasko@gmail.com>
2014-10-06 16:07:23 +0300

    Add more UpdateCascadingComboboxOptions command handler Unit tests KP-4349 No error appears when user clicks 'Apply' in 'Edit Options' window if there are no options.
    --HG--
    branch : release-candidate
    
    
    Former-commit-id: acb146a43a51df1c59e6d19aac18a48d098bcfea

--- src/Tests/WB.Core.BoundedContexts.Designer.Tests/AddMultiOptionQuestionHandlerTests/when_adding_multi_option_question_and_variable_name_is_empty.cs
+++ src/Tests/WB.Core.BoundedContexts.Designer.Tests/AddMultiOptionQuestionHandlerTests/when_adding_multi_option_question_and_variable_name_is_empty.cs
@@ -23,7 +23,7 @@ namespace WB.Core.BoundedContexts.Designer.Tests.AddMultiOptionQuestionHandlerTe
                     parentGroupId: chapterId,
                     title: title,
                     variableName: emptyVariableName,
-                variableLabel: null,
+                    variableLabel: null,
                     isMandatory: isMandatory,
                     scope: QuestionScope.Interviewer,
                     enablementCondition: enablementCondition,
