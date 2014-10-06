758240e8d9677fcd746be8116f559984a80da25c
Anatoliy 'TLK' Kolesnick <none@none>
2014-10-06 17:18:25 +0300

    Tests\CodeGenerations: 5 more tests converted to work in isolated app domains KP-4136
    --HG--
    branch : csharp-business-language
    
    
    Former-commit-id: 3bab5ce7f1e3ba94e71259515b8512e4c400e2ed

--- src/Tests/WB.Core.BoundedContexts.Designer.Tests/CodeGenerationTests/when_expression_state_processes_mandatory_question.cs
+++ src/Tests/WB.Core.BoundedContexts.Designer.Tests/CodeGenerationTests/when_expression_state_processes_mandatory_question.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using AppDomainToolkit;
 using Machine.Specifications;
 using Main.Core.Documents;
 using Microsoft.Practices.ServiceLocation;
@@ -9,39 +10,60 @@ using It = Machine.Specifications.It;
 
 namespace WB.Core.BoundedContexts.Designer.Tests.CodeGenerationTests
 {
-    [Ignore("bulk test run failed on server build")]
     internal class when_expression_state_processes_mandatory_question : CodeGenerationTestsContext
     {
         Establish context = () =>
         {
-            var serviceLocatorMock = new Mock<IServiceLocator> { DefaultValue = DefaultValue.Mock };
-            ServiceLocator.SetLocatorProvider(() => serviceLocatorMock.Object);
-
-            questionnaireDocument = CreateQuestionnaireDocumenteHavingMandatoryQuestions(questionnaireId, question1Id, question2Id, question3Id, question4Id);
-            state = GetInterviewExpressionState(questionnaireDocument);
-            
+            appDomainContext = AppDomainContext.Create();
         };
 
         Because of = () =>
-            state.ProcessValidationExpressions(out questionsToBeValid, out questionsToBeInvalid);
+            results = RemoteFunc.Invoke(appDomainContext.Domain, () =>
+            {
+                Guid questionnaireId = Guid.Parse("11111111111111111111111111111111");
+                Guid question1Id = Guid.Parse("11111111111111111111111111111112");
+                Guid question2Id = Guid.Parse("21111111111111111111111111111112");
+                Guid question3Id = Guid.Parse("31111111111111111111111111111112");
+                Guid question4Id = Guid.Parse("41111111111111111111111111111112");
+
+                List<Identity> questionsToBeValid;
+                List<Identity> questionsToBeInvalid;
+
+                var serviceLocatorMock = new Mock<IServiceLocator> { DefaultValue = DefaultValue.Mock };
+                ServiceLocator.SetLocatorProvider(() => serviceLocatorMock.Object);
+
+                QuestionnaireDocument questionnaireDocument = CreateQuestionnaireDocumenteHavingMandatoryQuestions(questionnaireId, question1Id, question2Id, question3Id, question4Id);
+                IInterviewExpressionState state = GetInterviewExpressionState(questionnaireDocument);
+
+                state.ProcessValidationExpressions(out questionsToBeValid, out questionsToBeInvalid);
+
+                return new InvokeResults
+                {
+                    ValidQuestionsCount = questionsToBeValid.Count,
+                    InvalidQuestionsCount = questionsToBeInvalid.Count,
+                };
+            });
 
         It should_valid_question_count_equal_4 = () =>
-            questionsToBeValid.Count.ShouldEqual(4);
+            results.ValidQuestionsCount.ShouldEqual(4);
 
         It should_invalid_question_count_equal_0 = () =>
-            questionsToBeInvalid.Count.ShouldEqual(0);
+            results.InvalidQuestionsCount.ShouldEqual(0);
 
-        private static Guid questionnaireId = Guid.Parse("11111111111111111111111111111111");
-        private static Guid question1Id = Guid.Parse("11111111111111111111111111111112");
-        private static Guid question2Id = Guid.Parse("21111111111111111111111111111112");
-        private static Guid question3Id = Guid.Parse("31111111111111111111111111111112");
-        private static Guid question4Id = Guid.Parse("41111111111111111111111111111112");
+        Cleanup stuff = () =>
+        {
+            appDomainContext.Dispose();
+            appDomainContext = null;
+        };
 
-        private static QuestionnaireDocument questionnaireDocument;
+        private static AppDomainContext appDomainContext;
+        private static InvokeResults results;
 
-        private static IInterviewExpressionState state;
-
-        private static List<Identity> questionsToBeValid;
-        private static List<Identity> questionsToBeInvalid;
+        [Serializable]
+        internal class InvokeResults
+        {
+            public int ValidQuestionsCount { get; set; }
+            public int InvalidQuestionsCount { get; set; }
+        }
     }
 }
