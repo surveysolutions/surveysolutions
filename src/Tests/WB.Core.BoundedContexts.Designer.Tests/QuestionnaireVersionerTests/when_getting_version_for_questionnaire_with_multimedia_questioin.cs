0a1a518c77813bf0691691a2ff26d2f65a2ebd58
Slava Scherbak <slava.scherbak@gmail.com>
2014-10-06 09:13:57 -0400

    Designer: icreased version of questionnaire document if it contains cascadig question to 4.0.0 KP-4353
    --HG--
    branch : release-candidate
    
    
    Former-commit-id: 3cfe67f6bdac9c6cbf6e6603a1faa57c8d0ace78

--- src/Tests/WB.Core.BoundedContexts.Designer.Tests/QuestionnaireVersionerTests/when_getting_version_for_questionnaire_with_multimedia_questioin.cs
+++ src/Tests/WB.Core.BoundedContexts.Designer.Tests/QuestionnaireVersionerTests/when_getting_version_for_questionnaire_with_multimedia_questioin.cs
@@ -13,6 +13,49 @@ using WB.Core.SharedKernels.QuestionnaireVerification.ValueObjects;
 
 namespace WB.Core.BoundedContexts.Designer.Tests.QuestionnaireVersionerTests
 {
+    internal class when_getting_version_for_questionnaire_with_cascading_questioin : QuestionnaireVersionerTestContext
+    {
+        Establish context = () =>
+        {
+            questionnaire =
+                CreateQuestionnaireDocumentWithOneChapter(new Group()
+                {
+                    PublicKey = Guid.Parse("22222222222222222222222222222222"),
+                    Children = new List<IComposite>()
+                    {
+                        new SingleQuestion
+                        {
+                            PublicKey = cascadingParentId
+                        },
+                        new SingleQuestion
+                        {
+                            PublicKey = cascadingId,
+                            CascadeFromQuestionId = cascadingParentId
+                        }
+                    }
+                });
+            versioner = CreateQuestionnaireVersioner();
+        };
+
+        Because of = () =>
+            version = versioner.GetVersion(questionnaire);
+
+        It should_set_Major_property_to_4 = () =>
+            version.Major.ShouldEqual(4);
+
+        It should_set_Minor_property_to_0 = () =>
+            version.Minor.ShouldEqual(0);
+
+        It should_set_Patch_property_to_0 = () =>
+            version.Patch.ShouldEqual(0);
+
+        private static QuestionnaireVersion version;
+        private static QuestionnaireDocument questionnaire;
+        private static QuestionnaireVersioner versioner;
+        private static readonly Guid cascadingId = Guid.Parse("11111111111111111111111111111111");
+        private static readonly Guid cascadingParentId = Guid.Parse("33333333333333333333333333333333");
+    }
+
     internal class when_getting_version_for_questionnaire_with_multimedia_questioin : QuestionnaireVersionerTestContext
     {
         Establish context = () =>
