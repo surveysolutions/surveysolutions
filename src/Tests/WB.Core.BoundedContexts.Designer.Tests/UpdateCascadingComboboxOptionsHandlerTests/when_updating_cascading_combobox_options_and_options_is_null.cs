2f332ba614fe3cb01a05ad1acc5b3a1382f12c09
Dima Pasko <dimapasko@gmail.com>
2014-10-06 16:07:23 +0300

    Add more UpdateCascadingComboboxOptions command handler Unit tests KP-4349 No error appears when user clicks 'Apply' in 'Edit Options' window if there are no options.
    --HG--
    branch : release-candidate
    
    
    Former-commit-id: acb146a43a51df1c59e6d19aac18a48d098bcfea

--- src/Tests/WB.Core.BoundedContexts.Designer.Tests/UpdateCascadingComboboxOptionsHandlerTests/when_updating_cascading_combobox_options_and_options_is_null.cs
+++ src/Tests/WB.Core.BoundedContexts.Designer.Tests/UpdateCascadingComboboxOptionsHandlerTests/when_updating_cascading_combobox_options_and_options_is_null.cs
@@ -6,9 +6,9 @@ using WB.Core.BoundedContexts.Designer.Aggregates;
 using WB.Core.BoundedContexts.Designer.Exceptions;
 using WB.Core.BoundedContexts.Designer.Tests.QuestionnaireTests;
 
-namespace WB.Core.BoundedContexts.Designer.Tests.UpdateSingleOptionQuestionHandlerTests
+namespace WB.Core.BoundedContexts.Designer.Tests.UpdateCascadingComboboxOptionsHandlerTests
 {
-    internal class when_updating_cascading_combobox_options_and_1_option_has_empty_parent_value : QuestionnaireTestsContext
+    internal class when_updating_cascading_combobox_options_and_options_is_null : QuestionnaireTestsContext
     {
         Establish context = () =>
         {
@@ -28,13 +28,14 @@ namespace WB.Core.BoundedContexts.Designer.Tests.UpdateSingleOptionQuestionHandl
                     new Answer { AnswerText = "Option 2", AnswerValue = "2" }
                 }
             });
-            questionnaire.Apply(new NewQuestionAdded
+            questionnaire.Apply(new NewQuestionAdded()
             {
                 PublicKey = questionId,
                 GroupPublicKey = chapterId,
                 QuestionType = QuestionType.SingleOption,
                 QuestionText = "text",
-                StataExportCaption = "var2",
+                StataExportCaption = "var",
+                IsFilteredCombobox = false,
                 ResponsibleId = responsibleId,
                 CascadeFromQuestionId = parentQuestionId
             });
@@ -42,14 +43,14 @@ namespace WB.Core.BoundedContexts.Designer.Tests.UpdateSingleOptionQuestionHandl
 
         Because of = () =>
             exception = Catch.Exception(() =>
-                questionnaire.UpdateCascadingComboboxOptions(questionId: questionId, responsibleId: responsibleId, options: options));
+                questionnaire.UpdateCascadingComboboxOptions(questionId: questionId, responsibleId: responsibleId, options: null));
 
         It should_throw_QuestionnaireException = () =>
             exception.ShouldBeOfExactType<QuestionnaireException>();
 
-        It should_throw_exception_with_message_containting__empty____parentvalue__ = () =>
-            new[] { "empty", "parentvalue" }.ShouldEachConformTo(
-                keyword => exception.Message.ToLower().Contains(keyword));
+        It should_throw_exception_with_message_containting__should_have_two_options_at_least__ = () =>
+             new[] { "should have", "two options at least" }.ShouldEachConformTo(
+                    keyword => exception.Message.ToLower().Contains(keyword));
 
 
         private static Questionnaire questionnaire;
@@ -58,10 +59,5 @@ namespace WB.Core.BoundedContexts.Designer.Tests.UpdateSingleOptionQuestionHandl
         private static Guid parentQuestionId = Guid.Parse("22222222222222222222222222222222");
         private static Guid chapterId = Guid.Parse("CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC");
         private static Guid responsibleId = Guid.Parse("DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD");
-        private static Option[] options =
-        {
-            new Option(Guid.NewGuid(), "1", "Option 1", "1"), 
-            new Option(Guid.NewGuid(), "2", "Option 2", (string)null)
-        };
     }
 }
