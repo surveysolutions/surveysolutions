5ce4cc8af69e50a4ea8c35364aacbed79f29bd03
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2014-10-13 13:02:29 +0300

    Questionnaire\MigrateExpressionsToCSharp: added command and empty command handler KP-4376
    --HG--
    branch : csharp-business-language
    
    
    Former-commit-id: bebf5ee91024fc6ecda425ba26815649db317898

---
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Commands/Questionnaire/Question/MigrateExpressionsToCSharp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Ncqrs.Commanding.CommandExecution.Mapping.Attributes;
+using WB.Core.BoundedContexts.Designer.Commands.Questionnaire.Base;
+
+namespace WB.Core.BoundedContexts.Designer.Commands.Questionnaire.Question
+{
+    [MapsToAggregateRootMethod(typeof(Aggregates.Questionnaire), "MigrateExpressionsToCSharp")]
+    public class MigrateExpressionsToCSharp : QuestionnaireCommand
+    {
+        public MigrateExpressionsToCSharp(Guid questionnaireId, Guid responsibleId)
+            : base(questionnaireId, responsibleId) {}
+    }
+}
