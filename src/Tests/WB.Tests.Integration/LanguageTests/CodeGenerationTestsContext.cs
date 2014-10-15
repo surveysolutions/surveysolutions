496a7dc781f1359d1600a5aa4673f6518bbed921
Dima Pasko <dimapasko@gmail.com>
2014-10-15 18:16:04 +0300

    Language, Tests: refactor test, move reusable parts to heplers KP-4379 Create Roslyn-specific test framework
    Former-commit-id: 3505f7b9113254f777966328decc36a37fe117ec

--- src/Tests/WB.Tests.Integration/LanguageTests/CodeGenerationTestsContext.cs
+++ src/Tests/WB.Tests.Integration/LanguageTests/CodeGenerationTestsContext.cs
@@ -1,15 +1,58 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
 using Main.Core.Documents;
+using Microsoft.Practices.ServiceLocation;
+using Moq;
+using Ncqrs.Eventing;
 using WB.Core.BoundedContexts.Designer.Implementation.Services.CodeGeneration;
 using WB.Core.SharedKernels.DataCollection;
+using WB.Core.SharedKernels.DataCollection.Events.Interview;
+using WB.Core.SharedKernels.DataCollection.Implementation.Aggregates;
+using WB.Core.SharedKernels.DataCollection.Implementation.Providers;
+using WB.Core.SharedKernels.DataCollection.Repositories;
 
 namespace WB.Tests.Integration.LanguageTests
 {
     internal class CodeGenerationTestsContext
     {
+        protected static Interview SetupInterview(Guid actorId, QuestionnaireDocument questionnaireDocument, Guid questionnaireId)
+        {
+            var questionnaire = Create.Questionnaire(actorId, questionnaireDocument);
+
+            var questionnaireRepository = Mock.Of<IQuestionnaireRepository>(repository
+                => repository.GetQuestionnaire(questionnaireId) == questionnaire.GetQuestionnaire()
+                    && repository.GetHistoricalQuestionnaire(questionnaireId, questionnaire.GetQuestionnaire().Version) == questionnaire.GetQuestionnaire()
+                    && repository.GetHistoricalQuestionnaire(questionnaireId, 1) == questionnaire.GetQuestionnaire());
+
+            Mock.Get(ServiceLocator.Current)
+                .Setup(locator => locator.GetInstance<IQuestionnaireRepository>())
+                .Returns(questionnaireRepository);
+
+            SetupRoslyn(questionnaireDocument);
+
+            var interview = Create.Interview(questionnaireId: questionnaireId);
+            return interview;
+        }
+
+        public static QuestionsEnabled GetQuestionEnabledEvent(IEnumerable<UncommittedEvent> events)
+        {
+            return ((QuestionsEnabled)events.First(b => b.Payload is QuestionsEnabled).Payload);
+        }
+
+        public static void SetupRoslyn(QuestionnaireDocument questionnaireDocument)
+        {
+            IInterviewExpressionState state = GetInterviewExpressionState(questionnaireDocument);
+
+            var statePrototypeProvider = Mock.Of<IInterviewExpressionStatePrototypeProvider>(a => a.GetExpressionState(It.IsAny<Guid>(), It.IsAny<long>()) == state);
+
+            Mock.Get(ServiceLocator.Current)
+                .Setup(locator => locator.GetInstance<IInterviewExpressionStatePrototypeProvider>())
+                .Returns(statePrototypeProvider);
+        }
+
         public static IInterviewExpressionState GetInterviewExpressionState(QuestionnaireDocument questionnaireDocument)
         {
             var expressionProcessorGenerator = new QuestionnireExpressionProcessorGenerator();
--- src/Tests/WB.Tests.Integration/LanguageTests/when_answering_on_question_which_is_part_of_condition_expression_in_another_question.cs
+++ src/Tests/WB.Tests.Integration/LanguageTests/when_answering_on_question_which_is_part_of_condition_expression_in_another_question.cs
@@ -1,19 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using AppDomainToolkit;
 using Machine.Specifications;
 using Main.Core.Documents;
-using Main.Core.Entities.SubEntities;
-using Main.Core.Entities.SubEntities.Question;
 using Microsoft.Practices.ServiceLocation;
 using Moq;
+using Ncqrs.Eventing;
 using Ncqrs.Spec;
-using WB.Core.SharedKernels.DataCollection;
 using WB.Core.SharedKernels.DataCollection.Aggregates;
 using WB.Core.SharedKernels.DataCollection.Events.Interview;
 using WB.Core.SharedKernels.DataCollection.Implementation.Aggregates;
-using WB.Core.SharedKernels.DataCollection.Implementation.Providers;
 using WB.Core.SharedKernels.DataCollection.Repositories;
 using It = Machine.Specifications.It;
 
@@ -28,75 +24,32 @@ namespace WB.Tests.Integration.LanguageTests
 
         Because of = () =>
             results = RemoteFunc.Invoke(appDomainContext.Domain, () =>
-            {
+            {                
                 var questionnaireId = Guid.Parse("00000000000000000000000000000000");
+                var actorId = Guid.Parse("99999999999999999999999999999999");
                 var question1Id = Guid.Parse("11111111111111111111111111111111");
                 var question2Id = Guid.Parse("22222222222222222222222222222222");
-                var actorId = Guid.Parse("99999999999999999999999999999999");
 
                 var serviceLocatorMock = new Mock<IServiceLocator> { DefaultValue = DefaultValue.Mock };
                 ServiceLocator.SetLocatorProvider(() => serviceLocatorMock.Object);
 
-                var questionnaireDocument = new QuestionnaireDocument() { PublicKey = questionnaireId };
+                var questionnaireDocument = Create.QuestionnaireDocument(questionnaireId,
+                    Create.NumericIntegerQuestion(question1Id, "q1"),
+                    Create.NumericIntegerQuestion(question2Id, "q2", "q1 > 3")
+               );
 
-                questionnaireDocument.Add(new NumericQuestion
-                {
-                    QuestionType = QuestionType.Numeric,
-                    PublicKey = question1Id,
-                    StataExportCaption = "q1",
-                    IsInteger = true,
-                }, questionnaireId, null);
-
-                questionnaireDocument.Add(new NumericQuestion
-                {
-                    QuestionType = QuestionType.Numeric,
-                    PublicKey = question2Id,
-                    StataExportCaption = "q2",
-                    IsInteger = true,
-                    ConditionExpression = "q1 > 3"
-                }, questionnaireId, null);
-
-                var questionnaire = new Questionnaire(actorId, questionnaireDocument, false, string.Empty);
-
-                IQuestionnaire questionaire = questionnaire.GetQuestionnaire();
-
-                var questionnaireRepository = Mock.Of<IQuestionnaireRepository>(repository
-                    => repository.GetQuestionnaire(questionnaireId) == questionaire
-                        && repository.GetHistoricalQuestionnaire(questionnaireId, questionaire.Version) == questionaire
-                        && repository.GetHistoricalQuestionnaire(questionnaireId, 1) == questionaire);
-
-                Mock.Get(ServiceLocator.Current)
-                    .Setup(locator => locator.GetInstance<IQuestionnaireRepository>())
-                    .Returns(questionnaireRepository);
-
-                IInterviewExpressionState state = GetInterviewExpressionState(questionnaireDocument);
-
-                var statePrototypeProvider = Mock.Of<IInterviewExpressionStatePrototypeProvider>(a => a.GetExpressionState(questionnaireId, questionaire.Version) == state);
-
-                Mock.Get(ServiceLocator.Current)
-                    .Setup(locator => locator.GetInstance<IInterviewExpressionStatePrototypeProvider>())
-                    .Returns(statePrototypeProvider);
-
-                var interview = new Interview(
-                    new Guid("A0A0A0A0B0B0B0B0A0A0A0A0B0B0B0B0"),
-                    actorId,
-                    questionnaireId,
-                    1,
-                    new Dictionary<Guid, object>(),
-                    new DateTime(2012, 12, 20),
-                    new Guid("D222D222D222D222D222D222D222D222"));
+                var interview = SetupInterview(actorId, questionnaireDocument, questionnaireId);
 
                 var eventContext = new EventContext();
 
                 interview.AnswerNumericIntegerQuestion(actorId, question1Id, new decimal[0], DateTime.Now, 4);
 
-                var questionsEnabledEvent = eventContext.Events.First(b => b.Payload is QuestionsEnabled);
-                var result = ((QuestionsEnabled) questionsEnabledEvent.Payload).Questions.FirstOrDefault(q => q.Id == question2Id);
+                var result = GetQuestionEnabledEvent(eventContext.Events).Questions.FirstOrDefault(q => q.Id == question2Id);
 
                 return new InvokeResults { Questions2Enabled = result != null};
             });
 
-        It should_disabled_question_count_equal_0 = () =>
+        It should_enable_second_question = () =>
             results.Questions2Enabled.ShouldBeTrue();
 
         Cleanup stuff = () =>
