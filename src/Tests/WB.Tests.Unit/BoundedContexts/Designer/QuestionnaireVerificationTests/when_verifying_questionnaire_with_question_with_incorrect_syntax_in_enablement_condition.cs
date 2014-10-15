2aab3c175688ef351c50b3976c314fad10238333
Roman Lytvyn <litvin.roma@gmail.com>
2014-10-15 18:18:52 +0300

    questionnaire verifier: added tests for checking by conditions and validations. KP-4378
    Former-commit-id: 3f53be1c5bf7f12080aafc3f93d0e6a8f605880d

---
+++ src/Tests/WB.Tests.Unit/BoundedContexts/Designer/QuestionnaireVerificationTests/when_verifying_questionnaire_with_question_with_incorrect_syntax_in_enablement_condition.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Machine.Specifications;
+using Main.Core.Documents;
+using Main.Core.Entities.SubEntities.Question;
+using Moq;
+using WB.Core.BoundedContexts.Designer.Implementation.Services;
+using WB.Core.BoundedContexts.Designer.Services;
+using WB.Core.BoundedContexts.Designer.ValueObjects;
+using It = Machine.Specifications.It;
+
+namespace WB.Tests.Unit.BoundedContexts.Designer.QuestionnaireVerificationTests
+{
+    internal class when_verifying_questionnaire_with_question_with_incorrect_syntax_in_enablement_condition : QuestionnaireVerifierTestsContext
+    {
+        private Establish context = () =>
+        {
+            questionnaire = CreateQuestionnaireDocumentWithOneChapter(
+                new TextQuestion
+                {
+                    PublicKey = questionId,
+                    ConditionExpression = "[hehe] &=+< 5",
+                    StataExportCaption = "var1"
+                });
+
+            var expressionProcessorGeneratorMock = new Mock<IExpressionProcessorGenerator>();
+            string resultAssembly;
+            expressionProcessorGeneratorMock.Setup(
+                _ => _.GenerateProcessorStateAssembly(Moq.It.IsAny<QuestionnaireDocument>(), out resultAssembly))
+                .Returns(new GenerationResult(){ Success = false});
+
+            verifier = CreateQuestionnaireVerifier(expressionProcessorGenerator: expressionProcessorGeneratorMock.Object);
+        };
+
+        Because of = () =>
+            resultErrors = verifier.Verify(questionnaire);
+
+        It should_return_1_error = () =>
+            resultErrors.Count().ShouldEqual(1);
+
+        It should_return_error_with_code__WB0003__ = () =>
+            resultErrors.First().Code.ShouldEqual("WB0003");
+
+        It should_return_error_with_single_reference = () =>
+            resultErrors.First().References.Count().ShouldEqual(1);
+
+        It should_return_error_referencing_with_type_of_question = () =>
+            resultErrors.First().References.Single().Type.ShouldEqual(QuestionnaireVerificationReferenceType.Question);
+
+        It should_return_error_referencing_with_specified_question_id = () =>
+            resultErrors.First().References.Single().Id.ShouldEqual(questionId);
+
+        private static IEnumerable<QuestionnaireVerificationError> resultErrors;
+        private static QuestionnaireVerifier verifier;
+        private static QuestionnaireDocument questionnaire;
+        private static Guid questionId = Guid.Parse("11111111111111111111111111111111");
+    }
+}
