42083fae7fcc99191712ea1a9b644437d1b3fe37
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2014-10-16 17:51:05 -0400

    Fix real prefilled questions on interview creation [KP-4407]
    Former-commit-id: 9fb677408d4c7f6f06fe59976a696fd3e588baea

---
+++ src/Tests/WB.Core.SharedKernels.DataCollection.Tests/InterviewTests/when_creating_interview_with_preloaded_data_with_real_question.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using Machine.Specifications;
+using Main.Core.Entities.SubEntities;
+using Microsoft.Practices.ServiceLocation;
+using Moq;
+using Ncqrs.Spec;
+using WB.Core.SharedKernels.DataCollection.Aggregates;
+using WB.Core.SharedKernels.DataCollection.DataTransferObjects.Preloading;
+using WB.Core.SharedKernels.DataCollection.Events.Interview;
+using WB.Core.SharedKernels.DataCollection.Implementation.Aggregates;
+using WB.Core.SharedKernels.DataCollection.Implementation.Providers;
+using WB.Core.SharedKernels.DataCollection.Repositories;
+using It = Machine.Specifications.It;
+
+namespace WB.Core.SharedKernels.DataCollection.Tests.InterviewTests
+{
+    internal class when_creating_interview_with_preloaded_data_with_real_question : InterviewTestsContext
+    {
+        private Establish context = () =>
+        {
+            interviewId = Guid.Parse("11111111111111111111111111111111");
+            questionnaireId = Guid.Parse("22220000000000000000000000000000");
+            userId = Guid.Parse("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+            supervisorId = Guid.Parse("BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB");
+            prefilledQuestionId = Guid.Parse("CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC");
+            prefilledQuestionAnswer = 2;
+            preloadedDataDto = new PreloadedDataDto("id",
+                new[]
+                {
+                    new PreloadedLevelDto(new decimal[0], new Dictionary<Guid, object> { { prefilledQuestionId, prefilledQuestionAnswer } }),
+                });
+            answersTime = new DateTime(2013, 09, 01);
+
+            var questionaire = Mock.Of<IQuestionnaire>(_
+                => _.GetQuestionType(prefilledQuestionId) == QuestionType.Numeric
+                    && _.HasQuestion(prefilledQuestionId) == true
+                    && _.GetFixedRosterGroups(null) == new Guid[] { Guid.NewGuid() });
+
+            var questionnaireRepository = Mock.Of<IQuestionnaireRepository>(repository
+                => repository.GetHistoricalQuestionnaire(questionnaireId, 1) == questionaire);
+
+            Mock.Get(ServiceLocator.Current)
+                .Setup(locator => locator.GetInstance<IQuestionnaireRepository>())
+                .Returns(questionnaireRepository);
+
+
+            SetupInstanceToMockedServiceLocator<IInterviewExpressionStatePrototypeProvider>(
+                CreateInterviewExpressionStateProviderStub());
+
+            eventContext = new EventContext();
+        };
+
+        private Because of = () =>
+            new Interview(interviewId, userId, questionnaireId, 1, preloadedDataDto, answersTime, supervisorId);
+
+        private Cleanup stuff = () =>
+        {
+            eventContext.Dispose();
+            eventContext = null;
+        };
+
+        private It should_raise_InterviewFromPreloadedDataCreated_event = () =>
+            eventContext.ShouldContainEvent<InterviewFromPreloadedDataCreated>();
+
+        private It should_raise_valid_TextQuestionAnswered_event = () =>
+            eventContext.ShouldContainEvent<NumericRealQuestionAnswered>(@event
+                => @event.Answer == prefilledQuestionAnswer && @event.QuestionId == prefilledQuestionId);
+
+
+        private static EventContext eventContext;
+        private static Guid interviewId;
+        private static Guid userId;
+        private static Guid questionnaireId;
+        private static PreloadedDataDto preloadedDataDto;
+        private static DateTime answersTime;
+        private static Guid supervisorId;
+        private static Guid prefilledQuestionId;
+        private static decimal prefilledQuestionAnswer;
+    }
+}
