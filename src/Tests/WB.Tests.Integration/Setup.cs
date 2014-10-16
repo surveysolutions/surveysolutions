0a254b7915dac7abed77aa3157779c24ac00822a
Dima Pasko <dimapasko@gmail.com>
2014-10-16 13:29:20 +0300

    Language, Tests: code refactoring KP-4379 Create Roslyn-specific test framework
    Former-commit-id: 1b5e1f20747132829e1826ac44a5b26a5b79ddbd

---
+++ src/Tests/WB.Tests.Integration/Setup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Practices.ServiceLocation;
+using Moq;
+
+namespace WB.Tests.Integration
+{
+    internal static class Setup
+    {
+        public static void SetupMockedServiceLocator()
+        {
+            var serviceLocatorMock = new Mock<IServiceLocator> { DefaultValue = DefaultValue.Mock };
+            ServiceLocator.SetLocatorProvider(() => serviceLocatorMock.Object);
+        }
+    }
+}
