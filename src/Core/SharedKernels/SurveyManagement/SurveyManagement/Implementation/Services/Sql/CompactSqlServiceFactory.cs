8cfa313153bf59a0ff1dcf87c17a75b1c6232511
Anastasya Kholodova <nastya.kholodova@gmail.com>
2014-10-21 16:29:12 -0400

    HQ\Export: extract all sql compact calls KP-4411
    --HG--
    branch : export-all
    
    
    Former-commit-id: da107c2e67940b33a52080f32af7a112dd5f60ec

---
+++ src/Core/SharedKernels/SurveyManagement/SurveyManagement/Implementation/Services/Sql/CompactSqlServiceFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WB.Core.Infrastructure.FileSystem;
+using WB.Core.SharedKernels.SurveyManagement.Services.Sql;
+
+namespace WB.Core.SharedKernels.SurveyManagement.Implementation.Services.Sql
+{
+    internal class CompactSqlServiceFactory : ISqlServiceFactory
+    {
+        private readonly IFileSystemAccessor fileSystemAccessor;
+
+        public CompactSqlServiceFactory(IFileSystemAccessor fileSystemAccessor)
+        {
+            this.fileSystemAccessor = fileSystemAccessor;
+        }
+
+        public ISqlService CreateSqlService(string dbPath)
+        {
+            return new CompactSqlService(dbPath, fileSystemAccessor);
+        }
+    }
+}
---
+++ src/Core/SharedKernels/SurveyManagement/SurveyManagement/Services/Sql/ISqlService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WB.Core.SharedKernels.SurveyManagement.Services.Sql
+{
+    internal interface ISqlService : IDisposable
+    {
+        void ExecuteCommand(string commandText);
+        void ExecuteCommands(IEnumerable<string> commands);
+        object[][] ExecuteReader(string query);
+    }
+}
