fe634b65b27aade023c9c15c23abdbe1e7c76e82
Anastasya Kholodova <nastya.kholodova@gmail.com>
2014-10-22 14:02:29 -0400

    Hq\Export: unit tests for delete KP-4447
    --HG--
    branch : export-all
    
    
    Former-commit-id: 73298f8d66e5835c123da4317d572ec4aa2c2600

---
+++ src/Tests/WB.Core.SharedKernels.SurveyManagement.Tests/ServiceTests/DataExport/SqlDataExportWriterTests/SqlDataExportWriterTestContext.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Machine.Specifications;
+using Main.Core.Entities.SubEntities;
+using Moq;
+using WB.Core.GenericSubdomains.Utils;
+using WB.Core.Infrastructure.FileSystem;
+using WB.Core.SharedKernels.DataCollection.ValueObjects;
+using WB.Core.SharedKernels.SurveyManagement.Factories;
+using WB.Core.SharedKernels.SurveyManagement.Implementation.Services.DataExport;
+using WB.Core.SharedKernels.SurveyManagement.Services;
+using WB.Core.SharedKernels.SurveyManagement.Services.Sql;
+using WB.Core.SharedKernels.SurveyManagement.Views.DataExport;
+using It = Moq.It;
+
+namespace WB.Core.SharedKernels.SurveyManagement.Tests.ServiceTests.DataExport.SqlDataExportWriterTests
+{
+    [Subject(typeof(SqlDataExportWriter))]
+    internal class SqlDataExportWriterTestContext
+    {
+        protected static SqlDataExportWriter CreateSqlDataExportWriter(ISqlService sqlService = null, IFileSystemAccessor fileSystemAccessor=null)
+        {
+            return new SqlDataExportWriter(Mock.Of<ICsvWriterFactory>(), fileSystemAccessor?? CreateIFileSystemAccessorMock().Object,
+                Mock.Of<ISqlServiceFactory>(_ => _.CreateSqlService(
+                    It.IsAny<string>()) == sqlService));
+        }
+
+        protected static HeaderStructureForLevel CreateHeaderStructureForLevel(string levelName = "table name", string[] referenceNames = null, ValueVector<Guid> levelScopeVector=null)
+        {
+            return new HeaderStructureForLevel()
+            {
+                LevelScopeVector = levelScopeVector?? new ValueVector<Guid>(),
+                LevelName = levelName,
+                LevelIdColumnName = "Id",
+                IsTextListScope = referenceNames != null,
+                ReferencedNames = referenceNames,
+                HeaderItems =
+                    new Dictionary<Guid, ExportedHeaderItem>
+                    {
+                        { Guid.NewGuid(), CreateExportedHeaderItem() },
+                        { Guid.NewGuid(), CreateExportedHeaderItem(QuestionType.Numeric, new[] { "a" }) }
+                    }
+            };
+        }
+
+        protected static InterviewDataExportLevelView CreateInterviewDataExportLevelView(Guid? interviewId = null, string levelName = "main level", ValueVector<Guid> levelVector =null, InterviewDataExportRecord[] records=null)
+        {
+            var interviewIdNotNull = interviewId ?? Guid.NewGuid();
+            return new InterviewDataExportLevelView(levelVector ?? new ValueVector<Guid>(), levelName,
+                records ?? new[]
+                {
+                    CreateInterviewDataExportRecord(interviewIdNotNull)
+                }, interviewIdNotNull.FormatGuid());
+        }
+
+        protected static InterviewDataExportRecord CreateInterviewDataExportRecord(Guid interviewId, string recordId=null, string[] referenceValues=null, string[] parentIds=null, ExportedQuestion[] questions=null)
+        {
+            return new InterviewDataExportRecord(interviewId, recordId ?? interviewId.FormatGuid(), referenceValues ?? new string[0],
+                parentIds ?? new string[0],
+                questions ?? new[] { CreateExportedQuestion(), CreateExportedQuestion(new[] { "1" }, QuestionType.Numeric) });
+        }
+        protected static ExportedQuestion CreateExportedQuestion(string[] answers = null, QuestionType questionType = QuestionType.Text)
+        {
+            return new ExportedQuestion() { Answers = answers ?? new[] { "a" }, QuestionType = questionType };
+        }
+
+        protected static Mock<IFileSystemAccessor> CreateIFileSystemAccessorMock()
+        {
+            var result = new Mock<IFileSystemAccessor>();
+            result.Setup(x => x.GetFileName(It.IsAny<string>())).Returns("");
+            result.Setup(x => x.CombinePath(It.IsAny<string>(), It.IsAny<string>())).Returns<string, string>(Path.Combine);
+            return result;
+        } 
+        protected static ExportedHeaderItem CreateExportedHeaderItem(QuestionType type = QuestionType.Text, string[] columnNames=null)
+        {
+            return new ExportedHeaderItem() { ColumnNames = columnNames ?? new[] { "1" }, QuestionType = type };
+        }
+    }
+
+    internal class SqlServiceTestable : ISqlService
+    {
+        private readonly Dictionary<string,string[]> tableWithColumns;
+
+        public SqlServiceTestable(Dictionary<string, string[]> tableWithColumns=null)
+        {
+            this.tableWithColumns = tableWithColumns?? new Dictionary<string, string[]>();
+        }
+
+        private readonly List<string> commandsToExecute = new List<string>();
+        public void Dispose() {}
+
+        public void ExecuteCommand(string commandText)
+        {
+            commandsToExecute.Add(commandText);
+        }
+
+        public void ExecuteCommands(IEnumerable<string> commands)
+        {
+            commandsToExecute.AddRange(commands);
+        }
+
+        public object[][] ExecuteReader(string query)
+        {
+            commandsToExecute.Add(query);
+            if (query.Contains("information_schema.tables"))
+                return tableWithColumns.Keys.Select(k => new object[] { k }).ToArray();
+
+            if (query.Contains("INFORMATION_SCHEMA.COLUMNS"))
+            {
+                var tableNameWhere = "TABLE_NAME = '";
+                var beniningOfTableName = query.IndexOf(tableNameWhere) + tableNameWhere.Length;
+                var tableName = query.Substring(beniningOfTableName, query.LastIndexOf("'") - beniningOfTableName);
+                if(!tableWithColumns.ContainsKey(tableName))
+                    return new object[0][];
+                return tableWithColumns[tableName].Select(c => new object[] { c }).ToArray();
+            }
+            return new object[0][];
+        }
+
+        public List<string> CommandsToExecute { get { return commandsToExecute; } }
+    }
+}
