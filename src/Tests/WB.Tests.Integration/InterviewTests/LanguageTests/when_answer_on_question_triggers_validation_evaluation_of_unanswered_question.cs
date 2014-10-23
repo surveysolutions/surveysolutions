4df8249e1cffba008c9d21014f36a75aa6f7d14e
Dima Pasko <dimapasko@gmail.com>
2014-10-23 16:40:30 +0300

    Language, Tests: fix test KP-4385 - Misc Corner Cases
    Former-commit-id: 9c38538106071b00dd2e0fbb068ec45129b5cfa2

---
+++ src/Tests/WB.Tests.Integration/InterviewTests/LanguageTests/when_answer_on_question_triggers_validation_evaluation_of_unanswered_question.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AppDomainToolkit;
+using Machine.Specifications;
+using Ncqrs.Spec;
+using WB.Core.SharedKernels.DataCollection.Events.Interview;
+using WB.Core.SharedKernels.DataCollection.Events.Interview.Dtos;
+
+namespace WB.Tests.Integration.InterviewTests.LanguageTests
+{
+    internal class when_answer_on_question_triggers_validation_evaluation_of_unanswered_question : InterviewTestsContext
+    {
+        Establish context = () =>
+        {
+            appDomainContext = AppDomainContext.Create();
+        };
+
+        Because of = () =>
+            results = Execute.InStandaloneAppDomain(appDomainContext.Domain, () =>
+            {
+                var questionnaireId = Guid.Parse("10000000000000000000000000000000");
+                var userId = Guid.Parse("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF");
+
+                var answeredQuestionId = Guid.Parse("11111111111111111111111111111111");
+                var dependentOnAnsweredQuestionId = Guid.Parse("22222222222222222222222222222222");
+
+                Setup.SetupMockedServiceLocator();
+
+                var questionnaireDocument = Create.QuestionnaireDocument(questionnaireId,
+                    Create.NumericIntegerQuestion(answeredQuestionId, "q1"),
+                    Create.NumericIntegerQuestion(dependentOnAnsweredQuestionId, "q2", validationExpression: "q1 + q2 > 0")
+               );
+
+                var interview = SetupInterview(questionnaireDocument, new List<object>()
+                {
+                    new AnswersDeclaredInvalid(new[]{ new Identity(dependentOnAnsweredQuestionId, new decimal[0])}),
+                    new NumericIntegerQuestionAnswered(userId, dependentOnAnsweredQuestionId, new decimal[]{}, DateTime.Now, 1)
+                });
+
+                var result = new InvokeResults();
+
+                using (var eventContext = new EventContext())
+                {
+                    interview.AnswerNumericIntegerQuestion(userId, answeredQuestionId, new decimal[] { }, DateTime.Now, 1);
+
+                    result.AnswerIsValid = GetFirstEventByType<AnswersDeclaredValid>(eventContext.Events).Questions.FirstOrDefault(q => q.Id == dependentOnAnsweredQuestionId) != null;
+                }
+
+                return result;
+            });
+
+        It should_not_enable_groupId = () =>
+            results.AnswerIsValid.ShouldBeTrue();
+
+        Cleanup stuff = () =>
+        {
+            appDomainContext.Dispose();
+            appDomainContext = null;
+        };
+
+        private static InvokeResults results;
+        private static AppDomainContext appDomainContext;
+
+        [Serializable]
+        internal class InvokeResults
+        {
+            public bool AnswerIsValid { get; set; }
+        } 
+    }
+}
