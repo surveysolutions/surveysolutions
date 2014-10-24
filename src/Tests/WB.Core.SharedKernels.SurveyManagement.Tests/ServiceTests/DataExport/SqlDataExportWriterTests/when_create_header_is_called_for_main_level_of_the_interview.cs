08f4f663e53f3e1c0f7a66bacac7311be163f15d
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2014-10-24 17:48:31 -0400

    HQ\Export: sql commands refactoring
    --HG--
    branch : export-all
    
    
    Former-commit-id: 84bd4b86f652ed3d27b477663a7a882c1de523f0

--- src/Tests/WB.Core.SharedKernels.SurveyManagement.Tests/ServiceTests/DataExport/SqlDataExportWriterTests/when_create_header_is_called_for_main_level_of_the_interview.cs
+++ src/Tests/WB.Core.SharedKernels.SurveyManagement.Tests/ServiceTests/DataExport/SqlDataExportWriterTests/when_create_header_is_called_for_main_level_of_the_interview.cs
@@ -25,10 +25,10 @@ namespace WB.Core.SharedKernels.SurveyManagement.Tests.ServiceTests.DataExport.S
             sqlServiceTestable.CommandsToExecute.Count.ShouldEqual(3);
 
         It should_command_with_level_table_creation_be_executed = () =>
-            sqlServiceTestable.CommandsToExecute[0].ShouldEqual("create table [my table] (Id nvarchar(512), [1] ntext, [a] money);");
+            sqlServiceTestable.CommandsToExecute[0].ShouldEqual("create table [my table] ([Id] nvarchar(512),[1] ntext,[a] money);");
 
         It should_command_with_index_creation_for_level_table_be_executed = () =>
-            sqlServiceTestable.CommandsToExecute[1].ShouldEqual("CREATE INDEX [idxMain] ON [my table](Id);");
+            sqlServiceTestable.CommandsToExecute[1].ShouldEqual("CREATE INDEX [idxmy table] ON [my table]([Id]);");
 
         private static SqlDataExportWriter sqlDataExportWriter;
         private static SqlServiceTestable sqlServiceTestable;
