dcbfb6e298c0b78794b4e92f047d89e58feadc25
Dima Pasko <dimapasko@gmail.com>
2014-10-27 18:13:49 +0200

    Language, Teste: fixed test when_answering_question_which_enable_group_with_mandatory_question KP-4385 Misc Corner Cases
    Former-commit-id: ad40a89198cfa2b65f82afbc42716ba37ef042a8

---
+++ src/Tests/WB.Tests.Integration/InterviewTests/LanguageTests/when_answering_question_which_enable_group_with_mandatory_question.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+using AppDomainToolkit;
+using Machine.Specifications;
+using Main.Core.Entities.Composite;
+using Ncqrs.Spec;
+using WB.Core.SharedKernels.DataCollection.Events.Interview;
+
+namespace WB.Tests.Integration.InterviewTests.LanguageTests
+{
+    internal class when_answering_question_which_enable_group_with_mandatory_question : InterviewTestsContext
+    {
+        Establish context = () =>
+        {
+            appDomainContext = AppDomainContext.Create();
+        };
+
+        Because of = () =>
+            result = Execute.InStandaloneAppDomain(appDomainContext.Domain, () =>
+            {
+                Setup.MockedServiceLocator();
+
+                var userId = Guid.Parse("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+                var mandatoryQuestionId = Guid.Parse("CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC");
+                var answeringQuestionId = Guid.Parse("11111111111111111111111111111111");
+
+                var interview = SetupInterview(
+                    Create.QuestionnaireDocument(children: new IComposite[]
+                    {
+                        Create.NumericIntegerQuestion(id: answeringQuestionId, variable: "a", isMandatory: true),
+                        Create.Group(enablementCondition: "a == 0", children: new IComposite[]
+                        {
+                            Create.NumericIntegerQuestion(id: mandatoryQuestionId, isMandatory: true)
+                        })
+                    })
+                );
+
+                using (var eventContext = new EventContext())
+                {
+                    interview.AnswerNumericIntegerQuestion(userId, answeringQuestionId, Empty.RosterVector, DateTime.Now, 0);
+
+                    return new InvokeResult
+                    {
+                        AnswersDeclaredValid = eventContext.GetSingleEvent<AnswersDeclaredValid>().Questions.Any(identity => identity.Id == mandatoryQuestionId),
+                        AnswersDeclaredInvalid = eventContext.GetSingleEvent<AnswersDeclaredInvalid>().Questions.Any(identity => identity.Id == mandatoryQuestionId)
+                    };
+                }
+            });
+
+        Cleanup stuff = () =>
+        {
+            appDomainContext.Dispose();
+            appDomainContext = null;
+        };
+
+        It should_not_raise_AnswersDeclaredValid_event = () =>
+            result.AnswersDeclaredValid.ShouldBeFalse();
+
+        It should_raise_AnswersDeclaredInvalid_event = () =>
+            result.AnswersDeclaredInvalid.ShouldBeTrue();
+
+        private static AppDomainContext appDomainContext;
+        private static InvokeResult result;
+
+        [Serializable]
+        private class InvokeResult
+        {
+            public bool AnswersDeclaredValid { get; set; }
+            public bool AnswersDeclaredInvalid { get; set; }
+        }
+    }
+}
