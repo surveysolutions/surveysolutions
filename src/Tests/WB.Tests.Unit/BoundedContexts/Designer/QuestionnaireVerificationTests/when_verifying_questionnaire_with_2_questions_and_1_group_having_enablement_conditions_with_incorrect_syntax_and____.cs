fae37044be412756757f906bfcb90e31ac350f7d
Dima Pasko <dimapasko@gmail.com>
2014-10-27 19:29:15 +0200

    Language, Tests: fix verifier tests KP-4385 Misc Corner Cases
    Former-commit-id: ba6ec17925a9bb01b72e5d389d2f1c7394d450ae

--- src/Tests/WB.Tests.Unit/BoundedContexts/Designer/QuestionnaireVerificationTests/when_verifying_questionnaire_with_2_questions_and_1_group_having_enablement_conditions_with_incorrect_syntax_and____.cs
+++ src/Tests/WB.Tests.Unit/BoundedContexts/Designer/QuestionnaireVerificationTests/when_verifying_questionnaire_with_2_questions_and_1_group_having_enablement_conditions_with_incorrect_syntax_and____.cs
@@ -5,22 +5,20 @@ using Machine.Specifications;
 using Main.Core.Documents;
 using Main.Core.Entities.SubEntities;
 using Main.Core.Entities.SubEntities.Question;
-using Moq;
 using WB.Core.BoundedContexts.Designer.Implementation.Services;
+using WB.Core.BoundedContexts.Designer.Implementation.Services.CodeGeneration;
 using WB.Core.BoundedContexts.Designer.ValueObjects;
-using WB.Core.SharedKernels.ExpressionProcessor.Services;
 using It = Machine.Specifications.It;
 
 namespace WB.Tests.Unit.BoundedContexts.Designer.QuestionnaireVerificationTests
 {
-    [Ignore("C#, KP-4385 Misc Corner Cases")]
     internal class when_verifying_questionnaire_with_2_questions_and_1_group_having_enablement_conditions_with_incorrect_syntax_and_1_question_and_1_group_having_correct_syntax :
             QuestionnaireVerifierTestsContext
     {
         private Establish context = () =>
         {
             const string InvalidExpression = "[hehe] &=+< 5";
-            const string ValidExpression = "[33333333333333333333333333333333] == 2";
+            const string ValidExpression = "var1 > 0";
 
             firstIncorrectQuestionId = Guid.Parse("11111111111111111111111111111111");
             secondIncorrectQuestionId = Guid.Parse("22222222222222222222222222222222");
@@ -51,48 +49,44 @@ namespace WB.Tests.Unit.BoundedContexts.Designer.QuestionnaireVerificationTests
                 new Group { PublicKey = correctGroupId, ConditionExpression = ValidExpression }
                 );
 
-            var expressionProcessor = Mock.Of<IExpressionProcessor>(processor
-                => processor.IsSyntaxValid(InvalidExpression) == false
-                    && processor.IsSyntaxValid(ValidExpression) == true);
-
-            verifier = CreateQuestionnaireVerifier(expressionProcessor: expressionProcessor);
+            verifier = CreateQuestionnaireVerifier(expressionProcessorGenerator: new QuestionnireExpressionProcessorGenerator());
         };
 
-        private Because of = () =>
+        Because of = () =>
             resultErrors = verifier.Verify(questionnaire);
 
-        private It should_return_3_errors = () =>
+        It should_return_3_errors = () =>
             resultErrors.Count().ShouldEqual(3);
 
-        private It should_return_errors_each_with_code__WB0003__ = () =>
+        It should_return_errors_each_with_code__WB0003__ = () =>
             resultErrors.ShouldEachConformTo(error
                 => error.Code == "WB0003");
 
-        private It should_return_errors_each_having_single_reference = () =>
+        It should_return_errors_each_having_single_reference = () =>
             resultErrors.ShouldEachConformTo(error
                 => error.References.Count() == 1);
 
-        private It should_return_error_referencing_first_incorrect_question = () =>
+        It should_return_error_referencing_first_incorrect_question = () =>
             resultErrors.ShouldContain(error
                 => error.References.Single().Type == QuestionnaireVerificationReferenceType.Question
                     && error.References.Single().Id == firstIncorrectQuestionId);
 
-        private It should_return_error_referencing_second_incorrect_question = () =>
+        It should_return_error_referencing_second_incorrect_question = () =>
             resultErrors.ShouldContain(error
                 => error.References.Single().Type == QuestionnaireVerificationReferenceType.Question
                     && error.References.Single().Id == secondIncorrectQuestionId);
 
-        private It should_return_error_referencing_incorrect_group = () =>
+        It should_return_error_referencing_incorrect_group = () =>
             resultErrors.ShouldContain(error
                 => error.References.Single().Type == QuestionnaireVerificationReferenceType.Group
                     && error.References.Single().Id == incorrectGroupId);
 
-        private It should_not_return_error_referencing_correct_question = () =>
+        It should_not_return_error_referencing_correct_question = () =>
             resultErrors.ShouldNotContain(error
                 => error.References.Single().Type == QuestionnaireVerificationReferenceType.Question
                     && error.References.Single().Id == correctQuestionId);
 
-        private It should_not_return_error_referencing_correct_group = () =>
+        It should_not_return_error_referencing_correct_group = () =>
             resultErrors.ShouldNotContain(error
                 => error.References.Single().Type == QuestionnaireVerificationReferenceType.Group
                     && error.References.Single().Id == correctGroupId);
