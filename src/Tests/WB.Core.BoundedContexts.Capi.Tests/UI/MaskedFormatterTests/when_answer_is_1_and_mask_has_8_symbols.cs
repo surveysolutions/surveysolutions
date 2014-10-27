1bb0a4a31e93b6c03adb256228bf6066d1efe377
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2014-10-27 14:36:49 -0400

    CAPI: fix KP-4497
    --HG--
    branch : hotfixes-for-2-2-3
    
    
    Former-commit-id: 8066277c17601ecc078ef1eb99a03fe18add8336

---
+++ src/Tests/WB.Core.BoundedContexts.Capi.Tests/UI/MaskedFormatterTests/when_answer_is_1_and_mask_has_8_symbols.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Machine.Specifications;
+using WB.Core.BoundedContexts.Capi.UI.MaskFormatter;
+
+namespace WB.Core.BoundedContexts.Capi.Tests.UI.MaskedFormatterTests
+{
+    [Subject(typeof(MaskedFormatter))]
+    internal class when_answer_is_1_and_mask_has_8_symbols
+    {
+        Establish context = () =>
+        {
+            maskedFormatter = new MaskedFormatter(mask);
+        };
+
+        Because of = () => formatted = maskedFormatter.FormatValue("1", ref cursorPosition);
+
+        It should_return_formatted_string = () => formatted.ShouldEqual("1_______");
+
+        It should_cursor_be_equal_to_1 = () =>
+           cursorPosition.ShouldEqual(1);
+
+        private static string mask = "########";
+        private static MaskedFormatter maskedFormatter;
+        private static string formatted;
+        private static int cursorPosition = 0;
+    }
+}
