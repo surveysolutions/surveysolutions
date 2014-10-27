2db93a91cc742981fc7c5e5f7f3c7837d5f9a6b8
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2014-10-27 14:25:00 +0200

    Tests\QuestionnaireInfoViewFactory: covered csharp-related methods with tests KP-4377
    Former-commit-id: 14959a53fde1bdf47ba3072cdd2f58a4f9adcd26

--- src/Tests/WB.Core.BoundedContexts.Designer.Tests/Setup.cs
+++ src/Tests/WB.Core.BoundedContexts.Designer.Tests/Setup.cs
@@ -1,7 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 using Main.Core.Documents;
 using Microsoft.Practices.ServiceLocation;
 using Moq;
 using WB.Core.BoundedContexts.Designer.Services;
+using WB.Core.Infrastructure.ReadSide.Repository;
+using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
 
 namespace WB.Core.BoundedContexts.Designer.Tests
 {
@@ -23,6 +29,26 @@ namespace WB.Core.BoundedContexts.Designer.Tests
                 .Returns<QuestionnaireDocument>(questionnaireDocument => questionnaireDocument);
 
             Setup.InstanceToMockedServiceLocator(questionnaireDocumentUpgrader);
+        }
+
+        public static IQueryableReadSideRepositoryReader<TEntity> QueryableReadSideRepositoryReaderByQueryResultType<TEntity, TResult>(IEnumerable<TEntity> entities)
+            where TEntity : class, IReadSideRepositoryEntity
+        {
+            var repositoryReader = Mock.Of<IQueryableReadSideRepositoryReader<TEntity>>();
+
+            Mock.Get(repositoryReader)
+                .Setup(reader => reader.Query(It.IsAny<Func<IQueryable<TEntity>, TResult>>()))
+                .Returns<Func<IQueryable<TEntity>, TResult>>(query => query.Invoke(entities.AsQueryable()));
+
+            Mock.Get(repositoryReader)
+                .Setup(reader => reader.QueryAll(It.IsAny<Expression<Func<TEntity, bool>>>()))
+                .Returns<Expression<Func<TEntity, bool>>>(condition =>
+                {
+                    Func<TEntity, bool> predicate = condition.Compile();
+                    return entities.Where(predicate);
+                });
+
+            return repositoryReader;
         }
     }
 }
