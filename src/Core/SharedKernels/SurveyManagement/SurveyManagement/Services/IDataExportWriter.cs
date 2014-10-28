5d5a95bd655fd0a0f3321e78e43e57a492b27d6d
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2014-10-28 13:57:14 -0400

    HQ\Export: unit tests for cached interviews KP-4413
    Former-commit-id: bb2cd11d46e8628b0ce32493f9016dd712343c5c

--- src/Core/SharedKernels/SurveyManagement/SurveyManagement/Services/IDataExportWriter.cs
+++ src/Core/SharedKernels/SurveyManagement/SurveyManagement/Services/IDataExportWriter.cs
@@ -9,6 +9,6 @@ namespace WB.Core.SharedKernels.SurveyManagement.Services
         void AddOrUpdateInterviewRecords(InterviewDataExportView items, string basePath);
         void CreateStructure(QuestionnaireExportStructure header, string basePath);
         void DeleteInterviewRecords(string basePath, Guid interviewId);
-        void BatchInsert(string basePath, IEnumerable<InterviewDataExportView> interviewDatas, IEnumerable<InterviewActionExportView> interviewActions);
+        void BatchInsert(string basePath, IEnumerable<InterviewDataExportView> interviewDatas, IEnumerable<InterviewActionExportView> interviewActions, IEnumerable<Guid> interviewsForDelete);
     }
 }
---
+++ src/Tests/WB.Core.SharedKernels.SurveyManagement.Tests/ServiceTests/DataExport/FileBasedDataExportServiceTests/when_AddExportedDataByInterview_is_called_and_interview_is_deleted.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Machine.Specifications;
+using Moq;
+using WB.Core.SharedKernels.SurveyManagement.Implementation.Services.DataExport;
+using WB.Core.SharedKernels.SurveyManagement.Services;
+using WB.Core.SharedKernels.SurveyManagement.Services.Export;
+using WB.Core.SharedKernels.SurveyManagement.Views.DataExport;
+using WB.Core.SharedKernels.SurveyManagement.Views.Interview;
+using It = Machine.Specifications.It;
+
+namespace WB.Core.SharedKernels.SurveyManagement.Tests.ServiceTests.DataExport.FileBasedDataExportServiceTests
+{
+    internal class when_AddExportedDataByInterview_is_called_and_interview_is_deleted : FileBasedDataExportServiceTestContext
+    {
+        Establish context = () =>
+        {
+            dataExportWriterMock=new Mock<IDataExportWriter>();
+            var filebaseExportRouteServiceMock = new Mock<IFilebaseExportDataAccessor>();
+            filebaseExportRouteServiceMock.Setup(x => x.GetFolderPathOfDataByQuestionnaireOrThrow(Moq.It.IsAny<Guid>(), Moq.It.IsAny<long>()))
+                .Throws<InterviewDataExportException>();
+            fileBasedDataExportRepositoryWriter =
+                CreateFileBasedDataExportService(filebaseExportDataAccessor: filebaseExportRouteServiceMock.Object, interviewData: new InterviewData(){IsDeleted = true}, dataExportWriter:dataExportWriterMock.Object);
+        };
+
+        Because of = () =>
+            fileBasedDataExportRepositoryWriter.AddExportedDataByInterview(Guid.NewGuid());
+
+        It should_AddOrUpdateInterviewRecords_be_never_called = () =>
+            dataExportWriterMock.Verify(x => x.AddOrUpdateInterviewRecords(Moq.It.IsAny<InterviewDataExportView>(), Moq.It.IsAny<string>()), Times.Never);
+
+        private static FileBasedDataExportRepositoryWriter fileBasedDataExportRepositoryWriter;
+        private static Mock<IDataExportWriter> dataExportWriterMock;
+    }
+}
