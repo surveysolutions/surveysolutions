07e76b529da20ce9d0d7ffd5878c6d81155fa195
Roman Lytvyn <none@none>
2014-10-29 16:13:35 +0200

    capi:  changed red border color to text color for Wizard: Sync Point field. KP-4502
    Former-commit-id: f39b67c31d5b0e3b506b1a04ba86eef47c4e15a5

--- src/UI/Capi/WB.UI.Capi/Utils/BooleanToTextColorValueConverter.cs
+++ src/UI/Capi/WB.UI.Capi/Utils/BooleanToTextColorValueConverter.cs
@@ -3,10 +3,10 @@ using Cirrious.MvvmCross.Plugins.Color;
 
 namespace WB.UI.Capi.Utils
 {
-    public class BackgroundColorValueConverter : MvxColorValueConverter
+    public class BooleanToTextColorValueConverter : MvxColorValueConverter
     {
-        private static readonly MvxColor TrueBgColor = new MvxColor(0xFF000000);
-        private static readonly MvxColor FalseBgColor = new MvxColor(0xFFFF0000);
+        private static readonly MvxColor TrueBgColor = MvxColors.Black;
+        private static readonly MvxColor FalseBgColor = MvxColors.Red;
 
         protected override MvxColor Convert(object value, object parameter, System.Globalization.CultureInfo culture)
         {
