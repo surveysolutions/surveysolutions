e211f30d63455fa296514b45c0c20057fd0599a2
Slava Scherbak <slava.scherbak@gmail.com>
2014-11-03 13:10:52 -0500

    Pdf: added tests for cloning questions and questionnaires KP-4534
    Former-commit-id: cc1a9e4988d44a34c3ecd3f8fe3db7e67201c08c

--- src/Tests/WB.Core.BoundedContexts.Designer.Tests/PdfQuestionnaireDenormalizerTests/when_handling_QuestionCloned_event_with_validation_expression.cs
+++ src/Tests/WB.Core.BoundedContexts.Designer.Tests/PdfQuestionnaireDenormalizerTests/when_handling_QuestionCloned_event_with_validation_expression.cs
@@ -1,23 +1,29 @@
-﻿using System;
+using System;
 using Machine.Specifications;
-using Main.Core.Documents;
 using Main.Core.Entities.SubEntities;
+using Main.Core.Events.Questionnaire;
 using Moq;
 using Ncqrs.Eventing.ServiceModel.Bus;
-using WB.Core.BoundedContexts.Designer.Events.Questionnaire;
 using WB.Core.BoundedContexts.Designer.Views.Questionnaire.Pdf;
 using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
 using It = Machine.Specifications.It;
 
 namespace WB.Core.BoundedContexts.Designer.Tests.PdfQuestionnaireDenormalizerTests
 {
-    internal class when_handling_TextListQuestionAdded_event : PdfQuestionnaireDenormalizerTestContext
+    internal class when_handling_QuestionCloned_event_with_validation_expression : PdfQuestionnaireDenormalizerTestContext
     {
         Establish context = () =>
         {
             var parentGroupId = Guid.Parse("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
 
-            @event = CreatePublishedEvent(new TextListQuestionAdded() { PublicKey = questionId, GroupId = parentGroupId, QuestionText = questionTitle });
+            @event = CreatePublishedEvent(new QuestionCloned()
+            {
+                QuestionType = QuestionType.Text,
+                PublicKey = questionId, 
+                GroupPublicKey = parentGroupId, 
+                QuestionText = "someTitle",
+                ValidationExpression = "some expression"
+            });
 
             pdfQuestionnaireDocument = CreatePdfQuestionnaire(CreateGroup(parentGroupId));
 
@@ -29,16 +35,18 @@ namespace WB.Core.BoundedContexts.Designer.Tests.PdfQuestionnaireDenormalizerTes
         Because of = () =>
             denormalizer.Handle(@event);
 
-        It should_set_TextList_question_type_be_QuestionType_TextList = () =>
-            pdfQuestionnaireDocument.GetEntityById<PdfQuestionView>(questionId).QuestionType.ShouldEqual(QuestionType.TextList);
+        It should_set_Text_question_type_for_cloned_question = () =>
+            pdfQuestionnaireDocument.GetEntityById<PdfQuestionView>(questionId).QuestionType.ShouldEqual(QuestionType.Text);
 
-        It should_set_TextList_question_title_be_equal_to_passed_title = () =>
-            pdfQuestionnaireDocument.GetEntityById<PdfQuestionView>(questionId).Title.ShouldEqual(questionTitle);
+        It should_set__someTitle__as_title_for_cloned_question = () =>
+            pdfQuestionnaireDocument.GetEntityById<PdfQuestionView>(questionId).Title.ShouldEqual("someTitle");
+
+        It should_set__some_expression___as_validation_for_cloned_question = () =>
+            pdfQuestionnaireDocument.GetEntityById<PdfQuestionView>(questionId).ValidationExpression.ShouldEqual("some expression");
 
         private static PdfQuestionnaireDenormalizer denormalizer;
-        private static IPublishedEvent<TextListQuestionAdded> @event;
+        private static IPublishedEvent<QuestionCloned> @event;
         private static Guid questionId = Guid.Parse("11111111111111111111111111111111");
-        private static string questionTitle = "someTitle";
         private static PdfQuestionnaireView pdfQuestionnaireDocument;
     }
 }
