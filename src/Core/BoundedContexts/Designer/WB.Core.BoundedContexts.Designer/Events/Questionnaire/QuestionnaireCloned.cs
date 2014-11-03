84806e2771552dfbe9d0c0cbfa11b7eafa57b8b5
Slava Scherbak <slava.scherbak@gmail.com>
2014-11-03 13:10:52 -0500

    Pdf: added tests for cloning questions and questionnaires KP-4534
    Former-commit-id: cc1a9e4988d44a34c3ecd3f8fe3db7e67201c08c

--- src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Events/Questionnaire/QuestionnaireCloned.cs
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Events/Questionnaire/QuestionnaireCloned.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Main.Core.Documents;
 
 namespace WB.Core.BoundedContexts.Designer.Events.Questionnaire
