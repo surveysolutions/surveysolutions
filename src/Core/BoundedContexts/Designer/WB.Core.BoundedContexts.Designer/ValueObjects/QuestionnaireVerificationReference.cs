7c7e313c8208af2b622a9e94c2e3d4b1419627f1
Andrii Kozhyn <hedgic@gmail.com>
2014-11-04 10:28:25 +0200

    KP-4528: Fixed unit tests and build and feature
    Former-commit-id: e60e3120ffe822efa05ca48fdca2b176be0e34b0

--- src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/ValueObjects/QuestionnaireVerificationReference.cs
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/ValueObjects/QuestionnaireVerificationReference.cs
@@ -17,5 +17,26 @@ namespace WB.Core.BoundedContexts.Designer.ValueObjects
         public QuestionnaireVerificationReferenceType Type { get; private set; }
         public Guid Id { get; private set; }
         public string ItemId { get; private set; }
+
+        protected bool Equals(QuestionnaireVerificationReference other)
+        {
+            return this.Id.Equals(other.Id) && this.Type == other.Type;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((QuestionnaireVerificationReference)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (this.Id.GetHashCode() * 397) ^ (int)this.Type;
+            }
+        }
     }
 }
