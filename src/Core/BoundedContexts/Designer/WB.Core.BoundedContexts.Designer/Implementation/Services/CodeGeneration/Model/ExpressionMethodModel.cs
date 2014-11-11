0f93199c5c5d3e7f1cb3b8b9500efe19d7b5785e
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2014-11-10 19:20:02 -0500

    Fix verification timings [KP-4555]
    --HG--
    branch : hotfix-3-0-1
    
    
    Former-commit-id: ec6ec39265aaba494b4de59175ec5fffb25345f8

---
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Implementation/Services/CodeGeneration/Model/ExpressionMethodModel.cs
@@ -0,0 +1,10 @@
+﻿namespace WB.Core.BoundedContexts.Designer.Implementation.Services.CodeGeneration.Model
+{
+    public class ExpressionMethodModel
+    {
+        public string GeneratedClassName { set; get; }
+        public string GeneratedMethodName { set; get; }
+
+        public string ExpressionString { set; get; }
+    }
+}
