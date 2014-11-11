0bbd37648a6e69b1862abb84833de1454223cefc
Anatoliy 'TLK' Kolesnick <none@none>
2014-11-11 21:17:15 +0200

    CommandBus: registered mapped to constructor commands from designer bounded context (using hand-written tool), command bus now supports initializers KP-4337
    Former-commit-id: 1060b269d22311a972ba4819f18dc63a5edfdae3

--- src/Core/Infrastructure/WB.Core.Infrastructure/CommandBus/CommandRegistry.cs
+++ src/Core/Infrastructure/WB.Core.Infrastructure/CommandBus/CommandRegistry.cs
@@ -6,50 +6,67 @@ namespace WB.Core.Infrastructure.CommandBus
 {
     public static class CommandRegistry
     {
-        private static readonly Dictionary<string, HandlerDescriptor> Handlers = new Dictionary<string, HandlerDescriptor>();
+        #region Handlers
 
-        public class AggregateRegistry<TAggregate>
-            where TAggregate : IAggregateRoot
-        {
-            public AggregateRegistry<TAggregate> Add<TCommand>(Func<TCommand, Guid> aggregateRootIdResolver, Action<TCommand, TAggregate> commandHandler)
-                where TCommand : ICommand
-            {
-                CommandRegistry.Add(aggregateRootIdResolver, commandHandler);
-                return this;
-            }
-        }
+        private static readonly Dictionary<string, HandlerDescriptor> Handlers = new Dictionary<string, HandlerDescriptor>();
 
         private class HandlerDescriptor
         {
-            public HandlerDescriptor(Type aggregateType, Func<ICommand, Guid> idResolver, Action<ICommand, IAggregateRoot> handler)
+            public HandlerDescriptor(Type aggregateType, bool isConstructor, Func<ICommand, Guid> idResolver, Action<ICommand, IAggregateRoot> handler)
             {
+                this.IsConstructor = isConstructor;
                 this.AggregateType = aggregateType;
                 this.IdResolver = idResolver;
                 this.Handler = handler;
             }
 
             public Type AggregateType { get; private set; }
+            public bool IsConstructor { get; set; }
             public Func<ICommand, Guid> IdResolver { get; private set; }
             public Action<ICommand, IAggregateRoot> Handler { get; private set; }
         }
 
-        public static AggregateRegistry<TAggregate> For<TAggregate>()
+        #endregion
+
+        #region Fluent setup
+
+        public class AggregateSetup<TAggregate>
             where TAggregate : IAggregateRoot
         {
-            return new AggregateRegistry<TAggregate>();
+            public AggregateSetup<TAggregate> InitializedWith<TCommand>(Func<TCommand, Guid> aggregateRootIdResolver, Action<TCommand, TAggregate> commandHandler)
+                where TCommand : ICommand
+            {
+                Register(aggregateRootIdResolver, commandHandler, isConstructor: true);
+                return this;
+            }
+
+            public AggregateSetup<TAggregate> Handles<TCommand>(Func<TCommand, Guid> aggregateRootIdResolver, Action<TCommand, TAggregate> commandHandler)
+                where TCommand : ICommand
+            {
+                Register(aggregateRootIdResolver, commandHandler, isConstructor: false);
+                return this;
+            }
         }
 
-        public static void Add<TCommand, TAggregate>(Func<TCommand, Guid> aggregateRootIdResolver, Action<TCommand, TAggregate> commandHandler)
-            where TCommand : ICommand
+        public static AggregateSetup<TAggregate> Setup<TAggregate>()
             where TAggregate : IAggregateRoot
         {
-            string commandName = typeof(TCommand).Name;
+            return new AggregateSetup<TAggregate>();
+        }
+
+        #endregion
+
+        private static void Register<TCommand, TAggregate>(Func<TCommand, Guid> aggregateRootIdResolver, Action<TCommand, TAggregate> commandHandler, bool isConstructor)
+            where TCommand : ICommand where TAggregate : IAggregateRoot
+        {
+            string commandName = typeof (TCommand).Name;
 
             if (Handlers.ContainsKey(commandName))
                 throw new ArgumentException(string.Format("Command {0} is already registered.", commandName));
 
             Handlers.Add(commandName, new HandlerDescriptor(
-                typeof(TAggregate),
+                typeof (TAggregate),
+                isConstructor,
                 command => aggregateRootIdResolver.Invoke((TCommand) command),
                 (command, aggregate) => commandHandler.Invoke((TCommand) command, (TAggregate) aggregate)));
         }
@@ -59,17 +76,22 @@ namespace WB.Core.Infrastructure.CommandBus
             return Handlers.ContainsKey(command.GetType().Name);
         }
 
-        public static Type GetAggregateRootType(ICommand command)
+        internal static Type GetAggregateRootType(ICommand command)
         {
             return Handlers[command.GetType().Name].AggregateType;
         }
 
-        public static Func<ICommand, Guid> GetAggregateRootIdResolver(ICommand command)
+        internal static bool IsConstructor(ICommand command)
+        {
+            return Handlers[command.GetType().Name].IsConstructor;
+        }
+
+        internal static Func<ICommand, Guid> GetAggregateRootIdResolver(ICommand command)
         {
             return Handlers[command.GetType().Name].IdResolver;
         }
 
-        public static Action<ICommand, IAggregateRoot> GetCommandHandler(ICommand command)
+        internal static Action<ICommand, IAggregateRoot> GetCommandHandler(ICommand command)
         {
             return Handlers[command.GetType().Name].Handler;
         }
--- src/Tools/WB.Tools.CommandMapper/Program.cs
+++ src/Tools/WB.Tools.CommandMapper/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using Ncqrs.Commanding.CommandExecution.Mapping.Attributes;
@@ -17,46 +18,69 @@ namespace WB.Tools.CommandMapper
                 .Assembly
                 .GetTypes();
 
-            IEnumerable<Tuple<Type, CustomAttributeData>> commandsAndAttributes =
-                Enumerable.Concat(
+            IEnumerable<Tuple<Type, CustomAttributeData, bool>> commandsAndAttributes =
+                ConcatMany(
+                    types
+                        .Where(HasAttribute<MapsToAggregateRootConstructorAttribute>)
+                        .Select(type => Tuple.Create(type, GetAttribute<MapsToAggregateRootConstructorAttribute>(type), true)),
                     types
                         .Where(HasAttribute<MapsToAggregateRootMethodAttribute>)
-                        .Select(type => Tuple.Create(type, GetAttribute<MapsToAggregateRootMethodAttribute>(type))),
+                        .Select(type => Tuple.Create(type, GetAttribute<MapsToAggregateRootMethodAttribute>(type), false)),
                     types
                         .Where(HasAttribute<MapsToAggregateRootMethodOrConstructorAttribute>)
-                        .Select(type => Tuple.Create(type, GetAttribute<MapsToAggregateRootMethodOrConstructorAttribute>(type))));
+                        .Select(type => Tuple.Create(type, GetAttribute<MapsToAggregateRootMethodOrConstructorAttribute>(type), true)));
 
             var descriptors = commandsAndAttributes
                 .Select(x => new
                 {
                     Command = x.Item1,
-                    Aggregate = (Type) x.Item2.ConstructorArguments.First().Value,
-                    Method = (string) x.Item2.ConstructorArguments.Last().Value,
+                    Aggregate = GetAggregateType(x.Item2),
+                    Method = GetMethodName(x.Item1, x.Item2),
+                    IsConstructor = x.Item3,
                 })
                 .Select(x => new
                 {
                     Aggregate = x.Aggregate.Name,
                     x.Method,
+                    x.IsConstructor,
                     Command = x.Command.Name,
                     IdProperty = GetAggregateIdProperty(x.Command),
                     Parameters = GetParams(x.Aggregate, x.Method, x.Command)
                 })
-                .OrderBy(x => x.Command)
-                .GroupBy(x => x.Aggregate);
+                .OrderByDescending(x => x.IsConstructor)
+                .ThenBy(x => x.Method)
+                .GroupBy(x => x.Aggregate)
+                .OrderBy(x => x.Key);
 
             foreach (var aggregate in descriptors)
             {
                 Console.WriteLine();
-                Console.WriteLine(@"            CommandRegistry.For<Aggregates.{0}>()", aggregate.Key);
+                Console.WriteLine(@"            CommandRegistry");
+                Console.WriteLine(@"                .Setup<Aggregates.{0}>()", aggregate.Key);
 
                 foreach (var descriptor in aggregate)
                 {
-                    Console.Write(@"                .Add<{0}>(", descriptor.Command);
+                    Console.Write(@"                .{1}<{0}>(", descriptor.Command, descriptor.IsConstructor ? "InitializedWith" : "Handles");
                     Console.Write(@"command => command.{0}, ", descriptor.IdProperty);
                     Console.WriteLine(@"(command, aggregate) => aggregate.{0}({1}))", descriptor.Method, string.Join(", ", descriptor.Parameters.Select(p => "command." + p)));
                 }
                 Console.WriteLine(@"            ;");
             }
+        }
+
+        private static string GetMethodName(Type command, CustomAttributeData attribute)
+        {
+            return attribute.ConstructorArguments.Count == 2 ? (string) attribute.ConstructorArguments.Last().Value : command.Name.Replace("Command", "");
+        }
+
+        private static Type GetAggregateType(CustomAttributeData attribute)
+        {
+            return (Type) attribute.ConstructorArguments.First().Value;
+        }
+
+        private static IEnumerable<T> ConcatMany<T>(IEnumerable<T> first, IEnumerable<T> second, IEnumerable<T> third)
+        {
+            return first.Concat(second).Concat(third);
         }
 
         private static CustomAttributeData GetAttribute<TAttribute>(Type type)
@@ -66,17 +90,34 @@ namespace WB.Tools.CommandMapper
 
         private static IEnumerable<string> GetParams(Type aggregate, string methodName, Type command)
         {
-            MethodInfo method = aggregate.GetMethod(methodName);
-            
-            IEnumerable<string> properties = command.GetProperties().Select(property => property.Name).ToList();
-            IEnumerable<string> parameters = method.GetParameters().Select(parameter => parameter.Name).ToList();
+            IEnumerable<string> properties = command
+                .GetProperties()
+                .Select(property => property.Name)
+                .Where(property => property != "CommandIdentifier" && property != "KnownVersion")
+                .ToList();
 
-            return parameters.Select(parameter => properties.Single(property => string.Equals(parameter, property, StringComparison.InvariantCultureIgnoreCase)));
+            MethodInfo method = aggregate.GetMethod(methodName);
+
+            if (method != null)
+            {
+                IEnumerable<string> parameters = method
+                    .GetParameters()
+                    .Select(parameter => parameter.Name)
+                    .ToList();
+
+                return parameters.Select(parameter => properties.Single(property => string.Equals(parameter, property, StringComparison.InvariantCultureIgnoreCase)));
+            }
+            else
+            {
+                return properties;
+            }
         }
 
         private static string GetAggregateIdProperty(Type command)
         {
-            return command.GetProperties().Single(HasAttribute<AggregateRootIdAttribute>).Name;
+            var idProperty = command.GetProperties().SingleOrDefault(HasAttribute<AggregateRootIdAttribute>);
+            
+            return idProperty != null ? idProperty.Name : "?";
         }
 
         private static bool HasAttribute<TAttribute>(PropertyInfo property)
