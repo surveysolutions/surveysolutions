1353e381b67d611957f966104c0d568a83c386cf
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2014-11-11 22:20:49 -0500

    Page for generated expression class [KP-4464]
    Former-commit-id: 0faf20163e9ce62e5fc2581448110fd960c9ce94

--- src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Services/IExpressionProcessorGenerator.cs
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Services/IExpressionProcessorGenerator.cs
@@ -1,9 +1,13 @@
-﻿using Main.Core.Documents;
+﻿using System.Collections.Generic;
+using Main.Core.Documents;
 
 namespace WB.Core.BoundedContexts.Designer.Services
 {
     public interface IExpressionProcessorGenerator
     {
         GenerationResult GenerateProcessorStateAssembly(QuestionnaireDocument questionnaire, out string generatedAssembly);
+        Dictionary<string, string> GenerateProcessorStateClasses(QuestionnaireDocument questionnaire);
+        string GenerateProcessorStateSingleClass(QuestionnaireDocument questionnaire);
+
     }
 }
---
+++ src/UI/Designer/WB.UI.Designer/Api/ExpressionGenerationController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Web.Http;
+using Raven.Abstractions.Logging;
+using WB.Core.BoundedContexts.Designer.Services;
+using WB.Core.BoundedContexts.Designer.Views.Questionnaire.Edit;
+using WB.Core.GenericSubdomains.Logging;
+using WB.Core.Infrastructure.ReadSide;
+using WB.UI.Shared.Web.Filters;
+
+namespace WB.UI.Designer.Api
+{
+    [LocalOrDevelopmentAccessOnly]
+    public class ExpressionGenerationController : ApiController
+    {
+        private IExpressionProcessorGenerator expressionProcessorGenerator;
+        private readonly ILogger logger;
+        private readonly IViewFactory<QuestionnaireViewInputModel, QuestionnaireView> questionnaireViewFactory;
+
+        public ExpressionGenerationController(ILogger logger, IExpressionProcessorGenerator expressionProcessorGenerator, IViewFactory<QuestionnaireViewInputModel, QuestionnaireView> questionnaireViewFactory)
+        {
+            this.logger = logger;
+            this.expressionProcessorGenerator = expressionProcessorGenerator;
+            this.questionnaireViewFactory = questionnaireViewFactory;
+        }
+
+        [HttpGet]
+        public HttpResponseMessage GenerateExpressionsClass(Guid id)
+        {
+            var questionnaire = GetQuestionnaire(id).Source;
+            string generated = expressionProcessorGenerator.GenerateProcessorStateSingleClass(questionnaire);
+
+            return Request.CreateResponse(HttpStatusCode.OK, generated);
+        }
+
+        [HttpGet]
+        public HttpResponseMessage GetAllClasses(Guid id)
+        {
+            var questionnaire = GetQuestionnaire(id).Source;
+            var generated = expressionProcessorGenerator.GenerateProcessorStateClasses(questionnaire);
+            
+            var resultBuilder =new StringBuilder();
+            
+            foreach (KeyValuePair<string, string> keyValuePair in generated)
+            {
+                resultBuilder.AppendLine(string.Format("//{0}", keyValuePair.Key));
+                resultBuilder.AppendLine(keyValuePair.Value);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, resultBuilder.ToString());
+        }
+
+        [HttpGet]
+        public HttpResponseMessage GetAssembly(Guid id)
+        {
+            //do async
+            
+            var questionnaire = GetQuestionnaire(id).Source;
+            string assembly;
+            var generated = expressionProcessorGenerator.GenerateProcessorStateAssembly(questionnaire, out assembly);
+            if (generated.Success)
+            {
+                var response = new HttpResponseMessage(HttpStatusCode.OK)
+                {
+                    Content = new  ByteArrayContent(Convert.FromBase64String(assembly))
+                };
+
+                response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/x-msdownload");
+                response.Content.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment"); 
+                response.Content.Headers.ContentDisposition.FileName = string.Format("expressions-{0}.dll", id);
+                return response;
+            }
+            else
+            {
+                var errorLocations = generated.Diagnostics.Select(x => x.Location).Distinct().Aggregate("Errors: \r\n", (current, location) => current + (current + "\r\n" + location));
+
+                return Request.CreateErrorResponse(HttpStatusCode.PreconditionFailed, errorLocations);
+            }
+        }
+
+        private QuestionnaireView GetQuestionnaire(Guid id)
+        {
+            var questionnaire = this.questionnaireViewFactory.Load(new QuestionnaireViewInputModel(id));
+
+            if (questionnaire == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            return questionnaire;
+        }
+
+    }
+
+
+
+}
