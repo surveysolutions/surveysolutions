7589d3708fe13863913b5169a2e992809f29dc76
Slava Scherbak <slava.scherbak@gmail.com>
2014-11-12 16:53:48 -0500

    Portable: merged WB.Core.BoundedContexts.Capi.Synchronization into WB.Core.BoundedContexts.Capi with tests KP-4496
    Former-commit-id: 297e2a9b8f1dc90a4d7b12307893d008790850fc

---
+++ src/Tests/WB.Core.BoundedContexts.Capi.Tests/CapiDataSynchronizationServiceTests/CapiDataSynchronizationServiceTestContext.cs
@@ -0,0 +1,36 @@
+﻿using Moq;
+using WB.Core.BoundedContexts.Capi.ChangeLog;
+using WB.Core.BoundedContexts.Capi.Implementation.Services;
+using WB.Core.BoundedContexts.Capi.Services;
+using WB.Core.BoundedContexts.Capi.Views.Login;
+using WB.Core.GenericSubdomains.Logging;
+using WB.Core.Infrastructure.CommandBus;
+using WB.Core.Infrastructure.ReadSide;
+using WB.Core.SharedKernel.Utils.Compression;
+using WB.Core.SharedKernel.Utils.Serialization;
+using WB.Core.SharedKernels.DataCollection.Implementation.Accessors;
+using WB.Core.SharedKernels.DataCollection.Repositories;
+
+namespace WB.Core.BoundedContexts.Capi.Tests.CapiDataSynchronizationServiceTests
+{
+    internal class CapiDataSynchronizationServiceTestContext
+    {
+        protected static CapiDataSynchronizationService CreateCapiDataSynchronizationService(IChangeLogManipulator changeLogManipulator,
+            ICommandService commandService = null, IJsonUtils jsonUtils = null,
+            IViewFactory<LoginViewInput, LoginView> loginViewFactory = null,
+            IPlainQuestionnaireRepository plainQuestionnaireRepository = null, ICapiSynchronizationCacheService capiSynchronizationCacheService = null,
+            ICapiCleanUpService capiCleanUpService = null, IQuestionnaireAssemblyFileAccessor questionnareAssemblyFileAccessor = null)
+        {
+            var mockOfCompressor = new Mock<IStringCompressor>();
+            mockOfCompressor.Setup(x => x.DecompressString(Moq.It.IsAny<string>())).Returns<string>(s => s);
+
+            return new CapiDataSynchronizationService(changeLogManipulator, commandService ?? Mock.Of<ICommandService>(),
+                loginViewFactory ?? Mock.Of<IViewFactory<LoginViewInput, LoginView>>(),
+                plainQuestionnaireRepository ?? Mock.Of<IPlainQuestionnaireRepository>(),
+                capiCleanUpService ?? Mock.Of<ICapiCleanUpService>(),
+                Mock.Of<ILogger>(), capiSynchronizationCacheService ?? Mock.Of<ICapiSynchronizationCacheService>(), 
+                mockOfCompressor.Object, jsonUtils ?? Mock.Of<IJsonUtils>(),
+                questionnareAssemblyFileAccessor ?? Mock.Of<IQuestionnaireAssemblyFileAccessor>());
+        }
+    }
+}
---
+++ src/Tests/WB.Core.BoundedContexts.Capi.Tests/SyncPackageRestoreServiceTests/when_synchronization_item_doesnt_exist.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Machine.Specifications;
+using WB.Core.BoundedContexts.Capi.Implementation.Services;
+
+namespace WB.Core.BoundedContexts.Capi.Tests.SyncPackageRestoreServiceTests
+{
+    internal class when_synchronization_item_doesnt_exist : SyncPackageRestoreServiceTestContext
+    {
+        Establish context = () =>
+        {
+            syncPackageRestoreService = CreateSyncPackageRestoreService();
+        };
+        
+        Because of = () => result = syncPackageRestoreService.CheckAndApplySyncPackage(Guid.NewGuid());
+        
+        It should_result_be_true = () => result.ShouldBeTrue();
+
+        private static SyncPackageRestoreService syncPackageRestoreService;
+        private static bool result;
+    }
+}
