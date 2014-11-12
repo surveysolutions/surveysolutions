dbeabc12caf4068bf3f546ade30f9fd4ac360f35
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2014-11-12 16:51:08 +0200

    HQ,SV: switched to new command bus, registered commands from DataCollection shared kernel KP-4337
    Former-commit-id: fa91ba6c7071c8844100306e1afc3c2e5f794bf6

--- src/Core/SharedKernels/DataCollection/DataCollection/Events/User/UserLockedBySupervisor.cs
+++ src/Core/SharedKernels/DataCollection/DataCollection/Events/User/UserLockedBySupervisor.cs
@@ -3,13 +3,5 @@
 namespace WB.Core.SharedKernels.DataCollection.Events.User
 {
     [Serializable]
-    public class UserLockedBySupervisor 
-    {
-        public Guid UserId { get; private set; }
-
-        public UserLockedBySupervisor(Guid userId)
-        {
-            this.UserId = userId;
-        }
-    }
+    public class UserLockedBySupervisor {}
 }
