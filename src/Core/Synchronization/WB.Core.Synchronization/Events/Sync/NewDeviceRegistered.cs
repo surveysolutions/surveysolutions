1be96e3999914c7e22ee3b0ff168528b8ffb0139
Slava Scherbak <slava.scherbak@gmail.com>
2014-11-13 13:38:15 -0500

    Portable: moved some files from Main.Core to other contexts KP-4496
    Former-commit-id: 0499b4b36ff35d40aaa2d545d109e9b7c4005c5f

---
+++ src/Core/Synchronization/WB.Core.Synchronization/Events/Sync/NewDeviceRegistered.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Ncqrs.Eventing.Storage;
+
+// ReSharper disable once CheckNamespace
+namespace Main.Core.Events.Synchronization
+{
+    /// <summary>
+    /// The new device registered
+    /// </summary>
+    [Serializable]
+    [EventName("RavenQuestionnaire.Core:Events:NewDeviceRegistered")]
+    public class NewDeviceRegistered
+    {
+        #region PublicProperties
+
+        /// <summary>
+        /// Gets or sets TabletId.
+        /// </summary>
+        public Guid IdForRegistration { get; set; }
+
+        /// <summary>
+        /// Gets or sets Registrator.
+        /// </summary>
+        public Guid Registrator { get; set; }
+
+        /// <summary>
+        /// Gets or sets PublicKey.
+        /// </summary>
+        public byte[] SecretKey { get; set; }
+
+        /// <summary>
+        /// Gets or sets Description.
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Gets or sets the creation date.
+        /// </summary>
+        public DateTime RegisteredDate { get; set; }
+
+        #endregion
+    }
+}
