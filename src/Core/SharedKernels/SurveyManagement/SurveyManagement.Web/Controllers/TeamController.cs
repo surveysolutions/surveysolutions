73a3f3e0b0e16538f59c701e17fe4d7c743b005f
Roman Lytvyn <none@none>
2014-11-14 12:45:08 +0200

    hq/sv: changed manage account page. KP-4594
    --HG--
    branch : hotfix-3-0-3
    
    
    Former-commit-id: 005981510b51fc7c051dd5a8a426ca8b8147c82e

--- src/Core/SharedKernels/SurveyManagement/SurveyManagement.Web/Controllers/TeamController.cs
+++ src/Core/SharedKernels/SurveyManagement/SurveyManagement.Web/Controllers/TeamController.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using Main.Core.Entities.SubEntities;
-using Main.Core.Utility;
 using Main.Core.View;
 using Ncqrs.Commanding.ServiceModel;
 using WB.Core.GenericSubdomains.Logging;
@@ -14,8 +13,8 @@ namespace WB.Core.SharedKernels.SurveyManagement.Web.Controllers
 {
     public class TeamController : BaseController
     {
-        private readonly IViewFactory<UserViewInputModel, UserView> userViewFactory;
-        private readonly IPasswordHasher passwordHasher;
+        protected readonly IViewFactory<UserViewInputModel, UserView> userViewFactory;
+        protected readonly IPasswordHasher passwordHasher;
 
         public TeamController(ICommandService commandService, 
                               IGlobalInfoProvider globalInfo, 
@@ -49,7 +48,7 @@ namespace WB.Core.SharedKernels.SurveyManagement.Web.Controllers
             CreateUser(user: supervisor, role: UserRoles.Supervisor);
         }
 
-        protected void UpdateSupervisorOrInterviewer(UserView user, UserEditModel editModel)
+        protected void UpdateAccount(UserView user, UserEditModel editModel)
         {
             this.CommandService.Execute(new ChangeUserCommand(publicKey: user.PublicKey, email: editModel.Email,
                 roles: user.Roles.ToArray(),
