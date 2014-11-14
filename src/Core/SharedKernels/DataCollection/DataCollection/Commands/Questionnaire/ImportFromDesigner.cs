d2244a764a9a4886d1777c1c20a0c00ac590e72c
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2014-11-14 15:51:42 +0200

    CommandBus: implemented most readable commands registration, used DataCollection questionnaire as example KP-4337
    Former-commit-id: 06722f77755b2c02e9eb1aa8e4952db136b65f5e

--- src/Core/SharedKernels/DataCollection/DataCollection/Commands/Questionnaire/ImportFromDesigner.cs
+++ src/Core/SharedKernels/DataCollection/DataCollection/Commands/Questionnaire/ImportFromDesigner.cs
@@ -1,20 +1,18 @@
 using System;
 using Main.Core.Documents;
-using Ncqrs.Commanding;
 using Ncqrs.Commanding.CommandExecution.Mapping.Attributes;
 
 namespace WB.Core.SharedKernels.DataCollection.Commands.Questionnaire
 {
     [MapsToAggregateRootMethodOrConstructor(typeof(Implementation.Aggregates.Questionnaire), "ImportFromDesigner")]
-    public class ImportFromDesigner : CommandBase
+    public class ImportFromDesigner : QuestionnaireCommand
     {
         public ImportFromDesigner(Guid createdBy, IQuestionnaireDocument source, bool allowCensusMode, string supportingAssembly)
-            : base(source.PublicKey)
+            : base(source.PublicKey, source.PublicKey)
         {
             this.AllowCensusMode = allowCensusMode;
             CreatedBy = createdBy;
             Source = source;
-            QuestionnaireId = source.PublicKey;
             this.SupportingAssembly = supportingAssembly;
         }
 
@@ -22,8 +20,5 @@ namespace WB.Core.SharedKernels.DataCollection.Commands.Questionnaire
         public bool AllowCensusMode { get; private set; }
         public IQuestionnaireDocument Source { get; private set; }
         public string SupportingAssembly { get; private set; }
-
-        [AggregateRootId]
-        public Guid QuestionnaireId { get; private set; }
     }
 }
--- src/Core/SharedKernels/DataCollection/DataCollection/Commands/Questionnaire/RegisterPlainQuestionnaire.cs
+++ src/Core/SharedKernels/DataCollection/DataCollection/Commands/Questionnaire/RegisterPlainQuestionnaire.cs
@@ -5,21 +5,17 @@ using Ncqrs.Commanding.CommandExecution.Mapping.Attributes;
 namespace WB.Core.SharedKernels.DataCollection.Commands.Questionnaire
 {
     [MapsToAggregateRootMethodOrConstructor(typeof(Implementation.Aggregates.Questionnaire), "RegisterPlainQuestionnaire")]
-    public class RegisterPlainQuestionnaire : CommandBase
+    public class RegisterPlainQuestionnaire : QuestionnaireCommand
     {
-        [AggregateRootId]
-        public Guid QuestionnaireId { get; private set; }
-
         public Guid Id { get; private set; }
         public long Version { get; private set; }
         public bool AllowCensusMode { get; private set; }
         public string SupportingAssembly { get; private set; }
 
         public RegisterPlainQuestionnaire(Guid questionnaireId, long version, bool allowCensusMode, string supportingAssembly)
-            : base(questionnaireId)
+            : base(questionnaireId, questionnaireId)
         {
             this.AllowCensusMode = allowCensusMode;
-            this.QuestionnaireId = questionnaireId;
             this.Id = questionnaireId;
             this.Version = version;
             this.SupportingAssembly = supportingAssembly;
