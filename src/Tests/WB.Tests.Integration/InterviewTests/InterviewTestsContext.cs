e17986eb2ea31521dc8dfb1611d4f4d7048cf989
Slava Scherbak <slava.scherbak@gmail.com>
2014-11-17 11:16:55 -0500

    Designer: fixed bug with playing conditions for rosters on designer by fixing topological sortiong of conditional items KP-4602
    --HG--
    branch : hotfix-3-0-4
    
    
    Former-commit-id: 2fce82b8528abddf6d2f5ff6b9fc14b9f242441c

--- src/Tests/WB.Tests.Integration/InterviewTests/InterviewTestsContext.cs
+++ src/Tests/WB.Tests.Integration/InterviewTests/InterviewTestsContext.cs
@@ -18,6 +18,8 @@ using WB.Core.SharedKernels.DataCollection.Implementation.Aggregates;
 using WB.Core.SharedKernels.DataCollection.Implementation.Entities;
 using WB.Core.SharedKernels.DataCollection.Implementation.Providers;
 using WB.Core.SharedKernels.DataCollection.Repositories;
+using WB.Core.SharedKernels.ExpressionProcessor.Implementation.Services;
+using WB.Core.SharedKernels.ExpressionProcessor.Services;
 using It = Moq.It;
 
 namespace WB.Tests.Integration.InterviewTests
