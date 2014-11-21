4ab313edd7af82dff7440261933279afe83c8d13
Roman Lytvyn <none@none>
2014-11-21 15:56:30 +0200

    designer: added control panel page to make any user of administrator. KP-4485
    Former-commit-id: d4adfef7d4680e1a53478ee3699fd5958b6f6375

--- src/UI/Designer/WB.UI.Designer/Controllers/ControlPanelController.cs
+++ src/UI/Designer/WB.UI.Designer/Controllers/ControlPanelController.cs
@@ -1,18 +1,23 @@
 ﻿using System;
 using System.Linq;
 using System.Web.Mvc;
+using System.Web.Security;
+using System.Web.UI;
 using Microsoft.Practices.ServiceLocation;
 using WB.Core.Infrastructure.ReadSide;
+using WB.UI.Designer.Models;
 using WB.UI.Shared.Web.Filters;
+using WB.UI.Shared.Web.Membership;
 
 namespace WB.UI.Designer.Controllers
 {
     [LocalOrDevelopmentAccessOnly]
-    public class ControlPanelController : Controller
+    public class ControlPanelController : BaseController
     {
         private readonly IServiceLocator serviceLocator;
 
-        public ControlPanelController(IServiceLocator serviceLocator)
+        public ControlPanelController(IServiceLocator serviceLocator, IMembershipUserService userHelper)
+            : base(userHelper)
         {
             this.serviceLocator = serviceLocator;
         }
@@ -95,6 +100,53 @@ namespace WB.UI.Designer.Controllers
 
         public ActionResult ExpressionGeneration()
         {
+            return this.View();
+        }
+
+        public ActionResult MakeAdmin()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public ActionResult MakeAdmin(MakeAdminViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var account = Membership.GetUser(model.UserName);
+                if (account == null)
+                {
+                    this.Error(string.Format("Account '{0}' does not exists", model.UserName));
+                }
+                else
+                {
+                    if (model.MakeAdmin)
+                    {
+                        if (Roles.IsUserInRole(model.UserName, UserHelper.ADMINROLENAME))
+                        {
+                            this.Error(string.Format("Account '{0}' has administrator role", model.UserName));
+                        }
+                        else
+                        {
+                            Roles.AddUserToRole(account.ProviderUserKey.ToString(), UserHelper.ADMINROLENAME);
+                            this.Success(string.Format("Administrator role for '{0}' successfully added", model.UserName));   
+                        }
+                    }
+                    else
+                    {
+                        if (!Roles.IsUserInRole(model.UserName, UserHelper.ADMINROLENAME))
+                        {
+                            this.Error(string.Format("Account '{0}' is not in administrator role", model.UserName));
+                        }
+                        else
+                        {
+                            Roles.RemoveUserFromRole(model.UserName, UserHelper.ADMINROLENAME);
+                            this.Success(string.Format("Administrator role for '{0}' successfully removed", model.UserName));    
+                        }
+                    }
+                }
+            }
+
             return this.View();
         }
     }
