7cbbe5a2225573011424186c5626f13baa8781df
Dima Pasko <dimapasko@gmail.com>
2014-11-24 17:49:09 +0200

    Core: add UnitTests to test GetChunkMetaDataCreatedAfter() KP-4553 Change json settings of Questionnaire DTO and other DTOs and Views Core\Raven so it will not store nulls
    Former-commit-id: fb6e32e4a3cb106651e24d39cca3244cf03252b9

--- src/Core/Infrastructure/WB.Core.Infrastructure/ReadSide/Repository/Accessors/ReadSideRepositoryWriterExtensions.cs
+++ src/Core/Infrastructure/WB.Core.Infrastructure/ReadSide/Repository/Accessors/ReadSideRepositoryWriterExtensions.cs
@@ -6,17 +6,17 @@ namespace WB.Core.Infrastructure.ReadSide.Repository.Accessors
 {
     public static class ReadSideRepositoryWriterExtensions
     {
-        public static T GetById<T>(this IReadSideRepositoryWriter<T> writer, Guid id) where T : class, IReadSideRepositoryEntity
+        public static T GetById<T>(this IReadSideRepositoryWriter<T> writer, Guid? id) where T : class, IReadSideRepositoryEntity
         {
             return writer.GetById(id.FormatGuid());
         }
 
-        public static void Remove<T>(this IReadSideRepositoryWriter<T> writer, Guid id) where T : class, IReadSideRepositoryEntity
+        public static void Remove<T>(this IReadSideRepositoryWriter<T> writer, Guid? id) where T : class, IReadSideRepositoryEntity
         {
             writer.Remove(id.FormatGuid());
         }
 
-        public static void Store<T>(this IReadSideRepositoryWriter<T> writer, T view, Guid id) where T : class, IReadSideRepositoryEntity
+        public static void Store<T>(this IReadSideRepositoryWriter<T> writer, T view, Guid? id) where T : class, IReadSideRepositoryEntity
         {
             writer.Store(view, id.FormatGuid());
         }
