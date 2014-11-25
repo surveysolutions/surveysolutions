0d15ec24f8de27c05e4e0727545569de34c3a5f5
Roman Lytvyn <none@none>
2014-11-25 14:23:59 +0200

    designer: rebuild readside page refactoring. KP-4440
    Former-commit-id: ba10cac9eb6131a54a488e7ab854842e88e594ea

---
+++ src/UI/Designer/WB.UI.Designer/Models/RebuildReadSideInputViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WB.UI.Designer.Models
+{
+    public class RebuildReadSideInputViewModel
+    {
+        public string[] ListOfHandlers { get; set; }
+        public int NumberOfSkipedEvents { get; set; }
+        public Guid[] ListOfEventSources { get; set; }
+        public RebuildReadSideType RebuildType { get; set; }
+    }
+}
