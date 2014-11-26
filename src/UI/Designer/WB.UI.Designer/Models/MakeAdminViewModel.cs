a0c12ef22f75d3dd9923e8a052249739a6b1eb8e
Roman Lytvyn <litvin.roma@gmail.com>
2014-11-26 15:49:41 +0200

    designer: fix by review. KP-4485
    Former-commit-id: bcf3db092a1e805b8bea9de2181e22328b626ccd

--- src/UI/Designer/WB.UI.Designer/Models/MakeAdminViewModel.cs
+++ src/UI/Designer/WB.UI.Designer/Models/MakeAdminViewModel.cs
@@ -1,8 +1,12 @@
-﻿namespace WB.UI.Designer.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WB.UI.Designer.Models
 {
     public class MakeAdminViewModel
     {
+        [Display(Name = "Login")]
         public string UserName { get; set; }
+        [Display(Name = "Is admin")]
         public bool MakeAdmin { get; set; }
     }
 }
