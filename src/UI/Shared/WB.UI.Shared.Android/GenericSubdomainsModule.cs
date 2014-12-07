87e89af1b4154929345beeb6ef3aeae3ba168926
Roman Lytvyn <litvin.roma@gmail.com>
2014-12-07 13:54:04 +0200

    IN/TESTER: refactoring after moving rest to utils. KP-4642
    Former-commit-id: 2e184851f1a11ad07b98344c183026fdb06072d7

---
+++ src/UI/Shared/WB.UI.Shared.Android/GenericSubdomainsModule.cs
@@ -0,0 +1,17 @@
+﻿using Ninject.Modules;
+using WB.Core.GenericSubdomains.Utils.Implementation.Services.Rest;
+using WB.Core.GenericSubdomains.Utils.Services;
+using WB.Core.GenericSubdomains.Utils.Services.Rest;
+using WB.UI.Shared.Android.Services;
+
+namespace WB.UI.Shared.Android
+{
+    public class GenericSubdomainsModule : NinjectModule
+    {
+        public override void Load()
+        {
+            this.Bind<INetworkService>().To<AndroidNetworkService>().InSingletonScope();
+            this.Bind<IRestService>().To<RestService>().InSingletonScope();
+        }
+    }
+}
