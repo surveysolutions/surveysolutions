68a7531e960920efc435c63f403bd09cafd4a7c6
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2014-12-08 13:22:53 +0200

    Tests: moved from Ncqrs.Spec and Ncqrs.Tests to WB.Tests.Unit KP-4484
    Former-commit-id: 7b8317cf6efc239643b7c937c565d072a60f5d5f

---
+++ src/Tests/WB.Tests.Unit/Infrastructure/Ncqrs/Domain/Storage/AggregateRootCreationExceptionTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using FluentAssertions;
+using Microsoft.Practices.ServiceLocation;
+using Moq;
+using Ncqrs.Domain.Storage;
+using NUnit.Framework;
+
+namespace Ncqrs.Tests.Domain.Storage
+{
+    [TestFixture]
+    public class AggregateRootCreationExceptionTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            ServiceLocator.SetLocatorProvider(() => new Mock<IServiceLocator> { DefaultValue = DefaultValue.Mock }.Object);
+        }
+
+        [Test]
+        public void Constructing_an_instance_should_initialize_the_message()
+        {
+            String message = "Hello world";
+
+            var target = new AggregateRootCreationException(message);
+
+            target.Message.Should().Be(message);
+        }
+
+        [Test]
+        public void Constructing_an_instance_should_initialize_the_inner_exception()
+        {
+            String aMessage = "Hello world";
+            var theInnerException = new Exception();
+
+            var target = new AggregateRootCreationException(aMessage, theInnerException);
+
+            target.InnerException.Should().Be(theInnerException);
+        }
+    }
+}
