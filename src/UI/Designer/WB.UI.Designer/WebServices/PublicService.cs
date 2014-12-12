952058448cbb9195f5989c30470e049d9c569093
Anastasya Kholodova <nastya.kholodova@gmail.com>
2014-12-11 19:33:30 -0500

    manual post merge
    Former-commit-id: 1b927d3cbeac62d944fa6f35d4984fe0411484aa

--- src/UI/Designer/WB.UI.Designer/WebServices/PublicService.cs
+++ src/UI/Designer/WB.UI.Designer/WebServices/PublicService.cs
@@ -9,6 +9,7 @@ using WB.Core.BoundedContexts.Designer.Views.Questionnaire.QuestionnaireList;
 using WB.Core.Infrastructure.ReadSide;
 using WB.Core.SharedKernel.Utils.Compression;
 using WB.Core.SharedKernels.SurveySolutions.Services;
+using WB.UI.Designer.Resources;
 using WB.UI.Designer.WebServices.Questionnaire;
 using WB.UI.Shared.Web.Extensions;
 using WB.UI.Shared.Web.Membership;
