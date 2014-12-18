a3edbaefc973d66edff87c0a69fa8e501ef13187
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2014-12-18 15:30:08 -0500

    Designer: fix tests KP-4599
    Former-commit-id: 82050c006be23c0d633b37b425d3a4b57f9295a4

--- src/Tests/WB.Tests.Unit/Applications/Designer/QuestionnaireControllerTests/when_editing_options_and_uploaded_file_has_wrong_csv_format.cs
+++ src/Tests/WB.Tests.Unit/Applications/Designer/QuestionnaireControllerTests/when_editing_options_and_uploaded_file_has_wrong_csv_format.cs
@@ -24,7 +24,7 @@ namespace WB.Tests.Unit.Applications.Designer.QuestionnaireControllerTests
         Because of = () => controller.EditOptions(postedFile);
 
         It should_add_error_message_to_temp_data = () =>
-            controller.TempData[Alerts.ERROR].ShouldEqual("CSV-file has wrong format or file is corrupted.");
+            controller.TempData[Alerts.ERROR].ShouldEqual("Only tab-separated values files are accepted");
 
         Cleanup stuff = () =>
         {
