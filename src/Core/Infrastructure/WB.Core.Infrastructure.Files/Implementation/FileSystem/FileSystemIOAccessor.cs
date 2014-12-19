dd207ed02066253f880d555270382202f6fe76fe
Andrii Kozhyn <hedgic@gmail.com>
2014-12-19 16:08:40 +0200

    KP-4678: Load assembly from temp folder instead of bin of the site.
    Former-commit-id: 393647e50df3cd50cabf8159d27bc0c819c66761

--- src/Core/Infrastructure/WB.Core.Infrastructure.Files/Implementation/FileSystem/FileSystemIOAccessor.cs
+++ src/Core/Infrastructure/WB.Core.Infrastructure.Files/Implementation/FileSystem/FileSystemIOAccessor.cs
@@ -177,7 +177,7 @@ namespace WB.Core.Infrastructure.Files.Implementation.FileSystem
             }
             else
             {
-                this.CopyFile(sourceDir, targetDir);
+                this.BackupFile(sourceDir, targetDir);
             }
         }
 
@@ -188,9 +188,9 @@ namespace WB.Core.Infrastructure.Files.Implementation.FileSystem
             return Assembly.LoadFrom(assemblyFile);
         }
 
-        public string GetTempFolder()
+        public string GetTempFile()
         {
-            return Path.GetTempPath();
+            return Path.GetTempFileName();
         }
 
         private string RemoveNonAscii(string s)
@@ -198,12 +198,17 @@ namespace WB.Core.Infrastructure.Files.Implementation.FileSystem
             return Regex.Replace(s, @"[^\u0000-\u007F]", string.Empty);
         }
 
-        private void CopyFile(string sourcePath, string backupFolderPath)
+        private void BackupFile(string sourcePath, string backupFolderPath)
         {
             var sourceFileName = GetFileName(sourcePath);
             if (sourceFileName == null)
                 return;
             ZlpIOHelper.CopyFile(sourcePath, CombinePath(backupFolderPath, sourceFileName), true);
+        }
+
+        public void CopyFile(string sourceFile, string destFile)
+        {
+            File.Copy(sourceFile, destFile, true);
         }
     }
 }
