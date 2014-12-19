c9ec51b03ecc43c24885a8e36541b67c3a9e46fb
Andrii Kozhyn <hedgic@gmail.com>
2014-12-19 18:19:23 +0200

    Backed out changeset: c64a84d6958c
    Former-commit-id: b1edd0df93a0c10cc423c37be93bf755610db26b

---
+++ src/Tests/WB.Tests.Unit/SharedKernels/DataCollection/InterviewExpressionStatePrototypeProviderTests/when_getting_expression_state.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using AppDomainToolkit;
+using Machine.Specifications;
+using Moq;
+using WB.Core.Infrastructure.FileSystem;
+using WB.Core.SharedKernels.DataCollection;
+using WB.Core.SharedKernels.DataCollection.Implementation.Accessors;
+using WB.Core.SharedKernels.DataCollection.Implementation.Providers;
+using It = Machine.Specifications.It;
+
+namespace WB.Tests.Unit.SharedKernels.DataCollection.InterviewExpressionStatePrototypeProviderTests
+{
+    internal class when_getting_expression_state : InterviewExpressionStatePrototypeProviderTestContext
+    {
+        Establish context = () =>
+        {
+            appDomainContext = AppDomainContext.Create();
+        };
+
+        Because of = () =>
+            isResultNotNull = Execute.InStandaloneAppDomain(appDomainContext.Domain, () =>
+            {
+                Guid id = Guid.Parse("33332222111100000000111122223333");
+                long version = 3;
+
+                var path = typeof (IInterviewExpressionState).Assembly.Location;
+
+                AssemblyContext.SetupServiceLocator();
+
+                Mock<IQuestionnaireAssemblyFileAccessor> questionnareAssemblyFileAccessorMock = 
+                    CreateIQuestionnareAssemblyFileAccessorMock(path);
+
+                InterviewExpressionStatePrototypeProvider interviewExpressionStatePrototype = 
+                    CreateInterviewExpressionStatePrototype(questionnareAssemblyFileAccessorMock.Object);
+
+                return interviewExpressionStatePrototype.GetExpressionState(id, version) != null;
+            });
+
+        It should_provide_not_null_value = () =>
+            isResultNotNull.ShouldBeTrue();
+
+        Cleanup stuff = () =>
+        {
+            appDomainContext.Dispose();
+            appDomainContext = null;
+        };
+
+        static AppDomainContext appDomainContext;
+        static bool isResultNotNull;
+    }
+}
