df2d5c2931566a9bf4aa9b49a9b190857cbd7e95
Roman Lytvyn <litvin.roma@gmail.com>
2014-12-28 15:37:45 +0200

    TESTER: added supporting for old versions of application. KP-4771
    --HG--
    branch : release-candidate
    
    
    Former-commit-id: 2e5bbf77c4c60ca9a3ce11a31cb1e1f9b18cc149

---
+++ src/UI/Designer/WB.UI.Designer/Api/TesterController.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Http;
+using System.Net;
+using System.Web.Http;
+using Resources;
+using WB.UI.Designer.Api.Attributes;
+
+namespace WB.UI.Designer.Api
+{
+    [ApiBasicAuth]
+    public class TesterController : ApiController
+    {
+        [HttpGet]
+        public HttpResponseMessage GetAllTemplates()
+        {
+            return Request.CreateErrorResponse(HttpStatusCode.UpgradeRequired, Strings.ApplicationUpdateRequired);
+        }
+
+        [HttpPost]
+        public HttpResponseMessage ValidateCredentials()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, true);
+        }
+    }
+}
