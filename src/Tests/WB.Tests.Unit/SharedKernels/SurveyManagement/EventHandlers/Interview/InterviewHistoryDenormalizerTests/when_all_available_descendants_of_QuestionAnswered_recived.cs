c8def6e539bd4df101bffbd6acbe3c308f357347
Anastasya Kholodova <nastya.kholodova@gmail.com>
2014-12-29 16:46:48 -0500

    HQ\Export: code review - unit tests KP-4739
    Former-commit-id: edd5fff599e21ddf89204fa6df29c5012d896908

---
+++ src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/EventHandlers/Interview/InterviewHistoryDenormalizerTests/when_all_available_descendants_of_QuestionAnswered_recived.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Machine.Specifications;
+using Ncqrs.Eventing.ServiceModel.Bus;
+using WB.Core.SharedKernels.DataCollection.Events.Interview;
+using WB.Core.SharedKernels.DataCollection.Events.Interview.Base;
+using WB.Core.SharedKernels.SurveyManagement.EventHandler;
+using WB.Core.SharedKernels.SurveyManagement.Views.InterviewHistory;
+
+namespace WB.Tests.Unit.SharedKernels.SurveyManagement.EventHandlers.Interview.InterviewHistoryDenormalizerTests
+{
+    internal class when_all_available_descendants_of_QuestionAnswered_recived : InterviewHistoryDenormalizerTestContext
+    {
+        Establish context = () =>
+        {
+            availableDescendants = AppDomain.CurrentDomain.GetAssemblies()
+                .SelectMany(s => s.GetTypes()).Where(myType => myType.IsClass && !myType.IsAbstract && myType.IsSubclassOf(typeof(QuestionAnswered))).ToList();
+        };
+
+        Because of =
+            () =>
+            {
+                foreach (var eventToPublish in availableDescendants)
+                {
+                    var publishedEventClosedType = typeof(PublishedEvent<>).MakeGenericType(eventToPublish);
+                    var handleMethod = typeof(InterviewHistoryDenormalizer).GetMethod("Update", new[] { typeof(InterviewHistoryView), publishedEventClosedType });
+
+                    if (handleMethod == null)
+                        missingHandlers.Add(eventToPublish);
+                }
+            };
+
+        It should_each_QuestionAnswered_descendant_be_handled_by_InterviewHistoryDenormalizer = () =>
+            missingHandlers.ShouldBeEmpty();
+
+        private static HashSet<Type> missingHandlers = new Quartz.Collection.HashSet<Type>();
+
+        private static List<Type> availableDescendants;
+    }
+}
