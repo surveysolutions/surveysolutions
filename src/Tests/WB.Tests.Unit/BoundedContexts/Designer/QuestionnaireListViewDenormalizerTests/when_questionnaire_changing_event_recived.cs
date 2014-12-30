67205c507c9063285ef9769863df93facc8d31b6
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2014-12-30 14:22:39 -0500

    Designer: unit tests for KP-2831
    --HG--
    branch : release-candidate
    
    
    Former-commit-id: 97f2e375a8e65426318b2e00ec9e8d45f19aa3ed

---
+++ src/Tests/WB.Tests.Unit/BoundedContexts/Designer/QuestionnaireListViewDenormalizerTests/when_questionnaire_changing_event_recived.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Machine.Specifications;
+using Main.Core.Entities.SubEntities;
+using Main.Core.Events.Questionnaire;
+using Moq;
+using Ncqrs.Eventing;
+using Ncqrs.Eventing.ServiceModel.Bus;
+using WB.Core.BoundedContexts.Designer.Events.Questionnaire;
+using WB.Core.BoundedContexts.Designer.Views.Questionnaire.QuestionnaireList;
+using WB.Core.GenericSubdomains.Utils;
+using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
+using WB.Tests.Unit.SharedKernels.SurveyManagement;
+using It = Machine.Specifications.It;
+
+namespace WB.Tests.Unit.BoundedContexts.Designer.QuestionnaireListViewDenormalizerTests
+{
+    internal class when_questionnaire_changing_event_recived : QuestionnaireListViewItemDenormalizerTestContext
+    {
+        Establish context = () =>
+        {
+            var eventsToPublish = new List<object>
+            {
+                new NewGroupAdded(),
+                new GroupCloned(),
+                new QuestionnaireItemMoved(),
+                new QuestionDeleted(),
+                new NewQuestionAdded(),
+                new QuestionCloned(),
+                new QuestionChanged(),
+                new NumericQuestionAdded(),
+                new NumericQuestionCloned(),
+                new NumericQuestionChanged(),
+                new GroupDeleted(),
+                new GroupUpdated(),
+                new GroupBecameARoster(Guid.NewGuid(), Guid.NewGuid()),
+                new RosterChanged(Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid(), RosterSizeSourceType.FixedTitles,
+                    new string[0], null),
+                new GroupStoppedBeingARoster(Guid.NewGuid(), Guid.NewGuid()),
+                new TextListQuestionAdded(),
+                new TextListQuestionCloned(),
+                new TextListQuestionChanged(),
+                new QRBarcodeQuestionAdded(),
+                new QRBarcodeQuestionUpdated(),
+                new QRBarcodeQuestionCloned(),
+                new MultimediaQuestionUpdated(),
+                new StaticTextAdded(),
+                new StaticTextUpdated(),
+                new StaticTextCloned(),
+                new StaticTextDeleted()
+            };
+
+            eventAndResultView = eventsToPublish.ToDictionary(e => CreateCommittedEvent(e, questionnaireId, RandomDate()),
+                e => (DateTime?)null);
+
+            questionnaireListViewItemInMemoryWriter.Store(
+                new QuestionnaireListViewItem(questionnaireId, "test", DateTime.Now, DateTime.Now, null, true), questionnaireId);
+
+            questionnaireListViewItemDenormalizer = CreateQuestionnaireListViewItemDenormalizer(questionnaireListViewItemInMemoryWriter);
+        };
+
+        Because of = () =>
+        {
+            foreach (var eventToPublish in eventAndResultView.Keys.ToList())
+            {
+                var publishedEventClosedType = typeof(PublishedEvent<>).MakeGenericType(eventToPublish.Payload.GetType());
+                var handleMethod = typeof(QuestionnaireListViewItemDenormalizer).GetMethod("Handle", new[] { publishedEventClosedType });
+
+                var publishedEvent =
+                    (PublishedEvent)
+                        Activator.CreateInstance(publishedEventClosedType, eventToPublish);
+
+                handleMethod.Invoke(questionnaireListViewItemDenormalizer, new object[] { publishedEvent });
+
+                eventAndResultView[eventToPublish] = questionnaireListViewItemInMemoryWriter.GetById(questionnaireId).LastEntryDate;
+            }
+        };
+
+        It should_update_LastEntryDate_each_time_when_event_piblished = () =>
+            eventAndResultView.ShouldEachConformTo(eventWithView=>eventWithView.Key.EventTimeStamp==eventWithView.Value);
+
+        private static DateTime RandomDate()
+        {
+            DateTime start = new DateTime(1984, 4, 18);
+            Random gen = new Random();
+
+            int range = (DateTime.Today - start).Days;
+            return start.AddDays(gen.Next(range));
+        }
+
+        private static QuestionnaireListViewItemDenormalizer questionnaireListViewItemDenormalizer;
+
+        private static TestInMemoryWriter<QuestionnaireListViewItem> questionnaireListViewItemInMemoryWriter =
+            new TestInMemoryWriter<QuestionnaireListViewItem>();
+
+        private static Dictionary<CommittedEvent, DateTime?> eventAndResultView =
+            new Dictionary<CommittedEvent, DateTime?>();
+
+        private static readonly Guid questionnaireId = Guid.Parse("11111111111111111111111111111111");
+    }
+}
