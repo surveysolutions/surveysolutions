1a3c22ad3267e9d6b82ba09481868de1e37f2534
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2015-01-09 16:00:38 -0500

    Fix SoF Exeption [KP-4799]
    --HG--
    branch : release-candidate
    
    
    Former-commit-id: cfdbcb6a3a5a241e02cd2a3b44a1ee3db272caac

--- src/Tests/WB.Tests.Unit/BoundedContexts/Designer/QuestionnaireVerificationTests/when_verifying_questionnaire_that_has_group_with_condition_expression_with_more_than_10000_chars_length.cs
+++ src/Tests/WB.Tests.Unit/BoundedContexts/Designer/QuestionnaireVerificationTests/when_verifying_questionnaire_that_has_group_with_condition_expression_with_more_than_10000_chars_length.cs
@@ -6,7 +6,9 @@ using Main.Core.Documents;
 using Main.Core.Entities.Composite;
 using Main.Core.Entities.SubEntities;
 using Main.Core.Entities.SubEntities.Question;
+using Moq;
 using WB.Core.BoundedContexts.Designer.Implementation.Services;
+using WB.Core.BoundedContexts.Designer.Services;
 using WB.Core.BoundedContexts.Designer.ValueObjects;
 using It = Machine.Specifications.It;
 
@@ -27,7 +29,13 @@ namespace WB.Tests.Unit.BoundedContexts.Designer.QuestionnaireVerificationTests
                 })
             });
 
-            verifier = CreateQuestionnaireVerifier();
+            questionnireExpressionProcessorGeneratorMock = new Mock<IExpressionProcessorGenerator>();
+            string generationResult;
+            questionnireExpressionProcessorGeneratorMock.Setup(
+                _ => _.GenerateProcessorStateAssembly(Moq.It.IsAny<QuestionnaireDocument>(), out generationResult))
+                .Returns(new GenerationResult() { Success = true, Diagnostics = new List<GenerationDiagnostic>() });
+
+            verifier = CreateQuestionnaireVerifier(expressionProcessorGenerator: questionnireExpressionProcessorGeneratorMock.Object);
         };
 
         Because of = () =>
@@ -48,10 +56,16 @@ namespace WB.Tests.Unit.BoundedContexts.Designer.QuestionnaireVerificationTests
         It should_return_error_with_one_references_with_id_equals_groupId = () =>
             resultErrors.First().References.First().Id.ShouldEqual(groupId);
 
+        private It should_not_call_GenerateProcessorStateAssembly = () =>
+            questionnireExpressionProcessorGeneratorMock.Verify(x => x.GenerateProcessorStateAssembly(Moq.It.IsAny<QuestionnaireDocument>(), out generationResult), Times.Never);
+
         private static IEnumerable<QuestionnaireVerificationError> resultErrors;
         private static QuestionnaireVerifier verifier;
         private static QuestionnaireDocument questionnaire;
         private static Guid groupId = Guid.Parse("DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD");
         private static Guid parentGroupId = Guid.Parse("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+
+        private static Mock<IExpressionProcessorGenerator> questionnireExpressionProcessorGeneratorMock;
+        private static string generationResult;
     }
 }
