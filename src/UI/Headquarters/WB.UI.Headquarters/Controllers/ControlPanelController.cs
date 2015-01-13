4a945cd00197237b9ac51c0dac2ba2ed28cf1256
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2015-01-12 20:37:06 -0500

    Assembly transfer now supports compression

--- src/UI/Headquarters/WB.UI.Headquarters/Controllers/ControlPanelController.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/Controllers/ControlPanelController.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.ServiceModel;
-using System.Threading.Tasks;
 using System.Web.Mvc;
 using Main.Core.Entities.SubEntities;
 using Main.Core.Utility;
@@ -12,11 +9,7 @@ using Ncqrs.Commanding.ServiceModel;
 using WB.Core.GenericSubdomains.Logging;
 using WB.Core.GenericSubdomains.Utils;
 using WB.Core.Infrastructure.ReadSide;
-using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
 using WB.Core.SharedKernels.DataCollection.Commands.User;
-using WB.Core.SharedKernels.DataCollection.ValueObjects.Interview;
-using WB.Core.SharedKernels.SurveyManagement.Implementation.ReadSide.Indexes;
-using WB.Core.SharedKernels.SurveyManagement.Services;
 using WB.Core.SharedKernels.SurveyManagement.Views.User;
 using WB.Core.SharedKernels.SurveyManagement.Web.Code;
 using WB.Core.SharedKernels.SurveyManagement.Web.Controllers;
@@ -35,14 +28,10 @@ namespace WB.UI.Headquarters.Controllers
         private readonly IIncomePackagesRepository incomePackagesRepository;
         private readonly IViewFactory<UserViewInputModel, UserView> userViewFactory;
         private readonly IPasswordHasher passwordHasher;
-        private static string lastReexportMessage = "no reexport performed";
-        private readonly IDataExportRepositoryWriter dataExportRepositoryWriter;
-        private readonly IReadSideRepositoryIndexAccessor readSideRepositoryIndexAccessor;
 
         public ControlPanelController(IServiceLocator serviceLocator, IIncomePackagesRepository incomePackagesRepository,
             ICommandService commandService, IGlobalInfoProvider globalInfo, ILogger logger,
-            IViewFactory<UserViewInputModel, UserView> userViewFactory, IPasswordHasher passwordHasher,
-            IDataExportRepositoryWriter dataExportRepositoryWriter, IReadSideRepositoryIndexAccessor readSideRepositoryIndexAccessor)
+            IViewFactory<UserViewInputModel, UserView> userViewFactory, IPasswordHasher passwordHasher)
             : base(commandService: commandService, globalInfo: globalInfo, logger: logger)
         {
             this.serviceLocator = serviceLocator;
@@ -50,8 +39,6 @@ namespace WB.UI.Headquarters.Controllers
 
             this.userViewFactory = userViewFactory;
             this.passwordHasher = passwordHasher;
-            this.dataExportRepositoryWriter = dataExportRepositoryWriter;
-            this.readSideRepositoryIndexAccessor = readSideRepositoryIndexAccessor;
         }
 
         /// <remarks>
@@ -206,58 +193,6 @@ namespace WB.UI.Headquarters.Controllers
         public string GetRevalidateInterviewStatus()
         {
             return this.RevalidateInterviewsAdministrationService.GetReadableStatus();
-        }
-
-        #endregion
-
-        #region reexport
-
-        public ActionResult ReexportInterviews()
-        {
-            return this.View();
-        }
-
-        public ActionResult StartReexportApprovedInterviews(int? skip)
-        {
-            new Task(() => this.ReexportApprovedInterviewsImpl(skip ?? 0)).Start();
-
-            return this.RedirectToAction("ReexportInterviews");
-        }
-
-        private void ReexportApprovedInterviewsImpl(int skip)
-        {
-            string indexName = typeof(InterviewsSearchIndex).Name;
-            int pageSize = 20;
-
-            int count = this.GetApprovedInterviewIds(indexName).Count();
-            int processed = skip;
-
-            lastReexportMessage = string.Format("found {0} interviews", count);
-            while (processed < count)
-            {
-                List<Guid> interviewIds = this.GetApprovedInterviewIds(indexName).Skip(processed).Take(pageSize).ToList();
-
-                foreach (var interviewId in interviewIds)
-                {
-                    this.dataExportRepositoryWriter.AddExportedDataByInterview(interviewId);
-                    processed++;
-
-                    lastReexportMessage = string.Format("last processed interview index: {0}", processed);
-                }
-            }
-        }
-
-        private IQueryable<Guid> GetApprovedInterviewIds(string indexName)
-        {
-            return this.readSideRepositoryIndexAccessor.Query<SeachIndexContent>(indexName)
-                .Where(x => x.Status == InterviewStatus.ApprovedByHeadquarters || x.Status == InterviewStatus.ApprovedBySupervisor)
-                .Select(x => x.InterviewId);
-        }
-
-        [OutputCache(NoStore = true, Duration = 0, VaryByParam = "*")]
-        public string GetReexportStatus()
-        {
-            return lastReexportMessage;
         }
 
         #endregion
