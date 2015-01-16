b0a2df12b4d3225dda66e0e9bdda3ade2c3d8fd5
Roman Lytvyn <litvin.roma@gmail.com>
2015-01-16 16:39:45 +0200

    hq/sv: added history by statuses. KP-4556

--- src/Core/SharedKernels/SurveyManagement/SurveyManagement.Web/Models/DetailsViewModel.cs
+++ src/Core/SharedKernels/SurveyManagement/SurveyManagement.Web/Models/DetailsViewModel.cs
@@ -1,4 +1,7 @@
-﻿using WB.Core.SharedKernels.SurveyManagement.Views.Interview;
+﻿using System.Collections.Generic;
+using WB.Core.SharedKernels.SurveyManagement.Views.ChangeStatus;
+using WB.Core.SharedKernels.SurveyManagement.Views.Interview;
+using WB.Core.SharedKernels.SurveyManagement.Views.InterviewHistory;
 
 namespace WB.Core.SharedKernels.SurveyManagement.Web.Models
 {
@@ -6,6 +9,8 @@ namespace WB.Core.SharedKernels.SurveyManagement.Web.Models
     {
         public InterviewDetailsSortBy SortBy { get; set; }
         public InterviewDetailsFilter Filter { get; set; }
-        public InterviewDetailsView InterviewDetails { get; set; }
+        public InterviewDetailsView FilteredInterviewDetails { get; set; }
+        public IEnumerable<InterviewGroupView> Groups { get; set; }
+        public ChangeStatusView History { get; set; }
     }
 }
