70fe357e07d16f42b7cfbbc362226d8ce5396e5e
Andrii Kozhyn <hedgic@gmail.com>
2015-01-20 15:16:58 +0200

    KP-4129: Fixed 0 when cloning numeric question

---
+++ src/Tests/WB.Tests.Unit/BoundedContexts/Designer/QuestionnaireTests/Clone/when_cloning_cloned_numeric_integer_question.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Machine.Specifications;
+using Main.Core.Entities.SubEntities;
+using Main.Core.Events.Questionnaire;
+using Ncqrs.Spec;
+using WB.Core.BoundedContexts.Designer.Aggregates;
+
+namespace WB.Tests.Unit.BoundedContexts.Designer.QuestionnaireTests.Clone
+{
+    public class when_cloning_cloned_numeric_integer_question : QuestionnaireTestsContext
+    {
+        Establish context = () =>
+        {
+            responsibleId = Guid.Parse("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+            questionnaire = CreateQuestionnaire(responsibleId);
+            var groupId = Guid.Parse("BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB");
+            questionnaire.Apply(new NewGroupAdded { PublicKey = groupId });
+            sourceQuestionId = Guid.Parse("CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC");
+
+            var questionCloned = new QuestionCloned
+            {
+                PublicKey = sourceQuestionId,
+                QuestionText = "text",
+                QuestionType = QuestionType.Numeric,
+                StataExportCaption = "varrr",
+                VariableLabel = "varlabel",
+                IsInteger = true
+            };
+            questionnaire.Apply(questionCloned);
+
+            eventContext = new EventContext();
+        };
+
+        Because of = () => questionnaire.CloneQuestionById(sourceQuestionId, responsibleId, targetId);
+
+        It should_clone_Reset_MaxValue_property = () => eventContext.ShouldContainEvent<QuestionCloned>(x => x.PublicKey == targetId && x.MaxValue == null);
+
+        static Questionnaire questionnaire;
+        static Guid sourceQuestionId;
+        static EventContext eventContext;
+        static Guid responsibleId;
+        static Guid targetId = Guid.Parse("DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD");
+    }
+}
+
