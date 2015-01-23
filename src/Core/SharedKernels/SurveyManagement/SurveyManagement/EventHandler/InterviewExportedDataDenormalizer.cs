d3b1f106205308d16f3284c79cbffb44d271e6dc
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2015-01-23 02:10:57 +0200

    HQ,SV: read side repo accessors which did not use querying replaced with key/value storage

--- src/Core/SharedKernels/SurveyManagement/SurveyManagement/EventHandler/InterviewExportedDataDenormalizer.cs
+++ src/Core/SharedKernels/SurveyManagement/SurveyManagement/EventHandler/InterviewExportedDataDenormalizer.cs
@@ -52,13 +52,13 @@ namespace WB.Core.SharedKernels.SurveyManagement.EventHandler
         IEventHandler<InterviewRestored>
     {
         private readonly InterviewExportedAction[] listOfActionsAfterWhichFirstAnswerSetAtionShouldBeRecorded = new[] { InterviewExportedAction.InterviewerAssigned, InterviewExportedAction.RejectedBySupervisor, InterviewExportedAction.Restarted };
-        private readonly IReadSideRepositoryWriter<RecordFirstAnswerMarkerView> recordFirstAnswerMarkerViewWriter;
+        private readonly IReadSideKeyValueStorage<RecordFirstAnswerMarkerView> recordFirstAnswerMarkerViewWriter;
         private readonly IReadSideRepositoryWriter<UserDocument> users;
         private readonly IReadSideRepositoryWriter<InterviewSummary> interviewSummaryStorage;
         private readonly IDataExportRepositoryWriter dataExportWriter;
 
         public InterviewExportedDataDenormalizer(IDataExportRepositoryWriter dataExportWriter,
-            IReadSideRepositoryWriter<RecordFirstAnswerMarkerView> recordFirstAnswerMarkerViewWriter, 
+            IReadSideKeyValueStorage<RecordFirstAnswerMarkerView> recordFirstAnswerMarkerViewWriter, 
             IReadSideRepositoryWriter<UserDocument> userDocumentWriter, IReadSideRepositoryWriter<InterviewSummary> interviewSummaryStorage)
         {
             this.dataExportWriter = dataExportWriter;
