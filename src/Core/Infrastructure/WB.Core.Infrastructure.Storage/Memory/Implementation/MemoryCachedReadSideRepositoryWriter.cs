c9099c9b947052f6c4c84d8c104633a06f29342e
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2015-01-28 14:59:37 -0500

    memory cache for writers with ugly injection

---
+++ src/Core/Infrastructure/WB.Core.Infrastructure.Storage/Memory/Implementation/MemoryCachedReadSideRepositoryWriter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
+using WB.Core.Infrastructure.Storage.Raven.Implementation.ReadSide.RepositoryAccessors;
+using WB.Core.SharedKernels.SurveySolutions;
+
+namespace WB.Core.Infrastructure.Storage.Memory.Implementation
+{
+    internal class MemoryCachedReadSideRepositoryWriter<TEntity> : MemoryCachedReadSideStore<TEntity>,
+        IReadSideRepositoryWriter<TEntity> where TEntity : class, IReadSideRepositoryEntity
+    {
+        public MemoryCachedReadSideRepositoryWriter(RavenReadSideRepositoryWriter<TEntity> readSideStorage)
+            : base(readSideStorage)
+        {
+        }
+    }
+}
--- src/Core/Infrastructure/WB.Core.Infrastructure.Storage/Raven/Implementation/ReadSide/RepositoryAccessors/RavenReadSideRepositoryWriter.cs
+++ src/Core/Infrastructure/WB.Core.Infrastructure.Storage/Raven/Implementation/ReadSide/RepositoryAccessors/RavenReadSideRepositoryWriter.cs
@@ -18,109 +18,28 @@ using WB.Core.SharedKernels.SurveySolutions;
 namespace WB.Core.Infrastructure.Storage.Raven.Implementation.ReadSide.RepositoryAccessors
 {
     #warning TLK: make string identifiers here after switch to new storage
-    public class RavenReadSideRepositoryWriter<TEntity> : RavenReadSideRepositoryAccessor<TEntity>, IReadSideRepositoryWriter<TEntity>, IReadSideRepositoryWriter, IReadSideRepositoryCleaner
+    public class RavenReadSideRepositoryWriter<TEntity> : RavenReadSideRepositoryAccessor<TEntity>,
+        IReadSideRepositoryWriter<TEntity>, IReadSideRepositoryCleaner
         where TEntity : class, IReadSideRepositoryEntity
     {
-        private const int MaxCountOfCachedEntities = 256;
-        private const int MaxCountOfEntitiesInOneStoreOperation = 16;
-
-        private bool isCacheEnabled = false;
-
-        private readonly Dictionary<string, TEntity> cache = new Dictionary<string, TEntity>();
-        private readonly RavenReadSideRepositoryWriterSettings settings;
-        private readonly ILogger logger;
-
-        public RavenReadSideRepositoryWriter(IDocumentStore ravenStore, ILogger logger, RavenReadSideRepositoryWriterSettings settings)
+        public RavenReadSideRepositoryWriter(IDocumentStore ravenStore)
             : base(ravenStore)
         {
-            this.logger = logger;
-            this.settings = settings;
         }
 
         public TEntity GetById(string id)
         {
-            return this.isCacheEnabled
-                ? this.GetByIdUsingCache(id)
-                : this.GetByIdAvoidingCache(id);
+            return this.GetByIdAvoidingCache(id);
         }
 
         public void Remove(string id)
         {
-            if (this.isCacheEnabled)
-            {
-                this.RemoveUsingCache(id);
-            }
-            else
-            {
-                this.RemoveAvoidingCache(id);
-            }
+            this.RemoveAvoidingCache(id);
         }
 
         public void Store(TEntity view, string id)
         {
-            if (this.isCacheEnabled)
-            {
-                this.StoreUsingCache(view, id);
-            }
-            else
-            {
-                this.StoreAvoidingCache(view, id);
-            }
-        }
-
-        public void EnableCache()
-        {
-            this.isCacheEnabled = true;
-        }
-
-        public void DisableCache()
-        {
-            while (cache.Any())
-            {
-                this.StoreBulkEntitiesToRepository(cache.Keys.ToList());
-            }
-
-            this.isCacheEnabled = false;
-        }
-
-        public string GetReadableStatus()
-        {
-            int cachedEntities = this.cache.Count;
-
-            return string.Format("cache {0};    cached: {1};",
-                this.isCacheEnabled ? "enabled" : "disabled",
-                cachedEntities);
-        }
-
-        public Type ViewType
-        {
-            get { return typeof (TEntity); }
-        }
-
-        private TEntity GetByIdUsingCache(string id)
-        {
-            if (cache.ContainsKey(id))
-                return cache[id];
-
-            var entity = GetByIdAvoidingCache(id);
-
-            cache[id] = entity;
-
-            this.ReduceCacheIfNeeded();
-
-            return entity;
-        }
-
-        private void RemoveUsingCache(string id)
-        {
-            this.cache.Remove(id);
-        }
-
-        private void StoreUsingCache(TEntity entity, string id)
-        {
-            this.cache[id] = entity;
-
-            this.ReduceCacheIfNeeded();
+            this.StoreAvoidingCache(view, id);
         }
 
         private TEntity GetByIdAvoidingCache(string id)
@@ -142,8 +61,8 @@ namespace WB.Core.Infrastructure.Storage.Raven.Implementation.ReadSide.Repositor
                 string ravenId = ToRavenId(id);
 
                 var view = session.Load<TEntity>(id: ravenId);
-                
-                if(view==null)
+
+                if (view == null)
                     return;
 
                 session.Delete(view);
@@ -160,61 +79,6 @@ namespace WB.Core.Infrastructure.Storage.Raven.Implementation.ReadSide.Repositor
                 session.Store(entity: entity, id: ravenId);
                 session.SaveChanges();
             }
-        }
-
-        private void ReduceCacheIfNeeded()
-        {
-            if (this.IsCacheLimitReached())
-            {
-                this.ReduceCache();
-            }
-        }
-
-        private void ReduceCache()
-        {
-            var bulk = this.cache.Keys.Take(MaxCountOfEntitiesInOneStoreOperation).ToList();
-            this.StoreBulkEntitiesToRepository(bulk);
-        }
-
-        private bool IsCacheLimitReached()
-        {
-            return this.cache.Count >= MaxCountOfCachedEntities;
-        }
-
-        private void StoreBulkEntitiesToRepository(IEnumerable<string> bulkOfEntityIds)
-        {
-            try
-            {
-                using (
-                    var session =
-                        this.RavenStore.BulkInsert(options:
-                            new BulkInsertOptions() {OverwriteExisting = true, BatchSize = settings.BulkInsertBatchSize})
-                    )
-                {
-                    foreach (var entityId in bulkOfEntityIds)
-                    {
-                        StoreCachedEntityToRepository(session, entityId, cache[entityId]);
-                    }
-                }
-
-                foreach (var entityId in bulkOfEntityIds)
-                {
-                    cache.Remove(entityId);
-                }
-            }
-            catch (Exception e)
-            {
-                logger.Error(e.Message, e);
-                throw;
-            }
-        }
-
-        private void StoreCachedEntityToRepository(BulkInsertOperation bulkOperation, string id, TEntity entity)
-        {
-            if(entity==null)
-                return;
-            string ravenId = ToRavenId(id);
-            bulkOperation.Store(entity: entity, id: ravenId);
         }
 
         public void Clear()
