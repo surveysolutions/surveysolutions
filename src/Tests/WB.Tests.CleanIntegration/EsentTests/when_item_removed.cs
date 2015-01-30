f6b58a654fe8051e36a0dcbed6f33509a2714c77
Andrii Kozhyn <hedgic@gmail.com>
2015-01-30 19:05:53 +0200

    KP-4870: Added integration tests for ESENT

---
+++ src/Tests/WB.Tests.CleanIntegration/EsentTests/when_item_removed.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Machine.Specifications;
+
+namespace WB.Tests.Integration.EsentTests
+{
+    internal class when_item_removed : with_esent_store<TestStoredEntity>
+    {
+        Establish context = () =>
+        {
+            transientEntity = new TestStoredEntity
+            {
+                Id = Guid.Parse("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"),
+                IntegerProperty = 5,
+                StringProperty = "Some test string"
+            };
+            storage.Store(transientEntity, itemId);
+        };
+
+        Because of = () => storage.Remove(itemId);
+
+        It should_be_removed_from_store = () => storage.GetById(itemId).ShouldBeNull();
+
+        const string itemId = "id";
+        static TestStoredEntity transientEntity;
+    }
+}
+
