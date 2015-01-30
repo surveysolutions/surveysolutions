549cec9ecda02c898e65679f9f916c2b6cb24e49
Andrii Kozhyn <hedgic@gmail.com>
2015-01-30 13:41:10 +0200

    KP-4870: Removed not needed tests. Added esent tests

---
+++ src/Tests/WB.Tests.Integration/EsentTests/with_esent_store.cs
@@ -0,0 +1,23 @@
+﻿using Machine.Specifications;
+using Microsoft.Isam.Esent.Collections.Generic;
+using WB.Core.Infrastructure.Storage.Esent.Implementation;
+using WB.Core.SharedKernels.SurveySolutions;
+
+namespace WB.Tests.Integration.EsentTests
+{
+    internal class with_esent_store<T> where T : class, IReadSideRepositoryEntity
+    {
+        Establish context = () =>
+        {
+            storage = new EsentKeyValueStorage<T>(new EsentSettings("TempStore"));
+        };
+
+        Cleanup things = () =>
+        {
+            storage.Dispose();
+            PersistentDictionaryFile.DeleteFiles("TempStore");
+        };
+
+        protected static EsentKeyValueStorage<T> storage;
+    }
+}
