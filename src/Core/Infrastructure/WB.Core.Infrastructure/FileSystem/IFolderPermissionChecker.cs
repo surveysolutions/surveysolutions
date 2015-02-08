882a9c0e1677079dfc6677aae15825b5d68dc3d0
Pavel Stovpivskyi <sby@rambler.ru>
2015-02-08 22:15:18 +0200

    Added check on permissions fro AppData

---
+++ src/Core/Infrastructure/WB.Core.Infrastructure/FileSystem/IFolderPermissionChecker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WB.Core.Infrastructure.FileSystem
+{
+    public interface IFolderPermissionChecker
+    {
+        FolderPermissionCheckResult Check();
+    }
+
+    public class FolderPermissionCheckResult
+    {
+        public string[] AllowedFolders { get; set; }
+        public string[] DenidedFolders { get; set; }
+        public string ProcessRunedUnder { get; set; }
+    }
+}
