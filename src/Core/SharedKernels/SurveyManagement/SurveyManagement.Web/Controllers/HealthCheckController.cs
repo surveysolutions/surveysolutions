032a4813cd812b48cf98a5674e9be10735a233cc
Pavel Stovpivskyi <sby@rambler.ru>
2015-02-09 16:58:06 +0200

    Added healthcheck icon in top right corner and blank page

---
+++ src/Core/SharedKernels/SurveyManagement/SurveyManagement.Web/Controllers/HealthCheckController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+using WB.Core.GenericSubdomains.Utils.Services;
+using WB.Core.Infrastructure.CommandBus;
+using WB.Core.SharedKernels.SurveyManagement.Web.Utils.Membership;
+
+namespace WB.Core.SharedKernels.SurveyManagement.Web.Controllers
+{
+    [AllowAnonymous]
+    public class HealthCheckController : BaseController
+    {
+        public HealthCheckController(ICommandService commandService, IGlobalInfoProvider globalInfo, ILogger logger)
+            : base(commandService, globalInfo, logger)
+        {
+        }
+
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
