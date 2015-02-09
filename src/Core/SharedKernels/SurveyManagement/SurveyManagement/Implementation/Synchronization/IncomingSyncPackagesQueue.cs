27e270082b23d9185965901af4419823962fa2f8
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2015-02-09 10:05:04 -0500

    HQ\Sync: code review KP-4877

--- src/Core/SharedKernels/SurveyManagement/SurveyManagement/Implementation/Synchronization/IncomingSyncPackagesQueue.cs
+++ src/Core/SharedKernels/SurveyManagement/SurveyManagement/Implementation/Synchronization/IncomingSyncPackagesQueue.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Linq;
+using Main.Core.Events;
 using WB.Core.GenericSubdomains.Utils;
+using WB.Core.GenericSubdomains.Utils.Services;
 using WB.Core.Infrastructure.FileSystem;
+using WB.Core.SharedKernel.Structures.Synchronization;
+using WB.Core.SharedKernels.DataCollection.ValueObjects.Interview;
+using WB.Core.SharedKernels.SurveyManagement.Synchronization;
 using WB.Core.Synchronization;
 
 namespace WB.Core.SharedKernels.SurveyManagement.Implementation.Synchronization
@@ -12,11 +17,19 @@ namespace WB.Core.SharedKernels.SurveyManagement.Implementation.Synchronization
         private readonly IFileSystemAccessor fileSystemAccessor;
         private readonly string incomingUnprocessedPackagesDirectory;
         private readonly SyncSettings syncSettings;
+        private readonly ILogger logger;
+        private readonly IJsonUtils jsonUtils;
+        private readonly IArchiveUtils archiver;
+        private IUnhandledPackageStorage unhandledPackageStorage;
 
-        public IncomingSyncPackagesQueue(IFileSystemAccessor fileSystemAccessor, SyncSettings syncSettings)
+        public IncomingSyncPackagesQueue(IFileSystemAccessor fileSystemAccessor, SyncSettings syncSettings, ILogger logger, IJsonUtils jsonUtils, IArchiveUtils archiver, IUnhandledPackageStorage unhandledPackageStorage)
         {
             this.fileSystemAccessor = fileSystemAccessor;
             this.syncSettings = syncSettings;
+            this.logger = logger;
+            this.jsonUtils = jsonUtils;
+            this.archiver = archiver;
+            this.unhandledPackageStorage = unhandledPackageStorage;
 
             this.incomingUnprocessedPackagesDirectory = fileSystemAccessor.CombinePath(syncSettings.AppDataDirectory,
                 syncSettings.IncomingUnprocessedPackagesDirectoryName);
@@ -44,9 +57,43 @@ namespace WB.Core.SharedKernels.SurveyManagement.Implementation.Synchronization
             }
         }
 
-        public string DeQueue()
+        public IncomingSyncPackages DeQueue()
         {
-            return this.fileSystemAccessor.GetFilesInDirectory(this.incomingUnprocessedPackagesDirectory).FirstOrDefault();
+            var pathToPackage = fileSystemAccessor.GetFilesInDirectory(incomingUnprocessedPackagesDirectory).FirstOrDefault();
+
+            if (string.IsNullOrEmpty(pathToPackage) || !fileSystemAccessor.IsFileExists(pathToPackage))
+                return null;
+
+            Guid? interviewId = null;
+            try
+            {
+                var syncItem = jsonUtils.Deserialize<SyncItem>(fileSystemAccessor.ReadAllText(pathToPackage));
+
+                interviewId = syncItem.RootId;
+
+                var meta =
+                    jsonUtils.Deserialize<InterviewMetaInfo>(archiver.DecompressString(syncItem.MetaInfo));
+
+                var eventsToSync =
+                    jsonUtils.Deserialize<AggregateRootEvent[]>(archiver.DecompressString(syncItem.Content))
+                        .Select(e => e.Payload)
+                        .ToArray();
+
+                return new IncomingSyncPackages(meta.PublicKey, meta.ResponsibleId, meta.TemplateId,
+                        meta.TemplateVersion, (InterviewStatus)meta.Status, eventsToSync, meta.CreatedOnClient ?? false, syncSettings.Origin, pathToPackage);
+            }
+            catch (Exception e)
+            {
+                logger.Error(string.Format("package '{0}' wasn't parsed. Reason: '{1}'", pathToPackage, e.Message), e);
+                unhandledPackageStorage.StoreUnhandledPackage(pathToPackage, interviewId);
+                DeleteSyncItem(pathToPackage);
+            }
+            return null;
+        }
+
+        public void DeleteSyncItem(string syncItemPath)
+        {
+            fileSystemAccessor.DeleteFile(syncItemPath);
         }
     }
 }
--- src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/IncomingPackagesQueueTests/when_DeQueue_method_called.cs
+++ src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/IncomingPackagesQueueTests/when_DeQueue_method_called.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Machine.Specifications;
+using Main.Core.Events;
 using Moq;
+using WB.Core.GenericSubdomains.Utils.Services;
 using WB.Core.Infrastructure.FileSystem;
 using WB.Core.SharedKernels.SurveyManagement.Implementation.Synchronization;
 using It = Machine.Specifications.It;
@@ -15,23 +17,34 @@ namespace WB.Tests.Unit.SharedKernels.SurveyManagement.IncomingPackagesQueueTest
     {
         Establish context = () =>
         {
+            var jsonUtilsMock = new Mock<IJsonUtils>();
+            jsonUtilsMock.Setup(x => x.Deserialize<SyncItem>(Moq.It.IsAny<string>())).Returns(new SyncItem() { RootId = interviewId });
+            jsonUtilsMock.Setup(x => x.Deserialize<InterviewMetaInfo>(Moq.It.IsAny<string>())).Returns(new InterviewMetaInfo());
+            jsonUtilsMock.Setup(x => x.Deserialize<AggregateRootEvent[]>(Moq.It.IsAny<string>())).Returns(new []{ new AggregateRootEvent() });
+
+
             fileSystemAccessorMock = new Mock<IFileSystemAccessor>();
             fileSystemAccessorMock.Setup(x => x.GetFilesInDirectory(Moq.It.IsAny<string>()))
                 .Returns(filesInFolder);
+            fileSystemAccessorMock.Setup(x => x.IsFileExists(Moq.It.IsAny<string>()))
+              .Returns(true);
 
-            incomingSyncPackagesQueue = CreateIncomingPackagesQueue(fileSystemAccessor: fileSystemAccessorMock.Object);
+            incomingSyncPackagesQueue = CreateIncomingPackagesQueue(fileSystemAccessor: fileSystemAccessorMock.Object, jsonUtils: jsonUtilsMock.Object);
         };
 
         Because of = () =>
             result = incomingSyncPackagesQueue.DeQueue();
 
         It should_result_be_equal_to_first_file_in_folder = () =>
-           result.ShouldEqual(filesInFolder.First());
+            result.PathToPackage.ShouldEqual(filesInFolder.First());
+
+        It should_not_delete_sync_package = () =>
+            fileSystemAccessorMock.Verify(x => x.DeleteFile(Moq.It.IsAny<string>()), Times.Never);
 
         private static IncomingSyncPackagesQueue incomingSyncPackagesQueue;
         private static Mock<IFileSystemAccessor> fileSystemAccessorMock;
-
-        private static string result;
+        private static Guid interviewId = Guid.Parse("CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC");
+        private static IncomingSyncPackages result;
         private static string[] filesInFolder = new[] { "f1", "f2" };
     }
 }
