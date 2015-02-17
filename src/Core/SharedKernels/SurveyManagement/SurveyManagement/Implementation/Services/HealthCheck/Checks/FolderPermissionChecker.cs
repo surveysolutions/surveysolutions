891d445a5e9b228f9cbcf8d797550a3f0c46f483
Pavel Stovpivskyi <sby@rambler.ru>
2015-02-17 18:38:29 +0200

    Added IsWritePermissionExists method to FileSystemIoAccessor

--- src/Core/SharedKernels/SurveyManagement/SurveyManagement/Implementation/Services/HealthCheck/Checks/FolderPermissionChecker.cs
+++ src/Core/SharedKernels/SurveyManagement/SurveyManagement/Implementation/Services/HealthCheck/Checks/FolderPermissionChecker.cs
@@ -1,22 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Security.AccessControl;
 using System.Security.Principal;
+using WB.Core.Infrastructure.FileSystem;
 using WB.Core.SharedKernels.SurveyManagement.ValueObjects.HealthCheck;
 
 namespace WB.Core.SharedKernels.SurveyManagement.Implementation.Services.HealthCheck.Checks
 {
     public class FolderPermissionChecker : IAtomicHealthCheck<FolderPermissionCheckResult>
     {
+        private readonly IFileSystemAccessor fileSystemAccessor;
         private readonly string folderPath;
 
-        public FolderPermissionChecker(string folderPath)
+        public FolderPermissionChecker(string folderPath, IFileSystemAccessor fileSystemAccessor)
         {
+            this.fileSystemAccessor = fileSystemAccessor;
             this.folderPath = folderPath;
-        }        
-        
+        }
+
         public FolderPermissionCheckResult Check()
         {
             HashSet<string> allowedFolders = new HashSet<string>();
@@ -59,7 +61,7 @@ namespace WB.Core.SharedKernels.SurveyManagement.Implementation.Services.HealthC
 
             try
             {
-                var directories = Directory.GetDirectories(path);
+                var directories = fileSystemAccessor.GetDirectoriesInDirectory(path);
                 folders.UnionWith(directories);
 
                 foreach (var directory in directories)
@@ -79,51 +81,10 @@ namespace WB.Core.SharedKernels.SurveyManagement.Implementation.Services.HealthC
 
         public bool CheckWriteAccess(WindowsIdentity currentUserReference, string path)
         {
-            if (string.IsNullOrEmpty(path) || !Directory.Exists(path)) 
+            if (string.IsNullOrEmpty(path) || !fileSystemAccessor.IsDirectoryExists(path)) 
                 return false;
 
-            try
-            {
-                DirectorySecurity security = Directory.GetAccessControl(path);
-                var authorizationRuleCollection = security.GetAccessRules(true, true, typeof(SecurityIdentifier));
-
-                var identityReferences = new List<IdentityReference> { currentUserReference.User };
-                if (currentUserReference.Groups != null)
-                    identityReferences.AddRange(currentUserReference.Groups);
-
-                var isAllowWriteForUser = IsAllowWriteForIdentityReferance(authorizationRuleCollection, identityReferences);
-                return isAllowWriteForUser;
-            }
-            catch(UnauthorizedAccessException)
-            {
-                return false;
-            }
-        }
-
-        private static bool IsAllowWriteForIdentityReferance(
-            AuthorizationRuleCollection authorizationRuleCollection, List<IdentityReference> identityReferences)
-        {
-            var writeAllow = false;
-            var writeDeny = false;
-
-            foreach (AuthorizationRule authorizationRule in authorizationRuleCollection)
-            {
-                var isUserPermission = identityReferences.Any(ir => ir.Equals(authorizationRule.IdentityReference));
-                if (isUserPermission)
-                {
-                    FileSystemAccessRule rule = ((FileSystemAccessRule) authorizationRule);
-
-                    if ((rule.FileSystemRights & FileSystemRights.WriteData) == FileSystemRights.WriteData)
-                    {
-                        if (rule.AccessControlType == AccessControlType.Allow)
-                            writeAllow = true;
-                        else if (rule.AccessControlType == AccessControlType.Deny)
-                            writeDeny = true;
-                    }
-                }
-            }
-
-            return writeAllow && !writeDeny;
+            return fileSystemAccessor.IsWritePermissionExists(path);
         }
     }
 }
