38e5b753d26aace234929e659c9a0aedd76453ec
Roman Lytvyn <litvin.roma@gmail.com>
2015-02-19 00:23:24 +0200

    TESTER: handling internal server errors on the dashboard and login pages.

--- src/Core/BoundedContexts/WB.Core.BoundedContexts.QuestionnaireTester/ViewModels/LoginViewModel.cs
+++ src/Core/BoundedContexts/WB.Core.BoundedContexts.QuestionnaireTester/ViewModels/LoginViewModel.cs
@@ -82,6 +82,10 @@ namespace WB.Core.BoundedContexts.QuestionnaireTester.ViewModels
                     case HttpStatusCode.RequestTimeout:
                         this.UIDialogs.Alert(UIResources.RequestTimeout);
                         break;
+                    case HttpStatusCode.InternalServerError:
+                        this.Logger.Error("Internal server error when login.", ex);
+                        this.UIDialogs.Alert(UIResources.InternalServerError);
+                        break;
                     default:
                         throw;
                 }
