c4d45af776fd1a8c74e8ed6d603afe69533c1fc4
Andrii Kozhyn <hedgic@gmail.com>
2015-02-19 14:35:21 +0200

    KP-4962: Fixed interviewers selection

--- src/Core/SharedKernels/SurveyManagement/SurveyManagement/Views/Interviewer/InterviewersViewFactory.cs
+++ src/Core/SharedKernels/SurveyManagement/SurveyManagement/Views/Interviewer/InterviewersViewFactory.cs
@@ -23,28 +23,29 @@ namespace WB.Core.SharedKernels.SurveyManagement.Views.Interviewer
 
         public InterviewersView Load(InterviewersInputModel input)
         {
-            IEnumerable<UserDocument> interviewers = this.GetInterviewersListForViewer(input.ViewerId);
+            IQueryable<UserDocument> interviewers = this.GetInterviewersListForViewer(input.ViewerId);
             
-            var items = interviewers.AsQueryable()
-                                    .OrderUsingSortExpression(input.Order)
+            var items = interviewers.OrderUsingSortExpression(input.Order)
                                     .Skip((input.Page - 1) * input.PageSize)
                                     .Take(input.PageSize)
+                                    .ToList()
                                     .Select(x => new InterviewersItem(x.PublicKey, x.UserName, x.Email, x.CreationDate, x.IsLockedBySupervisor, x.IsLockedByHQ));
+
             return new InterviewersView() {Items = items, TotalCount = interviewers.Count()};
         }
 
-        protected IEnumerable<UserDocument> GetInterviewersListForViewer(Guid viewerId)
+        protected IQueryable<UserDocument> GetInterviewersListForViewer(Guid viewerId)
         {
             return this.GetTeamMembersForViewer(viewerId)
                 .Where(viewer => viewer.Roles.Any(role => role == UserRoles.Operator));
         }
 
-        protected IEnumerable<UserDocument> GetTeamMembersForViewer(Guid viewerId)
+        protected IQueryable<UserDocument> GetTeamMembersForViewer(Guid viewerId)
         {
             var viewer = this.users.GetById(viewerId);
 
             if (viewer == null)
-                return Enumerable.Empty<UserDocument>();
+                return Enumerable.Empty<UserDocument>().AsQueryable();
 
             if (viewer.IsHq())
                 return this.GetTeamMembersForHeadquarter();
@@ -57,18 +58,17 @@ namespace WB.Core.SharedKernels.SurveyManagement.Views.Interviewer
                 String.Concat(viewer.Roles)));
         }
 
-        protected IEnumerable<UserDocument> GetTeamMembersForSupervisor(Guid supervisorId)
+        protected IQueryable<UserDocument> GetTeamMembersForSupervisor(Guid supervisorId)
         {
             var indexName = typeof (UserDocumentsByBriefFields).Name;
 
             return this.indexAccessor.Query<UserDocument>(indexName)
                 .Where(user => 
                     (user.Roles.Any(role => role == UserRoles.Operator) && user.Supervisor.Id == supervisorId) || 
-                        user.PublicKey == supervisorId)
-                .ToList();
+                        user.PublicKey == supervisorId);
         }
 
-        protected IEnumerable<UserDocument> GetTeamMembersForHeadquarter()
+        protected IQueryable<UserDocument> GetTeamMembersForHeadquarter()
         {
             var indexName = typeof (UserDocumentsByBriefFields).Name;
 
