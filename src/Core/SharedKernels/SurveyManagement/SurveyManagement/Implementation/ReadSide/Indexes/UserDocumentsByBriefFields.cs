d3ce354d55795b54deecfcd71becc03e668b9520
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2015-02-19 12:34:14 +0200

    moved class outside of index

--- src/Core/SharedKernels/SurveyManagement/SurveyManagement/Implementation/ReadSide/Indexes/UserDocumentsByBriefFields.cs
+++ src/Core/SharedKernels/SurveyManagement/SurveyManagement/Implementation/ReadSide/Indexes/UserDocumentsByBriefFields.cs
@@ -8,18 +8,8 @@ using WB.Core.SharedKernels.DataCollection.Views;
 
 namespace WB.Core.SharedKernels.SurveyManagement.Implementation.ReadSide.Indexes
 {
-    public class UserDocumentsByBriefFields : AbstractIndexCreationTask<UserDocument, UserDocumentsByBriefFields.UserDocumentBrief>
+    public class UserDocumentsByBriefFields : AbstractIndexCreationTask<UserDocument, UserDocumentBrief>
     {
-        public class UserDocumentBrief
-        {
-            // ReSharper disable once InconsistentNaming
-            public Guid Supervisor_Id { get; set; }
-            public Guid PublicKey { get; set; }
-            public string UserName { get; set; }
-            public string Password { get; set; }
-            public List<UserRoles> Roles { get; set; }
-        }
-
         public UserDocumentsByBriefFields()
         {
             this.Map = users => from doc in users
@@ -35,5 +25,15 @@ namespace WB.Core.SharedKernels.SurveyManagement.Implementation.ReadSide.Indexes
             Store(x => x.Roles, FieldStorage.Yes);
             Index(x => x.Roles, FieldIndexing.Analyzed);
         }
+    }
+
+    public class UserDocumentBrief
+    {
+        // ReSharper disable once InconsistentNaming
+        public Guid Supervisor_Id { get; set; }
+        public Guid PublicKey { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public List<UserRoles> Roles { get; set; }
     }
 }
