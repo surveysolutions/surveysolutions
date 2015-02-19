be9b4fcfce1434baeb2659576a81481791b9b371
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2015-02-19 12:32:01 +0200

    Manual merge fix

--- src/Core/SharedKernels/SurveyManagement/SurveyManagement/Implementation/ReadSide/Indexes/UserDocumentsByBriefFields.cs
+++ src/Core/SharedKernels/SurveyManagement/SurveyManagement/Implementation/ReadSide/Indexes/UserDocumentsByBriefFields.cs
@@ -8,35 +8,32 @@ using WB.Core.SharedKernels.DataCollection.Views;
 
 namespace WB.Core.SharedKernels.SurveyManagement.Implementation.ReadSide.Indexes
 {
-    public class UserDocumentsByBriefFields : AbstractIndexCreationTask<UserDocument, UserDocumentBrief>
+    public class UserDocumentsByBriefFields : AbstractIndexCreationTask<UserDocument, UserDocumentsByBriefFields.UserDocumentBrief>
     {
+        public class UserDocumentBrief
+        {
+            // ReSharper disable once InconsistentNaming
+            public Guid Supervisor_Id { get; set; }
+            public Guid PublicKey { get; set; }
+            public string UserName { get; set; }
+            public string Password { get; set; }
+            public List<UserRoles> Roles { get; set; }
+        }
 
         public UserDocumentsByBriefFields()
         {
-            this.Map = interviews => from doc in interviews
-                                     where doc.IsDeleted == false
-                                     select new UserDocumentBrief { Supervisor_Id = doc.Supervisor.Id, PublicKey = doc.PublicKey, Roles = doc.Roles, UserName = doc.UserName};
+            this.Map = users => from doc in users
+                                select new UserDocumentBrief
+                                {
+                                    Supervisor_Id = doc.Supervisor.Id,
+                                    PublicKey = doc.PublicKey,
+                                    Roles = doc.Roles,
+                                    Password = doc.Password,
+                                    UserName = doc.UserName
+                                };
+
             Store(x => x.Roles, FieldStorage.Yes);
             Index(x => x.Roles, FieldIndexing.Analyzed);
         }
     }
-
-    public class UserDocumentBrief
-    {
-        // ReSharper disable once InconsistentNaming
-        public Guid Supervisor_Id { get; set; }
-        public Guid PublicKey { get; set; }
-        public string UserName { get; set; }
-            public string Password { get; set; }
-        public List<UserRoles> Roles { get; set; }
-    }
-            this.Map = users => from doc in users
-                select new UserDocumentBrief
-                {
-                    Supervisor_Id = doc.Supervisor.Id, 
-                    PublicKey = doc.PublicKey, 
-                    Roles = doc.Roles, 
-                    Password = doc.Password,
-                    UserName = doc.UserName
-                };
 }
