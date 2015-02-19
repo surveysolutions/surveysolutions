107216dd6eeafe4bc31c7e96704b5e57e455b9b1
Andrii Kozhyn <hedgic@gmail.com>
2015-02-19 11:41:58 +0200

    Removed garbage

--- src/Core/SharedKernels/SurveyManagement/SurveyManagement.Web/Models/User/UserBrowseViewFactory.cs
+++ src/Core/SharedKernels/SurveyManagement/SurveyManagement.Web/Models/User/UserBrowseViewFactory.cs
@@ -6,46 +6,15 @@ using WB.Core.SharedKernels.DataCollection.Views;
 
 namespace WB.Core.SharedKernels.SurveyManagement.Web.Models.User
 {
-    /// <summary>
-    /// The user browse view factory.
-    /// </summary>
     public class UserBrowseViewFactory : IViewFactory<UserBrowseInputModel, UserBrowseView>
     {
-        #region Fields
-
-        /// <summary>
-        /// The document item session.
-        /// </summary>
         private readonly IQueryableReadSideRepositoryReader<UserDocument> documentItemSession;
 
-        #endregion
-
-        #region Constructors and Destructors
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="UserBrowseViewFactory"/> class.
-        /// </summary>
-        /// <param name="documentItemSession">
-        /// The document item session.
-        /// </param>
         public UserBrowseViewFactory(IQueryableReadSideRepositoryReader<UserDocument> documentItemSession)
         {
             this.documentItemSession = documentItemSession;
         }
 
-        #endregion
-
-        #region Public Methods and Operators
-
-        /// <summary>
-        /// The load.
-        /// </summary>
-        /// <param name="input">
-        /// The input.
-        /// </param>
-        /// <returns>
-        /// The RavenQuestionnaire.Core.Views.User.UserBrowseView.
-        /// </returns>
         public UserBrowseView Load(UserBrowseInputModel input)
         {
             int count = this.documentItemSession.Query(queryableItems => queryableItems.Count());
@@ -61,7 +30,5 @@ namespace WB.Core.SharedKernels.SurveyManagement.Web.Models.User
             UserBrowseItem[] items = query.Select(x => new UserBrowseItem(x.PublicKey, x.UserName, x.Email, x.CreationDate, x.IsLockedBySupervisor, x.IsLockedByHQ, x.Supervisor)).ToArray();
             return new UserBrowseView(input.Page, input.PageSize, count, items.ToArray());
         }
-
-        #endregion
     }
 }
