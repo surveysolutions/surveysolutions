ccc89915315a5411fc8936fad66107c612cd54dc
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2015-02-19 14:17:10 +0200

    KP-4962: Removed base type that existed for no reason

--- src/Core/SharedKernels/SurveyManagement/SurveyManagement/Views/TakeNew/TakeNewInterviewViewFactory.cs
+++ src/Core/SharedKernels/SurveyManagement/SurveyManagement/Views/TakeNew/TakeNewInterviewViewFactory.cs
@@ -1,21 +1,30 @@
+using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
+using Main.Core.Entities.SubEntities;
 using WB.Core.GenericSubdomains.Utils;
 using WB.Core.Infrastructure.ReadSide;
 using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
 using WB.Core.SharedKernels.DataCollection.Views;
 using WB.Core.SharedKernels.DataCollection.Views.Questionnaire;
+using WB.Core.SharedKernels.SurveyManagement.Implementation.ReadSide.Indexes;
 
 namespace WB.Core.SharedKernels.SurveyManagement.Views.TakeNew
 {
-    public class TakeNewInterviewViewFactory : BaseUserViewFactory, IViewFactory<TakeNewInterviewInputModel, TakeNewInterviewView> 
+    public class TakeNewInterviewViewFactory : IViewFactory<TakeNewInterviewInputModel, TakeNewInterviewView> 
     {
         private readonly IReadSideKeyValueStorage<QuestionnaireDocumentVersioned> surveys;
+        private readonly IQueryableReadSideRepositoryReader<UserDocument> users;
+        private readonly IReadSideRepositoryIndexAccessor indexAccessor;
 
-        public TakeNewInterviewViewFactory(IReadSideKeyValueStorage<QuestionnaireDocumentVersioned> surveys, IQueryableReadSideRepositoryReader<UserDocument> users)
-            : base(users)
+        public TakeNewInterviewViewFactory(IReadSideKeyValueStorage<QuestionnaireDocumentVersioned> surveys, 
+            IQueryableReadSideRepositoryReader<UserDocument> users,
+            IReadSideRepositoryIndexAccessor indexAccessor)
         {
             this.surveys = surveys;
             this.users = users;
+            this.indexAccessor = indexAccessor;
         }
 
         public TakeNewInterviewView Load(TakeNewInterviewInputModel input)
@@ -30,6 +39,19 @@ namespace WB.Core.SharedKernels.SurveyManagement.Views.TakeNew
                 };
 
             return view;
+        }
+
+        private IEnumerable<UserDocument> GetSupervisorsListForViewer(Guid viewerId)
+        {
+            var viewer = this.users.GetById(viewerId);
+
+            if (viewer == null || !viewer.IsHq())
+                return Enumerable.Empty<UserDocument>();
+
+            var indexName = typeof(UserDocumentsByBriefFields).Name;
+
+            return this.indexAccessor.Query<UserDocument>(indexName)
+                .Where(user => user.Roles.Any(role => role == UserRoles.Supervisor));
         }
     }
 }
