2d7a82a7805b8d02c01cfaf1f82e1195152d3775
Roman Lytvyn <litvin.roma@gmail.com>
2015-02-22 02:19:35 +0200

    TESTER: fixed multi login

--- src/UI/QuestionnaireTester/WB.UI.QuestionnaireTester/Implementation/Services/QuestionnaireMetaInfoAccessor.cs
+++ src/UI/QuestionnaireTester/WB.UI.QuestionnaireTester/Implementation/Services/QuestionnaireMetaInfoAccessor.cs
@@ -2,26 +2,43 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Sqo.Transactions;
+using WB.Core.GenericSubdomains.Utils.Services;
 using WB.Core.Infrastructure.PlainStorage;
 using WB.Core.SharedKernel.Structures.Synchronization.Designer;
+using WB.UI.QuestionnaireTester.Implementation.Services;
 
 namespace WB.UI.QuestionnaireTester.Implementation.Services
 {
     internal class QuestionnaireMetaInfoAccessor : IQueryablePlainStorageAccessor<QuestionnaireMetaInfo>
     {
+        private readonly IPrincipal principal;
+
+        public QuestionnaireMetaInfoAccessor(IPrincipal principal)
+        {
+            this.principal = principal;
+        }
+
+        private class QuestionnaireMetaInfoStorageViewModel
+        {
+            public string UserName { get; set; }
+            public QuestionnaireMetaInfo MetaInfo { get; set; }
+        }
+
         public QuestionnaireMetaInfo GetById(string id)
         {
-            return SiaqodbFactory.GetInstance().Query<QuestionnaireMetaInfo>().SqoFirstOrDefault(_ => _.Id == id);
+            var storedQuestionnaireMetaInfo = SiaqodbFactory.GetInstance().Query<QuestionnaireMetaInfoStorageViewModel>().SqoFirstOrDefault(_ => _.MetaInfo.Id == id);
+
+            return storedQuestionnaireMetaInfo == null ? null : storedQuestionnaireMetaInfo.MetaInfo;
         }
 
         public void Remove(string id)
         {
-            SiaqodbFactory.GetInstance().DeleteObjectBy<QuestionnaireMetaInfo>(new Dictionary<string, object>() { { "Id", id } });
+            SiaqodbFactory.GetInstance().DeleteObjectBy<QuestionnaireMetaInfoStorageViewModel>(new Dictionary<string, object>() { { "MetaInfo.Id", id } });
         }
 
         public void Store(QuestionnaireMetaInfo view, string id)
         {
-            SiaqodbFactory.GetInstance().StoreObject(view);
+            this.Store(new[] {new Tuple<QuestionnaireMetaInfo, string>(view, view.Id)});
         }
 
         public void Store(IEnumerable<Tuple<QuestionnaireMetaInfo, string>> entities)
@@ -33,7 +50,11 @@ namespace WB.UI.QuestionnaireTester.Implementation.Services
             {
                 foreach (var entity in entities)
                 {
-                    siaqodb.StoreObject(entity, transaction);
+                    siaqodb.StoreObject(new QuestionnaireMetaInfoStorageViewModel()
+                    {
+                        MetaInfo = entity.Item1,
+                        UserName = this.principal.CurrentIdentity.Name
+                    }, transaction);
                 }
 
                 transaction.Commit();
@@ -50,9 +71,14 @@ namespace WB.UI.QuestionnaireTester.Implementation.Services
 
         }
 
+        private IEnumerable<QuestionnaireMetaInfoStorageViewModel> AllEntities()
+        {
+            return SiaqodbFactory.GetInstance().LoadAllLazy<QuestionnaireMetaInfoStorageViewModel>().Where(_=>_.UserName == this.principal.CurrentIdentity.Name);
+        }
+
         public IEnumerable<QuestionnaireMetaInfo> LoadAll()
         {
-            return SiaqodbFactory.GetInstance().LoadAllLazy<QuestionnaireMetaInfo>();
+            return AllEntities().Select(_=>_.MetaInfo);
         }
 
         public void RemoveAll()
@@ -62,7 +88,7 @@ namespace WB.UI.QuestionnaireTester.Implementation.Services
             ITransaction transaction = siaqodb.BeginTransaction();
             try
             {
-                foreach (var entity in this.LoadAll())
+                foreach (var entity in this.AllEntities())
                 {
                     siaqodb.Delete(entity, transaction);
                 }
--- src/UI/QuestionnaireTester/WB.UI.QuestionnaireTester/Implementation/Services/SiaqodbFactory.cs
+++ src/UI/QuestionnaireTester/WB.UI.QuestionnaireTester/Implementation/Services/SiaqodbFactory.cs
@@ -1,37 +1,18 @@
 using Microsoft.Practices.ServiceLocation;
 using PCLStorage;
 using Sqo;
-using WB.Core.GenericSubdomains.Utils.Services;
 
 namespace WB.UI.QuestionnaireTester.Implementation.Services
 {
     class SiaqodbFactory
     {
-        static SiaqodbFactory()
-        {
-            CreateDbFolder();
-        }
-
-        private static async void CreateDbFolder()
-        {
-            pathToPersonalUserStorage = PortablePath.Combine(FileSystem.Current.LocalStorage.Path,
-                ServiceLocator.Current.GetInstance<IPrincipal>().CurrentIdentity.Name);
-
-            if (await FileSystem.Current.LocalStorage.CheckExistsAsync(pathToPersonalUserStorage) != ExistenceCheckResult.FolderExists)
-            {
-                await FileSystem.Current.LocalStorage.CreateFolderAsync(pathToPersonalUserStorage, CreationCollisionOption.FailIfExists);
-            }
-        }
-
         private static Siaqodb instance;
-        private static string pathToPersonalUserStorage;
-
         public static Siaqodb GetInstance()
         {
             if (instance == null)
             {
                 SiaqodbConfigurator.SetDocumentSerializer(ServiceLocator.Current.GetInstance<IDocumentSerializer>());
-                instance = new Siaqodb(pathToPersonalUserStorage);
+                instance = new Siaqodb(FileSystem.Current.LocalStorage.Path);
             }
 
             return instance;
