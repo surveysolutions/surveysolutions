03a0b17b94373ca562959ec9a5b292f9a27763eb
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2015-02-23 12:35:09 +0200

    KP-4980: Fixed tests for concurrent rebuild of sync packages

---
+++ src/Tests/WB.Tests.CleanIntegration/Create.cs
@@ -0,0 +1,15 @@
+﻿using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
+using WB.Core.Synchronization.SyncStorage;
+
+namespace WB.Tests.CleanIntegration
+{
+    internal static class Create
+    {
+        public static ReadSideChunkWriter ReadSideChunkWriter(IReadSideRepositoryWriter<SynchronizationDeltaMetaInformation> metaWriter,
+            IReadSideKeyValueStorage<SynchronizationDeltaContent> inMemoryReadSideRepositoryAccessor,
+            IReadSideKeyValueStorage<SynchronizationDeltasCounter> storage)
+        {
+            return new ReadSideChunkWriter(metaWriter, inMemoryReadSideRepositoryAccessor, storage);
+        }
+    }
+}
