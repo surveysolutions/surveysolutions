db8a99fc738c74c9e1096a205f4f997a7e66fcbe
Anastasya Kholodova <nastya.kholodova@gmail.com>
2015-02-24 18:54:37 -0500

    HQ\Interview: introduce IDeleteQuestionnaireService KP-4967

---
+++ src/Core/SharedKernels/SurveyManagement/SurveyManagement/Services/DeleteQuestionnaireTemplate/IDeleteQuestionnaireService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace WB.Core.SharedKernels.SurveyManagement.Services.DeleteQuestionnaireTemplate
+{
+    public interface IDeleteQuestionnaireService
+    {
+        void DeleteQuestionnaire(Guid questionnaireId,
+            long questionnaireVersion, Guid? userId);
+    }
+}
