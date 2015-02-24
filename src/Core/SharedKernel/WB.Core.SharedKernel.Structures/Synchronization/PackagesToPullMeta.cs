f5d387935a4e5a5a2037b3dc87be905aa6d49792
Slava Scherbak <slava.scherbak@gmail.com>
2015-02-24 16:56:43 -0500

    implemented comments after code review for KP-4882

---
+++ src/Core/SharedKernel/WB.Core.SharedKernel.Structures/Synchronization/PackagesToPullMeta.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace WB.Core.SharedKernel.Structures.Synchronization
+{
+    public class PackagesToPullMeta
+    {
+        public PackagesToPullMeta(IEnumerable<SynchronizationChunkMeta> userPackages, IEnumerable<SynchronizationChunkMeta> questionnairePackages, IEnumerable<SynchronizationChunkMeta> interviewPackages)
+        {
+            this.UserPackages = userPackages;
+            this.QuestionnairePackages = questionnairePackages;
+            this.InterviewPackages = interviewPackages;
+        }
+        public IEnumerable<SynchronizationChunkMeta> UserPackages { get; private set; }
+        public IEnumerable<SynchronizationChunkMeta> QuestionnairePackages { get; private set; }
+        public IEnumerable<SynchronizationChunkMeta> InterviewPackages { get; private set; }
+    }
+}
