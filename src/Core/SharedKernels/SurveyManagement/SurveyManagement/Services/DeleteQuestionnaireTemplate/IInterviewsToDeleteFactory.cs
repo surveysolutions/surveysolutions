61533a2cf7654f040c2f96a8f6fe478e68346e3d
Anastasya Kholodova <nastya.kholodova@gmail.com>
2015-02-26 14:11:08 -0500

    HQ\DeleteQuestionnaire: cover with unit tests DeleteQuestionnaireService KP-4967

---
+++ src/Core/SharedKernels/SurveyManagement/SurveyManagement/Services/DeleteQuestionnaireTemplate/IInterviewsToDeleteFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WB.Core.SharedKernels.SurveyManagement.Views.Interview;
+
+namespace WB.Core.SharedKernels.SurveyManagement.Services.DeleteQuestionnaireTemplate
+{
+    internal interface IInterviewsToDeleteFactory
+    {
+        List<InterviewSummary> Load(Guid questionnaireId,
+            long questionnaireVersion);
+    }
+}
---
+++ src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/ServiceTests/DeleteQuestionnaireServiceTests/when_delete_unexisting_questionnaire.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Machine.Specifications;
+using Moq;
+using WB.Core.Infrastructure.CommandBus;
+using WB.Core.SharedKernels.SurveyManagement.Implementation.Services.DeleteQuestionnaireTemplate;
+using It = Machine.Specifications.It;
+
+namespace WB.Tests.Unit.SharedKernels.SurveyManagement.ServiceTests.DeleteQuestionnaireServiceTests
+{
+    internal class when_delete_unexisting_questionnaire : DeleteQuestionnaireServiceTestContext
+    {
+        Establish context = () =>
+        {
+            commandServiceMock=new Mock<ICommandService>();
+            deleteQuestionnaireService = CreateDeleteQuestionnaireService(commandService: commandServiceMock.Object);
+        };
+
+        Because of = () =>
+            exception = Catch.Exception(() =>
+                deleteQuestionnaireService.DeleteQuestionnaire(questionnaireId, questionnaireVersion, userId)) as ArgumentException;
+
+        It should_throw_ArgumentException = () =>
+            exception.ShouldBeOfExactType<ArgumentException>();
+
+        private static DeleteQuestionnaireService deleteQuestionnaireService;
+        private static Guid questionnaireId = Guid.Parse("11111111111111111111111111111111");
+        private static long questionnaireVersion = 5;
+        private static Guid userId = Guid.Parse("22222222222222222222222222222222");
+        private static Mock<ICommandService> commandServiceMock;
+        private static ArgumentException exception;
+    }
+}
