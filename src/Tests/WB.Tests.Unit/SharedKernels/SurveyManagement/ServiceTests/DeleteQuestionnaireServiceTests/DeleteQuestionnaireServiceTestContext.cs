2431fdb7829fdc12510f35d18dbd95b3f248aaaf
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2015-02-26 14:11:08 -0500

    HQ\DeleteQuestionnaire: cover with unit tests DeleteQuestionnaireService KP-4967

---
+++ src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/ServiceTests/DeleteQuestionnaireServiceTests/DeleteQuestionnaireServiceTestContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Machine.Specifications;
+using Moq;
+using WB.Core.GenericSubdomains.Utils.Services;
+using WB.Core.Infrastructure.CommandBus;
+using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
+using WB.Core.SharedKernels.DataCollection.Repositories;
+using WB.Core.SharedKernels.DataCollection.Views.Questionnaire;
+using WB.Core.SharedKernels.SurveyManagement.Implementation.Services.DeleteQuestionnaireTemplate;
+using WB.Core.SharedKernels.SurveyManagement.Services.DeleteQuestionnaireTemplate;
+
+namespace WB.Tests.Unit.SharedKernels.SurveyManagement.ServiceTests.DeleteQuestionnaireServiceTests
+{
+    [Subject(typeof(DeleteQuestionnaireService))]
+    internal class DeleteQuestionnaireServiceTestContext
+    {
+        protected static DeleteQuestionnaireService CreateDeleteQuestionnaireService(IInterviewsToDeleteFactory interviewsToDeleteFactory = null,
+           ICommandService commandService = null, IReadSideRepositoryReader<QuestionnaireBrowseItem> questionnaireBrowseItemStorage = null, IPlainQuestionnaireRepository plainQuestionnaireRepository=null)
+        {
+            return
+                new DeleteQuestionnaireService(
+                    interviewsToDeleteFactory ?? Mock.Of<IInterviewsToDeleteFactory>(),
+                    commandService ?? Mock.Of<ICommandService>(), Mock.Of<ILogger>(),
+                    questionnaireBrowseItemStorage ?? Mock.Of<IReadSideRepositoryReader<QuestionnaireBrowseItem>>(),
+                    plainQuestionnaireRepository ?? Mock.Of<IPlainQuestionnaireRepository>());
+        }
+    }
+}
