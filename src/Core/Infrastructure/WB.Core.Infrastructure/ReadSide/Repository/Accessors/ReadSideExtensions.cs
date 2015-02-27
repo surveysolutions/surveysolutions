2685a2d6eac33cd358618f0f18711c3e823e0a43
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2015-02-27 18:54:59 -0500

    HQ\DeleteQuestionnaire: code review KP-4981

--- src/Core/Infrastructure/WB.Core.Infrastructure/ReadSide/Repository/Accessors/ReadSideExtensions.cs
+++ src/Core/Infrastructure/WB.Core.Infrastructure/ReadSide/Repository/Accessors/ReadSideExtensions.cs
@@ -60,5 +60,10 @@ namespace WB.Core.Infrastructure.ReadSide.Repository.Accessors
             }
             return result;
         }
+
+        public static string AsCompositeKey(object key, object value)
+        {
+            return string.Format("{0}${1}", key, value);
+        }
     }
 }
