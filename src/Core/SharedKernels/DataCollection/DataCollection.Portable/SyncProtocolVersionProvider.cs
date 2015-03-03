374c302abed30cf1e353d9e6d7499bfb0f25f5ac
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2015-03-03 11:22:06 -0500

    Error message to reinstall message

--- src/Core/SharedKernels/DataCollection/DataCollection.Portable/SyncProtocolVersionProvider.cs
+++ src/Core/SharedKernels/DataCollection/DataCollection.Portable/SyncProtocolVersionProvider.cs
@@ -7,9 +7,17 @@ namespace WB.Core.SharedKernels.DataCollection
         //previous value 5962
         private const int SyncProtocolVersionNumber = 7000;
 
+
+        private const int NonUpdatableShiftVersionNumber = 7000;
+
         public int GetProtocolVersion()
         {
             return SyncProtocolVersionNumber;
+        }
+
+        public int GetLastNonUpdatableVersion()
+        {
+            return NonUpdatableShiftVersionNumber;
         }
     }
 }
