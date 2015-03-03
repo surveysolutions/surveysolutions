7bd39e91be7e09f42663549e8da223d34abfa1df
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2015-03-03 10:57:41 -0500

    Increase sync protocol version

--- src/Core/SharedKernels/DataCollection/DataCollection.Portable/SyncProtocolVersionProvider.cs
+++ src/Core/SharedKernels/DataCollection/DataCollection.Portable/SyncProtocolVersionProvider.cs
@@ -5,7 +5,7 @@ namespace WB.Core.SharedKernels.DataCollection
     public class SyncProtocolVersionProvider : ISyncProtocolVersionProvider
     {
         //previous value 5962
-        private const int SyncProtocolVersionNumber = 6002;
+        private const int SyncProtocolVersionNumber = 7000;
 
         public int GetProtocolVersion()
         {
