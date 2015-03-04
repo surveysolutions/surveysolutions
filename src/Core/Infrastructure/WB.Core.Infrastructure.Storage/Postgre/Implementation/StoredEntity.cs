e252ceb380cd6f85ae6d25adfa876272428b6057
Andrii Kozhyn <hedgic@gmail.com>
2015-03-04 14:17:40 +0200

    Initial version on raw sql implementation of key value store

---
+++ src/Core/Infrastructure/WB.Core.Infrastructure.Storage/Postgre/Implementation/StoredEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace WB.Core.Infrastructure.Storage.Postgre.Implementation
+{
+    public class StoredEntity
+    {
+        public string Id { get; set; }
+
+        public string Value { get; set; }
+    }
+}
