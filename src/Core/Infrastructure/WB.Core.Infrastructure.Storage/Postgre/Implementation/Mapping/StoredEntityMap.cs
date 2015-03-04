8dcb3380bbc2227461d71a82e18b94c415638bd6
Andrii Kozhyn <hedgic@gmail.com>
2015-03-04 14:17:40 +0200

    Initial version on raw sql implementation of key value store

---
+++ src/Core/Infrastructure/WB.Core.Infrastructure.Storage/Postgre/Implementation/Mapping/StoredEntityMap.cs
@@ -0,0 +1,13 @@
+﻿using NHibernate.Mapping.ByCode.Conformist;
+
+namespace WB.Core.Infrastructure.Storage.Postgre.Implementation.Mapping
+{
+    public class StoredEntityMap : ClassMapping<StoredEntity>
+    {
+        public StoredEntityMap()
+        {
+            Id(x => x.Id);
+            Property(x => x.Value);
+        }
+    }
+}
