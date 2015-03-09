e51d69657444122da1ac0c5fa4ba9c6a7d098e61
Slava Scherbak <slava.scherbak@gmail.com>
2015-03-09 14:27:36 -0400

    Designer: removed obsolete directive, multi option can be edited as text, extracted optionsService in separate file. KP-5052

--- src/UI/Designer/WB.UI.Designer/UpdatedDesigner/app/scripts/directives/matchOptionsPattern.js
+++ src/UI/Designer/WB.UI.Designer/UpdatedDesigner/app/scripts/directives/matchOptionsPattern.js
@@ -1,24 +1,4 @@
 angular.module('designerApp')
-    .directive('denyThisKeyword', function () {
-        'use strict';
-        return {
-            require: 'ngModel',
-            restrict: 'A',
-            link: function (scope, elm, attrs, ctrl) {
-                ctrl.$parsers.unshift(function (viewValue) {
-                    if (!_.isEmpty(viewValue)) {
-                        var thisKeywordUsed = viewValue.indexOf('[this]') > -1;
-                        ctrl.$setValidity('denyThisKeyword', !thisKeywordUsed);
-                    } else {
-                        ctrl.$setValidity('denyThisKeyword', true);
-                    }
-                    return viewValue;
-                });
-            }
-        };
-    });
-
-angular.module('designerApp')
     .directive('matchOptionsPattern', ['optionsService', function (optionsService) {
         'use strict';
         return {
