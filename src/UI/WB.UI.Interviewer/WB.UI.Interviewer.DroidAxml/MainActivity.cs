7ee2907324604f7694a3f9645cf0a9e98debcf8f
Pavel Stovpivskyi <630198+spuv@users.noreply.github.com>
2015-03-13 19:59:46 +0200

    Update DroidAxml project

--- src/UI/WB.UI.Interviewer/WB.UI.Interviewer.DroidAxml/MainActivity.cs
+++ src/UI/WB.UI.Interviewer/WB.UI.Interviewer.DroidAxml/MainActivity.cs
@@ -47,56 +47,48 @@ namespace AxmlTester.Droid
         {
         }
 
+        static Dictionary<Type, int> questionTemplates = new Dictionary<Type, int>()
+        {
+             { typeof(InterviewStaticText), Resource.Layout.InterviewStaticText },
+             { typeof(InterviewTextQuestion), Resource.Layout.InterviewTextQuestion },
+             { typeof(InterviewDateQuestion), Resource.Layout.InterviewDateQuestion },
+             { typeof(InterviewDecimalQuestion), Resource.Layout.InterviewDecimalQuestion },
+             { typeof(InterviewImageQuestion), Resource.Layout.InterviewImageQuestion },
+             { typeof(InterviewIntegerQuestion), Resource.Layout.InterviewIntegerQuestion },
+             { typeof(InterviewMultiChoiceQuestion), Resource.Layout.InterviewMultiChoiceQuestion },
+             { typeof(InterviewSingleChoiceQuestion), Resource.Layout.InterviewSingleChoiceQuestion },
+             { typeof(InterviewGroup), Resource.Layout.InterviewGroup },
+        }; 
+
         public override int GetItemViewType(int position)
         {
             var item = GetRawItem(position);
-            if (item is InterviewStaticText)
-                return 0;
-            else if (item is InterviewTextQuestion)
-                return 1;
-            else if (item is InterviewDateQuestion)
-                return 2;
-            else if (item is InterviewDecimalQuestion)
-                return 3;
-            else if (item is InterviewImageQuestion)
-                return 4;
-            else if (item is InterviewIntegerQuestion)
-                return 5;
-            else if (item is InterviewListQuestion)
-                return 6;
-            else if (item is InterviewQrBarcodeQuestion)
-                return 7;
+
+            int index = 0;
+            foreach (var pair in questionTemplates)
+            {
+                if (item.GetType().IsInstanceOfType(pair.Key))
+                    return index;
+
+                index++;
+            }
 
             return 0;
         }
 
         public override int ViewTypeCount
         {
-            get { return 8; }
+            get { return questionTemplates.Count; }
         }
 
         protected override View GetBindableView(View convertView, object source, int templateId)
         {
-            if (source is InterviewStaticText)
-                templateId = Resource.Layout.StaticText;
-            else if (source is InterviewTextQuestion)
-                templateId = Resource.Layout.TextQuestion;
-            else if (source is InterviewDateQuestion)
-                templateId = Resource.Layout.InterviewDateQuestion;
-            else if (source is InterviewDecimalQuestion)
-                templateId = Resource.Layout.InterviewDecimalQuestion;
-            else if (source is InterviewImageQuestion)
-                templateId = Resource.Layout.InterviewImageQuestion;
-            else if (source is InterviewIntegerQuestion)
-                templateId = Resource.Layout.InterviewIntegerQuestion;
-            else if (source is InterviewListQuestion)
-                templateId = Resource.Layout.InterviewListQuestion;
-            else if (source is InterviewQrBarcodeQuestion)
-                templateId = Resource.Layout.InterviewQrBarcodeQuestion;
+            Type type = source.GetType();
+            if (questionTemplates.ContainsKey(type))
+                templateId = questionTemplates[type];
 
             return base.GetBindableView(convertView, source, templateId);
         }
     }
-
 }
 
