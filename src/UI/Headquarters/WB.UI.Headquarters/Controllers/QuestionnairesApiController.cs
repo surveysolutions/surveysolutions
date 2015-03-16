343b16ea61ac7649005b5a929bc7dea2b9916697
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2015-03-16 15:43:04 -0400

    KP-5119 Allow "Delete template" function only for administrator

--- src/UI/Headquarters/WB.UI.Headquarters/Controllers/QuestionnairesApiController.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/Controllers/QuestionnairesApiController.cs
@@ -1,25 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
-using Raven.Client;
-using WB.Core.GenericSubdomains.Logging;
 using WB.Core.GenericSubdomains.Utils;
 using WB.Core.GenericSubdomains.Utils.Services;
 using WB.Core.Infrastructure.CommandBus;
 using WB.Core.Infrastructure.ReadSide;
-using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
-using WB.Core.SharedKernels.DataCollection.Commands.Interview;
-using WB.Core.SharedKernels.DataCollection.Commands.Questionnaire;
-using WB.Core.SharedKernels.DataCollection.Exceptions;
 using WB.Core.SharedKernels.DataCollection.Views.Questionnaire;
 using WB.Core.SharedKernels.DataCollection.Views.Questionnaire.BrowseItem;
-using WB.Core.SharedKernels.SurveyManagement.Implementation.ReadSide.Indexes;
-using WB.Core.SharedKernels.SurveyManagement.Views.Interview;
+using WB.Core.SharedKernels.SurveyManagement.Services.DeleteQuestionnaireTemplate;
 using WB.Core.SharedKernels.SurveyManagement.Web.Controllers;
 using WB.Core.SharedKernels.SurveyManagement.Web.Models;
 using WB.Core.SharedKernels.SurveyManagement.Web.Utils.Membership;
-using WB.Core.SharedKernels.SurveyManagement.Services.DeleteQuestionnaireTemplate;
 using WB.UI.Shared.Web.Filters;
 
 namespace WB.UI.Headquarters.Controllers
@@ -62,6 +53,7 @@ namespace WB.UI.Headquarters.Controllers
             return this.questionnaireBrowseViewFactory.Load(input);
         }
 
+        [Authorize(Roles = "Administrator")]
         [HttpPost]
         public JsonCommandResponse DeleteQuestionnaire(DeleteQuestionnaireRequestModel request)
         {
