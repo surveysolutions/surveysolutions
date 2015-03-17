76e4a14fa69d7142b0d9ff6e089141e2386a0bde
Pavel Stovpivskyi <sby@rambler.ru>
2015-03-17 19:39:22 +0200

    Updated InterviewImageQuestion

---
+++ src/UI/WB.UI.Interviewer/WB.UI.Interviewer.DroidAxml/Converters/ArrayOfBytesToImageConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using Android.Graphics;
+using Cirrious.CrossCore.Converters;
+
+namespace AxmlTester.Droid.Converters
+{
+    public class ByteArrayToImageConverter :
+      MvxValueConverter<byte[], Bitmap>
+    {
+        protected override Bitmap Convert(byte[] value,
+          Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return null;
+
+            var options = new BitmapFactory.Options { InPurgeable = true };
+            return BitmapFactory.DecodeByteArray(
+              value, 0, value.Length, options);
+        }
+    }
+}
