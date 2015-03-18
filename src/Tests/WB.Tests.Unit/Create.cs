72725b48c6c7e5543f027f0b4c819b0b5f54741a
Anastasya Kholodova <nastya.kholodova@gmail.com>
2015-03-18 10:08:34 -0400

    Supervisor\Synchronization: fix KP-5113

--- src/Tests/WB.Tests.Unit/Create.cs
+++ src/Tests/WB.Tests.Unit/Create.cs
@@ -1367,7 +1367,7 @@ namespace WB.Tests.Unit
                 questionsToBeEnabled ?? new List<WB.Core.SharedKernels.DataCollection.Identity>());
         }
 
-        public static InterviewState InterviewState(InterviewStatus? status = null, List<AnswerComment> answerComments = null)
+        public static InterviewState InterviewState(InterviewStatus? status = null, List<AnswerComment> answerComments = null, Guid? interviewerId=null)
         {
             return new InterviewState(Guid.NewGuid(), 1, status ?? InterviewStatus.SupervisorAssigned, new Dictionary<string, object>(),
                 new Dictionary<string, Tuple<Guid, decimal[], decimal[]>>(), new Dictionary<string, Tuple<Guid, decimal[], decimal[][]>>(),
@@ -1375,7 +1375,7 @@ namespace WB.Tests.Unit
                 answerComments ?? new List<AnswerComment>(),
                 new HashSet<string>(),
                 new HashSet<string>(), new Dictionary<string, DistinctDecimalList>(),
-                new HashSet<string>(), new HashSet<string>(), true, Mock.Of<IInterviewExpressionState>());
+                new HashSet<string>(), new HashSet<string>(), true, Mock.Of<IInterviewExpressionState>(), interviewerId?? Guid.NewGuid());
         }
 
         public static WB.Core.SharedKernels.DataCollection.Identity Identity(Guid id, decimal[] rosterVector)
