28e8aa927176dcc9e0d6337707f2324104fe926b
Andrii Kozhyn <hedgic@gmail.com>
2015-03-20 17:58:19 +0200

    KP-5101: Fixing tests

--- src/Tests/WB.Tests.Unit/Core/Synchronization/when_getting_questionnaire_ids_and_device_is_registered_and_last_package_id_is_not_empty.cs
+++ src/Tests/WB.Tests.Unit/Core/Synchronization/when_getting_questionnaire_ids_and_device_is_registered_and_last_package_id_is_not_empty.cs
@@ -9,6 +9,7 @@ using WB.Core.SharedKernel.Structures.Synchronization;
 using WB.Core.Synchronization.Documents;
 using WB.Core.Synchronization.Implementation.SyncManager;
 using WB.Core.Synchronization.SyncStorage;
+using WB.Tests.Unit.SharedKernels.SurveyManagement;
 using It = Machine.Specifications.It;
 
 namespace WB.Tests.Unit.Core.Synchronization
@@ -29,10 +30,14 @@ namespace WB.Tests.Unit.Core.Synchronization
             
             lastSyncedPackageId = questionnaireSyncPackageMetas[0].PackageId;
 
-            indexAccessorMock = new Mock<IReadSideRepositoryIndexAccessor>();
-            //indexAccessorMock.Setup(x => x.Query<QuestionnaireSyncPackageMeta>(questionnireQueryIndexName))
-            //    .Returns(questionnaireSyncPackageMetas.AsQueryable());
-            syncManager = CreateSyncManager(devices: devices, indexAccessor: indexAccessorMock.Object);
+
+            var writer = new TestInMemoryWriter<QuestionnaireSyncPackageMeta>();
+            foreach (var package in questionnaireSyncPackageMetas)
+            {
+                writer.Store(package, package.PackageId);
+            }
+
+            syncManager = CreateSyncManager(devices: devices, questionnairesReader: writer);
         };
 
         Because of = () =>
@@ -62,7 +67,6 @@ namespace WB.Tests.Unit.Core.Synchronization
         private static string lastSyncedPackageId = null;
 
         private static Guid questionnaireId = Guid.Parse("22222222222222222222222222222222");
-        private static Mock<IReadSideRepositoryIndexAccessor> indexAccessorMock;
         private static List<QuestionnaireSyncPackageMeta> questionnaireSyncPackageMetas;
     }
 }
