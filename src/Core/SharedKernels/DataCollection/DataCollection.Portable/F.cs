08c7444eea0308ec3a77b9e5f1010f7fd9aaa16c
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2015-03-20 15:44:15 -0400

    introduce F.cs

--- src/Core/SharedKernels/DataCollection/DataCollection.Portable/F.cs
+++ src/Core/SharedKernels/DataCollection/DataCollection.Portable/F.cs
@@ -6,9 +6,8 @@ using System.Text;
 
 namespace WB.Core.SharedKernels.DataCollection
 {
-    public static class ExtensionFunctions
+    public static class F
     {
-
         /// <summary>
         /// Checks if the value is within a closed interval defined by its boundary points
         /// </summary>
@@ -176,6 +175,36 @@ namespace WB.Core.SharedKernels.DataCollection
             if (IsMilTime(s.Substring(0, 4)) == false) return false;
             if (milZones.IndexOf(s.Substring(4, 1)) < 0) return false;
             return true;
+        }
+    }
+
+    public class BaseFunctions
+    {
+        public long CmCode(long? mo, long? year)
+        {
+            const long baseYear = 1900; // not sure how other calendars will be handled?? e.g. Afghanistan, Thailand, etc.
+            if (year == null) return -1;
+            if (mo == null) return -1;
+
+            if (mo.Value < 1 || mo.Value > 12) return -1;
+            if (year.Value < baseYear) return -1;
+
+            return (year.Value - baseYear) * 12 + mo.Value;
+        }
+
+        /// <summary>
+        /// Counts the occurrancies of a certain value in a group of single choice questions
+        /// </summary>
+        /// <param name="x">Specific value to be searched for.</param>
+        /// <param name="singleChoiceQuestions">One or more single choice questions.</param>
+        /// <returns>Number of occurrancies of the specified value or zero if it is never encountered.</returns>
+        public long CountValue(decimal x, params decimal?[] singleChoiceQuestions)
+        {
+            var c = 0;
+            foreach (var variable in singleChoiceQuestions)
+                if (variable.HasValue) if (variable.Value == x) c++;
+            //hello
+            return c;
         }
     }
 }
