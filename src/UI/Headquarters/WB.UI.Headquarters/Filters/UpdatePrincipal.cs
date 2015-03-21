9e9c188b0ae3a8e9b4664ad0e05cf889072a97af
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2015-03-21 00:12:24 -0400

    files for KP-5065 Introduce new Observer Role

---
+++ src/UI/Headquarters/WB.UI.Headquarters/Filters/UpdatePrincipal.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Principal;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using WB.UI.Headquarters.Code.Security;
+
+namespace WB.UI.Headquarters.Filters
+{
+    public class UpdatePrincipal : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            IPrincipal usr = HttpContext.Current.User;
+            if (usr.Identity.IsAuthenticated && usr.Identity.AuthenticationType == "Forms")
+            {
+                FormsIdentity fIdent = usr.Identity as FormsIdentity;
+                CustomIdentity ci = new CustomIdentity(fIdent.Ticket);
+                CustomPrincipal customPrincipal = new CustomPrincipal(ci);
+                HttpContext.Current.User = customPrincipal;
+                Thread.CurrentPrincipal = customPrincipal;
+            }
+
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
