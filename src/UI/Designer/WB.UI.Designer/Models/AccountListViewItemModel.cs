ab433b1ce9cec78f3103f66d72b3ded7c399001e
Slava Scherbak <slava.scherbak@gmail.com>
2015-03-23 15:25:48 -0400

    Designer: removed scaffolding for list of questionnaires and users. KP-5182

--- src/UI/Designer/WB.UI.Designer/Models/AccountListViewItemModel.cs
+++ src/UI/Designer/WB.UI.Designer/Models/AccountListViewItemModel.cs
@@ -1,62 +1,32 @@
-﻿namespace WB.UI.Designer.Models
-{
-    using System;
-    using System.ComponentModel;
-    using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 
-    /// <summary>
-    /// The account list view item model.
-    /// </summary>
-    [DisplayName("account")]
+namespace WB.UI.Designer.Models
+{
     public class AccountListViewItemModel : ActionItem
     {
-        #region Public Properties
-        /// <summary>
-        /// Gets or sets the creation date.
-        /// </summary>
         [Display(Name = "Created date", Order = 4)]
-        public string CreationDate { get; set; }
+        public DateTime CreationDate { get; set; }
 
-        /// <summary>
-        /// Gets or sets the email.
-        /// </summary>
         [Display(Name = "Email", Order = 2)]
         public string Email { get; set; }
 
-        /// <summary>
-        /// Gets or sets the id.
-        /// </summary>
         [Key]
         public Guid Id { get; set; }
 
-        /// <summary>
-        /// Gets or sets a value indicating whether is approved.
-        /// </summary>
         [Display(Name = "Approved?", Order = 5)]
         public bool IsApproved { get; set; }
 
-        /// <summary>
-        /// Gets or sets a value indicating whether is locked out.
-        /// </summary>
         [Display(Name = "Locked?", Order = 5)]
         public bool IsLockedOut { get; set; }
 
-        /// <summary>
-        /// Gets or sets the last login date.
-        /// </summary>
         [Display(Name = "Last login", Order = 3)]
         public string LastLoginDate { get; set; }
 
-        /// <summary>
-        /// Gets or sets the user name.
-        /// </summary>
         [Display(Name = "Name", Order = 1)]
         [Default]
         public string UserName { get; set; }
 
-        /// <summary>
-        ///     Gets a value indicating whether can copy.
-        /// </summary>
         public override bool CanCopy
         {
             get
@@ -65,9 +35,6 @@
             }
         }
 
-        /// <summary>
-        ///     Gets a value indicating whether can export.
-        /// </summary>
         public override bool CanExport
         {
             get
@@ -75,7 +42,5 @@
                 return false;
             }
         }
-
-        #endregion
     }
 }
