c007c1c4c158b6c50054d0a72fa4be9295e63320
Pavel Stovpivskyi <sby@rambler.ru>
2015-04-03 13:49:19 +0300

    Renames
    KP-5144

--- src/Tests/WB.Tests.Unit/Infrastructure/EventBusMobileTests/class_must_not_have_handle_events_after_unsubscribe.cs
+++ src/Tests/WB.Tests.Unit/Infrastructure/EventBusMobileTests/class_must_not_have_handle_events_after_unsubscribe.cs
@@ -5,12 +5,10 @@ using It = Machine.Specifications.It;
 
 namespace WB.Tests.Unit.Infrastructure.EventBusMobileTests
 {
-    internal class class_must_donot_handle_events_after_unsubscribe : EventBusTestsContext
+    internal class class_must_not_have_handle_events_after_unsubscribe : EventBusTestsContext
     {
         Establish context = () =>
         {
-            value = 10;
-            
             eventRegistry = CreateEventRegistry();
             eventBus = CreateEventBus(eventRegistry);
 
@@ -20,20 +18,19 @@ namespace WB.Tests.Unit.Infrastructure.EventBusMobileTests
         };
 
         Because of = () =>
-            eventBus.Publish(value);
+            eventBus.Publish(10);
 
-        It should_doesnot_call_Handle_for_int = () =>
+        It should_doesnot_call_Handle_for_int_subscription = () =>
             Mock.Get(subMock).Verify(s => s.Handle(Moq.It.IsAny<int>()), Times.Never);
 
-        It should_doesnot_call_Handle_for_long = () =>
+        It should_doesnot_call_Handle_for_long_subscription = () =>
             Mock.Get(subMock).Verify(s => s.Handle(Moq.It.IsAny<long>()), Times.Never);
 
-        It should_doesnot_call_Handle_for_string = () =>
+        It should_doesnot_call_Handle_for_string_subscription = () =>
             Mock.Get(subMock).Verify(s => s.Handle(Moq.It.IsAny<string>()), Times.Never);
 
         private static ILiteEventBus eventBus;
         private static IEventRegistry eventRegistry;
-        private static int value;
         private static DumyEventHandlers subMock;
     }
 }
