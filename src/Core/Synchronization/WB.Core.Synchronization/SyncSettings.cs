95950bdb4574701d2ae28feddad07b0e56a91f34
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2015-04-03 18:21:22 +0200

    Supervisor: code review KP-5241

--- src/Core/Synchronization/WB.Core.Synchronization/SyncSettings.cs
+++ src/Core/Synchronization/WB.Core.Synchronization/SyncSettings.cs
@@ -3,7 +3,7 @@
     public class SyncSettings
     {
         public SyncSettings(string appDataDirectory, string incomingCapiPackagesWithErrorsDirectoryName,
-            string incomingCapiPackageFileNameExtension, string incomingUnprocessedPackagesDirectoryName, string origin, int retryCount, int retryInterval)
+            string incomingCapiPackageFileNameExtension, string incomingUnprocessedPackagesDirectoryName, string origin, int retryCount, int retryIntervalInSeconds)
         {
             this.AppDataDirectory = appDataDirectory;
             this.IncomingCapiPackagesWithErrorsDirectoryName = incomingCapiPackagesWithErrorsDirectoryName;
@@ -11,7 +11,7 @@
             this.IncomingUnprocessedPackagesDirectoryName = incomingUnprocessedPackagesDirectoryName;
             this.Origin = origin;
             this.RetryCount = retryCount;
-            this.RetryInterval = retryInterval;
+            this.RetryIntervalInSeconds = retryIntervalInSeconds;
         }
 
         public string AppDataDirectory { get; private set; }
@@ -20,6 +20,6 @@
         public string IncomingCapiPackageFileNameExtension { get; private set; }
         public string Origin { get; private set; }
         public int RetryCount { get; private set; }
-        public int RetryInterval { get; private set; }
+        public int RetryIntervalInSeconds { get; private set; }
     }
 }
