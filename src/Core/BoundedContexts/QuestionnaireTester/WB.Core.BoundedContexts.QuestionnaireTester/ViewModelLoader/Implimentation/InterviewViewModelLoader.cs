f0e564d84135e579e400babe1deb8815ba990302
Pavel Stovpivskyi <sby@rambler.ru>
2015-04-06 15:57:24 +0300

    Added empty VM loader

---
+++ src/Core/BoundedContexts/QuestionnaireTester/WB.Core.BoundedContexts.QuestionnaireTester/ViewModelLoader/Implimentation/InterviewViewModelLoader.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using WB.Core.BoundedContexts.QuestionnaireTester.ViewModels;
+
+namespace WB.Core.BoundedContexts.QuestionnaireTester.ViewModelLoader.Implimentation
+{
+    public class InterviewViewModelLoader : IViewModelLoader
+    {
+        public InterviewViewModelLoader()
+        {
+        }
+
+        public ChapterViewModel Load(string interviewId, string chapterId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
