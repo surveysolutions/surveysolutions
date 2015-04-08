0640634a92735732fec50dcc29e207e6136140f9
Slava Scherbak <slava.scherbak@gmail.com>
2015-04-08 18:23:14 -0400

    Tester: added command registration for StatefullInterview and StatefullQuestionnaire AR. KP-5178

---
+++ src/Core/BoundedContexts/QuestionnaireTester/WB.Core.BoundedContexts.QuestionnaireTester/Implementation/Aggregates/StatefullInterview.cs
@@ -0,0 +1,191 @@
+﻿using WB.Core.SharedKernels.DataCollection.Events.Interview;
+
+namespace WB.Core.SharedKernels.DataCollection.Implementation.Aggregates.InterviewInfrastructure
+{
+    internal class StatefullInterview : Interview
+    {
+        #region Interview initialization
+        internal override void Apply(InterviewCreated @event)
+        {
+            base.Apply(@event);
+        }
+
+        internal override void Apply(InterviewForTestingCreated @event)
+        {
+            base.Apply(@event);
+        }
+
+        internal override void Apply(InterviewOnClientCreated @event)
+        {
+            base.Apply(@event);
+        }
+
+        internal override void Apply(InterviewSynchronized @event)
+        {
+            base.Apply(@event);
+        }
+
+        internal override void Apply(SynchronizationMetadataApplied @event)
+        {
+            base.Apply(@event);
+        } 
+        #endregion
+
+        #region Applying answers
+
+        internal override void Apply(TextQuestionAnswered @event)
+        {
+            base.Apply(@event);
+        }
+
+        internal override void Apply(QRBarcodeQuestionAnswered @event)
+        {
+            base.Apply(@event);
+        }
+
+        internal override void Apply(PictureQuestionAnswered @event)
+        {
+            base.Apply(@event);
+        }
+
+        internal override void Apply(NumericRealQuestionAnswered @event)
+        {
+            base.Apply(@event);
+        }
+
+        internal override void Apply(NumericIntegerQuestionAnswered @event)
+        {
+            base.Apply(@event);
+        }
+
+        internal override void Apply(DateTimeQuestionAnswered @event)
+        {
+            base.Apply(@event);
+        }
+
+        internal override void Apply(SingleOptionQuestionAnswered @event)
+        {
+            base.Apply(@event);
+        }
+
+        internal override void Apply(MultipleOptionsQuestionAnswered @event)
+        {
+            base.Apply(@event);
+        }
+
+        internal override void Apply(GeoLocationQuestionAnswered @event)
+        {
+            base.Apply(@event);
+        }
+
+        internal override void Apply(TextListQuestionAnswered @event)
+        {
+            base.Apply(@event);
+        }
+
+
+        internal override void Apply(SingleOptionLinkedQuestionAnswered @event)
+        {
+            base.Apply(@event);
+        }
+
+        internal override void Apply(MultipleOptionsLinkedQuestionAnswered @event)
+        {
+            base.Apply(@event);
+        }
+        
+        #endregion
+
+        internal override void Apply(AnswerCommented @event)
+        {
+            base.Apply(@event);
+        }
+
+
+        #region Group and question status and validity
+        internal override void Apply(AnswersRemoved @event)
+        {
+            base.Apply(@event);
+        }
+
+        internal override void Apply(AnswersDeclaredValid @event)
+        {
+            base.Apply(@event);
+        }
+
+        internal override void Apply(AnswersDeclaredInvalid @event)
+        {
+            base.Apply(@event);
+        }
+
+        internal override void Apply(GroupsDisabled @event)
+        {
+            base.Apply(@event);
+        }
+
+        internal override void Apply(GroupsEnabled @event)
+        {
+            base.Apply(@event);
+        }
+
+        internal override void Apply(QuestionsDisabled @event)
+        {
+            base.Apply(@event);
+        }
+
+        internal override void Apply(QuestionsEnabled @event)
+        {
+            base.Apply(@event);
+        }
+        
+        #endregion
+
+        #region Roster instances and titles
+
+        internal override void Apply(RosterInstancesTitleChanged @event)
+        {
+            base.Apply(@event);
+        }
+
+        internal override void Apply(RosterInstancesAdded @event)
+        {
+            base.Apply(@event);
+        }
+
+        internal override void Apply(RosterInstancesRemoved @event)
+        {
+            base.Apply(@event);
+        }
+
+        #endregion
+
+        #region Interview status and validity
+        
+        internal override void Apply(InterviewStatusChanged @event)
+        {
+            base.Apply(@event);
+        }
+
+        internal override void Apply(InterviewCompleted @event)
+        {
+            base.Apply(@event);
+        }
+
+        internal override void Apply(InterviewRestarted @event)
+        {
+            base.Apply(@event);
+        }
+
+        internal override void Apply(InterviewDeclaredValid @event)
+        {
+            base.Apply(@event);
+        }
+
+        internal override void Apply(InterviewDeclaredInvalid @event)
+        {
+            base.Apply(@event);
+        } 
+
+        #endregion
+    }
+}
---
+++ src/Core/BoundedContexts/QuestionnaireTester/WB.Core.BoundedContexts.QuestionnaireTester/Implementation/Aggregates/StatefullQuestionnaire.cs
@@ -0,0 +1,29 @@
+﻿using Main.Core.Events.Questionnaire;
+using WB.Core.SharedKernels.DataCollection.Events.Questionnaire;
+using WB.Core.SharedKernels.DataCollection.Implementation.Aggregates;
+
+namespace WB.Core.BoundedContexts.QuestionnaireTester.Implementation.Aggregates
+{
+    internal class StatefullQuestionnaire : Questionnaire
+    {
+        protected override internal void Apply(TemplateImported e)
+        {
+        }
+
+        protected override internal void Apply(QuestionnaireDeleted e)
+        {
+        }
+
+        protected override internal void Apply(QuestionnaireDisabled e)
+        {
+        }
+
+        protected override void Apply(PlainQuestionnaireRegistered e)
+        {
+        }
+
+        protected override internal void Apply(QuestionnaireAssemblyImported e)
+        {
+        }
+    }
+}
