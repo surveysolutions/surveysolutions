1a50bc1d54a10d466654be2bf05adb9ff03cea3c
Roman Lytvyn <litvin.roma@gmail.com>
2015-04-09 15:51:03 +0300

    Tester: Identity class renamed to UserIdentity KP-5174

--- src/Core/GenericSubdomains/Utils/Utils/Services/IPrincipal.cs
+++ src/Core/GenericSubdomains/Utils/Utils/Services/IPrincipal.cs
@@ -2,7 +2,7 @@ namespace WB.Core.GenericSubdomains.Utils.Services
 {
     public interface IPrincipal
     {
-        IIdentity CurrentIdentity { get; }
+        IUserIdentity CurrentUserIdentity { get; }
         void SignIn(string userName, string password, bool rememberMe);
         void SignOut();
     }
--- src/UI/QuestionnaireTester/WB.UI.QuestionnaireTester/Implementation/Views/UserIdentity.cs
+++ src/UI/QuestionnaireTester/WB.UI.QuestionnaireTester/Implementation/Views/UserIdentity.cs
@@ -2,7 +2,7 @@ using WB.Core.GenericSubdomains.Utils.Services;
 
 namespace WB.UI.QuestionnaireTester.Implementation.Views
 {
-    public class Identity : IIdentity
+    public class UserIdentity : IUserIdentity
     {
         public bool IsAuthenticated { get; set; }
         public string Name { get; set; }
