d45d4f3ad579d6242a5dd3a348de00625e5f9eb0
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2015-04-13 19:09:53 -0400

    Fix tests KP-5267 Fixed roster: allow users to assign fixed roster row values

--- src/Tests/WB.Tests.Unit/BoundedContexts/Designer/QuestionnaireTests/when_adding_roster_group_by_fixed_titles_and_have_more_than_250_titles.cs
+++ src/Tests/WB.Tests.Unit/BoundedContexts/Designer/QuestionnaireTests/when_adding_roster_group_by_fixed_titles_and_have_more_than_250_titles.cs
@@ -16,7 +16,7 @@ namespace WB.Tests.Unit.BoundedContexts.Designer.QuestionnaireTests
             parentGroupId = Guid.Parse("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
             groupId = Guid.Parse("BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB");
             rosterSizeSourceType = RosterSizeSourceType.FixedTitles;
-            rosterFixedTitles = new Tuple<decimal, string>[251];
+            rosterFixedTitles = new Tuple<string, string>[251];
 
             questionnaire = CreateQuestionnaire(responsibleId: responsibleId);
             questionnaire.Apply(new NewGroupAdded { PublicKey = chapterId });
@@ -56,7 +56,7 @@ namespace WB.Tests.Unit.BoundedContexts.Designer.QuestionnaireTests
         private static Guid groupId;
         private static Guid parentGroupId;
         private static RosterSizeSourceType rosterSizeSourceType;
-        private static Tuple<decimal, string>[] rosterFixedTitles;
+        private static Tuple<string, string>[] rosterFixedTitles;
         private static Exception exception;
     }
 }
--- src/Tests/WB.Tests.Unit/BoundedContexts/Designer/QuestionnaireTests/when_updating_roster_group_by_fixed_titles_and_fixed_titles_have_empty_title.cs
+++ src/Tests/WB.Tests.Unit/BoundedContexts/Designer/QuestionnaireTests/when_updating_roster_group_by_fixed_titles_and_fixed_titles_have_empty_title.cs
@@ -16,7 +16,9 @@ namespace WB.Tests.Unit.BoundedContexts.Designer.QuestionnaireTests
             parentGroupId = Guid.Parse("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
             groupId = Guid.Parse("BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB");
             rosterSizeSourceType = RosterSizeSourceType.FixedTitles;
-            rosterFixedTitles = new[] { new Tuple<decimal,string>(1,"fixed title 1"), new Tuple<decimal,string>(2," "), new Tuple<decimal,string>(2,"fixed title 3") };
+            rosterFixedTitles = new[] { new Tuple<string,string>("1","fixed title 1"), 
+                new Tuple<string,string>("2"," "), 
+                new Tuple<string,string>("2","fixed title 3") };
 
             questionnaire = CreateQuestionnaire(responsibleId: responsibleId);
             questionnaire.Apply(new NewGroupAdded { PublicKey = chapterId });
@@ -55,7 +57,7 @@ namespace WB.Tests.Unit.BoundedContexts.Designer.QuestionnaireTests
         private static Guid groupId;
         private static Guid parentGroupId;
         private static RosterSizeSourceType rosterSizeSourceType;
-        private static Tuple<decimal,string>[] rosterFixedTitles;
+        private static Tuple<string,string>[] rosterFixedTitles;
         private static Exception exception;
     }
 }
