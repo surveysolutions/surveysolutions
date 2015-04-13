da3e94ebc43cae499ec0a5cbd1e4efbbc3da1d9c
Slava Scherbak <slava.scherbak@gmail.com>
2015-04-13 16:00:58 -0400

    Tester: fixed bugs with processing imported from Designer questionnaire and assembly. KP-5178

--- src/Core/SharedKernels/DataCollection/DataCollection/Implementation/Aggregates/StatefullQuestionnaire.cs
+++ src/Core/SharedKernels/DataCollection/DataCollection/Implementation/Aggregates/StatefullQuestionnaire.cs
@@ -1,12 +1,18 @@
-﻿using Main.Core.Events.Questionnaire;
+﻿using Main.Core.Documents;
+using Main.Core.Events.Questionnaire;
 using WB.Core.SharedKernels.DataCollection.Events.Questionnaire;
 
 namespace WB.Core.SharedKernels.DataCollection.Implementation.Aggregates
 {
     internal class StatefullQuestionnaire : Questionnaire
     {
+        public StatefullQuestionnaire() { }
+
+        public QuestionnaireDocument Questionnaire { get; private set; }
+
         protected override internal void Apply(TemplateImported e)
         {
+            this.Questionnaire = e.Source;
         }
 
         protected override internal void Apply(QuestionnaireAssemblyImported e)
