a30bb22d8f268bc1c3914f745cfb0f6b67564bf0
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2015-04-13 16:39:30 -0400

    Designer: condition compilation depending on client version KP-5279

--- src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Services/IExpressionProcessorGenerator.cs
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Services/IExpressionProcessorGenerator.cs
@@ -1,13 +1,18 @@
 ﻿using System.Collections.Generic;
 using Main.Core.Documents;
+using WB.Core.SharedKernels.DataCollection;
 
 namespace WB.Core.BoundedContexts.Designer.Services
 {
     public interface IExpressionProcessorGenerator
     {
-        GenerationResult GenerateProcessorStateAssembly(QuestionnaireDocument questionnaire, out string generatedAssembly);
+        GenerationResult GenerateProcessorStateAssembly(QuestionnaireDocument questionnaire,
+            out string generatedAssembly);
+
+        GenerationResult GenerateProcessorStateAssemblyForVersion(QuestionnaireDocument questionnaire, QuestionnaireVersion version,
+          out string generatedAssembly);
+
         Dictionary<string, string> GenerateProcessorStateClasses(QuestionnaireDocument questionnaire);
         string GenerateProcessorStateSingleClass(QuestionnaireDocument questionnaire);
-
     }
 }
