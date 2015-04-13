74205bea4a3d9805df28d829a186abc0e14c26a2
Slava Scherbak <slava.scherbak@gmail.com>
2015-04-13 17:22:56 -0400

    Tester: fixed and moved interview creation to dashboard. KP-5178

--- src/Core/SharedKernels/DataCollection/DataCollection/Implementation/Aggregates/StatefullQuestionnaire.cs
+++ src/Core/SharedKernels/DataCollection/DataCollection/Implementation/Aggregates/StatefullQuestionnaire.cs
@@ -1,18 +1,27 @@
 ﻿using Main.Core.Documents;
 using Main.Core.Events.Questionnaire;
+
+using Microsoft.Practices.ServiceLocation;
+
 using WB.Core.SharedKernels.DataCollection.Events.Questionnaire;
+using WB.Core.SharedKernels.DataCollection.Repositories;
 
 namespace WB.Core.SharedKernels.DataCollection.Implementation.Aggregates
 {
     internal class StatefullQuestionnaire : Questionnaire
     {
+        private static IPlainQuestionnaireRepository QuestionnaireRepository
+        {
+            get { return ServiceLocator.Current.GetInstance<IPlainQuestionnaireRepository>(); }
+        }
+
         public StatefullQuestionnaire() { }
 
         public QuestionnaireDocument Questionnaire { get; private set; }
 
         protected override internal void Apply(TemplateImported e)
         {
-            this.Questionnaire = e.Source;
+            QuestionnaireRepository.StoreQuestionnaire(e.Source.PublicKey, 1, e.Source);
         }
 
         protected override internal void Apply(QuestionnaireAssemblyImported e)
