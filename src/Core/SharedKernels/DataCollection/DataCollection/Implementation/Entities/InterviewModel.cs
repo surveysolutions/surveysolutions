557617e5d69c355ba2701afcd9ceeccc8f1c931e
Slava Scherbak <slava.scherbak@gmail.com>
2015-04-14 18:19:31 -0400

    Tester: added code to handle question enablement and validity changes. KP-5178

--- src/Core/SharedKernels/DataCollection/DataCollection/Implementation/Entities/InterviewModel.cs
+++ src/Core/SharedKernels/DataCollection/DataCollection/Implementation/Entities/InterviewModel.cs
@@ -5,19 +5,68 @@ using WB.Core.SharedKernels.DataCollection.Aggregates;
 
 namespace WB.Core.SharedKernels.DataCollection.Implementation.Entities
 {
+    public enum QuestionModelType
+    {
+        SingleOptionQuestionModel,
+        LinkedSingleOptionQuestionModel,
+        MultiOptionQuestionModel,
+        LinkedMultiOptionQuestionModel,
+        IntegerNumericQuestionModel,
+        RealNumericQuestionModel,
+        MaskedTextQuestionModel,
+        TextListQuestionModel,
+        QrBarcodeQuestionModel,
+        MultimediaQuestionModel,
+        DateTimeQuestionModel,
+        GpsCoordinatesQuestionModel,
+    }
+
     public class InterviewModel : IInterview
     {
+        public InterviewModel()
+        {
+            Answers = new Dictionary<string, AbstractInterviewQuestionModel>();
+            Rosters = new Dictionary<string, InterviewRosterModel>();
+            Groups = new Dictionary<string, InterviewGroupModel>();
+            QuestionIdToQuestionModelTypeMap = new Dictionary<Guid, QuestionModelType>();
+        }
         public Guid QuestionnaireId { get; set; }
         public long QuestionnaireVersion { get; set; }
 
         public Guid Id { get; set; }
 
-        public Dictionary<string, AbstractInterviewQuestion> Answers { get; set; }
+        public Dictionary<string, AbstractInterviewQuestionModel> Answers { get; set; }
+        public Dictionary<string, InterviewRosterModel> Rosters { get; set; }
+        public Dictionary<string, InterviewGroupModel> Groups { get; set; }
 
-        // TODO delete after implimentation, this is only dummy method
-        public T GetAnswerOnQuestion<T>(Guid questionId)
+        public Dictionary<Guid, QuestionModelType> QuestionIdToQuestionModelTypeMap { get; set; }
+
+        public Dictionary<QuestionModelType, Func<AbstractInterviewQuestionModel>> QuestionModelTypeToModelActivatorMap = new Dictionary<QuestionModelType, Func<AbstractInterviewQuestionModel>>
+                {
+                    { QuestionModelType.SingleOptionQuestionModel, () => new SingleOptionQuestionModel()},
+                    { QuestionModelType.LinkedSingleOptionQuestionModel, () => new LinkedSingleOptionQuestionModel()},
+                    { QuestionModelType.MultiOptionQuestionModel, () => new MultiOptionQuestionModel()},
+                    { QuestionModelType.LinkedMultiOptionQuestionModel, () => new LinkedMultiOptionQuestionModel()},
+                    { QuestionModelType.IntegerNumericQuestionModel, () => new IntegerNumericQuestionModel()},
+                    { QuestionModelType.RealNumericQuestionModel, () => new RealNumericQuestionModel()},
+                    { QuestionModelType.MaskedTextQuestionModel, () => new MaskedTextQuestionModel()},
+                    { QuestionModelType.TextListQuestionModel, () => new TextListQuestionModel()},
+                    { QuestionModelType.QrBarcodeQuestionModel, () => new QrBarcodeQuestionModel()},
+                    { QuestionModelType.MultimediaQuestionModel, () => new MultimediaQuestionModel()},
+                    { QuestionModelType.DateTimeQuestionModel, () => new DateTimeQuestionModel()},
+                    { QuestionModelType.GpsCoordinatesQuestionModel, () => new GpsCoordinatesQuestionModel()}
+                };
+
+        public MaskedTextQuestionModel GetTextQuestionModel(Identity identity)
         {
-            return default(T);
+            return GetQuestionModel<MaskedTextQuestionModel>(identity);
+        }
+
+        private T GetQuestionModel<T>(Identity identity) where T : AbstractInterviewQuestionModel
+        {
+            var questionId = ConversionHelper.ConvertIdentityToString(identity);
+            if (!Answers.ContainsKey(questionId)) return null;
+            return (T)Answers[questionId];
         }
     }
 }
