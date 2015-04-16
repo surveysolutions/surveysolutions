5d82be6f479852abbc006ae41ae5dc6b956c8e82
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2015-04-16 18:20:08 +0300

    KP-5312: Added tests for teams and statuses report

---
+++ src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/TeamsAndStatusesReportTests/when_empty_data_base.cs
@@ -0,0 +1,23 @@
+﻿using Machine.Specifications;
+using WB.Core.SharedKernels.SurveyManagement.Views.Reposts.Factories;
+using WB.Core.SharedKernels.SurveyManagement.Views.Reposts.InputModels;
+using WB.Core.SharedKernels.SurveyManagement.Views.Reposts.Views;
+
+namespace WB.Tests.Unit.SharedKernels.SurveyManagement.TeamsAndStatusesReportTests
+{
+    internal class when_empty_data_base : TeamsAndStatusesReportContext
+    {
+        Establish context = () =>
+        {
+            reportFactory = CreateTeamsAndStatusesReport();
+        };
+
+        private Because of = () => report = reportFactory.Load(new TeamsAndStatusesInputModel());
+
+        private It should_return_0_records = () => report.TotalCount.ShouldEqual(0);
+
+        private static TeamsAndStatusesReport reportFactory;
+        private static TeamsAndStatusesReportView report;
+    }
+}
+
