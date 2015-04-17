c6306b7aadab1a356c26221ec2cd92f3d7829056
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2015-04-17 14:02:19 -0400

    fix KP-5065 Introduce new Observer Role

---
+++ src/Core/SharedKernels/SurveyManagement/SurveyManagement.Web/Filters/ObserverNotAllowedAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using WB.Core.SharedKernels.SurveyManagement.Web.Code.Security;
+
+
+namespace WB.Core.SharedKernels.SurveyManagement.Web.Filters
+{
+    public class ObserverNotAllowedAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (HttpContext.Current.User.Identity.IsObserver())
+            {
+                throw new HttpException(403, "Observer is not allowed to perform this action");
+            }
+            
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
