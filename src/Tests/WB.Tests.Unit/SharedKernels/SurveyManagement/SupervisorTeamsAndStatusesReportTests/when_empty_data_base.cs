54ebd932fca1e040753d71437c9d11514e32abcf
Andrii Kozhyn <hedgic@gmail.com>
2015-04-17 15:54:57 +0300

    KP-5321: Added tests for SV teams and statuses report

--- src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/SupervisorTeamsAndStatusesReportTests/when_empty_data_base.cs
+++ src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/SupervisorTeamsAndStatusesReportTests/when_empty_data_base.cs
@@ -3,9 +3,9 @@ using WB.Core.SharedKernels.SurveyManagement.Views.Reposts.Factories;
 using WB.Core.SharedKernels.SurveyManagement.Views.Reposts.InputModels;
 using WB.Core.SharedKernels.SurveyManagement.Views.Reposts.Views;
 
-namespace WB.Tests.Unit.SharedKernels.SurveyManagement.HeadquartersTeamsAndStatusesReportTests
+namespace WB.Tests.Unit.SharedKernels.SurveyManagement.SupervisorTeamsAndStatusesReportTests
 {
-    internal class when_empty_data_base : HeadquartersTeamsAndStatusesReportContext
+    internal class when_empty_data_base : SupervisorTeamsAndStatusesReportContext
     {
         Establish context = () =>
         {
@@ -16,7 +16,7 @@ namespace WB.Tests.Unit.SharedKernels.SurveyManagement.HeadquartersTeamsAndStatu
 
         private It should_return_0_records = () => report.TotalCount.ShouldEqual(0);
 
-        private static HeadquartersTeamsAndStatusesReport reportFactory;
+        private static SupervisorTeamsAndStatusesReport reportFactory;
         private static TeamsAndStatusesReportView report;
     }
 }
