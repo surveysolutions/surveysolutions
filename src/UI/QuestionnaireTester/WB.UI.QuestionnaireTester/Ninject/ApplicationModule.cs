a45f5c2de6cb92335ed6d80f66cc62cb47827a7a
Roman Lytvyn <litvin.roma@gmail.com>
2015-04-22 22:53:36 +0300

    Tester: changes by code review. KP-5172

--- src/UI/QuestionnaireTester/WB.UI.QuestionnaireTester/Ninject/ApplicationModule.cs
+++ src/UI/QuestionnaireTester/WB.UI.QuestionnaireTester/Ninject/ApplicationModule.cs
@@ -1,7 +1,6 @@
 ﻿using Ncqrs.Eventing.Storage;
 using Ninject.Modules;
 using WB.Core.BoundedContexts.QuestionnaireTester.Implementation.Services;
-using WB.Core.BoundedContexts.QuestionnaireTester.Services;
 using WB.Core.GenericSubdomains.Utils.Rest;
 using WB.Core.GenericSubdomains.Utils.Services;
 using WB.UI.QuestionnaireTester.Implementation.Services;
@@ -16,8 +15,7 @@ namespace WB.UI.QuestionnaireTester.Ninject
             this.Bind<IJsonUtils>().To<NewtonJsonUtils>().InSingletonScope();
 
             this.Bind<ApplicationSettings>().ToSelf().InSingletonScope();
-            this.Bind<DesignerApiServiceAccessor>().ToSelf().InSingletonScope();
-            this.Bind<IErrorProcessor>().To<ErrorProcessor>().InSingletonScope();
+            this.Bind<DesignerApiService>().ToSelf().InSingletonScope();
 
             var evenStore = new InMemoryEventStore();
             var snapshotStore = new InMemoryEventStore();
