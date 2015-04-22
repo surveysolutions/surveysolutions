ed773704bcbec550dc44e09e3241668ee67d0210
Slava Scherbak <slava.scherbak@gmail.com>
2015-04-22 15:51:16 -0400

    Tests: added tests for NewtonJsonUtils for deserializing questionnaire. KP-5345

---
+++ src/Tests/WB.Tests.Unit/GenericSubdomains/Utils/NewtonJsonUtilsTests/NewtonJsonUtilsTestContext.cs
@@ -0,0 +1,12 @@
+﻿using WB.Core.GenericSubdomains.Utils.Rest;
+
+namespace WB.Tests.Unit.GenericSubdomains.Utils.NewtonJsonUtilsTests
+{
+    internal class NewtonJsonUtilsTestContext
+    {
+        public static NewtonJsonUtils CreateNewtonJsonUtils()
+        {
+            return new NewtonJsonUtils();
+        }
+    }
+}
