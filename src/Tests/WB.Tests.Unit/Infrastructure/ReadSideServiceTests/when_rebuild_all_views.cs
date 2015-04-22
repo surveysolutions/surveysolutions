c455e6ce414305a01892449bf65cbb629c544aeb
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2015-04-22 11:19:50 +0300

    ReadSideService: covered pin/unpin transaction manager logic with tests KP-5071

--- src/Tests/WB.Tests.Unit/Infrastructure/ReadSideServiceTests/when_rebuild_all_views.cs
+++ src/Tests/WB.Tests.Unit/Infrastructure/ReadSideServiceTests/when_rebuild_all_views.cs
@@ -16,6 +16,7 @@ using WB.Core.Infrastructure.Implementation.ReadSide;
 using WB.Core.Infrastructure.ReadSide;
 using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
 using WB.Core.Infrastructure.Storage.Raven.Implementation.ReadSide;
+using WB.Core.Infrastructure.Transactions;
 using It = Machine.Specifications.It;
 
 namespace WB.Tests.Unit.Infrastructure.ReadSideServiceTests
@@ -39,13 +40,24 @@ namespace WB.Tests.Unit.Infrastructure.ReadSideServiceTests
             streamableEventStoreMock=new Mock<IStreamableEventStore>();
             streamableEventStoreMock.Setup(x => x.GetAllEvents())
                 .Returns(new[] { committedEvent });
-            ravenReadSideService = CreateReadSideService(eventDispatcher: eventDispatcherMock.Object, streamableEventStore: streamableEventStoreMock.Object);
+
+            transactionManagerProviderManagerMock = Mock.Get(Mock.Of<ITransactionManagerProviderManager>(_
+                => _.GetTransactionManager() == Mock.Of<ITransactionManager>()));
+
+            readSideService = CreateReadSideService(
+                eventDispatcher: eventDispatcherMock.Object,
+                streamableEventStore: streamableEventStoreMock.Object,
+                transactionManagerProviderManager: transactionManagerProviderManagerMock.Object);
         };
 
-        Because of = () => WaitRebuildReadsideFinish();
+        Because of = () =>
+        {
+            readSideService.RebuildAllViewsAsync(0);
+            WaitRebuildReadsideFinish(readSideService);
+        };
 
         It should_rebuild_all_view = () =>
-            ravenReadSideService.AreViewsBeingRebuiltNow().ShouldEqual(false);
+            readSideService.AreViewsBeingRebuiltNow().ShouldEqual(false);
 
         It should_call_clean_method_for_registered_writers_once = () =>
             readSideRepositoryCleanerMock.Verify(x=>x.Clear(), Times.Once);
@@ -56,28 +68,25 @@ namespace WB.Tests.Unit.Infrastructure.ReadSideServiceTests
         It should_disable_cache_for_registered_writers_once = () =>
            readSideRepositoryWriterMock.Verify(x => x.DisableCache(), Times.Once);
 
+        It should_pin_readside_transaction_manager = () =>
+            transactionManagerProviderManagerMock.Verify(
+                _ => _.PinRebuildReadSideTransactionManager(), Times.Once);
+
+        It should_unpin_transaction_manager = () =>
+            transactionManagerProviderManagerMock.Verify(
+                _ => _.UnpinTransactionManager(), Times.Once);
+
         It should_publish_one_event_on_event_dispatcher = () =>
             eventDispatcherMock.Verify(x => x.PublishEventToHandlers(committedEvent, Moq.It.IsAny<Dictionary<IEventHandler, Stopwatch>>()), Times.Once);
 
-        private static ReadSideService ravenReadSideService;
+        private static ReadSideService readSideService;
         private static Mock<IEventDispatcher> eventDispatcherMock;
         private static Mock<IStreamableEventStore> streamableEventStoreMock;
         private static Mock<IEventHandler> eventHandlerMock;
         private static Mock<IReadSideRepositoryCleaner> readSideRepositoryCleanerMock;
         private static Mock<IChacheableRepositoryWriter> readSideRepositoryWriterMock;
+        private static Mock<ITransactionManagerProviderManager> transactionManagerProviderManagerMock;
 
         private static CommittedEvent committedEvent;
-
-        protected static void WaitRebuildReadsideFinish()
-        {
-            ravenReadSideService.RebuildAllViewsAsync(0);
-
-            Thread.Sleep(1000);
-
-            while (ravenReadSideService.AreViewsBeingRebuiltNow())
-            {
-                Thread.Sleep(1000);
-            }
-        }
     }
 }
