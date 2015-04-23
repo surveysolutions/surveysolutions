efecdfc8529272d881d571fb5aab6eea50b26db1
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2015-04-23 16:24:32 -0400

    CodeGeneration: unit tests KP-5330

---
+++ src/Tests/WB.Tests.Unit/Applications/Designer/ImportControllerTests/when_call_Questionnaire_method_but_questionnaire_cant_be_verified.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Machine.Specifications;
+using Main.Core.Documents;
+using Moq;
+using WB.Core.BoundedContexts.Designer.Services;
+using WB.Core.BoundedContexts.Designer.ValueObjects;
+using WB.Core.BoundedContexts.Designer.Views.Questionnaire.Edit;
+using WB.Core.Infrastructure.ReadSide;
+using WB.Core.SharedKernel.Structures.Synchronization.Designer;
+using WB.UI.Designer.Api;
+using WB.UI.Shared.Web.Membership;
+using It = Machine.Specifications.It;
+
+namespace WB.Tests.Unit.Applications.Designer.ImportControllerTests
+{
+    internal class when_call_Questionnaire_method_but_questionnaire_cant_be_verified : ImportControllerTestContext
+    {
+        Establish context = () =>
+        {
+            request = new DownloadQuestionnaireRequest()
+            {
+                QuestionnaireId = questionnaireId,
+                SupportedVersion = new QuestionnnaireVersion()
+            };
+
+            var membershipUserService =
+                Mock.Of<IMembershipUserService>(
+                    _ => _.WebUser == Mock.Of<IMembershipWebUser>(u => u.UserId == userId));
+
+            var questionnaireViewFactory =
+                Mock.Of<IViewFactory<QuestionnaireViewInputModel, QuestionnaireView>>(
+                    _ =>
+                        _.Load(Moq.It.IsAny<QuestionnaireViewInputModel>()) ==
+                        new QuestionnaireView(new QuestionnaireDocument() { CreatedBy = userId }));
+
+            var expressionsEngineVersionService =
+                Mock.Of<IExpressionsEngineVersionService>(
+                    _ => _.IsClientVersionSupported(Moq.It.IsAny<Version>()) == true);
+
+            var questionnaireVerifier =
+                Mock.Of<IQuestionnaireVerifier>(
+                    _ =>
+                        _.Verify(Moq.It.IsAny<QuestionnaireDocument>()) ==
+                        new[] {new QuestionnaireVerificationError("ee", "mm")});
+
+            importController = CreateImportController(membershipUserService: membershipUserService,
+                questionnaireViewFactory: questionnaireViewFactory,
+                expressionsEngineVersionService: expressionsEngineVersionService,
+                questionnaireVerifier: questionnaireVerifier);
+        };
+
+        Because of = () =>
+            exception = Catch.Exception(() =>
+                importController.Questionnaire(request));
+
+        It should_throw_HttpResponseException = () =>
+            exception.ShouldBeOfExactType<HttpResponseException>();
+
+        It should_throw_HttpResponseException_with_StatusCode_PreconditionFailed = () =>
+            ((HttpResponseException)exception).Response.StatusCode.ShouldEqual(HttpStatusCode.PreconditionFailed);
+
+        It should_throw_HttpResponseException_with_explanation_in_ReasonPhrase = () =>
+            ((HttpResponseException)exception).Response.ReasonPhrase.ShouldEqual(
+                    "Your questionnaire has errors. Please verify the questionnaire on Designer.");
+
+        private static ImportController importController;
+        private static Exception exception;
+        private static DownloadQuestionnaireRequest request;
+        private static Guid questionnaireId = Guid.Parse("22222222222222222222222222222222");
+        private static Guid userId = Guid.Parse("33333333333333333333333333333333");
+    }
+}
