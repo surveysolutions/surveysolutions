daa1c0c423c568bcbbf4f15e20773520e3756e66
Slava Scherbak <slava.scherbak@gmail.com>
2015-04-24 16:19:08 -0400

    Tester: restored removed view models for question types. KP-5351

--- src/Core/BoundedContexts/QuestionnaireTester/WB.Core.BoundedContexts.QuestionnaireTester/ViewModels/QuestionsViewModels/RosterReferenceViewModel.cs
+++ src/Core/BoundedContexts/QuestionnaireTester/WB.Core.BoundedContexts.QuestionnaireTester/ViewModels/QuestionsViewModels/RosterReferenceViewModel.cs
@@ -1,7 +1,17 @@
-﻿namespace WB.Core.BoundedContexts.QuestionnaireTester.ViewModels.QuestionsViewModels
+﻿using WB.Core.SharedKernels.DataCollection.Implementation.Entities;
+using WB.Core.SharedKernels.DataCollection.Repositories;
+
+namespace WB.Core.BoundedContexts.QuestionnaireTester.ViewModels.QuestionsViewModels
 {
     public class RosterReferenceViewModel : GroupReferenceViewModel
     {
         public string RosterTitle { get; set; }
+
+        public RosterReferenceViewModel(
+            IPlainRepository<QuestionnaireModel> questionnaireRepository, 
+            IPlainRepository<InterviewModel> interviewRepository)
+            : base(questionnaireRepository, interviewRepository)
+        {
+        }
     }
 }
--- src/Core/BoundedContexts/QuestionnaireTester/WB.Core.BoundedContexts.QuestionnaireTester/ViewModels/QuestionsViewModels/TextListQuestionViewModel.cs
+++ src/Core/BoundedContexts/QuestionnaireTester/WB.Core.BoundedContexts.QuestionnaireTester/ViewModels/QuestionsViewModels/TextListQuestionViewModel.cs
@@ -1,53 +1,57 @@
 ﻿using System;
-
+using Cirrious.MvvmCross.ViewModels;
 using WB.Core.GenericSubdomains.Utils.Services;
 using WB.Core.Infrastructure.CommandBus;
 using WB.Core.SharedKernels.DataCollection;
 using WB.Core.SharedKernels.DataCollection.Implementation.Entities;
 using WB.Core.SharedKernels.DataCollection.Implementation.Entities.QuestionModels;
+using WB.Core.SharedKernels.DataCollection.Repositories;
 
 namespace WB.Core.BoundedContexts.QuestionnaireTester.ViewModels.QuestionsViewModels
 {
-    public class TextListQuestionViewModel : BaseInterviewItemViewModel
+    public class TextListQuestionViewModel : MvxNotifyPropertyChanged, IInterviewEntity
     {
-        private readonly ICommandService commandService;
+       private readonly ICommandService commandService;
         private readonly IPrincipal principal;
+        private IPlainRepository<QuestionnaireModel> questionnaireRepository;
+        private IPlainRepository<InterviewModel> interviewRepository;
         private Identity identity;
-        private InterviewModel interviewModel;
-        private QuestionnaireModel questionnaireModel;
+        private Guid interviewId;
 
-        public TextListQuestionViewModel(ICommandService commandService, IPrincipal principal)
+        public TextListQuestionViewModel(
+            ICommandService commandService,
+            IPrincipal principal,
+            IPlainRepository<QuestionnaireModel> questionnaireRepository,
+            IPlainRepository<InterviewModel> interviewRepository)
         {
+            if (commandService == null) throw new ArgumentNullException("commandService");
+            if (principal == null) throw new ArgumentNullException("principal");
+            if (questionnaireRepository == null) throw new ArgumentNullException("questionnaireRepository");
+            if (interviewRepository == null) throw new ArgumentNullException("interviewRepository");
+
             this.commandService = commandService;
             this.principal = principal;
+            this.questionnaireRepository = questionnaireRepository;
+            this.interviewRepository = interviewRepository;
         }
 
-        public override void Init(Identity identity, InterviewModel interviewModel, QuestionnaireModel questionnaireModel)
+        public void Init(string interviewId, Identity identity)
         {
             if (identity == null) throw new ArgumentNullException("identity");
-            if (interviewModel == null) throw new ArgumentNullException("interviewModel");
-            if (questionnaireModel == null) throw new ArgumentNullException("questionnaireModel");
+
+            var interview = this.interviewRepository.Get(interviewId);
+            //var questionnaire = this.questionnaireRepository.Get(interview.QuestionnaireId);
 
             this.identity = identity;
-            this.interviewModel = interviewModel;
-            this.questionnaireModel = questionnaireModel;
+            this.interviewId = interview.Id;
 
-            var questionModel = (TextListQuestionModel)this.questionnaireModel.Questions[this.identity.Id];
-            var answerModel = this.interviewModel.GetTextListAnswerModel(this.identity);
-
-            Title = questionModel.Title;
+            //var questionModel = (TextListQuestionModel)questionnaire.Questions[this.identity.Id];
+            var answerModel = interview.GetTextListAnswerModel(this.identity);
 
             if (answerModel != null)
             {
                 Answer = answerModel.Answers;
             }
-        }
-
-        private string title;
-        public string Title
-        {
-            get { return title; }
-            set { title = value; RaisePropertyChanged(() => Title); }
         }
 
         private Tuple<decimal, string>[] answer;
