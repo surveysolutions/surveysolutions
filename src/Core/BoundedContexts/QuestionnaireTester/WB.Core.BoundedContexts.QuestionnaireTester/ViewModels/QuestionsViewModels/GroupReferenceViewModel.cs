9138e7349fd712fe7e9eeb78bf88af344bbb3c9c
Slava Scherbak <slava.scherbak@gmail.com>
2015-04-24 15:01:51 -0400

    Revert "Tester: removed not exising interview entity view models and their templates."
    This reverts commit c6f1bb33e19a198e95d1b9dc2ad8446ebf473997.

---
+++ src/Core/BoundedContexts/QuestionnaireTester/WB.Core.BoundedContexts.QuestionnaireTester/ViewModels/QuestionsViewModels/GroupReferenceViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using WB.Core.SharedKernels.DataCollection;
+using WB.Core.SharedKernels.DataCollection.Implementation.Entities;
+
+namespace WB.Core.BoundedContexts.QuestionnaireTester.ViewModels.QuestionsViewModels
+{
+    public class GroupReferenceViewModel : BaseInterviewItemViewModel
+    {
+        public override void Init(Identity identity, InterviewModel interviewModel, QuestionnaireModel questionnaireModel)
+        {
+           
+        }
+
+        public bool IsComplete { get; set; }
+        public int CountOfAnsweredQuestions { get; set; }
+        public int CountOfCompletedGroups { get; set; }
+        public bool IsDisabled { get; set; }
+
+        private string title;
+        private Guid id;
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; RaisePropertyChanged(() => Title); }
+        }
+    }
+}
