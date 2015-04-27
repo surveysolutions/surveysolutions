b6b5ddbe8079d86785f3b95f3d13c187e13a56de
Pavel Stovpivskyi <sby@rambler.ru>
2015-04-27 11:38:23 +0300

    Added TextQuestion

--- src/Core/Infrastructure/WB.Core.Infrastructure/EventBus/Lite/Implementation/LiteEventBus.cs
+++ src/Core/Infrastructure/WB.Core.Infrastructure/EventBus/Lite/Implementation/LiteEventBus.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using Ncqrs.Eventing;
+using Ncqrs.Eventing.Sourcing;
+using Ncqrs.Eventing.Storage;
 using WB.Core.Infrastructure.Aggregates;
 
 
@@ -10,15 +12,25 @@ namespace WB.Core.Infrastructure.EventBus.Lite.Implementation
     public class LiteEventBus : ILiteEventBus
     {
         private readonly ILiteEventRegistry liteEventRegistry;
+        private readonly IEventStore eventStore;
 
-        public LiteEventBus(ILiteEventRegistry liteEventRegistry)
+        public LiteEventBus(ILiteEventRegistry liteEventRegistry, IEventStore eventStore)
         {
             this.liteEventRegistry = liteEventRegistry;
+            this.eventStore = eventStore;
         }
 
         public void PublishUncommitedEventsFromAggregateRoot(IAggregateRoot aggregateRoot, string origin)
         {
             UncommittedEvent[] uncommittedChanges = aggregateRoot.GetUncommittedChanges().ToArray();
+            var eventStream = new UncommittedEventStream(origin);
+
+            foreach (UncommittedEvent @event in uncommittedChanges)
+            {
+                eventStream.Append(@event);
+            }
+
+            this.eventStore.Store(eventStream);
 
             try
             {
