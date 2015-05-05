22b6a1bd14753817c378c3fc3abe74cded39bbbe
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2015-05-05 18:43:28 +0300

    Utils: minor renamings and comments added KP-5145

--- src/Core/GenericSubdomains/Utils/Utils/Unit.cs
+++ src/Core/GenericSubdomains/Utils/Utils/Unit.cs
@@ -1,14 +1,19 @@
 namespace WB.Core.GenericSubdomains.Utils
 {
+    /// <summary>
+    /// The unit type is the terminal object in the category of types and typed functions.
+    /// It should not be confused with the zero or bottom type, which allows no values and is the initial object in this category.
+    /// http://en.wikipedia.org/wiki/Unit_type
+    /// </summary>
     public class Unit
     {
         private Unit() { }
 
-        private static readonly Unit empty = new Unit();
+        private static readonly Unit value = new Unit();
 
-        public static Unit Empty
+        public static Unit Value
         {
-            get { return empty; }
+            get { return value; }
         }
 
         public override string ToString()
