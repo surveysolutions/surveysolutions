44610abcad421bbe6584c0323d51fcb2182ac64e
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2015-05-06 21:15:18 -0400

    Designer: move QuestionnaireVersion back to data collection :( KP-5388

--- src/UI/Designer/WB.UI.Designer/WebServices/Questionnaire/DownloadQuestionnaireRequest.cs
+++ src/UI/Designer/WB.UI.Designer/WebServices/Questionnaire/DownloadQuestionnaireRequest.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-using System.Runtime.Serialization;
 using System.ServiceModel;
-using WB.Core.SharedKernel.Structures.Synchronization.Designer;
+using WB.Core.SharedKernels.DataCollection;
 
 namespace WB.UI.Designer.WebServices.Questionnaire
 {
@@ -15,30 +14,6 @@ namespace WB.UI.Designer.WebServices.Questionnaire
         public Guid QuestionnaireId { get; set; }
 
         [MessageHeader]
-        public QuestionnnaireVersion SupportedQuestionnaireVersion { get; set; }
+        public QuestionnaireVersion SupportedQuestionnaireVersion { get; set; }
     }
-    [Obsolete("please do not remove this class, it is here for backward compatibility with HQ version smaller then 4")]
-    [DataContract]
-    public sealed class QuestionnaireVersion
-    {
-        [DataMember]
-        public int Major { get; set; }
-
-        [DataMember]
-        public int Minor { get; set; }
-
-        [DataMember]
-        public int Patch { get; set; }
-
-        public QuestionnaireVersion()
-        {
-        }
-
-        public QuestionnaireVersion(int major, int minor, int patch)
-        {
-            this.Major = major;
-            this.Minor = minor;
-            this.Patch = patch;
-        }
-    }
-} 
+}
