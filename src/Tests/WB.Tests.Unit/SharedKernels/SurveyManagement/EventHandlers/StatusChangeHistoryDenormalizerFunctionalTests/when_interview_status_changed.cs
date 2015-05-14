3c5a16c0e501046c36fae054837f3e0455ceda40
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2015-05-14 18:08:19 -0400

    HQ\Reports: return unit tests for status denormalizer KP-5411

---
+++ src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/EventHandlers/StatusChangeHistoryDenormalizerFunctionalTests/when_interview_status_changed.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Machine.Specifications;
+using WB.Core.SharedKernels.DataCollection.ValueObjects.Interview;
+using WB.Core.SharedKernels.SurveyManagement.EventHandler;
+using WB.Core.SharedKernels.SurveyManagement.Views.Interview;
+
+namespace WB.Tests.Unit.SharedKernels.SurveyManagement.EventHandlers.StatusChangeHistoryDenormalizerFunctionalTests
+{
+    internal class when_interview_status_changed : StatusChangeHistoryDenormalizerFunctionalTestContext
+    {
+        private Establish context = () =>
+        {
+            history = new InterviewStatuses();
+            history.InterviewCommentedStatuses.Add(new InterviewCommentedStatus {Status = InterviewStatus.InterviewerAssigned});
+            denormalizer = CreateDenormalizer();
+        };
+
+        private Because of =
+            () =>
+                history =
+                    denormalizer.Update(history,
+                        Create.InterviewCompletedEvent(comment: "comment"));
+
+        private It should_put_comment_to_last_status =
+            () => history.InterviewCommentedStatuses.Last().Comment.ShouldEqual("comment");
+
+        private static StatusChangeHistoryDenormalizerFunctional denormalizer;
+        private static InterviewStatuses history;
+
+    }
+}
