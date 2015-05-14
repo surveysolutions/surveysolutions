db576e6f661f0d3abf45550335b3768e9e74dbf8
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2015-05-14 18:08:19 -0400

    HQ\Reports: return unit tests for status denormalizer KP-5411

---
+++ src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/EventHandlers/StatusChangeHistoryDenormalizerFunctionalTests/StatusChangeHistoryDenormalizerFunctionalTestContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
+using WB.Core.SharedKernels.DataCollection.Views;
+using WB.Core.SharedKernels.SurveyManagement.EventHandler;
+using WB.Core.SharedKernels.SurveyManagement.Views.Interview;
+
+namespace WB.Tests.Unit.SharedKernels.SurveyManagement.EventHandlers.StatusChangeHistoryDenormalizerFunctionalTests
+{
+    internal class StatusChangeHistoryDenormalizerFunctionalTestContext
+    {
+        public static StatusChangeHistoryDenormalizerFunctional CreateDenormalizer()
+        {
+            return new StatusChangeHistoryDenormalizerFunctional(null, Mock.Of<IReadSideRepositoryWriter<UserDocument>>(),
+                Mock.Of<IReadSideRepositoryWriter<InterviewSummary>>(
+                    _ => _.GetById(Moq.It.IsAny<string>()) == new InterviewSummary()));
+        }
+    }
+}
---
+++ src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/EventHandlers/StatusChangeHistoryDenormalizerFunctionalTests/when_interview_deleted_and_restored_by_synchronization.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Machine.Specifications;
+using WB.Core.GenericSubdomains.Utils;
+using WB.Core.SharedKernels.SurveyManagement.EventHandler;
+using WB.Core.SharedKernels.SurveyManagement.Views.Interview;
+using WB.Core.SharedKernels.SurveyManagement.Implementation.Synchronization;
+
+namespace WB.Tests.Unit.SharedKernels.SurveyManagement.EventHandlers.StatusChangeHistoryDenormalizerFunctionalTests
+{
+
+    internal class when_interview_deleted_and_restored_by_synchronization : StatusChangeHistoryDenormalizerFunctionalTestContext
+    {
+        private Establish context = () =>
+        {
+            viewModel = new InterviewStatuses();
+
+            responsibleId = Guid.Parse("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA").FormatGuid();
+            denormalizerFunctional = CreateDenormalizer();
+        };
+
+        Because of = () =>
+        {
+            viewModel = denormalizerFunctional.Update(viewModel,
+                Create.InterviewDeletedEvent(userId: responsibleId, origin: Constants.HeadquartersSynchronizationOrigin));
+            viewModel = denormalizerFunctional.Update(viewModel,
+                Create.InterviewRestoredEvent(userId: responsibleId, origin: Constants.HeadquartersSynchronizationOrigin));
+        };
+
+        It should_not_add_sync_related_status_to_history = () => viewModel.InterviewCommentedStatuses.ShouldBeEmpty();
+
+        static InterviewStatuses viewModel;
+        static StatusChangeHistoryDenormalizerFunctional denormalizerFunctional;
+        static string responsibleId;
+    }
+}
