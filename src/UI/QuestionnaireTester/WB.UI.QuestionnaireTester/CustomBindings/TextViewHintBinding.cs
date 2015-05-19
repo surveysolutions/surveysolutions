ae88606f5965f427917ecf7760efdcfc983534d5
Pavel Stovpivskyi <sby@rambler.ru>
2015-05-19 16:16:54 +0300

    Added fixed hint style

--- src/UI/QuestionnaireTester/WB.UI.QuestionnaireTester/CustomBindings/TextViewHintBinding.cs
+++ src/UI/QuestionnaireTester/WB.UI.QuestionnaireTester/CustomBindings/TextViewHintBinding.cs
@@ -1,26 +1,65 @@
-﻿using Android.Graphics;
+﻿using System.Linq;
+using Android.Graphics;
 using Android.Text;
+using Android.Util;
 using Android.Widget;
 using Cirrious.MvvmCross.Binding;
-using WB.Core.GenericSubdomains.Utils;
+
 
 namespace WB.UI.QuestionnaireTester.CustomBindings
 {
     public class TextViewHintBinding : BindingWrapper<TextView, string>
     {
+        private bool subscribed;
+
         public TextViewHintBinding(TextView target)
             : base(target)
         {
+            Target.TextChanged += TextChangedHandler;
+            subscribed = true;
         }
 
         protected override void SetValueToView(TextView view, string value)
         {
-            Target.HintFormatted = Html.FromHtml("<small><i>{0}</i></small>".FormatString(value));
+            Target.Hint = value;
+        }
+
+
+        private void TextChangedHandler(object sender, TextChangedEventArgs e)
+        {
+            if (e.AfterCount > 0 && e.BeforeCount > 0 && Target.Typeface.IsBold)
+                return;
+
+            if (e.AfterCount == 0 && e.BeforeCount == 0 && Target.Typeface.IsItalic)
+                return;
+
+            if (e.Text.Any())
+            {
+                Target.SetTypeface(null, TypefaceStyle.Bold);
+            }
+            else
+            {
+                Target.SetTypeface(null, TypefaceStyle.Italic);
+            }
         }
 
         public override MvxBindingMode DefaultMode
         {
             get { return MvxBindingMode.OneWay; }
+        }
+
+        protected override void Dispose(bool isDisposing)
+        {
+            if (isDisposing)
+            {
+                var editText = Target;
+                if (editText != null && subscribed)
+                {
+                    editText.TextChanged -= TextChangedHandler;
+                    subscribed = false;
+                }
+            }
+            base.Dispose(isDisposing);
         }
     }
 }
