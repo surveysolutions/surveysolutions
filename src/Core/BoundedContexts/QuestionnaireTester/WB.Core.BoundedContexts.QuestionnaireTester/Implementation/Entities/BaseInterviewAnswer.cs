116c638f128ebbb90e79f77ba12b44cd33ab52b7
Slava Scherbak <slava.scherbak@gmail.com>
2015-05-19 15:50:22 -0400

    Tester: interview model stores only one interview now. KP-5344

--- src/Core/BoundedContexts/QuestionnaireTester/WB.Core.BoundedContexts.QuestionnaireTester/Implementation/Entities/BaseInterviewAnswer.cs
+++ src/Core/BoundedContexts/QuestionnaireTester/WB.Core.BoundedContexts.QuestionnaireTester/Implementation/Entities/BaseInterviewAnswer.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 
 namespace WB.Core.BoundedContexts.QuestionnaireTester.Implementation.Entities
 {
@@ -21,11 +20,11 @@ namespace WB.Core.BoundedContexts.QuestionnaireTester.Implementation.Entities
         public Guid Id { get; set; }
         public decimal[] RosterVector { get; set; }
 
-        public List<string> Comments { get; set; }
-
         public bool IsValid { get; set; }
         public bool IsEnabled { get; set; }
         public bool IsAnswered { get; set; }
+
+        public string InterviewerComment { get; set; }
 
         public abstract void RemoveAnswer();
     }
