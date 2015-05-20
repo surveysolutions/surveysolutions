4639090764f69f0987ddaf07cb1bfc30e417d3ea
Pavel Stovpivskyi <630198+spuv@users.noreply.github.com>
2015-05-20 16:40:21 +0300

    refactored tests for Event Bus
    KP-5144

--- src/Tests/WB.Tests.Unit/Infrastructure/LiteEventBusTests/LiteEventBusTestsContext.cs
+++ src/Tests/WB.Tests.Unit/Infrastructure/LiteEventBusTests/LiteEventBusTestsContext.cs
@@ -1,36 +1,18 @@
 using System;
 using System.Linq;
-using Machine.Specifications;
 using Moq;
 using Ncqrs.Eventing;
 using WB.Core.Infrastructure.Aggregates;
-using WB.Core.Infrastructure.EventBus.Lite;
-using WB.Core.Infrastructure.EventBus.Lite.Implementation;
+
 
 namespace WB.Tests.Unit.Infrastructure.LiteEventBusTests
 {
-    [Subject(typeof(LiteEventBus))]
     public class LiteEventBusTestsContext
     {
-        public class DumyLiteEventHandlers : ILiteEventHandler<int>, ILiteEventHandler<long>, ILiteEventHandler<string>
-        {
-            public virtual void Handle(int @event) { }
+        public class DummyEvent  { }
 
-            public virtual void Handle(long @event) { }
+        public class DifferentDummyEvent  { }
 
-            public virtual void Handle(string @event) { }
-        }
-
-        public class DummyEvent
-        {
-            public object Obj { get; set; } 
-        }
-
-        public static DummyEvent CreateDummyEvent()
-        {
-            return new DummyEvent();
-        }         
-        
         public static IAggregateRoot CreateDummyAggregateRoot(params object[] events)
         {
             UncommittedEvent[] uncommittedEvents = events.Select(CreateUncommitedEvent).ToArray();
@@ -41,13 +23,23 @@ namespace WB.Tests.Unit.Infrastructure.LiteEventBusTests
 
         private static UncommittedEvent CreateUncommitedEvent(object @event)
         {
-            //return e => Mock.Of<UncommittedEvent>(m => m.Payload == e);
             return new UncommittedEvent(Guid.Empty, Guid.Empty, 0, 0, DateTime.Now, @event);
         }
 
-        public static DumyLiteEventHandlers CreateDummyClassWithEventHandlers() 
+        internal static DummyEvent CreateDummyEvent()
         {
-            return Mock.Of<DumyLiteEventHandlers>();
-        }  
+            return new DummyEvent();
+        }
+
+        protected static IAggregateRoot SetupAggregateRootWithEventReadyForPublishing<T>(T @event)
+        {
+            return CreateDummyAggregateRoot(@event);
+        }
+
+        protected static IAggregateRoot SetupAggregateRootWithOneEventReadyForPublishing<T>() where T:new()
+        {
+            T obj = new T();
+            return CreateDummyAggregateRoot(obj);
+        }
     }
 }
---
+++ src/Tests/WB.Tests.Unit/Infrastructure/LiteEventBusTests/when_publishing_event_after_handler_was_subscribed_on_different_event.cs
@@ -0,0 +1,36 @@
+using Machine.Specifications;
+using Moq;
+using WB.Core.Infrastructure.Aggregates;
+using WB.Core.Infrastructure.EventBus.Lite;
+using It = Machine.Specifications.It;
+
+namespace WB.Tests.Unit.Infrastructure.LiteEventBusTests
+{
+    internal class when_publishing_event_after_handler_was_subscribed_on_different_event : LiteEventBusTestsContext
+    {
+        Establish context = () =>
+        {
+            eventStub = CreateDummyEvent();
+            aggregateRoot = SetupAggregateRootWithEventReadyForPublishing(eventStub);
+
+            var eventRegistry = Create.LiteEventRegistry();
+            eventBus = Create.LiteEventBus(eventRegistry);
+
+            handlerMock = new Mock<ILiteEventHandler<DifferentDummyEvent>>();
+            eventRegistry.Subscribe(handlerMock.Object);
+        };
+
+        Because of = () =>
+            eventBus.PublishUncommitedEventsFromAggregateRoot(aggregateRoot, null);
+
+        It should_not_call_Handle_for_this_handler = () =>
+            handlerMock.Verify(s => s.Handle(Moq.It.IsAny<DifferentDummyEvent>()), Times.Never);
+
+
+
+        private static ILiteEventBus eventBus;
+        private static DummyEvent eventStub;
+        private static IAggregateRoot aggregateRoot;
+        private static Mock<ILiteEventHandler<DifferentDummyEvent>> handlerMock;
+    }
+}
