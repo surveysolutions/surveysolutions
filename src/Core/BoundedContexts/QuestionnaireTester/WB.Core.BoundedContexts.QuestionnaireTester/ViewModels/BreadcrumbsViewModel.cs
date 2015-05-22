4140dee144415bbfe8a1b0acaa5798884f83124e
Andrii Kozhyn <hedgic@gmail.com>
2015-05-22 16:42:17 +0300

    KP-5192: Implemented breadcrumbs navigation

--- src/Core/BoundedContexts/QuestionnaireTester/WB.Core.BoundedContexts.QuestionnaireTester/ViewModels/BreadcrumbsViewModel.cs
+++ src/Core/BoundedContexts/QuestionnaireTester/WB.Core.BoundedContexts.QuestionnaireTester/ViewModels/BreadcrumbsViewModel.cs
@@ -1,46 +1,55 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using Cirrious.MvvmCross.ViewModels;
+using WB.Core.BoundedContexts.QuestionnaireTester.Implementation.Entities;
+using WB.Core.BoundedContexts.QuestionnaireTester.Implementation.Entities.QuestionModels;
+using WB.Core.BoundedContexts.QuestionnaireTester.Repositories;
+using WB.Core.Infrastructure.PlainStorage;
 using WB.Core.SharedKernels.DataCollection;
 
 namespace WB.Core.BoundedContexts.QuestionnaireTester.ViewModels
 {
     public class BreadcrumbsViewModel : MvxNotifyPropertyChanged
     {
+        private readonly IPlainKeyValueStorage<QuestionnaireModel> questionnaireRepository;
+        private readonly IStatefullInterviewRepository interviewRepository;
         private NavigationState navigationState;
+
+        public BreadcrumbsViewModel(IPlainKeyValueStorage<QuestionnaireModel> questionnaireRepository,
+            IStatefullInterviewRepository interviewRepository)
+        {
+            this.questionnaireRepository = questionnaireRepository;
+            this.interviewRepository = interviewRepository;
+        }
 
         public void Init(NavigationState navigationState)
         {
-            if(navigationState == null) throw new ArgumentNullException("navigationState");
+            if (navigationState == null) throw new ArgumentNullException("navigationState");
             if (this.navigationState != null) throw new Exception("ViewModel already initialized");
 
             this.navigationState = navigationState;
             this.navigationState.OnGroupChanged += navigationState_OnGroupChanged;
-
-            this.Items = new ReadOnlyCollection<BreadCrumbItemViewModel>(new List<BreadCrumbItemViewModel>
-            {
-                new BreadCrumbItemViewModel
-                {
-                    Text =  "section C: consumption of food over pst week / "
-                },
-                new BreadCrumbItemViewModel
-                {
-                    Text = "Cereals and cereals products / "
-                },
-                new BreadCrumbItemViewModel
-                {
-                    Text = "rice (paddy) / "
-                }, new BreadCrumbItemViewModel
-                {
-                    Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit / "
-                },
-            });
         }
 
         void navigationState_OnGroupChanged(Identity newGroupIdentity)
         {
-            
+            var questionnaire = questionnaireRepository.GetById(this.navigationState.QuestionnaireId);
+            List<QuestionnaireReferenceModel> parentItems = questionnaire.GroupParents[newGroupIdentity.Id];
+            var breadCrumbs = parentItems.Select(x => new BreadCrumbItemViewModel(this.navigationState)
+            {
+                ItemId = new Identity(x.Id, new decimal[] {}),
+                Text = questionnaire.GroupsWithoutNestedChildren[x.Id].Title + " / "
+            }).ToList();
+
+            breadCrumbs.Add(new BreadCrumbItemViewModel(this.navigationState)
+            {
+                ItemId = newGroupIdentity,
+                Text = questionnaire.GroupsWithoutNestedChildren[newGroupIdentity.Id].Title
+            });
+
+            this.Items = new ReadOnlyCollection<BreadCrumbItemViewModel>(breadCrumbs);
         }
 
         private ReadOnlyCollection<BreadCrumbItemViewModel> items;
@@ -53,7 +62,22 @@ namespace WB.Core.BoundedContexts.QuestionnaireTester.ViewModels
 
     public class BreadCrumbItemViewModel
     {
+        private readonly NavigationState navigationState;
+
+        public BreadCrumbItemViewModel(NavigationState navigationState)
+        {
+            this.navigationState = navigationState;
+        }
+
         public string Text { get; set; }
         public Identity ItemId { get; set; }
+
+        public IMvxCommand NavigateCommand
+        {
+            get
+            {
+                return new MvxCommand(() => navigationState.NavigateTo(ItemId));
+            }
+        }
     }
 }
