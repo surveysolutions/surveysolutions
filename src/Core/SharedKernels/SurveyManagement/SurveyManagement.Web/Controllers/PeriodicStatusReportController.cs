c24c0773d727c7876de317b0e6eb2d49a2bc1164
Anastasya Kholodova <nastya.kholodova@gmail.com>
2015-05-28 10:08:14 -0400

    HQ\SpeedReport: Reuse UI from quantity reportKP-5414

---
+++ src/Core/SharedKernels/SurveyManagement/SurveyManagement.Web/Controllers/PeriodicStatusReportController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using WB.Core.GenericSubdomains.Utils.Services;
+using WB.Core.Infrastructure.CommandBus;
+using WB.Core.Infrastructure.ReadSide;
+using WB.Core.SharedKernels.SurveyManagement.Views.Reposts;
+using WB.Core.SharedKernels.SurveyManagement.Views.Reposts.InputModels;
+using WB.Core.SharedKernels.SurveyManagement.Views.Reposts.Views;
+using WB.Core.SharedKernels.SurveyManagement.Views.UsersAndQuestionnaires;
+using WB.Core.SharedKernels.SurveyManagement.Web.Models;
+using WB.Core.SharedKernels.SurveyManagement.Web.Resources;
+using WB.Core.SharedKernels.SurveyManagement.Web.Utils.Membership;
+
+namespace WB.Core.SharedKernels.SurveyManagement.Web.Controllers
+{
+    public class PeriodicStatusReportController : BaseController
+    {
+        private readonly IViewFactory<AllUsersAndQuestionnairesInputModel, AllUsersAndQuestionnairesView> allUsersAndQuestionnairesFactory;
+
+        public PeriodicStatusReportController(
+            ICommandService commandService, 
+            IGlobalInfoProvider globalInfo, 
+            ILogger logger, 
+            IViewFactory<AllUsersAndQuestionnairesInputModel, 
+            AllUsersAndQuestionnairesView> allUsersAndQuestionnairesFactory
+            ) : base(commandService, globalInfo, logger)
+        {
+            this.allUsersAndQuestionnairesFactory = allUsersAndQuestionnairesFactory;
+        }
+
+        public ActionResult QuantityByInterviewers(Guid? supervisorId)
+        {
+            this.ViewBag.ActivePage = MenuItem.NumberOfCompletedInterviews;
+
+            AllUsersAndQuestionnairesView usersAndQuestionnaires =
+                this.allUsersAndQuestionnairesFactory.Load(new AllUsersAndQuestionnairesInputModel());
+
+            var model = new PeriodicStatusReportModel
+            {
+                WebApiActionName = "ByInterviewers",
+                CanNavigateToQuantityByTeamMember = false,
+                CanNavigateToQuantityBySupervisors = this.GlobalInfo.IsAdministrator || this.GlobalInfo.IsHeadquarter,
+                Questionnaires = usersAndQuestionnaires.Questionnaires.ToArray(),
+                ReportName = "Quantity",
+                ReportTitle = QuantityReport.NumberOfCompletedInterviews
+            };
+
+            return this.View("PeriodicStatusReport", model);
+        }
+
+        [Authorize(Roles = "Administrator, Headquarter")]
+        public ActionResult QuantityBySupervisors()
+        {
+            this.ViewBag.ActivePage = MenuItem.NumberOfCompletedInterviews;
+
+            AllUsersAndQuestionnairesView usersAndQuestionnaires =
+                this.allUsersAndQuestionnairesFactory.Load(new AllUsersAndQuestionnairesInputModel());
+
+            var model = new PeriodicStatusReportModel
+            {
+                WebApiActionName = "BySupervisors",
+                CanNavigateToQuantityByTeamMember = true,
+                CanNavigateToQuantityBySupervisors = false,
+                Questionnaires = usersAndQuestionnaires.Questionnaires.ToArray(),
+                ReportName = "Quantity",
+                ReportTitle = QuantityReport.NumberOfCompletedInterviews
+            };
+
+            return this.View("PeriodicStatusReport", model);
+        }
+        public ActionResult SpeedByInterviewers(Guid? supervisorId)
+        {
+            this.ViewBag.ActivePage = MenuItem.SpeedOfCompletingInterviews;
+
+            AllUsersAndQuestionnairesView usersAndQuestionnaires =
+                this.allUsersAndQuestionnairesFactory.Load(new AllUsersAndQuestionnairesInputModel());
+
+            var model = new PeriodicStatusReportModel
+            {
+                WebApiActionName = "ByInterviewers",
+                CanNavigateToQuantityByTeamMember = false,
+                CanNavigateToQuantityBySupervisors = this.GlobalInfo.IsAdministrator || this.GlobalInfo.IsHeadquarter,
+                Questionnaires = usersAndQuestionnaires.Questionnaires.ToArray(),
+                ReportName = "Speed",
+                ReportTitle = QuantityReport.TimeSpentToCompleteInterviewInMinutes
+            };
+
+            return this.View("PeriodicStatusReport", model);
+        }
+
+        [Authorize(Roles = "Administrator, Headquarter")]
+        public ActionResult SpeedBySupervisors()
+        {
+            this.ViewBag.ActivePage = MenuItem.SpeedOfCompletingInterviews;
+
+            AllUsersAndQuestionnairesView usersAndQuestionnaires =
+                this.allUsersAndQuestionnairesFactory.Load(new AllUsersAndQuestionnairesInputModel());
+
+            var model = new PeriodicStatusReportModel
+            {
+                WebApiActionName = "BySupervisors",
+                CanNavigateToQuantityByTeamMember = true,
+                CanNavigateToQuantityBySupervisors = false,
+                Questionnaires = usersAndQuestionnaires.Questionnaires.ToArray(),
+                ReportName = "Speed",
+                ReportTitle = QuantityReport.TimeSpentToCompleteInterviewInMinutes
+            };
+
+            return this.View("PeriodicStatusReport", model);
+        }
+    }
+}
---
+++ src/Core/SharedKernels/SurveyManagement/SurveyManagement.Web/Models/SpeedBySupervisorsReportModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WB.Core.GenericSubdomains.Utils;
+using WB.Core.SharedKernels.SurveyManagement.Views.Reposts.InputModels;
+
+namespace WB.Core.SharedKernels.SurveyManagement.Web.Models
+{
+    public class SpeedBySupervisorsReportModel : IGridRequest<SpeedBySupervisorsReportInputModel>
+    {
+        public PagerData Pager { get; set; }
+
+        public SpeedBySupervisorsReportInputModel Request { get; set; }
+
+        public IEnumerable<OrderRequestItem> SortOrder { get; set; }
+    }
+}
