4abe584b2f87bee1a6b83354833bf81a0157da90
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2015-05-28 15:11:09 -0400

    HQ\PeriodicReport: unit tests and naming clean up for periodic preports KP-5414

--- src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/PeriodicStatusReportTests/when_request_speed_report_by_supervisors.cs
+++ src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/PeriodicStatusReportTests/when_request_speed_report_by_supervisors.cs
@@ -1,13 +1,17 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
 using Machine.Specifications;
 using WB.Core.SharedKernels.SurveyManagement.Views.Reposts;
-using WB.Core.SharedKernels.SurveyManagement.Web.Controllers;
 using WB.Core.SharedKernels.SurveyManagement.Web.Models;
-using It = Machine.Specifications.It;
+using WB.Core.SharedKernels.SurveyManagement.Web.Controllers;
 
 namespace WB.Tests.Unit.SharedKernels.SurveyManagement.PeriodicStatusReportTests
 {
-    internal class when_request_report_by_supervisors : PeriodicStatusReportControllerTestContext
+    internal class when_request_speed_report_by_supervisors : PeriodicStatusReportControllerTestContext
     {
         Establish context = () =>
         {
@@ -15,10 +19,10 @@ namespace WB.Tests.Unit.SharedKernels.SurveyManagement.PeriodicStatusReportTests
         };
 
         Because of = () =>
-            result = periodicStatusReportController.QuantityBySupervisors() as ViewResult;
+            result = periodicStatusReportController.SpeedBySupervisors() as ViewResult;
 
-        It should_active_page_be_NumberOfCompletedInterviews = () =>
-            ((MenuItem)result.ViewBag.ActivePage).ShouldEqual(MenuItem.NumberOfCompletedInterviews);
+        It should_active_page_be_SpeedOfCompletedInterviews = () =>
+            ((MenuItem)result.ViewBag.ActivePage).ShouldEqual(MenuItem.SpeedOfCompletingInterviews);
 
         It should_responsible_name_be_a_link = () =>
             ((PeriodicStatusReportModel)result.Model).CanNavigateToQuantityByTeamMember.ShouldEqual(true);
@@ -26,8 +30,11 @@ namespace WB.Tests.Unit.SharedKernels.SurveyManagement.PeriodicStatusReportTests
         It should_go_back_to_supervisor_button_be_invisible = () =>
             ((PeriodicStatusReportModel)result.Model).CanNavigateToQuantityBySupervisors.ShouldEqual(false);
 
-        It should_WebApiActionName_be_QuantityBySupervisors = () =>
+        It should_WebApiActionName_be_BySupervisors = () =>
            ((PeriodicStatusReportModel)result.Model).WebApiActionName.ShouldEqual("BySupervisors");
+
+        It should_ReportName_be_Speed = () =>
+           ((PeriodicStatusReportModel)result.Model).ReportName.ShouldEqual("Speed");
 
         private static PeriodicStatusReportController periodicStatusReportController;
         private static ViewResult result;
