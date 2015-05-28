9b3207256143baee921c38dc6209437feace9f6f
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2015-05-28 10:08:14 -0400

    HQ\SpeedReport: Reuse UI from quantity reportKP-5414

---
+++ src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/PeriodicStatusReportTests/when_request_report_by_interviewers_for_user_in_supervisor_role.cs
@@ -0,0 +1,38 @@
+﻿using System.Web.Mvc;
+using Machine.Specifications;
+using Moq;
+using WB.Core.SharedKernels.SurveyManagement.Views.Reposts;
+using WB.Core.SharedKernels.SurveyManagement.Web.Controllers;
+using WB.Core.SharedKernels.SurveyManagement.Web.Models;
+using WB.Core.SharedKernels.SurveyManagement.Web.Utils.Membership;
+using It = Machine.Specifications.It;
+
+namespace WB.Tests.Unit.SharedKernels.SurveyManagement.PeriodicStatusReportTests
+{
+    internal class when_request_report_by_interviewers_for_user_in_supervisor_role : PeriodicStatusReportControllerTestContext
+    {
+        Establish context = () =>
+        {
+            IGlobalInfoProvider globalInfoProvider = Mock.Of<IGlobalInfoProvider>(_ => _.IsSurepvisor == true);
+            periodicStatusReportController = CreatePeriodicStatusReportController(globalInfoProvider: globalInfoProvider);
+        };
+
+        Because of = () =>
+            result = periodicStatusReportController.QuantityByInterviewers(null) as ViewResult;
+
+        It should_active_page_be_NumberOfCompletedInterviews = () =>
+            ((MenuItem)result.ViewBag.ActivePage).ShouldEqual(MenuItem.NumberOfCompletedInterviews);
+
+        It should_responsible_name_be_not_a_link = () =>
+            ((PeriodicStatusReportModel)result.Model).CanNavigateToQuantityByTeamMember.ShouldEqual(false);
+
+        It should_go_back_to_supervisor_button_be_invisible = () =>
+            ((PeriodicStatusReportModel)result.Model).CanNavigateToQuantityBySupervisors.ShouldEqual(false);
+
+        It should_WebApiActionName_be_QuantityByInterviewers = () =>
+           ((PeriodicStatusReportModel)result.Model).WebApiActionName.ShouldEqual("ByInterviewers");
+            
+        private static PeriodicStatusReportController periodicStatusReportController;
+        private static ViewResult result;
+    }
+}
