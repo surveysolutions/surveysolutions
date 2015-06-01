2fdb9a324a23df55c4eaacc23d2172781037ae4b
Slava Scherbak <slava.scherbak@gmail.com>
2015-06-01 14:40:23 -0400

    Tester: added basic tests for text list view model. KP-5155

---
+++ src/Tests/WB.Tests.Unit/BoundedContexts/Tester/TextListQuestionViewModelTests/TextListQuestionViewModelTestContext.cs
@@ -0,0 +1,35 @@
+﻿using Chance.MvvmCross.Plugins.UserInteraction;
+
+using Moq;
+using WB.Core.BoundedContexts.QuestionnaireTester.Implementation.Entities;
+using WB.Core.BoundedContexts.QuestionnaireTester.Infrastructure;
+using WB.Core.BoundedContexts.QuestionnaireTester.Repositories;
+using WB.Core.BoundedContexts.QuestionnaireTester.ViewModels;
+using WB.Core.BoundedContexts.QuestionnaireTester.ViewModels.QuestionsViewModels;
+using WB.Core.BoundedContexts.QuestionnaireTester.ViewModels.QuestionStateViewModels;
+using WB.Core.Infrastructure.PlainStorage;
+using WB.Core.SharedKernels.DataCollection.Events.Interview;
+
+using it = Moq.It;
+
+namespace WB.Tests.Unit.BoundedContexts.Tester.TextListQuestionViewModelTests
+{
+    public class TextListQuestionViewModelTestContext
+    {
+        protected static TextListQuestionViewModel CreateTextListQuestionViewModel(IPrincipal principal = null,
+            IPlainKeyValueStorage<QuestionnaireModel> questionnaireRepository = null,
+            IStatefullInterviewRepository interviewRepository = null,
+            QuestionStateViewModel<TextListQuestionAnswered> questionStateViewModel = null,
+            IUserInteraction userInteraction = null,
+            AnsweringViewModel answering = null)
+        {
+            return new TextListQuestionViewModel(
+                principal ?? Mock.Of<IPrincipal>(),
+                questionnaireRepository ?? Mock.Of<IPlainKeyValueStorage<QuestionnaireModel>>(),
+                interviewRepository ?? Mock.Of<IStatefullInterviewRepository>(),
+                questionStateViewModel ?? Mock.Of<QuestionStateViewModel<TextListQuestionAnswered>>(),
+                userInteraction ?? Mock.Of<IUserInteraction>(),
+                answering ?? Mock.Of<AnsweringViewModel>());
+        }
+    }
+}
