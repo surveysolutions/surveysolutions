bc779548b5aec78232cd9f1c5ce0aa3cdfbc9ba7
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2015-06-01 17:46:09 -0400

    interface part KP-5472 Remove mandatory flag for question

--- src/UI/Designer/WB.UI.Designer/UpdatedDesigner/app/scripts/services/helpMessages.js
+++ src/UI/Designer/WB.UI.Designer/UpdatedDesigner/app/scripts/services/helpMessages.js
@@ -18,7 +18,8 @@
                 'validationExpression': 'A logical expression that validates an answer to the current question. Might include values of other questions.',
                 'validationMessage': 'Error message is shown when an answer to the current question fails a validation condition.',
                 'sourceQuestion': 'Source question (answer generates number of rows)',
-                'fixedTitles': 'Number of items determines the roster size. Each element of the list corresponds to a title of the roster row. Each element of the list should be placed on a separate line.'
+                'fixedTitles': 'Number of items determines the roster size. Each element of the list corresponds to a title of the roster row. Each element of the list should be placed on a separate line.',
+                'mandatory': 'This property will be deprecated soon. You may only uncheck this option if it was checked.'
             };
 
             helpService.getHelpMessage = function(key) {
