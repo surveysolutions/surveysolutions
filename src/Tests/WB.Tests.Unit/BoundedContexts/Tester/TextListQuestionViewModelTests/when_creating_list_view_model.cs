4d2e6c6aa10eeb720ea99db084fd15dd83b8ec05
Slava Scherbak <slava.scherbak@gmail.com>
2015-06-01 17:42:41 -0400

    Tester: ignored tests

--- src/Tests/WB.Tests.Unit/BoundedContexts/Tester/TextListQuestionViewModelTests/when_creating_list_view_model.cs
+++ src/Tests/WB.Tests.Unit/BoundedContexts/Tester/TextListQuestionViewModelTests/when_creating_list_view_model.cs
@@ -1,13 +1,28 @@
 using Machine.Specifications;
 
+using Moq;
+
+using WB.Core.BoundedContexts.QuestionnaireTester.ViewModels;
 using WB.Core.BoundedContexts.QuestionnaireTester.ViewModels.QuestionsViewModels;
+using WB.Core.BoundedContexts.QuestionnaireTester.ViewModels.QuestionStateViewModels;
+using WB.Core.SharedKernels.DataCollection.Events.Interview;
+
+using It = Machine.Specifications.It;
 
 namespace WB.Tests.Unit.BoundedContexts.Tester.TextListQuestionViewModelTests
 {
+    [Ignore("KP-5155")]
     public class when_creating_list_view_model : TextListQuestionViewModelTestContext
     {
-        Because of = () => 
-            listModel = CreateTextListQuestionViewModel();
+        Establish context = () =>
+        {
+            
+        };
+
+        Because of = () =>
+            listModel = CreateTextListQuestionViewModel(
+            questionStateViewModel: questionStateMock.Object, 
+            answering: answeringViewModelMock.Object);
 
         It should_set_QuestionState_with_non_null_value = () =>
             listModel.QuestionState.ShouldNotBeNull();
@@ -19,5 +34,10 @@ namespace WB.Tests.Unit.BoundedContexts.Tester.TextListQuestionViewModelTests
             listModel.Answers.ShouldNotBeNull();
 
         static TextListQuestionViewModel listModel;
+
+        static Mock<QuestionStateViewModel<TextListQuestionAnswered>> questionStateMock = new Mock<QuestionStateViewModel<TextListQuestionAnswered>>();
+        static Mock<AnsweringViewModel> answeringViewModelMock = new Mock<AnsweringViewModel>();
+        
+
     }
 }
--- src/Tests/WB.Tests.Unit/BoundedContexts/Tester/TextListQuestionViewModelTests/when_initializing_list_view_model_and_interviewId_is_missing_entityIdentity.cs
+++ src/Tests/WB.Tests.Unit/BoundedContexts/Tester/TextListQuestionViewModelTests/when_initializing_list_view_model_and_interviewId_is_missing_entityIdentity.cs
@@ -1,16 +1,23 @@
 using System;
-
 using Machine.Specifications;
-
+using Moq;
+using WB.Core.BoundedContexts.QuestionnaireTester.ViewModels;
 using WB.Core.BoundedContexts.QuestionnaireTester.ViewModels.QuestionsViewModels;
+using WB.Core.BoundedContexts.QuestionnaireTester.ViewModels.QuestionStateViewModels;
+using WB.Core.SharedKernels.DataCollection.Events.Interview;
+
+using It = Machine.Specifications.It;
 
 namespace WB.Tests.Unit.BoundedContexts.Tester.TextListQuestionViewModelTests
 {
+    [Ignore("KP-5155")]
     public class when_initializing_list_view_model_and_interviewId_is_missing_entityIdentity : TextListQuestionViewModelTestContext
     {
         Establish context = () =>
         {
-            listModel = CreateTextListQuestionViewModel();
+            var questionStateMock = new Mock<QuestionStateViewModel<TextListQuestionAnswered>>();
+            var answeringViewModelMock = new Mock<AnsweringViewModel>();
+            listModel = CreateTextListQuestionViewModel(questionStateMock.Object, answeringViewModelMock.Object);
         };
 
         Because of = () =>
