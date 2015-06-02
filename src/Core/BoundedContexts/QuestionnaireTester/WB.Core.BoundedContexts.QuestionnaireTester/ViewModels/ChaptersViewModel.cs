d6996110338ec3d21120d37f30fb5d4671fcfae6
Slava Scherbak <slava.scherbak@gmail.com>
2015-06-02 13:24:22 -0400

    Tester: added basic infrastructure for list of chapters. KP-5484

--- src/Core/BoundedContexts/QuestionnaireTester/WB.Core.BoundedContexts.QuestionnaireTester/ViewModels/ChaptersViewModel.cs
+++ src/Core/BoundedContexts/QuestionnaireTester/WB.Core.BoundedContexts.QuestionnaireTester/ViewModels/ChaptersViewModel.cs
@@ -1,20 +1,65 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
 using Cirrious.MvvmCross.ViewModels;
+
+using WB.Core.BoundedContexts.QuestionnaireTester.Implementation.Entities;
+using WB.Core.BoundedContexts.QuestionnaireTester.Repositories;
+using WB.Core.Infrastructure.PlainStorage;
 using WB.Core.SharedKernels.DataCollection;
 
 namespace WB.Core.BoundedContexts.QuestionnaireTester.ViewModels
 {
+    public class ChapterViewModel : MvxNotifyPropertyChanged
+    {
+        private Identity chapterIdentity;
+
+        private string title;
+        public string Title
+        {
+            get { return this.title; }
+            set { this.title = value; this.RaisePropertyChanged(); }
+        }
+
+        public ChapterViewModel(Guid id, decimal[] rosterVector, string chapterTitle)
+        {
+            chapterIdentity = new Identity(id, rosterVector);
+            Title = chapterTitle;
+        }
+    }
+
     public class ChaptersViewModel : MvxNotifyPropertyChanged
     {
         private NavigationState navigationState;
 
-        public void Init(NavigationState navigationState)
+        private readonly IPlainKeyValueStorage<QuestionnaireModel> questionnaireRepository;
+        private readonly IStatefullInterviewRepository interviewRepository;
+
+        public IList<ChapterViewModel> Chapters { get; set; }
+
+        public ChaptersViewModel(
+            IPlainKeyValueStorage<QuestionnaireModel> questionnaireRepository, 
+            IStatefullInterviewRepository interviewRepository)
+        {
+            this.questionnaireRepository = questionnaireRepository;
+            this.interviewRepository = interviewRepository;
+        }
+
+        public void Init(string interviewId, string questionnaireId, NavigationState navigationState)
         {
             if (navigationState == null) throw new ArgumentNullException("navigationState");
             if (this.navigationState != null) throw new Exception("ViewModel already initialized");
 
             this.navigationState = navigationState;
             this.navigationState.OnGroupChanged += navigationState_OnGroupChanged;
+
+            var questionnaire = questionnaireRepository.GetById(questionnaireId);
+            Chapters = questionnaire
+                .GroupsHierarchy
+                .Select(x => new ChapterViewModel(x.Id, new decimal[0], x.Title))
+                .ToList();
         }
 
         void navigationState_OnGroupChanged(Identity newGroupIdentity)
