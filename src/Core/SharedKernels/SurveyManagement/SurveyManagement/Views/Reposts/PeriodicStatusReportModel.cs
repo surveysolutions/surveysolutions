80cdc1eee5a847cf399eb97eb1b677d089a46c54
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2015-06-03 17:23:57 -0400

    HQ\SpeedRepoert: code review KP-5414

--- src/Core/SharedKernels/SurveyManagement/SurveyManagement/Views/Reposts/PeriodicStatusReportModel.cs
+++ src/Core/SharedKernels/SurveyManagement/SurveyManagement/Views/Reposts/PeriodicStatusReportModel.cs
@@ -1,15 +1,17 @@
+using System;
 using WB.Core.SharedKernels.SurveyManagement.Views.Reposts.Views;
 
 namespace WB.Core.SharedKernels.SurveyManagement.Views.Reposts
 {
     public class PeriodicStatusReportModel
     {
-        public string WebApiActionName { get; set; }
+        public PeriodicStatusReportWebApiActionName WebApiActionName { get; set; }
         public bool CanNavigateToQuantityByTeamMember { get; set; }
         public bool CanNavigateToQuantityBySupervisors { get; set; }
         public TemplateViewItem[] Questionnaires { get; set; }
         public string ReportTitle { get; set; }
         public string ReportName { get; set; }
         public string ResponsibleColumnName { get; set; }
+        public Guid? SupervisorId { get; set; }
     }
 }
