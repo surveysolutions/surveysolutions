7aaf41f2eb26980515ffdfb91527ee377ad31bd9
Anastasya Kholodova <nastya.kholodova@gmail.com>
2015-06-05 15:59:34 -0400

    HQ\Synchronization: categorize synchronization errors KP-5329

--- src/Core/SharedKernels/DataCollection/DataCollection/Exceptions/InterviewException.cs
+++ src/Core/SharedKernels/DataCollection/DataCollection/Exceptions/InterviewException.cs
@@ -4,10 +4,19 @@ namespace WB.Core.SharedKernels.DataCollection.Exceptions
 {
     public class InterviewException : Exception
     {
-        internal InterviewException(string message)
-            : base(message) {}
+        public readonly InterviewDomainExceptionType ErrorType;
 
-        internal InterviewException(string message, Exception innerException)
-            : base(message, innerException) {}
+        internal InterviewException(string message, InterviewDomainExceptionType errorType = InterviewDomainExceptionType.Undefined)
+            : base(message)
+        {
+            ErrorType = errorType;
+        }
+
+        internal InterviewException(string message, Exception innerException,
+            InterviewDomainExceptionType errorType = InterviewDomainExceptionType.Undefined)
+            : base(message, innerException)
+        {
+            ErrorType = errorType;
+        }
     }
 }
