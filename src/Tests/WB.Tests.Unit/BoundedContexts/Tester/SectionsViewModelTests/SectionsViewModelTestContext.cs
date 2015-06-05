38b495acecab541448a4e0dfb7c22de20b7f87ce
Slava Scherbak <slava.scherbak@gmail.com>
2015-06-05 12:55:15 -0400

    Tester: added tests for sections view model. KP-5484

---
+++ src/Tests/WB.Tests.Unit/BoundedContexts/Tester/SectionsViewModelTests/SectionsViewModelTestContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using Cirrious.MvvmCross.Plugins.Messenger;
+
+using Moq;
+using WB.Core.BoundedContexts.QuestionnaireTester.Implementation.Entities;
+using WB.Core.BoundedContexts.QuestionnaireTester.ViewModels;
+using WB.Core.Infrastructure.PlainStorage;
+
+using it = Moq.It;
+
+namespace WB.Tests.Unit.BoundedContexts.Tester.SectionsViewModelTests
+{
+    public class SectionsViewModelTestContext
+    {
+        protected static SectionsViewModel CreateSectionsViewModel(
+            IMvxMessenger messenger = null,
+            IPlainKeyValueStorage<QuestionnaireModel> questionnaireRepository = null)
+        {
+            return new SectionsViewModel(
+                messenger ?? Mock.Of<IMvxMessenger>(),
+                questionnaireRepository ?? Mock.Of<IPlainKeyValueStorage<QuestionnaireModel>>());
+        }
+
+        protected static NavigationState CreateNavigationState()
+        {
+            return new NavigationState();
+        }
+        protected static GroupsHierarchyModel CreateGroupsHierarchyModel(Guid id, string title)
+        {
+            return new GroupsHierarchyModel
+                   {
+                       Id = id,
+                       Title = title,
+                       IsRoster = false
+                   };
+        }
+    }
+}
