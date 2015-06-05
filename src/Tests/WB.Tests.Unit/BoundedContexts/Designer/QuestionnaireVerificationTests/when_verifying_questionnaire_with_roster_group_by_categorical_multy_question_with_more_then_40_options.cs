74e7cc00656210a0fb758c15717b7263e6323872
Anastasya Kholodova <nastya.kholodova@gmail.com>
2015-06-05 10:12:39 -0400

    Verifier: fix verification of multy option roster size question KP-4921

---
+++ src/Tests/WB.Tests.Unit/BoundedContexts/Designer/QuestionnaireVerificationTests/when_verifying_questionnaire_with_roster_group_by_categorical_multy_question_with_more_then_40_options.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Machine.Specifications;
+using Main.Core.Documents;
+using Main.Core.Entities.Composite;
+using Main.Core.Entities.SubEntities;
+using Main.Core.Entities.SubEntities.Question;
+using WB.Core.BoundedContexts.Designer.Implementation.Services;
+using WB.Core.BoundedContexts.Designer.ValueObjects;
+
+namespace WB.Tests.Unit.BoundedContexts.Designer.QuestionnaireVerificationTests
+{
+    internal class when_verifying_questionnaire_with_roster_group_by_categorical_multy_question_with_more_then_40_options : QuestionnaireVerifierTestsContext
+    {
+        Establish context = () =>
+        {
+            rosterGroupId = Guid.Parse("10000000000000000000000000000000");
+            rosterSizeQuestionId = Guid.Parse("13333333333333333333333333333333");
+            questionnaire = CreateQuestionnaireDocument();
+            var options = new List<Answer>();
+            for (int i = 0; i < 50; i++)
+            {
+                options.Add(Create.Option(text: i.ToString(), value: i.ToString()));
+            }
+            questionnaire.Children.Add(new MultyOptionsQuestion("question 1")
+            {
+                PublicKey = rosterSizeQuestionId,
+                StataExportCaption = "var1",
+                Answers = options
+            });
+
+            questionnaire.Children.Add(new Group()
+            {
+                PublicKey = rosterGroupId,
+                IsRoster = true,
+                VariableName = "a",
+                RosterSizeSource = RosterSizeSourceType.Question,
+                RosterSizeQuestionId = rosterSizeQuestionId,
+                Children = new List<IComposite>()
+            });
+            verifier = CreateQuestionnaireVerifier();
+        };
+
+        Because of = () =>
+            resultErrors = verifier.Verify(questionnaire);
+
+        It should_return_1_error = () =>
+            resultErrors.Count().ShouldEqual(1);
+
+        It should_return_error_with_code__WB0026__ = () =>
+            resultErrors.Single().Code.ShouldEqual("WB0026");
+
+        It should_return_error_with_1_references = () =>
+            resultErrors.Single().References.Count().ShouldEqual(1);
+
+        It should_return_error_reference_with_type_question = () =>
+            resultErrors.Single().References.First().Type.ShouldEqual(QuestionnaireVerificationReferenceType.Question);
+
+        It should_return_error_reference_with_id_of_rosterSizeQuestionId = () =>
+            resultErrors.Single().References.First().Id.ShouldEqual(rosterSizeQuestionId);
+
+        private static IEnumerable<QuestionnaireVerificationError> resultErrors;
+        private static QuestionnaireVerifier verifier;
+        private static QuestionnaireDocument questionnaire;
+        private static Guid rosterGroupId;
+        private static Guid rosterSizeQuestionId;
+    }
+}
