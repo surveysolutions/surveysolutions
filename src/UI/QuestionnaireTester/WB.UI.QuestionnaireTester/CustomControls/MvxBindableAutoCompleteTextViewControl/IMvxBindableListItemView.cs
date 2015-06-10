267ae866c3066c757b786d81d126f495fa324cb9
Pavel Stovpivskyi <sby@rambler.ru>
2015-06-10 16:52:33 +0300

    Removed unused code

--- src/UI/QuestionnaireTester/WB.UI.QuestionnaireTester/CustomControls/MvxBindableAutoCompleteTextViewControl/IMvxBindableListItemView.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WB.UI.QuestionnaireTester.CustomControls.MvxBindableAutoCompleteTextViewControl
-{
-    public interface IMvxBindableListItemView
-    {
-        int TemplateId { get; }
-        void BindTo(object source);
-    }
-}
