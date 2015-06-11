e4667e386f4fff05a2226f4c4bdbac0e3b882398
Anastasya Kholodova <nastya.kholodova@gmail.com>
2015-06-11 14:38:28 -0400

    Designer: unit tests for history denormalizer KP-5463

---
+++ src/Tests/WB.Tests.Unit/BoundedContexts/Designer/QuestionnaireChangeHistoryDenormalizerTests/when_questionnaire_group_is_changing.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Machine.Specifications;
+using Moq;
+using WB.Core.BoundedContexts.Designer.Views.Account;
+using WB.Core.BoundedContexts.Designer.Views.Questionnaire.ChangeHistory;
+using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
+using WB.Tests.Unit.SharedKernels.SurveyManagement;
+using It = Machine.Specifications.It;
+
+namespace WB.Tests.Unit.BoundedContexts.Designer.QuestionnaireChangeHistoryDenormalizerTests
+{
+    internal class when_questionnaire_group_is_changing : QuestionnaireChangeHistoryDenormalizerTestContext
+    {
+        Establish context = () =>
+        {
+            questionnaireStateTackerStorage =
+                Mock.Of<IReadSideKeyValueStorage<QuestionnaireStateTacker>>(
+                    _ => _.GetById(Moq.It.IsAny<string>()) == Create.QuestionnaireStateTacker());
+            questionnaireChangeRecordStorage = new TestInMemoryWriter<QuestionnaireChangeRecord>();
+
+            questionnaireChangeHistoryDenormalizer =
+                CreateQuestionnaireChangeHistoryDenormalizer(questionnaireStateTacker: questionnaireStateTackerStorage,
+                    questionnaireChangeRecord: questionnaireChangeRecordStorage);
+        };
+
+        Because of = () =>
+        {
+            questionnaireChangeHistoryDenormalizer.Handle(Create.NewGroupAddedEvent(groupId));
+            questionnaireChangeHistoryDenormalizer.Handle(Create.GroupClonedEvent(groupId2));
+            questionnaireChangeHistoryDenormalizer.Handle(Create.GroupUpdatedEvent(groupId, groupTitle));
+            questionnaireChangeHistoryDenormalizer.Handle(Create.GroupBecameARosterEvent(groupId));
+            questionnaireChangeHistoryDenormalizer.Handle(Create.RosterChanged(groupId));
+            questionnaireChangeHistoryDenormalizer.Handle(Create.GroupStoppedBeingARosterEvent(groupId));
+            questionnaireChangeHistoryDenormalizer.Handle(Create.GroupDeletedEvent(groupId));
+        };
+
+        It should_store_7_changes = () =>
+           GetAllRecords(questionnaireChangeRecordStorage).Length.ShouldEqual(7);
+
+        It should_store_5_change_records_with_target_type_equal_to_group = () =>
+            GetAllRecords(questionnaireChangeRecordStorage).Count(r => r.TargetItemType == QuestionnaireItemType.Group)
+                .ShouldEqual(5);
+
+        It should_store_2_change_records_with_target_type_equal_to_roster = () =>
+           GetAllRecords(questionnaireChangeRecordStorage).Count(r => r.TargetItemType == QuestionnaireItemType.Roster)
+               .ShouldEqual(2);
+
+        private static QuestionnaireChangeHistoryDenormalizer questionnaireChangeHistoryDenormalizer;
+        private static string groupId = "11111111111111111111111111111111";
+        private static string groupId2 = "22222222222222222222222222222222";
+
+        private static string groupTitle = "test";
+        private static IReadSideKeyValueStorage<QuestionnaireStateTacker> questionnaireStateTackerStorage;
+        private static TestInMemoryWriter<QuestionnaireChangeRecord> questionnaireChangeRecordStorage;
+    }
+}
---
+++ src/Tests/WB.Tests.Unit/BoundedContexts/Designer/QuestionnaireChangeHistoryDenormalizerTests/when_questionnaire_is_cloned_but_source_of_cloning_equal_to_cloned_one.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Machine.Specifications;
+using WB.Core.BoundedContexts.Designer.Views.Questionnaire.ChangeHistory;
+using WB.Core.GenericSubdomains.Utils;
+using WB.Tests.Unit.SharedKernels.SurveyManagement;
+
+namespace WB.Tests.Unit.BoundedContexts.Designer.QuestionnaireChangeHistoryDenormalizerTests
+{
+    internal class when_questionnaire_is_cloned_but_source_of_cloning_equal_to_cloned_one : QuestionnaireChangeHistoryDenormalizerTestContext
+    {
+        Establish context = () =>
+        {
+            questionnaireStateTackerStorage = new TestInMemoryWriter<QuestionnaireStateTacker>();
+            questionnaireChangeRecordStorage = new TestInMemoryWriter<QuestionnaireChangeRecord>();
+            questionnaireChangeHistoryDenormalizer = CreateQuestionnaireChangeHistoryDenormalizer(questionnaireStateTacker: questionnaireStateTackerStorage, questionnaireChangeRecord: questionnaireChangeRecordStorage);
+        };
+
+        Because of = () =>
+            questionnaireChangeHistoryDenormalizer.Handle(Create.QuestionnaireClonedEvent(questionnaireId: questionnaireId, questionnaireTitle: questionnaireTitle, clonedFromQuestionnaireId: Guid.Parse(questionnaireId)));
+
+        It should_store_change_record_with_target_action_equal_to_add = () =>
+            GetFirstChangeRecord(questionnaireChangeRecordStorage, questionnaireId).ActionType.ShouldEqual(QuestionnaireActionType.Add);
+
+        It should_store_change_record_with_target_type_equal_to_questionnaire = () =>
+            GetFirstChangeRecord(questionnaireChangeRecordStorage, questionnaireId).TargetItemType.ShouldEqual(QuestionnaireItemType.Questionnaire);
+
+        It should_store_change_record_with_target_title_equal_to_test = () =>
+            GetFirstChangeRecord(questionnaireChangeRecordStorage, questionnaireId).TargetItemTitle.ShouldEqual(questionnaireTitle);
+
+        It should_store_change_record_with_empty_references = () =>
+           GetFirstChangeRecord(questionnaireChangeRecordStorage, questionnaireId).References.ShouldBeEmpty();
+
+        It should_store_questionnaire_title_in_state_tracker = () =>
+            questionnaireStateTackerStorage.GetById(questionnaireId).GroupsState[Guid.Parse(questionnaireId)].ShouldEqual(questionnaireTitle);
+
+        private static QuestionnaireChangeHistoryDenormalizer questionnaireChangeHistoryDenormalizer;
+        private static string questionnaireId = "11111111111111111111111111111111";
+        private static string questionnaireTitle = "test";
+        private static TestInMemoryWriter<QuestionnaireStateTacker> questionnaireStateTackerStorage;
+        private static TestInMemoryWriter<QuestionnaireChangeRecord> questionnaireChangeRecordStorage;
+    }
+}
