b31f0b908d1d1f612e09893cf52eb89a59aa287f
Pavel Stovpivskyi <sby@rambler.ru>
2015-06-11 15:46:30 +0300

    Added tests
    KP-5158

---
+++ src/Tests/WB.Tests.Unit/BoundedContexts/QuestionnaireTester/ViewModels/FilteredSingleOptionQuestionViewModelTests/FilteredSingleOptionQuestionViewModelTestsContext.cs
@@ -0,0 +1,36 @@
+﻿using Chance.MvvmCross.Plugins.UserInteraction;
+using Moq;
+using WB.Core.BoundedContexts.QuestionnaireTester.Implementation.Entities;
+using WB.Core.BoundedContexts.QuestionnaireTester.Infrastructure;
+using WB.Core.BoundedContexts.QuestionnaireTester.Repositories;
+using WB.Core.BoundedContexts.QuestionnaireTester.ViewModels;
+using WB.Core.BoundedContexts.QuestionnaireTester.ViewModels.QuestionsViewModels;
+using WB.Core.BoundedContexts.QuestionnaireTester.ViewModels.QuestionStateViewModels;
+using WB.Core.Infrastructure.PlainStorage;
+using WB.Core.SharedKernels.DataCollection.Events.Interview;
+
+namespace WB.Tests.Unit.BoundedContexts.QuestionnaireTester.ViewModels.FilteredSingleOptionQuestionViewModelTests
+{
+    public class FilteredSingleOptionQuestionViewModelTestsContext
+    {
+        protected static FilteredSingleOptionQuestionViewModel CreateFilteredSingleOptionQuestionViewModel(
+            QuestionStateViewModel<SingleOptionQuestionAnswered> questionStateViewModel,
+            AnsweringViewModel answering,
+            IPrincipal principal = null,
+            IPlainKeyValueStorage<QuestionnaireModel> questionnaireRepository = null,
+            IStatefulInterviewRepository interviewRepository = null)
+        {
+            return new FilteredSingleOptionQuestionViewModel(
+                principal ?? Mock.Of<IPrincipal>(),
+                questionnaireRepository ?? Mock.Of<IPlainKeyValueStorage<QuestionnaireModel>>(),
+                interviewRepository ?? Mock.Of<IStatefulInterviewRepository>(),
+                questionStateViewModel ?? Mock.Of<QuestionStateViewModel<SingleOptionQuestionAnswered>>(),
+                answering ?? Mock.Of<AnsweringViewModel>());
+        }
+
+        protected static NavigationState CreateNavigationState()
+        {
+            return new NavigationState();
+        }
+    }
+}
