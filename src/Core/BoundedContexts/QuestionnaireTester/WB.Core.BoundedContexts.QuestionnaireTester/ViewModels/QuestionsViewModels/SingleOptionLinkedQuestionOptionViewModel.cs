d9dbcaf7be00f2db2d03b645f733b2f7a7acbaf5
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2015-06-11 13:11:45 +0300

    Tester\SingleOptionLinked: added basic view models with dummy data KP-5184

---
+++ src/Core/BoundedContexts/QuestionnaireTester/WB.Core.BoundedContexts.QuestionnaireTester/ViewModels/QuestionsViewModels/SingleOptionLinkedQuestionOptionViewModel.cs
@@ -0,0 +1,44 @@
+using System;
+using Cirrious.MvvmCross.ViewModels;
+using WB.Core.BoundedContexts.QuestionnaireTester.ViewModels.QuestionStateViewModels;
+
+namespace WB.Core.BoundedContexts.QuestionnaireTester.ViewModels.QuestionsViewModels
+{
+    public class SingleOptionLinkedQuestionOptionViewModel : MvxNotifyPropertyChanged
+    {
+        public event EventHandler BeforeSelected;
+
+        public EnablementViewModel Enablement { get; set; }
+
+        public decimal[] RosterVector { get; set; }
+        public string Title { get; set; }
+
+        private bool selected;
+        public bool Selected
+        {
+            get { return this.selected; }
+
+            set
+            {
+                try
+                {
+                    if (value == true)
+                    {
+                        this.OnBeforeSelected();
+                    }
+
+                    this.selected = value;
+                }
+                finally
+                {
+                    this.RaisePropertyChanged();
+                }
+            }
+        }
+
+        private void OnBeforeSelected()
+        {
+            if (this.BeforeSelected != null) this.BeforeSelected.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
