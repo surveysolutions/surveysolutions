ba6aec632d3f250755bfc00ed84ec1311c666f3c
Anastasya Kholodova <nastya.kholodova@gmail.com>
2015-06-11 17:41:52 -0400

    fix build

---
+++ src/Core/SharedKernels/DataCollection/DataCollection/Implementation/Services/DummyPreconditionsService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WB.Core.SharedKernels.DataCollection.Services;
+
+namespace WB.Core.SharedKernels.DataCollection.Implementation.Services
+{
+    public class DummyPreconditionsService : IInterviewPreconditionsService
+    {
+        public int? GetMaxAllowedInterviewsCount()
+        {
+            return null;
+        }
+
+        public int? GetInterviewsCountAllowedToCreateUntilLimitReached()
+        {
+            return null;
+        }
+    }
+}
