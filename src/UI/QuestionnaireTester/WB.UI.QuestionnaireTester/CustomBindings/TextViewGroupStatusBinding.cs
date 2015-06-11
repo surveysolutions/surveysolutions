25f3a99dc6785efd4b2b6b8b5c0ba911f7251b1b
Pavel Stovpivskyi <sby@rambler.ru>
2015-06-11 16:48:54 +0300

    Removed calculation statistics, but save UI part
    KP-5423

--- src/UI/QuestionnaireTester/WB.UI.QuestionnaireTester/CustomBindings/TextViewGroupStatusBinding.cs
+++ src/UI/QuestionnaireTester/WB.UI.QuestionnaireTester/CustomBindings/TextViewGroupStatusBinding.cs
@@ -13,7 +13,7 @@ using WB.Core.GenericSubdomains.Portable;
 
 namespace WB.UI.QuestionnaireTester.CustomBindings
 {
-    public class TextViewGroupStatusBinding : BaseBinding<TextView, GroupStatistics>
+    public class TextViewGroupStatusBinding : BaseBinding<TextView, PreviousGroupNavigationViewModel.GroupStatistics>
     {
         public TextViewGroupStatusBinding(TextView androidControl)
             : base(androidControl)
@@ -25,7 +25,7 @@ namespace WB.UI.QuestionnaireTester.CustomBindings
             get { return MvxBindingMode.OneWay; }
         } 
 
-        protected override void SetValueToView(TextView control, GroupStatistics value)
+        protected override void SetValueToView(TextView control, PreviousGroupNavigationViewModel.GroupStatistics value)
         {
             if (value == null) return;
 
@@ -36,7 +36,7 @@ namespace WB.UI.QuestionnaireTester.CustomBindings
             control.TextFormatted = spannableText;
         }
 
-        private SpannableString GetSpannableStringForUnansweredState(GroupStatistics statistics)
+        private SpannableString GetSpannableStringForUnansweredState(PreviousGroupNavigationViewModel.GroupStatistics statistics)
         {
             var unansweredQuestionsFormatString = UIResources.Interview_PreviousGroupNavigation_UnansweredQuestions;
             var groupStatisticsText = unansweredQuestionsFormatString.FormatString(statistics.UnansweredQuestionsCount);
@@ -53,7 +53,7 @@ namespace WB.UI.QuestionnaireTester.CustomBindings
             return spannableText;
         }
 
-        private SpannableString GetSpannableStringForAnsweredState(GroupStatistics statistics)
+        private SpannableString GetSpannableStringForAnsweredState(PreviousGroupNavigationViewModel.GroupStatistics statistics)
         {
             var groupStatisticsText = UIResources.Interview_PreviousGroupNavigation_AnsweredQuestions.FormatString(statistics.EnabledQuestionsCount);
             groupStatisticsText = groupStatisticsText.ToUpper();
