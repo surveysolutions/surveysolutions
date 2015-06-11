075e81e5d74cf72f51ba1ac41de127dcbbd2d51f
Andrii Kozhyn <hedgic@gmail.com>
2015-06-11 13:55:21 +0300

    KP-5146: Added unit tests for MO question VM

---
+++ src/Tests/WB.Tests.Unit/BoundedContexts/QuestionnaireTester/ViewModels/MultiOptionQuestionViewModelTests/when_handling_question_answered_event.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using Machine.Specifications;
+using Moq;
+using WB.Core.BoundedContexts.QuestionnaireTester.Implementation.Aggregates;
+using WB.Core.BoundedContexts.QuestionnaireTester.Implementation.Entities;
+using WB.Core.BoundedContexts.QuestionnaireTester.Implementation.Entities.QuestionModels;
+using WB.Core.BoundedContexts.QuestionnaireTester.Repositories;
+using WB.Core.BoundedContexts.QuestionnaireTester.ViewModels;
+using WB.Core.BoundedContexts.QuestionnaireTester.ViewModels.QuestionsViewModels;
+using WB.Core.Infrastructure.PlainStorage;
+using WB.Core.SharedKernels.DataCollection;
+using WB.Core.SharedKernels.DataCollection.Events.Interview;
+using It = Machine.Specifications.It;
+
+namespace WB.Tests.Unit.BoundedContexts.QuestionnaireTester.ViewModels.MultiOptionQuestionViewModelTests
+{
+    public class when_handling_question_answered_event : MultiOptionQuestionViewModelTestsContext
+    {
+        Establish context = () =>
+        {
+            questionGuid = Guid.Parse("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+            questionId = new Identity(questionGuid, Empty.RosterVector);
+
+            var questionnaire = BuildDefaultQuestionnaire(questionId);
+            ((MultiOptionQuestionModel) questionnaire.Questions.First().Value).AreAnswersOrdered = true;
+
+            var multiOptionAnswer = new MultiOptionAnswer(questionGuid, Empty.RosterVector);
+            multiOptionAnswer.SetAnswers(new[] { 2m });
+
+            var interview = Mock.Of<IStatefulInterview>(x => x.GetMultiOptionAnswer(questionId) == multiOptionAnswer);
+
+            var questionnaireStorage = new Mock<IPlainKeyValueStorage<QuestionnaireModel>>();
+            var interviewRepository = new Mock<IStatefulInterviewRepository>();
+
+            questionnaireStorage.SetReturnsDefault(questionnaire);
+            interviewRepository.SetReturnsDefault(interview);
+
+            viewModel = CreateViewModel(questionnaireStorage: questionnaireStorage.Object,
+                interviewRepository: interviewRepository.Object);
+
+            viewModel.Init("blah", questionId, new NavigationState());
+            viewModel.Options.First().Checked = true;
+        };
+
+        Because of = () => viewModel.Handle(new MultipleOptionsQuestionAnswered(Guid.NewGuid(), questionGuid, Empty.RosterVector, DateTime.Now, new []{2m, 1m}));
+
+        It should_set_checked_order_to_options = () => viewModel.Options.First().CheckedOrder.ShouldEqual(2);
+
+        static MultiOptionQuestionViewModel viewModel;
+        static Identity questionId;
+        private static Guid questionGuid;
+    }
+}
+
---
+++ src/Tests/WB.Tests.Unit/BoundedContexts/QuestionnaireTester/ViewModels/MultiOptionQuestionViewModelTests/when_initializing_answered_question.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Machine.Specifications;
+using Moq;
+using WB.Core.BoundedContexts.QuestionnaireTester.Implementation.Aggregates;
+using WB.Core.BoundedContexts.QuestionnaireTester.Implementation.Entities;
+using WB.Core.BoundedContexts.QuestionnaireTester.Implementation.Entities.QuestionModels;
+using WB.Core.BoundedContexts.QuestionnaireTester.Infrastructure;
+using WB.Core.BoundedContexts.QuestionnaireTester.Repositories;
+using WB.Core.BoundedContexts.QuestionnaireTester.ViewModels;
+using WB.Core.BoundedContexts.QuestionnaireTester.ViewModels.QuestionsViewModels;
+using WB.Core.BoundedContexts.QuestionnaireTester.ViewModels.QuestionStateViewModels;
+using WB.Core.Infrastructure.EventBus.Lite;
+using WB.Core.Infrastructure.PlainStorage;
+using WB.Core.SharedKernels.DataCollection;
+using WB.Core.SharedKernels.DataCollection.Events.Interview;
+using It = Machine.Specifications.It;
+
+namespace WB.Tests.Unit.BoundedContexts.QuestionnaireTester.ViewModels.MultiOptionQuestionViewModelTests
+{
+    public class when_initializing_answered_question : MultiOptionQuestionViewModelTestsContext
+    {
+        Establish context = () =>
+        {
+            interviewId = "interview";
+            questionGuid = Guid.Parse("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+            questionId = new Identity(questionGuid, Empty.RosterVector);
+            navigationState = new NavigationState();
+
+
+            var questionnaire = BuildDefaultQuestionnaire(questionId);
+
+            var multiOptionAnswer = new MultiOptionAnswer(questionGuid, Empty.RosterVector);
+            multiOptionAnswer.SetAnswers(new[] {1m});
+            
+            var interview = Mock.Of<IStatefulInterview>(x => x.GetMultiOptionAnswer(questionId) == multiOptionAnswer);
+
+            var questionnaireStorage = new Mock<IPlainKeyValueStorage<QuestionnaireModel>>();
+            var interviewRepository = new Mock<IStatefulInterviewRepository>();
+
+            questionnaireStorage.SetReturnsDefault(questionnaire);
+            interviewRepository.SetReturnsDefault(interview);
+
+            eventRegistry = new Mock<ILiteEventRegistry>();
+
+            viewModel = CreateViewModel(questionnaireStorage: questionnaireStorage.Object, 
+                interviewRepository: interviewRepository.Object,
+                eventRegistry: eventRegistry.Object);
+        };
+
+        Because of = () => viewModel.Init(interviewId, questionId, navigationState);
+
+        It should_build_options = () => viewModel.Options.Count.ShouldEqual(2);
+
+        It should_mark_answered_options_as_checked = () =>
+        {
+            var firstOption = viewModel.Options.First();
+            firstOption.Checked.ShouldBeTrue();
+            firstOption.Title.ShouldEqual("item1");
+            firstOption.CheckedOrder.ShouldEqual(1);
+            firstOption.Value.ShouldEqual(1m);
+        };
+
+        It should_subscribe_model_in_events_registry = () => eventRegistry.Verify(x => x.Subscribe(viewModel));
+
+        static MultiOptionQuestionViewModel viewModel;
+        static string interviewId;
+        static Identity questionId;
+        static NavigationState navigationState;
+        private static Guid questionGuid;
+        private static Mock<ILiteEventRegistry> eventRegistry;
+    }
+}
+
