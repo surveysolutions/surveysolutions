31fe3c7f33b1f179b7aa34f150393e49786bee51
Andrii Kozhyn <hedgic@gmail.com>
2015-06-12 12:34:04 +0300

    KP-5532: Added tests for linked multi option question

---
+++ src/Tests/WB.Tests.Unit/BoundedContexts/QuestionnaireTester/ViewModels/MultiOptionLinkedQuestionOptionViewModelTests/when_setting_checked_property.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Machine.Specifications;
+using Moq;
+using WB.Core.BoundedContexts.QuestionnaireTester.ViewModels.QuestionsViewModels;
+using It = Machine.Specifications.It;
+
+namespace WB.Tests.Unit.BoundedContexts.QuestionnaireTester.ViewModels.MultiOptionLinkedQuestionOptionViewModelTests
+{
+    public class when_setting_checked_property
+    {
+        Establish context = () =>
+        {
+            viewModel = new MultiOptionLinkedQuestionOptionViewModel(Mock.Of<MultiOptionLinkedQuestionViewModel>());
+        };
+
+        Because of = () => viewModel.Checked = true;
+
+        It should_set_checked_timestamp = () => viewModel.CheckedTimeStamp.ShouldNotEqual(DateTime.MinValue);
+        
+        static MultiOptionLinkedQuestionOptionViewModel viewModel;
+    }
+}
+
