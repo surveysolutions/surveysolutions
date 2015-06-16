ef7afd6a2a174e05ce99f7cc24367d28afbbd2b8
Pavel Stovpivskyi <630198+spuv@users.noreply.github.com>
2015-06-16 15:38:17 +0300

    Added tests for filtering mask text

---
+++ src/Tests/WB.Tests.Unit/BoundedContexts/QuestionnaireTester/Services/MaskTextTests/when_filtering_text_coincidents_with_mask.cs
@@ -0,0 +1,24 @@
+﻿using Machine.Specifications;
+using WB.Core.BoundedContexts.QuestionnaireTester.Services.MaskText;
+using It = Machine.Specifications.It;
+
+
+namespace WB.Tests.Unit.BoundedContexts.QuestionnaireTester.Services.MaskTextTests
+{
+    internal class when_filtering_text_coincidents_with_mask : MaskTextTestsContext
+    {
+        Establish context = () =>
+        {
+            maskedText = CreateMaskedText("*-**");
+        };
+
+        Because of = () =>
+            filterResult = maskedText.FilterInserting("s-ss", 0);
+
+        It should_be_accept_only_AnyChars = () =>
+            filterResult.ShouldEqual("sss");
+
+        static string filterResult;
+        static MaskedText maskedText;
+    }
+}
