eec2d119df1c312420b5a623fe1c3dbccc689322
Pavel Stovpivskyi <sby@rambler.ru>
2015-06-16 16:13:53 +0300

    Added tests for filtering mask text

---
+++ src/Tests/WB.Tests.Unit/BoundedContexts/QuestionnaireTester/Services/MaskTextTests/when_filtering_text_dont_consided_to_mask_with_digits_and_letters.cs
@@ -0,0 +1,24 @@
+﻿using Machine.Specifications;
+using WB.Core.BoundedContexts.QuestionnaireTester.Services.MaskText;
+using It = Machine.Specifications.It;
+
+
+namespace WB.Tests.Unit.BoundedContexts.QuestionnaireTester.Services.MaskTextTests
+{
+    internal class when_filtering_text_dont_consided_to_mask_with_digits_and_letters : MaskTextTestsContext
+    {
+        Establish context = () =>
+        {
+            maskedText = CreateMaskedText("*-##-~~~");
+        };
+
+        Because of = () =>
+            filterResult = maskedText.FilterInserting("---2-Z3.", 0);
+
+        It should_be_accept_only_AnyChars = () =>
+            filterResult.ShouldEqual("-_2Z__");
+
+        static string filterResult;
+        static MaskedText maskedText;
+    }
+}
