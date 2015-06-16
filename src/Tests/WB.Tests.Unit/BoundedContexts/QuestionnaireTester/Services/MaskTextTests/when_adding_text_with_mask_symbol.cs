4f219d0c989df8e5c63029100ac8963c6944d5fd
Pavel Stovpivskyi <630198+spuv@users.noreply.github.com>
2015-06-16 16:13:53 +0300

    Added tests for filtering mask text

---
+++ src/Tests/WB.Tests.Unit/BoundedContexts/QuestionnaireTester/Services/MaskTextTests/when_adding_text_with_mask_symbol.cs
@@ -0,0 +1,25 @@
+﻿using Machine.Specifications;
+using WB.Core.BoundedContexts.QuestionnaireTester.Services.MaskText;
+using It = Machine.Specifications.It;
+
+
+namespace WB.Tests.Unit.BoundedContexts.QuestionnaireTester.Services.MaskTextTests
+{
+    internal class when_adding_text_with_mask_symbol : MaskTextTestsContext
+    {
+        Establish context = () =>
+        {
+            maskedText = CreateMaskedText("*--**");
+        };
+
+        Because of = () =>
+            maskedText.AddString("__1", 0, ref selection);
+
+        It should_be_accept_char = () =>
+            maskedText.MakeMaskedText().ShouldEqual("1--__");
+
+
+        static int selection;
+        static MaskedText maskedText;
+    }
+}
---
+++ src/Tests/WB.Tests.Unit/BoundedContexts/QuestionnaireTester/Services/MaskTextTests/when_filtering_text_coincidents_with_mask_with_digits_and_letters.cs
@@ -0,0 +1,24 @@
+﻿using Machine.Specifications;
+using WB.Core.BoundedContexts.QuestionnaireTester.Services.MaskText;
+using It = Machine.Specifications.It;
+
+
+namespace WB.Tests.Unit.BoundedContexts.QuestionnaireTester.Services.MaskTextTests
+{
+    internal class when_filtering_text_coincidents_with_mask_with_digits_and_letters : MaskTextTestsContext
+    {
+        Establish context = () =>
+        {
+            maskedText = CreateMaskedText("*-##-~~~");
+        };
+
+        Because of = () =>
+            filterResult = maskedText.FilterInserting("s-12-ZxC", 0);
+
+        It should_be_accept_only_AnyChars = () =>
+            filterResult.ShouldEqual("s-12-ZxC");
+
+        static string filterResult;
+        static MaskedText maskedText;
+    }
+}
