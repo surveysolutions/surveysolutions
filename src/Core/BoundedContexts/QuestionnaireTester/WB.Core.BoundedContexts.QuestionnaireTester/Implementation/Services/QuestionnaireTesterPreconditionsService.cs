95e5153d8556105bba6b7baf96534c2e065d87d6
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2015-06-17 14:35:30 -0400

    Tester: fix dublication in style file and injection of percondition service

---
+++ src/Core/BoundedContexts/QuestionnaireTester/WB.Core.BoundedContexts.QuestionnaireTester/Implementation/Services/QuestionnaireTesterPreconditionsService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WB.Core.SharedKernels.DataCollection.Services;
+
+namespace WB.Core.BoundedContexts.QuestionnaireTester.Implementation.Services
+{
+    internal class QuestionnaireTesterPreconditionsService : IInterviewPreconditionsService
+    {
+        public int? GetMaxAllowedInterviewsCount()
+        {
+            return null;
+        }
+
+        public int? GetInterviewsCountAllowedToCreateUntilLimitReached()
+        {
+            return null;
+        }
+    }
+}
