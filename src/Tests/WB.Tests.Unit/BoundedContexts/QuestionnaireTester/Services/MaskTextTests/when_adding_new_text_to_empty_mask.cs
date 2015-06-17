148e11a3973daa42959effdd13592aa3a78391d3
Pavel Stovpivskyi <sby@rambler.ru>
2015-06-17 14:02:16 +0300

    Restored tests

--- src/Tests/WB.Tests.Unit/BoundedContexts/QuestionnaireTester/Services/MaskTextTests/when_adding_new_text_to_empty_mask.cs
+++ src/Tests/WB.Tests.Unit/BoundedContexts/QuestionnaireTester/Services/MaskTextTests/when_adding_new_text_to_empty_mask.cs
@@ -5,7 +5,6 @@ using It = Machine.Specifications.It;
 
 namespace WB.Tests.Unit.BoundedContexts.QuestionnaireTester.Services.MaskTextTests
 {
-    [Ignore("temporary")]
     internal class when_adding_new_text_to_empty_mask : MaskTextTestsContext
     {
         Establish context = () =>
