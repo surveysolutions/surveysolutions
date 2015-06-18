74a7e86714c39593ebb515a8d369215e51ec0c82
Pavel Stovpivskyi <630198+spuv@users.noreply.github.com>
2015-06-18 17:03:11 +0300

    Mask text shows instead of watermark
    KP-5424

---
+++ src/Tests/WB.Tests.Unit/BoundedContexts/QuestionnaireTester/Services/MaskTextTests/when_making_text_after_setting_mask.cs
@@ -0,0 +1,25 @@
+﻿using Machine.Specifications;
+using WB.Core.BoundedContexts.QuestionnaireTester.Services.MaskText;
+using It = Machine.Specifications.It;
+
+
+namespace WB.Tests.Unit.BoundedContexts.QuestionnaireTester.Services.MaskTextTests
+{
+    internal class when_making_text_after_setting_mask : MaskTextTestsContext
+    {
+        Establish context = () =>
+        {
+            maskedText = CreateMaskedText("*-##--~~[]");
+        };
+
+        Because of = () =>
+            result = maskedText.MakeMaskedText();
+
+        It should_be_make_mask_with_correct_char_indexes = () =>
+            result.ShouldEqual("_-__--__[]");
+
+
+        static string result;
+        static MaskedText maskedText;
+    }
+}
