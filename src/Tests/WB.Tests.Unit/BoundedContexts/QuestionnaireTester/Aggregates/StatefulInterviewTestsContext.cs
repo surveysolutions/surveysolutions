b38090bda27cea88fe5dc5ceb8b5820313084aa5
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2015-06-19 19:13:28 +0300

    StatefulInterview.FindAnswersOfReferencedQuestionForLinkedQuestion: covered with tests KP-5184

--- src/Tests/WB.Tests.Unit/BoundedContexts/QuestionnaireTester/Aggregates/StatefulInterviewTestsContext.cs
+++ src/Tests/WB.Tests.Unit/BoundedContexts/QuestionnaireTester/Aggregates/StatefulInterviewTestsContext.cs
@@ -18,5 +18,65 @@ namespace WB.Tests.Unit.BoundedContexts.QuestionnaireTester.Aggregates
                 && _.GetRosterLevelForQuestion(referencedQuestionId) == referencedQuestionRosters.Length
                 && _.GetRostersFromTopToSpecifiedQuestion(referencedQuestionId) == referencedQuestionRosters);
         }
+
+        protected static void FillInterviewWithInstancesForOneRosterAndAnswersToTextQuestionInThatRoster(StatefulInterview interview, Guid rosterId, Guid questionId)
+        {
+            interview.Apply(Create.Event.RosterInstancesAdded(rosterId,
+                new[] { 1m },
+                new[] { 2m }));
+
+            interview.Apply(Create.Event.TextQuestionAnswered(questionId, new[] { 1m }, "1"));
+            interview.Apply(Create.Event.TextQuestionAnswered(questionId, new[] { 2m }, "2"));
+        }
+
+        protected static void FillInterviewWithInstancesForTwoNestedRostersAndAnswersToTextQuestionInLastRoster(StatefulInterview interview, Guid roster1Id, Guid roster2Id, Guid questionId)
+        {
+            interview.Apply(Create.Event.RosterInstancesAdded(roster1Id,
+                new[] { 1m },
+                new[] { 2m }));
+
+            interview.Apply(Create.Event.RosterInstancesAdded(roster2Id,
+                new[] { 1m, 1m },
+                new[] { 1m, 2m },
+                new[] { 2m, 1m },
+                new[] { 2m, 2m }));
+
+            interview.Apply(Create.Event.TextQuestionAnswered(questionId, new[] { 1m, 1m }, "1-1"));
+            interview.Apply(Create.Event.TextQuestionAnswered(questionId, new[] { 1m, 2m }, "1-2"));
+            interview.Apply(Create.Event.TextQuestionAnswered(questionId, new[] { 2m, 1m }, "2-1"));
+            interview.Apply(Create.Event.TextQuestionAnswered(questionId, new[] { 2m, 2m }, "2-2"));
+        }
+
+        protected static void FillInterviewWithInstancesForThreeNestedRostersAndAnswersToTextQuestionInLastRoster(StatefulInterview interview, Guid roster1Id, Guid roster2Id, Guid roster3Id, Guid questionId)
+        {
+            interview.Apply(Create.Event.RosterInstancesAdded(roster1Id,
+                new[] { 1m },
+                new[] { 2m }));
+
+            interview.Apply(Create.Event.RosterInstancesAdded(roster2Id,
+                new[] { 1m, 1m },
+                new[] { 1m, 2m },
+                new[] { 2m, 1m },
+                new[] { 2m, 2m }));
+
+            interview.Apply(Create.Event.RosterInstancesAdded(roster3Id,
+                new[] { 1m, 1m, 1m },
+                new[] { 1m, 1m, 2m },
+                new[] { 1m, 2m, 1m },
+                new[] { 1m, 2m, 2m },
+                new[] { 2m, 1m, 1m },
+                new[] { 2m, 1m, 2m },
+                new[] { 2m, 2m, 1m },
+                new[] { 2m, 2m, 2m }));
+
+            interview.Apply(Create.Event.TextQuestionAnswered(questionId, new[] { 1m, 1m, 1m }, "1-1-1"));
+            interview.Apply(Create.Event.TextQuestionAnswered(questionId, new[] { 1m, 1m, 2m }, "1-1-2"));
+            interview.Apply(Create.Event.TextQuestionAnswered(questionId, new[] { 1m, 2m, 1m }, "1-2-1"));
+            interview.Apply(Create.Event.TextQuestionAnswered(questionId, new[] { 1m, 2m, 2m }, "1-2-2"));
+            interview.Apply(Create.Event.TextQuestionAnswered(questionId, new[] { 2m, 1m, 1m }, "2-1-1"));
+            interview.Apply(Create.Event.TextQuestionAnswered(questionId, new[] { 2m, 1m, 2m }, "2-1-2"));
+            interview.Apply(Create.Event.TextQuestionAnswered(questionId, new[] { 2m, 2m, 1m }, "2-2-1"));
+            interview.Apply(Create.Event.TextQuestionAnswered(questionId, new[] { 2m, 2m, 2m }, "2-2-2"));
+        }
     }
 }
