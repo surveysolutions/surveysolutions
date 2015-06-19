5e710540735908dbd6ed4d82227c1254ad9b2916
Pavel Stovpivskyi <sby@rambler.ru>
2015-06-19 14:25:48 +0300

    Removed old files

--- src/UI/QuestionnaireTester/WB.UI.QuestionnaireTester/CustomControls/MaskedEditTextControl/Range.cs
+++
@@ -1,8 +0,0 @@
-namespace WB.UI.QuestionnaireTester.CustomControls.MaskedEditTextControl
-{
-    public class Range
-    {
-        public int? Start { get; set; }
-        public int? End { get; set; }
-    }
-}
