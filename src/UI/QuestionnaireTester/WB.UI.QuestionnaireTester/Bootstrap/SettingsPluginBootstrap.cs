3cd6d23fafd2d6f4829788b559b474c52d086bb2
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2015-06-23 17:02:44 +0300

    Updated nuget packages

--- src/UI/QuestionnaireTester/WB.UI.QuestionnaireTester/Bootstrap/SettingsPluginBootstrap.cs
+++ src/UI/QuestionnaireTester/WB.UI.QuestionnaireTester/Bootstrap/SettingsPluginBootstrap.cs
@@ -3,7 +3,5 @@ using Cirrious.CrossCore.Plugins;
 namespace WB.UI.QuestionnaireTester.Bootstrap
 {
     public class SettingsPluginBootstrap
-        : MvxPluginBootstrapAction<Cheesebaron.MvxPlugins.Settings.PluginLoader>
-    {
-    }
+        : MvxPluginBootstrapAction<Cheesebaron.MvxPlugins.Settings.PluginLoader> { }
 }
