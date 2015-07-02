000c7fbd7e33979928a0f768081d70d4495f6bb6
Pavel Stovpivskyi <sby@rambler.ru>
2015-07-02 13:27:48 +0300

    Toolbar color change color by group status
    KP-5457

---
+++ src/UI/QuestionnaireTester/WB.UI.QuestionnaireTester/CustomBindings/ViewGroupInterviewStatusBinding.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Android.Graphics;
+using Android.Support.V7.Widget;
+using Android.Views;
+using WB.Core.BoundedContexts.QuestionnaireTester.ViewModels.QuestionsViewModels;
+
+
+namespace WB.UI.QuestionnaireTester.CustomBindings
+{
+    public class ViewGroupInterviewStatusBinding : BaseBinding<ViewGroup, GroupStatus>
+    {
+        public ViewGroupInterviewStatusBinding(ViewGroup androidControl)
+            : base(androidControl)
+        {
+        }
+
+        protected override void SetValueToView(ViewGroup target, GroupStatus value)
+        {
+            switch (value)
+            {
+                case GroupStatus.CompletedInvalid:
+                case GroupStatus.StartedInvalid:
+                    SetBackgroundColor(target, Resource.Color.interviewHeaderErrors);
+                    break;
+
+                case GroupStatus.Completed:
+                    SetBackgroundColor(target, Resource.Color.interviewHeaderComplited);
+                    break;
+
+                case GroupStatus.Started:
+                case GroupStatus.NotStarted:
+                default:
+                    SetBackgroundColor(target, Resource.Color.interviewHeaderInProgress);
+                    break;
+            }
+        }
+
+        private static void SetBackgroundColor(ViewGroup target, int colorResourceId)
+        {
+            target.SetBackgroundColor(target.Resources.GetColor(colorResourceId));
+        }
+    }
+}
