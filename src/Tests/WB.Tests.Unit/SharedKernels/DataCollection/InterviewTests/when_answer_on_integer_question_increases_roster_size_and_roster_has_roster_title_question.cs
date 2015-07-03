d2e94a4be40703e7abf48a567db4cd60adb2d7f5
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2015-07-03 20:52:13 +0300

    Interview: roster titles logic for rosters from int question covered with tests KP-5635

---
+++ src/Tests/WB.Tests.Unit/SharedKernels/DataCollection/InterviewTests/when_answer_on_integer_question_increases_roster_size_and_roster_has_roster_title_question.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Linq;
+using Machine.Specifications;
+using Main.Core.Entities.SubEntities;
+using Microsoft.Practices.ServiceLocation;
+using Moq;
+using Ncqrs.Spec;
+using WB.Core.SharedKernels.DataCollection.Aggregates;
+using WB.Core.SharedKernels.DataCollection.Events.Interview;
+using WB.Core.SharedKernels.DataCollection.Implementation.Aggregates;
+using WB.Core.SharedKernels.DataCollection.Repositories;
+using It = Machine.Specifications.It;
+
+namespace WB.Tests.Unit.SharedKernels.DataCollection.InterviewTests
+{
+    internal class when_answer_on_integer_question_increases_roster_size_and_roster_has_roster_title_question : InterviewTestsContext
+    {
+        Establish context = () =>
+        {
+            var questionnaireId = Guid.Parse("10000000000000000000000000000000");
+            userId = Guid.Parse("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF");
+
+            rosterGroupId = Guid.Parse("11111111111111111111111111111111");
+
+            questionWhichIncreasesRosterSizeId = Guid.Parse("22222222222222222222222222222222");
+
+
+            var questionnaire = Mock.Of<IQuestionnaire>(_
+
+                => _.HasQuestion(questionWhichIncreasesRosterSizeId) == true
+                && _.GetQuestionType(questionWhichIncreasesRosterSizeId) == QuestionType.Numeric
+                && _.IsQuestionInteger(questionWhichIncreasesRosterSizeId) == true
+                && _.IsQuestionInteger(questionWhichIncreasesRosterSizeId) == true
+                && _.GetRosterGroupsByRosterSizeQuestion(questionWhichIncreasesRosterSizeId) == new Guid[] { rosterGroupId }
+
+                && _.HasGroup(rosterGroupId) == true
+                && _.IsRosterTitleQuestionAvailable(rosterGroupId) == true
+                && _.GetRosterLevelForGroup(rosterGroupId) == 1
+                && _.GetRostersFromTopToSpecifiedGroup(rosterGroupId) == new Guid[] { rosterGroupId });
+
+            var questionnaireRepository = CreateQuestionnaireRepositoryStubWithOneQuestionnaire(questionnaireId, questionnaire);
+
+            Mock.Get(ServiceLocator.Current)
+                .Setup(locator => locator.GetInstance<IQuestionnaireRepository>())
+                .Returns(questionnaireRepository);
+
+            interview = CreateInterview(questionnaireId: questionnaireId);
+
+            eventContext = new EventContext();
+        };
+
+        Cleanup stuff = () =>
+        {
+            eventContext.Dispose();
+            eventContext = null;
+        };
+
+        Because of = () =>
+           interview.AnswerNumericIntegerQuestion(userId, questionWhichIncreasesRosterSizeId, new decimal[] { }, DateTime.Now, 3);
+
+        It should_raise_RosterInstancesAdded_event_for_that_roster = () =>
+            eventContext.ShouldContainEvent<RosterInstancesAdded>(@event
+                => @event.Instances.All(instance => instance.GroupId == rosterGroupId));
+
+        It should_not_raise_RosterInstancesTitleChanged_event = () =>
+            eventContext.ShouldNotContainEvent<RosterInstancesTitleChanged>();
+
+        It should_not_raise_RosterInstancesRemoved_event = () =>
+            eventContext.ShouldNotContainEvent<RosterInstancesRemoved>();
+
+        private static EventContext eventContext;
+        private static Interview interview;
+        private static Guid userId;
+        private static Guid questionWhichIncreasesRosterSizeId;
+        private static Guid rosterGroupId;
+    }
+}
