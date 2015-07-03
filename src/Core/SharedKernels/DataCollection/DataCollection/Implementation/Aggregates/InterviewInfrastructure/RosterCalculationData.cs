b9cbe74bdf5216a2218dde06498733df5e9f4369
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2015-07-03 20:33:33 +0300

    Interview: now if roster size is specified by int question and there are no roster title question, then order number is used as title KP-5635

--- src/Core/SharedKernels/DataCollection/DataCollection/Implementation/Aggregates/InterviewInfrastructure/RosterCalculationData.cs
+++ src/Core/SharedKernels/DataCollection/DataCollection/Implementation/Aggregates/InterviewInfrastructure/RosterCalculationData.cs
@@ -1,14 +1,15 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace WB.Core.SharedKernels.DataCollection.Implementation.Aggregates
 {
     public class RosterCalculationData
     {
         public RosterCalculationData(List<RosterIdentity> rosterInstancesToAdd,
-            Dictionary<decimal, string> titlesForRosterInstancesToAdd)
+            Dictionary<decimal, string> titlesForRosterInstancesByInstanceId)
             : this(
                 rosterInstancesToAdd: rosterInstancesToAdd,
-                titlesForRosterInstancesToAdd: titlesForRosterInstancesToAdd,
+                titlesForRosterInstancesByInstanceId: titlesForRosterInstancesByInstanceId,
                 rosterInstancesToRemove: new List<RosterIdentity>(),
                 rosterInstancesToChange: new List<RosterIdentity>(),
                 answersToRemoveByDecreasedRosterSize: new List<Identity>(),
@@ -21,7 +22,7 @@ namespace WB.Core.SharedKernels.DataCollection.Implementation.Aggregates
             List<Identity> answersToRemoveByDecreasedRosterSize,
             List<Identity> disabledAnswersToEnableByDecreasedRosterSize,
             List<Identity> disabledGroupsToEnableByDecreasedRosterSize,
-            Dictionary<decimal, string> titlesForRosterInstancesToAdd,
+            Dictionary<decimal, string> titlesForRosterInstancesByInstanceId,
             List<RosterCalculationData> rosterInstantiatesFromNestedLevels)
         {
             this.RosterInstancesToAdd = rosterInstancesToAdd;
@@ -30,11 +31,12 @@ namespace WB.Core.SharedKernels.DataCollection.Implementation.Aggregates
             this.AnswersToRemoveByDecreasedRosterSize = answersToRemoveByDecreasedRosterSize;
             this.DisabledAnswersToEnableByDecreasedRosterSize = disabledAnswersToEnableByDecreasedRosterSize;
             this.DisabledGroupsToEnableByDecreasedRosterSize = disabledGroupsToEnableByDecreasedRosterSize;
-            this.TitlesForRosterInstancesToAdd = titlesForRosterInstancesToAdd;
+            this.TitlesForRosterInstancesByInstanceId = titlesForRosterInstancesByInstanceId;
             this.RosterInstantiatesFromNestedLevels = rosterInstantiatesFromNestedLevels;
         }
 
-        public Dictionary<decimal, string> TitlesForRosterInstancesToAdd { get; set; }
+        private Dictionary<decimal, string> TitlesForRosterInstancesByInstanceId { get; set; }
+        private Dictionary<Guid, Dictionary<decimal, string>> TitlesForRosterInstancesByRosterIdAndInstanceId { get; set; }
         public List<RosterIdentity> RosterInstancesToAdd { get; private set; }
         public List<RosterIdentity> RosterInstancesToRemove { get; private set; }
         public List<RosterIdentity> RosterInstancesToChange { get; private set; }
@@ -42,5 +44,42 @@ namespace WB.Core.SharedKernels.DataCollection.Implementation.Aggregates
         public List<Identity> DisabledAnswersToEnableByDecreasedRosterSize { get; private set; }
         public List<Identity> DisabledGroupsToEnableByDecreasedRosterSize { get; private set; }
         public List<RosterCalculationData> RosterInstantiatesFromNestedLevels { get; private set; }
+
+        public string GetRosterInstanceTitle(Guid rosterId, decimal rosterInstanceId)
+        {
+            return this.GetRosterInstanceTitleByRosterIdAndInstanceId(rosterId, rosterInstanceId) ?? this.GetRosterInstanceTitleByInstanceId(rosterInstanceId);
+        }
+
+        private string GetRosterInstanceTitleByRosterIdAndInstanceId(Guid rosterId, decimal rosterInstanceId)
+        {
+            return this.TitlesForRosterInstancesByRosterIdAndInstanceId != null
+                && this.TitlesForRosterInstancesByRosterIdAndInstanceId.ContainsKey(rosterId)
+                && this.TitlesForRosterInstancesByRosterIdAndInstanceId[rosterId].ContainsKey(rosterInstanceId)
+                ? this.TitlesForRosterInstancesByRosterIdAndInstanceId[rosterId][rosterInstanceId]
+                : null;
+        }
+
+        private string GetRosterInstanceTitleByInstanceId(decimal rosterInstanceId)
+        {
+            return this.TitlesForRosterInstancesByInstanceId != null
+                && this.TitlesForRosterInstancesByInstanceId.ContainsKey(rosterInstanceId)
+                ? this.TitlesForRosterInstancesByInstanceId[rosterInstanceId]
+                : null;
+        }
+
+        public void SetTitlesForRosterInstances(Dictionary<decimal, string> titlesForRosterInstances)
+        {
+            this.TitlesForRosterInstancesByInstanceId = titlesForRosterInstances;
+        }
+
+        public void SetTitlesForRosterInstances(Dictionary<Guid, Dictionary<decimal, string>> titlesForRosterInstances)
+        {
+            this.TitlesForRosterInstancesByRosterIdAndInstanceId = titlesForRosterInstances;
+        }
+
+        public bool AreTitlesForRosterInstancesSpecified()
+        {
+            return this.TitlesForRosterInstancesByInstanceId != null || this.TitlesForRosterInstancesByRosterIdAndInstanceId != null;
+        }
     }
 }
