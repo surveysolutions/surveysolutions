4b065431eaae79e025a2639756918bd4d3a9d933
Slava Scherbak <slava.scherbak@gmail.com>
2015-07-06 15:33:45 -0400

    Tester: added tests for cascading question. KP-5649

--- src/Tests/WB.Tests.Unit/BoundedContexts/QuestionnaireTester/ViewModels/CascadingSingleOptionQuestionViewModelTests/when_handling_SingleOptionQuestionAnswered_for_parent_question_as_first_answer.cs
+++ src/Tests/WB.Tests.Unit/BoundedContexts/QuestionnaireTester/ViewModels/CascadingSingleOptionQuestionViewModelTests/when_handling_SingleOptionQuestionAnswered_for_parent_question_as_first_answer.cs
@@ -1,8 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+
 using Machine.Specifications;
+
 using Moq;
+
 using WB.Core.BoundedContexts.QuestionnaireTester.Implementation.Aggregates;
 using WB.Core.BoundedContexts.QuestionnaireTester.Implementation.Entities;
 using WB.Core.BoundedContexts.QuestionnaireTester.Implementation.Entities.QuestionModels;
@@ -15,14 +18,13 @@ using It = Machine.Specifications.It;
 
 namespace WB.Tests.Unit.BoundedContexts.QuestionnaireTester.ViewModels.CascadingSingleOptionQuestionViewModelTests
 {
-    public class when_setting_FilterText_after_handling_SingleOptionQuestionAnswered_for_parent_question : CascadingSingleOptionQuestionViewModelTestContext
+    public class when_handling_SingleOptionQuestionAnswered_for_parent_question_as_first_answer : CascadingSingleOptionQuestionViewModelTestContext
     {
         Establish context = () =>
         {
             SetUp();
-
-            var childAnswer = Mock.Of<SingleOptionAnswer>(_ => _.IsAnswered == true && _.Answer == 3);
-            var parentOptionAnswer = Mock.Of<SingleOptionAnswer>(_ => _.IsAnswered == true && _.Answer == 1);
+            var childAnswer = Mock.Of<SingleOptionAnswer>(_ => _.IsAnswered == false);
+            var parentOptionAnswer = Mock.Of<SingleOptionAnswer>(_ => _.IsAnswered == false);
             var secondParentOptionAnswer = Mock.Of<SingleOptionAnswer>(_ => _.IsAnswered == true && _.Answer == 2);
 
             StatefulInterviewMock.Setup(x => x.Id).Returns(interviewGuid);
@@ -35,7 +37,7 @@ namespace WB.Tests.Unit.BoundedContexts.QuestionnaireTester.ViewModels.Cascading
 
             var cascadingQuestionModel = Mock.Of<CascadingSingleOptionQuestionModel>(_
                 => _.Id == questionIdentity.Id
-                   && _.Options == options
+                   && _.Options == Options
                    && _.CascadeFromQuestionId == parentIdentity.Id
                    && _.RosterLevelDepthOfParentQuestion == 1);
 
@@ -50,47 +52,32 @@ namespace WB.Tests.Unit.BoundedContexts.QuestionnaireTester.ViewModels.Cascading
             cascadingModel.Init(interviewGuid.FormatGuid(), questionIdentity, navigationState);
 
             StatefulInterviewMock.Setup(x => x.GetSingleOptionAnswer(parentIdentity)).Returns(secondParentOptionAnswer);
-
-            cascadingModel.Handle(Create.Event.SingleOptionQuestionAnswered(parentIdentity.Id, parentIdentity.RosterVector, 2));
         };
 
         Because of = () =>
-            cascadingModel.FilterText = "c";
+            cascadingModel.Handle(Create.Event.SingleOptionQuestionAnswered(parentIdentity.Id, parentIdentity.RosterVector, 2));
 
         It should_set_not_empty_list_in_AutoCompleteSuggestions = () =>
             cascadingModel.AutoCompleteSuggestions.ShouldNotBeEmpty();
 
-        It should_set_2_options_in_AutoCompleteSuggestions = () =>
-            cascadingModel.AutoCompleteSuggestions.Count.ShouldEqual(2);
+        It should_set_3_items_in_AutoCompleteSuggestions = () =>
+            cascadingModel.AutoCompleteSuggestions.Count.ShouldEqual(3);
 
-        It should_format_first_option_in_AutoCompleteSuggestions = () =>
-        {
-            var firstOption = cascadingModel.AutoCompleteSuggestions.ElementAt(0);
-            firstOption.Text.ShouldEqual("title <b>c</b>cc 5");
-            firstOption.Value.ShouldEqual(5);
-            firstOption.ParentValue.ShouldEqual(2);
-            firstOption.OriginalText.ShouldEqual("title ccc 5");
-        };
+        It should_create_option_models_with_specified_Texts = () =>
+            cascadingModel.AutoCompleteSuggestions.Select(x => x.Text).ShouldContainOnly(OptionsIfParentAnswerIs2.Select(x => x.Title));
 
-        It should_format_second_option_in_AutoCompleteSuggestions = () =>
-        {
-            var firstOption = cascadingModel.AutoCompleteSuggestions.ElementAt(1);
-            firstOption.Text.ShouldEqual("title b<b>c</b>w 6");
-            firstOption.Value.ShouldEqual(6);
-            firstOption.ParentValue.ShouldEqual(2);
-            firstOption.OriginalText.ShouldEqual("title bcw 6");
-        };
+        It should_create_option_models_with_specified_OriginalTexts = () =>
+            cascadingModel.AutoCompleteSuggestions.Select(x => x.OriginalText).ShouldContainOnly(OptionsIfParentAnswerIs2.Select(x => x.Title));
+
+        It should_create_option_models_with_specified_values = () =>
+            cascadingModel.AutoCompleteSuggestions.Select(x => x.Value).ShouldContainOnly(OptionsIfParentAnswerIs2.Select(x => x.Value));
+
+        It should_create_option_models_with_specified_ParentValues = () =>
+            cascadingModel.AutoCompleteSuggestions.Select(x => x.ParentValue).ShouldContainOnly(OptionsIfParentAnswerIs2.Select(x => x.ParentValue));
+
+        private static readonly List<CascadingOptionModel> OptionsIfParentAnswerIs2 = Options.Where(x => x.ParentValue == 2).ToList();
 
         private static CascadingSingleOptionQuestionViewModel cascadingModel;
-        protected static readonly List<CascadingOptionModel> options = new List<CascadingOptionModel>
-        {
-            Create.CascadingOptionModel(1, "title abc 1", 1),
-            Create.CascadingOptionModel(2, "title def 2", 1),
-            Create.CascadingOptionModel(3, "title klo 3", 1),
-            Create.CascadingOptionModel(4, "title gha 4", 2),
-            Create.CascadingOptionModel(5, "title ccc 5", 2),
-            Create.CascadingOptionModel(6, "title bcw 6", 2)
-        };
 
         private static readonly Mock<IStatefulInterview> StatefulInterviewMock = new Mock<IStatefulInterview>();
     }
