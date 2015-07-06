5c21d5f32542caa86625c1d10dd5fb2bef875bc6
Slava Scherbak <slava.scherbak@gmail.com>
2015-07-06 15:33:45 -0400

    Tester: added tests for cascading question. KP-5649

--- src/Tests/WB.Tests.Unit/BoundedContexts/QuestionnaireTester/ViewModels/CascadingSingleOptionQuestionViewModelTests/when_handling_SingleOptionQuestionAnswered_for_parent_question.cs
+++ src/Tests/WB.Tests.Unit/BoundedContexts/QuestionnaireTester/ViewModels/CascadingSingleOptionQuestionViewModelTests/when_handling_SingleOptionQuestionAnswered_for_parent_question.cs
@@ -1,11 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Machine.Specifications;
 using Moq;
 using WB.Core.BoundedContexts.QuestionnaireTester.Implementation.Aggregates;
 using WB.Core.BoundedContexts.QuestionnaireTester.Implementation.Entities;
 using WB.Core.BoundedContexts.QuestionnaireTester.Implementation.Entities.QuestionModels;
-using WB.Core.BoundedContexts.QuestionnaireTester.Infrastructure;
 using WB.Core.BoundedContexts.QuestionnaireTester.Repositories;
 using WB.Core.BoundedContexts.QuestionnaireTester.ViewModels.QuestionsViewModels;
 using WB.Core.GenericSubdomains.Portable;
@@ -54,8 +54,25 @@ namespace WB.Tests.Unit.BoundedContexts.QuestionnaireTester.ViewModels.Cascading
         Because of = () =>
             cascadingModel.Handle(Create.Event.SingleOptionQuestionAnswered(parentIdentity.Id, parentIdentity.RosterVector, 2));
 
-        It should_set_ShouldClearText_in_null = () =>
-            cascadingModel.ResetTextInEditor.ShouldBeNull();
+        It should_set_not_empty_list_in_AutoCompleteSuggestions = () =>
+            cascadingModel.AutoCompleteSuggestions.ShouldNotBeEmpty();
+
+        It should_set_3_items_in_AutoCompleteSuggestions = () =>
+            cascadingModel.AutoCompleteSuggestions.Count.ShouldEqual(3);
+
+        It should_create_option_models_with_specified_Texts = () =>
+            cascadingModel.AutoCompleteSuggestions.Select(x => x.Text).ShouldContainOnly(OptionsIfParentAnswerIs2.Select(x => x.Title));
+
+        It should_create_option_models_with_specified_OriginalTexts = () =>
+            cascadingModel.AutoCompleteSuggestions.Select(x => x.OriginalText).ShouldContainOnly(OptionsIfParentAnswerIs2.Select(x => x.Title));
+
+        It should_create_option_models_with_specified_values = () =>
+            cascadingModel.AutoCompleteSuggestions.Select(x => x.Value).ShouldContainOnly(OptionsIfParentAnswerIs2.Select(x => x.Value));
+
+        It should_create_option_models_with_specified_ParentValues = () =>
+            cascadingModel.AutoCompleteSuggestions.Select(x => x.ParentValue).ShouldContainOnly(OptionsIfParentAnswerIs2.Select(x => x.ParentValue));
+
+        private static readonly List<CascadingOptionModel> OptionsIfParentAnswerIs2 = Options.Where(x => x.ParentValue == 2).ToList();
 
         private static CascadingSingleOptionQuestionViewModel cascadingModel;
 
