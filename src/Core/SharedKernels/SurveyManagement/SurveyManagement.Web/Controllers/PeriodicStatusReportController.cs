e729bb6bc661f8477d6c241c1dc43dfb0752b4cb
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2015-07-10 17:59:10 -0400

    HQ\Supervisor: add limit attribute

--- src/Core/SharedKernels/SurveyManagement/SurveyManagement.Web/Controllers/PeriodicStatusReportController.cs
+++ src/Core/SharedKernels/SurveyManagement/SurveyManagement.Web/Controllers/PeriodicStatusReportController.cs
@@ -11,12 +11,14 @@ using WB.Core.SharedKernels.SurveyManagement.Views.Reposts;
 using WB.Core.SharedKernels.SurveyManagement.Views.Reposts.InputModels;
 using WB.Core.SharedKernels.SurveyManagement.Views.Reposts.Views;
 using WB.Core.SharedKernels.SurveyManagement.Views.UsersAndQuestionnaires;
+using WB.Core.SharedKernels.SurveyManagement.Web.Filters;
 using WB.Core.SharedKernels.SurveyManagement.Web.Models;
 using WB.Core.SharedKernels.SurveyManagement.Web.Resources;
 using WB.Core.SharedKernels.SurveyManagement.Web.Utils.Membership;
 
 namespace WB.Core.SharedKernels.SurveyManagement.Web.Controllers
 {
+    [LimitsFilter]
     public class PeriodicStatusReportController : BaseController
     {
         private readonly IViewFactory<AllUsersAndQuestionnairesInputModel, AllUsersAndQuestionnairesView> allUsersAndQuestionnairesFactory;
