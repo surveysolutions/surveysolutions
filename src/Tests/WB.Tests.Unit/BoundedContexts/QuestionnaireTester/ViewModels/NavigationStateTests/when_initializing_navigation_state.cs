b57d46bcf3c6d64c9646006d5f380014f9e9341e
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2015-07-10 10:51:41 +0300

    Tests\NavigationState: minor refactoring KP-5535

---
+++ src/Tests/WB.Tests.Unit/BoundedContexts/QuestionnaireTester/ViewModels/NavigationStateTests/when_initializing_navigation_state.cs
@@ -0,0 +1,26 @@
+using Machine.Specifications;
+using WB.Core.BoundedContexts.QuestionnaireTester.ViewModels;
+
+namespace WB.Tests.Unit.BoundedContexts.QuestionnaireTester.ViewModels.NavigationStateTests
+{
+    public class when_initializing_navigation_state
+    {
+        Establish context = () =>
+        {
+            navigationState = Create.NavigationState();
+        };
+
+        Because of = () =>
+            navigationState.Init(interviewId, questionnaireId);
+
+        It should_set_InterviewId_as_specified = () =>
+            navigationState.InterviewId.ShouldEqual(interviewId);
+
+        It should_set_QuestionnaireId_as_specified = () =>
+            navigationState.QuestionnaireId.ShouldEqual(questionnaireId);
+
+        private static NavigationState navigationState;
+        private static readonly string interviewId = "Some interviewId";
+        private static readonly string questionnaireId = "Questionnaire Id";
+    }
+}
