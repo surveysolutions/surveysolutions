24a3d93e3f33102289c6c740f3e13d528b3ab09e
Slava Scherbak <slava.scherbak@gmail.com>
2015-07-10 15:43:22 -0400

    Tester: added one more test for client validation for negative numeric integer answers. KP-5673

---
+++ src/Tests/WB.Tests.Unit/BoundedContexts/QuestionnaireTester/ViewModels/IntegerQuestionViewModelTests/when_answering_numeric_question_with_negative_value.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+
+using Machine.Specifications;
+
+using Moq;
+
+using WB.Core.BoundedContexts.QuestionnaireTester.Implementation.Aggregates;
+using WB.Core.BoundedContexts.QuestionnaireTester.Implementation.Entities;
+using WB.Core.BoundedContexts.QuestionnaireTester.Implementation.Entities.QuestionModels;
+using WB.Core.BoundedContexts.QuestionnaireTester.Repositories;
+using WB.Core.BoundedContexts.QuestionnaireTester.ViewModels.QuestionsViewModels;
+using WB.Core.GenericSubdomains.Portable;
+using WB.Core.Infrastructure.PlainStorage;
+using WB.Core.SharedKernels.DataCollection.Commands.Interview;
+
+using It = Machine.Specifications.It;
+
+namespace WB.Tests.Unit.BoundedContexts.QuestionnaireTester.ViewModels.IntegerQuestionViewModelTests
+{
+    public class when_answering_numeric_question_with_negative_value : IntegerQuestionViewModelTestContext
+    {
+        Establish context = () =>
+        {
+            SetUp();
+
+            var integerNumericAnswer = Mock.Of<IntegerNumericAnswer>(_ => _.IsAnswered == false);
+
+            var interview = Mock.Of<IStatefulInterview>(_
+                => _.QuestionnaireId == questionnaireId
+                   && _.GetIntegerNumericAnswer(questionIdentity) == integerNumericAnswer);
+
+            var interviewRepository = Mock.Of<IStatefulInterviewRepository>(x => x.Get(interviewGuid.FormatGuid()) == interview);
+
+            var cascadingQuestionModel = Mock.Of<IntegerNumericQuestionModel>(_
+                => _.Id == questionIdentity.Id && _.IsRosterSizeQuestion == false);
+
+            var questionnaireModel = Mock.Of<QuestionnaireModel>(_ => _.Questions == new Dictionary<Guid, BaseQuestionModel> { { questionIdentity.Id, cascadingQuestionModel } });
+
+            var questionnaireRepository = Mock.Of<IPlainKeyValueStorage<QuestionnaireModel>>(x => x.GetById(questionnaireId) == questionnaireModel);
+
+            integerModel = CreateIntegerQuestionViewModel(
+                interviewRepository: interviewRepository,
+                questionnaireRepository: questionnaireRepository);
+
+            integerModel.Init(interviewGuid.FormatGuid(), questionIdentity, navigationState);
+        };
+
+        Because of = () =>
+        {
+            integerModel.AnswerAsString = "-4";
+            integerModel.ValueChangeCommand.Execute();
+        };
+
+        It should_not_mark_question_as_invalid_with_message = () =>
+            ValidityModelMock.Verify(x => x.MarkAnswerAsInvalidWithMessage(Moq.It.IsAny<string>()), Times.Never);
+
+        It should_send_answer_command = () =>
+            AnsweringViewModelMock.Verify(x => x.SendAnswerQuestionCommand(Moq.It.IsAny<AnswerNumericIntegerQuestionCommand>()), Times.Once);
+
+
+        private static IntegerQuestionViewModel integerModel;
+    }
+}
