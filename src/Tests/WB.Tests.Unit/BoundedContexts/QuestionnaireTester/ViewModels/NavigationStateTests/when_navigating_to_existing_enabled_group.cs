ab21f25580f0d2f5dce282ef7a555525d5c99cce
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2015-07-10 12:49:12 +0300

    Tests\NavigationState: navigation to disabled or not existing groups covered with tests KP-5535

---
+++ src/Tests/WB.Tests.Unit/BoundedContexts/QuestionnaireTester/ViewModels/NavigationStateTests/when_navigating_to_existing_enabled_group.cs
@@ -0,0 +1,35 @@
+using System;
+using Machine.Specifications;
+using Moq;
+using WB.Core.BoundedContexts.QuestionnaireTester.Implementation.Aggregates;
+using WB.Core.BoundedContexts.QuestionnaireTester.ViewModels;
+using WB.Core.SharedKernels.DataCollection;
+using It = Machine.Specifications.It;
+
+namespace WB.Tests.Unit.BoundedContexts.QuestionnaireTester.ViewModels.NavigationStateTests
+{
+    public class when_navigating_to_existing_enabled_group
+    {
+        Establish context = () =>
+        {
+            var interview = Mock.Of<IStatefulInterview>(_
+                => _.HasGroup(existingEnabledGroup) == true
+                   && _.IsEnabled(existingEnabledGroup) == true);
+
+            navigationState = Create.NavigationState(
+                interviewRepository: Setup.StatefulInterviewRepository(interview));
+
+            navigationState.OnGroupChanged += eventArgs => navigatedTo = eventArgs.TargetGroup;
+        };
+
+        Because of = () =>
+            navigationState.NavigateTo(existingEnabledGroup);
+
+        It should_navigate_to_that_group = () =>
+            navigatedTo.ShouldEqual(existingEnabledGroup);
+
+        private static NavigationState navigationState;
+        private static Identity existingEnabledGroup = Create.Identity(Guid.Parse("11111111111111111111111111111111"), Empty.RosterVector);
+        private static Identity navigatedTo;
+    }
+}
