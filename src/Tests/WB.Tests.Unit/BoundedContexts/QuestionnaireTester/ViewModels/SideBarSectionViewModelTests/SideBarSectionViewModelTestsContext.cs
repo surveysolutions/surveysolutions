ff1f1b61d892408ee1c6cc29d9f689f9b88ea9a4
Andrii Kozhyn <hedgic@gmail.com>
2015-07-13 12:07:39 +0300

    KP-5663: Added unit test for roster title changed event

---
+++ src/Tests/WB.Tests.Unit/BoundedContexts/QuestionnaireTester/ViewModels/SideBarSectionViewModelTests/SideBarSectionViewModelTestsContext.cs
@@ -0,0 +1,27 @@
+﻿using Cirrious.MvvmCross.Plugins.Messenger;
+using Moq;
+using WB.Core.BoundedContexts.QuestionnaireTester.Implementation.Entities;
+using WB.Core.BoundedContexts.QuestionnaireTester.Repositories;
+using WB.Core.BoundedContexts.QuestionnaireTester.ViewModels;
+using WB.Core.Infrastructure.PlainStorage;
+
+namespace WB.Tests.Unit.BoundedContexts.QuestionnaireTester.ViewModels.SideBarSectionViewModelTests
+{
+    public class SideBarSectionViewModelTestsContext
+    {
+        protected static SideBarSectionViewModel CreateViewModel(QuestionnaireModel questionnaire = null)
+        {
+            Mock<IStatefulInterviewRepository> interviewRepository = new Mock<IStatefulInterviewRepository>();
+            Mock<IPlainKeyValueStorage<QuestionnaireModel>> questionnaireRepository = new Mock<IPlainKeyValueStorage<QuestionnaireModel>>();
+            questionnaireRepository.SetReturnsDefault(questionnaire);
+
+            return new SideBarSectionViewModel(interviewRepository.Object, 
+                questionnaireRepository.Object, 
+                Create.SubstitutionService(), 
+                Create.LiteEventRegistry(), 
+                Stub.MvxMainThreadDispatcher(), 
+                Stub.SideBarSectionViewModelsFactory(),
+                Mock.Of<IMvxMessenger>());
+        }
+    }
+}
