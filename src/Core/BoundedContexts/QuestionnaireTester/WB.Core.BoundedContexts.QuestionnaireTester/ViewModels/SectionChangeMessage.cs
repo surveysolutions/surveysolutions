d003354d64f3acbbb5917ed472b015a92376780b
Slava Scherbak <slava.scherbak@gmail.com>
2015-07-13 10:01:26 -0400

    Tester: fixed exception by removing unnecessary code. KP-5569

--- src/Core/BoundedContexts/QuestionnaireTester/WB.Core.BoundedContexts.QuestionnaireTester/ViewModels/SectionChangeMessage.cs
+++ src/Core/BoundedContexts/QuestionnaireTester/WB.Core.BoundedContexts.QuestionnaireTester/ViewModels/SectionChangeMessage.cs
@@ -4,16 +4,13 @@ namespace WB.Core.BoundedContexts.QuestionnaireTester.ViewModels
 {
     public class ScrollToAnchorMessage: MvxMessage
     {
-        public ScrollToAnchorMessage(object sender, int anchorElementIndex, int offsetInsideOfAnchoredItemInPercentage)
+        public ScrollToAnchorMessage(object sender, int anchorElementIndex)
             : base(sender)
         {
-            this.OffsetInsideOfAnchoredItemInPercentage = offsetInsideOfAnchoredItemInPercentage;
             this.AnchorElementIndex = anchorElementIndex;
         }
 
         public int AnchorElementIndex { get; private set; }
-
-        public int OffsetInsideOfAnchoredItemInPercentage { get; private set; }
     }
 
     public class SectionChangeMessage : MvxMessage
