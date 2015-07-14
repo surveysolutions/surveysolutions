7eda8535989ba01ecc93327df7ba82ffcc502527
Andrii Kozhyn <hedgic@gmail.com>
2015-07-14 11:11:14 +0300

    KP-5663: Moved initialization into Init method

---
+++ src/Core/BoundedContexts/QuestionnaireTester/WB.Core.BoundedContexts.QuestionnaireTester/ViewModels/SideBarSectionViewModelFactory.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Practices.ServiceLocation;
+using WB.Core.SharedKernels.DataCollection;
+
+namespace WB.Core.BoundedContexts.QuestionnaireTester.ViewModels
+{
+    internal class SideBarSectionViewModelFactory : ISideBarSectionViewModelsFactory
+    {
+        readonly IServiceLocator serviceLocator;
+
+        public SideBarSectionViewModelFactory(IServiceLocator serviceLocator)
+        {
+            this.serviceLocator = serviceLocator;
+        }
+
+        public SideBarSectionViewModel BuildSectionItem(SideBarSectionViewModel sectionToAddTo, 
+            Identity enabledSubgroupIdentity, 
+            NavigationState navigationState, 
+            string interviewId)
+        {
+            var sideBarItem = serviceLocator.GetInstance<SideBarSectionViewModel>();
+            sideBarItem.Init(interviewId, enabledSubgroupIdentity, sectionToAddTo, navigationState);
+            return sideBarItem;
+        }
+    }
+
+    public interface ISideBarSectionViewModelsFactory
+    {
+        SideBarSectionViewModel BuildSectionItem(SideBarSectionViewModel sectionToAddTo, Identity enabledSubgroupIdentity, NavigationState navigationState, string interviewId);
+    }
+}
