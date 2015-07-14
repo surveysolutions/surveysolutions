6df0d025e9369c8af0b4be52f673018a8a2d71e0
Andrii Kozhyn <hedgic@gmail.com>
2015-07-14 19:09:07 +0300

    Fixed unit tests

--- src/Tests/WB.Tests.Unit/Infrastructure/LiteEventBusTests/LiteEventBusTestsContext.cs
+++ src/Tests/WB.Tests.Unit/Infrastructure/LiteEventBusTests/LiteEventBusTestsContext.cs
@@ -13,17 +13,18 @@ namespace WB.Tests.Unit.Infrastructure.LiteEventBusTests
 
         public class DifferentDummyEvent  { }
 
-        public static IAggregateRoot CreateDummyAggregateRoot(params object[] events)
+        public static IAggregateRoot CreateDummyAggregateRoot(Guid eventSourceId, params object[] events)
         {
-            UncommittedEvent[] uncommittedEvents = events.Select(CreateUncommitedEvent).ToArray();
+            UncommittedEvent[] uncommittedEvents = events.Select(o => CreateUncommitedEvent(o, eventSourceId)).ToArray();
             Mock<IAggregateRoot> mock = new Mock<IAggregateRoot>();
             mock.Setup(a => a.GetUncommittedChanges()).Returns(uncommittedEvents);
+            mock.Setup(x => x.EventSourceId).Returns(eventSourceId);
             return mock.Object;
         }
 
-        private static UncommittedEvent CreateUncommitedEvent(object @event)
+        private static UncommittedEvent CreateUncommitedEvent(object @event, Guid eventSourceId)
         {
-            return new UncommittedEvent(Guid.Empty, Guid.Empty, 0, 0, DateTime.Now, @event);
+            return new UncommittedEvent(Guid.Empty, eventSourceId, 0, 0, DateTime.Now, @event);
         }
 
         internal static DummyEvent CreateDummyEvent()
@@ -31,15 +32,15 @@ namespace WB.Tests.Unit.Infrastructure.LiteEventBusTests
             return new DummyEvent();
         }
 
-        protected static IAggregateRoot SetupAggregateRootWithEventReadyForPublishing<T>(T @event)
+        protected static IAggregateRoot SetupAggregateRootWithEventReadyForPublishing<T>(Guid eventSourceId, T @event)
         {
-            return CreateDummyAggregateRoot(@event);
+            return CreateDummyAggregateRoot(eventSourceId, @event);
         }
 
         protected static IAggregateRoot SetupAggregateRootWithOneEventReadyForPublishing<T>() where T:new()
         {
             T obj = new T();
-            return CreateDummyAggregateRoot(obj);
+            return CreateDummyAggregateRoot(Guid.NewGuid(), obj);
         }
     }
 }
