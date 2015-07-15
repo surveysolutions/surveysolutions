f63a72a29e9a316a90a68b80b97c363de119a415
Andrii Kozhyn <hedgic@gmail.com>
2015-07-15 12:36:01 +0300

    KP-5689: Added check that group still exists on back button

---
+++ src/Tests/WB.Tests.Unit/BoundedContexts/Tester/ViewModels/NavigationStateTests/when_navigating_back_to_removed_roster_instance.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Machine.Specifications;
+using NSubstitute;
+using WB.Core.BoundedContexts.Tester.Implementation.Aggregates;
+using WB.Core.BoundedContexts.Tester.ViewModels;
+using WB.Core.GenericSubdomains.Portable.Tasks;
+using WB.Core.SharedKernels.DataCollection;
+
+namespace WB.Tests.Unit.BoundedContexts.Tester.ViewModels.NavigationStateTests
+{
+    [Subject(typeof (NavigationState))]
+    public class when_navigating_back_to_removed_roster_instance
+    {
+        Establish context = () =>
+        {
+            rosterIdentity = Create.Identity(Guid.Parse("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"), new[] {0m});
+            interview = Substitute.For<IStatefulInterview>();
+            interview.HasGroup(rosterIdentity)
+                     .Returns(true);
+            interview.IsEnabled(rosterIdentity)
+                     .Returns(true);
+
+            navigationState = Create.NavigationState(Setup.StatefulInterviewRepository(interview));
+            navigationState.NavigateTo(rosterIdentity).WaitAndUnwrapException();
+
+            emptyHistoryHandler = () => emptyHandlerCalled = true;
+        };
+
+        Because of = () =>
+        {
+            interview.HasGroup(rosterIdentity).Returns(false);
+            navigationState.NavigateBack(emptyHistoryHandler).WaitAndUnwrapException();
+        };
+
+        It should_skip_removed_group = () => emptyHandlerCalled.ShouldBeTrue();
+
+        static NavigationState navigationState;
+        static Identity rosterIdentity;
+        static IStatefulInterview interview;
+        static Action emptyHistoryHandler;
+        static bool emptyHandlerCalled;
+    }
+}
+
