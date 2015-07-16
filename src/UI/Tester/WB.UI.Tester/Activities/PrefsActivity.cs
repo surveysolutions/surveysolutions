865dbee3431fb5b7ecf177f09220d24158942b5c
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2015-07-16 13:16:11 +0300

    Removed engine version from settings page

--- src/UI/Tester/WB.UI.Tester/Activities/PrefsActivity.cs
+++ src/UI/Tester/WB.UI.Tester/Activities/PrefsActivity.cs
@@ -3,42 +3,22 @@ using Android.App;
 using Android.Content.PM;
 using Android.OS;
 using Android.Preferences;
-using Microsoft.Practices.ServiceLocation;
-using WB.Core.BoundedContexts.Tester.Infrastructure;
 
 namespace WB.UI.Tester.Activities
 {
     [Activity(Label = "Preferences activity", NoHistory = false)]
     public class PrefsActivity : PreferenceActivity
     {
-        private readonly IExpressionsEngineVersionService engineVersionService;
         private static int tapTimes = 0;
         private Preference devSettingsCategory;
-        private const string DevSettingName = "pref_dev";
-
-        public PrefsActivity()
-            : this(ServiceLocator.Current.GetInstance<IExpressionsEngineVersionService>()) // needed for android
-        {
-        }
-
-        public PrefsActivity(IExpressionsEngineVersionService engineVersionService)
-        {
-            this.engineVersionService = engineVersionService;
-        }
 
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
             this.AddPreferencesFromResource(Resource.Xml.preferences);
 
-            var settings = PreferenceManager.GetDefaultSharedPreferences(Application.Context);
-            var devSettingsEnabled = settings.GetBoolean(DevSettingName, false);
-
-            if (!devSettingsEnabled)
-            {
-                this.devSettingsCategory = this.FindPreference("dev_settings_category");
-                this.PreferenceScreen.RemovePreference(this.devSettingsCategory);
-            }
+            this.devSettingsCategory = this.FindPreference("dev_settings_category");
+            this.PreferenceScreen.RemovePreference(this.devSettingsCategory);
 
             this.SetupVersionPreference();
         }
@@ -51,20 +31,14 @@ namespace WB.UI.Tester.Activities
                 Interlocked.Increment(ref tapTimes);
                 if (tapTimes > 7)
                 {
-                    var defaultSharedPreferences = PreferenceManager.GetDefaultSharedPreferences(Application.Context);
-                    var sharedPreferencesEditor = defaultSharedPreferences.Edit();
-                    sharedPreferencesEditor.PutBoolean(DevSettingName, true);
-                    sharedPreferencesEditor.Commit();
-
                     this.PreferenceScreen.AddPreference(this.devSettingsCategory);
-
                     tapTimes = 0;
                 }
             };
 
             PackageInfo pInfo = this.PackageManager.GetPackageInfo(this.PackageName, 0);
             string format = this.Resources.GetString(Resource.String.Prefs_VersionSummaryFormat);
-            var summary = string.Format(format, pInfo.VersionName, System.Environment.NewLine, this.engineVersionService.GetExpressionsEngineSupportedVersion());
+            var summary = string.Format(format, pInfo.VersionName, System.Environment.NewLine);
             versionPreference.Summary = summary;
         }
     }
