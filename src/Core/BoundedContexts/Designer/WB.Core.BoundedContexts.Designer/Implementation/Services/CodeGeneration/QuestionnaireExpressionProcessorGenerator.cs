cd4e39eb8ca97c7beb4b25d64a247bf235377dda
Pavel Stovpivskyi <sby@rambler.ru>
2015-07-16 19:49:55 +0300

    Added DataCollection for Profile24 as resource
    KP-5684 Tester: Two different formats for old app and new one

--- src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Implementation/Services/CodeGeneration/QuestionnaireExpressionProcessorGenerator.cs
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Implementation/Services/CodeGeneration/QuestionnaireExpressionProcessorGenerator.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using Main.Core.Documents;
+using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Emit;
+using WB.Core.BoundedContexts.Designer.Resources;
 using WB.Core.BoundedContexts.Designer.Services;
 using WB.Core.BoundedContexts.Designer.Services.CodeGeneration;
+using WB.Core.SharedKernels.DataCollection;
 
 namespace WB.Core.BoundedContexts.Designer.Implementation.Services.CodeGeneration
 {
@@ -28,15 +31,34 @@ namespace WB.Core.BoundedContexts.Designer.Implementation.Services.CodeGeneratio
         {
             var generatedEvaluator = this.codeGenerator.GenerateEvaluator(questionnaire, targetVersion);
             var dynamicCompillerSettings = this.compilerSettingsProvider.GetSettings(targetVersion);
+            var referencedPortableAssemblies = GetReferencedPortableAssemblies(targetVersion);
 
             EmitResult emitedResult = this.codeCompiler.TryGenerateAssemblyAsStringAndEmitResult(
                 questionnaire.PublicKey, 
                 generatedEvaluator, 
-                new string[] { },
+                referencedPortableAssemblies.ToArray(),
                 dynamicCompillerSettings,
                 out generatedAssembly);
 
             return new GenerationResult(emitedResult.Success, emitedResult.Diagnostics);
+        }
+
+        static List<PortableExecutableReference> GetReferencedPortableAssemblies(Version targetVersion)
+        {
+            var referencedPortableAssemblies = new List<PortableExecutableReference>();
+            if (targetVersion.Major < 8)
+            {
+                var refProfile24 = AssemblyMetadata.CreateFromImage(
+                    RoslynCompilerResources.WB_Core_SharedKernels_DataCollection_Portable_Profile24
+                    ).GetReference();
+                referencedPortableAssemblies.Add(refProfile24);
+            }
+            else
+            {
+                referencedPortableAssemblies.Add(
+                    AssemblyMetadata.CreateFromFile(typeof (Identity).Assembly.Location).GetReference());
+            }
+            return referencedPortableAssemblies;
         }
 
         public Dictionary<string, string> GenerateProcessorStateClasses(QuestionnaireDocument questionnaire, Version targetVersion)
