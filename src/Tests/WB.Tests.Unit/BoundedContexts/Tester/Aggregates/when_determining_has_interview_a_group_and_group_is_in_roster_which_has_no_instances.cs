0152747521c092c8d9cb05e48f189ae522ad98c3
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2015-07-16 14:46:33 +0300

    StatefulInterview.HasGroup: fully covered with tests KP-5535

--- src/Tests/WB.Tests.Unit/BoundedContexts/Tester/Aggregates/when_determining_has_interview_a_group_and_group_is_in_roster_which_has_no_instances.cs
+++ src/Tests/WB.Tests.Unit/BoundedContexts/Tester/Aggregates/when_determining_has_interview_a_group_and_group_is_in_roster_which_has_no_instances.cs
@@ -5,12 +5,13 @@ using WB.Core.SharedKernels.DataCollection;
 
 namespace WB.Tests.Unit.BoundedContexts.QuestionnaireTester.Aggregates
 {
-    internal class when_determining_has_interview_a_group_and_questionnaire_has_no_such_group : StatefulInterviewTestsContext
+    internal class when_determining_has_interview_a_group_and_group_is_in_roster_which_has_no_instances : StatefulInterviewTestsContext
     {
         Establish context = () =>
         {
             Setup.QuestionnaireWithRepositoryToMockedServiceLocator(questionnaireId, _
-                => _.HasGroup(group.Id) == false);
+                => _.HasGroup(group.Id) == true
+                && _.GetRostersFromTopToSpecifiedGroup(group.Id) == new [] { rosterId });
 
             interview = Create.StatefulInterview(questionnaireId: questionnaireId);
         };
@@ -24,6 +25,7 @@ namespace WB.Tests.Unit.BoundedContexts.QuestionnaireTester.Aggregates
         private static bool result;
         private static StatefulInterview interview;
         private static Guid questionnaireId = Guid.Parse("CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC");
-        static Identity group = new Identity(Guid.Parse("11111111111111111111111111111111"), new decimal[] { });
+        private static Guid rosterId = Guid.Parse("44444444444444444444444444444444");
+        static Identity group = new Identity(Guid.Parse("11111111111111111111111111111111"), new [] { 4444m });
     }
 }
