3d318ae5d54afc0b23cb308f02bf36af9f7b6a57
Pavel Stovpivskyi <sby@rambler.ru>
2015-07-16 19:49:55 +0300

    Added DataCollection for Profile24 as resource
    KP-5684 Tester: Two different formats for old app and new one

--- src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Services/CodeGeneration/IDynamicCompiler.cs
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Services/CodeGeneration/IDynamicCompiler.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Emit;
 
 namespace WB.Core.BoundedContexts.Designer.Services.CodeGeneration
@@ -7,5 +8,12 @@ namespace WB.Core.BoundedContexts.Designer.Services.CodeGeneration
     public interface IDynamicCompiler
     {
         EmitResult TryGenerateAssemblyAsStringAndEmitResult(Guid templateId, Dictionary<string, string> generatedClasses, string[] referencedPortableAssemblies, IDynamicCompilerSettings settings, out string generatedAssembly);
+
+        EmitResult TryGenerateAssemblyAsStringAndEmitResult(
+            Guid templateId,
+            Dictionary<string, string> generatedClasses,
+            PortableExecutableReference[] referencedPortableAssemblies,
+            IDynamicCompilerSettings settings,
+            out string generatedAssembly);
     }
 }
