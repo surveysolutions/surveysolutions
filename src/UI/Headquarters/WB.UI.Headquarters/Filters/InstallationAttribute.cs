68a4993da4ac8c7f54adc6efc4395d9d7c8b711f
Roman Lytvyn <litvin.roma@gmail.com>
2015-07-20 15:53:51 +0300

    HQ: fixed installation page. KP-5605

--- src/UI/Headquarters/WB.UI.Headquarters/Filters/InstallationAttribute.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/Filters/InstallationAttribute.cs
@@ -21,26 +21,22 @@ namespace WB.UI.Headquarters.Filters
 
         public override void OnActionExecuting(ActionExecutingContext filterContext)
         {
-            if (!Installed)
+            if (Installed) return;
+
+            if (filterContext.Controller is Controllers.ControlPanelController) return;
+            if (filterContext.Controller is MaintenanceController) return;
+
+            var isInstallController = filterContext.Controller is InstallController;
+            Installed = identityManager.GetUsersInRole(UserRoles.Administrator.ToString()).Any();
+
+            if (isInstallController && Installed)
+                throw new HttpException(404, string.Empty);
+
+            if (!isInstallController && !Installed)
             {
-
-                if (filterContext.Controller is WB.UI.Headquarters.Controllers.ControlPanelController) return;
-                if (filterContext.Controller is MaintenanceController) return;
-
-                var isInstallController = filterContext.Controller is InstallController;
-            var isAdminExists = identityManager.GetUsersInRole(UserRoles.Administrator.ToString()).Any();
-
-            if (isInstallController && (isAdminExists))
-                    throw new HttpException(404, string.Empty);
-
-            if (!isInstallController && !isAdminExists)
-                {
-                    filterContext.Result =
-                        new RedirectToRouteResult(
-                            new RouteValueDictionary(new { controller = "Install", action = "Finish" }));
-                }
-
-                Installed = true;
+                filterContext.Result =
+                    new RedirectToRouteResult(
+                        new RouteValueDictionary(new {controller = "Install", action = "Finish"}));
             }
         }
     }
