dd282061cd12fb9c83be5c5837e11535071d872f
slava <slava.scherbak@gmail.com>
2015-07-20 15:46:48 -0400

    Tests: committed lost file. KP-5684

--- src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Services/CodeGeneration/IDynamicCompilerSettingsProvider.cs
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Services/CodeGeneration/IDynamicCompilerSettingsProvider.cs
@@ -1,12 +1,11 @@
 using System;
 using System.Collections.Generic;
 using Microsoft.CodeAnalysis;
-using WB.Core.BoundedContexts.Designer.Implementation.Services.CodeGeneration;
 
 namespace WB.Core.BoundedContexts.Designer.Services.CodeGeneration
 {
     public interface IDynamicCompilerSettingsProvider
     {
-        IEnumerable<PortableExecutableReference> GetAssembliesToRoslyn(Version apiVersion);
+        IEnumerable<PortableExecutableReference> GetAssembliesToReference(Version apiVersion);
     }
 }
