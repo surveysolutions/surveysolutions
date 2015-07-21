0a16d77f0affa5435cd2f0b4394e6d74a23c81cf
Roman Lytvyn <litvin.roma@gmail.com>
2015-07-21 17:10:41 +0300

    HQ: fixed duplicating of questionnaires on the map report. KP-5397

--- src/Core/SharedKernels/SurveyManagement/SurveyManagement.Web/Api/ReportDataApiController.cs
+++ src/Core/SharedKernels/SurveyManagement/SurveyManagement.Web/Api/ReportDataApiController.cs
@@ -120,7 +120,7 @@ namespace WB.Core.SharedKernels.SurveyManagement.Web.Api
             QuestionnaireBrowseView questionnaireBrowseView = this.questionnaireBrowseViewFactory.Load(input);
             var result = new QuestionnaireAndVersionsView
             {
-                Items = questionnaireBrowseView.Items
+                Items = questionnaireBrowseView.Items.GroupBy(x => x.QuestionnaireId).Select(x => x.First())
                                                .Select(x => new QuestionnaireAndVersionsItem {
                                                         QuestionnaireId = x.QuestionnaireId,
                                                         Title = x.Title,
