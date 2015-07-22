2d9ee5156bd62600008ac84d13f7d66be83f4c34
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2015-07-22 15:00:12 +0300

    Utils: added GlobalStopwatcher usable for performance analysis KP-5719

--- src/Core/GenericSubdomains/WB.Core.GenericSubdomains.Portable/DictionaryExtensions.cs
+++ src/Core/GenericSubdomains/WB.Core.GenericSubdomains.Portable/DictionaryExtensions.cs
@@ -5,6 +5,12 @@ namespace WB.Core.GenericSubdomains.Portable
 {
     public static class DictionaryExtensions
     {
+        public static TValue GetOrAdd<TKey, TValue>(this Dictionary<TKey, TValue> dictionary, TKey key)
+            where TValue : new()
+        {
+            return dictionary.GetOrAdd(key, () => new TValue());
+        }
+
         public static TValue GetOrAdd<TKey, TValue>(this Dictionary<TKey, TValue> dict, TKey key, Func<TValue> valueInit)
         {
             TValue value;
