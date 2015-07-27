6234a9c8883f1305dce2edaef34639d6e5c3586b
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2015-07-27 22:41:20 +0300

    HQ\UserPreloading: draft verion of user preloading KP-5591

---
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/UserPreloading/Services/IUserPreloadingService.cs
@@ -0,0 +1,32 @@
+using System;
+using System.IO;
+using WB.Core.BoundedContexts.Headquarters.UserPreloading.Dto;
+
+namespace WB.Core.BoundedContexts.Headquarters.UserPreloading.Services
+{
+    public interface IUserPreloadingService
+    {
+        string CreateUserPreloadingProcess(Stream data, string fileName);
+
+        void DeletePreloadingProcess(string preloadingProcessId);
+
+        void FinishValidationProcess(string preloadingProcessId);
+
+        void FinishPreloadingProcess(string preloadingProcessId);
+
+        void FinishPreloadingProcessWithError(string preloadingProcessId, string errorMessage);
+
+        void EnqueueForValidation(string preloadingProcessId);
+        void EnqueueForUserCreation(string preloadingProcessId);
+
+        string DeQueuePreloadingProcessIdReadyToBeValidated();
+        string DeQueuePreloadingProcessIdReadyToCreateUsers();
+        
+        UserPreloadingProcess[] GetPreloadingProcesses();
+        UserPreloadingProcess GetPreloadingProcesseDetails(string preloadingProcessId);
+
+        void PushVerificationError(string preloadingProcessId, string code, string message, int rowNumber, string columnName, string cellValue);
+        void IncreaseCountCreateUsers(string preloadingProcessId);
+        string[] GetAvaliableDataColumnNames();
+    }
+}
---
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/UserPreloading/Tasks/UserPreloadingVerifierTask.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Quartz;
+using WB.Core.BoundedContexts.Headquarters.UserPreloading.Jobs;
+
+namespace WB.Core.BoundedContexts.Headquarters.UserPreloading.Tasks
+{
+    public class UserPreloadingVerifierTask
+    {
+        readonly IScheduler scheduler;
+
+        public UserPreloadingVerifierTask(IScheduler scheduler)
+        {
+            if (scheduler == null) throw new ArgumentNullException("scheduler");
+            this.scheduler = scheduler;
+        }
+
+        public void Configure()
+        {
+            IJobDetail job = JobBuilder.Create<UserPreloadingVerifier>()
+                .WithIdentity("user preloading verification", "Verification")
+                .StoreDurably(true)
+                .Build();
+
+            ITrigger trigger = TriggerBuilder.Create()
+                .WithIdentity("user preloading verification", "Verification")
+                .StartNow()
+                .WithSimpleSchedule(x => x
+                    .WithIntervalInSeconds(5)
+                    .RepeatForever())
+                .Build();
+
+            this.scheduler.ScheduleJob(job, trigger);
+
+            this.scheduler.AddJob(job, true);
+        }
+    }
+}
