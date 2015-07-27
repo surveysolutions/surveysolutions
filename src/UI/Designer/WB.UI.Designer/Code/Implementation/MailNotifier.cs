9c5b63016139ed7ea5b05c0846e494f0c269434c
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2015-07-27 12:51:42 +0300

    review KP-5596 Notify people about shared questionnaire

--- src/UI/Designer/WB.UI.Designer/Code/Implementation/MailNotifier.cs
+++ src/UI/Designer/WB.UI.Designer/Code/Implementation/MailNotifier.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Main.Core.Entities.SubEntities;
 using WB.Core.GenericSubdomains.Portable;
+using WB.Core.GenericSubdomains.Portable.Services;
 using WB.UI.Designer.Mailers;
 using WB.UI.Designer.Models;
 using WB.UI.Designer.Resources;
@@ -9,12 +11,14 @@ namespace WB.UI.Designer.Code.Implementation
 {
     public class MailNotifier : IRecipientNotifier
     {
-        public MailNotifier(ISystemMailer mailer)
+        public MailNotifier(ISystemMailer mailer, ILogger logger)
         {
             this.mailer = mailer;
+            this.logger = logger;
         }
 
         private readonly ISystemMailer mailer;
+        private readonly ILogger logger;
         
         public void NotifyTargetPersonAboutShareChange(ShareChangeType shareChangeType, string email, string userName, Guid questionnaireId, 
             string questionnaireTitle, ShareType shareType, string actionPersonEmail)
@@ -26,11 +30,11 @@ namespace WB.UI.Designer.Code.Implementation
                                     Email = email.ToWBEmailAddress(),
                                     UserCallName = String.IsNullOrWhiteSpace(userName) ? email : userName,
                                     QuestionnaireId = questionnaireId,
-                                    QuestionnaireDisplayTitle = String.IsNullOrWhiteSpace(questionnaireTitle) ? NotificationMessages.MailNotifier_NotifyTargetPersonAboutShareChange_link : questionnaireTitle,
-                                    ShareTypeName = shareType == ShareType.Edit ? NotificationMessages.MailNotifier_NotifyTargetPersonAboutShareChange_edit : NotificationMessages.MailNotifier_NotifyOwnerAboutShareChange_view,
-                                    ActionPersonCallName = String.IsNullOrWhiteSpace(actionPersonEmail) ? NotificationMessages.MailNotifier_NotifyTargetPersonAboutShareChange_user : actionPersonEmail
+                                    QuestionnaireDisplayTitle = String.IsNullOrWhiteSpace(questionnaireTitle) ? NotificationResources.MailNotifier_NotifyTargetPersonAboutShareChange_link : questionnaireTitle,
+                                    ShareTypeName = shareType == ShareType.Edit ? NotificationResources.MailNotifier_NotifyTargetPersonAboutShareChange_edit : NotificationResources.MailNotifier_NotifyOwnerAboutShareChange_view,
+                                    ActionPersonCallName = String.IsNullOrWhiteSpace(actionPersonEmail) ? NotificationResources.MailNotifier_NotifyTargetPersonAboutShareChange_user : actionPersonEmail
                                 });
-            message.SendAsync();
+            message.SendAsync().ContinueWith(exception => logger.Error("Sending failed: " + exception), TaskContinuationOptions.OnlyOnFaulted);
         }
         
         public void NotifyOwnerAboutShareChange(ShareChangeType shareChangeType, string email, string userName, Guid questionnaireId, string questionnaireTitle,
@@ -43,14 +47,14 @@ namespace WB.UI.Designer.Code.Implementation
                                     Email = email.ToWBEmailAddress(),
                                     UserCallName = String.IsNullOrWhiteSpace(userName) ? email : userName,
                                     QuestionnaireId = questionnaireId,
-                                    QuestionnaireDisplayTitle = String.IsNullOrWhiteSpace(questionnaireTitle) ? NotificationMessages.MailNotifier_NotifyTargetPersonAboutShareChange_link : questionnaireTitle,
-                                    ShareTypeName = shareType == ShareType.Edit ? NotificationMessages.MailNotifier_NotifyTargetPersonAboutShareChange_edit : NotificationMessages.MailNotifier_NotifyOwnerAboutShareChange_view,
-                                    ActionPersonCallName = String.IsNullOrWhiteSpace(actionPersonEmail) ? NotificationMessages.MailNotifier_NotifyTargetPersonAboutShareChange_user : actionPersonEmail,
-                                    SharedWithPersonEmail = String.IsNullOrWhiteSpace(sharedWithPersonEmail) ? NotificationMessages.MailNotifier_NotifyTargetPersonAboutShareChange_user : sharedWithPersonEmail
+                                    QuestionnaireDisplayTitle = String.IsNullOrWhiteSpace(questionnaireTitle) ? NotificationResources.MailNotifier_NotifyTargetPersonAboutShareChange_link : questionnaireTitle,
+                                    ShareTypeName = shareType == ShareType.Edit ? NotificationResources.MailNotifier_NotifyTargetPersonAboutShareChange_edit : NotificationResources.MailNotifier_NotifyOwnerAboutShareChange_view,
+                                    ActionPersonCallName = String.IsNullOrWhiteSpace(actionPersonEmail) ? NotificationResources.MailNotifier_NotifyTargetPersonAboutShareChange_user : actionPersonEmail,
+                                    SharedWithPersonEmail = String.IsNullOrWhiteSpace(sharedWithPersonEmail) ? NotificationResources.MailNotifier_NotifyTargetPersonAboutShareChange_user : sharedWithPersonEmail
 
                                 });
-            message.SendAsync();
+
+            message.SendAsync().ContinueWith(exception => logger.Error("Sending failed: " + exception), TaskContinuationOptions.OnlyOnFaulted);
         }
-        
     }
 }
