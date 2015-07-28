c36e6effdd4a7124c7514a4acaf1ed065a8cbe95
Roman Lytvyn <litvin.roma@gmail.com>
2015-07-28 16:44:30 +0300

    Tester: fixed showing unauthorized message on login activity if pulling request on the dashboard. KP-5820 "Invalid username/password" message when logging out

--- src/Core/BoundedContexts/Tester/WB.Core.BoundedContexts.Tester/ViewModels/DashboardViewModel.cs
+++ src/Core/BoundedContexts/Tester/WB.Core.BoundedContexts.Tester/ViewModels/DashboardViewModel.cs
@@ -274,7 +274,7 @@ namespace WB.Core.BoundedContexts.Tester.ViewModels
             {
                 string errorMessage = this.friendlyMessageService.GetFriendlyErrorMessageByRestException(ex);
 
-                if (!string.IsNullOrEmpty(errorMessage))
+                if (!string.IsNullOrEmpty(errorMessage) && this.IsInitialized)
                     this.userInteractionService.Alert(errorMessage);
                 else
                     throw;
