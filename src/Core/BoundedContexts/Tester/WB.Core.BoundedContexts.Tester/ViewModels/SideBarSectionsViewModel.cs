49eb353691da8cd9573016ff114568e2ad39e6ba
Pavel Stovpivskyi <sby@rambler.ru>
2015-07-31 16:51:57 +0300

    Fixed SideBar depth

--- src/Core/BoundedContexts/Tester/WB.Core.BoundedContexts.Tester/ViewModels/SideBarSectionsViewModel.cs
+++ src/Core/BoundedContexts/Tester/WB.Core.BoundedContexts.Tester/ViewModels/SideBarSectionsViewModel.cs
@@ -95,7 +95,9 @@ namespace WB.Core.BoundedContexts.Tester.ViewModels
 
         private void HighlightCurrentSection(GroupChangedEventArgs navigationParams)
         {
-            SideBarSectionViewModel selectedGroup = AllVisibleSections
+            var allTreeElements = new ReadOnlyCollection<SideBarSectionViewModel>(this.Sections)
+                .TreeToEnumerable(x => x.Children).ToList();
+            SideBarSectionViewModel selectedGroup = allTreeElements
                 .FirstOrDefault(x => x.SectionIdentity.Equals(navigationParams.TargetGroup));
 
             var sideBarSectionToHighlight = selectedGroup;
@@ -117,6 +119,7 @@ namespace WB.Core.BoundedContexts.Tester.ViewModels
             sideBarSectionToHighlight.TreeToEnumerable(s => s.Children)
                 .Where(s => !s.IsSelected)
                 .ForEach(s => s.IsSelected = true);
+            selectedGroup.Expanded = true;
 
             this.UpdateSideBarTree();
         }
