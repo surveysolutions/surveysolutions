9f8ed2fbfbdb0536028761cc3f5ca752eb4c7afb
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2015-08-04 12:51:32 +0300

    Fixed files module KP-5853

--- src/Core/Infrastructure/WB.Core.Infrastructure.Files.Android/FileInfrastructureModule.cs
+++ src/Core/Infrastructure/WB.Core.Infrastructure.Files.Android/FileInfrastructureModule.cs
@@ -2,13 +2,12 @@
 using WB.Core.Infrastructure.Files.Implementation.FileSystem;
 using WB.Core.Infrastructure.FileSystem;
 
-namespace WB.Core.Infrastructure.Files
+namespace WB.Core.Infrastructure.Files.Android
 {
     public class FileInfrastructureModule : NinjectModule
     {
         public override void Load()
         {
-            this.Bind<IFileSystemAccessor>().To<FileSystemIOAccessor>();
             this.Bind<IArchiveUtils>().To<ZipArchiveUtils>();
         }
     }
