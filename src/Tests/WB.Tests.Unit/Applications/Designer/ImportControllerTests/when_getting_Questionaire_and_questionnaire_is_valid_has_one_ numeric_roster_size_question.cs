96e160ddc8f2136aa79c672625d664b523a6d3b8
Slava Scherbak <slava.scherbak@gmail.com>
2015-08-04 17:32:08 -0400

    fixed merge

--- src/Tests/WB.Tests.Unit/Applications/Designer/ImportControllerTests/when_getting_Questionaire_and_questionnaire_is_valid_has_one_ numeric_roster_size_question.cs
+++ src/Tests/WB.Tests.Unit/Applications/Designer/ImportControllerTests/when_getting_Questionaire_and_questionnaire_is_valid_has_one_ numeric_roster_size_question.cs
@@ -8,12 +8,13 @@ using Moq;
 using WB.Core.BoundedContexts.Designer.Services;
 using WB.Core.BoundedContexts.Designer.ValueObjects;
 using WB.Core.BoundedContexts.Designer.Views.Questionnaire.Edit;
-using WB.Core.GenericSubdomains.Utils.Services;
+using WB.Core.GenericSubdomains.Portable.Services;
 using WB.Core.Infrastructure.ReadSide;
 using WB.Core.SharedKernel.Structures.Synchronization.Designer;
 using WB.Core.SharedKernels.SurveySolutions.Documents;
 using WB.UI.Designer.Api;
 using WB.UI.Shared.Web.Membership;
+
 using It = Machine.Specifications.It;
 
 namespace WB.Tests.Unit.Applications.Designer.ImportControllerTests
