b3563e26d4e274ca3fa9ee215a010ce7d6e36603
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2015-08-07 11:14:36 +0300

    Display group titles on rosters KP-5884

--- src/UI/Capi/WB.UI.Capi/Setup.cs
+++ src/UI/Capi/WB.UI.Capi/Setup.cs
@@ -101,6 +101,7 @@ namespace WB.UI.Capi
             registry.RegisterCustomBindingFactory<EditText>("DateChange", editText => new EditTextDateBinding(editText));
             registry.RegisterCustomBindingFactory<TextView>("GroupInfo", textView => new TextViewGroupInfoBinding(textView));
             registry.RegisterCustomBindingFactory<Button>("ButtonGroupStyle", button => new ButtonGroupStyleBinding(button));
+            registry.RegisterCustomBindingFactory<Button>("GroupTitle", button => new ButtonGroupTitleBinding(button));
             registry.RegisterCustomBindingFactory<TextView>("GroupStatus", textView => new TextViewGroupStatusBinding(textView));
             registry.RegisterCustomBindingFactory<View>("HideKeyboardOnClick", view => new ViewHideKeyboardOnClickBinding(view));
             registry.RegisterCustomBindingFactory<MvxAutoCompleteTextView>("HidePopupOnDone", view => new MvxAutoCompleteTextViewHidePopupOnDoneBinding(view));
