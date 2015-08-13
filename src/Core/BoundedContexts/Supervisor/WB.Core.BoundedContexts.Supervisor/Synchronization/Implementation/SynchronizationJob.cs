cfb7cb3551c02b4e300de972f7fd91988731b48c
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2015-08-13 17:16:03 -0400

    Supervisor\Sync: KP-5916 blind attempt to fix interview creation dublication

--- src/Core/BoundedContexts/Supervisor/WB.Core.BoundedContexts.Supervisor/Synchronization/Implementation/SynchronizationJob.cs
+++ src/Core/BoundedContexts/Supervisor/WB.Core.BoundedContexts.Supervisor/Synchronization/Implementation/SynchronizationJob.cs
@@ -1,5 +1,6 @@
 using System;
 using Quartz;
+using WB.Core.Infrastructure.Storage;
 
 namespace WB.Core.BoundedContexts.Supervisor.Synchronization.Implementation
 {
@@ -14,9 +15,18 @@ namespace WB.Core.BoundedContexts.Supervisor.Synchronization.Implementation
 
         public void Execute(IJobExecutionContext context)
         {
-            ISynchronizer synchronizer = this.synchronizerFactory.Invoke(); // we want synchronizer be transient but job is singleton, so we use factory in-between
+            IsolatedThreadManager.MarkCurrentThreadAsIsolated();
+            try
+            {
+                ISynchronizer synchronizer = this.synchronizerFactory.Invoke();
+                // we want synchronizer be transient but job is singleton, so we use factory in-between
 
-            synchronizer.Pull();
+                synchronizer.Pull();
+            }
+            finally
+            {
+                IsolatedThreadManager.ReleaseCurrentThreadFromIsolation();
+            }
         }
     }
 }
