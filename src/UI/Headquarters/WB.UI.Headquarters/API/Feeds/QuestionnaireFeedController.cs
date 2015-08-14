3b2b30c34a7e6d05447ec69838c9e7ac948a9a44
Anastasya Kholodova <nastya.kholodova@gmail.com>
2015-08-14 15:44:26 -0400

    HQ\Sync: KP-5916 add second order by

--- src/UI/Headquarters/WB.UI.Headquarters/API/Feeds/QuestionnaireFeedController.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/API/Feeds/QuestionnaireFeedController.cs
@@ -37,7 +37,7 @@ namespace WB.UI.Headquarters.API.Feeds
 
             if (totalFeedEntriesCount%PageSize > 0)
             {
-                questionnaireChangedFeedEntries = this.questionnaireChangedReader.Query(_ => _.OrderBy(x => x.Timestamp).SkipFullPages(PageSize, totalFeedEntriesCount).ToList());
+                questionnaireChangedFeedEntries = this.questionnaireChangedReader.Query(_ => _.OrderBy(x => x.Timestamp).ThenBy(x => x.EntryId).SkipFullPages(PageSize, totalFeedEntriesCount).ToList());
             }
 
             var feed = this.GetFeed(questionnaireChangedFeedEntries);
@@ -55,7 +55,7 @@ namespace WB.UI.Headquarters.API.Feeds
         [HttpGet]
         public HttpResponseMessage Archive(int page)
         {
-            var changedFeedEntries = this.questionnaireChangedReader.Query(_ => _.OrderBy(x => x.Timestamp).GetPage(page, PageSize).ToList());
+            var changedFeedEntries = this.questionnaireChangedReader.Query(_ => _.OrderBy(x => x.Timestamp).ThenBy(x => x.EntryId).GetPage(page, PageSize).ToList());
             SyndicationFeed syndicationFeed = this.GetFeed(changedFeedEntries);
 
             if (page > 1)
