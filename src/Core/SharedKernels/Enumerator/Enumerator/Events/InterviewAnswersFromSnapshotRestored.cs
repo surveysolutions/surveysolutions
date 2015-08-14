dc7916cdfce3d4b7b5452eaafa2028f9c0a81cf1
slava <slava.scherbak@gmail.com>
2015-08-14 17:59:10 -0400

    Interviewer: fixed applying of sync package on interview opening. KP-5921

---
+++ src/Core/SharedKernels/Enumerator/Enumerator/Events/InterviewAnswersFromSnapshotRestored.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using WB.Core.SharedKernels.DataCollection.Events.Interview.Base;
+using WB.Core.SharedKernels.Enumerator.DataTransferObjects;
+
+namespace WB.Core.SharedKernels.Enumerator.Events
+{
+    public class InterviewAnswersFromSnapshotRestored : InterviewActiveEvent
+    {
+        public InterviewAnswersFromSnapshotRestored(InterviewAnswerDto[] interviewData, Guid userId)
+            : base(userId)
+        {
+            this.Answers = interviewData;
+        }
+
+        public InterviewAnswerDto[] Answers { get; private set; }
+    }
+}
