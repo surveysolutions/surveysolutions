5a7e6e54449a40766ea05549cc7ad47efac0d367
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2015-08-14 18:04:18 -0400

    HQ\Sync: KP-5916 fix unit tests

--- src/Tests/WB.Tests.Unit/SharedKernels/DataCollection/InterviewTests/when_synchronizing_from_hq_interview_which_existed_before.cs
+++ src/Tests/WB.Tests.Unit/SharedKernels/DataCollection/InterviewTests/when_synchronizing_from_hq_interview_which_existed_before.cs
@@ -23,8 +23,17 @@ namespace WB.Tests.Unit.SharedKernels.DataCollection.InterviewTests
         {
             var questionnaireId = Guid.NewGuid();
 
-            interview = CreateInterview(questionnaireId: questionnaireId);
+            var questionnaire = Mock.Of<IQuestionnaire>();
 
+            var questionnaireRepository = Mock.Of<IQuestionnaireRepository>(repository
+                =>
+                repository.GetHistoricalQuestionnaire(Moq.It.IsAny<Guid>(), Moq.It.IsAny<long>()) == questionnaire);
+
+            Mock.Get(ServiceLocator.Current)
+                .Setup(locator => locator.GetInstance<IQuestionnaireRepository>())
+                .Returns(questionnaireRepository);
+
+            interview = CreateInterview(questionnaireId: questionnaireId);
         };
 
         Because of = () => exception = Catch.Only<InterviewException>(() => interview.SynchronizeInterviewFromHeadquarters(interview.EventSourceId, Guid.NewGuid(), Guid.NewGuid(), Create.InterviewSynchronizationDto(), DateTime.Now));
