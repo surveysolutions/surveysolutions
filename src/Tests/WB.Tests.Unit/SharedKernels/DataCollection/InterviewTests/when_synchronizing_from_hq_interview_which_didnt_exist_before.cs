3fe5736fb17c84a754b87fdc53175db2a50461f3
Anastasya Kholodova <nastya.kholodova@gmail.com>
2015-08-14 17:39:17 -0400

    Super\Sync: KP-5916 prevent interview from second creation

---
+++ src/Tests/WB.Tests.Unit/SharedKernels/DataCollection/InterviewTests/when_synchronizing_from_hq_interview_which_didnt_exist_before.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Machine.Specifications;
+using Ncqrs.Spec;
+using WB.Core.SharedKernels.DataCollection.Events.Interview;
+using WB.Core.SharedKernels.DataCollection.Exceptions;
+using WB.Core.SharedKernels.DataCollection.Implementation.Aggregates;
+
+namespace WB.Tests.Unit.SharedKernels.DataCollection.InterviewTests
+{
+    internal class when_synchronizing_from_hq_interview_which_didnt_exist_before : InterviewTestsContext
+    {
+        Establish context = () =>
+        {
+            var questionnaireId = Guid.NewGuid();
+
+            interview = new Interview();
+
+            eventContext = new EventContext();
+        };
+
+        Cleanup stuff = () =>
+        {
+            eventContext.Dispose();
+            eventContext = null;
+        };
+
+        Because of = () => interview.SynchronizeInterviewFromHeadquarters(interview.EventSourceId, Guid.NewGuid(), Guid.NewGuid(), Create.InterviewSynchronizationDto(), DateTime.Now);
+
+        It should_raise_InterviewCreated_event = () =>
+           eventContext.ShouldContainEvent<InterviewCreated>();
+
+        private static Interview interview;
+
+        private static EventContext eventContext;
+    }
+}
---
+++ src/Tests/WB.Tests.Unit/SharedKernels/DataCollection/InterviewTests/when_synchronizing_from_hq_interview_which_existed_before.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Machine.Specifications;
+using Microsoft.Practices.ServiceLocation;
+using Moq;
+using Ncqrs.Spec;
+using WB.Core.SharedKernels.DataCollection.Aggregates;
+using WB.Core.SharedKernels.DataCollection.DataTransferObjects.Synchronization;
+using WB.Core.SharedKernels.DataCollection.Events.Interview;
+using WB.Core.SharedKernels.DataCollection.Exceptions;
+using WB.Core.SharedKernels.DataCollection.Implementation.Aggregates;
+using WB.Core.SharedKernels.DataCollection.Implementation.Providers;
+using WB.Core.SharedKernels.DataCollection.Repositories;
+using WB.Core.SharedKernels.DataCollection.V2;
+using WB.Core.SharedKernels.DataCollection.ValueObjects.Interview;
+using It = Machine.Specifications.It;
+
+namespace WB.Tests.Unit.SharedKernels.DataCollection.InterviewTests
+{
+    internal class when_synchronizing_from_hq_interview_which_existed_before : InterviewTestsContext
+    {
+        Establish context = () =>
+        {
+            var questionnaireId = Guid.NewGuid();
+
+            interview = CreateInterview(questionnaireId: questionnaireId);
+
+        };
+
+        Because of = () => exception = Catch.Only<InterviewException>(() => interview.SynchronizeInterviewFromHeadquarters(interview.EventSourceId, Guid.NewGuid(), Guid.NewGuid(), Create.InterviewSynchronizationDto(), DateTime.Now));
+
+        It should_raise_InterviewException = () =>
+           exception.ShouldNotBeNull();
+
+        private static Interview interview;
+        private static InterviewException exception;
+    }
+}
