a122511841ceefd0f95c191a326968cda1f91ae1
slava <slava.scherbak@gmail.com>
2015-08-17 18:38:30 -0400

    Tester: removed DownloadCache plugin. KP-5857

--- src/UI/Tester/WB.UI.Tester/Bootstrap/DownloadCachePluginBootstrap.cs
+++
@@ -1,9 +0,0 @@
-using Cirrious.CrossCore.Plugins;
-
-namespace WB.UI.Tester.Bootstrap
-{
-    public class DownloadCachePluginBootstrap
-        : MvxPluginBootstrapAction<Cirrious.MvvmCross.Plugins.DownloadCache.PluginLoader>
-    {
-    }
-}
