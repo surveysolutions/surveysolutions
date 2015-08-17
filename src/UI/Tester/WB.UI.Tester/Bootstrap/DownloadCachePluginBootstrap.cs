6ab58b692c3ea493b476cd1d10c8fd76bd2fb47d
slava <slava.scherbak@gmail.com>
2015-08-17 19:23:40 -0400

    Revert "Tester: removed DownloadCache plugin. KP-5857"
    This reverts commit a122511841ceefd0f95c191a326968cda1f91ae1.

---
+++ src/UI/Tester/WB.UI.Tester/Bootstrap/DownloadCachePluginBootstrap.cs
@@ -0,0 +1,9 @@
+using Cirrious.CrossCore.Plugins;
+
+namespace WB.UI.Tester.Bootstrap
+{
+    public class DownloadCachePluginBootstrap
+        : MvxPluginBootstrapAction<Cirrious.MvvmCross.Plugins.DownloadCache.PluginLoader>
+    {
+    }
+}
