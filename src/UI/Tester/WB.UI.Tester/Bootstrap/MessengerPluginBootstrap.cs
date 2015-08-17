c5dfbd49b2b27d7aadf1ea07424ec3b4d21f676a
Slava Scherbak <slava.scherbak@gmail.com>
2015-08-17 18:20:39 -0400

    Interviewer, Tester: removed Messenger nuget package from UI projects. KP-5857

--- src/UI/Tester/WB.UI.Tester/Bootstrap/MessengerPluginBootstrap.cs
+++
@@ -1,9 +0,0 @@
-using Cirrious.CrossCore.Plugins;
-
-namespace WB.UI.Tester.Bootstrap
-{
-    public class MessengerPluginBootstrap
-        : MvxPluginBootstrapAction<Cirrious.MvvmCross.Plugins.Messenger.PluginLoader>
-    {
-    }
-}
