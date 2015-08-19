78eae319db40683aa71e9169a7c04124a89e0df8
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2015-08-19 11:10:15 +0300

    Minor code adjustments during code review

--- src/Tests/WB.Tests.Unit/SharedKernels/DataCollection/InterviewTests/when_synchronizing_from_hq_interview_which_existed_before.cs
+++ src/Tests/WB.Tests.Unit/SharedKernels/DataCollection/InterviewTests/when_synchronizing_from_hq_interview_which_existed_before.cs
@@ -1,18 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using Machine.Specifications;
 using Microsoft.Practices.ServiceLocation;
 using Moq;
-using Ncqrs.Spec;
 using WB.Core.SharedKernels.DataCollection.Aggregates;
-using WB.Core.SharedKernels.DataCollection.DataTransferObjects.Synchronization;
-using WB.Core.SharedKernels.DataCollection.Events.Interview;
 using WB.Core.SharedKernels.DataCollection.Exceptions;
 using WB.Core.SharedKernels.DataCollection.Implementation.Aggregates;
-using WB.Core.SharedKernels.DataCollection.Implementation.Providers;
 using WB.Core.SharedKernels.DataCollection.Repositories;
-using WB.Core.SharedKernels.DataCollection.V2;
-using WB.Core.SharedKernels.DataCollection.ValueObjects.Interview;
 using It = Machine.Specifications.It;
 
 namespace WB.Tests.Unit.SharedKernels.DataCollection.InterviewTests
@@ -38,8 +31,7 @@ namespace WB.Tests.Unit.SharedKernels.DataCollection.InterviewTests
 
         Because of = () => exception = Catch.Only<InterviewException>(() => interview.SynchronizeInterviewFromHeadquarters(interview.EventSourceId, Guid.NewGuid(), Guid.NewGuid(), Create.InterviewSynchronizationDto(), DateTime.Now));
 
-        It should_raise_InterviewException = () =>
-           exception.ShouldNotBeNull();
+        It should_throw_InterviewException = () => exception.ShouldNotBeNull();
 
         private static Interview interview;
         private static InterviewException exception;
