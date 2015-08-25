781c78eef31ad6f39b10bdee25963bab66dfda4c
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2015-08-25 11:41:23 -0400

    Sync: KP-5963 move interview details sync to separate job

---
+++ src/Core/SharedKernels/SurveyManagement/SurveyManagement/Synchronization/Schedulers/InterviewDetailsDataScheduler/InterviewDetailsBackgroundJob.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Practices.ServiceLocation;
+using Quartz;
+using WB.Core.Synchronization;
+
+namespace WB.Core.SharedKernels.SurveyManagement.Synchronization.Schedulers.InterviewDetailsDataScheduler
+{
+    [DisallowConcurrentExecution]
+    internal class InterviewDetailsBackgroundJob : IJob
+    {
+        ISyncPackagesProcessor SyncPackagesProcessor
+        {
+            get { return ServiceLocator.Current.GetInstance<ISyncPackagesProcessor>(); }
+        }
+
+        public void Execute(IJobExecutionContext context)
+        {
+            SyncPackagesProcessor.ProcessNextSyncPackage();
+        }
+    }
+}
