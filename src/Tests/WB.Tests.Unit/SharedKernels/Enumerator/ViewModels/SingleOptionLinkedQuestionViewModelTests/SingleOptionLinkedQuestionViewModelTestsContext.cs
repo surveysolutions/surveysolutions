867087197714c29e3a71ce8e2d2e28aec5bb0087
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2015-08-26 13:48:32 +0300

    Tests: corrected placement and namespaces of Inteviewer/Tester/Enumerator tests KP-5851

---
+++ src/Tests/WB.Tests.Unit/SharedKernels/Enumerator/ViewModels/SingleOptionLinkedQuestionViewModelTests/SingleOptionLinkedQuestionViewModelTestsContext.cs
@@ -0,0 +1,18 @@
+using System;
+using WB.Core.SharedKernels.Enumerator.Models.Questionnaire;
+using WB.Core.SharedKernels.Enumerator.Models.Questionnaire.Questions;
+
+namespace WB.Tests.Unit.SharedKernels.Enumerator.ViewModels.SingleOptionLinkedQuestionViewModelTests
+{
+    public class SingleOptionLinkedQuestionViewModelTestsContext
+    {
+        protected static QuestionnaireModel SetupQuestionnaireModelWithSingleOptionQuestionLinkedToTextQuestion(Guid questionId, Guid linkedToQuestionId)
+        {
+            return Create.QuestionnaireModel(questions: new BaseQuestionModel[]
+            {
+                new LinkedSingleOptionQuestionModel { Id = questionId, LinkedToQuestionId = linkedToQuestionId },
+                new TextQuestionModel { Id = linkedToQuestionId },
+            });
+        }
+    }
+}
