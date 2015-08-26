d4378509713c074fd36be1ae47f72c3d3df28f88
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2015-08-26 13:48:32 +0300

    Tests: corrected placement and namespaces of Inteviewer/Tester/Enumerator tests KP-5851

---
+++ src/Tests/WB.Tests.Unit/SharedKernels/Enumerator/ViewModels/IntegerQuestionViewModelTests/when_answering_roster_size_numeric_question_with_big_value_and_question_has_no_set_MaxValue.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using Machine.Specifications;
+using Moq;
+using WB.Core.Infrastructure.PlainStorage;
+using WB.Core.SharedKernels.DataCollection.Commands.Interview;
+using WB.Core.SharedKernels.Enumerator.Aggregates;
+using WB.Core.SharedKernels.Enumerator.Entities.Interview;
+using WB.Core.SharedKernels.Enumerator.Models.Questionnaire;
+using WB.Core.SharedKernels.Enumerator.Models.Questionnaire.Questions;
+using WB.Core.SharedKernels.Enumerator.Repositories;
+using WB.Core.SharedKernels.Enumerator.ViewModels.InterviewDetails.Questions;
+using It = Machine.Specifications.It;
+
+namespace WB.Tests.Unit.SharedKernels.Enumerator.ViewModels.IntegerQuestionViewModelTests
+{
+    public class when_answering_roster_size_numeric_question_with_big_value_and_question_has_no_set_MaxValue : IntegerQuestionViewModelTestContext
+    {
+        Establish context = () =>
+        {
+            SetUp();
+
+            var integerNumericAnswer = Mock.Of<IntegerNumericAnswer>(_ => _.IsAnswered == true && _.Answer == 1);
+
+            var interview = Mock.Of<IStatefulInterview>(_
+                => _.QuestionnaireId == questionnaireId
+                   && _.GetIntegerNumericAnswer(questionIdentity) == integerNumericAnswer);
+
+            var interviewRepository = Mock.Of<IStatefulInterviewRepository>(x => x.Get(interviewId) == interview);
+
+            var cascadingQuestionModel = Mock.Of<IntegerNumericQuestionModel>(_
+                => _.Id == questionIdentity.Id
+                   && _.IsRosterSizeQuestion == true);
+
+            var questionnaireModel = Mock.Of<QuestionnaireModel>(_ => _.Questions == new Dictionary<Guid, BaseQuestionModel> { { questionIdentity.Id, cascadingQuestionModel } });
+
+            var questionnaireRepository = Mock.Of<IPlainKeyValueStorage<QuestionnaireModel>>(x => x.GetById(questionnaireId) == questionnaireModel);
+
+            integerModel = CreateIntegerQuestionViewModel(
+                interviewRepository: interviewRepository,
+                questionnaireRepository: questionnaireRepository);
+
+            integerModel.Init(interviewId, questionIdentity, navigationState);
+        };
+
+        Because of = () =>
+        {
+            integerModel.AnswerAsString = "50";
+            integerModel.ValueChangeCommand.Execute();
+        };
+
+        It should_mark_question_as_invalid_with_message = () =>
+            ValidityModelMock.Verify(x => x.MarkAnswerAsNotSavedWithMessage("Answer '50' is incorrect because answer is greater than Roster upper bound '40'."), Times.Once);
+
+        It should_not_send_answer_command = () =>
+            AnsweringViewModelMock.Verify(x => x.SendAnswerQuestionCommandAsync(Moq.It.IsAny<AnswerNumericIntegerQuestionCommand>()), Times.Never);
+
+        It should_reset_AnswerAsString_to_previous_value = () =>
+            integerModel.AnswerAsString.ShouldEqual("50");
+
+        private static IntegerQuestionViewModel integerModel;
+    }
+}
---
+++ src/Tests/WB.Tests.Unit/SharedKernels/Enumerator/ViewModels/SectionsViewModelTests/when_section_get_disable_event.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Machine.Specifications;
+using NSubstitute;
+using WB.Core.SharedKernels.DataCollection;
+using WB.Core.SharedKernels.DataCollection.Events.Interview;
+using WB.Core.SharedKernels.Enumerator.Aggregates;
+using WB.Core.SharedKernels.Enumerator.Models.Questionnaire;
+using WB.Core.SharedKernels.Enumerator.ViewModels;
+using WB.Core.SharedKernels.Enumerator.ViewModels.InterviewDetails;
+using It = Machine.Specifications.It;
+
+
+namespace WB.Tests.Unit.SharedKernels.Enumerator.ViewModels.SectionsViewModelTests
+{
+    public class when_section_get_disable_event : SectionsViewModelTestContext
+    {
+        Establish context = () =>
+        {
+            sectionAId = Guid.Parse("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+            sectionBId = Guid.Parse("BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB");
+
+            var questionnaire = Create.QuestionnaireModel();
+            questionnaire.GroupsHierarchy = new List<GroupsHierarchyModel>
+            {
+                CreateGroupsHierarchyModel(sectionAId, "A"),
+                CreateGroupsHierarchyModel(sectionBId, "B")
+            };
+            questionnaire.GroupsWithFirstLevelChildrenAsReferences = new Dictionary<Guid, GroupModel>();
+            questionnaire.GroupsWithFirstLevelChildrenAsReferences[sectionAId] = new GroupModel { Id = sectionAId, Title = "A" };
+            questionnaire.GroupsWithFirstLevelChildrenAsReferences[sectionBId] = new GroupModel { Id = sectionBId, Title = "B" };
+
+            var interview = Substitute.For<IStatefulInterview>();
+            interview.IsEnabled(Moq.It.IsAny<Identity>()).ReturnsForAnyArgs(true);
+
+            viewModel = CreateSectionsViewModel(questionnaire, interview);
+            navigationState = Substitute.For<NavigationState>();
+            viewModel.Init("", "", navigationState);
+
+            firstSelectedSection = viewModel.Sections[0];
+            secondSection = viewModel.Sections[1];
+            secondSection.SectionIdentity = new Identity(sectionBId, Empty.RosterVector);
+        };
+
+        Because of = () =>
+        {
+            viewModel.Handle(new GroupsDisabled(new[] { new Identity(sectionBId, new decimal[0]) }));
+        };
+
+        It should_contains_only_one_section = () =>
+            viewModel.Sections.Count.ShouldEqual(1);
+
+        It should_contains_only_first_section = () =>
+            viewModel.Sections.Single().ShouldEqual(firstSelectedSection);
+
+
+
+        static SideBarSectionsViewModel viewModel;
+        static SideBarSectionViewModel firstSelectedSection;
+        static NavigationState navigationState;
+        static Guid sectionBId;
+        static Guid sectionAId;
+        static SideBarSectionViewModel secondSection;
+    }
+}
