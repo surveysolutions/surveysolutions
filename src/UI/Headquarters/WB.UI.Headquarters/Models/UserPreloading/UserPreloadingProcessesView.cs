e478d6ba0793645c41008a4229939fe178682d1e
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2015-09-02 14:55:09 -0400

    HQ: KP-5955 rename user batch upload to user preloading

--- src/UI/Headquarters/WB.UI.Headquarters/Models/UserPreloading/UserPreloadingProcessesView.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/Models/UserPreloading/UserPreloadingProcessesView.cs
@@ -1,12 +1,12 @@
 using System.Collections.Generic;
 
-namespace WB.UI.Headquarters.Models.UserBatchUpload
+namespace WB.UI.Headquarters.Models.UserPreloading
 {
-    public class UserBatchUploadProcessesView
+    public class UserPreloadingProcessesView
     {
         public int Page { get; set; }
         public int PageSize { get; set; }
         public int TotalCount { get; set; }
-        public IEnumerable<UserBatchUploadProcessView> Items { get; set; }
+        public IEnumerable<UserPreloadingProcessView> Items { get; set; }
     }
 }
