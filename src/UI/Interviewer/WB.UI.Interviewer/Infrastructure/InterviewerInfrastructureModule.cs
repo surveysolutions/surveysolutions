15922bb67cd24485f4834bf854b08c76c01554cc
Roman Lytvyn <litvin.roma@gmail.com>
2015-09-07 12:50:38 +0300

    IN/TESTER: shared async plain storage. KP-5745

--- src/UI/Interviewer/WB.UI.Interviewer/Infrastructure/InterviewerInfrastructureModule.cs
+++ src/UI/Interviewer/WB.UI.Interviewer/Infrastructure/InterviewerInfrastructureModule.cs
@@ -1,8 +1,14 @@
 ﻿using Ninject.Modules;
+using Sqo;
+using WB.Core.GenericSubdomains.Portable.Services;
 using WB.Core.SharedKernels.Enumerator;
+using WB.Core.SharedKernels.Enumerator.Implementation.Services;
 using WB.Core.SharedKernels.Enumerator.Services.Infrastructure;
+using WB.Core.SharedKernels.Enumerator.Services.Infrastructure.Storage;
+using WB.Infrastructure.Shared.Enumerator;
 using WB.UI.Interviewer.Infrastructure.Internals.Security;
-using WB.UI.Interviewer.Infrastructure.Internals.Settings;
+using WB.UI.Interviewer.Settings;
+using InterviewerSettings = WB.UI.Interviewer.Infrastructure.Internals.Settings.InterviewerSettings;
 
 namespace WB.UI.Interviewer.Infrastructure
 {
@@ -10,8 +16,14 @@ namespace WB.UI.Interviewer.Infrastructure
     {
         public override void Load()
         {
+            SiaqodbConfigurator.SetLicense(@"yrwPAibl/TwJ+pR5aBOoYieO0MbZ1HnEKEAwjcoqtdrUJVtXxorrxKZumV+Z48/Ffjj58P5pGVlYZ0G1EoPg0w==");
+            this.Bind<ISiaqodb>().ToConstant(new Siaqodb(AndroidPathUtils.GetPathToSubfolderInLocalDirectory("database")));
+
+            this.Bind(typeof(IAsyncPlainStorage<>)).To(typeof(SiaqodbPlainStorage<>)).InSingletonScope();
+
             this.Bind<IEnumeratorSettings>().To<InterviewerSettings>();
             this.Bind<IPrincipal>().To<InterviewerPrincipal>().InSingletonScope();
+            this.Bind<IRestServiceSettings>().To<RestServiceSettings>();
         }
     }
 }
