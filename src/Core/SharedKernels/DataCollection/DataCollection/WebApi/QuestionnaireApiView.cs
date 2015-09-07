e0459433890c8cca472623e98e2389d28a79122b
Roman Lytvyn <litvin.roma@gmail.com>
2015-09-07 13:12:58 +0300

    IN: changed synchronization service. KP-5745

---
+++ src/Core/SharedKernels/DataCollection/DataCollection/WebApi/QuestionnaireApiView.cs
@@ -0,0 +1,10 @@
+﻿using Main.Core.Documents;
+
+namespace WB.Core.SharedKernels.DataCollection.WebApi
+{
+    public class QuestionnaireApiView
+    {
+        public QuestionnaireDocument Document { get; set; }
+        public bool AllowCensus { get; set; }
+    }
+}
