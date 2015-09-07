3f9c3b37921229deded0dab58f03bdb5100a2119
Roman Lytvyn <litvin.roma@gmail.com>
2015-09-07 12:19:18 +0300

    IN: new synchronization. relink activity. KP-5745

---
+++ src/UI/Interviewer/WB.UI.Interviewer/Activities/RelinkDeviceActivity.cs
@@ -0,0 +1,26 @@
+using Android.App;
+using Android.OS;
+using Android.Support.V7.Widget;
+using Android.Views;
+using WB.Core.BoundedContexts.Interviewer.Views;
+using WB.UI.Shared.Enumerator.Activities;
+
+namespace WB.UI.Interviewer.Activities
+{
+    [Activity(WindowSoftInputMode = SoftInput.StateHidden, Theme = "@style/GrayAppTheme")]
+    public class RelinkDeviceActivity : BaseActivity<RelinkDeviceViewModel>
+    {
+        protected override int ViewResourceId
+        {
+            get { return Resource.Layout.relink; }
+        }
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            var toolbar = this.FindViewById<Toolbar>(Resource.Id.toolbar);
+            toolbar.Title = "";
+            this.SetSupportActionBar(toolbar);
+        }
+    }
+}
