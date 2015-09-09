24698c34fbf8d17fd23f3a20474a354ea23fb8a4
Roman Lytvyn <litvin.roma@gmail.com>
2015-09-09 14:00:42 +0300

    SV: fixed bug with showed supervior in interviewer assigned autocomplete textbox(interviews page) KP-6024

--- src/Core/SharedKernels/SurveyManagement/SurveyManagement/Views/User/ITeamViewFactory.cs
+++ src/Core/SharedKernels/SurveyManagement/SurveyManagement/Views/User/ITeamViewFactory.cs
@@ -5,7 +5,7 @@ namespace WB.Core.SharedKernels.SurveyManagement.Views.User
     public interface ITeamViewFactory
     {
         UsersView GetAssigneeSupervisorsAndDependentInterviewers(int pageSize, string searchBy);
-        UsersView GetSupervisorAndDependentInterviewers(int pageSize, string searchBy, Guid supervisorId);
+        UsersView GetInterviewers(int pageSize, string searchBy, Guid supervisorId);
         UsersView GetAllSupervisors(int pageSize, string searchBy);
         UsersView GetAsigneeInterviewersBySupervisor(int pageSize, string searchBy, Guid supervisorId);
     }
