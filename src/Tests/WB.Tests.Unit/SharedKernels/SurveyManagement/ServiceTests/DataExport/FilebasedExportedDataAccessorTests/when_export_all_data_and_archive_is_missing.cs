01da36cca26dee52e76fbf4f645ef52c3f72874a
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2015-09-10 13:13:18 -0400

    DataExport: KP-6064 remove dta files from exported data

--- src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/ServiceTests/DataExport/FilebasedExportedDataAccessorTests/when_export_all_data_and_archive_is_missing.cs
+++ src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/ServiceTests/DataExport/FilebasedExportedDataAccessorTests/when_export_all_data_and_archive_is_missing.cs
@@ -20,10 +20,12 @@ namespace WB.Tests.Unit.SharedKernels.SurveyManagement.ServiceTests.DataExport.F
         };
 
         Because of = () =>
-            archiveName= filebasedExportedDataAccessor.GetFilePathToExportedCompressedData(questionnaireId,questionnaireVersion, ExportDataType.Tab);
+            archiveName =
+                filebasedExportedDataAccessor.GetFilePathToExportedCompressedData(questionnaireId, questionnaireVersion,
+                    ExportDataType.Tab);
 
         It should_archive_name_contain_questionnaire_id_and_version = () =>
-            archiveName.ShouldContain("exported_data_11111111-1111-1111-1111-111111111111_3");
+            archiveName.ShouldContain("exported_data_11111111-1111-1111-1111-111111111111_3_Tab_All");
 
         It should_archive_contain_data_files_and_environment_files = () =>
             addedFiles.ShouldEqual(new[] { "f1", "f2", "e1", "e2" });
