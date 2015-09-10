219df81c36f8cad93aa9a5a25122a62919445f8f
Pavel Stovpivskyi <sby@rambler.ru>
2015-09-10 14:37:37 +0300

    Refactoring

--- src/Core/BoundedContexts/Interviewer/WB.Core.BoundedContexts.Interviewer/Views/Dashboard/DashboardInformation.cs
+++ src/Core/BoundedContexts/Interviewer/WB.Core.BoundedContexts.Interviewer/Views/Dashboard/DashboardInformation.cs
@@ -4,11 +4,9 @@ namespace WB.Core.BoundedContexts.Interviewer.Views.Dashboard
 {
     public class DashboardInformation
     {
-         public int NewInterviewsCount {get; set; }
-         public int StartedInterviewsCount { get; set; }
-         public int CompletedInterviewsCount { get; set; }
-         public int RejectedInterviewsCount { get; set; }
-
-         public List<IDashboardItem> DashboardItems { get; set; }
+         public List<IDashboardItem> NewInterviews { get; set; }
+         public List<IDashboardItem> StartedInterviews { get; set; }
+         public List<IDashboardItem> CompletedInterviews { get; set; }
+         public List<IDashboardItem> RejectedInterviews { get; set; }
     }
 }
