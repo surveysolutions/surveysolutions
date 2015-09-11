56804a17c56d35635aa71431f93b3bec5432b776
Pavel Stovpivskyi <sby@rambler.ru>
2015-09-11 19:14:25 +0300

    Add stus binding and converter

---
+++ src/UI/Interviewer/WB.UI.Interviewer/Converters/InterviewStatusToColorConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using Cirrious.CrossCore.Converters;
+using WB.Core.BoundedContexts.Interviewer.Views.Dashboard;
+using WB.Core.GenericSubdomains.Portable;
+
+
+namespace WB.UI.Interviewer.Converters
+{
+    public class InterviewStatusToColorConverter : MvxValueConverter<DashboardInterviewCategories, int>
+    {
+        protected override int Convert(DashboardInterviewCategories status, Type targetType, object parameter, CultureInfo culture)
+        {
+            switch (status)
+            {
+                case DashboardInterviewCategories.New:
+                    return Resource.Color.dashboard_new_interview_status;
+
+                case DashboardInterviewCategories.InProgress:
+                    return Resource.Color.dashboard_in_progress_tab;
+
+                case DashboardInterviewCategories.Complited:
+                    return Resource.Color.dashboard_complited_tab;
+
+                case DashboardInterviewCategories.Rejected:
+                    return Resource.Color.dashboard_rejected_tab;
+            }
+
+            throw new ArgumentException("status is unknown - {0}".FormatString(status));
+        }
+    }
+}
---
+++ src/UI/Interviewer/WB.UI.Interviewer/CustomBindings/TextViewTextColorBinding.cs
@@ -0,0 +1,32 @@
+﻿using Android.Graphics;
+using Android.Views;
+using Android.Widget;
+using Cirrious.MvvmCross.Binding;
+using WB.UI.Shared.Enumerator.CustomBindings;
+
+namespace WB.UI.Interviewer.CustomBindings
+{
+    public class TextViewTextColorBinding : BaseBinding<TextView, int?>
+    {
+        public TextViewTextColorBinding(TextView androidControl) : base(androidControl) { }
+
+        protected override void SetValueToView(TextView control, int? colorId)
+        {
+            this.SetBackgroundDrawable(control, colorId);
+        }
+
+        public override MvxBindingMode DefaultMode
+        {
+            get { return MvxBindingMode.OneWay; }
+        }
+
+        private void SetBackgroundDrawable(TextView androidControl, int? colorId)
+        {
+            if (colorId.HasValue)
+            {
+                Color color = androidControl.Resources.GetColor(colorId.Value);
+                androidControl.SetTextColor(color); 
+            }
+        }
+    }
+}
