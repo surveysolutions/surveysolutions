c666734a78684f5781b5311f5207435f32811654
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2015-09-14 19:31:03 -0400

    DataCollection: KP-6060 update answering model

--- src/Core/SharedKernels/DataCollection/DataCollection/Commands/Interview/RemoveAnswerCommand.cs
+++ src/Core/SharedKernels/DataCollection/DataCollection/Commands/Interview/RemoveAnswerCommand.cs
@@ -3,11 +3,13 @@ using WB.Core.SharedKernels.DataCollection.Commands.Interview.Base;
 
 namespace WB.Core.SharedKernels.DataCollection.Commands.Interview
 {
-    public class RemoveAnswerCommand : AnswerQuestionCommand
+    public class RemoveAnswerCommand : QuestionCommand
     {
+        public DateTime RemoveTime { get; private set; }
         public RemoveAnswerCommand(Guid interviewId, Guid userId, Guid questionId, decimal[] rosterVector,
-            DateTime answerTime) : base(interviewId, userId, questionId, rosterVector, answerTime)
+            DateTime removeTime) : base(interviewId, userId, questionId, rosterVector)
         {
+            this.RemoveTime = removeTime;
         }
     }
 }
