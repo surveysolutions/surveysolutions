230f237c698c74db86da1a6dbabcc38c666ab056
Slava Scherbak <slava.scherbak@gmail.com>
2015-09-14 13:40:34 -0400

    Interviewer\Login: added validation and error messages on UI. Backend show only exception message, not friendly error messages. KP-5608

---
+++ src/UI/Interviewer/WB.UI.Interviewer/Converters/TextEditValidationStyleBackgroundConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+using Cirrious.CrossCore.Converters;
+
+namespace WB.UI.Interviewer.Converters
+{
+    public class TextEditValidationStyleBackgroundConverter : MvxValueConverter<bool, int>
+    {
+        protected override int Convert(bool value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value)
+            {
+                return Resource.Drawable.textedit_valid;
+            }
+            return Resource.Drawable.textedit_invalid;
+        }
+    }
+}
