a27c326e977e403bdaef690f5638f6be05c69836
Roman Lytvyn <litvin.roma@gmail.com>
2015-09-15 19:07:55 +0300

    SV: fixed synchronization for separated HQ/SV. KP-5745

--- src/UI/Supervisor/WB.UI.Supervisor/App_Start/WebApiConfig.cs
+++ src/UI/Supervisor/WB.UI.Supervisor/App_Start/WebApiConfig.cs
@@ -20,7 +20,21 @@ namespace WB.UI.Supervisor.App_Start
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional });
-            
+
+            config.Routes.MapHttpRoute("InterviewerApplicationApi", "api/interviewer/{apiVersion}/{action}", new { controller = "interviewer" });
+
+            config.Routes.MapHttpRoute("InterviewerUsersApiWithAction", "api/interviewer/{apiVersion}/users/{action}/{id}", new { controller = "InterviewerUsers", id = RouteParameter.Optional });
+            config.Routes.MapHttpRoute("InterviewerUsersApi", "api/interviewer/{apiVersion}/users/{id}", new { controller = "InterviewerUsers", id = RouteParameter.Optional });
+
+            config.Routes.MapHttpRoute("InterviewerDevicesApiWithAction", "api/interviewer/{apiVersion}/devices/{action}/{id}", new { controller = "InterviewerDevices", id = RouteParameter.Optional });
+            config.Routes.MapHttpRoute("InterviewerDevicesApi", "api/interviewer/{apiVersion}/devices/{id}", new { controller = "InterviewerDevices", id = RouteParameter.Optional });
+
+            config.Routes.MapHttpRoute("InterviewerQuestionnairesApiWithAction", "api/interviewer/{apiVersion}/questionnaires/{action}/{id}", new { controller = "InterviewerQuestionnaires", id = RouteParameter.Optional });
+            config.Routes.MapHttpRoute("InterviewerQuestionnairesApi", "api/interviewer/{apiVersion}/questionnaires/{id}", new { controller = "InterviewerQuestionnaires", id = RouteParameter.Optional });
+
+            config.Routes.MapHttpRoute("InterviewerInterviewsApiWithAction", "api/interviewer/{apiVersion}/interviews/{action}/{id}", new { controller = "InterviewerInterviews", id = RouteParameter.Optional });
+            config.Routes.MapHttpRoute("InterviewerInterviewsApi", "api/interviewer/{apiVersion}/interviews/{id}", new { controller = "InterviewerInterviews", id = RouteParameter.Optional });
+
             //support json for browser requests
             config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
 
