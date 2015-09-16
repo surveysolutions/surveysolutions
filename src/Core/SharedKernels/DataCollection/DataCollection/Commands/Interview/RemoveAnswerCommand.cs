8eca52612ccea90b4ef87ce634d38565c23ff0ce
Andrii Kozhyn <hedgic@gmail.com>
2015-09-16 11:13:33 +0300

    KP-6061: Remove answer from qrbarcode question implemented

--- src/Core/SharedKernels/DataCollection/DataCollection/Commands/Interview/RemoveAnswerCommand.cs
+++ src/Core/SharedKernels/DataCollection/DataCollection/Commands/Interview/RemoveAnswerCommand.cs
@@ -6,8 +6,11 @@ namespace WB.Core.SharedKernels.DataCollection.Commands.Interview
     public class RemoveAnswerCommand : QuestionCommand
     {
         public DateTime RemoveTime { get; private set; }
-        public RemoveAnswerCommand(Guid interviewId, Guid userId, Guid questionId, decimal[] rosterVector,
-            DateTime removeTime) : base(interviewId, userId, questionId, rosterVector)
+
+        public RemoveAnswerCommand(Guid interviewId,
+            Guid userId,
+            Identity questionId,
+            DateTime removeTime) : base(interviewId, userId, questionId.Id, questionId.RosterVector)
         {
             this.RemoveTime = removeTime;
         }
