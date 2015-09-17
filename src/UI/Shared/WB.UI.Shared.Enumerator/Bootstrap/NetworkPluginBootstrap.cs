ecc55206979b3e257439693da8174e574b20676b
Roman Lytvyn <litvin.roma@gmail.com>
2015-09-17 15:57:10 +0300

    IN: extended handling of rest exceptions. fixed solution and build. KP-5745

---
+++ src/UI/Shared/WB.UI.Shared.Enumerator/Bootstrap/NetworkPluginBootstrap.cs
@@ -0,0 +1,9 @@
+using Cirrious.CrossCore.Plugins;
+
+namespace WB.UI.Shared.Enumerator.Bootstrap
+{
+    public class NetworkPluginBootstrap
+        : MvxPluginBootstrapAction<Cirrious.MvvmCross.Plugins.Network.PluginLoader>
+    {
+    }
+}
