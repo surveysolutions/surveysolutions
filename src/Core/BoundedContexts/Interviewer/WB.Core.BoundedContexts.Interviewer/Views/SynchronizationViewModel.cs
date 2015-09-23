06eaf464014722e656875a34a52102065b66b428
Roman Lytvyn <litvin.roma@gmail.com>
2015-09-23 22:16:43 +0300

    IN: fixed double cancelling of synchronization. KP-5743

--- src/Core/BoundedContexts/Interviewer/WB.Core.BoundedContexts.Interviewer/Views/SynchronizationViewModel.cs
+++ src/Core/BoundedContexts/Interviewer/WB.Core.BoundedContexts.Interviewer/Views/SynchronizationViewModel.cs
@@ -464,7 +464,8 @@ namespace WB.Core.BoundedContexts.Interviewer.Views
 
         public void CancelSynchronizaion()
         {
-            this.synchronizationCancellationTokenSource.Cancel();
+            if (!this.synchronizationCancellationTokenSource.IsCancellationRequested)
+                this.synchronizationCancellationTokenSource.Cancel();
         }    
     }
 }
