162c7f734c75d4ec0e5614b5f5a5f441a6441b65
Pavel Stovpivskyi <sby@rambler.ru>
2015-09-24 13:34:16 +0300

    renamed eventSourceId to aggregateRootId

--- src/Core/Infrastructure/WB.Core.Infrastructure/EventBus/Lite/ILiteEventRegistry.cs
+++ src/Core/Infrastructure/WB.Core.Infrastructure/EventBus/Lite/ILiteEventRegistry.cs
@@ -7,9 +7,9 @@ namespace WB.Core.Infrastructure.EventBus.Lite
 {
     public interface ILiteEventRegistry
     {
-        void Subscribe(ILiteEventHandler handler, string eventSourceId);
+        void Subscribe(ILiteEventHandler handler, string aggregateRootId);
 
-        void Unsubscribe(ILiteEventHandler handler, string eventSourceId);
+        void Unsubscribe(ILiteEventHandler handler, string aggregateRootId);
 
         IEnumerable<Action<object>> GetHandlers(UncommittedEvent @event);
     }
