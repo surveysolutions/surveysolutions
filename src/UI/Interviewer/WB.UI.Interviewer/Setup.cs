b10b06af2ebf03e01700179a06eaff37ba2d7281
Pavel Stovpivskyi <sby@rambler.ru>
2015-09-29 16:30:11 +0300

    Refactoring Markup of dashboard item

--- src/UI/Interviewer/WB.UI.Interviewer/Setup.cs
+++ src/UI/Interviewer/WB.UI.Interviewer/Setup.cs
@@ -88,7 +88,6 @@ namespace WB.UI.Interviewer
                 // Add assemblies with other views we use.  When the XML is inflated
                 // MvvmCross knows about the types and won't complain about them.  This
                 // speeds up inflation noticeably.
-                toReturn.Add(typeof(CardView).Assembly);
                 return toReturn;
             }
         }
