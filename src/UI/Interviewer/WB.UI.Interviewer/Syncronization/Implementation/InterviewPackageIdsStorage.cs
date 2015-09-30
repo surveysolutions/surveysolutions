ea132b3a480e5c12f8605efdd87cc4fa63801692
Roman Lytvyn <litvin.roma@gmail.com>
2015-09-30 10:28:13 +0300

    IN: changes by code review comments. KP-5745

--- src/UI/Interviewer/WB.UI.Interviewer/Syncronization/Implementation/InterviewPackageIdsStorage.cs
+++ src/UI/Interviewer/WB.UI.Interviewer/Syncronization/Implementation/InterviewPackageIdsStorage.cs
@@ -8,18 +8,6 @@ using WB.Core.SharedKernels.Enumerator.Services.Infrastructure;
 
 namespace WB.UI.Interviewer.Syncronization.Implementation
 {
-    public class InterviewPackageIdsStorageSettings
-    {
-        public InterviewPackageIdsStorageSettings(string pathToDatabase, string databaseName)
-        {
-            this.PathToDatabase = pathToDatabase;
-            this.DatabaseName = databaseName;
-        }
-
-        public string PathToDatabase { get; private set; }
-        public string DatabaseName { get; private set; }
-    }
-
     public class InterviewPackageIdsStorage : IInterviewPackageIdsStorage, IBackupable
     {
         private readonly IFileSystemAccessor fileSystemAccessor;
