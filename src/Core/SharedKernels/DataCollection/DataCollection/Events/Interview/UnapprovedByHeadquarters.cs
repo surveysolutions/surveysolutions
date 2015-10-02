6d16934c39be56fa32ff8bb23a37b7a532c2fa94
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2015-10-02 14:32:37 -0400

    KP-6173 Add option for HQ to Un-approve interview

--- src/Core/SharedKernels/DataCollection/DataCollection/Events/Interview/UnapprovedByHeadquarters.cs
+++ src/Core/SharedKernels/DataCollection/DataCollection/Events/Interview/UnapprovedByHeadquarters.cs
@@ -3,11 +3,11 @@ using WB.Core.SharedKernels.DataCollection.Events.Interview.Base;
 
 namespace WB.Core.SharedKernels.DataCollection.Events.Interview
 {
-    public class InterviewApprovedByHQRevoked : InterviewActiveEvent
+    public class UnapprovedByHeadquarters : InterviewActiveEvent
     {
         public string Comment { get; private set; }
 
-        public InterviewApprovedByHQRevoked(Guid userId, string comment)
+        public UnapprovedByHeadquarters(Guid userId, string comment)
             : base(userId)
         {
             this.Comment = comment;
