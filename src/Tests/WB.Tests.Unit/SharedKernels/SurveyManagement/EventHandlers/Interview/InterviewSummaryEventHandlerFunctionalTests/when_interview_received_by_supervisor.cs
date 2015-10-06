114f1e945526485ef85adbb8fa08e7bf85862c32
Andrii Kozhyn <hedgic@gmail.com>
2015-10-06 16:28:59 +0300

    KP-6207: Display warning when reassigning interviews that are received by interviewer

--- src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/EventHandlers/Interview/InterviewSummaryEventHandlerFunctionalTests/when_interview_received_by_supervisor.cs
+++ src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/EventHandlers/Interview/InterviewSummaryEventHandlerFunctionalTests/when_interview_received_by_supervisor.cs
@@ -1,21 +1,21 @@
-﻿using Machine.Specifications;
-using WB.Core.SharedKernels.DataCollection.ValueObjects.Interview;
+using Machine.Specifications;
 using WB.Core.SharedKernels.SurveyManagement.EventHandler;
 using WB.Core.SharedKernels.SurveyManagement.Views.Interview;
 
 namespace WB.Tests.Unit.SharedKernels.SurveyManagement.EventHandlers.Interview.InterviewSummaryEventHandlerFunctionalTests
 {
-    internal class when_interview_received_by_interviewer : InterviewSummaryDenormalizerTestsContext
+    internal class when_interview_received_by_supervisor : InterviewSummaryDenormalizerTestsContext
     {
         Establish context = () =>
         {
             viewModel = Create.InterviewSummary();
+            viewModel.ReceivedByInterviewer = true;
             denormalizer = CreateDenormalizer();
         };
 
-        Because of = () => updatedModel = denormalizer.Update(viewModel, Create.Event.InterviewReceivedByInterviewer().ToPublishedEvent());
+        Because of = () => updatedModel = denormalizer.Update(viewModel, Create.Event.InterviewReceivedBySupervisor().ToPublishedEvent());
 
-        It should_mark_summary_as_received_by_interviewer = () => updatedModel.ReceivedByInterviewer.ShouldBeTrue();
+        It should_mark_summary_as_received_by_interviewer = () => updatedModel.ReceivedByInterviewer.ShouldBeFalse();
 
         static InterviewSummary viewModel;
         static InterviewSummaryDenormalizer denormalizer;
