660e87eb0bebc95d4d14d9684cffd569443a34d7
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2015-10-15 17:51:20 -0400

    HQ\UserBatchUpload: KP-5952 fix delete of a user batch upload process when the precess is in the verification state

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/UserPreloading/Services/UserPreloadingCleaner.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/UserPreloading/Services/UserPreloadingCleaner.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using Microsoft.Practices.ServiceLocation;
+using WB.Core.BoundedContexts.Headquarters.UserPreloading.Dto;
 using WB.Core.BoundedContexts.Headquarters.UserPreloading.Tasks;
 using WB.Core.Infrastructure.PlainStorage;
 using WB.Core.Infrastructure.Transactions;
@@ -9,30 +10,39 @@ namespace WB.Core.BoundedContexts.Headquarters.UserPreloading.Services
 {
     internal class UserPreloadingCleaner : IUserPreloadingCleaner
     {
-        private readonly IUserPreloadingService userPreloadingService;
+        private readonly IPlainStorageAccessor<UserPreloadingProcess> userPreloadingProcessStorage;
 
         private readonly UserPreloadingSettings userPreloadingSettings;
 
         private readonly IPlainTransactionManager plainTransactionManager;
 
-        public UserPreloadingCleaner(IUserPreloadingService userPreloadingService, IPlainTransactionManager plainTransactionManager, UserPreloadingSettings userPreloadingSettings)
+        public UserPreloadingCleaner(
+            IPlainTransactionManager plainTransactionManager, 
+            UserPreloadingSettings userPreloadingSettings, 
+            IPlainStorageAccessor<UserPreloadingProcess> userPreloadingProcessStorage)
         {
-            this.userPreloadingService = userPreloadingService;
             this.plainTransactionManager = plainTransactionManager;
             this.userPreloadingSettings = userPreloadingSettings;
+            this.userPreloadingProcessStorage = userPreloadingProcessStorage;
         }
 
         public void CleanUpInactiveUserPreloadingProcesses()
         {
             var processeIdsToClean =
-                this.plainTransactionManager.ExecuteInPlainTransaction(() => userPreloadingService.GetPreloadingProcesses()
-                    .Where(p => p.LastUpdateDate < DateTime.Now.AddDays(-userPreloadingSettings.HowOldInDaysProcessShouldBeInOrderToBeCleaned)).Select(p=>p.UserPreloadingProcessId)
-                    .ToArray());
+                this.plainTransactionManager.ExecuteInPlainTransaction(
+                    () => userPreloadingProcessStorage.Query(_ => _.OrderBy(p => p.LastUpdateDate)
+                        .Where(
+                            p =>
+                                p.LastUpdateDate <
+                                DateTime.Now.AddDays(
+                                    -userPreloadingSettings.HowOldInDaysProcessShouldBeInOrderToBeCleaned))
+                        .Select(p => p.UserPreloadingProcessId)
+                        .ToArray()));
 
             foreach (var processeIdToClean in processeIdsToClean)
             {
                 this.plainTransactionManager.ExecuteInPlainTransaction(
-                    () => userPreloadingService.DeletePreloadingProcess(processeIdToClean));
+                    () => userPreloadingProcessStorage.Remove(processeIdToClean));
             }
         }
     }
