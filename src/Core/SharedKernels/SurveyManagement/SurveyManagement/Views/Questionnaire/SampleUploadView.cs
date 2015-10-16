f319387ca395704205b3eaad7261e0d0a3e6b4dd
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2015-10-16 14:19:05 -0400

    HQ\SampleUpload: KP-6238 fix sampleupload of gps questions

---
+++ src/Core/SharedKernels/SurveyManagement/SurveyManagement/Views/Questionnaire/SampleUploadView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using WB.Core.SharedKernels.DataCollection.Views.Questionnaire;
+
+namespace WB.Core.SharedKernels.SurveyManagement.Views.Questionnaire
+{
+    public class SampleUploadView
+    {
+        public SampleUploadView(Guid questionnaireId, long questionnaireVersion, List<FeaturedQuestionItem> columnListToPreload)
+        {
+            this.QuestionnaireId = questionnaireId;
+            this.QuestionnaireVersion = questionnaireVersion;
+            this.ColumnListToPreload = columnListToPreload;
+        }
+
+        public Guid QuestionnaireId { get; private set; }
+        public long QuestionnaireVersion { get; private set; }
+        public List<FeaturedQuestionItem> ColumnListToPreload { get; private set; }
+    }
+}
