137858c6c534f12816950a73970c16c921736766
Anastasya Kholodova <nastya.kholodova@gmail.com>
2015-10-16 14:19:05 -0400

    HQ\SampleUpload: KP-6238 fix sampleupload of gps questions

---
+++ src/Core/SharedKernels/SurveyManagement/SurveyManagement/Views/Questionnaire/SampleUploadViewFactory.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NHibernate.Mapping;
+using WB.Core.GenericSubdomains.Portable;
+using WB.Core.Infrastructure.ReadSide;
+using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
+using WB.Core.SharedKernels.DataCollection.Views.Questionnaire;
+using WB.Core.SharedKernels.SurveyManagement.Views.DataExport;
+
+namespace WB.Core.SharedKernels.SurveyManagement.Views.Questionnaire
+{
+    public class SampleUploadViewFactory : IViewFactory<SampleUploadViewInputModel, SampleUploadView>
+    {
+        private readonly IReadSideRepositoryReader<QuestionnaireBrowseItem> questionnaires;
+        private readonly IReadSideKeyValueStorage<QuestionnaireExportStructure> questionnaireExportStructureStorage;
+
+        public SampleUploadViewFactory(IReadSideRepositoryReader<QuestionnaireBrowseItem> questionnaires, IReadSideKeyValueStorage<QuestionnaireExportStructure> questionnaireExportStructureStorage)
+        {
+            this.questionnaires = questionnaires;
+            this.questionnaireExportStructureStorage = questionnaireExportStructureStorage;
+        }
+
+        public SampleUploadView Load(SampleUploadViewInputModel input)
+        {
+            var questionnaire = this.questionnaires.AsVersioned().Get(input.QuestionnaireId.FormatGuid(), input.Version);
+            if (questionnaire == null)
+                return null;
+
+            var questionnaireExportStructure = this.questionnaireExportStructureStorage.AsVersioned().Get(input.QuestionnaireId.FormatGuid(), input.Version);
+            if (questionnaireExportStructure == null)
+                return null;
+
+            var topLevelOfQuestionnaire =
+                questionnaireExportStructure.HeaderToLevelMap.Values.FirstOrDefault(
+                    level => level.LevelScopeVector.Count == 0);
+            if (topLevelOfQuestionnaire == null)
+                return null;
+
+            var columnListToPreload = new List<FeaturedQuestionItem>();
+            
+            foreach (var featuredQuestionItem in questionnaire.FeaturedQuestions)
+            {
+                if(!topLevelOfQuestionnaire.HeaderItems.ContainsKey(featuredQuestionItem.Id))
+                    continue;
+
+                var questionExportedColumn = topLevelOfQuestionnaire.HeaderItems[featuredQuestionItem.Id];
+                foreach (var columnName in questionExportedColumn.ColumnNames)
+                {
+                    columnListToPreload.Add(new FeaturedQuestionItem(featuredQuestionItem.Id, featuredQuestionItem.Title,
+                        columnName));
+                }
+            }
+            return new SampleUploadView(input.QuestionnaireId, input.Version, columnListToPreload);
+        }
+    }
+}
