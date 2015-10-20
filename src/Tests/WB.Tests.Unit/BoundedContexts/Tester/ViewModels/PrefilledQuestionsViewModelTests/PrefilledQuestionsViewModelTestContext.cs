bcc6ec0a7ed39a1709edd8cfd8ddae97b663d234
Pavel Stovpivskyi <630198+spuv@users.noreply.github.com>
2015-10-20 10:30:49 -0400

    Added test
    KP-6130

---
+++ src/Tests/WB.Tests.Unit/BoundedContexts/Tester/ViewModels/PrefilledQuestionsViewModelTests/PrefilledQuestionsViewModelTestContext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Cirrious.MvvmCross.Test.Core;
+using Moq;
+using WB.Core.BoundedContexts.Tester.Implementation.Services;
+using WB.Core.BoundedContexts.Tester.Services;
+using WB.Core.BoundedContexts.Tester.Views;
+using WB.Core.GenericSubdomains.Portable.Services;
+using WB.Core.Infrastructure.CommandBus;
+using WB.Core.Infrastructure.PlainStorage;
+using WB.Core.SharedKernels.Enumerator.Models.Questionnaire;
+using WB.Core.SharedKernels.Enumerator.Repositories;
+using WB.Core.SharedKernels.Enumerator.Services;
+using WB.Core.SharedKernels.Enumerator.Services.Infrastructure;
+using WB.Core.SharedKernels.Enumerator.Services.Infrastructure.Storage;
+using WB.Core.SharedKernels.Enumerator.ViewModels;
+
+namespace WB.Tests.Unit.BoundedContexts.Tester.ViewModels.PrefilledQuestionsViewModelTests
+{
+    internal class PrefilledQuestionsViewModelTestContext : MvxIoCSupportingTest
+    {
+        public PrefilledQuestionsViewModelTestContext()
+        {
+            base.Setup();
+        }
+
+        public static PrefilledQuestionsViewModel CreatePrefilledQuestionsViewModel(
+            IInterviewViewModelFactory interviewViewModelFactory = null,
+            IPlainKeyValueStorage<QuestionnaireModel> plainQuestionnaireRepository = null,
+            IStatefulInterviewRepository interviewRepository = null,
+            IViewModelNavigationService viewModelNavigationService = null,
+            ILogger logger = null)
+        {
+            return new PrefilledQuestionsViewModel(interviewViewModelFactory: interviewViewModelFactory ?? Mock.Of<IInterviewViewModelFactory>(),
+                plainQuestionnaireRepository: plainQuestionnaireRepository ?? Mock.Of<IPlainKeyValueStorage<QuestionnaireModel>>(),
+                interviewRepository: interviewRepository ?? Mock.Of<IStatefulInterviewRepository>(),
+                viewModelNavigationService: viewModelNavigationService ?? Mock.Of<IViewModelNavigationService>(),
+                logger: logger ?? Mock.Of<ILogger>());
+        }
+    }
+}
