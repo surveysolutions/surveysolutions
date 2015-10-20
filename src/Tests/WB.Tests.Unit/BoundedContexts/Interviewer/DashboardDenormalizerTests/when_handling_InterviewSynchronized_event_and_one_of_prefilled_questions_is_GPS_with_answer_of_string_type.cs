7155d9e32980926f90e99508581b82911a919a1e
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2015-10-20 16:30:28 -0400

    DashboardDenormalized: covered prefilled GPS answer deserialization with tests KP-6231

--- src/Tests/WB.Tests.Unit/BoundedContexts/Interviewer/DashboardDenormalizerTests/when_handling_InterviewSynchronized_event_and_one_of_prefilled_questions_is_GPS_with_answer_of_string_type.cs
+++ src/Tests/WB.Tests.Unit/BoundedContexts/Interviewer/DashboardDenormalizerTests/when_handling_InterviewSynchronized_event_and_one_of_prefilled_questions_is_GPS_with_answer_of_string_type.cs
@@ -5,7 +5,6 @@ using Ncqrs.Eventing.ServiceModel.Bus;
 using WB.Core.BoundedContexts.Interviewer.Views.Dashboard.OldDashboardCapability;
 using WB.Core.GenericSubdomains.Portable;
 using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
-using WB.Core.SharedKernels.DataCollection.DataTransferObjects.Synchronization;
 using WB.Core.SharedKernels.DataCollection.Events.Interview;
 using WB.Core.SharedKernels.DataCollection.Views.Questionnaire;
 using WB.UI.Interviewer.ViewModel.Dashboard;
@@ -14,14 +13,18 @@ using it = Moq.It;
 
 namespace WB.Tests.Unit.BoundedContexts.Interviewer.DashboardDenormalizerTests
 {
-    internal class when_handling_InterviewOnClientCreated_event_and_one_of_prefilled_questions_is_GPS
+    internal class when_handling_InterviewSynchronized_event_and_one_of_prefilled_questions_is_GPS_with_answer_of_string_type
     {
         Establish context = () =>
         {
+            dashboardItem = Create.QuestionnaireDTO();
+
             @event = Create.Event
-                .InterviewOnClientCreated(
-                    questionnaireId: Guid.Parse("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"),
-                    questionnaireVersion: 33)
+                .InterviewSynchronized(
+                    Create.InterviewSynchronizationDto(
+                        questionnaireId: Guid.Parse("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"),
+                        questionnaireVersion: 33,
+                        answers: new[] { Create.AnsweredQuestionSynchronizationDto(questionId: prefilledGpsQuestionId, answer: stringGpsAnswer) }))
                 .ToPublishedEvent(
                     eventSourceId: interviewId);
 
@@ -35,11 +38,14 @@ namespace WB.Tests.Unit.BoundedContexts.Interviewer.DashboardDenormalizerTests
                 Mock.Of<IReadSideKeyValueStorage<QuestionnaireDocumentVersioned>>(storage
                     => storage.GetById("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa$33") == versionedQuestionnaire);
 
-            var questionnaireDtoDocumentStorage = Mock.Of<IReadSideRepositoryWriter<QuestionnaireDTO>>();
+            var questionnaireDtoDocumentStorage = Mock.Of<IReadSideRepositoryWriter<QuestionnaireDTO>>(writer
+                => writer.GetById(it.IsAny<string>()) == dashboardItem);
 
             Mock.Get(questionnaireDtoDocumentStorage)
                 .Setup(storage => storage.Store(it.IsAny<QuestionnaireDTO>(), interviewId.FormatGuid()))
                 .Callback<QuestionnaireDTO, string>((view, id) => dashboardItem = view);
+
+            denormalizer = Create.DashboardDenormalizer(questionnaireDtoDocumentStorage: questionnaireDtoDocumentStorage);
 
             denormalizer = Create.DashboardDenormalizer(
                 questionnaireStorage: questionnaireStorage,
@@ -52,10 +58,19 @@ namespace WB.Tests.Unit.BoundedContexts.Interviewer.DashboardDenormalizerTests
         It should_store_prefilled_GPS_question_ID_to_result_dashboard_item = () =>
             dashboardItem.GpsLocationQuestionId.ShouldEqual(prefilledGpsQuestionId.FormatGuid());
 
+        It should_store_latitude_from_answer_to_prefilled_GPS_question_to_result_dashboard_item = () =>
+            dashboardItem.GpsLocationLatitude.ShouldEqual(prefilledGpsQuestionLatitude);
+
+        It should_store_longitude_from_answer_to_prefilled_GPS_question_to_result_dashboard_item = () =>
+            dashboardItem.GpsLocationLongitude.ShouldEqual(prefilledGpsQuestionLongitude);
+
         private static DashboardDenormalizer denormalizer;
-        private static IPublishedEvent<InterviewOnClientCreated> @event;
+        private static IPublishedEvent<InterviewSynchronized> @event;
         private static QuestionnaireDTO dashboardItem;
         private static Guid prefilledGpsQuestionId = Guid.Parse("11111111111111111111111111111111");
         private static Guid interviewId = Guid.Parse("cccccccccccccccccccccccccccccccc");
+        private static double prefilledGpsQuestionLatitude = 10.43;
+        private static double prefilledGpsQuestionLongitude = -3.405;
+        private static string stringGpsAnswer = "10.43,-3.405[0]0";
     }
 }
