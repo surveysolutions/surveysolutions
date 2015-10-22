8a40ba3e16df8b092feeac425aed0514f49eea2d
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2015-10-22 13:26:09 -0400

    Web\Maintenance: renamings and refactoring KP-6241

--- src/Core/SharedKernels/SurveyManagement/SurveyManagement.Web/Controllers/MaintenanceController.cs
+++ src/Core/SharedKernels/SurveyManagement/SurveyManagement.Web/Controllers/MaintenanceController.cs
@@ -6,7 +6,7 @@ namespace WB.Core.SharedKernels.SurveyManagement.Web.Controllers
     public class MaintenanceController : Controller
     {
         [NoTransaction]
-        public ActionResult WaitForReadLayerRebuild(string returnUrl)
+        public ActionResult WaitForReadSideRebuild(string returnUrl)
         {
             return this.View(model: returnUrl);
         }
