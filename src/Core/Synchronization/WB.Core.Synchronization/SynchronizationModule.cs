e663923c6105acf6b56b2be3d491f1bb3b0ebc12
Roman Lytvyn <litvin.roma@gmail.com>
2015-10-22 12:00:04 +0300

    HQ/SV: removed unused denormalizers/models. KP-6228

--- src/Core/Synchronization/WB.Core.Synchronization/SynchronizationModule.cs
+++ src/Core/Synchronization/WB.Core.Synchronization/SynchronizationModule.cs
@@ -3,10 +3,7 @@ using WB.Core.Infrastructure.CommandBus;
 using WB.Core.Synchronization.Aggregates;
 using WB.Core.Synchronization.Commands;
 using WB.Core.Synchronization.Implementation.ImportManager;
-using WB.Core.Synchronization.Implementation.SyncLogger;
-using WB.Core.Synchronization.Implementation.SyncManager;
 using WB.Core.Synchronization.MetaInfo;
-using WB.Core.Synchronization.SyncStorage;
 
 namespace WB.Core.Synchronization
 {
@@ -21,8 +18,6 @@ namespace WB.Core.Synchronization
 
         public override void Load()
         {
-            this.Bind<ISyncLogger>().To<SyncLogger>();
-            this.Bind<ISyncManager>().To<SyncManager>();
             this.Bind<IBackupManager>().To<DefaultBackupManager>();
             this.Bind<SyncSettings>().ToConstant(this.syncSettings);
             this.Bind<IMetaInfoBuilder>().To<MetaInfoBuilder>();
