30d8cf5f45870e824e4005143bf89a7ce3b93802
Andrii Kozhyn <hedgic@gmail.com>
2015-10-26 14:57:55 -0400

    KP-6275: Removed 404 errors from logs

---
+++ src/Core/SharedKernels/SurveyManagement/SurveyManagement.Web/Utils/ExceptionExtensoins.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web;
+
+namespace WB.Core.SharedKernels.SurveyManagement.Web.Utils
+{
+    public static class ExceptionExtensions
+    {
+        public static bool IsHttpNotFound(this Exception e)
+        {
+            var httpException = e as HttpException;
+            return httpException != null && httpException.GetHttpCode() == 404;
+        }
+    }
+}
