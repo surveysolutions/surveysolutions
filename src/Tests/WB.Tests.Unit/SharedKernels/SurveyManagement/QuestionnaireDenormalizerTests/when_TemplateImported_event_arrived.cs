78bc1bfa6f06992a8b3b1470e3f7a9f0ae4edcc6
Andrii Kozhyn <hedgic@gmail.com>
2015-10-27 19:15:45 -0400

    KP-6012: Split IPublishableEvent and IUncommittedEvent fixed unit tests

--- src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/QuestionnaireDenormalizerTests/when_TemplateImported_event_arrived.cs
+++ src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/QuestionnaireDenormalizerTests/when_TemplateImported_event_arrived.cs
@@ -35,10 +35,11 @@ namespace WB.Tests.Unit.SharedKernels.SurveyManagement.QuestionnaireDenormalizer
         };
 
         Because of = () =>
-            questionnaireBrowseItemDenormalizer.Handle(CreatePublishedEvent(questionnaireId, new TemplateImported() { AllowCensusMode = true, Source = questionnaireDocument }));
+            questionnaireBrowseItemDenormalizer.Handle(CreatePublishedEvent(questionnaireId, 
+                new TemplateImported() { AllowCensusMode = true, Source = questionnaireDocument, Version = 1}));
 
         It should_questionnaireBrowseItem_be_stored_at_versionedReadSideRepositoryWriter_once = () =>
-           questionnaireDocumentVersionedStorageMock.Verify(x => x.Store(Moq.It.Is<QuestionnaireDocumentVersioned>(b => b.Version == 1 && b.Questionnaire.PublicKey == questionnaireDocument.PublicKey), questionnaireId.FormatGuid() + "$0"), Times.Once);
+           questionnaireDocumentVersionedStorageMock.Verify(x => x.Store(Moq.It.Is<QuestionnaireDocumentVersioned>(b => b.Version == 1 && b.Questionnaire.PublicKey == questionnaireDocument.PublicKey), questionnaireId.FormatGuid() + "$1"), Times.Once);
 
         private static QuestionnaireDenormalizer questionnaireBrowseItemDenormalizer;
         private static Guid questionnaireId = Guid.Parse("11111111111111111111111111111111");
