1f44852086df911e4d2eb2000ac82ce625bf0d41
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2015-10-29 10:53:43 -0400

    HQ\ParaData: KP-6272 remove exported data reference dto

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/DataExport/Views/ExportedDataReferencesView.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/DataExport/Views/ExportedDataReferencesView.cs
@@ -8,18 +8,10 @@ namespace WB.Core.BoundedContexts.Headquarters.DataExport.Views
         public ExportedDataReferencesView()
         {
         }
-
-        public ExportedDataReferencesView(string exportedDataReferenceId, DataExportFormat dataExportFormat, bool hasDataToExport, DateTime lastUpdateDate, int progressInPercents)
-        {
-            this.DataExportFormat = dataExportFormat;
-            this.HasDataToExport = hasDataToExport;
-            this.LastUpdateDate = lastUpdateDate;
-            this.ProgressInPercents = progressInPercents;
-        }
-        public string ExportedDataReferenceId { get; set; }
         public DataExportFormat DataExportFormat { get; set; }
         public bool HasDataToExport { get; set; }
         public DateTime LastUpdateDate { get; set; }
         public int ProgressInPercents { get; set; }
+        public DataExportStatus StatusOfLatestExportprocess { get; set; }
     }
 }
