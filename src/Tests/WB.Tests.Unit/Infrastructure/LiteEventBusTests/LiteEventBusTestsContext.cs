dbb20deb4d824dbd2b7b282569958fb8f40e3a2a
Pavel Stovpivskyi <sby@rambler.ru>
2015-10-30 12:59:27 -0400

    Added support ILiteEvent into ILiteEventHandler

--- src/Tests/WB.Tests.Unit/Infrastructure/LiteEventBusTests/LiteEventBusTestsContext.cs
+++ src/Tests/WB.Tests.Unit/Infrastructure/LiteEventBusTests/LiteEventBusTestsContext.cs
@@ -1,13 +1,14 @@
 using System;
 using Ncqrs.Eventing;
+using WB.Core.Infrastructure.EventBus.Lite;
 
 namespace WB.Tests.Unit.Infrastructure.LiteEventBusTests
 {
     public class LiteEventBusTestsContext
     {
-        public class DummyEvent  { }
+        public class DummyEvent : ILiteEvent  { }
 
-        public class DifferentDummyEvent { }
+        public class DifferentDummyEvent : ILiteEvent { }
 
         internal static DummyEvent CreateDummyEvent()
         {
