4c350a91f701484b032ba9a246288deef3032f2c
Pavel Stovpivskyi <sby@rambler.ru>
2015-10-30 12:59:27 -0400

    Added support ILiteEvent into ILiteEventHandler

--- src/Core/SharedKernels/DataCollection/DataCollection/Events/Interview/Base/InterviewPassiveEvent.cs
+++ src/Core/SharedKernels/DataCollection/DataCollection/Events/Interview/Base/InterviewPassiveEvent.cs
@@ -1,4 +1,6 @@
-﻿namespace WB.Core.SharedKernels.DataCollection.Events.Interview.Base
+﻿using WB.Core.Infrastructure.EventBus.Lite;
+
+namespace WB.Core.SharedKernels.DataCollection.Events.Interview.Base
 {
-    public abstract class InterviewPassiveEvent {}
+    public abstract class InterviewPassiveEvent: ILiteEvent {}
 }
