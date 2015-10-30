6c7773deecd68e5dac4173f4b8c13be6f933e149
Anastasya Kholodova <nastya.kholodova@gmail.com>
2015-10-30 17:44:37 -0400

    Hq\ParaData: KP-6225 fix progress reporting

--- src/Core/Infrastructure/WB.Core.Infrastructure/Ncqrs/Eventing/Storage/IStreamableEventStore.cs
+++ src/Core/Infrastructure/WB.Core.Infrastructure/Ncqrs/Eventing/Storage/IStreamableEventStore.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 
 namespace Ncqrs.Eventing.Storage
@@ -11,27 +10,5 @@ namespace Ncqrs.Eventing.Storage
         IEnumerable<CommittedEvent> GetAllEvents();
 
         IEnumerable<EventSlice> GetEventsAfterPosition(EventPosition? position);
-    }
-
-    public class EventSlice : IEnumerable<CommittedEvent>
-    {
-        private readonly IEnumerable<CommittedEvent> events;
-
-        public EventSlice(IEnumerable<CommittedEvent> events, EventPosition position)
-        {
-            this.events = events;
-            this.Position = position;
-        }
-
-        public EventPosition Position { get; private set; }
-        public IEnumerator<CommittedEvent> GetEnumerator()
-        {
-            return events.GetEnumerator();
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return this.GetEnumerator();
-        }
     }
 }
