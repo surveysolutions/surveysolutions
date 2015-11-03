50449728154567e5fe33a603e7504d0c3f40d988
slava <slava.scherbak@gmail.com>
2015-11-03 15:12:32 -0500

    Macros: added commands and events to add, update and remove macroses. KP-6312

---
+++ src/Core/SharedKernels/SurveySolutions/SurveySolutions/Documents/Macros.cs
@@ -0,0 +1,9 @@
+namespace WB.Core.SharedKernels.SurveySolutions.Documents
+{
+    public class Macros
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Expression { get; set; }
+    }
+}
