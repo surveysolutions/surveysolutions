324563bab69d5d129a4a9c4b4006e3a9332e8740
Roman Lytvyn <litvin.roma@gmail.com>
2015-11-09 13:43:31 +0200

    HQ: questionnaire title for batch upload pages. fixed factory by questionnaire browse items. KP-6336

--- src/Core/SharedKernels/DataCollection/DataCollection/Views/Questionnaire/IQuestionnaireBrowseViewFactory.cs
+++ src/Core/SharedKernels/DataCollection/DataCollection/Views/Questionnaire/IQuestionnaireBrowseViewFactory.cs
@@ -1,3 +1,4 @@
+using WB.Core.SharedKernels.DataCollection.Implementation.Entities;
 using WB.Core.SharedKernels.DataCollection.Views.Questionnaire.BrowseItem;
 
 namespace WB.Core.SharedKernels.DataCollection.Views.Questionnaire
@@ -5,5 +6,6 @@ namespace WB.Core.SharedKernels.DataCollection.Views.Questionnaire
     public interface IQuestionnaireBrowseViewFactory 
     {
         QuestionnaireBrowseView Load(QuestionnaireBrowseInputModel input);
+        QuestionnaireBrowseItem GetById(QuestionnaireIdentity identity);
     }
 }
