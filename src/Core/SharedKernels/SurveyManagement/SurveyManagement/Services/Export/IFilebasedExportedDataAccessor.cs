e52b899ff8676dde544a08449b2b3481b9a58db2
Anastasya Kholodova <nastya.kholodova@gmail.com>
2015-11-09 12:26:00 -0500

    HQ\DataExport: KP-6308 move ddi export to DAtaExportApiController

--- src/Core/SharedKernels/SurveyManagement/SurveyManagement/Services/Export/IFilebasedExportedDataAccessor.cs
+++ src/Core/SharedKernels/SurveyManagement/SurveyManagement/Services/Export/IFilebasedExportedDataAccessor.cs
@@ -4,9 +4,7 @@ namespace WB.Core.SharedKernels.SurveyManagement.Services.Export
 {
     public interface IFilebasedExportedDataAccessor
     {
-        string GetFolderPathOfFilesByQuestionnaireForInterview(Guid questionnaireId, long version, Guid interviewId);
         string GetFolderPathOfFilesByQuestionnaire(Guid questionnaireId, long version);
-        string GetFilePathToExportedBinaryData(Guid questionnaireId, long version);
         string GetFilePathToExportedDDIMetadata(Guid questionnaireId, long version);
 
         string GetArchiveFilePathForExportedTabularData(Guid questionnaireId, long version);
