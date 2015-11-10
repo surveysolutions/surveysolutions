42e1282bea85c4e827c816e3e269e179c3ec1324
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2015-11-09 23:23:32 -0500

    KP-6338 Controller for copy-paste

---
+++ src/Tests/WB.Tests.Unit/Applications/Designer/CommandInflaterTests/CommandInflaterTestsContext.cs
@@ -0,0 +1,26 @@
+﻿using Moq;
+using WB.UI.Designer.Code.Implementation;
+using WB.UI.Shared.Web.Membership;
+using Main.Core.Documents;
+using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
+using System;
+
+namespace WB.Tests.Unit.Applications.Designer.CommandPostProcessorTests
+{
+    internal class CommandInflaterTestsContext
+    {
+        protected static CommandInflater CreateCommandInflater(
+            IMembershipUserService userHelper = null,
+            IReadSideKeyValueStorage<QuestionnaireDocument> storage = null)
+        {
+            return new CommandInflater(
+                userHelper ?? Mock.Of<IMembershipUserService>(),
+                storage ?? Mock.Of<IReadSideKeyValueStorage<QuestionnaireDocument>>());
+        }
+
+        protected static QuestionnaireDocument CreateQuestionnaireDocument(Guid questoinnaireId, string title, Guid creator)
+        {
+            return new QuestionnaireDocument() {PublicKey = questoinnaireId, Title = title, CreatedBy = creator, IsPublic = true};
+        }
+    }
+}
--- src/UI/Designer/WB.UI.Designer/Code/Implementation/CommandInflaitingException.cs
+++ src/UI/Designer/WB.UI.Designer/Code/Implementation/CommandInflaitingException.cs
@@ -4,6 +4,13 @@ namespace WB.UI.Designer.Code.Implementation
 {
     public class CommandInflaitingException : Exception
     {
-        public CommandInflaitingException(string message) : base( message) { }
+        public readonly CommandInflatingExceptionType ExceptionType;
+
+        public CommandInflaitingException(string message) : this(CommandInflatingExceptionType.Common, message) { }
+
+        public CommandInflaitingException(CommandInflatingExceptionType exceptionType, string message) : base(message)
+        {
+            this.ExceptionType = exceptionType;
+        }
     }
 }
