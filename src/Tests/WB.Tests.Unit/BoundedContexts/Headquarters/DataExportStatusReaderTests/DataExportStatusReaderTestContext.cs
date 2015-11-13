d1744eeff5599158f6a96ccf6820e289b6865aba
Anastasya Kholodova <nastya.kholodova@gmail.com>
2015-11-13 18:24:56 -0500

    HQ\Export: KP-6278 hide refresh button in questionnaire doesn't have multimedia questions

--- src/Tests/WB.Tests.Unit/BoundedContexts/Headquarters/DataExportStatusReaderTests/DataExportStatusReaderTestContext.cs
+++ src/Tests/WB.Tests.Unit/BoundedContexts/Headquarters/DataExportStatusReaderTests/DataExportStatusReaderTestContext.cs
@@ -4,6 +4,8 @@ using WB.Core.BoundedContexts.Headquarters.DataExport.Accessors;
 using WB.Core.BoundedContexts.Headquarters.DataExport.Services;
 using WB.Core.BoundedContexts.Headquarters.DataExport.Views;
 using WB.Core.Infrastructure.FileSystem;
+using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
+using WB.Core.SharedKernels.SurveyManagement.Views.DataExport;
 
 namespace WB.Tests.Unit.BoundedContexts.Headquarters.DataExportStatusReaderTests
 {
@@ -14,11 +16,16 @@ namespace WB.Tests.Unit.BoundedContexts.Headquarters.DataExportStatusReaderTests
             IDataExportProcessesService dataExportProcessesService = null,
             IFilebasedExportedDataAccessor filebasedExportedDataAccessor = null,
             IParaDataAccessor paraDataAccessor = null,
-            IFileSystemAccessor fileSystemAccessor = null)
+            IFileSystemAccessor fileSystemAccessor = null,
+            IReadSideKeyValueStorage<QuestionnaireExportStructure> questionnaireReader =null)
         {
+            var questionnaireExportStructure = Create.QuestionnaireExportStructure();
             return new DataExportStatusReader(dataExportProcessesService ?? Mock.Of<IDataExportProcessesService>(),
                 filebasedExportedDataAccessor ?? Mock.Of<IFilebasedExportedDataAccessor>(),
-                paraDataAccessor ?? Mock.Of<IParaDataAccessor>(), fileSystemAccessor ?? Mock.Of<IFileSystemAccessor>());
+                paraDataAccessor ?? Mock.Of<IParaDataAccessor>(), fileSystemAccessor ?? Mock.Of<IFileSystemAccessor>(),
+                questionnaireReader ??
+                Mock.Of<IReadSideKeyValueStorage<QuestionnaireExportStructure>>(
+                    _ => _.GetById(Moq.It.IsAny<string>()) == questionnaireExportStructure));
         }
     }
 }
