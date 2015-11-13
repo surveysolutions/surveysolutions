8ff2c5378888732ce75dd46e1e23267d234f2e07
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2015-11-13 16:29:00 +0200

    KP-6306: Moved decimal formatter to separate class

---
+++ src/Core/SharedKernels/SurveyManagement/SurveyManagement/Implementation/Services/DecimalToHeaderConverter.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+
+namespace WB.Core.SharedKernels.SurveyManagement.Implementation.Services
+{
+    public static class DecimalToHeaderConverter
+    {
+        private static readonly NumberFormatInfo NumberFormat = new NumberFormatInfo
+        {
+            NumberDecimalSeparator = "_",
+            NegativeSign = "n"
+        };
+
+
+        public static string ToHeader(decimal value)
+        {
+            return value.ToString(NumberFormat);
+        }
+
+        public static decimal ToValue(string value)
+        {
+            return decimal.Parse(value, NumberFormat);
+        }
+    }
+}
