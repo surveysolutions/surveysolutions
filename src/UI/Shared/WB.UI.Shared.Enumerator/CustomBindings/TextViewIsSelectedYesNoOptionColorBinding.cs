5dae475185f725e8417ac40595c28c1fbfffab85
Pavel Stovpivskyi <sby@rambler.ru>
2015-11-13 16:40:01 +0200

    Changed styles for Y/N question
    KP-6304

---
+++ src/UI/Shared/WB.UI.Shared.Enumerator/CustomBindings/TextViewIsSelectedYesNoOptionColorBinding.cs
@@ -0,0 +1,20 @@
+﻿using Android.Graphics;
+using Android.Widget;
+
+namespace WB.UI.Shared.Enumerator.CustomBindings
+{
+    public class TextViewIsSelectedYesNoOptionColorBinding : BaseBinding<TextView, bool>
+    {
+        public TextViewIsSelectedYesNoOptionColorBinding(TextView textView)
+            : base(textView) {}
+
+        protected override void SetValueToView(TextView textView, bool isSelected)
+        {
+            Color textColor = isSelected
+                ? textView.Resources.GetColor(Resource.Color.interview_question_yesno_selected_text)
+                : textView.Resources.GetColor(Resource.Color.interview_question_yesno_not_selected_text);
+
+            textView.SetTextColor(textColor);
+        }
+    }
+}
