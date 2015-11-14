eb8839f2017197c7a0520bb2c8c2a19f5fd5598b
slava <slava.scherbak@gmail.com>
2015-11-14 12:39:07 -0500

    fixed merge

--- src/Tests/WB.Tests.Unit/Stub.cs
+++ src/Tests/WB.Tests.Unit/Stub.cs
@@ -2,10 +2,8 @@ using System;
 using Cirrious.CrossCore.Core;
 using Cirrious.MvvmCross.Plugins.Messenger;
 using Moq;
-using WB.Core.BoundedContexts.Tester.ViewModels;
 using WB.Core.Infrastructure.PlainStorage;
 using WB.Core.SharedKernels.DataCollection.Services;
-using WB.Core.SharedKernels.DataCollection.V5;
 using WB.Core.SharedKernels.Enumerator.Models.Questionnaire;
 using WB.Core.SharedKernels.Enumerator.Repositories;
 using WB.Core.SharedKernels.Enumerator.ViewModels.InterviewDetails;
@@ -58,7 +56,7 @@ namespace WB.Tests.Unit
 
         public static IInterviewExpressionStatePrototypeProvider InterviewExpressionStateProvider()
         {
-            var expressionState =   new InterviewExpressionStateStub();
+            var expressionState = new InterviewExpressionStateStub();
 
             return Mock.Of<IInterviewExpressionStatePrototypeProvider>(_ => 
                 _.GetExpressionState(It.IsAny<Guid>(), It.IsAny<long>()) == expressionState);
