671c6f97df89d4008341a927f945fa60ec03fe02
Anastasya Kholodova <nastya.kholodova@gmail.com>
2015-11-17 10:08:30 -0500

    HQ\Export: KP-6311 unit tests for export handlers

---
+++ src/Tests/WB.Tests.Unit/BoundedContexts/Headquarters/BinaryFormatDataExportHandlerTests/BinaryFormatDataExportHandlerTestContext.cs
@@ -0,0 +1,42 @@
+﻿using Machine.Specifications;
+using Moq;
+using WB.Core.BoundedContexts.Headquarters.DataExport.Accessors;
+using WB.Core.BoundedContexts.Headquarters.DataExport.ExportProcessHandlers;
+using WB.Core.BoundedContexts.Headquarters.DataExport.Services;
+using WB.Core.Infrastructure.FileSystem;
+using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
+using WB.Core.Infrastructure.Transactions;
+using WB.Core.SharedKernels.DataCollection.Repositories;
+using WB.Core.SharedKernels.SurveyManagement.Views.DataExport;
+using WB.Core.SharedKernels.SurveyManagement.Views.Interview;
+using WB.Core.SharedKernels.SurveyManagement.Views.InterviewHistory;
+
+namespace WB.Tests.Unit.BoundedContexts.Headquarters.BinaryFormatDataExportHandlerTests
+{
+    [Subject(typeof(BinaryFormatDataExportHandler))]
+    internal class BinaryFormatDataExportHandlerTestContext
+    {
+        protected static BinaryFormatDataExportHandler CreateBinaryFormatDataExportHandler(
+            IFileSystemAccessor fileSystemAccessor=null,
+            IPlainInterviewFileStorage plainFileRepository = null,
+            IFilebasedExportedDataAccessor filebasedExportedDataAccessor = null,
+            IQueryableReadSideRepositoryReader<InterviewSummary> interviewSummaries = null,
+            IArchiveUtils archiveUtils = null,
+            IReadSideKeyValueStorage<InterviewData> interviewDatas = null,
+            IReadSideKeyValueStorage<QuestionnaireExportStructure> questionnaireReader = null,
+            IDataExportProcessesService dataExportProcessesService = null)
+        {
+            return new BinaryFormatDataExportHandler(
+                fileSystemAccessor ?? Mock.Of<IFileSystemAccessor>(),
+                plainFileRepository ?? Mock.Of<IPlainInterviewFileStorage>(),
+                filebasedExportedDataAccessor ?? Mock.Of<IFilebasedExportedDataAccessor>(),
+                new InterviewDataExportSettings(),
+                Mock.Of<ITransactionManager>(),
+                interviewSummaries ?? Mock.Of<IQueryableReadSideRepositoryReader<InterviewSummary>>(),
+                archiveUtils ?? Mock.Of<IArchiveUtils>(),
+                interviewDatas ?? Mock.Of<IReadSideKeyValueStorage<InterviewData>>(),
+                questionnaireReader ?? Mock.Of<IReadSideKeyValueStorage<QuestionnaireExportStructure>>(),
+                dataExportProcessesService ?? Mock.Of<IDataExportProcessesService>());
+        }
+    }
+}
