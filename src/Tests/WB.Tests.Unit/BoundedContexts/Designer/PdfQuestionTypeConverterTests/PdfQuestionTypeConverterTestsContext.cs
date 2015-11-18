22754c883bd521198b7319fedb61dc85e9a62b69
Pavel Stovpivskyi <630198+spuv@users.noreply.github.com>
2015-11-18 19:14:43 +0200

    Added Y/N question to PDF export
    KP-6420

---
+++ src/Tests/WB.Tests.Unit/BoundedContexts/Designer/PdfQuestionTypeConverterTests/PdfQuestionTypeConverterTestsContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using Main.Core.Entities.SubEntities;
+using Moq;
+using WB.Core.BoundedContexts.Designer.Views.Questionnaire.Pdf;
+
+namespace WB.Tests.Unit.BoundedContexts.Designer.PdfQuestionTypeConverterTests
+{
+    public class PdfQuestionTypeConverterTestsContext
+    {
+        protected static PdfQuestionTypeConverter CreatePdfQuestionTypeConverter()
+        {
+            return new PdfQuestionTypeConverter();
+        }
+
+        protected static PdfQuestionType[] GetAllPdfQuestionTypes()
+        {
+            return (PdfQuestionType[]) Enum.GetValues(typeof (PdfQuestionType));
+        }
+
+        protected static QuestionType[] GetAllQuestionTypes()
+        {
+            var type = typeof(QuestionType);
+            var questionTypesNames = Enum.GetNames(typeof (QuestionType));
+            return questionTypesNames
+                .Where(qt => type.GetMember(qt.ToString())[0].GetCustomAttributes(typeof(ObsoleteAttribute), false).Length == 0)
+                .Select(qn => (QuestionType)Enum.Parse(typeof(QuestionType), qn))
+                .ToArray();
+        }
+
+        protected static IQuestion CreateQuestion(QuestionType questionType)
+        {
+            return Mock.Of<IQuestion>(q => q.QuestionType == questionType);
+        }
+    }
+}
