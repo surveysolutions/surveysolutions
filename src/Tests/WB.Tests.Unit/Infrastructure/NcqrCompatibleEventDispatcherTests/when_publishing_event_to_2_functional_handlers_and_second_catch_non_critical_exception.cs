219df903a86af2ddb3b742a6a5e7bdea83b8c925
Roman Lytvyn <litvin.roma@gmail.com>
2015-11-19 13:28:46 +0200

    HQ: code review changes by catching exceptions in event handlers. added test for functional event handlers. KP-6279

--- src/Tests/WB.Tests.Unit/Infrastructure/NcqrCompatibleEventDispatcherTests/when_publishing_event_to_2_functional_handlers_and_second_catch_non_critical_exception.cs
+++ src/Tests/WB.Tests.Unit/Infrastructure/NcqrCompatibleEventDispatcherTests/when_publishing_event_to_2_functional_handlers_and_second_catch_non_critical_exception.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using Machine.Specifications;
 using Moq;
@@ -8,52 +7,51 @@ using Ncqrs.Eventing.ServiceModel.Bus;
 using WB.Core.GenericSubdomains.Portable;
 using WB.Core.GenericSubdomains.Portable.Services;
 using WB.Core.Infrastructure.EventBus;
+using WB.Core.Infrastructure.EventHandlers;
 using WB.Core.Infrastructure.Implementation.EventDispatcher;
+using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
+using WB.Core.SharedKernels.SurveySolutions;
 using It = Machine.Specifications.It;
 
 namespace WB.Tests.Unit.Infrastructure.NcqrCompatibleEventDispatcherTests
 {
-    internal class when_publishing_event_to_2_old_school_handlers_and_first_catch_non_critical_exception : NcqrCompatibleEventDispatcherTestContext
+    internal class when_publishing_event_to_2_functional_handlers_and_second_catch_non_critical_exception : NcqrCompatibleEventDispatcherTestContext
     {
-        private class FirstEventHandler : IEventHandler, IEventHandler<object>
+        private class FunctionalEventHandlerEvent { }
+
+        private class FunctionalEventHandler :
+            AbstractFunctionalEventHandler<IReadSideRepositoryEntity, IReadSideStorage<IReadSideRepositoryEntity>>,
+            IUpdateHandler<IReadSideRepositoryEntity, FunctionalEventHandlerEvent>
         {
-            public void Handle(IPublishedEvent<object> evnt)
+            public FunctionalEventHandler(IReadSideStorage<IReadSideRepositoryEntity> readSideStorage) : base(readSideStorage)
+            {
+            }
+
+            public IReadSideRepositoryEntity Update(IReadSideRepositoryEntity state, IPublishedEvent<FunctionalEventHandlerEvent> @event)
             {
                 throw new NotImplementedException();
             }
-
-            public string Name => "First Event Handler";
-            public object[] Readers { get; }
-            public object[] Writers { get; }
         }
-
         Establish context = () =>
         {
-            publishableEvent = Create.PublishableEvent();
+            publishableEvent = Create.PublishableEvent(payload: new FunctionalEventHandlerEvent());
+
+            var secondFunctionalEventHandler = new FunctionalEventHandler(Mock.Of<IReadSideStorage<IReadSideRepositoryEntity>>());
 
             eventDispatcher = Create.NcqrCompatibleEventDispatcher(logger: loggerMock.Object,
                 eventBusSettings: new EventBusSettings()
                 {
                     EventHandlerTypesWithIgnoredExceptions = new[]
                     {
-                        typeof (FirstEventHandler)
+                        secondFunctionalEventHandler.GetType()
                     },
                     DisabledEventHandlerTypes = new Type[0]
                 });
             eventDispatcher.OnCatchingNonCriticalEventHandlerException +=
                 (e) => { handledNonCriticalEventHandlerException = e; };
 
-            var firstEventHandler = new FirstEventHandler();
-
-            var uniqueEventHandlerMock = new Mock<IEnumerable<bool>>();
-            var eventHandlerMock = uniqueEventHandlerMock.As<IEventHandler>();
-            secondOldSchoolEventHandlerMock = eventHandlerMock.As<IEventHandler<object>>();
-            secondOldSchoolEventHandlerMock
-                .Setup(_ => _.Handle(Moq.It.IsAny<IPublishedEvent<object>>()))
-                .Throws<Exception>();;
-
-            eventDispatcher.Register(firstEventHandler);
-            eventDispatcher.Register(eventHandlerMock.Object);
+            eventDispatcher.Register(Setup.FailingFunctionalEventHandlerHavingUniqueType<int>());
+            eventDispatcher.Register(secondFunctionalEventHandler);
         };
 
         Because of = () =>
@@ -65,11 +63,6 @@ namespace WB.Tests.Unit.Infrastructure.NcqrCompatibleEventDispatcherTests
 
         It should_put_1_exception_to_AggregateException = () =>
             aggregateException.InnerExceptions.Count.ShouldEqual(1);
-
-        It should_call_2_event_handlers = () =>
-            secondOldSchoolEventHandlerMock.Verify(x => x.Handle(
-                Moq.It.IsAny<IPublishedEvent<object>>()),
-                Times.Once);
 
         It should_log_catched_exception = () =>
             loggerMock.Verify(x => x.Error(Moq.It.IsAny<string>(), Moq.It.IsAny<Exception>()), Times.Once);
@@ -83,7 +76,6 @@ namespace WB.Tests.Unit.Infrastructure.NcqrCompatibleEventDispatcherTests
         private static NcqrCompatibleEventDispatcher eventDispatcher;
         private static IPublishableEvent publishableEvent;
         private static AggregateException aggregateException;
-        private static Mock<IEventHandler<object>> secondOldSchoolEventHandlerMock;
         private static EventHandlerException handledNonCriticalEventHandlerException;
         private static readonly Mock<ILogger> loggerMock = new Mock<ILogger>();
     }
