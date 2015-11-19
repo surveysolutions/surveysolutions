4211ffd371becda2b03dea7f62c27185d7a84029
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2015-11-19 14:22:03 +0200

    Tests\Interview\YesNo: added test on questionnaire not existance KP-6416

--- src/Tests/WB.Tests.Unit/SharedKernels/DataCollection/InterviewTests/when_answering_yesno_question_on_hard_deleted_interview.cs
+++ src/Tests/WB.Tests.Unit/SharedKernels/DataCollection/InterviewTests/when_answering_yesno_question_on_hard_deleted_interview.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Machine.Specifications;
 using WB.Core.SharedKernels.DataCollection.Commands.Interview;
 using WB.Core.SharedKernels.DataCollection.Exceptions;
@@ -6,7 +7,7 @@ using It = Machine.Specifications.It;
 
 namespace WB.Tests.Unit.SharedKernels.DataCollection.InterviewTests
 {
-    internal class when_answering_yesno_question_on_hard_deleted_interview : InterviewTestsContext
+    internal class when_answering_yesno_question_on_hard_deleted_interview
     {
         Establish context = () =>
         {
@@ -21,14 +22,14 @@ namespace WB.Tests.Unit.SharedKernels.DataCollection.InterviewTests
             exception = Catch.Only<InterviewException>(() =>
                 interview.AnswerYesNoQuestion(command));
 
-        It should_raise_InterviewException = () =>
+        It should_throw_InterviewException = () =>
             exception.ShouldNotBeNull();
 
-        It should_raise_InterviewException_with_type_InterviewHardDeleted = () =>
+        It should_throw_InterviewException_with_type_InterviewHardDeleted = () =>
             exception.ExceptionType.ShouldEqual(InterviewDomainExceptionType.InterviewHardDeleted);
 
-        private static InterviewException exception;
-        private static Interview interview;
         private static AnswerYesNoQuestion command;
+        private static Interview interview;
+        private static InterviewException exception;
     }
 }
