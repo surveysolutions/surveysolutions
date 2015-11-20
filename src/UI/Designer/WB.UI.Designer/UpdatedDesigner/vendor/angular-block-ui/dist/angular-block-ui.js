02edbf365cf22d0f1fa09c64c480ded538ceefc2
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2015-11-20 16:32:17 -0500

    update angular 1.4.7

--- src/UI/Designer/WB.UI.Designer/UpdatedDesigner/vendor/angular-block-ui/dist/angular-block-ui.js
+++ src/UI/Designer/WB.UI.Designer/UpdatedDesigner/vendor/angular-block-ui/dist/angular-block-ui.js
@@ -1,5 +1,5 @@
 /*!
-   angular-block-ui v0.2.0
+   angular-block-ui v0.2.1
    (c) 2015 (null) McNull https://github.com/McNull/angular-block-ui
    License: MIT
 */
@@ -384,7 +384,10 @@ blkUI.factory('blockUIHttpInterceptor', ["$q", "$injector", "blockUIConfig", "$t
 blkUI.factory('blockUI', ["blockUIConfig", "$timeout", "blockUIUtils", "$document", function(blockUIConfig, $timeout, blockUIUtils, $document) {
 
   var $body = $document.find('body');
-
+  
+  // These properties are not allowed to be specified in the start method.
+  var reservedStateProperties = ['id', 'blockCount', 'blocking'];
+  
   function BlockUI(id) {
 
     var self = this;
@@ -400,15 +403,37 @@ blkUI.factory('blockUI', ["blockUIConfig", "$timeout", "blockUIUtils", "$documen
 
     this._refs = 0;
 
-    this.start = function(message) {
-
-      if(state.blockCount > 0) {
-        message = message || state.message || blockUIConfig.message;
+    this.start = function(messageOrOptions) {
+      
+      messageOrOptions = messageOrOptions || {};
+      
+      if(angular.isString(messageOrOptions)) {
+        messageOrOptions = {
+          message: messageOrOptions
+        };
       } else {
-        message = message || blockUIConfig.message;
+        angular.forEach(reservedStateProperties, function(x) {
+          if(messageOrOptions[x]) {
+            throw new Error('The property ' + x + ' is reserved for the block state.');
+          }
+        });
+      } 
+      
+      angular.extend(state, messageOrOptions);
+      
+      if(state.blockCount > 0) {
+        state.message = messageOrOptions.message || state.message || blockUIConfig.message;
+      } else {
+        state.message = messageOrOptions.message || blockUIConfig.message;
       }
+      
+      // if(state.blockCount > 0) {
+      //   messageOrOptions = messageOrOptions || state.message || blockUIConfig.message;
+      // } else {
+      //   messageOrOptions = messageOrOptions || blockUIConfig.message;
+      // }
 
-      state.message = message;
+      // state.message = messageOrOptions;
 
       state.blockCount++;
 
@@ -429,17 +454,22 @@ blkUI.factory('blockUI', ["blockUIConfig", "$timeout", "blockUIUtils", "$documen
 
         $timeout(function() {
           // Ensure we still need to blur
-          if(self._restoreFocus) {
+          // Don't restore if active element is body, since this causes IE to switch windows (see http://tjvantoll.com/2013/08/30/bugs-with-document-activeelement-in-internet-explorer/)
+          if (self._restoreFocus && self._restoreFocus !== $body[0]) {
             self._restoreFocus.blur();
           }
         });
       }
 
-      if (!startPromise) {
-        startPromise = $timeout(function() {
-          startPromise = null;
-          state.blocking = true;
-        }, blockUIConfig.delay);
+      if (!startPromise && blockUIConfig.delay !== 0) {
+        startPromise = $timeout(block, blockUIConfig.delay);
+      } else if (blockUIConfig.delay === 0) {
+        block();
+      }
+
+      function block () {
+        startPromise = null;
+        state.blocking = true;
       }
     };
 
@@ -456,6 +486,10 @@ blkUI.factory('blockUI', ["blockUIConfig", "$timeout", "blockUIUtils", "$documen
       if (state.blockCount === 0) {
         self.reset(true);
       }
+    };
+
+    this.isBlocking = function () {
+        return state.blocking;
     };
 
     this.message = function(value) {
@@ -482,7 +516,23 @@ blkUI.factory('blockUI', ["blockUIConfig", "$timeout", "blockUIUtils", "$documen
 
       if(self._restoreFocus && 
          (!$document[0].activeElement || $document[0].activeElement === $body[0])) {
-        self._restoreFocus.focus();
+        
+        //IE8 will throw if element for setting focus is invisible
+        try {
+          self._restoreFocus.focus();
+        } catch(e1) {
+          (function () {
+              var elementToFocus = self._restoreFocus;
+              $timeout(function() { 
+                if(elementToFocus) { 
+                  try { 
+                    elementToFocus.focus(); 
+                  } catch(e2) { }
+              } 
+            },100);
+          })();
+        }
+        
         self._restoreFocus = null;
       }
       
