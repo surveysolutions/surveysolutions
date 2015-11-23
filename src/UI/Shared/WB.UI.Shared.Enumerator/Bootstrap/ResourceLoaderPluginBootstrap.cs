8992b03a431201eede5209b694ae5d3d1bf2f3b7
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2015-11-23 16:19:55 -0500

    fix build of tester

---
+++ src/UI/Shared/WB.UI.Shared.Enumerator/Bootstrap/ResourceLoaderPluginBootstrap.cs
@@ -0,0 +1,9 @@
+using Cirrious.CrossCore.Plugins;
+
+namespace WB.UI.Shared.Enumerator.Bootstrap
+{
+    public class ResourceLoaderPluginBootstrap
+        : MvxPluginBootstrapAction<Cirrious.MvvmCross.Plugins.ResourceLoader.PluginLoader>
+    {
+    }
+}
