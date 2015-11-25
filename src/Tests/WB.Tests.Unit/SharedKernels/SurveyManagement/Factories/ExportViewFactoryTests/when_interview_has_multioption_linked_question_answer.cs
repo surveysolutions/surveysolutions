a76c9ed4c017c805197368a89a77a8890f06e33e
Anastasya Kholodova <nastya.kholodova@gmail.com>
2015-11-25 14:05:49 -0500

    HQ\Export: KP-6439 add unit tests for linked multi option questions

---
+++ src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/Factories/ExportViewFactoryTests/when_interview_has_multioption_linked_question_answer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Machine.Specifications;
+using Main.Core.Entities.SubEntities;
+using WB.Core.BoundedContexts.Headquarters.DataExport.Denormalizers;
+using WB.Core.SharedKernels.SurveyManagement.Views.DataExport;
+using WB.Core.SharedKernels.SurveyManagement.Views.Interview;
+
+namespace WB.Tests.Unit.SharedKernels.SurveyManagement.Factories.ExportViewFactoryTests
+{
+    internal class when_interview_has_multioption_linked_question_answer : ExportViewFactoryTestsContext
+    {
+        Establish context = () =>
+        {
+            multyOptionLinkedQuestionId = Guid.Parse("d7127d06-5668-4fa3-b255-8a2a0aaaa020");
+            linkedSourceQuestionId = Guid.NewGuid();
+
+            var questionnaire = Create.QuestionnaireDocumentWithOneChapter(
+                Create.Roster(rosterId: Guid.NewGuid(), variable: "row", fixedTitles: new[] { "1", "2" },
+                    children: new[] { Create.TextQuestion(questionId: linkedSourceQuestionId, variable: "varTxt") }),
+                Create.MultyOptionsQuestion(id: multyOptionLinkedQuestionId,
+                    variable: "mult",
+                    linkedToQuestionId: linkedSourceQuestionId));
+
+            exportViewFactory = CreateExportViewFactory();
+            questionnaaireExportStructure = exportViewFactory.CreateQuestionnaireExportStructure(questionnaire, 1);
+
+            interview = Create.InterviewData(Create.InterviewQuestion(multyOptionLinkedQuestionId, new[] { 2 }));
+        };
+
+        Because of = () => result = exportViewFactory.CreateInterviewDataExportView(questionnaaireExportStructure, interview);
+
+        It should_put_answers_to_export_in_appropriate_order = () =>
+        {
+            InterviewDataExportLevelView first = result.Levels.First();
+            var exportedQuestion = first.Records.First().Questions.First();
+            exportedQuestion.QuestionType.ShouldEqual(QuestionType.MultyOption);
+            exportedQuestion.QuestionId.ShouldEqual(multyOptionLinkedQuestionId);
+            exportedQuestion.Answers.Length.ShouldEqual(2);
+            exportedQuestion.Answers.SequenceEqual(new[] {"2", ""}).ShouldBeTrue();
+        };
+
+        static ExportViewFactory exportViewFactory;
+        static QuestionnaireExportStructure questionnaaireExportStructure;
+        static InterviewDataExportView result;
+        static InterviewData interview;
+        static Guid multyOptionLinkedQuestionId;
+        static Guid linkedSourceQuestionId;
+    }
+}
