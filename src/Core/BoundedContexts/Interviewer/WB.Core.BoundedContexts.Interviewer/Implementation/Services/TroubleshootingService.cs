39a17e6e47b1610dd54f821847910ad13dbff2c8
Roman Lytvyn <litvin.roma@gmail.com>
2015-12-04 20:01:15 +0200

    IN: backup/restore/send tablet information after moving to siaqo db. KP-6210

--- src/Core/BoundedContexts/Interviewer/WB.Core.BoundedContexts.Interviewer/Implementation/Services/TroubleshootingService.cs
+++ src/Core/BoundedContexts/Interviewer/WB.Core.BoundedContexts.Interviewer/Implementation/Services/TroubleshootingService.cs
@@ -1,5 +1,6 @@
-﻿using System.IO;
+﻿using System;
 using System.Threading.Tasks;
+using PCLStorage;
 using WB.Core.BoundedContexts.Interviewer.Services;
 using WB.Core.Infrastructure.FileSystem;
 
@@ -8,30 +9,34 @@ namespace WB.Core.BoundedContexts.Interviewer.Implementation.Services
     public class TroubleshootingService : ITroubleshootingService
     {
         private readonly IArchiveUtils archiver;
-        private readonly IFileSystemAccessor fileSystemAccessor;
 
-        public TroubleshootingService(IArchiveUtils archiver, IFileSystemAccessor fileSystemAccessor)
+        public TroubleshootingService(IArchiveUtils archiver)
         {
             this.archiver = archiver;
-            this.fileSystemAccessor = fileSystemAccessor;
         }
 
         public async Task<byte[]> GetSystemBackupAsync()
         {
             return await Task.FromResult(
-                this.archiver.ZipDirectoryToByteArray(PCLStorage.FileSystem.Current.LocalStorage.Path, fileFilter: @"\.log$;\.dll$;\.mdb$;"));
+                this.archiver.ZipDirectoryToByteArray(FileSystem.Current.LocalStorage.Path, fileFilter: @"\.log$;\.dll$;\.mdb$;"));
         }
 
-        public async Task BackupAsync(string backupFilePath)
+        public async Task BackupAsync(string backupToFolderPath)
         {
+            var backupFileName = $"backup-interviewer-{DateTime.Now.ToString("yyyyMMddTH-mm")}.ibak";
+            var backupFolder = await FileSystem.Current.GetFolderFromPathAsync(backupToFolderPath);
+            var emptyBackupFile = await backupFolder.CreateFileAsync(backupFileName, CreationCollisionOption.GenerateUniqueName);
+
             var backup = await this.GetSystemBackupAsync();
-            var backupFile = this.fileSystemAccessor.OpenOrCreateFile(backupFilePath, false);
-            await new MemoryStream(backup).CopyToAsync(backupFile);
+            using (var stream = await emptyBackupFile.OpenAsync(FileAccess.ReadAndWrite))
+            {
+                stream.Write(backup, 0, backup.Length);
+            }
         }
 
         public async Task RestoreAsync(string backupFilePath)
         {
-            await Task.Run(() => this.archiver.Unzip(backupFilePath, PCLStorage.FileSystem.Current.LocalStorage.Path));
+            await Task.Run(() => this.archiver.Unzip(backupFilePath, FileSystem.Current.LocalStorage.Path, true));
         }
     }
 }
--- src/Core/Infrastructure/WB.Core.Infrastructure/FileSystem/IArchiveUtils.cs
+++ src/Core/Infrastructure/WB.Core.Infrastructure/FileSystem/IArchiveUtils.cs
@@ -7,7 +7,7 @@ namespace WB.Core.Infrastructure.FileSystem
         void ZipDirectory(string directory, string archiveFile);
         byte[] ZipDirectoryToByteArray(string sourceDirectory, string directoryFilter = null, string fileFilter = null);
         void ZipFiles(IEnumerable<string> files, string archiveFilePath);
-        void Unzip(string archivedFile, string extractToFolder);
+        void Unzip(string archivedFile, string extractToFolder, bool ignoreRootDirectory = false);
         bool IsZipFile(string filePath);
         Dictionary<string, long> GetArchivedFileNamesAndSize(string filePath);
         string CompressString(string stringToCompress);
