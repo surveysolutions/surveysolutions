5c598a435c586b4fdcc9c734fdf39169006fa426
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2015-12-07 12:01:40 -0500

    Designer: KP-6468 store lookup table in questionnaire document

---
+++ src/Core/SharedKernels/SurveySolutions/SurveySolutions/Documents/LookupTable.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace WB.Core.SharedKernels.SurveySolutions.Documents
+{
+    public class LookupTable
+    {
+        public string FileName { get; set; }
+        public HashSet<string> VariableNames { get; set; }
+        public List<LookupTable> Rows { get; set; }
+    }
+
+    public class LookupTableRecord
+    {
+        public decimal RowCode { get; set; }
+        public Dictionary<string,decimal> Variables { get; set; }
+    }
+}
