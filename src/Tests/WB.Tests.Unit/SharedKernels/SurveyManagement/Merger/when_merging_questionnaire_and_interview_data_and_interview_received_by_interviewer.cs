049beb103feb319946da3dfd032588ca0d56ec0d
Pavel Stovpivskyi <sby@rambler.ru>
2015-12-08 15:26:55 +0200

    Added tests for  Supervisor can't change interview if it is marked as taken by interviewer
    KP-6441

---
+++ src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/Merger/when_merging_questionnaire_and_interview_data_and_interview_received_by_interviewer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Machine.Specifications;
+using Main.Core.Documents;
+using Main.Core.Entities.Composite;
+using Main.Core.Entities.SubEntities;
+using Main.Core.Entities.SubEntities.Question;
+using Microsoft.Practices.ServiceLocation;
+using Moq;
+using WB.Core.SharedKernels.DataCollection.Views;
+using WB.Core.SharedKernels.DataCollection.Views.Questionnaire;
+using WB.Core.SharedKernels.SurveyManagement.Views;
+using WB.Core.SharedKernels.SurveyManagement.Views.Interview;
+using WB.Core.SharedKernels.SurveyManagement.Views.Questionnaire;
+using It = Machine.Specifications.It;
+
+namespace WB.Tests.Unit.SharedKernels.SurveyManagement.Merger
+{
+    internal class when_merging_questionnaire_and_interview_data_and_interview_received_by_interviewer : InterviewDataAndQuestionnaireMergerTestContext
+    {
+        Establish context = () =>
+        {
+            merger = CreateMerger();
+            
+            var questionnaireDocument = CreateQuestionnaireDocumentWithOneChapter(
+                new Group(nestedGroupTitle)
+                {
+                    PublicKey = nestedGroupId,
+                    Children = new List<IComposite>()
+                });
+
+            interview = CreateInterviewData(interviewId);
+            interview.ReceivedByInterviewer = true;
+
+            questionnaire = CreateQuestionnaireWithVersion(questionnaireDocument);
+            questionnaireReferenceInfo = CreateQuestionnaireReferenceInfo();
+            questionnaireRosters = CreateQuestionnaireRosterStructure(questionnaireDocument);
+            user = Mock.Of<UserDocument>();
+        };
+
+        Because of = () =>
+            mergeResult = merger.Merge(interview, questionnaire, questionnaireReferenceInfo, questionnaireRosters, user);
+
+        It should_answered_question_be_enabled = () =>
+            mergeResult.ReceivedByInterviewer.ShouldBeTrue();
+
+
+        private static InterviewDataAndQuestionnaireMerger merger;
+        private static InterviewDetailsView mergeResult;
+        private static InterviewData interview;
+        private static QuestionnaireDocumentVersioned questionnaire;
+        private static ReferenceInfoForLinkedQuestions questionnaireReferenceInfo;
+        private static QuestionnaireRosterStructure questionnaireRosters;
+        private static UserDocument user;
+        private static Guid nestedGroupId = Guid.Parse("11111111111111111111111111111111");
+        private static Guid interviewId = Guid.Parse("33333333333333333333333333333333");
+        private static string nestedGroupTitle = "nested Group";
+    }
+}
