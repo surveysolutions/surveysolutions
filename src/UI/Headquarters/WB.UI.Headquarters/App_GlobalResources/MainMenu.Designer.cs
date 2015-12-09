ae72669a5ab417e7fb7ae8f78512e86b9a9bc20b
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2015-12-09 14:22:49 -0500

    KP-6456 API: create page to manage users, who can send api requests

--- src/UI/Headquarters/WB.UI.Headquarters/App_GlobalResources/MainMenu.Designer.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/App_GlobalResources/MainMenu.Designer.cs
@@ -19,7 +19,7 @@ namespace Resources {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option or rebuild the Visual Studio project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Web.Application.StronglyTypedResourceProxyBuilder", "12.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Web.Application.StronglyTypedResourceProxyBuilder", "14.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class MainMenu {
@@ -57,6 +57,15 @@ namespace Resources {
             }
             set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to API Users.
+        /// </summary>
+        internal static string ApiUsers {
+            get {
+                return ResourceManager.GetString("ApiUsers", resourceCulture);
             }
         }
         
