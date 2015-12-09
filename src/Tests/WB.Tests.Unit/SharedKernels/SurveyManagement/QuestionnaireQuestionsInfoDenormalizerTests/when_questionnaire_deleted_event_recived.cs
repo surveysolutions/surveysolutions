0e9877d9cbfe87adc1a2a537cc2a3f14a8579055
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2015-12-09 18:46:41 +0200

    fixed unit tests

--- src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/QuestionnaireQuestionsInfoDenormalizerTests/when_questionnaire_deleted_event_recived.cs
+++ src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/QuestionnaireQuestionsInfoDenormalizerTests/when_questionnaire_deleted_event_recived.cs
@@ -3,6 +3,7 @@ using Machine.Specifications;
 using Moq;
 using Ncqrs.Eventing.ServiceModel.Bus;
 using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
+using WB.Core.SharedKernels.DataCollection.Implementation.Entities;
 using WB.Core.SharedKernels.DataCollection.Utils;
 using WB.Core.SharedKernels.SurveyManagement.EventHandler;
 using WB.Core.SharedKernels.SurveyManagement.EventHandler.WB.Core.SharedKernels.SurveyManagement.Views.Questionnaire;
@@ -24,7 +25,7 @@ namespace WB.Tests.Unit.SharedKernels.SurveyManagement.QuestionnaireQuestionsInf
             denormalizer.Handle(evnt);
 
         It should_view_with_id_equal_to_combination_of_questionnaireid_and_version_be_removed = () =>
-            questionnaireQuestionsInfoWriter.Verify(x => x.Remove(RepositoryKeysHelper.GetVersionedKey(evnt.EventSourceId, evnt.Payload.QuestionnaireVersion)));
+            questionnaireQuestionsInfoWriter.Verify(x => x.Remove(new QuestionnaireIdentity(evnt.EventSourceId, 2).ToString()));
 
         static Guid questionnaireId = Guid.Parse("33332222111100000000111122223333");
         static QuestionnaireQuestionsInfoDenormalizer denormalizer;
