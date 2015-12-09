0b845738f80f5acf2a18e656ab42dbe65d5f5fe4
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2015-12-09 16:59:58 -0500

    review KP-6355 HQ/SV: improvements on TabletReport/Packages page

--- src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/ServiceTests/FileBasedTabletInformationServiceTests/when_requesting_list_of_packages_and_storage_contains_a_valid_package_with_user.cs
+++ src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/ServiceTests/FileBasedTabletInformationServiceTests/when_requesting_list_of_packages_and_storage_contains_a_valid_package_with_user.cs
@@ -9,7 +9,7 @@ using WB.Core.SharedKernels.SurveyManagement.Views.TabletInformation;
 
 namespace WB.Tests.Unit.SharedKernels.SurveyManagement.ServiceTests.FileBasedTabletInformationServiceTests
 {
-    internal class when_requesting_list_of_packages_and_storage_contains_2_invalid_and_1valid_package : FileBasedTabletInformationServiceTestContext
+    internal class when_requesting_list_of_packages_and_storage_contains_a_valid_package_with_user : FileBasedTabletInformationServiceTestContext
     {
         Establish context = () =>
         {
@@ -18,13 +18,22 @@ namespace WB.Tests.Unit.SharedKernels.SurveyManagement.ServiceTests.FileBasedTab
 
         Because of = () => returnedPackages = fileBasedTabletInformationService.GetAllTabletInformationPackages();
 
-        It should_contain_1_element_in_list = () => returnedPackages.Count.ShouldEqual(3);
-        It should_android_id_be_equal_2 = () => returnedPackages[0].AndroidId.ShouldEqual("2");
+        It should_contain_1_element_in_list = () => returnedPackages.Count.ShouldEqual(1);
+        It should_android_id_be_equal_provided_value = () => returnedPackages[0].AndroidId.ShouldEqual(androidId);
         It should_registration_id_be_equal_5 = () => returnedPackages[0].RegistrationId.ShouldEqual("5");
+        It should_username_be_as_provided = () => returnedPackages[0].UserName.ShouldEqual(userName);
+        It should_userid_be_as_provided = () => returnedPackages[0].UserId.ShouldEqual(userId);
 
         private static FileBasedTabletInformationService fileBasedTabletInformationService;
 
         private static List<TabletInformationView> returnedPackages;
-        private static string[] presentFileNames = new[] { "1", "2.zip", "2@5@6.zip", "2@5@3@.zip", "3@3@6@a!er.zip"};
+
+        private static Guid userId = Guid.Parse("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        private static string userName = "test";
+        private static string androidId = "3dqda33";
+        private static string[] presentFileNames = new[]
+        {
+            string.Format("{0}@5@6@{1}!{2}.zip", androidId,userName,userId)
+        };
     }
 }
