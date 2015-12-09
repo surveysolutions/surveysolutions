990d21ea47934c2a8da268521da9c654b93649ab
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2015-12-09 16:39:42 -0500

    KP-6456 API: create page to manage users, who can send api requests

--- src/Core/SharedKernels/SurveyManagement/SurveyManagement.Web/Controllers/TeamController.cs
+++ src/Core/SharedKernels/SurveyManagement/SurveyManagement.Web/Controllers/TeamController.cs
@@ -62,13 +62,17 @@ namespace WB.Core.SharedKernels.SurveyManagement.Web.Controllers
 
         protected void UpdateAccount(UserView user, UserEditModel editModel)
         {
-            this.CommandService.Execute(new ChangeUserCommand(publicKey: user.PublicKey, email: editModel.Email,
+            this.CommandService.Execute(new ChangeUserCommand(publicKey: user.PublicKey, 
+                email: editModel.Email,
                 isLockedBySupervisor: editModel.IsLockedBySupervisor,
                 isLockedByHQ: this.GlobalInfo.IsHeadquarter || this.GlobalInfo.IsAdministrator ? editModel.IsLocked : user.IsLockedByHQ,
                 passwordHash:
                     string.IsNullOrEmpty(editModel.Password)
                         ? user.Password
-                        : passwordHasher.Hash(editModel.Password), personName:editModel.PersonName, phoneNumber:editModel.PhoneNumber, userId: this.GlobalInfo.GetCurrentUser().Id));
+                        : passwordHasher.Hash(editModel.Password), 
+                personName:editModel.PersonName, 
+                phoneNumber:editModel.PhoneNumber, 
+                userId: this.GlobalInfo.GetCurrentUser().Id));
         }
 
         private void CreateUser(UserModel user, UserRoles role, Guid? supervisorId = null)
---
+++ src/Tests/WB.Tests.Unit/Applications/Headquarters/ApiUserControllerTests/when_creating_api_user.cs
@@ -0,0 +1,44 @@
+﻿using Machine.Specifications;
+using Moq;
+using System.Web.Mvc;
+using WB.Core.Infrastructure.CommandBus;
+using WB.Core.SharedKernels.SurveyManagement.Web.Models;
+using WB.UI.Headquarters.Controllers;
+using WB.Core.SharedKernels.DataCollection.Commands.User;
+using It = Machine.Specifications.It;
+
+namespace WB.Tests.Unit.Applications.Headquarters.ApiUserControllerTests
+{
+    internal class when_creating_api_user : ApiUserControllerTestContext
+    {
+        Establish context = () =>
+        {
+            inputModel = new UserModel()
+            {
+                UserName = "apiTest",
+                Password = "12345",
+                ConfirmPassword = "12345"
+            };
+            controller = CreateApiUserController(commandServiceMock.Object);
+        };
+
+        Because of = () =>
+        {
+            actionResult = controller.Create(inputModel);
+        };
+
+        It should_return_ViewResult = () =>
+            actionResult.ShouldBeOfExactType<RedirectToRouteResult>();
+
+        It should_execute_CreateUserCommand_onece = () =>
+            commandServiceMock.Verify(x => x.Execute(Moq.It.IsAny<CreateUserCommand>(), Moq.It.IsAny<string>()),Times.Once);
+
+
+        private static Mock<ICommandService> commandServiceMock = new Mock<ICommandService>();
+        private static ActionResult actionResult ;
+        private static ApiUserController controller;
+        private static UserModel inputModel;
+    }
+
+
+}
