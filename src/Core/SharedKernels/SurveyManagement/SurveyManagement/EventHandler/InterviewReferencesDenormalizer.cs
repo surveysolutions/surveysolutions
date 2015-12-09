a2111b72164591075efb769c88b1139bfbaee2f7
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2015-12-09 12:28:09 +0200

    HQ/SV: interview references denormalizer made functional KP-6434

--- src/Core/SharedKernels/SurveyManagement/SurveyManagement/EventHandler/InterviewReferencesDenormalizer.cs
+++ src/Core/SharedKernels/SurveyManagement/SurveyManagement/EventHandler/InterviewReferencesDenormalizer.cs
@@ -1,42 +1,33 @@
 using System;
 using Ncqrs.Eventing.ServiceModel.Bus;
 using WB.Core.Infrastructure.EventBus;
+using WB.Core.Infrastructure.EventHandlers;
 using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
 using WB.Core.SharedKernels.DataCollection.Events.Interview;
 using WB.Core.SharedKernels.SurveyManagement.Views.Interview;
 
 namespace WB.Core.SharedKernels.SurveyManagement.EventHandler
 {
-    public class InterviewReferencesDenormalizer : BaseDenormalizer,
-        IEventHandler<InterviewCreated>,
-        IEventHandler<InterviewFromPreloadedDataCreated>,
-        IEventHandler<InterviewOnClientCreated>
+    public class InterviewReferencesDenormalizer : AbstractFunctionalEventHandler<InterviewReferences, IReadSideKeyValueStorage<InterviewReferences>>,
+        IUpdateHandler<InterviewReferences, InterviewCreated>,
+        IUpdateHandler<InterviewReferences, InterviewFromPreloadedDataCreated>,
+        IUpdateHandler<InterviewReferences, InterviewOnClientCreated>
     {
-        private readonly IReadSideKeyValueStorage<InterviewReferences> interviewReferencesStorage;
+        public InterviewReferencesDenormalizer(IReadSideKeyValueStorage<InterviewReferences> readSideStorage)
+            : base(readSideStorage) {}
 
-        public InterviewReferencesDenormalizer(IReadSideKeyValueStorage<InterviewReferences> interviewReferencesStorage)
+        public InterviewReferences Update(InterviewReferences state, IPublishedEvent<InterviewCreated> e)
+            => CreateInterviewReferences(e.EventSourceId, e.Payload.QuestionnaireId, e.Payload.QuestionnaireVersion);
+
+        public InterviewReferences Update(InterviewReferences state, IPublishedEvent<InterviewFromPreloadedDataCreated> e)
+            => CreateInterviewReferences(e.EventSourceId, e.Payload.QuestionnaireId, e.Payload.QuestionnaireVersion);
+
+        public InterviewReferences Update(InterviewReferences state, IPublishedEvent<InterviewOnClientCreated> e)
+            => CreateInterviewReferences(e.EventSourceId, e.Payload.QuestionnaireId, e.Payload.QuestionnaireVersion);
+
+        private static InterviewReferences CreateInterviewReferences(Guid interviewId, Guid questionnaireId, long questionnaireVersion)
         {
-            this.interviewReferencesStorage = interviewReferencesStorage;
-        }
-
-        public override object[] Writers => new object[] { this.interviewReferencesStorage };
-
-        public void Handle(IPublishedEvent<InterviewCreated> e) => this.StoreInterviewReferences(e.EventSourceId, e.Payload.QuestionnaireId, e.Payload.QuestionnaireVersion);
-
-        public void Handle(IPublishedEvent<InterviewFromPreloadedDataCreated> e) => this.StoreInterviewReferences(e.EventSourceId, e.Payload.QuestionnaireId, e.Payload.QuestionnaireVersion);
-
-        public void Handle(IPublishedEvent<InterviewOnClientCreated> e) => this.StoreInterviewReferences(e.EventSourceId, e.Payload.QuestionnaireId, e.Payload.QuestionnaireVersion);
-
-        private void StoreInterviewReferences(Guid interviewId, Guid questionnaireId, long questionnaireVersion)
-        {
-            var interviewReferences = new InterviewReferences
-            {
-                InterviewId = interviewId,
-                QuestionnaireId = questionnaireId,
-                QuestionnaireVersion = questionnaireVersion,
-            };
-
-            this.interviewReferencesStorage.Store(interviewReferences, interviewId);
+            return new InterviewReferences(interviewId, questionnaireId, questionnaireVersion);
         }
     }
 }
