15587eff394b1ddade18678499cb4081e414c281
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2015-12-10 17:36:25 -0500

    KP-6487 Shared questionnaire doesn't allow to copy from to shared person

--- src/Tests/WB.Tests.Unit/Applications/Designer/CommandInflaterTests/CommandInflaterTestsContext.cs
+++ src/Tests/WB.Tests.Unit/Applications/Designer/CommandInflaterTests/CommandInflaterTestsContext.cs
@@ -3,6 +3,7 @@ using WB.UI.Designer.Code.Implementation;
 using WB.UI.Shared.Web.Membership;
 using Main.Core.Documents;
 using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
+using WB.Core.BoundedContexts.Designer.Views.Questionnaire.SharedPersons;
 using System;
 
 namespace WB.Tests.Unit.Applications.Designer.CommandPostProcessorTests
@@ -11,16 +12,18 @@ namespace WB.Tests.Unit.Applications.Designer.CommandPostProcessorTests
     {
         protected static CommandInflater CreateCommandInflater(
             IMembershipUserService userHelper = null,
-            IReadSideKeyValueStorage<QuestionnaireDocument> storage = null)
+            IReadSideKeyValueStorage<QuestionnaireDocument> storage = null,
+            IReadSideKeyValueStorage<QuestionnaireSharedPersons> sharedPersons = null)
         {
             return new CommandInflater(
                 userHelper ?? Mock.Of<IMembershipUserService>(),
-                storage ?? Mock.Of<IReadSideKeyValueStorage<QuestionnaireDocument>>());
+                storage ?? Mock.Of<IReadSideKeyValueStorage<QuestionnaireDocument>>(),
+                sharedPersons ?? Mock.Of<IReadSideKeyValueStorage<QuestionnaireSharedPersons>>());
         }
 
-        protected static QuestionnaireDocument CreateQuestionnaireDocument(Guid questoinnaireId, string title, Guid creator)
+        protected static QuestionnaireDocument CreateQuestionnaireDocument(Guid questoinnaireId, string title, Guid creator, bool isPublic = true)
         {
-            return new QuestionnaireDocument() {PublicKey = questoinnaireId, Title = title, CreatedBy = creator, IsPublic = true};
+            return new QuestionnaireDocument() {PublicKey = questoinnaireId, Title = title, CreatedBy = creator, IsPublic = isPublic};
         }
     }
 }
