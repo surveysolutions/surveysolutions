be6c5ce83aa92ed9bd27210f70f0b9222f75b6ce
Anastasya Kholodova <nastya.kholodova@gmail.com>
2015-12-11 18:51:34 -0500

    Interviewer\Diagnostics: KP-6476 check for new version

--- src/UI/Shared/WB.UI.Shared.Enumerator/CustomServices/ExternalAppLauncher.cs
+++ src/UI/Shared/WB.UI.Shared.Enumerator/CustomServices/ExternalAppLauncher.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Globalization;
+using System.IO;
+using System.Net;
 using Android.App;
 using Android.Content;
 using Cirrious.CrossCore;
@@ -31,6 +33,30 @@ namespace WB.UI.Shared.Enumerator.CustomServices
             shareIntent.PutExtra(Intent.ExtraText, info);
             shareIntent.SetType("text/plain");
             this.CurrentActivity.StartActivity(Intent.CreateChooser(shareIntent, title));
+        }
+
+        public void UpdateTheApp(string url)
+        {
+            var applicationFileName = "interviewer.apk";
+
+            string pathTofile =
+                Path.Combine(
+                    Path.Combine(global::Android.OS.Environment.ExternalStorageDirectory.AbsolutePath, "download"),
+                    applicationFileName);
+            // generate unique name instead of delete
+            if (File.Exists(pathTofile))
+                File.Delete(pathTofile);
+
+            var client = new WebClient();
+            var uri = new Uri(new Uri(url), "/api/InterviewerSync/GetLatestVersion");
+            client.DownloadFile(uri, pathTofile);
+
+            Intent promptInstall =
+                new Intent(Intent.ActionView).SetDataAndType(
+                    global::Android.Net.Uri.FromFile(new Java.IO.File(pathTofile)),
+                    "application/vnd.android.package-archive");
+            promptInstall.AddFlags(ActivityFlags.NewTask);
+            Application.Context.StartActivity(promptInstall);
         }
     }
 }
