01004d21287134919aaa8a88fdb8d8e642b14a19
Pavel Stovpivskyi <sby@rambler.ru>
2015-12-11 10:08:41 +0200

    Removed old interviewers pages
    KP-6441

---
+++ src/Core/SharedKernels/SurveyManagement/SurveyManagement.Web/Controllers/InterviewersController.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Mvc;
+using WB.Core.GenericSubdomains.Portable;
+using WB.Core.GenericSubdomains.Portable.Services;
+using WB.Core.Infrastructure.CommandBus;
+using WB.Core.SharedKernels.SurveyManagement.Views.User;
+using WB.Core.SharedKernels.SurveyManagement.Web.Filters;
+using WB.Core.SharedKernels.SurveyManagement.Web.Models;
+using WB.Core.SharedKernels.SurveyManagement.Web.Utils.Membership;
+
+namespace WB.Core.SharedKernels.SurveyManagement.Web.Controllers
+{
+    [ValidateInput(false)]
+    [Authorize(Roles = "Administrator, Headquarter, Supervisor, Observer")]
+    public class InterviewersController : TeamController
+    {
+        public InterviewersController(ICommandService commandService, IGlobalInfoProvider globalInfo, 
+            ILogger logger, IUserViewFactory userViewFactory, IPasswordHasher passwordHasher)
+            : base(commandService, globalInfo, logger, userViewFactory, passwordHasher)
+        {
+        }
+
+        public ActionResult Index(InterviewersFilter filter)
+        {
+            InterviewersModel pageModel = new InterviewersModel();
+            return this.View(pageModel);
+        }
+    }
+}
