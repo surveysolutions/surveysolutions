6426594ed69861af19e1893508dff5e9b6d3bd44
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2015-12-16 14:16:53 +0200

    InterviewDataExportView is now stored in DB as independent records

--- src/Core/SharedKernels/SurveyManagement/SurveyManagement/Views/DataExport/InterviewDataExportLevelView.cs
+++ src/Core/SharedKernels/SurveyManagement/SurveyManagement/Views/DataExport/InterviewDataExportLevelView.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using WB.Core.SharedKernels.DataCollection.ValueObjects;
 
 namespace WB.Core.SharedKernels.SurveyManagement.Views.DataExport
--- src/Core/SharedKernels/SurveyManagement/SurveyManagement/Views/DataExport/InterviewDataExportView.cs
+++ src/Core/SharedKernels/SurveyManagement/SurveyManagement/Views/DataExport/InterviewDataExportView.cs
@@ -1,13 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using WB.Core.SharedKernels.SurveySolutions;
 
 namespace WB.Core.SharedKernels.SurveyManagement.Views.DataExport
 {
     public class InterviewDataExportView : IReadSideRepositoryEntity
     {
-        public InterviewDataExportView(InterviewDataExportLevelView[] levels)
+        private InterviewDataExportView(InterviewDataExportLevelView[] levels)
         {
+            this.Levels = levels;
+        }
+
+        public InterviewDataExportView(Guid interviewId, InterviewDataExportLevelView[] levels)
+        {
+            this.InterviewId = interviewId;
             this.Levels = levels;
         }
 
@@ -16,5 +23,29 @@ namespace WB.Core.SharedKernels.SurveyManagement.Views.DataExport
         public InterviewDataExportLevelView[] Levels { get; set; }
 
         public IList<InterviewDataExportRecord> Records { get; private set; }
+
+        public static InterviewDataExportView CreateFromRecords(List<InterviewDataExportRecord> records)
+        {
+            Guid interviewId = records.Select(record => record.InterviewId).Distinct().Single();
+
+            InterviewDataExportLevelView[] levels = records
+                .GroupBy(record => record.LevelName)
+                .Select(grouping => new InterviewDataExportLevelView(null, grouping.Key, grouping.ToArray()))
+                .ToArray();
+
+            return new InterviewDataExportView(interviewId, levels);
+        }
+
+        public IEnumerable<InterviewDataExportRecord> GetAsRecords()
+        {
+            foreach (var level in this.Levels)
+                foreach (var record in level.Records)
+                {
+                    record.InterviewId = this.InterviewId;
+                    record.LevelName = level.LevelName;
+
+                    yield return record;
+                }
+        }
     }
 }
