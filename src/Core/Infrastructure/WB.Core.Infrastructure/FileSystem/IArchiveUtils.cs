05a921868fab973997c0896d932781269c8730ae
Roman Lytvyn <litvin.roma@gmail.com>
2015-12-16 12:52:40 +0200

    HQ: import interviews by prefilled questions speedup

--- src/Core/Infrastructure/WB.Core.Infrastructure/FileSystem/IArchiveUtils.cs
+++ src/Core/Infrastructure/WB.Core.Infrastructure/FileSystem/IArchiveUtils.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.IO;
 
 namespace WB.Core.Infrastructure.FileSystem
 {
@@ -8,9 +9,18 @@ namespace WB.Core.Infrastructure.FileSystem
         byte[] ZipDirectoryToByteArray(string sourceDirectory, string directoryFilter = null, string fileFilter = null);
         void ZipFiles(IEnumerable<string> files, string archiveFilePath);
         void Unzip(string archivedFile, string extractToFolder, bool ignoreRootDirectory = false);
+        IEnumerable<UnzippedFile> UnzipStream(Stream zipStream);
         bool IsZipFile(string filePath);
+
+        bool IsZipStream(Stream zipStream);
         Dictionary<string, long> GetArchivedFileNamesAndSize(string filePath);
         string CompressString(string stringToCompress);
         string DecompressString(string stringToDecompress);
+    }
+
+    public class UnzippedFile
+    {
+        public string FileName { get; set; }
+        public Stream FileStream { get; set; }
     }
 }
