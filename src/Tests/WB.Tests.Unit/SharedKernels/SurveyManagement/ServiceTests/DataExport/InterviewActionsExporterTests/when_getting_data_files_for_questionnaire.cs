690d385a3fa68a8b0a897df3aafdb2e9b86f5d75
Andrii Kozhyn <hedgic@gmail.com>
2015-12-18 13:04:02 +0200

    KP-6471: fixing unit tests

--- src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/ServiceTests/DataExport/InterviewActionsExporterTests/when_getting_data_files_for_questionnaire.cs
+++ src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/ServiceTests/DataExport/InterviewActionsExporterTests/when_getting_data_files_for_questionnaire.cs
@@ -5,20 +5,18 @@ using System.Linq;
 using System.Threading;
 using Machine.Specifications;
 using Moq;
-using Nito.AsyncEx.Synchronous;
 using WB.Core.BoundedContexts.Headquarters.DataExport.Factories;
 using WB.Core.BoundedContexts.Headquarters.DataExport.Services;
+using WB.Core.BoundedContexts.Headquarters.DataExport.Services.Exporters;
 using WB.Core.Infrastructure.FileSystem;
-using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
 using WB.Core.SharedKernels.DataCollection.Implementation.Entities;
 using WB.Core.SharedKernels.DataCollection.ValueObjects;
-using WB.Core.SharedKernels.SurveyManagement.Services;
 using WB.Core.SharedKernels.SurveyManagement.Views.Interview;
 using It = Machine.Specifications.It;
 
-namespace WB.Tests.Unit.SharedKernels.SurveyManagement.ServiceTests.DataExport.ReadSideToTabularFormatExportServiceTests
+namespace WB.Tests.Unit.SharedKernels.SurveyManagement.ServiceTests.DataExport.InterviewActionsExporterTests
 {
-    internal class when_getting_data_files_for_questionnaire : ReadSideToTabularFormatExportServiceTestContext
+    internal class when_getting_data_files_for_questionnaire : InterviewActionsExporterTestsContext
     {
         Establish context = () =>
         {
@@ -40,17 +38,17 @@ namespace WB.Tests.Unit.SharedKernels.SurveyManagement.ServiceTests.DataExport.R
             var headerStructureForLevel = Create.HeaderStructureForLevel();
             headerStructureForLevel.LevelName = "1";
             questionnaireExportStructure.HeaderToLevelMap.Add(new ValueVector<Guid>(), headerStructureForLevel);
-            readSideToTabularFormatExportService = CreateReadSideToTabularFormatExportService(csvWriter: csvWriterMock.Object,
+            readSideToTabularFormatExportService = CreateExporter(csvWriter: csvWriterMock.Object,
                 fileSystemAccessor: fileSystemAccessor.Object, interviewStatuses: interviewStatuses, questionnaireExportStructure: questionnaireExportStructure);
         };
 
         Because of = () =>
-            readSideToTabularFormatExportService.ExportInterviewsInTabularFormat(new QuestionnaireIdentity(questionnaireId, questionnaireVersion), "", new Progress<int>(), CancellationToken.None);
+            readSideToTabularFormatExportService.ExportAll(new QuestionnaireIdentity(questionnaireId, questionnaireVersion), "", new Progress<int>());
 
         It should_record_one_completed_action = () =>
            csvWriterMock.Verify(x=>x.WriteData(Moq.It.IsAny<string>(),Moq.It.Is<IEnumerable<string[]>>(s=>s.Any(c=>c.Contains("Completed"))), Moq.It.IsAny<string>()), Times.Once);
 
-        private static ReadSideToTabularFormatExportService readSideToTabularFormatExportService;
+        private static InterviewActionsExporter readSideToTabularFormatExportService;
         private static Guid questionnaireId = Guid.Parse("11111111111111111111111111111111");
         private static long questionnaireVersion = 3;
         private static Mock<IFileSystemAccessor> fileSystemAccessor;
