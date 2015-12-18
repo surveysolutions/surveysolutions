685baf7e39868a0d93a809875e251ceb966f2907
Slava Scherbak <slava.scherbak@gmail.com>
2015-12-18 12:44:15 -0500

    Fixed typo in error message, fixed bug with preloading missing values. KP-6465

--- src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Resources/ExceptionMessages.Designer.cs
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Resources/ExceptionMessages.Designer.cs
@@ -214,7 +214,7 @@ namespace WB.Core.BoundedContexts.Designer.Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Too many columns in uploaded file. Max coumns count is {0}.
+        ///   Looks up a localized string similar to Too many columns in uploaded file. Max columns count is {0}.
         /// </summary>
         internal static string LookupTables_too_many_columns {
             get {
