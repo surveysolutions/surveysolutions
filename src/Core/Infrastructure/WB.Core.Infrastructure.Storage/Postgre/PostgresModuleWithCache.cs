d0fa663437d5d107ac6132ef92b3ad769bc8ccba
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2015-12-22 18:23:41 +0200

    PostgresModule: added choosing between esent and memory cache depending on cache settings (which are now hardcoded) KP-6471

--- src/Core/Infrastructure/WB.Core.Infrastructure.Storage/Postgre/PostgresModuleWithCache.cs
+++ src/Core/Infrastructure/WB.Core.Infrastructure.Storage/Postgre/PostgresModuleWithCache.cs
@@ -6,6 +6,7 @@ using Ninject.Modules;
 using WB.Core.Infrastructure.FileSystem;
 using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
 using WB.Core.Infrastructure.Storage.Esent.Implementation;
+using WB.Core.Infrastructure.Storage.Memory.Implementation;
 using WB.Core.SharedKernels.SurveySolutions;
 
 namespace WB.Core.Infrastructure.Storage.Postgre
@@ -27,7 +28,11 @@ namespace WB.Core.Infrastructure.Storage.Postgre
             Type storageEntityType = context.GenericArguments[0];
 
             MethodInfo createCachingStorageMethod = typeof(PostgresModuleWithCache)
-                .GetMethod(nameof(CreateEsentCachedReadSideStorage), BindingFlags.NonPublic | BindingFlags.Instance)
+                .GetMethod(
+                    this.cacheSettings.EnableEsentCache
+                        ? nameof(this.CreateEsentCachedReadSideStorage)
+                        : nameof(this.CreateMemoryCachedReadSideStorage),
+                    BindingFlags.NonPublic | BindingFlags.Instance)
                 .MakeGenericMethod(storageEntityType);
 
             return createCachingStorageMethod.Invoke(this, new object[] { context });
@@ -40,6 +45,14 @@ namespace WB.Core.Infrastructure.Storage.Postgre
             var fileSystemAccessor = context.Kernel.Get<IFileSystemAccessor>();
 
             return new EsentCachedReadSideStorage<TEntity>(postgresStorage, fileSystemAccessor, this.cacheSettings);
+        }
+
+        private MemoryCachedReadSideStorage<TEntity> CreateMemoryCachedReadSideStorage<TEntity>(IContext context)
+            where TEntity : class, IReadSideRepositoryEntity
+        {
+            IReadSideStorage<TEntity> postgresStorage = this.GetPostgresReadSideStorage<TEntity>(context);
+
+            return new MemoryCachedReadSideStorage<TEntity>(postgresStorage, this.cacheSettings);
         }
 
         public override void Load()
