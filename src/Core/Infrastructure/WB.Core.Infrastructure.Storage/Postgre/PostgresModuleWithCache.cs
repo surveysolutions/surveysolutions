c6d058fb783852238b196d60fe1a2f69dc5d75d1
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2015-12-22 17:48:55 +0200

    PostgresModule: prepared for work with different caches KP-6471

--- src/Core/Infrastructure/WB.Core.Infrastructure.Storage/Postgre/PostgresModuleWithCache.cs
+++ src/Core/Infrastructure/WB.Core.Infrastructure.Storage/Postgre/PostgresModuleWithCache.cs
@@ -1,9 +1,12 @@
 using System;
+using System.Reflection;
 using Ninject;
 using Ninject.Activation;
 using Ninject.Modules;
 using WB.Core.Infrastructure.FileSystem;
+using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
 using WB.Core.Infrastructure.Storage.Esent.Implementation;
+using WB.Core.SharedKernels.SurveySolutions;
 
 namespace WB.Core.Infrastructure.Storage.Postgre
 {
@@ -16,19 +19,27 @@ namespace WB.Core.Infrastructure.Storage.Postgre
             this.cacheSettings = cacheSettings;
         }
 
-        protected abstract object GetPostgresReadSideStorage(IContext context);
+        protected abstract IReadSideStorage<TEntity> GetPostgresReadSideStorage<TEntity>(IContext context)
+            where TEntity : class, IReadSideRepositoryEntity;
 
         protected object GetReadSideStorageWrappedWithCache(IContext context)
         {
-            object postgresStorage = this.GetPostgresReadSideStorage(context);
+            Type storageEntityType = context.GenericArguments[0];
+
+            MethodInfo createCachingStorageMethod = typeof(PostgresModuleWithCache)
+                .GetMethod(nameof(CreateEsentCachedReadSideStorage), BindingFlags.NonPublic | BindingFlags.Instance)
+                .MakeGenericMethod(storageEntityType);
+
+            return createCachingStorageMethod.Invoke(this, new object[] { context });
+        }
+
+        private EsentCachedReadSideStorage<TEntity> CreateEsentCachedReadSideStorage<TEntity>(IContext context)
+            where TEntity : class, IReadSideRepositoryEntity
+        {
+            IReadSideStorage<TEntity> postgresStorage = this.GetPostgresReadSideStorage<TEntity>(context);
             var fileSystemAccessor = context.Kernel.Get<IFileSystemAccessor>();
 
-            Type cachingStorageType = typeof(EsentCachedReadSideStorage<>).MakeGenericType(context.GenericArguments[0]);
-
-            object cachingStorage = Activator.CreateInstance(cachingStorageType,
-                postgresStorage, fileSystemAccessor, this.cacheSettings);
-
-            return cachingStorage;
+            return new EsentCachedReadSideStorage<TEntity>(postgresStorage, fileSystemAccessor, this.cacheSettings);
         }
 
         public override void Load()
