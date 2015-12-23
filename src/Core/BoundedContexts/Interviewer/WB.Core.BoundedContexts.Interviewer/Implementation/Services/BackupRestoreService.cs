ba5d8c2d3de1f280817352fcb140e254c492b54f
Anastasya Kholodova <nastya.kholodova@gmail.com>
2015-12-23 16:50:09 -0500

    Interviewer\Diagnostics: KP-6510 add Asynchronous file system acessor

--- src/Core/BoundedContexts/Interviewer/WB.Core.BoundedContexts.Interviewer/Implementation/Services/BackupRestoreService.cs
+++ src/Core/BoundedContexts/Interviewer/WB.Core.BoundedContexts.Interviewer/Implementation/Services/BackupRestoreService.cs
@@ -8,13 +8,13 @@ namespace WB.Core.BoundedContexts.Interviewer.Implementation.Services
     public class BackupRestoreService : IBackupRestoreService
     {
         private readonly IArchiveUtils archiver;
-        private readonly IFileSystemAccessor fileSystemAccessor;
+        private readonly IAsynchronousFileSystemAccessor fileSystemAccessor;
         private readonly IInterviewerSettings interviewerSettings;
         private readonly string privateStorage;
 
         public BackupRestoreService(
             IArchiveUtils archiver,
-            IFileSystemAccessor fileSystemAccessor, 
+            IAsynchronousFileSystemAccessor fileSystemAccessor, 
             IInterviewerSettings interviewerSettings, 
             string privateStorage)
         {
@@ -24,41 +24,36 @@ namespace WB.Core.BoundedContexts.Interviewer.Implementation.Services
             this.privateStorage = privateStorage;
         }
 
-        public Task<byte[]> GetSystemBackupAsync()
+        public async Task<byte[]> GetSystemBackupAsync()
         {
-            return Task.Run(() =>
-            {
-                var pathToCrushFile = this.interviewerSettings.CrushFilePath;
+            var pathToCrushFile = this.interviewerSettings.CrushFilePath;
 
-                if (this.fileSystemAccessor.IsFileExists(pathToCrushFile))
-                    this.fileSystemAccessor.CopyFileOrDirectory(pathToCrushFile, privateStorage);
+            if (await this.fileSystemAccessor.IsFileExistsAsync(pathToCrushFile))
+                await this.fileSystemAccessor.CopyFileAsync(pathToCrushFile, privateStorage);
 
-                return this.archiver.ZipDirectoryToByteArray(privateStorage,
-                    fileFilter: @"\.log$;\.dll$;\.mdb$;");
-            });
+            return await this.archiver.ZipDirectoryToByteArrayAsync(privateStorage,
+                fileFilter: @"\.log$;\.dll$;\.mdb$;");
         }
 
         public async Task<string> BackupAsync(string backupToFolderPath)
         {
             var backupFileName = $"backup-interviewer-{DateTime.Now.ToString("yyyyMMddTH-mm-ss")}.ibak";
             var backup = await this.GetSystemBackupAsync();
-            var emptyBackupFile =
-                this.fileSystemAccessor.CombinePath(backupToFolderPath, backupFileName);
-            await Task.Run(() =>
-            {
-                if (!this.fileSystemAccessor.IsDirectoryExists(backupToFolderPath))
-                    this.fileSystemAccessor.CreateDirectory(backupToFolderPath);
+            var emptyBackupFile = this.fileSystemAccessor.CombinePath(backupToFolderPath, backupFileName);
 
-                this.fileSystemAccessor.WriteAllBytes(emptyBackupFile, backup);
-            });
+            var isBackupFolderExists = await this.fileSystemAccessor.IsDirectoryExistsAsync(backupToFolderPath);
+            if (!isBackupFolderExists)
+                await this.fileSystemAccessor.CreateDirectoryAsync(backupToFolderPath);
+
+            await this.fileSystemAccessor.WriteAllBytesAsync(emptyBackupFile, backup);
             return emptyBackupFile;
         }
 
         public async Task RestoreAsync(string backupFilePath)
         {
-            if (this.fileSystemAccessor.IsFileExists(backupFilePath))
+            if (await this.fileSystemAccessor.IsFileExistsAsync(backupFilePath))
             {
-                await Task.Run(() => this.archiver.Unzip(backupFilePath, privateStorage, true));
+                await this.archiver.UnzipAsync(backupFilePath, privateStorage, true);
             }
         }
     }
---
+++ src/Infrastructure/Shared/Enumerator/WB.Infrastructure.Shared.Enumerator/Internals/FileSystem/AsynchronousFileSystemAccessor.cs
@@ -0,0 +1,69 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using WB.Core.Infrastructure.FileSystem;
+using PCLStorage;
+
+namespace WB.Infrastructure.Shared.Enumerator.Internals.FileSystem
+{
+    internal class AsynchronousFileSystemAccessor: IAsynchronousFileSystemAccessor
+    {
+        public async Task WriteAllBytesAsync(string pathToFile, byte[] content)
+        {
+            var parentFolderPath = Directory.GetParent(pathToFile).FullName;
+            var parentFolder = await PCLStorage.FileSystem.Current.GetFolderFromPathAsync(parentFolderPath);
+
+            var emptyBackupFile = await parentFolder.CreateFileAsync(Path.GetFileName(pathToFile), CreationCollisionOption.GenerateUniqueName);
+
+            using (var stream = await emptyBackupFile.OpenAsync(PCLStorage.FileAccess.ReadAndWrite))
+            {
+                stream.Write(content, 0, content.Length);
+            }
+        }
+
+        public async Task CopyFileAsync(string sourceDir, string targetDir)
+        {
+            var parentFolderPath = Directory.GetParent(targetDir).FullName;
+            var parentFolder = await PCLStorage.FileSystem.Current.GetFolderFromPathAsync(parentFolderPath);
+
+            var copyOfTheFile = await parentFolder.CreateFileAsync(Path.GetFileName(sourceDir), CreationCollisionOption.GenerateUniqueName);
+            var originalFile = await PCLStorage.FileSystem.Current.GetFileFromPathAsync(sourceDir);
+
+            using (var originalStream = await originalFile.OpenAsync(PCLStorage.FileAccess.Read))
+            {
+                using (var copyStream = await copyOfTheFile.OpenAsync(PCLStorage.FileAccess.ReadAndWrite))
+                {
+                    await originalStream.CopyToAsync(copyStream);
+                }
+            }
+        }
+
+        public async Task CreateDirectoryAsync(string path)
+        {
+            var parentFolderPath = Directory.GetParent(path).FullName;
+            var parentFolder = await PCLStorage.FileSystem.Current.GetFolderFromPathAsync(parentFolderPath);
+            await parentFolder.CreateFolderAsync(Path.GetDirectoryName(path), CreationCollisionOption.FailIfExists);
+        }
+
+        public async Task<bool> IsDirectoryExistsAsync(string pathToDirectory)
+        {
+            var parentFolderPath = Directory.GetParent(pathToDirectory).FullName;
+            var parentFolder = await PCLStorage.FileSystem.Current.GetFolderFromPathAsync(parentFolderPath);
+            return (await parentFolder.CheckExistsAsync(Path.GetDirectoryName(pathToDirectory))) ==
+                   ExistenceCheckResult.FolderExists;
+        }
+
+        public async Task<bool> IsFileExistsAsync(string pathToFile)
+        {
+            var parentFolderPath = Directory.GetParent(pathToFile).FullName;
+            var parentFolder = await PCLStorage.FileSystem.Current.GetFolderFromPathAsync(parentFolderPath);
+
+            return (await parentFolder.CheckExistsAsync(Path.GetFileName(pathToFile))) ==
+                  ExistenceCheckResult.FileExists;
+        }
+
+        public string CombinePath(string path1, string path2)
+        {
+            return PortablePath.Combine(path1, path2);
+        }
+    }
+}
