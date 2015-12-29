da6fd2386bbb890a87118cfff08ab9716524d2f5
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2015-12-29 11:48:43 +0200

    Use dapper for export. Changed default settings for exporter

---
+++ src/Tests/WB.Tests.Unit/GenericSubdomains/Utils/ProgressAggregator/when_reporting_progress_from_two_progresses.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Machine.Specifications;
+using WB.Core.GenericSubdomains.Portable.Tasks;
+
+namespace WB.Tests.Unit.GenericSubdomains.Utils.ProgressAggregator
+{
+    public class when_reporting_progress_from_two_progresses
+    {
+        Establish context = () =>
+        {
+            proggressAggregator = new ProggressAggregator();
+
+            progress1 = new Progress<int>();
+            proggressAggregator.Add((Progress<int>) progress1, 0.5);
+
+            progress2 = new Progress<int>();
+            proggressAggregator.Add((Progress<int>)progress2, 0.5);
+
+            proggressAggregator.ProgressChanged += (sender, progress) =>
+              reportedProgress = progress;
+        };
+
+        Because of = () =>
+        {
+            progress1.Report(100);
+            progress2.Report(50);
+        };
+
+        It should_report_something = () => { reportedProgress.ShouldEqual(75); };
+
+        private static ProggressAggregator proggressAggregator;
+        private static IProgress<int> progress1;
+        private static IProgress<int> progress2;
+        private static int reportedProgress;
+    }
+}
