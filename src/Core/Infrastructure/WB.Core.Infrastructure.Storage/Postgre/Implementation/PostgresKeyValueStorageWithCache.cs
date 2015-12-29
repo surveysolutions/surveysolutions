a95f7a82e7d607fdbffbb672ab82e02a75ae5018
Pavel Stovpivskyi <630198+spuv@users.noreply.github.com>
2015-12-29 18:40:14 +0200

    Make memoryCache as static

--- src/Core/Infrastructure/WB.Core.Infrastructure.Storage/Postgre/Implementation/PostgresKeyValueStorageWithCache.cs
+++ src/Core/Infrastructure/WB.Core.Infrastructure.Storage/Postgre/Implementation/PostgresKeyValueStorageWithCache.cs
@@ -14,51 +14,49 @@ namespace WB.Core.Infrastructure.Storage.Postgre.Implementation
     internal abstract class PostgresKeyValueStorageWithCache<TEntity> : PostgresKeyValueStorage<TEntity>
         where TEntity: class
     {
-        MemoryCache memoryCache;
-        readonly string cacheKey = "K/V memory cache";
+        static MemoryCache memoryCache = new MemoryCache(typeof(TEntity).Name + " K/V memory cache");
 
         public PostgresKeyValueStorageWithCache(string connectionString, ILogger logger)
             : base(connectionString, logger)
         {
-            memoryCache = new MemoryCache(cacheKey);
         }
 
 
         public override TEntity GetById(string id)
         {
-            var value = this.memoryCache.Get(id) as TEntity;
+            var value = memoryCache.Get(id) as TEntity;
             if (value != null)
                 return value;
 
             value = base.GetById(id);
             if (value != null)
-                this.memoryCache.Add(id, value, DateTimeOffset.Now.AddSeconds(30));
+                memoryCache.Add(id, value, DateTimeOffset.Now.AddSeconds(30));
 
             return value;
         }
 
         public override void Remove(string id)
         {
-            this.memoryCache.Remove(id);
+            memoryCache.Remove(id);
             base.Remove(id);
         }
 
         public override void Store(TEntity view, string id)
         {
-            this.memoryCache.Remove(id);
+            memoryCache.Remove(id);
             base.Store(view, id);
         }
 
         public override void BulkStore(List<Tuple<TEntity, string>> bulk)
         {
-            bulk.ForEach(i => this.memoryCache.Remove(i.Item2));
+            bulk.ForEach(i => memoryCache.Remove(i.Item2));
             base.BulkStore(bulk);
         }
 
         public override void Clear()
         {
-            this.memoryCache.Dispose();
-            this.memoryCache = new MemoryCache(cacheKey);
+            memoryCache.Dispose();
+            memoryCache = new MemoryCache(typeof(TEntity).Name + " K/V memory cache");
             base.Clear();
         }
 
