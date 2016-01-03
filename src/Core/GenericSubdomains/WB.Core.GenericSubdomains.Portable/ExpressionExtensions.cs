11478cd6b25ba7543b45eb04e6f4681b863ad7ee
Roman Lytvyn <litvin.roma@gmail.com>
2016-01-03 02:09:14 +0200

    HQ: Added double underscores for preloaded data KP-6531

---
+++ src/Core/GenericSubdomains/WB.Core.GenericSubdomains.Portable/ExpressionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace WB.Core.GenericSubdomains.Portable
+{
+    public static class ExpressionExtensions
+    {
+        public static string GetName<TSource, TField>(Expression<Func<TSource, TField>> Field)
+        {
+            return (Field.Body as MemberExpression ?? ((UnaryExpression)Field.Body).Operand as MemberExpression).Member.Name;
+        }
+    }
+}
