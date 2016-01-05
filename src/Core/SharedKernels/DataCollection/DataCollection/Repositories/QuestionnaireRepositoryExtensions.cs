d0ce1025720b8d3592edfbfefd50ccf7eae5dd58
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2016-01-05 20:44:46 +0200

    QuestionnaireModel: replaced usage of GroupsRosterLevelDepth with IQuestionnaire methods, removed GroupsRosterLevelDepth KP-6527

---
+++ src/Core/SharedKernels/DataCollection/DataCollection/Repositories/QuestionnaireRepositoryExtensions.cs
@@ -0,0 +1,11 @@
+﻿using WB.Core.SharedKernels.DataCollection.Aggregates;
+using WB.Core.SharedKernels.DataCollection.Implementation.Entities;
+
+namespace WB.Core.SharedKernels.DataCollection.Repositories
+{
+    public static class QuestionnaireRepositoryExtensions
+    {
+        public static IQuestionnaire GetQuestionnaire(this IQuestionnaireRepository repository, QuestionnaireIdentity identity)
+            => repository.GetHistoricalQuestionnaire(identity.QuestionnaireId, identity.Version);
+    }
+}
