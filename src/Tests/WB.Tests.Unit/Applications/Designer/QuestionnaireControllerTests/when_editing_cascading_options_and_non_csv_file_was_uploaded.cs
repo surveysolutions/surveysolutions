f46a5eebcaf5a3d18e5a718ba68fd6253d3feaa6
Roman Lytvyn <litvin.roma@gmail.com>
2016-01-05 19:11:10 +0200

    fixed build by tests after updating csv helper in unit tests project.

--- src/Tests/WB.Tests.Unit/Applications/Designer/QuestionnaireControllerTests/when_editing_cascading_options_and_non_csv_file_was_uploaded.cs
+++ src/Tests/WB.Tests.Unit/Applications/Designer/QuestionnaireControllerTests/when_editing_cascading_options_and_non_csv_file_was_uploaded.cs
@@ -1,6 +1,7 @@
 ﻿using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
+using System.Text;
 using System.Web;
 using Machine.Specifications;
 using Moq;
@@ -17,15 +18,8 @@ namespace WB.Tests.Unit.Applications.Designer.QuestionnaireControllerTests
             controller = CreateQuestionnaireController();
             SetControllerContextWithSession(controller, "options", new QuestionnaireController.EditOptionsViewModel());
 
-            stream = new MemoryStream();
-            bmp = new Bitmap(10, 10);
-
-            var graphics = Graphics.FromImage(bmp);
-            graphics.FillRectangle(Brushes.Orange, 0, 0, 10, 10);
-            bmp.Save(stream, ImageFormat.Jpeg);
-
-            stream.Position = 0;
-            postedFile = Mock.Of<HttpPostedFileBase>(pf => pf.InputStream == stream && pf.FileName == "image.jpeg");
+            var stream = new MemoryStream(Encoding.UTF8.GetBytes("Simple text file")) {Position = 0};
+            postedFile = Mock.Of<HttpPostedFileBase>(pf => pf.InputStream == stream);
         };
 
         Because of = () => controller.EditCascadingOptions(postedFile);
@@ -33,15 +27,7 @@ namespace WB.Tests.Unit.Applications.Designer.QuestionnaireControllerTests
         It should_add_error_message_to_temp_data = () =>
             controller.TempData[Alerts.ERROR].ShouldEqual("Only tab-separated values files are accepted");
 
-        Cleanup stuff = () =>
-        {
-            stream.Dispose();
-            bmp.Dispose();
-        };
-
         private static QuestionnaireController controller;
         private static HttpPostedFileBase postedFile;
-        private static Stream stream = new MemoryStream();
-        private static Bitmap bmp;
     }
 }
