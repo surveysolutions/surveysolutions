188b0e7ddd91fd2bde72360bd777efa37dbf7700
Pavel Stovpivskyi <630198+spuv@users.noreply.github.com>
2016-01-06 13:04:22 +0200

    Added lock

--- src/Core/Infrastructure/WB.Core.Infrastructure.Storage/Postgre/Implementation/PostgresKeyValueStorageWithCache.cs
+++ src/Core/Infrastructure/WB.Core.Infrastructure.Storage/Postgre/Implementation/PostgresKeyValueStorageWithCache.cs
@@ -14,6 +14,8 @@ namespace WB.Core.Infrastructure.Storage.Postgre.Implementation
     internal abstract class PostgresKeyValueStorageWithCache<TEntity> : PostgresKeyValueStorage<TEntity>
         where TEntity: class
     {
+        private object lockObject = new object();
+
         static MemoryCache memoryCache = new MemoryCache(typeof(TEntity).Name + " K/V memory cache");
 
         public PostgresKeyValueStorageWithCache(string connectionString, ILogger logger)
@@ -24,40 +26,55 @@ namespace WB.Core.Infrastructure.Storage.Postgre.Implementation
 
         public override TEntity GetById(string id)
         {
-            var value = memoryCache.Get(id) as TEntity;
-            if (value != null)
+            lock (lockObject)
+            {
+                var value = memoryCache.Get(id) as TEntity;
+                if (value != null)
+                    return value;
+
+                value = base.GetById(id);
+                if (value != null)
+                    memoryCache.Set(id, value, DateTimeOffset.Now.AddSeconds(10));
+
                 return value;
-
-            value = base.GetById(id);
-            if (value != null)
-                memoryCache.Set(id, value, DateTimeOffset.Now.AddSeconds(10));
-
-            return value;
+            }
         }
 
         public override void Remove(string id)
         {
-            memoryCache.Remove(id);
-            base.Remove(id);
+            lock (lockObject)
+            {
+                memoryCache.Remove(id);
+                base.Remove(id);
+            }
         }
 
         public override void Store(TEntity view, string id)
         {
-            memoryCache.Set(id, view, DateTimeOffset.Now.AddSeconds(10));
-            base.Store(view, id);
+            lock (lockObject)
+            {
+                memoryCache.Set(id, view, DateTimeOffset.Now.AddSeconds(10));
+                base.Store(view, id);
+            }
         }
 
         public override void BulkStore(List<Tuple<TEntity, string>> bulk)
         {
-            bulk.ForEach(i => memoryCache.Remove(i.Item2));
-            base.BulkStore(bulk);
+            lock (lockObject)
+            {
+                bulk.ForEach(i => memoryCache.Remove(i.Item2));
+                base.BulkStore(bulk);
+            }
         }
 
         public override void Clear()
         {
-            memoryCache.Dispose();
-            memoryCache = new MemoryCache(typeof(TEntity).Name + " K/V memory cache");
-            base.Clear();
+            lock (lockObject)
+            {
+                memoryCache.Dispose();
+                memoryCache = new MemoryCache(typeof (TEntity).Name + " K/V memory cache");
+                base.Clear();
+            }
         }
 
 
