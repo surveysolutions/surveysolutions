657d833d948eb28a48c41cc0783c41f8d5264c7f
Roman Lytvyn <litvin.roma@gmail.com>
2016-01-08 15:10:19 +0200

    IN: autosized siaqo changed to fixed siaqo with 512 mb max size. KP-6540

--- src/Core/SharedKernels/Enumerator/Enumerator/Implementation/Services/SiaqodbPlainStorageWithCache.cs
+++ src/Core/SharedKernels/Enumerator/Enumerator/Implementation/Services/SiaqodbPlainStorageWithCache.cs
@@ -1,15 +1,19 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Sqo;
+using WB.Core.SharedKernels.Enumerator.Services;
 using WB.Core.SharedKernels.Enumerator.Services.Infrastructure.Storage;
 
 namespace WB.Core.SharedKernels.Enumerator.Implementation.Services
 {
     public class SiaqodbPlainStorageWithCache<TEntity> : SiaqodbPlainStorage<TEntity> where TEntity: class, IPlainStorageEntity
     {
-        readonly Dictionary<string, TEntity> memoryStorage = new Dictionary<string, TEntity>(); 
-        public SiaqodbPlainStorageWithCache(ISiaqodb storage) : base(storage)
+        readonly Dictionary<string, TEntity> memoryStorage = new Dictionary<string, TEntity>();
+
+        public SiaqodbPlainStorageWithCache(ISiaqodb storage, IUserInteractionService userInteractionService)
+            : base(storage, userInteractionService)
         {
         }
 
@@ -37,27 +41,33 @@ namespace WB.Core.SharedKernels.Enumerator.Implementation.Services
 
         public override async Task StoreAsync(IEnumerable<TEntity> entities)
         {
-            var entitiesToStore = entities.Where(entity => entity != null).ToList();
-
-            foreach (var entity in entitiesToStore)
+            try
             {
-                await this.Storage.StoreObjectAsync(entity);
+                foreach (var entity in entities.Where(entity => entity != null))
+                {
+                    await this.Storage.StoreObjectAsync(entity);
+                    this.StoreToMemory(entity);
+                }
             }
-            this.StoreToMemory(entitiesToStore);
+            catch (Exception ex)
+            {
+                this.Logger.Fatal(ex.Message, ex);
+                if (ex.Message.IndexOf("Environment mapsize limit reached", StringComparison.OrdinalIgnoreCase) > -1)
+                {
+                    await this.UserInteractionService.AlertAsync("Database is full. Please, send tablet information and contact to Survey Solutions team.", "Critical exception");
+                }
+            }
         }
 
-        private void StoreToMemory(IEnumerable<TEntity> entities)
+        private void StoreToMemory(TEntity entity)
         {
-            foreach (var entity in entities)
+            if (!this.memoryStorage.ContainsKey(entity.Id))
             {
-                if (!this.memoryStorage.ContainsKey(entity.Id))
-                {
-                    this.memoryStorage.Add(entity.Id, entity);
-                }
-                else
-                {
-                    this.memoryStorage[entity.Id] = entity;
-                }
+                this.memoryStorage.Add(entity.Id, entity);
+            }
+            else
+            {
+                this.memoryStorage[entity.Id] = entity;
             }
         }
 
