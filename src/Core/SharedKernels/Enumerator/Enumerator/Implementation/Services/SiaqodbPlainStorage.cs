bc72193ddfa164944dd5774a7e199b900abd1336
Roman Lytvyn <litvin.roma@gmail.com>
2016-01-08 15:10:19 +0200

    IN: autosized siaqo changed to fixed siaqo with 512 mb max size. KP-6540

--- src/Core/SharedKernels/Enumerator/Enumerator/Implementation/Services/SiaqodbPlainStorage.cs
+++ src/Core/SharedKernels/Enumerator/Enumerator/Implementation/Services/SiaqodbPlainStorage.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Sqo;
 using Sqo.Transactions;
+using WB.Core.GenericSubdomains.Portable.Services;
+using WB.Core.SharedKernels.Enumerator.Services;
 using WB.Core.SharedKernels.Enumerator.Services.Infrastructure.Storage;
 
 namespace WB.Core.SharedKernels.Enumerator.Implementation.Services
@@ -11,10 +13,14 @@ namespace WB.Core.SharedKernels.Enumerator.Implementation.Services
     public class SiaqodbPlainStorage<TEntity> : IAsyncPlainStorage<TEntity> where TEntity: class, IPlainStorageEntity
     {
         protected readonly ISiaqodb Storage;
+        protected readonly IUserInteractionService UserInteractionService;
+        protected readonly ILogger Logger;
 
-        public SiaqodbPlainStorage(ISiaqodb storage)
+        public SiaqodbPlainStorage(ISiaqodb storage, IUserInteractionService userInteractionService, ILogger logger)
         {
             this.Storage = storage;
+            this.UserInteractionService = userInteractionService;
+            this.Logger = logger;
         }
 
         public virtual TEntity GetById(string id)
@@ -44,9 +50,20 @@ namespace WB.Core.SharedKernels.Enumerator.Implementation.Services
 
         public virtual async Task StoreAsync(IEnumerable<TEntity> entities)
         {
-            foreach (var entity in entities.Where(entity => entity != null))
+            try
             {
-                await this.Storage.StoreObjectAsync(entity);
+                foreach (var entity in entities.Where(entity => entity != null))
+                {
+                    await this.Storage.StoreObjectAsync(entity);
+                }
+            }
+            catch (Exception ex)
+            {
+                this.Logger.Fatal(ex.Message, ex);
+                if (ex.Message.IndexOf("Environment mapsize limit reached", StringComparison.OrdinalIgnoreCase) > -1)
+                {
+                    await this.UserInteractionService.AlertAsync("Database is full. Please, send tablet information and contact to Survey Solutions team.", "Critical exception");
+                }
             }
         }
 
