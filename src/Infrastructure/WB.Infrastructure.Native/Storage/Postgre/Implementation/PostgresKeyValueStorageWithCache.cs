9a0d666b3f2678beda5412db0f50a41c52beaf9b
Roman Lytvyn <litvin.roma@gmail.com>
2016-01-08 16:44:10 +0200

    manual merge master with rc

---
+++ src/Infrastructure/WB.Infrastructure.Native/Storage/Postgre/Implementation/PostgresKeyValueStorageWithCache.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Caching;
+using WB.Core.GenericSubdomains.Portable.Services;
+
+namespace WB.Infrastructure.Native.Storage.Postgre.Implementation
+{
+    internal abstract class PostgresKeyValueStorageWithCache<TEntity> : PostgresKeyValueStorage<TEntity>
+        where TEntity : class
+    {
+        private readonly object lockObject = new object();
+
+        static MemoryCache memoryCache = new MemoryCache(typeof(TEntity).Name + " K/V memory cache");
+
+        public PostgresKeyValueStorageWithCache(string connectionString, ILogger logger)
+            : base(connectionString, logger)
+        {
+        }
+
+
+        public override TEntity GetById(string id)
+        {
+            lock (lockObject)
+            {
+                var value = memoryCache.Get(id) as TEntity;
+                if (value != null)
+                    return value;
+
+                value = base.GetById(id);
+                if (value != null)
+                    memoryCache.Set(id, value, DateTimeOffset.Now.AddSeconds(10));
+
+                return value;
+            }
+        }
+
+        public override void Remove(string id)
+        {
+            lock (lockObject)
+            {
+                memoryCache.Remove(id);
+                base.Remove(id);
+            }
+        }
+
+        public override void Store(TEntity view, string id)
+        {
+            lock (lockObject)
+            {
+                memoryCache.Set(id, view, DateTimeOffset.Now.AddSeconds(10));
+                base.Store(view, id);
+            }
+        }
+
+        public override void BulkStore(List<Tuple<TEntity, string>> bulk)
+        {
+            lock (lockObject)
+            {
+                bulk.ForEach(i => memoryCache.Remove(i.Item2));
+                base.BulkStore(bulk);
+            }
+        }
+
+        public override void Clear()
+        {
+            lock (lockObject)
+            {
+                memoryCache.Dispose();
+                memoryCache = new MemoryCache(typeof(TEntity).Name + " K/V memory cache");
+                base.Clear();
+            }
+        }
+
+
+        public override string GetReadableStatus()
+        {
+            return "Postgres with Cache K/V :/";
+        }
+    }
+}
