382895a16e832addb1b647625d67f5ad5269a0ea
Andrii Kozhyn <hedgic@gmail.com>
2016-01-13 10:07:31 +0200

    KP-6528: Moved all ddi related code to separate namespace

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/DataExport/Ddi/Impl/MetaDescriptionFactory.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/DataExport/Ddi/Impl/MetaDescriptionFactory.cs
@@ -1,6 +1,6 @@
 using ddidotnet;
 
-namespace WB.Core.BoundedContexts.Headquarters.DataExport.Factories
+namespace WB.Core.BoundedContexts.Headquarters.DataExport.Ddi.Impl
 {
     internal class MetaDescriptionFactory : IMetaDescriptionFactory
     {
