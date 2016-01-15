44e28fa9443837f1d495472320edf7c4ba054b15
Slava Scherbak <slava.scherbak@gmail.com>
2016-01-15 15:58:06 -0500

    implemented changes after code review KP-6584

--- src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/TeamViewFactoryTests/when_getting_all_supervisors.cs
+++ src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/TeamViewFactoryTests/when_getting_all_supervisors.cs
@@ -14,9 +14,9 @@ namespace WB.Tests.Unit.SharedKernels.SurveyManagement.TeamViewFactoryTests
         {
             new List<UserDocument>()
             {
-                Create.UserDocument(Id.g1, userName: "b_locked_super", isLockedByHQ: true),
-                Create.UserDocument(Id.g2, userName: "a_super1"),
-                Create.UserDocument(Id.g3, userName: "c_super2"),
+                Create.UserDocument(Id.g1, userName: superBName, isLockedByHQ: true),
+                Create.UserDocument(Id.g2, userName: superAName),
+                Create.UserDocument(Id.g3, userName: superCName),
                 Create.UserDocument(Id.g4, userName: "inter1", supervisorId: Id.g2),
                 Create.UserDocument(Id.g5, userName: "inter2", supervisorId: Id.g2)
             }.ForEach(x => usersStorage.Store(x, x.PublicKey.FormatGuid()));
@@ -28,32 +28,35 @@ namespace WB.Tests.Unit.SharedKernels.SurveyManagement.TeamViewFactoryTests
         Because of = () =>
             result = teamFactory.GetAllSupervisors(12, "", showLocked: true);
 
-        It should_return_2_active_and_1_locked_supervisors = () =>
+        It should_return_3_supervisors = () =>
             result.TotalCountByQuery.ShouldEqual(3);
 
         It should_return_supervisor_with_specified_properties_at_position_0 = () =>
         {
             var user = result.Users.ElementAt(0);
             user.UserId.ShouldEqual(Id.g2);
-            user.UserName.ShouldEqual("a_super1");
+            user.UserName.ShouldEqual(superAName);
         };
 
         It should_return_supervisor_with_specified_properties_at_position_1 = () =>
         {
             var user = result.Users.ElementAt(1);
             user.UserId.ShouldEqual(Id.g1);
-            user.UserName.ShouldEqual("b_locked_super");
+            user.UserName.ShouldEqual(superBName);
         };
 
         It should_return_supervisor_with_specified_properties_at_position_2 = () =>
         {
             var user = result.Users.ElementAt(2);
             user.UserId.ShouldEqual(Id.g3);
-            user.UserName.ShouldEqual("c_super2");
+            user.UserName.ShouldEqual(superCName);
         };
 
         private static TeamViewFactory teamFactory;
         private static UsersView result;
         private static readonly TestInMemoryWriter<UserDocument> usersStorage = new TestInMemoryWriter<UserDocument>();
+        private static readonly string superAName = "a_super1";
+        private static readonly string superBName = "b_locked_super";
+        private static readonly string superCName = "c_super2";
     }
 }
