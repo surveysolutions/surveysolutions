29b36f87c563a6ce156cc0cce4ded6ff6ae31f96
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2016-01-15 17:11:08 +0200

    Siaqo: implemented autoincrease KP-6568

--- src/Core/SharedKernels/Enumerator/Enumerator/Implementation/Services/SiaqodbPlainStorage.cs
+++ src/Core/SharedKernels/Enumerator/Enumerator/Implementation/Services/SiaqodbPlainStorage.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Sqo;
 using Sqo.Transactions;
+using WB.Core.GenericSubdomains.Portable;
 using WB.Core.GenericSubdomains.Portable.Services;
 using WB.Core.SharedKernels.Enumerator.Services;
 using WB.Core.SharedKernels.Enumerator.Services.Infrastructure.Storage;
@@ -12,11 +13,12 @@ namespace WB.Core.SharedKernels.Enumerator.Implementation.Services
 {
     public class SiaqodbPlainStorage<TEntity> : IAsyncPlainStorage<TEntity> where TEntity: class, IPlainStorageEntity
     {
-        private readonly ISiaqodb storage;
+        private readonly Siaqodb storage;
         private readonly IUserInteractionService userInteractionService;
         private readonly ILogger logger;
+        private static readonly object dbSizeLockObject = new object();
 
-        public SiaqodbPlainStorage(ISiaqodb storage, IUserInteractionService userInteractionService, ILogger logger)
+        public SiaqodbPlainStorage(Siaqodb storage, IUserInteractionService userInteractionService, ILogger logger)
         {
             this.storage = storage;
             this.userInteractionService = userInteractionService;
@@ -25,31 +27,32 @@ namespace WB.Core.SharedKernels.Enumerator.Implementation.Services
 
         public virtual TEntity GetById(string id)
         {
-            return this.Query(entities => entities.FirstOrDefault(entity => entity.Id == id));
+            return this.Query(entities => entities.SingleOrDefault(entity => entity.Id == id));
         }
 
         public async Task RemoveAsync(string id)
         {
-            TEntity entity = await Task.FromResult(this.GetById(id));
+            TEntity entity = this.GetById(id);
 
             await this.RemoveAsync(new[] { entity });
         }
 
         public virtual async Task RemoveAsync(IEnumerable<TEntity> entities)
         {
-            ITransaction transaction = this.storage.BeginTransaction();
+            ITransaction transaction = this.BeginTransactionRespectingDBSize();
             try
             {
                 foreach (var entity in entities.Where(entity => entity != null))
                 {
                     await this.storage.DeleteAsync(entity, transaction);
                 }
-                transaction.Commit();
+                await transaction.CommitAsync();
             }
             catch (Exception ex)
             {
                 this.logger.Fatal(ex.Message, ex);
-                transaction.Rollback();
+                await transaction.RollbackAsync();
+                throw;
             }
         }
 
@@ -60,31 +63,69 @@ namespace WB.Core.SharedKernels.Enumerator.Implementation.Services
 
         public virtual async Task StoreAsync(IEnumerable<TEntity> entities)
         {
-            ITransaction transaction = this.storage.BeginTransaction();
+            ITransaction transaction = this.BeginTransactionRespectingDBSize();
             try
             {
                 foreach (var entity in entities.Where(entity => entity != null))
                 {
                     await this.storage.StoreObjectAsync(entity, transaction);
                 }
-                transaction.Commit();
+                await transaction.CommitAsync(); 
             }
             catch (Exception ex)
             {
                 this.logger.Fatal(ex.Message, ex);
+
+                await transaction.RollbackAsync();
+
                 if (ex.Message.IndexOf("Environment mapsize limit reached", StringComparison.OrdinalIgnoreCase) > -1)
                 {
                     await this.userInteractionService.AlertAsync("Database is full. Please, restart application to increase database size", "Critical exception");
                 }
-                transaction.Rollback();
+                else
+                {
+                    throw;
+                }
             }
         }
 
         public TResult Query<TResult>(Func<IQueryable<TEntity>, TResult> query)
         {
-            var queryable = this.storage.Cast<TEntity>().AsQueryable();
-            var result = query.Invoke(queryable);
-            return result;
+            ITransaction transaction = this.BeginTransactionRespectingDBSize();
+            try
+            {
+                var queryable = this.storage.Cast<TEntity>().AsQueryable();
+                var result = query.Invoke(queryable);
+                return result;
+            }
+            finally
+            {
+                transaction.Rollback();
+            }
+        }
+
+        private ITransaction BeginTransactionRespectingDBSize()
+        {
+            if (this.ShouldIncreaseDBSize())
+            {
+                lock (dbSizeLockObject)
+                {
+                    if (this.ShouldIncreaseDBSize())
+                    {
+                        var storagePath = this.storage.DbInfo.Path;
+
+                        this.storage.Close();
+                        this.storage.Open(storagePath);
+                    }
+                }
+            }
+
+            return this.storage.BeginTransaction();
+        }
+
+        private bool ShouldIncreaseDBSize()
+        {
+            return this.storage.DbInfo.UsedSize > 0.75m * this.storage.DbInfo.MaxSize;
         }
     }
 }
