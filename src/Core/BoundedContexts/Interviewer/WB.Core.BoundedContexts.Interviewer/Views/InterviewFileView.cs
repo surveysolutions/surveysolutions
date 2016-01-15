a3092672b6a9faf5e41384130098c8ab24f2b8ad
Roman Lytvyn <litvin.roma@gmail.com>
2016-01-15 21:03:05 +0200

    IN: added sqlite plain storage repository. KP-6568

--- src/Core/BoundedContexts/Interviewer/WB.Core.BoundedContexts.Interviewer/Views/InterviewFileView.cs
+++ src/Core/BoundedContexts/Interviewer/WB.Core.BoundedContexts.Interviewer/Views/InterviewFileView.cs
@@ -1,10 +1,13 @@
 using System;
+using SQLite.Net.Attributes;
 using WB.Core.SharedKernels.Enumerator.Services.Infrastructure.Storage;
 
 namespace WB.Core.BoundedContexts.Interviewer.Views
 {
     public class InterviewFileView : IPlainStorageEntity
     {
+        [PrimaryKey, AutoIncrement]
+        public int OID { get; set; }
         public string Id { get; set; }
         public byte[] File { get; set; }
     }
