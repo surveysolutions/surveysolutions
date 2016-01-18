9ec4b772ec98e3c6573bcfd2381dab4c136713b2
Roman Lytvyn <litvin.roma@gmail.com>
2016-01-18 15:21:10 +0200

    IN: removed link to android assembly of siaqo db from WB.Core.SharedKernels.Enumerator

--- src/UI/Shared/WB.UI.Shared.Enumerator/CustomServices/SiaqodbPlainStorageWithCache.cs
+++ src/UI/Shared/WB.UI.Shared.Enumerator/CustomServices/SiaqodbPlainStorageWithCache.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,7 +6,7 @@ using WB.Core.GenericSubdomains.Portable.Services;
 using WB.Core.SharedKernels.Enumerator.Services;
 using WB.Core.SharedKernels.Enumerator.Services.Infrastructure.Storage;
 
-namespace WB.Core.SharedKernels.Enumerator.Implementation.Services
+namespace WB.UI.Shared.Enumerator.CustomServices
 {
     public class SiaqodbPlainStorageWithCache<TEntity> : SiaqodbPlainStorage<TEntity> where TEntity: class, IPlainStorageEntity
     {
