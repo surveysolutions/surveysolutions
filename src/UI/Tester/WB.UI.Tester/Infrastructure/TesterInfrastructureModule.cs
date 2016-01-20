2e94e76eee6e770b6d2707d6bb62f6a39d3d2c44
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2016-01-20 11:32:26 -0500

    Tester: KP-6610 replace siaqo with sqlite

--- src/UI/Tester/WB.UI.Tester/Infrastructure/TesterInfrastructureModule.cs
+++ src/UI/Tester/WB.UI.Tester/Infrastructure/TesterInfrastructureModule.cs
@@ -1,8 +1,6 @@
-﻿using System.Collections.Generic;
-using Main.Core.Documents;
+﻿using Main.Core.Documents;
 using Ncqrs.Eventing.Storage;
 using Ninject.Modules;
-using Sqo;
 using WB.Core.BoundedContexts.Tester.Implementation.Services;
 using WB.Core.GenericSubdomains.Portable.Implementation;
 using WB.Core.GenericSubdomains.Portable.Implementation.Services;
@@ -17,13 +15,14 @@ using WB.Core.SharedKernels.Enumerator.Services.Infrastructure;
 using WB.Core.SharedKernels.Enumerator.Services.Infrastructure.Storage;
 using WB.Infrastructure.Shared.Enumerator;
 using WB.UI.Shared.Enumerator.CustomServices;
-using WB.UI.Tester.CustomServices;
 using WB.UI.Tester.Infrastructure.Internals;
 using WB.UI.Tester.Infrastructure.Internals.Log;
 using WB.UI.Tester.Infrastructure.Internals.Rest;
 using WB.UI.Tester.Infrastructure.Internals.Security;
 using WB.UI.Tester.Infrastructure.Internals.Settings;
 using WB.UI.Tester.Infrastructure.Internals.Storage;
+using SQLite.Net.Interop;
+using SQLite.Net.Platform.XamarinAndroid;
 using ILogger = WB.Core.GenericSubdomains.Portable.Services.ILogger;
 
 namespace WB.UI.Tester.Infrastructure
@@ -45,10 +44,13 @@ namespace WB.UI.Tester.Infrastructure
             this.Bind<IPlainKeyValueStorage<QuestionnaireDocument>>().To<InMemoryKeyValueStorage<QuestionnaireDocument>>().InSingletonScope();
             this.Bind<IPlainKeyValueStorage<QuestionnaireModel>>().To<InMemoryKeyValueStorage<QuestionnaireModel>>().InSingletonScope();
 
-            SiaqodbConfigurator.SetLicense(@"yrwPAibl/TwJ+pR5aBOoYieO0MbZ1HnEKEAwjcoqtdrUJVtXxorrxKZumV+Z48/Ffjj58P5pGVlYZ0G1EoPg0w==");
-            this.Bind<Siaqodb>().ToConstant(new Siaqodb(AndroidPathUtils.GetPathToSubfolderInLocalDirectory("database")));
-
-            this.Bind(typeof(IAsyncPlainStorage<>)).To(typeof(SiaqodbPlainStorage<>)).InSingletonScope();
+            this.Bind<ISQLitePlatform>().To<SQLitePlatformAndroid>();
+            this.Bind<SqliteSettings>().ToConstant(
+                new SqliteSettings()
+                {
+                    PathToDatabaseDirectory = AndroidPathUtils.GetPathToSubfolderInLocalDirectory("data")
+                });
+            this.Bind(typeof(IAsyncPlainStorage<>)).To(typeof(SqlitePlainStorage<>)).InSingletonScope();
 
             this.Bind<ILogger>().To<XamarinInsightsLogger>().InSingletonScope();
 
