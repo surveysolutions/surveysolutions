e2e9212185027a607ac2caa587de84cecb69c59a
Roman Lytvyn <litvin.roma@gmail.com>
2016-01-25 14:02:53 +0200

    IN: fixed started interview status on answering by yes/no question. KP-6547

---
+++ src/Tests/WB.Tests.Unit/BoundedContexts/Interviewer/DashboardDenormalizerTests/when_handling_YesNoQuestionAnswered_event.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Threading.Tasks;
+using Machine.Specifications;
+using Moq;
+using Ncqrs.Eventing.ServiceModel.Bus;
+using WB.Core.BoundedContexts.Interviewer.Views;
+using WB.Core.SharedKernels.DataCollection;
+using WB.Core.SharedKernels.DataCollection.Events.Interview;
+using WB.Core.SharedKernels.DataCollection.Events.Interview.Dtos;
+using WB.Core.SharedKernels.Enumerator.Services.Infrastructure.Storage;
+using It = Machine.Specifications.It;
+using it = Moq.It;
+
+namespace WB.Tests.Unit.BoundedContexts.Interviewer.DashboardDenormalizerTests
+{
+    internal class when_handling_YesNoQuestionAnswered_event
+    {
+        Establish context = () =>
+        {
+            dashboardItem = Create.InterviewView();
+
+            @event = Create.Event.YesNoQuestionAnswered(Guid.Parse("11111111111111111111111111111111"), new AnsweredYesNoOption[0]).ToPublishedEvent();
+
+            var storeAsyncTask = new Task(()=> {});
+            storeAsyncTask.Start();
+
+            var interviewViewStorage = Mock.Of<IAsyncPlainStorage<InterviewView>>(writer => 
+            writer.GetById(it.IsAny<string>()) == dashboardItem);
+
+            Mock.Get(interviewViewStorage)
+                .Setup(storage => storage.StoreAsync(it.IsAny<InterviewView>()))
+                .Callback<InterviewView>((view) => dashboardItem = view)
+                .Returns(storeAsyncTask);
+
+            denormalizer = Create.DashboardDenormalizer(interviewViewRepository: interviewViewStorage);
+        };
+
+        Because of = () =>
+            denormalizer.Handle(@event);
+
+        It should_interview_be_strated = () =>
+            dashboardItem.StartedDateTime.ShouldNotBeNull();
+
+        private static InterviewEventHandler denormalizer;
+        private static IPublishedEvent<YesNoQuestionAnswered> @event;
+        private static InterviewView dashboardItem;
+    }
+}
