bd4aff2eba6ee84749ce41601ed02fbc4c7e52d9
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2016-01-27 01:52:25 -0500

    KP-6559 Map report can't display huge amount of data (about 100K and more)

--- src/Core/SharedKernels/SurveyManagement/SurveyManagement/Views/Reposts/InputModels/MapReportInputModel.cs
+++ src/Core/SharedKernels/SurveyManagement/SurveyManagement/Views/Reposts/InputModels/MapReportInputModel.cs
@@ -7,5 +7,11 @@ namespace WB.Core.SharedKernels.SurveyManagement.Views.Reposts.InputModels
         public Guid QuestionnaireId { get; set; }
         public long QuestionnaireVersion { get; set; }
         public string Variable { get; set; }
+
+        public double NorthEastCornerLongtitude { get; set; }
+        public double NorthEastCornerLatitude { get; set; }
+
+        public double SouthWestCornerLongtitude { get; set; }
+        public double SouthWestCornerLatitude { get; set; }
     }
 }
