e5294f77226aee1e7a734f64f3deff1a5a7d9de5
Andrii Kozhyn <hedgic@gmail.com>
2016-01-28 16:49:06 +0200

    KP-6628: Removed Keychain and settings plugins

--- src/UI/Tester/WB.UI.Tester/Infrastructure/Internals/Security/TesterUserIdentity.cs
+++ src/UI/Tester/WB.UI.Tester/Infrastructure/Internals/Security/TesterUserIdentity.cs
@@ -1,10 +1,14 @@
 using System;
+using SQLite.Net.Attributes;
 using WB.Core.SharedKernels.Enumerator.Services.Infrastructure;
+using WB.Core.SharedKernels.Enumerator.Services.Infrastructure.Storage;
 
 namespace WB.UI.Tester.Infrastructure.Internals.Security
 {
-    internal class TesterUserIdentity : IUserIdentity
+    internal class TesterUserIdentity : IUserIdentity, IPlainStorageEntity
     {
+        [PrimaryKey]
+        public string Id  { get; set; }
         public string Name { get; set; }
         public string Password { get; set; }
         public Guid UserId { get; set; }
