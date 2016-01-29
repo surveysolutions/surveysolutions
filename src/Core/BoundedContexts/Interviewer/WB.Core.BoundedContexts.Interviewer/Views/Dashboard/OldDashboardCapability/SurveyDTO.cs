7b7fac9aad6f07336020846645a221279f6d364e
Andrii Kozhyn <hedgic@gmail.com>
2016-01-29 10:48:12 +0200

    KP-6628: restored backward compatibility with 5.3 code

---
+++ src/Core/BoundedContexts/Interviewer/WB.Core.BoundedContexts.Interviewer/Views/Dashboard/OldDashboardCapability/SurveyDTO.cs
@@ -0,0 +1,34 @@
+using System;
+using SQLite.Net.Attributes;
+
+namespace WB.UI.Interviewer.ViewModel.Dashboard
+{
+    [Obsolete]
+    public class SurveyDto
+    {
+        [PrimaryKey]
+        public string Id { get; set; }
+        public string SurveyTitle { get; set; }
+        public string QuestionnaireId { get; set; }
+
+        public long QuestionnaireVersion
+        {
+            get
+            {
+                if (this.questionnaireVersion == 0)
+                {
+                    return this.TemplateMaxVersion;
+                }
+                return this.questionnaireVersion;
+            }
+            private set
+            {
+                this.questionnaireVersion = value;
+            }
+        }
+
+        private long questionnaireVersion;
+        public bool AllowCensusMode { get; set; }
+        public long TemplateMaxVersion { get; set; }
+    }
+}
