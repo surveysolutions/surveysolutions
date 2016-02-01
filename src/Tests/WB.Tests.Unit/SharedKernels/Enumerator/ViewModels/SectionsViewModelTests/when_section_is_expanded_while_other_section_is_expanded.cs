50c5ca6be2c794dde266095368df0ba18a79c44b
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2016-02-01 11:16:53 +0200

    Renamed delegate to remove non existent domain name

--- src/Tests/WB.Tests.Unit/SharedKernels/Enumerator/ViewModels/SectionsViewModelTests/when_section_is_expanded_while_other_section_is_expanded.cs
+++ src/Tests/WB.Tests.Unit/SharedKernels/Enumerator/ViewModels/SectionsViewModelTests/when_section_is_expanded_while_other_section_is_expanded.cs
@@ -46,7 +46,7 @@ namespace WB.Tests.Unit.SharedKernels.Enumerator.ViewModels.SectionsViewModelTes
 
         Because of = () =>
         {
-            navigationState.ScreenChanged += Raise.Event<ScreenChanged>(new ScreenChangedEventArgs { TargetGroup = secondSection.SectionIdentity });
+            navigationState.ScreenChanged += Raise.Event<GroupChanged>(new ScreenChangedEventArgs { TargetGroup = secondSection.SectionIdentity });
         };
 
         It should_collapse_previous_group = () => firstSelectedSection.Expanded.ShouldBeFalse();
