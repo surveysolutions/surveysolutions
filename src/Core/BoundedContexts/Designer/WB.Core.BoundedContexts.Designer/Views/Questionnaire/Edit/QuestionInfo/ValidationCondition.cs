65e80f385fea97e1d66f6c25c387da2afcf26520
Andrii Kozhyn <hedgic@gmail.com>
2016-02-02 12:42:11 +0200

    KP-6651: Added display of already created validation conditions to designer UI

---
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Views/Questionnaire/Edit/QuestionInfo/ValidationCondition.cs
@@ -0,0 +1,8 @@
+﻿namespace WB.Core.BoundedContexts.Designer.Views.Questionnaire.Edit.QuestionInfo
+{
+    public class ValidationCondition
+    {
+        public string Expression { get; set; } 
+        public string Message { get; set; } 
+    }
+}
