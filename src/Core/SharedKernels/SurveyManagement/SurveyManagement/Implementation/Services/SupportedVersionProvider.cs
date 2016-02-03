74120f3325f5f28be81e7e03a7e5c9115d4c744c
Anastasya Kholodova <nastya.kholodova@gmail.com>
2016-02-03 14:39:44 -0500

    Designer: KP-6672 allow to use numeric rosters and app engine version

--- src/Core/SharedKernels/SurveyManagement/SurveyManagement/Implementation/Services/SupportedVersionProvider.cs
+++ src/Core/SharedKernels/SurveyManagement/SurveyManagement/Implementation/Services/SupportedVersionProvider.cs
@@ -5,7 +5,7 @@ namespace WB.Core.SharedKernels.SurveyManagement.Implementation.Services
 {
     public class SupportedVersionProvider : ISupportedVersionProvider
     {
-        private static readonly Version ExpressionsEngineVersion = new Version(11, 0, 0);
+        private static readonly Version ExpressionsEngineVersion = new Version(12, 0, 0);
 
         public Version GetSupportedQuestionnaireVersion()
         {
