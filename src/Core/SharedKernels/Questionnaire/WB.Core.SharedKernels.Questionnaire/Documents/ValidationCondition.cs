c050934e5010e0f6bf132e89c26cb178e28f14db
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2016-02-04 12:24:37 +0200

    KP-6658: Added test with cloning multiple validations

--- src/Core/SharedKernels/Questionnaire/WB.Core.SharedKernels.Questionnaire/Documents/ValidationCondition.cs
+++ src/Core/SharedKernels/Questionnaire/WB.Core.SharedKernels.Questionnaire/Documents/ValidationCondition.cs
@@ -2,6 +2,16 @@
 {
     public class ValidationCondition
     {
+        public ValidationCondition()
+        {
+        }
+
+        public ValidationCondition(string expression, string message)
+        {
+            this.Expression = expression;
+            this.Message = message;
+        }
+
         public string Expression { get; set; } 
         public string Message { get; set; } 
     }
