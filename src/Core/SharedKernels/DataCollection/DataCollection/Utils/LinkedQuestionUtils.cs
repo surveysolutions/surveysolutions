97d38ba9699c05179ef0b494e5455098042d3273
Anastasya Kholodova <nastya.kholodova@gmail.com>
2016-02-09 16:40:40 -0500

    SurveyManagment: KP-6702 first version of linked on roster question

--- src/Core/SharedKernels/DataCollection/DataCollection/Utils/LinkedQuestionUtils.cs
+++ src/Core/SharedKernels/DataCollection/DataCollection/Utils/LinkedQuestionUtils.cs
@@ -10,40 +10,42 @@ namespace WB.Core.SharedKernels.DataCollection.Utils
 {
     public static class LinkedQuestionUtils
     {
-        public static bool IsLevelAllowedToBeUsedAsLinkSourceInCurrentScope(decimal[] referensedLevelRosterVector, ValueVector<Guid> referensedLevelRosterScopeVector,
+        public static bool IsLevelAllowedToBeUsedAsLinkSourceInCurrentScope(decimal[] referencedLevelRosterVector, ValueVector<Guid> referencedLevelRosterScopeVector,
            decimal[] linkedQuestionRosterVector, ValueVector<Guid> linkedQuestionRosterScopeVector)
         {
-            for (int i = 0; i < Math.Min(referensedLevelRosterVector.Length - 1, linkedQuestionRosterVector.Length); i++)
+            for (int i = 0; i < Math.Min(referencedLevelRosterVector.Length - 1, linkedQuestionRosterVector.Length); i++)
             {
-                if (referensedLevelRosterScopeVector[i] != linkedQuestionRosterScopeVector[i])
+                if (referencedLevelRosterScopeVector[i] != linkedQuestionRosterScopeVector[i])
                     continue;
-                if (referensedLevelRosterVector[i] != linkedQuestionRosterVector[i])
+                if (referencedLevelRosterVector[i] != linkedQuestionRosterVector[i])
                     return false;
             }
             return true;
         }
 
-        public static string BuildLinkedQuestionOptionTitle(string referencedQuestionAnswer,
-            Func<Guid, decimal[], string> getLevelName,
-            decimal[] referensedQuestionRosterVector,
-            ValueVector<Guid> referensedQuestionRosterScopeVector, decimal[] linkedQuestionRosterVector, ValueVector<Guid> linkedQuestionRosterScope,
-            QuestionnaireRosterStructure questionnaireRosters)
+        public static string BuildLinkedQuestionOptionTitle(
+            string referencedQuestionAnswer,
+            Func<decimal[], string> getLevelName,
+            decimal[] referencedQuestionRosterVector,
+            ValueVector<Guid> referencedQuestionRosterScopeVector, 
+            decimal[] linkedQuestionRosterVector, 
+            ValueVector<Guid> linkedQuestionRosterScope)
         {
             var combinedRosterTitles = new List<string>();
 
-            for (int i = 0; i < referensedQuestionRosterScopeVector.Length - 1; i++)
+            for (int i = 0; i < referencedQuestionRosterScopeVector.Length - 1; i++)
             {
-                var scopeId = referensedQuestionRosterScopeVector[i];
-                var rosterScopeDescription = questionnaireRosters.RosterScopes[new ValueVector<Guid>(referensedQuestionRosterScopeVector.Take(i+1).ToArray())];
+                var scopeId = referencedQuestionRosterScopeVector[i];
+             /*   var rosterScopeDescription = questionnaireRosters.RosterScopes[new ValueVector<Guid>(referensedQuestionRosterScopeVector.Take(i+1).ToArray())];
 
-                var firstScreenInScopeId = rosterScopeDescription.RosterIdToRosterTitleQuestionIdMap.Keys.First();
+                var firstScreenInScopeId = rosterScopeDescription.RosterIdToRosterTitleQuestionIdMap.Keys.First();*/
                 var firstScreeninScopeRosterVector =
-                    referensedQuestionRosterVector.Take(i + 1).ToArray();
+                    referencedQuestionRosterVector.Take(i + 1).ToArray();
 
                 if (linkedQuestionRosterScope.Length > i && linkedQuestionRosterScope[i] == scopeId &&
                     linkedQuestionRosterVector.Length >= firstScreeninScopeRosterVector.Length)
                     continue;
-                var levelName = getLevelName(firstScreenInScopeId, firstScreeninScopeRosterVector);
+                var levelName = getLevelName(firstScreeninScopeRosterVector);
                 if (!string.IsNullOrEmpty(levelName))
                     combinedRosterTitles.Add(levelName);
             }
