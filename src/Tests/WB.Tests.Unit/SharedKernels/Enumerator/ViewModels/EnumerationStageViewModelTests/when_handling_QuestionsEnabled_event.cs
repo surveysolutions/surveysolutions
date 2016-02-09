3c40d9be46e25fb3c7576189d4a8df0b046639e8
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2016-02-09 13:21:09 +0200

    EnumerationStageViewModel: covered QuestionsEnabled handler hide-if-disabled logic with tests KP-6678

--- src/Tests/WB.Tests.Unit/SharedKernels/Enumerator/ViewModels/EnumerationStageViewModelTests/when_handling_QuestionsEnabled_event.cs
+++ src/Tests/WB.Tests.Unit/SharedKernels/Enumerator/ViewModels/EnumerationStageViewModelTests/when_handling_QuestionsEnabled_event.cs
@@ -5,7 +5,6 @@ using Moq;
 using WB.Core.SharedKernels.DataCollection;
 using WB.Core.SharedKernels.DataCollection.Implementation.Entities;
 using WB.Core.SharedKernels.Enumerator.Aggregates;
-using WB.Core.SharedKernels.Enumerator.Repositories;
 using WB.Core.SharedKernels.Enumerator.Services;
 using WB.Core.SharedKernels.Enumerator.ViewModels;
 using WB.Core.SharedKernels.Enumerator.ViewModels.InterviewDetails;
@@ -13,26 +12,26 @@ using It = Machine.Specifications.It;
 
 namespace WB.Tests.Unit.SharedKernels.Enumerator.ViewModels.EnumerationStageViewModelTests
 {
-    internal class when_handling_GroupsEnabled_event_for_group_which_was_not_in_items_list
+    internal class when_handling_QuestionsEnabled_event
     {
         Establish context = () =>
         {
             var questionnaireRepository = Setup.QuestionnaireRepositoryWithOneQuestionnaire(questionnaireIdentity, questionnaire
-                => questionnaire.ShouldBeHiddenIfDisabled(disabledAndHideIfDisabledGroup.Id) == true
-                && questionnaire.ShouldBeHiddenIfDisabled(disabledAndNotHideIfDisabledGroup.Id) == false);
+                => questionnaire.ShouldBeHiddenIfDisabled(disabledAndHideIfDisabledQuestion.Id) == true
+                   && questionnaire.ShouldBeHiddenIfDisabled(disabledAndNotHideIfDisabledQuestion.Id) == false);
 
             var interviewRepository = Setup.StatefulInterviewRepository(Mock.Of<IStatefulInterview>(interview
                 => interview.QuestionnaireIdentity == questionnaireIdentity
-                && interview.IsEnabled(disabledAndHideIfDisabledGroup) == false
-                && interview.IsEnabled(disabledAndNotHideIfDisabledGroup) == false
-                && interview.IsEnabled(enabledGroup) == true));
+                   && interview.IsEnabled(disabledAndHideIfDisabledQuestion) == false
+                   && interview.IsEnabled(disabledAndNotHideIfDisabledQuestion) == false
+                   && interview.IsEnabled(enabledQuestion) == true));
 
             var interviewViewModelFactory = Mock.Of<IInterviewViewModelFactory>(__ =>
                 __.GetEntities(Moq.It.IsAny<string>(), Moq.It.IsAny<Identity>(), Moq.It.IsAny<NavigationState>()) == new[]
                 {
-                    Mock.Of<IInterviewEntityViewModel>(_ => _.Identity == enabledGroup),
-                    Mock.Of<IInterviewEntityViewModel>(_ => _.Identity == disabledAndHideIfDisabledGroup),
-                    Mock.Of<IInterviewEntityViewModel>(_ => _.Identity == disabledAndNotHideIfDisabledGroup),
+                    Mock.Of<IInterviewEntityViewModel>(_ => _.Identity == enabledQuestion),
+                    Mock.Of<IInterviewEntityViewModel>(_ => _.Identity == disabledAndHideIfDisabledQuestion),
+                    Mock.Of<IInterviewEntityViewModel>(_ => _.Identity == disabledAndNotHideIfDisabledQuestion),
                 });
 
             viemModel = Create.EnumerationStageViewModel(
@@ -46,24 +45,24 @@ namespace WB.Tests.Unit.SharedKernels.Enumerator.ViewModels.EnumerationStageView
 
         Because of = () =>
             viemModel.Handle(
-                Create.Event.GroupsEnabled());
+                Create.Event.QuestionsEnabled());
 
-        It should_put_enabled_group_to_items_list = () =>
+        It should_put_enabled_question_to_items_list = () =>
             viemModel.Items.OfType<IInterviewEntityViewModel>().Select(entity => entity.Identity)
-                .ShouldContain(enabledGroup);
+                .ShouldContain(enabledQuestion);
 
-        It should_not_put_disabled_group_which_is_set_to_be_hidden_if_disabled_to_items_list = () =>
+        It should_not_put_disabled_question_which_is_set_to_be_hidden_if_disabled_to_items_list = () =>
             viemModel.Items.OfType<IInterviewEntityViewModel>().Select(entity => entity.Identity)
-                .ShouldNotContain(disabledAndHideIfDisabledGroup);
+                .ShouldNotContain(disabledAndHideIfDisabledQuestion);
 
-        It should_put_disabled_group_which_is_not_set_to_be_hidden_if_disabled_to_items_list = () =>
+        It should_put_disabled_question_which_is_not_set_to_be_hidden_if_disabled_to_items_list = () =>
             viemModel.Items.OfType<IInterviewEntityViewModel>().Select(entity => entity.Identity)
-                .ShouldContain(disabledAndNotHideIfDisabledGroup);
+                .ShouldContain(disabledAndNotHideIfDisabledQuestion);
 
         private static EnumerationStageViewModel viemModel;
-        private static Identity enabledGroup = Create.Identity("EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE", RosterVector.Empty);
-        private static Identity disabledAndHideIfDisabledGroup = Create.Identity("DDDDDDDDDDDDDDDDD111111111111111", RosterVector.Empty);
-        private static Identity disabledAndNotHideIfDisabledGroup = Create.Identity("DDDDDDDDDDDDDDD00000000000000000", RosterVector.Empty);
+        private static Identity enabledQuestion = Create.Identity("EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE", RosterVector.Empty);
+        private static Identity disabledAndHideIfDisabledQuestion = Create.Identity("DDDDDDDDDDDDDDDDD111111111111111", RosterVector.Empty);
+        private static Identity disabledAndNotHideIfDisabledQuestion = Create.Identity("DDDDDDDDDDDDDDD00000000000000000", RosterVector.Empty);
         private static QuestionnaireIdentity questionnaireIdentity = new QuestionnaireIdentity(Guid.Parse("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"), 99);
         private static string interviewId = "11111111111111111111111111111111";
     }
