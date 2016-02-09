08aedca73a4239874dadd6a11d8d0c7d50df7cba
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2016-02-09 12:56:05 +0200

    shortened file names which are too long for build server KP-6678

--- find-too-long-filenames.ps1
+++ find-too-long-filenames.ps1
@@ -2,6 +2,6 @@
 
 Get-ChildItem -Recurse `
     | %{ GetPathRelativeToCurrectLocation $_.FullName } `
-    | ?{ $_.Length -gt 219 } `
-    | %{ New-Object PSObject -Property @{ File = $_; ExcessiveCharacters = $_.Length - 219 } } `
+    | ?{ $_.Length -gt 222 } `
+    | %{ New-Object PSObject -Property @{ File = $_; ExcessiveCharacters = $_.Length - 222 } } `
     | Format-List `
