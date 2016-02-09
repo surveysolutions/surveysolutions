5240c95430829b09e94563e5339e92b7622e9f3b
Andrii Kozhyn <hedgic@gmail.com>
2016-02-09 18:23:46 +0200

    KP-6680: Added filling failed condition indexes in denormalizer

--- src/Core/SharedKernels/DataCollection/DataCollection/DataTransferObjects/Synchronization/InterviewSynchronizationDto.cs
+++ src/Core/SharedKernels/DataCollection/DataCollection/DataTransferObjects/Synchronization/InterviewSynchronizationDto.cs
@@ -9,7 +9,7 @@ namespace WB.Core.SharedKernels.DataCollection.DataTransferObjects.Synchronizati
         public InterviewSynchronizationDto()
         {
             Answers = new AnsweredQuestionSynchronizationDto[0];
-            this.FailedValidationConditions = new Dictionary<Identity, List<FailedValidationCondition>>();
+            this.FailedValidationConditions = new Dictionary<Identity, IReadOnlyList<FailedValidationCondition>>();
         }
 
         public InterviewSynchronizationDto(Guid id, 
@@ -26,7 +26,7 @@ namespace WB.Core.SharedKernels.DataCollection.DataTransferObjects.Synchronizati
             HashSet<InterviewItemId> validAnsweredQuestions,
             HashSet<InterviewItemId> invalidAnsweredQuestions,
             Dictionary<InterviewItemId, RosterSynchronizationDto[]> rosterGroupInstances,
-            Dictionary<Identity, List<FailedValidationCondition>> failedValidationConditions,
+            Dictionary<Identity, IReadOnlyList<FailedValidationCondition>> failedValidationConditions,
             bool wasCompleted,
             bool createdOnClient = false)
         {
@@ -66,7 +66,7 @@ namespace WB.Core.SharedKernels.DataCollection.DataTransferObjects.Synchronizati
         public HashSet<InterviewItemId> ValidAnsweredQuestions { get;  set; }
         public HashSet<InterviewItemId> InvalidAnsweredQuestions { get;  set; }
 
-        public Dictionary<Identity, List<FailedValidationCondition>> FailedValidationConditions { get; set; } 
+        public Dictionary<Identity, IReadOnlyList<FailedValidationCondition>> FailedValidationConditions { get; set; } 
         
         public Dictionary<InterviewItemId, RosterSynchronizationDto[]> RosterGroupInstances { get; set; }
 
