413587f1176817d9c9116773d98e642042445bca
Slava Scherbak <slava.scherbak@gmail.com>
2016-02-09 16:49:59 -0500

    Tests: added tests for questionnaire content version. KP-6432

---
+++ src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/InterviewerQuestionnairesControllerTests/when_getting_questionnaire_and_interviewer_content_version_is_less_then_in_questionnaire.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using Machine.Specifications;
+using Moq;
+using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
+using WB.Core.SharedKernels.DataCollection.Views.Questionnaire;
+using WB.Core.SharedKernels.SurveyManagement.Web.Api.Interviewer;
+using It = Machine.Specifications.It;
+
+namespace WB.Tests.Unit.SharedKernels.SurveyManagement.InterviewerQuestionnairesControllerTests
+{
+    public class when_getting_questionnaire_and_interviewer_content_version_is_less_then_in_questionnaire
+    {
+        Establish context = () =>
+        {
+            var questionnaire = Mock.Of<QuestionnaireDocumentVersioned>(x => x.QuestionnaireContentVersion == HqQuestionnaireContentVersion);
+            var questionnaireStorage = Mock.Of<IReadSideKeyValueStorage<QuestionnaireDocumentVersioned>>(x => x.GetById("11111111111111111111111111111111$1") == questionnaire);
+            controller = Create.Controller.InterviewerQuestionnaires(questionnaireStorage);
+        };
+
+        Because of = () =>
+            response = controller.Get(Id.g1, 1, InterviewerQuestionnaireContentVersion);
+            
+        It should_return_message_to_upgrade = () =>
+            response.StatusCode.ShouldEqual(HttpStatusCode.UpgradeRequired);
+
+        private static HttpResponseMessage response;
+        private static InterviewerQuestionnairesController controller;
+        private static readonly int InterviewerQuestionnaireContentVersion = 7;
+        private static readonly int HqQuestionnaireContentVersion = 12;
+    }
+}
