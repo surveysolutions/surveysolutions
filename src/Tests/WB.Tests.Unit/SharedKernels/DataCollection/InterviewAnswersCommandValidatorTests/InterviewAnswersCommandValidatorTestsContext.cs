da25b8f94a2f624b6dbfce703cf84155f751d0f1
Roman Lytvyn <litvin.roma@gmail.com>
2016-02-09 12:42:25 +0200

    HQ: supervisors could not answer on questions if interview is approved by headquarters. KP-5470

---
+++ src/Tests/WB.Tests.Unit/SharedKernels/DataCollection/InterviewAnswersCommandValidatorTests/InterviewAnswersCommandValidatorTestsContext.cs
@@ -0,0 +1,14 @@
+﻿using Machine.Specifications;
+using WB.Core.SharedKernels.SurveyManagement.Implementation.Services;
+
+namespace WB.Tests.Unit.SharedKernels.DataCollection.InterviewAnswersCommandValidatorTests
+{
+    [Subject(typeof (InterviewAnswersCommandValidator))]
+    internal class InterviewAnswersCommandValidatorTestsContext
+    {
+        protected static InterviewAnswersCommandValidator CreateInterviewAnswersCommandValidator()
+        {
+            return new InterviewAnswersCommandValidator();
+        }
+    }
+}
