c370ff3dabb9ab52b321158a0483d0636ac6c05d
slava <slava.scherbak@gmail.com>
2016-02-12 15:14:03 -0500

    HQ: removed supervisor from list of interviewers. KP-6728

--- src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/InterviewersViewFactoryTests/when_loading_interviewers_for_supervisor.cs
+++ src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/InterviewersViewFactoryTests/when_loading_interviewers_for_supervisor.cs
@@ -3,12 +3,10 @@ using System.Linq;
 using Machine.Specifications;
 using WB.Core.SharedKernels.DataCollection.Views;
 using WB.Core.SharedKernels.SurveyManagement.Views.Interviewer;
-using It = Machine.Specifications.It;
-
 
 namespace WB.Tests.Unit.SharedKernels.SurveyManagement.InterviewersViewFactoryTests
 {
-    internal class when_supervisor_filtering : InterviewersViewFactoryTestContext
+    internal class when_loading_interviewers_for_supervisor : InterviewersViewFactoryTestContext
     {
         Establish context = () =>
         {
@@ -20,16 +18,15 @@ namespace WB.Tests.Unit.SharedKernels.SurveyManagement.InterviewersViewFactoryTe
             UserDocument interviewer21 = CreateInterviewer(interviewer21Id, supervisor2, "interviewer21", "device21");
 
             var readerWithUsers = CreateQueryableReadSideRepositoryReaderWithUsers(
-                supervisor1, supervisor2, 
+                supervisor1, supervisor2,
                 interviewer11, interviewer12,
                 interviewer21);
 
             interviewersViewFactory = CreateInterviewersViewFactory(readerWithUsers);
 
-            interviewersInputModel = new InterviewersInputModel()
+            interviewersInputModel = new InterviewersInputModel
             {
                 ViewerId = supervisor1Id,
-                SearchBy = "interviewer",
                 Page = 0,
                 PageSize = 20
             };
@@ -44,7 +41,7 @@ namespace WB.Tests.Unit.SharedKernels.SurveyManagement.InterviewersViewFactoryTe
             result.Items.Count().ShouldEqual(2);
         };
 
-        It should_return_only_own_interviewers_in_correct_order = () =>
+        It should_return_only_own_interviewers = () =>
         {
             result.Items.Skip(0).First().UserName.ShouldEqual("interviewer11");
             result.Items.Skip(1).First().UserName.ShouldEqual("interviewer12");
@@ -55,10 +52,10 @@ namespace WB.Tests.Unit.SharedKernels.SurveyManagement.InterviewersViewFactoryTe
         private static InterviewersInputModel interviewersInputModel;
         private static InterviewersView result;
         private static IInterviewersViewFactory interviewersViewFactory;
-        private static Guid supervisor1Id = Guid.Parse("11111111111111111111111111111111");
-        private static Guid supervisor2Id = Guid.Parse("22222222222222222222222222222222");
-        private static Guid interviewer11Id = Guid.Parse("01111111111111111111111111111111");
-        private static Guid interviewer12Id = Guid.Parse("01222222222222222222222222222222");
-        private static Guid interviewer21Id = Guid.Parse("02111111111111111111111111111111");
+        private static readonly Guid supervisor1Id = Guid.Parse("11111111111111111111111111111111");
+        private static readonly Guid supervisor2Id = Guid.Parse("22222222222222222222222222222222");
+        private static readonly Guid interviewer11Id = Guid.Parse("01111111111111111111111111111111");
+        private static readonly Guid interviewer12Id = Guid.Parse("01222222222222222222222222222222");
+        private static readonly Guid interviewer21Id = Guid.Parse("02111111111111111111111111111111");
     }
 }
