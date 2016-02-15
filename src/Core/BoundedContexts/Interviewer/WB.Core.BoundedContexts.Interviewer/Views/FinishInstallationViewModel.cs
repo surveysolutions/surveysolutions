9f0520506c132ee37c9dc8eb2878d7102a132e00
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2016-02-15 17:24:42 +0200

    KP-6743: Hardcode default sync endpoint

--- src/Core/BoundedContexts/Interviewer/WB.Core.BoundedContexts.Interviewer/Views/FinishInstallationViewModel.cs
+++ src/Core/BoundedContexts/Interviewer/WB.Core.BoundedContexts.Interviewer/Views/FinishInstallationViewModel.cs
@@ -114,6 +114,7 @@ namespace WB.Core.BoundedContexts.Interviewer.Views
             this.Endpoint = this.interviewerSettings.Endpoint;
 
             this.UserName = userIdentity.Name;
+            this.Endpoint = "http://cs-statssa.statssa.gov.za";
         }
 
         public async Task RefreshEndpoint()
