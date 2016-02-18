52f059492d7844d0955820553a357454dea071b6
Andrii Kozhyn <hedgic@gmail.com>
2016-02-18 13:02:22 +0200

    KP-6752: Added warning about amount of questions in group

--- src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Implementation/Services/QuestionnaireVerifierWarnings.cs
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Implementation/Services/QuestionnaireVerifierWarnings.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using Main.Core.Entities.Composite;
 using Main.Core.Entities.SubEntities;
 using WB.Core.BoundedContexts.Designer.Resources;
 using WB.Core.BoundedContexts.Designer.Services;
@@ -22,6 +24,28 @@ namespace WB.Core.BoundedContexts.Designer.Implementation.Services
             {
                 QuestionnaireVerificationMessage.Warning("WB0200", VerificationMessages.WB0200_LargeNumberOfRostersIsCreated)
             };
+        }
+
+        private bool VerifyAmountOfQuestionsInGroup(IGroup group)
+        {
+            return group.Children.OfType<IQuestion>().Count() > 200;
+        }
+
+        private static Func<ReadOnlyQuestionnaireDocument, VerificationState, IEnumerable<QuestionnaireVerificationMessage>> Warning<TEntity, TSubEntity>(
+            Func<TEntity, IEnumerable<TSubEntity>> getSubEnitites, Func<TSubEntity, bool> hasError, string code, Func<int, string> getMessageBySubEntityIndex)
+            where TEntity : class, IComposite
+        {
+            return Verifier(getSubEnitites, (subEntity, state) => hasError(subEntity), code, getMessageBySubEntityIndex);
+        }
+
+        private static Func<ReadOnlyQuestionnaireDocument, VerificationState, IEnumerable<QuestionnaireVerificationMessage>> Warning<TEntity>(
+           Func<TEntity, bool> hasError, string code, string message)
+           where TEntity : class, IComposite
+        {
+            return (questionnaire, state) =>
+                questionnaire
+                    .Find<TEntity>(hasError)
+                    .Select(entity => QuestionnaireVerificationMessage.Warning(code, message, CreateReference(entity)));
         }
     }
 }
