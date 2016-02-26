0d5432b33358664f9f59d3ea4604318a25f1e3dd
Pavel Stovpivskyi <630198+spuv@users.noreply.github.com>
2016-02-26 13:17:24 +0200

    KP-6747 Research usage of fragments

--- src/UI/Shared/WB.UI.Shared.Enumerator/Activities/BaseFragment.cs
+++ src/UI/Shared/WB.UI.Shared.Enumerator/Activities/BaseFragment.cs
@@ -14,44 +14,10 @@ namespace WB.UI.Shared.Enumerator.Activities
     {
         protected abstract int ViewResourceId { get; }
 
-        /*        protected override void OnCreate(Bundle bundle)
-                {
-                    var setup = MvxAndroidSetupSingleton.EnsureSingletonAvailable(ApplicationContext);
-                    setup.EnsureInitialized();
-
-                    base.OnCreate(bundle);
-                }
-
-                protected override void OnViewModelSet()
-                {
-                    base.OnViewModelSet();
-                    this.SetContentView(this.ViewResourceId);
-                }*/
-
-
-        public override Android.Views.View OnCreateView(Android.Views.LayoutInflater inflater, Android.Views.ViewGroup container, Bundle savedInstanceState)
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
             var ignored = base.OnCreateView(inflater, container, savedInstanceState);
             return this.BindingInflate(ViewResourceId, null);
         }
-
-        /* protected override void OnCreate(Bundle bundle)
-         {
-             base.OnCreate(bundle);
-             SetContentView(Resource.Layout.FirstView);
-
-             var sub = (SubFrag)SupportFragmentManager.FindFragmentById(Resource.Id.sub1);
-             sub.ViewModel = ((FirstViewModel)ViewModel).Sub;
-
-             var dub = (DubFrag)SupportFragmentManager.FindFragmentById(Resource.Id.dub1);
-             dub.ViewModel = ((FirstViewModel)ViewModel).Sub;
-         }*/
-
-        /*
-                public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
-                {
-                    base.OnCreateView(inflater, container, savedInstanceState);
-                    return this.BindingInflate(ViewResourceId, null);
-                }*/
     }
 }
---
+++ src/UI/Shared/WB.UI.Shared.Enumerator/CustomBindings/FrameLayoutCurrentScreenBinding.cs
@@ -0,0 +1,34 @@
+﻿using Android.App;
+using Android.Graphics;
+using Android.Support.V4.App;
+using Android.Widget;
+using MvvmCross.Core.ViewModels;
+using MvvmCross.Droid.Support.V7.Fragging.Fragments;
+using MvvmCross.Platform;
+using MvvmCross.Platform.Droid.Platform;
+using WB.Core.SharedKernels.Enumerator.ViewModels.InterviewDetails;
+using WB.UI.Shared.Enumerator.Activities;
+
+namespace WB.UI.Shared.Enumerator.CustomBindings
+{
+    public class FrameLayoutCurrentScreenBinding : BaseBinding<FrameLayout, MvxViewModel>
+    {
+        public FrameLayoutCurrentScreenBinding(FrameLayout frameLayout)
+            : base(frameLayout) {}
+
+        protected override void SetValueToView(FrameLayout frameLayout, MvxViewModel frameViewModel)
+        {
+            MvxFragment mvxFragment = frameViewModel is ActiveStageViewModel
+                ? Mvx.Resolve<InterviewEntitiesListFragment>()
+                : (MvxFragment)Mvx.Resolve<CompleteInterviewFragment>();
+
+            mvxFragment.ViewModel = frameViewModel;
+
+            IMvxAndroidCurrentTopActivity topActivity = Mvx.Resolve<IMvxAndroidCurrentTopActivity>();
+            var activity = (FragmentActivity)topActivity.Activity;
+            var trans = activity.SupportFragmentManager.BeginTransaction();
+            trans.Replace(frameLayout.Id, mvxFragment);
+            trans.Commit();
+        }
+    }
+}
