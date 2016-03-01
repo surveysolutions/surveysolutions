64536d106ef1dbeea654d982fcaef413de18f83f
Andrii Kozhyn <hedgic@gmail.com>
2016-03-01 11:47:31 +0200

    KP-3151: Added a warning about single question in roster

---
+++ src/Tests/WB.Tests.Unit/BoundedContexts/Designer/QuestionnaireVerificationTests/when_roster_has_single_question.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Machine.Specifications;
+using Main.Core.Documents;
+using WB.Core.BoundedContexts.Designer.Implementation.Services;
+using WB.Core.BoundedContexts.Designer.ValueObjects;
+
+namespace WB.Tests.Unit.BoundedContexts.Designer.QuestionnaireVerificationTests
+{
+    internal class when_roster_has_single_question : QuestionnaireVerifierTestsContext
+    {
+        Establish context = () =>
+        {
+            rosterId = Guid.Parse("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+            questionnaire = CreateQuestionnaireDocumentWithOneChapter(Create.Roster(
+                rosterId: rosterId,
+                children: new[] {Create.Question()}));
+            verifier = CreateQuestionnaireVerifier();
+        };
+
+        Because of = () => { errors = verifier.Verify(questionnaire); };
+
+        It should_contain_WB0203_warning = () => errors.ShouldContain(item => item.Code == "WB0203" && item.MessageLevel == VerificationMessageLevel.Warning);
+
+        It should_reference_to_a_roster = () => errors.FirstOrDefault(item => item.Code == "WB0203")
+            .References.First().Type.ShouldEqual(QuestionnaireVerificationReferenceType.Group);
+
+        It should_reference_to_a_roster_with_single_question = () => errors.FirstOrDefault(item => item.Code == "WB0203")
+            .References.First().Id.ShouldEqual(rosterId);
+
+        It should_add_single_reference = () => errors.FirstOrDefault(item => item.Code == "WB0203").References.Count.ShouldEqual(1);
+
+        static QuestionnaireVerifier verifier;
+        static QuestionnaireDocument questionnaire;
+        static IEnumerable<QuestionnaireVerificationMessage> errors;
+        static Guid rosterId;
+    }
+}
