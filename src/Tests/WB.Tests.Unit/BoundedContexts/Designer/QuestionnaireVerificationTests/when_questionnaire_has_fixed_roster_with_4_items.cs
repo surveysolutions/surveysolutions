71ca748481187c8d08a8f0fc0bee659241dcd8e5
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2016-03-04 18:36:29 +0200

    Verifier: corrected logic of warning WB0207 on fixed roster with 3 or less items, covered with tests KP-6783

--- src/Tests/WB.Tests.Unit/BoundedContexts/Designer/QuestionnaireVerificationTests/when_questionnaire_has_fixed_roster_with_4_items.cs
+++ src/Tests/WB.Tests.Unit/BoundedContexts/Designer/QuestionnaireVerificationTests/when_questionnaire_has_fixed_roster_with_4_items.cs
@@ -1,6 +1,4 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using Machine.Specifications;
 using Main.Core.Documents;
 using Main.Core.Entities.Composite;
@@ -9,20 +7,13 @@ using WB.Core.BoundedContexts.Designer.ValueObjects;
 
 namespace WB.Tests.Unit.BoundedContexts.Designer.QuestionnaireVerificationTests
 {
-    internal class when_questionnaire_has_fixed_roster_with_3_descendants : QuestionnaireVerifierTestsContext
+    internal class when_questionnaire_has_fixed_roster_with_4_items : QuestionnaireVerifierTestsContext
     {
         Establish context = () =>
         {
             questionnaire = Create.QuestionnaireDocumentWithOneChapter(children: new IComposite[]
             {
-                Create.FixedRoster(rosterId: rosterId, children: new IComposite[]
-                {
-                    Create.Group(children: new IComposite[]
-                    {
-                        Create.Group(),
-                        Create.TextQuestion(),
-                    }),
-                }),
+                Create.FixedRoster(fixedTitles: new[] { "1", "2", "3", "4" }),
             });
 
             verifier = CreateQuestionnaireVerifier();
@@ -31,15 +22,11 @@ namespace WB.Tests.Unit.BoundedContexts.Designer.QuestionnaireVerificationTests
         Because of = () =>
             messages = verifier.Verify(questionnaire);
 
-        It should_return_warning_WB0207 = () =>
-            messages.ShouldContainWarning("WB0207");
-
-        It should_put_reference_to_that_roster_to_message_WB0207 = () =>
-            messages.GetWarning("WB0207").References.Single().Id.ShouldEqual(rosterId);
+        It should_not_return_message_WB0207 = () =>
+            messages.ShouldNotContainMessage("WB0207");
 
         private static QuestionnaireDocument questionnaire;
         private static QuestionnaireVerifier verifier;
         private static IEnumerable<QuestionnaireVerificationMessage> messages;
-        private static Guid rosterId = Guid.Parse("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
     }
 }
