18e2835f054724e08506ff46d56f9f153bff7b96
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2016-03-07 15:55:08 -0500

    Designer\CodeGeneration: KP-6796 change interface of linked question filtering results

---
+++ src/Core/SharedKernels/DataCollection/DataCollection.Portable/LinkedQuestionOptionsChanges.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WB.Core.SharedKernels.DataCollection
+{
+    public class LinkedQuestionOptionsChanges
+    {
+        internal LinkedQuestionOptionsChanges() : this(new List<LinkedQuestionOption>(), new List<LinkedQuestionOption>()) { }
+        public LinkedQuestionOptionsChanges(List<LinkedQuestionOption> optionsDeclaredEnabled, List<LinkedQuestionOption> optionsDeclaredDisabled)
+        {
+            this.OptionsDeclaredEnabled = optionsDeclaredEnabled;
+            this.OptionsDeclaredDisabled = optionsDeclaredDisabled;
+        }
+
+        public List<LinkedQuestionOption> OptionsDeclaredEnabled { get; private set; }
+        public List<LinkedQuestionOption> OptionsDeclaredDisabled { get; private set; }
+
+        public void Clear()
+        {
+            this.OptionsDeclaredEnabled.Clear();
+            this.OptionsDeclaredDisabled.Clear();
+        }
+    }
+
+    public class LinkedQuestionOption
+    {
+        public Guid LinkedQuestionId { get; set; }
+        public RosterVector RosterVector { get; set; }
+    }
+}
