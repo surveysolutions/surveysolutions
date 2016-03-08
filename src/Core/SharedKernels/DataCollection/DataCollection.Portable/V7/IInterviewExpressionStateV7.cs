6733a2f7f554b2534e87f52ddcf4fe9c1e028ed5
Anastasya Kholodova <nastya.kholodova@gmail.com>
2016-03-08 10:46:36 -0500

    Designer\CodeGeneration: KP-6796 save previous enablement state of linked option

--- src/Core/SharedKernels/DataCollection/DataCollection.Portable/V7/IInterviewExpressionStateV7.cs
+++ src/Core/SharedKernels/DataCollection/DataCollection.Portable/V7/IInterviewExpressionStateV7.cs
@@ -6,6 +6,8 @@ namespace WB.Core.SharedKernels.DataCollection.V7
     public interface IInterviewExpressionStateV7: IInterviewExpressionStateV6
     {
         LinkedQuestionOptionsChanges ProcessLinkedQuestionFilters();
+        void EnableLinkedQuestionOptions(Identity[] options);
+        void DisableLinkedQuestionOptions(Identity[] options);
         new IInterviewExpressionStateV7 Clone();
     }
 }
