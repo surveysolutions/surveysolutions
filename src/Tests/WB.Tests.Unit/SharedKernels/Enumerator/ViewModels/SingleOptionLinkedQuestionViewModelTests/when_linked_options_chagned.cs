99a2693ccc52717b44955f7e871cd285b9058804
Andrii Kozhyn <hedgic@gmail.com>
2016-03-09 18:11:19 +0200

    KP-6798: Added basic handling of LinkedOptionsChanged event

---
+++ src/Tests/WB.Tests.Unit/SharedKernels/Enumerator/ViewModels/SingleOptionLinkedQuestionViewModelTests/when_linked_options_chagned.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using Machine.Specifications;
+using NSubstitute;
+using WB.Core.GenericSubdomains.Portable;
+using WB.Core.SharedKernels.DataCollection;
+using WB.Core.SharedKernels.DataCollection.Aggregates;
+using WB.Core.SharedKernels.DataCollection.Events.Interview.Dtos;
+using WB.Core.SharedKernels.Enumerator.Aggregates;
+using WB.Core.SharedKernels.Enumerator.Entities.Interview;
+using WB.Core.SharedKernels.Enumerator.ViewModels.InterviewDetails.Questions;
+
+namespace WB.Tests.Unit.SharedKernels.Enumerator.ViewModels.SingleOptionLinkedQuestionViewModelTests
+{
+    internal class when_linked_options_chagned : SingleOptionLinkedQuestionViewModelTestsContext
+    {
+        Establish context = () =>
+        {
+            linkSourceQuestionId = Create.Identity(Guid.Parse("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"), RosterVector.Empty);
+            linkedQuestionId = Create.Identity(Guid.Parse("BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB"), RosterVector.Empty);
+            interviewId = Guid.Parse("CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC").FormatGuid();
+
+            eventData = new[]
+            {
+                new ChangedLinkedOptions(linkedQuestionId, 
+                    new[]
+                    {
+                        Create.RosterVector(1),
+                        Create.RosterVector(2)
+                    }),
+            };
+
+            linkedOptionTextInInterview = "answer in init";
+
+            IStatefulInterview interview = Substitute.For<IStatefulInterview>();
+            interview.FindAnswersOfReferencedQuestionForLinkedQuestion(linkSourceQuestionId.Id, linkedQuestionId)
+                    .Returns(new List<BaseInterviewAnswer> { Create.TextAnswer(linkedOptionTextInInterview) });
+            interview.FindBaseAnswerByOrDeeperRosterLevel(linkSourceQuestionId.Id, Create.RosterVector(1))
+                     .Returns(Create.TextAnswer("one"));
+            interview.FindBaseAnswerByOrDeeperRosterLevel(linkSourceQuestionId.Id, Create.RosterVector(2))
+                     .Returns(Create.TextAnswer("two"));
+
+            IQuestionnaire questionnaire = Substitute.For<IQuestionnaire>();
+            questionnaire.GetQuestionReferencedByLinkedQuestion(linkedQuestionId.Id)
+                         .Returns(linkSourceQuestionId.Id);
+
+            viewModel = Create.SingleOptionLinkedQuestionViewModel(interview: interview, questionnaire: questionnaire);
+            viewModel.Init(interviewId, linkedQuestionId, Create.NavigationState());
+        };
+
+        Because of = () => viewModel.Handle(Create.Event.LinkedOptionsChanged(eventData));
+
+        It should_replace_answer_title_from_add_provided_by_event_options_to_self = () => viewModel.Options.Count.ShouldEqual(2);
+
+        static SingleOptionLinkedQuestionViewModel viewModel;
+        static Identity linkSourceQuestionId;
+        static Identity linkedQuestionId;
+        static string interviewId;
+        static ChangedLinkedOptions[] eventData;
+        static string linkedOptionTextInInterview;
+    }
+}
