7975cdc189290a0dd8464d81df0695fdc3dfb5e9
slava <slava.scherbak@gmail.com>
2016-03-10 18:31:25 -0500

    Attachments: fixed questionnaire cloning. KP-6778

--- src/Core/SharedKernels/Questionnaire/WB.Core.SharedKernels.Questionnaire/Documents/Attachment.cs
+++ src/Core/SharedKernels/Questionnaire/WB.Core.SharedKernels.Questionnaire/Documents/Attachment.cs
@@ -7,5 +7,15 @@ namespace WB.Core.SharedKernels.SurveySolutions.Documents
         public Guid AttachmentId { get; set; }
         public string Name { get; set; }
         public string FileName { get; set; }
+
+        public Attachment Clone()
+        {
+            return new Attachment
+            {
+                AttachmentId = this.AttachmentId,
+                Name = this.Name,
+                FileName = this.FileName
+            };
+        }
     }
 }
