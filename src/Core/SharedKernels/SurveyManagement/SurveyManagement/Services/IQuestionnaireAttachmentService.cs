68045db123aeffafaadb8c78574b1e373f03c8c2
Roman Lytvyn <litvin.roma@gmail.com>
2016-03-15 15:13:13 +0200

    HQ/SV: attachments. modified service/views/mappings. KP-6800

--- src/Core/SharedKernels/SurveyManagement/SurveyManagement/Services/IQuestionnaireAttachmentService.cs
+++ src/Core/SharedKernels/SurveyManagement/SurveyManagement/Services/IQuestionnaireAttachmentService.cs
@@ -1,16 +1,9 @@
-using System;
-using System.Collections.Generic;
-using WB.Core.SharedKernels.DataCollection.Implementation.Entities;
-using WB.Core.SharedKernels.SurveyManagement.Views.QuestionnaireAttachments;
-
 namespace WB.Core.SharedKernels.SurveyManagement.Services
 {
     public interface IQuestionnaireAttachmentService
     {
-        void SaveAttachment(QuestionnaireIdentity questionnaireIdentity, string md5OfAttachment, QuestionnaireAttachmentType type,
-            string contentType, byte[] binaryContent);
-        byte[] GetAttachment(string md5OfAttachment);
-        IEnumerable<QuestionnaireAttachment> GetAttachments(QuestionnaireIdentity questionnaireIdentity);
-        void DeleteAttachment(QuestionnaireIdentity questionnaireIdentity, string md5OfAttachment);
+        void SaveAttachment(string attachmentHash, string contentType, byte[] content);
+        byte[] GetAttachment(string attachmentHash);
+        void DeleteAttachment(string attachmentHash);
     }
 }
