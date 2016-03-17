0b504d9fd0f8879154d81a6bd0d2b807f010b9a1
Pavel Stovpivskyi <sby@rambler.ru>
2016-03-17 13:38:35 +0200

    Interviewer Attachments
    KP-6804

--- src/Core/SharedKernels/SurveyManagement/SurveyManagement.Web/Api/Interviewer/v2/QuestionnairesApiV2Controller.cs
+++ src/Core/SharedKernels/SurveyManagement/SurveyManagement.Web/Api/Interviewer/v2/QuestionnairesApiV2Controller.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Linq;
+using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Web.Http;
 using Main.Core.Entities.SubEntities;
 using WB.Core.GenericSubdomains.Portable.Services;
@@ -8,6 +11,7 @@ using WB.Core.SharedKernels.DataCollection.Implementation.Accessors;
 using WB.Core.SharedKernels.DataCollection.Repositories;
 using WB.Core.SharedKernels.SurveyManagement.Factories;
 using WB.Core.SharedKernels.SurveyManagement.Views.Questionnaire;
+using WB.Core.SharedKernels.SurveyManagement.Views.SynchronizationLog;
 using WB.Core.SharedKernels.SurveyManagement.Web.Code;
 
 namespace WB.Core.SharedKernels.SurveyManagement.Web.Api.Interviewer.v2
@@ -39,8 +43,26 @@ namespace WB.Core.SharedKernels.SurveyManagement.Web.Api.Interviewer.v2
         public override void LogQuestionnaireAsSuccessfullyHandled(Guid id, int version) => base.LogQuestionnaireAsSuccessfullyHandled(id, version);
         [HttpPost]
         public override void LogQuestionnaireAssemblyAsSuccessfullyHandled(Guid id, int version) => base.LogQuestionnaireAssemblyAsSuccessfullyHandled(id, version);
+
         [HttpGet]
-        public override HttpResponseMessage GetAttachments(Guid id, int version) => base.GetAttachments(id, version);
-        
+        [WriteToSyncLog(SynchronizationLogType.GetQuestionnaireAttachments)]
+        public HttpResponseMessage GetAttachments(Guid id, int version)
+        {
+            var questionnaireDocumentVersioned = this.plainQuestionnaireRepository.GetQuestionnaireDocument(id, version);
+
+            if (questionnaireDocumentVersioned == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            var attachmentIds = questionnaireDocumentVersioned.Attachments.Select(a => a.ContentId).ToList();
+
+            var response = this.Request.CreateResponse(attachmentIds);
+            response.Headers.CacheControl = new CacheControlHeaderValue()
+            {
+                Public = true,
+                MaxAge = TimeSpan.FromDays(10)
+            };
+
+            return response;
+        }
     }
 }
