84f0c8efb1e5643c19aba4051b7b905d664471b5
Roman Lytvyn <litvin.roma@gmail.com>
2016-03-17 16:03:45 +0200

    HQ/SV: added tests for attachment content service. KP-6800

---
+++ src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/ServiceTests/AttachmentContentServiceTests/when_deleting_attachment_content.cs
@@ -0,0 +1,28 @@
+﻿using Machine.Specifications;
+using Moq;
+using WB.Core.Infrastructure.PlainStorage;
+using WB.Core.SharedKernels.SurveyManagement.Implementation.Services;
+using WB.Core.SharedKernels.SurveyManagement.Views.Questionnaire;
+using WB.Tests.Unit.SharedKernels.SurveyManagement.ServiceTests.DataExport.CommentsExporterTests;
+using It = Machine.Specifications.It;
+
+namespace WB.Tests.Unit.SharedKernels.SurveyManagement.ServiceTests.AttachmentContentServiceTests
+{
+    class when_deleting_attachment_content : CommentsExporterTestsContext
+    {
+        Establish context = () =>
+        {
+            attachmentContentService = Create.CreateAttachmentContentService(mockOfAttachmentContentPlainStorage.Object);
+        };
+
+        Because of = () =>
+            attachmentContentService.DeleteAttachmentContent(contentHash);
+
+        It should_remove_attachment_content_to_plain_storage = () =>
+            mockOfAttachmentContentPlainStorage.Verify(x => x.Remove(contentHash), Times.Once);
+
+        private static AttachmentContentService attachmentContentService;
+        private static readonly Mock<IPlainStorageAccessor<AttachmentContent>> mockOfAttachmentContentPlainStorage = new Mock<IPlainStorageAccessor<AttachmentContent>>();
+        private static string contentHash = "content hash";
+    }
+}
