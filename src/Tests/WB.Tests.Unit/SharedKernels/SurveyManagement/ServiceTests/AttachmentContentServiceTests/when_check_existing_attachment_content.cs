e76a7bcb3ce3fe65a347d270ead11141b83d2fad
Roman Lytvyn <litvin.roma@gmail.com>
2016-03-17 16:03:45 +0200

    HQ/SV: added tests for attachment content service. KP-6800

---
+++ src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/ServiceTests/AttachmentContentServiceTests/when_check_existing_attachment_content.cs
@@ -0,0 +1,33 @@
+﻿using Machine.Specifications;
+using WB.Core.SharedKernels.SurveyManagement.Implementation.Services;
+using WB.Core.SharedKernels.SurveyManagement.Views.Questionnaire;
+using WB.Tests.Unit.SharedKernels.SurveyManagement.ServiceTests.DataExport.CommentsExporterTests;
+using It = Machine.Specifications.It;
+
+namespace WB.Tests.Unit.SharedKernels.SurveyManagement.ServiceTests.AttachmentContentServiceTests
+{
+    class when_check_existing_attachment_content : CommentsExporterTestsContext
+    {
+        Establish context = () =>
+        {
+            var attachmentContentPlainStorage = new TestPlainStorage<AttachmentContent>();
+            attachmentContentPlainStorage.Store(
+                new AttachmentContent
+                {
+                    ContentHash = contentHash,
+                    Content = new byte[] { 1, 2, 3 }
+                }, contentHash);
+            attachmentContentService = Create.CreateAttachmentContentService(attachmentContentPlainStorage);
+        };
+
+        Because of = () =>
+            isAttachmentContentExists = attachmentContentService.HasAttachmentContent(contentHash);
+
+        It should_attachment_content_exists_in_plain_storage = () =>
+            isAttachmentContentExists.ShouldBeTrue();
+
+        private static AttachmentContentService attachmentContentService;
+        private static string contentHash = "content id";
+        private static bool isAttachmentContentExists;
+    }
+}
