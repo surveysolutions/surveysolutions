8005af4625cefc2710ed4e61f9cdcff0709ac620
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2016-03-18 19:13:45 -0400

    KP-6865 Designer: Show thumbnail instead of full size image for attachment

--- src/UI/Designer/WB.UI.Designer/AttachmentHandler.ashx.cs
+++ src/UI/Designer/WB.UI.Designer/AttachmentHandler.ashx.cs
@@ -1,15 +1,20 @@
 ﻿using System;
+using System.IO;
 using System.Web;
 using System.Web.Routing;
 using Microsoft.Practices.ServiceLocation;
 using WB.Core.BoundedContexts.Designer.Implementation.Services.AttachmentService;
 using WB.Core.BoundedContexts.Designer.Services;
 using WB.Core.Infrastructure.PlainStorage;
+using ImageResizer;
 
 namespace WB.UI.Designer
 {
     public class AttachmentHandler : IHttpHandler
     {
+        private const string thumbnailTransform = "thumbnail";
+        private const int defaultImageSizeToScale = 156;
+
         private IAttachmentService attachmentService => ServiceLocator.Current.GetInstance<IAttachmentService>();
 
         IPlainTransactionManager TransactionManager => ServiceLocator.Current.GetInstance<IPlainTransactionManager>();
@@ -24,6 +29,16 @@ namespace WB.UI.Designer
         public void ProcessRequest(HttpContext context)
         {
             var attachmentId = Guid.Parse(context.Request.RequestContext.RouteData.Values["attachmentId"].ToString());
+            var transformationType = context.Request.RequestContext.RouteData.Values["transform"].ToString();
+
+            TransformationType transform = TransformationType.unknown;
+            if (!string.IsNullOrWhiteSpace(transformationType))
+            {
+                if (!Enum.TryParse(transformationType, out transform))
+                {
+                    Return404(context);
+                }
+            }
 
             QuestionnaireAttachment attachment;
 
@@ -40,27 +55,85 @@ namespace WB.UI.Designer
             }
             if (attachment == null)
             {
-                context.Response.ClearHeaders();
-                context.Response.Clear();
-
-                context.Response.StatusCode = 404;
-                context.Response.SuppressContent = true;
+                Return404(context);
             }
             else
             {
-                TimeSpan cacheTime = new TimeSpan(2, 0, 0);
-                context.Response.Cache.VaryByParams["*"] = true;
-                context.Response.Cache.SetExpires(DateTime.Now.Add(cacheTime));
-                context.Response.Cache.SetMaxAge(cacheTime);
-                context.Response.Cache.SetCacheability(HttpCacheability.Public);
+                if (transform == TransformationType.thumbnail)
+                    TransformContent(context, attachment, transformationType);
+                else
+                    GetOriginalContent(context, attachment);
+            }
+        }
 
-                context.Response.Cache.SetETag(attachment.AttachmentContentId);
-                context.Response.ContentType = attachment.ContentType;
-                context.Response.AddHeader("content-disposition", $"attachment; filename={attachment.FileName}");
-                context.Response.BinaryWrite(attachment.Content);
+        private static void Return404(HttpContext context)
+        {
+            context.Response.ClearHeaders();
+            context.Response.Clear();
+
+            context.Response.StatusCode = 404;
+            context.Response.SuppressContent = true;
+        }
+
+        private static void GetOriginalContent(HttpContext context, QuestionnaireAttachment attachment)
+        {
+            TimeSpan cacheTime = new TimeSpan(2, 0, 0);
+            context.Response.Cache.VaryByParams["*"] = true;
+            context.Response.Cache.SetExpires(DateTime.Now.Add(cacheTime));
+            context.Response.Cache.SetMaxAge(cacheTime);
+            context.Response.Cache.SetCacheability(HttpCacheability.Public);
+
+            context.Response.Cache.SetETag(attachment.AttachmentContentId);
+            context.Response.ContentType = attachment.ContentType;
+            context.Response.AddHeader("content-disposition", $"attachment; filename={attachment.FileName}");
+            context.Response.BinaryWrite(attachment.Content);
+        }
+
+        private static void TransformContent(HttpContext context, QuestionnaireAttachment attachment, string transformation)
+        {
+            //later should handle video and produce image preview 
+
+            var defaultSizeToScale = defaultImageSizeToScale;
+            int size;
+            if (int.TryParse(context.Request.RequestContext.RouteData.Values["size"].ToString(), out size))
+                defaultSizeToScale = size;
+
+            var resizeSettings = new ResizeSettings
+            {
+                MaxWidth = defaultSizeToScale,
+                MaxHeight = defaultSizeToScale
+            };
+
+            byte[] resultStream = GetTrasformedContent(attachment, resizeSettings);
+
+            TimeSpan cacheTime = new TimeSpan(2, 0, 0);
+            context.Response.Cache.VaryByParams["*"] = true;
+            context.Response.Cache.SetExpires(DateTime.Now.Add(cacheTime));
+            context.Response.Cache.SetMaxAge(cacheTime);
+            context.Response.Cache.SetCacheability(HttpCacheability.Public);
+
+            context.Response.Cache.SetETag(attachment.AttachmentContentId);
+            context.Response.ContentType = attachment.ContentType;
+            context.Response.AddHeader("content-disposition", $"attachment; filename={attachment.FileName}");
+            context.Response.BinaryWrite(resultStream);
+        }
+
+        private static byte[] GetTrasformedContent(QuestionnaireAttachment attachment, ResizeSettings resizeSettings)
+        {
+            using (var outputStream = new MemoryStream())
+            {
+                ImageBuilder.Current.Build(attachment.Content, outputStream, resizeSettings);
+                return outputStream.ToArray();
             }
         }
 
         public bool IsReusable => false;
     }
+
+    public enum TransformationType
+    {
+        unknown = 0,
+        thumbnail = 1
+    }
+
 }
