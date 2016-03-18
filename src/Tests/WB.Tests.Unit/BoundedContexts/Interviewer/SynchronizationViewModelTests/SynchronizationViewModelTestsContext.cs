2c9082ed5467e7eeb99633b6f707574ef28e9e7b
Pavel Stovpivskyi <sby@rambler.ru>
2016-03-18 18:20:59 +0200

    Added test for SynchronizationViewModel

---
+++ src/Tests/WB.Tests.Unit/BoundedContexts/Interviewer/SynchronizationViewModelTests/SynchronizationViewModelTestsContext.cs
@@ -0,0 +1,12 @@
+﻿using Machine.Specifications;
+using WB.Core.BoundedContexts.Interviewer.Views;
+
+
+namespace WB.Tests.Unit.BoundedContexts.Interviewer.SynchronizationViewModelTests
+{
+    [Subject(typeof(SynchronizationViewModel))]
+    internal class SynchronizationViewModelTestsContext
+    {
+
+    }
+}
