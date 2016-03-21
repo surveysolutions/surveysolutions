7bb8df84f7013c04cc3b4501f863eac376d1ffb6
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2016-03-21 17:58:04 -0400

    fix IE and image resize: KP-6802 HQ/SV: attachments in static texts on interview details page

--- src/Core/SharedKernels/SurveyManagement/SurveyManagement/Views/Interview/InterviewAttachmentViewModel.cs
+++ src/Core/SharedKernels/SurveyManagement/SurveyManagement/Views/Interview/InterviewAttachmentViewModel.cs
@@ -7,15 +7,18 @@ namespace WB.Core.SharedKernels.SurveyManagement.Views.Interview
     {
         private readonly string[] imageContentTypes = { "image/png", "image/jpg", "image/gif", "image/jpeg", "image/pjpeg" };
 
-        public InterviewAttachmentViewModel(string contentId, string contentType)
+        public InterviewAttachmentViewModel(string contentId, string contentType, string contentName)
         {
             this.ContentId = contentId;
             this.ContentType = contentType;
+            this.ContentName = contentName;
         }
 
         public string ContentId { get; set; }
         public string ContentType { get; set; }
-        
+
+        public string ContentName { get; set; }
+
         public bool IsImage => this.imageContentTypes.Contains(this.ContentType);
     }
     
