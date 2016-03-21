55a84688926929590a6a0c4c45d5d6de2f55a1d3
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2016-03-21 10:47:17 +0200

    KP-6868: Fixed usage of constructor in tests

--- src/Tests/WB.Tests.Unit/SharedKernels/Enumerator/ViewModels/MultiOptionLinkedQuestionViewModelTests/when_roster_titles_changed.cs
+++ src/Tests/WB.Tests.Unit/SharedKernels/Enumerator/ViewModels/MultiOptionLinkedQuestionViewModelTests/when_roster_titles_changed.cs
@@ -21,7 +21,7 @@ namespace WB.Tests.Unit.SharedKernels.Enumerator.ViewModels.MultiOptionLinkedQue
             var linkToQuestionId = Guid.Parse("CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC");
             var linkedQuestionId = Guid.Parse("DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD");
             topRosterId = Guid.Parse("EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE");
-            var questionIdentity = new Identity(linkedQuestionId, RosterVector.Empty);
+            var questionIdentity = Create.Identity(linkedQuestionId, RosterVector.Empty);
 
             var questionnaire = Create.QuestionnaireDocumentWithOneChapter(
                 Create.TextListQuestion(questionId: level1TriggerId),
