aa1862c7660426f5a1b9c7774769148f8f287234
Pavel Stovpivskyi <sby@rambler.ru>
2016-03-21 17:21:05 +0200

    Show default image if can show image

--- src/UI/Shared/WB.UI.Shared.Enumerator/Converters/InMemoryImageValueWithDefaultConverter.cs
+++ src/UI/Shared/WB.UI.Shared.Enumerator/Converters/InMemoryImageValueWithDefaultConverter.cs
@@ -11,14 +11,18 @@ namespace WB.UI.Shared.Enumerator.Converters
     {
         protected override Bitmap Convert(byte[] value, Type targetType, object parameter, CultureInfo culture)
         {
-            var options = new BitmapFactory.Options { InPurgeable = true };
-
             if (value != null)
-                return BitmapFactory.DecodeByteArray(value, 0, value.Length, options);
+            {
+                var options = new BitmapFactory.Options { InPurgeable = true };
+                var bitmap = BitmapFactory.DecodeByteArray(value, 0, value.Length, options);
+                if (bitmap != null)
+                    return bitmap;
+            }
 
             var mvxAndroidCurrentTopActivity = ServiceLocator.Current.GetInstance<IMvxAndroidCurrentTopActivity>();
             var resources = mvxAndroidCurrentTopActivity.Activity.Resources;
-            return BitmapFactory.DecodeResource(resources, Resource.Drawable.no_image_found, options);
+            var noImageOptions = new BitmapFactory.Options { InPurgeable = true };
+            return BitmapFactory.DecodeResource(resources, Resource.Drawable.no_image_found, noImageOptions);
         }
     }
 }
