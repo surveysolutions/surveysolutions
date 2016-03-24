24b3953ebf735e3ead80c40c6f1fcd95d995f6dc
Roman Lytvyn <litvin.roma@gmail.com>
2016-03-24 17:36:41 +0200

    D: manual merge rc to master by attachments. KP-6778

--- src/Tests/WB.Tests.Unit.Designer/BoundedContexts/AttachmentServiceTests/when_creating_attachment_content_id.cs
+++ src/Tests/WB.Tests.Unit.Designer/BoundedContexts/AttachmentServiceTests/when_creating_attachment_content_id.cs
@@ -3,7 +3,7 @@ using WB.Core.BoundedContexts.Designer.Implementation.Services.AttachmentService
 
 namespace WB.Tests.Unit.Designer.BoundedContexts.AttachmentServiceTests
 {
-    internal class when_getting_attachment_content_id : AttachmentServiceTestContext
+    internal class when_creating_attachment_content_id : AttachmentServiceTestContext
     {
         Establish context = () =>
         {
@@ -11,7 +11,7 @@ namespace WB.Tests.Unit.Designer.BoundedContexts.AttachmentServiceTests
         };
 
         Because of = () =>
-            expectedAttachmentContentId = attachmentService.GetAttachmentContentId(fileBytes);
+            expectedAttachmentContentId = attachmentService.CreateAttachmentContentId(fileBytes);
 
         It should_return_sha1_hash_of_file_bytes = () => expectedAttachmentContentId.ShouldEqual("7037807198C22A7D2B0807371D763779A84FDFCF");
 
