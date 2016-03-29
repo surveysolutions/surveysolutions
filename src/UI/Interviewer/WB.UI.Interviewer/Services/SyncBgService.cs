e7e4567ccd810b2afba0c8b9aaa3bdd5eb33faa1
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2016-03-29 11:12:58 +0300

    KP-6857: Moved sync process to separate service separated from UI

---
+++ src/UI/Interviewer/WB.UI.Interviewer/Services/SyncBgService.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading;
+using Android.OS;
+using Android.App;
+using Android.Content;
+using MvvmCross.Platform;
+
+namespace WB.UI.Interviewer.Services
+{
+    [Service]
+    public class SyncBgService : Service
+    {
+        private bool syncStarted = false;
+
+        private SyncServiceBinder binder;
+
+        public void StartSync()
+        {
+            Mvx.Trace("service started");
+        }
+
+        public override IBinder OnBind(Intent intent)
+        {
+            this.binder = new SyncServiceBinder(this);
+            return this.binder;
+        }
+    }
+}
---
+++ src/UI/Interviewer/WB.UI.Interviewer/Services/SyncServiceConnection.cs
@@ -0,0 +1,31 @@
+﻿using Android.Content;
+using Android.OS;
+using WB.UI.Interviewer.Activities;
+
+namespace WB.UI.Interviewer.Services
+{
+    public class SyncServiceConnection : Java.Lang.Object, IServiceConnection
+    {
+        readonly DashboardActivity activity;
+
+        public SyncServiceConnection(DashboardActivity activity)
+        {
+            this.activity = activity;
+        }
+
+        public void OnServiceConnected(ComponentName name, IBinder service)
+        {
+            var demoServiceBinder = service as SyncServiceBinder;
+            if (demoServiceBinder != null)
+            {
+                activity.Binder = demoServiceBinder;
+                activity.IsSyncServiceBound = true;
+            }
+        }
+
+        public void OnServiceDisconnected(ComponentName name)
+        {
+            activity.IsSyncServiceBound = false;
+        }
+    }
+}
