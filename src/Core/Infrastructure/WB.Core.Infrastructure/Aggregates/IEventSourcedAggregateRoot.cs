97f31aa9c75b13119e975ddcf3a9cf4ee6dba1b5
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2016-03-31 17:47:01 +0300

    CommandService: wrote a logic for plain aggregates (without repositories support), covered happy flow with tests KP-6770

--- src/Core/Infrastructure/WB.Core.Infrastructure/Aggregates/IEventSourcedAggregateRoot.cs
+++ src/Core/Infrastructure/WB.Core.Infrastructure/Aggregates/IEventSourcedAggregateRoot.cs
@@ -7,7 +7,6 @@ namespace WB.Core.Infrastructure.Aggregates
 {
     public interface IEventSourcedAggregateRoot : IAggregateRoot, IEventSource
     {
-        void SetId(Guid id);
         bool HasUncommittedChanges();
         IEnumerable<UncommittedEvent> GetUnCommittedChanges();
         void MarkChangesAsCommitted();
