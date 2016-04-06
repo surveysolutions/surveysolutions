e4666cb3cc04980554d1c512f3e8f9858d771217
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2016-04-06 17:12:28 -0400

    fix merge

--- src/Core/SharedKernels/Questionnaire/WB.Core.SharedKernels.Questionnaire/Documents/IQuestion.cs
+++ src/Core/SharedKernels/Questionnaire/WB.Core.SharedKernels.Questionnaire/Documents/IQuestion.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using Main.Core.Entities.Composite;
+using WB.Core.SharedKernels.QuestionnaireEntities;
 
 namespace Main.Core.Entities.SubEntities
 {
