fab007f5f7dc7a842bb371b55b28d1eea83c3b10
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2016-04-06 12:08:04 -0400

    HQ\Users: KP-5265 add an explanation message to obsolete attribute at the events

--- src/Core/SharedKernels/DataCollection/DataCollection/Events/User/UserArchived.cs
+++ src/Core/SharedKernels/DataCollection/DataCollection/Events/User/UserArchived.cs
@@ -8,7 +8,9 @@ using WB.Core.Infrastructure.EventBus.Lite;
 
 namespace WB.Core.SharedKernels.DataCollection.Events.User
 {
-    [Obsolete]
+    [Obsolete("This event must be deleted after all clients who STARTED data collection with version 5.6 or earlier will fade away. " +
+              "Pay attentions on the word 'STARTED'." +
+              "If the client started with 5.5 and had been updated later to 5.8 he still might have the event which need to be handled")]
     public class UserArchived : IEvent
     {
     }
