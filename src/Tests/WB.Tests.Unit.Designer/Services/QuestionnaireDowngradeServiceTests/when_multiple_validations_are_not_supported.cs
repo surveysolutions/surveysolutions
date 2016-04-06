260fb0b6d064bd1905728f9a5a69de76787d9e9d
Andrii Kozhyn <hedgic@gmail.com>
2016-04-06 10:54:06 +0300

    KP-6992: Backward compatibility for HQ that doesn't support multipe validations

---
+++ src/Tests/WB.Tests.Unit.Designer/Services/QuestionnaireDowngradeServiceTests/when_multiple_validations_are_not_supported.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Machine.Specifications;
+using Main.Core.Documents;
+using Main.Core.Entities.SubEntities.Question;
+using WB.Core.SharedKernels.QuestionnaireEntities;
+using WB.UI.Designer.Services;
+
+namespace WB.Tests.Unit.Designer.Services.QuestionnaireDowngradeServiceTests
+{
+    [Subject(typeof(QuestionnaireDowngradeService))]
+    internal class when_multiple_validations_are_not_supported
+    {
+        Establish context = () =>
+        {
+            questionId = Guid.Parse("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+            questionnaire = Create.QuestionnaireDocumentWithOneChapter(Create.TextQuestion(
+                questionId: questionId,
+                validationConditions: new List<ValidationCondition>
+                {
+                    new ValidationCondition("expression", "message")
+                }));
+
+            service = new QuestionnaireDowngradeService();
+        };
+
+        Because of = () => service.Downgrade(questionnaire, new Version(12, 0));
+
+        It should_put_validation_message_to_validation_message_field = () => questionnaire.GetQuestion<TextQuestion>(questionId).ValidationMessage.ShouldEqual("message");
+
+        static QuestionnaireDowngradeService service;
+        static QuestionnaireDocument questionnaire;
+        static Guid questionId;
+    }
+}
