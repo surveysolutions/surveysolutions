a6a56ac63ae42ebedded95577290e95aafd8bb65
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2016-04-07 13:52:37 +0300

    Tests\QuestionnaireAssembly: added simple test on question which enables static text (ignored), introduced expected event KP-6970

---
+++ src/Core/SharedKernels/DataCollection/DataCollection/Events/Interview/Base/StaticTextsPassiveEvent.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+
+namespace WB.Core.SharedKernels.DataCollection.Events.Interview.Base
+{
+    public abstract class StaticTextsPassiveEvent : InterviewPassiveEvent
+    {
+        public Identity[] StaticTexts { get; private set; }
+
+        protected StaticTextsPassiveEvent(Identity[] staticTexts)
+        {
+            this.StaticTexts = staticTexts.ToArray();
+        }
+    }
+}
