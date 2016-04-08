f0310c3030aa7efd3e295eb1f0b8d2a1d63e7492
Slava Scherbak <slava.scherbak@gmail.com>
2016-04-08 16:10:57 -0400

    HQ: fixed bugs with duplicate records in export file and possibly disappearing records from the end of export file. KP-7006

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/DataExport/Services/ReadSideToTabularFormatExportService.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/DataExport/Services/ReadSideToTabularFormatExportService.cs
@@ -120,32 +120,32 @@ namespace WB.Core.BoundedContexts.Headquarters.DataExport.Services
                                   !x.IsDeleted;
             }
 
-            List<Guid> interviewIdsToExport = new List<Guid>();
+            HashSet<Guid> interviewIdsToExport = new HashSet<Guid>();
 
             var stopwatch = Stopwatch.StartNew();
-            int totalInterviewsToExport =
-                this.transactionManager.GetTransactionManager()
-                    .ExecuteInQueryTransaction(() => this.interviewSummaries.Query(_ => _.Count(expression)));
-
-            while (interviewIdsToExport.Count < totalInterviewsToExport)
+           
+            string lastRecivedId = null;
+            while (true)
             {
                 var ids = this.transactionManager.GetTransactionManager().ExecuteInQueryTransaction(() =>
                     this.interviewSummaries.Query(_ => _
                         .Where(expression)
-                        .OrderBy(x => x.InterviewId)
+                        .OrderBy(x => x.SummaryId)
+                        .Where(x => lastRecivedId == null || x.SummaryId.CompareTo(lastRecivedId) > 0)
                         .Select(x => x.InterviewId)
-                        .Skip(interviewIdsToExport.Count)
                         .Take(this.exportSettings.InterviewIdsQueryBatchSize)
                         .ToList()));
+
                 if (ids.Count == 0) break;
 
                 cancellationToken.ThrowIfCancellationRequested();
-                interviewIdsToExport.AddRange(ids);
+                ids.ForEach(x => interviewIdsToExport.Add(x));
+                lastRecivedId = ids.Last().FormatGuid();
                 this.logger.Debug($"Received {interviewIdsToExport.Count:n0} interview interview ids.");
             }
             stopwatch.Stop();
             this.logger.Info($"Received {interviewIdsToExport.Count:N0} interviewIds to start export. Took {stopwatch.Elapsed:g} to complete.");
-            return interviewIdsToExport;
+            return interviewIdsToExport.ToList();
         }
 
         public void CreateHeaderStructureForPreloadingForQuestionnaire(QuestionnaireIdentity questionnaireIdentity, string basePath)
