7dec3e26acf5a7c994f7abc959e9a6fa161cb378
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2016-04-08 11:56:49 -0400

    Enumerator: KP-6920 fix InterviewExpressionStateUpgrader state casting

--- src/Core/SharedKernels/DataCollection/DataCollection/Implementation/Aggregates/InterviewInfrastructure/InterviewExpressionStateUpgrader.cs
+++ src/Core/SharedKernels/DataCollection/DataCollection/Implementation/Aggregates/InterviewInfrastructure/InterviewExpressionStateUpgrader.cs
@@ -1,4 +1,5 @@
-﻿using WB.Core.SharedKernels.DataCollection.Implementation.Aggregates.Adapters.V2;
+﻿using WB.Core.SharedKernels.DataCollection.Implementation.Aggregates.Adapters;
+using WB.Core.SharedKernels.DataCollection.Implementation.Aggregates.Adapters.V2;
 using WB.Core.SharedKernels.DataCollection.Implementation.Aggregates.Adapters.V4;
 using WB.Core.SharedKernels.DataCollection.Implementation.Aggregates.Adapters.V5;
 using WB.Core.SharedKernels.DataCollection.Implementation.Aggregates.Adapters.V6;
@@ -7,6 +8,7 @@ using WB.Core.SharedKernels.DataCollection.V2;
 using WB.Core.SharedKernels.DataCollection.V4;
 using WB.Core.SharedKernels.DataCollection.V5;
 using WB.Core.SharedKernels.DataCollection.V6;
+using WB.Core.SharedKernels.DataCollection.V7;
 
 namespace WB.Core.SharedKernels.DataCollection.Implementation.Aggregates
 {
@@ -14,16 +16,16 @@ namespace WB.Core.SharedKernels.DataCollection.Implementation.Aggregates
     {
         public ILatestInterviewExpressionState UpgradeToLatestVersionIfNeeded(IInterviewExpressionState state)
         {
-            var stateV7 = state as ILatestInterviewExpressionState;
+            var stateV7 = state as IInterviewExpressionStateV7;
             if (stateV7 != null)
-                return stateV7;
+                return new InterviewExpressionStateV7ToLatestAdapter(stateV7);
 
             var stateV2 = state as IInterviewExpressionStateV2 ?? new InterviewExpressionStateV1ToV2Adapter(state);
             var stateV4 = stateV2 as IInterviewExpressionStateV4 ?? new InterviewExpressionStateV2ToV4Adapter(stateV2);
             var stateV5 = stateV4 as IInterviewExpressionStateV5 ?? new InterviewExpressionStateV4ToV5Adapter(stateV4);
             var stateV6 = stateV5 as IInterviewExpressionStateV6 ?? new InterviewExpressionStateV5ToV6Adapter(stateV5);
 
-            return new InterviewExpressionStateV6ToV7Adapter(stateV6) ;
+            return new InterviewExpressionStateV6ToV7Adapter(stateV6);
         }
     }
 }
