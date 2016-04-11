671fe8b27ad98d3bacaaf0bd023429f4b59e6481
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2016-04-11 10:00:00 -0400

    add obsolete attribute on binders

--- src/Core/GenericSubdomains/WB.Core.GenericSubdomains.Portable/Services/SerializationBinderSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WB.Core.GenericSubdomains.Portable.Services
-{
-    public enum SerializationBinderSettings
-    {
-        Default = 0,
-        OldToNew = 1,
-        NewToOld = 2
-    }
-}
