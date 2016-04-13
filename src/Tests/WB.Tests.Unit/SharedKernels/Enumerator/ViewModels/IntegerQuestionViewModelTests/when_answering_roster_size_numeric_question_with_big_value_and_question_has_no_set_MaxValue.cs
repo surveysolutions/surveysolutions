00586ee65b7a1f29f8fc5a46f35c16b18789f235
Roman Lytvyn <litvin.roma@gmail.com>
2016-04-13 16:31:42 +0300

    EN: added formatting for numeric questions. fixed tests KP-6962

--- src/Tests/WB.Tests.Unit/SharedKernels/Enumerator/ViewModels/IntegerQuestionViewModelTests/when_answering_roster_size_numeric_question_with_big_value_and_question_has_no_set_MaxValue.cs
+++ src/Tests/WB.Tests.Unit/SharedKernels/Enumerator/ViewModels/IntegerQuestionViewModelTests/when_answering_roster_size_numeric_question_with_big_value_and_question_has_no_set_MaxValue.cs
@@ -38,7 +38,7 @@ namespace WB.Tests.Unit.SharedKernels.Enumerator.ViewModels.IntegerQuestionViewM
 
         Because of = () =>
         {
-            integerModel.AnswerAsString = "50";
+            integerModel.Answer = 50;
             integerModel.ValueChangeCommand.Execute();
         };
 
@@ -49,7 +49,7 @@ namespace WB.Tests.Unit.SharedKernels.Enumerator.ViewModels.IntegerQuestionViewM
             AnsweringViewModelMock.Verify(x => x.SendAnswerQuestionCommandAsync(Moq.It.IsAny<AnswerNumericIntegerQuestionCommand>()), Times.Never);
 
         It should_reset_AnswerAsString_to_previous_value = () =>
-            integerModel.AnswerAsString.ShouldEqual("50");
+            integerModel.Answer.ShouldEqual(50);
 
         private static IntegerQuestionViewModel integerModel;
     }
