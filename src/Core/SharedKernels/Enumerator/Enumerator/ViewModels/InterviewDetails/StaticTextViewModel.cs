7d22d4067df0892e1a5b304f82c8698c6d5b80f7
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2016-04-13 12:59:09 +0300

    KP-6974: Implemented disablement on IN/T for static texts

--- src/Core/SharedKernels/Enumerator/Enumerator/ViewModels/InterviewDetails/StaticTextViewModel.cs
+++ src/Core/SharedKernels/Enumerator/Enumerator/ViewModels/InterviewDetails/StaticTextViewModel.cs
@@ -1,34 +1,39 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using MvvmCross.Core.ViewModels;
 using WB.Core.SharedKernels.DataCollection;
+using WB.Core.SharedKernels.DataCollection.Implementation.Entities;
 using WB.Core.SharedKernels.DataCollection.Repositories;
 using WB.Core.SharedKernels.Enumerator.Repositories;
+using WB.Core.SharedKernels.Enumerator.Utils;
 using WB.Core.SharedKernels.Enumerator.ViewModels.InterviewDetails.Questions.State;
 
 namespace WB.Core.SharedKernels.Enumerator.ViewModels.InterviewDetails
 {
-    public class StaticTextViewModel : MvxNotifyPropertyChanged, IInterviewEntityViewModel
+    public class StaticTextViewModel : MvxNotifyPropertyChanged, 
+        IInterviewEntityViewModel
     {
         private readonly IPlainQuestionnaireRepository questionnaireRepository;
         private readonly IStatefulInterviewRepository interviewRepository;
+        private static readonly Regex htmlRemovalRegex = new Regex("<.*?>");
 
-        public EnablementViewModel Enablement { get; set; }
         public AttachmentViewModel Attachment { get; set; }
+        public StaticTextStateViewModel QuestionState { get; set; }
 
         public StaticTextViewModel(
             IPlainQuestionnaireRepository questionnaireRepository,
             IStatefulInterviewRepository interviewRepository,
             AttachmentViewModel attachmentViewModel,
-            EnablementViewModel enablement)
+            StaticTextStateViewModel questionState)
         {
             this.questionnaireRepository = questionnaireRepository;
             this.interviewRepository = interviewRepository;
             this.Attachment = attachmentViewModel;
-            this.Enablement = enablement;
+            this.QuestionState = questionState;
         }
 
-        public Identity Identity => this.questionIdentity;
+        public Identity Identity => this.identity;
 
         public async Task InitAsync(string interviewId, Identity entityIdentity, NavigationState navigationState)
         {
@@ -38,22 +43,41 @@ namespace WB.Core.SharedKernels.Enumerator.ViewModels.InterviewDetails
             var interview = this.interviewRepository.Get(interviewId);
             var questionnaire = this.questionnaireRepository.GetQuestionnaire(interview.QuestionnaireIdentity);
 
-            this.questionIdentity = entityIdentity;
-            this.StaticText = questionnaire.GetStaticText(entityIdentity.Id);
+            this.identity = entityIdentity;
 
-            this.Enablement.Init(interviewId, entityIdentity, navigationState);
+            this.QuestionState.Init(interviewId, entityIdentity, navigationState);
+            this.rawText = questionnaire.GetStaticText(entityIdentity.Id);
+
+            this.StaticText = interview.IsEnabled(entityIdentity) ? this.rawText : RemoveHtmlTags(this.rawText);
+
+            this.QuestionState.Enablement.EntityDisabled += this.EnablementOnEntityDisabled;
+            this.QuestionState.Enablement.EntityEnabled += EnablementOnEntityEnabled;
             await this.Attachment.InitAsync(interviewId, entityIdentity);
         }
 
-        private string staticText;
-        private Identity questionIdentity;
+        private void EnablementOnEntityEnabled(object sender, EventArgs eventArgs)
+        {
+            this.StaticText = rawText;
+        }
 
+        private void EnablementOnEntityDisabled(object sender, EventArgs eventArgs)
+        {
+            this.StaticText = RemoveHtmlTags(rawText);
+        }
+
+        private Identity identity;
+        private string rawText;
+
+        private string staticText;
         public string StaticText
         {
             get { return this.staticText; }
-            set { this.staticText = value; this.RaisePropertyChanged(); }
+            set { this.RaiseAndSetIfChanged(ref this.staticText, value); }
         }
 
-
+        private static string RemoveHtmlTags(string rawText)
+        {
+            return htmlRemovalRegex.Replace(rawText, string.Empty);
+        }
     }
 }
