5e9c2317cd0843e16d573cb4bee401d7f87c9d6a
Anastasya Kholodova <nastya.kholodova@gmail.com>
2016-04-15 17:26:34 -0400

    HQ\QuantityRepost: KP-7017 do not count reinits in quantity reports

---
+++ src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/EventHandlers/InterviewStatusTimeSpanDenormalizerTests/when_interview_completed_event_received_after_restart.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Machine.Specifications;
+using WB.Core.SharedKernels.SurveyManagement.EventHandler;
+using WB.Core.SharedKernels.SurveyManagement.Views.DataExport;
+using WB.Core.SharedKernels.SurveyManagement.Views.Interview;
+using WB.Core.GenericSubdomains.Portable;
+using System.Linq;
+
+namespace WB.Tests.Unit.SharedKernels.SurveyManagement.EventHandlers.InterviewStatusTimeSpanDenormalizerTests
+{
+    internal class when_interview_completed_event_received_after_restart : InterviewStatusTimeSpanDenormalizerTestContext
+    {
+        Establish context = () =>
+        {
+            interviewStatusTimeSpansStorage = new TestInMemoryWriter<InterviewStatusTimeSpans>();
+            interviewStatusesStorage = new TestInMemoryWriter<InterviewStatuses>();
+            interviewStatuses =
+                Create.InterviewStatuses(interviewid: interviewId, statuses:
+                    new[]
+                    {
+                        Create.InterviewCommentedStatus(interviewId, status: InterviewExportedAction.InterviewerAssigned),
+                        Create.InterviewCommentedStatus(interviewId, status: InterviewExportedAction.FirstAnswerSet),
+                        Create.InterviewCommentedStatus(interviewId, status: InterviewExportedAction.Completed),
+                        Create.InterviewCommentedStatus(interviewId, status: InterviewExportedAction.Restarted)
+                    });
+            interviewStatusesStorage.Store(interviewStatuses, interviewId.FormatGuid());
+
+            interviewStatusTimeSpansStorage.Store(
+                Create.InterviewStatusTimeSpans(interviewId: interviewId.FormatGuid(),
+                    timeSpans:
+                        new[]
+                        {
+                            Create.TimeSpanBetweenStatuses(interviewerId: interviewId,
+                                endStatus: InterviewExportedAction.Completed)
+                        }), interviewId.FormatGuid());
+
+            denormalizer = CreateInterviewStatusTimeSpanDenormalizer(statuses: interviewStatusesStorage, interviewCustomStatusTimestampStorage: interviewStatusTimeSpansStorage);
+        };
+
+        Because of = () => denormalizer.Handle(Create.InterviewCompletedEvent(interviewId: interviewId));
+
+        It should_contain_only_one_complete_record =
+            () =>
+                interviewStatusTimeSpansStorage.GetById(interviewId.FormatGuid())
+                    .TimeSpansBetweenStatuses.Count(ts=>ts.EndStatus== InterviewExportedAction.Completed).ShouldEqual(1);
+
+        It should_record_interviewer_assign_as_begin_status =
+           () =>
+               interviewStatusTimeSpansStorage.GetById(interviewId.FormatGuid())
+                   .TimeSpansBetweenStatuses.First()
+                   .BeginStatus.ShouldEqual(InterviewExportedAction.InterviewerAssigned);
+
+        private static InterviewStatusTimeSpanDenormalizer denormalizer;
+        private static TestInMemoryWriter<InterviewStatuses> interviewStatusesStorage;
+        private static TestInMemoryWriter<InterviewStatusTimeSpans> interviewStatusTimeSpansStorage;
+        private static Guid interviewId = Guid.Parse("11111111111111111111111111111111");
+        private static InterviewStatuses interviewStatuses;
+
+    }
+}
