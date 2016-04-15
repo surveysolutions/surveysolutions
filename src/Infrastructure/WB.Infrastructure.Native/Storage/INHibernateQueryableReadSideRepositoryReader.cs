4ceec998d51ca5e5c11fc74aff26bc3c5b29f7f0
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2016-04-15 10:04:22 -0400

    HQ\ColulativeRepost: KP-6966 add inrastructure for nhibernate's QueryOver

---
+++ src/Infrastructure/WB.Infrastructure.Native/Storage/INHibernateQueryableReadSideRepositoryReader.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using NHibernate;
+using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
+using WB.Core.SharedKernels.SurveySolutions;
+
+namespace WB.Infrastructure.Native.Storage
+{
+    public interface INHibernateQueryableReadSideRepositoryReader<TEntity>: IQueryableReadSideRepositoryReader<TEntity> where TEntity : class, IReadSideRepositoryEntity
+    {
+        TResult QueryOver<TResult>(Func<IQueryOver<TEntity, TEntity>, TResult> query);
+    }
+}
