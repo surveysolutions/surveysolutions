95b3b79f153041c79565f719bc4b822d023fbee3
Anastasya Kholodova <nastya.kholodova@gmail.com>
2016-04-18 12:17:50 -0400

    HQ\Reposrts: KP-6966 add integration tests for comulative report

---
+++ src/Tests/WB.Tests.Integration/OldschoolChartStatisticsDataProviderTests/when_count_of_interviews_is_zero.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Machine.Specifications;
+using Moq;
+using NHibernate;
+using Npgsql;
+using WB.Core.GenericSubdomains.Portable.Services;
+using WB.Core.Infrastructure.Transactions;
+using WB.Core.SharedKernels.SurveyManagement.EventHandler;
+using WB.Core.SharedKernels.SurveyManagement.Implementation.Factories;
+using WB.Core.SharedKernels.SurveyManagement.Mappings;
+using WB.Core.SharedKernels.SurveyManagement.Views;
+using WB.Core.SharedKernels.SurveyManagement.Views.Interview;
+using WB.Infrastructure.Native.Storage.Postgre.Implementation;
+using WB.Tests.Integration.PostgreSQLTests;
+using It = Machine.Specifications.It;
+
+namespace WB.Tests.Integration.OldschoolChartStatisticsDataProviderTests
+{
+    internal class when_count_of_interviews_is_zero: with_postgres_db
+    {
+        Establish context = () =>
+        {
+            var sessionFactory = Create.SessionFactory(connectionStringBuilder.ConnectionString, new[] { typeof(CumulativeReportStatusChangeMap) });
+            postgresTransactionManager = new CqrsPostgresTransactionManager(sessionFactory ?? Mock.Of<ISessionFactory>());
+
+            pgSqlConnection = new NpgsqlConnection(connectionStringBuilder.ConnectionString);
+            pgSqlConnection.Open();
+
+            cumulativeReportStatusChangeStorage = new PostgreReadSideStorage<CumulativeReportStatusChange>(postgresTransactionManager, Mock.Of<ILogger>());
+
+            oldschoolChartStatisticsDataProvider = new OldschoolChartStatisticsDataProvider(cumulativeReportStatusChangeStorage);
+        };
+
+        Cleanup things = () => { pgSqlConnection.Close(); };
+
+        Because of = () => result= postgresTransactionManager.ExecuteInQueryTransaction(()=>oldschoolChartStatisticsDataProvider.GetStatisticsInOldFormat(Guid.NewGuid(), 1));
+
+        It should_return_null =
+            () => result.ShouldBeNull();
+
+        private static OldschoolChartStatisticsDataProvider oldschoolChartStatisticsDataProvider;
+        private static StatisticsGroupedByDateAndTemplate result;
+        private static CqrsPostgresTransactionManager postgresTransactionManager;
+        static NpgsqlConnection pgSqlConnection;
+        private static PostgreReadSideStorage<CumulativeReportStatusChange> cumulativeReportStatusChangeStorage;
+    }
+}
