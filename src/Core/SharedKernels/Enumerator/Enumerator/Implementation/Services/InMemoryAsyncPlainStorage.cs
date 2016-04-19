e257e922f59a3f9a6bf182029a3719c01ba2777c
Slava Scherbak <slava.scherbak@gmail.com>
2016-04-19 17:21:26 -0400

    Enumerator: cascading and filtered options moved into separate options repository. KP-7043

---
+++ src/Core/SharedKernels/Enumerator/Enumerator/Implementation/Services/InMemoryAsyncPlainStorage.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using WB.Core.GenericSubdomains.Portable;
+using WB.Core.SharedKernels.Enumerator.Services.Infrastructure.Storage;
+
+namespace WB.Core.SharedKernels.Enumerator.Implementation.Services
+{
+    public class InMemoryAsyncPlainStorage<TEntity> : IAsyncPlainStorage<TEntity>
+        where TEntity : class, IPlainStorageEntity
+    {
+        private readonly Dictionary<string, TEntity> inMemroyStorage = new Dictionary<string, TEntity>();
+
+        public TEntity GetById(string id)
+        {
+            return this.inMemroyStorage.ContainsKey(id) ? this.inMemroyStorage[id] : null;
+        }
+
+        public IReadOnlyCollection<TEntity> Where(Expression<Func<TEntity, bool>> predicate)
+        {
+            return this.inMemroyStorage.Values.AsQueryable().Where(predicate).ToReadOnlyCollection();
+        }
+
+        public TEntity FirstOrDefault()
+        {
+            return this.inMemroyStorage.Values.AsQueryable().FirstOrDefault();
+        }
+
+        public IReadOnlyCollection<TEntity> LoadAll()
+        {
+            return this.inMemroyStorage.Values.ToReadOnlyCollection();
+        }
+
+        public int Count(Expression<Func<TEntity, bool>> predicate)
+        {
+            return this.inMemroyStorage.Values.AsQueryable().Count(predicate);
+        }
+
+        public async Task<TEntity> GetByIdAsync(string id) => await Task.Run(() => this.GetById(id));
+
+        public async Task StoreAsync(TEntity entity) => await Task.Run(() => this.inMemroyStorage[entity.Id] = entity);
+
+        public async Task<IReadOnlyCollection<TEntity>> WhereAsync(Expression<Func<TEntity, bool>> predicate) => await Task.Run(() => this.Where(predicate));
+
+        public async Task<IReadOnlyCollection<TEntity>> LoadAllAsync() => await Task.Run(this.LoadAllAsync);
+
+        public async Task<int> CountAsync(Expression<Func<TEntity, bool>> predicate) => await Task.Run(() => this.CountAsync(predicate));
+
+        public async Task RemoveAsync(string id)
+        {
+            await Task.Run(() =>
+            {
+                if (this.inMemroyStorage.ContainsKey(id))
+                    this.inMemroyStorage.Remove(id);
+            });
+        }
+
+        public async Task RemoveAsync(IEnumerable<TEntity> entities)
+        {
+            foreach (var entity in entities.Where(entity => entity != null))
+            {
+                await this.RemoveAsync(entity.Id);
+            }
+        }
+
+        public async Task StoreAsync(IEnumerable<TEntity> entities)
+        {
+            foreach (var entity in entities.Where(entity => entity != null))
+            {
+                await this.StoreAsync(entity);
+            }
+        }
+
+        public void Dispose()
+        {
+            this.inMemroyStorage.Clear();
+        }
+    }
+}
---
+++ src/Core/SharedKernels/Enumerator/Enumerator/Services/IOptionsRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Main.Core.Documents;
+using Main.Core.Entities.SubEntities;
+using WB.Core.SharedKernels.DataCollection.Implementation.Entities;
+using WB.Core.SharedKernels.Enumerator.ViewModels.InterviewDetails.Questions;
+
+namespace WB.Core.SharedKernels.Enumerator.Services
+{
+    public interface IOptionsRepository
+    {
+        IReadOnlyList<CategoricalQuestionOption> GetQuestionOptions(QuestionnaireIdentity questionnaireId, Guid questionId);
+        Task RemoveOptionsForQuestionnaireAsync(QuestionnaireIdentity questionnaireId);
+        Task StoreQuestionOptionsForQuestionnaireAsync(QuestionnaireIdentity questionnaireIdentity, QuestionnaireDocument serializedQuestionnaireDocument);
+    }
+}
