9e237a0ddb391acc81182785d833faae93a1f98f
Roman Lytvyn <litvin.roma@gmail.com>
2016-04-22 12:20:37 +0300

    Tests by validation\enablement of static texts during synchronization. KP-6971

---
+++ src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/EventHandlers/Interview/InterviewEventHandlerFunctionalTests/when_StaticTextsDisabled_received.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Machine.Specifications;
+using Moq;
+using Ncqrs.Eventing.ServiceModel.Bus;
+using WB.Core.SharedKernels.DataCollection.Events.Interview;
+using WB.Core.SharedKernels.DataCollection.Views.Questionnaire;
+using WB.Core.SharedKernels.SurveyManagement.EventHandler;
+using WB.Core.SharedKernels.SurveyManagement.Views.Interview;
+using It = Machine.Specifications.It;
+
+namespace WB.Tests.Unit.SharedKernels.SurveyManagement.EventHandlers.Interview.InterviewEventHandlerFunctionalTests
+{
+    internal class when_StaticTextsDisabled_received : InterviewEventHandlerFunctionalTestContext
+    {
+        Establish context = () =>
+        {
+            var questionnaireRosterStructure = new Mock<QuestionnaireRosterStructure>();
+
+            interviewEventHandlerFunctional = CreateInterviewEventHandlerFunctional(questionnaireRosterStructure.Object);
+            viewState = CreateViewWithSequenceOfInterviewData();
+
+            var identity = Create.Identity(staticTextId, new decimal[0]);
+
+            publishedEvent = CreatePublishableEvent(new StaticTextsDisabled(new[] {identity}));
+
+            viewState.Levels["#"].StaticTexts.Add(staticTextId, new InterviewStaticText(staticTextId) {IsEnabled = true});
+        };
+
+        Because of = () =>
+            viewState = interviewEventHandlerFunctional.Update(viewState, publishedEvent);
+
+        It should_static_text_store_in_cache = () =>
+            GetStaticText().ShouldNotBeNull();
+
+        It should_static_text_be_disabled = () =>
+            GetStaticText().IsEnabled.ShouldBeFalse();
+
+        private static InterviewStaticText GetStaticText()
+        {
+            return viewState.Levels["#"].StaticTexts[staticTextId];
+        }
+
+        private static InterviewEventHandlerFunctional interviewEventHandlerFunctional;
+        private static InterviewData viewState;
+        private static Guid staticTextId = Guid.Parse("13333333333333333333333333333333");
+        private static IPublishedEvent<StaticTextsDisabled> publishedEvent;
+    }
+}
