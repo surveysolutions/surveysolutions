31edb6eaf75849dda65eca24aa575ac542f6fd4a
Roman Lytvyn <litvin.roma@gmail.com>
2016-04-22 14:13:35 +0300

    Tests by validation\enablement of static texts during synchronization. KP-6972

---
+++ src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/InterviewSynchronizationDtoFactoryTests/when_build_from_interview_with_static_texts.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using Machine.Specifications;
+using WB.Core.SharedKernels.DataCollection;
+using WB.Core.SharedKernels.DataCollection.DataTransferObjects.Synchronization;
+using WB.Core.SharedKernels.DataCollection.ValueObjects;
+using WB.Core.SharedKernels.SurveyManagement.Implementation.Factories;
+using WB.Core.SharedKernels.SurveyManagement.Views.Interview;
+using It = Machine.Specifications.It;
+
+namespace WB.Tests.Unit.SharedKernels.SurveyManagement.InterviewSynchronizationDtoFactoryTests
+{
+    internal class when_build_from_interview_with_static_texts : InterviewSynchronizationDtoFactoryTestContext
+    {
+        Establish context = () =>
+        {
+            interviewData = CreateInterviewData();
+            interviewData.Levels.Add("#1", new InterviewLevel
+            {
+                RosterVector = RosterVector.Empty,
+                StaticTexts = new Dictionary<Guid, InterviewStaticText>()
+                {
+                    {disabledStaticText1Id.Id, new InterviewStaticText {Id = disabledStaticText1Id.Id, IsEnabled = false, IsInvalid = false}},
+                    {validStaticText1Id.Id, new InterviewStaticText {Id = validStaticText1Id.Id, IsInvalid = false, IsEnabled = true}},
+                    {
+                        invalidStaticText1Id.Id,
+                        new InterviewStaticText
+                        {
+                            Id = invalidStaticText1Id.Id,
+                            IsInvalid = true,
+                            IsEnabled = true,
+                            FailedValidationConditions = new List<FailedValidationCondition>()
+                        }
+                    }
+                }
+            });
+            interviewData.Levels.Add("#2", new InterviewLevel
+            {
+                RosterVector = new RosterVector(new decimal[] {1, 2, 3}),
+                StaticTexts = new Dictionary<Guid, InterviewStaticText>()
+                {
+                    {disabledStaticText2Id.Id, new InterviewStaticText {Id = disabledStaticText2Id.Id, IsEnabled = false, IsInvalid = false}},
+                    {validStaticText2Id.Id, new InterviewStaticText {Id = validStaticText2Id.Id, IsEnabled = true, IsInvalid = false}},
+                    {
+                        invalidStaticText2Id.Id,
+                        new InterviewStaticText
+                        {
+                            Id = invalidStaticText2Id.Id,
+                            IsEnabled = true,
+                            IsInvalid = true,
+                            FailedValidationConditions = new List<FailedValidationCondition>()
+                        }
+                    }
+                }
+
+            });
+
+            var questionnaireDocument = CreateQuestionnaireDocumentWithOneChapter();
+
+            interviewSynchronizationDtoFactory = CreateInterviewSynchronizationDtoFactory(questionnaireDocument);
+        };
+
+        Because of = () =>
+            result = interviewSynchronizationDtoFactory.BuildFrom(interviewData, "comment", null, null);
+
+        It should_return_specified_disabled_static_texts = () =>
+        {
+            result.DisabledStaticTexts.Count.ShouldEqual(2);
+            result.DisabledStaticTexts.ShouldEachConformTo(sttid => sttid == disabledStaticText1Id || sttid == disabledStaticText2Id);
+        };
+
+        It should_return_specified_valid_static_texts = () =>
+        {
+            result.ValidStaticTexts.Count.ShouldEqual(4);
+            result.ValidStaticTexts.ShouldEachConformTo(sttid =>
+                sttid == validStaticText1Id || sttid == validStaticText2Id || sttid == disabledStaticText1Id ||sttid == disabledStaticText2Id);
+        };
+
+        It should_return_specified_invalid_static_texts = () =>
+        {
+            result.InvalidStaticTexts.Count.ShouldEqual(2);
+            result.InvalidStaticTexts.ShouldEachConformTo(sttid => sttid.Key == invalidStaticText1Id || sttid.Key == invalidStaticText2Id);
+        };
+
+        private static InterviewSynchronizationDtoFactory interviewSynchronizationDtoFactory;
+        private static InterviewData interviewData;
+        private static InterviewSynchronizationDto result;
+
+        private static readonly Identity disabledStaticText1Id = new Identity(Guid.Parse("11111111111111111111111111111111"), RosterVector.Empty);
+        private static readonly Identity disabledStaticText2Id = new Identity(Guid.Parse("22222222222222222222222222222222"), new RosterVector(new decimal[]{1, 2, 3}));
+
+        private static readonly Identity validStaticText1Id = new Identity(Guid.Parse("33333333333333333333333333333333"), RosterVector.Empty);
+        private static readonly Identity validStaticText2Id = new Identity(Guid.Parse("44444444444444444444444444444444"), new RosterVector(new decimal[] { 1, 2, 3 }));
+
+        private static readonly Identity invalidStaticText1Id = new Identity(Guid.Parse("55555555555555555555555555555555"), RosterVector.Empty);
+        private static readonly Identity invalidStaticText2Id = new Identity(Guid.Parse("66666666666666666666666666666666"), new RosterVector(new decimal[] { 1, 2, 3 }));
+    }
+}
