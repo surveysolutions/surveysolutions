c7458461cadcb5495b84656261eef909897a2ee2
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2016-05-03 19:16:39 +0300

    KP-7109: Format numbers according to current culture on the IN app

---
+++ src/Tests/WB.Tests.Unit/ChangeCurrentCulture.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+
+namespace WB.Tests.Unit
+{
+    public class ChangeCurrentCulture : IDisposable
+    {
+        private readonly CultureInfo original;
+
+        public ChangeCurrentCulture(CultureInfo culture)
+        {
+            original = Thread.CurrentThread.CurrentCulture;
+            Change(culture);
+        }
+
+        private void Change(CultureInfo culture)
+        {
+            Thread.CurrentThread.CurrentCulture = culture;
+            Thread.CurrentThread.CurrentUICulture = culture;
+        }
+
+        public void Dispose()
+        {
+            Change(original);
+        }
+    }
+}
