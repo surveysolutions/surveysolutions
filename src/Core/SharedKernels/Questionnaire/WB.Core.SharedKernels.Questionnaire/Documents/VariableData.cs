77fa0cd0971831285dc4dbadce6a513eea1fcbba
Pavel Stovpivskyi <sby@rambler.ru>
2016-05-05 18:40:53 -0400

    KP-7125 D: Backend Commands + Events

---
+++ src/Core/SharedKernels/Questionnaire/WB.Core.SharedKernels.Questionnaire/Documents/VariableData.cs
@@ -0,0 +1,16 @@
+﻿namespace WB.Core.SharedKernels.QuestionnaireEntities
+{
+    public class VariableData
+    {
+        public VariableData(VariableType type, string name, string body)
+        {
+            this.Type = type;
+            this.Name = name;
+            this.Body = body;
+        }
+
+        public VariableType Type { get; }
+        public string Name { get; }
+        public string Body { get; }
+    }
+}
