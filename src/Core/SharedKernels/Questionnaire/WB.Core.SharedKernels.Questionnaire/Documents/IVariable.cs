2961d48ca0338bb60943f4bdccd2c3d84185722b
Pavel Stovpivskyi <sby@rambler.ru>
2016-05-05 18:40:53 -0400

    KP-7125 D: Backend Commands + Events

---
+++ src/Core/SharedKernels/Questionnaire/WB.Core.SharedKernels.Questionnaire/Documents/IVariable.cs
@@ -0,0 +1,12 @@
+﻿using Main.Core.Entities.Composite;
+using Main.Core.Entities.SubEntities;
+
+namespace WB.Core.SharedKernels.QuestionnaireEntities
+{
+    public interface IVariable : IComposite
+    {
+        VariableType Type { get; }
+        string Name { get; }
+        string Body { get; }
+    }
+}
---
+++ src/Core/SharedKernels/Questionnaire/WB.Core.SharedKernels.Questionnaire/Documents/Variable.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using Main.Core.Entities.Composite;
+
+namespace WB.Core.SharedKernels.QuestionnaireEntities
+{
+    public class Variable : IVariable
+    {
+        public Variable(Guid publicKey, VariableData variableData)
+        {
+            this.PublicKey = publicKey;
+            this.Type = variableData.Type;
+            this.Name = variableData.Name;
+            this.Body = variableData.Body;
+        }
+
+        public Guid PublicKey { get; }
+        public VariableType Type { get; }
+        public string Name { get; }
+        public string Body { get; }
+
+        public List<IComposite> Children
+        {
+            get { return new List<IComposite>(0); }
+            set { }
+        }
+
+        private IComposite parent;
+
+        public IComposite GetParent()
+        {
+            return this.parent;
+        }
+
+        public void SetParent(IComposite parent)
+        {
+            this.parent = parent;
+        }
+
+        public T Find<T>(Guid publicKey) where T : class, IComposite
+        {
+            return null;
+        }
+
+        public IEnumerable<T> Find<T>(Func<T, bool> condition) where T : class
+        {
+            return new T[0];
+        }
+
+        public T FirstOrDefault<T>(Func<T, bool> condition) where T : class
+        {
+            return null;
+        }
+
+        public void ConnectChildrenWithParent()
+        {
+        }
+
+        public IComposite Clone()
+        {
+            var variable = (IVariable)this.MemberwiseClone();
+            variable.SetParent(null);
+            return variable;
+        }
+    }
+}
