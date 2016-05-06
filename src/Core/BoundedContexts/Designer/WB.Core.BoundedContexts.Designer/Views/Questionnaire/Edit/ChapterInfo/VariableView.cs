c23c2f5c6d6dd4cee3c536d1f4097bb5460169b6
Andrii Kozhyn <hedgic@gmail.com>
2016-05-06 20:44:13 +0300

    KP-7131: Added denormalizers for variables

---
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Views/Questionnaire/Edit/ChapterInfo/VariableView.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using WB.Core.SharedKernels.QuestionnaireEntities;
+
+namespace WB.Core.BoundedContexts.Designer.Views.Questionnaire.Edit.ChapterInfo
+{
+    public class VariableView : IQuestionnaireItem
+    {
+        public string ItemId { get; set; }
+
+        public VariableData VariableData { get; set; }
+
+        public ChapterItemType ItemType => ChapterItemType.Variable;
+
+        public List<IQuestionnaireItem> Items
+        {
+            get { return new List<IQuestionnaireItem>(); }
+            set { }
+        }
+    }
+}
