45ba1e5211e5c6e02b60c2989d70a92c7b9e8d37
SPuV <sby@rambler.ru>
2016-05-06 10:01:35 -0400

    Variable commands
    KP-7125

---
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Commands/Questionnaire/Variable/AddVariable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using WB.Core.BoundedContexts.Designer.Commands.Questionnaire.Base;
+using WB.Core.SharedKernels.QuestionnaireEntities;
+
+namespace WB.Core.BoundedContexts.Designer.Commands.Questionnaire.Variable
+{
+    [Serializable]
+    public class AddVariable : QuestionnaireEntityAddCommand
+    {
+        public AddVariable(Guid questionnaireId, Guid entityId, VariableData variableData, Guid responsibleId, Guid parentId, int? index = null)
+            : base(questionnaireId, entityId, responsibleId, parentId)
+        {
+            Index = index;
+            VariableData = variableData;
+        }
+
+        public VariableData VariableData { get; private set; }
+        public int? Index { get; private set; }
+    }
+}
