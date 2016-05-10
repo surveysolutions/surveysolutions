3cb4c41e75ce1936cd8c804d97d2fd553a0b8bcd
Anastasya Kholodova <nastya.kholodova@gmail.com>
2016-05-10 13:35:58 -0400

    Designer\CodeGeneration: KP-7127 code review

--- src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Implementation/Services/CodeGeneration/Model/RosterTemplateModel.cs
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Implementation/Services/CodeGeneration/Model/RosterTemplateModel.cs
@@ -5,22 +5,43 @@ namespace WB.Core.BoundedContexts.Designer.Implementation.Services.CodeGeneratio
 {
     public class RosterTemplateModel : RosterScopeBaseModel
     {
-        public RosterTemplateModel()
+        public RosterTemplateModel(
+            string rosterScopeName, 
+            string typeName, 
+            List<Guid> rosterScope, 
+            string parentTypeName, 
+            string conditions, 
+            Guid id, 
+            string variableName, 
+            string parentScopeTypeName,
+            List<LinkedQuestionFilterExpressionModel> linkedQuestionFilterExpressions)
+            : base(
+                rosterScopeName, 
+                typeName, 
+                new List<GroupTemplateModel>(), 
+                new List<QuestionTemplateModel>(), 
+                new List<StaticTextTemplateModel>(), 
+                new List<RosterTemplateModel>(), 
+                rosterScope, 
+                new List<VariableTemplateModel>(), 
+                parentTypeName)
         {
-            this.LinkedQuestionFilterExpressions = new List<LinkedQuestionFilterExpressionModel>();
+            this.Conditions = conditions;
+            this.Id = id;
+            this.VariableName = variableName;
+            this.ParentScopeTypeName = parentScopeTypeName;
+            this.LinkedQuestionFilterExpressions = linkedQuestionFilterExpressions;
         }
 
-        public string Conditions { set; get; }
-
-        public Guid Id { set; get; }
-
-        public string VariableName { set; get; }
-        public string ParentScopeTypeName { get; set; }
+        public string Conditions { get; }
+        public Guid Id { get; }
+        public string VariableName { get; }
+        public string ParentScopeTypeName { get; }
+        public List<LinkedQuestionFilterExpressionModel> LinkedQuestionFilterExpressions { get; }
 
         public string StateName => CodeGenerator.PrivateFieldsPrefix + VariableName + CodeGenerator.StateSuffix;
         public string IdName => CodeGenerator.PrivateFieldsPrefix + VariableName + CodeGenerator.IdSuffix;
         public string ConditionsMethodName => CodeGenerator.EnablementPrefix + VariableName;
 
-        public List<LinkedQuestionFilterExpressionModel> LinkedQuestionFilterExpressions { get; set; }
     }
 }
