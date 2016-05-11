6cccfad154f6d789d2c1615ffd2f00632a7cade2
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2016-05-11 15:43:55 +0300

    HQ\Questionnaire\Clone: added 404 response for missing questionnaires and title emptyness invariant also started to implement separate page for specifying title while cloning KP-6883

---
+++ src/Core/SharedKernels/SurveyManagement/SurveyManagement.Web/Models/CloneQuestionnaireModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace WB.Core.SharedKernels.SurveyManagement.Web.Models
+{
+    public class CloneQuestionnaireModel
+    {
+        public CloneQuestionnaireModel(Guid id, long version, string title)
+        {
+            this.Id = id;
+            this.Version = version;
+            this.Title = title;
+        }
+
+        public Guid Id { get; }
+        public long Version { get; }
+        public string Title { get; }
+    }
+}
