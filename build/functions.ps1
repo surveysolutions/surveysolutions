5da2b8ecf4495b96444da6ef839cfbd8cdd0a000
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2016-05-12 19:56:07 +0300

    comment out nunit

--- build/functions.ps1
+++ build/functions.ps1
@@ -187,7 +187,7 @@ function RunTestsFromProject($Project, $BuildConfiguration) {
 
         $resultXml = (Get-Item $assembly).BaseName + '.NUnit-Result.xml'
         .\packages\NUnit.ConsoleRunner.3.2.1\tools\nunit3-console.exe $assembly --out:$resultXml | Write-Host
-        Write-Host "##teamcity[importData type='nunit' path='$resultXml']"
+        #Write-Host "##teamcity[importData type='nunit' path='$resultXml']"
     }
 
     Write-Host "##teamcity[blockClosed name='$Project']"
