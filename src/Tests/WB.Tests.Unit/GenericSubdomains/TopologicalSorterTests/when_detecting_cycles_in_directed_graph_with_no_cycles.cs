7929d6ddd946d10077d435e4d03541180769a6dd
slava <slava.scherbak@gmail.com>
2016-05-13 12:17:51 -0400

    Verifier: fixed topological sort, added tests. KP-7123

---
+++ src/Tests/WB.Tests.Unit/GenericSubdomains/TopologicalSorterTests/when_detecting_cycles_in_directed_graph_with_no_cycles.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Machine.Specifications;
+using WB.Core.GenericSubdomains.Portable;
+
+namespace WB.Tests.Unit.GenericSubdomains.TopologicalSorterTests
+{
+    internal class when_detecting_cycles_in_directed_graph_with_no_cycles
+    {
+        Establish context = () =>
+        {
+            dependencies = new Dictionary<int, int[]>()
+            {
+                {0, new[] {1}},
+                {1, new[] {2}},
+                {2, new[] {3}}
+            };
+            sorter = Create.TopologicalSorter();
+        };
+
+        Because of = () =>
+            cycles = sorter.DetectCycles(dependencies);
+
+        It should_find_0_cycles = () =>
+            cycles.Count.ShouldEqual(0);
+
+        private static ITopologicalSorter<int> sorter;
+        private static List<List<int>> cycles;
+        private static Dictionary<int, int[]> dependencies;
+    }
+}
