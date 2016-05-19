f3e8fcf8116e6916dd9d4e8d5ec200151ae1d56c
Slava Scherbak <slava.scherbak@gmail.com>
2016-05-19 18:25:45 -0400

    Verifier: changes after code review. KP-7202

--- src/UI/Designer/WB.UI.Designer/UpdatedDesigner/app/scripts/controllers/main.js
+++ src/UI/Designer/WB.UI.Designer/UpdatedDesigner/app/scripts/controllers/main.js
@@ -113,10 +113,6 @@ angular.module('designerApp')
                 }, 500);
             };
 
-            $scope.areErrorsAreActiveNow = function() {
-                return $scope.verificationStatus.typeOfMessageToBeShown === ERROR;
-            }
-
             $scope.showVerificationErrors = function () {
                 if ($scope.verificationStatus.errors.length === 0)
                     return;
