482b8c3c173b173ae465248bda33c2ae35c4ca4d
Roman Lytvyn <litvin.roma@gmail.com>
2016-05-20 18:46:20 +0300

    EN: numeric edit text formatter moved to portable shared.enumerator. KP-7112

---
+++ src/Core/SharedKernels/Enumerator/Enumerator/Utils/NumericTextFormatterSettings.cs
@@ -0,0 +1,14 @@
+namespace WB.Core.SharedKernels.Enumerator.Utils
+{
+    public class NumericTextFormatterSettings
+    {
+        public string GroupingSeparator { get; set; }
+        public string DecimalSeparator { get; set; }
+        public string NegativeSign { get; set; }
+        
+        public bool IsDecimal { get; set; }
+        public int MaxDigitsBeforeDecimal { get; set; }
+        public int MaxDigitsAfterDecimal { get; set; }
+        public bool UseGroupSeparator { get; set; }
+    }
+}
