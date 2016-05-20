91eed9d17dd701a031cebfd9b19bda22da80b4da
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2016-05-20 13:25:28 +0300

    Tests\Create: moved Create.Answer to separate factory class KP-7207

--- src/Tests/WB.Tests.Unit/Create.cs
+++ src/Tests/WB.Tests.Unit/Create.cs
@@ -7,12 +7,14 @@ using WB.Tests.Unit.TestFactories;
 
 namespace WB.Tests.Unit
 {
-    internal partial class Create
+    internal class Create
     {
         public static readonly CommandFactory Command = new CommandFactory();
         public static readonly EventFactory Event = new EventFactory();
         public static readonly ViewModelFactory ViewModel = new ViewModelFactory();
         public static readonly ControllerFactory Controller = new ControllerFactory();
+        public static readonly AnswerFactory Answer = new AnswerFactory();
+
         public static readonly OtherFactory Other = new OtherFactory();
     }
 }
