83fa7075da9e3225bf6b4295d521db308cd3ec63
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2016-05-20 12:01:44 +0300

    Tests\Create: moved Create.Event to separate factory class KP-7207

---
+++ src/Tests/WB.Tests.Unit/Create.Clean.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WB.Tests.Unit.TestFactories;
+
+namespace WB.Tests.Unit
+{
+    internal partial class Create
+    {
+        public static readonly CommandFactory Command = new CommandFactory();
+
+        public static readonly EventFactory Event = new EventFactory();
+    }
+}
