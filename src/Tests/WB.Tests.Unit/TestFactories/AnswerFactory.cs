aa1db0bb0a033fa7b41924a30cb94850833e3a0f
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2016-05-20 13:25:28 +0300

    Tests\Create: moved Create.Answer to separate factory class KP-7207

---
+++ src/Tests/WB.Tests.Unit/TestFactories/AnswerFactory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using WB.Core.SharedKernels.DataCollection;
+using WB.Core.SharedKernels.Enumerator.Entities.Interview;
+
+namespace WB.Tests.Unit.TestFactories
+{
+    internal class AnswerFactory
+    {
+        public LinkedSingleOptionAnswer LinkedSingleOptionAnswer(RosterVector answer, Guid? id = null)
+        {
+            var model = new LinkedSingleOptionAnswer(id ?? Guid.NewGuid(), new decimal[0]);
+            model.SetAnswer(answer);
+            return model;
+        }
+
+        public IntegerNumericAnswer IntegerNumericAnswer(Identity answerIdentity = null, int answer = 42)
+        {
+            answerIdentity = answerIdentity ?? Create.Other.Identity(Guid.NewGuid(), Core.SharedKernels.DataCollection.RosterVector.Empty);
+            var model = new IntegerNumericAnswer(answerIdentity.Id, answerIdentity.RosterVector);
+
+            model.SetAnswer(answer);
+            return model;
+        }
+
+        public RealNumericAnswer RealNumericAnswer(Identity answerIdentity = null, decimal answer = 42.42m)
+        {
+            answerIdentity = answerIdentity ?? Create.Other.Identity(Guid.NewGuid(), Core.SharedKernels.DataCollection.RosterVector.Empty);
+            var model = new RealNumericAnswer(answerIdentity.Id, answerIdentity.RosterVector);
+
+            model.SetAnswer(answer);
+            return model;
+        }
+    }
+}
