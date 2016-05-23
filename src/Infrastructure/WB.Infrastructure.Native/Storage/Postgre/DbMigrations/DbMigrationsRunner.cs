093e0c440d8b12a3f9bd20441156b239ffccb289
Andrii Kozhyn <hedgic@gmail.com>
2016-05-23 12:59:42 +0300

    KP-6953: Added initial migration option for HQ read side

---
+++ src/Infrastructure/WB.Infrastructure.Native/Storage/Postgre/DbMigrations/DbMigrationsRunner.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using FluentMigrator;
+using FluentMigrator.Runner;
+using FluentMigrator.Runner.Announcers;
+using FluentMigrator.Runner.Initialization;
+using Microsoft.Practices.ServiceLocation;
+using WB.Core.GenericSubdomains.Portable.Services;
+
+namespace WB.Infrastructure.Native.Storage.Postgre.DbMigrations
+{
+    internal static class DbMigrationsRunner
+    {
+        public class MigrationOptions : IMigrationProcessorOptions
+        {
+            public bool PreviewOnly { get; set; }
+            public string ProviderSwitches { get; set; }
+            public int Timeout { get; set; }
+        }
+
+        public static void MigrateToLatest(string connectionString, Assembly migrationsAssembly)
+        {
+            // var announcer = new NullAnnouncer();
+            var logger = ServiceLocator.Current.GetInstance<ILoggerProvider>().GetForType(typeof(DbMigrationsRunner));
+            var announcer = new TextWriterAnnouncer(s => logger.Info(s)); 
+
+            var migrationContext = new RunnerContext(announcer);
+
+            var options = new MigrationOptions { PreviewOnly = false, Timeout = 60 };
+            var factory = new FluentMigrator.Runner.Processors.Postgres.PostgresProcessorFactory();
+
+            using (var processor = factory.Create(connectionString, announcer, options))
+            {
+                var runner = new MigrationRunner(migrationsAssembly, migrationContext, processor);
+                runner.MigrateUp(true);
+            }
+        }
+    }
+}
