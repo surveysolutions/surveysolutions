1800e34355e12ebb315d197a263e6cf256666b7f
Roman Lytvyn <litvin.roma@gmail.com>
2016-05-24 16:13:29 +0300

    merge after rebase

---
+++ src/Tests/WB.Tests.Unit/SharedKernels/Enumerator/Utils/NumericTextFormatterTests/when_filter_formatted_decimal_with_max_digits_before_decimal_separator.cs
@@ -0,0 +1,33 @@
+﻿using Machine.Specifications;
+using WB.Core.SharedKernels.Enumerator.Utils;
+
+namespace WB.Tests.Unit.SharedKernels.Enumerator.Utils.NumericTextFormatterTests
+{
+    public class when_filter_formatted_decimal_with_max_digits_before_decimal_separator : NumericTextFormatterTestsContext
+    {
+        Establish context = () =>
+        {
+            var numericTextFormatterSettings = new NumericTextFormatterSettings
+            {
+                IsDecimal = true,
+                NegativeSign = "-",
+                DecimalSeparator = ".",
+                GroupingSeparator = ",",
+                MaxDigitsAfterDecimal = 15,
+                MaxDigitsBeforeDecimal = 2,
+                UseGroupSeparator = true
+            };
+
+            formatter = CreateNumericTextFormatter(numericTextFormatterSettings);
+        };
+
+        Because of = () => filteredResult = formatter.FilterFormatted(addedText: "1", sourceText: "11", insertToIndex: 2);
+
+        It should_return_empty_string = () =>
+            filteredResult.ShouldEqual("");
+        
+        static NumericTextFormatter formatter;
+
+        private static string filteredResult;
+    }
+}
