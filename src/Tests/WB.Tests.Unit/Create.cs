30d07174bb331ce27b0ba680cfbd0c6204bccd79
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2016-05-24 12:03:40 +0300

    Tests\Create: formatted methods in Create.PublishedEvent KP-7207

--- src/Tests/WB.Tests.Unit/Create.cs
+++ src/Tests/WB.Tests.Unit/Create.cs
@@ -9,13 +9,13 @@ namespace WB.Tests.Unit
 {
     internal class Create
     {
-        public static readonly CommandFactory Command = new CommandFactory(); // cleaned by TLK (remove this comment after KP-7207 is done_
+        public static readonly CommandFactory Command = new CommandFactory(); // cleaned by TLK (remove this comment after KP-7207 is done)
         public static readonly EventFactory Event = new EventFactory();
-        public static readonly PublishedEventFactory PublishedEvent = new PublishedEventFactory();
+        public static readonly PublishedEventFactory PublishedEvent = new PublishedEventFactory(); // cleaned by TLK (remove this comment after KP-7207 is done)
         public static readonly ViewModelFactory ViewModel = new ViewModelFactory();
         public static readonly ControllerFactory Controller = new ControllerFactory();
         public static readonly AnswerFactory Answer = new AnswerFactory();
-        public static readonly ServiceFactory Service = new ServiceFactory(); // cleaned by TLK (remove this comment after KP-7207 is done_
+        public static readonly ServiceFactory Service = new ServiceFactory(); // cleaned by TLK (remove this comment after KP-7207 is done)
 
         public static readonly OtherFactory Other = new OtherFactory();
     }
