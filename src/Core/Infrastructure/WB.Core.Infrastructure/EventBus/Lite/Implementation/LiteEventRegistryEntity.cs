da21cffe8b3753c71b9a7865f190879bb407b78d
Pavel Stovpivskyi <sby@rambler.ru>
2016-05-25 14:11:53 +0300

    Removed HybridEventBus from interviewer
    KP-6643

---
+++ src/Core/Infrastructure/WB.Core.Infrastructure/EventBus/Lite/Implementation/LiteEventRegistryEntity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace WB.Core.Infrastructure.EventBus.Lite.Implementation
+{
+    internal class LiteEventRegistryEntity
+    {
+        public LiteEventRegistryEntity(ILiteEventHandler eventHandler, ILiteEventRaiseFilter filter)
+        {
+            if (eventHandler == null)
+                throw new ArgumentNullException(nameof(eventHandler));
+
+            this.EventHandler = eventHandler;
+            this.Filter = filter;
+        }
+
+        public ILiteEventHandler EventHandler { get; }
+        public ILiteEventRaiseFilter Filter { get; }
+
+        public override int GetHashCode()
+        {
+            return EventHandler.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            var objEntity = obj as LiteEventRegistryEntity;
+            if (objEntity == null)
+                return false;
+            return EventHandler.Equals(objEntity.EventHandler);
+        }
+    }
+}
--- src/Core/Infrastructure/WB.Core.Infrastructure/EventBus/Lite/Implementation/RaiseFilters/AggregateRootRaiseFilter.cs
+++ src/Core/Infrastructure/WB.Core.Infrastructure/EventBus/Lite/Implementation/RaiseFilters/AggregateRootRaiseFilter.cs
@@ -1,19 +1,24 @@
-﻿using Ncqrs.Eventing;
+﻿using System;
+using Ncqrs.Eventing;
+using WB.Core.GenericSubdomains.Portable;
 
 namespace WB.Core.Infrastructure.EventBus.Lite.Implementation.RaiseFilters
 {
     public class AggregateRootRaiseFilter : ILiteEventRaiseFilter
     {
-        private readonly string aggregateRootId;
+        private readonly Guid aggregateRootId;
 
         public AggregateRootRaiseFilter(string aggregateRootId)
         {
-            this.aggregateRootId = aggregateRootId;
+            if (aggregateRootId.IsNullOrEmpty())
+                throw new ArgumentNullException(nameof(aggregateRootId));
+
+            this.aggregateRootId = Guid.Parse(aggregateRootId); 
         }
 
         public bool IsNeedRaise(CommittedEvent @event)
         {
-            return @event.EventSourceId.ToString() == this.aggregateRootId;
+            return @event.EventSourceId == this.aggregateRootId;
         }
     }
 }
