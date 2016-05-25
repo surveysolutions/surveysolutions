59952d6983204298ad86121d82a1734ca0444e3b
Pavel Stovpivskyi <sby@rambler.ru>
2016-05-25 15:42:28 +0300

    Fixed same tests
    KP-6643

--- src/Core/Infrastructure/WB.Core.Infrastructure/EventBus/Lite/Implementation/RaiseFilters/AggregateRootRaiseFilter.cs
+++ src/Core/Infrastructure/WB.Core.Infrastructure/EventBus/Lite/Implementation/RaiseFilters/AggregateRootRaiseFilter.cs
@@ -6,19 +6,19 @@ namespace WB.Core.Infrastructure.EventBus.Lite.Implementation.RaiseFilters
 {
     public class AggregateRootRaiseFilter : ILiteEventRaiseFilter
     {
-        private readonly Guid aggregateRootId;
+        private readonly string aggregateRootId;
 
         public AggregateRootRaiseFilter(string aggregateRootId)
         {
             if (aggregateRootId.IsNullOrEmpty())
                 throw new ArgumentNullException(nameof(aggregateRootId));
 
-            this.aggregateRootId = Guid.Parse(aggregateRootId); 
+            this.aggregateRootId = aggregateRootId; 
         }
 
         public bool IsNeedRaise(CommittedEvent @event)
         {
-            return @event.EventSourceId == this.aggregateRootId;
+            return @event.EventSourceId.FormatGuid() == this.aggregateRootId;
         }
     }
 }
