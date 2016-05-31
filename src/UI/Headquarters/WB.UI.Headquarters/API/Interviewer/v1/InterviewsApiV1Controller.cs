01765ffc64c4c4f7c9cdf845ba72d4b244eb33d3
Andrii Kozhyn <hedgic@gmail.com>
2016-05-31 15:06:50 +0300

    KP-7213: Removed obsolete code

--- src/UI/Headquarters/WB.UI.Headquarters/API/Interviewer/v1/InterviewsApiV1Controller.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/API/Interviewer/v1/InterviewsApiV1Controller.cs
@@ -9,13 +9,10 @@ using Main.Core.Entities.SubEntities;
 using WB.Core.BoundedContexts.Headquarters.Services;
 using WB.Core.BoundedContexts.Headquarters.Views.Interview;
 using WB.Core.BoundedContexts.Headquarters.Views.SynchronizationLog;
-using WB.Core.GenericSubdomains.Portable;
 using WB.Core.GenericSubdomains.Portable.Services;
 using WB.Core.Infrastructure.CommandBus;
-using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
 using WB.Core.SharedKernel.Structures.Synchronization;
 using WB.Core.SharedKernel.Structures.Synchronization.SurveyManagement;
-using WB.Core.SharedKernels.DataCollection.Commands.Interview;
 using WB.Core.SharedKernels.DataCollection.DataTransferObjects.Synchronization;
 using WB.Core.SharedKernels.DataCollection.Repositories;
 using WB.Core.SharedKernels.DataCollection.ValueObjects.Interview;
@@ -23,7 +20,6 @@ using WB.Core.SharedKernels.DataCollection.WebApi;
 using WB.Core.SharedKernels.SurveyManagement.Web.Code;
 using WB.Core.SharedKernels.SurveyManagement.Web.Utils.Membership;
 using WB.Core.Synchronization.MetaInfo;
-using WB.Core.Synchronization.SyncStorage;
 
 namespace WB.Core.SharedKernels.SurveyManagement.Web.Api.Interviewer.v1
 {
@@ -38,7 +34,6 @@ namespace WB.Core.SharedKernels.SurveyManagement.Web.Api.Interviewer.v1
             IInterviewInformationFactory interviewsFactory,
             IInterviewPackagesService incomingSyncPackagesQueue,
             ICommandService commandService,
-            IQueryableReadSideRepositoryReader<InterviewSyncPackageMeta> syncPackagesMetaReader,
             IMetaInfoBuilder metaBuilder,
             IJsonAllTypesSerializer synchronizationSerializer) : base(
                 plainInterviewFileStorage: plainInterviewFileStorage, 
@@ -46,7 +41,6 @@ namespace WB.Core.SharedKernels.SurveyManagement.Web.Api.Interviewer.v1
                 interviewsFactory: interviewsFactory,
                 interviewPackagesService: incomingSyncPackagesQueue,
                 commandService: commandService,
-                syncPackagesMetaReader: syncPackagesMetaReader,
                 metaBuilder: metaBuilder,
                 synchronizationSerializer: synchronizationSerializer)
         {
@@ -94,145 +88,6 @@ namespace WB.Core.SharedKernels.SurveyManagement.Web.Api.Interviewer.v1
         public void Post(Guid id, [FromBody]string package) => this.interviewPackagesService.StorePackage(item: package);
         [HttpPost]
         public override void PostImage(PostFileRequest request) => base.PostImage(request);
-
-        [HttpGet]
-        [WriteToSyncLog(SynchronizationLogType.GetInterviewPackages)]
-        public InterviewPackagesApiView GetPackages(string lastPackageId = null)
-        {
-            var interviewPackages = this.GetInterviewPackages(
-                userId: this.globalInfoProvider.GetCurrentUser().Id,
-                lastSyncedPackageId: lastPackageId);
-
-            var interviewsByPackages = this.interviewsFactory.GetInterviewsByIds(
-                interviewPackages.Where(package => package.ItemType == SyncItemType.Interview)
-                    .Select(package => package.InterviewId).Distinct().ToArray());
-
-            return new InterviewPackagesApiView()
-            {
-                Packages = interviewPackages,
-                Interviews = interviewsByPackages.Select(interview => new InterviewApiView()
-                {
-                    Id = interview.Id,
-                    QuestionnaireIdentity = interview.QuestionnaireIdentity,
-                    IsRejected = interview.IsRejected
-                }).ToList()
-            };
-        }
-
-        [HttpGet]
-        [WriteToSyncLog(SynchronizationLogType.GetInterviewPackage)]
-        public InterviewSyncPackageDto GetPackage(string id)
-        {
-            var packageMetaInformation = this.syncPackagesMetaReader.GetById(id);
-            if (packageMetaInformation == null)
-                throw new HttpResponseException(HttpStatusCode.NotFound);
-
-            var interviewSynchronizationPackage = new InterviewSyncPackageDto
-            {
-                PackageId = packageMetaInformation.PackageId
-            };
-
-            if (packageMetaInformation.ItemType == SyncItemType.Interview)
-            {
-                var interviewSynchronizationDto = this.interviewsFactory.GetInterviewDetails(packageMetaInformation.InterviewId);
-
-                interviewSynchronizationPackage.Content = this.synchronizationSerializer.Serialize(interviewSynchronizationDto);
-                interviewSynchronizationPackage.MetaInfo =
-                    this.synchronizationSerializer.Serialize(this.metaBuilder.GetInterviewMetaInfo(interviewSynchronizationDto));
-            }
-            else
-            {
-                interviewSynchronizationPackage.Content = packageMetaInformation.InterviewId.FormatGuid();
-            }
-
-            return interviewSynchronizationPackage;
-        }
-
-        [HttpPost]
-        [WriteToSyncLog(SynchronizationLogType.InterviewPackageProcessed)]
-        public void LogPackageAsSuccessfullyHandled(string id)
-        {
-            InterviewSyncPackageMeta syncPackage = this.syncPackagesMetaReader.GetById(id);
-            if (syncPackage != null && syncPackage.ItemType == SyncItemType.Interview)
-            {
-                this.commandService.Execute(new MarkInterviewAsReceivedByInterviewer(syncPackage.InterviewId, GlobalInfo.GetCurrentUser().Id));
-            }
-        }
-
-        [HttpPost]
-        [WriteToSyncLog(SynchronizationLogType.PostPackage)]
-        public void PostPackage(Guid id, [FromBody] string package) => this.Post(id, package);
-
-        private List<SynchronizationChunkMeta> GetInterviewPackages(Guid userId, string lastSyncedPackageId)
-        {
-            IList<InterviewSyncPackageMeta> allUpdatesFromLastPackage =
-                this.GetUpdateFromLastPackage(userId, lastSyncedPackageId, this.GetGroupedInterviewSyncPackage, this.GetLastInterviewSyncPackage);
-
-            return allUpdatesFromLastPackage.Select(x =>
-                new SynchronizationChunkMeta(x.PackageId, x.SortIndex, x.UserId, x.ItemType)
-                {
-                    InterviewId = x.InterviewId
-                }).ToList();
-        }
-        
-        private IQueryable<InterviewSyncPackageMeta> GetLastInterviewSyncPackage(Guid userId)
-        {
-            List<InterviewSyncPackageMeta> result = this.syncPackagesMetaReader.Query(_ =>
-               _.Where(x => x.UserId == userId).ToList()
-            );
-
-            return result.AsQueryable();
-        }
-        
-        private IQueryable<InterviewSyncPackageMeta> GetGroupedInterviewSyncPackage(Guid userId, long? lastSyncedSortIndex)
-        {
-            var packages = this.syncPackagesMetaReader.Query(_ =>
-            {
-                var filteredItems = _.Where(x => x.UserId == userId);
-
-                if (lastSyncedSortIndex.HasValue)
-                {
-                    filteredItems = filteredItems.Where(x => x.SortIndex > lastSyncedSortIndex.Value);
-                }
-
-                return filteredItems.OrderBy(x => x.SortIndex).ThenBy(x => x.PackageId).ToList();
-            });
-
-            IEnumerable<InterviewSyncPackageMeta> result =
-                from p in packages
-                group p by p.InterviewId into g
-                select g.Last();
-
-            if (lastSyncedSortIndex == null)
-            {
-                result = result.Where(x => x.ItemType != SyncItemType.DeleteInterview);
-            }
-
-            return result.AsQueryable();
-        }
-        
-        private IList<T> GetUpdateFromLastPackage<T>(Guid userId, string lastSyncedPackageId,
-            Func<Guid, long?, IQueryable<T>> groupedQuery, Func<Guid, IQueryable<T>> allQuery)
-            where T : InterviewSyncPackageMeta
-        {
-            if (lastSyncedPackageId == null)
-            {
-                return groupedQuery(userId, null).ToList();
-            }
-
-            var queryable = allQuery(userId).ToList();
-            var lastSyncedPackage = queryable
-                .FirstOrDefault(x => x.PackageId == lastSyncedPackageId);
-
-            if (lastSyncedPackage == null)
-            {
-                throw new SyncPackageNotFoundException($"Sync package with id {lastSyncedPackageId} was not found on server");
-            }
-
-            long lastSyncedSortIndex = lastSyncedPackage.SortIndex;
-
-            return groupedQuery(userId, lastSyncedSortIndex).ToList();
-        }
 
         private InterviewAnswerApiView ToInterviewApiView(AnsweredQuestionSynchronizationDto answer)
         {
