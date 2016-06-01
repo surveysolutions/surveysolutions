496f7e730564a4164fb9a9a670b9bb0271d94d11
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2016-06-01 12:08:39 +0300

    Tests\Create: moved stubs from Create.Other to Create.Fake KP-7207

--- src/Tests/WB.Tests.Unit/SharedKernels/Enumerator/StatefulInterviewRepositoryTests/StatefulInterviewRepositoryNUnitTests.cs
+++ src/Tests/WB.Tests.Unit/SharedKernels/Enumerator/StatefulInterviewRepositoryTests/StatefulInterviewRepositoryNUnitTests.cs
@@ -63,9 +63,9 @@ namespace WB.Tests.Unit.SharedKernels.Enumerator.StatefulInterviewRepositoryTest
         {
             var aggregateRootId = Guid.Parse("11111111111111111111111111111111");
             AssemblyContext.SetupServiceLocator();
-            var snapshotStore = Create.Other.SnapshotStore(aggregateRootId);
-            var eventStore = Create.Other.EventStore(aggregateRootId, Array.Empty<CommittedEvent>());
-            var aggregateSnapshotter = Create.Other.AggregateSnapshotter();
+            var snapshotStore = Create.Fake.SnapshotStore(aggregateRootId);
+            var eventStore = Create.Fake.EventStore(aggregateRootId, Array.Empty<CommittedEvent>());
+            var aggregateSnapshotter = Create.Fake.AggregateSnapshotter();
             Setup.InstanceToMockedServiceLocator(Create.AggregateRoot.StatefulInterview(questionnaireId: Guid.NewGuid(),
                 userId: Guid.NewGuid(), questionnaire: null));
             var domaiRepository = Create.Service.DomainRepository(aggregateSnapshotter: aggregateSnapshotter, serviceLocator: ServiceLocator.Current);
--- src/Tests/WB.Tests.Unit/TestFactories/OtherFactory.cs
+++ src/Tests/WB.Tests.Unit/TestFactories/OtherFactory.cs
@@ -1,24 +1,13 @@
 ﻿using System;
 using Moq;
 using Ncqrs.Eventing;
-using Ncqrs.Eventing.ServiceModel.Bus;
-using System.Collections.Generic;
-using Ncqrs.Eventing.Storage;
 using Ncqrs.Spec;
-using NSubstitute;
 using WB.Core.Infrastructure.CommandBus;
-using WB.Core.SharedKernels.DataCollection.Aggregates;
-using WB.Core.SharedKernels.DataCollection.Implementation.Entities;
-using WB.Core.SharedKernels.DataCollection.Repositories;
 using WB.Core.SharedKernels.DataCollection.Views.BinaryData;
-using WB.Core.SharedKernels.Enumerator.Aggregates;
 using WB.Core.SharedKernels.Enumerator.Repositories;
 using WB.Core.SharedKernels.Enumerator.Services;
 using WB.Core.SharedKernels.Enumerator.ViewModels;
 using IEvent = WB.Core.Infrastructure.EventBus.IEvent;
-using Ncqrs.Domain;
-using Ncqrs.Domain.Storage;
-using Ncqrs.Eventing.Sourcing.Snapshotting;
 
 namespace WB.Tests.Unit.TestFactories
 {
@@ -59,55 +48,12 @@ namespace WB.Tests.Unit.TestFactories
             return result;
         }
 
-
-        public IPublishableEvent PublishableEvent(Guid? eventSourceId = null, IEvent payload = null)
-        {
-            return Mock.Of<IPublishableEvent>(_ => _.Payload == (payload ?? Mock.Of<IEvent>()) && _.EventSourceId == (eventSourceId ?? Guid.NewGuid()));
-        }
-
-        public IPlainQuestionnaireRepository QuestionnaireRepositoryStubWithOneQuestionnaire(
-            Guid questionnaireId, IQuestionnaire questionnaire = null, long? questionnaireVersion = null)
-        {
-            questionnaire = questionnaire ?? Mock.Of<IQuestionnaire>();
-
-            return Mock.Of<IPlainQuestionnaireRepository>(repository
-                => repository.GetHistoricalQuestionnaire(questionnaireId, questionnaireVersion ?? questionnaire.Version) == questionnaire
-                && repository.GetHistoricalQuestionnaire(questionnaireId, questionnaireVersion ?? 1) == questionnaire
-                && repository.GetQuestionnaire(Moq.It.IsAny<QuestionnaireIdentity>()) == questionnaire);
-        }
-
-        public UncommittedEvent UncommittedEvent(Guid? eventSourceId = null, 
+        public UncommittedEvent UncommittedEvent(Guid? eventSourceId = null,
             IEvent payload = null,
             int sequence = 1,
             int initialVersion = 1)
         {
             return new UncommittedEvent(Guid.NewGuid(), eventSourceId ?? Guid.NewGuid(), sequence, initialVersion, DateTime.Now, payload);
-        }
-
-        public ISnapshotStore SnapshotStore(Guid aggregateRootId, Snapshot snapshot = null)
-        {
-            return Mock.Of<ISnapshotStore>(_ => _.GetSnapshot(aggregateRootId, Moq.It.IsAny<int>()) == snapshot);
-        }
-
-        public IEventStore EventStore(Guid eventSourceId, IEnumerable<CommittedEvent> committedEvents)
-        {
-            return Mock.Of<IEventStore>(_ =>
-                _.Read(eventSourceId, Moq.It.IsAny<int>()) == new CommittedEventStream(eventSourceId, committedEvents));
-        }
-
-        public IAggregateSnapshotter AggregateSnapshotter(EventSourcedAggregateRoot aggregateRoot = null, bool isARLoadedFromSnapshotSuccessfully = false)
-        {
-            return Mock.Of<IAggregateSnapshotter>(_ =>
-                _.TryLoadFromSnapshot(Moq.It.IsAny<Type>(), Moq.It.IsAny<Snapshot>(),
-                    Moq.It.IsAny<CommittedEventStream>(), out aggregateRoot) ==
-                isARLoadedFromSnapshotSuccessfully);
-        }
-
-        public IStatefulInterviewRepository StatefulInterviewRepositoryWith(IStatefulInterview interview)
-        {
-            var result = Substitute.For<IStatefulInterviewRepository>();
-            result.Get(null).ReturnsForAnyArgs(interview);
-            return result;
         }
     }
 }
