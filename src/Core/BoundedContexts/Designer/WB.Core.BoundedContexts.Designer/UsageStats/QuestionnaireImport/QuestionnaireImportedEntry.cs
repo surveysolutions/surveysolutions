a5ad8cacb77e0d0d70f356ec597941e68d598ef3
Andrii Kozhyn <hedgic@gmail.com>
2016-06-02 13:33:24 +0300

    KP-7284: Store ids of questionnaires that were imported on HQ

---
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/UsageStats/QuestionnaireImport/QuestionnaireImportedEntry.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WB.Core.BoundedContexts.Designer.UsageStats.QuestionnaireImport
+{
+    public class QuestionnaireImportedEntry
+    {
+        public virtual DateTime ImportDateUtc { get; set; }
+
+        public virtual Guid QuestionnaireId { get; set; }
+
+        public virtual int[] SupportedByHqVersion { get; set; }
+    }
+}
