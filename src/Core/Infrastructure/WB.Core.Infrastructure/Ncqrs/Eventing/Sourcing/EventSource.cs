f341249d0cbf5b0daf25903adb65c3bbcdee8d3d
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2016-06-02 11:48:28 +0300

    IEventSource: removed override of method InititalizeFromHistory KP-7058

--- src/Core/Infrastructure/WB.Core.Infrastructure/Ncqrs/Eventing/Sourcing/EventSource.cs
+++ src/Core/Infrastructure/WB.Core.Infrastructure/Ncqrs/Eventing/Sourcing/EventSource.cs
@@ -68,14 +68,6 @@ namespace Ncqrs.Eventing.Sourcing
             _initialVersion = _currentVersion = snapshot.Version;
         }
 
-        public void InitializeFromHistory(CommittedEventStream history)
-        {
-            if (history == null)
-                throw new ArgumentNullException(nameof(history));
-
-            this.InitializeFromHistory(history.SourceId, history);
-        }
-
         public void InitializeFromHistory(Guid eventSourceId, IEnumerable<CommittedEvent> history)
         {
             if (history == null)
