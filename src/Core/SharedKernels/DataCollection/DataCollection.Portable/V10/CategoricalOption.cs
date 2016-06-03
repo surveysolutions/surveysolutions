3041b3e1921f821e75138c48ee940e185a8f71f4
Slava Scherbak <slava.scherbak@gmail.com>
2016-06-03 17:00:33 -0400

    Expression state: added method to filter options. KP-7266

---
+++ src/Core/SharedKernels/DataCollection/DataCollection.Portable/V10/CategoricalOption.cs
@@ -0,0 +1,9 @@
+namespace WB.Core.SharedKernels.DataCollection.V10
+{
+    public class CategoricalOption
+    {
+        public long Value { get; set; }
+        public long? ParentValue { get; set; }
+        public string Title { get; set; }
+    }
+}
