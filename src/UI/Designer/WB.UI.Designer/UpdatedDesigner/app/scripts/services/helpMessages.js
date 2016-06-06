38dcd37fedbcad844e41959397a0ac6caa5ec177
Roman Lytvyn <litvin.roma@gmail.com>
2016-06-06 17:40:06 +0300

    D: changed some massages by timestamp question. KP-6975

--- src/UI/Designer/WB.UI.Designer/UpdatedDesigner/app/scripts/services/helpMessages.js
+++ src/UI/Designer/WB.UI.Designer/UpdatedDesigner/app/scripts/services/helpMessages.js
@@ -22,7 +22,8 @@
                 fixedTitles: 'Number of items determines the roster size. Each element of the list corresponds to a title of the roster row. Each element of the list should be placed on a separate line.',
                 useFormatting: 'When checked, the numeric field on the tablet will be formatted as: d,ddd,ddd.dd, "d" represents digits (0-9), “,”(comma) is a thousand separator, and "." (dot) is a decimal point symbol.  Thousand separator and the decimal point symbol may vary for tablets with different cultures.',
                 hideIfDisabled: 'By default, a disabled item is greyed-out. Checking this option will hide a disabled question or section on a tablet.',
-                hideInstructions: 'By default, an instruction text is shown on a tablet. Checking this option will show only the first line of instructions. Tapping on that line will show the complete text.'
+                hideInstructions: 'By default, an instruction text is shown on a tablet. Checking this option will show only the first line of instructions. Tapping on that line will show the complete text.',
+                isTimestamp: 'This option allows the user to stamp the current time on tablet'
             };
 
             helpService.getHelpMessage = function(key) {
