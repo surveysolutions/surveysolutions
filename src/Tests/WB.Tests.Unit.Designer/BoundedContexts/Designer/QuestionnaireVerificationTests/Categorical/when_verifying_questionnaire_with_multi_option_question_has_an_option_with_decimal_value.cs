c26c35a982747504f81702863fa3c93a077fc8aa
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2016-06-06 15:11:46 -0400

    Unit tests for integer restrictions : KP-7280

--- src/Tests/WB.Tests.Unit.Designer/BoundedContexts/Designer/QuestionnaireVerificationTests/Categorical/when_verifying_questionnaire_with_multi_option_question_has_an_option_with_decimal_value.cs
+++ src/Tests/WB.Tests.Unit.Designer/BoundedContexts/Designer/QuestionnaireVerificationTests/Categorical/when_verifying_questionnaire_with_multi_option_question_has_an_option_with_decimal_value.cs
@@ -9,14 +9,18 @@ using It = Machine.Specifications.It;
 
 namespace WB.Tests.Unit.Designer.BoundedContexts.Designer.QuestionnaireVerificationTests.Categorical
 {
-    internal class when_verifying_questionnaire_with_single_option_question_has_an_option_with_decimal_value : QuestionnaireVerifierTestsContext
+    internal class when_verifying_questionnaire_with_multi_option_question_has_an_option_with_decimal_value : QuestionnaireVerifierTestsContext
     {
         Establish context = () =>
         {
             questionnaire = CreateQuestionnaireDocument(
-                Create.SingleOptionQuestion(questionId: optionQuestionId, variable: "var",
-                    answerCodes: new[] {3.3m, 4m}));
-            
+                Create.MultyOptionsQuestion(id: multiOptionQuestionId, variable: "var",
+                    options: new[]
+                    {
+                        Create.Answer("1.3", 1.3m),
+                        Create.Answer("2", 2)
+                    }));
+
 
             verifier = CreateQuestionnaireVerifier();
         };
@@ -27,8 +31,8 @@ namespace WB.Tests.Unit.Designer.BoundedContexts.Designer.QuestionnaireVerificat
         It should_have_1_error = () =>
             resultErrors.Count().ShouldEqual(1);
 
-        It should_return_message_with_code__WB0114__ = () =>
-        resultErrors.Single().Code.ShouldEqual("WB0114");
+        It should_return_message_with_code__WB0008__ = () =>
+        resultErrors.Single().Code.ShouldEqual("WB0008");
 
         It should_return_message_with_one_references = () =>
             resultErrors.Single().References.Count().ShouldEqual(1);
@@ -37,11 +41,11 @@ namespace WB.Tests.Unit.Designer.BoundedContexts.Designer.QuestionnaireVerificat
             resultErrors.Single().References.First().Type.ShouldEqual(QuestionnaireVerificationReferenceType.Question);
 
         It should_return_message_reference_with_id_of_multi_option_question = () =>
-            resultErrors.Single().References.First().Id.ShouldEqual(optionQuestionId);
+            resultErrors.Single().References.First().Id.ShouldEqual(multiOptionQuestionId);
 
         private static IEnumerable<QuestionnaireVerificationMessage> resultErrors;
         private static QuestionnaireVerifier verifier;
         private static QuestionnaireDocument questionnaire;
-        private static Guid optionQuestionId = Guid.Parse("12222222222222222222222222222222");
+        private static Guid multiOptionQuestionId = Guid.Parse("12222222222222222222222222222222");
     }
 }
--- src/Tests/WB.Tests.Unit.Designer/BoundedContexts/Designer/QuestionnaireVerificationTests/Categorical/when_verifying_questionnaire_with_single_option_question_has_an_option_with_negative_integer_value.cs
+++ src/Tests/WB.Tests.Unit.Designer/BoundedContexts/Designer/QuestionnaireVerificationTests/Categorical/when_verifying_questionnaire_with_single_option_question_has_an_option_with_negative_integer_value.cs
@@ -9,13 +9,13 @@ using It = Machine.Specifications.It;
 
 namespace WB.Tests.Unit.Designer.BoundedContexts.Designer.QuestionnaireVerificationTests.Categorical
 {
-    internal class when_verifying_questionnaire_with_single_option_question_has_an_option_with_decimal_value : QuestionnaireVerifierTestsContext
+    internal class when_verifying_questionnaire_with_single_option_question_has_an_option_with_negative_integer_value : QuestionnaireVerifierTestsContext
     {
         Establish context = () =>
         {
             questionnaire = CreateQuestionnaireDocument(
                 Create.SingleOptionQuestion(questionId: optionQuestionId, variable: "var",
-                    answerCodes: new[] {3.3m, 4m}));
+                    answerCodes: new[] {-3m, -14}));
             
 
             verifier = CreateQuestionnaireVerifier();
@@ -24,20 +24,8 @@ namespace WB.Tests.Unit.Designer.BoundedContexts.Designer.QuestionnaireVerificat
         Because of = () =>
             resultErrors = verifier.CheckForErrors(questionnaire);
 
-        It should_have_1_error = () =>
-            resultErrors.Count().ShouldEqual(1);
-
-        It should_return_message_with_code__WB0114__ = () =>
-        resultErrors.Single().Code.ShouldEqual("WB0114");
-
-        It should_return_message_with_one_references = () =>
-            resultErrors.Single().References.Count().ShouldEqual(1);
-
-        It should_return_message_reference_with_type_Question = () =>
-            resultErrors.Single().References.First().Type.ShouldEqual(QuestionnaireVerificationReferenceType.Question);
-
-        It should_return_message_reference_with_id_of_multi_option_question = () =>
-            resultErrors.Single().References.First().Id.ShouldEqual(optionQuestionId);
+        It should_have_0_error = () =>
+            resultErrors.Count().ShouldEqual(0);
 
         private static IEnumerable<QuestionnaireVerificationMessage> resultErrors;
         private static QuestionnaireVerifier verifier;
