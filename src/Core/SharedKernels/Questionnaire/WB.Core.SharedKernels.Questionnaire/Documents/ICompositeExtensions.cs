d9a4e45d05820c190e4ede72686a5a17c738a416
Andrii Kozhyn <hedgic@gmail.com>
2016-06-07 12:32:02 +0300

    KP-7259: Extracted common code to get entity title

---
+++ src/Core/SharedKernels/Questionnaire/WB.Core.SharedKernels.Questionnaire/Documents/ICompositeExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Main.Core.Entities.Composite;
+using Main.Core.Entities.SubEntities;
+
+namespace WB.Core.SharedKernels.Questionnaire.Documents
+{
+    public static class ICompositeExtensions
+    {
+        public static string GetTitle(this IComposite entity)
+        {
+            return  (entity as IQuestion)?.QuestionText ?? (entity as IGroup)?.Title ?? (entity as IStaticText)?.Text;
+        }
+    }
+}
