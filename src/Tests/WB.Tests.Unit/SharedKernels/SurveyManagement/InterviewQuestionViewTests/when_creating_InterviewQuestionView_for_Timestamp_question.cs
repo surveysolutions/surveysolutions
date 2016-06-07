58128279f48e722fe176838991c43f378fae4064
Roman Lytvyn <litvin.roma@gmail.com>
2016-06-07 13:53:44 +0300

    HQ/SV: Current Time Question. Interview details page. code review changes. KP-6976

---
+++ src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/InterviewQuestionViewTests/when_creating_InterviewQuestionView_for_Timestamp_question.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Machine.Specifications;
+using Main.Core.Entities.SubEntities.Question;
+using WB.Core.BoundedContexts.Headquarters.Views.Interview;
+using WB.Core.SharedKernels.DataCollection.ValueObjects.Interview;
+
+namespace WB.Tests.Unit.SharedKernels.SurveyManagement.InterviewQuestionViewTests
+{
+    internal class when_creating_InterviewQuestionView_for_Timestamp_question
+    {
+        Establish context = () =>
+        {
+            questionTemplate = Create.Entity.DateTimeQuestion(questionId: questionId, isTimestamp: true);
+            questionData = Create.Entity.InterviewQuestion(questionId);
+            questionData.Answer = new DateTime(2016, 06, 07, 12, 0, 01);
+        };
+
+        Because of = () => timestampQuestionView = new InterviewQuestionView(questionTemplate, questionData, null, false, new decimal[0], InterviewStatus.Completed);
+
+        It should_view_has_specified_settings = () =>
+            ((DateTimeQuestionSettings)timestampQuestionView.Settings).IsTimestamp.ShouldEqual(true);
+
+        private static InterviewQuestionView timestampQuestionView;
+        private static DateTimeQuestion questionTemplate;
+        private static InterviewQuestion questionData;
+        private static readonly Guid questionId = Guid.Parse("44444444444444444444444444444444");
+    }
+}
