8d4faca727b9a1946d474a534bece3988a430896
Anastasiia Kholodova <nastya.kholodova@gmail.com>
2016-06-09 12:26:16 -0400

    Interviewer: KP-7252 show loading screen

--- src/Core/SharedKernels/Enumerator/Enumerator/Repositories/IStatefulInterviewRepository.cs
+++ src/Core/SharedKernels/Enumerator/Enumerator/Repositories/IStatefulInterviewRepository.cs
@@ -1,4 +1,7 @@
+using System;
 using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 using WB.Core.SharedKernels.Enumerator.Aggregates;
 
 namespace WB.Core.SharedKernels.Enumerator.Repositories
@@ -6,5 +9,6 @@ namespace WB.Core.SharedKernels.Enumerator.Repositories
     public interface IStatefulInterviewRepository
     {
         IStatefulInterview Get(string interviewId);
+        Task<IStatefulInterview> GetAsync(string interviewId, IProgress<int> progress, CancellationToken cancellationToken);
     }
 }
--- src/Infrastructure/WB.Infrastructure.Native/Storage/EventStore/Implementation/WriteSideEventStore.cs
+++ src/Infrastructure/WB.Infrastructure.Native/Storage/EventStore/Implementation/WriteSideEventStore.cs
@@ -86,6 +86,11 @@ namespace WB.Infrastructure.Native.Storage.EventStore.Implementation
             } while (!currentSlice.IsEndOfStream);
         }
 
+        public IEnumerable<CommittedEvent> Read(Guid id, int minVersion, IProgress<int> progress, CancellationToken cancellationToken)
+        {
+            return Read(id, minVersion);
+        }
+
         public IEnumerable<CommittedEvent> GetAllEvents()
         {
             Position position = Position.Start;
