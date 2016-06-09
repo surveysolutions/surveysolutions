b943085b115994bda5b4b1b0ff48ad908204c0a3
Anastasya Kholodova <nastya.kholodova@gmail.com>
2016-06-09 14:12:03 -0400

    Interviewer: KP-7252 add toolbar navigation

--- src/UI/Interviewer/WB.UI.Interviewer/Activities/LoadingActivity.cs
+++ src/UI/Interviewer/WB.UI.Interviewer/Activities/LoadingActivity.cs
@@ -1,19 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 using Android.App;
 using Android.Content;
 using Android.OS;
-using Android.Runtime;
+using Android.Support.V7.Widget;
 using Android.Views;
-using Android.Widget;
-using MvvmCross.Platform;
-using MvvmCross.Platform.Platform;
+using WB.Core.BoundedContexts.Interviewer.Properties;
 using WB.Core.BoundedContexts.Interviewer.Views;
-using WB.UI.Interviewer.ViewModel;
 using WB.UI.Shared.Enumerator.Activities;
 
 namespace WB.UI.Interviewer.Activities
@@ -21,13 +13,17 @@ namespace WB.UI.Interviewer.Activities
     [Activity(Label = "",
         Theme = "@style/GrayAppTheme",
         WindowSoftInputMode = SoftInput.StateHidden,
-        ConfigurationChanges = Android.Content.PM.ConfigChanges.Orientation | Android.Content.PM.ConfigChanges.ScreenSize)]
+        ConfigurationChanges =
+            Android.Content.PM.ConfigChanges.Orientation | Android.Content.PM.ConfigChanges.ScreenSize)]
     public class LoadingActivity : BaseActivity<LoadingViewModel>
     {
         protected override int ViewResourceId => Resource.Layout.loading;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
+            var toolbar = this.FindViewById<Toolbar>(Resource.Id.toolbar);
+            this.SetSupportActionBar(toolbar);
 
             Task.Run(async () =>
             {
@@ -38,7 +34,48 @@ namespace WB.UI.Interviewer.Activities
 
         public override async void OnBackPressed()
         {
-            await this.ViewModel.NavigateBackToDashboardCommand.ExecuteAsync();
+            await this.ViewModel.NavigateToDashboardCommand.ExecuteAsync();
+            CancelLoadingAndFinishActivity();
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            switch (item.ItemId)
+            {
+                case Resource.Id.menu_dashboard:
+                    this.ViewModel.NavigateToDashboardCommand.Execute();
+                    CancelLoadingAndFinishActivity();
+                    break;
+                case Resource.Id.menu_signout:
+                    this.ViewModel.SignOutCommand.Execute();
+                    CancelLoadingAndFinishActivity();
+                    break;
+                case Resource.Id.menu_settings:
+                    Intent intent = new Intent(this, typeof(PrefsActivity));
+                    this.StartActivity(intent);
+                    CancelLoadingAndFinishActivity();
+                    break;
+                case Android.Resource.Id.Home:
+                    CancelLoadingAndFinishActivity();
+                    return true;
+            }
+            return base.OnOptionsItemSelected(item);
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            this.MenuInflater.Inflate(Resource.Menu.diagnostics, menu);
+
+            menu.LocalizeMenuItem(Resource.Id.menu_dashboard, InterviewerUIResources.MenuItem_Title_Dashboard);
+            menu.LocalizeMenuItem(Resource.Id.menu_settings, InterviewerUIResources.MenuItem_Title_Settings);
+            menu.LocalizeMenuItem(Resource.Id.menu_signout, InterviewerUIResources.MenuItem_Title_SignOut);
+            
+            return base.OnCreateOptionsMenu(menu);
+        }
+
+        private void CancelLoadingAndFinishActivity()
+        {
+            this.ViewModel.CancelLoadingCommand.Execute();
             this.Finish();
         }
     }
