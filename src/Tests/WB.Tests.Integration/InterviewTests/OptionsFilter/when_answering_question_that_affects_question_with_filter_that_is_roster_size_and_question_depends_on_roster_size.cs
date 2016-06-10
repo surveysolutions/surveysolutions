65015bac8227c3c08acb24dd65380c058e0adcd9
Slava Scherbak <slava.scherbak@gmail.com>
2016-06-10 12:08:10 -0400

    Expression state: added failed test for options filter and roster.  KP-7266

---
+++ src/Tests/WB.Tests.Integration/InterviewTests/OptionsFilter/when_answering_question_that_affects_question_with_filter_that_is_roster_size_and_question_depends_on_roster_size.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using AppDomainToolkit;
+using Machine.Specifications;
+using Main.Core.Entities.Composite;
+using Main.Core.Entities.SubEntities;
+using Ncqrs.Spec;
+using WB.Core.SharedKernels.DataCollection;
+using WB.Core.SharedKernels.DataCollection.Events.Interview;
+using WB.Core.SharedKernels.DataCollection.Implementation.Aggregates;
+
+namespace WB.Tests.Integration.InterviewTests.OptionsFilter
+{
+    internal class when_answering_question_that_affects_question_with_filter_that_is_roster_size_and_question_depends_on_roster_size : InterviewTestsContext
+    {
+        Establish context = () =>
+        {
+            appDomainContext = AppDomainContext.Create();
+        };
+
+        Because of = () =>
+            results = Execute.InStandaloneAppDomain(appDomainContext.Domain, () =>
+            {
+                Setup.MockedServiceLocator();
+
+                var options = new List<Answer>
+                {
+                    Create.Option(value: "1", text: "Option 1"),
+                    Create.Option(value: "2", text: "Option 2"),
+                    Create.Option(value: "3", text: "Option 3"),
+                    Create.Option(value: "12", text: "Option 12"),
+                };
+
+                var questionnaireDocument = Create.QuestionnaireDocument(questionnaireId, children: new IComposite[]
+                {
+                    Create.NumericIntegerQuestion(q1Id, variable: "q1"),
+                    Create.Roster(rosterId, rosterSizeSourceType: RosterSizeSourceType.FixedTitles, fixedRosterTitles: new []{ Create.FixedRosterTitle(1, "Hello")}, children: new IComposite[]
+                    {
+                        Create.MultyOptionsQuestion(q2Id, variable: "q2", options: options, optionsFilter: "@optioncode < q1"),
+                        Create.Roster(roster2Id, variable:"r", rosterSizeQuestionId: q2Id, rosterSizeSourceType: RosterSizeSourceType.Question, children: new IComposite[]{}),
+                        Create.SingleQuestion(q3Id, variable: "q3", options: options, enablementCondition: "r.Count() < 2"),
+                    })
+                });
+
+                ILatestInterviewExpressionState interviewState = GetInterviewExpressionState(questionnaireDocument);
+
+                var interview = SetupInterview(questionnaireDocument, precompiledState: interviewState);
+
+                interview.AnswerNumericIntegerQuestion(userId, q1Id, RosterVector.Empty, DateTime.Now, 10);
+                interview.AnswerMultipleOptionsQuestion(userId, q2Id, Create.RosterVector(1), DateTime.Now, new[] { 1m, 2m, 3m });
+
+                var result = new InvokeResults();
+
+                using (var eventContext = new EventContext())
+                {
+                    interview.AnswerNumericIntegerQuestion(userId, q1Id, RosterVector.Empty, DateTime.Now, 2);
+
+                    result.QuestionsQ3Enabled = eventContext.AnyEvent<QuestionsEnabled>(x => x.Questions.Any(q => q.Id == q3Id));
+                }
+
+                return result;
+            });
+
+        It should_disable_q3 = () =>
+            results.QuestionsQ3Enabled.ShouldBeTrue();
+
+        Cleanup stuff = () =>
+        {
+            appDomainContext.Dispose();
+            appDomainContext = null;
+        };
+
+        private static InvokeResults results;
+        private static AppDomainContext<AssemblyTargetLoader, PathBasedAssemblyResolver> appDomainContext;
+        private static readonly Guid questionnaireId = Guid.Parse("99999999999999999999999999999999");
+        private static readonly Guid rosterId = Guid.Parse("88888888888888888888888888888888");
+        private static readonly Guid roster2Id = Guid.Parse("77777777777777777777777777777777");
+        private static readonly Guid q1Id = Guid.Parse("11111111111111111111111111111111");
+        private static readonly Guid q2Id = Guid.Parse("22222222222222222222222222222222");
+        private static readonly Guid q3Id = Guid.Parse("33333333333333333333333333333333");
+        private static readonly Guid userId = Guid.Parse("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+
+        [Serializable]
+        internal class InvokeResults
+        {
+            public bool QuestionsQ3Enabled { get; set; }
+        }
+    }
+}
