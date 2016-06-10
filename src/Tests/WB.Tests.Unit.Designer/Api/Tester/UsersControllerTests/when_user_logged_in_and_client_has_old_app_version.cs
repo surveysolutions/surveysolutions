18f732196dcfa9faa3dad0c512438b732ace8e95
Roman Lytvyn <litvin.roma@gmail.com>
2016-06-10 13:53:05 +0300

    D: added tests for tester's controllers by api version. KP-7272

---
+++ src/Tests/WB.Tests.Unit.Designer/Api/Tester/UsersControllerTests/when_user_logged_in_and_client_has_old_app_version.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+using System.Web.Http;
+using Machine.Specifications;
+using WB.Core.SharedKernels.SurveySolutions.Api.Designer;
+using WB.UI.Designer.Api.Tester;
+
+namespace WB.Tests.Unit.Designer.Api.Tester.UsersControllerTests
+{
+    public class when_user_logged_in_and_client_has_old_app_version : UsersControllerTestsContext
+    {
+        Establish context = () =>
+        {
+            controller = CreateUserController();
+        };
+
+        Because of = () => expectedException =
+            Catch.Exception(() => controller.Login(version: ApiVersion.Tester - 1));
+
+        It should_response_code_be_UpgradeRequired = () =>
+            ((HttpResponseException)expectedException).Response.StatusCode.ShouldEqual(HttpStatusCode.UpgradeRequired);
+
+        private static Exception expectedException;
+        private static UserController controller;
+    }
+}
