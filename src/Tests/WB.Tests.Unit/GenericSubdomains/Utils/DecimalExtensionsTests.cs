9d076772e8ce2db47608fe4df660ca273233c571
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2016-06-13 15:11:08 -0400

    fix int formatting : KP-7327 Zero value is not displayed for numeric question if it uses "1000 separator"

--- src/Tests/WB.Tests.Unit/GenericSubdomains/Utils/DecimalExtensionsTests.cs
+++ src/Tests/WB.Tests.Unit/GenericSubdomains/Utils/DecimalExtensionsTests.cs
@@ -11,7 +11,27 @@ namespace WB.Tests.Unit.GenericSubdomains.Utils
         [TestCase(11111111.45, "11.111.111,45")]
         [TestCase(0.555, "0,555")]
         [TestCase(-0.555, "-0,555")]
+        [TestCase(0.0001, "0,0001")]
+        [TestCase(0, "0")]
+        [TestCase(0.0, "0")]
+        [TestCase(00.01, "0,01")]
         public void decimalFormatting(decimal value, string expectedResult)
+        {
+            var formattedDecimal = value.FormatDecimal();
+            Assert.That(formattedDecimal, Is.EqualTo(expectedResult));
+        }
+
+        [SetCulture("en-US")]
+        [TestCase(1, "1")]
+        [TestCase(11245, "11,245")]
+        [TestCase(11111111.45, "11,111,111.45")]
+        [TestCase(0.555, "0.555")]
+        [TestCase(-0.555, "-0.555")]
+        [TestCase(0.0001, "0.0001")]
+        [TestCase(0, "0")]
+        [TestCase(0.0, "0")]
+        [TestCase(00.01, "0.01")]
+        public void decimalFormattingEN(decimal value, string expectedResult)
         {
             var formattedDecimal = value.FormatDecimal();
             Assert.That(formattedDecimal, Is.EqualTo(expectedResult));
