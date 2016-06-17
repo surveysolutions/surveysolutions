de27c9a69128b06e382e2829bd59aad1d7ed3677
Slava Scherbak <slava.scherbak@gmail.com>
2016-06-17 11:00:32 -0400

    Code generation: added more tests for linked questions. KP-7266

--- src/Tests/WB.Tests.Integration/InterviewTests/LinkedQuestions/when_answering_question_that_change_options_list_of_linked_that_is_outside_roster_with_source_question.cs
+++ src/Tests/WB.Tests.Integration/InterviewTests/LinkedQuestions/when_answering_question_that_change_options_list_of_linked_that_is_outside_roster_with_source_question.cs
@@ -12,7 +12,7 @@ using WB.Core.SharedKernels.DataCollection.Implementation.Aggregates;
 
 namespace WB.Tests.Integration.InterviewTests.LinkedQuestions
 {
-    internal class when_answering_question_ : InterviewTestsContext
+    internal class when_answering_question_that_change_osptions_list_of_linked_that_is_outside_roster_with_source_question : InterviewTestsContext
     {
         Establish context = () =>
         {
@@ -34,9 +34,9 @@ namespace WB.Tests.Integration.InterviewTests.LinkedQuestions
                     Create.MultyOptionsQuestion(q2Id, variable: "q2", options: options),
                     Create.Roster(rosterId, variable:"r1", rosterSizeQuestionId: q2Id, rosterSizeSourceType: RosterSizeSourceType.Question, children: new IComposite[]
                     {
-                        Create.NumericIntegerQuestion(q3Id, variable: "age"),
-                        Create.SingleQuestion(q4Id, variable: "q4", linkedToQuestionId: q3Id, linkedFilter: "age > current.age")
-                    })
+                        Create.NumericIntegerQuestion(q3Id, variable: "age")
+                    }),
+                    Create.SingleQuestion(q4Id, variable: "q4", linkedToQuestionId: q3Id, linkedFilter: "age > 15")
                 });
 
                 ILatestInterviewExpressionState interviewState = GetInterviewExpressionState(questionnaireDocument);
@@ -55,26 +55,16 @@ namespace WB.Tests.Integration.InterviewTests.LinkedQuestions
 
                     var optionsChangedEvent = eventContext.GetSingleEvent<LinkedOptionsChanged>();
 
-                    var optionsForLinked1 = optionsChangedEvent.ChangedLinkedQuestions.SingleOrDefault(x => x.QuestionId.Equals(Create.Identity(q4Id, Create.RosterVector(1))));
-                    var optionsForLinked2 = optionsChangedEvent.ChangedLinkedQuestions.SingleOrDefault(x => x.QuestionId.Equals(Create.Identity(q4Id, Create.RosterVector(2))));
-                    var optionsForLinked3 = optionsChangedEvent.ChangedLinkedQuestions.SingleOrDefault(x => x.QuestionId.Equals(Create.Identity(q4Id, Create.RosterVector(3))));
+                    var changedLinkedOptions = optionsChangedEvent.ChangedLinkedQuestions.SingleOrDefault(x => x.QuestionId.Equals(Create.Identity(q4Id, RosterVector.Empty)));
 
-                    result.OptionsCountForQuestion4InRoster1 = optionsForLinked1?.Options.Length ?? 0;
-                    result.OptionsCountForQuestion4InRoster2 = optionsForLinked2?.Options.Length ?? 0;
-                    result.OptionsCountForQuestion4InRoster3 = optionsForLinked3?.Options.Length ?? 0;
+                    result.OptionsCountForLinkedQuestion = changedLinkedOptions?.Options.Length ?? 0;
                 }
 
                 return result;
             });
 
-        It should_return_1_option_for_linked_question_in_1_roster = () =>
-            results.OptionsCountForQuestion4InRoster1.ShouldEqual(1);
-
-        It should_return_2_options_for_linked_question_in_2_roster = () =>
-            results.OptionsCountForQuestion4InRoster2.ShouldEqual(2);
-
-        It should_not_return_options_for_linked_question_in_3_roster = () =>
-            results.OptionsCountForQuestion4InRoster3.ShouldEqual(0);
+        It should_return_2_options_for_linked_question = () =>
+            results.OptionsCountForLinkedQuestion.ShouldEqual(2);
 
         Cleanup stuff = () =>
         {
@@ -86,19 +76,15 @@ namespace WB.Tests.Integration.InterviewTests.LinkedQuestions
         private static AppDomainContext<AssemblyTargetLoader, PathBasedAssemblyResolver> appDomainContext;
         private static readonly Guid questionnaireId = Guid.Parse("99999999999999999999999999999999");
         private static readonly Guid rosterId = Guid.Parse("88888888888888888888888888888888");
-        private static readonly Guid q1Id = Guid.Parse("11111111111111111111111111111111");
         private static readonly Guid q2Id = Guid.Parse("22222222222222222222222222222222");
         private static readonly Guid q3Id = Guid.Parse("33333333333333333333333333333333");
         private static readonly Guid q4Id = Guid.Parse("44444444444444444444444444444444");
-        private static readonly Guid q5Id = Guid.Parse("55555555555555555555555555555555");
         private static readonly Guid userId = Guid.Parse("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
 
         [Serializable]
         internal class InvokeResults
         {
-            public int OptionsCountForQuestion4InRoster1 { get; set; }
-            public int OptionsCountForQuestion4InRoster2 { get; set; }
-            public int OptionsCountForQuestion4InRoster3 { get; set; }
+            public int OptionsCountForLinkedQuestion { get; set; }
         }
     }
 }
