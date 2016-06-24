0811d016275ef3ecc207095c3a38a1bdd90bf015
Roman Lytvyn <litvin.roma@gmail.com>
2016-06-24 18:12:30 +0300

    D/HQ: Added description about unsupported questionnaire features when import questionnaire from D to HQ. Fixed unit tests KP-7355

--- src/Tests/WB.Tests.Unit/Applications/Headquarters/DesignerQuestionnairesApiControllerTests/when_importing_questionnaire_from_designer_and_command_service_throws_not_a_questionnaire_exception.cs
+++ src/Tests/WB.Tests.Unit/Applications/Headquarters/DesignerQuestionnairesApiControllerTests/when_importing_questionnaire_from_designer_and_command_service_throws_not_a_questionnaire_exception.cs
@@ -41,7 +41,7 @@ namespace WB.Tests.Unit.Applications.Headquarters.DesignerQuestionnairesApiContr
                 .Returns(new QuestionnaireDocument());
 
             var restServiceMock = new Mock<IRestService>();
-            restServiceMock.Setup(x => x.PostAsync<QuestionnaireCommunicationPackage>(Moq.It.IsAny<string>(), Moq.It.IsAny<Action<DownloadProgressChangedEventArgs>>(), Moq.It.IsAny<object>(), Moq.It.IsAny<RestCredentials>(), Moq.It.IsAny<CancellationToken?>()))
+            restServiceMock.Setup(x => x.GetAsync<QuestionnaireCommunicationPackage>(Moq.It.IsAny<string>(), Moq.It.IsAny<Action<DownloadProgressChangedEventArgs>>(), Moq.It.IsAny<object>(), Moq.It.IsAny<RestCredentials>(), Moq.It.IsAny<CancellationToken?>()))
                 .Returns(Task.FromResult(new QuestionnaireCommunicationPackage()));
 
             controller = CreateDesignerQuestionnairesApiController(commandService: commandService,
