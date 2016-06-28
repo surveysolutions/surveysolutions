e8f4039fc7f79a778cce22d095cf708134c91b1c
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2016-06-28 16:26:48 -0400

    fix linked questions : KP-7391 Compatibility: options in Linked question don't update after rejecting

--- src/Core/SharedKernels/DataCollection/DataCollection.Portable/LinkedQuestionOptionsChanges.cs
+++ src/Core/SharedKernels/DataCollection/DataCollection.Portable/LinkedQuestionOptionsChanges.cs
@@ -6,12 +6,13 @@ namespace WB.Core.SharedKernels.DataCollection
     public class LinkedQuestionOptionsChanges
     {
         internal LinkedQuestionOptionsChanges() : this(new Dictionary<Guid, RosterVector[]>()) { }
+
         public LinkedQuestionOptionsChanges(Dictionary<Guid, RosterVector[]> linkedQuestionOptions)
         {
             this.LinkedQuestionOptions = linkedQuestionOptions;
         }
 
-        [Obsolete]
+        [Obsolete ("v 5.10, release 01 jul 16")]
         public Dictionary<Guid, RosterVector[]> LinkedQuestionOptions { get; private set; }
 
         public Dictionary<Identity, RosterVector[]> LinkedQuestionOptionsSet { get; private set; } = new Dictionary<Identity, RosterVector[]>();
