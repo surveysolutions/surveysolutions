701e3feb1fc8e0cde98191f03bdb1cb358b7bb81
Andrii Kozhyn <hedgic@gmail.com>
2016-06-29 14:07:00 +0300

    KP-7387: Skip failed interviews during synchronization

--- src/Core/BoundedContexts/Interviewer/WB.Core.BoundedContexts.Interviewer/Views/Dashboard/DashboardViewModel.cs
+++ src/Core/BoundedContexts/Interviewer/WB.Core.BoundedContexts.Interviewer/Views/Dashboard/DashboardViewModel.cs
@@ -72,6 +72,10 @@ namespace WB.Core.BoundedContexts.Interviewer.Views.Dashboard
         public int NewInterviewsCount => this.dashboardInformation.NewInterviews.Count();
         public int StartedInterviewsCount => this.dashboardInformation.StartedInterviews.Count();
         public int CompletedInterviewsCount => this.dashboardInformation.CompletedInterviews.Count();
+
+        public int FailedInterviewsCount => this.synchronization.Statistics.FailedToCreateInterviewsCount +
+                                            this.synchronization.Statistics.FailedToUploadInterviwesCount;
+
         public int RejectedInterviewsCount => this.dashboardInformation.RejectedInterviews.Count();
 
         public bool IsExistsAnyCensusQuestionniories => this.dashboardInformation.CensusQuestionnaires.Any();
