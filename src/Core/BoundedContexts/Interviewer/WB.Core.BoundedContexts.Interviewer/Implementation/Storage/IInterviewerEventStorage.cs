6ab4801a77ac93f13ab5557ba282ef086ae9aa3a
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2016-06-30 22:21:45 -0400

    separate db per interview : KP-7399 Store each inteview to separate SQLite DB

--- src/Core/BoundedContexts/Interviewer/WB.Core.BoundedContexts.Interviewer/Implementation/Storage/IInterviewerEventStorage.cs
+++ src/Core/BoundedContexts/Interviewer/WB.Core.BoundedContexts.Interviewer/Implementation/Storage/IInterviewerEventStorage.cs
@@ -1,8 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
-using Ncqrs.Eventing;
 using Ncqrs.Eventing.Storage;
 
 namespace WB.Core.BoundedContexts.Interviewer.Implementation.Storage
@@ -10,5 +6,6 @@ namespace WB.Core.BoundedContexts.Interviewer.Implementation.Storage
     public interface IInterviewerEventStorage : IEventStore
     {
         void RemoveEventSourceById(Guid interviewId);
+        int GetMaxEventSequenceById(Guid interviewId);
     }
 }
