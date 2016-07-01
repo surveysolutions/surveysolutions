0e97e9ae92b36fa64dfb8f2af1b2acec52381018
Roman Lytvyn <litvin.roma@gmail.com>
2016-07-01 15:30:06 +0300

    HQ: Added support of ReturnUrl KP-6046

--- src/UI/Headquarters/WB.UI.Headquarters/App_Start/RouteConfig.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/App_Start/RouteConfig.cs
@@ -7,36 +7,10 @@ namespace WB.UI.Headquarters
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.IgnoreRoute(@"{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                "InterviewRoute",
-                "Interview/{action}/{id}",
-                new { controller = "Interview", id = UrlParameter.Optional });
-
-            routes.MapRoute(
-                "BackupRoute",
-                "Backup/{action}/{id}",
-                new { controller = "Backup", action = "Index", id = UrlParameter.Optional });
-
-            routes.MapRoute(
-                "ImportExportRoute",
-                "ImportExport/{action}/{id}",
-                new { controller = "ImportExport", action = "Index", id = UrlParameter.Optional });
-
-            routes.MapRoute(
-                "SurveyRoute",
-                "Survey/{action}/{id}",
-                new { controller = "Survey", action = "Index", id = UrlParameter.Optional });
-
-            routes.MapRoute(
-                "TabletReportRoute",
-                "TabletReport/{action}/{id}",
-                new { controller = "TabletReport", action = "Index", id = UrlParameter.Optional });
-
-            routes.MapRoute("Default", "{controller}/{action}/{id}",
-                new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-                );
+            routes.MapRoute(@"Default", @"{controller}/{action}/{id}",
+                new { controller = @"Account", action = @"Index", id = UrlParameter.Optional });
         }
     }
 }
