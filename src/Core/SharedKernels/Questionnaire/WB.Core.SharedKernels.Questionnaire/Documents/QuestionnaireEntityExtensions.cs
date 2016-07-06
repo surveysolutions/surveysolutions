415b372561bd13f9fb1c1534534b4a6e156084b8
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2016-07-06 18:13:12 +0300

    QuestionnaireTranslator: minor refactoring KP-7419

--- src/Core/SharedKernels/Questionnaire/WB.Core.SharedKernels.Questionnaire/Documents/QuestionnaireEntityExtensions.cs
+++ src/Core/SharedKernels/Questionnaire/WB.Core.SharedKernels.Questionnaire/Documents/QuestionnaireEntityExtensions.cs
@@ -1,16 +1,15 @@
-﻿using Main.Core.Entities.Composite;
-using Main.Core.Entities.SubEntities;
+﻿using Main.Core.Entities.SubEntities;
 
 namespace WB.Core.SharedKernels.Questionnaire.Documents
 {
-    public static class ICompositeExtensions
+    public static class QuestionnaireEntityExtensions
     {
-        public static string GetTitle(this IComposite entity)
+        public static string GetTitle(this IQuestionnaireEntity entity)
             => (entity as IQuestion)?.QuestionText
             ?? (entity as IStaticText)?.Text
             ?? (entity as IGroup)?.Title;
 
-        public static void SetTitle(this IComposite entity, string title)
+        public static void SetTitle(this IQuestionnaireEntity entity, string title)
         {
             var question = entity as IQuestion;
             var staticText = entity as IStaticText;
