b4507c02ea5ee86ee3159e057fc41e5960af231b
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2016-07-06 18:12:35 +0300

    QuestionnaireTranslator: implemented for validation messages KP-7419

--- src/Core/SharedKernels/Questionnaire/WB.Core.SharedKernels.Questionnaire/Documents/IValidatable.cs
+++ src/Core/SharedKernels/Questionnaire/WB.Core.SharedKernels.Questionnaire/Documents/IValidatable.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections.Generic;
+using Main.Core.Entities.Composite;
+using WB.Core.SharedKernels.Questionnaire.Documents;
 using WB.Core.SharedKernels.QuestionnaireEntities;
 
 namespace Main.Core.Entities.SubEntities
 {
-    public interface IValidatable
+    public interface IValidatable : IQuestionnaireEntity
     {
         IList<ValidationCondition> ValidationConditions { get; set; }
     }
--- src/Core/SharedKernels/Questionnaire/WB.Core.SharedKernels.Questionnaire/Translator/QuestionnaireTranslator.cs
+++ src/Core/SharedKernels/Questionnaire/WB.Core.SharedKernels.Questionnaire/Translator/QuestionnaireTranslator.cs
@@ -7,6 +7,7 @@ using Main.Core.Documents;
 using Main.Core.Entities.Composite;
 using Main.Core.Entities.SubEntities;
 using WB.Core.SharedKernels.Questionnaire.Documents;
+using WB.Core.SharedKernels.QuestionnaireEntities;
 
 namespace WB.Core.SharedKernels.Questionnaire.Translator
 {
@@ -38,8 +39,15 @@ namespace WB.Core.SharedKernels.Questionnaire.Translator
                 }
             }
 
-            // options
-            // error messages
+            foreach (var validatableEntity in questionnaireDocument.Find<IValidatable>())
+            {
+                for (int index = 0; index < validatableEntity.ValidationConditions.Count; index++)
+                {
+                    var validationCondition = validatableEntity.ValidationConditions[index];
+                    TranslateValidationMessage(validatableEntity.PublicKey, index + 1, validationCondition, translation);
+                }
+            }
+
             // roster fixed titles
         }
 
@@ -62,6 +70,14 @@ namespace WB.Core.SharedKernels.Questionnaire.Translator
             answerOption.AnswerText = Translate(
                 original: answerOption.AnswerText,
                 translated: translation.GetAnswerOption(questionId, answerOption.AnswerValue));
+        }
+
+        private static void TranslateValidationMessage(Guid validatableEntityId, int validationOneBasedIndex,
+            ValidationCondition validation, IQuestionnaireTranslation translation)
+        {
+            validation.Message = Translate(
+                original: validation.Message,
+                translated: translation.GetValidationMessage(validatableEntityId, validationOneBasedIndex));
         }
 
         private static string Translate(string original, string translated)
