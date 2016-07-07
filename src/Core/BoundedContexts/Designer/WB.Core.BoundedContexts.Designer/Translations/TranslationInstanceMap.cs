217114e578ca45734e03538e24f03a1fd3f86f03
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2016-07-07 11:29:36 +0300

    KP-7433: Added table for storing of translations

---
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Translations/TranslationInstanceMap.cs
@@ -0,0 +1,23 @@
+﻿using NHibernate.Mapping.ByCode.Conformist;
+using WB.Core.Infrastructure.PlainStorage;
+
+namespace WB.Core.BoundedContexts.Designer.Translations
+{
+    [PlainStorage]
+    public class TranslationInstanceMap : ClassMapping<TranslationInstance>
+    {
+        public TranslationInstanceMap()
+        {
+            ComposedId(map =>
+            {
+                map.Property(x => x.QuestionnaireId, ptp => ptp.NotNullable(true));
+                map.Property(x => x.Type, ptp => ptp.NotNullable(true));
+                map.Property(x => x.QuestionnaireEntityId, ptp => ptp.NotNullable(true));
+                map.Property(x => x.TranslationIndex);
+                map.Property(x => x.Culture, ptp => ptp.NotNullable(true));
+            });
+
+            Property(x => x.Translation, ptp => ptp.NotNullable(true));
+        }
+    }
+}
---
+++ src/UI/Designer/WB.UI.Designer/Migrations/PlainStore/M001-100/M003_AddTranslationsTable.cs
@@ -0,0 +1,30 @@
+﻿using FluentMigrator;
+
+namespace WB.UI.Designer.Migrations.PlainStore
+{
+    [Migration(3)]
+    public class M003_AddTranslationsTable : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("translationinstances")
+                .WithColumn("questionnaireid").AsGuid().NotNullable().PrimaryKey()
+                .WithColumn("type").AsInt32().NotNullable().PrimaryKey()
+                .WithColumn("questionnaireentityid").AsGuid().NotNullable().PrimaryKey()
+                .WithColumn("translationindex").AsInt32().Nullable().PrimaryKey()
+                .WithColumn("culture").AsString().NotNullable().PrimaryKey()
+                .WithColumn("translation").AsString().NotNullable();
+
+            Create.Index("translationinstances_questionnaire")
+                .OnTable("translationinstances")
+                .OnColumn("questionnaireid").Ascending()
+                .OnColumn("culture").Ascending()
+                .OnColumn("questionnaireentityid").Ascending();
+        }
+
+        public override void Down()
+        {
+            Delete.Table("translationinstances");
+        }
+    }
+}
