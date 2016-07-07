b724bda4535d6bfbc6cdaddab75556610eef30a4
Andrii Kozhyn <hedgic@gmail.com>
2016-07-07 11:29:36 +0300

    KP-7433: Added table for storing of translations

---
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Translations/TranslationInstance.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace WB.Core.BoundedContexts.Designer.Translations
+{
+    public class TranslationInstance
+    {
+        public virtual Guid QuestionnaireId { get; set; }
+
+        public virtual TranslationType Type { get; set; }
+
+        public virtual Guid QuestionnaireEntityId { get; set; }
+
+        public virtual int? TranslationIndex { get; set; }
+
+        public virtual string Culture { get; set; }
+
+        public virtual string Translation { get; set; }
+
+        #region equals
+
+        protected bool Equals(TranslationInstance other)
+        {
+            return this.QuestionnaireId.Equals(other.QuestionnaireId) && this.Type == other.Type &&
+                   this.QuestionnaireEntityId.Equals(other.QuestionnaireEntityId) &&
+                   this.TranslationIndex == other.TranslationIndex && string.Equals(this.Culture, other.Culture);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((TranslationInstance) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = this.QuestionnaireId.GetHashCode();
+                hashCode = (hashCode*397) ^ (int) this.Type;
+                hashCode = (hashCode*397) ^ this.QuestionnaireEntityId.GetHashCode();
+                hashCode = (hashCode*397) ^ this.TranslationIndex.GetHashCode();
+                hashCode = (hashCode*397) ^ (this.Culture != null ? this.Culture.GetHashCode() : 0);
+                return hashCode;
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Translations/TranslationType.cs
@@ -0,0 +1,11 @@
+﻿namespace WB.Core.BoundedContexts.Designer.Translations
+{
+    public enum TranslationType
+    {
+        Unknown = 0,
+        Title = 1,
+        ValidationMessage = 2,
+        Instruction = 3,
+        OptionTitle = 4
+    }
+}
