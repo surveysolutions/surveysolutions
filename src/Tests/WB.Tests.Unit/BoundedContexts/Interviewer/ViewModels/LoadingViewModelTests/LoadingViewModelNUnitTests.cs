d2d39dd91c759b3b94dd37b9c0295597fbd8bfaa
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2016-07-08 16:58:09 +0300

    KP-7442: Loading screen should show error and navigate to dashboard if loading failed

--- src/Tests/WB.Tests.Unit/BoundedContexts/Interviewer/ViewModels/LoadingViewModelTests/LoadingViewModelNUnitTests.cs
+++ src/Tests/WB.Tests.Unit/BoundedContexts/Interviewer/ViewModels/LoadingViewModelTests/LoadingViewModelNUnitTests.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
+using Moq;
 using Ncqrs.Eventing.Storage;
 using NSubstitute;
 using NUnit.Framework;
 using WB.Core.BoundedContexts.Interviewer.Views;
+using WB.Core.GenericSubdomains.Portable.Services;
 using WB.Core.Infrastructure.CommandBus;
 using WB.Core.SharedKernels.DataCollection.Commands.Interview;
 using WB.Core.SharedKernels.DataCollection.Repositories;
@@ -88,7 +90,9 @@ namespace WB.Tests.Unit.BoundedContexts.Interviewer.ViewModels.LoadingViewModelT
                 viewModelNavigationService ?? Substitute.For<IViewModelNavigationService>(), 
                 interviewRepository ?? Substitute.For<IStatefulInterviewRepository>(),
                 commandService ?? Substitute.For<ICommandService>(),
-                questionnaireRepository:Substitute.For<IPlainQuestionnaireRepository>());
+                questionnaireRepository: Substitute.For<IPlainQuestionnaireRepository>(),
+                logger: Mock.Of<ILogger>(),
+                interactionService: Mock.Of<IUserInteractionService>());
         }
     }
 }
