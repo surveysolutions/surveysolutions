1635b32b9293e18c5a09485a47362bb320474286
Pavel Stovpivskyi <sby@rambler.ru>
2016-07-11 19:13:09 +0300

    added test for translation

--- src/Tests/WB.Tests.Unit.Designer/BoundedContexts/Designer/QuestionnaireTranslationTests/QuestionnaireTranslationTestsContext.cs
+++ src/Tests/WB.Tests.Unit.Designer/BoundedContexts/Designer/QuestionnaireTranslationTests/QuestionnaireTranslationTestsContext.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using WB.Core.BoundedContexts.Designer.Translations;
 using WB.Core.SharedKernels.Questionnaire.Translations;
+using TranslationInstance = WB.Core.BoundedContexts.Designer.Translations.TranslationInstance;
 
 namespace WB.Tests.Unit.Designer.BoundedContexts.Designer.QuestionnaireTranslationTests
 {
