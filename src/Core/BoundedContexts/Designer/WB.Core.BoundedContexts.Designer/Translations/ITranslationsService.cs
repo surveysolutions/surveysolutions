27a110d01ece37928e4ee592765a033ad8166b32
Andrii Kozhyn <hedgic@gmail.com>
2016-07-14 14:01:37 +0300

    KP-7424: culture to language

--- src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Translations/ITranslationsService.cs
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Translations/ITranslationsService.cs
@@ -5,10 +5,10 @@ namespace WB.Core.BoundedContexts.Designer.Translations
 {
     public interface ITranslationsService
     {
-        ITranslation Get(Guid questionnaireId, string culture);
-        TranslationFile GetAsExcelFile(Guid questionnaireId, Guid? culture);
-        void Store(Guid questionnaireId, string culture, byte[] excelRepresentation);
+        ITranslation Get(Guid questionnaireId, string language);
+        TranslationFile GetAsExcelFile(Guid questionnaireId, Guid? languageId);
+        void Store(Guid questionnaireId, string language, byte[] excelRepresentation);
         void CloneTranslation(Guid questionnaireId, string language, Guid newQuestionnaireId, string newCulture);
-        void Delete(Guid questionnaireId, string culture);
+        void Delete(Guid questionnaireId, string language);
     }
 }
