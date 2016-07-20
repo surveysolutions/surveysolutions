0483ad6100cb10516827a8002bf0b584d84a67db
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2016-07-20 16:49:25 +0300

    Interview: cowered SwitchTranslation with tests KP-7427

---
+++ src/Tests/WB.Tests.Unit/SharedKernels/DataCollection/InterviewTests/when_switching_translation_language_to_existing_language.cs
@@ -0,0 +1,31 @@
+using System;
+using Machine.Specifications;
+using Ncqrs.Spec;
+using WB.Core.SharedKernels.DataCollection.Commands.Interview;
+using WB.Core.SharedKernels.DataCollection.Events.Interview;
+using WB.Core.SharedKernels.DataCollection.Implementation.Aggregates;
+
+namespace WB.Tests.Unit.SharedKernels.DataCollection.InterviewTests
+{
+    internal class when_switching_translation_language_to_existing_language : InterviewTestsContext
+    {
+        Establish context = () =>
+        {
+            var questionnaireRepository = Setup.QuestionnaireRepositoryWithOneQuestionnaire(Guid.NewGuid(), questionnaire
+                => questionnaire.GetTranslationLanguages() == new [] { "Русский" });
+
+            interview = CreateInterview(questionnaireRepository: questionnaireRepository);
+
+            eventContext = new EventContext();
+        };
+
+        Because of = () =>
+            interview.SwitchTranslation(new SwitchTranslation(Guid.NewGuid(), "Русский", Guid.NewGuid()));
+
+        It should_publish_TranslationSwitched_event_with_target_language = () =>
+            eventContext.ShouldContainEvent<TranslationSwitched>(@event => @event.Language == "Русский");
+
+        private static Interview interview;
+        private static EventContext eventContext;
+    }
+}
