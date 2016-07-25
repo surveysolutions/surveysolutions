f397cd7efec8f8e264d2af4f3d175ae6d179a512
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2016-07-25 18:56:16 +0300

    Tests: minor compilation errors KP-7479

--- src/Tests/WB.Tests.Unit/BoundedContexts/Tester/ViewModels/PrefilledQuestionsViewModelTests/PrefilledQuestionsViewModelTestContext.cs
+++ src/Tests/WB.Tests.Unit/BoundedContexts/Tester/ViewModels/PrefilledQuestionsViewModelTests/PrefilledQuestionsViewModelTestContext.cs
@@ -1,6 +1,8 @@
 ﻿using Moq;
 using MvvmCross.Test.Core;
+using WB.Core.BoundedContexts.Tester.ViewModels;
 using WB.Core.GenericSubdomains.Portable.Services;
+using WB.Core.Infrastructure.CommandBus;
 using WB.Core.SharedKernels.DataCollection.Repositories;
 using WB.Core.SharedKernels.Enumerator.Repositories;
 using WB.Core.SharedKernels.Enumerator.Services;
@@ -29,7 +31,8 @@ namespace WB.Tests.Unit.BoundedContexts.Tester.ViewModels.PrefilledQuestionsView
                 interviewRepository ?? Mock.Of<IStatefulInterviewRepository>(),
                 viewModelNavigationService ?? Mock.Of<IViewModelNavigationService>(),
                 logger ?? Mock.Of<ILogger>(),
-                principal ?? Mock.Of<IPrincipal>());
+                principal ?? Mock.Of<IPrincipal>(),
+                Mock.Of<ICommandService>());
         }
     }
 }
