c956f3ffca0825690439882048c4521332e09b84
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2016-07-25 15:43:04 +0300

    KP-7500: Cloning of questionnaire should not break initial

--- src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Translations/TranslationInstance.cs
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Translations/TranslationInstance.cs
@@ -9,5 +9,18 @@ namespace WB.Core.BoundedContexts.Designer.Translations
         public virtual Guid QuestionnaireId { get; set; }
 
         public virtual Guid Id { get; set; }
+
+        public virtual TranslationInstance Clone()
+        {
+            return new TranslationInstance
+            {
+                QuestionnaireId = this.QuestionnaireId,
+                QuestionnaireEntityId = this.QuestionnaireEntityId,
+                TranslationId = this.TranslationId,
+                TranslationIndex = this.TranslationIndex,
+                Type = this.Type,
+                Value = this.Value
+            };
+        }
     }
 }
