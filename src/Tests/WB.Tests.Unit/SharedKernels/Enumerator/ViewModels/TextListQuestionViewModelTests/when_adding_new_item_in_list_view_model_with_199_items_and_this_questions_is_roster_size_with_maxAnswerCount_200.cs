320b095c13cf9fc86457d18c3ac1af84d0029bc5
Slava Scherbak <slava.scherbak@gmail.com>
2016-08-09 12:02:05 -0400

    Interviewer: added tests for view model that validates user input for questions that are source of roster size for long rosters.  KP-7558

--- src/Tests/WB.Tests.Unit/SharedKernels/Enumerator/ViewModels/TextListQuestionViewModelTests/when_adding_new_item_in_list_view_model_with_199_items_and_this_questions_is_roster_size_with_maxAnswerCount_200.cs
+++ src/Tests/WB.Tests.Unit/SharedKernels/Enumerator/ViewModels/TextListQuestionViewModelTests/when_adding_new_item_in_list_view_model_with_199_items_and_this_questions_is_roster_size_with_maxAnswerCount_200.cs
@@ -1,8 +1,6 @@
 using System;
-using System.Linq;
 using Machine.Specifications;
 using Moq;
-using WB.Core.SharedKernels.DataCollection.Commands.Interview;
 using WB.Core.SharedKernels.DataCollection.Events.Interview;
 using WB.Core.SharedKernels.Enumerator.Aggregates;
 using WB.Core.SharedKernels.Enumerator.Entities.Interview;
@@ -16,7 +14,7 @@ using It = Machine.Specifications.It;
 
 namespace WB.Tests.Unit.SharedKernels.Enumerator.ViewModels.TextListQuestionViewModelTests
 {
-    internal class when_adding_new_item_in_list_view_model_and_there_is_answer_in_interview : TextListQuestionViewModelTestContext
+    internal class when_adding_new_item_in_list_view_model_with_199_items_and_this_questions_is_roster_size_with_maxAnswerCount_200 : TextListQuestionViewModelTestContext
     {
         Establish context = () =>
         {
@@ -26,9 +24,14 @@ namespace WB.Tests.Unit.SharedKernels.Enumerator.ViewModels.TextListQuestionView
                 => _.QuestionnaireId == questionnaireId
                    && _.GetTextListAnswer(questionIdentity) == textListAnswer);
 
+            for (int i = 1; i <= 199; i++)
+            {
+                savedAnswers[i - 1] = new Tuple<decimal, string>(i, $"Answer {i}");
+            }
+
             var interviewRepository = Mock.Of<IStatefulInterviewRepository>(_ => _.Get(interviewId) == interview);
 
-            var questionnaireRepository = SetupQuestionnaireRepositoryWithListQuestion();
+            var questionnaireRepository = SetupQuestionnaireRepositoryWithListQuestion(isRosterSizeQuestion: true, maxAnswerCount: 200);
 
             var userIdentity = Mock.Of<IUserIdentity>(_ => _.UserId == userId);
             var principal = Mock.Of<IPrincipal>(_ => _.CurrentUserIdentity == userIdentity);
@@ -49,45 +52,27 @@ namespace WB.Tests.Unit.SharedKernels.Enumerator.ViewModels.TextListQuestionView
             listModel.ValueChangeCommand.Execute();
         };
 
-        It should_create_list_with_5_answers = () =>
-            listModel.Answers.Count.ShouldEqual(5);
-
-        It should_add_item_with_Title_equals_trimmed_newListItemTitle = () =>
-            listModel.Answers.Last().Title.ShouldEqual(newListItemTitle.Trim());
-
-        It should_add_new_item_with_Value_equals_9 = () =>
-            listModel.Answers.Last().Value.ShouldEqual(9m);
+        It should_create_list_with_200_answers = () =>
+            listModel.Answers.Count.ShouldEqual(200);
 
         It should_set_IsAddNewItemVisible_flag_in_false = () =>
             listModel.IsAddNewItemVisible.ShouldBeFalse();
 
-        It should_clear_NewListItem_field = () =>
-            listModel.NewListItem.ShouldBeEmpty();
-
-        It should_send_answer_command = () =>
-            AnsweringViewModelMock.Verify(x => x.SendAnswerQuestionCommandAsync(Moq.It.IsAny<AnswerTextListQuestionCommand>()), Times.Once);
-
         private static TextListQuestionViewModel listModel;
         private static readonly NavigationState navigationState = Create.Other.NavigationState();
 
-        private static readonly Mock<QuestionStateViewModel<TextListQuestionAnswered>> QuestionStateMock = 
-            new Mock<QuestionStateViewModel<TextListQuestionAnswered>>{ DefaultValue = DefaultValue.Mock };
+        private static readonly Mock<QuestionStateViewModel<TextListQuestionAnswered>> QuestionStateMock =
+            new Mock<QuestionStateViewModel<TextListQuestionAnswered>> { DefaultValue = DefaultValue.Mock };
 
-        private static readonly Mock<AnsweringViewModel> AnsweringViewModelMock = 
-            new Mock<AnsweringViewModel>() { DefaultValue = DefaultValue.Mock };
+        private static readonly Mock<AnsweringViewModel> AnsweringViewModelMock =
+            new Mock<AnsweringViewModel> { DefaultValue = DefaultValue.Mock };
 
         private static readonly string interviewId = "44444444444444444444444444444444";
 
         private static readonly string questionnaireId = "Questionnaire Id";
         private static readonly Guid userId = Guid.Parse("ffffffffffffffffffffffffffffffff");
 
-        private static readonly Tuple<decimal, string>[] savedAnswers = new[]
-        {
-            new Tuple<decimal, string>(1m, "Answer 1"),
-            new Tuple<decimal, string>(3m, "Answer 3"),
-            new Tuple<decimal, string>(4m, "Answer 5"),
-            new Tuple<decimal, string>(8m, "Answer 8")
-        };
+        private static readonly Tuple<decimal, string>[] savedAnswers = new Tuple<decimal, string>[199];
 
         private static readonly string newListItemTitle = "   Hello World!      ";
     }
