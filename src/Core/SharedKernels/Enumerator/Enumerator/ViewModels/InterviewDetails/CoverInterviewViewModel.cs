c58ecc4b3aa120670fd5890adb65e5b76d0eb998
Slava Scherbak <slava.scherbak@gmail.com>
2016-08-11 18:06:29 -0400

    Cover: added questionnaire title and start button

--- src/Core/SharedKernels/Enumerator/Enumerator/ViewModels/InterviewDetails/CoverInterviewViewModel.cs
+++ src/Core/SharedKernels/Enumerator/Enumerator/ViewModels/InterviewDetails/CoverInterviewViewModel.cs
@@ -1,8 +1,17 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Main.Core.Entities.SubEntities;
 using MvvmCross.Core.ViewModels;
 using MvvmCross.Plugins.Messenger;
 using WB.Core.Infrastructure.CommandBus;
+using WB.Core.SharedKernels.DataCollection;
+using WB.Core.SharedKernels.DataCollection.Aggregates;
+using WB.Core.SharedKernels.DataCollection.Repositories;
+using WB.Core.SharedKernels.Enumerator.Aggregates;
 using WB.Core.SharedKernels.Enumerator.Properties;
+using WB.Core.SharedKernels.Enumerator.Repositories;
 using WB.Core.SharedKernels.Enumerator.Services;
 using WB.Core.SharedKernels.Enumerator.Services.Infrastructure;
 using WB.Core.SharedKernels.Enumerator.ViewModels.InterviewDetails.Groups;
@@ -11,10 +20,14 @@ namespace WB.Core.SharedKernels.Enumerator.ViewModels.InterviewDetails
 {
     public class CoverInterviewViewModel : MvxViewModel
     {
+        
         private readonly IViewModelNavigationService viewModelNavigationService;
         private readonly IMvxMessenger messenger;
         private readonly ICommandService commandService;
         private readonly IEntityWithErrorsViewModelFactory entityWithErrorsViewModelFactory;
+        private readonly IQuestionnaireStorage questionnaireRepository;
+        private readonly IStatefulInterviewRepository interviewRepository;
+        private readonly IAnswerToStringService answerToStringService;
         protected readonly IPrincipal principal;
 
         public InterviewStateViewModel InterviewState { get; set; }
@@ -27,7 +40,10 @@ namespace WB.Core.SharedKernels.Enumerator.ViewModels.InterviewDetails
             IMvxMessenger messenger,
             IEntityWithErrorsViewModelFactory entityWithErrorsViewModelFactory,
             InterviewStateViewModel interviewState,
-            DynamicTextViewModel dynamicTextViewModel)
+            DynamicTextViewModel dynamicTextViewModel, 
+            IQuestionnaireStorage questionnaireRepository, 
+            IStatefulInterviewRepository interviewRepository, 
+            IAnswerToStringService answerToStringService)
         {
             this.viewModelNavigationService = viewModelNavigationService;
             this.commandService = commandService;
@@ -37,16 +53,64 @@ namespace WB.Core.SharedKernels.Enumerator.ViewModels.InterviewDetails
 
             this.InterviewState = interviewState;
             this.Name = dynamicTextViewModel;
+            this.questionnaireRepository = questionnaireRepository;
+            this.interviewRepository = interviewRepository;
+            this.answerToStringService = answerToStringService;
+           
         }
 
+        public string QuestionnaireTitle { get; set; }
+        public IEnumerable<SideBarPrefillQuestion> PrefilledQuestions { get; set; }
+
         protected Guid interviewId;
+        protected NavigationState navigationState;
 
         public virtual void Init(string interviewId, NavigationState navigationState)
         {
+            this.navigationState = navigationState;
             this.interviewId = Guid.Parse(interviewId);
 
             this.InterviewState.Init(interviewId, null);
             this.Name.InitAsStatic(UIResources.Interview_Cover_Screen_Title);
+
+            var interview = this.interviewRepository.Get(interviewId);
+            var questionnaire = this.questionnaireRepository.GetQuestionnaire(interview.QuestionnaireIdentity, interview.Language);
+
+            this.firstSectionIdentity = new Identity(questionnaire.GetAllSections().First(), RosterVector.Empty);
+            this.QuestionnaireTitle = questionnaire.Title;
+            this.PrefilledQuestions = questionnaire
+                .GetPrefilledQuestions()
+                .Select(questionId => new SideBarPrefillQuestion
+                {
+                    Question = questionnaire.GetQuestionTitle(questionId),
+                    Answer = this.GetAnswer(questionnaire, interview, questionId),
+                    StatsInvisible = questionnaire.GetQuestionType(questionId) == QuestionType.GpsCoordinates,
+                })
+                .ToList();
+        }
+
+        private IMvxCommand startInterviewCommand;
+        private Identity firstSectionIdentity;
+
+        public IMvxCommand StartInterviewCommand
+        {
+            get
+            {
+                return this.startInterviewCommand ?? (this.startInterviewCommand = new MvxCommand(async () => await this.StartInterviewAsync()));
+            }
+        }
+
+        private async Task StartInterviewAsync()
+        {
+            await this.commandService.WaitPendingCommandsAsync();
+            this.navigationState.NavigateTo(NavigationIdentity.CreateForGroup(firstSectionIdentity));
+        }
+
+        private string GetAnswer(IQuestionnaire questionnaire, IStatefulInterview interview, Guid referenceToQuestionId)
+        {
+            var identityAsString = ConversionHelper.ConvertIdAndRosterVectorToString(referenceToQuestionId, RosterVector.Empty);
+            var interviewAnswer = interview.Answers.ContainsKey(identityAsString) ? interview.Answers[identityAsString] : null;
+            return this.answerToStringService.AnswerToUIString(referenceToQuestionId, interviewAnswer, interview, questionnaire);
         }
     }
 }
