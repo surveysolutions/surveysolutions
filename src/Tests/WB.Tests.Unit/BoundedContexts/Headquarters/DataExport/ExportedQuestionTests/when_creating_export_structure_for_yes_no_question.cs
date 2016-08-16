6f16495282c7758bd8bf7e20e7788c1d0a9dc82a
Pavel Stovpivskyi <sby@rambler.ru>
2016-08-16 15:43:52 +0300

    KP-7559 Distinguish missings from disabled questions

---
+++ src/Tests/WB.Tests.Unit/BoundedContexts/Headquarters/DataExport/ExportedQuestionTests/when_creating_export_structure_for_yes_no_question.cs
@@ -0,0 +1,32 @@
+﻿using Machine.Specifications;
+using Main.Core.Entities.SubEntities;
+using WB.Core.BoundedContexts.Headquarters.Views.DataExport;
+using WB.Core.SharedKernels.DataCollection.Events.Interview.Dtos;
+
+namespace WB.Tests.Unit.BoundedContexts.Headquarters.DataExport.ExportedQuestionTests
+{
+    public class when_creating_export_structure_for_yes_no_question : ExportedQuestionTestContext
+    {
+        Establish context = () => { };
+
+        Because of = () =>
+        {
+            filledQuestion = CreateFilledExportedQuestion(QuestionType.MultyOption, 3, new object[2]
+            {
+                new AnsweredYesNoOption(2, true),
+                new AnsweredYesNoOption(0, false),
+            }, QuestionSubtype.MultyOption_YesNo);
+            disabledQuestion = CreateDisabledExportedQuestion(QuestionType.MultyOption, QuestionSubtype.MultyOption_YesNo, columnsCount: 3);
+            missingQuestion = CreateMissingValueExportedQuestion(QuestionType.MultyOption, QuestionSubtype.MultyOption_YesNo, columnsCount: 3);
+        };
+
+        It should_return_correct_filled_answer = () => filledQuestion.Answers.ShouldEqual(new []{ "0", MissingNumericQuestionValue, "1" });
+        It should_return_correct_disabled_answer = () => disabledQuestion.Answers.ShouldEqual(new []{ DisableQuestionValue, DisableQuestionValue, DisableQuestionValue });
+        It should_return_correct_missing_answer = () => missingQuestion.Answers.ShouldEqual(new []{ MissingNumericQuestionValue, MissingNumericQuestionValue, MissingNumericQuestionValue });
+
+
+        private static ExportedQuestion filledQuestion;
+        private static ExportedQuestion disabledQuestion;
+        private static ExportedQuestion missingQuestion;
+    }
+}
