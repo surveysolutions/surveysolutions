73d5c4c0b6186a4832f161588a84c2a21f25bc5c
Pavel Stovpivskyi <sby@rambler.ru>
2016-08-16 15:43:52 +0300

    KP-7559 Distinguish missings from disabled questions

---
+++ src/Tests/WB.Tests.Unit/BoundedContexts/Headquarters/DataExport/ExportedQuestionTests/when_creating_export_structure_for_ordered_yes_no_question.cs
@@ -0,0 +1,34 @@
+﻿using Machine.Specifications;
+using Main.Core.Entities.SubEntities;
+using WB.Core.BoundedContexts.Headquarters.Views.DataExport;
+using WB.Core.SharedKernels.DataCollection.Events.Interview.Dtos;
+
+namespace WB.Tests.Unit.BoundedContexts.Headquarters.DataExport.ExportedQuestionTests
+{
+    public class when_creating_export_structure_for_ordered_yes_no_question : ExportedQuestionTestContext
+    {
+        Establish context = () => { };
+
+        Because of = () =>
+        {
+            filledQuestion = CreateFilledExportedQuestion(QuestionType.MultyOption, 5, new object[]
+            {
+                new AnsweredYesNoOption(4, true),
+                new AnsweredYesNoOption(0, false),
+                new AnsweredYesNoOption(2, true),
+                new AnsweredYesNoOption(3, false),
+            }, QuestionSubtype.MultyOption_YesNoOrdered);
+            disabledQuestion = CreateDisabledExportedQuestion(QuestionType.MultyOption, QuestionSubtype.MultyOption_YesNoOrdered, columnsCount: 5);
+            missingQuestion = CreateMissingValueExportedQuestion(QuestionType.MultyOption, QuestionSubtype.MultyOption_YesNoOrdered, columnsCount: 5);
+        };
+
+        It should_return_correct_filled_answer = () => filledQuestion.Answers.ShouldEqual(new []{ "0", MissingNumericQuestionValue, "2", "0", "1" });
+        It should_return_correct_disabled_answer = () => disabledQuestion.Answers.ShouldEqual(new []{ DisableQuestionValue, DisableQuestionValue, DisableQuestionValue, DisableQuestionValue, DisableQuestionValue });
+        It should_return_correct_missing_answer = () => missingQuestion.Answers.ShouldEqual(new []{ MissingNumericQuestionValue, MissingNumericQuestionValue, MissingNumericQuestionValue, MissingNumericQuestionValue, MissingNumericQuestionValue });
+
+
+        private static ExportedQuestion filledQuestion;
+        private static ExportedQuestion disabledQuestion;
+        private static ExportedQuestion missingQuestion;
+    }
+}
---
+++ src/Tests/WB.Tests.Unit/BoundedContexts/Headquarters/DataExport/ExportedQuestionTests/when_creating_export_structure_for_qr_barcode_question.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Machine.Specifications;
+using Main.Core.Entities.SubEntities;
+using WB.Core.BoundedContexts.Headquarters.Views.DataExport;
+
+namespace WB.Tests.Unit.BoundedContexts.Headquarters.DataExport.ExportedQuestionTests
+{
+    public class when_creating_export_structure_for_qr_barcode_question : ExportedQuestionTestContext
+    {
+        Establish context = () => { };
+
+        Because of = () =>
+        {
+            filledQuestion = CreateFilledExportedQuestion(QuestionType.QRBarcode, "qr bar code");
+            disabledQuestion = CreateDisabledExportedQuestion(QuestionType.QRBarcode);
+            missingQuestion = CreateMissingValueExportedQuestion(QuestionType.QRBarcode);
+        };
+
+        It should_return_correct_filled_answer = () => filledQuestion.Answers.ShouldEqual(new []{ "qr bar code" });
+        It should_return_correct_disabled_answer = () => disabledQuestion.Answers.ShouldEqual(new []{ DisableQuestionValue });
+        It should_return_correct_missing_answer = () => missingQuestion.Answers.ShouldEqual(new []{ MissingStringQuestionValue });
+
+
+        private static ExportedQuestion filledQuestion;
+        private static ExportedQuestion disabledQuestion;
+        private static ExportedQuestion missingQuestion;
+    }
+}
