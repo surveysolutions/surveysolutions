5572b0dcb05aca7e1d915f34c3c373764317be4a
Andrii Kozhyn <hedgic@gmail.com>
2016-08-26 13:38:35 +0300

    KP-7657: Initial split of view to smaller parts

---
+++ src/Core/SharedKernels/Enumerator/Enumerator/ViewModels/InterviewDetails/Questions/IDetailsCompositeItem.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace WB.Core.SharedKernels.Enumerator.ViewModels.InterviewDetails.Questions
+{
+    public interface IDetailsCompositeItem
+    {
+        IEnumerable<object> Children { get; }
+    }
+}
