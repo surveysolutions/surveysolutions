8752fa649b00dd0cf3eefbfae800695802c1406e
Pavel Stovpivskyi <sby@rambler.ru>
2016-08-29 11:54:48 +0300

    Custom errors for designer
    KP-7439

--- src/UI/Designer/WB.UI.Designer/Controllers/ErrorController.cs
+++ src/UI/Designer/WB.UI.Designer/Controllers/ErrorController.cs
@@ -1,25 +1,31 @@
-﻿using System.Web.Mvc;
+﻿using System.Net;
+using System.Web.Mvc;
+using WB.UI.Shared.Web.Attributes;
 
 namespace WB.UI.Designer.Controllers
 {
     [AllowAnonymous]
     public class ErrorController : Controller
     {
+        [NoTransaction]
         public ActionResult Index()
         {
             return View();
         }
 
+        [NoTransaction]
         public ActionResult NotFound()
         {
             return View();
         }
 
+        [NoTransaction]
         public ActionResult AccessDenied()
         {
             return View();
         }
 
+        [NoTransaction]
         public ActionResult Forbidden()
         {
             return this.View();
