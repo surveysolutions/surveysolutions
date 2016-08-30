f02c1414685d2def4f72f7168167adeea6fc591c
Slava Scherbak <slava.scherbak@gmail.com>
2016-08-30 16:12:28 -0400

    Interviewer: moved instructions to separate view model. added instructions for single and multi questions. KP-7665

--- src/Core/SharedKernels/Enumerator/Enumerator/ViewModels/InterviewDetails/Questions/State/QuestionInstructionsViewModel.cs
+++ src/Core/SharedKernels/Enumerator/Enumerator/ViewModels/InterviewDetails/Questions/State/QuestionInstructionsViewModel.cs
@@ -2,32 +2,35 @@ using System;
 using System.Windows.Input;
 using MvvmCross.Core.ViewModels;
 using WB.Core.SharedKernels.DataCollection;
-using WB.Core.SharedKernels.Enumerator.Repositories;
 using WB.Core.SharedKernels.DataCollection.Aggregates;
 using WB.Core.SharedKernels.DataCollection.Repositories;
+using WB.Core.SharedKernels.Enumerator.Repositories;
 
 namespace WB.Core.SharedKernels.Enumerator.ViewModels.InterviewDetails.Questions.State
 {
-    public class QuestionHeaderViewModel : MvxNotifyPropertyChanged,
-        IDisposable
+    public class QuestionInstructionViewModel : MvxNotifyPropertyChanged
     {
         private readonly IStatefulInterviewRepository interviewRepository;
         private readonly IQuestionnaireStorage questionnaireRepository;
 
-        public event EventHandler ShowComments;
-
-        public DynamicTextViewModel Title { get; }
-
-        public QuestionHeaderViewModel(
+        public QuestionInstructionViewModel(
             IQuestionnaireStorage questionnaireRepository,
-            IStatefulInterviewRepository interviewRepository,
-            DynamicTextViewModel dynamicTextViewModel)
+            IStatefulInterviewRepository interviewRepository)
         {
             this.questionnaireRepository = questionnaireRepository;
             this.interviewRepository = interviewRepository;
-
-            this.Title = dynamicTextViewModel;
         }
+
+        public string Instruction { get; private set; }
+
+        private bool isInstructionsHidden;
+        public bool IsInstructionsHidden
+        {
+            get { return this.isInstructionsHidden; }
+            set { this.RaiseAndSetIfChanged(ref this.isInstructionsHidden, value); }
+        }
+
+        public bool HasInstructions => !string.IsNullOrWhiteSpace(Instruction);
 
         public void Init(string interviewId, Identity questionIdentity)
         {
@@ -36,15 +39,11 @@ namespace WB.Core.SharedKernels.Enumerator.ViewModels.InterviewDetails.Questions
 
             var interview = this.interviewRepository.Get(interviewId);
             IQuestionnaire questionnaire = this.questionnaireRepository.GetQuestionnaire(interview.QuestionnaireIdentity, interview.Language);
-            
-            this.Title.Init(interviewId, questionIdentity, questionnaire.GetQuestionTitle(questionIdentity.Id));
+
+            this.IsInstructionsHidden = questionnaire.GetHideInstructions(questionIdentity.Id);
+            this.Instruction = questionnaire.GetQuestionInstruction(questionIdentity.Id);
         }
 
-        public ICommand ShowCommentEditorCommand => new MvxCommand(() => ShowComments?.Invoke(this, EventArgs.Empty));
-
-        public void Dispose()
-        {
-            this.Title.Dispose();
-        }
+        public ICommand ShowInstructions => new MvxCommand(() => this.IsInstructionsHidden = false);
     }
 }
