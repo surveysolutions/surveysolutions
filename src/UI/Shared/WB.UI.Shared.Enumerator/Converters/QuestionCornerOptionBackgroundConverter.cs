b2452b80f1373c3316f790a33993fafb1c3d43da
Andrii Kozhyn <hedgic@gmail.com>
2016-09-05 18:04:16 +0300

    KP-7705: Fixed layout of options for multi option question

--- src/UI/Shared/WB.UI.Shared.Enumerator/Converters/QuestionCornerOptionBackgroundConverter.cs
+++ src/UI/Shared/WB.UI.Shared.Enumerator/Converters/QuestionCornerOptionBackgroundConverter.cs
@@ -4,20 +4,21 @@ using MvvmCross.Platform.Converters;
 
 namespace WB.UI.Shared.Enumerator.Converters
 {
-    public class QuestionOptionBackgroundConverter :  MvxValueConverter<QuestionStateStyle, int>
+    public class QuestionCornerOptionBackgroundConverter : MvxValueConverter<QuestionStateStyle, int>
     {
         protected override int Convert(QuestionStateStyle value, Type targetType, object parameter, CultureInfo culture)
         {
+            bool isTop = (bool)parameter;
             switch (value)
             {
                 case QuestionStateStyle.NonAnswered:
-                    return Resource.Drawable.question_option_background_nonanswered;
+                    return isTop ? Resource.Drawable.question_option_top_border_nonanswered : Resource.Drawable.question_option_bottom_border_nonanswered;
 
                 case QuestionStateStyle.Invalid:
-                    return Resource.Drawable.question_option_background_invalid;
+                    return isTop ? Resource.Drawable.question_option_top_border_invalid : Resource.Drawable.question_option_bottom_border_invalid;
 
                 case QuestionStateStyle.Answered:
-                    return Resource.Drawable.question_option_background_answered;
+                    return Resource.Drawable.question_option_corner_answered;
 
                 default:
                     return Resource.Drawable.question_text_question;
