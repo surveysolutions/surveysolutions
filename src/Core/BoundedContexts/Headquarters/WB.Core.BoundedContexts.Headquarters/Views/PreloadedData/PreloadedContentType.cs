91e15148ac3e73511cf224da74c07a2a03531da4
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2016-09-08 17:56:48 -0400

    KP-7733 Leave one implementation of import

---
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Views/PreloadedData/PreloadedContentType.cs
@@ -0,0 +1,9 @@
+namespace WB.Core.BoundedContexts.Headquarters.Views.PreloadedData
+{
+    public enum PreloadedContentType
+    {
+        Unknown = 0,
+        Sample = 1,
+        Panel = 2
+    }
+}
