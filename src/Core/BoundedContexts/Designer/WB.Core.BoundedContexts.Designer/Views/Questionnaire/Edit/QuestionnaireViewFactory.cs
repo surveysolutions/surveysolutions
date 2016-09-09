6c597b42ccb4b0b7ebd35258dbb7eecd82c941cf
Slava Scherbak <slava.scherbak@gmail.com>
2016-09-09 15:04:44 -0400

    Designer: fixed access to private questionnaires for non-admin users. KP-7774

--- src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Views/Questionnaire/Edit/QuestionnaireViewFactory.cs
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Views/Questionnaire/Edit/QuestionnaireViewFactory.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Main.Core.Documents;
+using WB.Core.BoundedContexts.Designer.Views.Questionnaire.SharedPersons;
 using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
 
 namespace WB.Core.BoundedContexts.Designer.Views.Questionnaire.Edit
@@ -7,15 +10,21 @@ namespace WB.Core.BoundedContexts.Designer.Views.Questionnaire.Edit
     public interface IQuestionnaireViewFactory
     {
         QuestionnaireView Load(QuestionnaireViewInputModel input);
+
+        bool HasUserAccessToQuestionnaire(Guid questionnaireId, Guid userId);
     }
 
     public class QuestionnaireViewFactory : IQuestionnaireViewFactory
     {
         private readonly IReadSideKeyValueStorage<QuestionnaireDocument> questionnaireStorage;
+        private readonly IReadSideKeyValueStorage<QuestionnaireSharedPersons> sharedPersonsStorage;
 
-        public QuestionnaireViewFactory(IReadSideKeyValueStorage<QuestionnaireDocument> questionnaireStorage)
+        public QuestionnaireViewFactory(
+            IReadSideKeyValueStorage<QuestionnaireDocument> questionnaireStorage, 
+            IReadSideKeyValueStorage<QuestionnaireSharedPersons> sharedPersonsStorage)
         {
             this.questionnaireStorage = questionnaireStorage;
+            this.sharedPersonsStorage = sharedPersonsStorage;
         }
 
         public QuestionnaireView Load(QuestionnaireViewInputModel input)
@@ -23,7 +32,20 @@ namespace WB.Core.BoundedContexts.Designer.Views.Questionnaire.Edit
             var doc = GetQuestionnaireDocument(input);
             return doc == null ? null : new QuestionnaireView(doc);
         }
-        
+
+        public bool HasUserAccessToQuestionnaire(Guid questionnaireId, Guid userId)
+        {
+            var questionnaire = this.questionnaireStorage.GetById(questionnaireId);
+            if (questionnaire == null || questionnaire.IsDeleted)
+                return false;
+
+            if (questionnaire.CreatedBy == userId)
+                return true;
+
+            var sharedPersons = sharedPersonsStorage.GetById(questionnaireId)?.SharedPersons ?? new List<SharedPerson>();
+            return sharedPersons.Any(x => x.Id == userId);
+        }
+
         private QuestionnaireDocument GetQuestionnaireDocument(QuestionnaireViewInputModel input)
         {
             try
