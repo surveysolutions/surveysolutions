0bc50c6338fac8049c26ac9b10c4228476d7dd9e
Pavel Stovpivskyi <630198+spuv@users.noreply.github.com>
2016-09-09 18:37:19 +0300

    KP-7737 Remove ChaptersInfoViewDenormalizer

--- src/Core/GenericSubdomains/WB.Core.GenericSubdomains.Portable/Tree.cs
+++ src/Core/GenericSubdomains/WB.Core.GenericSubdomains.Portable/Tree.cs
@@ -66,5 +66,19 @@ namespace WB.Core.GenericSubdomains.Portable
                 }
             }
         }
+
+        public static void ForEachTreeElement<T>(this T root, Func<T, IEnumerable<T>> getChildren, Action<T, T> parentWithChildren)
+        {
+            var stack = new Stack<Tuple<T, T>>();
+            stack.Push(new Tuple<T, T>(default(T), root));
+            while (stack.Count > 0)
+            {
+                var current = stack.Pop();
+                parentWithChildren(current.Item1, current.Item2);
+                IEnumerable<T> childItems = getChildren(current.Item2);
+                foreach (var child in childItems)
+                    stack.Push(new Tuple<T, T>(current.Item2, child));
+            }
+        }
     }
 }
