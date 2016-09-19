e5d14d1e33b2a1556e3116d4ac10d29a51c6f999
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2016-09-19 13:07:41 +0300

    KP-7815: notify item replaced on enablement change

---
+++ src/Tests/WB.Tests.Unit/SharedKernels/Enumerator/CompositeCollectionTests.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Specialized;
+using NUnit.Framework;
+using WB.Core.SharedKernels.Enumerator.Utils;
+
+namespace WB.Tests.Unit.SharedKernels.Enumerator
+{
+    [TestOf(typeof (CompositeCollection<>))]
+    public class CompositeCollectionTests
+    {
+        [Test]
+        public void when_clearing_child_collection_should_raise_items_removed_event()
+        {
+            CompositeCollection<string> items = new CompositeCollection<string>();
+
+            items.Add("zero");
+            var childCollection = new CompositeCollection<string> {"one", "two"};
+            items.AddCollection(childCollection);
+            items.Add("three");
+
+            NotifyCollectionChangedEventArgs collectionChangedArgs = null;
+            items.CollectionChanged += (sender, args) => collectionChangedArgs = args;
+
+            childCollection.Clear();
+
+            Assert.That(collectionChangedArgs, Is.Not.Null);
+            Assert.That(collectionChangedArgs.Action, Is.EqualTo(NotifyCollectionChangedAction.Remove));
+            Assert.That(collectionChangedArgs.OldStartingIndex, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void when_notifing_item_changed()
+        {
+            CompositeCollection<string> items = new CompositeCollection<string>();
+
+            items.Add("zero");
+            items.AddCollection(new CompositeCollection<string> { "one", "two" });
+            items.Add("three");
+
+            NotifyCollectionChangedEventArgs collectionChangedArgs = null;
+            items.CollectionChanged += (sender, args) => collectionChangedArgs = args;
+
+            items.NotifyItemChanged("one");
+
+            Assert.That(collectionChangedArgs, Is.Not.Null);
+            Assert.That(collectionChangedArgs.Action, Is.EqualTo(NotifyCollectionChangedAction.Replace));
+            Assert.That(collectionChangedArgs.OldStartingIndex, Is.EqualTo(1));
+            Assert.That(collectionChangedArgs.NewStartingIndex, Is.EqualTo(1));
+        }
+    }
+}
