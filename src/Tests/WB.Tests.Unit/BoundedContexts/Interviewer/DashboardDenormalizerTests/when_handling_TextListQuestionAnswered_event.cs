386311810a3a31e74a84a40fa11073973cf75580
Slava Scherbak <slava.scherbak@gmail.com>
2016-09-22 14:15:49 +0300

    Added tests after code review. KP-7756

--- src/Tests/WB.Tests.Unit/BoundedContexts/Interviewer/DashboardDenormalizerTests/when_handling_TextListQuestionAnswered_event.cs
+++ src/Tests/WB.Tests.Unit/BoundedContexts/Interviewer/DashboardDenormalizerTests/when_handling_TextListQuestionAnswered_event.cs
@@ -2,30 +2,24 @@ using System;
 using Machine.Specifications;
 using Main.Core.Entities.Composite;
 using Ncqrs.Eventing.ServiceModel.Bus;
+using NUnit.Framework;
 using WB.Core.BoundedContexts.Interviewer.Views.Dashboard;
 using WB.Core.GenericSubdomains.Portable;
 using WB.Core.SharedKernels.DataCollection.Events.Interview;
-using WB.Core.SharedKernels.DataCollection.Events.Interview.Dtos;
 using WB.Core.SharedKernels.DataCollection.Implementation.Entities;
 using WB.Tests.Unit.SharedKernels.SurveyManagement;
-using It = Machine.Specifications.It;
 
 namespace WB.Tests.Unit.BoundedContexts.Interviewer.DashboardDenormalizerTests
 {
-    internal class when_handling_YesNoQuestionAnswered_event
+    internal class when_handling_TextListQuestionAnswered_event
     {
-        Establish context = () =>
+        [OneTimeSetUp]
+        public void context()
         {
-            interviewId = Guid.Parse("22222222222222222222222222222222");
-
-            var questionnaireIdentity = new QuestionnaireIdentity(Guid.Parse("33333333333333333333333333333333"), 1);
-            var questionId = Guid.Parse("11111111111111111111111111111111");
-
-            @event = Create.Event.YesNoQuestionAnswered(questionId, new AnsweredYesNoOption[0]).ToPublishedEvent(interviewId);
+            evnt = Create.Event.TextListQuestionAnswered(questionId, answerTimeUtc: this.answerTimeUtc).ToPublishedEvent(interviewId);
 
             interviewViewStorage = new SqliteInmemoryStorage<InterviewView>();
-            interviewViewStorage.Store(Create.Entity.InterviewView(interviewId: interviewId,
-                questionnaireId: questionnaireIdentity.ToString()));
+            interviewViewStorage.Store(Create.Entity.InterviewView(interviewId: interviewId, questionnaireId: questionnaireIdentity.ToString()));
 
             var plainQuestionnaireRepository = Create.Fake.QuestionnaireRepositoryWithOneQuestionnaire(
                 questionnaireId: questionnaireIdentity.QuestionnaireId,
@@ -33,21 +27,26 @@ namespace WB.Tests.Unit.BoundedContexts.Interviewer.DashboardDenormalizerTests
                 questionnaire: Create.Entity.PlainQuestionnaire(
                     Create.Entity.QuestionnaireDocument(questionnaireIdentity.QuestionnaireId, new IComposite[]
                     {
-                        Create.Entity.YesNoQuestion(questionId: questionId)
+                        Create.Entity.TextListQuestion(questionId: questionId)
                     })));
 
             denormalizer = Create.Service.DashboardDenormalizer(interviewViewRepository: interviewViewStorage, questionnaireStorage: plainQuestionnaireRepository);
-        };
+        }
 
-        Because of = () =>
-            denormalizer.Handle(@event);
+        [SetUp]
+        public void because_of() =>
+            denormalizer.Handle(evnt);
 
-        It should_interview_be_strated = () =>
-            interviewViewStorage.GetById(interviewId.FormatGuid())?.StartedDateTime.ShouldNotBeNull();
+        [Test]
+        public void should_set_answer_time_as_start_date_for_interview() =>
+            interviewViewStorage.GetById(interviewId.FormatGuid())?.StartedDateTime.ShouldEqual(answerTimeUtc);
 
         private static InterviewerDashboardEventHandler denormalizer;
-        private static IPublishedEvent<YesNoQuestionAnswered> @event;
-        private static Guid interviewId;
+        private static IPublishedEvent<TextListQuestionAnswered> evnt;
+        private static Guid interviewId = Guid.Parse("22222222222222222222222222222222");
         private static SqliteInmemoryStorage<InterviewView> interviewViewStorage;
+        private static readonly QuestionnaireIdentity questionnaireIdentity = new QuestionnaireIdentity(Guid.Parse("33333333333333333333333333333333"), 1);
+        private static readonly Guid questionId = Guid.Parse("11111111111111111111111111111111");
+        private readonly DateTime answerTimeUtc = new DateTime(2000, 3, 28).ToUniversalTime();
     }
 }
--- src/Tests/WB.Tests.Unit/TestFactories/EventFactory.cs
+++ src/Tests/WB.Tests.Unit/TestFactories/EventFactory.cs
@@ -121,6 +121,15 @@ namespace WB.Tests.Unit.TestFactories
                 DateTime.Now, 
                 selectedRosterVectors ?? new decimal[][]{});
 
+
+        public PictureQuestionAnswered PictureQuestionAnswered(Guid? questionId = null, decimal[] rosterVector = null, string answer = null, DateTime? answerTimeUtc = null)
+         => new PictureQuestionAnswered(
+               Guid.NewGuid(),
+               questionId ?? Guid.NewGuid(),
+               rosterVector ?? RosterVector.Empty,
+               answerTimeUtc ?? DateTime.Now,
+               answer ?? "file.png");
+
         public QuestionsDisabled QuestionsDisabled(Identity[] questions)
             => new QuestionsDisabled(questions);
 
@@ -231,6 +240,15 @@ namespace WB.Tests.Unit.TestFactories
                 rosterVector ?? WB.Core.SharedKernels.DataCollection.RosterVector.Empty,
                 DateTime.Now,
                 answer ?? "answer");
+
+        public TextListQuestionAnswered TextListQuestionAnswered(
+           Guid? questionId = null, decimal[] rosterVector = null, Tuple<decimal, string>[] answers = null, DateTime? answerTimeUtc = null)
+           => new TextListQuestionAnswered(
+               Guid.NewGuid(),
+               questionId ?? Guid.NewGuid(),
+               rosterVector ?? RosterVector.Empty,
+               answerTimeUtc ?? DateTime.Now,
+               answers ?? new Tuple<decimal, string>[0]);
 
         public VariablesChanged VariablesChanged(params ChangedVariable[] changedVariables)
              => new VariablesChanged(changedVariables);
