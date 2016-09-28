b0ebbe6699391469373e4801ce1fa5208790cfc4
Andrii Kozhyn <hedgic@gmail.com>
2016-09-28 11:59:41 +0300

    KP-7881: Replaced parsing code with appropriate method calls

--- src/Core/SharedKernels/Questionnaire/WB.Core.SharedKernels.Questionnaire/Documents/QuestionnaireDocument.cs
+++ src/Core/SharedKernels/Questionnaire/WB.Core.SharedKernels.Questionnaire/Documents/QuestionnaireDocument.cs
@@ -435,7 +435,7 @@ namespace Main.Core.Documents
         {
             foreach (var answer in question.Answers)
             {
-                answer.AnswerCode = decimal.Parse(answer.AnswerValue, NumberStyles.Number, CultureInfo.InvariantCulture);
+                answer.AnswerCode = answer.GetParsedValue();
                 if (!string.IsNullOrEmpty(answer.ParentValue))
                 {
                     answer.ParentCode = decimal.Parse(answer.ParentValue, NumberStyles.Number, CultureInfo.InvariantCulture);
