a9a8338f394d8b8e6e387e93b1044cd8f270e102
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2016-09-29 12:55:53 -0400

    migrations fix : KP-7886 History: record about copy/paste is cropped

---
+++ src/UI/Designer/WB.UI.Designer/Migrations/PlainStore/M001-100/M007_UpdateQuestionnairechangereferencesTable.cs
@@ -0,0 +1,33 @@
+﻿using FluentMigrator;
+
+namespace WB.UI.Designer.Migrations.PlainStore
+{
+    [Migration(7)]
+    public class M007_UpdateQuestionnairechangereferencesTable : Migration
+    {
+        public override void Up()
+        {
+            if (Schema.Table("questionnairechangereferences").Exists())
+            {
+                if (Schema.Table("questionnairechangereferences").Index("questionnairechangerecords_questionnairechangereferences").Exists())
+                {
+                    Delete.Index("questionnairechangerecords_questionnairechangereferences").OnTable("questionnairechangereferences");
+                }
+
+                if (Schema.Table("questionnairechangereferences").Column("questionnairechangerecord").Exists())
+                {
+                    Delete.Column("questionnairechangerecord").FromTable("questionnairechangereferences");
+                }
+
+                Create.Index("questionnairechangerecords_questionnairechangereferences")
+                    .OnTable("questionnairechangereferences")
+                    .OnColumn("questionnairechangerecordid");
+            }
+        }
+
+        public override void Down()
+        {
+            
+        }
+    }
+}
