8bc3c340800ec6cace0a0267c93af45ab29df6a5
Roman Lytvyn <litvin.roma@gmail.com>
2016-09-30 09:56:13 +0300

    HQ: code review changes by: "Download template archive" file could ask about password even if encryption was turned off KP-6726

--- src/Infrastructure/WB.Infrastructure.Native/Files/Implementation/FileSystem/ZipArchiveUtils.cs
+++ src/Infrastructure/WB.Infrastructure.Native/Files/Implementation/FileSystem/ZipArchiveUtils.cs
@@ -45,7 +45,7 @@ namespace WB.Infrastructure.Native.Files.Implementation.FileSystem
         public Task ZipDirectoryToFileAsync(string sourceDirectory, string archiveFilePath, string directoryFilter = null,
             string fileFilter = null)
         {
-            return Task.Run(() => this.ZipDirectoryToFile(sourceDirectory, archiveFilePath, directoryFilter, fileFilter));
+            throw new NotImplementedException();
         }
 
         public void ZipFiles(IEnumerable<string> files, string archiveFilePath)
@@ -69,7 +69,7 @@ namespace WB.Infrastructure.Native.Files.Implementation.FileSystem
 
         public Task UnzipAsync(string archivedFile, string extractToFolder, bool ignoreRootDirectory = false)
         {
-            return Task.Run(() => this.Unzip(archivedFile, extractToFolder, ignoreRootDirectory));
+            throw new NotImplementedException();
         }
 
         public IEnumerable<UnzippedFile> UnzipStream(Stream zipStream)
@@ -155,9 +155,21 @@ namespace WB.Infrastructure.Native.Files.Implementation.FileSystem
             }
         }
 
-        public Stream GetZipWithPassword(Stream inputZipStream, string password)
+        public Stream ProtectZipWithPassword(Stream inputZipStream, string password)
         {
-            throw new NotImplementedException();
+            var outputZipStream = new MemoryStream();
+            using (var zipFile = ZipFile.Read(inputZipStream))
+            {
+                zipFile.Password = password;
+                foreach (ZipEntry zipEntry in zipFile)
+                {
+                    zipEntry.Password = password;
+                }
+                zipFile.Save(outputZipStream);
+            }
+            outputZipStream.Position = 0;
+
+            return outputZipStream;
         }
     }
 }
