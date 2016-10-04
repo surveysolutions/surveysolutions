8999b3f8ce76cd8f876fc38a66b6915c212afd26
Andrii Kozhyn <hedgic@gmail.com>
2016-10-04 11:50:55 +0300

    KP-7902: adding find-replace to designer

--- src/UI/Designer/WB.UI.Designer/questionnaire/details/scripts/controllers/findReplace.js
+++ src/UI/Designer/WB.UI.Designer/questionnaire/details/scripts/controllers/findReplace.js
@@ -1,9 +1,10 @@
 ﻿angular.module('designerApp')
-    .controller('findReplaceCtrl', function ($scope, $http, $state) {
+    .controller('findReplaceCtrl', function ($rootScope, $scope, $http, $state) {
         var baseUrl = '../../api/findReplace';
 
         $scope.searchFor = '';
         $scope.relaceWith = '';
+        var indexOfCurrentReference = -1;
         $scope.foundReferences = [];
 
         $scope.findAll = function() {
@@ -17,6 +18,22 @@
                 headers: { 'Content-Type': 'application/json', 'Accept': 'application/json' }
             }).then(function(response) {
                 $scope.foundReferences = response.data;
+                $scope.indexOfCurrentReference = -1;
             });
         }
+        $scope.navigateNext = function() {
+            indexOfCurrentReference++;
+            if (indexOfCurrentReference > $scope.foundReferences.length) {
+                indexOfCurrentReference = 0; 
+            } else {
+                $rootScope.navigateTo($scope.foundReferences[indexOfCurrentReference]);
+            }
+        };
+        $scope.navigatePrev = function() {
+            indexOfCurrentReference--;
+            if (indexOfCurrentReference < 0) {
+                indexOfCurrentReference = 0;
+            }
+            $rootScope.navigateTo($scope.foundReferences[indexOfCurrentReference]);
+        };
     });
