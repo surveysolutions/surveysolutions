e44da96b193191feb4d390ca6e9b8802ee5fa28c
Andrii Kozhyn <hedgic@gmail.com>
2016-10-04 17:18:17 +0300

    KP-7902: Updated angular bootstrap lib

--- src/UI/Designer/WB.UI.Designer/questionnaire/details/scripts/controllers/attachments.js
+++ src/UI/Designer/WB.UI.Designer/questionnaire/details/scripts/controllers/attachments.js
@@ -1,6 +1,6 @@
 ﻿angular.module('designerApp')
     .controller('AttachmentsCtrl',
-        function ($rootScope, $scope, $state, hotkeys, commandService, utilityService, confirmService, Upload, $modal, notificationService, moment) {
+        function ($rootScope, $scope, $state, hotkeys, commandService, utilityService, confirmService, Upload, $uibModal, notificationService, moment) {
             'use strict';
 
             $scope.downloadLookupFileBaseUrl = '../../attachments';
@@ -208,7 +208,7 @@
 
             $scope.previewAttachment = function (attachment) {
                 var baseURL = $scope.downloadLookupFileBaseUrl;
-                var modalInstance = $modal.open({
+                var modalInstance = $uibModal.open({
                     templateUrl: 'views/attachment-preview.html',
                     controller: function ($scope, attachment) {
                         $scope.attachment = attachment;
