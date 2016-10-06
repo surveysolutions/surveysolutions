7309208e47cdef16e73fff1f031b52dd5c10edbb
Pavel Stovpivskyi <630198+spuv@users.noreply.github.com>
2016-10-06 19:21:23 +0300

    KP-7919 Add support schemas for migrations

---
+++ src/Infrastructure/WB.Infrastructure.Native/Storage/Postgre/DbMigrations/InSchemaVersionTable .cs
@@ -0,0 +1,17 @@
+﻿using FluentMigrator.VersionTableInfo;
+
+namespace WB.Infrastructure.Native.Storage.Postgre.DbMigrations
+{
+    [VersionTableMetaData]
+    public class InSchemaVersionTable : DefaultVersionTableMetaData
+    {
+        private string schemaName;
+
+        public override string SchemaName => this.schemaName;
+
+        public void SetSchemaName(string value)
+        {
+            this.schemaName = value;
+        }
+    }
+}
