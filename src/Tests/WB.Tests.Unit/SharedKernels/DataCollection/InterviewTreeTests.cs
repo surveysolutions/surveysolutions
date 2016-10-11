d6c28bd5be31974db0bdffbc126ec4a02d5f0dc3
Roman Lytvyn <litvin.roma@gmail.com>
2016-10-11 18:42:23 +0300

    Interview: code review changes by: apply events by interview trees. KP-7908

--- src/Tests/WB.Tests.Unit/SharedKernels/DataCollection/InterviewTreeTests.cs
+++ src/Tests/WB.Tests.Unit/SharedKernels/DataCollection/InterviewTreeTests.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using FluentAssertions;
 using NUnit.Framework;
 using WB.Core.SharedKernels.DataCollection;
 using WB.Core.SharedKernels.DataCollection.Implementation.Aggregates.InterviewEntities;
@@ -13,7 +11,7 @@ namespace WB.Tests.Unit.SharedKernels.DataCollection
     public class InterviewTreeTests
     {
         [Test]
-        public void When_FindDiff_and_changed_tree_has_2_nodes_which_dont_have_source_tree_Then_should_return_2_diff_nodes()
+        public void When_Compare_and_changed_tree_has_2_nodes_which_dont_have_source_tree_Then_should_return_2_diff_nodes()
         {
             //arrange
             var interviewId = Guid.Parse("11111111111111111111111111111111");
@@ -26,10 +24,10 @@ namespace WB.Tests.Unit.SharedKernels.DataCollection
             changedTreeMainSection.AddChildren(Create.Entity.InterviewTreeQuestion(addedQuestionIdentity));
             changedTreeMainSection.AddChildren(Create.Entity.InterviewTreeRoster(addedRosterIdentity));
 
-            var sourceTree = new InterviewTree(interviewId, new List<InterviewTreeSection>() {sourceTreeMainSection});
-            var changedTree = new InterviewTree(interviewId, new List<InterviewTreeSection>() { changedTreeMainSection });
+            var sourceTree = Create.Entity.InterviewTree(interviewId, sourceTreeMainSection);
+            var changedTree = Create.Entity.InterviewTree(interviewId, changedTreeMainSection);
             //act
-            var diff = sourceTree.FindDiff(changedTree).ToList();
+            var diff = sourceTree.Compare(changedTree).ToList();
             //assert
             Assert.That(diff.Count, Is.EqualTo(2));
             Assert.That(diff[0].SourceNode, Is.EqualTo(null));
@@ -42,7 +40,7 @@ namespace WB.Tests.Unit.SharedKernels.DataCollection
         }
 
         [Test]
-        public void When_FindDiff_and_source_tree_has_2_nodes_which_dont_have_changed_tree_Then_should_return_2_diff_nodes()
+        public void When_Compare_and_source_tree_has_2_nodes_which_dont_have_changed_tree_Then_should_return_2_diff_nodes()
         {
             //arrange
             var interviewId = Guid.Parse("11111111111111111111111111111111");
@@ -55,10 +53,10 @@ namespace WB.Tests.Unit.SharedKernels.DataCollection
             sourceTreeMainSection.AddChildren(Create.Entity.InterviewTreeQuestion(addedQuestionIdentity));
             sourceTreeMainSection.AddChildren(Create.Entity.InterviewTreeRoster(addedRosterIdentity));
 
-            var sourceTree = new InterviewTree(interviewId, new List<InterviewTreeSection>() { sourceTreeMainSection });
-            var changedTree = new InterviewTree(interviewId, new List<InterviewTreeSection>() { changedTreeMainSection });
+            var sourceTree = Create.Entity.InterviewTree(interviewId, sourceTreeMainSection);
+            var changedTree = Create.Entity.InterviewTree(interviewId, changedTreeMainSection);
             //act
-            var diff = sourceTree.FindDiff(changedTree).ToList();
+            var diff = sourceTree.Compare(changedTree).ToList();
             //assert
             Assert.That(diff.Count, Is.EqualTo(2));
             Assert.That(diff[0].ChangedNode, Is.EqualTo(null));
@@ -71,36 +69,7 @@ namespace WB.Tests.Unit.SharedKernels.DataCollection
         }
 
         [Test]
-        public void When_FindDiff_and_source_and_changed_trees_has_2_nodes_Then_should_return_2_diff_nodes()
-        {
-            //arrange
-            var interviewId = Guid.Parse("11111111111111111111111111111111");
-            var sectionIdentity = Create.Entity.Identity(Guid.Parse("22222222222222222222222222222222"));
-            var addedQuestionIdentity = Create.Entity.Identity(Guid.Parse("33333333333333333333333333333333"));
-            var addedRosterIdentity = Create.Entity.Identity(Guid.Parse("44444444444444444444444444444444"));
-            var sourceTreeSection = Create.Entity.InterviewTreeSection(sectionIdentity);
-
-            sourceTreeSection.AddChildren(Create.Entity.InterviewTreeQuestion(addedQuestionIdentity));
-            sourceTreeSection.AddChildren(Create.Entity.InterviewTreeRoster(addedRosterIdentity));
-
-            var sourceTree = new InterviewTree(interviewId, new List<InterviewTreeSection>() { sourceTreeSection });
-            //act
-            var diff = sourceTree.FindDiff(sourceTree).ToList();
-            //assert
-            Assert.That(diff.Count, Is.EqualTo(2));
-            Assert.IsAssignableFrom<InterviewTreeQuestion>(diff[0].SourceNode);
-            Assert.IsAssignableFrom<InterviewTreeRoster>(diff[1].SourceNode);
-            Assert.That(diff[0].SourceNode.Identity, Is.EqualTo(addedQuestionIdentity));
-            Assert.That(diff[1].SourceNode.Identity, Is.EqualTo(addedRosterIdentity));
-
-            Assert.IsAssignableFrom<InterviewTreeQuestion>(diff[0].ChangedNode);
-            Assert.IsAssignableFrom<InterviewTreeRoster>(diff[1].ChangedNode);
-            Assert.That(diff[0].ChangedNode.Identity, Is.EqualTo(addedQuestionIdentity));
-            Assert.That(diff[1].ChangedNode.Identity, Is.EqualTo(addedRosterIdentity));
-        }
-
-        // TODO Roma KP-7908 [Test]
-        public void When_FindDiff_on_same_trees_Then_should_return_no_diffs()
+        public void When_Compare_on_same_trees_Then_should_return_no_diffs()
         {
             //arrange
             var interviewId = Guid.Parse("11111111111111111111111111111111");
@@ -111,14 +80,14 @@ namespace WB.Tests.Unit.SharedKernels.DataCollection
             var targetTree = CreateSimpleTree(interviewId, sectionIdentity, questionIdentity);
 
             //act
-            var diff = sourceTree.FindDiff(targetTree).ToList();
+            var diff = sourceTree.Compare(targetTree).ToList();
 
             //assert
             Assert.That(diff.Count, Is.EqualTo(0));
         }
 
-        // TODO Roma KP-7908 [Test]
-        public void When_FindDiff_and_answer_changed_Then_should_return_1_diff_node_with_that_question()
+        [Test]
+        public void When_Compare_and_answer_changed_Then_should_return_1_diff_node_with_that_question()
         {
             //arrange
             var interviewId = Guid.Parse("11111111111111111111111111111111");
@@ -132,7 +101,7 @@ namespace WB.Tests.Unit.SharedKernels.DataCollection
             var targetTree = CreateSimpleTree(interviewId, sectionIdentity, question2);
 
             //act
-            var diff = sourceTree.FindDiff(targetTree).ToList();
+            var diff = sourceTree.Compare(targetTree).ToList();
 
             //assert
             Assert.That(diff.Count, Is.EqualTo(1));
@@ -140,8 +109,8 @@ namespace WB.Tests.Unit.SharedKernels.DataCollection
             Assert.That(diff[0].ChangedNode, Is.EqualTo(question2));
         }
 
-        // TODO Roma KP-7908 [Test]
-        public void When_FindDiff_and_section_disabled_Then_should_return_1_diff_node_with_that_section()
+        [Test]
+        public void When_Compare_and_section_disabled_Then_should_return_1_diff_node_with_that_section()
         {
             //arrange
             var interviewId = Guid.Parse("11111111111111111111111111111111");
@@ -154,7 +123,7 @@ namespace WB.Tests.Unit.SharedKernels.DataCollection
             var targetTree = CreateSimpleTree(interviewId, section2);
 
             //act
-            var diff = sourceTree.FindDiff(targetTree).ToList();
+            var diff = sourceTree.Compare(targetTree).ToList();
 
             //assert
             Assert.That(diff.Count, Is.EqualTo(1));
