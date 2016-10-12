37f6a7a30da785b18d4885a600e9d09023e324e4
Roman Lytvyn <litvin.roma@gmail.com>
2016-10-12 16:17:44 +0300

    Interview: added applying enable/disable events and updating enablement changes in expression state by interview tree diff. KP-7952

--- src/Core/SharedKernels/DataCollection/DataCollection/Implementation/Aggregates/Interview.ExpressionState.cs
+++ src/Core/SharedKernels/DataCollection/DataCollection/Implementation/Aggregates/Interview.ExpressionState.cs
@@ -13,17 +13,41 @@ namespace WB.Core.SharedKernels.DataCollection.Implementation.Aggregates
 
             UpdateAnswersInExpressionState(diff, expressionState);
             UpdateRostersInExpressionState(diff, expressionState);
+            UpdateDisablingInExpressionState(diff, expressionState);
+        }
+
+        private void UpdateDisablingInExpressionState(IReadOnlyCollection<InterviewTreeNodeDiff> diff, ILatestInterviewExpressionState expressionState)
+        {
+            var allNotNullableNodes = diff.Where(x => x.SourceNode != null && x.ChangedNode != null).ToList();
+
+            var disabledGroups = allNotNullableNodes.OfType<InterviewTreeGroupDiff>().Where(IsDisabledNode).Select(x => x.SourceNode.Identity).ToArray();
+            var enabledGroups = allNotNullableNodes.OfType<InterviewTreeGroupDiff>().Where(IsEnabledNode).Select(x => x.SourceNode.Identity).ToArray();
+
+            var disabledQuestions = allNotNullableNodes.OfType<InterviewTreeQuestionDiff>().Where(IsDisabledNode).Select(x => x.SourceNode.Identity).ToArray();
+            var enabledQuestions = allNotNullableNodes.OfType<InterviewTreeQuestionDiff>().Where(IsEnabledNode).Select(x => x.SourceNode.Identity).ToArray();
+
+            var disabledStaticTexts = allNotNullableNodes.OfType<InterviewTreeStaticTextDiff>().Where(IsDisabledNode).Select(x => x.SourceNode.Identity).ToArray();
+            var enabledStaticTexts = allNotNullableNodes.OfType<InterviewTreeStaticTextDiff>().Where(IsEnabledNode).Select(x => x.SourceNode.Identity).ToArray();
+
+            var disabledVariables = allNotNullableNodes.OfType<InterviewTreeVariableDiff>().Where(IsDisabledNode).Select(x => x.SourceNode.Identity).ToArray();
+            var enabledVariables = allNotNullableNodes.OfType<InterviewTreeVariableDiff>().Where(IsEnabledNode).Select(x => x.SourceNode.Identity).ToArray();
+
+            if (disabledGroups.Any()) expressionState.DisableGroups(disabledGroups);
+            if (enabledGroups.Any()) expressionState.EnableGroups(enabledGroups);
+            if (disabledQuestions.Any()) expressionState.DisableQuestions(disabledQuestions);
+            if (enabledQuestions.Any()) expressionState.EnableQuestions(enabledQuestions);
+            if (disabledStaticTexts.Any()) expressionState.DisableStaticTexts(disabledStaticTexts);
+            if (enabledStaticTexts.Any()) expressionState.EnableStaticTexts(enabledStaticTexts);
+            if (disabledVariables.Any()) expressionState.DisableVariables(disabledVariables);
+            if (enabledVariables.Any()) expressionState.EnableVariables(enabledVariables);
         }
 
         private static void UpdateAnswersInExpressionState(IReadOnlyCollection<InterviewTreeNodeDiff> diff, ILatestInterviewExpressionState expressionState)
         {
-            var diffByChangedQuestions = diff
-                .Where(x=>x.SourceNode is InterviewTreeQuestion || x.ChangedNode is InterviewTreeQuestion);
-
-            foreach (var diffByQuestion in diffByChangedQuestions)
+            foreach (var diffByQuestion in diff.OfType<InterviewTreeQuestionDiff>())
             {
-                var sourceQuestion = diffByQuestion.SourceNode as InterviewTreeQuestion;
-                var changedQuestion = diffByQuestion.ChangedNode as InterviewTreeQuestion;
+                var sourceQuestion = diffByQuestion.SourceNode;
+                var changedQuestion = diffByQuestion.ChangedNode;
 
                 if (sourceQuestion == null) continue;
 
@@ -47,25 +71,25 @@ namespace WB.Core.SharedKernels.DataCollection.Implementation.Aggregates
                         changedQuestion.Identity.RosterVector, changedQuestion.AsTextList.GetAnswer());
                 }
 
-                if (changedQuestion.IsDouble && changedQuestion.AsDouble.IsAnswered)
+                if (changedQuestion.IsDouble)
                 {
                     expressionState.UpdateNumericRealAnswer(changedQuestion.Identity.Id,
                         changedQuestion.Identity.RosterVector, changedQuestion.AsDouble.GetAnswer());
                 }
 
-                if (changedQuestion.IsInteger && changedQuestion.AsInteger.IsAnswered)
+                if (changedQuestion.IsInteger)
                 {
                     expressionState.UpdateNumericIntegerAnswer(changedQuestion.Identity.Id,
                         changedQuestion.Identity.RosterVector, changedQuestion.AsInteger.GetAnswer());
                 }
 
-                if (changedQuestion.IsDateTime && changedQuestion.AsDateTime.IsAnswered)
+                if (changedQuestion.IsDateTime)
                 {
                     expressionState.UpdateDateAnswer(changedQuestion.Identity.Id,
                         changedQuestion.Identity.RosterVector, changedQuestion.AsDateTime.GetAnswer());
                 }
 
-                if (changedQuestion.IsGps && changedQuestion.AsGps.IsAnswered)
+                if (changedQuestion.IsGps)
                 {
                     var gpsAnswer = changedQuestion.AsGps.GetAnswer();
                     expressionState.UpdateGeoLocationAnswer(changedQuestion.Identity.Id,
@@ -85,19 +109,19 @@ namespace WB.Core.SharedKernels.DataCollection.Implementation.Aggregates
                         changedQuestion.Identity.RosterVector, changedQuestion.AsMultimedia.GetAnswer());
                 }
 
-                if (changedQuestion.IsYesNo && changedQuestion.AsYesNo.IsAnswered)
+                if (changedQuestion.IsYesNo)
                 {
                     expressionState.UpdateYesNoAnswer(changedQuestion.Identity.Id,
                         changedQuestion.Identity.RosterVector, ConvertToYesNoAnswersOnly(changedQuestion.AsYesNo.GetAnswer()));
                 }
 
-                if (changedQuestion.IsSingleOption && changedQuestion.AsSingleOption.IsAnswered)
+                if (changedQuestion.IsSingleOption)
                 {
                     expressionState.UpdateSingleOptionAnswer(changedQuestion.Identity.Id,
                         changedQuestion.Identity.RosterVector, changedQuestion.AsSingleOption.GetAnswer());
                 }
 
-                if (changedQuestion.IsSingleOption && changedQuestion.AsSingleOption.IsAnswered)
+                if (changedQuestion.IsSingleOption)
                 {
                     expressionState.UpdateSingleOptionAnswer(changedQuestion.Identity.Id,
                         changedQuestion.Identity.RosterVector, changedQuestion.AsSingleOption.GetAnswer());
@@ -125,15 +149,17 @@ namespace WB.Core.SharedKernels.DataCollection.Implementation.Aggregates
 
         private static void UpdateRostersInExpressionState(IReadOnlyCollection<InterviewTreeNodeDiff> diff, ILatestInterviewExpressionState expressionState)
         {
-            var removedRosters =
-                diff.Where(x => x.SourceNode is InterviewTreeRoster && x.ChangedNode == null)
-                    .Select(x => x.SourceNode)
-                    .ToArray();
+            var removedRosters = diff
+                .OfType<InterviewTreeRosterDiff>()
+                .Where(x => x.SourceNode != null && x.ChangedNode == null)
+                .Select(x => x.SourceNode)
+                .ToArray();
 
-            var addedRosters =
-                diff.Where(x => x.SourceNode == null && x.ChangedNode is InterviewTreeRoster)
-                    .Select(x => x.ChangedNode)
-                    .ToArray();
+            var addedRosters = diff
+                .OfType<InterviewTreeRosterDiff>()
+                .Where(x => x.SourceNode == null && x.ChangedNode != null)
+                .Select(x => x.ChangedNode)
+                .ToArray();
 
             foreach (var removedRosterIdentity in removedRosters.Select(ToRosterInstance))
             {
