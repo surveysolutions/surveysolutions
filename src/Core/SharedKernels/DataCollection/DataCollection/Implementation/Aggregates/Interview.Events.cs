c5221603ad8341dd2aff648d0109be470c9854b8
Slava Scherbak <slava.scherbak@gmail.com>
2016-10-14 14:42:58 -0400

    Interview: fixed fire of disablement event if just added question is disabled. KP-7942

--- src/Core/SharedKernels/DataCollection/DataCollection/Implementation/Aggregates/Interview.Events.cs
+++ src/Core/SharedKernels/DataCollection/DataCollection/Implementation/Aggregates/Interview.Events.cs
@@ -54,28 +54,28 @@ namespace WB.Core.SharedKernels.DataCollection.Implementation.Aggregates
 
         private void ApplyEnablementEvents(IReadOnlyCollection<InterviewTreeNodeDiff> diff)
         {
-            var allNotNullableNodes = diff.Where(x => x.SourceNode != null && x.ChangedNode != null).ToList();
+            var allNotNullableNodes = diff.Where(x => x.ChangedNode != null).ToList();
 
-            var disabledGroups = allNotNullableNodes.OfType<InterviewTreeGroupDiff>().Where(IsDisabledNode).Select(x => x.SourceNode.Identity).ToArray();
-            var enabledGroups = allNotNullableNodes.OfType<InterviewTreeGroupDiff>().Where(IsEnabledNode).Select(x => x.SourceNode.Identity).ToArray();
+            var disabledGroups = allNotNullableNodes.OfType<InterviewTreeGroupDiff>().Where(IsDisabledNode).Select(x => x.ChangedNode.Identity).ToArray();
+            var enabledGroups = allNotNullableNodes.OfType<InterviewTreeGroupDiff>().Where(IsEnabledNode).Select(x => x.ChangedNode.Identity).ToArray();
 
-            var disabledQuestions = allNotNullableNodes.OfType<InterviewTreeQuestionDiff>().Where(IsDisabledNode).Select(x => x.SourceNode.Identity).ToArray();
-            var enabledQuestions = allNotNullableNodes.OfType<InterviewTreeQuestionDiff>().Where(IsEnabledNode).Select(x => x.SourceNode.Identity).ToArray();
+            var disabledQuestions = allNotNullableNodes.OfType<InterviewTreeQuestionDiff>().Where(IsDisabledNode).Select(x => x.ChangedNode.Identity).ToArray();
+            var enabledQuestions = allNotNullableNodes.OfType<InterviewTreeQuestionDiff>().Where(IsEnabledNode).Select(x => x.ChangedNode.Identity).ToArray();
 
-            var disabledStaticTexts = allNotNullableNodes.OfType<InterviewTreeStaticTextDiff>().Where(IsDisabledNode).Select(x => x.SourceNode.Identity).ToArray();
-            var enabledStaticTexts = allNotNullableNodes.OfType<InterviewTreeStaticTextDiff>().Where(IsEnabledNode).Select(x => x.SourceNode.Identity).ToArray();
+            var disabledStaticTexts = allNotNullableNodes.OfType<InterviewTreeStaticTextDiff>().Where(IsDisabledNode).Select(x => x.ChangedNode.Identity).ToArray();
+            var enabledStaticTexts = allNotNullableNodes.OfType<InterviewTreeStaticTextDiff>().Where(IsEnabledNode).Select(x => x.ChangedNode.Identity).ToArray();
 
-            var disabledVariables = allNotNullableNodes.OfType<InterviewTreeVariableDiff>().Where(IsDisabledNode).Select(x => x.SourceNode.Identity).ToArray();
-            var enabledVariables = allNotNullableNodes.OfType<InterviewTreeVariableDiff>().Where(IsEnabledNode).Select(x => x.SourceNode.Identity).ToArray();
+            var disabledVariables = allNotNullableNodes.OfType<InterviewTreeVariableDiff>().Where(IsDisabledNode).Select(x => x.ChangedNode.Identity).ToArray();
+            var enabledVariables = allNotNullableNodes.OfType<InterviewTreeVariableDiff>().Where(IsEnabledNode).Select(x => x.ChangedNode.Identity).ToArray();
 
-            if(disabledGroups.Any()) this.Apply(new GroupsDisabled(disabledGroups));
-            if (enabledGroups.Any()) this.Apply(new GroupsEnabled(enabledGroups));
-            if (disabledQuestions.Any()) this.Apply(new QuestionsDisabled(disabledQuestions));
-            if(enabledQuestions.Any()) this.Apply(new QuestionsEnabled(enabledQuestions));
-            if(disabledStaticTexts.Any()) this.Apply(new StaticTextsDisabled(disabledStaticTexts));
-            if(enabledStaticTexts.Any()) this.Apply(new StaticTextsEnabled(enabledStaticTexts));
-            if(disabledVariables.Any()) this.Apply(new VariablesDisabled(disabledVariables));
-            if(enabledVariables.Any()) this.Apply(new VariablesEnabled(enabledVariables));
+            if(disabledGroups.Any()) this.ApplyEvent(new GroupsDisabled(disabledGroups));
+            if (enabledGroups.Any()) this.ApplyEvent(new GroupsEnabled(enabledGroups));
+            if (disabledQuestions.Any()) this.ApplyEvent(new QuestionsDisabled(disabledQuestions));
+            if(enabledQuestions.Any()) this.ApplyEvent(new QuestionsEnabled(enabledQuestions));
+            if(disabledStaticTexts.Any()) this.ApplyEvent(new StaticTextsDisabled(disabledStaticTexts));
+            if(enabledStaticTexts.Any()) this.ApplyEvent(new StaticTextsEnabled(enabledStaticTexts));
+            if(disabledVariables.Any()) this.ApplyEvent(new VariablesDisabled(disabledVariables));
+            if(enabledVariables.Any()) this.ApplyEvent(new VariablesEnabled(enabledVariables));
         }
 
         private void ApplyUpdateAnswerEvents(InterviewTreeQuestionDiff[] diffByQuestions, Guid responsibleId)
