a467807c8b5d42641541347fc721a0e148c390c5
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2016-10-17 12:22:15 +0300

    KP-8005: Added validation of arguments for search/replace

--- src/UI/Designer/WB.UI.Designer/questionnaire/details/scripts/services/errorReportingService.js
+++ src/UI/Designer/WB.UI.Designer/questionnaire/details/scripts/services/errorReportingService.js
@@ -4,8 +4,12 @@
         function ($q, notificationService) {
             return {
                 responseError: function (rejection) {
-                    if (rejection.status === 406 || rejection.status === 403) {
-                        notificationService.notice(rejection.data.Message);
+                    if (rejection.status === 406 || rejection.status === 403 || rejection.status === 400) {
+                        if (rejection.data.message) {
+                            notificationService.notice(rejection.data.message);
+                        } else {
+                            notificationService.notice(rejection.data.Message);
+                        }
                     } else {
                         notificationService.error('Request failed unexpectedly.');
                     }
