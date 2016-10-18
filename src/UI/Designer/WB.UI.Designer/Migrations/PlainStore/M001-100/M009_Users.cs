0b5f51d91ff4910eab131a9636be66475ebb8728
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2016-10-18 17:57:30 +0300

    D\Users: initiak fluent migration corrected, more indexes added KP-8023

--- src/UI/Designer/WB.UI.Designer/Migrations/PlainStore/M001-100/M009_Users.cs
+++ src/UI/Designer/WB.UI.Designer/Migrations/PlainStore/M001-100/M009_Users.cs
@@ -2,45 +2,51 @@
 
 namespace WB.UI.Designer.Migrations.PlainStore
 {
-    [Migration(9)]
+    [Migration(9, "Creates users if they were not migrated from read side")]
     public class M009_Users : Migration
     {
         public override void Up()
         {
-            this.Create.Table("users")
-                .WithColumn("id").AsString(255).PrimaryKey()
-                .WithColumn("applicationname").AsString().Nullable()
-                .WithColumn("comment").AsString().Nullable()
-                .WithColumn("confirmationtoken").AsString().Nullable()
-                .WithColumn("createdat").AsDateTime().Nullable()
-                .WithColumn("email").AsString().Nullable()
-                .WithColumn("isconfirmed").AsBoolean().Nullable()
-                .WithColumn("islockedout").AsBoolean().Nullable()
-                .WithColumn("isonline").AsBoolean().Nullable()
-                .WithColumn("lastactivityat").AsDateTime().Nullable()
-                .WithColumn("lastlockedoutat").AsDateTime().Nullable()
-                .WithColumn("lastloginat").AsDateTime().Nullable()
-                .WithColumn("lastpasswordchangeat").AsDateTime().Nullable()
-                .WithColumn("password").AsString().Nullable()
-                .WithColumn("passwordanswer").AsString().Nullable()
-                .WithColumn("passwordquestion").AsString().Nullable()
-                .WithColumn("passwordresetexpirationdate").AsDateTime().Nullable()
-                .WithColumn("passwordresettoken").AsString().Nullable()
-                .WithColumn("passwordsalt").AsString().Nullable()
-                .WithColumn("provideruserkey").AsGuid().Nullable()
-                .WithColumn("username").AsString().Nullable();
+            if (!this.Schema.Table("users").Exists())
+            {
+                this.Create.Table("users")
+                    .WithColumn("id").AsString(255).PrimaryKey()
+                    .WithColumn("applicationname").AsString().Nullable()
+                    .WithColumn("comment").AsString().Nullable()
+                    .WithColumn("confirmationtoken").AsString().Nullable()
+                    .WithColumn("createdat").AsDateTime().Nullable()
+                    .WithColumn("email").AsString().Nullable()
+                    .WithColumn("isconfirmed").AsBoolean().Nullable()
+                    .WithColumn("islockedout").AsBoolean().Nullable()
+                    .WithColumn("isonline").AsBoolean().Nullable()
+                    .WithColumn("lastactivityat").AsDateTime().Nullable()
+                    .WithColumn("lastlockedoutat").AsDateTime().Nullable()
+                    .WithColumn("lastloginat").AsDateTime().Nullable()
+                    .WithColumn("lastpasswordchangeat").AsDateTime().Nullable()
+                    .WithColumn("password").AsString().Nullable()
+                    .WithColumn("passwordanswer").AsString().Nullable()
+                    .WithColumn("passwordquestion").AsString().Nullable()
+                    .WithColumn("passwordresetexpirationdate").AsDateTime().Nullable()
+                    .WithColumn("passwordresettoken").AsString().Nullable()
+                    .WithColumn("passwordsalt").AsString().Nullable()
+                    .WithColumn("provideruserkey").AsGuid().Nullable()
+                    .WithColumn("username").AsString().Nullable();
+            }
 
-            this.Create.Table("simpleroles")
-                .WithColumn("userid").AsString(255)
-                .WithColumn("simpleroleid").AsInt32().Nullable();
+            if (!this.Schema.Table("simpleroles").Exists())
+            {
+                this.Create.Table("simpleroles")
+                    .WithColumn("userid").AsString(255)
+                    .WithColumn("simpleroleid").AsInt32().Nullable();
 
-            this.Create.ForeignKey()
-                .FromTable("simpleroles").ForeignColumn("userid")
-                .ToTable("users").PrimaryColumn("id");
+                this.Create.ForeignKey()
+                    .FromTable("simpleroles").ForeignColumn("userid")
+                    .ToTable("users").PrimaryColumn("id");
 
-            this.Create.Index("simpleroles_userid_indx")
-                .OnTable("simpleroles")
-                .OnColumn("userid").Ascending();
+                this.Create.Index("simpleroles_userid_indx")
+                    .OnTable("simpleroles")
+                    .OnColumn("userid").Ascending();
+            }
         }
 
         public override void Down()
