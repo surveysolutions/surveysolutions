1612c86ffd14a87eaef0d81fc32b01e7abd6e67f
Roman Lytvyn <litvin.roma@gmail.com>
2016-10-18 19:50:24 +0300

    Interview: fixed test by removing questions with answers in nested roster. KP-7908

--- src/Tests/WB.Tests.Unit/SharedKernels/DataCollection/InterviewTests/when_roster_row_with_nested_roster_is_removed.cs
+++ src/Tests/WB.Tests.Unit/SharedKernels/DataCollection/InterviewTests/when_roster_row_with_nested_roster_is_removed.cs
@@ -36,8 +36,7 @@ namespace WB.Tests.Unit.SharedKernels.DataCollection.InterviewTests
 
                 Create.Entity.Roster(rosterId: parentRosterGroupId, rosterSizeQuestionId: questionWhichIncreasesRosterSizeId, children: new IComposite[]
                 {
-                    Create.Entity.Question(questionId: questionInParentRosterId),
-
+                    Create.Entity.NumericIntegerQuestion(id: questionInParentRosterId),
                     Create.Entity.Roster(rosterId: rosterGroupId, rosterSizeQuestionId: questionWhichIncreasesRosterSizeId),
                 }),
             }));
@@ -53,8 +52,7 @@ namespace WB.Tests.Unit.SharedKernels.DataCollection.InterviewTests
             interview.Apply(Create.Event.RosterInstancesAdded(rosterGroupId, new decimal[] { 0 }, 0, null));
             interview.Apply(Create.Event.RosterInstancesAdded(rosterGroupId, new decimal[] { 1 }, 0, null));
 
-            interview.Apply(new NumericIntegerQuestionAnswered(userId, questionInParentRosterId, new decimal[]{0}, DateTime.Now, 2));
-
+            interview.Apply(new NumericIntegerQuestionAnswered(userId, questionInParentRosterId, new decimal[] {0}, DateTime.Now, 2));
             interview.Apply(new NumericIntegerQuestionAnswered(userId, questionInParentRosterId, new decimal[] { 1 }, DateTime.Now, 2));
 
             eventContext = new EventContext();
