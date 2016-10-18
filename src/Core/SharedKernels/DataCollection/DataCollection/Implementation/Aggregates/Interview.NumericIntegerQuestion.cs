f0345836d42e8379a4a061b12ae9b22d4adaf7e8
Slava Scherbak <slava.scherbak@gmail.com>
2016-10-18 11:01:21 -0400

    Interview: move numeric integer command handler in partial class. KP-8013

---
+++ src/Core/SharedKernels/DataCollection/DataCollection/Implementation/Aggregates/Interview.NumericIntegerQuestion.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using WB.Core.SharedKernels.DataCollection.Aggregates;
+using WB.Core.SharedKernels.DataCollection.Commands.Interview;
+using WB.Core.SharedKernels.DataCollection.Implementation.Aggregates.Invariants;
+
+namespace WB.Core.SharedKernels.DataCollection.Implementation.Aggregates
+{
+    public partial class Interview
+    {
+        public void AnswerNumericIntegerQuestion(AnswerNumericIntegerQuestionCommand command)
+            => AnswerNumericIntegerQuestion(command.UserId, command.QuestionId, command.RosterVector, command.AnswerTime, command.Answer);
+
+        internal void AnswerNumericIntegerQuestion(Guid userId, Guid questionId, RosterVector rosterVector, DateTime answerTime, int answer)
+        {
+            new InterviewPropertiesInvariants(this.properties).RequireAnswerCanBeChanged();
+
+            var answeredQuestion = new Identity(questionId, rosterVector);
+
+            IQuestionnaire questionnaire = this.GetQuestionnaireOrThrow(this.questionnaireId, this.questionnaireVersion, this.language);
+
+            var sourceInterviewTree = this.BuildInterviewTree(questionnaire, this.interviewState);
+
+            this.CheckNumericIntegerQuestionInvariants(questionId, rosterVector, answer, questionnaire, answeredQuestion,
+                this.interviewState);
+
+            var changedInterviewTree = this.BuildInterviewTree(questionnaire, this.interviewState);
+
+            var changedQuestionIdentities = new List<Identity> { answeredQuestion };
+            changedInterviewTree.GetQuestion(answeredQuestion).AsInteger.SetAnswer(answer);
+
+            this.ApplyQuestionAnswer(userId, changedInterviewTree, questionnaire, changedQuestionIdentities, sourceInterviewTree);
+        }
+    }
+}
