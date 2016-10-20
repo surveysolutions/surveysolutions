2267914f171beb4741fae9ee0546f22416e5b353
Slava Scherbak <slava.scherbak@gmail.com>
2016-10-20 12:04:04 -0400

    Splitted InterviewTree class into separate files to simplify navigation. KP-8028

---
+++ src/Core/SharedKernels/DataCollection/DataCollection/Implementation/Aggregates/InterviewEntities/InterviewTreeRoster.cs
@@ -0,0 +1,146 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using WB.Core.GenericSubdomains.Portable;
+
+namespace WB.Core.SharedKernels.DataCollection.Implementation.Aggregates.InterviewEntities
+{
+    public class InterviewTreeRoster : InterviewTreeGroup
+    {
+        public InterviewTreeRoster(Identity identity,
+            IEnumerable<IInterviewTreeNode> children,
+            bool isDisabled = false,
+            string rosterTitle = null,
+            int sortIndex = 0,
+            RosterType rosterType = RosterType.Fixed,
+            Guid? rosterSizeQuestion = null,
+            Identity rosterTitleQuestionIdentity = null)
+            : base(identity, children, isDisabled)
+        {
+            this.RosterTitle = rosterTitle;
+            this.SortIndex = sortIndex;
+
+            switch (rosterType)
+            {
+                case RosterType.Fixed:
+                    this.AsFixed = new InterviewTreeFixedRoster();
+                    break;
+                case RosterType.Numeric:
+                    this.AsNumeric = new InterviewTreeNumericRoster(rosterSizeQuestion.Value, rosterTitleQuestionIdentity);
+                    break;
+                case RosterType.YesNo:
+                    this.AsYesNo = new InterviewTreeYesNoRoster(rosterSizeQuestion.Value);
+                    break;
+                case RosterType.Multi:
+                    this.AsMulti = new InterviewTreeMultiRoster(rosterSizeQuestion.Value);
+                    break;
+                case RosterType.List:
+                    this.AsList = new InterviewTreeListRoster(rosterSizeQuestion.Value);
+                    break;
+            }
+        }
+
+        public string RosterTitle { get; set; }
+        public int SortIndex { get; set; } = 0;
+
+        public InterviewTreeNumericRoster AsNumeric { get; }
+        public InterviewTreeListRoster AsList { get; }
+        public InterviewTreeYesNoRoster AsYesNo { get; }
+        public InterviewTreeMultiRoster AsMulti { get; }
+        public InterviewTreeFixedRoster AsFixed { get; }
+
+        public bool IsNumeric => this.AsNumeric != null;
+        public bool IsList => this.AsList != null;
+        public bool IsYesNo => this.AsYesNo != null;
+        public bool IsMulti => this.AsMulti != null;
+        public bool IsFixed => this.AsFixed != null;
+
+        public override string ToString()
+            => $"Roster ({this.Identity}) [{this.RosterTitle}]" + Environment.NewLine
+               + string.Join(Environment.NewLine, this.Children.Select(child => StringExtensions.PrefixEachLine(child.ToString(), "  ")));
+
+        public void SetRosterTitle(string rosterTitle)
+        {
+            this.RosterTitle = rosterTitle;
+        }
+
+        public void UpdateRosterTitle(Func<decimal, string> getCategoricalAnswerOptionText = null)
+        {
+            if (!this.IsList && !this.IsNumeric) return;
+
+            if (this.IsList)
+            {
+                var sizeQuestionId = this.AsList.RosterSizeQuestion;
+                var rosterSizeQuestion = this.GetQuestionFromThisOrUpperLevel(sizeQuestionId);
+                this.SetRosterTitle(rosterSizeQuestion.AsTextList.GetTitleByItemCode(this.Identity.RosterVector.Last()));
+            }
+
+            if (this.IsNumeric)
+            {
+                var titleQuestion = this.Tree.GetQuestion(this.AsNumeric.RosterTitleQuestionIdentity);
+                if (titleQuestion == null) return;
+                var rosterTitle = titleQuestion.IsAnswered()
+                    ? titleQuestion.GetAnswerAsString(getCategoricalAnswerOptionText)
+                    : null;
+                this.SetRosterTitle(rosterTitle);
+            }
+        }
+    }
+
+
+    public enum RosterType
+    {
+        Fixed = 1,
+        Numeric = 2,
+        YesNo = 3,
+        Multi = 4,
+        List = 5
+    }
+
+    public class InterviewTreeFixedRoster
+    {
+    }
+
+    public class InterviewTreeMultiRoster
+    {
+        public Guid RosterSizeQuestion { get; set; }
+
+        public InterviewTreeMultiRoster(Guid rosterSizeQuestion)
+        {
+            this.RosterSizeQuestion = rosterSizeQuestion;
+        }
+    }
+
+    public class InterviewTreeYesNoRoster
+    {
+        public Guid RosterSizeQuestion { get; set; }
+
+        public InterviewTreeYesNoRoster(Guid rosterSizeQuestion)
+        {
+            this.RosterSizeQuestion = rosterSizeQuestion;
+        }
+    }
+
+    public class InterviewTreeNumericRoster
+    {
+        public Guid RosterSizeQuestion { get; set; }
+        public Identity RosterTitleQuestionIdentity { get; set; }
+        public bool HasTitleQuestion => RosterTitleQuestionIdentity != null;
+
+        public InterviewTreeNumericRoster(Guid rosterSizeQuestion, Identity rosterTitleQuestionIdentity)
+        {
+            this.RosterSizeQuestion = rosterSizeQuestion;
+            this.RosterTitleQuestionIdentity = rosterTitleQuestionIdentity;
+        }
+    }
+
+    public class InterviewTreeListRoster
+    {
+        public Guid RosterSizeQuestion { get; set; }
+
+        public InterviewTreeListRoster(Guid rosterSizeQuestion)
+        {
+            this.RosterSizeQuestion = rosterSizeQuestion;
+        }
+    }
+}
