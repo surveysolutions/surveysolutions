c2505f0cfa4a9c49ddc01f831a2001a43d0ad9af
Andrii Kozhyn <hedgic@gmail.com>
2016-10-21 13:49:07 +0300

    KP-8038: fixed cache usage for variables

--- src/Tests/WB.Tests.Unit/SharedKernels/DataCollection/PlainQuestionnaireTests/PlainQuestionnaireTestsTestsContext.cs
+++ src/Tests/WB.Tests.Unit/SharedKernels/DataCollection/PlainQuestionnaireTests/PlainQuestionnaireTestsTestsContext.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using Main.Core.Entities;
-using Main.Core.Entities.SubEntities;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using WB.Core.SharedKernels.DataCollection.Implementation.Entities;
-using WB.Core.SharedKernels.QuestionnaireEntities;
 
 namespace WB.Tests.Unit.SharedKernels.DataCollection.PlainQuestionnaireTests
 {
