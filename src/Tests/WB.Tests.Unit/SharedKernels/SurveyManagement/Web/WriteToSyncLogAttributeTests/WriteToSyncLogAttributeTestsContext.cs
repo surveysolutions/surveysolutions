d933322ed1b845be0e6f1bddaca3be84b43f3a09
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2016-10-25 15:57:51 -0400

    review : KP-7969 Error updating sync log The given key was not present in the dictionary

---
+++ src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/Web/WriteToSyncLogAttributeTests/WriteToSyncLogAttributeTestsContext.cs
@@ -0,0 +1,21 @@
+﻿using Moq;
+using WB.Core.BoundedContexts.Headquarters.Views.SynchronizationLog;
+using WB.Core.Infrastructure.PlainStorage;
+using WB.UI.Headquarters.Code;
+
+namespace WB.Tests.Unit.SharedKernels.SurveyManagement.Web.WriteToSyncLogAttributeTests
+{
+    public class WriteToSyncLogAttributeTestsContext
+    {
+        public static void SetupContext(IPlainStorageAccessor<SynchronizationLogItem> accessor = null)
+        {
+            Setup.InstanceToMockedServiceLocator(accessor?? new Mock<IPlainStorageAccessor<SynchronizationLogItem>>().Object);
+        }
+
+        public static WriteToSyncLogAttribute Create(SynchronizationLogType logType)
+        {
+            return new WriteToSyncLogAttribute(logType);
+        }
+
+    }
+}
