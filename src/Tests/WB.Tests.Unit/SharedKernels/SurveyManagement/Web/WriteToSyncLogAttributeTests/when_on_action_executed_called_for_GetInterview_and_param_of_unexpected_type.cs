f72a88b5ea34fa28e68f5caec5ee51567ba17abd
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2016-10-25 15:57:51 -0400

    review : KP-7969 Error updating sync log The given key was not present in the dictionary

---
+++ src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/Web/WriteToSyncLogAttributeTests/when_on_action_executed_called_for_GetInterview_and_param_of_unexpected_type.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+using Machine.Specifications;
+using Moq;
+using WB.Core.BoundedContexts.Headquarters.Views.SynchronizationLog;
+using WB.Core.Infrastructure.PlainStorage;
+using WB.UI.Headquarters.Code;
+using It = Machine.Specifications.It;
+
+namespace WB.Tests.Unit.SharedKernels.SurveyManagement.Web.WriteToSyncLogAttributeTests
+{
+    internal class when_on_action_executed_called_for_GetInterview_and_param_of_unexpected_type : WriteToSyncLogAttributeTestsContext
+    {
+        Establish context = () =>
+        {
+            synchronizationLogItemPlainStorageAccessorMock = new Mock<IPlainStorageAccessor<SynchronizationLogItem>>();
+
+            SetupContext(synchronizationLogItemPlainStorageAccessorMock.Object);
+            attribute = Create(SynchronizationLogType.GetInterview);
+
+            actionContext = CreateActionContext();
+            actionContext.ActionContext.ActionArguments.Add("id", "test");
+        };
+
+        Because of = () => attribute.OnActionExecuted(actionContext);
+
+        private It should_store_log_item = () =>
+            synchronizationLogItemPlainStorageAccessorMock.Verify(
+                x => x.Store(Moq.It.IsAny<SynchronizationLogItem>(), Moq.It.IsAny<Guid>()),Times.Once);
+
+        private static Mock<IPlainStorageAccessor<SynchronizationLogItem>> synchronizationLogItemPlainStorageAccessorMock; 
+
+        private static WriteToSyncLogAttribute attribute;
+        private static HttpActionExecutedContext actionContext;
+
+        protected static HttpActionExecutedContext CreateActionContext()
+        {
+            return new HttpActionExecutedContext(
+                new HttpActionContext(new HttpControllerContext(), new ReflectedHttpActionDescriptor()), null); 
+        }
+    }
+}
