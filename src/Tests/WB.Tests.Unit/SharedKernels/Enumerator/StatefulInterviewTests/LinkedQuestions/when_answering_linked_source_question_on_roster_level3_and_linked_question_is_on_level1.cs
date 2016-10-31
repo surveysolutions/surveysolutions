3ea680922c83397c78f44c1a3b663fc399a8e359
Slava Scherbak <slava.scherbak@gmail.com>
2016-10-31 18:59:14 -0400

    Linked questions: add test that covers scenarios with up to 3 levels rosters and linked question outside roster and on the first level. KP-8066

---
+++ src/Tests/WB.Tests.Unit/SharedKernels/Enumerator/StatefulInterviewTests/LinkedQuestions/when_answering_linked_source_question_on_roster_level3_and_linked_question_is_on_level1.cs
@@ -0,0 +1,98 @@
+using System;
+using System.Linq;
+using Machine.Specifications;
+using Main.Core.Entities.Composite;
+using WB.Core.SharedKernels.DataCollection;
+using WB.Core.SharedKernels.DataCollection.Implementation.Entities;
+using WB.Core.SharedKernels.Enumerator.Entities.Interview;
+using WB.Core.SharedKernels.Enumerator.Implementation.Aggregates;
+
+namespace WB.Tests.Unit.SharedKernels.Enumerator.StatefulInterviewTests.LinkedQuestions
+{
+    internal class when_answering_linked_source_question_on_roster_level3_and_linked_question_is_on_level1 : StatefulInterviewTestsContext
+    {
+        Establish context = () =>
+        {
+            var questionnaireDocument = Create.Entity.QuestionnaireDocumentWithOneChapter(children: new IComposite[]
+            {
+                Create.Entity.NumericIntegerQuestion(id: rosterSizeQuestionId),
+                Create.Entity.Roster(rosterId: roster1Id, rosterSizeQuestionId: rosterSizeQuestionId, children: new IComposite[]
+                {
+                    Create.Entity.MultipleOptionsQuestion(questionId: nestedRosterSizeQuestionId, answers: new [] {1m, 2m}),
+                    Create.Entity.Roster(rosterId: roster2Id, rosterSizeQuestionId: nestedRosterSizeQuestionId, children: new IComposite[]
+                    {
+                        Create.Entity.FixedRoster(roster3Id, fixedRosterTitles: new [] { Create.Entity.FixedRosterTitle(100, "Title 1"), Create.Entity.FixedRosterTitle(200, "Title 2") }, children: new IComposite[]
+                        {
+                            Create.Entity.TextQuestion(questionId: sourceOfLinkedQuestionId)
+                        })
+                    }),
+                    Create.Entity.SingleQuestion(id: linkedSingleQuestionId, linkedToQuestionId: sourceOfLinkedQuestionId),
+                    Create.Entity.MultyOptionsQuestion(id: linkedMultiQuestionId, linkedToQuestionId: sourceOfLinkedQuestionId)
+                })
+            });
+            var plainQuestionnaire = new PlainQuestionnaire(questionnaireDocument, 0);
+
+            interview = Create.AggregateRoot.StatefulInterview(questionnaire: plainQuestionnaire);
+            interview.AnswerNumericIntegerQuestion(interviewerId, rosterSizeQuestionId, RosterVector.Empty, DateTime.UtcNow, 2);
+            interview.AnswerMultipleOptionsQuestion(interviewerId, nestedRosterSizeQuestionId, Create.Entity.RosterVector(0), DateTime.UtcNow, new[] { 1m, 2m });
+            interview.AnswerMultipleOptionsQuestion(interviewerId, nestedRosterSizeQuestionId, Create.Entity.RosterVector(1), DateTime.UtcNow, new[] { 1m, 2m });
+
+            interview.AnswerTextQuestion(interviewerId, sourceOfLinkedQuestionId, Create.Entity.RosterVector(0, 1, 100), DateTime.UtcNow, "answer 0.1.100");
+            interview.AnswerTextQuestion(interviewerId, sourceOfLinkedQuestionId, Create.Entity.RosterVector(0, 2, 100), DateTime.UtcNow, "answer 0.2.100");
+        };
+
+        Because of = () =>
+            interview.AnswerTextQuestion(interviewerId, sourceOfLinkedQuestionId, Create.Entity.RosterVector(0, 2, 200), DateTime.UtcNow, "answer 0.2.200");
+
+        It should_linked_single_question_from_roster1_has_3_options = () =>
+        {
+            var answersToBeOptions = interview
+                .FindAnswersOfReferencedQuestionForLinkedQuestion(sourceOfLinkedQuestionId, Create.Entity.Identity(linkedSingleQuestionId, Create.Entity.RosterVector(0)))
+                .ToList();
+
+            answersToBeOptions.Count.ShouldEqual(3);
+            answersToBeOptions.OfType<TextAnswer>().Select(x => x.Answer).ShouldContainOnly("answer 0.1.100","answer 0.2.100", "answer 0.2.200");
+        };
+
+        It should_linked_single_question_from_roster2_has_no_options = () =>
+        {
+            var answersToBeOptions = interview
+                .FindAnswersOfReferencedQuestionForLinkedQuestion(sourceOfLinkedQuestionId, Create.Entity.Identity(linkedSingleQuestionId, Create.Entity.RosterVector(1)))
+                .ToList();
+
+            answersToBeOptions.ShouldBeEmpty();
+        };
+
+        It should_linked_multi_question_from_roster1_has_3_options = () => {
+            var answersToBeOptions = interview
+                .FindAnswersOfReferencedQuestionForLinkedQuestion(sourceOfLinkedQuestionId, Create.Entity.Identity(linkedMultiQuestionId, Create.Entity.RosterVector(0)))
+                .ToList();
+
+            answersToBeOptions.Count.ShouldEqual(3);
+            answersToBeOptions.OfType<TextAnswer>().Select(x => x.Answer).ShouldContainOnly("answer 0.1.100", "answer 0.2.100", "answer 0.2.200");
+        };
+
+        It should_linked_multi_question_from_roster2_has_no_options = () => {
+            var answersToBeOptions = interview
+                .FindAnswersOfReferencedQuestionForLinkedQuestion(sourceOfLinkedQuestionId, Create.Entity.Identity(linkedMultiQuestionId, Create.Entity.RosterVector(1)))
+                .ToList();
+
+            answersToBeOptions.ShouldBeEmpty();
+        };
+
+        static StatefulInterview interview;
+
+        static readonly Guid roster1Id = Guid.Parse("11111111111111111111111111111111");
+        static readonly Guid roster2Id = Guid.Parse("22222222222222222222222222222222");
+        static readonly Guid roster3Id = Guid.Parse("33333333333333333333333333333333");
+
+        static readonly Guid rosterSizeQuestionId = Guid.Parse("44444444444444444444444444444444");
+        static readonly Guid nestedRosterSizeQuestionId = Guid.Parse("55555555555555555555555555555555");
+
+        static readonly Guid linkedSingleQuestionId = Guid.Parse("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+        static readonly Guid linkedMultiQuestionId = Guid.Parse("BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB");
+
+        static readonly Guid sourceOfLinkedQuestionId = Guid.Parse("CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC");
+        static readonly Guid interviewerId = Guid.Parse("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF");
+    }
+}
