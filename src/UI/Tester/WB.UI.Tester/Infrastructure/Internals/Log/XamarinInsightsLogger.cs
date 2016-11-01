c2b42330eb3198ab3f52602978ef6d858a8c4c83
Pavel Stovpivskyi <sby@rambler.ru>
2016-11-01 15:26:45 +0200

    KP-8065 Add addition information in Insight

--- src/UI/Tester/WB.UI.Tester/Infrastructure/Internals/Log/XamarinInsightsLogger.cs
+++ src/UI/Tester/WB.UI.Tester/Infrastructure/Internals/Log/XamarinInsightsLogger.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using MvvmCross.Platform;
 using WB.Core.SharedKernels.Enumerator.Services.Infrastructure;
 using WB.Core.GenericSubdomains.Portable.Services;
 using Xamarin;
@@ -13,17 +16,30 @@ namespace WB.UI.Tester.Infrastructure.Internals.Log
 
         public void Warn(string message, Exception exception = null)
         {
-            Insights.Report(exception: exception, warningLevel: Insights.Severity.Warning);
+            Insights.Report(exception: exception, warningLevel: Insights.Severity.Warning, extraData: CollectExtraData());
         }
 
         public void Error(string message, Exception exception = null)
         {
-            Insights.Report(exception: exception, warningLevel: Insights.Severity.Error);
+            Insights.Report(exception: exception, warningLevel: Insights.Severity.Error, extraData: CollectExtraData());
         }
 
         public void Fatal(string message, Exception exception = null)
         {
-            Insights.Report(exception: exception, warningLevel: Insights.Severity.Critical);
+            Insights.Report(exception: exception, warningLevel: Insights.Severity.Critical, extraData: CollectExtraData());
+        }
+
+        private IDictionary CollectExtraData()
+        {
+            var settings = Mvx.Resolve<IRestServiceSettings>();
+            var principal = Mvx.Resolve<IPrincipal>();
+
+            var extraData = new Dictionary<string, string>
+            {
+                {"Endpoint", settings.Endpoint},
+                {"User",     principal.CurrentUserIdentity?.Name}
+            };
+            return extraData;
         }
     }
 }
