c2e59fd52ee00afa7e35472f2f95796559d38134
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2016-11-02 16:57:55 -0400

    KP-7810 Impossible to leave comment on the last question in roster

--- src/UI/Tester/WB.UI.Tester/Activities/InterviewActivity.cs
+++ src/UI/Tester/WB.UI.Tester/Activities/InterviewActivity.cs
@@ -7,7 +7,7 @@ using WB.UI.Shared.Enumerator.Activities;
 namespace WB.UI.Tester.Activities
 {
     [Activity(Label = "", Theme = "@style/BlueAppTheme", HardwareAccelerated = true,
-        WindowSoftInputMode = SoftInput.StateAlwaysHidden | SoftInput.AdjustPan,
+        WindowSoftInputMode = SoftInput.StateAlwaysHidden | SoftInput.AdjustResize,
         ConfigurationChanges = Android.Content.PM.ConfigChanges.Orientation | Android.Content.PM.ConfigChanges.ScreenSize)]
     public class InterviewActivity : BaseInterviewActivity<InterviewViewModel>
     {
