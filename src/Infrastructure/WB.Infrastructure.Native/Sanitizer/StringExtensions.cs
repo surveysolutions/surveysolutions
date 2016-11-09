7ff4f9c16f014e9a04d7fa2358187c53b30ba95e
Andrii Kozhyn <hedgic@gmail.com>
2016-11-09 13:58:04 +0200

    KP-8074: Cut html tags in group names

--- src/Infrastructure/WB.Infrastructure.Native/Sanitizer/StringExtensions.cs
+++ src/Infrastructure/WB.Infrastructure.Native/Sanitizer/StringExtensions.cs
@@ -1,11 +1,13 @@
-﻿namespace WB.Infrastructure.Native.Sanitizer
+﻿using Ganss.XSS;
+
+namespace WB.Infrastructure.Native.Sanitizer
 {
     public static class StringExtensions
     {
         public static string RemoveHtmlTags(this string value)
         {
             if (value == null) return null;
-            var sanitizer = new WbHtmlSanitizer();
+            var sanitizer = new HtmlSanitizer { KeepChildNodes = true };
             sanitizer.AllowedAttributes.Clear();
             sanitizer.AllowedTags.Clear();
 
