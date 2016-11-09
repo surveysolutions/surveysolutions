e7a56391cda55bb271f86ccb0b8696917461bd10
Pavel Stovpivskyi <sby@rambler.ru>
2016-11-09 15:59:28 +0200

    rename variable description to label

--- src/UI/Designer/WB.UI.Designer/Api/Designer/QuestionnaireController.cs
+++ src/UI/Designer/WB.UI.Designer/Api/Designer/QuestionnaireController.cs
@@ -97,7 +97,7 @@ namespace WB.UI.Designer.Api
                 TypeOptions = variableView.TypeOptions,
                 Type = variableView.VariableData.Type,
                 breadcrumbs = variableView.Breadcrumbs,
-                description = variableView.VariableData.Label
+                label = variableView.VariableData.Label
             });
 
             return result;
