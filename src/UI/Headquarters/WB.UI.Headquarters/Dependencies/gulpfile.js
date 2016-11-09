ee12b1e59cc674be01c91f93f269df89289b6675
Slava Scherbak <slava.scherbak@gmail.com>
2016-11-09 18:37:51 -0500

    Login: added joined into lib.js libraries. KP-8067

--- src/UI/Headquarters/WB.UI.Headquarters/Dependencies/gulpfile.js
+++ src/UI/Headquarters/WB.UI.Headquarters/Dependencies/gulpfile.js
@@ -13,14 +13,15 @@
 var config = {
     production: !!util.env.production,
     buildDir: './build',
+    loginDir: '../Views/Account/',
     loginPath: '../Views/Account/LogIn.cshtml',
     cssSource: './css/markup.scss',
-    cssInjectSectionName: 'markup'
+    cssInjectSectionName: 'markup',
+    jsInjectSectionName: 'libs'
 };
 
 gulp.task('styles', function () {
-    var target = gulp.src(config.cssSource);
-    return target
+    return gulp.src(config.cssSource)
         .pipe(sass())
         .pipe(autoprefixer('last 2 version'))
         .pipe(gulp.dest(config.buildDir))
@@ -30,17 +31,31 @@ gulp.task('styles', function () {
     	.pipe(gulp.dest(config.buildDir));
 });
 
-gulp.task('login', ['styles'], function () {
+gulp.task('bowerJs', function () {
+    return gulp.src(mainBowerFiles('**/*.js'))
+        .pipe(debug())
+        .pipe(plugins.ngAnnotate())
+      	.pipe(concat('libs.js'))
+        .pipe(gulp.dest(config.buildDir))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(plugins.uglify())
+        .pipe(plugins.rev())
+    	.pipe(gulp.dest(config.buildDir));
+});
+
+gulp.task('login', ['styles', 'bowerJs'], function () {
     var target = gulp.src(config.loginPath);
-    var sources = gulp.src(config.buildDir + '/markup-*.min.css', { read: false });
+    var cssMarkup = gulp.src(config.buildDir + '/markup-*.min.css', { read: false });
+    var jsLibs = gulp.src(config.buildDir + '/libs-*.min.css', { read: false });
 
     if (config.production) {
         return target
-            .pipe(plugins.inject(sources, { relative: true, name: config.cssInjectSectionName }))
-            .pipe(gulp.dest('../Views/Account/'));
+            .pipe(plugins.inject(cssMarkup, { relative: true, name: config.cssInjectSectionName }))
+            .pipe(plugins.inject(jsLibs, { relative: true, name: config.jsInjectSectionName }))
+            .pipe(gulp.dest(config.loginDir));
     }
 
-    return sources.pipe(debug());
+    return cssMarkup.pipe(debug());
 });
 
 gulp.task('clean', function () {
@@ -48,5 +63,5 @@ gulp.task('clean', function () {
 });
 
 gulp.task('default', ['clean'], function () {
-    gulp.start('styles', 'login');
+    gulp.start('styles', 'bowerJs', 'login');
 });
