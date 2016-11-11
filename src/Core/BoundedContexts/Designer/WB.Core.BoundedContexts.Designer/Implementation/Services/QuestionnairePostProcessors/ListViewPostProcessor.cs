e440de04a4186ccd06de5c6c09ceccbecf16d0fd
Pavel Stovpivskyi <sby@rambler.ru>
2016-11-11 19:16:51 +0200

    KP-8113 Designer: Revert to version for questionnaire. Update list view item

--- src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Implementation/Services/QuestionnairePostProcessors/ListViewPostProcessor.cs
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Implementation/Services/QuestionnairePostProcessors/ListViewPostProcessor.cs
@@ -73,7 +73,8 @@ namespace WB.Core.BoundedContexts.Designer.Implementation.Services.Questionnaire
         ICommandPostProcessor<Questionnaire, UpdateLookupTable>,
         ICommandPostProcessor<Questionnaire, DeleteLookupTable>,
         ICommandPostProcessor<Questionnaire, UpdateCascadingComboboxOptions>,
-        ICommandPostProcessor<Questionnaire, UpdateFilteredComboboxOptions>
+        ICommandPostProcessor<Questionnaire, UpdateFilteredComboboxOptions>,
+        ICommandPostProcessor<Questionnaire, RevertVersionQuestionnaire>
     {
         private IPlainStorageAccessor<User> accountStorage
             => ServiceLocator.Current.GetInstance<IPlainStorageAccessor<User>>();
@@ -299,5 +300,7 @@ namespace WB.Core.BoundedContexts.Designer.Implementation.Services.Questionnaire
         public void Process(Questionnaire aggregate, DeleteLookupTable command) => this.Update(command.QuestionnaireId.FormatGuid());
         public void Process(Questionnaire aggregate, UpdateCascadingComboboxOptions command) => this.Update(command.QuestionnaireId.FormatGuid());
         public void Process(Questionnaire aggregate, UpdateFilteredComboboxOptions command) => this.Update(command.QuestionnaireId.FormatGuid());
+        public void Process(Questionnaire aggregate, RevertVersionQuestionnaire command)
+            => this.Update(command.QuestionnaireId.FormatGuid(), aggregate.QuestionnaireDocument.Title, aggregate.QuestionnaireDocument.IsPublic);
     }
 }
