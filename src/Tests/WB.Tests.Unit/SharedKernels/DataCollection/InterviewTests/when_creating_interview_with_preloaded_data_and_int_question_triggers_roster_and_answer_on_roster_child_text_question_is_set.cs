0cc0229242cc2b5ff45caba068f46ce550813b7d
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2016-11-14 18:19:59 +0200

    compilation errors fixed during deploy

--- src/Tests/WB.Tests.Unit/SharedKernels/DataCollection/InterviewTests/when_creating_interview_with_preloaded_data_and_int_question_triggers_roster_and_answer_on_roster_child_text_question_is_set.cs
+++ src/Tests/WB.Tests.Unit/SharedKernels/DataCollection/InterviewTests/when_creating_interview_with_preloaded_data_and_int_question_triggers_roster_and_answer_on_roster_child_text_question_is_set.cs
@@ -7,6 +7,7 @@ using WB.Core.SharedKernels.DataCollection.Commands.Interview;
 using WB.Core.SharedKernels.DataCollection.DataTransferObjects.Preloading;
 using WB.Core.SharedKernels.DataCollection.Events.Interview;
 using WB.Core.SharedKernels.DataCollection.Implementation.Aggregates;
+using WB.Core.SharedKernels.DataCollection.Implementation.Aggregates.InterviewEntities.Answers;
 
 namespace WB.Tests.Unit.SharedKernels.DataCollection.InterviewTests
 {
@@ -27,9 +28,9 @@ namespace WB.Tests.Unit.SharedKernels.DataCollection.InterviewTests
                 new[]
                 {
                     new PreloadedLevelDto(new decimal[0],
-                        new Dictionary<Guid, object> {{prefilledIntQuestionId, prefilledIntQuestionAnswer}}),
+                        new Dictionary<Guid, AbstractAnswer> { { prefilledIntQuestionId, NumericIntegerAnswer.FromInt(prefilledIntQuestionAnswer) } }),
                     new PreloadedLevelDto(new decimal[] {0},
-                        new Dictionary<Guid, object> {{prefilledTextQuestionId, prefilledTextQuestionAnswer}})
+                        new Dictionary<Guid, AbstractAnswer> {{prefilledTextQuestionId, TextAnswer.FromString(prefilledTextQuestionAnswer) }})
                 });
 
             answersTime = new DateTime(2013, 09, 01);
