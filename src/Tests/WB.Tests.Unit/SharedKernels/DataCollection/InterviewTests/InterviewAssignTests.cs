c0ec47fe611c8bb5db130f917b26cf3a671eac52
Pavel Stovpivskyi <sby@rambler.ru>
2016-11-16 13:00:34 +0200

    Refactoring after code review

--- src/Tests/WB.Tests.Unit/SharedKernels/DataCollection/InterviewTests/InterviewAssignTests.cs
+++ src/Tests/WB.Tests.Unit/SharedKernels/DataCollection/InterviewTests/InterviewAssignTests.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Linq;
-using Machine.Specifications;
 using Moq;
 using Ncqrs.Spec;
 using NUnit.Framework;
@@ -17,32 +16,43 @@ namespace WB.Tests.Unit.SharedKernels.DataCollection.InterviewTests
     [TestFixture]
     internal class InterviewAssignTests : InterviewTestsContext
     {
+        EventContext eventContext;
+
+        public Interview SetupInterview()
+        {
+            var questionnaire = Mock.Of<IQuestionnaire>();
+            var questionnaireRepository = CreateQuestionnaireRepositoryStubWithOneQuestionnaire(questionnaireId, questionnaire);
+            return CreateInterview(questionnaireId: questionnaireId, questionnaireRepository: questionnaireRepository);
+        }
+
+        public void SetupEventContext()
+        {
+            eventContext = Create.Other.EventContext();
+        }
+
+        [TearDown]
+        public void CleanTests()
+        {
+            eventContext?.Dispose();
+            eventContext = null;
+        }
+
         [Test]
         public void When_Interview_in_status_SupervisorAssigned_And_interview_being_moved_to_other_team_As_result_supervisor_should_be_changed()
         {
             // arrange
-            var supervisorId    = Guid.Parse("11111111111111111111111111111111");
-            var supervisorId2   = Guid.Parse("22222222222222222222222222222222");
-            var questionnaireId = Guid.Parse("33333333333333333333333333333333");
-
-            var questionnaire = Mock.Of<IQuestionnaire>();
-            var questionnaireRepository = CreateQuestionnaireRepositoryStubWithOneQuestionnaire(questionnaireId, questionnaire);
-            var interview = CreateInterview(questionnaireId: questionnaireId, questionnaireRepository: questionnaireRepository);
+            var interview = SetupInterview();
             interview.Apply(Create.Event.SupervisorAssigned(supervisorId, supervisorId));
             interview.Apply(Create.Event.InterviewStatusChanged(InterviewStatus.SupervisorAssigned));
-
-            var eventContext = Create.Other.EventContext();
+            SetupEventContext();
 
             // act
-            var exception = Catch.Exception(() => interview.AssignSupervisor(supervisorId, supervisorId2));
+            interview.AssignSupervisor(supervisorId, supervisorId2);
 
             // assert
-            Assert.That(exception, Is.Null);
-            eventContext.AssertIfDoesntContainEvent<SupervisorAssigned>();
-            eventContext.AssertIfContainEvent<InterviewStatusChanged>();
-            eventContext.AssertIfContainEvent<InterviewerAssigned>();
-
-            eventContext.Dispose();
+            eventContext.AssertThatContainsEvent<SupervisorAssigned>();
+            eventContext.AssertThatDoesNotContainEvent<InterviewStatusChanged>();
+            eventContext.AssertThatDoesNotContainEvent<InterviewerAssigned>();
         }
 
 
@@ -50,44 +60,26 @@ namespace WB.Tests.Unit.SharedKernels.DataCollection.InterviewTests
         public void Interview_in_status_InterviewerAssigned_And_interview_being_reassigned_within_one_team_As_result_interviewer_should_be_changed()
         {
             // arrange
-            var interviewerId   = Guid.Parse("99999999999999999999999999999999");
-            var interviewerId2  = Guid.Parse("22222222222222222222222222222222");
-            var supervisorId    = Guid.Parse("11111111111111111111111111111111");
-            var questionnaireId = Guid.Parse("33333333333333333333333333333333");
-
-            var questionnaire = Mock.Of<IQuestionnaire>();
-            var questionnaireRepository = CreateQuestionnaireRepositoryStubWithOneQuestionnaire(questionnaireId, questionnaire);
-            var interview = CreateInterview(questionnaireId: questionnaireId, questionnaireRepository: questionnaireRepository);
+            var interview = SetupInterview();
             interview.Apply(Create.Event.SupervisorAssigned(supervisorId, supervisorId));
             interview.Apply(Create.Event.InterviewerAssigned(supervisorId, interviewerId, DateTime.UtcNow.AddHours(-1)));
             interview.Apply(Create.Event.InterviewStatusChanged(InterviewStatus.InterviewerAssigned));
-
-            var eventContext = Create.Other.EventContext();
+            SetupEventContext();
 
             // act
-            var exception = Catch.Exception(() => interview.AssignInterviewer(supervisorId, interviewerId2, DateTime.UtcNow));
+            interview.AssignInterviewer(supervisorId, interviewerId2, DateTime.UtcNow);
 
             // assert
-            Assert.That(exception, Is.Null);
-            eventContext.AssertIfDoesntContainEvent<InterviewerAssigned>();
-            eventContext.AssertIfContainEvent<SupervisorAssigned>();
-            eventContext.AssertIfContainEvent<InterviewStatusChanged>();
-
-            eventContext.Dispose();
+            eventContext.AssertThatContainsEvent<InterviewerAssigned>();
+            eventContext.AssertThatDoesNotContainEvent<SupervisorAssigned>();
+            eventContext.AssertThatDoesNotContainEvent<InterviewStatusChanged>();
         }
 
         [Test]
         public void Interview_in_status_RejectedBySupervisor_And_interview_being_reassigned_within_one_team_As_result_interviewer_should_be_changed_Status_should_be_intact()
         {
             // arrange
-            var interviewerId   = Guid.Parse("99999999999999999999999999999999");
-            var supervisorId    = Guid.Parse("11111111111111111111111111111111");
-            var headquarterId   = Guid.Parse("77777777777777777777777777777777");
-            var questionnaireId = Guid.Parse("33333333333333333333333333333333");
-
-            var questionnaire = Mock.Of<IQuestionnaire>();
-            var questionnaireRepository = CreateQuestionnaireRepositoryStubWithOneQuestionnaire(questionnaireId, questionnaire);
-            var interview = CreateInterview(questionnaireId: questionnaireId, questionnaireRepository: questionnaireRepository);
+            var interview = SetupInterview();
             interview.Apply(Create.Event.SupervisorAssigned(supervisorId, supervisorId));
             interview.Apply(Create.Event.InterviewerAssigned(supervisorId, interviewerId, DateTime.UtcNow.AddHours(-1)));
             interview.Apply(Create.Event.InteviewCompleted());
@@ -95,19 +87,15 @@ namespace WB.Tests.Unit.SharedKernels.DataCollection.InterviewTests
             interview.Apply(Create.Event.InterviewRejectedByHQ(headquarterId));
             interview.Apply(Create.Event.InterviewRejected(supervisorId));
             interview.Apply(Create.Event.InterviewStatusChanged(InterviewStatus.RejectedBySupervisor));
-
-            var eventContext = Create.Other.EventContext();
+            SetupEventContext();
 
             // act
-            var exception = Catch.Exception(() => interview.AssignInterviewer(supervisorId, interviewerId, DateTime.UtcNow));
+            interview.AssignInterviewer(supervisorId, interviewerId, DateTime.UtcNow);
 
             // assert
-            Assert.That(exception, Is.Null);
-            eventContext.AssertIfDoesntContainEvent<InterviewerAssigned>();
-            eventContext.AssertIfContainEvent<SupervisorAssigned>();
-            eventContext.AssertIfContainEvent<InterviewStatusChanged>();
-
-            eventContext.Dispose();
+            eventContext.AssertThatContainsEvent<InterviewerAssigned>();
+            eventContext.AssertThatDoesNotContainEvent<SupervisorAssigned>();
+            eventContext.AssertThatDoesNotContainEvent<InterviewStatusChanged>();
         }
 
 
@@ -115,34 +103,22 @@ namespace WB.Tests.Unit.SharedKernels.DataCollection.InterviewTests
         public void Interview_in_status_Completed_And_interview_being_moved_to_other_team_As_result_supervisor_should_be_changed_and_interviewer_set_to_null()
         {
             // arrange
-            var interviewerId   = Guid.Parse("99999999999999999999999999999999");
-            var supervisorId2   = Guid.Parse("22222222222222222222222222222222");
-            var supervisorId    = Guid.Parse("11111111111111111111111111111111");
-            var headquarterId   = Guid.Parse("77777777777777777777777777777777");
-            var questionnaireId = Guid.Parse("33333333333333333333333333333333");
-
-            var questionnaire = Mock.Of<IQuestionnaire>();
-            var questionnaireRepository = CreateQuestionnaireRepositoryStubWithOneQuestionnaire(questionnaireId, questionnaire);
-            var interview = CreateInterview(questionnaireId: questionnaireId, questionnaireRepository: questionnaireRepository);
+            var interview = SetupInterview();
             interview.Apply(Create.Event.SupervisorAssigned(supervisorId, supervisorId));
             interview.Apply(Create.Event.InterviewerAssigned(supervisorId, interviewerId, DateTime.UtcNow.AddHours(-1)));
             interview.Apply(Create.Event.InteviewCompleted());
             interview.Apply(Create.Event.InterviewApproved(supervisorId));
             interview.Apply(Create.Event.InterviewRejectedByHQ(headquarterId));
             interview.Apply(Create.Event.InterviewStatusChanged(InterviewStatus.RejectedByHeadquarters));
-
-            var eventContext = Create.Other.EventContext();
+            SetupEventContext();
 
             // act
-            var exception = Catch.Exception(() => interview.AssignSupervisor(headquarterId, supervisorId2));
+            interview.AssignSupervisor(headquarterId, supervisorId2);
 
             // assert
-            Assert.That(exception, Is.Null);
-            eventContext.AssertIfDoesntContainEvent<SupervisorAssigned>();
-            eventContext.AssertIfContainEvent<InterviewerAssigned>();
-            eventContext.AssertIfContainEvent<InterviewStatusChanged>();
-
-            eventContext.Dispose();
+            eventContext.AssertThatContainsEvent<SupervisorAssigned>();
+            eventContext.AssertThatDoesNotContainEvent<InterviewerAssigned>();
+            eventContext.AssertThatDoesNotContainEvent<InterviewStatusChanged>();
         }
 
 
@@ -150,42 +126,35 @@ namespace WB.Tests.Unit.SharedKernels.DataCollection.InterviewTests
         public void Interview_in_status_RejectedByHQ_And_interview_being_moved_to_other_team_As_result_supervisor_should_be_changed_and_interviewer_set_to_null()
         {
             // arrange
-            var interviewerId   = Guid.Parse("99999999999999999999999999999999");
-            var supervisorId2   = Guid.Parse("22222222222222222222222222222222");
-            var supervisorId    = Guid.Parse("11111111111111111111111111111111");
-            var headquarterId   = Guid.Parse("77777777777777777777777777777777");
-            var questionnaireId = Guid.Parse("33333333333333333333333333333333");
-
-            var questionnaire = Mock.Of<IQuestionnaire>();
-            var questionnaireRepository = CreateQuestionnaireRepositoryStubWithOneQuestionnaire(questionnaireId, questionnaire);
-            var interview = CreateInterview(questionnaireId: questionnaireId, questionnaireRepository: questionnaireRepository);
+            var interview = SetupInterview();
             interview.Apply(Create.Event.SupervisorAssigned(supervisorId, supervisorId));
             interview.Apply(Create.Event.InterviewerAssigned(supervisorId, interviewerId, DateTime.UtcNow.AddHours(-1)));
             interview.Apply(Create.Event.InteviewCompleted());
             interview.Apply(Create.Event.InterviewStatusChanged(InterviewStatus.Completed));
-
-            var eventContext = Create.Other.EventContext();
+            SetupEventContext();
 
             // act
-            var exception = Catch.Exception(() => interview.AssignSupervisor(headquarterId, supervisorId2));
+            interview.AssignSupervisor(headquarterId, supervisorId2);
 
             // assert
-            Assert.That(exception, Is.Null);
-            eventContext.AssertIfDoesntContainEvent<SupervisorAssigned>();
-            eventContext.AssertIfContainEvent<InterviewerAssigned>();
-            eventContext.AssertIfContainEvent<InterviewStatusChanged>();
-
-            eventContext.Dispose();
+            eventContext.AssertThatContainsEvent<SupervisorAssigned>();
+            eventContext.AssertThatDoesNotContainEvent<InterviewerAssigned>();
+            eventContext.AssertThatDoesNotContainEvent<InterviewStatusChanged>();
         }
 
 
-
+        readonly Guid interviewerId   = Guid.Parse("99999999999999999999999999999999");
+        readonly Guid interviewerId2  = Guid.Parse("22222222222222222222222222222222");
+        readonly Guid supervisorId    = Guid.Parse("11111111111111111111111111111111");
+        readonly Guid supervisorId2   = Guid.Parse("88888888888888888888888888888888");
+        readonly Guid headquarterId   = Guid.Parse("77777777777777777777777777777777");
+        readonly Guid questionnaireId = Guid.Parse("33333333333333333333333333333333");
     }
 
     internal static class EventContextExtension
     {
         [DebuggerStepThrough]
-        public static void AssertIfDoesntContainEvent<TEvent>(this EventContext eventContext, Func<TEvent, bool> condition = null)
+        public static void AssertThatContainsEvent<TEvent>(this EventContext eventContext, Func<TEvent, bool> condition = null)
         {
             bool isExists = condition == null
                 ? eventContext.Events.Any(@event => @event.Payload is TEvent)
@@ -194,7 +163,7 @@ namespace WB.Tests.Unit.SharedKernels.DataCollection.InterviewTests
         }
 
         [DebuggerStepThrough]
-        public static void AssertIfContainEvent<TEvent>(this EventContext eventContext, Func<TEvent, bool> condition = null)
+        public static void AssertThatDoesNotContainEvent<TEvent>(this EventContext eventContext, Func<TEvent, bool> condition = null)
         {
             bool isExists = condition == null
                 ? eventContext.Events.Any(@event => @event.Payload is TEvent)
