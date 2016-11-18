b3f37b9594e2de6dd0fcb404e972b6bab7f66a25
Roman Lytvyn <litvin.roma@gmail.com>
2016-11-18 19:07:45 +0200

    EN: fix all tests by changing interview state to interview tree. part 5 KP-8159

--- src/Tests/WB.Tests.Unit/SharedKernels/Enumerator/StatefulInterviewTests/when_looking_for_options_of_linked_question_which_linked_on_nested_roster_title.cs
+++ src/Tests/WB.Tests.Unit/SharedKernels/Enumerator/StatefulInterviewTests/when_looking_for_options_of_linked_question_which_linked_on_nested_roster_title.cs
@@ -9,7 +9,6 @@ using WB.Core.SharedKernels.SurveySolutions.Documents;
 
 namespace WB.Tests.Unit.SharedKernels.Enumerator.StatefulInterviewTests
 {
-    [Ignore("KP-8159")]
     internal class when_looking_for_options_of_linked_question_which_linked_on_nested_roster_title : StatefulInterviewTestsContext
     {
         Establish context = () =>
@@ -36,7 +35,7 @@ namespace WB.Tests.Unit.SharedKernels.Enumerator.StatefulInterviewTests
                                         },
                                     children: new[]
                                     {
-                                        Create.Entity.NumericIntegerQuestion(sourceOfLinkedToRosterId)
+                                        Create.Entity.NumericIntegerQuestion()
                                     })
                             }),
                         Create.Entity.MultyOptionsQuestion(linkedToQuestionIdentity.Id, linkedToRosterId: sourceOfLinkedToRosterId)
@@ -47,20 +46,18 @@ namespace WB.Tests.Unit.SharedKernels.Enumerator.StatefulInterviewTests
 
         It should_order_options_by_roster_sort_index_at_first = () =>
         {
-            interview.GetLinkedMultiOptionQuestion(linkedToQuestionIdentity)
-            .Options.Select(a => a.First()).ToArray().ShouldEqual(new decimal[] { 1, 1, 2, 2 });
+            interview.GetLinkedMultiOptionQuestion(linkedToQuestionIdentity).Options.Select(a => a.First()).ToArray().ShouldEqual(new decimal[] { 1, 1, 2, 2 });
         };
 
         It should_order_options_by_nested_roster_sort_index_in_scope_of_parent_roster = () =>
         {
-            interview.GetLinkedMultiOptionQuestion(linkedToQuestionIdentity)
-            .Options.Select(a => a.Last()).ToArray().ShouldEqual(new decimal[] { 1, 2, 1, 2 });
+            interview.GetLinkedMultiOptionQuestion(linkedToQuestionIdentity).Options.Select(a => a.Last()).ToArray().ShouldEqual(new decimal[] { 1, 2, 1, 2 });
         };
 
         private static IQuestionnaireStorage questionnaireRepository;
         private static StatefulInterview interview;
         private static Guid questionnaireId = Guid.Parse("44444444444444444444444444444444");
-        private static Identity linkedToQuestionIdentity = Identity.Create(Guid.Parse("33333333333333333333333333333333"), Create.Entity.RosterVector(0));
+        private static Identity linkedToQuestionIdentity = Identity.Create(Guid.Parse("33333333333333333333333333333333"), Create.Entity.RosterVector());
         private static Guid sourceOfLinkedToRosterId = Guid.Parse("22222222222222222222222222222222");
     }
 }
