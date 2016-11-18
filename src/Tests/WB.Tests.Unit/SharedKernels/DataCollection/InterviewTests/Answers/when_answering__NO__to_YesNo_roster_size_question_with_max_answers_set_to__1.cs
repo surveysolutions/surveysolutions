a50ddad6af9d4de78658141aee477aa491f92ae6
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2016-11-18 10:57:02 +0200

    KP-8168: Rewrite test to nunit

--- src/Tests/WB.Tests.Unit/SharedKernels/DataCollection/InterviewTests/Answers/when_answering__NO__to_YesNo_roster_size_question_with_max_answers_set_to__1.cs
+++ src/Tests/WB.Tests.Unit/SharedKernels/DataCollection/InterviewTests/Answers/when_answering__NO__to_YesNo_roster_size_question_with_max_answers_set_to__1.cs
@@ -1,15 +1,17 @@
 ﻿using System;
-using Machine.Specifications;
 using Main.Core.Entities.SubEntities;
 using Ncqrs.Spec;
+using NUnit.Framework;
 using WB.Core.SharedKernels.DataCollection.Events.Interview;
 using WB.Core.SharedKernels.DataCollection.Implementation.Aggregates;
 
 namespace WB.Tests.Unit.SharedKernels.DataCollection.InterviewTests.Answers
 {
+    [TestFixture, TestOf(typeof(Interview))]
     internal class when_answering__No__to_YesNo_roster_size_question_with_max_answers_set_to__1 : InterviewTestsContext
     {
-        Establish context = () =>
+        [OneTimeSetUp]
+        public void context()
         {
             rosterTriggerId = Guid.Parse("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
             var rosterId = Guid.Parse("BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB");
@@ -35,17 +37,28 @@ namespace WB.Tests.Unit.SharedKernels.DataCollection.InterviewTests.Answers
             interview = CreateInterview(questionnaireId: questionnaireId, questionnaireRepository: questionnaireRepository);
 
             eventContext = new EventContext();
-        };
 
-        Because of = () => interview.AnswerYesNoQuestion(Create.Command.AnswerYesNoQuestion(
+            // Act
+            interview.AnswerYesNoQuestion(Create.Command.AnswerYesNoQuestion(
                 questionId: rosterTriggerId,
                 answeredOptions: new[]
                 {
                     Create.Entity.AnsweredYesNoOption(1m, false),
                     Create.Entity.AnsweredYesNoOption(2m, false)
                 }));
+        }
 
-        It should_allow_no_answers_more_than_max_answers_count = () => eventContext.ShouldContainEvent<YesNoQuestionAnswered>();
+        [Test]
+        public void should_allow_no_answers_more_than_max_answers_count()
+        {
+            eventContext.ShouldContainEvent<YesNoQuestionAnswered>();
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            eventContext.Dispose();
+        }
 
         static Interview interview;
         static Guid rosterTriggerId;
