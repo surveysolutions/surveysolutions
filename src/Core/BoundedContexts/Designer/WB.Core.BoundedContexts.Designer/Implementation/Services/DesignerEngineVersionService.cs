cef29c8bba25fe19d8ea92f6f357aed3a1ef4b79
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2016-11-18 13:36:47 +0200

    KP-8176: Increment content version if question linked to question used

--- src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Implementation/Services/DesignerEngineVersionService.cs
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Implementation/Services/DesignerEngineVersionService.cs
@@ -136,13 +136,25 @@ namespace WB.Core.BoundedContexts.Designer.Implementation.Services
             },
             new QuestionnaireContentVersion
             {
-                Version = ApiVersion.MaxQuestionnaireVersion, /*When will be added new version, it should be changed to previous value of ApiVersion.MaxQuestionnaireVersion*/
+                Version = 17, 
                 NewFeatures = new []
                 {
                     new QuestionnaireFeature
                     {
                           HasQuestionnaire = questionnaire =>  questionnaire.Translations.Any(),
                           Description = "Multilanguage questionnaire"
+                    }
+                }
+            },
+             new QuestionnaireContentVersion
+            {
+                Version = ApiVersion.MaxQuestionnaireVersion, /*When will be added new version, it should be changed to previous value of ApiVersion.MaxQuestionnaireVersion*/
+                NewFeatures = new []
+                {
+                    new QuestionnaireFeature
+                    {
+                          HasQuestionnaire = questionnaire => questionnaire.Find<AbstractQuestion>(q => q.LinkedToQuestionId.HasValue).Any(),
+                          Description = "Question linked to List question"
                     }
                 }
             }
--- src/Core/SharedKernel/WB.Core.SharedKernel.Structures/Api/Designer/ApiVersion.cs
+++ src/Core/SharedKernel/WB.Core.SharedKernel.Structures/Api/Designer/ApiVersion.cs
@@ -3,6 +3,6 @@ namespace WB.Core.SharedKernels.SurveySolutions.Api.Designer
     public static class ApiVersion
     {
         public const int CurrentTesterProtocolVersion = 20;
-        public const int MaxQuestionnaireVersion = 17;
+        public const int MaxQuestionnaireVersion = 18;
     }
 }
