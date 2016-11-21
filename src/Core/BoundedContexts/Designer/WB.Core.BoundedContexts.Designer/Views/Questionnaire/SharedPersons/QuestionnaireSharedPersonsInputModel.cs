b2671f85eded74f0e1baa57af38aafb6faf2b480
Andrii Kozhyn <hedgic@gmail.com>
2016-11-21 16:46:54 +0200

    KP-8160: Completely removed usages of QuestionnaireSharedPersons

--- src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Views/Questionnaire/SharedPersons/QuestionnaireSharedPersonsInputModel.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace WB.Core.BoundedContexts.Designer.Views.Questionnaire.SharedPersons
-{
-    public class QuestionnaireSharedPersonsInputModel
-    {
-        public Guid QuestionnaireId { get; set; }
-    }
-}
