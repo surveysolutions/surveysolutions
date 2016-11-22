1723e449971bf7275713c0ce89d1e78238e08e91
Roman Lytvyn <litvin.roma@gmail.com>
2016-11-22 16:45:43 +0200

    T: fixed nullable variable values. KP-8191

--- src/Core/SharedKernels/Enumerator/Enumerator/ViewModels/InterviewDetails/VariableViewModel.cs
+++ src/Core/SharedKernels/Enumerator/Enumerator/ViewModels/InterviewDetails/VariableViewModel.cs
@@ -53,7 +53,10 @@ namespace WB.Core.SharedKernels.Enumerator.ViewModels.InterviewDetails
             this.registry.Subscribe(this, interviewId);
         }
 
-        public string Text => this.variableName + ": " + (this.variableValue.ToString() ?? UIResources.VariableEmptyValue);
+        public string Text
+            => $"{this.variableName} : " +
+               $"{(string.IsNullOrEmpty(this.variableValue?.ToString()) ? UIResources.VariableEmptyValue : this.variableValue?.ToString())}";
+
         public string Label => this.variableLabel;
         public bool HasLabel => !string.IsNullOrEmpty(this.variableLabel);
 
