08c135f1f52d8f6b749cc9b0d256d8ba1172d073
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2016-11-22 15:19:45 -0500

    tests KP-8096 HQ: Categorical questions linked to list. Interview details

---
+++ src/Tests/WB.Tests.Unit/SharedKernels/Enumerator/StatefulInterviewTests/when_options_for_linked_to_list_question_changed.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Machine.Specifications;
+using Main.Core.Entities.Composite;
+using WB.Core.SharedKernels.DataCollection;
+using WB.Core.SharedKernels.Enumerator.Implementation.Aggregates;
+
+namespace WB.Tests.Unit.SharedKernels.Enumerator.StatefulInterviewTests
+{
+    internal class when_options_for_linked_to_list_question_changed : StatefulInterviewTestsContext
+    {
+        Establish context = () =>
+        {
+            linkedQuestionId = Guid.Parse("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+            linkSourceId = Guid.Parse("BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB");
+
+            linkedQuestionIdentity = Create.Entity.Identity(linkedQuestionId, RosterVector.Empty);
+
+            var questionnaire =
+                Create.Entity.PlainQuestionnaire(Create.Entity.QuestionnaireDocumentWithOneChapter(new IComposite[]
+                {
+                    Create.Entity.TextListQuestion(questionId: linkSourceId),
+                    Create.Entity.MultyOptionsQuestion(linkedQuestionId, linkedToQuestionId: linkSourceId)
+                }));
+
+            interview = Create.AggregateRoot.StatefulInterview(questionnaire: questionnaire);
+            
+        };
+
+        Because of = () => interview.Apply(Create.Event.TextListQuestionAnswered(linkSourceId, RosterVector.Empty, new[] { new Tuple<decimal, string>(1, "one"), }));
+
+        It should_calculate_state_of_options_for_linked_question = () =>
+        {
+            interview.GetMultiOptionLinkedToListQuestion(linkedQuestionIdentity)
+                .Options.Count.ShouldEqual(1);
+        };
+
+        static StatefulInterview interview;
+        static Guid linkedQuestionId;
+        static Guid linkSourceId;
+        static Identity linkedQuestionIdentity;
+    }
+}
