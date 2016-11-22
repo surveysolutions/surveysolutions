ec99e62fa73e5fd3435a5fe0d2921c30cab57c4e
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2016-11-22 18:37:06 -0500

    KP-8186 An error occured when filtering interview

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Views/Interview/InterviewDetails/InterviewEntityViewFactory.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Views/Interview/InterviewDetails/InterviewEntityViewFactory.cs
@@ -109,7 +109,7 @@ namespace WB.Core.BoundedContexts.Headquarters.Views.Interview
                 categoricalTypes.Contains(questionView.QuestionType) &&
                 !string.IsNullOrEmpty(question.Properties.OptionsFilterExpression);
 
-            if (question.Answers != null && question.Answers.Count > 0)
+            if (question.Answers != null && questionView.Options == null)
             {
                 questionView.Options = question.Answers.Select(a => new QuestionOptionView
                 {
