48d6cf19551f90dca5915d0329b1e3a2f4d8e7b8
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2016-11-23 13:49:41 -0500

    test : KP-8186 An error occured when filtering interview

---
+++ src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/InterviewEntityViewFactoryTests/when_creating_InterviewQuestionView_for_not_answered_TextList_question.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Machine.Specifications;
+using Main.Core.Entities.SubEntities.Question;
+using WB.Core.BoundedContexts.Headquarters.Views.Interview;
+using WB.Core.SharedKernels.DataCollection.ValueObjects.Interview;
+using WB.Core.SharedKernels.DataCollection.Views.Interview;
+using WB.Tests.Unit.SharedKernels.SurveyManagement.InterviewQuestionViewTests;
+
+namespace WB.Tests.Unit.SharedKernels.SurveyManagement.InterviewEntityViewFactoryTests
+{
+    internal class when_creating_InterviewQuestionView_for_not_answered_TextList_question : InterviewEntityViewFactoryTestsContext
+    {
+        Establish context = () =>
+        {
+            textListQuestionTemplate = new TextListQuestion("text list");
+            textListQuestionData = new InterviewQuestion(textListQuestionId)
+            {
+                Answer = new InterviewTextListAnswers(new List<Tuple<decimal, string>>())
+            };
+            interviewEntityViewFactory = CreateInterviewEntityViewFactory();
+        };
+
+        Because of = () => textListQuestionView = interviewEntityViewFactory.BuildInterviewQuestionView(textListQuestionTemplate, 
+            textListQuestionData, null, false, new decimal[0], InterviewStatus.Completed);
+
+        It should_answer_be_null = () =>
+            textListQuestionView.Answer.ShouldBeNull();
+
+        It should_have_options_not_null = () =>
+            textListQuestionView.Options.ShouldNotBeNull();
+        
+        private static IInterviewEntityViewFactory interviewEntityViewFactory;
+        private static InterviewQuestionView textListQuestionView;
+        private static TextListQuestion textListQuestionTemplate;
+        private static InterviewQuestion textListQuestionData;
+        private static Guid textListQuestionId = Guid.Parse("44444444444444444444444444444444");
+    }
+}
