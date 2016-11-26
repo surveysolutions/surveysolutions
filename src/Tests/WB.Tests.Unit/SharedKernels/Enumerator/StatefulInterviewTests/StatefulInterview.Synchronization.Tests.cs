a44754445650ec9cb4f27ccaec4a82fa4c49e511
Roman Lytvyn <litvin.roma@gmail.com>
2016-11-26 13:42:48 +0200

    IN: fixed: Answer on linked question with list source after interview reject  crashes Interviewer KP-8229

--- src/Tests/WB.Tests.Unit/SharedKernels/Enumerator/StatefulInterviewTests/StatefulInterview.Synchronization.Tests.cs
+++ src/Tests/WB.Tests.Unit/SharedKernels/Enumerator/StatefulInterviewTests/StatefulInterview.Synchronization.Tests.cs
@@ -28,5 +28,36 @@ namespace WB.Tests.Unit.SharedKernels.Enumerator.StatefulInterviewTests
             Assert.That(questionComments, Is.Not.Empty);
             Assert.That(questionComments, Has.Count.EqualTo(1));
         }
+
+        [Test]
+        public void When_rejected_interview_has_linked_to_list_and_text_list_question_is_answered_Should_linked_to_list_question_has_options()
+        {
+            //arrange
+            var textListQuestionId = Guid.Parse("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+            var multiLinkedToListId = Guid.Parse("BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB");
+
+            var questionnaire = Create.Entity.QuestionnaireDocumentWithOneChapter(
+                Create.Entity.TextListQuestion(textListQuestionId),
+                Create.Entity.FixedRoster(fixedTitles: new[] {Create.Entity.FixedTitle(1)},
+                    children: new[]
+                    {
+                        Create.Entity.MultyOptionsQuestion(multiLinkedToListId, linkedToQuestionId: textListQuestionId)
+                    }));
+
+            var interview = Setup.StatefulInterview(questionnaire);
+
+            var answerOnTextList = Create.Entity.AnsweredQuestionSynchronizationDto(questionId: textListQuestionId,
+                answer: new[] {new Tuple<decimal, string>(1, "option 1"), new Tuple<decimal, string>(5, "option 2")});
+
+            var syncDto = Create.Entity.InterviewSynchronizationDto(answers: new[] { answerOnTextList });
+
+            //act
+            interview.SynchronizeInterview(Guid.NewGuid(), syncDto);
+
+            //assert
+            var multiLinkedToList = interview.GetMultiOptionLinkedToListQuestion(Create.Entity.Identity(multiLinkedToListId, Create.Entity.RosterVector(1)));
+
+            Assert.That(multiLinkedToList.Options, Is.EquivalentTo(new[] {1, 5}));
+        }
     }
 }
