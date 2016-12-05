c854eac196c98229cf8a1eb01db8f17164227275
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2016-12-05 13:29:51 +0200

    KP-8270: Implemented permissions check for gps question

--- src/UI/Shared/WB.UI.Shared.Enumerator/Activities/BaseInterviewActivity.cs
+++ src/UI/Shared/WB.UI.Shared.Enumerator/Activities/BaseInterviewActivity.cs
@@ -1,11 +1,14 @@
 ﻿using Android.App;
 using Android.Content;
+using Android.Content.PM;
 using Android.OS;
 using Android.Support.V4.Widget;
 using Android.Support.V7.App;
 using Android.Views;
 using MvvmCross.Platform;
 using MvvmCross.Plugins.Messenger;
+using Plugin.CurrentActivity;
+using Plugin.Permissions;
 using WB.Core.SharedKernels.Enumerator.ViewModels.InterviewDetails;
 
 namespace WB.UI.Shared.Enumerator.Activities
@@ -36,6 +39,14 @@ namespace WB.UI.Shared.Enumerator.Activities
                 var viewModel = this.ViewModel;
                 viewModel.Sections.UpdateStatuses.Execute(null); // for some reason custom binding on drawerlayout is not working. 
             };
+
+            CrossCurrentActivity.Current.Activity = this;
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            CrossCurrentActivity.Current.Activity = this;
         }
 
         protected override void OnStart()
@@ -57,6 +68,12 @@ namespace WB.UI.Shared.Enumerator.Activities
                 this.ViewModel.NavigateBack();
                 this.Finish();
             });
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
+        {
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
 
         public override bool OnOptionsItemSelected(IMenuItem item)
