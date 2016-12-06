4a7690fff538837256fd186366ee0db8d07a6ed7
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2016-12-06 12:18:26 +0200

    KP-8271: Added implementation for picture chooser

--- src/Infrastructure/Shared/Enumerator/WB.Infrastructure.Shared.Enumerator/Internals/PictureChooser.cs
+++ src/Infrastructure/Shared/Enumerator/WB.Infrastructure.Shared.Enumerator/Internals/PictureChooser.cs
@@ -1,12 +1,30 @@
-﻿using WB.Core.SharedKernels.Enumerator.Services;
+﻿using System.IO;
+using System.Threading.Tasks;
+using Plugin.Media.Abstractions;
+using WB.Core.SharedKernels.Enumerator.Implementation.Services;
+using WB.Core.SharedKernels.Enumerator.Services;
 
 namespace WB.Infrastructure.Shared.Enumerator.Internals
 {
-    public class PictureChooser : IPictureChooser
+    internal class PictureChooser : IPictureChooser
     {
-        public void TakePicture()
-        {
+        private readonly IMedia media;
 
+        public PictureChooser(IMedia media)
+        {
+            this.media = media;
+        }
+
+        public async Task<Stream> TakePicture()
+        {
+            await this.media.Initialize().ConfigureAwait(false);
+            var storeCameraMediaOptions = new StoreCameraMediaOptions
+            {
+                CompressionQuality = 95,
+                PhotoSize = PhotoSize.Full
+            };
+            var photo = await this.media.TakePhotoAsync(storeCameraMediaOptions).ConfigureAwait(false);
+            return photo?.GetStream();
         }
     }
 }
