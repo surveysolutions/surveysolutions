c0299cbc3d3ebaa9dfa32a4ab4f45a9fb9ed4687
Andrii Kozhyn <hedgic@gmail.com>
2016-12-06 11:19:46 +0200

    KP-8271: Added generic service for pictures

---
+++ src/Infrastructure/Shared/Enumerator/WB.Infrastructure.Shared.Enumerator/Internals/PictureChooser.cs
@@ -0,0 +1,12 @@
+﻿using WB.Core.SharedKernels.Enumerator.Services;
+
+namespace WB.Infrastructure.Shared.Enumerator.Internals
+{
+    public class PictureChooser : IPictureChooser
+    {
+        public void TakePicture()
+        {
+
+        }
+    }
+}
