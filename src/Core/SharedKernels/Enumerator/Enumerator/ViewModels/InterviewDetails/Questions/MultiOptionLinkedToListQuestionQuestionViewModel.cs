263118c28194cd67524712a04cb269b91539ffa2
Roman Lytvyn <litvin.roma@gmail.com>
2016-12-07 16:35:35 +0200

    EN: code review changes by: Interviewer/Tester crashes after removing list answer that is not currently selected in Linked question KP-8220

--- src/Core/SharedKernels/Enumerator/Enumerator/ViewModels/InterviewDetails/Questions/MultiOptionLinkedToListQuestionQuestionViewModel.cs
+++ src/Core/SharedKernels/Enumerator/Enumerator/ViewModels/InterviewDetails/Questions/MultiOptionLinkedToListQuestionQuestionViewModel.cs
@@ -292,7 +292,7 @@ namespace WB.Core.SharedKernels.Enumerator.ViewModels.InterviewDetails.Questions
 
             foreach (var removedOptionValue in removedOptionValues)
             {
-                var removedOption = this.options.FirstOrDefault(option => option.Value == removedOptionValue);
+                var removedOption = this.options.SingleOrDefault(option => option.Value == removedOptionValue);
                 if (removedOption == null) continue;
 
                 this.mainThreadDispatcher.RequestMainThreadAction(() => this.options.Remove(removedOption));
