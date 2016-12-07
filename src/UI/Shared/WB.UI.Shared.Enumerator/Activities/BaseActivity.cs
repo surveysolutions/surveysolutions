84ff8dfa1b7d0946f23c819ccffecff453182845
Andrii Kozhyn <hedgic@gmail.com>
2016-12-07 15:37:31 +0200

    KP-8271: Moved nlog configuration to code

--- src/UI/Shared/WB.UI.Shared.Enumerator/Activities/BaseActivity.cs
+++ src/UI/Shared/WB.UI.Shared.Enumerator/Activities/BaseActivity.cs
@@ -1,6 +1,8 @@
 using Android.OS;
 using MvvmCross.Droid.Platform;
 using MvvmCross.Droid.Support.V7.AppCompat;
+using Plugin.CurrentActivity;
+using Plugin.Permissions;
 using WB.Core.SharedKernels.Enumerator.ViewModels;
 
 namespace WB.UI.Shared.Enumerator.Activities
@@ -13,8 +15,20 @@ namespace WB.UI.Shared.Enumerator.Activities
         {
             var setup = MvxAndroidSetupSingleton.EnsureSingletonAvailable(ApplicationContext);
             setup.EnsureInitialized();
-
+            CrossCurrentActivity.Current.Activity = this;
             base.OnCreate(bundle);
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            CrossCurrentActivity.Current.Activity = this;
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Android.Content.PM.Permission[] grantResults)
+        {
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
 
         protected override void OnViewModelSet()
