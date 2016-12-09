01a172c8a15cafa144f43b554c6f32d89e9e7cff
Slava Scherbak <slava.scherbak@gmail.com>
2016-12-09 15:57:11 -0500

    HQ: added active page, added footer, changed gulp script.  KP-8249

--- src/UI/Headquarters/WB.UI.Headquarters/Views/Shared/HtmlExtensions.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/Views/Shared/HtmlExtensions.cs
@@ -2,7 +2,9 @@
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Mvc.Html;
+using Resources;
 using WB.Core.SharedKernels.SurveyManagement.Web.Models;
+using WB.UI.Headquarters.Resources;
 
 namespace ASP
 {
@@ -17,6 +19,41 @@ namespace ASP
             MvcHtmlString part2 = html.ActionLink(linkText, actionName, controllerName, new {area = ""}, new object());
 
             return new MvcHtmlString(liStartTag + part2 + "</li>");
+        }
+
+        public static IHtmlString ActivePage(this HtmlHelper html)
+        {
+            MenuItem page = html.ViewBag.ActivePage ?? MenuItem.Logon;
+            return new MvcHtmlString(GetMenuItemTitle(page));
+        }
+
+        private static string GetMenuItemTitle(MenuItem page)
+        {
+            switch (page)
+            {
+                case MenuItem.Surveys: return @MainMenu.SurveysAndStatuses;
+                case MenuItem.ApiUsers: return @MainMenu.ApiUsers;
+                case MenuItem.DataExport: return @MainMenu.DataExport;
+                case MenuItem.Docs: return @MainMenu.Interviews;
+                case MenuItem.Headquarters: return @MainMenu.Headquarters;
+                case MenuItem.Interviewers: return @MainMenu.Interviewers;
+                case MenuItem.InterviewsChart: return @MainMenu.CumulativeChart;
+                case MenuItem.ManageAccount: return Strings.SurverManagement_MainMenu_ManageAccount;
+                case MenuItem.MapReport: return @MainMenu.MapReport;
+                case MenuItem.NumberOfCompletedInterviews: return @MainMenu.Quantity;
+                case MenuItem.SpeedOfCompletingInterviews: return @MainMenu.Speed;
+                case MenuItem.Observers: return @MainMenu.Observers;
+                case MenuItem.Questionnaires: return @MainMenu.Questionnaires;
+                case MenuItem.Settings: return Resources.Common.Settings;
+                case MenuItem.Summary: return @MainMenu.TeamsAndStatuses;
+                case MenuItem.Teams: return @MainMenu.Supervisors;
+                case MenuItem.UserBatchUpload: return @MainMenu.UserBatchUpload;
+                case MenuItem.Statuses: return @MainMenu.SurveysAndStatuses;
+                case MenuItem.Logon: return "Logon";
+                case MenuItem.Devices: return "Devices";
+                case MenuItem.SyncLog: return "SyncLog";
+                default: return String.Empty;
+            }
         }
     }
 }
