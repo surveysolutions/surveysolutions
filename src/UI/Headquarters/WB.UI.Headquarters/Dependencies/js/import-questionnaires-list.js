9a336d313094c776d7a54b298def2832a9657f50
Slava Scherbak <slava.scherbak@gmail.com>
2016-12-12 18:20:26 -0500

    HQ: fixed page margins and padding. extracted some variables. KP-8252

--- src/UI/Headquarters/WB.UI.Headquarters/Dependencies/js/import-questionnaires-list.js
+++ src/UI/Headquarters/WB.UI.Headquarters/Dependencies/js/import-questionnaires-list.js
@@ -6,33 +6,46 @@ $(function () {
     var requestHeaders = {};
     requestHeaders[input.settings.acsrf.tokenName] = input.settings.acsrf.token;
 
-    $('table.import-interview').DataTable({
-        processing: true,
-        serverSide: true,
-        ajax: {
-            url: questionnaireListUrl,
-            type: "POST",
-            headers: requestHeaders
-        },
-        "columns": [
-            {
-                data: "title",
-                "class": ""
+    $('table.import-interview')
+        .on('init.dt', function () {
+            $('#DataTables_Table_0_filter label').on('click', function (e) {
+                if (e.target !== this)
+                    return;
+                if ($(this).hasClass("active")) {
+                    $(this).removeClass("active");
+                }
+                else {
+                    $(this).addClass("active");
+                }
+                $(".column-questionnaire-title").toggleClass("padding-left-lide");
+            });
+        })
+        .DataTable({
+            processing: true,
+            serverSide: true,
+            ajax: {
+                url: questionnaireListUrl,
+                type: "POST",
+                headers: requestHeaders
             },
-            {
-                data:
+            "columns": [
                 {
-                    _: "lastModified.display",
-                    sort: "lastModified.timestamp"
+                    data: "title"
                 },
-                "class": "changed-recently"
-            },
-            {
-                data: "createdBy",
-                "class": "created-by"
-            }
-        ],
-        "pagingType": "full_numbers",
-        "lengthChange": false,
-    });
+                {
+                    data:
+                    {
+                        _: "lastModified.display",
+                        sort: "lastModified.timestamp"
+                    },
+                    "class": "changed-recently"
+                },
+                {
+                    data: "createdBy",
+                    "class": "created-by"
+                }
+            ],
+            "pagingType": "full_numbers",
+            "lengthChange": false,
+        });
 });
