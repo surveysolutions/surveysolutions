1e3c0264d151d18623f63770418f0c2c378a170b
Slava Scherbak <slava.scherbak@gmail.com>
2016-12-12 15:41:33 -0500

    HQ: added draft version of inport questionnaire page. KP-8252

---
+++ src/UI/Headquarters/WB.UI.Headquarters/Dependencies/js/import-questionnaires-list.js
@@ -0,0 +1,27 @@
+﻿'use strict';
+
+$(function () {
+    var questionnaireListUrl = $('#questionnaireListUrl').attr('href');
+
+    var requestHeaders = {};
+    requestHeaders[input.settings.acsrf.tokenName] = input.settings.acsrf.token;
+
+    $('table.import-interview').DataTable({
+        processing: true,
+        serverSide: true,
+        ajax: {
+            url: questionnaireListUrl,
+            type: "POST",
+            headers: requestHeaders
+        },
+        "columns": [
+            { data: "title" },
+            { data: "lastModified" },
+            { data: {
+                    _: "lastModified.display",
+                    sort: "lastModified.timestamp"
+            } },
+            { data: "createdBy" }
+        ]
+    });
+});
