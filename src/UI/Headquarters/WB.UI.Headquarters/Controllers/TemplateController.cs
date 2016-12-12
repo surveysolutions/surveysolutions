16b922a4693f666fa9b25a00970fd3b379e598d5
Slava Scherbak <slava.scherbak@gmail.com>
2016-12-12 17:54:41 -0500

    HQ: returned old import page. KP-8252

--- src/UI/Headquarters/WB.UI.Headquarters/Controllers/TemplateController.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/Controllers/TemplateController.cs
@@ -50,7 +50,7 @@ namespace WB.UI.Headquarters.Controllers
             }
 
             return
-                this.View();
+                this.View("ImportOld");
         }
 
         public ActionResult LoginToDesigner()
