9f10b9a3608fe154f71bbae8b0d2a16a0412fa89
Slava Scherbak <slava.scherbak@gmail.com>
2016-12-13 17:27:22 -0500

    HQ: moved string to  recources. KP-8252

---
+++ src/UI/Headquarters/WB.UI.Headquarters/Models/Api/DataTableResponseModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+
+namespace WB.UI.Headquarters.Models.Api
+{
+    public class DataTableResponse<T>
+    {
+        public int Draw { get; set; }
+        public int RecordsTotal { get; set; }
+        public int RecordsFiltered { get; set; }
+        public IEnumerable<T> Data { get; set; }
+    }
+
+    public class QuestionnaireToBeImported
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string LastModified { get; set; }
+        public string CreatedBy { get; set; }
+    }
+}
