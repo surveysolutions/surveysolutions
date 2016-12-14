7eb5a2f2bb20529bc992722f2e55d428652956d3
Roman Lytvyn <litvin.roma@gmail.com>
2016-12-14 15:40:05 +0200

    HQ: fixed preloading verifier for yes no questions which used as roster size questions. KP-8335

---
+++ src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/PreloadedDataServiceTests/when_GetAvalibleIdListForParent_is_called_for_file_corresponding_to_roster_triggered_by_yesno_question.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Main.Core.Entities.SubEntities;
+using NUnit.Framework;
+using WB.Core.BoundedContexts.Headquarters.Views.PreloadedData;
+using WB.Core.SharedKernels.DataCollection.ValueObjects;
+
+namespace WB.Tests.Unit.SharedKernels.SurveyManagement.PreloadedDataServiceTests
+{
+    internal class when_GetAvalibleIdListForParent_is_called_for_file_corresponding_to_roster_triggered_by_yesno_question : PreloadedDataServiceTestContext
+    {
+        [Test]
+        public void should_not_throw_invalid_cast_exception()
+        {
+            //arrange
+            Guid ynQuestionId = Guid.Parse("11111111111111111111111111111111");
+            var ynVariable = "yn1";
+
+            var questionnaireDocument =
+                CreateQuestionnaireDocumentWithOneChapter(
+                    Create.Entity.YesNoQuestion(ynQuestionId, variable: ynVariable, answers: new []{1}),
+                    Create.Entity.Roster(rosterSizeSourceType: RosterSizeSourceType.Question, rosterSizeQuestionId: ynQuestionId));
+            
+            //act
+            var preloadedDataService = CreatePreloadedDataService(questionnaireDocument);
+
+            // assert
+            Assert.DoesNotThrow(() => preloadedDataService.GetAvailableIdListForParent(
+                CreatePreloadedDataByFile(new[] {"Id", ynVariable + "__1"}, new[] {new[] {"1", "1"}},
+                    questionnaireDocument.Title), new ValueVector<Guid> {ynQuestionId}, new[] {"1"},
+                new PreloadedDataByFile[0]));
+        }
+    }
+}
