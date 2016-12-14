27568be96991411854119e89aeb638a623c3c9e5
Andrii Kozhyn <hedgic@gmail.com>
2016-12-14 13:24:23 +0200

    KP-8271: Handle permissions exception on multimedia question

--- src/Core/SharedKernels/Enumerator/Enumerator/Properties/UIResources.Designer.cs
+++ src/Core/SharedKernels/Enumerator/Enumerator/Properties/UIResources.Designer.cs
@@ -244,7 +244,7 @@ namespace WB.Core.SharedKernels.Enumerator.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Application needs to have a permission to use location of current device in order to answer this question. Retry answering question and allow access. If you don&apos;t see permissions popup than go to Settings to allow application to use device location..
+        ///   Looks up a localized string similar to Application needs to have a permission to use location of current device in order to answer this question. Retry answering question and allow access. If you don&apos;t see permissions popup than go to Settings and allow application to use device location..
         /// </summary>
         public static string GpsQuestion_MissingPermissions {
             get {
@@ -856,7 +856,7 @@ namespace WB.Core.SharedKernels.Enumerator.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Application needs to have a permission to use camera in order to answer this question. Retry answering question and allow access. If you don&apos;t see permissions popup than go to Settings to allow application to use camera..
+        ///   Looks up a localized string similar to Application needs to have a permission to use camera in order to answer this question. Retry answering question and allow access. If you don&apos;t see permissions popup than go to Settings and allow application to use camera..
         /// </summary>
         public static string MissingPermissions_Camera {
             get {
