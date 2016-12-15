8a0f1571fcc6259462d2ecb1b18142f0d25d3df3
Andrii Kozhyn <hedgic@gmail.com>
2016-12-15 13:09:13 +0200

    KP-8342: Show single error message for invalid credentials

--- src/UI/Headquarters/WB.UI.Headquarters/Models/LogOnModel.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/Models/LogOnModel.cs
@@ -11,14 +11,12 @@ namespace WB.Core.SharedKernels.SurveyManagement.Web.Models
         /// <summary>
         /// Gets or sets UserName.
         /// </summary>
-        [Required(ErrorMessageResourceName = nameof(FieldsAndValidations.RequiredUserNameErrorMessage), ErrorMessageResourceType = typeof(FieldsAndValidations))]
         [Display(ResourceType = typeof(FieldsAndValidations), Name = nameof(FieldsAndValidations.LogOnModel_UserName))]
         public string UserName { get; set; }
 
         /// <summary>
         /// Gets or sets Password.
         /// </summary>
-        [Required(ErrorMessageResourceName = nameof(FieldsAndValidations.RequiredPasswordErrorMessage), ErrorMessageResourceType = typeof(FieldsAndValidations))]
         [DataType(DataType.Password)]
         [Display(ResourceType = typeof(FieldsAndValidations), Name = nameof(FieldsAndValidations.LogOnModel_Password))]
         public string Password { get; set; }
