7df73586eb6b56e8879b731221ed477e4a72fe01
Roman Lytvyn <litvin.roma@gmail.com>
2016-12-16 19:21:09 +0200

    EN: add/remove roster optimization. fixed tests. part 2 KP-8236

--- src/Tests/WB.Tests.Unit/SharedKernels/Enumerator/ViewModels/SectionsViewModelTests/when_roster_instances_added.cs
+++ src/Tests/WB.Tests.Unit/SharedKernels/Enumerator/ViewModels/SectionsViewModelTests/when_roster_instances_added.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using Machine.Specifications;
+using Main.Core.Entities.Composite;
 using Moq;
 using NSubstitute;
+using WB.Core.GenericSubdomains.Portable;
 using WB.Core.SharedKernels.DataCollection;
 using WB.Core.SharedKernels.DataCollection.Aggregates;
 using WB.Core.SharedKernels.Enumerator.Aggregates;
+using WB.Core.SharedKernels.Enumerator.Implementation.Aggregates;
 using WB.Core.SharedKernels.Enumerator.ViewModels.InterviewDetails;
 using It = Machine.Specifications.It;
 
@@ -15,54 +18,52 @@ namespace WB.Tests.Unit.SharedKernels.Enumerator.ViewModels.SectionsViewModelTes
     {
         Establish context = () =>
         {
-            interview = Substitute.For<IStatefulInterview>();
-            sectionIdentity = new Identity(sectionId, Empty.RosterVector);
-            addedInstanceIdentity = new Identity(rosterId, new[] {3m});
-            interview.GetParentGroup(addedInstanceIdentity)
-                .Returns(sectionIdentity);
+            interviewerId = Guid.Parse("22222222222222222222222222222222");
+            textListQuestionId = Guid.Parse("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
 
-            interview.GetEnabledSubgroups(sectionIdentity)
-               .Returns(new List<Identity> {
-                    new Identity(group1Id, Empty.RosterVector),
-                    new Identity(rosterId, new []{0m}),
-                    new Identity(rosterId, new []{1m}),
-                    new Identity(group2Id, Empty.RosterVector)
-                });
-            interview.IsEnabled(Moq.It.IsAny<Identity>()).ReturnsForAnyArgs(true);
+            var chapterId = Guid.Parse("DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD");
 
-            var questionnaire = Mock.Of<IQuestionnaire>(_ 
-                => _.GetAllSections() == listOfSections);
+            var questionnaire = Create.Entity.QuestionnaireDocumentWithOneChapter(chapterId,
+                Create.Entity.Group(group1Id),
+                Create.Entity.TextListQuestion(textListQuestionId),
+                Create.Entity.Roster(rosterId, rosterSizeQuestionId: textListQuestionId, children: new IComposite[]
+                {
+                    Create.Entity.TextQuestion()
+                }),
+                Create.Entity.Group(group2Id));
 
-            viewModel = CreateSectionsViewModel(questionnaire, interview);
-            viewModel.Init("", Create.Entity.QuestionnaireIdentity(), Create.Other.NavigationState());
-            viewModel.Sections[0].SectionIdentity = new Identity(sectionId, Empty.RosterVector);
+            interview = Setup.StatefulInterview(questionnaire);
+            interview.AnswerTextListQuestion(interviewerId, textListQuestionId, RosterVector.Empty, DateTime.Now,
+                new[] {Tuple.Create(0m, "zero"), Tuple.Create(1m, "one")});
+
+            viewModel = CreateSectionsViewModel(Create.Entity.PlainQuestionnaire(questionnaire), interview);
+
+            var questionnaireIdentity = Create.Entity.QuestionnaireIdentity();
+            var navigationState = Create.Other.NavigationState();
+            navigationState.Init(interview.Id.FormatGuid(), questionnaireIdentity.ToString());
+            
+            viewModel.Init(interview.Id.FormatGuid(), questionnaireIdentity, navigationState);
+            viewModel.Sections[0].SectionIdentity = new Identity(chapterId, Empty.RosterVector);
+            viewModel.Sections[0].Expanded = true;
         };
 
         Because of = () =>
         {
-             interview.GetEnabledSubgroups(sectionIdentity)
-               .Returns(new List<Identity> {
-                    new Identity(group1Id, Empty.RosterVector),
-                    new Identity(rosterId, new []{0m}),
-                    new Identity(rosterId, new []{1m}),
-                    new Identity(rosterId, new []{2m}),
-                    new Identity(group2Id, Empty.RosterVector)
-                });
-            viewModel.Handle(Create.Event.RosterInstancesAdded(addedInstanceIdentity.Id, addedInstanceIdentity.RosterVector));
+            interview.AnswerTextListQuestion(interviewerId, textListQuestionId, RosterVector.Empty, DateTime.Now,
+               new[] { Tuple.Create(0m, "zero"), Tuple.Create(1m, "one"), Tuple.Create(2m, "two")});
+            viewModel.Handle(Create.Event.RosterInstancesAdded(rosterId, Create.Entity.RosterVector(2)));
         };
 
         It should_add_roster_into_a_tree = () => 
-            viewModel.Sections.Second().Children.Count.ShouldEqual(5);
+            viewModel.Sections[0].Children.Count.ShouldEqual(4);
 
         static SideBarSectionsViewModel viewModel;
-        static IStatefulInterview interview;
-        private static Identity sectionIdentity;
-        private static Identity addedInstanceIdentity;
+        static StatefulInterview interview;
         private static Guid rosterId = Guid.Parse("BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB");
         private static Guid group1Id = Guid.Parse("CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC");
         private static Guid group2Id = Guid.Parse("11111111111111111111111111111111");
-        private static readonly Guid sectionId = Guid.Parse("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-        private static readonly List<Guid> listOfSections = new List<Guid> { sectionId };
+        private static Guid interviewerId;
+        private static Guid textListQuestionId;
     }
 }
 
