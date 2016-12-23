e29d0c4e3526d579074bb09fa722d1ff420af554
Pavel Stovpivskyi <sby@rambler.ru>
2016-12-23 17:17:37 +0200

    added password for binary data  and ddi metadata
    KP-8386

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/DataExport/Ddi/IDdiMetadataAccessor.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/DataExport/Ddi/IDdiMetadataAccessor.cs
@@ -5,5 +5,7 @@ namespace WB.Core.BoundedContexts.Headquarters.DataExport.Ddi
     public interface IDdiMetadataAccessor
     {
         string GetFilePathToDDIMetadata(QuestionnaireIdentity questionnaireId);
+
+        void ClearFiles();
     }
 }
