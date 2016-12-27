4a8888bcbdee1dc601748b20f43ee4bdf2076259
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2016-12-27 13:23:25 +0200

    KP-8399: Added only build script

---
+++ build/build-only.ps1
@@ -0,0 +1,59 @@
+param([string]$VersionPrefix,
+[INT]$BuildNumber,
+[string]$KeystorePassword)
+
+$ErrorActionPreference = "Stop"
+
+$scriptFolder = (Get-Item $MyInvocation.MyCommand.Path).Directory.FullName
+
+. "$scriptFolder\functions.ps1"
+
+$ProjectDesigner = 'src\UI\Designer\WB.UI.Designer\WB.UI.Designer.csproj'
+$ProjectHeadquarters = 'src\UI\Headquarters\WB.UI.Headquarters\WB.UI.Headquarters.csproj'
+
+$versionString = (GetVersionString 'src\core')
+UpdateProjectVersion $BuildNumber -ver $versionString
+try {
+	$PackageName = 'WBCapi.apk'
+		. "$scriptFolder\build-android-package.ps1" `
+			-VersionName $versionString `
+			-VersionCode $BuildNumber `
+			-BuildConfiguration $BuildConfiguration `
+			-KeystorePassword $KeystorePassword `
+			-KeystoreName 'WBCapi.keystore' `
+			-KeystoreAlias 'wbcapipublish' `
+			-CapiProject 'src\UI\Interviewer\WB.UI.Interviewer\WB.UI.Interviewer.csproj' `
+			-OutFileName $PackageName | %{ if (-not $_) { Exit } }
+
+	RunConfigTransform $ProjectDesigner $BuildConfiguration
+	BuildStatiContent "src\UI\Designer\WB.UI.Designer\questionnaire" $false | %{ if (-not $_) { 
+		Write-Host "##teamcity[message status='ERROR' text='Unexpected error occurred in BuildStatiContent']"
+		Write-Host "##teamcity[buildProblem description='Failed to build static content for Designer']"
+		Exit 
+	}}
+	
+	BuildWebPackage $ProjectDesigner $BuildConfiguration | %{ if (-not $_) { Exit } }
+
+	RunConfigTransform $ProjectHeadquarters $BuildConfiguration
+	BuildStatiContent "src\UI\Headquarters\WB.UI.Headquarters\Dependencies" $true | %{ if (-not $_) {
+		Write-Host "##teamcity[message status='ERROR' text='Unexpected error occurred in BuildStatiContent']"
+		Write-Host "##teamcity[buildProblem description='Failed to build static content for HQ']"
+		Exit 
+	}}
+	CopyCapi -Project $ProjectHeadquarters -source $PackageName
+	BuildWebPackage $ProjectHeadquarters $BuildConfiguration | %{ if (-not $_) { Exit } }
+	
+	$artifactsFolder = (Get-Location).Path + "\Artifacts"
+	If (Test-Path "$artifactsFolder"){
+		Remove-Item "$artifactsFolder" -Force -Recurse
+	}
+	AddArtifacts $ProjectDesigner $BuildConfiguration -folder "Designer"
+	AddArtifacts $ProjectHeadquarters $BuildConfiguration -folder "Headquarters"
+
+	Write-Host "##teamcity[publishArtifacts '$artifactsFolder']"
+}
+catch {
+	Write-Host "##teamcity[message status='ERROR' text='Unexpected error occurred']"
+	Write-Host "##teamcity[buildProblem description='Unexpected error occurred']"
+	throw
+}
