4ff69f6628bd4573e3767f4605e16ca287bde5ce
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2016-12-28 11:41:27 +0200

    KP-8421: Allow administrator to access control panel

--- src/UI/Designer/WB.UI.Designer/Controllers/ControlPanelController.cs
+++ src/UI/Designer/WB.UI.Designer/Controllers/ControlPanelController.cs
@@ -1,17 +1,13 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Web.Mvc;
 using System.Web.Security;
 using WB.UI.Designer.Models;
-using WB.UI.Shared.Web.Attributes;
-using WB.UI.Shared.Web.Filters;
 using WB.UI.Shared.Web.Membership;
 using WB.UI.Shared.Web.Settings;
 
 namespace WB.UI.Designer.Controllers
 {
-    [AllowAnonymous]
-    [LocalOrDevelopmentAccessOnly]
+    [Authorize(Roles = "Administrator")]
     public class ControlPanelController : BaseController
     {
         readonly ISettingsProvider settingsProvider;
