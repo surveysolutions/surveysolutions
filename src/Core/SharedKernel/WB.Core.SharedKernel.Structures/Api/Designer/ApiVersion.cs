130d0ac9ee608846b03229113bcf22fe79324234
Andrii Kozhyn <hedgic@gmail.com>
2016-12-29 16:39:34 +0200

    require tester upgrade

--- src/Core/SharedKernel/WB.Core.SharedKernel.Structures/Api/Designer/ApiVersion.cs
+++ src/Core/SharedKernel/WB.Core.SharedKernel.Structures/Api/Designer/ApiVersion.cs
@@ -2,7 +2,7 @@ namespace WB.Core.SharedKernels.SurveySolutions.Api.Designer
 {
     public static class ApiVersion
     {
-        public const int CurrentTesterProtocolVersion = 21;
+        public const int CurrentTesterProtocolVersion = 22;
         public const int MaxQuestionnaireVersion = 19;
     }
 }
