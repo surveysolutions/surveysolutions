092c1f1feb8585aada534a312f0e88ed6245a4d8
Pavel Stovpivskyi <630198+spuv@users.noreply.github.com>
2016-12-30 16:59:33 +0200

    Added cache for company logo
    KP-8387 Company logo should support caching

--- src/UI/Headquarters/WB.UI.Headquarters/API/Resources/CompanyLogoController.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/API/Resources/CompanyLogoController.cs
@@ -1,8 +1,11 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 using System.IO;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Security.Cryptography;
+using System.Web;
 using System.Web.Http;
 using ImageResizer;
 using WB.Core.Infrastructure.PlainStorage;
@@ -29,17 +32,35 @@ namespace WB.UI.Headquarters.API.Resources
 
             if (companyLogo != null)
             {
+                var stringEtag = this.GetEtagValue(companyLogo);
+                var etag = $"\"{stringEtag}\"";
+
+                var incomingEtag = HttpContext.Current.Request.Headers[@"If-None-Match"];
+                if (string.Compare(incomingEtag, etag, StringComparison.InvariantCultureIgnoreCase) == 0)
+                    return new HttpResponseMessage(HttpStatusCode.NotModified);
+
                 var response = new HttpResponseMessage(HttpStatusCode.OK)
                 {
                     Content = new ByteArrayContent(GetTrasformedContent(companyLogo, defaultImageHeightToScale, defaultImageWidthToScale))
                 };
 
-                response.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("image/png");
+                response.Content.Headers.ContentType = MediaTypeHeaderValue.Parse(@"image/png");
+                response.Headers.ETag = new EntityTagHeaderValue(etag);
 
                 return response;
             }
 
             return Request.CreateResponse(HttpStatusCode.NoContent);
+        }
+
+        private string GetEtagValue(byte[] bytes)
+        {
+            using (var hasher = SHA1.Create())
+            {
+                var computeHash = hasher.ComputeHash(bytes);
+                string hash = BitConverter.ToString(computeHash).Replace("-", "");
+                return hash;
+            }
         }
 
         private static byte[] GetTrasformedContent(byte[] source, int? height = null, int? width = null)
