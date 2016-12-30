157fa8f46f99162582c35c2191f5189d1607de19
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2016-12-30 13:36:57 +0200

    StatefulInterview moved to DataCollection (namespaces kept) KP-8441

--- src/Core/SharedKernels/DataCollection/DataCollection/Events/Interview/InterviewAnswersFromSyncPackageRestored.cs
+++ src/Core/SharedKernels/DataCollection/DataCollection/Events/Interview/InterviewAnswersFromSyncPackageRestored.cs
@@ -1,18 +1,14 @@
 ﻿using System;
 using WB.Core.SharedKernels.DataCollection.Events.Interview.Base;
-using WB.Core.SharedKernels.Enumerator.DataTransferObjects;
 
 namespace WB.Core.SharedKernels.Enumerator.Events
 {
     [Obsolete("Since v6.0")]
     public class InterviewAnswersFromSyncPackageRestored : InterviewActiveEvent
     {
-        public InterviewAnswersFromSyncPackageRestored(InterviewAnswerDto[] answers, Guid userId)
+        public InterviewAnswersFromSyncPackageRestored(Guid userId)
             : base(userId)
         {
-            this.Answers = answers;
         }
-
-        public InterviewAnswerDto[] Answers { get; private set; }
     }
 }
