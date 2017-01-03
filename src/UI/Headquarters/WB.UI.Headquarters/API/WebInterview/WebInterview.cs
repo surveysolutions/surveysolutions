ee0c01d670152a32bb8d731aa063b95ff0467267
Roman Lytvyn <litvin.roma@gmail.com>
2017-01-03 14:29:42 +0200

    HQ: added statefull interview signalr hub. KP-8444

---
+++ src/UI/Headquarters/WB.UI.Headquarters/API/WebInterview/WebInterview.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNet.SignalR;
+using WB.Core.SharedKernels.DataCollection.Aggregates;
+using WB.Core.SharedKernels.DataCollection.Repositories;
+
+namespace WB.UI.Headquarters.API.WebInterview
+{
+    public partial class WebInterview : Hub
+    {
+        private readonly IStatefulInterviewRepository statefulInterviewRepository;
+        
+        private string interviewId => Context.ConnectionId;
+        private IStatefulInterview CurrentInterview => this.statefulInterviewRepository.Get(this.interviewId);
+
+        public WebInterview(IStatefulInterviewRepository statefulInterviewRepository)
+        {
+            this.statefulInterviewRepository = statefulInterviewRepository;
+        }
+    }
+}
