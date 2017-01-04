8d24335f1496d7ead240fd5cd80e7a350de0f32c
Andrii Kozhyn <hedgic@gmail.com>
2017-01-04 17:06:42 +0200

    KP-8461: Added route for list of questions

---
+++ src/UI/Headquarters/WB.UI.Headquarters.Interview/src/components/prefilled.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1></h1>
+        <ul>
+            <li v-for="question in prefilledQuestions">
+                {{question.title}}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script lang="ts">
+  import {mapGetters} from "vuex"
+
+  export default {
+    name: 'prefilled-view',
+    watch: {
+        "$route": (to, from) => {
+
+        }
+    },
+    computed: mapGetters(["prefilledQuestions"]),
+    beforeMount () {
+    //   this.$store.dispatch('InterviewMount', {
+    //     id: this.$route.params.id
+    //   })
+    }
+  }
+</script>
