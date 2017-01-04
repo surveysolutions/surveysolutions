c259675a06cd555c8ea13ede77789807c593f3f5
Victor Hladkikh <me@alfeg.net>
2017-01-04 13:20:21 +0200

    KP-8440 HQ: Web Interview. UI Setup # Whole stack moved to typescript. Source maps. Say no to `babel`

---
+++ src/UI/Headquarters/WB.UI.Headquarters.Interview/src/services/index.ts
@@ -0,0 +1,12 @@
+declare var require: any
+
+import { signalrPath } from './../config'
+
+require('jquery')
+
+var $script = require("scriptjs");
+$script(signalrPath, function() {
+  // ... HERE is SignalR available to configuration
+  // $.signalr.hub
+  
+});
