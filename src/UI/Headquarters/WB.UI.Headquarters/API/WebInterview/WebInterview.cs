a9ba392aeaa2ede761c195c7bfdcd8db3e18794f
Roman Lytvyn <litvin.roma@gmail.com>
2017-01-04 16:21:47 +0200

    HQ: Web Interview. Interview SignalR HUB. Remove old version of handling events KP-8443

--- src/UI/Headquarters/WB.UI.Headquarters/API/WebInterview/WebInterview.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/API/WebInterview/WebInterview.cs
@@ -1,62 +1,72 @@
-﻿using System.Linq;
+﻿using System;
 using System.Threading.Tasks;
 using Microsoft.AspNet.SignalR;
-using Ncqrs.Eventing.Sourcing;
+using Microsoft.AspNet.SignalR.Hubs;
+using WB.Core.BoundedContexts.Headquarters.Views.User;
+using WB.Core.GenericSubdomains.Portable;
 using WB.Core.GenericSubdomains.Portable.Services;
 using WB.Core.Infrastructure.CommandBus;
-using WB.Core.Infrastructure.EventBus.Lite;
 using WB.Core.SharedKernels.DataCollection.Aggregates;
-using WB.Core.SharedKernels.DataCollection.Implementation.Aggregates;
+using WB.Core.SharedKernels.DataCollection.Commands.Interview;
+using WB.Core.SharedKernels.DataCollection.Implementation.Entities;
 using WB.Core.SharedKernels.DataCollection.Repositories;
 using WB.UI.Shared.Web.CommandDeserialization;
 
 namespace WB.UI.Headquarters.API.WebInterview
 {
+    [HubName(@"interview")]
     public partial class WebInterview : Hub
     {
         private readonly IStatefulInterviewRepository statefulInterviewRepository;
-        private readonly ILiteEventBus liteEventBus;
         private readonly ICommandDeserializer commandDeserializer;
         private readonly ICommandService commandService;
         private readonly ILogger logger;
+        private readonly IUserViewFactory usersRepository;
 
-        private string interviewId => Context.ConnectionId;
+        private string interviewId;
         private IStatefulInterview CurrentInterview => this.statefulInterviewRepository.Get(this.interviewId);
 
         public WebInterview(
             IStatefulInterviewRepository statefulInterviewRepository, 
-            ILiteEventBus liteEventBus,
             ICommandDeserializer commandDeserializer,
             ICommandService commandService,
-            ILogger logger)
+            ILogger logger,
+            IUserViewFactory usersRepository)
         {
             this.statefulInterviewRepository = statefulInterviewRepository;
-            this.liteEventBus = liteEventBus;
             this.commandDeserializer = commandDeserializer;
             this.commandService = commandService;
             this.logger = logger;
+            this.usersRepository = usersRepository;
         }
 
-        public void StartInterview()
-            => this.liteEventBus.OnEventsPublished += LiteEventBus_OnEventsPublished;
+        public void CreateInterview(string questionnaireId, string interviewerName)
+        {
+            var questionnaireIdentity = QuestionnaireIdentity.Parse(questionnaireId);
+
+            var interviewer = this.usersRepository.Load(new UserViewInputModel(UserName: interviewerName, UserEmail: null));
+
+            var createInterviewOnClientCommand = new CreateInterviewOnClientCommand(Guid.NewGuid(),
+                interviewer.PublicKey, questionnaireIdentity, DateTime.UtcNow,
+                interviewer.Supervisor.Id);
+
+            this.commandService.Execute(createInterviewOnClientCommand);
+
+            this.interviewId = createInterviewOnClientCommand.Id.FormatGuid();
+            this.Clients.Caller.startInterview(this.interviewId);
+        }
+
+        public void StartInterview(string interviewId)
+        {
+            this.interviewId = interviewId;
+        }
 
 
         public override Task OnDisconnected(bool stopCalled)
         {
             // statefull interview can be removed from cache here
-            this.liteEventBus.OnEventsPublished -= LiteEventBus_OnEventsPublished;
 
             return base.OnDisconnected(stopCalled);
-        }
-
-        private void LiteEventBus_OnEventsPublished(PublishedEventsArgs args)
-        {
-            var interviewEvents = args.PublishedEvents
-                .Where(@evnt => @evnt.EventSourceId == this.CurrentInterview.Id)
-                .Select(@evnt => @evnt.Payload)
-                .ToArray();
-
-            if (interviewEvents.Any()) this.Clients.Caller.applyEvents(interviewEvents);
         }
     }
 }
