df200cd12cbdf5aa59d67fd7c6d5143be51acdfb
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2017-01-04 12:26:08 -0500

    KP-8463 Interviewers list redesign

--- src/UI/Headquarters/WB.UI.Headquarters/Controllers/InterviewersController.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/Controllers/InterviewersController.cs
@@ -4,11 +4,10 @@ using WB.Core.BoundedContexts.Headquarters.Views.User;
 using WB.Core.GenericSubdomains.Portable;
 using WB.Core.GenericSubdomains.Portable.Services;
 using WB.Core.Infrastructure.CommandBus;
-using WB.Core.SharedKernels.SurveyManagement.Web.Filters;
+using WB.Core.SharedKernels.SurveyManagement.Web.Controllers;
 using WB.Core.SharedKernels.SurveyManagement.Web.Models;
-using WB.Core.SharedKernels.SurveyManagement.Web.Utils.Membership;
 
-namespace WB.Core.SharedKernels.SurveyManagement.Web.Controllers
+namespace WB.UI.Headquarters.Controllers
 {
     [ValidateInput(false)]
     [Authorize(Roles = "Administrator, Headquarter, Supervisor, Observer")]
@@ -21,6 +20,14 @@ namespace WB.Core.SharedKernels.SurveyManagement.Web.Controllers
         }
 
         public ActionResult Index(InterviewersFilter filter)
+        {
+            this.ViewBag.ActivePage = MenuItem.Interviewers;
+
+            InterviewersModel pageModel = new InterviewersModel();
+            return this.View(pageModel);
+        }
+
+        public ActionResult IndexN(InterviewersFilter filter)
         {
             this.ViewBag.ActivePage = MenuItem.Interviewers;
 
