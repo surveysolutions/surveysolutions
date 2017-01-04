c3a821721da12b8c5374189293dfe74f59c703c5
Victor Hladkikh <me@alfeg.net>
2017-01-04 16:29:30 +0200

    KP-8440 HQ: Web Interview. UI Setup # CSS initial support

--- src/UI/Headquarters/WB.UI.Headquarters.Interview/src/router/index.ts
+++ src/UI/Headquarters/WB.UI.Headquarters.Interview/src/router/index.ts
@@ -1,17 +1,17 @@
 declare var require: any
 
-import Vue = require('vue')
-import VueRouter = require('vue-router')
-import { virtualPath } from './../config'
+import Vue = require("vue")
+import VueRouter = require("vue-router")
+import { virtualPath } from "./../config"
 
 Vue.use(VueRouter)
 
-import Interview from '../components/Interview'
+import Interview from "./../components/Interview"
 
 export default new VueRouter({
-    base: virtualPath + '/',
-    mode: 'history',
+    base: virtualPath + "/",
+    mode: "history",
     routes: [
-        { path: '/start/:id', component: Interview } // questionier
+        { path: "/start/:id", component: Interview } // questionier
     ]
 })
