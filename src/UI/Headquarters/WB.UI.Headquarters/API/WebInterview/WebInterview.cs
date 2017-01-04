ca045d756096bc225e753c8087c93a831fe29090
Roman Lytvyn <litvin.roma@gmail.com>
2017-01-04 13:20:23 +0200

    HQ: Web interview. SignalR command service. Server side KP-8447

--- src/UI/Headquarters/WB.UI.Headquarters/API/WebInterview/WebInterview.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/API/WebInterview/WebInterview.cs
@@ -2,10 +2,13 @@
 using System.Threading.Tasks;
 using Microsoft.AspNet.SignalR;
 using Ncqrs.Eventing.Sourcing;
+using WB.Core.GenericSubdomains.Portable.Services;
+using WB.Core.Infrastructure.CommandBus;
 using WB.Core.Infrastructure.EventBus.Lite;
 using WB.Core.SharedKernels.DataCollection.Aggregates;
 using WB.Core.SharedKernels.DataCollection.Implementation.Aggregates;
 using WB.Core.SharedKernels.DataCollection.Repositories;
+using WB.UI.Shared.Web.CommandDeserialization;
 
 namespace WB.UI.Headquarters.API.WebInterview
 {
@@ -13,14 +16,25 @@ namespace WB.UI.Headquarters.API.WebInterview
     {
         private readonly IStatefulInterviewRepository statefulInterviewRepository;
         private readonly ILiteEventBus liteEventBus;
+        private readonly ICommandDeserializer commandDeserializer;
+        private readonly ICommandService commandService;
+        private readonly ILogger logger;
 
         private string interviewId => Context.ConnectionId;
         private IStatefulInterview CurrentInterview => this.statefulInterviewRepository.Get(this.interviewId);
 
-        public WebInterview(IStatefulInterviewRepository statefulInterviewRepository, ILiteEventBus liteEventBus)
+        public WebInterview(
+            IStatefulInterviewRepository statefulInterviewRepository, 
+            ILiteEventBus liteEventBus,
+            ICommandDeserializer commandDeserializer,
+            ICommandService commandService,
+            ILogger logger)
         {
             this.statefulInterviewRepository = statefulInterviewRepository;
             this.liteEventBus = liteEventBus;
+            this.commandDeserializer = commandDeserializer;
+            this.commandService = commandService;
+            this.logger = logger;
         }
 
         public void StartInterview()
@@ -42,7 +56,7 @@ namespace WB.UI.Headquarters.API.WebInterview
                 .Select(@evnt => @evnt.Payload)
                 .ToArray();
 
-            if (interviewEvents.Any()) this.Clients.Caller.apply(interviewEvents);
+            if (interviewEvents.Any()) this.Clients.Caller.applyEvents(interviewEvents);
         }
     }
 }
