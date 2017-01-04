d0bf061f419504d820b7fe99ebe6b0de2588b61e
Roman Lytvyn <litvin.roma@gmail.com>
2017-01-04 13:19:35 +0200

    HQ: Web Interview. Statefull interview signalr hub. New conception KP-8444

--- src/UI/Headquarters/WB.UI.Headquarters/API/WebInterview/WebInterview.StatefullInterview.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/API/WebInterview/WebInterview.StatefullInterview.cs
@@ -1,17 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
+﻿using System.Linq;
 using WB.Core.SharedKernels.DataCollection;
-using WB.Core.SharedKernels.DataCollection.Implementation.Aggregates;
-using WB.Core.SharedKernels.DataCollection.Implementation.Aggregates.InterviewEntities;
-using WB.Core.SharedKernels.DataCollection.ValueObjects.Interview;
 
 namespace WB.UI.Headquarters.API.WebInterview
 {
     public partial class WebInterview
     {
-        public string GetTitleText(Identity entityIdentity) 
-            => this.CurrentInterview.GetTitleText(entityIdentity);
+        public void OnTitlesWithSubstitutionsChanged(Identity[] entityIdentities)
+        {
+            var updatedEntities = entityIdentities
+                .Select(identity => new
+                {
+                    identity,
+                    title = this.CurrentInterview.GetTitleText(identity)
+                })
+                .ToArray();
 
+            if (updatedEntities.Any())
+                this.Clients.Caller.updateTitlesWithSubstitutions(updatedEntities);
+        }
+
+        public void OnRosterTitlesChanged(Identity[] rosterInstanceIdentities)
+        {
+            var updatedRosterTitles = rosterInstanceIdentities
+                .Select(identity => new
+                {
+                    identity,
+                    rosterTitle = this.CurrentInterview.GetRosterTitle(identity)
+                })
+                .ToArray();
+
+            if (updatedRosterTitles.Any())
+                this.Clients.Caller.updateRosterTitles(updatedRosterTitles);
+        }
     }
 }
