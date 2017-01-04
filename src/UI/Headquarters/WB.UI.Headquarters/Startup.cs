276188f6cca3cecaf92388e2307b6daa4ccde2bf
Roman Lytvyn <litvin.roma@gmail.com>
2017-01-04 16:20:06 +0200

    HQ: Web interview. SignalR command service. Initialize signalr KP-8447

---
+++ src/UI/Headquarters/WB.UI.Headquarters/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+using WB.UI.Headquarters;
+
+[assembly: OwinStartup(typeof(Startup))]
+namespace WB.UI.Headquarters
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
