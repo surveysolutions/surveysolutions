7656f3e3b7b867e00199d29619a03d662001e0ca
Victor Hladkikh <me@alfeg.net>
2017-01-04 17:57:07 +0200

    KP-8440 HQ: Web Interview. UI Setup # Fixed connection to SignalR

--- src/UI/Headquarters/WB.UI.Headquarters.Interview/src/services/index.ts
+++ src/UI/Headquarters/WB.UI.Headquarters.Interview/src/services/index.ts
@@ -2,11 +2,12 @@ declare var require: any
 
 import { signalrPath } from "./../config"
 
-require("jquery")
+(window as any).$ = (window as any).jQuery = require("jquery");
+
+require('ms-signalr-client');
 
 var $script = require("scriptjs");
 $script(signalrPath, function() {
   // ... HERE is SignalR available to configuration
   // $.signalr.hub
-  
 });
