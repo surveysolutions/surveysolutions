0ca95134eeeddd87e2463015b6a3fd700bb6d555
Roman Lytvyn <litvin.roma@gmail.com>
2017-01-04 13:20:23 +0200

    HQ: Web interview. SignalR command service. Server side KP-8447

---
+++ src/UI/Headquarters/WB.UI.Headquarters/API/WebInterview/WebInterview.CommandService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using WB.Core.GenericSubdomains.Portable;
+using WB.Core.Infrastructure.CommandBus;
+using WB.Core.SharedKernels.DataCollection.Exceptions;
+using WB.UI.Headquarters.Code.CommandTransformation;
+
+namespace WB.UI.Headquarters.API.WebInterview
+{
+    public partial class WebInterview
+    {
+        public void ExecuteCommand(string commandType, string command)
+        {
+            try
+            {
+                ICommand concreteCommand = this.commandDeserializer.Deserialize(commandType, command);
+                ICommand transformedCommand = new CommandTransformator().TransformCommnadIfNeeded(concreteCommand);
+                this.commandService.Execute(transformedCommand);
+            }
+            catch (OverflowException e)
+            {
+                this.logger.Error(e.Message, e);
+                this.Clients.Caller.unhandledException();
+            }
+            catch (Exception e)
+            {
+                var interviewException = e.GetSelfOrInnerAs<InterviewException>();
+                if (interviewException != null)
+                    this.Clients.Caller.interviewException(interviewException);
+
+                var userException = e.GetSelfOrInnerAs<UserException>();
+                if (userException != null)
+                    this.Clients.Caller.userException(userException);
+
+                if (interviewException == null && userException == null)
+                {
+                    this.logger.Error(e.Message, e);
+                    this.Clients.Caller.unhandledException();
+                }
+            }
+        }
+    }
+}
