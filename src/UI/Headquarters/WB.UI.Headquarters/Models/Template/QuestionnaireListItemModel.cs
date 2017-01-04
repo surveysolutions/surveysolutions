2922608cb38a73c57fbeb68baf803efbbd8e2004
Slava Scherbak <slava.scherbak@gmail.com>
2017-01-04 13:57:57 -0500

    Survey Setup: changed date format. KP-8458

---
+++ src/UI/Headquarters/WB.UI.Headquarters/Models/Template/QuestionnaireListItemModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace WB.UI.Headquarters.Models.Template
+{
+    public class QuestionnaireListItemModel
+    {
+        public virtual string Id { get; set; }
+
+        public virtual Guid QuestionnaireId { get; set; }
+
+        public virtual long Version { get; set; }
+
+        public virtual string Title { get; set; }
+        
+        public virtual bool AllowCensusMode { get; set; }
+
+        public virtual bool Disabled { get; set; }
+
+        public virtual string CreationDate { get; set; }
+
+        public virtual string LastEntryDate { get; set; }
+
+        public virtual string ImportDate { get; set; }
+    }
+}
