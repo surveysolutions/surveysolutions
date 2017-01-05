560f83027815f643631a3cc2668d49d187c95f55
Victor Hladkikh <me@alfeg.net>
2017-01-05 16:01:04 +0200

    KP-8440 HQ: Web Interview. UI Setup # CSS support

--- src/UI/Headquarters/WB.UI.Headquarters.Interview/src/App.vue
+++ src/UI/Headquarters/WB.UI.Headquarters.Interview/src/App.vue
@@ -1,15 +1,24 @@
 <template>
-    <div>
-        <h1>Hello</h1>
-        <ul>
-            <li><span class="glyphicon glyphicon-backward" aria-hidden="true"></span>
-                <router-link to='/'>Start</router-link>
-            </li>
-            <li>
-                <router-link to='/prefilled/580eba69f1844f1892147342f568bb0f$1'>Preffiled</router-link>
-            </li>
-        </ul>
-        <router-view></router-view>
+    <div class="container">
+        <div class="header clearfix">
+            <nav>
+                <ul class="nav nav-pills pull-right">
+                    <router-link tag="li" to="/" role="presentation" activeClass="active">
+                        <a>Start</a>
+                    </router-link>
+                    <router-link tag="li" to="/prefilled/580eba69f1844f1892147342f568bb0f$1" role="presentation" activeClass="active">
+                        <a>Preffiled</a>
+                    </router-link>
+                </ul>
+            </nav>
+            <h3 class="text-muted">Web interview</h3>
+        </div>
+        <div>
+            <router-view></router-view>
+        </div>
+        <footer class="footer">
+            <p>&copy; 2017 World Bank, Inc.</p>
+        </footer>
     </div>
 </template>
 <script lang='ts'>
