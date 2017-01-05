058bf201f3f7d83728caa2c42f63ed1b9a318b76
Andrii Kozhyn <hedgic@gmail.com>
2017-01-05 18:07:03 +0200

    KP-8461: Stub UI added for questions

--- src/UI/Headquarters/WB.UI.Headquarters.Interview/src/components/questions/TextQuestion.vue
+++ src/UI/Headquarters/WB.UI.Headquarters.Interview/src/components/questions/TextQuestion.vue
@@ -1,14 +1,18 @@
 <template>
     <div class="question">
-        <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit <small>Text question</small></h5>
-        <div class="question-unit">
-            <div class="options-group">
-                <div class="form-group">
-                    <div class="field answered">
-                        <input type="number" class="field-to-fill" placeholder="Tap to enter number" value="6">
-                        <button type="submit" class="btn btn-link btn-clear">
-                            <span></span>
-                        </button>
+        <div class="question-editor text-question">
+            <h5>
+                Text question basic template <small>{{entity.Identity}}</small>
+            </h5>
+            <div class="question-unit">
+                <div class="options-group">
+                    <div class="form-group">
+                        <div class="field answered">
+                            <input type="text" class="field-to-fill" placeholder="Tap to enter number" value="Selling groceries">
+                            <button type="submit" class="btn btn-link btn-clear">
+                                <span></span>
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -17,6 +21,7 @@
 </template>
 <script lang="ts">
     export default {
-        name: 'TextQuestion'
+        name: 'TextQuestion',
+        props: ['entity']
     }
 </script>
