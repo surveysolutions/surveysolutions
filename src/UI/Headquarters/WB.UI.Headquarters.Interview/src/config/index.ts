ea95c79d5ab41aa8c4e74f19be7738769c25752c
Victor Gladkikh <me@alfeg.net>
2017-01-05 10:16:48 +0200

    KP-8440 HQ: Web Interview. UI Setup # Proper SignalR service handling

--- src/UI/Headquarters/WB.UI.Headquarters.Interview/src/config/index.ts
+++ src/UI/Headquarters/WB.UI.Headquarters.Interview/src/config/index.ts
@@ -1,4 +1,4 @@
 declare var INTERVIEW_APP_CONFIG: any;
 
-export const virtualPath:string = INTERVIEW_APP_CONFIG.virtualPath;
-export const signalrPath:string = INTERVIEW_APP_CONFIG.signalrPath;
+export const virtualPath: string = INTERVIEW_APP_CONFIG.virtualPath;
+export const signalrPath: string = INTERVIEW_APP_CONFIG.signalrPath;
