57cd453ce941afe3b3f7d75ff06fdb98f6e16222
Andrii Kozhyn <hedgic@gmail.com>
2017-01-05 10:01:12 +0200

    KP-8461: Initial list render with its component added

--- src/UI/Headquarters/WB.UI.Headquarters.Interview/src/components/prefilled.vue
+++ src/UI/Headquarters/WB.UI.Headquarters.Interview/src/components/prefilled.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
-        <h1></h1>
+        <h1>Prefilled questions</h1>
+        <button @click="getPrefilledQuestions($route.params.id)">Get questions</button>
         <ul>
             <li v-for="question in prefilledQuestions">
                 {{question.title}}
@@ -10,20 +11,11 @@
 </template>
 
 <script lang="ts">
-  import {mapGetters} from "vuex"
+    import { mapGetters, mapActions } from "vuex"
 
-  export default {
-    name: 'prefilled-view',
-    watch: {
-        "$route": (to, from) => {
-
-        }
-    },
-    computed: mapGetters(["prefilledQuestions"]),
-    beforeMount () {
-    //   this.$store.dispatch('InterviewMount', {
-    //     id: this.$route.params.id
-    //   })
+    export default {
+        name: 'prefilled-view',
+        computed: mapGetters(["prefilledQuestions"]),
+        methods: mapActions(["getPrefilledQuestions"])
     }
-  }
 </script>
