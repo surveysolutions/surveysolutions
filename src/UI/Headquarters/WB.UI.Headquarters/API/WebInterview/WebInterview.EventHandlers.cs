770fa624b2fe11fe7e8add5bc693e42cf69c0218
Roman Lytvyn <litvin.roma@gmail.com>
2017-01-05 12:55:45 +0200

    HQ: Web Interview. Interview SignalR HUB. Implement event handlers KP-8443

---
+++ src/UI/Headquarters/WB.UI.Headquarters/API/WebInterview/WebInterview.EventHandlers.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+using WB.Core.Infrastructure.EventBus.Lite;
+using WB.Core.SharedKernels.DataCollection.Events.Interview;
+using WB.Core.SharedKernels.DataCollection.Utils;
+
+namespace WB.UI.Headquarters.API.WebInterview
+{
+    public partial class WebInterview : 
+        ILiteEventHandler<SubstitutionTitlesChanged>,
+        ILiteEventHandler<RosterInstancesTitleChanged>
+    {
+        public void Handle(SubstitutionTitlesChanged @event)
+        {
+            var updatedEntities = @event.Groups.Concat(@event.Questions).Concat(@event.StaticTexts)
+                .Select(identity => new
+                {
+                    identity,
+                    title = this.currentInterview.GetTitleText(identity)
+                })
+                .ToArray();
+
+            if (updatedEntities.Any())
+                this.Clients.Group(this.interviewId).updateTitlesWithSubstitutions(updatedEntities);
+        }
+
+        public void Handle(RosterInstancesTitleChanged @event)
+        {
+            var updatedRosterTitles = @event.ChangedInstances
+                .Select(identity => new
+                {
+                    identity = identity.RosterInstance.GetIdentity(),
+                    rosterTitle = identity.Title
+                })
+                .ToArray();
+
+            if (updatedRosterTitles.Any())
+                this.Clients.Group(this.interviewId).updateRosterTitles(updatedRosterTitles);
+        }
+    }
+}
