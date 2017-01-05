174a11d1ad73642af040862487542c63f2b1aaef
Victor Hladkikh <me@alfeg.net>
2017-01-05 12:05:53 +0200

    KP-8440 HQ: Web Interview. UI Setup # CSS support

--- src/UI/Headquarters/WB.UI.Headquarters.Interview/build/webpack.base.conf.js
+++ src/UI/Headquarters/WB.UI.Headquarters.Interview/build/webpack.base.conf.js
@@ -9,90 +9,89 @@ var env = process.env.NODE_ENV
 var cssSourceMapDev = (env === 'development' && config.dev.cssSourceMap)
 var cssSourceMapProd = (env === 'production' && config.build.productionSourceMap)
 var useCssSourceMap = cssSourceMapDev || cssSourceMapProd
+var currentConfig = process.env.NODE_ENV === 'production' ? config.build : config.dev
 
 module.exports = {
-  entry: {
-    app: './src/main.ts'
-  },
-  output: {
-    path: config.build.assetsRoot,
-    publicPath: process.env.NODE_ENV === 'production' ? config.build.assetsPublicPath : config.dev.assetsPublicPath,
-    filename: '[name].js'
-  },
-  resolve: {
-    extensions: ['', '.js', '.ts', '.vue', '.json'],
-    fallback: [path.join(__dirname, '../node_modules')],
-    alias: {
-      'vue$': 'vue/dist/vue.common.js',
-      'src': path.resolve(__dirname, '../src'),
-      'assets': path.resolve(__dirname, '../src/assets'),
-      'components': path.resolve(__dirname, '../src/components')
-    }
-  },
-  resolveLoader: {
-    fallback: [path.join(__dirname, '../node_modules')]
-  },
-  module: {
-    preLoaders: [
-      {
-          test: /\.ts$/,
-          loader: 'tslint-loader'
-      }
-    ],
-    loaders: [
-      {
-        test: /\.vue$/,
-        loader: 'vue'
-      },
-      {
-        test: /\.ts$/,
-        loader: 'ts',
-        include: projectRoot,
-        exclude: /node_modules/
-      },
-      {
-        test: /\.json$/,
-        loader: 'json'
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-        loader: 'url',
-        query: {
-          limit: 10000,
-          name: utils.assetsPath('img/[name].[hash:7].[ext]')
+    entry: {
+        app: './src/main.ts'
+    },
+    output: {
+        path: config.build.assetsRoot,
+        publicPath: process.env.NODE_ENV === 'production' ? config.build.assetsPublicPath : config.dev.assetsPublicPath,
+        filename: '[name].js'
+    },
+    resolve: {
+        extensions: ['', '.js', '.ts', '.vue', '.json'],
+        fallback: [path.join(__dirname, '../node_modules')],
+        alias: {
+            'vue$': 'vue/dist/vue.common.js',
+            'src': path.resolve(__dirname, '../src'),
+            'assets': path.resolve(__dirname, '../src/assets'),
+            'components': path.resolve(__dirname, '../src/components')
         }
-      },
-      {
-        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-        loader: 'url',
-        query: {
-          limit: 10000,
-          name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
-        }
-      },
-      {
-        test: /\.scss$/,
-        loaders: ["style-loader", "css-loader", "sass-loader"]
-      },
+    },
+    resolveLoader: {
+        fallback: [path.join(__dirname, '../node_modules')]
+    },
+    module: {
+        preLoaders: [
             {
-        test: /\.sass$/,
-        loaders: ["style-loader", "css-loader", "sass-loader"]
-      }
-    ]
-  },
-  sassLoader: {
-    sourceMap: true
-  },
-  vue: {
-    loaders: utils.cssLoaders({ sourceMap: useCssSourceMap }),
-    esModule: true,
-    postcss: [
-      require('autoprefixer')({
-        browsers: ['last 3 versions']
-      })
-    ]
-  },
-  ts: {
-    appendTsSuffixTo: [/\.vue$/]
-  }
+                test: /\.ts$/,
+                loader: 'tslint-loader'
+            }
+        ],
+        loaders: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.ts$/,
+                loader: 'ts-loader',
+                include: projectRoot,
+                exclude: /node_modules/
+            },
+            {
+                test: /\.json$/,
+                loader: 'json-loader'
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+                loader: 'url-loader',
+                query: {
+                    limit: 10000,
+                    publicPath: currentConfig.assetsRelativePath,
+                    name: utils.assetsPath('img/[name].[ext]')
+                }
+            },
+            {
+                test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+                loader: 'url-loader',
+                query: {
+                    limit: 10000,
+                    publicPath: currentConfig.assetsRelativePath,
+                    name: utils.assetsPath('fonts/[name].[ext]')
+                }
+            },
+            {
+                test: /\.s[a|c]ss$/,
+                loaders: ["style-loader", "css-loader", "sass-loader"]
+            }
+        ]
+    },
+    sassLoader: {
+        sourceMap: true
+    },
+    vue: {
+        esModule: true,
+        loaders: utils.cssLoaders({ sourceMap: useCssSourceMap }),
+        postcss: [
+            require('autoprefixer')({
+                browsers: ['last 3 versions']
+            })
+        ]
+    },
+    ts: {
+        appendTsSuffixTo: [/\.vue$/]
+    }
 }
