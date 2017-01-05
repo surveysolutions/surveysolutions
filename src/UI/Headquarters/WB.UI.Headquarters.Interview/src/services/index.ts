89b08631c9519a002442fa15b36d0df850454430
Victor Hladkikh <me@alfeg.net>
2017-01-05 10:16:48 +0200

    KP-8440 HQ: Web Interview. UI Setup # Proper SignalR service handling

--- src/UI/Headquarters/WB.UI.Headquarters.Interview/src/services/index.ts
+++ src/UI/Headquarters/WB.UI.Headquarters.Interview/src/services/index.ts
@@ -1,19 +1,51 @@
-declare var require: any
-
-import { signalrPath } from "./../config"
-
 import * as jQuery from "jquery"
+import { signalrPath } from "./../config"
 (window as any).$ = (window as any).jQuery = jQuery
-
-import "signalr";
-
+import "ms-signalr-client"
 import * as $script from "scriptjs"
+import store from "../store"
 
-$script(signalrPath, function () {
-    // ... HERE is SignalR available to configuration
-    // $.signalr.hub
-    // jQuery.connection.hub.start().done(function () {
-    // }).fail((e) => {
-    //     console.log(e);
-    // });
-});
+export const hub = {
+    instance: null
+}
+
+export const stateConversion = { 0: "connecting", 1: "connected", 2: "reconnecting", 4: "disconnected" };
+
+new Promise<any>((res, rej) => {
+    $script(signalrPath, () => {
+        jQuery.signalR.interview.logging = true;
+        jQuery.signalR.hub.start()
+            .done(() => {
+                res(jQuery.signalR.interview);
+            })
+            .fail(e => {
+                console.error(e)
+                rej(e)
+            })
+    })
+}).then(h => {
+    hub.instance = jQuery.signalR.interview
+
+    store.dispatch("HubStateChanged", {
+        state: { oldState: 0, newState: 1 },
+        title: "Connected"
+    })
+
+    jQuery.signalR.hub.stateChanged(change => {
+        switch (change.newState) {
+            case jQuery.signalR.connectionState.connected:
+                store.dispatch("HubStateChanged", {
+                    state: change,
+                    title: "Connected"
+                })
+                break;
+            default:
+                store.dispatch("HubStateChanged", {
+                    state: change,
+                    title: "Disconnected"
+                })
+                break;
+        }
+
+    })
+})
