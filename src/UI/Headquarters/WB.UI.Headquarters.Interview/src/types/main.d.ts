508b592a66552375555e9fe8b0e3e8695325f1b1
Victor Hladkikh <me@alfeg.net>
2017-01-06 15:57:16 +0200

    KP-8440 HQ: Web Interview. UI Setup # Readme

--- src/UI/Headquarters/WB.UI.Headquarters.Interview/src/types/main.d.ts
+++ src/UI/Headquarters/WB.UI.Headquarters.Interview/src/types/main.d.ts
@@ -2,3 +2,15 @@ declare class InterviewEntity {
     id: string
     type: string
 }
+
+declare class SignalrHubChange {
+    public oldState: number
+    public newState: number
+}
+
+declare class HubChangedEvent {
+    public state: SignalrHubChange
+    public title: string
+
+    constructor(state: SignalrHubChange, title: string)
+}
