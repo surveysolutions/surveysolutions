ff7d92db7f76c7778b9d27a91119208a36af5ece
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2017-01-10 19:02:07 +0200

    trying to fix random test fails

--- src/Tests/WB.Tests.Unit/SharedKernels/DataCollection/PlainQuestionnaireTests/PlainQuestionnaireTestsTestsContext.cs
+++ src/Tests/WB.Tests.Unit/SharedKernels/DataCollection/PlainQuestionnaireTests/PlainQuestionnaireTestsTestsContext.cs
@@ -1,4 +1,7 @@
-﻿using NUnit.Framework;
+﻿using Machine.Specifications;
+using NUnit.Framework;
+using WB.Core.GenericSubdomains.Portable.Implementation.Services;
+using WB.Core.GenericSubdomains.Portable.Services;
 using WB.Core.SharedKernels.DataCollection.Implementation.Entities;
 
 namespace WB.Tests.Unit.SharedKernels.DataCollection.PlainQuestionnaireTests
@@ -6,6 +9,9 @@ namespace WB.Tests.Unit.SharedKernels.DataCollection.PlainQuestionnaireTests
     [TestOf(typeof(PlainQuestionnaire))]
     internal class PlainQuestionnaireTestsContext
     {
-        
+        public static Establish baseContext = () =>
+        {
+            Setup.InstanceToMockedServiceLocator<ISubstitutionService>(new SubstitutionService());
+        };
     }
 }
