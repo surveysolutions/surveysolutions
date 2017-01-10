42e5b500fe9ce27a2b8879e3f4ad5d8a202b6a88
Andrii Kozhyn <hedgic@gmail.com>
2017-01-10 13:24:19 +0200

    KP-8510: Fix web link url

--- src/UI/Headquarters/WB.UI.Headquarters/Controllers/WebInterviewController.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/Controllers/WebInterviewController.cs
@@ -5,6 +5,7 @@ using Microsoft.Ajax.Utilities;
 using WB.Core.BoundedContexts.Headquarters.Services;
 using WB.Core.GenericSubdomains.Portable.Services;
 using WB.Core.Infrastructure.CommandBus;
+using WB.Core.SharedKernels.DataCollection.Implementation.Entities;
 using WB.UI.Headquarters.Controllers;
 
 namespace WB.Core.SharedKernels.SurveyManagement.Web.Controllers
