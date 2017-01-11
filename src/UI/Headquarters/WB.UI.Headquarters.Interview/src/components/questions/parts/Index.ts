223f3b6acbdd32d344876849c8cb4befc076bc7b
Victor Hladkikh <me@alfeg.net>
2017-01-11 19:18:55 +0200

    KP-8496 Support for validations. Initial support for server sent events. Renaming

--- src/UI/Headquarters/WB.UI.Headquarters.Interview/src/components/questions/parts/Index.ts
+++ src/UI/Headquarters/WB.UI.Headquarters.Interview/src/components/questions/parts/Index.ts
@@ -2,6 +2,8 @@ import * as vue from "vue"
 
 import Instructions from "./Instructions"
 import Title from "./Title"
+import Validation from "./Validation"
 
 vue.component("wb-instructions", Instructions)
 vue.component("wb-title", Title)
+vue.component("wb-validation", Validation)
