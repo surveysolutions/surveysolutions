8afff4fac3dc00633c97de1c759111007dde1420
Andrii Kozhyn <hedgic@gmail.com>
2017-01-11 19:08:46 +0200

    small code reformatting

--- src/UI/Headquarters/WB.UI.Headquarters.Interview/src/components/questions/Index.ts
+++ src/UI/Headquarters/WB.UI.Headquarters.Interview/src/components/questions/Index.ts
@@ -2,12 +2,12 @@ import * as vue from "vue"
 
 import CategoricalSingle from "./CategoricalSingle"
 import DateTime from "./DateTime"
-import GenericQuestion from "./GenericQuestion"
 import Integer from "./Integer"
+import Question from "./Question"
 import TextQuestion from "./TextQuestion"
 
 vue.component("DateTime", DateTime)
 vue.component("Integer", Integer)
 vue.component("TextQuestion", TextQuestion)
 vue.component("CategoricalSingle", CategoricalSingle)
-vue.component("GenericQuestion", GenericQuestion)
+vue.component("wb-question", Question)
