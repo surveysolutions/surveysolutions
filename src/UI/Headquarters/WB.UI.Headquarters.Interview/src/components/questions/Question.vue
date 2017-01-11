819e57af2b0664b3245576bca0e5ea50db36cdeb
Victor Gladkikh <me@alfeg.net>
2017-01-11 19:18:55 +0200

    KP-8496 Support for validations. Initial support for server sent events. Renaming

--- src/UI/Headquarters/WB.UI.Headquarters.Interview/src/components/questions/Question.vue
+++ src/UI/Headquarters/WB.UI.Headquarters.Interview/src/components/questions/Question.vue
@@ -4,12 +4,7 @@
             <wb-title :id="question.id" />
             <wb-instructions :id="question.id" />
             <slot />
-            <div class="information-block text-danger" v-if="!question.validity.isValid">
-                <h6>Answer is invalid</h6>
-                <template v-for="message in question.validity.messages">
-                    <p>{{ message }}</p>
-                </template>
-            </div>
+            <wb-validation :id="question.id" />
         </div>
     </div>
 </template>
