23131c77a67ebb70d16002b6838a3133eebf2697
Victor Hladkikh <me@alfeg.net>
2017-01-11 19:18:55 +0200

    KP-8496 Support for validations. Initial support for server sent events. Renaming

---
+++ src/UI/Headquarters/WB.UI.Headquarters.Interview/src/components/questions/parts/Validation.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="information-block text-danger" v-if="!$me.validity.isValid">
+        <h6>Answer is invalid</h6>
+        <template v-for="message in $me.validity.messages">
+            <p>{{ message }}</p>
+        </template>
+    </div>
+</template>
+<script lang="ts">
+    import { entityPartial } from "components/mixins"
+
+    export default {
+        mixins: [entityPartial],
+        name: "wb-validation"
+    }
+</script>
