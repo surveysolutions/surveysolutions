8190b210448061cbe632d68e4d7f28dc826160f1
Victor Gladkikh <me@alfeg.net>
2017-01-11 19:40:43 +0200

    KP-8496 Support for validations. Initial support for server sent events # Tests fix

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/HeadquartersBoundedContextModule.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/HeadquartersBoundedContextModule.cs
@@ -293,7 +293,7 @@ namespace WB.Core.BoundedContexts.Headquarters
 
 
             this.Kernel.RegisterDenormalizer<InterviewEventHandlerFunctional>();
-            this.Kernel.RegisterDenormalizer<InterviewValidationHandlerFunctional>();
+            this.Kernel.RegisterDenormalizer<InterviewLifecycleEventHandler>();
             this.Kernel.RegisterDenormalizer<StatusChangeHistoryDenormalizerFunctional>();
             this.Kernel.RegisterDenormalizer<InterviewExportedCommentariesDenormalizer>();
             this.Kernel.RegisterDenormalizer<InterviewStatusTimeSpanDenormalizer>();
