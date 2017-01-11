bdd4bf28b89ffc6925a18cd410ac41e17f205d66
Victor Hladkikh <me@alfeg.net>
2017-01-11 12:53:13 +0200

    SignalR more proper ninject configuration

--- src/UI/Headquarters/WB.UI.Headquarters/Startup.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/Startup.cs
@@ -1,10 +1,7 @@
 ﻿using Microsoft.AspNet.SignalR;
-using Microsoft.AspNet.SignalR.Hubs;
-using Microsoft.AspNet.SignalR.Ninject;
 using Microsoft.Owin;
 using Owin;
 using WB.UI.Headquarters;
-using WB.UI.Headquarters.API.WebInterview;
 
 [assembly: OwinStartup(typeof(Startup))]
 namespace WB.UI.Headquarters
@@ -13,12 +10,9 @@ namespace WB.UI.Headquarters
     {
         public void Configuration(IAppBuilder app)
         {
-            var ninjectDependencyResolver = new NinjectDependencyResolver(new Ninject.Web.Common.Bootstrapper().Kernel);
-            ninjectDependencyResolver.Resolve<IHubPipeline>().AddModule(new PlainSignalRTransactionManager());
             app.MapSignalR(new HubConfiguration
             {
-                EnableDetailedErrors = true,
-                Resolver = ninjectDependencyResolver
+                EnableDetailedErrors = true
             });
         }
     }
