529f40b6438af8c6053f1b4521745688291337dd
Victor Gladkikh <me@alfeg.net>
2017-01-12 12:21:30 +0200

    KP-8496 Support for validations # renamings per code review
    Moved existing question stubs to wb-question component

--- src/UI/Headquarters/WB.UI.Headquarters.Interview/src/components/entities/Integer.vue
+++ src/UI/Headquarters/WB.UI.Headquarters.Interview/src/components/entities/Integer.vue
@@ -1,8 +1,10 @@
 <template>
-    <div class="question" v-if="$me">
-        <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit <small>Integer question</small></h5>
-        <wb-instructions />
-    </div>
+    <wb-question :question="$me" questionCssClassName="single-select-question">
+        <div class="question-unit">
+            <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit <small>Integer question</small></h5>
+            <wb-instructions />
+        </div>
+    </wb-question>
 </template>
 <script lang="ts">
     import { entityDetails } from "components/mixins"
