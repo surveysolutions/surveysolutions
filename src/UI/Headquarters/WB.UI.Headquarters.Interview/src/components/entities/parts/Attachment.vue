dbb1f6889f70162da1099c4376020e673f7f0564
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2017-01-12 14:59:22 -0500

    Attachments: KP-8499 Static text

--- src/UI/Headquarters/WB.UI.Headquarters.Interview/src/components/entities/parts/Attachment.vue
+++ src/UI/Headquarters/WB.UI.Headquarters.Interview/src/components/entities/parts/Attachment.vue
@@ -1,5 +1,12 @@
 <template>
-    <h5>{{$me.attachment}}</h5>
+    <div class="image-zoom-box static-text-image" v-if="$me.attachmentContent != null"> 
+        <img :src="'../../../api/Attachments/Content/' + $me.attachmentContent + '?maxSize=435'" alt="static-text-image" class="zoomImg"> 
+            <div class="modal-img">
+                <span class="close-zoomming-img">×</span>
+                <img class="modal-img-content" src="" alt="">
+                <span class="caption"></span>
+            </div>
+    </div>
 </template>
 <script lang="ts">
     import { entityPartial } from "components/mixins"
