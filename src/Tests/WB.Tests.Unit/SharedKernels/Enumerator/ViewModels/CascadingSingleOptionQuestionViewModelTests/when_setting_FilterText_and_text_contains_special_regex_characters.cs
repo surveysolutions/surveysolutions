1a37326f7ac1706c4bd178eef55cd660f9ecd951
Roman Lytvyn <litvin.roma@gmail.com>
2017-01-13 11:13:19 +0200

    EN: fixed crash on cascading question with spec symbols in option titles. KP-8539

---
+++ src/Tests/WB.Tests.Unit/SharedKernels/Enumerator/ViewModels/CascadingSingleOptionQuestionViewModelTests/when_setting_FilterText_and_text_contains_special_regex_characters.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Machine.Specifications;
+using Moq;
+using WB.Core.SharedKernels.DataCollection;
+using WB.Core.SharedKernels.DataCollection.Implementation.Aggregates.InterviewEntities;
+using WB.Core.SharedKernels.Enumerator.Aggregates;
+
+using WB.Core.SharedKernels.Enumerator.Repositories;
+using WB.Core.SharedKernels.Enumerator.ViewModels.InterviewDetails.Questions;
+using It = Machine.Specifications.It;
+
+namespace WB.Tests.Unit.SharedKernels.Enumerator.ViewModels.CascadingSingleOptionQuestionViewModelTests
+{
+    internal class when_setting_FilterText_and_text_contains_special_regex_characters : CascadingSingleOptionQuestionViewModelTestContext
+    {
+        private Establish context = () =>
+        {
+            SetUp();
+
+            var childAnswer = Mock.Of<InterviewTreeSingleOptionQuestion>(_ => _.IsAnswered == false);
+            var parentOptionAnswer = Mock.Of<InterviewTreeSingleOptionQuestion>(_ => _.IsAnswered == true && _.GetAnswer() == Create.Entity.SingleOptionAnswer(1));
+
+            var interview = new Mock<IStatefulInterview>();
+
+            interview.Setup(x => x.QuestionnaireIdentity).Returns(questionnaireId);
+            interview.Setup(x => x.GetSingleOptionQuestion(questionIdentity)).Returns(childAnswer);
+            interview.Setup(x => x.GetSingleOptionQuestion(parentIdentity)).Returns(parentOptionAnswer);
+            interview.Setup(x => x.GetTopFilteredOptionsForQuestion(Moq.It.IsAny<Identity>(), Moq.It.IsAny<int?>(), Moq.It.IsAny<string>(), Moq.It.IsAny<int>()))
+                .Returns((Identity identity, int? value, string filter, int count) => new List<CategoricalOption>(new[]
+                {
+                    new CategoricalOption
+                    {
+                        Title = "G.C.E. (O Level) / C.X.C/ (+ General), 1 or 2 Subjects (+ Basic) and Technical Training",
+                        Value = 3,
+                        ParentValue = 1
+                    }
+                }));
+
+            var interviewRepository = Mock.Of<IStatefulInterviewRepository>(x => x.Get(interviewId) == interview.Object);
+
+            var questionnaireRepository = SetupQuestionnaireRepositoryWithCascadingQuestion();
+
+            cascadingModel = CreateCascadingSingleOptionQuestionViewModel(
+                interviewRepository: interviewRepository,
+                questionnaireRepository: questionnaireRepository);
+
+            cascadingModel.Init(interviewId, questionIdentity, navigationState);
+        };
+
+        Because of = () => exception = Catch.Only<ArgumentException>(() => { cascadingModel.FilterText = @"(+"; });
+
+        It should_not_throw_argument_exception = () => exception.ShouldBeNull();
+
+        private static CascadingSingleOptionQuestionViewModel cascadingModel;
+        private static ArgumentException exception;
+    }
+}
