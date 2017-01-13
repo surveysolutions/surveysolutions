866721e188ec42c2431965afdfa343f510a152b2
Pavel Stovpivskyi <sby@rambler.ru>
2017-01-13 16:47:45 +0200

    added confirm for remove rosters

--- src/UI/Headquarters/WB.UI.Headquarters.Interview/src/main.ts
+++ src/UI/Headquarters/WB.UI.Headquarters.Interview/src/main.ts
@@ -1,3 +1,10 @@
+
+// tslint:disable-next-line:ordered-imports
+import * as jQuery from "jquery"
+const $ = (window as any).$ = (window as any).jQuery = jQuery
+import "bootstrap-sass/assets/javascripts/bootstrap/modal"
+
+
 import "es6-promise/auto"
 import "jquery-mask-plugin"
 import * as Vue from "vue"
