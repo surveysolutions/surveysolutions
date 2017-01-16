c1bfa541e15c9743baf07fabbdd69a8e2d51d3e1
Victor Hladkikh <me@alfeg.net>
2017-01-16 15:50:18 +0200

    Moved component's specific jquery plugins initialization into entities/index.ts

--- src/UI/Headquarters/WB.UI.Headquarters.Interview/src/main.ts
+++ src/UI/Headquarters/WB.UI.Headquarters.Interview/src/main.ts
@@ -1,13 +1,5 @@
-
-// tslint:disable-next-line:ordered-imports
-import * as jQuery from "jquery"
-const $ = (window as any).$ = (window as any).jQuery = jQuery
-import "bootstrap-sass/assets/javascripts/bootstrap/modal"
-
 import "es6-promise/auto"
-import "jquery-mask-plugin"
-// tslint:disable-next-line:ordered-imports
-import "autoNumeric"
+
 import * as Vue from "vue"
 import * as Vuex from "vuex"
 
