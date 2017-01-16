878a2b380fd520d469894235b36729641c397738
Victor Hladkikh <me@alfeg.net>
2017-01-16 15:50:18 +0200

    Moved component's specific jquery plugins initialization into entities/index.ts

--- src/UI/Headquarters/WB.UI.Headquarters.Interview/src/components/entities/Index.ts
+++ src/UI/Headquarters/WB.UI.Headquarters.Interview/src/components/entities/Index.ts
@@ -1,3 +1,14 @@
+declare const require: any
+
+// tslint:disable-next-line:ordered-imports
+import * as jQuery from "jquery"
+const $ = (window as any).$ = (window as any).jQuery = jQuery
+import "bootstrap-sass/assets/javascripts/bootstrap/modal"
+
+import "jquery-mask-plugin"
+// tslint:disable-next-line:ordered-imports
+import "autoNumeric"
+
 import * as vue from "vue"
 
 import CategoricalMulti from "./CategoricalMulti"
@@ -6,9 +17,8 @@ import DateTime from "./DateTime"
 import Double from "./Double"
 import Integer from "./Integer"
 import Question from "./Question"
-import TextQuestion from "./TextQuestion"
-
 import StaticText from "./StaticText"
+import TextQuestion from "./TextQuestion"
 
 vue.component("DateTime", DateTime)
 vue.component("Integer", Integer)
@@ -16,7 +26,5 @@ vue.component("Double", Double)
 vue.component("TextQuestion", TextQuestion)
 vue.component("CategoricalSingle", CategoricalSingle)
 vue.component("CategoricalMulti", CategoricalMulti)
-
 vue.component("wb-question", Question)
-
 vue.component("StaticText", StaticText)
