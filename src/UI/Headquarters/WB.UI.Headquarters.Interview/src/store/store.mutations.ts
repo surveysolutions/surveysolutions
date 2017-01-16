0e833852e937008475ad09905761803d01c93949
Victor Gladkikh <me@alfeg.net>
2017-01-16 15:33:15 +0200

    KP-8550 Next section / to parent button # Even more changes around section handling

--- src/UI/Headquarters/WB.UI.Headquarters.Interview/src/store/store.mutations.ts
+++ src/UI/Headquarters/WB.UI.Headquarters.Interview/src/store/store.mutations.ts
@@ -5,17 +5,27 @@ export default {
     SET_QUESTIONNAIRE_INFO(state, questionnaireInfo: IQuestionnaireInfo) {
         Vue.set(state.interview, "questionnaire", questionnaireInfo)
     },
-    SET_ENTITY_DETAILS(state, entity) {
+    SET_ENTITY_DETAILS(state, entity: IInterviewEntity) {
         Vue.set(state.details.entities, entity.id, entity)
     },
-    SET_SECTION(state, section) {
-        Vue.set(state.details.sections, section.id, section)
+    SET_SECTION_DATA(state, sectionData: ISectionData) {
+        Vue.set(state.interview, "currentSection", sectionData.info.id)
+        Vue.set(state.details.sections, sectionData.info.id, sectionData)
     },
-    SET_CURRENT_SECTION(state, { id }) {
-        Vue.set(state.interview, "currentSection", id)
-    },
-    SET_INTERVIEW_SECTIONS(state, { sections }) {
+    SET_INTERVIEW_SECTIONS(state, sections: ISectionInfo[]) {
         Vue.set(state.interview, "sections", sections)
+
+        sections.forEach(sectionInfo => {
+            const details = state.details.sections[sectionInfo.id]
+
+            if (details) {
+                Vue.set(details, "info", sectionInfo)
+            } else {
+                Vue.set(state.details.sections, sectionInfo.id, {
+                    info: sectionInfo
+                })
+            }
+        })
     },
     SET_ANSWER_NOT_SAVED(state, {id, message}) {
         const validity = state.details.entities[id].validity
