a63246b3e39ae370f42fcd4a2d075994f69fa5a6
Pavel Stovpivskyi <sby@rambler.ru>
2017-01-18 18:46:15 +0200

    added limit count of digits for double question
    KP-8563

--- src/UI/Headquarters/WB.UI.Headquarters.Interview/src/components/entities/Double.vue
+++ src/UI/Headquarters/WB.UI.Headquarters.Interview/src/components/entities/Double.vue
@@ -4,8 +4,8 @@
             <div class="options-group">
                 <div class="form-group">
                     <div class="field answered">
-                        <input type="text" autocomplete="off" class="field-to-fill" placeholder="Tap to enter number" :value="$me.answer" @blur="answerDoubleQuestion"
-                            v-numericFormatting="{aSep: formattingChar, mDec: countOfDecimalPlaces, vMin: -9999999999999999, vMax: 9999999999999999, aPad: false }">
+                        <input type="text" autocomplete="off" inputmode="numeric" pattern="[0-9]*" class="field-to-fill" placeholder="Tap to enter number" :value="$me.answer" @blur="answerDoubleQuestion"
+                            v-numericFormatting="{aSep: formattingChar, mDec: countOfDecimalPlaces, vMin: '-999999999999999', vMax: '999999999999999', aPad: false }">
                         <wb-remove-answer />
                     </div>
                 </div>
@@ -25,7 +25,7 @@
                 return this.$me.useFormatting ? ',' : ''
             },
             countOfDecimalPlaces() {
-                return this.$me.countOfDecimalPlaces || 15
+                return this.$me.countOfDecimalPlaces || 16
             }
         },
         methods: {
@@ -36,6 +36,13 @@
             answerDoubleQuestion(evnt) {
 
                 const answerString = $(evnt.target).autoNumeric('get');
+                if (answerString.replace(/[^0-9]/g,"").length > 15)
+                {
+                    this.markAnswerAsNotSavedWithMessage('Entered value is bigger. Allow only 15 digits')
+                    return
+                }
+
+
                 const answer = answerString != undefined && answerString != ''
                                 ? parseFloat(answerString)
                                 : null
@@ -46,7 +53,7 @@
                     return
                 }
 
-                if (answer > 9999999999999999 || answer < -9999999999999999)
+                if (answer > 999999999999999 || answer < -999999999999999)
                 {
                     this.markAnswerAsNotSavedWithMessage('Entered value can not be parsed as decimal value')
                     return
