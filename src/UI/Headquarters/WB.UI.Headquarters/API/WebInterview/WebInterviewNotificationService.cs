1f7e5915d48adf8b97c90fde5f0c6b875dd86e34
Victor Gladkikh <me@alfeg.net>
2017-01-23 13:30:48 +0200

    KP-8533 Filter events based on opened section # Hack around the way connections stored in hub group

--- src/UI/Headquarters/WB.UI.Headquarters/API/WebInterview/WebInterviewNotificationService.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/API/WebInterview/WebInterviewNotificationService.cs
@@ -1,26 +1,60 @@
 using System;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNet.SignalR;
 using WB.Core.BoundedContexts.Headquarters.Services.WebInterview;
 using WB.Core.GenericSubdomains.Portable;
 using WB.Core.SharedKernels.DataCollection;
+using WB.Core.SharedKernels.DataCollection.Repositories;
+using WB.Core.SharedKernels.DataCollection.Aggregates;
 
 namespace WB.UI.Headquarters.API.WebInterview
 {
     public class WebInterviewNotificationService : IWebInterviewNotificationService
     {
+        private readonly IStatefulInterviewRepository statefulInterviewRepository;
+
+        public WebInterviewNotificationService(IStatefulInterviewRepository statefulInterviewRepository)
+        {
+            this.statefulInterviewRepository = statefulInterviewRepository;
+        }
+
         // Singleton instance
         private static readonly Lazy<IHubContext> _instance = new Lazy<IHubContext>(
             () => GlobalHost.ConnectionManager.GetHubContext<WebInterview>());
 
         public void RefreshEntities(Guid interviewId, params Identity[] entities)
         {
-            var questionToRefresh = entities.Select(q => q.ToString()).ToArray();
-            if (questionToRefresh.Length > 0)
+            var interview = this.statefulInterviewRepository.Get(interviewId.FormatGuid());
+
+            var refreshLists = from entity in entities
+                     let key = GetRefreshKey(interview, entity)
+                     where key != null
+                     group entity by key into g
+                     select new
+                     {
+                         Key = g.Key,
+                         Questions = g.Select(q => q.ToString()).ToArray()
+                     };
+            
+            foreach(var list in refreshLists)
             {
-                _instance.Value.Clients.Group(interviewId.FormatGuid()).refreshEntities(questionToRefresh);
+                _instance.Value.Clients.Group(list.Key).refreshEntities(list.Questions);
+            }            
+        }
+
+        private string GetRefreshKey(IStatefulInterview interview, Identity entity)
+        {
+            var question = interview.GetQuestion(entity);
+            if (question == null) return null;
+
+            var prefilled = interview.IsQuestionPrefilled(entity);
+            if (prefilled)
+            {
+                return "_" + interview.Id.FormatGuid();
             }
+
+            var parent = interview.GetParentGroup(entity);
+            return $"{parent?.ToString() ?? ""}_{interview.Id.FormatGuid()}";
         }
     }
 }
