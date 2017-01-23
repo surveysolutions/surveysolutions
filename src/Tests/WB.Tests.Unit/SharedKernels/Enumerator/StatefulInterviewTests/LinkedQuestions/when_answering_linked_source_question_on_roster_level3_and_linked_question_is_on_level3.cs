4be83b4535cca70ae65b57944ce28a027f22498c
Pavel Stovpivskyi <sby@rambler.ru>
2017-01-23 19:40:46 +0200

    Added tests for check
    KP-8586

---
+++ src/Tests/WB.Tests.Unit/SharedKernels/Enumerator/StatefulInterviewTests/LinkedQuestions/when_answering_linked_source_question_on_roster_level3_and_linked_question_is_on_level3.cs
@@ -0,0 +1,108 @@
+using System;
+using System.Linq;
+using Machine.Specifications;
+using Main.Core.Entities.Composite;
+using WB.Core.SharedKernels.DataCollection;
+using WB.Core.SharedKernels.DataCollection.Implementation.Aggregates;
+using WB.Core.SharedKernels.DataCollection.Implementation.Entities;
+
+namespace WB.Tests.Unit.SharedKernels.Enumerator.StatefulInterviewTests.LinkedQuestions
+{
+    internal class when_answering_linked_source_question_on_roster_level3_and_linked_question_is_on_level3 : StatefulInterviewTestsContext
+    {
+        Establish context = () =>
+        {
+            var questionnaireDocument = Create.Entity.QuestionnaireDocumentWithOneChapter(children: new IComposite[]
+            {
+                Create.Entity.TextListQuestion(questionId: rosterSizeQuestion1Id),
+                Create.Entity.ListRoster(roster1Id, rosterSizeQuestionId: rosterSizeQuestion1Id, children: new IComposite[]
+                {
+                    Create.Entity.TextListQuestion(questionId: rosterSizeQuestion2Id),
+                    Create.Entity.ListRoster(roster2Id, rosterSizeQuestionId: rosterSizeQuestion2Id, children: new IComposite[]
+                    {
+                        Create.Entity.TextListQuestion(questionId: rosterSizeQuestion3Id),
+                        Create.Entity.ListRoster(roster3Id, rosterSizeQuestionId: rosterSizeQuestion3Id, children: new IComposite[]
+                        {
+                            Create.Entity.SingleOptionQuestion(questionId: linkedSingleQuestionId, linkedToRosterId: roster3Id),
+                            Create.Entity.MultipleOptionsQuestion(questionId: linkedMultiQuestionId, linkedToRosterId: roster3Id),
+                        }),
+                    }),
+                }),
+            });
+            var plainQuestionnaire = new PlainQuestionnaire(questionnaireDocument, 0);
+
+            interview = Create.AggregateRoot.StatefulInterview(questionnaire: plainQuestionnaire);
+            interview.AnswerTextListQuestion(interviewerId, rosterSizeQuestion1Id, RosterVector.Empty, DateTime.UtcNow, new[]
+            {
+                new Tuple<decimal, string>(1, "house 1"),
+                new Tuple<decimal, string>(2, "house 2"),
+            });
+            interview.AnswerTextListQuestion(interviewerId, rosterSizeQuestion2Id, Create.Entity.RosterVector(1), DateTime.UtcNow, new[]
+            {
+                new Tuple<decimal, string>(1, "house 1 person 1"),
+                new Tuple<decimal, string>(2, "house 1 person 2"),
+            });
+            interview.AnswerTextListQuestion(interviewerId, rosterSizeQuestion2Id, Create.Entity.RosterVector(2), DateTime.UtcNow, new[]
+            {
+                new Tuple<decimal, string>(1, "house 2 person 1"),
+                new Tuple<decimal, string>(2, "house 2 person 2"),
+            });
+            interview.AnswerTextListQuestion(interviewerId, rosterSizeQuestion3Id, Create.Entity.RosterVector(1, 1), DateTime.UtcNow, new[]
+            {
+                new Tuple<decimal, string>(1, "house 1 person 1 pet 1"),
+                new Tuple<decimal, string>(2, "house 1 person 1 pet 2"),
+            });
+            interview.AnswerTextListQuestion(interviewerId, rosterSizeQuestion3Id, Create.Entity.RosterVector(1, 2), DateTime.UtcNow, new[]
+            {
+                new Tuple<decimal, string>(1, "house 1 person 2 pet 1"),
+                new Tuple<decimal, string>(2, "house 1 person 2 pet 2"),
+            });
+            interview.AnswerTextListQuestion(interviewerId, rosterSizeQuestion3Id, Create.Entity.RosterVector(2, 1), DateTime.UtcNow, new[]
+            {
+                new Tuple<decimal, string>(1, "house 2 person 1 pet 1"),
+                new Tuple<decimal, string>(2, "house 2 person 1 pet 2"),
+            });
+        };
+
+        Because of = () =>
+            interview.AnswerTextListQuestion(interviewerId, rosterSizeQuestion3Id, Create.Entity.RosterVector(2, 2), DateTime.UtcNow, new[]
+            {
+                new Tuple<decimal, string>(1, "house 2 person 2 pet 1"),
+                new Tuple<decimal, string>(2, "house 2 person 2 pet 2"),
+            });
+
+        It should_linked_single_question_has_2_options = () =>
+        {
+            var identity = Create.Entity.Identity(linkedSingleQuestionId, Create.Entity.RosterVector(2, 2, 2));
+            var linkedSingleOptionQuestion = interview.GetLinkedSingleOptionQuestion(identity);
+            linkedSingleOptionQuestion.Options.Count.ShouldEqual(2);
+            
+            interview.GetLinkedOptionTitle(identity, linkedSingleOptionQuestion.Options.First()).ShouldEqual("house 2 person 2 pet 1");
+            interview.GetLinkedOptionTitle(identity, linkedSingleOptionQuestion.Options.Second()).ShouldEqual("house 2 person 2 pet 2");
+        };
+
+        It should_linked_multi_question_has_2_options = () =>
+        {
+            var identity = Create.Entity.Identity(linkedSingleQuestionId, Create.Entity.RosterVector(2, 2, 2));
+            var linkedSingleOptionQuestion = interview.GetLinkedSingleOptionQuestion(identity);
+            linkedSingleOptionQuestion.Options.Count.ShouldEqual(2);
+            
+            interview.GetLinkedOptionTitle(identity, linkedSingleOptionQuestion.Options.First()).ShouldEqual("house 2 person 2 pet 1");
+            interview.GetLinkedOptionTitle(identity, linkedSingleOptionQuestion.Options.Second()).ShouldEqual("house 2 person 2 pet 2");
+        };
+
+        static StatefulInterview interview;
+
+        static readonly Guid roster1Id = Guid.Parse("11111111111111111111111111111111");
+        static readonly Guid roster2Id = Guid.Parse("22222222222222222222222222222222");
+        static readonly Guid roster3Id = Guid.Parse("33333333333333333333333333333333");
+
+        static readonly Guid rosterSizeQuestion1Id = Guid.Parse("44444444444444444444444444444444");
+        static readonly Guid rosterSizeQuestion2Id = Guid.Parse("55555555555555555555555555555555");
+        static readonly Guid rosterSizeQuestion3Id = Guid.Parse("77777777777777777777777777777777");
+
+        static readonly Guid linkedSingleQuestionId = Guid.Parse("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+        static readonly Guid linkedMultiQuestionId  = Guid.Parse("BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB");
+        static readonly Guid interviewerId = Guid.Parse("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF");
+    }
+}
