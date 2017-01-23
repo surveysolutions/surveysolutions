66d5697ddee9f646b726420e9093b579969ae468
Victor Hladkikh <me@alfeg.net>
2017-01-23 15:32:10 +0200

    More dead code eliminated.

--- src/UI/Headquarters/WB.UI.Headquarters.Interview/src/store/store.mutations.ts
+++ src/UI/Headquarters/WB.UI.Headquarters.Interview/src/store/store.mutations.ts
@@ -8,13 +8,13 @@ export default {
     SET_ENTITY_DETAILS(state, entity: IInterviewEntity) {
         Vue.set(state.entityDetails, entity.id, entity)
     },
-    SET_SECTION_DATA(state, sectionData: ISectionData) {
+    SET_SECTION_DATA(state, sectionData: IInterviewEntityWithType[]) {
         state.entities = sectionData
     },
     CLEAR_ENTITY(state, id) {
         Vue.delete(state.entityDetails, id)
     },
-    SET_ANSWER_NOT_SAVED(state, {id, message}) {
+    SET_ANSWER_NOT_SAVED(state, { id, message }) {
         const validity = state.entityDetails[id].validity
         Vue.set(validity, "errorMessage", true)
         validity.messages = [message]
