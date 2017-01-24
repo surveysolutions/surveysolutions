b0c0a198a109d920e77701b0e3f027771f91848d
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2017-01-24 11:07:30 -0500

    KP-8590 Move questionnaire assemblies to DB

---
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Views/Questionnaire/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace WB.Core.BoundedContexts.Headquarters.Views.Questionnaire
+{
+    public class AssemblyInfo
+    {
+        public virtual string AssemblyId { get; set; }
+        public virtual DateTime CreationDate { get; set; }
+        public virtual byte[] Content { get; set; }
+    }
+}
