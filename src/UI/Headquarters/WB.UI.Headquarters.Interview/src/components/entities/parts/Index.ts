3a0cc3371ed4958f31d0c54a84ccf3279b990065
Victor Gladkikh <me@alfeg.net>
2017-01-25 14:04:32 +0200

    KP-8524: Answering progress report # Added fetch notification on each answer

--- src/UI/Headquarters/WB.UI.Headquarters.Interview/src/components/entities/parts/Index.ts
+++ src/UI/Headquarters/WB.UI.Headquarters.Interview/src/components/entities/parts/Index.ts
@@ -2,6 +2,7 @@ import * as vue from "vue"
 
 import Attachment from "./Attachment"
 import Instructions from "./Instructions"
+import Progress from "./Progress"
 import RemoveAnswer from "./RemoveAnswer"
 import Title from "./Title"
 import Validation from "./Validation"
@@ -11,3 +12,4 @@ vue.component("wb-title", Title)
 vue.component("wb-validation", Validation)
 vue.component("wb-remove-answer", RemoveAnswer)
 vue.component("wb-attachment", Attachment)
+vue.component("wb-progress", Progress)
---
+++ src/UI/Headquarters/WB.UI.Headquarters.Interview/src/components/entities/parts/Progress.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="progress question-progress" v-if="isVisible">
+        <div class="progress-bar progress-bar-striped active" role="progressbar" :style="style">
+            <span class="sr-only">{{progress}}% Complete</span>
+        </div>
+    </div>
+</template>
+<script lang="ts">
+    import * as delay from "lodash/delay"
+    export default {
+        name: "wb-progress",
+        props: {
+            valuenow: { type: Number, default: 100 },
+            valuemax: { type: Number, default: 100 },
+            visible: { type: Boolean, default: false },
+            delay: { type: Number, default: 100 } // 100 ms default delay
+        },
+        watch: {
+            visible(to: Boolean, from: Boolean) {
+                if (from === false) {
+                    this.timerId = delay(() => this.isVisible = to, this.delay)
+                } else {
+                    if(this.timerId != null){
+                        clearTimeout(this.timerId)
+                        this.timerId = null
+                    }
+
+                    this.isVisible = this.to
+                }
+            }
+        },
+        data() {
+            return {
+                isVisible: false,
+                timerId: null
+            }
+        },
+        computed: {
+            progress() {
+                return Math.round((this.valuemax / this.valuenow) * 100)
+            },
+            style() {
+                return {
+                    width: this.progress + "%"
+                }
+            }
+        }
+    }
+</script>
