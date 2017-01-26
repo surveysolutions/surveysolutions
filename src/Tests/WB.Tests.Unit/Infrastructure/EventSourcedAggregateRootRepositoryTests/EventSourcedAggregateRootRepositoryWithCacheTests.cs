9425b2a3eb76be1883129c1ff51202b14df89461
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2017-01-26 11:19:08 +0200

    EventSourcedAggregateRootRepositoryWithCache no longer returns dirty aggregates KP-8571

---
+++ src/Tests/WB.Tests.Unit/Infrastructure/EventSourcedAggregateRootRepositoryTests/EventSourcedAggregateRootRepositoryWithCacheTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace WB.Tests.Unit.Infrastructure.EventSourcedAggregateRootRepositoryTests
+{
+    //[TestFixture]
+    //internal class EventSourcedAggregateRootRepositoryWithCacheTests
+    //{
+    //    public void When_cached_aggregate_has_uncommitted_changes_Then_returns_newly_loaded_aggregate()
+    //    {
+            
+    //    }
+    //}
+}
