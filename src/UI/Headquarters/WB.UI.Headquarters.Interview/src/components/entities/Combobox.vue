1e49939ceb0110e4c7ffabd0fd80879e0b460dc8
Andrii Kozhyn <hedgic@gmail.com>
2017-01-30 12:36:44 +0200

    KP-8500: Implemented default template for combobox

---
+++ src/UI/Headquarters/WB.UI.Headquarters.Interview/src/components/entities/Combobox.vue
@@ -0,0 +1,36 @@
+<template>
+    <wb-question :question="$me" questionCssClassName="single-select-question">
+        <div class="question-unit">
+            <div class="options-group">
+                <div class="form-group">
+                    <div class="field answered">
+                        <input type="text" class="field-to-fill" />
+                        <wb-remove-answer />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </wb-question>
+</template>
+<script lang="ts">
+    import { entityDetails } from "components/mixins"
+
+    export default {
+        name: 'ComboboxQuestion',
+        mixins: [entityDetails],
+        data() {
+            return {
+                selected: null
+            }
+        },
+        computed: {
+
+        },
+        methods: {
+        },
+        directives: {
+
+        },
+        components: {  }
+    }
+</script>
--- src/UI/Headquarters/WB.UI.Headquarters.Interview/src/components/entities/TextQuestion.vue
+++ src/UI/Headquarters/WB.UI.Headquarters.Interview/src/components/entities/TextQuestion.vue
@@ -3,7 +3,7 @@
         <div class="question-unit">
             <div class="options-group">
                 <div class="form-group">
-                    <div class="field answered">
+                    <div class="field" :class="{answered: $me.isAnswered}">
                         <input autocomplete="off" type="text" class="field-to-fill" :placeholder="'Enter answer ' + userFriendlyMask" :value="$me.answer"
                             @blur="answerTextQuestion" v-mask="$me.mask" :data-mask-completed="$me.isAnswered">
                         <wb-remove-answer />
