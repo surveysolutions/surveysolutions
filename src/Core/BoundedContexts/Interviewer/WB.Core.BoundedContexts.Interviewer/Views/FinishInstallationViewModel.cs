293c0d7cc230f9d69ce4f30fa72d72e4f9d1b250
Roman Lytvyn <litvin.roma@gmail.com>
2017-01-31 17:27:44 +0200

    IN: next try to fix: Crash Interviewer app on answering Picture question KP-8635

--- src/Core/BoundedContexts/Interviewer/WB.Core.BoundedContexts.Interviewer/Views/FinishInstallationViewModel.cs
+++ src/Core/BoundedContexts/Interviewer/WB.Core.BoundedContexts.Interviewer/Views/FinishInstallationViewModel.cs
@@ -118,9 +118,9 @@ namespace WB.Core.BoundedContexts.Interviewer.Views
             this.UserName = this.userIdentity.Name;
 
 #if DEBUG
-            this.Endpoint = "https://superhq-rc.mysurvey.solutions";
-            this.UserName = "ank";
-            this.Password = "Qwerty1234";
+            this.Endpoint = "http://192.168.88./headquarters";
+            this.UserName = "int";
+            this.Password = "1";
 #endif
         }
 
