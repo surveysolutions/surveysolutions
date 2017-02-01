dc136ea89f0e931e6e354bdec2b2ebb89e3df1a2
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2017-02-01 13:37:01 +0200

    Interview\Invariants: separated invariants for preloading/answer and for linked/fixed for single option questions KP-7837

--- src/Core/SharedKernels/DataCollection/DataCollection/Implementation/Aggregates/Interview.SingleOptionLinkedQuestion.cs
+++ src/Core/SharedKernels/DataCollection/DataCollection/Implementation/Aggregates/Interview.SingleOptionLinkedQuestion.cs
@@ -18,7 +18,7 @@ namespace WB.Core.SharedKernels.DataCollection.Implementation.Aggregates
 
             IQuestionnaire questionnaire = this.GetQuestionnaireOrThrow();
 
-            this.CheckLinkedSingleOptionQuestionInvariants(questionIdentity, selectedRosterVector, questionnaire, this.Tree);
+            this.RequireLinkedToRosterSingleOptionAnswerAllowed(questionIdentity, selectedRosterVector, questionnaire, this.Tree);
 
             var changedInterviewTree = this.Tree.Clone();
 
