e232844f54e6fb027b5f015ec3bef25fad22d872
Pavel Stovpivskyi <630198+spuv@users.noreply.github.com>
2017-02-02 12:20:54 +0200

    KP-8553 Categorical question filters support

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/HeadquartersBoundedContextModule.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/HeadquartersBoundedContextModule.cs
@@ -291,6 +291,7 @@ namespace WB.Core.BoundedContexts.Headquarters
 
             this.Kernel.RegisterDenormalizer<InterviewEventHandlerFunctional>();
             this.Kernel.RegisterDenormalizer<InterviewLifecycleEventHandler>();
+            this.Kernel.RegisterDenormalizer<WebInterviewFilteredOptionsRefreshDenormalizer>();
             this.Kernel.RegisterDenormalizer<StatusChangeHistoryDenormalizerFunctional>();
             this.Kernel.RegisterDenormalizer<InterviewExportedCommentariesDenormalizer>();
             this.Kernel.RegisterDenormalizer<InterviewStatusTimeSpanDenormalizer>();
