868d6fb999d72c460ec4e17e89214ec88b084a70
Victor Hladkikh <me@alfeg.net>
2017-02-04 10:21:36 +0200

    Added webpack build visualization to build artifacts

--- src/UI/Headquarters/WB.UI.Headquarters.Interview/build/webpack.prod.conf.js
+++ src/UI/Headquarters/WB.UI.Headquarters.Interview/build/webpack.prod.conf.js
@@ -6,6 +6,8 @@ var merge = require('webpack-merge')
 var baseWebpackConfig = require('./webpack.base.conf')
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
+
+var Visualizer = require('webpack-visualizer-plugin');
 var env = process.env.NODE_ENV === 'testing'
   ? require('../config/test.env')
   : config.build.env
@@ -26,6 +28,7 @@ var webpackConfig = merge(baseWebpackConfig, {
       extract: true
     })
   },
+  profile: true,
   plugins: [
     // http://vuejs.github.io/vue-loader/en/workflow/production.html
     new webpack.DefinePlugin({
@@ -77,7 +80,8 @@ var webpackConfig = merge(baseWebpackConfig, {
     new webpack.optimize.CommonsChunkPlugin({
       name: 'manifest',
       chunks: ['vendor']
-    })
+    }),
+    new Visualizer()
   ]
 })
 
