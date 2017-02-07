d44dc4afe642d020c809b738683989503aebba40
Roman Lytvyn <litvin.roma@gmail.com>
2017-02-07 15:58:07 +0200

    HQ: Web interview. Fixed text list without max answers count. KP-8545

--- src/UI/Headquarters/WB.UI.Headquarters.Interview/src/components/entities/TextList.vue
+++ src/UI/Headquarters/WB.UI.Headquarters.Interview/src/components/entities/TextList.vue
@@ -8,7 +8,7 @@
                         <button type="submit" class="btn btn-link btn-clear" v-on:click="confirmAndRemoveRow(index)"><span></span></button>
                     </div>
                 </div>
-                <div class="form-group" v-if="$me.rows == undefined || $me.maxAnswersCount > $me.rows.length">
+                <div class="form-group" v-if="$me.rows == undefined || $me.maxAnswersCount == null || $me.maxAnswersCount > $me.rows.length">
                     <div class="field answered">
                         <input autocomplete="off" type="text" class="field-to-fill" placeholder="Tap to enter new item" @blur="addRow" @keyup.enter="addRow"/>
                     </div>
