353ad4c08d04e002d6a40b5a3d9c451927afc48c
Victor Hladkikh <me@alfeg.net>
2017-02-08 19:33:26 +0200

    KP-8453 HQ: Web interview. Limit for parallel web interviews # Proper counter

--- src/UI/Headquarters/WB.UI.Headquarters/API/WebInterview/Pipeline/WebInterviewConnectionsCounter.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/API/WebInterview/Pipeline/WebInterviewConnectionsCounter.cs
@@ -5,6 +5,7 @@ using System.Diagnostics;
 using System.Linq;
 using Microsoft.AspNet.SignalR.Hubs;
 using Prometheus;
+using WB.Core.GenericSubdomains.Portable.CustomCollections;
 using WB.UI.Headquarters.API.WebInterview.Services;
 
 namespace WB.UI.Headquarters.API.WebInterview.Pipeline
@@ -18,68 +19,46 @@ namespace WB.UI.Headquarters.API.WebInterview.Pipeline
         }
 
         private readonly Gauge currentConnectionsCount = Metrics.CreateGauge(ConnectionLimiter.ConnectedMetricName, @"Number of connection to interview");
-        private readonly Gauge messageProcessTime = Metrics.CreateGauge("webinterview_message_processing_time", "Processing time per metric", "type", "action");
-        private readonly Counter messagesTotal = Metrics.CreateCounter(@"webinterview_messages_processed", @"Total count messages", "direction", "action");
+        private readonly Gauge messageProcessTime = Metrics.CreateGauge("webinterview_message_processing_time", "Processing time per metric", "type");
+        private readonly Counter messagesTotal = Metrics.CreateCounter(@"webinterview_messages_processed", @"Total count messages", "direction");
 
-        private readonly ConcurrentDictionary<string, HashSet<string>> connectedClients = new ConcurrentDictionary<string, HashSet<string>>();
+        private readonly ConcurrentDictionary<string, ConcurrentHashSet<string>> connectedClients = new ConcurrentDictionary<string, ConcurrentHashSet<string>>();
+
+        private ConcurrentHashSet<string> ConnectedToInterview(string interviewId) => this.connectedClients.GetOrAdd(interviewId, key => new ConcurrentHashSet<string>());
+        private string InterviewId(IHub hub) => hub.Context.QueryString["interviewId"];
 
         protected override bool OnBeforeConnect(IHub hub)
         {
-            var interviewId = hub.Context.QueryString.Get(@"interviewId");
-            this.connectedClients.AddOrUpdate(interviewId,
-                id =>
-                {
-                    this.currentConnectionsCount.Inc();
-                    return new HashSet<string>() { hub.Context.ConnectionId };
-                }, (id, list) =>
-                {
-                    if (!list.Any())
-                    {
-                        this.currentConnectionsCount.Inc();
-                    }
+            var interviewId = InterviewId(hub);
 
-                    list.Add(hub.Context.ConnectionId);
-                    return list;
-                });
-            
+            var list = ConnectedToInterview(interviewId);
+            list.Add(hub.Context.ConnectionId);
+            CalculateConnected();
             return base.OnBeforeConnect(hub);
         }
 
         protected override void OnAfterReconnect(IHub hub)
         {
-            var interviewId = hub.Context.QueryString.Get(@"interviewId");
-
-            this.connectedClients.AddOrUpdate(interviewId,
-                id =>
-                {
-                    this.currentConnectionsCount.Inc();
-                    return new HashSet<string> {hub.Context.ConnectionId};
-                }, 
-                (id, list) =>
-                {
-                    list.Add(hub.Context.ConnectionId);
-                    return list;
-                });
-
+            var interviewId = InterviewId(hub);
+            var list = ConnectedToInterview(interviewId);
+            list.Add(hub.Context.ConnectionId);
+            CalculateConnected();
             base.OnAfterReconnect(hub);
         }
 
         protected override void OnAfterDisconnect(IHub hub, bool stopCalled)
         {
-            var interviewId = hub.Context.QueryString.Get(@"interviewId");
-            this.connectedClients.AddOrUpdate(interviewId, key => new HashSet<string>(),
-                (id, list) =>
-                {
-                    bool isListEmpty = !list.Any();
-                    list.Remove(hub.Context.ConnectionId);
-
-                    if (!isListEmpty && !list.Any())
-                    {
-                        this.currentConnectionsCount.Dec();
-                    }
-                    return list;
-                });
+            var interviewId = InterviewId(hub);
+            var list = ConnectedToInterview(interviewId);
+            list.Remove(hub.Context.ConnectionId);
+            CalculateConnected();
             base.OnAfterDisconnect(hub, stopCalled);
+        }
+
+        private void CalculateConnected()
+        {
+            var count = this.connectedClients.Count(l => l.Value.Any());
+            this.currentConnectionsCount.Set(count);
         }
 
         private readonly ConcurrentDictionary<string, Stopwatch> _timeMetric = new ConcurrentDictionary<string, Stopwatch>();
@@ -87,9 +66,9 @@ namespace WB.UI.Headquarters.API.WebInterview.Pipeline
         protected override bool OnBeforeIncoming(IHubIncomingInvokerContext context)
         {
             var connId = context.Hub.Context.ConnectionId;
-            this.messagesTotal.Labels("incoming", "").Inc();
+            this.messagesTotal.Labels("incoming").Inc();
 
-            while (!this._timeMetric.TryAdd(connId, Stopwatch.StartNew())) ;
+            while (!this._timeMetric.TryAdd(connId, Stopwatch.StartNew())) { }
 
             return base.OnBeforeIncoming(context);
         }
@@ -100,10 +79,15 @@ namespace WB.UI.Headquarters.API.WebInterview.Pipeline
             Stopwatch sw;
             if (this._timeMetric.TryRemove(connId, out sw))
             {
-                this.messageProcessTime.Labels("incoming", "").Set(sw.Elapsed.TotalSeconds);
+                this.messageProcessTime.Labels("incoming").Set(sw.Elapsed.TotalSeconds);
             }
 
             return base.OnAfterIncoming(result, context);
+        }
+
+        protected override void OnAfterOutgoing(IHubOutgoingInvokerContext context)
+        {
+            this.messagesTotal.Labels("outgoing").Inc();
         }
     }
 }
