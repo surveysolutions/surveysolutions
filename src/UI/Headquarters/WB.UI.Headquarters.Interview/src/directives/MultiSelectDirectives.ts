20e423401217757ef0fcb874ad0e1ff598a3c2a8
Andrii Kozhyn <hedgic@gmail.com>
2017-02-08 15:46:15 +0200

    Do not use jquery to disable inputs

--- src/UI/Headquarters/WB.UI.Headquarters.Interview/src/directives/MultiSelectDirectives.ts
+++ src/UI/Headquarters/WB.UI.Headquarters.Interview/src/directives/MultiSelectDirectives.ts
@@ -1,11 +1,10 @@
-import * as $ from "jquery"
 import * as Vue from "vue"
 
 Vue.directive("disabledWhenUnchecked", {
-    bind: (el, binding) => {
-        $(el).prop("disabled", binding.value && !el.checked)
+    bind: (el: HTMLInputElement, binding) => {
+        el.disabled = binding.value && !el.checked
     },
-    update: (el, binding) => {
-        $(el).prop("disabled", binding.value && !(<HTMLInputElement> el).checked)
+    update: (el: HTMLInputElement, binding) => {
+        el.disabled = binding.value && !el.checked
     }
 })
