015963bfb5d1086787a3f87a1e2456de06017bce
Roman Lytvyn <litvin.roma@gmail.com>
2017-02-09 21:42:46 +0200

    HQ: Web interview. code review changes by: Human readable interview id KP-8609

--- src/UI/Headquarters/WB.UI.Headquarters/Migrations/ReadSide/M001-100/M008_AddInterviewHumanId.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/Migrations/ReadSide/M001-100/M008_AddInterviewHumanId.cs
@@ -10,16 +10,16 @@ namespace WB.UI.Headquarters.Migrations.ReadSide
             this.Create.Column(@"humanid").OnTable(@"interviewsummaries").AsInt32().Nullable();
             this.Create.Index(@"interviewsummaries_humanid").OnTable(@"interviewsummaries").OnColumn(@"humanid");
             
-            this.Execute.Sql(Properties.Resources.add_human_interview_id);
+            this.Execute.EmbeddedScript(@"add-human-interview-id.sql");
         }
 
         public override void Down()
         {
             this.Delete.Column(@"humanid").FromTable(@"interviewsummaries");
             this.Delete.Index(@"interviewsummaries_humanid").OnTable(@"interviewsummaries");
-            
 
-            this.Execute.Sql(Properties.Resources.remove_human_interview_id);
+
+            this.Execute.EmbeddedScript(@"remove-human-interview-id.sql");
         }
     }
 }
