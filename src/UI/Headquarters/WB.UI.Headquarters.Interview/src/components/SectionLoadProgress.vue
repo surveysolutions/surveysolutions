af55e0e9c7de955e10ec1288752a1af136b408f7
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2017-02-10 13:51:46 +0200

    KP-8717: Counter for fetched entities on client
    KP-8717: merge

---
+++ src/UI/Headquarters/WB.UI.Headquarters.Interview/src/components/SectionLoadProgress.vue
@@ -0,0 +1,19 @@
+<template>
+    <div v-if="show">
+        <p class="text-warning">Loading...</p>
+    </div>
+</template>
+<script lang="ts">
+    import * as toastr from "toastr"
+    export default {
+        computed: {
+            show() {
+                const loadedCount = this.$store.state.loadedEntitiesCount
+                const totalCount = this.$store.state.entities.length
+
+                return false /*temp disable this before we have markup*/ || loadedCount === 0 || totalCount === 0 || (loadedCount < totalCount)
+            }
+        }
+    }
+
+</script>
