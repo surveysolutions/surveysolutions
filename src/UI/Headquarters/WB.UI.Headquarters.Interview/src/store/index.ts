3abc136c64a424d7dba4b6d981a5524d85a80e85
Andrii Kozhyn <hedgic@gmail.com>
2017-02-14 14:25:25 +0200

    KP-8708: Added warning message on slow connection and offline

--- src/UI/Headquarters/WB.UI.Headquarters.Interview/src/store/index.ts
+++ src/UI/Headquarters/WB.UI.Headquarters.Interview/src/store/index.ts
@@ -4,12 +4,13 @@ import * as Vuex from "vuex"
 import { safeStore } from "../errors"
 
 import actions from "./store.actions"
+import connection from "./store.connection"
 import fetch from "./store.fetch"
 import mutations from "./store.mutations"
 import sidebar from "./store.sidebar"
 
 const store = new Vuex.Store(safeStore({
-    modules: { fetch, sidebar },
+    modules: { fetch, sidebar, connection },
     state: {
         lastActivityTimestamp: new Date(),
         hasPrefilledQuestions: false,
---
+++ src/UI/Headquarters/WB.UI.Headquarters.Interview/src/store/store.connection.ts
@@ -0,0 +1,56 @@
+import * as bootbox from "bootbox"
+import * as toastr from "toastr"
+import * as Vue from "vue"
+import { Store } from "vuex"
+import { apiStop } from "../api"
+
+declare interface IConnectionState {
+    isReconnecting: boolean,
+    isDisconnected: boolean
+}
+
+const connectionStore = {
+    state: {
+        isReconnecting: false,
+        isDisconnected: false
+    },
+    actions: {
+        connectionSlow() {
+            toastr.warning("Network connection is slow")
+        },
+        tryingToReconnect({commit}, isReconnecting: boolean) {
+            commit("IS_RECONNECTING", isReconnecting)
+        },
+        disconnected({state, commit}: Store<IConnectionState>) {
+            if (state.isReconnecting && !state.isDisconnected) {
+                commit("IS_DISCONNECTED", true)
+                apiStop()
+                bootbox.alert({
+                    title: "Disconnected",
+                    message: "<p>Connection to server is lost.</p>" +
+                    "<p>Please reload the page to restore connection and continue this interview.</p>",
+                    callback: () => {
+                        location.reload()
+                    },
+                    onEscape: false,
+                    closeButton: false,
+                    buttons: {
+                        ok: {
+                            label: "Reload",
+                            className: "btn-success"
+                        }
+                    }
+                })
+            }
+        }
+    },
+    mutations: {
+        IS_RECONNECTING(state: IConnectionState, isReconnecting: boolean) {
+            state.isReconnecting = isReconnecting
+        },
+        IS_DISCONNECTED(state: IConnectionState, isDisconnected: boolean) {
+            state.isDisconnected = isDisconnected
+        }
+    }
+}
+export default connectionStore
