ae35a1461151443ba0adf4e8bd3dec6b9c736f7e
Victor Gladkikh <me@alfeg.net>
2017-02-15 11:48:24 +0200

    Fix for possible NRE in WebInterviewAllowedModule. Added exceptions counter

--- src/UI/Headquarters/WB.UI.Headquarters/API/WebInterview/Pipeline/WebInterviewAllowedModule.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/API/WebInterview/Pipeline/WebInterviewAllowedModule.cs
@@ -13,18 +13,12 @@ namespace WB.UI.Headquarters.API.WebInterview.Pipeline
 {
     public class WebInterviewAllowedModule : HubPipelineModule
     {
-        private IReadSideKeyValueStorage<InterviewReferences> interviewReferences
-            => ServiceLocator.Current.GetInstance<IReadSideKeyValueStorage<InterviewReferences>>();
-
         private IWebInterviewConfigProvider configProvider =>
             ServiceLocator.Current.GetInstance<IWebInterviewConfigProvider>();
 
         private IStatefullWebInterviewFactory statefullWebInterviewFactory =>
             ServiceLocator.Current.GetInstance<IStatefullWebInterviewFactory>();
-
-        private IQueryableReadSideRepositoryReader<InterviewSummary> InterviewSummaryStorage => 
-            ServiceLocator.Current.GetInstance<IQueryableReadSideRepositoryReader<InterviewSummary>>();
-
+        
         private IPlainTransactionManager transactionManager
           => ServiceLocator.Current.GetInstance<IPlainTransactionManagerProvider>().GetPlainTransactionManager();
 
@@ -34,19 +28,14 @@ namespace WB.UI.Headquarters.API.WebInterview.Pipeline
         protected override bool OnBeforeConnect(IHub hub)
         {
             QuestionnaireIdentity questionnaireIdentity = null;
-            bool interviewIsCompleted = this.readTransactionManager.ExecuteInQueryTransaction(() =>
+            bool interviewAcceptAnswers = this.readTransactionManager.ExecuteInQueryTransaction(() =>
             {
-                var interviewId = this.statefullWebInterviewFactory.GetInterviewIdByHumanId(hub.Context.QueryString.Get(@"interviewId"));
-
-                var questionnaire = this.interviewReferences.GetById(interviewId);
-                questionnaireIdentity = new QuestionnaireIdentity(questionnaire.QuestionnaireId, questionnaire.QuestionnaireVersion);
-
-                var interviewSummary = this.InterviewSummaryStorage.GetById(interviewId);
-
-                return interviewSummary.Status != InterviewStatus.InterviewerAssigned;
+                var interview = this.statefullWebInterviewFactory.Get(hub.Context.QueryString.Get(@"interviewId"));
+                questionnaireIdentity = interview?.QuestionnaireIdentity;
+                return interview?.AcceptsInterviewerAnswers() ?? false;
             });
 
-            if (interviewIsCompleted || questionnaireIdentity == null) throw new NotAuthorizedException();
+            if (!interviewAcceptAnswers || questionnaireIdentity == null) throw new NotAuthorizedException();
 
             WebInterviewConfig webInterviewConfig = null;
             this.transactionManager.ExecuteInPlainTransaction(() =>
