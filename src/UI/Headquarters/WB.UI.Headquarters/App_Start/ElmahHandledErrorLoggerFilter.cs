7ad472a5cf663b6b7a2ef5c7f6247cf6e45ee7b4
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2017-02-14 19:01:02 -0500

    KP-8747 Do not display handled exception in Elmah

--- src/UI/Headquarters/WB.UI.Headquarters/App_Start/ElmahHandledErrorLoggerFilter.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/App_Start/ElmahHandledErrorLoggerFilter.cs
@@ -1,5 +1,6 @@
 ﻿using System.Web.Http.Filters;
 using Elmah;
+using WB.UI.Headquarters.Code;
 
 namespace WB.UI.Headquarters
 {
@@ -7,7 +8,11 @@ namespace WB.UI.Headquarters
     {
         public override void OnException(HttpActionExecutedContext actionExecutedContext)
         {
+
             base.OnException(actionExecutedContext);
+
+            if (actionExecutedContext.Exception is WebInterviewAccessException)
+                return;
 
             ErrorSignal.FromCurrentContext().Raise(actionExecutedContext.Exception);
         }
