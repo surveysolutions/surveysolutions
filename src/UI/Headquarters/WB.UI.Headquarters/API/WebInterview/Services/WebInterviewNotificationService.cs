c79970664e99d77e450684b1d421baaa5a51d5b7
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2017-02-16 15:30:47 -0500

    KP-8767 Sub-section/roster buttons don't change the color in accordance with the status

--- src/UI/Headquarters/WB.UI.Headquarters/API/WebInterview/Services/WebInterviewNotificationService.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/API/WebInterview/Services/WebInterviewNotificationService.cs
@@ -30,7 +30,26 @@ namespace WB.UI.Headquarters.API.WebInterview.Services
         {
             var interview = this.statefulInterviewRepository.Get(interviewId.FormatGuid());
 
-            var entitiesToRefresh = entities.Select(identity => Tuple.Create(this.GetClientGroupIdentity(identity, interview), identity));
+            var entitiesToRefresh = new List<Tuple<string, Identity>>();
+
+            foreach (var identity in entities)
+            {
+                var parent = this.CheckAndGetParentIdentity(identity, interview);
+                entitiesToRefresh.Add(Tuple.Create(WebInterview.GetConnectedClientSectionKey(parent?.ToString(), interview.Id.FormatGuid()), identity));
+
+                if (parent != null)
+                {
+                    while (parent != null)
+                    {
+                        var newEntity = parent;
+                        parent = this.CheckAndGetParentIdentity(newEntity, interview);
+                        if (parent != null)
+                            entitiesToRefresh.Add(
+                                Tuple.Create(WebInterview.GetConnectedClientSectionKey(parent?.ToString(),
+                                        interview.Id.FormatGuid()), newEntity));
+                    }
+                }
+            }
 
             foreach (var questionsGroupedByParent in entitiesToRefresh.GroupBy(x => x.Item1))
             {
@@ -42,7 +61,7 @@ namespace WB.UI.Headquarters.API.WebInterview.Services
                 var clients = this.webInterviewHubContext.Clients;
                 var group = clients.Group(questionsGroupedByParent.Key);
 
-                group.refreshEntities(questionsGroupedByParent.Select(p => p.Item2.ToString()).ToArray());
+                group.refreshEntities(questionsGroupedByParent.Select(p => p.Item2.ToString()).Distinct().ToArray());
             }
 
             this.webInterviewHubContext.Clients.Group(interviewId.FormatGuid()).refreshSection();
@@ -105,18 +124,9 @@ namespace WB.UI.Headquarters.API.WebInterview.Services
             }
         }
 
-        private Identity GetParentIdentity(Identity identity, IStatefulInterview interview)
+        private Identity CheckAndGetParentIdentity(Identity identity, IStatefulInterview interview)
         {
-            return ((IInterviewTreeNode) interview.GetQuestion(identity)
-                    ?? (IInterviewTreeNode) interview.GetStaticText(identity)
-                    ?? (IInterviewTreeNode) interview.GetRoster(identity)
-                    ?? (IInterviewTreeNode) interview.GetGroup(identity))
-                ?.Parent?.Identity;
-        }
-
-        private string GetClientGroupIdentity(Identity identity, IStatefulInterview interview)
-        {
-            string sectionKey;
+            Identity sectionKey;
 
             if (interview.GetQuestion(identity)?.IsPrefilled ?? false)
             {
@@ -124,10 +134,20 @@ namespace WB.UI.Headquarters.API.WebInterview.Services
             }
             else
             {
-                sectionKey = this.GetParentIdentity(identity, interview)?.ToString();
+                sectionKey = ((IInterviewTreeNode)interview.GetQuestion(identity)
+                    ?? (IInterviewTreeNode)interview.GetStaticText(identity)
+                    ?? (IInterviewTreeNode)interview.GetRoster(identity)
+                    ?? (IInterviewTreeNode)interview.GetGroup(identity))?.Parent?.Identity;
             }
 
+            return sectionKey;
+        }
+
+        private string GetClientGroupIdentity(Identity identity, IStatefulInterview interview)
+        {
+            var sectionKey = CheckAndGetParentIdentity(identity, interview)?.ToString();
             return WebInterview.GetConnectedClientSectionKey(sectionKey, interview.Id.FormatGuid());
         }
+        
     }
 }
