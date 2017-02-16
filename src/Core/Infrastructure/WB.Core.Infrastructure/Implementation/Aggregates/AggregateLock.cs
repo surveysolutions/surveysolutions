f28b6145f3ec2bfa5034849b41972a57a0c0f2c3
Victor Hladkikh <me@alfeg.net>
2017-02-16 17:44:07 +0200

    KP-8759 Broken event streams can be generated # Lock for command executions

---
+++ src/Core/Infrastructure/WB.Core.Infrastructure/Implementation/Aggregates/AggregateLock.cs
@@ -0,0 +1,20 @@
+using System;
+using WB.Core.GenericSubdomains.Portable;
+
+namespace WB.Core.Infrastructure.Implementation.Aggregates
+{
+    public class AggregateLock : IAggregateLock
+    {
+        private readonly NamedLocker locker = new NamedLocker();
+        
+        public T RunWithLock<T>(string aggregateGuid, Func<T> run)
+        {
+            return this.locker.RunWithLock(aggregateGuid, run);
+        }
+
+        public void RunWithLock(string aggregateGuid, Action run)
+        {
+            this.locker.RunWithLock(aggregateGuid, run);
+        }
+    }
+}
