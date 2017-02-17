52550884c82ce477865887457c20bcf1ea69a9a3
Roman Lytvyn <litvin.roma@gmail.com>
2017-02-17 12:35:29 +0200

    code review changes by: Y/N question does not notify other questions about going into unanswered state KP-8645

--- src/Tests/WB.Tests.Integration/InterviewTests/LanguageTests/when_removing_answer_from_yesno_question.cs
+++ src/Tests/WB.Tests.Integration/InterviewTests/LanguageTests/when_removing_answer_from_yesno_question.cs
@@ -10,40 +10,46 @@ namespace WB.Tests.Integration.InterviewTests.LanguageTests
 {
     internal class when_removing_answer_from_yesno_question : InterviewTestsContext
     {
+        private AppDomainContext<AssemblyTargetLoader, PathBasedAssemblyResolver> appDomainContext;
+
+        [SetUp]
+        public void BeforeTest() => this.appDomainContext = AppDomainContext.Create();
+
+        [TearDown]
+        public void AfterTest() => this.appDomainContext.Dispose();
+
         [Test]
         public void should_dependent_variable_update_value()
         {
-            var appDomainContext = AppDomainContext.Create();
-            var variableValue = Execute.InStandaloneAppDomain(appDomainContext.Domain, () =>
+            var variableValue = Execute.InStandaloneAppDomain(this.appDomainContext.Domain, () =>
             {
                 Setup.MockedServiceLocator();
                 var questionId = Guid.Parse("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
                 var variableId = Guid.Parse("BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB");
                 var questionnaireDocument = Create.QuestionnaireDocumentWithOneChapter(children: new IComposite[]
                 {
-                    Create.YesNoQuestion(questionId: questionId, variable: "hhAssets", answers: new[] { 11 }),
+                    Create.YesNoQuestion(questionId: questionId, variable: "hhAssets", answers: new[] {11}),
                     Create.Variable(variableId, VariableType.LongInteger, expression: "hhAssets.Missing.Length")
                 });
 
                 var interview = SetupStatefullInterview(questionnaireDocument);
-                interview.AnswerYesNoQuestion(Create.Command.AnswerYesNoQuestion(
-                questionId: questionId,
-                rosterVector: RosterVector.Empty,
-                answeredOptions: new [] { Create.AnsweredYesNoOption(11m, true) },
-                answerTime: DateTime.UtcNow));
 
                 interview.AnswerYesNoQuestion(Create.Command.AnswerYesNoQuestion(
-                questionId: questionId,
-                rosterVector: RosterVector.Empty,
-                answeredOptions: new AnsweredYesNoOption[] {  },
-                answerTime: DateTime.UtcNow));
+                    questionId: questionId,
+                    rosterVector: RosterVector.Empty,
+                    answeredOptions: new[] {Create.AnsweredYesNoOption(11m, true)},
+                    answerTime: DateTime.UtcNow));
 
-                return (long)interview.GetVariableValueByOrDeeperRosterLevel(variableId, RosterVector.Empty);
+                interview.AnswerYesNoQuestion(Create.Command.AnswerYesNoQuestion(
+                    questionId: questionId,
+                    rosterVector: RosterVector.Empty,
+                    answeredOptions: new AnsweredYesNoOption[] {},
+                    answerTime: DateTime.UtcNow));
+
+                return (long) interview.GetVariableValueByOrDeeperRosterLevel(variableId, RosterVector.Empty);
             });
 
             Assert.That(variableValue, Is.EqualTo(1));
-
-            appDomainContext.Dispose();
         }
     }
 }
