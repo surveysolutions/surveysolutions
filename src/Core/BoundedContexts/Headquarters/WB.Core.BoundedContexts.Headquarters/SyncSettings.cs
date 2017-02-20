c5a245264b66a4aacef3f8e2d3db8d1c967c364b
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2017-02-20 17:33:55 +0200

    KP-8763: Removed obsolete queue implementation

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/SyncSettings.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/SyncSettings.cs
@@ -13,32 +13,11 @@ namespace WB.Core.BoundedContexts.Headquarters
             this.Origin = origin;
         }
 
-        public SyncSettings(string appDataDirectory, string incomingCapiPackagesWithErrorsDirectoryName,
-            string incomingCapiPackageFileNameExtension, string incomingUnprocessedPackagesDirectoryName, string origin,
-            int retryCount, int retryIntervalInSeconds, bool useBackgroundJobForProcessingPackages)
+        public SyncSettings(string origin, bool useBackgroundJobForProcessingPackages)
         {
-            this.AppDataDirectory = appDataDirectory;
-            this.IncomingCapiPackagesWithErrorsDirectoryName = incomingCapiPackagesWithErrorsDirectoryName;
-            this.IncomingCapiPackageFileNameExtension = incomingCapiPackageFileNameExtension;
-            this.IncomingUnprocessedPackagesDirectoryName = incomingUnprocessedPackagesDirectoryName;
             this.Origin = origin;
-            this.RetryCount = retryCount;
-            this.RetryIntervalInSeconds = retryIntervalInSeconds;
             this.UseBackgroundJobForProcessingPackages = useBackgroundJobForProcessingPackages;
         }
-
-        [Obsolete("Since v 5.8")]
-        public string AppDataDirectory { get; private set; }
-        [Obsolete("Since v 5.8")]
-        public string IncomingUnprocessedPackagesDirectoryName { get; private set; }
-        [Obsolete("Since v 5.8")]
-        public string IncomingCapiPackagesWithErrorsDirectoryName { get; private set; }
-        [Obsolete("Since v 5.8")]
-        public string IncomingCapiPackageFileNameExtension { get; private set; }
-        [Obsolete("Since v 5.8")]
-        public int RetryCount { get; private set; }
-        [Obsolete("Since v 5.8")]
-        public int RetryIntervalInSeconds { get; private set; }
 
         public string Origin { get; private set; }
 
