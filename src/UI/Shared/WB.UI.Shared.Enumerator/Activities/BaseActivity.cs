105486d742e30361d7eb9a04b0a155c6fcbecba5
Pavel Stovpivskyi <630198+spuv@users.noreply.github.com>
2017-02-21 13:04:38 +0200

    Added log entry for low memory warning

--- src/UI/Shared/WB.UI.Shared.Enumerator/Activities/BaseActivity.cs
+++ src/UI/Shared/WB.UI.Shared.Enumerator/Activities/BaseActivity.cs
@@ -1,8 +1,12 @@
+using Android.App;
+using Android.Content;
 using Android.OS;
 using MvvmCross.Droid.Platform;
 using MvvmCross.Droid.Support.V7.AppCompat;
+using MvvmCross.Platform;
 using Plugin.CurrentActivity;
 using Plugin.Permissions;
+using WB.Core.GenericSubdomains.Portable;
 using WB.Core.SharedKernels.Enumerator.ViewModels;
 
 namespace WB.UI.Shared.Enumerator.Activities
@@ -36,5 +40,48 @@ namespace WB.UI.Shared.Enumerator.Activities
             base.OnViewModelSet();
             this.SetContentView(this.ViewResourceId);
         }
+
+        public override void OnLowMemory()
+        {
+            this.TryWriteMemoryInformationToLog();
+            base.OnLowMemory();
+        }
+
+        private void TryWriteMemoryInformationToLog()
+        {
+            try
+            {
+                Mvx.Error($"LowMemory natification:" + System.Environment.NewLine);
+                Mvx.Error($"RAM: {this.GetRAMInformation()} {System.Environment.NewLine}");
+                Mvx.Error($"Disk: {this.GetDiskInformation()} {System.Environment.NewLine}");
+            }
+            catch
+            {
+            }
+        }
+
+        private string GetRAMInformation()
+        {
+            ActivityManager activityManager = Application.Context.GetSystemService(Context.ActivityService) as ActivityManager;
+            if (activityManager == null)
+                return "UNKNOWN";
+
+            ActivityManager.MemoryInfo mi = new ActivityManager.MemoryInfo();
+            activityManager.GetMemoryInfo(mi);
+            return $"{FileSizeUtils.SizeSuffix(mi.TotalMem)} total, avaliable {(int)(((double)(100 * mi.AvailMem)) / mi.TotalMem)}% ({FileSizeUtils.SizeSuffix(mi.AvailMem)})";
+        }
+
+        private string GetDiskInformation()
+        {
+            string path = global::Android.OS.Environment.DataDirectory.Path;
+            StatFs stat = new StatFs(path);
+            long blockSize = stat.BlockSizeLong;
+            long availableBlocks = stat.AvailableBlocksLong;
+            long totalBlocks = stat.BlockCountLong;
+            var availableInternalMemorySize = (availableBlocks * blockSize);
+            var totalInternalMemorySize = totalBlocks * blockSize;
+            return $"{FileSizeUtils.SizeSuffix(totalInternalMemorySize)} total, avaliable {(int)(((double)(100 * availableInternalMemorySize)) / totalInternalMemorySize)}% ({FileSizeUtils.SizeSuffix(availableInternalMemorySize)})";
+        }
+
     }
 }
