a75bdd38a66ae180bda578d5eacb35e3a3208cce
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2017-02-21 13:38:55 +0200

    KP-8792: Reduce amount of strings used in compiled assembly

---
+++ src/Tests/WB.Tests.Integration/UtilsTest/GuidExtensionsTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
+using NUnit.Framework;
+using WB.Core.BoundedContexts.Designer.Implementation.Services.CodeGeneration.Helpers;
+
+namespace WB.Tests.Integration.UtilsTest
+{
+    public class GuidExtensionsTests
+    {
+        [Test]
+        public async Task when_rendering_guid_should_not_change_its_value()
+        {
+            Guid originalGuid = new Guid("DE6976CF-686E-4E5D-9369-CE6FC501DC0F");
+            var compiledGuid = await CSharpScript.RunAsync<Guid>(originalGuid.AsBytesString());
+            Assert.That(compiledGuid.ReturnValue, Is.EqualTo(originalGuid));
+        }
+    }
+}
