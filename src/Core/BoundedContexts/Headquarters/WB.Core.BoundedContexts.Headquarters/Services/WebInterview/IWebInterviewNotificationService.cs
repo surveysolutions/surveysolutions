94905927b312303e69c715c7ec1d5aa5001e0893
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2017-02-21 14:35:45 -0500

    KP-8776 Many errors when naviagting to deleted item

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Services/WebInterview/IWebInterviewNotificationService.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Services/WebInterview/IWebInterviewNotificationService.cs
@@ -5,11 +5,11 @@ namespace WB.Core.BoundedContexts.Headquarters.Services.WebInterview
 {
     public interface IWebInterviewNotificationService
     {
-        void RefreshEntities(Guid interviewId, params Identity[] questions);
+        void RefreshEntities(Guid interviewId, params Identity[] entities);
         void ReloadInterview(Guid interviewId);
         void MarkAnswerAsNotSaved(string interviewId, string questionId, string errorMessage);
 
-        void RefreshRemovedEntities(Guid interviewId, params Identity[] questions);
+        void RefreshRemovedEntities(Guid interviewId, params Identity[] entities);
         void FinishInterview(Guid interviewId);
     }
 }
