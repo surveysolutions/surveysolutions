d61918584884a73409eaceba3dd2c94126f73d50
Anatoliy 'TLK' Kolesnick <anatoliy@kolesnick.com>
2017-02-27 13:25:15 +0200

    StatefulInterview: covered client-side date formatting with test KP-8803

---
+++ src/Tests/WB.Tests.Unit/SharedKernels/Enumerator/ViewModels/QuestionHeaderViewModelTests/when_answered_datetime_question_used_in_substitution_title.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Machine.Specifications;
+using Main.Core.Entities.Composite;
+using WB.Core.SharedKernels.DataCollection;
+using WB.Core.SharedKernels.DataCollection.Events.Interview.Dtos;
+using WB.Core.SharedKernels.DataCollection.Implementation.Aggregates;
+using WB.Core.SharedKernels.DataCollection.Implementation.Entities;
+using WB.Core.SharedKernels.DataCollection.Repositories;
+
+namespace WB.Tests.Unit.SharedKernels.Enumerator.ViewModels.QuestionHeaderViewModelTests
+{
+    internal class when_answered_datetime_question_used_in_substitution_title : QuestionHeaderViewModelTestsContext
+    {
+        Establish context = () =>
+        {
+            var questionnaireDocument = Create.Entity.QuestionnaireDocumentWithOneChapter(children: new IComposite[]
+            {
+                Create.Entity.StaticText(publicKey: staticTextIdentity.Id, text: "%date_time%"),
+                Create.Entity.DateTimeQuestion(questionId: questionId, variable: "date_time"),
+            });
+
+            statefullInterview = Create.AggregateRoot.StatefulInterview(
+                questionnaireRepository: Setup.QuestionnaireRepositoryWithOneQuestionnaire(questionnaireDocument));
+        };
+
+        Because of = () => statefullInterview.AnswerDateTimeQuestion(Guid.NewGuid(), questionId, RosterVector.Empty, DateTime.UtcNow, new DateTime(2000, 10, 10));
+
+        It should_put_time_opening_tag_to_browser_ready_title_html = () => statefullInterview.GetBrowserReadyTitleHtml(staticTextIdentity)
+            .ShouldContain("<time datetime=");
+
+        It should_put_time_closing_tag_to_browser_ready_title_html = () => statefullInterview.GetBrowserReadyTitleHtml(staticTextIdentity)
+            .ShouldContain("</time>");
+
+        It should_not_put_time_opening_tag_to_title = () => statefullInterview.GetTitleText(staticTextIdentity)
+            .ShouldNotContain("<time");
+
+        It should_not_put_time_closing_tag_to_title = () => statefullInterview.GetTitleText(staticTextIdentity)
+            .ShouldNotContain("</time>");
+
+        static StatefulInterview statefullInterview;
+        static Guid questionId = Guid.Parse("BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB");
+        static Identity staticTextIdentity = Identity.Create(Guid.Parse("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"), RosterVector.Empty);
+    }
+}
