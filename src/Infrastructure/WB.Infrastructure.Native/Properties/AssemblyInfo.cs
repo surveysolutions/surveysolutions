527925f8972cce6fd7c40fde758395ac055579f6
Andrii Kozhyn <hedgic@gmail.com>
2017-02-27 16:36:51 +0200

    KP-8851: migration tool from event store to postgres

--- src/Infrastructure/WB.Infrastructure.Native/Properties/AssemblyInfo.cs
+++ src/Infrastructure/WB.Infrastructure.Native/Properties/AssemblyInfo.cs
@@ -44,3 +44,4 @@ using System.Runtime.InteropServices;
 [assembly: InternalsVisibleTo("event-store-scanner")]
 [assembly: InternalsVisibleTo("EventStoreToPlainStorageMigrator")]
 [assembly: InternalsVisibleTo("A2DB")]
+[assembly: InternalsVisibleTo("EsToPg")]
