a376b679e4a9b57c1afd556a749330fe81a6c37e
Slava Scherbak <slava.scherbak@gmail.com>
2017-03-01 14:27:46 -0500

    Added generation of question properties, linked and categorical filters and classes for levels. KP-8804

---
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Implementation/Services/CodeGenerationV2/V11/OptionsFilterMethodTemplateV11.Extended.cs
@@ -0,0 +1,12 @@
+﻿namespace WB.Core.BoundedContexts.Designer.Implementation.Services.CodeGenerationV2.V11
+{
+    public partial class OptionsFilterMethodTemplateV11
+    {
+        public OptionsFilterMethodTemplateV11(OptionsFilterMethodModel model)
+        {
+            this.Model = model;
+        }
+
+        protected OptionsFilterMethodModel Model { get; set; }
+    }
+}
--- src/Core/SharedKernels/DataCollection/DataCollection.Portable/V11/IInterviewExpressionProcessorV11.cs
+++ src/Core/SharedKernels/DataCollection/DataCollection.Portable/V11/IInterviewExpressionProcessorV11.cs
@@ -5,35 +5,8 @@ namespace WB.Core.SharedKernels.DataCollection.V11
 {
     public interface IInterviewExpressionProcessorV11
     {
-        // Fix
-        void UpdateSingleOptionAnswer(Identity questionId, int answer);
-        void UpdateMultiOptionAnswer(Identity questionId,  int[] answer);
-        void UpdateTextListAnswer(Identity questionId,  ListAnswerRow[] answers);
-        void UpdateLinkedSingleOptionAnswer(Identity questionId,  RosterVector selectedPropagationVector);
-        void UpdateLinkedMultiOptionAnswer(Identity questionId, RosterVector[] selectedPropagationVectors);
-        void UpdateYesNoAnswer(Identity questionId, YesNoAnswersOnlyV2 answers);
+        void Init(IInterviewStateAccessorV11 state);
 
-        // Maybe
-        void UpdateNumericIntegerAnswer(Identity questionId, long? answer);
-
-        // OK
-        void UpdateNumericRealAnswer(Identity questionId, double? answer);
-        void UpdateDateAnswer(Identity questionId, DateTime? answer);
-        void UpdateMediaAnswer(Identity questionId, string answer);
-        void UpdateTextAnswer(Identity questionId, string answer);
-        void UpdateQrBarcodeAnswer(Identity questionId, string answer);
-        void UpdateGeoLocationAnswer(Identity questionId,  double latitude, double longitude, double accuracy, double altitude);
-
-        void DisableGroups(IEnumerable<Identity> groupsToDisable);
-        void EnableGroups(IEnumerable<Identity> groupsToEnable);
-        void DisableQuestions(IEnumerable<Identity> questionsToDisable);
-        void EnableQuestions(IEnumerable<Identity> questionsToEnable);
-        void DisableStaticTexts(IEnumerable<Identity> staticTextsToDisable);
-        void EnableStaticTexts(IEnumerable<Identity> staticTextsToEnable);
-        void DisableVariables(IEnumerable<Identity> variablesToDisable);
-        void EnableVariables(IEnumerable<Identity> variablesToEnable);
-
-        void AddRoster(Identity rosterId, int? sortIndex);
         void RemoveRoster(Identity rosterId);
 
         void SetInterviewProperties(IInterviewProperties properties);
