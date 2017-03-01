6d027cb02bd630a88339bf017d3e99ed1832f6a9
Slava Scherbak <slava.scherbak@gmail.com>
2017-03-01 15:24:05 -0500

    fixed merge

---
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Implementation/Services/CodeGenerationV2/ICodeGeneratorV2.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Main.Core.Documents;
+
+namespace WB.Core.BoundedContexts.Designer.Implementation.Services.CodeGenerationV2
+{
+    public interface ICodeGeneratorV2
+    {
+        Dictionary<string, string> Generate(QuestionnaireDocument questionnaire, int targetVersion);
+    }
+}
