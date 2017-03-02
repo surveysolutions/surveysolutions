f5aebd0d90091ddfee39cdf0a929f6b838227a43
Slava Scherbak <slava.scherbak@gmail.com>
2017-03-02 14:41:31 -0500

    Added ids map and method to create rosters. KP-8804

--- src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Implementation/Services/CodeGenerationV2/V11/LevelTemplateV11.Extended.cs
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Implementation/Services/CodeGenerationV2/V11/LevelTemplateV11.Extended.cs
@@ -1,18 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace WB.Core.BoundedContexts.Designer.Implementation.Services.CodeGenerationV2.V11
 {
     public partial class LevelTemplateV11
     {
         public LevelModel Model { get; }
+        public CodeGenerationModel Processor { get; }
 
-        public LevelTemplateV11(LevelModel model)
+        public LevelTemplateV11(LevelModel model, CodeGenerationModel processor)
         {
             this.Model = model;
+            this.Processor = processor;
         }
     }
 }
---
+++ src/Core/SharedKernels/DataCollection/DataCollection.Portable/V11/IInterviewState.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WB.Core.SharedKernels.DataCollection.V11
+{
+    public interface IInterviewState
+    {
+        T GetAnswer<T>(Guid questionId, IEnumerable<int> rosterVector);
+
+        int GetRosterIndex(Identity rosterIdentity);
+
+        string GetRosterTitle(Identity rosterIdentity);
+    }
+}
