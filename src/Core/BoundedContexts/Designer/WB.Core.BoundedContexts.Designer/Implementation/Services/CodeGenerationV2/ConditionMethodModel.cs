ab3af1da4697006fc53fe7364abf54275475a228
Slava Scherbak <slava.scherbak@gmail.com>
2017-03-03 18:09:05 -0500

    Added static texts, collections of expressions. KP-8804

---
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Implementation/Services/CodeGenerationV2/ConditionMethodModel.cs
@@ -0,0 +1,32 @@
+using WB.Core.BoundedContexts.Designer.Services;
+
+namespace WB.Core.BoundedContexts.Designer.Implementation.Services.CodeGenerationV2
+{
+    public class ConditionMethodModel
+    {
+        public ConditionMethodModel(ExpressionLocation location, 
+            string className, 
+            string methodName, 
+            string expression, 
+            bool generateSelf, 
+            string variable, 
+            string returnType = "bool")
+        {
+            this.Location = location;
+            this.ClassName = className;
+            this.MethodName = methodName;
+            this.Expression = expression;
+            this.Variable = variable;
+            this.GenerateSelf = generateSelf;
+            this.ReturnType = returnType;
+        }
+
+        public ExpressionLocation Location { get; set; }
+        public string ClassName { set; get; }
+        public string MethodName { set; get; }
+        public string Expression { set; get; }
+        public string Variable { set; get; }
+        public bool GenerateSelf { set; get; }
+        public string ReturnType { get; set; }
+    }
+}
