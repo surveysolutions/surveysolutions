4e4c806d8cef633ed4e81c5b523ae9914a6ff5ee
Victor Hladkikh <me@alfeg.net>
2017-03-06 16:19:15 +0200

    KP-8873 Show captcha after couple unsuccessful logins # Temporary solution with cached failed logins

---
+++ src/UI/Shared/WB.UI.Shared.Web/Modules/CaptchaModule.cs
@@ -0,0 +1,36 @@
+﻿using Ninject;
+using Ninject.Modules;
+using WB.UI.Shared.Web.Captcha;
+using WB.UI.Shared.Web.Configuration;
+
+namespace WB.UI.Shared.Web.Modules
+{
+    public class CaptchaModule : NinjectModule
+    {
+        public override void Load()
+        {
+            var configuration = this.Kernel.Get<IConfigurationManager>().AppSettings;
+
+            var captcha = configuration.Get("CaptchaService");
+
+            if (string.IsNullOrWhiteSpace(captcha))
+            {
+                this.Bind<ICaptchaProvider>().To<NoCaptchaProvider>();
+            }
+            else
+            {
+                switch (captcha.ToLowerInvariant())
+                {
+                    case "recaptcha": this.Bind<ICaptchaProvider>().To<ReCaptchaProvider>();
+                        break;
+                    case "hosted":
+                        this.Bind<ICaptchaProvider>().To<HostedCaptchaProvider>();
+                        break;
+                    default:
+                        this.Bind<ICaptchaProvider>().To<NoCaptchaProvider>();
+                        break;
+                }
+            }
+        }
+    }
+}
