cab996233e30cfc63062f7422fa1a74267ad2dc5
Victor Hladkikh <me@alfeg.net>
2017-03-10 15:31:33 +0200

    KP-8838 Sign each questionnaire request with server version + build number # RestService now fill User-Agent string with specific info

--- src/UI/Headquarters/WB.UI.Headquarters/Views/DesignerQuestionnaireApiRestServiceSettings.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/Views/DesignerQuestionnaireApiRestServiceSettings.cs
@@ -1,6 +1,10 @@
 using System;
-
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Web;
 using WB.Core.GenericSubdomains.Portable.Services;
+using WB.Core.Infrastructure.Versions;
+using WB.Core.SharedKernels.SurveyManagement.Web.Code;
 using WB.UI.Shared.Web.Configuration;
 using WB.UI.Shared.Web.Extensions;
 
@@ -9,10 +13,12 @@ namespace WB.UI.Headquarters.Views
     public class DesignerQuestionnaireApiRestServiceSettings : IRestServiceSettings
     {
         private readonly IConfigurationManager configurationManager;
+        private readonly IProductVersion productVersion;
 
-        public DesignerQuestionnaireApiRestServiceSettings(IConfigurationManager configurationManager)
+        public DesignerQuestionnaireApiRestServiceSettings(IConfigurationManager configurationManager, IProductVersion productVersion)
         {
             this.configurationManager = configurationManager;
+            this.productVersion = productVersion;
         }
 
         public string Endpoint
@@ -37,6 +43,37 @@ namespace WB.UI.Headquarters.Views
         {
             get { return false; }
             set { throw new NotImplementedException(); }
+        }
+
+        private static string _userAgent = null;
+
+        [Localizable(false)]
+        public string UserAgent
+        {
+            get
+            {
+                if (_userAgent != null) return _userAgent;
+
+                var flags = new List<string>();
+
+                if (AppSettings.IsDebugBuilded)
+                {
+                    flags.Add("DEBUG");
+                }
+
+                if (HttpContext.Current?.IsDebuggingEnabled ?? false)
+                {
+                    flags.Add("WEB_DEBUG");
+                }
+
+                if (AcceptUnsignedSslCertificate)
+                {
+                    flags.Add("UNSIGNED_SSL");
+                }
+
+                _userAgent = $"WB.Headquarters/{this.productVersion.ToString()} ({string.Join(@" ", flags)})";
+                return _userAgent;
+            }
         }
     }
 }
