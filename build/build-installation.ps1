b08c9519db9832fcdf7c5ef91642d0a70910e900
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2017-03-13 15:22:00 -0400

    build for KP-8826 Wix installation and build setup

--- build/build-installation.ps1
+++ build/build-installation.ps1
@@ -30,8 +30,7 @@ if (!(Test-Path $HQsitePath)) {
 Copy-Item $sitePatha\* $HQsitePath -Force -Recurse
 
 
-
-    & (GetPathToMSBuild) $InstallationProject '/t:Build' '/p:HarvestDir=$HQsitePath' "/p:Configuration=Relaese" "/p:Platform=x64" | Write-Host
+    & (GetPathToMSBuild) $InstallationProject '/t:Build' '/p:HarvestDir=$HQsitePath' "/p:Configuration=Release" "/p:Platform=x64" | Write-Host
 
     $wasBuildSuccessfull = $LASTEXITCODE -eq 0
 
