cba558ffecaa1e6d774791832175c3a0d99fa556
Andrii Kozhyn <hedgic@gmail.com>
2017-03-13 11:22:55 +0200

    KP-8836: Show custom error page if application initialization failed

--- src/UI/Headquarters/WB.UI.Headquarters/Code/ApplicationSettings.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/Code/ApplicationSettings.cs
@@ -3,12 +3,22 @@ using WB.UI.Shared.Web.Extensions;
 
 namespace WB.UI.Headquarters.Code
 {
-    public static class LegacyOptions
+    public static class ApplicationSettings
     {
         public static int InterviewDetailsDataSchedulerSynchronizationInterval => 
             WebConfigurationManager.AppSettings.GetInt("InterviewDetailsDataScheduler.SynchronizationInterval", 10);
 
         public static bool WebInterviewEnabled => 
             WebConfigurationManager.AppSettings.GetBool("WebInterviewEnabled", false);
+
+        public static CustomErrorsMode CustomErrorsMode
+        {
+            get
+            {
+                CustomErrorsSection customErrors =
+                    (CustomErrorsSection) WebConfigurationManager.GetSection("system.web/customErrors");
+                return customErrors.Mode;
+            }
+        }
     }
 }
