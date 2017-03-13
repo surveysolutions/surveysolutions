9a9d7d08b7428fc9a56999ea26a7ce996911e896
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2017-03-13 13:08:06 -0400

    build for KP-8826 Wix installation and build setup

---
+++ build/build-installation.ps1
@@ -0,0 +1,58 @@
+param(
+#      [string]$cmd,
+      [string]$HQSourcePath, 
+	  [switch]$noDestCleanup)
+
+$ErrorActionPreference = "Stop"
+
+$scriptFolder = (Get-Item $MyInvocation.MyCommand.Path).Directory.FullName
+. "$scriptFolder\functions.ps1"
+
+$InstallationProject = 'src\Installation\SurveySolutions\SurveySolutions.wixproj'
+
+$sourceCleanup = $False
+
+$workdir = Get-Location
+if ($HQSourcePath -eq "") {
+	$HQSourcePath = Join-Path $workdir "HQpackage"
+	$sourceCleanup = $True
+}
+
+Set-Location $HQSourcePath
+$sitePatha = (Get-ChildItem -recurse | Where-Object {$_.PSIsContainer -eq $true -and $_.Name -match "PackageTmp"}).FullName
+
+$HQsitePath = Join-path $workdir "HQwork"
+if (!(Test-Path $HQsitePath)) {
+	New-Item $HQsitePath -ItemType Directory
+	New-Item (Join-Path $HQsitePath "App_Data") -ItemType Directory
+}
+
+Copy-Item $sitePatha\* $HQsitePath -Force -Recurse
+
+
+
+    & (GetPathToMSBuild) $InstallationProject '/t:Build' '/p:HarvestDir=$HQsitePath' "/p:Configuration=$BuildConfiguration" | Write-Host
+
+    $wasBuildSuccessfull = $LASTEXITCODE -eq 0
+
+	if (-not $wasBuildSuccessfull) {
+        Write-Host "##teamcity[message status='ERROR' text='Failed to build installation']"
+
+        if (-not $MultipleSolutions) {
+            Write-Host "##teamcity[buildProblem description='Failed to build installation']"
+        }
+    }
+	
+
+
+Set-Location $workdir
+Remove-Item $setupProject -Force
+
+if (!($noDestCleanup)) {
+	Remove-Item $HQsitePath -Force -Recurse
+}
+if ($sourceCleanup) {
+	Remove-Item $HQSourcePath -Force -Recurse
+}
+
+Write-Host "##teamcity[publishArtifacts 'SurveySolutions.msi']"
