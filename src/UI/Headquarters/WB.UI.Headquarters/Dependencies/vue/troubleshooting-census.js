0b802033de0c91657f84a635bb3fa0971276cf7d
Slava Scherbak <slava.scherbak@gmail.com>
2017-03-14 18:35:49 -0400

    Added empty datatable component. KP-8845

--- src/UI/Headquarters/WB.UI.Headquarters/Dependencies/vue/troubleshooting-census.js
+++ src/UI/Headquarters/WB.UI.Headquarters/Dependencies/vue/troubleshooting-census.js
@@ -2,6 +2,7 @@
 import VueResource from 'vue-resource'
 import UserSelector from './UserSelector.vue'
 import DatePicker from './DatePicker.vue'
+import InterviewTable from './InterviewTable.vue'
 import VeeValidate from 'vee-validate';
 
 Vue.use(VeeValidate);
@@ -9,6 +10,7 @@ Vue.use(VueResource);
 
 Vue.component('Flatpickr', DatePicker);
 Vue.component("user-selector", UserSelector);
+Vue.component("interview-table", InterviewTable);
 
 var app = new Vue({
     data: {
