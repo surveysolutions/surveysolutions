d8dfee946e6eb94ce0af41c238e92e5577005767
Roman Lytvyn <litvin.roma@gmail.com>
2017-03-15 14:14:25 +0200

    HQ: fixed synchronization next try. KP-8914

--- src/Core/SharedKernels/DataCollection/DataCollection/WebApi/DeviceInfoApiView.cs
+++ src/Core/SharedKernels/DataCollection/DataCollection/WebApi/DeviceInfoApiView.cs
@@ -43,6 +43,5 @@ namespace WB.Core.SharedKernels.DataCollection.WebApi
     {
         public double Latitude { get; set; }
         public double Longitude { get; set; }
-        public string[] Address { get; set; }
     }
 }
