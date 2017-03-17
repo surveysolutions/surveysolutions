75a3e16ac1ec895731bad79d74bbf5165a8a1fe4
Victor Hladkikh <me@alfeg.net>
2017-03-17 17:55:28 +0200

    KP-8871: Change cryptographic algorithm from SHA1 to SHA2  # More unit tests

---
+++ src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/Web/ApiBasicAuthAttributeTests/when_authorizing_interviewer_in_SHA1_compatibility_mode.cs
@@ -0,0 +1,29 @@
+using System.Net.Http.Headers;
+using System.Threading;
+using Machine.Specifications;
+using Main.Core.Entities.SubEntities;
+using NUnit.Framework;
+using WB.Core.BoundedContexts.Headquarters.Views.User;
+using WB.Tests.Abc;
+
+namespace WB.Tests.Unit.SharedKernels.SurveyManagement.Web.ApiBasicAuthAttributeTests
+{
+    public class when_authorizing_interviewer_in_SHA1_compatibility_mode : ApiBasicAuthNunitBasedSpecification
+    {
+        protected override void Context()
+        {
+            this.SetupInterviwer(Create.Entity.HqUser(role: UserRoles.Interviewer, passwordHashSha1: "open sesame"));
+            this.HashCompatibilityProvider.Setup(h => h.IsSHA1Required(Moq.It.IsAny<HqUser>())).Returns(true);
+
+            this.attribute = this.CreateApiBasicAuthAttribute();
+            this.actionContext = CreateActionContext();
+            this.actionContext.Request.Headers.Authorization = new AuthenticationHeaderValue("Basic", "QWxhZGRpbjpvcGVuIHNlc2FtZQ==");
+        }
+
+        [Test]
+        public void Should_Authorize_User() => Thread.CurrentPrincipal.ShouldNotBeNull();
+
+        [Test]
+        public void Should_not_return_any_errors() => this.actionContext.Response.ShouldBeNull();
+    }
+}
