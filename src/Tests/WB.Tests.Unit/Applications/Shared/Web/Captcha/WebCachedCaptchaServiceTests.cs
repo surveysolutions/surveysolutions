6b4cba2a0e9cae1b9d0b8a39403a7f8339aa6a15
Victor Hladkikh <me@alfeg.net>
2017-03-20 10:53:54 +0200

    KP-8873 Show captcha after couple unsuccessful logins

--- src/Tests/WB.Tests.Unit/Applications/Shared/Web/Captcha/WebCachedCaptchaServiceTests.cs
+++ src/Tests/WB.Tests.Unit/Applications/Shared/Web/Captcha/WebCachedCaptchaServiceTests.cs
@@ -2,51 +2,45 @@
 using Machine.Specifications;
 using NUnit.Framework;
 using WB.Tests.Abc;
-using WB.UI.Shared.Web.Captcha;
 
 namespace WB.Tests.Unit.Applications.Shared.Web.Captcha
 {
     public class WebCachedCaptchaServiceTests
     {
-        private WebCacheBasedCaptchaService Subject;
-        private const int NumberOfAttempts = 5;
-
-        [SetUp]
-        public void Setup()
-        {
-            this.Subject = Create.Service.WebCacheBasedCaptchaService(NumberOfAttempts);
-        }
-
+        const int NumberOfAttempts = 5;
+        
         [Test]
         public void When_FailingLogins_Should_NotRequireCaptchaForFirst5Attempts()
         {
+            var subject = Create.Service.WebCacheBasedCaptchaService(NumberOfAttempts);
             var userName = Guid.NewGuid().ToString();
 
-            this.Subject.ShouldShowCaptcha(userName).ShouldBeFalse();
+            subject.ShouldShowCaptcha(userName).ShouldBeFalse();
 
             for (int i = 0; i < NumberOfAttempts - 1; i++)
             {
-                this.Subject.RegisterFailedLogin(userName);
-                this.Subject.ShouldShowCaptcha(userName).ShouldBeFalse();
+                subject.RegisterFailedLogin(userName);
+                subject.ShouldShowCaptcha(userName).ShouldBeFalse();
             }
 
-            this.Subject.RegisterFailedLogin(userName);
-            this.Subject.ShouldShowCaptcha(userName).ShouldBeTrue();
+            subject.RegisterFailedLogin(userName);
+            subject.ShouldShowCaptcha(userName).ShouldBeTrue();
         }
 
         [Test]
         public void When_ResetFailedLogin_Should_ResetCaptchaRequirement()
         {
+            var subject = Create.Service.WebCacheBasedCaptchaService(NumberOfAttempts);
             var userName = Guid.NewGuid().ToString();
             
             for (int i = 0; i < NumberOfAttempts; i++)
             {
-                this.Subject.RegisterFailedLogin(userName);
+                subject.RegisterFailedLogin(userName);
             }
-            this.Subject.ShouldShowCaptcha(userName).ShouldBeTrue();
-            this.Subject.ResetFailedLogin(userName);
+            subject.ShouldShowCaptcha(userName).ShouldBeTrue();
+            subject.ResetFailedLogin(userName);
 
-            this.Subject.ShouldShowCaptcha(userName).ShouldBeFalse();
+            subject.ShouldShowCaptcha(userName).ShouldBeFalse();
         }
     }
 }
