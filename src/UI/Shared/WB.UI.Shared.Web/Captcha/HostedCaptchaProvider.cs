a88bdcec19f04fd7001f538778cf17c93897c220
Victor Hladkikh <me@alfeg.net>
2017-03-21 13:44:55 +0200

    KP-8873 Show captcha after couple unsuccessful logins # Fixed some layout issues
    MVC Captcha is a bit annoing to configure, and still produce that crazy amount of BR tags. So I kill two of them without hesistate.
    Miniature min-height in inlince style. Sorry to this.

--- src/UI/Shared/WB.UI.Shared.Web/Captcha/HostedCaptchaProvider.cs
+++ src/UI/Shared/WB.UI.Shared.Web/Captcha/HostedCaptchaProvider.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Web;
 using System.Web.Mvc;
 using CaptchaMvc.HtmlHelpers;
@@ -6,8 +7,26 @@ namespace WB.UI.Shared.Web.Captcha
 {
     public class HostedCaptchaProvider : ICaptchaProvider
     {
-        public IHtmlString RenderCaptcha(HtmlHelper helper) => helper.Captcha(Resources.Captcha.TryAnother, Resources.Captcha.EnterText, 5, Resources.Captcha.Required, true);
+        private const string BrTag = "<br/>";
+
+        public IHtmlString RenderCaptcha(HtmlHelper helper)
+        {
+            var captchaMarkup = helper.Captcha(Resources.Captcha.TryAnother, Resources.Captcha.EnterText, 5,
+                Resources.Captcha.Required, true).ToHtmlString();
+
+            // there is no way to exclude those <br /> tags, so I will remove them(only first two)
+            captchaMarkup = RemoveFirstOccurence(captchaMarkup, BrTag);
+            captchaMarkup = RemoveFirstOccurence(captchaMarkup, BrTag);
+
+            return new MvcHtmlString(captchaMarkup);
+        }
 
         public bool IsCaptchaValid(Controller controller) => controller.IsCaptchaValid(Resources.Captcha.InvalidCaptcha);
+
+        private string RemoveFirstOccurence(string source, string pattern)
+        {
+            var index = source.IndexOf(pattern, StringComparison.Ordinal);
+            return index >= 0 ? source.Substring(0, index) + source.Substring(index + pattern.Length) : source;
+        }
     }
 }
