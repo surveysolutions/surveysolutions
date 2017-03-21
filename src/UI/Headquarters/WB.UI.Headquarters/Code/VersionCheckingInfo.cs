95e104cd8911bffaddb0cc204a7cda5007725653
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2017-03-21 15:32:44 -0400

    KP-8839 Show 'New version available' message on HQ

---
+++ src/UI/Headquarters/WB.UI.Headquarters/Code/VersionCheckingInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace WB.UI.Headquarters.Code
+{
+    public class VersionCheckingInfo
+    {
+        public int Build { set; get; }
+        public string Version { set; get; }
+        public string VersionString { set; get; }
+    }
+}
