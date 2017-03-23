24a575b0aa7892b12d3d34447ced2b0c09b415f6
Pavel Stovpivskyi <630198+spuv@users.noreply.github.com>
2017-03-23 18:28:52 +0200

    KP-8948 Change cookie expiration time to 24 hrs

--- src/UI/Shared/WB.UI.Shared.Web/Configuration/HqSecuritySection.cs
+++ src/UI/Shared/WB.UI.Shared.Web/Configuration/HqSecuritySection.cs
@@ -1,16 +1,25 @@
-﻿using System.Configuration;
+﻿using System;
+using System.Configuration;
 
 namespace WB.UI.Shared.Web.Configuration
 {
     public class HqSecuritySection : ConfigurationSection
     {
         private const string passwordPolicyElementName = "passwordPolicy";
+        private const string cookieSettingsElementName = "cookieSettings";
 
         [ConfigurationProperty(passwordPolicyElementName)]
         public PasswordPolicyElement PasswordPolicy
         {
             get { return ((PasswordPolicyElement)(base[passwordPolicyElementName])); }
             set { base[passwordPolicyElementName] = value; }
+        }
+
+        [ConfigurationProperty(cookieSettingsElementName)]
+        public CookieSettingsElement CookieSettings
+        {
+            get { return ((CookieSettingsElement)(base[cookieSettingsElementName])); }
+            set { base[cookieSettingsElementName] = value; }
         }
     }
 
@@ -39,6 +48,26 @@ namespace WB.UI.Shared.Web.Configuration
         {
             get { return (string)base[passwordStrengthRegularExpressionElementName]; }
             set { base[passwordStrengthRegularExpressionElementName] = value; }
+        }
+    }
+
+    public class CookieSettingsElement : ConfigurationElement
+    {
+        private const string experationTimeElementName = "experationTime";
+        private const string slidingExpirationElementName = "slidingExpiration";
+
+        [ConfigurationProperty(experationTimeElementName)]
+        public int ExperationTime 
+        {
+            get { return (int)base[experationTimeElementName]; }
+            set { base[experationTimeElementName] = value; }
+        }
+
+        [ConfigurationProperty(slidingExpirationElementName)]
+        public bool SlidingExpiration
+        {
+            get { return (bool)base[slidingExpirationElementName]; }
+            set { base[slidingExpirationElementName] = value; }
         }
     }
 }
