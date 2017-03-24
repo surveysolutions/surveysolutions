b1f6c9a495e2887dc5f13153baa102ee555c106a
Andrii Kozhyn <hedgic@gmail.com>
2017-03-24 12:00:11 +0200

    KP-8944: More uniqueue human ids

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Services/Internal/RandomValuesSource.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Services/Internal/RandomValuesSource.cs
@@ -1,14 +1,23 @@
 using System;
+using WB.Core.GenericSubdomains.Portable.CustomCollections;
 
 namespace WB.Core.BoundedContexts.Headquarters.Services.Internal
 {
     internal class RandomValuesSource : IRandomValuesSource
     {
         private readonly Random random = new Random();
+        private readonly ConcurrentHashSet<int> generatedIds = new ConcurrentHashSet<int>();
 
         public int Next(int maxInterviewKeyValue)
         {
-            return this.random.Next(maxInterviewKeyValue);
+            var next = this.random.Next(maxInterviewKeyValue);
+            while (this.generatedIds.Contains(next))
+            {
+                next = this.random.Next(maxInterviewKeyValue);
+            }
+
+            this.generatedIds.Add(next);
+            return next;
         }
     }
 }
