118efda8bcb3ea218a1a597df0c43b97c9ddfb39
Roman Lytvyn <litvin.roma@gmail.com>
2017-03-27 13:58:48 +0300

    HQ: code review changes by: Add interviewer profile page KP-8844

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Repositories/DeviceSyncInfoRepository.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Repositories/DeviceSyncInfoRepository.cs
@@ -7,35 +7,38 @@ namespace WB.Core.BoundedContexts.Headquarters.Repositories
 {
     internal class DeviceSyncInfoRepository : IDeviceSyncInfoRepository
     {
+        private readonly HQPlainStorageDbContext dbContext;
+        public DeviceSyncInfoRepository(HQPlainStorageDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
         public void AddOrUpdate(DeviceSyncInfo deviceSyncInfo)
         {
-            using (var dbContext = new HQPlainStorageDbContext())
-            {
-                if (deviceSyncInfo.Statistics != null)
-                    deviceSyncInfo.StatisticsId = dbContext.SyncStatistics.Add(deviceSyncInfo.Statistics).Id;
-                dbContext.DeviceSyncInfo.AddOrUpdate(deviceSyncInfo);
-                dbContext.SaveChanges();
-            }
+            if (deviceSyncInfo.Statistics != null)
+                deviceSyncInfo.StatisticsId = dbContext.SyncStatistics.Add(deviceSyncInfo.Statistics).Id;
+            dbContext.DeviceSyncInfo.AddOrUpdate(deviceSyncInfo);
+            dbContext.SaveChanges();
         }
 
         public DeviceSyncInfo GetLastByInterviewerId(Guid interviewerId)
-            => new HQPlainStorageDbContext().DeviceSyncInfo.OrderByDescending(deviceInfo => deviceInfo.Id)
+            => this.dbContext.DeviceSyncInfo.OrderByDescending(deviceInfo => deviceInfo.Id)
                 .FirstOrDefault(deviceInfo => deviceInfo.InterviewerId == interviewerId);
 
         public DeviceSyncInfo GetLastSuccessByInterviewerId(Guid interviewerId)
-            => new HQPlainStorageDbContext().DeviceSyncInfo.OrderByDescending(deviceInfo => deviceInfo.Id)
+            => this.dbContext.DeviceSyncInfo.OrderByDescending(deviceInfo => deviceInfo.Id)
                 .FirstOrDefault(deviceInfo => deviceInfo.InterviewerId == interviewerId && deviceInfo.StatisticsId != null);
 
         public DeviceSyncInfo GetLastFailedByInterviewerId(Guid interviewerId)
-            => new HQPlainStorageDbContext().DeviceSyncInfo.OrderByDescending(deviceInfo => deviceInfo.Id)
+            => this.dbContext.DeviceSyncInfo.OrderByDescending(deviceInfo => deviceInfo.Id)
                 .FirstOrDefault(deviceInfo => deviceInfo.InterviewerId == interviewerId && deviceInfo.StatisticsId == null);
 
         public int GetSuccessSynchronizationsCount(Guid interviewerId)
-            => new HQPlainStorageDbContext().DeviceSyncInfo.OrderByDescending(deviceInfo => deviceInfo.Id)
+            => this.dbContext.DeviceSyncInfo.OrderByDescending(deviceInfo => deviceInfo.Id)
                 .Count(deviceInfo => deviceInfo.InterviewerId == interviewerId && deviceInfo.StatisticsId != null);
 
         public int GetFailedSynchronizationsCount(Guid interviewerId)
-            => new HQPlainStorageDbContext().DeviceSyncInfo.OrderByDescending(deviceInfo => deviceInfo.Id)
+            => this.dbContext.DeviceSyncInfo.OrderByDescending(deviceInfo => deviceInfo.Id)
                 .Count(deviceInfo => deviceInfo.InterviewerId == interviewerId && deviceInfo.StatisticsId == null);
     }
 }
