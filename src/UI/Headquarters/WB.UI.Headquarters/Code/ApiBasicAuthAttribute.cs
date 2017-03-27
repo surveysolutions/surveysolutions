6f025d44a54f406e46b1146e1bf0a5b43a69b5f1
Victor Hladkikh <me@alfeg.net>
2017-03-27 14:54:43 +0300

    KP-8983 User auth problem after upgrade # Code cleanup

--- src/UI/Headquarters/WB.UI.Headquarters/Code/ApiBasicAuthAttribute.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/Code/ApiBasicAuthAttribute.cs
@@ -2,10 +2,7 @@
 using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Security.Claims;
-using System.Text;
 using System.Threading;
-using System.Web;
 using System.Web.Http.Controllers;
 using System.Web.Http.Filters;
 using Main.Core.Entities.SubEntities;
@@ -14,12 +11,9 @@ using WB.Core.Infrastructure.ReadSide;
 using WB.UI.Headquarters.Resources;
 using System.Threading.Tasks;
 using WB.Core.BoundedContexts.Headquarters.OwinSecurity;
-using WB.Core.BoundedContexts.Headquarters.Views.User;
-using WB.Core.GenericSubdomains.Portable;
 
 namespace WB.UI.Headquarters.Code
 {
-
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
     public class ApiBasicAuthAttribute : AuthorizationFilterAttribute
     {
@@ -78,56 +72,10 @@ namespace WB.UI.Headquarters.Code
             await base.OnAuthorizationAsync(actionContext, cancellationToken);
         }
 
-        private bool CheckHashedPassword(HqUser userInfo, BasicCredentials basicCredentials)
-        {
-            var compatibilityProvider = ServiceLocator.Current.GetInstance<IHashCompatibilityProvider>();
-
-            if (compatibilityProvider.IsInSha1CompatibilityMode() && userInfo.IsInRole(UserRoles.Interviewer))
-            {
-                return userInfo.PasswordHashSha1 == basicCredentials.Password;
-            }
-
-            return userInfo.PasswordHash == basicCredentials.Password;
-        }
-
-        private BasicCredentials ExtractFromAuthorizationHeader(ApiAuthenticationScheme scheme, HttpActionContext actionContext)
-        {
-            try
-            {
-                string schemeString = scheme.ToString();
-                string header = actionContext?.Request.Headers?.Authorization?.ToString();
-
-                if (header == null) return null;
-                if (!header.StartsWith(schemeString, StringComparison.OrdinalIgnoreCase)) return null;
-
-                string credentials = Encoding.ASCII.GetString(Convert.FromBase64String(header.Substring(schemeString.Length + 1)));
-                int splitOn = credentials.IndexOf(':');
-
-                return new BasicCredentials
-                {
-                    Username = credentials.Substring(0, splitOn),
-                    Password = credentials.Substring(splitOn + 1),
-                    Scheme = scheme
-                };
-            }
-            catch
-            {
-            }
-
-            return null;
-        }
-
-        internal class BasicCredentials
-        {
-            public string Username { get; set; }
-            public string Password { get; set; }
-            public ApiAuthenticationScheme Scheme { get; set; }
-        }
-
         private void RespondWithMessageThatUserDoesNotExists(HttpActionContext actionContext)
         {
             actionContext.Response = new HttpResponseMessage(HttpStatusCode.Unauthorized) { ReasonPhrase = string.Format(TabletSyncMessages.InvalidUserFormat, actionContext.Request.RequestUri.GetLeftPart(UriPartial.Authority)) };
-            actionContext.Response.Headers.Add("WWW-Authenticate", string.Format("Basic realm=\"{0}\"", actionContext.Request.RequestUri.DnsSafeHost));
+            actionContext.Response.Headers.Add(@"WWW-Authenticate", $@"Basic realm=""{actionContext.Request.RequestUri.DnsSafeHost}""");
         }
 
         private void RespondWithMaintenanceMessage(HttpActionContext actionContext)
