56565ecf2b93d0584518ceffb7e2ffda5eb2127a
Victor Gladkikh <me@alfeg.net>
2017-03-28 16:10:21 +0300

    KP-8982 Compatibility: authentication error after version upgrade # code review fixes on naming

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Services/IAuthorizedUser.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Services/IAuthorizedUser.cs
@@ -10,7 +10,7 @@ namespace WB.Core.BoundedContexts.Headquarters.Services
         bool IsSupervisor { get; }
         bool IsObserver { get; }
         bool IsObserving { get; }
-        bool IsAuthorized { get; }
+        bool IsAuthenticated { get; }
 
         Guid Id { get; }
         string UserName { get; }
