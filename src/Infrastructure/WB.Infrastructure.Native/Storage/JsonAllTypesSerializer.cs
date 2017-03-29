18e36c0b16acc7be25527c74f051738ca07e6135
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2017-03-29 14:52:53 +0300

    KP-9008: Fixed backward/forward compatibility of roster vector serialization

--- src/Infrastructure/WB.Infrastructure.Native/Storage/JsonAllTypesSerializer.cs
+++ src/Infrastructure/WB.Infrastructure.Native/Storage/JsonAllTypesSerializer.cs
@@ -19,7 +19,7 @@ namespace WB.Infrastructure.Native.Storage
                 TypeNameHandling = TypeNameHandling.All,
                 NullValueHandling = NullValueHandling.Ignore,
                 FloatParseHandling = FloatParseHandling.Decimal,
-                Converters = new List<JsonConverter> { IdentityJsonConverter.Instance, RosterVectorConvertor.Instance },
+                Converters = new List<JsonConverter> { IdentityJsonConverter.Instance, RosterVectorConverter.Instance },
                 Binder = new OldToNewAssemblyRedirectSerializationBinder()
             };
         }
