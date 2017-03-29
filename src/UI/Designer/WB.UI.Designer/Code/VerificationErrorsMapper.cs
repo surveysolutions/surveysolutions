11ed781f66f7d57ec17d87bd9f2ca8cbfb9d4437
Slava Scherbak <slava.scherbak@gmail.com>
2017-03-29 14:18:15 -0400

    Designer: fixed failed verification after removing attachment. KP-9023

--- src/UI/Designer/WB.UI.Designer/Code/VerificationErrorsMapper.cs
+++ src/UI/Designer/WB.UI.Designer/Code/VerificationErrorsMapper.cs
@@ -61,13 +61,13 @@ namespace WB.UI.Designer.Code
             {
                 if (reference.Type == QuestionnaireVerificationReferenceType.Attachment)
                 {
-                    var attachment = questionnaireDocument.Attachments.Single(x => x.AttachmentId == reference.Id);
+                    var attachment = questionnaireDocument.Attachments.SingleOrDefault(x => x.AttachmentId == reference.Id);
                     yield return new VerificationReferenceEnriched
                     {
                         ItemId = reference.Id.FormatGuid(),
                         Type = QuestionnaireVerificationReferenceType.Attachment,
-                        Variable = attachment.Name,
-                        Title = attachment.Name
+                        Variable = attachment?.Name,
+                        Title = attachment?.Name
                     };
                     continue;
                 }
