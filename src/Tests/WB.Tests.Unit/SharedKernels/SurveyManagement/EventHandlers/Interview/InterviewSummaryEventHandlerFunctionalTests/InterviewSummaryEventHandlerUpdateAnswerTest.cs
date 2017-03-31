fcae8b3df9207f978d84c37a12061308d1650eb7
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2017-03-31 11:52:01 +0300

    KP-9042: Convert prefilled categorical questions to strings after sync

--- src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/EventHandlers/Interview/InterviewSummaryEventHandlerFunctionalTests/InterviewSummaryEventHandlerUpdateAnswerTest.cs
+++ src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/EventHandlers/Interview/InterviewSummaryEventHandlerFunctionalTests/InterviewSummaryEventHandlerUpdateAnswerTest.cs
@@ -116,6 +116,7 @@ namespace WB.Tests.Unit.SharedKernels.SurveyManagement.EventHandlers.Interview.I
             var questionId = Guid.Parse("10000000000000000000000000000000");
             var userId = Guid.Parse("20000000000000000000000000000002");
 
+            var questionnaire = Create.Entity.QuestionnaireDocumentWithOneChapter(children:Create.Entity.TextQuestion(questionId));
             var savedInterviewSummary = CreateInterviewSummaryQuestions(
                     new QuestionAnswer()
                     {
@@ -127,9 +128,9 @@ namespace WB.Tests.Unit.SharedKernels.SurveyManagement.EventHandlers.Interview.I
 
             var featuredQuestionsMeta = new []{ Create.Entity.AnsweredQuestionSynchronizationDto(questionId, new decimal[0], answer ) };
 
-            var interviewSummaryEventHandler = CreateInterviewSummaryEventHandlerFunctional();
+            var interviewSummaryEventHandler = CreateInterviewSummaryEventHandlerFunctional(questionnaire);
 
-            var synchronizationMetadataApplied = new SynchronizationMetadataApplied(userId, questionnaireId,1, InterviewStatus.Created, featuredQuestionsMeta, false, null, null, null);
+            var synchronizationMetadataApplied = new SynchronizationMetadataApplied(userId, questionnaireId, 1, InterviewStatus.Created, featuredQuestionsMeta, false, null, null, null);
 
             var updatedInterviewSummary = this.CallUpdateMethod(interviewSummaryEventHandler, savedInterviewSummary,
                 synchronizationMetadataApplied);
@@ -179,16 +180,13 @@ namespace WB.Tests.Unit.SharedKernels.SurveyManagement.EventHandlers.Interview.I
             return publishableEventMock.Object;
         }
 
-        protected static InterviewSummaryDenormalizer CreateInterviewSummaryEventHandlerFunctional(QuestionnaireDocument questionnaire=null)
+        protected static InterviewSummaryDenormalizer CreateInterviewSummaryEventHandlerFunctional(QuestionnaireDocument questionnaire = null)
         {
             var mockOfInterviewSummary = new Mock<IReadSideRepositoryWriter<InterviewSummary>>();
-            return new InterviewSummaryDenormalizer(mockOfInterviewSummary.Object,
+            return new InterviewSummaryDenormalizer(
+                mockOfInterviewSummary.Object,
                 new Mock<IUserViewFactory>().Object,
-                questionnaireStorage:
-                    Mock.Of<IQuestionnaireStorage>(
-                        _ =>
-                            _.GetQuestionnaireDocument(Moq.It.IsAny<Guid>(), Moq.It.IsAny<long>()) ==
-                            questionnaire));
+                Mock.Of<IQuestionnaireStorage>(_ => _.GetQuestionnaireDocument(Moq.It.IsAny<Guid>(), Moq.It.IsAny<long>()) == questionnaire));
         }
 
         protected static InterviewSummary CreateInterviewSummaryQuestions(params QuestionAnswer[] questions)
