36ab1196263ac0a119ebe4394a29d922c5567f73
Victor Gladkikh <me@alfeg.net>
2017-04-06 16:46:39 +0300

    Custom datamigration utility for EF

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/OwinSecurity/Configuration.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/OwinSecurity/Configuration.cs
@@ -1,14 +1,7 @@
-using System;
 using System.Data.Entity;
 using System.Data.Entity.Migrations;
-using System.Linq;
-using Main.Core.Entities.SubEntities;
-using Microsoft.Practices.ServiceLocation;
+using WB.Core.BoundedContexts.Headquarters.Migrator;
 using WB.Core.BoundedContexts.Headquarters.Views.User;
-using WB.Core.GenericSubdomains.Portable;
-using WB.Core.Infrastructure.PlainStorage;
-using WB.Core.Infrastructure.Transactions;
-using WB.Core.SharedKernels.DataCollection.Views;
 
 namespace WB.Core.BoundedContexts.Headquarters.OwinSecurity
 {
@@ -19,94 +12,21 @@ namespace WB.Core.BoundedContexts.Headquarters.OwinSecurity
         {
             AutomaticMigrationsEnabled = true;
             AutomaticMigrationDataLossAllowed = true;
-            SetHistoryContextFactory("Npgsql",
-                (connection, defaultSchema) => new SchemaBasedHistoryContext(connection, defaultSchema, SchemaName));
+            SetHistoryContextFactory("Npgsql", (connection, defaultSchema) => new SchemaBasedHistoryContext(connection, defaultSchema, SchemaName));
         }
 
         public DbSet<HqUserProfile> UserProfiles { get; set; }
 
         private static readonly object onMigration = new object();
-        
+
         protected override void Seed(HQIdentityDbContext context)
         {
+            //  This method will be called after migrating to the latest version. And after each build...
+
             lock (onMigration)
             {
-                //  This method will be called after migrating to the latest version.
-
-                if (context.Roles.Any()) return;
-
-                var plainTransactionManager = ServiceLocator.Current.GetInstance<IPlainTransactionManager>();
-                var userPlainStorageRepository = ServiceLocator.Current.GetInstance<IPlainStorageAccessor<UserDocument>>();
-
-                using (var dbContextTransaction = context.Database.BeginTransaction())
-                {
-                    try
-                    {
-                        context.Configuration.AutoDetectChangesEnabled = false;
-
-                        foreach (UserRoles userRole in Enum.GetValues(typeof(UserRoles)))
-                        {
-                            context.Roles.AddOrUpdate(new HqRole
-                            {
-                                Id = userRole.ToUserId(),
-                                Name = Enum.GetName(typeof(UserRoles), userRole)
-                            });
-                        }
-
-                        plainTransactionManager.ExecuteInPlainTransaction(() =>
-                        {
-                            foreach (var oldUser in userPlainStorageRepository.Query(users => users.ToList()))
-                            {
-                                HqUserProfile hqUserProfile = null;
-                                if (oldUser.Supervisor != null || !string.IsNullOrEmpty(oldUser.DeviceId))
-                                    hqUserProfile = new HqUserProfile
-                                    {
-                                        DeviceId = oldUser.DeviceId,
-                                        SupervisorId = oldUser.Supervisor?.Id,
-                                    };
-
-                                var hqUser = new HqUser
-                                {
-                                    Id = oldUser.PublicKey,
-                                    CreationDate = oldUser.CreationDate,
-                                    Email = oldUser.Email,
-                                    IsArchived = oldUser.IsArchived,
-                                    IsLockedByHeadquaters = oldUser.IsLockedByHQ,
-                                    IsLockedBySupervisor = oldUser.IsLockedBySupervisor,
-                                    FullName = oldUser.PersonName,
-                                    PhoneNumber = oldUser.PhoneNumber,
-                                    UserName = oldUser.UserName,
-                                    PasswordHash = oldUser.Password,
-                                    PasswordHashSha1 = oldUser.Password,
-                                    SecurityStamp = Guid.NewGuid().FormatGuid(),
-                                    Profile = hqUserProfile
-                                };
-
-                                foreach (var oldUserRole in oldUser.Roles)
-                                    hqUser.Roles.Add(new HqUserRole
-                                    {
-                                        UserId = oldUser.PublicKey,
-                                        RoleId = oldUserRole.ToUserId()
-                                    });
-
-                                context.Users.Add(hqUser);
-                            }
-                        });
-
-                        context.SaveChanges();
-
-                        dbContextTransaction.Commit();
-                    }
-                    catch (Exception)
-                    {
-                        dbContextTransaction.Rollback();
-                        throw;
-                    }
-                    finally
-                    {
-                        context.Configuration.AutoDetectChangesEnabled = true;
-                    }
-                }
+                var migrator = new DataMigrator<HQIdentityDbContext>();
+                migrator.MigrateToLatest(context);
             }
         }
     }
---
+++ src/Tests/WB.Tests.Integration/UtilsTest/DataMigrationTests/When_migration_success_DataMigratorTests.cs
@@ -0,0 +1,107 @@
+﻿using System.Data.Common;
+using System.Data.Entity;
+using System.Threading.Tasks;
+using Npgsql;
+using NUnit.Framework;
+using WB.Core.BoundedContexts.Headquarters.Migrator;
+using WB.Tests.Integration.PostgreSQLTests;
+
+namespace WB.Tests.Integration.UtilsTest.DataMigrationTests
+{
+    [TestFixture]
+    public class When_migration_success_DataMigratorTests : with_postgres_db
+    {
+        [OneTimeSetUp]
+        public void Setup() => Context();
+
+        [OneTimeTearDown]
+        public void TearDown() => Cleanup();
+
+        [Test]
+        public async Task Should_run_two_data_migrations()
+        {
+            var ctx = new TestOkDbContext(new NpgsqlConnection(connectionStringBuilder.ConnectionString));
+
+            var migrations = await ctx.DataMigrations.ToListAsync();
+
+            Assert.That(migrations, Has.Count.EqualTo(2));
+            Assert.That(migrations[0], Has.Property(nameof(DataMigrationInfo.Id)).EqualTo("20170407124702"));
+            Assert.That(migrations[0], Has.Property(nameof(DataMigrationInfo.Name)).EqualTo(nameof(TestOkEntityMigrationTest1)));
+
+            Assert.That(migrations[1], Has.Property(nameof(DataMigrationInfo.Id)).EqualTo("20170407125222"));
+            Assert.That(migrations[1], Has.Property(nameof(DataMigrationInfo.Name)).EqualTo(nameof(TestOkEntityMigrationTest2)));
+        }
+
+        [Test]
+        public void Should_run_first_migration_and_fill_data()
+        {
+            var ctx = new TestOkDbContext(new NpgsqlConnection(connectionStringBuilder.ConnectionString));
+
+            var entity = ctx.TestEntities.Find(1);
+            Assert.That(entity.Id, Is.EqualTo(1));
+            Assert.That(entity.Value, Is.EqualTo("Test migration 1"));
+        }
+
+        [Test]
+        public void Should_run_second_migration_and_fill_data()
+        {
+            var ctx = new TestOkDbContext(new NpgsqlConnection(connectionStringBuilder.ConnectionString));
+
+            var entity = ctx.TestEntities.Find(2);
+            Assert.That(entity.Id, Is.EqualTo(2));
+            Assert.That(entity.Value, Is.EqualTo("Test migration 2"));
+        }
+        
+        internal sealed class TestOkDbContext : DbContext, IDataMigrationContext
+        {
+            public TestOkDbContext(DbConnection db) : base(db, true)
+            {
+                Database.SetInitializer(new DropCreateDatabaseWithDataMigration<TestOkDbContext>());
+            }
+
+            // Here you define your own DbSet's
+            protected override void OnModelCreating(DbModelBuilder modelBuilder)
+            {
+                base.OnModelCreating(modelBuilder);
+
+                modelBuilder.Entity<TestEntity>().ToTable("entity", "test");
+                modelBuilder.Entity<DataMigrationInfo>().ToTable("dataMigrations", "test");
+            }
+
+            public IDbSet<DataMigrationInfo> DataMigrations { get; set; }
+            public IDbSet<TestEntity> TestEntities { get; set; }
+        }
+
+        internal class TestOkEntityMigrationTest1 : DataMigration<TestOkDbContext>
+        {
+            public override string Id => "20170407124702";
+
+            public override void Up(TestOkDbContext context)
+            {
+                context.TestEntities.Add(new TestEntity()
+                {
+                    Id = 1,
+                    Value = "Test migration 1"
+                });
+
+                context.SaveChanges();
+            }
+        }
+
+        internal class TestOkEntityMigrationTest2 : DataMigration<TestOkDbContext>
+        {
+            public override string Id => "20170407125222";
+
+            public override void Up(TestOkDbContext context)
+            {
+                context.TestEntities.Add(new TestEntity()
+                {
+                    Id = 2,
+                    Value = "Test migration 2"
+                });
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
