4e90398939aceb9b64b12ed0b9f4ea6e21669001
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2017-04-06 11:30:09 -0400

    version up for tester <-> Designer sync

--- src/Core/SharedKernel/WB.Core.SharedKernel.Structures/Api/Designer/ApiVersion.cs
+++ src/Core/SharedKernel/WB.Core.SharedKernel.Structures/Api/Designer/ApiVersion.cs
@@ -2,7 +2,7 @@ namespace WB.Core.SharedKernels.SurveySolutions.Api.Designer
 {
     public static class ApiVersion
     {
-        public const int CurrentTesterProtocolVersion = 26;
+        public const int CurrentTesterProtocolVersion = 27;
         public const int MaxQuestionnaireVersion = 19;
     }
 }
