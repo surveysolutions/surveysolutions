a3c3f10738e3046dc0a4299bf656b7855c878b42
Victor Gladkikh <me@alfeg.net>
2017-04-07 16:02:45 +0300

    KP-9032 Interviewers list: Enumerators with outdated software # Cleanup

---
+++ src/UI/Headquarters/WB.UI.Headquarters/Code/InterviewerVersionReader.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.Hosting;
+using WB.Core.BoundedContexts.Headquarters.Services;
+using WB.Core.Infrastructure.FileSystem;
+using WB.UI.Headquarters.API;
+
+namespace WB.UI.Headquarters.Code
+{
+    class InterviewerVersionReader : IInterviewerVersionReader
+    {
+        private readonly IAndroidPackageReader androidPackageReader;
+        private readonly IFileSystemAccessor fileSystemAccessor;
+
+        public InterviewerVersionReader(IAndroidPackageReader androidPackageReader, IFileSystemAccessor fileSystemAccessor)
+        {
+            this.androidPackageReader = androidPackageReader;
+            this.fileSystemAccessor = fileSystemAccessor;
+        }
+
+        public int? Version
+        {
+            get
+            {
+                string pathToInterviewerApp = this.fileSystemAccessor.CombinePath(HostingEnvironment.MapPath(InterviewerApkInfo.Directory), InterviewerApkInfo.FileName);
+
+                int? interviewerApkVersion = !this.fileSystemAccessor.IsFileExists(pathToInterviewerApp)
+                    ? null
+                    : this.androidPackageReader.Read(pathToInterviewerApp).Version;
+
+                return interviewerApkVersion;
+            }
+        }
+    }
+}
