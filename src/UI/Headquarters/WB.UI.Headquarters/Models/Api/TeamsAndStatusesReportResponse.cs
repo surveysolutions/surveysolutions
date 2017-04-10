5d430cd3b723f78ae86b98e4ca18942c5fcecc0d
Andrii Kozhyn <hedgic@gmail.com>
2017-04-10 16:33:53 +0300

    KP-9060: Initial redesigned page implementation

---
+++ src/UI/Headquarters/WB.UI.Headquarters/Models/Api/TeamsAndStatusesReportResponse.cs
@@ -0,0 +1,9 @@
+﻿using WB.Core.BoundedContexts.Headquarters.Views.Reposts.Views;
+
+namespace WB.UI.Headquarters.Models.Api
+{
+    public class TeamsAndStatusesReportResponse : DataTableResponse<TeamsAndStatusesReportLine>
+    {
+        public TeamsAndStatusesReportLine TotalRow { get; set; }
+    }
+}
