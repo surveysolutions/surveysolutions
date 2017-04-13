c077f1abc9fefb084d72ef24c1c81895d95bea49
Andrii Kozhyn <hedgic@gmail.com>
2017-04-13 13:01:22 +0300

    KP-9133: Ignore "Random" column on preloading

---
+++ src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/PreloadedDataVerifierTests/when_sample_data_has__ssSys_IRnd__column.cs
@@ -0,0 +1,31 @@
+using System;
+using NUnit.Framework;
+using WB.Tests.Abc;
+
+namespace WB.Tests.Unit.SharedKernels.SurveyManagement.PreloadedDataVerifierTests
+{
+    internal class when_sample_data_has__ssSys_IRnd__column : PreloadedDataVerifierTestContext
+    {
+        [Test]
+        public void should_ignore_column()
+        {
+            var questionnaireId = Guid.Parse("11111111111111111111111111111111");
+            var numericQuestionId = Guid.Parse("21111111111111111111111111111111");
+            var question = Create.Entity.NumericQuestion(questionId: numericQuestionId, variableName: "num");
+
+            var questionnaire = CreateQuestionnaireDocumentWithOneChapter(question);
+            questionnaire.Title = "questionnaire";
+            var preloadedDataByFile = CreatePreloadedDataByFile(new[] { "Id", "num", "ssSys_IRnd" },
+                new[] { new string[] { "1", "5", "3.22" } },
+                "questionnaire.csv");
+
+            var preloadedDataService = Create.Service.PreloadedDataService(questionnaire);
+
+            var preloadedDataVerifier = CreatePreloadedDataVerifier(questionnaire, preloadedDataService);
+
+            var result = preloadedDataVerifier.VerifyPanel(questionnaireId, 1, new[] { preloadedDataByFile });
+
+            Assert.That(result.Errors, Is.Empty);
+        }
+    }
+}
