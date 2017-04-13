8e95566d1b18f0892ef5b47105f38cb1c2051d9f
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2017-04-13 12:54:06 -0400

    KP-9098 Android support of "Area" question

---
+++ src/Infrastructure/Enumerator/Internals/AreaEditor/AreaEditResult.cs
@@ -0,0 +1,7 @@
+﻿namespace WB.Infrastructure.Shared.Enumerator.Internals.AreaEditor
+{
+    public class AreaEditResult
+    {
+        public string Area { set; get; }
+    }
+}
---
+++ src/Infrastructure/Enumerator/Internals/AreaEditor/AreaEditorActivity.cs
@@ -0,0 +1,20 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+
+namespace WB.Infrastructure.Shared.Enumerator.Internals.AreaEditor
+{
+    [Activity(ConfigurationChanges = ConfigChanges.KeyboardHidden | ConfigChanges.ScreenSize | ConfigChanges.Orientation, 
+        Label = "AreaEditorActivity", Theme = "@android:style/Theme.Black.NoTitleBar.Fullscreen")]
+    public class AreaEditorActivity : Activity
+    {
+        public static event System.Action<AreaEditResult> OnAreaEditCompleted;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+        }
+    }
+}
