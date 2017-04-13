61c130df208ca50d9ca5239b13b2f1dc614ad761
Victor Gladkikh <me@alfeg.net>
2017-04-13 13:48:05 +0300

    KP-9047 Edit account # Interviewer edit/create

--- src/UI/Headquarters/WB.UI.Headquarters/App_GlobalResources/Users.Designer.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/App_GlobalResources/Users.Designer.cs
@@ -19,7 +19,7 @@ namespace Resources {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option or rebuild the Visual Studio project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Web.Application.StronglyTypedResourceProxyBuilder", "14.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Web.Application.StronglyTypedResourceProxyBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Users {
@@ -417,6 +417,15 @@ namespace Resources {
         internal static string SupervisorsInTeam {
             get {
                 return ResourceManager.GetString("SupervisorsInTeam", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Edit Supervisor.
+        /// </summary>
+        internal static string Supervisors_Edit {
+            get {
+                return ResourceManager.GetString("Supervisors_Edit", resourceCulture);
             }
         }
         
