5776b19db0066bc0ac2fb7aac2b2213130430bd4
Slava Scherbak <slava.scherbak@gmail.com>
2017-04-14 16:26:41 -0400

    Refactored map report, removed version dropdown. KP-9063

--- src/UI/Headquarters/WB.UI.Headquarters/Models/ComponentModels/ComboboxModel.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/Models/ComponentModels/ComboboxModel.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace WB.UI.Headquarters.Models.ComponentModels
+﻿namespace WB.UI.Headquarters.Models.ComponentModels
 {
     public class ComboboxModel
     {
