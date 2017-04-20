c464be4ccaed690bc0cc2b57b0c485da4e1fe28c
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2017-04-20 17:33:45 -0400

    new label: KP-9182 General improvements

--- src/UI/Headquarters/WB.UI.Headquarters/Resources/FieldsAndValidations.Designer.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/Resources/FieldsAndValidations.Designer.cs
@@ -124,6 +124,15 @@ namespace WB.UI.Headquarters.Resources {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Confirm new password.
+        /// </summary>
+        public static string ConfirmNewPasswordFieldName {
+            get {
+                return ResourceManager.GetString("ConfirmNewPasswordFieldName", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to The password and confirmation password do not match.
         /// </summary>
         public static string ConfirmPasswordErrorMassage {
@@ -133,7 +142,7 @@ namespace WB.UI.Headquarters.Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Confirm new password.
+        ///   Looks up a localized string similar to Confirm password.
         /// </summary>
         public static string ConfirmPasswordFieldName {
             get {
@@ -205,6 +214,15 @@ namespace WB.UI.Headquarters.Resources {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to New password.
+        /// </summary>
+        public static string NewPasswordFieldName {
+            get {
+                return ResourceManager.GetString("NewPasswordFieldName", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to To change password provide valid existing password.
         /// </summary>
         public static string OldPasswordErrorMessage {
@@ -232,7 +250,7 @@ namespace WB.UI.Headquarters.Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to New password.
+        ///   Looks up a localized string similar to Password.
         /// </summary>
         public static string PasswordFieldName {
             get {
