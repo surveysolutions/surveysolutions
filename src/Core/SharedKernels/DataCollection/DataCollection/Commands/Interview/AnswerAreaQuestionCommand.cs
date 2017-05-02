962097bc8f43f65ae8a59bee3af2ab6705be2340
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2017-05-02 13:43:44 -0400

    map and area : KP-9098 Android support of "Area" question

--- src/Core/SharedKernels/DataCollection/DataCollection/Commands/Interview/AnswerAreaQuestionCommand.cs
+++ src/Core/SharedKernels/DataCollection/DataCollection/Commands/Interview/AnswerAreaQuestionCommand.cs
@@ -5,12 +5,17 @@ namespace WB.Core.SharedKernels.DataCollection.Commands.Interview
 {
     public class AnswerAreaQuestionCommand : AnswerQuestionCommand
     {
-        public string Answer { get; private set; }
+        public string Geometry { get; private set; }
+        public string MapName { get; private set; }
+        public double Area { get; private set; }
 
-        public AnswerAreaQuestionCommand(Guid interviewId, Guid userId, Guid questionId, decimal[] rosterVector, DateTime answerTime, string answer)
+        public AnswerAreaQuestionCommand(Guid interviewId, Guid userId, Guid questionId, decimal[] rosterVector, 
+            DateTime answerTime, string geometry, string mapName, double area)
             : base(interviewId, userId, questionId, rosterVector, answerTime)
         {
-            this.Answer = answer;
+            this.Geometry = geometry;
+            this.MapName = mapName;
+            this.Area = area;
         }
     }
 }
