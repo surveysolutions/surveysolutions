8a0a8224354ee17c479989d296ac880bfe55cf3c
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2017-05-10 16:41:08 -0400

    update and clean up scripts : KP-8833 Reduce amound of js scripts from Designer

--- src/UI/Designer/WB.UI.Designer/Scripts/_references.js
+++ src/UI/Designer/WB.UI.Designer/Scripts/_references.js
@@ -1,4 +1,3 @@
 ﻿/// <reference path="jquery-3.1.1.js" />
 /// <reference path="jquery.validate.js" />
 /// <reference path="jquery.validate.unobtrusive.js" />
-/// <reference path="knockout-2.1.0.debug.js" />
