64db43b5689b87dc70d8ca8dd8618188f6f69328
Andrii Kozhyn <hedgic@gmail.com>
2017-05-17 16:55:40 +0300

    KP-9277: fixed test

--- src/Tests/WB.Tests.Unit/Applications/Headquarters/ExportApiTests/when_getting_export_process_details_for_paradata.cs
+++ src/Tests/WB.Tests.Unit/Applications/Headquarters/ExportApiTests/when_getting_export_process_details_for_paradata.cs
@@ -40,7 +40,7 @@ namespace WB.Tests.Unit.Applications.Headquarters.ExportApiTests
             controller = CreateExportController(dataExportStatusReader: dataExportStatusReader);
         };
 
-        Because of = () => result = controller.ProcessDetails(questionnaireIdentity.ToString(), DataExportFormat.Tabular);
+        Because of = () => result = controller.ProcessDetails(questionnaireIdentity.ToString(), DataExportFormat.Paradata);
 
         It should_return_http_ok_response = () =>
             result.ShouldBeOfExactType<OkNegotiatedContentResult<ExportController.ExportDetails>>();
