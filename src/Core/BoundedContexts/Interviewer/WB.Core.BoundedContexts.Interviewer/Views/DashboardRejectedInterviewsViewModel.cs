6f11c5157048e13f28ffe4bb6ab65e67e6fc84ff
Roman Lytvyn <litvin.roma@gmail.com>
2017-05-17 16:31:16 +0300

    IN: swipe for dashboard navigation between tabs. KP-6122

---
+++ src/Core/BoundedContexts/Interviewer/WB.Core.BoundedContexts.Interviewer/Views/DashboardRejectedInterviewsViewModel.cs
@@ -0,0 +1,52 @@
+using System.Collections.Generic;
+using System.Linq;
+using WB.Core.BoundedContexts.Interviewer.Properties;
+using WB.Core.BoundedContexts.Interviewer.Views.Dashboard;
+using WB.Core.BoundedContexts.Interviewer.Views.Dashboard.DashboardItems;
+using WB.Core.SharedKernels.DataCollection.ValueObjects.Interview;
+using WB.Core.SharedKernels.Enumerator.Services;
+using WB.Core.SharedKernels.Enumerator.Services.Infrastructure;
+using WB.Core.SharedKernels.Enumerator.Services.Infrastructure.Storage;
+
+namespace WB.Core.BoundedContexts.Interviewer.Views
+{
+    public class DashboardRejectedInterviewsViewModel : ListViewModel<InterviewDashboardItemViewModel>
+    {
+        private readonly IPlainStorage<InterviewView> interviewViewRepository;
+        private readonly IInterviewViewModelFactory viewModelFactory;
+        private readonly IPrincipal principal;
+
+        public DashboardRejectedInterviewsViewModel(
+            IPlainStorage<InterviewView> interviewViewRepository,
+            IInterviewViewModelFactory viewModelFactory,
+            IPrincipal principal)
+        {
+            this.interviewViewRepository = interviewViewRepository;
+            this.viewModelFactory = viewModelFactory;
+            this.principal = principal;
+        }
+
+        public void Load()
+        {
+            this.Items = this.GetRejectedInterviews().ToList();
+            this.Title = string.Format(InterviewerUIResources.Dashboard_RejectedLinkText, this.Items.Count);
+        }
+
+        private IEnumerable<InterviewDashboardItemViewModel> GetRejectedInterviews()
+        {
+            var interviewerId = this.principal.CurrentUserIdentity.UserId;
+
+            var interviewViews = this.interviewViewRepository.Where(interview =>
+                interview.ResponsibleId == interviewerId &&
+                interview.Status == InterviewStatus.RejectedBySupervisor);
+
+            foreach (var interviewView in interviewViews)
+            {
+                var interviewDashboardItem = this.viewModelFactory.GetNew<InterviewDashboardItemViewModel>();
+                interviewDashboardItem.Init(interviewView);
+
+                yield return interviewDashboardItem;
+            }
+        }
+    }
+}
---
+++ src/UI/Interviewer/WB.UI.Interviewer/Activities/DashboardQuestionnairesFragment.cs
@@ -0,0 +1,11 @@
+using Android.Runtime;
+using WB.Core.BoundedContexts.Interviewer.Views;
+
+namespace WB.UI.Interviewer.Activities
+{
+    [Register("wb.ui.interviewer.activities.DashboardQuestionnairesFragment")]
+    public class DashboardQuestionnairesFragment : RecyclerViewFragment<DashboardQuestionnairesAndNewInterviewsViewModel>
+    {
+        
+    }
+}
