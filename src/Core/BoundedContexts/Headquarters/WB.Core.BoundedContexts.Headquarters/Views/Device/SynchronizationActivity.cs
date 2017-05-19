f970d532fb890488f3a14e1befb9fd69992337d0
Roman Lytvyn <litvin.roma@gmail.com>
2017-05-19 16:50:50 +0300

    HQ: synchronization activity changes by QA comments. KP-8972

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Views/Device/SynchronizationActivity.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Views/Device/SynchronizationActivity.cs
@@ -15,12 +15,32 @@ namespace WB.Core.BoundedContexts.Headquarters.Views.Device
 
     public class SyncDayQuarter
     {
+        private const int maxActionsCount = 6;
         public int DownloadedQuestionnairesCount { get; set; }
         public int DownloadedInterviewsCount { get; set; }
         public int UploadedInterviewsCount { get; set; }
-        public int StartedInterviewsCount { get; set; }
+        public int NewInterviewsOnDeviceCount { get; set; }
+
+        public int SynchronizationsWithoutChangesCount { get; set; }
 
         public int FailedSynchronizationsCount { get; set; }
-        public int SynchronizationsWithoutChangesCount { get; set; }
+
+        public bool HasAnyActivity => this.DownloadedInterviewsCount > 0 ||
+                                      this.DownloadedQuestionnairesCount > 0 ||
+                                      this.UploadedInterviewsCount > 0;
+
+        public bool HasMoreThanMaxActionsCount
+            => (this.DownloadedInterviewsCount + this.DownloadedQuestionnairesCount + this.UploadedInterviewsCount) > maxActionsCount;
+
+        public int DownloadedQuestionnairesInProportionCount
+            => (int) (this.DownloadedQuestionnairesCount * this.actionWeight);
+        public int DownloadedInterviewsInProportionCount => (int) (this.DownloadedInterviewsCount * this.actionWeight);
+        public int UploadedInterviewsInProportionCount => (int) (this.UploadedInterviewsCount * this.actionWeight);
+
+        private int allActionsCount
+            => this.DownloadedQuestionnairesCount + this.DownloadedInterviewsCount + this.UploadedInterviewsCount;
+
+        private double actionWeight => this.HasMoreThanMaxActionsCount ? (double)maxActionsCount / allActionsCount : 1;
+
     }
 }
