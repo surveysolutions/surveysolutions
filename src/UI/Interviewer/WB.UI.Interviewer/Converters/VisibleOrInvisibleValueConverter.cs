51b50aa61d73fe560648323a63d3ed8bb43e8314
Victor Hladkikh <me@alfeg.net>
2017-05-22 10:15:19 +0300

    KP-9269: Make progress bar more visible # Make progress bar invisible, so no jumping occur

---
+++ src/UI/Interviewer/WB.UI.Interviewer/Converters/VisibleOrInvisibleValueConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+using Android.Views;
+using MvvmCross.Platform.Converters;
+using MvvmCross.Platform.Platform;
+
+namespace WB.UI.Interviewer.Converters
+{
+    public class VisibleOrInvisibleValueConverter : MvxValueConverter<bool, ViewStates>
+    {
+        protected override ViewStates Convert(bool value, Type targetType, object parameter, CultureInfo culture)
+        {
+            MvxTrace.Error("VisibleOrInvisibleValueConverter.Convert");
+            return value ? ViewStates.Visible : ViewStates.Invisible;
+        }
+    }
+}
