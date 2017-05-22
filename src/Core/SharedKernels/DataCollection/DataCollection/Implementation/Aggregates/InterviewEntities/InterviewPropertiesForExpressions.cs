880c2339260608257e6c2cfa6aae06bb5acc921a
Slava Scherbak <slava.scherbak@gmail.com>
2017-05-22 18:04:30 -0400

    Extended properties with users ids. KP-8804

--- src/Core/SharedKernels/DataCollection/DataCollection/Implementation/Aggregates/InterviewEntities/InterviewPropertiesForExpressions.cs
+++ src/Core/SharedKernels/DataCollection/DataCollection/Implementation/Aggregates/InterviewEntities/InterviewPropertiesForExpressions.cs
@@ -1,13 +1,24 @@
-﻿using WB.Core.SharedKernels.DataCollection.ExpressionStorage;
+﻿using System;
+using WB.Core.SharedKernels.DataCollection.ExpressionStorage;
 
 namespace WB.Core.SharedKernels.DataCollection.Implementation.Aggregates.InterviewEntities
 {
     public class InterviewPropertiesForExpressions : IInterviewPropertiesForExpressions
     {
-        public double Random { get; set; }
+        private readonly InterviewProperties properties;
+        public double Random { get; }
 
-        public InterviewPropertiesForExpressions(DataCollection.InterviewProperties interviewProperties)
+        public Guid SupervisorId => this.properties.SupervisorId ?? Guid.Empty;
+
+        public Guid InterviewerId => this.properties.InterviewerId ?? Guid.Empty;
+
+        public string InterviewId => this.properties.Id;
+
+        public InterviewPropertiesForExpressions(
+            DataCollection.InterviewProperties interviewProperties, 
+            InterviewProperties properties)
         {
+            this.properties = properties;
             this.Random = interviewProperties.IRnd();
         }
     }
