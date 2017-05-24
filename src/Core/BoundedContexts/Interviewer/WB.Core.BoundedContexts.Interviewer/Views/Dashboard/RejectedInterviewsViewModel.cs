a69bc20235fe751ef327c42008a091454b6eb524
Roman Lytvyn <litvin.roma@gmail.com>
2017-05-24 14:13:51 +0300

    IN: tabs on dashboard. KP-6122

--- src/Core/BoundedContexts/Interviewer/WB.Core.BoundedContexts.Interviewer/Views/Dashboard/RejectedInterviewsViewModel.cs
+++ src/Core/BoundedContexts/Interviewer/WB.Core.BoundedContexts.Interviewer/Views/Dashboard/RejectedInterviewsViewModel.cs
@@ -2,15 +2,18 @@ using System.Collections.Generic;
 using System.Linq;
 using WB.Core.BoundedContexts.Interviewer.Properties;
 using WB.Core.BoundedContexts.Interviewer.Views.Dashboard.DashboardItems;
-using WB.Core.SharedKernels.DataCollection.ValueObjects.Interview;
 using WB.Core.SharedKernels.Enumerator.Services;
 using WB.Core.SharedKernels.Enumerator.Services.Infrastructure;
 using WB.Core.SharedKernels.Enumerator.Services.Infrastructure.Storage;
+using WB.Core.SharedKernels.Enumerator.ViewModels.InterviewDetails.Groups;
 
 namespace WB.Core.BoundedContexts.Interviewer.Views.Dashboard
 {
     public class RejectedInterviewsViewModel : ListViewModel<InterviewDashboardItemViewModel>
     {
+        public string Description => InterviewerUIResources.Dashboard_RejectedTabText;
+        public override GroupStatus InterviewStatus => GroupStatus.StartedInvalid;
+
         private readonly IPlainStorage<InterviewView> interviewViewRepository;
         private readonly IInterviewViewModelFactory viewModelFactory;
         private readonly IPrincipal principal;
@@ -37,7 +40,7 @@ namespace WB.Core.BoundedContexts.Interviewer.Views.Dashboard
 
             var interviewViews = this.interviewViewRepository.Where(interview =>
                 interview.ResponsibleId == interviewerId &&
-                interview.Status == InterviewStatus.RejectedBySupervisor);
+                interview.Status == SharedKernels.DataCollection.ValueObjects.Interview.InterviewStatus.RejectedBySupervisor);
 
             foreach (var interviewView in interviewViews)
             {
