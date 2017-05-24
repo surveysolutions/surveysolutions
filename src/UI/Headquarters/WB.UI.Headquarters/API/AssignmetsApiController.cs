dde3150c11784730d426ea2855f15f073d651f90
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2017-05-24 13:26:29 +0300

    KP-9302: Added filtering by questionnaire and responsible to assignments list.

---
+++ src/UI/Headquarters/WB.UI.Headquarters/API/AssignmetsApiController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Web.Http;
+using WB.Core.BoundedContexts.Headquarters.Assignments;
+using WB.Core.Infrastructure.PlainStorage;
+using WB.Core.SharedKernels.DataCollection.Implementation.Entities;
+using WB.UI.Headquarters.Code;
+using WB.UI.Headquarters.Models.Api;
+
+namespace WB.UI.Headquarters.API
+{
+    [Authorize(Roles = "Administrator, Headquarter, Supervisor")]
+    [CamelCase]
+    public class AssignmetsApiController : ApiController
+    {
+        private readonly IAssignmentViewFactory assignmentViewFactory;
+
+        public AssignmetsApiController(IAssignmentViewFactory assignmentViewFactory)
+        {
+            this.assignmentViewFactory = assignmentViewFactory;
+        }
+        
+        public IHttpActionResult Get([FromUri]AssignmentsDataTableRequest request)
+        {
+            QuestionnaireIdentity questionnaireIdentity = null;
+            if (!string.IsNullOrEmpty(request.QuestionnaireId))
+            {
+                QuestionnaireIdentity.TryParse(request.QuestionnaireId, out questionnaireIdentity);
+            }
+
+            var input = new AssignmentsInputModel
+            {
+                Page = request.PageIndex,
+                PageSize = request.PageSize,
+                Order = request.GetSortOrder(),
+                SearchBy = request.Search.Value,
+                QuestionnaireId = questionnaireIdentity?.QuestionnaireId,
+                QuestionnaireVersion = questionnaireIdentity?.Version,
+                ResponsibleId = request.ResponsibleId
+            };
+
+            var result = this.assignmentViewFactory.Load(input);
+
+            var response = new AssignmetsDataTableResponse
+            {
+                Draw = request.Draw + 1,
+                RecordsTotal = result.TotalCount,
+                RecordsFiltered = result.TotalCount,
+                Data = result.Items
+            };
+            return this.Ok(response);
+        }
+
+        public class AssignmetsDataTableResponse : DataTableResponse<AssignmentWithoutIdentifingData>
+        {
+        }
+
+        public class AssignmentsDataTableRequest : DataTableRequest
+        {
+            public string QuestionnaireId { get; set; }
+            public Guid? ResponsibleId { get; set; }
+        }
+    }
+}
--- src/UI/Headquarters/WB.UI.Headquarters/Controllers/AssignmentsController.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/Controllers/AssignmentsController.cs
@@ -1,40 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Web.Http;
+﻿using System.Collections.Generic;
 using System.Web.Mvc;
-using Microsoft.Practices.ServiceLocation;
-using WB.Core.BoundedContexts.Headquarters.Assignments;
 using WB.Core.BoundedContexts.Headquarters.Views.Reposts.Views;
 using WB.Core.BoundedContexts.Headquarters.Views.UsersAndQuestionnaires;
 using WB.Core.GenericSubdomains.Portable.Services;
 using WB.Core.Infrastructure.CommandBus;
-using WB.Core.Infrastructure.PlainStorage;
-using WB.Core.SharedKernels.DataCollection.Implementation.Entities;
 using WB.Core.SharedKernels.SurveyManagement.Web.Filters;
 using WB.Core.SharedKernels.SurveyManagement.Web.Models;
-using WB.UI.Headquarters.Code;
 using WB.UI.Headquarters.Filters;
-using WB.UI.Headquarters.Models.Api;
 
 namespace WB.UI.Headquarters.Controllers
 {
-    [System.Web.Mvc.Authorize(Roles = "Administrator, Headquarter, Supervisor, Observer")]
+    [Authorize(Roles = "Administrator, Headquarter, Supervisor, Observer")]
     [LimitsFilter]
     [ActivePage(MenuItem.Assignments)]
     public class AssignmentsController : BaseController
     {
         private readonly IAllUsersAndQuestionnairesFactory usersAndQuestionnairesFactory;
-        private readonly IAssignmentViewFactory assignmentViewFactory;
 
-        public AssignmentsController(ICommandService commandService, 
+        public AssignmentsController(ICommandService commandService,
             ILogger logger,
-            IAllUsersAndQuestionnairesFactory usersAndQuestionnairesFactory,
-            IAssignmentViewFactory assignmentViewFactory)
+            IAllUsersAndQuestionnairesFactory usersAndQuestionnairesFactory)
             : base(commandService, logger)
         {
             this.usersAndQuestionnairesFactory = usersAndQuestionnairesFactory;
-            this.assignmentViewFactory = assignmentViewFactory;
         }
 
         public ActionResult Index()
@@ -45,31 +33,7 @@ namespace WB.UI.Headquarters.Controllers
 
             return View(model);
         }
-
-        public ActionResult List(AssignmentsDataTableRequest request)
-        {
-            var input = new AssignmentsInputModel();
-
-            var result = this.assignmentViewFactory.Load(input);
-
-            var response = new AssignmetsDataTableResponse
-            {
-                Draw = request.Draw + 1,
-                RecordsTotal = result.TotalCount,
-                RecordsFiltered = result.TotalCount,
-                Data = result.Items
-            };
-            return JsonCamelCase(response);
-        }
-
-    public class AssignmetsDataTableResponse : DataTableResponse<AssignmentWithoutIdentifingData>
-    {
     }
-
-    public class AssignmentsDataTableRequest : DataTableRequest
-    {
-    }
-}
 
     public class AssignmentsFilters
     {
