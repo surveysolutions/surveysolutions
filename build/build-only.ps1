7d619c6e4645821b86b1bcad0add7bc72ffd8ffc
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2017-05-25 14:00:25 -0400

    publish 2 apk

--- build/build-only.ps1
+++ build/build-only.ps1
@@ -57,7 +57,7 @@ try {
 			-OutFileName $PackageName `
 			-ExcludeExtra TRUE | %{ if (-not $_) { Exit } }
 	
-	CopyCapi -Project $ProjectHeadquarters -source $PackageName -clean $true
+	CopyCapi -Project $ProjectHeadquarters -source $PackageName -clean TRUE
 	
 	$ExtPackageName = 'WBCapi.Ext.apk'
 		. "$scriptFolder\build-android-package.ps1" `
@@ -71,7 +71,7 @@ try {
 			-OutFileName $ExtPackageName `
 			-ExcludeExtra FALSE | %{ if (-not $_) { Exit } }	
 	
-	CopyCapi -Project $ProjectHeadquarters -source $ExtPackageName -clean $true
+	CopyCapi -Project $ProjectHeadquarters -source $ExtPackageName -clean FALSE
 	
 	BuildWebPackage $ProjectHeadquarters $BuildConfiguration | %{ if (-not $_) { Exit } }
 	
