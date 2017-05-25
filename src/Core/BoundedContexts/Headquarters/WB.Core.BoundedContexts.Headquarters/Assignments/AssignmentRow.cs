1ffa29737f42e596b53a5bbd6ee050f56ec55c5a
Andrii Kozhyn <hedgic@gmail.com>
2017-05-25 11:41:07 +0300

    KP-9302: Added identifying questions column

---
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Assignments/AssignmentRow.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+
+namespace WB.Core.BoundedContexts.Headquarters.Assignments
+{
+    public class AssignmentRow
+    {
+        public DateTime CreatedAtUtc { get; set; }
+        public Guid ResponsibleId { get; set; }
+        public DateTime UpdatedAtUtc { get; set; }
+        public int? Capacity { get; set; }
+        public int InterviewsCount { get; set; }
+        public int Id { get; set; }
+        public string Responsible { get; set; }
+
+        public IEnumerable<string> IdentifyingQuestions { get; set; }
+    }
+}
