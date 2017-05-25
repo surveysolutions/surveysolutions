eb4eb6731e2ae2dcaa8da7e5a5f471723e0b0ede
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2017-05-25 16:51:45 -0400

    fix params for build

--- build/build-android-package.ps1
+++ build/build-android-package.ps1
@@ -6,7 +6,7 @@ param([string]$VersionName,
 [string]$KeystoreAlias,
 [string]$CapiProject,
 [string]$OutFileName,
-[string]$ExcludeExtra)
+[bool]$ExcludeExtra)
 
 if(!$VersionCode){
 	Write-Host "##teamcity[buildProblem description='VersionCode param is not set']"
@@ -93,7 +93,7 @@ function BuildAndroidApp($AndroidProject, $BuildConfiguration, $ExcludeExtension
 	{
 	    Write-Host "##teamcity[message text='Building apk excluding extra']"
 	
-		& (GetPathToMSBuild) $AndroidProject '/t:PackageForAndroid' '/v:m' '/nologo' /p:CodeContractsRunCodeAnalysis=false "/p:Configuration=$BuildConfiguration" /p:DefineConstants='EXCLUDEEXTENSIONS' | Write-Host
+		& (GetPathToMSBuild) $AndroidProject '/t:PackageForAndroid' '/v:m' '/nologo' /p:CodeContractsRunCodeAnalysis=false "/p:Configuration=$BuildConfiguration" /p:DefineConstants=`"EXCLUDEEXTENSIONS`" | Write-Host
 	}
 	else
 	{
