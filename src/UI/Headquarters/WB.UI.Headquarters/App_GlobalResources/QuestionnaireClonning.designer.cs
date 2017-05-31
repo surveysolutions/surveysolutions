44085f77bd9d754e68f5ca2b37495fc3510f4c54
Roman Lytvyn <litvin.roma@gmail.com>
2017-05-31 16:18:38 +0300

    removed "census" from resources KP-9339

--- src/UI/Headquarters/WB.UI.Headquarters/App_GlobalResources/QuestionnaireClonning.designer.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/App_GlobalResources/QuestionnaireClonning.designer.cs
@@ -19,7 +19,7 @@ namespace Resources {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option or rebuild the Visual Studio project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Web.Application.StronglyTypedResourceProxyBuilder", "14.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Web.Application.StronglyTypedResourceProxyBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class QuestionnaireClonning {
@@ -75,24 +75,6 @@ namespace Resources {
         internal static string CloneQuestionnaireTitle {
             get {
                 return ResourceManager.GetString("CloneQuestionnaireTitle", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to in census mode.
-        /// </summary>
-        internal static string inCensusMode {
-            get {
-                return ResourceManager.GetString("inCensusMode", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to in regular mode.
-        /// </summary>
-        internal static string inRegularMode {
-            get {
-                return ResourceManager.GetString("inRegularMode", resourceCulture);
             }
         }
         
