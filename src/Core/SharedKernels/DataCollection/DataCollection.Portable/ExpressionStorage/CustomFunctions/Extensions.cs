fb934e3164abc5c1db66990ddc0b1e8e1ff1babd
Slava Scherbak <slava.scherbak@gmail.com>
2017-05-31 13:31:30 -0400

    Added extensions for int? and int[]. KP-8804

---
+++ src/Core/SharedKernels/DataCollection/DataCollection.Portable/ExpressionStorage/CustomFunctions/Extensions.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+
+namespace WB.Core.SharedKernels.DataCollection.ExpressionStorage.CustomFunctions
+{
+    public static class Extensions
+    {
+        public static bool InRange(this int? value, int? low, int? high)
+        {
+            if (value < low) return false;
+            if (value > high) return false;
+            return true;
+        }
+
+        public static bool InList(this int? value, params int?[] valuesList) => 
+            valuesList.Length != 0 && valuesList.Any(v => v == value);
+
+        public static bool ContainsAll(this int[] multichoice, params int[] valuesList)
+        {
+            if (multichoice == null) return false;
+            if (multichoice.Length == 0) return false;
+
+            if (valuesList == null) return true;
+            if (valuesList.Length == 0) return true;
+
+            return valuesList.All(multichoice.Contains);
+        }
+
+        public static bool ContainsAny(this int[] multichoice, params int[] valuesList)
+        {
+            if (multichoice == null) return false;
+            if (multichoice.Length == 0) return false;
+
+            if (valuesList == null) return true;
+            if (valuesList.Length == 0) return true;
+
+            return valuesList.Any(value => multichoice.Any(multiEmelent => multiEmelent == value));
+        }
+
+        public static bool ContainsOnly(this int[] multichoice, params int[] valuesList) => 
+            multichoice?.Length == valuesList.Length && valuesList.All(multichoice.Contains);
+    }
+}
