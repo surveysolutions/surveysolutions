33369eedc058d5fedeb1080d8df4ca9558110974
Andrii Kozhyn <hedgic@gmail.com>
2017-06-01 10:15:51 +0300

    KP-9363: extract implementation to separate file

---
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Assignments/AssignmetnsDeletionService.cs
@@ -0,0 +1,36 @@
+using NHibernate;
+using Ninject;
+using WB.Core.SharedKernels.DataCollection.Implementation.Entities;
+using WB.Infrastructure.Native.Storage.Postgre;
+
+namespace WB.Core.BoundedContexts.Headquarters.Assignments
+{
+    internal class AssignmetnsDeletionService : IAssignmetnsDeletionService
+    {
+        private readonly ISessionFactory sessionFactory;
+
+        public AssignmetnsDeletionService([Named(PostgresPlainStorageModule.SessionFactoryName)]ISessionFactory sessionFactory)
+        {
+            this.sessionFactory = sessionFactory;
+        }
+
+        public void Delete(QuestionnaireIdentity questionnaireIdentity)
+        {
+            using (var session = this.sessionFactory.OpenSession())
+            {
+                using (var transaction = session.BeginTransaction())
+                {
+                    var queryText = $"DELETE from plainstore.assignments a " +
+                        $"WHERE a.QuestionnaireId = :questionnaireId " +
+                        $"AND a.QuestionnaireVersion =  :questionnaireVersion";
+                    var query = session.CreateSQLQuery(queryText);
+                    query.SetParameter("questionnaireId", questionnaireIdentity.QuestionnaireId);
+                    query.SetParameter("questionnaireVersion", questionnaireIdentity.Version);
+
+                    query.ExecuteUpdate();
+                    transaction.Commit();
+                }
+            }
+        }
+    }
+}
