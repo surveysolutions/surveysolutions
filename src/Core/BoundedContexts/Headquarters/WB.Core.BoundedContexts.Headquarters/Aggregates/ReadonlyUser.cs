8864054c2fce237a1e6e960013fa50eea96ae317
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2017-06-06 12:25:56 +0300

    KP-9302: Added role icon to responsible on assignments page

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Aggregates/ReadonlyUser.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Aggregates/ReadonlyUser.cs
@@ -1,14 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace WB.Core.BoundedContexts.Headquarters.Aggregates
 {
     public class ReadonlyUser
     {
+        public ReadonlyUser()
+        {
+            this.RoleIds = new HashSet<Guid>();
+        }
+
         public virtual Guid Id { get; protected set; }
 
         public virtual string Name { get; protected set; }
 
         public virtual ReadonlyProfile ReadonlyProfile { get; protected set; }
+
+        public virtual ISet<Guid> RoleIds { get; protected set; }
     }
 
     public class ReadonlyProfile
