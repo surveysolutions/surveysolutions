3bdfa9bcc957107adf6128701c47166cc80e6154
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2017-06-06 16:20:05 +0300

    KP-9126: Added test for getting user by email

---
+++ src/Tests/WB.Tests.Unit.Designer/BoundedContexts/Designer/AccountRepositoryTests/when_getting_user_using_email.cs
@@ -0,0 +1,26 @@
+﻿using Moq;
+using NUnit.Framework;
+using WB.Core.BoundedContexts.Designer.Views.Account;
+
+namespace WB.Tests.Unit.Designer.BoundedContexts.Designer.AccountRepositoryTests
+{
+    internal class when_getting_user_using_email : AccountRepositoryTestsContext
+    {
+        [Test]
+        public void should_get_using_existing_email()
+        {
+            var accountViewFactory = new Mock<IAccountViewFactory>();
+            var accountView = Mock.Of<IAccountView>();
+            var userEmail = "a@test.com";
+
+            accountViewFactory
+                .Setup(x => x.Load(It.Is<AccountViewInputModel>(input => input.AccountEmail == userEmail)))
+                .Returns(accountView);
+
+            var repository = CreateAccountRepository(accountViewFactory: accountViewFactory.Object);
+
+            var account = repository.GetByNameOrEmail(userEmail);
+            Assert.That(account, Is.SameAs(accountView));
+        }
+    }
+}
