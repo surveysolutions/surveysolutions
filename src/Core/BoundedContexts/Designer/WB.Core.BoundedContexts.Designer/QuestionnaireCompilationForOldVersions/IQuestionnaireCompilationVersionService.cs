348311db3023db2ec453441b5a21a5fcf7f11fc5
Slava Scherbak <slava.scherbak@gmail.com>
2017-06-07 14:01:49 -0400

    Added page where admin can set compilation version for a questionnaire. KP-9407

---
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/QuestionnaireCompilationForOldVersions/IQuestionnaireCompilationVersionService.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+namespace WB.Core.BoundedContexts.Designer.QuestionnaireCompilationForOldVersions
+{
+    public interface IQuestionnaireCompilationVersionService
+    {
+        IEnumerable<QuestionnaireCompilationVersion> GetCompilationVersions();
+        void Update(QuestionnaireCompilationVersion version);
+        void Remove(Guid questionnaireId);
+        void Add(QuestionnaireCompilationVersion version);
+        QuestionnaireCompilationVersion GetById(int id);
+    }
+}
