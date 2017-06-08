ef9e799d422c61ef94a02b3dbe5a648b6262f5d9
Roman Lytvyn <litvin.roma@gmail.com>
2017-06-08 17:05:48 +0300

    IN: manual merge dashboard items. KP-9408

--- src/UI/Interviewer/WB.UI.Interviewer/Activities/DashboardQuestionnairesFragment.cs
+++ src/UI/Interviewer/WB.UI.Interviewer/Activities/DashboardQuestionnairesFragment.cs
@@ -1,12 +1,10 @@
 using Android.Runtime;
 using WB.Core.BoundedContexts.Interviewer.Views.Dashboard;
-using WB.UI.Shared.Enumerator.Activities;
 
 namespace WB.UI.Interviewer.Activities
 {
     [Register("wb.ui.interviewer.activities.DashboardQuestionnairesFragment")]
-    public class DashboardQuestionnairesFragment : BaseFragment<CreateNewViewModel>
+    public class DashboardQuestionnairesFragment : RecyclerViewFragment<CreateNewViewModel>
     {
-        protected override int ViewResourceId => Resource.Layout.fragment_dashboard_tab;
     }
 }
