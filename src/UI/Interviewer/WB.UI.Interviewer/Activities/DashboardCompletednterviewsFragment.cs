8472c549366573ab621101678438abb5555d9cad
Roman Lytvyn <litvin.roma@gmail.com>
2017-06-08 17:05:48 +0300

    IN: manual merge dashboard items. KP-9408

--- src/UI/Interviewer/WB.UI.Interviewer/Activities/DashboardCompletednterviewsFragment.cs
+++ src/UI/Interviewer/WB.UI.Interviewer/Activities/DashboardCompletednterviewsFragment.cs
@@ -1,12 +1,11 @@
 using Android.Runtime;
 using WB.Core.BoundedContexts.Interviewer.Views.Dashboard;
-using WB.UI.Shared.Enumerator.Activities;
 
 namespace WB.UI.Interviewer.Activities
 {
     [Register("wb.ui.interviewer.activities.DashboardCompletednterviewsFragment")]
-    public class DashboardCompletednterviewsFragment : BaseFragment<CompletedInterviewsViewModel>
+    public class DashboardCompletednterviewsFragment : RecyclerViewFragment<CompletedInterviewsViewModel>
     {
-        protected override int ViewResourceId => Resource.Layout.fragment_dashboard_tab;
+        
     }
 }
---
+++ src/UI/Interviewer/WB.UI.Interviewer/Activities/RecyclerViewFragment.cs
@@ -0,0 +1,27 @@
+﻿using Android.OS;
+using Android.Views;
+using MvvmCross.Binding.Droid.BindingContext;
+using MvvmCross.Droid.Support.V7.RecyclerView;
+using WB.Core.BoundedContexts.Interviewer.Views.Dashboard;
+using WB.UI.Shared.Enumerator.Activities;
+
+namespace WB.UI.Interviewer.Activities
+{
+    public abstract class RecyclerViewFragment<TViewModel> : BaseFragment<ListViewModel<TViewModel>> where TViewModel : class
+    {
+        protected override int ViewResourceId => Resource.Layout.fragment_dashboard_tab;
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            base.OnCreateView(inflater, container, savedInstanceState);
+
+            var view = this.BindingInflate(this.ViewResourceId, null);
+
+            var recyclerView = view.FindViewById<MvxRecyclerView>(Resource.Id.dashboard_tab_recycler);
+            if (recyclerView != null)
+                recyclerView.Adapter = new DashboardRecyclerViewAdapter(recyclerView, (IMvxAndroidBindingContext)base.BindingContext);
+
+            return view;
+        }
+    }
+}
