ae7e5e1d781283c40b99a6c806352ad7690c3cc5
Slava Scherbak <slava.scherbak@gmail.com>
2017-06-08 12:53:37 -0400

    KP-9415: Added roster collection to override access to roster by its rowcode.

--- src/Core/SharedKernels/DataCollection/DataCollection.Portable/ExpressionStorage/IInterviewLevel.cs
+++ src/Core/SharedKernels/DataCollection/DataCollection.Portable/ExpressionStorage/IInterviewLevel.cs
@@ -4,6 +4,9 @@ namespace WB.Core.SharedKernels.DataCollection.ExpressionStorage
 {
     public interface IInterviewLevel
     {
+        int RowCode { get; }
+        int RowIndex { get; }
+
         Func<bool> GetConditionExpression(Identity entitIdentity);
         Func<bool>[] GetValidationExpressions(Identity entitIdentity);
         Func<IInterviewLevel, bool> GetLinkedQuestionFilter(Identity identity);
