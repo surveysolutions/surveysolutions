5aebd9b09ad8f63135486a8373ecc523171ffe0a
Roman Lytvyn <litvin.roma@gmail.com>
2017-06-08 17:05:48 +0300

    IN: manual merge dashboard items. KP-9408

--- src/UI/Interviewer/WB.UI.Interviewer/Activities/DashboardRejectednterviewsFragment.cs
+++ src/UI/Interviewer/WB.UI.Interviewer/Activities/DashboardRejectednterviewsFragment.cs
@@ -1,12 +1,10 @@
 using Android.Runtime;
 using WB.Core.BoundedContexts.Interviewer.Views.Dashboard;
-using WB.UI.Shared.Enumerator.Activities;
 
 namespace WB.UI.Interviewer.Activities
 {
     [Register("wb.ui.interviewer.activities.DashboardRejectednterviewsFragment")]
-    public class DashboardRejectednterviewsFragment : BaseFragment<RejectedInterviewsViewModel>
+    public class DashboardRejectednterviewsFragment : RecyclerViewFragment<RejectedInterviewsViewModel>
     {
-        protected override int ViewResourceId => Resource.Layout.fragment_dashboard_tab;
     }
 }
--- src/UI/Interviewer/WB.UI.Interviewer/Activities/DashboardStartedInterviewsFragment.cs
+++ src/UI/Interviewer/WB.UI.Interviewer/Activities/DashboardStartedInterviewsFragment.cs
@@ -1,12 +1,10 @@
 using Android.Runtime;
 using WB.Core.BoundedContexts.Interviewer.Views.Dashboard;
-using WB.UI.Shared.Enumerator.Activities;
 
 namespace WB.UI.Interviewer.Activities
 {
     [Register("wb.ui.interviewer.activities.DashboardStartedInterviewsFragment")]
-    public class DashboardStartedInterviewsFragment : BaseFragment<StartedInterviewsViewModel>
+    public class DashboardStartedInterviewsFragment : RecyclerViewFragment<StartedInterviewsViewModel>
     {
-        protected override int ViewResourceId => Resource.Layout.fragment_dashboard_tab;
     }
 }
