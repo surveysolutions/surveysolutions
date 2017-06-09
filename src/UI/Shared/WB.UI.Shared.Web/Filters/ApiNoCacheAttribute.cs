aad3eb72f0e737ed2465313fbf8fb56bb2495eef
Andrii Kozhyn <hedgic@gmail.com>
2017-06-09 15:58:13 +0300

    KP-9409: Disabled cache for responsible list

---
+++ src/UI/Shared/WB.UI.Shared.Web/Filters/ApiNoCacheAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Http.Headers;
+using System.Web.Http.Filters;
+
+namespace WB.UI.Shared.Web.Filters
+{
+    public class ApiNoCacheAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuted(HttpActionExecutedContext filterContext)
+        {
+            base.OnActionExecuted(filterContext);
+            filterContext.Response.Headers.CacheControl = new CacheControlHeaderValue
+            {
+                NoCache = true
+            };
+        }
+    }
+}
