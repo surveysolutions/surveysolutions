b8719c342c6b88ba5ae78c2648cbcd0f150efa17
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2017-06-12 13:56:02 -0400

    KP-9371 Download page to select one of versions

---
+++ src/UI/Headquarters/WB.UI.Headquarters/Controllers/DownloadController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace WB.UI.Headquarters.Controllers
+{
+    public class DownloadController : Controller
+    {
+        // GET: Download
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
