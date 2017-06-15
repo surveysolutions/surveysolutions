e346db74f370c3a5c8e473dacaa8ed7ddef5c4ef
Pavel Stovpivskyi <sby@rambler.ru>
2017-06-15 15:36:57 +0300

    KP-9398 Reload Questionnaire: message appears in case of no changes were made

---
+++ src/Tests/WB.Tests.Unit/SharedKernels/DataCollection/RosterVectorTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using NUnit.Framework;
+using WB.Core.SharedKernels.DataCollection;
+using WB.Tests.Abc;
+
+namespace WB.Tests.Unit.SharedKernels.DataCollection
+{
+    [TestOf(typeof(RosterVector))]
+    public class RosterVectorTests
+    {
+        [Test]
+        public void ConvertToArray_should_convert_collections_to_roster_vector_array_from_readonly_collection()
+        {
+            //arrange
+            var list = new List<RosterVector>()
+            {
+                Create.Entity.RosterVector(2),
+                Create.Entity.RosterVector(5)
+            };
+            var readonlyCollection = new ReadOnlyCollection<RosterVector>(list);
+
+            //act
+            var rosterVectors = RosterVector.ConvertToArray(readonlyCollection);
+
+            //assert
+            Assert.That(rosterVectors.Length, Is.EqualTo(2));
+            Assert.That(rosterVectors, Is.EqualTo(new[] { Create.Entity.RosterVector(2), Create.Entity.RosterVector(5) }));
+        }
+
+    }
+}
