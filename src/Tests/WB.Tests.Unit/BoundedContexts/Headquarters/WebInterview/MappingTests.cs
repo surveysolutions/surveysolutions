143410d692ba264b1b1864e313ed5506d79d454e
Victor Hladkikh <me@alfeg.net>
2017-06-15 15:54:24 +0300

    KP-9467: Broken web interview. Missing mapping for TextList answers

---
+++ src/Tests/WB.Tests.Unit/BoundedContexts/Headquarters/WebInterview/MappingTests.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using Main.Core.Entities.SubEntities;
+using NUnit.Framework;
+using WB.Core.SharedKernels.DataCollection;
+using WB.Core.SharedKernels.DataCollection.Implementation.Aggregates.InterviewEntities.Answers;
+using WB.Tests.Abc;
+using WB.UI.Headquarters.Models.WebInterview;
+
+namespace WB.Tests.Unit.BoundedContexts.Headquarters.WebInterview
+{
+    public class MappingToDtoTests
+    {
+        private IMapper mapper;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            var autoMapperConfig = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new WebInterviewAutoMapProfile());
+            });
+
+            mapper = autoMapperConfig.CreateMapper();
+        }
+
+        [Test]
+        public void should_be_able_to_map_TextList_questions()
+        {
+            var question = Create.Entity.InterviewTreeQuestion(Create.Entity.Identity(), questionType: QuestionType.TextList);
+            question.SetAnswer(TextListAnswer.FromTextListAnswerRows(new []
+            {
+                new TextListAnswerRow(0, "test"),
+                new TextListAnswerRow(1, "test")
+            }));
+
+            var dto = this.mapper.Map<InterviewTextListQuestion>(question);
+
+            Assert.That(dto.Rows, Has.Count.EqualTo(2));
+            Assert.That(dto.Rows[0].Text, Is.EqualTo("test"));
+            Assert.That(dto.Rows[1].Value, Is.EqualTo(1).Within(0.1m));
+        }
+    }
+}
