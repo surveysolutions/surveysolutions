786e27583e13f2eeadac4dab0fdad08adbc5ae19
Andrii Kozhyn <hedgic@gmail.com>
2017-06-16 16:23:01 +0300

    KP-9472: Fixed double event subscription for same card after synchronization

--- src/UI/Interviewer/WB.UI.Interviewer/Activities/Dashboard/ExpandableViewHolder.cs
+++ src/UI/Interviewer/WB.UI.Interviewer/Activities/Dashboard/ExpandableViewHolder.cs
@@ -1,3 +1,4 @@
+using System;
 using Android.Views;
 using MvvmCross.Binding.Droid.BindingContext;
 using MvvmCross.Droid.Support.V7.RecyclerView;
@@ -12,11 +13,22 @@ namespace WB.UI.Interviewer.Activities.Dashboard
 
         public View ExpandHandle { get; }
         public ExpandableViewHolder(View itemView, IMvxAndroidBindingContext context) : base(itemView, context)
-
         {
             this.DetailsView = itemView.FindViewById<View>(Resource.Id.dashboardItemDetails);
             this.DashboardItem = itemView.FindViewById<View>(Resource.Id.dashboardItem);
             this.ExpandHandle = itemView.FindViewById<View>(Resource.Id.expandHandle);
+
+            if (this.DashboardItem != null)
+            {
+                this.DashboardItem.Click += (sender, args) => this.OnCardClick();
+            }
+        }
+
+        public event EventHandler CardClick;
+
+        protected virtual void OnCardClick()
+        {
+            this.CardClick?.Invoke(this, EventArgs.Empty);
         }
     }
 }
