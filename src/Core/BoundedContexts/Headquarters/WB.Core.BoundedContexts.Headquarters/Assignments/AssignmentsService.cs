d62aad08a5db9caf8d69eb27d902d57dac414b0e
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2017-06-20 12:28:42 +0300

    KP-9479: Extracted logic into assignments service

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Assignments/AssignmentsService.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Assignments/AssignmentsService.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using WB.Core.Infrastructure.PlainStorage;
+using WB.Core.SharedKernels.DataCollection.Implementation.Entities;
+using WB.Infrastructure.Native.Fetching;
 
 namespace WB.Core.BoundedContexts.Headquarters.Assignments
 {
@@ -27,6 +29,28 @@ namespace WB.Core.BoundedContexts.Headquarters.Assignments
         public Assignment GetAssignment(int id)
         {
             return this.assignmentsAccessor.GetById(id);
+        }
+
+        public List<Assignment> GetAssignmentsReadyForWebInterview(QuestionnaireIdentity questionnaireId)
+        {
+            var assignmentsReadyForWebInterview = this.assignmentsAccessor.Query(_ => _
+                .Where(x => x.QuestionnaireId.QuestionnaireId == questionnaireId.QuestionnaireId &&
+                    x.QuestionnaireId.Version == questionnaireId.Version &&
+                    x.Responsible.ReadonlyProfile.SupervisorId != null &&
+                    !x.Archived)
+                .OrderBy(x => x.Id)
+                .Fetch(x => x.IdentifyingData)
+                .ToList());
+            return assignmentsReadyForWebInterview;
+        }
+
+        public int GetCountOfAssignmentsReadyForWebInterview(QuestionnaireIdentity questionnaireId)
+        {
+            return this.assignmentsAccessor.Query(_ => _.Count(
+                x => x.QuestionnaireId.QuestionnaireId == questionnaireId.QuestionnaireId &&
+                    x.QuestionnaireId.Version == questionnaireId.Version &&
+                    x.Responsible.ReadonlyProfile.SupervisorId != null &&
+                    !x.Archived));
         }
     }
 }
--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Assignments/IAssignmentsService.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Assignments/IAssignmentsService.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using WB.Core.SharedKernels.DataCollection;
+using WB.Core.SharedKernels.DataCollection.Implementation.Entities;
 
 namespace WB.Core.BoundedContexts.Headquarters.Assignments
 {
@@ -7,5 +9,8 @@ namespace WB.Core.BoundedContexts.Headquarters.Assignments
     {
         List<Assignment> GetAssignments(Guid responsibleId);
         Assignment GetAssignment(int id);
+
+        List<Assignment> GetAssignmentsReadyForWebInterview(QuestionnaireIdentity questionnaireId);
+        int GetCountOfAssignmentsReadyForWebInterview(QuestionnaireIdentity questionnaireId);
     }
 }
