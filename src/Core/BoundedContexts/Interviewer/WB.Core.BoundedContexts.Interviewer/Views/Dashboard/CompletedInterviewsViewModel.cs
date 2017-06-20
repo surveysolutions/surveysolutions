ef91ed1def69702c509feabd8b922e4eb6fe1326
Victor Gladkikh <me@alfeg.net>
2017-06-20 16:19:57 +0300

    KP-9483: Black screen displays for few secs when navigating to Dashboard having many items (~560)

--- src/Core/BoundedContexts/Interviewer/WB.Core.BoundedContexts.Interviewer/Views/Dashboard/CompletedInterviewsViewModel.cs
+++ src/Core/BoundedContexts/Interviewer/WB.Core.BoundedContexts.Interviewer/Views/Dashboard/CompletedInterviewsViewModel.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using WB.Core.BoundedContexts.Interviewer.Properties;
 using WB.Core.BoundedContexts.Interviewer.Views.Dashboard.DashboardItems;
 using WB.Core.GenericSubdomains.Portable;
@@ -33,13 +34,23 @@ namespace WB.Core.BoundedContexts.Interviewer.Views.Dashboard
 
         public void Load()
         {
-            this.Items = this.GetCompletedInterviews().ToList();
+            
+            Task.Run(() =>
+            {
+                var items = this.GetCompletedInterviews().ToList();
+                var subTitle = this.viewModelFactory.GetNew<DashboardSubTitleViewModel>();
+                subTitle.Title = InterviewerUIResources.Dashboard_CompletedTabText;
+                var uiItems = subTitle.ToEnumerable().Concat(items).ToList();
+                return Tuple.Create(items, uiItems);
+            }).ContinueWith(task =>
+            {
+                this.Items = task.Result.Item1;
+                this.UiItems = task.Result.Item2;
 
-            var subTitle = this.viewModelFactory.GetNew<DashboardSubTitleViewModel>();
-            subTitle.Title = InterviewerUIResources.Dashboard_CompletedTabText;
-            this.UiItems = subTitle.ToEnumerable().Concat(this.Items).ToList();
-
-            this.Title = string.Format(InterviewerUIResources.Dashboard_CompletedLinkText, this.Items.Count);
+                this.Title = string.Format(InterviewerUIResources.Dashboard_CompletedLinkText, this.Items.Count);
+            }, TaskScheduler.FromCurrentSynchronizationContext());
+            
+            this.Title = string.Format(InterviewerUIResources.Dashboard_CompletedLinkText, 0);
         }
 
         private IEnumerable<IDashboardItem> GetCompletedInterviews()
