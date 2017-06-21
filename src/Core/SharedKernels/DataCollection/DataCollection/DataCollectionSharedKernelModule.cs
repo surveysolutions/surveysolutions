ec034abf6d49dd5eabfc928010d418e755735321
Andrii Kozhyn <hedgic@gmail.com>
2017-06-21 18:27:44 +0300

    KP-9325: restored internals visibility

--- src/Core/SharedKernels/DataCollection/DataCollection/DataCollectionSharedKernelModule.cs
+++ src/Core/SharedKernels/DataCollection/DataCollection/DataCollectionSharedKernelModule.cs
@@ -1,6 +1,12 @@
+using System.Runtime.CompilerServices;
 using WB.Core.Infrastructure.Modularity;
 using WB.Core.SharedKernels.DataCollection.Implementation.Repositories;
 using WB.Core.SharedKernels.DataCollection.Repositories;
+
+[assembly: InternalsVisibleTo("WB.Tests.Unit")]
+[assembly: InternalsVisibleTo("WB.Tests.Integration")]
+[assembly: InternalsVisibleTo("PerformanceTest")]
+[assembly: InternalsVisibleTo("WB.Tests.Abc")]
 
 namespace WB.Core.SharedKernels.DataCollection
 {
