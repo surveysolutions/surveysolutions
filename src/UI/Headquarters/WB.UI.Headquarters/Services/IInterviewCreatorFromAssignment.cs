794ab8dc49c194a9f35c8363327b426464125ae9
Victor Gladkikh <me@alfeg.net>
2017-07-05 16:40:08 +0300

    KP-9576: Cannot create assignment from API

--- src/UI/Headquarters/WB.UI.Headquarters/Services/IInterviewCreatorFromAssignment.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/Services/IInterviewCreatorFromAssignment.cs
@@ -1,16 +1,16 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
+using WB.Core.BoundedContexts.Headquarters.Assignments;
 using WB.Core.BoundedContexts.Headquarters.Views.User;
 using WB.Core.SharedKernels.DataCollection.Implementation.Aggregates.InterviewEntities.Answers;
 using WB.Core.SharedKernels.DataCollection.Implementation.Entities;
-using WB.UI.Headquarters.API.PublicApi.Models;
-using WB.UI.Headquarters.Code.CommandTransformation;
 
 namespace WB.UI.Headquarters.Services
 {
     public interface IInterviewCreatorFromAssignment
     {
-        void CreateInterviewIfQuestionnaireIsOld(HqUser responsible, QuestionnaireIdentity questionnaireIdentity, int assignmentId, List<AssignmentIdentifyingDataItem> identifyingData);
+        void CreateInterviewIfQuestionnaireIsOld(HqUser responsible, QuestionnaireIdentity questionnaireIdentity, int assignmentId, IList<IdentifyingAnswer> identifyingData);
         void CreateInterviewIfQuestionnaireIsOld(Guid responsibleId, QuestionnaireIdentity questionnaireIdentity, int assignmentId, List<InterviewAnswer> interviewAnswers);
     }
 }
