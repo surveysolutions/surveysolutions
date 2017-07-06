d0a281dcbfc195fe57d9a877d7901cc5d03fa1a1
Roman Lytvyn <litvin.roma@gmail.com>
2017-07-06 11:44:37 +0300

    IN: fixed code after manual merge. KP-9508

--- src/Core/BoundedContexts/Interviewer/WB.Core.BoundedContexts.Interviewer/Views/Dashboard/DashboardItems/ExpandableQuestionsDashboardItemViewModel.cs
+++ src/Core/BoundedContexts/Interviewer/WB.Core.BoundedContexts.Interviewer/Views/Dashboard/DashboardItems/ExpandableQuestionsDashboardItemViewModel.cs
@@ -5,16 +5,24 @@ namespace WB.Core.BoundedContexts.Interviewer.Views.Dashboard.DashboardItems
 {
     public class ExpandableQuestionsDashboardItemViewModel : MvxNotifyPropertyChanged, IDashboardItem
     {
-        public bool HasExpandedView { get; set; }
-        private bool isExpanded = false;
 
+        public bool HasExpandedView { get; protected set; }
+
+        private bool isExpanded = true;
         public bool IsExpanded
         {
             get => this.isExpanded;
-            set => this.RaiseAndSetIfChanged(ref this.isExpanded, value, alsoNotify: nameof(PrefilledQuestions));
+            set
+            {
+                if (this.isExpanded == value) return;
+
+                this.isExpanded = value;
+                this.RaisePropertyChanged();
+                this.RaisePropertyChanged(() => this.PrefilledQuestions);
+            }
         }
 
-        public List<PrefilledQuestion> PrefilledQuestions =>
+        public List<PrefilledQuestion> PrefilledQuestions  =>
             this.IsExpanded ? this.DetailedIdentifyingData : this.IdentifyingData;
         
         protected List<PrefilledQuestion> IdentifyingData;
