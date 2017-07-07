4d38a7fe995bc453e5a5fe71dea608effcb3111c
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2017-07-07 16:48:23 +0300

    KP-9578: Assignment should not count deleted interviews
    # Conflicts:
    #	src/Tests/WB.Tests.Abc/TestFactories/EntityFactory.cs
    #	src/Tests/WB.Tests.Unit/WB.Tests.Unit.csproj
    #	src/UI/Headquarters/WB.UI.Headquarters/Controllers/WebInterviewController.cs

---
+++ src/Tests/WB.Tests.Unit/BoundedContexts/Headquarters/Assignments/AssignmentTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using WB.Core.BoundedContexts.Headquarters.Assignments;
+using WB.Tests.Abc;
+
+namespace WB.Tests.Unit.BoundedContexts.Headquarters.Assignments
+{
+    [TestOf(typeof(Assignment))]
+    public class AssignmentTests
+    {
+        [Test]
+        public void should_calculate_interviews_needed_quantity_without_deleted_interviews()
+        {
+            var assignment = Create.Entity.Assignment(quantity: 2);
+
+            assignment.InterviewSummaries.Add(Create.Entity.InterviewSummary(isDeleted: true));
+            assignment.InterviewSummaries.Add(Create.Entity.InterviewSummary(isDeleted: false));
+
+            Assert.That(assignment.InterviewsNeeded, Is.EqualTo(1));
+            Assert.That(assignment.IsCompleted, Is.False);
+        }
+
+        [Test]
+        public void should_not_complete_assignment_when_it_is_unlimited()
+        {
+            var assignment = Create.Entity.Assignment(quantity: null);
+
+            assignment.InterviewSummaries.Add(Create.Entity.InterviewSummary(isDeleted: true));
+            assignment.InterviewSummaries.Add(Create.Entity.InterviewSummary(isDeleted: false));
+
+            Assert.That(assignment.IsCompleted, Is.False);
+            Assert.That(assignment.InterviewsNeeded, Is.Null);
+
+        }
+    }
+}
