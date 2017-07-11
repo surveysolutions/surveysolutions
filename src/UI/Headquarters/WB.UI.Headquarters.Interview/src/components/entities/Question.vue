36dade74ee972fd823969f1e85d59a815ec9c41f
Pavel Stovpivskyi <630198+spuv@users.noreply.github.com>
2017-07-11 19:04:36 +0300

    KP-9610 show comments for question

--- src/UI/Headquarters/WB.UI.Headquarters.Interview/src/components/entities/Question.vue
+++ src/UI/Headquarters/WB.UI.Headquarters.Interview/src/components/entities/Question.vue
@@ -1,12 +1,22 @@
 <template>
     <div class="question" v-if="isEnabled" :class="questionClass" :id="hash">
         <button class="section-blocker" disabled="disabled" v-if="isFetchInProgress"></button>
+        <div class="dropdown aside-menu" v-if="!noComments">
+            <button type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="btn btn-link">
+                <span></span>
+            </button>
+            <ul class="dropdown-menu">
+                <li v-if="!isShowingAddCommentDialog"><a href="javascript:void(0)" @click="showAddComment">Write comment</a></li>
+                <li v-if="isShowingAddCommentDialog"><a href="javascript:void(0)" @click="hideAddComment">Hide comment</a></li>
+            </ul>
+        </div>
 
         <div class="question-editor" :class="questionEditorClass">
             <wb-title v-if="!noTitle" />
             <wb-instructions v-if="!noInstructions" />
             <slot />
             <wb-validation v-if="!noValidation" />
+            <wb-comments v-if="!noComments" :isShowingAddCommentDialog="isShowingAddCommentDialog" />
         </div>
         <wb-progress :visible="isFetchInProgress" :valuenow="valuenow" :valuemax="valuemax" />
     </div>
@@ -16,7 +26,12 @@
 
     export default {
         name: 'wb-question',
-        props: ["question", 'questionCssClassName', 'noTitle', 'noInstructions', 'noValidation', 'noAnswer'],
+        props: ["question", 'questionCssClassName', 'noTitle', 'noInstructions', 'noValidation', 'noAnswer', 'noComments'],
+        data() {
+            return {
+                isShowingAddCommentDialog: false
+            }
+        },
         computed: {
             id() {
                 return this.question.id
@@ -50,6 +65,14 @@
                     answered: this.question.isAnswered && !this.noAnswer,
                     'has-error': !this.question.validity.isValid
                 }, this.questionCssClassName]
+            }
+        },
+        methods : {
+            showAddComment(){
+                this.isShowingAddCommentDialog = true;
+            },
+            hideAddComment(){
+                this.isShowingAddCommentDialog = false;
             }
         }
     }
--- src/UI/Headquarters/WB.UI.Headquarters.Interview/src/components/entities/parts/Index.ts
+++ src/UI/Headquarters/WB.UI.Headquarters.Interview/src/components/entities/parts/Index.ts
@@ -1,11 +1,13 @@
 import Vue from "vue"
 
+import Comments from "./Comments"
 import Instructions from "./Instructions"
 import Progress from "./Progress"
 import RemoveAnswer from "./RemoveAnswer"
 import Title from "./Title"
 import Validation from "./Validation"
 
+Vue.component("wb-comments", Comments)
 Vue.component("wb-instructions", Instructions)
 Vue.component("wb-title", Title)
 Vue.component("wb-validation", Validation)
