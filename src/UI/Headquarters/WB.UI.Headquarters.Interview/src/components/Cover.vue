ae1462fc08b3405309a9b506af69132eb395ffd7
Slava Scherbak <slava.scherbak@gmail.com>
2017-07-13 15:58:46 +0300

    KP-9609: added comments on cover page for web-interview

--- src/UI/Headquarters/WB.UI.Headquarters.Interview/src/components/Cover.vue
+++ src/UI/Headquarters/WB.UI.Headquarters.Interview/src/components/Cover.vue
@@ -18,6 +18,25 @@
             </div>
         </div>
 
+        <div class="wrapper-info" v-if="hasSupervisorComment">
+            <div class="container-info">
+                <h4 class="gray-uppercase">Supervisor note:</h4>
+                <p><b>{{supervisorComment}}</b></p>
+            </div>
+        </div>
+
+        <div class="wrapper-info" v-if="commentedQuestions.length > 0">
+            <div class="container-info">
+                <h4 class="gray-uppercase">{{hasMoreQuestionsWithComments ? 'First ' + commentedQuestions.length + ' entities with comments:' : 'Questions with comments:'}}</h4>
+                <ul class="list-unstyled marked-questions">
+                    <li v-for="commentedQuestion in commentedQuestions">
+                        <a href="#" @click="navigateTo(commentedQuestion)">{{ commentedQuestion.title }}</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+
         <template v-for="question in questions">
             <div class="wrapper-info" v-if="question.isReadonly">
                 <div class="container-info" :id="question.identity">
@@ -42,6 +61,8 @@
 </template>
 
 <script lang="ts">
+    import * as isEmpty from "lodash/isempty"
+
     export default {
         name: "cover-readonly-view",
         beforeMount() {
@@ -52,18 +73,47 @@
                return this.$store.state.questionnaireTitle
            },
            questions() {
-               return this.$store.state.samplePrefilledInfo.questions
+               return this.$store.state.coverInfo.identifyingQuestions
+           },
+           commentedQuestions() {
+               return this.$store.state.coverInfo.entitiesWithComments
            },
            firstSectionId() {
                return this.$store.state.firstSectionId
-           }
+           },
+           hasMoreQuestionsWithComments() {
+                return this.$store.state.coverInfo.entitiesWithComments.length < this.$store.state.coverInfo.commentedQuestionsCount
+            },
+            supervisorComment(){
+                return this.$store.state.coverInfo.supervisorRejectComment
+            },
+            hasSupervisorComment(){
+                return !isEmpty(this.$store.state.coverInfo.supervisorRejectComment)
+            }
         },
         methods: {
             fetch() {
-                this.$store.dispatch("fetchSamplePrefilled")
+                this.$store.dispatch("fetchCoverInfo")
             },
             getGpsUrl(question: IReadonlyPrefilledQuestion) {
                 return `http://maps.google.com/maps?q=${question.answer}`
+            },
+            navigateTo(commentedQuestion) {
+                if(commentedQuestion.isPrefilled){
+                    this.$router.push({ name: "prefilled" })
+                    return;
+                }
+
+                const navigateToEntity = {
+                    name: 'section',
+                    params: {
+                        sectionId: commentedQuestion.parentId,
+                        interviewId: this.$route.params.interviewId
+                    }
+                }
+
+                this.$store.dispatch("sectionRequireScroll", { id: commentedQuestion.id })
+                this.$router.push(navigateToEntity)
             }
         }
     }
