4d54765df22d1d0dc1bd951bcb49c5ff6853e49a
Victor Hladkikh <me@alfeg.net>
2017-07-14 14:20:57 +0300

    KP-9593 HQ: Interviews list for interviewer # Fixed production build

---
+++ src/UI/Headquarters/WB.UI.Headquarters/Dependencies/app/components/FilterBlock.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="block-filter">
+        <h5>{{title}}</h5>
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    props: [
+        "title"
+    ]
+}
+</script>
