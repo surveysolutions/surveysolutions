2b8fd310aae1025b8012563bc650fd08aa13952e
Pavel Stovpivskyi <sby@rambler.ru>
2017-07-17 16:41:57 +0300

    KP-9583 maked base classes for audio and images storages

--- src/Core/BoundedContexts/Interviewer/WB.Core.BoundedContexts.Interviewer/Implementation/Services/InterviewerAudioFileStorage.cs
+++ src/Core/BoundedContexts/Interviewer/WB.Core.BoundedContexts.Interviewer/Implementation/Services/InterviewerAudioFileStorage.cs
@@ -1,87 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using WB.Core.BoundedContexts.Interviewer.Views;
-using WB.Core.GenericSubdomains.Portable;
+﻿using WB.Core.BoundedContexts.Interviewer.Views;
 using WB.Core.SharedKernels.DataCollection.Repositories;
-using WB.Core.SharedKernels.DataCollection.Views.BinaryData;
 using WB.Core.SharedKernels.Enumerator.Services.Infrastructure.Storage;
 
 
 namespace WB.Core.BoundedContexts.Interviewer.Implementation.Services
 {
-    public class InterviewerAudioFileStorage : IAudioFileStorage
+    public class InterviewerAudioFileStorage : InterviewerFileStorage<AudioFileMetadataView, AudioFileView>, IAudioFileStorage
     {
-        private readonly IPlainStorage<AudioFileMetadataView> audioFileMetadataViewStorage;
-        private readonly IPlainStorage<AudioFileView> audioFileViewStorage;
-
         public InterviewerAudioFileStorage(
-            IPlainStorage<AudioFileMetadataView> audioFileMetadataViewStorage,
-            IPlainStorage<AudioFileView> audioFileViewStorage)
+            IPlainStorage<AudioFileMetadataView> fileMetadataViewStorage,
+            IPlainStorage<AudioFileView> fileViewStorage)
+            :base(fileMetadataViewStorage, fileViewStorage)
         {
-            this.audioFileMetadataViewStorage = audioFileMetadataViewStorage;
-            this.audioFileViewStorage = audioFileViewStorage;
-        }
-
-        public byte[] GetInterviewBinaryData(Guid interviewId, string fileName)
-        {
-            var imageView =
-                this.audioFileMetadataViewStorage.Where(image => image.InterviewId == interviewId && image.FileName == fileName)
-                    .SingleOrDefault();
-
-            if (imageView == null) return null;
-
-            var fileView = this.audioFileViewStorage.GetById(imageView.FileId);
-
-            return fileView?.Content;
-        }
-
-        public List<InterviewBinaryDataDescriptor> GetBinaryFilesForInterview(Guid interviewId)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void StoreInterviewBinaryData(Guid interviewId, string fileName, byte[] data)
-        {
-            var imageView =
-                this.audioFileMetadataViewStorage.Where(image => image.InterviewId == interviewId && image.FileName == fileName)
-                    .SingleOrDefault();
-
-            if (imageView == null)
-            {
-                string fileId = Guid.NewGuid().FormatGuid();
-                this.audioFileViewStorage.Store(new AudioFileView
-                {
-                    Id = fileId,
-                    Content = data
-                });
-
-                this.audioFileMetadataViewStorage.Store(new AudioFileMetadataView
-                {
-                    Id = Guid.NewGuid().FormatGuid(),
-                    InterviewId = interviewId,
-                    FileId = fileId,
-                    FileName = fileName
-                });
-            }
-            else
-            {
-                this.audioFileViewStorage.Store(new AudioFileView
-                {
-                    Id = imageView.FileId,
-                    Content = data
-                });
-            }
-        }
-
-        public void RemoveInterviewBinaryData(Guid interviewId, string fileName)
-        {
-            var imageView = this.audioFileMetadataViewStorage.Where(image => image.InterviewId == interviewId && image.FileName == fileName).SingleOrDefault();
-
-            if (imageView == null) return;
-
-            this.audioFileViewStorage.Remove(imageView.FileId);
-            this.audioFileMetadataViewStorage.Remove(imageView.Id);
         }
     }
 }
