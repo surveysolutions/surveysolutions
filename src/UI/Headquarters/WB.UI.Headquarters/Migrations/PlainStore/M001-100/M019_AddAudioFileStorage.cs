b0c851960e54696c7df2cc32953795cacaf931e3
Pavel Stovpivskyi <sby@rambler.ru>
2017-07-17 14:32:25 +0300

    KP-9583 Renamed migration

--- src/UI/Headquarters/WB.UI.Headquarters/Migrations/PlainStore/M001-100/M019_AddAudioFileStorage.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/Migrations/PlainStore/M001-100/M019_AddAudioFileStorage.cs
@@ -4,23 +4,25 @@ using FluentMigrator;
 namespace WB.UI.Headquarters.Migrations.PlainStore
 {
     [Migration(19), Localizable(false)]
-    public class M019_AddDatabaseFileStorage : Migration
+    public class M019_AddAudioFileStorage : Migration
     {
         public override void Up()
         {
-            this.Create.Table("databasefiles")
+            this.Create.Table("audiofiles")
                 .WithColumn("id").AsString().PrimaryKey()
                 .WithColumn("interviewid").AsGuid().NotNullable()
                 .WithColumn("filename").AsString().NotNullable()
                 .WithColumn("data").AsCustom("integer[]").NotNullable();
 
-            this.Create.Index("databasefiles_interviewid").OnTable("databasefiles");
+            this.Create.Index("audiofiles_interviewid")
+                .OnTable("audiofiles")
+                .OnColumn("interviewid");
         }
 
         public override void Down()
         {
-            this.Delete.Index("databasefiles_interviewid").OnTable("databasefiles");
-            this.Delete.Table("databasefiles");
+            this.Delete.Index("audiofiles_interviewid");
+            this.Delete.Table("audiofiles");
         }
     }
 }
