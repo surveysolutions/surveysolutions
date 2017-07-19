fd6dced5df4beec65ba30299e2ea5e1c0bd5b831
Andrii Kozhyn <hedgic@gmail.com>
2017-07-19 15:46:59 +0300

    KP-9581: Added resource controller for audio file

--- src/Core/SharedKernels/DataCollection/DataCollection/Implementation/Repositories/AudioFileStorage.cs
+++ src/Core/SharedKernels/DataCollection/DataCollection/Implementation/Repositories/AudioFileStorage.cs
@@ -10,10 +10,15 @@ namespace WB.Core.SharedKernels.DataCollection.Implementation.Repositories
 {
     public class AudioFile : IView
     {
-        public virtual string Id { get; set; } 
+        public virtual string Id { get; set; }
+
         public virtual Guid InterviewId { get; set; }
+
         public virtual string FileName { get; set; }
+
         public virtual byte[] Data { get; set; }
+
+        public virtual string ContentType { get; set; }
 
         public static string GetFileId(Guid interviewId, string fileName) => $"{interviewId}#{fileName}";
     }
