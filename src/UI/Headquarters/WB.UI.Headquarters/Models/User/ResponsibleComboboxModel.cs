3ec050230e02f00134bfc90805b08febb431de0b
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2017-07-19 11:44:29 +0300

    KP-9481: Show icon in responsible dropdown on assignments list

---
+++ src/UI/Headquarters/WB.UI.Headquarters/Models/User/ResponsibleComboboxModel.cs
@@ -0,0 +1,15 @@
+﻿using WB.UI.Headquarters.Controllers;
+
+namespace WB.UI.Headquarters.Models.User
+{
+    public class ResponsibleComboboxModel
+    {
+        public ResponsibleComboboxModel(ResponsibleComboboxOptionModel[] options, int? total = null)
+        {
+            this.Options = options ?? new ResponsibleComboboxOptionModel[0];
+            this.Total = total ?? options.Length;
+        }
+        public ResponsibleComboboxOptionModel[] Options { get; private set; }
+        public int Total { get; private set; }
+    }
+}
