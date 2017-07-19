8d975dcf1052232ddb3ea563233556228badf8cc
Slava Scherbak <slava.scherbak@gmail.com>
2017-07-19 12:32:43 +0300

    KP-9620:  moved recorder to popup

--- src/UI/Headquarters/WB.UI.Headquarters.Interview/src/components/entities/Audio.vue
+++ src/UI/Headquarters/WB.UI.Headquarters.Interview/src/components/entities/Audio.vue
@@ -2,13 +2,31 @@
     <wb-question :question="$me" questionCssClassName=" audio-question">
         <div class="question-unit">
             <div class="options-group">
-                <button type="button" class="btn btn-default btn-lg btn-action-questionnaire" v-if="!isRecording" v-on:click="requestAccessAndStartRecording">Tap to record audio</button>
-                <button type="button" class="btn btn-link" v-if="isRecording" v-on:click="stopRecording">Done</button>
+                <button type="button" class="btn btn-default btn-lg btn-action-questionnaire" v-if="!isRecording" v-on:click="startRecording">Tap to record audio</button>
             </div>
         </div>
-        <canvas id="analyser" width="100" height="100"></canvas>
-        <canvas id="wavedisplay" width="100" height="50"></canvas>
-        <audio ref="audiofile" preload="auto"></audio>
+        <div class="modal fade" tabindex="-1" role="dialog">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h4 class="modal-title">Modal title</h4>
+                        <h5 v-dateTimeFormatting v-html="$me.title"></h5>
+                    </div>
+                    <div class="modal-body">
+                        <canvas class="analyser" width="100" height="100"></canvas>
+                    </div>
+                    <div class="modal-footer">
+                        <span></span>
+                        <button type="button" v-on:click="cancelRecording" class="btn btn-link " data-dismiss="modal">Cancel</button>
+                        <button type="button" v-on:click="stopRecording" class="btn btn-primary" v-if="isRecording" >Done</button>
+                    </div>
+                </div>
+                <!-- /.modal-content -->
+            </div>
+            <!-- /.modal-dialog -->
+        </div>
+        <div class="modal-backdrop in" style="display: none"></div>
+        <!-- /.modal -->
     </wb-question>
 </template>
 <script lang="ts">
@@ -21,32 +39,50 @@ export default {
     mixins: [entityDetails],
     data() {
         return {
-            isRecording: false
+            isRecording: false,
+            startDate: null
         }
     },
     computed: {
     },
     methods: {
+        showModal(){
+            console.log("-");
+            var modal = $(this.$el).find(".modal");
+            console.log("--");
+            $(this.$el).find(".modal-backdrop").show()
+            console.log("---");
+            modal.addClass('in');
+            console.log("----");
+            modal.show();
+            console.log("-----");
+        },
+        closeModal() {
+            var modal = $(this.$el).find(".modal");
+            $(this.$el).find(".modal-backdrop").hide()
+            modal.removeClass('in');
+            modal.hide()
+        },
         stopRecording() {
             this.isRecording = false;
+            this.closeModal();
             AudioRecorder.stop();
         },
-        requestAccessAndStartRecording() {
+        cancelRecording() {
+            this.isRecording = false;
+            AudioRecorder.cancel();
+            this.closeModal();
+        },
+        startRecording() {
+            this.showModal();
             const self = this
             AudioRecorder.initAudio({
-                analyserEl: $(this.$el).find("#analyser")[0],
-                wavedisplayEl: $(this.$el).find("#wavedisplay")[0],
+                analyserEl: $(this.$el).find(".analyser")[0],
                 errorCallback: (e) => {
                     self.markAnswerAsNotSavedWithMessage("Audio initialization failed")
                     console.log(e)
                 },
                 doneCallback: (blob) => {
-                    var url = URL.createObjectURL(blob);
-                    var audioElement = $(self.$el).find("audio")[0];
-
-                    audioElement.controls = true;
-                    audioElement.src = url;
-
                     self.$store.dispatch('answerAudioQuestion', {
                         id: self.id,
                         file: blob,
