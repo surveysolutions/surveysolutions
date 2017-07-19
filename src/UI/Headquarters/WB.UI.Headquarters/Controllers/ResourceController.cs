dc00860aa2978c23ff37866762d9f202d314a403
Andrii Kozhyn <hedgic@gmail.com>
2017-07-19 15:46:59 +0300

    KP-9581: Added resource controller for audio file

--- src/UI/Headquarters/WB.UI.Headquarters/Controllers/ResourceController.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/Controllers/ResourceController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Linq;
 using System.Reflection;
 using System.Web.Mvc;
 using WB.Core.BoundedContexts.Headquarters.Views;
 using WB.Core.GenericSubdomains.Portable.Services;
 using WB.Core.Infrastructure.CommandBus;
+using WB.Core.Infrastructure.PlainStorage;
+using WB.Core.SharedKernels.DataCollection.Implementation.Repositories;
 using WB.Core.SharedKernels.DataCollection.Repositories;
 
 namespace WB.UI.Headquarters.Controllers
@@ -12,12 +15,15 @@ namespace WB.UI.Headquarters.Controllers
     public class ResourceController : BaseController
     {
         private readonly IImageFileStorage imageFileRepository;
+        private readonly IPlainStorageAccessor<AudioFile> audioFileStorage;
 
         public ResourceController(ICommandService commandService, ILogger logger,
-            IImageFileStorage imageFileRepository)
+            IImageFileStorage imageFileRepository,
+            IPlainStorageAccessor<AudioFile> audioFileStorage)
             : base(commandService, logger)
         {
             this.imageFileRepository = imageFileRepository;
+            this.audioFileStorage = audioFileStorage;
         }
 
         public ActionResult InterviewFile(Guid interviewId, string fileName)
@@ -33,6 +39,20 @@ namespace WB.UI.Headquarters.Controllers
                             .GetManifestResourceStream("WB.UI.Headquarters.Content.img.no_image_found.jpg"),
                         "image/jpeg", "no_image_found.jpg");
             return this.File(file, "image/jpeg", fileName);
+        }
+
+        public ActionResult AudioRecord(Guid interviewId, string fileName)
+        {
+            AudioFile file = null;
+            if (fileName != null)
+            {
+                file = this.audioFileStorage.Query(_=> _.FirstOrDefault(x => x.InterviewId == interviewId && x.FileName == fileName));
+            }
+
+            if (file == null || file.Data.Length == 0)
+                return HttpNotFound();
+
+            return this.File(file.Data, file.ContentType, fileName);
         }
     }
 }
