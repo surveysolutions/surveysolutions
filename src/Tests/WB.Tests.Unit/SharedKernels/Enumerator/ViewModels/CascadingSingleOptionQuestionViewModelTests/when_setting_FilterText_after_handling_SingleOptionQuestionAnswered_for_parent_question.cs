8e47cb537070b22f7f89a8bfe8817a1e32f1c448
Andrii Kozhyn <hedgic@gmail.com>
2017-07-19 20:03:25 +0300

    Migrated test to async with normal await

--- src/Tests/WB.Tests.Unit/SharedKernels/Enumerator/ViewModels/CascadingSingleOptionQuestionViewModelTests/when_setting_FilterText_after_handling_SingleOptionQuestionAnswered_for_parent_question.cs
+++ src/Tests/WB.Tests.Unit/SharedKernels/Enumerator/ViewModels/CascadingSingleOptionQuestionViewModelTests/when_setting_FilterText_after_handling_SingleOptionQuestionAnswered_for_parent_question.cs
@@ -1,23 +1,22 @@
 using System;
 using System.Linq;
-using System.Threading;
-using Machine.Specifications;
+using System.Threading.Tasks;
+using FluentAssertions;
 using Moq;
+using NUnit.Framework;
 using WB.Core.GenericSubdomains.Portable;
 using WB.Core.SharedKernels.DataCollection;
 using WB.Core.SharedKernels.DataCollection.Aggregates;
 using WB.Core.SharedKernels.DataCollection.Implementation.Aggregates.InterviewEntities;
 using WB.Core.SharedKernels.DataCollection.Repositories;
-using WB.Core.SharedKernels.Enumerator.Repositories;
-using WB.Core.SharedKernels.Enumerator.ViewModels.InterviewDetails.Questions;
 using WB.Tests.Abc;
-using It = Machine.Specifications.It;
 
 namespace WB.Tests.Unit.SharedKernels.Enumerator.ViewModels.CascadingSingleOptionQuestionViewModelTests
 {
     internal class when_setting_FilterText_after_handling_SingleOptionQuestionAnswered_for_parent_question : CascadingSingleOptionQuestionViewModelTestContext
     {
-        Establish context = () =>
+        [Test]
+        public async Task should_update_list_of_suggestions()
         {
             SetUp();
 
@@ -25,51 +24,43 @@ namespace WB.Tests.Unit.SharedKernels.Enumerator.ViewModels.CascadingSingleOptio
             var parentOptionAnswer = Mock.Of<InterviewTreeSingleOptionQuestion>(_ => _.IsAnswered == true && _.GetAnswer() == Create.Entity.SingleOptionAnswer(1));
             var secondParentOptionAnswer = Mock.Of<InterviewTreeSingleOptionQuestion>(_ => _.IsAnswered == true && _.GetAnswer() == Create.Entity.SingleOptionAnswer(2));
 
-            StatefulInterviewMock.Setup(x => x.Id).Returns(interviewGuid);
-            StatefulInterviewMock.Setup(x => x.QuestionnaireIdentity).Returns(questionnaireId);
-            StatefulInterviewMock.Setup(x => x.GetSingleOptionQuestion(questionIdentity)).Returns(childAnswer);
-            StatefulInterviewMock.Setup(x => x.GetSingleOptionQuestion(parentIdentity)).Returns(parentOptionAnswer);
+            Mock<IStatefulInterview> statefulInterviewMock = new Mock<IStatefulInterview>();
+            statefulInterviewMock.Setup(x => x.Id).Returns(interviewGuid);
+            statefulInterviewMock.Setup(x => x.QuestionnaireIdentity).Returns(questionnaireId);
+            statefulInterviewMock.Setup(x => x.GetSingleOptionQuestion(questionIdentity)).Returns(childAnswer);
+            statefulInterviewMock.Setup(x => x.GetSingleOptionQuestion(parentIdentity)).Returns(parentOptionAnswer);
 
-            StatefulInterviewMock.Setup(x => x.GetOptionForQuestionWithoutFilter(questionIdentity, 3, 1))
+            statefulInterviewMock.Setup(x => x.GetOptionForQuestionWithoutFilter(questionIdentity, 3, 1))
                 .Returns(new CategoricalOption() {Title = "3", Value = 3, ParentValue = 1});
 
-            StatefulInterviewMock.Setup(x => x.GetTopFilteredOptionsForQuestion(Moq.It.IsAny<Identity>(), Moq.It.IsAny<int?>(),
+            statefulInterviewMock.Setup(x => x.GetTopFilteredOptionsForQuestion(Moq.It.IsAny<Identity>(), Moq.It.IsAny<int?>(),
                 Moq.It.IsAny<string>(), Moq.It.IsAny<int>()))
                 .Returns((Identity identity, int? value, string filter, int count) => 
                     Options.Where(x => x.ParentValue == value && x.Title.IndexOf(filter, StringComparison.OrdinalIgnoreCase) >= 0).ToList());
 
 
-            var interviewRepository = Mock.Of<IStatefulInterviewRepository>(x => x.Get(interviewGuid.FormatGuid()) == StatefulInterviewMock.Object);
+            var interviewRepository = Mock.Of<IStatefulInterviewRepository>(x => x.Get(interviewGuid.FormatGuid()) == statefulInterviewMock.Object);
 
             var questionnaireRepository = SetupQuestionnaireRepositoryWithCascadingQuestion();
 
-            cascadingModel = CreateCascadingSingleOptionQuestionViewModel(
+            var cascadingModel = CreateCascadingSingleOptionQuestionViewModel(
                 interviewRepository: interviewRepository,
                 questionnaireRepository: questionnaireRepository);
 
             cascadingModel.Init(interviewGuid.FormatGuid(), questionIdentity, navigationState);
 
-            StatefulInterviewMock.Setup(x => x.GetSingleOptionQuestion(parentIdentity)).Returns(secondParentOptionAnswer);
+            statefulInterviewMock.Setup(x => x.GetSingleOptionQuestion(parentIdentity)).Returns(secondParentOptionAnswer);
 
             cascadingModel.Handle(Create.Event.SingleOptionQuestionAnswered(parentIdentity.Id, parentIdentity.RosterVector, 2));
-        };
 
-        Because of = () => cascadingModel.FilterCommand.ExecuteAsync("c").Await();
+            //Act 
+            await cascadingModel.FilterCommand.ExecuteAsync("c");
 
-        It should_set_not_empty_list_in_AutoCompleteSuggestions = () =>
-            cascadingModel.AutoCompleteSuggestions.ShouldNotBeEmpty();
-
-        It should_set_2_options_in_AutoCompleteSuggestions = () =>
-            cascadingModel.AutoCompleteSuggestions.Count.ShouldEqual(2);
-
-        It should_format_first_option_in_AutoCompleteSuggestions = () =>
-            cascadingModel.AutoCompleteSuggestions.ElementAt(0).ShouldEqual("title <b>c</b>cc 5");
-
-        It should_format_second_option_in_AutoCompleteSuggestions = () =>
-            cascadingModel.AutoCompleteSuggestions.ElementAt(1).ShouldEqual("title b<b>c</b>w 6");
-
-        private static CascadingSingleOptionQuestionViewModel cascadingModel;
-
-        private static readonly Mock<IStatefulInterview> StatefulInterviewMock = new Mock<IStatefulInterview>();
+            // Assert
+            cascadingModel.AutoCompleteSuggestions.Should().NotBeEmpty();
+            cascadingModel.AutoCompleteSuggestions.Should().HaveCount(2);
+            cascadingModel.AutoCompleteSuggestions.Should().HaveElementAt(0, "title <b>c</b>cc 5");
+            cascadingModel.AutoCompleteSuggestions.Should().HaveElementAt(1, "title b<b>c</b>w 6");
+        }
     }
 }
