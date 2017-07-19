a1705cc243bdc35a15e693eaf58417ec9b6ea543
Victor Gladkikh <me@alfeg.net>
2017-07-19 12:55:57 +0300

    KP-9592, KP-9593 HQ: Assignments list for interviewer # Progress dialog

--- src/UI/Headquarters/WB.UI.Headquarters/Dependencies/app/startup.js
+++ src/UI/Headquarters/WB.UI.Headquarters/Dependencies/app/startup.js
@@ -1,14 +1,15 @@
 import Vue from 'vue'
+
 import Typeahead from './components/Typeahead'
 import Layout from "./components/Layout"
 import Filters from "./components/Filters"
 import FilterBlock from "./components/FilterBlock"
 import DataTables from "./components/DataTables"
-import configurator from "./plugins/configurator"
-
+import ModalFrame from "./components/ModalFrame"
+import Confirm from './components/Confirm'
 import locale from "./plugins/locale"
+import common_store from "./store"
 
-Vue.use(configurator)
 Vue.use(locale)
 
 Vue.component("Layout", Layout)
@@ -16,15 +17,16 @@ Vue.component("Filters", Filters)
 Vue.component("FilterBlock", FilterBlock)
 Vue.component("Typeahead", Typeahead)
 Vue.component("DataTables", DataTables)
+Vue.component("ModalFrame", ModalFrame)
+Vue.component("Confirm", Confirm)
 
-import confirm from './components/Confirm'
-Vue.component("Confirm", confirm)
-
-export default (app, options) => {
+export default ({ app, options}) => {
+    const store = require("./store").default
 
     const vueApp = new Vue(_.assign({
         el: "#vueApp",
         render: h => h(app),
-        components: { app }
+        components: { app },
+        store
     }, options));
 }
