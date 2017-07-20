ef40e88090a1c85f700a31bc19164d7d8668014f
Victor Gladkikh <me@alfeg.net>
2017-07-20 12:58:23 +0300

    Revert "Cherry pick build changes from master branch"
    This reverts commit 210702b11df191705d90ffbf066ade73b7471e10.

--- build/build-only.ps1
+++ build/build-only.ps1
@@ -1,7 +1,7 @@
 param([string]$VersionPrefix,
-    [INT]$BuildNumber,
-    [string]$KeystorePassword,
-    [string]$BuildConfiguration = "Release")
+[INT]$BuildNumber,
+[string]$KeystorePassword,
+[string]$BuildConfiguration)
 
 $ErrorActionPreference = "Stop"
 
@@ -15,105 +15,98 @@ $ProjectHeadquarters = 'src\UI\Headquarters\WB.UI.Headquarters\WB.UI.Headquarter
 $MainSolution = 'src\WB.sln'
 $SupportToolSolution = 'src\Tools\support\support.sln'
 
-versionCheck
 
 $versionString = (GetVersionString 'src\core')
 UpdateProjectVersion $BuildNumber -ver $versionString
-Write-Host "##teamcity[setParameter name='system.VersionString' value='$versionString']"
 
-$artifactsFolder = (Get-Location).Path + "\Artifacts"
-
-Write-Host "Artifacts Folder: $artifactsFolder"
-
-If (Test-Path "$artifactsFolder") {
-    Remove-Item "$artifactsFolder" -Force -Recurse
+function EnsureGlobalNpmPackagesInstalled(){
+    Write-Host "Ensuring that global npm deps installed"
+    &npm install -g bower | Write-Host
 }
-
-New-Item $artifactsFolder -Type Directory -Force
-
 
 try {
-    $buildSuccessful = BuildSolution `
-        -Solution $MainSolution `
-        -BuildConfiguration $BuildConfiguration
-    if ($buildSuccessful) { 
+    EnsureGlobalNpmPackagesInstalled
 
-        BuildStaticContent "Designer Questionnaire" "src\UI\Designer\WB.UI.Designer\questionnaire" | % { if (-not $_) { 
-            Write-Host "##teamcity[message status='ERROR' text='Unexpected error occurred in BuildStaticContent']"
-            Write-Host "##teamcity[buildProblem description='Failed to build static content for Designer']"
-            Exit 
-        }}
-        
-        BuildStaticContent "Hq Deps" "src\UI\Headquarters\WB.UI.Headquarters\Dependencies" | % { if (-not $_) {
-                Write-Host "##teamcity[message status='ERROR' text='Unexpected error occurred in BuildStaticContent']"
-                Write-Host "##teamcity[buildProblem description='Failed to build static content for HQ']"
-                Exit 
-            }}
+	BuildSolution `
+                -Solution $MainSolution `
+                -BuildConfiguration $BuildConfiguration
+			
+	RunConfigTransform $ProjectDesigner $BuildConfiguration
+	BuildStaticContent "src\UI\Designer\WB.UI.Designer\questionnaire" $false | %{ if (-not $_) { 
+		Write-Host "##teamcity[message status='ERROR' text='Unexpected error occurred in BuildStaticContent']"
+		Write-Host "##teamcity[buildProblem description='Failed to build static content for Designer']"
+		Exit 
+	}}
+		 
+	if(-not (BuildWebInterviewApp $ProjectWebInterview)){
+	 	Write-Host "##teamcity[message status='ERROR' text='Unexpected error occurred in BuildNodeApp']"
+	 	Write-Host "##teamcity[buildProblem description='Failed to build Web interview application']"
+	 	Exit 		
+	}
+	
+	BuildStaticContent "src\UI\Headquarters\WB.UI.Headquarters\Dependencies" $true | %{ if (-not $_) {
+		Write-Host "##teamcity[message status='ERROR' text='Unexpected error occurred in BuildStaticContent']"
+		Write-Host "##teamcity[buildProblem description='Failed to build static content for HQ']"
+		Exit 
+	}}
+	
 
-        # BuildStaticContent "Hq UI" "src\UI\Headquarters\WB.UI.Headquarters" | % { if (-not $_) {
-        #         Write-Host "##teamcity[message status='ERROR' text='Unexpected error occurred in BuildStaticContent']"
-        #         Write-Host "##teamcity[buildProblem description='Failed to build static content for HQ']"
-        #         Exit 
-        #     }}
+	RunConfigTransform $ProjectHeadquarters $BuildConfiguration	
+	
+	$ExtPackageName = 'WBCapi.Ext.apk'
+		. "$scriptFolder\build-android-package.ps1" `
+			-VersionName $versionString `
+			-VersionCode $BuildNumber `
+			-BuildConfiguration $BuildConfiguration `
+			-KeystorePassword $KeystorePassword `
+			-KeystoreName 'WBCapi.keystore' `
+			-KeystoreAlias 'wbcapipublish' `
+			-CapiProject 'src\UI\Interviewer\WB.UI.Interviewer\WB.UI.Interviewer.csproj' `
+			-OutFileName $ExtPackageName `
+			-ExcludeExtra $false | %{ if (-not $_) { Exit } }	
+	
+	CopyCapi -Project $ProjectHeadquarters -source $ExtPackageName -cleanUp $true | %{ if (-not $_) { Exit } }
 
-        BuildStaticContent "Web Interview" $ProjectWebInterview | % { if (-not $_) { 
-            Write-Host "##teamcity[message status='ERROR' text='Unexpected error occurred in BuildStaticContent']"
-            Write-Host "##teamcity[buildProblem description='Failed to build Web interview application']"
-            Exit
-        }
-        else {
-            Move-Item "..\WB.UI.Headquarters\InterviewApp\stats.html" "$artifactsFolder\WebInterview.stats.html" -ErrorAction SilentlyContinue
-        }}
+	#remove leftovers after previous build
+	
+	#CleanFolders 'bin' | %{ if (-not $_) { Exit } }
+    CleanFolders 'obj' | %{ if (-not $_) { Exit } }
+	
+	$PackageName = 'WBCapi.apk'
+		. "$scriptFolder\build-android-package.ps1" `
+			-VersionName $versionString `
+			-VersionCode $BuildNumber `
+			-BuildConfiguration $BuildConfiguration `
+			-KeystorePassword $KeystorePassword `
+			-KeystoreName 'WBCapi.keystore' `
+			-KeystoreAlias 'wbcapipublish' `
+			-CapiProject 'src\UI\Interviewer\WB.UI.Interviewer\WB.UI.Interviewer.csproj' `
+			-OutFileName $PackageName `
+			-ExcludeExtra $true | %{ if (-not $_) { Exit } }
+	
+	CopyCapi -Project $ProjectHeadquarters -source $PackageName -cleanUp $false | %{ if (-not $_) { Exit } }
+	
+	BuildWebPackage $ProjectHeadquarters $BuildConfiguration | %{ if (-not $_) { Exit } }
+	
+	BuildWebPackage $ProjectDesigner $BuildConfiguration | %{ if (-not $_) { Exit } }
+	
+	$artifactsFolder = (Get-Location).Path + "\Artifacts"
+	If (Test-Path "$artifactsFolder"){
+		Remove-Item "$artifactsFolder" -Force -Recurse
+	}
+	
+	BuildAndDeploySupportTool $SupportToolSolution $BuildConfiguration | %{ if (-not $_) { Exit } }
+	
+	$webpackStats = "src\UI\Headquarters\WB.UI.Headquarters\InterviewApp\stats.html"
+	MoveArtifacts $webpackStats -folder "BuildStats"
+	Remove-Item $webpackStats
+	AddArtifacts $ProjectDesigner $BuildConfiguration -folder "Designer"
+	AddArtifacts $ProjectHeadquarters $BuildConfiguration -folder "Headquarters"	
 
-        RunConfigTransform $ProjectDesigner $BuildConfiguration
-        RunConfigTransform $ProjectHeadquarters $BuildConfiguration
-
-        $ExtPackageName = 'WBCapi.Ext.apk'
-        . "$scriptFolder\build-android-package.ps1" `
-            -VersionName $versionString `
-            -VersionCode $BuildNumber `
-            -BuildConfiguration $BuildConfiguration `
-            -KeystorePassword $KeystorePassword `
-            -KeystoreName 'WBCapi.keystore' `
-            -KeystoreAlias 'wbcapipublish' `
-            -CapiProject 'src\UI\Interviewer\WB.UI.Interviewer\WB.UI.Interviewer.csproj' `
-            -OutFileName $ExtPackageName `
-            -ExcludeExtra $false | % { if (-not $_) { Exit } }
-
-         CopyCapi -Project $ProjectHeadquarters -source $ExtPackageName -cleanUp $true | % { if (-not $_) { Exit } }
-
-        #remove leftovers after previous build
-
-        #CleanFolders 'bin' | %{ if (-not $_) { Exit } }
-        #CleanFolders 'obj' | %{ if (-not $_) { Exit } }
-
-        $PackageName = 'WBCapi.apk'
-        . "$scriptFolder\build-android-package.ps1" `
-            -VersionName $versionString `
-            -VersionCode $BuildNumber `
-            -BuildConfiguration $BuildConfiguration `
-            -KeystorePassword $KeystorePassword `
-            -KeystoreName 'WBCapi.keystore' `
-            -KeystoreAlias 'wbcapipublish' `
-            -CapiProject 'src\UI\Interviewer\WB.UI.Interviewer\WB.UI.Interviewer.csproj' `
-            -OutFileName $PackageName `
-            -ExcludeExtra $true | % { if (-not $_) { Exit } }
-
-        CopyCapi -Project $ProjectHeadquarters -source $PackageName -cleanUp $false | % { if (-not $_) { Exit } }
-
-        BuildWebPackage $ProjectHeadquarters $BuildConfiguration | % { if (-not $_) { Exit } }
-        BuildWebPackage $ProjectDesigner $BuildConfiguration | % { if (-not $_) { Exit } }
-
-        BuildAndDeploySupportTool $SupportToolSolution $BuildConfiguration | % { if (-not $_) { Exit } }
-
-        AddArtifacts $ProjectDesigner $BuildConfiguration -folder "Designer"
-        AddArtifacts $ProjectHeadquarters $BuildConfiguration -folder "Headquarters"
-
-        Write-Host "##teamcity[publishArtifacts '$artifactsFolder']"
-    }
+	Write-Host "##teamcity[publishArtifacts '$artifactsFolder']"
 }
 catch {
-    Write-Host "##teamcity[message status='ERROR' text='Unexpected error occurred']"
-    Write-Host "##teamcity[buildProblem description='Unexpected error occurred']"
-    throw
+	Write-Host "##teamcity[message status='ERROR' text='Unexpected error occurred']"
+	Write-Host "##teamcity[buildProblem description='Unexpected error occurred']"
+	throw
 }
