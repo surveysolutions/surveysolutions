d9062170677eb29b8e5590697c42f2b4acb58dd1
Victor Hladkikh <me@alfeg.net>
2017-07-21 15:08:33 +0300

    KP-9693 Logo for logged in Interviewer should lead to dashboard

--- src/UI/Headquarters/WB.UI.Headquarters.Interview/src/config/index.ts
+++ src/UI/Headquarters/WB.UI.Headquarters.Interview/src/config/index.ts
@@ -11,3 +11,4 @@ export const verboseMode = INTERVIEW_APP_CONFIG.verboseLogging
 export const assetsPath = INTERVIEW_APP_CONFIG.assetsPath
 export const appVersion = INTERVIEW_APP_CONFIG.appVersion
 export const googleApiKey: string = INTERVIEW_APP_CONFIG.googleApiKey
+export const hqLink: string = INTERVIEW_APP_CONFIG.hqLink
