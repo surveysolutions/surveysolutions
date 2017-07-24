bcd6c79820ffa17e67e4a54773776fc077fc6b19
Andrii Kozhyn <hedgic@gmail.com>
2017-07-24 12:12:00 +0300

    KP-9709: Fixed apply methods discovery method

---
+++ src/Tests/WB.Tests.Unit/Infrastructure/Ncqrs/Eventing/ConventionBasedEventHandlerMappingStrategyTests.cs
@@ -0,0 +1,21 @@
+﻿using Ncqrs.Eventing.Sourcing;
+using Ncqrs.Eventing.Sourcing.Mapping;
+using NUnit.Framework;
+using WB.Core.BoundedContexts.Headquarters.Aggregates;
+using WB.Core.Synchronization.Events.Sync;
+
+namespace Ncqrs.Tests.Eventing
+{
+    [TestOf(typeof(ConventionBasedEventHandlerMappingStrategy))]
+    public class ConventionBasedEventHandlerMappingStrategyTests
+    {
+        [Test]
+        public void should_find_apply_methods_when_they_are_protected()
+        {
+            ConventionBasedEventHandlerMappingStrategy strategy = new ConventionBasedEventHandlerMappingStrategy();
+
+            var canHandle = strategy.CanHandleEvent(new Tablet(), typeof(TabletRegistered));
+            Assert.That(canHandle, Is.True);
+        }
+    }
+}
