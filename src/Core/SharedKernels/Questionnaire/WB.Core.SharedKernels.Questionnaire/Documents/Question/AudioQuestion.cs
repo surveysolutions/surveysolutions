d31b0818283196590a477a5f3d5a20f43666f745
Andrii Kozhyn <hedgic@gmail.com>
2017-07-24 16:25:47 +0300

    KP-9580: Apply quality options from interviewer

--- src/Core/SharedKernels/Questionnaire/WB.Core.SharedKernels.Questionnaire/Documents/Question/AudioQuestion.cs
+++ src/Core/SharedKernels/Questionnaire/WB.Core.SharedKernels.Questionnaire/Documents/Question/AudioQuestion.cs
@@ -1,21 +1,14 @@
+using WB.Core.SharedKernels.Questionnaire.Documents.Question;
+
 namespace Main.Core.Entities.SubEntities.Question
 {
-    public enum Quality
-    {
-        Lowest = 0,
-        Low = 1,
-        Medium = 2,
-        High = 3,
-        Highest = 4
-    }
-
     public class AudioQuestion : ExternalServiceQuestion
     {
         public AudioQuestion()
         {
-            this.Quality = Quality.Medium;
+            this.Quality = AudioQuality.Default;
         }
 
-        public Quality Quality { get; set; }
+        public AudioQuality Quality { get; set; }
     }
 }
