5e391eab59d8babdb207536fc650ede7e3fff9bb
Victor Hladkikh <me@alfeg.net>
2017-07-24 10:14:53 +0300

    KP-9593 HQ: Interviews list for interviewer # confirmation on discard

--- src/UI/Headquarters/WB.UI.Headquarters/Dependencies/app/Interviewer/createNew.js
+++ src/UI/Headquarters/WB.UI.Headquarters/Dependencies/app/Interviewer/createNew.js
@@ -14,22 +14,6 @@ store.registerModule('createNew', {
                 dispatch("showProgress", true);
                 window.location = response;
             }).then(() => dispatch("showProgress", false));
-
-        },
-
-        discardInterview(context, interviewId) {
-            $.post(context.rootState.config.interviewerHqEndpoint + "/DiscardInterview/" + interviewId, response => {
-                this.reload();
-            });
-        },
-
-        restartInterview(context, interviewId) {
-            this.$refs.confirmation.promt(() => {
-                $.post(context.rootState.config.interviewerHqEndpoint + "/RestartInterview/" + interviewId, { comment: this.restart_comment }, response => {
-                    dispatch("showProgress", true);
-                    window.location = context.rootState.config.interviewerHqEndpoint + "/OpenInterview/" + interviewId;
-                })
-            });
         }
     }
 })
