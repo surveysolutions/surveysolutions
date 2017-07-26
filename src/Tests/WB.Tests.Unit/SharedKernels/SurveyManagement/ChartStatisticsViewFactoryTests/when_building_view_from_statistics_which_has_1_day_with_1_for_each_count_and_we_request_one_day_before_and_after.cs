6cad9bbd9b46b998e72edfa273478189b5cfc929
Victor Gladkikh <me@alfeg.net>
2017-07-26 17:11:45 +0300

    KP-9731 Add Restarted to all HQ filters # Fixed tests for charts

--- src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/ChartStatisticsViewFactoryTests/when_building_view_from_statistics_which_has_1_day_with_1_for_each_count_and_we_request_one_day_before_and_after.cs
+++ src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/ChartStatisticsViewFactoryTests/when_building_view_from_statistics_which_has_1_day_with_1_for_each_count_and_we_request_one_day_before_and_after.cs
@@ -1,18 +1,17 @@
 using System;
 using System.Collections.Generic;
 using Machine.Specifications;
-using Moq;
+using NUnit.Framework;
 using WB.Core.BoundedContexts.Headquarters.EventHandler;
 using WB.Core.BoundedContexts.Headquarters.Implementation.Factories;
 using WB.Core.BoundedContexts.Headquarters.Views.Interviews;
-using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
-using It = Machine.Specifications.It;
 
 namespace WB.Tests.Unit.SharedKernels.SurveyManagement.ChartStatisticsViewFactoryTests
 {
-    internal class when_building_view_from_statistics_which_has_1_day_with_1_for_each_count_and_we_request_two_days_before_and_one_after : ChartStatisticsViewFactoryTestsContext
+    public class when_building_view_from_statistics_which_has_1_day_with_1_for_each_count_and_we_request_two_days_before_and_one_after : ChartStatisticsViewFactoryTestsContext
     {
-        Establish context = () =>
+        [OneTimeSetUp]
+        public void Establish()
         {
             var statistics = CreateStatisticsGroupedByDateAndTemplate(new Dictionary<DateTime, QuestionnaireStatisticsForChart>
             {
@@ -32,36 +31,46 @@ namespace WB.Tests.Unit.SharedKernels.SurveyManagement.ChartStatisticsViewFactor
             };
 
             chartStatisticsViewFactory = CreateChartStatisticsViewFactory(statistics: statistics);
-        };
+            Because();
+        }
 
-        Because of = () => view = chartStatisticsViewFactory.Load(input);
+        public void Because() => view = chartStatisticsViewFactory.Load(input);
 
-        It should_return_7_lines_the_same_as_statuses_count = () =>
-            view.Lines.Length.ShouldEqual(7);
+        [Test]
+        public void should_return_8_lines_the_same_as_statuses_count() =>
+            view.Lines.Length.ShouldEqual(8);
 
-        It should_set_1st_point_horizontal_coord_of_all_lines_equal_to_2014_08_19 = () =>
+        [Test]
+        public void should_set_1st_point_horizontal_coord_of_all_lines_equal_to_2014_08_19() =>
             view.Lines.ShouldEachConformTo(line => (string)line[0][0] == "2014-08-19");
 
-        It should_set_2nd_point_horizontal_coord_of_all_lines_equal_to_2014_08_20 = () =>
+        [Test]
+        public void should_set_2nd_point_horizontal_coord_of_all_lines_equal_to_2014_08_20() =>
             view.Lines.ShouldEachConformTo(line => (string)line[1][0] == "2014-08-20");
 
-        It should_set_3rd_point_horizontal_coord_of_all_lines_equal_to_2014_08_21 = () =>
+        [Test]
+        public void should_set_3rd_point_horizontal_coord_of_all_lines_equal_to_2014_08_21() =>
             view.Lines.ShouldEachConformTo(line => (string)line[2][0] == "2014-08-21");
 
-        It should_set_4th_point_horizontal_coord_of_all_lines_equal_to_2014_08_22 = () =>
+        [Test]
+        public void should_set_4th_point_horizontal_coord_of_all_lines_equal_to_2014_08_22() =>
             view.Lines.ShouldEachConformTo(line => (string)line[3][0] == "2014-08-22");
 
-        It should_set_1st_point_vertical_size_of_all_lines_equal_to_0_as_starting_day_with_no_data = () =>
-            view.Lines.ShouldEachConformTo(line => (int) line[0][1] == 0);
+        [Test]
+        public void should_set_1st_point_vertical_size_of_all_lines_equal_to_0_as_starting_day_with_no_data() =>
+            view.Lines.ShouldEachConformTo(line => (int)line[0][1] == 0);
 
-        It should_set_2nd_point_vertical_size_of_all_lines_equal_to_0_as_starting_day_with_no_data = () =>
-            view.Lines.ShouldEachConformTo(line => (int) line[1][1] == 0);
+        [Test]
+        public void should_set_2nd_point_vertical_size_of_all_lines_equal_to_0_as_starting_day_with_no_data() =>
+            view.Lines.ShouldEachConformTo(line => (int)line[1][1] == 0);
 
-        It should_set_3rd_point_vertical_size_of_all_lines_equal_to_1 = () =>
-            view.Lines.ShouldEachConformTo(line => (int) line[2][1] == 1);
+        [Test]
+        public void should_set_3rd_point_vertical_size_of_all_lines_equal_to_1() =>
+            view.Lines.ShouldEachConformTo(line => (int)line[2][1] == 1);
 
-        It should_set_4th_point_vertical_size_of_all_lines_equal_to_1_as_prev_day = () =>
-            view.Lines.ShouldEachConformTo(line => (int) line[3][1] == 1);
+        [Test]
+        public void should_set_4th_point_vertical_size_of_all_lines_equal_to_1_as_prev_day() =>
+            view.Lines.ShouldEachConformTo(line => (int)line[3][1] == 1);
 
         private static ChartStatisticsViewFactory chartStatisticsViewFactory;
         private static ChartStatisticsInputModel input;
