3a0be0e78fee7b04a7cc60d668f1f57d51e5846a
Victor Hladkikh <me@alfeg.net>
2017-07-28 14:22:19 +0300

    KP-9747 Incorrect number of interview quantity on Android if not completed web interview was sent to tablet # added test

---
+++ src/Tests/WB.Tests.Unit/BoundedContexts/Headquarters/Assignments/AssignmentsApiV2ControllerTests.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Moq;
+using NHibernate.Util;
+using NUnit.Framework;
+using WB.Core.BoundedContexts.Headquarters.Assignments;
+using WB.Core.BoundedContexts.Headquarters.Services;
+using WB.Core.BoundedContexts.Headquarters.Views.Interview;
+using WB.Core.SharedKernels.DataCollection.ValueObjects.Interview;
+using WB.Core.SharedKernels.DataCollection.WebApi;
+using WB.Tests.Abc;
+using WB.UI.Headquarters.API.Interviewer.v2;
+
+namespace WB.Tests.Unit.BoundedContexts.Headquarters.Assignments
+{
+    public class AssignmentsApiV2ControllerTests
+    {
+        [Test]
+        public async Task should_return_assignmentsList_accounting_created_interviews()
+        {
+            var assignmentsList = new List<Assignment>
+            {
+                Create.Entity.Assignment(quantity: 10, interviewSummary: new HashSet<InterviewSummary>
+                {
+                    Create.Entity.InterviewSummary(status: InterviewStatus.Completed),
+                    Create.Entity.InterviewSummary(status: InterviewStatus.Completed),
+
+                    Create.Entity.InterviewSummary(status: InterviewStatus.InterviewerAssigned),
+                    Create.Entity.InterviewSummary(status: InterviewStatus.InterviewerAssigned),
+
+                    Create.Entity.InterviewSummary(status: InterviewStatus.RejectedByHeadquarters),
+                    Create.Entity.InterviewSummary(status: InterviewStatus.RejectedBySupervisor)
+                })
+            };
+
+            var assignmentService = Mock.Of<IAssignmentsService>(s => s.GetAssignments(It.IsAny<Guid>()) == assignmentsList);
+
+            var authorizedUser = new Mock<IAuthorizedUser>();
+
+            var controller = new AssignmentsApiV2Controller(authorizedUser.Object, assignmentService);
+
+            var assignments = await controller.GetAssignmentsAsync(new CancellationToken());
+
+            Assert.That(assignments.First(), Has.Property(nameof(AssignmentApiView.Quantity))
+                .EqualTo(10 /* assignment.Quantity */ - 6 /* interviewSummary.Count */ + 3 /* interviews sent to device */));
+        }
+
+        [Test]
+        public void should_return_assignment_accounting_created_interviews()
+        {
+            var assignmentEntity = Create.Entity.Assignment(quantity: 10, interviewSummary: new HashSet<InterviewSummary>
+            {
+                Create.Entity.InterviewSummary(status: InterviewStatus.Completed),
+                Create.Entity.InterviewSummary(status: InterviewStatus.Completed),
+
+                Create.Entity.InterviewSummary(status: InterviewStatus.InterviewerAssigned),
+                Create.Entity.InterviewSummary(status: InterviewStatus.InterviewerAssigned),
+
+                Create.Entity.InterviewSummary(status: InterviewStatus.RejectedByHeadquarters),
+                Create.Entity.InterviewSummary(status: InterviewStatus.RejectedBySupervisor)
+            });
+            
+            var assignmentServiceImpl = new AssignmentsService(null, null);
+
+            var assignment = assignmentServiceImpl.MapAssignment(assignmentEntity);
+            
+            Assert.That(assignment, Has.Property(nameof(AssignmentApiView.Quantity))
+                .EqualTo(10 /* assignment.Quantity */ - 6 /* interviewSummary.Count */ + 3 /* interviews sent to device */));
+        }
+    }
+}
--- src/UI/Headquarters/WB.UI.Headquarters/API/Interviewer/v2/AssignmentsApiV2Controller.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/API/Interviewer/v2/AssignmentsApiV2Controller.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
