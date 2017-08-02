209dda49be0233a1d565d660b127e4ca0e83ea81
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2017-08-02 16:34:47 -0400

    KP-9796 Add dash as separator in filename when audio question placed in first level roster

--- src/Core/SharedKernels/Enumerator/Enumerator/ViewModels/InterviewDetails/Questions/MultimediaQuestionViewModel.cs
+++ src/Core/SharedKernels/Enumerator/Enumerator/ViewModels/InterviewDetails/Questions/MultimediaQuestionViewModel.cs
@@ -212,7 +212,7 @@ namespace WB.Core.SharedKernels.Enumerator.ViewModels.InterviewDetails.Questions
             }
         }
 
-        private string GetPictureFileName() => $"{this.variableName}{string.Join("-", this.questionIdentity.RosterVector)}.jpg";
+        private string GetPictureFileName() => $"{this.variableName}_{this.questionIdentity.RosterVector}-{Guid.NewGuid().GetHashCode()}.jpg";
 
         public void Dispose()
         {
