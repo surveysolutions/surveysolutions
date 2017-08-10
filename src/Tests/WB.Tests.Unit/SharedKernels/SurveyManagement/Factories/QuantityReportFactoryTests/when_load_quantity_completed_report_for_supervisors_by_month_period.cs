379d4c5dd37baf52b5fdbed3d3dd40a208d448a7
Andrii Kozhyn <hedgic@gmail.com>
2017-08-10 13:12:35 +0300

    KP-9843: Made speed/quantity reports to count backwards

--- src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/Factories/QuantityReportFactoryTests/when_load_quantity_completed_report_for_supervisors_by_month_period.cs
+++ src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/Factories/QuantityReportFactoryTests/when_load_quantity_completed_report_for_supervisors_by_month_period.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 using System.Linq;
-using Machine.Specifications;
+using FluentAssertions;
+using NUnit.Framework;
 using WB.Core.BoundedContexts.Headquarters.Views.DataExport;
 using WB.Core.BoundedContexts.Headquarters.Views.Interview;
 using WB.Core.BoundedContexts.Headquarters.Views.Reposts.Factories;
 using WB.Core.BoundedContexts.Headquarters.Views.Reposts.InputModels;
 using WB.Core.BoundedContexts.Headquarters.Views.Reposts.Views;
-using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
 using WB.Tests.Abc;
 using WB.Tests.Abc.Storage;
 
@@ -14,57 +14,79 @@ namespace WB.Tests.Unit.SharedKernels.SurveyManagement.Factories.QuantityReportF
 {
     internal class when_load_quantity_completed_report_for_supervisors_by_month_period : QuantityReportFactoryTestContext
     {
-        Establish context = () =>
+        private QuantityBySupervisorsReportInputModel input;
+        private TestInMemoryWriter<InterviewStatusTimeSpans> interviewStatusTimeSpansStorage;
+
+        private QuantityReportFactory quantityReportFactory;
+        private QuantityByResponsibleReportView result;
+
+        [OneTimeSetUp]
+        public void Context()
         {
-            input = CreateQuantityBySupervisorsReportInputModel(period: "m");
+            input = CreateQuantityBySupervisorsReportInputModel("m");
             input.InterviewStatuses = new[] {InterviewExportedAction.Completed};
             var user = Guid.NewGuid();
 
             interviewStatusTimeSpansStorage = new TestInMemoryWriter<InterviewStatusTimeSpans>();
             interviewStatusTimeSpansStorage.Store(
-                Create.Entity.InterviewStatusTimeSpans(questionnaireId: input.QuestionnaireId,
-                    questionnaireVersion: input.QuestionnaireVersion,
+                Create.Entity.InterviewStatusTimeSpans(input.QuestionnaireId,
+                    input.QuestionnaireVersion,
                     timeSpans: new[]
                     {
                         Create.Entity.TimeSpanBetweenStatuses(supervisorId: user,
-                            timestamp: input.From.Date.AddHours(1), endStatus:InterviewExportedAction.Completed),
+                            timestamp: input.From.Date.AddHours(-1), endStatus: InterviewExportedAction.Completed),
                         Create.Entity.TimeSpanBetweenStatuses(supervisorId: user,
-                            timestamp: input.From.Date.AddMonths(2), endStatus:InterviewExportedAction.Completed),
+                            timestamp: input.From.Date.AddMonths(-2), endStatus: InterviewExportedAction.Completed),
                         Create.Entity.TimeSpanBetweenStatuses(supervisorId: user,
-                            timestamp: input.From.Date.AddMonths(-2), endStatus:InterviewExportedAction.Completed)
+                            timestamp: input.From.Date.AddMonths(2), endStatus: InterviewExportedAction.Completed)
                     }), "2");
 
-            quantityReportFactory = CreateQuantityReportFactory(interviewStatusTimeSpansStorage: interviewStatusTimeSpansStorage);
-        };
+            quantityReportFactory =
+                CreateQuantityReportFactory(interviewStatusTimeSpansStorage: interviewStatusTimeSpansStorage);
 
-        Because of = () =>
             result = quantityReportFactory.Load(input);
+        }
 
-        It should_return_one_row = () =>
-            result.Items.Count().ShouldEqual(1);
+        [Test]
+        public void should_return_one_row()
+        {
+            result.Items.Should().HaveCount(1);
+        }
 
-        It should_return_first_row_with_1_interview_at_first_period_and_zero_interviews_at_second = () =>
-            result.Items.First().QuantityByPeriod.ShouldEqual(new long[] { 1, 0 });
+        [Test]
+        public void should_return_first_row_with_1_interview_at_first_period_and_zero_interviews_at_second()
+        {
+            result.Items.First().QuantityByPeriod.Should().Equal(1, 0);
+        }
 
-        It should_return_first_row_with_1_in_Total = () =>
-            result.Items.First().Total.ShouldEqual(1);
+        [Test]
+        public void should_return_first_row_with_1_in_Total()
+        {
+            Assert.That(result.Items.First().Total, Is.EqualTo(1));
+        }
 
-        It should_return_first_row_with_0_5_in_Average = () =>
-           result.Items.First().Average.ShouldEqual(0.5);
+        [Test]
+        public void should_return_first_row_with_0_5_in_Average()
+        {
+            Assert.That(result.Items.First().Average, Is.EqualTo(0.5));
+        }
 
-        It should_return_total_row_with_1_interview_at_first_period_and_zero_interviews_at_second = () =>
-          result.TotalRow.QuantityByPeriod.ShouldEqual(new long[] { 1, 0 });
+        [Test]
+        public void should_return_total_row_with_1_interview_at_first_period_and_zero_interviews_at_second()
+        {
+            result.TotalRow.QuantityByPeriod.Should().Equal(1, 0);
+        }
 
-        It should_return_total_row_with_1_in_Total = () =>
-            result.TotalRow.Total.ShouldEqual(1);
+        [Test]
+        public void should_return_total_row_with_1_in_Total()
+        {
+            Assert.That(result.TotalRow.Total, Is.EqualTo(1));
+        }
 
-        It should_return_total_row_with_0_5_in_Average = () =>
-           result.TotalRow.Average.ShouldEqual(0.5);
-
-        private static QuantityReportFactory quantityReportFactory;
-        private static QuantityBySupervisorsReportInputModel input;
-        private static QuantityByResponsibleReportView result;
-        private static TestInMemoryWriter<InterviewStatusTimeSpans> interviewStatusTimeSpansStorage;
-        private static Guid supervisorId = Guid.Parse("11111111111111111111111111111111");
+        [Test]
+        public void should_return_total_row_with_0_5_in_Average()
+        {
+            Assert.That(result.TotalRow.Average, Is.EqualTo(0.5));
+        }
     }
 }
