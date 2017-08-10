1b63d76a2cca0ed96e77572f20e74b097f5b4963
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2017-08-10 14:16:26 +0300

    KP-9843: Fixed tests for reports speed/quantity

--- src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/Factories/QuantityReportFactoryTests/when_load_quantity_report_for_interviewers_where_one_out_of_three_periods_is_in_future.cs
+++ src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/Factories/QuantityReportFactoryTests/when_load_quantity_report_for_interviewers_where_one_out_of_three_periods_is_in_future.cs
@@ -1,39 +1,37 @@
-﻿using System;
-using System.Linq;
-using Machine.Specifications;
+using System;
+using FluentAssertions;
 using WB.Core.BoundedContexts.Headquarters.Views.Reposts.Factories;
 using WB.Core.BoundedContexts.Headquarters.Views.Reposts.InputModels;
 using WB.Core.BoundedContexts.Headquarters.Views.Reposts.Views;
-using WB.Core.SharedKernels.DataCollection.Views;
 
 namespace WB.Tests.Unit.SharedKernels.SurveyManagement.Factories.QuantityReportFactoryTests
 {
     internal class when_load_quantity_report_for_interviewers_where_one_out_of_three_periods_is_in_future : QuantityReportFactoryTestContext
     {
-        Establish context = () =>
-        {
+        [NUnit.Framework.OneTimeSetUp] public void context () {
             input = CreateQuantityByInterviewersReportInputModel(supervisorId: supervisorId, columnCount: 3,
-                from: now.AddDays(-1));
+                from: now.AddDays(1));
 
             quantityReportFactory = CreateQuantityReportFactory();
-        };
+            BecauseOf();
+        }
 
-        Because of = () =>
+        private void BecauseOf() =>
             result = quantityReportFactory.Load(input);
-        
-        It should_return_2_periods= () =>
-            result.DateTimeRanges.Length.ShouldEqual(2);
 
-        It should_return_today_as_a_period = () =>
-            result.DateTimeRanges[1].From.Date.ShouldEqual(now.Date);
+        [NUnit.Framework.Test] public void should_return_2_periods() =>
+            result.DateTimeRanges.Should().HaveCount(2);
 
-        It should_return_yesterday_as_a_period = () =>
-            result.DateTimeRanges[0].From.Date.ShouldEqual(now.Date.AddDays(-1));
+        [NUnit.Framework.Test] public void should_return_today_as_a_period () =>
+            result.DateTimeRanges[1].From.Date.Should().Be(now.Date);
 
-        private static QuantityReportFactory quantityReportFactory;
-        private static QuantityByInterviewersReportInputModel input;
-        private static QuantityByResponsibleReportView result;
-        private static Guid supervisorId = Guid.Parse("11111111111111111111111111111111");
-        private static DateTime now = DateTime.Now;
+        [NUnit.Framework.Test] public void should_return_yesterday_as_a_period () =>
+            result.DateTimeRanges[0].From.Date.Should().Be(now.Date.AddDays(-1));
+
+        private QuantityReportFactory quantityReportFactory;
+        private QuantityByInterviewersReportInputModel input;
+        private QuantityByResponsibleReportView result;
+        private readonly Guid supervisorId = Guid.Parse("11111111111111111111111111111111");
+        private DateTime now = DateTime.Now;
     }
 }
