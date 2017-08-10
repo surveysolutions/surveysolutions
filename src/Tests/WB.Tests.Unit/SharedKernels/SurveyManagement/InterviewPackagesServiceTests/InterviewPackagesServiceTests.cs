e1672a1ed567dbe45c6aaee1ac82d5334ef08891
Roman Lytvyn <litvin.roma@gmail.com>
2017-08-10 10:12:28 +0300

    KP-9672 HQ: Completed interview goes to unhandled if template was deleted. [REVERTED]

--- src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/InterviewPackagesServiceTests/InterviewPackagesServiceTests.cs
+++ src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/InterviewPackagesServiceTests/InterviewPackagesServiceTests.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Threading;
-using Machine.Specifications;
-using Main.Core.Events;
 using Moq;
 using NUnit.Framework;
 using WB.Core.BoundedContexts.Headquarters;
@@ -12,13 +8,8 @@ using WB.Core.GenericSubdomains.Portable.Services;
 using WB.Core.Infrastructure.CommandBus;
 using WB.Core.Infrastructure.FileSystem;
 using WB.Core.Infrastructure.PlainStorage;
-using WB.Core.SharedKernel.Structures.Synchronization;
-using WB.Core.SharedKernels.DataCollection.Commands.Interview;
-using WB.Core.SharedKernels.DataCollection.Exceptions;
 using WB.Core.SharedKernels.DataCollection.Services;
-using WB.Core.SharedKernels.DataCollection.ValueObjects.Interview;
 using WB.Tests.Abc.Storage;
-using It = Machine.Specifications.It;
 
 namespace WB.Tests.Unit.SharedKernels.SurveyManagement.InterviewPackagesServiceTests
 {
@@ -42,46 +33,6 @@ namespace WB.Tests.Unit.SharedKernels.SurveyManagement.InterviewPackagesServiceT
                 commandService: commandService ?? Mock.Of<ICommandService>(),
                 uniqueKeyGenerator: Mock.Of<IInterviewUniqueKeyGenerator>(),
                 interviews: new TestInMemoryWriter<InterviewSummary>());
-        }
-
-        [Test]
-        public void When_processing_package_and_questionnaire_does_not_exist_Than_broken_package_should_not_be_added()
-        {
-            //arrange
-            string decompressedEvents = "decompressed events";
-
-            var serializer =
-                Mock.Of<IJsonAllTypesSerializer>(x => x.Deserialize<SyncItem>(Moq.It.IsAny<string>()) == new SyncItem() &&
-                                                      x.Deserialize<InterviewMetaInfo>(Moq.It.IsAny<string>()) == new InterviewMetaInfo { Status = 0 } &&
-                                                      x.Deserialize<AggregateRootEvent[]>(decompressedEvents) == new AggregateRootEvent[0]);
-            var syncSettings = Mock.Of<SyncSettings>(x => x.UseBackgroundJobForProcessingPackages == false);
-
-            var brokenPackagesStorage = new Mock<TestPlainStorage<BrokenInterviewPackage>>();
-
-            var mockOfCommandService = new Mock<ICommandService>();
-            mockOfCommandService.Setup(x => x.Execute(Moq.It.IsAny<SynchronizeInterviewEventsCommand>(),
-                Moq.It.IsAny<string>())).Throws(new InterviewException("Questionnaire deleted", null, InterviewDomainExceptionType.QuestionnaireIsMissing));
-
-            var interviewPackagesService = CreateInterviewPackagesService(
-                serializer: serializer, 
-                brokenInterviewPackageStorage: brokenPackagesStorage.Object,
-                commandService: mockOfCommandService.Object,
-                syncSettings: syncSettings);
-
-            //act
-            interviewPackagesService.StoreOrProcessPackage(new InterviewPackage
-            {
-                InterviewId = Guid.Parse("11111111111111111111111111111111"),
-                QuestionnaireId = Guid.Parse("22222222222222222222222222222222"),
-                QuestionnaireVersion = 111,
-                ResponsibleId = Guid.Parse("33333333333333333333333333333333"),
-                InterviewStatus = InterviewStatus.Restarted,
-                IsCensusInterview = false,
-                Events = "compressed serialized events"
-            });
-
-            //assert
-            brokenPackagesStorage.Verify(x => x.Store(Moq.It.IsAny<BrokenInterviewPackage>(), null), Times.Never);
         }
     }
 }
