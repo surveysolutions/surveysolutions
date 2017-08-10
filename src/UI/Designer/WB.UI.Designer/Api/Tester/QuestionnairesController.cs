c6917fd627a897123eeddec25e4caa8d8c284d7a
Victor Hladkikh <me@alfeg.net>
2017-08-10 10:03:24 +0300

    KP-9815 Tester app sign in using email # Code review fix

--- src/UI/Designer/WB.UI.Designer/Api/Tester/QuestionnairesController.cs
+++ src/UI/Designer/WB.UI.Designer/Api/Tester/QuestionnairesController.cs
@@ -111,7 +111,6 @@ namespace WB.UI.Designer.Api.Tester
 
             var userId = this.userHelper.WebUser.UserId;
             var isAdmin = this.userHelper.WebUser.IsAdmin;
-            var email = this.userHelper.WebUser.MembershipUser.Email;
             var userName = this.userHelper.WebUser.UserName;
             
             var questionnaireViews = this.viewFactory.GetUserQuestionnaires(userId, isAdmin, pageIndex, pageSize);
@@ -122,7 +121,7 @@ namespace WB.UI.Designer.Api.Tester
                 Title = questionnaire.Title,
                 LastEntryDate = questionnaire.LastEntryDate,
                 Owner = questionnaire.CreatorName,
-                IsOwner = questionnaire.CreatorName == userName || questionnaire.CreatorName == email,
+                IsOwner = questionnaire.CreatorName == userName,
                 IsPublic = questionnaire.IsPublic || isAdmin,
                 IsShared = questionnaire.SharedPersons.Any(sharedPerson => sharedPerson.UserId == userId)
             });
