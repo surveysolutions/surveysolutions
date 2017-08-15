9e1abf0bdbb7bb21832f89bea357945a00c87692
Victor Gladkikh <me@alfeg.net>
2017-08-15 13:30:20 +0300

    One click build for all assets

---
+++ build.all.deps.bat
@@ -0,0 +1,16 @@
+echo Build WB.UI.Headquarters.Interview
+pushd src\UI\Headquarters\WB.UI.Headquarters.Interview
+call yarn && call yarn run production
+popd
+
+echo Build WB.UI.Headquarters\Dependencies
+pushd src\UI\Headquarters\WB.UI.Headquarters\Dependencies
+call yarn 
+call node_modules\.bin\bower install
+call node_modules\.bin\gulp --production
+popd
+
+echo Build WB.UI.Headquarters\HqApp
+pushd src\UI\Headquarters\WB.UI.Headquarters\HqApp
+call yarn && call yarn run production
+popd
