fe35703f1f44680a48e5c6fafc10da540e951094
Pavel Stovpivskyi <sby@rambler.ru>
2017-08-15 17:09:47 +0300

    added support for assignments

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Views/Reposts/Factories/CountDaysOfInterviewInStatusReport.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Views/Reposts/Factories/CountDaysOfInterviewInStatusReport.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using WB.Core.BoundedContexts.Headquarters.Assignments;
 using WB.Core.BoundedContexts.Headquarters.Views.DataExport;
 using WB.Core.BoundedContexts.Headquarters.Views.Interview;
 using WB.Core.BoundedContexts.Headquarters.Views.Reports.InputModels;
 using WB.Core.BoundedContexts.Headquarters.Views.Reports.Views;
+using WB.Core.Infrastructure.PlainStorage;
 using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
 
 
@@ -13,10 +15,13 @@ namespace WB.Core.BoundedContexts.Headquarters.Views.Reports.Factories
     public class CountDaysOfInterviewInStatusReport : ICountDaysOfInterviewInStatusReport
     {
         private readonly IQueryableReadSideRepositoryReader<InterviewStatuses> interviewStatusesStorage;
+        private readonly IPlainStorageAccessor<Assignment> assignmentsStorage;
 
-        public CountDaysOfInterviewInStatusReport(IQueryableReadSideRepositoryReader<InterviewStatuses> interviewStatusesStorage)
+        public CountDaysOfInterviewInStatusReport(IQueryableReadSideRepositoryReader<InterviewStatuses> interviewStatusesStorage,
+            IPlainStorageAccessor<Assignment> assignmentsStorage)
         {
             this.interviewStatusesStorage = interviewStatusesStorage;
+            this.assignmentsStorage = assignmentsStorage;
         }
 
         class CounterObject
@@ -47,21 +52,53 @@ namespace WB.Core.BoundedContexts.Headquarters.Views.Reports.Factories
                         Status = g.Key.Status,
                         StatusDate = g.Key.Date,
                         InterviewsCount = g.Count(),
-                    }
-                    ).ToList();
+                    });
 
                 return statusWithTime;
             });
 
+            var assignmentsDatesAndCounts = assignmentsStorage.Query(_ =>
+            {
+                var filteredAssignments = _;
+
+                if (input.TemplateId.HasValue && input.TemplateVersion.HasValue)
+                {
+                    filteredAssignments = filteredAssignments.Where(x
+                        => x.QuestionnaireId.QuestionnaireId == input.TemplateId.Value
+                           && x.QuestionnaireId.Version == input.TemplateVersion.Value);
+                }
+
+                var statusWithTime = (from f in filteredAssignments
+                    group f by new { f.CreatedAtUtc } into g
+                    select new CounterObject
+                    {
+                        StatusDate = g.Key.CreatedAtUtc,
+                        InterviewsCount = g.Count(),
+                    }
+                );
+
+                return statusWithTime;
+            });
 
             Dictionary<DateTime, Dictionary<InterviewExportedAction, int>> dictStatistics = new Dictionary<DateTime, Dictionary<InterviewExportedAction, int>>();
 
             foreach (var counterObject in datesAndStatuses.AsQueryable())
             {
+                if (counterObject.Status == InterviewExportedAction.InterviewerAssigned)
+                    continue; // ignore, this statistics will be counted in assignments
+
                 if (!dictStatistics.ContainsKey(counterObject.StatusDate))
                     dictStatistics[counterObject.StatusDate] = new Dictionary<InterviewExportedAction, int>();
 
                 dictStatistics[counterObject.StatusDate][counterObject.Status] = counterObject.InterviewsCount;
+            }
+
+            foreach (var counterObject in assignmentsDatesAndCounts.AsQueryable())
+            {
+                if (!dictStatistics.ContainsKey(counterObject.StatusDate))
+                    dictStatistics[counterObject.StatusDate] = new Dictionary<InterviewExportedAction, int>();
+
+                dictStatistics[counterObject.StatusDate][InterviewExportedAction.InterviewerAssigned] = counterObject.InterviewsCount;
             }
 
             var utcNow = DateTime.UtcNow;
@@ -84,7 +121,7 @@ namespace WB.Core.BoundedContexts.Headquarters.Views.Reports.Factories
                     };
             }
 
-            var ranges = new List<int?> { 1, 2, 3, 4, 5, 10, 15, 20, 30 };
+            var ranges = new List<int?> { 0, 1, 2, 3, 4, 5, 10, 15, 20, 30 };
             var defaultGroups =
                 from row in rows
                 group row by ranges.LastOrDefault(range => row.DaysCount >= range) into g
