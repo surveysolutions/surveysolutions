c81a9aaa62a88cbddfe0a676eba76f7b44b505d9
Andrii Kozhyn <hedgic@gmail.com>
2017-08-15 15:26:49 +0300

    KP-9843: Some redirections to interviewers list added

--- src/UI/Headquarters/WB.UI.Headquarters/HqApp/app/Views/Reports/InterviewersAndDevices.vue
+++ src/UI/Headquarters/WB.UI.Headquarters/HqApp/app/Views/Reports/InterviewersAndDevices.vue
@@ -11,6 +11,7 @@ export default {
     },
     computed: {
         tableOptions() {
+            var self = this;
             return {
                 deferLoading: 0,
                 columns: [
@@ -25,14 +26,26 @@ export default {
                         name: "NeverSynchedCount",
                         "class": "type-numeric",
                         title: this.$t("DevicesInterviewers.NeverSynchedCount"),
-                        orderable: true
+                        orderable: true,
+                        render: function(data, type, row) {
+                            if(data === 0) return `<span>${data}</span>`;
+                            else {
+                                return `<a href='${self.$config.interviewersBaseUrl}?supervisor=${row.teamName}&InterviewerOptionFilter=NotSynced'>${data}</a>`;
+                            }
+                        }
                     },
                     {
                         data: "outdatedCount",
                         name: "OutdatedCount",
                         "class": "type-numeric",
                         orderable: true,
-                        title: this.$t("DevicesInterviewers.OldInterviewerVersion")
+                        title: this.$t("DevicesInterviewers.OldInterviewerVersion"),
+                        render: function(data, type, row) {
+                            if(data === 0) return `<span>${data}</span>`;
+                            else {
+                                return `<a href='${self.$config.interviewersBaseUrl}?supervisor=${row.teamName}&InterviewerOptionFilter=Outdated'>${data}</a>`;
+                            }
+                        }
                     },
                     {
                         data: "lowStorageCount",
--- src/UI/Headquarters/WB.UI.Headquarters/Models/Reports/DevicesInterviewersModel.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/Models/Reports/DevicesInterviewersModel.cs
@@ -9,5 +9,7 @@ namespace WB.UI.Headquarters.Models.Reports
         public string DataUrl { get; set; }
 
         public TranslationModel Resources { get; set; }
+
+        public string InterviewersBaseUrl { get; set; }
     }
 }
