9db6b8d7f5bfba57bf2cb6707a054fac2c19d92d
Pavel Stovpivskyi <630198+spuv@users.noreply.github.com>
2017-08-15 19:58:40 +0300

    moved script to sql file

---
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Views/Reposts/Factories/CountDaysOfInterviewInStatusReport.sql
@@ -0,0 +1,9 @@
+﻿select status, timestamp as StatusDate, COUNT(*) as InterviewsCount from (
+	SELECT status, cast(timestamp as date), row_number() over (partition by "interviewid" order by position desc) rnk
+		FROM readside.interviewcommentedstatuses ics
+			INNER JOIN readside.interviewstatuses ist ON ics.interviewid = ist.id
+		WHERE (ist.questionnaireid = @questionnaireid OR @questionnaireid is NULL)
+		  AND (ist.questionnaireversion = @questionnaireversion OR @questionnaireversion is NULL)
+	) tmp 
+where rnk = 1
+GROUP BY status, timestamp
