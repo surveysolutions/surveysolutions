d28218d1de5147c95bc01c5b6fb49e6db7f3d263
Victor Hladkikh <me@alfeg.net>
2017-08-15 14:20:31 +0300

    KP-9848 Add api for interview creation

---
+++ src/UI/Headquarters/WB.UI.Headquarters/API/PublicApi/Models/CreateInterviewRequest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace WB.UI.Headquarters.API.PublicApi.Models
+{
+    public class CreateInterviewRequest
+    {
+        /// <summary>
+        /// Assignment Id
+        /// </summary>
+        public int AssignmentId { get; set; }
+    }
+
+    public class CreateInterviewResult
+    {
+        public int AssignmentId { get; set; }
+        public Guid InterviewId { get; set; }
+        public string InterviewKey { get; set; }
+        public string AssignedTo { get; set; }
+        public Guid AssignedToId { get; set; }
+    }
+}
