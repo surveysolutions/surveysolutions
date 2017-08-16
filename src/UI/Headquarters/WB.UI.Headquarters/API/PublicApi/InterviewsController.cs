5a2b7813bfed2c480c08b71ff016dc9fb175e0e7
Victor Hladkikh <me@alfeg.net>
2017-08-16 12:47:54 +0300

    KP-9848 Add api for interview creation # Code review fix

--- src/UI/Headquarters/WB.UI.Headquarters/API/PublicApi/InterviewsController.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/API/PublicApi/InterviewsController.cs
@@ -217,17 +217,19 @@ namespace WB.UI.Headquarters.API.PublicApi
         public CreateInterviewResult CreateInterview(CreateInterviewRequest request)
         {
             var assignment = this.assignmentStorageAccessor.GetById(request.AssignmentId)
-                ?? throw new InvalidOperationException($"Cannot find assignment with id: {request.AssignmentId}");
+                             ?? throw new HttpResponseException(HttpStatusCode.NotFound);
 
             if (assignment.InterviewsNeeded.HasValue && assignment.InterviewsNeeded <= 0)
             {
-                throw new InvalidOperationException($"Cannot create more interviews from this assignmentId: {request.AssignmentId}");
+                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.BadRequest,
+                    $@"Cannot create more interviews from this assignmentId: {request.AssignmentId}"));
             }
 
             var interviewer = this.userViewFactory.GetUser(new UserViewInputModel(assignment.ResponsibleId));
 
             if (!interviewer.IsInterviewer())
-                throw new InvalidOperationException($"Assignment {assignment.Id} has responsible that is not an interviewer. Interview cannot be created");
+                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.BadRequest,
+                    $@"Assignment {assignment.Id} has responsible that is not an interviewer. Interview cannot be created"));
 
             var interviewId = Guid.NewGuid();
 
