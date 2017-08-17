745a368309a0a2a72dc9f5679399db1026751717
Pavel Stovpivskyi <sby@rambler.ru>
2017-08-17 19:28:27 +0300

    KP-9818 Get status from interview summory

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Views/Reposts/Factories/CountDaysOfInterviewInStatusReport.sql
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Views/Reposts/Factories/CountDaysOfInterviewInStatusReport.sql
@@ -1,9 +1,16 @@
 ﻿select status, timestamp as StatusDate, COUNT(*) as InterviewsCount from (
-	SELECT status, cast(timestamp as date), row_number() over (partition by "interviewid" order by position desc) rnk
+	SELECT isum.status, cast(timestamp as date), row_number() over (partition by ics."interviewid" order by position desc) rnk
 		FROM readside.interviewcommentedstatuses ics
 			INNER JOIN readside.interviewstatuses ist ON ics.interviewid = ist.id
-		WHERE (ist.questionnaireid = @questionnaireid OR @questionnaireid is NULL)
-		  AND (ist.questionnaireversion = @questionnaireversion OR @questionnaireversion is NULL)
+			INNER JOIN readside.interviewsummaries isum ON CAST(ics.interviewid as uuid) = isum.interviewid
+		WHERE 
+		(
+		   (isum.status = 100 /*Completed*/ AND ics.status = 3 /*Completed*/ )
+		   OR (isum.status = 120 /*ApprovedBySupervisor*/ AND ics.status = 5 /* ApprovedBySupervisor */)
+		   OR (isum.status = 65  /*RejectedBySupervisor*/ AND ics.status = 7 /* RejectedBySupervisor */)
+		)
+		AND (ist.questionnaireid = @questionnaireid OR @questionnaireid is NULL)
+		AND (ist.questionnaireversion = @questionnaireversion OR @questionnaireversion is NULL)
 	) tmp 
 where rnk = 1
 GROUP BY status, timestamp
