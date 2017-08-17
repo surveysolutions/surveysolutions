47261003f9b58f9f495faaef4873da6ab09fcf1e
Andrii Kozhyn <hedgic@gmail.com>
2017-08-17 10:49:06 +0300

    KP-9819: Fixed usage of different DbContexts for queries

---
+++ src/Tests/WB.Tests.Abc/Storage/QueryableDbSetMock.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using Moq;
+
+namespace WB.Tests.Abc.Storage
+{
+    public class QueryableDbSetMock
+    {
+        public static IDbSet<T> GetQueryableMockDbSet<T>(params T[] sourceList) where T : class
+        {
+            var queryable = sourceList.AsQueryable();
+
+            var dbSet = new Mock<IDbSet<T>>();
+            dbSet.As<IQueryable<T>>().Setup(m => m.Provider).Returns(queryable.Provider);
+            dbSet.As<IQueryable<T>>().Setup(m => m.Expression).Returns(queryable.Expression);
+            dbSet.As<IQueryable<T>>().Setup(m => m.ElementType).Returns(queryable.ElementType);
+            dbSet.As<IQueryable<T>>().Setup(m => m.GetEnumerator()).Returns(queryable.GetEnumerator());
+
+            return dbSet.Object;
+        }
+    }
+}
