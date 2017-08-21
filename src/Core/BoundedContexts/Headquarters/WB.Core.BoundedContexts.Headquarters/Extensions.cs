10a50dd2a34496ee577e90bb930277666a75c53b
Victor Gladkikh <me@alfeg.net>
2017-08-21 19:00:51 +0300

    KP-9900 Wrong assignment dates # fix for interviews list

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Extensions.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Extensions.cs
@@ -6,7 +6,7 @@ namespace WB.Core.BoundedContexts.Headquarters
     public static class Extensions
     {
         public static string ClientDateTimeFormat = "MMM DD, YYYY HH:mm";
-        public static string ServerDateTimeFormat = "MMM DD, yyy HH:mm";
+        public static string ServerDateTimeFormat = "MMM dd, yyy HH:mm";
 
         public static string FormatDateWithTime(this DateTime dateTime) 
             => dateTime.ToString(ServerDateTimeFormat, CultureInfo.CurrentUICulture);
