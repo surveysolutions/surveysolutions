556eaf30a999a143331605cfcec0dcfbe5fe639e
Victor Hladkikh <me@alfeg.net>
2017-08-21 16:20:18 +0300

    KP-9900 Wrong assignment dates

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Extensions.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Extensions.cs
@@ -5,8 +5,8 @@ namespace WB.Core.BoundedContexts.Headquarters
 {
     public static class Extensions
     {
-        public static string ClientDateTimeFormat = "MMM d, YYYY HH:mm";
-        public static string ServerDateTimeFormat = "MMM d, yyy HH:mm";
+        public static string ClientDateTimeFormat = "MMM DD, YYYY HH:mm";
+        public static string ServerDateTimeFormat = "MMM DD, yyy HH:mm";
 
         public static string FormatDateWithTime(this DateTime dateTime) 
             => dateTime.ToString(ServerDateTimeFormat, CultureInfo.CurrentUICulture);
