eef01e499fc438f8deaa678549c99f7f0a50fde6
Andrii Kozhyn <hedgic@gmail.com>
2017-08-22 18:24:26 +0300

    KP-9911: Fixed query by application version

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Views/User/InterviewerIssuesConstants.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Views/User/InterviewerIssuesConstants.cs
@@ -2,7 +2,7 @@
 {
     public class InterviewerIssuesConstants
     {
-        public static readonly int LowMemoryInBytesSize = 1024 * 100;
+        public static readonly int LowMemoryInBytesSize = 1024 * 1024 * 100;
         public static readonly int MinAndroidSdkVersion = 20;
         public static readonly int MinutesForWrongTime = 10;
     }
