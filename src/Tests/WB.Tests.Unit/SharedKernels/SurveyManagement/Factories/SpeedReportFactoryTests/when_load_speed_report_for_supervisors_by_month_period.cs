e106570c1b846dcf91db15d91c73764dd73a7f20
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2017-08-22 17:02:07 -0400

    KP-9894 Average interview duration report should take into consideration only the first complete [fix tests]

--- src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/Factories/SpeedReportFactoryTests/when_load_speed_report_for_supervisors_by_month_period.cs
+++ src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/Factories/SpeedReportFactoryTests/when_load_speed_report_for_supervisors_by_month_period.cs
@@ -48,7 +48,7 @@ namespace WB.Tests.Unit.SharedKernels.SurveyManagement.Factories.SpeedReportFact
             result.Items.Count().ShouldEqual(1);
 
         It should_return_first_row_with_35_minutes_per_interview_at_first_period_and_null_minutes_per_interview_at_second = () =>
-            result.Items.First().SpeedByPeriod.ShouldEqual(new double?[] { null, 35 });
+            result.Items.First().SpeedByPeriod.ShouldEqual(new double?[] { 35, null });
 
         It should_return_first_row_with_35_minutes_in_Total = () =>
             result.Items.First().Total.ShouldEqual(35);
