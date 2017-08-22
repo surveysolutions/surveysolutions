fd24272852332c293348cc09a9322973b5f49da9
Pavel Stovpivskyi <sby@rambler.ru>
2017-08-22 15:00:39 +0300

    Added index

---
+++ src/UI/Headquarters/WB.UI.Headquarters/Migrations/ReadSide/M001-100/M013_AddStatusIndexToStatusHistory.cs
@@ -0,0 +1,19 @@
+﻿using System.Data;
+using FluentMigrator;
+
+namespace WB.UI.Headquarters.Migrations.ReadSide
+{
+    [Migration(13)]
+    public class M013_AddStatusIndexToStatusHistory : Migration
+    {
+        public override void Up()
+        {
+            Create.Index("interviewcommentedstatuses_status_indx").OnTable("interviewcommentedstatuses").OnColumn("status");
+        }
+
+        public override void Down()
+        {
+            Delete.Index("interviewcommentedstatuses_status_indx");
+        }
+    }
+}
