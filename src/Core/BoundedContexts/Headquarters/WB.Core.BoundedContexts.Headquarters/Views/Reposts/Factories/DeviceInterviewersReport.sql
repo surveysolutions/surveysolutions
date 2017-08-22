a18cb453466d8c3c1194d7814250d390993a78d5
Andrii Kozhyn <hedgic@gmail.com>
2017-08-22 16:54:14 +0300

    KP-9911: Fixed inconsistency between report and filtered users

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Views/Reposts/Factories/DeviceInterviewersReport.sql
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Views/Reposts/Factories/DeviceInterviewersReport.sql
@@ -3,7 +3,8 @@
        SUM(case when up."DeviceId" IS NULL then 1 else 0 end) NeverSynchedCount, 
        SUM(case when up."DeviceAppBuildVersion" IS NOT NULL AND up."DeviceAppBuildVersion" < @latestAppBuildVersion then 1 else 0 end) OutdatedCount, 
        SUM(case when lastSync."StorageFreeInBytes" < @neededFreeStorageInBytes then 1 else 0 end) LowStorageCount, 
-       SUM(case when DATE_PART('minute', lastSync."DeviceDate" - lastSync."SyncDate") > @minutesMismatch then 1 else 0 end) WrongDateOnTabletCount,
+       SUM(case when lastSync."DeviceDate" = '0001-01-01T00:00:00' OR 
+					 abs(extract(epoch from date_trunc('minute', lastSync."SyncDate") - date_trunc('minute', lastSync."DeviceDate"))) / 60 > @minutesMismatch then 1 else 0 end) WrongDateOnTabletCount,
        SUM(case when lastSync."AndroidSdkVersion" < @targetAndroidSdkVersion then 1 else 0 end) OldAndroidCount,
        SUM(case when anySync."InterviewerId" IS NULL then 1 else 0 end) NeverUploadedCount,
        SUM(case when wasReassign."InterviewerId" IS NOT NULL then 1 else 0 end) ReassignedCount,
@@ -19,20 +20,20 @@ from users.users u
 			    FROM plainstore.devicesyncinfo dsi GROUP BY dsi."InterviewerId") AS p2
 		  ON (dsi1."Id" = p2.maxid)
 		  LEFT JOIN  plainstore.devicesyncstatistics dss on dsi1."StatisticsId" = dss."Id"
-		) AS lastSync ON lastSync."InterviewerId" = u."Id"::uuid 
+		) AS lastSync ON lastSync."InterviewerId" = u."Id" 
 
 -- find if there was a sync with > 0 uploaded interviews
 LEFT JOIN (SELECT DISTINCT dsi."InterviewerId" FROM plainstore.devicesyncinfo dsi WHERE EXISTS(SELECT 1 FROM plainstore.devicesyncstatistics WHERE dsi."StatisticsId" = "Id" AND "UploadedInterviewsCount" > 0)) 
-	AS anySync ON anySync."InterviewerId" = u."Id"::uuid 
+	AS anySync ON anySync."InterviewerId" = u."Id"
 
 -- find how many interviewers have more than 1 tablet
 LEFT JOIN (SELECT "InterviewerId" FROM plainstore.devicesyncinfo dsi 
 GROUP BY "InterviewerId"
-HAVING COUNT(DISTINCT "DeviceId") > 1) AS wasReassign ON wasReassign."InterviewerId" = u."Id"::uuid 
+HAVING COUNT(DISTINCT "DeviceId") > 1) AS wasReassign ON wasReassign."InterviewerId" = u."Id"
 
 -- find if there was any questionnaire received
 LEFT JOIN (SELECT DISTINCT dsi."InterviewerId" FROM plainstore.devicesyncinfo dsi WHERE EXISTS(SELECT 1 FROM plainstore.devicesyncstatistics WHERE dsi."StatisticsId" = "Id" AND "DownloadedQuestionnairesCount" > 0)) 
-	AS anySyncWithQuestionnaire ON anySyncWithQuestionnaire."InterviewerId" = u."Id"::uuid 
+	AS anySyncWithQuestionnaire ON anySyncWithQuestionnaire."InterviewerId" = u."Id"
 
 -- find supervisor name
 INNER JOIN users.users userNames ON up."SupervisorId" = userNames."Id"
