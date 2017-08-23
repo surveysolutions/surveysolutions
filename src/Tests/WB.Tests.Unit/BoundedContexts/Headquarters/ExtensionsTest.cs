d7f92a659e54ce2e5f5b6c7079898edede3fc322
Victor Gladkikh <me@alfeg.net>
2017-08-23 15:22:34 +0300

    KP-9900 Wrong assignment dates # Code review fix

---
+++ src/Tests/WB.Tests.Unit/BoundedContexts/Headquarters/ExtensionsTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using WB.Core.BoundedContexts.Headquarters;
+
+namespace WB.Tests.Unit.BoundedContexts.Headquarters
+{
+    public class ExtensionsTest
+    {
+        [Test]
+        public void Should_format_date_properly()
+        {
+            // November 22, 1963 at 12:30 p.m.
+            var date = new DateTime(1963, 11, 22, 12, 30, 00);
+            using (new ChangeCurrentCulture(CultureInfo.GetCultureInfo("en-US")))
+            {
+                var formatted = date.FormatDateWithTime();
+                Assert.That(formatted, Is.EqualTo("Nov 22, 1963 12:30"));
+            }
+        }
+    }
+}
