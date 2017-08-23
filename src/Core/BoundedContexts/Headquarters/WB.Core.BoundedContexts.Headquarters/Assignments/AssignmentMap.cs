28525cc8ce848b7efd4b1c8d8439652b01313e9f
Andrii Kozhyn <hedgic@gmail.com>
2017-08-23 10:42:11 +0300

    KP-9908: Fix loading of list with huge assignment

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Assignments/AssignmentMap.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Assignments/AssignmentMap.cs
@@ -32,7 +32,7 @@ namespace WB.Core.BoundedContexts.Headquarters.Assignments
             Set(x => x.InterviewSummaries, set =>
             {
                 set.Key(key => key.Column("assignmentid"));
-                set.Lazy(CollectionLazy.Lazy);
+                set.Lazy(CollectionLazy.Extra);
                 set.Cascade(Cascade.None);
             },
 
