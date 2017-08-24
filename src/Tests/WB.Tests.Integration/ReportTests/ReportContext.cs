8a7f8b88f3e0931e5cf486f460625f27d0f0f43c
Slava Scherbak <slava.scherbak@gmail.com>
2017-08-24 14:49:42 -0400

    KP-9937: fixed broken on empty DB surveys and statuses report.

---
+++ src/Tests/WB.Tests.Integration/ReportTests/ReportContext.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using Moq;
+using NHibernate;
+using Npgsql;
+using NUnit.Framework;
+using WB.Core.BoundedContexts.Headquarters.Mappings;
+using WB.Core.BoundedContexts.Headquarters.Views.Interview;
+using WB.Core.BoundedContexts.Headquarters.Views.Reposts.Factories;
+using WB.Core.GenericSubdomains.Portable;
+using WB.Core.GenericSubdomains.Portable.Services;
+using WB.Infrastructure.Native.Storage;
+using WB.Infrastructure.Native.Storage.Postgre.Implementation;
+
+namespace WB.Tests.Integration.ReportTests
+{
+    internal abstract class ReportContext
+    {
+        internal ReportContext()
+        {
+            Sv = new SvReport(this);
+            Hq = new HqReport(this);
+        }
+
+        public readonly SvReport Sv;
+        public readonly HqReport Hq;
+
+        protected PostgreReadSideStorage<InterviewSummary> CreateInterviewSummaryRepository()
+        {
+            var sessionFactory = IntegrationCreate.SessionFactory(connectionStringBuilder.ConnectionString, new[] { typeof(InterviewSummaryMap), typeof(QuestionAnswerMap), typeof(InterviewStatusesMap) }, true);
+            transactionManager = new CqrsPostgresTransactionManager(sessionFactory ?? Mock.Of<ISessionFactory>());
+
+            pgSqlConnection = new NpgsqlConnection(connectionStringBuilder.ConnectionString);
+            pgSqlConnection.Open();
+
+           return new PostgreReadSideStorage<InterviewSummary>(transactionManager, Mock.Of<ILogger>(), "InterviewId");
+        }
+
+        protected NpgsqlConnection pgSqlConnection;
+        protected CqrsPostgresTransactionManager transactionManager;
+
+        [SetUp]
+        public void Context()
+        {
+            TestConnectionString = ConfigurationManager.ConnectionStrings["TestConnection"].ConnectionString;
+            databaseName = "testdb_" + Guid.NewGuid().FormatGuid();
+            connectionStringBuilder = new NpgsqlConnectionStringBuilder(TestConnectionString)
+            {
+                Database = databaseName
+            };
+
+            using (var connection = new NpgsqlConnection(TestConnectionString))
+            {
+                connection.Open();
+                var command = $@"CREATE DATABASE {databaseName} ENCODING = 'UTF8'";
+                using (var sqlCommand = connection.CreateCommand())
+                {
+                    sqlCommand.CommandText = command;
+                    sqlCommand.ExecuteNonQuery();
+                }
+                connection.Close();
+            }
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            pgSqlConnection.Close();
+
+            using (var connection = new NpgsqlConnection(TestConnectionString))
+            {
+                connection.Open();
+                var command = string.Format(
+                    @"SELECT pg_terminate_backend (pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = '{0}'; DROP DATABASE {0};",
+                    databaseName);
+                using (var sqlCommand = connection.CreateCommand())
+                {
+                    sqlCommand.CommandText = command;
+                    sqlCommand.ExecuteNonQuery();
+                }
+                connection.Close();
+            }
+        }
+
+        protected void ExecuteInCommandTransaction(Action action)
+        {
+            try
+            {
+                transactionManager.BeginCommandTransaction();
+
+                action();
+
+                transactionManager.CommitCommandTransaction();
+            }
+            catch
+            {
+                transactionManager.RollbackCommandTransaction();
+                throw;
+            }
+        }
+
+
+        protected static NpgsqlConnectionStringBuilder connectionStringBuilder;
+        protected static string TestConnectionString;
+        private static string databaseName;
+
+        internal class SvReport
+        {
+            private readonly ReportContext reportContext;
+
+            public SvReport(ReportContext reportContext)
+            {
+                this.reportContext = reportContext;
+            }
+
+            public HeadquartersTeamsAndStatusesReport TeamsAndStatuses(INativeReadSideStorage<InterviewSummary> reader = null)
+            {
+                if (reader == null)
+                {
+                    reader = reportContext.CreateInterviewSummaryRepository();
+                }
+                return new HeadquartersTeamsAndStatusesReport(reader);
+            }
+
+
+            public SurveysAndStatusesReport SurveyAndStatuses(INativeReadSideStorage<InterviewSummary> reader = null)
+            {
+                if (reader == null)
+                {
+                    reader = reportContext.CreateInterviewSummaryRepository();
+                }
+                return new SurveysAndStatusesReport(reader);
+            }
+
+            internal SurveysAndStatusesReport SurveyAndStatuses(List<InterviewSummary> interviews)
+            {
+                var reader = reportContext.CreateInterviewSummaryRepository();
+                reportContext.ExecuteInCommandTransaction(() => interviews.ForEach(x => reader.Store(x, x.InterviewId.FormatGuid())));
+                return SurveyAndStatuses(reader);
+            }
+        }
+
+        internal class HqReport
+        {
+            private readonly ReportContext reportContext;
+
+            public HqReport(ReportContext reportContext)
+            {
+                this.reportContext = reportContext;
+            }
+
+            public SupervisorTeamsAndStatusesReport TeamsAndStatuses(INativeReadSideStorage<InterviewSummary> reader = null)
+            {
+                if (reader == null)
+                {
+                    reader = reportContext.CreateInterviewSummaryRepository();
+                }
+                return new SupervisorTeamsAndStatusesReport(reader);
+            }
+
+            public SurveysAndStatusesReport SurveyAndStatuses(INativeReadSideStorage<InterviewSummary> reader = null)
+            {
+                if (reader == null)
+                {
+                    reader = reportContext.CreateInterviewSummaryRepository();
+                }
+                return new SurveysAndStatusesReport(reader);
+            }
+
+            public SurveysAndStatusesReport SurveyAndStatuses(List<InterviewSummary> interviews)
+            {
+                var reader = reportContext.CreateInterviewSummaryRepository();
+                reportContext.ExecuteInCommandTransaction(() => interviews.ForEach(x => reader.Store(x, x.InterviewId.FormatGuid())));
+                return SurveyAndStatuses(reader);
+            }
+        }
+    }
+}
--- src/Tests/WB.Tests.Integration/ReportTests/TeamsAndStatusesTests/Sv/when_report_requested_by_viewer.cs
+++ src/Tests/WB.Tests.Integration/ReportTests/TeamsAndStatusesTests/Sv/when_report_requested_by_viewer.cs
@@ -9,33 +9,35 @@ using WB.Core.BoundedContexts.Headquarters.Views.Reposts.Views;
 using WB.Core.GenericSubdomains.Portable;
 using WB.Core.Infrastructure.Transactions;
 using WB.Core.SharedKernels.DataCollection.ValueObjects.Interview;
+using WB.Tests.Integration.ReportTests.TeamsAndStatusesTests.Hq;
 
-namespace WB.Tests.Integration.HeadquartersTeamsAndStatusesReportTests
+namespace WB.Tests.Integration.ReportTests.TeamsAndStatusesTests.Sv
 {
-    internal class when_report_requested_by_viewer : HeadquartersTeamsAndStatusesReportContext
+    internal class when_report_requested_by_viewer : TeamsAndStatusesReportContext
     {
         Establish context = () =>
         {
             viewerId = Guid.Parse("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-
+            var responsible = Guid.Parse("BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB");
+            
             List<InterviewSummary> interviews = new List<InterviewSummary>
             {
-                Abc.Create.Entity.InterviewSummary(teamLeadId: viewerId, status: InterviewStatus.Completed),
-                Abc.Create.Entity.InterviewSummary(teamLeadId: viewerId, status: InterviewStatus.Completed),
-                Abc.Create.Entity.InterviewSummary(teamLeadId: Guid.NewGuid(), status: InterviewStatus.Completed),
+                Abc.Create.Entity.InterviewSummary(responsibleId: responsible, teamLeadId: viewerId, status: InterviewStatus.Completed),
+                Abc.Create.Entity.InterviewSummary(responsibleId: responsible, teamLeadId: viewerId, status: InterviewStatus.Completed),
+                Abc.Create.Entity.InterviewSummary(responsibleId: responsible, teamLeadId: Guid.NewGuid(), status: InterviewStatus.Completed),
             };
 
             var repository = CreateInterviewSummaryRepository();
             ExecuteInCommandTransaction(() => interviews.ForEach(x => repository.Store(x, x.InterviewId.FormatGuid())));
 
-            reportFactory = CreateTeamsAndStatusesReport(repository);
+            reportFactory = CreateSvTeamsAndStatusesReport(repository);
         };
 
         Because of = () => report = postgresTransactionManager.ExecuteInQueryTransaction(() => reportFactory.Load(new TeamsAndStatusesInputModel { ViewerId = viewerId }));
 
         It should_count_number_of_interviews_for_teamlead = () => report.Items.First().CompletedCount.ShouldEqual(2);
 
-        static HeadquartersTeamsAndStatusesReport reportFactory;
+        static SupervisorTeamsAndStatusesReport reportFactory;
         static TeamsAndStatusesReportView report;
         static Guid viewerId;
     }
