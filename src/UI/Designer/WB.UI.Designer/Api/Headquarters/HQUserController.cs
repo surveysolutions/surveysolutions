7a4c4bf623c094f8de03795572d5a4e0da9e4043
Slava Scherbak <slava.scherbak@gmail.com>
2017-08-28 17:41:36 -0400

    Added method to return user details for deployment website

--- src/UI/Designer/WB.UI.Designer/Api/Headquarters/HQUserController.cs
+++ src/UI/Designer/WB.UI.Designer/Api/Headquarters/HQUserController.cs
@@ -1,14 +1,24 @@
-﻿using System.Web.Http;
+﻿using System;
+using System.Web.Http;
+using System.Web.Security;
 using WB.UI.Designer.Api.Attributes;
 
 namespace WB.UI.Designer.Api.Headquarters
 {
-    [ApiBasicAuth(onlyAllowedAddresses: true)]
     [RoutePrefix("api/hq/user")]
     public class HQUserController : ApiController
     {
         [HttpGet]
         [Route("login")]
+        [ApiBasicAuth(onlyAllowedAddresses: true)]
         public void Login() { }
+
+        [HttpGet]
+        [ApiBasicAuth(onlyAllowedAddresses: false)]
+        [Route("userdetails")]
+        public MembershipUser UserDetails(Guid id)
+        {
+            return Membership.GetUser(id, false);
+        }
     }
 }
