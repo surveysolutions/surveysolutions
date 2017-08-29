b756fc5f78a2b37a045a69f715011f5baad07014
Slava Scherbak <slava.scherbak@gmail.com>
2017-08-29 11:20:41 -0400

    Added method to get user details for external deployment app.

---
+++ src/UI/Designer/WB.UI.Designer/Models/DeploymentUserModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WB.UI.Designer.Models
+{
+    public class DeploymentUserModel
+    {
+        public Guid Id { get; set; }
+        public string[] Roles { get; set; }
+        public string Login { get; set; }
+        public string Email { get; set; }
+    }
+}
