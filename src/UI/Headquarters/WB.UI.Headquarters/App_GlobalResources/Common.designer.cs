95eb63668726774665f75b3c33dedaa2ba698778
Victor Hladkikh <me@alfeg.net>
2017-08-31 13:20:19 +0300

    KP-9949: Cosmetic changes on Interview page

--- src/UI/Headquarters/WB.UI.Headquarters/App_GlobalResources/Common.designer.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/App_GlobalResources/Common.designer.cs
@@ -79,11 +79,38 @@ namespace Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to All questionnaires.
+        ///   Looks up a localized string similar to All Questionnaires.
         /// </summary>
         internal static string AllQuestionnaires {
             get {
                 return ResourceManager.GetString("AllQuestionnaires", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to All Responsible.
+        /// </summary>
+        internal static string AllResponsible {
+            get {
+                return ResourceManager.GetString("AllResponsible", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to All Statuses.
+        /// </summary>
+        internal static string AllStatuses {
+            get {
+                return ResourceManager.GetString("AllStatuses", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to All Supervisors.
+        /// </summary>
+        internal static string AllSupervisors {
+            get {
+                return ResourceManager.GetString("AllSupervisors", resourceCulture);
             }
         }
         
@@ -345,6 +372,15 @@ namespace Resources {
         internal static string PrefilledQuestions {
             get {
                 return ResourceManager.GetString("PrefilledQuestions", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Questionnaire.
+        /// </summary>
+        internal static string Questionnaire {
+            get {
+                return ResourceManager.GetString("Questionnaire", resourceCulture);
             }
         }
         
