f8e12197e5798ead49d4ac2201cf1664feb5f86c
Roman Lytvyn <litvin.roma@gmail.com>
2017-08-31 14:14:39 +0300

    KP-9955 HQ: Remove usage InterviewData from interview details. added new plain storage for interview entities.

---
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Mappings/DbQuestionMap.cs
@@ -0,0 +1,44 @@
+﻿using NHibernate.Mapping.ByCode.Conformist;
+using WB.Core.BoundedContexts.Headquarters.Views.Interview;
+using WB.Core.Infrastructure.PlainStorage;
+using WB.Infrastructure.Native.Storage.Postgre.NhExtensions;
+
+namespace WB.Core.BoundedContexts.Headquarters.Mappings
+{
+    [PlainStorage]
+    public class DbQuestionMap : ClassMapping<DbQuestion>
+    {
+        public DbQuestionMap()
+        {
+            this.Table("interviewquestions");
+
+            this.ComposedId(x =>
+            {
+                x.Property(m=>m.InterviewId);
+                x.Property(m=>m.QuestionIdentity, cmp =>
+                {
+                    cmp.Column("QuestionId");
+                    cmp.Column(y =>
+                    {
+                        y.Name("RosterVector");
+                        y.SqlType("numeric[]");
+                    });
+                });
+            });
+
+            this.Property(x => x.InterviewId, pm => pm.Column(cm => cm.Index("interviewquestion_interviewId")));
+            this.Component(x => x.QuestionIdentity, cmp =>
+            {
+                cmp.Property(x => x.Id, ptp=>ptp.Column("QuestionId"));
+                
+                cmp.Property(x => x.RosterVector, ptp =>
+                {
+                    ptp.Type<PostgresRosterVector>();
+                    ptp.Column(clm => clm.SqlType("numeric[]"));
+                });
+            });
+            
+            this.Property(x => x.IsFlagged);
+        }
+    }
+}
---
+++ src/UI/Headquarters/WB.UI.Headquarters/Migrations/PlainStore/M001-100/M023_AddInterviewQuestionsTable.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+using FluentMigrator;
+
+namespace WB.UI.Headquarters.Migrations.PlainStore
+{
+    [Localizable(false)]
+    [Migration(23)]
+    public class M023_AddInterviewQuestionsTable : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("interviewquestions")
+                .WithColumn("interviewid").AsGuid().PrimaryKey()
+                .WithColumn("questionid").AsGuid().PrimaryKey()
+                .WithColumn("rostervector").AsCustom("numeric[]").PrimaryKey()
+                .WithColumn("isflagged").AsBoolean().WithDefaultValue(false);
+            
+
+            Create.Index("interviewquestions_interviewId")
+                .OnTable("interviewquestions")
+                .OnColumn("interviewid");
+
+            // uncomment this in the future when interviewid will be primary key
+            //Create.ForeignKey("fk_interviewquestions_interviewsummaries")
+            //    .FromTable("interviewquestions")
+            //    .ForeignColumn("interviewid")
+            //    .ToTable("interviewsummaries").PrimaryColumn("interviewid");
+        }
+
+        public override void Down()
+        {
+            Delete.Table("interviewquestions");
+            Delete.Index("interviewquestions_interviewId");
+        }
+    }
+}
