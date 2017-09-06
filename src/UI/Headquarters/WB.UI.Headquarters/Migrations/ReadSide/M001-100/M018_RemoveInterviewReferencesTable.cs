0cfc71d46c71f50ffd2f7fd38576ef65f084d945
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2017-09-06 12:56:15 +0300

    KP-10034: Added migration for removing interview references

---
+++ src/UI/Headquarters/WB.UI.Headquarters/Migrations/ReadSide/M001-100/M018_RemoveInterviewReferencesTable.cs
@@ -0,0 +1,18 @@
+﻿using FluentMigrator;
+
+namespace WB.UI.Headquarters.Migrations.ReadSide
+{
+    [Migration(18)]
+    public class M018_RemoveInterviewReferencesTable : Migration
+    {
+        public override void Up()
+        {
+            Delete.Table("interviewreferences");
+        }
+
+        public override void Down()
+        {
+            // Table is going to be created on first use
+        }
+    }
+}
