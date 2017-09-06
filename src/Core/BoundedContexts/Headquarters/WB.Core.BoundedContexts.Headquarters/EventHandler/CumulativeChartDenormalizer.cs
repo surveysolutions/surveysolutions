a4e7597ea01b98f92eb530ca996485af14f6373a
Slava Scherbak <slava.scherbak@gmail.com>
2017-09-06 16:18:55 -0400

    KP-10031: removed table lastinterviewstatus and made cumulative chart table self-sufficient.

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/EventHandler/CumulativeChartDenormalizer.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/EventHandler/CumulativeChartDenormalizer.cs
@@ -1,4 +1,5 @@
-﻿using Ncqrs.Eventing.ServiceModel.Bus;
+﻿using System.Linq;
+using Ncqrs.Eventing.ServiceModel.Bus;
 using WB.Core.BoundedContexts.Headquarters.DataExport.Accessors;
 using WB.Core.BoundedContexts.Headquarters.Views.Interview;
 using WB.Core.GenericSubdomains.Portable;
@@ -6,42 +7,41 @@ using WB.Core.Infrastructure.EventBus;
 using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
 using WB.Core.SharedKernels.DataCollection.Events.Interview;
 using WB.Core.SharedKernels.DataCollection.ValueObjects.Interview;
+using WB.Infrastructure.Native.Storage;
 
 namespace WB.Core.BoundedContexts.Headquarters.EventHandler
 {
     public class CumulativeChartDenormalizer : BaseDenormalizer, 
-        IEventHandler<InterviewStatusChanged>,
-        IEventHandler<InterviewHardDeleted>
+        IEventHandler<InterviewStatusChanged>
     {
-        private readonly IReadSideKeyValueStorage<LastInterviewStatus> lastStatusesStorage;
+        private readonly INativeReadSideStorage<CumulativeReportStatusChange> cumulativeReportReader;
         private readonly IReadSideRepositoryWriter<CumulativeReportStatusChange> cumulativeReportStatusChangeStorage;
         private readonly IQueryableReadSideRepositoryReader<InterviewSummary> interviewReferencesStorage;
 
         public CumulativeChartDenormalizer(
-            IReadSideKeyValueStorage<LastInterviewStatus> lastStatusesStorage,
             IReadSideRepositoryWriter<CumulativeReportStatusChange> cumulativeReportStatusChangeStorage,
-            IQueryableReadSideRepositoryReader<InterviewSummary> interviewReferencesStorage)
+            IQueryableReadSideRepositoryReader<InterviewSummary> interviewReferencesStorage,
+            INativeReadSideStorage<CumulativeReportStatusChange> cumulativeReportReader)
         {
-            this.lastStatusesStorage = lastStatusesStorage;
             this.cumulativeReportStatusChangeStorage = cumulativeReportStatusChangeStorage;
             this.interviewReferencesStorage = interviewReferencesStorage;
+            this.cumulativeReportReader = cumulativeReportReader;
         }
 
         public override object[] Readers => new object[] { this.interviewReferencesStorage };
 
-        public override object[] Writers => new object[] { this.lastStatusesStorage, this.cumulativeReportStatusChangeStorage };
+        public override object[] Writers => new object[] { this.cumulativeReportStatusChangeStorage };
 
         public void Handle(IPublishedEvent<InterviewStatusChanged> @event)
         {
-            string interviewId = @event.EventSourceId.FormatGuid();
+            InterviewStatus? oldStatus = cumulativeReportReader.Query(_ => _
+                .Where(x => x.InterviewId == @event.EventSourceId && x.ChangeValue > 0)
+                .OrderByDescending(x => x.EventSequence)
+                .FirstOrDefault())?.Status;
 
-            InterviewStatus? oldStatus = this.lastStatusesStorage.GetById(interviewId)?.Status;
             InterviewStatus newStatus = @event.Payload.Status;
 
             var questionnaireIdentity = this.interviewReferencesStorage.GetQuestionnaireIdentity(@event.EventSourceId);
-
-            var lastInterviewStatus = new LastInterviewStatus(interviewId, newStatus);
-            this.lastStatusesStorage.Store(lastInterviewStatus, lastInterviewStatus.EntryId);
 
             if (oldStatus != null)
             {
@@ -51,7 +51,9 @@ namespace WB.Core.BoundedContexts.Headquarters.EventHandler
                     questionnaireIdentity.Version,
                     @event.EventTimeStamp.Date,
                     oldStatus.Value,
-                    -1);
+                    -1,
+                    @event.EventSourceId,
+                    @event.GlobalSequence);
 
                 this.cumulativeReportStatusChangeStorage.Store(minusChange, minusChange.EntryId);
             }
@@ -62,14 +64,11 @@ namespace WB.Core.BoundedContexts.Headquarters.EventHandler
                 questionnaireIdentity.Version,
                 @event.EventTimeStamp.Date,
                 newStatus,
-                +1);
+                +1,
+                @event.EventSourceId,
+                @event.GlobalSequence);
 
             this.cumulativeReportStatusChangeStorage.Store(plusChange, plusChange.EntryId);
-        }
-
-        public void Handle(IPublishedEvent<InterviewHardDeleted> evnt)
-        {
-            this.lastStatusesStorage.Remove(evnt.EventSourceId);
         }
     }
 }
