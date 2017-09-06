4df6bc887a99e3d73ad57bfccee65189ab5d8713
Slava Scherbak <slava.scherbak@gmail.com>
2017-09-06 16:18:55 -0400

    KP-10031: removed table lastinterviewstatus and made cumulative chart table self-sufficient.

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Views/Interview/CumulativeReportStatusChange.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Views/Interview/CumulativeReportStatusChange.cs
@@ -1,4 +1,5 @@
 using System;
+using WB.Core.SharedKernels.DataCollection.Implementation.Entities;
 using WB.Core.SharedKernels.DataCollection.ValueObjects.Interview;
 using WB.Core.SharedKernels.SurveySolutions;
 
@@ -8,23 +9,30 @@ namespace WB.Core.BoundedContexts.Headquarters.Views.Interview
     {
         protected CumulativeReportStatusChange() {}
 
-        public CumulativeReportStatusChange(string entryId, Guid questionnaireId, long questionnaireVersion, DateTime date, InterviewStatus status, int changeValue)
+        public CumulativeReportStatusChange(string entryId, Guid questionnaireId, long questionnaireVersion, DateTime date, InterviewStatus status, int changeValue,
+            Guid interviewId, long eventSequence)
         {
             this.EntryId = entryId;
-            this.QuestionnaireId = questionnaireId;
-            this.QuestionnaireVersion = questionnaireVersion;
+            this.QuestionnaireIdentity = new QuestionnaireIdentity(questionnaireId, questionnaireVersion).ToString();
             this.Date = date;
             this.Status = status;
             this.ChangeValue = changeValue;
+            this.InterviewId = interviewId;
+            this.EventSequence = eventSequence;
         }
 
         public virtual string EntryId { get; set; }
 
-        public virtual Guid QuestionnaireId { get; set; }
-        public virtual long QuestionnaireVersion { get; set; }
+        public virtual string QuestionnaireIdentity { get; set; }
+
+        public virtual Guid InterviewId { get; set; }
+
+        public virtual long EventSequence { get; set; }
+      
         public virtual DateTime Date { get; set; }
 
         public virtual InterviewStatus Status { get; set; }
+
         public virtual int ChangeValue { get; set; }
     }
 }
---
+++ src/UI/Headquarters/WB.UI.Headquarters/Migrations/ReadSide/M001-100/M019_RemoveLastInterviewStatusAndAddColumnsToCumulativeChart.cs
@@ -0,0 +1,41 @@
+﻿using FluentMigrator;
+
+namespace WB.UI.Headquarters.Migrations.ReadSide
+{
+    [Migration(19)]
+    public class M019_RemoveLastInterviewStatusAndAddColumnsToCumulativeChart : Migration
+    {
+        private static readonly string CumulativeReportStatusChangesTable = "cumulativereportstatuschanges";
+        private static readonly string QuestionnaireIdentityColumn = "questionnaireidentity";
+        private static readonly string InterviewIdColumn = "interviewid";
+        private static readonly string EventSequenceColumn = "eventsequence";
+        public override void Up()
+        {
+            Delete.Table("lastinterviewstatuses");
+
+            Alter.Table(CumulativeReportStatusChangesTable).AddColumn(QuestionnaireIdentityColumn).AsString().Nullable();
+            Alter.Table(CumulativeReportStatusChangesTable).AddColumn(InterviewIdColumn).AsGuid().Nullable();
+            Alter.Table(CumulativeReportStatusChangesTable).AddColumn(EventSequenceColumn).AsInt32().Nullable();
+
+            Execute.Sql($@"UPDATE readside.{CumulativeReportStatusChangesTable} 
+                           SET {QuestionnaireIdentityColumn}=concat(replace(questionnaireid::text, '-', ''), '$', questionnaireversion)");
+
+            Execute.Sql($@"UPDATE readside.{CumulativeReportStatusChangesTable}
+                SET ({InterviewIdColumn}, {EventSequenceColumn}) = (
+                    SELECT eventsourceid, globalsequence
+                    FROM events.events
+                    WHERE substr(readside.{CumulativeReportStatusChangesTable}.entryid, 0, 33) = replace(events.events.id::text, '-', ''))");
+
+            Delete.Column("questionnaireid").FromTable(CumulativeReportStatusChangesTable);
+            Delete.Column("questionnaireversion").FromTable(CumulativeReportStatusChangesTable);
+        }
+
+        public override void Down()
+        {
+            Delete.Column(QuestionnaireIdentityColumn).FromTable(CumulativeReportStatusChangesTable);
+            Delete.Column(InterviewIdColumn).FromTable(CumulativeReportStatusChangesTable);
+            Delete.Column(EventSequenceColumn).FromTable(CumulativeReportStatusChangesTable);
+            // Table is going to be created on first use
+        }
+    }
+}
