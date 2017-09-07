641a5ab3ccd5344196d5035861cfa724c1c5fd7c
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2017-09-07 11:07:31 -0400

    KP-10038 Remove table readsideversions

---
+++ src/UI/Headquarters/WB.UI.Headquarters/Migrations/ReadSide/M001-100/M201709071054_Remove_Readsideversions.cs
@@ -0,0 +1,16 @@
+﻿using FluentMigrator;
+
+namespace WB.UI.Headquarters.Migrations.ReadSide
+{
+    public class M201709071054_Remove_Readsideversions : Migration
+    {
+        public override void Up()
+        {
+            Delete.Table("readsideversions");
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
