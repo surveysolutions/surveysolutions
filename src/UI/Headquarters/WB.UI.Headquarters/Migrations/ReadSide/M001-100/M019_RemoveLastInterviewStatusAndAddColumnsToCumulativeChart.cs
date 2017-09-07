74019d8bead1d12d0cceb75153ea195d7587c10d
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2017-09-07 14:54:50 -0400

    fix delete lastinterviewstatuses

--- src/UI/Headquarters/WB.UI.Headquarters/Migrations/ReadSide/M001-100/M019_RemoveLastInterviewStatusAndAddColumnsToCumulativeChart.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/Migrations/ReadSide/M001-100/M019_RemoveLastInterviewStatusAndAddColumnsToCumulativeChart.cs
@@ -13,24 +13,26 @@ namespace WB.UI.Headquarters.Migrations.ReadSide
 
         public override void Up()
         {
-            Delete.Table("lastinterviewstatuses");
-
+            if (Schema.Table("lastinterviewstatuses").Exists())
+                Delete.Table("lastinterviewstatuses");
+            
             Alter.Table(CumulativeReportStatusChangesTable).AddColumn(QuestionnaireIdentityColumn).AsString().Nullable();
             Alter.Table(CumulativeReportStatusChangesTable).AddColumn(InterviewIdColumn).AsGuid().Nullable();
             Alter.Table(CumulativeReportStatusChangesTable).AddColumn(EventSequenceColumn).AsInt32().Nullable();
-
             Alter.Table(CumulativeReportStatusChangesTable).AddColumn(TempEntryraw).AsGuid().Nullable();
 
             Execute.Sql($@"UPDATE readside.{CumulativeReportStatusChangesTable} 
-                SET {QuestionnaireIdentityColumn}=concat(replace(questionnaireid::text, '-', ''), '$', questionnaireversion),
+                SET {
+                        QuestionnaireIdentityColumn
+                    }=concat(replace(questionnaireid::text, '-', ''), '$', questionnaireversion),
                 {TempEntryraw}=uuid(substr(entryid, 0, 33))");
-            
-            Execute.Sql($@"UPDATE readside.{CumulativeReportStatusChangesTable}
+
+                Execute.Sql($@"UPDATE readside.{CumulativeReportStatusChangesTable}
                 SET ({InterviewIdColumn}, {EventSequenceColumn}) = (
                     SELECT eventsourceid, globalsequence
                     FROM events.events                    
                     WHERE readside.{CumulativeReportStatusChangesTable}.{TempEntryraw} = events.events.id)");
-
+            
             Delete.Column(TempEntryraw).FromTable(CumulativeReportStatusChangesTable);
             Delete.Column("questionnaireid").FromTable(CumulativeReportStatusChangesTable);
             Delete.Column("questionnaireversion").FromTable(CumulativeReportStatusChangesTable);
