0ccf98d33f284a4385a5b909e953bb8e83897c09
Pavel Stovpivskyi <630198+spuv@users.noreply.github.com>
2017-09-07 14:21:55 +0300

    KP-10033 Added migration to remove InterviewStatuses

---
+++ src/UI/Headquarters/WB.UI.Headquarters/Migrations/ReadSide/M001-100/M020_RemoveInterviewStatuses.cs
@@ -0,0 +1,22 @@
+﻿using FluentMigrator;
+
+namespace WB.UI.Headquarters.Migrations.ReadSide
+{
+    [Migration(20)]
+    public class M020_RemoveInterviewStatuses : Migration
+    {
+        public override void Up()
+        {
+            Delete.Table("interviewstatuses");
+
+            Create.ForeignKey("FK_InterviewSummary_InterviewCommentedStatuses")
+                .FromTable("interviewcommentedstatuses").ForeignColumn("interviewid")
+                .ToTable("interviewsummaries").PrimaryColumn("summaryid");
+        }
+
+        public override void Down()
+        {
+            // Table is going to be created on first use
+        }
+    }
+}
