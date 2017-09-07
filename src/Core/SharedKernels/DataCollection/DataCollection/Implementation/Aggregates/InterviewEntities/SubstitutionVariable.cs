71000969b55895f4b842dc1aec5c521eaa5402a3
Victor Hladkikh <me@alfeg.net>
2017-09-07 10:19:06 +0300

    Refactored the way SubstitutionText works

---
+++ src/Core/SharedKernels/DataCollection/DataCollection/Implementation/Aggregates/InterviewEntities/SubstitutionVariable.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace WB.Core.SharedKernels.DataCollection.Implementation.Aggregates.InterviewEntities
+{
+    public class SubstitutionVariable
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
