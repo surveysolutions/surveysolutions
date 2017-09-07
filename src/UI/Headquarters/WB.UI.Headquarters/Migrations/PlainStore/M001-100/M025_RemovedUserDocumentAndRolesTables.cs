b1c81d7c488abfe1701c1266ee0a2ffc053f980f
Victor Hladkikh <me@alfeg.net>
2017-09-07 17:54:22 +0300

    KP-10040 Remove user management related tables from plainstore

---
+++ src/UI/Headquarters/WB.UI.Headquarters/Migrations/PlainStore/M001-100/M025_RemovedUserDocumentAndRolesTables.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel;
+using FluentMigrator;
+
+namespace WB.UI.Headquarters.Migrations.PlainStore
+{
+    [Migration(25)]
+    [Localizable(false)]
+    public class M025_RemovedUserDocumentAndRolesTables : Migration
+    {
+        public override void Up()
+        {
+            Delete.Table("userdocuments");
+            Delete.Table("roles");
+        }
+
+        public override void Down()
+        {
+            Create.Table("userdocuments")
+                .WithColumn("id").AsString(255).PrimaryKey()
+                .WithColumn("creationdate").AsDateTime().Nullable()
+                .WithColumn("email").AsString().Nullable()
+                .WithColumn("islockedbyhq").AsBoolean().Nullable()
+                .WithColumn("isarchived").AsBoolean().Nullable()
+                .WithColumn("islockedbysupervisor").AsBoolean().Nullable()
+                .WithColumn("password").AsString().Nullable()
+                .WithColumn("publickey").AsGuid().Nullable()
+                .WithColumn("supervisorid").AsGuid().Nullable()
+                .WithColumn("supervisorname").AsString().Nullable()
+                .WithColumn("username").AsString().Nullable()
+                .WithColumn("lastchangedate").AsDateTime().Nullable()
+                .WithColumn("deviceid").AsString().Nullable()
+                .WithColumn("personname").AsString().Nullable()
+                .WithColumn("phonenumber").AsString().Nullable();
+
+            Create.Table("roles")
+                .WithColumn("userid").AsString(255).NotNullable()
+                .WithColumn("roleid").AsInt32().Nullable();
+        }
+    }
+}
