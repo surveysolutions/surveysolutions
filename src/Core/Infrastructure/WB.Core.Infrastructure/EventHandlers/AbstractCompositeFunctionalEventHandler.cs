e44f882311e513e9608761a26d025097b628d5df
Pavel Stovpivskyi <630198+spuv@users.noreply.github.com>
2017-09-08 16:14:37 +0300

    KP-10033 InterviewSummaryCompositeDenormalizer

---
+++ src/Core/Infrastructure/WB.Core.Infrastructure/EventHandlers/AbstractCompositeFunctionalEventHandler.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Ncqrs;
+using Ncqrs.Eventing.ServiceModel.Bus;
+using Ncqrs.Eventing.Storage;
+using WB.Core.GenericSubdomains.Portable;
+using WB.Core.Infrastructure.EventBus;
+using WB.Core.Infrastructure.Implementation.StorageStrategy;
+using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
+using WB.Core.SharedKernels.SurveySolutions;
+
+namespace WB.Core.Infrastructure.EventHandlers
+{
+    public interface ICompositeFunctionalPartEventHandler<TEntity, TStorage>
+        where TEntity : class, IReadSideRepositoryEntity
+        where TStorage : class, IReadSideStorage<TEntity>
+    { }
+
+
+    public abstract class AbstractCompositeFunctionalEventHandler<TEntity, TStorage> : AbstractFunctionalEventHandler<TEntity, TStorage>
+        where TEntity : class, IReadSideRepositoryEntity
+        where TStorage : class, IReadSideStorage<TEntity>
+    {
+        public abstract ICompositeFunctionalPartEventHandler<TEntity, TStorage>[] Handlers { get; }
+
+        protected AbstractCompositeFunctionalEventHandler(TStorage readSideStorage) : base(readSideStorage)
+        {
+        }
+
+        protected override bool Handles(IUncommittedEvent evt)
+        {
+            Type genericUpgrader = typeof(IUpdateHandler<,>);
+            var typeInfo = genericUpgrader.MakeGenericType(typeof(TEntity), evt.Payload.GetType()).GetTypeInfo();
+            foreach (var handler in Handlers)
+            {
+                if (typeInfo.IsAssignableFrom(handler.GetType()))
+                    return true;
+            }
+            return false;
+        }
+
+        protected bool Handles(IUncommittedEvent evt, ICompositeFunctionalPartEventHandler<TEntity, TStorage> handler)
+        {
+            Type genericUpgrader = typeof(IUpdateHandler<,>);
+            var typeInfo = genericUpgrader.MakeGenericType(typeof(TEntity), evt.Payload.GetType()).GetTypeInfo();
+            return typeInfo.IsAssignableFrom(handler.GetType());
+        }
+
+        protected override TEntity ApplyEventOnEntity(IPublishableEvent evt, Type eventType, TEntity currentState)
+        {
+            TEntity newState = currentState;
+
+            foreach (var functionalEventHandler in Handlers)
+            {
+                if (!Handles(evt, functionalEventHandler))
+                    continue;
+
+                newState = (TEntity)functionalEventHandler
+                    .GetType()
+                    .GetTypeInfo().GetMethod("Update", new[] { typeof(TEntity), eventType })
+                    .Invoke(functionalEventHandler, new object[] { newState, this.CreatePublishedEvent(evt) });
+            }
+
+            return newState;
+        }
+
+        public override void RegisterHandlersInOldFashionNcqrsBus(InProcessEventBus oldEventBus)
+        {
+            var handlerMethodNames = new string[] { "Create", "Update", "Delete" };
+
+            List<MethodInfo> handlers = new List<MethodInfo>();
+
+            foreach (var handler in Handlers)
+            {
+                handlers.AddRange(handler.GetType().GetTypeInfo().GetMethods().Where(m => handlerMethodNames.Contains(m.Name)));
+            }
+
+            handlers.ForEach((handler) => this.RegisterOldFashionHandler(oldEventBus, handler));
+        }
+    }
+}
