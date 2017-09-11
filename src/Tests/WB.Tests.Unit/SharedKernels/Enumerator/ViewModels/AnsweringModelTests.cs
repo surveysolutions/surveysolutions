3e03e2089e10de15e6856c16a15a818b989c1b47
Victor Hladkikh <me@alfeg.net>
2017-09-11 13:21:58 +0300

    KP-10059 Interviewer crashes when saving disabled question # Test fix

--- src/Tests/WB.Tests.Unit/SharedKernels/Enumerator/ViewModels/AnsweringModelTests.cs
+++ src/Tests/WB.Tests.Unit/SharedKernels/Enumerator/ViewModels/AnsweringModelTests.cs
@@ -1,4 +1,6 @@
-﻿using Moq;
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Moq;
 using NUnit.Framework;
 using WB.Core.Infrastructure.CommandBus;
 using WB.Core.SharedKernels.DataCollection.Exceptions;
@@ -12,20 +14,28 @@ namespace WB.Tests.Unit.SharedKernels.Enumerator.ViewModels
     public class AnsweringModelTests
     {
         [Test]
-        public void should_not_shallow_exception_InterviewException()
+        public async Task should_not_shallow_exception_InterviewException()
         {
             var commandServiceMock = new Mock<ICommandService>();
             commandServiceMock
-                .Setup(cs => cs.Execute(It.IsAny<ICommand>(), It.IsAny<string>()))
+                .Setup(cs => cs.ExecuteAsync(It.IsAny<ICommand>(), It.IsAny<string>(), It.IsAny<CancellationToken>()))
                 .Throws(new InterviewException("Some message that should be passed up"));
 
             var answering = new AnsweringViewModel(
                 commandServiceMock.Object, 
                 Mock.Of<IUserInterfaceStateService>());
 
-            Assert.Throws<InterviewException>(async () => 
+            try
+            {
                 await answering.SendAnswerQuestionCommandAsync(
-                    Create.Command.AnswerNumericRealQuestionCommand(Id.g1, Id.g2, 1)));
+                    Create.Command.AnswerNumericRealQuestionCommand(Id.g1, Id.g2, 1));
+            }
+            catch (InterviewException)
+            {
+                Assert.Pass();
+            }
+
+            Assert.Fail("Should throw InterviewException");
         }
     }
 }
