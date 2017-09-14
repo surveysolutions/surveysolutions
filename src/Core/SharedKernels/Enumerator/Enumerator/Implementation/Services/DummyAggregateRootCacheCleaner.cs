22c1002547cc39969b56d022c99cc14658949ec7
Slava Scherbak <slava.scherbak@gmail.com>
2017-09-14 16:18:54 -0400

    KP-10078: added registration of dummy repository cleaner for AR

---
+++ src/Core/SharedKernels/Enumerator/Enumerator/Implementation/Services/DummyAggregateRootCacheCleaner.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using WB.Core.Infrastructure.Implementation.Aggregates;
+
+namespace WB.Core.SharedKernels.Enumerator.Implementation.Services
+{
+    public class DummyAggregateRootCacheCleaner : IAggregateRootCacheCleaner
+    {
+        public void Evict(Guid aggregateId)
+        {
+        }
+    }
+}
