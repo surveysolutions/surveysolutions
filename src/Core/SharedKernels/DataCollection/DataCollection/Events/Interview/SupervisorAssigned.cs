306f1a938c6e3452674ce34b20e593d030525e03
Pavel Stovpivskyi <sby@rambler.ru>
2017-09-20 19:31:19 +0300

    KP-10101 Interview history: time is not chronological in interview history and interview actions

--- src/Core/SharedKernels/DataCollection/DataCollection/Events/Interview/SupervisorAssigned.cs
+++ src/Core/SharedKernels/DataCollection/DataCollection/Events/Interview/SupervisorAssigned.cs
@@ -5,12 +5,14 @@ namespace WB.Core.SharedKernels.DataCollection.Events.Interview
 {
     public class SupervisorAssigned : InterviewActiveEvent
     {
-        public Guid SupervisorId { get; private set; }
+        public Guid SupervisorId { get; }
+        public DateTime? AssignTime { get; }
 
-        public SupervisorAssigned(Guid userId, Guid supervisorId)
+        public SupervisorAssigned(Guid userId, Guid supervisorId, DateTime? assignTime = null)
             : base(userId)
         {
             this.SupervisorId = supervisorId;
+            this.AssignTime = assignTime;
         }
     }
 }
