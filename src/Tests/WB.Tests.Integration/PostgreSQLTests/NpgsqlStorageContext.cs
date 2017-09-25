5f9a67c3fda658abd387c78a0fe99784f8669d92
Slava Scherbak <slava.scherbak@gmail.com>
2017-09-25 13:04:28 -0400

    KP-10113: added integration test for method GetIdsStartWith

---
+++ src/Tests/WB.Tests.Integration/PostgreSQLTests/NpgsqlStorageContext.cs
@@ -0,0 +1,24 @@
+﻿using Moq;
+using NHibernate;
+using Npgsql;
+using WB.Core.BoundedContexts.Headquarters.Mappings;
+using WB.Core.BoundedContexts.Headquarters.Views.DataExport;
+using WB.Core.GenericSubdomains.Portable.Services;
+using WB.Infrastructure.Native.Storage.Postgre.Implementation;
+
+namespace WB.Tests.Integration.PostgreSQLTests
+{
+    internal abstract class NpgsqlStorageContext : NpgsqlTestContext
+    {
+        protected PostgreReadSideStorage<InterviewDataExportRecord> CreateInterviewExportRepository()
+        {
+            var sessionFactory = IntegrationCreate.SessionFactory(connectionStringBuilder.ConnectionString, new[] { typeof(InterviewDataExportRecordMap) }, true);
+            transactionManager = new CqrsPostgresTransactionManager(sessionFactory ?? Mock.Of<ISessionFactory>());
+
+            pgSqlConnection = new NpgsqlConnection(connectionStringBuilder.ConnectionString);
+            pgSqlConnection.Open();
+
+            return new PostgreReadSideStorage<InterviewDataExportRecord>(transactionManager, Mock.Of<ILogger>(), "RecordId");
+        }
+    }
+}
---
+++ src/Tests/WB.Tests.Integration/PostgreSQLTests/PostgreReadSideStorageTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using WB.Core.BoundedContexts.Headquarters.Views.DataExport;
+using WB.Core.Infrastructure.Transactions;
+using WB.Tests.Abc;
+
+namespace WB.Tests.Integration.PostgreSQLTests
+{
+    [TestFixture]
+    internal class PostgreReadSideStorageTests : NpgsqlStorageContext
+    {
+        [Test]
+        public void When_getting_records_what_starts_with_specified_string()
+        {
+            List<InterviewDataExportRecord> exportRecords = new List<InterviewDataExportRecord>
+            {
+                Create.Entity.InterviewDataExportRecord(id: "i1$level1", interviewId: Guid.NewGuid()),
+                Create.Entity.InterviewDataExportRecord(id: "i2$roster1", interviewId: Guid.NewGuid()),
+                Create.Entity.InterviewDataExportRecord(id: "i1$roster2", interviewId: Guid.NewGuid()),
+                Create.Entity.InterviewDataExportRecord(id: "i2$roster2", interviewId: Guid.NewGuid())
+            };
+
+            var storage = CreateInterviewExportRepository();
+            ExecuteInCommandTransaction(() => exportRecords.ForEach(x => storage.Store(x, x.Id)));
+
+            var ids = transactionManager.ExecuteInQueryTransaction(() => storage.GetIdsStartWith("i1").ToArray());
+        
+            CollectionAssert.AreEqual(new [] { "i1$level1", "i1$roster2" }, ids);
+        }
+    }
+}
+
