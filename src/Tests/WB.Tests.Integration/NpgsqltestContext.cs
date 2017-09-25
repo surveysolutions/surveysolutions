41ad781afa111a31dadc30ae20b63cc815d6316a
Slava Scherbak <slava.scherbak@gmail.com>
2017-09-25 13:04:28 -0400

    KP-10113: added integration test for method GetIdsStartWith

---
+++ src/Tests/WB.Tests.Integration/NpgsqltestContext.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Configuration;
+using Npgsql;
+using NUnit.Framework;
+using WB.Core.GenericSubdomains.Portable;
+using WB.Infrastructure.Native.Storage.Postgre.Implementation;
+
+namespace WB.Tests.Integration
+{
+    internal abstract class NpgsqlTestContext
+    {
+        protected NpgsqlConnection pgSqlConnection;
+        protected CqrsPostgresTransactionManager transactionManager;
+        protected static NpgsqlConnectionStringBuilder connectionStringBuilder;
+        protected static string TestConnectionString;
+        private static string databaseName;
+
+        [SetUp]
+        public void Context()
+        {
+            TestConnectionString = ConfigurationManager.ConnectionStrings["TestConnection"].ConnectionString;
+            databaseName = "testdb_" + Guid.NewGuid().FormatGuid();
+            connectionStringBuilder = new NpgsqlConnectionStringBuilder(TestConnectionString)
+            {
+                Database = databaseName
+            };
+
+            using (var connection = new NpgsqlConnection(TestConnectionString))
+            {
+                connection.Open();
+                var command = $@"CREATE DATABASE {databaseName} ENCODING = 'UTF8'";
+                using (var sqlCommand = connection.CreateCommand())
+                {
+                    sqlCommand.CommandText = command;
+                    sqlCommand.ExecuteNonQuery();
+                }
+                connection.Close();
+            }
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            pgSqlConnection.Close();
+
+            using (var connection = new NpgsqlConnection(TestConnectionString))
+            {
+                connection.Open();
+                var command = string.Format(
+                    @"SELECT pg_terminate_backend (pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = '{0}'; DROP DATABASE {0};",
+                    databaseName);
+                using (var sqlCommand = connection.CreateCommand())
+                {
+                    sqlCommand.CommandText = command;
+                    sqlCommand.ExecuteNonQuery();
+                }
+                connection.Close();
+            }
+        }
+
+        protected void ExecuteInCommandTransaction(Action action)
+        {
+            try
+            {
+                transactionManager.BeginCommandTransaction();
+
+                action();
+
+                transactionManager.CommitCommandTransaction();
+            }
+            catch
+            {
+                transactionManager.RollbackCommandTransaction();
+                throw;
+            }
+        }
+    }
+}
--- src/Tests/WB.Tests.Integration/ReportTests/ReportContext.cs
+++ src/Tests/WB.Tests.Integration/ReportTests/ReportContext.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
+﻿using System.Collections.Generic;
 using Moq;
 using NHibernate;
 using Npgsql;
-using NUnit.Framework;
 using WB.Core.BoundedContexts.Headquarters.Mappings;
 using WB.Core.BoundedContexts.Headquarters.Views.Interview;
 using WB.Core.BoundedContexts.Headquarters.Views.Reposts.Factories;
@@ -15,7 +12,7 @@ using WB.Infrastructure.Native.Storage.Postgre.Implementation;
 
 namespace WB.Tests.Integration.ReportTests
 {
-    internal abstract class ReportContext
+    internal abstract class ReportContext : NpgsqlTestContext
     {
         internal ReportContext()
         {
@@ -36,74 +33,6 @@ namespace WB.Tests.Integration.ReportTests
 
            return new PostgreReadSideStorage<InterviewSummary>(transactionManager, Mock.Of<ILogger>(), "InterviewId");
         }
-
-        protected NpgsqlConnection pgSqlConnection;
-        protected CqrsPostgresTransactionManager transactionManager;
-
-        [SetUp]
-        public void Context()
-        {
-            TestConnectionString = ConfigurationManager.ConnectionStrings["TestConnection"].ConnectionString;
-            databaseName = "testdb_" + Guid.NewGuid().FormatGuid();
-            connectionStringBuilder = new NpgsqlConnectionStringBuilder(TestConnectionString)
-            {
-                Database = databaseName
-            };
-
-            using (var connection = new NpgsqlConnection(TestConnectionString))
-            {
-                connection.Open();
-                var command = $@"CREATE DATABASE {databaseName} ENCODING = 'UTF8'";
-                using (var sqlCommand = connection.CreateCommand())
-                {
-                    sqlCommand.CommandText = command;
-                    sqlCommand.ExecuteNonQuery();
-                }
-                connection.Close();
-            }
-        }
-
-        [TearDown]
-        public void Cleanup()
-        {
-            pgSqlConnection.Close();
-
-            using (var connection = new NpgsqlConnection(TestConnectionString))
-            {
-                connection.Open();
-                var command = string.Format(
-                    @"SELECT pg_terminate_backend (pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = '{0}'; DROP DATABASE {0};",
-                    databaseName);
-                using (var sqlCommand = connection.CreateCommand())
-                {
-                    sqlCommand.CommandText = command;
-                    sqlCommand.ExecuteNonQuery();
-                }
-                connection.Close();
-            }
-        }
-
-        protected void ExecuteInCommandTransaction(Action action)
-        {
-            try
-            {
-                transactionManager.BeginCommandTransaction();
-
-                action();
-
-                transactionManager.CommitCommandTransaction();
-            }
-            catch
-            {
-                transactionManager.RollbackCommandTransaction();
-                throw;
-            }
-        }
-
-
-        protected static NpgsqlConnectionStringBuilder connectionStringBuilder;
-        protected static string TestConnectionString;
-        private static string databaseName;
 
         internal class SvReport
         {
