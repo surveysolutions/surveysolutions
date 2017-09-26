9dd2e182ddf4926784f840027ba65d185a58ac38
Victor Hladkikh <me@alfeg.net>
2017-09-26 18:24:23 +0300

    Revert "Cleanup of questionnaire sources output"
    This reverts commit f2e7561acc117e2d16c582a89acb11e765e92ed7.

--- src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/CodeGenerationV2/CodeTemplates/Extentions/LinkedFilterMethodTemplate.Extended.cs
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/CodeGenerationV2/CodeTemplates/Extentions/LinkedFilterMethodTemplate.Extended.cs
@@ -4,11 +4,11 @@ namespace WB.Core.BoundedContexts.Designer.CodeGenerationV2.CodeTemplates
 {
     public partial class LinkedFilterMethodTemplate
     {
-        public LinkedFilterMethodTemplate(GroupedModel<LinkedFilterMethodModel> model)
+        public LinkedFilterMethodTemplate(LinkedFilterMethodModel model)
         {
             this.Model = model;
         }
 
-        protected GroupedModel<LinkedFilterMethodModel> Model { get; set; }
+        protected LinkedFilterMethodModel Model { get; set; }
     }
 }
