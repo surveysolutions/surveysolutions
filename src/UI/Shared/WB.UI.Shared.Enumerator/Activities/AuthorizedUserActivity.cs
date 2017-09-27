913a9c5720ca8e52fdc31ab818c3a7a4f5b0a826
Andrii Kozhyn <hedgic@gmail.com>
2017-09-27 13:48:40 +0300

    KP-10002: Moved resolve of dashboard listener to mvx app start

--- src/UI/Shared/WB.UI.Shared.Enumerator/Activities/AuthorizedUserActivity.cs
+++ src/UI/Shared/WB.UI.Shared.Enumerator/Activities/AuthorizedUserActivity.cs
@@ -1,37 +1,8 @@
-using Android.OS;
 using MvvmCross.Core.ViewModels;
-using MvvmCross.Platform;
-using WB.Core.SharedKernels.Enumerator.Services;
-using WB.Core.SharedKernels.Enumerator.Services.Infrastructure;
 
 namespace WB.UI.Shared.Enumerator.Activities
 {
     public abstract class AuthorizedUserActivity<TViewModel> : BaseActivity<TViewModel> where TViewModel : MvxViewModel
     {
-        protected override void OnCreate(Bundle bundle)
-        {
-            base.OnCreate(bundle);
-            this.CheckPrincipalAndNavigateToSplash();
-        }
-
-        protected override void OnResume()
-        {
-            base.OnResume();
-            this.CheckPrincipalAndNavigateToSplash();
-        }
-
-        /*protected override void OnRestart()
-        {
-            base.OnRestart();
-            this.CheckPrincipalAndNavigateToSplash();
-        }*/
-
-        private void CheckPrincipalAndNavigateToSplash()
-        {
-            if (Mvx.Resolve<IPrincipal>()?.CurrentUserIdentity?.Name == null) // happens when application is restarted after crash
-            {
-                Mvx.Resolve<IViewModelNavigationService>().NavigateToSplashScreen();
-            }
-        }
     }
 }
