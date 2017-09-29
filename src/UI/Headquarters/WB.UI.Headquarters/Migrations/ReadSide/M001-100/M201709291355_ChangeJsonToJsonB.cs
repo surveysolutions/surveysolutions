5322dc86b57fdf5b490c9510844b0b23922013ac
Roman Lytvyn <litvin.roma@gmail.com>
2017-09-29 16:29:44 +0300

    KP-10256 HQ: Converted existing `json` fields in Postgre to `jsonb`

---
+++ src/UI/Headquarters/WB.UI.Headquarters/Migrations/ReadSide/M001-100/M201709291355_ChangeJsonToJsonB.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+using FluentMigrator;
+
+namespace WB.UI.Headquarters.Migrations.ReadSide
+{
+    [Localizable(false)]
+    [Migration(201709291355)]
+    public class M201709291355_ChangeJsonToJsonB : Migration
+    {
+        public override void Up()
+        {
+            Execute.Sql(GetScriptToChangeJsonToJsonb(@"readside.interviews", "asintmatrix"));
+            Execute.Sql(GetScriptToChangeJsonToJsonb("readside.interviews", "asgps"));
+            Execute.Sql(GetScriptToChangeJsonToJsonb("readside.interviews", "asyesno"));
+            Execute.Sql(GetScriptToChangeJsonToJsonb("readside.interviews", "asaudio"));
+            Execute.Sql(GetScriptToChangeJsonToJsonb("readside.interviews", "asarea"));
+            Execute.Sql(GetScriptToChangeJsonToJsonb("readside.interviews", "aslist"));
+
+            Execute.Sql(GetScriptToChangeJsonToJsonb("plainstore.appsettings", "value"));
+            Execute.Sql(GetScriptToChangeJsonToJsonb("plainstore.assignments", "answers"));
+            Execute.Sql(GetScriptToChangeJsonToJsonb("plainstore.questionnairedocuments", "value"));
+
+            Execute.Sql(GetScriptToChangeJsonToJsonb("plainstore.webinterviewconfigs", "value"));
+            Execute.Sql(GetScriptToChangeJsonToJsonb("events.events", "value"));
+        }
+
+        private static string GetScriptToChangeJsonToJsonb(string tableName, string columnName)
+            => $"alter table {tableName} alter column {columnName} type jsonb using {columnName}::text::jsonb;";
+
+        public override void Down()
+        {
+        }
+    }
+}
