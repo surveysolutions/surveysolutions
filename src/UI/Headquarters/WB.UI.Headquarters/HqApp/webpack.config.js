f821db0c2a6dfc574b40e9d9c24be8d889200943
Victor Gladkikh <me@alfeg.net>
2017-10-03 13:37:49 +0300

    KP-10172 Build generic infrastructure for UI on HQ # Few changes in build. Speed up dev build

--- src/UI/Headquarters/WB.UI.Headquarters/HqApp/webpack.config.js
+++ src/UI/Headquarters/WB.UI.Headquarters/HqApp/webpack.config.js
@@ -12,40 +12,7 @@ var webpackConfig = merge(commonConfig, {
     entry: {
         hqapp: "./src/hqapp/main.js",
         webinterview: "./src/webinterview/main.js"
-    },
-    resolve: {
-        alias: {
-            wi: path.resolve()
-        }
     }
 })
-
-webpackConfig.plugins.unshift(new webpack.optimize.CommonsChunkPlugin({
-    name: 'common'
-}));
-
-// webpackConfig.plugins.unshift(
-//     // split vendor js into its own file
-//     new webpack.optimize.CommonsChunkPlugin({
-//         name: 'vendor',
-//         minChunks: function (module, count) {
-//             // any required modules inside node_modules are extracted to vendor
-//             return (
-//                 module.resource &&
-//                 /\.(js|ts)$/.test(module.resource) &&
-//                 module.resource.indexOf(
-//                     path.join(__dirname, './node_modules')
-//                 ) === 0
-//             )
-//         }
-//     }),
-
-//     // // extract webpack runtime and module manifest to its own file in order to
-//     // // prevent vendor hash from being updated whenever app bundle is updated
-//     new webpack.optimize.CommonsChunkPlugin({
-//         name: 'manifest',
-//         chunks: ['vendor']
-//     })
-// )
 
 module.exports = webpackConfig
