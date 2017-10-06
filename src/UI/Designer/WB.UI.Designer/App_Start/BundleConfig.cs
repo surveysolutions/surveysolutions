6dc56a1ab0c8f59e168648032ab60d1abb69688e
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2017-10-06 11:15:30 +0300

    KP-10296: moved dependencies from node_modules

--- src/UI/Designer/WB.UI.Designer/App_Start/BundleConfig.cs
+++ src/UI/Designer/WB.UI.Designer/App_Start/BundleConfig.cs
@@ -14,7 +14,7 @@ namespace WB.UI.Designer
 
             bundles.Add(
                 new StyleBundle("~/Content/css-list").Include(
-                    "~/questionnaire/vendor/perfect-scrollbar/css/perfect-scrollbar.css",
+                    "~/Content/plugins/perfect-scrollbar.css",
                     "~/Content/designer-list.css"));
 
             bundles.Add(
@@ -22,29 +22,21 @@ namespace WB.UI.Designer
                     "~/questionnaire/content/designer-start/bootstrap-custom.css"));
 
             bundles.Add(
-                new StyleBundle("~/content/css").Include(
-                    "~/questionnaire/vendor/bootstrap/dist/bootstrap.css",
-                    "~/Content/font-awesome.min.css",
-                    "~/Content/body.css", 
-                    "~/Content/bootstrap-responsive.css", 
-                    "~/Content/bootstrap-mvc-validation.css"));
-
-            bundles.Add(
                 new ScriptBundle("~/simplepage").Include(
-                    "~/questionnaire/vendor/jquery/dist/jquery.min.js",
-                    "~/questionnaire/vendor/bootstrap/dist/bootstrap.js"));
+                    "~/Content/plugins/jquery.min.js",
+                    "~/Content/plugins/bootstrap.min.js"));
 
             bundles.Add(
                 new ScriptBundle("~/editform").Include(
-                    "~/questionnaire/vendor/jquery-validation/dist/jquery.validate.js",
+                    "~/Content/plugins/jquery.validate.js",
                     "~/Scripts/custom/jquery.validate.unobtrusive-custom-for-bootstrap.js",
                     "~/Scripts/custom/bootstrap3-unobtrusive-hack.js",
-                    "~/questionnaire/vendor/jquery-placeholder/jquery.placeholder.js",
+                    "~/Content/plugins/jquery-placeholder/jquery.placeholder.js",
                     "~/Scripts/custom/editForm.js"));
             bundles.Add(
                 new ScriptBundle("~/list").Include(
-                    "~/questionnaire/vendor/jquery-mousewheel/jquery.mousewheel.js",
-                    "~/questionnaire/vendor/perfect-scrollbar/js/perfect-scrollbar.jquery.js",
+                    "~/Content/plugins/jquery.mousewheel.js",
+                    "~/Content/plugins/perfect-scrollbar.jquery.js",
                     "~/Scripts/custom/common.js"));
         }
     }
