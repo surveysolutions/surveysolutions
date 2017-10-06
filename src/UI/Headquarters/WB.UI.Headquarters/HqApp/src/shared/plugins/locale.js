4bb5f437c6473eb45e7b2c89df35123c3766a32c
Victor Hladkikh <me@alfeg.net>
2017-10-06 13:28:49 +0300

    KP-10172: Build generic infrastructure for UI on HQ # Adding proper translations handle

--- src/UI/Headquarters/WB.UI.Headquarters/HqApp/src/shared/plugins/locale.js
+++ src/UI/Headquarters/WB.UI.Headquarters/HqApp/src/shared/plugins/locale.js
@@ -1,15 +1,36 @@
 import i18next from "i18next"
+import i18nextXHRBackend from "i18next-xhr-backend"
+import i18nextBrowserLanguageDetector from "i18next-browser-languagedetector"
+import moment from "moment"
 
 /*  the Plugin */
 var VueI18Next = {
     install: function (Vue, options) {
         /*  determine options  */
+        i18next
+            .use(i18nextXHRBackend)
+            .use(i18nextBrowserLanguageDetector);
+
         i18next.init(Object.assign({
-            fallbackLng: 'en'
+            debug: false,
+            //lng: 'de', // If not given, i18n will detect the browser language.
+            fallbackLng: 'en', // Default is dev
+            nsSeparator: '.',
+            keySeparator: ':',
+            load: 'languageOnly',
+            backend: {
+                loadPath: Vue.$config.basePath + 'dist/resources/{{ns}}.{{lng}}.json'
+            },
+            useCookie: false,
+            useLocalStorage: false
         }, options))
 
+        i18next.on('languageChanged', function (lng) {
+            moment.locale(lng);
+        });
+
         function translate(key, options) {
-            if(_.isArray(options)) {
+            if (_.isArray(options)) {
                 const res = i18next.t(key);
 
                 // js will not replace all occurences, and we have resources with several {0} in one string
