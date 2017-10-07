3d7819f3d73ca6b5da3fbad01a0f5ef4ea69326c
Victor Hladkikh <me@alfeg.net>
2017-10-07 21:11:38 +0300

    KP-10172: Build generic infrastructure for UI on HQ # Build update

--- src/UI/Headquarters/WB.UI.Headquarters/HqApp/webpack.config.js
+++ src/UI/Headquarters/WB.UI.Headquarters/HqApp/webpack.config.js
@@ -10,26 +10,21 @@ console.log(`Building HQ UI js in ${(devMode ? "DEVELOPMENT" : "PRODUCTION")} mo
 
 var commonConfig = require("./.build/webpack.common")
 
-var webpackConfig = merge(commonConfig, {
-    entry: {
-        hqapp: "./src/hqapp/main.js",        
-        webinterview: "./src/webinterview/main.js"
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            inject: false,
-            filename: path.resolve(hqViewsFolder, "shared", "partial.webInterview.cshtml"),
-            excludeChunks: ["hqapp"],
-            template: '!!handlebars-loader!src/template.hbs'
-        }),
+const entry = {
+    hq: "./src/hqapp/main.js",
+    test: "./src/hqapp/test.js",
+    webinterview: "./src/webinterview/main.js"
+};
 
-        new HtmlWebpackPlugin({
-            inject: false,
-            filename: path.resolve(hqViewsFolder, "shared", "partial.hq.cshtml"),
-            excludeChunks: ["webinterview"],
-            template: '!!handlebars-loader!src/template.hbs'
-        })
-    ]
-})
+const entryNames = Object.keys(entry)
 
-module.exports = webpackConfig
+const plugins = entryNames.map((e) => {
+    return new HtmlWebpackPlugin({
+        inject: false,
+        filename: path.resolve(hqViewsFolder, "shared", `partial.${e}.cshtml`),
+        excludeChunks: entryNames.filter((name) => name !== e),
+        template: '!!handlebars-loader!src/template.hbs'
+    })
+});
+
+module.exports = merge(commonConfig, { entry, plugins })
