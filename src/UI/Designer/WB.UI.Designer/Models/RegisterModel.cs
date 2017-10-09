8d19532f3b88037e9d84617da4415b49fbb44f5d
Roman Lytvyn <litvin.roma@gmail.com>
2017-10-09 11:32:13 +0300

    KP-10185 Move all strings from static pages to resources. Localized some fields validations

--- src/UI/Designer/WB.UI.Designer/Models/RegisterModel.cs
+++ src/UI/Designer/WB.UI.Designer/Models/RegisterModel.cs
@@ -8,12 +8,12 @@ namespace WB.UI.Designer.Models
     [DisplayName("Registration")]
     public class RegisterModel : PasswordModel
     {
-        [Required]
-        [EmailAddress]
+        [Required(ErrorMessageResourceType = typeof(ErrorMessages), ErrorMessageResourceName = "Email_required")]
+        [EmailAddress(ErrorMessageResourceType = typeof(ErrorMessages), ErrorMessageResourceName = "InvalidEmailAddress")]
         [Display(Name = "Email", Order = 4)]
         public string Email { get; set; }
 
-        [Required]
+        [Required(ErrorMessageResourceType = typeof(ErrorMessages), ErrorMessageResourceName = "UserName_required")]
         [Display(Name = "User name", Order = 1)]
         [RegularExpression("^[a-zA-Z0-9_]{3,15}$", ErrorMessageResourceType = typeof(ErrorMessages), ErrorMessageResourceName = "User_name_needs_to_be_between_3_and_15_characters")]
         public string UserName { get; set; }
