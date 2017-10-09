735e3b329fd5f82b682d4038c16c1f08107d5acd
Andrii Kozhyn <hedgic@gmail.com>
2017-10-09 13:08:26 +0300

    KP-10287: Changed format of date time question to be the same on all pages

--- src/Tests/WB.Tests.Integration/InterviewTests/LinkedQuestions/when_answering_linked_single_option_question_which_links_to_datetime_question_and_which_is_roster_title_for_2_rosters_and_roster_level_is_1.cs
+++ src/Tests/WB.Tests.Integration/InterviewTests/LinkedQuestions/when_answering_linked_single_option_question_which_links_to_datetime_question_and_which_is_roster_title_for_2_rosters_and_roster_level_is_1.cs
@@ -8,6 +8,7 @@ using WB.Core.SharedKernels.DataCollection;
 using WB.Core.SharedKernels.DataCollection.Events.Interview;
 using WB.Core.SharedKernels.DataCollection.Events.Interview.Dtos;
 using WB.Core.SharedKernels.DataCollection.Implementation.Aggregates;
+using WB.Core.SharedKernels.DataCollection.Implementation.Aggregates.InterviewEntities;
 using WB.Tests.Abc;
 using It = Machine.Specifications.It;
 
@@ -35,8 +36,8 @@ namespace WB.Tests.Integration.InterviewTests.LinkedQuestions
             var linkedOption1Answer = new DateTime(2014, 2, 23);
             var linkedOption2Answer = new DateTime(2014, 3, 8);
             var linkedOption3Answer = new DateTime(2014, 5, 9);
-            linkedOption2TextInvariantCulture = linkedOption2Answer.ToShortDateString();//"03/08/2014";
-
+            linkedOption2TextInvariantCulture = linkedOption2Answer.ToString(DateTimeFormat.DateFormat);//"03/08/2014";
+            
             var triggerQuestionId = Guid.NewGuid();
             var questionnaireDocument = Abc.Create.Entity.QuestionnaireDocumentWithOneChapter(id: questionnaireId, children: new IComposite[]
             {
