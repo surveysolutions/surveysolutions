61ce6febacb6c86b5775a90b2d64a78858fb2ab5
Victor Hladkikh <me@alfeg.net>
2017-10-11 17:52:35 +0300

    KP-10322: Broken counters on Assignments page

--- src/UI/Headquarters/WB.UI.Headquarters/HqApp/src/shared/localization/__tests__/customFormatter.test.js
+++ src/UI/Headquarters/WB.UI.Headquarters/HqApp/src/shared/localization/__tests__/customFormatter.test.js
@@ -1,9 +1,9 @@
-const parse = require("./customFormatter").parse;
+const tokenize = require("../customFormatter").tokenize;
 
 describe("Custom i18n formatter", () => {
 
   it('able to parse tokens with text ""Enter the {{building}} with me {{inside}} force""', () => {
-    const result = parse("Enter the {{building}} with me {{inside}} force");
+    const result = tokenize("Enter the {{building}} with me {{inside}} force");
 
     expect(result).toEqual(
       [{ "type": "text", "value": "Enter the " },
@@ -12,9 +12,9 @@ describe("Custom i18n formatter", () => {
       { "type": "named", "value": "inside" },
       { "type": "text", "value": " force" }])
   });
-  
+
   it('able to parse tokens with spaces inside tokens ""Enter the {{  building }} with me {{  inside   }} force""', () => {
-    const result = parse("Enter the {{building}} with me {{inside}} force");
+    const result = tokenize("Enter the {{building}} with me {{inside}} force");
 
     expect(result).toEqual(
       [{ "type": "text", "value": "Enter the " },
@@ -25,7 +25,7 @@ describe("Custom i18n formatter", () => {
   });
 
   it('able to parse single token {{building}}', () => {
-    const result = parse("{{building}}");
+    const result = tokenize("{{building}}");
 
     expect(result).toEqual([
       { "type": "named", "value": "building" }
@@ -33,11 +33,26 @@ describe("Custom i18n formatter", () => {
   });
 
   it('able to parse several tokens "{{token}}{{building}}"', () => {
-    const result = parse("{{token}}{{building}}");
+    const result = tokenize("{{token}}{{building}}");
 
     expect(result).toEqual(
       [
         { "type": "named", "value": "token" },
+        { "type": "named", "value": "building" }
+      ])
+  });
+
+  it('able to parse list tokens "{0}{1 }{ 2}{ 3 } inner text {{building}}"', () => {
+    const result = tokenize("{0}{1 }{ 2}{ 3 } inner text {{building}}");
+
+    expect(result)
+      .toEqual(
+      [
+        { "type": "list", "value": "0" },
+        { "type": "list", "value": "1" },
+        { "type": "list", "value": "2" },
+        { "type": "list", "value": "3" },
+        { "type": "text", "value": " inner text " },
         { "type": "named", "value": "building" }
       ])
   });
