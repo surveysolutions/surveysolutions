5a4864ee8d572e2518b7af472f8c2e358cded4ab
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2017-10-11 13:53:24 +0300

    KP-9807: Added validation

--- src/UI/Headquarters/WB.UI.Headquarters/Models/Account/IPasswordRequired.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/Models/Account/IPasswordRequired.cs
@@ -11,11 +11,13 @@ namespace WB.Core.SharedKernels.SurveyManagement.Web.Models
         [PasswordRegularExpression(ErrorMessageResourceName = nameof(FieldsAndValidations.PasswordErrorMessage), ErrorMessageResourceType = typeof(FieldsAndValidations))]
         [DataType(DataType.Password)]
         [Display(Name = nameof(FieldsAndValidations.NewPasswordFieldName), ResourceType = typeof(FieldsAndValidations), Order = 1)]
+        [Required(ErrorMessageResourceName = nameof(FieldsAndValidations.RequiredPasswordErrorMessage), ErrorMessageResourceType = typeof(FieldsAndValidations))]
         string Password { get; set; }
 
         [DataType(DataType.Password)]
         [Compare(nameof(Password), ErrorMessageResourceName = nameof(FieldsAndValidations.ConfirmPasswordErrorMassage), ErrorMessageResourceType = typeof(FieldsAndValidations))]
         [Display(Name = nameof(FieldsAndValidations.ConfirmPasswordFieldName), ResourceType = typeof(FieldsAndValidations), Order = 2)]
+        [Required(ErrorMessageResourceName = nameof(FieldsAndValidations.ConfirmPasswordRequired), ErrorMessageResourceType = typeof(FieldsAndValidations))]
         string ConfirmPassword { get; set; }
         
         Guid Id { get; set; }
