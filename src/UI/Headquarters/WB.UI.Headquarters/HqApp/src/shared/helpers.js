2532c89eca005aea6855af8c5094120ae6d672c5
Victor Hladkikh <me@alfeg.net>
2017-10-12 17:40:05 +0300

    Fixed localization for IE11

--- src/UI/Headquarters/WB.UI.Headquarters/HqApp/src/shared/helpers.js
+++ src/UI/Headquarters/WB.UI.Headquarters/HqApp/src/shared/helpers.js
@@ -1,6 +1,4 @@
-var navigator = window.navigator;
-var lang = navigator.languages ? navigator.languages[0] : (navigator.language || navigator.userLanguage)
-export const browserLanguage = lang
+export const browserLanguage = window.CONFIG.locale.locale;
 
 export function getLocationHash(questionid) {
     return "loc_" + questionid
