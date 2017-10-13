95587b01aa0f4e133225c10e5fd222409d5d22e5
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2017-10-13 16:33:47 +0300

    KP-10350: Restored check of options before apply them on question

--- src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Resources/ExceptionMessages.Designer.cs
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Resources/ExceptionMessages.Designer.cs
@@ -19,7 +19,7 @@ namespace WB.Core.BoundedContexts.Designer.Resources {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class ExceptionMessages {
@@ -345,6 +345,15 @@ namespace WB.Core.BoundedContexts.Designer.Resources {
         public static string OnlyQuestionnaireDocumentsAreSupported {
             get {
                 return ResourceManager.GetString("OnlyQuestionnaireDocumentsAreSupported", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Option value should have only number characters.
+        /// </summary>
+        public static string OptionValuesShouldBeNumbers {
+            get {
+                return ResourceManager.GetString("OptionValuesShouldBeNumbers", resourceCulture);
             }
         }
         
---
+++ src/Tests/WB.Tests.Unit.Designer/QuestionnaireTests/when_updating_options_for_filtered_combo_and_one_option_is_not_number.cs
@@ -0,0 +1,41 @@
+﻿extern alias designer;
+using System;
+using designer::WB.Core.BoundedContexts.Designer.Exceptions;
+using designer::WB.Core.BoundedContexts.Designer.Resources;
+using Main.Core.Entities.SubEntities;
+using NUnit.Framework;
+using WB.Tests.Unit.Designer.BoundedContexts.QuestionnaireTests;
+
+namespace WB.Tests.Unit.Designer.QuestionnaireTests
+{
+    class when_updating_options_for_filtered_combo_and_one_option_is_not_number : QuestionnaireTestsContext
+    {
+        [Test]
+        public void should_throw_exception()
+        {
+            Guid responsibleId = Id.g1;
+            Guid chapterId = Id.gA;
+            Guid questionId = Id.gB;
+
+            var questionnaire = CreateQuestionnaire(responsibleId: responsibleId);
+            questionnaire.AddGroup(chapterId, responsibleId: responsibleId);
+            questionnaire.AddSingleOptionQuestion(
+                questionId,
+                chapterId,
+                responsibleId,
+                title: "text",
+                variableName: "var",
+                isFilteredCombobox: true);
+
+            var exception = Assert.Throws<QuestionnaireException>(() =>
+                questionnaire.UpdateFilteredComboboxOptions(questionId: questionId, responsibleId: responsibleId,
+                    options: new[]
+                    {
+                        new Option(Guid.NewGuid(), "1", "Option 1"),
+                        new Option(Guid.NewGuid(), "not number value", "Option 2")
+                    }));
+
+            Assert.That(exception.Message, Is.EqualTo(ExceptionMessages.OptionValuesShouldBeNumbers));
+        }
+    }
+}
