27168f6a33dfd64321b95efb5558a904a67d20c1
Victor Gladkikh <me@alfeg.net>
2017-10-16 15:03:14 +0300

    Build all deps updated

--- build.all.deps.bat
+++ build.all.deps.bat
@@ -1,10 +1,16 @@
 echo Build WB.UI.Headquarters\Dependencies
 pushd src\UI\Headquarters\WB.UI.Headquarters\Dependencies
 call yarn 
-call node_modules\.bin\gulp
+call yarn gulp
 popd
 
 echo Build WB.UI.Headquarters\HqApp
 pushd src\UI\Headquarters\WB.UI.Headquarters\HqApp
-call yarn && call yarn run production
+call yarn && call yarn dev
+popd
+
+
+echo Build WB.UI.Designer\questionnaire
+pushd src\UI\Designer\WB.UI.Designer\questionnaire
+call yarn && call yarn dev
 popd
