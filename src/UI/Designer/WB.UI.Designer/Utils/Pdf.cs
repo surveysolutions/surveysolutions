3f729692ea990ea25fda02b6f2b0db6597be3048
Slava Scherbak <slava.scherbak@gmail.com>
2017-10-16 18:22:34 -0400

    KP-10357: localized pdf view

---
+++ src/UI/Designer/WB.UI.Designer/Utils/Pdf.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace WB.UI.Designer.Utils
+{
+    public static class Pdf
+    {
+        public static MvcHtmlString Format(string stringWithFormat, params  object[] args)
+        {
+            return MvcHtmlString.Create(string.Format(stringWithFormat, args));
+        }
+
+        public static string WrapWith(this int number, string tag)
+        {
+            return $"<{tag}>{number}</{tag}>";
+        }
+    }
+}
