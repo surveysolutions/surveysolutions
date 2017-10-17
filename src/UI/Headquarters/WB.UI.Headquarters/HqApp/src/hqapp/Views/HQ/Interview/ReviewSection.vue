ab44d611589f7a4a30e89f07d711ea73c67edfce
Andrii Kozhyn <hedgic@gmail.com>
2017-10-17 12:45:53 +0300

    KP-10174: Added page template for review mode of the interview

---
+++ src/UI/Headquarters/WB.UI.Headquarters/HqApp/src/hqapp/Views/HQ/Interview/ReviewSection.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="questionnaire details-interview">
+      Section here
+      <a :href="oldPageUrl">Old interview details</a>
+  </section>
+</template>
+<script>
+export default {
+     computed: {
+        oldPageUrl() {
+            return window.input.settings.config.basePath + "Interview/Details/" + this.$route.params.id;
+        }
+    }
+}
+</script>
