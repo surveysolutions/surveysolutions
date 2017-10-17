8b417405585288d993a7c9f5ea78a3b38b8e97e1
Victor Hladkikh <me@alfeg.net>
2017-10-17 12:31:25 +0300

    KP-10194: Select Default language on Designer # Tests for PdfFactory

--- src/Tests/WB.Tests.Unit.Designer/BoundedContexts/Designer/PdfFactoryTests/when_load_and_shared_persons_contains_requested_user.cs
+++ src/Tests/WB.Tests.Unit.Designer/BoundedContexts/Designer/PdfFactoryTests/when_load_and_shared_persons_contains_requested_user.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using Machine.Specifications;
 using Main.Core.Documents;
 using Moq;
@@ -10,18 +11,21 @@ using WB.Core.BoundedContexts.Designer.Views.Questionnaire.QuestionnaireList;
 using WB.Core.GenericSubdomains.Portable;
 using WB.Core.Infrastructure.Implementation;
 using WB.Core.Infrastructure.PlainStorage;
+using WB.Tests.Abc;
+using It = Moq.It;
 
 namespace WB.Tests.Unit.Designer.BoundedContexts.Designer.PdfFactoryTests
 {
     public class when_load_and_shared_persons_contains_requested_user : PdfFactoryTestsContext
     {
         [OneTimeSetUp]
-        public void Context () {
+        public void Context()
+        {
             var accountDocument = Create.AccountDocument(userName);
             var questionnaireDocument = Create.QuestionnaireDocument();
 
             var accountsDocumentReader = Mock.Of<IPlainStorageAccessor<User>>(x => x.GetById(userId.FormatGuid()) == accountDocument);
-            var questionnaireRepository = Mock.Of<IPlainKeyValueStorage<QuestionnaireDocument>>(x=>x.GetById(questionnaireId.FormatGuid()) == questionnaireDocument);
+            var questionnaireRepository = Mock.Of<IPlainKeyValueStorage<QuestionnaireDocument>>(x => x.GetById(questionnaireId.FormatGuid()) == questionnaireDocument);
             var questionnaireChangeHistoryStorage = new InMemoryPlainStorageAccessor<QuestionnaireChangeRecord>();
             questionnaireChangeHistoryStorage.Store(
                 new QuestionnaireChangeRecord
@@ -37,22 +41,22 @@ namespace WB.Tests.Unit.Designer.BoundedContexts.Designer.PdfFactoryTests
             questionnaireListItemStorage.Store(questionnaireListViewItem, questionnaireId.FormatGuid());
 
             factory = CreateFactory(accountsDocumentReader: accountsDocumentReader,
-                questionnaireStorage: questionnaireRepository, 
+                questionnaireStorage: questionnaireRepository,
                 questionnaireChangeHistoryStorage: questionnaireChangeHistoryStorage,
                 questionnaireListViewItemStorage: questionnaireListItemStorage);
+
             BecauseOf();
         }
 
-        private void BecauseOf() =>
-            view = factory.Load(questionnaireId.FormatGuid(), userId, userName);
+        private void BecauseOf() => view = factory.Load(questionnaireId.FormatGuid(), userId, userName);
 
-        [Test] public void should_shared_persons_be_empty () =>
-            view.SharedPersons.ShouldBeEmpty();
+        [Test]
+        public void should_shared_persons_be_empty() => Assert.False(view.SharedPersons.Any());
 
         private static PdfQuestionnaireModel view;
         private static PdfFactory factory;
-        private static Guid questionnaireId = Guid.Parse("11111111111111111111111111111111");
-        private static Guid userId = Guid.Parse("22222222222222222222222222222222");
+        private static Guid questionnaireId = Id.g1;
+        private static Guid userId = Id.g2;
         private static string userName = "user";
         private static string userEmail = "user@e.mail";
     }
