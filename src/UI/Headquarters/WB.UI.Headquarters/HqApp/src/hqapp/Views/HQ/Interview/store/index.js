ddb190ebc10ed124df283e41b5e1b7e2596ea290
Victor Hladkikh <me@alfeg.net>
2017-10-18 16:18:40 +0300

    KP-10174: moving api to hq # Sidebar loaded

--- src/UI/Headquarters/WB.UI.Headquarters/HqApp/src/hqapp/Views/HQ/Interview/store/index.js
+++ src/UI/Headquarters/WB.UI.Headquarters/HqApp/src/hqapp/Views/HQ/Interview/store/index.js
@@ -4,9 +4,4 @@ Vue.use(Vuex)
 
 import webinterview from "~/webinterview/store/store.object"
 
-const store = {
-    key: "webinterview",
-    value: webinterview
-}
-
-export default store;
+export default webinterview;
