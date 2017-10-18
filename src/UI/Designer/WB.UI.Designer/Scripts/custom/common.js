36c8fd8aa2f5bd92c205fae51073c9fb9887f5b1
Pavel Stovpivskyi <sby@rambler.ru>
2017-10-18 15:52:16 +0300

    Added language selector to pdf export (draft version)

--- src/UI/Designer/WB.UI.Designer/Scripts/custom/common.js
+++ src/UI/Designer/WB.UI.Designer/Scripts/custom/common.js
@@ -15,7 +15,7 @@
         $('#delete-modal-questionnaire-title').html(self.itemName);
     };
 
-    self.exportItemAsPdf = function (id, type, name, pdfDownloadUrl, pdfStatusUrl, pdfRetryUrl) {
+    self.exportItemAsPdf = function (id, type, name, pdfDownloadUrl, pdfStatusUrl, pdfRetryUrl, getLanguagesUrl) {
         var encName = decodeURIComponent(name);
         self.itemName = encName;
         self.itemType = type;
@@ -23,6 +23,7 @@
         self.pdfStatusUrl = pdfStatusUrl;
         self.pdfDownloadUrl = pdfDownloadUrl;
         self.pdfRetryUrl = pdfRetryUrl;
+        self.getLanguagesUrl = getLanguagesUrl;
 
         $('#export-pdf-modal-questionnaire-id').val(self.itemId);
         $('#export-pdf-modal-questionnaire-title').text(self.itemName);
@@ -30,7 +31,8 @@
         $('#pdfDownloadButton').hide();
         $('#pdfRetryGenerate').hide();
 
-        self.updateExportPdfStatusNeverending();
+        self.getLanguages(getLanguagesUrl);
+        //self.updateExportPdfStatusNeverending();
     };
 
     self.retryPdfExport = function() {
@@ -39,10 +41,10 @@
         self.setPdfMessage("Retrying export as PDF.");
     };
 
-    self.updateExportPdfStatus = function () {
+    self.updateExportPdfStatus = function (translationId) {
         if (self.pdfStatusUrl == '') return { always: function() {} };
         return $.ajax({
-            url: self.pdfStatusUrl,
+            url: self.pdfStatusUrl + '?translation=' + translationId,
             cache: false
         }).done(function (result) {
             if (result.Message != null) {
@@ -54,7 +56,7 @@
                 $('#pdfDownloadButton').unbind('click');
                 $('#pdfDownloadButton').click(function () {
                     self.pdfStatusUrl = '';
-                    window.location = self.pdfDownloadUrl;
+                    window.location = self.pdfDownloadUrl + '?translation=' + translationId;
                     $('#pdfCancelButton').click();
                 });
                 $('#pdfDownloadButton').show();
@@ -70,9 +72,9 @@
         });
     }
 
-    self.updateExportPdfStatusNeverending = function () {
-        $.when(self.updateExportPdfStatus()).always(function () {
-            setTimeout(self.updateExportPdfStatusNeverending, 1000);
+    self.updateExportPdfStatusNeverending = function (translation) {
+        $.when(self.updateExportPdfStatus(translation)).always(function () {
+            setTimeout(self.updateExportPdfStatusNeverending(translation), 1000);
         });
     }
 
@@ -81,6 +83,61 @@
             message
             //+ '\r\n\r\n' + 'Status updated ' + new Date().toLocaleTimeString()
         );
+    }
+
+    self.getLanguages = function (languagesUrl) {
+        $.ajax({
+            url: languagesUrl,
+            cache: false,
+            method: "POST"
+        }).done(function (result) {
+            if (result.length && result.length > 1) {
+                self.initLanguageComboBox(result);
+            } else {
+                self.updateExportPdfStatusNeverending(null);
+            }
+        }).fail(function (xhr, status, error) {
+            self.pdfStatusUrl = '';
+            self.setPdfMessage("Unexpected error occurred.\r\nPlease contact support@mysurvey.solutions if problem persists.");
+        });
+    }
+
+    self.initLanguageComboBox = function (translationList) {
+        var typeaheadCtrl = $(".languages-combobox");
+        //typeahead.typeahead('destroy')
+        typeaheadCtrl.typeahead({
+            source: /*translationList*/ ["111", "222", "333"],
+            //items: "all",
+            autoSelect: true,
+            //minLength: 0,
+            showHintOnFocus: true,
+            /*displayText: function(item) {
+                return item.name;
+            },*/
+            /*matcher: function() {
+                return true;
+            },
+            updater: function(item) {
+                return item.id;
+            },*/
+            afterSelect: function(item) {
+                self.updateExportPdfStatusNeverending(item.id);
+            }
+        });
+        $(".languages-combobox").change(function (evt) {
+            var current = typeaheadCtrl.typeahead("getActive");
+            if (current) {
+                // Some item from your model is active!
+                if (current.name == typeaheadCtrl.val()) {
+                    // This means the exact match is found. Use toLowerCase() if you want case insensitive match.
+                } else {
+                    // This means it is only a partial match, you can either add a new item
+                    // or take the active if you don't want new items
+                }
+            } else {
+                // Nothing is active so it is a new value (or maybe empty value)
+            }
+        });
     }
 }
 
