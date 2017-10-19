155fe57a003791076b393743d1821374de84ae1d
Andrii Kozhyn <hedgic@gmail.com>
2017-10-19 17:22:34 +0300

    KP-10175: Fixed display of cover page on review interview

--- src/UI/Headquarters/WB.UI.Headquarters/HqApp/src/hqapp/Views/HQ/Interview/ReviewSection.vue
+++ src/UI/Headquarters/WB.UI.Headquarters/HqApp/src/hqapp/Views/HQ/Interview/ReviewSection.vue
@@ -1,17 +1,19 @@
 <template>
-    <section class="questionnaire details-interview">
-        <!-- <Navbar /> -->
-        <a :href="oldPageUrl">Old interview details</a>
-        <Section />
-    </section>
+<div>
+    <a :href="oldPageUrl">Old interview details</a>
+    <Section />
+</div>
 </template>
 <script>
-
 export default {
-    computed: {
-        oldPageUrl() {
-            return window.input.settings.config.basePath + "Interview/Details/" + this.$route.params.interviewId;
-        }
+  computed: {
+    oldPageUrl() {
+      return (
+        window.input.settings.config.basePath +
+        "Interview/Details/" +
+        this.$route.params.interviewId
+      );
     }
-}
+  }
+};
 </script>
