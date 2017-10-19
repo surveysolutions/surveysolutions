07d04b647c33677285eae0118bb66cd33d2309c3
Victor Gladkikh <me@alfeg.net>
2017-10-19 14:59:47 +0300

    KP-10174: moving api to hq # splitting hq app store per views - reports

--- src/UI/Headquarters/WB.UI.Headquarters/HqApp/src/hqapp/Views/HQ/Reports/index.js
+++ src/UI/Headquarters/WB.UI.Headquarters/HqApp/src/hqapp/Views/HQ/Reports/index.js
@@ -1,12 +1,53 @@
 import InterviewersAndDevices from "./InterviewersAndDevices"
 import StatusDuration from "./StatusDuration"
 
+const store = {
+    state: {
+        exportUrls: {
+            excel: "",
+            csv: "",
+            tab: ""
+        }
+    },
+    actions: {
+        setExportUrls(context, urls) {
+            context.commit("SET_EXPORT_URLS", urls);
+        }
+    },
+    mutations: {
+        SET_EXPORT_URLS(state, urls) {
+            state.exportUrls.excel = urls.excel;
+            state.exportUrls.csv = urls.csv;
+            state.exportUrls.tab = urls.tab;
+        }
+    }
+}
+
 export default class ReportComponent {
+    constructor(rootStore) {
+        this.rootStore = rootStore;
+    }
+
     get routes() {
-        return [{
+        const beforeEnter = (from, to, next) => this.beforeEnter(from, to, next);
+        
+        const routes =  [{
             path: '/Reports/InterviewersAndDevices', component: InterviewersAndDevices
         }, {
             path: '/Reports/StatusDuration', component: StatusDuration
         }]
+
+        routes.forEach((path) => path.beforeEnter = beforeEnter);
+
+        return routes;
     }
+
+    async beforeEnter(to, from, next) {
+        if (this.rootStore.state[this.moduleName] == null) {
+            this.rootStore.registerModule(this.moduleName, store)
+        }
+        next();
+    }
+
+    get moduleName() { return "reports" }
 }
