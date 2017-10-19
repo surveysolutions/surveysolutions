25f9b3d43b90226b5c7066d6c5a1f31b939210a5
Roman Lytvyn <litvin.roma@gmail.com>
2017-10-19 12:58:39 +0300

    KP-10035 HQ: Remove table interviewdatas. Added tests. Part 2

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Views/Interview/InterviewGpsAnswer.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Views/Interview/InterviewGpsAnswer.cs
@@ -1,11 +1,36 @@
 using System;
+using System.Diagnostics;
 
 namespace WB.Core.BoundedContexts.Headquarters.Views.Interview
 {
+    [DebuggerDisplay("{ToString()}")]
     public class InterviewGpsAnswer
     {
         public Guid InterviewId { get; set; }
         public double Latitude { get; set; }
         public double Longitude { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            var target = obj as InterviewGpsAnswer;
+            if (target == null) return false;
+
+            return this.Equals(target);
+        }
+
+        protected bool Equals(InterviewGpsAnswer other) => InterviewId.Equals(other.InterviewId) &&
+                                                           Latitude.Equals(other.Latitude) &&
+                                                           Longitude.Equals(other.Longitude);
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = InterviewId.GetHashCode();
+                hashCode = (hashCode * 397) ^ Latitude.GetHashCode();
+                hashCode = (hashCode * 397) ^ Longitude.GetHashCode();
+                return hashCode;
+            }
+        }
     }
 }
