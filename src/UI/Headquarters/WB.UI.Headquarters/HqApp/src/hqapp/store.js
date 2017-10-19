dee06cb5b6420f07f5e83cb9493f4c8834739ee8
Victor Hladkikh <me@alfeg.net>
2017-10-19 14:59:47 +0300

    KP-10174: moving api to hq # splitting hq app store per views - reports

--- src/UI/Headquarters/WB.UI.Headquarters/HqApp/src/hqapp/store.js
+++ src/UI/Headquarters/WB.UI.Headquarters/HqApp/src/hqapp/store.js
@@ -3,28 +3,6 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
-const store = new Vuex.Store({
-    state: {
-        pendingHandle: null,
-        exportUrls: {
-            excel: "",
-            csv: "",
-            tab: ""
-        }
-    },
-    actions: {
-        setExportUrls(context, urls) {
-            context.commit("SET_EXPORT_URLS", urls);
-        }
-    },
-    mutations: {
-       
-        SET_EXPORT_URLS(state, urls) {
-            state.exportUrls.excel = urls.excel;
-            state.exportUrls.csv = urls.csv;
-            state.exportUrls.tab = urls.tab;
-        }
-    }
-})
+const store = new Vuex.Store({})
 
 export default store
