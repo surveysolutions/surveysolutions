13bebec5d8a1eed0df661e7b6cf25b2792310014
Victor Hladkikh <me@alfeg.net>
2017-10-21 00:38:32 +0300

    Extracted common packages into precompiled webpack dll js

--- src/UI/Headquarters/WB.UI.Headquarters/HqApp/webpack.config.js
+++ src/UI/Headquarters/WB.UI.Headquarters/HqApp/webpack.config.js
@@ -1,3 +1,4 @@
+const fs = require("fs")
 const webpack = require('webpack')
 const devMode = process.env.NODE_ENV != 'production';
 const path = require("path")
@@ -8,37 +9,54 @@ const hqViewsFolder = path.resolve(__dirname, "..", "Views")
 const localization = require("./.build/localization")
 
 
-var commonConfig = require("./.build/webpack.common")
+module.exports = new Promise(async (resolve) => {
 
-const config = {
-    hq: {
-        entry: "./src/hqapp/main.js",
-        locales: ["Pages", "WebInterviewUI", "Common", "Users", "Assignments", "Strings", "Reports", "DevicesInterviewers"]
-    },
-    webinterview: {
-        entry: "./src/webinterview/main.js",
-        locales: ["WebInterviewUI", "Common"]
+    const dllConfig = require("./webpack.vendor.config");
+
+    function buildMainConfig() {
+        var commonConfig = require("./.build/webpack.common")
+
+        const config = {
+            hq: {
+                entry: "./src/hqapp/main.js",
+                locales: ["Pages", "WebInterviewUI", "Common", "Users", "Assignments", "Strings", "Reports", "DevicesInterviewers"]
+            },
+            webinterview: {
+                entry: "./src/webinterview/main.js",
+                locales: ["WebInterviewUI", "Common"]
+            }
+        };
+
+        const localizationInfo = localization.buildLocalizationFiles(config);
+
+        const entryNames = Object.keys(config)
+
+        const plugins = entryNames.map((e) => {
+            return new HtmlWebpackPlugin({
+                inject: false,
+                filename: path.resolve(hqViewsFolder, "shared", `partial.${e}.cshtml`),
+                excludeChunks: entryNames.filter((name) => name !== e),
+                template: '!!handlebars-loader!src/template.hbs',
+                locales: JSON.stringify(localizationInfo[e]),
+                entry: e
+            })
+        });
+
+        const webpackConfig = merge(commonConfig, {
+            entry: entryNames.reduce((result, key) => { result[key] = config[key].entry; return result; }, {}),
+            plugins
+        })
+
+        return webpackConfig;
     }
-};
 
-const localizationInfo = localization.buildLocalizationFiles(config);
-
-const entryNames = Object.keys(config)
-
-const plugins = entryNames.map((e) => {
-    return new HtmlWebpackPlugin({
-        inject: false,
-        filename: path.resolve(hqViewsFolder, "shared", `partial.${e}.cshtml`),
-        excludeChunks: entryNames.filter((name) => name !== e),
-        template: '!!handlebars-loader!src/template.hbs',
-        locales: JSON.stringify(localizationInfo[e]),
-        entry: e
-    })
+    fs.stat("./dist/shared_vendor.manifest.json", (err, stats) => {
+        if (err) {
+            webpack(dllConfig, (err, stats) => {
+                resolve(buildMainConfig());
+            });
+        } else {
+            resolve(buildMainConfig());
+        }
+    });
 });
-
-const webpackConfig = merge(commonConfig, {
-    entry: entryNames.reduce((result, key) => { result[key] = config[key].entry; return result; }, {}),
-    plugins
-})
-
-module.exports = webpackConfig
---
+++ src/UI/Headquarters/WB.UI.Headquarters/HqApp/webpack.vendor.config.js
@@ -0,0 +1,37 @@
+const webpack = require('webpack')
+const path = require('path')
+const baseAppPath = "./"
+
+const merge = require('webpack-merge');
+
+const devMode = process.env.NODE_ENV != 'production';
+
+const packageName = "shared_vendor";
+
+console.log("Building HQ UI vendor libs");
+
+const shared = require("./.build/webpack.shared.vendor");
+
+module.exports = merge(shared(packageName), {
+    entry: {
+        [packageName]: [
+            "autonumeric",
+            "axios",
+            "bootbox",
+            "bootstrap/dist/js/bootstrap.js",
+            "date-fns",
+            "flatpickr",
+            "jquery-mask-plugin",
+            "jquery",
+            "moment",
+            "scriptjs",
+            "signalr",
+            "toastr",
+            "vue-i18n",
+            "vue-router",
+            "vue",
+            "vuex-router-sync",
+            "vuex"
+        ]
+    }
+})
