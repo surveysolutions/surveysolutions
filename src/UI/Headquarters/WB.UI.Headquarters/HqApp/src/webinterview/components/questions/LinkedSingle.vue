78d03fe7c0cb4a507a5a0d41380988d441e243d0
Victor Hladkikh <me@alfeg.net>
2017-10-23 15:36:02 +0300

    KP-10176: HQ review state questions # Linked single question

--- src/UI/Headquarters/WB.UI.Headquarters/HqApp/src/webinterview/components/questions/LinkedSingle.vue
+++ src/UI/Headquarters/WB.UI.Headquarters/HqApp/src/webinterview/components/questions/LinkedSingle.vue
@@ -4,7 +4,12 @@
             <div class="options-group" v-bind:class="{ 'dotted': noOptions }">
                 <div class="radio" v-for="option in $me.options" :key="$me.id + '_' + option.value">
                     <div class="field">
-                        <input class="wb-radio" type="radio" :id="$me.id + '_' + option.value" :name="$me.id" :value="option.value" v-model="answer">
+                        <input class="wb-radio" type="radio" 
+                            :id="$me.id + '_' + option.value" 
+                            :name="$me.id" 
+                            :value="option.value"
+                            :disabled="disabled"
+                            v-model="answer">
                         <label :for="$me.id + '_' + option.value">
                                 <span class="tick"></span> {{option.title}}
                         </label>
@@ -13,6 +18,7 @@
                 </div>
                 <div v-if="noOptions" class="options-not-available">{{ $t("WebInterviewUI.OptionsAvailableAfterAnswer") }}</div>
             </div>
+            <wb-lock />
         </div>
     </wb-question>
 </template>
@@ -30,8 +36,10 @@
                     return find(this.$me.options, (a) => { return isEqual(a.rosterVector, this.$me.answer) }).value;
                 },
                 set(value) {
-                    const selectedOption = find(this.$me.options, { 'value': value });
-                    this.$store.dispatch("answerLinkedSingleOptionQuestion", { answer: selectedOption.rosterVector, questionIdentity: this.$me.id })
+                    this.sendAnswer(() => {
+                        const selectedOption = find(this.$me.options, { 'value': value });
+                        this.$store.dispatch("answerLinkedSingleOptionQuestion", { answer: selectedOption.rosterVector, questionIdentity: this.$me.id })
+                    })
                 }
             },
             noOptions() {
