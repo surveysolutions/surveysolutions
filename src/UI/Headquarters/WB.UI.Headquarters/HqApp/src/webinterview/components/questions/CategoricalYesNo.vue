48de13a1c3e6b720cdec22c7e97f3e6fc2d2bb4c
Victor Gladkikh <me@alfeg.net>
2017-10-23 13:54:08 +0300

    KP-10176: HQ review state questions # Yes/No question

--- src/UI/Headquarters/WB.UI.Headquarters/HqApp/src/webinterview/components/questions/CategoricalYesNo.vue
+++ src/UI/Headquarters/WB.UI.Headquarters/HqApp/src/webinterview/components/questions/CategoricalYesNo.vue
@@ -5,24 +5,35 @@
             <div class="options-group">
                 <div class="radio" v-for="option in $me.options" :key="$me.id + '_' + option.value">
                     <div class="field">
-                        <input class="wb-radio" type="radio" :name="$me.id + '_' + option.value" :id="$me.id + '_' + option.value + '_yes'" :checked="isYesChecked(option.value)" value="true"
-                            @click="answerYes(option.value)" v-disabledWhenUnchecked="allAnswersGiven" />
+                        <input class="wb-radio" type="radio" 
+                            :name="$me.id + '_' + option.value" 
+                            :id="$me.id + '_' + option.value + '_yes'" 
+                            :checked="isYesChecked(option.value)" value="true"
+                            :disabled="!$me.acceptAnswer"
+                            @click="answerYes(option.value)" 
+                            v-disabledWhenUnchecked="allAnswersGiven" />
                         <label :for="$me.id + '_' + option.value + '_yes'">
                             <span class="tick"></span>
                         </label>
                         <b>/</b>
-                        <input class="wb-radio" type="radio" :name="$me.id + '_' + option.value" :id="$me.id + '_' + option.value + '_no'" :checked="isNoChecked(option.value)" value="false"
+                        <input class="wb-radio" type="radio" 
+                            :name="$me.id + '_' + option.value" 
+                            :id="$me.id + '_' + option.value + '_no'" 
+                            :checked="isNoChecked(option.value)"
+                            :disabled="!$me.acceptAnswer"
+                            value="false"
                             @click="answerNo(option.value)" />
                         <label :for="$me.id + '_' + option.value + '_no'">
                             <span class="tick"></span>
                         </label>
                         <span>{{option.title}}</span>
-                        <button type="submit" class="btn btn-link btn-clear" @click="clearAnswer(option.value)">
+                        <button type="submit" v-if="$me.acceptAnswer" class="btn btn-link btn-clear" @click="clearAnswer(option.value)">
                             <span></span>
                         </button>
                         <div class="badge" v-if="$me.ordered">{{ getAnswerOrder(option.value)}}</div>
                     </div>
                 </div>
+                <wb-lock />
             </div>
         </div>
     </wb-question>
@@ -57,30 +68,32 @@
                  return true;
             },
             isCanBeenAnswered(optionValue){
-                const yesAnswers = $.grep(this.$me.answer, function(e){ return e.yes; });
+                if(!this.$me.acceptAnswer) return false;
+
+                const yesAnswers = $.grep(this.$me.answer, (e) => { return e.yes; });
                 const isMaxLimitReached = this.$me.maxSelectedAnswersCount && yesAnswers.length >= this.$me.maxSelectedAnswersCount;
                 return isMaxLimitReached;
             },
             getAnswerOrder(optionValue){
-                const yesAnswers = $.grep(this.$me.answer, function(e){ return e.yes; });
-                const answerIndex = findIndex(yesAnswers, function(x) { return x.value == optionValue })
+                const yesAnswers = $.grep(this.$me.answer, (e) => { return e.yes; });
+                const answerIndex = findIndex(yesAnswers, (x) =>  { return x.value == optionValue })
                 return  answerIndex > -1 ? answerIndex + 1 : ""
             },
             isYesChecked(optionValue) {
-                const answerObj = $.grep(this.$me.answer, function(e){ return e.value == optionValue; });
+                const answerObj = $.grep(this.$me.answer, (e) => { return e.value == optionValue; });
                 return answerObj.length == 0 ? false : answerObj[0].yes;
             },
             isNoChecked(optionValue) {
-                const answerObj = $.grep(this.$me.answer, function(e){ return e.value == optionValue; });
+                const answerObj = $.grep(this.$me.answer, (e) => { return e.value == optionValue; });
                 return answerObj.length == 0 ? false : !answerObj[0].yes;
             },
             sendAnswer(optionValue, answerValue) {
                 const previousAnswer = this.$me.answer;
-                const isChangedAnswerOnOption = findIndex(previousAnswer, function(x) { return x.value == optionValue && x.yes == answerValue}) < 0;
+                const isChangedAnswerOnOption = findIndex(previousAnswer, (x) => { return x.value == optionValue && x.yes == answerValue}) < 0;
                 if (!isChangedAnswerOnOption)
                     return;
 
-                const previousAnswerWithoutCurrecntAnswered = $.grep(this.$me.answer, function(e){ return e.value != optionValue; });
+                const previousAnswerWithoutCurrecntAnswered = $.grep(this.$me.answer, (e) =>{ return e.value != optionValue; });
                 const newAnswer = answerValue == null
                     ? previousAnswerWithoutCurrecntAnswered
                     : previousAnswerWithoutCurrecntAnswered.concat([{ value: optionValue, yes: answerValue }]);
