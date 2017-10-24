218c6a51ea7e20f4987b49f597639b88862c1bb1
Slava Scherbak <slava.scherbak@gmail.com>
2017-10-24 15:38:49 -0400

    KP-10190: added migration to add missing column to questionnaire list view items table

---
+++ src/UI/Designer/WB.UI.Designer/Migrations/PlainStore/M001-100/M2001710241530_AddFolderIdToQuestionnaireListViewItemTable.cs
@@ -0,0 +1,18 @@
+﻿using FluentMigrator;
+
+namespace WB.UI.Designer.Migrations.PlainStore
+{
+    [Migration(2001710241530)]
+    public class M2001710241530_AddFolderIdToQuestionnaireListViewItemTable : Migration
+    {
+        public override void Up()
+        {
+            this.Create.Column("folderid").OnTable("questionnairelistviewitems").AsGuid().Nullable();
+        }
+
+        public override void Down()
+        {
+            this.Delete.Column("folderid").FromTable("questionnairelistviewitems");
+        }
+    }
+}
