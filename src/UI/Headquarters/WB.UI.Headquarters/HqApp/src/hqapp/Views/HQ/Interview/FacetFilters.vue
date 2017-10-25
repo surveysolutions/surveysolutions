849a3ce78369a1e07b1e08a9b8527440ff5ae530
Victor Hladkikh <me@alfeg.net>
2017-10-25 12:55:56 +0300

    KP-10181: Filters #  Basic store implementation and link it to filters selection

--- src/UI/Headquarters/WB.UI.Headquarters/HqApp/src/hqapp/Views/HQ/Interview/FacetFilters.vue
+++ src/UI/Headquarters/WB.UI.Headquarters/HqApp/src/hqapp/Views/HQ/Interview/FacetFilters.vue
@@ -1,63 +1,42 @@
 <template>
     <div>
+        <li class="dropdown language">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" :title="currentLanguage">{{currentLanguage}}
+                <span class="caret" v-if="canChangeLanguage"></span>
+            </a>
+            <ul class="dropdown-menu" v-if="canChangeLanguage">
+                <li v-if="currentLanguage != $store.state.webinterview.originalLanguageName">
+                    <a href="javascript:void(0)" @click="changeLanguage()">{{ $store.state.webinterview.originalLanguageName }}</a>
+                </li>
+                <li :key="language.OriginalLanguageName" v-for="language in $store.state.webinterview.languages" v-if="language != $store.state.webinterview.currentLanguage">
+                    <a href="javascript:void(0)" @click="changeLanguage(language)">{{ language }}</a>
+                </li>
+            </ul>
+        </li>
+
         <div class="filters-container">
-            <h4>Filter Questions</h4>
-            <div class="block-filter separate-filters-group">
-                <div class="form-group">
-                    <input class="checkbox-filter" id="flagged" type="checkbox">
-                    <label for="flagged">
-                        <span class="tick"></span>Flagged
-                    </label>
-                </div>
-                <div class="form-group">
-                    <input class="checkbox-filter" id="with-comments" type="checkbox">
-                    <label for="with-comments">
-                        <span class="tick"></span>With comments
-                    </label>
-                </div>
-            </div>
-            <div class="block-filter separate-filters-group">
-                <div class="form-group">
-                    <input class="checkbox-filter" id="Invalid" type="checkbox">
-                    <label for="Invalid">
-                        <span class="tick"></span>Invalid
-                    </label>
-                </div>
-                <div class="form-group">
-                    <input class="checkbox-filter" id="valid" type="checkbox">
-                    <label for="valid">
-                        <span class="tick"></span>Valid
-                    </label>
-                </div>
-            </div>
-            <div class="block-filter separate-filters-group">
-                <div class="form-group">
-                    <input class="checkbox-filter" id="answered" type="checkbox">
-                    <label for="answered">
-                        <span class="tick"></span>Answered
-                    </label>
-                </div>
-                <div class="form-group">
-                    <input class="checkbox-filter" id="unanswered" type="checkbox">
-                    <label for="unanswered">
-                        <span class="tick"></span>Unanswered
-                    </label>
-                </div>
-            </div>
-            <div class="block-filter">
-                <div class="form-group">
-                    <input class="checkbox-filter" id="for-supervisor" type="checkbox">
-                    <label for="for-supervisor">
-                        <span class="tick"></span>For supervisor
-                    </label>
-                </div>
-                <div class="form-group">
-                    <input class="checkbox-filter" id="for-interviewer" type="checkbox">
-                    <label for="for-interviewer">
-                        <span class="tick"></span>For interviewer
-                    </label>
-                </div>
-            </div>
+            <h4>{{ $t("WebInterview.Filters_Title") }}</h4>
+
+            <filters-block>
+                <filter-item id="flagged" :title="$t('WebInterview.Filters_Flagged')" @change="change" :state="state.flagged" />
+                <filter-item id="withComments" :title="$t('WebInterview.Filters_WithComments')" @change="change" :state="state.withComments" />
+            </filters-block>
+
+            <filters-block>
+                <filter-item id="invalid" :title="$t('WebInterview.Filters_Invalid')" @change="change" :state="state.invalid" />
+                <filter-item id="valid" :title="$t('WebInterview.Filters_Valid')" @change="change" :state="state.valid" />
+            </filters-block>
+
+            <filters-block>
+                <filter-item id="answered" :title="$t('WebInterview.Filters_Answered')" @change="change" :state="state.answered" />
+                <filter-item id="unanswered" :title="$t('WebInterview.Filters_Unanswered')" @change="change" :state="state.unanswered" />
+            </filters-block>
+
+            <filters-block>
+                <filter-item id="forSupevisor" :title="$t('WebInterview.Filters_ForSupervisor')" @change="change" :state="state.forSupevisor" />
+                <filter-item id="forInterviewer" :title="$t('WebInterview.Filters_ForInterviewer')" @change="change" :state="state.forInterviewer" />
+            </filters-block>
+
         </div>
         <div class="preset-filters-container">
             <div class="block-filter">
@@ -69,15 +48,45 @@
     </div>
 </template>
 <script>
+import FiltersBlock from "./components/FiltersBlock";
+import FilterItem from "./components/FilterItem";
+
 export default {
   computed: {
+    state() {
+      return this.$store.getters.filteringState;
+    },
+
     oldPageUrl() {
       return (
         window.input.settings.config.basePath +
         "Interview/Details/" +
         this.$route.params.interviewId
       );
+    },
+    canChangeLanguage() {
+      return (
+        this.$store.state.webinterview.languages != undefined &&
+        this.$store.state.webinterview.languages.length > 0
+      );
+    },
+    currentLanguage() {
+      return (
+        this.$store.state.webinterview.currentLanguage ||
+        this.$store.state.webinterview.originalLanguageName
+      );
     }
+  },
+  methods: {
+    change({ id, value }) {
+      this.$store.dispatch("applyFiltering", { filter: id, value });
+    },
+    changeLanguage(language) {
+      this.$store.dispatch("changeLanguage", { language: language });
+    }
+  },
+  components: {
+      FilterItem, FiltersBlock
   }
 };
 </script>
---
+++ src/UI/Headquarters/WB.UI.Headquarters/HqApp/src/hqapp/Views/HQ/Interview/components/FiltersBlock.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="block-filter separate-filters-group">
+        <slot />
+    </div>
+</template>
+
+<script>
+export default {
+    
+};
+</script>
