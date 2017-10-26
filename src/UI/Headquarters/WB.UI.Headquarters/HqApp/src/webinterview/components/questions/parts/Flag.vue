873f6ac9d717bcf02c3e6ee747e1ac18c0dca20f
Roman Lytvyn <litvin.roma@gmail.com>
2017-10-26 15:10:29 +0300

    KP-10182 HQ: Interview details. ability to put a flag on question

---
+++ src/UI/Headquarters/WB.UI.Headquarters/HqApp/src/webinterview/components/questions/parts/Flag.vue
@@ -0,0 +1,18 @@
+<template>
+    <button type="button" class="btn-link button-flag" @click="setFlag">
+    </button>
+</template>
+<script lang="js">
+    import { entityPartial } from "~/webinterview/components/mixins"
+
+    export default {
+        mixins: [entityPartial],
+        name: "wb-flag",
+        methods:{
+            setFlag(){
+                const hasFlag = this.$store.getters.flags.indexOf(this.$me.id) > -1;
+                this.$store.dispatch("setFlag", { questionId: this.$me.id, hasFlag: !hasFlag})
+            }
+        }
+    }
+</script>
