9bfe0830db20792f6d40bf563fe8c0c143efbadb
Victor Hladkikh <me@alfeg.net>
2017-10-31 14:19:28 +0200

    Improved hq app runtime performance
    https://medium.com/tldr-tech/web-performance-a-poc-by-example-41307c7e6cbe

--- src/UI/Headquarters/WB.UI.Headquarters/HqApp/.build/webpack.common.js
+++ src/UI/Headquarters/WB.UI.Headquarters/HqApp/.build/webpack.common.js
@@ -160,7 +160,28 @@ module.exports = function (appConfig) {
 
             // minizing js files
             devMode ? null : new webpack.optimize.UglifyJsPlugin({
-                sourceMap: true
+                sourceMap: true,
+                minimize: true,
+                compress: { // https://medium.com/tldr-tech/web-performance-a-poc-by-example-41307c7e6cbe
+                  warnings: false, // warn about potentially dangerous optimizations/code
+                  sequences: true,  // join consecutive statemets with the “comma operator”
+                  properties: true,  // optimize property access: a["foo"] → a.foo
+                  dead_code: true,  // discard unreachable code
+                  drop_debugger: true,  // discard “debugger” statements
+                  unsafe: true, // some unsafe optimizations (see below)
+                  conditionals: true,  // optimize if-s and conditional expressions
+                  comparisons: true,  // optimize comparisons
+                  evaluate: true,  // evaluate constant expressions
+                  booleans: true,  // optimize boolean expressions
+                  loops: true,  // optimize loops
+                  unused: true,  // drop unused variables/functions
+                  hoist_funs: true,  // hoist function declarations
+                  hoist_vars: true, // hoist variable declarations
+                  if_return: true,  // optimize if-s followed by return/continue
+                  join_vars: true,  // join var declarations
+                  cascade: true,  // try to cascade `right` into `left` in sequences
+                  side_effects: true,  // drop side-effect-free statements
+                }
             }),
 
             // make sure that build will be optimized for production. Required by vuejs
