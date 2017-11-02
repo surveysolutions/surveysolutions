ab04894bf2bf52694970d975dfb0157a11755bb8
Victor Hladkikh <me@alfeg.net>
2017-11-02 09:16:03 +0200

    HqApp # Changed the way localization load

--- src/UI/Headquarters/WB.UI.Headquarters/HqApp/src/shared/plugins/locale.js
+++ src/UI/Headquarters/WB.UI.Headquarters/HqApp/src/shared/plugins/locale.js
@@ -3,34 +3,27 @@ import VueI18n from 'vue-i18n'
 import BaseFormatter from '~/shared/localization/customFormatter'
 Vue.use(VueI18n)
 
-import api from '~/shared/api'
-
 export default {
-    initializeAsync(browserLanguage) {
+    initialize(browserLanguage) {
         const locale = browserLanguage.split('-')[0];
-        
-        return api.resources.locale(locale)
-            .then(messages => {
 
-                const options = {
-                    locale,
-                    formatter: new BaseFormatter(),
-                    fallbackLocale: 'en',
-                    messages: {
-                        [locale]: messages.data
-                    }
-                }
+        const options = {
+            locale,
+            formatter: new BaseFormatter(),
+            fallbackLocale: 'en',
+            messages: {
+                [locale]: window.CONFIG.locale.data
+            }
+        }
 
-                const i18n = new VueI18n(options);
-                Vue.use(i18n);
+        const i18n = new VueI18n(options);
+        Vue.use(i18n);
 
-                // setting up global access to $t function
-                Vue.$t = function () {
-                    return i18n.t.apply(i18n, arguments);
-                }
+        // setting up global access to $t function
+        Vue.$t = function() {
+            return i18n.t.apply(i18n, arguments);
+        }
 
-                return i18n;
-            });
+        return i18n;
     }
 }
-
