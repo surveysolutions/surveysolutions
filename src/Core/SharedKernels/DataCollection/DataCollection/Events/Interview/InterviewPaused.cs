5f8a2532862d583bcd6913549f2cd635c7c65722
Andrii Kozhyn <hedgic@gmail.com>
2017-11-02 12:05:18 +0200

    KP-10205: Added events/commands for pause resume

---
+++ src/Core/SharedKernels/DataCollection/DataCollection/Events/Interview/InterviewPaused.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using WB.Core.SharedKernels.DataCollection.Events.Interview.Base;
+
+namespace WB.Core.SharedKernels.DataCollection.Events.Interview
+{
+    public class InterviewPaused : InterviewActiveEvent
+    {
+        public InterviewPaused(Guid userId) : base(userId)
+        {
+        }
+
+        public DateTime LocalTime { get; set; }
+    }
+}
