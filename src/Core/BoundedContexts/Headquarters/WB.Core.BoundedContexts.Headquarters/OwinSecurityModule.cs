9c0f3739d4c044619c3c9997324d5cd9a82ab84d
Andrii Kozhyn <hedgic@gmail.com>
2017-11-02 17:57:33 +0200

    KP-10205: Implemented persistence of interview paused event

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/OwinSecurityModule.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/OwinSecurityModule.cs
@@ -26,7 +26,7 @@ namespace WB.Core.BoundedContexts.Headquarters
             this.Bind<IIdentityValidator<string>>().To<HqPasswordValidator>();
 
             //this.Bind<IOwinContext>().ToMethod(context => HttpContext.Current.GetOwinContext());
-            this.Bind<IAuthenticationManager>().ToMethod(context => HttpContext.Current.GetOwinContext().Authentication);
+            this.Bind<IAuthenticationManager>().ToMethod(context => HttpContext.Current.GetOwinContext().Authentication).InRequestScope();
 
             // no on per request scope required - lifetime managed by their parents controllers/handlers
             this.Bind<HQIdentityDbContext>().ToSelf();
