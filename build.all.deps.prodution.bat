58f11af7e3b4f79e76041d283f140434cafdd7d7
Victor Gladkikh <me@alfeg.net>
2017-11-06 15:49:37 +0200

    KP-10437 Apply new style files # Fixed glob pattern for css selction. Fixed webinterview

---
+++ build.all.deps.prodution.bat
@@ -0,0 +1,3 @@
+start cmd  /c "TITLE hq deps && cd src\UI\Headquarters\WB.UI.Headquarters\Dependencies && yarn && yarn gulp --production"
+start cmd /c "TITLE Hq App && cd src\UI\Headquarters\WB.UI.Headquarters\HqApp && yarn && yarn gulp --production"
+start cmd /c "TITLE Designer Questionnaire && cd src\UI\Designer\WB.UI.Designer\questionnaire && yarn && yarn gulp --production"
