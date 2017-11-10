9cf9f17cf8fff205ebdcd3f0d97eafe57d4b9e9b
Pavel Stovpivskyi <630198+spuv@users.noreply.github.com>
2017-11-10 13:53:27 +0200

    Added show location in tooltip

--- src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Views/Questionnaire/QuestionnaireList/IQuestionnaireListViewFactory.cs
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Views/Questionnaire/QuestionnaireList/IQuestionnaireListViewFactory.cs
@@ -7,6 +7,7 @@ namespace WB.Core.BoundedContexts.Designer.Views.Questionnaire.QuestionnaireList
     {
         QuestionnaireListView Load(QuestionnaireListInputModel input);
         QuestionnaireListView LoadFoldersAndQuestionnaires(QuestionnaireListInputModel input);
+        IEnumerable<QuestionnaireListFolderLocation> LoadFoldersLocation(IEnumerable<QuestionnaireListViewFolder> folders);
         IReadOnlyCollection<QuestionnaireListViewItem> GetUserQuestionnaires(Guid userId, bool isAdmin, int pageIndex = 1, int pageSize = 128);
     }
 }
---
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Views/Questionnaire/QuestionnaireList/QuestionnaireListFolderLocation.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+namespace WB.Core.BoundedContexts.Designer.Views.Questionnaire.QuestionnaireList
+{
+    public class QuestionnaireListFolderLocation
+    {
+        public Guid PublicId { get; set; }
+
+        public string Location { get; set; }
+    }
+}
