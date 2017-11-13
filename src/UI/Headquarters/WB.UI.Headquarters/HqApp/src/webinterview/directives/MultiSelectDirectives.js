a8c7b1562df1879b61ee6c58dffd30d9acb36e38
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2017-11-13 16:44:36 -0500

    KP-10458 Interview details: admin/HQ/SP able to select Yes option in Yes/No question

--- src/UI/Headquarters/WB.UI.Headquarters/HqApp/src/webinterview/directives/MultiSelectDirectives.js
+++ src/UI/Headquarters/WB.UI.Headquarters/HqApp/src/webinterview/directives/MultiSelectDirectives.js
@@ -3,9 +3,19 @@ import Vue from "vue"
 Vue.directive("disabledWhenUnchecked", {
     bind: (el, binding) => {
         if(el.disabled) return;
-        el.disabled = binding.value && !el.checked
+
+        if(binding.value.answerNotAllowed){
+            el.disabled = true;
+            return;
+        }
+        el.disabled = binding.value.maxAnswerReached && !el.checked
     },
     update: (el, binding) => {
-        el.disabled = binding.value && !el.checked
+
+        if(binding.value.answerNotAllowed){
+            el.disabled = true;
+            return;
+        }
+        el.disabled = binding.value.maxAnswerReached && !el.checked
     }
 })
