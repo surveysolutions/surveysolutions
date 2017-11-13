738856fb002ed26e59607a1d5207f51a1bf84ca4
Pavel Stovpivskyi <sby@rambler.ru>
2017-11-13 18:39:23 +0200

    KP-10484 disable tree folders scripts for usual user & disable remove in popup

--- src/UI/Designer/WB.UI.Designer/App_Start/BundleConfig.cs
+++ src/UI/Designer/WB.UI.Designer/App_Start/BundleConfig.cs
@@ -48,7 +48,7 @@ namespace WB.UI.Designer
                     "~/Content/plugins/bootbox.min.js",
                     "~/Scripts/custom/public-folders.js" }));
             bundles.Add(
-                new StyleBundle("~/Content/plugins/skin").Include( new [] {
+                new StyleBundle("~/Content/plugins/folders-skin").Include( new [] {
                     "~/Content/plugins/ui.fancytree.min.css",
                     "~/Content/plugins/jquery.contextMenu.min.css"
                 }));
