e61688c8b868f5bdba96d311be8cd84b5ef7e956
Andrii Kozhyn <hedgic@gmail.com>
2017-11-16 14:44:40 +0200

    Moved types to own files

---
+++ src/Core/SharedKernels/DataCollection/DataCollection/Commands/Interview/CloseInterviewBySupervisorCommand.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace WB.Core.SharedKernels.DataCollection.Commands.Interview
+{
+    public class CloseInterviewBySupervisorCommand : PauseInterviewCommand
+    {
+        public CloseInterviewBySupervisorCommand(Guid interviewId, Guid userId, DateTime localTime) : base(interviewId, userId, localTime)
+        {
+        }
+    }
+}
---
+++ src/Core/SharedKernels/DataCollection/DataCollection/Commands/Interview/OpenInterviewBySupervisorCommand.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace WB.Core.SharedKernels.DataCollection.Commands.Interview
+{
+    public class OpenInterviewBySupervisorCommand : ResumeInterviewCommand
+    {
+        public OpenInterviewBySupervisorCommand(Guid interviewId, Guid userId, DateTime localTime) : base(interviewId, userId, localTime)
+        {
+        }
+    }
+}
