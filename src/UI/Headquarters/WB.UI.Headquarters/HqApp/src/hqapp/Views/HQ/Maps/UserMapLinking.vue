adb770291cfccb5a053bad8bcbaed92a35777248
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2017-11-20 16:42:36 -0500

    KP-10510 Upload users page information section is shifted

--- src/UI/Headquarters/WB.UI.Headquarters/HqApp/src/hqapp/Views/HQ/Maps/UserMapLinking.vue
+++ src/UI/Headquarters/WB.UI.Headquarters/HqApp/src/hqapp/Views/HQ/Maps/UserMapLinking.vue
@@ -11,20 +11,22 @@
                     <p>{{$t("Pages.MapLinking_Description")}}</p>  
         </div>                
         <div class="row flex-row">
-            <div class="flex-block" style="margin-left:0px;">
-                <div class="selection-box">
+            <div class="flex-block">
+                <div class="selection-box reset-margin">
                     <div class="block">
                         <h3>{{$t("Pages.MapLinking_UploadDescriptionTitle")}}</h3>
                         <p>{{$t("Pages.MapLinking_UploadDescription")}}</p>
                     </div>
                     <div>
                         <a :href="$config.model.downloadAllUrl">{{$t("Pages.MapLinking_DownloadExisting")}}</a>
-                    <form :action="$config.model.uploadUrl" enctype="multipart/form-data" id="MapsUploadForm" method="post">
-                        <label class="btn btn-success btn-file">
-                            {{$t("Pages.MapLinking_UploadFile")}}
+                    <div class="info-block">
+                        <form :action="$config.model.uploadUrl" enctype="multipart/form-data" id="MapsUploadForm" method="post">
+                            <label class="btn btn-success btn-file">
+                                {{$t("Pages.MapLinking_UploadFile")}}
                             <input accept=".tsv" id="File" name="File" onchange="this.form.submit()" type="file" value="" />
-                        </label>
-                    </form>
+                            </label>
+                        </form>
+                    </div>
                     <div>                    
                         <p>{{$t("Pages.MapLinking_UploadFileDescription")}}</p>
                         <p>{{$t("Pages.MapLinking_UploadFileDescription1")}}</p>
