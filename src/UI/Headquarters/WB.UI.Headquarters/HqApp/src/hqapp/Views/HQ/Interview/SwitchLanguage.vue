b405057295b81d31c6f9d949009897815a20197a
Roman Lytvyn <litvin.roma@gmail.com>
2017-11-21 11:54:29 +0200

    KP-10509 HQ UI: If translation has a long title - it is overlaid onto the section

--- src/UI/Headquarters/WB.UI.Headquarters/HqApp/src/hqapp/Views/HQ/Interview/SwitchLanguage.vue
+++ src/UI/Headquarters/WB.UI.Headquarters/HqApp/src/hqapp/Views/HQ/Interview/SwitchLanguage.vue
@@ -1,36 +1,28 @@
 <template>
     <div class="filters-container">
         <h4>{{$t("Pages.InterviewDetails_TranslationLabel")}}</h4>
-        <div class="dropdown">
-            <button class="btn btn-default dropdown-toggle" type="button" id="languageMenu" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-                {{currentLanguage}}
-                <span class="caret"></span>
-            </button>
-            <ul class="dropdown-menu" aria-labelledby="languageMenu">
-                <li v-if="currentLanguage != $store.state.webinterview.originalLanguageName">
-                    <a href="javascript:void(0)" @click="changeLanguage()">{{ $store.state.webinterview.originalLanguageName }}</a>
-                </li>
-                <li :key="language.OriginalLanguageName" v-for="language in $store.state.webinterview.languages" v-if="language != $store.state.webinterview.currentLanguage">
-                    <a href="javascript:void(0)" @click="changeLanguage(language)">{{ language }}</a>
-                </li>
-            </ul>
-        </div>
+        <select ref="select" v-model="currentLanguage">
+            <option :value="null">{{$store.state.webinterview.originalLanguageName}}</option>
+            <option :key="language.OriginalLanguageName" v-for="language in $store.state.webinterview.languages">{{ language }}</option>
+        </select>
     </div>
 </template>
 <script>
 export default {
-  computed: {
-    currentLanguage() {
-      return (
-        this.$store.state.webinterview.currentLanguage ||
-        this.$store.state.webinterview.originalLanguageName
-      );
-    }
+  data: function() {
+    return { currentLanguage: null };
   },
   methods: {
     changeLanguage(language) {
-      this.$store.dispatch("changeLanguage", { language: language });
+      this.$store.dispatch("changeLanguage", { language });
     }
+  },
+  updated() {
+    $(this.$refs.select).selectpicker("refresh");
+  },
+  mounted() {
+    this.currentLanguage = this.$store.state.webinterview.currentLanguage;
+    this.$watch("currentLanguage", this.changeLanguage);
   }
 };
 </script>
