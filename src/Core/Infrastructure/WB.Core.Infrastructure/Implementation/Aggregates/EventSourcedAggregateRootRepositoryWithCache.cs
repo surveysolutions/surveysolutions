228c2f099276297ce882098af3a2bbe62a1b197f
Pavel Stovpivskyi <sby@rambler.ru>
2017-11-21 19:43:22 +0200

    Revert "KP-10515 Removed EventSourcedAggregateRootRepositoryWithCache "
    This reverts commit 8df0e190307f37bab2e259dfb8df720f1c8dcfcb.
    
    # Conflicts:
    #	src/Tests/WB.Tests.Unit/Infrastructure/EventSourcedAggregateRootRepositoryTests/EventSourcedAggregateRootRepositoryWithCacheTests.cs

---
+++ src/Core/Infrastructure/WB.Core.Infrastructure/Implementation/Aggregates/EventSourcedAggregateRootRepositoryWithCache.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using Ncqrs.Domain.Storage;
+using Ncqrs.Eventing.Storage;
+using WB.Core.GenericSubdomains.Portable;
+using WB.Core.Infrastructure.Aggregates;
+using WB.Core.Infrastructure.WriteSide;
+
+namespace WB.Core.Infrastructure.Implementation.Aggregates
+{
+    internal class EventSourcedAggregateRootRepositoryWithCache : EventSourcedAggregateRootRepository, IEventSourcedAggregateRootRepositoryWithCache
+    {
+        private readonly IAggregateLock aggregateLock;
+        private readonly ConcurrentDictionary<Type, IEventSourcedAggregateRoot> memoryCache = new ConcurrentDictionary<Type, IEventSourcedAggregateRoot>();
+
+        public EventSourcedAggregateRootRepositoryWithCache(IEventStore eventStore, ISnapshotStore snapshotStore, IDomainRepository repository, IAggregateLock aggregateLock)
+            : base(eventStore, snapshotStore, repository)
+        {
+            this.aggregateLock = aggregateLock;
+        }
+        
+        public override IEventSourcedAggregateRoot GetLatest(Type aggregateType, Guid aggregateId)
+            => this.GetLatest(aggregateType, aggregateId, null, CancellationToken.None);
+
+        public override IEventSourcedAggregateRoot GetLatest(Type aggregateType, Guid aggregateId, IProgress<EventReadingProgress> progress, CancellationToken cancellationToken)
+        {
+            return aggregateLock.RunWithLock(aggregateId.FormatGuid(), () =>
+            {
+                var aggregateRoot =
+                    this.GetFromCache(aggregateType, aggregateId) ??
+                    base.GetLatest(aggregateType, aggregateId, progress, cancellationToken);
+
+                if (aggregateRoot != null)
+                {
+                    this.memoryCache[aggregateType] = aggregateRoot;
+                }
+
+                return aggregateRoot;
+            });
+        }
+
+        private IEventSourcedAggregateRoot GetFromCache(Type aggregateType, Guid aggregateId)
+        {
+            bool foundInCache = this.memoryCache.TryGetValue(aggregateType, out var cachedAggregate);
+            if (!foundInCache)
+                return null;
+
+            bool hasSameId = cachedAggregate.EventSourceId == aggregateId;
+            if (!hasSameId)
+                return null;
+
+            bool isDirty = cachedAggregate.HasUncommittedChanges();
+            if (isDirty)
+                return null;
+
+            return cachedAggregate;
+        }
+
+        public void CleanCache()
+        {
+            this.memoryCache.Clear();
+        }
+    }
+}
