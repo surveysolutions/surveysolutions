4d1ec126bee3806d20bbe25882d57aa3c377c0d7
Roman Lytvyn <litvin.roma@gmail.com>
2017-11-22 10:12:50 +0200

    KP-10530 IN: Interviewer crashes after sign out on interview details

--- src/Core/SharedKernels/DataCollection/DataCollection/Commands/Interview/Base/InterviewCommand.cs
+++ src/Core/SharedKernels/DataCollection/DataCollection/Commands/Interview/Base/InterviewCommand.cs
@@ -6,26 +6,13 @@ namespace WB.Core.SharedKernels.DataCollection.Commands.Interview.Base
     public abstract class InterviewCommand : CommandBase
     {
         public Guid InterviewId { get; private set; }
+        public Guid UserId { get; set; }
 
-        private Guid userId;
-        public Guid UserId
-        {
-            get
-            {
-                if (this.userId == Guid.Empty)
-                    throw new ArgumentException("User ID cannot be empty.");
-
-                return this.userId;
-            }
-
-            set { this.userId = value; }
-        }
-
-        protected InterviewCommand(Guid interviewId, Guid userId)
+        protected InterviewCommand(Guid interviewId, Guid? userId)
             : base(interviewId)
         {
             this.InterviewId = interviewId;
-            this.UserId = userId;
+            this.UserId = userId ?? Guid.Empty;
         }
     }
 }
