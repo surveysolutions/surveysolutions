34c1767cfb7d7e10f8529c998f8728ba4e68821b
Victor Gladkikh <me@alfeg.net>
2017-11-24 17:08:04 +0200

    KP-10495 Setup telemetry infra # Fix for StateFullInterviewsCount

---
+++ src/Infrastructure/WB.Infrastructure.Native/Monitoring/Counter.cs
@@ -0,0 +1,44 @@
+﻿namespace WB.Infrastructure.Native.Monitoring
+{
+    public class Counter : Counter.ICounter
+    {
+        private readonly Prometheus.Counter counter;
+
+        public Counter(string name, string help, params string[] labelNames)
+        {
+            this.counter = Prometheus.Metrics.CreateCounter(name, help, labelNames);
+        }
+
+        public void Inc(double amount = 1)
+        {
+            counter.Inc(amount);
+        }
+
+        public ICounter Label(params string[] labels)
+        {
+            return new ChildCounter(counter, labels);
+        }
+
+        private class ChildCounter : ICounter
+        {
+            private readonly Prometheus.Counter counter;
+            private readonly string[] labels;
+
+            public ChildCounter(Prometheus.Counter counter, string[] labels)
+            {
+                this.counter = counter;
+                this.labels = labels;
+            }
+
+            public void Inc(double amount = 1)
+            {
+                this.counter.Labels(labels).Inc(amount);
+            }
+        }
+
+        public interface ICounter
+        {
+            void Inc(double amount = 1);
+        }
+    }
+}
