f412ab91972ccc6c55f658557faab27a730e9bc0
Andrii Kozhyn <hedgic@gmail.com>
2017-11-29 16:56:54 +0200

    fixed merge

--- src/Core/SharedKernels/DataCollection/DataCollection/Commands/Interview/PauseInterviewCommand.cs
+++ src/Core/SharedKernels/DataCollection/DataCollection/Commands/Interview/PauseInterviewCommand.cs
@@ -3,15 +3,10 @@ using WB.Core.SharedKernels.DataCollection.Commands.Interview.Base;
 
 namespace WB.Core.SharedKernels.DataCollection.Commands.Interview
 {
-    public class PauseInterviewCommand : InterviewCommand
+    public class PauseInterviewCommand : TimestampedInterviewCommand
     {
-        public PauseInterviewCommand(Guid interviewId, Guid userId, DateTime localTime, DateTime utcTime) : base(interviewId, userId)
+        public PauseInterviewCommand(Guid interviewId, Guid userId, DateTime localTime, DateTime utcTime) : base(interviewId, userId, localTime, utcTime)
         {
-            LocalTime = localTime;
-            UtcTime = utcTime;
         }
-
-        public DateTime LocalTime { get; set; }
-        public DateTime UtcTime { get; }
     }
 }
