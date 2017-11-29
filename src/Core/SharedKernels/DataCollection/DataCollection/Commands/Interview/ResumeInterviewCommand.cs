c98cbc5e845c216a826a05c8f5969409e4d83bc4
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2017-11-29 16:56:54 +0200

    fixed merge

--- src/Core/SharedKernels/DataCollection/DataCollection/Commands/Interview/ResumeInterviewCommand.cs
+++ src/Core/SharedKernels/DataCollection/DataCollection/Commands/Interview/ResumeInterviewCommand.cs
@@ -3,15 +3,10 @@ using WB.Core.SharedKernels.DataCollection.Commands.Interview.Base;
 
 namespace WB.Core.SharedKernels.DataCollection.Commands.Interview
 {
-    public class ResumeInterviewCommand : InterviewCommand
+    public class ResumeInterviewCommand : TimestampedInterviewCommand
     {
-        public ResumeInterviewCommand(Guid interviewId, Guid userId, DateTime localTime, DateTime utcTime) : base(interviewId, userId)
+        public ResumeInterviewCommand(Guid interviewId, Guid userId, DateTime localTime, DateTime utcTime) : base(interviewId, userId, localTime, utcTime)
         {
-            LocalTime = localTime;
-            UtcTime = utcTime;
         }
-
-        public DateTime LocalTime { get; set; }
-        public DateTime UtcTime { get; }
     }
 }
