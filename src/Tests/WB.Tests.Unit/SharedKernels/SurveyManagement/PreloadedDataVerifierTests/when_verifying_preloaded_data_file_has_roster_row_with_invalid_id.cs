723235eab4aace49083df26ecb6b633e99abdf08
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2017-12-01 19:12:38 -0500

    KP-10606 In Export Files rename Id columns to meaningful interview__Id [fix tests]

--- src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/PreloadedDataVerifierTests/when_verifying_preloaded_data_file_has_roster_row_with_invalid_id.cs
+++ src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/PreloadedDataVerifierTests/when_verifying_preloaded_data_file_has_roster_row_with_invalid_id.cs
@@ -40,9 +40,9 @@ namespace WB.Tests.Unit.SharedKernels.SurveyManagement.PreloadedDataVerifierTest
             preloadedDataServiceMock.Setup(x => x.GetIdColumnIndex(preloadedDataByFileRosterLevel)).Returns(0);
             preloadedDataServiceMock.Setup(x => x.GetParentIdColumnIndexes(preloadedDataByFileRosterLevel)).Returns(new []{1});
             preloadedDataServiceMock.Setup(x => x.GetColumnIndexByHeaderName(preloadedDataByFileRosterLevel, Moq.It.IsAny<string>())).Returns(-1);
-            preloadedDataServiceMock.Setup(x => x.FindLevelInPreloadedData(preloadedDataByFileTopLevel.FileName)).Returns(new HeaderStructureForLevel());
+            preloadedDataServiceMock.Setup(x => x.FindLevelInPreloadedData(preloadedDataByFileTopLevel.FileName)).Returns(new HeaderStructureForLevel(){LevelIdColumnName = ServiceColumns.InterviewId});
             preloadedDataServiceMock.Setup(x => x.FindLevelInPreloadedData(preloadedDataByFileRosterLevel.FileName))
-                .Returns(new HeaderStructureForLevel { LevelScopeVector = new ValueVector<Guid>(new[] { Guid.NewGuid() }) });
+                .Returns(new HeaderStructureForLevel { LevelIdColumnName = preloadedDataByFileRosterLevel.FileName + "__id", LevelScopeVector = new ValueVector<Guid>(new[] { Guid.NewGuid() }) });
             preloadedDataServiceMock.Setup(x => x.GetParentDataFile(preloadedDataByFileRosterLevel.FileName, files))
                 .Returns(preloadedDataByFileTopLevel);
 
