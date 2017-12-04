ea3a201cc6db2c9d62ddaca02bc2be574c0224ff
Victor Hladkikh <me@alfeg.net>
2017-12-04 15:50:20 +0200

    KP-10589 Parent section color should be blue in case child section is red
    Removed and rewrite a bunch of tests. Cause everyone do not like TestCases. Hope it helps.

---
+++ src/Tests/WB.Tests.Unit/Applications/Headquarters/WebInterview/Review/Api/GetGroupDetailsTests/when_valid_section_have_invalid_question_in_subgroup.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Main.Core.Documents;
+using Main.Core.Entities.Composite;
+using NUnit.Framework;
+using WB.Tests.Abc;
+using WB.UI.Headquarters.Models.WebInterview;
+
+namespace WB.Tests.Unit.Applications.Headquarters.WebInterview.Review.Api.GetGroupDetailsTests
+{
+    public class when_valid_section_have_invalid_question_in_subgroup : WebInterviewInterviewEntityFactorySpecification
+    {
+        protected override QuestionnaireDocument GetDocument()
+        {
+            return Create.Entity.QuestionnaireDocument(Guid.NewGuid(),
+
+                Create.Entity.Group(SectionA.Id, "Section A", "SecA", children: new IComposite[]
+                {
+                    Create.Entity.TextQuestion(SecA_InterviewerQuestion.Id, text: "Interviewer Question", variable: "text_in"),
+                    
+                    Create.Entity.FixedRoster(SecA_Roster.Id, title: "roster", children: new IComposite[]
+                    {
+                        Create.Entity.TextQuestion(SecA_Roster_InterviewerQuestion.Id, text: "interviewer q in roster",
+                            variable: "text_in_r"),
+                       
+                    }, fixedTitles: new[] {Create.Entity.FixedTitle(1, "Test")}),
+                }));
+        }
+
+        protected override void Because()
+        {
+            AnswerTextQuestions(SecA_InterviewerQuestion, SecA_Roster_InterviewerQuestion);
+            MarkQuestionAsInvalid(SecA_Roster_InterviewerQuestion);
+        }
+
+        [Test]
+        public void should_have_section_validity_valid() => 
+            Assert.That(GetGroupDetails(SectionA, asReviewer: false).Validity.IsValid, Is.EqualTo(true));
+
+        [Test]
+        public void should_have_section_status_Started() => 
+            Assert.That(GetGroupDetails(SectionA, asReviewer: false).Status, Is.EqualTo(GroupStatus.Started));
+    }
+}
