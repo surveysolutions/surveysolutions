ec46c4d9e36b30cbaedcebae5e012b2e13ec4580
Pavel Stovpivskyi <sby@rambler.ru>
2017-12-06 18:41:04 +0200

    moved HeadquartersUIModule to IModule

---
+++ src/UI/Shared/WB.UI.Shared.Web/Modules/IWebIocRegistry.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Http.Filters;
+using WB.Core.Infrastructure.Modularity;
+
+namespace WB.UI.Shared.Web.Modules
+{
+    public interface IWebIocRegistry : IIocRegistry
+    {
+        void BindMvcFilter<T>(System.Web.Mvc.FilterScope filterScope, int? order);
+
+        void BindMvcFilterWhenActionMethodHasNoAttribute<T, TAttribute>(System.Web.Mvc.FilterScope filterScope, int? order);
+
+        void BindHttpFilter<T>(System.Web.Http.Filters.FilterScope filterScope, int? order) where T : IFilter;
+
+        void BindHttpFilterWhenActionMethodHasNoAttribute<T, TAttribute>(System.Web.Http.Filters.FilterScope filterScope, int? order = null) where T : IFilter;
+    }
+}
