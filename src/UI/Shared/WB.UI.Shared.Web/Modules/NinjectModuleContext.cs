5ffb68bdebe7a7e47429020bf09b35e858e0a2a1
Pavel Stovpivskyi <sby@rambler.ru>
2017-12-06 16:40:44 +0200

    moved HeadquartersUIModule to IModule

---
+++ src/UI/Shared/WB.UI.Shared.Web/Modules/NinjectModuleContext.cs
@@ -0,0 +1,21 @@
+﻿using Ninject;
+using Ninject.Activation;
+using WB.Core.Infrastructure.Modularity;
+
+namespace WB.UI.Shared.Web.Modules
+{
+    public class NinjectModuleContext : IModuleContext
+    {
+        private readonly IContext context;
+
+        public NinjectModuleContext(IContext context)
+        {
+            this.context = context;
+        }
+
+        public T Resolve<T>()
+        {
+            return context.Kernel.Get<T>();
+        }
+    }
+}
