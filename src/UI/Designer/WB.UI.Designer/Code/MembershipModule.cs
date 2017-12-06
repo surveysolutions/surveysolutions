46188d22dc68ed964ac1eb30fb86af3b296c4940
Pavel Stovpivskyi <sby@rambler.ru>
2017-12-06 19:50:45 +0200

    moved MembershipModule and DesignerRegistry to IModule

--- src/UI/Designer/WB.UI.Designer/Code/MembershipModule.cs
+++ src/UI/Designer/WB.UI.Designer/Code/MembershipModule.cs
@@ -1,22 +1,21 @@
-﻿using Ninject.Modules;
-using WB.Core.BoundedContexts.Designer.Implementation.Services.Accounts.PasswordStrategies;
+﻿using WB.Core.BoundedContexts.Designer.Implementation.Services.Accounts.PasswordStrategies;
 using WB.Core.BoundedContexts.Designer.MembershipProvider.Roles;
 using WB.Core.BoundedContexts.Designer.Services.Accounts;
 using WB.Core.BoundedContexts.Designer.Views.Account;
 using WB.Core.GenericSubdomains.Portable;
-using WB.UI.Shared.Web.MembershipProvider.Accounts;
+using WB.Core.Infrastructure.Modularity;
 using WB.UI.Shared.Web.MembershipProvider.Settings;
 
 namespace WB.UI.Designer.Code
 {
-    public class MembershipModule : NinjectModule
+    public class MembershipModule : IModule
     {
-        public override void Load()
+        public void Load(IIocRegistry registry)
         {
-            this.Bind<IPasswordStrategy>().To<HashPasswordStrategy>().InSingletonScope();
-            this.Bind<IPasswordPolicy>().ToMethod(_ => PasswordPolicyFactory.CreatePasswordPolicy()).InSingletonScope();
-            this.Bind<IAccountRepository>().ToMethod(c => AccountRepositoryFactory.CreateRepository()).InSingletonScope();
-            this.Bind<IRoleRepository>().To<CQRSRoleRepository>().InSingletonScope();
+            registry.BindAsSingleton<IPasswordStrategy, HashPasswordStrategy>();
+            registry.BindToConstant<IPasswordPolicy>(() => PasswordPolicyFactory.CreatePasswordPolicy());
+            registry.BindToConstant<IAccountRepository>(() => AccountRepositoryFactory.CreateRepository());
+            registry.BindAsSingleton<IRoleRepository, CQRSRoleRepository>();
         }
     }
 }
