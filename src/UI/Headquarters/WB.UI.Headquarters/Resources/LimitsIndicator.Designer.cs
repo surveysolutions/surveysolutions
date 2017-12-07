58a7586532e65bb5821c5dcc2698db3998616760
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2017-12-07 17:56:14 +0200

    Migration to .net 4.7.1

--- src/UI/Headquarters/WB.UI.Headquarters/Resources/LimitsIndicator.Designer.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/Resources/LimitsIndicator.Designer.cs
@@ -19,7 +19,7 @@ namespace WB.UI.Headquarters.Resources {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class LimitsIndicator {
@@ -79,7 +79,7 @@ namespace WB.UI.Headquarters.Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to You have exceeded your limit {0} interview(s). You can&apos;t create new interviews any more....
+        ///   Looks up a localized string similar to You have exceeded your limit of {0} interview(s). You can&apos;t create new interviews anymore..
         /// </summary>
         public static string YouHaveExceededYourLimitFormat {
             get {
