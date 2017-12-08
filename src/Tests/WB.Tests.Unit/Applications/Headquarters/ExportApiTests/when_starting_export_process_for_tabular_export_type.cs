35cebb5596205519bf51114a3a288867c5aeab21
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2017-12-08 16:07:09 +0200

    KP-10675: Migration of tests to nunit

--- src/Tests/WB.Tests.Unit/Applications/Headquarters/ExportApiTests/when_starting_export_process_for_tabular_export_type.cs
+++ src/Tests/WB.Tests.Unit/Applications/Headquarters/ExportApiTests/when_starting_export_process_for_tabular_export_type.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Web.Http;
 using System.Web.Http.Results;
 using Machine.Specifications;
@@ -14,17 +14,17 @@ namespace WB.Tests.Unit.Applications.Headquarters.ExportApiTests
 {
     public class when_starting_export_process_for_tabular_export_type : ExportControllerTestsContext
     {
-        Establish context = () =>
-        {
+        [NUnit.Framework.OneTimeSetUp] public void context () {
             controller = CreateExportController(dataExportProcessesService: mockOfDataExportProcessesService.Object);
-        };
+            BecauseOf();
+        }
 
-        Because of = () => result = controller.StartProcess(questionnaireIdentity.ToString(), DataExportFormat.Tabular);
+        private void BecauseOf() => result = controller.StartProcess(questionnaireIdentity.ToString(), DataExportFormat.Tabular);
 
-        It should_return_http_ok_response = () =>
+        [NUnit.Framework.Test] public void should_return_http_ok_response () =>
             result.ShouldBeOfExactType<OkResult>();
 
-        It should_call_add_export_method_in_data_export_processes_service = () =>
+        [NUnit.Framework.Test] public void should_call_add_export_method_in_data_export_processes_service () =>
             mockOfDataExportProcessesService.Verify(x=>x.AddDataExport(questionnaireIdentity, DataExportFormat.Tabular, null), Times.Once);
 
         private static ExportController controller;
