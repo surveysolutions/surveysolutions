2506912c1dffaa5fdd1c21283e0ffe2009af9de7
Andrii Kozhyn <hedgic@gmail.com>
2017-12-11 13:45:03 +0200

    KP-10675: Ported tests to clear nunit

--- src/Tests/WB.Tests.Unit.Designer/BoundedContexts/Designer/InterviewCompilerTests/when_generating_assembly_as_string.cs
+++ src/Tests/WB.Tests.Unit.Designer/BoundedContexts/Designer/InterviewCompilerTests/when_generating_assembly_as_string.cs
@@ -1,43 +1,27 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using Machine.Specifications;
-using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.Emit;
-using WB.Core.BoundedContexts.Designer.Services.CodeGeneration;
+using NUnit.Framework;
+using WB.Tests.Abc;
 
 namespace WB.Tests.Unit.Designer.BoundedContexts.Designer.InterviewCompilerTests
 {
     internal class when_generating_assembly_as_string : InterviewCompilerTestsContext
     {
-        [NUnit.Framework.OneTimeSetUp] public void context () {
-            compiler = CreateRoslynCompiler();
-            referencedPortableAssemblies = CreateReferencesForCompiler();
-            generatedClasses.Add("main", testClassToCompile);
-            BecauseOf();
+        [Test] public void context ()
+        {
+            var compiler = CreateRoslynCompiler();
+            var referencedPortableAssemblies = CreateReferencesForCompiler();
+            var generatedClasses = new Dictionary<string, string> {{"main", TestClassToCompile}};
+
+            //Act
+            var emitResult = compiler.TryGenerateAssemblyAsStringAndEmitResult(Id.g1, generatedClasses,
+                referencedPortableAssemblies, out string _);
+
+            //Assert
+            Assert.That(emitResult, Has.Property(nameof(emitResult.Success)).EqualTo(true));
+            Assert.That(emitResult.Diagnostics, Is.Empty);
         }
 
-        private void BecauseOf() =>
-            emitResult = compiler.TryGenerateAssemblyAsStringAndEmitResult(id, generatedClasses, referencedPortableAssemblies, out resultAssembly);
-
-
-        [NUnit.Framework.Test] public void should_result_succeded () =>
-            emitResult.Success.ShouldEqual(true);
-
-        [NUnit.Framework.Test] public void should_diagnostics_count_equal_0 () =>
-            emitResult.Diagnostics.Count().ShouldEqual(0);
-
-        [NUnit.Framework.Test] public void should_ () =>
-            resultAssembly.Length.ShouldBeGreaterThan(0);
-        
-        private static IDynamicCompiler compiler;
-        private static Guid id = Guid.Parse("11111111111111111111111111111111");
-        private static string resultAssembly;
-        private static EmitResult emitResult;
-        private static Dictionary<string, string> generatedClasses = new Dictionary<string, string>();
-        private static List<MetadataReference> referencedPortableAssemblies;
-
-        public static string testClassToCompile =
+        const string TestClassToCompile =
             @"using System.Collections.Generic;
                 using System.Linq;
                 using WB.Core.SharedKernels.DataCollection;
