eac2f8a20752ca80f56e49e9fa315a10823bcd65
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2017-12-12 12:40:29 +0200

    KP-10675: Consolidate all referenced packages. Use net standard 2.0

--- src/UI/Designer/WB.UI.Designer/DynamicCompilerSettingsGroup.cs
+++ src/UI/Designer/WB.UI.Designer/DynamicCompilerSettingsGroup.cs
@@ -13,39 +13,5 @@ namespace WB.UI.Designer
 
         [ConfigurationProperty("dumpFolder", IsRequired = true)]
         public string DumpFolder => (string) this["dumpFolder"];
-
-        public IEnumerable<IDynamicCompilerSettings> SettingsCollection
-        {
-            get
-            {
-                var list = from DynamicCompilerSettings settings in settingsCollection select settings;
-                return list;
-            }
-        }
-
-        [ConfigurationProperty("settings")]
-        DynamicCompilerSettingsCollection settingsCollection => (DynamicCompilerSettingsCollection)base["settings"];
-    }
-
-    [ConfigurationCollection(typeof(DynamicCompilerSettings), AddItemName = "dynamicCompilerSettings")]
-    public class DynamicCompilerSettingsCollection : ConfigurationElementCollection
-    {
-        protected override ConfigurationElement CreateNewElement()
-        {
-            return new DynamicCompilerSettings();
-        }
-
-        protected override object GetElementKey(ConfigurationElement element)
-        {
-            return ((DynamicCompilerSettings)element).name;
-        }
-    }
-
-    public class DynamicCompilerSettings : ConfigurationElement, IDynamicCompilerSettings
-    {
-        public string Name => name;
-
-        [ConfigurationProperty("name", IsRequired = true)]
-        public string name => (string)this["name"];
     }
 }
