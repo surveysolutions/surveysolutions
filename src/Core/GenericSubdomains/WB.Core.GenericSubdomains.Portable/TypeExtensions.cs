a5dc2bcc65157f4a8f7e3634a442a1b53e0133fb
Victor Hladkikh <me@alfeg.net>
2017-12-12 17:59:46 +0200

    Revert "KP-10630 Performance issues on Burkina Faso questionnaire # removed all usages of T<>. Changed IInterviewStateForExpressions.GetAnswer to have constraint"
    This reverts commit 279e45eac98b4546338c74cd2689d6584086f429.

--- src/Core/GenericSubdomains/WB.Core.GenericSubdomains.Portable/TypeExtensions.cs
+++ src/Core/GenericSubdomains/WB.Core.GenericSubdomains.Portable/TypeExtensions.cs
@@ -18,5 +18,22 @@ namespace WB.Core.GenericSubdomains.Portable
             }
             return false;
         }
+
+        public static T To<T>(this object obj)
+        {
+            Type t = typeof(T);
+            Type u = Nullable.GetUnderlyingType(t);
+            Type o = obj?.GetType();
+
+            if (u != null)
+            {
+                return (obj == null) ? default(T) : (T)Convert.ChangeType(obj, u);
+            }
+
+            if (t == o)
+                return (T) obj;
+
+            return (T)Convert.ChangeType(obj, t);
+        }
     }
 }
