eeaad53c91c00ddaeefd2396c7385caa373a12aa
Victor Hladkikh <me@alfeg.net>
2017-12-13 11:24:12 +0200

    Split broken packages by type in prometheus metrics # code review fix

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Implementation/Synchronization/BrokenPackagesStatsCollector.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Implementation/Synchronization/BrokenPackagesStatsCollector.cs
@@ -2,6 +2,7 @@ using System;
 using System.Diagnostics;
 using System.Linq;
 using NHibernate;
+using WB.Core.BoundedContexts.Headquarters.Views;
 using WB.Infrastructure.Native.Monitoring;
 
 namespace WB.Core.BoundedContexts.Headquarters.Implementation.Synchronization
@@ -10,10 +11,11 @@ namespace WB.Core.BoundedContexts.Headquarters.Implementation.Synchronization
     {
         private readonly ISessionFactory sessionFactory;
         private readonly Stopwatch throttle = new Stopwatch();
-
+        
         public BrokenPackagesStatsCollector(ISessionFactory sessionFactory)
         {
             this.sessionFactory = sessionFactory;
+            throttle.Start();
         }
 
         public void RegisterMetrics()
@@ -23,22 +25,25 @@ namespace WB.Core.BoundedContexts.Headquarters.Implementation.Synchronization
 
         public void UpdateMetrics()
         {
-            if (throttle.Elapsed <= TimeSpan.FromMinutes(5)) return;
-
-            using (var session = sessionFactory.OpenStatelessSession())
+            lock (throttle)
             {
-                var packagesCount = session.CreateSQLQuery(@"
-                    select exceptiontype, count(exceptiontype) 
-                    from plainstore.brokeninterviewpackages 
-                    group by exceptiontype");
-                
-                foreach (var package in packagesCount.List().Cast<object[]>())
-                {
-                    CommonMetrics.BrokenPackagesCount.Labels((string)package[0]).Set((long)package[1]);
-                }
-            }
+                if (throttle.Elapsed <= TimeSpan.FromMinutes(1)) return;
 
-            throttle.Reset();
+                using (var session = sessionFactory.OpenStatelessSession())
+                {
+                    var packages = from bip in session.Query<BrokenInterviewPackage>()
+                        group bip by bip.ExceptionType
+                        into g
+                        select new {Type = g.Key, Count = g.Count()};
+
+                    foreach (var package in packages.ToList())
+                    {
+                        CommonMetrics.BrokenPackagesCount.Labels(package.Type).Set(package.Count);
+                    }
+                }
+
+                throttle.Reset();
+            }
         }
     }
 }
