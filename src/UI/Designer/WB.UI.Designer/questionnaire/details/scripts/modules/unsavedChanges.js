341c2676b852c7cf497970d10491ec031374e20a
Pavel Stovpivskyi <sby@rambler.ru>
2017-12-18 13:03:59 +0200

    Revert "temporaly logEnabled for designer"

--- src/UI/Designer/WB.UI.Designer/questionnaire/details/scripts/modules/unsavedChanges.js
+++ src/UI/Designer/WB.UI.Designer/questionnaire/details/scripts/modules/unsavedChanges.js
@@ -9,7 +9,7 @@ angular.module('unsavedChanges', ['resettable'])
     var _this = this;
 
     // defaults
-    var logEnabled = true;
+    var logEnabled = false;
     var useTranslateService = true;
     var routeEvent = ['$locationChangeStart', '$stateChangeStart'];
     var navigateMessage = 'You will lose unsaved changes if you leave this page';
