9fbe2cc39b2c23b0bdc78f5e7d0674459911db66
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2017-12-20 14:03:29 +0200

    KP-10666: Show answering time on tester

--- src/UI/Tester/WB.UI.Tester/Activities/InterviewActivity.cs
+++ src/UI/Tester/WB.UI.Tester/Activities/InterviewActivity.cs
@@ -1,10 +1,17 @@
 using Android.App;
+using Android.Support.Design.Widget;
 using Android.Views;
+using Humanizer;
+using Humanizer.Localisation;
+using MvvmCross.Platform;
+using MvvmCross.Plugins.Messenger;
 using WB.Core.BoundedContexts.Tester.Properties;
 using WB.Core.BoundedContexts.Tester.ViewModels;
 using WB.Core.GenericSubdomains.Portable.ServiceLocation;
 using WB.Core.SharedKernels.Enumerator;
+using WB.Core.SharedKernels.Enumerator.ViewModels.InterviewDetails;
 using WB.UI.Shared.Enumerator.Activities;
+using WB.UI.Tester.Infrastructure.Internals.Settings;
 
 namespace WB.UI.Tester.Activities
 {
@@ -13,10 +20,12 @@ namespace WB.UI.Tester.Activities
         ConfigurationChanges = Android.Content.PM.ConfigChanges.Orientation | Android.Content.PM.ConfigChanges.ScreenSize)]
     public class InterviewActivity : BaseInterviewActivity<InterviewViewModel>
     {
+        private MvxSubscriptionToken answerAcceptedSubsribtion;
         protected override int LanguagesMenuGroupId => Resource.Id.interview_languages;
         protected override int OriginalLanguageMenuItemId => Resource.Id.interview_language_original;
         protected override int LanguagesMenuItemId => Resource.Id.interview_language;
         protected override int MenuId => Resource.Menu.interview;
+        private bool showAnswerAcceptedToast = true;
 
         protected override MenuDescription MenuDescriptor => new MenuDescription
         {
@@ -57,8 +66,41 @@ namespace WB.UI.Tester.Activities
             if (!this.ViewModel.IsSuccessfullyLoaded) return;
 
             var shouldShowVariables = ServiceLocator.Current.GetInstance<IEnumeratorSettings>()?.ShowVariables ?? false;
-            if (this.ViewModel.IsVariablesShowed  != shouldShowVariables)
+            if (this.ViewModel.IsVariablesShowed != shouldShowVariables)
                 this.ViewModel?.ReloadCommand?.Execute();
+
+            SetupAnswerTimeMeasurement();
+        }
+
+        private void SetupAnswerTimeMeasurement()
+        {
+            var settings = Mvx.Resolve<TesterSettings>();
+
+            if (settings.ShowAnswerTime && answerAcceptedSubsribtion == null)
+            {
+                var mvxMessenger = ServiceLocator.Current.GetInstance<IMvxMessenger>();
+                answerAcceptedSubsribtion = mvxMessenger.Subscribe<AnswerAcceptedMessage>(msg =>
+                {
+                    if (showAnswerAcceptedToast)
+                    {
+                        var message = string.Format(TesterUIResources.AnswerRecordedMsg,
+                            msg.Elapsed.Humanize(maxUnit: TimeUnit.Minute));
+
+                        Snackbar.Make(this.FindViewById(Resource.Id.drawer_layout),
+                                message,
+                                Snackbar.LengthIndefinite)
+                            .SetAction(TesterUIResources.AnswerRecordedMsgDismiss, view => { })
+                            .Show(); // Don’t forget to show!
+                    }
+                });
+            }
+        }
+
+        protected override void OnPause()
+        {
+            base.OnPause();
+            this.answerAcceptedSubsribtion?.Dispose();
+            this.answerAcceptedSubsribtion = null;
         }
     }
 }
