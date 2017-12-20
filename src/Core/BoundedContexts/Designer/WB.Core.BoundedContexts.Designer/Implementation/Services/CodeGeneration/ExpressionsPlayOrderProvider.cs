8752de6fddf4b8e1b1a418f78dd6db1ae0fe57d0
Pavel Stovpivskyi <sby@rambler.ru>
2017-12-20 15:52:24 +0200

    Added tests for validations

--- src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Implementation/Services/CodeGeneration/ExpressionsPlayOrderProvider.cs
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Implementation/Services/CodeGeneration/ExpressionsPlayOrderProvider.cs
@@ -32,26 +32,7 @@ namespace WB.Core.BoundedContexts.Designer.Implementation.Services.CodeGeneratio
         public Dictionary<Guid, List<Guid>> GetValidationDependencyGraph(ReadOnlyQuestionnaireDocument questionnaire)
         {
             var graph = this.expressionsGraphProvider.BuildValidationDependencyGraph(questionnaire);
-            return InvertGraph(graph);
-        }
-
-        private Dictionary<T, List<T>> InvertGraph<T>(Dictionary<T, List<T>> graph)
-        {
-            Dictionary<T, HashSet<T>> invertedGraph = new Dictionary<T, HashSet<T>>();
-
-            foreach (var keyValue in graph)
-            {
-                foreach (var value in keyValue.Value)
-                {
-                    if (!invertedGraph.ContainsKey(value))
-                        invertedGraph.Add(value, new HashSet<T>() { keyValue.Key });
-                    else
-                        invertedGraph[value].Add(keyValue.Key);
-                }
-            }
-
-            return invertedGraph.ToDictionary(k => k.Key, v => v.Value.ToList());
-
+            return graph;
         }
     }
 }
--- src/Tests/WB.Tests.Unit.Designer/CodeGeneration/ExpressionsPlayOrderProviderTests.cs
+++ src/Tests/WB.Tests.Unit.Designer/CodeGeneration/ExpressionsPlayOrderProviderTests.cs
@@ -2,6 +2,7 @@
 using NUnit.Framework;
 using WB.Core.BoundedContexts.Designer.Implementation.Services;
 using WB.Core.BoundedContexts.Designer.Implementation.Services.CodeGeneration;
+using WB.Core.SharedKernels.QuestionnaireEntities;
 using WB.Tests.Abc.TestFactories;
 
 namespace WB.Tests.Unit.Designer.CodeGeneration
@@ -28,6 +29,7 @@ namespace WB.Tests.Unit.Designer.CodeGeneration
             Assert.That(expressionsPlayOrder, Is.EqualTo(new[] { chapterId, intQuestionId, variableId }));
         }
 
+
         [Test]
         public void when_GetExpressionsPlayOrder_for_2_questions_and_depended_variable()
         {
@@ -50,7 +52,6 @@ namespace WB.Tests.Unit.Designer.CodeGeneration
         }
 
 
-
         [Test]
         public void when_GetExpressionsPlayOrder_for_sections_without_condition_dependencies()
         {
@@ -70,6 +71,82 @@ namespace WB.Tests.Unit.Designer.CodeGeneration
             var expressionsPlayOrder = expressionsPlayOrderProvider.GetExpressionsPlayOrder(questionnaireDocument.AsReadOnly());
 
             Assert.That(expressionsPlayOrder, Is.EqualTo(new[] { chapterId, textQuestionId, realNumericQuestion, intQuestionId }));
+        }
+
+        [Test]
+        public void when_GetValidationDependencyGraph_for_2_questions_and_depended_variable_without_validations()
+        {
+            var variableId = Guid.NewGuid();
+            var intQuestionId = Guid.NewGuid();
+            var textQuestionId = Guid.NewGuid();
+            var chapterId = Guid.NewGuid();
+
+            var questionnaireDocument = Create.QuestionnaireDocumentWithOneChapter(chapterId,
+                Create.NumericIntegerQuestion(intQuestionId, variable: "i", enablementCondition: "v > 5"),
+                Create.Variable(variableId, variableName: "v", expression: "i"),
+                Create.TextQuestion(textQuestionId, enablementCondition: "v < 10"));
+
+            var expressionProcessor = Create.RoslynExpressionProcessor();
+            var expressionsPlayOrderProvider = new ServiceFactory().ExpressionsPlayOrderProvider(expressionProcessor);
+
+            var expressionsPlayOrder = expressionsPlayOrderProvider.GetValidationDependencyGraph(questionnaireDocument.AsReadOnly());
+
+            Assert.That(expressionsPlayOrder.Count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void when_GetValidationDependencyGraph_for_2_questions_and_depended_variable_with_validations()
+        {
+            var variableId = Guid.NewGuid();
+            var intQuestionId = Guid.NewGuid();
+            var int2QuestionId = Guid.NewGuid();
+            var textQuestionId = Guid.NewGuid();
+            var chapterId = Guid.NewGuid();
+
+            var questionnaireDocument = Create.QuestionnaireDocumentWithOneChapter(chapterId,
+                Create.NumericIntegerQuestion(intQuestionId, variable: "i", validationConditions: new[] {new ValidationCondition("v > 5", "error"),}),
+                Create.NumericIntegerQuestion(int2QuestionId, variable: "i2", validationConditions: new[] {new ValidationCondition("i2 > 5 && v> 5", "error"),}),
+                Create.Variable(variableId, variableName: "v", expression: "i"),
+                Create.TextQuestion(textQuestionId, variable: "t", enablementCondition: "v < 10", validationConditions: new[] {new ValidationCondition("t == \"\"", "error"),})
+            );
+
+            var expressionProcessor = Create.RoslynExpressionProcessor();
+            var expressionsPlayOrderProvider = new ServiceFactory().ExpressionsPlayOrderProvider(expressionProcessor);
+
+            var expressionsPlayOrder = expressionsPlayOrderProvider.GetValidationDependencyGraph(questionnaireDocument.AsReadOnly());
+
+            Assert.That(expressionsPlayOrder.Count, Is.EqualTo(3));
+            Assert.That(expressionsPlayOrder[variableId], Is.EqualTo(new[] { intQuestionId, int2QuestionId }));
+            Assert.That(expressionsPlayOrder[int2QuestionId], Is.EqualTo(new[] { int2QuestionId }));
+            Assert.That(expressionsPlayOrder[textQuestionId], Is.EqualTo(new[] { textQuestionId }));
+        }
+
+        [Test]
+        public void when_GetValidationDependencyGraph_for_question_inside_roster()
+        {
+            var intQuestionId = Guid.NewGuid();
+            var int2QuestionId = Guid.NewGuid();
+            var rosterId = Guid.NewGuid();
+            var rosterTrigerId = Guid.NewGuid();
+            var chapterId = Guid.NewGuid();
+
+            var questionnaireDocument = Create.QuestionnaireDocumentWithOneChapter(chapterId,
+                Create.NumericIntegerQuestion(rosterTrigerId, variable: "rt"),
+                Create.Roster(rosterId, variable:"r", rosterSizeQuestionId: rosterTrigerId, children: new []
+                {
+                    Create.NumericIntegerQuestion(intQuestionId, variable: "i", validationConditions: new[] { new ValidationCondition("r.Count > 5", "error"), }),
+                    Create.NumericIntegerQuestion(int2QuestionId, variable: "i2", validationConditions: new[] { new ValidationCondition("rt > 5", "error"), }),
+                })
+            );
+
+            var expressionProcessor = Create.RoslynExpressionProcessor();
+            var expressionsPlayOrderProvider = new ServiceFactory().ExpressionsPlayOrderProvider(expressionProcessor);
+
+            var expressionsPlayOrder = expressionsPlayOrderProvider.GetValidationDependencyGraph(questionnaireDocument.AsReadOnly());
+
+            Assert.That(expressionsPlayOrder.Count, Is.EqualTo(2));
+            Assert.That(expressionsPlayOrder[rosterId], Is.EqualTo(new[] { intQuestionId }));
+            Assert.That(expressionsPlayOrder[rosterTrigerId], Is.EqualTo(new[] { int2QuestionId }));
         }
     }
 }
