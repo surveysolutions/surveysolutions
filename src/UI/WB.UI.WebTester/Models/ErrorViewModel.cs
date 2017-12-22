6f5f048b4ce47fe8a87c36548af365c68639c7d7
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2017-12-22 16:14:39 +0200

    KP-10678: Added empty project template for web tester

---
+++ src/UI/WB.UI.WebTester/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WB.UI.WebTester.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
