349db5f5c2e3379a420a7c9519704b8d6443ce5d
Andrii Kozhyn <hedgic@gmail.com>
2017-12-22 18:23:45 +0200

    KP-10678: Replaced web tester with asp.net project

---
+++ src/UI/WB.UI.WebTester/Controllers/WebTesterController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace WB.UI.WebTester.Controllers
+{
+    public class WebTesterController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
