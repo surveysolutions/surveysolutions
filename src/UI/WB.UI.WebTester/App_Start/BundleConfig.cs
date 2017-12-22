68c5a1a28fc1643ce18659ebc36f607779915f2f
Andrii Kozhyn <hedgic@gmail.com>
2017-12-22 18:23:45 +0200

    KP-10678: Replaced web tester with asp.net project

---
+++ src/UI/WB.UI.WebTester/App_Start/BundleConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Optimization;
+
+namespace WB.UI.WebTester
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+          
+        }
+    }
+}
---
+++ src/UI/WB.UI.WebTester/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(WB.UI.WebTester.Startup))]
+
+namespace WB.UI.WebTester
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+        }
+    }
+}
