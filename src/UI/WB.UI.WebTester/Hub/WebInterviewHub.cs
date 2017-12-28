be7f4697655b01156e194d282731bb2e6ab961c4
Andrii Kozhyn <hedgic@gmail.com>
2017-12-28 16:15:36 +0200

    KP-10733: enabled signalr

---
+++ src/UI/WB.UI.WebTester/Hub/WebInterviewHub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNet.SignalR.Hubs;
+using WB.Core.Infrastructure.CommandBus;
+using WB.Core.SharedKernels.DataCollection.Repositories;
+using WB.Enumerator.Native.WebInterview;
+
+namespace WB.UI.WebTester.Hub
+{
+    [HubName(@"interview")]
+    public class WebInterviewHub : WebInterview
+    {
+        public WebInterviewHub(IStatefulInterviewRepository statefulInterviewRepository, ICommandService commandService, IQuestionnaireStorage questionnaireRepository, IWebInterviewNotificationService webInterviewNotificationService, IWebInterviewInterviewEntityFactory interviewEntityFactory) : base(statefulInterviewRepository, commandService, questionnaireRepository, webInterviewNotificationService, interviewEntityFactory)
+        {
+        }
+    }
+}
