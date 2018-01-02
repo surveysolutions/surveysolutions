a481659f08e0525d9528cff3dff2594f0752abc5
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2018-01-02 12:39:24 +0200

    KP-10666: moving registrations between bounded contexts and UI

--- src/UI/WB.UI.WebTester/Controllers/WebTesterController.cs
+++ src/UI/WB.UI.WebTester/Controllers/WebTesterController.cs
@@ -1,13 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Web.Mvc;
+using WB.Core.SharedKernels.DataCollection.Repositories;
 
 namespace WB.UI.WebTester.Controllers
 {
     public class WebTesterController : Controller
     {
+        private readonly IStatefulInterviewRepository statefulInterviewRepository;
+
+        public WebTesterController(IStatefulInterviewRepository statefulInterviewRepository)
+        {
+            this.statefulInterviewRepository = statefulInterviewRepository ?? throw new ArgumentNullException(nameof(statefulInterviewRepository));
+        }
+
         public ActionResult Index()
         {
+
+
             return View();
         }
     }
