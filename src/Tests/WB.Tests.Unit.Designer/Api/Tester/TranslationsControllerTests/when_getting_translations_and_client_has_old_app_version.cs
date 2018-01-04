5c284b646b7388d87920b1e568859b0510a382a5
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2018-01-04 11:35:12 -0500

    KP-10752 Translations are imported including history [fix tests]

--- src/Tests/WB.Tests.Unit.Designer/Api/Tester/TranslationsControllerTests/when_getting_translations_and_client_has_old_app_version.cs
+++ src/Tests/WB.Tests.Unit.Designer/Api/Tester/TranslationsControllerTests/when_getting_translations_and_client_has_old_app_version.cs
@@ -9,7 +9,8 @@ namespace WB.Tests.Unit.Designer.Api.Tester.TranslationsControllerTests
 {
     public class when_getting_translations_and_client_has_old_app_version : TranslationsControllerTestsContext
     {
-        [NUnit.Framework.OneTimeSetUp] public void context () {
+        [NUnit.Framework.OneTimeSetUp]
+        public void context () {
             controller = CreateTranslationsController();
             BecauseOf();
         }
@@ -17,7 +18,8 @@ namespace WB.Tests.Unit.Designer.Api.Tester.TranslationsControllerTests
         private void BecauseOf() => expectedException =
             Catch.Exception(() => controller.Get(Guid.Parse("11111111111111111111111111111111"),  version: ApiVersion.CurrentTesterProtocolVersion - 1));
 
-        [NUnit.Framework.Test] public void should_response_code_be_UpgradeRequired () =>
+        [NUnit.Framework.Test]
+        public void should_response_code_be_UpgradeRequired () =>
             ((HttpResponseException)expectedException).Response.StatusCode.ShouldEqual(HttpStatusCode.UpgradeRequired);
 
         private static Exception expectedException;
