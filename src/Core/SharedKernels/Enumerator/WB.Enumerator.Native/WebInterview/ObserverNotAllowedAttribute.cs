e2ea26496cf3a173f75e6c1b9343c4bab67fcce6
Andrii Kozhyn <hedgic@gmail.com>
2018-01-05 17:12:18 +0200

    KP-10737: Implemented authorization for signalr hub

--- src/Core/SharedKernels/Enumerator/WB.Enumerator.Native/WebInterview/ObserverNotAllowedAttribute.cs
+++ src/Core/SharedKernels/Enumerator/WB.Enumerator.Native/WebInterview/ObserverNotAllowedAttribute.cs
@@ -4,5 +4,8 @@ namespace WB.Enumerator.Native.WebInterview
 {
     public class ObserverNotAllowedAttribute : Attribute
     {
+        public static string Id = "ObserverNowAllowed";
+
+        public override object TypeId => Id;
     }
 }
