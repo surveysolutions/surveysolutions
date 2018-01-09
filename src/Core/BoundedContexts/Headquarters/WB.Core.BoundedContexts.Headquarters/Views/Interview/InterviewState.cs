8a782be226779875a516e5e9dc95f4e801d4271d
Roman Lytvyn <litvin.roma@gmail.com>
2018-01-09 13:51:38 +0200

    KP-10632 [code review] HQ:InterviewDenormalizer should be functional

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Views/Interview/InterviewState.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Views/Interview/InterviewState.cs
@@ -1,21 +1,13 @@
-using System;
 using System.Collections.Generic;
 
 namespace WB.Core.BoundedContexts.Headquarters.Views.Interview
 {
-    public class InterviewState
+    public class InterviewState : StateBase
     {
-        public InterviewState(Guid id)
-        {
-            this.Id = id;
-        }
-
-        public Guid Id { get; }
         public Dictionary<InterviewStateIdentity, bool> Enablement { get; set; } = new Dictionary<InterviewStateIdentity, bool>();
         public Dictionary<InterviewStateIdentity, InterviewStateValidation> Validity { get; set; } = new Dictionary<InterviewStateIdentity, InterviewStateValidation>();
         public Dictionary<InterviewStateIdentity, InterviewStateAnswer> Answers { get; set; } = new Dictionary<InterviewStateIdentity, InterviewStateAnswer>();
         public List<InterviewStateIdentity> ReadOnly { get; set; } = new List<InterviewStateIdentity>();
         public List<InterviewStateIdentity> Removed { get; set; } = new List<InterviewStateIdentity>();
-
     }
 }
