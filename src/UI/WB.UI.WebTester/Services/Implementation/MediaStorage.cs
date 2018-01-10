17fdc591bddc2e84a5baf49b5fdc4fde25d5f6cf
Victor Hladkikh <me@alfeg.net>
2018-01-10 16:07:11 +0200

    KP-10759 Implement upload and storing of Images/Audio recordings

---
+++ src/UI/WB.UI.WebTester/Services/Implementation/MediaStorage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Web;
+using System.Web.Caching;
+
+namespace WB.UI.WebTester.Services.Implementation
+{
+    public class MediaStorage : IMediaStorage
+    {
+        private Cache Cache => HttpRuntime.Cache;
+
+        public void Store(MultimediaFile file, Guid interviewId)
+        {
+            Cache.Insert(key(interviewId, file.Filename), file, null, Cache.NoAbsoluteExpiration, TimeSpan.FromMinutes(5));
+        }
+
+        public MultimediaFile Get(Guid interviewId, string filename)
+        {
+            return Cache.Get(key(interviewId, filename)) as MultimediaFile;
+        }
+
+        string key(Guid interviewId, string filename) => $"{interviewId.ToString()}:{filename}";
+    }
+}
