29fe6363166f96c12ff49de8af18f1990affc808
Victor Gladkikh <me@alfeg.net>
2018-01-10 16:07:11 +0200

    KP-10759 Implement upload and storing of Images/Audio recordings

---
+++ src/UI/WB.UI.WebTester/Services/IMediaStorage.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace WB.UI.WebTester.Services
+{
+    public interface IMediaStorage
+    {
+        void Store(MultimediaFile file, Guid interviewId);
+        MultimediaFile Get(Guid interviewId, string filename);
+    }
+}
