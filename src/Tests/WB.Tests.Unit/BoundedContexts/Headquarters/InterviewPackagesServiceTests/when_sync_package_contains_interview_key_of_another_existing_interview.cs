7dfa240a7da3e358ad33c920f5654dc9afb6848e
Slava Scherbak <slava.scherbak@gmail.com>
2018-01-11 17:25:23 -0500

    KP-10653: Fixed tests

--- src/Tests/WB.Tests.Unit/BoundedContexts/Headquarters/InterviewPackagesServiceTests/when_sync_package_contains_interview_key_of_another_existing_interview.cs
+++ src/Tests/WB.Tests.Unit/BoundedContexts/Headquarters/InterviewPackagesServiceTests/when_sync_package_contains_interview_key_of_another_existing_interview.cs
@@ -10,14 +10,18 @@ using WB.Tests.Abc.Storage;
 
 namespace WB.Tests.Unit.BoundedContexts.Headquarters.InterviewPackagesServiceTests
 {
-    class when_sync_package_contains_interview_key_of_another_existing_interview: InterviewPackagesServiceTestsContext
+    class when_sync_package_contains_interview_key_of_another_existing_interview
     {
         [Test]
         public void should_generate_new_interview_key()
         {
             InterviewKey existingInterviewKey = new InterviewKey(1324);
-
+            
+            SynchronizeInterviewEventsCommand syncCommand = null;
             Mock<ICommandService> commandService = new Mock<ICommandService>();
+            commandService
+                .Setup(x => x.Execute(It.IsAny<ICommand>(), It.IsAny<string>()))
+                .Callback((ICommand c, string o) => { syncCommand = c as SynchronizeInterviewEventsCommand; });
 
             var interviews = new TestInMemoryWriter<InterviewSummary>();
             var existingSummary = Create.Entity.InterviewSummary(key: existingInterviewKey.ToString());
@@ -32,7 +36,8 @@ namespace WB.Tests.Unit.BoundedContexts.Headquarters.InterviewPackagesServiceTes
             service.ProcessPackage(Create.Entity.InterviewPackage(events: new[] { aggregateRootEvent }));
 
             // Assert
-            commandService.Verify(x => x.Execute(It.Is<SynchronizeInterviewEventsCommand>(cmd => cmd.InterviewKey == GeneratedInterviewKey), It.IsAny<string>()));
+            Assert.That(syncCommand, Is.Not.Null);
+            Assert.That(syncCommand.InterviewKey, Is.EqualTo(new InterviewKey(5533)));
         }
     }
 }
