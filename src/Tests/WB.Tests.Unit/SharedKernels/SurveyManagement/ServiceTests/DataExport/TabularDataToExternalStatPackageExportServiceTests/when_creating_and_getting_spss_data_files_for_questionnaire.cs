5e006375fc11deb0c545c6b04c1e2ffd4030da1b
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2018-01-12 18:11:26 -0500

    frix tests

--- src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/ServiceTests/DataExport/TabularDataToExternalStatPackageExportServiceTests/when_creating_and_getting_spss_data_files_for_questionnaire.cs
+++ src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/ServiceTests/DataExport/TabularDataToExternalStatPackageExportServiceTests/when_creating_and_getting_spss_data_files_for_questionnaire.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Threading;
 using Machine.Specifications;
 using Moq;
+using NUnit.Framework;
 using StatData.Core;
 using StatData.Writers;
 using WB.Core.BoundedContexts.Headquarters.DataExport.Accessors;
@@ -15,22 +16,36 @@ using It = Machine.Specifications.It;
 
 namespace WB.Tests.Unit.SharedKernels.SurveyManagement.ServiceTests.DataExport.TabularDataToExternalStatPackageExportServiceTests
 {
-    internal class when_creating_and_getting_spss_data_files_for_questionnaire : TabularDataToExternalStatPackageExportServiceTestContext
+    internal class TabularDataToExternalStatPackageExportServiceTest : TabularDataToExternalStatPackageExportServiceTestContext
     {
-        Establish context = () =>
+        [Test]
+        public void when_creating_and_getting_spss_data_files_for_questionnaire()
         {
-            var questionnaireExportStructure = CreateQuestionnaireExportStructure(CreateHeaderStructureForLevel("main level"),
-                CreateHeaderStructureForLevel("nested roster level", referenceNames: new[] { "r1", "r2" },
-                    levelScopeVector: new ValueVector<Guid>(new[] { Guid.NewGuid()})));
+            TabularDataToExternalStatPackageExportService _tabularDataToExternalStatPackagesTabDataExportService;
+            Guid questionnaireId = Guid.Parse("11111111111111111111111111111111");
+            long questionnaireVersion = 3;
+            string[] filePaths;
+            string mailLevelFileName = "main level.tab";
+            string nestedRosterFileName = "nested roster level.tab";
+            string fileNameExported = "main level.sav";
+            Mock<IDatasetWriter> datasetWriter;
+
+            
+            var questionnaireExportStructure = CreateQuestionnaireExportStructure(
+                CreateHeaderStructureForLevel("main level"),
+                CreateHeaderStructureForLevel("nested roster level", referenceNames: new[] {"r1", "r2"},
+                    levelScopeVector: new ValueVector<Guid>(new[] {Guid.NewGuid()})));
 
             var fileSystemAccessor = new Mock<IFileSystemAccessor>();
             fileSystemAccessor.Setup(x => x.IsDirectoryExists(Moq.It.IsAny<string>())).Returns(true);
-            fileSystemAccessor.Setup(x => x.ChangeExtension(Moq.It.IsAny<string>(), Moq.It.IsAny<string>())).Returns(fileNameExported);
-            fileSystemAccessor.Setup(x => x.GetFileNameWithoutExtension(Moq.It.IsAny<string>())).Returns<string>(Path.GetFileNameWithoutExtension);
+            fileSystemAccessor.Setup(x => x.ChangeExtension(Moq.It.IsAny<string>(), Moq.It.IsAny<string>()))
+                .Returns(fileNameExported);
+            fileSystemAccessor.Setup(x => x.GetFileNameWithoutExtension(Moq.It.IsAny<string>()))
+                .Returns<string>(Path.GetFileNameWithoutExtension);
 
             var tabFileReader = new Mock<ITabFileReader>();
-            tabFileReader.Setup(x => x.GetMetaFromTabFile(Moq.It.IsAny<string>())).Returns(meta);
-            tabFileReader.Setup(x => x.GetDataFromTabFile(Moq.It.IsAny<string>())).Returns(new string[,]{{"1","1"}});
+            tabFileReader.Setup(x => x.GetMetaFromTabFile(Moq.It.IsAny<string>())).Returns(new DatasetMeta(new IDatasetVariable[] { new DatasetVariable("a"), new DatasetVariable("ParentId1") }));
+            tabFileReader.Setup(x => x.GetDataFromTabFile(Moq.It.IsAny<string>())).Returns(new string[,] {{"1", "1"}});
 
             datasetWriter = new Mock<IDatasetWriter>();
 
@@ -38,25 +53,20 @@ namespace WB.Tests.Unit.SharedKernels.SurveyManagement.ServiceTests.DataExport.T
             datasetWriterFactory.Setup(x => x.CreateDatasetWriter(DataExportFormat.SPSS)).Returns(datasetWriter.Object);
 
             _tabularDataToExternalStatPackagesTabDataExportService = CreateSqlToTabDataExportService(
-                fileSystemAccessor: fileSystemAccessor.Object, questionnaireExportStructure: questionnaireExportStructure,
+                fileSystemAccessor: fileSystemAccessor.Object,
+                questionnaireExportStructure: questionnaireExportStructure,
                 tabFileReader: tabFileReader.Object, datasetWriterFactory: datasetWriterFactory.Object);
-        };
+            
+            //
+            filePaths =
+                _tabularDataToExternalStatPackagesTabDataExportService.CreateAndGetSpssDataFilesForQuestionnaire(
+                    questionnaireId, questionnaireVersion, new[] {mailLevelFileName, nestedRosterFileName},
+                    new Progress<int>(), CancellationToken.None);
 
-        Because of = () =>
-            filePaths = _tabularDataToExternalStatPackagesTabDataExportService.CreateAndGetSpssDataFilesForQuestionnaire(questionnaireId, questionnaireVersion, new[] { mailLevelFileName, nestedRosterFileName }, new Progress<int>(), CancellationToken.None);
-
-        private It should_call_write_to_file = () =>
-            datasetWriter.Verify(x => x.WriteToFile(Moq.It.IsAny<string>(), Moq.It.IsAny<IDatasetMeta>(), Moq.It.IsAny<IDataQuery>()), Times.Exactly(2));
-
-        private static TabularDataToExternalStatPackageExportService _tabularDataToExternalStatPackagesTabDataExportService;
-        private static Guid questionnaireId = Guid.Parse("11111111111111111111111111111111");
-        private static long questionnaireVersion = 3;
-        private static string[] filePaths;
-        private static string mailLevelFileName = "main level.tab";
-        private static string nestedRosterFileName = "nested roster level.tab";
-        private static string fileNameExported = "main level.sav";
-        private static Mock<IDatasetWriter> datasetWriter;
-
-        private static DatasetMeta meta = new DatasetMeta(new IDatasetVariable[] {new DatasetVariable("a"), new DatasetVariable("ParentId1") });
+           //Assert
+            datasetWriter.Verify(
+                x => x.WriteToFile(Moq.It.IsAny<string>(), Moq.It.IsAny<IDatasetMeta>(), Moq.It.IsAny<IDataQuery>()),
+                Times.Exactly(2));
+        }
     }
 }
