c4835139943c23a0b2ac7fabd79052363c5b741f
Pavel Stovpivskyi <630198+spuv@users.noreply.github.com>
2018-01-12 16:17:00 +0200

    KP-10780 Error in the questionnaire (dependency on conditions)

--- src/Tests/WB.Tests.Integration/InterviewTests/InterviewTestsContext.cs
+++ src/Tests/WB.Tests.Integration/InterviewTests/InterviewTestsContext.cs
@@ -51,7 +51,11 @@ namespace WB.Tests.Integration.InterviewTests
         {
             Guid questionnaireId = questionnaireDocument.PublicKey;
             questionnaireDocument.IsUsingExpressionStorage = true;
-            questionnaireDocument.ExpressionsPlayOrder = IntegrationCreate.ExpressionsPlayOrderProvider().GetExpressionsPlayOrder(questionnaireDocument.AsReadOnly());
+            var playOrderProvider = IntegrationCreate.ExpressionsPlayOrderProvider();
+            var readOnlyQuestionnaireDocument = questionnaireDocument.AsReadOnly();
+            questionnaireDocument.ExpressionsPlayOrder = playOrderProvider.GetExpressionsPlayOrder(readOnlyQuestionnaireDocument);
+            questionnaireDocument.DependencyGraph = playOrderProvider.GetDependencyGraph(readOnlyQuestionnaireDocument);
+            questionnaireDocument.ValidationDependencyGraph = playOrderProvider.GetValidationDependencyGraph(readOnlyQuestionnaireDocument);
 
             var questionnaireRepository = Mock.Of<IQuestionnaireStorage>(repository
                 => repository.GetQuestionnaire(It.IsAny<QuestionnaireIdentity>(), It.IsAny<string>()) == Create.Entity.PlainQuestionnaire(questionnaireDocument, 1));
@@ -108,7 +112,11 @@ namespace WB.Tests.Integration.InterviewTests
         {
             questionnaireIdentity = questionnaireIdentity ?? new QuestionnaireIdentity(questionnaireDocument.PublicKey, 1);
             questionnaireDocument.IsUsingExpressionStorage = true;
-            questionnaireDocument.ExpressionsPlayOrder = IntegrationCreate.ExpressionsPlayOrderProvider().GetExpressionsPlayOrder(questionnaireDocument.AsReadOnly());
+            var playOrderProvider = IntegrationCreate.ExpressionsPlayOrderProvider();
+            var readOnlyQuestionnaireDocument = questionnaireDocument.AsReadOnly();
+            questionnaireDocument.ExpressionsPlayOrder = playOrderProvider.GetExpressionsPlayOrder(readOnlyQuestionnaireDocument);
+            questionnaireDocument.DependencyGraph = playOrderProvider.GetDependencyGraph(readOnlyQuestionnaireDocument);
+            questionnaireDocument.ValidationDependencyGraph = playOrderProvider.GetValidationDependencyGraph(readOnlyQuestionnaireDocument);
 
             var state = GetLatestExpressionStorage(questionnaireDocument);
 
