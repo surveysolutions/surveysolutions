d2a8aa2d47fea4f98f97204a4a1c9f463d9efc5a
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2018-01-15 13:18:02 +0200

    KP-10800: Pass exception to caller from isolated scope

---
+++ src/Tests/WB.Tests.Integration/WebTester/Services/AppdomainsPerInterviewManagerTestsBase.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using Moq;
+using NUnit.Framework;
+using WB.Core.GenericSubdomains.Portable.Services;
+using WB.Tests.Integration.InterviewTests;
+using WB.UI.WebTester.Services.Implementation;
+
+namespace WB.Tests.Integration.WebTester.Services
+{
+    [TestOf(typeof(AppdomainsPerInterviewManager))]
+    public class AppdomainsPerInterviewManagerTestsBase : InterviewTestsContext
+    {
+        protected AppdomainsPerInterviewManager CreateManager()
+        {
+            var bin = Path.GetDirectoryName(typeof(when_configured).Assembly.Location);
+            return new AppdomainsPerInterviewManager(bin, Mock.Of<ILogger>());
+        }
+    }
+}
