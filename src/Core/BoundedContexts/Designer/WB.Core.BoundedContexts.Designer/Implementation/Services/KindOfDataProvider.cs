842fe3476f0c850a463386cf3d380fe666133fc7
Pavel Stovpivskyi <630198+spuv@users.noreply.github.com>
2018-01-15 14:54:00 +0200

    KP-10646 Moved dropdowns in resources

---
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Implementation/Services/KindOfDataProvider.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using WB.Core.SharedKernels.Questionnaire.Documents;
+
+namespace WB.Core.BoundedContexts.Designer.Implementation.Services
+{
+    public class KindOfDataItem
+    {
+        public KindOfData Code { get; set; }
+        public string Title { get; set; }
+    }
+
+    public static class KindOfDataProvider
+    {
+        public static List<KindOfDataItem> GetKindOfDataItems()
+        {
+            var codes = GetKindOfDataCodes();
+            return codes.Select(code => new KindOfDataItem()
+            {
+                Code = code,
+                Title = GetKindOfDataTitleByCode(code)
+            }).ToList();
+        }
+
+        public static string GetKindOfDataTitleByCode(KindOfData code)
+        {
+            return Resources.KindOfData.ResourceManager.GetString(code.ToString());
+        }
+
+        public static List<KindOfData> GetKindOfDataCodes()
+        {
+            return new List<KindOfData>()
+            {
+                KindOfData.SampleSurveyData,
+                KindOfData.CensusEnumerationData,
+                KindOfData.AdministrativeRecordsData,
+                KindOfData.AggregateData,
+                KindOfData.ClinicalData,
+                KindOfData.EventTransactionData,
+                KindOfData.ObservationDataRatings,
+                KindOfData.ProcessProducedData,
+                KindOfData.TimeBudgetDiaries
+            };
+        }
+    }
+}
