1098fe2fc67cdc9212945c2576055d35c088950c
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2018-01-16 16:42:36 +0200

    KP-10738: Use different path for assets

--- src/UI/WB.UI.WebTester/App_Start/BundleConfig.cs
+++ src/UI/WB.UI.WebTester/App_Start/BundleConfig.cs
@@ -6,7 +6,7 @@ namespace WB.UI.WebTester
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new StyleBundle("~/css").Include(
+            bundles.Add(new StyleBundle("~/Content/Styles").Include(
                 "~/Content/Styles/markup.css",
                 "~/Content/Styles/markup-web-interview.css"
             ));
