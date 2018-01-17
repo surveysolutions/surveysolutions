944d092b6f66509a37ac08cd5078cda94a9687e8
Victor Hladkikh <me@alfeg.net>
2018-01-17 13:43:11 +0200

    KP-10809 Handle interview expiration. Cleanup resources # Tests

--- src/UI/WB.UI.WebTester/Services/IMediaStorage.cs
+++ src/UI/WB.UI.WebTester/Services/IMediaStorage.cs
@@ -4,7 +4,7 @@ namespace WB.UI.WebTester.Services
 {
     public interface IMediaStorage
     {
-        void Store(MultimediaFile file, Guid interviewId);
+        void Store(Guid interviewId, MultimediaFile file);
         MultimediaFile Get(Guid interviewId, string filename);
     }
 }
