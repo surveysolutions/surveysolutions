3d156614cd2aa3e6bda91c5acef442eab2157f65
Slava Scherbak <slava.scherbak@gmail.com>
2018-01-22 11:10:12 -0500

    KP-10804: added test after code review

--- src/Tests/WB.Tests.Unit/SharedKernels/DataCollection/AnswerUtilsTests.cs
+++ src/Tests/WB.Tests.Unit/SharedKernels/DataCollection/AnswerUtilsTests.cs
@@ -15,6 +15,22 @@ namespace WB.Tests.Unit.SharedKernels.DataCollection
         {
             var question = Create.Entity.SingleQuestion(options: new List<Answer>
             {
+                Create.Entity.Option(1, "Hello 1", null),
+                Create.Entity.Option(2, "Hello 2", null),
+                Create.Entity.Option(3, "Hello 3", "1"),
+                Create.Entity.Option(4, "Hello 4", "2"),
+            });
+
+            var options = AnswerUtils.GetCategoricalOptionsFromQuestion(question, null, null).ToArray();
+
+            Assert.That(options.Length, Is.EqualTo(2));
+        }
+        
+        [Test]
+        public void When_Getting_options_from_categorical_question_in_old_format_and_parent_value()
+        {
+            var question = Create.Entity.SingleQuestion(options: new List<Answer>
+            {
                 Create.Entity.Option("1", "Hello 1", "1"),
                 Create.Entity.Option("2", "Hello 2", "1"),
                 Create.Entity.Option("3", "Hello 3", "2"),
