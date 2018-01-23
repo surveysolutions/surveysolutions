8614eb6a600459926320eaf49743c03285ecfa5c
Victor Hladkikh <me@alfeg.net>
2018-01-23 11:01:44 +0200

    KP-10803 Handle options filter # using less data to serialize

--- src/UI/WB.UI.WebTester/Services/IAppdomainsPerInterviewManager.cs
+++ src/UI/WB.UI.WebTester/Services/IAppdomainsPerInterviewManager.cs
@@ -12,7 +12,11 @@ namespace WB.UI.WebTester.Services
         void SetupForInterview(Guid interviewId, QuestionnaireDocument questionnaireDocument, string supportingAssembly);
         void TearDown(Guid interviewId);
         List<CommittedEvent> Execute(ICommand command);
+        
+        List<CategoricalOption> GetFirstTopFilteredOptionsForQuestion(Guid interviewId, Identity questionIdentity, 
+            int? parentQuestionValue, string filter, int itemsCount = 200);
 
-        List<CategoricalOption> FilteredCategoricalOptions(Guid interviewId, CategoricalOptionsFilter categoricalOptionsFilter);
+        CategoricalOption GetOptionForQuestionWithFilter(Guid interviewId, Identity question,
+            string optionText, int? parentQuestionValue = null);
     }
 }
