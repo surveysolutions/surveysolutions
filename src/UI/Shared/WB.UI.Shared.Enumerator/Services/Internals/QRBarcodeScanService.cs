8ed64dfa38dcb0bd24d287f98d5a3143c84e619a
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2018-01-24 12:12:01 +0200

    Revert "KP-10821: updated barcode scanning lib"
    This reverts commit ca1cd5391be68affa30dc13b9a437b50fc72aeb7.

--- src/UI/Shared/WB.UI.Shared.Enumerator/Services/Internals/QRBarcodeScanService.cs
+++ src/UI/Shared/WB.UI.Shared.Enumerator/Services/Internals/QRBarcodeScanService.cs
@@ -1,8 +1,4 @@
-using System;
-using System.Drawing;
 using System.Threading.Tasks;
-using Android.App;
-using MvvmCross.Platform;
 using MvvmCross.Platform.Droid.Platform;
 using MWBarcodeScanner;
 using Plugin.Permissions.Abstractions;
@@ -12,10 +8,6 @@ namespace WB.UI.Shared.Enumerator.Services.Internals
 {
     internal class QRBarcodeScanService : IQRBarcodeScanService
     {
-        public static RectangleF RECT_FULL_1D = new RectangleF(6, 6, 88, 88);
-        public static RectangleF RECT_FULL_2D = new RectangleF(20, 6, 60, 88);
-        public static RectangleF RECT_DOTCODE = new RectangleF(30, 20, 40, 60);
-
         private readonly IMvxAndroidCurrentTopActivity androidCurrentTopActivity;
         private readonly IPermissions permissions;
 
@@ -29,59 +21,19 @@ namespace WB.UI.Shared.Enumerator.Services.Internals
         {
             await this.permissions.AssureHasPermission(Permission.Camera);
 
-            var activity = this.androidCurrentTopActivity.Activity;
-            var scanner = new Scanner(activity);
+            var scanner = new Scanner(this.androidCurrentTopActivity.Activity);
             scanner.setInterfaceOrientation(
-                activity.RequestedOrientation.ToString());
+                this.androidCurrentTopActivity.Activity.RequestedOrientation.ToString());
 
-            this.CustomizeScanner(activity);
+            this.CustomizeScanner();
             var result = await scanner.Scan();
 
             return result != null ? new QRBarcodeScanResult() { Code = result.code, RawBytes = result.bytes } : null;
         }
 
-        private void CustomizeScanner(Activity activity)
+        private void CustomizeScanner()
         {
-            int registerResult = BarcodeConfig.MWB_registerSDK("uaEgBR8jq/WJ7+CWNIP0iOEUYiTL0ayG6WGHMA2U6/U=", activity);
-            
-            BarcodeConfig.MWB_setActiveCodes(BarcodeConfig.MWB_CODE_MASK_25 |
-                                             BarcodeConfig.MWB_CODE_MASK_39 |
-                                             BarcodeConfig.MWB_CODE_MASK_93 |
-                                             BarcodeConfig.MWB_CODE_MASK_128 |
-                                             BarcodeConfig.MWB_CODE_MASK_AZTEC |
-                                             BarcodeConfig.MWB_CODE_MASK_DM |
-                                             BarcodeConfig.MWB_CODE_MASK_EANUPC |
-                                             BarcodeConfig.MWB_CODE_MASK_PDF |
-                                             BarcodeConfig.MWB_CODE_MASK_QR |
-                                             BarcodeConfig.MWB_CODE_MASK_CODABAR |
-                                             BarcodeConfig.MWB_CODE_MASK_RSS |
-                                             BarcodeConfig.MWB_CODE_MASK_MAXICODE |
-                                             BarcodeConfig.MWB_CODE_MASK_POSTAL);
-
-            BarcodeConfig.MWB_setDirection(BarcodeConfig.MWB_SCANDIRECTION_HORIZONTAL | BarcodeConfig.MWB_SCANDIRECTION_VERTICAL);
-            BarcodeConfig.MWB_setLevel(2);
-
-            BarcodeConfig.MWBsetScanningRect(BarcodeConfig.MWB_CODE_MASK_25, RECT_FULL_1D);
-            BarcodeConfig.MWBsetScanningRect(BarcodeConfig.MWB_CODE_MASK_39, RECT_FULL_1D);
-            BarcodeConfig.MWBsetScanningRect(BarcodeConfig.MWB_CODE_MASK_93, RECT_FULL_1D);
-            BarcodeConfig.MWBsetScanningRect(BarcodeConfig.MWB_CODE_MASK_128, RECT_FULL_1D);
-            BarcodeConfig.MWBsetScanningRect(BarcodeConfig.MWB_CODE_MASK_AZTEC, RECT_FULL_2D);
-            BarcodeConfig.MWBsetScanningRect(BarcodeConfig.MWB_CODE_MASK_DM, RECT_FULL_2D);
-            BarcodeConfig.MWBsetScanningRect(BarcodeConfig.MWB_CODE_MASK_EANUPC, RECT_FULL_1D);
-            BarcodeConfig.MWBsetScanningRect(BarcodeConfig.MWB_CODE_MASK_PDF, RECT_FULL_1D);
-            BarcodeConfig.MWBsetScanningRect(BarcodeConfig.MWB_CODE_MASK_QR, RECT_FULL_2D);
-            BarcodeConfig.MWBsetScanningRect(BarcodeConfig.MWB_CODE_MASK_RSS, RECT_FULL_1D);
-            BarcodeConfig.MWBsetScanningRect(BarcodeConfig.MWB_CODE_MASK_CODABAR, RECT_FULL_1D);
-            BarcodeConfig.MWBsetScanningRect(BarcodeConfig.MWB_CODE_MASK_DOTCODE, RECT_DOTCODE);
-            BarcodeConfig.MWBsetScanningRect(BarcodeConfig.MWB_CODE_MASK_MAXICODE, RECT_FULL_2D);
-            BarcodeConfig.MWBsetScanningRect(BarcodeConfig.MWB_CODE_MASK_POSTAL, RECT_FULL_1D);
-
-            BarcodeConfig.MWB_setMinLength(BarcodeConfig.MWB_CODE_MASK_25, 5);
-            BarcodeConfig.MWB_setMinLength(BarcodeConfig.MWB_CODE_MASK_MSI, 5);
-            BarcodeConfig.MWB_setMinLength(BarcodeConfig.MWB_CODE_MASK_39, 5);
-            BarcodeConfig.MWB_setMinLength(BarcodeConfig.MWB_CODE_MASK_CODABAR, 5);
-            BarcodeConfig.MWB_setMinLength(BarcodeConfig.MWB_CODE_MASK_11, 5);
-            ScannerActivity.setParserMask(BarcodeConfig.MWP_PARSER_MASK_NONE);
+            BarcodeConfig.MWB_registerSDK("uaEgBR8jq/WJ7+CWNIP0iOEUYiTL0ayG6WGHMA2U6/U=");
         }
     }
 }
