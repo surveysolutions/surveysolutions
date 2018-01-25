a08fd87a2141ddc68ea095eee20c9e958d6a73c2
Andrii Kozhyn <hedgic@gmail.com>
2018-01-25 13:40:11 +0200

    use same method to close interview as others

--- src/UI/WB.UI.WebTester/Hub/WebInterviewHub.cs
+++ src/UI/WB.UI.WebTester/Hub/WebInterviewHub.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNet.SignalR.Hubs;
-using WB.Core.GenericSubdomains.Portable;
 using WB.Core.Infrastructure.CommandBus;
 using WB.Core.SharedKernels.DataCollection.Repositories;
 using WB.Enumerator.Native.WebInterview;
@@ -11,19 +10,21 @@ namespace WB.UI.WebTester.Hub
     [HubName(@"interview")]
     public class WebInterviewHub : WebInterview
     {
+        private readonly IWebInterviewNotificationService webInterviewNotificationService;
         private readonly IAppdomainsPerInterviewManager appdomainsPerInterviewManager;
 
         public WebInterviewHub(IStatefulInterviewRepository statefulInterviewRepository, ICommandService commandService, IQuestionnaireStorage questionnaireRepository, IWebInterviewNotificationService webInterviewNotificationService, IWebInterviewInterviewEntityFactory interviewEntityFactory,
             IAppdomainsPerInterviewManager appdomainsPerInterviewManager) : 
             base(statefulInterviewRepository, commandService, questionnaireRepository, webInterviewNotificationService, interviewEntityFactory)
         {
+            this.webInterviewNotificationService = webInterviewNotificationService;
             this.appdomainsPerInterviewManager = appdomainsPerInterviewManager;
         }
 
         public override void CompleteInterview(CompleteInterviewRequest completeInterviewRequest)
         {
             appdomainsPerInterviewManager.TearDown(GetCallerInterview().Id);
-            this.Clients.Group(base.GetCallerInterview().Id.FormatGuid()).shutDown();
+            webInterviewNotificationService.ShutDownInterview(base.GetCallerInterview().Id);
         }
     }
 }
