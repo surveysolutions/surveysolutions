2cd2e6db03676af24e48af3f623f706d36b717a6
Victor Gladkikh <me@alfeg.net>
2018-01-30 11:52:09 +0200

    KP-10759 Implement upload and storing of Images/Audio recording # code review fixes

--- src/UI/Headquarters/WB.UI.Headquarters/HqApp/src/webinterview/components/questions/Audio.vue
+++ src/UI/Headquarters/WB.UI.Headquarters/HqApp/src/webinterview/components/questions/Audio.vue
@@ -100,18 +100,6 @@ export default {
             },
             isRecorded() {
                 return this.isRecording == false && this.$me.isAnswered;
-            },
-            audioRecordHash() {
-                const str = this.$me.updatedAt.toString()
-                
-                let hash = 0, i, chr;
-                if (str.length === 0) return hash;
-                for (i = 0; i < str.length; i++) {
-                    chr   = str.charCodeAt(i);
-                    hash  = ((hash << 5) - hash) + chr;
-                    hash |= 0; // Convert to 32bit integer
-                }
-                return hash;
             }
         },
     methods: {
