1cfaba282d31fbd308630e9c04822b8aa6eef883
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2018-01-31 16:53:06 -0500

    KP-10735 Allow HQ user to reject completed interviews

--- src/UI/Headquarters/WB.UI.Headquarters/Resources/Interviews.Designer.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/Resources/Interviews.Designer.cs
@@ -194,7 +194,7 @@ namespace WB.UI.Headquarters.Resources {
         
         /// <summary>
         ///   Looks up a localized string similar to &lt;h3&gt;&lt;b&gt;{0}&lt;/b&gt; interview(s) will be rejected.&lt;/h3&gt;
-        ///&lt;b&gt;Note!&lt;/b&gt; You can reject interviews with the following status only: &lt;i&gt;{1}&lt;/i&gt;..
+        ///&lt;b&gt;Note!&lt;/b&gt; You can reject interviews with the following status only: &lt;i&gt;{1}&lt;/i&gt; and &lt;i&gt;{2}&lt;/i&gt;..
         /// </summary>
         public static string RejectConfirmMessageHQ {
             get {
