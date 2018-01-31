d3a904c56a0b9e4a15ca8b4ac3820c9e7a9c5ed7
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2018-01-31 12:20:11 -0500

    KP-10653 Reassign interviewer [Increase protocol version] (reverted from commit ea3b8e3e973bd425bcfbf5411f926de27dd99a5a)

--- src/Core/SharedKernels/DataCollection/DataCollection.Portable/SyncProtocolVersionProvider.cs
+++ src/Core/SharedKernels/DataCollection/DataCollection.Portable/SyncProtocolVersionProvider.cs
@@ -2,8 +2,8 @@
 {
     public class SyncProtocolVersionProvider : ISyncProtocolVersionProvider
     {
-        //previous values: 5962, 7018, 7034, 7050, 7060
-        public int GetProtocolVersion() => 7070;
+        //previous values: 5962, 7018, 7034, 7050
+        public int GetProtocolVersion() => 7060;
 
         public int GetLastNonUpdatableVersion() => 7000;
     }
