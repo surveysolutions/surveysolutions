6fc773ce28231750dea1b3518c0175fe8a98151f
Roman Lytvyn <litvin.roma@gmail.com>
2018-02-07 17:13:24 +0200

    KP-4479 Refactor verification data on preloading. fixed failed tests

--- src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/PreloadedDataVerifierTests/when_verifying_preloaded_data_file_with_locked_interviewer_by_supervisor.cs
+++ src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/PreloadedDataVerifierTests/when_verifying_preloaded_data_file_with_locked_interviewer_by_supervisor.cs
@@ -28,7 +28,8 @@ namespace WB.Tests.Unit.SharedKernels.SurveyManagement.PreloadedDataVerifierTest
             preloadedDataServiceMock = new Mock<IPreloadedDataService>();
 
             preloadedDataServiceMock.Setup(x => x.FindLevelInPreloadedData(QuestionnaireCsvFileName)).Returns(new HeaderStructureForLevel(){LevelIdColumnName = ServiceColumns.InterviewId });
-            preloadedDataServiceMock.Setup(x => x.GetColumnIndexByHeaderName(preloadedDataByFile, Moq.It.IsAny<string>())).Returns(1);
+            preloadedDataServiceMock.Setup(x => x.GetColumnIndexByHeaderName(preloadedDataByFile, ServiceColumns.ResponsibleColumnName)).Returns(1);
+            preloadedDataServiceMock.Setup(x => x.GetColumnIndexByHeaderName(preloadedDataByFile, ServiceColumns.AssignmentsCountColumnName)).Returns(-1);
             var userViewFactory = new Mock<IUserViewFactory>();
 
             var user = new UserView()
