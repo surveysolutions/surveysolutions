113c15e0bb841c04276e92498980db71aac4318d
Pavel Stovpivskyi <sby@rambler.ru>
2018-02-08 17:18:31 +0200

    Moved ErrorsByCircularReferences to last stage if not exists errors on duplicate variables or ids

--- src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Verifier/QuestionnaireVerifier.cs
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Verifier/QuestionnaireVerifier.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Main.Core.Documents;
+using Main.Core.Entities.Composite;
 using WB.Core.BoundedContexts.Designer.Implementation.Services;
 using WB.Core.BoundedContexts.Designer.Implementation.Services.CodeGeneration;
 using WB.Core.BoundedContexts.Designer.QuestionnaireCompilationForOldVersions;
@@ -27,7 +28,9 @@ namespace WB.Core.BoundedContexts.Designer.Verifier
         private readonly ITranslationsService translationService;
         private readonly IQuestionnaireTranslator questionnaireTranslator;
         private readonly IQuestionnaireCompilationVersionService questionnaireCompilationVersionService;
-        
+        private readonly ITopologicalSorter<Guid> topologicalSorter;
+        private readonly IExpressionsPlayOrderProvider graphProvider;
+
 
         public QuestionnaireVerifier(IExpressionProcessor expressionProcessor, 
             IFileSystemAccessor fileSystemAccessor, 
@@ -50,13 +53,15 @@ namespace WB.Core.BoundedContexts.Designer.Verifier
             this.translationService = translationService;
             this.questionnaireTranslator = questionnaireTranslator;
             this.questionnaireCompilationVersionService = questionnaireCompilationVersionService;
+            this.topologicalSorter = topologicalSorter;
+            this.graphProvider = graphProvider;
 
             verifiers = new IPartialVerifier[]
             {
                 new QuestionVerifications(),
                 new GroupVerifications(fileSystemAccessor),
                 new AttachmentVerifications(attachmentService), 
-                new ExpressionVerifications(macrosSubstitutionService, expressionProcessor, topologicalSorter, compilerSettings, graphProvider), 
+                new ExpressionVerifications(macrosSubstitutionService, expressionProcessor, compilerSettings), 
                 new LookupVerifications(lookupTableService, keywordsProvider), 
                 new MacroVerifications(), 
                 new QuestionnaireVerifications(substitutionService, keywordsProvider), 
@@ -92,9 +97,16 @@ namespace WB.Core.BoundedContexts.Designer.Verifier
                 verificationMessagesByQuestionnaire.AddRange(errors);
             }
 
+            var errorsCodeForSkipCircleReferanceVerifier = new HashSet<string>() { "WB0102", "WB0026" };
+            if (verificationMessagesByQuestionnaire.Any(er => errorsCodeForSkipCircleReferanceVerifier.Contains(er.Code)))
+                return verificationMessagesByQuestionnaire;
+
+            var errorsByCircleReferances = this.ErrorsByCircularReferences(questionnaire);
+            verificationMessagesByQuestionnaire.AddRange(errorsByCircleReferances);
+
             if (verificationMessagesByQuestionnaire.Any(e => e.MessageLevel == VerificationMessageLevel.Critical))
                 return verificationMessagesByQuestionnaire;
-            
+
             var verificationMessagesByCompiler = this.ErrorsByCompiler(questionnaire).ToList();
 
             return verificationMessagesByQuestionnaire.Concat(verificationMessagesByCompiler);
@@ -116,6 +128,22 @@ namespace WB.Core.BoundedContexts.Designer.Verifier
             foreach (var elementWithErrors in elementsWithErrorMessages)
             {
                 yield return CreateExpressionSyntaxError(new ExpressionLocation(elementWithErrors.Key), elementWithErrors.ToList());
+            }
+        }
+
+        private IEnumerable<QuestionnaireVerificationMessage> ErrorsByCircularReferences(QuestionnaireDocument questionnaire)
+        {
+            var dependencyGraph = graphProvider.GetDependencyGraph(questionnaire.AsReadOnly());
+            var cycles = topologicalSorter.DetectCycles(dependencyGraph);
+
+            foreach (var cycle in cycles)
+            {
+                var references =
+                    cycle.Select(guid => questionnaire.Find<IComposite>(guid))
+                        .Select(x => QuestionnaireNodeReference.CreateFrom(x))
+                        .ToArray();
+
+                yield return QuestionnaireVerificationMessage.Error("WB0056", VerificationMessages.WB0056_EntityShouldNotHaveCircularReferences, references);
             }
         }
 
