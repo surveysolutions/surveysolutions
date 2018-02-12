4a9491647e6b85963e2bb024c5825b70d3f74405
Pavel Stovpivskyi <630198+spuv@users.noreply.github.com>
2018-02-12 15:04:56 +0200

    KP-10949 Add events

---
+++ src/Core/SharedKernels/DataCollection/DataCollection/Events/Interview/AnswersDeclaredPlausible.cs
@@ -0,0 +1,10 @@
+﻿using WB.Core.SharedKernels.DataCollection.Events.Interview.Base;
+
+namespace WB.Core.SharedKernels.DataCollection.Events.Interview
+{
+    public class AnswersDeclaredPlausible : QuestionsPassiveEvent
+    {
+        public AnswersDeclaredPlausible(Identity[] questions)
+            : base(questions) {}
+    }
+}
