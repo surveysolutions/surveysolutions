0e6ccbe0025a100aadcce5edf4743aaa40ed532d
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2018-02-15 15:00:16 -0500

    KP-10957 Update substutions service to use labels of special values

--- src/Tests/WB.Tests.Unit/SharedKernels/Enumerator/ViewModels/QuestionHeaderViewModelTests/when_variable_value_and_substitution_title_changed.cs
+++ src/Tests/WB.Tests.Unit/SharedKernels/Enumerator/ViewModels/QuestionHeaderViewModelTests/when_variable_value_and_substitution_title_changed.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Machine.Specifications;
+using NUnit.Framework;
 using WB.Core.SharedKernels.DataCollection;
 using WB.Core.SharedKernels.DataCollection.Events.Interview.Dtos;
 using WB.Core.SharedKernels.DataCollection.Implementation.Aggregates;
@@ -10,28 +11,37 @@ namespace WB.Tests.Unit.SharedKernels.Enumerator.ViewModels.QuestionHeaderViewMo
 {
     internal class when_variable_value_and_substitution_title_changed : QuestionHeaderViewModelTestsContext
     {
-        private Establish context = () =>
+        [OneTimeSetUp]
+        public void SetUp()
         {
-            staticTextWithSubstitutionIdentity = Identity.Create(Guid.Parse("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"), RosterVector.Empty);
-            var substitutedVariableIdentity = Identity.Create(Guid.Parse("CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC"), RosterVector.Empty);
+            staticTextWithSubstitutionIdentity =
+                Identity.Create(Guid.Parse("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"), RosterVector.Empty);
+            var substitutedVariableIdentity =
+                Identity.Create(Guid.Parse("CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC"), RosterVector.Empty);
 
             var questionnaire = Create.Entity.QuestionnaireDocumentWithOneChapter(
-                Create.Entity.StaticText(publicKey: staticTextWithSubstitutionIdentity.Id, text: "Your answer on question is %var1% and variable is %var2%"),
+                Create.Entity.StaticText(publicKey: staticTextWithSubstitutionIdentity.Id,
+                    text: "Your answer on question is %var1% and variable is %var2%"),
                 Create.Entity.NumericIntegerQuestion(variable: "var1", id: substitutedQuestionId),
-                Create.Entity.Variable(variableName: "var2",id: substitutedVariableIdentity.Id, expression: "(var1*100)/20")
+                Create.Entity.Variable(variableName: "var2", id: substitutedVariableIdentity.Id,
+                    expression: "(var1*100)/20")
             );
 
-            statefullInterview = Create.AggregateRoot.StatefulInterview(questionnaire: questionnaire, setupLevel: level =>
-            {
-                level.Setup(x => x.GetVariableExpression(Create.Identity(substitutedVariableIdentity.Id))).Returns(() => 10);
-            });
-            statefullInterview.Apply(Create.Event.VariablesChanged(new ChangedVariable(substitutedVariableIdentity, 10)));
-        };
+            statefullInterview = Create.AggregateRoot.StatefulInterview(questionnaire: questionnaire,
+                setupLevel: level =>
+                {
+                    level.Setup(x => x.GetVariableExpression(Create.Identity(substitutedVariableIdentity.Id)))
+                        .Returns(() => 10);
+                });
+            statefullInterview.Apply(
+                Create.Event.VariablesChanged(new ChangedVariable(substitutedVariableIdentity, 10)));
 
-        Because of = () => 
-            statefullInterview.AnswerNumericIntegerQuestion(interviewerId, substitutedQuestionId, RosterVector.Empty, DateTime.UtcNow, 2);
+            statefullInterview.AnswerNumericIntegerQuestion(interviewerId, substitutedQuestionId, RosterVector.Empty,
+                DateTime.UtcNow, 2);
+        }
 
-        It should_change_item_title = () => statefullInterview.GetTitleText(staticTextWithSubstitutionIdentity)
+        [Test]
+        public void should_change_item_title() => statefullInterview.GetTitleText(staticTextWithSubstitutionIdentity)
             .ShouldEqual($"Your answer on question is 2 and variable is 10");
         
         static StatefulInterview statefullInterview;
