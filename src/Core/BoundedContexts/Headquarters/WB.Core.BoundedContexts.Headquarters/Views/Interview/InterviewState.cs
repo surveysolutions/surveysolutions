9e773f74a27daf25ab8dfc6357845025d04cac8c
Pavel Stovpivskyi <sby@rambler.ru>
2018-02-15 17:46:57 +0200

    KP-10950 Add reaction on new event in HQ

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Views/Interview/InterviewState.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Views/Interview/InterviewState.cs
@@ -6,6 +6,7 @@ namespace WB.Core.BoundedContexts.Headquarters.Views.Interview
     {
         public Dictionary<InterviewStateIdentity, bool> Enablement { get; set; } = new Dictionary<InterviewStateIdentity, bool>();
         public Dictionary<InterviewStateIdentity, InterviewStateValidation> Validity { get; set; } = new Dictionary<InterviewStateIdentity, InterviewStateValidation>();
+        public Dictionary<InterviewStateIdentity, InterviewStateValidation> Warnings { get; set; } = new Dictionary<InterviewStateIdentity, InterviewStateValidation>();
         public Dictionary<InterviewStateIdentity, InterviewStateAnswer> Answers { get; set; } = new Dictionary<InterviewStateIdentity, InterviewStateAnswer>();
         public List<InterviewStateIdentity> ReadOnly { get; set; } = new List<InterviewStateIdentity>();
         public List<InterviewStateIdentity> Removed { get; set; } = new List<InterviewStateIdentity>();
