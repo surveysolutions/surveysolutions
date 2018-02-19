c86c6cb2c54d51d7bc7364692aacc1227762d356
Roman Lytvyn <litvin.roma@gmail.com>
2018-02-19 16:39:59 +0200

    KP-10670 HQ: Interview review. Add compact view for questions with options

--- src/UI/Headquarters/WB.UI.Headquarters/HqApp/src/webinterview/components/questions/CategoricalSingle.vue
+++ src/UI/Headquarters/WB.UI.Headquarters/HqApp/src/webinterview/components/questions/CategoricalSingle.vue
@@ -2,21 +2,18 @@
     <wb-question :question="$me" questionCssClassName="single-select-question" :noAnswer="noOptions">
         <div class="question-unit">
             <div class="options-group" v-bind:class="{ 'dotted': noOptions }">
-                <div class="radio" v-for="option in $me.options" :key="$me.id + '_' + option.value">
+                <div class="radio" v-for="option in answeredOrAllOptions" :key="$me.id + '_' + option.value">
                     <div class="field">
-                        <input class="wb-radio" 
-                            type="radio" 
-                            :id="$me.id + '_' + option.value" 
-                            :name="$me.id" 
-                            :value="option.value" 
-                            :disabled="!$me.acceptAnswer"
-                            v-model="answer">
+                        <input class="wb-radio" type="radio" :id="$me.id + '_' + option.value" :name="$me.id" :value="option.value" :disabled="!$me.acceptAnswer" v-model="answer">
                         <label :for="$me.id + '_' + option.value">
-                                <span class="tick"></span> {{option.title}}
+                            <span class="tick"></span> {{option.title}}
                         </label>
                         <wb-remove-answer />
                     </div>
                 </div>
+                <button type="button" class="btn btn-link btn-horizontal-hamburger" @click="toggleOptions" v-if="shouldShowAnsweredOptionsOnly && !showAllOptions">
+                    <span></span>
+                </button>
                 <div v-if="noOptions" class="options-not-available">{{ $t("WebInterviewUI.OptionsAvailableAfterAnswer") }}</div>
                 <wb-lock />
             </div>
@@ -25,10 +22,26 @@
 </template>
 <script lang="js">
     import { entityDetails } from "../mixins"
+    import { find } from "lodash"
 
     export default {
         name: 'CategoricalSingle',
+        data(){
+            return {
+                showAllOptions: false
+            }
+        },
         computed: {
+            shouldShowAnsweredOptionsOnly(){
+                return !this.showAllOptions && this.$store.getters.isReviewMode && !this.noOptions && this.$me.answer;
+            },
+            answeredOrAllOptions(){
+                if(!this.shouldShowAnsweredOptionsOnly)
+                    return this.$me.options;
+                
+                var self = this;
+                return [find(this.$me.options, function(o) { return o.value == self.answer; })];
+            },
             answer: {
                 get() {
                     return this.$me.answer
@@ -43,6 +56,11 @@
                 return this.$me.options == null || this.$me.options.length == 0
             }
         },
-        mixins: [entityDetails]
+        mixins: [entityDetails],
+        methods: {
+            toggleOptions(){
+                this.showAllOptions = !this.showAllOptions;
+            }
+        }
     }
 </script>
