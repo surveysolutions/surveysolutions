16816e2c804814ad5635f3a68bde00fa66302e6d
Roman Lytvyn <litvin.roma@gmail.com>
2018-02-19 21:09:40 +0200

    KP-10999 HQ: Remove old interview details page

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Views/ChangeStatus/ChangeStatusInputModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace WB.Core.BoundedContexts.Headquarters.Views.ChangeStatus
-{
-    public class ChangeStatusInputModel
-    {
-        public Guid InterviewId { get; set; }
-    }
-}
