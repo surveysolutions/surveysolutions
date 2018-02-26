bceb6508134d9b9605448644b49b0a9dc4cf2289
Slava Scherbak <slava.scherbak@gmail.com>
2018-02-26 11:26:06 -0500

    Fixed issue with 2 negative answers in a row for roster size question with special values

---
+++ src/Tests/WB.Tests.Unit/SharedKernels/Enumerator/ViewModels/SpecialValuesViewModelTests.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+using WB.Core.SharedKernels.Enumerator.ViewModels.InterviewDetails.Questions.State;
+
+namespace WB.Tests.Unit.SharedKernels.Enumerator.ViewModels
+{
+    [TestFixture(typeof(SpecialValuesViewModel))]
+    public class SpecialValuesViewModelTests
+    {
+
+    }
+}
