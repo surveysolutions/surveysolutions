d241e28e7436f14f93b551e5e03e02390fdbb304
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2018-02-27 17:42:07 -0500

    KP-10997 Add space in comments line in exported files [review]

---
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Utils.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+namespace WB.Core.BoundedContexts.Headquarters
+{
+    public static class Utils
+    {
+        private static readonly Regex RemoveNewLineRegEx = new Regex(@"\t|\n|\r", RegexOptions.Compiled);
+        public static string RemoveNewLine(string value)
+        {
+            return string.IsNullOrEmpty(value) ? string.Empty : RemoveNewLineRegEx.Replace(value, " ");
+        }
+    }
+}
