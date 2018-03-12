8c4fbb5112f263facf04ca8ff14873d96c7fe6d6
Victor Gladkikh <me@alfeg.net>
2018-03-12 16:52:01 +0200

    KP-10698 Export questionnaire on HQ # Import of lookup tables to hq

---
+++ src/UI/Designer/WB.UI.Designer/Api/Headquarters/HQLookupController.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+using Newtonsoft.Json;
+using WB.Core.BoundedContexts.Designer.Implementation.Services.LookupTableService;
+using WB.Core.BoundedContexts.Designer.Services;
+using WB.UI.Designer.Api.Attributes;
+using WB.UI.Designer.Code;
+
+namespace WB.UI.Designer.Api.Headquarters
+{
+    [ApiBasicAuth(onlyAllowedAddresses: true)]
+    [RoutePrefix("api/hq/lookup")]
+    public class HQLookupController : ApiController
+    {
+        private readonly ILookupTableService lookupTableService;
+        public HQLookupController(ILookupTableService lookupTableService)
+        {
+            this.lookupTableService = lookupTableService;
+        }
+
+        [HttpGet]
+        [Route("{id}/{tableId}")]
+        public HttpResponseMessage Get(string id, string tableId)
+        {
+            var lookupFile = this.lookupTableService.GetLookupTableContentFile(Guid.Parse(id), Guid.Parse(tableId));
+
+            if (lookupFile == null) return this.Request.CreateResponse(HttpStatusCode.NotFound);
+
+            var result = JsonConvert.SerializeObject(lookupFile, Formatting.None, new JsonSerializerSettings
+            { 
+                TypeNameHandling = TypeNameHandling.None
+            });
+            var response = this.Request.CreateResponse(HttpStatusCode.OK);
+            response.Content = new StringContent(result, Encoding.UTF8, "application/json"); 
+            return response;
+        }
+    }
+}
