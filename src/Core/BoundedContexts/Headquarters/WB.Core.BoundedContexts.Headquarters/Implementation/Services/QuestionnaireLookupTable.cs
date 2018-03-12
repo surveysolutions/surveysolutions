c43ca975239485251b62c0e50627b99a6e9724a2
Victor Hladkikh <me@alfeg.net>
2018-03-12 16:52:01 +0200

    KP-10698 Export questionnaire on HQ # Import of lookup tables to hq

---
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Implementation/Services/QuestionnaireLookupTable.cs
@@ -0,0 +1,11 @@
+﻿using WB.Core.SharedKernels.DataCollection.Implementation.Entities;
+
+namespace WB.Core.BoundedContexts.Headquarters.Implementation.Services
+{
+    public class QuestionnaireLookupTable
+    {
+        public QuestionnaireIdentity Id { get; set; }
+        public string FileName { get; set; }
+        public string Content { get; set; }
+    }
+}
