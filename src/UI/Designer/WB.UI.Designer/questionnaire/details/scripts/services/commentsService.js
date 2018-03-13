b3269c35ceeb7f3032c31e557dba38fd5cf9086a
Slava Scherbak <slava.scherbak@gmail.com>
2018-03-13 14:26:39 -0400

    KP-11048: Added controller for comments editor

---
+++ src/UI/Designer/WB.UI.Designer/questionnaire/details/scripts/services/commentsService.js
@@ -0,0 +1,20 @@
+﻿(function() {
+    'use strict';
+    angular.module('designerApp')
+        .factory('commentsService', [
+            '$http', 'commandService', 'utilityService',
+            function($http, commandService, string) {
+
+                var urlBase = '../../api/comments';
+                var commentsService = {};
+
+                commentsService.getItemCommentsById = function(questionnaireId, itemId) {
+                    var url = string.format('{0}/get/{1}?itemId={2}', urlBase, questionnaireId, itemId);
+                    return $http.get(url);
+                };
+
+                return commentsService;
+            }
+        ]);
+
+}());
