ae60c7a69c677ca9a12ff58db8d3b0b170771da5
Slava Scherbak <slava.scherbak@gmail.com>
2018-03-13 14:26:39 -0400

    KP-11048: Added controller for comments editor

---
+++ src/UI/Designer/WB.UI.Designer/questionnaire/details/scripts/controllers/comments.js
@@ -0,0 +1,6 @@
+﻿angular.module('designerApp')
+    .controller('CommentsCtrl', 
+        function ($rootScope, $scope, $state, $i18next, commandService, utilityService, $log, confirmService, questionnaireService, hotkeys) {
+            'use strict';
+
+        });
---
+++ src/UI/Designer/WB.UI.Designer/questionnaire/details/scripts/controllers/commentsEditor.js
@@ -0,0 +1,19 @@
+﻿angular.module('designerApp')
+    .controller('CommentsEditorCtrl', 
+        function ($rootScope, $scope, $state, commentsService, $i18next, commandService, utilityService, $log, confirmService,  hotkeys) {
+            'use strict';
+
+            $scope.loadComments = function() {
+                commentsService.getItemCommentsById($state.params.questionnaireId, $state.params.itemId)
+                    .then(function(result) {
+                        var data = result.data;
+                        console.log(data);
+                        $scope.comments = data;
+                        $scope.activeComment = {
+                            comment: ''
+                        };
+                    });
+            };
+            $scope.currentChapterId = $state.params.chapterId;
+            $scope.loadComments();
+        });
