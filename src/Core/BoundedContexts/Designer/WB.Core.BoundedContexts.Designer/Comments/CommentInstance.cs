c2143359467c5e8acc0e4eb0120ae888d85ad46e
Slava Scherbak <slava.scherbak@gmail.com>
2018-03-13 19:15:03 -0400

    KP-11048: added backend for comments

---
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Comments/CommentInstance.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace WB.Core.BoundedContexts.Designer.Comments
+{
+    public class CommentInstance
+    {
+        public virtual Guid Id { get; set; }
+        public virtual Guid QuestionnaireId { get; set; }
+        public virtual Guid EntityId { get; set; }
+        public virtual DateTime Date { get; set; }
+        public virtual string Comment { get; set; }
+        public virtual string UserName { get; set; }
+        public virtual string UserEmail { get; set; }
+        public virtual DateTime? ResolveDate { get; set; }
+    }
+}
