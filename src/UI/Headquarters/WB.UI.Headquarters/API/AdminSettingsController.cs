b9b917edf4956387e9dc9c7189c2d5a81ef908ec
Roman Lytvyn <litvin.roma@gmail.com>
2018-03-13 14:10:21 +0200

    KP-11051 Auto update of the IN  app. Implemented HQ part

--- src/UI/Headquarters/WB.UI.Headquarters/API/AdminSettingsController.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/API/AdminSettingsController.cs
@@ -2,6 +2,7 @@
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using WB.Core.BoundedContexts.Headquarters.DataExport.Security;
 using WB.Core.BoundedContexts.Headquarters.ValueObjects;
 using WB.Core.Infrastructure.PlainStorage;
 
@@ -10,41 +11,45 @@ namespace WB.UI.Headquarters.API
     [Authorize(Roles = "Administrator")]
     public class AdminSettingsController : ApiController
     {
-        public class GlobalNoticeModel
+        public class SettingsModel
         {
-            public string Message { get; set; }
+            public string GlobalNotice { get; set; }
+            public bool InterviewerAutoUpdatesEnabled { get; set; }
         }
 
         private readonly IPlainKeyValueStorage<GlobalNotice> appSettingsStorage;
-        public AdminSettingsController(IPlainKeyValueStorage<GlobalNotice> appSettingsStorage)
+        private readonly IPlainKeyValueStorage<InterviewerSettings> interviewerSettingsStorage;
+
+        public AdminSettingsController(IPlainKeyValueStorage<GlobalNotice> appSettingsStorage,
+            IPlainKeyValueStorage<InterviewerSettings> interviewerSettingsStorage)
         {
             if (appSettingsStorage == null) throw new ArgumentNullException(nameof(appSettingsStorage));
             this.appSettingsStorage = appSettingsStorage;
+            this.interviewerSettingsStorage = interviewerSettingsStorage;
         }
 
-        public HttpResponseMessage Get()
+        public HttpResponseMessage Get() => Request.CreateResponse(new SettingsModel
         {
-            var globalNotice = this.appSettingsStorage.GetById(GlobalNotice.GlobalNoticeKey);
-            if (globalNotice == null)
-            {
-                return Request.CreateResponse(new GlobalNoticeModel());
-            }
+            GlobalNotice = this.appSettingsStorage.GetById(AppSetting.GlobalNoticeKey)?.Message,
+            InterviewerAutoUpdatesEnabled = this.interviewerSettingsStorage.GetById(AppSetting.InterviewerSettings)?.AutoUpdateEnabled ?? true
+        });
 
-            return Request.CreateResponse(new GlobalNoticeModel {Message = globalNotice.Message});
-        }
-
-        public HttpResponseMessage Post([FromBody] GlobalNoticeModel message)
+        public HttpResponseMessage Post([FromBody] SettingsModel message)
         {
-            if (string.IsNullOrEmpty(message?.Message))
+            if (string.IsNullOrEmpty(message?.GlobalNotice))
             {
                 this.appSettingsStorage.Remove(GlobalNotice.GlobalNoticeKey);
             }
             else
             {
                 var globalNotice = this.appSettingsStorage.GetById(GlobalNotice.GlobalNoticeKey) ?? new GlobalNotice();
-                globalNotice.Message = message.Message.Length > 1000 ? message.Message.Substring(0, 1000) : message.Message;
+                globalNotice.Message = message.GlobalNotice.Length > 1000 ? message.GlobalNotice.Substring(0, 1000) : message.GlobalNotice;
                 this.appSettingsStorage.Store(globalNotice, GlobalNotice.GlobalNoticeKey);
             }
+
+            this.interviewerSettingsStorage.Store(
+                new InterviewerSettings {AutoUpdateEnabled = message.InterviewerAutoUpdatesEnabled},
+                AppSetting.InterviewerSettings);
 
             return Request.CreateResponse(HttpStatusCode.OK, new {sucess = true});
         }
