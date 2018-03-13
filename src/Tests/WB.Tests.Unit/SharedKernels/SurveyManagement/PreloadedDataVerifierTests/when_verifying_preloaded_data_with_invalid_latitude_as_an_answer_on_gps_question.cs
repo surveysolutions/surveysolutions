f548fd65130d87ad9f6ec9977bfc017aa1073d9c
Andrii Kozhyn <hedgic@gmail.com>
2018-03-13 12:04:35 +0200

    Migrated preloading tests to nunit

--- src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/PreloadedDataVerifierTests/when_verifying_preloaded_data_with_invalid_latitude_as_an_answer_on_gps_question.cs
+++ src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/PreloadedDataVerifierTests/when_verifying_preloaded_data_with_invalid_latitude_as_an_answer_on_gps_question.cs
@@ -1,8 +1,7 @@
-﻿using System;
+using System;
 using System.Linq;
-using Machine.Specifications;
+using FluentAssertions;
 using Main.Core.Documents;
-using WB.Core.BoundedContexts.Headquarters.AssignmentImport;
 using WB.Core.BoundedContexts.Headquarters.AssignmentImport.Parser;
 using WB.Core.BoundedContexts.Headquarters.AssignmentImport.Verifier;
 using WB.Core.BoundedContexts.Headquarters.ValueObjects.PreloadedData;
@@ -13,8 +12,7 @@ namespace WB.Tests.Unit.SharedKernels.SurveyManagement.PreloadedDataVerifierTest
 {
     internal class when_verifying_preloaded_data_with_invalid_latitude_as_an_answer_on_gps_question : PreloadedDataVerifierTestContext
     {
-        private Establish context = () =>
-        {
+        [NUnit.Framework.OneTimeSetUp] public void context () {
             questionnaireId = Guid.Parse("11111111111111111111111111111111");
             gpsQuestionId = Guid.Parse("21111111111111111111111111111111");
             var gpsQuestion = Create.Entity.GpsCoordinateQuestion(gpsQuestionId, "gps");
@@ -22,50 +20,50 @@ namespace WB.Tests.Unit.SharedKernels.SurveyManagement.PreloadedDataVerifierTest
             questionnaire = CreateQuestionnaireDocumentWithOneChapter(gpsQuestion);
             questionnaire.Title = "questionnaire";
             preloadedDataByFile = CreatePreloadedDataByFile(new[] { ServiceColumns.InterviewId, "gps__Latitude", "gps__Longitude" },
-                new string[][] { new string[] { "1", "90.00001", "3" }, new string[] { "1", "-90.00001", "3" } },
+                new[] { new[] { "1", "90.00001", "3" }, new[] { "1", "-90.00001", "3" } },
                 "questionnaire.csv");
 
             var preloadedDataService =
                 Create.Service.PreloadedDataService(questionnaire);
 
             importDataVerifier = CreatePreloadedDataVerifier(questionnaire, preloadedDataService);
-        };
+            BecauseOf();
+        }
 
-        Because of =
-            () => importDataVerifier.VerifyPanelFiles(questionnaireId, 1, Create.Entity.PreloadedDataByFile(preloadedDataByFile), status);
+        private void BecauseOf() => importDataVerifier.VerifyPanelFiles(questionnaireId, 1, Create.Entity.PreloadedDataByFile(preloadedDataByFile), status);
 
-        It should_result_has_2_errors = () =>
-            status.VerificationState.Errors.Count().ShouldEqual(2);
+        [NUnit.Framework.Test] public void should_result_has_2_errors () =>
+            status.VerificationState.Errors.Count().Should().Be(2);
 
-        It should_return_first_PL0030_error = () =>
-            status.VerificationState.Errors.First().Code.ShouldEqual("PL0032");
+        [NUnit.Framework.Test] public void should_return_first_PL0030_error () =>
+            status.VerificationState.Errors.First().Code.Should().Be("PL0032");
 
-        It should_return_first_error_with_single_reference = () =>
-            status.VerificationState.Errors.First().References.Count().ShouldEqual(1);
+        [NUnit.Framework.Test] public void should_return_first_error_with_single_reference () =>
+            status.VerificationState.Errors.First().References.Count().Should().Be(1);
 
-        It should_return_first_error_with_single_reference_of_type_Cell = () =>
-            status.VerificationState.Errors.First().References.First().Type.ShouldEqual(PreloadedDataVerificationReferenceType.Cell);
+        [NUnit.Framework.Test] public void should_return_first_error_with_single_reference_of_type_Cell () =>
+            status.VerificationState.Errors.First().References.First().Type.Should().Be(PreloadedDataVerificationReferenceType.Cell);
 
-        It should_return_first_error_with_single_reference_pointing_on_first_column = () =>
-            status.VerificationState.Errors.First().References.First().PositionX.ShouldEqual(1);
+        [NUnit.Framework.Test] public void should_return_first_error_with_single_reference_pointing_on_first_column () =>
+            status.VerificationState.Errors.First().References.First().PositionX.Should().Be(1);
 
-        It should_return_first_error_with_single_reference_pointing_on_second_row = () =>
-            status.VerificationState.Errors.First().References.First().PositionY.ShouldEqual(0);
+        [NUnit.Framework.Test] public void should_return_first_error_with_single_reference_pointing_on_second_row () =>
+            status.VerificationState.Errors.First().References.First().PositionY.Should().Be(0);
 
-        It should_return_second_PL0030_error = () =>
-            status.VerificationState.Errors.Second().Code.ShouldEqual("PL0032");
+        [NUnit.Framework.Test] public void should_return_second_PL0030_error () =>
+            status.VerificationState.Errors.Second().Code.Should().Be("PL0032");
 
-        It should_return_second_error_with_single_reference = () =>
-            status.VerificationState.Errors.Second().References.Count().ShouldEqual(1);
+        [NUnit.Framework.Test] public void should_return_second_error_with_single_reference () =>
+            status.VerificationState.Errors.Second().References.Count().Should().Be(1);
 
-        It should_return_second_error_with_single_reference_of_type_Cell = () =>
-            status.VerificationState.Errors.Second().References.First().Type.ShouldEqual(PreloadedDataVerificationReferenceType.Cell);
+        [NUnit.Framework.Test] public void should_return_second_error_with_single_reference_of_type_Cell () =>
+            status.VerificationState.Errors.Second().References.First().Type.Should().Be(PreloadedDataVerificationReferenceType.Cell);
 
-        It should_return_second_error_with_single_reference_pointing_on_first_column = () =>
-            status.VerificationState.Errors.Second().References.First().PositionX.ShouldEqual(1);
+        [NUnit.Framework.Test] public void should_return_second_error_with_single_reference_pointing_on_first_column () =>
+            status.VerificationState.Errors.Second().References.First().PositionX.Should().Be(1);
 
-        It should_return_second_error_with_single_reference_pointing_on_second_row = () =>
-            status.VerificationState.Errors.Second().References.First().PositionY.ShouldEqual(1);
+        [NUnit.Framework.Test] public void should_return_second_error_with_single_reference_pointing_on_second_row () =>
+            status.VerificationState.Errors.Second().References.First().PositionY.Should().Be(1);
 
 
         private static ImportDataVerifier importDataVerifier;
