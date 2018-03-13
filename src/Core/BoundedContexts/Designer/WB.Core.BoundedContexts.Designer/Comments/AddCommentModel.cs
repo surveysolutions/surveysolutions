622149374418de21a550aa0efce64547d6468508
Slava Scherbak <slava.scherbak@gmail.com>
2018-03-13 17:38:11 -0400

    KP-11048: finished UI for adding comment for question

---
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Comments/AddCommentModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WB.Core.BoundedContexts.Designer.Comments
+{
+    public class AddCommentModel
+    {
+        public Guid Id { get; set; }
+        public Guid EntityId { get; set; }
+        public Guid QuestionnaireId { get; set; }
+        public string Comment { get; set; }
+    }
+}
