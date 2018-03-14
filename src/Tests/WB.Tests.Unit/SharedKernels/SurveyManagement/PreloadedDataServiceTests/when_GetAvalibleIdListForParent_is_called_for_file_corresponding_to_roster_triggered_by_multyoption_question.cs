376fcf53e022b78c4092754148a376f6436890c7
Andrii Kozhyn <hedgic@gmail.com>
2018-03-14 10:21:18 +0200

    Migrated tests to nunit

--- src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/PreloadedDataServiceTests/when_GetAvalibleIdListForParent_is_called_for_file_corresponding_to_roster_triggered_by_multyoption_question.cs
+++ src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/PreloadedDataServiceTests/when_GetAvalibleIdListForParent_is_called_for_file_corresponding_to_roster_triggered_by_multyoption_question.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using Machine.Specifications;
-using Main.Core.Documents;
 using Main.Core.Entities.SubEntities;
 using Main.Core.Entities.SubEntities.Question;
+using NUnit.Framework;
 using WB.Core.BoundedContexts.Headquarters.AssignmentImport.Parser;
 using WB.Core.GenericSubdomains.Portable.Implementation.ServiceVariables;
 using WB.Core.SharedKernels.DataCollection.ValueObjects;
@@ -15,9 +13,15 @@ namespace WB.Tests.Unit.SharedKernels.SurveyManagement.PreloadedDataServiceTests
     internal class when_GetAvalibleIdListForParent_is_called_for_file_corresponding_to_roster_triggered_by_multyoption_question :
         PreloadedDataServiceTestContext
     {
-        private Establish context = () =>
+        [Test]
+        [Ignore("KP-11068")]
+        public void should_return_single_id()
         {
-            questionnaireDocument =
+            Guid rosterGroupId = Guid.NewGuid();
+            Guid rosterSizeQuestionId = Guid.NewGuid();
+            string rosterSizeQuestionVariableName = "var";
+
+            var questionnaireDocument =
                 CreateQuestionnaireDocumentWithOneChapter(
                     new MultyOptionsQuestion()
                     {
@@ -34,29 +38,21 @@ namespace WB.Tests.Unit.SharedKernels.SurveyManagement.PreloadedDataServiceTests
                         RosterSizeQuestionId = rosterSizeQuestionId
                     });
 
-            importDataParsingService = CreatePreloadedDataService(questionnaireDocument);
-        };
+            var importDataParsingService = CreatePreloadedDataService(questionnaireDocument);
 
-        private Because of =
-            () =>
-                result =
-                    importDataParsingService.GetAvailableIdListForParent(
-                        CreatePreloadedDataByFile(
-                            new string[] { ServiceColumns.InterviewId, rosterSizeQuestionVariableName + "_0", rosterSizeQuestionVariableName + "_1" },
-                            new string[][] { new string[] { "1", "3", "" } },
-                            questionnaireDocument.Title),new ValueVector<Guid> { rosterSizeQuestionId}, new []{"1"}, Create.Entity.PreloadedDataByFile(new PreloadedDataByFile[0]));
+            // Act
+            var result =
+                importDataParsingService.GetAvailableIdListForParent(
+                    CreatePreloadedDataByFile(
+                        new string[] { ServiceColumns.InterviewId, rosterSizeQuestionVariableName + "_0", rosterSizeQuestionVariableName + "_1" },
+                        new string[][] { new string[] { "1", "3", "" } },
+                        questionnaireDocument.Title),
+                    new ValueVector<Guid> { rosterSizeQuestionId }, 
+                    new[] { "1" }, 
+                    Create.Entity.PreloadedDataByFile(new PreloadedDataByFile[0]));
 
-        private It should_return_not_null_result = () =>
-            result.ShouldNotBeNull();
-
-        private It should_result_have_2_ids_1_and_2 = () =>
-            result.SequenceEqual(new [] { 1 });
-
-        private static ImportDataParsingService importDataParsingService;
-        private static QuestionnaireDocument questionnaireDocument;
-        private static int[] result;
-        private static Guid rosterGroupId = Guid.NewGuid();
-        private static Guid rosterSizeQuestionId = Guid.NewGuid();
-        private static string rosterSizeQuestionVariableName = "var";
+            // Assert
+            Assert.That(result, Is.EqualTo(new[] { 1 }));
+        }
     }
 }
