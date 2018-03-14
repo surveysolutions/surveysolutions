b6e2b76e051ea397989ae83012d05d1708a9cac6
Slava Scherbak <slava.scherbak@gmail.com>
2018-03-14 12:35:17 -0400

    KP-11047: added comments index panel

--- src/UI/Designer/WB.UI.Designer/questionnaire/details/scripts/controllers/comments.js
+++ src/UI/Designer/WB.UI.Designer/questionnaire/details/scripts/controllers/comments.js
@@ -3,4 +3,42 @@
         function ($rootScope, $scope, $state, $i18next, commandService, utilityService, $log, confirmService, questionnaireService, hotkeys) {
             'use strict';
 
+            var hideCommentsPane = 'ctrl+alt+c';
+
+            if (hotkeys.get(hideCommentsPane) !== false) {
+                hotkeys.del(hideCommentsPane);
+            }
+
+            hotkeys.add(hideCommentsPane, $i18next.t('HotkeysCloseLookup'), function (event) {
+                event.preventDefault();
+                $scope.foldback();
+            });
+
+            $scope.commentThreads = [];
+
+            $scope.isFolded = false;
+
+            $scope.unfold = function () {
+                $scope.isFolded = true;
+            };
+
+            $scope.foldback = function () {
+                $scope.isFolded = false;
+                $rootScope.$broadcast("closeComments", {});
+            };
+
+            $scope.$on('openComments', function (scope, params) {
+                $scope.unfold();
+                if (!_.isUndefined(params) && !_.isUndefined(params.focusOn)) {
+                    setTimeout(function () { utilityService.focus("focusComment" + params.focusOn); }, 500);
+                }
+            });
+
+            $scope.$on('closeCommentsRequested', function () {
+                $scope.foldback();
+            });
+
+            $rootScope.$on('questionnaireLoaded', function () {
+                //$scope.loadLookupTables();
+            });
         });
