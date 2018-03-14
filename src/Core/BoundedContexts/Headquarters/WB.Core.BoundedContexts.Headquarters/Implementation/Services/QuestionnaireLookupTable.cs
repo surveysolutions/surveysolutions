d9d95e9f862c093cd4205451b311ec2df8a2cb87
Victor Hladkikh <me@alfeg.net>
2018-03-14 11:16:01 +0200

    KP-10698 Export questionnaire on HQ # Proper lookup table handling and deletion

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Implementation/Services/QuestionnaireLookupTable.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Implementation/Services/QuestionnaireLookupTable.cs
@@ -1,10 +1,7 @@
-﻿using WB.Core.SharedKernels.DataCollection.Implementation.Entities;
-
-namespace WB.Core.BoundedContexts.Headquarters.Implementation.Services
+﻿namespace WB.Core.BoundedContexts.Headquarters.Implementation.Services
 {
     public class QuestionnaireLookupTable
     {
-        public QuestionnaireIdentity Id { get; set; }
         public string FileName { get; set; }
         public string Content { get; set; }
     }
