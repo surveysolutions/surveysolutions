fdaa2a1de5632f395e732b84cc4ed3eb252e2937
Pavel Stovpivskyi <630198+spuv@users.noreply.github.com>
2018-03-16 13:07:56 +0200

    KP-10659 Added filter by unknown exception type

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Implementation/Synchronization/InterviewPackagesService.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Implementation/Synchronization/InterviewPackagesService.cs
@@ -139,7 +139,11 @@ namespace WB.Core.BoundedContexts.Headquarters.Implementation.Synchronization
             return this.brokenInterviewPackageStorage.Query(_ =>
                 _.Any(p => (p.ExceptionType == UnknownExceptionType || p.ExceptionType == undefined)
                     && p.InterviewId == interviewId
-                    && (p.ReprocessAttemptsCount > 2 || _.Count(d => d.InterviewId == interviewId) > 1)
+                    && 
+                    (
+                        p.ReprocessAttemptsCount > 2 
+                        || _.Count(d => d.InterviewId == interviewId && (p.ExceptionType == UnknownExceptionType || p.ExceptionType == undefined)) > 1
+                    )
                 )
             );
         }
@@ -157,7 +161,7 @@ namespace WB.Core.BoundedContexts.Headquarters.Implementation.Synchronization
             return this.brokenInterviewPackageStorage.Query(_ =>
             {
                 var filteredByError = _.Where(p => (p.ExceptionType == UnknownExceptionType || p.ExceptionType == undefined)
-                    && _.Count(d => d.InterviewId == p.InterviewId) == 1);
+                    && _.Count(d => d.InterviewId == p.InterviewId && (p.ExceptionType == UnknownExceptionType || p.ExceptionType == undefined)) == 1);
 
                 var filteredByTries = filteredByError.Where(p => 
                     p.ProcessingDate < utcNow5Minutes && p.ReprocessAttemptsCount < 1
