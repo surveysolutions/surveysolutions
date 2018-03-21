4a4b0492658d1456d433f4159adaa60dbbeef1af
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2018-03-21 12:38:17 +0200

    KP-7798: Converted bounded context to nunit

--- src/Tests/WB.Tests.Unit/BoundedContexts/Interviewer/DashboardDenormalizerTests/when_handling_InterviewSynchronized_event_and_one_of_prefilled_questions_is_GPS_with_answer_of_GeoPosition_type.cs
+++ src/Tests/WB.Tests.Unit/BoundedContexts/Interviewer/DashboardDenormalizerTests/when_handling_InterviewSynchronized_event_and_one_of_prefilled_questions_is_GPS_with_answer_of_GeoPosition_type.cs
@@ -1,6 +1,6 @@
 using System;
 using System.Threading.Tasks;
-using Machine.Specifications;
+using FluentAssertions;
 using Main.Core.Entities.SubEntities;
 using Moq;
 using Ncqrs.Eventing.ServiceModel.Bus;
@@ -19,8 +19,7 @@ namespace WB.Tests.Unit.BoundedContexts.Interviewer.DashboardDenormalizerTests
 {
     internal class when_handling_InterviewSynchronized_event_and_one_of_prefilled_questions_is_GPS_with_answer_of_GeoPosition_type
     {
-        Establish context = () =>
-        {
+        [NUnit.Framework.OneTimeSetUp] public void context () {
             dashboardItem = Create.Entity.InterviewView(prefilledGpsQuestionId);
 
             @event = Create.Event
@@ -50,19 +49,20 @@ namespace WB.Tests.Unit.BoundedContexts.Interviewer.DashboardDenormalizerTests
 
             denormalizer = Create.Service.DashboardDenormalizer(interviewViewRepository: interviewViewStorage,
                 questionnaireStorage: questionnaireStorage);
-        };
+            BecauseOf();
+        }
 
-        Because of = () =>
+        public void BecauseOf() =>
             denormalizer.Handle(@event);
 
-        It should_store_prefilled_GPS_question_ID_to_result_dashboard_item = () =>
-            dashboardItem.LocationQuestionId.ShouldEqual(prefilledGpsQuestionId);
+        [NUnit.Framework.Test] public void should_store_prefilled_GPS_question_ID_to_result_dashboard_item () =>
+            dashboardItem.LocationQuestionId.Should().Be(prefilledGpsQuestionId);
 
-        It should_store_latitude_from_answer_to_prefilled_GPS_question_to_result_dashboard_item = () =>
-            dashboardItem.LocationLatitude.ShouldEqual(prefilledGpsQuestionLatitude);
+        [NUnit.Framework.Test] public void should_store_latitude_from_answer_to_prefilled_GPS_question_to_result_dashboard_item () =>
+            dashboardItem.LocationLatitude.Should().Be(prefilledGpsQuestionLatitude);
 
-        It should_store_longitude_from_answer_to_prefilled_GPS_question_to_result_dashboard_item = () =>
-            dashboardItem.LocationLongitude.ShouldEqual(prefilledGpsQuestionLongitude);
+        [NUnit.Framework.Test] public void should_store_longitude_from_answer_to_prefilled_GPS_question_to_result_dashboard_item () =>
+            dashboardItem.LocationLongitude.Should().Be(prefilledGpsQuestionLongitude);
 
         private static InterviewerDashboardEventHandler denormalizer;
         private static IPublishedEvent<InterviewSynchronized> @event;
