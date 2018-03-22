e1eb677aa7f939f8b16afd3e9af2ba5ce8768e4c
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2018-03-22 18:17:39 -0400

    KP-9119 Update autonumeric component

--- src/UI/Headquarters/WB.UI.Headquarters/HqApp/src/webinterview/components/questions/Integer.vue
+++ src/UI/Headquarters/WB.UI.Headquarters/HqApp/src/webinterview/components/questions/Integer.vue
@@ -11,7 +11,7 @@
                         :disabled="isSpecialValueSelected || !$me.acceptAnswer"
                         :class="{ 'special-value-selected': isSpecialValueSelected }"
                         @blur="answerIntegerQuestion" 
-                        v-numericFormatting="{aSep: groupSeparator, mDec: 0, vMin: '-2147483648', vMax: '2147483647', aPad: false }">
+                        v-numericFormatting="{digitGroupSeparator: groupSeparator, decimalPlaces: 0, decimalPlaces: 0, minimumValue: '-2147483648', maximumValue: '2147483647'}">
                         <wb-remove-answer v-if="!isSpecialValueSelected" :on-remove="removeAnswer"/>
                     </div>
                 </div>
@@ -41,8 +41,12 @@
     import * as $ from "jquery"
     import modal from "../modal"
     import numerics from "../numerics"
-
+    
     export default {
+        data() {
+            return {
+                autoNumericElement: null}
+            },
         name: 'Integer',
         mixins: [entityDetails],
         computed: { 
@@ -74,7 +78,7 @@
         },
         methods: {
             answerIntegerQuestion(evnt) {
-                const answerString = numerics.get($(evnt.target))
+                const answerString = this.autoNumericElement.getNumericString();
                 const answer = answerString != undefined && answerString != ''
                     ? parseInt(answerString)
                     : null;
@@ -143,6 +147,10 @@
                 if (!this.$me.isAnswered) {
                     return
                 }
+                
+                if(this.autoNumericElement)
+                    this.autoNumericElement.clear()
+                
                 if (!this.$me.isRosterSize) {
                     this.$store.dispatch("removeAnswer", this.id)
                     return
@@ -176,7 +184,11 @@
                 }
                 return false;
             }
+        },
+        beforeDestroy () {
+            if (this.autoNumericElement) {
+                this.autoNumericElement.remove()
+            }
         }
     }
-
 </script>
--- src/UI/Headquarters/WB.UI.Headquarters/HqApp/src/webinterview/directives/NumericFormatting.js
+++ src/UI/Headquarters/WB.UI.Headquarters/HqApp/src/webinterview/directives/NumericFormatting.js
@@ -1,23 +1,17 @@
 import * as $ from "jquery"
 import Vue from "vue"
-import numerics from "../components/numerics"
+import AutoNumeric from "autoNumeric"
 
 const defaults = {
-        aSep: "",
-        aPad: false
+    digitGroupSeparator: "",
+    decimalPlaces: 0
 }
 
-Vue.directive("numericFormatting", {
-    bind: (el, binding) => {
-        const settings = $.extend( {}, defaults, binding.value )
-        numerics.init(el, settings)// $(el).autoNumeric("init", settings)
-        numerics.update(el, settings) // $(el).autoNumeric("update", settings)
-    },
-    update: (el, binding) => {
-        if (binding.value) {
-            const settings = $.extend( {}, defaults, binding.value )
-            numerics.update(el, settings) // $(el).autoNumeric("update", settings)
-        }
-    },
-    unbind: (el) => numerics.destroy(el)
+Vue.directive("numericFormatting",{ 
+    bind:(el, binding, vnode) => {
+       const settings = $.extend( {}, defaults, binding.value )
+       vnode.context.autoNumericElement = new AutoNumeric(
+           el,
+           settings)   
+   }
 })
