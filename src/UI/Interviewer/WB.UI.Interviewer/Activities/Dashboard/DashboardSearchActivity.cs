1d482926868a1915f9ec5a5c3cd9d2b3654a4550
Andrii Kozhyn <hedgic@gmail.com>
2018-03-23 17:19:41 +0200

    KP-11115: Add back button to search panel

--- src/UI/Interviewer/WB.UI.Interviewer/Activities/Dashboard/DashboardSearchActivity.cs
+++ src/UI/Interviewer/WB.UI.Interviewer/Activities/Dashboard/DashboardSearchActivity.cs
@@ -1,21 +1,11 @@
-﻿using System.ComponentModel;
-using Android.App;
-using Android.Content;
+﻿using Android.App;
 using Android.OS;
-using Android.Support.Design.Widget;
-using Android.Support.V4.View;
 using Android.Support.V7.Widget;
 using Android.Views;
 using MvvmCross.Binding.Droid.BindingContext;
-using MvvmCross.Droid.Support.V4;
 using MvvmCross.Droid.Support.V7.RecyclerView;
-using MvvmCross.Platform;
-using WB.Core.BoundedContexts.Interviewer.Properties;
-using WB.Core.BoundedContexts.Interviewer.Services;
 using WB.Core.BoundedContexts.Interviewer.Views.Dashboard;
-using WB.UI.Interviewer.Services;
 using WB.UI.Shared.Enumerator.Activities;
-using MvxFragmentStatePagerAdapter = WB.UI.Interviewer.CustomControls.MvxFragmentStatePagerAdapter;
 
 namespace WB.UI.Interviewer.Activities.Dashboard
 {
@@ -28,51 +18,14 @@ namespace WB.UI.Interviewer.Activities.Dashboard
     {
         protected override int ViewResourceId => Resource.Layout.dashboard_search;
 
-        protected override void OnPause()
-        {
-            base.OnPause();
-        }
-
-        protected override void OnResume()
-        {
-            base.OnResume();
-        }
-
         protected override void OnCreate(Bundle bundle)
         {
-            Mvx.Trace("Dashboard Search activity started");
             base.OnCreate(bundle);
             this.SetSupportActionBar(this.FindViewById<Toolbar>(Resource.Id.toolbar));
-            
+
             var recyclerView = this.FindViewById<MvxRecyclerView>(Resource.Id.dashboard_tab_recycler);
             recyclerView.HasFixedSize = true;
-            if (recyclerView != null)
-                recyclerView.Adapter = new RecyclerViewAdapter((IMvxAndroidBindingContext)base.BindingContext);
-        }
-
-        protected override void OnStart()
-        {
-            base.OnStart();
-        }
-
-        protected override void OnViewModelSet()
-        {
-            base.OnViewModelSet();
-        }
-
-        public override void OnBackPressed()
-        {
-            base.OnBackPressed();
-        }
-
-        public override bool OnCreateOptionsMenu(IMenu menu)
-        {
-            return base.OnCreateOptionsMenu(menu);
-        }
-
-        public override bool OnOptionsItemSelected(IMenuItem item)
-        {
-            return base.OnOptionsItemSelected(item);
+            recyclerView.Adapter = new RecyclerViewAdapter((IMvxAndroidBindingContext)base.BindingContext);
         }
     }
 }
