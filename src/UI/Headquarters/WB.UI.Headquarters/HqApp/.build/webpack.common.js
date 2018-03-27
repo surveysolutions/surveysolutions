3162b433c51efedb45549e63d6a93063e56557ae
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2018-03-27 17:53:50 -0400

    KP-9119 Update autonumeric component

--- src/UI/Headquarters/WB.UI.Headquarters/HqApp/.build/webpack.common.js
+++ src/UI/Headquarters/WB.UI.Headquarters/HqApp/.build/webpack.common.js
@@ -163,27 +163,9 @@ module.exports = function (appConfig) {
             // minizing js files
             new UglifyJsPlugin({
                 sourceMap: true,
-                //minimize: true,
-                uglifyOptions: { // https://medium.com/tldr-tech/web-performance-a-poc-by-example-41307c7e6cbe
-                  warnings: false, // warn about potentially dangerous optimizations/code
-                  sequences: true,  // join consecutive statemets with the “comma operator”
-                  properties: true,  // optimize property access: a["foo"] → a.foo
-                  dead_code: true,  // discard unreachable code
-                  drop_debugger: true,  // discard “debugger” statements
-                  unsafe: true, // some unsafe optimizations (see below)
-                  conditionals: true,  // optimize if-s and conditional expressions
-                  comparisons: true,  // optimize comparisons
-                  evaluate: true,  // evaluate constant expressions
-                  booleans: true,  // optimize boolean expressions
-                  loops: true,  // optimize loops
-                  unused: true,  // drop unused variables/functions
-                  hoist_funs: true,  // hoist function declarations
-                  hoist_vars: true, // hoist variable declarations
-                  if_return: true,  // optimize if-s followed by return/continue
-                  join_vars: true,  // join var declarations
-                  cascade: true,  // try to cascade `right` into `left` in sequences
-                  side_effects: true,  // drop side-effect-free statements         
-				  
+                cache: true,
+				parallel:true,
+                uglifyOptions: {                  
 				  compress:true
 				}
             }),
