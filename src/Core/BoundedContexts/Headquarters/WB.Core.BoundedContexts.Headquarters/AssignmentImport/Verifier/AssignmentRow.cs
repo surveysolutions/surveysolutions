455b40e042f48cc5f955e0bb5f10b3acf9de94ec
Roman Lytvyn <litvin.roma@gmail.com>
2018-03-27 15:50:02 +0300

    KP-4479 Refactor verification data on preloading. Split file dtos and dtos to verify.

---
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/AssignmentImport/Verifier/AssignmentRow.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using WB.Core.BoundedContexts.Headquarters.Views.User;
+
+namespace WB.Core.BoundedContexts.Headquarters.AssignmentImport.Verifier
+{
+    public class AssignmentRow
+    {
+        public AssignmentValue[] Answers { get; set; }
+    }
+
+    public abstract class AssignmentValue
+    {
+        public string FileName { get; set; }
+        public int Row { get; set; }
+        public string Column { get; set; }
+        public string Value { get; set; }
+        public string InterviewId { get; set; }
+    }
+
+    public class AssignmentTextAnswer : AssignmentAnswer
+    {
+        public string Mask { get; set; }
+    }
+    public class AssignmentGpsAnswer : AssignmentAnswers { }
+
+    public class AssignmentIntegerAnswer : AssignmentAnswer
+    {
+        public bool IsRosterSizeForLongRoster { get; set; }
+        public bool IsRosterSize { get; set; }
+        public int? Answer { get; set; }
+    }
+    public class AssignmentDataTimeAnswer : AssignmentAnswer
+    {
+        public DateTime? Answer { get; set; }
+    }
+
+    public class AssignmentDoubleAnswer : AssignmentAnswer
+    {
+        public double? Answer { get; set; }
+    }
+
+    public class AssignmentCategoricalMultiAnswer : AssignmentAnswers
+    {
+        public int? MaxAnswersCount { get; set; }
+    }
+
+    public class AssignmentAnswer : AssignmentValue
+    {
+        public string VariableName { get; set; }
+    }
+
+    public class AssignmentRosterInstanceCode : AssignmentAnswer
+    {
+        public int Code { get; set; }
+    }
+
+    public class AssignmentAnswers : AssignmentValue
+    {
+        public string VariableName { get; set; }
+        public AssignmentAnswer[] Values { get; set; }
+    }
+
+    public class AssignmentResponsible : AssignmentValue
+    {
+        public UserToVerify Responsible { get; set; }
+    }
+
+    public class AssignmentQuantity : AssignmentValue
+    {
+        public int? Quantity { get; set; }
+    }
+}
