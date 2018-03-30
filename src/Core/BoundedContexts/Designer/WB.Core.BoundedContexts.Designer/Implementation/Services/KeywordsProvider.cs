07ea61c92d7912f8673047526e1d884438370811
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2018-03-30 16:16:15 -0400

    KP-10756 Export doesn't work

--- src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Implementation/Services/KeywordsProvider.cs
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Implementation/Services/KeywordsProvider.cs
@@ -17,6 +17,7 @@ namespace WB.Core.BoundedContexts.Designer.Implementation.Services
                 .Union(SpssReservedKeywords)
                 .Union(new[] { substitutionService.RosterTitleSubstitutionReference })
                 .Union(SurveySolutionsServiceVariablesKeywords)
+                .Union(WindowsFileNamingReservedNames)
                 .Union(ServiceColumns.SystemVariables.Values.Select(x=>x.VariableExportColumnName))
                 .Select(x => x.ToLower())
                 .ToArray();
@@ -53,9 +54,16 @@ namespace WB.Core.BoundedContexts.Designer.Implementation.Services
 
         private static readonly List<string> SurveySolutionsServiceVariablesKeywords = new List<string>()
         {
-            "rowcode","rowname","rowindex","roster","id", "parentid1", "parentid2", "parentid3", "parentid4", "self", "state", "quest", "optioncode"
+            "rowcode","rowname","rowindex","roster","id", "parentid1", "parentid2", "parentid3", "parentid4",
+            "self", "state", "quest", "optioncode"
         };
-        
+
+        private static readonly List<string> WindowsFileNamingReservedNames = new List<string>()
+        {
+            "con", "prn", "aux", "nul", "com1", "com2", "com3", "com4", "com5", "com6", "com7", "com8","com9",
+            "lpt1", "lpt2","lpt3", "lpt4", "lpt5", "lpt6", "lpt7", "lpt8", "lpt9"
+        };
+
         public bool IsReservedKeyword(string keyword)
         {
             if (string.IsNullOrEmpty(keyword)) return false;
