7083be4c89f40eb534a4420d4db00bd89b8f8cd2
Andrii Kozhyn <hedgic@gmail.com>
2018-04-10 15:15:52 +0300

    KP-11142: Implemented progress reporting

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/AssignmentImport/Upgrade/AssignmentsUpgrader.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/AssignmentImport/Upgrade/AssignmentsUpgrader.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
+using Quartz.Util;
 using WB.Core.BoundedContexts.Headquarters.AssignmentImport.Preloading;
 using WB.Core.BoundedContexts.Headquarters.Assignments;
 using WB.Core.Infrastructure.PlainStorage;
+using WB.Core.Infrastructure.Transactions;
+using WB.Core.SharedKernels.DataCollection.Implementation.Aggregates.InterviewEntities.Answers;
 using WB.Core.SharedKernels.DataCollection.Implementation.Entities;
 using WB.Core.SharedKernels.DataCollection.Repositories;
 
@@ -20,53 +24,70 @@ namespace WB.Core.BoundedContexts.Headquarters.AssignmentImport.Upgrade
         private readonly IInterviewImportService importService;
         private readonly IQuestionnaireStorage questionnaireStorage;
         private readonly IAssignmentsUpgradeService upgradeService;
+        private readonly IPlainTransactionManager transactionManager;
 
         public AssignmentsUpgrader(IPlainStorageAccessor<Assignment> assignments,
             IInterviewImportService importService,
             IQuestionnaireStorage questionnaireStorage,
-            IAssignmentsUpgradeService upgradeService
+            IAssignmentsUpgradeService upgradeService,
+            IPlainTransactionManager transactionManager
            )
         {
             this.assignments = assignments ?? throw new ArgumentNullException(nameof(assignments));
             this.importService = importService ?? throw new ArgumentNullException(nameof(importService));
             this.questionnaireStorage = questionnaireStorage ?? throw new ArgumentNullException(nameof(questionnaireStorage));
             this.upgradeService = upgradeService;
+            this.transactionManager = transactionManager;
         }
 
         public void Upgrade(Guid processId, QuestionnaireIdentity migrateFrom, QuestionnaireIdentity migrateTo)
         {
-            var idsToMigrate = assignments.Query(_ =>
-                _.Where(x => x.QuestionnaireId.Id == migrateFrom.Id && x.QuestionnaireId.Version == migrateFrom.Version).Select(x => x.Id).ToList());
+            var idsToMigrate = this.transactionManager.ExecuteInPlainTransaction(() => assignments.Query(_ =>
+                _.Where(x => x.QuestionnaireId.Id == migrateFrom.Id && x.QuestionnaireId.Version == migrateFrom.Version).Select(x => x.Id).ToList()));
 
-            var targetQuestionnaire = this.questionnaireStorage.GetQuestionnaire(migrateTo, null);
+            var targetQuestionnaire = this.transactionManager.ExecuteInPlainTransaction(() =>this.questionnaireStorage.GetQuestionnaire(migrateTo, null));
             int migratedSuccessfully = 0;
             List<AssignmentUpgradeError> upgradeErrors = new List<AssignmentUpgradeError>();
 
             foreach (var assignmentId in idsToMigrate)
             {
-                var oldAssignment = assignments.GetById(assignmentId);
-                if (!oldAssignment.IsCompleted)
+                this.transactionManager.ExecuteInPlainTransaction(() =>
                 {
-                    var assignmentVerification = this.importService.VerifyAssignment(oldAssignment.Answers.GroupedByLevels(), targetQuestionnaire);
-                    if (assignmentVerification.Status)
+                    var oldAssignment = assignments.GetById(assignmentId);
+                    if (!oldAssignment.IsCompleted)
                     {
-                        oldAssignment.Archive();
+                        var assignmentVerification = this.importService.VerifyAssignment(oldAssignment.Answers.GroupedByLevels(), targetQuestionnaire);
+                        if (assignmentVerification.Status)
+                        {
+                            oldAssignment.Archive();
 
-                        var newAssignment = new Assignment(migrateTo, oldAssignment.ResponsibleId, oldAssignment.Quantity);
-                        newAssignment.SetAnswers(oldAssignment.Answers);
-                        newAssignment.SetIdentifyingData(oldAssignment.IdentifyingData);
-                        assignments.Store(newAssignment, null);
-                        migratedSuccessfully++;
+                            var newAssignment = new Assignment(migrateTo, oldAssignment.ResponsibleId, oldAssignment.Quantity);
+
+                            newAssignment.SetAnswers(oldAssignment.Answers.ToList());
+                            var newIdentifyingData = new List<IdentifyingAnswer>();
+                            foreach (var oldIdentifyingQuestion in oldAssignment.IdentifyingData)
+                            {
+                                newIdentifyingData.Add(IdentifyingAnswer.Create(newAssignment, 
+                                    targetQuestionnaire, 
+                                    oldIdentifyingQuestion.Answer, 
+                                    oldIdentifyingQuestion.Identity,
+                                    oldIdentifyingQuestion.VariableName));
+                            }
+
+                            newAssignment.SetIdentifyingData(newIdentifyingData);
+                            assignments.Store(newAssignment, null);
+                            migratedSuccessfully++;
+                        }
+                        else
+                        {
+                            upgradeErrors.Add(new AssignmentUpgradeError(assignmentId, assignmentVerification.ErrorMessage));
+                        }
                     }
                     else
                     {
-                        upgradeErrors.Add(new AssignmentUpgradeError(assignmentId, assignmentVerification.ErrorMessage));
+                        migratedSuccessfully++;
                     }
-                }
-                else
-                {
-                    migratedSuccessfully++;
-                }
+                });
 
                 this.upgradeService.ReportProgress(processId, new AssignmentUpgradeProgressDetails(migrateFrom, migrateTo, idsToMigrate.Count, migratedSuccessfully, upgradeErrors, AssignmentUpgradeStatus.InProgress));
             }
---
+++ src/UI/Headquarters/WB.UI.Headquarters/API/AssignmentsUpgradeApiController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Dynamic;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WB.Core.BoundedContexts.Headquarters.AssignmentImport;
+using WB.Core.BoundedContexts.Headquarters.Factories;
+using WB.Core.GenericSubdomains.Portable.Services;
+using WB.Core.Infrastructure.CommandBus;
+using WB.UI.Headquarters.Code;
+using WB.UI.Headquarters.Controllers;
+using WB.UI.Headquarters.Resources;
+using WB.UI.Shared.Web.Filters;
+
+namespace WB.UI.Headquarters.API
+{
+    [AuthorizeOr403(Roles = "Administrator, Headquarter")]
+    [ApiNoCache]
+    public class AssignmentsUpgradeApiController : BaseApiController
+    {
+        private readonly IAssignmentsUpgradeService upgradeService;
+        private readonly IQuestionnaireBrowseViewFactory browseViewFactory;
+
+        public AssignmentsUpgradeApiController(ICommandService commandService, 
+            ILogger logger, 
+            IAssignmentsUpgradeService upgradeService,
+            IQuestionnaireBrowseViewFactory browseViewFactory) : base(commandService, logger)
+        {
+            this.upgradeService = upgradeService;
+            this.browseViewFactory = browseViewFactory;
+        }
+
+        
+        [CamelCase]
+        [HttpGet]
+        public HttpResponseMessage Status(string id)
+        {
+            AssignmentUpgradeProgressDetails assignmentUpgradeProgressDetails = this.upgradeService.Status(Guid.Parse(id));
+            if (assignmentUpgradeProgressDetails != null)
+            {
+                dynamic response = new ExpandoObject();
+                response.progressDetails = assignmentUpgradeProgressDetails;
+
+                var questionnaireTo = this.browseViewFactory.GetById(assignmentUpgradeProgressDetails.MigrateTo);
+                response.migrateToTitle = string.Format(Pages.QuestionnaireNameFormat, questionnaireTo.Title,
+                    questionnaireTo.Version);
+                var questionnaireFrom = this.browseViewFactory.GetById(assignmentUpgradeProgressDetails.MigrateFrom);
+                response.migrateFromTitle = string.Format(Pages.QuestionnaireNameFormat, questionnaireFrom.Title,
+                    questionnaireFrom.Version);
+
+                return Request.CreateResponse(HttpStatusCode.OK, (object)response);
+            }
+
+            throw new HttpResponseException(HttpStatusCode.NotFound);
+        }
+    }
+}
