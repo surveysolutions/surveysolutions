72593ffad8a14511575765f820d6da6b2165c649
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2018-04-10 10:09:04 -0400

    KP-11154 Show the picture taken on tablet full screen on the tablet

---
+++ src/Core/SharedKernels/Enumerator/Enumerator/ViewModels/PhotoViewViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading.Tasks;
+using WB.Core.GenericSubdomains.Portable.Services;
+using WB.Core.SharedKernels.DataCollection.Repositories;
+using WB.Core.SharedKernels.Enumerator.Services;
+using WB.Core.SharedKernels.Enumerator.Services.Infrastructure;
+
+namespace WB.Core.SharedKernels.Enumerator.ViewModels
+{
+    public class PhotoViewViewModel : BaseViewModel<PhotoViewViewModelArgs>
+    {
+        private readonly ILogger logger;
+        private readonly IInterviewFileStorage imageFileStorage;
+        private byte[] answer;
+
+        private Guid interviewId;
+        private string fileName;
+
+        private const string StateKey = "interviewPhotoPreview";
+
+        public PhotoViewViewModel(IPrincipal principal, IViewModelNavigationService viewModelNavigationService,
+            ILogger logger, IInterviewFileStorage imageFileStorage) 
+            : base(principal, viewModelNavigationService)
+        {
+            this.logger = logger;
+            this.imageFileStorage = imageFileStorage;
+        }
+
+        public byte[] Answer
+        {
+            get => this.answer;
+            set
+            {
+                this.answer = value;
+                this.RaisePropertyChanged();
+            }
+        }
+
+        public override void Prepare(PhotoViewViewModelArgs parameter)
+        {
+            this.interviewId = parameter.InterviewId;
+            this.fileName = parameter.FileName;
+        }
+
+        public override async Task Initialize()
+        {
+            await base.Initialize();
+            if (interviewId == Guid.Empty) throw new ArgumentException(nameof(interviewId));
+
+            this.Answer = this.imageFileStorage.GetInterviewBinaryData(this.interviewId, this.fileName);
+
+            if (this.Answer == null)
+                throw new ArgumentException(nameof(Answer));
+        }
+    }
+}
---
+++ src/UI/Shared/WB.UI.Shared.Enumerator/Activities/PhotoViewActivity.cs
@@ -0,0 +1,68 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Android.Views;
+using WB.Core.SharedKernels.Enumerator.ViewModels;
+
+namespace WB.UI.Shared.Enumerator.Activities
+{
+    [Activity(WindowSoftInputMode = SoftInput.StateHidden, Theme = "@style/AppTheme", ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize)]
+    public class PhotoViewActivity : BaseActivity<PhotoViewViewModel>
+    {
+        protected override int ViewResourceId => Resource.Layout.interview_photo_view;
+        
+        public override void OnBackPressed()
+        {
+            this.Cancel();
+        }
+
+        private void Cancel()
+        {
+            this.Finish();
+        }
+        
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            //var toolbar = this.FindViewById<Toolbar>(Android.Resource.Id.toolbar);
+            //toolbar.Title = "";
+            //this.SetSupportActionBar(toolbar);
+
+            //this.ViewModel.OnAreaEditCompleted += OnAreaEditCompleted;
+
+            //workaround
+            //inflated map doesn't work (should be fixed in next Esri release) 
+            /*var map = new MapView();
+            var bindingSet = this.CreateBindingSet<AreaEditorActivity, AreaEditorViewModel>();
+
+            bindingSet.Bind(map)
+                .For(v => v.Map)
+                .To(vm => vm.Map);
+
+            bindingSet.Apply();
+
+            this.ViewModel.MapView = map;
+
+            var container = this.FindViewById<LinearLayout>(Android.Resource.Id.area_map_view_container);
+            container.AddView(map);*/
+        }
+
+        /*public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            this.MenuInflater.Inflate(Android.Resource.Menu.area_editor, menu);
+
+            menu.LocalizeMenuItem(Android.Resource.Id.map_editor_exit, UIResources.MenuItem_Title_AreaCancelEdit);
+
+            return base.OnCreateOptionsMenu(menu);
+        }*/
+        /*public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            if(item.ItemId == Android.Resource.Id.map_editor_exit)
+                this.ViewModel.CancelCommand.Execute();
+            
+            return base.OnOptionsItemSelected(item);
+        }*/
+
+    }
+}
