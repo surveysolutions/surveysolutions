ce8dc329a7106b51ae08ee4566356223397f4d4d
Andrii Kozhyn <hedgic@gmail.com>
2018-04-11 15:44:38 +0300

    KP-11167: Download errors link added

--- src/UI/Headquarters/WB.UI.Headquarters/HqApp/src/hqapp/Views/HQ/SurveySetup/UpgradeProgress.vue
+++ src/UI/Headquarters/WB.UI.Headquarters/HqApp/src/hqapp/Views/HQ/SurveySetup/UpgradeProgress.vue
@@ -38,19 +38,19 @@
             </div>
         </div>
         <div class="row-fluid" v-else-if="progress.progressDetails.status === 'Done'">
-            <div class="col-sm-12 prefilled-data-info info-block">
-                {{$t('Assignments.UpgradeProgressDone')}}
-                <p>
-                    <ul class="list-unstyled">
-                        <li>
-                            {{$t('Assignments.UpgradeProgressDoneCount', {processed: progress.progressDetails.assignmentsMigratedSuccessfully})}}
-                        </li>
-                        <li v-if="errorsCount">
-                            {{$t('Assignments.UpgradeProgressErrorCount', {count: errorsCount})}}
-                            <a :href="errorsExportUrl" target="_blank">({{$t('Assignments.UpgradeDownloadFailed')}})</a>
-                        </li>
-                    </ul>
-                </p>
+            <div class="col-sm-7 col-xs-12 action-block preloading-done active-preloading">
+                <div class="import-progress">
+                    <p>
+                        {{$t('Assignments.UpgradeProgressDoneCount', {processed: progress.progressDetails.assignmentsMigratedSuccessfully})}}
+                    </p>
+                    <p v-if="errorsCount">
+                        {{$t('Assignments.UpgradeProgressErrorCount', {count: errorsCount})}}
+                        <a :href="errorsExportUrl" target="_blank">({{$t('Assignments.UpgradeDownloadFailed')}})</a>
+                    </p>
+                </div>
+                <div class="action-buttons">
+                    <a class="btn btn-primary" :href="$config.model.surveySetupUrl">{{$t('MainMenu.SurveySetup')}}</a>
+                </div>
             </div>
         </div>
     </HqLayout>
@@ -72,24 +72,31 @@ export default {
   mounted() {
     this.updateStatus();
     this.timerId = window.setInterval(() => {
-      this.updateStatus()
+      this.updateStatus();
     }, 3000);
   },
   computed: {
     processId() {
-      return this.$route.params.processId
+      return this.$route.params.processId;
     },
     errorsExportUrl() {
-        return `${this.$config.model.exportErrorsUrl}/${this.processId}`
+      return `${this.$config.model.exportErrorsUrl}/${this.processId}`;
     },
     errorsCount() {
-      return this.progress.progressDetails.assignmentsMigratedWithError.length
+      return this.progress.progressDetails.assignmentsMigratedWithError.length;
     },
     totalProcessedCount() {
-      return this.progress.progressDetails.assignmentsMigratedSuccessfully + this.progress.progressDetails.assignmentsMigratedWithError.length
+      return (
+        this.progress.progressDetails.assignmentsMigratedSuccessfully +
+        this.progress.progressDetails.assignmentsMigratedWithError.length
+      );
     },
     overallProgressPercent() {
-      return Math.round(this.totalProcessedCount / this.progress.progressDetails.totalAssignmentsToMigrate * 100);
+      return Math.round(
+        this.totalProcessedCount /
+          this.progress.progressDetails.totalAssignmentsToMigrate *
+          100
+      );
     }
   },
   methods: {
@@ -99,20 +106,23 @@ export default {
         .get(`${this.$config.model.progressUrl}/${this.processId}`)
         .then(
           response => {
-            self.progress = response.data
-            if (self.progress.status === "Cancelled" || self.progress.status === "Done") {
-              window.clearInterval(self.timerId)
+            self.progress = response.data;
+            if (
+              self.progress.status === "Cancelled" ||
+              self.progress.status === "Done"
+            ) {
+              window.clearInterval(self.timerId);
             }
           },
           error => {
             if (error.response.status === 404) {
-              window.location = $config.model.surveySetupUrl
+              window.location = $config.model.surveySetupUrl;
             }
           }
         );
     },
     stop() {
-      this.$http.post(`${this.$config.model.stopUrl}/${this.processId}`)
+      this.$http.post(`${this.$config.model.stopUrl}/${this.processId}`);
     }
   }
 };
