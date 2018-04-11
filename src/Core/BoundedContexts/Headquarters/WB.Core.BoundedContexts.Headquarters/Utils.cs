59307ff1c7cd2bbae91b014a1c97191fc35aa420
Victor Gladkikh <me@alfeg.net>
2018-04-11 18:43:40 +0300

    KP-11251 Cannot export interview with negative roster vector value

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Utils.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Utils.cs
@@ -1,4 +1,6 @@
-﻿using System.Text.RegularExpressions;
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace WB.Core.BoundedContexts.Headquarters
 {
@@ -8,6 +10,32 @@ namespace WB.Core.BoundedContexts.Headquarters
         public static string RemoveNewLine(string value)
         {
             return string.IsNullOrEmpty(value) ? string.Empty : RemoveNewLineRegEx.Replace(value, " ");
+        }
+
+        public static int[] ParseMinusDelimitedIntArray(this string arrayString)
+        {
+            if (string.IsNullOrWhiteSpace(arrayString) || string.IsNullOrWhiteSpace(arrayString.Trim('_'))) return null;
+
+            //"-1-2--3".Split('-') => string[5] { "", "1", "2", "", "3" }
+            // every empty space mean that we encounter negative number
+            var items = arrayString.Split('-');
+            var result = new List<int>();
+
+            for (int i = 0; i < items.Length; i++)
+            {
+                if (string.IsNullOrWhiteSpace(items[i]))
+                {
+                    if (i == items.Length - 1) // if this is last item in array
+                        continue;
+
+                    // parse next item and increment index
+                    result.Add(-int.Parse(items[++i]));
+                }
+                else
+                    result.Add(int.Parse(items[i]));
+            }
+
+            return result.ToArray();
         }
     }
 }
