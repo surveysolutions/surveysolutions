9be0967d418b2a93ade890c9b97db73169271839
Victor Hladkikh <me@alfeg.net>
2018-04-11 18:43:40 +0300

    KP-11251 Cannot export interview with negative roster vector value

---
+++ src/Tests/WB.Tests.Integration/UtilsTest/MinusDelimitedIntArrayParserTest.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+using WB.Core.BoundedContexts.Headquarters;
+
+namespace WB.Tests.Integration.UtilsTest
+{
+    [TestFixture]
+    public class MinusDelimitedIntArrayParserTest
+    { 
+        [TestCase("1-2-3", 1, 2, 3)]
+        [TestCase("-1-2-3", -1, 2, 3)]
+        [TestCase("-3", -3)]
+        [TestCase("1--2--3", 1, -2, -3)]
+        [TestCase("1--2--3-", 1, -2, -3)]
+        public void should_parse_input_string_into_int_array_result(string input, params int[] result)
+        {
+            var parse = input.ParseMinusDelimitedIntArray();
+            Assert.That(parse, Is.EqualTo(result));
+        }
+    }
+}
