298338b6bd52664c2bfd30a2ec38e622963afe76
Victor Hladkikh <me@alfeg.net>
2018-04-13 16:22:14 +0300

    Typeahead component now support fuzzy search. props: fuzzy

--- src/UI/Headquarters/WB.UI.Headquarters/HqApp/src/hqapp/components/Typeahead.vue
+++ src/UI/Headquarters/WB.UI.Headquarters/HqApp/src/hqapp/components/Typeahead.vue
@@ -16,20 +16,20 @@
                 role="menu">
                 <li v-if="!noSearch">
                     <input type="text"
-                           ref="searchBox"
-                           :id="inputId"
-                           placeholder="Search"
-                           @input="updateOptionsList"
-                           @keyup.down="onSearchBoxDownKey"
-                           v-model="searchTerm" />
+                    ref="searchBox" 
+                    :id="inputId" 
+                    placeholder="Search" 
+                    @input="updateOptionsList" 
+                    @keyup.down="onSearchBoxDownKey" 
+                    v-model="searchTerm" />
                 </li>
                 <li v-for="option in options"
-                    :key="option.key">
+                    :key="option.item.key">
                     <a 
-                       :class="[option.iconClass]"
+                       :class="[option.item.iconClass]"
                        href="javascript:void(0);"
-                       @click="selectOption(option)"
-                       v-html="highlight(option.value, searchTerm)"
+                        @click="selectOption(option.item)" 
+                       v-html="highlight(option, searchTerm)"
                        @keydown.up="onOptionUpKey"></a>
                 </li>
                 <li v-if="isLoading">
@@ -49,37 +49,43 @@
 </template>
 
 <script>
+import Fuse from "fuse.js";
+
 export default {
     name: 'Typeahead',
 
-    props: {
-        fetchUrl: String,
-        controlId: String,
-        value: Object,
-        placeholder: String,
-        ajaxParams: Object,
-        values: Array,
-        noSearch: Boolean
-    },
+  props: {
+    fetchUrl: String,
+    controlId: String,
+    value: Object,
+    placeholder: String,
+    ajaxParams: Object,
+    values: Array,
+    noSearch: Boolean,
+    fuzzy: {
+        type: Boolean,
+        default: false
+    }
+  },
 
-    data() {
-        return {
-            options: [],
-            isLoading: false,
+  data() {
+    return {
+      options: [],
+      isLoading: false,
             searchTerm: ''
-        };
-    },
+    };
+  },
 
-    computed: {
-        inputId() {
-            return `sb_${this.controlId}`;
-        },
-        placeholderText() {
-            return this.placeholder || "Select";
-        }
+  computed: {
+    inputId() {
+      return `sb_${this.controlId}`;
     },
+    placeholderText() {
+      return this.placeholder || "Select";
+    }
+  },
 
-    mounted() {
+  mounted() {
         const jqEl = $(this.$el)
         const focusTo = jqEl.find(`#${this.inputId}`)
 
@@ -91,61 +97,114 @@ export default {
         jqEl.on('hidden.bs.dropdown', () => {
             this.searchTerm = ""
         })
-    },
 
-    methods: {
-        onSearchBoxDownKey() {
+        this.fuseOptions = {
+            shouldSort: true,
+            includeMatches: true,
+            threshold: this.fuzzy ? 0.35 : 0,
+            location: 0, distance: 100,
+            maxPatternLength: 32, minMatchCharLength: 1,
+            keys: ["value"]        
+        };
+  },
+
+  methods: {
+    onSearchBoxDownKey() {
             const $firstOptionAnchor = $(this.$refs.dropdownMenu).find('a').first();
-            $firstOptionAnchor.focus();
-        },
-        onOptionUpKey(event) {
+      $firstOptionAnchor.focus();
+    },
+    onOptionUpKey(event) {
             const isFirstOption = $(event.target).parent().index() === 1;
 
-            if (isFirstOption) {
-                this.$refs.searchBox.focus();
-                event.stopPropagation();
-            }
-        },
-        fetchOptions(filter = "") {
-            if (this.values) {
-                this.options = this.values;
-                return;
-            }
+      if (isFirstOption) {
+        this.$refs.searchBox.focus();
+        event.stopPropagation();
+      }
+    },
 
-            this.isLoading = true;
-            const requestParams = Object.assign({ query: filter, cache: false }, this.ajaxParams);
+    fetchOptions(filter = "") {
+      if (this.values) {
+        
+        if(filter != ""){
+            const fuse = new Fuse(this.values, this.fuseOptions)
+            this.options = this.setOptions(fuse.search(filter), false)            
+        }
+        else {
+            this.options = this.setOptions(this.values)
+        }
+        return;
+      }
 
-            this.$http.get(this.fetchUrl, {params: requestParams})
-                .then(response => {
-                    this.options = response.data.options || [];
-                    this.isLoading = false;
-                })
-                .catch(() => this.isLoading = false)
-        },
-        clear() {
+      this.isLoading = true;
+      const requestParams = Object.assign({ query: filter, cache: false }, this.ajaxParams);
+
+      this.$http.get(this.fetchUrl, {params: requestParams})
+        .then(response => {
+            this.options = this.setOptions(response.data.options || []);
+            this.isLoading = false;
+      }).catch(() => this.isLoading = false)      
+    },
+
+    setOptions(values, wrap = true){
+        if(wrap == false) return values
+        
+        return _.map(values, v => {
+            return {
+                item: v,
+                matches: null
+            }
+        })
+    },
+
+    clear() {
             this.$emit('selected', null, this.controlId);
-            this.searchTerm = "";
-        },
-        selectOption(value) {
+      this.searchTerm = "";
+    },
+    selectOption(value) {
             this.$emit('selected', value, this.controlId);
-        },
-        updateOptionsList(e) {
-            this.fetchOptions(e.target.value);
-        },
-        highlight(title, searchTerm) {
-            var encodedTitle = _.escape(title);
-            if (searchTerm) {
-                var safeSearchTerm = _.escape(_.escapeRegExp(searchTerm));
+    },
+    updateOptionsList(e) {
+      this.fetchOptions(e.target.value);
+    },
+    highlight(option, searchTerm) {
+        if(option.matches == null){
+            const encodedTitle = _.escape(option.item.value);
 
-                var iQuery = new RegExp(safeSearchTerm, "ig");
+            if (searchTerm) {
+                const safeSearchTerm = _.escape(_.escapeRegExp(searchTerm));
+                const iQuery = new RegExp(safeSearchTerm, "ig");
+
                 return encodedTitle.replace(iQuery, (matchedTxt) => {
                     return `<strong>${matchedTxt}</strong>`;
                 });
             }
 
             return encodedTitle;
-        }
-    }
+        } else {          
+            return generateHighlightedText(option.item.value, option.matches[0].indices)
+        }      
+    }    
+  }
+}
+
+// Does not account for overlapping highlighted regions, if that exists at all O_o..
+function generateHighlightedText(text, regions, start = "<strong>", end = "</strong>") {
+  if(!regions) return text;
+
+  var content = '', nextUnhighlightedRegionStartingIndex = 0;
+
+  regions.forEach((region) => {
+    content += '' + text.substring(nextUnhighlightedRegionStartingIndex, region[0]) +
+     start +
+        text.substring(region[0], region[1] + 1) +
+     end + '';
+
+    nextUnhighlightedRegionStartingIndex = region[1] + 1;
+  });
+
+  content += text.substring(nextUnhighlightedRegionStartingIndex);
+
+  return content;
 }
 
 </script>
