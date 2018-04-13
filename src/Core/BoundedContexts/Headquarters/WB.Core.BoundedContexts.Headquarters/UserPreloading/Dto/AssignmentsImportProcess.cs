fdb719d1297f96d2f4a814d3607df56809677f4b
Roman Lytvyn <litvin.roma@gmail.com>
2018-04-13 22:07:40 +0300

    KP-4479 Refactor verification data on preloading. UI

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/UserPreloading/Dto/AssignmentsImportProcess.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/UserPreloading/Dto/AssignmentsImportProcess.cs
@@ -7,8 +7,6 @@ namespace WB.Core.BoundedContexts.Headquarters.UserPreloading.Dto
         public virtual int Id { get; set; }
         public virtual string QuestionnaireId { get; set; }
         public virtual string FileName { get; set; }
-        public virtual int AssignedToSupervisorsCount { get; set; }
-        public virtual int AssignedToInterviewersCount { get; set; }
         public virtual int TotalCount { get; set; }
         public virtual string Responsible { get; set; }
         public virtual DateTime StartedDate { get; set; }
