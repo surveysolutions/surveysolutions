7f83577f16fe3d74c975286227f9b49194504ce6
Andrii Kozhyn <hedgic@gmail.com>
2018-04-23 12:00:59 +0300

    KP-11257: Initial nuget update

--- src/UI/Interviewer/WB.UI.Interviewer/Bootstrap/JsonPluginBootstrap.cs
+++
@@ -1,9 +0,0 @@
-using MvvmCross.Platform.Plugins;
-
-namespace WB.UI.Interviewer.Bootstrap
-{
-    public class JsonPluginBootstrap
-        : MvxPluginBootstrapAction<MvvmCross.Plugins.Json.PluginLoader>
-    {
-    }
-}
