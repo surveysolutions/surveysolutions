1b17ba755a83ae6e26cf74d4d8b344f5e59cc454
Andrii Kozhyn <hedgic@gmail.com>
2018-04-23 12:00:59 +0300

    KP-11257: Initial nuget update

--- src/UI/Shared/WB.UI.Shared.Enumerator/Bootstrap/NetworkPluginBootstrap.cs
+++
@@ -1,9 +0,0 @@
-using MvvmCross.Platform.Plugins;
-
-namespace WB.UI.Shared.Enumerator.Bootstrap
-{
-    public class NetworkPluginBootstrap
-        : MvxPluginBootstrapAction<MvvmCross.Plugins.Network.PluginLoader>
-    {
-    }
-}
