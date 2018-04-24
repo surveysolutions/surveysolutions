5a64905b4c83ad0b7ea584ac346d037744f271db
Andrii Kozhyn <hedgic@gmail.com>
2018-04-24 18:33:05 +0300

    KP-11153: Deleted animations to improve stability

--- src/UI/Shared/WB.UI.Shared.Enumerator/CustomBindings/ImageButtonPlaybackToggleBinding.cs
+++ src/UI/Shared/WB.UI.Shared.Enumerator/CustomBindings/ImageButtonPlaybackToggleBinding.cs
@@ -1,9 +1,5 @@
-﻿using Android.Graphics.Drawables;
-using Android.OS;
-using Android.Support.Graphics.Drawable;
-using Android.Widget;
+﻿using Android.Widget;
 using MvvmCross.Binding;
-using MvvmCross.Platform;
 
 namespace WB.UI.Shared.Enumerator.CustomBindings
 {
@@ -17,28 +13,9 @@ namespace WB.UI.Shared.Enumerator.CustomBindings
 
         protected override void SetValueToView(ImageButton control, bool value)
         {
-            AnimatedVectorDrawableCompat.RegisterAnimationCallback(control.Drawable, new AnimationCallbackCompat(Target, value));
-            var animatable = control.Drawable as IAnimatable;
-            animatable.Start();
-        }
-
-        class AnimationCallbackCompat : Animatable2CompatAnimationCallback
-        {
-            private readonly ImageButton btn;
-            private readonly bool isPlaying;
-
-            public AnimationCallbackCompat(ImageButton btn, bool isPlaying)
-            {
-                this.btn = btn;
-                this.isPlaying = isPlaying;
-            }
-
-            public override void OnAnimationEnd(Drawable drawable)
-            {
-                btn.SetImageResource(isPlaying
-                    ? Resource.Drawable.stop_to_play_animation
-                    : Resource.Drawable.play_to_stop_animation);
-            }
+            control.SetImageResource(value
+                ? Resource.Drawable.stop_icon
+                : Resource.Drawable.play_icon);
         }
     }
 }
