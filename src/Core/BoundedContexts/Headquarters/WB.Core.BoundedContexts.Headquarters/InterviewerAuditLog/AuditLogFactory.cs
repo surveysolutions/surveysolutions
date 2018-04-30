71d1dcfa837909ef0e3c61ccc99114f5300a4c69
Pavel Stovpivskyi <sby@rambler.ru>
2018-04-30 16:49:47 +0300

    KP-11165 Changed request to show 7 existed days

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/InterviewerAuditLog/AuditLogFactory.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/InterviewerAuditLog/AuditLogFactory.cs
@@ -19,6 +19,30 @@ namespace WB.Core.BoundedContexts.Headquarters.InterviewerAuditLog
             return GetRecordsImpl(responsibleId, startDate: startDateTime, endDate: endDateTime);
         }
 
+        public AuditLogResult GetLastExisted7DaysRecords(Guid responsibleId, DateTime dateTime)
+        {
+            var last8Days = storageAccessor.Query(_ => _.Where(r => r.ResponsibleId == responsibleId && r.TimeUtc.Date <= dateTime)
+                .Select(r => r.TimeUtc.Date)
+                .GroupBy(r => r.Date)
+                .Select(r => r.Key)
+                .OrderByDescending(r => r)
+                .Take(8))
+                .ToList();
+
+            var last7Days = last8Days.Take(7).ToList();
+
+            var records = storageAccessor.Query(_ =>
+                _.Where(r => r.ResponsibleId == responsibleId && last7Days.Contains(r.TimeUtc.Date))
+                    .OrderByDescending(r => r.RecordId)
+            ).ToList();
+
+            return new AuditLogResult()
+            {
+                Records = records,
+                NextBatchRecordDate = last8Days.Count == 8 ? last8Days[7] : (DateTime?)null
+            };
+        }
+
         public IEnumerable<AuditLogRecord> GetRecordsFor7Days(Guid responsibleId)
         {
             var sevenDaysAgo = DateTime.UtcNow.Date.AddDays(-7);
