12fdebda5852b4b5e7a82599f53109bf432fcffc
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2018-05-01 14:25:08 -0400

    KP-11130 Allow to use area question in conditions [review]

--- src/Core/SharedKernels/DataCollection/DataCollection.Portable/Georgaphy.cs
+++ src/Core/SharedKernels/DataCollection/DataCollection.Portable/Georgaphy.cs
@@ -2,13 +2,6 @@
 {
     public class Georgaphy
     {
-        /*public Georgaphy(double area, double length, int pointsCount)
-        {
-            this.Area = area;
-            this.Length = length;
-            this.PointsCount = pointsCount;
-        }*/
-
         public double Area { get; set; }
         public double Length { get; set; }
         public int PointsCount { get; set; }
