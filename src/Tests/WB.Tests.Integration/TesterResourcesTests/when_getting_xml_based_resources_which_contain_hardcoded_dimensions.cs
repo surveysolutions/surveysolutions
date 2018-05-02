bcb83368d02e7e06c6b638fa0e0d8472a58f2c39
Andrii Kozhyn <hedgic@gmail.com>
2018-05-02 16:17:45 +0300

    KP-11148: Replaced sprites with appropriate

--- src/Tests/WB.Tests.Integration/TesterResourcesTests/when_getting_xml_based_resources_which_contain_hardcoded_dimensions.cs
+++ src/Tests/WB.Tests.Integration/TesterResourcesTests/when_getting_xml_based_resources_which_contain_hardcoded_dimensions.cs
@@ -6,13 +6,14 @@ namespace WB.Tests.Integration.TesterResourcesTests
 {
     internal class when_getting_xml_based_resources_which_contain_hardcoded_dimensions : TesterResourcesTestsContext
     {
-        [OneTimeSetUp]
-        public void Because() =>
+        [Test]
+        public void should_return_only_dimensions_xmls_from_values_folders()
+        {
             resources = GetXmlResourcesHavingHardcodedDimensions("UI/Tester/WB.UI.Tester/Resources").ToArray();
 
-        [Test]
-        public void should_return_only_dimensions_xmls_from_values_folders() =>
-            resources.Should().OnlyContain(resource => resource.ToLower().StartsWith("values") && resource.ToLower().Contains("dimensions"));
+            resources.Should().OnlyContain(resource =>
+                resource.ToLower().StartsWith("values") && resource.ToLower().Contains("dimensions"));
+        }
 
         private static string[] resources;
     }
