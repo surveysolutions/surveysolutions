b8e14c91901d2ea65884510782318f2601b677cc
Roman Lytvyn <litvin.roma@gmail.com>
2018-05-04 15:07:03 +0300

    KP-11303 Not informative message in case of preloading cascading option of other parent

--- src/Tests/WB.Tests.Unit/BoundedContexts/Headquarters/Assignments/ImportDataVerifierTests.InterviewTree.cs
+++ src/Tests/WB.Tests.Unit/BoundedContexts/Headquarters/Assignments/ImportDataVerifierTests.InterviewTree.cs
@@ -1,6 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using Moq;
 using NUnit.Framework;
+using WB.Core.GenericSubdomains.Portable;
 using WB.Core.SharedKernels.DataCollection.Implementation.Aggregates.InterviewEntities.Answers;
+using WB.Core.SharedKernels.DataCollection.Implementation.Entities;
+using WB.Core.SharedKernels.DataCollection.Repositories;
+using WB.Enumerator.Native.Questionnaire.Impl;
 using WB.Tests.Abc;
 
 namespace WB.Tests.Unit.BoundedContexts.Headquarters.Assignments
@@ -21,6 +27,50 @@ namespace WB.Tests.Unit.BoundedContexts.Headquarters.Assignments
             var verificationError = verifier.VerifyWithInterviewTree(answers, null, questionnaireDocument);
 
             Assert.That(verificationError, Is.Not.Null);
+        }
+
+        [Test]
+        public void when_verify_answers_on_interview_tree_and_answer_on_cascading_question_but_no_answer_on_parent_question_should_report_specified_error()
+        {
+            // arrange
+            var parentCascadingQuestionId = Guid.Parse("11111111111111111111111111111111");
+            var cascadingQuestionId = Guid.Parse("22222222222222222222222222222222");
+
+            var answerOnParentQuestion = 99999m;
+            var answer = 9999m;
+            var expectedParentValue = 0m;
+
+            var questionnaire = Create.Entity.PlainQuestionnaire(
+                Create.Entity.QuestionnaireDocumentWithOneChapter(
+                    Create.Entity.SingleOptionQuestion(parentCascadingQuestionId, answerCodes: new[] { answerOnParentQuestion, answer }),
+                    Create.Entity.SingleOptionQuestion(cascadingQuestionId,
+                        cascadeFromQuestionId: parentCascadingQuestionId, answerCodes: new[] { answerOnParentQuestion, answer },
+                        parentCodes: new[] { 1m, expectedParentValue })));
+
+            var questionnaireRepository = Mock.Of<IQuestionnaireStorage>(repository
+                => repository.GetQuestionnaire(It.IsAny<QuestionnaireIdentity>(), It.IsAny<string>()) == questionnaire);
+
+            Setup.InstanceToMockedServiceLocator(questionnaireRepository);
+            Setup.InstanceToMockedServiceLocator<IQuestionOptionsRepository>(new QuestionnaireQuestionOptionsRepository(questionnaireRepository));
+
+            var verifier = Create.Service.ImportDataVerifier(interviewTreeBuilder: Create.Service.InterviewTreeBuilder());
+
+            var answers = new List<InterviewAnswer>
+            {
+                Create.Entity.InterviewAnswer(Create.Identity(parentCascadingQuestionId), Create.Entity.SingleOptionAnswer((int)answerOnParentQuestion)),
+                Create.Entity.InterviewAnswer(Create.Identity(cascadingQuestionId), Create.Entity.SingleOptionAnswer((int)answer))
+            };
+
+            // act
+            var verificationError = verifier.VerifyWithInterviewTree(answers, null, questionnaire);
+
+            // assert
+            Assert.That(verificationError.ErrorMessage, Does.Contain("PL0011"));
+            Assert.That(verificationError.ErrorMessage, Does.Contain($"Error during import of assignment with answers: {answerOnParentQuestion}, {answer}."));
+            Assert.That(verificationError.ErrorMessage, Does.Contain("Exception: Selected value do not correspond to the parent answer selected value"));
+            Assert.That(verificationError.ErrorMessage, Does.Contain($"Question ID: {cascadingQuestionId.FormatGuid()}"));
+            Assert.That(verificationError.ErrorMessage, Does.Contain($"ProvidedAnswer: {answer}"));
+            Assert.That(verificationError.ErrorMessage, Does.Contain($"ParentValue: {expectedParentValue}"));
         }
     }
 }
