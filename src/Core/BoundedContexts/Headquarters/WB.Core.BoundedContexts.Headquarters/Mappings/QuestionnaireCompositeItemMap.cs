5ecf9dd30776a5a1f5a0c6c095245c52a4b37660
Victor Hladkikh <me@alfeg.net>
2018-05-07 16:47:18 +0300

    Added support for generic entity key in ReadSide interfaces

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Mappings/QuestionnaireCompositeItemMap.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Mappings/QuestionnaireCompositeItemMap.cs
@@ -1,17 +1,14 @@
 ﻿using NHibernate.Mapping.ByCode;
 using NHibernate.Mapping.ByCode.Conformist;
 using WB.Core.BoundedContexts.Headquarters.Views.Questionnaire;
-using WB.Core.Infrastructure.PlainStorage;
 
 namespace WB.Core.BoundedContexts.Headquarters.Mappings
 {
-    [PlainStorage]
     public class QuestionnaireCompositeItemMap : ClassMapping<QuestionnaireCompositeItem>
     {
         public QuestionnaireCompositeItemMap()
         {
             Table("questionnaire_entities");
-            Schema("readside");
             Id(x => x.Id, map => map.Generator(Generators.Identity));
 
             Property(x => x.EntityId);
