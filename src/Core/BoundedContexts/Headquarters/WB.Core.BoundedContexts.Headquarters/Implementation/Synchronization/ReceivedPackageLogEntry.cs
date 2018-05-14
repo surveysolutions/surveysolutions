f958fbe9074a3706198071cb6a38e833fc5d6c25
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2018-05-14 12:01:17 +0300

    KP-11339: Added index on id column that is used as PK now. Track received sync packages to prevent double submit

---
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Implementation/Synchronization/ReceivedPackageLogEntry.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WB.Core.BoundedContexts.Headquarters.Implementation.Synchronization
+{
+    public class ReceivedPackageLogEntry
+    {
+        public virtual int Id { get; set; }
+
+        public virtual Guid FirstEventId { get; set; }
+        public virtual Guid LastEventId { get; set; }
+        public virtual DateTime FirstEventTimestamp { get; set; }
+        public virtual DateTime LastEventTimestamp { get; set; }
+    }
+}
