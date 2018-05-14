731bcf988c4746ddcf59c75e655e9b58d8de4fc8
Victor Hladkikh <me@alfeg.net>
2018-05-14 16:35:12 +0300

    KP-11145 New report for survey statistics # Extract questionnaire data into queriable form

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Mappings/QuestionnaireCompositeItemMap.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Mappings/QuestionnaireCompositeItemMap.cs
@@ -17,7 +17,29 @@ namespace WB.Core.BoundedContexts.Headquarters.Mappings
             Property(x => x.QuestionScope, p => p.Column("question_scope"));
             Property(x => x.QuestionType, p => p.Column("question_type"));
             Property(x => x.EntityType, p => p.Column("entity_type"));
+            Property(x => x.CascadeFromQuestionId, p => p.Column("cascade_from_question_id"));
+            Property(x => x.LinkedToQuestionId, p => p.Column("linked_to_question_id"));
+            Property(x => x.LinkedToRosterId, p => p.Column("linked_to_roster_id"));
             Property(x => x.QuestionnaireIdentity);
+            Property(x => x.StatExportCaption, p => p.Column("stata_export_caption"));
+            Property(x => x.VariableLabel, p => p.Column("variable_label"));
+            Property(x => x.QuestionText, p => p.Column("question_text"));
+
+            Bag(x => x.Answers, mapper =>
+            {
+                mapper.Lazy(CollectionLazy.Extra);
+                mapper.Table("questionnaire_entities_answers");
+                mapper.Key(k => k.Column("entity_id"));
+                mapper.Cascade(Cascade.All);
+            }, r => r.Component(c =>
+            {
+                c.Property(x => x.Text);
+                c.Property(x => x.Value);
+                c.Property(x => x.Parent);
+                c.Property(x => x.AnswerCode, p => p.Column("answer_code"));
+                c.Property(x => x.ParentCode, p => p.Column("parent_code"));
+            }));
         }
     }
+
 }
