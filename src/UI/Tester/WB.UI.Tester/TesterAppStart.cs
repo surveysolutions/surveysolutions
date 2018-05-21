82482677c335ae6ed4cae52550501ade4e758328
Andrii Kozhyn <hedgic@gmail.com>
2018-05-21 17:10:29 +0300

    KP-11257: Tester app is launching and able to open interview

---
+++ src/UI/Tester/WB.UI.Tester/TesterAppStart.cs
@@ -0,0 +1,59 @@
+﻿using MvvmCross;
+using MvvmCross.ViewModels;
+using WB.Core.BoundedContexts.Tester.ViewModels;
+using WB.Core.GenericSubdomains.Portable.Tasks;
+using WB.Core.SharedKernels.DataCollection.Repositories;
+using WB.Core.SharedKernels.Enumerator.Services;
+using WB.Core.SharedKernels.Enumerator.Services.Infrastructure;
+using WB.Core.SharedKernels.Enumerator.Services.Infrastructure.Storage;
+using WB.Core.SharedKernels.Enumerator.Views;
+
+namespace WB.UI.Tester
+{
+    public class TesterAppStart : MvxAppStart
+    {
+        public TesterAppStart(IMvxApplication application) : base(application)
+        {
+        }
+
+        protected override void Startup(object hint = null)
+        {
+            ClearAttachmentStorage();
+            ClearPlainInterviewStorage();
+
+            Mvx.Resolve<IPlainStorage<TranslationInstance>>().RemoveAll();
+
+            IPrincipal principal = Mvx.Resolve<IPrincipal>();
+            IViewModelNavigationService viewModelNavigationService = Mvx.Resolve<IViewModelNavigationService>();
+
+            if (principal.IsAuthenticated)
+            {
+                viewModelNavigationService.NavigateToDashboardAsync().ConfigureAwait(false);
+            }
+            else
+            {
+                viewModelNavigationService.NavigateToLoginAsync().ConfigureAwait(false);
+            }
+
+            base.Startup(hint);
+        }
+
+        private void ClearAttachmentStorage()
+        {
+            var attachmentContentMetadataStorage = Mvx.Resolve<IPlainStorage<AttachmentContentMetadata>>();
+            attachmentContentMetadataStorage.RemoveAll();
+
+            var attachmentContentDataStorage = Mvx.Resolve<IPlainStorage<AttachmentContentData>>();
+            attachmentContentDataStorage.RemoveAll();
+        }
+
+        private void ClearPlainInterviewStorage()
+        {
+            var imageFileStorage = Mvx.Resolve<IImageFileStorage>();
+            (imageFileStorage as IPlainFileCleaner)?.Clear();
+
+            var audioFileStorage = Mvx.Resolve<IAudioFileStorage>();
+            (audioFileStorage as IPlainFileCleaner)?.Clear();
+        }
+    }
+}
