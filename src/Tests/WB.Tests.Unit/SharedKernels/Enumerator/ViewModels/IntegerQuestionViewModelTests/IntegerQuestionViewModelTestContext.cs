a0e257b4cb8c5fa7b1d67bf0b01f6e0a488445da
Slava Scherbak <slava.scherbak@gmail.com>
2018-06-06 16:07:56 -0400

    KP-11398: fixed failed tests

--- src/Tests/WB.Tests.Unit/SharedKernels/Enumerator/ViewModels/IntegerQuestionViewModelTests/IntegerQuestionViewModelTestContext.cs
+++ src/Tests/WB.Tests.Unit/SharedKernels/Enumerator/ViewModels/IntegerQuestionViewModelTests/IntegerQuestionViewModelTestContext.cs
@@ -45,7 +45,10 @@ namespace WB.Tests.Unit.SharedKernels.Enumerator.ViewModels.IntegerQuestionViewM
                 AnsweringViewModelMock.Object,
                 Mock.Of<QuestionInstructionViewModel>(),
                 Mock.Of<ILiteEventRegistry>(),
-                specialValuesViewModel ?? Mock.Of<SpecialValuesViewModel>());
+                specialValuesViewModel ?? Mock.Of<SpecialValuesViewModel>())
+            {
+                ThrottlePeriod = 0
+            };
         }
 
         protected static void SetUp()
