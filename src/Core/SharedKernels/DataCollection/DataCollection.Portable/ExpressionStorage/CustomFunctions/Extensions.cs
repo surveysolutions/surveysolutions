28e7645beeaa2a868e9de9838bdf65036257bddd
Victor Hladkikh <me@alfeg.net>
2018-06-06 17:57:09 +0300

    KP-11462 Type incompatibility # Try to fix new clients also

--- src/Core/SharedKernels/DataCollection/DataCollection.Portable/ExpressionStorage/CustomFunctions/Extensions.cs
+++ src/Core/SharedKernels/DataCollection/DataCollection.Portable/ExpressionStorage/CustomFunctions/Extensions.cs
@@ -1,117 +1,48 @@
-﻿using System;
-using System.Linq;
-
-// ReSharper disable once CheckNamespace
+﻿// ReSharper disable once CheckNamespace
 namespace WB.Core.SharedKernels.DataCollection.ExpressionStorage.CustomFunctions
 {
+    // backward compatibility - KP-11462
     public static class Extensions
     {
-        public static bool InRange(this int value, int low, int high)
+        public static bool InRange(int value, int low, int high)
         {
-            if (value < low) return false;
-            if (value > high) return false;
-            return true;
+            return value.InRange(low, high);
         }
 
-        public static bool InRange(this int? value, int? low, int? high)
+        public static bool InRange(int? value, int? low, int? high)
         {
-			if (value == null || low == null || high == null) return false;
-            if (value < low) return false;
-            if (value > high) return false;
-            return true;
+            return value.InRange(low, high);
         }
 
-        // backward compatibility
-        public static bool InRange(this int? value, double? low, double? high)
+        public static bool InRange(int? value, double? low, double? high)
         {
-			if (value == null || low == null || high == null) return false;
-            if (value < low) return false;
-            if (value > high) return false;
-            return true;
+            return value.InRange(low, high);
         }
 
-        public static bool InList(this int? value, params int?[] valuesList)
+        public static bool InList(int? value, params int?[] valuesList)
         {
-            if (valuesList.Length == 0) return false;
-
-            for (var index = 0; index < valuesList.Length; index++)
-            {
-                var v = valuesList[index];
-                if (v == value) return true;
-            }
-
-            return false;
+            return value.InList(valuesList);
         }
 
-        public static bool InList(this int value, params int?[] valuesList) =>
-            ((int?)value).InList(valuesList);
+        public static bool InList(int value, params int?[] valuesList) =>
+            value.InList(valuesList);
 
-        public static bool ContainsAll(this int[] multichoice, params int[] valuesList)
+        public static bool ContainsAll(int[] multichoice, params int[] valuesList)
         {
-            if (multichoice == null) return false;
-            if (multichoice.Length == 0) return false;
-
-            if (valuesList == null) return true;
-            if (valuesList.Length == 0) return true;
-
-            return valuesList.All(multichoice.Contains);
+            return multichoice.ContainsAll(valuesList);
         }
 
-        // backward compatibility
-        public static bool ContainsAll(this int[] multichoice, params decimal[] valuesList)
+        public static bool ContainsAll(int[] multichoice, params decimal[] valuesList)
         {
-            return ContainsAll(multichoice, valuesList?.Select(Convert.ToInt32).ToArray() ?? new int[0]);
+            return multichoice.ContainsAll(valuesList);
         }
 
-        public static bool ContainsAny(this int[] multichoice, params int[] valuesList)
+        public static bool ContainsAny(int[] multichoice, params int[] valuesList)
         {
-            if (multichoice == null) return false;
-            if (multichoice.Length == 0) return false;
-
-            if (valuesList == null) return true;
-            if (valuesList.Length == 0) return true;
-
-            return valuesList.Any(value => multichoice.Any(multiEmelent => multiEmelent == value));
+            return multichoice.ContainsAny(valuesList);
         }
 
-        public static bool ContainsOnly(this int[] multichoice, params int[] valuesList) => 
-            multichoice?.Length == valuesList.Length && valuesList.All(multichoice.Contains);
-
-        /// <summary>
-        /// For a single choice question checks that the selection is not mentioned in the specified list of values.
-        /// </summary>
-        /// <param name="singlechoice">single choice variable.</param>
-        /// <param name="valuesList">list of values (blacklist).</param>
-        /// <returns>True if the selection is not mentioned in the specified list of values.</returns>
-        public static bool IsNoneOf(this int? singlechoice, params int[] valuesList)
-        {
-            if (valuesList == null) return true;
-            if (valuesList.Length == 0) return true;
-            if (singlechoice.HasValue == false) return true;
-            return !valuesList.ContainsAny(singlechoice.Value);
-        }
-
-        #region March 2016 functions
-
-        public static bool ContainsAnyOtherThan(this int[] multichoice, params int[] valuesList)
-        {
-            if (multichoice == null) return false;
-            if (multichoice.Length == 0) return false;
-
-            if (valuesList == null) return true;
-            if (valuesList.Length == 0) return true;
-
-            for (var j = 0; j < multichoice.Length; j++)
-            {
-                var other = true;
-                for (var i = 0; i < valuesList.Length; i++)
-                    if (multichoice[j] == valuesList[i]) other = false;
-                if (other) return true;
-            }
-
-            return false;
-        }
-
-        #endregion
+        public static bool ContainsOnly(int[] multichoice, params int[] valuesList) =>
+            multichoice.ContainsOnly(valuesList);
     }
 }
