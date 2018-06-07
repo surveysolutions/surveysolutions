12e83a8992422b65381fe3e9fd1fb64275cb42d6
Victor Gladkikh <me@alfeg.net>
2018-06-07 15:27:12 +0300

    Fixed broken packages count collection. Use only DB as source of data. Also collect interviews data

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Implementation/Synchronization/BrokenPackagesStatsCollector.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Implementation/Synchronization/BrokenPackagesStatsCollector.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Diagnostics;
 using System.Linq;
+using Dapper;
 using NHibernate;
 using WB.Core.BoundedContexts.Headquarters.Views;
 using WB.Infrastructure.Native.Monitoring;
@@ -11,7 +12,7 @@ namespace WB.Core.BoundedContexts.Headquarters.Implementation.Synchronization
     {
         private readonly ISessionFactory sessionFactory;
         private readonly Stopwatch throttle = new Stopwatch();
-        
+
         public BrokenPackagesStatsCollector(ISessionFactory sessionFactory)
         {
             this.sessionFactory = sessionFactory;
@@ -27,23 +28,46 @@ namespace WB.Core.BoundedContexts.Headquarters.Implementation.Synchronization
         {
             lock (throttle)
             {
-                if (throttle.Elapsed <= TimeSpan.FromMinutes(1)) return;
+                if (throttle.Elapsed <= TimeSpan.FromSeconds(30)) return;
 
                 using (var session = sessionFactory.OpenStatelessSession())
                 {
                     var packages = from bip in session.Query<BrokenInterviewPackage>()
-                        group bip by bip.ExceptionType
-                        into g
-                        select new {Type = g.Key, Count = g.Count()};
+                       group bip by bip.ExceptionType into g
+                       select new { Type = g.Key, Count = g.Count() };
 
                     foreach (var package in packages.ToList())
                     {
-                        CommonMetrics.BrokenPackagesCount.Labels(package.Type).Set(package.Count);
+                        BrokenPackagesCount.Labels(package.Type).Set(package.Count);
+                    }
+
+                    var sizesData = session.Connection.Query<(string name, long rows, long size)>(
+                        @"SELECT relname AS table_name, c.reltuples AS row_estimate, 
+                            pg_total_relation_size(c.oid) AS total_bytes									
+                          FROM pg_class c
+                          where relname in ('events', 'interviews', 'interviewsummaries');");
+
+                    foreach (var data in sizesData)
+                    {
+                        DatabaseTableRowsCount.Labels(data.name).Set(data.rows);
+                        DatabaseTableSize.Labels(data.name).Set(data.size);
                     }
                 }
 
-            throttle.Restart();
+                throttle.Restart();
             }
         }
+        
+        private static readonly Gauge BrokenPackagesCount = new Gauge(
+            "wb_broken_packages_count",
+            "Amount of broken packages on server", "type");
+
+        private static readonly Gauge DatabaseTableRowsCount = new Gauge(
+            "wb_table_estimated_rows_count",
+            "Amount of rows in table", "table");
+
+        private static readonly Gauge DatabaseTableSize = new Gauge(
+            "wb_table_estimated_size_bytes",
+            "Size of the table in bytes", "table");
     }
 }
