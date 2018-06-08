22e182d1317436afbf367a1e0a4e1437185081b9
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2018-06-08 17:16:56 -0400

    KP-11459 Use QR codes for Interviewer app setup

--- src/UI/Headquarters/WB.UI.Headquarters/Injections/HeadquartersUIModule.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/Injections/HeadquartersUIModule.cs
@@ -98,6 +98,8 @@ namespace WB.UI.Headquarters.Injections
             registry.Bind<IInterviewerProfileFactory, InterviewerProfileFactory>();
             registry.Bind<ITranslationsExportService, TranslationsExportService>();
             registry.Bind<IQuestionnaireExporter, QuestionnaireExporter>();
+
+            registry.Bind<IQRCodeHelper, QRCodeHelper>();
         }
 
         public Task Init(IServiceLocator serviceLocator)
