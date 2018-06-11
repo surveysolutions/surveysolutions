619290ffd9533cc263a8f94f5bd41eb93b1605e2
Andrii Kozhyn <hedgic@gmail.com>
2018-06-11 12:39:36 +0300

    KP-11455: Initial version of overview screen

--- src/UI/Shared/WB.UI.Shared.Enumerator/Activities/CoverInterviewFragment.cs
+++ src/UI/Shared/WB.UI.Shared.Enumerator/Activities/CoverInterviewFragment.cs
@@ -1,9 +1,33 @@
+using Android.OS;
+using Android.Support.V7.Widget;
+using Android.Views;
+using MvvmCross.Droid.Support.V4;
+using MvvmCross.Droid.Support.V7.RecyclerView;
+using MvvmCross.Platforms.Android.Binding.BindingContext;
 using WB.Core.SharedKernels.Enumerator.ViewModels.InterviewDetails;
+using WB.Core.SharedKernels.Enumerator.ViewModels.InterviewDetails.Overview;
 
 namespace WB.UI.Shared.Enumerator.Activities
 {
     public class CoverInterviewFragment : BaseFragment<CoverInterviewViewModel>
     {
         protected override int ViewResourceId => Resource.Layout.interview_cover_screen;
+    }
+
+    public class OverviewFragment : BaseFragment<OverviewViewModel>
+    {
+        private MvxRecyclerView recyclerView;
+
+        protected override int ViewResourceId => Resource.Layout.interview_overview;
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            this.EnsureBindingContextIsSet(inflater);
+            var view = this.BindingInflate(ViewResourceId, container, false);
+            this.recyclerView = view.FindViewById<MvxRecyclerView>(Resource.Id.overview_recycler);
+
+            this.recyclerView.SetLayoutManager(new LinearLayoutManager(this.Context));
+            return view;
+        }
     }
 }
---
+++ src/UI/Shared/WB.UI.Shared.Enumerator/CustomControls/InterviewOverviewTemplateSelector.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using MvvmCross.Droid.Support.V7.RecyclerView.ItemTemplates;
+using WB.Core.SharedKernels.Enumerator.ViewModels.InterviewDetails;
+using WB.Core.SharedKernels.Enumerator.ViewModels.InterviewDetails.Overview;
+
+namespace WB.UI.Shared.Enumerator.CustomControls
+{
+    public class InterviewOverviewTemplateSelector : IMvxTemplateSelector
+    {
+        private readonly Dictionary<Type, int> typeMapping;
+
+        public InterviewOverviewTemplateSelector()
+        {
+            typeMapping = new Dictionary<Type, int>
+            {
+                {typeof(OverviewQuestion), Resource.Layout.interview_overview_question},
+                {typeof(OverviewGroup), Resource.Layout.interview_overview_group},
+                {typeof(OverviewStaticText), Resource.Layout.interview_overview_statictext}
+            };
+        }
+
+        public int GetItemViewType(object forItemObject)
+        {
+            if (typeMapping.TryGetValue(forItemObject.GetType(), out int result))
+            {
+                return result;
+            }
+
+            throw new NotSupportedException($"Display of entity of type {forItemObject.GetType()} not supported");
+        }
+
+        public int GetItemLayoutId(int fromViewType)
+        {
+            return fromViewType;
+        }
+
+        public int ItemTemplateId { get; set; }
+    }
+}
