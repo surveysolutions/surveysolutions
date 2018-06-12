acb8a4d038aac036130f11044e32d2b1f2da0ed6
Roman Lytvyn <litvin.roma@gmail.com>
2018-06-12 19:00:56 +0300

    KP-11489 Preloading bug

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/AssignmentImport/AssignmentsImportService.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/AssignmentImport/AssignmentsImportService.cs
@@ -455,7 +455,7 @@ namespace WB.Core.BoundedContexts.Headquarters.AssignmentImport
         {
             foreach (var answersByRosterInstanceId in answersGroupedByRosterInstanceId)
             foreach (var interviewAnswer in answersByRosterInstanceId)
-            foreach (var rosterLevel in levelsOfRostersByRosterSize)
+            foreach (var rosterLevel in levelsOfRostersByRosterSize.Distinct())
             {
                 if (rosterLevel > interviewAnswer.Identity.RosterVector.Length) continue;
 
