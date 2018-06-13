c4bc092a197cbfa77f32a9b035dafc21106ae8b3
Pavel Stovpivskyi <sby@rambler.ru>
2018-06-13 15:51:01 +0300

    KP-11453 Interview diagnostics file in export

---
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Implementation/Factories/InterviewDiagnosticsFactory.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using WB.Core.BoundedContexts.Headquarters.Factories;
+using WB.Core.BoundedContexts.Headquarters.Views.DataExport;
+using WB.Core.BoundedContexts.Headquarters.Views.Interview;
+using WB.Core.GenericSubdomains.Portable;
+using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
+
+namespace WB.Core.BoundedContexts.Headquarters.Implementation.Factories
+{
+    public class InterviewDiagnosticsFactory : IInterviewDiagnosticsFactory
+    {
+        private readonly IQueryableReadSideRepositoryReader<InterviewSummary> interviewSummaryReader;
+
+        public InterviewDiagnosticsFactory(IQueryableReadSideRepositoryReader<InterviewSummary> interviewSummaryReader)
+        {
+            this.interviewSummaryReader = interviewSummaryReader;
+        }
+
+        public InterviewDiagnosticsInfo GetById(Guid interviewId)
+        {
+            return GetByBatchIds(interviewId.ToEnumerable()).SingleOrDefault();
+        }
+
+        public List<InterviewDiagnosticsInfo> GetByBatchIds(IEnumerable<Guid> interviewIds)
+        {
+            return interviewSummaryReader.Query(_ =>
+            {
+                var interviews = _.Where(i => interviewIds.Contains(i.InterviewId));
+
+                return interviews.Select(i => new InterviewDiagnosticsInfo()
+                {
+                    InterviewKey = i.Key,
+                    Status = i.Status,
+                    ResponsibleId = i.ResponsibleId,
+                    ResponsibleName = i.ResponsibleName,
+                    NumberOfInterviewers = i.InterviewCommentedStatuses.Select(s => s.InterviewerId).Distinct().Count(),
+                    NumberRejectionsBySupervisor = i.InterviewCommentedStatuses.Where(s => s.Status == InterviewExportedAction.RejectedBySupervisor).Count(),
+                    NumberRejectionsByHq = i.InterviewCommentedStatuses.Where(s => s.Status == InterviewExportedAction.RejectedByHeadquarter).Count(),
+                    NumberValidQuetions = 0,
+                    NumberInvalidQuetions = i.ErrorsCount,
+                    NumberUnansweredQuetions = 0,
+                    NumberCommentedQuetions = i.CommentedEntitiesCount,
+                    InterviewDuration = i.InterviewDurationLong,
+                });
+            }).ToList();
+        }
+    }
+}
