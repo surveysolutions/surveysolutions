5f167ec32cd9e0e71819be2ba77286b82eee7291
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2018-06-13 15:12:27 -0400

    KP-11419 Add timezone column to the paradata export [review]

--- src/Core/SharedKernels/DataCollection/DataCollection/Events/Interview/AnswersDeclaredImplausible.cs
+++ src/Core/SharedKernels/DataCollection/DataCollection/Events/Interview/AnswersDeclaredImplausible.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Collections.ObjectModel;
+//using System.Collections.ObjectModel;
 using WB.Core.GenericSubdomains.Portable;
 using WB.Core.SharedKernels.DataCollection.Events.Interview.Base;
 
@@ -15,8 +18,8 @@ namespace WB.Core.SharedKernels.DataCollection.Events.Interview
             => this.failedValidationConditionsDictionary ?? (
                    this.failedValidationConditionsDictionary =
                        this.FailedValidationConditions != null ?
-                       this.FailedValidationConditions.ToDictionary() :
-                           new List<KeyValuePair<Identity, IReadOnlyList<FailedValidationCondition>>>().ToDictionary());
+                       (IReadOnlyDictionary<Identity, IReadOnlyList<FailedValidationCondition>>) this.FailedValidationConditions.ToDictionary() :
+                        new ReadOnlyDictionary<Identity, IReadOnlyList<FailedValidationCondition>>(new Dictionary<Identity, IReadOnlyList<FailedValidationCondition>>()));
 
         public AnswersDeclaredImplausible(List<KeyValuePair<Identity, IReadOnlyList<FailedValidationCondition>>> failedValidationConditions, 
             DateTimeOffset originDate) : base (originDate)
