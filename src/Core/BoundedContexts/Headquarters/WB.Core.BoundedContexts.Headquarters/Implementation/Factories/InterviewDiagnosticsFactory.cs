fd3538c84d2f7aa74b830bedb571e4c93a31b253
Pavel Stovpivskyi <sby@rambler.ru>
2018-06-14 14:04:16 +0300

    KP-11453 fixed commented entities count

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Implementation/Factories/InterviewDiagnosticsFactory.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Implementation/Factories/InterviewDiagnosticsFactory.cs
@@ -35,7 +35,7 @@ namespace WB.Core.BoundedContexts.Headquarters.Implementation.Factories
                     Status = i.Status,
                     ResponsibleId = i.ResponsibleId,
                     ResponsibleName = i.ResponsibleName,
-                    NumberOfInterviewers = i.InterviewCommentedStatuses.Select(s => s.InterviewerId).Distinct().Count(),
+                    NumberOfInterviewers = i.InterviewCommentedStatuses.Select(s => s.InterviewerId).Where(s => s != null).Distinct().Count(),
                     NumberRejectionsBySupervisor = i.InterviewCommentedStatuses.Where(s => s.Status == InterviewExportedAction.RejectedBySupervisor).Count(),
                     NumberRejectionsByHq = i.InterviewCommentedStatuses.Where(s => s.Status == InterviewExportedAction.RejectedByHeadquarter).Count(),
                     NumberValidQuetions = 0,
