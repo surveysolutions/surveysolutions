2f0ec225dd3eb4ece221e849b2fa8fef9027b2fe
Pavel Stovpivskyi <sby@rambler.ru>
2018-06-19 19:46:08 +0300

    KP-11503 first login

---
+++ src/Core/BoundedContexts/Supervisor/WB.Core.BoundedContexts.Supervisor/Services/Implementation/SynchronizationProcess.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using WB.Core.SharedKernels.Enumerator.Services;
+
+namespace WB.Core.BoundedContexts.Supervisor.Services
+{
+    public class SynchronizationProcess : ISynchronizationProcess
+    {
+
+
+        public Task SyncronizeAsync(IProgress<SyncProgressInfo> progress, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
