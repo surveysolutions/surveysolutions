3c9562d81940116c7d51e20798a0bde75a900699
Pavel Stovpivskyi <sby@rambler.ru>
2018-06-20 15:55:00 +0300

    KP-11503 moved all common interfaces to enumerator lib

--- src/Core/BoundedContexts/Supervisor/WB.Core.BoundedContexts.Supervisor/Services/Implementation/SynchronizationProcess.cs
+++ src/Core/BoundedContexts/Supervisor/WB.Core.BoundedContexts.Supervisor/Services/Implementation/SynchronizationProcess.cs
@@ -1,17 +1,84 @@
 ﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
+using WB.Core.BoundedContexts.Interviewer.Services;
+using WB.Core.BoundedContexts.Interviewer.Services.Infrastructure;
+using WB.Core.BoundedContexts.Interviewer.Services.Synchronization;
+using WB.Core.BoundedContexts.Interviewer.Views;
+using WB.Core.BoundedContexts.Interviewer.Views.Dashboard;
+using WB.Core.BoundedContexts.Supervisor.Views;
+using WB.Core.GenericSubdomains.Portable;
+using WB.Core.GenericSubdomains.Portable.Implementation;
+using WB.Core.GenericSubdomains.Portable.Services;
+using WB.Core.Infrastructure.EventBus.Lite;
+using WB.Core.SharedKernels.DataCollection.Repositories;
 using WB.Core.SharedKernels.Enumerator.Services;
+using WB.Core.SharedKernels.Enumerator.Services.Infrastructure;
+using WB.Core.SharedKernels.Enumerator.Services.Infrastructure.Storage;
 
 namespace WB.Core.BoundedContexts.Supervisor.Services
 {
-    public class SynchronizationProcess : ISynchronizationProcess
+    public class SynchronizationProcess : SynchronizationProcessBase
     {
+        private readonly ISupervisorSettings supervisorSettings;
+        private readonly IPrincipal principal;
+        private readonly IPlainStorage<SupervisorIdentity> supervisorsPlainStorage;
+        private readonly IPasswordHasher passwordHasher;
 
-
-        public Task SyncronizeAsync(IProgress<SyncProgressInfo> progress, CancellationToken cancellationToken)
+        public SynchronizationProcess(ISynchronizationService synchronizationService,
+            IPlainStorage<SupervisorIdentity> supervisorsPlainStorage,
+            IPlainStorage<InterviewView> interviewViewRepository,
+            IPrincipal principal,
+            ILogger logger,
+            IUserInteractionService userInteractionService,
+            IInterviewerQuestionnaireAccessor questionnairesAccessor,
+            IInterviewerInterviewAccessor interviewFactory,
+            IPlainStorage<InterviewMultimediaView> interviewMultimediaViewStorage,
+            IPlainStorage<InterviewFileView> imagesStorage,
+            CompanyLogoSynchronizer logoSynchronizer,
+            AttachmentsCleanupService cleanupService,
+            IPasswordHasher passwordHasher,
+            IAssignmentsSynchronizer assignmentsSynchronizer,
+            IQuestionnaireDownloader questionnaireDownloader,
+            IHttpStatistician httpStatistician,
+            IAssignmentDocumentsStorage assignmentsStorage,
+            IAudioFileStorage audioFileStorage,
+            ITabletDiagnosticService diagnosticService,
+            ISupervisorSettings supervisorSettings,
+            IAuditLogSynchronizer auditLogSynchronizer,
+            IAuditLogService auditLogService,
+            ILiteEventBus eventBus,
+            IEnumeratorEventStorage eventStore) : base(synchronizationService, interviewViewRepository, principal, logger,
+            userInteractionService, questionnairesAccessor, interviewFactory, interviewMultimediaViewStorage, imagesStorage,
+            logoSynchronizer, cleanupService, passwordHasher, assignmentsSynchronizer, questionnaireDownloader, httpStatistician,
+            assignmentsStorage, audioFileStorage, diagnosticService, auditLogSynchronizer, auditLogService,
+            eventBus, eventStore)
         {
-            throw new NotImplementedException();
+            this.principal = principal;
+            this.supervisorSettings = supervisorSettings;
+            this.supervisorsPlainStorage = supervisorsPlainStorage;
+            this.passwordHasher = passwordHasher;
+        }
+
+
+        protected override void CheckAfterStartSynchronization(CancellationToken cancellationToken)
+        {
+
+        }
+
+        protected override void UpdatePasswordOfResponsible(RestCredentials credentials)
+        {
+            var localInterviewer = this.supervisorsPlainStorage.FirstOrDefault();
+            localInterviewer.PasswordHash = this.passwordHasher.Hash(credentials.Password);
+            localInterviewer.Token = credentials.Token;
+
+            this.supervisorsPlainStorage.Store(localInterviewer);
+            this.principal.SignIn(localInterviewer.Name, credentials.Password, true);
+        }
+
+        protected override int GetApplicationVersionCode()
+        {
+            return supervisorSettings.GetApplicationVersionCode();
         }
     }
 }
