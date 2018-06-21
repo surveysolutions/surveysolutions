b63620b48f1d4d6fb546dada1a8e223db9b8f48e
Pavel Stovpivskyi <sby@rambler.ru>
2018-06-21 15:39:15 +0300

    KP-11503 created specific WebApiConfig for each version of api

---
+++ src/UI/Headquarters/WB.UI.Headquarters/API/DataCollection/Interviewer/InterviewerCommonWebApiConfig.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Web.Http;
+using WB.Core.SharedKernels.DataCollection.WebApi;
+using WB.Core.SharedKernels.SurveyManagement.Web.Filters;
+using WB.UI.Headquarters.API.DataCollection.Interviewer.v3;
+
+namespace WB.UI.Headquarters.API.DataCollection.Interviewer
+{
+    [Localizable(false)]
+    public class InterviewerCommonWebApiConfig
+    {
+#pragma warning disable 4014
+
+        public static void Register(HttpConfiguration config)
+        {
+            config.TypedRoute(@"api/interviewer", c => c.Action<InterviewerApiController>(x => x.Get()));
+            config.TypedRoute(@"api/interviewer/patch/{deviceVersion}", c => c.Action<InterviewerApiController>(x => x.Patch(Param.Any<int>())));
+            config.TypedRoute(@"api/interviewer/latestversion", c => c.Action<InterviewerApiController>(x => x.GetLatestVersion()));
+
+            config.TypedRoute(@"api/interviewer/extended", c => c.Action<InterviewerApiController>(x => x.GetExtended()));
+            config.TypedRoute(@"api/interviewer/extended/patch/{deviceVersion}", c => c.Action<InterviewerApiController>(x => x.PatchExtended(Param.Any<int>())));
+            config.TypedRoute(@"api/interviewer/extended/latestversion", c => c.Action<InterviewerApiController>(x => x.GetLatestExtendedVersion()));
+
+            config.TypedRoute(@"api/interviewer/tabletInfo", c => c.Action<InterviewerApiController>(x => x.PostTabletInformation()));
+            config.TypedRoute(@"api/interviewer/compatibility/{deviceid}/{deviceSyncProtocolVersion}",
+                c => c.Action<InterviewerApiController>(x => x.CheckCompatibility(Param.Any<string>(), Param.Any<int>())));
+        }
+
+#pragma warning restore 4014
+
+    }
+
+}
---
+++ src/UI/Headquarters/WB.UI.Headquarters/API/DataCollection/Interviewer/InterviewerV3WebApiConfig.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Threading;
+using System.Web.Http;
+using WB.Core.SharedKernel.Structures.TabletInformation;
+using WB.Core.SharedKernels.DataCollection.WebApi;
+using WB.Core.SharedKernels.SurveyManagement.Web.Filters;
+using WB.UI.Headquarters.API.DataCollection.Interviewer.v2;
+using WB.UI.Headquarters.API.DataCollection.Interviewer.v3;
+
+namespace WB.UI.Headquarters.API.DataCollection.Interviewer
+{
+    [Localizable(false)]
+    public class InterviewerV3WebApiConfig
+    {
+#pragma warning disable 4014
+
+        public static void Register(HttpConfiguration config)
+        {
+            config.TypedRoute("api/interviewer/v3/interviews/{id:guid}",
+                c => c.Action<InterviewsApiV3Controller>(x => x.DetailsV3(Param.Any<Guid>())));
+
+            config.TypedRoute("api/interviewer/v3/interviews/{id:guid}",
+                c => c.Action<InterviewsApiV3Controller>(x => x.PostV3(Param.Any<InterviewPackageApiView>())));
+
+            config.TypedRoute("api/interviewer/v3/interviews/CheckObsoleteInterviews",
+                c => c.Action<InterviewsApiV3Controller>(x => x.CheckObsoleteInterviews(Param.Any<List<ObsoletePackageCheck>>())));
+        }
+
+#pragma warning restore 4014
+
+    }
+}
