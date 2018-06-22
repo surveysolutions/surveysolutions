e4a97044555131d5a170d78ff125be2347fc692c
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2018-06-22 18:11:33 -0400

    fix deserialization issue

--- src/Tests/WB.Tests.Unit/SharedKernels/DataCollection/InterviewTests/Synchronization/when_synchronizing_interview_events_but_interview_was_reassigned_to_supervisor_in_other_team.cs
+++ src/Tests/WB.Tests.Unit/SharedKernels/DataCollection/InterviewTests/Synchronization/when_synchronizing_interview_events_but_interview_was_reassigned_to_supervisor_in_other_team.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using FluentAssertions;
 using Moq;
 using Ncqrs.Spec;
@@ -63,7 +65,13 @@ namespace WB.Tests.Unit.SharedKernels.DataCollection.InterviewTests
 
         static InterviewException exception;
 
-        static IEvent[] eventsToPublish = new IEvent[] { new AnswersDeclaredInvalid(new Identity[0], DateTimeOffset.Now), new GroupsEnabled(new Identity[0], DateTimeOffset.Now) };
+        static IEvent[] eventsToPublish = new IEvent[]
+        {
+            new AnswersDeclaredInvalid(
+                new Identity[0].ToDictionary<Identity, Identity, IReadOnlyList<FailedValidationCondition>>(question => question, question => new List<FailedValidationCondition>()), 
+                DateTimeOffset.Now),
+            new GroupsEnabled(new Identity[0], DateTimeOffset.Now)
+        };
         static SynchronizeInterviewEventsCommand command;
     }
 }
