c9ebfd898291ae39009a7af7a9497d3d1228facd
Pavel Stovpivskyi <sby@rambler.ru>
2018-06-22 18:45:19 +0300

    KP-11503 Refactored same controllers

--- src/UI/Headquarters/WB.UI.Headquarters/API/DataCollection/Supervisor/v1/SupervisorApiController.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/API/DataCollection/Supervisor/v1/SupervisorApiController.cs
@@ -10,7 +10,10 @@ using Microsoft.AspNet.Identity;
 using WB.Core.BoundedContexts.Headquarters.OwinSecurity;
 using WB.Core.BoundedContexts.Headquarters.Services;
 using WB.Core.BoundedContexts.Headquarters.Views.User;
+using WB.Core.BoundedContexts.Headquarters.Views.SynchronizationLog;
 using WB.Core.Infrastructure.FileSystem;
+using WB.Core.Infrastructure.Versions;
+using WB.Core.SharedKernels.DataCollection;
 using WB.UI.Headquarters.Code;
 using WB.UI.Shared.Web.Filters;
 
@@ -21,17 +24,21 @@ namespace WB.UI.Headquarters.API.DataCollection.Supervisor.v1
         private const string RESPONSEAPPLICATIONFILENAME = "supervisor.apk";
         private const string PHYSICALAPPLICATIONFILENAME = "supervisor.apk";
         private const string PHYSICALPATHTOAPPLICATION = "~/Client/";
+
         private readonly IFileSystemAccessor fileSystemAccessor;
         private readonly IAndroidPackageReader androidPackageReader;
-
         private readonly HqSignInManager signInManager;
         protected readonly IUserViewFactory userViewFactory;
         protected readonly ITabletInformationService tabletInformationService;
+        private readonly ISupervisorSyncProtocolVersionProvider syncVersionProvider;
+        private readonly IProductVersion productVersion;
 
         public SupervisorAppApiController(
             IFileSystemAccessor fileSystemAccessor,
             IAndroidPackageReader androidPackageReader, 
             ITabletInformationService tabletInformationService, 
+            ISupervisorSyncProtocolVersionProvider syncVersionProvider,
+            IProductVersion productVersion,
             IUserViewFactory userViewFactory, 
             HqSignInManager signInManager)
         {
@@ -39,6 +46,8 @@ namespace WB.UI.Headquarters.API.DataCollection.Supervisor.v1
             this.androidPackageReader = androidPackageReader;
             this.tabletInformationService = tabletInformationService;
             this.userViewFactory = userViewFactory;
+            this.syncVersionProvider = syncVersionProvider;
+            this.productVersion = productVersion;
             this.signInManager = signInManager;
         }
 
@@ -55,11 +64,32 @@ namespace WB.UI.Headquarters.API.DataCollection.Supervisor.v1
         }
 
         [ApiBasicAuth(UserRoles.Supervisor)]
+        [WriteToSyncLog(SynchronizationLogType.CanSynchronize)]
         [HttpGet]
         [ApiNoCache]
         public virtual HttpResponseMessage CheckCompatibility(string deviceId, int deviceSyncProtocolVersion)
         {
-            return this.Request.CreateResponse(HttpStatusCode.OK, @"158329303");
+            int serverSyncProtocolVersion = this.syncVersionProvider.GetProtocolVersion();
+            int lastNonUpdatableSyncProtocolVersion = this.syncVersionProvider.GetLastNonUpdatableVersion();
+            if (deviceSyncProtocolVersion < lastNonUpdatableSyncProtocolVersion)
+                return this.Request.CreateResponse(HttpStatusCode.UpgradeRequired);
+
+            var currentVersion = new Version(this.productVersion.ToString().Split(' ')[0]);
+            var supervisorVersion = GetSupervisorVersionFromUserAgent(this.Request);
+
+            if (supervisorVersion != null && supervisorVersion > currentVersion)
+            {
+                return this.Request.CreateResponse(HttpStatusCode.NotAcceptable);
+            }
+
+            if (deviceSyncProtocolVersion != serverSyncProtocolVersion)
+            {
+                return this.Request.CreateResponse(HttpStatusCode.NotAcceptable);
+            }
+
+            return this.authorizedUser.DeviceId != deviceId
+                ? this.Request.CreateResponse(HttpStatusCode.Forbidden)
+                : this.Request.CreateResponse(HttpStatusCode.OK, @"158329303");
         }
 
         private Version GetSupervisorVersionFromUserAgent(HttpRequestMessage request)
