bc3d473441de19df246872fcdd58e8b4ce78b053
Slava Scherbak <slava.scherbak@gmail.com>
2018-06-28 16:54:04 -0400

    KP-11532: added list of fake users in the popup

--- src/Core/BoundedContexts/Supervisor/WB.Core.BoundedContexts.Supervisor/ViewModel/Dashboard/InterviewerSelectorDialogViewModel.cs
+++ src/Core/BoundedContexts/Supervisor/WB.Core.BoundedContexts.Supervisor/ViewModel/Dashboard/InterviewerSelectorDialogViewModel.cs
@@ -2,16 +2,36 @@
 using System.Windows.Input;
 using MvvmCross.Commands;
 using MvvmCross.ViewModels;
+using WB.Core.BoundedContexts.Supervisor.ViewModel.Dashboard.Services;
 
 namespace WB.Core.BoundedContexts.Supervisor.ViewModel.Dashboard
 {
     public class InterviewerSelectorDialogViewModel : MvxViewModel
     {
+        private readonly IInterviewersListAccessor interviewersAccessor;
+
         public ICommand DoneCommand => new MvxCommand(this.Done);
         public ICommand CancelCommand => new MvxCommand(this.Cancel);
 
         public event EventHandler OnDone;
         public event EventHandler OnCancel;
+
+        private MvxObservableCollection<InterviewerAssignInfo> uiItems = new MvxObservableCollection<InterviewerAssignInfo>();
+        public MvxObservableCollection<InterviewerAssignInfo> UiItems
+        {
+            get => this.uiItems;
+            protected set => this.RaiseAndSetIfChanged(ref this.uiItems, value);
+        }
+
+        public InterviewerSelectorDialogViewModel(IInterviewersListAccessor interviewersAccessor)
+        {
+            this.interviewersAccessor = interviewersAccessor;
+        }
+
+        public void Init()
+        {
+            this.UiItems.ReplaceWith(interviewersAccessor.GetInterviewers());
+        }
 
         private string title;
         public string Title
