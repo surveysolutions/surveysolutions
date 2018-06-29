21baf681c0538c80ee43dabe2e502ef21ecc7bae
Andrii Kozhyn <hedgic@gmail.com>
2018-06-29 13:14:49 +0300

    KP-11534: Interview is trying to be displayed on SV

--- src/Core/SharedKernels/Enumerator/Enumerator/ViewModels/InterviewLoading/LoadingViewModelArg.cs
+++ src/Core/SharedKernels/Enumerator/Enumerator/ViewModels/InterviewLoading/LoadingViewModelArg.cs
@@ -5,5 +5,7 @@ namespace WB.Core.SharedKernels.Enumerator.ViewModels.InterviewLoading
     public class LoadingViewModelArg
     {
         public Guid InterviewId { get; set; }
+
+        public bool ShouldReopen { get; set; }
     }
 }
