a96c0a883896b864988967eb9b15a82644053e14
Roman Lytvyn <litvin.roma@gmail.com>
2018-07-03 19:43:38 +0300

    KP-11556 Dashboard: block user while sync is in progress

---
+++ src/UI/Supervisor/WB.UI.Supervisor/MvvmBindings/DrawerLockModeBinding.cs
@@ -0,0 +1,15 @@
+﻿using Android.Support.V4.Widget;
+using WB.UI.Shared.Enumerator.CustomBindings;
+
+namespace WB.UI.Supervisor.MvvmBindings
+{
+    public class DrawerLockModeBinding : BaseBinding<DrawerLayout, bool>
+    {
+        public DrawerLockModeBinding(DrawerLayout androidControl) : base(androidControl)
+        {
+        }
+
+        protected override void SetValueToView(DrawerLayout control, bool value) 
+            => control.SetDrawerLockMode(value ? DrawerLayout.LockModeLockedClosed : DrawerLayout.LockModeUnlocked);
+    }
+}
