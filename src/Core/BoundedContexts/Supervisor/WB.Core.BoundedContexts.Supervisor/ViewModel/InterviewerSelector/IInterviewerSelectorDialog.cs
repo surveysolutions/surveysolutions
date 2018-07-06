48c0e49340c23b7776dd0bf804aa9e19497758ac
Slava Scherbak <slava.scherbak@gmail.com>
2018-07-06 17:59:58 -0400

    KP-11532: added reassign on resolve screen

--- src/Core/BoundedContexts/Supervisor/WB.Core.BoundedContexts.Supervisor/ViewModel/InterviewerSelector/IInterviewerSelectorDialog.cs
+++ src/Core/BoundedContexts/Supervisor/WB.Core.BoundedContexts.Supervisor/ViewModel/InterviewerSelector/IInterviewerSelectorDialog.cs
@@ -1,11 +1,10 @@
 ﻿using System;
-using WB.Core.SharedKernels.Enumerator.Views;
 
 namespace WB.Core.BoundedContexts.Supervisor.ViewModel.InterviewerSelector
 {
     public interface IInterviewerSelectorDialog
     {
-        void SelectInterviewer(AssignmentDocument assignment);
+        void SelectInterviewer(string title);
         event EventHandler Cancelled;
         event EventHandler<InterviewerSelectedArgs> Selected;
         void CloseDialog();
