897a0ab55126c2308b3be128f583932010e20a01
Andrii Kozhyn <hedgic@gmail.com>
2018-07-06 16:00:26 +0300

    Split service into 2 parts for easier implementation

---
+++ src/Core/BoundedContexts/Interviewer/WB.Core.BoundedContexts.Interviewer/Implementation/Services/OfflineSynchronizationServicePart2.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using WB.Core.GenericSubdomains.Portable.Implementation;
+using WB.Core.SharedKernels.DataCollection.Implementation.Entities;
+using WB.Core.SharedKernels.DataCollection.WebApi;
+using WB.Core.SharedKernels.Questionnaire.Api;
+using WB.Core.SharedKernels.Questionnaire.Translations;
+
+namespace WB.Core.BoundedContexts.Interviewer.Implementation.Services
+{
+    public partial class OfflineSynchronizationService
+    {
+        public Task UploadInterviewAsync(Guid interviewId, InterviewPackageApiView completedInterview,
+            Action<decimal, long, long> onDownloadProgressChanged, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UploadInterviewImageAsync(Guid interviewId, string fileName, byte[] fileData,
+            Action<decimal, long, long> onDownloadProgressChanged,
+            CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UploadInterviewAudioAsync(Guid interviewId, string fileName, string contentType, byte[] fileData,
+            Action<decimal, long, long> onDownloadProgressChanged, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<string>> GetAttachmentContentsAsync(QuestionnaireIdentity questionnaire,
+            Action<decimal, long, long> onDownloadProgressChanged,
+            CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AttachmentContent> GetAttachmentContentAsync(string contentId,
+            Action<decimal, long, long> onDownloadProgressChanged, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<QuestionnaireIdentity>> GetServerQuestionnairesAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<TranslationDto>> GetQuestionnaireTranslationAsync(QuestionnaireIdentity questionnaireIdentity,
+            CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CompanyLogoInfo> GetCompanyLogo(string storedClientEtag, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SendSyncStatisticsAsync(SyncStatisticsApiView statistics, CancellationToken token,
+            RestCredentials credentials)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SendUnexpectedExceptionAsync(UnexpectedExceptionApiView exception, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<MapView>> GetMapList(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<RestStreamResult> GetMapContentStream(string mapName, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Guid> GetCurrentSupervisor(CancellationToken token, RestCredentials credentials)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> IsAutoUpdateEnabledAsync(CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UploadAuditLogEntityAsync(AuditLogEntitiesApiView auditLogEntity,
+            CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Guid>> CheckObsoleteInterviewsAsync(List<ObsoletePackageCheck> checks,
+            CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
