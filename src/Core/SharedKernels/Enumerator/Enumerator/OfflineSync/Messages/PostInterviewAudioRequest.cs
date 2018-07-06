4e0f48cfa215083deb61d4f0d2efac7007f72f4b
Andrii Kozhyn <hedgic@gmail.com>
2018-07-06 15:56:56 +0300

    KP-11565: added code to push images and audio for interview

---
+++ src/Core/SharedKernels/Enumerator/Enumerator/OfflineSync/Messages/PostInterviewAudioRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace WB.Core.SharedKernels.Enumerator.OfflineSync.Messages
+{
+    public class PostInterviewAudioRequest
+    {
+        public Guid InterviewId { get; set; }
+        public string FileName { get; set; }
+        public string ContentType { get; set; }
+        public byte[] Content { get; set; }
+
+        public PostInterviewAudioRequest(Guid interviewId, string fileName, string contentType, byte[] content)
+        {
+            InterviewId = interviewId;
+            FileName = fileName;
+            ContentType = contentType;
+            Content = content;
+        }
+    }
+}
