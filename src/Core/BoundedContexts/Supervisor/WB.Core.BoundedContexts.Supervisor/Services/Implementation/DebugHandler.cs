618c8bbca0ef7640729af1aecfedafd92c27f8c4
Victor Hladkikh <me@alfeg.net>
2018-07-09 19:52:24 +0300

    KP-11565 Synchronization process for offline sync # CompanyLogo

---
+++ src/Core/BoundedContexts/Supervisor/WB.Core.BoundedContexts.Supervisor/Services/Implementation/DebugHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using WB.Core.SharedKernels.Enumerator.OfflineSync.Messages;
+using WB.Core.SharedKernels.Enumerator.OfflineSync.Services;
+
+namespace WB.Core.BoundedContexts.Supervisor.Services.Implementation
+{
+    [Obsolete]
+    public class DebugHandler : IHandleCommunicationMessage
+    {
+        public void Register(IRequestHandler requestHandler)
+        {
+            requestHandler.RegisterHandler<SendBigAmountOfDataRequest, SendBigAmountOfDataResponse>(Handle);
+        }
+
+        public Task<SendBigAmountOfDataResponse> Handle(SendBigAmountOfDataRequest request)
+        {
+            return Task.FromResult(new SendBigAmountOfDataResponse
+            {
+                Data = request.Data
+            });
+        }
+    }
+}
---
+++ src/Core/BoundedContexts/Supervisor/WB.Core.BoundedContexts.Supervisor/Services/Implementation/OfflineSyncHandlers/SupervisorBinaryHandler.cs
@@ -0,0 +1,53 @@
+﻿using System.Threading.Tasks;
+using WB.Core.SharedKernels.DataCollection.WebApi;
+using WB.Core.SharedKernels.Enumerator.OfflineSync.Messages;
+using WB.Core.SharedKernels.Enumerator.OfflineSync.Services;
+using WB.Core.SharedKernels.Enumerator.Services.Infrastructure.Storage;
+using WB.Core.SharedKernels.Enumerator.Views;
+
+namespace WB.Core.BoundedContexts.Supervisor.Services.Implementation.OfflineSyncHandlers
+{
+    public class SupervisorBinaryHandler : IHandleCommunicationMessage
+    {
+        private readonly IPlainStorage<CompanyLogo> logoStorage;
+
+        public SupervisorBinaryHandler(IPlainStorage<CompanyLogo> logoStorage)
+        {
+            this.logoStorage = logoStorage;
+        }
+
+        public void Register(IRequestHandler requestHandler)
+        {
+            requestHandler.RegisterHandler<GetCompanyLogoRequest, GetCompanyLogoResponse>(GetCompanyLogo);
+        }
+
+        public Task<GetCompanyLogoResponse> GetCompanyLogo(GetCompanyLogoRequest request)
+        {
+            var existingLogo = logoStorage.GetById(CompanyLogo.StorageKey);
+            if (existingLogo == null)
+            {
+                return Task.FromResult(new GetCompanyLogoResponse
+                {
+                    LogoInfo = new CompanyLogoInfo
+                    {
+                        HasCustomLogo = false,
+                        LogoNeedsToBeUpdated = !string.IsNullOrEmpty(request.Etag)
+                    }
+                });
+            }
+
+            var needUpdate = existingLogo.ETag != request.Etag;
+
+            return Task.FromResult(new GetCompanyLogoResponse
+            {
+                LogoInfo = new CompanyLogoInfo
+                {
+                    Etag = existingLogo.ETag,
+                    Logo = needUpdate ? existingLogo.File : null,
+                    LogoNeedsToBeUpdated = needUpdate,
+                    HasCustomLogo = true
+                }
+            });
+        }
+    }
+}
