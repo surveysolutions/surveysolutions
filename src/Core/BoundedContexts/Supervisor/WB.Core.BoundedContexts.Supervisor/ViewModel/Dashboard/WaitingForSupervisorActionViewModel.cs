f87e1d62adfad33bf0436b8b57b829a96f17e647
Roman Lytvyn <litvin.roma@gmail.com>
2018-07-10 17:09:56 +0300

    KP-11557 Dashboard: Show last visited interview when navigating from interview details. Added navigation for "Waiting for your action" items

--- src/Core/BoundedContexts/Supervisor/WB.Core.BoundedContexts.Supervisor/ViewModel/Dashboard/WaitingForSupervisorActionViewModel.cs
+++ src/Core/BoundedContexts/Supervisor/WB.Core.BoundedContexts.Supervisor/ViewModel/Dashboard/WaitingForSupervisorActionViewModel.cs
@@ -1,9 +1,9 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using WB.Core.BoundedContexts.Supervisor.Properties;
+using WB.Core.BoundedContexts.Supervisor.ViewModel.Dashboard.Items;
 using WB.Core.BoundedContexts.Supervisor.ViewModel.Dashboard.Services;
-using WB.Core.GenericSubdomains.Portable;
 using WB.Core.SharedKernels.Enumerator.Services;
 using WB.Core.SharedKernels.Enumerator.ViewModels.Dashboard;
 using WB.Core.SharedKernels.Enumerator.ViewModels.InterviewDetails.Groups;
@@ -14,6 +14,17 @@ namespace WB.Core.BoundedContexts.Supervisor.ViewModel.Dashboard
     {
         private readonly IDashboardItemsAccessor dashboardItemsAccessor;
         private readonly IInterviewViewModelFactory viewModelFactory;
+
+        private int? highLightedItemIndex;
+        public int? HighLightedItemIndex
+        {
+            get => highLightedItemIndex;
+            set => SetProperty(ref highLightedItemIndex, value);
+        }
+
+        private Guid? lastVisitedInterviewId;
+
+        public void Prepare(Guid? parameter) => this.lastVisitedInterviewId = parameter;
 
         public override async Task Initialize()
         {
@@ -35,8 +46,21 @@ namespace WB.Core.BoundedContexts.Supervisor.ViewModel.Dashboard
             var subtitle = viewModelFactory.GetNew<DashboardSubTitleViewModel>();
             subtitle.Title = SupervisorDashboard.WaitingForSupervisorActionSubtitle;
 
-            var waitingForSupervisorAction = this.dashboardItemsAccessor.WaitingForSupervisorAction();
-            return  subtitle.ToEnumerable().Concat(waitingForSupervisorAction);
+            yield return subtitle;
+
+            var interviewIndex = 1;
+            foreach (var dashboardItem in this.dashboardItemsAccessor.WaitingForSupervisorAction())
+            {
+                if (dashboardItem is SupervisorDashboardInterviewViewModel interviewDashboardItem &&
+                    interviewDashboardItem.InterviewId == lastVisitedInterviewId)
+                {
+                    this.HighLightedItemIndex = interviewIndex;
+                }
+
+                interviewIndex++;
+
+                yield return dashboardItem;
+            }
         }
     }
 }
