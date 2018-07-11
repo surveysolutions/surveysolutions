e8a2809f8e9571e983ca4721a2dd9182f0b7a061
Andrii Kozhyn <hedgic@gmail.com>
2018-07-11 18:23:27 +0300

    KP-11589 unexpected exceptions should be send from IN to SV and then to HQ

---
+++ src/Core/BoundedContexts/Supervisor/WB.Core.BoundedContexts.Supervisor/Views/UnexpectedExceptionFromInterviewerView.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using WB.Core.SharedKernels.Enumerator.Services.Infrastructure.Storage;
+
+namespace WB.Core.BoundedContexts.Supervisor.Views
+{
+    public class UnexpectedExceptionFromInterviewerView : IPlainStorageEntity<int>
+    {
+        public int Id { get; set; }
+
+        public Guid InterviewerId { get; set; }
+
+        public string StackTrace { get; set; }
+        
+        public string Message { get; set; }
+    }
+}
