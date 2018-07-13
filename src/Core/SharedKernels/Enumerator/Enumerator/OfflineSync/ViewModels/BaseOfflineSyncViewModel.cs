679353b1bd5c7268f0c8c9750fdb8b7c02975e71
Victor Gladkikh <me@alfeg.net>
2018-07-13 14:52:15 +0300

    KP-11518 Sync IN <-> Supervisor # No more weird endpoints in messages

--- src/Core/SharedKernels/Enumerator/Enumerator/OfflineSync/ViewModels/BaseOfflineSyncViewModel.cs
+++ src/Core/SharedKernels/Enumerator/Enumerator/OfflineSync/ViewModels/BaseOfflineSyncViewModel.cs
@@ -50,11 +50,11 @@ namespace WB.Core.SharedKernels.Enumerator.OfflineSync.ViewModels
                     this.nearbyConnection.AcceptConnection(iniConnection.Endpoint);
                     break;
                 case NearbyEvent.Connected connected:
-                    SetStatus(ConnectionStatus.Connected, "Connected to " + connected.Endpoint);
+                    SetStatus(ConnectionStatus.Connected, "Connected to " + connected.Name);
                     Connected(connected.Endpoint);
                     break;
                 case NearbyEvent.Disconnected disconnected:
-                    SetStatus(ConnectionStatus.Discovering, "Disconnected from " + disconnected.Endpoint);
+                    SetStatus(ConnectionStatus.Discovering, "Disconnected from " + disconnected.Name ?? disconnected.Endpoint);
                     Disconnected(disconnected.Endpoint);
                     break;
                 case NearbyEvent.EndpointFound endpointFound:
@@ -73,7 +73,7 @@ namespace WB.Core.SharedKernels.Enumerator.OfflineSync.ViewModels
         protected virtual void SetStatus(ConnectionStatus connectionStatus, string details = null)
         {
             this.Status = connectionStatus.ToString();
-            this.StatusDetails = details ?? String.Empty + "\r\n"+ this.StatusDetails;
+            this.StatusDetails = details ?? String.Empty;
         }
 
         protected virtual void Connected(string connectedEndpoint)
@@ -81,15 +81,13 @@ namespace WB.Core.SharedKernels.Enumerator.OfflineSync.ViewModels
 
         }
 
-        protected virtual async void OnFound(string endpoint, NearbyDiscoveredEndpointInfo info)
+        protected virtual async void OnFound(string endpointId, NearbyDiscoveredEndpointInfo info)
         {
-            SetStatus(ConnectionStatus.Connecting,
-                $"Found endpoint: {info.EndpointName} [{endpoint}]. Requesting conection");
-            Log.Trace("OnFound {0} - {1}", endpoint, info.EndpointName);
-            await this.nearbyConnection.RequestConnection(this.principal.CurrentUserIdentity.Name, endpoint);
+            SetStatus(ConnectionStatus.Connecting, $"Found {info.EndpointName}. Requesting conection");
+            Log.Trace("OnFound {0} - {1}", endpointId, info.EndpointName);
+            await this.nearbyConnection.RequestConnection(this.principal.CurrentUserIdentity.Name, endpointId);
 
-            SetStatus(ConnectionStatus.Connecting,
-                $"Requested conection from endpoint: {info.EndpointName} [{endpoint}]");
+            SetStatus(ConnectionStatus.Connecting, $"Requested conection to {info.EndpointName}");
         }
         private string endpoint;
         public string Endpoint
@@ -134,13 +132,15 @@ namespace WB.Core.SharedKernels.Enumerator.OfflineSync.ViewModels
             SetStatus(ConnectionStatus.Advertising, "Waiting for interviewers connections");
         }
 
-        protected virtual async void OnConnection(string endpoint, NearbyConnectionInfo info)
+        protected virtual async void OnConnection(string endpointId, NearbyConnectionInfo info)
         {
-            SetStatus(ConnectionStatus.Connecting, $"Accepting connection from {endpoint} {info.EndpointName}");
-            Log.Trace("OnConnection from " + endpoint + " => " + info.EndpointName);
-            await this.nearbyConnection.AcceptConnection(endpoint);
-            Log.Trace("Accept connection from " + endpoint + " => " + info.EndpointName);
-            SetStatus(ConnectionStatus.Connected, $"Connected to {endpoint} {info.EndpointName}");
+            SetStatus(ConnectionStatus.Connecting, $"New incoming connection from {info.EndpointName}");
+
+            Log.Trace("OnConnection from " + endpointId + " => " + info.EndpointName);
+            await this.nearbyConnection.AcceptConnection(endpointId);
+            Log.Trace("Accept connection from " + endpointId + " => " + info.EndpointName);
+
+            SetStatus(ConnectionStatus.Connected, $"Connected to {info.EndpointName}");
         }
 
         private string status;
