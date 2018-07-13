7011ff7f18e4dd9c24bd3557cdefdb4be543d11e
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2018-07-13 12:40:39 +0300

    KP-11579 Change logic to calculate group state for supervisor

---
+++ src/Core/BoundedContexts/Supervisor/WB.Core.BoundedContexts.Supervisor/ViewModel/SupervisorGroupStateCalculationStrategy.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using WB.Core.SharedKernels.DataCollection;
+using WB.Core.SharedKernels.DataCollection.Aggregates;
+using WB.Core.SharedKernels.DataCollection.Implementation.Aggregates.InterviewEntities;
+using WB.Core.SharedKernels.Enumerator.ViewModels.InterviewDetails.Groups;
+
+namespace WB.Core.BoundedContexts.Supervisor.ViewModel
+{
+    public class SupervisorGroupStateCalculationStrategy : IGroupStateCalculationStrategy
+    {
+        public GroupStatus CalculateDetailedStatus(Identity groupIdentity, IStatefulInterview interview)
+        {
+            GroupStatus status;
+            var group = interview.GetGroup(groupIdentity);
+
+            var countEnabledAnsweredQuestions = group.CountEnabledAnsweredQuestionsForSupervisor();
+
+            if (group.HasUnansweredQuestionsForSupervisor())
+                status = countEnabledAnsweredQuestions > 0 ? GroupStatus.Started : GroupStatus.NotStarted;
+            else
+                status = GroupStatus.Completed;
+
+            foreach (var subGroup in GetSubgroupStatuses())
+            {
+                switch (status)
+                {
+                    case GroupStatus.Completed when subGroup != GroupStatus.Completed:
+                        return GroupStatus.Started;
+                    case GroupStatus.NotStarted when subGroup != GroupStatus.NotStarted:
+                        return GroupStatus.Started;
+                }
+            }
+
+            return status;
+
+            IEnumerable<GroupStatus> GetSubgroupStatuses()
+            {
+                return group.Children.OfType<InterviewTreeGroup>()
+                    .Where(c => c.IsDisabled() == false)
+                    .Select(subgroup => this.CalculateDetailedStatus(subgroup.Identity, interview));
+            }
+        }
+    }
+}
--- src/UI/Interviewer/WB.UI.Interviewer/ServiceLocation/InterviewerUIModule.cs
+++ src/UI/Interviewer/WB.UI.Interviewer/ServiceLocation/InterviewerUIModule.cs
@@ -3,10 +3,6 @@ using Ncqrs.Eventing.Storage;
 using WB.Core.BoundedContexts.Interviewer.Implementation.Services;
 using WB.Core.BoundedContexts.Interviewer.Implementation.Services.OfflineSync;
 using WB.Core.BoundedContexts.Interviewer.Services;
-using WB.Core.BoundedContexts.Interviewer.Services.Infrastructure;
-using WB.Core.BoundedContexts.Interviewer.Views;
-using WB.Core.BoundedContexts.Interviewer.Views.Dashboard;
-using WB.Core.BoundedContexts.Interviewer.Views.Dashboard.DashboardItems;
 using WB.Core.GenericSubdomains.Portable.Implementation;
 using WB.Core.GenericSubdomains.Portable.Implementation.Services;
 using WB.Core.GenericSubdomains.Portable.ServiceLocation;
@@ -18,22 +14,16 @@ using WB.Core.Infrastructure.Modularity;
 using WB.Core.SharedKernels.DataCollection;
 using WB.Core.SharedKernels.DataCollection.Services;
 using WB.Core.SharedKernels.Enumerator.Implementation.Services;
-using WB.Core.SharedKernels.Enumerator.Implementation.Services.MapSynchronization;
 using WB.Core.SharedKernels.Enumerator.Implementation.Services.Synchronization;
 using WB.Core.SharedKernels.Enumerator.Services;
 using WB.Core.SharedKernels.Enumerator.Services.Infrastructure;
 using WB.Core.SharedKernels.Enumerator.Services.MapService;
 using WB.Core.SharedKernels.Enumerator.Services.MapSynchronization;
 using WB.Core.SharedKernels.Enumerator.Services.Synchronization;
-using WB.Core.SharedKernels.Enumerator.ViewModels;
-using WB.Core.SharedKernels.Enumerator.ViewModels.Dashboard;
 using WB.Core.SharedKernels.Enumerator.ViewModels.InterviewDetails;
-using WB.Core.SharedKernels.Enumerator.ViewModels.InterviewLoading;
-using WB.Core.SharedKernels.Enumerator.Views;
+using WB.Core.SharedKernels.Enumerator.ViewModels.InterviewDetails.Groups;
 using WB.UI.Interviewer.Implementations.Services;
 using WB.UI.Interviewer.Services;
-using WB.UI.Interviewer.Settings;
-using WB.UI.Interviewer.ViewModel;
 using WB.UI.Shared.Enumerator.CustomServices;
 using WB.UI.Shared.Enumerator.Services;
 using WB.UI.Shared.Enumerator.Services.Internals;
@@ -45,9 +35,6 @@ namespace WB.UI.Interviewer.ServiceLocation
     {
         public void Load(IIocRegistry registry)
         {
-            registry.Bind<InterviewerMvxApplication>();
-            registry.Bind<InterviewerAppStart>();
-
             registry.Bind<ISideBarSectionViewModelsFactory, SideBarSectionViewModelFactory>();
             registry.Bind<IViewModelNavigationService, ViewModelNavigationService>();
             registry.Bind<ITabletDiagnosticService, TabletDiagnosticService>();
@@ -58,13 +45,11 @@ namespace WB.UI.Interviewer.ServiceLocation
             registry.Bind<IHttpClientFactory, AndroidHttpClientFactory>();
             registry.BindAsSingletonWithConstructorArgument<IRestService, RestService>("restServicePointManager", null);
             registry.Bind<IInterviewUniqueKeyGenerator, InterviewerInterviewUniqueKeyGenerator>();
-
-            registry.Bind<OfflineSynchronizationService, OfflineSynchronizationService>();
-            registry.Bind<SynchronizationService, SynchronizationService>();
+            registry.Bind<IInterviewUniqueKeyGenerator, InterviewerInterviewUniqueKeyGenerator>();
+            registry.Bind<IGroupStateCalculationStrategy, EnumeratorGroupStateCalculationStrategy>();
 
             registry.BindAsSingleton<ISynchronizationMode, SynchronizationModeSelector>();
             registry.Bind<ISynchronizationService, SyncronizationServiceWrapper>();
-            
 
             registry.Bind<IInterviewerSynchronizationService, SynchronizationService>();
             registry.Bind<IBattery, AndroidBattery>();
@@ -81,8 +66,6 @@ namespace WB.UI.Interviewer.ServiceLocation
             registry.Bind<IQuestionnaireDownloader, QuestionnaireDownloader>();
             
             registry.Bind<IAuditLogSynchronizer, AuditLogSynchronizer>();
-            registry.Bind<AttachmentsCleanupService>();
-            registry.Bind<CompanyLogoSynchronizer>();
             registry.Bind<IMapSyncProvider, MapSyncProvider>();
             registry.Bind<IMapService, MapService>();
             registry.Bind<IViewModelNavigationService, ViewModelNavigationService>();
@@ -91,37 +74,6 @@ namespace WB.UI.Interviewer.ServiceLocation
             registry.BindAsSingleton<IInterviewViewModelFactory, InterviewViewModelFactory>();
 
             registry.BindAsSingleton<ICommandService, SequentialCommandService>();
-
-            registry.Bind<LoginViewModel>();
-            registry.Bind<PrefilledQuestionsViewModel>();
-            registry.Bind<InterviewViewModel>();
-            registry.Bind<BackupRestoreViewModel>();
-            registry.Bind<SendTabletInformationViewModel>();
-            registry.Bind<BandwidthTestViewModel>();
-            registry.Bind<CheckNewVersionViewModel>();
-            registry.Bind<DiagnosticsViewModel>();
-            registry.Bind<FinishInstallationViewModel>();
-            registry.Bind<InterviewerCompleteInterviewViewModel>();
-            registry.Bind<SynchronizationViewModel>();
-            registry.Bind<MapSynchronizationViewModel>();
-            registry.Bind<RelinkDeviceViewModel>();
-            registry.Bind<DashboardViewModel>();
-            registry.Bind<DashboardSearchViewModel>();
-            registry.Bind<MapsViewModel>();
-            registry.Bind<CompletedInterviewsViewModel>();
-            registry.Bind<RejectedInterviewsViewModel>();
-            registry.Bind<StartedInterviewsViewModel>();
-            registry.Bind<InterviewerAssignmentDashboardItemViewModel>();
-            registry.Bind<CensusQuestionnaireDashboardItemViewModel>();
-            registry.Bind<ExpandableQuestionsDashboardItemViewModel>();
-            registry.Bind<InterviewDashboardItemViewModel>();
-            registry.Bind<CreateNewViewModel>();
-            registry.Bind<DashboardSubTitleViewModel>();
-            registry.Bind<CompanyLogoSynchronizer>();
-            registry.Bind<LoadingViewModel>();
-            registry.Bind<PhotoViewViewModel>();
-            registry.Bind<OfflineInterviewerSyncViewModel>();
-
 #if EXCLUDEEXTENSIONS
             registry.Bind<IAreaEditService, WB.UI.Shared.Enumerator.CustomServices.AreaEditor.DummyAreaEditService>();
             registry.Bind<ICheckVersionUriProvider, CheckForVersionUriProvider>();
