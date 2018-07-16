b51e4cc724cb09be33ded1eb4f5ecfe634fb163d
Victor Hladkikh <me@alfeg.net>
2018-07-16 15:32:41 +0300

    Exclude module setup from code coverage

--- src/Core/BoundedContexts/Tester/WB.Core.BoundedContexts.Tester/TesterBoundedContextModule.cs
+++ src/Core/BoundedContexts/Tester/WB.Core.BoundedContexts.Tester/TesterBoundedContextModule.cs
@@ -5,18 +5,17 @@ using WB.Core.BoundedContexts.Tester.Implementation.Services;
 using WB.Core.BoundedContexts.Tester.Services;
 using WB.Core.GenericSubdomains.Portable.ServiceLocation;
 using WB.Core.Infrastructure.CommandBus;
-using WB.Core.Infrastructure.CommandBus.Implementation;
 using WB.Core.Infrastructure.Implementation;
 using WB.Core.Infrastructure.Modularity;
 using WB.Core.Infrastructure.PlainStorage;
 using WB.Core.SharedKernels.DataCollection.Implementation.Repositories;
 using WB.Core.SharedKernels.DataCollection.Repositories;
-using WB.Core.SharedKernels.Enumerator.Implementation.Services;
 using WB.Core.SharedKernels.Enumerator.Services.Infrastructure.Storage;
 using WB.Core.SharedKernels.Enumerator.Views;
 
 namespace WB.Core.BoundedContexts.Tester
 {
+    [System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
     public class TesterBoundedContextModule : IModule
     {
         public void Load(IIocRegistry registry)
