b6e416715b07d543b8f5f5e5c46d56e1cd78312b
Victor Hladkikh <me@alfeg.net>
2018-07-18 16:41:52 +0300

    KP-11617 Log nearby communication errors and warning into log # code review

--- src/Core/SharedKernels/Enumerator/Enumerator/OfflineSync/Services/Implementation/NearbyConnectionsRequestHandler.cs
+++ src/Core/SharedKernels/Enumerator/Enumerator/OfflineSync/Services/Implementation/NearbyConnectionsRequestHandler.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using WB.Core.GenericSubdomains.Portable.Services;
 using WB.Core.SharedKernels.Enumerator.OfflineSync.Messages;
 
 namespace WB.Core.SharedKernels.Enumerator.OfflineSync.Services.Implementation
 {
     public class NearbyConnectionsRequestHandler : IRequestHandler
     {
+        private readonly ILogger logger;
+
+        public NearbyConnectionsRequestHandler(ILogger logger)
+        {
+            this.logger = logger;
+        }
+
         private readonly Dictionary<Type, Func<ICommunicationMessage, Task<ICommunicationMessage>>> handlers
             = new Dictionary<Type, Func<ICommunicationMessage, Task<ICommunicationMessage>>>();
         
@@ -30,8 +39,17 @@ namespace WB.Core.SharedKernels.Enumerator.OfflineSync.Services.Implementation
             {
                 handlers[typeof(TReq)] = async r =>
                 {
-                    var result = await handler((TReq)r);
-                    return result;
+                    try
+                    {
+                        var result = await handler((TReq) r);
+                        return result;
+                    }
+                    catch (Exception e)
+                    {
+                        e.Data["requestJson"] = JsonConvert.SerializeObject(r);
+                        this.logger.Error($"Error during request handling: {typeof(TReq).Name} => {typeof(TResp).Name}", e);
+                        throw;
+                    }
                 };
             }
         }
