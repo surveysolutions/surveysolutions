f5f3e60ecc0e92babd5864f2e453fc8d02551700
Roman Lytvyn <litvin.roma@gmail.com>
2018-07-18 13:52:14 +0300

    KP-11599 [code review] IN: Offline sync process indication

--- src/Core/SharedKernels/Enumerator/Enumerator/OfflineSync/ViewModels/BaseOfflineSyncViewModel.cs
+++ src/Core/SharedKernels/Enumerator/Enumerator/OfflineSync/ViewModels/BaseOfflineSyncViewModel.cs
@@ -2,7 +2,6 @@
 using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
 using MvvmCross.Logging;
-using Plugin.Permissions.Abstractions;
 using WB.Core.SharedKernels.Enumerator.OfflineSync.Entities;
 using WB.Core.SharedKernels.Enumerator.OfflineSync.Services;
 using WB.Core.SharedKernels.Enumerator.Services;
@@ -28,19 +27,19 @@ namespace WB.Core.SharedKernels.Enumerator.OfflineSync.ViewModels
     [ExcludeFromCodeCoverage()] // TODO: remove attribute when UI binding completed
     public abstract class BaseOfflineSyncViewModel : BaseViewModel, IOfflineSyncViewModel
     {
-        private readonly IPermissionsService permissions;
-        private readonly INearbyConnection nearbyConnection;
+        protected readonly IPermissionsService permissions;
+        protected readonly INearbyConnection nearbyConnection;
+        private readonly IEnumeratorSettings settings;
 
-        protected BaseOfflineSyncViewModel(
-            IPrincipal principal,
+        protected BaseOfflineSyncViewModel(IPrincipal principal,
             IViewModelNavigationService viewModelNavigationService,
             IPermissionsService permissions,
-            INearbyConnection nearbyConnection
-        )
+            INearbyConnection nearbyConnection, IEnumeratorSettings settings)
             : base(principal, viewModelNavigationService)
         {
             this.permissions = permissions;
             this.nearbyConnection = nearbyConnection;
+            this.settings = settings;
             SetStatus(ConnectionStatus.WaitingForGoogleApi);
             this.nearbyConnection.Events.Subscribe(HandleConnectionEvents);
         }
@@ -69,21 +68,11 @@ namespace WB.Core.SharedKernels.Enumerator.OfflineSync.ViewModels
             }
         }
 
-        protected virtual void Disconnected(string disconnectedEndpoint)
-        {
+        protected virtual void Disconnected(string disconnectedEndpoint) { }
 
-        }
+        protected virtual void SetStatus(ConnectionStatus connectionStatus, string details = null) { }
 
-        protected virtual void SetStatus(ConnectionStatus connectionStatus, string details = null)
-        {
-            this.Status = connectionStatus.ToString();
-            this.StatusDetails = details ?? String.Empty;
-        }
-
-        protected virtual void Connected(string connectedEndpoint, string connectedTo)
-        {
-
-        }
+        protected virtual void Connected(string connectedEndpoint, string connectedTo) { }
 
         protected virtual void OnError(string errorMessage, ConnectionStatusCode errorCode) { }
 
@@ -97,28 +86,8 @@ namespace WB.Core.SharedKernels.Enumerator.OfflineSync.ViewModels
 
             SetStatus(ConnectionStatus.Connecting, $"Requested conection to {info.EndpointName}");
         }
-        private string endpoint;
-        public string Endpoint
-        {
-            get => endpoint;
-            set => SetProperty(ref endpoint, value);
-        }
 
-        protected async Task StartDiscovery()
-        {
-            await permissions.AssureHasPermission(Permission.Location);
-
-            SetStatus(ConnectionStatus.StartDiscovering, $"Starting discovery");
-            var serviceName = GetServiceName();
-            var result = await this.nearbyConnection.StartDiscovery(serviceName);
-
-            if (!SetErrorOnResultIfNeeded(result)) return;
-
-            Endpoint = serviceName;
-            SetStatus(ConnectionStatus.Discovering, $"Searching for supervisor");
-        }
-
-        private bool SetErrorOnResultIfNeeded(NearbyStatus status)
+        protected bool SetErrorOnResultIfNeeded(NearbyStatus status)
         {
             if (status.IsSuccess == false)
             {
@@ -129,40 +98,14 @@ namespace WB.Core.SharedKernels.Enumerator.OfflineSync.ViewModels
             return status.IsSuccess;
         }
 
-        protected void StopDiscovery()
+        protected string GetServiceName()
         {
-            //this.nearbyConnection.StopAllEndpoint();
-            this.nearbyConnection.StopDiscovery();
-        }
-            
-        protected void StopAdvertising()
-        {
-            //this.nearbyConnection.StopAllEndpoint();
-            this.nearbyConnection.StopAdvertising();
+            var normalizedEndpoint = new Uri(this.settings.Endpoint).ToString().TrimEnd('/').ToLower();
+
+            return $"{normalizedEndpoint}/{this.GetDeviceIdentification()}";
         }
 
-        protected abstract string GetServiceName();
-
-        protected async Task StartAdvertising()
-        {
-            await permissions.AssureHasPermission(Permission.Location);
-
-            Log.Trace("StartAdvertising");
-
-            SetStatus(ConnectionStatus.StartAdvertising, $"Starting advertising");
-            var serviceName = GetServiceName();
-            try
-            {
-                await this.nearbyConnection.StartAdvertising(serviceName, this.principal.CurrentUserIdentity.Name);
-                Endpoint = serviceName;
-                SetStatus(ConnectionStatus.Advertising, "Waiting for interviewers connections");
-            }
-            catch (NearbyConnectionException nce)
-            {
-                SetStatus(ConnectionStatus.Error, nce.Message);
-            }
-        }
-
+        protected abstract string GetDeviceIdentification();
 
         protected virtual async void OnConnection(string endpointId, NearbyConnectionInfo info)
         {
@@ -177,21 +120,6 @@ namespace WB.Core.SharedKernels.Enumerator.OfflineSync.ViewModels
             SetStatus(ConnectionStatus.Connected, $"Connected to {info.EndpointName}");
         }
 
-        private string status;
-        private string statusDetails;
-
-        public string Status
-        {
-            get => status;
-            set => SetProperty(ref status, value);
-        }
-
-        public string StatusDetails
-        {
-            get => statusDetails;
-            set => SetProperty(ref statusDetails, value);
-        }
-
         protected abstract Task OnGoogleApiReady();
 
         public void SetGoogleAwaiter(Task<bool> apiConnected)
@@ -204,12 +132,6 @@ namespace WB.Core.SharedKernels.Enumerator.OfflineSync.ViewModels
             {
                 apiConnected.ContinueWith(async res => this.OnGoogleApiReady());
             }
-        }
-        
-        protected string NormalizeEndpoint(string endpoint)
-        {
-            var uri = new Uri(endpoint);
-            return uri.ToString().TrimEnd('/').ToLower();
         }
     }
 }
