baa29b1cbac2cb3688d04f288d6cd7bf8fed1ae2
Victor Hladkikh <me@alfeg.net>
2018-07-19 11:53:42 +0300

    Revert "KP-11635 Sync SV-IN shouldn't work if password was updated inHQ" - sync totally broken
    This reverts commit 4a312b7e9d4a092313ce022e840cb63e5db60589.

--- src/Core/BoundedContexts/Supervisor/WB.Core.BoundedContexts.Supervisor/Services/Implementation/SynchronizationProcess.cs
+++ src/Core/BoundedContexts/Supervisor/WB.Core.BoundedContexts.Supervisor/Services/Implementation/SynchronizationProcess.cs
@@ -174,10 +174,7 @@ namespace WB.Core.BoundedContexts.Supervisor.Services.Implementation
                         PasswordHash = interviewer.PasswordHash,
                         PhoneNumber = interviewer.PhoneNumber,
                         UserName = interviewer.UserName,
-                        FullaName = interviewer.FullName,
-                        Token = interviewer.Token,
-                        IsLockedBySupervisor = interviewer.IsLockedBySupervisor,
-                        IsLockedByHeadquarters = interviewer.IsLockedByHeadquarters
+                        FullaName = interviewer.FullName
                     };
                 }
                 else
@@ -186,9 +183,6 @@ namespace WB.Core.BoundedContexts.Supervisor.Services.Implementation
                     local.PasswordHash = interviewer.PasswordHash;
                     local.PhoneNumber = interviewer.PhoneNumber;
                     local.UserName = interviewer.UserName;
-                    local.Token = interviewer.Token;
-                    local.IsLockedBySupervisor = interviewer.IsLockedBySupervisor;
-                    local.IsLockedByHeadquarters = interviewer.IsLockedByHeadquarters;
                 }
 
                 this.interviewerViewRepository.Store(local);
