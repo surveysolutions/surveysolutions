2e4b795fb05a1acd1940f4c41235a9ae8ca3d00d
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2018-07-24 16:12:07 -0400

    KP-11635 Sync SV-IN shouldn't work if password was updated inHQ

--- src/Core/BoundedContexts/Interviewer/WB.Core.BoundedContexts.Interviewer/Services/IInterviewerSynchronizationService.cs
+++ src/Core/BoundedContexts/Interviewer/WB.Core.BoundedContexts.Interviewer/Services/IInterviewerSynchronizationService.cs
@@ -2,7 +2,6 @@
 using System.Threading.Tasks;
 using WB.Core.GenericSubdomains.Portable.Implementation;
 using WB.Core.SharedKernels.DataCollection.WebApi;
-using WB.Core.SharedKernels.Enumerator.Services;
 using WB.Core.SharedKernels.Enumerator.Services.Synchronization;
 
 namespace WB.Core.BoundedContexts.Interviewer.Services
@@ -10,5 +9,6 @@ namespace WB.Core.BoundedContexts.Interviewer.Services
     public interface IInterviewerSynchronizationService : ISynchronizationService
     {
         Task<InterviewerApiView> GetInterviewerAsync(RestCredentials credentials = null, CancellationToken? token = null);
+        string ApiDownloadAppPrefixUrl { get; }
     }
 }
