364f80505e1d8da6b16f15842e535c415f15cec5
Andrii Kozhyn <hedgic@gmail.com>
2018-07-26 12:07:52 +0300

    KP-11609 use same synchronization panel on supervisor and on interviewer for progress reporting

---
+++ src/Core/BoundedContexts/Supervisor/WB.Core.BoundedContexts.Supervisor/ViewModel/ConnectedDeviceSynchronizationViewModel.cs
@@ -0,0 +1,16 @@
+﻿using MvvmCross.ViewModels;
+using WB.Core.SharedKernels.Enumerator.ViewModels;
+
+namespace WB.Core.BoundedContexts.Supervisor.ViewModel
+{
+    public class ConnectedDeviceSynchronizationViewModel : SynchronizationViewModelBase
+    {
+        public override bool  IsSynchronizationInfoShowed => true;
+
+        public override bool HasUserAnotherDevice => false;
+
+        protected override void OnSyncCompleted()
+        {
+        }
+    }
+}
