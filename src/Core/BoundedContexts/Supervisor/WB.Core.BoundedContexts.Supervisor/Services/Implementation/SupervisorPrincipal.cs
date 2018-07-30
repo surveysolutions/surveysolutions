28ce1bc2c7c5b11b77717733bd5668f3138deda1
Victor Gladkikh <me@alfeg.net>
2018-07-30 19:44:08 +0300

    KP-11689 Users should not required to revalidate password with server

--- src/Core/BoundedContexts/Supervisor/WB.Core.BoundedContexts.Supervisor/Services/Implementation/SupervisorPrincipal.cs
+++ src/Core/BoundedContexts/Supervisor/WB.Core.BoundedContexts.Supervisor/Services/Implementation/SupervisorPrincipal.cs
@@ -22,7 +22,17 @@ namespace WB.Core.BoundedContexts.Supervisor.Services.Implementation
 
         public ISupervisorUserIdentity CurrentUserIdentity => (ISupervisorUserIdentity)base.currentUserIdentity;
 
+        protected override void UpdateUserHash(string hash)
+        {
+            var user = this.usersStorage.Where(u => u.Name.ToLower() == CurrentUserIdentity.Name).FirstOrDefault();
+            if (user != null)
+            {
+                user.PasswordHash = hash;
+                this.usersStorage.Store(user);
+            }
+        }
+
         protected override IUserIdentity GetUserByName(string userName)
-            => this.usersStorage.Where(user => user.Name.ToLower() == userName).FirstOrDefault();
+                => this.usersStorage.Where(user => user.Name.ToLower() == userName).FirstOrDefault();
     }
 }
