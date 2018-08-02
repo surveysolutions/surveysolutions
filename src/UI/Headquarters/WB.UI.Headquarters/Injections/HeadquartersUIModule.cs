70a248893e2f5e096e2e9e22b40a8bed8fb91b9b
Victor Gladkikh <me@alfeg.net>
2018-08-02 16:23:40 +0300

    KP-11706 Removed all IHttpClientFactory and derived abstractions

--- src/UI/Headquarters/WB.UI.Headquarters/Injections/HeadquartersUIModule.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/Injections/HeadquartersUIModule.cs
@@ -73,7 +73,6 @@ namespace WB.UI.Headquarters.Injections
             registry.Bind<IStringCompressor, JsonCompressor>();
             registry.BindAsSingleton<IRestServiceSettings, DesignerQuestionnaireApiRestServiceSettings>();
 
-            registry.Bind<IHttpClientFactory, DefaultHttpClientFactory>();
             registry.Bind<IRestService, RestService>(
                 new ConstructorArgument("networkService", _ => null),
                 new ConstructorArgument("restServicePointManager", _ => null),
