83228aa5823a3193a6bdce855f71877fb60f7f56
Andrii Kozhyn <hedgic@gmail.com>
2018-08-06 13:42:40 +0300

    KP-11745 remove profile111 reference

--- src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Services/CodeGeneration/ICompilerSettings.cs
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Services/CodeGeneration/ICompilerSettings.cs
@@ -1,11 +1,8 @@
-﻿using System.Collections.Generic;
-
-namespace WB.Core.BoundedContexts.Designer.Services.CodeGeneration
+﻿namespace WB.Core.BoundedContexts.Designer.Services.CodeGeneration
 {
     public interface ICompilerSettings
     {
         bool EnableDump { get; }
         string DumpFolder { get; }
-        IEnumerable<IDynamicCompilerSettings> SettingsCollection { get; }
     }
 }
