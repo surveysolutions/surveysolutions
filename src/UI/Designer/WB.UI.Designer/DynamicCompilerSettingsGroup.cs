3d8dfe66d2329156d279cc655642a79fd9b2ea4a
Andrii Kozhyn <hedgic@gmail.com>
2018-08-06 13:42:40 +0300

    KP-11745 remove profile111 reference

--- src/UI/Designer/WB.UI.Designer/DynamicCompilerSettingsGroup.cs
+++ src/UI/Designer/WB.UI.Designer/DynamicCompilerSettingsGroup.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Configuration;
-using System.Linq;
+﻿using System.Configuration;
 using WB.Core.BoundedContexts.Designer.Services.CodeGeneration;
 
 
@@ -13,71 +11,5 @@ namespace WB.UI.Designer
 
         [ConfigurationProperty("dumpFolder", IsRequired = true)]
         public string DumpFolder => (string)this["dumpFolder"];
-
-        public IEnumerable<IDynamicCompilerSettings> SettingsCollection
-        {
-            get
-            {
-                var list = from DynamicCompilerSettings settings in settingsCollection select settings;
-                return list;
-            }
-        }
-
-        [ConfigurationProperty("settings")]
-        DynamicCompilerSettingsCollection settingsCollection
-        {
-            get { return (DynamicCompilerSettingsCollection)base["settings"]; }
-        }
-    }
-
-    [ConfigurationCollection(typeof(DynamicCompilerSettings), AddItemName = "dynamicCompilerSettings")]
-    public class DynamicCompilerSettingsCollection : ConfigurationElementCollection
-    {
-        protected override ConfigurationElement CreateNewElement()
-        {
-            return new DynamicCompilerSettings();
-        }
-
-        protected override object GetElementKey(ConfigurationElement element)
-        {
-            return ((DynamicCompilerSettings)element).name;
-        }
-    }
-
-    public class DynamicCompilerSettings : ConfigurationElement, IDynamicCompilerSettings
-    {
-        public string Name { get { return name; } }
-
-        public string PortableAssembliesPath
-        {
-            get { return portableAssembliesPath; }
-        }
-
-        public IEnumerable<string> DefaultReferencedPortableAssemblies
-        {
-            get
-            {
-                string[] assemblies = this.portableAssemblies.Split(';').Select(s => s.Trim()).ToArray();
-                return assemblies;
-            }
-        }
-
-        [ConfigurationProperty("name", IsRequired = true)]
-        public string name
-        {
-            get { return (string)this["name"]; }
-        }
-
-        [ConfigurationProperty("portableAssembliesPath", IsRequired = true)]
-        public string portableAssembliesPath
-        {
-            get { return (string)this["portableAssembliesPath"]; }
-        }
-
-        [ConfigurationProperty("portableAssemblies")]
-        public string portableAssemblies
-        {
-            get { return (string)base["portableAssemblies"]; }
-        }
     }
 }
