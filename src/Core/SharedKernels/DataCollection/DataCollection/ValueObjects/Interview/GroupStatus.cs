48a355641c727086dfec229bdd0deaa2aaf0da26
Andrii Kozhyn <hedgic@gmail.com>
2018-08-09 18:59:14 +0300

    KP-11600 added test and changed js

--- src/Core/SharedKernels/DataCollection/DataCollection/ValueObjects/Interview/GroupStatus.cs
+++ src/Core/SharedKernels/DataCollection/DataCollection/ValueObjects/Interview/GroupStatus.cs
@@ -3,10 +3,10 @@ namespace WB.Core.SharedKernels.DataCollection.ValueObjects.Interview
     public enum GroupStatus
     {
         Disabled = 1,
-        NotStarted,
-        Started,
-        StartedInvalid,
-        Completed,
-        CompletedInvalid
+        NotStarted = 2,
+        Started = 3,
+        StartedInvalid = 4,
+        Completed = 5,
+        CompletedInvalid = 6
     }
 }
