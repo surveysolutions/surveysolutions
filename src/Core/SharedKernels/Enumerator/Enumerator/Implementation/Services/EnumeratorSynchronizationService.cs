8030c70b20d04c0a72356d219e6a269923cfea7f
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2018-08-14 16:29:43 -0400

    KP-11759 Add scan button on finish installation [review]

--- src/Core/SharedKernels/Enumerator/Enumerator/Implementation/Services/EnumeratorSynchronizationService.cs
+++ src/Core/SharedKernels/Enumerator/Enumerator/Implementation/Services/EnumeratorSynchronizationService.cs
@@ -51,9 +51,7 @@ namespace WB.Core.SharedKernels.Enumerator.Implementation.Services
         protected readonly ICheckVersionUriProvider checkVersionUriProvider;
         protected readonly ILogger logger;
         protected readonly IEnumeratorSettings enumeratorSettings;
-
-        public string ApiDownloadAppPrefixUrl => "/api/interviewersync";
-
+        
         protected RestCredentials restCredentials => this.principal.CurrentUserIdentity == null
             ? null
             : new RestCredentials {
---
+++ src/Tests/WB.Tests.Unit/BoundedContexts/Supervisor/ViewModels/FinishInstallationViewModelTests/FinishInstallationViewModelTests.cs
@@ -0,0 +1,96 @@
+﻿using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using WB.Core.BoundedContexts.Supervisor.ViewModel;
+using WB.Core.GenericSubdomains.Portable.Services;
+using WB.Core.SharedKernels.DataCollection.ValueObjects;
+using WB.Core.SharedKernels.Enumerator.Services.Infrastructure;
+using WB.Tests.Abc;
+
+namespace WB.Tests.Unit.BoundedContexts.Supervisor.ViewModels.FinishInstallationViewModelTests
+{
+    [TestOf(typeof(FinishInstallationViewModel))]
+    internal class FinishInstallationViewModelTests
+    {
+        [Test]
+        public async Task when_scanning_barcode_and_returns_valid_url()
+        {
+            var endpoint = "https://example.com";
+
+            var scanner = new Mock<IQRBarcodeScanService>();
+            scanner.Setup(x => x.ScanAsync()).Returns(Task.FromResult<QRBarcodeScanResult>(new QRBarcodeScanResult()
+            {
+                Code = endpoint
+            }));
+
+            var viewModel = Create.ViewModel.FinishInstallationViewModel(qrBarcodeScanService: scanner.Object);
+            await viewModel.ScanCommand.ExecuteAsync();
+
+
+            Assert.That(viewModel.Endpoint, Is.EqualTo(endpoint));
+        }
+
+        [Test]
+        public async Task when_scanning_barcode_that_returns_valid_long_url()
+        {
+            var endpoint = "https://example.com";
+
+            var scanner = new Mock<IQRBarcodeScanService>();
+            scanner.Setup(x => x.ScanAsync()).Returns(Task.FromResult<QRBarcodeScanResult>(new QRBarcodeScanResult()
+            {
+                Code = endpoint + "/blabla/1"
+            }));
+
+            var viewModel = Create.ViewModel.FinishInstallationViewModel(qrBarcodeScanService: scanner.Object);
+            await viewModel.ScanCommand.ExecuteAsync();
+
+            Assert.That(viewModel.Endpoint, Is.EqualTo(endpoint));
+        }
+
+        [Test]
+        public async Task when_scanning_barcode_that_returns_invalid_url()
+        {
+            var endpoint = "invalid_url";
+
+            var scanner = new Mock<IQRBarcodeScanService>();
+            scanner.Setup(x => x.ScanAsync()).Returns(Task.FromResult<QRBarcodeScanResult>(new QRBarcodeScanResult()
+            {
+                Code = endpoint
+            }));
+
+            var viewModel = Create.ViewModel.FinishInstallationViewModel(qrBarcodeScanService: scanner.Object);
+            await viewModel.ScanCommand.ExecuteAsync();
+
+
+            Assert.That(viewModel.Endpoint, Is.Null);
+        }
+
+        [Test]
+        public async Task when_scanning_barcode_that_returns_object()
+        {
+            var endpoint = "https://example.com";
+            var login = "username";
+
+            var scanner = new Mock<IQRBarcodeScanService>();
+            scanner.Setup(x => x.ScanAsync()).Returns(Task.FromResult<QRBarcodeScanResult>(new QRBarcodeScanResult()
+            {
+                Code = "dummy"
+            }));
+
+            var serializer = new Mock<ISerializer>();
+            serializer.Setup(x => x.Deserialize<FinishInstallationInfo>(It.IsAny<string>()))
+                .Returns(new FinishInstallationInfo()
+                {
+                    Url = endpoint,
+                    Login = login
+                });
+
+            var viewModel = Create.ViewModel.FinishInstallationViewModel(qrBarcodeScanService: scanner.Object,
+                serializer: serializer.Object);
+            await viewModel.ScanCommand.ExecuteAsync();
+
+            Assert.That(viewModel.Endpoint, Is.EqualTo(endpoint));
+            Assert.That(viewModel.UserName, Is.EqualTo(login));
+        }
+    }
+}
