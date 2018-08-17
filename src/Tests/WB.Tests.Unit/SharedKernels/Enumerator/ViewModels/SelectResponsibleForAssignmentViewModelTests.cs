e9d4f12e553e3a372340ec897ef7e84b878ec2e4
Roman Lytvyn <litvin.roma@gmail.com>
2018-08-17 13:32:17 +0300

    KP-11747 Apply design for assign dialog. added empty tests for green build

---
+++ src/Tests/WB.Tests.Unit/SharedKernels/Enumerator/ViewModels/SelectResponsibleForAssignmentViewModelTests.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using WB.Core.SharedKernels.Enumerator.ViewModels;
+
+namespace WB.Tests.Unit.SharedKernels.Enumerator.ViewModels
+{
+    [TestOf(typeof(SelectResponsibleForAssignmentViewModel))]
+    internal class SelectResponsibleForAssignmentViewModelTests
+    {
+        [Test]
+        public void when_prepare_and_reaasign_for_interview_should_list_of_interviewers_doesnot_exist_responsible_of_interview()
+        {
+            // arrange
+            // act
+            // assert
+        }
+
+        [Test]
+        public void when_prepare_and_reaasign_for_assignement_should_list_of_interviewers_doesnot_exist_responsible_of_assignment()
+        {
+            // arrange
+            // act
+            // assert
+        }
+
+        [Test]
+        public void when_prepare_and_interviewer_has_2_assignments_and_1_interview_by_one_of_them_and_2_rejected_by_sv_interviews_then_InterviewsCount_should_be_4()
+        {
+            // arrange
+            // act
+            // assert
+        }
+
+        [Test]
+        public void when_reassign_interview_then_responsible_of_interview_should_be_changed()
+        {
+            // arrange
+            // act
+            // assert
+        }
+
+        [Test]
+        public void when_reassign_assignment_then_responsible_of_assignment_should_be_changed()
+        {
+            // arrange
+            // act
+            // assert
+        }
+    }
+}
