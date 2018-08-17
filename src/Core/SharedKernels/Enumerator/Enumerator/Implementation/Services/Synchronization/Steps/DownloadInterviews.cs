273f28d3dc82a77c1b2ae016f3e322697168ff33
Slava Scherbak <slava.scherbak@gmail.com>
2018-08-17 12:28:08 +0300

    KP-11678: added test, implemented core review requests

--- src/Core/SharedKernels/Enumerator/Enumerator/Implementation/Services/Synchronization/Steps/DownloadInterviews.cs
+++ src/Core/SharedKernels/Enumerator/Enumerator/Implementation/Services/Synchronization/Steps/DownloadInterviews.cs
@@ -63,8 +63,8 @@ namespace WB.Core.SharedKernels.Enumerator.Implementation.Services.Synchronizati
             IEnumerable<Guid> obsoleteInterviews = await this.FindObsoleteInterviewsAsync(localInterviews, remoteInterviews, this.Context.Progress, this.Context.CancellationToken);
 
             var localInterviewIdsToRemove = localInterviewsToRemove
-                .Select(interview => interview.InterviewId)
                 .Where(IsNotPresentOnHq)
+                .Select(interview => interview.InterviewId)
                 .Concat(obsoleteInterviews)
                 .ToArray();
 
@@ -140,9 +140,9 @@ namespace WB.Core.SharedKernels.Enumerator.Implementation.Services.Synchronizati
         }
 
         
-        private bool IsNotPresentOnHq(Guid interviewId)
+        private bool IsNotPresentOnHq(InterviewView interview)
         {
-            return interviewSequenceViewRepository.GetById(interviewId) != null;
+            return interview.FromHqSyncDateTime == null;
         }
 
         private bool ShouldBeDownloadedBasedOnEventSequence(InterviewApiView interview)
