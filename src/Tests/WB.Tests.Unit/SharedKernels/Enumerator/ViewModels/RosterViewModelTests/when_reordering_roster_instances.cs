64e4f8bb986fbb264f8dbbeca6291e0d6c19193c
Slava Scherbak <slava.scherbak@gmail.com>
2018-08-22 16:32:17 +0300

    KP-11785: fixed bug with reordering roster instances on UI

--- src/Tests/WB.Tests.Unit/SharedKernels/Enumerator/ViewModels/RosterViewModelTests/when_reordering_roster_instances.cs
+++ src/Tests/WB.Tests.Unit/SharedKernels/Enumerator/ViewModels/RosterViewModelTests/when_reordering_roster_instances.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using Main.Core.Entities.Composite;
 using Main.Core.Entities.SubEntities;
@@ -10,54 +9,10 @@ using WB.Tests.Abc;
 namespace WB.Tests.Unit.SharedKernels.Enumerator.ViewModels.RosterViewModelTests
 {
     [TestFixture]
-    internal class when_roster_instances_removed : RosterViewModelTests
-    {
-        [Test]
-        public async Task should_remove_view_models()
-        {
-            var rosterId = Guid.Parse("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-            var chapterId = Guid.Parse("BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB");
-            var textListQuestionId = Guid.Parse("CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC");
-            var interviewerId = Guid.Parse("DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD");
-
-
-            var questionnaire = Create.Entity.QuestionnaireDocumentWithOneChapter(chapterId,
-                Create.Entity.TextListQuestion(textListQuestionId),
-                Create.Entity.Roster(rosterId, rosterSizeSourceType: RosterSizeSourceType.Question, rosterSizeQuestionId: textListQuestionId));
-
-            var interview = Setup.StatefulInterview(questionnaire);
-            interview.AnswerTextListQuestion(interviewerId, textListQuestionId, RosterVector.Empty, DateTime.UtcNow,
-                new[]
-                {
-                    new Tuple<decimal, string>(1, "option 1"),
-                    new Tuple<decimal, string>(5, "option 5"),
-                });
-
-            var statefulInterviewRepository = Create.Fake.StatefulInterviewRepositoryWith(interview);
-            var viewModel = this.CreateViewModel(interviewRepository: statefulInterviewRepository);
-            var navigationState = Create.Other.NavigationState(statefulInterviewRepository);
-
-            await navigationState.NavigateTo(Create.Entity.NavigationIdentity(Identity.Create(chapterId, RosterVector.Empty)));
-            viewModel.Init(null, Create.Entity.Identity(rosterId), navigationState);
-
-            interview.AnswerTextListQuestion(interviewerId, textListQuestionId, RosterVector.Empty, DateTime.UtcNow,
-                new[]
-                {
-                    new Tuple<decimal, string>(5, "option 5"),
-                });
-
-            viewModel.Handle(Create.Event.RosterInstancesRemoved(rosterId, new [] { Create.Entity.RosterVector(1)}));
-
-            Assert.That(viewModel.RosterInstances.Select(x => x.Identity).ToArray(),
-                Is.EquivalentTo(new[] {Identity.Create(rosterId, Create.Entity.RosterVector(5))}));
-        }
-    }
-
-    [TestFixture]
     internal class when_reordering_roster_instances : RosterViewModelTests
     {
         [Test]
-        public async Task should_remove_view_models()
+        public async Task should_reorder_roster_instances_in_the_list()
         {
             var questionnaire = Create.Entity.QuestionnaireDocumentWithOneChapter(Id.gA, children: new IComposite[]
             {
