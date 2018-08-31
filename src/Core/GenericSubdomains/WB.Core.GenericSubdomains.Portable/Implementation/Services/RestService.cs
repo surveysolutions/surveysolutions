88d364b36e12fdf98a6d2e44905203d9b4543d67
Pavel Stovpivskyi <sby@rambler.ru>
2018-08-31 14:43:53 +0300

    KP-11809 return dispose after merge

--- src/Core/GenericSubdomains/WB.Core.GenericSubdomains.Portable/Implementation/Services/RestService.cs
+++ src/Core/GenericSubdomains/WB.Core.GenericSubdomains.Portable/Implementation/Services/RestService.cs
@@ -232,6 +232,11 @@ namespace WB.Core.GenericSubdomains.Portable.Implementation.Services
 
                 throw new RestException(message: "Unexpected web exception", innerException: ex);
             }
+            finally
+            {
+                httpClient.Dispose();
+                messageHandler.Dispose();
+            }
         }
 
         public Task GetAsync(string url, object queryString, RestCredentials credentials, bool forceNoCache, 
