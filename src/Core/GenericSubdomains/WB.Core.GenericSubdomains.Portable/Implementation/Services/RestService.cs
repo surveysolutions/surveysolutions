aa2016278ede3dab123c3c6fdca549a9b5360e85
Pavel Stovpivskyi <sby@rambler.ru>
2018-08-31 12:17:06 +0300

    KP-11809 added dispose on messageHandler

--- src/Core/GenericSubdomains/WB.Core.GenericSubdomains.Portable/Implementation/Services/RestService.cs
+++ src/Core/GenericSubdomains/WB.Core.GenericSubdomains.Portable/Implementation/Services/RestService.cs
@@ -89,7 +89,8 @@ namespace WB.Core.GenericSubdomains.Portable.Implementation.Services
 
             var fullUrl = new Url(this.restServiceSettings.Endpoint, url, queryString);
 
-            var httpClient = new HttpClient(new ExtendedMessageHandler(new HttpClientHandler(), httpStatistician));
+            var messageHandler = new ExtendedMessageHandler(new HttpClientHandler(), httpStatistician);
+            var httpClient = new HttpClient(messageHandler);
             httpClient.Timeout = this.restServiceSettings.Timeout;
 
             var request = new HttpRequestMessage()
@@ -198,6 +199,7 @@ namespace WB.Core.GenericSubdomains.Portable.Implementation.Services
             finally
             {
                 httpClient.Dispose();
+                messageHandler.Dispose();
             }
         }
 
