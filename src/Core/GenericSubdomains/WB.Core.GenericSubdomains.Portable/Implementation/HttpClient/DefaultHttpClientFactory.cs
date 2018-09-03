7e7ab9bcc4c8df44a805ffcd52cf877842aeab8a
Pavel Stovpivskyi <sby@rambler.ru>
2018-09-03 14:59:02 +0300

    KP-11796 create HttpClient in WB.UI.Shared.Enumerator

--- src/Core/GenericSubdomains/WB.Core.GenericSubdomains.Portable/Implementation/HttpClient/DefaultHttpClientFactory.cs
+++ src/Core/GenericSubdomains/WB.Core.GenericSubdomains.Portable/Implementation/HttpClient/DefaultHttpClientFactory.cs
@@ -5,6 +5,11 @@ namespace WB.Core.GenericSubdomains.Portable.Implementation
 {
     public class DefaultHttpClientFactory : IHttpClientFactory
     {
+        public virtual HttpClient CreateClient(Url url, HttpMessageHandler handler, IHttpStatistician statistician)
+        {
+            return new HttpClient(new ExtendedMessageHandler(handler, statistician));
+        }
+
         public virtual HttpMessageHandler CreateMessageHandler()
         {
             return new HttpClientHandler();
