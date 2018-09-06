c19e8d32d9ccc2d4abbdad08a1d25ebdf382e468
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2018-09-06 11:15:52 +0300

    KP-11821 tool to migrate existing history

--- src/Tools/ShrinkQuestionnaireHistory/ShrinkQuestionnaireHistory/Program.cs
+++ src/Tools/ShrinkQuestionnaireHistory/ShrinkQuestionnaireHistory/Program.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Dapper;
-using Fclp;
 using Npgsql;
 
 namespace ShrinkQuestionnaireHistory
@@ -28,57 +29,58 @@ namespace ShrinkQuestionnaireHistory
         public string QuestionnaireId { get; set; }
     }
 
-
     [Localizable(false)]
     class Program
     {
         static async Task Main(string[] args)
         {
-            var parser = new FluentCommandLineParser<ApplicationArguments>();
-            parser.Setup(x => x.ConnectionString)
-                .As("cs")
-                .Required();
-
-            var parsedArguments = parser.Parse(args);
+            var connectionString = new NpgsqlConnectionStringBuilder(args[0]);
 
             var patcher = new DiffMatchPatch.diff_match_patch();
-
-            if (parsedArguments.HasErrors == false)
+            using (var connection = new NpgsqlConnection(connectionString.ConnectionString))
             {
-                using (var connection = new NpgsqlConnection(parser.Object.ConnectionString))
+                await connection.OpenAsync();
+
+                var questionnairesWithHistoryIds = (await connection.QueryAsync<QuestionnaireIdRecord>(
+                        "SELECT DISTINCT questionnaireid FROM plainstore.questionnairechangerecords WHERE resultingquestionnairedocument IS NOT NULL")
+                    ).ToList();
+
+                Console.WriteLine($"Found {questionnairesWithHistoryIds.Count} to migrate");
+
+                int processedCount = 0;
+                Stopwatch watch = Stopwatch.StartNew();
+
+                Parallel.ForEach(questionnairesWithHistoryIds, row =>
                 {
-                    await connection.OpenAsync();
-
-                    var questionnairesWithHistoryIds = (await connection.QueryAsync<QuestionnaireIdRecord>(
-                        "SELECT DISTINCT questionnaireid FROM plainstore.questionnairechangerecords WHERE resultingquestionnairedocument IS NOT NULL")).ToList();
-
-                    Console.WriteLine($"Found {questionnairesWithHistoryIds.Count} to migrate");
-
-                    foreach (var row in questionnairesWithHistoryIds)
+                    using (var localConnection = new NpgsqlConnection(connectionString.ConnectionString))
                     {
-                        using (var transaction = connection.BeginTransaction(IsolationLevel.RepeatableRead))
+                        localConnection.Open();
+                        using (var transaction = localConnection.BeginTransaction(IsolationLevel.RepeatableRead))
                         {
                             try
                             {
-                                var existingHistory = await connection.QueryAsync<ChangeRecord>(
+                                var existingHistory = localConnection.Query<ChangeRecord>(
                                     @"SELECT id as Id, resultingquestionnairedocument as Questionnaire 
-                                  FROM plainstore.questionnairechangerecords 
-                                  WHERE questionnaireid = @questionnaireId AND resultingquestionnairedocument IS NOT NULL
-                                  ORDER BY ""sequence""", new {row.QuestionnaireId});
+                                          FROM plainstore.questionnairechangerecords 
+                                          WHERE questionnaireid = @questionnaireId AND resultingquestionnairedocument IS NOT NULL
+                                          ORDER BY ""sequence""", new {row.QuestionnaireId});
+
                                 string reference = existingHistory.First().Questionnaire;
 
                                 foreach (ChangeRecord historyItem in existingHistory.Skip(1))
                                 {
-                                    var diff = patcher.patch_make(reference, historyItem.Questionnaire ?? string.Empty);
+                                    var diff = patcher.patch_make(reference,
+                                        historyItem.Questionnaire ?? string.Empty);
                                     string textPatch = patcher.patch_toText(diff);
 
                                     var appliedPatch = patcher.patch_apply(diff, reference)[0].ToString();
                                     if (!appliedPatch.Equals(historyItem.Questionnaire))
                                     {
-                                        throw new Exception($"Applied patch for questionnaire {row}. Applied patch does not match target questionnaire. Tested change id {historyItem.Id}"); await transaction.RollbackAsync();
+                                        throw new Exception(
+                                            $"Applied patch for questionnaire {row}. Applied patch does not match target questionnaire. Tested change id {historyItem.Id}");
                                     }
 
-                                    await connection.ExecuteAsync(@"UPDATE plainstore.questionnairechangerecords 
+                                    localConnection.Execute(@"UPDATE plainstore.questionnairechangerecords 
                                                                     SET resultingquestionnairedocument = NULL, diffwithpreviousversion = @diff 
                                                                     WHERE id = @id",
                                         new
@@ -90,17 +92,26 @@ namespace ShrinkQuestionnaireHistory
                                     reference = historyItem.Questionnaire;
                                 }
 
-                                await transaction.CommitAsync();
+                                transaction.Commit();
+                                Interlocked.Increment(ref processedCount);
+
+                                if (processedCount % 100 == 0)
+                                {
+                                    Console.WriteLine(
+                                        $"Processed {processedCount} questionnaires.Elapsed {watch.Elapsed:g}");
+                                }
                             }
                             catch (Exception e)
                             {
                                 Console.WriteLine(e.Message);
-                                await transaction.RollbackAsync();
+                                transaction.Rollback();
                             }
                         }
                     }
-                }
+                });
             }
+
+            Console.WriteLine("Done migration");
         }
     }
 }
