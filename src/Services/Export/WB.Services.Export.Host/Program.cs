996ab72dcb4d61b2095ba02301d8bd4554ea5f1d
Victor Hladkikh <me@alfeg.net>
2018-09-06 18:05:58 +0300

    KP-11819: Initial project added

---
+++ src/Services/Export/WB.Services.Export.Host/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace WB.Services.Export.Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var isService = !(Debugger.IsAttached || args.Contains("--console"));
+            var builder = CreateWebHostBuilder(args.Where(arg => arg != "--console").ToArray());
+
+            if (isService)
+            {
+                var pathToExe = Process.GetCurrentProcess().MainModule.FileName;
+                var pathToContentRoot = Path.GetDirectoryName(pathToExe);
+                builder.UseContentRoot(pathToContentRoot);
+            }
+
+            var host = builder.Build();
+
+            if (isService)
+            {
+                host.RunAsCustomService();
+            }
+            else
+            {
+                host.Run();
+            }
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    // Configure the app here.
+                })
+                .UseHttpSys(sys => { sys.UrlPrefixes.Add("http://+:80/export"); })
+                .UseStartup<Startup>();
+        }
+    }
+}
