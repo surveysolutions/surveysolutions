5fc394863902e23ef6a3eede656dc1af77d83e14
Andrii Kozhyn <hedgic@gmail.com>
2018-09-10 15:47:23 +0300

    KP-11824: Implemented feed for events reading from HQ

---
+++ src/UI/Headquarters/WB.UI.Headquarters/API/Filters/ExportServiceSettings.cs
@@ -0,0 +1,9 @@
+﻿using WB.Core.BoundedContexts.Headquarters.DataExport.Security;
+
+namespace WB.UI.Headquarters.API.Filters
+{
+    public class ExportServiceSettings : AppSetting
+    {
+        public string Key { get; set; }
+    }
+}
