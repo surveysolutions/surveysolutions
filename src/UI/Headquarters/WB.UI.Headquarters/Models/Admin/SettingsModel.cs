92a65b15b34dfcedcffa558988aec44b40661a5c
Andrii Kozhyn <hedgic@gmail.com>
2018-09-11 17:41:00 +0300

    KP-11827: Added token to settings in admin panel

---
+++ src/UI/Headquarters/WB.UI.Headquarters/Models/Admin/SettingsModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using WB.UI.Headquarters.API.Filters;
+using WB.UI.Shared.Web.Settings;
+
+namespace WB.UI.Headquarters.Models.Admin
+{
+    public class SettingsModel
+    {
+        public IEnumerable<ApplicationSetting> Settings { get; set; }
+
+        public ExportServiceSettings ExportSettings { get; set; }
+    }
+}
