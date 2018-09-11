0e990fd414ef34439b5451503e1d4d74f85254db
Victor Hladkikh <me@alfeg.net>
2018-09-11 13:01:18 +0300

    KP-11819: Able to work as windows service

---
+++ src/Services/Export/Tests/WB.Services.Export.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+//using System;
+//using WB.Services.Export.Host;
+//using Xunit;
+
+//namespace WB.Services.Export.Tests
+//{
+//    public class BasicTests
+//        : IClassFixture<WebApplicationFactory<Startup>>
+//    {
+//        private readonly WebApplicationFactory<Startup> _factory;
+
+//    }
+//}
