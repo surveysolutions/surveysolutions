1d9bb889d44894a11e5ccbe2b9c8947ba6434481
Victor Hladkikh <me@alfeg.net>
2018-09-11 13:01:18 +0300

    KP-11819: Able to work as windows service

---
+++ src/Services/Export/WB.Services.Export.Client/IExportServiceApi.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Refit;
+
+namespace WB.Services.Export.Client
+{
+    public interface IExportServiceApi
+    {
+        [Get("export")]
+        Task<string> Get(string tenant);
+    }
+
+    public static class DefaultClientFactory
+    {
+        public static IExportServiceApi ExportServiceApiClient(string baseUri) 
+            => RestService.For<IExportServiceApi>(baseUri + "api/v1/{tenant}/");
+    }
+}
---
+++ src/Services/Export/WB.Services.Export.Host/Controllers/ApiController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WB.Services.Export.Host.Controllers
+{
+    [Route("api/v1/{tenant}")]
+    [ApiController]
+    public class JobController : ControllerBase
+    {
+        public string Get()
+        {
+            return "Hello world, " + RouteData.Values["tenant"];
+        }
+    }
+}
