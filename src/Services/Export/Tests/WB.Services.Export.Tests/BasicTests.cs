b7d466c580b71bb4ce7cd2d82289b3e4d9d2fb62
Victor Hladkikh <me@alfeg.net>
2018-09-12 13:02:33 +0300

    KP-11819: Basic test to test build

---
+++ src/Services/Export/Tests/WB.Services.Export.Tests/BasicTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using NUnit.Framework;
+using WB.Services.Export.Host;
+
+namespace WB.Services.Export.Tests
+{
+    public class BasicTests
+    {
+        private readonly WebApplicationFactory<WB.Services.Export.Host.Startup> _factory;
+
+        public BasicTests()
+        {
+            _factory = new WebApplicationFactory<Startup>();
+        }
+
+        [Test]
+        public async Task Get_EndpointsReturnSuccessAndCorrectContentType()
+        {
+            // Arrange
+            
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync("/metrics");
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+
+        }
+    }
+}
