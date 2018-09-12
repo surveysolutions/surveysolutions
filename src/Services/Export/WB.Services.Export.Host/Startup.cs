d9ab38416ead5856e4a6b8474e0dbf9bde4177ea
Victor Hladkikh <me@alfeg.net>
2018-09-12 17:05:14 +0300

    KP-11819: Minimal depdendency configuration for AspNetCore

--- src/Services/Export/WB.Services.Export.Host/Startup.cs
+++ src/Services/Export/WB.Services.Export.Host/Startup.cs
@@ -19,7 +19,9 @@ namespace WB.Services.Export.Host
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvcCore()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
+                .AddJsonFormatters();            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
