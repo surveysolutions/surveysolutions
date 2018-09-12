31b0af62857419679a6ae6f7d7970268989ef074
Pavel Stovpivskyi <sby@rambler.ru>
2018-09-12 15:26:17 +0300

    KP-11738 retry only for error with "Unable to create application data"

--- src/Core/GenericSubdomains/WB.Core.GenericSubdomains.Portable/Implementation/HttpClient/ExtendedMessageHandler.cs
+++ src/Core/GenericSubdomains/WB.Core.GenericSubdomains.Portable/Implementation/HttpClient/ExtendedMessageHandler.cs
@@ -54,12 +54,17 @@ namespace WB.Core.GenericSubdomains.Portable.Implementation
             }
             catch (Exception ex)
             {
-                if (times > 0)
+                var sslHandshakeException = ex.GetSelfOrInner(e => e?.GetType().FullName == "Javax.Net.Ssl.SSLException");
+                var socketException = ex.GetSelfOrInner(e => e?.GetType().FullName == "Java.Net.SocketException");
+                if (sslHandshakeException != null || socketException != null)
                 {
-                    await Task.Delay(1000, cancellationToken);
-                    return await InnerSendAsync(call, request, cancellationToken, times - 1).ConfigureAwait(false);
+                    if (times > 0)
+                    {
+                        await Task.Delay(1000, cancellationToken);
+                        return await InnerSendAsync(call, request, cancellationToken, times - 1).ConfigureAwait(false);
+                    }
                 }
-                
+
                 throw new ExtendedMessageHandlerException(call, ex);
             }
         }
