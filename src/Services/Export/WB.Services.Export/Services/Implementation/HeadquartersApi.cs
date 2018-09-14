868e7ec56fb4673669c48e8b03ac6c1629381fd0
Andrii Kozhyn <hedgic@gmail.com>
2018-09-14 17:11:05 +0300

    KP-11851 trying to deserialize questionnaire

--- src/Services/Export/WB.Services.Export/Services/Implementation/HeadquartersApi.cs
+++ src/Services/Export/WB.Services.Export/Services/Implementation/HeadquartersApi.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
+using System.Reflection;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using WB.Services.Export.Interview;
 using WB.Services.Export.Questionnaire;
 using WB.Services.Export.Tenant;
@@ -52,8 +55,31 @@ namespace WB.Services.Export.Services.Implementation
             var responseString = await response.Content.ReadAsStringAsync();
             return JsonConvert.DeserializeObject<QuestionnaireDocument>(responseString, new JsonSerializerSettings
             {
-                TypeNameHandling = TypeNameHandling.All
+                SerializationBinder = new QuestionnaireDocumentSerializationBinder()
             });
+        }
+    }
+
+    internal class QuestionnaireDocumentSerializationBinder : DefaultSerializationBinder
+    {
+        private static readonly Assembly Assembly = typeof(QuestionnaireDocument).Assembly;
+        private static readonly Dictionary<string, Type> ResolvedTypes = new Dictionary<string, Type>();
+
+        public override Type BindToType(string assemblyName, string typeName)
+        {
+            if (!ResolvedTypes.ContainsKey(typeName))
+            {
+                var type = Assembly.GetTypes().FirstOrDefault(x => x.Name.Equals(typeName, StringComparison.Ordinal));
+                ResolvedTypes[typeName] = type;
+            }
+
+            return ResolvedTypes[typeName];
+        }
+
+        public override void BindToName(Type serializedType, out string assemblyName, out string typeName)
+        {
+            assemblyName = null;
+            typeName = serializedType.Name;
         }
     }
 }
