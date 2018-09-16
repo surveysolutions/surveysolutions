cf93de5f5f839d57ba289189f325f1e96232b601
Victor Hladkikh <me@alfeg.net>
2018-09-16 20:07:33 +0300

    KP-11851 Moved serialization binder in own file

---
+++ src/Services/Export/WB.Services.Export/Services/Implementation/QuestionnaireDocumentSerializationBinder.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Reflection;
+using Newtonsoft.Json.Serialization;
+using WB.Services.Export.Questionnaire;
+
+namespace WB.Services.Export.Services.Implementation
+{
+    internal class QuestionnaireDocumentSerializationBinder : DefaultSerializationBinder
+    {
+        private static readonly Assembly Assembly = typeof(QuestionnaireDocument).Assembly;
+        private static readonly ConcurrentDictionary<string, Type> ResolvedTypes = new ConcurrentDictionary<string, Type>();
+
+        public override Type BindToType(string assemblyName, string typeName)
+        {
+            return ResolvedTypes.GetOrAdd(typeName, tn =>
+            {
+                return Assembly.GetTypes().FirstOrDefault(x => x.IsPublic && x.Name.Equals(tn, StringComparison.Ordinal));
+            });
+        }
+
+        public override void BindToName(Type serializedType, out string assemblyName, out string typeName)
+        {
+            assemblyName = null;
+            typeName = serializedType.Name;
+        }
+    }
+}
