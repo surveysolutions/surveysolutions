059f8ea366e375c870ce2507c4ed041b9b582484
Victor Hladkikh <me@alfeg.net>
2018-09-16 20:07:33 +0300

    KP-11851 Moved serialization binder in own file

--- src/Services/Export/WB.Services.Export/Services/Implementation/HeadquartersApi.cs
+++ src/Services/Export/WB.Services.Export/Services/Implementation/HeadquartersApi.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
-using System.Reflection;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
 using WB.Services.Export.Interview;
 using WB.Services.Export.Questionnaire;
 using WB.Services.Export.Tenant;
@@ -61,29 +57,6 @@ namespace WB.Services.Export.Services.Implementation
             });
             questionnaireDocument.ConnectChildrenWithParent();
             return questionnaireDocument;
-        }
-    }
-
-    internal class QuestionnaireDocumentSerializationBinder : DefaultSerializationBinder
-    {
-        private static readonly Assembly Assembly = typeof(QuestionnaireDocument).Assembly;
-        private static readonly ConcurrentDictionary<string, Type> ResolvedTypes = new ConcurrentDictionary<string, Type>();
-
-        public override Type BindToType(string assemblyName, string typeName)
-        {
-            if (!ResolvedTypes.ContainsKey(typeName))
-            {
-                var type = Assembly.GetTypes().FirstOrDefault(x => x.IsPublic && x.Name.Equals(typeName, StringComparison.Ordinal));
-                ResolvedTypes[typeName] = type;
-            }
-
-            return ResolvedTypes[typeName];
-        }
-
-        public override void BindToName(Type serializedType, out string assemblyName, out string typeName)
-        {
-            assemblyName = null;
-            typeName = serializedType.Name;
         }
     }
 }
