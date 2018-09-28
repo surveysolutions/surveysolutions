910f90f28ce553543079bbf4a0d6d40fe547490d
Victor Hladkikh <me@alfeg.net>
2018-09-28 14:58:01 +0300

    KP-11925 Crash Supervisor app on sync with IN app # Downloading app updates in chunks

---
+++ src/Core/SharedKernels/Enumerator/Enumerator/OfflineSync/Messages/IChunkedByteArrayResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace WB.Core.SharedKernels.Enumerator.OfflineSync.Messages
+{
+    public interface IChunkedByteArrayResponse : ICommunicationMessage
+    {
+        byte[] Content { get; set; }
+        long Skip { get; set; }
+        int Length { get; set; }
+        long Total { get; set; }
+    }
+}
