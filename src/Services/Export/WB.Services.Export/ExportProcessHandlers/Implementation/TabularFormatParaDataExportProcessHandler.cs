3cda9809f41ab1d0dce5befa5aaefd9e8fbe6455
Victor Hladkikh <me@alfeg.net>
2018-10-02 13:53:21 +0300

    KP-11931 Move Paradata to Export Service # Cleanup

--- src/Services/Export/WB.Services.Export/ExportProcessHandlers/Implementation/TabularFormatParaDataExportProcessHandler.cs
+++ src/Services/Export/WB.Services.Export/ExportProcessHandlers/Implementation/TabularFormatParaDataExportProcessHandler.cs
@@ -1,73 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using WB.Services.Export.CsvExport;
 using WB.Services.Export.Infrastructure;
 using WB.Services.Export.Interview;
-using WB.Services.Export.Questionnaire;
-using WB.Services.Export.Questionnaire.Services;
 using WB.Services.Export.Services;
 using WB.Services.Export.Services.Processing;
 using WB.Services.Export.Utils;
 
 namespace WB.Services.Export.ExportProcessHandlers.Implementation
 {
-    public enum InterviewHistoricalAction
-    {
-        SupervisorAssigned,
-        InterviewerAssigned,
-        AnswerSet,
-        AnswerRemoved,
-        CommentSet,
-        Completed,
-        Restarted,
-        ApproveBySupervisor,
-        ApproveByHeadquarter,
-        RejectedBySupervisor,
-        RejectedByHeadquarter,
-        Deleted,
-        Restored,
-        QuestionEnabled,
-        QuestionDisabled,
-        GroupEnabled,
-        GroupDisabled,
-        QuestionDeclaredValid,
-        QuestionDeclaredInvalid,
-        UnapproveByHeadquarters,
-        ReceivedByInterviewer,
-        ReceivedBySupervisor,
-        VariableSet,
-        VariableEnabled,
-        VariableDisabled,
-        Paused,
-        Resumed,
-        KeyAssigned,
-        TranslationSwitched,
-        OpenedBySupervisor,
-        ClosedBySupervisor
-    }
-
-    public class InterviewHistoryView
-    {
-        public Guid InterviewId { get; set; }
-        public List<InterviewHistoricalRecordView> Records { get; set; }
-    }
-
-    public class InterviewHistoricalRecordView
-    {
-        public InterviewHistoricalAction Action { get; set; }
-        public string OriginatorName { get; set; }
-        public string OriginatorRole { get; set; }
-        public Dictionary<string, string> Parameters { get; set; }
-        public DateTime? Timestamp { get; set; }
-        public TimeSpan? Offset { get; set; }
-    }
-
     internal class TabularFormatParaDataExportProcessHandler : AbstractDataExportHandler
     {
         private readonly ICsvWriter csvWriter;
@@ -117,31 +62,8 @@ namespace WB.Services.Export.ExportProcessHandlers.Implementation
                 writer.WriteField("offset");
                 writer.WriteField("parameters");
                 writer.NextRecord();
-
-                void WriteParadata(InterviewHistoryView paradata)
-                {
-                    lock (writer)
-                    {
-                        for (int i = 0; i < paradata.Records.Count; i++)
-                        {
-                            var record = paradata.Records[i];
-
-                            writer.WriteField(paradata.InterviewId);
-                            writer.WriteField(i + 1);
-                            writer.WriteField(record.Action);
-                            writer.WriteField(record.OriginatorName);
-                            writer.WriteField(record.OriginatorRole);
-                            writer.WriteField(record.Timestamp?.ToString("s", CultureInfo.InvariantCulture) ?? "");
-                            writer.WriteField(record.Offset != null ? record.Offset.Value.ToString() : "");
-                            writer.WriteField(
-                                String.Join("||", record.Parameters.Values.Select(v => v.RemoveNewLine())));
-
-                            writer.NextRecord();
-                        }
-                    }
-                }
-
-                Parallel.ForEach(interviewsToExport.Batch(10), interviews =>
+               
+                Parallel.ForEach(interviewsToExport.Batch(interviewDataExportSettings.Value.ParadataQueryLimit), interviews =>
                 {
                     logger.LogTrace($"Query headquarters for interviews history");
                     var historyItems = api.GetInterviewsHistory(interviews.Select(i => i.Id).ToArray()).Result;
@@ -149,30 +71,37 @@ namespace WB.Services.Export.ExportProcessHandlers.Implementation
 
                     foreach (InterviewHistoryView paradata in historyItems)
                     {
-                        WriteParadata(paradata);
+                        WriteParadata(writer, paradata);
                     }
 
                     totalInterviewsProcessed += historyItems.Count;
                     progress.Report(totalInterviewsProcessed.PercentOf(interviewsToExport.Count));
                 });
-
-                //foreach (var interviews in interviewsToExport
-                //    .Batch(interviewDataExportSettings.Value.MaxRecordsCountPerOneExportQuery / 10))
-                //{
-                //    logger.LogTrace($"Query headquarters for interviews history");
-                //    var historyItems = await api.GetInterviewsHistory(interviews.Select(i => i.Id).ToArray());
-                //    logger.LogTrace($"Query headquarters for interviews history. Got {historyItems.Count} items");
-
-                //    foreach (InterviewHistoryView paradata in historyItems)
-                //    {
-                //        WriteParadata(paradata);
-                //    }
-
-                //    totalInterviewsProcessed += historyItems.Count;
-                //    progress.Report(totalInterviewsProcessed.PercentOf(interviewsToExport.Count));
-                //}
             }
+
             logger.LogInformation("Completed paradata export for " + settings);
+        }
+
+        void WriteParadata(ICsvWriterService writer, InterviewHistoryView paradata)
+        {
+            lock (writer)
+            {
+                for (int i = 0; i < paradata.Records.Count; i++)
+                {
+                    var record = paradata.Records[i];
+
+                    writer.WriteField(paradata.InterviewId);
+                    writer.WriteField(i + 1);
+                    writer.WriteField(record.Action);
+                    writer.WriteField(record.OriginatorName);
+                    writer.WriteField(record.OriginatorRole);
+                    writer.WriteField(record.Timestamp?.ToString("s", CultureInfo.InvariantCulture) ?? "");
+                    writer.WriteField(record.Offset != null ? record.Offset.Value.ToString() : "");
+                    writer.WriteField(String.Join("||", record.Parameters.Values.Select(v => v.RemoveNewLine())));
+
+                    writer.NextRecord();
+                }
+            }
         }
     }
 }
