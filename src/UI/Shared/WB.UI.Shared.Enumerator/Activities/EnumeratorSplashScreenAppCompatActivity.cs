a5490b06f3f10e47ab18822cabb4783ed6df4ae7
Roman Lytvyn <litvin.roma@gmail.com>
2018-10-03 20:40:34 +0300

    KP-11907 Implement encryption on tablet. Encrypt old data

--- src/UI/Shared/WB.UI.Shared.Enumerator/Activities/EnumeratorSplashScreenAppCompatActivity.cs
+++ src/UI/Shared/WB.UI.Shared.Enumerator/Activities/EnumeratorSplashScreenAppCompatActivity.cs
@@ -1,8 +1,14 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Threading.Tasks;
 using MvvmCross.Droid.Support.V7.AppCompat;
 using MvvmCross.ViewModels;
+using SQLite;
 using WB.Core.GenericSubdomains.Portable.ServiceLocation;
+using WB.Core.Infrastructure.FileSystem;
+using WB.Core.SharedKernels.Enumerator.Implementation.Services;
 using WB.Core.SharedKernels.Enumerator.Services;
+using WB.Core.SharedKernels.Enumerator.Services.Infrastructure.Storage;
+using WB.Core.SharedKernels.Enumerator.Views;
 
 namespace WB.UI.Shared.Enumerator.Activities
 {
@@ -25,20 +31,66 @@ namespace WB.UI.Shared.Enumerator.Activities
             var settings = ServiceLocator.Current.GetInstance<IEnumeratorSettings>();
             if (!settings.Encrypted)
             {
-                var encryptionProvider = ServiceLocator.Current.GetInstance<IEncryptionService>();
+                var encryptionService = ServiceLocator.Current.GetInstance<IEncryptionService>();
 
-                encryptionProvider.GenerateKeys();
+                encryptionService.GenerateKeys();
 
-                this.EncryptDatabase(encryptionProvider);
+                this.EncryptDatabase(encryptionService);
 
                 //settings.SetEncrypted(true);
             }
         }
 
-        private void EncryptDatabase(IEncryptionService encryptionProvider)
+        private void EncryptDatabase(IEncryptionService encryptionService)
         {
-            var encryptedText = encryptionProvider.Encrypt("Привет мир!");  
-            var decryptedText = encryptionProvider.Decrypt(encryptedText);
+            var identifyingQuestionsRepository = ServiceLocator.Current.GetInstance<IPlainStorage<PrefilledQuestionView>>();
+            var identifyingQuestions = identifyingQuestionsRepository.LoadAll();
+
+            foreach (var identifyingQuestion in identifyingQuestions)
+            {
+                identifyingQuestion.EncryptedQuestionText = encryptionService.Encrypt(identifyingQuestion.QuestionText);
+                identifyingQuestion.EncryptedAnswer = encryptionService.Encrypt(identifyingQuestion.Answer);
+
+                identifyingQuestionsRepository.Store(identifyingQuestion);
+            }
+
+            var assignmentAnswerRepository = ServiceLocator.Current.GetInstance<IAssignmentDocumentsStorage>();
+            var assignments = assignmentAnswerRepository.LoadAll();
+
+            foreach (var assignment in assignments)
+            {
+                var assignmentAnswers = assignmentAnswerRepository.FetchPreloadedData(assignment).Answers;
+
+                foreach (var assignmentAnswer in assignmentAnswers)
+                {
+                    assignmentAnswer.EncryptedQuestion = encryptionService.Encrypt(assignmentAnswer.Question);
+                    assignmentAnswer.EncryptedAnswerAsString = encryptionService.Encrypt(assignmentAnswer.AnswerAsString);
+                    assignmentAnswer.EncryptedSerializedAnswer = encryptionService.Encrypt(assignmentAnswer.SerializedAnswer);
+                }
+
+                assignmentAnswerRepository.Store(assignment);
+            }
+
+            var eventsSettings = ServiceLocator.Current.GetInstance<SqliteSettings>();
+            var fileAccessor = ServiceLocator.Current.GetInstance<IFileSystemAccessor>();
+
+            foreach (var interviewFile in fileAccessor.GetFilesInDirectory(eventsSettings.PathToInterviewsDirectory))
+            {
+                var sqConnection = new SQLiteConnectionString(interviewFile, true);
+                var connection = new SQLiteConnectionWithLock(sqConnection,
+                    openFlags: SQLiteOpenFlags.ReadWrite | SQLiteOpenFlags.FullMutex);
+
+                using (connection.Lock())
+                {
+                    var events = connection.Table<EventView>().ToList();
+                    foreach (var eventView in events)
+                    {
+                        eventView.EncryptedJsonEvent = encryptionService.Encrypt(eventView.JsonEvent);
+                    }
+
+                    connection.UpdateAll(events);
+                }
+            }
         }
     }
 }
