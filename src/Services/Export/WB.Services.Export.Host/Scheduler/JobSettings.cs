e9fb8b2636e3a798b1c5e5fefb98b1ca11c77573
Victor Gladkikh <me@alfeg.net>
2018-10-04 17:16:49 +0300

    KP-11909 Replace Hangfire with other worker queue # Using EF Core for queue

--- src/Services/Export/WB.Services.Export.Host/Scheduler/JobSettings.cs
+++ src/Services/Export/WB.Services.Export.Host/Scheduler/JobSettings.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 
-namespace WB.Services.Export.Host.Scheduler.PostgresWorkQueue
+namespace WB.Services.Export.Host.Scheduler
 {
     public class JobSettings
     {
         public string ConnectionName { get; set; } = "DefaultConnection";
         public int WorkerCount { get; set; } = Environment.ProcessorCount;
         public string SchemaName { get; set; } = "scheduler";
-        public int WorkerCountPerTenant { get; set; } = 2;
+        public int WorkerCountPerTenant { get; set; } = 1;
     }
 }
