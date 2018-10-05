a7a17ca2f30d2f3da87415ebe64d252be61d7d4d
Pavel Stovpivskyi <sby@rambler.ru>
2018-10-05 15:54:39 +0300

    KP-11894 removed old methods

--- src/Infrastructure/WB.Infrastructure.Native/Storage/Postgre/IUnitOfWork.cs
+++ src/Infrastructure/WB.Infrastructure.Native/Storage/Postgre/IUnitOfWork.cs
@@ -7,12 +7,5 @@ namespace WB.Infrastructure.Native.Storage.Postgre
     {
         void AcceptChanges();
         ISession Session { get; }
-
-        [Obsolete]
-        T ExecuteInQueryTransaction<T>(Func<T> func);
-        [Obsolete]
-        T ExecuteInPlainTransaction<T>(Func<T> func);
-        [Obsolete]
-        void ExecuteInPlainTransaction(Action action);
     }
 }
