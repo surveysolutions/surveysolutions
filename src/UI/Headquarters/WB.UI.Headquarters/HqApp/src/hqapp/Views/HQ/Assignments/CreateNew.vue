27af87367224d065b0ed5da5ddc80907f3a7e7c8
Andrii Kozhyn <hedgic@gmail.com>
2018-10-09 13:52:27 +0300

    KP-11904 Take new assignment renders questions list

--- src/UI/Headquarters/WB.UI.Headquarters/HqApp/src/hqapp/Views/HQ/Assignments/CreateNew.vue
+++ src/UI/Headquarters/WB.UI.Headquarters/HqApp/src/hqapp/Views/HQ/Assignments/CreateNew.vue
@@ -2,10 +2,14 @@
         <main class="web-interview web-interview-for-supervisor" >
             <div class="container-fluid">
                 <div class="row">
-                      <div id="questionsList" class="unit-section" :class="sectionClass">
-        
-                   <component v-for="entity in entities" :key="entity.identity" :is="entity.entityType" :id="entity.identity"></component>
-                      </div>
+                    <div class="unit-section complete-section">
+                        <div class="wrapper-info error">
+                            <div class="container-info">
+                                <h2> {{ questionnaireTitle }} </h2>
+                            </div>
+                        </div>
+                        <component v-for="entity in entities" :key="entity.identity" :is="entity.entityType" :id="entity.identity" fetchOnMount></component>
+                    </div>
                 </div>
             </div>
             <IdleTimeoutService />
@@ -16,12 +20,19 @@
 import Vue from "vue";
 
 export default {
-    computed: {},
+    computed: {
+        entities() {
+            return this.$store.state.takeNew.takeNew.entities
+        },
+        questionnaireTitle(){
+            return this.$store.state.takeNew.takeNew.interview.questionnaireTitle
+        }
+    },
 
     methods: {
         onResize() {
-            var screenWidth = document.documentElement.clientWidth;
-            this.$store.dispatch("screenWidthChanged", screenWidth);
+            var screenWidth = document.documentElement.clientWidth
+            this.$store.dispatch("screenWidthChanged", screenWidth)
         }
     },
 
@@ -31,28 +42,28 @@ export default {
                 interviewId: window.CONFIG.model.id,
                 review: false
             })
-            .then(() => this.$store.dispatch("loadTakeNew"));
+            .then(() => this.$store.dispatch("loadTakeNew"))
     },
 
     mounted() {
         const self = this;
 
         this.$nextTick(function() {
-            window.addEventListener("resize", self.onResize);
-            self.onResize();
+            window.addEventListener("resize", self.onResize)
+            self.onResize()
         });
     },
 
     updated() {
         Vue.nextTick(() => {
-            window.ajustNoticeHeight();
-            window.ajustDetailsPanelHeight();
+            window.ajustNoticeHeight()
+            window.ajustDetailsPanelHeight()
         });
     },
     components: {},
 
     beforeDestroy() {
-        window.removeEventListener("resize", this.onResize);
+        window.removeEventListener("resize", this.onResize)
     }
 };
 </script>
