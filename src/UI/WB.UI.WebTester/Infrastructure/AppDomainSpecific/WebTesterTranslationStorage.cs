bda3a2fc17165f1b9a88f36b4d653286da05ff6f
Pavel Stovpivskyi <sby@rambler.ru>
2018-10-09 13:54:59 +0300

    KP-11896 tests: split WebTesterTranslationService and WebTesterTranslationStorage

--- src/UI/WB.UI.WebTester/Infrastructure/AppDomainSpecific/WebTesterTranslationStorage.cs
+++ src/UI/WB.UI.WebTester/Infrastructure/AppDomainSpecific/WebTesterTranslationStorage.cs
@@ -1,29 +1,20 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using Main.Core.Documents;
-using WB.Core.GenericSubdomains.Portable.Services;
-using WB.Core.SharedKernels.DataCollection.Implementation.Entities;
-using WB.Core.SharedKernels.DataCollection.Repositories;
 using WB.Core.SharedKernels.Questionnaire.Translations;
 using WB.Core.SharedKernels.SurveySolutions.Documents;
 using WB.Enumerator.Native.Questionnaire;
 
-namespace WB.UI.WebTester.Infrastructure
+namespace WB.UI.WebTester.Infrastructure.AppDomainSpecific
 {
-    class WebTesterTranslationStorage : IWebTesterTranslationService
+    public class WebTesterTranslationStorage : IWebTesterTranslationStorage
     {
         private readonly IQuestionnaireTranslator translator;
         private List<TranslationInstance> translationsList;
-        private readonly IQuestionOptionsRepository questionOptionsRepository;
-        private readonly ISubstitutionService substitutionService;
 
-        public WebTesterTranslationStorage(IQuestionnaireTranslator translator, 
-            IQuestionOptionsRepository questionOptionsRepository,
-            ISubstitutionService substitutionService)
+        public WebTesterTranslationStorage(IQuestionnaireTranslator translator)
         {
             this.translator = translator;
-            this.questionOptionsRepository = questionOptionsRepository;
-            this.substitutionService = substitutionService;
         }
 
         public void Store(List<TranslationInstance> translations)
@@ -31,23 +22,27 @@ namespace WB.UI.WebTester.Infrastructure
             this.translationsList = translations;
         }
 
-        public PlainQuestionnaire Translate(QuestionnaireDocument questionnaire, long version, string language)
+        public QuestionnaireDocument GetTranslated(QuestionnaireDocument questionnaire, long version, string language, out Translation translation)
         {
-            Translation translationId = null;
+            translation = null;
             QuestionnaireDocument result = questionnaire;
 
             if (language != null)
             {
-                translationId = questionnaire.Translations.SingleOrDefault(t => t.Name == language);
+                translation = questionnaire.Translations.SingleOrDefault(t => t.Name == language);
 
-                var translation = translationId != null 
-                    ? new QuestionnaireTranslation(translationsList.Where(t => t.TranslationId == translationId.Id)) 
-                    : null;
+                QuestionnaireTranslation questionnaireTranslation = null;
 
-                result = this.translator.Translate(questionnaire, translation);
+                if (translation != null)
+                {
+                    var translationId = translation.Id;
+                    questionnaireTranslation = new QuestionnaireTranslation(translationsList.Where(t => t.TranslationId == translationId));
+                }
+
+                result = this.translator.Translate(questionnaire, questionnaireTranslation);
             }
 
-            return new PlainQuestionnaire(result, version, questionOptionsRepository, substitutionService, translationId);
+            return result;
         }
     }
 }
