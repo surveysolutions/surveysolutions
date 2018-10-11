a31d37f97311d8cf10885301cda5f6a6249354da
Roman Lytvyn <litvin.roma@gmail.com>
2018-10-11 15:10:19 +0300

    fixed build 2

--- src/Tests/WB.Tests.Unit/BoundedContexts/Supervisor/ViewModels/WaitingForSupervisorActionViewModelTests.cs
+++ src/Tests/WB.Tests.Unit/BoundedContexts/Supervisor/ViewModels/WaitingForSupervisorActionViewModelTests.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Moq;
+using MvvmCross.Base;
+using MvvmCross.Tests;
 using NUnit.Framework;
 using WB.Core.BoundedContexts.Supervisor.ViewModel.Dashboard;
 using WB.Core.BoundedContexts.Supervisor.ViewModel.Dashboard.Services;
@@ -9,8 +11,15 @@ using WB.Tests.Abc;
 namespace WB.Tests.Unit.BoundedContexts.Supervisor.ViewModels
 {
     [TestOf(typeof(WaitingForSupervisorActionViewModel))]
-    internal class WaitingForSupervisorActionViewModelTests
+    internal class WaitingForSupervisorActionViewModelTests : MvxIoCSupportingTest
     {
+        public WaitingForSupervisorActionViewModelTests()
+        {
+            base.Setup();
+
+            Ioc.RegisterSingleton(Create.Fake.MvxMainThreadAsyncDispatcher());
+        }
+
         [Test]
         public void when_getting_ui_items_and_view_model_has_last_visited_interview_id_then_view_model_should_have_specified_HighLightedItemIndex()
         {
