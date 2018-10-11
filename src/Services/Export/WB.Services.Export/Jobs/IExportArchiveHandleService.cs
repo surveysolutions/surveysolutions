b5e13b7a13ff7c79477308cd931aac820e2ac16f
Victor Hladkikh <me@alfeg.net>
2018-10-11 12:56:13 +0300

    Cleanup Services.Export project. Dispose cancellationtoken sources

---
+++ src/Services/Export/WB.Services.Export/Jobs/IExportArchiveHandleService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using WB.Services.Export.Interview;
+using WB.Services.Export.Services.Processing;
+using WB.Services.Infrastructure.Tenant;
+
+namespace WB.Services.Export.Jobs
+{
+    public interface IExportArchiveHandleService
+    {
+        Task<DataExportArchive> DownloadArchiveAsync(TenantInfo tenant, string archiveName,
+            DataExportFormat dataExportFormat, InterviewStatus? status,
+            DateTime? from, DateTime? to);
+
+        Task ClearAllExportArchives(TenantInfo tenant);
+    }
+}
