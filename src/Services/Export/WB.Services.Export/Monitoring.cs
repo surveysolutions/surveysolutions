3b9c066231b85a233b32ef886b388e409dc1b3ba
Victor Hladkikh <me@alfeg.net>
2018-10-22 16:53:50 +0300

    KP-11860 Add metrics to Services.Export # Outgoing requests monitoring

---
+++ src/Services/Export/WB.Services.Export/Monitoring.cs
@@ -0,0 +1,26 @@
+﻿using Prometheus;
+
+namespace WB.Services.Export
+{
+    public static class Monitoring
+    {
+        public static class Http
+        {
+            public const string HTTP_IN = "in";
+            public const string HTTP_OUT = "out";
+
+            public static Counter RequestsCounter = Prometheus.Metrics.CreateCounter(
+                "wb_http_total",
+                "Overall counter for handled requests", "direction", "tenant");
+
+            public static Counter DataServed = Prometheus.Metrics.CreateCounter(
+                "wb_http_bytes_count",
+                "Overall counter for handled requests bytes", "direction", "tenant");
+
+            public static Histogram Latency = Metrics.CreateHistogram(
+                "wb_http_latency",
+                "Histogram for requests latency", new[] {0, 0.2, 0.4, 0.6, 0.8, 0.9}
+                , "direction", "tenant");
+        }
+    }
+}
