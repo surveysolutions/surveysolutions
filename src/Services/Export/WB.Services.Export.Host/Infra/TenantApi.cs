d931908816d243c288c638435b69bd3f19272b8b
Victor Gladkikh <me@alfeg.net>
2018-10-22 16:53:50 +0300

    KP-11860 Add metrics to Services.Export # Outgoing requests monitoring

--- src/Services/Export/WB.Services.Export.Host/Infra/TenantApi.cs
+++ src/Services/Export/WB.Services.Export.Host/Infra/TenantApi.cs
@@ -4,8 +4,10 @@ using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.Net;
 using System.Net.Http;
+using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
+using Humanizer;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
@@ -44,7 +46,7 @@ namespace WB.Services.Export.Host.Infra
         {
             return cache.GetOrAdd(tenant, id =>
             {
-                var httpClient = new HttpClient(new ApiKeyHandler(tenant.Id, logger), true);
+                var httpClient = new HttpClient(new ApiKeyHandler(tenant, logger), true);
                 
                 httpClient.BaseAddress = new Uri(tenant.BaseUrl);
 
@@ -60,13 +62,13 @@ namespace WB.Services.Export.Host.Infra
 
         private class ApiKeyHandler : HttpClientHandler
         {
-            private readonly TenantId tenantId;
+            private readonly TenantInfo tenant;
             private readonly ILogger<TenantApi<T>> logger;
             private readonly RetryPolicy<HttpResponseMessage> policy;
 
-            public ApiKeyHandler(TenantId tenantId, ILogger<TenantApi<T>> logger)
+            public ApiKeyHandler(TenantInfo tenant, ILogger<TenantApi<T>> logger)
             {
-                this.tenantId = tenantId;
+                this.tenant = tenant;
                 this.logger = logger;
                 this.AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip;
                 this.policy = Policy
@@ -85,20 +87,48 @@ namespace WB.Services.Export.Host.Infra
             protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request,
                 CancellationToken cancellationToken)
             {
-                var uri = QueryHelpers.AddQueryString(request.RequestUri.ToString(), "apiKey", this.tenantId.Id);
+                var uri = QueryHelpers.AddQueryString(request.RequestUri.ToString(), "apiKey", this.tenant.Id.ToString());
                 request.RequestUri = new Uri(uri);
-                
+
                 var sw = Stopwatch.StartNew();
                 logger.LogDebug($"Calling {request.Method}: {request.RequestUri}");
 
                 var result = await this.policy.ExecuteAsync(() => base.SendAsync(request, cancellationToken));
-
                 sw.Stop();
 
-                logger.LogTrace($"Got {result.Content.Headers.ContentLength ?? 0}bytes in {sw.ElapsedMilliseconds}ms: {uri}");
+                var size = result.Content.Headers.ContentLength ?? GetRawSizeUsingReflection(result) ?? 0;
+                
+                Monitoring.Http.RequestsCounter
+                    .Labels(Monitoring.Http.HTTP_OUT, this.tenant.Name ?? this.tenant.Id.ToString()).Inc();
+
+                if (size != 0)
+                {
+                    Monitoring.Http.DataServed
+                        .Labels(Monitoring.Http.HTTP_OUT, this.tenant.Name ?? this.tenant.Id.ToString()).Inc(size);
+                }
+
+                Monitoring.Http.Latency
+                    .Labels(Monitoring.Http.HTTP_OUT, this.tenant.Name ?? this.tenant.Id.ToString()).Observe(sw.Elapsed.TotalSeconds);
+
+                logger.LogInformation($"Got {size.Bytes().Humanize("#.##")} in {sw.ElapsedMilliseconds}ms: {uri.Substring(0, 80)}...");
                 return result;
+            }
+
+            private static readonly ConcurrentDictionary<Type, FieldInfo> Cache = new ConcurrentDictionary<Type, FieldInfo>();
+            private readonly IAsyncPolicy<HttpResponseMessage> policy;
+
+
+            private static long? GetRawSizeUsingReflection(HttpResponseMessage result)
+            {
+                var field = Cache.GetOrAdd(result.Content.GetType(), type =>
+                {
+                    var bindFlags = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static;
+                    return type.BaseType.GetField("_originalContent", bindFlags);
+                });
+
+                var val = field.GetValue(result.Content) as HttpContent;
+                return val?.Headers.ContentLength;
             }
         }
     }
-
 }
