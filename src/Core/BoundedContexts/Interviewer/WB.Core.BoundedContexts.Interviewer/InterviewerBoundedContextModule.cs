f8efa9d7fad108502d7b8fdc32c8178a8baf310a
Andrii Kozhyn <hedgic@gmail.com>
2018-10-23 11:22:50 +0300

    KP-11947 do not send same sync message twice

--- src/Core/BoundedContexts/Interviewer/WB.Core.BoundedContexts.Interviewer/InterviewerBoundedContextModule.cs
+++ src/Core/BoundedContexts/Interviewer/WB.Core.BoundedContexts.Interviewer/InterviewerBoundedContextModule.cs
@@ -24,7 +24,6 @@ namespace WB.Core.BoundedContexts.Interviewer
             registry.BindWithConstructorArgument<ISynchronizationStep, SynchronizeLogo>("sortOrder", 50);
             registry.BindWithConstructorArgument<ISynchronizationStep, SynchronizeAuditLog>("sortOrder", 60);
             registry.BindWithConstructorArgument<ISynchronizationStep, InterviewerUpdateApplication>("sortOrder", 70);
-            registry.BindWithConstructorArgument<ISynchronizationStep, SynchronizeAuditLog>("sortOrder", 80);
         }
 
         public Task Init(IServiceLocator serviceLocator, UnderConstructionInfo status)
