fcf01f287a63be37c84794b94efeeca9e34c1dbc
Andrii Kozhyn <hedgic@gmail.com>
2018-11-05 10:51:23 +0200

    KP-11727 reverted temporary changes

--- src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Views/Questionnaire/Edit/GroupDetailsView.cs
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Views/Questionnaire/Edit/GroupDetailsView.cs
@@ -7,7 +7,7 @@ namespace WB.Core.BoundedContexts.Designer.Views.Questionnaire.Edit
         public Guid Id { get; set; }
         public string Title { get; set; }
         public string EnablementCondition { get; set; }
-        public bool? HideIfDisabled { get; set; }
+        public bool HideIfDisabled { get; set; }
         public string VariableName { get; set; }
     }
 }
