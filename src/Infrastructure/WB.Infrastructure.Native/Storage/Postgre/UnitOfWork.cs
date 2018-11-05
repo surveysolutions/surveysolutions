e6ca5378fe6527e582f9c6127935a6f06211cdd9
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2018-11-05 17:58:42 -0500

    fix merge

--- src/Infrastructure/WB.Infrastructure.Native/Storage/Postgre/UnitOfWork.cs
+++ src/Infrastructure/WB.Infrastructure.Native/Storage/Postgre/UnitOfWork.cs
@@ -8,28 +8,25 @@ using WB.Core.GenericSubdomains.Portable.Services;
 
 namespace WB.Infrastructure.Native.Storage.Postgre
 {
-    [DebuggerDisplay("Id = {Id}")]
+    [DebuggerDisplay("Id = {SessionId}")]
     public sealed class UnitOfWork : IUnitOfWork
     {
-        private readonly ISessionFactory sessionFactory;
         private ISession session;
         private ITransaction transaction;
         private bool isDisposed = false;
         private static int Counter = 0;
-        public Guid? sessionId;
+        public Guid? SessionId;
 
         private readonly ILogger logger = ServiceLocator.Current.GetInstance<ILogger>();
 
         public UnitOfWork(ISessionFactory sessionFactory)
         {
-            this.sessionFactory = sessionFactory;
-
             if (session != null) throw new InvalidOperationException("Unit of work already started");
             if (isDisposed == true) throw new ObjectDisposedException(nameof(UnitOfWork));
 
-            session = this.sessionFactory.OpenSession();
+            session = sessionFactory.OpenSession();
             transaction = session.BeginTransaction();
-            sessionId = (session as SessionImpl)?.SessionId;
+            SessionId = (session as SessionImpl)?.SessionId;
         }
 
         public void AcceptChanges()
@@ -45,7 +42,7 @@ namespace WB.Infrastructure.Native.Storage.Postgre
             {
                 if (isDisposed)
                 {
-                    logger.Info($"Error getting session. Old sessionId:{sessionId} Thread:{Thread.CurrentThread.ManagedThreadId}");
+                    logger.Info($"Error getting session. Old sessionId:{SessionId} Thread:{Thread.CurrentThread.ManagedThreadId}");
                     throw new ObjectDisposedException(nameof(UnitOfWork));
                 }
                 return session;
