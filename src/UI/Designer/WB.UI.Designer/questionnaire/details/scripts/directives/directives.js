32e0fed7b4fedb49a314922b2672ea5106d7f044
Roman Lytvyn <litvin.roma@gmail.com>
2018-11-06 12:39:48 +0200

    KP-6004 Default date for the date question. Fixed designer bugs and minor issues.

--- src/UI/Designer/WB.UI.Designer/questionnaire/details/scripts/directives/directives.js
+++ src/UI/Designer/WB.UI.Designer/questionnaire/details/scripts/directives/directives.js
@@ -92,31 +92,40 @@
                 restrict: 'A',
                 require: 'ngModel',
                 link: function (scope, element) {
-                    jqueryDatepicker($);
-                    $(element).datepicker({
-                        dateFormat: 'yy-mm-dd',
+                    var changeValue = function(date) {
+                        var ngModelName = $(element).attr('ng-model');
 
-                        onSelect: function (date) {
-                            var ngModelName = this.attributes['ng-model'].value;
+                        // if value for the specified ngModel is a property of 
+                        // another object on the scope
+                        if (ngModelName.indexOf(".") != -1) {
+                            var objAttributes = ngModelName.split(".");
+                            var lastAttribute = objAttributes.pop();
+                            var partialObjString = objAttributes.join(".");
+                            var partialObj = eval("scope." + partialObjString);
 
-                            // if value for the specified ngModel is a property of 
-                            // another object on the scope
-                            if (ngModelName.indexOf(".") != -1) {
-                                var objAttributes = ngModelName.split(".");
-                                var lastAttribute = objAttributes.pop();
-                                var partialObjString = objAttributes.join(".");
-                                var partialObj = eval("scope." + partialObjString);
-
-                                partialObj[lastAttribute] = date;
-                            }
-                            // if value for the specified ngModel is directly on the scope
-                            else {
-                                scope[ngModelName] = date;
-                            }
-                            scope.$apply();
+                            partialObj[lastAttribute] = date;
                         }
+                        // if value for the specified ngModel is directly on the scope
+                        else {
+                            scope[ngModelName] = date;
+                        }
+                        scope.questionForm.$setDirty();
+                        scope.$apply();
+                    };
 
+                    $(element).Zebra_DatePicker({
+                        disable_time_picker: true,
+                        onSelect: function(date) {
+                            changeValue(date);
+                        },
+                        onClear: function () {
+                            changeValue(null);
+                        }
                     });
+                    scope.$watch($(element).attr('ng-model'),
+                        function (newValue) {
+                            $(element).data('Zebra_DatePicker').set_date(moment(newValue).toDate());
+                        });
                 }
             };
         });
