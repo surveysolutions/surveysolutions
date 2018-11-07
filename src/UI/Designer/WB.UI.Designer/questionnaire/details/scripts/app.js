3a98f37b83bbff1b1ccd451f2a651dc198385c3e
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2018-11-07 09:45:34 +0200

    Fixed starts with in ie 11

--- src/UI/Designer/WB.UI.Designer/questionnaire/details/scripts/app.js
+++ src/UI/Designer/WB.UI.Designer/questionnaire/details/scripts/app.js
@@ -32,6 +32,13 @@
         moment.locale(lng);
     });
 
+    if (!String.prototype.startsWith) {
+        String.prototype.startsWith = function(searchString, position) {
+          position = position || 0;
+          return this.indexOf(searchString, position) === position;
+        };
+      }
+
     angular.module('designerApp', [
         'ngCookies',
         'ngSanitize',
