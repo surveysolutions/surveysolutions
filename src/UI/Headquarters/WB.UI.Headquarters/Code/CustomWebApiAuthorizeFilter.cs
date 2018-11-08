d52c7d04c533a46a76af1f30a46efa38b2dc1ff0
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2018-11-08 10:01:52 -0500

    KP-7005 Make ReadSide transactions and Plain transaction the same [fix export]

--- src/UI/Headquarters/WB.UI.Headquarters/Code/CustomWebApiAuthorizeFilter.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/Code/CustomWebApiAuthorizeFilter.cs
@@ -4,6 +4,7 @@ using System.Web;
 using System.Web.Http;
 using System.Web.Http.Controllers;
 using System.Web.Http.Filters;
+using WB.UI.Headquarters.API.Filters;
 
 namespace WB.UI.Headquarters.Code
 {
@@ -11,10 +12,18 @@ namespace WB.UI.Headquarters.Code
     {
         public override void OnAuthorization(HttpActionContext actionContext)
         {
-            bool skipAuthorization = actionContext.ActionDescriptor.GetCustomAttributes<AllowAnonymousAttribute>().Count > 0
-                      || actionContext.ActionDescriptor.ControllerDescriptor.GetCustomAttributes<AllowAnonymousAttribute>().Count > 0;
+            bool markedWithAllowAnonymousAttribute = 
+                actionContext.ActionDescriptor.GetCustomAttributes<AllowAnonymousAttribute>().Count > 0
+                || 
+                actionContext.ActionDescriptor.ControllerDescriptor.GetCustomAttributes<AllowAnonymousAttribute>().Count > 0;
 
-            if (skipAuthorization)
+
+            bool markedServiceApiKeyAuthorizationAttribute = 
+                actionContext.ActionDescriptor.GetCustomAttributes<ServiceApiKeyAuthorizationAttribute>().Count > 0
+                || 
+                actionContext.ActionDescriptor.ControllerDescriptor.GetCustomAttributes<ServiceApiKeyAuthorizationAttribute>().Count > 0;
+            
+            if (markedWithAllowAnonymousAttribute || markedServiceApiKeyAuthorizationAttribute)
             {
                 return;
             }
