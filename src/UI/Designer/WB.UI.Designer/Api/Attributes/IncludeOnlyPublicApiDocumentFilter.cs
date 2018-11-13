78e37536dca9fb86f91c661b6d67cf038466af1e
Slava Scherbak <slava.scherbak@gmail.com>
2018-11-13 12:56:11 -0500

    KP-12084: Added table with migration to store classifications

--- src/UI/Designer/WB.UI.Designer/Api/Attributes/IncludeOnlyPublicApiDocumentFilter.cs
+++ src/UI/Designer/WB.UI.Designer/Api/Attributes/IncludeOnlyPublicApiDocumentFilter.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.Web.Http.Description;
 using Swashbuckle.Swagger;
+using WB.UI.Designer.Api.Designer;
+using System.Linq;
 
 namespace WB.UI.Designer.Api.Attributes
 {
@@ -9,15 +11,16 @@ namespace WB.UI.Designer.Api.Attributes
         public void Apply(SwaggerDocument swaggerDoc, SchemaRegistry schemaRegistry, IApiExplorer apiExplorer)
         {
             IDictionary<string, PathItem> pathsToKeep = new Dictionary<string, PathItem>();
+            var filters = new string[] {"Api.Portal", nameof(ClassificationsController)};
             foreach (ApiDescription apiDescription in apiExplorer.ApiDescriptions)
             {
-                var controllerTypeNamespace = apiDescription.ActionDescriptor.ControllerDescriptor.ControllerType.Namespace;
+                var controllerTypeNamespace = apiDescription.ActionDescriptor.ControllerDescriptor.ControllerType.FullName;
 
-                if (controllerTypeNamespace.Contains("Api.Portal"))
-                {
-                    var pathToRemove = $"/{apiDescription.Route.RouteTemplate.TrimEnd('/')}";
-                    pathsToKeep[pathToRemove] = swaggerDoc.paths[pathToRemove];
-                }
+                if (controllerTypeNamespace == null) continue;
+                if (!filters.Any(x => controllerTypeNamespace.Contains(x))) continue;
+
+                var pathToRemove = $"/{apiDescription.Route.RouteTemplate.TrimEnd('/')}";
+                pathsToKeep[pathToRemove] = swaggerDoc.paths[pathToRemove];
             }
 
             swaggerDoc.paths = pathsToKeep;
