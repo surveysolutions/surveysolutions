b64b3a485247c00108d3c8afa57b430c5421a740
Pavel Stovpivskyi <630198+spuv@users.noreply.github.com>
2018-11-19 14:30:59 +0200

    KP-12029 added test

--- src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/EventHandlers/Interview/InterviewHistoryDenormalizerTests/InterviewHistoryDenormalizerTests.cs
+++ src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/EventHandlers/Interview/InterviewHistoryDenormalizerTests/InterviewHistoryDenormalizerTests.cs
@@ -1,7 +1,11 @@
 using System;
 using System.Collections.Generic;
 using NUnit.Framework;
+using WB.Core.BoundedContexts.Headquarters.EventHandler;
+using WB.Core.BoundedContexts.Headquarters.Views.InterviewHistory;
+using WB.Core.Infrastructure.EventHandlers;
 using WB.Core.SharedKernels.DataCollection.Events.Interview;
+using WB.Core.SharedKernels.DataCollection.Events.Interview.Base;
 using WB.Core.SharedKernels.DataCollection.Repositories;
 using WB.Tests.Abc;
 using IEvent = WB.Core.Infrastructure.EventBus.IEvent;
@@ -11,7 +15,8 @@ namespace WB.Tests.Unit.SharedKernels.SurveyManagement.EventHandlers.Interview.I
     internal class InterviewHistoryDenormalizerTests : InterviewHistoryDenormalizerTestContext
     {
         [Test]
-         public void when_AnswerSet_recived_with_dateTimeOffset() {
+        public void when_AnswerSet_recived_with_dateTimeOffset()
+        {
 
             Guid interviewId = Guid.NewGuid();
             Guid userId = Guid.NewGuid();
@@ -29,7 +34,7 @@ namespace WB.Tests.Unit.SharedKernels.SurveyManagement.EventHandlers.Interview.I
             var dateTimeOffset = DateTimeOffset.Now;
 
             var answerEvents = new List<IEvent>();
-            answerEvents.Add(new TextQuestionAnswered(userId, questionId, new decimal[]{1,2}, dateTimeOffset, "hi"));
+            answerEvents.Add(new TextQuestionAnswered(userId, questionId, new decimal[] {1, 2}, dateTimeOffset, "hi"));
 
             var interviewExportedDataDenormalizer = CreateInterviewHistoryDenormalizer(
                 questionnaire: CreateQuestionnaireExportStructure(questionId, variableName),
@@ -39,11 +44,23 @@ namespace WB.Tests.Unit.SharedKernels.SurveyManagement.EventHandlers.Interview.I
             PublishEventsOnOnInterviewExportedDataDenormalizer(answerEvents, interviewHistoryView,
                 interviewExportedDataDenormalizer);
 
-                
+
             Assert.AreEqual(interviewHistoryView.Records[0].Parameters["answer"], "hi");
             Assert.AreEqual(interviewHistoryView.Records[0].Timestamp, dateTimeOffset.LocalDateTime);
             Assert.AreEqual(interviewHistoryView.Records[0].Offset, dateTimeOffset.Offset);
 
+        }
+
+        [TestCase(typeof(QuestionsDisabled))]
+        [TestCase(typeof(QuestionsEnabled))]
+        [TestCase(typeof(GroupsDisabled))]
+        [TestCase(typeof(GroupsEnabled))]
+        public void should_not_impliment_handlers_for_events(Type eventType)
+        {
+            var interfaceForEventType = typeof(IUpdateHandler<,>).MakeGenericType(typeof(InterviewHistoryView), eventType);
+            var isImplimentedInterface = interfaceForEventType.IsAssignableFrom(typeof(InterviewParaDataEventHandler));
+
+            Assert.False(isImplimentedInterface);
         }
 
     }
