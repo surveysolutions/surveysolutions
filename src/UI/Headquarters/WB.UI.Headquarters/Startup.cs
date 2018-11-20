1059d0a7f3b4a456bab917ba1cd08959d1ab4bc2
Pavel Stovpivskyi <sby@rambler.ru>
2018-11-20 17:34:41 +0200

    removed UnitOfWorkScopeManager and created AutofacServiceLocatorFactory

--- src/UI/Headquarters/WB.UI.Headquarters/Startup.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/Startup.cs
@@ -90,8 +90,6 @@ namespace WB.UI.Headquarters
             autofacKernel.Init().Wait();
             var container = autofacKernel.Container;
 
-            UnitOfWorkScopeManager.SetScopeAdapter(container);
-
             InScopeExecutor.Init(new UnitOfWorkInScopeExecutor(container));
 
             var resolver = new AutofacWebApiDependencyResolver(container);
