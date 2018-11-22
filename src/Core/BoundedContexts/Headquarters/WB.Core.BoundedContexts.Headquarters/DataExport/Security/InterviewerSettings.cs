2e74d719acc030aeb2a207610a7a23d72060260e
Pavel Stovpivskyi <sby@rambler.ru>
2018-11-22 12:55:41 +0200

    KP-11982 Changes after code review

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/DataExport/Security/InterviewerSettings.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/DataExport/Security/InterviewerSettings.cs
@@ -4,7 +4,9 @@ namespace WB.Core.BoundedContexts.Headquarters.DataExport.Security
 {
     public class InterviewerSettings : AppSetting
     {
+        public const int HowManyMajorReleaseDontNeedUpdateDefaultValue = 4;
+
         public bool AutoUpdateEnabled { get; set; }
-        public int? HowManyMajorReleaseDontNeedUpdate { get; set; }
+        public int? HowManyMajorReleaseDontNeedUpdate { get; set; } = HowManyMajorReleaseDontNeedUpdateDefaultValue;
     }
 }
