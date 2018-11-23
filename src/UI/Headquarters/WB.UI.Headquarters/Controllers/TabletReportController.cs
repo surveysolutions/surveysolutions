7ef331358a52d3375152e4ee3d9f38d749fd5a76
Roman Lytvyn <litvin.roma@gmail.com>
2018-11-23 12:55:29 +0200

    KP-12139 HQ: Decrypt interviewer's backup file using control panel

--- src/UI/Headquarters/WB.UI.Headquarters/Controllers/TabletReportController.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/Controllers/TabletReportController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Linq;
 using System.Web.Mvc;
+using ImageResizer.ExtensionMethods;
 using WB.Core.BoundedContexts.Headquarters.Services;
+using WB.Core.BoundedContexts.Headquarters.Views.User;
 using WB.Core.SharedKernels.SurveyManagement.Web.Filters;
 using WB.UI.Headquarters.Code;
 
@@ -10,10 +12,15 @@ namespace WB.Core.SharedKernels.SurveyManagement.Web.Controllers
     public class TabletReportController : Controller
     {
         private readonly ITabletInformationService tabletInformationService;
+        private readonly IAuthorizedUser authorizedUser;
+        private readonly IUserViewFactory userViewFactory;
 
-        public TabletReportController(ITabletInformationService tabletInformationService)
+        public TabletReportController(ITabletInformationService tabletInformationService,
+            IAuthorizedUser authorizedUser, IUserViewFactory userViewFactory)
         {
             this.tabletInformationService = tabletInformationService;
+            this.authorizedUser = authorizedUser;
+            this.userViewFactory = userViewFactory;
         }
 
         [AuthorizeOr403(Roles = "Administrator")]
@@ -28,6 +35,26 @@ namespace WB.Core.SharedKernels.SurveyManagement.Web.Controllers
         public ActionResult PackagesInfo()
         {
             return this.View();
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Administrator")]
+        public ActionResult UploadBackup()
+        {
+            if (Request.Files.Count > 0)
+            {
+                var file = Request.Files[0];
+
+                if (file != null && file.ContentLength > 0)
+                {
+                    this.tabletInformationService.SaveTabletInformation(
+                        content: file.InputStream.CopyToMemoryStream().ToArray(),
+                        androidId: @"manual-restore",
+                        user: this.userViewFactory.GetUser(new UserViewInputModel(this.authorizedUser.Id)));
+                }
+            }
+            
+            return this.RedirectToAction("PackagesInfo");
         }
     }
 }
