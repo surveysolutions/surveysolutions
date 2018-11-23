b054aa04af22197402f21ec034f37e25fea5c695
Pavel Stovpivskyi <sby@rambler.ru>
2018-11-23 15:46:46 +0200

    KP-12136 text changes

--- src/UI/Designer/WB.UI.Designer/Resources/UnderConstruction.Designer.cs
+++ src/UI/Designer/WB.UI.Designer/Resources/UnderConstruction.Designer.cs
@@ -61,7 +61,7 @@ namespace WB.UI.Designer.Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to This server is during process of initialization. Please wait....
+        ///   Looks up a localized string similar to Server initialization is in progress. Please wait....
         /// </summary>
         public static string ServerOnMaintenance {
             get {
