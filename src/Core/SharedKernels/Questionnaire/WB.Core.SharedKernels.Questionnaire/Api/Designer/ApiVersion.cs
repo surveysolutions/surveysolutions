ac265adcdb49faa4be2ce87b916803c6ab9b67af
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2018-11-23 13:44:57 +0200

    Up tester version

--- src/Core/SharedKernels/Questionnaire/WB.Core.SharedKernels.Questionnaire/Api/Designer/ApiVersion.cs
+++ src/Core/SharedKernels/Questionnaire/WB.Core.SharedKernels.Questionnaire/Api/Designer/ApiVersion.cs
@@ -2,7 +2,7 @@ namespace WB.Core.SharedKernels.SurveySolutions.Api.Designer
 {
     public static class ApiVersion
     {
-        public const int CurrentTesterProtocolVersion = 41; // 18.11 release
+        public const int CurrentTesterProtocolVersion = 42; // 18.12 release
         public const int MaxQuestionnaireVersion = 24;
     }
 }
