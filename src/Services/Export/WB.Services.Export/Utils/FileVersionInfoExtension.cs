f1c6fb3bada3d3b449a92c2c872aacab21f7bb4c
Victor Hladkikh <me@alfeg.net>
2018-11-27 13:50:03 +0200

    KP-12140 Fix versioning of Export Service # Proper file version

---
+++ src/Services/Export/WB.Services.Export/Utils/FileVersionInfoExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace WB.Services.Export.Utils
+{
+    public static class FileVersionInfoExtension
+    {
+        public static bool IsPreReleaseVersion(this FileVersionInfo fvi)
+        {
+            // we consider as prereleased any version that contain letters in product version, i.e.
+            // 18.12.0-dev.322 is considered prereleased version
+            // 18.12.0.322 - is release version
+            return fvi.ProductVersion.Any(Char.IsLetter);
+        }
+    }
+}
