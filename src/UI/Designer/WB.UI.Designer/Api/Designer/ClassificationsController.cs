d43d137dd3df669c23e14e6aaf11950337c1b018
Slava Scherbak <slava.scherbak@gmail.com>
2018-11-29 15:33:39 -0500

    KP-12181: added some front end and back end for classifications editor

--- src/UI/Designer/WB.UI.Designer/Api/Designer/ClassificationsController.cs
+++ src/UI/Designer/WB.UI.Designer/Api/Designer/ClassificationsController.cs
@@ -2,21 +2,42 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Description;
+using System.Web.Http.Filters;
 using Newtonsoft.Json;
 using WB.Core.BoundedContexts.Designer.Classifications;
+using WB.Core.BoundedContexts.Designer.Exceptions;
 using WB.UI.Designer.Filters;
 using WB.UI.Shared.Web.Filters;
 
 namespace WB.UI.Designer.Api.Designer
 {
+    public class ClassificationsExceptionFilterAttribute : ExceptionFilterAttribute 
+    {
+        public override void OnException(HttpActionExecutedContext context)
+        {
+            if (!(context.Exception is ClassificationException exception)) return;
+            switch (exception.ErrorType)
+            {
+                case ClassificationExceptionType.Undefined:
+                    context.Response = new HttpResponseMessage(HttpStatusCode.BadRequest);
+                    break;
+                case ClassificationExceptionType.NoAccess:
+                    context.Response = new HttpResponseMessage(HttpStatusCode.Forbidden);
+                    break;
+            }
+        }
+    }
     [Authorize]
     [ApiNoCache]
     [Authorize]
     [RoutePrefix("api")]
     [CamelCase]
+    [ClassificationsExceptionFilter]
     public class ClassificationsController : ApiController
     {
         private readonly IClassificationsStorage classificationsStorage;
@@ -80,7 +101,7 @@ namespace WB.UI.Designer.Api.Designer
         }
 
         [HttpGet]
-        [Route("classifications/groups")]
+        [Route("groups")]
         public async Task<IEnumerable<ClassificationGroup>> GetGroups()
         {
             return await classificationsStorage.GetClassificationGroups();
@@ -98,6 +119,54 @@ namespace WB.UI.Designer.Api.Designer
         public async Task<IEnumerable<Category>> Categories([FromUri] Guid id)
         {
             return await classificationsStorage.GetCategories(id);
+        }
+
+        [HttpPatch]
+        [Route("classification/{id}")]
+        public async Task UpdateClassification([FromUri] Guid id)
+        {
+            await classificationsStorage.UpdateClassification(id);
+        }
+        [HttpPost]
+        [Route("classification")]
+        public async Task CreateClassification()
+        {
+            await classificationsStorage.CreateClassification();
+        }
+
+        [HttpDelete]
+        [Route("classification/{id}")]
+        public async Task DeleteClassification([FromUri] Guid id)
+        {
+            await classificationsStorage.DeleteClassification(id);
+        }
+
+        [HttpPatch]
+        [Route("group/{id}")]
+        public async Task UpdateClassificationGroup([FromUri] Guid id, ClassificationGroup group)
+        {
+            await classificationsStorage.UpdateClassificationGroup(group);
+        }
+
+        [HttpPost]
+        [Route("group")]
+        public async Task CreateClassificationGroup(ClassificationGroup group)
+        {
+            await classificationsStorage.CreateClassificationGroup(group);
+        }
+
+        [HttpDelete]
+        [Route("group/{id}")]
+        public async Task DeleteClassificationGroup([FromUri] Guid id)
+        {
+            await classificationsStorage.DeleteClassificationGroup(id);
+        }
+
+        [HttpPatch]
+        [Route("classification/{id}/categories")]
+        public async Task UpdateCategories([FromUri] Guid id)
+        {
+            await classificationsStorage.UpdateCategories(id);
         }
     }
 
