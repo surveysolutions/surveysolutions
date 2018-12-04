1a00f1efe052013f8621f20f2ba56da26c4bbaad
Slava Scherbak <slava.scherbak@gmail.com>
2018-12-04 17:55:47 -0500

    KP-12181: fixed issues with categories editor

---
+++ src/UI/Designer/WB.UI.Designer/Scripts/classifications/config.js
@@ -0,0 +1,16 @@
+﻿var ClassificationsConfig = {};
+ClassificationsConfig.install = function(Vue, options) {
+    Object.defineProperty(Vue, '$config',
+        {
+            get: function() {
+                return options;
+            }
+        });
+
+    Object.defineProperty(Vue.prototype, '$config',
+        {
+            get: function() {
+                return options;
+            }
+        });
+};
