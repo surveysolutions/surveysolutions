a96dd9ef22f315ba50be73960321512e00a8a194
Slava Scherbak <slava.scherbak@gmail.com>
2018-12-05 10:55:29 -0500

    KP-12181: added string options editor

--- src/UI/Designer/WB.UI.Designer/Scripts/classifications/components/CategoriesEditor.js
+++ src/UI/Designer/WB.UI.Designer/Scripts/classifications/components/CategoriesEditor.js
@@ -3,6 +3,9 @@
         template: '#categories-editor-template',
         data: function() {
             return {
+                optionsMode: true,
+                regex: new RegExp(/^(.+?)[\…\.\s]+([-+]?\d+)\s*$/),
+                stringifiedOptions: ''
             }
         },
         computed: {
@@ -11,9 +14,40 @@
             },
             isEditMode() {
                 return Vue.$config.isAdmin || Vue.$config.userId === this.$store.state.activeClassification.userId;
-            }
+            },
         },
         methods: {
+            validateOptionAsText(option) {
+                return this.regex.test((option || ""));
+            },
+
+            stringifyCategories() {
+                var stringifiedOptions = "";
+                var maxLength = _.max(_.map(this.categories, function (o) { return o.title.length; })) + 3;
+                _.each(this.categories, function (category) {
+                    if (!_.isEmpty(category)) {
+                        stringifiedOptions += (category.title || "") + Array(maxLength + 1 - (category.title || "").length).join('.') + (category.value === 0 ? "0" : (category.value || ""));
+                        stringifiedOptions += "\n";
+                    }
+                });
+                this.stringifiedOptions = stringifiedOptions.trim("\n");
+            },
+
+            parseOptions() {
+                var self = this;
+                var optionsStringList = (this.stringifiedOptions || "").split("\n");
+                _.filter(optionsStringList, _.isEmpty);
+
+                var options = _.map(optionsStringList, function(item) {
+                    var matches = item.match(self.regex);
+                    return {
+                        value: matches[2] * 1,
+                        title: matches[1]
+                    };
+                });
+
+                return options;
+            },
             moveFocus($event) {
                 var parentCell = $($event.target).closest('div.option-cell');
                 var nextCell = parentCell.next('div.option-cell');
@@ -26,6 +60,15 @@
             },
             deleteCategory(index) {
                 store.dispatch('deleteCategory', index);
+            },
+            showStrings() {
+                this.stringifyCategories();
+                this.optionsMode = false;
+            },
+            showList() {
+                var parsedCategories = this.parseOptions();
+                console.log(parsedCategories);
+                this.optionsMode = false;
             },
             save() {
                 var self = this;
