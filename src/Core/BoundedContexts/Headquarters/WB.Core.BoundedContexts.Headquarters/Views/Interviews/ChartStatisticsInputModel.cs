f08b268c65bbcee49e43f6c0a44daf5ccc5104ba
Pavel Stovpivskyi <sby@rambler.ru>
2018-12-11 15:49:15 +0200

    KP-12050 Cumulative chart cleanup

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Views/Interviews/ChartStatisticsInputModel.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Views/Interviews/ChartStatisticsInputModel.cs
@@ -5,8 +5,8 @@ namespace WB.Core.BoundedContexts.Headquarters.Views.Interviews
     public class ChartStatisticsInputModel
     {
         public DateTime CurrentDate { get; set; }
-        public Guid QuestionnaireId { get; set; }
-        public long QuestionnaireVersion { get; set; }
+        public Guid? QuestionnaireId { get; set; }
+        public long? QuestionnaireVersion { get; set; }
         public DateTime? From { get; set; }
         public DateTime? To { get; set; }
     }
