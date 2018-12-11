1e8e4d5aa92d4a4f075b56ea2b45763b38d47bd9
Victor Hladkikh <me@alfeg.net>
2018-12-11 14:44:25 +0200

    KP-12264 SQL connections pool Exhausted # Proper hub lifetime managment

--- src/UI/Headquarters/WB.UI.Headquarters/Code/CustomAutofacHubActivator.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/Code/CustomAutofacHubActivator.cs
@@ -6,19 +6,18 @@ namespace WB.UI.Headquarters.Code
     class CustomAutofacHubActivator : IHubActivator
     {
         private readonly ILifetimeScope lifetimeScope;
-        private readonly CustomLifetimeHubManager lifetimeHubManager;
-
-        public CustomAutofacHubActivator(CustomLifetimeHubManager lifetimeHubManager, ILifetimeScope lifetimeScope)
+        
+        public CustomAutofacHubActivator(ILifetimeScope lifetimeScope)
         {
             this.lifetimeScope = lifetimeScope;
-            this.lifetimeHubManager = lifetimeHubManager;
         }
 
         public IHub Create(HubDescriptor descriptor)
         {
-            return typeof(ILifetimeHub).IsAssignableFrom(descriptor.HubType) 
-                ? (IHub)this.lifetimeHubManager.CreateUnitOfWorkScopeAndResolveHub<ILifetimeHub>(descriptor.HubType, this.lifetimeScope) 
-                : this.lifetimeScope.Resolve(descriptor.HubType) as IHub;
+            return this.lifetimeScope.Resolve(descriptor.HubType) as IHub;
+            //return typeof(ILifetimeHub).IsAssignableFrom(descriptor.HubType) 
+            //    ? (IHub)this.lifetimeHubManager.CreateUnitOfWorkScopeAndResolveHub<ILifetimeHub>(descriptor.HubType, this.lifetimeScope) 
+            //    : this.lifetimeScope.Resolve(descriptor.HubType) as IHub;
         }
     }
 }
