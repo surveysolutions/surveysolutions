f9da9651427b3b8bb303504d4cb72a6afb8fa0c4
Victor Gladkikh <me@alfeg.net>
2018-12-12 23:48:18 +0200

    Getting rid of named tag scopes

--- src/Core/Infrastructure/WB.Core.Infrastructure/Modularity/IIocRegistry.cs
+++ src/Core/Infrastructure/WB.Core.Infrastructure/Modularity/IIocRegistry.cs
@@ -27,7 +27,6 @@ namespace WB.Core.Infrastructure.Modularity
         void BindAsSingleton(Type @interface, Type implementation);
         void BindGeneric(Type implementation);
         void BindInPerLifetimeScope<T1, T2>() where T2 : T1;
-        void BindInPerUnitOfWorkOrPerRequestScope<TInterface, TImplementation>() where TImplementation : TInterface;
 
         void BindWithConstructorArgumentInPerLifetimeScope<TInterface, TImplementation>(string argumentName,
             object argumentValue) where TImplementation : TInterface;
