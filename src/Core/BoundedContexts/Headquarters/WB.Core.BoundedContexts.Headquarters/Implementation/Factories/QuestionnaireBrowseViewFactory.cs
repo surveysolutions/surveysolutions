99368ce4da2c9ef90a8564e87dfcb127f89010f7
Andrii Kozhyn <hedgic@gmail.com>
2018-12-17 18:13:50 +0200

    KP-12186 Split questionnaire version and title into 2 dropdowns

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Implementation/Factories/QuestionnaireBrowseViewFactory.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Implementation/Factories/QuestionnaireBrowseViewFactory.cs
@@ -90,5 +90,35 @@ namespace WB.Core.BoundedContexts.Headquarters.Implementation.Factories
                     .Select(x => x.Id)
                     .ToArray())
                 .Select(QuestionnaireIdentity.Parse);
+
+        public QuestionnairesList UniqueQuestionnaireIds(string searchFor, int pageSize)
+        {
+            return this.reader.Query(queryable =>
+            {
+                var query = queryable.Where(x => !x.IsDeleted)
+                    .Select(x => new {x.QuestionnaireId, x.Title});
+
+                if (!string.IsNullOrEmpty(searchFor))
+                {
+                    var filterLowerCase = searchFor.ToLower();
+                    query = query.Where(x => x.Title.ToLower().Contains(filterLowerCase));
+                }
+
+                var list = query.ToList();
+                var questionnaireListItems = list.Distinct()
+                    .Select(x => new QuestionnaireListItem
+                    {
+                        Id = x.QuestionnaireId,
+                        Title = x.Title
+                    });
+
+                var listItems = questionnaireListItems.ToList();
+                return new QuestionnairesList
+                {
+                    Items = listItems,
+                    TotalCount = listItems.Count
+                };
+            });
+        }
     }
 }
