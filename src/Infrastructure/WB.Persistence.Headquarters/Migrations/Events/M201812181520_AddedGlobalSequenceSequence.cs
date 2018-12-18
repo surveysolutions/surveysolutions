ed256d4770a42b68f61c9302c937fdbf34e8a63c
Victor Gladkikh <me@alfeg.net>
2018-12-18 15:09:08 +0200

    KP-12284 Move next global sequence value to DB

---
+++ src/Infrastructure/WB.Persistence.Headquarters/Migrations/Events/M201812181520_AddedGlobalSequenceSequence.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using FluentMigrator;
+
+namespace WB.Persistence.Headquarters.Migrations.Events
+{
+    [Migration(201812181510)]
+    [SuppressMessage("ReSharper", "StringLiteralTypo")]
+    public class M201812181520_AddedGlobalSequenceSequence : Migration
+    {
+        public override void Up()
+        {
+            Execute.Sql(@"CREATE SEQUENCE if not exists events.globalsequence INCREMENT BY 1 MINVALUE 1 NO MAXVALUE CACHE 1 NO CYCLE");
+            Execute.Sql(@"select setval('events.globalsequence',  (SELECT MAX(globalsequence) FROM events.events));");
+            Execute.Sql(@"ALTER TABLE events.events ALTER COLUMN globalsequence SET DEFAULT nextval('events.globalsequence');");
+        }
+
+        public override void Down()
+        {
+            
+        }
+    }
+}
