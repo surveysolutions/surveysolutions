476cac6f5a6f414f0c1830c262ce7499e1618d86
Andrii Kozhyn <hedgic@gmail.com>
2018-12-19 15:58:18 +0200

    KP-12189 Split questionnaire id and version on interview duration

--- src/Tests/WB.Tests.Integration/ReportTests/SpeedReportTests/SpeedReportContext.cs
+++ src/Tests/WB.Tests.Integration/ReportTests/SpeedReportTests/SpeedReportContext.cs
@@ -1,6 +1,10 @@
-﻿using WB.Core.BoundedContexts.Headquarters.Views.Interview;
+﻿using System;
+using WB.Core.BoundedContexts.Headquarters.Views.DataExport;
+using WB.Core.BoundedContexts.Headquarters.Views.Interview;
 using WB.Core.BoundedContexts.Headquarters.Views.Reposts.Factories;
+using WB.Core.GenericSubdomains.Portable;
 using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
+using WB.Tests.Abc;
 
 namespace WB.Tests.Integration.ReportTests.SpeedReportTests
 {
@@ -9,6 +13,31 @@ namespace WB.Tests.Integration.ReportTests.SpeedReportTests
         protected SpeedReportFactory CreateSpeedReport(IQueryableReadSideRepositoryReader<InterviewSummary> summaries)
         {
             return new SpeedReportFactory(summaries);
+        }
+
+        
+        protected static InterviewSummary CreateCompletedInterviewWithDuration(TimeSpan interviewingTotalTime,
+            Guid supervisorId, 
+            Guid responsibleId,
+            DateTime dateTime,
+            Guid? questionnaireId = null, 
+            long? questionnaireVersion = null)
+        {
+            var interviewId = Guid.NewGuid();
+            var interview = Create.Entity.InterviewSummary(interviewingTotalTime: interviewingTotalTime,
+                interviewId: interviewId,
+                teamLeadId: supervisorId,
+                responsibleId: responsibleId,
+                questionnaireId: questionnaireId,
+                questionnaireVersion: questionnaireVersion);
+            interview.SummaryId = interviewId.FormatGuid();
+            var interviewCommentedStatus = Create.Entity.InterviewCommentedStatus(status: InterviewExportedAction.FirstAnswerSet,
+                timestamp: dateTime.AddMinutes(1),
+                timeSpanWithPreviousStatus: TimeSpan.FromMinutes(1),
+                supervisorId: supervisorId);
+            interviewCommentedStatus.InterviewSummary = interview;
+            interview.InterviewCommentedStatuses.Add(interviewCommentedStatus);
+            return interview;
         }
     }
 }
