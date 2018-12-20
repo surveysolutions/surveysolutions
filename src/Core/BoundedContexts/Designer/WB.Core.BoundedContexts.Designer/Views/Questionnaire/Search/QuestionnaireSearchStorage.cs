d4d23a1d79cdcd19506c1ee8a7d6e7a7a25ef63e
Pavel Stovpivskyi <sby@rambler.ru>
2018-12-20 12:21:50 +0200

    KP-12182 search storage

---
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Views/Questionnaire/Search/QuestionnaireSearchStorage.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Main.Core.Entities.Composite;
+using Main.Core.Entities.SubEntities;
+using WB.Core.SharedKernels.Questionnaire.Documents;
+using WB.Infrastructure.Native.Storage.Postgre;
+
+namespace WB.Core.BoundedContexts.Designer.Views.Questionnaire.Search
+{
+    public class QuestionnaireSearchStorage : IQuestionnaireSearchStorage
+    {
+        public const string TableName = "questionnairesearchentities";
+        public const string TableNameWithSchema = "plainstore." + TableName;
+
+        private readonly IUnitOfWork unitOfWork;
+
+        public QuestionnaireSearchStorage(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public void AddOrUpdateEntity(Guid questionnaireId, IComposite composite)
+        {
+            var isQuestion = composite is IQuestion;
+            if (!isQuestion)
+                throw new ArgumentException("composite type is not supported " + composite.GetType());
+
+            var sql = $"INSERT INTO {TableNameWithSchema} s (questionnaireid, entityid, entity, searchtext)" +
+                      $"VALUES(:questionnaireId, :entityid, :entity, :searchtext) " +
+                      $"ON CONFLICT (questionnaireid, entityid) DO UPDATE " +
+                      $"SET questionnaireid = :questionnaireId," +
+                      $"    entityid        = :entityId," +
+                      $"    entity          = :entity," +
+                      $"    searchtext      = :searchText";
+
+            var query = unitOfWork.Session.CreateQuery(sql);
+            query.SetParameter("questionnaireId", questionnaireId);
+            query.SetParameter("entityId", composite.PublicKey);
+            query.SetParameter("entity", composite);
+            query.SetParameter("searchText", GetTextUsedForSerach(composite));
+            query.ExecuteUpdate();
+        }
+
+        private string GetTextUsedForSerach(IComposite composite)
+        {
+            return composite.GetTitle();
+        }
+
+        public void Remove(Guid questionnaireId, Guid entityId)
+        {
+            var sql = $"DELETE from {TableNameWithSchema} s " +
+                      $"WHERE s.questionnaireid = :questionnaireId " +
+                      $"AND s.entityid =  :entityId";
+            var query = unitOfWork.Session.CreateQuery(sql);
+            query.SetParameter("questionnaireId", questionnaireId);
+            query.SetParameter("entityId", entityId);
+            query.ExecuteUpdate();
+        }
+
+        public void RemoveAllEntities(Guid questionnaireId)
+        {
+            var sql = $"DELETE from {TableNameWithSchema} s " +
+                      $"WHERE s.questionnaireid = :questionnaireId ";
+            var query = unitOfWork.Session.CreateQuery(sql);
+            query.SetParameter("questionnaireId", questionnaireId);
+            query.ExecuteUpdate();
+        }
+
+        public SearchResult Search(SearchInput input)
+        {
+            return null;
+        }
+    }
+
+    public interface IQuestionnaireSearchStorage
+    {
+        void AddOrUpdateEntity(Guid questionnaireId, IComposite composite);
+        void Remove(Guid questionnaireId, Guid entityId);
+        SearchResult Search(SearchInput input);
+        void RemoveAllEntities(Guid questionnaireId);
+    }
+}
---
+++ src/UI/Designer/WB.UI.Designer/Migrations/PlainStore/M001-100/M201812181637_AddQuestionnaireSearchEntities.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentMigrator;
+using WB.Core.BoundedContexts.Designer.Views.Questionnaire.Search;
+using WB.Core.GenericSubdomains.Portable.ServiceLocation;
+
+namespace WB.UI.Designer.Migrations.PlainStore
+{
+    [Migration(201812181637)]
+    public class M201812181637_AddQuestionnaireSearchEntities : Migration
+    {
+        private string tableName = QuestionnaireSearchStorage.TableName;
+        private string tableNameWithShema = QuestionnaireSearchStorage.TableNameWithSchema;
+
+        public override void Up()
+        {
+            this.Create.Table(tableName)
+                .WithColumn("questionnaireid").AsGuid()
+                .WithColumn("title").AsString()
+                .WithColumn("entityid").AsGuid()
+                .WithColumn("entity").AsCustom("jsonb")
+                .WithColumn("searchtext").AsCustom("tsvector");
+
+
+            this.Create.PrimaryKey($"primarykey_{tableName}")
+                .OnTable(tableName)
+                .Columns("questionnaireid", "entityid");
+
+
+//           this.Create.Index($"searchtext_{tableName}_idx")
+//               .OnTable(tableName)
+//               .OnColumn("searchtext");
+
+            this.Execute.Sql($"CREATE INDEX searchtext_{tableName}_idx ON {tableNameWithShema} USING GIN (searchtext);");
+
+            FillTableWithData();
+        }
+
+        private void FillTableWithData()
+        {
+            var questionnaireSearchStorage = ServiceLocator.Current.GetInstance<IQuestionnaireSearchStorage>();
+            var questionnaireSearchStorage2 = ServiceLocator.Current.GetInstance<IQuestionnaireSearchStorage>();
+
+            Execute.WithConnection((con, trans) =>
+            {
+                var questionnaireIds = new List<Guid>();
+
+                var dbCommand = con.CreateCommand();
+                dbCommand.CommandText = "SELECT publicid FROM plainstore.questionnairelistviewitems WHERE ispublic = true AND isdeleted = false; ";
+                using (var reader = dbCommand.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        questionnaireIds.Add((Guid)reader[0]);
+                    }
+                }
+
+                
+            });
+        }
+
+        public override void Down()
+        {
+            this.Delete.Table(tableName);
+        }
+    }
+}
