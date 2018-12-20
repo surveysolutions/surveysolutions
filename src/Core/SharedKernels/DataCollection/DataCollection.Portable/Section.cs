6e2ea2b518169483a3bc1f405c9f2cec1f5c9ff4
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2018-12-20 16:24:19 -0500

    KP-12156 Introduce new functions for sections

--- src/Core/SharedKernels/DataCollection/DataCollection.Portable/Section.cs
+++ src/Core/SharedKernels/DataCollection/DataCollection.Portable/Section.cs
@@ -1,14 +1,23 @@
-﻿namespace WB.Core.SharedKernels.DataCollection.Portable
+﻿using System;
+
+namespace WB.Core.SharedKernels.DataCollection.Portable
 {
     public class Section
     {
-        public Section(bool isDisabled)
+        public Section(Func<bool> isDisabled, Func<int> countEnabledQuestions, Func<int> enabledAnsweredQuestionsCount)
         {
             __isDisabled = isDisabled;
+            this.__countEnabledQuestions = countEnabledQuestions;
+            this.__enabledAnsweredQuestionsCount = enabledAnsweredQuestionsCount;
         }
 
-        private readonly bool __isDisabled;
+        private readonly Func<bool> __isDisabled;
+        public bool IsDisabled() => this.__isDisabled.Invoke();
 
-        public bool IsDisabled() => this.__isDisabled;
+        private readonly Func<int> __countEnabledQuestions;
+        public int GetEnabledQuestionsCount() => this.__countEnabledQuestions.Invoke();
+
+        private readonly Func<int> __enabledAnsweredQuestionsCount;
+        public int GetEnabledAnsweredQuestionsCount() => this.__enabledAnsweredQuestionsCount.Invoke();
     }
 }
