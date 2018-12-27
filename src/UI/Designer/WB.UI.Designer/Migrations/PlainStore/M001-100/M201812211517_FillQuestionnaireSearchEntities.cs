f10e199cfeafd5641793050bf16a8b4fef097836
Pavel Stovpivskyi <630198+spuv@users.noreply.github.com>
2018-12-27 13:38:31 +0200

    KP-12182 fixed migration

--- src/UI/Designer/WB.UI.Designer/Migrations/PlainStore/M001-100/M201812211517_FillQuestionnaireSearchEntities.cs
+++ src/UI/Designer/WB.UI.Designer/Migrations/PlainStore/M001-100/M201812211517_FillQuestionnaireSearchEntities.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
+using Autofac;
 using FluentMigrator;
 using Main.Core.Documents;
 using Main.Core.Entities.Composite;
@@ -16,42 +17,42 @@ using WB.Core.SharedKernels.QuestionnaireEntities;
 namespace WB.UI.Designer.Migrations.PlainStore
 {
     [Migration(201812211517)]
-    public class M201812211517_FillQuestionnaireSearchEntities : Migration
+    public class M201812211517_FillQuestionnaireSearchEntities : RepositoriesMigration
     {
-        private readonly IServiceLocator serviceLocator;
-
         public M201812211517_FillQuestionnaireSearchEntities(IServiceLocator serviceLocator)
+            : base(serviceLocator)
         {
-            this.serviceLocator = serviceLocator;
         }
 
         public override void Up()
         {
-            Thread.Sleep(5000);
-
-            var questionnaireListViewItemStorage = serviceLocator.GetInstance<IPlainStorageAccessor<QuestionnaireListViewItem>>();
-            var questionnaireDocumentStorage = serviceLocator.GetInstance<IPlainKeyValueStorage<QuestionnaireDocument>>();
-            var questionnaireSearchStorage = serviceLocator.GetInstance<IQuestionnaireSearchStorage>();
-
-            var questionnaireListViewItems = questionnaireListViewItemStorage.Query(_ => _
-                .Where(item => !item.IsDeleted && item.IsPublic)
-            );
-
-            foreach (var item in questionnaireListViewItems)
+            RunActionInScope(serviceLocator =>
             {
-                var questionnaireDocument = questionnaireDocumentStorage.GetById(item.QuestionnaireId);
-                var entities = questionnaireDocument.Children.TreeToEnumerable(e => e.Children);
-                foreach (var entity in entities)
+                var questionnaireListViewItemStorage = serviceLocator.GetInstance<IPlainStorageAccessor<QuestionnaireListViewItem>>();
+                var questionnaireDocumentStorage = serviceLocator.GetInstance<IPlainKeyValueStorage<QuestionnaireDocument>>();
+                var questionnaireSearchStorage = serviceLocator.GetInstance<IQuestionnaireSearchStorage>();
+
+                var questionnaireListViewItems = questionnaireListViewItemStorage.Query(_ => _
+                    .Where(item => !item.IsDeleted && item.IsPublic)
+                    .Select(item => item.QuestionnaireId)
+                );
+
+                foreach (var questionnaireId in questionnaireListViewItems)
                 {
-                    if (entity is IQuestion
-                        || entity is IVariable
-                        || entity is IGroup
-                        || entity is IStaticText)
+                    var questionnaireDocument = questionnaireDocumentStorage.GetById(questionnaireId);
+                    var entities = questionnaireDocument.Children.TreeToEnumerable(e => e.Children);
+                    foreach (var entity in entities)
                     {
-                        questionnaireSearchStorage.AddOrUpdateEntity(questionnaireDocument.PublicKey, entity);
+                        if (entity is IQuestion
+                            || entity is IVariable
+                            || entity is IGroup
+                            || entity is IStaticText)
+                        {
+                            questionnaireSearchStorage.AddOrUpdateEntity(questionnaireDocument.PublicKey, entity);
+                        }
                     }
                 }
-            }
+            });
         }
 
         public override void Down()
