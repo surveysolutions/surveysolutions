a0471934ae675f4a81b39f36cfadd8a43f6fa89e
Roman Lytvyn <litvin.roma@gmail.com>
2018-12-28 16:25:36 +0200

    KP-12183 [code review] D: Improvements for import options of cascading  question

--- src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Exceptions/DomainExceptionType.cs
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Exceptions/DomainExceptionType.cs
@@ -157,5 +157,6 @@
         GroupYouAreLinkedToIsNotRoster,
 
         VariableLabelContainsSubstitutionReference,
+        CategoricalCascadingOptionsContainsNotUniqueTitleAndParentValuePair
     }
 }
