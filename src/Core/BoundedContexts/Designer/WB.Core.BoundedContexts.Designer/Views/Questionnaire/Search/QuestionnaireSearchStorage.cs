1ad3b41d9f6b22afbac604e1273e010b82c20ba9
Pavel Stovpivskyi <630198+spuv@users.noreply.github.com>
2018-12-28 18:56:21 +0200

    KP-12182 added integration tests

--- src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Views/Questionnaire/Search/QuestionnaireSearchStorage.cs
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Views/Questionnaire/Search/QuestionnaireSearchStorage.cs
@@ -143,12 +143,4 @@ namespace WB.Core.BoundedContexts.Designer.Views.Questionnaire.Search
             return searchResult;
         }
     }
-
-    public interface IQuestionnaireSearchStorage
-    {
-        void AddOrUpdateEntity(Guid questionnaireId, IComposite composite);
-        void Remove(Guid questionnaireId, Guid entityId);
-        SearchResult Search(SearchInput input);
-        void RemoveAllEntities(Guid questionnaireId);
-    }
 }
