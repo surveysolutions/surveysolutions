d543dbf9b83ef9a2bc9b888f9f133d5c2f693b64
Andrii Kozhyn <hedgic@gmail.com>
2019-01-02 15:44:12 +0200

    KP-12333 check for unique title during cloning of questionnaire

--- src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/QuestionnaireValidatorTests/when_validating_ImportFromDesigner_command_and_same_questionnaire_with_such_title_already_exists.cs
+++ src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/QuestionnaireValidatorTests/when_validating_ImportFromDesigner_command_and_same_questionnaire_with_such_title_already_exists.cs
@@ -9,12 +9,11 @@ using WB.Core.BoundedContexts.Headquarters.Views.Questionnaire;
 using WB.Core.Infrastructure.PlainStorage;
 using WB.Tests.Abc;
 
-namespace WB.Tests.Unit.SharedKernels.SurveyManagement.QuestionnaireImportValidatorTests
+namespace WB.Tests.Unit.SharedKernels.SurveyManagement.QuestionnaireValidatorTests
 {
-    internal class when_validating_ImportFromDesigner_command_and_no_questionnaire_with_such_title_exists
+    internal class when_validating_ImportFromDesigner_command_and_same_questionnaire_with_such_title_already_exists
     {
-        [Test] public void should_not_throw_exception () 
-        {
+        [NUnit.Framework.Test] public void should_not_throw_exception () {
             command = Create.Command.ImportFromDesigner(title: title, questionnaireId: importedQuestionnaireId);
 
             var questionnaireBrowseItemStorage = Mock.Of<IPlainStorageAccessor<QuestionnaireBrowseItem>>();
@@ -23,7 +22,7 @@ namespace WB.Tests.Unit.SharedKernels.SurveyManagement.QuestionnaireImportValida
                 .Setup(reader => reader.Query(Moq.It.IsAny<Func<IQueryable<QuestionnaireBrowseItem>, List<QuestionnaireBrowseItem>>>()))
                 .Returns<Func<IQueryable<QuestionnaireBrowseItem>, List<QuestionnaireBrowseItem>>>(query => query.Invoke(new[]
                 {
-                      Create.Entity.QuestionnaireBrowseItem(title: "different title", variable:null,  questionnaireId: differentQuestionnaireId),
+                    Create.Entity.QuestionnaireBrowseItem(title: title, questionnaireId: importedQuestionnaireId),
                 }.AsQueryable()));
 
             validator = Create.Service.QuestionnaireNameValidator(questionnaireBrowseItemStorage: questionnaireBrowseItemStorage);
@@ -31,10 +30,9 @@ namespace WB.Tests.Unit.SharedKernels.SurveyManagement.QuestionnaireImportValida
             Assert.DoesNotThrow(() => validator.Validate(null, command));
         }
 
-        private static QuestionnaireImportValidator validator;
+        private static QuestionnaireValidator validator;
         private static ImportFromDesigner command;
         private static string title = "The Title";
         private static Guid importedQuestionnaireId = Guid.Parse("11111111111111111111111111111111");
-        private static Guid differentQuestionnaireId = Guid.Parse("22222222222222222222222222222222");
     }
 }
