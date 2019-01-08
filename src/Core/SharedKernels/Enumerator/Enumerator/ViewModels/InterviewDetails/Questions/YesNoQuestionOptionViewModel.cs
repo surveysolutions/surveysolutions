d8f7371caa61d64622d30340d026179759e0b625
Roman Lytvyn <litvin.roma@gmail.com>
2019-01-08 12:46:00 +0200

    KP-7038 Discrepancy between answers Yes and created rosters. Fixed yes/no questions

--- src/Core/SharedKernels/Enumerator/Enumerator/ViewModels/InterviewDetails/Questions/YesNoQuestionOptionViewModel.cs
+++ src/Core/SharedKernels/Enumerator/Enumerator/ViewModels/InterviewDetails/Questions/YesNoQuestionOptionViewModel.cs
@@ -1,177 +1,126 @@
 ﻿using System;
-using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 using MvvmCross.Commands;
 using MvvmCross.ViewModels;
-using WB.Core.GenericSubdomains.Portable;
+using WB.Core.SharedKernels.DataCollection;
 using WB.Core.SharedKernels.DataCollection.Events.Interview;
+using WB.Core.SharedKernels.Enumerator.Properties;
+using WB.Core.SharedKernels.Enumerator.Services;
 using WB.Core.SharedKernels.Enumerator.ViewModels.InterviewDetails.Questions.State;
 
 namespace WB.Core.SharedKernels.Enumerator.ViewModels.InterviewDetails.Questions
 {
     public class YesNoQuestionOptionViewModel : MvxNotifyPropertyChanged, ICompositeEntity
     {
-        public YesNoQuestionViewModel QuestionViewModel { get; private set; }
-        public QuestionStateViewModel<YesNoQuestionAnswered> QuestionState { get; set; }
+        private readonly IUserInteractionService userInteraction;
 
-        private event EventHandler<EventArgs> AnswerChanged; 
-
-        public YesNoQuestionOptionViewModel(YesNoQuestionViewModel questionViewModel,
-            QuestionStateViewModel<YesNoQuestionAnswered> questionState)
+        public YesNoQuestionOptionViewModel(IUserInteractionService userInteraction)
         {
-            this.QuestionViewModel = questionViewModel;
-            this.QuestionState = questionState;
-            this.AnswerChanged += (o, e) => this.RaiseToggleAnswer(e as YesNoEventArgs);
+            this.userInteraction = userInteraction;
         }
 
-        public decimal Value { get; set; }
+        public void Init(Identity identity, CategoricalOption model,
+            QuestionStateViewModel<YesNoQuestionAnswered> questionState, bool isRosterSizeQuestion, bool isProtected,
+            Action setAnswer)
+        {
+            this.QuestionState = questionState;
+            this.IsProtected = isProtected;
+            this.Value = model.Value;
+            this.Title = model.Title;
+            this.IsProtected = isProtected;
+            this.YesItemTag = $"{identity}_Opt_Yes_{model.Value}";
+            this.NoItemTag = $"{identity}_Opt_No_{model.Value}";
+
+            this.isRosterSizeQuestion = isRosterSizeQuestion;
+            this.setAnswer = setAnswer;
+        }
+
+        private bool isRosterSizeQuestion;
+        private Action setAnswer;
 
         private string title;
         public string Title
         {
             get => this.title;
-            set { this.title = value; this.RaisePropertyChanged(); }
+            set => this.SetProperty(ref this.title, value);
         }
 
-        private bool? selected;
-
-        public bool? Selected
-        {
-            get => this.selected;
-            set
-            {
-                if (this.selected == value)
-                    return;
-
-                this.selected = value;
-
-                if (this.QuestionViewModel.AreAnswersOrdered)
-                {
-                    if (value == true)
-                    {
-                        this.AnswerCheckedOrder = this.QuestionViewModel.Options
-                                                      .Select(x => x.AnswerCheckedOrder ?? 0)
-                                                      .DefaultIfEmpty(0)
-                                                      .Max() + 1;
-                        this.YesAnswerCheckedOrder = this.QuestionViewModel.Options
-                                                         .Select(x => x.YesAnswerCheckedOrder ?? 0)
-                                                         .DefaultIfEmpty(0)
-                                                         .Max() + 1;
-                    }
-                    else
-                    {
-                        if (this.AnswerCheckedOrder.HasValue)
-                        {
-                            this.QuestionViewModel.Options
-                                .Where(x => x.AnswerCheckedOrder > this.AnswerCheckedOrder)
-                                .ForEach(x => x.AnswerCheckedOrder -= 1);
-                        }
-
-                        if (this.YesAnswerCheckedOrder.HasValue)
-                        {
-                            this.QuestionViewModel.Options
-                                .Where(x => x.YesAnswerCheckedOrder > this.YesAnswerCheckedOrder)
-                                .ForEach(x => x.YesAnswerCheckedOrder -= 1);
-                        }
-
-                        this.AnswerCheckedOrder = null;
-                        this.YesAnswerCheckedOrder = null;
-                    }
-                }
-
-                this.RaisePropertyChanged();
-                this.RaisePropertyChanged(() => YesSelected);
-                this.RaisePropertyChanged(() => NoSelected);
-            }
-        }
-
+        private bool yesSelected;
         public bool YesSelected
         {
-            get => this.Selected == true;
-            set
-            {
-                if (this.YesSelected == value)
-                    return;
-
-                var oldValue = this.Selected;
-                this.Selected = value;
-                this.OnAnswerChanged(oldValue);
-            }
+            get => this.yesSelected;
+            set => this.SetProperty(ref this.yesSelected, value);
         }
 
+        private bool noSelected;
         public bool NoSelected
         {
-            get => this.Selected == false;
-            set
-            {
-                if (this.NoSelected == value)
-                    return;
-
-                var oldValue = this.Selected;
-                this.Selected = !value;
-                this.OnAnswerChanged(oldValue);
-            }
+            get => this.noSelected;
+            set => this.SetProperty(ref this.noSelected, value);
         }
 
         private int? yesAnswerCheckedOrder;
-        private bool yesCanBeChecked = true;
-
         public int? YesAnswerCheckedOrder
         {
             get => this.yesAnswerCheckedOrder;
-            set
-            {
-                if (this.yesAnswerCheckedOrder == value)
-                    return;
-
-                this.yesAnswerCheckedOrder = value;
-                this.RaisePropertyChanged();
-            }
+            set => this.SetProperty(ref this.yesAnswerCheckedOrder, value);
         }
 
-        public int? AnswerCheckedOrder { get; set; }
-
-        public bool IsProtected { get; set; }
-
+        private bool yesCanBeChecked = true;
         public bool YesCanBeChecked
         {
-            get => yesCanBeChecked;
-            set => SetProperty(ref yesCanBeChecked, value);
+            get => this.yesCanBeChecked;
+            set => this.SetProperty(ref yesCanBeChecked, value);
         }
 
-        public async void RaiseToggleAnswer(YesNoEventArgs e)
+        public QuestionStateViewModel<YesNoQuestionAnswered> QuestionState { get; private set; }
+        public decimal Value { get; private set; }
+        public bool IsProtected { get; private set; }
+        public string YesItemTag { get; private set; }
+        public string NoItemTag { get; private set; }
+
+        public IMvxCommand SetYesAnswerCommand => new MvxCommand(SetYesAsync);
+
+        public IMvxAsyncCommand SetNoAnswerCommand => new MvxAsyncCommand(SetNoAsync);
+
+        public IMvxAsyncCommand RemoveAnswerCommand => new MvxAsyncCommand(RemoveAnswerAsync);
+
+        private async Task RemoveAnswerAsync()
         {
-            await this.QuestionViewModel.ToggleAnswerAsync(this, e.OldValue); 
+            if (!await this.HasConfirmationByRemovingRosterInstanceAsync()) return;
+
+            this.YesSelected = false;
+            this.NoSelected = false;
+
+            this.setAnswer.Invoke();
         }
 
-        public IMvxCommand RemoveAnswerCommand
+        private void SetYesAsync()
         {
-            get
+            this.NoSelected = false;
+            this.setAnswer?.Invoke();
+        }
+
+        private async Task SetNoAsync()
+        {
+            if (await this.HasConfirmationByRemovingRosterInstanceAsync())
             {
-                return new MvxCommand(() =>
-                {
-                    bool? oldValue = this.Selected;
-                    this.Selected = null;
-                    this.OnAnswerChanged(oldValue);
-                });
+                this.YesSelected = false;
+                this.setAnswer?.Invoke();
             }
+            else
+                this.NoSelected = false;
         }
 
-        protected virtual void OnAnswerChanged(bool? oldValue)
+        private async Task<bool> HasConfirmationByRemovingRosterInstanceAsync()
         {
-            this.AnswerChanged?.Invoke(this, new YesNoEventArgs(oldValue));
-        }
+            if (!this.isRosterSizeQuestion) return true;
+            if (!this.YesSelected) return true;
 
-        public string YesItemTag => this.QuestionViewModel.Identity + "_Opt_Yes_" + Value;
-        public string NoItemTag => this.QuestionViewModel.Identity + "_Opt_No_" + Value;
+            if (this.userInteraction.HasPendingUserInteractions) return false;
+
+            return await this.userInteraction.ConfirmAsync(UIResources.Interview_Questions_RemoveRowFromRosterMessage);
+        }
     }
-
-    public class YesNoEventArgs : EventArgs
-    {
-        public bool? OldValue { get; }
-
-        public YesNoEventArgs(bool? oldValue)
-        {
-            OldValue = oldValue;
-        }
-    } 
 }
