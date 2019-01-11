71b44774ad1140b928774bda8f00e01933fbf484
Roman Lytvyn <litvin.roma@gmail.com>
2019-01-11 13:00:22 +0200

    KP-12239 Attachments (video, audio) aren't sown on Supervisor mobile app. Fixed video and audio

--- src/Tests/WB.Tests.Unit/BoundedContexts/Interviewer/AttachmentContentStorageTests.cs
+++ src/Tests/WB.Tests.Unit/BoundedContexts/Interviewer/AttachmentContentStorageTests.cs
@@ -1,12 +1,9 @@
 ﻿using System.IO;
+using System.Threading.Tasks;
 using Moq;
 using NUnit.Framework;
 using WB.Core.GenericSubdomains.Portable;
 using WB.Core.Infrastructure.FileSystem;
-using WB.Core.SharedKernels.Enumerator.Implementation.Repositories;
-using WB.Core.SharedKernels.Enumerator.Services;
-using WB.Core.SharedKernels.Enumerator.Services.Infrastructure.Storage;
-using WB.Core.SharedKernels.Enumerator.Views;
 using WB.Core.SharedKernels.Questionnaire.Api;
 using WB.Tests.Abc;
 
@@ -15,7 +12,7 @@ namespace WB.Tests.Unit.BoundedContexts.Interviewer
     public class AttachmentContentStorageTests
     {
         [Test]
-        public void should_store_video_files_on_fileSystem()
+        public async Task should_store_video_files_on_fileSystem()
         {
             var fs = new Mock<IFileSystemAccessor>();
             var contentId = Id.g10.FormatGuid();
@@ -28,7 +25,7 @@ namespace WB.Tests.Unit.BoundedContexts.Interviewer
             fs.Setup(f => f.IsFileExists(cachedFile)).Returns(false);
             fs.Setup(f => f.IsDirectoryExists(Path.GetDirectoryName(cachedFile))).Returns(true);
             
-            storage.StoreAsync(new AttachmentContent
+            await storage.StoreAsync(new AttachmentContent
             {
                 Id = contentId,
                 ContentType = "video/create",
@@ -41,7 +38,7 @@ namespace WB.Tests.Unit.BoundedContexts.Interviewer
 
 
         [Test]
-        public void should_not_store_video_files_on_fileSystem_if_exists()
+        public async Task should_not_store_video_files_on_fileSystem_if_exists()
         {
             var fs = new Mock<IFileSystemAccessor>();
             var contentId = Id.g10.FormatGuid();
@@ -54,7 +51,7 @@ namespace WB.Tests.Unit.BoundedContexts.Interviewer
             fs.Setup(f => f.IsFileExists(cachedFile)).Returns(true);
             fs.Setup(f => f.IsDirectoryExists(Path.GetDirectoryName(cachedFile))).Returns(true);
             
-            storage.StoreAsync(new AttachmentContent
+            await storage.StoreAsync(new AttachmentContent
             {
                 Id = contentId,
                 ContentType = "video/destroy",
@@ -66,7 +63,7 @@ namespace WB.Tests.Unit.BoundedContexts.Interviewer
         }
    
         [Test]
-        public void should_create_directory_if_not_exists_while_storing_video()
+        public async Task should_create_directory_if_not_exists_while_storing_video()
         {
             var fs = new Mock<IFileSystemAccessor>();
             var contentId = Id.g10.FormatGuid();
@@ -79,7 +76,7 @@ namespace WB.Tests.Unit.BoundedContexts.Interviewer
             fs.Setup(f => f.IsFileExists(cachedFile)).Returns(false);
             fs.Setup(f => f.IsDirectoryExists(cacheDir)).Returns(false);
             
-            storage.StoreAsync(new AttachmentContent
+            await storage.StoreAsync(new AttachmentContent
             {
                 Id = contentId,
                 ContentType = "video/content",
@@ -92,7 +89,7 @@ namespace WB.Tests.Unit.BoundedContexts.Interviewer
         }
 
         [Test]
-        public void should_not_store_image_files()
+        public async Task should_not_store_image_files()
         {
             var fs = new Mock<IFileSystemAccessor>();
             var contentId = Id.g10.FormatGuid();
@@ -105,7 +102,7 @@ namespace WB.Tests.Unit.BoundedContexts.Interviewer
             fs.Setup(f => f.IsFileExists(cachedFile)).Returns(false);
             fs.Setup(f => f.IsDirectoryExists(cacheDir)).Returns(false);
 
-            storage.StoreAsync(new AttachmentContent
+            await storage.StoreAsync(new AttachmentContent
             {
                 Id = contentId,
                 ContentType = "image/magic",
@@ -142,5 +139,58 @@ namespace WB.Tests.Unit.BoundedContexts.Interviewer
             Assert.That(contentFromStorage, Is.EqualTo(content));
         }
 
+        [Test]
+        public void when_Exists_and_no_metadata_by_attachment_in_db_then_should_be_false()
+        {
+            // arrange
+            var contentId = Id.g10.FormatGuid();
+            var storage = Create.Service.AttachmentContentStorage();
+
+            // act
+            var isExists = storage.Exists(contentId);
+
+            // assert
+            Assert.That(isExists, Is.False);
+        }
+
+        [Test]
+        public void when_Exists_and_no_file_by_attachment_on_file_system_then_should_be_false()
+        {
+            // arrange
+            var contentId = Id.g10.FormatGuid();
+            var attachmentContentMetadataRepository = Create.Storage.SqliteInmemoryStorage(
+                Create.Entity.AttachmentContentMetadata("image/png", contentId));
+            var fileSystemAccessor = Mock.Of<IFileSystemAccessor>(x => x.IsFileExists(It.IsAny<string>()) == true);
+
+            var storage = Create.Service.AttachmentContentStorage(
+                attachmentContentMetadataRepository: attachmentContentMetadataRepository,
+                files: fileSystemAccessor);
+
+            // act
+            var isExists = storage.Exists(contentId);
+
+            // assert
+            Assert.That(isExists, Is.True);
+        }
+
+        [Test]
+        public void when_Exists_and_no_attachment_content_in_db_and_on_file_system_then_should_be_false()
+        {
+            // arrange
+            var contentId = Id.g10.FormatGuid();
+            var attachmentContentMetadataRepository = Create.Storage.SqliteInmemoryStorage(
+                Create.Entity.AttachmentContentMetadata("image/png", contentId));
+            var fileSystemAccessor = Mock.Of<IFileSystemAccessor>(x => x.IsFileExists(It.IsAny<string>()) == false);
+
+            var storage = Create.Service.AttachmentContentStorage(
+                attachmentContentMetadataRepository: attachmentContentMetadataRepository,
+                files: fileSystemAccessor);
+
+            // act
+            var isExists = storage.Exists(contentId);
+
+            // assert
+            Assert.That(isExists, Is.False);
+        }
     }
 }
