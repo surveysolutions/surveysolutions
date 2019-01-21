074223b6714559d33a0cea042a3d9b19dfb3c345
Pavel Stovpivskyi <sby@rambler.ru>
2019-01-21 17:01:38 +0200

    KP-12213 Export audio recording on export side

---
+++ src/Services/Export/WB.Services.Export/Interview/AudioAuditView.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WB.Services.Export.Interview
+{
+    public class AudioAuditView
+    {
+        public Guid InterviewId { get; set; }
+        public AudioAuditFileView[] Files { get; set; }
+    }
+
+    public class AudioAuditFileView
+    {
+        public string FileName { get; set; }
+        public string ContentType { get; set; }
+    }
+}
