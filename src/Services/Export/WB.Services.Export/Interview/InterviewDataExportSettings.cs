4147de2e846f3ca95edb99f1a42ff4e2846e2f37
Pavel Stovpivskyi <sby@rambler.ru>
2019-01-28 18:24:35 +0200

    set default value to audio audit folder

--- src/Services/Export/WB.Services.Export/Interview/InterviewDataExportSettings.cs
+++ src/Services/Export/WB.Services.Export/Interview/InterviewDataExportSettings.cs
@@ -4,7 +4,7 @@
     {
         public int MaxRecordsCountPerOneExportQuery { get; set; } = 160;
         public string DirectoryPath { get; set; } = ".export";
-        public string AudioAuditFolderName { get; set; }
+        public string AudioAuditFolderName { get; set; } = "AudioAudit";
         public int ParadataQueryLimit { get; set; } = 4;
     }
 }
