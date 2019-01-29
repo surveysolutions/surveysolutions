07372acf10ce2fd3f6cc6700448be80e5acfc8dc
Pavel Stovpivskyi <sby@rambler.ru>
2019-01-29 19:31:16 +0200

    KP-12457 tests

---
+++ src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/Web/InterviewerInterviewsControllerTests/v2/when_marking_interview_as_received_by_interviewer.cs
@@ -0,0 +1,30 @@
+using System;
+using Moq;
+using WB.Core.BoundedContexts.Headquarters.Services;
+using WB.Core.Infrastructure.CommandBus;
+using WB.Core.SharedKernels.DataCollection.Commands.Interview;
+using WB.UI.Headquarters.API.DataCollection.Interviewer.v2;
+
+
+namespace WB.Tests.Unit.SharedKernels.SurveyManagement.Web.ApiTests.InterviewerInterviewsControllerTests.v2
+{
+    internal class when_marking_interview_as_received_by_interviewer : InterviewsApiV2ControllerTestsContext
+    {
+        [NUnit.Framework.OneTimeSetUp] public void context () {
+            controller = CreateInterviewerInterviewsController(
+                commandService: mockOfCommandService.Object,
+                authorizedUser: Mock.Of<IAuthorizedUser>(u => u.IsInterviewer == true));
+            BecauseOf();
+        }
+
+        public void BecauseOf() => controller.LogInterviewAsSuccessfullyHandled(interviewId);
+
+        [NUnit.Framework.Test] public void should_executed_command_to_mark_interview_as_received () =>
+            mockOfCommandService.Verify(x=>x.Execute(Moq.It.IsAny<MarkInterviewAsReceivedByInterviewer>(), Moq.It.IsAny<string>()), Times.Once);
+        
+        
+        private static InterviewsApiV2Controller controller;
+        private static readonly Guid interviewId = Guid.Parse("11111111111111111111111111111111");
+        private static readonly Mock<ICommandService> mockOfCommandService = new Mock<ICommandService>();
+    }
+}
