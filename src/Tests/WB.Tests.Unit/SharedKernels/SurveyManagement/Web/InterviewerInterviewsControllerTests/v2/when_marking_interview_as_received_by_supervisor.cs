fedde791782d367e8a730df6d129a6763da8a1a6
Pavel Stovpivskyi <630198+spuv@users.noreply.github.com>
2019-01-29 19:31:16 +0200

    KP-12457 tests

--- src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/Web/InterviewerInterviewsControllerTests/v2/when_marking_interview_as_received_by_supervisor.cs
+++ src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/Web/InterviewerInterviewsControllerTests/v2/when_marking_interview_as_received_by_supervisor.cs
@@ -1,5 +1,6 @@
 using System;
 using Moq;
+using WB.Core.BoundedContexts.Headquarters.Services;
 using WB.Core.Infrastructure.CommandBus;
 using WB.Core.SharedKernels.DataCollection.Commands.Interview;
 using WB.UI.Headquarters.API.DataCollection.Interviewer.v2;
@@ -7,18 +8,19 @@ using WB.UI.Headquarters.API.DataCollection.Interviewer.v2;
 
 namespace WB.Tests.Unit.SharedKernels.SurveyManagement.Web.ApiTests.InterviewerInterviewsControllerTests.v2
 {
-    internal class when_marking_interview_as_received : InterviewsApiV2ControllerTestsContext
+    internal class when_marking_interview_as_received_by_supervisor : InterviewsApiV2ControllerTestsContext
     {
         [NUnit.Framework.OneTimeSetUp] public void context () {
             controller = CreateInterviewerInterviewsController(
-                commandService: mockOfCommandService.Object);
+                commandService: mockOfCommandService.Object,
+                authorizedUser: Mock.Of<IAuthorizedUser>(u => u.IsInterviewer == false && u.IsSupervisor == true));
             BecauseOf();
         }
 
         public void BecauseOf() => controller.LogInterviewAsSuccessfullyHandled(interviewId);
 
         [NUnit.Framework.Test] public void should_executed_command_to_mark_interview_as_received () =>
-            mockOfCommandService.Verify(x=>x.Execute(Moq.It.IsAny<MarkInterviewAsReceivedByInterviewer>(), Moq.It.IsAny<string>()), Times.Once);
+            mockOfCommandService.Verify(x=>x.Execute(Moq.It.IsAny<MarkInterviewAsReceivedBySupervisor>(), Moq.It.IsAny<string>()), Times.Once);
         
         
         private static InterviewsApiV2Controller controller;
