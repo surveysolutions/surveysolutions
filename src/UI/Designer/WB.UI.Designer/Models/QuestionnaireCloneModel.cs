fbd500de95773fdf4d20cf1e58bac1415a8bb502
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2019-01-31 16:43:49 -0500

    KP-12467 Questionnaire title can't contain unicode characters

--- src/UI/Designer/WB.UI.Designer/Models/QuestionnaireCloneModel.cs
+++ src/UI/Designer/WB.UI.Designer/Models/QuestionnaireCloneModel.cs
@@ -11,7 +11,7 @@ namespace WB.UI.Designer.Models
         public Guid Id { get; set; }
 
         [Required(ErrorMessageResourceType = typeof(ErrorMessages), ErrorMessageResourceName = nameof(ErrorMessages.QuestionnaireTitle_required))]
-        [RegularExpression(QuestionnaireVerifications.QuestionnaireTitleRegularExpression, ErrorMessageResourceType = typeof(ErrorMessages), ErrorMessageResourceName = nameof(ErrorMessages.QuestionnaireTitle_rules))]
+        //[RegularExpression(QuestionnaireVerifications.QuestionnaireTitleRegularExpression, ErrorMessageResourceType = typeof(ErrorMessages), ErrorMessageResourceName = nameof(ErrorMessages.QuestionnaireTitle_rules))]
         [StringLength(AbstractVerifier.MaxTitleLength, ErrorMessageResourceName = nameof(ErrorMessages.QuestionnaireTitle_MaxLength), ErrorMessageResourceType = typeof(ErrorMessages), ErrorMessage = null)]
         public string Title { get; set; }
     }
