331b2c41521607031c15dcc9a9174283b879bacd
Pavel Stovpivskyi <630198+spuv@users.noreply.github.com>
2019-02-01 18:18:11 +0200

    KP-12466 added migrate data for NaN

---
+++ src/Infrastructure/WB.Persistence.Headquarters/Migrations/ReadSide/M001-100/M201902011754_MigrateDataForNaNValuesForDoubleInTabulateReport.cs
@@ -0,0 +1,23 @@
+﻿using FluentMigrator;
+
+namespace WB.Persistence.Headquarters.Migrations.ReadSide
+{
+    [Migration(201902011754)]
+    public class M201902011754_MigrateDataForNaNValuesForDoubleInTabulateReport : Migration
+    {
+        public override void Up()
+        {
+            Execute.Sql(@"SELECT ids.*, r.*
+                            FROM (SELECT distinct v.interviewid
+                                    FROM readside.interviews v
+                                   WHERE v.asdouble = 'NaN'::float
+                                 ) as ids,
+                         LATERAL readside.update_report_table_data(interviewid) r");
+        }
+
+        public override void Down()
+        {
+
+        }
+    }
+}
