52192a293045e532d1c93c0ce4c79bf08a6e6e0c
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2019-02-05 15:31:28 +0200

    KP-12440 Use approvals for testing query builder

--- src/Services/Export/WB.Services.Export.Tests/CsvExport/Exporters/InterviewsExporterTests.cs
+++ src/Services/Export/WB.Services.Export.Tests/CsvExport/Exporters/InterviewsExporterTests.cs
@@ -61,7 +61,7 @@ namespace WB.Services.Export.Tests.CsvExport.Exporters
             var interviewFactory = new Mock<IInterviewFactory>();
             interviewFactory.SetupIgnoreArgs(x => x.GetInterviewDataLevels(null, null))
                 .Returns(new Dictionary<string, InterviewLevel>());
-            interviewFactory.SetupIgnoreArgs(x => x.GetInterviewEntities(null, null))
+            interviewFactory.SetupIgnoreArgs(x => x.GetInterviewEntities(null, null,  null))
                 .Returns(Task.FromResult(new List<InterviewEntity>()));
 
             var exporter = Create.InterviewsExporter(csvWriter, interviewFactory.Object);
---
+++ src/Services/Export/WB.Services.Export.Tests/Services/Interview/InterviewQueryBuilderTests.cs
@@ -0,0 +1,31 @@
+﻿using ApprovalTests;
+using ApprovalTests.Core;
+using ApprovalTests.Namers;
+using ApprovalTests.Reporters;
+using NUnit.Framework;
+using WB.Services.Export.Interview;
+using WB.Services.Export.Questionnaire;
+
+namespace WB.Services.Export.Tests.Services.Interview
+{
+    [TestOf(typeof(InterviewQueryBuilder))]
+    [UseApprovalSubdirectory("approved")]
+    [IgnoreLineEndings(true)]
+    [UseReporter(typeof(DiffReporter), typeof(NUnitReporter))]
+    public class InterviewQueryBuilderTests
+    {
+        [Test]
+        public void should_build_query_for_group_with_question()
+        {
+            var questionnaire = Create.QuestionnaireDocument(Id.gA, 6, "quest", Create.Group(Id.gB, "group1",
+                Create.NumericIntegerQuestion(variable: "num1")));
+            questionnaire.ConnectChildrenWithParent();
+
+            // Act
+            var query = InterviewQueryBuilder.GetInterviewsQuery(Create.Tenant(name: "tenant"), questionnaire.Find<Group>(Id.gB));
+
+            // Assert
+            Approvals.Verify(query);
+        }
+    }
+}
