be6d2177f546e69d9a0a0cb2719fe7d26a476ec9
Victor Hladkikh <me@alfeg.net>
2019-02-06 16:05:44 +0200

    KP-12438 Initial template for functional events handler

---
+++ src/Services/Export/WB.Services.Export/Handlers/IFunctionalHandler.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using WB.Services.Export.Events.Interview.Base;
+
+namespace WB.Services.Export.Handlers
+{
+    public interface IFunctionalHandler<TState>
+    {
+        TState GetState(CancellationToken cancellationToken);
+
+        Task ApplyEvent<TEvent>(TState state, PublishedEvent<TEvent> @event, CancellationToken cancellationToken) where TEvent: IEvent;
+
+        Task SaveState(TState state, CancellationToken cancellationToken);
+    }
+}
---
+++ src/Services/Export/WB.Services.Export/Infrastructure/TenantContext.cs
@@ -0,0 +1,9 @@
+﻿using WB.Services.Infrastructure.Tenant;
+
+namespace WB.Services.Export.Infrastructure
+{
+    public class TenantContext : ITenantContext
+    {
+        public TenantInfo Tenant { get; set; }
+    }
+}
