0b2d0bc5ea31194caee8837b78210599432eb477
Victor Gladkikh <me@alfeg.net>
2019-02-06 16:13:48 +0200

    KP-12438 Initial template for functional events handler #3

--- src/Services/Export/WB.Services.Export/Handlers/IFunctionalHandler.cs
+++ src/Services/Export/WB.Services.Export/Handlers/IFunctionalHandler.cs
@@ -7,13 +7,13 @@ namespace WB.Services.Export.Handlers
 {
     public interface IFunctionalHandler<TState>
     {
-        TState GetState(CancellationToken cancellationToken);
+        TState GetStateAsync(CancellationToken cancellationToken);
 
-        Task SaveState(TState state, CancellationToken cancellationToken);
+        Task SaveStateAsync(TState state, CancellationToken cancellationToken);
     }
 
     public interface IEventHandler<TState, TEvent> where TEvent : IEvent
     {
-        Task<TState> Handle(TState state, PublishedEvent<TEvent> @event, CancellationToken cancellationToken);
+        Task<TState> HandleAsync(TState state, PublishedEvent<TEvent> @event, CancellationToken cancellationToken);
     }
 }
