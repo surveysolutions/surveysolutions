1cb1841529e5a2ef5c4eeea36e031f63128b5e48
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2019-02-06 15:59:24 +0200

    KP-12440 fixed table names for enablement and validity

--- src/Services/Export/WB.Services.Export/Interview/InterviewFactory.cs
+++ src/Services/Export/WB.Services.Export/Interview/InterviewFactory.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Dapper;
@@ -15,58 +14,6 @@ using WB.Services.Infrastructure.Tenant;
 
 namespace WB.Services.Export.Interview
 {
-    public static class InterviewQueryBuilder
-    {
-        public static string GetInterviewsQuery(TenantInfo tenant, Group group)
-        {
-            if (!group.Children.Any()) throw new ArgumentException("Cannot build query for group without questions");
-
-            string BuildSelectColumns(string alias)
-            {
-                List<string> columnsCollector = new List<string>();
-
-                foreach (var questionnaireEntity in group.Children)
-                {
-                    if (questionnaireEntity is Question question)
-                    {
-                        columnsCollector.Add($" {alias}.\"{question.ColumnName}\" as \"{alias}_{question.ColumnName}\" ");
-                    }
-
-                    if (questionnaireEntity is Variable variable)
-                    {
-                        columnsCollector.Add($" {alias}.\"{variable.ColumnName}\" as \"{alias}_{variable.ColumnName}\" ");
-                    }
-                }
-
-                return string.Join(", ", columnsCollector);
-            }
-
-            StringBuilder query = new StringBuilder($"select data.interview_id as data_interview_id, ");
-            if (group.IsInsideRoster)
-            {
-                query.Append("data.roster_vector as data_roster_vector, ");
-            }
-
-            query.Append(BuildSelectColumns("data"));
-            query.Append(", ");
-            query.Append(BuildSelectColumns("enablement"));
-            query.Append(", ");
-            query.Append(BuildSelectColumns("validity"));
-
-            query.AppendLine($" from ");
-            query.AppendLine($"\"{tenant.Name}\".\"{group.TableName}\" data ");
-                
-            query.AppendFormat("    INNER JOIN \"{0}\".\"{1}_enablement\" enablement ON data.interview_id = enablement.interview_id{2}",
-                tenant.Name, group.TableName, Environment.NewLine);
-            query.AppendFormat("    INNER JOIN \"{0}\".\"{1}_validity\" validity ON data.interview_id = validity.interview_id{2}",
-                tenant.Name, group.TableName, Environment.NewLine);
-            query.AppendFormat(" WHERE data.interview_id = ANY(@ids)");
-
-            return query.ToString();
-        }
-
-    }
-
     public class InterviewFactory : IInterviewFactory
     {
         private readonly ITenantApi<IHeadquartersApi> tenantApi;
@@ -88,7 +35,8 @@ namespace WB.Services.Export.Interview
                 {
                     await connection.OpenAsync();
 
-                    var reader = await connection.ExecuteReaderAsync(InterviewQueryBuilder.GetInterviewsQuery(tenant, group), new {ids = interviewsId});
+                    var interviewsQuery = InterviewQueryBuilder.GetInterviewsQuery(tenant, @group);
+                    var reader = await connection.ExecuteReaderAsync(interviewsQuery, new {ids = interviewsId});
 
                     while (reader.Read())
                     {
