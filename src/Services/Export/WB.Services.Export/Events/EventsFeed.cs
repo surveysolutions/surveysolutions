f580c08496c05ec6ed9c6f9f2e7b77b7731109f5
Victor Hladkikh <me@alfeg.net>
2019-02-07 16:08:54 +0200

    KP-12438 Trying to solve memory pressure on events deserialization

--- src/Services/Export/WB.Services.Export/Events/EventsFeed.cs
+++ src/Services/Export/WB.Services.Export/Events/EventsFeed.cs
@@ -8,7 +8,7 @@ namespace WB.Services.Export.Events
     public class EventsFeed
     {
         public long Total { get; set; }
-        public long NextSequence { get; set; }
+        public long? NextSequence { get; set; }
         public List<Event> Events { get; set; }
     }
 }
