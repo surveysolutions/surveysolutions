aa300604597c1f0d6a76c1b2558631a2bc1bac44
Pavel Stovpivskyi <sby@rambler.ru>
2019-02-07 12:59:56 +0200

    KP-12439 draft version of denormolizer to collect interview data

---
+++ src/Services/Export/WB.Services.Export/InterviewDataStorage/InterviewQuestionnaireReferenceStorage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using WB.Services.Export.Questionnaire;
+
+namespace WB.Services.Export.InterviewDataStorage
+{
+    public interface IInterviewQuestionnaireReferenceStorage
+    {
+        QuestionnaireId GetQuestionnaireIdByInterviewId(Guid interviewId);
+
+        void AddInterviewQuestionnaireReference(Guid interviewId, QuestionnaireId questionnaireId);
+    }
+
+    public class InterviewQuestionnaireReferenceStorage : IInterviewQuestionnaireReferenceStorage
+    {
+        public QuestionnaireId GetQuestionnaireIdByInterviewId(Guid interviewId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddInterviewQuestionnaireReference(Guid interviewId, QuestionnaireId questionnaireId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
