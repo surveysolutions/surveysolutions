20b11daceb9bf317044b99391ad031707cdda5a2
Pavel Stovpivskyi <630198+spuv@users.noreply.github.com>
2019-02-07 14:09:31 +0200

    KP-12438 added async to InterviewQuestionnaireReferenceStorage

--- src/Services/Export/WB.Services.Export/InterviewDataStorage/InterviewQuestionnaireReferenceStorage.cs
+++ src/Services/Export/WB.Services.Export/InterviewDataStorage/InterviewQuestionnaireReferenceStorage.cs
@@ -1,40 +1,100 @@
 ﻿using System;
+using System.Data;
+using System.Data.Common;
+using System.Threading;
+using System.Threading.Tasks;
 using WB.Services.Export.Questionnaire;
 using WB.Services.Export.Storage;
+using WB.Services.Infrastructure.Tenant;
 
 namespace WB.Services.Export.InterviewDataStorage
 {
     public interface IInterviewQuestionnaireReferenceStorage
     {
-        QuestionnaireId GetQuestionnaireIdByInterviewId(Guid interviewId);
+        Task<QuestionnaireId> GetQuestionnaireIdByInterviewIdAsync(Guid interviewId, CancellationToken cancellationToken);
 
-        void AddInterviewQuestionnaireReference(Guid interviewId, QuestionnaireId questionnaireId);
+        Task AddInterviewQuestionnaireReferenceAsync(Guid interviewId, QuestionnaireId questionnaireId, CancellationToken cancellationToken);
 
-        void RemoveInterviewQuestionnaireReference(Guid interviewId);
+        Task RemoveInterviewQuestionnaireReferenceAsync(Guid interviewId, CancellationToken cancellationToken);
     }
 
     public class InterviewQuestionnaireReferenceStorage : IInterviewQuestionnaireReferenceStorage
     {
+        private const string InterviewQuestionnaireReferenceTableName = "interview_questionnaire_reference";
+        private const string InterviewColumnName = "interviewid";
+        private const string QuestionnaireColumnName = "questionnaireid";
+
+        private readonly TenantInfo tenantInfo;
         private readonly ISession session;
 
-        public InterviewQuestionnaireReferenceStorage(ISession session)
+        public InterviewQuestionnaireReferenceStorage(TenantInfo tenantInfo, ISession session)
         {
+            this.tenantInfo = tenantInfo;
             this.session = session;
+
+            EnshureTableExists();
         }
 
-        public QuestionnaireId GetQuestionnaireIdByInterviewId(Guid interviewId)
+        public async Task<QuestionnaireId> GetQuestionnaireIdByInterviewIdAsync(Guid interviewId, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var commandText = $"SELECT questionnaireid FROM \"{tenantInfo.Name}\".\"{InterviewQuestionnaireReferenceTableName}\"" +
+                              $"  WHERE {InterviewColumnName} = @interviewId;";
+            var command = this.session.Connection.CreateCommand();
+            command.CommandText = commandText;
+            AddParameter(command, "@interviewId", DbType.Guid, interviewId);
+            var questionnaireId = (await command.ExecuteScalarAsync(cancellationToken)) as string;
+            return new QuestionnaireId(questionnaireId);
         }
 
-        public void AddInterviewQuestionnaireReference(Guid interviewId, QuestionnaireId questionnaireId)
+        public Task AddInterviewQuestionnaireReferenceAsync(Guid interviewId, QuestionnaireId questionnaireId, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var text = $"INSERT INTO \"{tenantInfo.Name}\".\"{InterviewQuestionnaireReferenceTableName}\" ({InterviewColumnName}, {QuestionnaireColumnName})" +
+                       $"           VALUES(@interviewId, @questionnaireId);";
+
+            var command = this.session.Connection.CreateCommand();
+            command.CommandText = text;
+            AddParameter(command, "@interviewId", DbType.Guid, interviewId);
+            AddParameter(command, "@questionnaireId", DbType.String, questionnaireId.Id);
+            return command.ExecuteNonQueryAsync(cancellationToken);
         }
 
-        public void RemoveInterviewQuestionnaireReference(Guid interviewId)
+        public Task RemoveInterviewQuestionnaireReferenceAsync(Guid interviewId, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var text = $"DELETE FROM \"{tenantInfo.Name}\".\"{InterviewQuestionnaireReferenceTableName}\" " +
+                       $"      WHERE {InterviewColumnName} = @interviewId;";
+
+            var command = this.session.Connection.CreateCommand();
+            command.CommandText = text;
+            AddParameter(command, "@interviewId", DbType.Guid, interviewId);
+            return command.ExecuteNonQueryAsync(cancellationToken);
+        }
+
+        private static void AddParameter(DbCommand command, string name, DbType dbType, object value)
+        {
+            var parameter = command.CreateParameter();
+            parameter.DbType = dbType;
+            parameter.ParameterName = name;
+            parameter.Value = value;
+            command.Parameters.Add(parameter);
+        }
+
+        private static bool doesExistTable = false;
+
+        protected void EnshureTableExists()
+        {
+            if (doesExistTable) return;
+
+            var command = $"CREATE TABLE IF NOT EXISTS \"{tenantInfo.Name}\".\"{InterviewQuestionnaireReferenceTableName}\"( " +
+                          $"     {InterviewColumnName}  uuid PRIMARY KEY," +
+                          $" {QuestionnaireColumnName}  text NOT NULL" +
+                          $")";
+            using (var sqlCommand = session.Connection.CreateCommand())
+            {
+                sqlCommand.CommandText = command;
+                sqlCommand.ExecuteNonQuery();
+            }
+
+            doesExistTable = true;
         }
     }
 }
