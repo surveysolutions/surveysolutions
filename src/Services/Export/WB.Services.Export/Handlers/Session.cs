032349f3faf61d78cc717597e214d856c6b1f607
Pavel Stovpivskyi <630198+spuv@users.noreply.github.com>
2019-02-07 13:28:58 +0200

    KP-12439 added dummy InterviewQuestionnaireReferenceStorage

--- src/Services/Export/WB.Services.Export/Handlers/Session.cs
+++ src/Services/Export/WB.Services.Export/Handlers/Session.cs
@@ -1,9 +1,10 @@
 ﻿using System.Data;
+using System.Data.Common;
 
 namespace WB.Services.Export.Handlers
 {
     public class Session : ISession
     {
-        public IDbConnection Connection { get; set; }
+        public DbConnection Connection { get; set; }
     }
 }
