5664fe1fbe14f81eeb6e33ec78c76b9524d4a01f
Pavel Stovpivskyi <630198+spuv@users.noreply.github.com>
2019-02-12 16:42:48 +0200

    KP-12439 added table with enablement for all rosters

--- src/Services/Export/WB.Services.Export/InterviewDataStorage/InterviewDataExport/UpdateValueInfo.cs
+++ src/Services/Export/WB.Services.Export/InterviewDataStorage/InterviewDataExport/UpdateValueInfo.cs
@@ -11,21 +11,5 @@ namespace WB.Services.Export.InterviewDataStorage.InterviewDataExport
         public NpgsqlDbType ValueType { get; set; }
 
         public override string ToString() => ColumnName + "-" + Value + "-" + ValueType;
-
-        public override bool Equals(object obj)
-        {
-            var item = obj as UpdateValueInfo;
-            if (item == null)
-                return false;
-
-            return (object.ReferenceEquals(this.ColumnName, item.ColumnName) || this.ColumnName != null && this.ColumnName.Equals(item.ColumnName))
-                   && (object.ReferenceEquals(this.Value, item.Value) || this.Value != null && this.Value.Equals(item.Value))
-                   && this.ValueType.Equals(item.ValueType);
-        }
-
-        public override int GetHashCode()
-        {
-            return this.ColumnName?.GetHashCode() ?? 0 ^ this.Value?.GetHashCode() ?? 0 ^ this.ValueType.GetHashCode();
-        }
     }
 }
