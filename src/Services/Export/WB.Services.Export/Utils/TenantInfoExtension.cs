974ce42ce9eba7f387eb7623c6d5c0bd6257bd21
Victor Hladkikh <me@alfeg.net>
2019-02-12 12:28:53 +0200

    KP-12440 Added tenantName schema

---
+++ src/Services/Export/WB.Services.Export/Utils/TenantInfoExtension.cs
@@ -0,0 +1,9 @@
+﻿using WB.Services.Infrastructure.Tenant;
+
+namespace WB.Services.Export
+{
+    public static class TenantInfoExtension
+    {
+        public static string SchemaName(this TenantInfo tenantInfo) => tenantInfo.Name + "_" + tenantInfo.Id;
+    }
+}
