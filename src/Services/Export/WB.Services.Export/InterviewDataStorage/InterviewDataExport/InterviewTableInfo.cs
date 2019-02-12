610c5570a32d4d28811fcac523f8516d7c8ad84a
Pavel Stovpivskyi <sby@rambler.ru>
2019-02-12 15:45:20 +0200

    KP-12439 hide dictionaries in state

---
+++ src/Services/Export/WB.Services.Export/InterviewDataStorage/InterviewDataExport/InterviewTableInfo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WB.Services.Export.InterviewDataStorage.InterviewDataExport
+{
+    public class InterviewTableInfo
+    {
+        public string TableName { get; set; }
+        public IEnumerable<Guid> InterviewIds { get; set; }
+    }
+}
---
+++ src/Services/Export/WB.Services.Export/InterviewDataStorage/InterviewDataExport/UpdateValueForTableRowInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace WB.Services.Export.InterviewDataStorage.InterviewDataExport
+{
+    public class UpdateValueForTableRowInfo
+    {
+        public string TableName { get; set; }
+        public RosterInfo RosterLevelInfo { get; set; }
+        public IEnumerable<UpdateValueInfo> UpdateValuesInfo { get; set; }
+    }
+}
