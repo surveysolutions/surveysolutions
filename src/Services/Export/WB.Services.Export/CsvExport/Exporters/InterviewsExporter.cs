c82f979d1573f1aba22d600b969222500e5192a2
Roman Lytvyn <litvin.roma@gmail.com>
2019-02-12 17:41:48 +0200

    Revert "KP-12490 Export: no section's varaible name in export_readme file"
    This reverts commit 1c0a783f9dce4866502d25d73b54d03e0e7bb9f4.

--- src/Services/Export/WB.Services.Export/CsvExport/Exporters/InterviewsExporter.cs
+++ src/Services/Export/WB.Services.Export/CsvExport/Exporters/InterviewsExporter.cs
@@ -319,8 +319,6 @@ namespace WB.Services.Export.CsvExport.Exporters
             var result = new List<string[]>();
             foreach (var headerItem in headerStructureForLevel.HeaderItems.Values)
             {
-                if(headerItem is ExportedGroupHeaderItem) continue;
-
                 var questionHeaderItem = headerItem as ExportedQuestionHeaderItem;
                 var variableHeaderItem = headerItem as ExportedVariableHeaderItem;
 
