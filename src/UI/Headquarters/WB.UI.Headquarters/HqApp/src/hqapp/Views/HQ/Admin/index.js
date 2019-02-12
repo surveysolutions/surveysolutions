b15cd22aa7af3a409cbe0de6484a51bde32cfc2b
Slava Scherbak <slava.scherbak@gmail.com>
2019-02-12 14:25:05 -0500

    KP-12509: added empty page to set up email providers

---
+++ src/UI/Headquarters/WB.UI.Headquarters/HqApp/src/hqapp/Views/HQ/Admin/index.js
@@ -0,0 +1,27 @@
+import EmailProviders from "./EmailProviders"
+
+
+import Vue from "vue"
+export default class AdminComponent {
+    constructor(rootStore) {
+        this.rootStore = rootStore;
+    }
+
+    get routes() {
+        return [{
+                path: '/Settings/EmailProviders',
+                component: EmailProviders
+            }
+        ]
+    }
+
+    initialize() {
+        const VeeValidate = require('vee-validate');
+        Vue.use(VeeValidate);
+        const installApi = require("~/webinterview/api").install
+
+        installApi(Vue, {
+            store: this.rootStore
+        });
+    }
+}
