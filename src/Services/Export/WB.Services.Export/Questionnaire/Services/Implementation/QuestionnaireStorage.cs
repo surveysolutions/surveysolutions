b5c4990402c002357b32ed5cbc2b406d2017914b
Victor Hladkikh <me@alfeg.net>
2019-02-13 11:42:38 +0200

    KP-12440 Proper lock for database creation per questionnaire

--- src/Services/Export/WB.Services.Export/Questionnaire/Services/Implementation/QuestionnaireStorage.cs
+++ src/Services/Export/WB.Services.Export/Questionnaire/Services/Implementation/QuestionnaireStorage.cs
@@ -2,6 +2,7 @@
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using WB.Services.Export.Infrastructure;
 using WB.Services.Export.InterviewDataStorage;
@@ -13,15 +14,18 @@ namespace WB.Services.Export.Questionnaire.Services.Implementation
     internal class QuestionnaireStorage : IQuestionnaireStorage
     {
         private readonly ITenantApi<IHeadquartersApi> tenantApi;
+        private readonly ILogger<QuestionnaireStorage> logger;
         private readonly IMemoryCache memoryCache;
         private readonly IInterviewDatabaseInitializer interviewDatabaseInitializer;
         private readonly JsonSerializerSettings serializer;
-        private static object schemaLock = new object();
 
-        public QuestionnaireStorage(ITenantApi<IHeadquartersApi> tenantApi, IMemoryCache memoryCache,
+        public QuestionnaireStorage(ITenantApi<IHeadquartersApi> tenantApi,
+            ILogger<QuestionnaireStorage> logger,
+            IMemoryCache memoryCache,
             IInterviewDatabaseInitializer interviewDatabaseInitializer)
         {
             this.tenantApi = tenantApi;
+            this.logger = logger;
             this.memoryCache = memoryCache;
             this.interviewDatabaseInitializer = interviewDatabaseInitializer;
             this.serializer = new JsonSerializerSettings
@@ -31,25 +35,47 @@ namespace WB.Services.Export.Questionnaire.Services.Implementation
             };
         }
 
+        private static readonly SemaphoreSlim CacheLock = new SemaphoreSlim(1);
+
         public async Task<QuestionnaireDocument> GetQuestionnaireAsync(TenantInfo tenant, QuestionnaireId questionnaireId, CancellationToken token = default)
         {
-            return await this.memoryCache.GetOrCreateAsync($"{nameof(QuestionnaireStorage)}:{tenant}:{questionnaireId}",
-                async entry =>
+            var key = $"{nameof(QuestionnaireStorage)}:{tenant}:{questionnaireId}";
+
+            if (memoryCache.TryGetValue(key, out var result))
+            {
+                return (QuestionnaireDocument)result;
+            }
+
+            await CacheLock.WaitAsync(token);
+
+            try
+            {
+                if (memoryCache.TryGetValue(key, out result))
                 {
-                    var questionnaireDocument = await this.tenantApi.For(tenant).GetQuestionnaireAsync(questionnaireId);
+                    return (QuestionnaireDocument)result;
+                }
 
-                    var questionnaire = JsonConvert.DeserializeObject<QuestionnaireDocument>(questionnaireDocument, serializer);
-                    entry.SlidingExpiration = TimeSpan.FromMinutes(1);
-                    questionnaire.QuestionnaireId = questionnaireId;
+                var questionnaireDocument = await this.tenantApi.For(tenant).GetQuestionnaireAsync(questionnaireId);
+                var questionnaire = JsonConvert.DeserializeObject<QuestionnaireDocument>(questionnaireDocument, serializer);
+                
+                questionnaire.QuestionnaireId = questionnaireId;
 
-                    lock (schemaLock)
-                    {
-                        interviewDatabaseInitializer.CreateQuestionnaireDbStructure(new TenantContext(null) {Tenant = tenant}, questionnaire);
-                    }
-
-                    return questionnaire;
+                memoryCache.Set(key, questionnaire, new MemoryCacheEntryOptions
+                {
+                    SlidingExpiration = TimeSpan.FromMinutes(5)
                 });
 
+                interviewDatabaseInitializer.CreateQuestionnaireDbStructure(
+                    new TenantContext(null) { Tenant = tenant }, questionnaire);
+                var tenantName = tenant.Name;
+                logger.LogInformation("Created database structure for {tenantName} ({questionnaireId})", tenantName, questionnaireId);
+
+                return questionnaire;
+            }
+            finally
+            {
+                CacheLock.Release();
+            }
         }
     }
 }
