0e1bd72ba9f38ca85220b7c363b1ea3eb455a7de
Victor Hladkikh <me@alfeg.net>
2019-02-21 10:47:44 +0200

    Pack multiple update statements into one command. Build fix

--- src/Services/Export/WB.Services.Export/Monitoring.cs
+++ src/Services/Export/WB.Services.Export/Monitoring.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.ObjectPool;
 using Prometheus;
 using WB.Services.Export.InterviewDataStorage;
 
