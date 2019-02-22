3739965de1599115cba5cdae28705c31a0c9b387
Roman Lytvyn <litvin.roma@gmail.com>
2019-02-22 20:09:27 +0200

    KP-12491 [code review] IN: render multiselect as combobox

--- src/Core/GenericSubdomains/WB.Core.GenericSubdomains.Portable/StringExtensions.cs
+++ src/Core/GenericSubdomains/WB.Core.GenericSubdomains.Portable/StringExtensions.cs
@@ -191,5 +191,18 @@ namespace WB.Core.GenericSubdomains.Portable
         {
             return Int32.TryParse(value, out var result) ? result : @default;
         }
+
+        public static IEnumerable<int> AllIndexesOf(this string str, string value)
+        {
+            if (string.IsNullOrEmpty(value))
+                throw new ArgumentException("the string to find may not be empty", nameof(value));
+            for (int index = 0; ; index += value.Length)
+            {
+                index = str.IndexOf(value, index, StringComparison.CurrentCultureIgnoreCase);
+                if (index == -1)
+                    break;
+                yield return index;
+            }
+        }
     }
 }
--- src/UI/Shared/WB.UI.Shared.Enumerator/Converters/TextWithHighlightedSearchTermConverter.cs
+++ src/UI/Shared/WB.UI.Shared.Enumerator/Converters/TextWithHighlightedSearchTermConverter.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 using System.Text.RegularExpressions;
 using MvvmCross.Converters;
+using WB.Core.GenericSubdomains.Portable;
 
 namespace WB.UI.Shared.Enumerator.Converters
 {
@@ -9,23 +12,35 @@ namespace WB.UI.Shared.Enumerator.Converters
     {
         protected override string Convert(string value, Type targetType, object parameter, CultureInfo culture)
         {
-            var searchTerm = (string) parameter;
+            if (string.IsNullOrEmpty(value)) return value;
 
+            var searchTerm = (string) parameter;
             if (string.IsNullOrEmpty(searchTerm)) return value;
 
-            var index = CultureInfo.CurrentCulture.CompareInfo.IndexOf(value, searchTerm, CompareOptions.IgnoreCase);
+            var allIndexesOfSearchTerms = value.AllIndexesOf(searchTerm).ToArray();
+            if (!allIndexesOfSearchTerms.Any()) return value;
 
-            string title;
-            if (index >= 0)
+            var valueWithHighlightedSearchTerms = string.Empty;
+            var prevIndexOfSearchTermEntry = -1;
+
+            foreach (var indexOfSearchTermEntry in allIndexesOfSearchTerms)
             {
-                var substringToHighlight = value.Substring(index, searchTerm.Length);
-                title = Regex.Replace(value, Regex.Escape(searchTerm), "<b>" + substringToHighlight + "</b>", RegexOptions.IgnoreCase);
+                var substringToHighlight = value.Substring(indexOfSearchTermEntry, searchTerm.Length);
+
+                if (prevIndexOfSearchTermEntry == -1 && indexOfSearchTermEntry > 0)
+                    valueWithHighlightedSearchTerms += value.Substring(0, indexOfSearchTermEntry);
+                else if(prevIndexOfSearchTermEntry > -1 && prevIndexOfSearchTermEntry + searchTerm.Length < indexOfSearchTermEntry)
+                    valueWithHighlightedSearchTerms += value.Substring(prevIndexOfSearchTermEntry + searchTerm.Length, indexOfSearchTermEntry + 1);
+
+                valueWithHighlightedSearchTerms += $"<b>{substringToHighlight}</b>";
+
+                prevIndexOfSearchTermEntry = indexOfSearchTermEntry;
             }
-            else
-            {
-                title = value;
-            }
-            return title;
+
+            if(prevIndexOfSearchTermEntry + searchTerm.Length < value.Length)
+                valueWithHighlightedSearchTerms += value.Substring(prevIndexOfSearchTermEntry + searchTerm.Length, value.Length - prevIndexOfSearchTermEntry - searchTerm.Length);
+
+            return valueWithHighlightedSearchTerms;
         }
     }
 }
