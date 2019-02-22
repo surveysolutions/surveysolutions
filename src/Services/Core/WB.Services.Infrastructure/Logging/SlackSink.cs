99faded247b28e60a2d08a5f67eb36da2a5c378a
Victor Hladkikh <me@alfeg.net>
2019-02-22 15:07:39 +0200

    Better slack messages layout

--- src/Services/Core/WB.Services.Infrastructure/Logging/SlackSink.cs
+++ src/Services/Core/WB.Services.Infrastructure/Logging/SlackSink.cs
@@ -27,59 +27,140 @@ namespace WB.Services.Infrastructure.Logging
         {
             if (logEvent.Level >= level)
             {
-                var fields = new List<object>();
-
-                void AddProp(string name, string title = null)
-                {
-                    title = title ?? name;
-                    if (logEvent.Properties.ContainsKey(name))
-                    {
-                        fields.Add(new
-                        {
-                            title, value = logEvent.Properties[name].ToString()
-                        });
-                    }
-                }
-
-                AddProp("tenantName", "Tenant");
-                fields.Add(new { title = "Instance", value = workerId});
-                AddProp("jobId", "Job");
-                AddProp("Host");
+                var message = new Message();
+                var blocks = message.Blocks;
                 
-                AddProp("VersionInfo");
+                var fields = new FieldsList();
+
+                blocks.Add(new Section(logEvent.RenderMessage()));
+                blocks.Add(new Divider());
+                blocks.Add(fields);
+
+                AddPropertyToField("tenantName", "Tenant");
+                AddPropertyToField("jobId", "Job");
 
                 if (logEvent.Exception != null)
                 {
                     foreach (var key in logEvent.Exception.Data.Keys)
                     {
-                        fields.Add(new
-                        {
-                            title = key,
-                            value = logEvent.Exception.Data[key]
-                        });
+                        fields.Add(key.ToString(), logEvent.Exception.Data[key]);
                     }
 
-                    fields.Add(new
+                    message.Attachments.Add(new Attachment
                     {
-                        title = "StackTrace",
-                        value = logEvent.Exception.ToStringDemystified().Substring(0, 400)
+                        text = $"```\r\n{logEvent.Exception.ToStringDemystified()}\r\n```"
                     });
                 }
+                
+                blocks.Add(new Context()
+                {
+                    Elements =
+                    {
+                        new TextItem($"[{logEvent.Properties["Host"].ToString()}]"),
+                        new TextItem(workerId),
+                        new TextItem(logEvent.Properties["VersionInfo"].ToString())
+                    }
+                });
 
                 http.PostAsync(this.webHook,
-                    new StringContent(JsonConvert.SerializeObject(new
+                    new StringContent(JsonConvert.SerializeObject(message), 
+                        Encoding.UTF8, "application/json"));
+
+                void AddPropertyToField(string name, string title = null)
+                {
+                    title = title ?? name;
+                    if (logEvent.Properties.ContainsKey(name))
                     {
-                        attachments = new object[]
-                        {
-                            new
-                            {
-                                pretext = logEvent.RenderMessage(),
-                                fallback = logEvent.RenderMessage(),
-                                fields
-                            }
-                        }
-                    }), Encoding.UTF8, "application/json"));
+                        fields.Add(title, logEvent.Properties[name].ToString());
+                    }
+                }
             }
         }
+
+        public abstract class Block
+        {
+            [JsonProperty("type")]
+            public abstract string Type { get; }
+        }
+
+        public class Divider : Block
+        {
+            public override string Type { get; } = "divider";
+        }
+
+        public class Section : Block
+        {
+            public Section(string text)
+            {
+                Text = new TextItem(text);
+            }
+
+            public override string Type { get; } = "section";
+
+            [JsonProperty("text")]
+            public TextItem Text { get; set; }
+        }
+
+        public class Attachment
+        {
+            public string mrkdwn_in { get; } = "text";
+            public string text { get; set; }
+        }
+
+        public class Message
+        {
+            [JsonProperty("blocks")]
+            public List<Block> Blocks { get; set; } = new List<Block>();
+
+            [JsonProperty("attachments")]
+            public List<Attachment> Attachments { get; set; } = new List<Attachment>();
+        }
+
+        public class FieldsList : Block
+        {
+            public override string Type { get; } = "section";
+
+            [JsonProperty("fields")]
+            public List<TextItem> Fields { get; set; } = new List<TextItem>();
+
+            public void Add(string message)
+            {
+                Fields.Add(new TextItem(message));
+            }
+
+            public void Add(string key, object value)
+            {
+                Fields.Add(new TextItem($"*{key}*: {value}"));
+            }
+        }
+
+        public class Context : Block
+        {
+            public override string Type { get; } = "context";
+
+            [JsonProperty("elements")]
+            public List<TextItem> Elements { get; set; } = new List<TextItem>();
+
+            public void Add(string item)
+            {
+                Elements.Add(new TextItem(item));
+            }
+        }
+
+        public class TextItem
+        {
+            public TextItem(string text)
+            {
+                Text = text;
+            }
+
+            [JsonProperty("type")]
+            public string Type { get; set; } = "mrkdwn";
+
+            [JsonProperty("text")]
+            public string Text { get; set; }
+        }
+
+
     }
 }
