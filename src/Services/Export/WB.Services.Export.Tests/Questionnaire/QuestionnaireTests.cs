4a6a19de54d20e2f6d105cd997d27f61a2ac468c
Andrii Kozhyn <hedgic@gmail.com>
2019-02-22 18:17:25 +0200

    KP-12440 Fixed export of groups inside roster

--- src/Services/Export/WB.Services.Export.Tests/Questionnaire/QuestionnaireTests.cs
+++ src/Services/Export/WB.Services.Export.Tests/Questionnaire/QuestionnaireTests.cs
@@ -6,7 +6,7 @@ using WB.Services.Export.Questionnaire;
 
 namespace WB.Services.Export.Tests.Questionnaire
 {
-    public class Questionnaire_GetAllStoredGroupsTests
+    public class QuestionnaireTests
     {
         [Test]
         public void should_return_groups_with_questions()
@@ -24,8 +24,8 @@ namespace WB.Services.Export.Tests.Questionnaire
                                 Create.TextQuestion(Id.g4, variable: "txt")
                             })
                     }),
-                    Create.Group(Id.g5, 
-                        children: 
+                    Create.Group(Id.g5,
+                        children:
                             Create.Group(Id.g6,
                                 children: Create.TextQuestion()
                     ))
@@ -36,6 +36,30 @@ namespace WB.Services.Export.Tests.Questionnaire
 
             // Assert
             Assert.That(storedGroups.Select(x => x.PublicKey), Is.EquivalentTo(new[] { Id.g1, Id.g3, Id.g6 }), "Groups with questions should be returned");
+        }
+
+        [Test]
+        public void IsInsideRoster_should_return_true_for_group_within_roster()
+        {
+            var target = Create.Group();
+            var questionnaire = Create.QuestionnaireDocumentWithOneChapter(Id.gA, 
+                children: new IQuestionnaireEntity[]
+                {
+                    Create.NumericIntegerQuestion(Id.g2, variable: "n1"),
+                    Create.Roster(Id.g3, variable: "rost1", rosterSizeQuestionId: Id.g2,
+                        rosterSizeSourceType: RosterSizeSourceType.Question,
+                        children: new IQuestionnaireEntity[]
+                        {
+                            target
+                        })
+                });
+            questionnaire.ConnectChildrenWithParent();
+
+            // Act 
+            var targetIsInsideRoster = target.IsInsideRoster;
+
+            // Assert
+            Assert.That(targetIsInsideRoster);
         }
     }
 }
