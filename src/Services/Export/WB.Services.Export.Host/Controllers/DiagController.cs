62bb1aba4ba848101a734a8bdc43dc63dc930b45
Victor Hladkikh <me@alfeg.net>
2019-02-26 13:24:12 +0200

    Added DiagController with settings dump

---
+++ src/Services/Export/WB.Services.Export.Host/Controllers/DiagController.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using WB.Services.Export.Services;
+
+namespace WB.Services.Export.Host.Controllers
+{
+    [ApiController]
+    public class DiagController : ControllerBase
+    {
+        private readonly IConfiguration configuration;
+        private readonly IProductVersion productVersion;
+
+        public DiagController(IConfiguration configuration, IProductVersion productVersion)
+        {
+            this.configuration = configuration;
+            this.productVersion = productVersion;
+        }
+
+        [Route(".diag")]
+        public ActionResult Diagnostics()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine("Export service version: " + productVersion.ToString());
+            foreach (var config in this.configuration.AsEnumerable())
+            {
+                sb.AppendLine($"[{config.Key}]={config.Value}");
+            }
+
+            return Content(sb.ToString());
+        }
+    }
+}
