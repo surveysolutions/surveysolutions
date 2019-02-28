bdab830e53402d534065965d7230706e8d55f405
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2019-02-28 17:05:17 -0500

    KP-12575 IN: Multi-select combo box list of values is not updated after selecting/removing

--- src/Core/SharedKernels/Enumerator/Enumerator/ViewModels/InterviewDetails/Questions/CategoricalComboboxAutocompleteViewModel.cs
+++ src/Core/SharedKernels/Enumerator/Enumerator/ViewModels/InterviewDetails/Questions/CategoricalComboboxAutocompleteViewModel.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using MvvmCross.Commands;
 using MvvmCross.ViewModels;
 using WB.Core.GenericSubdomains.Portable;
