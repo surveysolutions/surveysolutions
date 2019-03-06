006f71961004168a9e6b67ea6ba4f20a26537a6a
Pavel Stovpivskyi <sby@rambler.ru>
2019-03-06 19:33:19 +0200

    Revert "KP-12589 Tabular Format zip is not generated"
    This reverts commit 2a97d4a2c9f97a18f3086c65b5ec0eed803599cc.

--- src/Core/SharedKernels/Questionnaire/WB.Core.SharedKernels.Questionnaire/Documents/Constants.cs
+++ src/Core/SharedKernels/Questionnaire/WB.Core.SharedKernels.Questionnaire/Documents/Constants.cs
@@ -6,7 +6,6 @@
         public static readonly int MaxRosterRowCount = 60;
         public static readonly int MaxLongRosterRowCount = 200;
         public static readonly int MinLongRosterRowCount = 1;
-        public static readonly int MaxLinkedQuestionAnsweredOptionsCount = 200;
         public static readonly string HtmlRemovalPattern = "<.*?>";
         public static readonly int ThrottlePeriod = 500;
         public const int MaxRosterPropagationLimit = 10000;
