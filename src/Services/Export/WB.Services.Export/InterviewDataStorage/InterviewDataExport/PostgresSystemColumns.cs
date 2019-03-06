3baa4f7b3ceb1ca3585e0fc763d653e2414db1c4
Pavel Stovpivskyi <sby@rambler.ru>
2019-03-06 14:23:34 +0200

    KP-12593 mhpsac export is failing

---
+++ src/Services/Export/WB.Services.Export/InterviewDataStorage/InterviewDataExport/PostgresSystemColumns.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace WB.Services.Export.InterviewDataStorage.InterviewDataExport
+{
+    public class PostgresSystemColumns
+    {
+        static HashSet<string> systemColumns = new HashSet<string>()
+        {
+            "oid",
+            "tableoid",
+            "xmin",
+            "cmin",
+            "xmax",
+            "cmax",
+            "ctid",
+        };
+
+        public static string Escape(string columnName)
+        {
+            if (string.IsNullOrEmpty(columnName))
+                return columnName;
+            if (systemColumns.Contains(columnName.ToLower()))
+                return columnName + "__1";
+            return columnName;
+        }
+    }
+}
