10486a0a66080362e01979a9ffccc152bd274695
Pavel Stovpivskyi <sby@rambler.ru>
2019-03-07 15:03:16 +0200

    KP-12589 Tabular Format zip is not generated

--- src/Core/SharedKernels/Questionnaire/WB.Core.SharedKernels.Questionnaire/Documents/Constants.cs
+++ src/Core/SharedKernels/Questionnaire/WB.Core.SharedKernels.Questionnaire/Documents/Constants.cs
@@ -6,6 +6,7 @@
         public static readonly int MaxRosterRowCount = 60;
         public static readonly int MaxLongRosterRowCount = 200;
         public static readonly int MinLongRosterRowCount = 1;
+        public static readonly int MaxLinkedQuestionAnsweredOptionsCount = 200;
         public static readonly string HtmlRemovalPattern = "<.*?>";
         public static readonly int ThrottlePeriod = 500;
         public const int MaxRosterPropagationLimit = 10000;
