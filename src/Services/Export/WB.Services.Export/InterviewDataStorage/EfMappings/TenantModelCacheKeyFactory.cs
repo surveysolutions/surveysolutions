e19d438c76b5bc873554d9e765b17d5278202c36
Andrii Kozhyn <hedgic@gmail.com>
2019-03-08 11:39:19 +0200

    KP-12602 replace mappings in case of schema change

---
+++ src/Services/Export/WB.Services.Export/InterviewDataStorage/EfMappings/TenantModelCacheKeyFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using WB.Services.Export.Infrastructure;
+
+namespace WB.Services.Export.InterviewDataStorage.EfMappings
+{
+    public class TenantModelCacheKeyFactory : IModelCacheKeyFactory
+    {
+        public object Create(DbContext context)
+        {
+            var dbContext = context as TenantDbContext;
+            return new {
+                Type = context.GetType(),
+                Schema = dbContext.TenantContext.Tenant.SchemaName()
+            };
+        }
+    }
+}
