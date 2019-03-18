be6fdd9d7a202006220ba41e85587880a3ddc8cc
Roman Lytvyn <litvin.roma@gmail.com>
2019-03-18 20:12:08 +0200

    KP-12554 [code review] Extract Map report to own denormalizer

---
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Mappings/InterviewGpsMap.cs
@@ -0,0 +1,23 @@
+﻿using NHibernate.Mapping.ByCode.Conformist;
+using WB.Core.BoundedContexts.Headquarters.Views.Interview;
+
+namespace WB.Core.BoundedContexts.Headquarters.Mappings
+{
+    public class InterviewGpsMap : ClassMapping<InterviewGps>
+    {
+        public InterviewGpsMap()
+        {
+            Table("interview_geo_answers");
+            ComposedId(m =>
+            {
+                m.Property(x => x.InterviewId);
+                m.Property(x => x.QuestionId);
+                m.Property(x => x.RosterVector);
+            });
+            Property(x => x.Latitude);
+            Property(x => x.Longitude);
+            Property(x => x.Timestamp, mapper => mapper.Column("timestamp"));
+            Property(x => x.IsEnabled);
+        }
+    }
+}
