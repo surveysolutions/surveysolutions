bbfd4ad11a8b513e102dce7eeeb7cbafa58edd4e
Roman Lytvyn <litvin.roma@gmail.com>
2019-03-19 15:13:54 +0200

    KP-12555 [code review] Do not store flags in interviews table

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Views/Interview/InterviewFlag.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Views/Interview/InterviewFlag.cs
@@ -5,17 +5,17 @@ namespace WB.Core.BoundedContexts.Headquarters.Views.Interview
     public class InterviewFlag
     {
         public virtual string InterviewId { get; set; }
-        public virtual string Identity { get; set; }
+        public virtual string QuestionIdentity { get; set; }
 
         public override bool Equals(object obj) => obj is InterviewFlag flag &&
                                                    InterviewId == flag.InterviewId &&
-                                                   Identity == flag.Identity;
+                                                   QuestionIdentity == flag.QuestionIdentity;
 
         public override int GetHashCode()
         {
             var hashCode = -1791060676;
             hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(InterviewId);
-            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Identity);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(QuestionIdentity);
             return hashCode;
         }
     }
