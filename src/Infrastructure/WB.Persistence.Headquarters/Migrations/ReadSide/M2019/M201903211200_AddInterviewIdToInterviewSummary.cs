33717f06f97d807267a037adc8dd0a7ebf47a8ec
Victor Gladkikh <me@alfeg.net>
2019-03-21 12:13:40 +0200

    KP-12556 Added int Id to interview summary

---
+++ src/Infrastructure/WB.Persistence.Headquarters/Migrations/ReadSide/M2019/M201903211200_AddInterviewIdToInterviewSummary.cs
@@ -0,0 +1,21 @@
+﻿using FluentMigrator;
+
+namespace WB.Persistence.Headquarters.Migrations.ReadSide
+{
+    [Migration(201903211200)]
+    public class M201903211200_AddInterviewIdToInterviewSummary : Migration
+    {
+        public override void Up()
+        {
+            Alter.Table("interviewsummaries")
+                .AddColumn("id")
+                .AsCustom("serial")
+                .NotNullable();
+        }
+
+        public override void Down()
+        {
+            Delete.Column("id").FromTable("interviewsummaries");
+        }
+    }
+}
