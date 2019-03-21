40dd8f40498060f2c5b764985cb2e80452e9dd8d
Victor Hladkikh <me@alfeg.net>
2019-03-21 16:47:17 +0200

    Do not modify `interviews` table in migrations

--- src/Infrastructure/WB.Persistence.Headquarters/Migrations/ReadSide/M2019/M201902281841_Add_InterviewFlags_Table.cs
+++ src/Infrastructure/WB.Persistence.Headquarters/Migrations/ReadSide/M2019/M201902281841_Add_InterviewFlags_Table.cs
@@ -33,32 +33,22 @@ namespace WB.Persistence.Headquarters.Migrations.ReadSide
                 if(!npgsqlConnection.IsTableExistsInSchema("readside", "interviews_view")) return;
 
                 var flags = npgsqlConnection.Query<(Guid interviewId, Guid entityId, string rosterVector)>(
-                        "SELECT interviewid, entityid, rostervector FROM \"readside\".\"interviews_view\" where hasflag = true")
+                        @"SELECT interviewid, entityid, rostervector FROM ""readside"".""interviews_view"" where hasflag = true")
                     .ToArray();
 
-                var copyFromCommand = "COPY  \"readside\".\"interviewflags\" (interviewid, questionidentity) FROM STDIN BINARY;";
+                var copyFromCommand = @"COPY ""readside"".""interviewflags"" (interviewid, questionidentity) FROM STDIN BINARY;";
                 using (var writer = npgsqlConnection.BeginBinaryImport(copyFromCommand))
                 {
                     foreach (var flag in flags)
                     {
                         writer.StartRow();
                         writer.Write(flag.interviewId.ToString("N"), NpgsqlDbType.Char);
-                        writer.Write(Identity.Create(flag.entityId, RosterVector.Parse(flag.rosterVector)).ToString(), NpgsqlDbType.Text);
+                        writer.Write(Identity.Create(flag.entityId, 
+                                RosterVector.Parse(flag.rosterVector)).ToString(), NpgsqlDbType.Text);
                     }
 
                     writer.Complete();
                 }
-
-                connection.Execute("DROP VIEW readside.interviews_view");
-                connection.Execute(@"create or replace view readside.interviews_view as 
-                select s.interviewid, q.entityid, rostervector, isenabled, isreadonly, invalidvalidations, warnings,
-                    asstring, asint, aslong, asdouble, asdatetime, aslist, asintarray, asintmatrix, asgps, asbool,
-                    asyesno, asaudio, asarea, q.entity_type, q.parentid, q.question_type
-                from readside.interviews i
-                join readside.interviews_id s on s.id = i.interviewid
-                join readside.questionnaire_entities q on q.id = i.entityid");
-
-                connection.Execute("ALTER TABLE \"readside\".\"interviews\" DROP COLUMN hasflag");
             });
         }
 
