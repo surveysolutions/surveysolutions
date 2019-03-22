d20e2dba82474a525ad5a97f17ec78bce5e314fd
Roman Lytvyn <litvin.roma@gmail.com>
2019-03-22 15:39:06 +0200

    KP-12555 Do not store flags in interviews table. Fixed mapping nhibernate

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Mappings/InterviewFlagMap.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Mappings/InterviewFlagMap.cs
@@ -1,10 +1,8 @@
 ﻿using NHibernate.Mapping.ByCode.Conformist;
 using WB.Core.BoundedContexts.Headquarters.Views.Interview;
-using WB.Core.Infrastructure.PlainStorage;
 
 namespace WB.Core.BoundedContexts.Headquarters.Mappings
 {
-    [PlainStorage]
     public class InterviewFlagMap : ClassMapping<InterviewFlag>
     {
         public InterviewFlagMap()
