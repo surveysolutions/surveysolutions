94dd1d369b77d6b12c9a76a45ace46313da4b560
Victor Hladkikh <me@alfeg.net>
2019-03-22 12:37:06 +0200

    KP-12556 Moved to NH mapping

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/EventHandler/InterviewStatisticsReportDenormalizer.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/EventHandler/InterviewStatisticsReportDenormalizer.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using Dapper;
 using Main.Core.Entities.SubEntities;
+using Main.Core.Entities.SubEntities.Question;
 using Ncqrs.Eventing.ServiceModel.Bus;
+using NHibernate.Linq;
 using WB.Core.BoundedContexts.Headquarters.Views.Interview;
 using WB.Core.Infrastructure.EventHandlers;
 using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
@@ -14,7 +15,10 @@ using WB.Infrastructure.Native.Storage.Postgre;
 
 namespace WB.Core.BoundedContexts.Headquarters.EventHandler
 {
-    public interface IInterviewStatisticsReportDenormalizer : ICompositeFunctionalPartEventHandler<InterviewSummary, IReadSideRepositoryWriter<InterviewSummary>> { }
+    public interface IInterviewStatisticsReportDenormalizer : ICompositeFunctionalPartEventHandler<InterviewSummary,
+        IReadSideRepositoryWriter<InterviewSummary>>
+    {
+    }
 
     internal class InterviewStatisticsReportDenormalizer :
         IInterviewStatisticsReportDenormalizer,
@@ -29,7 +33,8 @@ namespace WB.Core.BoundedContexts.Headquarters.EventHandler
         private readonly IUnitOfWork unitOfWork;
         private readonly IQuestionnaireStorage questionnaireStorage;
 
-        public InterviewStatisticsReportDenormalizer(IUnitOfWork unitOfWork, IQuestionnaireStorage questionnaireStorage)
+        public InterviewStatisticsReportDenormalizer(IUnitOfWork unitOfWork,
+            IQuestionnaireStorage questionnaireStorage)
         {
             this.unitOfWork = unitOfWork;
             this.questionnaireStorage = questionnaireStorage;
@@ -39,64 +44,102 @@ namespace WB.Core.BoundedContexts.Headquarters.EventHandler
         {
             var questionId = @event.Payload.QuestionId;
             var rosterVector = new RosterVector(@event.Payload.RosterVector);
-            
-            UpdateReportStatisticsAnswer(state, questionId, rosterVector, StatisticsReportType.Categorical, @event.Payload.SelectedValue);
+
+            UpdateReportStatisticsAnswer(state, questionId, rosterVector, StatisticsReportType.Categorical,
+                @event.Payload.SelectedValue);
 
             return state;
         }
 
         public InterviewSummary Update(InterviewSummary state, IPublishedEvent<AnswersRemoved> @event)
         {
-            var questionnaire = questionnaireStorage.GetQuestionnaireDocument(state.QuestionnaireId, state.QuestionnaireVersion);
-            
-            unitOfWork.Session.Connection.Execute("delete from readside.report_statistics " +
-                                                  "where interview_id = @interviewId and entity_id = @entityId " +
-                                                  "and rostervector = @rostervector",
-                @event.Payload.Questions.Select(q => new
-                {
-                    InterviewId = state.Id,
-                    EntityId = questionnaire.EntitiesIdMap[q.Id],
-                    RosterVector = q.RosterVector.AsString()
-                }));
+            var questionnaire =
+                questionnaireStorage.GetQuestionnaireDocument(state.QuestionnaireId, state.QuestionnaireVersion);
+
+            List<Identity> questions = @event.Payload.Questions
+                .Where(q => IsEligibleQuestion(questionnaire.Find<IQuestion>(q.Id)))
+                .ToList();
+
+            foreach (var identity in questions)
+            {
+                unitOfWork.Session.Query<InterviewStatisticsReportRow>()
+                    .Where(s => s.InterviewId == state.Id
+                                && s.RosterVector == identity.RosterVector.AsString()
+                                && s.EntityId == questionnaire.EntitiesIdMap[identity.Id])
+                    .Delete();
+            }
 
             return state;
         }
 
         public InterviewSummary Update(InterviewSummary state, IPublishedEvent<MultipleOptionsQuestionAnswered> @event)
         {
-            UpdateReportStatisticsAnswer(state, @event.Payload.QuestionId, new RosterVector(@event.Payload.RosterVector), 
+            UpdateReportStatisticsAnswer(state, @event.Payload.QuestionId,
+                new RosterVector(@event.Payload.RosterVector),
                 StatisticsReportType.Categorical, @event.Payload.SelectedValues);
 
             return state;
-        }
-
-        private void UpdateReportStatisticsAnswer(InterviewSummary state, 
-            Guid questionId, RosterVector rv,StatisticsReportType type = StatisticsReportType.Categorical, params decimal[] answer)
-        {
-            var questionnaire = questionnaireStorage.GetQuestionnaireDocument(state.QuestionnaireId, state.QuestionnaireVersion);
-
-            var question = questionnaire.Find<IQuestion>(questionId);
-
-            if (!IsEligibleQuestion(question)) return;
-            
-            unitOfWork.Session.Connection.Execute(
-                @"insert into readside.report_statistics (interview_id, entity_id, rostervector, answer, ""type"")
-                    values(@interviewid,@entityId,@rostervector, @answer::int[], @type)
-                    on conflict (interview_id, entity_id, rostervector)
-                    do update set answer = @answer::int[]", new
-                {
-                    type,
-                    RosterVector = rv.AsString(),
-                    EntityId = questionnaire.EntitiesIdMap[questionId],
-                    InterviewId = state.Id,
-                    answer
-                });
         }
 
         public InterviewSummary Update(InterviewSummary state, IPublishedEvent<QuestionsDisabled> @event)
         {
             UpdateQuestionEnablement(state, false, @event.Payload.Questions);
             return state;
+        }
+
+        public InterviewSummary Update(InterviewSummary state, IPublishedEvent<QuestionsEnabled> @event)
+        {
+            UpdateQuestionEnablement(state, true, @event.Payload.Questions);
+            return state;
+        }
+
+        public InterviewSummary Update(InterviewSummary state, IPublishedEvent<NumericIntegerQuestionAnswered> @event)
+        {
+            UpdateReportStatisticsAnswer(state, @event.Payload.QuestionId,
+                new RosterVector(@event.Payload.RosterVector),
+                StatisticsReportType.Numeric, @event.Payload.Answer);
+
+            return state;
+        }
+
+        public InterviewSummary Update(InterviewSummary state, IPublishedEvent<NumericRealQuestionAnswered> @event)
+        {
+            UpdateReportStatisticsAnswer(state, @event.Payload.QuestionId,
+                new RosterVector(@event.Payload.RosterVector),
+                StatisticsReportType.Numeric, @event.Payload.Answer);
+
+            return state;
+        }
+
+        private bool IsEligibleQuestion(IQuestion question)
+        {
+            if (question.CascadeFromQuestionId != null) return false;
+            if (question.LinkedToQuestionId != null || question.LinkedToRosterId != null) return false;
+            if (question.IsFilteredCombobox == true) return false;
+            if (question is SingleQuestion || question is NumericQuestion) return true;
+            return false;
+        }
+
+        private void UpdateReportStatisticsAnswer(InterviewSummary state,
+            Guid questionId, RosterVector rv, StatisticsReportType type = StatisticsReportType.Categorical,
+            params decimal[] answer)
+        {
+            var questionnaire =
+                questionnaireStorage.GetQuestionnaireDocument(state.QuestionnaireId, state.QuestionnaireVersion);
+
+            var question = questionnaire.Find<IQuestion>(questionId);
+
+            if (!IsEligibleQuestion(question)) return;
+
+            this.unitOfWork.Session.SaveOrUpdate(new InterviewStatisticsReportRow
+            {
+                RosterVector = rv.AsString(),
+                EntityId = questionnaire.EntitiesIdMap[questionId],
+                InterviewId = state.Id,
+                Answer = answer.Select(a => (long)a).ToArray(),
+                Type = type,
+                IsEnabled = true
+            });
         }
 
         private void UpdateQuestionEnablement(InterviewSummary summary, bool enabled, Identity[] questionIds)
@@ -108,48 +151,17 @@ namespace WB.Core.BoundedContexts.Headquarters.EventHandler
                 .Where(q => IsEligibleQuestion(questionnaire.Find<IQuestion>(q.Id)))
                 .ToList();
 
-            unitOfWork.Session.Connection.Execute("update readside.report_statistics set is_enabled = @enabled " +
-                                                  "where interview_id = @interviewid " +
-                                                  "and rostervector = @rostervector and entity_id = @entityId",
-
-                questions.Select(q =>
-                    new
-                    {
-                        RosterVector = q.RosterVector.AsString(),
-                        EntityId = questionnaire.EntitiesIdMap[q.Id],
-                        InterviewId = summary.Id,
-                        enabled
-                    }));
-        }
-
-        public InterviewSummary Update(InterviewSummary state, IPublishedEvent<QuestionsEnabled> @event)
-        {
-            UpdateQuestionEnablement(state, true, @event.Payload.Questions);
-            return state;
-        }
-        
-        bool IsEligibleQuestion(IQuestion question)
-        {
-            if (question.CascadeFromQuestionId != null) return false;
-            if (question.LinkedToQuestionId != null || question.LinkedToRosterId != null) return false;
-            if (question.IsFilteredCombobox == true) return false;
-            return true;
-        }
-
-        public InterviewSummary Update(InterviewSummary state, IPublishedEvent<NumericIntegerQuestionAnswered> @event)
-        {
-            UpdateReportStatisticsAnswer(state, @event.Payload.QuestionId, new RosterVector(@event.Payload.RosterVector),
-                StatisticsReportType.Numeric, @event.Payload.Answer);
-
-            return state;
-        }
-
-        public InterviewSummary Update(InterviewSummary state, IPublishedEvent<NumericRealQuestionAnswered> @event)
-        {
-            UpdateReportStatisticsAnswer(state, @event.Payload.QuestionId, new RosterVector(@event.Payload.RosterVector),
-                StatisticsReportType.Numeric, @event.Payload.Answer);
-
-            return state;
+            foreach (var identity in questions)
+            {
+                this.unitOfWork.Session
+                    .Query<InterviewStatisticsReportRow>()
+                    .Where(x => x.InterviewId == summary.Id
+                                && x.RosterVector == identity.RosterVector.AsString()
+                                && x.EntityId == questionnaire.EntitiesIdMap[identity.Id])
+                    .UpdateBuilder()
+                    .Set(x => x.IsEnabled, x => enabled)
+                    .Update();
+            }
         }
     }
 }
