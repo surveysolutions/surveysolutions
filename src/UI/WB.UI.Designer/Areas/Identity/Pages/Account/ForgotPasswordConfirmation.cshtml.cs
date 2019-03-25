9a8803cba0801c720b7e1eed120c286c06fd4fb4
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2019-03-25 13:03:56 +0200

    KP-12170 scaffold identity pages

---
+++ src/UI/WB.UI.Designer/Areas/Identity/Pages/Account/ForgotPasswordConfirmation.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WB.UI.Designer.Areas.Identity.Pages.Account
+{
+    [AllowAnonymous]
+    public class ForgotPasswordConfirmation : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
