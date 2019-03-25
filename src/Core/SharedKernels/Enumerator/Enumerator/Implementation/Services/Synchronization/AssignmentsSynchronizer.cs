c352641b9303338e932385561c26915d814b10e0
Roman Lytvyn <litvin.roma@gmail.com>
2019-03-25 17:05:42 +0200

    KP-12469 Offline SV<->HQ sync: '0' always is shown in record "Synchronizing 0 of X assigment(s)"

--- src/Core/SharedKernels/Enumerator/Enumerator/Implementation/Services/Synchronization/AssignmentsSynchronizer.cs
+++ src/Core/SharedKernels/Enumerator/Enumerator/Implementation/Services/Synchronization/AssignmentsSynchronizer.cs
@@ -4,8 +4,6 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using WB.Core.GenericSubdomains.Portable;
-using WB.Core.GenericSubdomains.Portable.Implementation;
-using WB.Core.SharedKernels.DataCollection.Aggregates;
 using WB.Core.SharedKernels.DataCollection.Repositories;
 using WB.Core.SharedKernels.DataCollection.WebApi;
 using WB.Core.SharedKernels.Enumerator.Properties;
@@ -46,122 +44,93 @@ namespace WB.Core.SharedKernels.Enumerator.Implementation.Services.Synchronizati
         public virtual async Task SynchronizeAssignmentsAsync(IProgress<SyncProgressInfo> progress,
             SynchronizationStatistics statistics, CancellationToken cancellationToken)
         {
-            List<AssignmentApiView> remoteAssignments =
-                await this.synchronizationService.GetAssignmentsAsync(cancellationToken);
+            var remoteAssignments = await this.synchronizationService.GetAssignmentsAsync(cancellationToken);
+            var localAssignments = this.assignmentsRepository.LoadAll();
 
-            IReadOnlyCollection<AssignmentDocument> localAssignments = this.assignmentsRepository.LoadAll();
-
-            progress.Report(new SyncProgressInfo
-            {
-                Title = InterviewerUIResources.Synchronization_Of_AssignmentsFormat.FormatString(0,
-                    remoteAssignments.Count),
-                Statistics = statistics,
-                Status = SynchronizationStatus.Download,
-                Stage = SyncStage.AssignmentsSynchronization,
-                StageExtraInfo = new Dictionary<string, string>()
-                {
-                    { "processedCount", "0" },
-                    { "totalCount", remoteAssignments.Count.ToString()}
-                }
-            });
+            var remoteIds = remoteAssignments.ToLookup(ra => ra.Id);
+            var removedAssignments = localAssignments.Where(x => !remoteIds.Contains(x.Id));
+            var localIds = localAssignments.Except(removedAssignments).ToLookup(x => x.Id);
+            
+            var processedAssignmentsCount = 0;
+            var transferProgress = progress.AsTransferReport();
 
             // removing local assignments if needed
-            var remoteIds = remoteAssignments.ToLookup(ra => ra.Id);
-
-            foreach (var assignment in localAssignments)
+            foreach (var assignment in removedAssignments)
             {
-                if (remoteIds.Contains(assignment.Id)) continue;
-
                 statistics.RemovedAssignmentsCount += 1;
                 this.assignmentsRepository.Remove(assignment.Id);
             }
 
             // adding new, updating quantity for existing
-            var localAssignmentsLookup = this.assignmentsRepository.LoadAll().ToLookup(la => la.Id);
-            var processedAssignmentsCount = 0;
-
-            IProgress<TransferProgress> transferProgress = progress.AsTransferReport();
-
             foreach (var remoteItem in remoteAssignments)
             {
                 await this.questionnaireDownloader.DownloadQuestionnaireAsync(remoteItem.QuestionnaireId, statistics,
                     transferProgress, cancellationToken);
 
-                IQuestionnaire questionnaire =
-                    this.questionnaireStorage.GetQuestionnaire(remoteItem.QuestionnaireId, null);
-
-                var local = localAssignmentsLookup[remoteItem.Id].FirstOrDefault();
-                if (local == null)
-                {
-                    AssignmentApiDocument remote =
-                        await this.synchronizationService.GetAssignmentAsync(remoteItem.Id, cancellationToken);
-
-                    local = this.assignmentDocumentFromDtoBuilder.GetAssignmentDocument(remote, questionnaire);
-
-                    statistics.NewAssignmentsCount++;
-
-                    progress.Report(new SyncProgressInfo
-                    {
-                        Title = InterviewerUIResources.Synchronization_Of_AssignmentsFormat.FormatString(
-                            processedAssignmentsCount, remoteAssignments.Count),
-                        Statistics = statistics,
-                        Status = SynchronizationStatus.Download,
-                        Stage = SyncStage.AssignmentsSynchronization,
-                        StageExtraInfo = new Dictionary<string, string>()
-                        {
-                            { "processedCount", processedAssignmentsCount.ToString() },
-                            { "totalCount", remoteAssignments.Count.ToString()}
-                        }
-                    });
-
-                    this.assignmentsRepository.Store(local);
-                }
+                var local = localIds[remoteItem.Id].FirstOrDefault();
+                if (local != null)
+                    this.UpdateAssignment(local, remoteItem);
                 else
-                {
-                    if (local.Quantity != remoteItem.Quantity)
-                    {
-                        local.Quantity = remoteItem.Quantity;
-                    }
-
-                    if (ReceivedByInterviewerTimeShouldBeReset(remoteItem.ResponsibleId, local.ResponsibleId,
-                        local.OriginalResponsibleId))
-                    {
-                        local.ReceivedByInterviewerAt = null;
-                    }
-
-                    if (local.OriginalResponsibleId != remoteItem.ResponsibleId)
-                    {
-                        local.ResponsibleId = remoteItem.ResponsibleId;
-                        local.OriginalResponsibleId = remoteItem.ResponsibleId;
-                        local.ResponsibleName = remoteItem.ResponsibleName;
-                    }
-
-                    local.IsAudioRecordingEnabled = remoteItem.IsAudioRecordingEnabled;
-
-                    var interviewsCount = this.interviewViewRepository.Count(x => x.FromHqSyncDateTime == null && x.Assignment == local.Id);
-                     
-                    local.CreatedInterviewsCount = interviewsCount;
-                    this.assignmentsRepository.Store(local);
-                }
-
-                await this.synchronizationService.LogAssignmentAsHandledAsync(local.Id, cancellationToken);
+                    await this.CreateAssignmentAsync(remoteItem, statistics, cancellationToken);
 
                 processedAssignmentsCount++;
+                progress.Report(new SyncProgressInfo
+                {
+                    Title = InterviewerUIResources.Synchronization_Of_AssignmentsFormat.FormatString(
+                        processedAssignmentsCount, remoteAssignments.Count),
+                    Statistics = statistics,
+                    Status = SynchronizationStatus.Download,
+                    Stage = SyncStage.AssignmentsSynchronization,
+                    StageExtraInfo = new Dictionary<string, string>()
+                    {
+                        { "processedCount", processedAssignmentsCount.ToString() },
+                        { "totalCount", remoteAssignments.Count.ToString()}
+                    }
+                });
+            }
+        }
+
+        private async Task CreateAssignmentAsync(AssignmentApiView remoteItem, SynchronizationStatistics statistics,
+            CancellationToken cancellationToken)
+        {
+            var remoteAssignment = await this.synchronizationService.GetAssignmentAsync(remoteItem.Id, cancellationToken);
+            var questionnaire = this.questionnaireStorage.GetQuestionnaire(remoteItem.QuestionnaireId, null);
+
+            var localAssignment = this.assignmentDocumentFromDtoBuilder.GetAssignmentDocument(remoteAssignment, questionnaire);
+            this.assignmentsRepository.Store(localAssignment);
+
+            await this.synchronizationService.LogAssignmentAsHandledAsync(remoteItem.Id, cancellationToken);
+
+            statistics.NewAssignmentsCount++;
+        }
+
+        private void UpdateAssignment(AssignmentDocument local, AssignmentApiView remote)
+        {
+            if (local.Quantity != remote.Quantity)
+            {
+                local.Quantity = remote.Quantity;
             }
 
-            progress.Report(new SyncProgressInfo
+            if (ReceivedByInterviewerTimeShouldBeReset(remote.ResponsibleId, local.ResponsibleId,
+                local.OriginalResponsibleId))
             {
-                Title = InterviewerUIResources.Synchronization_Of_AssignmentsFormat.FormatString(
-                    processedAssignmentsCount, remoteAssignments.Count),
-                Statistics = statistics,
-                Status = SynchronizationStatus.Download,
-                Stage = SyncStage.AssignmentsSynchronization,
-                StageExtraInfo = new Dictionary<string, string>()
-                {
-                    { "processedCount", processedAssignmentsCount.ToString() },
-                    { "totalCount", remoteAssignments.Count.ToString()}
-                }
-            });
+                local.ReceivedByInterviewerAt = null;
+            }
+
+            if (local.OriginalResponsibleId != remote.ResponsibleId)
+            {
+                local.ResponsibleId = remote.ResponsibleId;
+                local.OriginalResponsibleId = remote.ResponsibleId;
+                local.ResponsibleName = remote.ResponsibleName;
+            }
+
+            local.IsAudioRecordingEnabled = remote.IsAudioRecordingEnabled;
+
+            var interviewsCount =
+                this.interviewViewRepository.Count(x => x.FromHqSyncDateTime == null && x.Assignment == local.Id);
+
+            local.CreatedInterviewsCount = interviewsCount;
+            this.assignmentsRepository.Store(local);
         }
 
         public bool ReceivedByInterviewerTimeShouldBeReset(Guid remoteResponsibleId, Guid localResponsibleId,
