2bd7bfede00159f4ea3d2adb7830385f3848cec1
Victor Hladkikh <me@alfeg.net>
2019-03-27 10:11:32 +0200

    KP-12524 Fix migration to handle non existent webinterviewconfigs

--- src/Infrastructure/WB.Persistence.Headquarters/Migrations/PlainStore/M2019/M201903261800_MigrateWebAssignments.cs
+++ src/Infrastructure/WB.Persistence.Headquarters/Migrations/PlainStore/M2019/M201903261800_MigrateWebAssignments.cs
@@ -7,11 +7,15 @@ namespace WB.Persistence.Headquarters.Migrations.PlainStore
     {
         public override void Up()
         {
-            Execute.Sql(@"INSERT INTO plainstore.invitations (assignmentid, ""token"", numberofreminderssent)
-                select ass.id as assignmentid, ass.id::text as ""token"", 0 as numberofreminderssent
-                from plainstore.webinterviewconfigs config
-                join plainstore.assignments ass on ass.questionnaire = config.id
-                where (config.value ->> 'Started')::bool = true");
+            if (Schema.Table("webinterviewconfigs").Exists())
+            {
+                Execute.Sql(@"INSERT INTO plainstore.invitations (assignmentid, ""token"", numberofreminderssent)
+                    select ass.id as assignmentid, ass.id::text as ""token"", 0 as numberofreminderssent
+                    from plainstore.webinterviewconfigs config
+                    join plainstore.assignments ass on ass.questionnaire = config.id
+                    where (config.value ->> 'Started')::bool = true 
+                        and not exists (select 1 from plainstore.invitations where assignmentid = ass.id)");
+            }
         }
 
         public override void Down()
