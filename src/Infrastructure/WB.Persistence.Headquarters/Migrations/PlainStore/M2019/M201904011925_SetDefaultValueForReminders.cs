06e69f59f72590f87f684c0e5733b9f2bae3240f
Pavel Stovpivskyi <630198+spuv@users.noreply.github.com>
2019-04-02 12:23:45 +0300

    KP-12670 'Do not send' option isn't saved for reminders when user updates settings

---
+++ src/Infrastructure/WB.Persistence.Headquarters/Migrations/PlainStore/M2019/M201904011925_SetDefaultValueForReminders.cs
@@ -0,0 +1,20 @@
+﻿using FluentMigrator;
+
+namespace WB.Persistence.Headquarters.Migrations.PlainStore
+{
+    [Migration(201904011925)]
+    public class M201904011925_SetDefaultValueForReminders : Migration
+    {
+        public override void Up()
+        {
+            if (Schema.Table("webinterviewconfigs").Exists())
+            {
+                Execute.Sql(@"UPDATE plainstore.webinterviewconfigs SET value = value::jsonb || jsonb '{""ReminderAfterDaysIfPartialResponse"":3, ""ReminderAfterDaysIfNoResponse"":3}'");
+            }
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
