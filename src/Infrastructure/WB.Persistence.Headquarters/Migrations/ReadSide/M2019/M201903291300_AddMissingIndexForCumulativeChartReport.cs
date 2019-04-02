06f2b7963b7ff1337b6bf2fa4d71f026c0801a7f
Victor Hladkikh <me@alfeg.net>
2019-04-02 11:18:24 +0300

    Added missing index for cumulative chart report

---
+++ src/Infrastructure/WB.Persistence.Headquarters/Migrations/ReadSide/M2019/M201903291300_AddMissingIndexForCumulativeChartReport.cs
@@ -0,0 +1,19 @@
+﻿using FluentMigrator;
+
+namespace WB.Persistence.Headquarters.Migrations.ReadSide
+{
+    [Migration(201903291300)]
+    public class M201903291300_AddMissingIndexForCumulativeChartReport : Migration
+    {
+        public override void Up()
+        {
+            Create.Index("cumulativereportstatuschanges_interviewid_idx").OnTable("cumulativereportstatuschanges")
+                .OnColumn("interviewid").Ascending();
+        }
+
+        public override void Down()
+        {
+
+        }
+    }
+}
