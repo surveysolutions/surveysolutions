64028d296fcaf6e718389a1fd1d8b905b2ecfc51
Victor Gladkikh <me@alfeg.net>
2019-04-03 13:44:35 +0300

    KP-12678 Supervisor will download interviewer apk if file hashes do not match

--- src/Tests/WB.Tests.Unit/Infrastructure/OfflineSync/OfflineSynchronizationChunkedSendTest.cs
+++ src/Tests/WB.Tests.Unit/Infrastructure/OfflineSync/OfflineSynchronizationChunkedSendTest.cs
@@ -90,7 +90,7 @@ namespace WB.Tests.Unit.Infrastructure.OfflineSync
                         Skipped = request.Skip,
                         Length = bytes.Length,
                         Total = content.Length,
-                        ContentMD5 = new MD5CryptoServiceProvider().ComputeHash(bytes)
+                        ContentMD5 = new MD5CryptoServiceProvider().ComputeHash(content)
                     };
 
                     CallsCount++;
