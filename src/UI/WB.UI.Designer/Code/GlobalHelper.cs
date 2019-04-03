4279e712e5ba363c5a0969a5218493f2eef69e95
Andrii Kozhyn <hedgic@gmail.com>
2019-04-03 16:12:49 +0300

    KP-12170 Ability to navigate to profile

--- src/UI/WB.UI.Designer/Code/GlobalHelper.cs
+++ src/UI/WB.UI.Designer/Code/GlobalHelper.cs
@@ -1,9 +1,22 @@
-﻿namespace WB.UI.Designer.Code
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace WB.UI.Designer.Code
 {
     public static class GlobalHelper
     {
         public const string EmptyString = "--//--";
 
         public const int GridPageItemsCount = 50;
+
+        public static string CurrentAction(this ViewContext viewContext)
+        {
+            return (string) viewContext.RouteData.Values["Action"];
+        }
+
+        public static string CurrentController(this ViewContext viewContext)
+        {
+            return (string) viewContext.RouteData.Values["Controller"];
+        }
     }
 }
