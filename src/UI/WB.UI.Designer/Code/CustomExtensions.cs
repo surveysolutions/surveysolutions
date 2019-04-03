62c7c6c8a6d018f001f6395a977a17883b9caa2b
Andrii Kozhyn <hedgic@gmail.com>
2019-04-03 12:32:54 +0300

    KP-12170 moving questionnaires list to core

---
+++ src/UI/WB.UI.Designer/Code/CustomExtensions.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using Ionic.Zip;
+using WB.UI.Designer.Code;
+using WB.UI.Designer.Resources;
+
+namespace WB.UI.Designer.Extensions
+{
+    public static class CustomExtensions
+    {
+        public static byte[] ReadToEnd(this Stream stream)
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                stream.CopyTo(memoryStream);
+                return memoryStream.ToArray();
+            }
+        }
+
+        public static void PutFileEntry(this ZipOutputStream stream, string filename, byte[] content)
+        {
+            var entry = new ZipEntry() {AlternateEncoding = Encoding.Unicode};
+            stream.PutNextEntry(filename);
+            stream.Write(content, 0, content.Length);
+        }
+
+        public static void PutTextFileEntry(this ZipOutputStream stream, string filename, string text)
+            => stream.PutFileEntry(filename, Encoding.UTF8.GetBytes(text ?? string.Empty));
+
+        public static Guid AsGuid(this object source)
+        {
+            if (source == null)
+                return Guid.Empty;
+            return Guid.Parse(source.ToString());
+        }
+
+        public static int? InvertBooleableInt(this int? val, bool needValue)
+        {
+            return needValue && !val.ToBool() ? 1 : (int?)null;
+        }
+
+        public static bool ToBool(this int? val)
+        {
+            return val.HasValue && (val.Value == 1);
+        }
+
+        public static string ToUIString(this DateTime source)
+        {
+            return DateTime.Compare(source.ToUniversalTime(), DateTime.MinValue) == 0
+                       ? GlobalHelper.EmptyString
+                       : source.ToString(CultureInfo.InvariantCulture);
+        }
+    }
+}
--- src/UI/WB.UI.Designer/Controllers/HomeController.cs
+++ src/UI/WB.UI.Designer/Controllers/HomeController.cs
@@ -1,16 +1,88 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using WB.Core.BoundedContexts.Designer.MembershipProvider.Roles;
+using WB.Core.BoundedContexts.Designer.Services;
+using WB.Core.BoundedContexts.Designer.Views.Questionnaire.QuestionnaireList;
+using WB.UI.Designer.BootstrapSupport.HtmlHelpers;
+using WB.UI.Designer.Code;
+using WB.UI.Designer.Extensions;
+using WB.UI.Designer.Models;
+using WB.UI.Designer1.Extensions;
 using WB.UI.Designer1.Models;
 
-namespace WB.UI.Designer1.Controllers
+namespace WB.UI.Designer.Controllers
 {
     [Authorize]
     public class HomeController : Controller
     {
+        private readonly IQuestionnaireHelper questionnaireHelper;
+        private readonly IPublicFoldersStorage publicFoldersStorage;
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Index(int? p, string sb, int? so, string f)
+            => this.View(this.GetQuestionnaires(pageIndex: p, sortBy: sb, sortOrder: so, searchFor: f, type: QuestionnairesType.My, folderId: null));
+
+        public ActionResult Public(int? p, string sb, int? so, string f, Guid? id)
+        {
+            var questionnaires = this.GetQuestionnaires(pageIndex: p, sortBy: sb, sortOrder: so, searchFor: f,
+                type: QuestionnairesType.Public, folderId: id);
+
+            var folderPath = publicFoldersStorage.GetFoldersPath(folderId: id);
+            var breadcrumbs = folderPath.Select(folder => new FolderBreadcrumbsModel()
+            {
+                FolderId = folder.PublicId,
+                Title = folder.Title
+            }).ToArray();
+
+            var model = new QuestionnaireListModel()
+            {
+                IsSupportAssignFolders = User.IsAdmin(),
+                CurrentFolderId = id,
+                Breadcrumbs = breadcrumbs,
+                Questionnaires = questionnaires
+            };
+
+            return this.View(model);
+        }
+
+        public ActionResult Shared(int? p, string sb, int? so, string f)
+            => this.View(this.GetQuestionnaires(pageIndex: p, sortBy: sb, sortOrder: so, searchFor: f, type: QuestionnairesType.Shared, folderId: null));
+
+
+        private IPagedList<QuestionnaireListViewModel> GetQuestionnaires(int? pageIndex, string sortBy, int? sortOrder, string searchFor, QuestionnairesType type, Guid? folderId)
+        {
+            this.SaveRequest(pageIndex: pageIndex, sortBy: ref sortBy, sortOrder: sortOrder, searchFor: searchFor, folderId: folderId);
+
+            return this.questionnaireHelper.GetQuestionnaires(
+                pageIndex: pageIndex,
+                sortBy: sortBy,
+                sortOrder: sortOrder,
+                searchFor: searchFor,
+                folderId: folderId,
+                viewerId: User.GetId(),
+                isAdmin: User.IsAdmin(),
+                type: type);
+        }
+
+        private void SaveRequest(int? pageIndex, ref string sortBy, int? sortOrder, string searchFor, Guid? folderId)
+        {
+            this.ViewBag.PageIndex = pageIndex;
+            this.ViewBag.SortBy = sortBy;
+            this.ViewBag.Filter = searchFor;
+            this.ViewBag.SortOrder = sortOrder;
+            this.ViewBag.FolderId = folderId;
+
+            if (sortOrder.ToBool())
+            {
+                sortBy = $"{sortBy} Desc";
+            }
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
