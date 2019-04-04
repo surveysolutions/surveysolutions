39295eafe7a46df2f8f91c6c0aed63e1fc1b06fe
Victor Gladkikh <me@alfeg.net>
2019-04-04 12:11:49 +0300

    KP-12556 Migrations to own files

---
+++ src/Infrastructure/WB.Persistence.Headquarters/Migrations/ReadSide/M2019/M201903291730_RestoreForeignKeys.cs
@@ -0,0 +1,38 @@
+﻿using System.Data;
+using FluentMigrator;
+
+namespace WB.Persistence.Headquarters.Migrations.ReadSide
+{
+    [Migration(201903291730)]
+    public class M201903291730_RestoreForeignKeys :Migration
+    {
+        public override void Up()
+        {
+            // recreating dropped by cascade foreign keys
+
+            Create.ForeignKey("fk_interviewsummaries_answerstofeaturedquestions")
+                .FromTable("answerstofeaturedquestions")
+                .ForeignColumn("interviewsummaryid")
+                .ToTable("interviewsummaries").PrimaryColumn("summaryid");
+
+            Create.ForeignKey("FK_InterviewSummary_InterviewCommentedStatuses")
+                .FromTable("interviewcommentedstatuses").ForeignColumn("interviewid")
+                .ToTable("interviewsummaries").PrimaryColumn("summaryid")
+                .OnDelete(Rule.Cascade);
+
+            Create.ForeignKey("FK_InterviewSummary_TimeSpansBetweenStatuses")
+                .FromTable("timespanbetweenstatuses").ForeignColumn("interviewid")
+                .ToTable("interviewsummaries").PrimaryColumn("summaryid")
+                .OnDelete(Rule.Cascade);
+
+            Create.ForeignKey("fk_interviewsummary_interviewflag").FromTable("interviewflags")
+                .ForeignColumn("interviewid").ToTable("interviewsummaries").PrimaryColumn("summaryid")
+                .OnDelete(Rule.Cascade);
+        }
+
+        public override void Down()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ src/Infrastructure/WB.Persistence.Headquarters/Migrations/ReadSide/M2019/M201904011100_MoveTimespanBetweenStatusesToInterviewId.cs
@@ -0,0 +1,30 @@
+﻿using FluentMigrator;
+
+namespace WB.Persistence.Headquarters.Migrations.ReadSide
+{
+    public class M201904011100_MoveTimespanBetweenStatusesToInterviewId : Migration
+    {
+        public override void Up()
+        {
+            Execute.Sql(@"ALTER TABLE readside.timespanbetweenstatuses ADD interview_id int4 NULL;
+
+                CREATE INDEX timespanbetweenstatuses_interview_idx ON readside.timespanbetweenstatuses (interviewid);
+
+                update readside.timespanbetweenstatuses c 
+	                set interview_id = s.id
+                from readside.interviewsummaries s
+                where s.summaryid = c.interviewid;
+
+                ALTER TABLE readside.timespanbetweenstatuses DROP COLUMN interviewid;
+
+                ALTER TABLE readside.timespanbetweenstatuses ALTER COLUMN interview_id SET NOT NULL;
+
+                CREATE INDEX timespanbetweenstatuses_interviewid_idx ON readside.timespanbetweenstatuses (interview_id);");
+        }
+
+        public override void Down()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
