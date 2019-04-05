4a969c56810cff615f653c203d867c52b1e25480
Roman Lytvyn <litvin.roma@gmail.com>
2019-04-05 17:23:42 +0300

    KP-12700 Interview GPS answers sync failed. Removed composite key for interview gps entities. Added id column. Added migration

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Views/Interview/InterviewGps.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Views/Interview/InterviewGps.cs
@@ -7,6 +7,7 @@ namespace WB.Core.BoundedContexts.Headquarters.Views.Interview
     [DebuggerDisplay("{ToString()}")]
     public class InterviewGps
     {
+        public virtual string Id { get; set; }
         public virtual string InterviewId { get; set; }
         public virtual Guid QuestionId { get; set; }
         public virtual string RosterVector { get; set; }
@@ -15,20 +16,8 @@ namespace WB.Core.BoundedContexts.Headquarters.Views.Interview
         public virtual DateTimeOffset Timestamp { get; set; }
         public virtual bool IsEnabled { get; set; }
 
-
-        public override bool Equals(object obj) => obj is InterviewGps gps &&
-                                                   InterviewId == gps.InterviewId &&
-                                                   QuestionId.Equals(gps.QuestionId) &&
-                                                   RosterVector == gps.RosterVector;
-
-        public override int GetHashCode()
-        {
-            var hashCode = 435978002;
-            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(InterviewId);
-            hashCode = hashCode * -1521134295 + EqualityComparer<Guid>.Default.GetHashCode(QuestionId);
-            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(RosterVector);
-            return hashCode;
-        }
+        public override bool Equals(object obj) => obj is InterviewGps gps && Id == gps.Id;
+        public override int GetHashCode() => 2108858624 + EqualityComparer<string>.Default.GetHashCode(Id);
 
         public override string ToString() =>
             $"[{Latitude}, {Longitude}] i: {InterviewId} q: {QuestionId}${RosterVector}";
