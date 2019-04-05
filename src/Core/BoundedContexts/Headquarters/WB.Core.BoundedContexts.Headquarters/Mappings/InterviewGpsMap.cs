623570efd020084c43a1f48227cab431b99bb302
Roman Lytvyn <litvin.roma@gmail.com>
2019-04-05 17:23:42 +0300

    KP-12700 Interview GPS answers sync failed. Removed composite key for interview gps entities. Added id column. Added migration

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Mappings/InterviewGpsMap.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Mappings/InterviewGpsMap.cs
@@ -9,12 +9,10 @@ namespace WB.Core.BoundedContexts.Headquarters.Mappings
         public InterviewGpsMap()
         {
             Table("interview_geo_answers");
-            ComposedId(m =>
-            {
-                m.Property(x => x.InterviewId);
-                m.Property(x => x.QuestionId);
-                m.Property(x => x.RosterVector);
-            });
+            Id(x => x.Id);
+            Property(x => x.InterviewId);
+            Property(x => x.QuestionId);
+            Property(x => x.RosterVector);
             Property(x => x.Latitude);
             Property(x => x.Longitude);
             Property(x => x.Timestamp, mapper =>
