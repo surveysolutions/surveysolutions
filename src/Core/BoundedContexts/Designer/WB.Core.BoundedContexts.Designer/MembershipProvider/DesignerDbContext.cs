2fa04ac823f7ad89fac1c47747ac30affe473860
Andrii Kozhyn <hedgic@gmail.com>
2019-04-08 13:56:47 +0300

    KP-12701 working on EF mappings

---
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/MembershipProvider/DesignerDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WB.Core.BoundedContexts.Designer.MembershipProvider.Mappings;
+using WB.Core.BoundedContexts.Designer.Views.Questionnaire.QuestionnaireList;
+
+namespace WB.Core.BoundedContexts.Designer.MembershipProvider
+{
+    public class DesignerDbContext : IdentityDbContext<DesignerIdentityUser>
+    {
+        public DesignerDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected DesignerDbContext()
+        {
+        }
+
+        public DbSet<QuestionnaireListViewItem> Questionnaires { get; private set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.HasDefaultSchema("plainstore");
+
+            builder.Entity<DesignerIdentityUser>(x => x.Property(p => p.PasswordSalt).HasColumnName("PasswordSalt"));
+            builder.Entity<DesignerIdentityUser>(x => x.Property(p => p.CanImportOnHq).HasColumnName("CanImportOnHq"));
+            builder.Entity<DesignerIdentityUser>(x => x.Property(p => p.CreatedAtUtc).HasColumnName("CreatedAtUtc"));
+            builder.ApplyConfiguration(new QuestionnaireListViewItemTypeConfig());
+        }
+    }
+}
