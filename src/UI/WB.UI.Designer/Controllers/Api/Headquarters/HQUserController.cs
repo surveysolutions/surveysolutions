19f50c3a893cbaecc904d9bf8fe0d6d882f09b15
Andrii Kozhyn <hedgic@gmail.com>
2019-04-11 11:20:42 +0300

    KP-12703 migrated webapi for hq

---
+++ src/UI/WB.UI.Designer/Controllers/Api/Headquarters/HQUserController.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+using WB.UI.Designer.Models;
+using WB.UI.Designer1.Extensions;
+
+namespace WB.UI.Designer.Api.Headquarters
+{
+    [Route("api/hq/user")]
+    public class HQUserController : ControllerBase
+    {
+        [HttpGet]
+        [Route("login")]
+        //[ApiBasicAuth(onlyAllowedAddresses: true)]
+        public void Login() { }
+
+        [HttpGet]
+        //[ApiBasicAuth(onlyAllowedAddresses: false)]
+        [Route("userdetails")]
+        public PortalUserModel UserDetails()
+        {
+            return new PortalUserModel
+            {
+                Id = User.GetId(),
+                Login = User.Identity.Name,
+                Roles = User.Claims.Where(x => x.Type == ClaimTypes.Role).Select(x => x.Value).ToArray(),
+                Email = User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.Email)?.Value
+            };
+        }
+    }
+}
