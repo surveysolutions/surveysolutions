c161055fca9eabb6e42c4f752d343edc3650c8f7
Andrii Kozhyn <hedgic@gmail.com>
2019-04-11 18:59:27 +0300

    KP-12715 Migrated api for portal

---
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/MembershipProvider/Mappings/QuestionnaireChangeReferenceTypeConfig.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WB.Core.BoundedContexts.Designer.Views.Questionnaire.ChangeHistory;
+
+namespace WB.Core.BoundedContexts.Designer.MembershipProvider.Mappings
+{
+    public class QuestionnaireChangeReferenceTypeConfig : IEntityTypeConfiguration<QuestionnaireChangeReference>
+    {
+        public void Configure(EntityTypeBuilder<QuestionnaireChangeReference> builder)
+        {
+            builder.ToTable("questionnairechangereferences", "plainstore");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(e => e.Id)
+                .HasColumnName("id")
+                .ValueGeneratedOnAdd();
+
+            builder.Property(e => e.QuestionnaireChangeRecordId)
+                .HasColumnName("questionnairechangerecordid")
+                .HasMaxLength(255);
+
+            builder.Property(e => e.ReferenceId).HasColumnName("referenceid");
+
+            builder.Property(e => e.ReferenceTitle).HasColumnName("referencetitle");
+
+            builder.Property(e => e.ReferenceType).HasColumnName("referencetype");
+
+            builder.HasOne(d => d.QuestionnaireChangeRecord)
+                .WithMany(p => p.References)
+                .HasForeignKey(d => d.QuestionnaireChangeRecordId)
+                .HasConstraintName("questionnairechangereferences_questionnairechangerecordid");
+        }
+    }
+}
--- src/UI/WB.UI.Designer/Models/DeploymentUserModel.cs
+++ src/UI/WB.UI.Designer/Models/DeploymentUserModel.cs
@@ -4,7 +4,7 @@ namespace WB.UI.Designer.Models
 {
     public class PortalUserModel
     {
-        public Guid Id { get; set; }
+        public string Id { get; set; }
         public string[] Roles { get; set; }
         public string Login { get; set; }
         public string Email { get; set; }
