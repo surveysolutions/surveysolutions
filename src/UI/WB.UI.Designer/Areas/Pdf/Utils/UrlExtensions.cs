e33d36085f4a67047ddf8371f08f12cb8ecda5f9
Pavel Stovpivskyi <630198+spuv@users.noreply.github.com>
2019-04-11 15:31:20 +0300

    KP-12713 pdf

---
+++ src/UI/WB.UI.Designer/Areas/Pdf/Utils/UrlExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
+
+namespace WB.UI.Designer.Code
+{
+    public static class UrlExtensions
+    {
+        public static string ContentAbsolute(this IUrlHelper urlHelper, string contentPath)
+        {
+            var path = urlHelper.Content(contentPath);
+            return ConvertToAbsoluteUrl(urlHelper, path);
+        }
+
+        public static string HttpRouteUrlAbsolute(this IUrlHelper urlHelper, string routeName, object routeValues)
+        {
+            var path = urlHelper.RouteUrl(new UrlRouteContext()
+            {
+                RouteName = routeName,
+                Values = routeValues
+            });
+            return ConvertToAbsoluteUrl(urlHelper, path);
+        }
+
+        private static string ConvertToAbsoluteUrl(this IUrlHelper urlHelper, string path)
+        {
+            var url = new Uri(new Uri(urlHelper.ActionContext.HttpContext.Request.GetDisplayUrl()), path);
+            return url.AbsoluteUri;
+        }
+    }
+}
