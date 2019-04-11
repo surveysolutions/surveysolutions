5f58780429dc1ed62cc06a2e779c21a56102ab24
Victor Gladkikh <me@alfeg.net>
2019-04-11 18:54:42 +0300

    KP-12732 Assests manage # Trying to fix details

--- src/UI/WB.UI.Designer/gulpfile.babel.js
+++ src/UI/WB.UI.Designer/gulpfile.babel.js
@@ -6,11 +6,9 @@ import questionnaire from "./build/questionnaire";
 import bundle from "./build/bundler";
 import config from "./build/config"
 
-export const cleanup = () => {
-  return src(config.dist, { allowEmpty: true }).pipe(clean());
-};
+export const cleanup = () => src(config.dist, { allowEmpty: true }).pipe(clean());
 
 export const questionnaires = questionnaire;
 export const bundler = bundle;
 
-export default series(cleanup, parallel(vendor, bundler, questionnaire));
+export default series(cleanup, parallel(vendor, bundler), questionnaire);
