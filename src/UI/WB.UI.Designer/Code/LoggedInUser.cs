b45846f259d6c03e3ff816b609ac85da4f14b4a3
Andrii Kozhyn <hedgic@gmail.com>
2019-04-12 13:39:20 +0300

    KP-12175 More code migrated to ef

---
+++ src/UI/WB.UI.Designer/Code/LoggedInUser.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using WB.Core.BoundedContexts.Designer.MembershipProvider;
+using WB.Core.GenericSubdomains.Portable;
+using WB.UI.Designer1.Extensions;
+
+namespace WB.UI.Designer.Code
+{
+    public class LoggedInUser : ILoggedInUser
+    {
+        private readonly IHttpContextAccessor httpContextAccessor;
+
+        public LoggedInUser(IHttpContextAccessor httpContextAccessor)
+        {
+            this.httpContextAccessor = httpContextAccessor;
+        }
+
+        public string Id => httpContextAccessor.HttpContext.User.GetId().FormatGuid();
+        public string Login => httpContextAccessor.HttpContext.User.GetUserName();
+        public bool IsAdmin => httpContextAccessor.HttpContext.User.IsAdmin();
+    }
+}
