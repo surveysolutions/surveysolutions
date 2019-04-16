369a6afa2fe1fff6001fc7dd9799344d69e789be
Andrii Kozhyn <andrii.kozhyn@outlook.com>
2019-04-16 10:11:02 +0300

    KP-12717 working on tests

--- src/Tests/WB.Tests.Unit.Designer/BoundedContexts/Designer/QuestionnaireInfoViewFactoryTests/QuestionnaireInfoViewFactoryContext.cs
+++ src/Tests/WB.Tests.Unit.Designer/BoundedContexts/Designer/QuestionnaireInfoViewFactoryTests/QuestionnaireInfoViewFactoryContext.cs
@@ -4,6 +4,7 @@ using Moq;
 using NSubstitute;
 using WB.Core.BoundedContexts.Designer.Aggregates;
 using WB.Core.BoundedContexts.Designer.Implementation.Services.Accounts.Membership;
+using WB.Core.BoundedContexts.Designer.MembershipProvider;
 using WB.Core.BoundedContexts.Designer.QuestionnaireCompilationForOldVersions;
 using WB.Core.BoundedContexts.Designer.Services;
 using WB.Core.BoundedContexts.Designer.Views.Questionnaire.Edit.QuestionnaireInfo;
@@ -21,8 +22,7 @@ namespace WB.Tests.Unit.Designer.BoundedContexts.Designer.QuestionnaireInfoViewF
 
         protected static QuestionnaireInfoViewFactory CreateQuestionnaireInfoViewFactory(
             IPlainKeyValueStorage<QuestionnaireDocument> repository = null,
-            IPlainStorageAccessor<User> accountsDocumentReader = null,
-            IPlainStorageAccessor<QuestionnaireListViewItem> questionnaireListViewItemStorage = null)
+            DesignerDbContext accountsDocumentReader = null)
         {
             var doc = new QuestionnaireDocument();
             var mockedListStorage = new Mock<IPlainStorageAccessor<QuestionnaireListViewItem>>();
@@ -30,10 +30,10 @@ namespace WB.Tests.Unit.Designer.BoundedContexts.Designer.QuestionnaireInfoViewF
 
             return
                 new QuestionnaireInfoViewFactory(repository ?? Mock.Of<IPlainKeyValueStorage<QuestionnaireDocument>>(x => x.GetById(It.IsAny<string>()) == doc),
-                                                questionnaireListViewItemStorage ?? mockedListStorage.Object, 
-                                                Mock.Of<IQuestionnaireCompilationVersionService>(),
-                                                accountsDocumentReader ?? Mock.Of<IPlainStorageAccessor<User>>(),
-                                                Mock.Of<IAttachmentService>(), Mock.Of<IMembershipUserService>(x=>x.WebUser == Substitute.For<IMembershipWebUser>()));
+                                                accountsDocumentReader ?? Create.InMemoryDbContext(),
+                                                Mock.Of<IQuestionnaireCompilationVersionService>(), 
+                                                Mock.Of<IAttachmentService>(),
+                                                Mock.Of<IIdentityService>());
         }
     }
 }
