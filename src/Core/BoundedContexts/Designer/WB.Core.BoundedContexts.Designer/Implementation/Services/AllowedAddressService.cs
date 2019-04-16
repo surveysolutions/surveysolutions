e947eb860a328f955269cab9279fba4f103a4ff8
Roman Lytvyn <litvin.roma@gmail.com>
2019-04-16 11:17:12 +0300

    KP-12702 Control panel. Fixed CRUD by allowed ip addresses

--- src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Implementation/Services/AllowedAddressService.cs
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Implementation/Services/AllowedAddressService.cs
@@ -21,11 +21,26 @@ namespace WB.Core.BoundedContexts.Designer.Implementation.Services
 
         public AllowedAddress GetAddressById(int id) => this.dbContext.AllowedAddress.Find(id);
 
-        public void Update(AllowedAddress allowedAddress) => this.dbContext.AllowedAddress.Update(allowedAddress);
+        public void Update(AllowedAddress allowedAddress)
+        {
+            this.dbContext.AllowedAddress.Update(allowedAddress);
+            this.dbContext.SaveChanges();
+        }
 
-        public void Remove(int id) => this.dbContext.AllowedAddress.Where(x => x.Id == id).Delete();
+        public void Remove(int id)
+        {
+            var address = this.dbContext.AllowedAddress.Find(id);
+            if (address == null) return;
 
-        public void Add(AllowedAddress allowedAddress) => this.dbContext.AllowedAddress.Add(allowedAddress);
+            this.dbContext.AllowedAddress.Remove(address);
+            this.dbContext.SaveChanges();
+        }
+
+        public void Add(AllowedAddress allowedAddress)
+        {
+            this.dbContext.AllowedAddress.Add(allowedAddress);
+            this.dbContext.SaveChanges();
+        }
 
         public bool IsAllowedAddress(IPAddress ip) => this.dbContext.AllowedAddress.Any(x => x.Address == ip.ToString());
     }
