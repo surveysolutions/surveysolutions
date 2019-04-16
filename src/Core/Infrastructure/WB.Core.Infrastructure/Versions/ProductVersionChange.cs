9b878ac3ea4371156c9ee14088ac555fa2033eb4
Roman Lytvyn <litvin.roma@gmail.com>
2019-04-16 16:40:22 +0300

    fixed build

--- src/Core/Infrastructure/WB.Core.Infrastructure/Versions/ProductVersionChange.cs
+++ src/Core/Infrastructure/WB.Core.Infrastructure/Versions/ProductVersionChange.cs
@@ -4,6 +4,14 @@ namespace WB.Core.Infrastructure.Versions
 {
     public class ProductVersionChange
     {
+        protected ProductVersionChange() { }
+
+        public ProductVersionChange(string productVersion, DateTime updateTimeUtc)
+        {
+            this.ProductVersion = productVersion;
+            this.UpdateTimeUtc = updateTimeUtc;
+        }
+
         public virtual string ProductVersion { get; set; }
         public virtual DateTime UpdateTimeUtc { get; set; }
     }
