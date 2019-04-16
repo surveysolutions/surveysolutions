ba655597d44bf8de8e1d91c1a7893dc440ec26be
Slava Scherbak <slava.scherbak@gmail.com>
2019-04-16 17:28:55 -0400

    KP-1275: improved handling of async operations

--- src/Core/SharedKernels/Enumerator/Enumerator/ViewModels/InterviewDetails/Questions/SingleOptionQuestionOptionViewModel.cs
+++ src/Core/SharedKernels/Enumerator/Enumerator/ViewModels/InterviewDetails/Questions/SingleOptionQuestionOptionViewModel.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using MvvmCross.Commands;
 using MvvmCross.ViewModels;
 using WB.Core.SharedKernels.Enumerator.ViewModels.InterviewDetails.Questions.State;
@@ -9,8 +10,8 @@ namespace WB.Core.SharedKernels.Enumerator.ViewModels.InterviewDetails.Questions
     public class SingleOptionQuestionOptionViewModel : MvxNotifyPropertyChanged,
         ICompositeEntity
     {
-        public event EventHandler BeforeSelected;
-        public event EventHandler AnswerRemoved;
+        public event Func<object, EventArgs, Task> BeforeSelected;
+        public event Func<object, EventArgs, Task> AnswerRemoved;
         public EnablementViewModel Enablement { get; set; }
 
         public int Value { get; set; }
