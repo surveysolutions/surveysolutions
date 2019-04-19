c84c0b0f596909b4ff935f0ad534910367b7c059
Roman Lytvyn <litvin.roma@gmail.com>
2019-04-19 15:48:41 +0300

    KP-12756 404 error page. Moved to code

--- src/UI/WB.UI.Designer/Controllers/ErrorController.cs
+++ src/UI/WB.UI.Designer/Controllers/ErrorController.cs
@@ -2,14 +2,16 @@
 
 namespace WB.UI.Designer.Controllers
 {
+    [Route("error")]
     public class ErrorController : Controller
     {
+        [Route("500")]
         public ActionResult Index() => View();
-
+        [Route("404")]
         public ActionResult NotFound() => View();
-
+        [Route("401")]
         public ActionResult AccessDenied() => View();
-
+        [Route("403")]
         public ActionResult Forbidden() => this.View();
     }
 }
--- src/UI/WB.UI.Designer/Startup.cs
+++ src/UI/WB.UI.Designer/Startup.cs
@@ -159,9 +159,7 @@ namespace WB.UI.Designer
             }
             else
             {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                // app.UseHsts();
+                app.UseStatusCodePagesWithReExecute("/error/{0}");
             }
 
             app.UseHttpsRedirection();
