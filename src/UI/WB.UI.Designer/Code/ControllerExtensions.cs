a56180fc5ebc0f96e5cc448e73f57324ea725dcf
Victor Hladkikh <me@alfeg.net>
2019-05-02 18:06:39 +0300

    Fix for undefinded errors in desiger

---
+++ src/UI/WB.UI.Designer/Code/ControllerExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WB.UI.Designer.Code
+{
+    public static class ControllerExtensions
+    {
+        public static IActionResult Error(this ControllerBase controller, int statusCode, string message)
+        {
+            return new JsonResult(new { message }) { StatusCode = statusCode };
+        }
+
+        public static IActionResult Error(this ControllerBase controller, int statusCode, object message)
+        {
+            return new JsonResult(message) { StatusCode = statusCode };
+        }
+    }
+}
