ef70e7344747130a61c9898a2dfb202b4868115f
Andrii Kozhyn <andrii.kozhyn@outlook.com>
2019-05-03 12:28:00 +0300

    Fixed namespace

--- src/Tests/WB.Tests.Unit.Designer/QuestionnaireTests/DeleteGroupTests.cs
+++ src/Tests/WB.Tests.Unit.Designer/QuestionnaireTests/DeleteGroupTests.cs
@@ -2,7 +2,6 @@
 using Main.Core.Entities.SubEntities;
 using NUnit.Framework;
 using WB.Core.BoundedContexts.Designer.Aggregates;
-using WB.Core.BoundedContexts.Designer.Exceptions;
 
 namespace WB.Tests.Unit.Designer.BoundedContexts.QuestionnaireTests
 {
