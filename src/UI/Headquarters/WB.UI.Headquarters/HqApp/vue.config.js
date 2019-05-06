d1bcde80f3c75a02d38bbb14c9d5ac1922edf532
Andrii Kozhyn <andrii.kozhyn@outlook.com>
2019-05-06 17:48:21 +0300

    KP-10769 version of questionnaire on take new page

--- src/UI/Headquarters/WB.UI.Headquarters/HqApp/vue.config.js
+++ src/UI/Headquarters/WB.UI.Headquarters/HqApp/vue.config.js
@@ -37,7 +37,7 @@ const pages = {
             "WebInterviewSetup",
             "WebInterviewSettings",
             "MapReport",
-			"Settings"
+            "Settings"
         ]
     },
     webinterview: {
