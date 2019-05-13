8c1180a6020b37073f7ec0d09f69e0b15e539124
Andrii Kozhyn <hedgic@gmail.com>
2019-05-13 14:16:15 +0300

    KP-12824 refresh suggestion list after answers removed action

--- src/Core/SharedKernels/Enumerator/Enumerator/ViewModels/InterviewDetails/Questions/CategoricalComboboxAutocompleteViewModel.cs
+++ src/Core/SharedKernels/Enumerator/Enumerator/ViewModels/InterviewDetails/Questions/CategoricalComboboxAutocompleteViewModel.cs
@@ -121,7 +121,7 @@ namespace WB.Core.SharedKernels.Enumerator.ViewModels.InterviewDetails.Questions
         {
             var suggestions = this.GetSuggestions(filterToUpdate).ToList();
 
-            await this.InvokeOnMainThreadAsync(async () =>
+            await this.InvokeOnMainThreadAsync(() =>
             {
                 this.AutoCompleteSuggestions = suggestions;
             });
@@ -129,11 +129,13 @@ namespace WB.Core.SharedKernels.Enumerator.ViewModels.InterviewDetails.Questions
 
         public async Task UpdateFilter(string filter, bool forced = false)
         {
+            var oldFilterText = this.FilterText;
             this.FilterText = filter;
-            await this.InvokeOnMainThreadAsync(async () =>
+
+            if (this.FilterText != oldFilterText)
             {
-                await this.RaisePropertyChanged(() => this.FilterText);
-            });
+                await this.RaisePropertyChanged(nameof(FilterText));
+            }
 
             if (this.filterToUpdate == filter && !forced)
             {
