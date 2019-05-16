d805c094ba9e42100fbe006baf188799c3d804e1
Andrii Kozhyn <andrii.kozhyn@outlook.com>
2019-05-16 15:12:06 +0300

    fixed check for secure connection

--- src/UI/Shared/WB.UI.Shared.Web/Filters/EnforceHttpsHandler.cs
+++ src/UI/Shared/WB.UI.Shared.Web/Filters/EnforceHttpsHandler.cs
@@ -13,11 +13,8 @@ namespace WB.UI.Shared.Web.Filters
         protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
             var isHttpRequest = request.RequestUri.Scheme != Uri.UriSchemeHttps;
-            var isForwardedFromHttps = request.Headers.Contains("X-Forwarded-Proto")
-                                       && request.Headers.GetValues("X-Forwarded-Proto").First()
-                                           .Equals("https", StringComparison.OrdinalIgnoreCase);
 
-            if ((isHttpRequest || !isForwardedFromHttps) && CoreSettings.IsHttpsRequired)
+            if (isHttpRequest && CoreSettings.IsHttpsRequired)
             {
                 var response = new HttpResponseMessage(HttpStatusCode.Forbidden)
                 {
