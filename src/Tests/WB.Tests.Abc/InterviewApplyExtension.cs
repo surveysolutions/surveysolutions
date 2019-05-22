2ccf119010099370d4f810862f447e75996d3d5e
Victor Hladkikh <me@alfeg.net>
2019-05-22 17:36:36 +0300

    Interview now do not use InterviewTree.Clone() to build diff

---
+++ src/Tests/WB.Tests.Abc/InterviewApplyExtension.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Reflection;
+using WB.Core.Infrastructure.EventBus;
+using WB.Core.SharedKernels.DataCollection.Implementation.Aggregates;
+
+namespace WB.Tests.Abc
+{
+    public static class InterviewApplyExtension
+    {
+        static readonly ConcurrentDictionary<Type, MethodInfo> reflectionCache = new ConcurrentDictionary<Type, MethodInfo>();
+
+        /// <summary>
+        /// Do not use this method in new tests! use Commands or InitializeFromHistory
+        /// </summary>
+        /// <param name="interview"></param>
+        /// <param name="event"></param>
+        public static void Apply(this Interview interview, IEvent @event)
+        {
+            var method =
+                reflectionCache.GetOrAdd(@event.GetType(), type =>
+                    typeof(Interview).GetMethod(
+                        "Apply",
+                        BindingFlags.Instance | BindingFlags.NonPublic,
+                        Type.DefaultBinder,
+                        new[] {type},
+                        null));
+
+            method.Invoke(interview, new[] { @event });
+
+            interview.DropChangedTree();
+        }
+    }
+}
