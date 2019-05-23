2abaf8b03c73098c362ba221bb9798d46f1f7260
Pavel Stovpivskyi <630198+spuv@users.noreply.github.com>
2019-05-23 16:15:51 +0300

    KP-12746 moved mask to separate directive

--- src/UI/Headquarters/WB.UI.Headquarters/HqApp/src/webinterview/components/questions/TableRoster.TextQuestion.vue
+++ src/UI/Headquarters/WB.UI.Headquarters/HqApp/src/webinterview/components/questions/TableRoster.TextQuestion.vue
@@ -7,7 +7,7 @@
         :placeholder="noAnswerWatermark"
         :value="$me.answer"
         :disabled="!$me.acceptAnswer"
-        v-mask="$me.mask"
+        v-maskedText="$me.mask"
         :data-mask-completed="$me.isAnswered" />
     <input v-else ref="inputWithoutMask"
         autocomplete="off"
@@ -70,32 +70,6 @@
                         this.$store.dispatch('answerTextQuestion', { identity: this.id, text: answer })
                     }
                 })
-            }
-        },
-        directives: {
-            mask: {
-                bind(el, binding) {
-                    if (binding.value) {
-                        const input = $(el)
-                        input.mask(binding.value, {
-                            onChange: function () {
-                                input.data("maskCompleted", false);
-                            },
-                            onComplete: function () {
-                                input.data("maskCompleted", true);
-                            },
-                            translation: {
-                                "0": { pattern: /0/, fallback: "0" },
-                                "~": { pattern: /[a-zA-Z]/ },
-                                "#": { pattern: /\d/ },
-                                "*": { pattern: /[a-zA-Z0-9]/ },
-                                "9": { pattern: /9/, fallback: "9" },
-                                'A': { pattern: /A/, fallback: "A" },
-                                'S': { pattern: /S/, fallback: "S" }
-                            }
-                        })
-                    }
-                }
             }
         },
         mounted() {
