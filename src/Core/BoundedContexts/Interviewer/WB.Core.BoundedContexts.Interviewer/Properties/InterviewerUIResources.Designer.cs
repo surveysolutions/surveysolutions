0631aacbd42a691ca2aa22398013e959906be9ff
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2019-05-31 18:05:38 -0400

    KP-12743 Update to new version in result of sync

--- src/Core/BoundedContexts/Interviewer/WB.Core.BoundedContexts.Interviewer/Properties/InterviewerUIResources.Designer.cs
+++ src/Core/BoundedContexts/Interviewer/WB.Core.BoundedContexts.Interviewer/Properties/InterviewerUIResources.Designer.cs
@@ -19,7 +19,7 @@ namespace WB.Core.BoundedContexts.Interviewer.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class InterviewerUIResources {
@@ -1632,6 +1632,15 @@ namespace WB.Core.BoundedContexts.Interviewer.Properties {
         public static string Synchronization_LockedAssignments_Text {
             get {
                 return ResourceManager.GetString("Synchronization_LockedAssignments_Text", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to New version available.
+        /// </summary>
+        public static string Synchronization_NewVersionExists {
+            get {
+                return ResourceManager.GetString("Synchronization_NewVersionExists", resourceCulture);
             }
         }
         
