b3db7e67e942eab0c2c372bab7fbd03f69c5684b
Pavel Stovpivskyi <sby@rambler.ru>
2019-06-06 18:56:13 +0300

    removed 'text'

--- src/UI/Headquarters/WB.UI.Headquarters/HqApp/src/webinterview/components/questions/TableRoster.QuestionTitle.vue
+++ src/UI/Headquarters/WB.UI.Headquarters/HqApp/src/webinterview/components/questions/TableRoster.QuestionTitle.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="ag-cell-label-container">    
 		<div class="ag-header-cell-label">    
-			<span class="ag-header-cell-text" v-html="params.displayName">text</span>    
+			<span class="ag-header-cell-text" v-html="params.displayName"></span>    
 		</div>
 	</div>
 </template>
