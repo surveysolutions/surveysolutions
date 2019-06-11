add9fbee48110d85683a2df3841d35146d40ee75
Andrii Kozhyn <hedgic@gmail.com>
2019-06-11 12:04:58 +0300

    KP-12927 Scenario is stored on designer

--- src/UI/WB.UI.Designer/Controllers/Api/WebTester/ScenariosController.cs
+++ src/UI/WB.UI.Designer/Controllers/Api/WebTester/ScenariosController.cs
@@ -1,17 +1,37 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using WB.Core.BoundedContexts.Designer.MembershipProvider;
+using WB.Core.BoundedContexts.Designer.Scenarios;
+using WB.Core.GenericSubdomains.Portable;
+using WB.UI.Designer.Controllers.Api.Designer;
 
 namespace WB.UI.Designer.Controllers.Api.WebTester
 {
     [Route("api/webtester/Scenarios")]
     [Authorize]
+    [QuestionnairePermissions]
     public class ScenariosController : ControllerBase
     {
+        private readonly DesignerDbContext dbContext;
+
+        public ScenariosController(DesignerDbContext dbContext)
+        {
+            this.dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+        }
 
         [Route("{id:Guid}")]
-        public IActionResult Post(Guid id, [FromForm]PostScenarioModel model)
+        public async Task<IActionResult> Post(Guid id, [FromForm]PostScenarioModel model)
         {
+            var newScenario = new StoredScenario();
+            newScenario.QuestionnaireId = id;
+            newScenario.Steps = model.ScenarioText;
+            newScenario.Title = "";
+
+            await this.dbContext.Scenarios.AddAsync(newScenario);
+            await this.dbContext.SaveChangesAsync();
+
             return Ok();
         }
     }
