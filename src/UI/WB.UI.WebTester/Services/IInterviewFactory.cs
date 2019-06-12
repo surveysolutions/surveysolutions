723ffb4d21ed220e1353f6336ec78f7d7c305175
Andrii Kozhyn <hedgic@gmail.com>
2019-06-12 13:08:14 +0300

    KP-12927 Scenario is replayed on tester

--- src/UI/WB.UI.WebTester/Services/IInterviewFactory.cs
+++ src/UI/WB.UI.WebTester/Services/IInterviewFactory.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using WB.Core.GenericSubdomains.Portable.Services;
 using WB.Core.Infrastructure.CommandBus;
-using WB.Core.SharedKernels.DataCollection;
 using WB.Core.SharedKernels.DataCollection.Commands.Interview;
 using WB.Core.SharedKernels.DataCollection.Commands.Interview.Base;
 using WB.Core.SharedKernels.DataCollection.Implementation.Aggregates.InterviewEntities.Answers;
+using WB.Core.SharedKernels.DataCollection.Implementation.Entities;
 using WB.Core.SharedKernels.DataCollection.Repositories;
+using WB.Core.SharedKernels.DataCollection.Scenarios;
 using WB.Core.SharedKernels.DataCollection.ValueObjects.Interview;
 
 namespace WB.UI.WebTester.Services
@@ -23,6 +26,8 @@ namespace WB.UI.WebTester.Services
         Task CreateInterview(Guid designerToken);
 
         Task<CreationResult> CreateInterview(Guid designerToken, Guid originalInterviewId);
+
+        Task<CreationResult> CreateInterview(Guid designerToken, int scenarioId);
     }
 
     public class InterviewFactory : IInterviewFactory
@@ -32,18 +37,30 @@ namespace WB.UI.WebTester.Services
         private readonly IImageFileStorage imageFileStorage;
         private readonly IEvictionNotifier evictionService;
         private readonly IQuestionnaireImportService questionnaireImportService;
+        private readonly IDesignerWebTesterApi webTesterApi;
+        private readonly IScenarioService scenarioService;
+        private readonly IQuestionnaireStorage questionnaireStorage;
+        private readonly ISerializer serializer;
 
         public InterviewFactory(ICacheStorage<List<ICommand>, Guid> executedCommandsStorage,
             ICommandService commandService,
             IImageFileStorage imageFileStorage,
             IEvictionNotifier evictionService,
-            IQuestionnaireImportService questionnaireImportService)
+            IQuestionnaireImportService questionnaireImportService, 
+            IDesignerWebTesterApi webTesterApi, 
+            IScenarioService scenarioService, 
+            IQuestionnaireStorage questionnaireStorage,
+            ISerializer serializer)
         {
             this.executedCommandsStorage = executedCommandsStorage ?? throw new ArgumentNullException(nameof(executedCommandsStorage));
             this.commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
             this.imageFileStorage = imageFileStorage;
             this.evictionService = evictionService ?? throw new ArgumentNullException(nameof(evictionService));
             this.questionnaireImportService = questionnaireImportService;
+            this.webTesterApi = webTesterApi ?? throw new ArgumentNullException(nameof(webTesterApi));
+            this.scenarioService = scenarioService ?? throw new ArgumentNullException(nameof(scenarioService));
+            this.questionnaireStorage = questionnaireStorage ?? throw new ArgumentNullException(nameof(questionnaireStorage));
+            this.serializer = serializer ?? throw new ArgumentNullException(nameof(serializer));
         }
 
         public async Task CreateInterview(Guid designerToken)
@@ -63,6 +80,48 @@ namespace WB.UI.WebTester.Services
                 isAudioRecordingEnabled:false);
 
             this.commandService.Execute(createInterview);
+        }
+
+        public async Task<CreationResult> CreateInterview(Guid designerToken, int scenarioId)
+        {
+            var questionnaire = await questionnaireImportService.ImportQuestionnaire(designerToken);
+
+            var createInterview = new CreateInterview(
+                interviewId: designerToken,
+                userId: Guid.Parse("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"),
+                questionnaireId: questionnaire,
+                answers: new List<InterviewAnswer>(),
+                protectedVariables: new List<string>(),
+                supervisorId: Guid.NewGuid(),
+                interviewerId: Guid.NewGuid(),
+                interviewKey: new InterviewKey(new Random().Next(99999999)),
+                assignmentId: null,
+                isAudioRecordingEnabled: false);
+
+            this.commandService.Execute(createInterview);
+
+            var scenarioSerialized = await this.webTesterApi.GetScenario(designerToken.ToString(), scenarioId);
+            var scenario = this.serializer.Deserialize<Scenario>(scenarioSerialized);
+
+            var questionnaireDocument = this.questionnaireStorage.GetQuestionnaire(questionnaire, null);
+            var commands = this.scenarioService.ConvertFromScenario(questionnaireDocument, scenario.Steps);
+            try
+            {
+
+                foreach (var existingInterviewCommand in commands)
+                {
+                    existingInterviewCommand.InterviewId = designerToken;
+                    this.commandService.Execute(existingInterviewCommand);
+                }
+
+                return CreationResult.DataRestored;
+            }
+            catch
+            {
+                evictionService.Evict(designerToken);
+                await this.CreateInterview(designerToken);
+                return CreationResult.EmptyCreated;
+            }
         }
 
         public async Task<CreationResult> CreateInterview(Guid designerToken, Guid originalInterviewId)
