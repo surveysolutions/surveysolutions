811e6f0e43e972796fa720e03ee8dc6bb5879cfa
Pavel Stovpivskyi <sby@rambler.ru>
2019-06-13 17:47:23 +0300

    KP-12931 added functional on panel

--- src/UI/WB.UI.Designer/questionnaire/scripts/controllers/scenarios.js
+++ src/UI/WB.UI.Designer/questionnaire/scripts/controllers/scenarios.js
@@ -1,6 +1,6 @@
 ﻿angular.module('designerApp')
     .controller('ScenariosCtrl',
-    function ($rootScope, $scope, $state, $i18next, hotkeys, commandService, utilityService, confirmService) {
+    function ($rootScope, $scope, $state, $i18next, hotkeys, commandService, utilityService, confirmService, webTesterService) {
             'use strict';
 
             var hideScenariosPane = 'ctrl+r';
@@ -17,11 +17,8 @@
             $scope.scenarios = [];
 
             var dataBind = function (scenario, scenarioDto) {
-                scenario.itemId = scenarioDto.itemId;
-                scenario.name = scenarioDto.name;
-                scenario.description = scenarioDto.description;
-                scenario.content = scenarioDto.content;
-                scenario.isDescriptionVisible = !_.isEmpty(scenario.description);
+                scenario.id = scenarioDto.id;
+                scenario.title = scenarioDto.title;
             };
 
             $scope.loadScenarios = function () {
@@ -30,7 +27,7 @@
 
                 _.each($scope.questionnaire.scenarios, function (scenarioDto) {
                     var scenario = { checkpoint: {} };
-                    if (!_.any($scope.scenarios, function (elem) { return elem.itemId === scenarioDto.itemId; })) {
+                    if (!_.any($scope.scenarios, function (elem) { return elem.id === scenarioDto.id; })) {
                         dataBind(scenario, scenarioDto);
                         dataBind(scenario.checkpoint, scenarioDto);
                         $scope.scenarios.push(scenario);
@@ -45,8 +42,18 @@
             };
 
             $scope.runScenario = function (scenario) {
-                //commandService.runScenario();
-                alert("run run run");
+                var webTesterWindow = window.open("about:blank", '_blank');
+
+                webTesterService.run($state.params.questionnaireId).then(function (response) {
+                    webTesterWindow.location.href = response.data + "?scenarioId=" + scenario.id;
+                });
+            };
+
+            $scope.saveScenario = function (scenario) {
+                commandService.updateScenario($state.params.questionnaireId, scenario).then(function () {
+                    dataBind(scenario.checkpoint, scenario);
+                    scenario.form.$setPristine();
+                });
             };
 
             $scope.cancel = function (scenario) {
@@ -56,109 +63,16 @@
 
             $scope.deleteScenario = function (index) {
                 var scenario = $scope.scenarios[index];
-                var scenarioName = scenario.name || $i18next.t("SideBarScenarioNoName");
+                var scenarioName = scenario.title || $i18next.t("SideBarScenarioNoName");
                 var modalInstance = confirmService.open(utilityService.createQuestionForDeleteConfirmationPopup(scenarioName));
 
                 modalInstance.result.then(function (confirmResult) {
                     if (confirmResult === 'ok') {
-                        commandService.deleteScenarios($state.params.questionnaireId, scenario.itemId).then(function () {
+                        commandService.deleteScenario($state.params.questionnaireId, scenario.id).then(function () {
                             $scope.scenarios.splice(index, 1);
                         });
                     }
                 });
-            };
-
-            $scope.isDescriptionEmpty = function (scenario) {
-                return _.isEmpty(scenario.description);
-            };
-            $scope.toggleDescription = function (scenario) {
-                scenario.isDescriptionVisible = !scenario.isDescriptionVisible;
-            };
-
-            $scope.aceLoaded = function (editor) {
-                var expressionEditorPlaceholder = $i18next.t('SideBarScenarioContent');
-
-                // Editor part
-                var renderer = editor.renderer;
-
-                // Options
-                editor.setOptions({
-                    maxLines: Infinity,
-                    mode: "ace/mode/csharp-extended",
-                    fontSize: 16,
-                    highlightActiveLine: false,
-                    theme: "ace/theme/github-extended",
-                    enableBasicAutocompletion: true,
-                    enableLiveAutocompletion: true
-                });
-                editor.$blockScrolling = Infinity;
-
-                $scope.aceEditorUpdateMode(editor);
-
-                $rootScope.$on('variablesChanged', function () {
-                    $scope.aceEditorUpdateMode(editor);
-                });
-
-                renderer.setShowGutter(false);
-                renderer.setPadding(0);
-
-                function update() {
-                    var shouldShow = !editor.session.getValue().length;
-                    var node = editor.renderer.emptyMessageNode;
-                    if (!shouldShow && node) {
-                        editor.renderer.scroller.removeChild(editor.renderer.emptyMessageNode);
-                        editor.renderer.emptyMessageNode = null;
-                    } else if (shouldShow && !node) {
-                        node = editor.renderer.emptyMessageNode = document.createElement("div");
-                        node.textContent = expressionEditorPlaceholder;
-                        node.className = "ace_invisible ace_emptyMessage";
-                        node.style.padding = "0";
-                        editor.renderer.scroller.appendChild(node);
-                    }
-                }
-                editor.on("input", update);
-
-                editor.commands.addCommand({
-                    name: "replace",
-                    bindKey: { win: "Tab|Shift-Tab", mac: "Tab" },
-                    exec: function (editor) {
-                        editor.blur();
-                    }
-                });
-
-                setTimeout(update, 100);
-            };
-
-            $scope.getVariablesNames = function() {
-                return _($rootScope.variableNames);
-            };
-
-            $scope.aceEditorUpdateMode = function (editor) {
-                if (editor) {
-                    var CSharpExtendableMode = window.ace.require("ace/mode/csharp-extended").Mode;
-                    editor.getSession().setMode(new CSharpExtendableMode(function () {
-                        return _.pluck($rootScope.variableNames, "name");
-                    }));
-
-                    var variablesCompletor =
-                    {
-                        getCompletions: function (editor, session, pos, prefix, callback) {
-                            var i = 0;
-                            callback(null,
-                                $scope.getVariablesNames()
-                                .sortBy('name')
-                                .reverse()
-                                .map(function (variable) {
-                                    return { name: variable.name, value: variable.name, score: i++, meta: variable.type }
-                                }));
-                        },
-
-                        identifierRegexps: [/[@a-zA-Z_0-9\$\-\u00A2-\uFFFF]/]
-                    };
-
-                    var lang_tool = ace.require("ace/ext/language_tools");
-                    lang_tool.setCompleters([variablesCompletor]);
-                }
             };
 
             $scope.foldback = function () {
