c119121f907e48a817dd59e03c1191842067c48b
Andrii Kozhyn <hedgic@gmail.com>
2019-06-25 12:00:40 +0300

    KP-12966 store audio audit in s3

---
+++ src/Infrastructure/WB.Persistence.Headquarters/Migrations/PlainStore/M2019/M201906251342_MakeDataForAudioAuditNullable.cs
@@ -0,0 +1,17 @@
+﻿using FluentMigrator;
+
+namespace WB.Persistence.Headquarters.Migrations.PlainStore
+{
+    [Migration(201906251342)]
+    public class M201906251342_MakeDataForAudioAuditNullable : Migration
+    {
+        public override void Up()
+        {
+            Alter.Column("data").OnTable("audioauditfiles").AsBinary().Nullable();
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
