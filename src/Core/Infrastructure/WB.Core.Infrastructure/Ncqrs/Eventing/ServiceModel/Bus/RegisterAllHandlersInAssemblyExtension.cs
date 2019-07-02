71d66ffcd905aadade7316d8876402786e217826
Andrii Kozhyn <andrii.kozhyn@outlook.com>
2019-07-02 13:49:01 +0300

    KP-12640 Removed unused methods

--- src/Core/Infrastructure/WB.Core.Infrastructure/Ncqrs/Eventing/ServiceModel/Bus/RegisterAllHandlersInAssemblyExtension.cs
+++ src/Core/Infrastructure/WB.Core.Infrastructure/Ncqrs/Eventing/ServiceModel/Bus/RegisterAllHandlersInAssemblyExtension.cs
@@ -2,41 +2,14 @@
 using System.Collections.Concurrent;
 using System.Linq;
 using System.Reflection;
-using Ncqrs.Eventing.Storage;
-using WB.Core.GenericSubdomains.Portable.Services;
 
 namespace Ncqrs.Eventing.ServiceModel.Bus
 {
     public static class RegisterAllHandlersInAssemblyExtension
     {
-        public static void RegisterAllHandlersInAssembly(this InProcessEventBus target, Assembly asm)
-        {
-            target.RegisterAllHandlersInAssembly(asm, CreateInstance);
-        }
-
-        public static void RegisterAllHandlersInAssembly(this InProcessEventBus target, Assembly asm, Func<Type, object> handlerFactory)
-        {
-            foreach (var typeInfo in asm.DefinedTypes.Where(ImplementsAtLeastOneIEventHandlerInterface))
-            {
-                var handler = handlerFactory(typeInfo.DeclaringType);
-
-                foreach (var handlerInterfaceType in typeInfo.ImplementedInterfaces.Where(IsIEventHandlerInterface))
-                {
-                    var eventDataType = handlerInterfaceType.GenericTypeArguments.First();
-                    target.RegisterHandler(handler, eventDataType);
-                }
-            }
-        }
-
-        private static object CreateInstance(Type type)
-        {
-            return Activator.CreateInstance(type);
-        }
-
-
         public static void RegisterHandler(this InProcessEventBus target, object handler, Type eventDataType)
         {
-            var targetMethod = _registerHandlerTargetMethodCache.GetOrAdd(eventDataType,
+            var targetMethod = registerHandlerTargetMethodCache.GetOrAdd(eventDataType,
                 type =>
                 {
                     var registerHandlerMethod = target.GetType().GetTypeInfo().GetMethods().Single
@@ -50,20 +23,6 @@ namespace Ncqrs.Eventing.ServiceModel.Bus
             targetMethod.Invoke(target, new[] { handler });
         }
 
-        private static readonly ConcurrentDictionary<Type, MethodInfo> _registerHandlerTargetMethodCache = new ConcurrentDictionary<Type, MethodInfo>();
-
-        private static bool ImplementsAtLeastOneIEventHandlerInterface(TypeInfo typeInfo)
-        {
-            return typeInfo.IsClass && !typeInfo.IsAbstract &&
-                   typeInfo.ImplementedInterfaces.Any(IsIEventHandlerInterface);
-        }
-
-        private static bool IsIEventHandlerInterface(Type type)
-        {
-            var typeInfo = type.GetTypeInfo();
-            return typeInfo.IsInterface &&
-                   typeInfo.IsGenericType &&
-                   typeInfo.GetGenericTypeDefinition() == typeof(IEventHandler<>);
-        }
+        private static readonly ConcurrentDictionary<Type, MethodInfo> registerHandlerTargetMethodCache = new ConcurrentDictionary<Type, MethodInfo>();
     }
 }
