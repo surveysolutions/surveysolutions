b9d737cb4f50421c24bb90a6006c4a1e97e266f2
Victor Hladkikh <me@alfeg.net>
2019-07-02 17:56:45 +0300

    KP-12974 Move HQ Client folder to web configurable setting

--- src/UI/Headquarters/WB.UI.Headquarters/API/DataCollection/Interviewer/v2/InterviewerApiV2Controller.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/API/DataCollection/Interviewer/v2/InterviewerApiV2Controller.cs
@@ -2,75 +2,43 @@
 using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Threading.Tasks;
-using System.Web.Hosting;
 using System.Web.Http;
 using WB.Core.BoundedContexts.Headquarters.Services;
 using WB.Core.BoundedContexts.Headquarters.Views.User;
-using WB.Core.Infrastructure.FileSystem;
-using WB.UI.Headquarters.Resources;
-using WB.UI.Headquarters.Utils;
-using WB.UI.Shared.Web.Extensions;
+using WB.UI.Headquarters.Services;
 
 namespace WB.UI.Headquarters.API.DataCollection.Interviewer.v2
 {
     [Obsolete(@"Since v 5.10")]
     public class InterviewerApiV2Controller : ApiController
     {
-        private const string RESPONSEAPPLICATIONFILENAME = "interviewer.apk";
-        private const string PHYSICALAPPLICATIONFILENAME = "wbcapi.apk";
-        private const string PHYSICALPATHTOAPPLICATION = "~/Client/";
-
-        private readonly IFileSystemAccessor fileSystemAccessor;
         protected readonly ITabletInformationService tabletInformationService;
         protected readonly IUserViewFactory userViewFactory;
-        private readonly IAndroidPackageReader androidPackageReader;
+        private readonly IClientApkProvider clientApkProvider;
 
         public InterviewerApiV2Controller(
-            IFileSystemAccessor fileSystemAccessor,
             ITabletInformationService tabletInformationService,
             IUserViewFactory userViewFactory,
-            IAndroidPackageReader androidPackageReader) 
+            IClientApkProvider clientApkProvider)
         {
-            this.fileSystemAccessor = fileSystemAccessor;
             this.tabletInformationService = tabletInformationService;
             this.userViewFactory = userViewFactory;
-            this.androidPackageReader = androidPackageReader;
+            this.clientApkProvider = clientApkProvider;
         }
 
         [HttpGet]
         public HttpResponseMessage Get()
         {
-            string pathToInterviewerApp = this.fileSystemAccessor.CombinePath(
-                HostingEnvironment.MapPath(PHYSICALPATHTOAPPLICATION), PHYSICALAPPLICATIONFILENAME);
-
-            if (!this.fileSystemAccessor.IsFileExists(pathToInterviewerApp))
-                return this.Request.CreateErrorResponse(HttpStatusCode.NotFound, TabletSyncMessages.FileWasNotFound);
-
-            var fileHash = this.fileSystemAccessor.ReadHash(pathToInterviewerApp);
-
-            if (this.RequestHasMatchingFileHash(fileHash))
-            {
-                return Request.CreateResponse(HttpStatusCode.NotModified);
-            }
-
-            return this.AsProgressiveDownload(this.fileSystemAccessor.ReadFile(pathToInterviewerApp),
-                @"application/vnd.android.package-archive",
-                RESPONSEAPPLICATIONFILENAME, 
-                fileHash);
+            return this.clientApkProvider.GetApkAsHttpResponse(Request,
+                ClientApkInfo.InterviewerFileName,
+                ClientApkInfo.InterviewerResponseFileName);
         }
 
         [HttpGet]
         public virtual int? GetLatestVersion()
         {
-            string pathToInterviewerApp =
-                this.fileSystemAccessor.CombinePath(HostingEnvironment.MapPath(PHYSICALPATHTOAPPLICATION),
-                    PHYSICALAPPLICATIONFILENAME);
-
-            return !this.fileSystemAccessor.IsFileExists(pathToInterviewerApp)
-                ? null
-                : this.androidPackageReader.Read(pathToInterviewerApp).Version;
+            return this.clientApkProvider.GetLatestVersion(ClientApkInfo.InterviewerFileName);
         }
 
         [HttpPost]
