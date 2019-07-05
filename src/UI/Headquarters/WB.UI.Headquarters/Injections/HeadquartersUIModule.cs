64768f19d11ba4479d675691f0d99b4de96170b2
Victor Hladkikh <me@alfeg.net>
2019-07-05 12:37:40 +0300

    HqFarm - more fixes regarding proper Url generation

--- src/UI/Headquarters/WB.UI.Headquarters/Injections/HeadquartersUIModule.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/Injections/HeadquartersUIModule.cs
@@ -4,15 +4,14 @@ using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using ASP;
 using WB.Core.BoundedContexts.Headquarters.AssignmentImport;
 using WB.Core.BoundedContexts.Headquarters.DataExport.DataExportDetails;
-using WB.Core.BoundedContexts.Headquarters.DataExport.Security;
 using WB.Core.BoundedContexts.Headquarters.DataExport.Services;
 using WB.Core.BoundedContexts.Headquarters.DataExport.Views;
 using WB.Core.BoundedContexts.Headquarters.Implementation;
 using WB.Core.GenericSubdomains.Portable.Implementation.Services;
 using WB.Core.GenericSubdomains.Portable.Services;
-using WB.Core.SharedKernels.SurveyManagement.Web.Code;
 using WB.UI.Headquarters.Views;
 using WB.UI.Shared.Web.Filters;
 using WB.Core.BoundedContexts.Headquarters.Implementation.Services;
@@ -33,7 +32,6 @@ using WB.Core.SharedKernels.Questionnaire.Translations;
 using WB.Core.SharedKernels.SurveyManagement.Web.Code.CommandDeserialization;
 using WB.Core.Synchronization.MetaInfo;
 using WB.Enumerator.Native.WebInterview.Services;
-using WB.Infrastructure.Native.Logging;
 using WB.Infrastructure.Native.Logging.Slack;
 using WB.Infrastructure.Native.Questionnaire;
 using WB.UI.Headquarters.API.WebInterview;
@@ -167,7 +165,7 @@ namespace WB.UI.Headquarters.Injections
                 if (appUrl != "/")
                     appUrl = "/" + appUrl;
 
-                var baseUrl = string.Format("{0}://{1}{2}", request.Url.Scheme, request.Url.Authority, appUrl);
+                var baseUrl = string.Format("{0}://{1}{2}", request.UrlScheme(), request.Url.Authority, appUrl);
 
                 return new Uri(baseUrl);
             }
