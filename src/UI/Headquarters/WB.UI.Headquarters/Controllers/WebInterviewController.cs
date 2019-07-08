b0cd248ca163cd16d00275797c568c17366b4ad0
Andrii Kozhyn <hedgic@gmail.com>
2019-07-08 15:43:56 +0300

    KP-12684 ability to configure complete button and note to supervisor fields

--- src/UI/Headquarters/WB.UI.Headquarters/Controllers/WebInterviewController.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/Controllers/WebInterviewController.cs
@@ -153,7 +153,15 @@ namespace WB.UI.Headquarters.Controllers
             }
 
             LogResume(interview);
-            return this.View("Index");
+            return this.View("Index", GetInterviewModel(webInterviewConfig));
+        }
+
+        private WebInterviewIndexPageModel GetInterviewModel(WebInterviewConfig webInterviewConfig)
+        {
+            return new WebInterviewIndexPageModel
+            {
+                CustomMessages = webInterviewConfig.CustomMessages
+            };
         }
 
         public string GenerateUrl(string action, string interviewId, string sectionId = null)
@@ -367,7 +375,7 @@ namespace WB.UI.Headquarters.Controllers
             LogResume(interview);
             ViewBag.AskForEmail = Request.Cookies.Get(AskForEmail)?.Value ?? "false";
 
-            return View("Index");
+            return View("Index", GetInterviewModel(webInterviewConfig));
         }
 
         private void LogResume(IStatefulInterview statefulInterview)
@@ -470,7 +478,7 @@ namespace WB.UI.Headquarters.Controllers
                 return this.RedirectToAction("Resume", routeValues: new { id, returnUrl });
             }
 
-            return View("Index");
+            return View("Index", GetInterviewModel(webInterviewConfig));
         }
 
         [HttpPost]
