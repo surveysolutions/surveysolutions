78d922fcf5c5b156c71f9b91dc708436798d3448
Andrii Kozhyn <hedgic@gmail.com>
2019-07-08 15:43:56 +0300

    KP-12684 ability to configure complete button and note to supervisor fields

---
+++ src/UI/Headquarters/WB.UI.Headquarters/Models/WebInterview/WebInterviewIndexPageModel.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using WB.Core.BoundedContexts.Headquarters.WebInterview;
+
+namespace WB.UI.Headquarters.Models.WebInterview
+{
+    public class WebInterviewIndexPageModel
+    {
+        public Dictionary<WebInterviewUserMessages, string> CustomMessages { get; set; }
+    }
+}
