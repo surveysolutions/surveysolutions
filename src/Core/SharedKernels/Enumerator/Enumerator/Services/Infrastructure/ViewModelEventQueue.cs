c7385175efcbd14b110c30dbda5bd7a783794414
Roman Lytvyn <litvin.roma@gmail.com>
2019-07-12 18:48:58 +0300

    KP-12978 [code review] Implement async version of ILiteEventHandler.Handle. Fixed some tests

--- src/Core/SharedKernels/Enumerator/Enumerator/Services/Infrastructure/ViewModelEventQueue.cs
+++ src/Core/SharedKernels/Enumerator/Enumerator/Services/Infrastructure/ViewModelEventQueue.cs
@@ -4,7 +4,7 @@ using WB.Core.Infrastructure.Services;
 
 namespace WB.Core.SharedKernels.Enumerator.Services.Infrastructure
 {
-    internal class ViewModelEventQueue : BackgroundService<CommittedEvent>
+    public class ViewModelEventQueue : BackgroundService<CommittedEvent>
     {
         public ViewModelEventQueue(IBackgroundJob<CommittedEvent> job) : base(job)
         {
