bdc9487b072d4daa2b2632a048b08944690a4376
Andrii Kozhyn <andrii.kozhyn@outlook.com>
2019-07-16 16:39:27 +0300

    KP-12990 do not allow old tablet devices receive resolved comments

--- src/Core/SharedKernels/DataCollection/DataCollection.Portable/SyncProtocolVersionProvider.cs
+++ src/Core/SharedKernels/DataCollection/DataCollection.Portable/SyncProtocolVersionProvider.cs
@@ -4,16 +4,19 @@
     {
         public static readonly int ProtectedVariablesIntroduced = 7070;
         public static readonly int AudioRecordingIntroduced = 7090;
+        public static readonly int ResolvedCommentsIntroduced = 7100;
 
         //previous values: 5962, 7018, 7034, 7050, 7060, 7070 - where KP-11462 was introduced 
-        public int GetProtocolVersion() => 7090;
+        public int GetProtocolVersion() => 7100;
 
         public int GetLastNonUpdatableVersion() => 7000;
     }
 
     public class SupervisorSyncProtocolVersionProvider : ISupervisorSyncProtocolVersionProvider
     {
-        public int GetProtocolVersion() => 1000;
+        public static readonly int ResolvedCommentsIntroduced = 1000;
+
+        public int GetProtocolVersion() => 1010;
 
         public int GetLastNonUpdatableVersion() => 999;
     }
