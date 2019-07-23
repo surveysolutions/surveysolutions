693ff406fc11ddbb22eb153db20c9eb4b25cc8c6
Roman Lytvyn <litvin.roma@gmail.com>
2019-07-23 14:14:53 +0300

    KP-12978 [code review] Implement async version of ILiteEventHandler.Handle

--- src/Core/Infrastructure/WB.Core.Infrastructure/Implementation/Services/AsyncEventQueue.cs
+++ src/Core/Infrastructure/WB.Core.Infrastructure/Implementation/Services/AsyncEventQueue.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Threading.Tasks.Dataflow;
 using Ncqrs.Eventing;
 using WB.Core.GenericSubdomains.Portable.Services;
@@ -36,11 +35,5 @@ namespace WB.Core.Infrastructure.Implementation.Services
         }
 
         public void Enqueue(IReadOnlyCollection<CommittedEvent> item) => this.queue.Post(item);
-
-        public Task StopAsync()
-        {
-            queue.Complete();
-            return queue.Completion;
-        }
     }
 }
