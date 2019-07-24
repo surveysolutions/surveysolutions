6aa80c1689e16caef5ca4ba4ec3b5b02d25a9412
Roman Lytvyn <litvin.roma@gmail.com>
2019-07-24 16:04:47 +0300

    KP-13016 Get rid of Esri on HQ. Added migration

---
+++ src/Infrastructure/WB.Persistence.Headquarters/Migrations/PlainStore/M2019/M201907241548_RemoveUnusedMapsColumns.cs
@@ -0,0 +1,23 @@
+﻿using FluentMigrator;
+
+namespace WB.Persistence.Headquarters.Migrations.PlainStore
+{
+    [Migration(201907241548)]
+    public class M201907241548_RemoveUnusedMapsColumns : Migration
+    {
+        public override void Up()
+        {
+            Delete.Column("wkid").FromTable("mapbrowseitems");
+            Delete.Column("xmaxval").FromTable("mapbrowseitems");
+            Delete.Column("xminval").FromTable("mapbrowseitems");
+            Delete.Column("ymaxval").FromTable("mapbrowseitems");
+            Delete.Column("yminval").FromTable("mapbrowseitems");
+            Delete.Column("maxscale").FromTable("mapbrowseitems");
+            Delete.Column("minscale").FromTable("mapbrowseitems");
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
