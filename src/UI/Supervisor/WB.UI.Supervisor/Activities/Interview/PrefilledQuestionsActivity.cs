48666877fd86ce05f367045b0dc476f723f77390
Victor Hladkikh <me@alfeg.net>
2019-08-09 13:11:31 +0300

    Added StartInterviewApi to Supervisor and Tester

--- src/UI/Supervisor/WB.UI.Supervisor/Activities/Interview/PrefilledQuestionsActivity.cs
+++ src/UI/Supervisor/WB.UI.Supervisor/Activities/Interview/PrefilledQuestionsActivity.cs
@@ -1,5 +1,6 @@
 using Android.App;
 using Android.Views;
+using Java.Interop;
 using WB.Core.BoundedContexts.Supervisor.ViewModel;
 using WB.Core.SharedKernels.Enumerator.Properties;
 using WB.UI.Shared.Enumerator.Activities;
@@ -49,5 +50,11 @@ namespace WB.UI.Supervisor.Activities.Interview
                 EnumeratorUIResources.MenuItem_Title_Language_Original
             },
         };
+        
+        [Export("StartInterviewApi")]
+        public void StartInterviewApi()
+        {
+            this.ViewModel.StartInterviewAsync().Wait();
+        }
     }
 }
