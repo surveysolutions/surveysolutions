c69919a52828604ee9e621cf0b5c54b96eb7076d
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2019-09-06 18:26:40 +0300

    KP-13157 Variable calculation is untimely or incorrect

--- src/Core/SharedKernels/DataCollection/DataCollection.Portable/V4/CustomFunctions/ExtentionsV4.cs
+++ src/Core/SharedKernels/DataCollection/DataCollection.Portable/V4/CustomFunctions/ExtentionsV4.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 
 namespace WB.Core.SharedKernels.DataCollection.V4.CustomFunctions
 {
@@ -7,8 +6,45 @@ namespace WB.Core.SharedKernels.DataCollection.V4.CustomFunctions
     {
         public static double GetRandomDouble(this Guid id)
         {
-            Random r = new Random(id.GetHashCode());
+            var hash = GenerateHash(id);
+            Random r = new Random(hash);
             return r.NextDouble();
         }
+
+        public static int GenerateHash(Guid id)
+        {
+            //hash 
+            //return this._a ^ (this._b << 16 | (ushort)this._c) ^ (this._f << 24 | this._k);
+
+            //return this._a ^ *Unsafe.Add<int>(ref this._a, 1) ^ *Unsafe.Add<int>(ref this._a, 2) ^ *Unsafe.Add<int>(ref this._a, 3);
+
+            var b = id.ToByteArray();
+
+            int _a = (int)b[3] << 24 | (int)b[2] << 16 | (int)b[1] << 8 | (int)b[0];
+            short _b = (short)((int)b[5] << 8 | (int)b[4]);
+            short _c = (short)((int)b[7] << 8 | (int)b[6]);
+            //var _d = b[8];
+            //var _e = b[9];
+            var _f = b[10];
+            //var _g = b[11];
+            //var _h = b[12];
+            //var _i = b[13];
+            //var _j = b[14];
+            var _k = b[15];
+
+
+            int result = _a ^ (_b | _c) ^ (_f | _k);
+
+            //int result = _a ^ *Unsafe.Add<int>(ref _a, 1) ^ *Unsafe.Add<int>(ref _a, 2) ^ *Unsafe.Add<int>(ref _a, 3);
+            return result;
+        }
+
+        /*internal static class Unsafe
+        {
+            public unsafe static T* Add<T>(ref T source, int elementOffset)
+            {
+                return ref source + (IntPtr) elementOffset * (IntPtr) sizeof(T);
+            }
+        }*/
     }
 }
