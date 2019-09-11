d2afef050509920bb5f2b25ed4ad27e0fba7d49a
Pavel Stovpivskyi <sby@rambler.ru>
2019-09-11 13:11:43 +0300

    KP-13139 assignments data migration

--- src/Infrastructure/WB.Persistence.Headquarters/Migrations/ReadSide/M2019/M201909091502_MigrateAssignments.sql
+++ src/Infrastructure/WB.Persistence.Headquarters/Migrations/ReadSide/M2019/M201909091502_MigrateAssignments.sql
@@ -1,26 +1,59 @@
 ﻿INSERT INTO readside.assignments (publickey, id, responsibleid, quantity, archived, createdatutc, updatedatutc, questionnaireid, questionnaireversion, answers, questionnaire, protectedvariables, receivedbytabletatutc, isaudiorecordingenabled, email, password, webmode)
-SELECT NEWID(), id, responsibleid, quantity, archived, createdatutc, updatedatutc, questionnaireid, questionnaireversion, answers, questionnaire, protectedvariables, receivedbytabletatutc, isaudiorecordingenabled, email, password, webmode
-	FROM plainstore.assignments;
+SELECT 
+    uuid_in(md5(random()::text || clock_timestamp()::text)::cstring),
+	id,
+	responsibleid, 
+	quantity, 
+	archived, 
+	createdatutc, 
+	updatedatutc, 
+	questionnaireid, 
+	questionnaireversion, 
+	answers, 
+	questionnaire, 
+	protectedvariables, 
+	receivedbytabletatutc, 
+	isaudiorecordingenabled, 
+	email, 
+	password, 
+	webmode
+FROM plainstore.assignments;
 	
 INSERT INTO readside.assignmentsidentifyinganswers(assignmentid, "position", questionid, answer, answerasstring, rostervector)
-SELECT assignmentid, "position", questionid, answer, answerasstring, rostervector
-	FROM plainstore.assignmentsidentifyinganswers;
+SELECT 
+	(SELECT ass.publickey FROM readside.assignments ass WHERE ass.id = ans.assignmentid), --assignmentid, 
+	ans."position", 
+	ans.questionid, 
+	ans.answer, 
+	ans.answerasstring, 
+	ans.rostervector
+FROM plainstore.assignmentsidentifyinganswers ans;
 	
-INSERT INTO events.events (id, origin, "timestamp", eventsourceid, value, eventsequence, eventtype)
-SELECT NEWID(), -- id
-	null,       -- origin
-	createdatutc, -- "timestamp"
-	publickey,  -- eventsourceid
-	'{
-		"id": ' + id + ',
-		"userId": "00000000-0000-0000-0000-000000000001", -- admin id for first user on site
-		"originDate": "' + createdatutc + '",
-		"assignmentId": ' + id + ',
-		"creationTime": "' + createdatutc + '",
-		"questionnaireId": "' + questionnaireid + '",
-		"questionnaireVersion": '+ questionnaireversion +',
-		"isAudioRecordingEnabled": ' + isaudiorecordingenabled + '
-	}',         -- value
-	1,          -- eventsequence
-	'AssignmentCreated' --eventtype
+INSERT INTO events.events (id, origin, "timestamp", eventsourceid, eventsequence, eventtype, value)
+SELECT 
+	uuid_in(md5(random()::text || clock_timestamp()::text)::cstring),      -- id
+	null,                -- origin
+	createdatutc,        -- "timestamp"
+	publickey,           -- eventsourceid
+	1,                   -- eventsequence
+	'AssignmentCreated', -- eventtype
+	concat('{ 
+		   "id": ', id, ', 
+		   "userId": "00000000-0000-0000-0000-000000000001", 
+		   "answers": ', answers::text, ', 
+		   "webMode": ', webmode::text, ',
+		   "quantity": ', quantity, ',
+		   "originDate": "', createdatutc, '",
+		   "responsibleId": "', responsibleId, '",
+		   "protectedVariables": ', protectedVariables::text, ',
+		   "questionnaireId": "', questionnaireid, '",
+		   "questionnaireVersion": ', questionnaireversion, ',
+		   "isAudioRecordingEnabled": ', isaudiorecordingenabled::text, '
+	}')::jsonb          -- value
 FROM readside.assignments;
+
+
+
+
+
+
