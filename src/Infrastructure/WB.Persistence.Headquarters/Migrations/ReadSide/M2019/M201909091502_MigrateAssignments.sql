f7eae7720426603df7901ccb386ef0afedaa177e
Pavel Stovpivskyi <sby@rambler.ru>
2019-09-16 11:15:01 +0300

    KP-13139 update migration

--- src/Infrastructure/WB.Persistence.Headquarters/Migrations/ReadSide/M2019/M201909091502_MigrateAssignments.sql
+++ src/Infrastructure/WB.Persistence.Headquarters/Migrations/ReadSide/M2019/M201909091502_MigrateAssignments.sql
@@ -40,12 +40,12 @@ SELECT
 	concat('{ 
 		   "id": ', id, ', 
 		   "userId": "00000000-0000-0000-0000-000000000001", 
-		   "answers": ', answers::text, ', 
+		   "answers": ', coalesce(answers::text, '[]'), ', 
 		   "webMode": ', webmode::text, ',
 		   "quantity": ', quantity, ',
 		   "originDate": "', createdatutc, '",
 		   "responsibleId": "', responsibleId, '",
-		   "protectedVariables": ', protectedVariables::text, ',
+		   "protectedVariables": ', coalesce(protectedVariables::text, '[]'), ',
 		   "questionnaireId": "', questionnaireid, '",
 		   "questionnaireVersion": ', questionnaireversion, ',
 		   "audioRecording": ', isaudiorecordingenabled::text, '
@@ -53,7 +53,32 @@ SELECT
 FROM readside.assignments;
 
 
+INSERT INTO events.events (id, origin, "timestamp", eventsourceid, eventsequence, eventtype, value)
+SELECT 
+	uuid_in(md5(random()::text || clock_timestamp()::text)::cstring),      -- id
+	null,                -- origin
+	updatedatutc,        -- "timestamp"
+	publickey,           -- eventsourceid
+	2,                   -- eventsequence
+	'AssignmentArchived', -- eventtype
+	concat('{ 
+		   "id": ', id, ', 
+		   "userId": "00000000-0000-0000-0000-000000000001" 
+	}')::jsonb          -- value
+FROM readside.assignments;
 
 
-
+INSERT INTO events.events (id, origin, "timestamp", eventsourceid, eventsequence, eventtype, value)
+SELECT 
+	uuid_in(md5(random()::text || clock_timestamp()::text)::cstring),      -- id
+	null,                -- origin
+	updatedatutc,        -- "timestamp"
+	publickey,           -- eventsourceid
+	(SELECT MAX(eventsequence) + 1 FROM events.events es WHERE es.eventsourceid = publickey),             -- eventsequence
+	'AssignmentReceivedByTablet', -- eventtype
+	concat('{ 
+		   "id": ', id, ', 
+		   "userId": "00000000-0000-0000-0000-000000000001"
+	}')::jsonb          -- value
+FROM readside.assignments;
 
