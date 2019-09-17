888fd3b1e97e82854068ded957304771c58563d1
Andrii Kozhyn <andrii.kozhyn@outlook.com>
2019-09-17 13:37:52 +0300

    Revert "Removed unused types"
    This reverts commit 0e09e3b9129bd04d441bb75d08e3e676ca2fa271.

---
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Services/ICsvWriterService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace WB.Core.BoundedContexts.Headquarters.Services
+{
+    public interface ICsvWriterService:IDisposable
+    {
+        void WriteField<T>(T cellValue);
+        void NextRecord();
+    }
+}
