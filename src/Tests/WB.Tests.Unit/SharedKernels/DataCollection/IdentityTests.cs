1bee051859b68420b1e0053fdec51ab729b8ee5f
Andrii Kozhyn <hedgic@gmail.com>
2019-10-02 11:18:32 +0300

    KP-13219 unit tests project compiles

--- src/Tests/WB.Tests.Unit/SharedKernels/DataCollection/IdentityTests.cs
+++ src/Tests/WB.Tests.Unit/SharedKernels/DataCollection/IdentityTests.cs
@@ -11,18 +11,20 @@ namespace WB.Tests.Unit.SharedKernels.DataCollection
         [Test]
         public void Two_identities_with_different_groupId_should_have_different_HashCode()
         {
-            Identity identity1 = Create.Entity.Identity(Guid.Parse("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"));
-            Identity identity2 = Create.Entity.Identity(Guid.Parse("BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB"));
+            Identity identity1 = Create.Identity(Id.gA);
+            Identity identity2 = Create.Identity(Id.gB);
 
-            Assert.That(identity1.Equals(identity2), Is.False);
-            Assert.That(identity1.GetHashCode(), Is.Not.EqualTo(identity2.GetHashCode()));
+            Assert.That(identity1, Is.Not.EqualTo(identity2));
+            var hash1 = identity1.GetHashCode();
+            var hash2 = identity2.GetHashCode();
+            Assert.That(hash1, Is.Not.EqualTo(hash2));
         }
 
         [Test]
         public void Two_identities_with_different_roster_vectors_have_different_HashCode()
         {
-            Identity identity1 = Create.Entity.Identity(Guid.Parse("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"), Create.Entity.RosterVector(1));
-            Identity identity2 = Create.Entity.Identity(Guid.Parse("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"), Create.Entity.RosterVector(2));
+            Identity identity1 = Create.Entity.Identity(Id.gA, Create.Entity.RosterVector(1));
+            Identity identity2 = Create.Entity.Identity(Id.gA, Create.Entity.RosterVector(2));
 
             Assert.That(identity1.Equals(identity2), Is.False);
             Assert.That(identity1.GetHashCode(), Is.Not.EqualTo(identity2.GetHashCode()));
@@ -31,8 +33,8 @@ namespace WB.Tests.Unit.SharedKernels.DataCollection
         [Test]
         public void Two_identities_with_should_be_equal()
         {
-            Identity identity1 = Create.Entity.Identity(Guid.Parse("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"), Create.Entity.RosterVector(1));
-            Identity identity2 = Create.Entity.Identity(Guid.Parse("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"), Create.Entity.RosterVector(1));
+            Identity identity1 = Create.Entity.Identity(Id.gA, Create.Entity.RosterVector(1));
+            Identity identity2 = Create.Entity.Identity(Id.gA, Create.Entity.RosterVector(1));
 
             Assert.That(identity1.Equals(identity2), Is.True);
             Assert.That(identity1.GetHashCode(), Is.EqualTo(identity2.GetHashCode()));
