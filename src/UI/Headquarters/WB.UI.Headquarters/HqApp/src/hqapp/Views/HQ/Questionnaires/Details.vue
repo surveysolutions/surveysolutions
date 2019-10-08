741f902b7ffcdb820b02b6ae29bc49671491dc69
Andrii Kozhyn <hedgic@gmail.com>
2019-10-08 16:37:21 +0300

    KP-13199 use hqlayout

--- src/UI/Headquarters/WB.UI.Headquarters/HqApp/src/hqapp/Views/HQ/Questionnaires/Details.vue
+++ src/UI/Headquarters/WB.UI.Headquarters/HqApp/src/hqapp/Views/HQ/Questionnaires/Details.vue
@@ -1,82 +1,78 @@
 <template>
-    <main>
-        <div class="container-fluid">
-            <div class="row">
-                <div class="page-header">
-                    <ol class="breadcrumb">
-                        <li>
-                            <a href="../../SurveySetup">{{$t('MainMenu.SurveySetup')}}</a>
-                        </li>
-                    </ol>
-                    <h1>{{$t('Pages.Questionnaire_Info')}}</h1>
-                </div>
-            </div>
+    <HqLayout :hasFilter="false" >
+        <div slot="headers">
+            <ol class="breadcrumb">
+                <li>
+                    <a href="../../SurveySetup">{{$t('MainMenu.SurveySetup')}}</a>
+                </li>
+            </ol>
+            <h1>{{$t('Pages.Questionnaire_Info')}}</h1>
+        </div>
 
-            <div class="row">
-                <div class="col-sm-6">
-                    <table class="table table-striped table-bordered">
-                        <tbody>
-                            <tr>
-                                <td>{{$t('Dashboard.Questionnaire')}}</td>
-                                <td>{{model.title}}</td>
-                            </tr>
-                            <tr>
-                                <td>{{$t('Dashboard.Version')}}</td>
-                                <td>{{model.version}}</td>
-                            </tr>
-                            <tr>
-                                <td>{{$t('Dashboard.ImportDate')}}</td>
-                                <td>{{formatUtcDate(model.importDateUtc)}}</td>
-                            </tr>
-                            <tr>
-                                <td>{{$t('Dashboard.LastEntryDate')}}</td>
-                                <td>{{formatUtcDate(model.lastEntryDateUtc)}}</td>
-                            </tr>
-                            <tr>
-                                <td>{{$t('Dashboard.CreationDate')}}</td>
-                                <td>{{formatUtcDate(model.creationDateUtc)}}</td>
-                            </tr>
-                            <tr>
-                                <td>{{$t('Dashboard.WebMode')}}</td>
-                                <td>{{model.webMode ? $t('Common.Yes') : $t('Common.No')}}</td>
-                            </tr>
-                            <tr>
-                                <td>{{$t('Dashboard.RecordAudio')}}</td>
-                                <td>{{model.audioAudit ? $t('Common.Yes') : $t('Common.No')}}</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-                <div class="col-sm-6">
-                    <h3>{{$t('Pages.Questionnaire_Stats')}}</h3>
-                    <table class="table table-striped table-bordered">
-                        <tbody>
-                            <tr>
-                                <td>{{$t('Pages.Questionnaire_SectionsCount')}}</td>
-                                <td>{{model.sectionsCount}}</td>
-                            </tr>
-                            <tr>
-                                <td>{{$t('Pages.Questionnaire_SubSectionsCount')}}</td>
-                                <td>{{model.subSectionsCount}}</td>
-                            </tr>
-                            <tr>
-                                <td>{{$t('Pages.Questionnaire_RostersCount')}}</td>
-                                <td>{{model.rostersCount}}</td>
-                            </tr>
-                            <tr>
-                                <td>{{$t('Pages.Questionnaire_QuestionsCount')}}</td>
-                                <td>{{model.questionsCount}}</td>
-                            </tr>
-                            <tr>
-                                <td>{{$t('Pages.Questionnaire_QuestionsCountWithCond')}}</td>
-                                <td>{{model.questionsWithConditionsCount}}</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
+        <div class="row">
+            <div class="col-sm-6">
+                <table class="table table-striped table-bordered">
+                    <tbody>
+                        <tr>
+                            <td>{{$t('Dashboard.Questionnaire')}}</td>
+                            <td>{{model.title}}</td>
+                        </tr>
+                        <tr>
+                            <td>{{$t('Dashboard.Version')}}</td>
+                            <td>{{model.version}}</td>
+                        </tr>
+                        <tr>
+                            <td>{{$t('Dashboard.ImportDate')}}</td>
+                            <td>{{formatUtcDate(model.importDateUtc)}}</td>
+                        </tr>
+                        <tr>
+                            <td>{{$t('Dashboard.LastEntryDate')}}</td>
+                            <td>{{formatUtcDate(model.lastEntryDateUtc)}}</td>
+                        </tr>
+                        <tr>
+                            <td>{{$t('Dashboard.CreationDate')}}</td>
+                            <td>{{formatUtcDate(model.creationDateUtc)}}</td>
+                        </tr>
+                        <tr>
+                            <td>{{$t('Dashboard.WebMode')}}</td>
+                            <td>{{model.webMode ? $t('Common.Yes') : $t('Common.No')}}</td>
+                        </tr>
+                        <tr>
+                            <td>{{$t('Dashboard.RecordAudio')}}</td>
+                            <td>{{model.audioAudit ? $t('Common.Yes') : $t('Common.No')}}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="col-sm-6">
+                <h3>{{$t('Pages.Questionnaire_Stats')}}</h3>
+                <table class="table table-striped table-bordered">
+                    <tbody>
+                        <tr>
+                            <td>{{$t('Pages.Questionnaire_SectionsCount')}}</td>
+                            <td>{{model.sectionsCount}}</td>
+                        </tr>
+                        <tr>
+                            <td>{{$t('Pages.Questionnaire_SubSectionsCount')}}</td>
+                            <td>{{model.subSectionsCount}}</td>
+                        </tr>
+                        <tr>
+                            <td>{{$t('Pages.Questionnaire_RostersCount')}}</td>
+                            <td>{{model.rostersCount}}</td>
+                        </tr>
+                        <tr>
+                            <td>{{$t('Pages.Questionnaire_QuestionsCount')}}</td>
+                            <td>{{model.questionsCount}}</td>
+                        </tr>
+                        <tr>
+                            <td>{{$t('Pages.Questionnaire_QuestionsCountWithCond')}}</td>
+                            <td>{{model.questionsWithConditionsCount}}</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
-    </main>
+    </HqLayout>
 </template>
 
 <script>
