890578444e8bbbcd78295f5e9b941a81e4ed4a9f
Andrii Kozhyn <hedgic@gmail.com>
2019-10-08 12:55:32 +0300

    KP-13199 page with questionnaire details added

---
+++ src/UI/Headquarters/WB.UI.Headquarters/Models/QuestionnaireDetailsModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using WB.Core.SharedKernels.DataCollection.Aggregates;
+
+namespace WB.UI.Headquarters.Models
+{
+    public class QuestionnaireDetailsModel
+    {
+        public string Title { get; set; }
+        public long Version { get; set; }
+        public DateTime ImportDateUtc { get; set; }
+        public DateTime LastEntryDateUtc { get; set; }
+        public DateTime CreationDateUtc { get; set; }
+        public bool AudioAudit { get; set; }
+        public bool WebMode { get; set; }
+        public int SectionsCount { get; set; }
+        public int SubSectionsCount { get; set; }
+        public int RostersCount { get; set; }
+        public int QuestionsCount { get; set; }
+        public int QuestionsWithConditionsCount { get; set; }
+    }
+}
