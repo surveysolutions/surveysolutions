3767b4b5f868d798a609df721c2fe17b4848a10a
Pavel Stovpivskyi <sby@rambler.ru>
2019-10-09 11:30:32 +0300

    KP-13226 moved methods to InterviewDataController

--- src/Core/SharedKernels/Enumerator/WB.Enumerator.Native/WebInterview/IWebInterviewNotificationService.cs
+++ src/Core/SharedKernels/Enumerator/WB.Enumerator.Native/WebInterview/IWebInterviewNotificationService.cs
@@ -9,6 +9,7 @@ namespace WB.Enumerator.Native.WebInterview
         void RefreshEntities(Guid interviewId, params Identity[] questions);
         void ReloadInterview(Guid interviewId);
         void MarkAnswerAsNotSaved(string interviewId, string questionId, string errorMessage);
+        void MarkAnswerAsNotSaved(Guid interviewId, string questionId, Exception exception);
         void RefreshRemovedEntities(Guid interviewId, params Identity[] entities);
         void FinishInterview(Guid interviewId);
         void RefreshLinkedToRosterQuestions(Guid interviewId, Identity[] rosterIdentities);
