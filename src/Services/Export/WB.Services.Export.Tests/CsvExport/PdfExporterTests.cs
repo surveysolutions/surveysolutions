08963ef8d55652e2679e8a04876335c9cb46905f
Andrii Kozhyn <andrii.kozhyn@outlook.com>
2019-10-10 16:16:38 +0300

    KP-13202 added tests for pdf export

---
+++ src/Services/Export/WB.Services.Export.Tests/CsvExport/PdfExporterTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Dropbox.Api.TeamLog;
+using Moq;
+using NUnit.Framework;
+using WB.Services.Export.CsvExport.Exporters;
+using WB.Services.Export.Infrastructure;
+using WB.Services.Export.Questionnaire;
+using WB.Services.Export.Services;
+
+namespace WB.Services.Export.Tests.CsvExport
+{
+    [TestOf(typeof(PdfExporter))]
+    public class PdfExporterTests
+    {
+        [Test]
+        public async Task should_export_pdf_file()
+        {
+            var questionnaire = Create.QuestionnaireDocument();
+            questionnaire.VariableName = "qVariable";
+            var translationId = Id.g1;
+            questionnaire.Translations.Add(new Translation
+            {
+                Id = translationId,
+                Name = "language name"
+            });
+
+            var hqApi = new Mock<IHeadquartersApi>();
+            var mainPdfFileFromHq = new byte[]{1,2,3};
+            var translatedPdfFileFromHq = new byte[]{4,5,6};
+            hqApi.Setup(x => x.GetPdfAsync(questionnaire.QuestionnaireId, null))
+                .ReturnsAsync(new ByteArrayContent(mainPdfFileFromHq));
+            hqApi.Setup(x => x.GetPdfAsync(questionnaire.QuestionnaireId, translationId))
+                .ReturnsAsync(new ByteArrayContent(translatedPdfFileFromHq));
+
+            var fileSystem = new Mock<IFileSystemAccessor>();
+            var mockMainStream = new MemoryStream();
+            var mockTranslatedStream = new MemoryStream();
+            fileSystem.Setup(x => x.OpenOrCreateFile($"testPath\\Pdf\\{questionnaire.VariableName}.pdf", false))
+                .Returns(mockMainStream);
+            fileSystem.Setup(x => x.OpenOrCreateFile($"testPath\\Pdf\\language name {questionnaire.VariableName}.pdf", false))
+                .Returns(mockTranslatedStream);
+
+            var service = Create.PdfExporter(Create.TenantHeadquartersApi(hqApi.Object), 
+                fileSystem.Object);
+
+            // Act
+            await service.ExportAsync(Create.Tenant(), questionnaire, "testPath");
+
+            // Assert
+            Assert.That(mockMainStream.ToArray(), Is.EquivalentTo(mainPdfFileFromHq), "Main pdf file should be written to export folder");
+            Assert.That(mockTranslatedStream.ToArray(), Is.EquivalentTo(translatedPdfFileFromHq), "Translated pdf file should be written to export folder");
+        }
+    }
+}
--- src/Services/Export/WB.Services.Export/CsvExport/Exporters/PdfExporter.cs
+++ src/Services/Export/WB.Services.Export/CsvExport/Exporters/PdfExporter.cs
@@ -20,12 +20,15 @@ namespace WB.Services.Export.CsvExport.Exporters
     public class PdfExporter : IPdfExporter
     {
         private readonly ITenantApi<IHeadquartersApi> tenantApi;
+        private readonly IFileSystemAccessor fileSystemAccessor;
         private readonly ILogger<PdfExporter> logger;
 
         public PdfExporter(ITenantApi<IHeadquartersApi> tenantApi,
+            IFileSystemAccessor fileSystemAccessor,
             ILogger<PdfExporter> logger)
         {
             this.tenantApi = tenantApi ?? throw new ArgumentNullException(nameof(tenantApi));
+            this.fileSystemAccessor = fileSystemAccessor ?? throw new ArgumentNullException(nameof(fileSystemAccessor));
             this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
@@ -44,7 +47,7 @@ namespace WB.Services.Export.CsvExport.Exporters
                 var mainPdf = await hqApi.GetPdfAsync(questionnaire.QuestionnaireId, null);
                 var mainFilePath = Path.Combine(targetFolder, targetFileName);
                 Directory.CreateDirectory(targetFolder);
-                using (var mainStream = File.OpenWrite(mainFilePath))
+                using (var mainStream = this.fileSystemAccessor.OpenOrCreateFile(mainFilePath, false))
                 {
                     await mainPdf.CopyToAsync(mainStream);
                 }
@@ -71,7 +74,7 @@ namespace WB.Services.Export.CsvExport.Exporters
                     var translatedPdf = await hqApi.GetPdfAsync(questionnaire.QuestionnaireId, translation.Id);
                     var translatedFilePath = Path.Combine(targetFolder, translation.Name + " " + targetFileName);
 
-                    using (var translatedStream = File.OpenWrite(translatedFilePath))
+                    using (var translatedStream = this.fileSystemAccessor.OpenOrCreateFile(translatedFilePath, false))
                     {
                         await translatedPdf.CopyToAsync(translatedStream);
                     }
