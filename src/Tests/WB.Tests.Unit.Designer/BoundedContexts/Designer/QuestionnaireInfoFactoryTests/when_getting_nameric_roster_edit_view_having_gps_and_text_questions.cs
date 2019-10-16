da5a4c6ecca0259c9fe02221522a43380e27f162
Victor Hladkikh <me@alfeg.net>
2019-10-16 21:18:22 +0300

    KP-13204 Unit tests fixed

--- src/Tests/WB.Tests.Unit.Designer/BoundedContexts/Designer/QuestionnaireInfoFactoryTests/when_getting_nameric_roster_edit_view_having_gps_and_text_questions.cs
+++ src/Tests/WB.Tests.Unit.Designer/BoundedContexts/Designer/QuestionnaireInfoFactoryTests/when_getting_nameric_roster_edit_view_having_gps_and_text_questions.cs
@@ -4,6 +4,7 @@ using Main.Core.Documents;
 using Main.Core.Entities.Composite;
 using Main.Core.Entities.SubEntities;
 using Moq;
+using WB.Core.BoundedContexts.Designer.Services;
 using WB.Core.BoundedContexts.Designer.Views.Questionnaire.Edit;
 using WB.Core.Infrastructure.PlainStorage;
 
@@ -13,7 +14,7 @@ namespace WB.Tests.Unit.Designer.BoundedContexts.Designer.QuestionnaireInfoFacto
     internal class when_getting_nameric_roster_edit_view_having_gps_and_text_questions : QuestionnaireInfoFactoryTestContext
     {
         [NUnit.Framework.OneTimeSetUp] public void context () {
-            questionDetailsReaderMock = new Mock<IPlainKeyValueStorage<QuestionnaireDocument>>();
+            questionDetailsReaderMock = new Mock<IDesignerQuestionnaireStorage>();
             questionnaireView = Create.QuestionnaireDocumentWithOneChapter(children: new List<IComposite>
             {
                 Create.NumericIntegerQuestion(q1Id, variable:"num_question", title: "num_question"),
@@ -26,7 +27,7 @@ namespace WB.Tests.Unit.Designer.BoundedContexts.Designer.QuestionnaireInfoFacto
                 
             });
             questionDetailsReaderMock
-                .Setup(x => x.GetById(questionnaireId))
+                .Setup(x => x.Get(questionnaireId))
                 .Returns(questionnaireView);
 
             factory = CreateQuestionnaireInfoFactory(questionDetailsReaderMock.Object);
@@ -42,7 +43,6 @@ namespace WB.Tests.Unit.Designer.BoundedContexts.Designer.QuestionnaireInfoFacto
         private static QuestionnaireInfoFactory factory;
         private static NewEditRosterView result;
         private static QuestionnaireDocument questionnaireView;
-        private static Mock<IPlainKeyValueStorage<QuestionnaireDocument>> questionDetailsReaderMock;
-        private static string questionnaireId = "11111111111111111111111111111111";
+        private static Mock<IDesignerQuestionnaireStorage> questionDetailsReaderMock;
     }
 }
