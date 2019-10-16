a9a6da83895d50359206d6a4affe2a6438efec1d
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2019-10-15 20:29:57 -0400

    KP-13225 Remove Iteedee.ApkReader

---
+++ src/Tests/WB.Tests.Unit/SharedKernels/SurveyManagement/ServiceTests/AndroidPackageReaderTests/AndroidPackageReaderTests.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System.Reflection;
+using NUnit.Framework;
+using WB.Core.BoundedContexts.Headquarters.Implementation.Services;
+
+namespace WB.Tests.Unit.SharedKernels.SurveyManagement.ServiceTests.AndroidPackageReaderTests
+{
+    internal class AndroidPackageReaderTests
+    {
+        [Test]
+        public void when_reading_package_info()
+        {
+            var reader = new AndroidPackageReader();
+
+            var assembly = Assembly.GetExecutingAssembly();
+            string resourceName = "WB.Tests.Unit.SharedKernels.SurveyManagement.ServiceTests.AndroidPackageReaderTests.TestManifest._apk";
+
+            var manifestResourceStream = assembly.GetManifestResourceStream(resourceName);
+
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                manifestResourceStream.CopyTo(memoryStream);
+
+                var version = reader.Read(memoryStream);
+                Assert.That(version.Version, Is.EqualTo(25987));
+            }
+        }
+    }
+}
