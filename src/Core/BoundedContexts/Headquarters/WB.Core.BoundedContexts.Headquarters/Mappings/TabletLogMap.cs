ab1f6229a11729c6622cb54f5fdb0480848efe62
Andrii Kozhyn <hedgic@gmail.com>
2019-10-18 17:45:57 +0300

    KP-13237 ability to download logs added for admin

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Mappings/TabletLogMap.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Mappings/TabletLogMap.cs
@@ -16,7 +16,11 @@ namespace WB.Core.BoundedContexts.Headquarters.Mappings
                 mapper.Generator(Generators.Identity);
                 mapper.Column("id");
             });
-            Property(x => x.Content, ptp => ptp.Column("content"));
+            Property(x => x.Content, ptp =>
+            {
+                ptp.Column("content");
+                ptp.Lazy(true);
+            });
             Property(x => x.UserName, ptp => ptp.Column("user_name"));
             Property(x => x.DeviceId, ptp => ptp.Column("device_id"));
             Property(x => x.ReceiveDateUtc, ptp => ptp.Column("receive_date_utc"));
