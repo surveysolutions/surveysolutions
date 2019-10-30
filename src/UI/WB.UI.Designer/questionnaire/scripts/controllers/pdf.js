225acaa600ecc607f7ce33885697894189d33d8b
Roman Lytvyn <litvin.roma@gmail.com>
2019-10-30 12:44:17 +0200

    KP-13210 Save as for questionnaire's history items. Fixed some issues. Added retry functionality

--- src/UI/WB.UI.Designer/questionnaire/scripts/controllers/pdf.js
+++ src/UI/WB.UI.Designer/questionnaire/scripts/controllers/pdf.js
@@ -23,59 +23,73 @@
             generateStatusMessage: ''
         };
 
-        $scope.viewModel.translations = $scope.questionnaire.translations;
-        $scope.viewModel.translations.splice(0, 0, { id: null, name: $i18next.t("Translation_Original") });
+        $scope.viewModel.translations = _.map($scope.questionnaire.translations, _.clone);
+        $scope.viewModel.translations.splice(0, 0, { translationId: null, name: $i18next.t("Translation_Original") });
         $scope.viewModel.selectedTranslation = $scope.viewModel.translations[0];
 
         var generateTimerId = null;
 
-        $scope.generate = function () {
-            updateExportPdfStatus();
-        };
-
-        var updateExportPdfStatus = function () {
-            var request = pdfService.updateExportPdfStatus($scope.questionnaire.questionnaireId,
-                $scope.viewModel.selectedTranslation.id);
-
-            request.then(function(result) {
-                var data = result.data;
-
-                if (data.message !== null)
-                    $scope.viewModel.generateStatusMessage = data.message;
-                else
-                    $scope.viewModel.generateStatusMessage =
-                        "Unexpected server response.\r\nPlease contact support@mysurvey.solutions if problem persists.";
-
-                $scope.viewModel.canDownload = data.readyForDownload;
-                $scope.viewModel.canRetryGenerate = data.canRetry;
-
-                if (data.message === null) return;
-
-                if (!$scope.viewModel.canDownload) {
-                    generateTimerId = setTimeout(function() {
-                            updateExportPdfStatus($scope.viewModel.selectedTranslation);
-                        },
-                        1500);
-                }
-                else $scope.isGenerating = false;
-            });
-        };
-
         $scope.selectTranslation = function(translation) {
-            $scope.selectedTranslation = translation;
-        };
-
-        $scope.retryGenerate = function() {
-
-        };
-        $scope.download = function () {
-            $scope.cancel();
-            window.location = '../../pdf/download/' + $scope.questionnaire.questionnaireId +
-                '?translation=' + $scope.viewModel.selectedTranslation.id;
+            $scope.viewModel.selectedTranslation = translation;
         };
 
         $scope.cancel = function () {
             clearTimeout(generateTimerId);
             $uibModalInstance.close();
+        };
+
+        $scope.retryGenerate = function () {
+            var translationId = $scope.viewModel.selectedTranslation.translationId;
+
+            pdfService.retryExportPdf($scope.questionnaire.questionnaireId,
+                translationId);
+
+            updateExportPdfStatus(translationId);
+        };
+
+        $scope.generate = function () {
+            $scope.viewModel.generateStatusMessage = $i18next.t("Initializing") + '...';
+            $scope.viewModel.isGenerating = true;
+
+            updateExportPdfStatus($scope.viewModel.selectedTranslation.translationId);
+        };
+
+        var updateExportPdfStatus = function (translationId) {
+
+            var request = pdfService.updateExportPdfStatus($scope.questionnaire.questionnaireId,
+                translationId);
+
+            request.then(function(result) {
+                onExportPdfStatusReceived(result.data, translationId);
+            });
+        };
+
+        var onExportPdfStatusReceived = function(data, translationId) {
+
+            if (data.message !== null)
+                $scope.viewModel.generateStatusMessage = data.message;
+            else
+                $scope.viewModel.generateStatusMessage =
+                    "Unexpected server response.\r\nPlease contact support@mysurvey.solutions if problem persists.";
+
+            $scope.viewModel.canDownload = data.readyForDownload;
+            $scope.viewModel.canRetryGenerate = data.canRetry;
+
+            if (!$scope.viewModel.canDownload) {
+                if ($scope.viewModel.canRetryGenerate) {
+                    clearTimeout(generateTimerId);
+                    $scope.viewModel.isGenerating = false;
+                } else {
+                    generateTimerId = setTimeout(function() {
+                            updateExportPdfStatus(translationId);
+                        },
+                        1500);
+                }
+            }
+            else {
+                $scope.cancel();
+                window.location = '../../pdf/download/' + $scope.questionnaire.questionnaireId +
+                    '?translation=' + translationId;
+            }
         };
 });
