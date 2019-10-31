5f02a769e5a5cb4be91cbdcc319d97d6ca93b027
Andrii Kozhyn <andrii.kozhyn@outlook.com>
2019-10-31 12:52:44 +0200

    KP-12455 moved to appcenter sdk

--- src/UI/Shared/WB.UI.Shared.Enumerator/Activities/BaseActivity.cs
+++ src/UI/Shared/WB.UI.Shared.Enumerator/Activities/BaseActivity.cs
@@ -1,72 +1,26 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Runtime.CompilerServices;
-using Android.Content.PM;
 using Android.OS;
 using Android.Support.Graphics.Drawable;
 using Android.Views;
-using Microsoft.AppCenter;
-using Microsoft.AppCenter.Analytics;
-using Microsoft.AppCenter.Crashes;
-using MvvmCross;
 using MvvmCross.Droid.Support.V7.AppCompat;
 using MvvmCross.Platforms.Android.Presenters.Attributes;
 using MvvmCross.ViewModels;
+using NLog;
 using Plugin.CurrentActivity;
 using Plugin.Permissions;
-using WB.Core.GenericSubdomains.Portable.Services;
-using WB.UI.Shared.Enumerator.Services;
 using WB.UI.Shared.Enumerator.Utils;
 
 namespace WB.UI.Shared.Enumerator.Activities
 {
-    static class CrashReportingConfiguration
-    {
-        public static bool IsCrashReportingConfigured;
-        public static object CrashLockObject = new object();
-    }
-
     [MvxActivityPresentation]
     public abstract class BaseActivity<TViewModel> : MvxAppCompatActivity<TViewModel> where TViewModel : class, IMvxViewModel
     {
         
         protected abstract int ViewResourceId { get; }
-        private ILogger log;
+        private Logger log;
 
         protected override void OnCreate(Bundle bundle)
         {
-            if (!CrashReportingConfiguration.IsCrashReportingConfigured)
-            {
-                lock (CrashReportingConfiguration.CrashLockObject)
-                {
-                    if (!CrashReportingConfiguration.IsCrashReportingConfigured)
-                    {
-                        Crashes.GetErrorAttachments = report =>
-                        {
-                            var result =  new List<ErrorAttachmentLog>();
-                            var pathToLocalDirectory = AndroidPathUtils.GetPathToInternalDirectory();
-                            
-                            var lastLogFile = Path.Combine(pathToLocalDirectory, "Logs", report.AppErrorTime.ToString("yyyy-MM-dd.log"));
-                            if (File.Exists(lastLogFile))
-                            {
-                                result.Add(ErrorAttachmentLog.AttachmentWithText(File.ReadAllText(lastLogFile),"Log.txt"));
-                            }
-
-                            return result;
-                        };
-
-                        ApplicationInfo ai = PackageManager.GetApplicationInfo(PackageName, PackageInfoFlags.MetaData);
-                        var myApiKey = ai.MetaData.GetString("net.hockeyapp.android.appIdentifier");
-                        AppCenter.Start(myApiKey, typeof(Analytics), typeof(Crashes));
-
-                        CrashReportingConfiguration.IsCrashReportingConfigured = true;
-                    }
-                }
-            }
-            
-
-            log = Mvx.IoCProvider.Resolve<ILoggerProvider>().GetForType(this.GetType());
+            log = LogManager.GetLogger(this.GetType().Name);
             log.Trace("Create");
             base.OnCreate(bundle);
             CrossCurrentActivity.Current.Init(this, bundle);
