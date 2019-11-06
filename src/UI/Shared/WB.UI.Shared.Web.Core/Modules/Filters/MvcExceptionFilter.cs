719803a124fdf26564118b8925f95217a4918ba6
Pavel Stovpivskyi <sby@rambler.ru>
2019-11-06 16:24:31 +0200

    KP-13227 added new project SharedWeb

---
+++ src/UI/Shared/WB.UI.Shared.Web.Core/Modules/Filters/MvcExceptionFilter.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+
+namespace WB.UI.Shared.Web.Modules.Filters
+{
+    public class MvcExceptionFilter<TFilter> : System.Web.Mvc.IExceptionFilter
+        where TFilter : System.Web.Mvc.IExceptionFilter
+    {
+        private readonly TFilter filter;
+
+        public MvcExceptionFilter(TFilter filter)
+        {
+            this.filter = filter;
+        }
+
+        public void OnException(ExceptionContext filterContext)
+        {
+            filter.OnException(filterContext);
+        }
+    }
+}
---
+++ src/UI/Shared/WB.UI.Shared.Web.Core/Modules/IWebModule.cs
@@ -0,0 +1,9 @@
+﻿using WB.Core.GenericSubdomains.Portable.ServiceLocation;
+using WB.Core.Infrastructure.Modularity;
+
+namespace WB.UI.Shared.Web.Modules
+{
+    public interface IWebModule : IModule<IWebIocRegistry>
+    {
+    }
+}
