51cb73baa7455fe2a9f3a28a47fd52db78a3cbb7
Pavel Stovpivskyi <sby@rambler.ru>
2019-11-08 12:42:27 +0200

    KP-13317 added ref on old project

--- src/UI/WebTester/App_Start/RouteConfig.cs
+++ src/UI/WebTester/App_Start/RouteConfig.cs
@@ -2,9 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Mvc;
-using System.Web.Mvc.Routing.Constraints;
-using System.Web.Routing;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.Routing.Constraints;
 
 namespace WB.UI.WebTester
 {
