db777ec3ee7c4b29232c8136a4f0b1fea50bb859
Pavel Stovpivskyi <630198+spuv@users.noreply.github.com>
2019-11-08 19:10:08 +0200

    KP-13317 try to fix ProgressiveDownload

--- src/UI/Shared/WB.UI.Shared.Web.Core/Extensions/ProgressiveDownloadHelpers.cs
+++ src/UI/Shared/WB.UI.Shared.Web.Core/Extensions/ProgressiveDownloadHelpers.cs
@@ -1,17 +1,18 @@
 ﻿using System.IO;
 using System.Net.Http;
+using Microsoft.AspNetCore.Http;
 
 namespace WB.UI.Shared.Web.Extensions
 {
     public static class ProgressiveDownloadHelpers
     {
-        public static HttpResponseMessage AsProgressiveDownload(this HttpRequestMessage request, Stream stream,
+        public static HttpResponseMessage AsProgressiveDownload(this HttpRequest request, Stream stream,
             string mediaType, string fileName = null, byte[] hash = null)
         {
             return new ProgressiveDownload(request).ResultMessage(stream, mediaType, fileName, hash);
         }
 
-        public static HttpResponseMessage AsProgressiveDownload(this HttpRequestMessage request, byte[] data,
+        public static HttpResponseMessage AsProgressiveDownload(this HttpRequest request, byte[] data,
             string mediaType, string fileName = null, byte[] hash = null)
         {
             return new ProgressiveDownload(request).ResultMessage(new MemoryStream(data), mediaType, fileName, hash);
