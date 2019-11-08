d8757f928987f5ac6fd01dce889b1e3b13c792a4
Pavel Stovpivskyi <630198+spuv@users.noreply.github.com>
2019-11-08 12:31:28 +0200

    KP-13317 synchronize old assembly with new

--- src/UI/Shared/WB.UI.Shared.Web.Core/Captcha/ICaptchaService.cs
+++
@@ -1,9 +0,0 @@
-namespace WB.UI.Shared.Web.Captcha
-{
-    public interface ICaptchaService
-    {
-        bool ShouldShowCaptcha(string username);
-        void RegisterFailedLogin(string username);
-        void ResetFailedLogin(string username);
-    }
-}
