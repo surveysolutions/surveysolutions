dcf504f5f983043ae40a9dcbe2350487b4efc9bc
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2019-11-12 10:08:51 -0500

    Tester<->Designer protocol version bump

--- src/Core/SharedKernels/Questionnaire/WB.Core.SharedKernels.Questionnaire/Api/Designer/ApiVersion.cs
+++ src/Core/SharedKernels/Questionnaire/WB.Core.SharedKernels.Questionnaire/Api/Designer/ApiVersion.cs
@@ -2,7 +2,7 @@ namespace WB.Core.SharedKernels.SurveySolutions.Api.Designer
 {
     public static class ApiVersion
     {
-        public const int CurrentTesterProtocolVersion = 45; // 19.08 release
+        public const int CurrentTesterProtocolVersion = 46; // 19.11 release
         public const int MaxQuestionnaireVersion = 27;
     }
 }
