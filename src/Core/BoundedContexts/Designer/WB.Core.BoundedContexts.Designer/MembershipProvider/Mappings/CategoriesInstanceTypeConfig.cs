3d2e6bb9116838cd11a6a7a88412c6b043436064
Roman Lytvyn <litvin.roma@gmail.com>
2019-11-19 16:08:10 +0200

    KP-13324 Ability to create options collection and reference it in categorical questions. Added categories export

---
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/MembershipProvider/Mappings/CategoriesInstanceTypeConfig.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WB.Core.BoundedContexts.Designer.Commands.Questionnaire.Categories;
+
+namespace WB.Core.BoundedContexts.Designer.MembershipProvider.Mappings
+{
+    public class CategoriesInstanceTypeConfig : IEntityTypeConfiguration<CategoriesInstance>
+    {
+        public void Configure(EntityTypeBuilder<CategoriesInstance> builder)
+        {
+            builder.ToTable("categoriesinstances", "plainstore");
+
+            builder.HasKey(x => new {x.QuestionnaireId, x.CategoriesId, x.Id});
+
+            builder.Property(e => e.QuestionnaireId).HasColumnName("questionnaireid");
+            builder.Property(e => e.CategoriesId).HasColumnName("categoriesid");
+            builder.Property(e => e.Id).HasColumnName("id");
+            builder.Property(e => e.ParentId).HasColumnName("parentid");
+            builder.Property(e => e.Text).HasColumnName("text");
+        }
+    }
+}
