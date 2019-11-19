f465d59d1cfa4947915de55f1eebc060bf4b7d0f
Roman Lytvyn <litvin.roma@gmail.com>
2019-11-19 18:31:02 +0200

    KP-13324 Ability to create options collection and reference it in categorical questions. Store categories. Added migration

---
+++ src/Infrastructure/WB.Persistence.Designer/Migrations/M001-100/M201911191659_AddStoredCategories.cs
@@ -0,0 +1,18 @@
+﻿using FluentMigrator;
+
+namespace WB.UI.Designer.Migrations.PlainStore
+{
+    [Migration(201911191659, "Added stored categories", BreakingChange = false)]
+    public class M201911191659_AddStoredCategories : AutoReversingMigration
+    {
+        public override void Up()
+        {
+            Create.Table("categories")
+                .WithColumn("questionnaireid").AsGuid().NotNullable().PrimaryKey()
+                .WithColumn("categoriesid").AsGuid().NotNullable().PrimaryKey()
+                .WithColumn("id").AsInt32().NotNullable().PrimaryKey()
+                .WithColumn("parentid").AsInt32().Nullable()
+                .WithColumn("text").AsString().NotNullable();
+        }
+    }
+}
