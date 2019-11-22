eb6438e180536c338e7f5ca662827bf9a5414335
Andrii Kozhyn <andrii.kozhyn@outlook.com>
2019-11-22 16:36:55 +0200

    KP-13221 empty asp.net core 3 project created

---
+++ src/UI/WB.UI.Headquarters.Core/Program.cs
@@ -0,0 +1,35 @@
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+namespace WB.UI.Headquarters
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, c) =>
+                {
+                    c.AddIniFile("appsettings.ini", false, true);
+                    c.AddIniFile("appsettings.cloud.ini", true, true);
+                    c.AddIniFile($"appsettings.{Environment.MachineName}.ini", true);
+                    c.AddIniFile("appsettings.Production.ini", true);
+                    c.AddCommandLine(args);
+
+                    if(hostingContext.HostingEnvironment.IsDevelopment())
+                    {
+                        c.AddUserSecrets<Startup>();
+                    }
+                })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
