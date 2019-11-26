3d6ce79cf8f6686ce1f5742139d4fca55405ff7b
Pavel Stovpivskyi <sby@rambler.ru>
2019-11-26 18:01:20 +0200

    KP-13324 moved to fill options to hq questionnaire storage

--- src/Core/SharedKernels/DataCollection/DataCollection/Implementation/Repositories/QuestionnaireStorage.cs
+++ src/Core/SharedKernels/DataCollection/DataCollection/Implementation/Repositories/QuestionnaireStorage.cs
@@ -5,12 +5,9 @@ using WB.Core.SharedKernels.DataCollection.Aggregates;
 using WB.Core.SharedKernels.DataCollection.Implementation.Entities;
 using WB.Core.SharedKernels.DataCollection.Repositories;
 using System.Collections.Concurrent;
-using System.Collections.Generic;
 using System.Linq;
-using Main.Core.Entities.SubEntities;
 using WB.Core.GenericSubdomains.Portable.Services;
 using WB.Core.SharedKernels.DataCollection.Services;
-using WB.Core.SharedKernels.Questionnaire.Documents;
 using WB.Core.SharedKernels.Questionnaire.Translations;
 using WB.Core.SharedKernels.SurveySolutions.Documents;
 
@@ -28,15 +25,14 @@ namespace WB.Core.SharedKernels.DataCollection.Implementation.Repositories
         private readonly IQuestionOptionsRepository questionOptionsRepository;
         private readonly ISubstitutionService substitutionService;
         private readonly IInterviewExpressionStatePrototypeProvider expressionStatePrototypeProvider;
-        private readonly IReusableCategoriesStorage reusableCategoriesStorage;
 
         public QuestionnaireStorage(IPlainKeyValueStorage<QuestionnaireDocument> repository, 
             ITranslationStorage translationStorage, 
             IQuestionnaireTranslator translator,
             IQuestionOptionsRepository questionOptionsRepository,
             ISubstitutionService substitutionService,
-            IInterviewExpressionStatePrototypeProvider expressionStatePrototypeProvider,
-            IReusableCategoriesStorage reusableCategoriesStorage)
+            IInterviewExpressionStatePrototypeProvider expressionStatePrototypeProvider
+            )
         {
             this.repository = repository;
             this.translationStorage = translationStorage;
@@ -44,7 +40,6 @@ namespace WB.Core.SharedKernels.DataCollection.Implementation.Repositories
             this.questionOptionsRepository = questionOptionsRepository;
             this.substitutionService = substitutionService;
             this.expressionStatePrototypeProvider = expressionStatePrototypeProvider ?? throw new ArgumentNullException(nameof(expressionStatePrototypeProvider));
-            this.reusableCategoriesStorage = reusableCategoriesStorage;
         }
 
         public virtual IQuestionnaire GetQuestionnaire(QuestionnaireIdentity identity, string language)
@@ -60,22 +55,7 @@ namespace WB.Core.SharedKernels.DataCollection.Implementation.Repositories
             if (questionnaireDocument == null || questionnaireDocument.IsDeleted)
                 return null;
 
-            if (questionnaireDocument.Categories.Any())
-            {
-                foreach (var question in questionnaireDocument.Find<ICategoricalQuestion>())
-                {
-                    if (question.CategoriesId.HasValue)
-                    {
-                        var options = reusableCategoriesStorage.GetOptions(identity, question.CategoriesId.Value);
-                        question.Answers = options.Select(o => new Answer()
-                        {
-                            AnswerText = o.Title,
-                            AnswerCode = o.Value,
-                            ParentCode = o.ParentValue
-                        }).ToList();
-                    }
-                }
-            }
+            FillPlainQuestionnaireDataOnCreate(identity, questionnaireDocument);
 
             Translation translationId = null;
             if (language != null)
@@ -102,6 +82,11 @@ namespace WB.Core.SharedKernels.DataCollection.Implementation.Repositories
             }
 
             return plainQuestionnaire;
+        }
+
+        protected virtual void FillPlainQuestionnaireDataOnCreate(QuestionnaireIdentity identity, QuestionnaireDocument questionnaireDocument)
+        {
+
         }
 
         public virtual void StoreQuestionnaire(Guid id, long version, QuestionnaireDocument questionnaireDocument)
