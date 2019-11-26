6a822cabfe90abb6099a97961511ab0b5fa86b4f
Andrii Kozhyn <hedgic@gmail.com>
2019-11-26 11:23:51 +0200

    KP-13221 Pull infrastructure code

--- src/UI/WB.UI.Headquarters.Core/Startup.cs
+++ src/UI/WB.UI.Headquarters.Core/Startup.cs
@@ -1,16 +1,25 @@
+using System.Collections.Generic;
+using System.Globalization;
+using Autofac;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using WB.UI.Headquarters.Data;
+using WB.Core.Infrastructure;
+using WB.Core.Infrastructure.Modularity.Autofac;
+using WB.Core.Infrastructure.Ncqrs;
+using WB.Core.SharedKernels.DataCollection;
+using WB.Infrastructure.Native.Logging;
+using WB.UI.Shared.Web.Versions;
 
 namespace WB.UI.Headquarters
 {
     public class Startup
     {
+        private AutofacKernel autofacKernel;
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -18,14 +27,38 @@ namespace WB.UI.Headquarters
 
         public IConfiguration Configuration { get; }
 
+        // ConfigureContainer is where you can register things directly
+        // with Autofac. This runs after ConfigureServices so the things
+        // here will override registrations made in ConfigureServices.
+        // Don't build the container; that gets done for you by the factory.
+        public void ConfigureContainer(ContainerBuilder builder)
+        {
+            autofacKernel = new AutofacKernel(builder);
+            autofacKernel.Load(
+                new NcqrsModule(),
+                new NLogLoggingModule(),
+                new InfrastructureModuleMobile(),
+                new DataCollectionSharedKernelModule(),
+                //new CaptchaModule("recaptcha"),
+                new ProductVersionModule(typeof(Startup).Assembly));
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+            services.AddOptions();
+            services.AddControllersWithViews()
+                .AddNewtonsoftJson();
+            services.AddDistributedMemoryCache();
+            services.AddSession();
+            services.AddResponseCaching();
+            services.AddResponseCompression();
+            services.AddLogging();
+            services.AddSignalR()
+                .AddNewtonsoftJsonProtocol();
+
+            services.AddHttpContextAccessor();
+
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
@@ -51,6 +84,24 @@ namespace WB.UI.Headquarters
 
             app.UseAuthentication();
             app.UseAuthorization();
+
+            app.UseRequestLocalization(opt =>
+            {
+                opt.DefaultRequestCulture = new RequestCulture("en-US");
+                opt.SupportedCultures = new List<CultureInfo>
+                {
+                    new CultureInfo("en")
+                };
+                opt.SupportedUICultures = new List<CultureInfo>
+                {
+                    new CultureInfo("en"),
+                    new CultureInfo("ru"),
+                    new CultureInfo("fr"),
+                    new CultureInfo("es"),
+                    new CultureInfo("ar"),
+                    new CultureInfo("zh")
+                };
+            });
 
             app.UseEndpoints(endpoints =>
             {
