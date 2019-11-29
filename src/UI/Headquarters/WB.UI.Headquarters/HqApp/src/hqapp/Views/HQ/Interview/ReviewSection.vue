0ec14995a501143837933ffe0a12b625ba49e72c
Andrii Kozhyn <hedgic@gmail.com>
2019-11-29 16:22:03 +0200

    KP-13383 fixed change section during first navigation

--- src/UI/Headquarters/WB.UI.Headquarters/HqApp/src/hqapp/Views/HQ/Interview/ReviewSection.vue
+++ src/UI/Headquarters/WB.UI.Headquarters/HqApp/src/hqapp/Views/HQ/Interview/ReviewSection.vue
@@ -3,9 +3,5 @@
 </template>
 <script>
 export default {
-    async beforeRouteUpdate(to, from, next) {
-        await this.$store.dispatch("changeSection", to.params.sectionId, from.params.sectionId);
-        next();
-    }
 };
 </script>
