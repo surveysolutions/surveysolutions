3f084e6252f3780418109c9ba6fd83359b4f56de
Pavel Stovpivskyi <630198+spuv@users.noreply.github.com>
2019-12-03 19:12:27 +0200

    KP-13328 removed not used code

--- src/Infrastructure/WB.Infrastructure.Native/Questionnaire/Impl/QuestionnaireQuestionOptionsRepository.cs
+++ src/Infrastructure/WB.Infrastructure.Native/Questionnaire/Impl/QuestionnaireQuestionOptionsRepository.cs
@@ -30,8 +30,5 @@ namespace WB.Enumerator.Native.Questionnaire.Impl
 
         public IEnumerable<CategoricalOption> GetOptionsByOptionValues(IQuestionnaire questionnaire, Guid questionId, int[] optionsValues, Translation translation) => 
             questionnaire.GetOptionsForQuestion(questionId, null, null, null).Where(x => optionsValues.Contains(x.Value));
-
-        private bool DoesSupportReusableCategories(IQuestionnaire questionnaire, Guid questionId)
-            => questionnaire.DoesSupportReusableCategories(questionId);
     }
 }
