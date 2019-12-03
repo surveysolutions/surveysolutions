fa65a319b9668a5f916c9de5396a5188b90a9b06
Andrii Kozhyn <hedgic@gmail.com>
2019-12-03 11:20:55 +0200

    KP-13221 user is able to login

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Views/User/HqUser.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Views/User/HqUser.cs
@@ -1,7 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using Main.Core.Entities.SubEntities;
 using Microsoft.AspNetCore.Identity;
