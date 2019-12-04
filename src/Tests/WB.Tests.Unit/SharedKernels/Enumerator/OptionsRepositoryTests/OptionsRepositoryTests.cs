63f30d30cd2ec22d2b858ff281a988975a7b8f3a
Pavel Stovpivskyi <sby@rambler.ru>
2019-12-04 15:51:33 +0200

    KP-13328 added tests

--- src/Tests/WB.Tests.Unit/SharedKernels/Enumerator/OptionsRepositoryTests/OptionsRepositoryTests.cs
+++ src/Tests/WB.Tests.Unit/SharedKernels/Enumerator/OptionsRepositoryTests/OptionsRepositoryTests.cs
@@ -7,6 +7,7 @@ using NUnit.Framework;
 using WB.Core.SharedKernels.DataCollection;
 using WB.Core.SharedKernels.Enumerator.Implementation.Services;
 using WB.Core.SharedKernels.Enumerator.Views;
+using WB.Core.SharedKernels.Questionnaire.Categories;
 using WB.Core.SharedKernels.Questionnaire.Translations;
 using WB.Tests.Abc;
 using WB.Tests.Abc.Storage;
@@ -254,6 +255,213 @@ namespace WB.Tests.Unit.SharedKernels.Enumerator.OptionsRepositoryTests
 
             // act
             var cascadingOption = storage.GetQuestionOption(questionnaireIdentity, questionId, "two", 2, null);
+
+            // assert
+            Assert.That(cascadingOption, Is.Not.Null);
+            Assert.That(cascadingOption.Value, Is.EqualTo(4));
+        }       
+        
+        [Test]
+        public void should_keep_categories_options_order_after_saving_options()
+        {
+            var categoryId = Guid.Parse("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+            var answerCodes = Enumerable.Range(0, 300).Reverse().Select(o => new CategoriesItem()
+            {
+                Id = o,
+                Text = o.ToString()
+            }).ToList();
+
+            var storage = new OptionsRepository(new SqliteInmemoryStorage<OptionView>());
+
+            var questionnaireIdentity = Create.Entity.QuestionnaireIdentity();
+            storage.StoreOptionsForCategory(questionnaireIdentity, categoryId, answerCodes, new List<TranslationDto>());
+
+            var filteredQuestionOptions = storage.GetFilteredCategoriesOptions(questionnaireIdentity, categoryId, null, null, null);
+
+            var actual = filteredQuestionOptions.Select(x => x.Value).ToList();
+            Assert.That(actual, Is.Not.Empty);
+            Assert.That(actual, Is.Ordered.Descending);
+        }
+
+        [Test]
+        public void should_return_categories_options_respecting_translation()
+        {
+            var questionnaireIdentity = Create.Entity.QuestionnaireIdentity();
+            var categoryId = Guid.Parse("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+
+            var options = new List<CategoriesItem>();
+            options.Add(Create.Entity.CategoriesItem(1.ToString(), 1));
+            options.Add(Create.Entity.CategoriesItem(2.ToString(), 2));
+
+            var translationId = Guid.Parse("1AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+
+            var optionTranslationValue = "test";
+
+            var translations = new List<TranslationDto>()
+            {
+                new TranslationDto()
+                {
+                    TranslationId = translationId,
+                    QuestionnaireEntityId = categoryId,
+                    TranslationIndex = 1.ToString(),
+                    Value = optionTranslationValue,
+                    Type = TranslationType.OptionTitle
+                }
+            };
+
+            var storage = new OptionsRepository(new SqliteInmemoryStorage<OptionView>());
+
+            storage.StoreOptionsForCategory(questionnaireIdentity, categoryId, options, translations);
+
+            var filteredQuestionOptions = storage.GetFilteredCategoriesOptions(questionnaireIdentity, categoryId, null, null, translationId);
+
+            var actual = filteredQuestionOptions.ToList();
+            Assert.That(actual, Is.Not.Empty);
+            Assert.That(actual.Count, Is.EqualTo(2));
+            Assert.That(actual.First().Title, Is.EqualTo(optionTranslationValue));
+        }
+
+        [Test]
+        public void should_return_categories_options_respecting_translation_with_no_extra()
+        {
+            var questionnaireIdentity = Create.Entity.QuestionnaireIdentity();
+
+            var categoryId = Guid.Parse("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+            var options = Enumerable.Range(0, 100)
+                .Reverse()
+                .Select(answerCode => Create.Entity.CategoriesItem(answerCode.ToString(), answerCode))
+                .ToList();
+
+            var translationId = Guid.Parse("1AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+
+            var translations = new List<TranslationDto>()
+            {
+                new TranslationDto()
+                {
+                    TranslationId = translationId,
+                    QuestionnaireEntityId = categoryId,
+                    TranslationIndex = 11.ToString(),
+                    Value = 11.ToString() + "b",
+                    Type = TranslationType.OptionTitle
+                },
+                new TranslationDto()
+                {
+                    TranslationId = translationId,
+                    QuestionnaireEntityId = categoryId,
+                    TranslationIndex = 21.ToString(),
+                    Value = 21.ToString() + "a",
+                    Type = TranslationType.OptionTitle
+                }
+            };
+
+            var storage = new OptionsRepository(new SqliteInmemoryStorage<OptionView>());
+
+            storage.StoreOptionsForCategory(questionnaireIdentity, categoryId, options, translations);
+
+            var filteredQuestionOptions = storage.GetFilteredCategoriesOptions(questionnaireIdentity, categoryId, null, null, translationId);
+
+            var actual = filteredQuestionOptions.Select(x => x.Value).ToList();
+            Assert.That(actual, Is.Not.Empty);
+            Assert.That(actual.Count, Is.EqualTo(100));
+            Assert.That(actual, Is.Ordered.Descending);
+
+        }
+
+        [Test]
+        public void should_return_requested_categories_option_by_title()
+        {
+            var questionnaireIdentity = Create.Entity.QuestionnaireIdentity();
+
+            var categoryId = Guid.Parse("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+
+            var options = new List<CategoriesItem>();
+            options.Add(Create.Entity.CategoriesItem(1.ToString(), 1));
+            options.Add(Create.Entity.CategoriesItem(2.ToString(), 2));
+
+            var translationId = Guid.Parse("1AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+
+            var optionTranslationValue = "test";
+
+            var translations = new List<TranslationDto>()
+            {
+                new TranslationDto()
+                {
+                    TranslationId = translationId,
+                    QuestionnaireEntityId = categoryId,
+                    TranslationIndex = 1.ToString(),
+                    Value = optionTranslationValue,
+                    Type = TranslationType.OptionTitle
+                }
+            };
+
+            var storage = new OptionsRepository(new SqliteInmemoryStorage<OptionView>());
+            storage.StoreOptionsForCategory(questionnaireIdentity, categoryId, options, translations);
+            var filteredQuestionOption = storage.GetCategoryOption(questionnaireIdentity, categoryId, 1.ToString(), null, translationId);
+
+            Assert.That(filteredQuestionOption, Is.Not.Null);
+            Assert.That(filteredQuestionOption.Value, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void should_return_requested_categories_option_by_value()
+        {
+            var questionnaireIdentity = Create.Entity.QuestionnaireIdentity();
+
+            var categoryId = Guid.Parse("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+
+            var options = new List<CategoriesItem>();
+            options.Add(Create.Entity.CategoriesItem(1.ToString(), 1));
+            options.Add(Create.Entity.CategoriesItem(2.ToString(), 2));
+
+            var translationId = Guid.Parse("1AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+
+            var optionTranslationValue = "test";
+
+            var translations = new List<TranslationDto>()
+            {
+                new TranslationDto()
+                {
+                    TranslationId = translationId,
+                    QuestionnaireEntityId = categoryId,
+                    TranslationIndex = 1.ToString(),
+                    Value = optionTranslationValue,
+                    Type = TranslationType.OptionTitle
+                }
+            };
+
+            var storage = new OptionsRepository(new SqliteInmemoryStorage<OptionView>());
+            storage.StoreOptionsForCategory(questionnaireIdentity, categoryId, options, translations);
+
+            // Assert
+            var filteredQuestionOption = storage.GetCategoryOptionByValue(questionnaireIdentity, categoryId, 1, translationId);
+             
+            CategoricalOption[] filteredOption1 = storage.GetCategoryOptionsByValues(questionnaireIdentity, categoryId, new[] {1}, translationId);
+
+            Assert.That(filteredOption1, Has.Length.EqualTo(1));
+            Assert.That(filteredQuestionOption, Is.Not.Null);
+            Assert.That(filteredQuestionOption.Value, Is.EqualTo(1));
+
+            CategoricalOption[] filteredOption2 = storage.GetCategoryOptionsByValues(questionnaireIdentity, categoryId, new[] {1, 2}, translationId);
+            Assert.That(filteredOption2, Has.Length.EqualTo(2));
+        }
+
+        [Test]
+        public void should_return_correct_categories_option_when_contains_duplicate_titles()
+        {
+            var options = new List<CategoriesItem>();
+            options.Add(Create.Entity.CategoriesItem("one", 1, 1));
+            options.Add(Create.Entity.CategoriesItem("two", 2, 1));
+            options.Add(Create.Entity.CategoriesItem("one", 3, 2));
+            options.Add(Create.Entity.CategoriesItem("two", 4, 2));
+
+            var categoryId = Guid.Parse("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+
+            var questionnaireIdentity = Create.Entity.QuestionnaireIdentity();
+            var storage = new OptionsRepository(new SqliteInmemoryStorage<OptionView>());
+            storage.StoreOptionsForCategory(questionnaireIdentity, categoryId, options, new List<TranslationDto>());
+
+            // act
+            var cascadingOption = storage.GetCategoryOption(questionnaireIdentity, categoryId, "two", 2, null);
 
             // assert
             Assert.That(cascadingOption, Is.Not.Null);
