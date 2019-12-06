770411b3e7b5d233892192054caec9557a8ca9d3
Andrii Kozhyn <hedgic@gmail.com>
2019-12-06 15:03:58 +0200

    KP-13221 fixed main menu controls

---
+++ src/UI/WB.UI.Headquarters.Core/Services/HtmlExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using WB.Core.SharedKernels.SurveyManagement.Web.Models;
+
+namespace WB.UI.Headquarters.Services
+{
+    public static class HtmlExtensions
+    {
+        public static IHtmlContent  MainMenuItem(this IHtmlHelper html, string actionName, string controllerName, string linkText, MenuItem renderedPage)
+        {
+            var page = html.ViewBag.ActivePage ?? MenuItem.Logon;
+            string isActive = page == renderedPage ? "active" : String.Empty;
+
+            string liStartTag = $"<li class='{isActive}'>{Environment.NewLine}";
+            var part2 = html.ActionLink(linkText, actionName, controllerName, new {area = "", id = ""}, new { title = linkText });
+
+            return new HtmlString(liStartTag + part2 + $"{Environment.NewLine}</li>");
+        }
+    }
+}
