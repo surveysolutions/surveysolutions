ef32d856a29ae9fe6c820bc3c7efe1dec469fcf3
Pavel Stovpivskyi <630198+spuv@users.noreply.github.com>
2019-12-10 19:03:19 +0200

    KP-13349 added common handler for categories

--- src/UI/Headquarters/WB.UI.Headquarters/API/DataCollection/Enumerator/EnumeratorWebApiConfig.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/API/DataCollection/Enumerator/EnumeratorWebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Web.Http;
 using WB.Core.SharedKernels.SurveyManagement.Web.Filters;
 using WB.UI.Headquarters.API.DataCollection.Enumerator;
+using WB.UI.Headquarters.API.DataCollection.Enumerator.v1;
 
 namespace WB.UI.Headquarters.API.DataCollection.Enumerator
 {
@@ -12,6 +13,8 @@ namespace WB.UI.Headquarters.API.DataCollection.Enumerator
         public static void Register(HttpConfiguration config)
         {
             config.TypedRoute("api/enumerator/logs", c => c.Action<LogsApiController>(x => x.Post()));
+
+            config.TypedRoute("api/enumerator/v1/categories/{id}", c => c.Action<ReusableCategoriesApiV1Controller>(x => x.Get(Param.Any<string>())));
         }
 #pragma warning restore 4014
     }
