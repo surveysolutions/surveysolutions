b794141a938b2a9a65abcb6049ca49fdcb88cf41
Andrii Kozhyn <hedgic@gmail.com>
2019-12-10 11:48:02 +0200

    Core project compiles

--- src/UI/Headquarters/WB.UI.Headquarters/Views/Shared/HtmlExtensions.cs
+++ src/UI/Headquarters/WB.UI.Headquarters/Views/Shared/HtmlExtensions.cs
@@ -19,12 +19,7 @@ namespace ASP
     [Localizable(false)]
     public static partial class HtmlExtensions
     {
-        public static IHtmlString ActivePage(this HtmlHelper html)
-        {
-            MenuItem page = html.ViewBag.ActivePage ?? MenuItem.Logon;
-            return new MvcHtmlString(GetMenuItemTitle(page));
-        }
-
+        
         public static string QuestionnaireName(this HtmlHelper html, string name, long version)
         {
             return string.Format(Pages.QuestionnaireNameFormat, name, version);
@@ -73,45 +68,7 @@ namespace ASP
             return MvcHtmlString.Empty;
         }
 
-        private static string GetMenuItemTitle(MenuItem page)
-        {
-            switch (page)
-            {
-                case MenuItem.Surveys: return @MainMenu.SurveysAndStatuses;
-                case MenuItem.ApiUsers: return @MainMenu.ApiUsers;
-                case MenuItem.DataExport: return @MainMenu.DataExport;
-                case MenuItem.Docs: return @MainMenu.Interviews;
-                case MenuItem.Headquarters: return @MainMenu.Headquarters;
-                case MenuItem.Interviewers: return @MainMenu.Interviewers;
-                case MenuItem.InterviewsChart: return @MainMenu.CumulativeChart;
-                case MenuItem.ManageAccount: return Strings.SurverManagement_MainMenu_ManageAccount;
-                case MenuItem.MapReport: return @MainMenu.MapReport;
-                case MenuItem.NumberOfCompletedInterviews: return @MainMenu.Quantity;
-                case MenuItem.SpeedOfCompletingInterviews: return @MainMenu.Speed;
-                case MenuItem.Observers: return @MainMenu.Observers;
-                case MenuItem.Questionnaires: return @MainMenu.SurveySetup;
-                case MenuItem.Settings: return Resources.Common.Settings;
-                case MenuItem.Summary: return @MainMenu.TeamsAndStatuses;
-                case MenuItem.Teams: return @MainMenu.Supervisors;
-                case MenuItem.UserBatchUpload: return @MainMenu.UserBatchUpload;
-                case MenuItem.Statuses: return @MainMenu.SurveysAndStatuses;
-                case MenuItem.Logon: return "Logon";
-                case MenuItem.Devices: return "Devices";
-                case MenuItem.SyncLog: return "SyncLog";
-                case MenuItem.CreateNew: return MainMenu.CreateNew;
-                case MenuItem.Started: return MainMenu.Started;
-                case MenuItem.Rejected: return MainMenu.Rejected;
-                case MenuItem.Completed: return MainMenu.Completed;
-                case MenuItem.SurveyAndStatuses: return MainMenu.SurveysAndStatuses;
-                case MenuItem.StatusDuration: return MainMenu.StatusDuration;
-                case MenuItem.DevicesInterviewers: return MainMenu.DevicesInterviewers;
-                case MenuItem.Assignments: return MainMenu.Assignments;
-                case MenuItem.AuditLog: return AuditLog.PageTitle;
-                case MenuItem.Maps: return MainMenu.Maps;
-                case MenuItem.SurveyStatistics: return MainMenu.SurveyStatistics;
-                default: return String.Empty;
-            }
-        }
+        
 
         private static readonly JsonSerializerSettings asJsonValueSettings = new JsonSerializerSettings
         {
