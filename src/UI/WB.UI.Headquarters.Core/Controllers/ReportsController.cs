97cfcbea65b35d47f3946f9cae1e5e196de89c5c
Andrii Kozhyn <hedgic@gmail.com>
2019-12-10 10:12:32 +0200

    Moved some of the reports controller

---
+++ src/UI/WB.UI.Headquarters.Core/Controllers/ReportsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WB.Core.SharedKernels.SurveyManagement.Web.Models;
+using WB.UI.Headquarters.Models.Reports;
+
+namespace WB.UI.Headquarters.Controllers
+{
+    public class ReportsController : Controller
+    {
+        [Authorize(Roles = "Administrator, Headquarter")]
+        [ActivePage(MenuItem.SurveyAndStatuses)]
+        public ActionResult SurveysAndStatuses(SurveysAndStatusesModel model)
+        {
+            return this.View(model);
+        }
+    }
+}
