a6ce0c186faff7d34e8c248461a10c1b9d90d262
Andrii Kozhyn <hedgic@gmail.com>
2019-12-10 12:24:48 +0200

    Removed old enumerator native project

--- src/Core/SharedKernels/Enumerator/WB.Enumerator.Native-old/WebInterview/Models/SectionData.cs
+++
@@ -1,8 +0,0 @@
-namespace WB.Enumerator.Native.WebInterview.Models
-{
-    public class SectionData
-    {
-        public InterviewEntityWithType[] Entities { get; set; }
-        public InterviewEntity[] Details { get; set; }
-    }
-}
