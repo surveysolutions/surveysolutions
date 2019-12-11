c1af13ddc2d670af40c63aad50933f8c5cbc3f29
Victor Gladkikh <me@alfeg.net>
2019-12-11 18:24:38 +0200

    Added static files builder. Need to extract injections

---
+++ src/UI/WB.UI.Frontend/tools/LocalizationPlugin.js
@@ -0,0 +1,18 @@
+const LocalizationBuilder = require("./localization");
+const PluginName = "LocalizationPlugin";
+
+class LocalizationPlugin {
+  constructor(options) {
+    this.localization = new LocalizationBuilder(options);
+  }
+
+  apply(compiler) {
+    compiler.hooks.beforeRun.tapAsync(PluginName, (params, callback) => {
+      this.localization.prepareLocalizationFiles();
+      params['localization'] = this.localization;
+      callback()
+    });
+  }
+}
+
+module.exports = LocalizationPlugin;
