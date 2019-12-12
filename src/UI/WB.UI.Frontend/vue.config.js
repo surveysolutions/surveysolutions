f757cb8c4fc9bddb64070ed5dc732a307dd36c6d
Victor Hladkikh <me@alfeg.net>
2019-12-12 18:44:33 +0200

    Packages update. Cleanup older signalr

--- src/UI/WB.UI.Frontend/vue.config.js
+++ src/UI/WB.UI.Frontend/vue.config.js
@@ -12,8 +12,10 @@ const assetsPath = isHot ? "http://localhost:8080/" : "~/dist/";
 
 const uiFolder = join("..");
 const hqFolder = path.join(uiFolder, "WB.UI.Headquarters.Core");
+const webTesterFolder = path.join(uiFolder, "WB.UI.WebTester");
 
 const StatsPlugin = require('stats-webpack-plugin')
+const CleanupPlugin = require('clean-webpack-plugin').CleanWebpackPlugin;
 
 const pages = {
     hq: {
@@ -75,10 +77,10 @@ const pages = {
 
 module.exports = {
     pages: Object.assign(pages, {
-        "markup":                   { entry: "src/assets/css/markup.scss" },
-        "markup-specific":          { entry: "src/assets/css/markup-specific.scss" },
-        "markup-web-interview":     { entry: "src/assets/css/markup-web-interview.scss" },
-        "markup-interview-review":  { entry: "src/assets/css/markup-interview-review.scss" }
+        "markup": { entry: "src/assets/css/markup.scss" },
+        "markup-specific": { entry: "src/assets/css/markup-specific.scss" },
+        "markup-web-interview": { entry: "src/assets/css/markup-web-interview.scss" },
+        "markup-interview-review": { entry: "src/assets/css/markup-interview-review.scss" }
     }),
 
     devServer: {
@@ -108,15 +110,15 @@ module.exports = {
                 destination: "./locale/.resources"
             }])
 
-        config.plugin('stats')
-            .use(StatsPlugin, ['stats.json',
-                {
-                    chunks: true,
-                    assets: false,
-                    chunkModules: false,
-                    modules: false,
-                    children: false
-                }]);
+        // config.plugin('stats')
+        //     .use(StatsPlugin, ['stats.json',
+        //         {
+        //             chunks: true,
+        //             assets: false,
+        //             chunkModules: false,
+        //             modules: false,
+        //             children: false
+        //         }]);
 
         config.merge({
             optimization: {
@@ -134,38 +136,51 @@ module.exports = {
         });
 
         config.plugin("provide").use(webpack.ProvidePlugin, [{
-                _: "lodash",
-                $: "jquery",
-                jQuery: "jquery",
-                moment: "moment"
-            }
-        ]);
+            _: "lodash",
+            $: "jquery",
+            jQuery: "jquery",
+            moment: "moment"
+        }]);
+
+        config.plugin('cleanup-dists')
+            .use(CleanupPlugin, [{
+                verbose: true,
+                dangerouslyAllowCleanPatternsOutsideProject: true,
+                dry: false,
+                cleanOnceBeforeBuildPatterns: [
+                    path.join(hqFolder, 'Views', 'Shared', 'partial.*.cshtml'),
+                    path.join(hqFolder, 'wwwroot', 'static', '**', '*.*'),
+                    path.join(webTesterFolder, 'Views', 'Shared', 'partial.*.cshtml')]
+            }]);
 
         config.plugin("runtime").use(RuntimePublicPathPlugin, [{
-                runtimePublicPath: "window.CONFIG.assetsPath"
-            }
-        ]);
+            runtimePublicPath: "window.CONFIG.assetsPath"
+        }]);
 
         config.plugin("fileManager").use(FileManagerPlugin, [
             {
+                verbose: true,
                 onEnd: {
                     copy: [
                         {
-                            source: join(
-                                "locale",
-                                ".resources",
-                                "**",
-                                "*.json"
-                            ),
+                            source: join("locale", ".resources", "**", "*.json"),
                             destination: join("dist")
                         },
                         {
                             source: join("dist", "img", "*.*"),
-                            destination: join(hqFolder, "wwwroot", "static", "img")
+                            destination: path.join(hqFolder, "wwwroot", "static", "img")
                         },
                         {
                             source: join("dist", "fonts", "*.*"),
-                            destination: join(hqFolder, "wwwroot", "static", "fonts")
+                            destination: path.join(hqFolder, "wwwroot", "static", "fonts")
+                        },
+                        {
+                            source: join("dist", "js", "*.*"),
+                            destination: path.join(hqFolder, "wwwroot", "static", "js")
+                        },
+                        {
+                            source: join("dist", "css", "*.*"),
+                            destination: path.join(hqFolder, "wwwroot", "static", "css")
                         }
                     ]
                 }
@@ -196,36 +211,42 @@ module.exports = {
             .set("~", join("src"));
 
         Object.keys(pages).forEach(page => {
-            if(pages[page].filename != null) 
-            config.plugin("html-" + page).tap(args => {
-                const options = args[0];
 
-                const baseTempalateGenerator = options.templateParameters;
+            if (pages[page].filename == null) {
+                config.plugins.delete("html-" + page);
+                config.plugins.delete("prefetch-" + page);
+                config.plugins.delete("preload-" + page);
+            } else {
+                config.plugin("html-" + page).tap(args => {
+                    const options = args[0];
 
-                options.templateParameters = function (compilation, assets, options) {
-                    const result = baseTempalateGenerator(compilation, assets, options)
-                    const localization = compilation.compiler.localization;
-                    const locales = localization.writeFiles(
-                        join("locale", ".resources"),
-                        path.join("locale", page),
-                        pages[page].locales
-                    );
-                    
-                    result.htmlWebpackPlugin.options.locales = {
-                        json: JSON.stringify(locales),
-                        dictionary: localization.getDictionaryDefinition(locales)
-                    };
+                    const baseTempalateGenerator = options.templateParameters;
 
-                    return result
-                }
+                    options.templateParameters = function (compilation, assets, options) {
+                        const result = baseTempalateGenerator(compilation, assets, options)
+                        const localization = compilation.compiler.localization;
+                        const locales = localization.writeFiles(
+                            join("locale", ".resources"),
+                            path.join("locale", page),
+                            pages[page].locales
+                        );
 
-                options.page = page;
-                options.isHot = isHot;
-                options.assetsPath = pages[page].assetsPath || assetsPath;
-                options.minify = false;
-                options.inject = false;
-                return args;
-            });
+                        result.htmlWebpackPlugin.options.locales = {
+                            json: JSON.stringify(locales),
+                            dictionary: localization.getDictionaryDefinition(locales)
+                        };
+
+                        return result
+                    }
+
+                    options.page = page;
+                    options.isHot = isHot;
+                    options.assetsPath = pages[page].assetsPath || assetsPath;
+                    options.minify = false;
+                    options.inject = false;
+                    return args;
+                });
+            }
         });
     }
 };
