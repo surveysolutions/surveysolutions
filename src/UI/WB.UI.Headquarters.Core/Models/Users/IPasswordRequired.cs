11ececac211a13b64221eee78137c4354f6f2544
Roman Lytvyn <litvin.roma@gmail.com>
2019-12-24 14:46:32 +0200

    KP-13399 Manage own profile/manage other profile. Own profile implementation

---
+++ src/UI/WB.UI.Headquarters.Core/Models/Users/IPasswordRequired.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace WB.UI.Headquarters.Models.Users
+{
+    public interface IPasswordRequired
+    {
+        //[PasswordStringLength(100, ErrorMessageResourceName = nameof(FieldsAndValidations.PasswordLengthMessage), ErrorMessageResourceType = typeof(FieldsAndValidations))]
+        //[PasswordRegularExpression(ErrorMessageResourceName = nameof(FieldsAndValidations.PasswordErrorMessage), ErrorMessageResourceType = typeof(FieldsAndValidations))]
+        //[DataType(DataType.Password)]
+        //[Display(Name = nameof(FieldsAndValidations.NewPasswordFieldName), ResourceType = typeof(FieldsAndValidations), Order = 1)]
+        //[Required(ErrorMessageResourceName = nameof(FieldsAndValidations.RequiredPasswordErrorMessage), ErrorMessageResourceType = typeof(FieldsAndValidations))]
+        string Password { get; set; }
+
+        //[DataType(DataType.Password)]
+        //[Compare(nameof(Password), ErrorMessageResourceName = nameof(FieldsAndValidations.ConfirmPasswordErrorMassage), ErrorMessageResourceType = typeof(FieldsAndValidations))]
+        //[Display(Name = nameof(FieldsAndValidations.ConfirmPasswordFieldName), ResourceType = typeof(FieldsAndValidations), Order = 2)]
+        //[Required(ErrorMessageResourceName = nameof(FieldsAndValidations.ConfirmPasswordRequired), ErrorMessageResourceType = typeof(FieldsAndValidations))]
+        string ConfirmPassword { get; set; }
+        
+        Guid Id { get; set; }
+        string UserName { get; set; }
+        string UpdatePasswordAction { get; set; }
+    }
+}
