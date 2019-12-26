7c6ae7befef83123e1d39acd76845252712ef333
Pavel Stovpivskyi <630198+spuv@users.noreply.github.com>
2019-12-26 19:17:08 +0200

    KP-13405 moved html to vue (not working)

--- src/UI/WB.UI.Headquarters.Core/Models/Reports/QuantityBySupervisorsReportModel.cs
+++ src/UI/WB.UI.Headquarters.Core/Models/Reports/QuantityBySupervisorsReportModel.cs
@@ -1,16 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using WB.Core.BoundedContexts.Headquarters.Views.Reposts;
-using WB.Core.GenericSubdomains.Portable;
+using WB.UI.Headquarters.Models.Api;
 
 namespace WB.Core.SharedKernels.SurveyManagement.Web.Models
 {
-    public class QuantityBySupervisorsReportModel
+    public class QuantityBySupervisorsReportModel : DataTableRequest
     {
-        public int PageIndex { get; set; }
-        public int PageSize { get; set; }
-        public IEnumerable<OrderRequestItem> SortOrder { get; set; }
-
         public DateTime From { get; set; }
         public Guid? QuestionnaireId { get; set; }
         public long? QuestionnaireVersion { get; set; }
