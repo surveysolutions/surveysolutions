b64ebcb835bd78a9947c55687fecc6831537398f
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2019-12-30 16:22:44 -0500

    KP-13505 Columns are shifted regarding to headers [fix width for elements]

--- src/UI/Headquarters/WB.UI.Headquarters/HqApp/src/webinterview/components/questions/MatrixRoster.CategoricalSingle.vue
+++ src/UI/Headquarters/WB.UI.Headquarters/HqApp/src/webinterview/components/questions/MatrixRoster.CategoricalSingle.vue
@@ -17,8 +17,8 @@
             </template>
 
         </popover>        
-            <div class="radio cell-bordered" v-for="option in editorParams.question.options" :key="$me.id + '_' + option.value">
-                    <div style="width:221px;" class="field"> 
+            <div class="radio cell-bordered"  v-for="option in editorParams.question.options" :key="$me.id + '_' + option.value">
+                    <div  class="field" style="width:219px;"> 
                         <input v-if="answeredOrAllOptions.some(e => e.value === option.value)" class="wb-radio" type="radio" 
                           :id="`${$me.id}_${option.value}`" 
                           :name="$me.id" 
