cb9f929168c973446e7bcfe2ad6a2f018460bf4a
Victor Hladkikh <me@alfeg.net>
2019-12-30 18:58:25 +0200

    KP-13412 Libs update and netstandard2.1 adoption

--- src/Core/Infrastructure/WB.Core.Infrastructure/Modularity/Autofac/ModuleExtensions.cs
+++ src/Core/Infrastructure/WB.Core.Infrastructure/Modularity/Autofac/ModuleExtensions.cs
@@ -1,8 +1,10 @@
-﻿namespace WB.Core.Infrastructure.Modularity.Autofac
+﻿using Microsoft.Extensions.Configuration;
+
+namespace WB.Core.Infrastructure.Modularity.Autofac
 {
     public static class ModuleExtensions
     {
-        public static global::Autofac.Core.IModule AsAutofac(this Core.Infrastructure.Modularity.IModule ourModule)
+        public static global::Autofac.Core.IModule AsAutofac(this IModule ourModule)
         {
             return new AutofacModuleAdapter(ourModule);
         }
