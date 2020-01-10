e2055e2313aef6d32adf259bd00a86484ad57644
Roman Lytvyn <litvin.roma@gmail.com>
2020-01-10 13:22:40 +0200

    KP-13399 Manage own profile/manage other profile. Inputs and error handling

---
+++ src/UI/WB.UI.Frontend/src/hqapp/components/FormGroup.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="block-filter">
+        <label v-if="label !== undefined">{{ label }}</label>
+        <div class="form-group" :class="{'has-error': error !== undefined}">
+            <slot></slot>
+            <span v-if="error !== undefined" class="help-block field-validation-error">
+                <span>{{ error }}</span>
+            </span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['label', 'error'],
+}
+</script>
