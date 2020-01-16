b602a8dd2d3d98077fa6d1ce70266b263a291023
Andrii Kozhyn <hedgic@gmail.com>
2020-01-16 12:26:31 +0200

    KP-13492 added serilog

--- src/UI/WB.UI.Headquarters.Core/Program.cs
+++ src/UI/WB.UI.Headquarters.Core/Program.cs
@@ -1,26 +1,58 @@
 using System;
+using System.IO;
 using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
+using WB.Core.Infrastructure.Versions;
 
 namespace WB.UI.Headquarters
 {
     public class Program
     {
-        public static void Main(string[] args) => CreateHostBuilder(args).Build().Run();
+        public static void Main(string[] args)
+        {
+            var appRoot = Path.GetDirectoryName(typeof(Program).Assembly.Location);
+            var logsFileLocation = Path.Combine(appRoot, "..", "logs", "log.log");
+            var verboseLog = Path.Combine(appRoot, "..", "logs", "verbose.log");
+
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .WriteTo.File(logsFileLocation, rollingInterval: RollingInterval.Day, 
+                    restrictedToMinimumLevel: LogEventLevel.Warning)
+                .WriteTo.File(verboseLog, rollingInterval: RollingInterval.Day,
+                    restrictedToMinimumLevel: LogEventLevel.Verbose, retainedFileCountLimit: 2)
+                .CreateLogger();
+
+            try
+            {
+                var host = CreateHostBuilder(args).Build();
+
+                var version = host.Services.GetRequiredService<IProductVersion>();
+                var applicationVersion = version.ToString();
+
+                Log.Logger.Warning("HQ application starting. Version {version}", applicationVersion);
+
+                host.Run();
+            }
+            catch (Exception e)
+            {
+                Log.Fatal(e, "Host terminated unexpectedly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureLogging(log =>
-                {
-                    log.ClearProviders()
-                        .AddConsole()
-                        .AddFilter("Microsoft", LogLevel.Warning)
-                        .SetMinimumLevel(LogLevel.Warning);
-                })
-                
+                .UseSerilog()
                 .UseServiceProviderFactory(new AutofacServiceProviderFactory())
                 .ConfigureAppConfiguration((hostingContext, c) =>
                 {
