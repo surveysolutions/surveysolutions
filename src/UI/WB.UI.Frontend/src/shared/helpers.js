f03f0d884f752581e4c51e96612b29322d7e1c02
Andrii Kozhyn <hedgic@gmail.com>
2020-01-20 15:35:08 +0200

    KP-13584 interviewer profile migration to .net core

--- src/UI/WB.UI.Frontend/src/shared/helpers.js
+++ src/UI/WB.UI.Frontend/src/shared/helpers.js
@@ -9,3 +9,18 @@ export const DateFormats = {
     date: 'YYYY-MM-DD',
     dateTimeInList: 'MMM DD, YYYY HH:mm'
 }
+export function humanFileSize(bytes, si) {
+    var thresh = si ? 1000 : 1024;
+    if(Math.abs(bytes) < thresh) {
+        return bytes + ' B';
+    }
+    var units = si
+        ? ['kB','MB','GB','TB','PB','EB','ZB','YB']
+        : ['KiB','MiB','GiB','TiB','PiB','EiB','ZiB','YiB'];
+    var u = -1;
+    do {
+        bytes /= thresh;
+        ++u;
+    } while(Math.abs(bytes) >= thresh && u < units.length - 1);
+    return bytes.toFixed(1)+' '+units[u];
+}
