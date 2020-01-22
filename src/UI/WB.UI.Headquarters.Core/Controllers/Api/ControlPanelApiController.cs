83a24755d04205b091d7b99c6f660dbd0e808a92
Andrii Kozhyn <hedgic@gmail.com>
2020-01-22 15:17:26 +0200

    KP-13591 package infos

--- src/UI/WB.UI.Headquarters.Core/Controllers/Api/ControlPanelApiController.cs
+++ src/UI/WB.UI.Headquarters.Core/Controllers/Api/ControlPanelApiController.cs
@@ -1,26 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using WB.Core.BoundedContexts.Headquarters.Services;
 using WB.Core.BoundedContexts.Headquarters.Views.TabletInformation;
+using WB.Core.BoundedContexts.Headquarters.Views.User;
 using WB.UI.Headquarters.Models.Api;
 
 namespace WB.UI.Headquarters.Controllers.Api
 {
-    [Authorize(Roles="Administrator")]
+    [Authorize(Roles = "Administrator")]
     [Route("api/{controller}/{action}/{id?}")]
     public class ControlPanelApiController : ControllerBase
     {
         private readonly IConfiguration configuration;
         private static readonly Regex ConnectionStringPasswordRegex = new Regex("password=([^;]*);", RegexOptions.IgnoreCase | RegexOptions.Compiled);
         private readonly ITabletInformationService tabletInformationService;
-
-
-        public ControlPanelApiController(IConfiguration configuration, 
+        
+        public ControlPanelApiController(IConfiguration configuration,
             ITabletInformationService tabletInformationService)
         {
             this.configuration = configuration;
@@ -35,7 +38,12 @@ namespace WB.UI.Headquarters.Controllers.Api
             if (!string.IsNullOrEmpty(request.Search?.Value))
                 items = items.Where(x => x.UserName != null && x.UserName.StartsWith(request.Search.Value, StringComparison.OrdinalIgnoreCase)).ToList();
 
-            var itemsSlice = items.Skip((request.PageIndex - 1) * request.PageSize).Take(request.PageSize);
+            var itemsSlice = items.Skip((request.PageIndex - 1) * request.PageSize).Take(request.PageSize).ToList();
+
+            foreach (var tabletInformationView in itemsSlice)
+            {
+                tabletInformationView.DownloadUrl = Url.Action("Download", new {id=tabletInformationView.PackageName});
+            }
             return new DataTableResponse<TabletInformationView>
             {
                 Data = itemsSlice,
@@ -59,6 +67,15 @@ namespace WB.UI.Headquarters.Controllers.Api
 
         private static string RemovePasswordFromConnectionString(string connectionString)
             => ConnectionStringPasswordRegex.Replace(connectionString, "Password=*****;");
+
+        public IActionResult Download(string id)
+        {
+            var hostName = this.Request.Host.ToString().Split('.').FirstOrDefault() ?? @"unknownhost";
+            var fullPathToContentFile = this.tabletInformationService.GetFullPathToContentFile(id);
+
+            return this.File(System.IO.File.OpenRead(fullPathToContentFile), "application/zip",
+                this.tabletInformationService.GetFileName(id, hostName));
+        }
     }
 }
 
