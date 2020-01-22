ebe2292a963b6795330cf1ae961f53bee0b67617
Pavel Stovpivskyi <630198+spuv@users.noreply.github.com>
2020-01-22 18:08:32 +0200

    KP-13597 Download page

--- src/UI/WB.UI.Headquarters.Core/Controllers/DownloadController.cs
+++ src/UI/WB.UI.Headquarters.Core/Controllers/DownloadController.cs
@@ -1,37 +1,49 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using WB.Core.BoundedContexts.Headquarters.Services;
+using WB.UI.Headquarters.API;
 using WB.UI.Headquarters.Models;
+using WB.UI.Headquarters.Services;
 
 namespace WB.UI.Headquarters.Controllers
 {
     public class DownloadController : Controller
     {
-        public DownloadController(IQRCodeHelper qRCodeHelper)
+        private readonly IQRCodeHelper qRCodeHelper;
+        private readonly IClientApkProvider clientApkProvider;
+
+        public DownloadController(IQRCodeHelper qRCodeHelper, IClientApkProvider clientApkProvider)
         {
             this.qRCodeHelper = qRCodeHelper;
+            this.clientApkProvider = clientApkProvider;
         }
 
-        private IQRCodeHelper qRCodeHelper;
-
-        // GET: Download
         public IActionResult Index()
         {
-            var model = new DownloadModel();
-            var noMapsApkUrl = Url.RouteUrl(new { action = "GetLatestVersion", controller = "InterviewerSync" });
-            var mapsApkUrl = Url.RouteUrl(new { action = "GetLatestExtendedVersion", controller = "InterviewerSync" });
+            var smallApkUrl = Url.Action("GetLatestVersion", "DownloadApi");
+            var fullApkUrl = Url.Action("GetLatestExtendedVersion", "DownloadApi");
 
-            model.QRBase64Apk = qRCodeHelper.GetQRCodeAsBase64StringSrc(qRCodeHelper.GetFullUrl(noMapsApkUrl), 250, 250);
-            model.QRBase64ApkWithMaps = qRCodeHelper.GetQRCodeAsBase64StringSrc(qRCodeHelper.GetFullUrl(mapsApkUrl), 250, 250);
-
-            return View(model);
+            return View(new
+            {
+                SupportQRCodeGeneration = qRCodeHelper.SupportQRCodeGeneration(),
+                SmallApkQRUrl = qRCodeHelper.GetQRCodeAsBase64StringSrc(qRCodeHelper.GetFullUrl(smallApkUrl), 250, 250),
+                FullApkQRUrl = qRCodeHelper.GetQRCodeAsBase64StringSrc(qRCodeHelper.GetFullUrl(fullApkUrl), 250, 250),
+                SmallApkUrl = smallApkUrl,
+                FullApkUrl = fullApkUrl,
+                SmallApkVersion = clientApkProvider.GetApplicationVersionString(ClientApkInfo.InterviewerFileName),
+                FullApkVersion = clientApkProvider.GetApplicationVersionString(ClientApkInfo.InterviewerExtendedFileName),
+            });
         }
 
         public IActionResult Supervisor()
         {
-            var model = new DownloadModel();
-            var httpRouteUrl = Url.RouteUrl(new { action = "GetLatestSupervisor", controller = "InterviewerSync" });
-            model.QRBase64Apk = qRCodeHelper.GetQRCodeAsBase64StringSrc(qRCodeHelper.GetFullUrl(httpRouteUrl), 250, 250);
-            return View(model);
+            var apkUrl = Url.Action("GetLatestSupervisor", "DownloadApi");
+            return View(new
+            {
+                SupportQRCodeGeneration = qRCodeHelper.SupportQRCodeGeneration(),
+                ApkQRUrl = qRCodeHelper.GetQRCodeAsBase64StringSrc(qRCodeHelper.GetFullUrl(apkUrl), 250, 250),
+                ApkUrl = apkUrl,
+                SupervisorVersion = clientApkProvider.GetApplicationVersionString(ClientApkInfo.SupervisorFileName)
+            });
         }
     }
 }
