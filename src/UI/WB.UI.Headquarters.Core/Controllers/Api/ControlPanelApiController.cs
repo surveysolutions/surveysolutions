4810001d45f81188c46ac3de7fbbdf6cb45fb95c
Andrii Kozhyn <andrii.kozhyn@outlook.com>
2020-01-23 18:30:39 +0200

    KP-13592 apks page

--- src/UI/WB.UI.Headquarters.Core/Controllers/Api/ControlPanelApiController.cs
+++ src/UI/WB.UI.Headquarters.Core/Controllers/Api/ControlPanelApiController.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using Humanizer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -11,7 +13,9 @@ using Microsoft.Extensions.Configuration;
 using WB.Core.BoundedContexts.Headquarters.Services;
 using WB.Core.BoundedContexts.Headquarters.Views.TabletInformation;
 using WB.Core.BoundedContexts.Headquarters.Views.User;
+using WB.Core.Infrastructure.FileSystem;
 using WB.UI.Headquarters.Models.Api;
+using WB.UI.Headquarters.Services;
 
 namespace WB.UI.Headquarters.Controllers.Api
 {
@@ -22,12 +26,21 @@ namespace WB.UI.Headquarters.Controllers.Api
         private readonly IConfiguration configuration;
         private static readonly Regex ConnectionStringPasswordRegex = new Regex("password=([^;]*);", RegexOptions.IgnoreCase | RegexOptions.Compiled);
         private readonly ITabletInformationService tabletInformationService;
+        private readonly IClientApkProvider clientApkProvider;
+        private readonly IFileSystemAccessor fileSystemAccessor;
+        private readonly IAndroidPackageReader androidPackageReader;
         
         public ControlPanelApiController(IConfiguration configuration,
-            ITabletInformationService tabletInformationService)
+            ITabletInformationService tabletInformationService, 
+            IClientApkProvider clientApkProvider, 
+            IFileSystemAccessor fileSystemAccessor, 
+            IAndroidPackageReader androidPackageReader)
         {
             this.configuration = configuration;
             this.tabletInformationService = tabletInformationService;
+            this.clientApkProvider = clientApkProvider;
+            this.fileSystemAccessor = fileSystemAccessor;
+            this.androidPackageReader = androidPackageReader;
         }
 
 
@@ -51,6 +64,52 @@ namespace WB.UI.Headquarters.Controllers.Api
                 RecordsFiltered = items.Count,
                 RecordsTotal = items.Count
             };
+        }
+        
+        public ActionResult<List<ApkInfo>> AppUpdates()
+        {
+            var folder = clientApkProvider.ApkClientsFolder();
+            var appFiles = Directory.EnumerateFiles(folder);
+
+            var enumerable = appFiles
+                .Select(app => new FileInfo(app))
+                .OrderBy(fi => fi.Name)
+                .Select(fi =>
+                {
+                    var result = new ApkInfo
+                    {
+                        FileName = fi.Name
+                    };
+
+                    if (fi.Name.EndsWith(".apk", StringComparison.OrdinalIgnoreCase))
+                    {
+                        var build = this.androidPackageReader.Read(fi.FullName)?.BuildNumber;
+                        if(build != null) 
+                            result.Build = build;
+                        var hash = Convert.ToBase64String(this.fileSystemAccessor.ReadHash(fi.FullName));
+                        result.Hash = hash;
+                    }
+
+                    result.FileSizeInBytes = fi.Length;
+                    result.LastWriteTimeUtc = fi.LastWriteTimeUtc;
+
+                    return result;
+                });
+            
+            return enumerable.ToList();
+        }
+
+        public class ApkInfo
+        {
+            public string FileName { get; set; }
+            
+            public int? Build { get; set; }
+            
+            public string Hash { get; set; }
+            
+            public long FileSizeInBytes { get; set; }
+            
+            public DateTime LastWriteTimeUtc { get; set; }
         }
 
         public ActionResult<List<KeyValuePair<string, string>>> Configuration()
