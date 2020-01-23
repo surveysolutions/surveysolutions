be05a18027111edd4ba84d5505b0c9eb63ff20a5
Roman Lytvyn <litvin.roma@gmail.com>
2020-01-23 14:31:30 +0200

    KP-13495 Batch assignments preloading page. Final implementation

--- src/UI/WB.UI.Frontend/src/hqapp/Views/HQ/Assignments/index.js
+++ src/UI/WB.UI.Frontend/src/hqapp/Views/HQ/Assignments/index.js
@@ -5,9 +5,10 @@ import Details from "./Details"
 import Upload from "./Upload/Index"
 import UploadErrors from "./Upload/Errors"
 import UploadVerification from "./Upload/Verification"
-import UploadImport from "./Upload/Import"
-import UploadComplete from "./Upload/Complete"
+import UploadProgress from "./Upload/Progress"
 import localStore from "./store"
+
+import config from "~/shared/config"
 
 import Vue from "vue"
 export default class AssignmentsComponent {
@@ -32,41 +33,73 @@ export default class AssignmentsComponent {
                         path: ':assignmentId', component: Details
                     },
                     {
-                        path: 'Upload/:questionnaireId', 
-                        component: Upload,
-                        name: "assignments-upload"
-                        // beforeEnter: (to, from, next) => {
-                        //     Vue.$http
-                        //         .get(config.model.api.importStatusUrl)
-                        //         .then(response => {
-                        //             self.rootStore.dispatch("setUploadStatus", response.data);
-                        //             if (response.data.isInProgress && response.data.isOwnerOfRunningProcess)
-                        //                 next({ name: "uploadassignmentsprogress" });
-                        //             else next()
-                        //         })
-                        //         .catch(() => next());
-                        // }
-                    },
-                    {
-                        path: 'Upload/Errors', 
-                        component: UploadErrors,
-                        name: 'assignments-upload-errors',
-                        beforeEnter: (to, from, next) => {
-                            if (self.rootStore.getters.upload.fileName == "")
-                                next({ name: "assignments-upload" })
-                            else next()
-                        }
-                    },
-                    {
-                        path: 'Upload/Verification', 
-                        component: UploadVerification,
-                        name: 'assignments-upload-verification',
-                    },
-                    {
-                        path: 'Upload/Import', 
-                        component: UploadImport,
-                        name: 'assignments-upload-import',
-                    }]
+                        path: 'Upload', component: Layout,
+                        children: [
+
+                            {
+                                path: ':questionnaireId',
+                                component: Upload,
+                                name: "assignments-upload",
+                                beforeEnter: (to, from, next) => {
+                                    if (config.model.status != null && config.model.status.isOwnerOfRunningProcess) {
+                                        if (config.model.status.processStatus == 2 /*Verification*/)
+                                            next({ name: "assignments-upload-verification" });
+                                        else if (config.model.status.processStatus == 3 /*Import*/ ||
+                                            config.model.status.processStatus == 4 /*ImportCompleted*/)
+                                            next({ name: "assignments-upload-progress" });
+                                        else next()
+                                    }
+                                    else next()
+                                }
+                            },
+                            {
+                                path: 'Errors',
+                                component: UploadErrors,
+                                name: 'assignments-upload-errors',
+                                beforeEnter: (to, from, next) => {
+                                    if (self.rootStore.getters.upload.fileName == "")
+                                        next({ name: "assignments-upload" })
+                                    else next()
+                                }
+                            },
+                            {
+                                path: 'Verification',
+                                component: UploadVerification,
+                                name: 'assignments-upload-verification',
+                                beforeEnter: (to, from, next) => {
+                                    var status = self.rootStore.getters.upload.progress ?? config.model.status
+                                    if (status != null && status.isOwnerOfRunningProcess) {
+                                        if (status.processStatus == 3 /*Import*/ || status.processStatus == 4 /*ImportCompleted*/)
+                                            next({ name: "assignments-upload-progress" });
+                                        else {
+                                            self.rootStore.dispatch('setUploadStatus', status)
+                                            next()
+                                        }
+                                    }
+                                    else next({ name: "assignments-upload" })
+                                }
+                            },
+                            {
+                                path: 'Progress',
+                                component: UploadProgress,
+                                name: 'assignments-upload-progress',
+                                beforeEnter: (to, from, next) => {
+                                    var status = self.rootStore.getters.upload.progress ?? config.model.status
+                                    if (status != null && status.isOwnerOfRunningProcess) {
+                                        if (status.processStatus == 2 /*Verification*/)
+                                            next({ name: "assignments-upload-verification" });
+                                        else {
+                                            self.rootStore.dispatch('setUploadStatus', status)
+                                            next()
+                                        }
+                                    }
+                                    else next({ name: "assignments-upload" })
+                                }
+                            }
+                        ]
+
+                    }
+                ]
             }];
     }
 
