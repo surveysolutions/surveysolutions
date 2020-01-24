72274235ab456eb4de1135aafa2c8bc1f8566f8a
Andrii Kozhyn <andrii.kozhyn@outlook.com>
2020-01-24 10:53:36 +0200

    KP-13591 fixed page title

--- src/UI/WB.UI.Frontend/src/hqapp/Views/HQ/ControlPanel/ControlPanelLayout.vue
+++ src/UI/WB.UI.Frontend/src/hqapp/Views/HQ/ControlPanel/ControlPanelLayout.vue
@@ -1,25 +1,18 @@
 <template>
-    <HqLayout :hasFilter="true" :hasRow="false">
-        <aside slot="filters" class="filters">
-            <div class="foldback-button" id="hide-filters">
-                <span class="arrow"></span>
-                <span class="arrow"></span>
-                <span class="glyphicon glyphicon-tasks" aria-hidden="true"></span>
-            </div>
-            <div class="filters-container">
-                <ol class="list-unstyled">
-                    <li>
-                        <router-link to="/ControlPanel/Configuration">{{$t('Pages.Configuration')}}</router-link>
-                    </li>
-                    <li>
-                        <router-link to="/ControlPanel/TabletInfos">{{$t('Pages.PackagesInfo_Header')}}</router-link>
-                    </li>
-                    <li>
-                        <router-link to="/ControlPanel/AppUpdates">{{$t('Pages.AppUpdates')}}</router-link>
-                    </li>
-                </ol>
-            </div>
-        </aside>
+    <HqLayout :hasFilter="true">
+        <Filters slot="filters" :title="''">
+            <ol class="list-unstyled">
+                <li>
+                    <router-link to="/ControlPanel/Configuration">{{$t('Pages.Configuration')}}</router-link>
+                </li>
+                <li>
+                    <router-link to="/ControlPanel/TabletInfos">{{$t('Pages.PackagesInfo_Header')}}</router-link>
+                </li>
+                <li>
+                    <router-link to="/ControlPanel/AppUpdates">{{$t('Pages.AppUpdates')}}</router-link>
+                </li>
+            </ol>
+        </Filters>
         <router-view />
     </HqLayout>
 </template>
