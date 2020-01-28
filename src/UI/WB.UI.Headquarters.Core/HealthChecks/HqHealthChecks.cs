927fbcb21d280f5dc459f36399b0fab8295742eb
Victor Hladkikh <me@alfeg.net>
2020-01-28 16:11:42 +0200

    KP-13442 Added S3 configuration check on startup

---
+++ src/UI/WB.UI.Headquarters.Core/HealthChecks/HqHealthChecks.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace WB.UI.Headquarters.HealthChecks
+{
+    public static class HqHealthChecks
+    {
+        public static void AddHeadquartersHealthCheck(this IServiceCollection services)
+        {
+            services.AddHostedService<AmazonS3CheckService>();
+        }
+    }
+}
