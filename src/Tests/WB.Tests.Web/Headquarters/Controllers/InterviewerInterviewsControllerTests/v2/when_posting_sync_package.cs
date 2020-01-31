272aed647bf1c299b5ecf71f1e5027d2ecf4c07f
Andrii Kozhyn <andrii.kozhyn@outlook.com>
2020-01-31 11:31:20 +0200

    KP-13479 implemented some controller tests

--- src/Tests/WB.Tests.Web/Headquarters/Controllers/InterviewerInterviewsControllerTests/v2/when_posting_sync_package.cs
+++ src/Tests/WB.Tests.Web/Headquarters/Controllers/InterviewerInterviewsControllerTests/v2/when_posting_sync_package.cs
@@ -5,36 +5,42 @@ using WB.Core.BoundedContexts.Headquarters.Views;
 using WB.Core.SharedKernel.Structures.Synchronization;
 using WB.Core.SharedKernels.DataCollection.ValueObjects.Interview;
 using WB.Core.SharedKernels.DataCollection.WebApi;
-using WB.UI.Headquarters.API.DataCollection.Interviewer.v2;
+using WB.UI.Headquarters.Controllers.Api.DataCollection.Interviewer.v2;
 
-
-namespace WB.Tests.Unit.SharedKernels.SurveyManagement.Web.ApiTests.InterviewerInterviewsControllerTests.v2
+namespace WB.Tests.Web.Headquarters.Controllers.InterviewerInterviewsControllerTests.v2
 {
     internal class when_posting_sync_package : InterviewsApiV2ControllerTestsContext
     {
-        [NUnit.Framework.OneTimeSetUp] public void context () {
+        [NUnit.Framework.OneTimeSetUp]
+        public void context()
+        {
             controller = CreateInterviewerInterviewsController(
                 incomingSyncPackagesQueue: mockOfInterviewPackagesService.Object);
             BecauseOf();
         }
 
-        public void BecauseOf() => controller.Post(new InterviewPackageApiView { InterviewId = interviewId, Events = eventsInJsonString, MetaInfo = interviewMetaInfo});
+        public void BecauseOf() => controller.Post(new InterviewPackageApiView
+            {InterviewId = interviewId, Events = eventsInJsonString, MetaInfo = interviewMetaInfo});
 
-        [NUnit.Framework.Test] public void should_store_package_to_storage () =>
+        [NUnit.Framework.Test]
+        public void should_store_package_to_storage() =>
             mockOfInterviewPackagesService.Verify(x =>
                 x.StoreOrProcessPackage(Moq.It.IsAny<InterviewPackage>()), Times.Once);
 
         private static InterviewsApiV2Controller controller;
         private static readonly Guid interviewId = Guid.Parse("11111111111111111111111111111111");
         private static readonly string eventsInJsonString = "serialized events";
+
         private static readonly InterviewMetaInfo interviewMetaInfo = new InterviewMetaInfo
         {
             TemplateId = Guid.Parse("22222222222222222222222222222222"),
             TemplateVersion = 111,
-            Status = (int)InterviewStatus.Restarted,
+            Status = (int) InterviewStatus.Restarted,
             ResponsibleId = Guid.Parse("33333333333333333333333333333333"),
             CreatedOnClient = true
         };
-        private static readonly Mock<IInterviewPackagesService> mockOfInterviewPackagesService = new Mock<IInterviewPackagesService>();
+
+        private static readonly Mock<IInterviewPackagesService> mockOfInterviewPackagesService =
+            new Mock<IInterviewPackagesService>();
     }
 }
--- src/Tests/WB.Tests.Web/Headquarters/Controllers/PeriodicStatusReportTests/when_request_quantity_report_by_supervisors.cs
+++ src/Tests/WB.Tests.Web/Headquarters/Controllers/PeriodicStatusReportTests/when_request_quantity_report_by_supervisors.cs
@@ -1,5 +1,5 @@
-using System.Web.Mvc;
 using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
 using WB.Core.BoundedContexts.Headquarters.Views.Reposts;
 using WB.Core.SharedKernels.SurveyManagement.Web.Models;
 using WB.Tests.Web;
@@ -18,17 +18,11 @@ namespace WB.Tests.Unit.SharedKernels.SurveyManagement.PeriodicStatusReportTests
         public void BecauseOf() =>
             result = reportController.QuantityBySupervisors(PeriodiceReportType.NumberOfCompletedInterviews) as ViewResult;
 
-        [NUnit.Framework.Test] public void should_active_page_be_NumberOfCompletedInterviews () =>
-            ((MenuItem)result.ViewBag.ActivePage).Should().Be(MenuItem.NumberOfCompletedInterviews);
-
         [NUnit.Framework.Test] public void should_responsible_name_be_a_link () =>
             ((PeriodicStatusReportModel)result.Model).CanNavigateToQuantityByTeamMember.Should().Be(true);
 
         [NUnit.Framework.Test] public void should_go_back_to_supervisor_button_be_invisible () =>
             ((PeriodicStatusReportModel)result.Model).CanNavigateToQuantityBySupervisors.Should().Be(false);
-
-        [NUnit.Framework.Test] public void should_WebApiActionName_be_BySupervisors () =>
-           ((PeriodicStatusReportModel)result.Model).WebApiActionName.Should().Be(PeriodicStatusReportWebApiActionName.BySupervisors);
 
         [NUnit.Framework.Test] public void should_ReportName_be_Quantity () =>
            ((PeriodicStatusReportModel)result.Model).ReportName.Should().Be("Quantity");
