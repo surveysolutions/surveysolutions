e687af1457de2216fe6a1770852acd4021cd962f
Pavel Stovpivskyi <sby@rambler.ru>
2020-02-04 19:57:46 +0200

    KP-13668 added progress based on operations

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Services/QuestionnaireImportResult.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Services/QuestionnaireImportResult.cs
@@ -10,9 +10,17 @@ namespace WB.Core.BoundedContexts.Headquarters.Services
         public string ImportError { get; set; }
         public string QuestionnaireId { get; set; }
         public QuestionnaireIdentity Identity { get; set; }
-        public int Percent { get; set; }
+        public Progress Progress { get; set; }
         public QuestionnaireImportStatus Status { get; set; }
         public Guid? MigrateAssignmentProcessId { get; set; }
+    }
+
+    public class Progress
+    {
+        public int Current { get; set; } = 0;
+        public int Total { get; set; } = 100;
+        public double Percent => Current * 100 / Total;
+
     }
 
     public enum QuestionnaireImportStatus
