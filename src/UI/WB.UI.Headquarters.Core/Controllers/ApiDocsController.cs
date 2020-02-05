e5b15f04eac0302e0e531904657a9a935aeff265
Pavel Stovpivskyi <sby@rambler.ru>
2020-02-05 16:10:17 +0200

    KP-13692 Swagger doesn't work

---
+++ src/UI/WB.UI.Headquarters.Core/Controllers/ApiDocsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WB.UI.Headquarters.Controllers
+{
+    [Route("apidocs")]
+    [ApiController]
+    public class ApiDocsController : ControllerBase
+    {
+        [Route("")]
+        [Route("index")]
+        [Route("index.html")]
+        public ContentResult Index()
+        {
+            var pathToHtml = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "apidocs", "index.html");
+            var content = System.IO.File.ReadAllText(pathToHtml);
+            return new ContentResult
+            {
+                ContentType = "text/html",
+                StatusCode = (int)HttpStatusCode.OK,
+                Content = content
+            };
+        }
+    }
+}
