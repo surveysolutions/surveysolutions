4324bd6bd039501f3b3fa173480f9b391e28cd26
Pavel Stovpivskyi <sby@rambler.ru>
2020-02-05 19:50:20 +0200

    KP-13692 changes after code review

--- src/UI/WB.UI.Headquarters.Core/Controllers/ApiDocsController.cs
+++ src/UI/WB.UI.Headquarters.Core/Controllers/ApiDocsController.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,19 +14,21 @@ namespace WB.UI.Headquarters.Controllers
     [ApiController]
     public class ApiDocsController : ControllerBase
     {
+        private readonly IWebHostEnvironment hostEnvironment;
+
+        public ApiDocsController(IWebHostEnvironment  hostEnvironment)
+        {
+            this.hostEnvironment = hostEnvironment;
+        }
+
         [Route("")]
         [Route("index")]
         [Route("index.html")]
-        public ContentResult Index()
+        public IActionResult Index()
         {
-            var pathToHtml = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "apidocs", "index.html");
-            var content = System.IO.File.ReadAllText(pathToHtml);
-            return new ContentResult
-            {
-                ContentType = "text/html",
-                StatusCode = (int)HttpStatusCode.OK,
-                Content = content
-            };
+            var pathToHtml = Path.Combine(hostEnvironment.WebRootPath, "apidocs", "index.html");
+            var fileStream = System.IO.File.OpenRead(pathToHtml);
+            return File(fileStream, "text/html");
         }
     }
 }
