6d49903c3b5286bb870794f4e178bd657b8206dd
Andrii Kozhyn <hedgic@gmail.com>
2020-02-05 19:07:30 +0200

    KP-13452 fixed basic authentication

--- src/UI/WB.UI.Headquarters.Core/Code/Authentication/BasicAuthenticationHandler.cs
+++ src/UI/WB.UI.Headquarters.Core/Code/Authentication/BasicAuthenticationHandler.cs
@@ -1,27 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using WB.Core.BoundedContexts.Headquarters.Views.User;
+using WB.Core.GenericSubdomains.Portable;
 using WB.UI.Shared.Web.Authentication;
 
 namespace WB.UI.Headquarters.Code.Authentication
 {
     public class BasicAuthenticationHandler : AuthenticationHandler<BasicAuthenticationSchemeOptions>
     {
-        private readonly SignInManager<HqUser> signInManager;
+        private readonly UserManager<HqUser> userManager;
 
         public BasicAuthenticationHandler(
             IOptionsMonitor<BasicAuthenticationSchemeOptions> options, 
             ILoggerFactory logger, 
             UrlEncoder encoder, 
             ISystemClock clock,
-            SignInManager<HqUser> signInManager) : base(options, logger, encoder, clock)
+            UserManager<HqUser> userManager) : base(options, logger, encoder, clock)
         {
-            this.signInManager = signInManager ?? throw new ArgumentNullException(nameof(signInManager));
+            this.userManager = userManager ?? throw new ArgumentNullException(nameof(userManager));
         }
 
         protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
@@ -38,15 +42,25 @@ namespace WB.UI.Headquarters.Code.Authentication
             {
                 return AuthenticateResult.Fail(e.Message);
             }
-            
-            var signinResult = await signInManager.PasswordSignInAsync(creds.Username, creds.Password, false, false);
-            if (!signinResult.Succeeded)
-            {
-                return AuthenticateResult.Fail(string.Empty);
-            }
 
-            var claimsPrincipal = signInManager.Context.User;
-            var ticket = new AuthenticationTicket(claimsPrincipal, Scheme.Name);
+            var user = await this.userManager.FindByNameAsync(creds.Username);
+            if(user == null) return AuthenticateResult.Fail("No user found");
+
+            var passwordIsValid = await this.userManager.CheckPasswordAsync(user, creds.Password);
+            if(!passwordIsValid) return AuthenticateResult.Fail("Invalid password");
+
+            var claims = new List<Claim> {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.FormatGuid()),
+                new Claim(ClaimTypes.Name, user.UserName),
+            };
+            foreach (var userRole in user.Roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, userRole.Name));
+            }
+            
+            var identity = new ClaimsIdentity(claims, Scheme.Name);
+            var principal = new ClaimsPrincipal(identity);
+            var ticket = new AuthenticationTicket(principal, Scheme.Name);
 
             return AuthenticateResult.Success(ticket);
         }
@@ -54,12 +68,14 @@ namespace WB.UI.Headquarters.Code.Authentication
         protected override Task HandleChallengeAsync(AuthenticationProperties properties)
         {
             HandleFail();
+            Response.StatusCode = StatusCodes.Status401Unauthorized;
             return Task.CompletedTask;
         }
 
         protected override Task HandleForbiddenAsync(AuthenticationProperties properties)
         {
             HandleFail();
+            Response.StatusCode = StatusCodes.Status403Forbidden;
             return Task.CompletedTask;
         }
 
