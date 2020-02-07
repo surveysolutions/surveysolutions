107fb5b61f1c6a0d1fd3a8161fb03bfef7dc5145
Pavel Stovpivskyi <sby@rambler.ru>
2020-02-07 14:37:07 +0200

    KP-13701 UTC date format is used for users 'Account created' field

--- src/UI/WB.UI.Frontend/src/hqapp/Views/HQ/Users/ApiUsers.vue
+++ src/UI/WB.UI.Frontend/src/hqapp/Views/HQ/Users/ApiUsers.vue
@@ -27,6 +27,7 @@
 
 import moment from 'moment'
 import { formatNumber } from './formatNumber'
+import { DateFormats } from '~/shared/helpers'
 
 export default {
     data() {
@@ -75,7 +76,7 @@ export default {
                         title: this.$t('Users.CreationDate'),
                         render: function(data, type, row) {
                             var localDate = moment.utc(data).local()
-                            return localDate.format(window.CONFIG.dateFormat)
+                            return localDate.format(DateFormats.dateTimeInList)
                         },
                     },
                     {
