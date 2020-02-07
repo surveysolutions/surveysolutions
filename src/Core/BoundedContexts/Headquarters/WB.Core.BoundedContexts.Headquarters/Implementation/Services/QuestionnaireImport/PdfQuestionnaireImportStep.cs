af788f68b20d5b6decc43c142e23f91886453380
Pavel Stovpivskyi <sby@rambler.ru>
2020-02-07 12:55:11 +0200

    KP-13668 split import on steps

---
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Implementation/Services/QuestionnaireImport/PdfQuestionnaireImportStep.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Main.Core.Documents;
+using Polly;
+using WB.Core.BoundedContexts.Headquarters.Designer;
+using WB.Core.BoundedContexts.Headquarters.Services;
+using WB.Core.GenericSubdomains.Portable;
+using WB.Core.GenericSubdomains.Portable.Services;
+using WB.Core.Infrastructure.PlainStorage;
+using WB.Core.SharedKernels.DataCollection.Implementation.Entities;
+using WB.Core.SharedKernels.Questionnaire.Synchronization.Designer;
+
+namespace WB.Core.BoundedContexts.Headquarters.Implementation.Services
+{
+    class PdfQuestionnaireImportStep : IQuestionnaireImportStep
+    {
+        private readonly QuestionnaireIdentity questionnaireIdentity;
+        private readonly QuestionnaireDocument questionnaire;
+        private readonly Progress progress;
+        private readonly IDesignerApi designerApi;
+        private readonly ILogger logger;
+        private readonly IPlainKeyValueStorage<QuestionnairePdf> pdfStorage;
+        private readonly Dictionary<string, QuestionnairePdf> pdfFiles = new Dictionary<string, QuestionnairePdf>();
+
+        public PdfQuestionnaireImportStep(QuestionnaireIdentity questionnaireIdentity, QuestionnaireDocument questionnaire, Progress progress, IDesignerApi designerApi, IPlainKeyValueStorage<QuestionnairePdf> pdfStorage, ILogger logger)
+        {
+            this.questionnaireIdentity = questionnaireIdentity;
+            this.questionnaire = questionnaire;
+            this.progress = progress;
+            this.designerApi = designerApi;
+            this.pdfStorage = pdfStorage;
+            this.logger = logger;
+        }
+
+        public int GetPrecessStepsCount()
+        {
+            return (1 + (questionnaire.Translations?.Count ?? 0)) * 3;
+        }
+
+        public async Task DownloadFromDesignerAsync()
+        {
+            await RequestToGeneratePdf();
+            await DownloadPdf();
+        }
+
+        private async Task RequestToGeneratePdf()
+        {
+            this.logger.Error($"Requesting pdf generator to start working for questionnaire {questionnaire.PublicKey}");
+
+            await designerApi.GetPdfStatus(questionnaireIdentity.QuestionnaireId);
+            progress.Current++;
+
+            foreach (var questionnaireTranslation in questionnaire.Translations)
+            {
+                await designerApi.GetPdfStatus(questionnaire.PublicKey, questionnaireTranslation.Id);
+                progress.Current++;
+            }
+        }
+
+        private async Task DownloadPdf()
+        {
+            logger.Verbose($"DownloadPdf: {questionnaire.Title}({questionnaire.PublicKey} rev.{questionnaire.Revision})");
+
+            var pdfRetry = Policy
+                .HandleResult<PdfStatus>(x => x.ReadyForDownload == false && x.CanRetry != true)
+                .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3));
+
+            await pdfRetry.ExecuteAsync(async () =>
+            {
+                this.logger.Trace($"Waiting for pdf to be ready {questionnaireIdentity}");
+                return await designerApi.GetPdfStatus(questionnaireIdentity.QuestionnaireId);
+            });
+
+            this.logger.Error("Loading pdf for default language");
+
+            var pdfFile = await designerApi.DownloadPdf(questionnaireIdentity.QuestionnaireId);
+            progress.Current++;
+
+            pdfFiles.Add(questionnaireIdentity.ToString(), new QuestionnairePdf { Content = pdfFile.Content });
+
+            this.logger.Error($"PDF for questionnaire stored {questionnaireIdentity}");
+
+            foreach (var translation in questionnaire.Translations)
+            {
+                this.logger.Error($"loading pdf for translation {translation}");
+
+                await pdfRetry.ExecuteAsync(async () =>
+                {
+                    this.logger.Trace($"Waiting for pdf to be ready {questionnaireIdentity}");
+
+                    return await designerApi.GetPdfStatus(questionnaireIdentity.QuestionnaireId, translation.Id);
+                });
+
+                var pdfTranslated = await designerApi.DownloadPdf(questionnaireIdentity.QuestionnaireId, translation.Id);
+                progress.Current++;
+                pdfFiles.Add($"{translation.Id.FormatGuid()}_{questionnaireIdentity}", new QuestionnairePdf { Content = pdfTranslated.Content });
+                this.logger.Error($"PDF for questionnaire stored {questionnaireIdentity} translation {translation.Id}, {translation.Name}");
+            }
+        }
+
+        public void SaveData()
+        {
+            foreach (var pdfFile in pdfFiles)
+            {
+                this.pdfStorage.Store(pdfFile.Value, pdfFile.Key);
+                progress.Current++;
+            }
+        }
+    }
+}
---
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Implementation/Services/QuestionnaireImport/TranslationsQuestionnaireImportStep.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Main.Core.Documents;
+using WB.Core.BoundedContexts.Headquarters.Designer;
+using WB.Core.BoundedContexts.Headquarters.Services;
+using WB.Core.GenericSubdomains.Portable.Services;
+using WB.Core.SharedKernels.DataCollection.Implementation.Entities;
+using WB.Core.SharedKernels.Questionnaire.Translations;
+using WB.Enumerator.Native.Questionnaire;
+
+namespace WB.Core.BoundedContexts.Headquarters.Implementation.Services
+{
+    class TranslationsQuestionnaireImportStep : IQuestionnaireImportStep
+    {
+        private readonly QuestionnaireIdentity questionnaireIdentity;
+        private readonly QuestionnaireDocument questionnaire;
+        private readonly Progress progress;
+        private readonly IDesignerApi designerApi;
+        private readonly ITranslationManagementService translationManagementService;
+        private readonly ILogger logger;
+        private List<TranslationDto> translationContent;
+
+        public TranslationsQuestionnaireImportStep(QuestionnaireIdentity questionnaireIdentity, QuestionnaireDocument questionnaire, Progress progress, IDesignerApi designerApi, ITranslationManagementService translationManagementService, ILogger logger)
+        {
+            this.questionnaireIdentity = questionnaireIdentity;
+            this.questionnaire = questionnaire;
+            this.progress = progress;
+            this.designerApi = designerApi;
+            this.translationManagementService = translationManagementService;
+            this.logger = logger;
+        }
+        public int GetPrecessStepsCount()
+        {
+            return questionnaire.Translations != null ? 2 : 0;
+        }
+
+        public async Task DownloadFromDesignerAsync()
+        {
+            this.logger.Debug($"loading translations {questionnaireIdentity.Id}");
+            translationContent = await designerApi.GetTranslations(questionnaire.PublicKey);
+            progress.Current++;
+        }
+
+        public void SaveData()
+        {
+            this.logger.Debug($"save translations {questionnaireIdentity.Id}");
+
+            translationManagementService.Delete(questionnaireIdentity);
+
+            translationManagementService.Store(translationContent.Select(x => new TranslationInstance
+            {
+                QuestionnaireId = questionnaireIdentity,
+                Value = x.Value,
+                QuestionnaireEntityId = x.QuestionnaireEntityId,
+                Type = x.Type,
+                TranslationIndex = x.TranslationIndex,
+                TranslationId = x.TranslationId
+            }));
+            progress.Current++;
+        }
+    }
+}
