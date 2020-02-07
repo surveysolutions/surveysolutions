69e675886e5a15589918b43d9e151cb4f6768539
Roman Lytvyn <litvin.roma@gmail.com>
2020-02-07 13:56:08 +0200

    KP-13697 Observer is able to add comment to interview

--- src/UI/WB.UI.Headquarters.Core/Startup.cs
+++ src/UI/WB.UI.Headquarters.Core/Startup.cs
@@ -231,6 +231,7 @@ namespace WB.UI.Headquarters
             services.AddScoped<InstallationFilter>();
             services.AddScoped<AntiForgeryFilter>();
             services.AddScoped<GlobalNotificationResultFilter>();
+            services.AddScoped<WebInterviewObserverNotAllowedActionFilter>();
             services.AddHeadquartersHealthCheck();
 
             FileStorageModule.Setup(services, Configuration);
@@ -242,6 +243,7 @@ namespace WB.UI.Headquarters
                 mvc.Filters.AddService<UnitOfWorkActionFilter>(1);
                 mvc.Filters.AddService<InstallationFilter>(100);
                 mvc.Filters.AddService<GlobalNotificationResultFilter>(200);
+                mvc.Filters.AddService<WebInterviewObserverNotAllowedActionFilter>(300);
                 mvc.Conventions.Add(new OnlyPublicApiConvention());
                 mvc.ModelBinderProviders.Insert(0, new DataTablesRequestModelBinderProvider());
                 var noContentFormatter = mvc.OutputFormatters.OfType<HttpNoContentOutputFormatter>().FirstOrDefault();
