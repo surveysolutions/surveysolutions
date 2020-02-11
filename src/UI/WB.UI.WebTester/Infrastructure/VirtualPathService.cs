515e6f13e54f65318857ea8a280cf45cdce865c0
Pavel Stovpivskyi <sby@rambler.ru>
2020-02-11 15:51:05 +0200

    KP-13710 Internal hyperlinks does not show up  for WebTester

--- src/UI/WB.UI.WebTester/Infrastructure/VirtualPathService.cs
+++ src/UI/WB.UI.WebTester/Infrastructure/VirtualPathService.cs
@@ -1,10 +1,24 @@
-﻿using System.IO;
+﻿using System;
+using Microsoft.AspNetCore.Http;
 using WB.UI.Shared.Web.Services;
 
 namespace WB.UI.WebTester.Infrastructure
 {
     public class VirtualPathService : IVirtualPathService
     {
-        public string GetAbsolutePath(string relativePath) => relativePath;
+        private readonly IHttpContextAccessor httpContextAccessor;
+
+        public VirtualPathService(IHttpContextAccessor httpContextAccessor)
+        {
+            this.httpContextAccessor = httpContextAccessor;
+        }
+
+        public string GetAbsolutePath(string relativePath)
+        {
+            return relativePath.TrimStart('~');
+            /*var request = httpContextAccessor.HttpContext.Request;
+            var url = new Uri(new Uri($"{request.Scheme}://{request.Host.Value}"), relativePath.TrimStart('~')).ToString();
+            return url;*/
+        }
     }
 }
