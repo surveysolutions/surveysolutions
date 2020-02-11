3aa9d61a18540dae3bab5514e6cc67b59df0b770
Pavel Stovpivskyi <sby@rambler.ru>
2020-02-11 15:52:09 +0200

    KP-13710 removed commented text

--- src/UI/WB.UI.WebTester/Infrastructure/VirtualPathService.cs
+++ src/UI/WB.UI.WebTester/Infrastructure/VirtualPathService.cs
@@ -1,24 +1,9 @@
-﻿using System;
-using Microsoft.AspNetCore.Http;
-using WB.UI.Shared.Web.Services;
+﻿using WB.UI.Shared.Web.Services;
 
 namespace WB.UI.WebTester.Infrastructure
 {
     public class VirtualPathService : IVirtualPathService
     {
-        private readonly IHttpContextAccessor httpContextAccessor;
-
-        public VirtualPathService(IHttpContextAccessor httpContextAccessor)
-        {
-            this.httpContextAccessor = httpContextAccessor;
-        }
-
-        public string GetAbsolutePath(string relativePath)
-        {
-            return relativePath.TrimStart('~');
-            /*var request = httpContextAccessor.HttpContext.Request;
-            var url = new Uri(new Uri($"{request.Scheme}://{request.Host.Value}"), relativePath.TrimStart('~')).ToString();
-            return url;*/
-        }
+        public string GetAbsolutePath(string relativePath) => relativePath.TrimStart('~');
     }
 }
