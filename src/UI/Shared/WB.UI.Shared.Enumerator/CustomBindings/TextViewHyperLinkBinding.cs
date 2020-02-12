5f3552ec7477082d765ed43fc0a499eb67e8aad2
Roman Lytvyn <litvin.roma@gmail.com>
2020-02-12 15:21:25 +0200

    KP-13699 replace answered gps question with map to a link leading to google maps. Implemented free google map on android apps

---
+++ src/UI/Shared/WB.UI.Shared.Enumerator/CustomBindings/TextViewHyperLinkBinding.cs
@@ -0,0 +1,22 @@
+﻿using Android.Text;
+using Android.Text.Method;
+using Android.Widget;
+
+namespace WB.UI.Shared.Enumerator.CustomBindings
+{
+    public class TextViewHyperLinkBinding : BaseBinding<TextView, (string text, string url)?>
+    {
+        public TextViewHyperLinkBinding(TextView androidControl) : base(androidControl)
+        {
+        }
+
+        protected override void SetValueToView(TextView control, (string text, string url)? value)
+        {
+            control.SetText(
+                Html.FromHtml(value == null ? "" : $"<a href=\"{value.Value.url}\">{value.Value.text}</a>", FromHtmlOptions.ModeCompact),
+                TextView.BufferType.Spannable);
+
+            control.MovementMethod = LinkMovementMethod.Instance;
+        }
+    }
+}
