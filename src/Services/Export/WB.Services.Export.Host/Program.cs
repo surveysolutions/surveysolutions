69f54aee3f872d46616acdbd80e06973dfa325e9
Victor Gladkikh <me@alfeg.net>
2020-02-12 18:16:29 +0200

    KP-13593 Added support for ini configuration in Export service

--- src/Services/Export/WB.Services.Export.Host/Program.cs
+++ src/Services/Export/WB.Services.Export.Host/Program.cs
@@ -151,8 +151,13 @@ namespace WB.Services.Export.Host
                     web.ConfigureAppConfiguration(c =>
                     {
                         c.AddJsonFile($"appsettings.{Environment.MachineName}.json", true);
+                        c.AddIniFile($"appsettings.{Environment.MachineName}.ini", true);
+
                         c.AddJsonFile($"appsettings.Cloud.json", true);
+                        c.AddIniFile($"appsettings.Cloud.ini", true);
+
                         c.AddJsonFile($"appsettings.Production.json", true);
+                        c.AddIniFile($"appsettings.Production.ini", true);
 
                         c.AddCommandLine(args);
                     });
