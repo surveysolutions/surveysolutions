5fc731b28c1161c220762f04a1167f4eda3d3aca
Roman Lytvyn <litvin.roma@gmail.com>
2020-02-13 15:07:02 +0200

    Update Extensions.cs

--- src/UI/WB.UI.Headquarters.Core/Code/Extensions.cs
+++ src/UI/WB.UI.Headquarters.Core/Code/Extensions.cs
@@ -105,14 +105,6 @@ namespace WB.UI.Headquarters.Code
         public static string MapPath(this IWebHostEnvironment hostingEnvironment, string path) =>
             Path.Combine(hostingEnvironment.ContentRootPath, "wwwroot", path);
 
-        /// <summary>
-        /// Determines whether the specified HTTP request is an AJAX request.
-        /// </summary>
-        /// 
-        /// <returns>
-        /// true if the specified HTTP request is an AJAX request; otherwise, false.
-        /// </returns>
-        /// <param name="request">The HTTP request.</param><exception cref="T:System.ArgumentNullException">The <paramref name="request"/> parameter is null (Nothing in Visual Basic).</exception>
         public static bool IsJsonRequest(this HttpRequest request)
         {
             if (request == null)
