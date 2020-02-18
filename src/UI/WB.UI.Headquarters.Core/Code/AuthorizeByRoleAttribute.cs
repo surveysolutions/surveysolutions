534c6add6bc613d550198f503d65eaa164056865
Pavel Stovpivskyi <sby@rambler.ru>
2020-02-18 15:23:31 +0200

    KP-13729 'Not found' is displayed when IN clicks on Edit personal info

---
+++ src/UI/WB.UI.Headquarters.Core/Code/AuthorizeByRoleAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+using Main.Core.Entities.SubEntities;
+using Microsoft.AspNetCore.Authorization;
+
+namespace WB.UI.Headquarters.Code
+{
+    public class AuthorizeByRoleAttribute : AuthorizeAttribute
+    {
+        public AuthorizeByRoleAttribute(params UserRoles[] userRoles)
+        {
+            Roles = string.Join(", ", userRoles.Select(role => role.ToString()));
+        }
+    }
+}
