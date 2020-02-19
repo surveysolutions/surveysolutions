42f7fc954d217006a0f29164f3f2e6b1713b8133
Victor Hladkikh <me@alfeg.net>
2020-02-19 11:07:05 +0200

    Renamed configs

--- src/UI/WB.UI.Headquarters.Core/Configs/MetricsConfig.cs
+++ src/UI/WB.UI.Headquarters.Core/Configs/MetricsConfig.cs
@@ -2,7 +2,7 @@
 {
     public class MetricsConfig
     {
-        public string Endpoint { get; set; } = "http://localhost:9001";
+        public string PushGatewayEndpoint { get; set; } = "http://localhost:9001";
         public bool UsePushGateway { get; set; } = true;
         public bool UseMetricsEndpoint { get; set; } = false;
     }
