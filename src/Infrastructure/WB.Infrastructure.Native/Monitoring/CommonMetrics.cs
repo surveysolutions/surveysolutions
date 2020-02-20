77c6f8a397fb63eb5217ae219eccc6ec679fea97
Victor Gladkikh <me@alfeg.net>
2020-02-20 10:55:04 +0200

    KP-13655 Added Npgsql counters

--- src/Infrastructure/WB.Infrastructure.Native/Monitoring/CommonMetrics.cs
+++ src/Infrastructure/WB.Infrastructure.Native/Monitoring/CommonMetrics.cs
@@ -9,5 +9,8 @@ namespace WB.Infrastructure.Native.Monitoring
         public static Counter StatefullInterviewEvicted   = new Counter("wb_statefullinterview_cache_evict", "Total number of times statefull interview cached", "reason");
         public static Counter StatefullInterviewCacheHit = new Counter("wb_statefullinterview_cache_hit", "Total number of times statefull interview cache hit");
         public static Counter StatefullInterviewCacheMiss  = new Counter("wb_statefullinterview_cache_miss", "Total number of times statefull interview cache missed");
+        public static Gauge NpgsqlConnections = new Gauge("npgsql_connections_current", "Number of connections managed by Npgsql", "type");
+        public static Gauge NpgsqlConnectionsPoolCount = new Gauge("npgsql_connection_pools_current", "Number of connection pools managed by Npgsql");
+        public static Counter NpgsqlDataCounter = new Counter("npgsql_data_total_bytes", "Amount of byte read/write by Npgsql", "direction");
     }
 }
