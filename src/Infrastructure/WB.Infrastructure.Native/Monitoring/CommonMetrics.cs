9527723eff6b1716666ab5eaca8d4ea0436299f9
Victor Hladkikh <me@alfeg.net>
2020-02-21 16:45:41 +0200

    KP-13655 Fixed npgsql pooling counters

--- src/Infrastructure/WB.Infrastructure.Native/Monitoring/CommonMetrics.cs
+++ src/Infrastructure/WB.Infrastructure.Native/Monitoring/CommonMetrics.cs
@@ -1,13 +1,12 @@
-﻿
-namespace WB.Infrastructure.Native.Monitoring
+﻿namespace WB.Infrastructure.Native.Monitoring
 {
     public static class CommonMetrics
     {
-        public static Counter WebInterviewConnection  = new Counter("wb_webinterview_connections",  "Number of times connection were open", "action", "mode");
-        public static Counter StatefullInterviewCached    = new Counter("wb_statefullinterview_cache_create", "Total number of times statefull interview cached");
-        public static Counter StatefullInterviewEvicted   = new Counter("wb_statefullinterview_cache_evict", "Total number of times statefull interview cached", "reason");
+        public static Counter WebInterviewConnection = new Counter("wb_webinterview_connections", "Number of times connection were open", "action", "mode");
+        public static Counter StatefullInterviewCached = new Counter("wb_statefullinterview_cache_create", "Total number of times statefull interview cached");
+        public static Counter StatefullInterviewEvicted = new Counter("wb_statefullinterview_cache_evict", "Total number of times statefull interview cached", "reason");
         public static Counter StatefullInterviewCacheHit = new Counter("wb_statefullinterview_cache_hit", "Total number of times statefull interview cache hit");
-        public static Counter StatefullInterviewCacheMiss  = new Counter("wb_statefullinterview_cache_miss", "Total number of times statefull interview cache missed");
+        public static Counter StatefullInterviewCacheMiss = new Counter("wb_statefullinterview_cache_miss", "Total number of times statefull interview cache missed");
 
         public static Gauge NpgsqlConnections = new Gauge("npgsql_connections_current", "Number of connections managed by Npgsql", "type");
         public static Gauge NpgsqlConnectionsPoolCount = new Gauge("npgsql_connection_pools_current", "Number of connection pools managed by Npgsql");
