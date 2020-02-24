c881166775282cf7b0fd2b98b63d472b853b86cc
Victor Gladkikh <me@alfeg.net>
2020-02-24 19:17:35 +0200

    KP-13771 Add .hc

--- src/UI/Shared/WB.UI.Shared.Web.Core/Diagnostics/DiagnosticsExtensions.cs
+++ src/UI/Shared/WB.UI.Shared.Web.Core/Diagnostics/DiagnosticsExtensions.cs
@@ -9,13 +9,14 @@ namespace WB.UI.Shared.Web.Diagnostics
     public static class DiagnosticsExtensions
     {
         private static string version = null;
-        public static void MapVersionEndpoint(this IEndpointRouteBuilder endpoints, string pattern = ".version")
+        public static IEndpointConventionBuilder MapVersionEndpoint(this IEndpointRouteBuilder endpoints, string pattern = ".version")
         {
-            endpoints.MapGet("/" + pattern, context =>
+            return endpoints.MapGet("/" + pattern, context =>
             {
                 version ??= context.RequestServices.GetRequiredService<IProductVersion>().ToString();
                 return context.Response.WriteAsync(version);
             });
         }
     }
+
 }
