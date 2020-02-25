cd4503653b1ba008722fd986cf10f3fff9155441
Andrii Kozhyn <hedgic@gmail.com>
2020-02-25 15:36:22 +0200

    KP-13416 fixed test

--- src/Tests/WB.Tests.Web/Headquarters/Controllers/PublicApiTests/AssignmentsTests/when_changing_quantity_with_AssignmentsPublicApi.cs
+++ src/Tests/WB.Tests.Web/Headquarters/Controllers/PublicApiTests/AssignmentsTests/when_changing_quantity_with_AssignmentsPublicApi.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using NUnit.Framework;
+using WB.Core.BoundedContexts.Headquarters.Assignments;
 using WB.Core.SharedKernels.DataCollection.Commands.Assignment;
 using WB.Tests.Abc;
+using WB.UI.Headquarters.API.PublicApi.Models;
 
 
 namespace WB.Tests.Unit.Applications.Headquarters.PublicApiTests.AssignmentsTests
@@ -20,6 +22,16 @@ namespace WB.Tests.Unit.Applications.Headquarters.PublicApiTests.AssignmentsTest
         public void should_store_updated_quantity()
         {
             this.SetupAssignment(Create.Entity.Assignment(id: 1, publicKey: Id.g1, quantity: 10));
+
+            mapper.Setup(x => x.Map<AssignmentDetails>(It.IsAny<Assignment>()))
+                .Returns((Assignment arg) =>
+                {
+                    return new AssignmentDetails
+                    {
+                        Id = arg.Id,
+                        Quantity = arg.Quantity
+                    };
+                });
 
             this.controller.ChangeQuantity(1, 30);
 
