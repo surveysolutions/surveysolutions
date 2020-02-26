cd27a499a1347c7390acbeb7d557eae0c8295563
Victor Hladkikh <me@alfeg.net>
2020-02-26 11:25:47 +0200

    KP-13771 Cleanup metrics calculation
    - added CPU usage
    - added DB usage

--- src/Infrastructure/WB.Infrastructure.Native/Monitoring/MetricExtensions.cs
+++ src/Infrastructure/WB.Infrastructure.Native/Monitoring/MetricExtensions.cs
@@ -5,7 +5,7 @@ namespace WB.Infrastructure.Native.Monitoring
     public static class MetricExtensions
     {
         /// <summary>
-        /// 
+        /// Get summ of all tracked values based on labels
         /// </summary>
         /// <param name="counter"></param>
         /// <param name="labels">* - for wildcard</param>
@@ -24,6 +24,42 @@ namespace WB.Infrastructure.Native.Monitoring
             }
 
             return summ;
+        }
+
+        /// <summary>
+        /// Get summ of all tracked values based on labels
+        /// </summary>
+        /// <param name="counter"></param>
+        /// <param name="labels">* - for wildcard</param>
+        /// <returns></returns>
+        public static double GetSummForLabels(this Gauge counter, params string[] labels)
+        {
+            var metricLabels = counter.AllLabels.ToList();
+            var summ = 0.0;
+
+            foreach (var label in metricLabels)
+            {
+                if (label.IsMatchToLabels(labels))
+                {
+                    summ += counter.Labels(label).Value;
+                }
+            }
+
+            return summ;
+        }
+
+        public static double GetDiffForLabels(this Counter counter, string[] labelsA, string[] labelsB)
+        {
+            var a = counter.GetSummForLabels(labelsA);
+            var b = counter.GetSummForLabels(labelsB);
+            return a - b;
+        }   
+        
+        public static double GetDiffForLabels(this Gauge counter, string[] labelsA, string[] labelsB)
+        {
+            var a = counter.GetSummForLabels(labelsA);
+            var b = counter.GetSummForLabels(labelsB);
+            return a - b;
         }
 
         private static bool IsMatchToLabels(this string[] a, string[] b)
---
+++ src/UI/WB.UI.Headquarters.Core/Metrics/HeadquartersHttpMetricsMiddleware.cs
@@ -0,0 +1,52 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Prometheus;
+
+namespace WB.UI.Headquarters.Metrics
+{
+    public class HeadquartersHttpMetricsMiddleware
+    {
+        private readonly RequestDelegate next;
+        private readonly Gauge httpInProgress = Prometheus.Metrics.CreateGauge("http_requests_in_progress", "Number or requests in progress", "system");
+        private readonly Histogram httpRequestsDuration = Prometheus.Metrics.CreateHistogram("http_requests_duration_seconds", "Duration of http requests per tracking system", "system");
+        
+        public HeadquartersHttpMetricsMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            string[] subsystem = new string[1];
+            var path = context.Request.Path;
+
+            // will not track request to diagnostics endpoints
+            if (path.StartsWithSegments("/metrics") || path.StartsWithSegments("/.hc") ||
+                path.StartsWithSegments("/.version"))
+            {
+                await next(context);
+                return;
+            }
+
+            // web interview commands requests
+            if (path.StartsWithSegments("/api/webinterview/commands")) subsystem[0] = "webcommand";
+
+            // interviewer app synchronization requests
+            else if (path.StartsWithSegments("/api/interviewer")) subsystem[0] = "interviewer";
+
+            // supervisor app synchronization requests
+            else if (path.StartsWithSegments("/api/supervisor")) subsystem[0] = "supervisor";
+
+            // web interview requests for interview details
+            else if (path.StartsWithSegments("/api/webinterview")) subsystem[0] = "webinterview";
+
+            // all other requests
+            else subsystem[0] = "other";
+
+            using var inprogress = httpInProgress.Labels(subsystem).TrackInProgress();
+            using var timer = this.httpRequestsDuration.Labels(subsystem).NewTimer();
+            await next(context);
+        }
+    }
+}
