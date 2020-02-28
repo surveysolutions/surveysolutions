c9aec3960505b4325084aa0c6653cf8b481de2d5
Andrii Kozhyn <andrii.kozhyn@outlook.com>
2020-02-28 11:27:52 +0200

    KP-13778 do not allow creation of administrator

---
+++ src/UI/WB.UI.Headquarters.Core/Controllers/Api/PublicApi/Models/Roles.cs
@@ -0,0 +1,14 @@
+﻿namespace WB.UI.Headquarters.Controllers.Api.PublicApi.Models
+{
+    /// <summary>
+    /// User role
+    /// </summary>
+    public enum Roles
+    {
+        Supervisor = 2,
+        Interviewer = 4,
+        Headquarter = 6,
+        Observer = 7,
+        ApiUser = 8
+    }
+}
