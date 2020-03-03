a22b395f104eb1fc20e753eef678a24974e5f017
Pavel Stovpivskyi <630198+spuv@users.noreply.github.com>
2020-03-03 13:56:45 +0200

    KP-13808 500 is displayed during installation

--- src/UI/WB.UI.Headquarters.Core/Filters/UnitOfWorkActionFilter.cs
+++ src/UI/WB.UI.Headquarters.Core/Filters/UnitOfWorkActionFilter.cs
@@ -1,22 +1,24 @@
-﻿using System;
+﻿using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Controllers;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.DependencyInjection;
 using WB.Infrastructure.Native.Storage.Postgre;
+using WB.UI.Shared.Web.Attributes;
 
 namespace WB.UI.Headquarters.Filters
 {
     public class UnitOfWorkActionFilter : IAsyncActionFilter
     {
-        private readonly IUnitOfWork unitOfWork;
-
-        public UnitOfWorkActionFilter(IUnitOfWork unitOfWork)
-        {
-            this.unitOfWork = unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
-        }
-
         public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
         {
+            var doesMarkAsNoTransaction = DoesMarkAsNoTransaction(context);
+            if (doesMarkAsNoTransaction)
+                return;
+
             var executedContext = await next.Invoke();
+
+            var unitOfWork = context.HttpContext.RequestServices.GetRequiredService<IUnitOfWork>();
             if (executedContext.Exception == null)
             {
                 unitOfWork.AcceptChanges();
@@ -25,6 +27,25 @@ namespace WB.UI.Headquarters.Filters
             {
                 unitOfWork.Dispose();
             }
+        }
+
+        private bool DoesMarkAsNoTransaction(ActionExecutingContext context)
+        {
+            if (context.Controller.GetType().GetCustomAttributes(typeof(NoTransactionAttribute), true).Length > 0)
+                return true;
+
+            if (context.ActionDescriptor is ControllerActionDescriptor controllerActionDescriptor)
+            {
+                // Check if the attribute exists on the action method
+                if (controllerActionDescriptor.MethodInfo?.GetCustomAttributes(inherit: true)?.Any(a => a.GetType() == typeof(NoTransactionAttribute)) ?? false)
+                    return true;
+
+                // Check if the attribute exists on the controller
+                if (controllerActionDescriptor.ControllerTypeInfo?.GetCustomAttributes(typeof(NoTransactionAttribute), true)?.Any() ?? false)
+                    return true;
+            }
+
+            return false;
         }
     }
 }
