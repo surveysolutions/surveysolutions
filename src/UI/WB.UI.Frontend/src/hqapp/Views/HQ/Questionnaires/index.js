0282c51941f1b060f76ce815fd4efef10fdbc87f
Pavel Stovpivskyi <sby@rambler.ru>
2020-03-04 16:37:22 +0200

    KP-13779 Add link to preloading page

--- src/UI/WB.UI.Frontend/src/hqapp/Views/HQ/Questionnaires/index.js
+++ src/UI/WB.UI.Frontend/src/hqapp/Views/HQ/Questionnaires/index.js
@@ -4,7 +4,7 @@ import Clone from './Clone'
 export default class MapComponent {
     get routes() {
         return [{
-            path: '/Questionnaires/Details/:questionnaireId', component: Details,
+            path: '/Questionnaires/Details/:questionnaireId', component: Details, name: 'questionnairedetails',
         },
         {
             path: '/Questionnaires/Clone/:questionnaireId', component: Clone,
