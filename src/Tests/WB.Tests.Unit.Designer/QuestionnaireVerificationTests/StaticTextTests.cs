c72c009ef176fa086568489ab9c4af0d41f29f09
Pavel Stovpivskyi <sby@rambler.ru>
2020-03-04 18:56:37 +0200

    KP-11010 Do not require text

---
+++ src/Tests/WB.Tests.Unit.Designer/QuestionnaireVerificationTests/StaticTextTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+
+namespace WB.Tests.Unit.Designer.QuestionnaireVerificationTests
+{
+    [TestFixture]
+    public class StaticTextTests
+    {
+        [Test]
+        public void static_text_with_text()
+            => Create.QuestionnaireDocumentWithOneChapter(new[]
+                {
+                    Create.StaticText(text: "static text"),
+                })
+                .ExpectNoError("WB0071");
+
+        [Test]
+        public void static_text_with_attachment()
+            => Create.QuestionnaireDocumentWithOneChapter(new[]
+                {
+                    Create.StaticText(attachmentName: "attachemnt#1"),
+                })
+                .ExpectNoError("WB0071");
+
+        [Test]
+        public void static_text_without_text_and_attachment()
+            => Create.QuestionnaireDocumentWithOneChapter(new[]
+                {
+                    Create.StaticText(text: ""),
+                })
+                .ExpectError("WB0071");
+    }
+}
