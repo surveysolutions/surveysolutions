cedeb79f3ccdb31a8c006a0bac02970d5e3ace1d
Andrii Kozhyn <andrii.kozhyn@outlook.com>
2020-03-11 12:42:02 +0200

    KP-13709 human id may become larger when needed

---
+++ src/Tests/WB.Tests.Unit/SharedKernels/DataCollection/ValueObjects/InterviewKeyTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using WB.Core.SharedKernels.DataCollection.ValueObjects.Interview;
+
+namespace WB.Tests.Unit.SharedKernels.DataCollection.ValueObjects
+{
+    [TestOf(typeof(InterviewKey))]
+    public class InterviewKeyTests
+    {
+        [Test]
+        public void should_format_human_id()
+        {
+            var key = new InterviewKey(00_00_31_11);
+
+            string formatted = key.ToString();
+
+            Assert.That(formatted, Is.EqualTo("00-00-31-11"));
+        }
+
+        [Test]
+        public void should_format_large_human_id()
+        {
+            var key = new InterviewKey(1_00_00_31_11);
+
+            string formatted = key.ToString();
+
+            Assert.That(formatted, Is.EqualTo("1-00-00-31-11"));
+        }
+    }
+}
