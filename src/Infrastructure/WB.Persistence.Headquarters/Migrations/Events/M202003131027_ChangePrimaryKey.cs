3376d3225109649cee4e0157ffcbf96e02c60dc4
Andrii Kozhyn <andrii.kozhyn@outlook.com>
2020-03-13 10:30:19 +0200

    KP-13709 change primary key to reduce table size

---
+++ src/Infrastructure/WB.Persistence.Headquarters/Migrations/Events/M202003131027_ChangePrimaryKey.cs
@@ -0,0 +1,20 @@
+﻿using FluentMigrator;
+
+namespace WB.Persistence.Headquarters.Migrations.Events
+{
+    [Migration(2020_03_13_10_27)]
+    public class M202003131027_ChangePrimaryKey : Migration
+    {
+        public override void Up()
+        {
+            Delete.PrimaryKey("pk").FromTable("events").InSchema("events");
+
+            // This is fastest way. https://dba.stackexchange.com/questions/8814/how-to-promote-an-existing-index-to-primary-key-in-postgresql
+            Execute.Sql("ALTER TABLE events.events ADD CONSTRAINT events_pk PRIMARY KEY USING INDEX globalsequence_indx;");
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
