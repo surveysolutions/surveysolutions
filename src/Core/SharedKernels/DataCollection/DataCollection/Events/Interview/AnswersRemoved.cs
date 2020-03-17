d5257cca444b56ad6985e33367fa0ef450bd5ffc
Pavel Stovpivskyi <sby@rambler.ru>
2020-03-17 14:06:21 +0200

    KP-13774 Paradata event "AnswerRemoved" is missing the Responsible and Role fields

--- src/Core/SharedKernels/DataCollection/DataCollection/Events/Interview/AnswersRemoved.cs
+++ src/Core/SharedKernels/DataCollection/DataCollection/Events/Interview/AnswersRemoved.cs
@@ -6,14 +6,17 @@ namespace WB.Core.SharedKernels.DataCollection.Events.Interview
     public class AnswersRemoved : QuestionsPassiveEvent
     {
         public DateTime? RemoveTime { get; set; }
+        public Guid? UserId { get; set; }
 
-        public AnswersRemoved(Identity[] questions, DateTimeOffset originDate)
+        public AnswersRemoved(Guid? userId, Identity[] questions, DateTimeOffset originDate)
             : base(questions, originDate)
         {
             if (originDate != default(DateTimeOffset))
             {
                 this.RemoveTime = originDate.UtcDateTime;
             }
+
+            UserId = userId;
         }
     }
 }
