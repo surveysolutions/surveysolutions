b792ca42849bd5e989d328f8bb2e462d10e52e8e
Victor Gladkikh <me@alfeg.net>
2020-03-18 11:27:29 +0200

    Adjusted metrics names according to Prometheus naming conventions
    https://prometheus.io/docs/practices/naming/

--- src/Infrastructure/WB.Infrastructure.Native/Monitoring/CommonMetrics.cs
+++ src/Infrastructure/WB.Infrastructure.Native/Monitoring/CommonMetrics.cs
@@ -2,7 +2,7 @@
 {
     public static class CommonMetrics
     {
-        public static Counter WebInterviewConnection = new Counter("wb_webinterview_connections", "Number of times connection were open (open/closed), (review,takenew,web)", "action", "mode");
+        public static Counter WebInterviewConnection = new Counter("wb_webinterview_connections_total", "Number of times connection were open (open/closed), (review,takenew,web)", "action", "mode");
         public static Counter StatefullInterviewCached = new Counter("wb_statefullinterview_cache_create", "Total number of times statefull interview cached");
         public static Counter StatefullInterviewEvicted = new Counter("wb_statefullinterview_cache_evict", "Total number of times statefull interview cached", "reason");
         public static Counter StatefullInterviewCacheHit = new Counter("wb_statefullinterview_cache_hit", "Total number of times statefull interview cache hit");
@@ -10,8 +10,8 @@
 
         public static Gauge NpgsqlConnections = new Gauge("npgsql_connections_current", "Number of connections managed by Npgsql (idle/busy)", "type");
         public static Gauge NpgsqlConnectionsPoolCount = new Gauge("npgsql_connection_pools_current", "Number of connection pools managed by Npgsql");
-        public static Counter NpgsqlDataCounter = new Counter("npgsql_data_total_bytes", "Amount of byte read/write by Npgsql", "direction");
-        public static Counter ExceptionsOccur = new Counter("wb_exceptions_total_count", "Total number of exceptions happen on site during it's life time");
+        public static Counter NpgsqlDataCounter = new Counter("npgsql_data_bytes_total", "Amount of byte read/write by Npgsql", "direction");
+        public static Counter ExceptionsOccur = new Counter("wb_exceptions_total", "Total number of exceptions happen on site during it's life time");
 
         public static Counter InterviewsCreatedCount = new Counter("wb_interviews_created_total", "Total number of interviews created during app life time");
         public static Counter EventsCreatedCount = new Counter("wb_events_created_total", "Total number of events created during app life time");
