afa88f603b5d99d68c51979825d7f6fa5d27f138
Pavel Stovpivskyi <sby@rambler.ru>
2020-03-18 14:00:46 +0200

    KP-13837 Search modal does not show results in the View revision mode

--- src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Services/IFindReplaceService.cs
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Services/IFindReplaceService.cs
@@ -1,11 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using Main.Core.Documents;
 using WB.Core.BoundedContexts.Designer.ValueObjects;
+using WB.Core.BoundedContexts.Designer.Views.Questionnaire.ChangeHistory;
 
 namespace WB.Core.BoundedContexts.Designer.Services
 {
     public interface IFindReplaceService
     {
-        IEnumerable<QuestionnaireEntityReference> FindAll(Guid questionnaireId, string searchFor, bool matchCase, bool matchWholeWord, bool useRegex);
+        IEnumerable<QuestionnaireEntityReference> FindAll(QuestionnaireRevision questionnaireId, string searchFor, bool matchCase, bool matchWholeWord, bool useRegex);
+
+        IEnumerable<QuestionnaireEntityReference> FindAll(QuestionnaireDocument questionnaire, string searchFor, bool matchCase, bool matchWholeWord, bool useRegex);
+
+        int ReplaceTexts(QuestionnaireRevision questionnaireId, Guid responsibleId, string searchFor, string replaceWith, bool matchCase, bool matchWholeWord, bool useRegex);
+
+        int ReplaceTexts(QuestionnaireDocument questionnaire, Guid responsibleId, string searchFor, string replaceWith, bool matchCase, bool matchWholeWord, bool useRegex);
     }
 }
