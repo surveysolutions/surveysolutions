0244c0a4b6eb711e66b2b369ce9c6f1832a6caa5
Victor Gladkikh <me@alfeg.net>
2020-03-23 09:21:00 +0200

    KP-13859 Missing indexes for AnswersOnFeaturedQuesions

---
+++ src/Infrastructure/WB.Persistence.Headquarters/Migrations/ReadSide/M2020/M202003230900_AddMissingIndexesForAnswersOnFeaturedQuesions.cs
@@ -0,0 +1,19 @@
+﻿using FluentMigrator;
+
+namespace WB.Persistence.Headquarters.Migrations.ReadSide
+{
+    [Migration(202003230900)]
+    public class M202003230900_AddMissingIndexesForAnswersOnFeaturedQuesions : Migration
+    {
+        public override void Up()
+        {
+            Execute.Sql(@"
+                CREATE INDEX IF NOT EXISTS answerstofeaturedquestions_interview_id_position_idx 
+                    ON readside.answerstofeaturedquestions USING btree(interview_id, ""position"")");
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
