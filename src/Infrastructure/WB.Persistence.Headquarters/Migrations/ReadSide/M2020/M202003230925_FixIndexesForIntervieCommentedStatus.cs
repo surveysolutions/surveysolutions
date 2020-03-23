c7146bf73d71ab6cc3a08e6ad86c76669bb9c71b
Victor Gladkikh <me@alfeg.net>
2020-03-23 09:32:49 +0200

    KP-13860 Fix PK and indexes for InterviewCommentedStatuses table

---
+++ src/Infrastructure/WB.Persistence.Headquarters/Migrations/ReadSide/M2020/M202003230925_FixIndexesForIntervieCommentedStatus.cs
@@ -0,0 +1,28 @@
+﻿using FluentMigrator;
+
+namespace WB.Persistence.Headquarters.Migrations.ReadSide
+{
+    [Migration(202003230925)]
+    public class M202003230925_FixIndexesForIntervieCommentedStatus : Migration
+    {
+        public override void Up()
+        {
+            Execute.Sql(@"ALTER TABLE readside.interviewcommentedstatuses 
+                ADD CONSTRAINT PK_interviewcommentedstatuses PRIMARY KEY 
+                USING INDEX ""UniqueIdIndex"";");
+
+            Execute.Sql(@"
+                DROP INDEX readside.interviewcommentedstatuses_interviewid_idx;
+                DROP INDEX readside.interviewcommentedstatuses_status_indx;
+                DROP INDEX readside.interviewcommentedstatuses_timestamp;");
+
+            Execute.Sql(@"CREATE INDEX interviewcommentedstatuses_interview_id_idx 
+                ON readside.interviewcommentedstatuses (interview_id,""position"");");
+        }
+
+        public override void Down()
+        {
+            
+        }
+    }
+}
