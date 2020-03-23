79596454a0146f18a38451930740bb8ec8dfa4ca
Victor Gladkikh <me@alfeg.net>
2020-03-23 12:32:22 +0200

    KP-13861 Drop unused indexes for CumulativeReportStatusChanges

---
+++ src/Infrastructure/WB.Persistence.Headquarters/Migrations/ReadSide/M2020/M202003230935_FixIndexesForCumulativeReportStatusChanges.cs
@@ -0,0 +1,20 @@
+﻿using FluentMigrator;
+
+namespace WB.Persistence.Headquarters.Migrations.ReadSide
+{
+    [Migration(202003230935)]
+    public class M202003230935_FixIndexesForCumulativeReportStatusChanges : Migration
+    {
+        public override void Up()
+        {
+            Execute.Sql(@"DROP INDEX readside.cumulativereportstatuschanges_date;
+                DROP INDEX readside.cumulativereportstatuschanges_eventsequence_indx;
+                DROP INDEX readside.cumulativereportstatuschanges_interviewid_idx;");
+        }
+
+        public override void Down()
+        {
+
+        }
+    }
+}
