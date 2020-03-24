611e9317014459d82b6096d9ca2a34b29f95ac2b
Andrey Kozhyn <andrii.kozhyn@outlook.com>
2020-03-24 13:15:16 +0200

    KP-13832 export assignments by questionnaire when no other filters selected

--- src/Services/Export/WB.Services.Export/Migrations/20200324104411_AddQuestionnaireIdToAssignment.Designer.cs
+++ src/Services/Export/WB.Services.Export/Migrations/20200324104411_AddQuestionnaireIdToAssignment.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using WB.Services.Export.Infrastructure;
@@ -9,9 +10,10 @@ using WB.Services.Export.Infrastructure;
 namespace WB.Services.Export.Migrations
 {
     [DbContext(typeof(TenantDbContext))]
-    partial class TenantDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200324104411_AddQuestionnaireIdToAssignment")]
+    partial class AddQuestionnaireIdToAssignment
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
