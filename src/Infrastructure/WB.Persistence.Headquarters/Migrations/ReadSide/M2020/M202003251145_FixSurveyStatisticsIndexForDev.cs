4f1a14587698596297a0d9c5b381de09eac26382
Victor Gladkikh <me@alfeg.net>
2020-03-25 11:57:28 +0200

    KP-13858 Revert of M202003201151_IndexOnSurveyStatistics
    It's actually impossible to use single index for report statistics table
    - index over interview_id is used during data denormalization
    - index over entity_id is used during report generation
    
    Order of columns in composite index is matter

---
+++ src/Infrastructure/WB.Persistence.Headquarters/Migrations/ReadSide/M2020/M202003251145_FixSurveyStatisticsIndexForDev.cs
@@ -0,0 +1,22 @@
+﻿using FluentMigrator;
+
+namespace WB.Persistence.Headquarters.Migrations.ReadSide
+{
+    [Migration(2020_03_25_11_45)]
+    public class M202003251145_FixSurveyStatisticsIndexForDev : Migration
+    {
+        public override void Up()
+        {
+            // this is revert of M202003201151_IndexOnSurveyStatistics for all dev/hq/rc envs that deployed old code
+            // this code will do nothing for production instances
+            Execute.Sql("CREATE INDEX IF NOT EXISTS idx_report_statistics_entityid on readside.report_statistics (entity_id)");
+            Execute.Sql("CREATE INDEX IF NOT EXISTS idx_report_statistics_interviewid on readside.report_statistics (entity_id)");
+            Execute.Sql("DROP INDEX IF EXISTS readside.report_statistics_interview_id_idx");
+        }
+
+        public override void Down()
+        {
+
+        }
+    }
+}
