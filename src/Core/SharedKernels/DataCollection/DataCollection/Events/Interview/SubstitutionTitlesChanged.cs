2f556426a9eb47bdca2507773afe195645e78878
Pavel Stovpivskyi <sby@rambler.ru>
2020-03-27 16:24:27 +0200

    KP-13869 don't save collections in json in db

--- src/Core/SharedKernels/DataCollection/DataCollection/Events/Interview/SubstitutionTitlesChanged.cs
+++ src/Core/SharedKernels/DataCollection/DataCollection/Events/Interview/SubstitutionTitlesChanged.cs
@@ -1,19 +1,26 @@
 ﻿using System;
 using System.Linq;
+using Newtonsoft.Json;
 using WB.Core.SharedKernels.DataCollection.Events.Interview.Base;
 
 namespace WB.Core.SharedKernels.DataCollection.Events.Interview
 {
-    public class SubstitutionTitlesChanged : QuestionsPassiveEvent
+    public class SubstitutionTitlesChanged : InterviewPassiveEvent
     {
+        [JsonIgnore]
+        public Identity[] Questions { get; }
+
+        [JsonIgnore]
         public Identity[] StaticTexts { get; }
 
-        public Identity[] Groups { get; set; }
+        [JsonIgnore]
+        public Identity[] Groups { get; }
 
         public SubstitutionTitlesChanged(Identity[] questions, Identity[] staticTexts, 
             Identity[] groups, DateTimeOffset originDate)
-            : base(questions, originDate)
+            : base(originDate)
         {
+            this.Questions = questions?.ToArray() ?? new Identity[] { };
             this.StaticTexts = staticTexts?.ToArray() ?? new Identity[]{};
             this.Groups = groups?.ToArray() ?? new Identity[] {};
         }
