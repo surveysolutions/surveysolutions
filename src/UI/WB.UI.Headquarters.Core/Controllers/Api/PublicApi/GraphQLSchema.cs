565202f36304d1dfeb8bbf062fbe41f8923da086
Andrii Kozhyn <hedgic@gmail.com>
2020-03-30 14:53:55 +0300

    KP-13885 extended schema with required fields, added paging

--- src/UI/WB.UI.Headquarters.Core/Controllers/Api/PublicApi/GraphQLSchema.cs
+++ src/UI/WB.UI.Headquarters.Core/Controllers/Api/PublicApi/GraphQLSchema.cs
@@ -2,6 +2,8 @@ using System.Linq;
 using HotChocolate;
 using HotChocolate.Types;
 using HotChocolate.Types.Filters;
+using HotChocolate.Types.Relay;
+using Main.Core.Entities.SubEntities;
 using WB.Core.BoundedContexts.Headquarters.Views.Interview;
 using WB.Core.SharedKernels.DataCollection.ValueObjects.Interview;
 using WB.Infrastructure.Native.Storage.Postgre;
@@ -17,8 +19,35 @@ namespace WB.UI.Headquarters.Controllers.Api.PublicApi
             descriptor.Field(x => x.SummaryId)
                 .Name("id")
                 .Type<NonNullType<IdType>>();
+            
             descriptor.Field(x => x.Status)
                 .Type<NonNullType<EnumType<InterviewStatus>>>();
+            
+            descriptor.Field(x => x.ResponsibleName)
+                .Type<NonNullType<StringType>>();
+
+            descriptor.Field(x => x.ResponsibleRole)
+                .Type<NonNullType<EnumType<UserRoles>>>();
+
+            descriptor.Field(x => x.AssignmentId)
+                .Type<NonNullType<IntType>>();
+
+            descriptor.Field(x => x.CreatedDate)
+                .Description("Utc creation date");
+
+            descriptor.Field(x => x.Key)
+                .Type<NonNullType<StringType>>();
+            
+            descriptor.Field(x => x.UpdateDate)
+                .Description("Utc last updated date");
+            
+            descriptor.Field(x => x.ReceivedByInterviewer);
+            
+            descriptor.Field(x => x.ErrorsCount)
+                .Name("invalidAnswersCount");
+
+            descriptor.Field(x => x.QuestionnaireId);
+            descriptor.Field(x => x.QuestionnaireVersion);
             
             descriptor.Field(x => x.AnswersToFeaturedQuestions)
                 .Name("questions")
@@ -32,7 +61,7 @@ namespace WB.UI.Headquarters.Controllers.Api.PublicApi
                         .ToList();
                     return answers;
                 })
-                .Type<NonNullType<ListType<AnswerObject>>>();
+                .Type<NonNullType<ListType<NonNullType<AnswerObject>>>>();
         }
     }
 
@@ -41,14 +70,12 @@ namespace WB.UI.Headquarters.Controllers.Api.PublicApi
         protected override void Configure(IObjectTypeDescriptor<QuestionAnswer> descriptor)
         {
             descriptor.BindFieldsExplicitly();
+            
             descriptor.Field(x => x.Answer)
                 .Type<StringType>();
             
             descriptor.Field(x => x.Variable)
-                .Type<NonNullType<IdType>>();
-
-            descriptor.Field(x => x.InterviewSummary)
-                .Name("interview");
+                .Type<NonNullType<StringType>>();
         }
     }
     
@@ -68,7 +95,8 @@ namespace WB.UI.Headquarters.Controllers.Api.PublicApi
 
             descriptor.Field(x => x.GetInterviews(default))
                 .Type<NonNullType<ListType<InterviewSummaryObject>>>()
-                .UseFiltering<InterviewsQueryFilerType>();
+                .UseFiltering<InterviewsQueryFilerType>()
+                .UsePaging<StringType>();
         }
     }
 
@@ -76,13 +104,61 @@ namespace WB.UI.Headquarters.Controllers.Api.PublicApi
     {
         protected override void Configure(IFilterInputTypeDescriptor<InterviewSummary> descriptor)
         {
-            descriptor.BindFieldsExplicitly()
-                .Filter(x => x.Status)
+            descriptor.BindFieldsExplicitly();
+
+            descriptor.Filter(x => x.Status)
                 .BindFiltersExplicitly()
-                .AllowEquals().And().AllowNotEquals().And().AllowIn().And().AllowNotIn();
+                .AllowEquals().And().AllowNotEquals().And().AllowIn();
+            
+            descriptor.Filter(x => x.Key)
+                .BindFiltersExplicitly()
+                .AllowEquals().And().AllowStartsWith().And().AllowContains().And().AllowIn();
+            descriptor.Filter(x => x.AssignmentId)
+                .BindFiltersExplicitly()
+                .AllowEquals().And().AllowIn().And().AllowNotEquals();
+            descriptor.Filter(x => x.CreatedDate)
+                .BindFiltersExplicitly()
+                .AllowGreaterThan()
+                .And().AllowGreaterThanOrEquals()
+                .And().AllowLowerThan()
+                .And().AllowLowerThanOrEquals()
+                .And().AllowNotGreaterThan()
+                .And().AllowNotGreaterThanOrEquals()
+                .And().AllowNotLowerThan()
+                .And().AllowNotLowerThanOrEquals();
+            descriptor.Filter(x => x.ResponsibleName)
+                .BindFiltersExplicitly()
+                .AllowEquals().And().AllowStartsWith().And().AllowIn().And().AllowNotIn();
+            descriptor.Filter(x => x.ResponsibleRole)
+                .BindFiltersExplicitly()
+                .AllowEquals();
+            
+            descriptor.Filter(x => x.UpdateDate).BindFiltersExplicitly()
+                .AllowGreaterThan()
+                .And().AllowGreaterThanOrEquals()
+                .And().AllowLowerThan()
+                .And().AllowLowerThanOrEquals()
+                .And().AllowNotGreaterThan()
+                .And().AllowNotGreaterThanOrEquals()
+                .And().AllowNotLowerThan()
+                .And().AllowNotLowerThanOrEquals();
+            
+            descriptor.Filter(x => x.ReceivedByInterviewer).BindFiltersExplicitly().AllowEquals();
+            descriptor.Filter(x => x.ErrorsCount).BindFiltersExplicitly()
+                .AllowEquals().And().AllowGreaterThan();
+            
             descriptor.List(x => x.AnswersToFeaturedQuestions)
                 .BindExplicitly()
-                .AllowSome(y => y.BindFieldsExplicitly().Filter(f => f.Answer).BindFiltersImplicitly()).Name("answers_some");
+                .AllowSome(y =>
+                {
+                    y.BindFieldsExplicitly();
+                    
+                    y.Filter(f => f.Answer)
+                        .BindFiltersImplicitly();
+                    y.Filter(x => x.Variable)
+                        .BindFiltersExplicitly()
+                        .AllowEquals();
+                }).Name("answers_some");
         }
     }
 }
