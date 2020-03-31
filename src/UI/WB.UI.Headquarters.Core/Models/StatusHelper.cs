352b77ca7dc6019ca93a46f414d17e8c01e6c04d
Pavel Stovpivskyi <sby@rambler.ru>
2020-03-31 20:03:40 +0300

    KP-13896 Do not show approved by hq for hq interviews list

--- src/UI/WB.UI.Headquarters.Core/Models/StatusHelper.cs
+++ src/UI/WB.UI.Headquarters.Core/Models/StatusHelper.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using WB.Core.BoundedContexts.Headquarters;
+using WB.Core.BoundedContexts.Headquarters.Resources;
+using WB.Core.BoundedContexts.Headquarters.Services;
+using WB.Core.BoundedContexts.Headquarters.Views.Reposts.Views;
 using WB.Core.BoundedContexts.Headquarters.Views.Survey;
 using WB.Core.SharedKernels.DataCollection.ValueObjects.Interview;
 
@@ -9,6 +12,25 @@ namespace WB.Core.SharedKernels.SurveyManagement.Web.Controllers
 {
     public static class StatusHelper
     {
+        private static ComboboxViewItem ComboboxViewItem(string value, string title) => new ComboboxViewItem()
+        {
+            Key = value,
+            Value = title,
+        };
+
+        private static readonly ComboboxViewItem[] headquartersStatuses =
+        {
+            ComboboxViewItem("All", Strings.AllInterviewers),
+            ComboboxViewItem("AllExceptApprovedByHQ", Strings.AllInterviewersExceptApprovedByHeadquarters),
+            ComboboxViewItem(InterviewStatus.ApprovedByHeadquarters.ToString(), Strings.InterviewStatus_ApprovedByHeadquarters),
+            ComboboxViewItem(InterviewStatus.ApprovedBySupervisor.ToString(), Strings.InterviewStatus_ApprovedBySupervisor),
+            ComboboxViewItem(InterviewStatus.Completed.ToString(), Strings.InterviewStatus_Completed),
+            ComboboxViewItem(InterviewStatus.InterviewerAssigned.ToString(), Strings.InterviewStatus_InterviewerAssigned),
+            ComboboxViewItem(InterviewStatus.RejectedByHeadquarters.ToString(), Strings.InterviewStatus_RejectedByHeadquarters),
+            ComboboxViewItem(InterviewStatus.RejectedBySupervisor.ToString(), Strings.InterviewStatus_RejectedBySupervisor),
+            ComboboxViewItem(InterviewStatus.SupervisorAssigned.ToString(), Strings.InterviewStatus_SupervisorAssigned),
+        };
+
         private static readonly InterviewStatus[] invisibleForUserStatuses =
         {
             InterviewStatus.Created,
@@ -27,24 +49,30 @@ namespace WB.Core.SharedKernels.SurveyManagement.Web.Controllers
                 InterviewStatus.Restarted,
             }).ToArray();
 
-        internal static IEnumerable<SurveyStatusViewItem> GetAllSurveyStatusViewItems(InterviewStatus[] skipStatuses)
+        internal static IEnumerable<ComboboxViewItem> GetAllSurveyStatusViewItems(InterviewStatus[] skipStatuses)
         {
             return from InterviewStatus status in Enum.GetValues(typeof(InterviewStatus))
                    where !skipStatuses.Contains(status)
-                   select new SurveyStatusViewItem
+                   select new ComboboxViewItem
                    {
-                       Status = status,
-                       StatusName = status.ToLocalizeString()
+                       Key = status.ToString(),
+                       Value = status.ToLocalizeString(),
                    };
         }
 
-        public static IEnumerable<SurveyStatusViewItem> GetOnlyActualSurveyStatusViewItems(bool isSupervisor)
+        public static IEnumerable<ComboboxViewItem> GetOnlyActualSurveyStatusViewItems(IAuthorizedUser authorizedUser)
         {
-            var ignoreStatuses = isSupervisor
+            if (authorizedUser.IsHeadquarter || authorizedUser.IsAdministrator)
+                return headquartersStatuses;
+
+            var ignoreStatuses = authorizedUser.IsSupervisor
                 ? invisibleForSupervisorStatuses
                 : invisibleForUserStatuses;
-            return GetAllSurveyStatusViewItems(ignoreStatuses)
-                .OrderBy(status=>status.StatusName);
+            var statuses = GetAllSurveyStatusViewItems(ignoreStatuses)
+                .OrderBy(status=>status.Key)
+                .ToList();
+
+            return statuses;
         }
     }
 }
