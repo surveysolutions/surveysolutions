839029166ff11010ea269cdc1c80ec8e37cb6604
Andrii Kozhyn <hedgic@gmail.com>
2020-03-31 12:57:26 +0300

    KP-13885 authorisation added

--- src/UI/WB.UI.Headquarters.Core/Controllers/Api/PublicApi/Graphql/InterviewsQuery.cs
+++ src/UI/WB.UI.Headquarters.Core/Controllers/Api/PublicApi/Graphql/InterviewsQuery.cs
@@ -1,5 +1,6 @@
 using System.Linq;
 using HotChocolate;
+using WB.Core.BoundedContexts.Headquarters.Services;
 using WB.Core.BoundedContexts.Headquarters.Views.Interview;
 using WB.Infrastructure.Native.Storage.Postgre;
 
@@ -7,11 +8,22 @@ namespace WB.UI.Headquarters.Controllers.Api.PublicApi.Graphql
 {
     public class InterviewsQuery
     {
-        public IQueryable<InterviewSummary> GetInterviews([Service] IUnitOfWork unitOfWork)
+        public IQueryable<InterviewSummary> GetInterviews([Service] IUnitOfWork unitOfWork, [Service]IAuthorizedUser user)
         {
             unitOfWork.DiscardChanges();
+
+            var interviewSummaries = unitOfWork.Session.Query<InterviewSummary>();
+            if (user.IsSupervisor)
+            {
+                interviewSummaries = interviewSummaries.Where(x => x.TeamLeadId == user.Id || x.ResponsibleId == user.Id);
+            }
+
+            if (user.IsInterviewer)
+            {
+                interviewSummaries = interviewSummaries.Where(x => x.ResponsibleId == user.Id);
+            }
             
-            return unitOfWork.Session.Query<InterviewSummary>();
+            return interviewSummaries;
         }
     }
 }
