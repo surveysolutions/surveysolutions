6e5a79c4cd191553ae76578618b6eda8737709e7
Andrii Kozhyn <hedgic@gmail.com>
2020-04-03 13:20:48 +0300

    KP-13891 fixing tests

--- src/Tests/WB.Tests.Integration/TeamInterviewsFactoryTests/TeamInterviewsFactoryTestContext.cs
+++ src/Tests/WB.Tests.Integration/TeamInterviewsFactoryTests/TeamInterviewsFactoryTestContext.cs
@@ -25,7 +25,8 @@ namespace WB.Tests.Integration.TeamInterviewsFactoryTests
             {
                 typeof(InterviewSummaryMap), typeof(TimeSpanBetweenStatusesMap),
                 typeof(QuestionAnswerMap), typeof(InterviewCommentedStatusMap),
-                typeof(InterviewStatisticsReportRowMap), typeof(InterviewCommentMap)
+                typeof(InterviewStatisticsReportRowMap), typeof(InterviewCommentMap),
+                typeof(QuestionnaireCompositeItemMap)
             }, true, schemaName: "readside");
 
             UnitOfWork = IntegrationCreate.UnitOfWork(sessionFactory);
