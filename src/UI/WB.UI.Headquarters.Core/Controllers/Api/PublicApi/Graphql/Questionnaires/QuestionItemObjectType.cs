f03ecffd402c1f9dc85f2e25a8f60b983e142281
Victor Gladkikh <me@alfeg.net>
2020-04-09 20:19:33 +0300

    KP-13886 Added more GraphQL bindings

--- src/UI/WB.UI.Headquarters.Core/Controllers/Api/PublicApi/Graphql/Questionnaires/QuestionItemObjectType.cs
+++ src/UI/WB.UI.Headquarters.Core/Controllers/Api/PublicApi/Graphql/Questionnaires/QuestionItemObjectType.cs
@@ -1,6 +1,16 @@
+#nullable enable
+using System.Collections.Generic;
+using System.Linq;
+using HotChocolate.Resolvers;
 using HotChocolate.Types;
 using Main.Core.Entities.SubEntities;
+using NHibernate.Linq;
 using WB.Core.BoundedContexts.Headquarters.Views.Questionnaire;
+using WB.Core.SharedKernels.DataCollection;
+using WB.Core.SharedKernels.DataCollection.Implementation.Entities;
+using WB.Core.SharedKernels.DataCollection.Repositories;
+using WB.Infrastructure.Native.Storage.Postgre;
+using WB.UI.Headquarters.Controllers.Api.PublicApi.Graphql.Interviews;
 
 namespace WB.UI.Headquarters.Controllers.Api.PublicApi.Graphql.Questionnaires
 {
@@ -13,19 +23,48 @@ namespace WB.UI.Headquarters.Controllers.Api.PublicApi.Graphql.Questionnaires
             descriptor.Name("Question");
             
             descriptor.Field(x => x.QuestionText)
-                .Type<NonNullType<StringType>>();
+                .Type<StringType>();
 
             descriptor.Field(x => x.StatExportCaption)
                 .Name("variable")
-                .Type<NonNullType<StringType>>();
+                .Type<StringType>();
 
             descriptor.Field(x => x.QuestionScope)
                 .Name("scope")
-                .Type<NonNullType<EnumType<QuestionScope>>>();
+                .Type<EnumType<QuestionScope>>();
+
+            descriptor.Field(x => x.QuestionType)
+                .Name("type")
+                .Type<EnumType<QuestionType>>();
 
             descriptor.Field(x => x.Featured)
                 .Name("identifying")
-                .Type<NonNullType<BooleanType>>();
+                .Type<BooleanType>();
+
+            descriptor.Field("options")
+                .Name("options")
+                .Resolver(context =>
+                    context.BatchDataLoader<int, List<CategoricalOption>>
+                    ("optionsByQuestion", async keys =>
+                    {
+                        var unitOfWork = context.Service<IUnitOfWork>();
+                        var questions = await unitOfWork.Session.Query<QuestionnaireCompositeItem>()
+                            .Where(q => keys.Contains(q.Id)).ToListAsync();
+
+                        if(!questions.Any()) return new Dictionary<int, List<CategoricalOption>>();
+                    //    var lang = context.Variables.GetVariable<string>("language");
+                        var questionnaireStorage = context.Service<IQuestionnaireStorage>();
+                        var questionnaires =  questions.Select(q
+                                => (q.Id, q.EntityId,
+                                    questionnaireStorage.GetQuestionnaire(
+                                        QuestionnaireIdentity.Parse(q.QuestionnaireIdentity), null))).ToList();
+
+                         return questionnaires.Where(q => q.Item3 != null).ToDictionary(
+                                q => q.Id, 
+                                q => q.Item3.GetOptionsForQuestion(q.EntityId, null, null, null).ToList());
+                      
+                    }).LoadAsync(context.Parent<QuestionnaireCompositeItem>().Id, default))
+                .Type<ListType<CategoricalOptionType>>();
         }
     }
 }
--- src/UI/WB.UI.Headquarters.Core/Controllers/Api/PublicApi/Graphql/Questionnaires/QuestionsResolver.cs
+++ src/UI/WB.UI.Headquarters.Core/Controllers/Api/PublicApi/Graphql/Questionnaires/QuestionsResolver.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using HotChocolate;
 using WB.Core.BoundedContexts.Headquarters.Views.Interview;
 using WB.Core.BoundedContexts.Headquarters.Views.Questionnaire;
+using WB.Core.SharedKernels.DataCollection.Implementation.Entities;
 using WB.Infrastructure.Native.Storage.Postgre;
 
 namespace WB.UI.Headquarters.Controllers.Api.PublicApi.Graphql.Questionnaires
@@ -12,8 +13,11 @@ namespace WB.UI.Headquarters.Controllers.Api.PublicApi.Graphql.Questionnaires
         public IQueryable<QuestionnaireCompositeItem> Questions(Guid id, long version, string language, [Service] IUnitOfWork unitOfWork)
         {
             unitOfWork.DiscardChanges();
+            var qid = new QuestionnaireIdentity(id, version).ToString();
 
-            return unitOfWork.Session.Query<QuestionnaireCompositeItem>().Where(x => x.EntityType == EntityType.Question);
+            return unitOfWork.Session.Query<QuestionnaireCompositeItem>()
+                .Where(x => x.QuestionnaireIdentity == qid)
+                .Where(x => x.EntityType == EntityType.Question);
         }
     }
 }
