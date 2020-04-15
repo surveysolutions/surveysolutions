773f496a456c21b0c96ff4e8188bc3ad3d744d57
Andrii Kozhyn <hedgic@gmail.com>
2020-04-15 11:27:29 +0300

    KP-13930 add received by tablet to public api

--- src/UI/WB.UI.Headquarters.Core/Controllers/Api/PublicApi/Models/Assignments.cs
+++ src/UI/WB.UI.Headquarters.Core/Controllers/Api/PublicApi/Models/Assignments.cs
@@ -71,14 +71,14 @@ namespace WB.UI.Headquarters.API.PublicApi.Models
         public bool Archived { get; set; } = false;
 
         /// <summary>
-        /// Date when assignment were created
+        /// Date (UTC) when assignment were created
         /// Can be used for ordering
         /// </summary>
         [DataMember]
         public DateTime CreatedAtUtc { get; set; }
 
         /// <summary>
-        /// Last Update Date of assignment
+        /// Last Update Date (UTC) of assignment
         /// Can be used for ordering
         /// </summary>
         [DataMember]
@@ -92,6 +92,12 @@ namespace WB.UI.Headquarters.API.PublicApi.Models
 
         [DataMember]
         public bool? WebMode { get; set; }
+        
+        /// <summary>
+        /// Represents date (UTC) when assignment was received by tablet
+        /// </summary>
+        [DataMember]
+        public DateTime? ReceivedByTabletAtUtc { get; set; }
     }
 
     public class AssignmentAssignRequest
