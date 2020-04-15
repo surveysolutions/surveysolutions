e8b2ab380f4ccc5696f8d63f150a742b1315e2b8
Victor Hladkikh <me@alfeg.net>
2020-04-15 15:48:27 +0300

    Revert "KP-13886 Cleaning up unused api"
    This reverts commit 7f287c3d45777c94259e4c236f574f2e9fcaae6f.

---
+++ src/Tests/WB.Tests.Web/Headquarters/Controllers/InterviewApiControllerTests/when_getting_all_interviews_containing_tags_in_prefield.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Moq;
+using WB.Core.BoundedContexts.Headquarters.Views.Interview;
+using WB.Core.SharedKernels.SurveyManagement.Web.Models;
+using WB.UI.Headquarters.Controllers.Api;
+using WB.UI.Headquarters.Models.Api;
+
+namespace WB.Tests.Web.Headquarters.Controllers.InterviewApiControllerTests
+{
+    internal class when_getting_all_interviews_containing_tags_in_prefilled : InterviewApiControllerTestsContext
+    {
+        [NUnit.Framework.OneTimeSetUp]
+        public void context()
+        {
+            var allInterviewsViewFactoryFactoryMock = new Mock<IAllInterviewsFactory>();
+            var interviewSummary = new AllInterviewsView()
+            {
+                Items = new List<AllInterviewsViewItem>()
+                {
+                    new AllInterviewsViewItem()
+                    {
+                        InterviewId = interviewId,
+                        FeaturedQuestions = new List<InterviewFeaturedQuestion>()
+                        {
+                            new InterviewFeaturedQuestion()
+                            {
+                                Question = titleWithTags
+                            }
+                        }
+                    }
+                }
+            };
+
+            allInterviewsViewFactoryFactoryMock.Setup(_ => _.Load(Moq.It.IsAny<AllInterviewsInputModel>()))
+                .Returns(interviewSummary);
+
+            controller = CreateController(allInterviewsViewFactory: allInterviewsViewFactoryFactoryMock.Object);
+            BecauseOf();
+        }
+
+        public void BecauseOf() =>
+            viewModel = controller.Interviews(new InterviewsDataTableRequest()
+                {Length = 10, Search = new DataTableRequest.SearchInfo()});
+
+        [NUnit.Framework.Test]
+        public void should_view_model_not_be_null() =>
+            viewModel.Should().NotBeNull();
+
+
+        [NUnit.Framework.Test]
+        public void should_question_title_have_removed_tags() =>
+            viewModel.Data.SingleOrDefault(x => x.InterviewId == interviewId).FeaturedQuestions.First().Question
+                .Should().Be("test &gt; 1");
+
+        private static InterviewApiController controller;
+        private static InterviewsDataTableResponse viewModel;
+        private static Guid interviewId = Guid.Parse("11111111111111111111111111111111");
+
+        private static string titleWithTags = "<i>test > 1</i>";
+    }
+}
