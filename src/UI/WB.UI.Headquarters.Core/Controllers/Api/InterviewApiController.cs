d9b09ee85e6c3be3a58b01936d7dbe670726d8b9
Victor Hladkikh <me@alfeg.net>
2020-04-15 12:56:03 +0300

    KP-13886 Cleaning up unused api

--- src/UI/WB.UI.Headquarters.Core/Controllers/Api/InterviewApiController.cs
+++ src/UI/WB.UI.Headquarters.Core/Controllers/Api/InterviewApiController.cs
@@ -1,22 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Text;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using WB.Core.BoundedContexts.Headquarters;
-using WB.Core.BoundedContexts.Headquarters.Services;
 using WB.Core.BoundedContexts.Headquarters.Views.ChangeStatus;
 using WB.Core.BoundedContexts.Headquarters.Views.Interview;
-using WB.Core.BoundedContexts.Headquarters.Views.Interviews;
 using WB.Core.SharedKernels.DataCollection.Utils;
-using WB.Core.SharedKernels.DataCollection.ValueObjects.Interview;
 using WB.Core.SharedKernels.SurveyManagement.Web.Models;
-using WB.Enumerator.Native.WebInterview;
-using WB.Infrastructure.Native.Sanitizer;
 
 namespace WB.UI.Headquarters.Controllers.Api
 {
@@ -24,145 +15,16 @@ namespace WB.UI.Headquarters.Controllers.Api
     [Route("api/{controller}/{action}")]
     public class InterviewApiController : ControllerBase
     {
-        private readonly IAuthorizedUser authorizedUser;
         private readonly IAllInterviewsFactory allInterviewsViewFactory;
-        private readonly ITeamInterviewsFactory teamInterviewViewFactory;
         private readonly IChangeStatusFactory changeStatusFactory;
 
         public InterviewApiController(
-            IAuthorizedUser authorizedUser, 
             IAllInterviewsFactory allInterviewsViewFactory,
-            ITeamInterviewsFactory teamInterviewViewFactory,
             IChangeStatusFactory changeStatusFactory)
         {
-            this.authorizedUser = authorizedUser;
             this.allInterviewsViewFactory = allInterviewsViewFactory;
-            this.teamInterviewViewFactory = teamInterviewViewFactory;
             this.changeStatusFactory = changeStatusFactory;
         }
-
-        [HttpGet]
-        public InterviewsDataTableResponse Interviews([FromQuery] InterviewsDataTableRequest request)
-        {
-            var input = new AllInterviewsInputModel
-            {
-                Page = request.PageIndex,
-                PageSize = request.PageSize,
-                Orders = request.GetSortOrderRequestItems(),
-                QuestionnaireId = request.QuestionnaireId,
-                QuestionnaireVersion = request.QuestionnaireVersion,
-                SupervisorOrInterviewerName = request.ResponsibleName,
-                Statuses = GetFilterByStatus(request),
-                SearchBy = request.SearchBy ?? request.Search?.Value,
-                TeamId = request.TeamId,
-                UnactiveDateStart = request.UnactiveDateStart?.ToUniversalTime(),
-                UnactiveDateEnd = request.UnactiveDateEnd?.ToUniversalTime(),
-                AssignmentId = request.AssignmentId
-            };
-
-            var allInterviews = this.allInterviewsViewFactory.Load(input);
-
-            foreach (var x in allInterviews.Items)
-            {
-                Enum.TryParse(x.Status, out InterviewStatus myStatus);
-                x.Status = myStatus.ToLocalizeString();
-
-                foreach (var y in x.FeaturedQuestions)
-                    y.Question = y.Question.RemoveHtmlTags();
-            }
-
-            var response = new InterviewsDataTableResponse
-            {
-                Draw = request.Draw + 1,
-                RecordsTotal = allInterviews.TotalCount,
-                RecordsFiltered = allInterviews.TotalCount,
-                Data = allInterviews.Items
-            };
-
-            return response;
-        }
-
-        private static InterviewStatus[] GetFilterByStatus(InterviewsDataTableRequest request)
-        {
-            if (string.IsNullOrEmpty(request.Status))
-                return null;
-
-            if (Enum.TryParse(request.Status, out InterviewStatus enumValue))
-                return new[] { enumValue };
-
-            if (request.Status == "AllExceptApprovedByHQ")
-                return ((InterviewStatus[])Enum.GetValues(typeof(InterviewStatus))).Where(s => s != InterviewStatus.ApprovedByHeadquarters).ToArray();
-
-            throw new ArgumentException("Unknown status filter value: " + request.Status);
-        }
-
-        [HttpGet]
-        [Authorize(Roles = "Interviewer")]
-        public InterviewsDataTableResponse GetInterviews(InterviewsDataTableRequest request)
-        {
-            var input = new AllInterviewsInputModel
-            {
-                Page = request.PageIndex,
-                PageSize = request.PageSize,
-                Orders = request.GetSortOrderRequestItems(),
-                QuestionnaireId = request.QuestionnaireId,
-                QuestionnaireVersion = request.QuestionnaireVersion,
-                Statuses = request.Statuses,
-                SearchBy = request.SearchBy ?? request.Search?.Value,
-                ResponsibleId = this.authorizedUser.Id,
-                AssignmentId = request.AssignmentId
-            };
-
-            var allInterviews = this.allInterviewsViewFactory.Load(input);
-
-            foreach (var x in allInterviews.Items) 
-                foreach (var y in x.FeaturedQuestions) y.Question = y.Question.RemoveHtmlTags();
-
-            var response = new InterviewsDataTableResponse
-            {
-                Draw = request.Draw + 1,
-                RecordsTotal = allInterviews.TotalCount,
-                RecordsFiltered = allInterviews.TotalCount,
-                Data = allInterviews.Items
-            };
-
-            return response;
-        }
-        
-        [HttpGet]
-        public TeamInterviewsDataTableResponse GetTeamInterviews([FromQuery] InterviewsDataTableRequest request)
-        {
-            var input = new TeamInterviewsInputModel
-            {
-                Page = request.PageIndex,
-                PageSize = request.PageSize,
-                Orders = request.GetSortOrderRequestItems(),
-                QuestionnaireId = request.QuestionnaireId,
-                QuestionnaireVersion = request.QuestionnaireVersion,
-                SearchBy = request.SearchBy,
-                Statuses = GetFilterByStatus(request),
-                ResponsibleName = request.ResponsibleName,
-                ViewerId = this.authorizedUser.Id,
-                UnactiveDateStart = request.UnactiveDateStart?.ToUniversalTime(),
-                UnactiveDateEnd = request.UnactiveDateEnd?.ToUniversalTime(),
-                AssignmentId = request.AssignmentId
-            };
-
-            var teamInterviews = this.teamInterviewViewFactory.Load(input);
-
-            foreach (var x in teamInterviews.Items) foreach (var y in x.FeaturedQuestions) y.Question = y.Question.RemoveHtmlTags();
-
-            var response = new TeamInterviewsDataTableResponse
-            {
-                Draw = request.Draw + 1,
-                RecordsTotal = teamInterviews.TotalCount,
-                RecordsFiltered = teamInterviews.TotalCount,
-                Data = teamInterviews.Items
-            };
-
-            return response;
-        }
-
 
         [HttpPost]
         [Authorize(Roles = "Administrator, Supervisor, Headquarter")]
