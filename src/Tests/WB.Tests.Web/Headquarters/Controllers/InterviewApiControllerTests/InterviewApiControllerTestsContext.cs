02093032f6988180c9bf53cf986a2e9ad8a57c1c
Victor Gladkikh <me@alfeg.net>
2020-04-15 15:48:27 +0300

    Revert "KP-13886 Cleaning up unused api"
    This reverts commit 81f2dc1ba4bfba0f4585e2b31b17507c737fdd66.

--- src/Tests/WB.Tests.Web/Headquarters/Controllers/InterviewApiControllerTests/InterviewApiControllerTestsContext.cs
+++ src/Tests/WB.Tests.Web/Headquarters/Controllers/InterviewApiControllerTests/InterviewApiControllerTestsContext.cs
@@ -1,5 +1,10 @@
-﻿using WB.Core.BoundedContexts.Headquarters.Views.ChangeStatus;
+﻿using Moq;
+using WB.Core.BoundedContexts.Headquarters.Services;
+using WB.Core.BoundedContexts.Headquarters.Views.ChangeStatus;
 using WB.Core.BoundedContexts.Headquarters.Views.Interview;
+using WB.Core.BoundedContexts.Headquarters.Views.Interviews;
+using WB.Core.GenericSubdomains.Portable.Services;
+using WB.Core.Infrastructure.CommandBus;
 using WB.Tests.Abc;
 using WB.UI.Headquarters.Controllers.Api;
 
@@ -8,12 +13,16 @@ namespace WB.Tests.Web.Headquarters.Controllers.InterviewApiControllerTests
     [NUnit.Framework.TestOf(typeof(InterviewApiController))]
     internal class InterviewApiControllerTestsContext
     {
-        protected static InterviewApiController CreateController(
+        protected static InterviewApiController CreateController(ICommandService commandService = null,
+            IAuthorizedUser authorizedUser = null, ILogger logger = null,
             IAllInterviewsFactory allInterviewsViewFactory = null,
+            ITeamInterviewsFactory teamInterviewViewFactory = null,
             IChangeStatusFactory changeStatusFactory = null)
         {
             return new InterviewApiController(
+                authorizedUser ?? Mock.Of<IAuthorizedUser>(), 
                 allInterviewsViewFactory ?? Stub<IAllInterviewsFactory>.WithNotEmptyValues,
+                teamInterviewViewFactory ?? Stub<ITeamInterviewsFactory>.WithNotEmptyValues,
                 changeStatusFactory ?? Stub<IChangeStatusFactory>.WithNotEmptyValues);
         }
     }
---
+++ src/UI/WB.UI.Headquarters.Core/Models/DocumentFilter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using WB.Core.BoundedContexts.Headquarters.Views.Reposts.Views;
+using WB.Core.BoundedContexts.Headquarters.Views.Survey;
+using WB.Core.BoundedContexts.Headquarters.Views.User;
+
+namespace WB.Core.SharedKernels.SurveyManagement.Web.Models
+{
+    public class DocumentFilter
+    {
+        public IEnumerable<UsersViewItem> Users { get; set; } 
+        public IEnumerable<UsersViewItem> Responsibles { get; set; }
+        public IEnumerable<TemplateViewItem> Templates { get; set; }
+        public IEnumerable<SurveyStatusViewItem> Statuses { get; set; }
+    }
+}
