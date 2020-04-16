33033c9916800229e0fed8a222ef248e02c0bff7
Andrii Kozhyn <hedgic@gmail.com>
2020-04-16 14:53:21 +0300

    KP-13937 web interviewer should also use graphql to load grids

--- src/UI/WB.UI.Headquarters.Core/Controllers/Api/PublicApi/Graphql/Interviews/InterviewSummaryObjectType.cs
+++ src/UI/WB.UI.Headquarters.Core/Controllers/Api/PublicApi/Graphql/Interviews/InterviewSummaryObjectType.cs
@@ -4,6 +4,7 @@ using HotChocolate.Resolvers;
 using HotChocolate.Types;
 using Main.Core.Entities.SubEntities;
 using NHibernate.Linq;
+using WB.Core.BoundedContexts.Headquarters.Resources;
 using WB.Core.BoundedContexts.Headquarters.Views.Interview;
 using WB.Core.SharedKernels.DataCollection.ValueObjects.Interview;
 using WB.Infrastructure.Native.Storage.Postgre;
@@ -24,27 +25,43 @@ namespace WB.UI.Headquarters.Controllers.Api.PublicApi.Graphql.Interviews
             descriptor.Field(x => x.Status)
                 .Type<EnumType<InterviewStatus>>();
             
-            descriptor.Field(x => x.ResponsibleName).Type<StringType>();
-            descriptor.Field(x => x.ResponsibleNameLowerCase).Type<StringType>();
+            descriptor.Field(x => x.ResponsibleName).Type<StringType>()
+                .Description("Login of current responsible user");
+            
+            descriptor.Field(x => x.ResponsibleNameLowerCase).Type<StringType>()
+                .Description($"Lower cased version of {nameof(InterviewSummary.ResponsibleName)} field");
 
             descriptor.Field(x => x.ResponsibleId).Type<UuidType>();
             descriptor.Field(x => x.ResponsibleRole).Type<EnumType<UserRoles>>();
 
-            descriptor.Field(x => x.TeamLeadName).Type<StringType>();
-            descriptor.Field(x => x.TeamLeadNameLowerCase).Type<StringType>();
+            descriptor.Field(x => x.TeamLeadName).Type<StringType>()
+                .Description("Supervisor login who is responsible for interview");
+            descriptor.Field(x => x.ReceivedByInterviewer)
+                .Type<StringType>();
+            descriptor.Field(x => x.WasCompleted)
+                .Description("Indicates if interview was ever completed by interviewer")
+                .Type<StringType>();
+            descriptor.Field(x => x.TeamLeadNameLowerCase)
+                .Description("Lowercased version of team lead name")
+                .Type<StringType>();
 
-            descriptor.Field(x => x.AssignmentId).Type<IntType>();
+            descriptor.Field(x => x.AssignmentId).Type<IntType>()
+                  .Description("Identifier for the assignment to which this interview belongs");
 
             descriptor.Field(x => x.CreatedDate)
                 .Description("Utc creation date");
 
-            descriptor.Field(x => x.Key).Type<StringType>();
+            descriptor.Field(x => x.Key).Type<StringType>()  
+                .Description("Short case identifier that appears throughout the system - in Headquarters, Supervisor, and Interviewer.");
             
-            descriptor.Field(x => x.UpdateDate).Description("Utc last updated date");
+            descriptor.Field(x => x.UpdateDate)
+                .Description("Represents date (UTC) when interview was changed last time");
             
-            descriptor.Field(x => x.ReceivedByInterviewer);
+            descriptor.Field(x => x.ReceivedByInterviewer)
+                .Description("Indicator for whether the interview is on the interviewer’s tablet now");
             
             descriptor.Field(x => x.ErrorsCount)
+                .Description("Shows total number of invalid questions and static texts in the interview. Multiple failed validation conditions on same entity are counted as 1.")
                 .Type<IntType>();
 
             descriptor.Field(x => x.QuestionnaireId);
@@ -52,6 +69,7 @@ namespace WB.UI.Headquarters.Controllers.Api.PublicApi.Graphql.Interviews
             
             descriptor.Field(x => x.AnswersToFeaturedQuestions)
                 .Name("identifyingQuestions")
+                .Description("Information that identifies each assignment. These are the answers to questions marked as identifying in Designer.")
                 .Resolver(context => 
                     context.GroupDataLoader<string, QuestionAnswer>
                         ("answersByInterview", async keys =>
