83314fb30bace80fbfe09c3bc8af7ed76f767bb2
Pavel Stovpivskyi <630198+spuv@users.noreply.github.com>
2020-04-21 16:19:43 +0300

    KP-13902 reject packages with non completed status

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Implementation/Synchronization/InterviewPackagesService.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Implementation/Synchronization/InterviewPackagesService.cs
@@ -21,6 +21,7 @@ using WB.Core.SharedKernels.DataCollection.Events;
 using WB.Core.SharedKernels.DataCollection.Events.Interview;
 using WB.Core.SharedKernels.DataCollection.Exceptions;
 using WB.Core.SharedKernels.DataCollection.Services;
+using WB.Core.SharedKernels.DataCollection.ValueObjects.Interview;
 using WB.Core.SharedKernels.DataCollection.WebApi;
 using WB.Enumerator.Native.WebInterview;
 using WB.Infrastructure.Native.Storage.Postgre;
@@ -263,8 +264,14 @@ namespace WB.Core.BoundedContexts.Headquarters.Implementation.Synchronization
             catch (Exception exception)
             {
                 this.logger.Error(
-                    $"Interview events by {interview.InterviewId} processing failed. Reason: '{exception.Message}'",
+                    $"Interview events by {interview.InterviewId} processing failed. Package status {interview.InterviewStatus}. Reason: '{exception.Message}'",
                     exception);
+
+                // don't save package for partial synch
+                if (interview.InterviewStatus != InterviewStatus.Completed)
+                {
+                    throw;
+                }
 
                 var interviewException = exception as InterviewException;
                 if (interviewException == null)
