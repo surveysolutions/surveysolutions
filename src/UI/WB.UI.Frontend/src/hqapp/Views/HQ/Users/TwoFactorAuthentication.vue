e8767df0a3e7e947f55298b328295bd8d3379a51
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2020-04-22 21:47:49 -0400

    KP-13936 Introduce 2fa [change navigation]

--- src/UI/WB.UI.Frontend/src/hqapp/Views/HQ/Users/TwoFactorAuthentication.vue
+++ src/UI/WB.UI.Frontend/src/hqapp/Views/HQ/Users/TwoFactorAuthentication.vue
@@ -23,77 +23,76 @@
             <h1>{{$t('Strings.HQ_Views_Manage_Title')}}</h1>
         </div>
         <div class="extra-margin-bottom">
-            <div class="profile flex-row">                
-                <div class="col-md-3">
-                    <ul class="nav flex-block">
-                        <li class="nav-item"><a class="nav-link"
-                            id="profile"
-                            :href="profileUrl">{{$t('Pages.AccountManage_Profile')}}</a></li>
-                        <li class="nav-item"><a class="nav-link"
-                            id="profile"
-                            v-bind:href="getUrl('../../Users/ChangePassword')">{{$t('Pages.AccountManage_ChangePassword')}}</a></li>                            
-                        <li class="nav-item open"><a class="nav-link active"
-                            id="two-factor"
-                            :href="tfaUrl">{{$t('Pages.AccountManage_TwoFactorAuth')}}</a></li>                            
-                    </ul>
-                </div>
-                <div class="col-md-9">
-                    <div>
-                        <div >
-                            <h2>{{$t('Strings.HQ_Views_TwoFactorAuthentication_Title')}}</h2>
-                        </div>
-                        <i>{{$t('Strings.HQ_Views_TwoFactorAuthentication_Description')}}</i>
-                        <form-group v-if="!isOwnProfile"
-                            :label="$t('Pages.AccountManage_Login')">
-                            <TextInput :value="userInfo.userName"
-                                id="UserName"
-                                disabled />
-                        </form-group>
-                        
-                        <div v-if="is2faEnabled">
-                            <div class="alert alert-danger" 
-                                v-if="recoveryCodesLeft == 0">
-                                <strong>You have no recovery codes left.</strong>
-                                <p>You must <a :href="getUrl('../../Users/GenerateRecoveryCodes')">generate a new set of recovery codes</a> before you can log in with a recovery code.</p>
-                            </div>                                
+            <div class="profile">
+                <ul class="nav nav-tabs extra-margin-bottom">
+                    <li class="nav-item"><a class="nav-link"
+                        id="profile"
+                        v-bind:href="getUrl('../../Users/Manage')">{{$t('Pages.AccountManage_Profile')}}</a></li>
+                    <li class="nav-item"><a class="nav-link"
+                        id="password"
+                        v-bind:href="getUrl('../../Users/ChangePassword')">{{$t('Pages.AccountManage_ChangePassword')}}</a></li>                                
+                    <li class="nav-item active"><a class="nav-link active"
+                        id="two-factor"
+                        v-bind:href="getUrl('../../Users/TwoFactorAuthentication')">{{$t('Pages.AccountManage_TwoFactorAuth')}}</a></li>
+                </ul> 
+                
+                <div class="col-sm-12">
                     
-                            <div class="alert alert-danger" 
-                                v-if="recoveryCodesLeft == 1">
-                                <strong>You have 1 recovery code left.</strong>
-                                <p>You can <a :href="getUrl('../../Users/GenerateRecoveryCodes')">generate a new set of recovery codes</a>.</p>
-                            </div>
-                            <div class="alert alert-warning" 
-                                v-if="recoveryCodesLeft <= 3">
-                                <strong>You have {{recoveryCodesLeft}} recovery codes left.</strong>
-                                <p>You should <a :href="getUrl('../../Users/GenerateRecoveryCodes')">generate a new set of recovery codes</a>.</p>
-                            </div>                    
-    
-                            <a :href="getUrl('./Disable2fa')" 
-                                class="btn btn-success"
-                                style="margin-right: 5px;">{{$t('Pages.Disable2fa')}}</a>
-                            <a :href="getUrl('../../Users/GenerateRecoveryCodes')" 
-                                class="btn btn-success">{{$t('Pages.ResetRecoveryCodes')}} </a>
+                    <!-- <div >
+                            <h2>{{$t('Strings.HQ_Views_TwoFactorAuthentication_Title')}}</h2>
+                        </div> -->
+                    <p>{{$t('Strings.HQ_Views_TwoFactorAuthentication_Description')}}</p>
+                    <form-group v-if="!isOwnProfile"
+                        :label="$t('Pages.AccountManage_Login')">
+                        <TextInput :value="userInfo.userName"
+                            id="UserName"
+                            disabled />
+                    </form-group>
+                        
+                    <div v-if="is2faEnabled">
+                        <div class="alert alert-danger" 
+                            v-if="recoveryCodesLeft == 0">
+                            <strong>You have no recovery codes left.</strong>
+                            <p>You must <a :href="getUrl('../../Users/GenerateRecoveryCodes')">generate a new set of recovery codes</a> before you can log in with a recovery code.</p>
+                        </div>                                
+                    
+                        <div class="alert alert-danger" 
+                            v-if="recoveryCodesLeft == 1">
+                            <strong>You have 1 recovery code left.</strong>
+                            <p>You can <a :href="getUrl('../../Users/GenerateRecoveryCodes')">generate a new set of recovery codes</a>.</p>
                         </div>
-
-                        <h5>{{$t('Pages.AccountManage_AuthenticatorApp')}}</h5>
-
-                        <a v-if="!hasAuthenticator" 
-                            id="enable-authenticator" 
-                            :href="getUrl('../../Users/SetupAuthenticator')" 
-                            class="btn btn-success">{{$t('Pages.AddAuthenticator')}}
-                        </a>                
-                        <a v-if="hasAuthenticator" 
-                            id="enable-authenticator" 
-                            :href="getUrl('../../Users/SetupAuthenticator')" 
-                            style="margin-right: 5px;"
-                            class="btn btn-success">{{$t('Pages.SetupAuthenticator')}}
-                        </a>
-                        <a v-if="hasAuthenticator" 
-                            id="reset-authenticator" 
-                            :href="getUrl('../../Users/ResetAuthenticator')" 
-                            class="btn btn-success">{{$t('Pages.ResetAuthenticator')}}
-                        </a>               
+                        <div class="alert alert-warning" 
+                            v-if="recoveryCodesLeft <= 3">
+                            <strong>You have {{recoveryCodesLeft}} recovery codes left.</strong>
+                            <p>You should <a :href="getUrl('../../Users/GenerateRecoveryCodes')">generate a new set of recovery codes</a>.</p>
+                        </div>                    
+    
+                        <a :href="getUrl('../../Users/Disable2fa')" 
+                            class="btn btn-success"
+                            style="margin-right: 5px;">{{$t('Pages.Disable2fa')}}</a>
+                        <a :href="getUrl('../../Users/GenerateRecoveryCodes')" 
+                            class="btn btn-success">{{$t('Pages.ResetRecoveryCodes')}} </a>
                     </div>
+
+                    <h5>{{$t('Pages.AccountManage_AuthenticatorApp')}}</h5>
+
+                    <a v-if="!hasAuthenticator" 
+                        id="enable-authenticator" 
+                        :href="getUrl('../../Users/SetupAuthenticator')" 
+                        class="btn btn-success">{{$t('Pages.AddAuthenticator')}}
+                    </a>                
+                    <a v-if="hasAuthenticator" 
+                        id="enable-authenticator" 
+                        :href="getUrl('../../Users/SetupAuthenticator')" 
+                        style="margin-right: 5px;"
+                        class="btn btn-success">{{$t('Pages.SetupAuthenticator')}}
+                    </a>
+                    <a v-if="hasAuthenticator" 
+                        id="reset-authenticator" 
+                        :href="getUrl('../../Users/ResetAuthenticator')" 
+                        class="btn btn-success">{{$t('Pages.ResetAuthenticator')}}
+                    </a>               
+                    
                 </div>
             </div>
         </div>
