60b58a578c159bfccccc07db7e13f518ea9483cb
Pavel Stovpivskyi <sby@rambler.ru>
2020-04-24 20:39:50 +0300

    KP-13902 test for partial upload

--- src/Tests/WB.Tests.Unit/BoundedContexts/Interviewer/SynchronizationSteeps/DownloadHQChangesForInterviewTests.cs
+++ src/Tests/WB.Tests.Unit/BoundedContexts/Interviewer/SynchronizationSteeps/DownloadHQChangesForInterviewTests.cs
@@ -26,7 +26,7 @@ using WB.Tests.Abc;
 namespace WB.Tests.Unit.BoundedContexts.Interviewer.SynchronizationSteeps
 {
     [TestFixture]
-    public class DownloadHQChangesForInterviewTests
+    public class DownloadHqChangesForInterviewTests
     {
         [Test]
         public async Task when_hq_contains_new_event_for_interview()
@@ -51,21 +51,122 @@ namespace WB.Tests.Unit.BoundedContexts.Interviewer.SynchronizationSteeps
             };
 
             var eventStore = Mock.Of<IEnumeratorEventStorage>(s => s.GetLastEventIdUploadedToHq(interviewId) == lastLocalEventId);
+            var eventBus = Mock.Of<ILiteEventBus>();
+
+            var synchronizationStep = CreateDownloadHQChangesForInterview(remoteInterviews, localInterviews, eventsAfter,
+                eventStore, eventBus);
+
+            await synchronizationStep.ExecuteAsync();
+
+            Mock.Get(eventStore).Verify(e =>
+                e.InsertEventsFromHqInEventsStream(interviewId, It.Is<CommittedEventStream>(s => s.Count == 1 && s.Single().EventIdentifier == newEventIdFromHq)), Times.Once);
+            Mock.Get(eventBus).Verify(e =>
+                e.PublishCommittedEvents(It.Is<IReadOnlyCollection<CommittedEvent>>(s => s.Count == 1 && s.Single().EventIdentifier == newEventIdFromHq)), Times.Once);
+        }
+
+        [Test]
+        public async Task when_hq_dont_contains_data_about_interview_created_on_client()
+        {
+            var interviewId = Id.g1;
+            var lastLocalEventId = Id.g2;
+
+            List<InterviewApiView> remoteInterviews = new List<InterviewApiView>()
+            {
+            };
+
+            List<InterviewView> localInterviews = new List<InterviewView>()
+            {
+                Create.Entity.InterviewView(interviewId: interviewId)
+            };
+
+            List<EventsAfter> eventsAfter = new List<EventsAfter>()
+            {
+            };
+
+            var eventStore = Mock.Of<IEnumeratorEventStorage>(s => s.GetLastEventIdUploadedToHq(interviewId) == lastLocalEventId);
 
             var synchronizationStep = CreateDownloadHQChangesForInterview(remoteInterviews, localInterviews, eventsAfter,
                 eventStore);
 
             await synchronizationStep.ExecuteAsync();
 
-            Mock.Get(eventStore).Verify(e => 
-                e.InsertEventsFromHqInEventsStream(interviewId, It.Is<CommittedEventStream>(s => s.Count == 1 && s.Single().EventIdentifier == newEventIdFromHq)), Times.Once);
+            Mock.Get(eventStore).Verify(e =>
+                e.InsertEventsFromHqInEventsStream(interviewId, It.IsAny<CommittedEventStream>()), Times.Never);
+        }
+
+        [Test]
+        public async Task when_hq_contains_the_same_data_about_interview()
+        {
+            var interviewId = Id.g1;
+            var lastLocalEventId = Id.g2;
+
+            List<InterviewApiView> remoteInterviews = new List<InterviewApiView>()
+            {
+                Create.Entity.InterviewApiView(interviewId, lastLocalEventId),
+            };
+
+            List<InterviewView> localInterviews = new List<InterviewView>()
+            {
+                Create.Entity.InterviewView(interviewId: interviewId)
+            };
+
+            List<EventsAfter> eventsAfter = new List<EventsAfter>()
+            {
+                CreateEventsAfter(interviewId, lastLocalEventId),
+            };
+
+            var eventStore = Mock.Of<IEnumeratorEventStorage>(s => s.GetLastEventIdUploadedToHq(interviewId) == lastLocalEventId);
+            var eventBus = Mock.Of<ILiteEventBus>();
+
+            var synchronizationStep = CreateDownloadHQChangesForInterview(remoteInterviews, localInterviews, eventsAfter,
+                eventStore, eventBus);
+
+            await synchronizationStep.ExecuteAsync();
+
+            Mock.Get(eventStore).Verify(e =>
+                e.InsertEventsFromHqInEventsStream(interviewId, It.IsAny<CommittedEventStream>()), Times.Never);
+            Mock.Get(eventBus).Verify(e =>
+                e.PublishCommittedEvents(It.IsAny<IReadOnlyCollection<CommittedEvent>>()), Times.Never);
+        }
+
+        [Test]
+        public async Task when_hq_contains_data_about_interview_but_localy_it_doesnot_exists()
+        {
+            var interviewId = Id.g1;
+
+            List<InterviewApiView> remoteInterviews = new List<InterviewApiView>()
+            {
+                Create.Entity.InterviewApiView(interviewId, Id.g10),
+            };
+
+            List<InterviewView> localInterviews = new List<InterviewView>()
+            {
+            };
+
+            List<EventsAfter> eventsAfter = new List<EventsAfter>()
+            {
+            };
+
+            var eventStore = Mock.Of<IEnumeratorEventStorage>();
+            var eventBus = Mock.Of<ILiteEventBus>();
+
+            var synchronizationStep = CreateDownloadHQChangesForInterview(remoteInterviews, localInterviews, eventsAfter,
+                eventStore, eventBus);
+
+            await synchronizationStep.ExecuteAsync();
+
+            Mock.Get(eventStore).Verify(e =>
+                e.InsertEventsFromHqInEventsStream(interviewId, It.IsAny<CommittedEventStream>()), Times.Never);
+            Mock.Get(eventBus).Verify(e =>
+                e.PublishCommittedEvents(It.IsAny<IReadOnlyCollection<CommittedEvent>>()), Times.Never);
         }
 
         private SynchronizationStep CreateDownloadHQChangesForInterview(
             List<InterviewApiView> remoteInterviews,
             List<InterviewView> localInterviews,
             List<EventsAfter> eventsAfter,
-            IEnumeratorEventStorage eventStore = null)
+            IEnumeratorEventStorage eventStore = null,
+            ILiteEventBus eventBus = null)
         {
             var synchronizationService = new Mock<ISynchronizationService>();
             synchronizationService.Setup(s => s.GetInterviewsAsync(It.IsAny<CancellationToken>())).Returns(Task.FromResult(remoteInterviews));
@@ -82,7 +183,8 @@ namespace WB.Tests.Unit.BoundedContexts.Interviewer.SynchronizationSteeps
 
             return CreateDownloadHQChangesForInterview(synchronizationService.Object,
                 localStorage,
-                eventStore: eventStore);
+                eventStore: eventStore,
+                eventBus: eventBus);
         }
 
         private SynchronizationStep CreateDownloadHQChangesForInterview(
