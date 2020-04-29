8a1fbb19a0fbc24b72beac189b27fde6d262d890
Andrii Kozhyn <hedgic@gmail.com>
2020-04-29 16:15:39 +0300

    KP-13973 Migrate from EPPPlus to ClosedXML

--- src/Tests/WB.Tests.Unit.Designer/BoundedContexts/Designer/TranslationServiceTests/when_questionnaire_contains_fixed_roster.cs
+++ src/Tests/WB.Tests.Unit.Designer/BoundedContexts/Designer/TranslationServiceTests/when_questionnaire_contains_fixed_roster.cs
@@ -1,11 +1,12 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using ClosedXML.Excel;
 using FluentAssertions;
 using Main.Core.Documents;
 using Main.Core.Entities.Composite;
 using Moq;
-using OfficeOpenXml;
 using WB.Core.BoundedContexts.Designer.Translations;
 using WB.Core.GenericSubdomains.Portable;
 using WB.Core.Infrastructure.PlainStorage;
@@ -54,28 +55,28 @@ namespace WB.Tests.Unit.Designer.BoundedContexts.Designer.TranslationServiceTest
         private void BecauseOf() 
         {
             excelFile = service.GetAsExcelFile(questionnaireId, translationId);
-            workbook = new ExcelPackage(new MemoryStream(excelFile.ContentAsExcelFile)).Workbook;
-            cells = workbook.Worksheets[0].Cells;
+            workbook = new XLWorkbook(new MemoryStream(excelFile.ContentAsExcelFile));
+            cells = workbook.Worksheets.First();
         }
 
         [NUnit.Framework.Test] public void should_output_roster_title_translation () 
         {
             var questionTitleRow = 3;
-            ((TranslationType)Enum.Parse(typeof(TranslationType), cells[questionTitleRow, translationTypeColumn].Text)).Should().Be(TranslationType.Title);
-            cells[questionTitleRow, translationIndexColumn].Value?.ToString().Should().BeNull();
-            cells[questionTitleRow, questionnaireEntityIdColumn].Value?.ToString().Should().Be(rosterId.FormatGuid());
-            cells[questionTitleRow, originalTextColumn].Value?.ToString().Should().Be("non translated title");
-            cells[questionTitleRow, translactionColumn].Value?.ToString().Should().BeNull();
+            ((TranslationType)Enum.Parse(typeof(TranslationType), cells.Cell(questionTitleRow, translationTypeColumn).GetString())).Should().Be(TranslationType.Title);
+            cells.Cell(questionTitleRow, translationIndexColumn).Value?.ToString().Should().BeEmpty();
+            cells.Cell(questionTitleRow, questionnaireEntityIdColumn).Value?.ToString().Should().Be(rosterId.FormatGuid());
+            cells.Cell(questionTitleRow, originalTextColumn).Value?.ToString().Should().Be("non translated title");
+            cells.Cell(questionTitleRow, translactionColumn).Value?.ToString().Should().BeEmpty();
         }
 
         [NUnit.Framework.Test] public void should_output_roster_fixed_option_title_translation () 
         {
             var questionTitleRow = 4;
-            ((TranslationType)Enum.Parse(typeof(TranslationType), cells[questionTitleRow, translationTypeColumn].Text)).Should().Be(TranslationType.FixedRosterTitle);
-            cells[questionTitleRow, translationIndexColumn].Value?.ToString().Should().Be("42");
-            cells[questionTitleRow, questionnaireEntityIdColumn].Value?.ToString().Should().Be(rosterId.FormatGuid());
-            cells[questionTitleRow, originalTextColumn].Value?.ToString().Should().Be("invariant option title");
-            cells[questionTitleRow, translactionColumn].Value?.ToString().Should().Be("fixed roster item 1");
+            ((TranslationType)Enum.Parse(typeof(TranslationType), cells.Cell(questionTitleRow, translationTypeColumn).GetString())).Should().Be(TranslationType.FixedRosterTitle);
+            cells.Cell(questionTitleRow, translationIndexColumn).Value?.ToString().Should().Be("42");
+            cells.Cell(questionTitleRow, questionnaireEntityIdColumn).Value?.ToString().Should().Be(rosterId.FormatGuid());
+            cells.Cell(questionTitleRow, originalTextColumn).Value?.ToString().Should().Be("invariant option title");
+            cells.Cell(questionTitleRow, translactionColumn).Value?.ToString().Should().Be("fixed roster item 1");
         }
 
 
@@ -84,7 +85,7 @@ namespace WB.Tests.Unit.Designer.BoundedContexts.Designer.TranslationServiceTest
         static Guid questionnaireId;
         static Guid translationId = Guid.Parse("ABBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB");
         static TranslationFile excelFile;
-        static ExcelWorkbook workbook;
-        static ExcelRange cells;
+        static IXLWorkbook workbook;
+        private IXLWorksheet cells;
     }
 }
