211b36287e6f0617a471f1bdff809f605b6c0976
Andrii Kozhyn <hedgic@gmail.com>
2020-04-30 12:46:16 +0300

    KP-13979 first version of migration

--- src/UI/WB.UI.Headquarters.Core/Controllers/Api/PublicApi/Graphql/Questionnaires/QuestionsResolver.cs
+++ src/UI/WB.UI.Headquarters.Core/Controllers/Api/PublicApi/Graphql/Questionnaires/QuestionsResolver.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using HotChocolate;
 using HotChocolate.Resolvers;
+using WB.Core.BoundedContexts.Headquarters.Factories;
+using WB.Core.BoundedContexts.Headquarters.Implementation.Factories;
 using WB.Core.BoundedContexts.Headquarters.Views.Interview;
 using WB.Core.BoundedContexts.Headquarters.Views.Questionnaire;
 using WB.Core.SharedKernels.DataCollection.Implementation.Entities;
@@ -12,13 +14,15 @@ namespace WB.UI.Headquarters.Controllers.Api.PublicApi.Graphql.Questionnaires
 {
     public class QuestionsResolver
     {
-        public IEnumerable<QuestionnaireCompositeItem> Questions(Guid id, 
+        public IEnumerable<QuestionnaireCompositeItem> Questions(string variable, 
             long version,
             string language,
             [Service] IQuestionnaireStorage storage, 
+            [Service] IQuestionnaireBrowseViewFactory browseViewFactory,
             [Service] IResolverContext resolverContext)
         {
-            var questionnaire = storage.GetQuestionnaire(new QuestionnaireIdentity(id, version), language);
+            var questionnaireBrowseItem = browseViewFactory.GetByVariableName(variable, version);
+            var questionnaire = storage.GetQuestionnaire(questionnaireBrowseItem.Identity(), language);
             var featured = questionnaire.GetPrefilledQuestions().ToHashSet();
 
             resolverContext.ScopedContextData = resolverContext.ScopedContextData.SetItem("language", language);
