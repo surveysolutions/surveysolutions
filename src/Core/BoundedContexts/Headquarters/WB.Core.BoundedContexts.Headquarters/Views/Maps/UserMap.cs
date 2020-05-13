0116887b3db0491abd460e4935797f586a9b8b94
Roman Lytvyn <litvin.roma@gmail.com>
2020-05-13 16:32:37 +0300

    KP-13993 Extend graphql with maps api. Fixed accessibility. Changed mapping

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Views/Maps/UserMap.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Views/Maps/UserMap.cs
@@ -5,5 +5,6 @@
         public virtual int Id { get; set; }
         public virtual string Map { get; set; }
         public virtual string UserName { get; set; }
+        public virtual MapBrowseItem Source { get; set; }
     }
 }
--- src/UI/WB.UI.Headquarters.Core/Controllers/Api/PublicApi/Graphql/Maps/MapsResolver.cs
+++ src/UI/WB.UI.Headquarters.Core/Controllers/Api/PublicApi/Graphql/Maps/MapsResolver.cs
@@ -1,21 +1,57 @@
-﻿using System.Linq;
+﻿using System;
+using System.IO;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
 using WB.Core.BoundedContexts.Headquarters.Repositories;
 using WB.Core.BoundedContexts.Headquarters.Views.Maps;
+using WB.Core.Infrastructure.FileSystem;
+using WB.Infrastructure.Native.Storage.Postgre;
 
 namespace WB.UI.Headquarters.Controllers.Api.PublicApi.Graphql.Maps
 {
     public class MapsResolver
     {
         private readonly IMapStorageService mapStorageService;
+        private readonly IUnitOfWork unitOfWork;
 
-        public MapsResolver(IMapStorageService mapStorageService)
+        public MapsResolver(IMapStorageService mapStorageService, IUnitOfWork unitOfWork)
         {
             this.mapStorageService = mapStorageService;
+            this.unitOfWork = unitOfWork;
         }
         public IQueryable<MapBrowseItem> GetMaps() => this.mapStorageService.GetAllMaps();
         public MapBrowseItem GetMap(string id) => this.mapStorageService.GetMapById(id);
-        public MapBrowseItem DeleteMap(string id) => this.mapStorageService.DeleteMap(id).Result;
-        public object DeleteUserFromMap(string id, string userName) => this.mapStorageService.DeleteMapUserLink(id, userName);
-        public object AddUserToMap(string id, string userName) => this.mapStorageService.AddUserToMap(id, userName);
+        public MapBrowseItem AddMap(IFormFile file)
+        {
+            var ms = new MemoryStream();
+            file.CopyTo(ms);
+            
+            return this.Execute(() => this.mapStorageService.SaveOrUpdateMapAsync(new ExtractedFile
+            {
+                Name = file.Name,
+                Size = file.Length,
+                Bytes = ms.ToArray()
+            }).Result);
+        }
+        public MapBrowseItem DeleteMap(string id) => this.Execute(() => this.mapStorageService.DeleteMap(id).Result);
+        public MapBrowseItem DeleteUserFromMap(string id, string userName) => this.Execute(() => this.mapStorageService.DeleteMapUserLink(id, userName));
+        public MapBrowseItem AddUserToMap(string id, string userName) => this.Execute(() => this.mapStorageService.AddUserToMap(id, userName));
+
+        private T Execute<T>(Func<T> action)
+        {
+            T result = default;
+            
+            try
+            {
+                result = action();
+                this.unitOfWork.AcceptChanges();
+            }
+            catch
+            {
+                this.unitOfWork.DiscardChanges();
+            }
+
+            return result;
+        }
     }
 }
