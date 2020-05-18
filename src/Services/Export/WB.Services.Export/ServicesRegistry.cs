0d115c4a817acf8b24f62fdf09e89a572fcd1037
Pavel Stovpivskyi <sby@rambler.ru>
2020-05-18 18:24:53 +0300

    KP-13932 moved settings to singleton  scope

--- src/Services/Export/WB.Services.Export/ServicesRegistry.cs
+++ src/Services/Export/WB.Services.Export/ServicesRegistry.cs
@@ -83,7 +83,7 @@ namespace WB.Services.Export
             services.AddTransient<IMetaDescriptionFactory, MetaDescriptionFactory>();
             services.AddTransient<IExportJob, ExportJob>();
             services.AddTransient<IDatabaseSchemaService, DatabaseSchemaService>();
-            services.AddTransient<InterviewDataExportBulkCommandBuilderSettings>();
+            services.AddSingleton<InterviewDataExportBulkCommandBuilderSettings>();
             services.AddTransient<IInterviewDataExportBulkCommandBuilder, InterviewDataExportBulkCommandBuilder>();
             services.AddTransient<ICommandExecutor, CommandExecutor>();
             services.AddTransient<IInterviewReferencesStorage, InterviewReferencesStorage>();
