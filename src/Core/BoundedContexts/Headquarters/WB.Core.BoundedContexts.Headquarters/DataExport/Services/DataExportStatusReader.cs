5bb61267f5b237634d2d934f36844d1fe7f4da65
Andrii Kozhyn <hedgic@gmail.com>
2020-05-19 10:55:52 +0300

    KP-13659 pass translation id to downloaded archive

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/DataExport/Services/DataExportStatusReader.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/DataExport/Services/DataExportStatusReader.cs
@@ -4,8 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
-using AngleSharp.Css.Dom;
-using OfficeOpenXml;
 using WB.Core.BoundedContexts.Headquarters.DataExport.Dtos;
 using WB.Core.BoundedContexts.Headquarters.DataExport.Views;
 using WB.Core.BoundedContexts.Headquarters.Services;
