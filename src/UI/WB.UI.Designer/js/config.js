161d3416857356880f09c7a8bbc8091b6f328006
Roman Lytvyn <litvin.roma@gmail.com>
2020-05-21 01:39:04 +0300

    KP-13994 UI for reusable categories editing. Edit categories moved to vue with full infrastructure

---
+++ src/UI/WB.UI.Designer/js/config.js
@@ -0,0 +1,21 @@
+﻿const config = window.CONFIG
+
+/*  the Plugin */
+export default Object.assign(window.CONFIG || {}, {
+    install(Vue) {
+        
+        // /*  expose a global API method  */
+        Object.defineProperty(Vue, '$config', {
+            get() {
+                return config
+            },
+        })
+
+        /*  expose a local API method  */
+        Object.defineProperty(Vue.prototype, '$config', {
+            get() {
+                return config
+            },
+        })
+    },
+})
---
+++ src/UI/WB.UI.Designer/js/helpers.js
@@ -0,0 +1,4 @@
+﻿import jQuery from 'jquery';
+
+export const $ = jQuery;
+export const browserLanguage = $('html').attr('lang')
