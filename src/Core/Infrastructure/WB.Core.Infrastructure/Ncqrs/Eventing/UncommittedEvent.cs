d8c1470e7d6a210d38c2ca0b12ffee898ee4e4d6
Victor Gladkikh <me@alfeg.net>
2020-05-22 11:14:57 +0300

    KP-14022 Do not reload page on prototype materialization

--- src/Core/Infrastructure/WB.Core.Infrastructure/Ncqrs/Eventing/UncommittedEvent.cs
+++ src/Core/Infrastructure/WB.Core.Infrastructure/Ncqrs/Eventing/UncommittedEvent.cs
@@ -12,59 +12,35 @@ namespace Ncqrs.Eventing
     [DebuggerDisplay("{Payload.GetType().Name} {EventIdentifier}")]
     public class UncommittedEvent : IUncommittedEvent
     {
-        private readonly WB.Core.Infrastructure.EventBus.IEvent _payload;
-        private readonly int _eventSequence;
-        private readonly Guid _eventIdentifier;
-        private readonly DateTime _eventTimeStamp;
-        private readonly Guid _eventSourceId;
-        private readonly int _initialVersionOfEventSource;
-        private Guid _commitId;
-        private string _origin;
-
         /// <summary>
         /// Gets the initial version of event source (the version it was just after creating/retrieving from the store)
         /// </summary>
-        public int InitialVersionOfEventSource
-        {
-            get { return _initialVersionOfEventSource; }
-        }
+        public int InitialVersionOfEventSource { get; }
 
         /// <summary>
         /// Gets the payload of the event.
         /// </summary>
-        public WB.Core.Infrastructure.EventBus.IEvent Payload
-        {
-            get { return _payload; }
-        }
+        public WB.Core.Infrastructure.EventBus.IEvent Payload { get; }
 
         public long GlobalSequence { get; set; }
 
         /// <summary>
         /// Gets the unique identifier for this event.
         /// </summary>
-        public Guid EventIdentifier
-        {
-            get { return _eventIdentifier; }
-        }
+        public Guid EventIdentifier { get; }
 
         /// <summary>
         /// Gets the time stamp for this event.
         /// </summary>
         /// <value>a <see cref="DateTime"/> UTC value that represents the point
         /// in time where this event occurred.</value>
-        public DateTime EventTimeStamp
-        {
-            get { return _eventTimeStamp; }
-        }
+        public DateTime EventTimeStamp { get; }
 
         /// <summary>
         /// Gets the id of the event source that caused the event.
         /// </summary>
         /// <value>The id of the event source that caused the event.</value>
-        public Guid EventSourceId
-        {
-            get { return _eventSourceId; }
-        }
+        public Guid EventSourceId { get; }
 
         /// <summary>
         /// Gets the event sequence number.
@@ -73,28 +49,19 @@ namespace Ncqrs.Eventing
         /// An sequence of events always starts with <c>1</c>. So the first event in a sequence has the <see cref="EventSequence"/> value of <c>1</c>.
         /// </remarks>
         /// <value>A number that represents the order of where this events occurred in the sequence.</value>
-        public int EventSequence
-        {
-            get { return _eventSequence; }
-        }
+        public int EventSequence { get; }
 
         /// <summary>
         /// If of a commit in which this event is to be stored (usually corresponds to a command id which caused this event).
         /// </summary>
-        public Guid CommitId
-        {
-            get { return _commitId; }
-        }
+        public Guid CommitId { get; private set; }
 
-        public string Origin
-        {
-            get { return _origin; }
-        }
+        public string Origin { get; private set; }
 
         public void OnAppendedToStream(Guid streamCommitId, string streamOrigin)
         {
-            _commitId = streamCommitId;
-            _origin = streamOrigin;
+            CommitId = streamCommitId;
+            Origin = streamOrigin;
         }
 
         public UncommittedEvent(Guid eventIdentifier, 
@@ -104,17 +71,14 @@ namespace Ncqrs.Eventing
             DateTime eventTimeStamp, 
             WB.Core.Infrastructure.EventBus.IEvent payload)            
         {
-            _payload = payload;
-            _initialVersionOfEventSource = initialVersionOfEventSource;
-            _eventSourceId = eventSourceId;
-            _eventSequence = eventSequence;
-            _eventIdentifier = eventIdentifier;
-            _eventTimeStamp = eventTimeStamp;
+            Payload = payload;
+            InitialVersionOfEventSource = initialVersionOfEventSource;
+            EventSourceId = eventSourceId;
+            EventSequence = eventSequence;
+            EventIdentifier = eventIdentifier;
+            EventTimeStamp = eventTimeStamp;
         }
 
-        public override string ToString()
-        {
-            return string.Format("{0}[{1}]", Payload.GetType().FullName, EventIdentifier.ToString("D"));
-        }
+        public override string ToString() => $"{Payload.GetType().FullName}[{EventIdentifier:D}]";
     }
 }
