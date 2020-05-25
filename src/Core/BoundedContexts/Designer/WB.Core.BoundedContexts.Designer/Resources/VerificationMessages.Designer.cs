65b8b50026b18296a7c9555c23a494b6f549692b
Victor Hladkikh <me@alfeg.net>
2020-05-25 16:13:32 +0300

    KP-14056 Not possible to compile questionnaire

--- src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Resources/VerificationMessages.Designer.cs
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Resources/VerificationMessages.Designer.cs
@@ -19,7 +19,7 @@ namespace WB.Core.BoundedContexts.Designer.Resources {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class VerificationMessages {
@@ -2110,6 +2110,24 @@ namespace WB.Core.BoundedContexts.Designer.Resources {
         public static string WB0304_TableRosterCannotHaveCustomTitle {
             get {
                 return ResourceManager.GetString("WB0304_TableRosterCannotHaveCustomTitle", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Reusable category cannot have row with same Id and ParentId values.
+        /// </summary>
+        public static string WB0305_DuplicatedCategoryIdParentIdPair {
+            get {
+                return ResourceManager.GetString("WB0305_DuplicatedCategoryIdParentIdPair", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Reusable category cannot have row with same ParentId and Text values.
+        /// </summary>
+        public static string WB0306_DuplicatedCategoryParentIdText {
+            get {
+                return ResourceManager.GetString("WB0306_DuplicatedCategoryParentIdText", resourceCulture);
             }
         }
     }
