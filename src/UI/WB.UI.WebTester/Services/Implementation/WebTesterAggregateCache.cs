f03d56a6dc5a3ce7b0e8a977a88e4e2fc706fc9c
Victor Hladkikh <me@alfeg.net>
2020-05-27 18:35:16 +0300

    KP-14022 Fixed WebTester

---
+++ src/UI/WB.UI.WebTester/Services/Implementation/WebTesterAggregateCache.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.Extensions.Caching.Memory;
+using WB.Core.Infrastructure.Aggregates;
+using WB.Core.Infrastructure.Implementation.Aggregates;
+
+namespace WB.UI.WebTester.Services.Implementation
+{
+    public class WebTesterAggregateCache : AggregateRootCache
+    {
+        private readonly IEvictionNotifier notify;
+
+        public WebTesterAggregateCache(
+            IAggregateLock aggregateLock, 
+            IMemoryCache memoryCache,
+            IEvictionNotifier notify) : base(aggregateLock, memoryCache)
+        {
+            this.notify = notify;
+        }
+
+        protected override void CacheItemRemoved(Guid id, EvictionReason reason)
+        {
+            if (reason != EvictionReason.Replaced)
+            {
+                notify.Evict(id);
+            }
+
+            base.CacheItemRemoved(id, reason);
+        }
+    }
+}
