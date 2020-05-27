921a90f2b1b5d3164ddda7685ab808f020051ec3
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2020-05-26 22:53:46 -0400

    KP-14038 Enable nullable reference types [Fix build]

--- src/Core/GenericSubdomains/WB.Core.GenericSubdomains.Portable/Services/ILogger.cs
+++ src/Core/GenericSubdomains/WB.Core.GenericSubdomains.Portable/Services/ILogger.cs
@@ -4,16 +4,16 @@ namespace WB.Core.GenericSubdomains.Portable.Services
 {
     public interface ILogger
     {
-        void Trace(string message, Exception? exception = null);
+        void Trace(string message, Exception exception = null);
 
-        void Debug(string message, Exception? exception = null);
+        void Debug(string message, Exception exception = null);
 
-        void Info(string message, Exception? exception = null);
+        void Info(string message, Exception exception = null);
 
-        void Warn(string message, Exception? exception = null);
+        void Warn(string message, Exception exception = null);
 
-        void Error(string message, Exception? exception = null);
+        void Error(string message, Exception exception = null);
 
-        void Fatal(string message, Exception? exception = null);
+        void Fatal(string message, Exception exception = null);
     }
 }
