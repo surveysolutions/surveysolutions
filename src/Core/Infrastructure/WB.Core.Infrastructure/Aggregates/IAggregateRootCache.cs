9c5e7576ab2f48c64b1c564ca845fd5307e54231
Victor Hladkikh <me@alfeg.net>
2020-05-28 15:16:20 +0300

    KP-14022 Build fix

--- src/Core/Infrastructure/WB.Core.Infrastructure/Aggregates/IAggregateRootCache.cs
+++ src/Core/Infrastructure/WB.Core.Infrastructure/Aggregates/IAggregateRootCache.cs
@@ -20,42 +20,12 @@ namespace WB.Core.Infrastructure.Aggregates
         /// <param name="factory">Aggregate root cache item factory</param>
         /// <param name="expirationPeriod">Sliding expiration period. Default is 5 minutes</param>
         /// <returns>The newly created <see cref="AggregateRootCacheItem"/> instance.</returns>
-        AggregateRootCacheItem CreateEntry(Guid aggregateId, Func<AggregateRootCacheItem, AggregateRootCacheItem> factory = null, TimeSpan? expirationPeriod = null);
+        AggregateRootCacheItem CreateEntry(Guid aggregateId, Func<AggregateRootCacheItem, AggregateRootCacheItem>? factory = null, TimeSpan? expirationPeriod = null);
 
         /// <summary>
         /// Removes the aggregate root associated with the given UUID.
         /// </summary>
         /// <param name="aggregateId">An aggregate root UUID identifying the entry.</param>
         void Evict(Guid aggregateId);
-    }
-
-    public static class AggregateRootCacheExtensions
-    {
-        public static AggregateRootCacheItem? Get(this IAggregateRootCache cache, Guid aggregateId)
-        {
-            return cache.TryGetValue(aggregateId, out var item) ? item : null;
-        }
-
-        public static AggregateRootCacheItem GetOrCreate(this IAggregateRootCache cache, Guid aggregateId)
-        {
-            var cacheItem = cache.TryGetValue(aggregateId, out var item) ? item : null;
-            return cacheItem ?? cache.CreateEntry(aggregateId);
-        }
-
-        public static AggregateRootCacheItem? Update(this IAggregateRootCache cache, Guid aggregateId, TimeSpan? slidingExpiration = null)
-        {
-            var item = cache.Get(aggregateId);
-
-            if (item == null)
-            {
-                return null;
-            }
-
-            return cache.CreateEntry(aggregateId, entry =>
-            {
-                entry.Meta = item.Meta;
-                return entry;
-            }, slidingExpiration);
-        }
     }
 }
