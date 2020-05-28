ba633b01cb879aa99c725b56690b22f24dcc1cd7
Victor Gladkikh <me@alfeg.net>
2020-05-28 11:20:33 +0300

    KP-14022 Fix Android applications functionality

---
+++ src/Core/Infrastructure/WB.Core.Infrastructure/Aggregates/AggregateRootCacheItem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Ncqrs.Eventing;
+using WB.Core.Infrastructure.Services;
+
+namespace WB.Core.Infrastructure.Aggregates
+{
+    public class AggregateRootCacheItem
+    {
+        public AggregateRootCacheItem(Guid id)
+        {
+            this.Id = id;
+        }
+
+        public Guid Id { get; }
+
+        public IEventSourcedAggregateRoot AggregateRoot { get; set; }
+        public IEnumerable<CommittedEvent> Events { get; set; }
+        public PrototypeType? PrototypeType { get; set; }
+
+        public AggregateRootCacheItem SetPrototypeType(PrototypeType? prototypeType)
+        {
+            PrototypeType = prototypeType;
+            return this;
+        }
+
+        public AggregateRootCacheItem SetEvents(IEnumerable<CommittedEvent> events)
+        {
+            Events = events;
+            return this;
+        }
+    }
+}
