e1709d2979dcbceb3c9ffb318b4fc758c5840440
Victor Gladkikh <me@alfeg.net>
2020-06-02 16:32:12 +0300

    KP-14063 Added test

---
+++ src/Tests/WB.Tests.Unit/SharedKernels/Enumerator/Native/WebInterview/InterviewLifecycleEventHandlerTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoFixture;
+using Moq;
+using NUnit.Framework;
+using WB.Enumerator.Native.WebInterview;
+using WB.Enumerator.Native.WebInterview.Services;
+using WB.Tests.Abc;
+
+namespace WB.Tests.Unit.SharedKernels.Enumerator.Native.WebInterview
+{
+    class InterviewLifecycleEventHandlerTests
+    {
+        [Test]
+        public void when_handling_prototype_of_TranslationSwitched_event_should_not_notify_connected_clients()
+        {
+            var fixture = Create.Other.AutoFixture();
+            fixture.FreezeMock<IWebInterviewNotificationService>();
+
+            var sut = fixture.Create<InterviewLifecycleEventHandler>();
+
+            var @event = Create.PublishedEvent.TranslationSwitched(origin: "prototype");
+            
+            sut.Handle(@event);
+            
+            fixture.GetMock<IWebInterviewNotificationService>()
+                .Verify(m => m.ReloadInterview(It.IsAny<Guid>()), Times.Never);
+        }
+        
+        [Test]
+        public void when_handling_TranslationSwitched_event_should_notify_connected_clients()
+        {
+            var fixture = Create.Other.AutoFixture();
+            fixture.FreezeMock<IWebInterviewNotificationService>();
+
+            var sut = fixture.Create<InterviewLifecycleEventHandler>();
+
+            var @event = Create.PublishedEvent.TranslationSwitched();
+            
+            sut.Handle(@event);
+            
+            fixture.GetMock<IWebInterviewNotificationService>()
+                .Verify(m => m.ReloadInterview(It.IsAny<Guid>()), Times.Once);
+        }
+    }
+}
