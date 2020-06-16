2924ee9f879f1b1408d28e1ceb81540082f5984b
Victor Hladkikh <me@alfeg.net>
2020-06-16 11:47:10 +0300

    Fixed handling of nullable reference types in export service job controller

--- src/Services/Export/WB.Services.Export/Services/Processing/IDataExportFileAccessor.cs
+++ src/Services/Export/WB.Services.Export/Services/Processing/IDataExportFileAccessor.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
 using System.Threading;
@@ -12,9 +11,9 @@ namespace WB.Services.Export.Services.Processing
     public interface IDataExportFileAccessor
     {
         string GetExternalStoragePath(TenantInfo tenant, string name);
-        IZipArchive CreateExportArchive(Stream outputStream, string archivePassword, CompressionLevel compressionLevel = CompressionLevel.Fastest);
-        void RecreateExportArchive(string exportTempDirectoryPath, string archiveName, string archivePassword, ExportProgress exportProgress);
+        IZipArchive CreateExportArchive(Stream outputStream, string? archivePassword, CompressionLevel compressionLevel = CompressionLevel.Fastest);
+        void RecreateExportArchive(string exportTempDirectoryPath, string archiveName, string? archivePassword, ExportProgress exportProgress);
         Task<bool> PublishArchiveToArtifactsStorageAsync(TenantInfo tenant, string archiveFile, ExportProgress exportProgress, CancellationToken cancellationToken);
-        void RecreateExportArchive(string exportTempDirectoryPath, IEnumerable<string> filesToArchive, string archiveFilePath, string password);
+        void RecreateExportArchive(string exportTempDirectoryPath, IEnumerable<string> filesToArchive, string archiveFilePath, string? password);
     }
 }
