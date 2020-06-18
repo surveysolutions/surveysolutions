31abae4426704d099d74e51a21d6b05e8dd6e291
Pavel Stovpivskyi <sby@rambler.ru>
2020-06-18 16:13:54 +0300

    removed unused method

--- src/Core/SharedKernels/DataCollection/DataCollection/Aggregates/IQuestionnaire.cs
+++ src/Core/SharedKernels/DataCollection/DataCollection/Aggregates/IQuestionnaire.cs
@@ -319,7 +319,6 @@ namespace WB.Core.SharedKernels.DataCollection.Aggregates
         GeometryType? GetQuestionGeometryType(Guid questionId);
         int GetEntityIdMapValue(Guid entityId);
         bool IsCoverPage(Guid identityId);
-        string GetCoverPageTitle();
         bool IsCoverPageSupported { get; }
         Guid CoverPageSectionId { get; }
     }
