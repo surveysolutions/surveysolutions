46b2a25faa6456a7a6e38cd61c4697eca0deb3f7
Pavel Stovpivskyi <630198+spuv@users.noreply.github.com>
2020-07-02 19:18:30 +0300

    KP-13189 Add old/new version numbers to the error message about a wrong source of the IN/SV app

---
+++ src/UI/WB.UI.Headquarters.Core/Filters/UpdateRequiredFilter.cs
@@ -0,0 +1,49 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using WB.Core.Infrastructure.Versions;
+
+namespace WB.UI.Headquarters.Filters
+{
+    public class UpdateRequiredFilter : IActionFilter
+    {
+        private readonly IProductVersion productVersion;
+
+        public UpdateRequiredFilter(IProductVersion productVersion)
+        {
+            this.productVersion = productVersion;
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            if (context.Result is IStatusCodeActionResult statusCodeResult
+                && statusCodeResult.StatusCode == StatusCodes.Status426UpgradeRequired)
+            {
+                var version = productVersion.ToString();
+                context.HttpContext.Response.Headers.Add("version", version);
+                
+                /*if (context.Result is JsonResult jsonResult && jsonResult.Value != null)
+                    return;
+                if (context.Result is ObjectResult objectResult && objectResult.Value != null)
+                    return;
+                
+                var version = productVersion.ToString();
+                var versionInfo = new ProductVersionInfo()
+                {
+                    Version = version
+                };
+                context.Result = new JsonResult(versionInfo)
+                {
+                    StatusCode = StatusCodes.Status426UpgradeRequired
+                };*/
+            }
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            
+        }
+    }
+}
