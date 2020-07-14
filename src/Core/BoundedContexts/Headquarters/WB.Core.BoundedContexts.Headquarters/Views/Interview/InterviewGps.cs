afb9bf01b902618c0bcc7c8501cd88a239150397
Victor Hladkikh <me@alfeg.net>
2020-07-14 13:18:02 +0300

    KP-14146 moved to regular gps answers collection

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Views/Interview/InterviewGps.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Views/Interview/InterviewGps.cs
@@ -7,7 +7,7 @@ namespace WB.Core.BoundedContexts.Headquarters.Views.Interview
     public class InterviewGps
     {
         public virtual int Id { get; set; }
-        public virtual string InterviewId { get; set; }
+        public virtual InterviewSummary InterviewSummary { get; set; }
         public virtual Guid QuestionId { get; set; }
         public virtual string RosterVector { get; set; }
         public virtual double Latitude { get; set; }
@@ -17,9 +17,9 @@ namespace WB.Core.BoundedContexts.Headquarters.Views.Interview
 
         public override bool Equals(object obj) => obj is InterviewGps gps && Id == gps.Id;
 
-        public override int GetHashCode() => 2108858624 + Id.GetHashCode();
+        public override int GetHashCode() => Id;
 
         public override string ToString() =>
-            $"[{Latitude}, {Longitude}] i: {InterviewId} q: {QuestionId}${RosterVector}";
+            $"[{Latitude}, {Longitude}] q: {QuestionId}${RosterVector}";
     }
 }
