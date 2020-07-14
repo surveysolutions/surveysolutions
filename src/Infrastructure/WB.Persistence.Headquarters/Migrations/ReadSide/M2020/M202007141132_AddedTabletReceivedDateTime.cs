1e4ced515379b1e243e337d492a83a23c56e6abc
Pavel Stovpivskyi <sby@rambler.ru>
2020-07-14 19:26:41 +0300

    fixed under construction

---
+++ src/Infrastructure/WB.Persistence.Headquarters/Migrations/ReadSide/M2020/M202007141132_AddedTabletReceivedDateTime.cs
@@ -0,0 +1,32 @@
+﻿using FluentMigrator;
+
+namespace WB.Persistence.Headquarters.Migrations.ReadSide
+{
+    [Migration(202007141132)]
+    public class M202007141132_AddedTabletReceivedDateTime : Migration
+    {
+        public override void Up()
+        {
+            Create.Column("receivedbyinterviewertabletatutc").OnTable("InterviewSummaries")
+                .AsDateTime()
+                .Nullable();
+            
+            Execute.Sql(@"update readside.interviewsummaries isum
+                set isum.receivedbyinterviewertabletatutc = (
+		        select e.""timestamp"" 
+                    from events.events e 
+                        where isum.interviewid = e.eventsourceid and e.eventtype = 'InterviewReceivedByInterviewer'
+                    order by eventsequence desc
+                    limit 1
+                        ) 
+                where isum.receivedbyinterviewer = true;");
+            
+            Delete.Column("receivedbyinterviewer").FromTable("InterviewSummaries");
+        }
+
+        public override void Down()
+        {
+            
+        }
+    }
+}
