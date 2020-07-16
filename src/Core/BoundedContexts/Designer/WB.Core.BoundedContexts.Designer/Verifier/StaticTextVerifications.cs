fb1222e785b8c7cf014d248c80e78de930d6b234
Andrii Kozhyn <andrii.kozhyn@outlook.com>
2020-07-16 11:30:45 +0300

    KP-14158 ability to use variable name as attachment name on designer

--- src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Verifier/StaticTextVerifications.cs
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Verifier/StaticTextVerifications.cs
@@ -6,6 +6,7 @@ using Main.Core.Entities.SubEntities;
 using WB.Core.BoundedContexts.Designer.Implementation.Services;
 using WB.Core.BoundedContexts.Designer.Resources;
 using WB.Core.BoundedContexts.Designer.ValueObjects;
+using WB.Core.SharedKernels.QuestionnaireEntities;
 
 namespace WB.Core.BoundedContexts.Designer.Verifier
 {
@@ -15,7 +16,18 @@ namespace WB.Core.BoundedContexts.Designer.Verifier
         {
             ErrorForTranslation<IStaticText>(StaticTextIsEmpty, "WB0071", VerificationMessages.WB0071_StaticTextIsEmpty),
             Error<IStaticText>(StaticTextRefersAbsentAttachment, "WB0095", VerificationMessages.WB0095_StaticTextRefersAbsentAttachment),
+            Error<IStaticText>(StaticTextRefersNonStringVariableInAttachment, "WB0390", VerificationMessages.WB0390_OnlyStringVariableAllowedAsAttachmentName),
         };
+
+        private bool StaticTextRefersNonStringVariableInAttachment(IStaticText staticText, MultiLanguageQuestionnaireDocument document)
+        {
+            if (string.IsNullOrWhiteSpace(staticText.AttachmentName))
+                return false;
+
+            var referencedVariable = document.Find<IVariable>(v => v.Name == staticText.AttachmentName)
+                .FirstOrDefault();
+            return  referencedVariable != null && referencedVariable.Type != VariableType.String;
+        }
 
         private static bool StaticTextIsEmpty(IStaticText staticText, MultiLanguageQuestionnaireDocument document)
         {
@@ -27,7 +39,8 @@ namespace WB.Core.BoundedContexts.Designer.Verifier
             if (string.IsNullOrWhiteSpace(staticText.AttachmentName))
                 return false;
 
-            return document.Attachments.All(x => x.Name != staticText.AttachmentName);
+            return document.Attachments.All(x => x.Name != staticText.AttachmentName) && 
+                   document.Find<IVariable>(v => v.Name == staticText.AttachmentName).FirstOrDefault() == null;
         }
 
         private static Func<MultiLanguageQuestionnaireDocument, IEnumerable<QuestionnaireVerificationMessage>> Error<TEntity>(
