b23b89f6d15a3c4b4cb4b44a0e98e3455b18df80
Andrii Kozhyn <hedgic@gmail.com>
2020-07-16 15:44:05 +0300

    KP-14158 do not allow referencing variable in deeper scope

--- src/Tests/WB.Tests.Unit.Designer/QuestionnaireVerificationTests/StaticTextTests.cs
+++ src/Tests/WB.Tests.Unit.Designer/QuestionnaireVerificationTests/StaticTextTests.cs
@@ -40,5 +40,18 @@ namespace WB.Tests.Unit.Designer.QuestionnaireVerificationTests
                 Create.Variable(variableName: "var1", type: VariableType.Boolean)
             }).ExpectError("WB0390");
         }
+        
+        [Test]
+        public void when_variable_from_deeper_scope_referenced_Should_return_WB0391_error()
+        {
+            Create.QuestionnaireDocumentWithOneChapter(children: new IComposite[]
+            {
+                Create.StaticText(attachmentName: "var1"),
+                Create.FixedRoster(children: new []
+                {
+                    Create.Variable(variableName: "var1", type: VariableType.String)
+                })
+            }).ExpectError("WB0391");
+        }
     }
 }
