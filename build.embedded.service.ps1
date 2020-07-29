9525268c555ea9e0dc1d987afead85bc324400d9
Victor Gladkikh <me@alfeg.net>
2020-07-29 08:51:01 +0300

    KP-14175 Make sure that Embeddable Export Service can work with IIS

---
+++ build.embedded.service.ps1
@@ -0,0 +1,23 @@
+param(
+    [ValidateSet("win-x64","linux-x64", "mac-os")]
+    $r = "win-x64"
+)
+
+
+try{
+    Push-Location ./src/Services/Export/WB.Services.Export.Host
+
+    dotnet publish -c Release -r $r --output ../../../UI/WB.UI.Headquarters.Core/Export.Service --version-suffix dev-build /p:BuildNumber=42 /p:PublishSingleFile=false /p:Selfcontained=false
+} finally {
+    Pop-Location
+}
+
+
+try {
+    Push-Location ./src/UI/WB.UI.Headquarters.Core
+
+    dotnet publish -c Release -r $r --output dist --version-suffix dev-build /p:PublishTrimmed=False /p:PublishProfile=WebDeployPackage /p:BuildNumber=42 
+} finally{
+    Pop-Location
+}
+
