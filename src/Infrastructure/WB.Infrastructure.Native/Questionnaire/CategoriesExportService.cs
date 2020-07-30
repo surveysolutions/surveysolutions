f16f1968e765d8eadf5d7627d88b406567a8406b
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2020-07-29 21:51:22 -0400

    KP-14088 Export json version of questionnaire [review]

--- src/Infrastructure/WB.Infrastructure.Native/Questionnaire/CategoriesExportService.cs
+++ src/Infrastructure/WB.Infrastructure.Native/Questionnaire/CategoriesExportService.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using ClosedXML.Excel;
-using WB.Core.GenericSubdomains.Portable;
 using WB.Core.SharedKernels.Questionnaire.Categories;
 using WB.Core.SharedKernels.SurveySolutions.ReusableCategories;
 
