844521564649b5ef57f0d88e705b2b18fe989163
Pavel Stovpivskyi <630198+spuv@users.noreply.github.com>
2020-08-06 14:40:44 +0300

    KP-14191 removed unused code

--- src/UI/Shared/WB.UI.Shared.Enumerator/Activities/CoverInterviewFragment.cs
+++ src/UI/Shared/WB.UI.Shared.Enumerator/Activities/CoverInterviewFragment.cs
@@ -32,63 +32,21 @@ namespace WB.UI.Shared.Enumerator.Activities
                 this.action.Invoke();
             }
         }
-        private MvxRecyclerView recyclerView;
-        private LinearLayoutManager layoutManager;
-        private InterviewEntityAdapter adapter;
-
-        /*public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
-        {
-            this.EnsureBindingContextIsSet(inflater);
-            var view = this.BindingInflate(ViewResourceId, container, false);
-            this.recyclerView = view.FindViewById<MvxRecyclerView>(Resource.Id.interviewEntitiesList);
-
-            this.layoutManager = new LinearLayoutManager(this.Context);
-            this.recyclerView.SetLayoutManager(this.layoutManager);
-
-            this.adapter = new InterviewEntityAdapter((IMvxAndroidBindingContext)this.BindingContext);
-            this.recyclerView.Adapter = this.adapter;
-
-            return view;
-        }*/
-
+        
         public override void OnViewCreated(View view, Bundle savedInstanceState)
         {
             base.OnViewCreated(view, savedInstanceState);
 
-            if (ViewModel?.ScrollToIndex != null)
+            if (ViewModel?.ScrollToIdentity != null)
             {
-                // var childView = this.recyclerView;
-                // int scrollTo =  childView.Top;
-
                 var nestedScrollView = view.FindViewById<NestedScrollView>(Resource.Id.tv_coverNestedScrollView);
                 nestedScrollView.Post(new InnerRunnable(() =>
                 {
                     var entityControl = nestedScrollView.FindViewWithTag($"tv_Title_{ViewModel.ScrollToIdentity}");
-                    var topOffset = entityControl.Top + entityControl.LayoutParameters.Height + ((View)entityControl.Parent).Top;
+                    var topOffset = entityControl.Top + ((View)entityControl.Parent).Top;
                     nestedScrollView.ScrollTo(0, topOffset);
-                    //nestedScrollView.SmoothScrollTo(0, 0);
-                    this.ViewModel.ScrollToIndex = null;
                     this.ViewModel.ScrollToIdentity = null;
                 }));
-                
-                
-                /*int scrollViewHeight = nestedScrollView.Height;
-                if (scrollViewHeight > 0) {
-                    nestedScrollView.ViewTreeObserver.RemoveOnGlobalLayoutListener(this);
-
-                    View lastView = nestedScrollView.GetChildAt(nestedScrollView.ChildCount - 1);
-                    int lastViewBottom = lastView.Bottom + nestedScrollView.PaddingBottom;
-                    int deltaScrollY = lastViewBottom - scrollViewHeight - nestedScrollView.ScrollY;
-                    /* If you want to see the scroll animation, call this. #1#
-                    //nestedScrollView.SmoothScrollBy(0, deltaScrollY);
-                    /* If you don't want, call this. #1#
-                    nestedScrollView.ScrollBy(0, deltaScrollY);
-                }*/
-                    //nestedScrollView.fullScroll(View.FOCUS_DOWN);
-
-                //nestedScrollView.ScrollTo(0, 1000);
-                //this.layoutManager?.ScrollToPosition(this.ViewModel.ScrollToIndex.Value);
-                //this.ViewModel.ScrollToIndex = null;
             }
         }
     }
