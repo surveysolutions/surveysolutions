d67f368da7fc7052b5587d28634f669203cefded
Pavel Stovpivskyi <630198+spuv@users.noreply.github.com>
2020-08-12 17:27:03 +0300

    KP-14195 added fonts

--- src/UI/WB.UI.Headquarters.Core/PdfInterview/PdfInterviewGenerator.cs
+++ src/UI/WB.UI.Headquarters.Core/PdfInterview/PdfInterviewGenerator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.IO;
 using MigraDocCore.DocumentObjectModel;
 using MigraDocCore.DocumentObjectModel.MigraDoc.DocumentObjectModel.Shapes;
@@ -15,6 +16,9 @@ using WB.Core.SharedKernels.DataCollection.Aggregates;
 using WB.Core.SharedKernels.DataCollection.Implementation.Aggregates.InterviewEntities;
 using WB.Core.SharedKernels.DataCollection.Repositories;
 using WB.UI.Headquarters.Resources;
+using WB.UI.Headquarters.Services.Impl;
+using Color = MigraDocCore.DocumentObjectModel.Color;
+using Font = MigraDocCore.DocumentObjectModel.Font;
 
 namespace WB.UI.Headquarters.PdfInterview
 {
@@ -25,6 +29,25 @@ namespace WB.UI.Headquarters.PdfInterview
         private readonly IImageFileStorage imageFileStorage;
         private readonly IAttachmentContentService attachmentContentService;
 
+        private static class PdfFonts
+        {
+            public static Font HeaderLineTitle = new Font() { Size = 22, Bold = true,  };
+            public static Font SectionHeader = new Font() { Size = 20, Bold = true, Name = "RobotoBold", Color = new Color(63, 63,63 ) };
+            public static Font GroupHeader = new Font() { Size = 20, Name = "RobotoRegular" };
+            public static Font RosterTitle = new Font() { Size = 20, Name = "RobotoRegular", Italic = true };
+            public static Font QuestionTitle = new Font() { Size = 16, Name = "RobotoLight" };
+            public static Font QuestionAnswer = new Font() { Size = 14, Name = "Arial, sans-serif"};
+            public static Font QuestionNotAnswered = new Font() { Size = 11, Name = "TrebuchetMSBold"};
+            public static Font QuestionAnswerDate = new Font() { Size = 12, Italic = true, Name = "Arial, sans-serif", Color = new Color(219, 223, 226)};
+            public static Font QuestionAnswerTime = new Font() { Size = 12, Italic = true, Name = "Arial, sans-serif", Color = new Color(63, 63,63 )};
+            public static Font StaticTextTitle = new Font() { Size = 16, Name = "RobotoLight" };
+            public static Font ValidateErrorTitle = new Font() { Size = 10, Name = "TrebuchetMSBold", Color = new Color(231, 73, 36)};
+            public static Font ValidateErrorMessage = new Font() { Size = 12, Name = "Arial, Helvetica, sans-serif", Italic = true, Color = new Color(231, 73, 36) };
+            public static Font CommentAuthor = new Font() { Size = 10, Name = "TrebuchetMSBold", Color = new Color(128, 128, 128)};
+            public static Font CommentDateTime = new Font() { Size = 10, Name = "TrebuchetMSBold", Color = new Color(219, 223, 226)};
+            public static Font CommentMessage = new Font() { Size = 12, Name = "Arial, Helvetica, sans-serif", Italic = true};
+        }
+        
         public PdfInterviewGenerator(IQuestionnaireStorage questionnaireStorage,
             IStatefulInterviewRepository statefulInterviewRepository,
             IImageFileStorage imageFileStorage,
@@ -43,26 +66,15 @@ namespace WB.UI.Headquarters.PdfInterview
                 return null;
 
             var questionnaire = questionnaireStorage.GetQuestionnaire(interview.QuestionnaireIdentity, interview.Language);
-            var interviewKey = interview.GetInterviewKey().ToString();
-            var status = interview.Status.ToLocalizeString();
 
+            //GlobalFontSettings.FontResolver = new PdfInterviewFontResolver();
             ImageSource.ImageSourceImpl = new ImageSharpImageSource<SixLabors.ImageSharp.PixelFormats.Bgr24>();
             
             Document document = new Document();
+            DefineStyles(document);
             var section = document.AddSection();
-            var paragraph = section.AddParagraph();
-            paragraph.Format.Font.Size = 18;
-            paragraph.Format.Font.Color = Color.FromCmyk(100, 30, 20, 50);
-            paragraph.AddFormattedText($"{questionnaire.Title} (v. {questionnaire.Version})", TextFormat.Bold);
-            paragraph.AddLineBreak();
-            paragraph.AddFormattedText(Common.InterviewKey + ": ", TextFormat.Bold);
-            paragraph.AddFormattedText(interviewKey, TextFormat.Bold);
-            paragraph.AddLineBreak();
-            paragraph.AddFormattedText(Details.Status.Replace(@"{{ name }}", status), TextFormat.Bold);
-            paragraph.AddLineBreak();
-            paragraph.AddLineBreak();
-            paragraph.AddLineBreak();
-            
+            WritePdfInterviewHeader(section, questionnaire, interview);
+
             var nodes = interview.GetAllInterviewNodes();
             foreach (IInterviewTreeNode node in nodes)
             {
@@ -93,6 +105,28 @@ namespace WB.UI.Headquarters.PdfInterview
             return memoryStream.ToArray();
         }
 
+        private void DefineStyles(Document document)
+        {
+            var style = document.Styles["ddd"];
+        }
+
+        private static void WritePdfInterviewHeader(Section section, IQuestionnaire questionnaire, IStatefulInterview interview)
+        {
+            var interviewKey = interview.GetInterviewKey().ToString();
+            var status = interview.Status.ToLocalizeString();
+
+            var paragraph = section.AddParagraph();
+            paragraph.AddFormattedText($"{questionnaire.Title} (v. {questionnaire.Version})", PdfFonts.HeaderLineTitle);
+            paragraph.AddLineBreak();
+            paragraph.AddFormattedText(Common.InterviewKey + ": ", PdfFonts.HeaderLineTitle);
+            paragraph.AddFormattedText(interviewKey, PdfFonts.HeaderLineTitle);
+            paragraph.AddLineBreak();
+            paragraph.AddFormattedText(Details.Status.Replace(@"{{ name }}", status), PdfFonts.HeaderLineTitle);
+            paragraph.AddLineBreak();
+            paragraph.AddLineBreak();
+            paragraph.AddLineBreak();
+        }
+
         private void WriteStaticTextData(Section section, InterviewTreeStaticText staticText,
             IStatefulInterview interview, IQuestionnaire questionnaire)
         {
@@ -100,7 +134,7 @@ namespace WB.UI.Headquarters.PdfInterview
 
             paragraph.AddTab();
             paragraph.AddTab();
-            paragraph.AddFormattedText(staticText.Title.Text, TextFormat.Italic);
+            paragraph.AddFormattedText(staticText.Title.Text, PdfFonts.StaticTextTitle);
 
             var attachment = questionnaire.GetAttachmentForEntity(staticText.Identity.Id);
             if (attachment != null)
@@ -114,6 +148,8 @@ namespace WB.UI.Headquarters.PdfInterview
                 var image = paragraph.AddImage(imageSource);
                 image.Width = Unit.FromPoint(400);
             }
+
+            WriteValidateData(paragraph, staticText, interview);
             
             paragraph.AddLineBreak();
             paragraph.AddLineBreak();
@@ -122,8 +158,16 @@ namespace WB.UI.Headquarters.PdfInterview
         private void WriteGroupData(Section section, InterviewTreeGroup @group)
         {
             var paragraph = section.AddParagraph();
-            paragraph.Format.Font.Size = 20;
-            paragraph.AddFormattedText(group.Title.Text);
+            var font = group is InterviewTreeSection 
+                ? PdfFonts.SectionHeader
+                : PdfFonts.GroupHeader;
+
+            if (@group is InterviewTreeRoster roster)
+            {
+                paragraph.AddFormattedText(roster.RosterTitle + " - ", PdfFonts.RosterTitle);
+            }
+            
+            paragraph.AddFormattedText(group.Title.Text, font);
             paragraph.AddLineBreak();
             paragraph.AddLineBreak();
         }
@@ -134,17 +178,17 @@ namespace WB.UI.Headquarters.PdfInterview
             Paragraph paragraph = section.AddParagraph();
 
             if (question.AnswerTimeUtc.HasValue)
-                paragraph.AddText(question.AnswerTimeUtc.Value.ToShortDateString());
+                paragraph.AddFormattedText(question.AnswerTimeUtc.Value.ToString("MMM DD"), PdfFonts.QuestionAnswerDate);
             else
                 paragraph.AddTab();
 
             paragraph.AddTab();
             
-            paragraph.AddFormattedText(question.Title.Text, TextFormat.Italic);
+            paragraph.AddFormattedText(question.Title.Text, PdfFonts.QuestionTitle);
             paragraph.AddLineBreak();
 
             if (question.AnswerTimeUtc.HasValue)
-                paragraph.AddText(question.AnswerTimeUtc.Value.ToShortTimeString());
+                paragraph.AddFormattedText(question.AnswerTimeUtc.Value.ToString("HH:mm"), PdfFonts.QuestionAnswerTime);
             else
                 paragraph.AddTab();
 
@@ -155,7 +199,7 @@ namespace WB.UI.Headquarters.PdfInterview
                 if (question.IsAudio)
                 {
                     var audioQuestion = question.GetAsInterviewTreeAudioQuestion();
-                    paragraph.AddText(audioQuestion.GetAnswer().FileName);
+                    paragraph.AddFormattedText(audioQuestion.GetAnswer().FileName, PdfFonts.QuestionAnswer);
                 }
                 else if (question.IsMultimedia)
                 {
@@ -169,45 +213,104 @@ namespace WB.UI.Headquarters.PdfInterview
                 else if (question.IsArea)
                 {
                     var areaQuestion = question.GetAsInterviewTreeAreaQuestion();
-                    paragraph.AddText(areaQuestion.GetAnswer().Value.ToString());
+                    paragraph.AddFormattedText(areaQuestion.GetAnswer().Value.ToString(), PdfFonts.QuestionAnswer);
                 }
                 else if (question.IsGps)
                 {
                     var gpsQuestion = question.GetAsInterviewTreeGpsQuestion();
                     var geoPosition = gpsQuestion.GetAnswer().Value;
-                    paragraph.AddText($"{geoPosition.Latitude}, {geoPosition.Longitude}");
+                    paragraph.AddFormattedText($"{geoPosition.Latitude}, {geoPosition.Longitude}", PdfFonts.QuestionAnswer);
                 }
                 else
                 {
-                    paragraph.AddFormattedText(question.GetAnswerAsString(), TextFormat.Bold);
+                    paragraph.AddFormattedText(question.GetAnswerAsString(), PdfFonts.QuestionAnswer);
                 }
             }
             else
             {
-                paragraph.AddText(WebInterviewUI.Interview_Overview_NotAnswered);
+                paragraph.AddFormattedText(WebInterviewUI.Interview_Overview_NotAnswered, PdfFonts.QuestionNotAnswered);
             }
             
+            WriteValidateData(paragraph, question, interview);
+            WriteCommentsData(paragraph, question, interview);
+
             paragraph.AddLineBreak();
             paragraph.AddLineBreak();
         }
         
-        /*internal void SaveImageAsPdf(string imageFileName, string pdfFileName, int width = 600)
+        private void WriteValidateData(Paragraph paragraph, IInterviewTreeValidateable validateable, IStatefulInterview interview)
         {
-            using (var document = new PdfDocument())
+            foreach (var error in validateable.ValidationMessages)
             {
-                PdfPage page = document.AddPage();
-                using (XImage image = XImage.FromImageSource(imageFileName))
-                {
-                    var height = (int)(((double)width / (double)image.PixelWidth) * image.PixelHeight);
-
-                    page.Width = width;
-                    page.Height = height;
-
-                    XGraphics graphics = XGraphics.FromPdfPage(page);
-                    graphics.DrawImage(image, 0, 0, width, height);                
-                }
-                document.Save(pdfFileName);
+                paragraph.AddTab();
+                paragraph.AddTab();
+                paragraph.AddFormattedText(error.Text, PdfFonts.ValidateErrorTitle);
+                paragraph.AddLineBreak();
+                paragraph.AddTab();
+                paragraph.AddTab();
+                paragraph.AddFormattedText(error.Text, PdfFonts.ValidateErrorMessage);
+                paragraph.AddLineBreak();
             }
-        }*/
+        }
+
+        private void WriteCommentsData(Paragraph paragraph, InterviewTreeQuestion question, IStatefulInterview interview)
+        {
+            foreach (var comment in question.AnswerComments)
+            {
+                paragraph.AddTab();
+                paragraph.AddTab();
+                paragraph.AddFormattedText(comment.UserRole.ToUiString(), PdfFonts.CommentAuthor);
+                paragraph.AddFormattedText($" ({comment.CommentTime.ToString()})", PdfFonts.CommentDateTime);
+                paragraph.AddLineBreak();
+                paragraph.AddTab();
+                paragraph.AddTab();
+                paragraph.AddFormattedText(comment.Comment, PdfFonts.CommentMessage);
+                paragraph.AddLineBreak();
+            }
+        }
+    }
+    
+    public class PdfInterviewFontResolver : IFontResolver
+    {
+        public byte[] GetFont(string faceName)
+        {
+            using(var ms = new MemoryStream())
+            {
+                //FontFamily fontFamily = new FontFamily(faceName);
+                //fontFamily.
+                using(var fs = File.Open(faceName, FileMode.Open))
+                {
+                    fs.CopyTo(ms);
+                    ms.Position = 0;
+                    return ms.ToArray();
+                }
+            }
+        }
+
+        public string DefaultFontName => "OpenSans";
+
+        public FontResolverInfo ResolveTypeface(string familyName, bool isBold, bool isItalic)
+        {
+            if (familyName.Equals("OpenSans", StringComparison.CurrentCultureIgnoreCase))
+            {
+                if (isBold && isItalic)
+                {
+                    return new FontResolverInfo("OpenSans-BoldItalic.ttf");
+                }
+                else if (isBold)
+                {
+                    return new FontResolverInfo("OpenSans-Bold.ttf");
+                }
+                else if (isItalic)
+                {
+                    return new FontResolverInfo("OpenSans-Italic.ttf");
+                }
+                else
+                {
+                    return new FontResolverInfo("OpenSans-Regular.ttf");
+                }
+            }
+            return null;
+        }
     }
 }
