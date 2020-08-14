497faead3018b612e7ecf84ccfc0c3bfbc713c63
Victor Hladkikh <me@alfeg.net>
2020-08-14 13:33:29 +0300

    Use common httpclient for WebTester Designer Api

---
+++ src/UI/WB.UI.WebTester/Infrastructure/DesignerApiConfigurator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+using Microsoft.Extensions.Configuration;
+using WB.Infrastructure.AspNetCore;
+using WB.UI.WebTester.Services;
+
+namespace WB.UI.WebTester.Infrastructure
+{
+    public class DesignerApiConfigurator : IHttpClientConfigurator<IDesignerWebTesterApi>
+    {
+        private string designerAddress;
+
+        public DesignerApiConfigurator(IConfiguration configuration)
+        {
+            this.designerAddress = configuration["DesignerAddress"].TrimEnd('/');
+        }
+
+        public void ConfigureHttpClient(HttpClient hc)
+        {
+            hc.MaxResponseContentBufferSize = 2_000_000_000;
+            hc.BaseAddress = new Uri(designerAddress);
+            hc.Timeout = TimeSpan.FromMinutes(3);
+        }
+    }
+}
