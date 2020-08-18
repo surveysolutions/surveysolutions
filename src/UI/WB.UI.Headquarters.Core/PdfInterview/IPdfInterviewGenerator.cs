54e1ccf1a267b22e088cc6f2f35ac17ac5289323
Pavel Stovpivskyi <630198+spuv@users.noreply.github.com>
2020-08-18 16:53:30 +0300

    KP-14195 render content depending on user role

--- src/UI/WB.UI.Headquarters.Core/PdfInterview/IPdfInterviewGenerator.cs
+++ src/UI/WB.UI.Headquarters.Core/PdfInterview/IPdfInterviewGenerator.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using System.Security.Principal;
 using WB.Core.SharedKernels.DataCollection.Aggregates;
 
 namespace WB.UI.Headquarters.PdfInterview
 {
     public interface IPdfInterviewGenerator
     {
-        byte[] Generate(Guid interviewId);
+        byte[] Generate(Guid interviewId, IPrincipal user);
     }
 }
