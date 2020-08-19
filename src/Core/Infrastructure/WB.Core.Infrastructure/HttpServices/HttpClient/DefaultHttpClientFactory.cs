b9b7853c64207ef415bb7973bf73ed1718d0bbc5
Victor Gladkikh <me@alfeg.net>
2020-08-19 15:01:24 +0300

    Fix for System.Net.Http warning - moving http related services to WB.Core.Infrastructure

---
+++ src/Core/Infrastructure/WB.Core.Infrastructure/HttpServices/HttpClient/DefaultHttpClientFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Net.Http;
+using WB.Core.GenericSubdomains.Portable.Services;
+
+namespace WB.Core.GenericSubdomains.Portable.Implementation
+{
+    public class DefaultHttpClientFactory : IHttpClientFactory
+    {
+        public HttpClient CreateClient(IHttpStatistician httpStatistician)
+        {
+            return new HttpClient(new ExtendedMessageHandler(new HttpClientHandler(), httpStatistician));
+        }
+    }
+}
