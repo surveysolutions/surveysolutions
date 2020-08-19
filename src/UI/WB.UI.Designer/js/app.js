c464d96e7922e6ca3589cca4d4bf1d363de82eb7
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2020-08-18 23:03:56 -0400

    KP-13994-UI-for-reusable-categories-editing [fix localization applying]

--- src/UI/WB.UI.Designer/js/app.js
+++ src/UI/WB.UI.Designer/js/app.js
@@ -1,19 +1,19 @@
 ﻿import Vue from 'vue';
-
 import Vuei18n from './locale'
-import { browserLanguage } from './helpers'
-const i18n = Vuei18n.initialize(browserLanguage)
-
 import config from './config'
-Vue.use(config)
-
 import categories from './components/categories';
 import vuetify from './vuetify';
 
-var app = new Vue({
-    vuetify : vuetify,
-    el: '#app',
-    components: {
-        categories
-    }
+Vue.use(config)
+//plugin should be loaded and initialized
+//before app is created
+
+Vue.use(Vuei18n, (i18next) => {
+    new Vue({
+        vuetify : vuetify,
+        el: '#app',     
+        components: {
+            categories}
+   });
+
 });
