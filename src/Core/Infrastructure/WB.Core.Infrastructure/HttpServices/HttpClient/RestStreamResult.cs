d59853c0a00e9b21e927cfd20915d9f29c013305
Victor Hladkikh <me@alfeg.net>
2020-08-19 15:01:24 +0300

    Fix for System.Net.Http warning - moving http related services to WB.Core.Infrastructure

---
+++ src/Core/Infrastructure/WB.Core.Infrastructure/HttpServices/HttpClient/RestStreamResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace WB.Core.GenericSubdomains.Portable.Implementation
+{
+    public class RestStreamResult : IDisposable
+    {
+        public Stream Stream { set; get; }
+        public long? ContentLength { set; get; }
+
+        public void Dispose()
+        {
+            Stream?.Dispose();
+        }
+    }
+}
