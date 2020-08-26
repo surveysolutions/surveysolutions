2a1982b58a71c6703b880771969b3db4dc4221eb
Victor Hladkikh <me@alfeg.net>
2020-08-26 16:05:51 +0300

    KP-14164 Drop Tenant request to export service should also delete export history

---
+++ src/Services/Core/WB.Services.Scheduler/Model/JobArchive.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace WB.Services.Scheduler.Model
+{
+    public class JobArchive
+    {
+        public long Id { get; set; }
+        public string Type { get; set; } = String.Empty;
+        public string Args { get; set; } = "{}";
+        public string? Tenant { get; set; }
+        public string TenantName { get; set; } = String.Empty;
+        public JobStatus Status { get; set; }
+        public DateTime? StartAt { get; set; }
+        public DateTime? EndAt { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastUpdateAt { get; set; }
+        public DateTime? ScheduleAt { get; set; }
+    }
+}
