f59dd56d1c641a6c2770a91fd66934bb050dc692
Pavel Stovpivskyi <sby@rambler.ru>
2020-08-27 14:42:49 +0300

    KP-14195 classes moved to separate files

---
+++ src/UI/WB.UI.Headquarters.Core/PdfInterview/PdfStyles.cs
@@ -0,0 +1,28 @@
+﻿namespace WB.UI.Headquarters.PdfInterview
+{
+    public static class PdfStyles
+    {
+        public const string Default = "DefaultPdfStyle";
+        public const string HeaderLineTitle = "HeaderLineTitle";
+        public const string HeaderLineValue = "HeaderLineValue";
+        public const string SectionHeader = "SectionHeader";
+        public const string GroupHeader = "GroupHeader";
+        public const string RosterTitle = "RosterTitle";
+        public const string QuestionTitle = "QuestionTitle";
+        public const string QuestionAnswer = "QuestionAnswer";
+        public const string QuestionNotAnswered = "QuestionNotAnswered";
+        public const string QuestionAnswerDate = "QuestionAnswerDate";
+        public const string QuestionAnswerTime = "QuestionAnswerTime";
+        public const string StaticTextTitle = "StaticTextTitle";
+        public const string ValidateErrorTitle = "ValidateErrorTitle";
+        public const string ValidateErrorMessage = "ValidateErrorMessage";
+        public const string ValidateWarningTitle = "ValidateWarningTitle";
+        public const string ValidateWarningMessage = "ValidateWarningMessage";
+        public const string CommentTitle = "CommentTitle";
+        public const string CommentAuthor = "CommentAuthor";
+        public const string CommentDateTime = "CommentDateTime";
+        public const string CommentMessage = "CommentMessage";
+        public const string TableOfContent = "TableOfContent";
+        public const string YesNoTitle = "YesNoTitle";
+    }
+}
