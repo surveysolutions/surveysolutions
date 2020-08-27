8ccbf8b28d790a45eabc3423e849306ef9c8fdf5
Pavel Stovpivskyi <630198+spuv@users.noreply.github.com>
2020-08-27 14:42:49 +0300

    KP-14195 classes moved to separate files

---
+++ src/UI/WB.UI.Headquarters.Core/PdfInterview/ParagraphExtensions.cs
@@ -0,0 +1,29 @@
+﻿using MigraDocCore.DocumentObjectModel;
+
+namespace WB.UI.Headquarters.PdfInterview
+{
+    public static class ParagraphExtensions
+    {
+        public static void AddWrappedText(this Paragraph paragraph, string text)
+        {
+            for (int i = 0; i < text.Length; i+=15)
+            {
+                var str = i + 15 > text.Length
+                    ? text.Substring(i)
+                    : text.Substring(i, 15);
+                paragraph.AddText(str);
+            }
+        }
+
+        public static void AddWrapFormattedText(this Paragraph paragraph, string text, string style)
+        {
+            for (int i = 0; i < text.Length; i+=15)
+            {
+                var str = i + 15 > text.Length
+                    ? text.Substring(i)
+                    : text.Substring(i, 15);
+                paragraph.AddFormattedText(str, style);
+            }
+        }
+    }
+}
