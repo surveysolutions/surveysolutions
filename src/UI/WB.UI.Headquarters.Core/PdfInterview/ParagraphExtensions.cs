24f150f35d20a62f394c930302f029bd9f9921ac
Pavel Stovpivskyi <630198+spuv@users.noreply.github.com>
2020-08-28 13:02:21 +0300

    KP-14195 header

--- src/UI/WB.UI.Headquarters.Core/PdfInterview/ParagraphExtensions.cs
+++ src/UI/WB.UI.Headquarters.Core/PdfInterview/ParagraphExtensions.cs
@@ -4,6 +4,18 @@ namespace WB.UI.Headquarters.PdfInterview
 {
     public static class ParagraphExtensions
     {
+        public static FormattedText AddFormattedText(this Paragraph paragraph, string text, bool? isBold = null, bool? isItalic = null, Unit? size = null)
+        {
+            var formattedText = paragraph.AddFormattedText(text);
+            if (isBold.HasValue)
+                formattedText.Font.Bold = isBold.Value;
+            if (isItalic.HasValue)
+                formattedText.Font.Italic = isItalic.Value;
+            if (size.HasValue)
+                formattedText.Font.Size = size.Value;
+            return formattedText;
+        }
+        
         public static void AddWrappedText(this Paragraph paragraph, string text)
         {
             for (int i = 0; i < text.Length; i+=15)
