f5873f8e35cc2757144726e0ec015a73cc7e103c
Pavel Stovpivskyi <sby@rambler.ru>
2020-08-31 17:12:07 +0300

    KP-14195 added pdf colors

---
+++ src/UI/WB.UI.Headquarters.Core/PdfInterview/PdfColors.cs
@@ -0,0 +1,12 @@
+﻿using MigraDocCore.DocumentObjectModel;
+
+namespace WB.UI.Headquarters.PdfInterview
+{
+    public class PdfColors
+    {
+        public static readonly Color Default = Colors.Black;
+        public static readonly Color Error = new Color(231, 73, 36);
+        public static readonly Color Warning = new Color(255, 138, 0);
+        public static readonly Color Comment = new Color(71, 27, 195);
+    }
+}
