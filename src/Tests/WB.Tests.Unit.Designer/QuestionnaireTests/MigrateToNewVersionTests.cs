31ba5afe90a44298015e27c738b8e446c6bd61aa
Pavel Stovpivskyi <sby@rambler.ru>
2020-09-04 18:46:06 +0300

    KP-14255 updated tests

--- src/Tests/WB.Tests.Unit.Designer/QuestionnaireTests/MigrateToNewVersionTests.cs
+++ src/Tests/WB.Tests.Unit.Designer/QuestionnaireTests/MigrateToNewVersionTests.cs
@@ -4,6 +4,7 @@ using Main.Core.Entities.SubEntities;
 using NUnit.Framework;
 using WB.Core.BoundedContexts.Designer.Aggregates;
 using WB.Core.BoundedContexts.Designer.Resources;
+using WB.Tests.Abc;
 using WB.Tests.Unit.Designer.BoundedContexts.QuestionnaireTests;
 
 namespace WB.Tests.Unit.Designer.QuestionnaireTests
@@ -16,9 +17,16 @@ namespace WB.Tests.Unit.Designer.QuestionnaireTests
         {
             // arrange
             Guid coverId = Guid.NewGuid();
-            Questionnaire questionnaire = CreateQuestionnaire(responsibleId: Guid.NewGuid());
+            var responsibleId = Guid.NewGuid();
+            Questionnaire questionnaire = CreateQuestionnaire(responsibleId: responsibleId);
             // it is need to get questionnaire in old format
             questionnaire.QuestionnaireDocument.CoverPageSectionId = coverId;
+            questionnaire.AddTextQuestion(Id.g1, questionnaire.QuestionnaireDocument.Children[1].PublicKey, responsibleId, 
+                isPreFilled: true);
+            questionnaire.AddTextQuestion(Id.g2, questionnaire.QuestionnaireDocument.Children[1].PublicKey, responsibleId, 
+                isPreFilled: false);
+            questionnaire.AddTextQuestion(Id.g3, questionnaire.QuestionnaireDocument.Children[1].PublicKey, responsibleId, 
+                isPreFilled: true);
 
             // act
             questionnaire.MigrateToNewVersion();
@@ -28,6 +36,23 @@ namespace WB.Tests.Unit.Designer.QuestionnaireTests
             Assert.That(cover, Is.Not.Null);
             Assert.That(cover.Title, Is.EqualTo(QuestionnaireEditor.CoverPageSection));
             Assert.That(questionnaire.QuestionnaireDocument.Children.First(), Is.EqualTo(cover));
+
+            var question1 = questionnaire.QuestionnaireDocument.Find<IQuestion>(Id.g1);
+            Assert.That(question1, Is.Not.Null);
+            Assert.That(question1.GetParent().PublicKey, Is.EqualTo(questionnaire.QuestionnaireDocument.CoverPageSectionId));
+            Assert.That(question1.Featured, Is.True);
+            Assert.That(cover.Children.First(), Is.EqualTo(question1));
+
+            var question2 = questionnaire.QuestionnaireDocument.Find<IQuestion>(Id.g2);
+            Assert.That(question2, Is.Not.Null);
+            Assert.That(question2.GetParent().PublicKey, Is.Not.EqualTo(questionnaire.QuestionnaireDocument.CoverPageSectionId));
+            Assert.That(question2.Featured, Is.False);
+
+            var question3 = questionnaire.QuestionnaireDocument.Find<IQuestion>(Id.g3);
+            Assert.That(question3, Is.Not.Null);
+            Assert.That(question3.GetParent().PublicKey, Is.EqualTo(questionnaire.QuestionnaireDocument.CoverPageSectionId));
+            Assert.That(question3.Featured, Is.False);
+            Assert.That(cover.Children.Second(), Is.EqualTo(question3));
         }
 
         [Test]
