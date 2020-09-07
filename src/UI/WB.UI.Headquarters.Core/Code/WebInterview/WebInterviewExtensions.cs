820e699a6f83990f5812e0a509e711c890997070
Pavel Stovpivskyi <sby@rambler.ru>
2020-09-07 17:41:13 +0300

    Save interview id in session

--- src/UI/WB.UI.Headquarters.Core/Code/WebInterview/WebInterviewExtensions.cs
+++ src/UI/WB.UI.Headquarters.Core/Code/WebInterview/WebInterviewExtensions.cs
@@ -1,24 +1,21 @@
 ﻿using System;
 using System.Linq;
 using Microsoft.AspNetCore.Http;
+using WB.Core.GenericSubdomains.Portable;
 using WB.Core.SharedKernels.DataCollection.Aggregates;
 
 namespace WB.UI.Headquarters.Code.WebInterview
 {
     public static class WebInterviewExtensions
     {
-        public static bool HasInterviewInfoInCookie(this IRequestCookieCollection cookieCollection, Guid interviewId)
+        public static void SaveWebInterviewAccessForCurrentUser(this HttpContext context, string interviewId)
         {
-            return cookieCollection.Keys.Where(key => key.StartsWith($"InterviewId-"))
-                .Any(key =>
-                    Guid.TryParse(cookieCollection[key], out Guid cookieInterviewId)
-                    && cookieInterviewId == interviewId
-                );
+            context.Session.Set("WebInterview-" + interviewId, true);
         }
         
-        public static bool HasAccessToWebInterviewAfterComplete(this HttpRequest request, IStatefulInterview interview)
+        public static bool HasAccessToWebInterviewAfterComplete(this HttpContext context, IStatefulInterview interview)
         {
-            var isExistsInterviewInCookie = request.Cookies.HasInterviewInfoInCookie(interview.Id);
+            var isExistsInterviewInCookie = context.Session.Get<bool>("WebInterview-" + interview.Id.FormatGuid());
             var hasAccess = isExistsInterviewInCookie && interview.CompletedDate.HasValue
                                                       && interview.CompletedDate.Value.UtcDateTime.AddHours(1) > DateTime.UtcNow;
             return hasAccess;
