2d02aa52983e74a73d8af027786ef10ded07e491
Victor Gladkikh <me@alfeg.net>
2020-09-08 17:40:21 +0300

    KP-14260 Fixed test for answering datetime

--- src/Tests/WB.Tests.Unit/SharedKernels/DataCollection/InterviewTests/when_answering_multi_linked_to_List_question_and_answer_is_specified.cs
+++ src/Tests/WB.Tests.Unit/SharedKernels/DataCollection/InterviewTests/when_answering_multi_linked_to_List_question_and_answer_is_specified.cs
@@ -53,7 +53,7 @@ namespace WB.Tests.Unit.SharedKernels.DataCollection.InterviewTests
             eventContext.GetSingleEvent<MultipleOptionsQuestionAnswered>().RosterVector.Should().BeEquivalentTo(propagationVector);
 
         [NUnit.Framework.Test] public void should_raise_MultipleOptionsQuestionAnswered_event_with_AnswerTime_equal_to_answerTime () =>
-            eventContext.GetSingleEvent<MultipleOptionsQuestionAnswered>().OriginDate.Value.UtcDateTime.Should().BeCloseTo(DateTime.UtcNow, 2000);
+            eventContext.GetSingleEvent<MultipleOptionsQuestionAnswered>().OriginDate.Value.UtcDateTime.Should().BeCloseTo(answerTime.UtcDateTime, 2000);
 
         private static EventContext eventContext;
         private static Interview interview;
