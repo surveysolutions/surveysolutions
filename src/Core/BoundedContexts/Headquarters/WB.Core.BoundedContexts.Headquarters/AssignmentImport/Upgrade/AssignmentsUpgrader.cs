ed216dcedc1a6e78868fe1be32d403b1a44ce41e
Andrii Kozhyn <hedgic@gmail.com>
2020-09-09 19:20:58 +0300

    KP-14268 assignments upgrade performance

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/AssignmentImport/Upgrade/AssignmentsUpgrader.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/AssignmentImport/Upgrade/AssignmentsUpgrader.cs
@@ -1,56 +1,39 @@
-﻿using System;
+﻿#nullable enable
+using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
 using WB.Core.BoundedContexts.Headquarters.Assignments;
-using WB.Core.BoundedContexts.Headquarters.Invitations;
-using WB.Core.BoundedContexts.Headquarters.Services.Preloading;
-using WB.Core.Infrastructure.CommandBus;
-using WB.Core.Infrastructure.PlainStorage;
-using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
-using WB.Core.SharedKernels.DataCollection.Commands.Assignment;
+using WB.Core.SharedKernels.DataCollection.Aggregates;
 using WB.Core.SharedKernels.DataCollection.Implementation.Entities;
 using WB.Core.SharedKernels.DataCollection.Repositories;
 
 namespace WB.Core.BoundedContexts.Headquarters.AssignmentImport.Upgrade
 {
-    public interface IAssignmentsUpgrader
-    {
-        void Upgrade(Guid processId, Guid userId, QuestionnaireIdentity migrateFrom, QuestionnaireIdentity migrateTo, CancellationToken cancellationToken);
-    }
-
     internal class AssignmentsUpgrader : IAssignmentsUpgrader
     {
         private readonly IAssignmentsService assignments;
-        private readonly IPreloadedDataVerifier importService;
         private readonly IQuestionnaireStorage questionnaireStorage;
         private readonly IAssignmentsUpgradeService upgradeService;
-        private readonly IAssignmentFactory assignmentFactory;
-        private readonly IInvitationService invitationService;
-        private readonly ICommandService commandService;
+        private readonly ISingleAssignmentUpgrader assignmentUpgrader;
 
         public AssignmentsUpgrader(IAssignmentsService assignments,
-            IPreloadedDataVerifier importService,
             IQuestionnaireStorage questionnaireStorage,
             IAssignmentsUpgradeService upgradeService,
-            IAssignmentFactory assignmentFactory,
-            IInvitationService invitationService,
-            ICommandService commandService)
+            ISingleAssignmentUpgrader assignmentUpgrader)
         {
             this.assignments = assignments ?? throw new ArgumentNullException(nameof(assignments));
-            this.importService = importService ?? throw new ArgumentNullException(nameof(importService));
-            this.questionnaireStorage = questionnaireStorage ?? throw new ArgumentNullException(nameof(questionnaireStorage));
+            this.questionnaireStorage =
+                questionnaireStorage ?? throw new ArgumentNullException(nameof(questionnaireStorage));
             this.upgradeService = upgradeService;
-            this.assignmentFactory = assignmentFactory;
-            this.invitationService = invitationService;
-            this.commandService = commandService;
+            this.assignmentUpgrader = assignmentUpgrader;
         }
 
-        public void Upgrade(Guid processId, Guid userId, QuestionnaireIdentity migrateFrom, QuestionnaireIdentity migrateTo, CancellationToken cancellation)
-        { 
+        public void Upgrade(Guid processId, Guid userId, QuestionnaireIdentity migrateFrom,
+            QuestionnaireIdentity migrateTo, CancellationToken cancellation)
+        {
             var idsToMigrate = assignments.GetAllAssignmentIdsForMigrateToNewVersion(migrateFrom);
 
-            var targetQuestionnaire = this.questionnaireStorage.GetQuestionnaire(migrateTo, null);
+            IQuestionnaire targetQuestionnaire = this.questionnaireStorage.GetQuestionnaireOrThrow(migrateTo, null);
             int migratedSuccessfully = 0;
             List<AssignmentUpgradeError> upgradeErrors = new List<AssignmentUpgradeError>();
 
@@ -60,41 +43,14 @@ namespace WB.Core.BoundedContexts.Headquarters.AssignmentImport.Upgrade
                 {
                     cancellation.ThrowIfCancellationRequested();
 
-                    var oldAssignment = assignments.GetAssignment(assignmentId);
-                    if (!oldAssignment.IsCompleted)
+                    try
                     {
-                        var assignmentVerification =
-                            this.importService.VerifyWithInterviewTree(oldAssignment.Answers, null,
-                                targetQuestionnaire);
-                        if (assignmentVerification == null)
-                        {
-                            var newAssignment = assignmentFactory.CreateAssignment(
-                                userId,
-                                migrateTo, 
-                                oldAssignment.ResponsibleId,
-                                oldAssignment.InterviewsNeeded, 
-                                oldAssignment.Email, 
-                                oldAssignment.Password, 
-                                oldAssignment.WebMode,
-                                oldAssignment.AudioRecording,
-                                oldAssignment.Answers.ToList(),
-                                oldAssignment.ProtectedVariables,
-                                oldAssignment.Comments);
-
-                            invitationService.MigrateInvitationToNewAssignment(assignmentId, newAssignment.Id);
-                            commandService.Execute(new UpgradeAssignmentCommand(oldAssignment.PublicKey, userId));
-
-                            migratedSuccessfully++;
-                        }
-                        else
-                        {
-                            upgradeErrors.Add(new AssignmentUpgradeError(assignmentId,
-                                assignmentVerification.ErrorMessage));
-                        }
-                    }
-                    else
-                    {
+                        this.assignmentUpgrader.Upgrade(assignmentId, targetQuestionnaire, userId, migrateTo);
                         migratedSuccessfully++;
+                    }
+                    catch (AssignmentUpgradeException e)
+                    {
+                        upgradeErrors.Add(new AssignmentUpgradeError(assignmentId, e.Message));
                     }
 
                     this.upgradeService.ReportProgress(processId,
