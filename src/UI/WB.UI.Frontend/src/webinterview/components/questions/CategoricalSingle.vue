758c45c6799852ede4d6aafe5734fcc5d6a83dc5
Andrii Kozhyn <andrii.kozhyn@outlook.com>
2020-09-10 16:30:59 +0300

    KP-14005 single linked option can be rendered as combobox

--- src/UI/WB.UI.Frontend/src/webinterview/components/questions/CategoricalSingle.vue
+++ src/UI/WB.UI.Frontend/src/webinterview/components/questions/CategoricalSingle.vue
@@ -6,30 +6,46 @@
         <div class="question-unit">
             <div class="options-group"
                 v-bind:class="{ 'dotted': noOptions }">
-                <div class="radio"
-                    v-for="option in answeredOrAllOptions"
-                    :key="$me.id + '_' + option.value">
-                    <div class="field">
-                        <input class="wb-radio"
-                            type="radio"
-                            :id="`${$me.id}_${option.value}`"
-                            :name="$me.id"
-                            :value="option.value"
+                <div class="form-group"
+                    v-if="$me.renderAsCombobox && !noOptions">
+                    <div class="field"
+                        :class="{answered: $me.isAnswered}">
+                        <wb-typeahead :questionId="$me.id"
                             :disabled="!$me.acceptAnswer"
-                            v-model="answer">
-                        <label :for="$me.id + '_' + option.value">
-                            <span class="tick"></span> {{option.title}}
-                        </label>
-                        <wb-remove-answer :id-suffix="`_opt_${option.value}`"/>
+                            :optionsSource="optionsSource"
+                            :value="answer"
+                            @input="answerSingleOption"
+                            :watermark="!$me.acceptAnswer && !$me.isAnswered ? $t('Details.NoAnswer') : null"/>
+                        <wb-remove-answer />
                     </div>
                 </div>
-                <button type="button"
-                    class="btn btn-link btn-horizontal-hamburger"
-                    @click="toggleOptions"
-                    v-if="shouldShowAnsweredOptionsOnly && !showAllOptions"
-                    :id="`btn_${$me.id}_ShowAllOptions`">
-                    <span></span>
-                </button>
+
+                <template  v-if="!$me.renderAsCombobox">
+                    <div class="radio"
+                        v-for="option in answeredOrAllOptions"
+                        :key="$me.id + '_' + option.value">
+                        <div class="field">
+                            <input class="wb-radio"
+                                type="radio"
+                                :id="`${$me.id}_${option.value}`"
+                                :name="$me.id"
+                                :value="option.value"
+                                :disabled="!$me.acceptAnswer"
+                                @input="answerSingleOption(option.value)">
+                            <label :for="$me.id + '_' + option.value">
+                                <span class="tick"></span> {{option.title}}
+                            </label>
+                            <wb-remove-answer :id-suffix="`_opt_${option.value}`"/>
+                        </div>
+                    </div>
+                    <button type="button"
+                        class="btn btn-link btn-horizontal-hamburger"
+                        @click="toggleOptions"
+                        v-if="shouldShowAnsweredOptionsOnly && !showAllOptions"
+                        :id="`btn_${$me.id}_ShowAllOptions`">
+                        <span></span>
+                    </button>
+                </template>
                 <div v-if="noOptions"
                     class="options-not-available">
                     {{ $t("WebInterviewUI.OptionsAvailableAfterAnswer") }}
@@ -41,7 +57,6 @@
 </template>
 <script lang="js">
 import { entityDetails } from '../mixins'
-import { find } from 'lodash'
 import { shouldShowAnsweredOptionsOnlyForSingle } from './question_helpers'
 
 export default {
@@ -61,17 +76,10 @@ export default {
                 return this.$me.options
 
             var self = this
-            return [find(this.$me.options, function(o) { return o.value == self.answer })]
+            return [this.$me.options.find((o) => { return o.value == self.answer })]
         },
-        answer: {
-            get() {
-                return this.$me.answer
-            },
-            set(value) {
-                this.sendAnswer(() => {
-                    this.$store.dispatch('answerSingleOptionQuestion', { answer: value, identity: this.$me.id })
-                })
-            },
+        answer() {
+            return this.$me.options.find((o) => o.value === this.$me.answer)
         },
         noOptions() {
             return this.$me.options == null || this.$me.options.length == 0
@@ -79,8 +87,22 @@ export default {
     },
     mixins: [entityDetails],
     methods: {
+        answerSingleOption(value){
+            this.sendAnswer(() => {
+                this.$store.dispatch('answerSingleOptionQuestion', { answer: value, identity: this.$me.id })
+            })
+        },
         toggleOptions(){
             this.showAllOptions = !this.showAllOptions
+        },
+        optionsSource(filter)
+        {
+            if(!filter)
+                return Promise.resolve(this.$me.options)
+            const search = filter.toLowerCase()
+            const filtered = this.$me.options.filter(v => v.title.toLowerCase().indexOf(search) >= 0)
+
+            return Promise.resolve(filtered)
         },
     },
 }
