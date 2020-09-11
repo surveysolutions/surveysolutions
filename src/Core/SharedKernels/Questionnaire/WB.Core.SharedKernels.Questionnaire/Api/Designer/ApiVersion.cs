13c12c9337cee3d383772877258d17a3895afae3
Andrii Kozhyn <andrii.kozhyn@outlook.com>
2020-09-11 10:51:39 +0300

    KP-14005 questionnaire version up on usage of lined combobox

--- src/Core/SharedKernels/Questionnaire/WB.Core.SharedKernels.Questionnaire/Api/Designer/ApiVersion.cs
+++ src/Core/SharedKernels/Questionnaire/WB.Core.SharedKernels.Questionnaire/Api/Designer/ApiVersion.cs
@@ -3,6 +3,6 @@ namespace WB.Core.SharedKernels.SurveySolutions.Api.Designer
     public static class ApiVersion
     {
         public const int CurrentTesterProtocolVersion = 53; // 20.09 release
-        public const int MaxQuestionnaireVersion = 31;
+        public const int MaxQuestionnaireVersion = 32;
     }
 }
