73cb9a853367acff7cb95ed920be0bd846bca8cf
Victor Gladkikh <me@alfeg.net>
2020-09-11 10:57:26 +0300

    KP-14266 Wait for payload  processing. Spelling fixes

--- src/Core/SharedKernels/Enumerator/Enumerator/OfflineSync/Services/Implementation/PayloadSerializer.cs
+++ src/Core/SharedKernels/Enumerator/Enumerator/OfflineSync/Services/Implementation/PayloadSerializer.cs
@@ -14,39 +14,24 @@ namespace WB.Core.SharedKernels.Enumerator.OfflineSync.Services.Implementation
             this.serializer = serializer;
         }
 
-        public Task<T> FromPayloadAsync<T>(byte[] payload)
+        public async Task<T> FromPayloadAsync<T>(byte[] payload)
         {
-            using (var ms = new MemoryStream(payload))
-            {
-                using (var zip = new GZipStream(ms, CompressionMode.Decompress))
-                {
-                    using (var sr = new StreamReader(zip))
-                    {
-                        // TODO: Fix to async as soon https://github.com/xamarin/xamarin-android/issues/3397 fixed
-                        var json = sr.ReadToEnd();
-                        return Task.FromResult(this.serializer.Deserialize<T>(json));
-                    }
-                }
-            }
+            var ms = new MemoryStream(payload);
+            await using var zip = new GZipStream(ms, CompressionMode.Decompress);
+            using var sr = new StreamReader(zip);
+            var json = await sr.ReadToEndAsync();
+            return this.serializer.Deserialize<T>(json);
         }
         
-        public Task<byte[]> ToPayloadAsync<T>(T message)
+        public async Task<byte[]> ToPayloadAsync<T>(T message)
         {
-            using (var ms = new MemoryStream())
-            {
-                using (var zip = new GZipStream(ms, CompressionLevel.Optimal))
-                {
-                    using (var sw = new StreamWriter(zip))
-                    {
-                        var json = this.serializer.Serialize(message);
-                        
-                        // TODO: Fix to async as soon https://github.com/xamarin/xamarin-android/issues/3397 fixed
-                        sw.Write(json);
-                    }
-                }
+            var ms = new MemoryStream();
+            await using var zip = new GZipStream(ms, CompressionLevel.Optimal);
+            await using var sw = new StreamWriter(zip);
+            var json = this.serializer.Serialize(message);
+            await sw.WriteAsync(json);
 
-                return Task.FromResult(ms.ToArray());
-            }
+            return ms.ToArray();
         }
     }
 }
