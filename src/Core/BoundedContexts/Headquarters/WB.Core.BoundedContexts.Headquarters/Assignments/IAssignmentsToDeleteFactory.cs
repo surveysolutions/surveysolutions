145c417219067b9ea94035ccd35cfd9ceaf3c035
Pavel Stovpivskyi <630198+spuv@users.noreply.github.com>
2020-09-11 14:32:09 +0300

    KP-14263 Moved remove methods inside remove classes

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Assignments/IAssignmentsToDeleteFactory.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Assignments/IAssignmentsToDeleteFactory.cs
@@ -6,7 +6,6 @@ namespace WB.Core.BoundedContexts.Headquarters.Assignments
 {
     public interface IAssignmentsToDeleteFactory
     {
-        void RemoveAllAssignments(QuestionnaireIdentity questionnaireIdentity);
-        void RemoveAllEventsForAssignments(QuestionnaireIdentity questionnaireIdentity);
+        void RemoveAllAssignmentsData(QuestionnaireIdentity questionnaireIdentity);
     }
 }
