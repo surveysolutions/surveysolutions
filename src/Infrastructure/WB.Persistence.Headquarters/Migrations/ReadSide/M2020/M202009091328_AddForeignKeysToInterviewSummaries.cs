1049cce2ce3611c5686d4f06e35deb48ac43eb64
Pavel Stovpivskyi <sby@rambler.ru>
2020-09-15 13:19:06 +0300

    KP-14263 added new index

--- src/Infrastructure/WB.Persistence.Headquarters/Migrations/ReadSide/M2020/M202009091328_AddForeignKeysToInterviewSummaries.cs
+++ src/Infrastructure/WB.Persistence.Headquarters/Migrations/ReadSide/M2020/M202009091328_AddForeignKeysToInterviewSummaries.cs
@@ -8,10 +8,6 @@ namespace WB.Persistence.Headquarters.Migrations.ReadSide
     {
         public override void Up()
         {
-            Create.Index("interviewsummaries_interviewid_unique_idx")
-                .OnTable("interviewsummaries").InSchema("readside")
-                .OnColumn("interviewid").Unique();
-            
             Create.ForeignKey("fk_interview_geo_answers_to_interviewsummaries")
                 .FromTable("interview_geo_answers").InSchema("readside").ForeignColumn("interview_id")
                 .ToTable("interviewsummaries").InSchema("readside").PrimaryColumn("id")
@@ -29,6 +25,11 @@ namespace WB.Persistence.Headquarters.Migrations.ReadSide
 
             Create.ForeignKey("fk_commentaries_to_interviewsummaries")
                 .FromTable("commentaries").InSchema("readside").ForeignColumn("summary_id")
+                .ToTable("interviewsummaries").InSchema("readside").PrimaryColumn("id")
+                .OnDelete(Rule.Cascade);
+
+            Create.ForeignKey("fk_interviewcommentedstatuses_to_interviewsummaries")
+                .FromTable("interviewcommentedstatuses").InSchema("readside").ForeignColumn("interview_id")
                 .ToTable("interviewsummaries").InSchema("readside").PrimaryColumn("id")
                 .OnDelete(Rule.Cascade);
         }
