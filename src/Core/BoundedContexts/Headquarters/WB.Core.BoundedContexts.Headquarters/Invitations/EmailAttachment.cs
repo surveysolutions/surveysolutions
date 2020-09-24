d7414e0b13191961f216ca8c799efebd10561420
Pavel Stovpivskyi <sby@rambler.ru>
2020-09-24 14:05:36 +0300

    KP-14289 use memory stream as attachment content

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Invitations/EmailAttachment.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Invitations/EmailAttachment.cs
@@ -11,7 +11,7 @@ namespace WB.Core.BoundedContexts.Headquarters.Invitations
     public class EmailAttachment
     {
         public string Filename { get; set; }
-        public string Base64String { get; set; }
+        public MemoryStream Content { get; set; }
         public string ContentType { get; set; }
         public string ContentId { get; set; }
         public EmailAttachmentDisposition Disposition { get; set; } = EmailAttachmentDisposition.Attachment;
