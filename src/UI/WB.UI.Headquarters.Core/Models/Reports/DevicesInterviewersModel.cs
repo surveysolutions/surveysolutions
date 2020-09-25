464cfb7e569582d303acf6c3eeb0960222aff4fd
Pavel Stovpivskyi <sby@rambler.ru>
2020-09-25 12:49:42 +0300

    KP-14235 Supervisor link leads to nowhere

--- src/UI/WB.UI.Headquarters.Core/Models/Reports/DevicesInterviewersModel.cs
+++ src/UI/WB.UI.Headquarters.Core/Models/Reports/DevicesInterviewersModel.cs
@@ -5,6 +5,7 @@
         public string DataUrl { get; set; }
         public string InterviewersBaseUrl { get; set; }
         public string InterviewerProfileUrl { get; set; }
+        public string SupervisorProfileUrl { get; set; }
         public string SupervisorName { get; set; }
         public string BaseReportUrl { get; set; }
     }
