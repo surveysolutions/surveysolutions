a94f56ed5c826cc1e839714ed4aaddab38e8c7b4
Pavel Stovpivskyi <630198+spuv@users.noreply.github.com>
2020-09-25 12:26:26 +0300

    KP-14289 added  test

--- src/Tests/WB.Tests.Unit/BoundedContexts/Headquarters/Invitations/EmailContentTests.cs
+++ src/Tests/WB.Tests.Unit/BoundedContexts/Headquarters/Invitations/EmailContentTests.cs
@@ -73,5 +73,41 @@ namespace WB.Tests.Unit.BoundedContexts.Headquarters.Invitations
             Assert.That(emailContent.MainText.Contains("barcode <img src='data:image/png;base64,/9j/"), Is.True);
             Assert.That(emailContent.MainText.Contains("qrcode <img src='data:image/png;base64,/9j/"), Is.True);
         }
+
+        [Test]
+        public void when_insert_interview_data_in_html_email_with_attachment()
+        {
+            string subject = string.Empty, password = string.Empty, link = string.Empty;
+            string text = @"text
+                    your answer %a% 
+                    barcode %a:barcode%
+                    qrcode %a:qrcode%
+                    thanks" ;
+
+            var questionnaireDocument = Create.Entity.QuestionnaireDocumentWithOneChapter(children: new IComposite[]
+            {
+                Create.Entity.TextQuestion(questionId: Id.g1, variable: "a")
+            });
+            var questionnaire = Create.Entity.PlainQuestionnaire(questionnaireDocument);
+            var interview = Create.AggregateRoot.StatefulInterview(interviewId: Id.g2, questionnaire: questionnaireDocument);
+            interview.AnswerTextQuestion(Guid.NewGuid(), Id.g1, RosterVector.Empty, DateTimeOffset.UtcNow, "answer");
+
+            var template = new WebInterviewEmailTemplate(subject, text, password, link);
+            
+            EmailContent emailContent = new EmailContent(template, "qtitle", "", "");
+            emailContent.AttachmentMode = EmailContentAttachmentMode.InlineAttachment;
+            emailContent.TextMode = EmailContentTextMode.Html;
+            
+            emailContent.RenderInterviewData(interview, questionnaire);
+
+            Assert.That(emailContent.Attachments.Count, Is.EqualTo(2));
+            Assert.That(emailContent.Attachments[0].Disposition, Is.EqualTo(EmailAttachmentDisposition.Inline));
+            Assert.That(emailContent.Attachments[1].Disposition, Is.EqualTo(EmailAttachmentDisposition.Inline));
+            Assert.That(emailContent.MainText, Is.EqualTo(@$"text
+                    your answer answer 
+                    barcode <img src='cid:{emailContent.Attachments[0].ContentId}'/>
+                    qrcode <img src='cid:{emailContent.Attachments[1].ContentId}'/>
+                    thanks"));
+        }
     }
 }
