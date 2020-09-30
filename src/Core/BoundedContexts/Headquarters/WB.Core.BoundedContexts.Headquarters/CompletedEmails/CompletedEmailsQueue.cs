30151cdec07d6de9b924ba3fe6ddb06b22873b60
Pavel Stovpivskyi <630198+spuv@users.noreply.github.com>
2020-09-30 19:41:58 +0300

    KP-14319 added wait on fail

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/CompletedEmails/CompletedEmailsQueue.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/CompletedEmails/CompletedEmailsQueue.cs
@@ -18,7 +18,21 @@ namespace WB.Core.BoundedContexts.Headquarters.Invitations
 
         public List<Guid> GetInterviewIdsForSend(int batchSize = 100)
         {
+            DateTime now = DateTime.UtcNow;
+            var now10Minutes = now.AddMinutes(-10);
+            var now20Minutes = now.AddMinutes(-20);
+            var now30Minutes = now.AddMinutes(-30);
+            var now1Hour = now.AddHours(-1);
+            var now3Hours = now.AddHours(-3);
+            
             return storage.Query(_ => _
+                .Where(r => r.FailedCount == 0
+                    || (r.FailedCount == 1 && r.RequestTime >= now10Minutes)
+                    || (r.FailedCount == 2 && r.RequestTime >= now20Minutes)
+                    || (r.FailedCount == 3 && r.RequestTime >= now30Minutes)
+                    || (r.FailedCount == 4 && r.RequestTime >= now1Hour)
+                    || (r.FailedCount >= 5 && r.RequestTime >= now3Hours)
+                )
                 .OrderBy(r => r.RequestTime)
                 .Select(r => r.InterviewId))
                 .Take(batchSize)
