56c78aef36236e1ab6d5463d5a59ad032f2239e3
Pavel Stovpivskyi <sby@rambler.ru>
2020-09-30 17:21:23 +0300

    KP-14319 added metrics for size on completed emails table

--- src/UI/WB.UI.Headquarters.Core/Metrics/UseMetricsExtensions.cs
+++ src/UI/WB.UI.Headquarters.Core/Metrics/UseMetricsExtensions.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Prometheus;
+using WB.Core.BoundedContexts.Headquarters.CompletedEmails;
 using WB.Core.BoundedContexts.Headquarters.Implementation.Synchronization;
 using WB.Infrastructure.Native.Monitoring;
 using WB.Infrastructure.Native.Storage.Postgre;
@@ -24,6 +25,7 @@ namespace WB.UI.Headquarters.Metrics
             services.AddTransient<IOnDemandCollector, BrokenPackagesStatsCollector>();
             services.AddTransient<IOnDemandCollector, NHibernateStatsCollector>();
             services.AddTransient<IOnDemandCollector, ThreadPoolStatsCollector>();
+            services.AddTransient<IOnDemandCollector, CompletedEmailStatsCollector>();
         }
   
         public static void UseMetrics(this IApplicationBuilder app, IConfiguration configuration)
