ce5e89dd7e0a6102030fa94b1ff93562528545d9
Andrii Kozhyn <hedgic@gmail.com>
2020-10-01 18:05:52 +0300

    KP-14331 initial markdown support

--- src/UI/WB.UI.Frontend/src/hqapp/Views/HQ/WebInterview/Start.vue
+++ src/UI/WB.UI.Frontend/src/hqapp/Views/HQ/WebInterview/Start.vue
@@ -2,7 +2,8 @@
     <div>
         <div class="row-element mb-20">
             <h2>{{ $config.model.welcomeText }}</h2>
-            <p v-if="$config.model.description">
+            <p v-if="$config.model.description"
+                v-html="description">
                 {{$config.model.description}}
             </p>
         </div>
@@ -14,10 +15,16 @@
 <script>
 
 import StartOrResumeForm from './_StartOrResumeForm'
+import marked from 'marked'
 
 export default {
     components: {
         StartOrResumeForm,
+    },
+    computed:{
+        description() {
+            return marked.parseInline(this.$config.model.description)
+        },
     },
 }
 
