d463a9f00843cb0f4773f8f362644757de7984e8
Victor Hladkikh <me@alfeg.net>
2020-10-01 15:00:21 +0300

    KP-13994 Code review fixes

--- src/UI/WB.UI.Designer/questionnaire-app/src/views/OptionsEditor/components/OptionItemsTable.vue
+++ src/UI/WB.UI.Designer/questionnaire-app/src/views/OptionsEditor/components/OptionItemsTable.vue
@@ -178,7 +178,6 @@ export default {
         },
 
         editItem(item) {
-            console.log('editItem', Object.assign({}, item));
             this.editedIndex = this.categories.indexOf(item);
             this.editedItem = Object.assign({}, item);
             this.dialog = true;
