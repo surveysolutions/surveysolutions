04573e4e311f46001b393346e7c1f16ea5a1e9d8
Pavel Stovpivskyi <sby@rambler.ru>
2020-10-01 11:27:34 +0300

    KP-14319 added nullable enable

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/EmailProviders/IEmailService.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/EmailProviders/IEmailService.cs
@@ -1,3 +1,5 @@
+#nullable enable
+
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using WB.Core.BoundedContexts.Headquarters.Invitations;
@@ -7,7 +9,7 @@ namespace WB.Core.BoundedContexts.Headquarters.EmailProviders
 {
     public interface IEmailService
     {
-        Task<string> SendEmailAsync(string to, string subject, string htmlBody, string textBody, List<EmailAttachment> attachments = null);
+        Task<string> SendEmailAsync(string to, string subject, string htmlBody, string textBody, List<EmailAttachment>? attachments = null);
         bool IsConfigured();
         ISenderInformation GetSenderInfo();
     }
