cc2e853be9caf64409af17b38fc2bc6274a52811
Victor Gladkikh <me@alfeg.net>
2020-10-01 15:00:21 +0300

    KP-13994 Code review fixes

--- src/UI/WB.UI.Designer/questionnaire-app/src/views/PageNotFound.vue
+++ src/UI/WB.UI.Designer/questionnaire-app/src/views/PageNotFound.vue
@@ -4,17 +4,7 @@
             <v-layout row wrap>
                 <v-flex xs12>
                     <v-card height="400" color="transparent" flat>
-                        <div class="display-3 mt-5">Page not found.</div>
-                        <div class="grey--text lighten-5">
-                            The page you are trying to get to never existed in
-                            this reality, or has migrated to a parallel
-                            universe.
-                        </div>
-                        <div class="paragraph-text mt-2">
-                            <br />
-                            <br />Try going back to home page and repeating your
-                            action
-                        </div>
+                        <div class="display-3 mt-5">404</div>
                     </v-card>
                 </v-flex>
             </v-layout>
