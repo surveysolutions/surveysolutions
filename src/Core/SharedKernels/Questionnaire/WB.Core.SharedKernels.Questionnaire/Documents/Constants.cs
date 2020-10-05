0b4ff60888b37f87e68fe81352c18cc49b5e57a7
Pavel Stovpivskyi <sby@rambler.ru>
2020-10-05 11:36:33 +0300

    KP-14324 increase bounce period

--- src/Core/SharedKernels/Questionnaire/WB.Core.SharedKernels.Questionnaire/Documents/Constants.cs
+++ src/Core/SharedKernels/Questionnaire/WB.Core.SharedKernels.Questionnaire/Documents/Constants.cs
@@ -9,7 +9,7 @@
         public static readonly int MinLongRosterRowCount = 1;
         public static readonly int MaxLinkedQuestionAnsweredOptionsCount = 200;
         public static readonly string HtmlRemovalPattern = "<.*?>";
-        public static readonly int ThrottlePeriod = 500;
+        public static readonly int ThrottlePeriod = 700;
         public const int MaxRosterPropagationLimit = 10000;
         public const int MaxInterviewsCountByAssignment = 10000;
         public const int MaxTotalRosterPropagationLimit = 80000;
