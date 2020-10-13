1d43ac0e869be8eed0e81985e776ed450f7d8c88
Andrii Kozhyn <hedgic@gmail.com>
2020-10-13 16:18:29 +0300

    KP-14344 device type added to interview resumed event

--- src/Core/SharedKernels/DataCollection/DataCollection/Commands/Interview/OpenInterviewBySupervisorCommand.cs
+++ src/Core/SharedKernels/DataCollection/DataCollection/Commands/Interview/OpenInterviewBySupervisorCommand.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using WB.Core.SharedKernels.DataCollection.Events.Interview;
 
 namespace WB.Core.SharedKernels.DataCollection.Commands.Interview
 {
     public class OpenInterviewBySupervisorCommand : ResumeInterviewCommand
     {
-        public OpenInterviewBySupervisorCommand(Guid interviewId, Guid userId) : base(interviewId, userId)
+        public OpenInterviewBySupervisorCommand(Guid interviewId, Guid userId, AgentDeviceType deviceType) : base(interviewId, userId, deviceType)
         {
         }
     }
