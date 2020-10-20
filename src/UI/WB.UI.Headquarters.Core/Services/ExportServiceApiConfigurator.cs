f1e7e9fe5077b3494aeb146451470f88ec09ed45
Andrii Kozhyn <hedgic@gmail.com>
2020-10-20 13:08:54 +0300

    Scoped inject of export settings

--- src/UI/WB.UI.Headquarters.Core/Services/ExportServiceApiConfigurator.cs
+++ src/UI/WB.UI.Headquarters.Core/Services/ExportServiceApiConfigurator.cs
@@ -2,6 +2,7 @@
 using System.Net.Http;
 using System.Net.Http.Headers;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using WB.Core.BoundedContexts.Headquarters;
 using WB.Core.BoundedContexts.Headquarters.DataExport;
@@ -17,6 +18,7 @@ namespace WB.UI.Headquarters.Services
         private readonly IOptionsSnapshot<ExportServiceConfig> exportOptions;
         private readonly IOptions<HeadquartersConfig> headquarterOptions;
         private readonly IPlainKeyValueStorage<ExportServiceSettings> exportServiceSettings;
+        private ILogger<ExportServiceApiConfigurator> logger;
 
         private readonly IHttpContextAccessor httpContextAccessor;
 
@@ -24,17 +26,21 @@ namespace WB.UI.Headquarters.Services
             IOptionsSnapshot<ExportServiceConfig> exportOptions,
             IOptions<HeadquartersConfig> headquarterOptions,
             IPlainKeyValueStorage<ExportServiceSettings> exportServiceSettings,
-            IHttpContextAccessor httpContextAccessor)
+            IHttpContextAccessor httpContextAccessor,
+            ILogger<ExportServiceApiConfigurator> logger)
         {
             this.exportOptions = exportOptions;
             this.headquarterOptions = headquarterOptions;
             this.exportServiceSettings = exportServiceSettings;
             this.httpContextAccessor = httpContextAccessor;
+            this.logger = logger;
         }
 
         public void ConfigureHttpClient(HttpClient hc)
         {
-            if (!Uri.TryCreate(headquarterOptions.Value.BaseUrl, UriKind.RelativeOrAbsolute, out var baseUrl))
+            var valueBaseUrl = headquarterOptions.Value.BaseUrl;
+            this.logger.LogDebug("Using {baseUrl} as export service url", valueBaseUrl);
+            if (!Uri.TryCreate(valueBaseUrl, UriKind.Absolute, out var baseUrl))
             {
                 var context = httpContextAccessor.HttpContext;
 
@@ -42,7 +48,7 @@ namespace WB.UI.Headquarters.Services
                 {
                     var request = context.Request;
 
-                    baseUrl = new Uri($"{request.Scheme}://{request.Host}{"/"}");
+                    baseUrl = new Uri($"{request.Scheme}://{request.Host}/");
                 }
             }
 
