73be83558bd328f60f7635d7c01d9499575e6a32
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2020-10-26 15:43:29 -0400

    KP-14248 Allow approving of rejected and received interviews. Add warning and checkbox [modal paddings]

--- src/UI/WB.UI.Frontend/src/hqapp/Views/HQ/Interviews/HqInterviews.vue
+++ src/UI/WB.UI.Frontend/src/hqapp/Views/HQ/Interviews/HqInterviews.vue
@@ -226,8 +226,8 @@
                     <p v-html="$t('Interviews.ApproveConfirmMessageHQ', {count: this.getFilteredToApprove().length, status1: 'Completed', status2: 'Approved by Supervisor', status3: 'Rejected by Supervisor'} )"></p>
                 </div>
 
-                <div class="form-group" 
-				    v-if="CountReceivedByInterviewerItems() > 0">
+                <div class="form-group"
+                    v-if="CountReceivedByInterviewerItems() > 0">
                     <br />
                     <input
                         type="checkbox"
