d84ea8a99fb1f71cf66fd400024ff887d512afdc
Andrii Kozhyn <andrii.kozhyn@outlook.com>
2020-11-06 11:22:10 +0200

    KP-14392 ability to get apks from remote server

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Services/IAndroidPackageReader.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Services/IAndroidPackageReader.cs
@@ -1,7 +1,10 @@
+using System.IO;
+
 namespace WB.Core.BoundedContexts.Headquarters.Services
 {
     public interface IAndroidPackageReader
     {
         AndroidPackageInfo Read(string pathToApkFile);
+        AndroidPackageInfo Read(Stream fileStream);
     }
 }
