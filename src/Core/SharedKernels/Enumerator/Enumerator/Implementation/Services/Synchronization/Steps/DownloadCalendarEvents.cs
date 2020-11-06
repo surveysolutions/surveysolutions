edd5ce3adcc58c628eec345b2735ac3aad6b5758
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2020-11-06 00:27:41 -0500

    KP-14381 Calendar Events Sync Interviewer <-> HQ [continue]

---
+++ src/Core/SharedKernels/Enumerator/Enumerator/Implementation/Services/Synchronization/Steps/DownloadCalendarEvents.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Ncqrs.Eventing;
+using WB.Core.GenericSubdomains.Portable.Implementation;
+using WB.Core.GenericSubdomains.Portable.Services;
+using WB.Core.Infrastructure.EventBus.Lite;
+using WB.Core.SharedKernels.DataCollection.WebApi;
+using WB.Core.SharedKernels.Enumerator.Services;
+using WB.Core.SharedKernels.Enumerator.Services.Synchronization;
+
+namespace WB.Core.SharedKernels.Enumerator.Implementation.Services.Synchronization.Steps
+{
+    public class DownloadCalendarEvents : SynchronizationStep
+    {
+        protected readonly IEnumeratorEventStorage EventStore;
+        private readonly ILiteEventBus eventBus;
+        
+        public DownloadCalendarEvents(int sortOrder, ISynchronizationService synchronizationService, 
+            ILogger logger, IEnumeratorEventStorage eventStore, ILiteEventBus eventBus) 
+            : base(sortOrder, synchronizationService, logger)
+        {
+            EventStore = eventStore;
+            this.eventBus = eventBus;
+        }
+
+        public override async Task ExecuteAsync()
+        {
+            List<CalendarEventApiView> remoteCalendarEvents = 
+                await this.synchronizationService.GetCalendarEventsAsync(this.Context.CancellationToken);
+
+            IProgress<TransferProgress> transferProgress = Context.Progress.AsTransferReport();
+            foreach (var calendarEvent in remoteCalendarEvents)
+            {
+                try
+                {
+                    var calendarEventStream = await this.synchronizationService.GetCalendarEventStreamAsync(
+                        calendarEvent.CalendarEventId, transferProgress, Context.CancellationToken);
+                
+                    EventStore.StoreEvents(new CommittedEventStream(calendarEvent.CalendarEventId, calendarEventStream));
+                    eventBus.PublishCommittedEvents(calendarEventStream);
+                }
+                catch (OperationCanceledException)
+                {
+
+                }
+            }
+        }
+    }
+}
---
+++ src/Core/SharedKernels/Enumerator/Enumerator/Implementation/Services/Synchronization/Steps/UploadCalendarEvents.cs
@@ -0,0 +1,63 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using WB.Core.GenericSubdomains.Portable;
+using WB.Core.GenericSubdomains.Portable.Services;
+using WB.Core.SharedKernels.DataCollection.WebApi;
+using WB.Core.SharedKernels.Enumerator.Services;
+using WB.Core.SharedKernels.Enumerator.Services.Infrastructure.Storage;
+using WB.Core.SharedKernels.Enumerator.Services.Synchronization;
+using WB.Core.SharedKernels.Enumerator.Views;
+
+namespace WB.Core.SharedKernels.Enumerator.Implementation.Services.Synchronization.Steps
+{
+    public class UploadCalendarEvents : SynchronizationStep
+    {
+        private readonly IEnumeratorEventStorage eventStorage;
+        private readonly IPlainStorage<InterviewView> interviewViewRepository;
+        private readonly IJsonAllTypesSerializer synchronizationSerializer;
+        
+        public UploadCalendarEvents(int sortOrder, ISynchronizationService synchronizationService, 
+            ILogger logger, IEnumeratorEventStorage eventStorage, 
+            IPlainStorage<InterviewView> interviewViewRepository, 
+            IJsonAllTypesSerializer synchronizationSerializer) 
+            : base(sortOrder, synchronizationService, logger)
+        {
+            this.eventStorage = eventStorage;
+            this.interviewViewRepository = interviewViewRepository;
+            this.synchronizationSerializer = synchronizationSerializer;
+        }
+
+        public override async Task ExecuteAsync()
+        {
+            var allInterviewsWithCalendarEvents = interviewViewRepository
+                .LoadAll()
+                .Where(x => x.CalendarEventId != null)
+                .ToList();
+
+            var transferProgress = Context.Progress.AsTransferReport();
+            
+            foreach (var interview in allInterviewsWithCalendarEvents)
+            {
+                var eventsToSend =  this.eventStorage.Read(interview.CalendarEventId.Value, 0)
+                    .ToReadOnlyCollection();
+                
+                var package = new CalendarEventPackageApiView()
+                {
+                    CalendarEventId = interview.CalendarEventId.Value,
+                    Events = this.synchronizationSerializer.Serialize(eventsToSend),
+                    MetaInfo = new CalendarEventMetaInfo()
+                    {
+                        ResponsibleId   = interview.ResponsibleId,
+                        LastUpdateDateTime = eventsToSend.Last().EventTimeStamp
+                    }
+                };
+
+                await this.synchronizationService.UploadCalendarEventAsync(
+                    interview.CalendarEventId.Value,
+                    package,
+                    transferProgress,
+                    Context.CancellationToken);
+            }
+        }
+    }
+}
