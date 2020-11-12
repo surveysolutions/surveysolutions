1b447accf2e0bcbe20d99536564f81019da4f249
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2020-11-12 00:22:49 -0500

    KP-14381 Calendar Events Sync Interviewer <-> HQ [fix build]

--- src/Core/BoundedContexts/Interviewer/WB.Core.BoundedContexts.Interviewer/Views/Dashboard/DashboardItems/CensusQuestionnaireDashboardItemViewModel.cs
+++ src/Core/BoundedContexts/Interviewer/WB.Core.BoundedContexts.Interviewer/Views/Dashboard/DashboardItems/CensusQuestionnaireDashboardItemViewModel.cs
@@ -78,7 +78,7 @@ namespace WB.Core.BoundedContexts.Interviewer.Views.Dashboard.DashboardItems
         public bool HasExpandedView => false;
         
         public bool IsExpanded { get; set; }
-        public event EventHandler? OnItemUpdated;
+        //public event EventHandler? OnItemUpdated;
 
         private async Task CreateNewInterviewAsync()
         {
