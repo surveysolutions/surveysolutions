9a5d62bd42bf2b82305673ff7f1de27a63e869ba
Victor Gladkikh <me@alfeg.net>
2020-11-12 15:35:50 +0200

    Fix for webtester and designer builds

--- local.docker.build.ps1
+++ local.docker.build.ps1
@@ -1,3 +1,4 @@
+param($app )
 # require docker
 $ENV:COMPOSE_PROJECT_NAME="suso"
 $ENV:DOCKER_BUILDKIT=1
@@ -7,4 +8,11 @@ dotnet gitversion /updateprojectfiles
 dotnet gitversion /showvariable AssemblySemFileVer > .version
 
 docker build -f Dockerfile.js -o build/jsFiles .
-docker-compose build --parallel
+
+
+
+docker-compose build --parallel `
+    --build-arg DESIGNER_JS_PATH=build/jsFiles/questionnaire_app `
+    --build-arg HQ_JS_PATH=build/jsFiles/package/hq `
+    --build-arg WEBTESTER_JS_PATH=build/jsFiles/package/webtester $app
+
