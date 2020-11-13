aded61ce0db5dfce0bab9c3ba165b14360b83529
Andrii Kozhyn <hedgic@gmail.com>
2020-11-13 11:18:17 +0200

    KP-14401 migrating existing schemas into primary

---
+++ src/Infrastructure/WB.Persistence.Headquarters/Migrations/Workspaces/M202011131055_MoveOldSchemasToWorkspace.cs
@@ -0,0 +1,57 @@
+using DocumentFormat.OpenXml.Office.CustomUI;
+using FluentMigrator;
+
+namespace WB.Persistence.Headquarters.Migrations.Workspaces
+{
+    [Migration(2020_11_13_10_55)]
+    public class M202011131055_MoveOldSchemasToWorkspace : Migration
+    {
+        const string primarySchemaName = "primary";
+
+        public override void Up()
+        {
+            Alter.Table("events").InSchema("events").ToSchema(primarySchemaName);
+
+            string[] plainStoreTables =
+            {
+                "appsettings", "assemblyinfos", "assignmentsimportprocess",
+                "assignmenttoimport",
+                "attachmentcontents", "audioauditfiles", "audiofiles", "auditlogrecords",
+                "brokeninterviewpackages", "completedemailrecords", "deviceinfos",
+                "devicesyncinfo", "devicesyncstatistics", "featuredquestions",
+                "hibernate_unique_key", "interviewpackages", "invitations",
+                "mapbrowseitems", "productversionhistory", "profilesettings",
+                "questionnairebackups", "questionnairebrowseitems", "questionnairedocuments",
+                "questionnairelookuptables", "questionnairepdfs", "receivedpackagelogentries",
+                "reusablecategoricaloptions", "synchronizationlog", "systemlog", "tablet_logs",
+                "translationinstances", "usermaps", "usersimportprocess",
+                "usertoimport", "webinterviewconfigs"
+            };
+
+            MoveToPrimarySchema(plainStoreTables, "plainstore");
+
+            string[] readSideTables =
+            {
+                "assignments", "assignmentsidentifyinganswers", "commentaries", "cumulativereportstatuschanges",
+                "identifyingentityvalue", "interview_geo_answers", "interviewcommentedstatuses", "interviewflags",
+                "interviewsummaries", "questionnaire_entities", "report_statistics", "timespanbetweenstatuses"
+            };
+
+
+            var schemaName = "readside";
+            MoveToPrimarySchema(readSideTables, schemaName);
+        }
+
+        private void MoveToPrimarySchema(string[] plainStoreTables, string schemaName)
+        {
+            foreach (var table in plainStoreTables)
+            {
+                Alter.Table(table).InSchema(schemaName).ToSchema(primarySchemaName);
+            }
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
