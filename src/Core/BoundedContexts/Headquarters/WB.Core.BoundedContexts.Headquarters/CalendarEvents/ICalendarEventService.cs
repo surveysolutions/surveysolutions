347b93618930b6172df85bd32ef23fff40b44f3a
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2020-11-17 16:26:56 -0500

    KP-14381 Calendar Events Sync Interviewer <-> HQ [fix assignment sync and comments]

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/CalendarEvents/ICalendarEventService.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/CalendarEvents/ICalendarEventService.cs
@@ -7,8 +7,9 @@ namespace WB.Core.BoundedContexts.Headquarters.CalendarEvents
     public interface ICalendarEventService
     {
         CalendarEvent? GetCalendarEventById(Guid id);
-        CalendarEvent? GetActiveCalendarEventByInterviewId(Guid id);
-
+        CalendarEvent? GetActiveCalendarEventForInterviewId(Guid id);
+        CalendarEvent? GetActiveCalendarEventForAssignmentId(int id);
+        
         List<CalendarEvent> GetAllCalendarEventsForUser(Guid userId);
         List<CalendarEvent> GetAllCalendarEventsUnderSupervisor(Guid supervisorId);
     }
