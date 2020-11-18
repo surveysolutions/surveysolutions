3e08113fa14636e58dd9620efccdfe90188dabe4
Andrii Kozhyn <hedgic@gmail.com>
2020-11-18 18:45:45 +0200

    KP-14403 adding list page for workspaces

---
+++ src/UI/WB.UI.Frontend/src/hqapp/Views/HQ/Workspaces/Workspaces.vue
@@ -0,0 +1,19 @@
+<template>
+    <HqLayout :hasFilter="false"
+        :title="$t('Dashboard.Workspaces')">
+
+        <DataTables
+            ref="table"
+            :tableOptions="tableOptions"
+            noSelect
+            :noPaging="false">
+        </DataTables>
+
+    </HqLayout>
+</template>
+
+<script>
+export default {
+
+}
+</script>
