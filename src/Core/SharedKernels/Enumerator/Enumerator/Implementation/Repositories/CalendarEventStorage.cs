798f7b0bf6798241d31fd7f593dabf229def5c9c
Pavel Stovpivskyi <sby@rambler.ru>
2020-11-18 16:17:51 +0200

    KP-14387 removed order column

--- src/Core/SharedKernels/Enumerator/Enumerator/Implementation/Repositories/CalendarEventStorage.cs
+++ src/Core/SharedKernels/Enumerator/Enumerator/Implementation/Repositories/CalendarEventStorage.cs
@@ -45,7 +45,7 @@ namespace WB.Core.SharedKernels.Enumerator.Implementation.Repositories
             {
                 var calendarEvents = documents.Connection.Table<CalendarEvent>()
                     .Where(a => a.IsSynchronized == false)
-                    .OrderBy(e => e.Order)
+                    //.OrderBy(e => e.LastUpdateDate)
                     .ToList();
 
                 return calendarEvents;
@@ -60,7 +60,7 @@ namespace WB.Core.SharedKernels.Enumerator.Implementation.Repositories
                     .Where(calendarEvent => calendarEvent.UserId == userId
                                         && calendarEvent.IsDeleted != true
                                         && calendarEvent.IsCompleted != true)
-                    .OrderBy(calendarEvent => calendarEvent.Order)
+                    //.OrderBy(calendarEvent => "rowid")
                     .ToList();
 
                 return calendarEvents;
