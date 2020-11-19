89a84d1435fd407a126d9e0f891e862455094f8c
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2020-11-19 13:41:28 -0500

    KP-14383 Calendar Events on HQ [fix CE selection on sync]

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/CalendarEvents/CalendarEventService.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/CalendarEvents/CalendarEventService.cs
@@ -2,7 +2,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using WB.Core.BoundedContexts.Headquarters.Assignments;
 using WB.Core.BoundedContexts.Headquarters.Users;
+using WB.Core.BoundedContexts.Headquarters.Views.Interview;
 using WB.Core.Infrastructure.ReadSide.Repository.Accessors;
 
 namespace WB.Core.BoundedContexts.Headquarters.CalendarEvents
@@ -11,12 +13,17 @@ namespace WB.Core.BoundedContexts.Headquarters.CalendarEvents
     {
         private readonly IQueryableReadSideRepositoryReader<CalendarEvent, Guid> calendarEventsAccessor;
         private readonly IUserRepository userRepository;
+        private readonly IQueryableReadSideRepositoryReader<Assignment, Guid> assignmentsAccessor;
+        private readonly InterviewerInterviewsFactory interviewerInterviewsFactory;
+        
 
         public CalendarEventService(IQueryableReadSideRepositoryReader<CalendarEvent, Guid> calendarEventsAccessor,
-            IUserRepository userRepository)
+            IUserRepository userRepository, IQueryableReadSideRepositoryReader<Assignment, Guid> assignmentsAccessor, InterviewerInterviewsFactory interviewerInterviewsFactory)
         {
             this.calendarEventsAccessor = calendarEventsAccessor ?? throw new ArgumentNullException(nameof(calendarEventsAccessor));
             this.userRepository = userRepository;
+            this.assignmentsAccessor = assignmentsAccessor;
+            this.interviewerInterviewsFactory = interviewerInterviewsFactory;
         }
 
         public CalendarEvent? GetCalendarEventById(Guid id)
@@ -36,35 +43,72 @@ namespace WB.Core.BoundedContexts.Headquarters.CalendarEvents
                 x => x.FirstOrDefault(y => y.InterviewId == null && y.AssignmentId == id));
         }
 
-        public List<CalendarEvent> GetAllCalendarEventsForUser(Guid userId)
+        public List<CalendarEvent> GetAllCalendarEventsForUser(Guid responsibleId)
         {
-            //probably we should remove userId and join with interviews and assignments
+            var userAssignmentsIds = this.assignmentsAccessor.Query(x =>
+                x.Where(assignment =>
+                        assignment.ResponsibleId == responsibleId
+                        && !assignment.Archived
+                        && (assignment.Quantity == null || assignment.InterviewSummaries.Count < assignment.Quantity)
+                        && (assignment.WebMode == null || assignment.WebMode == false))
+                    .Select(x => x.Id)
+                    .ToList());
             
-            var calendarEvents = calendarEventsAccessor.Query(_ => 
+            var calendarEventsByAssignments = calendarEventsAccessor.Query(_ => 
                 _.Where(x =>
                     x.IsDeleted != true 
                     && x.IsCompleted != true
-                    && x.UserId == userId)
+                    && x.InterviewId == null
+                    && userAssignmentsIds.Contains(x.AssignmentId) )
                     .Select(x => x).ToList());
 
-            return calendarEvents;
+            var userInterviewIds 
+                = interviewerInterviewsFactory.GetInProgressInterviewsForInterviewer(responsibleId)
+                    .Select(x=>x.Id as Guid?).ToList();
+            
+            var calendarEventsByInterviews = calendarEventsAccessor.Query(_ => 
+                _.Where(x =>
+                        x.IsDeleted != true 
+                        && x.IsCompleted != true
+                        && x.InterviewId != null
+                        && userInterviewIds.Contains(x.InterviewId) )
+                    .Select(x => x).ToList());
+            
+            return calendarEventsByInterviews.Union(calendarEventsByAssignments).ToList();
         }
 
         public List<CalendarEvent> GetAllCalendarEventsUnderSupervisor(Guid supervisorId)
         {
-            var interviewers = userRepository.Users
-                .Where(u => u.Profile.SupervisorId == supervisorId)
-                .Select(u => u.Id);
-
-            var calendarEvents = calendarEventsAccessor.Query(_ => _
-                .Where(x =>
+            var assignmentdForSupervisorIds =this.assignmentsAccessor.Query(x =>
+                x.Where(assignment =>
+                        (assignment.ResponsibleId == supervisorId || assignment.Responsible.ReadonlyProfile.SupervisorId == supervisorId)
+                        && !assignment.Archived
+                        && (assignment.Quantity == null || assignment.InterviewSummaries.Count < assignment.Quantity)
+                        && (assignment.WebMode == null || assignment.WebMode == false))
+                    .Select(x=>x.Id)
+                    .ToList());
+            
+            var calendarEventsByAssignments = calendarEventsAccessor.Query(_ => 
+                _.Where(x =>
                         x.IsDeleted != true 
                         && x.IsCompleted != true
-                        && interviewers.Contains(x.UserId))
-                .Select(x => x)
-                .ToList());
+                        && x.InterviewId == null
+                        && assignmentdForSupervisorIds.Contains(x.AssignmentId) )
+                    .Select(x => x).ToList());
 
-            return calendarEvents;
+            var teamInterviewIds =
+                interviewerInterviewsFactory.GetInProgressInterviewsForSupervisor(supervisorId)
+                    .Select(x=>x.Id as Guid?).ToList();
+            
+            var calendarEventsByInterviews = calendarEventsAccessor.Query(_ => 
+                _.Where(x =>
+                        x.IsDeleted != true 
+                        && x.IsCompleted != true
+                        && x.InterviewId != null
+                        && teamInterviewIds.Contains(x.InterviewId) )
+                    .Select(x => x).ToList());
+
+            return calendarEventsByInterviews.Union(calendarEventsByAssignments).ToList();
         }
     }
 }
