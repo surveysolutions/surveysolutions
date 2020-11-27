28188ccf2beff9ffcd81140c960a47dcab8a3176
Victor Hladkikh <me@alfeg.net>
2020-11-27 23:23:39 +0200

    KP-14400 Fixed URL generation

--- src/UI/WB.UI.Headquarters.Core/AppConfiguration.cs
+++ src/UI/WB.UI.Headquarters.Core/AppConfiguration.cs
@@ -1,6 +1,7 @@
 using System.IO;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using reCAPTCHA.AspNetCore;
 using WB.Core.BoundedContexts.Headquarters;
 using WB.Core.BoundedContexts.Headquarters.Configs;
@@ -8,6 +9,7 @@ using WB.Core.BoundedContexts.Headquarters.Maps;
 using WB.Core.BoundedContexts.Headquarters.Views.InterviewHistory;
 using WB.Core.SharedKernels.DataCollection;
 using WB.Infrastructure.Native;
+using WB.Infrastructure.Native.Workspaces;
 using WB.UI.Headquarters.Configs;
 using WB.UI.Shared.Web.Captcha;
 using WB.UI.Shared.Web.Services;
@@ -20,13 +22,23 @@ namespace WB.UI.Headquarters
 
         public static void AddOptionsConfiguration(this IServiceCollection services, IConfiguration configuration)
         {
+            services.AddTransient<IOptions<HeadquartersConfig>>(sp =>
+            {
+                var workspace = sp.GetRequiredService<IWorkspaceContextAccessor>();
+                var config = configuration.HeadquarterOptions().Get<HeadquartersConfig>();
+
+                config.BaseUrl = (config.BaseUrl + "/" + workspace.CurrentWorkspace()?.Name ?? string.Empty).TrimEnd('/');
+
+                return Options.Create(config);
+            });
+
             // configuration
             services.Configure<ApkConfig>(configuration.GetSection("Apks"));
             services.Configure<CaptchaConfig>(configuration.CaptchaOptionsSection());
             services.Configure<ExportServiceConfig>(configuration.GetSection("DataExport"));
             services.Configure<DesignerConfig>(configuration.GetSection("Designer"));
             services.Configure<GoogleMapsConfig>(configuration.GetSection("GoogleMap"));
-            services.Configure<HeadquartersConfig>(configuration.HeadquarterOptions());
+            
             services.Configure<PreloadingConfig>(configuration.GetSection("PreLoading"));
             services.Configure<RecaptchaSettings>(configuration.CaptchaOptionsSection());
             services.Configure<SchedulerConfig>(configuration.GetSection("Scheduler"));
