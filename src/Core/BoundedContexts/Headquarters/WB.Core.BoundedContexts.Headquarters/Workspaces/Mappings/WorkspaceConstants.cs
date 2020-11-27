ff5af37ed24069065245478b6bef4572086f9039
Andrii Kozhyn <hedgic@gmail.com>
2020-11-27 13:24:42 +0200

    KP-14405 redirect user into last visited workspace

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Workspaces/Mappings/WorkspaceConstants.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Workspaces/Mappings/WorkspaceConstants.cs
@@ -3,6 +3,7 @@ namespace WB.Core.BoundedContexts.Headquarters.Workspaces.Mappings
     public static class WorkspaceConstants
     {
         public static string SchemaName => "workspaces";
+        public const string ClaimType = "Workspace";
         public const string DefaultWorkspaceName = "primary";
     }
 }
---
+++ src/UI/WB.UI.Headquarters.Core/Code/Workspaces/WorkspaceInfoFilter.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using WB.Core.BoundedContexts.Headquarters.Workspaces;
+using WB.Core.BoundedContexts.Headquarters.Workspaces.Mappings;
+using WB.Infrastructure.Native.Workspaces;
+
+namespace WB.UI.Headquarters.Code.Workspaces
+{
+    public class WorkspaceInfoFilter : IResultFilter
+    {
+        private readonly IWorkspacesCache workspacesService;
+        private readonly IWorkspaceContextAccessor workspaceContextAccessor;
+
+        public const string CookieName = "currentWorkspace";
+        
+        public WorkspaceInfoFilter(IWorkspacesService workspacesService,
+            IWorkspaceContextAccessor workspaceContextAccessor)
+        {
+            this.workspacesService = workspacesService;
+            this.workspaceContextAccessor = workspaceContextAccessor;
+        }
+
+        public void OnResultExecuted(ResultExecutedContext context)
+        {
+        }
+
+        public void OnResultExecuting(ResultExecutingContext context)
+        {
+            if (context.HttpContext.User.Identity.IsAuthenticated && context.Result is ViewResult view)
+            {
+                var claims = context.HttpContext.User.Claims.Where(c => c.Type == WorkspaceConstants.ClaimType).Select(c => c.Value).ToHashSet();
+                var workspaces = this.workspacesService.GetWorkspaces();
+                var userWorkspaces = workspaces.Where(w => claims.Contains(w.Name));
+                view.ViewData["UserWorkspacesList"] = userWorkspaces;
+
+                context.HttpContext.Response.Cookies.Append(CookieName,
+                    workspaceContextAccessor.CurrentWorkspace().Name,
+                    new CookieOptions
+                    {
+                        Expires = DateTimeOffset.Now.AddDays(7)
+                    });
+            }
+
+        }
+    }
+}
