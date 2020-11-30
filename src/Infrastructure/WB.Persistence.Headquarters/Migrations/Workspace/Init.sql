af3946e153025d3131d82aea99c0eff2fc37a87c
Victor Gladkikh <me@alfeg.net>
2020-11-30 19:15:38 +0200

    KP-14400 Added missing aggregate functions for statistics report

--- src/Infrastructure/WB.Persistence.Headquarters/Migrations/Workspace/Init.sql
+++ src/Infrastructure/WB.Persistence.Headquarters/Migrations/Workspace/Init.sql
@@ -2138,3 +2138,23 @@ ALTER TABLE ONLY usermaps
 -- PostgreSQL database dump complete
 --
 
+CREATE OR REPLACE FUNCTION _final_median(numeric[])
+   RETURNS numeric AS
+$$
+   SELECT AVG(val)
+   FROM (
+     SELECT val
+     FROM unnest($1) val
+     ORDER BY 1
+     LIMIT  2 - MOD(array_upper($1, 1), 2)
+     OFFSET CEIL(array_upper($1, 1) / 2.0) - 1
+   ) sub;
+$$
+LANGUAGE 'sql' IMMUTABLE;
+
+CREATE AGGREGATE median(numeric) (
+  SFUNC=array_append,
+  STYPE=numeric[],
+  FINALFUNC=_final_median,
+  INITCOND='{}'
+);
