49cd72e9e8aed2cc8d193352a138bcec0a2fd2ec
Andrii Kozhyn <hedgic@gmail.com>
2020-12-03 17:45:38 +0200

    added missing table

--- src/Infrastructure/WB.Persistence.Headquarters/Migrations/Workspace/M202012031344_CreateMissingKeyValueTables.cs
+++ src/Infrastructure/WB.Persistence.Headquarters/Migrations/Workspace/M202012031344_CreateMissingKeyValueTables.cs
@@ -14,6 +14,7 @@ namespace WB.Persistence.Headquarters.Migrations.Workspace
                 "webinterviewconfigs",
                 "questionnairepdfs",
                 "profilesettings",
+                "appsettings",
                 "questionnairelookuptables"
             };
 
