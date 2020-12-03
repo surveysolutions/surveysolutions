fe36748cb9d3ef1b37553e970c4b3379c42bf737
Pavel Stovpivskyi <sby@rambler.ru>
2020-12-03 19:00:01 +0200

    KP-14382 support nullable ZonedDateTime in converter

--- src/UI/Shared/WB.UI.Shared.Enumerator/Converters/CalendarEventAndStatusToColorConverter.cs
+++ src/UI/Shared/WB.UI.Shared.Enumerator/Converters/CalendarEventAndStatusToColorConverter.cs
@@ -7,12 +7,15 @@ using WB.Core.SharedKernels.Enumerator.ViewModels.Dashboard;
 
 namespace WB.UI.Shared.Enumerator.Converters
 {
-    public class CalendarEventToColorConverter: MvxValueConverter<ZonedDateTime, int>
+    public class CalendarEventToColorConverter: MvxValueConverter<ZonedDateTime?, int>
     {
-        protected override int Convert(ZonedDateTime calendarEventStart, Type targetType, object parameter, CultureInfo culture)
+        protected override int Convert(ZonedDateTime? calendarEventStart, Type targetType, object parameter, CultureInfo culture)
         {
+            if (!calendarEventStart.HasValue)
+                return Resource.Color.dashboard_synchronization_regular_title;
+            
             var utc = DateTime.UtcNow;
-            return utc > calendarEventStart.ToDateTimeUtc()
+            return utc > calendarEventStart.Value.ToDateTimeUtc()
                 ? Resource.Color.dashboard_rejected_tab
                 : Resource.Color.dashboard_synchronization_regular_title;
         }
