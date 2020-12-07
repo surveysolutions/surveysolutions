a0f1742f08ff6a47a9d261e4040bfc5ad12389f0
Andrii Kozhyn <andrii.kozhyn@outlook.com>
2020-12-07 12:23:54 +0200

    KP-14403 moved workspaces management to admin menu

--- src/UI/WB.UI.Headquarters.Core/Controllers/WorkspacesController.cs
+++ src/UI/WB.UI.Headquarters.Core/Controllers/WorkspacesController.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using WB.Core.SharedKernels.SurveyManagement.Web.Models;
+using WB.UI.Headquarters.Filters;
 using WB.UI.Headquarters.Models.Workspaces;
 
 namespace WB.UI.Headquarters.Controllers
@@ -7,6 +9,7 @@ namespace WB.UI.Headquarters.Controllers
     [Authorize(Roles = "Administrator")]
     public class WorkspacesController : Controller
     {
+        [ActivePage(MenuItem.Workspaces)]
         public IActionResult Index() => View(new WorkspacesModel
         {
             DataUrl = Url.Action("Index", "WorkspacesPublicApi")
