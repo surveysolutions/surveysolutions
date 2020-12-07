c8052c1d46e0da5fe6c38afe5ae76204c67b762b
Victor Hladkikh <me@alfeg.net>
2020-12-07 17:39:41 +0200

    Fixed workspace graphql middleware resolver

--- src/UI/WB.UI.Headquarters.Core/Controllers/Api/PublicApi/Graphql/WorkspaceGraphQlMiddleware.cs
+++ src/UI/WB.UI.Headquarters.Core/Controllers/Api/PublicApi/Graphql/WorkspaceGraphQlMiddleware.cs
@@ -1,5 +1,7 @@
 #nullable enable
+using System.Linq;
 using System.Threading.Tasks;
+using HotChocolate.Language;
 using HotChocolate.Resolvers;
 using WB.Infrastructure.Native.Workspaces;
 
@@ -16,8 +18,10 @@ namespace WB.UI.Headquarters.Controllers.Api.PublicApi.Graphql
 
         public async Task InvokeAsync(IMiddlewareContext context, IWorkspaceContextSetter workspaceContextSetter)
         {
-            var workspace = context.Argument<string>("workspace") ?? WorkspaceConstants.DefaultWorkspaceName;
-             workspaceContextSetter.Set(workspace);
+            var workspace = context.Argument<string>("workspace")
+                ?? context.Variables.GetVariable<string>("workspace");
+
+            workspaceContextSetter.Set(workspace);
             await next(context);
         }
     }
