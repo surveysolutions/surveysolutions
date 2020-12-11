dc0464ebee8a04842491f173f8e7249433ce94bf
Victor Hladkikh <me@alfeg.net>
2020-12-11 13:48:59 +0200

    Fixed questionnaire import to support workspaces

--- src/UI/WB.UI.Headquarters.Core/Startup.cs
+++ src/UI/WB.UI.Headquarters.Core/Startup.cs
@@ -32,6 +32,7 @@ using WB.Core.BoundedContexts.Headquarters.DataExport;
 using WB.Core.BoundedContexts.Headquarters.DataExport.Views;
 using WB.Core.BoundedContexts.Headquarters.Designer;
 using WB.Core.BoundedContexts.Headquarters.EmailProviders;
+using WB.Core.BoundedContexts.Headquarters.Implementation;
 using WB.Core.BoundedContexts.Headquarters.Implementation.Services;
 using WB.Core.BoundedContexts.Headquarters.Implementation.Synchronization;
 using WB.Core.BoundedContexts.Headquarters.Storage;
