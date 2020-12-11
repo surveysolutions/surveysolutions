136fd6292e0f0459e604ea1600e39463a31ab4ee
Andrii Kozhyn <andrii.kozhyn@outlook.com>
2020-12-11 14:42:52 +0200

    Moved stata lib into project

---
+++ src/Services/Export/Utils/StatData/TestProject/StringExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+namespace TestProject
+{
+    public static class StringExtensions
+    {
+        public static string Repeat(this char chatToRepeat, int repeat)
+        {
+            return new string(chatToRepeat, repeat);
+        }
+        public static string Repeat(this string stringToRepeat, int repeat)
+        {
+            var builder = new StringBuilder(repeat * stringToRepeat.Length);
+            for (int i = 0; i < repeat; i++)
+            {
+                builder.Append(stringToRepeat);
+            }
+            return builder.ToString();
+        }
+    }
+}
