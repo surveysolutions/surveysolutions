8e16db3cf25b74eda962857da15edccf8fa2cd89
Victor Gladkikh <me@alfeg.net>
2020-12-11 11:57:30 +0200

    Redirect user to last seen workspace

--- src/UI/WB.UI.Headquarters.Core/Code/Workspaces/WorkspaceInfoFilter.cs
+++ src/UI/WB.UI.Headquarters.Core/Code/Workspaces/WorkspaceInfoFilter.cs
@@ -1,8 +1,8 @@
 using System;
+using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
-using WB.Core.BoundedContexts.Headquarters.Workspaces;
 using WB.Infrastructure.Native.Workspaces;
 using WB.UI.Shared.Web.Controllers;
 
@@ -12,14 +12,17 @@ namespace WB.UI.Headquarters.Code.Workspaces
     {
         private readonly IWorkspacesCache workspacesService;
         private readonly IWorkspaceContextAccessor workspaceContextAccessor;
+        private readonly IDataProtector dataProtector;
 
         public const string CookieName = "currentWorkspace";
         
         public WorkspaceInfoFilter(IWorkspacesCache workspacesService,
-            IWorkspaceContextAccessor workspaceContextAccessor)
+            IWorkspaceContextAccessor workspaceContextAccessor,
+            IDataProtectionProvider protectionProvider)
         {
             this.workspacesService = workspacesService;
             this.workspaceContextAccessor = workspaceContextAccessor;
+            this.dataProtector = protectionProvider.CreateProtector("ws_cookie");
         }
 
         public void OnResultExecuted(ResultExecutedContext context)
@@ -38,8 +41,10 @@ namespace WB.UI.Headquarters.Code.Workspaces
 
                 if (currentWorkspace != null)
                 {
+                    var payload = dataProtector.Protect(currentWorkspace.Name);
+
                     context.HttpContext.Response.Cookies.Append(CookieName,
-                        currentWorkspace.Name,
+                        payload,
                         new CookieOptions
                         {
                             Expires = DateTimeOffset.Now.AddDays(7)
