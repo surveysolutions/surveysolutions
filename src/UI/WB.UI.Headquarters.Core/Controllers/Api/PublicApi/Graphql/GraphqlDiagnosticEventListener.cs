442070285342fc684058fccaa41d84c8dc4b60f1
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2020-12-17 18:59:47 -0500

    KP-14420 Upgrade graphql library [add diagnostics]

---
+++ src/UI/WB.UI.Headquarters.Core/Controllers/Api/PublicApi/Graphql/GraphqlDiagnosticEventListener.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using HotChocolate;
+using HotChocolate.Execution;
+using HotChocolate.Execution.Instrumentation;
+using HotChocolate.Resolvers;
+using Microsoft.Extensions.Logging;
+
+namespace WB.UI.Headquarters.Controllers.Api.PublicApi.Graphql
+{
+    public class GraphqlDiagnosticEventListener : DiagnosticEventListener
+    {
+        private readonly ILogger logger;
+        public GraphqlDiagnosticEventListener(ILogger<GraphqlDiagnosticEventListener> logger)
+        {
+            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public override bool EnableResolveFieldValue => true;
+        
+        public override void ResolverError(IMiddlewareContext context, IError error)
+        {
+            LogError(error);
+        }
+        
+        public override void SyntaxError(IRequestContext context, IError error)
+        {
+            LogError(error);
+        }
+        
+        public override void TaskError(IExecutionTask task, IError error)
+        {
+            LogError(error);
+        }
+        public override void RequestError(IRequestContext context, Exception exception)
+        {
+            logger.LogError(exception, $"Request Error: {exception.Message}");
+        }
+
+        private void LogError(IError error)
+        {
+            if (error.Code != null)
+            {
+                logger.LogInformation("{0} {1} {2}", error.Path, error.Code, error.Message);
+            }
+
+            else
+            {
+                if (error.Exception == null)
+                {
+                    logger.LogError("{0}{2}{1}", error.Path, error.Message, Environment.NewLine);
+                }
+                else
+                {
+                    logger.LogError(error.Exception,
+                        "{0}{2}{1}", error.Path, error.Message, Environment.NewLine);
+                }
+            }
+        }
+    }
+}
