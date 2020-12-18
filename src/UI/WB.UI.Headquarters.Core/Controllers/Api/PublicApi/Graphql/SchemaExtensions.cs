5e9b7df72f27889cc4f6973d4eb2d4b29242f50c
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2020-12-18 00:00:03 -0500

    KP-14420 Upgrade graphql library [separate types]

---
+++ src/UI/WB.UI.Headquarters.Core/Controllers/Api/PublicApi/Graphql/SchemaExtensions.cs
@@ -0,0 +1,15 @@
+﻿using HotChocolate.Types;
+using WB.Infrastructure.Native.Workspaces;
+
+namespace WB.UI.Headquarters.Controllers.Api.PublicApi.Graphql
+{
+    public static class SchemaExtensions
+    {
+        public static IObjectFieldDescriptor HasWorkspace(this IObjectFieldDescriptor descriptor)
+        {
+            return descriptor.Argument("workspace",
+                a => a.Description("Workspace name").Type<NonNullType<StringType>>()
+                    .DefaultValue(WorkspaceConstants.DefaultWorkspaceName));
+        }
+    }
+}
