338a563bf9bc4b19f525c635e7aa8c2c740e68de
Victor Hladkikh <me@alfeg.net>
2020-12-28 19:13:53 +0200

    KP-14497 UsersImportJob migrated
    # Conflicts:
    #	src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Users/UserPreloading/Services/UserImportService.cs

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Users/UserPreloading/Jobs/UsersImportJob.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Users/UserPreloading/Jobs/UsersImportJob.cs
@@ -1,38 +1,33 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Identity;
+using MediatR;
 using Microsoft.Extensions.Logging;
 using Quartz;
-using WB.Core.BoundedContexts.Headquarters.Services;
+using WB.Core.BoundedContexts.Headquarters.QuartzIntegration;
 using WB.Core.BoundedContexts.Headquarters.Users.UserPreloading.Dto;
 using WB.Core.BoundedContexts.Headquarters.Users.UserPreloading.Services;
-using WB.Core.BoundedContexts.Headquarters.Views.User;
-using WB.Core.GenericSubdomains.Portable.ServiceLocation;
 using WB.Core.Infrastructure.Domain;
 
 namespace WB.Core.BoundedContexts.Headquarters.Users.UserPreloading.Jobs
 {
     [DisallowConcurrentExecution]
-    internal class UsersImportJob : IJob
+    [RetryFailedJob]
+    [DisplayName("Users import"), Category("Import")]
+    public class UsersImportJob : IJob<Unit>
     {
-        private readonly IInScopeExecutor inScopeExecutor;
+        private readonly IInScopeExecutor<IMediator> inScopeExecutor;
         private readonly ILogger<UsersImportJob> logger;
-        private readonly IUserImportService userImportService;
-        private readonly ISystemLog systemLog;
 
-        public UsersImportJob(IInScopeExecutor inScopeExecutor, 
-            ILogger<UsersImportJob> logger, 
-            IUserImportService userImportService,
-            ISystemLog systemLog)
+        public UsersImportJob(IInScopeExecutor<IMediator> inScopeExecutor,
+            ILogger<UsersImportJob> logger)
         {
             this.inScopeExecutor = inScopeExecutor;
             this.logger = logger;
-            this.userImportService = userImportService;
-            this.systemLog = systemLog;
         }
 
-        public async Task Execute(IJobExecutionContext context)
+        public async Task Execute(Unit @void, IJobExecutionContext context)
         {
             var sw = new Stopwatch();
             sw.Start();
@@ -47,73 +42,17 @@ namespace WB.Core.BoundedContexts.Headquarters.Users.UserPreloading.Jobs
         {
             try
             {
-                var userToImport = userImportService.GetUserToImport();
-                if (userToImport == null) return;
+                UserToImport userToImport = null;
 
                 do
                 {
-                    await this.inScopeExecutor.ExecuteAsync(async (serviceLocator) =>
-                    {
-                        await CreateUserOrUnarchiveAndUpdateAsync(userToImport, serviceLocator);
-
-                        userImportService.RemoveImportedUser(userToImport);
-
-                        userToImport = userImportService.GetUserToImport();
-                    });
+                    userToImport = await this.inScopeExecutor.ExecuteAsync(
+                        mediator => mediator.Send(new CreateOrUnArchiveUserRequest()));
                 } while (userToImport != null);
-
-                var completeStatus = userImportService.GetImportCompleteStatus();
-                var status = userImportService.GetImportStatus();
-
-                this.systemLog.UsersImported(completeStatus.SupervisorsCount, completeStatus.InterviewersCount, status.Responsible);
             }
             catch (Exception ex)
             {
                 logger.LogError(ex, "User import job: FAILED");
-            }
-        }
-
-        private static async Task CreateUserOrUnarchiveAndUpdateAsync(UserToImport userToCreate, IServiceLocator serviceLocator)
-        {
-            var userManager = serviceLocator.GetInstance<UserManager<HqUser>>();
-            var userRepository = serviceLocator.GetInstance<IUserRepository>();
-            var user = await userManager.FindByNameAsync(userToCreate.Login);
-
-            if (user == null)
-            {
-                Guid? supervisorId = null;
-
-                if (!string.IsNullOrEmpty(userToCreate.Supervisor))
-                    supervisorId = (await userManager.FindByNameAsync(userToCreate.Supervisor))?.Id;
-
-                var role = userRepository.FindRole(userToCreate.UserRole.ToUserId());
-                var hqUser = new HqUser
-                {
-                    Id = Guid.NewGuid(),
-                    UserName = userToCreate.Login,
-                    FullName = userToCreate.FullName,
-                    Email = userToCreate.Email,
-                    PhoneNumber = userToCreate.PhoneNumber,
-                    Profile = supervisorId.HasValue
-                        ? new HqUserProfile
-                        {
-                            SupervisorId = supervisorId
-                        }
-                        : null,
-                };
-                hqUser.Roles.Add(role);
-                await userManager.CreateAsync(hqUser, userToCreate.Password);
-            }
-            else
-            {
-                user.FullName = userToCreate.FullName;
-                user.Email = userToCreate.Email;
-                user.PhoneNumber = userToCreate.PhoneNumber;
-                user.IsArchived = false;
-
-                await userManager.UpdateAsync(user);
-                string passwordResetToken = await userManager.GeneratePasswordResetTokenAsync(user);
-                await userManager.ResetPasswordAsync(user, passwordResetToken, userToCreate.Password);
             }
         }
     }
