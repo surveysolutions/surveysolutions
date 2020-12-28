16b7df9a417a7d3324ea045fce369dfb54192da3
Victor Gladkikh <me@alfeg.net>
2020-12-28 14:37:17 +0200

    Added scoped to workspace IHttpClientFactory for workspace aware IExportServiceApi

--- src/UI/WB.UI.Headquarters.Core/Code/Workspaces/WorkspaceExtension.cs
+++ src/UI/WB.UI.Headquarters.Core/Code/Workspaces/WorkspaceExtension.cs
@@ -1,9 +1,13 @@
 #nullable enable
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using Microsoft.AspNetCore.Builder;
-using WB.Core.BoundedContexts.Headquarters.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Refit;
+using WB.Infrastructure.AspNetCore;
 using WB.Infrastructure.Native.Workspaces;
 
 namespace WB.UI.Headquarters.Code.Workspaces
@@ -18,6 +22,37 @@ namespace WB.UI.Headquarters.Code.Workspaces
         public static void UseRedirectIntoWorkspace(this IApplicationBuilder app)
         {
             app.UseMiddleware<WorkspaceRedirectMiddleware>();
+        }
+
+        private static readonly ConcurrentDictionary<string, IServiceProvider> providersCache = new();
+
+        public static void AddWorkspaceAwareHttpClient<TApi, TConfigurator>(this IServiceCollection services, 
+            RefitSettings? settings = null)
+            where TApi: class
+            where TConfigurator : class, IHttpClientConfigurator<TApi>
+        {
+            services.AddTransient<IHttpClientConfigurator<TApi>, TConfigurator>();
+
+            services.AddTransient<TApi>(provider =>
+            {
+                var workspace = provider.GetWorkspaceContext();
+                var workspaceName = workspace?.Name ?? WorkspaceConstants.DefaultWorkspaceName;
+                var configurator = provider.GetRequiredService<IHttpClientConfigurator<TApi>>();
+                var httpClient = providersCache.GetOrAdd(workspaceName, name =>
+                {
+                    var collection = new ServiceCollection();
+                    collection.AddRefitClient<TApi>(settings)
+                        .ConfigureHttpClient((sp, hc) =>
+                        {
+                            configurator.ConfigureHttpClient(hc);
+                        }).AddTransientHttpErrorsHandling();
+
+                    return collection.BuildServiceProvider();
+                });
+
+                return httpClient.GetRequiredService<TApi>();
+            });
+
         }
     }
 }
