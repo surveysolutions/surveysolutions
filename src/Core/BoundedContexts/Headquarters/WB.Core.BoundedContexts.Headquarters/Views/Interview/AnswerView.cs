93f6a3064fb66dadecc57a773ae17ae52f36b3d7
Pavel Stovpivskyi <sby@rambler.ru>
2020-12-29 16:49:49 +0200

    KP-14472 added identity questions on tooltip

---
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Views/Interview/AnswerView.cs
@@ -0,0 +1,8 @@
+namespace WB.Core.BoundedContexts.Headquarters.Views.Interview
+{
+    public class AnswerView
+    {
+        public string Title { get; set; }
+        public string Answer { get; set; }
+    }
+}
