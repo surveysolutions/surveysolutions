61feeebb81e01064fe5d533b23ae087097a0216b
Victor Gladkikh <me@alfeg.net>
2021-01-12 14:38:03 +0200

    KP-14472 Small code fixes

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Views/Interview/AnswerView.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Views/Interview/AnswerView.cs
@@ -2,7 +2,7 @@ namespace WB.Core.BoundedContexts.Headquarters.Views.Interview
 {
     public class AnswerView
     {
-        public string Title { get; set; }
-        public string Answer { get; set; }
+        public string Title { get; init; }
+        public string Answer { get; init; }
     }
 }
