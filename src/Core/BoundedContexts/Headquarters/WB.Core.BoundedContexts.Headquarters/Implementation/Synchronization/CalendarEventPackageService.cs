3aabfe67432e497f51bac650ff26968f54d5b3b3
Vitalii Balabanov <vitaliy.balabanov@gmail.com>
2021-01-13 21:40:04 -0500

    fix api

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Implementation/Synchronization/CalendarEventPackageService.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Implementation/Synchronization/CalendarEventPackageService.cs
@@ -70,6 +70,12 @@ namespace WB.Core.BoundedContexts.Headquarters.Implementation.Synchronization
                     bool shouldIgnorePackage = true;
                     var currentUser = userViewFactory.GetUser(currentResponsibleId.Value);
                     var packageUser = userViewFactory.GetUser(responsibleId);
+
+                    if (packageUser == null || currentUser == null)
+                    {
+                        throw new InvalidOperationException("User was not found");
+                    }
+
                     if (packageUser.IsSupervisor() 
                         && currentUser.IsInterviewer() 
                         && currentUser.Supervisor.Id == packageUser.PublicKey)
