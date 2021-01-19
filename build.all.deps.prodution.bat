a4378bd65c7bb834439b9d9e19502e6453de8a45
Victor Hladkikh <me@alfeg.net>
2021-01-19 08:53:26 +0200

    Android Http server build fix

--- build.all.deps.prodution.bat
+++
@@ -1,3 +0,0 @@
-start cmd /c "TITLE Designer Questionnaire && cd src\UI\WB.UI.Designer && yarn && yarn gulp --production"
-call cmd  /c "TITLE hq deps && cd src\UI\WB.UI.Frontend && yarn && yarn build"
-
