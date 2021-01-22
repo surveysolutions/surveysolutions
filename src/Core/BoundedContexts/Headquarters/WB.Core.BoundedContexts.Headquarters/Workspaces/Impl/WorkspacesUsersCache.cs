c7ae100ea686eff024c86948092325c11489d1ed
Victor Hladkikh <me@alfeg.net>
2021-01-22 16:16:36 +0200

    KP-14549 Fix for graphql execution

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Workspaces/Impl/WorkspacesUsersCache.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Workspaces/Impl/WorkspacesUsersCache.cs
@@ -7,17 +7,18 @@ using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Options;
 using WB.Core.BoundedContexts.Headquarters.Users;
 using WB.Core.GenericSubdomains.Portable;
+using WB.Core.Infrastructure.Domain;
 
 namespace WB.Core.BoundedContexts.Headquarters.Workspaces.Impl
 {
     class WorkspacesUsersCache : IWorkspacesUsersCache
     {
-        private readonly IUserRepository usersRepository;
+        private readonly IInScopeExecutor<IUserRepository> usersRepository;
 
         // Global workspace independent cache
         private readonly IMemoryCache memoryCache = new MemoryCache(Options.Create(new MemoryCacheOptions()));
 
-        public WorkspacesUsersCache(IUserRepository usersRepository)
+        public WorkspacesUsersCache(IInScopeExecutor<IUserRepository> usersRepository)
         {
             this.usersRepository = usersRepository;
         }
@@ -31,10 +32,13 @@ namespace WB.Core.BoundedContexts.Headquarters.Workspaces.Impl
         {
             return await this.memoryCache.GetOrCreateAsync(userId.FormatGuid(), async entry =>
             {
-                entry.AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(1);
+                entry.AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(5);
 
-                var user = await this.usersRepository.FindByIdAsync(userId, token);
-                return user.Workspaces.Select(w => w.Workspace.Name).OrderBy(o => o).ToList();
+                return await this.usersRepository.ExecuteAsync(async u =>
+                {
+                    var user = await u.FindByIdAsync(userId, token);
+                    return user.Workspaces.Select(w => w.Workspace.Name).OrderBy(o => o).ToList();
+                });
             });
         }
     }
