adfc30527a28cabf68afebdd9a180e49ca7717bd
Victor Hladkikh <me@alfeg.net>
2021-01-27 11:29:26 +0200

    KP-14556 Large amount of lookup tables slow compilation

--- src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Implementation/Services/CodeGeneration/Model/LookupTableTemplateModel.cs
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Implementation/Services/CodeGeneration/Model/LookupTableTemplateModel.cs
@@ -1,3 +1,6 @@
+using System.Globalization;
+using System.Linq;
+using System.Text;
 using WB.Core.BoundedContexts.Designer.Implementation.Services.LookupTableService;
 
 namespace WB.Core.BoundedContexts.Designer.Implementation.Services.CodeGeneration.Model
@@ -18,5 +21,19 @@ namespace WB.Core.BoundedContexts.Designer.Implementation.Services.CodeGeneratio
         public string TableNameField { set; get; }
         public string[] VariableNames { get; set; }
         public LookupTableRow[] Rows { get; set; }
+
+        public string RenderLookupRowsData()
+        {
+            var sb = new StringBuilder();
+
+            foreach (var row in Rows)
+            {
+                var variables = row.Variables.Select(v => v == null ? "null" : v.Value.ToString(CultureInfo.InvariantCulture));
+                var rowData = $"{row.RowCode}\t{string.Join("\t", variables)}";
+                sb.AppendJoin('\n', rowData);
+            }
+
+            return sb.ToString();
+        }
     }
 }
