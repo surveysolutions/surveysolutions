af83f68af5d58c2c41cbab878d749ada0d5f8994
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2021-02-01 13:39:56 -0500

    logging for graphql

--- src/UI/WB.UI.Headquarters.Core/Controllers/Api/PublicApi/Graphql/GraphQLErrorFilter.cs
+++ src/UI/WB.UI.Headquarters.Core/Controllers/Api/PublicApi/Graphql/GraphQLErrorFilter.cs
@@ -1,14 +1,33 @@
+using System;
 using HotChocolate;
+using Microsoft.Extensions.Logging;
 
 namespace WB.UI.Headquarters.Controllers.Api.PublicApi.Graphql
 {
     public class GraphQLErrorFilter : IErrorFilter
     {
+        private readonly ILogger logger;
+
+        public GraphQLErrorFilter(ILogger<GraphQLErrorFilter> logger)
+        {
+            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
         public IError OnError(IError error)
         {
             if (!string.IsNullOrEmpty(error.Code))
             {
                 return error;
+            }
+            
+            if (error.Exception == null)
+            {
+                logger.LogError("{0}{2}{1}", error.Path, error.Message, Environment.NewLine);
+            }
+            else
+            {
+                logger.LogError(error.Exception,
+                    "{0}{2}{1}", error.Path, error.Message, Environment.NewLine);
             }
             
             return ErrorBuilder.New()
