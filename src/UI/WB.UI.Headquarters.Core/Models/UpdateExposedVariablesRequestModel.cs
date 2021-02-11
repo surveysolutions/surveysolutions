0fd13dcf7d108b30a0f90da6dbf1416c9a5574db
VItaliy Balabanov <4949691+V-B@users.noreply.github.com>
2021-02-11 00:04:05 -0500

    KP-14591 Allow to add questions to the filter

---
+++ src/UI/WB.UI.Headquarters.Core/Models/UpdateExposedVariablesRequestModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace WB.Core.SharedKernels.SurveyManagement.Web.Models
+{
+    public class UpdateExposedVariablesRequestModel
+    {
+        public Guid QuestionnaireId { get; set; }
+        public int Version { get; set; }
+        public int[] Variables { get; set; }
+    }
+}
