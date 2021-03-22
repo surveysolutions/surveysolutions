29ff59a5440999cd63bcfb66a9ffd78d9cbff221
VItaliy Balabanov <vitaliy.balabanov@gmail.com>
2021-03-22 16:44:43 -0400

    hotfix-21-01-7 [change migration type]

--- src/Infrastructure/WB.Persistence.Headquarters/Migrations/Workspace/M202103221921_FixSequencesInLogs.cs
+++ src/Infrastructure/WB.Persistence.Headquarters/Migrations/Workspace/M202103221921_FixSequencesInLogs.cs
@@ -3,7 +3,7 @@ using FluentMigrator;
 namespace WB.Persistence.Headquarters.Migrations.Workspace
 {
     [Migration(2021_03_22_19_21)]
-    public class M202103221921_FixSequencesInLogs : Migration
+    public class M202103221921_FixSequencesInLogs : ForwardOnlyMigration
     {
         public override void Up()
         {
@@ -11,11 +11,6 @@ namespace WB.Persistence.Headquarters.Migrations.Workspace
                 SELECT setval('auditlogrecords_id_seq', (SELECT MAX(id) FROM auditlogrecords));
                 SELECT setval('systemlog_id_seq', (SELECT MAX(id) FROM systemlog));
             ");
-        }
-
-        public override void Down()
-        {
-            
         }
     }
 }
