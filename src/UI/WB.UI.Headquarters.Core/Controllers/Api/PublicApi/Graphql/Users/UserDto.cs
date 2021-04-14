57de92a8598ab0b19f97fee0ba8e6929450a4940
Andrii Kozhyn <andrii.kozhyn@outlook.com>
2021-04-14 20:40:14 +0300

    KP-14659 implemented roles filter

--- src/UI/WB.UI.Headquarters.Core/Controllers/Api/PublicApi/Graphql/Users/UserDto.cs
+++ src/UI/WB.UI.Headquarters.Core/Controllers/Api/PublicApi/Graphql/Users/UserDto.cs
@@ -8,7 +8,6 @@ namespace WB.UI.Headquarters.Controllers.Api.PublicApi.Graphql.Users
         public Guid Id { get; set; }
         public string UserName { get; set; }
         public UserRoles[] Roles { get; set; }
-
         public string[] Workspaces { get; set; }
     }
 }
