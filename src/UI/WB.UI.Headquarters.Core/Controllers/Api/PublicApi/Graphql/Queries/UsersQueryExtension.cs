4c8dfa5892e6474076e5dec7a4b752047bfaaaab
Andrii Kozhyn <andrii.kozhyn@outlook.com>
2021-04-14 21:14:50 +0300

    KP-14659 implemented user details

--- src/UI/WB.UI.Headquarters.Core/Controllers/Api/PublicApi/Graphql/Queries/UsersQueryExtension.cs
+++ src/UI/WB.UI.Headquarters.Core/Controllers/Api/PublicApi/Graphql/Queries/UsersQueryExtension.cs
@@ -1,7 +1,10 @@
 #nullable enable
+using System;
 using System.Reflection.Metadata.Ecma335;
+using HotChocolate;
 using HotChocolate.Types;
 using WB.Core.BoundedContexts.Headquarters.Views.User;
+using WB.Infrastructure.Native.Storage.Postgre;
 using WB.UI.Headquarters.Controllers.Api.PublicApi.Graphql.Paging;
 using WB.UI.Headquarters.Controllers.Api.PublicApi.Graphql.Users;
 
@@ -21,12 +24,28 @@ namespace WB.UI.Headquarters.Controllers.Api.PublicApi.Graphql.Queries
             descriptor.Field<UsersResolver>(x => x.GetUsers(default, default))
                 .Authorize()
                 .Name("users")
-                .Argument("workspace", 
-                    a => a.Type<StringType>().Description("SupervisorId field can be included only if workspace specified"))
                 .Type<ListType<UserType>>()
                 .UseSimplePaging<UserType, HqUser>()
                 .UseSorting<UsersSortInputType>()
                 .UseFiltering<UsersFilterInputType>();
+
+            descriptor.Field<UserResolver>(x => x.GetUser(default, default))
+                .Authorize()
+                .Name("user")
+                .Description("Gets detailed information about single user within workspace")
+                .Type<WorkspaceUserType>()
+                .HasWorkspace()
+                .Argument("id", a => a.Type<NonNullType<IdType>>());
+        }
+    }
+
+    public class UserResolver
+    {
+        public HqUser GetUser(Guid id,
+            [Service] IUnitOfWork unitOfWork)
+        {
+            unitOfWork.DiscardChanges();
+            return unitOfWork.Session.Get<HqUser>(id);
         }
     }
 }
