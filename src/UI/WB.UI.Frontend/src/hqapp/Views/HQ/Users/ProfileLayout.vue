ab833e1d59ec40e49dadd995947c95e6fc4e8f97
Pavel Stovpivskyi <spuvik@gmail.com>
2021-05-05 20:29:48 +0300

    KP-14753 fixed urls

--- src/UI/WB.UI.Frontend/src/hqapp/Views/HQ/Users/ProfileLayout.vue
+++ src/UI/WB.UI.Frontend/src/hqapp/Views/HQ/Users/ProfileLayout.vue
@@ -33,28 +33,28 @@
                         v-bind:class=" {'active': currentTab == 'account'}" >
                         <a class="nav-link"
                             id="profile"
-                            v-bind:href="getUrl('../Manage')">{{$t('Pages.AccountManage_Profile')}}</a>
+                            v-bind:href="getUrl('./Manage')">{{$t('Pages.AccountManage_Profile')}}</a>
                     </li>
                     <li class="nav-item"
                         v-if="showWorkspaces && !forceChangePassword"
                         v-bind:class=" {'active': currentTab == 'workspaces'}" >
                         <a class="nav-link"
                             id="profile"
-                            v-bind:href="getUrl(`../Workspaces`)">{{$t('Workspaces.UserWorkspacesTab')}}</a>
+                            v-bind:href="getUrl(`./Workspaces`)">{{$t('Workspaces.UserWorkspacesTab')}}</a>
                     </li>
                     <li class="nav-item"
                         v-if="canChangePassword"
                         v-bind:class="{'active': currentTab=='password'}">
                         <a class="nav-link"
                             id="password"
-                            v-bind:href="getUrl('../ChangePassword')">{{$t('Pages.AccountManage_ChangePassword')}}</a>
+                            v-bind:href="getUrl('./ChangePassword')">{{$t('Pages.AccountManage_ChangePassword')}}</a>
                     </li>
                     <li class="nav-item"
                         v-if="!forceChangePassword"
                         v-bind:class="{'active': currentTab=='two-factor'}">
                         <a class="nav-link"
                             id="two-factor"
-                            v-bind:href="getUrl('../TwoFactorAuthentication')">{{$t('Pages.AccountManage_TwoFactorAuth')}}</a>
+                            v-bind:href="getUrl('./TwoFactorAuthentication')">{{$t('Pages.AccountManage_TwoFactorAuth')}}</a>
                     </li>
                 </ul>
 
