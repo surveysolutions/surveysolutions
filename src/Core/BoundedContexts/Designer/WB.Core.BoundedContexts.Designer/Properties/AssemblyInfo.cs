339af4bf0789d992facf45382448744560c1c5eb
Zurab Sajaia <16375540+vavalomi@users.noreply.github.com>
2021-05-19 14:09:11 -0400

    version strings are written to all DLLs and exactly in the same way

--- src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Properties/AssemblyInfo.cs
+++ src/Core/BoundedContexts/Designer/WB.Core.BoundedContexts.Designer/Properties/AssemblyInfo.cs
@@ -1,10 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0 (build 1)")]
-
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
 [assembly: InternalsVisibleTo("WB.Tests.Unit")]
 [assembly: InternalsVisibleTo("WB.Tests.Unit.Designer")]
