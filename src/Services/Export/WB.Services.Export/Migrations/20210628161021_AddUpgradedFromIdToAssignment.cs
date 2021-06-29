aea2cdd93a65621ca555f0e10fdcaf936ad1b1dc
VItaliy Balabanov <vitaliy.balabanov@gmail.com>
2021-06-29 11:40:44 -0400

    KP-14124 Assignment history to reflect upgrades.

---
+++ src/Services/Export/WB.Services.Export/Migrations/20210628161021_AddUpgradedFromIdToAssignment.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WB.Services.Export.Migrations
+{
+    public partial class AddUpgradedFromIdToAssignment : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "upgraded_from_id",
+                table: "__assignment",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "upgraded_from_id",
+                table: "__assignment");
+        }
+    }
+}
