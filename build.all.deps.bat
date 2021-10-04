72a1e9deb3de21a8ce3eb7a892afdb954062f987
Pavel Stovpivskyi <spuvik@gmail.com>
2021-10-04 16:29:57 +0300

    Change button label #1422

--- build.all.deps.bat
+++ build.all.deps.bat
@@ -1,3 +1,3 @@
-start cmd /c "TITLE Designer Questionnaire && cd src\UI\WB.UI.Designer && npm install && npm run gulp"
+start cmd /c "TITLE Designer Questionnaire && cd src\UI\WB.UI.Designer && npm install && npm run dev"
 call cmd  /c "TITLE hq deps && cd src\UI\WB.UI.Frontend && npm install && npm run build"
 
