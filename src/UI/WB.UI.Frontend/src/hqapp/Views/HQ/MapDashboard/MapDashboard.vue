ba661fd670a44a9bdc5a3ec633e531660a2ae73b
Pavel Stovpivskyi <spuvik@gmail.com>
2021-10-12 16:23:34 +0300

    Removed buttons 'approve' and 'reject' for interviews in RejectedByHeadquarters status for HQ #1446

--- src/UI/WB.UI.Frontend/src/hqapp/Views/HQ/MapDashboard/MapDashboard.vue
+++ src/UI/WB.UI.Frontend/src/hqapp/Views/HQ/MapDashboard/MapDashboard.vue
@@ -127,11 +127,11 @@
                         click="rejectSvInterview">{{ $t("Common.Reject")}}</button>
                     <button
                         class="btn btn-sm btn-success"
-                        v-if="model.userRole == 'Headquarter' && (selectedTooltip.status == 'Completed' || selectedTooltip.status == 'ApprovedBySupervisor' || selectedTooltip.status == 'RejectedByHeadquarters')"
+                        v-if="model.userRole == 'Headquarter' && (selectedTooltip.status == 'Completed' || selectedTooltip.status == 'ApprovedBySupervisor')"
                         click="approveHqInterview">{{ $t("Common.Approve")}}</button>
                     <button
                         class="btn btn-sm reject"
-                        v-if="model.userRole == 'Headquarter' && (selectedTooltip.status == 'Completed' || selectedTooltip.status == 'ApprovedBySupervisor' || selectedTooltip.status == 'RejectedByHeadquarters')"
+                        v-if="model.userRole == 'Headquarter' && (selectedTooltip.status == 'Completed' || selectedTooltip.status == 'ApprovedBySupervisor')"
                         click="rejectHqInterview">{{ $t("Common.Reject")}}</button>
                     <button
                         class="btn btn-sm btn-primary"
