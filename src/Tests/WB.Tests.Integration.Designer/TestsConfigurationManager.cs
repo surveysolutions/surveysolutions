376b04533ddd9259669ae6f60e33224a2dbfe964
VItaliy Balabanov <vitaliy.balabanov@gmail.com>
2021-10-12 17:49:50 -0400

    connection string parametrization

---
+++ src/Tests/WB.Tests.Integration.Designer/TestsConfigurationManager.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace WB.Tests.Integration
+{
+    public static class TestsConfigurationManager
+    {
+        private static readonly IConfigurationRoot Config;
+        
+        static TestsConfigurationManager()
+        {
+            Config = new ConfigurationBuilder().AddIniFile("appsettings.test.designer.ini").Build();
+        }
+
+        public static string TestConnectionStringFormat => Config["ConnectionSettings:TestConnectionStringFormat"];
+    }
+}
