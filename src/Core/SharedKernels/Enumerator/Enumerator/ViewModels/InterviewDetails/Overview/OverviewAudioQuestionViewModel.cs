c05bd657f560e89a2e16ef4cb1edc9b75f8aa57b
Pavel Stovpivskyi <spuvik@gmail.com>
2021-12-13 13:18:15 +0200

    dispose event in OverviewAudioQuestionViewModel

--- src/Core/SharedKernels/Enumerator/Enumerator/ViewModels/InterviewDetails/Overview/OverviewAudioQuestionViewModel.cs
+++ src/Core/SharedKernels/Enumerator/Enumerator/ViewModels/InterviewDetails/Overview/OverviewAudioQuestionViewModel.cs
@@ -20,6 +20,8 @@ namespace WB.Core.SharedKernels.Enumerator.ViewModels.InterviewDetails.Overview
         private readonly Guid interviewId;
         private readonly Identity questionIdentity;
 
+        private MvxWeakEventSubscription<IAudioService, PlaybackCompletedEventArgs> audioServiceSubscription;
+
         public OverviewAudioQuestionViewModel(InterviewTreeQuestion treeQuestion,
             IAudioFileStorage audioFileStorage,
             IAudioService audioService,
@@ -36,7 +38,7 @@ namespace WB.Core.SharedKernels.Enumerator.ViewModels.InterviewDetails.Overview
                 fileName = audioQuestion.GetAnswer().FileName;
 
                 this.Audio = audioFileStorage.GetInterviewBinaryData(interviewId, fileName);
-                this.audioService.WeakSubscribe<IAudioService, PlaybackCompletedEventArgs>(nameof(audioService.OnPlaybackCompleted), PlaybackCompleted);
+                audioServiceSubscription = this.audioService.WeakSubscribe<IAudioService, PlaybackCompletedEventArgs>(nameof(audioService.OnPlaybackCompleted), PlaybackCompleted);
             }
         }
 
@@ -79,6 +81,13 @@ namespace WB.Core.SharedKernels.Enumerator.ViewModels.InterviewDetails.Overview
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public override void Dispose()
+        {
+            base.Dispose();
+            
+            audioServiceSubscription?.Dispose();
         }
     }
 }
