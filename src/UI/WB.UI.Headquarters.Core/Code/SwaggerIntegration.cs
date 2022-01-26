7a403073bbcd2f340174703f5dde484c38479c6c
Pavel Stovpivskyi <spuvik@gmail.com>
2022-01-26 20:00:41 +0200

    changed swagger to to relevant #1526

--- src/UI/WB.UI.Headquarters.Core/Code/SwaggerIntegration.cs
+++ src/UI/WB.UI.Headquarters.Core/Code/SwaggerIntegration.cs
@@ -117,7 +117,7 @@ namespace WB.UI.Headquarters.Code
         {
             appBuilder.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Survey Solutions API");
+                c.SwaggerEndpoint("../swagger/v1/swagger.json", "Survey Solutions API");
                 c.RoutePrefix = "apidocs";
             });
         }
