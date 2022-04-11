0b92981a13ebebe32f5ab41f3e16a1b60501d7f0
VItaliy Balabanov <vitaliy.balabanov@gmail.com>
2022-04-11 18:42:04 -0400

    return 404 on invalid id on questionnaire details page. fix #1683

--- src/UI/WB.UI.Headquarters.Core/Controllers/QuestionnairesController.cs
+++ src/UI/WB.UI.Headquarters.Core/Controllers/QuestionnairesController.cs
@@ -72,7 +72,9 @@ namespace WB.UI.Headquarters.Controllers
 
         public IActionResult Details(string id)
         {
-            var questionnaireIdentity = QuestionnaireIdentity.Parse(id);
+            if (!QuestionnaireIdentity.TryParse(id, out var questionnaireIdentity))
+                return NotFound();
+            
             var questionnaire = this.questionnaireStorage.GetQuestionnaire(questionnaireIdentity, null);
             if (questionnaire == null)
                 return NotFound(string.Format(HQ.QuestionnaireNotFoundFormat, questionnaireIdentity.QuestionnaireId.FormatGuid(), questionnaireIdentity.Version));
