4172557f94df0f94be6fa77919b54fdc3ed9562f
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2022-06-07 16:42:40 -0400

    fix uploading of the maps in graphql

--- src/UI/WB.UI.Headquarters.Core/Controllers/Api/PublicApi/Graphql/Mutations/MapsMutationExtension.cs
+++ src/UI/WB.UI.Headquarters.Core/Controllers/Api/PublicApi/Graphql/Mutations/MapsMutationExtension.cs
@@ -61,7 +61,7 @@ namespace WB.UI.Headquarters.Controllers.Api.PublicApi.Graphql.Mutations
                     nameof(UserRoles.ApiUser)
                 })
                 .HasWorkspace()
-                .Type<Map>()
+                .Type<ListType<Map>>()
                 .Argument("file", a => a.Type<NonNullType<UploadType>>());
         }
     }
