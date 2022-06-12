e02fb64bceeb12e55f07b6ac340c69882dd799f6
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2022-06-12 11:25:55 -0400

    fix designer locked user login check

--- src/UI/WB.UI.Designer/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ src/UI/WB.UI.Designer/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -127,14 +127,6 @@ namespace WB.UI.Designer.Areas.Identity.Pages.Account
                         if (result.Succeeded)
                         {
                             this.captchaService.ResetFailedLogin(Input.Email);
-                            logger.LogInformation("User logged in.");
-                            return LocalRedirect(returnUrl);
-                        }
-
-                        if (result.RequiresTwoFactor)
-                        {
-                            return RedirectToPage("./LoginWith2fa",
-                                new {ReturnUrl = returnUrl, RememberMe = Input.RememberMe});
                         }
 
                         if (result.IsLockedOut)
@@ -142,7 +134,18 @@ namespace WB.UI.Designer.Areas.Identity.Pages.Account
                             logger.LogWarning("User account locked out.");
                             return RedirectToPage("./Lockout");
                         }
-
+                        
+                        if (result.RequiresTwoFactor)
+                        {
+                            return RedirectToPage("./LoginWith2fa",
+                                new {ReturnUrl = returnUrl, RememberMe = Input.RememberMe});
+                        }
+                        
+                        if (result.Succeeded)
+                        {
+                            logger.LogInformation("User logged in.");
+                            return LocalRedirect(returnUrl);
+                        }
 
                         this.captchaService.RegisterFailedLogin(Input.Email);
                         this.ErrorMessage = AccountResources.InvalidPassword;
