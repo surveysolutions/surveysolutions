77dcfbcc7c227b97f8e7457ee592456eed3eef42
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2022-08-11 17:09:30 -0400

    change lookup table getter param type to QuestionnaireRevision

--- src/UI/WB.UI.Designer/Controllers/Api/Headquarters/HQLookupController.cs
+++ src/UI/WB.UI.Designer/Controllers/Api/Headquarters/HQLookupController.cs
@@ -5,7 +5,6 @@ using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using WB.Core.BoundedContexts.Designer.Services;
 using WB.Core.BoundedContexts.Designer.Views.Questionnaire.ChangeHistory;
-using WB.UI.Designer.Code.Attributes;
 
 namespace WB.UI.Designer.Controllers.Api.Headquarters
 {
@@ -21,9 +20,9 @@ namespace WB.UI.Designer.Controllers.Api.Headquarters
 
         [HttpGet]
         [Route("{id}/{tableId}")]
-        public IActionResult Get(string id, string tableId)
+        public IActionResult Get(QuestionnaireRevision id, string tableId)
         {
-            var lookupFile = this.lookupTableService.GetLookupTableContentFile(new QuestionnaireRevision(Guid.Parse(id)), Guid.Parse(tableId));
+            var lookupFile = this.lookupTableService.GetLookupTableContentFile(id, Guid.Parse(tableId));
 
             if (lookupFile == null) return NotFound();
 
