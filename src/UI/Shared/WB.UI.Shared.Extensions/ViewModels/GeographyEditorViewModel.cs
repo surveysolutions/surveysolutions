405cff0a89c23a54a07a8d00dd54d8b89c44a705
Pavel Stovpivskyi <spuvik@gmail.com>
2022-10-27 17:00:24 +0300

    added Neighbors for auto mode#1983

--- src/UI/Shared/WB.UI.Shared.Extensions/ViewModels/GeographyEditorViewModel.cs
+++ src/UI/Shared/WB.UI.Shared.Extensions/ViewModels/GeographyEditorViewModel.cs
@@ -119,7 +119,7 @@ namespace WB.UI.Shared.Extensions.ViewModels
                         await this.MapView.SetViewpointGeometryAsync(this.Geometry, 120).ConfigureAwait(false);
                 }
 
-                await DrawNeighborsAsync(this.Geometry, this.GeographyNeighbors).ConfigureAwait(false);
+                await DrawNeighborsAsync(this.RequestedGeometryType, this.Geometry, this.GeographyNeighbors).ConfigureAwait(false);
 
                 var result = await GetGeometry().ConfigureAwait(false);
 
@@ -210,11 +210,12 @@ namespace WB.UI.Shared.Extensions.ViewModels
                     : UIResources.AreaMap_LengthFormat, length.ToString("#.##")) : string.Empty;
         }
 
-        private async Task DrawNeighborsAsync(Geometry geometry, GeometryNeighbor[] neighbors)
+        private async Task DrawNeighborsAsync(GeometryType? geometryType, Geometry geometry, GeometryNeighbor[] neighbors)
         {
             if (neighbors == null || neighbors.Length == 0)
                 return;
 
+            var gType = geometryType ?? GeometryType.Polygon;
             var esriGeometryType = neighbors[0].Geometry.GeometryType;
 
             IEnumerable<Field> fields = new Field[]
@@ -222,9 +223,9 @@ namespace WB.UI.Shared.Extensions.ViewModels
                 new Field(FieldType.Text, "name", "Name", 50)
             };
             var neighborsFeatureCollectionTable = new FeatureCollectionTable(fields, esriGeometryType, this.MapView.SpatialReference);
-            neighborsFeatureCollectionTable.Renderer = CreateRenderer(esriGeometryType, Color.Blue);
+            neighborsFeatureCollectionTable.Renderer = CreateRenderer(gType, Color.Blue);
             var overlappingNeighborsFeatureCollectionTable = new FeatureCollectionTable(fields, esriGeometryType, this.MapView.SpatialReference);
-            overlappingNeighborsFeatureCollectionTable.Renderer = CreateRenderer(esriGeometryType, Color.Orange);
+            overlappingNeighborsFeatureCollectionTable.Renderer = CreateRenderer(gType, Color.Orange);
 
             List<string> overlappingTitles = new List<string>();
 
@@ -379,14 +380,14 @@ namespace WB.UI.Shared.Extensions.ViewModels
             }
         }
         
-        private Renderer CreateRenderer(EsriGeometryType rendererType, Color color)
+        private Renderer CreateRenderer(GeometryType rendererType, Color color)
         {
             Symbol sym = rendererType switch
             {
-                EsriGeometryType.Point => new SimpleMarkerSymbol(SimpleMarkerSymbolStyle.Circle, color, 18),
-                EsriGeometryType.Multipoint => new SimpleMarkerSymbol(SimpleMarkerSymbolStyle.Circle, color, 18),
-                EsriGeometryType.Polyline => new SimpleLineSymbol(SimpleLineSymbolStyle.Solid, color, 2),
-                EsriGeometryType.Polygon => new SimpleLineSymbol(SimpleLineSymbolStyle.Solid, color, 2),
+                GeometryType.Point => new SimpleMarkerSymbol(SimpleMarkerSymbolStyle.Circle, color, 18),
+                GeometryType.Multipoint => new SimpleMarkerSymbol(SimpleMarkerSymbolStyle.Circle, color, 18),
+                GeometryType.Polyline => new SimpleLineSymbol(SimpleLineSymbolStyle.Solid, color, 2),
+                GeometryType.Polygon => new SimpleLineSymbol(SimpleLineSymbolStyle.Solid, color, 2),
                 _ => null
             };
 
@@ -422,7 +423,7 @@ namespace WB.UI.Shared.Extensions.ViewModels
             locationOverlay.Renderer = new SimpleRenderer(locationPointSymbol);
             this.MapView.GraphicsOverlays.Add(locationOverlay);
 
-            //await DrawNeighborsAsync(this.RequestedGeometryType, this.Geometry, this.GeographyNeighbors).ConfigureAwait(false);
+            await DrawNeighborsAsync(this.RequestedGeometryType, this.Geometry, this.GeographyNeighbors).ConfigureAwait(false);
 
             //project and use current map
             polylineBuilder = new PolylineBuilder(this.MapView.SpatialReference);
@@ -587,12 +588,15 @@ namespace WB.UI.Shared.Extensions.ViewModels
                     {
                         polylineBuilder.AddPoint(lastPositionProjected);
 
-                        await mainThreadAsyncDispatcher.ExecuteOnMainThreadAsync(() => 
+                        await mainThreadAsyncDispatcher.ExecuteOnMainThreadAsync(async () => 
                         {
                             // Remove the old line.
                             locationLineOverlay.Graphics.Clear();
                             // Add the updated line.
-                            locationLineOverlay.Graphics.Add(new Graphic(polylineBuilder.ToGeometry()));
+                            var geometry = polylineBuilder.ToGeometry();
+                            locationLineOverlay.Graphics.Add(new Graphic(geometry));
+
+                            await UpdateDrawNeighborsAsync(geometry, this.GeographyNeighbors);
                         });
                     }
                     
@@ -753,7 +757,7 @@ namespace WB.UI.Shared.Extensions.ViewModels
                 this.MapView?.SketchEditor.ClearGeometry();
                 this.MapView?.SketchEditor.ReplaceGeometry(geometry);
                 
-                await DrawNeighborsAsync(geometry, this.GeographyNeighbors).ConfigureAwait(false);
+                await DrawNeighborsAsync(this.RequestedGeometryType, geometry, this.GeographyNeighbors).ConfigureAwait(false);
             }
         });
 
