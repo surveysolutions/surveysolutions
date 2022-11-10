612f343fcb5fbd69d4ed5958a9359490c5f552f9
Pavel Stovpivskyi <spuvik@gmail.com>
2022-11-10 16:42:45 +0100

    added calculate variables #2130

--- src/Services/Export/WB.Services.Export/InterviewDataStorage/InterviewDataExport/QuestionnaireLevelDatabaseTable.cs
+++ src/Services/Export/WB.Services.Export/InterviewDataStorage/InterviewDataExport/QuestionnaireLevelDatabaseTable.cs
@@ -8,7 +8,7 @@ namespace WB.Services.Export.InterviewDataStorage.InterviewDataExport
     public class QuestionnaireLevelDatabaseTable
     {
         private const int ColumnsLimit = 1400;
-        private const int DataMaximumSizeLimit = 8000; //https://www.postgresql.org/docs/current/limits.html
+        private const int DataMaximumSizeLimit = 7800; //https://www.postgresql.org/docs/current/limits.html
 
         public Guid Id { get; set; }
         public bool IsRoster { get; set; }
@@ -24,25 +24,47 @@ namespace WB.Services.Export.InterviewDataStorage.InterviewDataExport
         
         public bool CanAddChildren(Group group)
         {
-            var questionsCount = group.Children.Count(entity => entity is Question);
-            var variablesCount = group.Children.Count(entity => entity is Variable);
-            var staticTextCount = group.Children.Count(entity => entity is StaticText);
-            var groupsCount = group.Children.Count(entity => entity is Group roster && !roster.IsRoster);
-            if (questionsCount + variablesCount + DataColumns.Count > ColumnsLimit)
-                return false;
-            if (questionsCount + variablesCount + staticTextCount + groupsCount + EnablementColumns.Count > ColumnsLimit)
-                return false;
-            if (questionsCount + staticTextCount + ValidityColumns.Count > ColumnsLimit)
-                return false;
-            var columnsDataSize = group.Children.Where(entity => entity is Question)
-                .Cast<Question>()
-                .Sum(question => GetDataRowSize(question));
-            if (rowDataSize + columnsDataSize > DataMaximumSizeLimit)
-                return false;
+            var overDataColumnsCount = ColumnsLimit - DataColumns.Count;
+            var overEnablementColumnsCount = ColumnsLimit - EnablementColumns.Count;
+            var overValidityColumnsCount = ColumnsLimit - ValidityColumns.Count;
+            var columnsDataSize = DataMaximumSizeLimit - rowDataSize;
+            
+            foreach (var entity in group.Children)
+            {
+                switch (entity)
+                {
+                    case Question question:
+                        overDataColumnsCount--;
+                        overEnablementColumnsCount--;
+                        overValidityColumnsCount--;
+                        columnsDataSize -= GetDataRowSize(question);
+                        break;
+                    case Variable variable:
+                        overDataColumnsCount--;
+                        overEnablementColumnsCount--;
+                        columnsDataSize -= GetDataRowSize(variable);
+                        break;
+                    case StaticText staticText:
+                        overEnablementColumnsCount--;
+                        overValidityColumnsCount--;
+                        break;
+                    case Group { IsRoster: false }:
+                        overEnablementColumnsCount--;
+                        break;
+                }
 
+                if (overDataColumnsCount <= 0
+                    || overEnablementColumnsCount <= 0
+                    || overValidityColumnsCount <= 0
+                    || columnsDataSize <= 0)
+                {
+                    return false;
+                }
+            }
+            
             return true;
         }
-
+        
         private static int GetDataRowSize(Question question)
         {
             switch (question.QuestionType)
@@ -67,16 +89,25 @@ namespace WB.Services.Export.InterviewDataStorage.InterviewDataExport
             }
         }
 
+        private static int GetDataRowSize(Variable variable)
+        {
+            switch (variable.Type)
+            {
+                case VariableType.Boolean: /* bool */ return 2;
+                case VariableType.Double: /* double */ return 8;
+                case VariableType.String: /* text */ return 24;
+                case VariableType.DateTime: /* timestamp */ return 8;
+                case VariableType.LongInteger: /* int64 */ return 8;
+                default:
+                    throw new Exception("Unknown variable type: " + variable.Type);
+            }
+        }
+
         public void AddChildren(Group group)
         {
             if (group.Children.Count() > ColumnsLimit)
                 throw new ArgumentException($"Group {group.PublicKey} ({group.Children.Count()} children) fail to insert in table with {Entities.Count} entities. Max allowed {ColumnsLimit}");
 
-            var columnsDataSize = group.Children.Where(entity => entity is Question)
-                .Cast<Question>()
-                .Sum(question => GetDataRowSize(question));
-            rowDataSize += columnsDataSize;
-            
             Entities.Add(group);
 
             if (!(group is QuestionnaireDocument))
@@ -84,7 +115,8 @@ namespace WB.Services.Export.InterviewDataStorage.InterviewDataExport
                 EnablementColumns.Add(group);
             }
 
-            foreach (var child in @group.Children)
+
+            foreach (var child in group.Children)
             {
                 if (!child.IsExportable) continue;
 
@@ -95,11 +127,13 @@ namespace WB.Services.Export.InterviewDataStorage.InterviewDataExport
                         EnablementColumns.Add(question);
                         ValidityColumns.Add(question);
                         Entities.Add(child);
+                        rowDataSize += GetDataRowSize(question);
                         break;
                     case Variable variable:
                         DataColumns.Add(variable);
                         EnablementColumns.Add(variable);
                         Entities.Add(child);
+                        rowDataSize += GetDataRowSize(variable);
                         break;
                     case StaticText staticText:
                         EnablementColumns.Add(staticText);
