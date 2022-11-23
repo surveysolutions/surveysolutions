20d00375d8e013616d76ac2deb0f5772fe286278
Pavel Stovpivskyi <spuvik@gmail.com>
2022-11-23 22:25:03 +0100

    up schema export version to 7

--- src/Services/Export/WB.Services.Export/Infrastructure/TenantDbContext.cs
+++ src/Services/Export/WB.Services.Export/Infrastructure/TenantDbContext.cs
@@ -31,7 +31,7 @@ namespace WB.Services.Export.Infrastructure
 
         private readonly Lazy<string> connectionString;
 
-        private const long ContextSchemaVersion = 6;
+        private const long ContextSchemaVersion = 7;
 
         private readonly IOptions<DbConnectionSettings> connectionSettings;
         private readonly ILogger<TenantDbContext>? logger;
