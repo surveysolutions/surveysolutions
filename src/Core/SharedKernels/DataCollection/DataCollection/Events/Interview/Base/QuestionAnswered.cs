b16e52c076e4c3756f782e1272adb96666767ee5
Pavel Stovpivskyi <spuvik@gmail.com>
2022-11-29 16:16:45 +0100

    Added Obsolete attribute

--- src/Core/SharedKernels/DataCollection/DataCollection/Events/Interview/Base/QuestionAnswered.cs
+++ src/Core/SharedKernels/DataCollection/DataCollection/Events/Interview/Base/QuestionAnswered.cs
@@ -4,6 +4,7 @@ namespace WB.Core.SharedKernels.DataCollection.Events.Interview.Base
 {
     public abstract class QuestionAnswered : QuestionActiveEvent
     {
+        [Obsolete("Please use OriginDate property")]
         public DateTime? AnswerTimeUtc { get; set; }
 
         protected QuestionAnswered(Guid userId, Guid questionId, decimal[] rosterVector, DateTimeOffset originDate)
