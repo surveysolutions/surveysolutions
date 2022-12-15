1af9debe0f1ba57a279fffb07e10c4dfe620e322
Pavel Stovpivskyi <spuvik@gmail.com>
2022-12-15 16:46:28 +0100

    remove from linker old code

--- src/UI/Interviewer/WB.UI.Interviewer/LinkerPleaseInclude.cs
+++ src/UI/Interviewer/WB.UI.Interviewer/LinkerPleaseInclude.cs
@@ -147,19 +147,6 @@ namespace WB.UI.LinkerInclusion
             service = new MvxNavigationService( loader, viewDispatcher,iocProvider);
         }
 
-        public void Include(ConsoleColor color)
-        {
-            Console.Write("");
-            Console.WriteLine("");
-            color = Console.ForegroundColor;
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.ForegroundColor = ConsoleColor.Magenta;
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.ForegroundColor = ConsoleColor.Gray;
-            Console.ForegroundColor = ConsoleColor.DarkGray;
-        }
-
         public void Include(RecyclerView.ViewHolder vh, MvxRecyclerView list)
         {
             vh.ItemView.Click += (sender, args) => { };
