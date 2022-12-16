e3013e5985c73c2dc232d137736e8471cd3f2409
Pavel Stovpivskyi <spuvik@gmail.com>
2022-12-16 20:34:21 +0100

    disable temporally path applier

---
+++ src/UI/Shared/WB.UI.Shared.Enumerator/Additions/TypedRange.cs
@@ -0,0 +1,12 @@
+﻿using Object = Java.Lang.Object;
+
+namespace Com.Google.Archivepatcher.Shared
+{
+    public partial class TypedRange : Object, IComparable
+    {
+        int IComparable.CompareTo(object obj)
+        {
+            return 0;
+        }
+    }
+}
