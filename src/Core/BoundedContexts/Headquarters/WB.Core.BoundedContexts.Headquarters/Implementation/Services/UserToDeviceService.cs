f515c8b14b6141197fe57c57e26fdc2ea88915cd
Pavel Stovpivskyi <spuvik@gmail.com>
2023-01-03 22:11:18 +0100

    reset flag after relink #2228

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Implementation/Services/UserToDeviceService.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/Implementation/Services/UserToDeviceService.cs
@@ -43,6 +43,7 @@ namespace WB.Core.BoundedContexts.Headquarters.Implementation.Services
 
             user.Profile.DeviceId = deviceId;
             user.Profile.DeviceRegistrationDate = DateTime.UtcNow;
+            user.Profile.AllowRelinkDate = null;
         }
     }
 }
