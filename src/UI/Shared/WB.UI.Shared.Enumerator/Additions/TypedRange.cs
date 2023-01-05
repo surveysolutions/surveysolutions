7b178f960edaa693cc116732f22ca3611d6d576e
Pavel Stovpivskyi <spuvik@gmail.com>
2023-01-05 20:08:45 +0100

    include java lib patch-applier.jar

--- src/UI/Shared/WB.UI.Shared.Enumerator/Additions/TypedRange.cs
+++ src/UI/Shared/WB.UI.Shared.Enumerator/Additions/TypedRange.cs
@@ -1,12 +1,20 @@
-﻿using Object = Java.Lang.Object;
+﻿using Java.Lang;
+using Object = Java.Lang.Object;
 
 namespace Com.Google.Archivepatcher.Shared
 {
-    public partial class TypedRange : Object, IComparable
+    public partial class TypedRange : Object, Java.Lang.IComparable
     {
+        /*
         int IComparable.CompareTo(object obj)
         {
             return 0;
+        }
+        */
+
+        public int CompareTo(Object o)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
