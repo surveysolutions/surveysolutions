e6376c783052939f654cf7d1157590dc1a2b9092
Pavel Stovpivskyi <spuvik@gmail.com>
2023-07-13 16:12:19 +0200

    Note for supervisor is not cleared out after completing-rejecting #2620

--- src/Core/SharedKernels/Enumerator/Enumerator/Implementation/Services/LastCompletionComments.cs
+++ src/Core/SharedKernels/Enumerator/Enumerator/Implementation/Services/LastCompletionComments.cs
@@ -2,29 +2,38 @@
 using System.Collections.Generic;
 using WB.Core.GenericSubdomains.Portable;
 using WB.Core.SharedKernels.Enumerator.Services;
+using WB.Core.SharedKernels.Enumerator.Services.Infrastructure.Storage;
+using WB.Core.SharedKernels.Enumerator.Views;
 
 namespace WB.Core.SharedKernels.Enumerator.Implementation.Services
 {
     public class LastCompletionComments : ILastCompletionComments
     {
-        private readonly Dictionary<Guid, string> comments = new Dictionary<Guid, string>();
+        private readonly IPlainStorage<InterviewView> interviewViewRepository;
+
+        public LastCompletionComments(IPlainStorage<InterviewView> interviewViewRepository)
+        {
+            this.interviewViewRepository = interviewViewRepository;
+        }
 
         public void Store(Guid interviewId, string comment)
         {
-            comments[interviewId] = comment;
+            var interviewView = interviewViewRepository.GetById(interviewId.FormatGuid());
+            interviewView.InterviewComment = comment;
+            interviewViewRepository.Store(interviewView);
         }
 
         public string Get(Guid interviewId)
         {
-            return comments.GetOrNull(interviewId);
+            var interviewView = interviewViewRepository.GetById(interviewId.FormatGuid());
+            return interviewView.InterviewComment;
         }
 
         public void Remove(Guid interviewId)
         {
-            if (comments.ContainsKey(interviewId))
-            {
-                comments.Remove(interviewId);
-            }
+            var interviewView = interviewViewRepository.GetById(interviewId.FormatGuid());
+            interviewView.InterviewComment = null;
+            interviewViewRepository.Store(interviewView);
         }
     }
 }
