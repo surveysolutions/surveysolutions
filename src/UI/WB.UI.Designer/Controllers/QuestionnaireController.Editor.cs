ab8f16bc22ef98012db7c5655db957985ce29eb5
Pavel Stovpivskyi <spuvik@gmail.com>
2023-09-21 21:01:51 +0200

    fix response

--- src/UI/WB.UI.Designer/Controllers/QuestionnaireController.Editor.cs
+++ src/UI/WB.UI.Designer/Controllers/QuestionnaireController.Editor.cs
@@ -9,10 +9,30 @@ namespace WB.UI.Designer.Controllers
     {
         [HttpGet]
         [VuePage]
-        [Route("q/details")]
+        //[AntiForgeryFilter]
+        [Route("q/details/{id}")]
+        [Route("q/details/{id}/chapter/{chapterId}")]
+        [Route("q/details/{id}/chapter/{chapterId}/{entityType}/{entityId}")]
         public IActionResult QuestionnaireDetails(QuestionnaireRevision? id, Guid? chapterId, string entityType, Guid? entityId)
         {
-            return Ok();
+            if(id == null)
+                return this.RedirectToAction("Index", "QuestionnaireList");
+
+            var questionnaire = questionnaireViewFactory.Load(id);
+            if (questionnaire == null || questionnaire.Source.IsDeleted)
+                return NotFound();
+
+            if (ShouldRedirectToOriginalId(id))
+            {
+                return RedirectToAction("Details", new RouteValueDictionary
+                {
+                    { "id", id.OriginalQuestionnaireId.FormatGuid() }, { "chapterId", chapterId?.FormatGuid() }, { "entityType", entityType }, { "entityid", entityId?.FormatGuid() }
+                });
+            }
+
+            return (User.IsAdmin() || this.UserHasAccessToEditOrViewQuestionnaire(id.QuestionnaireId))
+                ? this.Ok()
+                : this.LackOfPermits();
         }
     }
 }
