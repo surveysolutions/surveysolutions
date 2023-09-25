271105c88ac95bde19d6572735bd9b75623380d1
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2023-09-25 17:41:05 -0400

    Adding store to the app

--- src/UI/WB.UI.Designer/questionnaire-app/src/main.js
+++ src/UI/WB.UI.Designer/questionnaire-app/src/main.js
@@ -1,12 +1,15 @@
 import { createApp } from 'vue';
+import { createPinia } from 'pinia'
 import App from './App.vue';
 import router from './router';
 import {vuetify, i18n} /*, install, i18n */ from './plugins/vuetify';
 
 /** Register Vue */
+const pinia = createPinia();
 const vue = createApp(App);
 vue.use(router);
-vue.use(vuetify);
+vue.use(pinia);
+vue.use(vuetify); //reqired by options component. consider either remove or use.
 vue.use(i18n);
 
 // Run!
