d4959e5f0c2828d18a6b81990ae3c8c76e66cc4b
Pavel Stovpivskyi <spuvik@gmail.com>
2023-10-11 16:23:20 +0200

    include uiv

--- src/UI/WB.UI.Designer/questionnaire-app/src/main.js
+++ src/UI/WB.UI.Designer/questionnaire-app/src/main.js
@@ -5,6 +5,8 @@ import router from './router';
 import { vuetify, i18n /*, install, i18n */ } from './plugins/vuetify';
 
 import '../../questionnaire/content/markup.less';
+//import 'bootstrap/dist/css/bootstrap.min.css';
+import * as uiv from 'uiv';
 
 import './directives';
 
@@ -15,6 +17,7 @@ vue.use(router);
 vue.use(pinia);
 vue.use(vuetify); //reqired by options component. consider either remove or use.
 vue.use(i18n);
+vue.use(uiv);
 
 //import { useQuestionnaireStore } from './store'
 //vue.provide('store', useQuestionnaireStore)
