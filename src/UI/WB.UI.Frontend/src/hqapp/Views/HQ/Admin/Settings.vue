aecf96aef418fb72f266668b8b1a558c7fe5be3a
Pavel Stovpivskyi <spuvik@gmail.com>
2023-11-20 15:51:43 +0100

    format error message

--- src/UI/WB.UI.Frontend/src/hqapp/Views/HQ/Admin/Settings.vue
+++ src/UI/WB.UI.Frontend/src/hqapp/Views/HQ/Admin/Settings.vue
@@ -72,8 +72,8 @@
                         <span v-if="statusDropExportCache == 'Removed'" style="color:green">
                             {{ $t('Settings.RemoveExportCacheSuccess') }}
                         </span>
-                        <span v-if="statusDropExportCache == 'Error'" style="color:red">
-                            {{ $t('Settings.RemoveExportCacheFail') }}
+                        <span v-if="statusDropExportCache == 'Error'" style="color:red"
+                            v-html="$t('Settings.RemoveExportCacheFail').replaceAll('\n', '<br/>')">
                         </span>
                     </div>
                 </div>
@@ -757,8 +757,8 @@ export default {
                         label: self.$t('Common.Clear'),
                         className: 'btn btn-danger',
                         callback: async () => {
-                            this.statusDropExportCache = null
                             this.allowToRemoveExportCache = false
+                            this.statusDropExportCache = 'Removing'
                             await this.runDropExportSchema()
                         },
                     },
