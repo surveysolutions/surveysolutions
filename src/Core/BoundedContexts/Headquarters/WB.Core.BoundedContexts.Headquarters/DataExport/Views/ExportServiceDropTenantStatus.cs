5ef552c197cc642906d6a1aacb0453eabea644e4
Pavel Stovpivskyi <spuvik@gmail.com>
2023-11-30 17:02:14 +0100

    Revert "return error"
    This reverts commit 7ef1d20ccf03f919f1791e55b7548efe18fe2ead.

--- src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/DataExport/Views/ExportServiceDropTenantStatus.cs
+++ src/Core/BoundedContexts/Headquarters/WB.Core.BoundedContexts.Headquarters/DataExport/Views/ExportServiceDropTenantStatus.cs
@@ -3,7 +3,6 @@ namespace WB.Core.BoundedContexts.Headquarters.DataExport.Views;
 public class ExportServiceDropTenantStatus
 {
     public StopTenantStatus Status { get; set; }
-    public string LastError { get; set; }
 }
 
 public enum StopTenantStatus
