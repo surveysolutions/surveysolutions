f2916c7106ffd6eafd8f125d3195a9f55435d090
Pavel Stovpivskyi <spuvik@gmail.com>
2023-12-26 19:51:01 +0100

    text question

---
+++ src/UI/WB.UI.Designer/questionnaire-app/src/views/App/components/parts/TextQuestion.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="form-group">
+        <label for="edit-mask">{{ $t('QuestionnaireEditor.QuestionPattern') }}
+            <help link="mask" />
+        </label>
+        <input id="edit-mask" type="text" v-model="activeQuestion.mask" class="form-control">
+    </div>
+</template>
+
+<script>
+
+import Help from './../Help.vue'
+
+export default {
+    name: 'TextQuestion',
+    components: {
+        Help,
+    },
+    props: {
+        activeQuestion: { type: Object, required: true }
+    },
+    data() {
+        return {
+
+        }
+    },
+}
+</script>
