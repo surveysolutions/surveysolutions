7ddc588501918ad5355012075eb589aa675b366e
Pavel Stovpivskyi <spuvik@gmail.com>
2023-12-28 20:57:10 +0100

    v-i18next to detect missing translations

--- src/UI/WB.UI.Designer/questionnaire-app/src/directives/i18next.js
+++ src/UI/WB.UI.Designer/questionnaire-app/src/directives/i18next.js
@@ -2,13 +2,22 @@ import { i18n } from '../plugins/localization';
 
 const i18next = app => {
     app.directive('i18next', {
-        mounted(el, binding) {
-            const key = binding.value
-                ? binding.value
-                : el.text;
-            i18n.t('$'+ key);
+        beforeMount(el, binding) {
+            init(el, binding);
+        },
+        updated(el, binding) {
+            init(el, binding);
         }
     });
 };
+
+function init(el, binding) {
+    el.innerHTML = '<b style="color:red">Need migrate translation</b>';
+    /*let key = binding.value || el.getAttribute('v-i18next');
+    if (key) {
+        const translatedText = i18n.t('QuestionnaireEditor.' + key);
+        el.innerText = translatedText;
+    }*/
+}
 
 export default i18next;
