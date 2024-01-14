08bd269fa759dd8794c47426ed70d97be0636f00
V-B <vitaliy.balabanov@gmail.com>
2024-01-13 19:24:50 -0500

    add autocomplete to editors

--- src/UI/WB.UI.Designer/questionnaire-app/src/views/App/components/ExpressionEditor.vue
+++ src/UI/WB.UI.Designer/questionnaire-app/src/views/App/components/ExpressionEditor.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="pseudo-form-control" ref="editorHolder">        
+    <div class="pseudo-form-control" ref="editorHolder">
         <v-ace-editor ref="editor" v-model:value="editorValue" @init="editorInit" theme="github"
             :lang="mode !== 'substitutions' ? 'csharp' : 'text'" :options="{
                 maxLines: 30,
@@ -16,11 +16,18 @@
 
 <script>
 import { VAceEditor } from 'vue3-ace-editor';
+
 import themeGithubUrl from 'ace-builds/src-noconflict/theme-github?url';
 import modeCsharpUrl from 'ace-builds/src-noconflict/mode-csharp?url';
+import tools from 'ace-builds/src-noconflict/ext-language_tools?url';
 
 ace.config.setModuleUrl('ace/theme/github', themeGithubUrl);
 ace.config.setModuleUrl('ace/mode/csharp', modeCsharpUrl);
+ace.config.setModuleUrl('ace/ext/language_tools', tools);
+
+import { addCompleter, setCompleters } from 'ace-builds/src-noconflict/ext-language_tools';
+import _ from 'lodash'
+import { useTreeStore } from '../../../stores/tree';
 
 export default {
     name: 'ExpressionEditor',
@@ -35,6 +42,13 @@ export default {
             //
         };
     },
+    setup() {
+        const treeStore = useTreeStore();
+
+        return {
+            treeStore
+        };
+    },
     computed: {
         editorValue: {
             get() {
@@ -47,6 +61,7 @@ export default {
     },
     methods: {
         editorInit(editor) {
+            self = this;
             var renderer = editor.renderer;
             renderer.setPadding(12);
 
@@ -57,8 +72,7 @@ export default {
             var session = editor.getSession();
 
             //extend text mode
-            if (session.$mode.$id == "ace/mode/text") {
-
+            if (this.mode === 'substitutions') {
                 var rules = session.$mode.$highlightRules.getRules();
                 for (var stateName in rules) {
                     if (Object.prototype.hasOwnProperty.call(rules, stateName)) {
@@ -72,6 +86,39 @@ export default {
                 session.$mode.$tokenizer = null;
                 session.bgTokenizer.setTokenizer(session.$mode.getTokenizer());
                 session.bgTokenizer.start(0);
+            }
+            else if (self.mode !== 'substitutions') {
+                
+                ace.config.loadModule('ace/ext/language_tools', function () {
+                    var variablesCompletor =
+                    {
+                        getCompletions: function(editor, session, pos, prefix, callback) {
+                            var i = 0;
+
+                            var variables =                             
+                            _.orderBy(self.treeStore.getChapter.variableNames, 'name', 'desc')                                
+                                .map(function(variable) {
+                                    return {
+                                        name: variable.name,
+                                        value: variable.name,
+                                        score: i++,
+                                        meta: variable.type
+                                    }
+                                });
+                            callback(null, variables);
+                        },
+
+                        identifierRegexps : [/[@a-zA-Z_0-9\$\-\u00A2-\uFFFF]/]
+                    };
+
+                    addCompleter(variablesCompletor);
+
+                    editor.setOptions({
+                        enableBasicAutocompletion: true,
+                        enableSnippets: false,
+                        enableLiveAutocompletion: true
+                    });
+                });
             }
 
             var holderDiv = this.$refs.editorHolder;
