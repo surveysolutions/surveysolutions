63138923b250a1f7f41110c69d0f5968feb74c98
V-B <vitaliy.balabanov@gmail.com>
2024-01-16 17:34:39 -0500

    highlight variables in expressions editors

--- src/UI/WB.UI.Designer/questionnaire-app/src/views/App/components/ExpressionEditor.vue
+++ src/UI/WB.UI.Designer/questionnaire-app/src/views/App/components/ExpressionEditor.vue
@@ -25,7 +25,7 @@ ace.config.setModuleUrl('ace/theme/github', themeGithubUrl);
 ace.config.setModuleUrl('ace/mode/csharp', modeCsharpUrl);
 ace.config.setModuleUrl('ace/ext/language_tools', tools);
 
-import { addCompleter, setCompleters } from 'ace-builds/src-noconflict/ext-language_tools';
+import { setCompleters } from 'ace-builds/src-noconflict/ext-language_tools';
 import _ from 'lodash'
 import { useTreeStore } from '../../../stores/tree';
 
@@ -71,7 +71,7 @@ export default {
 
             var session = editor.getSession();
 
-            //extend text mode
+            //extend text mode with substitutions
             if (this.mode === 'substitutions') {
                 var rules = session.$mode.$highlightRules.getRules();
                 for (var stateName in rules) {
@@ -88,30 +88,71 @@ export default {
                 session.bgTokenizer.start(0);
             }
             else if (self.mode !== 'substitutions') {
-                
+                session.on("changeMode", function () {
+                    if (session.$mode.$id !== 'ace/mode/csharp')
+                        return;
+
+                    var rules = session.$mode.$highlightRules.getRules();
+                    for (var stateName in rules) {
+                        if (Object.prototype.hasOwnProperty.call(rules, stateName)) {
+
+                            var mapperRule = rules[stateName].find(function (rule) {
+                                return rule.regex == '[a-zA-Z_$][a-zA-Z0-9_$]*\\b';
+                            });
+                            if (mapperRule == undefined || mapperRule == null)
+                                continue;
+                            else {
+
+                                var supportVariableList = "";
+                                var variables = self.treeStore.getChapter.variableNames;
+                                if (variables)
+                                    supportVariableList = variables.map(function (el) {
+                                        return el.name;
+                                    }).join('|');
+
+
+                                var keywordMapper = session.$mode.$highlightRules.createKeywordMapper({
+                                    "variable.language": "this|self",
+                                    "support.variable": supportVariableList,
+                                    "keyword": "abstract|event|new|struct|as|explicit|null|switch|base|extern|object|this|bool|false|operator|throw|break|finally|out|true|byte|fixed|override|try|case|float|params|typeof|catch|for|private|uint|char|foreach|protected|ulong|checked|goto|public|unchecked|class|if|readonly|unsafe|const|implicit|ref|ushort|continue|in|return|using|decimal|int|sbyte|virtual|default|interface|sealed|volatile|delegate|internal|short|void|do|is|sizeof|while|double|lock|stackalloc|else|long|static|enum|namespace|string|var|dynamic",
+                                    "constant.language": "null|true|false"
+                                }, "identifier");
+
+
+                                mapperRule.token = keywordMapper;
+                                mapperRule.onMatch = keywordMapper;
+                            }
+                        }
+                    }
+
+                    session.$mode.$tokenizer = null;
+                    session.bgTokenizer.setTokenizer(session.$mode.getTokenizer());
+                    session.bgTokenizer.start(0);
+                });
+
                 ace.config.loadModule('ace/ext/language_tools', function () {
                     var variablesCompletor =
                     {
-                        getCompletions: function(editor, session, pos, prefix, callback) {
+                        getCompletions: function (editor, session, pos, prefix, callback) {
                             var i = 0;
 
-                            var variables =                             
-                            _.orderBy(self.treeStore.getChapter.variableNames, 'name', 'desc')                                
-                                .map(function(variable) {
-                                    return {
-                                        name: variable.name,
-                                        value: variable.name,
-                                        score: i++,
-                                        meta: variable.type
-                                    }
-                                });
+                            var variables =
+                                _.orderBy(self.treeStore.getChapter.variableNames, 'name', 'desc')
+                                    .map(function (variable) {
+                                        return {
+                                            name: variable.name,
+                                            value: variable.name,
+                                            score: i++,
+                                            meta: variable.type
+                                        }
+                                    });
                             callback(null, variables);
                         },
 
-                        identifierRegexps : [/[@a-zA-Z_0-9\$\-\u00A2-\uFFFF]/]
+                        identifierRegexps: [/[@a-zA-Z_0-9\$\-\u00A2-\uFFFF]/]
                     };
 
-                    addCompleter(variablesCompletor);
+                    setCompleters([variablesCompletor]);
 
                     editor.setOptions({
                         enableBasicAutocompletion: true,
