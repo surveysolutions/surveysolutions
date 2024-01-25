5d310e34dcd92b3c5813e95483de0e2dfe40f621
V-B <vitaliy.balabanov@gmail.com>
2024-01-24 20:00:00 -0500

    fix negative option values invariantly to culture

--- src/Core/SharedKernels/Enumerator/Enumerator/Implementation/Services/OptionsRepository.cs
+++ src/Core/SharedKernels/Enumerator/Enumerator/Implementation/Services/OptionsRepository.cs
@@ -311,8 +311,8 @@ namespace WB.Core.SharedKernels.Enumerator.Implementation.Services
             var answers = options.Select(a => new Answer()
             {
                 AnswerText = a.Text,
-                ParentValue = a.ParentId.ToString(),
-                AnswerValue = a.Id.ToString(),
+                ParentValue = a.ParentId?.ToString(CultureInfo.InvariantCulture),
+                AnswerValue = a.Id.ToString(CultureInfo.InvariantCulture),
                 AttachmentName = a.AttachmentName,
             }).ToList();
             StoreOptionsImpl(questionnaireIdentity, null, categoryId, answers, translations);
