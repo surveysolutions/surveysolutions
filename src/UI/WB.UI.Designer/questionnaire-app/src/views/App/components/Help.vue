c829497b92fd05d55ffcba8c05e4f6ed1105731f
Pavel Stovpivskyi <spuvik@gmail.com>
2024-02-13 16:36:08 +0100

    changed root tag

--- src/UI/WB.UI.Designer/questionnaire-app/src/views/App/components/Help.vue
+++ src/UI/WB.UI.Designer/questionnaire-app/src/views/App/components/Help.vue
@@ -1,10 +1,10 @@
 <template>
-    <span ref="tooltipBoundary">
+    <div ref="tooltipBoundary" style="display: inline;">
         <span class="help-bracket">(</span>
         <a ref="tooltipTrigger" data-bs-toggle="tooltip" class="tooltip-help-trigger" href="javascript:void(0);"
             tabindex="-1">?</a>
         <span class="help-bracket">)</span>
-    </span>
+    </div>
 </template>
   
 <script>
