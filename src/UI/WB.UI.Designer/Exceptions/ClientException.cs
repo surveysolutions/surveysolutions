7c38e6c85710bcf51e101e2011f244f64e6bbdc5
Pavel Stovpivskyi <spuvik@gmail.com>
2024-02-14 18:56:55 +0100

    fix error handling

--- src/UI/WB.UI.Designer/Exceptions/ClientException.cs
+++ src/UI/WB.UI.Designer/Exceptions/ClientException.cs
@@ -7,13 +7,15 @@ public class ClientException : Exception
 {
     public ClientException(ClientErrorModel errorModel) : base(errorModel.Message)
     {
-        Source  = errorModel.AdditionalData["component"];
+        if (errorModel.AdditionalData.TryGetValue("component", out var component))
+            Source = component;
             
         foreach (var key in errorModel.AdditionalData.Keys)
         {
             Data[key] = errorModel.AdditionalData[key];
         }
             
-        ExceptionDispatchInfo.SetRemoteStackTrace(this, errorModel.AdditionalData["stack"]);
+        if (errorModel.AdditionalData.TryGetValue("stack", out var stack))
+            ExceptionDispatchInfo.SetRemoteStackTrace(this, stack);
     }
 }
