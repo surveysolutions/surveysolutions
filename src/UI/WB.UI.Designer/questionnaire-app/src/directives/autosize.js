9aca39e001317ed875402971748fe29b75cda5d6
Pavel Stovpivskyi <spuvik@gmail.com>
2024-02-19 18:04:02 +0100

    Scroll jumps up while editing of some long comment.

--- src/UI/WB.UI.Designer/questionnaire-app/src/directives/autosize.js
+++ src/UI/WB.UI.Designer/questionnaire-app/src/directives/autosize.js
@@ -1,37 +1,15 @@
-const minHeight = 21;
-const minHeightPx = minHeight + 'px';
+import { attach } from '@frsource/autoresize-textarea';
 
 const autosize = app => {
     app.directive('autosize', {
         mounted(el) {
-            el.style.overflow = 'hidden';
-            el.style.resize = 'none';
-            el.style.boxSizing = 'border-box';
             el.rows = 1;
 
-            const calculateHeight = () => {
-                el.style.height = 'auto';
-                const newHeight =
-                    el.scrollHeight + el.offsetHeight - el.clientHeight;
-                var calculatedHeight =
-                    newHeight < minHeight ? minHeightPx : newHeight + 'px';
-
-                if (el.style.height !== calculatedHeight)
-                    el.style.height = calculatedHeight;
-            };
-
-            el.calculateHeight = calculateHeight;
-
-            requestAnimationFrame(() => {
-                calculateHeight();
-            });
-
-            calculateHeight();
-
-            el.addEventListener('input', calculateHeight);
+            const { detach } = attach(el);
+            el.detachAutosize = detach;
         },
         unmounted(el) {
-            el.removeEventListener('input', el.calculateHeight);
+            el.detachAutosize();
         }
     });
 };
