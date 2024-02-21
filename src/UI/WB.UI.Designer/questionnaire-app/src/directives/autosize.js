42e7ace89347c0543a226133b121c9b93059d4bc
Pavel Stovpivskyi <spuvik@gmail.com>
2024-02-21 14:09:47 +0100

    Extra scroll is displayed on TEXT VIEW of Categorical, Numeric questions and Fixed rosters.

--- src/UI/WB.UI.Designer/questionnaire-app/src/directives/autosize.js
+++ src/UI/WB.UI.Designer/questionnaire-app/src/directives/autosize.js
@@ -3,6 +3,9 @@ import { attach } from '@frsource/autoresize-textarea';
 const autosize = app => {
     app.directive('autosize', {
         mounted(el) {
+            el.style.overflow = 'hidden';
+            el.style.resize = 'none';
+            el.style.boxSizing = 'border-box';
             el.rows = 1;
 
             const { detach } = attach(el);
