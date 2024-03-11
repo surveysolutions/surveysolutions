5373b0f44337fc7fafda9c7967b547d3f975c467
Pavel Stovpivskyi <spuvik@gmail.com>
2024-03-11 13:49:16 +0100

    fixed: Cannot read properties of undefined (reading 'backdrop')

--- src/UI/WB.UI.Designer/questionnaire-app/src/views/App/components/Tree.vue
+++ src/UI/WB.UI.Designer/questionnaire-app/src/views/App/components/Tree.vue
@@ -150,7 +150,7 @@
 import { useTreeStore } from '../../../stores/tree';
 import { Draggable, dragContext, walkTreeData } from '@he-tree/vue';
 import '@he-tree/vue/style/default.css';
-import { ref, nextTick, computed } from 'vue';
+import { ref } from 'vue';
 import SearchDialog from './SearchDialog.vue';
 import SearchForQuestion from './SearchForQuestion.vue';
 
@@ -451,7 +451,7 @@ export default {
         },
         async showSearch() {
             this.search.open = true;
-            await nextTick();
+            await this.$nextTick();
             document.getElementById('chapterSearch').focus();
         },
         hideSearch() {
