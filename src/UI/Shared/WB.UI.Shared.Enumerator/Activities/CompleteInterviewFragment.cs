6fd360b5e3122ba7ea1a18855ecd0d7b81a4669a
Pavel Stovpivskyi <spuvik@gmail.com>
2024-04-24 17:50:50 +0200

    removed commen #2989

--- src/UI/Shared/WB.UI.Shared.Enumerator/Activities/CompleteInterviewFragment.cs
+++ src/UI/Shared/WB.UI.Shared.Enumerator/Activities/CompleteInterviewFragment.cs
@@ -12,21 +12,5 @@ namespace WB.UI.Shared.Enumerator.Activities
     public class CompleteInterviewFragment : BaseFragment<CompleteInterviewViewModel>
     {
         protected override int ViewResourceId => Resource.Layout.interview_complete;
-
-        /*public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
-        {
-            this.EnsureBindingContextIsSet(inflater);
-            //base.OnCreateView(inflater, container, savedInstanceState);
-            //var view = base.OnCreateView(inflater, container, savedInstanceState);
-            var view = this.BindingInflate(ViewResourceId, container, false);
-            
-            var adapter = new CompleteInformationAdapter(Context, (IMvxAndroidBindingContext)base.BindingContext);
-            //var expandableListView = view.FindViewById<MvxExpandableListView>(Resource.Id.tv_Complete_Groups_Information);
-            var expandableListView = view.FindViewById<AutosizeExpandableListView>(Resource.Id.tv_Complete_Groups_Information);
-
-            expandableListView.SetAdapter(adapter);
-
-            return view;
-        }*/
     }
 }
