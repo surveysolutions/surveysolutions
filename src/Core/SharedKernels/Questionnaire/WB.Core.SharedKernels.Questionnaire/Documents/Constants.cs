b6fc2dd82b90056f70c7da45380d14b17e5ddfae
Pavel Stovpivskyi <spuvik@gmail.com>
2024-05-15 19:36:12 +0200

    changed Max number of items in rosters to 200

--- src/Core/SharedKernels/Questionnaire/WB.Core.SharedKernels.Questionnaire/Documents/Constants.cs
+++ src/Core/SharedKernels/Questionnaire/WB.Core.SharedKernels.Questionnaire/Documents/Constants.cs
@@ -3,7 +3,7 @@
     public class Constants
     {
         public static readonly int MaxAmountOfItemsInLongRoster = 30;
-        public static readonly int MaxRosterRowCount = 60;
+        public static readonly int MaxRosterRowCount = 200;
         public static readonly int MaxLongRosterRowCount = 200;
         public static readonly int MaxMultiComboboxAnswersCount = 200;
         public static readonly int MinLongRosterRowCount = 1;
