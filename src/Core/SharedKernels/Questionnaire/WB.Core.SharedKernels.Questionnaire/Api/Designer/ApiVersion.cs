94b8d673285d553420612c119e848e16f4285554
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2024-06-03 11:03:27 -0400

    update protocol version for tester

--- src/Core/SharedKernels/Questionnaire/WB.Core.SharedKernels.Questionnaire/Api/Designer/ApiVersion.cs
+++ src/Core/SharedKernels/Questionnaire/WB.Core.SharedKernels.Questionnaire/Api/Designer/ApiVersion.cs
@@ -2,7 +2,7 @@ namespace WB.Core.SharedKernels.SurveySolutions.Api.Designer
 {
     public static class ApiVersion
     {
-        public const int CurrentTesterProtocolVersion = 59; // 23.09.4 release
+        public const int CurrentTesterProtocolVersion = 60; // 23.09.4 release
         public const int MaxQuestionnaireVersion = 36;
     }
 }
