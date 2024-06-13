5f42adf5a1d6970553e436f8c1de499d025d2b1c
Pavel Stovpivskyi <spuvik@gmail.com>
2024-06-13 14:56:07 +0200

    removed same angular libs #3310

--- src/UI/WB.UI.Designer/gulpfile.js
+++ src/UI/WB.UI.Designer/gulpfile.js
@@ -1,32 +1,34 @@
-const { series, parallel, watch } = require("gulp");
-const path = require("path");
+const { series, parallel, watch } = require('gulp');
+const path = require('path');
 
-const vendor = require("./build/vendor");
-const questionnaire = require("./build/questionnaire");
-const bundler = require("./build/bundler");
-const vue = require("./build/vue");
+const vendor = require('./build/vendor');
+//const questionnaire = require("./build/questionnaire");
+const bundler = require('./build/bundler');
+const vue = require('./build/vue');
 
-const config = require("./build/config");
-const rimraf = require("rimraf");
-const helper = require("./build/plugins/helpers");
+const config = require('./build/config');
+const rimraf = require('rimraf');
+const helper = require('./build/plugins/helpers');
 
-const cleanup = done => rimraf(path.join(config.dist, "**/*"), done);
+const cleanup = (done) => rimraf(path.join(config.dist, '**/*'), done);
 
 module.exports = Object.assign(
-  helper.flatten(
-    {
-      vendor,
-      questionnaire,
-      bundler, 
-      vue
-    }, ":", true
-  ),
-  {
-    default: series(
-      cleanup,
-      parallel(vendor.default, bundler.default, vue.default),
-      questionnaire.default
+    helper.flatten(
+        {
+            vendor,
+            //questionnaire,
+            bundler,
+            vue,
+        },
+        ':',
+        true
     ),
-    cleanup
-  }
+    {
+        default: series(
+            cleanup,
+            parallel(vendor.default, bundler.default, vue.default)
+            //questionnaire.default
+        ),
+        cleanup,
+    }
 );
