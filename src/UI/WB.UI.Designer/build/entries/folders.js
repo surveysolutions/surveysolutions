601dc2994aa9e0e9e0b5be318451fda70cbd0ec5
Pavel Stovpivskyi <spuvik@gmail.com>
2024-06-25 15:45:18 +0200

    try to inject styles via vite

---
+++ src/UI/WB.UI.Designer/build/entries/folders.js
@@ -0,0 +1,6 @@
+import '../../node_modules/jquery.fancytree/dist/jquery.fancytree-all-deps.js';
+import '../../questionnaire/external/jquery.fancytree.contextMenu.js';
+import '../../node_modules/jquery-contextmenu/dist/jquery.contextMenu.js';
+import '../../Scripts/custom/public-folders.js';
+import '../../node_modules/jquery.fancytree/dist/skin-bootstrap/ui.fancytree.min.css';
+import '../../node_modules/jquery-contextmenu/dist/jquery.contextMenu.min.css';
