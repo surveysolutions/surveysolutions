e2e5b0e8f23d612c6cf3a3741c2273c83fdfb891
Pavel Stovpivskyi <spuvik@gmail.com>
2024-06-27 16:36:54 +0200

    updated HotChocolate to version 13.9.5

--- src/UI/WB.UI.Headquarters.Core/Controllers/Api/PublicApi/Graphql/GraphQLIntegration.cs
+++ src/UI/WB.UI.Headquarters.Core/Controllers/Api/PublicApi/Graphql/GraphQLIntegration.cs
@@ -72,7 +72,9 @@ namespace WB.UI.Headquarters.Controllers.Api.PublicApi.Graphql
                 .AddConvention<INamingConventions>(new CompatibilityNamingConvention())
                 .BindRuntimeType<string, CustomStringOperationFilterInput>()
                 .BindRuntimeType<IdentifyEntityValue, IdentifyEntityValueFilterInput>()
+                .AddType<IdentifyEntityValueFilterInput>()
                 .BindRuntimeType<QuestionnaireCompositeItem, QuestionnaireItemsFilterType>()
+                .AddType<QuestionnaireItemsFilterType>()
                 .AddSorting();
         }
 
