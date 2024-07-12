b3020923b710bad206d6ef7143ebc209a5a3a865
Pavel Stovpivskyi <spuvik@gmail.com>
2024-07-12 15:37:27 +0200

    same changes in scripts

--- src/UI/WB.UI.Designer/build/entries/list.js
+++ src/UI/WB.UI.Designer/build/entries/list.js
@@ -15,10 +15,25 @@ if (lists.length > 0) {
     );
 }
 
-import '/node_modules/moment/min/moment-with-locales.js';
-import '/node_modules/perfect-scrollbar/dist/css/perfect-scrollbar.css';
+import moment from 'moment';
+window.moment = moment;
+//import '/node_modules/perfect-scrollbar/dist/css/perfect-scrollbar.css';
 import '/questionnaire/content/designer-start/bootstrap-custom.less';
 import '/Styles/designer-list.less';
 import '/Scripts/custom/common.js';
 
-//export default $;
+$(function () {
+    if (window.moment) {
+        moment.locale('@CultureInfo.CurrentUICulture');
+
+        $('time').each(function () {
+            var me = $(this);
+            var date = me.attr('datetime') || me.attr('time');
+            var momentDate = moment
+                .utc(date)
+                .local()
+                .format('MMM DD, YYYY HH:mm');
+            me.text(momentDate);
+        });
+    }
+});
--- src/UI/WB.UI.Designer/build/entries/validation.js
+++ src/UI/WB.UI.Designer/build/entries/validation.js
@@ -1,3 +1,5 @@
 //"node_modules",
-import '/node_modules/jquery-validation/dist/jquery.validate.js';
-import '/node_modules/jquery-validation-unobtrusive/dist/jquery.validate.unobtrusive.js';
+//import '/node_modules/jquery-validation/dist/jquery.validate.js';
+import 'jquery-validation';
+//import '/node_modules/jquery-validation-unobtrusive/dist/jquery.validate.unobtrusive.js';
+import 'jquery-validation-unobtrusive';
