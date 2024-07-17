36f5adfc745a9810fdda68e1de5e835e11bd7a04
Pavel Stovpivskyi <spuvik@gmail.com>
2024-07-17 14:09:00 +0200

    perfect scrollbar

--- src/UI/WB.UI.Designer/build/entries/list.js
+++ src/UI/WB.UI.Designer/build/entries/list.js
@@ -1,24 +1,29 @@
 //import '/node_modules/jquery/dist/jquery.js';
 //import $ from '/node_modules/jquery/dist/jquery.js';
 import $ from 'jquery';
-window.jQuery = window.$ = $;
+
 import '/node_modules/jquery-mousewheel/jquery.mousewheel.js';
 //import '/node_modules/perfect-scrollbar/dist/perfect-scrollbar.js';
-//import 'perfect-scrollbar';
-import perfectScrollbar from 'perfect-scrollbar';
+import PerfectScrollbar from 'perfect-scrollbar';
+//import perfectScrollbar from 'perfect-scrollbar';
 //import perfectScrollbar from '/node_modules/perfect-scrollbar/dist/perfect-scrollbar.js';
 //$.perfectScrollbar = perfectScrollbar();
-const lists = $('#table-content-holder > .scroller-container');
-if (lists.length > 0) {
+//const lists = $('#table-content-holder > .scroller-container');
+//lists.perfectScrollbar();
+//const container = document.querySelector('#container');
+
+/*if (lists.length > 0) {
     perfectScrollbar.initialize(
         $('#table-content-holder > .scroller-container')[0]
     );
-}
+}*/
+
+window.jQuery = window.$ = $;
 
 import moment from 'moment';
 window.moment = moment;
 
-import '/node_modules/perfect-scrollbar/dist/css/perfect-scrollbar.css';
+import '/node_modules/perfect-scrollbar/css/perfect-scrollbar.css';
 import '/questionnaire/content/designer-start/bootstrap-custom.less';
 import '/Styles/designer-list.less';
 import '/Scripts/custom/common.js';
@@ -26,6 +31,13 @@ import '/Scripts/custom/common.js';
 import './simplepage';
 
 $(function () {
+    const lists = $('#table-content-holder > .scroller-container');
+    if (lists.length > 0) {
+        const ps = new PerfectScrollbar(
+            '#table-content-holder > .scroller-container'
+        );
+    }
+
     if (window.moment) {
         moment.locale('@CultureInfo.CurrentUICulture');
 
--- src/UI/WB.UI.Designer/build/entries/validation.js
+++ src/UI/WB.UI.Designer/build/entries/validation.js
@@ -1,3 +1,6 @@
+import 'jquery';
+//import $ from 'jquery';
+//window.jQuery = window.$ = $;
 //"node_modules",
 //import '/node_modules/jquery-validation/dist/jquery.validate.js';
 import 'jquery-validation';
