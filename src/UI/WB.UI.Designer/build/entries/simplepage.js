f05a87d13f769426048d14f972b67bf131833a55
Pavel Stovpivskyi <spuvik@gmail.com>
2024-07-19 15:44:03 +0200

    errors handler
    # Conflicts:
    #	src/UI/WB.UI.Designer/build/entries/simplepage.js

--- src/UI/WB.UI.Designer/build/entries/simplepage.js
+++ src/UI/WB.UI.Designer/build/entries/simplepage.js
@@ -1,7 +1,9 @@
+import { setupErrorHandler } from './errorsHandler';
+setupErrorHandler();
+
 import $ from 'jquery';
 window.jQuery = window.$ = $;
 
-//import '/node_modules/bootstrap/dist/js/bootstrap.js';
 import 'bootstrap';
 import '/Scripts/custom/utils.js';
 
