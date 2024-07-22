e8cfb05e0e3972b684d0a2678b8eaa8b5a8e9375
Pavel Stovpivskyi <spuvik@gmail.com>
2024-07-22 16:09:16 +0200

    moved validate to simplepage

--- src/UI/WB.UI.Designer/build/entries/validation.js
+++ src/UI/WB.UI.Designer/build/entries/validation.js
@@ -1,8 +1,2 @@
-//import 'jquery';
-//import $ from 'jquery';
-//window.jQuery = window.$ = $;
-//"node_modules",
-//import '/node_modules/jquery-validation/dist/jquery.validate.js';
 import 'jquery-validation';
-//import '/node_modules/jquery-validation-unobtrusive/dist/jquery.validate.unobtrusive.js';
 import 'jquery-validation-unobtrusive';
