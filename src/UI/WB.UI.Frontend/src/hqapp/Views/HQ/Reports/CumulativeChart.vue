c8d4ce3fc7925705738e14ba2128e055939c759b
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2024-08-15 22:01:38 -0400

    charts migration

--- src/UI/WB.UI.Frontend/src/hqapp/Views/HQ/Reports/CumulativeChart.vue
+++ src/UI/WB.UI.Frontend/src/hqapp/Views/HQ/Reports/CumulativeChart.vue
@@ -1,10 +1,69 @@
 <template>
-    <Line :data="chartData" />
+    <div class="interviewChart">
+        <Line :data="chartData" :options="chartOptions" />
+    </div>
 </template>
 
 <script>
 
 const chartOptions = {
+    elements: {
+        point: { radius: 0 },
+        line: { fill: true, tension: 0 },
+    },
+    responsive: true,
+    maintainAspectRatio: false,
+    tooltips: {
+        mode: 'x',
+        intersect: false,
+    },
+    hover: {
+        mode: 'index',
+        intersect: false,
+    },
+
+    scales: {
+        x:
+        {
+            type: 'time',
+            gridLines: {
+                display: true,
+                tickMarkLength: 10,
+            },
+
+            ticks: {
+                source: 'data',
+                autoSkipPadding: 10,
+                maxRotation: 45,
+                autoSkip: true,
+            },
+
+        },
+
+        y:
+        {
+            afterDataLimits: function (axis) {
+                axis.max += 1 // add 1px to top
+                axis.min = 0
+            },
+            type: 'linear',
+            stacked: true,
+            beginAtZero: true,
+            ticks: {
+
+                callback: function (label, index, labels) {
+                    // when the floored value is the same as the value we have a whole number
+                    if (Math.floor(label) === label) {
+                        return label
+                    }
+                },
+            },
+        },
+    },
+}
+
+
+const chartOptions1 = {
     chartId: 'interviewChart',
     elements: {
         point: { radius: 0 },
@@ -70,9 +129,10 @@ const chartOptions = {
 }
 
 import { Line } from 'vue-chartjs'
-import { Chart, LineController, LineElement, PointElement, LinearScale, Title, CategoryScale } from 'chart.js'
+import 'chartjs-adapter-moment'
+import { Chart, LineController, LineElement, PointElement, LinearScale, Title, CategoryScale, TimeScale } from 'chart.js'
 
-Chart.register(LineController, LineElement, PointElement, LinearScale, Title, CategoryScale);
+Chart.register(LineController, LineElement, PointElement, LinearScale, Title, CategoryScale, TimeScale);
 
 
 export default {
