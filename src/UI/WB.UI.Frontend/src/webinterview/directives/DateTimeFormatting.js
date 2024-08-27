8796cf05553ac3c7b3d9d89adaf181610059a792
Pavel Stovpivskyi <spuvik@gmail.com>
2024-08-27 19:08:19 +0200

    date time formatting directive

--- src/UI/WB.UI.Frontend/src/webinterview/directives/DateTimeFormatting.js
+++ src/UI/WB.UI.Frontend/src/webinterview/directives/DateTimeFormatting.js
@@ -1,23 +1,30 @@
-import { DateFormats } from '~/shared/helpers'
-import { forEach } from 'lodash'
-import moment from 'moment'
+import { DateFormats } from '~/shared/helpers';
+import { forEach } from 'lodash';
+import moment from 'moment';
 
-export function registerDateTimeFormatting(vue) {
+export function registerDateTimeFormatting(app) {
+    app.directive('dateTimeFormatting', {
+        mounted(el) {
+            formatTimeElements(el);
+        },
+        updated(el) {
+            formatTimeElements(el);
+        }
+    });
+}
 
-    vue.directive('dateTimeFormatting', (el) => {
-        const timeElements = el.getElementsByTagName('time')
+function formatTimeElements(el) {
+    const timeElements = el.getElementsByTagName('time');
 
-        forEach(timeElements, timeElement => {
-            const dateTimeAttr = timeElement.getAttribute('datetime')
+    forEach(timeElements, (timeElement) => {
+        const dateTimeAttr = timeElement.getAttribute('datetime');
 
-            if (dateTimeAttr) {
-                const dateTime = new Date(dateTimeAttr)
-                timeElement.innerHTML = moment(dateTime).format(DateFormats.dateTime)
-            }
-            else {
-                const date = timeElement.getAttribute('date')
-                timeElement.innerHTML = moment(date).format(DateFormats.date)
-            }
-        })
-    })
+        if (dateTimeAttr) {
+            const dateTime = new Date(dateTimeAttr);
+            timeElement.innerHTML = moment(dateTime).format(DateFormats.dateTime);
+        } else {
+            const date = timeElement.getAttribute('date');
+            timeElement.innerHTML = moment(date).format(DateFormats.date);
+        }
+    });
 }
