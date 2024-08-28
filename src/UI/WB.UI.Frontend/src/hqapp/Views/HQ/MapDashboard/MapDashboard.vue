6ab19e633a836c3db96ad1a88f6ebf53ddd3e6d2
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2024-08-28 17:14:08 -0400

    update maps refs and params

--- src/UI/WB.UI.Frontend/src/hqapp/Views/HQ/MapDashboard/MapDashboard.vue
+++ src/UI/WB.UI.Frontend/src/hqapp/Views/HQ/MapDashboard/MapDashboard.vue
@@ -198,7 +198,7 @@
                 <div class="form-group">
                     <label class="control-label" for="newResponsibleId">{{
                         $t('Assignments.SelectResponsible')
-                    }}</label>
+                        }}</label>
                     <Typeahead control-id="newResponsibleId" :placeholder="$t('Common.Responsible')"
                         :value="newResponsibleId" :ajax-params="{}" @selected="newResponsibleSelected"
                         :fetch-url="model.responsible"></Typeahead>
@@ -961,7 +961,7 @@ export default {
                 }
             })
 
-            google.maps.event.addDomListener(mapDiv, 'click', (event) => {
+            google.maps.event.addEventListener(mapDiv, 'click', (event) => {
                 if (event.srcElement.nodeName == 'BUTTON') {
                     var methodName = event.srcElement.getAttribute('click')
                     if (methodName) self[methodName].call(self)
