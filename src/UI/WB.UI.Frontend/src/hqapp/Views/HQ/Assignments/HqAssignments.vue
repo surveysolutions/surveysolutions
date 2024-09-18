df4ba52efcc3f8b2285d283df4dbfafa462d4fc9
Pavel Stovpivskyi <spuvik@gmail.com>
2024-09-18 13:09:53 +0200

    This field must be a valid number' error message does not show up immediately after entering invalid value

--- src/UI/WB.UI.Frontend/src/hqapp/Views/HQ/Assignments/HqAssignments.vue
+++ src/UI/WB.UI.Frontend/src/hqapp/Views/HQ/Assignments/HqAssignments.vue
@@ -166,7 +166,8 @@
                     <Field type="text" class="form-control" v-model.trim="editedQuantity" name="editedQuantity"
                         :rules="validateQuantity" :data-vv-as="$t('Assignments.Expected')" maxlength="5"
                         autocomplete="off" @keyup.enter="updateQuantity" id="newQuantity" placeholder="1"
-                        :disabled="!canEditQuantity" />
+                        :disabled="!canEditQuantity" :validateOnBlur="true" :validateOnChange="true"
+                        :validateOnInput="true" />
                     <span class="text-danger">
                         <ErrorMessage name="editedQuantity" />
                     </span>
