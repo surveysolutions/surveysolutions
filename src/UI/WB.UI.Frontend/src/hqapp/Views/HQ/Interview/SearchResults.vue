e0c1768b2a10d6fa1b1fe6bf3cf9875ea537633a
Pavel Stovpivskyi <spuvik@gmail.com>
2024-10-02 17:17:36 +0200

    removed debugger

--- src/UI/WB.UI.Frontend/src/hqapp/Views/HQ/Interview/SearchResults.vue
+++ src/UI/WB.UI.Frontend/src/hqapp/Views/HQ/Interview/SearchResults.vue
@@ -49,7 +49,6 @@ export default {
         },
 
         async infiniteHandler($state) {
-            debugger
             const self = this
 
             await this.$store.dispatch('fetchSearchResults')
