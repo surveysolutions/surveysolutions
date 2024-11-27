b52cf953f4a58cbd91041ad176442b504b83bd8a
Pavel Stovpivskyi <spuvik@gmail.com>
2024-11-27 10:09:44 +0100

    added popup menu

--- src/UI/Shared/WB.UI.Shared.Extensions/Activities/AssignmentMapActivity.cs
+++ src/UI/Shared/WB.UI.Shared.Extensions/Activities/AssignmentMapActivity.cs
@@ -4,10 +4,12 @@ using Android.OS;
 using Android.Views;
 using AndroidX.AppCompat.Widget;
 using Esri.ArcGISRuntime.UI.Controls;
+using MvvmCross.WeakSubscription;
 using WB.Core.SharedKernels.Enumerator.Properties;
 using WB.UI.Shared.Enumerator.Activities;
 using WB.UI.Shared.Enumerator.Services;
 using WB.UI.Shared.Extensions.ViewModels;
+using PopupMenu = AndroidX.AppCompat.Widget.PopupMenu;
 using Toolbar = AndroidX.AppCompat.Widget.Toolbar;
 
 namespace WB.UI.Shared.Extensions.Activities;
@@ -26,14 +28,124 @@ public class AssignmentMapActivity : MarkersMapActivity<AssignmentMapViewModel, 
         this.ViewModel.NavigateToDashboardCommand.Execute();
         this.Finish();
     }
+    
+    private MvxWeakEventSubscription<ImageButton> clickedMenuButton;
+    private MvxWeakEventSubscription<ImageButton> clickedEyeButton;
 
     protected override void OnCreate(Bundle savedInstanceState)
     {
         base.OnCreate(savedInstanceState);
 
         this.ViewModel.MapView = this.FindViewById<MapView>(Resource.Id.map_view);
+        
+        var buttonMenu = this.FindViewById<ImageButton>(Resource.Id.butMenu);
+        clickedMenuButton = buttonMenu.WeakSubscribe(nameof(buttonMenu.Click), this.ClickedMenuButton);
+        
+        var buttonEye = this.FindViewById<ImageButton>(Resource.Id.butEye);
+        clickedEyeButton = buttonEye.WeakSubscribe(nameof(buttonEye.Click), this.ClickedEyeButton);
     }
     
+    public class CustomMenuItem
+    {
+        public CustomMenuItem()
+        {
+        }
+
+        public CustomMenuItem(string title, Action action): this()
+        {
+            Title = title;
+            Action = action;
+        }
+
+        public CustomMenuItem(string title, Action action, int? iconResId) : this(title, action)
+        {
+            IconResId = iconResId;
+        }
+
+        public string Title { get; set; }
+        public Action Action { get; set; }
+        public int? IconResId { get; set; }
+    }
+
+    private void ClickedEyeButton(object sender, EventArgs e)
+    {
+        ShowPopupMenu((ImageButton)sender, [
+            new CustomMenuItem("Zoom all map", () => this.ViewModel.ShowFullMapCommand.Execute()),
+            new CustomMenuItem("Zoom all collected data", () => this.ViewModel.ShowAllItemsCommand.Execute()),
+            new CustomMenuItem("Zoom all tracking data", () => this.ViewModel.ShowFullMapCommand.Execute())
+        ]);
+    }
+
+    private void ClickedMenuButton(object sender, EventArgs e)
+    {
+        ShowPopupMenu((ImageButton)sender, [
+            new CustomMenuItem("Change Map", () => this.ViewModel.LoadShapefile.Execute()),
+            //new CustomMenuItem("Change Boundaries", () => this.ViewModel.LoadShapefile.Execute()),
+            new CustomMenuItem(UIResources.MenuItem_Title_GeoTracking, () => this.ViewModel.StartGeoTrackingCommand.Execute()),
+            new CustomMenuItem(UIResources.MenuItem_Title_Geofencing, () => this.ViewModel.StartGeofencingCommand.Execute()),
+            new CustomMenuItem("Exit to dashboard", () => this.ViewModel.NavigateToDashboardCommand.Execute()),
+        ]);
+    }
+
+    private void ShowPopupMenu(View anchorView, CustomMenuItem[] menuActions)
+    {
+        var context = new ContextThemeWrapper(this, Resource.Style.CustomPopupMenu);
+        PopupMenu popupMenu = new PopupMenu(context, anchorView);
+        
+        for (int i = 0; i < menuActions.Length; i++)
+        {
+            var customMenuItem = menuActions[i];
+            var menuItem = popupMenu.Menu.Add(0, i, i, customMenuItem.Title);
+            if (customMenuItem.IconResId.HasValue)
+                menuItem.SetIcon(customMenuItem.IconResId.Value);
+            
+            /*var menuItem = popupMenu.Menu.GetItem(i);
+            
+            var layoutInflater = LayoutInflater.From(this);
+            var customView = layoutInflater.Inflate(Resource.Layout.menu_item_custom, null);
+
+
+            var textView = customView.FindViewById<TextView>(Resource.Id.menu_item_text);
+            var imageView = customView.FindViewById<ImageView>(Resource.Id.menu_item_icon);
+            
+            textView.Text = menuActions[i].ToString() + menuActions[i].ToString();
+            imageView.SetImageResource(Resource.Drawable.icon_button_default);
+            
+            menuItem.SetActionView(customView);*/
+        }
+
+        popupMenu.WeakSubscribe<PopupMenu, PopupMenu.MenuItemClickEventArgs>(nameof(popupMenu.MenuItemClick), (s, args) =>
+        {
+            var menuItem = menuActions[args.Item.ItemId];
+            menuItem.Action.Invoke();
+        });
+        
+        ForceShowIcons(popupMenu);
+        popupMenu.Show();
+    }
+    
+    private void ForceShowIcons(PopupMenu popupMenu)
+    {
+        try
+        {
+            var fields = popupMenu.Class.GetDeclaredFields();
+            foreach (var field in fields)
+            {
+                if (field.Name == "mPopup")
+                {
+                    field.Accessible = true;
+                    var mPopup = field.Get(popupMenu);
+                    var method = mPopup.Class.GetDeclaredMethod("setForceShowIcon", Java.Lang.Boolean.Type);
+                    method.Invoke(mPopup, true);
+                    break;
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error enabling icons in PopupMenu: {ex}");
+        }
+    }
 
     public override bool OnCreateOptionsMenu(IMenu menu)
     {
@@ -53,5 +165,19 @@ public class AssignmentMapActivity : MarkersMapActivity<AssignmentMapViewModel, 
             this.ViewModel.StartGeoTrackingCommand.Execute();
             
         return base.OnOptionsItemSelected(item);
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing)
+        {
+            clickedMenuButton?.Dispose();
+            clickedMenuButton = null;
+            
+            clickedEyeButton?.Dispose();
+            clickedEyeButton = null;
+        }
+        
+        base.Dispose(disposing);
     }
 }
