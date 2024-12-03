8f537d7afdedbfa909e1895271b922aad637182c
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2024-12-03 11:19:23 -0800

    finally fix await

--- src/UI/Shared/WB.UI.Shared.Enumerator/Services/AndroidPathUtils.cs
+++ src/UI/Shared/WB.UI.Shared.Enumerator/Services/AndroidPathUtils.cs
@@ -62,7 +62,7 @@ namespace WB.UI.Shared.Enumerator.Services
 
         public async Task<string> GetRootDirectoryAsync()
         {
-            return await GetPathToInternalDirectory();
+            return await Task.FromResult(GetPathToInternalDirectory());
         }
     }
 }
