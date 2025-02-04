16550cf9588ac071fd15dd8ef06a2d23692845a4
Pavel Stovpivskyi <spuvik@gmail.com>
2025-02-04 16:04:32 -0500

    removed commands to run inner app

--- build.all.deps.bat
+++ build.all.deps.bat
@@ -1,3 +1,3 @@
-start cmd /c "TITLE Designer Questionnaire && cd src\UI\WB.UI.Designer && npm install && npm run devb && cd .\questionnaire && npm install && npm run devb"
+start cmd /c "TITLE Designer Questionnaire && cd src\UI\WB.UI.Designer && npm install && npm run devb"
 call cmd  /c "TITLE hq deps && cd src\UI\WB.UI.Frontend && npm install && npm run build"
 
