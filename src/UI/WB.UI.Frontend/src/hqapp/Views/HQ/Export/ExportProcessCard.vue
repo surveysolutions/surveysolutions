9eb9582e1a43c648603897262a281ba39726280a
Pavel Stovpivskyi <spuvik@gmail.com>
2025-03-12 17:28:29 -0400

    show local time #3413

--- src/UI/WB.UI.Frontend/src/hqapp/Views/HQ/Export/ExportProcessCard.vue
+++ src/UI/WB.UI.Frontend/src/hqapp/Views/HQ/Export/ExportProcessCard.vue
@@ -224,7 +224,7 @@ export default {
         },
         formatDate(date) {
             if (date)
-                return moment(date).format(DateFormats.dateTimeInList)
+                return moment.utc(date).local().format(DateFormats.dateTimeInList)
             return null
         },
     },
