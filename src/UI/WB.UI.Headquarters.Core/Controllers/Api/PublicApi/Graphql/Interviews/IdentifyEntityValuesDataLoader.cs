908d9959274be7b53cb27270b190a4460ee1816f
Pavel Stovpivskyi <spuvik@gmail.com>
2025-03-27 17:55:58 -0400

    added fetch for preloading data #3445

--- src/UI/WB.UI.Headquarters.Core/Controllers/Api/PublicApi/Graphql/Interviews/IdentifyEntityValuesDataLoader.cs
+++ src/UI/WB.UI.Headquarters.Core/Controllers/Api/PublicApi/Graphql/Interviews/IdentifyEntityValuesDataLoader.cs
@@ -30,6 +30,7 @@ public class IdentifyEntityValuesDataLoader : BatchDataLoader<string, IReadOnlyL
         var questionAnswers = await unitOfWork.Session.Query<IdentifyEntityValue>()
             .Where(a => keys.Contains(a.InterviewSummary.SummaryId) && a.Identifying)
             .OrderBy(a => a.Position)
+            .Fetch(q => q.Entity)
             .ToListAsync(cancellationToken)
             .ConfigureAwait(false);
 
