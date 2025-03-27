33c7fc3b52bc348570acb34164302fd3763e3f0c
Pavel Stovpivskyi <spuvik@gmail.com>
2025-03-27 12:04:52 -0400

    fixed Workspace is ignored if it is listed in link #3788

--- src/UI/WB.UI.Headquarters.Core/Controllers/Api/PublicApi/Graphql/GraphQLIntegration.cs
+++ src/UI/WB.UI.Headquarters.Core/Controllers/Api/PublicApi/Graphql/GraphQLIntegration.cs
@@ -57,6 +57,11 @@ namespace WB.UI.Headquarters.Controllers.Api.PublicApi.Graphql
                     x.Use<WorkspaceGraphQlMiddleware>();
                 })
                 .AddAuthorization()
+                .ModifyOptions(o =>
+                {
+                    o.DefaultQueryDependencyInjectionScope = DependencyInjectionScope.Request;
+                    o.DefaultMutationDependencyInjectionScope = DependencyInjectionScope.Request;
+                })
                 .ModifyPagingOptions(o => { o.MaxPageSize = 200; })
                 .ModifyCostOptions(o => { o.EnforceCostLimits = false; })
                 .AddQueryType(x => x.Name("HeadquartersQuery"))
