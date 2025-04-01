510e980eac30861e66d89779cf7f7d0efc0fb25a
Pavel Stovpivskyi <spuvik@gmail.com>
2025-04-01 14:56:03 -0400

    added try catch #3445

--- src/UI/WB.UI.Headquarters.Core/Controllers/Api/PublicApi/Graphql/Interviews/QuestionnaireCompositeItemDataLoader.cs
+++ src/UI/WB.UI.Headquarters.Core/Controllers/Api/PublicApi/Graphql/Interviews/QuestionnaireCompositeItemDataLoader.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using GreenDonut;
+using Microsoft.AspNetCore.Http;
 using NHibernate.Linq;
+using StackExchange.Exceptional;
 using WB.Core.BoundedContexts.Headquarters.Views.Interview;
 using WB.Core.BoundedContexts.Headquarters.Views.Questionnaire;
 using WB.Infrastructure.Native.Storage.Postgre;
@@ -14,14 +16,17 @@ namespace WB.UI.Headquarters.Controllers.Api.PublicApi.Graphql.Interviews;
 public class QuestionnaireCompositeItemDataLoader : BatchDataLoader<int, QuestionnaireCompositeItem>
 {
     private readonly IUnitOfWork unitOfWork;
+    private readonly HttpContextAccessor httpContextAccessor;
 
     public QuestionnaireCompositeItemDataLoader(
         IBatchScheduler batchScheduler,
         IUnitOfWork unitOfWork,
-        DataLoaderOptions options = null)
+        DataLoaderOptions options = null,
+        HttpContextAccessor httpContextAccessor = null)
         : base(batchScheduler, options ?? new DataLoaderOptions(){ MaxBatchSize = 0 })
     {
         this.unitOfWork = unitOfWork;
+        this.httpContextAccessor = httpContextAccessor;
     }
 
     protected override bool AllowCachePropagation => false;
@@ -31,15 +36,25 @@ public class QuestionnaireCompositeItemDataLoader : BatchDataLoader<int, Questio
     protected override async Task<IReadOnlyDictionary<int, QuestionnaireCompositeItem>> LoadBatchAsync(
         IReadOnlyList<int> keys, CancellationToken cancellationToken)
     {
-        if (!unitOfWork.Session.IsOpen)
+        try
         {
-            throw new InvalidOperationException("GraphQL: session is closed before query execution.");
-        }
+            if (!unitOfWork.Session.IsOpen)
+            {
+                throw new InvalidOperationException("GraphQL: session is closed before query execution.");
+            }
 
-        var compositeItems = await unitOfWork.Session.Query<QuestionnaireCompositeItem>()
-            .Where(q => keys.Contains(q.Id))
-            .ToListAsync(cancellationToken);
+            var compositeItems = await unitOfWork.Session.Query<QuestionnaireCompositeItem>()
+                .Where(q => keys.Contains(q.Id))
+                .ToListAsync(cancellationToken);
         
-        return compositeItems.ToDictionary(x => x.Id);
+            return compositeItems.ToDictionary(x => x.Id);
+        }
+        catch (Exception e)
+        {
+            var httpContext = httpContextAccessor.HttpContext;
+            if (httpContext != null)
+                e.Log(httpContext);
+            throw new Exception("TEST: fail to read query entity", e);
+        }
     }
 }
