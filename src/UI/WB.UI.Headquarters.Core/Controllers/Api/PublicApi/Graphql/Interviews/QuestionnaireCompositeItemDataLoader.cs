b8d02a556600969aefdcc327286584fef0691758
Pavel Stovpivskyi <spuvik@gmail.com>
2025-04-01 16:18:09 -0400

    removed try catch #3445

--- src/UI/WB.UI.Headquarters.Core/Controllers/Api/PublicApi/Graphql/Interviews/QuestionnaireCompositeItemDataLoader.cs
+++ src/UI/WB.UI.Headquarters.Core/Controllers/Api/PublicApi/Graphql/Interviews/QuestionnaireCompositeItemDataLoader.cs
@@ -4,10 +4,7 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using GreenDonut;
-using Microsoft.AspNetCore.Http;
 using NHibernate.Linq;
-using StackExchange.Exceptional;
-using WB.Core.BoundedContexts.Headquarters.Views.Interview;
 using WB.Core.BoundedContexts.Headquarters.Views.Questionnaire;
 using WB.Infrastructure.Native.Storage.Postgre;
 
@@ -33,24 +30,16 @@ public class QuestionnaireCompositeItemDataLoader : BatchDataLoader<int, Questio
     protected override async Task<IReadOnlyDictionary<int, QuestionnaireCompositeItem>> LoadBatchAsync(
         IReadOnlyList<int> keys, CancellationToken cancellationToken)
     {
-        try
+        if (!unitOfWork.Session.IsOpen)
         {
-            if (!unitOfWork.Session.IsOpen)
-            {
-                throw new InvalidOperationException("GraphQL: session is closed before query execution.");
-            }
+            throw new InvalidOperationException("GraphQL: session is closed before query execution.");
+        }
 
-            var compositeItems = await unitOfWork.Session.Query<QuestionnaireCompositeItem>()
-                .Where(q => keys.Contains(q.Id))
-                .ToListAsync(cancellationToken)
-                .ConfigureAwait(false);
-        
-            return compositeItems.ToDictionary(x => x.Id);
-        }
-        catch (Exception e)
-        {
-            e.LogNoContext();
-            throw new Exception("TEST: fail to read query entity", e);
-        }
+        var compositeItems = await unitOfWork.Session.Query<QuestionnaireCompositeItem>()
+            .Where(q => keys.Contains(q.Id))
+            .ToListAsync(cancellationToken)
+            .ConfigureAwait(false);
+    
+        return compositeItems.ToDictionary(x => x.Id);
     }
 }
