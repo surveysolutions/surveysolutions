328d772811dd89993c6deaf31546be300d0c7a36
Pavel Stovpivskyi <spuvik@gmail.com>
2025-04-08 17:47:15 -0400

    added tests #3751

---
+++ src/Tests/WB.Tests.Unit/SharedKernels/DataCollection/InterviewTests/Answers/TextQuestionAnswersTests.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using Ncqrs.Spec;
+using NUnit.Framework;
+using WB.Core.SharedKernels.DataCollection;
+using WB.Core.SharedKernels.DataCollection.Events.Interview;
+using WB.Core.SharedKernels.DataCollection.Implementation.Aggregates;
+using WB.Core.SharedKernels.DataCollection.Implementation.Aggregates.InterviewEntities.Answers;
+using WB.Tests.Abc;
+
+namespace WB.Tests.Unit.SharedKernels.DataCollection.InterviewTests.Answers;
+
+public class TextQuestionAnswersTests : InterviewTestsContext
+{
+    readonly Guid questionId = Id.g1;
+    readonly Guid userId = Id.gA;
+    private StatefulInterview interview;
+
+    [SetUp]
+    public void Should_not_allow_removing_protected_answer()
+    {
+        var variableName = "text_variable";
+
+        var questionnaire = Create.Entity.QuestionnaireDocumentWithOneChapter(
+            Create.Entity.TextQuestion(questionId, variable: variableName));
+
+        interview = Create.AggregateRoot.StatefulInterview(shouldBeInitialized: false,
+            questionnaire: questionnaire);
+
+        var questionIdentity = Create.Identity(questionId);
+        var command = Create.Command.CreateInterview(
+            questionnaire.PublicKey, 1,
+            null, new List<InterviewAnswer>(),
+            userId);
+
+        interview.CreateInterview(command);
+    }
+
+    [Test]
+    public void When_answer_non_empty_text_answer_Should_allow()
+    {
+        using (EventContext eventContext = new EventContext())
+        {
+            interview.AnswerTextQuestion(userId, questionId, RosterVector.Empty, DateTime.UtcNow, "new answer");
+
+            eventContext.ShouldContainEvent<TextQuestionAnswered>();
+        }
+    }
+
+    [Test]
+    public void When_answer_with_spaces_in_text_answer_Should_remove_answer()
+    {
+        interview.AnswerTextQuestion(userId, questionId, RosterVector.Empty, DateTime.UtcNow, "value");
+        
+        using (EventContext eventContext = new EventContext())
+        {
+            interview.AnswerTextQuestion(userId, questionId, RosterVector.Empty, DateTime.UtcNow, "      ");
+
+            eventContext.ShouldNotContainEvent<TextQuestionAnswered>();
+            eventContext.ShouldContainEvent<AnswersRemoved>();
+        }
+    }
+}
