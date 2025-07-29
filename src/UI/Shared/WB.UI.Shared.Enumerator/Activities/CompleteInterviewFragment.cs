eb95e56474f36702012ea60ce5a180caa37eec5a
Pavel Stovpivskyi <spuvik@gmail.com>
2025-07-28 22:16:27 -0400

    fixed display binding

--- src/UI/Shared/WB.UI.Shared.Enumerator/Activities/CompleteInterviewFragment.cs
+++ src/UI/Shared/WB.UI.Shared.Enumerator/Activities/CompleteInterviewFragment.cs
@@ -29,7 +29,8 @@ namespace WB.UI.Shared.Enumerator.Activities
         
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
-            var view = inflater.Inflate(Resource.Layout.interview_complete, container, false);
+            var view = base.OnCreateView(inflater, container, savedInstanceState);
+            //var view = inflater.Inflate(Resource.Layout.interview_complete, container, false);
 
             tabLayout = view.FindViewById<TabLayout>(Resource.Id.tabLayout);
             viewPager = view.FindViewById<ViewPager2>(Resource.Id.viewPager);
