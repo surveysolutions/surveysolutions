9bb0592d821ad30d9b626f86f4a9887110d7d5b8
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2025-11-17 15:30:27 -0500

    fix crash on drawer

--- src/UI/Shared/WB.UI.Shared.Enumerator/CustomBindings/DashboardStatusBarColorByDashboardGroupTypeBinding.cs
+++ src/UI/Shared/WB.UI.Shared.Enumerator/CustomBindings/DashboardStatusBarColorByDashboardGroupTypeBinding.cs
@@ -1,7 +1,6 @@
 ﻿using Android.Graphics;
 using Android.Views;
 using AndroidX.Core.Content;
-using WB.Core.SharedKernels.DataCollection.ValueObjects.Interview;
 using WB.Core.SharedKernels.Enumerator.ViewModels.Dashboard;
 
 namespace WB.UI.Shared.Enumerator.CustomBindings
@@ -15,41 +14,59 @@ namespace WB.UI.Shared.Enumerator.CustomBindings
 
         protected override void SetValueToView(ViewGroup target, DashboardGroupType value)
         {
-            switch (value)
+            // Defer window modifications until after the current layout pass
+            // to prevent interrupting DrawerLayout measurement
+            target.Post(() =>
             {
-                case DashboardGroupType.Assignments:
-                    SetBackgroundColor(target, Resource.Color.assignmentsStatusBar);
-                    break;
-                
-                case DashboardGroupType.InvalidInterviews:
-                    SetBackgroundColor(target, Resource.Color.interviewStatusBarErrors);
-                    break;
+                switch (value)
+                {
+                    case DashboardGroupType.Assignments:
+                        SetBackgroundColor(target, Resource.Color.assignmentsStatusBar);
+                        break;
+                    
+                    case DashboardGroupType.InvalidInterviews:
+                        SetBackgroundColor(target, Resource.Color.interviewStatusBarErrors);
+                        break;
 
-                case DashboardGroupType.CompletedInterviews:
-                    SetBackgroundColor(target, Resource.Color.interviewStatusBarCompleted);
-                    break;
-                
-                case DashboardGroupType.None:
-                    SetBackgroundColor(target, Resource.Color.interviewStatusBarDisabled);
-                    break;
+                    case DashboardGroupType.CompletedInterviews:
+                        SetBackgroundColor(target, Resource.Color.interviewStatusBarCompleted);
+                        break;
+                    
+                    case DashboardGroupType.None:
+                        SetBackgroundColor(target, Resource.Color.interviewStatusBarDisabled);
+                        break;
 
-                case DashboardGroupType.InProgressInterviews:
-                case DashboardGroupType.WebInterviews:
-                default:
-                    SetBackgroundColor(target, Resource.Color.interviewStatusBarInProgress);
-                    break;
-            }
+                    case DashboardGroupType.InProgressInterviews:
+                    case DashboardGroupType.WebInterviews:
+                    default:
+                        SetBackgroundColor(target, Resource.Color.interviewStatusBarInProgress);
+                        break;
+                }
+            });
         }
 
         private static void SetBackgroundColor(ViewGroup target, int colorResourceId)
         {
-            var color = new Color(ContextCompat.GetColor(target.Context, colorResourceId));
+            try
+            {
+                var color = new Color(ContextCompat.GetColor(target.Context, colorResourceId));
 
-            var activity = (Activity)target.Context;
-            Window window = activity.Window;
-            window.AddFlags(WindowManagerFlags.DrawsSystemBarBackgrounds);
-            window.ClearFlags(WindowManagerFlags.TranslucentStatus);
-            window.SetStatusBarColor(color);
+                if (target.Context is Activity activity)
+                {
+                    Window window = activity.Window;
+                    if (window != null)
+                    {
+                        window.AddFlags(WindowManagerFlags.DrawsSystemBarBackgrounds);
+                        window.ClearFlags(WindowManagerFlags.TranslucentStatus);
+                        window.SetStatusBarColor(color);
+                    }
+                }
+            }
+            catch
+            {
+                // Silently fail if context is no longer valid
+            }
         }
     }
 }
+
