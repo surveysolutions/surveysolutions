d677ff36121804d9d24a287efae1e8ec10c9cfb7
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2025-11-26 12:34:26 -0500

    additional locations

--- src/UI/WB.UI.Frontend/src/webinterview/components/Complete.vue
+++ src/UI/WB.UI.Frontend/src/webinterview/components/Complete.vue
@@ -30,7 +30,8 @@
             </li>
         </ul>
 
-        <div v-if="hasAnyIssue" class="tab-content wrapper-info list-unstyled marked-questions" :class="activeGroup.cssClass">
+        <div v-if="hasAnyIssue" class="tab-content wrapper-info list-unstyled marked-questions"
+            :class="activeGroup.cssClass">
             <div class="tab-content-item" v-for="item in activeGroup.items" :key="item.id" @click="navigateTo(item)"
                 :class="{ 'critical-rule': item.type === 'critical-rule' }">
                 <a class="item-title" v-if="item.parentId || item.isPrefilled" href="javascript:void(0);"
@@ -44,7 +45,8 @@
                 </div>
             </div>
 
-            <div class="and-more" v-if="moreCount > 0">{{ $t('WebInterviewUI.Complete_AndMore', { count: moreCount }) }}</div>
+            <div class="and-more" v-if="moreCount > 0">{{ $t('WebInterviewUI.Complete_AndMore', { count: moreCount }) }}
+            </div>
         </div>
 
         <div class="wrapper-info note-supervisor">
@@ -291,6 +293,7 @@
     border-top: 1px solid #000000;
     padding-top: 40px;
     margin-top: 0px;
+    white-space: pre-line;
 }
 
 
@@ -413,7 +416,7 @@ export default {
             const criticalUnanswered = this.criticalityInfo?.unansweredCriticalQuestions || []
             const critical = criticalFailedRules.concat(criticalUnanswered)
             const criticalTotal = (this.criticalityInfo?.failedCriticalRulesTotal || 0)
-                                + (this.criticalityInfo?.unansweredCriticalQuestionsTotal || 0);
+                + (this.criticalityInfo?.unansweredCriticalQuestionsTotal || 0);
             groups.push({
                 title: this.$t('WebInterviewUI.Complete_Tab_CriticalErrors'),
                 items: critical,
