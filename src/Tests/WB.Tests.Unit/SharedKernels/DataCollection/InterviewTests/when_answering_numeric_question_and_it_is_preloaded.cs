09ab0f5bd33f918a1c6a92552d4b9e7cce9af3fa
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2026-03-03 11:29:00 -0500

    fix prefield question answer

---
+++ src/Tests/WB.Tests.Unit/SharedKernels/DataCollection/InterviewTests/when_answering_numeric_question_and_it_is_preloaded.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NUnit.Framework;
+using WB.Core.SharedKernels.DataCollection.Exceptions;
+using WB.Tests.Abc;
+
+namespace WB.Tests.Unit.SharedKernels.DataCollection.InterviewTests
+{
+    public class when_answering_numeric_question_and_it_is_preloaded : InterviewTestsContext
+    {
+        [Test]
+        public void should_throw_interview_exception()
+        {
+            var questionnaire =
+                Create.Entity.QuestionnaireDocumentWithOneChapter(Create.Entity.NumericIntegerQuestion(id: Id.g1));
+
+            var interview = CreateInterview(questionnaire);
+
+            interview.Apply(Create.Event.QuestionsMarkedAsReadonly( DateTime.Now,new[]{Create.Identity(Id.g1)}));
+
+            TestDelegate act = () => interview.AnswerNumericIntegerQuestion(Create.Command.AnswerNumericIntegerQuestionCommand(questionId: Id.g1));
+
+            Assert.That(act, Throws.Exception.InstanceOf<InterviewException>()
+                                   .And.Message.EqualTo("Answer cannot be changed for preloaded question."));
+        }
+    }
+}
