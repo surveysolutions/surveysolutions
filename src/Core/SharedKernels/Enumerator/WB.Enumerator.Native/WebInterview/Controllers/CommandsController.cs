7e0d93bcd5a2cffaa69a0377974aad7f7ef345e8
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2026-03-11 15:55:11 -0400

    one more check

--- src/Core/SharedKernels/Enumerator/WB.Enumerator.Native/WebInterview/Controllers/CommandsController.cs
+++ src/Core/SharedKernels/Enumerator/WB.Enumerator.Native/WebInterview/Controllers/CommandsController.cs
@@ -90,6 +90,9 @@ namespace WB.Enumerator.Native.WebInterview.Controllers
         {
             if (!TryGetIdentity(answerRequest, out var identity)) 
                 return BadRequest(new { errorMessage = InvalidRequestMessage });
+            
+            if (answerRequest.Answer == null)
+                return BadRequest(new { errorMessage = AnswerCannotBeNull });
 
             this.ExecuteQuestionCommand(new AnswerTextListQuestionCommand(interviewId,
                 this.GetCommandResponsibleId(interviewId), 
