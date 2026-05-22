17e1a3ce1d8fb970f4817786801bd28777365979
Pavel Stovpivskyi <spuvik@gmail.com>
2026-05-22 17:58:24 -0400

    Revert "Add support for multiple primary actions on dashboard cards"
    This reverts commit a751a04cc2fb9c6fa31829e89f7ff805b72e66f5.
    
    # Conflicts:
    #	src/Core/BoundedContexts/Supervisor/WB.Core.BoundedContexts.Supervisor/ViewModel/Dashboard/Items/SupervisorAssignmentDashboardItemViewModel.cs

--- src/Core/SharedKernels/Enumerator/Enumerator/ViewModels/Dashboard/ExpandableQuestionsDashboardItemViewModel.cs
+++ src/Core/SharedKernels/Enumerator/Enumerator/ViewModels/Dashboard/ExpandableQuestionsDashboardItemViewModel.cs
@@ -57,7 +57,6 @@ namespace WB.Core.SharedKernels.Enumerator.ViewModels.Dashboard
         private void CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
         {
             RaisePropertyChanged(nameof(PrimaryAction));
-            RaisePropertyChanged(nameof(PrimaryActions));
             RaisePropertyChanged(nameof(SecondaryAction));
             RaisePropertyChanged(nameof(ContextMenu));
             RaisePropertyChanged(nameof(HasContextMenu));
@@ -101,8 +100,7 @@ namespace WB.Core.SharedKernels.Enumerator.ViewModels.Dashboard
         protected List<PrefilledQuestion> IdentifyingData;
         protected List<PrefilledQuestion> DetailedIdentifyingData;
 
-        public ActionDefinition PrimaryAction => Actions.FirstOrDefault(a => a.ActionType == ActionType.Primary);
-        public IEnumerable<ActionDefinition> PrimaryActions => Actions.Where(a => a.ActionType == ActionType.Primary);
+        public ActionDefinition PrimaryAction => Actions.SingleOrDefault(a => a.ActionType == ActionType.Primary);
         public ActionDefinition SecondaryAction => Actions.SingleOrDefault(a => a.ActionType == ActionType.Secondary);
         public ActionDefinition ExtraAction => Actions.SingleOrDefault(a => a.ActionType == ActionType.Extra);
         public ActionDefinition TargetAreaAction => Actions.SingleOrDefault(a => a.ActionType == ActionType.TargetArea);
--- src/Core/SharedKernels/Enumerator/Enumerator/ViewModels/Dashboard/IDashboardItem.cs
+++ src/Core/SharedKernels/Enumerator/Enumerator/ViewModels/Dashboard/IDashboardItem.cs
@@ -36,7 +36,6 @@ namespace WB.Core.SharedKernels.Enumerator.ViewModels.Dashboard
 
         MvxObservableCollection<ActionDefinition> Actions { get; }
         ActionDefinition PrimaryAction {get;}
-        IEnumerable<ActionDefinition> PrimaryActions { get; }
         ActionDefinition SecondaryAction {get;}
         ActionDefinition TargetAreaAction {get;}
         IEnumerable<ActionDefinition> ContextMenu { get; }
