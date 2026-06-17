4ef6e1b9b69cb188652351cd2de1c9e3f1702a5f
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2026-06-17 12:51:05 -0400

    Update index.js

--- src/UI/WB.UI.Frontend/src/hqapp/components/index.js
+++ src/UI/WB.UI.Frontend/src/hqapp/components/index.js
@@ -1,35 +1,36 @@
 //import Vue from 'vue'
 
 import Confirm from './Confirm'
-import DataTables from './DataTables'
 import FilterBlock from './FilterBlock'
 import Filters from './Filters'
 import HqLayout from './HqLayout'
 import ModalFrame from './ModalFrame'
-import Typeahead from './Typeahead'
 import TextInput from './TextInput'
 import Checkbox from './Checkbox'
 import Radio from './Radio'
-import DatePicker from './DatePicker'
 import FormGroup from './FormGroup'
 import FilterInput from './FilterInput'
 import InlineSelector from './InlineSelector'
 import ExpandableList from './ExpandableList.vue'
 import Select from './Select.vue'
 
+const AsyncDataTables = () => import('./DataTables')
+const AsyncTypeahead = () => import('./Typeahead')
+const AsyncDatePicker = () => import('./DatePicker')
+
 export function registerComponents(vue) {
 
     vue.component('Confirm', Confirm)
-    vue.component('DataTables', DataTables)
+    vue.component('DataTables', AsyncDataTables)
     vue.component('FilterBlock', FilterBlock)
     vue.component('Filters', Filters)
     vue.component('HqLayout', HqLayout)
     vue.component('ModalFrame', ModalFrame)
-    vue.component('Typeahead', Typeahead)
+    vue.component('Typeahead', AsyncTypeahead)
     vue.component('TextInput', TextInput)
     vue.component('Checkbox', Checkbox)
     vue.component('Radio', Radio)
-    vue.component('DatePicker', DatePicker)
+    vue.component('DatePicker', AsyncDatePicker)
     vue.component('form-group', FormGroup)
     vue.component('FilterInput', FilterInput)
     vue.component('InlineSelector', InlineSelector)
