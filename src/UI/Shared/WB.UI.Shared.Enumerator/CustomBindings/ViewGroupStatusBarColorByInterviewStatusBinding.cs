b28ae064e4235d297b6f8b853c52dc60182fe857
Vitaliy Balabanov <vitaliy.balabanov@gmail.com>
2026-06-25 11:00:38 -0400

    try to fix crash

--- src/UI/Shared/WB.UI.Shared.Enumerator/CustomBindings/ViewGroupStatusBarColorByInterviewStatusBinding.cs
+++ src/UI/Shared/WB.UI.Shared.Enumerator/CustomBindings/ViewGroupStatusBarColorByInterviewStatusBinding.cs
@@ -14,37 +14,54 @@ namespace WB.UI.Shared.Enumerator.CustomBindings
 
         protected override void SetValueToView(ViewGroup target, GroupStatus value)
         {
-            switch (value)
+            target.Post(() =>
             {
-                case GroupStatus.CompletedInvalid:
-                case GroupStatus.StartedInvalid:
-                    SetBackgroundColor(target, Resource.Color.interviewStatusBarErrors);
-                    break;
+                switch (value)
+                {
+                    case GroupStatus.CompletedInvalid:
+                    case GroupStatus.StartedInvalid:
+                        SetBackgroundColor(target, Resource.Color.interviewStatusBarErrors);
+                        break;
 
-                case GroupStatus.Completed:
-                    SetBackgroundColor(target, Resource.Color.interviewStatusBarCompleted);
-                    break;
-                case GroupStatus.Disabled:
-                    SetBackgroundColor(target, Resource.Color.interviewStatusBarDisabled);
-                    break;
+                    case GroupStatus.Completed:
+                        SetBackgroundColor(target, Resource.Color.interviewStatusBarCompleted);
+                        break;
+                    case GroupStatus.Disabled:
+                        SetBackgroundColor(target, Resource.Color.interviewStatusBarDisabled);
+                        break;
 
-                case GroupStatus.Started:
-                case GroupStatus.NotStarted:
-                default:
-                    SetBackgroundColor(target, Resource.Color.interviewStatusBarInProgress);
-                    break;
-            }
+                    case GroupStatus.Started:
+                    case GroupStatus.NotStarted:
+                    default:
+                        SetBackgroundColor(target, Resource.Color.interviewStatusBarInProgress);
+                        break;
+                }
+            });
         }
 
         private static void SetBackgroundColor(ViewGroup target, int colorResourceId)
         {
-            var color = new Color(ContextCompat.GetColor(target.Context, colorResourceId));
+            try
+            {
+                var color = new Color(ContextCompat.GetColor(target.Context, colorResourceId));
 
-            var activity = (Activity)target.Context;
-            Window window = activity.Window;
-            window.AddFlags(WindowManagerFlags.DrawsSystemBarBackgrounds);
-            window.ClearFlags(WindowManagerFlags.TranslucentStatus);
-            window.SetStatusBarColor(color);
+                if (target.Context is not Activity activity)
+                    return;
+
+                Window window = activity.Window;
+                if (window == null)
+                    return;
+
+                window.AddFlags(WindowManagerFlags.DrawsSystemBarBackgrounds);
+                window.ClearFlags(WindowManagerFlags.TranslucentStatus);
+                window.SetStatusBarColor(color);
+            }
+            catch (ObjectDisposedException)
+            {
+            }
+            catch (InvalidOperationException)
+            {
+            }
         }
     }
 }
